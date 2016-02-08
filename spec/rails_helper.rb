07d4755ae1d55c203c14b29f5ff872af50723f0f
Shane Russell <shane1337@gmail.com>
2016-02-08 10:32:57 -0500

    Add sniffybara automated accessibility checks

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,6 +26,9 @@ require "rspec/rails"
 # If you are not using ActiveRecord, you can remove this line.
 # ActiveRecord::Migration.maintain_test_schema!
 
+require "capybara"
+Capybara.current_driver = :sniffybara
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
