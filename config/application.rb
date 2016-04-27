dd070bbb923cb58751f1b1958a50a8ff564807ac
Alex Yale-Loehr <ayaleloehr@gmail.com>
2016-04-27 08:32:28 -0400

    Add Rack::Deflater to gzip content automatically

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module RoadrunnerRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Gzip files when possible
+    config.middleware.use Rack::Deflater
   end
 end
