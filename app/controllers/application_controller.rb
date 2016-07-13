70019138af2deea5c45c8e2476de3c606341ec30
Shauni Deshmukh <ideshmukh@omb.eop.gov>
2016-07-13 15:02:24 -0400

    added git SHA response header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_app_info_headers
+
+  def set_app_info_headers
+  	headers['X-GitSHA'] = AppInfo::GIT_REVISION
+  end
 end
--- config/initializers/git_revision.rb
+++ config/initializers/git_revision.rb
@@ -1,8 +1,9 @@
 module AppInfo
-  #TODO: figure out where revision info is stored based on deployment
-  if Rails.env.production?
-    GIT_REVISION = "unknown"
-  else
+  if Rails.env.development?
     GIT_REVISION = `git rev-parse HEAD`.chomp
+  else
+    #TODO: git SHA available once devops PR #271 is merged:
+    #https://github.com/department-of-veterans-affairs/devops/pull/271
+    GIT_REVISION = "unknown"
   end
 end
