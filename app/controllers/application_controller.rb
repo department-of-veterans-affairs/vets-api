506691cac121a1e93bad907542010c40996f730a
Shauni Deshmukh <ideshmukh@omb.eop.gov>
2016-07-15 10:54:13 -0400

    added new status spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :set_app_info_headers
 
   def set_app_info_headers
-    headers["X-GitSHA"] = AppInfo::GIT_REVISION
+    headers["X-GitHub-Repository"] = "https://github.com/department-of-veterans-affairs/vets-api"
+    headers["X-Git-SHA"] = AppInfo::GIT_REVISION
   end
 end
--- config/initializers/git_revision.rb
+++ config/initializers/git_revision.rb
@@ -1,9 +1,9 @@
 module AppInfo
-  if Rails.env.development?
-    GIT_REVISION = `git rev-parse HEAD`.chomp
-  else
+  if Rails.env.production?
     #TODO: git SHA available once devops PR #271 is merged:
     #https://github.com/department-of-veterans-affairs/devops/pull/271
     GIT_REVISION = "unknown"
+  else
+    GIT_REVISION = `git rev-parse HEAD`.chomp
   end
 end
