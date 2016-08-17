c69b9888dee02924a907667378a57ad9f552fa9a
Lihan Li <frankieteardrop@gmail.com>
2016-08-17 18:18:10 -0400

    add spring test randomization fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,10 @@ end
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # fix for test rspec test randomization when using spring
+  # https://github.com/rails/spring/issues/113#issuecomment-135896880
+  config.seed = srand % 0xFFFF unless ARGV.any? { |arg| arg =~ /seed/ }
+
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
   # rspec-expectations config goes here. You can use an alternate
