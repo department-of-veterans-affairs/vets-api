6dbd905b041a9d8e6b34511c4f279de0b61b0cca
Lihan Li <frankieteardrop@gmail.com>
2016-08-24 12:59:43 -0400

    edu benefits claim uses activerecord now

--- app/models/education_benefits_claim.rb
+++ app/models/education_benefits_claim.rb
@@ -1,22 +1,9 @@
-class EducationBenefitsClaim < RedisStore
-  attribute :submitted_at, ActiveSupport::TimeWithZone
-  attribute :json, String
-  attribute :processed_at, ActiveSupport::TimeWithZone
-  attribute :uuid, String
-  alias redis_key uuid
-
-  after_initialize(:generate_uuid)
+class EducationBenefitsClaim < ActiveRecord::Base
   after_initialize(:set_submitted_at)
 
-  validates(:uuid, :json, :submitted_at, presence: true)
-
-  private
+  validates(:json, :submitted_at, presence: true)
 
   def set_submitted_at
     self.submitted_at ||= Time.zone.now
-  end
-
-  def generate_uuid
-    self.uuid ||= "#{self.class.to_s.underscore}:#{SecureRandom.uuid}"
   end
 end
