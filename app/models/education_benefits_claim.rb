117c0530e694b4fab85cee4c12d025374aea3b7d
Lihan Li <frankieteardrop@gmail.com>
2016-08-24 18:15:02 -0400

    put form schema in constant

--- app/models/education_benefits_claim.rb
+++ app/models/education_benefits_claim.rb
@@ -1,4 +1,6 @@
 class EducationBenefitsClaim < ActiveRecord::Base
+  FORM_SCHEMA = JSON.parse(File.read(Rails.root.join("app", "vets-json-schema", "dist", "edu-benefits-schema.json")))
+
   validates(:form, presence: true)
 
   # initially only completed claims are allowed, later we can allow claims that dont have a submitted_at yet
