348857851cd8b2b298935fb1f49c5ee665a30603
Austin <amartin857+github@gmail.com>
2016-08-24 15:11:28 -0700

    Remove whitespace

--- app/controllers/v0/claims_controller.rb
+++ app/controllers/v0/claims_controller.rb
@@ -1,7 +1,7 @@
 module V0
   class ClaimsController < ApplicationController
     skip_before_action :authenticate
-    
+
     def index
       render json: current_user.claims,
              serializer: ActiveModel::Serializer::CollectionSerializer,
