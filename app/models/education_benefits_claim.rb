4f6b0bfa2ef18aa91a0ba4ad1c2ac990fd4e540b
Lihan Li <frankieteardrop@gmail.com>
2016-08-24 18:45:31 -0400

    json schema validation of form

--- app/models/education_benefits_claim.rb
+++ app/models/education_benefits_claim.rb
@@ -2,9 +2,18 @@ class EducationBenefitsClaim < ActiveRecord::Base
   FORM_SCHEMA = JSON.parse(File.read(Rails.root.join("app", "vets-json-schema", "dist", "edu-benefits-schema.json")))
 
   validates(:form, presence: true)
+  validate(:form_matches_schema)
 
   # initially only completed claims are allowed, later we can allow claims that dont have a submitted_at yet
   before_validation(:set_submitted_at, on: :create)
+
+  private
+
+  def form_matches_schema
+    errors[:form].concat(JSON::Validator.fully_validate(FORM_SCHEMA, form))
+
+    true
+  end
 
   def set_submitted_at
     self.submitted_at = Time.zone.now
