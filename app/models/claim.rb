ce6a18d96956f5386fb7de4b1b14f2f59cf5693f
austin-martinez <amartin857+github@gmail.com>
2016-08-25 13:16:09 -0700

    Migrate benefits API to vets API (#41)
    * Migrate benefits API to vets API
    
    * Remove whitespace
    
    * Explicitly require external service
    
    * Freeze constant
    
    * Add lib to watchable dirs

---
+++ app/models/claim.rb
@@ -0,0 +1,21 @@
+require_dependency "evss/claims_service"
+
+class Claim < ActiveModelSerializers::Model
+  attr_accessor :id
+
+  EVSS_CLAIM_KEYS = %w(openClaims historicalClaims).freeze
+
+  def self.fetch_all(headers)
+    evss_client = EVSS::ClaimsService.new(headers)
+    raw_claims = evss_client.claims.body
+    EVSS_CLAIM_KEYS.each_with_object([]) do |key, claims|
+      next unless raw_claims[key]
+      claims << raw_claims[key].map do |raw_claim|
+        attrs = {
+          id: raw_claim["id"]
+        }
+        Claim.new(attrs)
+      end
+    end.flatten
+  end
+end
