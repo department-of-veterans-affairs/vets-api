8f2f4c814b05f2daa9eb74c62d96f8f7b47962e5
Shauni Deshmukh <ideshmukh@omb.eop.gov>
2016-08-26 11:04:48 -0400

    fix options routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
   before_action :authenticate
   before_action :set_app_info_headers
+  skip_before_action :authenticate, only: [:cors_preflight]
+
+  #TODO(shauni #45) - replace with rack-cors
+  def cors_preflight
+    head(:ok)
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  # TODO(#45): add rack-cors middleware to streamline CORS config
+  # Adding CORS preflight routes here for now to unblock front-end dev
+  match '/v0/*path', to: 'application#cors_preflight', via: [:options]
+
   get '/saml/metadata', to: 'saml#metadata'
   get '/auth/saml/callback', to: 'v0/sessions#saml_callback', module: 'v0'
   post '/auth/saml/callback', to: 'v0/sessions#saml_callback', module: 'v0'
@@ -7,13 +11,11 @@ Rails.application.routes.draw do
   namespace :v0, defaults: {format: 'json'} do
     resource :sessions, only: [:new, :destroy] do
       get :saml_callback, to: 'sessions#saml_callback'
-      match 'current', to: 'sessions#show', via: [:get, :options]
+      get 'current', to: 'sessions#show'
     end
 
-    # TODO(#45): add rack-cors middleware to streamline CORS config
-    # Adding :options here for now to unblock front-end dev
-    match 'user', to: 'users#show', via: [:get, :options]
-    match 'profile', to: 'users#show', via: [:get, :options]
+    get 'user', to: 'users#show'
+    get 'profile', to: 'users#show'
 
     resources :claims, only: [:index]
 
