c95b7a615b394b35ab774244b1ad1ef10407b4e1
Mark Olson <theothermarkolson@gmail.com>
2016-08-26 11:01:59 -0400

    Add a schema validation helper for the vets-json-schema submodule

--- spec/jobs/education_form/create_daily_spool_files_spec.rb
+++ spec/jobs/education_form/create_daily_spool_files_spec.rb
@@ -1,9 +1,21 @@
 require "rails_helper"
 
-RSpec.describe EducationForm::CreateDailySpoolFiles, type: :model do
+RSpec.describe EducationForm::CreateDailySpoolFiles, type: :model, form: :education_benefits do
   subject { described_class.new }
 
+  let(:application) do
+    {
+      fullName: {
+        first: "Mark",
+        last: "Olson"
+      }
+    }
+  end
+
   context "#format_application" do
+    it "uses conformant sample data in the tests" do
+      expect(application).to match_vets_schema("edu-benefits-schema")
+    end
     # TODO: Does it make sense to check against a known-good submission? Probably.
     it "formats a 22-1990 submission in textual form" do
       result = subject.format_application(form: "CH33_30", fullName: { first: "Mark", last: "Olson" })
