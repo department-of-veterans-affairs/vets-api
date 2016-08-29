e4db57321c56da71cb55fb0fa25e15aa8c171d8b
Kam Karshenas <kam@saneshark.com>
2016-08-29 16:54:07 -0400

    adding tracking-url links to tracking objects fixes #60

--- app/serializers/tracking_serializer.rb
+++ app/serializers/tracking_serializer.rb
@@ -5,6 +5,13 @@ class TrackingSerializer < ActiveModel::Serializer
 
   link(:self) { rx_v1_prescription_trackings_url(object.prescription_id) }
   link(:prescription) { rx_v1_prescription_url(object.prescription_id) }
+  link(:tracking_url) do
+    if object.delivery_service.upcase == 'UPS'
+      "https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=#{object.tracking_number}"
+    else
+      "https://tools.usps.com/go/TrackConfirmAction?tLabels=#{object.tracking_number}"
+    end
+  end
 
   attribute :tracking_number
   attribute :prescription_id
