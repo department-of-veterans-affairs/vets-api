12afaccbe2ba06594d0752126d81e6cb7e1a871a
Alastair Dawson <alastair@adhocteam.us>
2016-08-29 14:57:02 -0700

    mvi message builder adds find candidate body

--- lib/mvi/message_builder.rb
+++ lib/mvi/message_builder.rb
@@ -2,7 +2,7 @@ require 'ox'
 
 module MVI
   class MessageBuilder
-    attr_reader :message
+    attr_reader :doc
 
     SITE_KEY = 'ABC123'.freeze
     ROOT_ID = '2.16.840.1.113883.4.349'.freeze
@@ -14,34 +14,20 @@ module MVI
     }.freeze
 
     def initialize
-      @message = Ox::Document.new(:version => '1.0')
+      @doc = Ox::Document.new(:version => '1.0')
     end
 
-    def build_find_candidate(vcid, first, last, dob, ssn)
-      self.header(vcid, EXTENSIONS[:find_candidate])
-      Ox.dump(@message)
+    def build_find_candidate(vcid, first_name, last_name, dob, ssn)
+      @message = xml_tag('PRPA_IN201305UV02')
+      header(vcid, EXTENSIONS[:find_candidate])
+      find_candidate_body(parameter_list(first_name, last_name, dob, ssn))
+      @doc << @message
+      Ox.dump(@doc)
     end
-
-    # Generates a header like:
-    #
-    # <id root="2.16.840.1.113883.3.933" extension="MCID‐12345" />
-    # <creationTime value="20070810140900" />
-    # <interactionId root="2.16.840.1.113883.1.6" extension="PRPA_IN201309UV02" />
-    # <processingCode code="T" />
-    # <processingModeCode code="T" />
-    # <acceptAckCode code="AL" />
-    # <receiver typeCode="RCV">
-    #   <device classCode="DEV" determinerCode="INSTANCE"> <id root="2.16.840.1.113883.4.349" />
-    #   </device>
-    # </receiver>
-    # <sender typeCode="SND">
-    #   <device classCode="DEV" determinerCode="INSTANCE">
-    #   <id root="2.16.840.1.113883.3.933" extension="SITEKEY_OBTAINED_FROM_MVI"/> </device>
-    # </sender>
 
     def header(vcid, extension)
       @message << element('id', root: ROOT_ID, extension: "#{vcid}#{INTERACTION_ID}")
-      @message << element('creationTime', value: Time.now.strftime.utc('%Y%m%d%M%H%M%S'))
+      @message << element('creationTime', value: Time.now.utc.strftime('%Y%m%d%M%H%M%S'))
       @message << element('interactionId', root: ROOT_ID, extension: extension)
       @message << element('processingCode', code: Rails.env.production? ? 'P' : 'D')
       @message << element('processingModeCode', code: 'T')
@@ -60,10 +46,74 @@ module MVI
       @message << sender
     end
 
+    private
+
     def element(name, attrs = nil)
       el = Ox::Element.new(name)
       return el unless attrs
-      attrs.each { |k, v| el[k] = v }
+      attrs.each { |k, v| k == :text! ? el.replace_text(v) : el[k] = v }
+      el
+    end
+
+    def xml_tag(operation_id)
+      element(operation_id,
+        xmlns: 'urn:hl7‐org:v3',
+        :'xmlns:ps' => 'http://vaww.oed.oit.va.gov',
+        :'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema‐instance",
+        :'xsi:schemaLocation' => 'urn:hl7‐org:v3 ../../schema/HL7V3/NE2008/multicacheschemas/PRPA_IN201305UV02.xsd',
+        ITSVersion: 'XML_1.0'
+      )
+    end
+
+    def find_candidate_body(parameter_list)
+      control_act_process = control_act_process()
+      query_by_parameter = query_by_parameter()
+      query_by_parameter << parameter_list
+      control_act_process << query_by_parameter
+      @message << control_act_process
+    end
+
+    def query_by_parameter
+      el = element('queryByParameter')
+      el << element('queryId', root: '2.16.840.1.113883.3.933', extension: '18204')
+      el << element('statusCode', code: 'new')
+      el << element('initialValue', value: 1)
+      el
+    end
+
+    def parameter_list(first_name, last_name, dob, ssn)
+      el = element('parameterList')
+      el << living_subject_name(first_name, last_name)
+      el << living_subject_birth_time(dob)
+      el << living_subject_id(ssn)
+      el
+    end
+
+    def control_act_process
+      el = element('controlActProcess', classCode: 'CACT', moodCode: 'EVN')
+      el << element('code', code: 'PRPA_TE201305UV02', codeSystem: '2.16.840.1.113883.1.6')
+      el
+    end
+
+    def living_subject_name(first_name, last_name)
+      el = element('livingSubjectName')
+      value = element('value', use: 'L')
+      value << element('given', text!: first_name)
+      value << element('family', text!: last_name)
+      el << value
+    end
+
+    def living_subject_birth_time(dob)
+      el = element('livingSubjectBirthTime')
+      el << element('value', value: dob.strftime('%Y%m%d'))
+      el << element('semanticsText', text!: 'LivingSubject..birthTime')
+      el
+    end
+
+    def living_subject_id(ssn)
+      el = element('livingSubjectId')
+      el << element('value', root: '2.16.840.1.113883.4.1', extention: ssn)
+      el << element('semanticsText', text!: 'SSN')
       el
     end
   end
