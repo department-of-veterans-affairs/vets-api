dff68da4e06faec9149440bdb628a0a7b2a639bf
Kam Karshenas <kam@saneshark.com>
2016-08-30 02:33:57 -0400

    fix deprecation warnings

--- spec/support/serializer_spec_helper.rb
+++ spec/support/serializer_spec_helper.rb
@@ -2,7 +2,7 @@ module SerializerSpecHelper
   def serialize(obj, opts = {})
     serializer_class = opts.delete(:serializer_class) || "#{obj.class.name}Serializer".constantize
     serializer = serializer_class.send(:new, obj)
-    adapter = ActiveModel::Serializer::Adapter.create(serializer, opts)
+    adapter = ActiveModelSerializers::Adapter.create(serializer, opts)
     adapter.to_json
   end
 end
