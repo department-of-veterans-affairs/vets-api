a05d9ce1459cf91857d989668982da38b765a28a
Lihan Li <frankieteardrop@gmail.com>
2016-09-01 14:57:54 -0400

    add olivebranch and use header in spec

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'va_rx', git: 'https://github.com/department-of-veterans-affairs/va_rx.git',
 gem 'faraday'
 gem 'faraday_middleware'
 gem 'httpclient'
+gem 'olive_branch'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -51,6 +52,7 @@ group :test do
   gem 'timecop'
   gem 'json-schema'
   gem 'vcr'
+  gem 'awrence'
 end
 
 group :development do
--- spec/controllers/v0/education_benefits_claims_controller_spec.rb
+++ spec/controllers/v0/education_benefits_claims_controller_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe V0::EducationBenefitsClaimsController, type: :controller do
+RSpec.describe V0::EducationBenefitsClaimsController, type: :request do
   describe 'POST create' do
     subject do
-      post(:create, params)
+      post(v0_education_benefits_claims_path, params.to_json, { 'CONTENT_TYPE' => 'application/json', 'HTTP_X_KEY_INFLECTION' => 'camel' })
     end
 
     context 'with valid params' do
       let(:params) do
         {
-          education_benefits_claim: {
+          educationBenefitsClaim: {
             form: { chapter33: true }
           }
         }
@@ -23,14 +23,14 @@ RSpec.describe V0::EducationBenefitsClaimsController, type: :controller do
 
       it 'should render json of the new model' do
         subject
-        expect(response.body).to eq(EducationBenefitsClaim.last.to_json)
+        expect(response.body).to eq(EducationBenefitsClaim.last.attributes.to_camelback_keys.to_json)
       end
     end
 
     context 'with invalid params' do
       let(:params) do
         {
-          education_benefits_claim: { form: nil }
+          educationBenefitsClaim: { form: nil }
         }
       end
 
@@ -39,7 +39,7 @@ RSpec.describe V0::EducationBenefitsClaimsController, type: :controller do
 
         expect(response.code).to eq('400')
         expect(response.body).to eq(
-          EducationBenefitsClaim.new(params[:education_benefits_claim]).tap(&:valid?).errors.to_json
+          EducationBenefitsClaim.new(params[:educationBenefitsClaim]).tap(&:valid?).errors.to_json
         )
       end
     end
