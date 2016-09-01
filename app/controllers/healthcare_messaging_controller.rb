234f3d2def1532a0b93c676fa06a092472dbb57d
mphprogrammer <marc.harbatkin@va.gov>
2016-09-01 13:17:09 -0400

    Completed triage teams specs

--- app/controllers/healthcare_messaging_controller.rb
+++ app/controllers/healthcare_messaging_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 class HealthcareMessagingController < ApplicationController
-  attr_reader :client, :mhv_correlation_id
-
   # FIXME: when ID.me is working we need to use it here, but for now skip
   #   and just rely on the http basic authentication.
   skip_before_action :authenticate
@@ -13,14 +11,13 @@ class HealthcareMessagingController < ApplicationController
   ## Instantiates a new configuration used in establishing a session.
   ######################################################################################################################
   MHV_CONFIG = VAHealthcareMessaging::Configuration.new(
-    host: ENV["MHV_SM_HOST"],
-    app_token: ENV["MHV_SM_APP_TOKEN"],
+    host: ENV['MHV_SM_HOST'],
+    app_token: ENV['MHV_SM_APP_TOKEN'],
     enforce_ssl: Rails.env.production?
   ).freeze
 
   ######################################################################################################################
   ## Pagingation constants
-  ## TODO - uncomment when ready.
   ######################################################################################################################
   DEFAULT_PER_PAGE = 10
   MAXIMUM_PER_PAGE = 100
@@ -32,7 +29,16 @@ class HealthcareMessagingController < ApplicationController
   ## Establishes a session using the MHV correlation id for authentication.
   ######################################################################################################################
   def client
-    @client ||= VAHealthcareMessaging::Client.new(config: MHV_CONFIG, session: { user_id: @mhv_correlation_id })
+    @client ||= VAHealthcareMessaging::Client.new(config: MHV_CONFIG, session: { user_id: correlation_id })
+  end
+
+  ######################################################################################################################
+  ## correlation_id
+  ## Abstracting out how correlation id is obtained in the id.me world.
+  ## TODO: recode once id.me is established.
+  ######################################################################################################################
+  def correlation_id
+    @mhv_correlation_id ||= params[:id]
   end
 
   ######################################################################################################################
@@ -40,7 +46,6 @@ class HealthcareMessagingController < ApplicationController
   ## Establishes a session using the MHV correlation id for authentication.
   ######################################################################################################################
   def authenticate_client
-    @mhv_correlation_id = params[:id]
     client.authenticate if client.session.expired?
   end
 end
--- spec/serializers/triage_team_serializer_spec.rb
+++ spec/serializers/triage_team_serializer_spec.rb
@@ -1,25 +1,26 @@
-require "rails_helper"
+# frozen_string_literal: true
+require 'rails_helper'
 
 RSpec.describe TriageTeamSerializer, type: :serializer do
   let(:triage_team) { build :triage_team }
-  let(:data) { JSON.parse(subject)["data"] }
-  let(:attributes) { data["attributes"] }
+  let(:data) { JSON.parse(subject)['data'] }
+  let(:attributes) { data['attributes'] }
 
   subject { serialize(triage_team, serializer_class: described_class) }
 
-  it "should include id" do
-    expect(data["id"].to_i).to eq(triage_team.triage_team_id)
+  it 'should include id' do
+    expect(data['id'].to_i).to eq(triage_team.triage_team_id)
   end
 
-  it "should include triage_team_id" do
-    expect(attributes["triage-team-id"].to_i).to eq(triage_team.triage_team_id)
+  it 'should include triage_team_id' do
+    expect(attributes['triage-team-id'].to_i).to eq(triage_team.triage_team_id)
   end
 
   it "should include the team's name" do
-    expect(attributes["name"]).to eq(triage_team.name)
+    expect(attributes['name']).to eq(triage_team.name)
   end
 
   it "should include the team's patient relationship type" do
-    expect(attributes["relation-type"]).to eq(triage_team.relation_type)
+    expect(attributes['relation-type']).to eq(triage_team.relation_type)
   end
 end
