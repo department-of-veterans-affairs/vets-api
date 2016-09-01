9b70de1bf6a6d6bc6b713b1540bd8fa9087b4687
Lihan Li <frankieteardrop@gmail.com>
2016-09-01 15:03:33 -0400

    move request spec to request dir

--- spec/request/education_benefits_claims_request_spec.rb
+++ spec/request/education_benefits_claims_request_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe V0::EducationBenefitsClaimsController, type: :request do
+RSpec.describe 'Education Benefits Claims Integration', type: :request do
   describe 'POST create' do
     subject do
       post(
