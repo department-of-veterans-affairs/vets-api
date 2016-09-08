5d50a5987607f6708a15132a23f4c2e38a44cdde
Alastair Dawson <alastair@adhocteam.us>
2016-09-07 17:21:37 -0700

    completed mvi service find candidate endpoint

--- lib/mvi/service.rb
+++ lib/mvi/service.rb
@@ -1,3 +1,5 @@
+require 'savon'
+
 module MVI
   class Service
     def self.add_person
@@ -7,6 +9,13 @@ module MVI
     end
 
     def self.find_candidate(vcid, first_name, last_name, dob, ssn)
+      message_builder = MVI::Messages::FindCandidateMessage.new
+      message = message_builder.build(vcid, first_name, last_name, dob, ssn)
+      client.call(MVI::Messages::FindCandidateMessage::EXTENSION, message: message)
+    end
+
+    def self.client
+      @client ||= Savon.client(wsdl: ENV['MVI_WSDL'])
     end
   end
 end
