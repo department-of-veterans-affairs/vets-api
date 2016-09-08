a5cb29e34a8563a0422d101f0d6b9e704ecfde8a
Lihan Li <frankieteardrop@gmail.com>
2016-09-08 16:52:54 -0400

    spool file methods should be public

--- app/models/education_benefits_claim.rb
+++ app/models/education_benefits_claim.rb
@@ -9,16 +9,6 @@ class EducationBenefitsClaim < ActiveRecord::Base
   # initially only completed claims are allowed, later we can allow claims that dont have a submitted_at yet
   before_validation(:set_submitted_at, on: :create)
 
-  private
-
-  def form_matches_schema
-    errors[:form].concat(JSON::Validator.fully_validate(FORM_SCHEMA, form))
-  end
-
-  def set_submitted_at
-    self.submitted_at = Time.zone.now
-  end
-
   # This converts the form data into an OpenStruct object so that the template
   # rendering can be cleaner. Piping it through the JSON serializer was a quick
   # and easy way to deeply transform the object.
@@ -35,5 +25,15 @@ class EducationBenefitsClaim < ActiveRecord::Base
   # TODO: Add logic for determining field type(s) that need to be places in the application header
   def application_type
     return 'CH1606' if @application.chapter1606
+  end
+
+  private
+
+  def form_matches_schema
+    errors[:form].concat(JSON::Validator.fully_validate(FORM_SCHEMA, form))
+  end
+
+  def set_submitted_at
+    self.submitted_at = Time.zone.now
   end
 end
