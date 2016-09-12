6d6d6dc1da57fa4904ab1d3c5680ef5202c69a02
Carl Johnson <me@carlmjohnson.net>
2016-09-12 16:28:01 -0400

    Use a common VCR cassette

--- spec/request/documents_spec.rb
+++ spec/request/documents_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe 'Documents management', type: :request do
   end
 
   it 'should upload a file' do
-    VCR.use_cassette('evss/documents/create') do
+    VCR.use_cassette('evss/documents/upload') do
       doctors_note = fixture_file_upload(
         "#{::Rails.root}/spec/fixtures/files/doctors-note.pdf",
         'application/pdf'
       )
       params = { file: doctors_note, tracked_item: 33 }
-      post '/v0/claims/3/documents', params
+      post '/v0/claims/189625/documents', params
       expect(response).to be_success
       expect(response.body).to be_empty
     end
