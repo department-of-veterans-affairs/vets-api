26ec8af5f9059fc40e708051451f899707fe9a97
Carl Johnson <me@carlmjohnson.net>
2016-09-12 14:24:06 -0400

    Fix test
    Still not resolved because now I'm not getting a success message from EVSS

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_dependency 'evss/claims_service'
+require_dependency 'evss/documents_service'
 
 class Claim < ActiveModelSerializers::Model
   attr_accessor :id
--- spec/request/documents_spec.rb
+++ spec/request/documents_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe 'Documents management', type: :request do
     post '/v0/claims/1/documents'
     expect(response).to_not be_success
   end
+
+  it 'should upload a file' do
+    VCR.use_cassette('evss/documents/create') do
+      doctors_note = fixture_file_upload(
+        "#{::Rails.root}/spec/fixtures/files/doctors-note.pdf",
+        'application/pdf'
+      )
+      params = { file: doctors_note, tracked_item: 33 }
+      post '/v0/claims/3/documents', params
+      expect(response).to be_success
+      expect(response.body).to be_empty
+    end
+  end
 end
