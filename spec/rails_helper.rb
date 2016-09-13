6790b321d38523473cc8a4e169dc4866c1714653
Alastair Dawson <alastair@adhocteam.us>
2016-09-13 15:24:02 -0700

    added xml matchers, mvi find candidate build validation specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,32 @@ require 'support/api_schema_matcher'
 require 'support/validation_helpers'
 
 WebMock.disable_net_connect!(allow_localhost: true)
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+#
+# The following line is provided for convenience purposes. It has the downside
+# of increasing the boot-up time by auto-requiring all files in the support
+# directory. Alternatively, in the individual `*_spec.rb` files, manually
+# require only the support files necessary.
+#
+# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+# Checks for pending migration and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+# ActiveRecord::Migration.maintain_test_schema!
+
+# TODO: https://github.com/department-of-veterans-affairs/platform-team/issues/78
+# Figure out how to allow rspec to route external URLs, so this actually matters:
+# WebMock.disable_net_connect!(allow_localhost: true)
+require "vcr"
+require "json-schema"
+require "support/api_schema_matcher"
+require 'support/xml_matchers'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/support/vcr_cassettes'
