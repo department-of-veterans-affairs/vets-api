510f941e3bb687514fcaa59c37ee728a81d83422
Alastair Dawson <alastair@adhocteam.us>
2016-09-13 17:26:38 -0700

    converted message builder .build to class method

--- spec/lib/mvi/service_spec.rb
+++ spec/lib/mvi/service_spec.rb
@@ -9,19 +9,17 @@ describe MVI::Service do
   before(:all) { savon.mock! }
   after(:all)  { savon.unmock! }
 
-  let(:vcid) { 'abc123' }
-  let(:first_name) { 'john' }
-  let(:last_name) { 'smith' }
+  let(:first_name) { 'John' }
+  let(:last_name) { 'Smith' }
   let(:dob) { Time.new(1980, 1, 1) }
-  let(:ssn) { '555-43-2222' }
+  let(:ssn) { '555-44-3333' }
 
   describe ".find_candidate" do
-    it "authenticates the user with the service" do
-      message_builder = MVI::Messages::FindCandidateMessage.new
-      message = message_builder.build(vcid, first_name, last_name, dob, ssn)
-      expected = File.read('spec/support/mvi/find_candidate_response.xml')
-      savon.expects(MVI::Messages::FindCandidateMessage::EXTENSION).with(message: message).returns(expected)
-      response = MVI::Service.find_candidate(vcid, first_name, last_name, dob, ssn)
+    it "calls the prpa_in201305_uv02 endpoint with a find candidate message" do
+      message = MVI::Messages::FindCandidateMessage.build(first_name, last_name, dob, ssn)
+      expected = File.read("#{ENV['MVI_FILE_PATH']}/spec/support/find_candidate_response.xml")
+      savon.expects(:prpa_in201305_uv02).with(xml: message).returns(expected)
+      response = MVI::Service.find_candidate(first_name, last_name, dob, ssn)
       expect(response).to be_successful
     end
   end
