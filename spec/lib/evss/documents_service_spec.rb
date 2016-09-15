b0cee1497699ef3cdbaad11cb55403bc76ccaf2f
Carl Johnson <carl@adhocteam.us>
2016-09-15 13:51:41 -0400

    EVSS service specs: Use sample claimant VAAFI headers (#107)
    * EVSS service specs: Use sample claimant VAAFI headers
    
    I was trying to test the endpoint with a different sample user and then confused why nothing changed: because we didn't have the sample user in the test but a copy of the headers!
    
    * Add comment to va_eauth_authenticationmethod

--- spec/lib/evss/documents_service_spec.rb
+++ spec/lib/evss/documents_service_spec.rb
@@ -4,22 +4,7 @@ require_dependency 'evss/documents_service'
 
 describe EVSS::DocumentsService do
   let(:vaafi_headers) do
-    {
-      'va_eauth_pnidtype' => 'SSN',
-      'va_eauth_csid' => 'DSLogon',
-      'va_eauth_firstName' => 'Jane',
-      'va_eauth_lastName' => 'Doe',
-      'va_eauth_authenticationauthority' => 'eauth',
-      'iv-user' => 'dslogoneauthuser',
-      'va_eauth_emailAddress' => 'jane.doe@va.gov',
-      'va_eauth_birthdate' => '1999-10-09T08:06:12-04:00',
-      'va_eauth_pid' => '123456789',
-      'va_eauth_issueinstant' => '2015-04-17T14:52:48Z',
-      'va_eauth_dodedipnid' => '1105051936',
-      'va_eauth_middleName' => 'A',
-      'va_eauth_authenticationmethod' => 'DSLogon',
-      'va_eauth_assurancelevel' => '2'
-    }
+    User.sample_claimant.vaafi_headers
   end
 
   subject { described_class.new(vaafi_headers) }
