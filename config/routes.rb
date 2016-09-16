59404fc09b66e924e281a1aa8844fb361755ba31
Lihan Li <frankieteardrop@gmail.com>
2016-09-16 16:16:23 -0400

    sidekiq web interface with http auth

--- config/routes.rb
+++ config/routes.rb
@@ -48,4 +48,10 @@ Rails.application.routes.draw do
   end
 
   root 'v0/example#index', module: 'v0'
+
+  require "sidekiq/web"
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 end
