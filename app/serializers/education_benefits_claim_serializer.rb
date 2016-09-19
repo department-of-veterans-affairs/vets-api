b9e121f91a60e448710bd520be8759808ef2430b
Lihan Li <frankieteardrop@gmail.com>
2016-09-19 15:48:50 -0400

    add regional office to serializer

--- app/serializers/education_benefits_claim_serializer.rb
+++ app/serializers/education_benefits_claim_serializer.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 class EducationBenefitsClaimSerializer < ActiveModel::Serializer
-  attributes :id, :form, :submitted_at
+  attributes :id, :form, :submitted_at, :regional_office
 end
