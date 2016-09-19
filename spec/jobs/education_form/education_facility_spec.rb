16dc3380a7f86c133ad64cad3b17f8a5ec8788ab
Lihan Li <frankieteardrop@gmail.com>
2016-09-19 14:47:44 -0400

    regional office for eastern addr

--- spec/jobs/education_form/education_facility_spec.rb
+++ spec/jobs/education_form/education_facility_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe EducationForm::EducationFacility, type: :model do
-  let(:record) {  }
+  let(:education_benefits_claim) { build(:education_benefit_claim) }
+  let(:record) do
+    education_benefits_claim.open_struct_form
+  end
+
+  describe '#regional_office_for' do
+    context 'with an eastern address' do
+      before do
+        new_form = education_benefits_claim.parsed_form
+        new_form['school']['address']['state'] = 'VA'
+        education_benefits_claim.form = new_form.to_json
+      end
+
+      it 'should return the right address' do
+        expect(described_class.regional_office_for(record)).to eq("Eastern Region\nVA Regional Office\nP.O. Box 4616\nBuffalo, NY 14240-4616")
+      end
+    end
+  end
 end
