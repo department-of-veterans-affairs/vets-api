1b6a5c8343429acfbf9f23c858f27aed24f40c92
Kam Karshenas <kam@saneshark.com>
2016-09-19 18:34:45 -0500

    rubocop is so stylish

--- lib/sm/api/sessions.rb
+++ lib/sm/api/sessions.rb
@@ -8,8 +8,8 @@ module SM
         req_headers = env.request_headers
         res_headers = env.response_headers
         SM::ClientSession.new(user_id: req_headers['mhvCorrelationId'],
-                                                 expires_at: res_headers['expires'],
-                                                 token: res_headers['token'])
+                              expires_at: res_headers['expires'],
+                              token: res_headers['token'])
       end
     end
   end
