3d18eea0239a42951e17f3c93ac8182df9346e45
Kam Karshenas <kam@saneshark.com>
2016-09-21 12:00:31 -0500

    fixing environment and initializers

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Rails.application.routes.draw do
   root 'v0/example#index', module: 'v0'
 
   if Rails.env.development? || (ENV['SIDEKIQ_ADMIN_PANEL'] == 'true')
-    require "sidekiq/web"
-    mount Sidekiq::Web, at: "/sidekiq"
+    require 'sidekiq/web'
+    mount Sidekiq::Web, at: '/sidekiq'
   end
 end
