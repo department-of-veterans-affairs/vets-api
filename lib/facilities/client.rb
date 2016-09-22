1c1f7cabafb13693401d9f1ca686483d1fcfab18
Patrick Vinograd <patrick@adhocteam.us>
2016-09-22 14:08:48 -0700

    Rubocop

--- lib/facilities/client.rb
+++ lib/facilities/client.rb
@@ -1,40 +1,40 @@
+# frozen_string_literal: true
 require 'geoservices'
 require 'openssl'
 
-# TODO PV: Obviously this is not useful, but maps.va.gov has a self-signed cert
+# TODO: PV: Obviously this is not useful, but maps.va.gov has a self-signed cert
 # so try this for testing purposes.
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 module Facilities
   class Client
-
     def initialize(url:, layer:, id_field:)
       @url = url
       @layer = layer
       @id_field = id_field
-      @service = Geoservice::MapService.new(:url => @url)
+      @service = Geoservice::MapService.new(url: @url)
     end
 
     def query(bbox:, where: nil)
       params = {
         geometry: bbox,
-        geometryType: "esriGeometryEnvelope",
+        geometryType: 'esriGeometryEnvelope',
         where: where,
         inSR: 4326,
         outSR: 4326,
         returnGeometry: true,
         returnCountOnly: false,
-        outFields: "*",
-        resultRecordCount: nil, #limit, numeric
+        outFields: '*',
+        resultRecordCount: nil, # limit, numeric
         returnDistinctValues: false,
-        orderByFields: "StationID",
+        orderByFields: 'StationID'
       }
       response = @service.query(@layer, params)
-      response["features"] 
+      response['features']
     end
 
     def get(identifier:)
-      # TODO ActiveRecord.Santizers.ClassMethods.santize_sql_for_conditions
+      # TODO: ActiveRecord.Santizers.ClassMethods.santize_sql_for_conditions
       # looks stronger for complete where clause, but is protected
       where_clause = "#{@id_field}=#{ActiveRecord::Base.sanitize(identifier)}"
       params = {
@@ -43,15 +43,13 @@ module Facilities
         outSR: 4326,
         returnGeometry: true,
         returnCountOnly: false,
-        outFields: "*",
-        resultRecordCount: nil, #limit, numeric
+        outFields: '*',
+        resultRecordCount: nil, # limit, numeric
         returnDistinctValues: false,
-        orderByFields: "StationID",
+        orderByFields: 'StationID'
       }
       response = @service.query(@layer, params)
-      response["features"] 
-         
+      response['features']
     end
-
   end
 end
--- spec/controllers/v0/facilities/va_controller_spec.rb
+++ spec/controllers/v0/facilities/va_controller_spec.rb
@@ -1,5 +1,5 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe V0::Facilities::VaController, type: :controller do
-
 end
