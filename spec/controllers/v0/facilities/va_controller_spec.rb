f9ac56164c899acc5869cef2c928e5947789b36c
Patrick Vinograd <patrick@adhocteam.us>
2016-09-22 16:41:43 -0700

    Rspecs

--- spec/controllers/v0/facilities/va_controller_spec.rb
+++ spec/controllers/v0/facilities/va_controller_spec.rb
@@ -2,4 +2,10 @@
 require 'rails_helper'
 
 RSpec.describe V0::Facilities::VaController, type: :controller do
+  context 'with bad service param' do
+    it 'returns 400' do
+      post :index, services: ['OilChange']
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
 end
