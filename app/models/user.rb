919844330eaa90771295538e34eeae5984fecd5f
Alastair Dawson <alastair@adhocteam.us>
2016-09-26 17:12:50 -0700

    added mock mvi service

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 require 'common/models/base'
+require 'mvi/messages/find_candidate_message'
+require 'mvi/service'
 
 class User < RedisStore
   NAMESPACE = REDIS_CONFIG['user_store']['namespace']
@@ -10,12 +12,17 @@ class User < RedisStore
   attribute :uuid
   attribute :email
   attribute :first_name
+  attribute :middle_name
   attribute :last_name
+  attribute :gender
+  attribute :dob
   attribute :zip
 
-  # vaafi attributes
+  # vaafi/mvi attributes
   attribute :last_signed_in, Common::UTCTime
   attribute :edipi
+  attribute :icn
+  attribute :mhv_id
   attribute :participant_id
   attribute :ssn
 
@@ -25,9 +32,34 @@ class User < RedisStore
   validates :uuid, presence: true
   validates :email, presence: true
 
+  after_initialize :fetch_mvi_data
+
   def self.sample_claimant
     attrs = JSON.load(ENV['EVSS_SAMPLE_CLAIMANT_USER'])
     attrs[:last_signed_in] = Time.now.utc
     User.new attrs
+  end
+
+  def fetch_mvi_data
+    unless mvi_ids?
+      message = MVI::Messages::FindCandidateMessage.new(
+        [first_name, middle_name],
+        last_name,
+        dob,
+        ssn,
+        gender
+      )
+      response = MVI::Service.find_candidate(message)
+      self.edipi = response[:edipi]
+      self.icn = response[:icn]
+      self.mhv_id = response[:mhv_id]
+      save
+    end
+  rescue MVI::ServiceError => e
+    logger.error "service error: #{e.message} retrieving MVI data for user: #{uuid}"
+  end
+
+  def mvi_ids?
+    [edipi, icn, mhv_id].all? { |id| !id.nil? }
   end
 end
