0419ea87170a0d0c9caedf6a242f10edafd82b84
Austin <amartin857+github@gmail.com>
2016-09-27 18:09:10 -0700

    Document ENV vars for S3

--- spec/uploaders/disability_claim_document_uploader_spec.rb
+++ spec/uploaders/disability_claim_document_uploader_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe DisabilityClaimDocumentUploader do
 
   describe '#store!' do
     it 'raises an error when the file is larger than 25 megabytes' do
-      file = double(size: 25.megabytes+1)
+      file = double(size: 25.megabytes + 1)
       expect { subject.store!(file) }.to raise_error(CarrierWave::UploadError)
     end
   end
