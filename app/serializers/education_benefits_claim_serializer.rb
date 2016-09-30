eb55e4d1ceba139fdac6f05e905b5c85eb1d5552
lihan <frankieteardrop@gmail.com>
2016-09-30 13:25:10 -0400

    Add confirmation number (#161)
    * add confirmation number method
    
    * add confirmation number to serializer
    
    * add confirmation number to api doc
    
    * lint
    
    * change test to lookup claim
    
    * include confirmation number in spool
    
    * fix vets json schema commit

--- app/serializers/education_benefits_claim_serializer.rb
+++ app/serializers/education_benefits_claim_serializer.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 class EducationBenefitsClaimSerializer < ActiveModel::Serializer
-  attributes :id, :form, :submitted_at, :regional_office
+  attributes :id, :form, :submitted_at, :regional_office, :confirmation_number
 end
