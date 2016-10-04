b0f62d3fb7861553faebd22acd547d6ba6d825ce
Mark Olson <theothermarkolson@gmail.com>
2016-10-04 12:56:16 -0400

    Remove TIFF generation from 22-1990 preview endpoint

--- app/controllers/v0/education_benefits_claims_controller.rb
+++ app/controllers/v0/education_benefits_claims_controller.rb
@@ -15,23 +15,11 @@ module V0
     # defense in depth. This functionality should not be included once EducationForm is
     # released in production, but rather should be available in a dedicated admin interface
     def show
-      (redirect_to root_path and return) unless (Rails.env.development? || (ENV['EDU_FORM_SHOW'] == 'true'))
+      (redirect_to(root_path) && return) unless Rails.env.development? || (ENV['EDU_FORM_SHOW'] == 'true')
 
       form = EducationBenefitsClaim.find(params[:id])
       txt = ::EducationForm::CreateDailySpoolFiles.new.format_application(form.open_struct_form)
-
-      if params[:format] == 'tiff'
-        text = Tempfile.new('edu_txt')
-        tiff = Tempfile.new('edu_tiff')
-        text.write(txt)
-        text.close
-        # we fully control these paths.
-        `convert -bordercolor red -border 3 -append -density 100 -font CourierNew text:#{text.path} #{tiff.path}.tiff`
-        send_file "#{tiff.path}.tiff", :disposition => 'inline', filename: '22-1990.tiff'
-        text.close && text.unlink && tiff.close && tiff.unlink
-      else
-        render text: txt
-      end
+      render text: txt
     end
 
     private
