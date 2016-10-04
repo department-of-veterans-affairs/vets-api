292a6cf2cccd85350e0363beb87396c037c90166
Alastair Dawson <alastair.j.dawson@gmail.com>
2016-10-04 11:51:50 -0700

    Cache mvi response i148 with user delegate (#205)
    * moved mvi response formatting and status into new MVI::Response class
    
    * adding mvi response class
    
    * added redis caching to mvi service
    
    * added mvi response spec
    
    * added mock mvi service
    
    * user mvi data now a hash rather than prefixed
    
    * updated README with MVI mock service info
    
    * moved mvi after initialize callback for user to a delegate
    
    * completed refactoring user after_initialization to mvi delegate
    
    * adding model/delegates to load path
    
    * change dob to birth_date
    
    * add user required fields for mvi find candidate
    
    * added constraint to simplecov version
    
    * user will handle validation rather than mvi message builders
    
    * adding mock service mocked_responses test for coverage
    
    * store ssn in user without dashes

--- lib/mvi/messages/find_candidate_message.rb
+++ lib/mvi/messages/find_candidate_message.rb
@@ -7,31 +7,27 @@ module MVI
     # Builds an MVI SOAP XML message.
     #
     # = Usage
-    # Call the .build passing in the candidate's given and family names, dob, and ssn.
+    # Call the .build passing in the candidate's given and family names, birth_date, and ssn.
     #
     # Example:
-    #  dob = Time.new(1980, 1, 1).utc
-    #  message = MVI::Messages::FindCandidateMessage.new(['John', 'William'], 'Smith', dob, '555-44-3333').to_xml
+    #  birth_date = Time.new(1980, 1, 1).utc
+    #  message = MVI::Messages::FindCandidateMessage.new(['John', 'William'], 'Smith', birth_date, '555-44-3333').to_xml
     #
     class FindCandidateMessage
       include MVI::Messages::MessageBuilder
       EXTENSION = 'PRPA_IN201305UV02'
 
-      attr_reader :given_names, :family_name, :dob, :ssn, :gender
+      attr_reader :given_names, :family_name, :birth_date, :ssn, :gender
 
-      validate :validate_types
-      validates_format_of :ssn, with: /\d{3}-\d{2}-\d{4}/
-
-      def initialize(given_names, family_name, dob, ssn, gender)
+      def initialize(given_names, family_name, birth_date, ssn, gender = nil)
         @given_names = given_names
         @family_name = family_name
-        @dob = dob
+        @birth_date = birth_date
         @ssn = ssn
         @gender = gender
       end
 
       def to_xml
-        raise FindCandidateMessageError, errors.full_messages.join(', ') unless valid?
         super(EXTENSION, build_body)
       rescue => e
         Rails.logger.error "failed to build find candidate message: #{e.message}"
@@ -39,14 +35,6 @@ module MVI
       end
 
       private
-
-      def validate_types
-        if !@given_names.is_a?(Array) || @given_names.any? { |i| !i.is_a? String }
-          errors.add(:given_names, 'should be an array of Strings')
-        end
-        errors.add(:family_name, 'should be a String') unless @family_name.is_a?(String)
-        errors.add(:dob, 'should be a Time object') unless @dob.is_a?(Time)
-      end
 
       def build_body
         control_act_process = build_control_act_process
@@ -74,7 +62,7 @@ module MVI
         el << build_living_subject_name
         el << build_living_subject_birth_time
         el << build_living_subject_id
-        el << build_gender
+        el << build_gender unless @gender.nil?
       end
 
       def build_living_subject_name
@@ -89,7 +77,7 @@ module MVI
 
       def build_living_subject_birth_time
         el = element('livingSubjectBirthTime')
-        el << element('value', value: @dob.strftime('%Y%m%d'))
+        el << element('value', value: @birth_date.strftime('%Y%m%d'))
         el << element('semanticsText', text!: 'LivingSubject..birthTime')
       end
 
