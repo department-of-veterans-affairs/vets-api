b4fda029cdce821298080063f24f988a0fecdbee
Alastair Dawson <alastair@adhocteam.us>
2016-10-04 11:34:16 -0700

    remove unneeded rails.root for spec support file loading

--- spec/support/mvi/mvi_savon_response.rb
+++ spec/support/mvi/mvi_savon_response.rb
@@ -2,18 +2,18 @@
 def mvi_savon_valid_response
   instance_double(
     'Savon::Response',
-    body: Oj.load(File.read("#{Rails.root}/spec/support/mvi/savon_response_body.json")),
-    xml: File.read("#{Rails.root}/spec/support/mvi/find_candidate_response.xml")
+    body: Oj.load(File.read('spec/support/mvi/savon_response_body.json')),
+    xml: File.read('spec/support/mvi/find_candidate_response.xml')
   )
 end
 
 def mvi_savon_invalid_response
-  xml = File.read("#{Rails.root}/spec/support/mvi/find_candidate_invalid_response.xml")
+  xml = File.read('spec/support/mvi/find_candidate_invalid_response.xml')
   bad_response('AR', xml)
 end
 
 def mvi_savon_failure_response
-  xml = File.read("#{Rails.root}/spec/support/mvi/find_candidate_failure_response.xml")
+  xml = File.read('spec/support/mvi/find_candidate_failure_response.xml')
   bad_response('AE', xml)
 end
 
