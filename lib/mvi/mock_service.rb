8b1341ddb541c1e631f29952b4d12f9fe2c152e6
Alastair Dawson <alastair@adhocteam.us>
2016-10-03 22:56:21 -0700

    remove rails root from mvi mock service yaml loading

--- lib/mvi/mock_service.rb
+++ lib/mvi/mock_service.rb
@@ -2,7 +2,7 @@
 module MVI
   class MockService
     def self.mocked_responses
-      @responses ||= YAML.load_file("#{Rails.root}/config/mvi_schema/mock_mvi_responses.yml")
+      @responses ||= YAML.load_file('config/mvi_schema/mock_mvi_responses.yml')
     end
 
     def self.find_candidate
