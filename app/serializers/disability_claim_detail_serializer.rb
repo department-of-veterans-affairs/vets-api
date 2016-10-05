138566d5545cedd3350195f4c53a5b2f336b864c
Carl Johnson <me@carlmjohnson.net>
2016-10-05 09:25:27 -0400

    Remove fake dates from timeline

--- app/serializers/disability_claim_detail_serializer.rb
+++ app/serializers/disability_claim_detail_serializer.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 class DisabilityClaimDetailSerializer < DisabilityClaimBaseSerializer
-  attributes :contention_list, :va_representative,
-             :updates
+  attributes :contention_list, :va_representative, :events_timeline
 
   def contention_list
     object.data['contentionList']
@@ -11,43 +10,24 @@ class DisabilityClaimDetailSerializer < DisabilityClaimBaseSerializer
     object.data['poa']
   end
 
-  TRACKED_ITEM_FIELDS = %w[
-    neverReceivedFromOthersList neverReceivedFromYouList
-    receivedFromOthersList receivedFromYouList stillNeedFromYouList
-  ]
-
-  def updates
-    updates = [
+  def events_timeline
+    events = [
       create_event_from_string_date(:filed, 'date'),
-      create_event_from_string_date(:completed, 'claimCompleteDate'),
-      create_event_from_yes_no(:development_letter_sent, 'developmentLetterSent'),
-      create_event_from_yes_no(:decision_notification_sent, 'decisionNotificationSent'),
-      create_event_from_bool(:requested_decision, 'waiver5103Submitted')
+      create_event_from_string_date(:completed, 'claimCompleteDate')
     ]
 
     # Do the 8 phases
     (1..8).each do |n|
-      updates << create_event_from_string_date("phase#{n}", 'claimPhaseDates', "phase#{n}CompleteDate")
+      events << create_event_from_string_date(
+        "phase#{n}", 'claimPhaseDates', "phase#{n}CompleteDate"
+      )
     end
 
     # Add tracked items
-    TRACKED_ITEM_FIELDS.each do |field|
-      list_objects_with_key(["claimTrackedItems", field], ["openedDate"]).each do |obj|
-        updates << {
-          type: field.snakecase,
-          date: Date.strptime(obj['openedDate'], '%m/%d/%Y'),
-          description: obj['description'],
-          display_name: obj['displayedName'],
-          overdue: obj['overdue'],
-          tracked_item_id: obj['trackedItemId'],
-          tracked_item_status: obj['trackedItemStatus']
-        } if obj['openedDate']
-      end
-    end
+    events += create_events_for_tracked_items
 
     # Filter out events that were nil and make reverse chron
-    updates.compact.sort_by{ |h| h[:date] }.reverse
-
+    events.compact.sort_by { |h| h[:date] }.reverse
   end
 
   private
@@ -61,24 +41,26 @@ class DisabilityClaimDetailSerializer < DisabilityClaimBaseSerializer
     }
   end
 
-  # Some states in the updates timeline are sent to us as just strings/booleans
-  # without a date associated with it. If they haven't been seen by
-  # vets-api before, we'll say that it happened today, so it shows up at
-  # the top of the timeline.
-  def create_event_from_yes_no(type, *from_keys)
-    return nil unless bool_from_yes_no(*from_keys)
-    {
-      type: type,
-      date: Time.current.to_date()
-    }
-  end
+  TRACKED_ITEM_FIELDS = %w(
+    neverReceivedFromOthersList neverReceivedFromYouList
+    receivedFromOthersList receivedFromYouList stillNeedFromYouList
+  ).freeze
 
-  def create_event_from_bool(type, *from_keys)
-    date = date_from_string(*from_keys)
-    return nil unless object.data.dig(*from_keys)
-    {
-      type: type,
-      date: Time.current.to_date()
-    }
+  def create_events_for_tracked_items
+    events = []
+    TRACKED_ITEM_FIELDS.each do |field|
+      list_objects_with_key(['claimTrackedItems', field], ['openedDate']).each do |obj|
+        events << {
+          type: field.snakecase,
+          date: Date.strptime(obj['openedDate'], '%m/%d/%Y'),
+          description: obj['description'],
+          display_name: obj['displayedName'],
+          overdue: obj['overdue'],
+          tracked_item_id: obj['trackedItemId'],
+          tracked_item_status: obj['trackedItemStatus']
+        } if obj['openedDate']
+      end
+    end
+    events
   end
 end
