902cf0035e94458a75b11da890a035a1ccaaf1d9
Austin <amartin857+github@gmail.com>
2016-10-04 17:37:19 -0700

    Only persist claim when necessary in spec

--- spec/services/disability_claim_service_spec.rb
+++ spec/services/disability_claim_service_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 
 RSpec.describe DisabilityClaimService do
   let(:user) { User.sample_claimant }
-  let!(:claim) do
-    FactoryGirl.create(:disability_claim, user_uuid: user.uuid)
-  end
   let(:client_stub) { instance_double('EVSS::ClaimsService') }
   subject { described_class.new(user) }
 
@@ -14,6 +11,7 @@ RSpec.describe DisabilityClaimService do
       it 'returns all claims for the user' do
         allow(client_stub).to receive(:all_claims) { raise Faraday::Error::TimeoutError }
         allow(subject).to receive(:client) { client_stub }
+        claim = FactoryGirl.create(:disability_claim, user_uuid: user.uuid)
         claims = subject.all
         expect(claims).to eq([claim])
         expect(claims.first.sync_failed).to eq(true)
@@ -24,6 +22,7 @@ RSpec.describe DisabilityClaimService do
       it 'returns claim' do
         allow(client_stub).to receive(:find_claim_by_id) { raise Faraday::Error::TimeoutError }
         allow(subject).to receive(:client) { client_stub }
+        claim = FactoryGirl.build(:disability_claim, user_uuid: user.uuid)
         updated_claim = subject.update_from_remote(claim)
         expect(updated_claim).to eq(claim)
         expect(updated_claim.sync_failed).to eq(true)
