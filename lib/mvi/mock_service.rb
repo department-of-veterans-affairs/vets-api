a378fbe24d7386909e8ed80197d81abb7b0e2d94
Alastair Dawson <alastair@adhocteam.us>
2016-10-05 11:33:00 -0700

    mock mvi service needs message arg

--- lib/mvi/mock_service.rb
+++ lib/mvi/mock_service.rb
@@ -5,7 +5,7 @@ module MVI
       @responses ||= YAML.load_file('config/mvi_schema/mock_mvi_responses.yml')
     end
 
-    def self.find_candidate
+    def self.find_candidate(_message)
       mocked_responses['find_candidate']
     end
   end
