90eaef0bc59114d2f7b18d23f06af17e55f189dd
Carl Johnson <carl@adhocteam.us>
2016-10-05 16:51:53 -0400

    Add events_timeline array to get claims details response (#215)
    * Add a description of EDIPI
    
    * Create date sub-serializers
    
    * Make cert check more efficient
    
    * WIP adding events array to claim details
    
    * Add some more event types
    
    * WIP Austin's comments, nested keys
    
    * Deal with tracked items in the updates feed
    
    * Fix rubocop issues
    
    * Remove fake dates from timeline
    
    * Modify schema to pass rspec
    
    * Add bools, serializer test
    
    * Fix nits
    
    * Check that events_timeline is sorted

--- app/serializers/disability_claim_detail_serializer.rb
+++ app/serializers/disability_claim_detail_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class DisabilityClaimDetailSerializer < DisabilityClaimBaseSerializer
-  attributes :contention_list, :va_representative
+  attributes :contention_list, :va_representative, :events_timeline
 
   def contention_list
     object.data['contentionList']
@@ -8,5 +8,68 @@ class DisabilityClaimDetailSerializer < DisabilityClaimBaseSerializer
 
   def va_representative
     object.data['poa']
+  end
+
+  def events_timeline
+    events = [
+      create_event_from_string_date(:filed, 'date'),
+      create_event_from_string_date(:completed, 'claimCompleteDate')
+    ]
+
+    # Do the 8 phases
+    (1..8).each do |n|
+      events << create_event_from_string_date(
+        "phase#{n}", 'claimPhaseDates', "phase#{n}CompleteDate"
+      )
+    end
+
+    # Add tracked items
+    events += create_events_for_tracked_items
+
+    # Filter out events that were nil and make reverse chron
+    events.compact.sort_by { |h| h[:date] }.reverse
+  end
+
+  private
+
+  def create_event_from_string_date(type, *from_keys)
+    date = date_from_string(*from_keys)
+    return nil unless date
+    {
+      type: type,
+      date: date
+    }
+  end
+
+  TRACKED_ITEM_FIELDS = %w(
+    neverReceivedFromOthersList neverReceivedFromYouList
+    receivedFromOthersList receivedFromYouList stillNeedFromYouList
+  ).freeze
+
+  def create_events_for_tracked_items
+    events = []
+    TRACKED_ITEM_FIELDS.each do |field|
+      list_objects_with_key(['claimTrackedItems', field], ['openedDate']).each do |obj|
+        events << {
+          type: field.snakecase,
+          date: Date.strptime(obj['openedDate'], '%m/%d/%Y'),
+          description: obj['description'],
+          display_name: obj['displayedName'],
+          overdue: obj['overdue'],
+          tracked_item_id: obj['trackedItemId'],
+          tracked_item_status: obj['trackedItemStatus']
+        } if obj['openedDate']
+      end
+    end
+    events
+  end
+
+  def list_objects_with_key(parents, sub_keys)
+    parent = object.data.dig(*parents)
+    parent = [] if parent.blank?
+    parent.each do |obj|
+      val = obj.dig(*sub_keys)
+      obj if val.present?
+    end.compact
   end
 end
