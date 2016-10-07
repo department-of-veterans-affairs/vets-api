47bb4a8e05c0cc09be2d54abca4a4878f9a9dcb8
omgitsbillryan <william.ryann@gmail.com>
2016-10-07 18:44:26 -0400

    fix broken spec

--- spec/request/saml_metadata_spec.rb
+++ spec/request/saml_metadata_spec.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 require 'rails_helper'
+require_dependency 'saml/settings_service'
 
 RSpec.describe 'SAML Metadata', type: :request do
+  let(:rubysaml_settings) { FactoryGirl.build(:rubysaml_settings) }
+  before(:each) do
+    allow_any_instance_of(SAML::SettingsService).to receive(:saml_settings).and_return(rubysaml_settings)
+  end
+
   it 'Provides XML SAML metadata' do
     get '/saml/metadata'
     assert_response :success
@@ -9,6 +15,6 @@ RSpec.describe 'SAML Metadata', type: :request do
     xml = Nokogiri::XML(response.body)
 
     entity_id = xml.at_xpath('//md:EntityDescriptor/@entityID', 'md' => 'urn:oasis:names:tc:SAML:2.0:metadata')
-    expect(entity_id.value).to eq(SAML::CONFIG['issuer'])
+    expect(entity_id.value).to eq(SAML_CONFIG['issuer'])
   end
 end
