3097e7da7c7be81032137c1e65e45c03b260b7e6
Carl Johnson <me@carlmjohnson.net>
2016-10-07 11:34:24 -0400

    Add phase integer

--- app/serializers/disability_claim_base_serializer.rb
+++ app/serializers/disability_claim_base_serializer.rb
@@ -3,7 +3,7 @@ class DisabilityClaimBaseSerializer < ActiveModel::Serializer
   attributes :id, :evss_id, :date_filed, :min_est_date, :max_est_date,
              :phase_change_date, :open, :waiver_submitted, :documents_needed,
              :development_letter_sent, :decision_letter_sent, :successful_sync,
-             :updated_at
+             :updated_at, :phase
 
   def date_filed
     date_from_string 'date'
@@ -41,6 +41,10 @@ class DisabilityClaimBaseSerializer < ActiveModel::Serializer
     bool_from_yes_no 'decisionNotificationSent'
   end
 
+  def phase
+    phase_from_keys 'status'
+  end
+
   protected
 
   def with_object_data(*keys)
@@ -63,6 +67,22 @@ class DisabilityClaimBaseSerializer < ActiveModel::Serializer
   def date_from_string(*keys, format: '%m/%d/%Y')
     with_object_data(*keys) do |s|
       Date.strptime(s, format)
+    end
+  end
+
+  def phase_from_keys(*keys)
+    case s = object.data.dig(*keys)&.downcase
+    when 'claim received' then 1
+    when 'under review' then 2
+    when 'gathering of evidence' then 3
+    when 'review of evidence' then 4
+    when 'preparation for decision' then 5
+    when 'pending decision approval' then 6
+    when 'preparation for notification' then 7
+    when 'complete' then 8
+    else
+      Rails.logger.error "Expected EVSS #{keys} to be a phase. Got '#{s}'."
+      nil
     end
   end
 end
