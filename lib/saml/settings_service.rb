7753b2bd113fbe94d3d539e12a024db889d70db1
omgitsbillryan <william.ryann@gmail.com>
2016-10-07 19:02:45 -0400

    removed an unnecessary instance variable

--- lib/saml/settings_service.rb
+++ lib/saml/settings_service.rb
@@ -7,18 +7,20 @@ module SAML
   class SettingsService
     include Singleton
 
+    attr_reader :settings
+
     def initialize
       # populate with SP settings
-      @settings = OneLogin::RubySaml::Settings.new
-      @settings.certificate  = SAML_CONFIG['certificate']
-      @settings.private_key  = SAML_CONFIG['key']
-      @settings.issuer       = SAML_CONFIG['issuer']
-      @settings.assertion_consumer_service_url = SAML_CONFIG['callback_url']
+      settings = OneLogin::RubySaml::Settings.new
+      settings.certificate  = SAML_CONFIG['certificate']
+      settings.private_key  = SAML_CONFIG['key']
+      settings.issuer       = SAML_CONFIG['issuer']
+      settings.assertion_consumer_service_url = SAML_CONFIG['callback_url']
 
       # This will get moved out of here and will be set per application
       # To require just a username and password, use "authentication" as the context;
       # for full identity proofing, use the LOA3 url.
-      @settings.authn_context = 'authentication'
+      settings.authn_context = 'authentication'
       # @settings.authn_context = 'http://idmanagement.gov/ns/assurance/loa/3'
     end
 
@@ -31,7 +33,7 @@ module SAML
     ## Makes an external web call to get IDP metadata and populates SETTINGS
     def fetch_idp_metadata
       parser = OneLogin::RubySaml::IdpMetadataParser.new
-      parser.parse_remote(SAML_CONFIG['metadata_url'], true, settings: @settings)
+      parser.parse_remote(SAML_CONFIG['metadata_url'], true, settings: settings)
     end
   end
 end
