da8ef2bc9d34f3fd6da5f85a374be3b84c5d15fe
omgitsbillryan <william.ryann@gmail.com>
2016-10-10 11:18:45 -0400

    have to mock application controller in this spec also

--- spec/request/saml_metadata_spec.rb
+++ spec/request/saml_metadata_spec.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 require 'rails_helper'
-require_dependency 'saml/settings_service'
 
 RSpec.describe 'SAML Metadata', type: :request do
   let(:rubysaml_settings) { FactoryGirl.build(:rubysaml_settings) }
   before(:each) do
-    allow_any_instance_of(SAML::SettingsService).to receive(:saml_settings).and_return(rubysaml_settings)
+    allow_any_instance_of(ApplicationController).to receive(:saml_settings).and_return(rubysaml_settings)
   end
 
   it 'Provides XML SAML metadata' do
