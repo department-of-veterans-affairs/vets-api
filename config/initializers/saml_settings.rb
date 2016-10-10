7559feb783df0045296bad42ed434a12687aabf3
Bill Ryan <bill.ryan@adhocteam.us>
2016-10-10 15:15:40 -0400

    Move external Idp metadata call out of /initializers (#223)
    * moved our SAML config out of /initializers and into /lib as a service.  The service has a singleton class that will fetch IDP metadata in a just-in-time the first time and read from cache every time thereafter
    
    * added rspec support: rubysaml_settings factory
    
    * added spec for SAML settings service

--- config/initializers/saml_settings.rb
+++ config/initializers/saml_settings.rb
@@ -1,20 +1,2 @@
 # frozen_string_literal: true
-module SAML
-  CONFIG = Rails.application.config_for(:saml).freeze
-  SETTINGS = OneLogin::RubySaml::Settings.new
-
-  SETTINGS.certificate  = CONFIG['certificate']
-  SETTINGS.private_key  = CONFIG['key']
-  SETTINGS.issuer       = CONFIG['issuer']
-  SETTINGS.assertion_consumer_service_url = CONFIG['callback_url']
-
-  # ---------------------------------------------------------------
-  # This will get moved out of here and will be set per application
-  # To require just a username and password, use "authentication" as the context;
-  # for full identity proofing, use the LOA3 url.
-  SETTINGS.authn_context                  = 'authentication'
-  # SAML_SETTINGS.authn_context            = "http://idmanagement.gov/ns/assurance/loa/3"
-
-  parser = OneLogin::RubySaml::IdpMetadataParser.new
-  parser.parse_remote(CONFIG['metadata_url'], true, settings: SETTINGS)
-end
+SAML_CONFIG = Rails.application.config_for(:saml).freeze
