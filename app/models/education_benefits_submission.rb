de02b69d3af8a498357593f321b8bb928b19d358
Lihan Li <frankieteardrop@gmail.com>
2016-10-12 18:16:53 -0400

    add comment to edu benefits submission

--- app/models/education_benefits_submission.rb
+++ app/models/education_benefits_submission.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class EducationBenefitsSubmission < ActiveRecord::Base
+  # don't delete this table, we need to keep the data for a report
   validates(:region, presence: true)
   validates(:region, inclusion: EducationForm::EducationFacility::REGIONS.map(&:to_s))
 end
