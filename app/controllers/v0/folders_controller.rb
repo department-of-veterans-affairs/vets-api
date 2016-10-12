9d6ecad7641195ae0ca45e1ada0662f0888c0381
mphprogrammer <marc.harbatkin@va.gov>
2016-10-12 12:32:10 -0400

    Removed folder sorting

--- app/controllers/v0/folders_controller.rb
+++ app/controllers/v0/folders_controller.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 module V0
   class FoldersController < SMController
-    SORT_FIELDS   = %w(name).freeze
-    SORT_TYPES    = (SORT_FIELDS + SORT_FIELDS.map { |field| "-#{field}" }).freeze
-    DEFAULT_SORT  = 'name'
-
     def index
       resource = client.get_folders
-      resource = resource.sort(params[:sort] || DEFAULT_SORT, allowed: SORT_TYPES)
       resource = resource.paginate(pagination_params)
 
       render json: resource.data,
