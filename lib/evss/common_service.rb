1313fd0e31e31e25c29f5d73cf212dc60307cfd6
Carl Johnson <me@carlmjohnson.net>
2016-10-13 14:40:43 -0400

    Common service version down too

--- lib/evss/common_service.rb
+++ lib/evss/common_service.rb
@@ -4,14 +4,14 @@ require_dependency 'evss/base_service'
 module EVSS
   class CommonService < BaseService
     def find_rating_info
-      post 'ratingInfoService/11.1/findRatingInfoPID',
+      post 'ratingInfoService/11.0/findRatingInfoPID',
            { participantId: @user.participant_id }.to_json
     end
 
     protected
 
     def base_url
-      "#{ENV['EVSS_BASE_URL']}/wss-common-services-web-11.1/rest/"
+      "#{ENV['EVSS_BASE_URL']}/wss-common-services-web-11.0/rest/"
     end
   end
 end
