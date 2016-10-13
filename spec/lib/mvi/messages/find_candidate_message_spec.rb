f3b505d26cbb75b61a64de31cf58c67ea3555afd
Alastair Dawson <alastair@adhocteam.us>
2016-10-13 08:31:24 -0700

    mvi update generated find candidate xml

--- spec/lib/mvi/messages/find_candidate_message_spec.rb
+++ spec/lib/mvi/messages/find_candidate_message_spec.rb
@@ -13,34 +13,40 @@ describe MVI::Messages::FindCandidateMessage do
       let(:idm_path) { 'env:Body/idm:PRPA_IN201305UV02' }
       let(:parameter_list_path) { "#{idm_path}/controlActProcess/queryByParameter/parameterList" }
 
-      it 'should have a USDSVA extension with a uuid' do
+      it 'has a USDSVA extension with a uuid' do
         expect(xml).to match_at_path("#{idm_path}/id/@extension", /200VGOV-\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)
       end
 
-      it 'should have a sender extension' do
+      it 'has a sender extension' do
         expect(xml).to eq_at_path("#{idm_path}/sender/device/id/@extension", '200VGOV')
       end
 
-      it 'should have a receiver extension' do
+      it 'has a receiver extension' do
         expect(xml).to eq_at_path("#{idm_path}/receiver/device/id/@extension", '200M')
       end
 
-      it 'should have a name node' do
+      it 'has a name node' do
         expect(xml).to eq_text_at_path("#{parameter_list_path}/livingSubjectName/value/given[0]", 'John')
         expect(xml).to eq_text_at_path("#{parameter_list_path}/livingSubjectName/value/given[1]", 'William')
         expect(xml).to eq_text_at_path("#{parameter_list_path}/livingSubjectName/value/family", 'Smith')
+        expect(xml).to eq_text_at_path("#{parameter_list_path}/livingSubjectName/semanticsText", 'Legal Name')
       end
 
-      it 'should have a birth time node' do
+      it 'has a birth time node' do
         expect(xml).to eq_at_path("#{parameter_list_path}/livingSubjectBirthTime/value/@value", '19800101')
+        expect(xml).to eq_text_at_path("#{parameter_list_path}/livingSubjectBirthTime/semanticsText", 'Date of Birth')
       end
 
-      it 'should have a social security number node' do
-        expect(xml).to eq_at_path("#{parameter_list_path}/livingSubjectId/value/@extention", '555-44-3333')
+      it 'has a social security number node' do
+        expect(xml).to eq_at_path("#{parameter_list_path}/livingSubjectId/value/@extension", '555-44-3333')
       end
 
-      it 'should have a gender node' do
+      it 'has a gender node' do
         expect(xml).to eq_at_path("#{parameter_list_path}/livingSubjectAdministrativeGender/value/@code", 'M')
+        expect(xml).to eq_text_at_path(
+          "#{parameter_list_path}/livingSubjectAdministrativeGender/semanticsText",
+          'Gender'
+        )
       end
     end
 
