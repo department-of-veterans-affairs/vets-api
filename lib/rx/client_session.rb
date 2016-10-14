1ea6a6f895ab780a2a8bd0c8897d174985485ec0
Kam Karshenas <kam@saneshark.com>
2016-10-14 04:31:49 -0400

    need to use separate redis namespaces for the two mhv userids

--- lib/rx/client_session.rb
+++ lib/rx/client_session.rb
@@ -3,7 +3,7 @@ require 'common/client/session'
 module Rx
   # Session model
   class ClientSession < Common::Client::Session
-    redis_store REDIS_CONFIG['mhv_store']['namespace']
+    redis_store REDIS_CONFIG['rx_store']['namespace']
     redis_ttl 900
     redis_key :user_id
   end
