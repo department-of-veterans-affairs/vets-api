2023e9670b9bf90fd1a2c02ba0f9a3bf3647da47
Kam Karshenas <kam@saneshark.com>
2016-10-15 06:23:12 -0400

    updating triage controller and model to use new style

--- app/models/triage_team.rb
+++ app/models/triage_team.rb
@@ -3,8 +3,8 @@ require 'common/models/base'
 
 # TriageTeam model
 class TriageTeam < Common::Base
-  attribute :triage_team_id, Integer, filterable: true
-  attribute :name, String, filterable: true
+  attribute :triage_team_id, Integer
+  attribute :name, String, sortable: { order: 'ASC', default: true }
   attribute :relation_type, String
 
   def <=>(other)
