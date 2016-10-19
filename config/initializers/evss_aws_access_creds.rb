d0de9a63e7746d58752659228deaf708a49d878f
Austin <amartin857+github@gmail.com>
2016-10-19 14:07:49 -0700

    Rename method

--- config/initializers/evss_aws_access_creds.rb
+++ config/initializers/evss_aws_access_creds.rb
@@ -2,4 +2,4 @@
 require_dependency 'evss/aws_creds'
 
 evss_s3_uploads = ENV['EVSS_S3_UPLOADS'] == 'true'
-EVSS_AWS_ACCESS_CREDS = EVSS::AwsCreds.fetch(evss_s3_uploads)
+EVSS_AWS_ACCESS_CREDS = EVSS::AwsCreds.load(evss_s3_uploads)
--- lib/evss/aws_creds.rb
+++ lib/evss/aws_creds.rb
@@ -5,8 +5,8 @@ module EVSS
   class AwsCreds
     METADATA_ENDPOINT = 'http://169.254.169.254/latest/meta-data/iam/security-credentials/CloudWatchRole/'
 
-    def self.fetch(fetch_from_remote = false)
-      if fetch_from_remote
+    def self.load(fetch_from_metadata = false)
+      if fetch_from_metadata
         meta = JSON.parse Net::HTTP.get(URI.parse(METADATA_ENDPOINT))
         {
           aws_access_key_id: meta['AccessKeyId'],
