0eb003e25abe4139ab9f7039082ec32b7254d222
Kam Karshenas <kam@saneshark.com>
2016-10-19 04:36:16 -0400

    improving prescription_spec to use factory

--- spec/models/prescription_spec.rb
+++ spec/models/prescription_spec.rb
@@ -2,24 +2,23 @@
 require 'rails_helper'
 
 describe Prescription do
-  let(:original_camel_cased_json) { File.read('spec/support/fixtures/get_rx_1435525.json') }
-  let(:json_string) { File.read('spec/support/fixtures/prescription_1435525_snakecase.json').strip }
+  let(:prescription_attributes) { attributes_for(:prescription) }
 
   context 'with valid attributes' do
-    subject { described_class.new(parsed_json_object) }
+    subject { described_class.new(prescription_attributes) }
 
-    xit 'has attributes' do
+    it 'has attributes' do
       expect(subject).to have_attributes(refill_status: 'active', refill_remaining: 9, facility_name: 'ABC1223',
                                          is_refillable: true, is_trackable: false, prescription_id: 1_435_525,
                                          quantity: 10,  prescription_number: '2719324',
                                          prescription_name: 'Drug 1 250MG TAB', station_number: '23')
     end
 
-    xit 'has additional aliased rubyesque methods' do
+    it 'has additional aliased rubyesque methods' do
       expect(subject).to have_attributes(trackable?: false, refillable?: true)
     end
 
-    xit 'has date attributes' do
+    it 'has date attributes' do
       expect(subject).to have_attributes(refill_submit_date: Time.parse('Tue, 26 Apr 2016 00:00:00 EDT').in_time_zone,
                                          refill_date: Time.parse('Thu, 21 Apr 2016 00:00:00 EDT').in_time_zone,
                                          ordered_date: Time.parse('Tue, 29 Mar 2016 00:00:00 EDT').in_time_zone,
@@ -28,33 +27,28 @@ describe Prescription do
     end
 
     context 'inherited methods' do
-      xit 'responds to to_json' do
+      it 'responds to to_json' do
         expect(subject.to_json).to be_a(String)
       end
     end
   end
 
   context 'it sorts' do
-    let(:p1) { described_class.new(parsed_json_object) }
-    let(:p2) { described_class.new(parsed_json_object) }
-    let(:p3) { described_class.new(data_attr_merge(prescription_id: '2', refill_date: Time.now.utc)) }
-    let(:p4) { described_class.new(data_attr_merge(prescription_id: '3', refill_data: 1.year.ago.utc)) }
+    let(:p1) { described_class.new(prescription_attributes) }
+    let(:p2) { described_class.new(prescription_attributes) }
+    let(:p3) { described_class.new(attributes_for(:prescription, prescription_id: '2', refill_date: Time.now.utc)) }
+    let(:p4) { described_class.new(attributes_for(:prescription, prescription_id: '3', refill_data: 1.year.ago.utc)) }
 
     subject { [p1, p2, p3, p4] }
 
-    xit 'sorts by prescription_id by default' do
+    it 'sorts by prescription_id by default' do
       expect(subject.sort.map(&:prescription_id))
         .to eq([2, 3, 1_435_525, 1_435_525])
     end
 
-    xit 'sorts by sort_by field' do
+    it 'sorts by sort_by field' do
       expect(subject.sort_by(&:refill_date).map(&:prescription_id))
         .to eq([1_435_525, 1_435_525, 3, 2])
     end
-  end
-
-  def data_attr_merge(attributes = {})
-    data = parsed_json_object[:data]
-    parsed_json_object.merge(data: data.merge(attributes))
   end
 end
