70a17d8c48cd6838c9eeea3f3753878321342506
Kenneth Koski <ken@adhocteam.us>
2016-10-20 11:24:28 -0500

    Freezing strings to quiet rubocop

--- config/initializers/evss_aws_access_creds.rb
+++ config/initializers/evss_aws_access_creds.rb
@@ -2,6 +2,6 @@
 
 evss_s3_uploads = ENV['EVSS_S3_UPLOADS'] == 'true'
 EVSS_AWS_ACCESS_CREDS = {
-  aws_access_key_id: ENV['EVSS_AWS_ACCESS_KEY_ID'],
-  aws_secret_access_key: ENV['EVSS_AWS_SECRET_ACCESS_KEY']
+  aws_access_key_id: ENV['EVSS_AWS_ACCESS_KEY_ID'].freeze,
+  aws_secret_access_key: ENV['EVSS_AWS_SECRET_ACCESS_KEY'].freeze
 }
