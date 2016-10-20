5062e33a005b51daa26a9e66b9cd90a0ce283822
Kenneth Koski <ken@adhocteam.us>
2016-10-20 11:17:13 -0500

    Reading EVSS AWS creds from ENV

--- config/initializers/evss_aws_access_creds.rb
+++ config/initializers/evss_aws_access_creds.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
-require_dependency 'evss/aws_creds'
 
 evss_s3_uploads = ENV['EVSS_S3_UPLOADS'] == 'true'
-EVSS_AWS_ACCESS_CREDS = EVSS::AwsCreds.load(evss_s3_uploads)
+EVSS_AWS_ACCESS_CREDS = {
+  aws_access_key_id: ENV['EVSS_AWS_ACCESS_KEY_ID'],
+  aws_secret_access_key: ENV['EVSS_AWS_SECRET_ACCESS_KEY']
+}
