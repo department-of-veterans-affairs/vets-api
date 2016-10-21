aa6f993e45bd0300b71986a178c8ed862b2b6214
austin-martinez <amartin857+github@gmail.com>
2016-10-21 14:47:56 -0700

    Fetch and cache aws creds when needed (#317)
    * Fetch and cache aws creds when needed
    
    * Use carrierwave-aws gem and allow aws-sdk to get creds from instance metadata

--- spec/uploaders/disability_claim_document_uploader_spec.rb
+++ spec/uploaders/disability_claim_document_uploader_spec.rb
@@ -24,18 +24,13 @@ RSpec.describe DisabilityClaimDocumentUploader do
         env_vars = {
           EVSS_S3_UPLOADS: 'true',
           EVSS_AWS_S3_BUCKET: 'evss_s3_bucket',
-          EVSS_AWS_S3_REGION: 'evss_s3_region',
-          EVSS_AWS_ACCESS_KEY_ID: 'aws_access_key_id',
-          EVSS_AWS_SECRET_ACCESS_KEY: 'aws_secret_access_key'
+          EVSS_AWS_S3_REGION: 'evss_s3_region'
         }
         ClimateControl.modify(env_vars) do
-          expect(subject.class.storage).to eq(CarrierWave::Storage::Fog)
-          expect(subject.fog_credentials).to eq(provider: 'AWS',
-                                                aws_access_key_id: 'aws_access_key_id',
-                                                aws_secret_access_key: 'aws_secret_access_key',
-                                                region: 'evss_s3_region')
-          expect(subject.fog_public).to eq(false)
-          expect(subject.fog_directory).to eq('evss_s3_bucket')
+          expect(subject.class.storage).to eq(CarrierWave::Storage::AWS)
+          expect(subject.aws_credentials).to eq(region: 'evss_s3_region')
+          expect(subject.aws_acl).to eq('private')
+          expect(subject.aws_bucket).to eq('evss_s3_bucket')
         end
       end
     end
