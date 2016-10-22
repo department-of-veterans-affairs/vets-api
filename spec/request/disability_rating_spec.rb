8e99e5f640f857c907fa8fbc4ea6ba4a618fe07a
Austin <amartin857+github@gmail.com>
2016-10-21 21:15:30 -0700

    Use FactoryGirl in spec

--- spec/request/disability_rating_spec.rb
+++ spec/request/disability_rating_spec.rb
@@ -2,16 +2,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Disability rating', type: :request do
-  let(:user) do
-    user = FactoryGirl.build(:mvi_user)
-    user.save
-    user
-  end
-  let(:session) do
-    session = Session.new(uuid: user.uuid)
-    session.save
-    session
-  end
+  let(:user) { FactoryGirl.create(:mvi_user) }
+  let(:session) { Session.create(uuid: user.uuid) }
 
   it 'shows the disability rating record' do
     VCR.use_cassette('evss/common/rating_record') do
