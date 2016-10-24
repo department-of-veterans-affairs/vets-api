47df1f58188e9759c2e24cd74135c7e3b414fd93
mphprogrammer <marc.harbatkin@va.gov>
2016-10-24 17:00:04 -0400

    Sprucing again

--- lib/sm/api/message_drafts.rb
+++ lib/sm/api/message_drafts.rb
@@ -6,29 +6,38 @@ module SM
       # a parameter, then the message draft is updated.
       def post_create_message_draft(args = {})
         # Prevent call if this is a reply draft, otherwise reply-to message suject can change.
-        json = if args[:id].present? && history?(args[:id])
-                 { data: args.merge(has_message: true) }
-               else
-                 perform(:post, 'message/draft', args, token_headers)
-               end
+        draft = build_draft(args)
+        return draft unless draft.valid?
 
+        json = perform(:post, 'message/draft', args, token_headers)
         MessageDraft.new(json)
       end
 
       def post_create_message_draft_reply(id, args = {})
-        # Allow call if this is a new reply draft, or an existing draft with an association to a reply-to message
-        if args[:id].blank? || history?(args[:id])
-          json = perform(:post, "message/#{id}/replydraft", args, token_headers)
-          json[:data][:has_message] = true
-        else
-          json = { data: args }
-        end
+        # prevent call if this an existing draft with no association to a reply-to message
+        draft = build_reply_draft(args)
+        return draft unless draft.valid?
+
+        json = perform(:post, "message/#{id}/replydraft", args, token_headers)
+        json[:data][:has_message] = true
 
         MessageDraft.new(json).as_reply
       end
 
-      def history?(id)
+      def reply_draft?(id)
         get_message_history(id).data.present?
+      end
+
+      def build_draft(args)
+        draft = MessageDraft.new(args)
+        args[:id] && reply_draft?(args[:id]) ? draft.as_reply : draft
+      end
+
+      def build_reply_draft(args)
+        draft = MessageDraft.new(args).as_reply
+        draft.has_message = !args[:id] || reply_draft?(args[:id])
+
+        draft
       end
     end
   end
