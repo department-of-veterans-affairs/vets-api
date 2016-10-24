1030e33cc9b709b8e2a3f77cce5756068343f360
Mark Olson <theothermarkolson@gmail.com>
2016-10-24 14:10:31 -0400

    Switch from whenever/cron to sidekiq-scheduler for EDU

--- spec/jobs/education_form/create_daily_spool_files_spec.rb
+++ spec/jobs/education_form/create_daily_spool_files_spec.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.inline!
+Sidekiq::Logging.logger = nil
 
 RSpec.describe EducationForm::CreateDailySpoolFiles, type: :model, form: :education_benefits do
   subject { described_class.new }
