0853a8486d0ec3695f6c5e93400233d592c5f8cc
Kam Karshenas <kam@saneshark.com>
2016-10-24 12:13:29 -0400

    additional comment from code review

--- lib/sm/client.rb
+++ lib/sm/client.rb
@@ -110,6 +110,7 @@ module SM
         conn.use :breakers
         conn.request :multipart
         conn.request :json
+        # Uncomment this out for generating curl output to send to MHV dev and test only
         # conn.request :curl, ::Logger.new(STDOUT), :warn
 
         # conn.response :logger, ::Logger.new(STDOUT), bodies: true
