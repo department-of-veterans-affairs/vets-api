cc75e5c55da80e2bdf415663972c109ae37f77b4
Carl Johnson <me@carlmjohnson.net>
2016-10-26 16:44:39 -0400

    Use _id for tracked_item
    Front end is sending trackedItem, so it needs to be changed anyway.

--- app/controllers/v0/documents_controller.rb
+++ app/controllers/v0/documents_controller.rb
@@ -9,7 +9,7 @@ module V0
       document_data = DisabilityClaimDocument.new(
         evss_claim_id: claim.evss_id,
         file_name: params[:file].original_filename,
-        tracked_item_id: params[:tracked_item],
+        tracked_item_id: params[:tracked_item_id],
         document_type: params[:document_type]
       )
       raise Common::Exceptions::ValidationErrors, document_data unless document_data.valid?
