29381aa2284a219137c23a28c2ff3620c82b1fdd
Kam Karshenas <kam@saneshark.com>
2016-10-27 11:17:17 -0400

    adding filter for this helper as it is triggering simplecov issues while temporarily disabling authentication

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ unless ENV['NOCOVERAGE']
     add_filter 'lib/tasks/support/shell_command.rb'
     add_filter 'lib/config_helper.rb'
     add_filter 'lib/feature_flipper.rb'
+    add_filter 'spec/support/authenticated_session_helper'
     SimpleCov.minimum_coverage_by_file 90
   end
 end
