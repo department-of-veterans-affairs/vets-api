fc904f1cdcc8e7dc8d0040803b643fa68f3ac03b
mphprogrammer <marc.harbatkin@va.gov>
2016-10-27 15:09:48 -0400

    Changed default sorting on prescriptions

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -5,11 +5,11 @@ require 'common/models/base'
 class Prescription < Common::Base
   attribute :prescription_id, Integer, sortable: { order: 'ASC' }, filterable: %w(eq not_eq)
   attribute :refill_status, String, sortable: { order: 'ASC' }, filterable: %w(eq not_eq)
-  attribute :refill_submit_date, Common::UTCTime
+  attribute :refill_submit_date, Common::UTCTime, sortable: { order: 'DESC', default: true }, filterable: %w(eq not_eq)
   attribute :refill_date, Common::UTCTime, sortable: { order: 'DESC' }
   attribute :refill_remaining, Integer
   attribute :facility_name, String, sortable: { order: 'ASC' }, filterable: %w(eq not_eq)
-  attribute :ordered_date, Common::UTCTime, sortable: { order: 'DESC', default: true }
+  attribute :ordered_date, Common::UTCTime, sortable: { order: 'DESC' }
   attribute :quantity, Integer
   attribute :expiration_date, Common::UTCTime
   attribute :prescription_number, String
