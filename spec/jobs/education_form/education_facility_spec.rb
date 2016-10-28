159e9f0a02b8949b16bea19b48df373d51096aa5
Alex Taylor <alex@goldenmeanie.net>
2016-10-28 13:52:35 -0400

    Update test

--- spec/jobs/education_form/education_facility_spec.rb
+++ spec/jobs/education_form/education_facility_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe EducationForm::EducationFacility do
     {
       eastern: ['VA', "Eastern Region\nVA Regional Office\nP.O. Box 4616\nBuffalo, NY 14240-4616"],
       southern: ['PR', "Southern Region\nVA Regional Office\nP.O. Box 100022\nDecatur, GA 30031-7022"],
-      central: ['CO', "Central Region\nVA Regional Office\nP.O. Box 66830\nSt. Louis, MO 63166-6830"],
+      central: ['CO', "Central Region\nVA Regional Office\n9770 Page Avenue\nSuite 101 Education\n
+St. Louis, MO 63132-1502"],
       western: ['AK', "Western Region\nVA Regional Office\nP.O. Box 8888\nMuskogee, OK 74402-8888"]
     }.each do |region, region_data|
       context "with a #{region} address" do
