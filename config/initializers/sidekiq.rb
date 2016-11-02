c72156dfae9bd02e392e11f8a4c2085885ce441a
James Kassemi <jkassemi@gmail.com>
2016-11-01 23:22:20 -0600

    Instrument sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,8 +6,20 @@ Sidekiq.configure_server do |config|
     Sidekiq.schedule = YAML.load_file(File.expand_path('../../sidekiq_scheduler.yml', __FILE__))
     Sidekiq::Scheduler.reload_schedule!
   end
+
+  config.server_middleware do |chain|
+    chain.add Sidekiq::Instrument::ServerMiddleware
+  end
+
+  config.client_middleware do |chain|
+    chain.add Sidekiq::Instrument::ClientMiddleware
+  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = REDIS_CONFIG['redis']
+
+  config.client_middleware do |chain|
+    chain.add Sidekiq::Instrument::ClientMiddleware
+  end
 end
