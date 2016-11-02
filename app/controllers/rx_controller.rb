60b907a3aed36ba8a65d426c67900cbb20b7233b
Kam Karshenas <kam@saneshark.com>
2016-11-02 14:55:54 -0400

    DRY up MHV concerns

--- app/controllers/rx_controller.rb
+++ app/controllers/rx_controller.rb
@@ -3,26 +3,11 @@ require 'rx/client'
 
 class RxController < ApplicationController
   include ActionController::Serialization
-
-  before_action :authenticate
-  before_action :authorize_rx
-  before_action :authenticate_client
+  include MHVControllerConcerns
 
   protected
 
   def client
     @client ||= Rx::Client.new(session: { user_id: current_user.mhv_correlation_id })
-  end
-
-  def authorize_rx
-    current_user&.can_access_mhv? || raise_access_denied
-  end
-
-  def raise_access_denied
-    raise Common::Exceptions::Forbidden, detail: 'You do not have access to prescriptions'
-  end
-
-  def authenticate_client
-    client.authenticate if client.session.expired?
   end
 end
--- app/controllers/sm_controller.rb
+++ app/controllers/sm_controller.rb
@@ -3,25 +3,11 @@ require 'sm/client'
 
 class SMController < ApplicationController
   include ActionController::Serialization
-
-  before_action :authorize_sm
-  before_action :authenticate_client
+  include MHVControllerConcerns
 
   protected
 
   def client
     @client ||= SM::Client.new(session: { user_id: current_user.mhv_correlation_id })
-  end
-
-  def authorize_sm
-    current_user&.can_access_mhv? || raise_access_denied
-  end
-
-  def raise_access_denied
-    raise Common::Exceptions::Forbidden, detail: 'You do not have access to messaging'
-  end
-
-  def authenticate_client
-    client.authenticate if client.session.expired?
   end
 end
