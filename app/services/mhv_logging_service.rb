6ab06499b472222025f1d14d41c2866b691556ff
Kam Karshenas <kam@saneshark.com>
2016-11-04 18:14:52 -0400

    only do it mhv_correlation_id exists

--- app/services/mhv_logging_service.rb
+++ app/services/mhv_logging_service.rb
@@ -4,9 +4,9 @@ module MHVLogging
   class Service
     def self.login(current_user)
       # If login has already been submitted, do nothing
-      return if current_user.mhv_last_signed_in
+      return if current_user.mhv_last_signed_in || current_user.mhv_correlation_id.nil?
       # Otherwise send the login audit trail
-      MHVLogging::Client.new(session: { user_id: mhv_correlation_id })
+      MHVLogging::Client.new(session: { user_id: current_user.mhv_correlation_id })
         .authenticate
         .auditlogin
       # Update the user object with the time of login
@@ -16,9 +16,9 @@ module MHVLogging
 
     def self.logout(current_user)
       # If login has never been sent, no need to send logout
-      return unless current_user.mhv_last_signed_in
+      return unless current_user.mhv_last_signed_in || current_user.mhv_correlation_id.nil?
       # Otherwise send the logout audit trail
-      MHVLogging::Client.new(session: { user_id: mhv_correlation_id })
+      MHVLogging::Client.new(session: { user_id: current_user.mhv_correlation_id })
         .authenticate
         .auditlogout
       # Update the user object with nil to indicate not logged in
