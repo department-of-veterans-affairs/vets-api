7c2dd53295b742a5e7c1b7b916b8072b32529c2b
Kam Karshenas <kam@saneshark.com>
2016-11-04 19:54:56 -0400

    oops

--- app/services/mhv_logging_service.rb
+++ app/services/mhv_logging_service.rb
@@ -3,7 +3,7 @@ require 'mhv_logging/client'
 class MHVLoggingService
   def self.login(current_user)
     # If login has already been submitted, do nothing
-    return false if current_user.mhv_last_signed_in
+    return false if current_user.mhv_correlation_id.nil? || current_user.mhv_last_signed_in
     # Otherwise send the login audit trail
     MHVLogging::Client.new(session: { user_id: current_user.mhv_correlation_id })
       .authenticate
