e29b66302a15a8dba0e161158d96e644d31753ae
Kam Karshenas <kam@saneshark.com>
2016-11-04 19:28:15 -0400

    get test coverage to 100%

--- app/services/mhv_logging_service.rb
+++ app/services/mhv_logging_service.rb
@@ -3,7 +3,7 @@ require 'mhv_logging/client'
 class MHVLoggingService
   def self.login(current_user)
     # If login has already been submitted, do nothing
-    return if current_user.mhv_last_signed_in || current_user.mhv_correlation_id.nil?
+    return false if current_user.mhv_last_signed_in || current_user.mhv_correlation_id.nil?
     # Otherwise send the login audit trail
     MHVLogging::Client.new(session: { user_id: current_user.mhv_correlation_id })
       .authenticate
@@ -11,11 +11,12 @@ class MHVLoggingService
     # Update the user object with the time of login
     current_user.mhv_last_signed_in = Time.current
     current_user.save
+    true
   end
 
   def self.logout(current_user)
     # If login has never been sent, no need to send logout
-    return unless current_user.mhv_last_signed_in || current_user.mhv_correlation_id.nil?
+    return false unless current_user.mhv_last_signed_in || current_user.mhv_correlation_id.nil?
     # Otherwise send the logout audit trail
     MHVLogging::Client.new(session: { user_id: current_user.mhv_correlation_id })
       .authenticate
@@ -23,5 +24,6 @@ class MHVLoggingService
     # Update the user object with nil to indicate not logged in
     current_user.mhv_last_signed_in = nil
     current_user.save
+    true
   end
 end
