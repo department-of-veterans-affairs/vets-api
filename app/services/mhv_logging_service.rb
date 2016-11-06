4e9b05a8601ae488b7d945ae235f42fc22c84cdc
Kam Karshenas <kam@saneshark.com>
2016-11-05 20:17:56 -0400

    MHV Login/Logout facility Integration (#452)
    * adding the logout logging facility for mhv as an integration, specs to follow
    
    * remove unused redis_store change
    
    * only do it mhv_correlation_id exists
    
    * allow specs to pass but still need to add specs to test service
    
    * get test coverage to 100%
    
    * addressing comments from @ayal
    
    * oops
    
    * fixing linter issues

---
+++ app/services/mhv_logging_service.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+require 'mhv_logging/client'
+class MHVLoggingService
+  def self.login(current_user)
+    # If login has already been submitted, do nothing
+    return false if current_user.mhv_correlation_id.nil? || current_user.mhv_last_signed_in
+    # Otherwise send the login audit trail
+    MHVLogging::Client.new(session: { user_id: current_user.mhv_correlation_id })
+                      .authenticate
+                      .auditlogin
+    # Update the user object with the time of login
+    current_user.mhv_last_signed_in = Time.current
+    current_user.save
+    true
+  end
+
+  def self.logout(current_user)
+    # If login has never been sent, no need to send logout
+    return false unless current_user.mhv_correlation_id.nil? || current_user.mhv_last_signed_in
+    # Otherwise send the logout audit trail
+    MHVLogging::Client.new(session: { user_id: current_user.mhv_correlation_id })
+                      .authenticate
+                      .auditlogout
+    # Update the user object with nil to indicate not logged in
+    current_user.mhv_last_signed_in = nil
+    current_user.save
+    true
+  end
+end
