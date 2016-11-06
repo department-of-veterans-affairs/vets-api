ec235381c42fdd79b99f101fea8d85de9bf6db07
austin-martinez <amartin857+github@gmail.com>
2016-11-05 19:00:57 -0700

    Enable breakers for EVSS services (#457)

--- spec/services/disability_claim_service_spec.rb
+++ spec/services/disability_claim_service_spec.rb
@@ -55,28 +55,25 @@ RSpec.describe DisabilityClaimService do
     end
   end
 
-  # TODO: (AJM) add these tests back when turning breakers back on (post testing)
-  # :nocov:
   context 'when EVSS client has an outage' do
     describe '#all' do
-      xit 'returns all claims for the user' do
+      it 'returns all claims for the user' do
         EVSS::ClaimsService.breakers_service.begin_forced_outage!
         claim = FactoryGirl.create(:disability_claim, user_uuid: user.uuid)
-        claims = subject.all
+        claims, synchronized = subject.all
         expect(claims).to eq([claim])
-        expect(claims.first.successful_sync).to eq(false)
+        expect(synchronized).to eq(false)
       end
     end
 
     describe '#update_from_remote' do
-      xit 'returns claim' do
+      it 'returns claim' do
         EVSS::ClaimsService.breakers_service.begin_forced_outage!
         claim = FactoryGirl.build(:disability_claim, user_uuid: user.uuid)
-        updated_claim = subject.update_from_remote(claim)
+        updated_claim, synchronized = subject.update_from_remote(claim)
         expect(updated_claim).to eq(claim)
-        expect(updated_claim.successful_sync).to eq(false)
+        expect(synchronized).to eq(false)
       end
     end
-    # :nocov:
   end
 end
