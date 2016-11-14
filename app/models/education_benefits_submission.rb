9e4b4e9f0e205cc9d5de1a398a54bbcfa30b16d6
Lihan Li <frankieteardrop@gmail.com>
2016-11-14 16:48:40 -0500

    fix create_education_benefits_submission

--- app/models/education_benefits_submission.rb
+++ app/models/education_benefits_submission.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class EducationBenefitsSubmission < ActiveRecord::Base
   # don't delete this table, we need to keep the data for a report
-  validates(:region, presence: true)
+  validates(:region, :education_benefits_claim, presence: true)
   validates(:region, inclusion: EducationForm::EducationFacility::REGIONS.map(&:to_s))
   validates(:status, inclusion: %w(processed submitted))
 
