1f122c27ba0508a2cf9be304daba5a1d0b1afa1e
Lihan Li <frankieteardrop@gmail.com>
2016-11-14 16:39:14 -0500

    add status and education_benefits_claim_id to submissions table

--- app/models/education_benefits_submission.rb
+++ app/models/education_benefits_submission.rb
@@ -3,4 +3,5 @@ class EducationBenefitsSubmission < ActiveRecord::Base
   # don't delete this table, we need to keep the data for a report
   validates(:region, presence: true)
   validates(:region, inclusion: EducationForm::EducationFacility::REGIONS.map(&:to_s))
+  validates(:status, inclusion: %w(processed submitted))
 end
---
+++ db/migrate/20161114211400_add_status_to_education_benefits_submissions.rb
@@ -0,0 +1,13 @@
+class AddStatusToEducationBenefitsSubmissions < ActiveRecord::Migration
+  def change
+    add_column(:education_benefits_submissions, :status, :string)
+
+    EducationBenefitsSubmission.update_all(status: 'processed')
+    change_column(:education_benefits_submissions, :status, :string, null: false)
+
+    add_column(:education_benefits_submissions, :education_benefits_claim_id, :integer)
+    # don't know id for old submissions but it doesn't matter
+    EducationBenefitsSubmission.update_all(education_benefits_claim_id: (EducationBenefitsClaim.first&.id || 1))
+    change_column(:education_benefits_submissions, :education_benefits_claim_id, :integer, null: false)
+  end
+end
