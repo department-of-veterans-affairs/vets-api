3657462c4ecea5cd6f03e1f684243f5b7e950748
Aubrey Holland <aubreyholland@gmail.com>
2016-11-17 09:33:01 -0500

    catch exceptions

--- lib/statsd_middleware.rb
+++ lib/statsd_middleware.rb
@@ -8,7 +8,13 @@ class StatsdMiddleware
     status, headers, response = @app.call(env)
     params = env['action_dispatch.request.path_parameters']
     key = "api.external.request#status=#{status},controller=#{params[:controller]},action=#{params[:action]}"
-    StatsD.increment(key, 1)
+    # rubocop:disable Lint/HandleExceptions
+    begin
+      StatsD.increment(key, 1)
+    rescue
+      # we want to ensure this doesn't break the response cyce
+    end
+    # rubocop:enable Lint/HandleExceptions
     [status, headers, response]
   end
 end
