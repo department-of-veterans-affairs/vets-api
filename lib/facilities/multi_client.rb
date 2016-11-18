c4857903a40a18fbafd1950ae26d02593e776e18
Patrick Vinograd <patrick@adhocteam.us>
2016-11-18 12:01:58 -0800

    Use an exception type that actually exists

--- lib/facilities/multi_client.rb
+++ lib/facilities/multi_client.rb
@@ -2,6 +2,7 @@
 require 'typhoeus'
 require 'json'
 require 'common/client/errors'
+require 'common/exceptions'
 
 module Facilities
   class MultiClient
@@ -15,7 +16,7 @@ module Facilities
       requests.map do |req|
         if req.response.timed_out?
           Rails.logger.error "GIS request timed out: #{req.url}"
-          raise Common::Client::Errors::RequestTimeout
+          raise Common::Exceptions::ClientError.new('Facility request timed out', {})
         end
         raise Common::Client::Errors::ClientResponse.new(req.response.code, {}) unless
           req.response.success?
