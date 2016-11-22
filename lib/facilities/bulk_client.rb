ffe316254ed7e9cd62764ff3078335347a06b691
Patrick Vinograd <patrick@adhocteam.us>
2016-11-21 16:32:07 -0800

    Rubocop

--- lib/facilities/bulk_client.rb
+++ lib/facilities/bulk_client.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
- require 'facilities/gis_middleware'
+# frozen_string_literal: true
+require 'facilities/gis_middleware'
 
-# Client class for doing periodic bulk refreshes of GIS data. 
-# Allows inspection of last modified date to update only when needed. 
+# Client class for doing periodic bulk refreshes of GIS data.
+# Allows inspection of last modified date to update only when needed.
 module Facilities
   class BulkClient
-
     BATCH_SIZE = 1000.0
     OPEN_TIMEOUT = 2
     REQUEST_TIMEOUT = 6
 
-    def initialize(url) 
+    def initialize(url)
       @url = url
-      @conn = Faraday.new(:url => @url) do |conn|
+      @conn = Faraday.new(url: @url) do |conn|
         conn.options.open_timeout = OPEN_TIMEOUT
         conn.options.timeout = REQUEST_TIMEOUT
         conn.use Facilities::Middleware::GISJson
@@ -23,7 +23,7 @@ module Facilities
     end
 
     def last_edit_date
-      response = @conn.get '', { :f => 'json' }
+      response = @conn.get '', f: 'json'
       response.body.dig('editingInfo', 'lastEditDate')
     rescue Faraday::ConnectionFailed => e
       Rails.logger.warn "Facility lastEditDate check connection failed: #{e.message}"
@@ -32,15 +32,15 @@ module Facilities
       Rails.logger.warn 'Facility lastEditDate check timeout'
       return nil
     rescue Facilities::Errors::ServiceError => e
-      Rails.logger.warn "Facility lastEditDate check request failed: "#{e.message}"
+      Rails.logger.warn "Facility lastEditDate check request failed: #{e.message}"
       return nil
     end
 
     def fetch_all
-       Rails.logger.info "Refreshing from #{@url}"
-       query_url = [@url, 'query'].join('/')
-       count = get_count(query_url)
-       fetch_in_batches(query_url, count) unless count.nil?
+      Rails.logger.info "Refreshing from #{@url}"
+      query_url = [@url, 'query'].join('/')
+      count = get_count(query_url)
+      fetch_in_batches(query_url, count) unless count.nil?
     rescue Faraday::ConnectionFailed => e
       Rails.logger.error "Facility fetch connection failed: #{e.message}"
       raise Facilities::Errors::ServiceError, 'Facility connection failed'
@@ -48,15 +48,15 @@ module Facilities
       Rails.logger.error 'Facility fetch timeout'
       raise Facilities::Errors::ServiceError, 'Facility connection timed out'
     rescue Facilities::Errors::ServiceError => e
-      Rails.logger.error "Facility fetch request failed: "#{e.message}"
+      Rails.logger.error "Facility fetch request failed: #{e.message}"
       raise
     end
- 
+
     COUNT_PARAMS = {
       where: '1=1',
       returnCountOnly: true,
       f: 'json'
-    }
+    }.freeze
 
     def get_count(url)
       response = @conn.get url, COUNT_PARAMS
@@ -64,26 +64,26 @@ module Facilities
     end
 
     ALL_PARAMS = {
-       where: '1=1',
-       inSR: 4326,
-       outSR: 4326,
-       returnGeometry: true,
-       outFields: '*',
-       f: 'json'
-     }
+      where: '1=1',
+      inSR: 4326,
+      outSR: 4326,
+      returnGeometry: true,
+      outFields: '*',
+      f: 'json'
+    }.freeze
 
     def fetch_in_batches(url, count)
-       max = (count / BATCH_SIZE).ceil - 1
-       facilities = []
-       (0..max).each do |i|
-         batch_params = {
-           resultOffset: (i * BATCH_SIZE).to_i,
-           resultRecordCount: BATCH_SIZE.to_i
-         }
-         response = @conn.get url, ALL_PARAMS.merge(batch_params)
-         facilities += response.body.dig('features').to_a
-       end
-       facilities
+      max = (count / BATCH_SIZE).ceil - 1
+      facilities = []
+      (0..max).each do |i|
+        batch_params = {
+          resultOffset: (i * BATCH_SIZE).to_i,
+          resultRecordCount: BATCH_SIZE.to_i
+        }
+        response = @conn.get url, ALL_PARAMS.merge(batch_params)
+        facilities += response.body.dig('features').to_a
+      end
+      facilities
     end
   end
 end
