188cb1d8aa0ed4e5dc1c8ecd945ffab75dfdabb5
Alastair Dawson <alastair@adhocteam.us>
2016-11-23 12:37:04 -0800

    adding memoist for saml settings service specs

--- lib/saml/settings_service.rb
+++ lib/saml/settings_service.rb
@@ -1,38 +1,43 @@
 # frozen_string_literal: true
+require 'memoist'
+
 module SAML
   # This class is responsible for putting together a complete ruby-saml
   # SETTINGS object, meaning, our static SP settings + the IDP settings
   # which must be fetched once and only once via IDP metadata.
   class SettingsService
     class << self
+      extend Memoist
+
       METADATA_RETRIES = 3
       OPEN_TIMEOUT = 2
       TIMEOUT = 15
 
       def saml_settings
-        @saml_settings ||= OneLogin::RubySaml::IdpMetadataParser.new.parse(metadata, settings: settings)
+        OneLogin::RubySaml::IdpMetadataParser.new.parse(metadata, settings: settings)
       rescue => e
         Rails.logger.error "SAML::SettingService failed to parse SAML metadata: #{e.message}"
         raise e
       end
+      memoize :saml_settings
 
       private
 
       def connection
-        @connection ||= Faraday.new(SAML_CONFIG['metadata_url']) do |conn|
+        Faraday.new(SAML_CONFIG['metadata_url']) do |conn|
           conn.options.open_timeout = OPEN_TIMEOUT
           conn.options.timeout = TIMEOUT
           conn.adapter :net_http
         end
       end
+      memoize :connection
 
       def metadata
-        return @metadata unless @metadata.nil?
         begin
           attempt ||= 0
           response = connection.get
           raise SAML::InternalServerError, response.status if (400..504).cover? response.status.to_i
-          @metadata = response.body
+          response.body
         rescue StandardError => e
           Rails.logger.error "Failed to load SAML metadata: #{e.message}: try #{attempt + 1} of #{METADATA_RETRIES}"
           if (attempt += 1) < METADATA_RETRIES
--- spec/lib/saml/saml_settings_service_spec.rb
+++ spec/lib/saml/saml_settings_service_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 require 'saml/settings_service'
 
 RSpec.describe SAML::SettingsService do
-  after(:each) do
-    SAML.send(:remove_const, 'SettingsService')
-    load 'lib/saml/settings_service.rb'
-  end
   describe '.saml_settings' do
     context 'with a 200 response' do
       it 'should only ever make 1 external web call' do
@@ -17,7 +13,7 @@ RSpec.describe SAML::SettingsService do
         expect(a_request(:get, SAML_CONFIG['metadata_url'])).to have_been_made.at_most_once
       end
       it 'returns a settings instance' do
-        expect(SAML::SettingsService.saml_settings).to be_an_instance_of(OneLogin::RubySaml::Settings)
+        expect(SAML::SettingsService.saml_settings(true)).to be_an_instance_of(OneLogin::RubySaml::Settings)
       end
     end
     context 'with metadata 500 responses' do
@@ -26,14 +22,14 @@ RSpec.describe SAML::SettingsService do
           status: 500, body: 'bad news bears'
         )
         expect(Rails.logger).to receive(:error).exactly(3).times.with(/Failed to load SAML metadata: 500: try \d of 3/)
-        SAML::SettingsService.saml_settings
+        SAML::SettingsService.saml_settings(true)
       end
     end
     context 'when a parsing error occurs' do
       it 'should log and reraise the error' do
         stub_request(:get, SAML_CONFIG['metadata_url']).to_return(status: 200, body: '<xml></')
         expect(Rails.logger).to receive(:error).once
-        expect { SAML::SettingsService.saml_settings }.to raise_error(REXML::ParseException)
+        expect { SAML::SettingsService.saml_settings(true) }.to raise_error(REXML::ParseException)
       end
     end
   end
