6e62ff4ae456c5953aeec4df09fa872365ed2d61
Patrick Vinograd <patrick@adhocteam.us>
2016-11-29 15:48:33 -0800

    Spec updates to avoid instance_variable_set

--- spec/lib/facilities/local_store_spec.rb
+++ spec/lib/facilities/local_store_spec.rb
@@ -4,19 +4,15 @@ require 'facilities/local_store'
 
 RSpec.describe Facilities::LocalStore do
   let(:url) { 'https://www.example.com/VBA_Facilities' }
-  let(:bulk_client_stub) { instance_double('Facilities::BulkClient') }
+  let(:service_error) { Facilities::Errors::ServiceError.new('GIS Error') }
   let(:adapter_stub) { class_double('VBAFacilityAdapter') }
   subject { described_class.new(url, nil, adapter_stub) }
-
-  before(:each) do
-    subject.instance_variable_set(:@client, bulk_client_stub)
-  end
 
   describe '#check_for_freshness' do
     context 'bulk client raises service error' do
       it 'should log error' do
-        allow(bulk_client_stub).to receive(:last_edit_date).and_return(1_234_567_890)
-        allow(bulk_client_stub).to receive(:fetch_all).and_raise(Facilities::Errors::ServiceError.new('GIS Error'))
+        allow_any_instance_of(Facilities::BulkClient).to receive(:last_edit_date).and_return(1_234_567_890)
+        allow_any_instance_of(Facilities::BulkClient).to receive(:fetch_all).and_raise(service_error)
         expect(Rails.logger).to receive(:error)
         subject.get('314c')
       end
@@ -24,8 +20,8 @@ RSpec.describe Facilities::LocalStore do
 
     context 'bulk client raises standard error' do
       it 'should log error' do
-        allow(bulk_client_stub).to receive(:last_edit_date).and_return(1_234_567_890)
-        allow(bulk_client_stub).to receive(:fetch_all).and_raise(StandardError.new('GIS Error'))
+        allow_any_instance_of(Facilities::BulkClient).to receive(:last_edit_date).and_return(1_234_567_890)
+        allow_any_instance_of(Facilities::BulkClient).to receive(:fetch_all).and_raise(StandardError.new('GIS Error'))
         expect(Rails.logger).to receive(:error)
         subject.get('314c')
       end
