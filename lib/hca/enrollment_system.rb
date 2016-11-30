95d32d113d1b0aad68417eba48e3df6d44c79cb2
Lihan Li <frankieteardrop@gmail.com>
2016-11-30 16:54:11 -0500

    add form template

--- lib/hca/enrollment_system.rb
+++ lib/hca/enrollment_system.rb
@@ -2,6 +2,22 @@ module HCA
   module EnrollmentSystem
     module_function
 
+    FORM_TEMPLATE = {
+      form: {
+        formIdentifier: {
+          type: '100',
+          value: '1010EZ',
+          version: 1986360435
+        }
+      },
+      identity: {
+        authenticationLevel: {
+          type: '100',
+          value: 'anonymous'
+        }
+      }
+    }
+
     def transform(data)
     end
   end
