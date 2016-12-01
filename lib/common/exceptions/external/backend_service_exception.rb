c0d380111a18c811117c364b60bef1800d52f659
Kam Karshenas <kam@saneshark.com>
2016-12-01 18:04:54 -0500

    minor change to ensure status and source are set if available

--- lib/common/exceptions/external/backend_service_exception.rb
+++ lib/common/exceptions/external/backend_service_exception.rb
@@ -2,11 +2,16 @@
 module Common
   module Exceptions
     # BackendServiceException - This will return a generic error, to customize
-    # you must define the minor code in the locales file.
+    # you must define the minor code in the locales file and call this class from
+    # raise_error middleware.
     class BackendServiceException < BaseError
-      def initialize(i18n_key = nil, response_values = {})
-        @response_values = response_values
-        @i18n_key = i18n_key || 'backend_service_exception'
+      def initialize(key = nil, response_values = {})
+        if response_values[:status]&.between?(400, 999)
+          @response_values = response_values
+        else
+          raise NotImplementedError, 'only invoke from raise_error middleware'
+        end
+        @key = key || 'backend_service_exception'
       end
 
       def message
@@ -14,13 +19,28 @@ module Common
       end
 
       def errors
-        Array(SerializableError.new(i18n_data))
+        Array(SerializableError.new(i18n_data.merge(status: _status, detail: _detail, source: _source)))
       end
 
       private
 
+      # The http status code. This is very important and required for rendering
+      def _status
+        @response_values[:status] || 400
+      end
+
+      # Default detail should be based on i18n but fallback if it is explicitly set to nil
+      def _detail
+        i18n_data[:detail].presence || @response_values[:detail]
+      end
+
+      # This should usually be a developer message of some sort from the backend service
+      def _source
+        @response_values[:source]
+      end
+
       def i18n_key
-        "common.exceptions.#{@i18n_key}"
+        "common.exceptions.#{@key}"
       end
     end
   end
