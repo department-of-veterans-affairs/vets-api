f8b837db99cc10c81064cabc47eabab1abfb009c
Kam Karshenas <kam@saneshark.com>
2016-12-07 16:48:00 -0500

    these rubocop changes can go in a different PR

--- app/controllers/v0/messages_controller.rb
+++ app/controllers/v0/messages_controller.rb
@@ -35,11 +35,11 @@ module V0
       message_params[:id] = message_params.delete(:draft_id) if message_params[:draft_id].present?
       create_message_params = { message: message_params }.merge(upload_params)
 
-      if message.uploads.present?
-        client_response = client.post_create_message_with_attachment(create_message_params)
-      else
-        client_response = client.post_create_message(message_params)
-      end
+      client_response = if message.uploads.present?
+                          client.post_create_message_with_attachment(create_message_params)
+                        else
+                          client.post_create_message(message_params)
+                        end
 
       render json: client_response,
              serializer: MessageSerializer,
