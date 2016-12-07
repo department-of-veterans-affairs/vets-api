8bf9f3ce03a1060631b3dbbbb676182abad0146a
Kam Karshenas <kam@saneshark.com>
2016-12-07 15:45:27 -0500

    moved normalization of errors into its own middleware

---
+++ lib/common/client/middleware/response/mhv_errors.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+module Common
+  module Client
+    module Middleware
+      module Response
+        class MHVErrors < Faraday::Response::Middleware
+          def on_complete(env)
+            return if env.success?
+            env[:body]['code'] = env[:body].delete('errorCode')
+            env[:body]['detail'] = env[:body].delete('message')
+            env[:body]['source'] = env[:body].delete('developerMessage')
+          end
+        end
+      end
+    end
+  end
+end
+
+Faraday::Response.register_middleware mhv_errors: Common::Client::Middleware::Response::MHVErrors
