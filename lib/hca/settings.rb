c186c720fb51e1205944bb58381bf0b0519dcf5f
Patrick Vinograd <patrick@adhocteam.us>
2016-12-16 17:16:41 -0800

    Avoid uninitialized constant

--- lib/hca/settings.rb
+++ lib/hca/settings.rb
@@ -14,7 +14,11 @@ module HCA
     HEALTH_CHECK_ID = 377_609_264
     WSDL = Rails.root.join('config', 'health_care_application', 'wsdl', 'voa.wsdl')
     ENDPOINT = CONFIG['endpoint']
-    CERT_STORE = cert_store(CONFIG['ca']) if CONFIG['ca']
+    if CONFIG['ca']
+      CERT_STORE = cert_store(CONFIG['ca'])
+    else
+      CERT_STORE = nil
+    end
     SSL_CERT = begin
       OpenSSL::X509::Certificate.new(File.read(ENV['ES_CLIENT_CERT_PATH']))
     rescue => e
