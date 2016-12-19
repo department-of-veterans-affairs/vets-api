598faaa3ce5888ab0496d7cda071f2ce7f6e04cc
Carl Johnson <me@carlmjohnson.net>
2016-12-19 14:18:26 -0500

    Add some more tests

--- spec/lib/evss/auth_headers_spec.rb
+++ spec/lib/evss/auth_headers_spec.rb
@@ -3,11 +3,26 @@ require 'rails_helper'
 require 'evss/auth_headers'
 
 describe EVSS::AuthHeaders do
-  let(:current_user) { FactoryGirl.create(:loa3_user) }
-
   subject { described_class.new(current_user) }
 
-  it 'has the right LoA' do
-    expect(subject.to_h['va_eauth_assurancelevel']).to eq '3'
+  context 'with an LoA3 user' do
+    let(:current_user) { FactoryGirl.build(:loa3_user) }
+
+    it 'has the right LoA' do
+      expect(subject.to_h['va_eauth_assurancelevel']).to eq '3'
+    end
+
+    it 'has only lowercase first letters in key names' do
+      # EVSS requires us to pass the HTTP headers as lowercase
+      expect(subject.to_h.find{|k,_| k.match /^[[:upper:]]/}).to be nil
+    end
+  end
+
+  context 'with an LoA1 user' do
+    let(:current_user) { FactoryGirl.build(:loa1_user) }
+
+    it 'has the right LoA' do
+      expect(subject.to_h['va_eauth_assurancelevel']).to eq '1'
+    end
   end
 end
