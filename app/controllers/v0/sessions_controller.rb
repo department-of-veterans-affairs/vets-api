f59af566713782e8ef4aedfc0e5efa1bf77d39e7
omgitsbillryan <bill.ryan@adhocteam.us>
2016-12-29 05:23:09 +1030

    add sentry logging for failed login attempts

--- app/controllers/v0/sessions_controller.rb
+++ app/controllers/v0/sessions_controller.rb
@@ -31,10 +31,7 @@ module V0
         async_create_evss_account(@current_user)
         redirect_to SAML_CONFIG['relay'] + '?token=' + @session.token
       else
-        logger.warn 'Authentication attempt did not succeed in saml_callback, reasons...'
-        logger.warn "  SAML Response: valid?=#{@saml_response.is_valid?} errors=#{@saml_response.errors}"
-        logger.warn "  User: valid?=#{@current_user&.valid?} errors=#{@current_user&.errors&.messages}"
-        logger.warn "  Session: valid?=#{@session&.valid?} errors=#{@session&.errors&.messages}"
+        log_errors
         redirect_to SAML_CONFIG['relay'] + '?auth=fail'
       end
     end
@@ -88,5 +85,25 @@ module V0
       end
     end
     # :nocov:
+
+    def log_errors
+      saml_error    = "valid?=#{@saml_response.is_valid?} errors=#{@saml_response.errors}"
+      user_error    = "valid?=#{@current_user&.valid?} errors=#{@current_user&.errors&.full_messages}"
+      session_error = "valid?=#{@session&.valid?} errors=#{@session&.errors&.full_messages}"
+
+      logger.error 'Login with ID.me failed!'
+      logger.error saml_error
+      logger.error user_error
+      logger.error session_error
+
+      if ENV['SENTRY_DSN'].present?
+        Raven.extra_context(
+          saml_response: saml_error,
+          session: session_error,
+          user: user_error
+        )
+        Raven.capture_exception('Login with ID.me failed!')
+      end
+    end
   end
 end
