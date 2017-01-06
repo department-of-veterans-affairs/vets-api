73c7273a011c694a088ad252515cabb00fd50e7d
Alastair Dawson <alastair.j.dawson@gmail.com>
2017-01-06 15:12:37 -0800

    Common SOAP client (#681)
    * creates a base SOAP client for MVI and HCA
    
    * update hca to use common soap client, added breakers support

---
+++ lib/common/client/configuration/rest.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+require_relative 'base'
+
+module Common
+  module Client
+    module Configuration
+      class REST < Base
+        self.request_types = %i(get put post delete).freeze
+        self.base_request_headers = {
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json',
+          'User-Agent' => user_agent
+        }.freeze
+      end
+    end
+  end
+end
