b3368ef437e4a4f6eba49fcd1def9e3e96551e87
austin-martinez <amartin857+github@gmail.com>
2017-01-10 14:05:51 -0500

    Update f/all-claims from master (#693)
    * Do some refactoring in preperation for addition new EDU forms
    
    Much of the existing EDU Form submission is built around the 22-1990 form,
    as that's the first one that we added. With the 1990E, 5490 and 1995 coming
    up we need to make the serialization code a bit more modular. This work
    pulls the 22-1990 specific code into a class that isolates the functions
    the erb template calls into and lays the groundwork for switching between
    form types in the same spool file.
    
    Existing conformance tests will be broken out to reflect that they're only
    applicable for the 22-1990, and tests will be added for the Form classes.
    
    There will be additional work around the model to store which type of form
    the submission is, the Year-To-Date report to include the new submission
    types, the JSON-Schema to be able to validate the differing types, and the
    metric collection process to differentiate counts between the form types.
    
    * Make it easier to re-submit submissions if needed
    
    * Re-add now-failing full_address tests
    
    * Add additional specs for EDU form rendering
    
    * Keep stats consistent with the existing format
    
    * Simplify #group_submissions_by_region by using ActiveSupport
    
    * Common SOAP client (#681)
    
    * creates a base SOAP client for MVI and HCA
    
    * update hca to use common soap client, added breakers support
    
    * Prevent redirect_to from logging the session token value (#679)
    
    * initial commit for removing the redirect_to.action_controller LogSubscriber
    
    * finishing touches
    
    * no longer need this initializer
    
    * Hca controller (#668)
    
    * fix routes
    
    * use submit_form in controller
    
    * allow empty values for enrollment system methods
    
    * render success spec working
    
    * controller should handle service errors
    
    * capture error with raven
    
    * lint
    
    * dont need to stub return val
    
    * validate form to json schema
    
    * render json schema errors
    
    * schema validation errors spec
    
    * lint
    
    * soap module renamed
    
    * Update Brakeman (#692)
    
    * Update Brakeman
    
    From 3.4.1+, Brakeman reads the .ruby_version file. This will get
    rid of the parse errors that we've been seeing in the output for
    this. It also introduces a new `plain` type of output, which in
    most cases I've seen makes the output readable on a normal sized
    terminal.
    
    * Unlock Brakeman

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,10 @@ Rails.application.routes.draw do
 
     resource :education_benefits_claims, only: [:create]
 
-    resource :health_care_application, only: [:create] do
-      resource :healthcheck, only: [:show], to: 'health_care_applications#healthcheck'
+    resource :health_care_applications, only: [:create] do
+      collection do
+        get(:healthcheck)
+      end
     end
 
     resource :disability_rating, only: [:show]
--- spec/lib/common/client/middleware/request/soap_headers_spec.rb
+++ spec/lib/common/client/middleware/request/soap_headers_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 require 'rails_helper'
-require 'soap/middleware/request/headers'
+require 'common/client/middleware/request/soap_headers'
 
-describe SOAP::Middleware::Request::Headers do
+describe Common::Client::Middleware::Request::SOAPHeaders do
   context 'with a request without headers' do
     let(:env) { instance_double('Faraday::Env') }
     let(:request_headers) { { 'User-Agent' => 'Faraday v0.9.2', 'Soapaction' => 'PRPA_IN201305UV02' } }
@@ -17,7 +17,6 @@ describe SOAP::Middleware::Request::Headers do
       subject.call(env)
       expect(env.request_headers).to eq(
         'Content-Length' => '11',
-        'Content-Type' => 'text/xml;charset=UTF-8',
         'Date' => now.utc.strftime('%a, %d %b %Y %H:%M:%S GMT'),
         'Soapaction' => 'PRPA_IN201305UV02',
         'User-Agent' => 'Faraday v0.9.2'
