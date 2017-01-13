614099b1b278bcd90087760fc1d2a102e786cf6b
Aubrey Holland <aubreyholland@gmail.com>
2017-01-13 07:28:24 -0500

    trigger build

--- app/controllers/v0/sessions_controller.rb
+++ app/controllers/v0/sessions_controller.rb
@@ -35,7 +35,7 @@ module V0
         redirect_to SAML_CONFIG['relay'] + '?token=' + @session.token
 
         obscure_token = Session.obscure_token(@session.token)
-        Rails.logger.info("Logged in user with id: #{@session.uuid}, token: #{obscure_token}")
+        Rails.logger.info("Logged in user with id #{@session.uuid}, token #{obscure_token}")
       else
         log_errors
         redirect_to SAML_CONFIG['relay'] + '?auth=fail'
