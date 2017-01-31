f8fa5738da6300549a9ff12b0c92350f453a887f
Lihan Li <frankieteardrop@gmail.com>
2017-01-30 21:05:06 -0500

    use build_form_for_user in veteran_to_save_submit_form

--- lib/hca/enrollment_system.rb
+++ lib/hca/enrollment_system.rb
@@ -615,7 +615,7 @@ module HCA
     def veteran_to_save_submit_form(veteran, current_user)
       return {} if veteran.blank?
 
-      request = FORM_TEMPLATE.deep_dup
+      request = build_form_for_user(current_user)
       request['va:form']['va:summary'] = veteran_to_summary(veteran)
       request['va:form']['va:applications'] = {
         'va:applicationInfo' => [{
