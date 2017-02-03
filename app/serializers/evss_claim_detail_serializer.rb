fc0c35821d48635044b99a56b4064b11f22a6033
austin-martinez <amartin857+github@gmail.com>
2017-02-03 14:55:15 -0500

    Use upload date when present (#751)

--- app/serializers/evss_claim_detail_serializer.rb
+++ app/serializers/evss_claim_detail_serializer.rb
@@ -78,12 +78,14 @@ class EVSSClaimDetailSerializer < EVSSClaimBaseSerializer
   EVENT_DATE_FIELDS = %i(
     closed_date
     received_date
+    upload_date
     opened_date
     requested_date
     suspense_date
   ).freeze
 
   def create_tracked_item_event(type, obj)
+    documents = create_documents(obj['vba_documents'] || [])
     event = {
       type: type,
       tracked_item_id: obj['tracked_item_id'],
@@ -98,7 +100,8 @@ class EVSSClaimDetailSerializer < EVSSClaimBaseSerializer
       received_date: date_or_nil_from(obj, 'received_date'),
       closed_date: date_or_nil_from(obj, 'closed_date'),
       suspense_date: date_or_nil_from(obj, 'suspense_date'),
-      documents: create_documents(obj['vba_documents'] || [])
+      documents: documents,
+      upload_date: latest_upload_date(documents)
     }
     event[:date] = event.slice(*EVENT_DATE_FIELDS).values.compact.first
     event
@@ -130,5 +133,9 @@ class EVSSClaimDetailSerializer < EVSSClaimBaseSerializer
     date = obj[key]
     return nil unless date.present?
     Date.strptime(date.to_s, format)
+  end
+
+  def latest_upload_date(documents)
+    documents.map { |doc| doc[:upload_date] }.sort.reverse.first
   end
 end
