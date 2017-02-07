4246fbb426bdf89648594e4ad0f7c4880c269f11
lihan <frankieteardrop@gmail.com>
2017-02-07 16:21:29 -0500

    [wip] form 1995 model changes (#683)
    * add form type attr
    
    * add form_type route
    
    * validate form_type
    
    * is form type helper methods
    
    * run generate benefits only for 1990s
    
    * pass in model to education facility
    
    * region_for 1995 changes
    
    * add form_type to edu benefits submissions table
    
    * create_education_benefits_submission for 1995
    
    * fix form_matches_schema for 1995
    
    * form_type works in controller
    
    * lint
    
    * dont use conditionals in region_for
    
    * 1995 year to date report changes (#689)
    
    * support 1995 in calculate submissions
    
    * fix calculate_submissions spec
    
    * lint
    
    * dont need to put privacy agreement in twice
    
    * change create csv header for 1995
    
    * fixed create_csv_data_row
    
    * fix create_csv_array
    
    * fix calculating grand totals
    
    * fix create csv array test
    
    * lint
    
    * reduce method length calculate_submissions
    
    * rename create data row to create data block
    
    * reduce length create_csv_data_block
    
    * add 1995 schema
    
    * fix form type inclusion spec
    
    * test 1995 schema validation
    
    * lint
    
    * set vets_json_schema to master
    
    * 1995 spool file (#714)
    
    * add stub class and template
    
    * build will look up form type
    
    * put format in base
    
    * put header in partial
    
    * use parse_with_template_path in format
    
    * header generation working
    
    * assert test form is valid
    
    * add school method to both forms
    
    * add va file num
    
    * add social and name
    
    * add home and mobile phone
    
    * add mailing addr
    
    * add civilianBenefitsAssistance
    
    * add civilianBenefitsAssistance to the json file
    
    * change form to match 2016 version
    
    * add benefit and education type
    
    * add education objective
    
    * add program name
    
    * add new school
    
    * school_name_and_addr method
    
    * add old school and reason for change
    
    * add bank account section
    
    * add footer template
    
    * add footer to 1995
    
    * fix deprecation warning
    
    * add generated kitchen sink spool
    
    * use run_at for 1995 spec
    
    * dont need ./
    
    * lint
    
    * 1995 conditional requires (#741)
    
    * working on get form schema
    
    * deep freeze form schemas
    
    * add conditional requires to schema
    
    * test conditional requires
    
    * lint
    
    * required fields in schema
    
    * 1995 privacy agreement (#760)
    
    * update vets-json-schema
    
    * fix edu claim spec
    
    * fix va file number regex validation
    
    * todo done

---
+++ db/migrate/20170105195635_add_form_type_to_education_benefits_submissions.rb
@@ -0,0 +1,8 @@
+class AddFormTypeToEducationBenefitsSubmissions < ActiveRecord::Migration
+  def change
+    add_column(:education_benefits_submissions, :form_type, :string, null: false, default: '1990')
+
+    remove_index(:education_benefits_submissions, name: :index_education_benefits_submissions_on_region_and_created_at)
+    add_index(:education_benefits_submissions, %w(region created_at form_type), name: 'index_edu_benefits_subs_ytd')
+  end
+end
