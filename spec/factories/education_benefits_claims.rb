7bc57de24b31bd091a613bcd660b5af3628dbc01
Lihan Li <frankieteardrop@gmail.com>
2017-02-06 22:11:40 -0500

    fix edu claim spec

--- spec/factories/education_benefits_claims.rb
+++ spec/factories/education_benefits_claims.rb
@@ -122,7 +122,8 @@ FactoryGirl.define do
 
     factory :education_benefits_claim_1995 do
       form({
-        veteranSocialSecurityNumber: '111223333'
+        veteranSocialSecurityNumber: '111223333',
+        privacyAgreementAccepted: true
       }.to_json)
       form_type('1995')
 
