74d863d280d762193e1fec792746e70b11884500
Kam Karshenas <kam@saneshark.com>
2017-02-17 17:51:36 -0500

    adding some specs to verify JSONAPI and TODO comment

--- spec/lib/gi/client_spec.rb
+++ spec/lib/gi/client_spec.rb
@@ -6,18 +6,27 @@ describe 'gi client' do
   let(:client) { GI::Client.new }
 
   it 'gets a list of autocomplete suggestions', :vcr do
-    client.get_autocomplete_suggestions('university')
+    client_response = client.get_autocomplete_suggestions('university')
+    expect(client_response[:data]).to be_an(Array)
+    # TODO: This should probably be fixed to conform to JSONAPI
+    # expect(client_response[:data].first.keys).to contain_exactly(:id, :type, :attributes)
   end
 
   it 'gets the calculator constants', :vcr do
-    client.get_calculator_constants
+    client_response = client.get_calculator_constants
+    expect(client_response[:data]).to be_an(Array)
+    expect(client_response[:data].first.keys).to contain_exactly(:id, :type, :attributes)
   end
 
   it 'gets search results', :vcr do
-    client.get_search_results(name: 'illinois')
+    client_response = client.get_search_results(name: 'illinois')
+    expect(client_response[:data]).to be_an(Array)
+    expect(client_response[:data].first.keys).to contain_exactly(:id, :type, :attributes, :links)
   end
 
   it 'gets the institution details', :vcr do
-    client.get_institution_details('20603613')
+    client_response = client.get_institution_details('20603613')
+    expect(client_response[:data]).to be_a(Hash)
+    expect(client_response[:data].keys).to contain_exactly(:id, :type, :attributes, :links)
   end
 end
