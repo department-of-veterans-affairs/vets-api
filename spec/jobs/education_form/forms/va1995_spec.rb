fc358b90b6dd6281632685be6d3bbe396e89f7b8
Lihan Li <frankieteardrop@gmail.com>
2017-02-17 14:08:24 -0500

    use test_spool_file for 1995

--- spec/jobs/education_form/forms/va1995_spec.rb
+++ spec/jobs/education_form/forms/va1995_spec.rb
@@ -10,20 +10,5 @@ RSpec.describe EducationForm::Forms::VA1995 do
     expect(described_class::TYPE).to eq('22-1995')
   end
 
-  describe '#text' do
-    let(:kitchen_sink) { 'spec/fixtures/education_benefits_claims/1995/kitchen_sink.' }
-
-    before do
-      education_benefits_claim.form = File.read("#{kitchen_sink}json")
-      education_benefits_claim.save!
-      allow(education_benefits_claim).to receive(:id).and_return(1)
-    end
-
-    it 'should generate the spool file correctly', run_at: '2017-01-17 03:00:00 -0500' do
-      expected_text = File.read("#{kitchen_sink}spl").rstrip
-      expected_text.gsub!("\n", EducationForm::WINDOWS_NOTEPAD_LINEBREAK)
-
-      expect(subject.text).to eq(expected_text)
-    end
-  end
+  test_spool_file('1995', 'kitchen_sink')
 end
