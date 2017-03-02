a42639a620ea091ca71658f037302f27a5406a8b
Lihan Li <frankieteardrop@gmail.com>
2017-03-01 19:43:28 -0500

    remove unneeded code

--- spec/jobs/education_form/forms/va1990e_spec.rb
+++ spec/jobs/education_form/forms/va1990e_spec.rb
@@ -2,10 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe EducationForm::Forms::VA1990e do
-  let(:education_benefits_claim) { build(:education_benefits_claim_1990e) }
-
-  subject { described_class.new(education_benefits_claim) }
-
   %w(kitchen_sink simple).each do |test_application|
     test_spool_file('1990e', test_application)
   end
