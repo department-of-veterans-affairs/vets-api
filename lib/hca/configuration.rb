5cf87db25dad993fcd73cfb5777e44d91f943f9f
Kenneth Koski <ken@adhocteam.us>
2017-03-02 16:11:46 -0600

    Linting

--- lib/hca/configuration.rb
+++ lib/hca/configuration.rb
@@ -16,10 +16,10 @@ module HCA
     HEALTH_CHECK_ID = 377_609_264
     WSDL = Rails.root.join('config', 'health_care_application', 'wsdl', 'voa.wsdl')
     CERT_STORE = if Settings.hca.ca&.empty?
-      nil
-    else
-      cert_store(Settings.hca.ca)
-    end
+                   nil
+                 else
+                   cert_store(Settings.hca.ca)
+                 end
 
     SSL_CERT = begin
       OpenSSL::X509::Certificate.new(File.read(Settings.hca.cert_path))
