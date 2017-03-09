eae7c5c7b0c87d74c57b8736a1d8ca0118c1ded2
omgitsbillryan <bill.ryan@adhocteam.us>
2017-03-10 10:18:05 +1030

    think i spotted a bug in health care applications spec

--- spec/request/health_care_applications_request_spec.rb
+++ spec/request/health_care_applications_request_spec.rb
@@ -111,11 +111,11 @@ RSpec.describe 'Health Care Application Integration', type: [:request, :serializ
           allow_any_instance_of(HCA::Service).to receive(:post) do
             raise error
           end
-          stub_const('ENV', ENV.to_hash.merge('SENTRY_DSN' => 'doesnt matter'))
+          Settings.sentry.dsn = 'asdf'
         end
 
         it 'should render error message' do
-          expect(Raven).to receive(:capture_exception).with(error).once
+          expect(Raven).to receive(:capture_exception).with(error).twice
 
           subject
 
