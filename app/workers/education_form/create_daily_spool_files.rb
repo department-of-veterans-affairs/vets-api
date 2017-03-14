c72babeb7b9a2f6422c52f64b83ef17c804c6579
Mark Olson <theothermarkolson@gmail.com>
2017-03-14 10:52:15 -0400

    Gate EDU types available for transmission

--- app/workers/education_form/create_daily_spool_files.rb
+++ app/workers/education_form/create_daily_spool_files.rb
@@ -9,6 +9,7 @@ module EducationForm
   end
 
   class CreateDailySpoolFiles
+    LIVE_FORM_TYPES = %w(1990 1995).freeze
     include Sidekiq::Worker
     sidekiq_options queue: 'default',
                     retry: 5
@@ -19,7 +20,7 @@ module EducationForm
     # Be *EXTREMELY* careful running this manually as it may overwrite
     # existing files on the SFTP server if one was already written out
     # for the day.
-    def perform(records: EducationBenefitsClaim.unprocessed)
+    def perform(records: EducationBenefitsClaim.unprocessed.where(form_type: LIVE_FORM_TYPES))
       return false if federal_holiday?
       # Group the formatted records into different regions
       if records.count.zero?
