771db83892d5da5a65b718077a4cc32b36de1ecf
Lihan Li <frankieteardrop@gmail.com>
2017-03-16 20:34:34 -0400

    finish kitchen sink

---
+++ spec/jobs/education_form/forms/va1990n_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe EducationForm::Forms::VA1990n do
+  %w(kitchen_sink).each do |test_application|
+    test_spool_file('1990n', test_application)
+  end
+end
