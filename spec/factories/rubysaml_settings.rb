a9b00eb39dc2192d16fdd5e3625a7f4620f1e917
Bill Ryan <bill.ryan@adhocteam.us>
2017-03-29 05:30:19 +1030

    SAML Health Check (#883)
    * initial commit for SAML metadata health check
    
    * added happy-path spec and some small fixes
    
    * code review comment update

--- spec/factories/rubysaml_settings.rb
+++ spec/factories/rubysaml_settings.rb
@@ -12,6 +12,11 @@ FactoryGirl.define do
     idp_slo_target_url              'https://api.idmelabs.com/saml/SingleLogoutService'
     idp_sso_target_url              'https://api.idmelabs.com/saml/SingleSignOnService'
     name_identifier_format          'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified'
+
+    trait :invalid_cert do
+      # fun fact - this is an actual cert ID.me once served to us in prod (it contains \r chars)
+      idp_cert File.read("#{::Rails.root}/spec/fixtures/files/invalid_idme_cert.crt")
+    end
   end
 
   factory :settings_no_context, class: 'OneLogin::RubySaml::Settings' do
