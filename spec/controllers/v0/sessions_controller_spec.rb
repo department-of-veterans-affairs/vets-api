2c58ce63fd874ea0209b7fbeb4b88547ea9e29aa
omgitsbillryan <bill.ryan@adhocteam.us>
2017-03-31 02:14:33 +1030

    use .and for spec

--- spec/controllers/v0/sessions_controller_spec.rb
+++ spec/controllers/v0/sessions_controller_spec.rb
@@ -120,13 +120,12 @@ RSpec.describe V0::SessionsController, type: :controller do
           expect(post(:saml_callback)).to redirect_to(Settings.saml.relay + '?auth=fail')
           expect(response).to have_http_status(:found)
         end
-        it 'increments the failed statsd counter' do
+        it 'increments the failed and total statsd counters' do
           once = { times: 1, value: 1 }
-          expect do
-            post(:saml_callback)
-          end.to trigger_statsd_increment(
-            described_class::STATSD_LOGIN_FAILED_KEY, tags: ['error:auth_too_early'], **once
-          ) & trigger_statsd_increment(described_class::STATSD_LOGIN_TOTAL_KEY, **once)
+          early_msg_tag = ['error:auth_too_early']
+          expect { post(:saml_callback) }
+            .to trigger_statsd_increment(described_class::STATSD_LOGIN_FAILED_KEY, tags: early_msg_tag, **once)
+            .and trigger_statsd_increment(described_class::STATSD_LOGIN_TOTAL_KEY, **once)
         end
       end
       context ' when a required saml attribute is missing' do
