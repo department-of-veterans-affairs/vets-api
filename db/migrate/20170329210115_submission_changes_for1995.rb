f5f2ecbeff7be275822db8ca1ae38e02ae89dcb0
Mark Olson <theothermarkolson@gmail.com>
2017-03-31 11:50:46 -0400

    Ensure that we reload the model in a migration after altering the table

--- db/migrate/20170329210115_submission_changes_for1995.rb
+++ db/migrate/20170329210115_submission_changes_for1995.rb
@@ -4,6 +4,8 @@ class SubmissionChangesFor1995 < ActiveRecord::Migration
       add_column(:education_benefits_submissions, benefit, :boolean, default: false, null: false)
     end
 
+    # reload the model info so that it picks up the new column
+    EducationBenefitsSubmission.reset_column_information
     EducationBenefitsSubmission.where(form_type: '1995').find_each do |education_benefits_submission|
       parsed_form = education_benefits_submission.education_benefits_claim&.parsed_form || {}
       benefit = parsed_form['benefit']
