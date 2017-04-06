ebbfcf127787fa0df799e96c490e5060143988fe
Patrick Vinograd <patrick@adhocteam.us>
2017-04-06 11:32:13 -0700

    Transit forward proxy for sideloading file

--- lib/bb/streaming_client.rb
+++ lib/bb/streaming_client.rb
@@ -19,7 +19,9 @@ module BB
 
     # doctype must be one of: txt or pdf
     def get_download_report(doctype, headers, yielder)
-      uri = URI('https://essapi-sysb.myhealth.va.gov/vetsgov/90mb.file')
+      uri = URI("#{Settings.mhv.rx.host}/vetsgov/1mb.file")
+      #uri = URI("#{Settings.mhv.rx.host}/vetsgov/90mb.file")
+      #uri = URI('https://essapi-sysb.myhealth.va.gov/vetsgov/90mb.file')
       # uri = URI.join(base_path, "bluebutton/bbreport/#{doctype}")
       request = Net::HTTP::Get.new(uri)
       @api_client.token_headers.each do |k, v|
