14ff5e27a41ae13a6c160c26e2b45f24898af813
Lihan Li <frankieteardrop@gmail.com>
2017-04-11 20:09:07 -0400

    report mailer spec

--- app/mailers/failed_claims_report_mailer.rb
+++ app/mailers/failed_claims_report_mailer.rb
@@ -15,7 +15,9 @@ class FailedClaimsReportMailer < ApplicationMailer
     mail(
       opt.merge(
         subject: "EVSS claims failed to upload",
-        body: failed_uploads.join('<br>')
+        body: failed_uploads.map do |failed_upload|
+          ERB::Util.html_escape(failed_upload)
+        end.join('<br>')
       )
     )
   end
