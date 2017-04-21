60fe46dee51aa3361c4724b683479b1ed2928477
Alastair Dawson <alastair@adhocteam.us>
2017-04-21 10:48:43 -0700

    completed validate_virus_free_spec

--- spec/lib/shrine/plugins/validate_virus_free_spec.rb
+++ spec/lib/shrine/plugins/validate_virus_free_spec.rb
@@ -1,34 +1,39 @@
 # frozen_string_literal: true
 require 'rails_helper'
-require 'shrine'
-require 'shrine/storage/memory'
 require 'shrine/plugins/validate_virus_free'
 
-
-
-
 describe Shrine::Plugins::ValidateVirusFree do
-  let(:uploader) do
-    uploader_class = Class.new(Shrine)
-    uploader_class.storages[:cache] = Shrine::Storage::Memory.new
-    uploader_class.storages[:store] = Shrine::Storage::Memory.new
-    uploader_class.new(:store)
-  end
-
-  let(:attacher) do
-    Object.send(:remove_const, "User") if defined?(User) # for warnings
-    user_class = Object.const_set("User", Struct.new(:avatar_data, :id))
-    user_class.include uploader.class::Attachment.new(:avatar)
-    user_class.new.avatar_attacher
-  end
-
   describe '#validate_virus_free' do
-    it 'adds an error if clam scan returns not safe' do
-      a = attacher do
-        plugin :validation_helpers
-        plugin :validate_virus_free
+    let(:klass) do
+      Class.new do
+        include Shrine::Plugins::ValidateVirusFree::AttacherMethods
+        def get
+          'stuff'
+        end
       end
-      puts a.methods
+    end
+
+    let(:instance) { klass.new }
+
+    before(:each) do
+      allow_any_instance_of(klass).to receive(:get)
+        .and_return(instance_double('Shrine::UploadedFile', to_io: instance_double('File', path: 'foo/bar.jpg')))
+    end
+
+    it 'adds an error if clam scan returns not safe' do
+      allow(ClamScan::Client).to receive(:scan)
+        .and_return(instance_double('ClamScan::Response', safe?: false))
+
+      expect(instance).to receive(:add_error).once
+      instance.validate_virus_free
+    end
+
+    it 'does not add an error if clam scan returns safe' do
+      allow(ClamScan::Client).to receive(:scan)
+        .and_return(instance_double('ClamScan::Response', safe?: true))
+
+      expect(instance).not_to receive(:add_error)
+      instance.validate_virus_free
     end
   end
 end
