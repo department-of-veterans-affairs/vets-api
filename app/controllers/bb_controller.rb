89db5a1c19b9109b321c5a246ce9038e09895464
Patrick Vinograd <patrick@adhocteam.us>
2017-04-27 15:54:15 -0700

    Cleanup streaming_client refs

--- app/controllers/bb_controller.rb
+++ app/controllers/bb_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 require 'bb/client'
-require 'bb/streaming_client'
 
 class BBController < ApplicationController
   include ActionController::Serialization
@@ -10,10 +9,6 @@ class BBController < ApplicationController
 
   def client
     @client ||= BB::Client.new(session: { user_id: current_user.mhv_correlation_id })
-  end
-
-  def streaming_client
-    @streaming_client ||= BB::StreamingClient.new(client)
   end
 
   def raise_access_denied
