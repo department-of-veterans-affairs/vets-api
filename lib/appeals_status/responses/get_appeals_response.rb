d56ffb02b83bd359fb42258c3e842260718e5556
Aubrey Holland <aubreyholland@gmail.com>
2017-05-01 13:39:53 -0400

    Add support for appeals status (#952)
    * getting started with appeals
    
    * rename things again
    
    * wrap up appeals mock service/endpoints

---
+++ lib/appeals_status/responses/get_appeals_response.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+module AppealsStatus
+  module Responses
+    class GetAppealsResponse
+      attr_accessor :status
+      attr_accessor :appeals
+
+      def initialize(status:, appeals:)
+        @status = status
+        @appeals = appeals
+      end
+    end
+  end
+end
