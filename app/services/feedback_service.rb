3e3f54edbfe99798f9f45112fbd9103effc9e6ae
Alastair Dawson <alastair@adhocteam.us>
2017-05-03 11:26:45 -0400

    stubbed settings in feeback service spec

--- app/services/feedback_service.rb
+++ app/services/feedback_service.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 class FeedbackService
-
-  ACCESS_TOKEN = Settings.github.access_token
-  FEEDBACK_REPO = Settings.github.feedback_repo
-
   def submit_feedback(title, message, category, email, url)
     message = "Feedback submitted from #{email} via #{url}:\n> #{message}"
     labels = %w(UserVoice)
     labels << category
-    client.create_issue(FEEDBACK_REPO, title, message, options = {labels: labels})
+    client.create_issue(Settings.github.feedback_repo, title, message, options = {labels: labels})
   end
 
   private
 
   def client
-    @client ||= Octokit::Client.new(access_token: ACCESS_TOKEN)
+    @client ||= Octokit::Client.new(access_token: Settings.github.access_token)
   end
 end
