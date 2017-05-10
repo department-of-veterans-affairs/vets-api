92c3782f56c35f0780ffbc3f7ddb6cd2c4e1bdef
marc harbatkin <marc.harbatkin@va.gov>
2017-05-10 10:50:17 -0400

    Interim

--- app/swagger/requests/messages/messages.rb
+++ app/swagger/requests/messages/messages.rb
@@ -5,24 +5,6 @@ module Swagger
       class Messages
         include Swagger::Blocks
 
-        swagger_path '/v0/messaging/health/messages/{id}' do
-          operation :get do
-            key :description, 'Get the message'
-            key :operationId, 'messagesShow'
-            key :tags, %w(messages)
-
-            parameter name: :id, in: :path, required: true, type: :integer, description: 'the message id'
-
-            response 200 do
-              key :description, 'message show response'
-
-              schema do
-                key :'$ref', :Message
-              end
-            end
-          end
-        end
-
         swagger_path '/v0/messaging/health/messages/{id}/thread' do
           operation :get do
             key :description, 'Gets the messages in a thread'
@@ -36,6 +18,24 @@ module Swagger
 
               schema do
                 key :'$ref', :MessagesThread
+              end
+            end
+          end
+        end
+
+        swagger_path '/v0/messaging/health/messages/{id}' do
+          operation :get do
+            key :description, 'Get the message'
+            key :operationId, 'messagesShow'
+            key :tags, %w(messages)
+
+            parameter name: :id, in: :path, required: true, type: :integer, description: 'the message id'
+
+            response 200 do
+              key :description, 'message show response'
+
+              schema do
+                key :'$ref', :Message
               end
             end
           end
