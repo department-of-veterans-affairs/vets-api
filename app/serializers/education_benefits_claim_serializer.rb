d8bc5a19bc97a554beb52c40907176ebc633e0ab
Mark Olson <theothermarkolson@gmail.com>
2017-05-11 12:11:02 -0400

    Revert serializer change

--- app/serializers/education_benefits_claim_serializer.rb
+++ app/serializers/education_benefits_claim_serializer.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 class EducationBenefitsClaimSerializer < ActiveModel::Serializer
   attributes :id, :form, :submitted_at, :regional_office, :confirmation_number
-  attribute :spool, if: proc { Rails.env.development? }
-
-  def spool
-    EducationForm::Forms::Base.build(object).text
-  rescue
-    ''
-  end
 end
