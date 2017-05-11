7892af084833ee0c812e9c7edf70bac0ce50849f
Mark Olson <theothermarkolson@gmail.com>
2017-05-11 11:28:53 -0400

    Return spool file entry in edu development response

--- app/serializers/education_benefits_claim_serializer.rb
+++ app/serializers/education_benefits_claim_serializer.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 class EducationBenefitsClaimSerializer < ActiveModel::Serializer
   attributes :id, :form, :submitted_at, :regional_office, :confirmation_number
+  attribute :spool, if: proc { Rails.env.development? }
+
+  def spool
+    EducationForm::Forms::Base.build(object).text
+  rescue
+    ''
+  end
 end
