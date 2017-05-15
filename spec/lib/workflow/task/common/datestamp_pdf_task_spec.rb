d78807dd1d7c0e53a2c93199104a99407fde4db3
Alastair Dawson <alastair@adhocteam.us>
2017-05-15 15:22:58 -0700

    converted datestamp pdf job to workflow task

--- spec/lib/workflow/task/common/datestamp_pdf_task_spec.rb
+++ spec/lib/workflow/task/common/datestamp_pdf_task_spec.rb
@@ -1,18 +1,29 @@
 # frozen_string_literal: true
 require 'rails_helper'
+require 'workflow/task/common/datestamp_pdf_task'
 
-RSpec.describe DocumentUploads::PdfDatestampJob do
-  describe '#perform' do
+RSpec.describe Workflow::Task::Common::DatestampPdfTask do
+  describe '#run' do
     before(:all) do
-      @file_path = Rails.root.join('spec/fixtures/files/stamp_pdf_1_page.pdf')
+      @file_path = Rails.root.join('spec', 'fixtures', 'files', 'stamp_pdf_1_page.pdf')
       Prawn::Document.new.render_file @file_path
+    end
+
+    let(:attacher) do
+      a = Shrine::Attacher.new(InternalAttachment.new, :file)
+      a.assign(File.open(@file_path))
+      a
+    end
+
+    let(:instance) do
+      described_class.new({ text: 'Received via vets.gov at', x: 10, y: 10 }, internal: { file: attacher.read })
     end
 
     context 'with a succesful pdf stamp' do
       it 'should add text with a datestamp at the given location' do
         Timecop.travel(Time.zone.local(1999, 12, 31, 23, 59, 59)) do
-          subject.perform(@file_path, 'Received via vets.gov at', 0, 0)
-          text_analysis = PDF::Inspector::Text.analyze(@file_path)
+          instance.run(text: 'Received via vets.gov at', x: 10, y: 10)
+          text_analysis = PDF::Inspector::Text.analyze(instance.file.read)
           expect(text_analysis.strings.first).to eq('Received via vets.gov at 1999-12-31T23:59:59+00:00')
         end
       end
@@ -25,9 +36,9 @@ RSpec.describe DocumentUploads::PdfDatestampJob do
         it 'should log and reraise the error and not call stamp' do
           allow(Prawn::Document).to receive(:generate).and_raise(error_message)
           expect(Rails.logger).to receive(:error).once.with("Failed to generate datestamp file: #{error_message}")
-          expect(subject).not_to receive(:stamp)
+          expect(instance).not_to receive(:stamp)
           expect do
-            subject.perform(@file_path, 'Received via vets.gov at', 0, 0)
+            instance.run(text: 'Received via vets.gov at', x: 10, y: 10)
           end.to raise_error(StandardError, error_message)
         end
       end
@@ -38,7 +49,7 @@ RSpec.describe DocumentUploads::PdfDatestampJob do
           expect(Rails.logger).to receive(:error).once.with("Failed to datestamp PDF file: #{error_message}")
           expect(File).to receive(:delete).once
           expect do
-            subject.perform(@file_path, 'Received via vets.gov at', 0, 0)
+            instance.run(text: 'Received via vets.gov at', x: 10, y: 10)
           end.to raise_error(StandardError, error_message)
         end
       end
