513163f3ebda80d7745e62161a21a776c28cd9b4
Aubrey Holland <aubreyholland@gmail.com>
2017-05-19 12:04:32 -0400

    Some tweaks for the terms and conditions (#993)
    * some tweaks for the terms and conditions
    
    * fix schema

--- app/models/terms_and_conditions.rb
+++ app/models/terms_and_conditions.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 class TermsAndConditions < ActiveRecord::Base
-  has_many :terms_and_conditions_acceptances
+  has_many :acceptances, class_name: 'TermsAndConditionsAcceptance'
 
   scope :latest, -> { find_by(latest: true) }
 
   validates :name, presence: true
-  validates :title, presence: true
   validates :terms_content, presence: true
   validates :yes_content, presence: true
-  validates :no_content, presence: true
   validates :version, presence: true
 end
