2e9ed4523f9ad7cb01a7853bcad8c302a112db77
Kam Karshenas <kam@saneshark.com>
2017-05-23 02:54:34 -0400

    get coverage to 90%+

--- spec/factories/mhv_accounts.rb
+++ spec/factories/mhv_accounts.rb
@@ -5,11 +5,5 @@ FactoryGirl.define do
     account_state 'unknown'
     registered_at nil
     upgraded_at nil
-
-    factory :mhv_account_with_user do
-      after(:build) do |mhv_account|
-        create(:user, uuid: mhv_account.user_uuid)
-      end
-    end
   end
 end
