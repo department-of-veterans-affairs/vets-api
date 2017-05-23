cdcd51598a79fb5aef6383f0b4149fa81ddfc393
Lihan Li <frankieteardrop@gmail.com>
2017-05-23 00:10:02 -0400

    spec deletes file afterwards

--- spec/lib/pdf_fill/filler_spec.rb
+++ spec/lib/pdf_fill/filler_spec.rb
@@ -5,9 +5,11 @@ require 'pdf_fill/filler'
 describe PdfFill::Filler do
   describe '#fill_form' do
     it 'should fill the form correctly' do
-      described_class.fill_form('21-527', {
+      file_path = described_class.fill_form('21-527', {
         'vaFileNumber' => "c12345678"
       })
+
+      File.delete(file_path)
     end
   end
 end
