279c02cd658d4c01c8df456fb4bb515efe737da3
Lihan Li <frankieteardrop@gmail.com>
2017-05-24 22:59:18 -0400

    merge fields spec

--- lib/pdf_fill/forms/va21p527ez.rb
+++ lib/pdf_fill/forms/va21p527ez.rb
@@ -1,9 +1,28 @@
 module PdfFill
   module Forms
     module VA21P527EZ
+      module_function
       KEY = {
         'vaFileNumber' => 'F[0].Page_5[0].VAfilenumber[0]'
       }
+
+      def combine_full_name(full_name)
+        combined_name = []
+
+        %w(first middle last suffix).each do |key|
+          combined_name << full_name[key]
+        end
+
+        combined_name.compact.join(' ')
+      end
+
+      def merge_fields(form_data)
+        form_data_merged = form_data.deep_dup
+
+        form_data_merged['veteranFullName'] = combine_full_name(form_data_merged['veteranFullName'])
+
+        form_data_merged
+      end
     end
   end
 end
---
+++ spec/lib/pdf_fill/forms/va21p527ez_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+require 'pdf_fill/forms/va21p527ez'
+
+describe PdfFill::Forms::VA21P527EZ do
+  let(:form_data) do
+    {
+      'veteranFullName' => {
+        'first' => 'john',
+        'middle' => 'middle',
+        'last' => 'smith',
+        'suffix' => 'Sr.'
+      }
+    }
+  end
+
+  describe '#combine_full_name' do
+    let(:full_name) do
+      form_data['veteranFullName']
+    end
+
+    subject do
+      described_class.combine_full_name(full_name)
+    end
+
+    context 'with missing fields' do
+      before do
+        full_name.delete('middle')
+        full_name.delete('suffix')
+      end
+
+      it 'should combine a full name' do
+        expect(subject).to eq("john smith")
+      end
+    end
+
+    it 'should combine a full name' do
+      expect(subject).to eq("john middle smith Sr.")
+    end
+  end
+
+  describe '#merge_fields' do
+    it 'should merge the right fields' do
+      expect(described_class.merge_fields(form_data)).to eq(
+        {"veteranFullName"=>"john middle smith Sr."}
+      )
+    end
+  end
+end
