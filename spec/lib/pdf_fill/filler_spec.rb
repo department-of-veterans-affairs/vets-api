ccb8325165e7619ee4a942562e0cf1f4346801ea
Lihan Li <frankieteardrop@gmail.com>
2017-05-25 20:57:59 -0400

    test that form data matches json schema

--- spec/lib/pdf_fill/filler_spec.rb
+++ spec/lib/pdf_fill/filler_spec.rb
@@ -4,14 +4,20 @@ require 'pdf_fill/filler'
 
 describe PdfFill::Filler do
   describe '#fill_form' do
-    it 'should fill the form correctly' do
-      file_path = described_class.fill_form('21P-527EZ', {
+    let(:form_data) do
+      {
         'vaFileNumber' => "c12345678",
         "veteranFullName" => {
           "first" => "Mark",
           "last" => "Olson"
         }
-      })
+      }
+    end
+
+    it 'should fill the form correctly' do
+      form_code = '21P-527EZ'
+      expect(form_data.to_json).to match_vets_schema(form_code)
+      file_path = described_class.fill_form(form_code, form_data)
 
       # File.delete(file_path)
     end
