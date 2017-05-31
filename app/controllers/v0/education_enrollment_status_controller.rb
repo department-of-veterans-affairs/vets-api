6f2246281f1b643db9ddaa8b8e1d17fd44f0b4a6
Bill Ryan <bill.ryan@adhocteam.us>
2017-05-31 17:11:03 -0400

    ignore coverage for unimplemented controller

--- app/controllers/v0/education_enrollment_status_controller.rb
+++ app/controllers/v0/education_enrollment_status_controller.rb
@@ -2,9 +2,11 @@
 module V0
   class EducationEnrollmentStatusController < ApplicationController
     # GET /edcuation_enrollment_status
+    # :nocov:
     def show
       # TODO: - attach to serializer
       head :ok
     end
+    # :nocov:
   end
 end
