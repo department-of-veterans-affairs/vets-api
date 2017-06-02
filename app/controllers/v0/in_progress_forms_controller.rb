b518efe6f4243eecc92ae5d8d0f3f98dcc2e4a16
lihan <frankieteardrop@gmail.com>
2017-06-01 20:48:32 -0400

    Add version and return_url fields to in progress forms (#1023)
    * add metadata field
    
    * add metadata to get req
    
    * fix x key inflection spec
    
    * permit metadata in put
    
    * fix put insert spec
    
    * lint

--- app/controllers/v0/in_progress_forms_controller.rb
+++ app/controllers/v0/in_progress_forms_controller.rb
@@ -4,13 +4,13 @@ module V0
     def show
       form_id = params[:id]
       form = InProgressForm.form_for_user(form_id, @current_user)
-      result = form ? form.form_data : FormProfile.new.prefill_form(form_id, @current_user)
+      result = form ? form.data_and_metadata : FormProfile.new.prefill_form(form_id, @current_user)
       render json: result
     end
 
     def update
       form = InProgressForm.where(form_id: params[:id], user_uuid: @current_user.uuid).first_or_initialize
-      result = form.update(form_data: params[:form_data])
+      result = form.update(form_data: params[:form_data], metadata: params[:metadata])
       raise Common::Exceptions::InternalServerError unless result
       head :ok
     end
