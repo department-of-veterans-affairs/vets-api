2f7f60bfe664e1ead65e25ebc09b7b1a8680d759
Lihan Li <frankieteardrop@gmail.com>
2017-06-07 19:30:20 -0400

    attendingCollege

--- lib/pdf_fill/forms/va21p527ez.rb
+++ lib/pdf_fill/forms/va21p527ez.rb
@@ -41,6 +41,7 @@ module PdfFill
           'phoneAreaCode' => 'F[0].Page_5[0].Unittelephoneareacode[0]'
         },
         'children' => {
+          'attendingCollege' => "children.attendingCollege[#{ITERATOR}]",
           'married' => "children.married[#{ITERATOR}]",
           'disabled' => "children.disabled[#{ITERATOR}]",
           'biological' => "children.biological[#{ITERATOR}]",
