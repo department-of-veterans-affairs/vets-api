216eb51fcc1b52cff3388d89c32af46190efb28e
Lihan Li <frankieteardrop@gmail.com>
2017-06-07 18:59:07 -0400

    merge fields

--- spec/lib/pdf_fill/forms/va21p527ez_spec.rb
+++ spec/lib/pdf_fill/forms/va21p527ez_spec.rb
@@ -553,7 +553,7 @@ describe PdfFill::Forms::VA21P527EZ do
          "cityState" => "Baltimore, MD, 21231, USA",
          "disabilities"=>[{"disabilityStartDate"=>"2016-12-01"}],
          "gender"=>"M",
-         "children" => [{"childFullName"=>"Mark1 Olson", "adopted"=>true, "previouslyMarried"=>true}, {"childFullName"=>"Mark2 Olson", "adopted"=>true, "previouslyMarried"=>true}, {"childFullName"=>"Mark3 Olson", "adopted"=>true, "previouslyMarried"=>true}],
+         "children" => [{"childFullName"=>"Mark1 Olson", "stepchild"=>true, "adopted"=>true, "previouslyMarried"=>true}, {"childFullName"=>"Mark2 Olson", "stepchild"=>true, "adopted"=>true, "previouslyMarried"=>true}, {"childFullName"=>"Mark3 Olson", "stepchild"=>true, "adopted"=>true, "previouslyMarried"=>true}],
          "genderMale"=>true,
          "genderFemale"=>false,
          "veteranDateOfBirth" => "1985-03-07",
