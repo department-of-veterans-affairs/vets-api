2e76ff7be7a46fd75123eddcb91fe1d55efa4fb6
Lihan Li <frankieteardrop@gmail.com>
2017-06-07 20:10:29 -0400

    marriage type

--- lib/pdf_fill/forms/va21p527ez.rb
+++ lib/pdf_fill/forms/va21p527ez.rb
@@ -35,6 +35,7 @@ module PdfFill
           'daysMissed' => "F[0].Page_5[0].Dayslostduetodisability[#{ITERATOR}]"
         },
         'marriages' => {
+          'marriageType' => "marriages.marriageType[#{ITERATOR}]",
           'reasonForSeparation' => "marriages.reasonForSeparation[#{ITERATOR}]"
         },
         'nationalGuard' => {
