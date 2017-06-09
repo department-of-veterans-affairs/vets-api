540f9043048b1d210ac7838c8327acfa1066dab4
Aubrey Holland <aubreyholland@gmail.com>
2017-06-09 13:24:06 -0400

    start hitting the real demo appeals service

--- lib/appeals_status/service.rb
+++ lib/appeals_status/service.rb
@@ -3,10 +3,12 @@ require 'appeals_status/models/appeals'
 require 'appeals_status/responses/get_appeals_response'
 
 module AppealsStatus
-  class Service
+  class Service < Common::Client::Base
+    configuration AppealsStatus::Configuration
+
     def get_appeals(user)
       response = try_mocks(:mocked_get_appeals_responses, user, AppealsStatus::Responses::GetAppealsResponse)
-      # response ||= make_the_actual_request
+      response ||= fetch_appeals(user)
       response
     end
 
@@ -14,6 +16,14 @@ module AppealsStatus
 
     def mocked_get_appeals_responses
       @mocked_get_appeals_responses ||= YAML.load_file('config/appeals_status/mock_get_appeals_responses.yml')
+    end
+
+    def fetch_appeals(user)
+      raw_response = perform(:get, '', {}, request_headers(user))
+      AppealsStatus::Responses::GetAppealsResponse.new(
+        status: raw_response.status,
+        appeals: AppealsStatus::Models::Appeals.new(raw_response.body)
+      )
     end
 
     def try_mocks(mock_responses_method, user, response_class)
@@ -33,6 +43,13 @@ module AppealsStatus
 
     def should_mock?
       [true, 'true'].include?(Settings.appeals_status.mock)
+    end
+
+    def request_headers(user)
+      {
+        'ssn' => user.ssn,
+        'Authorization' => "Token token=#{config.app_token}"
+      }
     end
   end
 end
--- spec/lib/appeals_status/service_spec.rb
+++ spec/lib/appeals_status/service_spec.rb
@@ -4,20 +4,21 @@ require 'appeals_status/service'
 require 'appeals_status/responses/get_appeals_response'
 
 describe AppealsStatus::Service do
-  let(:user) do
-    user_hash = {
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      middle_name: Faker::Name.last_name,
-      birth_date: Faker::Date.between(50.years.ago, 20.years.ago).to_s,
-      ssn: '796126859'
-    }
-    build(:loa3_user, user_hash)
-  end
   let(:appeals_service) { AppealsStatus::Service.new }
 
   describe 'get_appeals' do
     context 'with a working mock for the user' do
+      let(:user) do
+        user_hash = {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          middle_name: Faker::Name.last_name,
+          birth_date: Faker::Date.between(50.years.ago, 20.years.ago).to_s,
+          ssn: '796126859'
+        }
+        build(:loa3_user, user_hash)
+      end
+
       it 'returns the response' do
         expect(appeals_service.get_appeals(user)).to be_an_instance_of(AppealsStatus::Responses::GetAppealsResponse)
       end
@@ -28,6 +29,35 @@ describe AppealsStatus::Service do
 
       it 'returns 200 as the status' do
         expect(appeals_service.get_appeals(user).status).to eq(200)
+      end
+    end
+
+    context 'with real responses' do
+      let(:user) do
+        user_hash = {
+          first_name: Faker::Name.first_name,
+          last_name: Faker::Name.last_name,
+          middle_name: Faker::Name.last_name,
+          birth_date: Faker::Date.between(50.years.ago, 20.years.ago).to_s,
+          ssn: '111223333'
+        }
+        build(:loa3_user, user_hash)
+      end
+
+      before do
+        allow(appeals_service).to receive(:should_mock?) { false }
+      end
+
+      it 'returns the real response' do
+        VCR.use_cassette('appeals_status/valid_appeals') do
+          expect(appeals_service.get_appeals(user)).to be_an_instance_of(AppealsStatus::Responses::GetAppealsResponse)
+        end
+      end
+
+      it 'contains the appeals' do
+        VCR.use_cassette('appeals_status/valid_appeals') do
+          expect(appeals_service.get_appeals(user).appeals.appeals.count).to eq(2)
+        end
       end
     end
   end
