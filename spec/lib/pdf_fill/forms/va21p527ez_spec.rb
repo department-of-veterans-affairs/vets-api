1b06b7d1351094a4c90eee3d9a5023c7a616890a
Lihan Li <frankieteardrop@gmail.com>
2017-06-09 18:14:17 -0400

    merge fields

--- spec/lib/pdf_fill/forms/va21p527ez_spec.rb
+++ spec/lib/pdf_fill/forms/va21p527ez_spec.rb
@@ -709,6 +709,7 @@ describe PdfFill::Forms::VA21P527EZ do
          "activeServiceDateRangeEnd"=>"2013-04-10",
          "powDateRangeStart"=>"2012-04-10",
          "hasLiveWithSpouse" => true,
+         "spouseSocialSecurityNumber" => "111223334",
          "noLiveWithSpouse" => false,
          "powDateRangeEnd"=>"2013-05-10",
          "spouseDateOfBirth" => "2012-06-26",
