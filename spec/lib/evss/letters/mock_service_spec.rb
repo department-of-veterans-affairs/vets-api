ccc25a1909ac57bbac5029fd2c5a0ab146c5248e
Alastair Dawson <alastair@adhocteam.us>
2017-06-09 08:36:46 -0700

    raising ioerror if mock letter data yaml file not found

--- spec/lib/evss/letters/mock_service_spec.rb
+++ spec/lib/evss/letters/mock_service_spec.rb
@@ -4,14 +4,27 @@ require 'rails_helper'
 describe EVSS::Letters::MockService do
   describe '.find_by_user' do
     let(:root) { Rails.root }
-    before do
-      allow(Rails.root).to receive(:join).and_return(root.join('config', 'evss', 'mock_letters_response.yml.example'))
+    context 'when the yaml file exists' do
+      before do
+        allow(Rails.root).to receive(:join).and_return(root.join('config', 'evss', 'mock_letters_response.yml.example'))
+      end
+      it 'returns a hash of the hard coded response' do
+        response = subject.get_letters
+        expect(response.address.address_line1).to eq('2476 MAIN STREET')
+        expect(response.letters.count).to eq(8)
+        expect(response.letters.first.as_json).to eq('name' => 'Commissary Letter', 'letter_type' => 'commissary')
+      end
     end
-    it 'returns a hash of the hard coded response' do
-      response = subject.get_letters
-      expect(response.address.address_line1).to eq('2476 MAIN STREET')
-      expect(response.letters.count).to eq(8)
-      expect(response.letters.first.as_json).to eq('name' => 'Commissary Letter', 'letter_type' => 'commissary')
+
+    context 'when the yaml file does not exist' do
+      before do
+        allow(Rails.root).to receive(:join).and_return(root.join('config', 'evss', 'foo.yml'))
+      end
+      it 'raises an IOError' do
+        expect { subject.get_letters }.to raise_error(
+          IOError, 'letters mock data not found in path: /Users/vhaisfdawsoa/Documents/va/vets-api/config/evss/foo.yml'
+        )
+      end
     end
   end
 end
