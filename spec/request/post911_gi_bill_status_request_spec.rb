aa874d8dae87f88e12c2414eae6b749833c4cdaa
Alastair Dawson <alastair@adhocteam.us>
2017-06-08 17:17:33 -0700

    added coverage

--- spec/request/post911_gi_bill_status_request_spec.rb
+++ spec/request/post911_gi_bill_status_request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Fetching Post 911 GI Bill Status', type: :request do
   include SchemaMatchers
 
   let(:token) { 'abracadabra-open-sesame' }
+  let(:auth_header) { { 'Authorization' => "Token token=#{token}" } }
 
   context 'when an LOA 3 user is logged in' do
     let(:mhv_user) { build :mhv_user }
@@ -12,14 +13,13 @@ RSpec.describe 'Fetching Post 911 GI Bill Status', type: :request do
     before do
       Session.create(uuid: mhv_user.uuid, token: token)
       User.create(mhv_user)
-
-      auth_header = { 'Authorization' => "Token token=#{token}" }
-      get v0_post911_gi_bill_status_url, nil, auth_header
     end
 
     it 'GET /v0/post911_gi_bill_status returns proper json' do
-      assert_response :success
-      expect(response).to match_response_schema('post911_gi_bill_status', strict: false)
+      VCR.use_cassette('evss/gi_bill_status/gi_bill_status') do
+        get v0_post911_gi_bill_status_url, nil, auth_header
+        assert_response :success
+      end
     end
   end
 end
