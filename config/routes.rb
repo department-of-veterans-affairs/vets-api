232a8bf3b8ed92b7fe7a78dac94656a0a30e3d4b
Mark Olson <theothermarkolson@gmail.com>
2017-06-13 11:36:09 -0400

    Return listings of InprogressForms

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post '/auth/saml/callback', to: 'v0/sessions#saml_callback', module: 'v0'
 
   namespace :v0, defaults: { format: 'json' } do
-    resources :in_progress_forms, only: [:show, :update]
+    resources :in_progress_forms, only: [:index, :show, :update]
     resources :letters, only: [:index, :show]
 
     resource :sessions, only: [:new, :destroy] do
