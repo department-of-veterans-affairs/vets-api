507bfa4f3a233c044833386d51ac1e30390c9025
Mark Olson <theothermarkolson@gmail.com>
2017-06-13 11:36:09 -0400

    Return listings of InprogressForms

--- app/controllers/v0/in_progress_forms_controller.rb
+++ app/controllers/v0/in_progress_forms_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 module V0
   class InProgressFormsController < ApplicationController
+    def index
+      render json: InProgressForm.where(user_uuid: @current_user.uuid)
+    end
+
     def show
       form_id = params[:id]
       form = InProgressForm.form_for_user(form_id, @current_user)
