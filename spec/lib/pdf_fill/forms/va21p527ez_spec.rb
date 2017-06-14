52e46087943aa40e8aea69053acd1019218e030f
Lihan Li <frankieteardrop@gmail.com>
2017-06-14 19:34:30 -0400

    fix expand_expected_incomes spec

--- spec/lib/pdf_fill/forms/va21p527ez_spec.rb
+++ spec/lib/pdf_fill/forms/va21p527ez_spec.rb
@@ -18,7 +18,12 @@ describe PdfFill::Forms::VA21P527EZ do
           'salary' => 1
         }
       ).expand_expected_incomes).to eq(
-        [{ 'recipient' => 'Myself', 'amount' => 1 }, nil, nil, nil, nil, nil]
+        [{"recipient"=>"Myself", "amount"=>1},
+         nil,
+         {"recipient"=>"Myself", "amount"=>0},
+         nil,
+         nil,
+         nil]
       )
     end
   end
