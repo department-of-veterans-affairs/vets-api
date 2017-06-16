e3eb877e93ff432ebc2cece349b307a93261c224
Lihan Li <frankieteardrop@gmail.com>
2017-06-15 22:03:00 -0400

    generate method

--- lib/pdf_fill/extras_generator.rb
+++ lib/pdf_fill/extras_generator.rb
@@ -9,9 +9,13 @@ module PdfFill
     end
 
     def generate
-      Prawn::Document.generate("hello.pdf") do
-        text "Hello World!"
+      file_path = "tmp/pdfs/extras_#{Time.zone.now}.pdf"
+
+      Prawn::Document.generate(file_path) do
+        text(@text)
       end
+
+      file_path
     end
   end
 end
