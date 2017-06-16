aac57d13c839b43280101fcddba5519e244545a7
Lihan Li <frankieteardrop@gmail.com>
2017-06-15 21:23:20 -0400

    add text method extras generator

--- lib/pdf_fill/extras_generator.rb
+++ lib/pdf_fill/extras_generator.rb
@@ -1,4 +1,17 @@
 module PdfFill
   class ExtrasGenerator
+    def initialize
+      @text = ''
+    end
+
+    def add_text(text)
+      @text += "#{text}\n"
+    end
+
+    def generate
+      Prawn::Document.generate("hello.pdf") do
+        text "Hello World!"
+      end
+    end
   end
 end
