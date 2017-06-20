bc9eff30c1aa731e8014e37c9c3b09ffc39bf349
Mark Olson <theothermarkolson@gmail.com>
2017-06-20 14:39:16 -0400

    Add address method to Pension/Burial

--- lib/pension_burial/processing_office.rb
+++ lib/pension_burial/processing_office.rb
@@ -6,8 +6,21 @@ module PensionBurial
     MAPPINGS = Hash[CSV.read(FILE)]
     DEFAULT = 'Milwaukee'
 
-    def self.from_zip(code)
+    PO_BOX = {
+      'Milwaukee' => 5192,
+      'St. Paul' => 5365,
+      'Philadelphia' => 5206
+
+    }.freeze
+
+    def self.for_zip(code)
       MAPPINGS[code.to_s] || DEFAULT
+    end
+
+    def self.address_for(code)
+      office = for_zip(code)
+      box = PO_BOX[office]
+      ["Attention:  #{office} Pension Center", "P.O. Box #{box}", "Janesville, WI 53547-#{box}"]
     end
   end
 end
