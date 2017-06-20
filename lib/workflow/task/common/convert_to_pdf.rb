1cbe0d3969fd5bafbd781eb84c10ba93d24542f6
Alastair Dawson <alastair.j.dawson@gmail.com>
2017-06-19 22:33:30 -0700

    revert pdf conversion task, add image verification

--- lib/workflow/task/common/convert_to_pdf.rb
+++ lib/workflow/task/common/convert_to_pdf.rb
@@ -1,24 +1,19 @@
 # frozen_string_literal: true
 require 'workflow/task/shrine_file/base'
-require 'vips'
-require 'fastimage'
 
 module Workflow::Task::Common
   class ConvertToPdf < Workflow::Task::ShrineFile::Base
     def run
-      image = Vips::Image.new_from_file @file.download.path
+      unless @file.content_type =~ /image\//
+        raise IOError, "PDF conversion failed, unsupported file type: #{@file.content_type}"
+      end
 
-      Tempfile.create(%w(temp .jpg)) do |jpg|
-        image.write_to_file jpg.path
-        w,h = FastImage.size jpg.path
-
-        Tempfile.create(%w(temp .pdf)) do |pdf|
-          Prawn::Document.generate(pdf.path, page_size: [w,h]) do
-            margin = 36 # pdf margin .5 inches (72 pts/in)
-            image jpg.path, at: [-margin, h-margin], width: w, height: h
-          end
-          update_file(io: pdf)
+      Tempfile.create(%w(temp .pdf)) do |pdf|
+        MiniMagick::Tool::Convert.new do |convert|
+          convert << @file.download.path
+          convert << pdf.path
         end
+        update_file(io: pdf)
       end
     end
   end
