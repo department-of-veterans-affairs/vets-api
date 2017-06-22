c036a6504ce69cf5709e95c916057a8a40faeb22
omgitsbillryan <bill.ryan@adhocteam.us>
2017-06-23 02:55:53 +0930

    plain language error messaging

--- config/evss_connection_tester.rb
+++ config/evss_connection_tester.rb
@@ -1,11 +1,12 @@
-puts "Bill ux here #{ARGV[0]}"
+def to_boolean(str)
+  str == 'true'
+end
 
 raise "Must provide a SSN!" unless ARGV[0]
 ssn = ARGV[0]
 
-Settings.mvi.mock = false
+Settings.mvi.mock = to_boolean(ARGV[1]) if ARGV[1]
 
-# build user
 user = User.new(
   uuid: SecureRandom.uuid,
   first_name: 'Mark',
@@ -21,7 +22,25 @@ user = User.new(
   }
 )
 user.last_signed_in = Time.now.utc
+begin
+  user.va_profile
+rescue Common::Exceptions::ValidationErrors => e
+  puts "User not found in MVI!"
+end
+puts "User not found in  real MVI!" if user.va_profile.nil? && Settings.mvi.mock == false
 user.va_profile.edipi = '1005329660'
 headers = EVSS::AuthHeaders.new(user).to_h
 service = EVSS::GiBillStatus::Service.new(headers)
-service.get_gi_bill_status
+
+puts "Connecting to... #{Settings.evss.url}"
+response = service.get_gi_bill_status
+
+returned_user = {
+  first_name: response.first_name,
+  last_name: response.last_name,
+  date_of_birth: response.date_of_birth
+}
+
+puts "Response code: #{response.status}"
+puts "Non-200 response code! Check rails log for details" if response.status != 200
+puts "Returned User : #{returned_user}"
