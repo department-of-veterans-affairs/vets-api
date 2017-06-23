0c28c4cf7f14249a3e7bb6c1693707c8badcda6f
Alastair Dawson <alastair.j.dawson@gmail.com>
2017-06-23 11:35:41 -0700

    mocks out letters beneficiary options endpoint (#1098)

--- lib/evss/letters/mock_service.rb
+++ lib/evss/letters/mock_service.rb
@@ -2,15 +2,33 @@
 module EVSS
   module Letters
     Struct.new('RawLetterResponse', :body, :status)
+
     class MockService
+      def initialize(headers)
+        @headers = headers
+      end
+
       def mocked_response
         path = Rails.root.join('config', 'evss', 'mock_letters_response.yml')
         @response ||= YAML.load_file(path) if File.exist? path
       end
 
       def get_letters
-        raw_response = Struct::RawLetterResponse.new(mocked_response[:body], mocked_response[:status])
-        EVSS::Letters::LettersResponse.new(mocked_response[:status], raw_response)
+        letters = mocked_response[:get_letters][ssn]
+        raw_response = Struct::RawLetterResponse.new(letters[:body], letters[:status])
+        EVSS::Letters::LettersResponse.new(raw_response.status, raw_response)
+      end
+
+      def get_letter_beneficiary
+        beneficiary = mocked_response[:get_letter_beneficiary][ssn]
+        raw_response = Struct::RawLetterResponse.new(beneficiary[:body], beneficiary[:status])
+        EVSS::Letters::BeneficiaryResponse.new(raw_response.status, raw_response)
+      end
+
+      private
+
+      def ssn
+        @headers['va_eauth_pnid']
       end
     end
   end
