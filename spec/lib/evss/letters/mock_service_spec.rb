e21da97480bc5dfae07907425034b2af3e8b43ef
Alastair Dawson <alastair.j.dawson@gmail.com>
2017-06-23 12:08:55 -0700

    mocks GET /v0/letters/:id

--- spec/lib/evss/letters/mock_service_spec.rb
+++ spec/lib/evss/letters/mock_service_spec.rb
@@ -10,7 +10,10 @@ describe EVSS::Letters::MockService do
     subject { EVSS::Letters::MockService.new(auth_headers) }
 
     before do
-      allow(Rails.root).to receive(:join).and_return(root.join('config', 'evss', 'mock_letters_response.yml.example'))
+      allow(Rails.root).to receive(:join).and_call_original
+      allow(Rails.root).to receive(:join)
+        .with('config', 'evss', 'mock_letters_response.yml')
+        .and_return(root.join('config', 'evss', 'mock_letters_response.yml.example'))
     end
 
     describe 'get_letters' do
@@ -24,6 +27,13 @@ describe EVSS::Letters::MockService do
       it 'returns a hash of the hard coded response' do
         response = subject.get_letter_beneficiary
         expect(response.military_service.count).to eq(2)
+      end
+    end
+
+    describe 'download_letter_by_type' do
+      it 'returns a hash of the hard coded response' do
+        response = subject.download_letter_by_type('commissary')
+        expect(response).to include('%PDF-1.4')
       end
     end
   end
