c31b1d668c00c2e8ff06204498797c3e162ddb5f
Lihan Li <frankieteardrop@gmail.com>
2017-06-26 15:02:14 -0400

    Integration PdfFill code with Pension/Burial Claims

--- app/models/saved_claim/pension.rb
+++ app/models/saved_claim/pension.rb
@@ -9,6 +9,10 @@ class SavedClaim::Pension < SavedClaim
     PensionBurial::ProcessingOffice.address_for(open_struct_form.veteranAddress.postalCode)
   end
 
+  def attachment_keys
+    self.class::ATTACHMENT_KEYS
+  end
+
   def to_pdf
     # Tempfile.new('not-really-implimented', '.pdf')
   end
