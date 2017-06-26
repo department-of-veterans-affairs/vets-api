e26dd00a7f167be2706df215038d02af33e2ce67
Alastair Dawson <alastair.j.dawson@gmail.com>
2017-06-26 14:44:12 -0700

    revert letters mock example

--- lib/evss/letters/service.rb
+++ lib/evss/letters/service.rb
@@ -39,14 +39,19 @@ module EVSS
         EVSS::Letters::BeneficiaryResponse.new(e.response[:status])
       end
 
-      def download_by_type(type)
-        response = download_conn.get type
-        raise Common::Exceptions::RecordNotFound, params[:id] if response.status.to_i == 404
+      def download_by_type(type, options = nil)
+        if options.blank?
+          response = download_conn.get type
+        else
+          response = download_conn.post do |req|
+            req.url "#{type}/generate"
+            req.headers['Content-Type'] = 'application/json'
+            req.body = options
+          end
+        end
+
+        raise Common::Exceptions::RecordNotFound, type if response.status.to_i == 404
         response.body
-      end
-
-      def download_by_type_with_options(type, options)
-
       end
 
       def self.breakers_service
@@ -58,8 +63,8 @@ module EVSS
       def download_conn
         @download_conn ||= Faraday.new(base_url, headers: @headers, ssl: ssl_options) do |faraday|
           faraday.options.timeout = timeout
-          faraday.use      :breakers
-          faraday.adapter  :httpclient
+          faraday.use :breakers
+          faraday.adapter :httpclient
         end
       end
     end
