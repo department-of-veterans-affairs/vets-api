15547d698ca7f98330dab3a842ef481ce4d89da8
Marc Harbatkin <marc.harbatkin@va.gov>
2017-06-27 15:59:10 -0400

    Completed first pass preneeds (#1066)
    * Completed first pass cemeteries
    
    * Added burial states
    
    * oopsey forgot a spec
    
    * Adding discharge types
    
    * Adding attachment types
    
    * Added branch of service
    
    * Added getMilitaryRankForBranchOfService
    
    * Some adjustments
    
    * Namespaced preneeds
    
    * More changes
    
    * Removed some middleware
    
    * Removed some middleware comments
    
    * Removed unused schema
    
    * Commented out settings for preneeds and moved into settings.local
    
    * Futzed arround with preneeds settings
    
    * Futzed arround with preneeds settings
    
    * Finally...
    
    * Finally...
    
    * Using utc time
    
    * Changed settings
    
    * Update settings.yml
    
    * Changed settings/config

---
+++ app/models/preneeds/cemetery.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require 'common/models/base'
+
+module Preneeds
+  class Cemetery < Common::Base
+    attribute :cemetery_type, String
+    attribute :name, String
+    attribute :num, String
+
+    def id
+      num
+    end
+
+    # Default sort should be by name ascending
+    def <=>(other)
+      name <=> other.name
+    end
+  end
+end
