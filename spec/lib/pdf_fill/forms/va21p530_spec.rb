7a5dfc66cccf0dd5e0fb6e037ac32c63f66dc141
Lihan Li <frankieteardrop@gmail.com>
2017-06-27 15:53:41 -0400

    extract middle i spec

--- spec/lib/pdf_fill/forms/va21p530_spec.rb
+++ spec/lib/pdf_fill/forms/va21p530_spec.rb
@@ -21,5 +21,34 @@ describe PdfFill::Forms::VA21P530 do
         expect(subject.extract_middle_i).to eq(nil)
       end
     end
+
+    context 'with no middle name' do
+      let(:form_data) do
+        {
+          'veteranFullName' => {}
+        }
+      end
+
+      it 'should return nil' do
+        expect(subject.extract_middle_i).to eq(nil)
+      end
+    end
+
+    context 'with a middle name' do
+      let(:form_data) do
+        {
+          'veteranFullName' => {
+            'middle' => 'middle'
+          }
+        }
+      end
+
+      it 'should extract middle initial' do
+        expect(subject.extract_middle_i).to eq({
+          'middle' => 'middle',
+          'middleInitial' => 'm'
+        })
+      end
+    end
   end
 end
