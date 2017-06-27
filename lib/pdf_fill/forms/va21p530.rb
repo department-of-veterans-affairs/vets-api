36461badad803a34a5ea7a8129af48bfcf2247d4
Lihan Li <frankieteardrop@gmail.com>
2017-06-27 15:01:14 -0400

    veteran first name key

--- lib/pdf_fill/forms/va21p530.rb
+++ lib/pdf_fill/forms/va21p530.rb
@@ -1,6 +1,15 @@
 module PdfFill
   module Forms
     class VA21P530
+      KEY = {
+        'veteranFullName' => {
+          'first' => {
+            key: 'form1[0].#subform[36].VeteransFirstName[0]',
+            limit: 12,
+            question: "1. DECEASED VETERAN'S FIRST NAME"
+          }
+        }
+      }
     end
   end
 end
