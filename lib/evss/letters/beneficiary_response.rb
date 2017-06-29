5d41751f68b6681c29466a475722922480d03891
Bill Ryan <bill.ryan@adhocteam.us>
2017-06-30 00:31:48 +0930

    Move 5 attrs into benefit_iformation (#1134)
    * move 5 attrs into benefit_iformation
    
    * updated the happy-path beneficiary response VCR cassette

--- lib/evss/letters/beneficiary_response.rb
+++ lib/evss/letters/beneficiary_response.rb
@@ -6,11 +6,6 @@ module EVSS
     class BeneficiaryResponse < EVSS::Response
       attribute :benefit_information, EVSS::Letters::BenefitInformation
       attribute :military_service, Array[EVSS::Letters::MilitaryService]
-      attribute :has_adapted_housing, Boolean
-      attribute :has_chapter35_eligibility, Boolean
-      attribute :has_death_result_of_disability, Boolean
-      attribute :has_individual_unemployability_granted, Boolean
-      attribute :has_special_monthly_compensation, Boolean
 
       def initialize(status, response = nil)
         attributes = response.body if response
