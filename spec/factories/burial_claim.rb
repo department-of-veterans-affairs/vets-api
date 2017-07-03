78f3c1a1e7f01d961ab01c15c266bde8ae88c83d
Lihan Li <frankieteardrop@gmail.com>
2017-07-03 18:41:02 -0400

    revert burial claim

--- spec/factories/burial_claim.rb
+++ spec/factories/burial_claim.rb
@@ -12,9 +12,9 @@ FactoryGirl.define do
         deathDate: '1989-12-13',
         veteranSocialSecurityNumber: '111223333',
         claimantAddress: {
-          country: 'US',
+          country: 'USA',
           state: 'CA',
-          postalCode1: '90210',
+          postalCode: '90210',
           street: '123 Main St',
           city: 'Anytown'
         }
