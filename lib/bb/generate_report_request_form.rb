22a7aa3c68e39e74c00b904b05f13b48d6e7afe2
Kam Karshenas <kam@saneshark.com>
2017-07-06 22:43:20 -0400

    temporary hack for blue button

--- lib/bb/generate_report_request_form.rb
+++ lib/bb/generate_report_request_form.rb
@@ -7,7 +7,7 @@ module BB
 
     ELIGIBLE_DATA_CLASSES = %w( seiactivityjournal seiallergies seidemographics
                                 familyhealthhistory seifoodjournal healthcareproviders healthinsurance
-                                seiimmunizations labsandtests medicalevents medications militaryhealthhistory
+                                seiimmunizations labsandtests medicalevents militaryhealthhistory
                                 seimygoalscurrent seimygoalscompleted treatmentfacilities
                                 vitalsandreadings prescriptions medications vaallergies
                                 vaadmissionsanddischarges futureappointments pastappointments
@@ -25,15 +25,22 @@ module BB
     # MHV error is preferable.
     # validates :from_date, date: { before: :to_date, message: 'must be before to date' }
     validates :data_classes, presence: true
-    validate  :data_classes_belongs_to_eligible_data_classes
+    # TODO: temporary hack to make eligible data classes work
+    # validate  :data_classes_belongs_to_eligible_data_classes
+    def overridden_data_classes
+      eligible_data_classes & data_classes
+    end
 
     def initialize(client, attributes = {})
       super(attributes)
       @client = client
     end
 
+    # TODO: change this back to data_classes when hack can be properly removed.
+    # TODO: also rollback spec changes made as part of this PR:
+    # TODO: tag PR here
     def params
-      { from_date: from_date.try(:httpdate), to_date: to_date.try(:httpdate), data_classes: data_classes }
+      { from_date: from_date.try(:httpdate), to_date: to_date.try(:httpdate), data_classes: overridden_data_classes }
     end
 
     private
