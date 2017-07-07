0201e33eff3a86a00a3d959d6cdb7832ca2db5c4
Alastair Dawson <alastair.j.dawson@gmail.com>
2017-07-07 09:09:56 -0700

    don't include address in reponse meta on GET letters error

--- lib/evss/letters/letters_response.rb
+++ lib/evss/letters/letters_response.rb
@@ -19,7 +19,9 @@ module EVSS
       end
 
       def metadata
-        super().merge(address: address)
+        meta = super()
+        meta.merge!(address: address) if ok?
+        meta
       end
     end
   end
