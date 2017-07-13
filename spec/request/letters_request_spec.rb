52826269a0ad4c70ff525609229aa77ee3c3a2ac
Alastair Dawson <alastair.j.dawson@gmail.com>
2017-07-13 11:48:00 -0700

    [WIP] - EVSS Letter error mapping (#1185)

--- spec/request/letters_request_spec.rb
+++ spec/request/letters_request_spec.rb
@@ -31,11 +31,8 @@ RSpec.describe 'letters', type: :request do
       it 'should return a not authorized response' do
         VCR.use_cassette('evss/letters/unauthorized') do
           get '/v0/letters', nil, auth_header
-          expect(response).to have_http_status(:ok)
-          expect(response).to match_response_schema('letters')
-          expect(Oj.load(response.body).dig('meta', 'status')).to eq(
-            Common::Client::ServiceStatus::RESPONSE_STATUS[:not_authorized]
-          )
+          expect(response).to have_http_status(:forbidden)
+          expect(response).to match_response_schema('letters_errors', strict: false)
         end
       end
     end
@@ -44,24 +41,18 @@ RSpec.describe 'letters', type: :request do
       it 'should return a not authorized response' do
         VCR.use_cassette('evss/letters/letters_403') do
           get '/v0/letters', nil, auth_header
-          expect(response).to have_http_status(:ok)
-          expect(response).to match_response_schema('letters')
-          expect(Oj.load(response.body).dig('meta', 'status')).to eq(
-            Common::Client::ServiceStatus::RESPONSE_STATUS[:not_authorized]
-          )
+          expect(response).to have_http_status(:forbidden)
+          expect(response).to match_response_schema('letters_errors', strict: false)
         end
       end
     end
 
-    context 'with a 500 response' do
+    context 'with a generic 500 response' do
       it 'should return a not found response' do
         VCR.use_cassette('evss/letters/letters_500') do
           get '/v0/letters', nil, auth_header
-          expect(response).to have_http_status(:ok)
-          expect(response).to match_response_schema('letters')
-          expect(Oj.load(response.body).dig('meta', 'status')).to eq(
-            Common::Client::ServiceStatus::RESPONSE_STATUS[:server_error]
-          )
+          expect(response).to have_http_status(:internal_server_error)
+          expect(response).to match_response_schema('letters_errors')
         end
       end
     end
@@ -126,11 +117,8 @@ RSpec.describe 'letters', type: :request do
       it 'should return a not authorized response' do
         VCR.use_cassette('evss/letters/beneficiary_403') do
           get '/v0/letters/beneficiary', nil, auth_header
-          expect(response).to have_http_status(:ok)
-          expect(response).to match_response_schema('letter_beneficiary')
-          expect(Oj.load(response.body).dig('meta', 'status')).to eq(
-            Common::Client::ServiceStatus::RESPONSE_STATUS[:not_authorized]
-          )
+          expect(response).to have_http_status(:forbidden)
+          expect(response).to match_response_schema('letters_errors', strict: false)
         end
       end
     end
@@ -139,13 +127,76 @@ RSpec.describe 'letters', type: :request do
       it 'should return a not found response' do
         VCR.use_cassette('evss/letters/beneficiary_500') do
           get '/v0/letters/beneficiary', nil, auth_header
-          expect(response).to have_http_status(:ok)
-          expect(response).to match_response_schema('letter_beneficiary')
-          expect(Oj.load(response.body).dig('meta', 'status')).to eq(
-            Common::Client::ServiceStatus::RESPONSE_STATUS[:server_error]
-          )
+          expect(response).to have_http_status(:internal_server_error)
+          expect(response).to match_response_schema('letters_errors')
         end
       end
+    end
+  end
+
+  describe 'error handling' do
+    # EVSS is working on getting users that throw these errors in their CI env
+    # until then these VCR cassettes have had their status and bodies
+    # manually created and should not be refreshed
+    context 'with a letter generator service error' do
+      it 'should return a not found response' do
+        VCR.use_cassette('evss/letters/letters_letter_generator_service_error') do
+          get '/v0/letters', nil, auth_header
+          expect(response).to have_http_status(:service_unavailable)
+          expect(response).to match_response_schema('letters_errors')
+        end
+      end
+    end
+
+    context 'with one or more letter destination errors' do
+      it 'should return a not found response' do
+        VCR.use_cassette('evss/letters/letters_letter_destination_error') do
+          get '/v0/letters', nil, auth_header
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(response).to match_response_schema('letters_errors')
+        end
+      end
+    end
+
+    context 'with a not eligible error' do
+      it 'should return a not found response' do
+        VCR.use_cassette('evss/letters/letters_not_eligible_error') do
+          get '/v0/letters', nil, auth_header
+          expect(response).to have_http_status(:forbidden)
+          expect(response).to match_response_schema('letters_errors')
+        end
+      end
+    end
+
+    context 'with can not determine eligibility error' do
+      it 'should return a not found response' do
+        VCR.use_cassette('evss/letters/letters_determine_eligibility_error') do
+          get '/v0/letters', nil, auth_header
+          expect(response).to have_http_status(:forbidden)
+          expect(response).to match_response_schema('letters_errors')
+        end
+      end
+    end
+  end
+
+  context 'with an http timeout' do
+    before do
+      allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(Faraday::TimeoutError)
+    end
+
+    it 'should return a not found response' do
+      get '/v0/letters', nil, auth_header
+      expect(response).to have_http_status(:gateway_timeout)
+      expect(JSON.load(response.body)).to have_deep_attributes(
+        'errors' => [
+          {
+            'title' => 'Gateway timeout',
+            'detail' => 'Did not receive a timely response from an upstream server',
+            'code' => '504',
+            'status' => '504'
+          }
+        ]
+      )
     end
   end
 end
