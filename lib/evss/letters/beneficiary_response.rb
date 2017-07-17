799bb1d0a63269fb49c239e2e29d75519286cdb5
Alastair Dawson <alastair.j.dawson@gmail.com>
2017-07-17 15:01:08 -0700

    veteran and dependent benefit_information as children of benefit_information

--- lib/evss/letters/beneficiary_response.rb
+++ lib/evss/letters/beneficiary_response.rb
@@ -11,6 +11,14 @@ module EVSS
         attributes = response.body if response
         super(status, attributes)
       end
+
+      def benefit_information=(value)
+        if value.has_key? 'has_service_connected_disabilities'
+          super EVSS::Letters::BenefitInformationVeteran.new(value)
+        else
+          super EVSS::Letters::BenefitInformationDependent.new(value)
+        end
+      end
     end
   end
 end
