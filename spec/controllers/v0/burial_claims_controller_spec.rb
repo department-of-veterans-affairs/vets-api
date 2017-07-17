a0074413695d18601072266a7741188720a2e481
lihan <frankieteardrop@gmail.com>
2017-07-17 13:00:33 -0400

    Ensure the form isn't saved after submitted (#1198)
    * clear saved form method
    
    * model exists method
    
    * clear saved form spec
    
    * edu benefits clear saved form
    
    * clear saved form 1010ez
    
    * burial claims saved form clearing spec
    
    * lint

--- spec/controllers/v0/burial_claims_controller_spec.rb
+++ spec/controllers/v0/burial_claims_controller_spec.rb
@@ -2,4 +2,11 @@
 require 'rails_helper'
 
 RSpec.describe V0::BurialClaimsController, type: :controller do
+  describe '#create' do
+    it 'should delete the saved form' do
+      expect_any_instance_of(ApplicationController).to receive(:clear_saved_form).with('21P-530').once
+
+      post(:create, burial_claim: { form: build(:burial_claim).form })
+    end
+  end
 end
