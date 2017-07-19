23b8537405c25fde2e090ea28800f8d9d94713dc
Bill Ryan <bill.ryan@adhocteam.us>
2017-07-19 12:14:01 -0400

    handle evss errors (#1207)

--- lib/evss/gi_bill_status/service.rb
+++ lib/evss/gi_bill_status/service.rb
@@ -10,18 +10,23 @@ module EVSS
         raw_response = get ''
         EVSS::GiBillStatus::GiBillStatusResponse.new(raw_response.status, raw_response)
       rescue Faraday::ParsingError => e
-        extra_context = { url: BASE_URL }
+        response = OpenStruct.new(e.response.to_hash)
+        content_type = response.response_headers['content-type']
+        extra_context = { response: response }
         log_exception_to_sentry(e, extra_context)
-        EVSS::GiBillStatus::GiBillStatusResponse.new(403)
+        EVSS::GiBillStatus::GiBillStatusResponse.new(response.status, response, false, content_type)
       rescue Faraday::TimeoutError
         log_message_to_sentry(
           'Timeout while connecting to GiBillStatus service', :error, extra_context: { url: BASE_URL }
         )
-        EVSS::GiBillStatus::GiBillStatusResponse.new(403)
+        EVSS::GiBillStatus::GiBillStatusResponse.new(999, nil, true)
       rescue Faraday::ClientError => e
-        extra_context = { url: BASE_URL, body: e.response[:body] }
+        # convert <Faraday::ClientError>.response hash to object to conform with a normal response
+        response = OpenStruct.new(e&.response)
+
+        extra_context = { url: BASE_URL, response: response }
         log_exception_to_sentry(e, extra_context)
-        EVSS::GiBillStatus::GiBillStatusResponse.new(e.response[:status])
+        EVSS::GiBillStatus::GiBillStatusResponse.new(response.status, response)
       end
 
       def self.breakers_service
