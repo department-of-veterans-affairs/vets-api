58e7555e996ef974db856d58ef7856f244d49e4c
Lihan Li <frankieteardrop@gmail.com>
2017-07-19 17:46:30 -0400

    move validation of form id to individual class

--- app/models/saved_claim/education_benefits.rb
+++ app/models/saved_claim/education_benefits.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 class SavedClaim::EducationBenefits < SavedClaim
+  # 1995 1990E 5490 5495 1990N
   CONFIRMATION = 'EBC'
 
   has_one(:education_benefits_claim, foreign_key: 'saved_claim_id', inverse_of: :saved_claim)
-
-  validates(:form_id, inclusion: %w(1990 1995 1990E 5490 5495 1990N).map { |c| "22-#{c}" })
 end
--- app/models/saved_claim/education_benefits/va1990.rb
+++ app/models/saved_claim/education_benefits/va1990.rb
@@ -2,4 +2,6 @@
 class SavedClaim::EducationBenefits::VA1990 < SavedClaim::EducationBenefits
   PERSISTENT_CLASS = nil
   FORM = '22-1990'
+
+  validates(:form_id, inclusion: %w(22-1990))
 end
