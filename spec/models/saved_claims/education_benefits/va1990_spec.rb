1dee033187ba95214af44aac8fd7c2a32a0db776
Lihan Li <frankieteardrop@gmail.com>
2017-07-19 18:23:37 -0400

    validate inclusion helper

--- spec/models/saved_claims/education_benefits/va1990_spec.rb
+++ spec/models/saved_claims/education_benefits/va1990_spec.rb
@@ -6,17 +6,5 @@ RSpec.describe SavedClaim::EducationBenefits::VA1990 do
   let(:instance) { FactoryGirl.build(:va1990) }
   it_should_behave_like 'saved_claim'
 
-  describe 'validations' do
-    subject do
-      described_class.new
-    end
-
-    it 'should validate form_id' do
-      subject.form_id = "22-1990"
-      expect_attr_valid(subject, :form_id)
-
-      subject.form_id = 'foo'
-      expect_attr_invalid(subject, :form_id, 'is not included in the list')
-    end
-  end
+  validate_inclusion(:form_id, '22-1990')
 end
