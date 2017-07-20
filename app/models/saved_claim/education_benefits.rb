ced611cfa23b02165f2ad857ec54466488136e84
Lihan Li <frankieteardrop@gmail.com>
2017-07-19 20:30:10 -0400

    add todo

--- app/models/saved_claim/education_benefits.rb
+++ app/models/saved_claim/education_benefits.rb
@@ -2,5 +2,6 @@
 class SavedClaim::EducationBenefits < SavedClaim
   CONFIRMATION = 'EBC'
 
+  # TODO require this
   has_one(:education_benefits_claim, foreign_key: 'saved_claim_id', inverse_of: :saved_claim)
 end
