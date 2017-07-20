27bde0eb40dd42f09a691eafd1a3b606c6e45867
Mark Olson <theothermarkolson@gmail.com>
2017-07-20 11:38:42 -0400

    Add explanatory comments/lies waiting to happen

--- app/uploaders/claim_documentation/pension_burial/workflow.rb
+++ app/uploaders/claim_documentation/pension_burial/workflow.rb
@@ -5,6 +5,9 @@ require 'workflow/task/shared/datestamp_pdf_task'
 require 'workflow/task/shared/move_to_lts'
 require 'workflow/task/pension_burial/upload'
 
+# Convert uploaded files for Pension/Burial to a format, stamping them twice
+# before transfering them over to the SFTP server.
+
 class ClaimDocumentation::PensionBurial::Workflow < Workflow::File
   run Workflow::Task::Shared::ConvertToPdf
   run Workflow::Task::Shared::DatestampPdfTask, text: 'Vets.gov', x: 0, y: 0
