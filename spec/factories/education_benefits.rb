e7244451e5c8a4ab6e7fd643984162b1dcac1df6
Lihan Li <frankieteardrop@gmail.com>
2017-07-20 21:13:05 -0400

    fix edu benefits factory

--- spec/factories/education_benefits.rb
+++ spec/factories/education_benefits.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 FactoryGirl.define do
   factory :education_benefits, class: SavedClaim::EducationBenefits do
-    after(:build) do |saved_claim|
-      saved_claim.education_benefits_claim ||= build(:education_benefits_claim)
-    end
   end
 end
