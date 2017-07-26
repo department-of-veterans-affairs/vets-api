c971df95b95846cd390d6f5b217f64e812cc9621
Lihan Li <frankieteardrop@gmail.com>
2017-07-26 15:26:05 -0400

    lint

--- app/uploaders/claim_documentation/pension_burial/workflow.rb
+++ app/uploaders/claim_documentation/pension_burial/workflow.rb
@@ -11,7 +11,13 @@ require 'workflow/task/pension_burial/upload'
 class ClaimDocumentation::PensionBurial::Workflow < Workflow::File
   run Workflow::Task::Shared::ConvertToPdf
   run Workflow::Task::Shared::DatestampPdfTask, text: 'Vets.gov', x: 0, y: 0
-  run Workflow::Task::Shared::DatestampPdfTask, text: 'FDC Reviewed - Vets.gov Submission', x: 429, y: 770, text_only: true
+  run(
+    Workflow::Task::Shared::DatestampPdfTask,
+    text: 'FDC Reviewed - Vets.gov Submission',
+    x: 429,
+    y: 770,
+    text_only: true
+  )
   run Workflow::Task::Shared::MoveToLTS, all: true
   # run Workflow::Task::Common::DeleteOriginalUpload
   run Workflow::Task::PensionBurial::Upload
