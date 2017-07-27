6d788e59a3b3139209f4d1714e5b50b507c0149c
omgitsbillryan <bill.ryan@adhocteam.us>
2017-07-27 11:34:30 +0930

    code review comments

--- lib/common/exceptions/internal/unexpected_forbidden.rb
+++ lib/common/exceptions/internal/unexpected_forbidden.rb
@@ -2,8 +2,8 @@
 module Common
   module Exceptions
     # Forbidden is excluded from Sentry logging. This exception is a duplicate
-    # that IS NOT excluded from Sentry loggin.
-    class EVSSForbidden < Forbidden
+    # that IS NOT excluded from Sentry logging
+    class UnexpectedForbidden < Forbidden
       def i18n_key
         "common.exceptions.#{self.class.superclass.name.split('::').last.underscore}"
       end
