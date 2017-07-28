5d506e02942bb982258420cd82fcd2e2c1b5e372
Kam Karshenas <kam@saneshark.com>
2017-07-27 22:41:26 -0400

    rubocop fix

--- app/controllers/concerns/mhv_controller_concerns.rb
+++ app/controllers/concerns/mhv_controller_concerns.rb
@@ -12,14 +12,7 @@ module MHVControllerConcerns
   def authorize
     raise_access_denied if !current_user&.loa3? || current_user.mhv_account.ineligible?
     raise_requires_terms_acceptance if current_user.mhv_account.needs_terms_acceptance?
-    begin
-      current_user.mhv_account.create_and_upgrade! unless current_user.mhv_account.upgraded?
-      original_error = nil
-    rescue Common::Exceptions::BackendServiceException => error
-      original_error = error
-    ensure
-      raise_something_went_wrong(original_error) unless current_user.mhv_account.upgraded?
-    end
+    create_mhv_account! unless current_user.mhv_account.upgraded?
   end
 
   def raise_requires_terms_acceptance
@@ -34,5 +27,14 @@ module MHVControllerConcerns
   def authenticate_client
     MHVLoggingService.login(current_user)
     client.authenticate if client.session.expired?
+  end
+
+  def create_mhv_account!
+    current_user.mhv_account.create_and_upgrade!
+    original_error = nil
+  rescue Common::Exceptions::BackendServiceException => error
+    original_error = error
+  ensure
+    raise_something_went_wrong(original_error) unless current_user.mhv_account.upgraded?
   end
 end
