f972f837ef8fa5e819f598af2dd7b9888cf7dce9
Patrick Vinograd <patrick@adhocteam.us>
2017-07-31 13:48:35 -0700

    Semantically complete

--- app/swagger/schemas/va_facilities.rb
+++ app/swagger/schemas/va_facilities.rb
@@ -7,10 +7,12 @@ module Swagger
       swagger_schema :VAFacilities do
         key :required, [:data]
 
-        property :data, type: :array
-        items do
-          key :'$ref', :VAFacilityObject
-        end
+        property :data do
+          key :type, :array
+          items do
+            key :'$ref', :VAFacilityObject
+          end
+        end 
       end
 
       swagger_schema :VAFacility do
@@ -24,17 +26,17 @@ module Swagger
       swagger_schema :VAFacilityObject do
         key :required, [:id, :type, :attributes]
 
-        property :id, type: :string
-        property :type, type: :string
+        property :id, type: :string, example: 'vha_999'
+        property :type, type: :string, example: 'va_facilities'
 
         property :attributes, type: :object do
-          property :unique_id, type: :string
-          property :name, type: :string
-          property :facility_type, type: :string
-          property :classification, type: :string
-          property :website, type: :string
-          property :lat, type: :float
-          property :long, type: :float
+          property :unique_id, type: :string, example: '999'
+          property :name, type: :string, example: 'Example VAMC'
+          property :facility_type, type: :string, example: 'va_health_facility'
+          property :classification, type: :string, example: 'VA Medical Center'
+          property :website, type: :string, example: 'http://www.example.com'
+          property :lat, type: :number, format: :float, example: -122.5
+          property :long, type: :number, format: :float, example: 45.5
           property :address do
             key :'$ref', :FacilityAddresses
           end
@@ -44,14 +46,21 @@ module Swagger
           property :hours do
             key :'$ref', :FacilityHours
           end
+          property :services do
+            key :'$ref', :FacilityServices
+          end
           property :feedback do
             key :'$ref', :FacilityFeedback
+          end
+          property :access do
+            key :'$ref', :FacilityAccess
           end
         end
       end
 
       swagger_schema :FacilityAddresses do
         key :type, :object
+        key :description, 'Physical and mailing addresses for facilities'
 
         property :physical do
           key :'$ref', :FacilityAddress
@@ -64,39 +73,78 @@ module Swagger
       swagger_schema :FacilityAddress do
         key :type, :object
 
-        property :address_1, type: :String
-        property :address_2, type: :String
-        property :address_3, type: :String
-        property :city, type: :String
-        property :state, type: :String
-        property :zip, type: :String
+        property :address_1, type: :string, example: '123 Fake Street'
+        property :address_2, type: :string, example: 'Suite 001'
+        property :address_3, type: :string
+        property :city, type: :string, example: 'Anytown'
+        property :state, type: :string, example: 'NY'
+        property :zip, type: :string, example: '00001'
       end
 
       swagger_schema :FacilityPhones do
         key :type, :object
+        key :description, 'Telephone numbers for facilities'
 
-        property :main, type: :string
-        property :fax, type: :string
-        property :after_hours, type: :string
-        property :patient_advocate, type: :string
-        property :enrollment_coordinator, type: :string
-        property :pharmacy, type: :string
-        property :mental_health_clinic, type: :string
+        property :main, type: :string, example: '212-555-1212'
+        property :fax, type: :string, example: '212-555-1212'
+        property :after_hours, type: :string, example: '212-555-1212'
+        property :patient_advocate, type: :string, example: '212-555-1212'
+        property :enrollment_coordinator, type: :string, example: '212-555-1212'
+        property :pharmacy, type: :string, example: '212-555-1212'
+        property :mental_health_clinic, type: :string, example: '212-555-1212 x 123'
       end
 
       swagger_schema :FacilityHours do
         key :required, [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
         key :type, :object
+        key :description, 'Hours of operation for facilities'
        
-        property :monday, type: :string
-        property :tuesday, type: :string
-        property :wednesday, type: :string
-        property :thursday, type: :string
-        property :friday, type: :string
-        property :saturday, type: :string
-        property :sunday, type: :string
+        property :monday, type: :string, example: '9am - 5pm'
+        property :tuesday, type: :string, example: '9am - 5pm'
+        property :wednesday, type: :string, example: '9am - 5pm'
+        property :thursday, type: :string, example: '9am - 5pm'
+        property :friday, type: :string, example: '9am - 5pm'
+        property :saturday, type: :string, example: 'Closed'
+        property :sunday, type: :string, example: 'Closed'
+      end
+
+      swagger_schema :FacilityServices do
+        key :type, :object
+
+        property :last_updated, type: :string, format: :date, example: '2017-07-01'
+        property :health do
+          key :'$ref', :HealthFacilityServices
+        end
+      end
+
+      swagger_schema :HealthFacilityServices do
+        key :type, :array
+        key :description, 'Available services at health facilities'
+
+        items do
+          key :'$ref', :HealthServiceCategory
+        end
       end
     
+      swagger_schema :HealthServiceCategory do
+        key :type, :object
+
+        property :sl1 do
+          key :type, :array
+          key :example, ['PrimaryCare']
+          items do
+            key :type, :string
+          end
+        end
+        property :sl2 do
+          key :type, :array
+          key :example, []
+          items do
+            key :type, :string
+          end
+        end
+      end
+
       swagger_schema :FacilityFeedback do
         key :type, :object
 
@@ -107,67 +155,64 @@ module Swagger
 
       swagger_schema :HealthFacilityFeedback do
         key :type, :object
+        key :description, 'Patient satisfaction scores for health facilities'
 
-        property :primary_care_routine, type: :string, format: :float
-        property :primary_care_urgent, type: :string, format: :float
-        property :specialty_care_routine, type: :string, format: :float
-        property :specialty_care_urgent, type: :string, format: :float
-        property :effective_date, type: :string, format: :date
+        property :primary_care_routine, type: :string, format: :float, example: 95.2
+        property :primary_care_urgent, type: :string, format: :float, example: 89.1
+        property :specialty_care_routine, type: :string, format: :float, example: 78
+        property :specialty_care_urgent, type: :string, format: :float, example: 75.3
+        property :effective_date, type: :string, format: :date, example: '2017-07-01'
+      end
+
+      swagger_schema :FacilityAccess do
+        key :type, :object
+
+        property :health do
+          key :'$ref', :HealthFacilityAccess
+        end
+      end
+
+      swagger_schema :HealthFacilityAccess do
+        key :type, :object
+
+        property :primary_care do
+          key :'$ref', :HealthAccessMetric
+        end
+        property :mental_health do
+          key :'$ref', :HealthAccessMetric
+        end
+        property :womens_health do
+          key :'$ref', :HealthAccessMetric
+        end
+        property :audiology do
+          key :'$ref', :HealthAccessMetric
+        end
+        property :cardiology do
+          key :'$ref', :HealthAccessMetric
+        end
+        property :gastroenterology do
+          key :'$ref', :HealthAccessMetric
+        end
+        property :opthalmology do
+          key :'$ref', :HealthAccessMetric
+        end
+        property :optometry do
+          key :'$ref', :HealthAccessMetric
+        end
+        property :urology_clinic do
+          key :'$ref', :HealthAccessMetric
+        end
+        property :effective_date, type: :string, format: :date, example: '2017-07-01'
+      end
+
+      swagger_schema :HealthAccessMetric do
+        key :type, :object
+        key :description, 'Health facility wait times for new and established patients'
+
+        property :new, type: :number, format: :float, example: 10.5
+        property :established, type: :number, format: :float, example: 5.1
       end
     end
   end
 end
 
-# "unique_id":"648",
-# "name":"Portland VA Medical Center",
-# "facility_type":"va_health_facility",
-# "classification":"VA Medical Center (VAMC)",
-# "website":"http://www.portland.va.gov/",
-# "lat":45.49746145,
-# "long":-122.68287208,
-# "address":{
-#  "physical":{"address_1":"3710 Southwest US Veterans Hospital Road",
-#              "address_2":null,
-#              "address_3":null,
-#              "city":"Portland",
-#              "state":"OR",
-#              "zip":"97239-2964"},
-#  "mailing":{}},
-# "phone":{"main":"503-721-1498 x",
-#   "fax":"503-273-5319 x",
-#   "after_hours":"503-220-8262 x",
-#   "patient_advocate":"503-273-5308 x",
-#   "enrollment_coordinator":"503-273-5069 x",
-#   "pharmacy":"503-273-5183 x",
-#   "mental_health_clinic":"503-273-5187"},
-# "hours":{"monday":"24/7",
-#          "tuesday":"24/7",
-#          "wednesday":"24/7",
-#          "thursday":"24/7",
-#          "friday":"24/7",
-#          "saturday":"24/7",
-#          "sunday":"24/7"},
-# "services":{
-#   "last_updated":"2017-06-09",
-#   "health":[{"sl1":["DentalServices"],"sl2":[]},
-#             {"sl1":["MentalHealthCare"],"sl2":[]},
-#             {"sl1":["PrimaryCare"],"sl2":[]}]
-#  },
-#  "feedback":{
-#   "health":{"primary_care_urgent":"0.72",
-#             "primary_care_routine":"0.82",
-#             "specialty_care_routine":"0.79",
-#             "specialty_care_urgent":"0.69",
-#             "effective_date":"2017-03-24"}
-#  },
-#  "access":{
-#   "health":{"primary_care":{"new":49.0,"established":10.0},
-#             "mental_health":{"new":19.0,"established":1.0},
-#             "womens_health":{"new":null,"established":1.0},
-#             "audiology":{"new":27.0,"established":10.0},
-#             "cardiology":{"new":28.0,"established":13.0},
-#             "gastroenterology":{"new":22.0,"established":12.0},
-#             "opthalmology":{"new":20.0,"established":8.0},
-#             "optometry":{"new":45.0,"established":20.0},
-#             "urology_clinic":{"new":20.0,"established":5.0},
-#             "effective_date":"2017-07-17"}
