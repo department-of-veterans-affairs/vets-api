15e31731c20ab801303e9b0f33ce922ce6aae74b
Lihan Li <frankieteardrop@gmail.com>
2017-08-03 22:04:54 -0400

    worker calls set result

--- app/workers/hca/service_job.rb
+++ app/workers/hca/service_job.rb
@@ -5,6 +5,9 @@ module HCA
     def perform(user_uuid, form, health_care_application_id)
       result = HCA::Service.new(User.find(user_uuid)).submit_form(form)
       Rails.logger.info "SubmissionID=#{result[:formSubmissionId]}"
+
+      health_care_application = HealthCareApplication.find(health_care_application_id)
+      health_care_application.set_result!(result)
     end
   end
 end
