4afed9a9f10842943e13d85c995b217fca8ea4ad
Mark Olson <theothermarkolson@gmail.com>
2017-08-04 12:34:10 -0400

    Coerce User UUIDs to strings for InProgres forms

--- app/models/in_progress_form.rb
+++ app/models/in_progress_form.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 class InProgressForm < ActiveRecord::Base
+  class CleanUUID < ActiveRecord::Type::String
+    def type_cast_for_database(value)
+      value.to_s.delete('-')
+    end
+
+    alias type_cast type_cast_for_database
+  end
+
   EXPIRES_AFTER = 60.days
+  attribute :user_uuid, CleanUUID.new
   attr_encrypted :form_data, key: Settings.db_encryption_key
   validates(:form_data, presence: true)
+  validates(:user_uuid, presence: true)
+  validate(:id_me_user_uuid)
   before_save :serialize_form_data
 
   def self.form_for_user(form_id, user)
@@ -26,6 +37,16 @@ class InProgressForm < ActiveRecord::Base
   end
 
   private
+
+  # Some IDs we get from ID.me are 22char hex strings
+  # > we started off with just 22 random hex chars (from openssl random bytes) years
+  # > ago, and switched to UUID v4 (minus dashes) later on
+  # https://dsva.slack.com/archives/C1A7KLZ9B/p1501856503336861
+  def id_me_user_uuid
+    if user_uuid && !user_uuid.length.in?([22, 32])
+      errors[:user_uuid] << "(#{user_uuid}) is not a proper length"
+    end
+  end
 
   def serialize_form_data
     self.form_data = form_data.to_json unless form_data.is_a?(String)
