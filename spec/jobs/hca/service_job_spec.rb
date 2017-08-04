bca8dbff5cb7ab889de21ed57a39e14c6d21ff67
Lihan Li <frankieteardrop@gmail.com>
2017-08-03 22:21:23 -0400

    service worker spec

--- spec/jobs/hca/service_job_spec.rb
+++ spec/jobs/hca/service_job_spec.rb
@@ -11,9 +11,29 @@ RSpec.describe HCA::ServiceJob, type: :job do
       timestamp: '2017-08-03 22:02:18 -0400'
     }
   end
+  let(:hca_service) do
+    double
+  end
 
   describe '#perform' do
+    before do
+      # this line is needed to make stub in next line work because the found user is not == to another instance of itself
+      expect(User).to receive(:find).with(user.uuid).once.and_return(user)
+      expect(HCA::Service).to receive(:new).with(user).once.and_return(hca_service)
+      expect(hca_service).to receive(:submit_form).with(form).once.and_return(result)
+    end
+
+    subject do
+      described_class.new.perform(user.uuid, form, health_care_application.id)
+    end
+
     it 'should call the service and save the results' do
+      subject
+      health_care_application.reload
+
+      expect(health_care_application.success?).to eq(true)
+      expect(health_care_application.form_submission_id).to eq(result[:formSubmissionId])
+      expect(health_care_application.timestamp).to eq(result[:timestamp])
     end
   end
 end
