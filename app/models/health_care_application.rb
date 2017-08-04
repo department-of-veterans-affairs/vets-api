973effa5d6ef743fd783daa5e16a35485e767b3f
Lihan Li <frankieteardrop@gmail.com>
2017-08-03 21:54:22 -0400

    validation presence of success fields

--- app/models/health_care_application.rb
+++ app/models/health_care_application.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class HealthCareApplication < ActiveRecord::Base
   validates(:state, presence: true, inclusion: %w(success error failed pending))
+  validates(:form_submission_id, :timestamp, presence: true, if: :success?)
 
   def success?
     state == 'success'
--- spec/models/health_care_application_spec.rb
+++ spec/models/health_care_application_spec.rb
@@ -19,5 +19,17 @@ RSpec.describe HealthCareApplication, type: :model do
       health_care_application.state = 'foo'
       expect_attr_invalid(health_care_application, :state, 'is not included in the list')
     end
+
+    it 'should validate presence of form_submission_id and timestamp if success' do
+      health_care_application = described_class.new
+
+      %w(form_submission_id timestamp).each do |attr|
+        health_care_application.state = 'success'
+        expect_attr_invalid(health_care_application, attr, "can't be blank")
+
+        health_care_application.state = 'pending'
+        expect_attr_valid(health_care_application, attr)
+      end
+    end
   end
 end
