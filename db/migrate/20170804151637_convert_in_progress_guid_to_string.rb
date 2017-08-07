0f867971a67948667591c6d70c8e833c6709753f
mark olson <markolson@users.noreply.github.com>
2017-08-07 09:38:42 -0400

    Coerce User UUIDs to strings for InProgres forms (#1260)

---
+++ db/migrate/20170804151637_convert_in_progress_guid_to_string.rb
@@ -0,0 +1,12 @@
+class ConvertInProgressGuidToString < ActiveRecord::Migration
+  def up
+    change_column :in_progress_forms, :user_uuid, :string, :null => false
+
+    InProgressForm.connection.schema_cache.clear!
+    InProgressForm.reset_column_information
+
+    InProgressForm.all.each { |form|
+      form.update_attribute(:user_uuid, form.user_uuid.gsub!('-', ''))
+    }
+  end
+end
