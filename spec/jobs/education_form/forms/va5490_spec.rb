5a9b1acafb9204be22c5e7cbba175bb85884ef6d
lihan <frankieteardrop@gmail.com>
2017-08-15 16:48:05 -0400

    Revert "Move EducationBenefitsClaim to Claim framework (#1275)" (#1283)
    This reverts commit 44ababdaa65ba70903f7f37b3194366062376ae3.

--- spec/jobs/education_form/forms/va5490_spec.rb
+++ spec/jobs/education_form/forms/va5490_spec.rb
@@ -3,13 +3,9 @@ require 'rails_helper'
 
 RSpec.describe EducationForm::Forms::VA5490 do
   context 'method tests' do
-    let(:education_benefits_claim) { create(:va5490).education_benefits_claim }
+    let(:education_benefits_claim) { build(:education_benefits_claim_5490) }
 
-    subject do
-      education_benefits_claim.instance_variable_set(:@application, nil)
-      education_benefits_claim.saved_claim.instance_variable_set(:@application, nil)
-      described_class.new(education_benefits_claim)
-    end
+    subject { described_class.new(education_benefits_claim) }
 
     before do
       allow_any_instance_of(described_class).to receive(:format)
@@ -18,7 +14,7 @@ RSpec.describe EducationForm::Forms::VA5490 do
     describe 'previous benefits' do
       context 'without previous benefits' do
         before do
-          education_benefits_claim.saved_claim.form = {
+          education_benefits_claim.form = {
             privacyAgreementAccepted: true,
             previousBenefits: {
               disability: false,
@@ -40,7 +36,7 @@ RSpec.describe EducationForm::Forms::VA5490 do
 
       context 'with previous benefits' do
         before do
-          education_benefits_claim.saved_claim.form = {
+          education_benefits_claim.form = {
             privacyAgreementAccepted: true,
             previousBenefits: {
               disability: true,
