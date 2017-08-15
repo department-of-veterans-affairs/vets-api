f78a1d7853078e5441f3c5d4fa9050e618d5bb43
Lihan Li <frankieteardrop@gmail.com>
2017-08-15 16:53:41 -0400

    working on benchmark req spec

---
+++ lib/benchmark_request.rb
@@ -0,0 +1,42 @@
+class BenchmarkRequest
+  def initialize(request_name)
+    @request_name = request_name
+    @benchmark_key = "benchmark_#{request_name}"
+    @redis = Redis.current
+  end
+
+  def log_benchmark(average, count)
+    log_message_to_sentry(
+      "Average #{@request_name} request in seconds",
+      :info,
+      { average: average, count: count },
+      backend_service: @request_name
+    )
+  end
+
+  def benchmark
+    start = Time.current
+    return_val = yield
+    diff = Time.current - start
+    count_key = "#{@benchmark_key}.count"
+    count = @redis.get(count_key)&.to_i
+    average = @redis.get(@benchmark_key)
+
+    if count.nil? || average.nil?
+      count = 1
+      average = diff
+    else
+      average = BigDecimal.new(average)
+      total = average * count + diff
+      count += 1
+      average = total / count
+    end
+
+    @redis.set(BENCHMARK_KEY, average)
+    @redis.set(count_key, count)
+
+    log_benchmark(average, count)
+
+    return_val
+  end
+end
---
+++ spec/lib/benchmark_request_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe BenchmarkRequest do
+  let(:benchmark_request) { described_class.new('evss') }
+end
