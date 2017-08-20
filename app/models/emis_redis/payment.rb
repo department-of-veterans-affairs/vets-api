1ae9b48947e0fa29878c0f1dbf1b40c64b6ad274
Lihan Li <frankieteardrop@gmail.com>
2017-08-19 21:54:53 -0400

    receives_va_pension call

---
+++ app/models/emis_redis/payment.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+module EMISRedis
+  class Payment < Model
+    CLASS_NAME = 'PaymentService'
+
+    def receives_va_pension
+      items_from_response('get_retirement_pay').each do |retirement_pay|
+        return true if retirement_pay.monthly_gross_amount > 0
+      end
+
+      false
+    end
+  end
+end
+
