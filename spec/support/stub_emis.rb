41f8fe870af2bfd7970d606f4a8dfbf781a03cec
Lihan Li <frankieteardrop@gmail.com>
2017-08-19 22:02:15 -0400

    fix stub emis

--- spec/support/stub_emis.rb
+++ spec/support/stub_emis.rb
@@ -1,4 +1,6 @@
 def stub_emis
   allow_any_instance_of(EMISRedis::VeteranStatus).to receive(:veteran?).and_return(true)
-  allow(EMISRedis::MilitaryInformation).to receive(:for_user).and_return(double.as_null_object)
+  %w(MilitaryInformation Payment).each do |klass|
+    allow("EMISRedis::#{klass}".constantize).to receive(:for_user).and_return(double.as_null_object)
+  end
 end
