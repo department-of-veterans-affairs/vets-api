ea295056c387f1d21175253035d66d70f7443a9b
Alastair Dawson <alastair.j.dawson@gmail.com>
2017-08-21 12:38:36 -0700

    Address validation i4415 (#1297)

--- lib/evss/pciu_address/address.rb
+++ lib/evss/pciu_address/address.rb
@@ -4,8 +4,15 @@ require 'common/models/base'
 module EVSS
   module PCIUAddress
     class Address
+      include ActiveModel::Validations
       include ActiveModel::Serialization
       include Virtus.model(nullify_blank: true)
+
+      ADDRESS_TYPES = {
+        domestic: 'DOMESTIC',
+        international: 'INTERNATIONAL',
+        military: 'MILITARY'
+      }.freeze
 
       attribute :type, String
       attribute :address_effective_date, DateTime
@@ -14,6 +21,22 @@ module EVSS
       attribute :address_three, String
       attribute :city, String
       attribute :country_name, String
+
+      validates :address_one, presence: true
+      validates :type, inclusion: { in: ADDRESS_TYPES.values }
+
+      def self.build_address(attrs)
+        case attrs['type']
+        when ADDRESS_TYPES[:domestic]
+          EVSS::PCIUAddress::DomesticAddress.new(attrs)
+        when ADDRESS_TYPES[:international]
+          EVSS::PCIUAddress::InternationalAddress.new(attrs)
+        when ADDRESS_TYPES[:military]
+          EVSS::PCIUAddress::MilitaryAddress.new(attrs)
+        else
+          raise ArgumentError, "Unsupported address type: #{attrs['type']}"
+        end
+      end
     end
   end
 end
