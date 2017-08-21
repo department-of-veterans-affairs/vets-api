04b3f29b4e9e869059d874791667fcd76a21250c
Lihan Li <frankieteardrop@gmail.com>
2017-08-21 17:14:26 -0400

    dont need stub emis anymore

--- spec/support/stub_emis.rb
+++ spec/support/stub_emis.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 def stub_emis
   allow_any_instance_of(EMISRedis::VeteranStatus).to receive(:veteran?).and_return(true)
-  %w(MilitaryInformation Payment).each do |klass|
-    allow("EMISRedis::#{klass}".constantize).to receive(:for_user).and_return(double.as_null_object)
-  end
 end
