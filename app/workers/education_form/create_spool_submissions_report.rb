4126be00b291431622ab20f61ec059e6b2244708
lihan <frankieteardrop@gmail.com>
2017-08-22 16:35:50 -0400

    Spool submissions report (#1243)
    * create csv array spec
    
    * add confirmation #
    
    * create spool submission spec
    
    * spool submissions report mailer
    
    * spool sub report mailer spec
    
    * fix create spool sub report spec
    
    * lint
    
    * lint
    
    * add entry to scheduler
    
    * script for getting past submissions
    
    * add emails
    
    * add comment about spool report
    
    * test recipients

---
+++ app/workers/education_form/create_spool_submissions_report.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+module EducationForm
+  class CreateSpoolSubmissionsReport
+    require 'csv'
+    include Sidekiq::Worker
+
+    def format_name(full_name)
+      return if full_name.blank?
+
+      [full_name['first'], full_name['last']].compact.join(' ')
+    end
+
+    def create_csv_array
+      csv_array = []
+      csv_array << ['Claimant Name', 'Veteran Name', 'Confirmation #', 'Time Submitted', 'RPO']
+
+      EducationBenefitsClaim.where(
+        processed_at: @date.beginning_of_day..@date.end_of_day
+      ).find_each do |education_benefits_claim|
+        parsed_form = education_benefits_claim.parsed_form
+
+        csv_array << [
+          format_name(parsed_form['relativeFullName']),
+          format_name(parsed_form['veteranFullName']),
+          education_benefits_claim.confirmation_number,
+          education_benefits_claim.processed_at.to_s,
+          education_benefits_claim.regional_processing_office
+        ]
+      end
+
+      csv_array
+    end
+
+    def perform
+      @date = Time.zone.today - 1.day
+      folder = 'tmp/spool_reports'
+      FileUtils.mkdir_p(folder)
+      filename = "#{folder}/#{@date}.csv"
+
+      CSV.open(filename, 'wb') do |csv|
+        create_csv_array.each do |row|
+          csv << row
+        end
+      end
+
+      return unless FeatureFlipper.send_email?
+      SpoolSubmissionsReportMailer.build(filename).deliver_now
+    end
+  end
+end
