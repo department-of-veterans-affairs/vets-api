3733ac8ad5cc7cbd8e2ca3c16016535f5600bfd8
Lihan Li <frankieteardrop@gmail.com>
2017-08-22 22:32:10 -0400

    add form profile va 1990

---
+++ app/models/form_profile/va1990.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+class FormProfile::VA1990 < FormProfile
+  def prefill(user)
+    super
+  end
+
+  def metadata
+    {
+      version: 0,
+      prefill: true,
+      # TODO ask what this does
+      returnUrl: '/veteran-information'
+    }
+  end
+end
