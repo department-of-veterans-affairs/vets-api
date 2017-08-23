25260a730a33e146aed39c82db0e4712a084b8ec
Lihan Li <frankieteardrop@gmail.com>
2017-08-22 23:46:12 -0400

    add service branches to MilitaryServiceEpisode

--- lib/emis/models/military_service_episode.rb
+++ lib/emis/models/military_service_episode.rb
@@ -5,6 +5,16 @@ module EMIS
     class MilitaryServiceEpisode
       include Virtus.model
 
+      SERVICE_BRANCHES = {
+        'F' => 'air force',
+        'A' => 'army',
+        'C' => 'coast guard',
+        'M' => 'marine corps',
+        'N' => 'navy',
+        'O' => 'noaa',
+        'H' => 'usphs'
+      }.freeze
+
       attribute :begin_date, Date
       attribute :end_date, Date
       attribute :termination_reason, String
@@ -28,6 +38,10 @@ module EMIS
       attribute :mgsr_service_agreement_duration_year_quantity_code, String
       attribute :dod_beneficiary_type_code, String
       attribute :reserve_under_age60_code, String
+
+      def branch_of_service
+        SERVICE_BRANCHES[branch_of_service_code]
+      end
     end
   end
 end
