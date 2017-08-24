f8e8e140408d5a34eee5ea122ae2cf82c1d07dfb
Lihan Li <frankieteardrop@gmail.com>
2017-08-24 17:20:13 -0400

    fix CreateSpoolSubmissionsReport spec

--- spec/factories/education_benefits_claims.rb
+++ spec/factories/education_benefits_claims.rb
@@ -5,4 +5,12 @@ FactoryGirl.define do
       education_benefits_claim.saved_claim ||= build(:va1990)
     end
   end
+
+  %w(1990e 1990n).each do |form_type|
+    factory :"education_benefits_claim_#{form_type}", class: 'EducationBenefitsClaim' do
+      after(:build) do |education_benefits_claim|
+        education_benefits_claim.saved_claim ||= build(:"va#{form_type}")
+      end
+    end
+  end
 end
