caafb215c9e207b8a2555ccdd508206127fefd50
Kam Karshenas <kam@saneshark.com>
2017-09-13 23:11:59 -0400

    make sure we dont override global saml_settings (current prod does this)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -136,7 +136,8 @@ class ApplicationController < ActionController::API
   end
 
   def saml_settings(options = {})
-    base_settings = SAML::SettingsService.saml_settings
+    # Make sure we're not changing the settings globally
+    base_settings = SAML::SettingsService.saml_settings.dup
 
     options.each do |option, value|
       next if value.nil?
