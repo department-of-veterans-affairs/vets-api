21815a9bb8f8de2d9ff0054dbc2419d3d6c8a31d
Kam Karshenas <kam@saneshark.com>
2017-09-14 00:31:29 -0400

    use heuristic to determine authn_context since RelayState is used by review instances

--- lib/saml/user.rb
+++ lib/saml/user.rb
@@ -5,11 +5,10 @@ require 'saml/user_attributes/dslogon'
 
 module SAML
   class User
-    attr_reader :saml_response, :attributes, :authn_context, :decorated
+    attr_reader :saml_response, :attributes, :decorated
 
     def initialize(saml_response)
       @saml_response = saml_response
-      @authn_context = saml_response&.settings&.authn_context
       @attributes = saml_response.attributes
       @decorated = decorator_constant.new(self)
     end
@@ -22,10 +21,24 @@ module SAML
       Hash[serializable_attributes.map { |k| [k, @decorated.send(k)] }]
     end
 
+    def authn_context
+      return 'dslogon' if dslogon?
+      return 'mhv' if mhv?
+      nil
+    end
+
     private
 
     def serializable_attributes
       @decorated.send(:serializable_attributes) + %i(last_signed_in)
+    end
+
+    def dslogon?
+      attributes.to_h.keys.include?('dslogon_uuid')
+    end
+
+    def mhv?
+      attributes.to_h.keys.include?('mhv_uuid')
     end
 
     def decorator_constant
