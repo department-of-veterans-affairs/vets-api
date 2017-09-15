6d810174776b698cc27af63c43bc1ae2d75a1c36
Kam Karshenas <kam@saneshark.com>
2017-09-15 02:05:18 -0400

    fixing specs

--- spec/lib/saml/dslogon_user_spec.rb
+++ spec/lib/saml/dslogon_user_spec.rb
@@ -4,18 +4,33 @@ require 'saml/user'
 require 'ruby-saml'
 
 RSpec.describe SAML::User do
-  let(:params) { { SAMLResponse: File.read("#{::Rails.root}/spec/fixtures/files/saml_responses/dslogon.params") } }
-  let(:saml_settings) { build(:settings_no_context) }
-  let(:saml_response) { OneLogin::RubySaml::Response.new(params[:SAMLResponse], settings: saml_settings) }
-  let(:described_instance) { described_class.new(saml_response) }
-
+  let(:saml_response) { instance_double(OneLogin::RubySaml::Response, attributes: saml_attributes) }
+  let(:saml_attributes) do
+    OneLogin::RubySaml::Attributes.new(
+      'dslogon_status' => ['SPONSOR'],
+      'dslogon_assurance' => ['2'],
+      'dslogon_gender' => ['male'],
+      'dslogon_deceased' => ['false'],
+      'dslogon_birth_date' => ['1973-09-03'],
+      'dslogon_fname' => ['KENT'],
+      'dslogon_mname' => ['Mayo'],
+      'dslogon_lname' => ['WELLS'],
+      'dslogon_idtype' => ['ssn'],
+      'dslogon_idvalue' => ['796178410'],
+      'uuid' => ['d09ae45773f4409c943ce85f668a527d'],
+      'dslogon_uuid' => ['1016980877'],
+      'email' => ['vets.gov.user+kent.wells@gmail.com'],
+      'multifactor' => ['true'],
+      'level_of_assurance' => [3]
+    )
+  end
   context 'LOA highest is lower than LOA current' do
+    let(:described_instance) { described_class.new(saml_response) }
     let(:user) { User.new(described_instance) }
-    let(:cert) { File.read 'spec/support/certificates/ruby-saml.crt' }
-    let(:key) { File.read 'spec/support/certificates/ruby-saml.key' }
+    let(:frozen_time) { Time.current }
 
     around(:each) do |example|
-      with_settings(Settings.saml, cert: cert, key: key) do
+      Timecop.freeze(frozen_time) do
         example.run
       end
     end
@@ -30,7 +45,7 @@ RSpec.describe SAML::User do
 
     it 'has various important attributes' do
       expect(user).to have_attributes(
-        uuid: '2d95596317cd45a3afd513cd15a980fb',
+        uuid: 'd09ae45773f4409c943ce85f668a527d',
         first_name: 'KENT',
         middle_name: 'Mayo',
         last_name: 'WELLS',
@@ -39,7 +54,9 @@ RSpec.describe SAML::User do
         zip: nil,
         ssn: '796178410',
         loa: { current: 3, highest: 3 },
-        multifactor: 'false',
+        multifactor: 'true',
+        authn_context: 'dslogon',
+        last_signed_in: frozen_time,
         mhv_last_signed_in: nil
       )
     end
