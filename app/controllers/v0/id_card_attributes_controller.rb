8b980cf35f71929be89c475d11b12811bfad635f
Patrick Vinograd <patrick@adhocteam.us>
2017-09-18 14:34:19 -0700

    Error tweaks

--- app/controllers/v0/id_card_attributes_controller.rb
+++ app/controllers/v0/id_card_attributes_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'vic/url_helper'
+require 'vic/id_card_attribute_error'
 
 module V0
   class IdCardAttributesController < ApplicationController
@@ -8,28 +9,29 @@ module V0
     def show
       id_attributes = IdCardAttributes.for_user(current_user)
       vic_url = VIC::URLHelper.generate_url(id_attributes)
-      raise Common::Exceptions::Forbidden, detail: 'Could not verify military service attributes', code: 'VIC005'
+      raise VIC::IDCardAttributeError, status: 502, code: 'VIC011', detail: 'Could not verify military service attributes'
       render json: { 'redirect' => vic_url }
     rescue => e
-      # TODO: Tighten this up. Only likely application error is branch of service data from eMIS.
-      # Could also get a more general error in signing request.
+      # Catch all potential errors looking up military service history
+      # Monitor sentry to make sure this is not catching more general errors
       log_exception_to_sentry(e)
-      raise Common::Exceptions::Forbidden, detail: 'Could not verify military service attributes', code: 'VIC005'
+      raise VIC::IDCardAttributeError, status: 502, code: 'VIC011', detail: 'Could not verify military service attributes'
     end
 
     private
 
     def authorize
-      raise Common::Exceptions::Forbidden, code: 'VIC001' unless current_user.loa3?
-      # TODO: possible change to more specific exceptions with actionable codes
-      raise Common::Exceptions::Forbidden, detail: 'Unable to verify EDIPI', code: 'VIC002' unless
+      raise Common::Exceptions::Forbidden, detail: 'You do not have access to ID card attributes' unless
+        current_user.loa3?
+      raise VIC::IDCardAttributeError, status: 403, code: 'VIC002', detail: 'Unable to verify EDIPI' unless
         current_user.edipi.present?
       begin
-        raise Common::Exceptions::Forbidden, detail: 'Not eligible for a Veteran ID Card', code: 'VIC003' unless
+        raise VIC::IDCardAttributeError, status: 403, code: 'VIC003', detail: 'Not eligible for a Veteran ID Card' unless
           current_user.veteran?
       rescue => e
+        # current_user.veteran? above may raise an error if user was not found or backend service was unavailable
         log_exception_to_sentry(e)
-        raise Common::Exceptions::Forbidden, detail: 'Could not verify Veteran status', code: 'VIC004'
+        raise VIC::IDCardAttributeError, status: 403, code: 'VIC010', detail: 'Could not verify Veteran status'
       end
     end
   end
