ffce2654c46124ae36d4f680bd60e68ab487b3a0
lihan <frankieteardrop@gmail.com>
2017-09-20 13:58:24 -0400

    1990 emis prefill (#1310)
    * move raises to emis response method
    
    * put response in its own context
    
    * dont put responses in own context
    
    * post 911 combat method
    
    * military information class
    
    * postNov111998Combat prefill
    
    * dont need post 911 combact indicator
    
    * dont use post 911 combact indicator
    
    * emis model parent class
    
    * spelling
    
    * unauthorized only used in veteran status
    
    * namespace veteran status model
    
    * military information redis store stub
    
    * use one redis config key for all emis models
    
    * service episodes by date method and spec
    
    * fix service episodes by date method
    
    * last branch of service method
    
    * military information emis method on user
    
    * stub emis
    
    * fix user serializer spec
    
    * fix military info spec
    
    * record not found error in emis redis model
    
    * rm unused file
    
    * record not found in emis model class
    
    * add todo
    
    * last entry date method
    
    * last entry date prefill
    
    * last discharge date method
    
    * prefill last discharge date
    
    * stub everything for emis mis
    
    * discharge type method
    
    * discharge type prefill
    
    * post nov 1998 combat method
    
    * post 911 combat prefill
    
    * add list of sw asia countries
    
    * add gulf war range
    
    * sw asia combat method
    
    * prefill sw asia combat
    
    * deployed to method
    
    * vietnam service method
    
    * deployed to spec
    
    * compensable_va_service_connected
    
    * fix last service branch name
    
    * is_va_service_connected method
    
    * is_va_service_connected prefill
    
    * receives_va_pension call
    
    * prefill receives_va_pension
    
    * fix stub emis
    
    * rescue entire emis block
    
    * remove var init from rescue
    
    * spec for rescue block
    
    * lint
    
    * lint
    
    * lint
    
    * lint
    
    * lint
    
    * dont need stub emis anymore
    
    * add payment receives_va_pension spec
    
    * add 1990 to form profile for
    
    * add form profile va 1990
    
    * 22-1990 stub form mapping
    
    * add service branches to MilitaryServiceEpisode
    
    * add todo
    
    * add generic service branch names
    
    * add todo
    
    * typo
    
    * add hca service branch method
    
    * branch of service spec
    
    * tours of duty includes service branch
    
    * tours of duty method
    
    * use expect prefilled method
    
    * tours of duty prefill spec
    
    * working on currently active duty
    
    * currently active duty spec
    
    * currently active duty returns hash
    
    * currently active duty prefill
    
    * fix emis error test
    
    * lint
    
    * fix user req spec
    
    * can prefill emis method
    
    * test user that cant prefill emis
    
    * lint
    
    * fix returnUrl

--- lib/emis/models/military_service_episode.rb
+++ lib/emis/models/military_service_episode.rb
@@ -5,6 +5,27 @@ module EMIS
     class MilitaryServiceEpisode
       include Virtus.model
 
+      SERVICE_BRANCHES = {
+        'A' => 'Army',
+        'C' => 'Coast Guard',
+        'D' => 'DoD',
+        'F' => 'Air Force',
+        'H' => 'Public Health Service',
+        'M' => 'Marine Corps',
+        'N' => 'Navy',
+        'O' => 'NOAA'
+      }.freeze
+
+      HCA_SERVICE_BRANCHES = {
+        'F' => 'air force',
+        'A' => 'army',
+        'C' => 'coast guard',
+        'M' => 'marine corps',
+        'N' => 'navy',
+        'O' => 'noaa',
+        'H' => 'usphs'
+      }.freeze
+
       attribute :begin_date, Date
       attribute :end_date, Date
       attribute :termination_reason, String
@@ -28,6 +49,14 @@ module EMIS
       attribute :mgsr_service_agreement_duration_year_quantity_code, String
       attribute :dod_beneficiary_type_code, String
       attribute :reserve_under_age60_code, String
+
+      def hca_branch_of_service
+        HCA_SERVICE_BRANCHES[branch_of_service_code] || 'other'
+      end
+
+      def branch_of_service
+        SERVICE_BRANCHES[branch_of_service_code]
+      end
     end
   end
 end
