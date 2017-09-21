3ff36afc7539cb4b70fed919407b8950a827561d
Patrick Vinograd <patrick@adhocteam.us>
2017-09-20 17:31:10 -0700

    Revert changes to forbidden exception class

--- lib/common/exceptions/internal/forbidden.rb
+++ lib/common/exceptions/internal/forbidden.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 module Common
   module Exceptions
-    # Forbidden
+    # Forbidden - We may eventually want different variations on this with distinct MinorCodes
     class Forbidden < BaseError
       def initialize(options = {})
         @detail = options[:detail]
-        @code = options[:code] || i18n_field(:code, {})
       end
 
       def errors
-        Array(SerializableError.new(i18n_data.merge(detail: @detail, code: @code)))
+        Array(SerializableError.new(i18n_data.merge(detail: @detail)))
       end
     end
   end
