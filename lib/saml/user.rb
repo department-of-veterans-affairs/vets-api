17751c2bf813f2708ab5d6977df7c09d692d566a
Kam Karshenas <kam@saneshark.com>
2017-09-22 15:26:56 -0400

    Kudos identity rebased with master (#1391)
    * basic changes to support mhv identity with comments and questions
    
    should fix broken specs, problem was with how delegator invokes method on super but not instance variable
    
    additional changes
    
    minor commit
    
    fixing sporadically broken spec in mhv_account_spec.rb
    
    additional changes to support dslogon
    
    adding specs for controller changes
    
    linting issues
    
    comment commit to trigger build again
    
    does this fix broken spec?
    
    fixing around each override in spec_helper breaking other spec
    
    why is secure random suddenly not freezing anymore?
    
    changing the recording stuff back to original
    
    this change breaks review instances
    
    changes per code review and minor refactor of saml_settings
    
    make sure we dont override global saml_settings (current prod does this)
    
    use heuristic to determine authn_context since RelayState is used by review instances
    
    making loa2 = loa3 because f/e changes will be severe otherwise
    
    linter issues
    
    moving saml_settings logic back above pagination_params for easier diff review
    
    add multifactor to user object, serializer, and fix schemas to support
    
    simplify logic and update comments on loa_highest / loa_current
    
    add specs for dslogon user
    
    trying to find out why specs fail jenkins
    
    add authn_context to session and use connect in build_url to support new flows
    
    linting issues
    
    fix sessions controller for real this time
    
    moving authn_context to current_user since FE needs it!!
    
    BONEHEAD error
    
    hopefully this fixes remaining specs
    
    seeing if this fixes specs
    
    fixing specs
    
    accidentally checked in experiment, removing
    
    remove safe_constantize references
    
    removing authn_context from session
    
    adding additional contextual comments
    
    * small tweaks to how we hydrate multifactor for each policy
    
    * improving comments around authn_context
    
    * fixing issue with wrong authn_context set on multifactor
    
    * fixing the multifactor deserializer methods
    
    * fixing broken spec

--- lib/saml/user.rb
+++ lib/saml/user.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 require 'saml/user_attributes/id_me'
 require 'saml/user_attributes/mhv'
+require 'saml/user_attributes/dslogon'
 
 module SAML
   class User
-    attr_reader :saml_response, :attributes, :authn_context, :decorated
+    attr_reader :saml_response, :attributes, :decorated
 
     def initialize(saml_response)
       @saml_response = saml_response
-      @authn_context = saml_response&.settings&.authn_context
       @attributes = saml_response.attributes
       @decorated = decorator_constant.new(self)
     end
@@ -17,35 +17,37 @@ module SAML
       Time.current.utc
     end
 
-    def loa
-      { current: loa_current, highest: loa_highest(attributes) }
-    end
-
     def to_hash
       Hash[serializable_attributes.map { |k| [k, @decorated.send(k)] }]
+    end
+
+    def authn_context
+      return 'dslogon' if dslogon?
+      return 'mhv' if mhv?
+      nil
     end
 
     private
 
     def serializable_attributes
-      @decorated.send(:serializable_attributes) + %i(last_signed_in loa)
+      @decorated.send(:serializable_attributes) + %i(authn_context last_signed_in)
+    end
+
+    def dslogon?
+      attributes.to_h.keys.include?('dslogon_uuid')
+    end
+
+    def mhv?
+      attributes.to_h.keys.include?('mhv_uuid')
     end
 
     def decorator_constant
-      "SAML::UserAttributes::#{@authn_context.upcase}".safe_constantize ||
+      case authn_context
+      when 'mhv'; then SAML::UserAttributes::MHV
+      when 'dslogon'; then SAML::UserAttributes::DSLogon
+      else
         SAML::UserAttributes::IdMe
-    end
-
-    def loa_current
-      @raw_loa ||= REXML::XPath.first(@saml_response.decrypted_document, '//saml:AuthnContextClassRef')&.text
-      LOA::MAPPING[@raw_loa]
-    end
-
-    def loa_highest(attributes)
-      Rails.logger.warn 'LOA.highest is nil!' if (loa = attributes['level_of_assurance']&.to_i).nil?
-      loa_highest = loa || loa_current
-      Rails.logger.warn 'LOA.highest is less than LOA.current' if loa_highest < loa_current
-      [loa_current, loa_highest].max
+      end
     end
   end
 end
