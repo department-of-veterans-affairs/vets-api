d466d01b211be62b527e52593aac12c8114bac8b
Bill Ryan <bill.ryan@adhocteam.us>
2017-09-25 11:40:27 -0400

    use proper html url (#1394)

--- app/workers/github/create_issue_job.rb
+++ app/workers/github/create_issue_job.rb
@@ -18,7 +18,7 @@ module Github
     def perform(feedback)
       feedback = Feedback.new(feedback)
       create_response = Github::GithubService.create_issue(feedback)
-      FeedbackSubmissionMailer.build(feedback, create_response&.url).deliver_now
+      FeedbackSubmissionMailer.build(feedback, create_response&.html_url).deliver_now
     end
     # :nocov:
   end
