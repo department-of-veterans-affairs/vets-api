4aa493ec6391a777549b97a850220046ab6201b7
James Kassemi <jkassemi@gmail.com>
2017-09-25 13:06:51 -0600

    rubocop corrections

--- app/controllers/v0/sessions_controller.rb
+++ app/controllers/v0/sessions_controller.rb
@@ -87,9 +87,9 @@ module V0
         obscure_token = Session.obscure_token(@session.token)
         Rails.logger.info("Logged in user with id #{@session.uuid}, token #{obscure_token}")
         Benchmark::Timer.stop(BENCHMARK_LOGIN, @saml_response.in_response_to, tags: [
-          'status:successful', "loa:#{@current_user.loa[:current]}", "context:#{@current_user&.authn_context}",
-          "multifactor:#{@current_user.multifactor}"
-        ])
+                                'status:successful', "loa:#{@current_user.loa[:current]}", "context:#{@current_user&.authn_context}",
+                                "multifactor:#{@current_user.multifactor}"
+                              ])
       else
         handle_login_error
         redirect_to Settings.saml.relay + '?auth=fail'
