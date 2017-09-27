985f7343c5339053e8cbbee1e2002492a6a14f0e
Lihan Li <frankieteardrop@gmail.com>
2017-09-27 15:17:39 -0400

    fix error middleware spec

--- spec/lib/evss/error_middleware_spec.rb
+++ spec/lib/evss/error_middleware_spec.rb
@@ -4,8 +4,7 @@ require 'evss/auth_headers'
 
 describe EVSS::ErrorMiddleware do
   let(:current_user) { FactoryGirl.build(:loa3_user) }
-  let(:auth_headers) { EVSS::AuthHeaders.new(current_user).to_h }
-  let(:claims_service) { EVSS::ClaimsService.new(auth_headers) }
+  let(:claims_service) { EVSS::Claims::Service.new(current_user) }
 
   it 'should raise the proper error' do
     VCR.use_cassette('evss/claims/claim_with_errors') do
