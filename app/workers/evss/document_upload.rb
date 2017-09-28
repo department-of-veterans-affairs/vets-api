0b9fcd050d62d1cd041ec747318a0bc848c23b1d
Lihan Li <frankieteardrop@gmail.com>
2017-09-28 16:04:57 -0400

    doc upload no longer requires auth headers

--- app/workers/evss/document_upload.rb
+++ app/workers/evss/document_upload.rb
@@ -2,9 +2,10 @@
 class EVSS::DocumentUpload
   include Sidekiq::Worker
 
-  def perform(auth_headers, user_uuid, document_hash)
+  def perform(user_uuid, document_hash)
+    user = User.find(user_uuid)
     document = EVSSClaimDocument.new document_hash
-    client = EVSS::Documents::Service.new(auth_headers)
+    client = EVSS::Documents::Service.new(user)
     uploader = EVSSClaimDocumentUploader.new(user_uuid, document.tracked_item_id)
     uploader.retrieve_from_store!(document.file_name)
     file_body = uploader.read_for_upload
