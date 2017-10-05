c5dce154d7a9d53026ad3f94cdc3480d14f5350a
Bill Ryan <bill.ryan@adhocteam.us>
2017-10-05 18:41:04 -0400

    allow for certificate rollover

--- config/initializers/saml.rb
+++ config/initializers/saml.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 Settings.saml.certificate = File.read(File.expand_path(Settings.saml.cert_path))
 Settings.saml.key = File.read(File.expand_path(Settings.saml.key_path))
+Settings.saml.certificate_new = File.read(File.expand_path(Settings.saml.cert_new_path))
