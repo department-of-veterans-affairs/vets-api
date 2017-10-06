9138c1727a2f6e0d37f12bcb57a019e61ab8fa37
Bill Ryan <bill.ryan@adhocteam.us>
2017-10-06 11:45:25 -0400

    allow for nil new_cert & add spec

--- config/initializers/saml.rb
+++ config/initializers/saml.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
+def new_saml_cert_exists?
+  !Settings.saml.cert_new_path.nil? && File.file?(File.expand_path(Settings.saml.cert_new_path))
+end
+
 Settings.saml.certificate = File.read(File.expand_path(Settings.saml.cert_path))
 Settings.saml.key = File.read(File.expand_path(Settings.saml.key_path))
-Settings.saml.certificate_new = File.read(File.expand_path(Settings.saml.cert_new_path))
+Settings.saml.certificate_new = new_saml_cert_exists? ? File.read(File.expand_path(Settings.saml.cert_new_path)) : nil
