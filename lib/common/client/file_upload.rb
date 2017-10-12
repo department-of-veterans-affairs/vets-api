839843ec456f7ccd457a968cace8f59b6ce4426d
Lihan Li <frankieteardrop@gmail.com>
2017-10-12 18:44:39 -0400

    use type method on mimemagic

--- lib/common/client/file_upload.rb
+++ lib/common/client/file_upload.rb
@@ -1,10 +1,11 @@
+# TODO: add spec
 module Common
   module Client
     module FileUpload
       def get_upload_io_object(file_path)
         Faraday::UploadIO.new(
           file_path,
-          MimeMagic.by_path(file_path)
+          MimeMagic.by_path(file_path).type
         )
       end
     end
