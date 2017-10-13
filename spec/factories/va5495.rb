5f947a5491396f540673f02472d608cfc2cd9e3d
lihan <frankieteardrop@gmail.com>
2017-10-12 21:59:47 -0400

    Edu benefits claims (#1284)
    * working on edu claims migration
    
    * edu benefits claim reference working
    
    * remove old edu benefits columlns
    
    * dont allow nulls for saved_claim id
    
    * run migration
    
    * validate form id inclusion
    
    * add form and confirmation constants
    
    * va 1990 factory
    
    * remove todo
    
    * move validation of form id to individual class
    
    * add va 1995
    
    * add form and validation method
    
    * validate inclusion helper
    
    * va 1995 spec
    
    * 1990e class
    
    * 5490
    
    * 5495 class
    
    * 1990n
    
    * no more submitted at
    
    * add todo
    
    * remove form validations
    
    * change confirmation code
    
    * bring back is form type methods
    
    * form type spec
    
    * fix is form type tests
    
    * fix open struct form
    
    * confirmation number method
    
    * fix create edu benefits submission
    
    * fix 1990 create edu submission spec
    
    * fix va1995 factory
    
    * add parent to all edu benefits factories
    
    * fix edu submission specs
    
    * fix copy from previous benefits spec
    
    * fix reprocess at spec
    
    * group_submissions_by_region spec
    
    * fix perform spec
    
    * fix write files spec
    
    * fix track form spec
    
    * fix 5495 spec
    
    * dont need delegate form type
    
    * fix 1995 spec
    
    * fix va 1990 spec
    
    * fix base spec
    
    * fix edu benefits ctrl
    
    * fix factory name
    
    * fix edu benefits serializer
    
    * fix edu request spec
    
    * fix 5490 spec
    
    * fix swagger spec
    
    * fix create year to date report spec
    
    * fix edu benefits factory
    
    * fix 5490 spec
    
    * fix serializer spec
    
    * fix edu facility spec
    
    * fix benefit type spec
    
    * lint
    
    * lint
    
    * delete form schemas constant
    
    * del unneeded line
    
    * require edu benefits claim on ed saved claim
    
    * lint
    
    * use form constant
    
    * fix migration
    
    * revert old confirmation number
    
    * fix write files spec
    
    * fix spool tests
    
    * lint
    
    * rerun tests
    
    * rename column
    
    * raw sql
    
    * fix sql
    
    * run on all claims
    
    * add form type explanation
    
    * split up migrations
    
    * raise on invalid form types
    
    * use postgres uuid gen function
    
    * fix CreateSpoolSubmissionsReport spec
    
    * dont need malformed records spec
    
    * fix format application
    
    * bring back invalid record spec
    
    * fix fiscal ytd spec
    
    * edu benefits doesnt use confirmation constant
    
    * fix workflow autoload
    
    * move out confirmation num specs

---
+++ spec/factories/va5495.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+FactoryGirl.define do
+  factory :va5495, class: SavedClaim::EducationBenefits::VA5495, parent: :education_benefits do
+    form({
+      relativeFullName: {
+        first: 'Mark',
+        last: 'Olson'
+      },
+      benefit: 'chapter35',
+      privacyAgreementAccepted: true
+    }.to_json)
+  end
+end
