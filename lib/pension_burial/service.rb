5be251a3a4295801219f36ae9097772f6f5b091a
Lihan Li <frankieteardrop@gmail.com>
2017-10-13 20:25:36 -0400

    dont need fileupload module

--- lib/pension_burial/service.rb
+++ lib/pension_burial/service.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 module PensionBurial
   class Service < Common::Client::Base
-    include Common::Client::FileUpload
     configuration PensionBurial::Configuration
 
-    def upload
+    def upload(metadata, file)
       request(
         :post,
         '',
         token:  Settings.pension_burial.upload.token,
-        metadata: {
-          filename: 'foo.pdf'
-        }.to_json,
-        document: get_upload_io_object('spec/fixtures/pdf_fill/extras.pdf')
+        metadata: metadata.to_json,
+        document: Faraday::UploadIO.new(
+          file.read,
+          file.mime_type
+        )
       )
     end
   end
--- lib/workflow/task/pension_burial/upload.rb
+++ lib/workflow/task/pension_burial/upload.rb
@@ -2,12 +2,16 @@
 module Workflow::Task::PensionBurial
   class Upload < Workflow::Task::ShrineFile::Base
     def run(_options = {})
+      binding.pry; fail
       path = File.join(Date.current.to_s, data[:form_id], data[:code])
       truncated_guid = data[:guid].split('-').first
       writer.write(@file.read, File.join(path, [truncated_guid, @file.original_filename].join('-')))
       PersistentAttachment.find(data[:id]).update(completed_at: Time.current)
     ensure
       writer.close
+    end
+
+    def service
     end
 
     def writer
