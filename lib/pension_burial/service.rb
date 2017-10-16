a98d1aa29b14cebf35dd6d45985772438545c50c
Lihan Li <frankieteardrop@gmail.com>
2017-10-16 12:59:55 -0400

    pass in file body and mime type to service

--- lib/pension_burial/service.rb
+++ lib/pension_burial/service.rb
@@ -3,15 +3,15 @@ module PensionBurial
   class Service < Common::Client::Base
     configuration PensionBurial::Configuration
 
-    def upload(metadata, file)
+    def upload(metadata, file_body, mime_type)
       request(
         :post,
         '',
         token:  Settings.pension_burial.upload.token,
         metadata: metadata.to_json,
         document: Faraday::UploadIO.new(
-          file.read,
-          file.mime_type
+          file_body,
+          mime_type
         )
       )
     end
