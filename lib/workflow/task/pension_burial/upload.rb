41015f19695c99dbffc3435bcbe9aeac96ef50e3
Lihan Li <frankieteardrop@gmail.com>
2017-10-16 13:07:57 -0400

    use service in upload.rb

--- lib/workflow/task/pension_burial/upload.rb
+++ lib/workflow/task/pension_burial/upload.rb
@@ -2,22 +2,18 @@
 module Workflow::Task::PensionBurial
   class Upload < Workflow::Task::ShrineFile::Base
     def run(_options = {})
-      binding.pry; fail
-      path = File.join(Date.current.to_s, data[:form_id], data[:code])
-      truncated_guid = data[:guid].split('-').first
-      writer.write(@file.read, File.join(path, [truncated_guid, @file.original_filename].join('-')))
+      service.upload(
+        data.slice(:form_id, :code, :guid).merge(
+          original_filename: @file.original_filename
+        ),
+        @file.read,
+        @file.mime_type
+      )
       PersistentAttachment.find(data[:id]).update(completed_at: Time.current)
-    ensure
-      writer.close
     end
 
     def service
-    end
-
-    def writer
-      @writer ||= SFTPWriter::Factory
-                  .get_writer(Settings.pension_burial.sftp)
-                  .new(Settings.pension_burial.sftp, logger: logger)
+      @service ||= PensionBurial::Service.new
     end
   end
 end
