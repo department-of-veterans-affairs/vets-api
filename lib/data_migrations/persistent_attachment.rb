9a26a0fc003a17b51a56d011a66694f6cfc56c30
Lihan Li <frankieteardrop@gmail.com>
2017-10-17 15:40:17 -0400

    spec for data migration

--- lib/data_migrations/persistent_attachment.rb
+++ lib/data_migrations/persistent_attachment.rb
@@ -1,9 +1,10 @@
+# TODO: delete after running
 module DataMigrations
   module PersistentAttachment
     module_function
 
     def run
-      PersistentAttachment.where(type: 'PersistentAttachment::PensionBurial').update_all(type: 'PersistentAttachments::PensionBurial')
+      ::PersistentAttachment.where(type: 'PersistentAttachment::PensionBurial').update_all(type: 'PersistentAttachments::PensionBurial')
     end
   end
 end
