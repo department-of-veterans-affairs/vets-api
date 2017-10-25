96c50d41007df3c51a4605aa10f8a0acdd6ef19a
Lihan Li <frankieteardrop@gmail.com>
2017-10-25 14:18:13 -0400

    add shared example for skip sentry 404

---
+++ spec/support/controller_spec_helper.rb
@@ -0,0 +1,19 @@
+shared_examples_for 'skip_sentry_404' do
+  before do
+    allow_any_instance_of(described_class).to receive(:authenticate) do
+      raise Common::Exceptions::RecordNotFound, 'some_id'
+    end
+
+    controller_klass = described_class
+    Rails.application.routes.draw do
+      get '/authenticate' => "#{ controller_klass.to_s.underscore.gsub('_controller', '')}#authenticate"
+    end
+  end
+
+  it 'should not log 404 to sentry' do
+    allow_any_instance_of(ApplicationController).to receive(:log_exception_to_sentry) { raise }
+
+    get('/authenticate')
+    expect(response.code).to eq('404')
+  end
+end
