a12fce0e94d0cf55afa3f3a7ab691bfa715638fb
lihan <frankieteardrop@gmail.com>
2017-10-26 14:32:15 -0400

    use Common::Client for EVSS (#1421)
    * add claims service configuration
    
    * use claims folder
    
    * rm old claims service
    
    * rm claims service from breakers
    
    * rm other claims service
    
    * restore old base service for now
    
    * fix evss service configuration
    
    * claims service init with current user
    
    * fix req decision
    
    * rename to claims req spec
    
    * add todo
    
    * fix find claim by id
    
    * fix client stub
    
    * remove duplicate requires
    
    * fix breakers spec
    
    * finish todo
    
    * rm claims service spec
    
    * fix error middleware spec
    
    * dont use auth headers in evss req decision
    
    * fix req decision spec
    
    * use perform with user headers method
    
    * fix find_rating_info
    
    * fix create user account
    
    * rm common service
    
    * change name to evsscommon
    
    * fix document service
    
    * rm old docu service
    
    * rm documents service requires
    
    * use documents config for breakers service
    
    * use class default timeout
    
    * pass in user uuid to CreateUserAccountJob
    
    * fix create user acct job
    
    * dont need to pass in auth headers anymore
    
    * dont pass in auth headers to doc upload
    
    * fix evsscommon name
    
    * dont require auth headers
    
    * rm old doc upload job
    
    * doc upload no longer requires auth headers
    
    * rm old spec
    
    * fix doc upload spec
    
    * fix documents spec
    
    * dont use base service in gi bill status
    
    * gi bill status config
    
    * use config in service
    
    * dont pass in headers to service
    
    * save current user in service
    
    * rm breakers service
    
    * fix gi bill status breakers
    
    * fix common client error spec
    
    * fix timeout error spec
    
    * common client already logs to sentry
    
    * rm base service
    
    * rm refs to base service
    
    * lint
    
    * bring back claims timeout
    
    * complete todo
    
    * put system name in evss service
    
    * dont need to pass in participant id

---
+++ lib/evss/claims/service.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+module EVSS
+  module Claims
+    class Service < EVSS::Service
+      DEFAULT_TIMEOUT = 120
+
+      configuration EVSS::Claims::Configuration
+
+      def initialize(current_user)
+        @current_user = current_user
+      end
+
+      def all_claims
+        perform_with_user_headers(:get, 'vbaClaimStatusService/getClaims', nil)
+      end
+
+      def find_claim_by_id(claim_id)
+        perform_with_user_headers(
+          :post,
+          'vbaClaimStatusService/getClaimDetailById',
+          { id: claim_id }.to_json
+        )
+      end
+
+      def request_decision(claim_id)
+        perform_with_user_headers(
+          :post,
+          'vbaClaimStatusService/set5103Waiver',
+          {
+            claimId: claim_id,
+            systemName: SYSTEM_NAME
+          }.to_json
+        )
+      end
+    end
+  end
+end
