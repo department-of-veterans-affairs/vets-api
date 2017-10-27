537c567be8a095ce8980f6a470849cfd7b14751a
Jeff Balboni <jbalboni@gmail.com>
2017-10-27 10:49:23 -0400

    Revert "use Common::Client for EVSS (#1421)" (#1488)
    This reverts commit aef996fee03d78dc9566dc0a03ce3a6354cc9429.

--- app/controllers/v0/sessions_controller.rb
+++ app/controllers/v0/sessions_controller.rb
@@ -155,7 +155,8 @@ module V0
 
     def async_create_evss_account(user)
       return unless user.can_access_evss?
-      EVSS::CreateUserAccountJob.perform_async(user.uuid)
+      auth_headers = EVSS::AuthHeaders.new(user).to_h
+      EVSS::CreateUserAccountJob.perform_async(auth_headers)
     end
 
     def handle_completed_slo
---
+++ lib/evss/claims_service.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require 'evss/base_service'
+
+module EVSS
+  class ClaimsService < BaseService
+    API_VERSION = Settings.evss.versions.claims
+    BASE_URL = "#{Settings.evss.url}/wss-claims-services-web-#{API_VERSION}/rest"
+    DEFAULT_TIMEOUT = 120 # in seconds
+
+    def initialize(*args)
+      super
+    end
+
+    def all_claims
+      get 'vbaClaimStatusService/getClaims'
+    end
+
+    def find_claim_by_id(claim_id)
+      post 'vbaClaimStatusService/getClaimDetailById', { id: claim_id }.to_json
+    end
+
+    def request_decision(claim_id)
+      post 'vbaClaimStatusService/set5103Waiver', {
+        claimId: claim_id,
+        systemName: SYSTEM_NAME
+      }.to_json
+    end
+
+    def self.breakers_service
+      BaseService.create_breakers_service(name: 'EVSS/Claims', url: BASE_URL)
+    end
+  end
+end
