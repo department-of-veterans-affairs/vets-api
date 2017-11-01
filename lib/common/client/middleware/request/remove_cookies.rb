dd6e26fc4df3754ef9f8d37dfd62daa6e98d3ec0
Lihan Li <frankieteardrop@gmail.com>
2017-11-01 12:58:35 -0400

    add comment about placement

--- lib/common/client/middleware/request/remove_cookies.rb
+++ lib/common/client/middleware/request/remove_cookies.rb
@@ -2,6 +2,7 @@ module Common
   module Client
     module Middleware
       module Request
+        # must be added right before http_client adapter
         class RemoveCookies < Faraday::Middleware
           def call(env)
             @app.client.cookie_manager.cookies = []
