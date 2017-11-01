55521643218aa8a4ca2c47da048ce230b9842a40
omgitsbillryan <bill.ryan@adhocteam.us>
2017-11-01 12:52:59 -0400

    all configuration of recording setting

--- config/initializers/betamocks.rb
+++ config/initializers/betamocks.rb
@@ -4,5 +4,5 @@ Betamocks.configure do |config|
   config.enabled = Settings.betamocks.enabled
   config.cache_dir = Settings.betamocks.cache_dir
   config.services_config = Settings.betamocks.services_config
-  config.recording = false
+  config.recording = Settings.betamocks.recording
 end
