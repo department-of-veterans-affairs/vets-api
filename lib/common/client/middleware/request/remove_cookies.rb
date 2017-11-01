72986ea0d928d2a6bfaf556549bf48ba2b648314
Lihan Li <frankieteardrop@gmail.com>
2017-11-01 00:47:00 -0400

    test remove cookies spec

--- lib/common/client/middleware/request/remove_cookies.rb
+++ lib/common/client/middleware/request/remove_cookies.rb
@@ -4,8 +4,7 @@ module Common
       module Request
         class RemoveCookies < Faraday::Middleware
           def call(env)
-            p @app.client.cookie_manager.cookies
-            # @app.client.cookie_manager.cookies = []
+            @app.client.cookie_manager.cookies = []
             @app.call(env)
           end
         end
--- spec/lib/common/client/middleware/request/remove_cookies_spec.rb
+++ spec/lib/common/client/middleware/request/remove_cookies_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Common::Client::Middleware::Request::RemoveCookies do
   class TestConfiguration < Common::Client::Configuration::REST
     def base_path
-      "http://127.0.0.1:3000/foos"
+      "http://127.0.0.1:9123/"
     end
 
     def connection
@@ -20,59 +20,35 @@ describe Common::Client::Middleware::Request::RemoveCookies do
 
   describe '#request', run_at: '2017-01-04 03:00:00 EDT' do
     it 'should strip cookies' do
-      # stub_request(:get, 'www.example.com/req1').to_return(headers: {
-      #   'Set-Cookie' => 'foo=bar',
-      #   'Connection' => 'close',
-      #   'Cache-Control' => 'no-cache'
-      # })
-
-      # stub_request(:get, "www.example.com/req2").with(
-        # headers: {
-        #   # 'Cookie' => 'foo=bar',
-        #   'User-Agent' => 'Faraday v0.9.2',
-        #   'Accept' => '*/*',
-        #   'Accept-Encoding' => 'gzip,deflate',
-        #   'Date' => 'Wed, 04 Jan 2017 07:00:00 GMT'
-        # }
-      # ).to_return(body: 'bad')
-
-      # # stub_request(:get, "www.example.com/req2").with(
-      # #   headers: {
-      # #     'User-Agent' => 'Faraday v0.9.2',
-      # #     'Accept' => '*/*',
-      # #     'Accept-Encoding' => 'gzip,deflate',
-      # #     'Date' => 'Wed, 04 Jan 2017 07:00:00 GMT'
-      # #   }
-      # # ).to_return(body: 'good')
-
-      # p TestService.new.send(:request, :get, '/req1', nil).body
-      # p TestService.new.send(:request, :get, '/req2', nil).body
-
-      # # match_requests_on: %i(method uri host path body headers)
-      VCR.use_cassette('common/remove_cookies', record: :once, match_requests_on: %i(method uri host path body headers)) do
-        TestService.new.send(:request, :get, '', nil)
-        TestService.new.send(:request, :get, '', nil)
-        # expect(WebMock).not_to have_requested(:get, 'http://127.0.0.1:3000/foos').with(headers: {
-        #     # 'Cookie' => 'foo=bar',
-        #     'User-Agent' => 'Faraday v0.9.2',
-        #     'Accept' => '*/*',
-        #     'Accept-Encoding' => 'gzip,deflate',
-        #     'Date' => 'Wed, 04 Jan 2017 07:00:00 GMT'
-        #   }
-        # )
+      VCR.config do |c|
+        c.allow_http_connections_when_no_cassette = true
       end
 
-      # stub_request(:get, "http://127.0.0.1:3000/foos").with(
-      #   headers: {
-      #     # 'Cookie' => 'foo=bar',
-      #     'User-Agent' => 'Faraday v0.9.2',
-      #     'Accept' => '*/*',
-      #     'Accept-Encoding' => 'gzip,deflate',
-      #     'Date' => 'Wed, 04 Jan 2017 07:00:00 GMT'
-      #   }
-      # ).to_return(body: 'bad')
+      server_thread = Thread.new do
+        server = WEBrick::HTTPServer.new(
+          Port: 9123
+        )
 
-      # p TestService.new.send(:request, :get, '', nil).body
+        server.mount_proc '/' do |req, res|
+          res.header['Set-Cookie'] = 'foo=bar'
+          res.body = req.header.try(:[], 'cookie').to_json
+        end
+
+        server.start
+      end
+
+      Timeout::timeout(30) do
+        while true
+          begin
+            if TestService.new.send(:request, :get, '', nil).status == 200
+              break
+            end
+          rescue Common::Client::Errors::ClientError
+          end
+        end
+      end
+
+      expect(TestService.new.send(:request, :get, '', nil).body).to eq('[]')
     end
   end
 end
