8cbfc86b9a0c328327c5de7853b0acd126076daf
Lihan Li <frankieteardrop@gmail.com>
2017-11-04 14:26:54 -0400

    can upload to api method

--- app/models/persistent_attachments/pension_burial.rb
+++ app/models/persistent_attachments/pension_burial.rb
@@ -2,4 +2,12 @@
 class PersistentAttachments::PensionBurial < PersistentAttachment
   UPLOADER_CLASS = ClaimDocumentation::PensionBurial::File
   include ::ClaimDocumentation::Uploader::Attachment.new(:file)
+
+  UPLOAD_TO_API_EMAILS = %w(
+    lihan@adhocteam.us
+  )
+
+  def can_upload_to_api?
+    UPLOAD_TO_API_EMAILS.include?(saved_claim.email)
+  end
 end
