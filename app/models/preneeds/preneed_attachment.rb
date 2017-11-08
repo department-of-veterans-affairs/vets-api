b3cfac44154d9745e6b371a67cbbae289c7ccc7b
Lihan Li <frankieteardrop@gmail.com>
2017-11-08 17:46:15 -0500

    make validate file size a module

--- app/models/preneeds/preneed_attachment.rb
+++ app/models/preneeds/preneed_attachment.rb
@@ -2,5 +2,6 @@
 module Preneeds
   class PreneedAttachment < ActiveRecord::Base
     attr_encrypted(:file_data, key: Settings.db_encryption_key)
+    mount_uploader(:file_data, PreneedAttachmentUploader)
   end
 end
