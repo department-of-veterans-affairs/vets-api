b5d212c520f00029983d5a2a5200bd33aaa115ff
Lihan Li <frankieteardrop@gmail.com>
2017-11-09 15:16:52 -0500

    preneed attachments controller working

--- app/models/preneeds/preneed_attachment.rb
+++ app/models/preneeds/preneed_attachment.rb
@@ -2,6 +2,16 @@
 module Preneeds
   class PreneedAttachment < ActiveRecord::Base
     attr_encrypted(:file_data, key: Settings.db_encryption_key)
-    mount_uploader(:file_data, PreneedAttachmentUploader)
+
+    after_initialize do
+      # TODO make this a module
+      self.guid ||= SecureRandom.uuid
+    end
+
+    def set_file_data(file)
+      preneed_attachment_uploader = PreneedAttachmentUploader.new(guid)
+      preneed_attachment_uploader.store!(file)
+      self.file_data = { filename: preneed_attachment_uploader.filename }.to_json
+    end
   end
 end
--- app/uploaders/preneed_attachment_uploader.rb
+++ app/uploaders/preneed_attachment_uploader.rb
@@ -4,4 +4,14 @@ class PreneedAttachmentUploader < CarrierWave::Uploader::Base
   include ValidateFileSize
 
   MAX_FILE_SIZE = 25.megabytes
+
+  def initialize(guid)
+    super
+    @guid = guid
+  end
+
+  def store_dir
+    raise 'missing guid' if @guid.blank?
+    "preneed_attachments/#{@guid}"
+  end
 end
