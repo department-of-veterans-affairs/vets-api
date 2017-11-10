e0393710a1bfddcd0fe983d81891e0937d3fe75b
Lihan Li <frankieteardrop@gmail.com>
2017-11-10 17:15:34 -0500

    set aws config in preneeds uploader

--- app/uploaders/preneed_attachment_uploader.rb
+++ app/uploaders/preneed_attachment_uploader.rb
@@ -2,13 +2,20 @@
 
 class PreneedAttachmentUploader < CarrierWave::Uploader::Base
   include ValidateFileSize
+  include SetAwsConfig
 
   MAX_FILE_SIZE = 25.megabytes
 
   def initialize(guid)
     super
     @guid = guid
-    # TODO: setup s3 config
+
+    set_aws_config(
+      Settings.preneeds.s3.aws_access_key_id,
+      Settings.preneeds.s3.aws_secret_access_key,
+      Settings.preneeds.s3.region,
+      Settings.preneeds.s3.bucket
+    ) if Rails.env.production?
   end
 
   def extension_white_list
