4ffc57cd806b73a6d9cfb45e5ec545f9c84da2ff
Ryan Baker <ryan.baker@adhocteam.us>
2017-11-11 19:58:30 -0800

    Add factories for other tables

--- spec/factories/education_benefits.rb
+++ spec/factories/education_benefits.rb
@@ -2,4 +2,10 @@
 FactoryGirl.define do
   factory :education_benefits, class: SavedClaim::EducationBenefits do
   end
+
+  factory :saved_claim, class: SavedClaim do
+  end
+
+  factory :education_benefits_submission, class: EducationBenefitsSubmission do
+  end
 end
