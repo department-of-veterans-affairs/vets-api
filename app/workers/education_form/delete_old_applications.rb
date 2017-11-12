4381efa6bcac213ad4012f5995b29510649fcbb0
Ryan Baker <ryan.baker@adhocteam.us>
2017-11-11 19:52:25 -0800

    Update specs and disable deleting submissions for now

--- app/workers/education_form/delete_old_applications.rb
+++ app/workers/education_form/delete_old_applications.rb
@@ -5,27 +5,26 @@ module EducationForm
 
     def perform
       edu_claim_ids = []
-      edu_submission_ids = []
       saved_claim_ids = []
+      # edu_submission_ids = []
 
-      EducationBenefitsClaim.where("processed_at < '#{2.months.ago}'").find_each do |record|
+      EducationBenefitsClaim.eager_load(:saved_claim) # , :education_benefits_submission)
+                            .where("processed_at < '#{2.months.ago}'")
+                            .find_each do |record|
         edu_claim_ids << record.id
-        edu_submission_ids << record&.education_benefits_submission.id
+        # edu_submission_ids << record&.education_benefits_submission.id
         saved_claim_ids << record&.saved_claim.id
       end
 
       edu_claim_ids.compact!
-      edu_submission_ids.compact!
+      # edu_submission_ids.compact!
       saved_claim_ids.compact!
 
-      total = edu_claim_ids.length + edu_submission_ids.length + saved_claim_ids.length
-
-      logger.info("Deleting #{total} total old records")
       logger.info("Deleting #{edu_claim_ids.length} old education benefits claims")
-      logger.info("Deleting #{edu_submission_ids.length} old education benefits submissions")
+      # logger.info("Deleting #{edu_submission_ids.length} old education benefits submissions")
       logger.info("Deleting #{saved_claim_ids.length} old saved claims")
 
-      EducationBenefitsSubmission.delete(edu_submission_ids)
+      # EducationBenefitsSubmission.delete(edu_submission_ids)
       EducationBenefitsClaim.delete(edu_claim_ids)
       SavedClaim.delete(saved_claim_ids)
     end
--- spec/jobs/education_form/delete_old_applications_spec.rb
+++ spec/jobs/education_form/delete_old_applications_spec.rb
@@ -3,16 +3,43 @@ require 'rails_helper'
 
 RSpec.describe EducationForm::DeleteOldApplications do
   before do
-    @claim_nil = create(:education_benefits_claim, processed_at: nil)
-    @claim_new = create(:education_benefits_claim, processed_at: Time.now.utc)
-    @claim_new = create(:education_benefits_claim, processed_at: 45.days.ago.utc)
-    @claim_old = create(:education_benefits_claim, processed_at: 3.months.ago)
+    @saved_claim_nil = create(:saved_claim)
+    @edu_claim_nil = create(:education_benefits_claim, processed_at: nil, saved_claim: @saved_claim_nil)
+    # @submission_nil = create(:education_benefits_submission, education_benefits_claim: @edu_claim_nil)
+
+    @saved_claim_new = create(:saved_claim)
+    @edu_claim_new = create(:education_benefits_claim, processed_at: Time.now.utc)
+    # @submission_new = create(:education_benefits_submission, education_benefits_claim: @edu_claim_new)
+
+    @saved_claim_semi_new = create(:saved_claim)
+    @edu_claim_semi_new = create(:education_benefits_claim, processed_at: 45.days.ago.utc)
+    # @submission_new = create(:education_benefits_submission, education_benefits_claim: @edu_claim_semi_new)
+
+    @saved_claim_old = create(:saved_claim)
+    @edu_claim_old = create(:education_benefits_claim, processed_at: 3.months.ago)
+    # @submission_old = create(:education_benefits_submission, education_benefits_claim: @edu_claim_old)
   end
 
   describe '#perform' do
     it 'deletes old records' do
       expect { subject.perform }.to change { EducationBenefitsClaim.count }.from(4).to(3)
-      expect { @claim_old.reload }.to raise_exception(ActiveRecord::RecordNotFound)
+      expect { subject.perform }.to change { SavedClaim.count }.from(4).to(3)
+      # expect { subject.perform }.to change { EducationBenefitsSubmission.count }.from(4).to(3)
+
+      expect { @edu_claim_old.reload }.to raise_exception(ActiveRecord::RecordNotFound)
+      expect { @saved_claim_old.reload }.to raise_exception(ActiveRecord::RecordNotFound)
+
+      expect { @edu_claim_new.reload }.not_to raise_exception(ActiveRecord::RecordNotFound)
+      expect { @saved_claim_new.reload }.not_to raise_exception(ActiveRecord::RecordNotFound)
+      expect { @edu_claim_semi_new.reload }.not_to raise_exception(ActiveRecord::RecordNotFound)
+      expect { @saved_claim_semi_new.reload }.not_to raise_exception(ActiveRecord::RecordNotFound)
+      expect { @edu_claim_nil.reload }.not_to raise_exception(ActiveRecord::RecordNotFound)
+      expect { @saved_claim_nil.reload }.not_to raise_exception(ActiveRecord::RecordNotFound)
+      # expect { @submission_old.reload }.to raise_exception(ActiveRecord::RecordNotFound)
+    end
+
+    it 'does not delete newer records' do
+      expect { subject.perform }.to change { EducationBenefitsClaim.count }.from(4).to(3)
     end
   end
 end
