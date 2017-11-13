8ec8938dfe05701894f50b87c765cf72c7286738
Ryan Baker <ryan.baker@adhocteam.us>
2017-11-12 18:31:19 -0800

    Change factories and temporarily disable submission deletion

--- spec/factories/education_benefits.rb
+++ spec/factories/education_benefits.rb
@@ -2,10 +2,4 @@
 FactoryGirl.define do
   factory :education_benefits, class: SavedClaim::EducationBenefits do
   end
-
-  factory :saved_claim, class: SavedClaim do
-  end
-
-  factory :education_benefits_submission, class: EducationBenefitsSubmission do
-  end
 end
---
+++ spec/factories/saved_claim.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+FactoryGirl.define do
+  factory :saved_claim, class: SavedClaim do
+  end
+end
