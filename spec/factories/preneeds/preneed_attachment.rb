ff9f6112e063c586365777f2c7d8435edbf9ea11
Lihan Li <frankieteardrop@gmail.com>
2017-11-15 16:03:36 -0500

    preneed attachment factory

---
+++ spec/factories/preneeds/preneed_attachment.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :preneed_attachment, class: Preneeds::PreneedAttachment do
+    after(:build) do |preneed_attachment|
+      preneed_attachment.set_file_data!(Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'pdf_fill', 'extras.pdf'), 'application/pdf'))
+    end
+  end
+end
--- spec/models/preneeds/preneed_attachment_hash_spec.rb
+++ spec/models/preneeds/preneed_attachment_hash_spec.rb
@@ -1,17 +1,23 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe Preneeds::PreneedAttachment do
-  let(:preneed_attachment) { build(:preneed_attachment) }
+RSpec.describe Preneeds::PreneedAttachmentHash do
+  include ActionDispatch::TestProcess
 
-  describe '#set_file_data!' do
+  let(:preneed_attachment) do
+    create(:preneed_attachment)
+  end
+
+  describe '#get_file' do
     it 'should store the file and set the file_data' do
+      set_file_data
       expect(preneed_attachment.parsed_file_data['filename']).to eq('extras.pdf')
     end
   end
 
   describe '#get_file' do
     it 'should get the file from storage' do
+      set_file_data
       preneed_attachment.save!
       preneed_attachment2 = described_class.find(preneed_attachment.id)
       file = preneed_attachment2.get_file
