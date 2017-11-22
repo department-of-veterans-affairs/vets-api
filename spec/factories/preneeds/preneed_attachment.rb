12642f89631a8c705da2251a1d8870ad4a3a5e95
Lihan Li <frankieteardrop@gmail.com>
2017-11-22 15:08:33 -0500

    use old extras.pdf

--- spec/factories/preneeds/preneed_attachment.rb
+++ spec/factories/preneeds/preneed_attachment.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :preneed_attachment, class: Preneeds::PreneedAttachment do
     after(:build) do |preneed_attachment|
       preneed_attachment.set_file_data!(
-        Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'pdf_fill', 'extras.pdf'), 'application/pdf')
+        Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'preneeds', 'extras.pdf'), 'application/pdf')
       )
     end
   end
