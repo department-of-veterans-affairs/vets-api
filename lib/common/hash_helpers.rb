f05c14f52eca1b3ce1ce5c3e1f780335503073dd
Lihan Li <frankieteardrop@gmail.com>
2017-11-27 12:42:37 -0500

    lint

--- lib/common/hash_helpers.rb
+++ lib/common/hash_helpers.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
 module Common
   module HashHelpers
     module_function
 
     def deep_compact(hash)
-      delete_if_block = Proc.new do |k, v|
-        v.kind_of?(Hash) ? (v.delete_if(&delete_if_block); nil) : v.nil?
+      delete_if_block = proc do |_k, v|
+        if v.is_a?(Hash)
+          v.delete_if(&delete_if_block)
+          nil
+        else
+          v.nil?
+        end
       end
 
       hash.delete_if(&delete_if_block)
