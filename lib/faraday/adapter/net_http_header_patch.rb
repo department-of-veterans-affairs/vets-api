37f0194a7e728b2b3054c149819484e7bd45b056
alastair <alastair.j.dawson@gmail.com>
2017-11-27 15:42:35 -0800

    revert net http adaper add rubocop ignore

--- lib/faraday/adapter/net_http_header_patch.rb
+++ lib/faraday/adapter/net_http_header_patch.rb
@@ -5,13 +5,15 @@ require_relative 'http'
 module Faraday
   class Adapter
     class NetHttpHeaderPatch < Faraday::Adapter::NetHttp
+      # rubocop:disable AssignmentInCondition
       def net_http_connection(env)
-        if proxy == env[:request][:proxy]
+        if proxy = env[:request][:proxy]
           Faraday::Adapter::HTTP::Proxy(proxy[:uri].host, proxy[:uri].port, proxy[:user], proxy[:password])
         else
           Faraday::Adapter::HTTP
         end.new(env[:url].host, env[:url].port || (env[:url].scheme == 'https' ? 443 : 80))
       end
+      # rubocop:enable AssignmentInCondition
     end
   end
 end
