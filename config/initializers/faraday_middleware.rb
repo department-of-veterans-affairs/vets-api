e065b20c7e755d5c1fafd9059e4d1671a82899f2
alastair <alastair.j.dawson@gmail.com>
2017-11-27 23:06:15 -0800

    spec test

--- config/initializers/faraday_middleware.rb
+++ config/initializers/faraday_middleware.rb
@@ -1,3 +1,2 @@
 # frozen_string_literal: true
 Faraday::Middleware.register_middleware remove_cookies: Common::Client::Middleware::Request::RemoveCookies
-Faraday::Adapter.register_middleware net_http_header_patch: -> { Faraday::Adapter::NetHttpHeaderPatch }
