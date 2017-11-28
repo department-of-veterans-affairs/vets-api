8a73bbcdb294ae2f299616643373fa75422509c3
Lihan Li <frankieteardrop@gmail.com>
2017-11-28 15:28:56 -0500

    add submitted_at to receive application

--- spec/models/preneeds/receive_application_spec.rb
+++ spec/models/preneeds/receive_application_spec.rb
@@ -7,14 +7,15 @@ RSpec.describe Preneeds::ReceiveApplication do
 
     let(:params) { attributes_for :receive_application }
 
-    it 'populates attributes' do
+    it 'populates attributes', run_at: '2017-01-04 03:00:00 EDT' do
       name_map = described_class.attribute_set.map(&:name)
 
-      expect(name_map).to contain_exactly(:tracking_number, :return_code, :application_uuid, :return_description)
+      expect(name_map).to contain_exactly(:tracking_number, :return_code, :application_uuid, :return_description, :submitted_at)
       expect(subject.tracking_number).to eq(params[:tracking_number])
       expect(subject.return_code).to eq(params[:return_code])
       expect(subject.application_uuid).to eq(params[:application_uuid])
       expect(subject.return_description).to eq(params[:return_description])
+      expect(subject.submitted_at).to eq(Time.zone.now)
     end
   end
 end
