4fb2a00ecc4ec59472f1e60a6cef3d7deaf0b3fc
Lihan Li <frankieteardrop@gmail.com>
2017-11-30 18:58:35 -0500

    use fake route

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -7,14 +7,14 @@ shared_examples_for 'skip_sentry_404' do
 
     controller_klass = described_class
     Rails.application.routes.draw do
-      get '/authenticate' => "#{controller_klass.to_s.underscore.gsub('_controller', '')}#authenticate"
+      get '/fake_route' => "#{controller_klass.to_s.underscore.gsub('_controller', '')}#authenticate"
     end
   end
 
   it 'should not log 404 to sentry' do
     allow_any_instance_of(ApplicationController).to receive(:log_exception_to_sentry) { raise }
 
-    get(controller.present? ? :authenticate : '/authenticate')
+    get(controller.present? ? :authenticate : '/fake_route')
     expect(response.code).to eq('404')
   end
 
