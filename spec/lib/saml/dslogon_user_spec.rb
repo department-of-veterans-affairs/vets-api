ee47e5a418fae261135af56e0236e75798976224
Kam Karshenas <kam@saneshark.com>
2017-11-29 19:13:48 -0800

    redoing some saml specs

--- spec/lib/saml/dslogon_user_spec.rb
+++ spec/lib/saml/dslogon_user_spec.rb
@@ -9,11 +9,10 @@ RSpec.describe SAML::User do
       instance_double(OneLogin::RubySaml::Response, attributes: saml_attributes,
                                                     decrypted_document: decrypted_document_partial)
     end
-    let(:decrypted_document_partial) { REXML::Document.new(dslogon_response) }
-    let(:dslogon_response) { File.read("#{::Rails.root}/spec/fixtures/files/saml_xml/dslogon_response.xml") }
-
+    let(:decrypted_document_partial) { REXML::Document.new(response_partial) }
+    let(:response_partial) { File.read("#{::Rails.root}/spec/fixtures/files/saml_responses/#{response_file}") }
+    let(:response_file) { 'dslogon.xml' }
     let(:described_instance) { described_class.new(saml_response) }
-    let(:user_identity) { UserIdentity.new(described_instance.to_hash) }
 
     context 'logging' do
       let(:saml_attributes) do
@@ -80,16 +79,8 @@ RSpec.describe SAML::User do
         )
       end
 
-      it 'properly constructs a user' do
-        expect(user_identity).to be_valid
-      end
-
-      it 'has email' do
-        expect(user_identity.email).to be_present
-      end
-
       it 'has various important attributes' do
-        expect(user_identity).to have_attributes(
+        expect(described_instance.to_hash).to eq(
           uuid: '5e7465d7c3ba47f3a388d00df1e1a982',
           email: 'fake.user@vets.gov',
           first_name: nil,
@@ -97,12 +88,26 @@ RSpec.describe SAML::User do
           last_name: nil,
           gender: nil,
           birth_date: nil,
-          zip: nil,
           ssn: nil,
           loa: { current: 1, highest: 3 },
-          multifactor: true,
-          authn_context: 'dslogon'
+          multifactor: 'true',
+          authn_context: 'dslogon',
+          dslogon_deceased: nil,
+          dslogon_edipi: '1606997570',
+          dslogon_status: nil
         )
+      end
+
+      it 'is not changing multifactor' do
+        expect(described_instance.changing_multifactor?).to be_falsey
+      end
+
+      context 'multifactor' do
+        let(:response_file) { 'dslogon_multifactor.xml' }
+
+        it 'is changing multifactor' do
+          expect(described_instance.changing_multifactor?).to be_truthy
+        end
       end
     end
 
@@ -127,16 +132,8 @@ RSpec.describe SAML::User do
         )
       end
 
-      it 'properly constructs a user' do
-        expect(user_identity.valid?(:loa3_user)).to be_truthy
-      end
-
-      it 'has email' do
-        expect(user_identity.email).to be_present
-      end
-
       it 'has various important attributes' do
-        expect(user_identity).to have_attributes(
+        expect(described_instance.to_hash).to eq(
           uuid: 'cf0f3deb1b424d3cb4f792e8346a4d71',
           email: 'fake.user@vets.gov',
           first_name: 'KENT',
@@ -144,38 +141,26 @@ RSpec.describe SAML::User do
           last_name: 'WELLS',
           gender: 'M',
           birth_date: '1973-09-03',
-          zip: nil,
           ssn: '796178410',
           loa: { current: 3, highest: 3 },
-          multifactor: false,
+          multifactor: 'false',
           authn_context: 'dslogon',
-        )
-      end
-    end
-
-    context 'premium user with gender (unknown)' do
-      let(:saml_attributes) do
-        OneLogin::RubySaml::Attributes.new(
-          'dslogon_status' => ['SPONSOR'],
-          'dslogon_assurance' => ['2'],
-          'dslogon_gender' => ['unknown'],
-          'dslogon_deceased' => ['false'],
-          'dslogon_idtype' => ['ssn'],
-          'uuid' => ['cf0f3deb1b424d3cb4f792e8346a4d71'],
-          'dslogon_uuid' => ['1016980877'],
-          'email' => ['fake.user@vets.gov'],
-          'multifactor' => [],
-          'level_of_assurance' => [],
-          'dslogon_birth_date' => ['1973-09-03'],
-          'dslogon_fname' => ['KENT'],
-          'dslogon_lname' => ['WELLS'],
-          'dslogon_mname' => ['Mayo'],
-          'dslogon_idvalue' => ['796178410']
+          dslogon_deceased: 'false',
+          dslogon_edipi: '1016980877',
+          dslogon_status: 'SPONSOR'
         )
       end
 
-      it 'properly constructs a user' do
-        expect(user_identity.multifactor).to be_falsey # when nil
+      it 'is not changing multifactor' do
+        expect(described_instance.changing_multifactor?).to be_falsey
+      end
+
+      context 'multifactor' do
+        let(:response_file) { 'dslogon_multifactor.xml' }
+
+        it 'is changing multifactor' do
+          expect(described_instance.changing_multifactor?).to be_truthy
+        end
       end
     end
   end
