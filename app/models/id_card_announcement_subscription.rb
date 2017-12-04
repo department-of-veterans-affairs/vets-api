98ad5f9f5f257270f74e634097b704f3e8ae2889
James Kassemi <jkassemi@users.noreply.github.com>
2017-12-03 21:53:34 -0700

    VIC announcement subscription email length limit (#1578)
    * VIC announcement subscription email length limit
    
    * Fix rubocop warning

--- app/models/id_card_announcement_subscription.rb
+++ app/models/id_card_announcement_subscription.rb
@@ -2,5 +2,6 @@
 class IdCardAnnouncementSubscription < ActiveRecord::Base
   validates :email,
             uniqueness: true,
+            length: { maximum: 255 },
             format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ } # Devise::email_regexp
 end
