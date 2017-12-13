5436ac008c05da485a702d8f9c22f0b1d9e9e583
Lihan Li <frankieteardrop@gmail.com>
2017-12-13 18:51:48 -0500

    match everything for claim with errors

--- spec/lib/evss/error_middleware_spec.rb
+++ spec/lib/evss/error_middleware_spec.rb
@@ -8,8 +8,8 @@ describe EVSS::ErrorMiddleware do
   let(:auth_headers) { EVSS::AuthHeaders.new(current_user).to_h }
   let(:claims_service) { EVSS::ClaimsService.new(auth_headers) }
 
-  it 'should raise the proper error' do
-    VCR.use_cassette('evss/claims/claim_with_errors') do
+  it 'should raise the proper error', run_at: 'Wed, 13 Dec 2017 23:45:40 GMT' do
+    VCR.use_cassette('evss/claims/claim_with_errors', VCR::MATCH_EVERYTHING) do
       expect { claims_service.find_claim_by_id 1 }.to raise_exception(described_class::EVSSError)
     end
   end
