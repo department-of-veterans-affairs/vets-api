7cdcb2f0ef54b27c08b082268753959e86cdb233
Lihan Li <frankieteardrop@gmail.com>
2017-12-13 17:39:57 -0500

    use evss user in error middleware spec

--- spec/lib/evss/error_middleware_spec.rb
+++ spec/lib/evss/error_middleware_spec.rb
@@ -4,7 +4,7 @@ require 'evss/claims_service'
 require 'evss/auth_headers'
 
 describe EVSS::ErrorMiddleware do
-  let(:current_user) { FactoryBot.build(:user, :loa3) }
+  let(:current_user) { FactoryBot.build(:evss_user) }
   let(:auth_headers) { EVSS::AuthHeaders.new(current_user).to_h }
   let(:claims_service) { EVSS::ClaimsService.new(auth_headers) }
 
