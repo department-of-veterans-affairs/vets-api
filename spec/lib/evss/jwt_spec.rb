1f09ff4f0a88f154f913de976910c0311e0c7d82
omgitsbillryan <bill.ryan@adhocteam.us>
2017-12-18 17:24:04 -0500

    lint fix and add spec for header

--- spec/lib/evss/jwt_spec.rb
+++ spec/lib/evss/jwt_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 require 'evss/jwt'
 
 describe EVSS::Jwt do
-  let(:some_random_time) { Time.local(2016, 9, 1, 12, 0, 0) }
+  let(:some_random_time) { Time.zone.at(1_513_634_300) }
   subject(:decrypted_token) do
     JWT.decode(
       described_class.new(current_user).encode,
@@ -18,26 +18,28 @@ describe EVSS::Jwt do
     before { Timecop.freeze(some_random_time) }
     after { Timecop.return }
 
-    it 'has the right properties' do
+    it 'has the right payload' do
       payload = decrypted_token[0]
-      headers = decrypted_token[1]
 
       expect(payload['correlationIds']).to_not be_nil
       expect(payload['jti']).to be_a_uuid
-      expect(payload).to include({
-          'iat' => 1472745600,
-          'exp' => 1472745900,
-          'iss' => 'Vets.gov',
-          'assuranceLevel' => 3,
-          'email' => current_user.email,
-          'firstName' => current_user.first_name,
-          'middleName' => current_user.middle_name,
-          'lastName' => current_user.last_name,
-          'birthDate' => current_user.birth_date,
-          'gender' => current_user.gender,
-          'prefix' =>"",
-          'suffix' =>""
-        })
+      expect(payload).to include('iat' => some_random_time.to_i,
+                                 'exp' => some_random_time.to_i + described_class::EXP_WINDOW,
+                                 'iss' => described_class::ISSUER,
+                                 'assuranceLevel' => 3,
+                                 'email' => current_user.email,
+                                 'firstName' => current_user.first_name,
+                                 'middleName' => current_user.middle_name,
+                                 'lastName' => current_user.last_name,
+                                 'birthDate' => current_user.birth_date,
+                                 'gender' => current_user.gender,
+                                 'prefix' => '',
+                                 'suffix' => '')
+    end
+
+    it 'has the right headers' do
+      headers = decrypted_token[1]
+      expect(headers).to eq('typ' => 'JWT', 'alg' => 'HS256')
     end
   end
 end
