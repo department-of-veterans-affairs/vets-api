7cd274fdf7e02bc1bd0d57ea01997f9d48b695b8
Ryan Baker <ryan.baker@adhocteam.us>
2017-12-20 15:25:33 -0800

    Include spaces and add spec

--- spec/lib/sftp_writer/remote_spec.rb
+++ spec/lib/sftp_writer/remote_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe SFTPWriter::Remote do
       expect(described_class.new({}, logger: {}).close).to eq(nil)
     end
   end
+
+  describe '#sanitize' do
+    it 'should return a filename without spaces or colons' do
+      result = described_class.new({}, logger: {}).send(:sanitize, 'test:foo bar.pdf')
+      expect(result).to eq('test_foo_bar.pdf')
+    end
+  end
 end
