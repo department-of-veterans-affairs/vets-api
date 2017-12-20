ebd11bf8c143258d7d13560823b447ed12f49580
Ryan Baker <ryan.baker@adhocteam.us>
2017-12-20 15:25:06 -0800

    Extract filename sanitization to method

--- lib/sftp_writer/remote.rb
+++ lib/sftp_writer/remote.rb
@@ -28,7 +28,7 @@ class SFTPWriter::Remote
   end
 
   def write(contents, filename)
-    path = File.join([write_path, filename.delete(':')].compact)
+    path = File.join([write_path, sanitize(filename)].compact)
     @logger.info("Writing #{path}")
     mkdir_safe(path)
     sftp.upload!(StringIO.new(contents), path)
@@ -57,5 +57,9 @@ class SFTPWriter::Remote
         raise if $ERROR_INFO.code != Net::SFTP::Constants::StatusCodes::FX_FILE_ALREADY_EXISTS
       end
     end
+  end
+
+  def sanitize(filename)
+    filename.gsub(/[: ]/, '_')
   end
 end
