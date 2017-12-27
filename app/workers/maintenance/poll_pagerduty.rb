33940d779012e8a7fbe1bdf74b7df265d6583f1e
Patrick Vinograd <patrick@adhocteam.us>
2017-12-27 14:58:14 -0800

    Specs for worker

--- app/workers/maintenance/poll_pagerduty.rb
+++ app/workers/maintenance/poll_pagerduty.rb
@@ -5,6 +5,7 @@ require 'pagerduty/maintenance_client'
 module PagerDuty
   class PollMaintenanceWindows
     include Sidekiq::Worker
+    include SentryLogging
 
     MESSAGE_INDICATOR = 'USER_MESSAGE:'
 
@@ -16,15 +17,17 @@ module PagerDuty
       client = PagerDuty::MaintenanceClient.new
       pd_windows = client.get_all
       pd_windows.each do |pd_win|
-        pd_win['description'] = parse_user_message(pd_win['description'])
-        window = MaintenanceWindow.find_or_initialize_by(pagerduty_id: pd_win.pagerduty_id,
-                                                         external_service: pd_win.external_service)
+        pd_win[:description] = parse_user_message(pd_win[:description])
+        window = MaintenanceWindow.find_or_initialize_by(pagerduty_id: pd_win[:pagerduty_id],
+                                                         external_service: pd_win[:external_service])
         window.update(pd_win)
       end
       open_ids = pd_windows.map { |x| x[:pagerduty_id] }
       MaintenanceWindow.end_after(Time.zone.now).each do |api_win|
         api_win.delete unless open_ids.include?(api_win.pagerduty_id)
       end
+    rescue Common::Exceptions::BackendServiceException, Common::Client::Errors::ClientError => e
+      log_exception_to_sentry(e)
     end
   end
 end
