8a5238d323019788c454e4232ce66fc015f9b18b
Patrick Vinograd <patrick@adhocteam.us>
2017-12-27 15:07:13 -0800

    Explanatory comments

--- app/workers/maintenance/poll_pagerduty.rb
+++ app/workers/maintenance/poll_pagerduty.rb
@@ -16,12 +16,17 @@ module PagerDuty
     def perform
       client = PagerDuty::MaintenanceClient.new
       pd_windows = client.get_all
+
+      # Add or update-in-place any open PagerDuty maintenance windows 
       pd_windows.each do |pd_win|
         pd_win[:description] = parse_user_message(pd_win[:description])
         window = MaintenanceWindow.find_or_initialize_by(pagerduty_id: pd_win[:pagerduty_id],
                                                          external_service: pd_win[:external_service])
         window.update(pd_win)
       end
+
+      # Delete any existing records that are not present in the PagerDuty API results
+      # These indicate deleted maintenance windows so we want to stop reporting them
       open_ids = pd_windows.map { |x| x[:pagerduty_id] }
       MaintenanceWindow.end_after(Time.zone.now).each do |api_win|
         api_win.delete unless open_ids.include?(api_win.pagerduty_id)
