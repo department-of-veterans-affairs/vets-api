5e2da89b3f589d8e757e437873136c8cb1e828d8
James Kassemi <jkassemi@users.noreply.github.com>
2017-12-28 17:06:22 -0700

    Rate limiting for feedback form submissions (#1623)
    * Rate limit feedback form submissions
    
    * Add sample endpoint, configuration tests

--- lib/statsd_middleware.rb
+++ lib/statsd_middleware.rb
@@ -12,15 +12,33 @@ class StatsdMiddleware
     status, headers, response = @app.call(env)
     duration = (Time.current - start_time) * 1000.0
 
-    controller = env['action_dispatch.request.path_parameters'][:controller]
-    action = env['action_dispatch.request.path_parameters'][:action]
+    path_parameters = env['action_dispatch.request.path_parameters']
 
-    duration_tags = ["controller:#{controller}", "action:#{action}"]
-    status_tags = duration_tags + ["status:#{status}"]
+    # When ActionDispatch middleware is not processed, as is the case when middleware
+    # such as Rack::Attack halts the call chain while applying a rate limit, path
+    # parameters are not parsed. In this case, we don't have a controller or action
+    # for the request.
+    #
+    # We should never use a dynamic path to apply the tag for the instrumentation,
+    # since this will permit a rogue actor to increase the number of time series
+    # exported from the process and causes instability in the metrics system. Effort
+    # should be taken to track known conditions carefully in alternate metrics. For
+    # the case of Rack::Attack rate limits, we can track the number of 429s responses
+    # based on component at the reverse proxy layer, or with instrumentation provided
+    # by the Rack::Attack middleware (which performs some rudimentary path matching)
 
-    # rubocop:disable Style/RescueModifier
-    StatsD.increment(STATUS_KEY, tags: status_tags) rescue nil
-    StatsD.measure(DURATION_KEY, duration, tags: duration_tags) rescue nil
+    if path_parameters
+      controller = path_parameters[:controller]
+      action = path_parameters[:action]
+
+      duration_tags = ["controller:#{controller}", "action:#{action}"]
+      status_tags = duration_tags + ["status:#{status}"]
+
+      # rubocop:disable Style/RescueModifier
+      StatsD.increment(STATUS_KEY, tags: status_tags) rescue nil
+      StatsD.measure(DURATION_KEY, duration, tags: duration_tags) rescue nil
+    end
+
     # rubocop:enable Style/RescueModifier
     [status, headers, response]
   end
