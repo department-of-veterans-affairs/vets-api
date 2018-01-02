d8e1c1e1d31865cc8e85fc29c8cedd1970e257db
alastair <alastair.j.dawson@gmail.com>
2018-01-02 12:37:26 -0800

    initialize letters/pciu address metrics

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -31,3 +31,10 @@ StatsD.increment(V0::Post911GIBillStatusesController::STATSD_GI_BILL_FAIL_KEY, 0
 EVSS::GiBillStatus::GiBillStatusResponse::KNOWN_ERRORS.each do |_error_key, error_val|
   StatsD.increment(V0::Post911GIBillStatusesController::STATSD_GI_BILL_FAIL_KEY, 0, tags: ["error:#{error_val}"])
 end
+
+# init letters/pciu address stats to 0
+method_keys = EVSS::Letters::Service.instance_methods(false) + EVSS::PCIUAddress::Service.instance_methods(false)
+method_keys.each do |k|
+  StatsD.increment("#{EVSS::Service::STATSD_KEY_PREFIX}.#{k}.total", 0)
+  StatsD.increment("#{EVSS::Service::STATSD_KEY_PREFIX}.#{k}.fail", 0)
+end
