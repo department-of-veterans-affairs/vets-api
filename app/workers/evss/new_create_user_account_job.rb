923e72303dea579969e342e7388751c9e5372534
Lihan Li <frankieteardrop@gmail.com>
2018-01-10 17:40:35 -0500

    new create user account job

--- app/workers/evss/new_create_user_account_job.rb
+++ app/workers/evss/new_create_user_account_job.rb
@@ -3,8 +3,9 @@ module EVSS
   class NewCreateUserAccountJob
     include Sidekiq::Worker
 
-    def perform(user)
-      # TODO
+    def perform(user_uuid)
+      user = User.find(user_uuid)
+      EVSS::EVSSCommon::Service.new(user).create_user_account
     end
   end
 end
---
+++ spec/jobs/evss/new_create_user_account_job_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe EVSS::NewCreateUserAccountJob, type: :job do
+  let(:user) { FactoryBot.create(:user, :loa3) }
+
+  it 'calls create_user_account EVSS API' do
+    client_stub = instance_double('EVSS::Common::Service')
+    expect(User).to receive(:find).with(user.uuid).and_return(user)
+    allow(EVSS::EVSSCommon::Service).to receive(:new).with(user) { client_stub }
+    expect(client_stub).to receive(:create_user_account).once
+    described_class.new.perform(user.uuid)
+  end
+end
