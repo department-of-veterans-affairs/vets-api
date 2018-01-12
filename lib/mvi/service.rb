45c923146e8295320bca0e5d7b536a756e704422
Lihan Li <frankieteardrop@gmail.com>
2018-01-12 17:51:10 -0500

    add opt for historical icns

--- lib/mvi/service.rb
+++ lib/mvi/service.rb
@@ -93,7 +93,7 @@ module MVI
         mvi_profile.birth_date,
         mvi_profile.ssn,
         mvi_profile.gender
-      ).to_xml
+      ).to_xml(historical_icns: true)
     end
 
     def message_user_attributes(user)
