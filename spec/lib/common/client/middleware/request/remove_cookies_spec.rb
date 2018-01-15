6ce48a9fb5c886600593afd4b4e8ee8bf6991073
Ryan Baker <ryan.baker@adhocteam.us>
2018-01-15 12:41:42 -0800

    Revert workaround

--- spec/lib/common/client/middleware/request/remove_cookies_spec.rb
+++ spec/lib/common/client/middleware/request/remove_cookies_spec.rb
@@ -23,7 +23,6 @@ describe Common::Client::Middleware::Request::RemoveCookies do
     let!(:server_thread) do
       Thread.new do
         server = WEBrick::HTTPServer.new(
-          Logger: WEBrick::Log.new(File.open(File::NULL, 'w')),
           Port: TestConfiguration.instance.port
         )
 
