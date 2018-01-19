a7bf6507590c668da91a2c96fc8ed00da6c7ae5d
Lihan Li <frankieteardrop@gmail.com>
2018-01-19 14:45:47 -0500

    change in progress data column

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,4 @@
 # encoding: UTF-8
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220225520) do
+ActiveRecord::Schema.define(version: 20180119194413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,7 +97,7 @@ ActiveRecord::Schema.define(version: 20171220225520) do
   create_table "in_progress_forms", force: :cascade do |t|
     t.string   "user_uuid",              null: false
     t.string   "form_id",                null: false
-    t.string   "encrypted_form_data",    null: false
+    t.text     "encrypted_form_data",    null: false
     t.string   "encrypted_form_data_iv", null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
