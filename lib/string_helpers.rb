434fe186e75b1951f8caf1289e27e87c90fc44ef
Kam Karshenas <kam@saneshark.com>
2018-01-22 11:15:50 -0800

    add string heuristics and use them for ssn mismatch

--- lib/string_helpers.rb
+++ lib/string_helpers.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
+require 'levenshtein'
+
 module StringHelpers
   module_function
 
   def capitalize_only(str)
     str.slice(0, 1).capitalize + str.slice(1..-1)
+  end
+
+  def levenshtein_distance(str_a, str_b)
+    Levenshtein.distance(str_a, str_b)
+  end
+
+  def heuristics(str_a, str_b)
+    {
+      length: [str_a.length, str_b.length],
+      only_digits: [str_a.scan(/^\d+$/).any?, str_b.scan(/^\d+$/).any?],
+      encoding: [str_a.encoding.to_s, str_b.encoding.to_s],
+      levenshtein_d: levenshtein_distance(str_a, str_b)
+    }
   end
 end
