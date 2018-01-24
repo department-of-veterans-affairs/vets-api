17ada1cc75769f0a63444eb97e1207240cf4c279
Kam Karshenas <kam@saneshark.com>
2018-01-24 15:48:00 -0800

    adding specs

---
+++ spec/models/va_facility_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe VAFacility, type: :model do
+  let(:bbox) { [-73.401, 40.685, -77.36, 43.03] }
+  let(:services) { nil }
+
+  describe 'class methods' do
+    context '#query' do
+      it 'fetches the list of vet_center facilities designated by bbox and type' do
+        VCR.use_cassette('facilities/va/ny_vetcenter') do
+          type = 'vet_center'
+          va_facilities = described_class.query(bbox: bbox, type: type, services: services)
+          expect(va_facilities).to be_an(Array)
+          expect(va_facilities).to all(be_a(VAFacility))
+          # NOTE: website does not exist for vetcenter
+          expect(va_facilities.first.attributes.keys).to contain_exactly(
+            :access, :address, :classification, :facility_type, :feedback, :hours, :lat,
+            :long, :name, :phone, :services, :unique_id
+          )
+          expect(va_facilities.map(&:hours)).to all(be_a(Hash).and include(
+            *%w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+          ))
+
+          # NOTE: this spec is going to fail because ARCGis Production doesnt have the values yet
+          #expect(va_facilities.map(&:hours).map(&:values).flatten).to all(be)
+        end
+      end
+
+      it 'fetches the list of health facilities designated by bbox and type' do
+        VCR.use_cassette('facilities/va/ny_health') do
+          type = 'health'
+          va_facilities = described_class.query(bbox: bbox, type: type, services: services)
+          expect(va_facilities).to be_an(Array)
+          expect(va_facilities).to all(be_a(VAFacility))
+          expect(va_facilities.first.attributes.keys).to contain_exactly(
+            :access, :address, :classification, :facility_type, :feedback, :hours, :lat,
+            :long, :name, :phone, :services, :unique_id, :website
+          )
+          expect(va_facilities.map(&:hours)).to all(be_a(Hash).and include(
+            *%w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+          ))
+          expect(va_facilities.map(&:hours).map(&:values).flatten).to all(be)
+        end
+      end
+
+      it 'fetches the list of benefits facilities designated by bbox and type' do
+        VCR.use_cassette('facilities/va/ny_benefits') do
+          type = 'benefits'
+          va_facilities = described_class.query(bbox: bbox, type: type, services: services)
+          expect(va_facilities).to be_an(Array)
+          expect(va_facilities).to all(be_a(VAFacility))
+          expect(va_facilities.first.attributes.keys).to contain_exactly(
+            :access, :address, :classification, :facility_type, :feedback, :hours, :lat,
+            :long, :name, :phone, :services, :unique_id, :website
+          )
+          expect(va_facilities.map(&:hours)).to all(be_a(Hash).and include(
+            *%w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+          ))
+          expect(va_facilities.map(&:hours).map(&:values).flatten).to all(be)
+        end
+      end
+
+      it 'fetches the list of cemetery facilities designated by bbox and type' do
+        VCR.use_cassette('facilities/va/ny_cemetery') do
+          type = 'cemetery'
+          va_facilities = described_class.query(bbox: bbox, type: type, services: services)
+          expect(va_facilities).to be_an(Array)
+          expect(va_facilities).to all(be_a(VAFacility))
+          expect(va_facilities.first.attributes.keys).to contain_exactly(
+            :access, :address, :classification, :facility_type, :feedback, :hours, :lat,
+            :long, :name, :phone, :services, :unique_id, :website
+          )
+          expect(va_facilities.map(&:hours)).to all(be_a(Hash).and include(
+            *%w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+          ))
+          expect(va_facilities.map(&:hours).map(&:values).flatten).to all(be)
+        end
+      end
+    end
+  end
+end
