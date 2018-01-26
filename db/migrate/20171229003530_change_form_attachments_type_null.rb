288509fadef328fdc094dc09c6b71f5d126dd3e7
lihan <frankieteardrop@gmail.com>
2018-01-26 14:45:42 -0500

    fix form attachment migration (#1673)
    * fix migration
    
    * put data migration in last migration

--- db/migrate/20171229003530_change_form_attachments_type_null.rb
+++ db/migrate/20171229003530_change_form_attachments_type_null.rb
@@ -1,6 +1,8 @@
 class ChangeFormAttachmentsTypeNull < ActiveRecord::Migration
+  safety_assured
+
   def change
+    FormAttachment.update_all(type: 'Preneeds::PreneedAttachment')
     change_column :form_attachments, :type, :string, null: false
-    change_column_default(:form_attachments, :type, nil)
   end
 end
