14aefc35b881598f7ccb94cd704d4e287f4b97e4
Lihan Li <frankieteardrop@gmail.com>
2018-01-29 18:46:54 -0500

    form matches schema spec

--- spec/models/vic/vic_submission_spec.rb
+++ spec/models/vic/vic_submission_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe VIC::VICSubmission, type: :model do
   describe '#update_state_to_completed' do
     it 'should set the state when then response is set' do
       submission = described_class.new
+      submission.save(validate: false)
       submission.response = { foo: true}
       expect(submission.valid?).to eq(true)
       expect(submission.state).to eq('success')
@@ -18,6 +19,16 @@ RSpec.describe VIC::VICSubmission, type: :model do
       allow_any_instance_of(described_class).to receive(:id).and_return(1)
       expect(VIC::SubmissionJob).to receive(:perform_async).with(1, vic_submission.form)
       vic_submission.save!
+    end
+  end
+
+  describe '#form_matches_schema' do
+    context 'with an invalid form' do
+      it 'should have a schema error' do
+        submission = described_class.new(form: { foo: 1 }.to_json)
+        expect(submission.valid?).to eq(false)
+        expect(submission.errors[:form][0].include?("The property '#/' contains additional properties [\"foo\"]")).to eq(true)
+      end
     end
   end
 end
