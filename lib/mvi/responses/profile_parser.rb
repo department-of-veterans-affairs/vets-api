0a1a490c174e4919b758950544fcf55b56da679f
Lihan Li <frankieteardrop@gmail.com>
2018-01-30 16:50:43 -0500

    lint

--- lib/mvi/responses/profile_parser.rb
+++ lib/mvi/responses/profile_parser.rb
@@ -89,8 +89,7 @@ module MVI
       def build_mvi_profile(patient)
         name = parse_name(get_patient_name(patient))
         correlation_ids = MVI::Responses::IdParser.new.parse(patient.locate('id'))
-        log_inactive_mhv_ids(correlation_ids[:mhv_ids].to_a,
-                             correlation_ids[:active_mhv_ids].to_a)
+        log_inactive_mhv_ids(correlation_ids[:mhv_ids].to_a, correlation_ids[:active_mhv_ids].to_a)
         MVI::Models::MviProfile.new(
           given_names: name[:given],
           family_name: name[:family],
