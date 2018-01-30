d6a5311815bd0666c1d781c67f4b55093fe35407
Lihan Li <frankieteardrop@gmail.com>
2018-01-30 16:35:21 -0500

    vic submissions guid index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126201308) do
+ActiveRecord::Schema.define(version: 20180130213405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -208,5 +208,7 @@ ActiveRecord::Schema.define(version: 20180126201308) do
     t.uuid     "guid",                           null: false
     t.json     "response"
   end
+
+  add_index "vic_submissions", ["guid"], name: "index_vic_submissions_on_guid", unique: true, using: :btree
 
 end
