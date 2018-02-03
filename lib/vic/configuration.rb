794a8df0b8dfac9cb587df1355bb886423a93e19
Lihan Li <frankieteardrop@gmail.com>
2018-02-02 19:04:28 -0500

    use vic service to get oauth token

--- lib/vic/configuration.rb
+++ lib/vic/configuration.rb
@@ -2,9 +2,14 @@
 
 module VIC
   class Configuration < Common::Client::Configuration::REST
+    def base_path
+      'https://va--VICDEV.cs33.my.salesforce.com/services/oauth2/token'
+    end
+
     def connection
       @conn ||= Faraday.new(base_path) do |faraday|
         faraday.use :breakers
+        faraday.request :url_encoded
         faraday.response :json
         faraday.adapter Faraday.default_adapter
       end
--- lib/vic/service.rb
+++ lib/vic/service.rb
@@ -4,6 +4,37 @@ module VIC
   class Service < Common::Client::Base
     configuration VIC::Configuration
 
+    SALESFORCE_USERNAME = 'vetsgov-devops@listserv.gsa.gov.vicdev'
+    SALESFORCE_HOST = 'https://test.salesforce.com'
+
+    def oauth_params
+      {
+        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        assertion: jwt_bearer_token
+      }
+    end
+
+    def jwt_bearer_token
+      JWT.encode(claim_set, private_key, 'RS256')
+    end
+
+    def claim_set
+      {
+        iss: Settings.salesforce.consumer_key,
+        sub: SALESFORCE_USERNAME,
+        aud: SALESFORCE_HOST,
+        exp: Time.now.utc.to_i.to_s
+      }
+    end
+
+    def private_key
+      OpenSSL::PKey::RSA.new(File.read(Settings.salesforce.signing_key_path))
+    end
+
+    def get_oauth_token
+      post('', oauth_params)
+    end
+
     def submit(_form)
       {
         confirmation_number: SecureRandom.uuid
