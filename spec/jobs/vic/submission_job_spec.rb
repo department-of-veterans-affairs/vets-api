65de9cadf22c38a8c2aa312f991fb66892c87c88
Lihan Li <frankieteardrop@gmail.com>
2018-02-07 18:52:13 -0500

    update submission job

--- spec/jobs/vic/submission_job_spec.rb
+++ spec/jobs/vic/submission_job_spec.rb
@@ -4,18 +4,22 @@ require 'rails_helper'
 
 RSpec.describe VIC::SubmissionJob do
   let(:uuid) { 'fab2eea7-982e-4171-a2cb-8e9455ab00ed' }
+  let(:user) { create(:user, :loa3) }
 
   describe '#perform' do
     it 'should update the vic submission response' do
       allow(SecureRandom).to receive(:uuid).and_return(uuid)
       vic_submission = build(:vic_submission)
-      vic_submission.user_uuid = create(:user).uuid
+      vic_submission.user_uuid = user.uuid
+      expect(User).to receive(:find).with(user.uuid).and_return(user)
+      expect_any_instance_of(VIC::Service).to receive(:submit).with(JSON.parse(vic_submission.form), user).and_return(case_id: uuid)
+      vic_submission.save!
       described_class.drain
       vic_submission.reload
 
       expect(vic_submission.state).to eq('success')
       expect(vic_submission.response).to eq(
-        'confirmation_number' => uuid
+        'case_id' => uuid
       )
     end
 
