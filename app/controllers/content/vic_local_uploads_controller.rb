b6a3f5683fd086e73bced640bc1fc67ef5e51e79
Ryan Baker <ryan.baker@adhocteam.us>
2018-02-07 15:15:14 -0800

    Ignore coverage for development endpoint

--- app/controllers/content/vic_local_uploads_controller.rb
+++ app/controllers/content/vic_local_uploads_controller.rb
@@ -3,10 +3,11 @@
 module Content
   class VICLocalUploadsController < ApplicationController
     skip_before_action :authenticate
-
+    # :nocov:
     def find_file
       path = Rails.root.join('public', params[:path])
       send_file(path, disposition: 'inline')
     end
+    # :nocov:
   end
 end
