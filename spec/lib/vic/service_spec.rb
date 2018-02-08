6b8665418597b854b0dcabac34b96684d610b48d
Lihan Li <frankieteardrop@gmail.com>
2018-02-07 19:22:05 -0500

    lint

--- spec/lib/vic/service_spec.rb
+++ spec/lib/vic/service_spec.rb
@@ -19,11 +19,11 @@ describe VIC::Service do
 
   describe '#add_user_data!' do
     it 'should add user data to the request form' do
-      converted_form = { "profile_data" => {} }
+      converted_form = { 'profile_data' => {} }
       expect(user.veteran_status).to receive(:title38_status).and_return('V1')
       service.add_user_data!(converted_form, user)
       expect(converted_form).to eq(
-        {"profile_data"=>{"sec_ID"=>"0001234567", "active_ICN"=>user.icn}, "title38_status"=>"V1 - Title 38 Veteran"}
+        'profile_data' => { 'sec_ID' => '0001234567', 'active_ICN' => user.icn }, 'title38_status' => 'V1 - Title 38 Veteran'
       )
     end
   end
@@ -31,12 +31,12 @@ describe VIC::Service do
   describe '#convert_form' do
     it 'should format the form' do
       expect(service.convert_form(parsed_form)).to eq(
-        {"service_branch"=>"Air Force",
-         "email"=>"foo@foo.com",
-         "veteran_full_name"=>{"first"=>"Mark", "last"=>"Olson"},
-         "veteran_address"=>{"city"=>"Milwaukee", "country"=>"US", "postal_code"=>"53130", "state"=>"WI", "street"=>"123 Main St", "street2"=>""},
-         "phone"=>"5551110000",
-         "profile_data"=>{"SSN"=>"111223333", "historical_ICN"=>[]}}
+        'service_branch' => 'Air Force',
+        'email' => 'foo@foo.com',
+        'veteran_full_name' => { 'first' => 'Mark', 'last' => 'Olson' },
+        'veteran_address' => { 'city' => 'Milwaukee', 'country' => 'US', 'postal_code' => '53130', 'state' => 'WI', 'street' => '123 Main St', 'street2' => '' },
+        'phone' => '5551110000',
+        'profile_data' => { 'SSN' => '111223333', 'historical_ICN' => [] }
       )
     end
   end
