6aee898eaadad03f128f59ea1aa2c91d85dfffd5
lihan <frankieteardrop@gmail.com>
2018-02-08 13:33:04 -0500

    Vic profile photo upload (#1679)
    * profile photo uploader
    
    * only set aws control in prod
    
    * test uploader initialize
    
    * dont set aws config
    
    * Use SecureRandom for filename and load test
    
    * Add example files and http load testing script
    
    * Clean up load testing scripts
    
    * Update vic.rake
    
    * Change example pdf and add another
    
    * Remove unnecessary functions and parameterize host
    
    * Fix SecureRandom implementation
    
    * Denote unused param
    
    * Trigger Jenkins build
    
    * Implement separation of authed and unauthed users
    
    * Update profile_photo_attachment.rb
    
    * Override get_file_data
    
    * Update model
    
    * Refactor model
    
    * Refactor get_file to not require in_progress_form
    
    * Refactor controller
    
    * Refactor specs
    
    * Add specs to support coverage
    
    * Add factory for profile photo
    
    * Remove debug printing
    
    * Add more specs to allow coverage to pass
    
    * Add upload retrieval endpoint to use in development
    
    * Ignore coverage for development endpoint
    
    * Improve photo upload mock
    
    * Remove public-read setting
    
    * Fix spec
    
    * Update coverage settings
    
    * Trim whitespace
    
    * Lint

---
+++ spec/controllers/v0/vic/profile_photo_attachments_controller_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe V0::VIC::ProfilePhotoAttachmentsController, type: :controller do
+  describe '#create' do
+    context 'with an anonymous user' do
+      it 'uploads a profile photo attachment' do
+        post(
+          :create,
+          profile_photo_attachment: {
+            file_data: fixture_file_upload('files/sm_file1.jpg')
+          }
+        )
+
+        data = JSON.parse(response.body)['data']['attributes']
+
+        expect(data).to have_key('filename')
+        expect(data['path']).to eq 'profile_photo_attachments/anonymous'
+      end
+    end
+
+    context 'with logged in user' do
+      let(:user) { create(:user, :loa3) }
+
+      before do
+        controller.instance_variable_set(:@current_user, user)
+      end
+
+      it 'uploads a profile photo attachment' do
+        before_count = InProgressForm.count
+
+        post(
+          :create,
+          profile_photo_attachment: {
+            file_data: fixture_file_upload('files/sm_file1.jpg')
+          }
+        )
+
+        data = JSON.parse(response.body)['data']['attributes']
+
+        expect(data).to have_key('filename')
+        expect(data['path']).to eq "profile_photo_attachments/#{InProgressForm.last.id}"
+        expect(InProgressForm.count).not_to eq(before_count)
+      end
+    end
+  end
+end
