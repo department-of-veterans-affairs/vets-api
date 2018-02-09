6bfd51b3c64c3a63f11762b9753eaec411b26756
lihan <frankieteardrop@gmail.com>
2018-02-09 17:19:55 -0500

    Vic virus scan (#1691)
    * extract virus scan into module
    
    * add virus scan
    
    * lint

--- app/uploaders/supporting_documentation_attachment_uploader.rb
+++ app/uploaders/supporting_documentation_attachment_uploader.rb
@@ -3,6 +3,7 @@
 class SupportingDocumentationAttachmentUploader < CarrierWave::Uploader::Base
   include ValidateFileSize
   include SetAwsConfig
+  include UploaderVirusScan
 
   MAX_FILE_SIZE = 25.megabytes
 
--- lib/shrine/plugins/validate_virus_free.rb
+++ lib/shrine/plugins/validate_virus_free.rb
@@ -6,9 +6,7 @@ class Shrine
       module AttacherMethods
         def validate_virus_free(message: nil)
           cached_path = get.download.path
-          # `clamd` runs within service group, needs group read
-          File.chmod(0o640, cached_path)
-          result = ClamScan::Client.scan(location: cached_path)
+          result = Common::VirusScan.scan(cached_path)
           # TODO: Log a the full result to sentry
           result.safe? || add_error_msg(message || result.body)
         end
