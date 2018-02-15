68b2bfafac9bd2f04294eb71168d918b80bc77a6
Ryan Baker <ryan.baker@adhocteam.us>
2018-02-15 15:04:32 -0800

    Refactor spec

--- spec/serializers/profile_photo_attachment_serializer_spec.rb
+++ spec/serializers/profile_photo_attachment_serializer_spec.rb
@@ -4,15 +4,15 @@ require 'rails_helper'
 
 RSpec.describe ProfilePhotoAttachmentSerializer, type: :serializer do
   let(:model) { ::VIC::ProfilePhotoAttachment.new(file_data: file_data.to_json, guid: 'abcd') }
-  let(:data) { JSON.parse(subject) }
+  let(:attributes) { JSON.parse(subject)['attributes'] }
 
   context 'with an anonymous upload' do
     let(:file_data) { { filename: 'test.jpg', path: 'test_dir' } }
     subject { serialize(model, serializer_class: described_class) }
 
     it 'should not include the filename and path' do
-      expect(data['filename']).to be_nil
-      expect(data['path']).to be_nil
+      expect(attributes['filename']).to be_nil
+      expect(attributes['path']).to be_nil
     end
   end
 
@@ -22,8 +22,8 @@ RSpec.describe ProfilePhotoAttachmentSerializer, type: :serializer do
     subject { serialize(model, serializer_class: described_class) }
 
     it 'should include the filename and path' do
-      expect(data['filename']).not_to be_nil
-      expect(data['path']).not_to be_nil
+      expect(attributes['filename']).not_to be_nil
+      expect(attributes['path']).not_to be_nil
     end
   end
 end
