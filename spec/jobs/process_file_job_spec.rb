44a68a2b517724298e15471d2f54546e15dafea8
Lihan Li <frankieteardrop@gmail.com>
2018-02-16 18:19:56 -0500

    spec for process file job

--- spec/jobs/process_file_job_spec.rb
+++ spec/jobs/process_file_job_spec.rb
@@ -6,12 +6,21 @@ RSpec.describe ProcessFileJob do
   describe '#perform' do
     it 'should save the new processed file and delete the old file' do
       profile_photo_attachment = build(:profile_photo_attachment)
+      parsed_file_data = profile_photo_attachment.parsed_file_data
       ProcessFileJob.new.perform(
-        profile_photo_attachment.parsed_file_data['path'],
-        profile_photo_attachment.parsed_file_data['filename']
+        parsed_file_data['path'],
+        parsed_file_data['filename']
       )
-      binding.pry; fail
-      # TOOD finish
+      expect(profile_photo_attachment.get_file.exists?).to eq(true)
+
+      # check old file deleted
+      form_id = parsed_file_data['form_id']
+      attachment_uploader = profile_photo_attachment.send(:get_attachment_uploader, form_id)
+
+      attachment_uploader.retrieve_from_store!(
+        parsed_file_data['filename']
+      )
+      expect(attachment_uploader.file.exists?).to eq(false)
     end
   end
 end
