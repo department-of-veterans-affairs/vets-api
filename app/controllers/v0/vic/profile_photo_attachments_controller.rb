237c8f16295837bd0521c01b952b82ca869f6648
Ryan Baker <rlbaker@users.noreply.github.com>
2018-02-19 14:40:28 -0800

    Update profile_photo_attachments_controller.rb

--- app/controllers/v0/vic/profile_photo_attachments_controller.rb
+++ app/controllers/v0/vic/profile_photo_attachments_controller.rb
@@ -3,6 +3,7 @@
 module V0
   module VIC
     class ProfilePhotoAttachmentsController < ApplicationController
+      # Taken from: https://stackoverflow.com/questions/7905929/how-to-test-valid-uuid-guid/13653180#13653180
       GUID_PATTERN = /[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i
 
       skip_before_action :authenticate, except: :show
