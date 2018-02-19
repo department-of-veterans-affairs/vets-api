7b82bbedc6f50a7deed91e26a12bd6f58fdd0f88
Ryan Baker <ryan.baker@adhocteam.us>
2018-02-19 14:59:49 -0800

    Switch to streaming

--- spec/controllers/v0/vic/profile_photo_attachments_controller_spec.rb
+++ spec/controllers/v0/vic/profile_photo_attachments_controller_spec.rb
@@ -24,13 +24,16 @@ RSpec.describe V0::VIC::ProfilePhotoAttachmentsController, type: :controller do
 
     context 'with a logged in user' do
       let(:user) { create(:user, :loa3) }
+      let(:file_data) { Random.new.bytes(1024) }
 
       before do
         expect_any_instance_of(described_class).to receive(:authenticate_token).at_least(:once).and_return(true)
+        expect_any_instance_of(CarrierWave::SanitizedFile).to receive(:read).at_least(:once).and_yield(file_data)
       end
 
       it 'allows retrieval of filename and path' do
         get(:show, id: guid)
+        puts response.headers
         expect(response).to be_success
       end
     end
