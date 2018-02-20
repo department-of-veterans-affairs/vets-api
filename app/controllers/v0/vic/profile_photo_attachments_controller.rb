51bff65d33e2987aa94aefa178c779ee2e669495
Ryan Baker <ryan.baker@adhocteam.us>
2018-02-20 10:10:07 -0800

    Revert to send_data

--- app/controllers/v0/vic/profile_photo_attachments_controller.rb
+++ app/controllers/v0/vic/profile_photo_attachments_controller.rb
@@ -3,7 +3,10 @@
 module V0
   module VIC
     class ProfilePhotoAttachmentsController < ApplicationController
-      skip_before_action :authenticate
+      skip_before_action :authenticate, only: :create
+
+      # Taken from: https://stackoverflow.com/questions/7905929/how-to-test-valid-uuid-guid/13653180#13653180
+      GUID_PATTERN = /[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i
 
       def create
         form_attachment = ::VIC::ProfilePhotoAttachment.new
@@ -15,6 +18,16 @@ module V0
         form_attachment.save!
 
         render(json: form_attachment, is_anonymous_upload: @current_user.blank?, stream: true)
+      end
+
+      def show
+        guid = params[:id]
+
+        raise ActiveRecord::RecordNotFound unless GUID_PATTERN.match(guid)
+
+        form_attachment = ::VIC::ProfilePhotoAttachment.where(guid: guid).first
+        file = form_attachment.get_file
+        send_data(file.read, filename: file.filename, type: file.content_type, disposition: :inline)
       end
 
       private
--- spec/controllers/v0/vic/profile_photo_attachments_controller_spec.rb
+++ spec/controllers/v0/vic/profile_photo_attachments_controller_spec.rb
@@ -14,8 +14,7 @@ RSpec.describe V0::VIC::ProfilePhotoAttachmentsController, type: :controller do
             file_data: fixture_file_upload('files/sm_file1.jpg')
           }
         )
-        expect(data['filename']).to be_nil
-        expect(data['path']).to be_nil
+        expect(data['guid']).not_to be_nil
       end
     end
 
@@ -36,9 +35,43 @@ RSpec.describe V0::VIC::ProfilePhotoAttachmentsController, type: :controller do
           }
         )
 
-        expect(data).to have_key('filename')
-        expect(data['path']).to eq "profile_photo_attachments/#{InProgressForm.last.id}"
         expect(InProgressForm.count).not_to eq(before_count)
+      end
+    end
+  end
+
+  describe '#show' do
+    let(:guid) { ::VIC::ProfilePhotoAttachment.last.guid }
+
+    before do
+      post(
+        :create,
+        profile_photo_attachment: {
+          file_data: fixture_file_upload('files/sm_file1.jpg')
+        }
+      )
+    end
+
+    context 'with an anonymous user' do
+      it 'fails' do
+        get(:show, id: guid)
+        expect(response).not_to be_success
+      end
+    end
+
+    context 'with a logged in user' do
+      let(:user) { create(:user, :loa3) }
+      let(:file_data) { Random.new.bytes(1024) }
+
+      before do
+        expect_any_instance_of(described_class).to receive(:authenticate_token).at_least(:once).and_return(true)
+      end
+
+      it 'allows retrieval of the file' do
+        get(:show, id: guid)
+        expect(response).to be_success
+        expect(response.headers['Content-Type']).to eq('image/jpeg')
+        expect(response.headers).to have_key('Content-Disposition')
       end
     end
   end
