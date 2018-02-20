0f7a667da261b4aac374b0d960cdd17a886c5d91
Derek <derekhall77@gmail.com>
2018-02-20 16:37:37 -0500

    passing ref to method for trickier mappings

--- lib/common/client/middleware/response/facility_parser.rb
+++ lib/common/client/middleware/response/facility_parser.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'facility_access'
+
 module Common
   module Client
     module Middleware
@@ -30,23 +32,61 @@ module Common
           def attribute_mappings(entry, mapping)
             attrs = entry['attributes']
             mapped_attrs = { 'address' => {}, 'services' => {} }
-            mapping.slice('unique_id', 'name', 'classification', 'website')
-                   .each { |key, value| mapped_attrs[key] = attrs[value] }
-            mapped_attrs['phone'] = nested_attributes(mapping['phone'], attrs)
-            mapped_attrs['address']['physical'] = nested_attributes(mapping['physical'], attrs)
-            mapped_attrs['address']['mailing'] = nested_attributes(mapping['mailing'], attrs) if mapping['mailing']
-            mapped_attrs['hours'] = nested_attributes(mapping['hours'], attrs)
-            mapped_attrs['services']['benefits'] = nested_attributes(mapping['benefits'], attrs) if mapping['benefits']
+            mapping.slice('unique_id', 'name', 'classification', 'website').each do |key, value|
+              mapped_attrs[key] = attrs[value]
+            end
+            mapped_attrs['phone'] = nested(mapping['phone'], attrs)
+            mapped_attrs['address']['physical'] = nested(mapping['physical'], attrs)
+            mapped_attrs['address']['mailing'] = nested(mapping['mailing'], attrs)
+            mapped_attrs['hours'] = nested(mapping['hours'], attrs)
+            mapped_attrs['access'] = nested(mapping['access'], attrs)
+            mapped_attrs['feedback'] = nested(mapping['feedback'], attrs)
+            mapped_attrs['services']['benefits'] = nested(mapping['benefits'], attrs)
             mapped_attrs.merge!(
               'lat' => entry['geometry']['x'],
               'long' => entry['geometry']['y']
             )
           end
 
-          def nested_attributes(item, attrs)
+          def nested(item, attrs)
+            return unless item
             item.each_with_object({}) do |(key, value), hash|
-              hash[key] = attrs[value]
+              hash[key] = value.respond_to?(:call) ? value.call(attrs) : attrs[value]
             end
+          end
+
+          def self.mh_clinic_phone(attrs)
+            val = attrs['MHClinicPhone']
+            val = attrs['MHPhone'] if val.blank?
+            return '' if val.blank?
+            result = val.to_s
+            result << ' x ' + attrs['Extension'].to_s unless
+              (attrs['Extension']).blank? || (attrs['Extension']).zero?
+            result
+          end
+
+          def self.to_date(dtstring)
+            Date.iso8601(dtstring).iso8601
+          end
+
+          def self.satisfaction_data(attrs)
+            result = {}
+            datum = FacilitySatisfaction.find(attrs['StationNumber'].upcase)
+            if datum.present?
+              datum.metrics.each { |k, v| result[k.to_s] = v.present? ? v.round(2) : nil }
+              result['effective_date'] = to_date(datum.source_updated)
+            end
+            result
+          end
+
+          def self.wait_time_data(attrs)
+            result = {}
+            datum = FacilityWaitTime.find(attrs['StationNumber'].upcase)
+            if datum.present?
+              datum.metrics.each { |k, v| result[k.to_s] = v }
+              result['effective_date'] = to_date(datum.source_updated)
+            end
+            result
           end
 
           NCA_MAP = {
@@ -122,14 +162,46 @@ module Common
                          'after_hours' => 'AfterHoursPhone',
                          'patient_advocate' => 'PatientAdvocatePhone',
                          'enrollment_coordinator' => 'EnrollmentCoordinatorPhone',
-                         'pharmacy' => 'PharmacyPhone' },
+                         'pharmacy' => 'PharmacyPhone', 'mental_health_clinic' => method(:mh_clinic_phone) },
             'physical' => { 'address_1' => 'Street', 'address_2' => 'Building',
                             'address_3' => 'Suite', 'city' => 'City', 'state' => 'State',
                             'zip' => 'Zip' },
             'hours' => { 'Monday' => '', 'Tuesday' => '',
                          'Wednesday' => '', 'Thursday' => '',
                          'Friday' => '', 'Saturday' => '',
-                         'Sunday' => '' }
+                         'Sunday' => '' },
+            'access' => { 'health' => method(:wait_time_data) },
+            'feedback' => { 'health' => method(:satisfaction_data) },
+            'services' => {
+              'Audiology' => [],
+              'ComplementaryAlternativeMed' => [],
+              'DentalServices' => [],
+              'DiagnosticServices' => %w[
+                ImagingAndRadiology LabServices
+              ],
+              'EmergencyDept' => [],
+              'EyeCare' => [],
+              'MentalHealthCare' => %w[
+                OutpatientMHCare OutpatientSpecMHCare VocationalAssistance
+              ],
+              'OutpatientMedicalSpecialty' => %w[
+                AllergyAndImmunology CardiologyCareServices DermatologyCareServices
+                Diabetes Dialysis Endocrinology Gastroenterology
+                Hematology InfectiousDisease InternalMedicine
+                Nephrology Neurology Oncology
+                PulmonaryRespiratoryDisease Rheumatology SleepMedicine
+              ],
+              'OutpatientSurgicalSpecialty' => %w[
+                CardiacSurgery ColoRectalSurgery ENT GeneralSurgery
+                Gynecology Neurosurgery Orthopedics PainManagement
+                PlasticSurgery Podiatry ThoracicSurgery Urology
+                VascularSurgery
+              ],
+              'PrimaryCare' => [],
+              'Rehabilitation' => [],
+              'UrgentCare' => [],
+              'WellnessAndPreventativeCare' => []
+            }
           }.freeze
         end
       end
