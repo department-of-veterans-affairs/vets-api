7ae31b8ac011c92fb096281e43ecff8e119eae0f
Ryan Baker <rlbaker@users.noreply.github.com>
2018-02-20 16:11:31 -0800

    VIC Profile Photo download endpoint (#1703)
    * Use send_data
    
    * Update profile_photo_attachment_uploader.rb
    
    * Change flow control
    
    * Update profile_photo_attachments_controller_spec.rb
    
    * Update profile_photo_attachments_controller.rb
    
    * Support async processing
    
    * Spec for invalid guid

--- app/controllers/v0/vic/profile_photo_attachments_controller.rb
+++ app/controllers/v0/vic/profile_photo_attachments_controller.rb
@@ -3,7 +3,10 @@
 module V0
   module VIC
     class ProfilePhotoAttachmentsController < ApplicationController
-      skip_before_action :authenticate, except: :show
+      skip_before_action :authenticate, only: :create
+
+      # Taken from: https://stackoverflow.com/questions/7905929/how-to-test-valid-uuid-guid/13653180#13653180
+      GUID_PATTERN = /[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/i
 
       def create
         form_attachment = ::VIC::ProfilePhotoAttachment.new
@@ -18,8 +21,13 @@ module V0
       end
 
       def show
-        form_attachment = ::VIC::ProfilePhotoAttachment.where(guid: params[:id]).first
-        render(json: form_attachment)
+        guid = params[:id]
+
+        raise Common::Exceptions::RecordNotFound, guid unless GUID_PATTERN.match(guid)
+
+        form_attachment = ::VIC::ProfilePhotoAttachment.where(guid: guid).first
+        file = form_attachment.get_file
+        send_data(file.read, filename: file.filename, type: file.content_type, disposition: :inline)
       end
 
       private
--- spec/controllers/v0/vic/profile_photo_attachments_controller_spec.rb
+++ spec/controllers/v0/vic/profile_photo_attachments_controller_spec.rb
@@ -3,42 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe V0::VIC::ProfilePhotoAttachmentsController, type: :controller do
-  describe '#show' do
-    let(:guid) { ::VIC::ProfilePhotoAttachment.last.guid }
-
-    before do
-      ::VIC::ProfilePhotoAttachment.new(
-        file_data: {
-          filename: 'test.jpg',
-          path: 'anonymous'
-        }.to_json
-      ).save!
-    end
-
-    context 'with an anonymous user' do
-      it 'does not allow retrieval of filename and path' do
-        get(:show, id: guid)
-        expect(response).not_to be_success
-      end
-    end
-
-    context 'with a logged in user' do
-      let(:user) { create(:user, :loa3) }
-      let(:attributes) { JSON.parse(response.body)['data']['attributes'] }
-
-      before do
-        expect_any_instance_of(described_class).to receive(:authenticate_token).at_least(:once).and_return(true)
-        expect_any_instance_of(described_class).to receive(:current_user).at_least(:once).and_return(user)
-      end
-
-      it 'allows retrieval of filename and path' do
-        get(:show, id: guid)
-        expect(attributes['filename']).not_to be_nil
-        expect(attributes['path']).not_to be_nil
-      end
-    end
-  end
-
   describe '#create' do
     let(:data) { JSON.parse(response.body)['data']['attributes'] }
 
@@ -50,8 +14,7 @@ RSpec.describe V0::VIC::ProfilePhotoAttachmentsController, type: :controller do
             file_data: fixture_file_upload('files/sm_file1.jpg')
           }
         )
-        expect(data['filename']).to be_nil
-        expect(data['path']).to be_nil
+        expect(data['guid']).not_to be_nil
       end
     end
 
@@ -72,9 +35,51 @@ RSpec.describe V0::VIC::ProfilePhotoAttachmentsController, type: :controller do
           }
         )
 
-        expect(data).to have_key('filename')
-        expect(data['path']).to eq "profile_photo_attachments/#{InProgressForm.last.id}"
         expect(InProgressForm.count).not_to eq(before_count)
+      end
+    end
+  end
+
+  describe '#show' do
+    let(:guid) { ::VIC::ProfilePhotoAttachment.last.guid }
+
+    before do
+      post(
+        :create,
+        profile_photo_attachment: {
+          file_data: fixture_file_upload('files/sm_file1.jpg')
+        }
+      )
+      ProcessFileJob.drain
+    end
+
+    context 'with an anonymous user' do
+      it 'fails' do
+        get(:show, id: guid)
+        expect(response).not_to be_success
+      end
+    end
+
+    context 'with a logged in user' do
+      let(:user) { create(:user, :loa3) }
+
+      before do
+        expect_any_instance_of(described_class).to receive(:authenticate_token).at_least(:once).and_return(true)
+      end
+
+      it 'allows retrieval of the file' do
+        get(:show, id: guid)
+        expect(response).to be_success
+        expect(response.headers['Content-Type']).to eq('image/jpeg')
+        expect(response.headers).to have_key('Content-Disposition')
+      end
+
+      context 'with an invalid guid' do
+        it 'fails' do
+          get(:show, id: '../../../../../../../../../../../etc/passwd')
+          expect(response).not_to be_success
+          expect(response.status).to eq(404)
+        end
       end
     end
   end
