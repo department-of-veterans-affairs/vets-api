7cc43a9938876d9f3a9f99c00739bc29b5328f09
Ryan Baker <rlbaker@users.noreply.github.com>
2018-02-20 16:11:31 -0800

    VIC Profile Photo download endpoint (#1703)
    * Use send_data
    
    * Update profile_photo_attachment_uploader.rb
    
    * Change flow control
    
    * Update profile_photo_attachments_controller_spec.rb
    
    * Update profile_photo_attachments_controller.rb
    
    * Support async processing
    
    * Spec for invalid guid

--- app/serializers/profile_photo_attachment_serializer.rb
+++ app/serializers/profile_photo_attachment_serializer.rb
@@ -2,20 +2,4 @@
 
 class ProfilePhotoAttachmentSerializer < ActiveModel::Serializer
   attribute :guid
-  attribute :filename
-  attribute :path
-
-  def filename
-    parsed['filename'] unless @instance_options[:is_anonymous_upload]
-  end
-
-  def path
-    parsed['path'] unless @instance_options[:is_anonymous_upload]
-  end
-
-  private
-
-  def parsed
-    @parsed ||= JSON.parse(object.file_data)
-  end
 end
--- app/uploaders/profile_photo_attachment_uploader.rb
+++ app/uploaders/profile_photo_attachment_uploader.rb
@@ -22,8 +22,6 @@ class ProfilePhotoAttachmentUploader < CarrierWave::Uploader::Base
         Settings.vic.s3.bucket
       )
     end
-
-    self.aws_acl = 'public-read'
   end
 
   def extension_white_list
