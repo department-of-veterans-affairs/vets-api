7e68c6a5f4c4c754e499438779fa9033e6ffdbef
Ryan Baker <ryan.baker@adhocteam.us>
2018-02-22 11:22:46 -0800

    404 on missing record

--- app/controllers/v0/vic/profile_photo_attachments_controller.rb
+++ app/controllers/v0/vic/profile_photo_attachments_controller.rb
@@ -26,6 +26,8 @@ module V0
         raise Common::Exceptions::RecordNotFound, guid unless GUID_PATTERN.match(guid)
 
         form_attachment = ::VIC::ProfilePhotoAttachment.where(guid: guid).first
+        raise Common::Exceptions::RecordNotFound, guid unless form_attachment
+
         file = form_attachment.get_file
         send_data(file.read, filename: file.filename, type: file.content_type, disposition: :inline)
       end
--- spec/controllers/v0/vic/profile_photo_attachments_controller_spec.rb
+++ spec/controllers/v0/vic/profile_photo_attachments_controller_spec.rb
@@ -81,6 +81,14 @@ RSpec.describe V0::VIC::ProfilePhotoAttachmentsController, type: :controller do
           expect(response.status).to eq(404)
         end
       end
+
+      context 'with a guid that doesnt exist' do
+        it 'fails' do
+          get(:show, id: 'C56A4180-65AA-42EC-A945-5FD21DEC0538')
+          expect(response).not_to be_success
+          expect(response.status).to eq(404)
+        end
+      end
     end
   end
 end
