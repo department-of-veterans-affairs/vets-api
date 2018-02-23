8de138300c06357733448157069581aba24a8209
lihan <frankieteardrop@gmail.com>
2018-02-23 13:26:48 -0500

    handle mvi not found and emis not found (#1720)

--- lib/vic/service.rb
+++ lib/vic/service.rb
@@ -145,14 +145,25 @@ module VIC
     def add_user_data!(converted_form, user)
       profile_data = converted_form['profile_data']
       va_profile = user.va_profile
-      profile_data['sec_ID'] = va_profile.sec_id
-      profile_data['active_ICN'] = user.icn
-      profile_data['historical_ICN'] = MVI::Service.new.find_historical_icns(user)
+
+      if va_profile.present?
+        profile_data['sec_ID'] = va_profile.sec_id
+        profile_data['active_ICN'] = user.icn
+        profile_data['historical_ICN'] = MVI::Service.new.find_historical_icns(user)
+      end
 
       if user.edipi.present?
-        title38_status = user.veteran_status.title38_status
+        title38_status =
+          begin
+            user.veteran_status.title38_status
+          rescue EMISRedis::VeteranStatus::RecordNotFound
+            nil
+          end
+
         converted_form['title38_status'] = title38_status
       end
+
+      Common::HashHelpers.deep_compact(converted_form)
     end
 
     def wait_for_processed(form, start_time)
