da2e28fd0de26e105f6943d5076618083914b45a
hpjaj <hmlevine@gmail.com>
2018-02-22 17:56:05 -0700

    Custom response for email_address endpoint

---
+++ lib/evss/pciu/email_address_response.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'evss/response'
+
+module EVSS
+  module PCIU
+    class EmailAddressResponse < EVSS::Response
+      attribute :email_address, Hash
+
+      def initialize(status, response = nil)
+        email_address = response&.body&.dig('cnp_email_address')
+        super(status, email_address: email_address)
+      end
+    end
+  end
+end
