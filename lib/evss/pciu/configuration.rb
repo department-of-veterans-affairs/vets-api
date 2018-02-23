2224194e1655f38124040635d04f797ce861886c
hpjaj <hmlevine@gmail.com>
2018-02-22 17:55:17 -0700

    Configuration for email_address endpoint

---
+++ lib/evss/pciu/configuration.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module EVSS
+  module PCIU
+    class Configuration < EVSS::Configuration
+      self.read_timeout = Settings.evss.pciu.timeout || 30
+
+      def base_path
+        "#{Settings.evss.url}/wss-pciu-services-web/rest"
+      end
+
+      def service_name
+        'EVSS/PCIU'
+      end
+
+      def mock_enabled?
+        Settings.evss.mock_pciu || false
+      end
+    end
+  end
+end
