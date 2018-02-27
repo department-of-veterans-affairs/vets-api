346bf28e12ce5e2c26b773a207513de41dbb3f1a
Lihan Li <frankieteardrop@gmail.com>
2018-02-26 22:01:32 -0500

    dont need to make variable instance

--- app/workers/vic/attachment_upload_job.rb
+++ app/workers/vic/attachment_upload_job.rb
@@ -7,8 +7,8 @@ module VIC
     def perform(case_id, form)
       Raven.tags_context(backend_service: :vic)
       parsed_form = JSON.parse(form)
-      @vic_service = Service.new
-      @vic_service.send_files(case_id, parsed_form)
+      vic_service = Service.new
+      vic_service.send_files(case_id, parsed_form)
     end
   end
 end
