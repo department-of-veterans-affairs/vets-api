9d7a66245b5e5fd57f6b4ad3de56db84197ec779
Ryan Baker <rlbaker@users.noreply.github.com>
2018-02-28 15:06:20 -0800

    Always use COMP2 code when querying MVI (#1730)
    * Always use MVI COMP2 code
    
    * Adjust vic service specs
    
    * Add new specs
    
    * Add comment for new field
    
    * Remove comment
    
    * Add lint exception for the time being
    
    * Lint
    
    * Use MATCH_EVERYTHING and set correct attributes

--- lib/mvi/models/mvi_profile.rb
+++ lib/mvi/models/mvi_profile.rb
@@ -34,6 +34,7 @@ module MVI
       attribute :participant_id, String
       attribute :birls_id, String
       attribute :sec_id, String
+      attribute :historical_icns, Array[String]
 
       def mhv_correlation_id
         @mhv_ids&.first
