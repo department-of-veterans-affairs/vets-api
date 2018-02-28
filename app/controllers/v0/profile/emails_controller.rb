c7b6407bc6eb67c1389e1a34de50bff6dae7ff8a
hpjaj <hmlevine@gmail.com>
2018-02-28 16:14:23 -0700

    Removes yard docs in favor of referencing swagger docs

--- app/controllers/v0/profile/emails_controller.rb
+++ app/controllers/v0/profile/emails_controller.rb
@@ -5,22 +5,6 @@ module V0
     class EmailsController < ApplicationController
       before_action { authorize :evss, :access? }
 
-      # Fetches the email address, and its effective datetime, for the current user
-      #
-      # @return [Response] Sample response.body:
-      #   {
-      #     "data" =>
-      #       {
-      #         "id" => "",
-      #         "type" => "evss_pciu_email_address_responses",
-      #         "attributes" =>
-      #           {
-      #             "email" => "test2@test1.net",
-      #             "effective_at" => "2018-02-27T14:41:32.283Z"
-      #           }
-      #       }
-      #   }
-      #
       def show
         response = service.get_email_address
 
