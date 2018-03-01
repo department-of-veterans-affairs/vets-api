b5f8f08f2d9d42dceba3e544bcd4c9c0cc71b08c
hpjaj <hmlevine@gmail.com>
2018-03-01 09:13:14 -0700

    Spec coverage for 400, 403, and 500 responses

--- spec/request/email_request_spec.rb
+++ spec/request/email_request_spec.rb
@@ -15,12 +15,46 @@ RSpec.describe 'email', type: :request do
   end
 
   describe 'GET /v0/profile/email' do
-    it 'returns a status of 200' do
-      VCR.use_cassette('evss/pciu/email') do
-        get '/v0/profile/email', nil, auth_header
+    context 'with a 200 response' do
+      it 'should match the email schema' do
+        VCR.use_cassette('evss/pciu/email') do
+          get '/v0/profile/email', nil, auth_header
 
-        expect(response).to have_http_status(:ok)
-        expect(response).to match_response_schema('email_address_response')
+          expect(response).to have_http_status(:ok)
+          expect(response).to match_response_schema('email_address_response')
+        end
+      end
+    end
+
+    context 'with a 400 response' do
+      it 'should match the errors schema' do
+        VCR.use_cassette('evss/pciu/email_status_400') do
+          get '/v0/profile/email', nil, auth_header
+
+          expect(response).to have_http_status(:bad_request)
+          expect(response).to match_response_schema('errors')
+        end
+      end
+    end
+
+    context 'with a 403 response' do
+      it 'should return a forbidden response' do
+        VCR.use_cassette('evss/pciu/email_status_403') do
+          get '/v0/profile/email', nil, auth_header
+
+          expect(response).to have_http_status(:forbidden)
+        end
+      end
+    end
+
+    context 'with a 500 response' do
+      it 'should match the errors schema' do
+        VCR.use_cassette('evss/pciu/email_status_500') do
+          get '/v0/profile/email', nil, auth_header
+
+          expect(response).to have_http_status(:bad_gateway)
+          expect(response).to match_response_schema('errors')
+        end
       end
     end
   end
