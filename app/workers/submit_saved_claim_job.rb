2a9b9e8646bdd69b6a9614c3c2dceb7f8b3984f5
Lihan Li <frankieteardrop@gmail.com>
2018-03-01 18:34:00 -0500

    stamp again with fdc reviewed

--- app/workers/submit_saved_claim_job.rb
+++ app/workers/submit_saved_claim_job.rb
@@ -6,7 +6,13 @@ class SubmitSavedClaimJob
   def perform(saved_claim_id)
     claim = SavedClaim.find(saved_claim_id)
     pdf_path = claim.to_pdf
-    stamped_path = PensionBurial::DatestampPdf.new(pdf_path).run(text: 'VETS.GOV', x: 5, y: 5)
+    stamped_path1 = PensionBurial::DatestampPdf.new(pdf_path).run(text: 'VETS.GOV', x: 5, y: 5)
+    stamped_path2 = PensionBurial::DatestampPdf.new(stamped_path1).run(
+      text: 'FDC Reviewed - Vets.gov Submission',
+      x: 429,
+      y: 770,
+      text_only: true
+    )
     binding.pry; fail
   end
 end
