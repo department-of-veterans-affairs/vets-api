a202cdc815ce34339fb409554500c7c1ebbaf29c
Lihan Li <frankieteardrop@gmail.com>
2018-03-07 17:40:34 -0500

    working on metadata

--- app/workers/submit_saved_claim_job.rb
+++ app/workers/submit_saved_claim_job.rb
@@ -4,15 +4,17 @@ class SubmitSavedClaimJob
   include Sidekiq::Worker
 
   def perform(saved_claim_id)
-    claim = SavedClaim.find(saved_claim_id)
-    submission = {
-      'document' => process_record(claim)
+    @claim = SavedClaim.find(saved_claim_id)
+    @submission = {
+      'document' => process_record(@claim)
     }
 
-    claim.persistent_attachments.each_with_index do |record, i|
+    @claim.persistent_attachments.each_with_index do |record, i|
       key = "attachment#{i + 1}"
-      submission[key] = process_record(record)
+      @submission[key] = process_record(record)
     end
+
+    metadata = generate_metadata
     binding.pry; fail
   end
 
@@ -28,5 +30,31 @@ class SubmitSavedClaimJob
   end
 
   def generate_metadata
+    form = @claim.parsed_form
+    binding.pry; fail
+
+    metadata = {
+      'veteranFirstName' => form['veteranFirstName'],
+      'veteranLastName' => form['veteranLastName'],
+      'fileNumber' => form['vaFileNumber'],
+      'receiveDt' => @claim.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+      'zipCode' => form['claimantAddress'].try(:[], 'postalCode'),
+      'uuid' => @claim.guid,
+      'source' => 'vets.gov',
+      'hashV' => Digest::SHA256.file(@submission['document']).hexdigest,
+      'numberAttachments' => @claim.persistent_attachments.count,
+      'docType' => nil,
+      'numberPages' => PDF::Reader.new(@submission['document']).pages.size
+    }
+
+    attachments = claim.persistent_attachments
+    attachments.each_with_index do |_attachment, index|
+      n = index + 1
+
+      metadata["ahash#{n}"] = nil # TODO: SHA-256 hash of attachment
+      metadata["numberPages#{n}"] = nil # TODO: number of pages for each attachment
+    end
+
+    metadata
   end
 end
