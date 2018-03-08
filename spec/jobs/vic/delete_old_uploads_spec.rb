7d0911237a5ce667cf1f3f5f3a08207d43cff922
Ryan Baker <ryan.baker@adhocteam.us>
2018-03-07 17:45:22 -0800

    Stub spec file

---
+++ spec/jobs/vic/delete_old_uploads_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe VIC::DeleteOldApplications do
+  before do
+  end
+
+  describe '#perform' do
+    it 'checks for uploads to keep from in progress forms' do
+    end
+  end
+end
