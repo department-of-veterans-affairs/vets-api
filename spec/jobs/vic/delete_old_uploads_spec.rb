c7c37597a4236c402ad59e7da01e43d5a4011c51
Ryan Baker <ryan.baker@adhocteam.us>
2018-03-08 09:32:40 -0800

    Create in-progress-forms for specs

--- spec/jobs/vic/delete_old_uploads_spec.rb
+++ spec/jobs/vic/delete_old_uploads_spec.rb
@@ -7,6 +7,19 @@ RSpec.describe VIC::DeleteOldApplications do
   end
 
   describe '#perform' do
+    let(:photo_guid) { SecureRandom.uuid }
+    let(:doc_guid) { SecureRandom.uuid }
+
+    before do
+      FactoryBot.create(
+        :in_progress_form,
+        form_id: 'vic',
+        form_data: {
+          'dd214' => { 'confirmationCode' => doc_guid },
+          'photo' => { 'confirmationCode' => photo_guid }
+        }.to_json
+      )
+    end
     it 'checks for uploads to keep from in progress forms' do
     end
   end
