2b9ca7af514ea25c40b91c30d21bf10bb905ef78
omgitsbillryan <bill.ryan@adhocteam.us>
2018-03-09 13:52:48 -0500

    lint

--- spec/routing/address_routing.rb
+++ spec/routing/address_routing.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'routes for Address', type: :routing do
-
   before(:all) { @cached_enabled_val = Settings.evss.reference_data_service.enabled }
   after(:all) do
     # leave the routes in the expected state for future specs
