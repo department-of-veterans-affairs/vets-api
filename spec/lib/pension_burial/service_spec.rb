13801bb52394dfabbba0854b51668cb30988019a
Lihan Li <frankieteardrop@gmail.com>
2018-03-09 21:38:32 -0500

    pension service spec

--- spec/lib/pension_burial/service_spec.rb
+++ spec/lib/pension_burial/service_spec.rb
@@ -4,19 +4,26 @@ require 'rails_helper'
 
 RSpec.describe PensionBurial::Service do
   describe '#upload' do
-    let(:file_path) { Rails.root.join('spec', 'fixtures', 'files', 'doctors-note.pdf') }
-
     it 'should upload a file' do
-      VCR.use_cassette('pension_burial/upload', match_requests_on: [:body]) do
+      VCR.use_cassette('pension_burial/upload', VCR::MATCH_EVERYTHING) do
         response = described_class.new.upload(
-          { form_id: '99-9999EZ', code: 'V-TESTTEST', guid: '123', original_filename: 'doctors-note.pdf' },
-          StringIO.new(File.read(file_path)),
-          'application/pdf'
+          {
+            "metadata" => "{\"veteranFirstName\":\"Test\",\"veteranLastName\":\"User\",\"fileNumber\":\"111223333\",\"receiveDt\":\"2018-03-10 02:13:33\",\"zipCode\":\"90210\",\"uuid\":\"07029d0e-60b5-4bc4-8606-ef3504f2835f\",\"source\":\"CSRA-V\",\"hashV\":\"4c60b247a2a4fe495819c5e6b042409fc6d9cad535ffa59599d88b203547ddb3\",\"numberAttachments\":1,\"docType\":\"21P-530\",\"numberPages\":3,\"ahash1\":\"b51f2aec3dbefff12026aed30860b3b2c73580e60d2e56ce7ce0c5699af575fe\",\"numberPages1\":1}",
+            document: Faraday::UploadIO.new(
+              'spec/fixtures/pension/form.pdf',
+              Mime[:pdf].to_s
+            ),
+            attachment1: Faraday::UploadIO.new(
+              'spec/fixtures/pension/attachment.pdf',
+              Mime[:pdf].to_s
+            )
+          }
         )
         body = response.body
 
-        expect(body['fileSize']).to eq(10_548)
-        expect(body['metaSize']).to eq(95)
+        expect(body).to eq(
+          {"fileSize"=>1759933, "metaSize"=>419, "md5"=>"1I2+7z80I4jgvKenhclh5w==", "md5hex"=>"d48dbeef3f342388e0bca7a785c961e7"}
+        )
         expect(response.status).to eq(200)
       end
     end
