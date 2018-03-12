bba229b9dc509031f6a36b499ca48466132d5c6a
Lihan Li <frankieteardrop@gmail.com>
2018-03-12 19:39:25 -0400

    fix header matcher

--- spec/lib/pension_burial/service_spec.rb
+++ spec/lib/pension_burial/service_spec.rb
@@ -5,7 +5,15 @@ require 'rails_helper'
 RSpec.describe PensionBurial::Service do
   describe '#upload' do
     it 'should upload a file' do
-      VCR.use_cassette('pension_burial/upload', VCR::MATCH_EVERYTHING) do
+      header_matcher = lambda do |r1, r2|
+        [r1, r2].each { |r| r.headers.delete('Content-Length') }
+        expect(r1.headers).to eq(r2.headers)
+      end
+
+      VCR.use_cassette(
+        'pension_burial/upload',
+        match_requests_on: [header_matcher, :body, :method, :uri]
+      ) do
         response = described_class.new.upload(
           metadata: get_fixture('pension/metadata').to_json,
           document: Faraday::UploadIO.new(
