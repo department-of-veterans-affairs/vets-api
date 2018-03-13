6be33aaea71952e2788b314298feb7c9788099cd
Ryan Baker <rlbaker@users.noreply.github.com>
2018-03-13 15:07:15 -0700

    Update profile_photo_attachment_spec.rb

--- spec/models/vic/profile_photo_attachment_spec.rb
+++ spec/models/vic/profile_photo_attachment_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe VIC::ProfilePhotoAttachment, type: :model do
   describe '#set_file_data!' do
-    it 'should store the filename and path' do
+    it 'should store the filename' do
       attachment = create(:profile_photo_attachment,
                           file_path: 'spec/fixtures/files/va.gif',
                           file_type: 'image/gif')
