af00dd4df5c945b23c684d3dbbad458cbb3dcf22
hpjaj <hmlevine@gmail.com>
2018-03-16 16:38:33 -0600

    rubicon adjustments

--- app/swagger/schemas/service_history.rb
+++ app/swagger/schemas/service_history.rb
@@ -15,7 +15,7 @@ module Swagger
             property :service_history do
               key :type, :array
               items do
-                key :required, [:branch_of_service, :begin_date]
+                key :required, %i[branch_of_service begin_date]
                 property :branch_of_service, type: :string, example: 'Air Force'
                 property :begin_date, type: :string, example: '2007-04-01'
                 property :end_date, type: :string, example: '2016-06-01'
