11936093510c07e44827cbf26a39b031f733892d
Bill Ryan <bill.ryan@adhocteam.us>
2018-03-19 16:40:35 -0400

    Respond 503 in correspondence with EVSS GIBS scheduled uptime (#1769)
    * first pass
    
    * new API for GIBS availability
    
    * oops
    
    * lint, specs, slight name change
    
    * swagger
    
    * more swagger
    
    * oops again
    
    * use proper timezone that accounts for DST
    
    * refactor, more specs
    
    * lint
    
    * code review comment
    
    * add backend_status controller & model
    
    * remove availability from gibs, specs, bug fixes, swagger
    
    * fix spec
    
    * lint

--- lib/evss/gi_bill_status/service.rb
+++ lib/evss/gi_bill_status/service.rb
@@ -7,6 +7,31 @@ module EVSS
     class Service < EVSS::Service
       configuration EVSS::GiBillStatus::Configuration
 
+      OPERATING_ZONE = 'Eastern Time (US & Canada)'
+      OPERATING_HOURS = {
+        start: 6,
+        end: 22,
+        saturday_end: 19
+      }.freeze
+
+      def self.within_scheduled_uptime?
+        current_time = Time.now.in_time_zone(OPERATING_ZONE)
+        if current_time.saturday?
+          (OPERATING_HOURS[:start]...OPERATING_HOURS[:saturday_end]).cover?(current_time.hour)
+        else
+          (OPERATING_HOURS[:start]...OPERATING_HOURS[:end]).cover?(current_time.hour)
+        end
+      end
+
+      def self.retry_after_time
+        current_time = Time.now.in_time_zone(OPERATING_ZONE)
+        tz = ActiveSupport::TimeZone.new(OPERATING_ZONE)
+        service_start_time = tz.parse(tz.today.to_s + ' 0' + OPERATING_HOURS[:start].to_s + ':00:00')
+
+        return service_start_time.httpdate if current_time.hour < OPERATING_HOURS[:start]
+        service_start_time.tomorrow.httpdate
+      end
+
       def get_gi_bill_status
         raw_response = perform(:get, '')
         EVSS::GiBillStatus::GiBillStatusResponse.new(raw_response.status, raw_response)
