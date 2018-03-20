eb90de2d18635f8cd210c4ba380bf7258ae89791
Alastair Dawson <alastair.j.dawson@gmail.com>
2018-03-20 15:56:14 -0700

    adding a check to the betamocks initializer that the services_config is interpolated correctly

--- config/initializers/betamocks.rb
+++ config/initializers/betamocks.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+unless Rails.env.production?
+  begin
+    ERB.new(File.read(Settings.betamocks.services_config)).result
+  rescue
+    raise ArgumentError, 'betamocks services_config error, check that vars from settings.yml are interpolated correctly'
+  end
+end
+
 Betamocks.configure do |config|
   config.enabled = Settings.betamocks.enabled
   config.cache_dir = Settings.betamocks.cache_dir
