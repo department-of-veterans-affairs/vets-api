cfab86794cfa78d03657f8f2c36f9b678bcec309
hpjaj <hmlevine@gmail.com>
2018-03-21 14:49:23 -0600

    Consolidates schema into swagger request
    The schemas/* file is not needed at this point, as DRY’ing things up is not necessary now.

--- app/swagger/requests/profile.rb
+++ app/swagger/requests/profile.rb
@@ -83,7 +83,23 @@ module Swagger
           response 200 do
             key :description, 'Response is OK'
             schema do
-              key :'$ref', :ServiceHistory
+              key :required, [:data]
+
+              property :data, type: :object do
+                key :required, [:attributes]
+                property :attributes, type: :object do
+                  key :required, [:service_history]
+                  property :service_history do
+                    key :type, :array
+                    items do
+                      key :required, %i[branch_of_service begin_date]
+                      property :branch_of_service, type: :string, example: 'Air Force'
+                      property :begin_date, type: :string, format: :date, example: '2007-04-01'
+                      property :end_date, type: :string, format: :date, example: '2016-06-01'
+                    end
+                  end
+                end
+              end
             end
           end
         end
