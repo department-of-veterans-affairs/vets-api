689ff43777994e8328886ffd5057c7ed626b5fad
hpjaj <hmlevine@gmail.com>
2018-03-21 08:24:07 -0600

    Adds yard docs

--- app/controllers/v0/profile/service_histories_controller.rb
+++ app/controllers/v0/profile/service_histories_controller.rb
@@ -5,6 +5,26 @@ module V0
     class ServiceHistoriesController < ApplicationController
       before_action { authorize :evss, :access? }
 
+      # Fetches the service history for the current user.  This is
+      # an array of select military service episode data.
+      #
+      # @return [Response] Sample response.body:
+      #   {
+      #     "data" => {
+      #       "id"         => "",
+      #       "type"       => "arrays",
+      #       "attributes" => {
+      #         "service_history" => [
+      #           {
+      #             "branch_of_service" => "Air Force",
+      #             "begin_date"        => "2007-04-01",
+      #             "end_date"          => "2016-06-01"
+      #           }
+      #         ]
+      #       }
+      #     }
+      #   }
+      #
       def show
         response = EMISRedis::MilitaryInformation.for_user(@current_user).service_history
 
