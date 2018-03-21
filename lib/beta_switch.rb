3c071b1c51ef2e4458f73049f338091b6312384e
Lihan Li <frankieteardrop@gmail.com>
2018-03-20 22:20:45 -0400

    lint

--- lib/beta_switch.rb
+++ lib/beta_switch.rb
@@ -2,6 +2,6 @@
 
 module BetaSwitch
   def beta_enabled?(uuid, feature_name)
-    BetaRegistration.where(user_uuid: uuid, feature: feature_name).count > 0
+    BetaRegistration.where(user_uuid: uuid, feature: feature_name).count.positive?
   end
 end
