1e9e62913688e8b77155b7481111e8b69f95dbaa
Lihan Li <frankieteardrop@gmail.com>
2018-03-20 21:53:02 -0400

    access common client spec

--- app/policies/evss_policy.rb
+++ app/policies/evss_policy.rb
@@ -4,4 +4,8 @@ EVSSPolicy = Struct.new(:user, :evss) do
   def access?
     user.edipi.present? && user.ssn.present? && user.participant_id.present?
   end
+
+  def access_common_client?
+    user.beta_enabled?(user.uuid, EVSSClaimService::EVSS_COMMON_CLIENT_KEY)
+  end
 end
--- spec/policies/evss_policy_spec.rb
+++ spec/policies/evss_policy_spec.rb
@@ -4,11 +4,26 @@ require 'rails_helper'
 
 describe EVSSPolicy do
   subject { described_class }
+  let(:user) { build(:user, :loa3) }
+
+  permissions :access_common_client? do
+    context 'with a user that has access' do
+      it 'should return true' do
+        expect(user).to receive(:beta_enabled?).with(user.uuid, 'evss_common_client').and_return(true)
+
+        expect(subject).to permit(user, :evss)
+      end
+    end
+
+    context 'when a user doesnt have access' do
+      it 'should return false' do
+        expect(subject).to_not permit(user, :evss)
+      end
+    end
+  end
 
   permissions :access? do
     context 'with a user who has the required evss attributes' do
-      let(:user) { build(:user, :loa3) }
-
       it 'grants access' do
         expect(subject).to permit(user, :evss)
       end
