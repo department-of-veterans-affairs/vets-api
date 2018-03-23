34ff58eb43e97507c57bb1689208bd02c45ca292
Ryan Baker <ryan.baker@adhocteam.us>
2018-03-23 11:11:47 -0700

    First attempt at how the query should be

--- app/workers/pension_burial_notifications.rb
+++ app/workers/pension_burial_notifications.rb
@@ -4,6 +4,12 @@ class PensionBurialNotifications
   include Sidekiq::Worker
 
   def perform
+    service = PensionBurial::Service.new
+    guids = SavedClaim.where(
+      form_id: ['21P-527EZ', '21P-530'],
+      claim_status: [nil, 'In Process']
+    ).find_each.map(&:guid)
+
     binding.pry
   end
 end
