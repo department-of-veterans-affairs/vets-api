91e58fad61e1162decdd39cfbb0616f9d9238553
Lihan Li <frankieteardrop@gmail.com>
2018-03-27 16:51:30 -0400

    tag jogs

--- app/workers/evss/create_user_account_job.rb
+++ app/workers/evss/create_user_account_job.rb
@@ -7,6 +7,7 @@ module EVSS
     include Sidekiq::Worker
 
     def perform(headers)
+      Sentry::TagRainbows.tag
       client = EVSS::CommonService.new(headers)
       client.create_user_account
     end
--- app/workers/process_file_job.rb
+++ app/workers/process_file_job.rb
@@ -4,6 +4,7 @@ class ProcessFileJob
   include Sidekiq::Worker
 
   def perform(processing_class, store_dir, filename)
+    Sentry::TagRainbows.tag
     uploader = processing_class.constantize.new(store_dir, filename)
     uploader.retrieve_from_store!(filename)
     old_file = uploader.file
