5a6e5233082afea7e0cc264d89b4769efeb62b45
Ryan Baker <ryan.baker@adhocteam.us>
2018-03-28 13:10:54 -0700

    Add example statuses to worker

--- app/workers/pension_burial_notifications.rb
+++ app/workers/pension_burial_notifications.rb
@@ -4,13 +4,14 @@ class PensionBurialNotifications
   include Sidekiq::Worker
 
   ENABLED_DATE = '2018-01-01'.to_date # TODO: Update once form is live
+  QUERY_STATUSES = [nil, 'in process'].freeze
   FORM_IDS = ['21P-527EZ', '21P-530'].freeze
 
   def perform
     claims = {}
     SavedClaim.where(
       form_id: FORM_IDS,
-      status: [nil, 'in process'],
+      status: QUERY_STATUSES,
       created_at: ENABLED_DATE.beginning_of_day..Time.zone.now
     ).find_each { |c| claims[c.guid] = c }
 
@@ -26,6 +27,13 @@ class PensionBurialNotifications
 
         if new_status != old_status
           # Status has changed, perform follow up actions
+          #
+          # case new_status
+          # when 'received' # received by ICMHS
+          # when 'in process' # being processed by ICMHS
+          # when 'success' # received by DMHS
+          # when 'error' # check 'errorMessage' field of status object
+          # end
 
           claim.status = new_status
           claim.save!
