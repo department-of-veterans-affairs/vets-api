0e2bb8d950d51b8cd2a0497c47c0d9cec6ba196d
hpjaj <hmlevine@gmail.com>
2018-04-03 09:50:42 -0600

    New POST email endpoint

--- spec/request/email_request_spec.rb
+++ spec/request/email_request_spec.rb
@@ -58,4 +58,111 @@ RSpec.describe 'email', type: :request do
       end
     end
   end
+
+  describe 'POST /v0/profile/email' do
+    let(:email_address) { build(:email_address) }
+
+    context 'with a 200 response' do
+      it 'should match the email address schema', :aggregate_failures do
+        VCR.use_cassette('evss/pciu/post_email_address') do
+          post(
+            '/v0/profile/email',
+            email_address.to_json,
+            auth_header.update(
+              'Content-Type' => 'application/json', 'Accept' => 'application/json'
+            )
+          )
+
+          expect(response).to have_http_status(:ok)
+          expect(response).to match_response_schema('email_address_response')
+        end
+      end
+    end
+
+    context 'with a missing email' do
+      it 'should match the errors schema', :aggregate_failures do
+        email_address = build :email_address, email: ''
+
+        post(
+          '/v0/profile/email',
+          email_address.to_json,
+          auth_header.update(
+            'Content-Type' => 'application/json', 'Accept' => 'application/json'
+          )
+        )
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response).to match_response_schema('errors')
+        expect(errors_for(response)).to include "email - can't be blank"
+      end
+    end
+
+    context 'with an invalid email' do
+      it 'should match the errors schema', :aggregate_failures do
+        email_address = build :email_address, email: 'johngmail.com'
+
+        post(
+          '/v0/profile/email',
+          email_address.to_json,
+          auth_header.update(
+            'Content-Type' => 'application/json', 'Accept' => 'application/json'
+          )
+        )
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response).to match_response_schema('errors')
+        expect(errors_for(response)).to include "email - is invalid"
+      end
+    end
+
+    context 'with a 400 response' do
+      it 'should match the errors schema', :aggregate_failures do
+        VCR.use_cassette('evss/pciu/post_email_address_status_400') do
+          post(
+            '/v0/profile/email',
+            email_address.to_json,
+            auth_header.update(
+              'Content-Type' => 'application/json', 'Accept' => 'application/json'
+            )
+          )
+
+          expect(response).to have_http_status(:bad_request)
+          expect(response).to match_response_schema('errors')
+        end
+      end
+    end
+
+    context 'with a 403 response' do
+      it 'should return a forbidden response' do
+        VCR.use_cassette('evss/pciu/post_email_address_status_403') do
+          post(
+            '/v0/profile/email',
+            email_address.to_json,
+            auth_header.update(
+              'Content-Type' => 'application/json', 'Accept' => 'application/json'
+            )
+          )
+
+          expect(response).to have_http_status(:forbidden)
+        end
+      end
+    end
+
+    context 'with a 500 response' do
+      it 'should match the errors schema', :aggregate_failures do
+        VCR.use_cassette('evss/pciu/post_email_address_status_500') do
+          post(
+            '/v0/profile/email',
+            email_address.to_json,
+            auth_header.update(
+              'Content-Type' => 'application/json', 'Accept' => 'application/json'
+            )
+          )
+
+          expect(response).to have_http_status(:bad_gateway)
+          expect(response).to match_response_schema('errors')
+        end
+      end
+    end
+  end
 end
