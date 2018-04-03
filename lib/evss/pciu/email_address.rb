2b61249a9d367ef24d307963abe215106f20d3e3
hpjaj <hmlevine@gmail.com>
2018-04-03 10:54:29 -0600

    Documents source of common regex
    This one ensures that:
    
    - there is an `@` symbol
    - there is something before the @ symbol
    - there is a `.` after the @ symbol
    
    I have found that these are the most common typos user’s make.

--- lib/evss/pciu/email_address.rb
+++ lib/evss/pciu/email_address.rb
@@ -3,6 +3,10 @@
 module EVSS
   module PCIU
     class EmailAddress < BaseModel
+      # This regex comes from the `validates_format_of` Rails docs
+      #
+      # @see https://apidock.com/rails/ActiveModel/Validations/HelperMethods/validates_format_of
+      #
       VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
       attribute :email, String
