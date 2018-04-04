8d58342c9dd840ef0e73102ef044760bf4f212e6
Johnny Holton <johnny@holton.co>
2018-04-04 14:38:36 -0400

    claims_service and documents_service:  Have Sentry ignore EVSS errors.  Log as level :warn

--- lib/evss/documents_service.rb
+++ lib/evss/documents_service.rb
@@ -1,25 +1,30 @@
 # frozen_string_literal: true
 
 require 'evss/base_service'
+require 'sentry/rescue_evss_errors.rb'
 
 module EVSS
   class DocumentsService < BaseService
+    include Sentry::RescueEVSSErrors
+
     API_VERSION = Settings.evss.versions.documents
     BASE_URL = "#{Settings.evss.url}/wss-document-services-web-#{API_VERSION}/rest/"
     # this service is only used from an async worker so long timeout is acceptable here
     DEFAULT_TIMEOUT = 180 # seconds
 
     def upload(file_body, document_data)
-      headers = { 'Content-Type' => 'application/octet-stream' }
-      post 'queuedDocumentUploadService/ajaxUploadFile', file_body, headers do |req|
-        req.params['systemName'] = SYSTEM_NAME
-        req.params['docType'] = document_data.document_type
-        req.params['docTypeDescription'] = document_data.description
-        req.params['claimId'] = document_data.evss_claim_id
-        # In theory one document can correspond to multiple tracked items
-        # To do that, add multiple query parameters
-        req.params['trackedItemIds'] = document_data.tracked_item_id
-        req.params['qqfile'] = document_data.file_name
+      rescue_evss_errors do
+        headers = { 'Content-Type' => 'application/octet-stream' }
+        post 'queuedDocumentUploadService/ajaxUploadFile', file_body, headers do |req|
+          req.params['systemName'] = SYSTEM_NAME
+          req.params['docType'] = document_data.document_type
+          req.params['docTypeDescription'] = document_data.description
+          req.params['claimId'] = document_data.evss_claim_id
+          # In theory one document can correspond to multiple tracked items
+          # To do that, add multiple query parameters
+          req.params['trackedItemIds'] = document_data.tracked_item_id
+          req.params['qqfile'] = document_data.file_name
+        end
       end
     end
 
---
+++ lib/sentry/rescue_evss_errors.rb
@@ -0,0 +1,11 @@
+module Sentry
+  module RescueEVSSErrors
+    private
+    def rescue_evss_errors
+      yield
+      rescue EVSS::ErrorMiddleware::EVSSError => e
+        log_message_to_sentry(e, :warn)
+        raise Sentry::IgnoredError, e.message
+    end
+  end
+end
