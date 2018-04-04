28b4d051134dda90d13946d68f8e63a84f3ef776
Johnny Holton <johnny@holton.co>
2018-04-04 14:58:22 -0400

    formatting clean up

--- lib/sentry/rescue_evss_errors.rb
+++ lib/sentry/rescue_evss_errors.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 module Sentry
   module RescueEVSSErrors
     private
+
     def rescue_evss_errors
       yield
-      rescue EVSS::ErrorMiddleware::EVSSError => e
-        log_message_to_sentry(e, :warn)
-        raise Sentry::IgnoredError, e.message
+    rescue EVSS::ErrorMiddleware::EVSSError => e
+      log_message_to_sentry(e, :warn)
+      raise Sentry::IgnoredError, e.message
     end
   end
 end
