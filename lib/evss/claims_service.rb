928a32a9b2cf9d190491f84c3a8af8d2940b9423
Johnny Holton <johnny@holton.co>
2018-04-04 14:38:36 -0400

    claims_service and documents_service:  Have Sentry ignore EVSS errors.  Log as level :warn

--- lib/evss/claims_service.rb
+++ lib/evss/claims_service.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 require 'evss/base_service'
+require 'sentry/rescue_evss_errors.rb'
 
 module EVSS
   class ClaimsService < BaseService
+    include Sentry::RescueEVSSErrors
+
     API_VERSION = Settings.evss.versions.claims
     BASE_URL = "#{Settings.evss.url}/wss-claims-services-web-#{API_VERSION}/rest"
     DEFAULT_TIMEOUT = 120 # in seconds
@@ -14,18 +17,24 @@ module EVSS
     end
 
     def all_claims
-      get 'vbaClaimStatusService/getClaims'
+      rescue_evss_errors do
+        get 'vbaClaimStatusService/getClaims'
+      end
     end
 
     def find_claim_by_id(claim_id)
-      post 'vbaClaimStatusService/getClaimDetailById', { id: claim_id }.to_json
+      rescue_evss_errors do
+        post 'vbaClaimStatusService/getClaimDetailById', { id: claim_id }.to_json
+      end
     end
 
     def request_decision(claim_id)
-      post 'vbaClaimStatusService/set5103Waiver', {
-        claimId: claim_id,
-        systemName: SYSTEM_NAME
-      }.to_json
+      rescue_evss_errors do
+        post 'vbaClaimStatusService/set5103Waiver', {
+          claimId: claim_id,
+          systemName: SYSTEM_NAME
+        }.to_json
+      end
     end
 
     def self.breakers_service
