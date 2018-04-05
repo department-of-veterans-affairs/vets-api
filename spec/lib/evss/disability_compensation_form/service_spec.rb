15ad9273dd8a35c38e8d47dca4fa2f9222d2a45d
egarreau <ergarreau@gmail.com>
2018-04-05 16:33:18 -0500

    add test

--- spec/lib/evss/disability_compensation_form/service_spec.rb
+++ spec/lib/evss/disability_compensation_form/service_spec.rb
@@ -38,137 +38,136 @@ describe EVSS::DisabilityCompensationForm::Service do
   end
 
   describe '#submit_form' do
-    context 'with valid input' do
-      let(:valid_form_content) do
-        {
-          "form526": {
-            "veteran": {
-              "emailAddress": 'string',
-              "alternateEmailAddress": 'string',
-              "mailingAddress": {
-                "addressLine1": 'string',
-                "addressLine2": 'string',
-                "addressLine3": 'string',
-                "city": 'string',
-                "state": 'IL',
-                "zipFirstFive": '11111',
-                "zipLastFour": '1111',
-                "country": 'string',
-                "militaryStateCode": 'AA',
-                "militaryPostOfficeTypeCode": 'APO',
-                "type": 'DOMESTIC'
-              },
-              "forwardingAddress": {
-                "addressLine1": 'string',
-                "addressLine2": 'string',
-                "addressLine3": 'string',
-                "city": 'string',
-                "state": 'IL',
-                "zipFirstFive": '11111',
-                "zipLastFour": '1111',
-                "country": 'string',
-                "militaryStateCode": 'AA',
-                "militaryPostOfficeTypeCode": 'APO',
-                "type": 'DOMESTIC',
-                "effectiveDate": '2018-03-29T18:50:03.014Z'
-              },
-              "primaryPhone": {
-                "areaCode": '202',
-                "phoneNumber": '4561111'
-              },
-              "homelessness": {
-                "hasPointOfContact": false
-              },
-              "serviceNumber": 'string'
+    let(:valid_form_content) do
+      {
+        "form526": {
+          "veteran": {
+            "emailAddress": 'string',
+            "alternateEmailAddress": 'string',
+            "mailingAddress": {
+              "addressLine1": 'string',
+              "addressLine2": 'string',
+              "addressLine3": 'string',
+              "city": 'string',
+              "state": 'IL',
+              "zipFirstFive": '11111',
+              "zipLastFour": '1111',
+              "country": 'string',
+              "militaryStateCode": 'AA',
+              "militaryPostOfficeTypeCode": 'APO',
+              "type": 'DOMESTIC'
             },
-            "attachments": [],
-            "militaryPayments": {
-              "payments": [],
-              "receiveCompensationInLieuOfRetired": false,
-              "receivingInactiveDutyTrainingPay": false,
-              "waveBenifitsToRecInactDutyTraiPay": false
+            "forwardingAddress": {
+              "addressLine1": 'string',
+              "addressLine2": 'string',
+              "addressLine3": 'string',
+              "city": 'string',
+              "state": 'IL',
+              "zipFirstFive": '11111',
+              "zipLastFour": '1111',
+              "country": 'string',
+              "militaryStateCode": 'AA',
+              "militaryPostOfficeTypeCode": 'APO',
+              "type": 'DOMESTIC',
+              "effectiveDate": '2018-03-29T18:50:03.014Z'
             },
-            "directDeposit": {
-              "accountType": 'CHECKING',
-              "accountNumber": '1234',
-              "bankName": 'string',
-              "routingNumber": '123456789'
+            "primaryPhone": {
+              "areaCode": '202',
+              "phoneNumber": '4561111'
             },
-            "serviceInformation": {
-              "servicePeriods": [
-                {
-                  "serviceBranch": 'string',
-                  "activeDutyBeginDate": '2018-03-29T18:50:03.015Z',
-                  "activeDutyEndDate": '2018-03-29T18:50:03.015Z'
-                }
-              ],
-              "reservesNationalGuardService": {
-                "title10Activation": {
-                  "title10ActivationDate": '2018-03-29T18:50:03.015Z',
-                  "anticipatedSeparationDate": '2018-03-29T18:50:03.015Z'
-                },
-                "obligationTermOfServiceFromDate": '2018-03-29T18:50:03.015Z',
-                "obligationTermOfServiceToDate": '2018-03-29T18:50:03.015Z',
-                "unitName": 'string',
-                "unitPhone": {
-                  "areaCode": '202',
-                  "phoneNumber": '4561111'
-                }
-              },
-              "servedInCombatZone": true,
-              "separationLocationName": 'OTHER',
-              "separationLocationCode": 'SOME VALUE',
-              "alternateNames": [
-                {
-                  "firstName": 'string',
-                  "middleName": 'string',
-                  "lastName": 'string'
-                }
-              ],
-              "confinements": [
-                {
-                  "confinementBeginDate": '2018-03-29T18:50:03.015Z',
-                  "confinementEndDate": '2018-03-29T18:50:03.015Z',
-                  "verifiedIndicator": false
-                }
-              ]
+            "homelessness": {
+              "hasPointOfContact": false
             },
-            "disabilities": [
+            "serviceNumber": 'string'
+          },
+          "attachments": [],
+          "militaryPayments": {
+            "payments": [],
+            "receiveCompensationInLieuOfRetired": false,
+            "receivingInactiveDutyTrainingPay": false,
+            "waveBenifitsToRecInactDutyTraiPay": false
+          },
+          "directDeposit": {
+            "accountType": 'CHECKING',
+            "accountNumber": '1234',
+            "bankName": 'string',
+            "routingNumber": '123456789'
+          },
+          "serviceInformation": {
+            "servicePeriods": [
               {
-                "diagnosticText": 'Diabetes mellitus',
-                "disabilityActionType": 'INCREASE',
-                "decisionCode": 'SVCCONNCTED',
-                "specialIssues": [
-                  {
-                    "code": 'TRM',
-                    "name": 'Personal Trauma PTSD'
-                  }
-                ],
-                "ratedDisabilityId": '0',
-                "ratingDecisionId": 63_655,
-                "diagnosticCode": 5235,
-                "secondaryDisabilities": [
-                  {
-                    "decisionCode": '',
-                    "ratedDisabilityId": '',
-                    "diagnosticText": 'string',
-                    "disabilityActionType": 'NONE'
-                  }
-                ]
+                "serviceBranch": 'string',
+                "activeDutyBeginDate": '2018-03-29T18:50:03.015Z',
+                "activeDutyEndDate": '2018-03-29T18:50:03.015Z'
               }
             ],
-            "treatments": [],
-            "specialCircumstances": [
+            "reservesNationalGuardService": {
+              "title10Activation": {
+                "title10ActivationDate": '2018-03-29T18:50:03.015Z',
+                "anticipatedSeparationDate": '2018-03-29T18:50:03.015Z'
+              },
+              "obligationTermOfServiceFromDate": '2018-03-29T18:50:03.015Z',
+              "obligationTermOfServiceToDate": '2018-03-29T18:50:03.015Z',
+              "unitName": 'string',
+              "unitPhone": {
+                "areaCode": '202',
+                "phoneNumber": '4561111'
+              }
+            },
+            "servedInCombatZone": true,
+            "separationLocationName": 'OTHER',
+            "separationLocationCode": 'SOME VALUE',
+            "alternateNames": [
               {
-                "name": 'string',
-                "code": 'string',
-                "needed": false
+                "firstName": 'string',
+                "middleName": 'string',
+                "lastName": 'string'
+              }
+            ],
+            "confinements": [
+              {
+                "confinementBeginDate": '2018-03-29T18:50:03.015Z',
+                "confinementEndDate": '2018-03-29T18:50:03.015Z',
+                "verifiedIndicator": false
               }
             ]
-          }
-        }.to_json
-      end
-
+          },
+          "disabilities": [
+            {
+              "diagnosticText": 'Diabetes mellitus',
+              "disabilityActionType": 'INCREASE',
+              "decisionCode": 'SVCCONNCTED',
+              "specialIssues": [
+                {
+                  "code": 'TRM',
+                  "name": 'Personal Trauma PTSD'
+                }
+              ],
+              "ratedDisabilityId": '0',
+              "ratingDecisionId": 63_655,
+              "diagnosticCode": 5235,
+              "secondaryDisabilities": [
+                {
+                  "decisionCode": '',
+                  "ratedDisabilityId": '',
+                  "diagnosticText": 'string',
+                  "disabilityActionType": 'NONE'
+                }
+              ]
+            }
+          ],
+          "treatments": [],
+          "specialCircumstances": [
+            {
+              "name": 'string',
+              "code": 'string',
+              "needed": false
+            }
+          ]
+        }
+      }.to_json
+    end
+    context 'with valid input' do
       it 'returns a form submit response object' do
         VCR.use_cassette('evss/disability_compensation_form/submit_form') do
           response = subject.submit_form(valid_form_content)
@@ -176,6 +175,20 @@ describe EVSS::DisabilityCompensationForm::Service do
           expect(response).to be_an EVSS::DisabilityCompensationForm::FormSubmitResponse
           expect(response.claim_id).to be_an Integer
         end
+      end
+    end
+
+    context 'with an http timeout' do
+      before do
+        allow_any_instance_of(Faraday::Connection).to receive(:post).and_raise(Faraday::TimeoutError)
+      end
+
+      it 'should log an error and raise GatewayTimeout' do
+        expect(StatsD).to receive(:increment).once.with(
+          'api.evss.submit_form.fail', tags: ['error:Common::Exceptions::GatewayTimeout']
+        )
+        expect(StatsD).to receive(:increment).once.with('api.evss.submit_form.total')
+        expect { subject.submit_form(valid_form_content) }.to raise_error(Common::Exceptions::GatewayTimeout)
       end
     end
   end
