63eff4df4697f6f5d2ff60f68a794e90979dc60c
hpjaj <hmlevine@gmail.com>
2018-04-05 17:47:03 -0600

    Initial work on address model

---
+++ lib/vet360/models/address.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Vet360
+  module Models
+    class Address < Base
+      attribute :address_line_1, String
+      attribute :address_line_2, String
+      attribute :address_line_3, String
+      attribute :address_pou, String  # purpose of use
+      attribute :address_type, String
+      attribute :city, String
+      attribute :country, String
+      attribute :country_code_iso2, String
+      attribute :country_code_iso3, String
+      attribute :county_code, String
+      attribute :county_name, String
+      attribute :created_at, Common::ISO8601Time
+      attribute :effective_end_date, Common::ISO8601Time
+      attribute :effective_start_date, Common::ISO8601Time
+      attribute :id, Integer
+      attribute :international_postal_code, String
+      attribute :source_date, Common::ISO8601Time
+      attribute :state_abbr, String
+      attribute :transaction_id, String
+      attribute :updated_at, Common::ISO8601Time
+      attribute :zip_code, String
+      attribute :zip_code_suffix, String
+
+      validates :source_date, presence: true
+    end
+  end
+end
