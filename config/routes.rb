fa79e1ebd3cf626a6659a4491ae3a5aa6cb76ac0
James Kassemi <jkassemi@users.noreply.github.com>
2018-04-06 10:02:43 -0600

    Stubs out documentation for new service (#1824)
    * Stubs out documentation for new service
    
    * Incremental improvements
    
    * Add metadat a schema, use security specification; rename
    controller/route
    
    * Benefits upload response documentation
    
    * Removes enum, fixes max, adds 413

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 require 'feature_flipper'
 Rails.application.routes.draw do
   match '/v0/*path', to: 'application#cors_preflight', via: [:options]
+  match '/services/*path', to: 'application#cors_preflight', via: [:options]
 
   get '/saml/metadata', to: 'saml#metadata'
   get '/auth/saml/logout', to: 'v0/sessions#saml_logout_callback', as: 'saml_logout'
@@ -201,6 +202,20 @@ Rails.application.routes.draw do
   end
 
   root 'v0/example#index', module: 'v0'
+
+  scope '/services' do
+    namespace :v0, defaults: { format: 'json' } do
+      namespace :docs, only: [] do
+        # namespace :health do
+        #   resources :prescriptions
+        #   resources :secure_messages
+        # end
+        # resources :health, only: [:index]
+
+        resources :benefits, only: [:index]
+      end
+    end
+  end
 
   if Rails.env.development? || Settings.sidekiq_admin_panel
     require 'sidekiq/web'
