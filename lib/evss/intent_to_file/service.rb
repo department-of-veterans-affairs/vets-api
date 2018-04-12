7f869bf13a8015ae088d9dfe639381acf4509f34
Silvio Luthi <silvio@adhocteam.us>
2018-04-12 16:08:50 -0700

    create common methods for get_active and create_intent_to_file useable by future type inclusion

--- lib/evss/intent_to_file/service.rb
+++ lib/evss/intent_to_file/service.rb
@@ -21,30 +21,38 @@ module EVSS
         handle_error(e)
       end
 
-      def get_active
+      def get_active_compensation
         # intent_to_file currently only handles 'compensation'
         # future expansion will include 'pension' and 'survivor' accessible via the path
-        with_monitoring do
-          raw_response = perform(:get, 'compensation/active')
-          EVSS::IntentToFile::IntentToFileResponse.new(raw_response.status, raw_response)
-        end
-      rescue StandardError => e
-        handle_error(e)
+        get_active('compensation')
       end
 
-      def create_intent_to_file(content)
+      def create_intent_to_file_compensation(content)
         # intent_to_file currently only handles 'compensation'
         # future expansion will include 'pension' and 'survivor' accessible via the path
-        with_monitoring do
-          headers = { 'Content-Type' => 'application/json' }
-          raw_response = perform(:post, 'compensation', content, headers)
-          EVSS::IntentToFile::IntentToFileResponse.new(raw_response.status, raw_response)
-        end
-      rescue StandardError => e
-        handle_error(e)
+        create_intent_to_file(content, 'compensation')
       end
 
       private
+
+      def get_active(type)
+        with_monitoring do
+          raw_response = perform(:get, "#{type}/active")
+          EVSS::IntentToFile::IntentToFileResponse.new(raw_response.status, raw_response)
+        end
+      rescue StandardError => e
+        handle_error(e)
+      end
+
+      def create_intent_to_file(content, type)
+        with_monitoring do
+          headers = { 'Content-Type' => 'application/json' }
+          raw_response = perform(:post, type.to_s, content, headers)
+          EVSS::IntentToFile::IntentToFileResponse.new(raw_response.status, raw_response)
+        end
+      rescue StandardError => e
+        handle_error(e)
+      end
 
       def handle_error(error)
         if error.is_a?(Common::Client::Errors::ClientError) && error.status != 403 && error.body.is_a?(Hash)
