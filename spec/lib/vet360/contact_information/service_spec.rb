dd9567382c407842e9fb805cd1842751dda18885
Ryan Baker <ryan.baker@adhocteam.us>
2018-04-12 11:38:34 -0700

    Assert exception properties

--- spec/lib/vet360/contact_information/service_spec.rb
+++ spec/lib/vet360/contact_information/service_spec.rb
@@ -16,6 +16,7 @@ describe Vet360::ContactInformation::Service do
         VCR.use_cassette('vet360/contact_information/person', match_requests_on: %i[body uri method]) do
           response = subject.get_person
           expect(response).to be_ok
+          expect(response.person).to be_a(Vet360::Models::Person)
         end
       end
     end
@@ -23,10 +24,12 @@ describe Vet360::ContactInformation::Service do
     context 'when successful' do
       it 'returns a status of 404' do
         VCR.use_cassette('vet360/contact_information/person_error', match_requests_on: %i[body uri method]) do
-          expect { subject.get_person }.to raise_error(
-            Common::Exceptions::BackendServiceException
-          )
-          # TODO: check exception attributes
+          expect { subject.get_person }.to raise_error do |e|
+            expect(e).to be_a(Common::Exceptions::BackendServiceException)
+            expect(e.status_code).to eq(404)
+            expect(e.errors.first.code).to eq('VET360_CORE103')
+            expect(e.errors.first.title).to eq('Not Found')
+          end
         end
       end
     end
