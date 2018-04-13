b9e1a10e3f2bdd1cf17a7a00dfd1b7c51abb0ad8
egarreau <ergarreau@gmail.com>
2018-04-13 13:12:36 -0500

    error codes

--- lib/evss/disability_compensation_form/service_exception.rb
+++ lib/evss/disability_compensation_form/service_exception.rb
@@ -6,15 +6,15 @@ module EVSS
   module DisabilityCompensationForm
     class ServiceException < Common::Exceptions::BaseError
       ERROR_MAP = {
-        notEligible: 'evss.526.not_eligible',
         serviceError: 'evss.526.external_service_unavailable',
         ServiceException: 'evss.526.external_service_unavailable',
+        notEligible: 'evss.526.not_eligible',
         InProcess: 'evss.526.form_in_process',
         disabled: 'evss.526.disabled',
-        marshalError: '',
-        startBatchJobError: '',
+        marshalError: 'evss.526.marshall_error',
+        startBatchJobError: 'evss.526.start_batch_job_error',
         Size: 'common.exceptions.validation_errors',
-        Pattern: 'common.exceptions.validation_errors'
+        Pattern: 'common.exceptions.validation_errors',
         NotNull: 'common.exceptions.validation_errors',
         header: 'common.exceptions.validation_errors',
         ActiveDuty13BirthDate: 'common.exceptions.validation_errors',
@@ -26,7 +26,7 @@ module EVSS
         militaryPayments: 'common.exceptions.validation_errors',
         serviceInformation: 'common.exceptions.validation_errors',
         treatments: 'common.exceptions.validation_errors',
-        veteran: 'common.exceptions.validation_errors',
+        veteran: 'common.exceptions.validation_errors'
       }.freeze
 
       attr_reader :key, :messages
@@ -49,7 +49,6 @@ module EVSS
       def messages_has_key?(key)
         @messages.any? { |m| m['key'].include? key.to_s }
       end
-
     end
   end
 end
