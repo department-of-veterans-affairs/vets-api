3409aee35e394db9d9600c0da4fbc9b97fdb3975
Ryan Baker <ryan.baker@adhocteam.us>
2018-04-13 11:36:46 -0700

    Remove effective start/end date for now

--- spec/factories/vet360/addresses.rb
+++ spec/factories/vet360/addresses.rb
@@ -10,9 +10,6 @@ FactoryBot.define do
     zip_code '80202'
     sequence(:id) { |n| n }
     sequence(:transaction_id, 100, &:to_s)
-    confirmation_date    '2017-04-09T11:52:03-06:00'
-    effective_start_date '2017-04-09T11:52:03-06:00'
-    effective_end_date   nil
     source_date          '2018-04-09T11:52:03-06:00'
     created_at           '2017-04-09T11:52:03-06:00'
     updated_at           '2017-04-09T11:52:03-06:00'
--- spec/factories/vet360/emails.rb
+++ spec/factories/vet360/emails.rb
@@ -5,8 +5,6 @@ FactoryBot.define do
     sequence(:id) { |n| n }
     sequence(:email_address, 100) { |n| "person#{n}@example.com" }
     sequence(:transaction_id, 100, &:to_s)
-    effective_start_date '2017-04-09T11:52:03-06:00'
-    effective_end_date   nil
     source_date          '2018-04-09T11:52:03-06:00'
     created_at           '2017-04-09T11:52:03-06:00'
     updated_at           '2017-04-09T11:52:03-06:00'
