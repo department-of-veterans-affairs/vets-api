87f19fa091a1ec691678a2e83d1c4ee0a113e9c0
Silvio Luthi <silvio@adhocteam.us>
2018-04-13 15:32:55 -0700

    Moved with_monitoring out to public methods to use correct method name as identifier

--- lib/evss/intent_to_file/service.rb
+++ lib/evss/intent_to_file/service.rb
@@ -22,34 +22,34 @@ module EVSS
       def get_active_compensation
         # intent_to_file currently only handles 'compensation'
         # future expansion will include 'pension' and 'survivor' accessible via the path
-        get_active('compensation')
+        with_monitoring do
+          get_active('compensation')
+        end
+      rescue StandardError => e
+        handle_error(e)
       end
 
       def create_intent_to_file_compensation(content)
         # intent_to_file currently only handles 'compensation'
         # future expansion will include 'pension' and 'survivor' accessible via the path
-        create_intent_to_file(content, 'compensation')
+        with_monitoring do
+          create_intent_to_file(content, 'compensation')
+        end
+      rescue StandardError => e
+        handle_error(e)
       end
 
       private
 
       def get_active(type)
-        with_monitoring do
-          raw_response = perform(:get, "#{type}/active")
-          EVSS::IntentToFile::IntentToFileResponse.new(raw_response.status, raw_response)
-        end
-      rescue StandardError => e
-        handle_error(e)
+        raw_response = perform(:get, "#{type}/active")
+        EVSS::IntentToFile::IntentToFileResponse.new(raw_response.status, raw_response)
       end
 
       def create_intent_to_file(content, type)
-        with_monitoring do
-          headers = { 'Content-Type' => 'application/json' }
-          raw_response = perform(:post, type.to_s, content, headers)
-          EVSS::IntentToFile::IntentToFileResponse.new(raw_response.status, raw_response)
-        end
-      rescue StandardError => e
-        handle_error(e)
+        headers = { 'Content-Type' => 'application/json' }
+        raw_response = perform(:post, type.to_s, content, headers)
+        EVSS::IntentToFile::IntentToFileResponse.new(raw_response.status, raw_response)
       end
 
       def handle_error(error)
