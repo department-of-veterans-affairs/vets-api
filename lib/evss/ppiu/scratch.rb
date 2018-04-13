67a72a00b88c5c68a605ae1a6a015549b06d980a
egarreau <ergarreau@gmail.com>
2018-04-13 14:27:10 -0500

    update headers in scratch

--- lib/evss/ppiu/scratch.rb
+++ lib/evss/ppiu/scratch.rb
@@ -5,17 +5,17 @@ require 'json'
 headers = {
 	"Content-Type" => "application/json",
 	"Accept" => "application/json",
-	"va_eauth_csid" => "DSLogon",
-	"va_eauth_authenticationmethod" => "DSLogon",
-  "va_eauth_authenticationauthority" => "eauth",
-	"va_eauth_assurancelevel" => "2",
+  "va_eauth_assurancelevel" => "2",
+	"va_eauth_pnid" => "123456789",
 	"va_eauth_firstName" => "Jane",
 	"va_eauth_lastName" => "Doe",
-	"va_eauth_issueinstant" => "2015-04-17T14:52:48Z",
-	"va_eauth_dodedipnid" => "1026070453",
+	"va_eauth_csid" => "DSLogon",
 	"va_eauth_pnidtype" => "SSN",
-	"va_eauth_pnid" => "123456789",
-  "va_eauth_authorization" =>  '{"authorizationResponse":{"id":"123001002","idType":"SSN","edi":"1026070453","firstName":"JANE","lastName":"DOE","gender":"MALE","status":"VETERAN"}}'
+	"va_eauth_dodedipnid" => "1026070453",
+	"va_eauth_authenticationmethod" => "DSLogon",
+  "va_eauth_authenticationauthority" => "eauth",
+	"va_eauth_issueinstant" => "2015-04-17T14:52:48Z",
+  "va_eauth_authorization" =>  '{"authorizationResponse":{"id":"123456789","idType":"SSN","edi":"1026070453","firstName":"JANE","lastName":"DOE","status":"VETERAN"}}'
 }
 
 
