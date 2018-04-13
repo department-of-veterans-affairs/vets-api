5d4577fb36af82be4fd6ca2d0dc708d616e1f624
Ryan Baker <ryan.baker@adhocteam.us>
2018-04-13 14:55:46 -0700

    Default to empty arrays if attributes are nil

--- lib/vet360/models/person.rb
+++ lib/vet360/models/person.rb
@@ -12,15 +12,18 @@ module Vet360
       attribute :updated_at, Common::ISO8601Time
 
       def self.from_response(body)
+        addresses = body['addresses']&.map { |a| Vet360::Models::Address.from_response(a) }
+        emails = body['emails']&.map { |e| Vet360::Models::Email.from_response(e) }
+        telephones = body['telephones']&.map { |t| Vet360::Models::Telephone.from_response(t) }
+
         Vet360::Models::Person.new(
           created_at: body['create_date'],
           source_date: body['source_date'],
           updated_at: body['update_date'],
           transaction_id: body['trx_audit_id'],
-          # @TODO should these be nil or [] when empty?
-          addresses: body['addresses']&.map { |a| Vet360::Models::Address.from_response(a) },
-          emails: body['emails']&.map { |e| Vet360::Models::Telephone.from_response(e) },
-          telephones: body['telephones']&.map { |t| Vet360::Models::Telephone.from_response(t) }
+          addresses: addresses || [],
+          emails: emails || [],
+          telephones: telephones || []
         )
       end
     end
