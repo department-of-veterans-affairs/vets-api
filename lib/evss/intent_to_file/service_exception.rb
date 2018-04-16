cf8002d8a215e4b3eb324c3a5e87bb5b68335209
Silvio Luthi <silvio@adhocteam.us>
2018-04-16 15:44:29 -0700

    changed to arrow type in hash for consistency

--- lib/evss/intent_to_file/service_exception.rb
+++ lib/evss/intent_to_file/service_exception.rb
@@ -10,7 +10,7 @@ module EVSS
         'service.error' => 'evss.intent_to_file.internal_service_error',
         'intentType.invalid' => 'evss.intent_to_file.intent_type_invalid',
         'partner.service.invalid' => 'evss.intent_to_file.partner_service_invalid',
-        default: 'common.exceptions.internal_server_error'
+        'default' => 'common.exceptions.internal_server_error'
       }.freeze
 
       attr_reader :key, :messages
