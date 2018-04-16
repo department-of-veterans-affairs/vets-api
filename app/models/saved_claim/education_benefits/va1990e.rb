d984b55aee84e3d78571fe26aa1b7e9936265301
lihan <frankieteardrop@gmail.com>
2018-04-16 12:05:39 -0400

    remove unused code (#1849)

--- app/models/saved_claim/education_benefits/va1990e.rb
+++ app/models/saved_claim/education_benefits/va1990e.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class SavedClaim::EducationBenefits::VA1990e < SavedClaim::EducationBenefits
-  PERSISTENT_CLASS = nil
   add_form_and_validation('22-1990E')
 end
--- app/models/saved_claim/education_benefits/va1990n.rb
+++ app/models/saved_claim/education_benefits/va1990n.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class SavedClaim::EducationBenefits::VA1990n < SavedClaim::EducationBenefits
-  PERSISTENT_CLASS = nil
   add_form_and_validation('22-1990N')
 end
