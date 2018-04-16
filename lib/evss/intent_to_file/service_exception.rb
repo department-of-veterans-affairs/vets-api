59d0e03a01cd36c7b908a76c9946530c5ad8bdde
Silvio Luthi <silvio@adhocteam.us>
2018-04-16 09:40:18 -0700

    clean up error keys

--- lib/evss/intent_to_file/service_exception.rb
+++ lib/evss/intent_to_file/service_exception.rb
@@ -6,10 +6,10 @@ module EVSS
   module IntentToFile
     class ServiceException < Common::Exceptions::BaseError
       ERROR_MAP = {
-        'intentToFile.partner.service.error' => 'evss.intent_to_file.partner_service_error',
-        'intentToFile.service.error' => 'evss.intent_to_file.internal_service_error',
-        'intenttofile.intentType.invalid' => 'evss.intent_to_file.intent_type_invalid',
-        'intenttofile.partner.service.invalid' => 'evss.intent_to_file.partner_service_invalid',
+        'partner.service.error' => 'evss.intent_to_file.partner_service_error',
+        'service.error' => 'evss.intent_to_file.internal_service_error',
+        'intentType.invalid' => 'evss.intent_to_file.intent_type_invalid',
+        'partner.service.invalid' => 'evss.intent_to_file.partner_service_invalid',
         default: 'common.exceptions.internal_server_error'
       }.freeze
 
