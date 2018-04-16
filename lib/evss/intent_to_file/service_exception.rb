d943f8b7e08f99b56f746a315c73359d6699f97a
Silvio Luthi <silvio@adhocteam.us>
2018-04-16 15:54:54 -0700

    Update service_exception file to use common base class

--- lib/evss/intent_to_file/service_exception.rb
+++ lib/evss/intent_to_file/service_exception.rb
@@ -4,7 +4,7 @@ require 'common/exceptions/base_error'
 
 module EVSS
   module IntentToFile
-    class ServiceException < Common::Exceptions::BaseError
+    class ServiceException < EVSS::ServiceException
       ERROR_MAP = {
         'partner.service.error' => 'evss.intent_to_file.partner_service_error',
         'service.error' => 'evss.intent_to_file.internal_service_error',
@@ -14,37 +14,6 @@ module EVSS
       }.freeze
 
       attr_reader :key, :messages
-
-      def initialize(original_body)
-        @messages = original_body['messages']
-        @key = error_key
-        super
-      end
-
-      def errors
-        Array(
-          Common::Exceptions::SerializableError.new(
-            i18n_data.merge(source: 'EVSS::Letters::Service', meta: { messages: @messages })
-          )
-        )
-      end
-
-      private
-
-      def error_key
-        # in case of multiple errors highest priority code is the one set for the http response
-        key = ERROR_MAP.select { |k, _v| messages_has_key?(k) }
-        return key.values.first unless key.empty?
-        ERROR_MAP[:default]
-      end
-
-      def messages_has_key?(key)
-        @messages.any? { |m| m['key'].include? key.to_s }
-      end
-
-      def i18n_key
-        @key
-      end
     end
   end
 end
