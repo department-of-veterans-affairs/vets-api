ad32f470527acf65432c11d7752eaee107f26922
James Kassemi <james.kassemi@adhocteam.us>
2018-04-17 12:19:55 -0600

    Expose Open API v3 from raw description

--- modules/vba_documents/app/controllers/vba_documents/docs/v0/api_controller.rb
+++ modules/vba_documents/app/controllers/vba_documents/docs/v0/api_controller.rb
@@ -4,47 +4,11 @@ module VBADocuments
   module Docs
     module V0
       class ApiController < ApplicationController
-        include Swagger::Blocks
-
-        skip_before_action :authenticate
-
-        swagger_root do
-          key :swagger, '2.0'
-          info do
-            key :version, '0.0.0'
-            key :title, 'Benefits'
-            key :description, 'Veterans Benefits Administration (VBA) document uploads.'
-            key :termsOfService, ''
-            contact do
-              key :name, 'Vets.gov'
-            end
-          end
-
-          security_definition :api_key do
-            key :type, :apiKey
-            key :name, :apikey
-            key :in, :query
-          end
-
-          tag do
-            key :name, 'document_uploads'
-            key :description, 'VA Benefits document upload functionality'
-          end
-
-          key :host, Settings.hostname
-          key :basePath, '/v0/benefits'
-          key :consumes, ['application/json']
-          key :produces, ['application/json']
-        end
-
-        # A list of all classes that have swagger_* declarations.
-        SWAGGERED_CLASSES = [
-          VBADocuments::V0::UploadsController,
-          self
-        ].freeze
+        skip_before_action(:authenticate)
 
         def index
-          render json: Swagger::Blocks.build_root_json(SWAGGERED_CLASSES)
+          swagger = YAML.safe_load(File.read(VBADocuments::Engine.root.join('README.yml')))
+          render json: swagger
         end
       end
     end
