34ef88290fdc5f2fb6b9a0dcfe494825f26d36d2
Silvio Luthi <silvio@adhocteam.us>
2018-04-19 16:57:57 -0700

    Move response out from render

--- app/controllers/v0/intent_to_files_controller.rb
+++ app/controllers/v0/intent_to_files_controller.rb
@@ -5,17 +5,20 @@ module V0
     before_action { authorize :evss, :access? }
 
     def index
-      render json: service.get_intent_to_file,
+      response = service.get_intent_to_file
+      render json: response,
              serializer: IntentToFileSerializer
     end
 
     def active_compensation
-      render json: service.get_active_compensation,
+      response = service.get_active_compensation
+      render json: response,
              serializer: IntentToFileSerializer
     end
 
     def submit_compensation
-      render json: service.create_intent_to_file_compensation,
+      response = service.create_intent_to_file_compensation
+      render json: response,
              serializer: IntentToFileSerializer
     end
 
