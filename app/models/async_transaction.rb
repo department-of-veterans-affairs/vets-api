fd1051b732a269b850aa31a7a9b41390269d03cd
Emerson Loustau <e@thesecretaddress.com>
2018-04-23 12:58:12 -0700

    validation + factory

--- app/models/async_transaction.rb
+++ app/models/async_transaction.rb
@@ -1,2 +1,9 @@
 class AsyncTransaction < ActiveRecord::Base
+
+  validates :user_uuid, :source_id, :source_type, :status, :transaction_id, presence: true
+
 end
+
+class AddressTransaction < AsyncTransaction; end
+class EmailTransaction < AsyncTransaction; end
+class TelephoneTransaction < AsyncTransaction; end
--- spec/factories/async_transactions.rb
+++ spec/factories/async_transactions.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :async_transaction do
     
+    sequence(:id) { |n| n }
+    type                'AddressTransaction'
+    user_uuid           'abcdb2b5-6af0-45e1-a9e2-394347af99'
+    source_id           sequence(:id) { |n| n }
+    source              'vet360'
+    status              'started'
+    transaction_id      sequence(:id) { |n| n }
+    transaction_status  'RECEIVED'
+    created_at           '2017-04-09T11:52:03-06:00'
+    updated_at           '2017-04-09T11:52:03-06:00'
+
   end
 end
