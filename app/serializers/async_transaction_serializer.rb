d7e7d3fb5d713d317d66efc3ad98f54d5d72ba3e
Emerson Loustau <e@thesecretaddress.com>
2018-04-23 12:06:51 -0700

    serializers and indexes

---
+++ app/serializers/async_transaction_serializer.rb
@@ -0,0 +1,11 @@
+class AsyncTransactionSerializer < ActiveModel::Serializer
+  attribute :id
+  # @TODO Do we actually need to serialize the rest of these?
+  # Does the front end actually need to know this data?
+  attribute :type
+  attribute :user_uuid
+  attribute :source_id
+  attribute :source
+  attribute :transaction_id
+  attribute :transaction_status
+end
--- db/migrate/20180423182604_add_indexes_to_async_transactions.rb
+++ db/migrate/20180423182604_add_indexes_to_async_transactions.rb
@@ -1,7 +1,10 @@
 class AddIndexesToAsyncTransactions < ActiveRecord::Migration
+  
+  disable_ddl_transaction!
+
   def change
-    add_index :async_transactions, :user_uuid
-    add_index :async_transactions, :source_id
-    add_index :async_transactions, :transaction_status
+    add_index :async_transactions, :user_uuid, algorithm: :concurrently
+    add_index :async_transactions, :source_id, algorithm: :concurrently
+    add_index :async_transactions, :transaction_id, algorithm: :concurrently
   end
 end
