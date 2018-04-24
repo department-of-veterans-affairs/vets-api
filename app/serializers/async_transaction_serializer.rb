59b9c47045366288d7e7b62e41e5fc38a1c3d4d4
Emerson Loustau <e@thesecretaddress.com>
2018-04-24 07:32:15 -0700

    serializer

--- app/serializers/async_transaction_serializer.rb
+++ app/serializers/async_transaction_serializer.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class AsyncTransactionSerializer < ActiveModel::Serializer
-  attribute :id
-  # @TODO Do we actually need to serialize the rest of these?
-  # Does the front end actually need to know this data?
-  attribute :type
-  attribute :user_uuid
-  attribute :source_id
-  attribute :source
+  
   attribute :transaction_id
   attribute :transaction_status
+
+  def id
+    nil
+  end
+
 end
