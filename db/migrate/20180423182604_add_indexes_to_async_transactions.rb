d1705b5e6fee9a7ec7987bf12406f6c15dc79a29
Emerson Loustau <e@thesecretaddress.com>
2018-04-24 07:26:16 -0700

    multi-column indexes

--- db/migrate/20180423182604_add_indexes_to_async_transactions.rb
+++ db/migrate/20180423182604_add_indexes_to_async_transactions.rb
@@ -6,5 +6,11 @@ class AddIndexesToAsyncTransactions < ActiveRecord::Migration
     add_index :async_transactions, :user_uuid, algorithm: :concurrently
     add_index :async_transactions, :source_id, algorithm: :concurrently
     add_index :async_transactions, :transaction_id, algorithm: :concurrently
+
+    add_index :async_transactions, [:user_uuid, :source], algorithm: :concurrently
+    add_index :async_transactions, 
+            [:transaction_id, :source_id, :source], 
+            algorithm: :concurrently, 
+            name: 'index_async_tx_on_tx_id_and_source_id_and_source'   
   end
 end
