e81c2888885b20b49a3a6c986e4b09d29324ae7e
Emerson Loustau <e@thesecretaddress.com>
2018-04-24 13:57:08 -0700

    factory fixes

--- spec/factories/async_transactions.rb
+++ spec/factories/async_transactions.rb
@@ -2,8 +2,28 @@
 
 FactoryBot.define do
   factory :address_transaction, class: AsyncTransaction::Vet360::AddressTransaction do
-    sequence(:id) { |n| n }
-    user_uuid           'abcdb2b5-6af0-45e1-a9e2-394347af99'
+    sequence(:id, 1) { |n| n }
+    user_uuid           'abcd789-6af0-45e1-a9e2-394347af99'
+    sequence(:source_id) { |n| n }
+    source              'vet360'
+    status              'started'
+    sequence(:transaction_id) { |n| n }
+    transaction_status  'RECEIVED'
+  end
+
+  factory :email_transaction, class: AsyncTransaction::Vet360::EmailTransaction do
+    sequence(:id, 200) { |n| n }
+    user_uuid           'abcdb456-6af0-45e1-a9e2-394347af98'
+    sequence(:source_id) { |n| n }
+    source              'vet360'
+    status              'started'
+    sequence(:transaction_id) { |n| n }
+    transaction_status  'RECEIVED'
+  end
+
+  factory :telephone_transaction, class: AsyncTransaction::Vet360::TelephoneTransaction do
+    sequence(:id, 300) { |n| n }
+    user_uuid           'abcdb123-6af0-45e1-a9e2-394347af97'
     sequence(:source_id) { |n| n }
     source              'vet360'
     status              'started'
--- spec/models/async_transaction/base_spec.rb
+++ spec/models/async_transaction/base_spec.rb
@@ -2,13 +2,14 @@
 
 require 'rails_helper'
 
-RSpec.describe AsyncTransaction::Vet360::Base, type: :model do
+RSpec.describe AsyncTransaction::Base, type: :model do
 
   describe '.save' do
 
       it 'works' do
-        saved = AsyncTransaction::Vet360::AddressTransaction.new(
+        saved = AsyncTransaction::Base.new(
             user_uuid: 'abcdef',
+            type: 'AsyncTransaction::Vet360::AddressTransaction',
             source_id: 42,
             source: 'vet360',
             status: 'sent',
@@ -54,11 +55,25 @@ RSpec.describe AsyncTransaction::Vet360::Base, type: :model do
   end
 
   describe 'Subclasses' do
-      let(:tx1) { build(:address_transaction) }
+      let(:tx1) { build(:address_transaction, transaction_id: 1) }
+      let(:tx2) { build(:email_transaction, transaction_id: 2) }
+      let(:tx3) { build(:telephone_transaction, transaction_id: 3) }
+
       it 'are queryable from the parent' do
         tx1.save
         r1 = AsyncTransaction::Vet360::Base.where(transaction_id: tx1.transaction_id, source: tx1.source).first
         expect(r1.id).to eq(tx1.id)
+        expect(r1).to be_instance_of(AsyncTransaction::Vet360::AddressTransaction)
+
+        tx2.save
+        r2 = AsyncTransaction::Vet360::Base.where(transaction_id: tx2.transaction_id, source: tx2.source).first
+        expect(r2.id).to eq(tx2.id)
+        expect(r2).to be_instance_of(AsyncTransaction::Vet360::EmailTransaction)
+        
+        tx3.save
+        r3 = AsyncTransaction::Vet360::Base.where(transaction_id: tx3.transaction_id, source: tx3.source).first
+        expect(r3.id).to eq(tx3.id)
+        expect(r3).to be_instance_of(AsyncTransaction::Vet360::TelephoneTransaction)
       end
 
   end
