ceb3781da15b096538e3b1762e286aba8a5213ce
Emerson Loustau <e@thesecretaddress.com>
2018-04-24 08:41:50 -0700

    validate unique source_id and scope

--- app/models/async_transaction/base.rb
+++ app/models/async_transaction/base.rb
@@ -4,6 +4,7 @@ module AsyncTransaction
 
   class Base < ActiveRecord::Base
     validates :user_uuid, :source_id, :source_type, :status, :transaction_id, presence: true
+    validates :transaction_id, uniqueness: { scope: :source_id }
   end
 
 end
