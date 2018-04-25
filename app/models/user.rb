f9e52b0061f18bf76e4ab17e4918830fa7c3e477
Johnny Holton <johnny@holton.co>
2018-04-25 09:57:17 -0400

    lint fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -238,7 +238,7 @@ class User < Common::RedisStore
   def pciu
     @pciu ||= EVSS::PCIU::Service.new self
   end
-  
+
   def vet360_contact_info
     return nil unless Settings.vet360.contact_information.enabled
     @vet360_contact_info ||= Vet360Redis::ContactInformation.for_user(self)
