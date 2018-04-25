7e79367e11b934e18e0f0b00f2c22feb6abb5745
Emerson Loustau <e@thesecretaddress.com>
2018-04-25 10:27:12 -0700

    rubocop fixes

--- spec/factories/async_transactions.rb
+++ spec/factories/async_transactions.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-
   factory :async_transaction do
     sequence(:id, 1) { |n| n }
     user_uuid 'abcd789-6af0-45e1-a9e2-394347af99'
-    sequence(:source_id) { |n| "#{n}" }
+    sequence(:source_id, &:to_s)
     source              'vet360'
     status              'started'
-    sequence(:transaction_id) { |n| "#{n}" }
+    sequence(:transaction_id, &:to_s)
     transaction_status 'RECEIVED'
 
     factory :address_transaction, class: AsyncTransaction::Vet360::AddressTransaction do
@@ -19,7 +18,5 @@ FactoryBot.define do
 
     factory :telephone_transaction, class: AsyncTransaction::Vet360::TelephoneTransaction do
     end
-
   end
-
 end
--- spec/models/async_transaction/base_spec.rb
+++ spec/models/async_transaction/base_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe AsyncTransaction::Base, type: :model do
-
   describe 'Validation' do
     let(:transaction1) { create(:address_transaction) }
     let(:transaction2) { build(:address_transaction) }
@@ -20,7 +19,7 @@ RSpec.describe AsyncTransaction::Base, type: :model do
     end
 
     it 'accepts a FactoryBot-made transaction' do
-      expect (transaction4.valid?).to be true
+      expect transaction4.valid?.to be true
     end
   end
 
@@ -30,15 +29,18 @@ RSpec.describe AsyncTransaction::Base, type: :model do
     let(:transaction3) { create(:telephone_transaction, transaction_id: 3) }
 
     it 'are queryable from the parent' do
-      record1 = AsyncTransaction::Vet360::Base.where(transaction_id: transaction1.transaction_id, source: transaction1.source).first
+      record1 = AsyncTransaction::Vet360::Base
+                .where(transaction_id: transaction1.transaction_id, source: transaction1.source).first
       expect(record1.id).to eq(transaction1.id)
       expect(record1).to be_instance_of(AsyncTransaction::Vet360::AddressTransaction)
 
-      record2 = AsyncTransaction::Vet360::Base.where(transaction_id: transaction2.transaction_id, source: transaction2.source).first
+      record2 = AsyncTransaction::Vet360::Base
+                .where(transaction_id: transaction2.transaction_id, source: transaction2.source).first
       expect(record2.id).to eq(transaction2.id)
       expect(record2).to be_instance_of(AsyncTransaction::Vet360::EmailTransaction)
 
-      record3 = AsyncTransaction::Vet360::Base.where(transaction_id: transaction3.transaction_id, source: transaction3.source).first
+      record3 = AsyncTransaction::Vet360::Base
+                .where(transaction_id: transaction3.transaction_id, source: transaction3.source).first
       expect(record3.id).to eq(transaction3.id)
       expect(record3).to be_instance_of(AsyncTransaction::Vet360::TelephoneTransaction)
     end
