a2ec3332dae64322966accd2a6ae219ed065a434
Emerson Loustau <e@thesecretaddress.com>
2018-04-25 08:56:00 -0700

    removed migration comment

--- db/migrate/20180423182604_add_indexes_to_async_transactions.rb
+++ db/migrate/20180423182604_add_indexes_to_async_transactions.rb
@@ -6,14 +6,6 @@ class AddIndexesToAsyncTransactions < ActiveRecord::Migration
     add_index :async_transactions, :user_uuid, algorithm: :concurrently
     add_index :async_transactions, :source_id, algorithm: :concurrently
     add_index :async_transactions, :transaction_id, algorithm: :concurrently
-
     add_index :async_transactions, [:transaction_id, :source], unique: true, algorithm: :concurrently
-
-    # # @TODO Multi-column indexes for the future when we know more about our queries 
-    # add_index :async_transactions, [:user_uuid, :source], algorithm: :concurrently
-    # add_index :async_transactions, 
-    #         [:transaction_id, :source_id, :source], 
-    #         algorithm: :concurrently, 
-    #         name: 'index_async_tx_on_tx_id_and_source_id_and_source'   
   end
 end
