82e56e7a9c095dbe1aa7847777c045c8715379d5
Emerson Loustau <e@thesecretaddress.com>
2018-04-26 12:31:10 -0700

    custom message

--- app/models/async_transaction/base.rb
+++ app/models/async_transaction/base.rb
@@ -5,6 +5,6 @@ module AsyncTransaction
     self.table_name = 'async_transactions'
     validates :id, uniqueness: true
     validates :user_uuid, :source_id, :source, :status, :transaction_id, presence: true
-    validates :transaction_id, uniqueness: { scope: :source }
+    validates :transaction_id, uniqueness: { scope: :source, message: 'Transaction ID must be unique within a source.'  }
   end
 end
