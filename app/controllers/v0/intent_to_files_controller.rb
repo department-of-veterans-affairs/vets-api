7c10940f589d5c9e1ed1d736b197fa7c7882b8d1
Silvio Luthi <silvio@adhocteam.us>
2018-04-27 10:50:30 -0700

    Update itf endpoints to use a dynamic  in path. Update swagger request to reflect those changes.

--- app/controllers/v0/intent_to_files_controller.rb
+++ app/controllers/v0/intent_to_files_controller.rb
@@ -3,6 +3,11 @@
 module V0
   class IntentToFilesController < ApplicationController
     before_action { authorize :evss, :access? }
+    before_action :validate_type_param, only: %i[active submit]
+
+    # currently, only `compensation` is supported. This will be expanded to
+    # include `pension` and `survivor` in the future.
+    TYPES = %w[compensation].freeze
 
     def index
       response = service.get_intent_to_file
@@ -10,19 +15,24 @@ module V0
              serializer: IntentToFileSerializer
     end
 
-    def active_compensation
-      response = service.get_active_compensation
+    def active
+      response = service.get_active(params[:type])
       render json: response,
              serializer: IntentToFileSerializer
     end
 
-    def submit_compensation
-      response = service.create_intent_to_file_compensation
+    def submit
+      response = service.create_intent_to_file(params[:type])
       render json: response,
              serializer: IntentToFileSerializer
     end
 
     private
+
+    def validate_type_param
+      raise Common::Exceptions::InvalidFieldValue.new('type', params[:type]) unless
+        TYPES.include?(params[:type])
+    end
 
     def service
       EVSS::IntentToFile::Service.new(@current_user)
