de66fc5c72e6eb9733fc79ce8be8d6afe6093197
hpjaj <hmlevine@gmail.com>
2018-04-30 14:37:37 -0600

    Captures and sets type of transaction
    As this is part of our standard response.body for these endpoints

--- lib/vet360/models/transaction.rb
+++ lib/vet360/models/transaction.rb
@@ -16,17 +16,19 @@ module Vet360
       attribute :messages, Array[Vet360::Models::Message]
       attribute :id, String
       attribute :status, String
+      attribute :type, String
 
       # Converts a decoded JSON response from Vet360 to an instance of the Transaction model
       # @params body [Hash] the decoded response body from Vet360
       # @return [Vet360::Models::Transaction] the model built from the response body
-      def self.build_from(body)
+      def self.build_from(body, type)
         messages = body['tx_messages']&.map { |m| Vet360::Models::Message.build_from(m) }
 
         Vet360::Models::Transaction.new(
           messages: messages || [],
           id: body['tx_audit_id'],
           status: body['tx_status']
+          type: type
         )
       end
     end
