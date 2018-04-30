e96d5b353758d64f418f6476d061374657a4c7c6
Derek <derekhall77@gmail.com>
2018-04-30 11:11:05 -0400

    lint fix

--- spec/jobs/facilities/facility_location_download_job_spec.rb
+++ spec/jobs/facilities/facility_location_download_job_spec.rb
@@ -100,8 +100,8 @@ RSpec.describe Facilities::FacilityLocationDownloadJob, type: :job do
     after(:each) { Facilities::FacilityMapping.validate_on_load = false }
     it 'raises an error when trying to retrieve and persist facilities data' do
       VCR.use_cassette('facilities/va/vha_facilities') do
-        expect{ Facilities::FacilityLocationDownloadJob.new.perform('vha') }.
-          to raise_error(Common::Client::Errors::ValidationError, 'invalid source data: duplicate ids')
+        expect { Facilities::FacilityLocationDownloadJob.new.perform('vha') }
+          .to raise_error(Common::Client::Errors::ValidationError, 'invalid source data: duplicate ids')
       end
     end
   end
