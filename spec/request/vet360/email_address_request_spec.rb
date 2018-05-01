0b9eeadc106abd8415d5c3932b7506f8d17b1e51
hpjaj <hmlevine@gmail.com>
2018-05-01 11:23:46 -0600

    Update email endpoint and happy path specs

--- spec/request/vet360/email_address_request_spec.rb
+++ spec/request/vet360/email_address_request_spec.rb
@@ -34,4 +34,25 @@ RSpec.describe 'email_address', type: :request do
       end
     end
   end
+
+  describe 'PUT /v0/profile/email_addresses' do
+    let(:email) { build(:email, vet360_id: user.vet360_id) }
+
+    context 'with a 200 response' do
+      it 'should match the email address schema', :aggregate_failures do
+        VCR.use_cassette('vet360/contact_information/put_email_success') do
+          put(
+            '/v0/profile/email_addresses',
+            { id: 42, email_address: 'person42@example.com' }.to_json,
+            auth_header.update(
+              'Content-Type' => 'application/json', 'Accept' => 'application/json'
+            )
+          )
+
+          expect(response).to have_http_status(:ok)
+          expect(response).to match_response_schema('vet360/transaction_response')
+        end
+      end
+    end
+  end
 end
