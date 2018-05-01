a5a72d1da24e65900a5973f40de35329b27ae8b4
hpjaj <hmlevine@gmail.com>
2018-05-01 10:53:35 -0600

    Three attrs are required to be set by the BE every time

--- lib/vet360/models/base.rb
+++ lib/vet360/models/base.rb
@@ -11,6 +11,26 @@ module Vet360
       include Virtus.model(nullify_blank: true)
 
       SOURCE_SYSTEM = 'VETSGOV'
+      DEFAULT_ATTRS = %i[effective_start_date source_date vet360_id].freeze
+
+      def self.with_defaults(user, params)
+        instance = new(params)
+        now      = Time.now.iso8601
+
+        return unless instance.default_attrs_present?
+
+        instance.tap do |record|
+          record.attributes = {
+            effective_start_date: now,
+            source_date: now,
+            vet360_id: user.vet360_id
+          }
+        end
+      end
+
+      def default_attrs_present?
+        attributes.keys & DEFAULT_ATTRS == DEFAULT_ATTRS
+      end
     end
   end
 end
