fc29482bb1b069bbf1f01f8c6e93c6b26165b5c1
hpjaj <hmlevine@gmail.com>
2018-05-02 16:15:54 -0600

    Telephone controller and routes

---
+++ app/controllers/v0/profile/telephones_controller.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module V0
+  module Profile
+    class TelephonesController < ApplicationController
+      before_action { authorize :vet360, :access? }
+
+      def create
+        telephone = Vet360::Models::Telephone.with_defaults(@current_user, telephone_params)
+
+        if telephone.valid?
+          response    = service.post_telephone telephone
+          transaction = AsyncTransaction::Vet360::TelephoneTransaction.start(@current_user, response)
+
+          render json: transaction, serializer: AsyncTransaction::BaseSerializer
+        else
+          raise Common::Exceptions::ValidationErrors, telephone
+        end
+      end
+
+      def update
+        telephone = Vet360::Models::Telephone.with_defaults(@current_user, telephone_params)
+
+        if telephone.valid?
+          response    = service.put_telephone telephone
+          transaction = AsyncTransaction::Vet360::TelephoneTransaction.start @current_user, response
+
+          render json: transaction, serializer: AsyncTransaction::BaseSerializer
+        else
+          raise Common::Exceptions::ValidationErrors, telephone
+        end
+      end
+
+      private
+
+      def service
+        Vet360::ContactInformation::Service.new @current_user
+      end
+
+      def telephone_params
+        params.permit(
+          :area_code,
+          :country_code,
+          :extension,
+          :effective_end_date,
+          :effective_start_date,
+          :id,
+          :is_international,
+          :is_textable,
+          :is_text_permitted,
+          :is_tty,
+          :is_voicemailable,
+          :phone_number,
+          :phone_type,
+          :source_date,
+          :transaction_id,
+          :vet360_id
+        )
+      end
+    end
+  end
+end
