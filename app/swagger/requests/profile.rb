398a246f762d51fb3095e8d757a1d85ade78cfd4
hpjaj <hmlevine@gmail.com>
2018-05-03 08:50:38 -0600

    Rubocop issues

--- app/swagger/requests/profile.rb
+++ app/swagger/requests/profile.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ClassLength
 module Swagger
   module Requests
     class Profile
@@ -500,3 +501,4 @@ module Swagger
     end
   end
 end
+# rubocop:enable Metrics/ClassLength
