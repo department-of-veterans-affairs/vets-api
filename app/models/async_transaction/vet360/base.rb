03c4f7069fe7401da3d58d7207fe819a975fe83c
hpjaj <hmlevine@gmail.com>
2018-05-03 09:36:25 -0600

    Creates constant for status types

--- app/models/async_transaction/vet360/base.rb
+++ app/models/async_transaction/vet360/base.rb
@@ -3,12 +3,13 @@
 module AsyncTransaction
   module Vet360
     class Base < AsyncTransaction::Base
+      SENT = 'sent'
       def self.start(user, response)
         create(
           user_uuid: user.uuid,
           source_id: user.vet360_id,
           source: 'vet360',
-          status: 'sent',
+          status: SENT,
           transaction_id: response.transaction.id,
           transaction_status: response.transaction.status
         )
