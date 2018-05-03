0c82a73966815a28fcd5634da16010dc82f3e02f
hpjaj <hmlevine@gmail.com>
2018-05-03 09:36:34 -0600

    Yard docs

--- app/models/async_transaction/vet360/base.rb
+++ app/models/async_transaction/vet360/base.rb
@@ -4,6 +4,14 @@ module AsyncTransaction
   module Vet360
     class Base < AsyncTransaction::Base
       SENT = 'sent'
+
+      # Creates an initial AsyncTransaction record for ongoing tracking
+      #
+      # @param user [User] The user associated with the transaction
+      # @param response [Vet360::ContactInformation::TransactionResponse] An instance of
+      #   a Vet360::ContactInformation::TransactionResponse class, be it Email, Address, etc.
+      # @return [AsyncTransaction::Vet360::Base] A AsyncTransaction::Vet360::Base record, be it Email, Address, etc.
+      #
       def self.start(user, response)
         create(
           user_uuid: user.uuid,
