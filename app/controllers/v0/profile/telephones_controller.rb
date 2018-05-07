bc3df18105191437d426720c91cad0fa46b1c5b0
hpjaj <hmlevine@gmail.com>
2018-05-07 13:32:59 -0600

    DRY’s up 6 common controller actions through Rails Concerns

--- app/controllers/v0/profile/telephones_controller.rb
+++ app/controllers/v0/profile/telephones_controller.rb
@@ -3,39 +3,19 @@
 module V0
   module Profile
     class TelephonesController < ApplicationController
+      include Vet360::Writeable
+
       before_action { authorize :vet360, :access? }
 
       def create
-        telephone = Vet360::Models::Telephone.new(telephone_params).set_defaults(@current_user)
-
-        if telephone.valid?
-          response    = service.post_telephone telephone
-          transaction = AsyncTransaction::Vet360::TelephoneTransaction.start(@current_user, response)
-
-          render json: transaction, serializer: AsyncTransaction::BaseSerializer
-        else
-          raise Common::Exceptions::ValidationErrors, telephone
-        end
+        write_to_vet360_and_render_transaction!('telephone', telephone_params)
       end
 
       def update
-        telephone = Vet360::Models::Telephone.new(telephone_params).set_defaults(@current_user)
-
-        if telephone.valid?
-          response    = service.put_telephone telephone
-          transaction = AsyncTransaction::Vet360::TelephoneTransaction.start @current_user, response
-
-          render json: transaction, serializer: AsyncTransaction::BaseSerializer
-        else
-          raise Common::Exceptions::ValidationErrors, telephone
-        end
+        write_to_vet360_and_render_transaction!('telephone', telephone_params, http_verb: 'put')
       end
 
       private
-
-      def service
-        Vet360::ContactInformation::Service.new @current_user
-      end
 
       def telephone_params
         params.permit(
