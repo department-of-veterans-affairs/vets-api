09995f0e45bb62369f4cb727e94abe7a83033ec4
Ryan Baker <ryan.baker@adhocteam.us>
2018-05-08 13:37:57 -0700

    Add telephone validations to docs

--- app/swagger/schemas/vet360/telephone.rb
+++ app/swagger/schemas/vet360/telephone.rb
@@ -8,31 +8,51 @@ module Swagger
 
         swagger_schema :PostVet360Telephone do
           key :required, %i[phone_number area_code phone_type]
-          property :phone_number, type: :string, example: '5551212'
-          property :area_code, type: :string, example: '303'
-          property :extension, type: :string, example: '101'
-          property :phone_type, type: :string, enum: %w[
-            MOBILE
-            HOME
-            WORK
-            FAX
-            TEMPORARY
-          ], example: 'MOBILE'
+          property :phone_number,
+                   type: :string,
+                   example: '5551212',
+                   minimum_length: 1,
+                   maximum_length: 14,
+                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect
+          property :area_code,
+                   type: :string,
+                   example: '303',
+                   minimum_length: 3,
+                   maximum_length: 3,
+                   pattern: ::Vet360::Models::Telephone::VALID_AREA_CODE_REGEX.inspect
+          property :extension,
+                   type: :string,
+                   example: '101',
+                   maximum_length: 10
+          property :phone_type,
+                   type: :string,
+                   enum: ::Vet360::Models::Telephone::PHONE_TYPES,
+                   example: ::Vet360::Models::Telephone::MOBILE
         end
 
         swagger_schema :PutVet360Telephone do
           key :required, %i[id phone_number area_code phone_type]
           property :id, type: :integer, example: 1
-          property :phone_number, type: :string, example: '5551212'
-          property :area_code, type: :string, example: '303'
-          property :extension, type: :string, example: '101'
-          property :phone_type, type: :string, enum: %w[
-            MOBILE
-            HOME
-            WORK
-            FAX
-            TEMPORARY
-          ], example: 'MOBILE'
+          property :phone_number,
+                   type: :string,
+                   example: '5551212',
+                   minimum_length: 1,
+                   maximum_length: 14,
+                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect
+          property :area_code,
+                   type: :string,
+                   example: '303',
+                   minimum_length: 3,
+                   maximum_length: 3,
+                   pattern: ::Vet360::Models::Telephone::VALID_AREA_CODE_REGEX.inspect
+          property :extension,
+                   type: :string,
+                   example: '101',
+                   maximum_length: 10
+          property :phone_type,
+                   type: :string,
+                   enum: ::Vet360::Models::Telephone::PHONE_TYPES,
+                   example: ::Vet360::Models::Telephone::MOBILE
         end
       end
     end
