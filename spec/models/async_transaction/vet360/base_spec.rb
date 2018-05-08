bb71fe38e80f324de43af88c4176ff5512cf3137
Emerson Loustau <e@thesecretaddress.com>
2018-05-08 13:09:37 -0700

    trigger ci

--- spec/models/async_transaction/vet360/base_spec.rb
+++ spec/models/async_transaction/vet360/base_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe AsyncTransaction::Vet360::Base, type: :model do
     let(:service) { ::Vet360::ContactInformation::Service.new(user) }
     it 'raises an error if passed unrecognized transaction' do
       expect do
-        AsyncTransaction::Vet360::Base.fetch_transaction(Struct.new('Surprise!'), nil)
+        AsyncTransaction::Vet360::Base.fetch_transaction(Struct.new('Surprise'), nil)
       end.to raise_exception
     end
   end
