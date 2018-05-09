2243961bbabb0604328af4dd0f7cb901140ffc19
Kam Karshenas <kam@adhocteam.us>
2018-05-08 17:01:14 -0700

    fix this one that was accidentally removed

--- db/migrate/20171229003530_change_form_attachments_type_null.rb
+++ db/migrate/20171229003530_change_form_attachments_type_null.rb
@@ -1,4 +1,6 @@
 class ChangeFormAttachmentsTypeNull < ActiveRecord::Migration
+  safety_assured
+
   def change
     FormAttachment.update_all(type: 'Preneeds::PreneedAttachment')
     change_column :form_attachments, :type, :string, null: false
