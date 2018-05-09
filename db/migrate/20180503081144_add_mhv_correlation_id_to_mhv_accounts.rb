0ad737aadbcc11e5fdeace63cbe37ca4c76a0782
Kam Karshenas <kam@adhocteam.us>
2018-05-08 17:04:14 -0700

    fix comment per code review

--- db/migrate/20180503081144_add_mhv_correlation_id_to_mhv_accounts.rb
+++ db/migrate/20180503081144_add_mhv_correlation_id_to_mhv_accounts.rb
@@ -1,14 +1,14 @@
 class AddMhvCorrelationIdToMhvAccounts < ActiveRecord::Migration
   def change
-    # accounts can be disabled, new ones could be created, as a log
+    # accounts can be disabled, new ones could be created
     # it is entirely feasible to have more than one mhv account created by vets.gov
     # if a previously created or upgraded one has since been disabled.
     # it's also entirely feasible for MHV to recycle existing mhv_correlation_ids
-    # so no uniquness constraints can exist here.
+    # so no uniqueness constraints can exist here.
 
     # first we will drop the index
     remove_index :mhv_accounts, column: :user_uuid
-    # remove uniquness constraint
+    # remove uniqueness constraint
     change_column :mhv_accounts, :user_uuid, :string, unique: false
     # add the new column; we would love to put null: false here but obviously thats not possible
     add_column :mhv_accounts, :mhv_correlation_id, :string, unique: false, null: true
