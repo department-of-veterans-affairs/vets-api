8b487ae510d8c579206ca84738d9de4e3ea78d82
Ryan Baker <ryan.baker@adhocteam.us>
2018-05-10 10:20:28 -0700

    Rubocop fixes

--- app/swagger/schemas/vet360/contact_information.rb
+++ app/swagger/schemas/vet360/contact_information.rb
@@ -16,25 +16,25 @@ module Swagger
                   property :id, type: :integer, example: 323
                   property :email_address, type: :string, example: 'john@example.com'
                   property :created_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_end_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_start_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :source_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :updated_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                 end
 
                 property :residential_address, type: :object do
@@ -43,9 +43,9 @@ module Swagger
                   property :address_line3, type: :string
                   property :address_pou, type: :string, example: ::Vet360::Models::Address::RESIDENCE
                   property :address_type,
-                    type: :string,
-                    enum: ::Vet360::Models::Address::ADDRESS_TYPES,
-                    example: ::Vet360::Models::Address::DOMESTIC
+                           type: :string,
+                           enum: ::Vet360::Models::Address::ADDRESS_TYPES,
+                           example: ::Vet360::Models::Address::DOMESTIC
                   property :city, type: :string, example: 'Fulton'
                   property :country, type: :string, example: 'United States of America'
                   property :country_code_iso2, type: :string, example: 'US'
@@ -58,25 +58,25 @@ module Swagger
                   property :zip_code, type: :string, example: '97062'
                   property :zip_code_suffix, type: :string, example: '1234'
                   property :created_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_end_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_start_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :source_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :updated_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                 end
 
                 property :mailing_address, type: :object do
@@ -84,12 +84,12 @@ module Swagger
                   property :address_line2, type: :string
                   property :address_line3, type: :string
                   property :address_pou,
-                    type: :string,
-                    example: ::Vet360::Models::Address::CORRESPONDENCE
+                           type: :string,
+                           example: ::Vet360::Models::Address::CORRESPONDENCE
                   property :address_type,
-                    type: :string,
-                    enum: ::Vet360::Models::Address::ADDRESS_TYPES,
-                    example: ::Vet360::Models::Address::DOMESTIC
+                           type: :string,
+                           enum: ::Vet360::Models::Address::ADDRESS_TYPES,
+                           example: ::Vet360::Models::Address::DOMESTIC
                   property :city, type: :string, example: 'Fulton'
                   property :country, type: :string, example: 'United States of America'
                   property :country_code_iso2, type: :string, example: 'US'
@@ -102,25 +102,25 @@ module Swagger
                   property :zip_code, type: :string, example: '97062'
                   property :zip_code_suffix, type: :string, example: '1234'
                   property :created_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_end_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_start_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :source_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :updated_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                 end
 
                 property :mobile_phone, type: :object do
@@ -136,25 +136,25 @@ module Swagger
                   property :phone_number, type: :string, example: '5551234'
                   property :phone_type, type: :string, example: ::Vet360::Models::Telephone::MOBILE
                   property :created_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_end_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_start_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :source_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :updated_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                 end
 
                 property :home_phone, type: :object do
@@ -170,25 +170,25 @@ module Swagger
                   property :phone_number, type: :string, example: '5551234'
                   property :phone_type, type: :string, example: ::Vet360::Models::Telephone::HOME
                   property :created_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_end_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_start_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :source_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :updated_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                 end
 
                 property :work_phone, type: :object do
@@ -204,25 +204,25 @@ module Swagger
                   property :phone_number, type: :string, example: '5551234'
                   property :phone_type, type: :string, example: ::Vet360::Models::Telephone::WORK
                   property :created_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_end_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_start_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :source_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :updated_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                 end
 
                 property :temporary_phone, type: :object do
@@ -238,25 +238,25 @@ module Swagger
                   property :phone_number, type: :string, example: '5551234'
                   property :phone_type, type: :string, example: ::Vet360::Models::Telephone::TEMPORARY
                   property :created_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_end_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_start_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :source_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :updated_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                 end
 
                 property :fax_number, type: :object do
@@ -272,25 +272,25 @@ module Swagger
                   property :phone_number, type: :string, example: '5551234'
                   property :phone_type, type: :string, example: ::Vet360::Models::Telephone::FAX
                   property :created_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_end_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :effective_start_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :source_date,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                   property :updated_at,
-                    type: :string,
-                    format: 'date-time',
-                    example: '2018-04-21T20:09:50Z'
+                           type: :string,
+                           format: 'date-time',
+                           example: '2018-04-21T20:09:50Z'
                 end
               end
             end
