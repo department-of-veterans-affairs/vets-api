a5620e0e4e2a5105ff600455a55c473b435af827
egarreau <ergarreau@gmail.com>
2018-05-10 10:31:13 -0500

    spec cleanup

--- spec/request/upload_ancillary_form_spec.rb
+++ spec/request/upload_ancillary_form_spec.rb
@@ -8,11 +8,9 @@ RSpec.describe 'Upload ancillary form', type: :request do
   describe 'Post /v0/upload_ancillary_form' do
     context 'with a valid response' do
       it 'should return a 200 and an upload guid' do
-        VCR.use_cassette('upload_ancillary_form') do
-          post '/v0/upload_ancillary_form', JSON.parse('{"ancillary_form_attachment": {"file_data": "filename"}}')
-          expect(response).to have_http_status(:ok)
-          expect(response).to match_response_schema('upload_ancillary_form')
-        end
+        post '/v0/upload_ancillary_form', JSON.parse('{"ancillary_form_attachment": {"file_data": "filename"}}')
+        expect(response).to have_http_status(:ok)
+        expect(response).to match_response_schema('upload_ancillary_form')
       end
     end
   end
