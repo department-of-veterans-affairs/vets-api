3e13ad4e5b94924ce1a1c2c975b8a933c511b6d4
Johnny Holton <johnny@holton.co>
2018-05-10 16:34:32 -0400

    fix DependentsApplicationsController namespacing

--- app/controllers/v0/dependents_applications_controller.rb
+++ app/controllers/v0/dependents_applications_controller.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-class V0::DependentsApplicationsController < ApplicationController
-  FORM_ID = '21-686C'
-  skip_before_action(:authenticate)
-  before_action(:tag_rainbows)
+module V0
+  class DependentsApplicationsController < ApplicationController
+    FORM_ID = '21-686C'
+    skip_before_action(:authenticate)
+    before_action(:tag_rainbows)
 
-  def create
-    form = JSON.parse(params[:form])
-    validate!(form)
-    render json: {}
-  end
+    def create
+      form = JSON.parse(params[:form])
+      validate!(form)
+      render json: {}
+    end
 
-  private
+    private
 
-  def validate!(form)
-    validation_errors = JSON::Validator.fully_validate(
-      VetsJsonSchema::SCHEMAS[FORM_ID],
-      form, validate_schema: true
-    )
+    def validate!(form)
+      validation_errors = JSON::Validator.fully_validate(
+        VetsJsonSchema::SCHEMAS[FORM_ID],
+        form, validate_schema: true
+      )
 
-    raise Common::Exceptions::SchemaValidationErrors, validation_errors if validation_errors.present?
+      raise Common::Exceptions::SchemaValidationErrors, validation_errors if validation_errors.present?
+    end
   end
 end
