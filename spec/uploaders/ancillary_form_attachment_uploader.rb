bc7f7946ad3d77ef97685c0691f5fdbae73b4c8d
egarreau <ergarreau@gmail.com>
2018-05-14 13:46:09 -0500

    rubocop cleanup

--- spec/uploaders/ancillary_form_attachment_uploader.rb
+++ spec/uploaders/ancillary_form_attachment_uploader.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe AncillaryFormAttachmentUploader do
   let(:guid) { '1234' }
-  subject { described_class.new(guid) } 
+  subject { described_class.new(guid) }
 
   it 'should whitelist image, pdf, and text files' do
     expect(subject.extension_white_list).to eq %w[pdf png gif tiff tif jpeg jpg bmp txt]
@@ -12,10 +12,10 @@ RSpec.describe AncillaryFormAttachmentUploader do
 
   it 'should return a store directory containing guid' do
     expect(subject.store_dir).to eq "disability_compensation_supporting_form/#{guid}"
-  end 
+  end
 
   it 'should throw an error if no guid is given' do
     blank_uploader = described_class.new(nil)
-    expect{ blank_uploader.store_dir }.to raise_error RuntimeError
+    expect { blank_uploader.store_dir }.to raise_error RuntimeError
   end
 end
