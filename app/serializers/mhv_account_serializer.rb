ef2392239afb82e2171e188e22409673412e9a57
Kam Karshenas <kam@adhocteam.us>
2018-05-14 16:08:09 -0700

    pushing up changes to the serializer

--- app/serializers/mhv_account_serializer.rb
+++ app/serializers/mhv_account_serializer.rb
@@ -2,4 +2,9 @@
 
 class MhvAccountSerializer < ActiveModel::Serializer
   attribute :account_state
+  attribute :account_level
+
+  def terms_and_conditions_accepted
+    object.terms_and_conditions_accepted?
+  end
 end
