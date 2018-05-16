b6c4f81a16cb5d245f6ddeefd533fb7e555a9d01
Emerson Loustau <e@thesecretaddress.com>
2018-05-16 15:49:08 -0700

    encrypted_

--- db/migrate/20180514191109_add_metadata_to_async_transaction.rb
+++
@@ -1,6 +0,0 @@
-class AddMetadataToAsyncTransaction < ActiveRecord::Migration
-  def change
-    add_column :async_transactions, :metadata, :string, unique: false, null: true
-    add_column :async_transactions, :metadata_vi, :string, unique: false, null: true
-  end
-end
