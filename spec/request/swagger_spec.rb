d53f17461a8932ff635ffe833d2a6f14ddb3de12
hpjaj <hmlevine@gmail.com>
2018-05-17 15:32:22 -0600

    Updates swagger docs with new 403 response case

--- spec/request/swagger_spec.rb
+++ spec/request/swagger_spec.rb
@@ -1348,6 +1348,24 @@ RSpec.describe 'the API documentation', type: :apivore, order: :defined do
         end
       end
     end
+
+    describe 'when EVSS authorization requirements are not met' do
+      let(:unauthorized_evss_user) { build(:unauthorized_evss_user, :loa3) }
+
+      before do
+        Session.create(uuid: unauthorized_evss_user.uuid, token: token)
+        User.create(unauthorized_evss_user)
+      end
+
+      it 'supports returning a custom 403 Forbidden response', :aggregate_failures do
+        expect(subject).to validate(:get, '/v0/profile/email', 403, auth_options)
+        expect(subject).to validate(:get, '/v0/profile/primary_phone', 403, auth_options)
+        expect(subject).to validate(:get, '/v0/profile/alternate_phone', 403, auth_options)
+        expect(subject).to validate(:post, '/v0/profile/email', 403, auth_options)
+        expect(subject).to validate(:post, '/v0/profile/primary_phone', 403, auth_options)
+        expect(subject).to validate(:post, '/v0/profile/alternate_phone', 403, auth_options)
+      end
+    end
   end
 
   context 'and' do
