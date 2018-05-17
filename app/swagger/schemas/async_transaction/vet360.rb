95993a0290051c90d73cd2afdfad8c5d6bb720fa
Emerson Loustau <e@thesecretaddress.com>
2018-05-17 10:25:22 -0700

    updated test schema

--- app/swagger/schemas/async_transaction/vet360.rb
+++ app/swagger/schemas/async_transaction/vet360.rb
@@ -65,35 +65,6 @@ module Swagger
             end
           end
         end
-
-        swagger_schema :AsyncTransactionsVet360 do
-          key :required, [:data]
-          property :data,
-                   type: :array do
-            items type: :object do
-              key :required, [:attributes]
-              property :attributes, type: :object do
-                property :transaction_status, type: :string, enum:
-                  %w[
-                    REJECTED
-                    RECEIVED
-                    RECEIVED_ERROR_QUEUE
-                    RECEIVED_DEAD_LETTER_QUEUE
-                    COMPLETED_SUCCESS
-                    COMPLETED_NO_CHANGES_DETECTED
-                    COMPLETED_FAILURE
-                  ], example: 'RECEIVED'
-                property :transaction_id, type: :string, example: '786efe0e-fd20-4da2-9019-0c00540dba4d'
-                property :type, type: :string, enum:
-                  %w[
-                    AsyncTransaction::Vet360::AddressTransaction
-                    AsyncTransaction::Vet360::EmailTransaction
-                    AsyncTransaction::Vet360::TelephoneTransaction
-                  ], example: 'AsyncTransaction::Vet360::EmailTransaction'
-              end
-            end
-          end
-        end
       end
     end
   end
