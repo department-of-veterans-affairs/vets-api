949d317879f17b94af98240b5c8c9f7448501116
Kam Karshenas <kam@adhocteam.us>
2018-05-16 21:40:05 -0700

    test coverage

--- app/serializers/mhv_account_serializer.rb
+++ app/serializers/mhv_account_serializer.rb
@@ -3,8 +3,4 @@
 class MhvAccountSerializer < ActiveModel::Serializer
   attribute :account_state
   attribute :account_level
-
-  def terms_and_conditions_accepted
-    object.terms_and_conditions_accepted?
-  end
 end
