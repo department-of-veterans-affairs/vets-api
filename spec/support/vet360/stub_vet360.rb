8b434ba1a0e6bc91b77923b27133c6f349ed141a
Ryan Baker <ryan.baker@adhocteam.us>
2018-05-21 15:49:36 -0700

    Update existing specs to use stubbed data

--- spec/support/vet360/stub_vet360.rb
+++ spec/support/vet360/stub_vet360.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 def stub_vet360(person = nil)
+  person ||= build(:person,
+                   addresses:  [build(:vet360_address, id: 123)],
+                   emails:     [build(:email, id: 456)],
+                   telephones: [build(:telephone, :home, id: 789)])
+
   allow_any_instance_of(Vet360::ContactInformation::Service).to receive(:safe_get_person).and_return(
     Vet360::ContactInformation::PersonResponse.new(
       200,
-      person: person || build(:person)
+      person: person
     )
   )
 end
