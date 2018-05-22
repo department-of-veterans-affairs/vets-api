117ac89bc01712867c28ef24a26a90f5b41e96f3
Ryan Baker <ryan.baker@adhocteam.us>
2018-05-22 09:45:30 -0700

    Add serializer specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -230,7 +230,7 @@ class User < Common::RedisStore
   end
 
   def vet360_contact_info
-    return nil unless Settings.vet360.contact_information.enabled
+    return nil unless Settings.vet360.contact_information.enabled && vet360_id.present?
     @vet360_contact_info ||= Vet360Redis::ContactInformation.for_user(self)
   end
 
