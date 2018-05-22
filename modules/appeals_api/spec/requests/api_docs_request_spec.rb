3d4ca49d162f75777c70b90b5e79ee037b5c7eab
Edward Paget <edward@cassetteta.pe>
2018-05-22 10:56:28 -0400

    Improve testing for coverage minimum

--- modules/appeals_api/spec/requests/api_docs_request_spec.rb
+++ modules/appeals_api/spec/requests/api_docs_request_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'VBA Document Uploads Documentation Endpoint', type: :request do
   describe '#get /docs/v0/api' do
     it 'should return Open API Spec v3 JSON' do
-      get '/services/vba_documents/docs/v0/api'
+      get '/services/appeals/docs/v0/api'
       expect(response).to have_http_status(:ok)
       JSON.parse(response.body)
     end
