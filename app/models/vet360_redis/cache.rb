3b7f9d25e32fd5f7f4c6089c94cdf6e2f50b4b49
hpjaj <hmlevine@gmail.com>
2018-05-22 18:12:36 -0600

    Yard docs for new cache method

--- app/models/vet360_redis/cache.rb
+++ app/models/vet360_redis/cache.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 module Vet360Redis
   class Cache
+
+    # Invalidates the cache set in Vet360Redis::ContactInformation through
+    # our Common::RedisStore#destroy method.
+    #
+    # @param url [User] The current user
+    #
     def self.invalidate(user)
       user.vet360_contact_info&.destroy
     end
