d8c8858f004e1b50dff3ed4221fc76cd6dd4bf76
Lihan Li <frankieteardrop@gmail.com>
2018-05-22 22:48:26 -0400

    make subclass for delete attach job

---
+++ app/workers/delete_attachment_job.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class DeleteAttachmentJob
+  include Sidekiq::Worker
+
+  sidekiq_options(unique_for: 30.minutes, retry: false)
+
+  EXPIRATION_TIME = 2.months
+
+  def perform
+    Sentry::TagRainbows.tag
+
+    self.class::ATTACHMENT_CLASS.where(
+      'created_at < ?', EXPIRATION_TIME.ago
+    ).where.not(guid: uuids_to_keep).find_each(&:destroy!)
+  end
+
+  def uuids_to_keep
+    uuids = []
+
+    InProgressForm.where(form_id: self.class::FORM_ID).find_each do |in_progress_form|
+      uuids += get_uuids(in_progress_form.data_and_metadata[:form_data])
+    end
+
+    uuids
+  end
+end
