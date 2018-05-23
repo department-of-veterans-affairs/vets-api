ea0a32ed4444bb573082cd9b09e22c5c8dff2101
lihan <frankieteardrop@gmail.com>
2018-05-23 17:42:55 -0400

    vic delete old uploads job bug (#1968)
    * make subclass for delete attach job
    
    * vic delete old uploads spec
    
    * use type field
    
    * lint
    
    * use array.wrap for preneeds
    
    * array.wrap in vic

--- app/workers/preneeds/delete_old_uploads.rb
+++ app/workers/preneeds/delete_old_uploads.rb
@@ -1,31 +1,15 @@
 # frozen_string_literal: true
 
 module Preneeds
-  class DeleteOldUploads
-    include Sidekiq::Worker
+  class DeleteOldUploads < DeleteAttachmentJob
+    ATTACHMENT_CLASSES = %w[Preneeds::PreneedAttachment].freeze
+    FORM_ID = '40-10007'
 
-    sidekiq_options(unique_for: 30.minutes, retry: false)
-
-    EXPIRATION_TIME = 2.months
-
-    def perform
-      Sentry::TagRainbows.tag
-
-      Preneeds::PreneedAttachment.where(
-        'created_at < ?', EXPIRATION_TIME.ago
-      ).where.not(guid: uuids_to_keep).find_each(&:destroy!)
-    end
-
-    def uuids_to_keep
+    def get_uuids(form_data)
       uuids = []
 
-      InProgressForm.where(form_id: '40-10007').find_each do |in_progress_form|
-        attachments = in_progress_form.data_and_metadata[:form_data]['preneedAttachments']
-        if attachments.present?
-          attachments.each do |attachment|
-            uuids << attachment['confirmationCode']
-          end
-        end
+      Array.wrap(form_data['preneedAttachments']).each do |attachment|
+        uuids << attachment['confirmationCode']
       end
 
       uuids
