de9c40129dca1301692c6f63333ba989024dd2bb
hpjaj <hmlevine@gmail.com>
2018-05-22 18:12:58 -0600

    Correct Yard docs typo

--- lib/vet360/contact_information/service.rb
+++ lib/vet360/contact_information/service.rb
@@ -26,21 +26,21 @@ module Vet360
       end
 
       # POSTs a new address to the vet360 API
-      # @params address [Vet360::Models::Address] the address to create
+      # @param address [Vet360::Models::Address] the address to create
       # @returns [Vet360::ContactInformation::AddressTransactionResponse] response wrapper around an transaction object
       def post_address(address)
         post_or_put_data(:post, address, 'addresses', AddressTransactionResponse)
       end
 
       # PUTs an updated address to the vet360 API
-      # @params address [Vet360::Models::Address] the address to update
+      # @param address [Vet360::Models::Address] the address to update
       # @returns [Vet360::ContactInformation::AddressTransactionResponse] response wrapper around a transaction object
       def put_address(address)
         post_or_put_data(:put, address, 'addresses', AddressTransactionResponse)
       end
 
       # GET's the status of an address transaction from the Vet360 api
-      # @params transaction_id [int] the transaction_id to check
+      # @param transaction_id [int] the transaction_id to check
       # @returns [Vet360::ContactInformation::EmailTransactionResponse] response wrapper around a transaction object
       def get_address_transaction_status(transaction_id)
         route = "#{@user.vet360_id}/addresses/status/#{transaction_id}"
@@ -48,21 +48,21 @@ module Vet360
       end
 
       # POSTs a new address to the vet360 API
-      # @params email [Vet360::Models::Email] the email to create
+      # @param email [Vet360::Models::Email] the email to create
       # @returns [Vet360::ContactInformation::EmailTransactionResponse] response wrapper around an transaction object
       def post_email(email)
         post_or_put_data(:post, email, 'emails', EmailTransactionResponse)
       end
 
       # PUTs an updated address to the vet360 API
-      # @params email [Vet360::Models::Email] the email to update
+      # @param email [Vet360::Models::Email] the email to update
       # @returns [Vet360::ContactInformation::EmailTransactionResponse] response wrapper around a transaction object
       def put_email(email)
         post_or_put_data(:put, email, 'emails', EmailTransactionResponse)
       end
 
       # GET's the status of an email transaction from the Vet360 api
-      # @params transaction_id [int] the transaction_id to check
+      # @param transaction_id [int] the transaction_id to check
       # @returns [Vet360::ContactInformation::EmailTransactionResponse] response wrapper around a transaction object
       def get_email_transaction_status(transaction_id)
         route = "#{@user.vet360_id}/emails/status/#{transaction_id}"
@@ -70,21 +70,21 @@ module Vet360
       end
 
       # POSTs a new telephone to the vet360 API
-      # @params telephone [Vet360::Models::Telephone] the telephone to create
+      # @param telephone [Vet360::Models::Telephone] the telephone to create
       # @returns [Vet360::ContactInformation::TelephoneUpdateResponse] response wrapper around a transaction object
       def post_telephone(telephone)
         post_or_put_data(:post, telephone, 'telephones', TelephoneTransactionResponse)
       end
 
       # PUTs an updated telephone to the vet360 API
-      # @params telephone [Vet360::Models::Telephone] the telephone to update
+      # @param telephone [Vet360::Models::Telephone] the telephone to update
       # @returns [Vet360::ContactInformation::TelephoneUpdateResponse] response wrapper around a transaction object
       def put_telephone(telephone)
         post_or_put_data(:put, telephone, 'telephones', TelephoneTransactionResponse)
       end
 
       # GET's the status of a telephone transaction from the Vet360 api
-      # @params transaction_id [int] the transaction_id to check
+      # @param transaction_id [int] the transaction_id to check
       # @returns [Vet360::ContactInformation::TelephoneTransactionResponse] response wrapper around a transaction object
       def get_telephone_transaction_status(transaction_id)
         route = "#{@user.vet360_id}/telephones/status/#{transaction_id}"
