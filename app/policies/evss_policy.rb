98fae58a8265b0411a7eb3123007d3dc44a2ff85
hpjaj <hmlevine@gmail.com>
2018-05-24 08:00:07 -0600

    Logging to determine user success/failure access rate

--- app/policies/evss_policy.rb
+++ app/policies/evss_policy.rb
@@ -5,19 +5,23 @@ EVSSPolicy = Struct.new(:user, :evss) do
 
   def access?
     if user.edipi.present? && user.ssn.present? && user.participant_id.present?
+      log_message_to_sentry('EVSS Pundit success log', :info) if user.loa3?
+
       true
     else
-      log_message_to_sentry(
-        'EVSS Pundit Policy bug',
-        :info,
-        {
-          edipi_present: user.edipi.present?,
-          ssn_present: user.ssn.present?,
-          participant_id_present: user.participant_id.present?,
-          user_loa: user&.loa
-        },
-        profile: 'pciu_profile'
-      )
+      if user.loa3?
+        log_message_to_sentry(
+          'EVSS Pundit failure log',
+          :info,
+          {
+            edipi_present: user.edipi.present?,
+            ssn_present: user.ssn.present?,
+            participant_id_present: user.participant_id.present?,
+            user_loa: user&.loa
+          },
+          profile: 'pciu_profile'
+        )
+      end
 
       false
     end
