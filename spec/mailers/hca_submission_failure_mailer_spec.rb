88dd52bed0098597bcd36a71382c149d3962467a
Johnny Holton <johnny@holton.co>
2018-05-29 16:09:07 -0400

    revert HCA submission failure email address. The new one is not ready yet.

--- spec/mailers/hca_submission_failure_mailer_spec.rb
+++ spec/mailers/hca_submission_failure_mailer_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe HCASubmissionFailureMailer, type: [:mailer] do
     it 'should include all info' do
       expect(subject.subject).to eq("We didn't receive your application")
       expect(subject.to).to eq(['foo@example.com'])
-      expect(subject.from).to eq(['va-notifications@public.govdelivery.com'])
       expect(subject.body.raw_source).to include(
         "We're sorry. Your application didn't go through because something went wrong on our end."
       )
