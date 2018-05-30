da1278eab7cae56baadf4dc435c2fbc502e45528
Lihan Li <frankieteardrop@gmail.com>
2018-05-30 19:30:19 -0400

    finish authorize method

--- app/controllers/v0/id_card_attributes_controller.rb
+++ app/controllers/v0/id_card_attributes_controller.rb
@@ -19,28 +19,30 @@ module V0
 
     private
 
+    def skip_sentry_exception_types
+      super + [Common::Exceptions::Forbidden, ::VIC::IDCardAttributeError]
+    end
+
     def authorize
       # TODO: Clean up this method, particularly around need to blanket rescue from
       # VeteranStatus method
       raise Common::Exceptions::Forbidden, detail: 'You do not have access to ID card attributes' unless
         current_user.loa3?
+      raise ::VIC::IDCardAttributeError, ::VIC::IDCardAttributeError::VIC002 if current_user.edipi.blank?
 
       title38_status = begin
         current_user.veteran_status&.title38_status
       rescue EMISRedis::VeteranStatus::RecordNotFound
         nil
+      rescue StandardError => e
+        log_exception_to_sentry(e)
+        raise ::VIC::IDCardAttributeError, ::VIC::IDCardAttributeError::VIC010
       end
 
-      raise ::VIC::IDCardAttributeError, status: 403, code: 'VIC002', detail: 'No EDIPI or not found in eMIS' if current_user.edipi.blank? || title38_status.blank?
-      begin
-        unless current_user.can_access_id_card?
-          raise ::VIC::IDCardAttributeError, status: 403, code: 'VIC003',
-                                             detail: 'Not eligible for a Veteran ID Card'
-        end
-      rescue
-        raise ::VIC::IDCardAttributeError, status: 403, code: 'VIC010',
-                                           detail: 'Could not verify Veteran status'
-      end
+      raise ::VIC::IDCardAttributeError, ::VIC::IDCardAttributeError::VIC002 if title38_status.blank?
+      raise ::VIC::IDCardAttributeError, ::VIC::IDCardAttributeError::NOT_ELIGIBLE.merge(
+        code: "VIC#{title38_status}"
+      ) unless current_user.can_access_id_card?
     end
   end
 end
