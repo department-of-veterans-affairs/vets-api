adf2458ba5d2b52dfc5c88ad71db68a644f0cfd1
hpjaj <hmlevine@gmail.com>
2018-06-05 11:51:30 -0600

    Method now applies when user is present or not
    When init_vet360_id is used for a rake task, a user will not be present, and we will be passing in individual ICNs.
    
    When init_vet360_id is used for a current user, we get the ICN dynamically from the icn_with_aaid, and do not pass in an ICN.
    
    This method now accounts for both use cases.
    
    In either case, this is an asynchronous transaction on the Vet360 side, so the response is a transaction, not an actual person object.

--- lib/vet360/person/service.rb
+++ lib/vet360/person/service.rb
@@ -8,16 +8,16 @@ module Vet360
     class Service < Vet360::Service
       include Common::Client::Monitoring
 
-      OID = MVI::Responses::IdParser::CORRELATION_ROOT_ID
       AAID = MVI::Responses::IdParser::VET360_ASSIGNING_AUTHORITY_ID
+      OID  = MVI::Responses::IdParser::CORRELATION_ROOT_ID
 
       configuration Vet360::ContactInformation::Configuration
 
-      def init_vet360_id(icn)
+      def init_vet360_id(icn = nil)
         with_monitoring do
-          raw_response = perform(:post, encoded_uri_for(icn), empty_body)
+          raw_response = perform(:post, encode_uri!(icn), empty_body)
 
-          Vet360::ContactInformation::PersonResponse.from(raw_response)
+          Vet360::ContactInformation::PersonTransactionResponse.from(raw_response)
         end
       rescue StandardError => e
         handle_error(e)
@@ -26,10 +26,20 @@ module Vet360
       private
 
       # rubocop:disable Lint/UriEscapeUnescape
-      def encoded_uri_for(icn, oid: OID, aaid: AAID)
-        URI.encode("#{oid}/#{icn}#{aaid}")
+      def encode_uri!(icn)
+        URI.encode("#{OID}/#{build_icn_with_aaid!(icn)}")
       end
       # rubocop:enable Lint/UriEscapeUnescape
+
+      def build_icn_with_aaid!(icn)
+        if icn.present?
+          "#{icn}#{AAID}"
+        elsif @user&.icn_with_aaid.present?
+          @user.icn_with_aaid
+        else
+          raise 'User does not have an ICN with an Assigning Authority ID'
+        end
+      end
 
       def empty_body
         {
