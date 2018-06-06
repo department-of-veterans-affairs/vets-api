69f050cbe7b458fb26ecf988f8061fac431ce71a
Ryan Baker <ryan.baker@adhocteam.us>
2018-06-06 15:58:38 -0700

    Allow key traversal

--- lib/vet360/reference_data/service.rb
+++ lib/vet360/reference_data/service.rb
@@ -9,21 +9,25 @@ module Vet360
 
       configuration Vet360::ReferenceData::Configuration
 
+      # GETs a the list of valid countries from Vet360
+      # @return [Vet360::ReferenceData::Response] response wrapper
       def countries
-        get_reference_data('countries')
+        get_reference_data('countries', 'country_list')
       end
 
       def states
-        get_reference_data('states')
+        get_reference_data('states', 'state_list')
       end
 
       def zipcodes
-        get_reference_data('zipCode5')
+        get_reference_data('zipCode5', 'zip_code5_list')
       end
 
-      def get_reference_data(path)
-        response = perform(:get, path)
-        ReferenceDataResponse.from(response)
+      private
+
+      def get_reference_data(route, key)
+        response = perform(:get, route)
+        Vet360::ReferenceData::Response.from(response, key)
       rescue StandardError => e
         handle_error(e)
       end
