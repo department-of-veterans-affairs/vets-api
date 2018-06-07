9156026659dbc1e78cf8c8d0e750c439234bf03b
Ryan Baker <ryan.baker@adhocteam.us>
2018-06-07 15:19:36 -0700

    Implement cache for reference data

---
+++ app/models/vet360_redis/reference_data.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'common/models/redis_store'
+require 'common/models/concerns/cache_aside'
+
+module Vet360Redis
+  # Facade for Vet360::ReferenceData::Service. The user_serializer delegates
+  # to this class through the User model.
+  #
+  # When a person is requested from the serializer, it returns either a cached
+  # response in Redis or from the Vet360::ContactInformation::Service.
+  #
+  class ReferenceData < Common::RedisStore
+    include Common::CacheAside
+
+    # Redis settings for ttl and namespacing reside in config/redis.yml
+    redis_config_key :vet360_reference_data_response
+
+    def countries
+      response_from_redis_or_service(:countries).reference_data
+    end
+
+    def states
+      response_from_redis_or_service(:states).reference_data
+    end
+
+    def zipcodes
+      response_from_redis_or_service(:zipcodes).reference_data
+    end
+
+    private
+
+    def response_from_redis_or_service(endpoint)
+      do_cached_with(key: "vet360_reference_data:#{endpoint}") do
+        reference_data_service.public_send(endpoint)
+      end
+    end
+
+    def reference_data_service
+      @service ||= Vet360::ReferenceData::Service.new(nil)
+    end
+  end
+end
