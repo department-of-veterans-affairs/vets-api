4d6865c75dd13400c2cf84a38b0efc3174a36561
Johnny Holton <johnny@holton.co>
2018-06-07 12:32:34 -0400

    spec improvement

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -5,22 +5,19 @@ shared_examples_for 'a controller that does not log 404 to Sentry' do
     allow_any_instance_of(described_class).to receive(:authenticate) do
       raise Common::Exceptions::RecordNotFound, 'some_id'
     end
-
-    controller_klass = described_class
-    Rails.application.routes.draw do
-      get '/fake_route' => "#{controller_klass.to_s.underscore.gsub('_controller', '')}#authenticate"
-    end
   end
 
   it 'should not log 404 to sentry' do
-    allow_any_instance_of(ApplicationController).to receive(:log_exception_to_sentry) { raise }
-
-    get(controller.present? ? :authenticate : '/fake_route')
-    expect(response.code).to eq('404')
-  end
-
-  after do
-    Rails.application.reload_routes!
+    with_routing do |routes|
+      @routes = routes
+      controller_klass = described_class
+      routes.draw do
+        get '/fake_route' => "#{controller_klass.to_s.underscore.gsub('_controller', '')}#authenticate"
+      end
+      allow_any_instance_of(ApplicationController).to receive(:log_exception_to_sentry) { raise }
+      get(controller.present? ? :authenticate : '/fake_route')
+      expect(response.code).to eq('404')
+    end
   end
 end
 
