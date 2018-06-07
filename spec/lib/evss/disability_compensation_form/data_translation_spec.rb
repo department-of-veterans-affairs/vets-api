43894b9182665a3867920e1416109457b18a08f6
Silvio Luthi <silvio@adhocteam.us>
2018-06-07 16:13:22 -0700

    Update lib test to account for ppiu service call

--- spec/lib/evss/disability_compensation_form/data_translation_spec.rb
+++ spec/lib/evss/disability_compensation_form/data_translation_spec.rb
@@ -6,12 +6,19 @@ require 'evss/disability_compensation_form/data_translation'
 describe EVSS::DisabilityCompensationForm::DataTranslation do
   let(:form_content) { JSON.parse(File.read 'spec/support/disability_compensation_form/front_end_submission.json') }
   let(:evss_json) { File.read 'spec/support/disability_compensation_form/evss_submission.json' }
+  let(:user) { build(:disabilities_compensation_user) }
 
-  subject { described_class.new(form_content) }
+  before do
+    User.create(user)
+  end
+
+  subject { described_class.new(user, form_content) }
 
   describe '#convert' do
     it 'should return correctly formatted json to send to EVSS' do
-      expect(subject.convert).to eq evss_json
+      VCR.use_cassette('evss/ppiu/payment_information') do
+        expect(subject.convert).to eq evss_json
+      end
     end
   end
 end
