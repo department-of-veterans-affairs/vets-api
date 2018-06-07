26a720f826e2c8d5cfb40f36e5ec0f341d9c6317
Ryan Baker <rlbaker@users.noreply.github.com>
2018-06-07 15:19:17 -0700

    Vet360 Reference Data Service (#2010)
    * Initial build of services
    
    * Prep first cassette
    
    * Implement first cassette
    
    * Add the rest of the reference data service
    
    * Allow key traversal
    
    * Finish documentation
    
    * Add more specs

---
+++ spec/lib/vet360/reference_data/service_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Vet360::ReferenceData::Service, skip_vet360: true do
+  let(:user) { build(:user, :loa3) }
+  subject { described_class.new(user) }
+
+  before { Timecop.freeze('2018-04-09T17:52:03Z') }
+  after  { Timecop.return }
+
+  describe '#countries' do
+    context 'when successful' do
+      it 'returns a status of 200', :aggregate_failures do
+        VCR.use_cassette('vet360/reference_data/countries', VCR::MATCH_EVERYTHING) do
+          response = subject.countries
+
+          expect(response).to be_ok
+          expect(response.reference_data).to be_a(Array)
+
+          data = response.reference_data.first
+          expect(data).to have_key('country_name')
+          expect(data).to have_key('country_code_iso2')
+          expect(data).to have_key('country_code_iso3')
+          expect(data).to have_key('country_code_fips')
+        end
+      end
+    end
+  end
+
+  describe '#states' do
+    context 'when successful' do
+      it 'returns a status of 200', :aggregate_failures do
+        VCR.use_cassette('vet360/reference_data/states', VCR::MATCH_EVERYTHING) do
+          response = subject.states
+
+          expect(response).to be_ok
+          expect(response.reference_data).to be_a(Array)
+
+          data = response.reference_data.first
+          expect(data).to have_key('state_name')
+          expect(data).to have_key('state_code')
+        end
+      end
+    end
+  end
+
+  describe '#zipcodes' do
+    context 'when successful' do
+      it 'returns a status of 200', :aggregate_failures do
+        VCR.use_cassette('vet360/reference_data/zipcodes', VCR::MATCH_EVERYTHING) do
+          response = subject.zipcodes
+
+          expect(response).to be_ok
+          expect(response.reference_data).to be_a(Array)
+
+          data = response.reference_data.first
+          expect(data).to have_key('zip_code')
+        end
+      end
+    end
+  end
+end
