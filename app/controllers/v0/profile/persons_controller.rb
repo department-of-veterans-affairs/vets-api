710ccb91602a2dc1a2eef759c489b083ebeac6d5
hpjaj <hmlevine@gmail.com>
2018-06-08 09:54:15 -0600

    Adds new endpoint to swagger docs and specs

--- app/controllers/v0/profile/persons_controller.rb
+++ app/controllers/v0/profile/persons_controller.rb
@@ -3,15 +3,19 @@
 module V0
   module Profile
     class PersonsController < ApplicationController
-      include Vet360::Writeable
-
-      after_action :invalidate_cache
+      after_action :invalidate_mvi_cache
 
       def initialize_vet360_id
         response    = Vet360::Person::Service.new(@current_user).init_vet360_id
         transaction = AsyncTransaction::Vet360::InitializePersonTransaction.start(@current_user, response)
 
         render json: transaction, serializer: AsyncTransaction::BaseSerializer
+      end
+
+      private
+
+      def invalidate_mvi_cache
+        @current_user.mvi&.destroy
       end
     end
   end
