2e57e6deaddb34cbc3764a77c2fce7b7d2000039
hpjaj <hmlevine@gmail.com>
2018-06-08 08:30:06 -0600

    Adds spec support for VCR::MATCH_EVERYTHING

--- spec/request/vet360/person_request_spec.rb
+++ spec/request/vet360/person_request_spec.rb
@@ -10,21 +10,32 @@ RSpec.describe 'person', type: :request do
   let(:user) { build(:user_with_suffix, :loa3) }
 
   before do
+    Timecop.freeze('2018-04-09T17:52:03Z')
     Session.create(uuid: user.uuid, token: token)
     User.create(user)
   end
 
+  after { Timecop.return }
+
   describe 'POST /v0/profile/initialize_vet360_id' do
+    let(:empty_body) do
+      {
+        bio: {
+          sourceDate: Time.zone.now.iso8601
+        }
+      }.to_json
+    end
+
     before do
       allow_any_instance_of(User).to receive(:vet360_id).and_return(nil)
     end
 
     context 'with a user that has an icn_with_aaid' do
       it 'should match the transaction response schema', :aggregate_failures do
-        VCR.use_cassette('vet360/person/init_vet360_id_success') do
+        VCR.use_cassette('vet360/person/init_vet360_id_success', VCR::MATCH_EVERYTHING) do
           post(
             '/v0/profile/initialize_vet360_id',
-            {},
+            empty_body,
             auth_header.update(
               'Content-Type' => 'application/json', 'Accept' => 'application/json'
             )
@@ -36,11 +47,11 @@ RSpec.describe 'person', type: :request do
       end
 
       it 'should create a new AsyncTransaction::Vet360::InitializePersonTransaction', :aggregate_failures do
-        VCR.use_cassette('vet360/person/init_vet360_id_success') do
+        VCR.use_cassette('vet360/person/init_vet360_id_success', VCR::MATCH_EVERYTHING) do
           expect do
             post(
               '/v0/profile/initialize_vet360_id',
-              {},
+              empty_body,
               auth_header.update(
                 'Content-Type' => 'application/json', 'Accept' => 'application/json'
               )
@@ -52,10 +63,10 @@ RSpec.describe 'person', type: :request do
 
     context 'with an error response' do
       it 'should match the transaction response schema', :aggregate_failures do
-        VCR.use_cassette('vet360/person/init_vet360_id_status_400') do
+        VCR.use_cassette('vet360/person/init_vet360_id_status_400', VCR::MATCH_EVERYTHING) do
           post(
             '/v0/profile/initialize_vet360_id',
-            {},
+            empty_body,
             auth_header.update(
               'Content-Type' => 'application/json', 'Accept' => 'application/json'
             )
