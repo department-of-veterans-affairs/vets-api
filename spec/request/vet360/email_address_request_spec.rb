032e909ae2c42b9db76931c1ebf8f7fe813125d3
Emerson Loustau <e@thesecretaddress.com>
2018-06-08 15:59:45 -0700

    address and email DELETE

--- spec/request/vet360/email_address_request_spec.rb
+++ spec/request/vet360/email_address_request_spec.rb
@@ -10,8 +10,13 @@ RSpec.describe 'email_address', type: :request do
   let(:user) { build(:user, :loa3) }
 
   before do
+    Timecop.freeze(Time.zone.local(2018, 6, 6, 15, 35, 55))
     Session.create(uuid: user.uuid, token: token)
     User.create(user)
+  end
+
+  after do
+    Timecop.return
   end
 
   describe 'POST /v0/profile/email_addresses' do
@@ -187,6 +192,35 @@ RSpec.describe 'email_address', type: :request do
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response).to match_response_schema('errors')
         expect(errors_for(response)).to include "email-address - can't be blank"
+      end
+    end
+  end
+
+  describe 'DELETE /v0/profile/email_addresses' do
+    let(:email) do
+      build(:email, vet360_id: user.vet360_id)
+    end
+
+    before do
+      allow_any_instance_of(User).to receive(:icn).and_return('64762895576664260')
+      email.id = 42
+    end
+
+    context 'with a 200 response from the service' do
+      it 'should match the transaction response schema', aggregate_failures: true, focus: true do
+        VCR.use_cassette('vet360/contact_information/delete_email_success', VCR::MATCH_EVERYTHING) do
+          # The cassette we're using includes the effectiveEndDate in the body.
+          # So this test will not pass if it's missing
+          delete(
+            '/v0/profile/email_addresses',
+            email.to_json,
+            auth_header.update(
+              'Content-Type' => 'application/json', 'Accept' => 'application/json'
+            )
+          )
+          expect(response).to have_http_status(:ok)
+          expect(response).to match_response_schema('vet360/transaction_response')
+        end
       end
     end
   end
