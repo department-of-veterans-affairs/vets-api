aad304441393f4a388b60c73bada0ac134c4b44e
Ryan Baker <ryan.baker@adhocteam.us>
2018-06-08 09:35:03 -0700

    Improve specs

--- spec/models/vet360_redis/reference_data_spec.rb
+++ spec/models/vet360_redis/reference_data_spec.rb
@@ -19,10 +19,12 @@ describe Vet360Redis::ReferenceData do
         end
 
         context 'when there is cached data' do
-          let(:response) { Vet360::ReferenceData::Response.new(200, reference_data: []) }
-
           it 'returns the cached data', :aggregate_failures do
-            subject.cache("vet360_reference_data_#{method}", response)
+            subject.cache(
+              "vet360_reference_data_#{method}",
+              Vet360::ReferenceData::Response.new(200, reference_data: [])
+            )
+
             expect_any_instance_of(Vet360::ReferenceData::Service).to_not receive(method)
             expect(subject.public_send(method)).to be_a(Array)
           end
