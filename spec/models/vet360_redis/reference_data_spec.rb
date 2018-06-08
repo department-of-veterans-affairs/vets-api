354820c218f7e6d83d65b7ac6664c3b073dcc586
Ryan Baker <ryan.baker@adhocteam.us>
2018-06-08 09:17:43 -0700

    Specs for all cached attributes

--- spec/models/vet360_redis/reference_data_spec.rb
+++ spec/models/vet360_redis/reference_data_spec.rb
@@ -4,45 +4,29 @@ require 'rails_helper'
 require 'common/exceptions'
 
 describe Vet360Redis::ReferenceData do
-  let(:reference_data) { Vet360Redis::ReferenceData.new }
-  let(:response) { Vet360::ReferenceData::Response.new(200, reference_data: data) }
+  subject { Vet360Redis::ReferenceData.new }
 
-  describe '#countries' do
-    let(:data) do
-      [
-        {
-          'country_name': 'Afghanistan',
-          'country_code_iso2': '',
-          'country_code_iso3': '',
-          'country_code_fips': ''
-        },
-        {
-          'country_name': 'Afghanistan',
-          'country_code_iso2': '',
-          'country_code_iso3': '',
-          'country_code_fips': ''
-        }
-      ]
-    end
+  describe 'cached attributes' do
+    %i[countries states zipcodes].each do |method|
+      context "##{method}" do
+        context 'when the cache is empty' do
+          it 'should cache and return the response', :aggregate_failures do
+            VCR.use_cassette("vet360/reference_data/#{method}", VCR::MATCH_EVERYTHING) do
+              expect(subject.redis_namespace).to receive(:set).once
+              expect(subject.public_send(method)).to be_a(Array)
+            end
+          end
+        end
 
-    context 'when the cache is empty' do
-      it 'should cache and return the response', :aggregate_failures do
-        allow_any_instance_of(
-          Vet360::ReferenceData::Service
-        ).to receive(:countries).and_return(response)
+        context 'when there is cached data' do
+          let(:response) { Vet360::ReferenceData::Response.new(200, reference_data: []) }
 
-        expect(reference_data.redis_namespace).to receive(:set).once
-        expect_any_instance_of(Vet360::ReferenceData::Service).to receive(:countries).once
-        expect(reference_data.countries).to have_deep_attributes(data)
-      end
-    end
-
-    context 'when there is cached data' do
-      it 'returns the cached data', :aggregate_failures do
-        reference_data.cache('vet360_reference_data_countries', response)
-
-        expect_any_instance_of(Vet360::ReferenceData::Service).to_not receive(:countries)
-        expect(reference_data.countries).to have_deep_attributes(data)
+          it 'returns the cached data', :aggregate_failures do
+            subject.cache("vet360_reference_data_#{method}", response)
+            expect_any_instance_of(Vet360::ReferenceData::Service).to_not receive(method)
+            expect(subject.public_send(method)).to be_a(Array)
+          end
+        end
       end
     end
   end
