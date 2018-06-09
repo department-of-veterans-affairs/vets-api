e7a34d0bc52ff88d37f864c2125acac888afd665
hpjaj <hmlevine@gmail.com>
2018-06-09 12:10:58 -0600

    Spec coverage

--- spec/request/vet360/person_request_spec.rb
+++ spec/request/vet360/person_request_spec.rb
@@ -57,6 +57,8 @@ RSpec.describe 'person', type: :request do
               )
             )
           end.to change { AsyncTransaction::Vet360::InitializePersonTransaction.count }.from(0).to(1)
+
+          expect(AsyncTransaction::Vet360::InitializePersonTransaction.first).to be_valid
         end
       end
 
@@ -76,7 +78,7 @@ RSpec.describe 'person', type: :request do
     end
 
     context 'with an error response' do
-      it 'should match the transaction response schema', :aggregate_failures do
+      it 'should match the errors response schema', :aggregate_failures do
         VCR.use_cassette('vet360/person/init_vet360_id_status_400', VCR::MATCH_EVERYTHING) do
           post(
             '/v0/profile/initialize_vet360_id',
@@ -84,6 +86,50 @@ RSpec.describe 'person', type: :request do
             auth_header.update(
               'Content-Type' => 'application/json', 'Accept' => 'application/json'
             )
+          )
+
+          expect(response).to have_http_status(:bad_request)
+          expect(response).to match_response_schema('errors')
+        end
+      end
+    end
+  end
+
+  describe 'GET /v0/profile/person/status/:transaction_id' do
+    it 'responds with a serialized transaction', :aggregate_failures do
+      transaction = create(
+          :initialize_person_transaction,
+          :init_vet360_id,
+          user_uuid: user.uuid,
+          transaction_id: '786efe0e-fd20-4da2-9019-0c00540dba4d'
+        )
+
+      VCR.use_cassette('vet360/contact_information/person_transaction_status') do
+        get(
+          "/v0/profile/person/status/#{transaction.transaction_id}",
+          nil,
+          auth_header
+        )
+
+        expect(response).to have_http_status(:ok)
+        expect(response).to match_response_schema('vet360/transaction_response')
+      end
+    end
+
+    context 'with an error response' do
+      it 'should match the errors response schema', :aggregate_failures do
+        transaction = create(
+            :initialize_person_transaction,
+            :init_vet360_id,
+            user_uuid: user.uuid,
+            transaction_id: 'd47b3d96-9ddd-42be-ac57-8e564aa38029'
+          )
+
+        VCR.use_cassette('vet360/contact_information/person_transaction_status_error', VCR::MATCH_EVERYTHING) do
+          get(
+            "/v0/profile/person/status/#{transaction.transaction_id}",
+            nil,
+            auth_header
           )
 
           expect(response).to have_http_status(:bad_request)
