ffb809e3eaa4ca8241ba71f4e24028d6fe6499e3
Silvio Luthi <silvio@adhocteam.us>
2018-06-11 11:24:55 -0700

    Fleshout service exceptions module for ppiu; it was missing a  method

--- lib/evss/ppiu/service_exception.rb
+++ lib/evss/ppiu/service_exception.rb
@@ -9,8 +9,23 @@ module EVSS
         exception: 'evss.external_service_unavailable',
         cnp: 'evss.ppiu.unprocessable_entity',
         indicators: 'evss.ppiu.unprocessable_entity',
-        modelvalidators: 'evss.ppiu.unprocessable_entity'
+        modelvalidators: 'evss.ppiu.unprocessable_entity',
+        default: 'common.exceptions.internal_server_error'
       }.freeze
+
+      def errors
+        Array(
+          Common::Exceptions::SerializableError.new(
+            i18n_data.merge(source: 'EVSS::Letters::Service', meta: { messages: @messages })
+          )
+        )
+      end
+
+      private
+
+      def i18n_key
+        @key
+      end
     end
   end
 end
