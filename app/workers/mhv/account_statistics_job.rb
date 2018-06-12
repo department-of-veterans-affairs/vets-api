9392219905dc016702ddfc96afbcf6bda45f5f2c
Derek <derekhall77@gmail.com>
2018-06-12 18:56:09 -0400

    schedule and job to log mhv account statistics

---
+++ app/workers/mhv/account_statistics_job.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module MHV
+  class AccountStatisticsJob
+    include Sidekiq::Worker
+
+    sidekiq_options retry: false
+
+    def perform
+      stats = { accounts_created_count: accounts_created.count,
+                existing_accounts_upgraded_count: existing_accounts_upgraded.count,
+                created_failed_upgrade_count: created_failed_upgrade.count,
+                created_and_upgraded_count: created_and_upgraded.count }
+      Sidekiq::Logging.logger.info('mhv_account_statistics', stats)
+    end
+
+    private
+
+    def accounts_created
+      MhvAccount.where.not(registered_at: nil)
+    end
+
+    def existing_accounts_upgraded
+      MhvAccount.where(registered_at: nil).where.not(upgraded_at: nil)
+    end
+
+    def created_failed_upgrade
+      accounts_created.where(account_state: :upgrade_failed)
+    end
+
+    def created_and_upgraded
+      accounts_created.where.not(upgraded_at: nil)
+    end
+  end
+end
