8c2dd56f26bc90e741b0739ec30ceadc3bee4f4d
Johnny Holton <johnny@holton.co>
2018-06-12 11:55:53 -0400

    in RescueTimeout pass Sentry tags_context as parameter

--- lib/common/client/middleware/request/rescue_timeout.rb
+++ lib/common/client/middleware/request/rescue_timeout.rb
@@ -8,15 +8,16 @@ module Common
         class RescueTimeout < Faraday::Middleware
           include SentryLogging
 
-          def initialize(app = nil, error_code = 'VA900')
+          def initialize(app = nil, error_code = 'VA900', error_tags_context = {})
             @error_code = error_code
+            @error_tags_context = error_tags_context
             super(app)
           end
 
           def call(env)
             @app.call(env)
           rescue Faraday::TimeoutError => e
-            log_exception_to_sentry(e, {}, { backend_service: :evss }, :warn)
+            log_exception_to_sentry(e, {}, @error_tags_context, :warn)
             raise Common::Exceptions::BackendServiceException, @error_code
           end
         end
