5ee8a1afdda4f2531bdefc09e73f41b44efc9dfb
Johnny Holton <johnny@holton.co>
2018-06-13 09:58:59 -0400

    spec improvement

--- spec/jobs/evss/create_user_account_job_spec.rb
+++ spec/jobs/evss/create_user_account_job_spec.rb
@@ -5,11 +5,16 @@ require 'rails_helper'
 RSpec.describe EVSS::CreateUserAccountJob, type: :job do
   let(:user) { FactoryBot.create(:user, :loa3) }
   let(:auth_headers) { EVSS::AuthHeaders.new(user).to_h }
+  let(:client_stub) { instance_double('EVSS::CommonService') }
 
+  before { allow(EVSS::CommonService).to receive(:new).with(auth_headers) { client_stub } }
   it 'calls create_user_account EVSS API' do
-    client_stub = instance_double('EVSS::CommonService')
-    allow(EVSS::CommonService).to receive(:new).with(auth_headers) { client_stub }
     expect(client_stub).to receive(:create_user_account).once
     described_class.new.perform(auth_headers)
+  end
+
+  it 'rescues Common::Exceptions::BackendServiceException (raised for timeouts)' do
+    allow(client_stub).to receive(:create_user_account).and_raise(Common::Exceptions::BackendServiceException)
+    expect { described_class.new.perform(auth_headers) }.to raise_error(Sentry::IgnoredError)
   end
 end
