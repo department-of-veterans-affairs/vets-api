fa2410f68a7bbb3ef69ac81404ef633075e82d26
Silvio Luthi <silvio@adhocteam.us>
2018-06-13 17:43:01 -0700

    fix broken module name

--- app/controllers/v0/disability_compensation_forms_controller.rb
+++ app/controllers/v0/disability_compensation_forms_controller.rb
@@ -14,7 +14,7 @@ module V0
       # Once we run this job asynchronosuly, this data translation can be moved into the
       # async `perform` method
       form_content = JSON.parse(request.body.string)
-      converted_form_content = EVSS::DataTranslation.new(@current_user, form_content).convert
+      converted_form_content = EVSS::DisabilityCompensationForm::DataTranslation.new(@current_user, form_content).convert
       response = service.submit_form(converted_form_content)
       EVSS::IntentToFile::ResponseStrategy.delete("#{@current_user.uuid}:compensation")
       EVSS::DisabilityCompensationForm::SubmitUploads.start(@current_user, response.claim_id)
