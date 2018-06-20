ac29752ff29b2d9a62201e5e8eb5c84c23ed0957
Ryan Baker <ryan.baker@adhocteam.us>
2018-06-20 13:32:09 -0600

    Add vet360 to service list

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -108,6 +108,7 @@ class UserSerializer < ActiveModel::Serializer
     service_list << BackendServices::FORM_PREFILL if object.can_access_prefill_data?
     service_list << BackendServices::ID_CARD if object.can_access_id_card?
     service_list << BackendServices::IDENTITY_PROOFED if object.identity_proofed?
+    service_list << BackendServices::VET360 if object.can_access_vet360?
     service_list += BetaRegistration.where(user_uuid: object.uuid).pluck(:feature) || []
     service_list
   end
