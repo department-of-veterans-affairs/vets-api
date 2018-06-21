e5c8f05e779f87d2985ecf4d4c075dd3ada4b694
Lihan Li <frankieteardrop@gmail.com>
2018-06-21 18:47:54 -0400

    zip plus four method

--- app/models/form_profiles/va1990.rb
+++ app/models/form_profiles/va1990.rb
@@ -1,6 +1,32 @@
 # frozen_string_literal: true
 
 class FormProfiles::VA1990 < FormProfile
+  def prefill(user)
+    return_val = super
+
+    if Settings.vet360.prefill
+      contact_information = Vet360Redis::ContactInformation.for_user(user)
+
+      {
+        'homePhone' => 'home_phone',
+      }
+
+      return_val[:form_data]['homePhone'] = contact_information.home_phone if contact_information.home_phone.present?
+    end
+    return_val
+  end
+
+  def convert_vets360_address(address)
+    {
+      'street' => address.address_line1,
+      'street2' => address.address_line2,
+      'city' => address.city,
+      'state' => address.state_code || address.province,
+      'country' => address.country_code_iso3,
+      'postalCode' => sdf
+    }
+  end
+
   def metadata
     {
       version: 0,
