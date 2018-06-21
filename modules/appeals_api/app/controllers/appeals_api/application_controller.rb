f17dd970d9ac78ae0a220d340b55cc0379b68346
Patrick Vinograd <patrick@adhocteam.us>
2018-06-20 23:17:11 -0700

    Update to appeals service to use OpenID auth, remove SSN header

--- modules/appeals_api/app/controllers/appeals_api/application_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module AppealsApi
-  class ApplicationController < ::ApplicationController
+  class ApplicationController < ::OpenidApplicationController
     skip_before_action :set_tags_and_extra_context
   end
 end
