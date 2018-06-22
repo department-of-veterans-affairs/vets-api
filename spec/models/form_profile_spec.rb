a2949b77dcda4829e560b624df7d94ea9f5d76bb
Lihan Li <frankieteardrop@gmail.com>
2018-06-21 20:04:46 -0400

    lint

--- spec/models/form_profile_spec.rb
+++ spec/models/form_profile_spec.rb
@@ -475,7 +475,13 @@ RSpec.describe FormProfile, type: :model do
           v22_1990_expected['email'] = Vet360Redis::ContactInformation.for_user(user).email.email_address
           v22_1990_expected['homePhone'] = '(303) 555-1234'
           v22_1990_expected['mobilePhone'] = '(303) 555-1234'
-          v22_1990_expected['veteranAddress'] = { 'street' => '1493 Martin Luther King Rd', 'city' => 'Fulton', 'state' => 'MS', 'country' => 'USA', 'postalCode' => '38843' }
+          v22_1990_expected['veteranAddress'] = {
+            'street' => '1493 Martin Luther King Rd',
+            'city' => 'Fulton',
+            'state' => 'MS',
+            'country' => 'USA',
+            'postalCode' => '38843'
+          }
         end
 
         it 'should prefill 1990' do
