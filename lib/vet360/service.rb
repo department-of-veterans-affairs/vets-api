d2449c58ec9f8e533cfef865cd171b886c345ee9
Ryan Baker <ryan.baker@adhocteam.us>
2018-06-25 10:32:50 -0700

    Move statsd logging so we capture info before error handling

--- lib/vet360/service.rb
+++ lib/vet360/service.rb
@@ -11,9 +11,10 @@ module Vet360
     end
 
     def perform(method, path, body = nil, headers = {})
+      increment_total
+
       config.base_request_headers.merge(headers)
       response = super(method, path, body, headers)
-      Vet360::Stats.increment('total_operations')
 
       response
     end
@@ -74,6 +75,10 @@ module Vet360
       else
         log_message_to_sentry('New Vet360 Exceptions Key', :info, key: exception_key)
       end
+    end
+
+    def increment_total
+      Vet360::Stats.increment('total_operations')
     end
   end
 end
