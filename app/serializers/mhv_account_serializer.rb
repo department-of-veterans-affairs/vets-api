d34090f752b9a8e4c31aca0fcbc884231fd4c812
Eugene Doan <eugene@adhocteam.us>
2018-06-26 14:10:01 -0400

    Added terms acceptance flag to MHV account serializer.

--- app/serializers/mhv_account_serializer.rb
+++ app/serializers/mhv_account_serializer.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class MhvAccountSerializer < ActiveModel::Serializer
-  attribute :account_state
   attribute :account_level
+  attribute :account_state
+  attribute :terms_and_conditions_accepted
+
+  def terms_and_conditions_accepted
+    object.terms_and_conditions_accepted?
+  end
 end
