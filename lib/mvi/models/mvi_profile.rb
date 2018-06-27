4cd91f58c31bc8203f0f25b3421d537c2b5fc825
Eugene Doan <eugene@adhocteam.us>
2018-06-27 02:15:52 -0400

    Revert "Mhv account creation cleanup (#1899)"
    This reverts commit 1f5b15265696577e77b2a4d52fbc587fa52ff000.

--- lib/mvi/models/mvi_profile.rb
+++ lib/mvi/models/mvi_profile.rb
@@ -30,7 +30,6 @@ module MVI
       attribute :icn, String
       attribute :icn_with_aaid, String
       attribute :mhv_ids, Array[String]
-      attribute :active_mhv_ids, Array[String]
       attribute :vha_facility_ids, Array[String]
       attribute :edipi, String
       attribute :participant_id, String
@@ -40,7 +39,7 @@ module MVI
       attribute :historical_icns, Array[String]
 
       def mhv_correlation_id
-        @active_mhv_ids&.first
+        @mhv_ids&.first
       end
 
       def normalized_suffix
