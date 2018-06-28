a553df4cd2e1bf6dc8d980c6c13b697a7ee26224
Ryan Baker <ryan.baker@adhocteam.us>
2018-06-28 10:08:27 -0700

    Add logging to debug cache invalidation

--- app/models/vet360_redis/cache.rb
+++ app/models/vet360_redis/cache.rb
@@ -14,11 +14,22 @@ module Vet360Redis
     def self.invalidate(user)
       contact_info = user.vet360_contact_info
 
+      log("Vet360: Response cache exists before invalidation: #{redis_key_exists}")
+
       if contact_info.present?
         contact_info.destroy
+        log("Vet360: Response cache exists after invalidation: #{redis_key_exists}")
       else
-        new.log_message_to_sentry('Vet360: Cannot invalidate a nil response cache', :info)
+        log('Vet360: Cannot invalidate a nil response cache')
       end
+    end
+
+    def self.redis_key_exists(user)
+      Redis.current.exists("vet360-contact-info-response:#{user.uuid}")
+    end
+
+    def self.log(message)
+      new.log_message_to_sentry(message, :info)
     end
   end
 end
