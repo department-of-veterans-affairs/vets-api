195e0b191ab2d307b79387d27ab970fcdd6a6295
egarreau <ergarreau@gmail.com>
2018-07-06 15:29:49 -0500

    add json validation to appeals response

--- lib/appeals/responses/appeals.rb
+++ lib/appeals/responses/appeals.rb
@@ -7,8 +7,16 @@ module Appeals
       attribute :status, Integer
 
       def initialize(body, status)
-        self.body = body
+        self.body = body if json_format_is_valid?(body)
         self.status = status
+      rescue JSON::Schema::ValidationError => error
+        raise Common::Client::Errors::ClientError.new(error.message, 500)
+      end
+
+      private
+
+      def json_format_is_valid?(body)
+        JSON::Validator.validate!('spec/support/schemas/appeals.json', body, strict: true)
       end
     end
   end
