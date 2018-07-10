7ebdd1eee46152067f75ff651842b3b20f01403c
Alastair Dawson <alastair.j.dawson@gmail.com>
2018-07-10 15:59:59 -0700

    required submit fields

--- rakelib/support/disability_compensation_form/setup_test.rb
+++ rakelib/support/disability_compensation_form/setup_test.rb
@@ -60,121 +60,165 @@ class SetupTest
 
   # rubocop:disable all
   def submit_hash
-    homeless = random_bool
-    
+    compact(
+      {
+        "form526" => {
+          "veteran" => veteran,
+          "attachments" => attachments,
+          "mililtaryPayments" => military_payments,
+          "serviceInformation" => service_information,
+          "disabilities" => disabilities,
+          "treatments" => treatments,
+          "specialCircumstances" => special_circumstances,
+          "standardClaim" => random_bool,
+          "claimantCertification" => random_bool
+        }
+      }
+    )
+  end
+
+  def compact(hash)
+    p = proc do |*args|
+      v = args.last
+      v.delete_if(&p) if v.respond_to? :delete_if
+      v.nil? || v.respond_to?(:"empty?") && v.empty?
+    end
+
+    hash.delete_if(&p)
+  end
+
+  def special_circumstances
+    if random_bool
+      [{
+        "name" => L.word,
+        "code" => L.word,
+        "needed" => random_bool
+      }]
+    end
+  end
+
+  def treatments
+    if random_bool
+      [{
+        "treatmentCenterName" => L.word,
+        "treatmentDateRange" => date_range,
+        "treatmentCenterAddress" => address.slice("country", "city", "state"),
+        "treatmentCenterType" => treatment_center_type
+      }]
+    end
+  end
+
+  def disabilities
+    [{
+      "name" => L.word,
+      "disabilityActionType" => disability_action_type,
+      "specialIssues" => special_issues,
+      "ratedDisabilityId" => (L.word if random_bool),
+      "ratingDecisionId" => (L.word if random_bool),
+      "diagnosticCode" => (NUM.number(5) if random_bool),
+      "classificationCode" => (L.word if random_bool)
+    }]
+  end
+
+  def special_issues
+    if random_bool
+      [{
+        "code" => special_issue_code,
+        "name" => L.word
+      }]
+    end
+  end
+
+  def service_information
     {
-      "form526" => {
-        "veteran" => {
-          "emailAddress" => I.email,
-          "alternateEmailAddress" => (I.email if random_bool),
-          "mailingAddress" => address,
-          "forwardingAddress" => (address.merge("effectiveDate" => date) if random_bool),
-          "primaryPhone" => NUM.number(10),
-          "homelessness" => {
-            "isHomeless" => homeless,
-            "pointOfContact" => (
-            if homeless
-              {
-                "pointOfContactName" => N.name,
-                "primaryPhone" => NUM.number(10)
-              }
-            end)
-          }.reject { |k, v| v.nil? },
-          "serviceNumber" => (NUM.number(9) if random_bool)
-        }.reject { |k, v| v.nil? },
-        "attachments" => (
-        if random_bool
-          [{
-            "documentName" => L.word,
-            "dateUploaded" => date,
-            "attachmentType" => L.word,
-            "inflightDocumentId" => NUM.number(10)
-          }]
-        end),
-        "mililtaryPayments" => (
+      "servicePeriods" => [{
+        "serviceBranch" => service_branch,
+        "dateRange" => date_range
+      }],
+      "reservesNationalGuardService" => reserves_national_guard_service,
+      "servedInCombatZone" => random_bool,
+      "separationLocationName" => (L.word if random_bool),
+      "separationLocationCode" => (L.word if random_bool),
+      "alternateNames" => ([N.name] if random_bool),
+      "confinements" => (
+      if random_bool
+        [{
+          "confinementDateRange" => date_range,
+          "verifiedIndicator" => random_bool
+        }]
+      end)
+    }
+  end
+
+  def reserves_national_guard_service
+    if random_bool
+      {
+        "title10Activation" => (
         if random_bool
           {
-            "payments" => [{
-              "payType" => pay_type,
-              "amount" => NUM.number(5)
-            }],
-            "receiveCompensationInLieuOfRetired" => random_bool
+            "title10ActivationDate" => date,
+            "anticipatedSeparationDate" => date
           }
         end),
-        "serviceInformation" => {
-          "servicePeriods" => [{
-            "serviceBranch" => service_branch,
-            "dateRange" => date_range
-          }],
-          "reservesNationalGuardService" => (
-          if random_bool
-            {
-              "title10Activation" => (
-              if random_bool
-                {
-                  "title10ActivationDate" : date,
-                  "anticipatedSeparationDate" : date
-                }
-              end),
-              "obligationTermOfServiceDateRange" => date_range,
-              "unitName" => L.word,
-              "unitPhone" => NUM.number(10),
-              "inactiveDutyTrainingPay" => (
-              if random_bool
-                {
-                  "waiveVABenefitsToRetainTrainingPay" : random_bool
-                }
-              end)
-            }.reject { |k, v| v.nil? }
-          end),
-          "servedInCombatZone" => random_bool,
-          "separationLocationName" => (L.word if random_bool),
-          "separationLocationCode" => (L.word if random_bool),
-          "alternateNames" => ([N.name] if random_bool),
-          "confinements" => (
-          if random_bool
-            [{
-              "confinementDateRange" : date_range,
-              "verifiedIndicator" : random_bool
-            }]
-          end)
-        }.reject { |k, v| v.nil? },
-        "disabilities" => [{
-          "name" => L.word,
-          "disabilityActionType" => disability_action_type,
-          "specialIssues" => (
-          if random_bool
-            [{
-              "code" => special_issue_code,
-              "name" => L.word
-            }]
-          end),
-          "ratedDisabilityId" => (L.word if random_bool),
-          "ratingDecisionId" => (L.word if random_bool),
-          "diagnosticCode" => (NUM.number(5) if random_bool),
-          "classificationCode" => (L.word if random_bool)
-        }.reject { |k, v| v.nil? }],
-        "treatments" => (
+        "obligationTermOfServiceDateRange" => date_range,
+        "unitName" => L.word,
+        "unitPhone" => NUM.number(10),
+        "inactiveDutyTrainingPay" => (
         if random_bool
-          [{
-            "treatmentCenterName" => L.word,
-            "treatmentDateRange" => (date_range if random_bool),
-            "treatmentCenterAddress" => address.slice("country", "city", "state"),
-            "treatmentCenterType" => treatment_center_type
-          }.reject { |k, v| v.nil? }]
-        end),
-        "specialCircumstances" => (
-        if random_bool
-          [{
-            "name" => L.word,
-            "code" => L.word,
-            "needed" => random_bool
-          }]
-        end),
-        "standardClaim" => random_bool,
-        "claimantCertification" => random_bool
-      }.reject { |k, v| v.nil? }
-    }.reject { |k, v| v.nil? }
+          {
+            "waiveVABenefitsToRetainTrainingPay" => random_bool
+          }
+        end)
+      }
+    end
+  end
+
+  def military_payments
+    if random_bool
+      {
+        "payments" => [{
+          "payType" => pay_type,
+          "amount" => NUM.number(5)
+        }],
+        "receiveCompensationInLieuOfRetired" => random_bool
+      }
+    end
+  end
+
+  def attachments
+    if random_bool
+      [{
+        "documentName" => L.word,
+        "dateUploaded" => date,
+        "attachmentType" => L.word,
+        "inflightDocumentId" => NUM.number(10)
+      }]
+    end
+  end
+
+  def veteran
+    {
+      "emailAddress" => I.email,
+      "alternateEmailAddress" => (I.email if random_bool),
+      "mailingAddress" => address,
+      "forwardingAddress" => (address.merge("effectiveDate" => date) if random_bool),
+      "primaryPhone" => NUM.number(10),
+      "homelessness" => homelessness(random_bool),
+      "serviceNumber" => (NUM.number(9) if random_bool)
+    }
+  end
+
+  def homelessness(homeless)
+    {
+      "isHomeless" => homeless,
+      "pointOfContact" => (
+      if homeless
+        {
+          "pointOfContactName" => N.name,
+          "primaryPhone" => NUM.number(10)
+        }
+      end)
+    }
   end
 
   # rubocop:enable all
@@ -185,13 +229,13 @@ class SetupTest
 
   def address
     {
-      "addressLine1" : A.street_address,
-      "addressLine2" : (A.secondary_address if random_bool),
-      "city" : A.city,
-      "state" : (A.state_abbr if random_bool),
-      "zipCode" : (A.zip if random_bool),
-      "country" : A.country
-    }.reject { |_k, v| v.nil? }
+      "addressLine1" => A.street_address,
+      "addressLine2" => (A.secondary_address if random_bool),
+      "city" => A.city,
+      "state" => (A.state_abbr if random_bool),
+      "zipCode" => (A.zip if random_bool),
+      "country" => A.country
+    }
   end
 
   # rubocop:disable all
@@ -207,8 +251,8 @@ class SetupTest
 
   def date_range
     {
-      "from" : from_date,
-      "to" : date
+      "from" => from_date,
+      "to" => date
     }
   end
 
