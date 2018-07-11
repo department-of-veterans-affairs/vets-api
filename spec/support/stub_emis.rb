52183de89122c9e69e839ec19b8c83520302e937
hpjaj <hmlevine@gmail.com>
2018-07-11 09:05:11 -0600

    As these require calls to eMIS, they must be stubbed for testing

--- spec/support/stub_emis.rb
+++ spec/support/stub_emis.rb
@@ -2,5 +2,8 @@
 
 def stub_emis
   allow_any_instance_of(EMISRedis::VeteranStatus).to receive(:veteran?).and_return(true)
+  allow_any_instance_of(EMISRedis::VeteranStatus).to receive(:post_911_combat_deployment?).and_return(true)
+  allow_any_instance_of(EMISRedis::VeteranStatus).to receive(:pre_911_combat_deployment?).and_return(false)
+  allow_any_instance_of(EMISRedis::VeteranStatus).to receive(:military_person?).and_return(true)
   allow_any_instance_of(FormProfile).to receive(:initialize_military_information).and_return({})
 end
