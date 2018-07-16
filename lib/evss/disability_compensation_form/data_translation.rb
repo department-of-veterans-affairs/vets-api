ef23a9f944f170b011c404b699ade6b43247bcde
Alastair Dawson <alastair.j.dawson@gmail.com>
2018-07-15 21:08:51 -0700

    update service name, and disability name (#2130)

--- lib/evss/disability_compensation_form/data_translation.rb
+++ lib/evss/disability_compensation_form/data_translation.rb
@@ -117,7 +117,14 @@ module EVSS
       end
 
       def service_branch(service_branch)
-        return 'National Oceanic &amp; Atmospheric Administration' if service_branch == 'NOAA'
+        branch_map = {
+          'Air Force Reserve' => 'Air Force Reserves',
+          'Army Reserve' => 'Army Reserves',
+          'Coast Guard Reserve' => 'Coast Guard Reserves',
+          'Marine Corps Reserve' => 'Marine Corps Reserves',
+          'NOAA' => 'National Oceanic &amp; Atmospheric Administration'
+        }
+        return branch_map[service_branch] if branch_map.key? service_branch
         service_branch
       end
 
