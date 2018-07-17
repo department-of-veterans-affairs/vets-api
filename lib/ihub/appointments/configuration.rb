3b221309181f4df3504ebac9ab582b864fbb7298
hpjaj <hmlevine@gmail.com>
2018-07-17 14:32:44 -0600

    Establishes base and nested service implementation
    This might not be necessary, for the initial discovery.  But decided to follow precedence in Vet360, EVSS, etc.

--- lib/ihub/appointments/configuration.rb
+++ lib/ihub/appointments/configuration.rb
@@ -2,25 +2,15 @@
 
 module IHub
   module Appointments
-    class Configuration < Common::Client::Configuration::REST
+    class Configuration < IHub::Configuration
+      self.read_timeout = Settings.ihub.appointments.timeout || 30
+
       def base_path
         "#{Settings.ihub.url}/api/Appointments/1.0/json/ftpCRM/"
       end
 
       def service_name
         'iHub/Appointments'
-      end
-
-      def connection
-        @conn ||= Faraday.new(base_path, headers: base_request_headers, request: request_options) do |faraday|
-          faraday.use      :breakers
-          faraday.use      Faraday::Response::RaiseError
-
-          faraday.response :betamocks if mock_enabled?
-          faraday.response :snakecase, symbolize: false
-          faraday.response :json, content_type: /\bjson/ # ensures only json content types parsed
-          faraday.adapter Faraday.default_adapter
-        end
       end
 
       def mock_enabled?
--- lib/ihub/appointments/service.rb
+++ lib/ihub/appointments/service.rb
@@ -1,27 +1,30 @@
 # frozen_string_literal: true
 
+require 'common/client/base'
+
 module IHub
   module Appointments
-    class Service < Common::Client::Base
+    class Service < IHub::Service
       include Common::Client::Monitoring
 
       configuration IHub::Appointments::Configuration
-
-      STATSD_KEY_PREFIX = 'api.ihub'
-
-      def initialize(user)
-        @user = user
-      end
 
       def appointments
         return nil if @user.icn.blank?
 
         with_monitoring do
           response = perform(:get, @user.icn, nil)
+
           IHub::Appointments::Response.from(response)
         end
       rescue StandardError => error
-        log_message_to_sentry(error.message, :error, extra_context: { url: config.base_path })
+        log_message_to_sentry(
+          error.message,
+          :error,
+          extra_context: { url: config.base_path },
+          ihub: 'appointments'
+        )
+
         raise error
       end
     end
