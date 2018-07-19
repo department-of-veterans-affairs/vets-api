a35de3fc312277f62ad70d8277fc930ae942d241
Ryan Baker <rlbaker@users.noreply.github.com>
2018-07-19 09:06:57 -0700

    iHub Appointments (#2067)
    * Initial iHub service stub
    
    * Creates base config settings for IHub integration

---
+++ lib/ihub/appointments/configuration.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module IHub
+  module Appointments
+    class Configuration < IHub::Configuration
+      self.read_timeout = Settings.ihub.appointments.timeout || 30
+
+      def base_path
+        "#{Settings.ihub.url}/api/Appointments/1.0/json/ftpCRM/"
+      end
+
+      def service_name
+        'iHub/Appointments'
+      end
+
+      def mock_enabled?
+        Settings.ihub.appointments.mock || false
+      end
+    end
+  end
+end
