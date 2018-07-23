f986fb5c21d24865e4332185383c741607ceda11
Harry Levine <hmlevine@gmail.com>
2018-07-23 09:22:44 -0600

    Scopes EDIPI presence counter to service history endpoint (#2147)

--- app/policies/emis_policy.rb
+++ app/policies/emis_policy.rb
@@ -2,12 +2,6 @@
 
 EMISPolicy = Struct.new(:user, :emis) do
   def access?
-    if user.edipi.present?
-      StatsD.increment("#{EMIS::Service::STATSD_KEY_PREFIX}.edipi", tags: ['present:true'])
-      true
-    else
-      StatsD.increment("#{EMIS::Service::STATSD_KEY_PREFIX}.edipi", tags: ['present:false'])
-      false
-    end
+    user.edipi.present?
   end
 end
