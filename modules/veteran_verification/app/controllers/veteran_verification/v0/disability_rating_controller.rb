04c45d8593129e37ecf33c4792b68d6834756668
Edward Paget <edward@cassetteta.pe>
2018-07-25 14:33:04 -0400

    Add disability rating endpoint and serializer

--- modules/veteran_verification/app/controllers/veteran_verification/v0/disability_rating_controller.rb
+++ modules/veteran_verification/app/controllers/veteran_verification/v0/disability_rating_controller.rb
@@ -1,8 +1,20 @@
+# frozen_string_literal: true
+
 module VeteranVerification
   module V0
     class DisabilityRatingController < ApplicationController
+      before_action { authorize :evss, :access? }
+
       def index
-        render json: {}
+        response = service.get_rated_disabilities
+        render json: response.rated_disabilities,
+               each_serializer: VeteranVerification::DisabilityRatingSerializer
+      end
+
+      private
+
+      def service
+        EVSS::DisabilityCompensationForm::Service.new(@current_user)
       end
     end
   end
