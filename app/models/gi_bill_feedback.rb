274cbad45746ddb12cf3c9a763d8642de20d577a
Lihan Li <frankieteardrop@gmail.com>
2018-07-26 17:17:18 -0400

    add submission job

--- app/models/gi_bill_feedback.rb
+++ app/models/gi_bill_feedback.rb
@@ -11,7 +11,30 @@ class GIBillFeedback < Common::RedisStore
   redis_ttl REDIS_CONFIG['gi_bill_feedback']['each_ttl']
   redis_key(:guid)
 
+  # TODO set these
   attribute(:state)
   attribute(:guid)
   attribute(:response)
+
+  def transform_form
+  end
+
+  def save
+    originally_persisted = @persisted
+    saved = super
+
+    if saved && !originally_persisted
+      create_submission_job
+    end
+
+    saved
+  end
+
+  private
+
+  def create_submission_job
+    puts 'soubmission job'
+    # binding.pry; fail
+    # SubmissionJob.perform_async(id, form, user&.uuid)
+  end
 end
