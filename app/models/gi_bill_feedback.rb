b62385712db42bde3219c88f0431f00686a1c660
Lihan Li <frankieteardrop@gmail.com>
2018-07-26 19:26:23 -0400

    update vets json schema

--- app/models/gi_bill_feedback.rb
+++ app/models/gi_bill_feedback.rb
@@ -1,9 +1,9 @@
 class GIBillFeedback < Common::RedisStore
   include SetGuid
-  include TempFormValidation
   include AsyncRequest
 
   attr_accessor(:user)
+  attr_accessor(:form)
 
   FORM_ID = 'complaint-tool'
 
@@ -11,10 +11,16 @@ class GIBillFeedback < Common::RedisStore
   redis_ttl REDIS_CONFIG['gi_bill_feedback']['each_ttl']
   redis_key(:guid)
 
-  # TODO set these
   attribute(:state)
   attribute(:guid)
   attribute(:response)
+
+  validate(:form_matches_schema, unless: :persisted?)
+  validates(:form, presence: true, unless: :persisted?)
+
+  def parsed_form
+    @parsed_form ||= JSON.parse(form)
+  end
 
   def transform_form
   end
@@ -31,6 +37,12 @@ class GIBillFeedback < Common::RedisStore
   end
 
   private
+
+  def form_matches_schema
+    if form.present?
+      errors[:form].concat(JSON::Validator.fully_validate(VetsJsonSchema::SCHEMAS[self.class::FORM_ID], parsed_form))
+    end
+  end
 
   def create_submission_job
     puts 'soubmission job'
