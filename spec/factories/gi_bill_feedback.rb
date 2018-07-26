e5c7d9b0a13c7fdc5c20e0bd375be2df1be0c37c
Lihan Li <frankieteardrop@gmail.com>
2018-07-26 19:39:11 -0400

    add sample form to factory

--- spec/factories/gi_bill_feedback.rb
+++ spec/factories/gi_bill_feedback.rb
@@ -1,5 +1,23 @@
 FactoryBot.define do
   factory :gi_bill_feedback do
     state('pending')
+    form(
+      {
+        onBehalfOf: 'Myself',
+        educationDetails: {
+          school: {
+            name: 'school'
+          },
+          programs: {
+            'MGIB-AD Ch 30': true
+          }
+        },
+        issue: {
+          'Student Loans': true
+        },
+        issueDescription: 'issueDescription',
+        issueResolution: 'issueResolution'
+      }.to_json
+    )
   end
 end
