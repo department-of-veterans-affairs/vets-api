d5aa8a25329a4f047c97070eff9c263bf7171f00
Lihan Li <frankieteardrop@gmail.com>
2018-07-27 19:28:18 -0400

    gi bill feedbacks ctrl

--- app/controllers/v0/gi_bill_feedbacks_controller.rb
+++ app/controllers/v0/gi_bill_feedbacks_controller.rb
@@ -7,6 +7,26 @@ module V0
 
     def create
       authenticate_token
+
+      gi_bill_feedback = GIBillFeedback.new(
+        params.require(:gi_bill_feedback).permit(:form).merge(
+          user: current_user
+        )
+      )
+
+      unless gi_bill_feedback.save
+        Raven.tags_context(validation: 'gibft')
+
+        raise Common::Exceptions::ValidationErrors, gi_bill_feedback
+      end
+
+      clear_saved_form(GIBillFeedback::FORM_ID)
+
+      render(json: gi_bill_feedback)
+    end
+
+    def show
+      render(json: GIBillFeedback.find(params[:id]))
     end
   end
 end
