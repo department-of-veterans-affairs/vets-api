7c7a02e98f9b43141ab483e39398f1cc1e4a1def
hpjaj <hmlevine@gmail.com>
2018-07-30 16:35:12 -0600

    Service object error handling
    Currently keeping everything at this /appointments level.
    
    Will want to abstract this up a layer to the main ihub/service class level, if we stick with iHub, and add other endpoints.

--- spec/lib/ihub/appointments/service_spec.rb
+++ spec/lib/ihub/appointments/service_spec.rb
@@ -37,8 +37,31 @@ describe IHub::Appointments::Service do
         allow_any_instance_of(User).to receive(:icn).and_return(nil)
       end
 
-      it 'raises an error' do
-        expect { subject.appointments }.to raise_error(StandardError, 'User has no ICN')
+      it 'raises an exception', :aggregate_failures do
+        expect { subject.appointments }.to raise_error do |e|
+          expect(e).to be_a(Common::Exceptions::BackendServiceException)
+          expect(e.status_code).to eq(502)
+          expect(e.original_body).to eq 'User has no ICN'
+          expect(e.errors.first.code).to eq('IHUB_102')
+        end
+      end
+    end
+
+    context 'when iHub returns error_occurred: true' do
+      before do
+        allow_any_instance_of(User).to receive(:icn).and_return('1234')
+      end
+
+      it 'raises an exception', :aggregate_failures do
+        VCR.use_cassette('ihub/appointments/error_occurred', VCR::MATCH_EVERYTHING) do
+          expect { subject.appointments }.to raise_error do |e|
+            expect(e).to be_a(Common::Exceptions::BackendServiceException)
+            expect(e.status_code).to eq(400)
+            expect(e.original_body['error_message']).to be_present
+            expect(e.original_body['debug_info']).to be_present
+            expect(e.errors.first.code).to eq('IHUB_101')
+          end
+        end
       end
     end
   end
