b15a228855e5e7a8c2207e1bae032813384e801e
Silvio Luthi <silvio@adhocteam.us>
2018-07-30 11:00:55 -0700

    Allow row to be updated if it exists already

--- app/controllers/v0/form526_opt_ins_controller.rb
+++ app/controllers/v0/form526_opt_ins_controller.rb
@@ -2,9 +2,13 @@
 
 module V0
   class Form526OptInsController < ApplicationController
+    before_action { authorize :evss, :access? }
+
     def create
-      Form526OptIn.new(user_uuid: current_user.uuid, email: params[:email]).save!
-      render json: { 'email': current_user.email, 'status': 'OK' },
+      opt_in = Form526OptIn.where(user_uuid: current_user.uuid).first_or_initialize
+      opt_in.email = params[:email]
+      opt_in.save!
+      render json: opt_in,
              serializer: Form526OptInSerializer
     end
   end
