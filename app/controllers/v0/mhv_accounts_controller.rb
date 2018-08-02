c012dcfb5a32d9784dbd04c99748acee93ffa1d0
Eugene Doan <U-DON@users.noreply.github.com>
2018-08-02 09:51:28 -0400

    MHV Upgrade Launch (#2080)
    Revert "Revert "Mhv account creation cleanup (#1899)" (#2075)"
    
    This reverts commit 042d27ce7a984b67127eaf327371e9ca11604b1a.

--- app/controllers/v0/mhv_accounts_controller.rb
+++ app/controllers/v0/mhv_accounts_controller.rb
@@ -2,63 +2,46 @@
 
 module V0
   class MhvAccountsController < ApplicationController
+    CREATE_ERROR = 'You are not eligible for creating an MHV account'
+    UPGRADE_ERROR = 'You are not eligible for upgrading an MHV account'
     include ActionController::Serialization
 
-    before_action :authorize, only: :create
-
     def show
-      render json: mhv_account,
-             serializer: MhvAccountSerializer
+      render_account
     end
 
     def create
-      register_mhv_account unless mhv_account.previously_registered?
-      upgrade_mhv_account
-      render json: mhv_account,
-             serializer: MhvAccountSerializer,
-             status: :accepted
+      if mhv_account.creatable?
+        mhv_accounts_service.create
+        render_account(status: :created)
+      else
+        raise Common::Exceptions::Forbidden, detail: CREATE_ERROR
+      end
     end
 
-    protected
-
-    def authorized?
-      mhv_account.eligible?
-    end
-
-    def authorize
-      raise_access_denied unless creatable_or_upgradable?
-      raise_requires_terms_acceptance if current_user.mhv_account.needs_terms_acceptance?
+    def upgrade
+      if mhv_account.upgradable?
+        mhv_accounts_service.upgrade
+        render_account(status: :accepted)
+      else
+        raise Common::Exceptions::Forbidden, detail: UPGRADE_ERROR
+      end
     end
 
     private
-
-    def creatable_or_upgradable?
-      current_user.authorize(:mhv_account_creation, :creatable?) ||
-        current_user.authorize(:mhv_account_creation, :upgradable?)
-    end
-
-    def raise_access_denied
-      raise Common::Exceptions::Forbidden, detail: 'You do not have access to MHV services'
-    end
-
-    def raise_requires_terms_acceptance
-      raise Common::Exceptions::Forbidden, detail: 'You have not accepted the terms of service'
-    end
-
-    def register_mhv_account
-      mhv_accounts_service.create
-    end
-
-    def upgrade_mhv_account
-      mhv_accounts_service.upgrade
-    end
 
     def mhv_account
       current_user.mhv_account
     end
 
     def mhv_accounts_service
-      @mhv_accounts_service ||= MhvAccountsService.new(current_user)
+      @mhv_accounts_service ||= MhvAccountsService.new(mhv_account)
+    end
+
+    def render_account(status: :ok)
+      render json: mhv_account,
+             serializer: MhvAccountSerializer,
+             status: status
     end
   end
 end
