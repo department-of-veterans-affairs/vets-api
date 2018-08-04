c3e41947939caac42421809f940f9eb23cb00aca
Lihan Li <frankieteardrop@gmail.com>
2018-08-03 21:50:26 -0400

    f

--- lib/evss/dependents/configuration.rb
+++ lib/evss/dependents/configuration.rb
@@ -4,7 +4,7 @@ module EVSS
   module Dependents
     class Configuration < EVSS::Configuration
       def base_path
-        'http://vaausessapp800.aac.va.gov:8001/wss-686-services-web-2.6/rest/'
+        "#{Settings.evss.url}/wss-686-services-web-2.6/rest/"
       end
 
       def service_name
