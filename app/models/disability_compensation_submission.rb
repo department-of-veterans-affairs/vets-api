f047d1e7e544bd6912004f41dedfb652a9c1dff1
Alastair Dawson <alastair.j.dawson@gmail.com>
2018-08-06 15:28:23 -0700

    update model

--- app/models/disability_compensation_submission.rb
+++ app/models/disability_compensation_submission.rb
@@ -11,5 +11,5 @@ class DisabilityCompensationSubmission < ActiveRecord::Base
 
   validates :user_uuid, presence: true, uniqueness: { scope: :form_type }
   validates :form_type, presence: true, uniqueness: { scope: :user_uuid }
-  validates :claim_id, presence: true, uniqueness: true
+  validates :status, presence: true
 end
