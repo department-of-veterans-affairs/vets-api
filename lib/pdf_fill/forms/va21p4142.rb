6bbc0a0843a969bc01011b2727a77f9d348786e5
Hope Lam <hope.lam@rsdcgroup.com>
2018-08-07 10:48:14 -0400

    closed off functions

---
+++ lib/pdf_fill/forms/va21p4142.rb
@@ -0,0 +1,175 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/ClassLength
+
+module PdfFill
+    module Forms
+     class Va21p4142 < FormBase
+
+        KEY = {
+            'veteranFullName' => {
+                'first' => {
+                    key: 'F[0].Page_1[0].VeteranFirstName[0]',
+                    limit: 12,
+                    question_num: 1,
+                    question_text: "VETERAN/BENEFICIARY'S FIRST NAME"
+                },
+                'middle' => {
+                    key: 'F[0].Page_1[0].VeteranMiddleInitial1[0]',
+                },
+                'last' => {
+                    key:'F[0].Page_1[0].VeteranLastName[0]',
+                    limit: 18,
+                    question_num: 1,
+                    question_text: "VETERAN/BENEFICIARY'S LAST NAME"
+                }
+            },
+            'veteranSocialSecurityNumber' => {
+                'first' => {
+                    key: 'F[0].Page_1[0].ClaimantsSocialSecurityNumber_FirstThreeNumbers[0]'
+                },
+                'second' => {
+                    key: 'F[0].Page_1[0].ClaimantsSocialSecurityNumber_SecondTwoNumbers[0]'
+                },
+                'third' => {
+                    key: 'F[0].Page_1[0].ClaimantsSocialSecurityNumber_LastFourNumbers[0]'
+                }
+            },
+            'vaFileNumber' => {
+                key: 'F[0].Page_1[0].VAFileNumber[0]'
+            },
+            'veteranDateOfBirth' => {
+                'month' => {
+                    key: 'F[0].Page_1[0].DOBmonth[0]'
+                },
+                'day' => {
+                    key: 'F[0].Page_1[0].DOBday[0]'
+                },
+                'year' => {
+                    key: 'F[0].Page_1[0].DOByear[0]'
+                }
+            },
+            'veteranServiceNumber' => {
+                key: 'F[0].Page_1[0].VeteransServiceNumber[0]'
+            },
+            'mailingAddress' => {
+                question_num: 6
+                question_text: "MAILING ADDRESS",
+
+                'veteranAddressLine1' => {
+                    key: 'F[0].Page_1[0].CurrentMailingAddress_NumberAndStreet[0]',
+                    limit: 30,
+                    question_num: 6,
+                    question_suffix: 'A',
+                    question_text: 'Number and Street'
+                },
+                'apartmentOrUnitNumber' => {
+                    key: 'F[0].Page_1[0].CurrentMailingAddress_ApartmentOrUnitNumber[0]',
+                    limit: 5,
+                    question_num: 6,
+                    question_suffix: 'B',
+                    question_text: 'Apartment or Unit Number'
+                },
+                'city' => {
+                    key: 'F[0].Page_1[0].CurrentMailingAddress_City[0]',
+                    limit: 18,
+                    question_num: 6,
+                    question_suffix: 'C',
+                    question_text: 'City'
+                },
+                'stateOrProvince' => {
+                    key: 'F[0].Page_1[0].CurrentMailingAddress_StateOrProvince[0]'
+                },
+                'country' => {
+                    key: 'F[0].Page_1[0].CurrentMailingAddress_Country[0]'
+                    limit: 2
+                },
+                'zipOrPostalCode' => {
+                    'firstFive' => {
+                        key: 'F[0].Page_1[0].CurrentMailingAddress_ZIPOrPostalCode_FirstFiveNumbers[0]'
+                    },
+                    'lastFour' => {
+                        key: 'F[0].Page_1[0].CurrentMailingAddress_ZIPOrPostalCode_LastFourNumbers[0]'
+                    } 
+                }
+            }
+            'emailAddress' => {
+                key: 'F[0].Page_1[0].EMAIL[0]'
+            },
+            'phoneNumber' => {
+                key: 'F[0].Page_1[0].EMAIL[1]'
+            },
+            'patientIdentification' => {
+                'patientFirstName' => {
+                    key: 'F[0].Page_1[0].VeteranFirstName[1]',
+                    limit: 12,
+                    question_num: 9,
+                    question_suffix: 'A',
+                    question_text: "PATIENT'S FIRST NAME"
+                },
+                'patientMiddleInitial' => {
+                    key: 'F[0].Page_1[0].VeteranMiddleInitial1[1]'
+                },
+                'patientLastName' => {
+                    key: 'F[0].Page_1[0].VeteranLastName[1]',
+                    limit: 18,
+                    question_num: 9,
+                    question_suffix: 'B',
+                    question_text: "PATIENT'S LAST NAME"
+                }
+                'patientSSN' => {
+                    'patientSsnFirst' => {
+                        key: 'F[0].Page_1[0].ClaimantsSocialSecurityNumber_FirstThreeNumbers[1]'
+                    },
+                    'patientSSNSecond' => {
+                        key: 'F[0].Page_1[0].ClaimantsSocialSecurityNumber_SecondTwoNumbers[1]'
+                    },
+                    'patientSsnThird' => {
+                        key: 'F[0].Page_1[0].ClaimantsSocialSecurityNumber_LastFourNumbers[1]'
+                    }
+                },
+                'patientVaFileNumber' => {
+                    key: 'F[0].Page_1[0].VAFileNumber[1]'
+                }
+            }
+            
+        }
+
+        def split_ssn
+            ssn = @form_data['veteranSocialSecurityNumber']
+            return if ssn.blank?
+            split_ssn = {
+              'first' => ssn[0..2],
+              'second' => ssn[3..4],
+              'third' => ssn[5..8]
+            }
+    
+            ['', '2'].each do |suffix|
+              @form_data["veteranSocialSecurityNumber#{suffix}"] = split_ssn
+            end
+    
+            split_ssn
+        end
+
+        # VA file number can be up to 10 digits long; An optional leading 'c' or 'C' followed by
+        # 7-9 digits. The file number field on the 4142 form has space for 9 characters so trim the
+        # potential leading 'c' to ensure the file number will fit into the form without overflow.
+        def extract_va_file_number(va_file_number)
+            return va_file_number if va_file_number.blank? || va_file_number.length < 10
+
+            va_file_number.sub(/^[Cc]/, '')
+        end
+
+
+        def merge_fields
+            # your class must include this method, it can be used to make changes to the form
+            # before final processing
+            split_ssn
+
+            @form_data['vaFileNumber'] = extract_va_file_number(@form_data['vaFileNumber'])
+
+            @form_data
+        end
+    end
+  end
+end
---
+++ lib/pdf_fill/forms/va21p4142a.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/ClassLength
+
+module PdfFill
+    module Forms
+     class Va21p4142a < FormBase
+     end
+    end
+end
