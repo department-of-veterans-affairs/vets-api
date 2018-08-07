61bd780dd8f778c88ad98b65f7e9ca99979a894a
Johnny Holton <johnny@holton.co>
2018-08-07 15:01:56 -0400

    process EVSS claims requests in background (#2171)
    * process EVSS claims requests in background via evss_claims_async_controller and Sidekiq

---
+++ app/workers/evss/update_claim_from_remote_job.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'evss/common_service'
+
+module EVSS
+  class UpdateClaimFromRemoteJob
+    include Sidekiq::Worker
+    sidekiq_options retry: false
+
+    def perform(user_uuid, claim_id)
+      Sentry::TagRainbows.tag
+      user = User.find user_uuid
+      claim = EVSSClaim.find claim_id
+      tracker = EVSSClaimsSyncStatusTracker.new(user_uuid: user_uuid, claim_id: claim_id)
+      auth_headers = EVSS::AuthHeaders.new(user).to_h
+      raw_claim = EVSS::ClaimsService.new(auth_headers).find_claim_by_id(claim.evss_id).body.fetch('claim', {})
+      claim.update_attributes(data: raw_claim)
+      tracker.set_single_status('SUCCESS')
+    rescue StandardError
+      tracker.set_single_status('FAILED')
+      raise
+    end
+  end
+end
