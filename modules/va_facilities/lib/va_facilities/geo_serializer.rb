7005380033b153d38282e2b6c826ef4327ffa20e
Patrick Vinograd <patrick@adhocteam.us>
2018-08-08 09:44:24 -0700

    Additional data elements in Feature

--- modules/va_facilities/lib/va_facilities/geo_serializer.rb
+++ modules/va_facilities/lib/va_facilities/geo_serializer.rb
@@ -42,7 +42,10 @@ module VaFacilities
         "website" => object.website,
         "address" => object.address,
         "phone" => object.phone,
-        "hours" => object.hours
+        "hours" => object.hours,
+        "services" => services(object),
+        "satisfaction" => object.feedback,
+        "wait_times" => object.access
       }
     end
 
@@ -57,6 +60,19 @@ module VaFacilities
       'va_cemetery' => 'nca',
       'vet_center' => 'vc'
     }.freeze
+
+    def self.services(object)
+      result = object.services.dup
+      if result.has_key?('health')
+        result['health'] = result['health'].map do |s|
+          [s['sl1'], s['sl2']]
+        end.flatten
+      end
+      if result.has_key?('benefits')
+        result['benefits'] = result['benefits']['standard']
+      end
+      result
+    end
 
   end
 end
