3e4af1e64d0d4212ee25dd0557fa5ca01aa36655
Lihan Li <frankieteardrop@gmail.com>
2018-08-09 16:20:20 -0400

    update form

--- lib/evss/dependents/configuration.rb
+++ lib/evss/dependents/configuration.rb
@@ -4,7 +4,7 @@ module EVSS
   module Dependents
     class Configuration < EVSS::Configuration
       def base_path
-        "http://internal-dsva-vetsgov-stag-forward-proxy-1891752030.us-gov-west-1.elb.amazonaws.com/wss-686-services-web-2.6/rest/"
+        "#{Settings.evss.url}/wss-686-services-web-2.6/rest/"
       end
 
       def service_name
