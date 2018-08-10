e5ede5392d929db52352caf537c5eb28a3c8b714
Keifer Furzland <kfrz.code@gmail.com>
2018-08-10 17:19:27 -0500

    Add aggregate_failure to spec

--- spec/lib/vet360/contact_information/service_spec.rb
+++ spec/lib/vet360/contact_information/service_spec.rb
@@ -210,7 +210,7 @@ describe Vet360::ContactInformation::Service, skip_vet360: true do
         end
       end
 
-      it 'does not include "failed_vet360_id_initializations" tag in sentry error' do
+      it 'does not include "failed_vet360_id_initializations" tag in sentry error', :aggregate_failures do
         VCR.use_cassette('vet360/contact_information/email_transaction_status_error', VCR::MATCH_EVERYTHING) do
           expect_any_instance_of(Vet360::Service).to receive(:log_message_to_sentry).with(anything, anything, anything)
           expect { subject.get_email_transaction_status(transaction_id) }.to raise_error do |e|
