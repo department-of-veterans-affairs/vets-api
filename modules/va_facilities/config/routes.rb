cfd79ba48a17545f5b340217a7fa0e038a8b394f
patrickvinograd <patrick@adhocteam.us>
2018-08-10 14:16:31 -0700

    Facilities module for public API (#2191)
    VA_Facilities API endpoint for external API consumers. Supports json/geojson results and a bulk "get all" endpoint in geojson and csv format.

---
+++ modules/va_facilities/config/routes.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+VaFacilities::Engine.routes.draw do
+  match '/v0/*path', to: 'application#cors_preflight', via: [:options]
+
+  namespace :v0 do
+    resources :facilities, only: %i[index show] do
+      get 'all', on: :collection
+    end
+  end
+
+  namespace :docs do
+    namespace :v0 do
+      resources :api, only: [:index]
+    end
+  end
+end
---
+++ modules/va_facilities/spec/requests/facilities_request_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Facilities API endpoint', type: :request do
+  include SchemaMatchers
+
+  let(:base_query_path) { '/services/va_facilities/v0/facilities?' }
+  let(:pdx_bbox) { 'bbox[]=-122.440689&bbox[]=45.451913&bbox[]=-122.786758&bbox[]=45.64' }
+
+  let(:setup_pdx) do
+    %w[vc_0617V nca_907 vha_648 vha_648A4 vha_648GI vba_348 vba_348a vba_348d vba_348e vba_348h].map { |id| create id }
+  end
+  let(:accept_json) { { 'HTTP_ACCEPT' => 'application/json' } }
+  let(:accept_geojson) { { 'HTTP_ACCEPT' => 'application/vnd.geo+json' } }
+  let(:accept_csv) { { 'HTTP_ACCEPT' => 'text/csv' } }
+
+  context 'when requesting JSON API format' do
+    it 'responds to GET #show for VHA prefix' do
+      create :vha_648A4
+      get '/services/va_facilities/v0/facilities/vha_648A4', nil, accept_json
+      expect(response).to be_success
+      expect(response.body).to be_a(String)
+      json = JSON.parse(response.body)
+      expect(json['data']['id']).to eq('vha_648A4')
+    end
+
+    it 'responds to GET #index with bbox' do
+      setup_pdx
+      get base_query_path + pdx_bbox, nil, accept_json
+      expect(response).to be_success
+      expect(response.body).to be_a(String)
+      json = JSON.parse(response.body)
+      expect(json['data'].length).to eq(10)
+    end
+  end
+
+  context 'when requesting GeoJSON format' do
+    it 'responds to GET #show for VHA prefix' do
+      create :vha_648A4
+      get '/services/va_facilities/v0/facilities/vha_648A4', nil, accept_geojson
+      expect(response).to be_success
+      expect(response.body).to be_a(String)
+      json = JSON.parse(response.body)
+      expect(json['type']).to eq('Feature')
+      expect(json['properties']['id']).to eq('vha_648A4')
+      expect(response.headers['Content-Type']).to eq 'application/vnd.geo+json; charset=utf-8'
+    end
+
+    it 'responds to GET #index with bbox' do
+      setup_pdx
+      get base_query_path + pdx_bbox, nil, accept_geojson
+      expect(response).to be_success
+      expect(response.body).to be_a(String)
+      json = JSON.parse(response.body)
+      expect(json['type']).to eq('FeatureCollection')
+      expect(json['features'].length).to eq(10)
+      expect(response.headers['Content-Type']).to eq 'application/vnd.geo+json; charset=utf-8'
+    end
+  end
+
+  context 'when requesting all facilities' do
+    it 'responds to GeoJSON format' do
+      setup_pdx
+      get '/services/va_facilities/v0/facilities/all', nil, accept_geojson
+      expect(response).to be_success
+      expect(response.headers['Content-Type']).to eq 'application/vnd.geo+json; charset=utf-8'
+      expect(response.body).to be_a(String)
+      json = JSON.parse(response.body)
+      expect(json['type']).to eq('FeatureCollection')
+    end
+
+    it 'responds to CSV format' do
+      setup_pdx
+      get '/services/va_facilities/v0/facilities/all', nil, accept_csv
+      expect(response).to be_success
+      expect(response.headers['Content-Type']).to eq 'text/csv'
+      expect(response.body).to be_a(String)
+    end
+  end
+
+  context 'with invalid request parameters' do
+    it 'returns 400 for nonsense bbox' do
+      get base_query_path + 'bbox[]=everywhere'
+      expect(response).to have_http_status(:bad_request)
+    end
+    it 'returns 400 for non-array bbox' do
+      get base_query_path + 'bbox=-90,180,45,80'
+      expect(response).to have_http_status(:bad_request)
+    end
+    it 'returns 400 for too many elements' do
+      get base_query_path + 'bbox[]=-45&bbox[]=-45&bbox[]=45&bbox=45&bbox=100'
+      expect(response).to have_http_status(:bad_request)
+    end
+    it 'returns 400 for not enough elements' do
+      get base_query_path + 'bbox[]=-45&bbox[]=-45&bbox[]=45'
+      expect(response).to have_http_status(:bad_request)
+    end
+    it 'returns 400 for non-numeric elements' do
+      get base_query_path + 'bbox[]=-45&bbox[]=-45&bbox[]=45&bbox=abc'
+      expect(response).to have_http_status(:bad_request)
+    end
+    it 'returns 400 for invalid type parameter' do
+      get base_query_path + pdx_bbox + '&type=bogus'
+      expect(response).to have_http_status(:bad_request)
+    end
+    it 'returns 400 for query with services but no type' do
+      get base_query_path + pdx_bbox + '&services[]=EyeCare'
+      expect(response).to have_http_status(:bad_request)
+    end
+    it 'returns 400 for health query with unknown service' do
+      get base_query_path + pdx_bbox + '&type=health&services[]=OilChange'
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+end
