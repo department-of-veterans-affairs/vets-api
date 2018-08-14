fd09e4f75c86148e6cdca322c5b0fa5db00695b9
Silvio Luthi <silvio@adhocteam.us>
2018-08-14 13:18:19 -0700

    Update disability compensation swagger docs

--- app/swagger/schemas/submit_disability_form.rb
+++ app/swagger/schemas/submit_disability_form.rb
@@ -6,19 +6,9 @@ module Swagger
       include Swagger::Blocks
 
       swagger_schema :SubmitDisabilityForm do
-        key :required, [:data]
+        key :required, [:job_id]
 
-        property :data, type: :object do
-          property :attributes, type: :object do
-            key :required, %i[claim_id end_product_claim_code end_product_claim_name inflight_document_id]
-            property :claim_id, type: :integer, example: -6_820_564_985_530_150_012
-            property :end_product_claim_code, type: :string, example: '400SUPP'
-            property :end_product_claim_name, type: :string, example: '400-eBenefits-Supplemental'
-            property :inflight_document_id, type: :integer, example: -7_166_975_058_082_066_996
-          end
-          property :id, type: :string, example: nil
-          property :type, type: :string, example: 'evss_disability_compensation_form_form_submit_response'
-        end
+        property :job_id, type: :string, example: 'gZEaC2dvIOgHLEk9Sw97Og'
       end
     end
   end
