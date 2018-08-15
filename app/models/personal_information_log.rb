c16139421419e9a634261f2e801a3953d9355566
Derek <derekhall77@gmail.com>
2018-08-15 16:37:50 -0400

    ugh... TODO fail

--- app/models/personal_information_log.rb
+++ app/models/personal_information_log.rb
@@ -3,7 +3,7 @@
 class PersonalInformationLog < ActiveRecord::Base
   validates(:data, :error_class, presence: true)
 
-  # TODO utility method for working with data persisted by logger middleware
+  # TODO: utility method for working with data persisted by logger middleware
   # consider removing once we have determined how we are going to analyze the data
   def decoded_data
     return data unless data.key?('request_body') && data.key?('response_body')
