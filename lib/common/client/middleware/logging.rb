6a7e74533f7d4ea66228d7a646d8d48b7fb8ec71
Derek <derekhall77@gmail.com>
2018-08-16 09:27:12 -0400

    great PR suggestions

--- lib/common/client/middleware/logging.rb
+++ lib/common/client/middleware/logging.rb
@@ -4,25 +4,23 @@ module Common
   module Client
     module Middleware
       class Logging < Faraday::Middleware
-        DEFAULT_TYPE_KEY = 'undefined'
-
         def initialize(app, type_key)
           super(app)
-          @type_key = type_key || DEFAULT_TYPE_KEY
+          @type_key = type_key
         end
 
         def call(env)
           request_body = Base64.encode64(env.body) if env.body
 
           @app.call(env).on_complete do |response_env|
-            PersonalInformationLog.create(
+            PersonalInformationLog.create!(
               error_class: @type_key, # TODO: error_class is probably worth renaming
               data: {
                 method: env.method,
                 url: env.url.to_s,
                 request_body: request_body,
                 response_body: Base64.encode64(response_env.body)
-              }.to_json
+              }
             )
           end
         end
