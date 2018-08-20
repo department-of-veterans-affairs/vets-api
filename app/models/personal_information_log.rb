92c2e1a8d9fccd822e9779008b753cd4e0617619
Alastair Dawson <alastair.j.dawson@gmail.com>
2018-08-20 13:08:52 -0700

    Post 911 gi bill logging (#2214)

--- app/models/personal_information_log.rb
+++ app/models/personal_information_log.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PersonalInformationLog < ActiveRecord::Base
+  scope :last_week, -> { where('created_at >= :date', date: 1.week.ago) }
   validates(:data, :error_class, presence: true)
 
   # TODO: utility method for working with data persisted by logger middleware
