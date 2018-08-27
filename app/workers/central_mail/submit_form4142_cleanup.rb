c03e96a689ab230a38ddbd90f2ea06c8a1e8ef70
Abhijit Kulkarni <akulkarni@meetveracity.com>
2018-08-27 09:24:51 -0400

    12487# Background process to submit 4142 to ICMHS
    1) Initial Set of files for 4142 PMR controller + model
    2) Added Centrail Mail Sidekiq worker class + clean up
    3) Async transaction class for 4142 submit

---
+++ app/workers/central_mail/submit_form4142_cleanup.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module CentralMail
+    class SubmitForm4142Cleanup
+      include Sidekiq::Worker
+
+      FORM_ID = '21-4142'
+
+      def perform(user_uuid)
+        user = User.find(user_uuid)
+
+        # Delete SiP form on successful submission
+        InProgressForm.form_for_user(FORM_ID, user)&.destroy
+      end
+    end
+ end
