fe0d2f10305e416a3e9979b936a3aae023908789
Johnny Holton <johnny@holton.co>
2018-08-30 13:21:49 -0400

    fix specs for PreNeeds to match schema changes (#2241)

--- spec/factories/preneeds/service_records.rb
+++ spec/factories/preneeds/service_records.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     service_branch 'AF' # Air Force
     discharge_type '1'
     highest_rank 'GEN'
-    national_guard_state 'N'
+    national_guard_state 'NY'
 
     date_range { attributes_for :date_range }
   end
