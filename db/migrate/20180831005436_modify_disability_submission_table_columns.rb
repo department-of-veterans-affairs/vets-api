222fab863c6c7446419206344a77db8a1760f9b7
Silvio Luthi <silvio@adhocteam.us>
2018-08-30 19:48:23 -0700

    Update migration

--- db/migrate/20180831005436_modify_disability_submission_table_columns.rb
+++ db/migrate/20180831005436_modify_disability_submission_table_columns.rb
@@ -1,8 +1,10 @@
 class ModifyDisabilitySubmissionTableColumns < ActiveRecord::Migration
   def change
-    add_column(:disability_compensation_submissions, :saved_claim_id, :integer)
+    add_column(:disability_compensation_submissions, :disability_compensation_id, :integer)
+    add_column(:disability_compensation_submissions, :async_transactions_id, :integer)
 
-    remove_index(:disability_compensation_submissions, name: :index_disability_compensation_submissions_on_uuid_and_form_type)
+    remove_index(:disability_compensation_submissions, name: :index_disability_compensation_submissions_on_uuid_and_form_type, column: [:user_uuid, :form_type])
+    remove_column(:disability_compensation_submissions, :job_id, :uuid)
     remove_column(:disability_compensation_submissions, :form_type, :string)
     remove_column(:disability_compensation_submissions, :status, :string)
     remove_column(:disability_compensation_submissions, :response, :json)
