43d8ff3f8edf21bd9878cbf76cf5c7e516dcdc83
SMLuthi <swisstifer@gmail.com>
2018-09-04 09:19:39 -0700

    526 claim job xref migration (#2248)
    * Update disability submission table to act as an xref table

---
+++ db/migrate/20180831180308_modify_disability_submission_table_columns.rb
@@ -0,0 +1,13 @@
+class ModifyDisabilitySubmissionTableColumns < ActiveRecord::Migration
+  def change
+    add_column(:disability_compensation_submissions, :disability_compensation_id, :integer)
+    add_column(:disability_compensation_submissions, :va526ez_submit_transaction_id, :integer)
+
+    remove_index(:disability_compensation_submissions, name: :index_disability_compensation_submissions_on_uuid_and_form_type, column: [:user_uuid, :form_type])
+    remove_column(:disability_compensation_submissions, :job_id, :uuid)
+    remove_column(:disability_compensation_submissions, :form_type, :string)
+    remove_column(:disability_compensation_submissions, :status, :string)
+    remove_column(:disability_compensation_submissions, :response, :json)
+    remove_column(:disability_compensation_submissions, :user_uuid, :uuid)
+  end
+end
