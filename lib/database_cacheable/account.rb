17369cf52e74ce32bd90d2c47da14045cafc3c95
Keifer Furzland <kfrz.code@gmail.com>
2018-09-04 17:08:53 -0500

    Add YARD doc to DatabaseCacheable

--- lib/database_cacheable/account.rb
+++ lib/database_cacheable/account.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module DatabaseCacheable
+  # The purpose of this module is to wrap around an Account record
+  # for caching, as well as providing a #cache? method to meet CacheAside
+  # implementation criteria
+  #
+  # @see lib/common/models/concerns/cache_aside.rb
+  #
   class Account
     attr_reader :user_account
 
