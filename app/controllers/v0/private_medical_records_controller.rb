253663fdf0858f5578d766ab77a302953198d0d0
Charles Stone <charles@zstones.com>
2018-09-04 09:36:36 -0700

    Add make_pdf function with endpoint to support testing and demonstration of PDF generation in isolation

--- app/controllers/v0/private_medical_records_controller.rb
+++ app/controllers/v0/private_medical_records_controller.rb
@@ -2,31 +2,53 @@
 
 module V0
   class PrivateMedicalRecordsController < ApplicationController
+    skip_before_action :authenticate
+
     FORM_ID = '21-4142'
 
+    # Use Submit Function to demonstrate PDF Generation in Isolation
     def submit
+      make_pdf
+    end
+
+    # Use this Submit Function for Production
+    # def submit
+    #   form_content = populate_form_content
+    #
+    #   if form_content.empty?
+    #     render json: { data: { attributes: { job_id: 'NA' } } },
+    #            status: :ok
+    #   else
+    #     # save the claim
+    #     claim = SavedClaim::PrivateMedicalRecord.new(form: form_content.to_json)
+    #     unless claim.save
+    #       StatsD.increment("#{stats_key}.failure")
+    #       raise Common::Exceptions::ValidationErrors, claim
+    #     end
+    #     StatsD.increment("#{stats_key}.success")
+    #
+    #     Rails.logger.info "ClaimID=#{claim.confirmation_number} Form=#{claim.class::FORM}"
+    #
+    #     jid = 0
+    #     # jid = CentralMail::SubmitForm4142Job.perform_async(
+    #     #   @current_user.uuid, form_content, claim
+    #     # )
+    #
+    #     render json: { data: { attributes: { job_id: jid } } },
+    #            status: :ok
+    #   end
+    # end
+
+    def make_pdf
       form_content = populate_form_content
 
       if form_content.empty?
-        render json: { data: { attributes: { job_id: 'NA' } } },
-               status: :ok
+        render json: {"message": "Unable to find form data"}, status: :ok
       else
-        # save the claim
-        claim = SavedClaim::PrivateMedicalRecord.new(form: form_content.to_json)
-        unless claim.save
-          StatsD.increment("#{stats_key}.failure")
-          raise Common::Exceptions::ValidationErrors, claim
-        end
-        StatsD.increment("#{stats_key}.success")
+        file_path = fill_ancillary_form(form_content, '12345678')
 
-        Rails.logger.info "ClaimID=#{claim.confirmation_number} Form=#{claim.class::FORM}"
-
-        jid = CentralMail::SubmitForm4142Job.perform_async(
-          @current_user.uuid, form_content, claim
-        )
-
-        render json: { data: { attributes: { job_id: jid } } },
-               status: :ok
+        render json: {"message": file_path}, status: :ok
+        # render pdf: file_path, status: :ok
       end
     end
 
@@ -52,6 +74,32 @@ module V0
         form_content = JSON.parse(form.form_data) unless form.nil?
       end
       form_content
+    end
+
+    def fill_ancillary_form(form_data, claim_id)
+      form_class = PdfFill::Forms::Va214142
+
+      folder = 'tmp/pdf'
+      FileUtils.mkdir_p(folder)
+      file_path = "#{folder}/#{FORM_ID}_#{claim_id}.pdf"
+
+      hash_converter = PdfFill::HashConverter.new(form_class.date_strftime)
+
+      new_hash = hash_converter.transform_data(
+        form_data: form_class.new(form_data).merge_fields,
+        pdftk_keys: form_class::KEY
+      )
+
+      PdfFill::Filler::PDF_FORMS.fill_form(
+        "lib/pdf_fill/forms/pdfs/#{FORM_ID}.pdf",
+        file_path,
+        new_hash,
+        flatten: false
+      )
+
+      PdfFill::Filler.combine_extras(file_path, hash_converter.extras_generator)
+
+      file_path
     end
   end
 end
