05ce7c53d233bb835836e08da325f57847357173
omgitsbillryan <bill.ryan@adhocteam.us>
2018-09-06 13:40:13 -0400

    use the relay state for redirect_to

--- app/controllers/v0/sessions_controller.rb
+++ app/controllers/v0/sessions_controller.rb
@@ -182,23 +182,17 @@ module V0
 
     def saml_callback_success_url
       if current_user.loa[:current] < current_user.loa[:highest]
-        SAML::SettingsService.idme_loa3_url(current_user, alt_relay: params['RelayState']&.start_with?('http'))
+        SAML::SettingsService.idme_loa3_url(current_user, success_relay: params['RelayState'])
+      elsif Settings.saml.relays&.to_h&.values&.include?(params['RelayState']) && params['RelayState'].blank? == false
+        params['RelayState']
       else
-        env_specific_success_url
+        (Settings.saml.relay || Saml.relay.relays.vetsgov) + '?token=' + @session.token
       end
     rescue NoMethodError
       Raven.user_context(user_context)
       Raven.tags_context(tags_context)
       log_message_to_sentry('SSO Callback Success URL', :warn)
       env_specific_success_url
-    end
-
-    def env_specific_success_url
-      if params['RelayState']&.start_with?('http')
-        Settings.saml.relay.alternate + '?token=' + @session.token
-      else
-        Settings.saml.relay.original + '?token=' + @session.token
-      end
     end
 
     def benchmark_tags(*tags)
