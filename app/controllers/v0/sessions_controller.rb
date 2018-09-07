fb5e52df08d1cebf1d117426463b0dd08c4dfb51
omgitsbillryan <bill.ryan@adhocteam.us>
2018-09-06 21:34:12 -0400

    config update

--- app/controllers/v0/sessions_controller.rb
+++ app/controllers/v0/sessions_controller.rb
@@ -183,7 +183,7 @@ module V0
     end
 
     def default_relay_url
-      (Settings.saml.relay || Saml.relay.relays.vetsgov)
+      (Saml.relay.relays.vetsgov || Settings.saml.relay )
     end
 
     def saml_login_relay_url
