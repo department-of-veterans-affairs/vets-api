09363b6bf986986a2fe4c7db7de47f1b0e072219
Kam Karshenas <kam@adhocteam.us>
2018-09-17 14:10:22 -0700

    update the specs for invalid secret and iv

--- spec/lib/aes_256_cbc_encryptor_spec.rb
+++ spec/lib/aes_256_cbc_encryptor_spec.rb
@@ -8,16 +8,19 @@ describe Aes256CbcEncryptor do
   let(:hex_iv)           { '00ABEB85AB5C293F15DDF1647449A00C' }
   subject { described_class.new(hex_secret, hex_iv) }
 
-  context 'it right pads short secrets and iv' do
+  context 'invalid secret' do
     let(:hex_secret) { 'EC121FF80513AE58ED478D5C5787075BF53C35733B' }
+
+    it 'secret must be 32 bytes' do
+      expect{subject}.to raise_error(ArgumentError, 'Secret must be 32 bytes.')
+    end
+  end
+
+  context 'invalid iv' do
     let(:hex_iv)     { '00ABEB85AB5C2' }
 
-    it 'returns a secret that is the correct length of 32 bytes' do
-      expect(subject.secret.length).to eq(32)
-    end
-
-    it 'returns an iv that is the correct length of 16 bytes' do
-      expect(subject.iv.length).to eq(16)
+    it 'iv must be 16 bytes' do
+      expect{subject}.to raise_error(ArgumentError, 'IV must be 16 bytes.')
     end
   end
 
