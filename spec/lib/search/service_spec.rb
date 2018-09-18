19a4f37849bbe8bfbc55530ebfeee9c1f9eedc28
hpjaj <hmlevine@gmail.com>
2018-09-18 09:02:02 -0600

    Merge conflict removal

--- spec/lib/search/service_spec.rb
+++ spec/lib/search/service_spec.rb
@@ -74,42 +74,5 @@ describe Search::Service do
         end
       end
     end
-
-    context 'with an empty search query' do
-      let(:query) { '' }
-
-      it 'returns a status of 400' do
-        VCR.use_cassette('search/status_400', VCR::MATCH_EVERYTHING) do
-          response = subject.results
-
-          expect(response).to be_a(Search::ResultsResponse)
-          expect(response.status).to eq 400
-        end
-      end
-    end
-
-    context 'with an invalid API access key' do
-      it 'returns a status of 400' do
-        VCR.use_cassette('search/invalid_access_key', VCR::MATCH_EVERYTHING) do
-          allow_any_instance_of(described_class).to receive(:access_key).and_return('INVALIDKEY')
-          response = subject.results
-
-          expect(response).to be_a(Search::ResultsResponse)
-          expect(response.status).to eq 400
-        end
-      end
-    end
-
-    context 'with an invalid affiliate' do
-      it 'returns a status of 400' do
-        VCR.use_cassette('search/invalid_affiliate', VCR::MATCH_EVERYTHING) do
-          allow_any_instance_of(described_class).to receive(:affiliate).and_return('INVALID')
-          response = subject.results
-
-          expect(response).to be_a(Search::ResultsResponse)
-          expect(response.status).to eq 400
-        end
-      end
-    end
   end
 end
