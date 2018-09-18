88e5e433782035214a0d7f8c3cc5167072218003
Edward Paget <edpaget@users.noreply.github.com>
2018-09-18 09:03:54 -0400

    Always log consumer_id and consumer_name in the same format (#2072)
    * Always log consumer_id and consumer_name in the same format
    
    * Add consumer id in upload controller
    
    * Fix passing error object to logger

--- modules/vba_documents/spec/factories/upload_submissions.rb
+++ modules/vba_documents/spec/factories/upload_submissions.rb
@@ -4,6 +4,8 @@ FactoryBot.define do
   factory :upload_submission, class: 'VBADocuments::UploadSubmission' do
     guid 'f7027a14-6abd-4087-b397-3d84d445f4c3'
     status 'pending'
+    consumer_id 'f7027a14-6abd-4087-b397-3d84d445f4c3'
+    consumer_name 'adhoc'
 
     trait :status_uploaded do
       status 'uploaded'
