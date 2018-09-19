f47d98f3829932fbe0cce0e0d6dc5227592d1310
Edward Paget <edward@cassetteta.pe>
2018-09-19 09:31:41 -0400

    Fix line length for rubucop

--- modules/veteran_verification/app/controllers/veteran_verification/v0/disability_rating_controller.rb
+++ modules/veteran_verification/app/controllers/veteran_verification/v0/disability_rating_controller.rb
@@ -18,7 +18,8 @@ module VeteranVerification
       end
 
       def auth_headers
-        EVSS::DisabilityCompensationAuthHeaders.new(@current_user).add_headers(EVSS::AuthHeaders.new(@current_user).to_h)
+        EVSS::DisabilityCompensationAuthHeaders.new(@current_user)
+          .add_headers(EVSS::AuthHeaders.new(@current_user).to_h)
       end
     end
   end
