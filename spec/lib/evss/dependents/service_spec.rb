2620dc36de9f642f90a2d9c30356298177768000
Lihan Li <frankieteardrop@gmail.com>
2018-09-21 15:01:04 -0700

    clean form record

--- spec/lib/evss/dependents/service_spec.rb
+++ spec/lib/evss/dependents/service_spec.rb
@@ -9,10 +9,23 @@ describe EVSS::Dependents::Service do
   describe '#retrieve' do
     it 'should get user details' do
       VCR.use_cassette(
-        'evss/dependents/retrieve'
+        'evss/dependents/retrieve',
+        VCR::MATCH_EVERYTHING
       ) do
         response = service.retrieve
         expect(response.body['submitProcess']['application']['has30Percent']).to eq(true)
+      end
+    end
+  end
+
+  describe '#clean_form' do
+    it 'should clean the form request' do
+      VCR.use_cassette(
+        'evss/dependents/clean_form',
+        record: :once
+      ) do
+        response = service.clean_form(get_fixture('dependents/retrieve'))
+        binding.pry; fail
       end
     end
   end
