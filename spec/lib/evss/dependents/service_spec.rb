ac08801afe02c5c6bd74176268fb63b67f6ac99e
Lihan Li <frankieteardrop@gmail.com>
2018-09-21 13:44:24 -0700

    686 service spec

---
+++ spec/lib/evss/dependents/service_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe EVSS::Dependents::Service do
+  let(:user) { create(:evss_user) }
+  let(:service) { described_class.new(user) }
+
+  describe '#retrieve' do
+    binding.pry; fail
+  end
+end
