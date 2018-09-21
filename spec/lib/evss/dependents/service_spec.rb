b6619962089d908ce2d01dbf9d8f61f326f507f0
Lihan Li <frankieteardrop@gmail.com>
2018-09-21 15:10:18 -0700

    returns form helper method

--- spec/lib/evss/dependents/service_spec.rb
+++ spec/lib/evss/dependents/service_spec.rb
@@ -6,14 +6,17 @@ describe EVSS::Dependents::Service do
   let(:user) { create(:evss_user) }
   let(:service) { described_class.new(user) }
 
+  def returns_form(response)
+    expect(response.body['submitProcess'].present?).to eq(true)
+  end
+
   describe '#retrieve' do
     it 'should get user details' do
       VCR.use_cassette(
         'evss/dependents/retrieve',
         VCR::MATCH_EVERYTHING
       ) do
-        response = service.retrieve
-        expect(response.body['submitProcess']['application']['has30Percent']).to eq(true)
+        returns_form(service.retrieve)
       end
     end
   end
@@ -23,8 +26,7 @@ describe EVSS::Dependents::Service do
       VCR.use_cassette(
         'evss/dependents/clean_form'
       ) do
-        response = service.clean_form(get_fixture('dependents/retrieve'))
-        expect(response.body['submitProcess'].present?).to eq(true)
+        returns_form(service.clean_form(get_fixture('dependents/retrieve')))
       end
     end
   end
