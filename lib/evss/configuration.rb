f0f9675c6c74e31304a141884161a48327bea8df
Lihan Li <frankieteardrop@gmail.com>
2018-09-25 11:56:38 -0700

    use set evss middlewares method

--- lib/evss/configuration.rb
+++ lib/evss/configuration.rb
@@ -38,18 +38,22 @@ module EVSS
         Settings.evss.root_cert_path.present?
     end
 
+    def set_evss_middlewares(faraday)
+      faraday.use      :breakers
+      faraday.use      EVSS::ErrorMiddleware
+      faraday.use      Faraday::Response::RaiseError
+      faraday.response :betamocks if mock_enabled?
+      # calls to EVSS returns non JSON responses for some scenarios that don't make it through VAAFI
+      # content_type: /\bjson$/ ensures only json content types are attempted to be parsed.
+      faraday.response :json, content_type: /\bjson$/
+      faraday.use :immutable_headers
+      faraday.adapter Faraday.default_adapter
+    end
+
     def connection
       @conn ||= Faraday.new(base_path, request: request_options, ssl: ssl_options) do |faraday|
-        faraday.use      :breakers
-        faraday.use      EVSS::ErrorMiddleware
-        faraday.use      Faraday::Response::RaiseError
-        faraday.response :betamocks if mock_enabled?
         faraday.response :snakecase, symbolize: false
-        # calls to EVSS returns non JSON responses for some scenarios that don't make it through VAAFI
-        # content_type: /\bjson$/ ensures only json content types are attempted to be parsed.
-        faraday.response :json, content_type: /\bjson$/
-        faraday.use :immutable_headers
-        faraday.adapter Faraday.default_adapter
+        set_evss_middlewares(faraday)
       end
     end
 
