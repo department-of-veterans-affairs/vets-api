190a2d62f7b0169e98453096b46f556ab912bc1f
Abhijit Kulkarni <akulkarni@meetveracity.com>
2018-09-25 17:33:12 -0400

    BAH Sprint 3 for 4142 (#2284)

--- spec/lib/pdf_fill/filler_spec.rb
+++ spec/lib/pdf_fill/filler_spec.rb
@@ -41,26 +41,26 @@ describe PdfFill::Filler do
     end
   end
 
+  def simplify_fields(fields)
+    fields.map do |field|
+      {
+        name: field.name,
+        value: field.value
+      }
+    end
+  end
+
+  def compare_pdfs(pdf1, pdf2)
+    fields = []
+    [pdf1, pdf2].each do |pdf|
+      fields << simplify_fields(described_class::PDF_FORMS.get_fields(pdf))
+    end
+
+    fields[0] == fields[1]
+  end
+
   describe '#fill_form', run_at: '2017-07-25 00:00:00 -0400' do
-    def simplify_fields(fields)
-      fields.map do |field|
-        {
-          name: field.name,
-          value: field.value
-        }
-      end
-    end
-
-    def compare_pdfs(pdf1, pdf2)
-      fields = []
-      [pdf1, pdf2].each do |pdf|
-        fields << simplify_fields(described_class::PDF_FORMS.get_fields(pdf))
-      end
-
-      fields[0] == fields[1]
-    end
-
-    %w[21P-527EZ 21P-530].each do |form_id|
+    %w[21P-530 21P-527EZ].each do |form_id|
       context "form #{form_id}" do
         %w[simple kitchen_sink overflow].each do |type|
           context "with #{type} test data" do
@@ -86,6 +86,7 @@ describe PdfFill::Filler do
 
               if type == 'overflow'
                 extras_path = the_extras_generator.generate
+
                 expect(
                   FileUtils.compare_file(extras_path, "spec/fixtures/pdf_fill/#{form_id}/overflow_extras.pdf")
                 ).to eq(true)
@@ -99,6 +100,47 @@ describe PdfFill::Filler do
 
               File.delete(file_path)
             end
+          end
+        end
+      end
+    end
+  end
+
+  describe '#fill_ancillary_form', run_at: '2017-07-25 00:00:00 -0400' do
+    %w[21-4142].each do |form_id|
+      %w[simple kitchen_sink overflow].each do |type|
+        context "with #{type} test data" do
+          let(:form_data) do
+            get_fixture("pdf_fill/#{form_id}/#{type}")
+          end
+
+          it 'should fill the form correctly' do
+            if type == 'overflow'
+              # compare_pdfs only compares based on filled fields, it doesn't read the extras page
+              the_extras_generator = nil
+              expect(described_class).to receive(:combine_extras).once do |old_file_path, extras_generator|
+                the_extras_generator = extras_generator
+                old_file_path
+              end
+            end
+
+            file_path = described_class.fill_ancillary_form(form_data, 1, form_id)
+
+            if type == 'overflow'
+              extras_path = the_extras_generator.generate
+
+              expect(
+                FileUtils.compare_file(extras_path, "spec/fixtures/pdf_fill/#{form_id}/overflow_extras.pdf")
+              ).to eq(true)
+
+              File.delete(extras_path)
+            end
+
+            expect(
+              compare_pdfs(file_path, "spec/fixtures/pdf_fill/#{form_id}/#{type}.pdf")
+            ).to eq(true)
+
+            File.delete(file_path)
           end
         end
       end
