d1060033c125a2480908d46b694e9b83f89bb6f6
Lihan Li <frankieteardrop@gmail.com>
2018-09-26 10:49:49 -0700

    app must have user

--- app/models/dependents_application.rb
+++ app/models/dependents_application.rb
@@ -3,6 +3,8 @@
 class DependentsApplication < Common::RedisStore
   include RedisForm
 
+  validates(:user, presence: true, unless: :persisted?)
+
   FORM_ID = '21-686C'
 
   def create_submission_job
