ca7f2ed57f69fa6160855d4bb967e73165f63aef
Silvio Luthi <silvio@adhocteam.us>
2018-09-26 12:12:37 -0700

    Added data migration and rake task

---
+++ lib/data_migrations/in_progress_form_initial_expiration.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module DataMigrations
+  module InProgressFormInitialExpiration
+    module_function
+
+    def run
+      rows_affected = 0
+
+      InProgressForm.where(expires_at: nil).find_each do |form|
+        form.expires_at = Time.current + 60.days
+        form.save!
+        rows_affected += 1
+      end
+
+      rows_affected
+    end
+  end
+end
