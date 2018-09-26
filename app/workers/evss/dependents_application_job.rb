2d5342b186849b507efabb1c399b83e88f63e0bf
Lihan Li <frankieteardrop@gmail.com>
2018-09-26 14:54:31 -0700

    add dep app job

---
+++ app/workers/evss/dependents_application_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module EVSS
+  class DependentsApplicationJob
+    include Sidekiq::Worker
+
+    def perform(id, form, user_uuid)
+      Sentry::TagRainbows.tag
+      dependents_application = DependentsApplication.find(id)
+      user = User.find(user_uuid)
+    end
+  end
+end
