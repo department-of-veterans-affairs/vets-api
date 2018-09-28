beb6fb4d68708b5c8f881c1353d4b7e8910fb3dd
Alastair Dawson <alastair.j.dawson@gmail.com>
2018-09-28 15:03:56 -0700

    rubocop

--- app/workers/evss/disability_compensation_form/submit_uploads.rb
+++ app/workers/evss/disability_compensation_form/submit_uploads.rb
@@ -20,7 +20,7 @@ module EVSS
       def perform(upload_data, claim_id, auth_headers)
         client = EVSS::DocumentsService.new(auth_headers)
         code = upload_data['confirmationCode']
-        file_body = SupportingEvidenceAttachment.find_by(guid: code)&.get_file.read
+        file_body = SupportingEvidenceAttachment.find_by(guid: code)&.get_file&.read
         raise ArgumentError, "supporting evidence attachment with guid #{code} has no file data" if file_body.nil?
         document_data = EVSSClaimDocument.new(
           evss_claim_id: claim_id,
