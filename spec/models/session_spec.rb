a72cf622d7557d86122df6efde71d758de957a5b
Kam Karshenas <kam@adhocteam.us>
2018-10-01 14:16:38 -0700

    update session_spec.rb to account for new vagovToken attribute

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -54,7 +54,8 @@ RSpec.describe Session, type: :model do
 
       it '#cookie_data returns certain attribute for user object' do
         expect(subject.cookie_data)
-          .to eq('patientIcn' => '1000123456V123456',
+          .to eq('vagovToken' => subject.token,
+                 'patientIcn' => '1000123456V123456',
                  'mhvCorrelationId' => '12345678901',
                  'expirationTime' => expry_time.iso8601(0))
       end
