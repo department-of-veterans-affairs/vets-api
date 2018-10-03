2837a1485600d6276cb0456c00d3a840286ece31
Lihan Li <frankieteardrop@gmail.com>
2018-10-03 13:01:22 -0700

    raise error if validate doesnt pass

--- app/workers/evss/dependents_application_job.rb
+++ app/workers/evss/dependents_application_job.rb
@@ -11,11 +11,20 @@ module EVSS
       dependents_application = DependentsApplication.find(id)
       user = User.find(user_uuid)
       service = Dependents::Service.new(user)
+
       # TODO: used cached method
       evss_form = service.retrieve
       merged_form = DependentsApplication.transform_form(form, evss_form)
       merged_form = service.clean_form(merged_form)
-      service.validate(merged_form)
+      res = service.validate(merged_form)
+
+      if res['errors'].present?
+        return dependents_application.update_attributes!(
+          state: 'failed',
+          response: res.to_json
+        )
+      end
+
     end
   end
 end
