ea0c3bd5cce3194387aa0598f0bc8e9ed3baf3d8
Lihan Li <frankieteardrop@gmail.com>
2018-10-03 07:55:30 -0700

    working on dep app job

--- app/workers/evss/dependents_application_job.rb
+++ app/workers/evss/dependents_application_job.rb
@@ -4,13 +4,18 @@ module EVSS
   class DependentsApplicationJob
     include Sidekiq::Worker
 
+    sidekiq_options retry: false
+
     def perform(id, form, user_uuid)
       Sentry::TagRainbows.tag
       dependents_application = DependentsApplication.find(id)
       user = User.find(user_uuid)
       service = Dependents::Service.new(user)
-      form = service.retrieve
-      # todo merge form
+      # TODO: used cached method
+      evss_form = service.retrieve
+      merged_form = DependentsApplication.transform_form(form, evss_form)
+      merged_form = service.clean_form(merged_form)
+      service.validate(merged_form)
     end
   end
 end
