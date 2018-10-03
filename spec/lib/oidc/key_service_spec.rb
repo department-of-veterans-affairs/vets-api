c75663386282f0ef639eb44c324b53f3301ba714
Edward Paget <edward@cassetteta.pe>
2018-10-03 16:52:04 -0400

    Correct settings for key services

--- spec/lib/oidc/key_service_spec.rb
+++ spec/lib/oidc/key_service_spec.rb
@@ -6,9 +6,17 @@ require 'oidc/key_service'
 RSpec.describe OIDC::KeyService do
   describe '::fetch_keys' do
     it 'should pull keys from the specified metadata endpoint' do
-      VCR.use_cassette('okta/keys') do
-        out = described_class.fetch_keys
-        expect(out['keys'].count).to eq(1)
+      with_settings(
+        Settings.oidc,
+        auth_server_metadata_url: 'https://example.com/oauth2/default/.well-known/oauth-authorization-server',
+        issuer: 'https://example.com/oauth2/default',
+        profile_api_url: 'https://example.com/api/v1/users/',
+        profile_api_token: 'token'
+      ) do
+        VCR.use_cassette('okta/keys') do
+          out = described_class.fetch_keys
+          expect(out['keys'].count).to eq(1)
+        end
       end
     end
   end
@@ -24,9 +32,17 @@ RSpec.describe OIDC::KeyService do
     end
 
     it 'should download new keys if it does not exist' do
-      VCR.use_cassette('okta/keys') do
-        key = described_class.get_key('1Z0tNc4Hxs_n7ySgwb6YT8JgWpq0wezqupEg136FZHU')
-        expect(key).to be_a(OpenSSL::PKey::RSA)
+      with_settings(
+        Settings.oidc,
+        auth_server_metadata_url: 'https://example.com/oauth2/default/.well-known/oauth-authorization-server',
+        issuer: 'https://example.com/oauth2/default',
+        profile_api_url: 'https://example.com/api/v1/users/',
+        profile_api_token: 'token'
+      ) do
+        VCR.use_cassette('okta/keys') do
+          key = described_class.get_key('1Z0tNc4Hxs_n7ySgwb6YT8JgWpq0wezqupEg136FZHU')
+          expect(key).to be_a(OpenSSL::PKey::RSA)
+        end
       end
     end
   end
