41e8134a5a60183e5a4266e7fd2bc5c82a2fc8b8
egarreau <ergarreau@gmail.com>
2018-10-09 13:59:43 -0500

    add specs

--- app/models/saved_claim/disability_compensation.rb
+++ app/models/saved_claim/disability_compensation.rb
@@ -23,8 +23,13 @@ class SavedClaim::DisabilityCompensation < SavedClaim
   end
 
   def to_submission_data(user)
+    form4142 = EVSS::DisabilityCompensationForm::Form4142.new(user, @form_hash).translate
+    # if form4142
+    #   @form_hash['form526']['overflowText'] = 'VA Form 21-4142/4142a has been completed by the applicant and sent to the PMR contractor for processing in accordance with M21-1 III.iii.1.D.2.'
+    # end
+
     {
-      'form_4142' => EVSS::DisabilityCompensationForm::Form4142.new(user, @form_hash).translate,
+      'form_4142' => form4142,
       'form_526' => EVSS::DisabilityCompensationForm::DataTranslation.new(
         user, @form_hash.except('attachments')
       ).translate,
