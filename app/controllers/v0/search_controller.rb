bcb2ff5bba557edec4777ada78eedfb42fa673e1
hpjaj <hmlevine@gmail.com>
2018-10-12 11:13:14 -0600

    Endpoint now optionally accepts an offset param
    This is so the FE can request ‘previous’ or ‘next’ sets of page results.

--- app/controllers/v0/search_controller.rb
+++ app/controllers/v0/search_controller.rb
@@ -7,7 +7,7 @@ module V0
     skip_before_action :authenticate
 
     def index
-      response = Search::Service.new(query).results
+      response = Search::Service.new(query, offset).results
 
       render json: response, serializer: SearchSerializer
     end
@@ -15,7 +15,7 @@ module V0
     private
 
     def search_params
-      params.permit(:query)
+      params.permit(:query, :offset)
     end
 
     # Returns a sanitized, permitted version of the passed query params.
@@ -25,6 +25,21 @@ module V0
     #
     def query
       sanitize search_params['query']
+    end
+
+    # The offset defines the number of results you want to skip from the first result.
+    # Search.gov's default is 0 and the maximum is 999.
+    #
+    # Returns a sanitized, permitted version of the passed offset params. If 'offset'
+    # is not supplied, it returns nil.
+    #
+    # @return [String]
+    # @return [NilClass]
+    # @see https://search.usa.gov/sites/7378/api_instructions
+    # @see https://api.rubyonrails.org/v4.2/classes/ActionView/Helpers/SanitizeHelper.html#method-i-sanitize
+    #
+    def offset
+      sanitize search_params['offset']
     end
   end
 end
