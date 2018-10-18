32cc0b52c0bdfba4a3f27336a2a2e7f4ee9169f9
Imee Cuison <cuison_imelda@bah.com>
2018-10-18 10:25:10 -0400

    WIP 781 pdf start

---
+++ lib/pdf_fill/forms/va210781.rb
@@ -0,0 +1,200 @@
+module PdfFill
+  module Forms
+    class Va210781a < FormBase
+      include FormHelper
+
+      KEY = {
+        'veteranFullName' => {
+          'first' => {
+            key: 'form1[0].#subform[0].ClaimantsFirstName[0]',
+            limit: 12,
+            question_num: 1,
+            question_suffix: 'A',
+            question_text: "VETERAN/BENEFICIARY'S FIRST NAME"
+          },
+          'middleInitial' => {
+            key: 'form1[0].#subform[0].ClaimantsMiddleInitial1[0]'
+          },
+          'last' => {
+            key: 'form1[0].#subform[0].ClaimantsLastName[0]',
+            limit: 18,
+            question_num: 1,
+            question_suffix: 'B',
+            question_text: "VETERAN/BENEFICIARY'S LAST NAME"
+          }
+        },
+          'veteranSocialSecurityNumber' => { #TODO ADD FOR OTHER SSN'S
+            'first' => {
+              key: 'form1[0].#subform[0].ClaimantsSocialSecurityNumber_FirstThreeNumbers[0]'
+            },
+            'second' => {
+              key: 'form1[0].#subform[0].ClaimantsSocialSecurityNumber_SecondTwoNumbers[0]'
+            },
+            'third' => {
+              key: 'form1[0].#subform[0].ClaimantsSocialSecurityNumber_LastFourNumbers[0]'
+            },
+            'vaFileNumber' => {
+              key: 'form1[0].#subform[0].VAFileNumber[0]'
+            },
+            'veteranDateOfBirth' => {
+              'month' => {
+                key: 'form1[0].#subform[0].DOBmonth[0]'
+              },
+              'day' => {
+                key: 'form1[0].#subform[0].DOBday[0]'
+              },
+              'year' => {
+                key: 'form1[0].#subform[0].DOByear[0]'
+              }
+            },
+            'veteranServiceNumber' => {
+              key: 'form1[0].#subform[0].VeteransServiceNumber[0]'
+            },
+            'email' => {
+              key: 'form1[0].#subform[0].PreferredEmail[0]'
+            },
+            'veteranPhone' => {
+              key: 'form1[0].#subform[0].PreferredEmail[1]'
+            },
+            'veteranSecondaryPhone' => {
+              key: 'form1[0].#subform[0].PreferredEmail[2]'
+            },
+            'incident' => {
+              limit: 2,
+              first_key: 'incidentDescription',
+              question_text: 'INCIDENTS',
+              question_num: 8,
+              'incidentDate' => {
+                'month' => {
+                  key: 'form1[0].#subform[0].DOBmonth[1]'
+                },
+                'day' => {
+                  key: 'form1[0].#subform[0].DOBday[1]'
+                },
+                'year' => {
+                  key: 'form1[0].#subform[0].DOByear[1]'
+                }
+              },
+              'unitAssignedDates' => {
+                'fromMonth' => {
+                  key: 'form1[0].#subform[0].DOBmonth[2]'
+                },
+                'fromDay' => {
+                  key: 'form1[0].#subform[0].DOBday[2]'
+                },
+                'fromYear' => {
+                  key: 'form1[0].#subform[0].DOByear[2]'
+                },
+                'toMonth' => {
+                  key: 'form1[0].#subform[0].DOBmonth[3]'
+                },
+                'toDay' => {
+                  key: 'form1[0].#subform[0].DOBday[3]'
+                },
+                'toYear' => {
+                  key: 'form1[0].#subform[0].DOByear[3]'
+                }
+              },
+              'incidentLocation' => {
+                question_num: 8,
+                limit: 3,
+                first_key: 'row0',
+                'row0' => {
+                  key: 'form1[0].#subform[0].TextField1[0]'
+                },
+                'row1' => {
+                  key: 'form1[0].#subform[0].TextField1[1]'
+                },
+                'row2' => {
+                  key: 'form1[0].#subform[0].TextField1[2]'
+                }
+              },
+              'unitAssigned' => {
+                question_num: 8,
+                limit: 3,
+                'row0' => {
+                  key: 'form1[0].#subform[0].TextField1[3]'
+                  limit: 30
+                },
+                'row1' => {
+                  key: 'form1[0].#subform[0].TextField1[4]'
+                  limit: 30
+                },
+                'row2' => {
+                  key: 'form1[0].#subform[0].TextField1[5]'
+                  limit: 30
+                }
+              },
+              'incidentDescription' => {
+                key: 'form1[0].#subform[0].Description[0]'
+              },
+              'medalsCitations': {
+                key: 'form1[0].#subform[0].Medals[0]'
+              },
+              'personInvolved' => {
+                'name' => {
+                  'first' => {
+                    key: 'form1[0].#subform[1].ClaimantsFirstName[1]'
+                  },
+                  'middleInitial' => {
+                    key: 'form1[0].#subform[1].ClaimantsMiddleInitial1[1]'
+                  },
+                  'last' => {
+                    key: 'form1[0].#subform[1].ClaimantsLastName[1]'
+                  }
+                },
+                'rank' => {
+                    key: 'form1[0].#subform[1].RANK4B[0]'
+                },
+                'injuryDeathDate' => {
+                  'month' => {
+                    key: 'form1[0].#subform[1].DOBmonth[4]'
+                  },
+                  'day' => {
+                    key: 'form1[0].#subform[1].DOBday[4]'
+                  },
+                  'year' => {
+                    key: 'form1[0].#subform[1].DOByear[4]'
+                  }
+                },
+                'injuryDeath' => {
+                  # TODO: DON'T KNOW HOW TO DO ENUM
+                },
+                'unitAssigned' => {
+                  question_num: 8,
+                  limit: 3,
+                  'row0' => {
+                    key: 'form1[0].#subform[1].TextField1[6]'
+                    limit: 30
+                  },
+                  'row1' => {
+                    key: 'form1[0].#subform[1].TextField1[7]'
+                    limit: 30
+                  },
+                  'row2' => {
+                    key: 'form1[0].#subform[1].TextField1[8]'
+                    limit: 30
+                  }
+                }
+
+              }
+            },
+          }
+
+      }.freeze
+
+      def expand_veteran_full_name
+        @form_data['veteranFullName'] = extract_middle_i(@form_data, 'veteranFullName')
+      end
+
+      #todo: expand_ssn - based on how many ssn's are on form
+
+      def merge_fields
+        expand_veteran_full_name
+
+        #todo expand_signature can copy from 781a
+      end
+
+    end
+  end
+end
--- spec/lib/pdf_fill/filler_spec.rb
+++ spec/lib/pdf_fill/filler_spec.rb
@@ -109,7 +109,7 @@ describe PdfFill::Filler do
   end
 
   describe '#fill_ancillary_form', run_at: '2017-07-25 00:00:00 -0400' do
-    %w[21-4142 21-0781a].each do |form_id|
+    %w[21-4142 21-0781a 21-0781].each do |form_id|
       context "form #{form_id}" do
         %w[simple kitchen_sink overflow].each do |type|
           context "with #{type} test data" do
@@ -143,7 +143,7 @@ describe PdfFill::Filler do
                 compare_pdfs(file_path, "spec/fixtures/pdf_fill/#{form_id}/#{type}.pdf")
               ).to eq(true)
 
-              File.delete(file_path)
+              # File.delete(file_path)
             end
           end
         end
