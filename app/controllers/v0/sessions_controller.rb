2bb56691555a8e3fe7cb6b688aa48b58673176c7
Derek <derekhall77@gmail.com>
2018-10-18 10:53:56 -0400

    destroy cookie from a later point in the logout flow

--- app/controllers/v0/sessions_controller.rb
+++ app/controllers/v0/sessions_controller.rb
@@ -70,6 +70,7 @@ module V0
       # in the future the FE shouldnt count on ?success=true
     ensure
       logout_request&.destroy
+      destroy_sso_cookie!
       redirect_to relay_state.logout_url + '?success=true'
     end
 
