978824bfda7bf664e8e3f808b1153fdc8bd782d2
Edward Paget <edward@cassetteta.pe>
2018-10-19 19:33:54 -0400

    Cops

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActiveRecord::SchemaDumper.ignore_tables = ["spatial_ref_sys"]
+ActiveRecord::SchemaDumper.ignore_tables = ['spatial_ref_sys']
