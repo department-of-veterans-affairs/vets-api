ebbca3033ff4e1f651dc30441ed144e7ca88619e
Kam Karshenas <kam@adhocteam.us>
2018-10-19 11:10:11 -0700

    replace rails default for rails with ARGV s and server, monkey patch

--- config/boot.rb
+++ config/boot.rb
@@ -3,3 +3,17 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+# BEGIN Monkeypatch Rails 4.2 to allow for localhost to work with cookies.
+if ARGV.first == 's' || ARGV.first == 'server'
+  require 'rails/commands/server'
+
+  module Rails
+    class Server
+      alias :default_options_bk :default_options
+      def default_options
+        default_options_bk.merge!(Host: '127.0.0.1')
+      end
+    end
+  end
+end
+# END Monkeypatch Rails 4.2 to allow for localhost to work with cookies.
