2bdcb6daa6d31a58f227cbc7ffad7a34e3008c36
Edward Paget <edpaget@users.noreply.github.com>
2018-10-22 16:07:01 -0500

    Load va facilities migrations from subdirectory properly (#2372)

--- modules/va_facilities/lib/va_facilities/engine.rb
+++ modules/va_facilities/lib/va_facilities/engine.rb
@@ -3,5 +3,13 @@
 module VaFacilities
   class Engine < ::Rails::Engine
     isolate_namespace VaFacilities
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
   end
 end
