c7f82287022185bb75aa8860e0388f9e0fe5c7c1
Silvio Luthi <silvio@adhocteam.us>
2018-10-23 10:02:28 -0700

    Remove all claim translation from test path

--- app/models/saved_claim/disability_compensation/form526_all_claim.rb
+++ app/models/saved_claim/disability_compensation/form526_all_claim.rb
@@ -5,8 +5,10 @@ class SavedClaim::DisabilityCompensation::Form526AllClaim < SavedClaim::Disabili
 
   private
 
+  # :nocov:
   def translate_data(user, form526)
     # TODO: This needs to implemented once EVSS has finalized their endpoint
     EVSS::DisabilityCompensationForm::DataTranslationAllClaim.new(user, form526).translate
   end
+  # :nocov:
 end
