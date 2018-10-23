5137f330e888906c1fb5f87e3e62a1bde25f17ce
Silvio Luthi <silvio@adhocteam.us>
2018-10-23 10:31:33 -0700

    Remove configs from test path

--- lib/evss/disability_compensation_form/configuration_increase_only.rb
+++ lib/evss/disability_compensation_form/configuration_increase_only.rb
@@ -3,9 +3,11 @@
 module EVSS
   module DisabilityCompensationForm
     class ConfigurationIncreaseOnly < EVSS::DisabilityCompensationForm::Configuration
+      # :nocov:
       def base_path
         "#{Settings.evss.url}/#{Settings.evss.service_name}/rest/form526/v1"
       end
+      # :nocov:
     end
   end
 end
