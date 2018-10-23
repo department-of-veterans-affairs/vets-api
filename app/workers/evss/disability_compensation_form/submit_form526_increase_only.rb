1bac66dfc7e7bbe86cbbe43c77c9ecfbc9ebea8f
Silvio Luthi <silvio@adhocteam.us>
2018-10-23 10:20:26 -0700

    Remove increase only serivce from test path

--- app/workers/evss/disability_compensation_form/submit_form526_increase_only.rb
+++ app/workers/evss/disability_compensation_form/submit_form526_increase_only.rb
@@ -3,11 +3,13 @@
 module EVSS
   module DisabilityCompensationForm
     class SubmitForm526IncreaseOnly < EVSS::DisabilityCompensationForm::SubmitForm526
+      # :nocov:
       def service(auth_headers)
         EVSS::DisabilityCompensationForm::ServiceIncreaseOnly.new(
           auth_headers
         )
       end
+      # :nocov:
     end
   end
 end
