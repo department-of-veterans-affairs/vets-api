3c4d4dadf685ab813e52e90896b17d6654582418
Edward Paget <edpaget@users.noreply.github.com>
2018-10-25 16:11:29 -0500

    Art.facility lat long migrations (#2363)
    * Support for location column in base_facilities
    Add postgis active record adapter
    Add location column with postgis st_point datatype
    Index column
    Populate the data in that column based on current lat/longs
    
    * Add line in readme about requiring postgis
    
    * Add OS X postgis native installation help
    
    * Move migrations to va facilities module
    
    create rake task for st point migration
    
    * Make sure rake task is globally available
    
    * Remove print statement
    
    * Disable coverage metrics for va facilities engine rb
    
    Since it's just loading rake tasks and migrations
    
    * Update modules/va_facilities/lib/tasks/facilities_tasks.rake
    
    Co-Authored-By: edpaget <ed.paget@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,6 +49,7 @@ unless ENV['NOCOVERAGE']
     add_group 'VaFacilities', 'modules/va_facilities/'
     add_group 'VeteranVerification', 'modules/veteran_verification/'
     add_filter 'version.rb'
+    add_filter 'modules/va_facilities/lib/va_facilities/engine.rb'
     SimpleCov.minimum_coverage_by_file 90
     SimpleCov.refuse_coverage_drop
   end
