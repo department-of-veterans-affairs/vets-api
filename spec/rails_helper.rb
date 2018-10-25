693621a8e985e55c9d188d9c349550ffe779f5c1
Kam Karshenas <kam@adhocteam.us>
2018-10-25 16:21:31 -0700

    change www.example.com to test-api.vets.gov in specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -160,6 +160,15 @@ RSpec.configure do |config|
 
   config.include StatsD::Instrument::Matchers
 
+  # Set the host used in specs to test-api.vets.gov instead of www.example.com
+  config.before :each, type: :request do
+    host! 'test-api.vets.gov'
+  end
+
+  config.before :each, type: :controller do
+    request.host = 'test-api.vets.gov'
+  end
+
   config.before(:all) do
     unless defined?(Sidekiq::Batch)
       Sidekiq::Batch = Class.new do
