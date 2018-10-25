33dc75a96d19053ae63cfeb242e98c663c583fd8
Erik Lopez <Erik.Lopez@ablevets.com>
2018-10-25 12:01:37 -0400

    fixed linting issue

--- app/controllers/v0/facilities/va_controller.rb
+++ app/controllers/v0/facilities/va_controller.rb
@@ -130,7 +130,7 @@ class V0::Facilities::VaController < FacilitiesController
   def validate_type_and_services_known
     return if params[:type] == 'cc_provider'
     raise Common::Exceptions::InvalidFieldValue.new('type', params[:type]) unless
-      BaseFacility::TYPES.include?(params[:type]) 
+      BaseFacility::TYPES.include?(params[:type])
     unknown = params[:services].to_a - BaseFacility::SERVICE_WHITELIST[params[:type]]
     raise Common::Exceptions::InvalidFieldValue.new('services', unknown) unless unknown.empty?
   end
