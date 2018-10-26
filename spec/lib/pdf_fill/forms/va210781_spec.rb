57fac1da4506243006df87b76759bc56d329510b
Hope Lam <hope.lam@rsdcgroup.com>
2018-10-26 17:10:23 -0400

    781 pdf

--- spec/lib/pdf_fill/forms/va210781_spec.rb
+++ spec/lib/pdf_fill/forms/va210781_spec.rb
@@ -19,7 +19,7 @@ describe PdfFill::Forms::Va210781 do
   end
   describe '#merge_fields' do
     it 'should merge the right fields', run_at: '2016-12-31 00:00:00 EDT' do
-      expect(described_class.new(get_fixture('pdf_fill/21-0781/kitchen_sink')).merge_fields).to eq(
+      expect(described_class.new(get_fixture('pdf_fill/21-0781/simple')).merge_fields).to eq(
         get_fixture('pdf_fill/21-0781/merge_fields')
       )
     end
@@ -93,4 +93,76 @@ describe PdfFill::Forms::Va210781 do
       end
     end
   end
+
+  describe '#expand_incident_date' do
+    incident = {
+      'incidentDate' => '2000-01-01'
+    }
+    it 'should expand the incident date correctly' do
+      expect(new_form_class.send(:expand_incident_date, incident)).to eq(
+        'month' => '01',
+        'day' => '01',
+        'year' => '2000'
+      )
+    end
+  end
+
+  # rubocop:disable Metrics/LineLength
+  describe '#expand_incident_location' do
+    it 'should expand the incident location into three lines one word each' do
+      expect(new_form_class.send(:expand_incident_location,
+                                 'incidentLocation' => 'abcdefghijklmnopqrs xxxxxxxxxxxxxxxxxx zzzzzzzzzzzzzzzzzzz')).to eq(
+                                   'row0' => 'abcdefghijklmnopqrs',
+                                   'row1' => 'xxxxxxxxxxxxxxxxxx',
+                                   'row2' => 'zzzzzzzzzzzzzzzzzzz'
+                                 )
+    end
+
+    it 'should ignore more than 90 characters' do
+      expect(JSON.parse(new_form_class.send(:expand_incident_location,
+                                            'incidentLocation' => 'abcdefghijklmno pqrstuvwxyz1234 abcdefghinopq rstuvwxyz1234 abcdefghijklmnopqrst uvwxyz1234').to_json)).to eq(
+                                              'row0' => 'abcdefghijklmno',
+                                              'row1' => 'pqrstuvwxyz1234 abcdefghinopq',
+                                              'row2' => 'rstuvwxyz1234',
+                                              'row3' => 'abcdefghijklmnopqrst',
+                                              'row4' => 'uvwxyz1234'
+                                            )
+    end
+  end
+
+  describe '#expand incident_unit_assignment' do
+    it 'should expand the incident unit assignment into three lines one word each' do
+      expect(new_form_class.send(:expand_incident_unit_assignment,
+                                 'unitAssigned' => 'abcdefghijklmnopqrs xxxxxxxxxxxxxxxxxx zzzzzzzzzzzzzzzzzzz')).to eq(
+                                   'row0' => 'abcdefghijklmnopqrs',
+                                   'row1' => 'xxxxxxxxxxxxxxxxxx',
+                                   'row2' => 'zzzzzzzzzzzzzzzzzzz'
+                                 )
+    end
+
+    it 'should expand the incident unit assignment into three lines multiple words' do
+      expect(new_form_class.send(:expand_incident_unit_assignment,
+                                 'unitAssigned' =>
+                                 'abcd defg hijk lmno pqrs xxxx yyyy zzzz aaaa bb cccc dddd eeee ffff ggg')).to eq(
+                                   'row0' => 'abcd defg hijk lmno pqrs xxxx',
+                                   'row1' => 'yyyy zzzz aaaa bb cccc dddd',
+                                   'row2' => 'eeee ffff ggg'
+                                 )
+    end
+
+    it 'should ignore more than 90 characters' do
+      expect(JSON.parse(new_form_class.send(
+        :expand_incident_unit_assignment,
+        'unitAssigned' =>
+        'abcdefghijklmno pqrstuvwxyz1234 abcdefghinopq rstuvwxyz1234 abcdefghijklmnopqrst uvwxyz1234'
+      ).to_json)).to eq(
+        'row0' => 'abcdefghijklmno',
+        'row1' => 'pqrstuvwxyz1234 abcdefghinopq',
+        'row2' => 'rstuvwxyz1234',
+        'row3' => 'abcdefghijklmnopqrst',
+        'row4' => 'uvwxyz1234'
+      )
+    end
+  end
+  # rubocop:enable Metrics/LineLength
 end
