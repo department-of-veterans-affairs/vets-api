7b3b8504d6ff706ed5f0e6676be06b32371b9b53
Keifer Furzland <kfrz.code@gmail.com>
2018-10-26 13:30:18 -0500

    Update specs for pagination

--- spec/lib/search/pagination_spec.rb
+++ spec/lib/search/pagination_spec.rb
@@ -3,26 +3,75 @@
 require 'rails_helper'
 
 describe Search::Pagination do
+  # raw_body instance data is determined by the VCR cassets in support/vcr_cassettes/search/*
   context 'when page number is 1' do
     let(:raw_body) do
       {
-        'total' => 85,
-        'next_offset' => 10
+        'web' =>
+          {
+            'total' => 85,
+            'next_offset' => 10
+          }
       }
     end
     subject { described_class.new(raw_body) }
 
-    it 'calculates the correct pagination object details' do
+    it 'calculates the correct pagination object details', :aggregate_failures do
       expect(subject.object).to include('current_page' => 1)
+      expect(subject.object).to include('per_page' => 10)
       expect(subject.object).to include('total_pages' => 9)
-      expect(subject.object).to include('results_per_page' => Search::Pagination::RESULTS_PER_PAGE)
+      expect(subject.object).to include('total_entries' => 85)
+    end
+  end
+
+  context 'when page number is 2' do
+    let(:raw_body) do
+      {
+        'web' =>
+          {
+            'total' => 85,
+            'next_offset' => 20
+          }
+      }
+    end
+    subject { described_class.new(raw_body) }
+
+    it 'calculates the correct pagination object details', :aggregate_failures do
+      expect(subject.object).to include('current_page' => 2)
+      expect(subject.object).to include('per_page' => 10)
+      expect(subject.object).to include('total_pages' => 9)
+      expect(subject.object).to include('total_entries' => 85)
+    end
+  end
+
+  context 'when page number is last page' do
+    let(:raw_body) do
+      {
+        'web' =>
+          {
+            'total' => 85,
+            'next_offset' => nil
+          }
+      }
+    end
+    subject { described_class.new(raw_body) }
+
+    it 'calculates the correct pagination object details', :aggregate_failures do
+      expect(subject.object).to include('current_page' => 9)
+      expect(subject.object).to include('per_page' => 10)
+      expect(subject.object).to include('total_pages' => 9)
+      expect(subject.object).to include('total_entries' => 85)
     end
   end
 
   context 'when page number given is greater than total pages' do
     let(:raw_body) do
       {
-        'total' => 85
+        'web' =>
+          {
+            'total' => 85,
+            'next_offset' => 1000
+          }
       }
     end
     subject { described_class.new(raw_body) }
