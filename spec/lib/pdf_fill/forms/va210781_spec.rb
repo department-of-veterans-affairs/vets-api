d318984a7693a87a57d2af8d382d8a3076b31386
Hope Lam <hope.lam@rsdcgroup.com>
2018-10-26 10:46:33 -0400

    latest 0781 pdf

--- spec/lib/pdf_fill/forms/va210781_spec.rb
+++ spec/lib/pdf_fill/forms/va210781_spec.rb
@@ -19,8 +19,8 @@ describe PdfFill::Forms::Va210781 do
   end
   describe '#merge_fields' do
     it 'should merge the right fields', run_at: '2016-12-31 00:00:00 EDT' do
-      expect(described_class.new(get_fixture('pdf_fill/21-0781a/simple')).merge_fields).to eq(
-        get_fixture('pdf_fill/21-0781a/merge_fields')
+      expect(described_class.new(get_fixture('pdf_fill/21-0781/kitchen_sink')).merge_fields).to eq(
+        get_fixture('pdf_fill/21-0781/merge_fields')
       )
     end
   end
