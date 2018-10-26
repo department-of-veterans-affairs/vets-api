e46152bb9e4d6006c4caf369665f58b56cfdbe02
SMLuthi <swisstifer@gmail.com>
2018-10-26 15:08:16 -0700

    14196 all claims endpoint (#2375)
    * Separate 'all_claims' and 'increase only' submissions into separate endpoints
    
    * Create separate saved claims models for 'all claims' and 'increase only'
    
    * Create separate workers for 'all claims' and 'increase only'. Update exhausted call back to not use instance methods

--- spec/factories/va526ez.rb
+++ spec/factories/va526ez.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :va526ez, class: SavedClaim::DisabilityCompensation do
+  factory :va526ez, class: SavedClaim::DisabilityCompensation::Form526IncreaseOnly do
     form(
       JSON.parse(
         File.read('spec/support/disability_compensation_form/front_end_submission.json')
