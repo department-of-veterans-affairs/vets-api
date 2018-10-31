a853c237dedc360de1bf5432cbdcf71b95fbaa4a
Johnny Holton <johnny@holton.co>
2018-10-31 15:09:02 -0400

    remove unused code

--- lib/evss/dependents/service.rb
+++ lib/evss/dependents/service.rb
@@ -8,27 +8,6 @@ module EVSS
       def retrieve
         perform(:get, 'load/retrieve')
       end
-
-      def clean_form(form)
-        perform(:post, 'inflightform/cleanForm', form.to_json, headers)
-      end
-
-      def validate(form)
-        perform(:post, 'inflightform/validateForm', form.to_json, headers)
-      end
-
-      def save(form)
-        perform(:post, 'inflightform/saveForm', form.to_json, headers)
-      end
-
-      def submit(form)
-        perform(
-          :post,
-          'form686submission/submit',
-          form.to_xml(root: 'submit686Request'),
-          'Content-Type' => 'application/xml'
-        )
-      end
     end
   end
 end
