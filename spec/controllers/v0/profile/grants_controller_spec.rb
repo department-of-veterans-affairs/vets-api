a0b55fecaa6f8a44d1ab00dfb231e2a450778289
Alex Teal <arteal89@gmail.com>
2018-10-31 12:20:44 -0400

    routes change, it needs index pending rspec test

---
+++ spec/controllers/v0/profile/grants_controller_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe V0::Profile::GrantsController, type: :controller do
+  context 'when user is logged in' do
+    pending 'returns the grants from okta'  
+  end
+end
