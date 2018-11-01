0c58aab9ee510e7be96403c9745bb76de05e0adf
Alex Teal <arteal89@gmail.com>
2018-11-01 15:37:58 -0400

    Fix swagger specs add description to the 204 for delete Use settings mocks in the tests since it'll probably be blank

--- spec/request/connected_applications_request_spec.rb
+++ spec/request/connected_applications_request_spec.rb
@@ -15,18 +15,34 @@ RSpec.describe 'Connected Applications API endpoint', type: :request do
 
   context 'with valid response from okta' do
     it 'should return list of grants by app' do
-      VCR.use_cassette('okta/grants') do
-        get '/v0/profile/connected_applications', nil, auth_header
-        expect(response).to have_http_status(:ok)
-        expect(response.body).to be_a(String)
-        expect(JSON.parse(response.body)['data'][0]['id']).to eq('0oa2ey2m6kEL2897N2p7')
+      with_settings(
+        Settings.oidc,
+        auth_server_metadata_url: 'https://example.com/oauth2/default/.well-known/oauth-authorization-server',
+        issuer: 'https://example.com/oauth2/default',
+        base_api_url: 'https://example.com/api/v1/',
+        base_api_token: 'token'
+      ) do
+        VCR.use_cassette('okta/grants') do
+          get '/v0/profile/connected_applications', nil, auth_header
+          expect(response).to have_http_status(:ok)
+          expect(response.body).to be_a(String)
+          expect(JSON.parse(response.body)['data'][0]['id']).to eq('0oa2ey2m6kEL2897N2p7')
+        end
       end
     end
 
     it 'should delete all the grants by app' do
-      VCR.use_cassette('okta/delete_grants') do
-        delete '/v0/profile/connected_applications/0oa2ey2m6kEL2897N2p7', nil, auth_header
-        expect(response).to have_http_status(:no_content)
+      with_settings(
+        Settings.oidc,
+        auth_server_metadata_url: 'https://example.com/oauth2/default/.well-known/oauth-authorization-server',
+        issuer: 'https://example.com/oauth2/default',
+        base_api_url: 'https://example.com/api/v1/',
+        base_api_token: 'token'
+      ) do
+        VCR.use_cassette('okta/delete_grants') do
+          delete '/v0/profile/connected_applications/0oa2ey2m6kEL2897N2p7', nil, auth_header
+          expect(response).to have_http_status(:no_content)
+        end
       end
     end
   end
