84e902c808515d514897830c436a97c3abc4cef9
Michael Bastos <bastosmichael@gmail.com>
2018-11-01 18:25:29 -0500

    fixed the JWT rabbit hole

--- modules/openid_auth/app/controllers/openid_auth/application_controller.rb
+++ modules/openid_auth/app/controllers/openid_auth/application_controller.rb
@@ -3,13 +3,5 @@
 module OpenidAuth
   class ApplicationController < ::OpenidApplicationController
     skip_before_action :set_tags_and_extra_content
-
-    def token_payload
-      @token_payload ||= if token
-                          #  pubkey = expected_key(token)
-                          #  return if pubkey.blank?
-                           JWT.decode(token, nil, false, algorithm: 'HS256')[0]['validated_token']
-                         end
-    end
   end
 end
