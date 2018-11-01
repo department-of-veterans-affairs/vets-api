cc466f824a1452db674f404dc64faa0ed6e9c838
Edward Paget <edward@cassetteta.pe>
2018-11-01 12:17:02 -0400

    Remove duplicated db/schema enable_extension

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20181017123729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "postgis"
   enable_extension "uuid-ossp"
   enable_extension "pg_trgm"
   enable_extension "btree_gin"
