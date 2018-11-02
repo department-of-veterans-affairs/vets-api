870075a6aad41c7f964d18047a1da5121dd7d3f1
Hope Lam <hope.lam@rsdcgroup.com>
2018-11-02 16:19:12 -0400

    form 8940 pdf education test data corrections and minor formatting

--- spec/lib/pdf_fill/forms/va218940_spec.rb
+++ spec/lib/pdf_fill/forms/va218940_spec.rb
@@ -20,13 +20,13 @@ describe PdfFill::Forms::Va218940 do
     new_form_class.instance_variable_get(:@form_data)
   end
 
-  # describe '#merge_fields' do
-  #   it 'should merge the right fields', run_at: '2016-12-31 00:00:00 EDT' do
-  #     expect(described_class.new(get_fixture('pdf_fill/21-8940/kitchen_sink')).merge_fields).to eq(
-  #       get_fixture('pdf_fill/21-8940/merge_fields')
-  #     )
-  #   end
-  # end
+  describe '#merge_fields' do
+    it 'should merge the right fields', run_at: '2016-12-31 00:00:00 EDT' do
+      expect(JSON.parse(described_class.new(get_fixture('pdf_fill/21-8940/kitchen_sink')).merge_fields.to_json)).to eq(
+        get_fixture('pdf_fill/21-8940/merge_fields')
+      )
+    end
+  end
 
   describe '#expand_ssn' do
     context 'ssn is not blank' do
