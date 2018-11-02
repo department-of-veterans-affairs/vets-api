b05b88a51e7fd9956a75cc3127d490cb61568f99
Alastair Dawson <alastair.j.dawson@gmail.com>
2018-11-02 11:52:16 -0700

    add migrations, models, and specs

---
+++ app/models/form526_job_status.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'upsert/active_record_upsert'
+
+class Form526JobStatus < ActiveRecord::Base
+  belongs_to :form526_submission
+
+  alias_attribute :submission, :form526_submission
+
+  STATUS = {
+    try: 'try',
+    success: 'success',
+    retryable_error: 'retryable_error',
+    non_retryable_error: 'non_retryable_error',
+    exhausted: 'exhausted'
+  }.freeze
+end
