4ac8f63f13cd3cdf48a04892ca05a5f2351ba7fa
Johnny Holton <johnny@holton.co>
2018-11-05 14:09:55 -0500

    686 Prefill: use DateTime instead of Date when converting from epoch miliseconds

--- app/models/form_profiles/va21686c.rb
+++ app/models/form_profiles/va21686c.rb
@@ -197,7 +197,7 @@ class FormProfiles::VA21686c < FormProfile
 
   def convert_date(date)
     return unless date
-    Date.strptime(date.to_s, '%Q').to_s
+    DateTime.strptime(date.to_s, '%Q').to_date.to_s
   end
 
   def prefill_dependents(children)
