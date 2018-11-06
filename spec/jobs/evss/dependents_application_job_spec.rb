ce4e5ddaf9b8cea8005f20a06dda6faa9262d03a
Lihan Li <frankieteardrop@gmail.com>
2018-11-06 14:09:50 -0800

    dep app job spec

--- spec/jobs/evss/dependents_application_job_spec.rb
+++ spec/jobs/evss/dependents_application_job_spec.rb
@@ -9,10 +9,16 @@ RSpec.describe EVSS::DependentsApplicationJob do
     it 'submits to the 686 api' do
       VCR.use_cassette(
         'evss/dependents/all',
-        record: :once
+        VCR::MATCH_EVERYTHING
       ) do
         dependents_application = create(:dependents_application, user: user)
         described_class.drain
+
+        dependents_application = DependentsApplication.find(dependents_application.id)
+        expect(dependents_application.state).to eq('success')
+        expect(dependents_application.parsed_response).to eq(
+          'submit686Response' => { 'confirmationNumber' => '600142505' }
+        )
       end
     end
   end
