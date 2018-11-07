f2997d61910ac0e4053171d797789693f332e93c
Alex Teal <arteal89@gmail.com>
2018-11-07 14:57:13 -0500

    refactor to use helper for configuring test okta

--- spec/request/connected_applications_request_spec.rb
+++ spec/request/connected_applications_request_spec.rb
@@ -5,23 +5,15 @@ require 'rails_helper'
 RSpec.describe 'Connected Applications API endpoint', type: :request do
   let(:token) { 'fa0f28d6-224a-4015-a3b0-81e77de269f2' }
   let(:auth_header) { { 'Authorization' => "Token token=#{token}" } }
-  let(:user) { build(:user, :loa3) }
+  let(:user) { create(:user, :loa3, uuid: '00u2fqgvbyT23TZNm2p7') }
 
   before do
-    user[:uuid] = '00u2fqgvbyT23TZNm2p7'
     Session.create(uuid: '00u2fqgvbyT23TZNm2p7', token: token)
-    User.create(user)
   end
 
   context 'with valid response from okta' do
     xit 'should return list of grants by app' do
-      with_settings(
-        Settings.oidc,
-        auth_server_metadata_url: 'https://example.com/oauth2/default/.well-known/oauth-authorization-server',
-        issuer: 'https://example.com/oauth2/default',
-        base_api_url: 'https://example.com/api/v1/',
-        base_api_token: 'token'
-      ) do
+      with_okta_configured do
         VCR.use_cassette('okta/grants') do
           get '/v0/profile/connected_applications', nil, auth_header
           expect(response).to have_http_status(:ok)
@@ -32,13 +24,7 @@ RSpec.describe 'Connected Applications API endpoint', type: :request do
     end
 
     xit 'should delete all the grants by app' do
-      with_settings(
-        Settings.oidc,
-        auth_server_metadata_url: 'https://example.com/oauth2/default/.well-known/oauth-authorization-server',
-        issuer: 'https://example.com/oauth2/default',
-        base_api_url: 'https://example.com/api/v1/',
-        base_api_token: 'token'
-      ) do
+      with_okta_configured do
         VCR.use_cassette('okta/delete_grants') do
           delete '/v0/profile/connected_applications/0oa2ey2m6kEL2897N2p7', nil, auth_header
           expect(response).to have_http_status(:no_content)
