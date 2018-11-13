c6a3c60d3218c8930edb0159885f8cd486d7cc7c
Johnny Holton <johnny@holton.co>
2018-11-13 14:40:00 -0500

    .

--- config/initializers/faraday_middleware.rb
+++ config/initializers/faraday_middleware.rb
@@ -2,6 +2,8 @@
 
 Faraday::Middleware.register_middleware remove_cookies: Common::Client::Middleware::Request::RemoveCookies
 Faraday::Middleware.register_middleware immutable_headers: Common::Client::Middleware::Request::ImmutableHeaders
+
 Faraday::Request.register_middleware rescue_timeout: Common::Client::Middleware::Request::RescueTimeout
+
 Faraday::Response.register_middleware hca_soap_parser: HCA::SOAPParser
 Faraday::Response.register_middleware rescue_timeout: Common::Client::Middleware::Response::RescueTimeout
