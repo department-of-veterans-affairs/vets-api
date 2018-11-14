5d497c95e5028dc90692f14880d8be5cd7a194b1
Alastair Dawson <alastair.j.dawson@gmail.com>
2018-11-13 17:18:56 -0800

    15113 pdf upload validation (#2573)

---
+++ spec/uploaders/validate_pdf_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ValidatePdf, uploader_helpers: true do
+  class ValidatePdfTest < CarrierWave::Uploader::Base
+    include ValidatePdf
+  end
+
+  def store_image
+    ValidatePdfTest.new.store!(file)
+  end
+
+  context 'with a file that is not a PDF' do
+    let(:file) { Rack::Test::UploadedFile.new('spec/fixtures/files/va.gif', 'image/gif') }
+
+    it 'should not raise an error' do
+      expect { store_image }.to_not raise_error
+    end
+  end
+
+  context 'with a valid PDF' do
+    let(:file) { Rack::Test::UploadedFile.new('spec/fixtures/files/doctors-note.pdf', 'application/pdf') }
+
+    it 'should not raise an error' do
+      expect { store_image }.to_not raise_error
+    end
+  end
+
+  context 'with an encrypted PDF' do
+    let(:file) { Rack::Test::UploadedFile.new('spec/fixtures/files/locked-pdf.pdf', 'application/pdf') }
+
+    it 'should raise an error' do
+      expect { store_image }.to raise_error(CarrierWave::UploadError, 'PDF is encrypted')
+    end
+  end
+
+  context 'with a corrupted PDF' do
+    let(:file) { Rack::Test::UploadedFile.new('spec/fixtures/files/malformed-pdf.pdf', 'application/pdf') }
+
+    it 'should raise an error' do
+      expect { store_image }.to raise_error(CarrierWave::UploadError, 'PDF is missing an end of file marker')
+    end
+  end
+end
