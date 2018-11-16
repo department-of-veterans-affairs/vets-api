4b6e1dc72c908b81575df8f14a0da2b10e9ed334
Michael Bastos <bastosmichael@gmail.com>
2018-11-16 17:58:48 -0600

    Returning MHV error when something fails or ICN is missing

--- modules/openid_auth/app/controllers/openid_auth/v0/validation_controller.rb
+++ modules/openid_auth/app/controllers/openid_auth/v0/validation_controller.rb
@@ -1,20 +1,29 @@
 # frozen_string_literal: true
 
 require_dependency 'openid_auth/application_controller'
+require 'mvi/errors/errors'
 
 module OpenidAuth
   module V0
     class ValidationController < ApplicationController
       def index
-        render json: validated_payload, serializer: OpenidAuth::ValidationSerializer
-      rescue StandardError => e
-        raise Common::Exceptions::InternalServerError, e
+        raise StandardError unless user_icn = @current_user.icn
+        render json: validated_payload(user_icn), serializer: OpenidAuth::ValidationSerializer
+      rescue StandardError
+        raise_error!
       end
 
       private
 
-      def validated_payload
-        @validated_payload ||= OpenStruct.new(token_payload.merge(va_identifiers: { icn: @current_user.icn }))
+      def raise_error!
+        raise Common::Exceptions::BackendServiceException.new(
+          'MHV_STATUS502',
+          source: self.class.to_s
+        )
+      end
+
+      def validated_payload(user_icn)
+        @validated_payload ||= OpenStruct.new(token_payload.merge(va_identifiers: { icn: user_icn }))
       end
     end
   end
