6883eddb8617ca427ea589880cd2486bdd5cb08c
Michael Bastos <bastosmichael@gmail.com>
2018-11-19 10:45:12 -0600

    adding 404 responses

--- modules/openid_auth/app/controllers/openid_auth/v0/validation_controller.rb
+++ modules/openid_auth/app/controllers/openid_auth/v0/validation_controller.rb
@@ -1,18 +1,34 @@
 # frozen_string_literal: true
 
 require_dependency 'openid_auth/application_controller'
+require 'awesome_print'
 
 module OpenidAuth
   module V0
     class ValidationController < ApplicationController
       def index
-        raise StandardError unless @current_user.icn
-        render json: validated_payload, serializer: OpenidAuth::ValidationSerializer
+        case @current_user.va_profile_status
+        when 'OK'
+          render json: validated_payload, serializer: OpenidAuth::ValidationSerializer
+        when 'server_error'
+          raise_error!
+        when 'not_found'
+          raise_not_found!
+        else
+          raise_not_found!
+        end
       rescue StandardError
         raise_error!
       end
 
       private
+
+      def raise_not_found!
+        raise Common::Exceptions::BackendServiceException.new(
+          'AUTHTOKEN_404',
+          source: self.class.to_s
+        )
+      end
 
       def raise_error!
         raise Common::Exceptions::BackendServiceException.new(
