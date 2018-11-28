19ce37ce4f6b42af5800635cd4b5a7bf4cbaf61a
Keifer Furzland <kfrz@users.noreply.github.com>
2018-11-27 18:05:36 -0600

    12413 create preferences endpoint (#2262)
    * Add alias for #preference_choices to Preference
    
    * Add route, controller, serializer and swagger docs for Preference
    
    * Add PreferencesController spec
    
    * Add json_body_for helper to RequestHelper
    
    * Add unique index to code column
    
    * Add non-null check on schema for preferences code
    
    * Add benefits preferences to factory

--- db/schema.rb
+++ db/schema.rb
@@ -333,11 +333,13 @@ ActiveRecord::Schema.define(version: 20181126171800) do
   add_index "preference_choices", ["preference_id"], name: "index_preference_choices_on_preference_id", using: :btree
 
   create_table "preferences", force: :cascade do |t|
-    t.string   "code"
+    t.string   "code",       null: false
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "preferences", ["code"], name: "index_preferences_on_code", unique: true, using: :btree
 
   create_table "preneed_submissions", force: :cascade do |t|
     t.string   "tracking_number",    null: false
