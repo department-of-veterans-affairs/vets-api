55503591a49e9ab718318b4389d4ea6a5709a148
Keifer Furzland <kfrz.code@gmail.com>
2018-12-04 11:20:43 -0600

    Add YARD docs and unit specs for User#preferences

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -452,4 +452,58 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe '#preferences' do
+    let(:user) { build(:user, :loa1) }
+
+    before(:each) do
+      allow_any_instance_of(User).to receive(:account).and_return(OpenStruct.new(id: 1))
+    end
+
+    context 'when the User has no UserPreferences' do
+      it 'returns an empty array' do
+        results = user.preferences
+        expect(results).to eq []
+      end
+    end
+
+    context 'when the User has a single Preference with UserPreferences' do
+      it 'returns a single object in the array' do
+        benefits = create(:preference, :benefits)
+        UserPreference.create(account_id: 1,
+                              preference_id: benefits.id,
+                              preference_choice_id: benefits.choices.first.id)
+
+        results = user.preferences
+
+        expect(results.size).to eq 1
+        expect(results.first).to have_key(:preference)
+        expect(results.first).to have_key(:user_preferences)
+      end
+    end
+
+    context 'when the User has multiple Preferences with UserPreferences' do
+      it 'returns an array of objects including preference/user preference pairs' do
+        benefits = create(:preference, :benefits)
+        notifications = create(:preference, :notifications)
+        UserPreference.create(account_id: 1,
+                              preference_id: benefits.id,
+                              preference_choice_id: benefits.choices.first.id)
+        UserPreference.create(account_id: 1,
+                              preference_id: notifications.id,
+                              preference_choice_id: notifications.choices.first.id)
+        UserPreference.create(account_id: 1,
+                              preference_id: notifications.id,
+                              preference_choice_id: notifications.choices.last.id)
+
+        results = user.preferences
+
+        expect(results.size).to eq 2
+        results.each do |result|
+          expect(result).to have_key(:preference)
+          expect(result).to have_key(:user_preferences)
+        end
+      end
+    end
+  end
 end
