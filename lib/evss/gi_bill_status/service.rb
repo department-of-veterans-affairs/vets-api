caa7a72af4eb7d17f702dd037861fe25b6f6f18a
Johnny Holton <johnny@holton.co>
2018-12-04 16:25:07 -0500

    Add `uptimeRemaining` to `BackendStatus` for gibs (#2631)
    * Add `uptimeRemaining` to `BackendStatus` for gibs

--- lib/evss/gi_bill_status/service.rb
+++ lib/evss/gi_bill_status/service.rb
@@ -15,7 +15,7 @@ module EVSS
       }.freeze
 
       def self.within_scheduled_uptime?
-        current_time = Time.now.in_time_zone(OPERATING_ZONE)
+        current_time = get_current_time
         if current_time.saturday?
           (OPERATING_HOURS[:start]...OPERATING_HOURS[:saturday_end]).cover?(current_time.hour)
         else
@@ -23,8 +23,20 @@ module EVSS
         end
       end
 
+      def self.seconds_until_downtime
+        if within_scheduled_uptime?
+          current_time = get_current_time
+          end_hour = current_time.saturday? ? OPERATING_HOURS[:saturday_end] : OPERATING_HOURS[:end]
+          tz = ActiveSupport::TimeZone.new(OPERATING_ZONE)
+          service_end_time = tz.parse(tz.today.to_s + ' ' + end_hour.to_s + ':00:00')
+          service_end_time - current_time
+        else
+          0
+        end
+      end
+
       def self.retry_after_time
-        current_time = Time.now.in_time_zone(OPERATING_ZONE)
+        current_time = get_current_time
         tz = ActiveSupport::TimeZone.new(OPERATING_ZONE)
         service_start_time = tz.parse(tz.today.to_s + ' 0' + OPERATING_HOURS[:start].to_s + ':00:00')
 
@@ -39,6 +51,12 @@ module EVSS
         response = OpenStruct.new(status: e.status, body: e.body)
         EVSS::GiBillStatus::GiBillStatusResponse.new(response.status, response)
       end
+
+      def self.get_current_time
+        Time.now.in_time_zone(OPERATING_ZONE)
+      end
+
+      private_class_method :get_current_time
     end
   end
 end
