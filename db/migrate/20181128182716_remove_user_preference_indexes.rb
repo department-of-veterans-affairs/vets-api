0f41db20b2b391da1e73e8b211277c997487823b
Keifer Furzland <kfrz.code@gmail.com>
2018-12-05 10:03:08 -0600

    Remove magic comment

--- db/migrate/20181128182716_remove_user_preference_indexes.rb
+++ db/migrate/20181128182716_remove_user_preference_indexes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class RemoveUserPreferenceIndexes < ActiveRecord::Migration
   def change
     remove_index(:user_preferences, :account_id)
--- db/migrate/20181128183509_add_back_user_preference_indexes.rb
+++ db/migrate/20181128183509_add_back_user_preference_indexes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddBackUserPreferenceIndexes < ActiveRecord::Migration
   disable_ddl_transaction!
    def change
