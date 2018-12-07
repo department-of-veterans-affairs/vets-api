b636e4962d4793be122038722d2c20cf2df8a8a7
Anna Carey <anna@adhocteam.us>
2018-12-07 15:05:11 -0500

    fix failing spec

--- spec/controllers/v0/sessions_controller_spec.rb
+++ spec/controllers/v0/sessions_controller_spec.rb
@@ -420,7 +420,7 @@ RSpec.describe V0::SessionsController, type: :controller do
           allow_any_instance_of(SSOService).to receive(:persist_authentication!).and_raise(NoMethodError)
           expect(Raven).to receive(:extra_context).twice
           expect(Raven).not_to receive(:user_context)
-          expect(Raven).not_to receive(:tags_context).once
+          expect(Raven).not_to receive(:tags_context).twice
           expect(controller).not_to receive(:log_message_to_sentry)
           post :saml_callback
         end
