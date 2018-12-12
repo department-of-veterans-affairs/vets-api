2d5ebce26c5019abe138155876cc7c70c59a6c00
ATeal <arteal89@gmail.com>
2018-12-12 13:26:52 -0500

    Update the test logging with the new generic log

--- modules/appeals_api/spec/requests/appeals_request_spec.rb
+++ modules/appeals_api/spec/requests/appeals_request_spec.rb
@@ -26,11 +26,11 @@ RSpec.describe 'Claim Appeals API endpoint', type: :request do
             'X-Consumer-Username' => 'TestConsumer',
             'X-VA-User' => 'adhoc.test.user'
         hash = Digest::SHA2.hexdigest '111223333'
-        expect(Rails.logger).to have_received(:info).with('Caseflow Request',
+        expect(Rails.logger).to have_received(:info).with('Appeals App Request',
                                                           'consumer' => 'TestConsumer',
                                                           'va_user' => 'adhoc.test.user',
                                                           'lookup_identifier' => hash)
-        expect(Rails.logger).to have_received(:info).with('Caseflow Response',
+        expect(Rails.logger).to have_received(:info).with('Appeals App Response',
                                                           'va_user' => 'adhoc.test.user',
                                                           'consumer' => 'TestConsumer',
                                                           'first_appeal_id' => '1196201',
