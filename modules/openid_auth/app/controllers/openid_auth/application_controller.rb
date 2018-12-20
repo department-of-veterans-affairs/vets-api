7e543159e9d1444af2347667f52ae0c3e35c10c1
Michael Bastos <bastosmichael@gmail.com>
2018-12-19 19:24:27 -0800

    Adding Logging to OpenId Validation end point

--- modules/openid_auth/app/controllers/openid_auth/application_controller.rb
+++ modules/openid_auth/app/controllers/openid_auth/application_controller.rb
@@ -3,5 +3,13 @@
 module OpenidAuth
   class ApplicationController < ::OpenidApplicationController
     skip_before_action :set_tags_and_extra_content
+    before_action :validate_user
+
+    def validate_user
+      raise Common::Exceptions::RecordNotFound, @current_user.uuid if @current_user.va_profile_status == 'NOT_FOUND'
+      raise Common::Exceptions::BadGateway if @current_user.va_profile_status == 'SERVER_ERROR'
+      obscure_token = Session.obscure_token(token)
+      Rails.logger.info("Logged in user with id #{@session.uuid}, token #{obscure_token}")
+    end
   end
 end
--- modules/openid_auth/app/controllers/openid_auth/v0/validation_controller.rb
+++ modules/openid_auth/app/controllers/openid_auth/v0/validation_controller.rb
@@ -6,7 +6,6 @@ module OpenidAuth
   module V0
     class ValidationController < ApplicationController
       def index
-        validate_user
         begin
           render json: validated_payload, serializer: OpenidAuth::ValidationSerializer
         rescue StandardError => e
@@ -15,11 +14,6 @@ module OpenidAuth
       end
 
       private
-
-      def validate_user
-        raise Common::Exceptions::RecordNotFound, @current_user.uuid if @current_user.va_profile_status == 'NOT_FOUND'
-        raise Common::Exceptions::BadGateway if @current_user.va_profile_status == 'SERVER_ERROR'
-      end
 
       def validated_payload
         @validated_payload ||= OpenStruct.new(token_payload.merge(va_identifiers: { icn: @current_user.icn }))
