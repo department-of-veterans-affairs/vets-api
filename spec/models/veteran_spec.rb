76b24b2e6b20064aba2559b3602a39203f571953
Charley Stran <charley.stran@gmail.com>
2018-12-28 11:07:58 -0600

    clean up spec

--- spec/models/veteran_spec.rb
+++ spec/models/veteran_spec.rb
@@ -7,19 +7,20 @@ describe Veteran do
     let(:user) { FactoryBot.create(:user, :loa3) }
     let(:auth_headers) { EVSS::AuthHeaders.new(user).to_h }
 
+    before do
+      @client_stub = instance_double('EVSS::CommonService')
+      allow(EVSS::CommonService).to receive(:new).with(auth_headers) { @client_stub }
+    end
+
     it 'should initialize from a user' do
-      client_stub = instance_double('EVSS::CommonService')
-      allow(EVSS::CommonService).to receive(:new).with(auth_headers) { client_stub }
-      allow(client_stub).to receive(:get_current_info) {get_fixture('json/veteran_with_poa')}
+      allow(@client_stub).to receive(:get_current_info) {get_fixture('json/veteran_with_poa')}
       veteran = Veteran.new(user)
       expect(veteran.veteran_name).to eq("JEFF TERRELL WATSON")
       expect(veteran.power_of_attorney.code).to eq("A1Q")
     end
 
     it 'should not bomb out if poa is missing' do
-      client_stub = instance_double('EVSS::CommonService')
-      allow(EVSS::CommonService).to receive(:new).with(auth_headers) { client_stub }
-      allow(client_stub).to receive(:get_current_info) {get_fixture('json/veteran_without_poa')}
+      allow(@client_stub).to receive(:get_current_info) {get_fixture('json/veteran_without_poa')}
       veteran = Veteran.new(user)
       expect(veteran.veteran_name).to eq("JEFF TERRELL WATSON")
       expect(veteran.power_of_attorney).to eq(nil)
