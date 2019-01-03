043cea01c074be768dc9d0046a548289668b0b4e
ATeal <arteal89@gmail.com>
2019-01-03 11:58:32 -0500

    remove the removal of the attributes

--- modules/claims_api/app/serializers/claims_api/serializer_base.rb
+++ modules/claims_api/app/serializers/claims_api/serializer_base.rb
@@ -2,19 +2,6 @@
 
 module ClaimsApi
   module SerializerBase
-    REMOVE_ATTRIBUTES = %i[
-      phase
-      phase_change_date
-      ever_phase_back
-      current_phase_back
-    ].freeze
-
-    def attributes(*args)
-      hash = super
-      REMOVE_ATTRIBUTES.each { |attr| hash.delete(attr) }
-      hash
-    end
-
     def status
       object.status_from_phase(phase)
     end
