a36f1bd1aebebabdfcb9d41d8683dff7437fc945
Michael Bastos <bastosmichael@gmail.com>
2019-01-04 17:45:46 -0600

    so simple

--- lib/evss/vso_search/service.rb
+++ lib/evss/vso_search/service.rb
@@ -5,9 +5,13 @@ module EVSS
     class Service < EVSS::Service
       configuration EVSS::VsoSearch::Configuration
 
+      def initialize(headers)
+        @headers = headers
+      end
+
       def get_current_info
         with_monitoring_and_error_handling do
-          perform(:post, 'getCurrentInfo').body
+          perform(:post, 'getCurrentInfo', nil, headers).body
         end
       end
     end
