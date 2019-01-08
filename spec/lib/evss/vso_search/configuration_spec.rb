072a7c13d8f77803b005220a4c626273a0d66424
Michael Bastos <bastosmichael@gmail.com>
2019-01-08 09:28:16 -0600

    increasing coverage

--- spec/lib/evss/vso_search/configuration_spec.rb
+++ spec/lib/evss/vso_search/configuration_spec.rb
@@ -3,6 +3,12 @@
 require 'rails_helper'
 
 describe EVSS::VsoSearch::Configuration do
+  describe '#base_path' do
+    it 'has a base path' do
+      expect(described_class.instance.base_path).to eq("#{Settings.evss.url}/wss-common-services-web-#{EVSS::VsoSearch::Configuration::API_VERSION}/rest/vsoSearch/11.0/")
+    end
+  end
+
   describe '#service_name' do
     it 'has the expected service name' do
       expect(described_class.instance.service_name).to eq('EVSS/VsoSearch')
@@ -11,7 +17,7 @@ describe EVSS::VsoSearch::Configuration do
 
   describe '#mock_enabled?' do
     it 'has a mock_enabled? method that returns a boolean' do
-      expect(Appeals::Configuration.instance.mock_enabled?).to be_in([true, false])
+      expect(described_class.instance.mock_enabled?).to be_in([true, false])
     end
   end
 end
