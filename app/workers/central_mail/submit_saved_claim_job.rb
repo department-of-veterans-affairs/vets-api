640997eb3f5cd103b69e2cdda8048521162c38d7
ATeal <arteal89@gmail.com>
2019-01-08 15:52:51 -0500

    Update saved claim job to use the new naming

--- app/workers/central_mail/submit_saved_claim_job.rb
+++ app/workers/central_mail/submit_saved_claim_job.rb
@@ -95,7 +95,7 @@ module CentralMail
         'receiveDt' => receive_date.strftime('%Y-%m-%d %H:%M:%S'),
         'uuid' => @claim.guid,
         'zipCode' => address['country'] == 'USA' ? address['postalCode'] : FOREIGN_POSTALCODE,
-        'source' => 'Vets.gov',
+        'source' => 'va.gov',
         'hashV' => form_pdf_metadata[:hash],
         'numberAttachments' => number_attachments,
         'docType' => @claim.form_id,
