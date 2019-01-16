fa24a1917f115d0e0f475066ea31e5797cf306ab
egarreau <ergarreau@gmail.com>
2019-01-16 14:01:34 -0600

    add ancillary item statuses and update specs

--- app/serializers/form526_job_status_serializer.rb
+++ app/serializers/form526_job_status_serializer.rb
@@ -4,6 +4,7 @@ class Form526JobStatusSerializer < ActiveModel::Serializer
   attribute :claim_id
   attribute :job_id
   attribute :status
+  attribute :ancillary_item_statuses
 
   def id
     nil
@@ -11,5 +12,13 @@ class Form526JobStatusSerializer < ActiveModel::Serializer
 
   def claim_id
     object.submission.submitted_claim_id
+  end
+
+  def ancillary_item_statuses
+    if object.job_class.include?("526")
+      object.submission.form526_job_statuses.select do |status|
+        status.id != object.id
+      end
+    end
   end
 end
