ac11bc8432f257846840d1d1471f98d25ce14031
Keifer Furzland <kfrz@users.noreply.github.com>
2019-01-24 10:02:50 -0600

    Refactor BackendServiceException Sentry logging (#2721)
    * Refactor BackendServiceException logging, to add the backtrace on the same line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -64,8 +64,7 @@ class ApplicationController < ActionController::API
   rescue_from 'Exception' do |exception|
     # report the original 'cause' of the exception when present
     if skip_sentry_exception_types.include?(exception.class)
-      Rails.logger.error "#{exception.message}."
-      Rails.logger.error exception.backtrace.join("\n") unless exception.backtrace.nil?
+      Rails.logger.error "#{exception.message}.", backtrace: exception.backtrace
     else
       extra = exception.respond_to?(:errors) ? { errors: exception.errors.map(&:to_hash) } : {}
       if exception.is_a?(Common::Exceptions::BackendServiceException)
