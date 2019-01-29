55e5d7e8bb3f57b35c40ae5c22d118e8eec7fcfe
Charley Stran <charley.stran@gmail.com>
2019-01-29 13:15:51 -0600

    add more logging

--- app/controllers/v0/bgs_controller.rb
+++ app/controllers/v0/bgs_controller.rb
@@ -6,13 +6,16 @@ class V0::BgsController < ApplicationController
   def index
     service = BGS::Services.new(
       env: 'development',
-      client_ip: '127.0.0.1',
+      client_ip: '172.30.2.135',
       client_station_id: '281',
       client_username: 'VAgovAPI',
       application: 'VAgovAPI',
       log: true,
       forward_proxy_url: 'https://internal-dsva-vagov-dev-fwdproxy-1893365470.us-gov-west-1.elb.amazonaws.com:4447'
     )
+    Rails.logger.warn '-----------'
+    Rails.logger.warn service.inspect
+    Rails.logger.warn '-----------'
     service.org.find_poas_by_claim_id('600118851')
   end
 end
