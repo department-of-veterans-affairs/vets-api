273472790cfd49ce6bbded6da127a0db637dfb6e
Charley Stran <charley.stran@gmail.com>
2019-01-29 12:40:12 -0600

    add extra logging

--- app/controllers/v0/bgs_controller.rb
+++ app/controllers/v0/bgs_controller.rb
@@ -10,6 +10,7 @@ class V0::BgsController < ApplicationController
       client_station_id: '281',
       client_username: 'VAgovAPI',
       application: 'VAgovAPI',
+      log: true,
       forward_proxy_url: 'https://internal-dsva-vagov-dev-fwdproxy-1893365470.us-gov-west-1.elb.amazonaws.com:4447'
     )
     service.org.find_poas_by_claim_id('123')
