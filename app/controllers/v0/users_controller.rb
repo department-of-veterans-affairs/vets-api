f8d0d611df824184a6dbe9794217452c75f2c6e5
Harry Levine <hmlevine@gmail.com>
2019-01-31 13:08:36 -0700

    External service backed UserSerializer attributes return null & populate a meta-outages hash with error details (#2738)
    * Exposes any errors captured in the MVI::Service#find_profile call
    
    Exposes errors that were collected as part of this PR:
    
    https://github.com/department-of-veterans-affairs/vets-api/pull/2731
    
    * Dedicated class for all of the user service authorization logic
    
    * Class for the error handling and serializing logic of a user’s profile
    
    During calls to MVI, EMIS, and Vet360 there are many possible error types.  This class deals with categorizing the captured errors, and serializing them into the format requested by the FE team.
    
    * Struct class that serves as the pre-serialized object for /user call
    
    Struct class serving as the pre-serialized object that is passed to the UserSerializer during the '/v0/user' endpoint call.
    
    * Fetches & serializes user's profile data for the '/v0/user' endpoint
    
    Fetches and serializes all of the initialized user's profile data that is returned in the '/v0/user' endpoint.
    
    If there are no external service errors, the status property is set to 200, and the outages property is set to nil.
    
    If there *are* errors from any associated external services, the status property is set to 296, and serialized versions of the errors are added to the `outages` array.
    
    * Updates UsersController & UserSerializer in light of pre-serializer

--- app/controllers/v0/users_controller.rb
+++ app/controllers/v0/users_controller.rb
@@ -3,7 +3,14 @@
 module V0
   class UsersController < ApplicationController
     def show
-      render json: @current_user
+      pre_serialized_profile = Users::Profile.new(current_user).pre_serialize
+
+      render(
+        json: pre_serialized_profile,
+        status: pre_serialized_profile.status,
+        serializer: UserSerializer,
+        meta: { errors: pre_serialized_profile.errors }
+      )
     end
   end
 end
