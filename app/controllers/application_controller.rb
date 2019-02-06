00d7bf4d244bc87f0559c409f35cda42afc3dbab
omgitsbillryan <bill.ryan@adhocteam.us>
2019-02-06 17:05:14 -0500

    dang rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,8 +39,8 @@ class ApplicationController < ActionController::API
   def clear_saved_form(form_id)
     InProgressForm.form_for_user(form_id, current_user)&.destroy if current_user
   end
-  
-  # foo
+
+  # foo foo
   # I'm commenting this out for now, we can put it back in if we encounter it
   # def action_missing(m, *_args)
   #   Rails.logger.error(m)
