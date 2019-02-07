cf4b6851d60b0f7f3a74facc2a00ef79718d5460
Edward Paget <edward@cassetteta.pe>
2019-02-07 11:43:39 -0500

    Remove memoized method

--- modules/openid_auth/app/controllers/openid_auth/v0/validation_controller.rb
+++ modules/openid_auth/app/controllers/openid_auth/v0/validation_controller.rb
@@ -15,11 +15,12 @@ module OpenidAuth
 
       private
 
-      def payload_object
-        @payload_object ||= OpenStruct.new(token_payload.merge(va_identifiers: { icn: nil }))
-      end
-
       def validated_payload
+        # Ensure the token has an `va_identifiers` key.
+        payload_object = OpenStruct.new(token_payload.merge(va_identifiers: { icn: nil }))
+
+        # Sometimes we'll already have an `icn` in the token. If we do, copy if down into `va_identifiers`
+        # for consistency. Otherwise use the ICN value we used to look up the MVI attributes.
         payload_object.va_identifiers[:icn] = payload_object.try(:icn) || @current_user.icn
         payload_object
       end
