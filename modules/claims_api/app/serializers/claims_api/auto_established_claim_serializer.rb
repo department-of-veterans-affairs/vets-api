f30a1d3b3864fd503e5ec8de81c6ab29e1f8e0d6
ATeal <arteal89@gmail.com>
2019-02-12 15:20:51 -0500

    Fix the serializer

--- modules/claims_api/app/serializers/claims_api/auto_established_claim_serializer.rb
+++ modules/claims_api/app/serializers/claims_api/auto_established_claim_serializer.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module ClaimsApi
-  class AutoEstablishedClaimSerializer
-    attributes :id
-    attributes :status
-    attributes :evss_id
+  class AutoEstablishedClaimSerializer < ActiveModel::Serializer
+    attribute :id
+    attribute :status
+    attribute :evss_id
   end
 end
