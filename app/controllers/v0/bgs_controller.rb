627a6b6157c0b1a58a65c069f31641358e0dc355
Charley Stran <charley.stran@gmail.com>
2019-02-12 08:25:44 -0600

    dynamically get the server IP

--- app/controllers/v0/bgs_controller.rb
+++ app/controllers/v0/bgs_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'socket'
 
 class V0::BgsController < ApplicationController
   skip_before_action :authenticate
@@ -6,7 +7,7 @@ class V0::BgsController < ApplicationController
   def find_poa_for_claim
     service = BGS::Services.new(
       env: 'Webtest',
-      client_ip: '172.30.2.135',
+      client_ip: server_ip_address,
       client_station_id: '281',
       client_username: 'VAgovAPI',
       external_uid: 'test',
@@ -16,5 +17,9 @@ class V0::BgsController < ApplicationController
       forward_proxy_url: 'https://internal-dsva-vagov-dev-fwdproxy-1893365470.us-gov-west-1.elb.amazonaws.com:4447'
     )
     service.org.find_poas_by_claim_id(params[:id])
+  end
+
+  def server_ip_address
+    Socket::getaddrinfo(Socket.gethostname,"echo",Socket::AF_INET)[0][3]
   end
 end
