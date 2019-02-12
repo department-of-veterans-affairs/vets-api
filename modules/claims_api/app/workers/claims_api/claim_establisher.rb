3a2da4d3a56cf6f55f079fafaf51e7ddbcb985f7
ATeal <arteal89@gmail.com>
2019-02-12 13:13:48 -0500

    Format class and remove some comments

--- modules/claims_api/app/workers/claims_api/claim_establisher.rb
+++ modules/claims_api/app/workers/claims_api/claim_establisher.rb
@@ -6,16 +6,10 @@ module ClaimsApi
   class ClaimEstablisher
     include Sidekiq::Worker
 
-    # parameter will probably be named something else
-    # once that model gets made
-
     def perform(auto_claim_id)
       auto_claim = ClaimsApi::AutoEstablishedClaim.find(auto_claim_id)
-      ## Get model persumably that model contrains the form attrs
-      form_data = auto_claim.form.to_internal
 
-      ## Not sure if we'll store these auth headers on the model as well
-      # or if that'll come in through parameters to the worker
+      form_data = auto_claim.form.to_internal
       auth_headers = auto_claim.auth_headers_encrypted
 
       response = service(auth_headers).submit_form526(form_data)
