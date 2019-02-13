4ea186b509a9b7aad1e000563c53089079cedd0e
Lihan Li <frankieteardrop@gmail.com>
2019-02-13 01:18:48 -0800

    add ineligibility_reason spec

--- spec/lib/hca/ee/service_spec.rb
+++ spec/lib/hca/ee/service_spec.rb
@@ -4,6 +4,17 @@ require 'rails_helper'
 
 describe HCA::EE::Service do
   describe '#lookup_user' do
+    context 'with a user that has an ineligibility_reason' do
+      it 'should get the ineligibility_reason' do
+        VCR.use_cassette(
+          'hca/ee/lookup_user_ineligibility_reason',
+          record: :always
+        ) do
+          described_class.new.lookup_user('0000001013030524V532318000000')
+        end
+      end
+    end
+
     it 'should lookup the user in the hca ee user', run_at: 'Fri, 08 Feb 2019 02:50:45 GMT' do
       VCR.use_cassette(
         'hca/ee/lookup_user',
