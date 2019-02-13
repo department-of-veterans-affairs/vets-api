fae8a58a64514cc883e4583e60650e46aacbdec2
ATeal <arteal89@gmail.com>
2019-02-13 10:31:27 -0500

    Claims Api engine append migrations

--- modules/claims_api/lib/claims_api/engine.rb
+++ modules/claims_api/lib/claims_api/engine.rb
@@ -3,5 +3,13 @@
 module ClaimsApi
   class Engine < ::Rails::Engine
     isolate_namespace ClaimsApi
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
   end
 end
