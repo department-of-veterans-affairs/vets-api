6bb2c0e332b01894d99bf1462166924e917481ed
ATeal <arteal89@gmail.com>
2019-02-19 15:57:53 -0500

    Ghetto test expanded with working auth headers Cassette will have to be re-recorded using our data so then we can call perform

--- modules/claims_api/spec/workers/claim_establisher_spec.rb
+++ modules/claims_api/spec/workers/claim_establisher_spec.rb
@@ -13,16 +13,18 @@ RSpec.describe ClaimsApi::ClaimEstablisher, type: :job do
 
   let(:user) { FactoryBot.create(:user, :loa3) }
   let(:auth_headers) do
-    EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h)
+    EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h).to_json
   end
   let(:claim) do
     create(:auto_established_claim,
            auth_headers: auth_headers)
   end
 
-  it 'submits successfully' do
-    expect do
-      subject.perform_async(claim.id)
-    end.to change(subject.jobs, :size).by(1)
+  it 'submits succesfully' do
+    VCR.use_cassette('evss/disability_compensation_form/submit_form') do
+      expect do
+        subject.perform_async(claim.id)
+      end.to change(subject.jobs, :size).by(1)
+    end
   end
 end
