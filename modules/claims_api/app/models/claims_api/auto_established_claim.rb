ed8248bca7f7aeef1a46c97ac41459ebcdbcae12
Michael Bastos <bastosmichael@gmail.com>
2019-02-18 22:54:34 -0600

    switching from alias method

--- modules/claims_api/app/models/claims_api/auto_established_claim.rb
+++ modules/claims_api/app/models/claims_api/auto_established_claim.rb
@@ -10,7 +10,7 @@ module ClaimsApi
     ESTABLISHED = 'established'
     ERRORED = 'errored'
 
-    alias :token, :id
+    alias token id
 
     def form
       @form ||= ClaimsApi::Form526.new(data: JSON.parse(form_data))
