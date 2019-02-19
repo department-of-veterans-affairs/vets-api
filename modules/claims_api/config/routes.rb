60e7c2b4228464bb1f0d08ce1f0e1a2403f1b45d
Charley Stran <charley.stran@gmail.com>
2019-02-19 09:40:01 -0600

    remove sub forms

--- modules/claims_api/config/routes.rb
+++ modules/claims_api/config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-FORM_526_SUBFORMS = %w[4142 0781 8940].freeze
-
 ClaimsApi::Engine.routes.draw do
   match '/v0/*path', to: 'application#cors_preflight', via: [:options]
 
@@ -10,9 +8,6 @@ ClaimsApi::Engine.routes.draw do
     namespace :forms do
       ## 526 Forms
       post '526', to: 'disability_compensation#form_526'
-      FORM_526_SUBFORMS.each do |sub_form|
-        post "526/:id/#{sub_form}", to: "disability_compensation#form_#{sub_form}"
-      end
     end
   end
 
--- modules/claims_api/spec/requests/disability_compensation_request_spec.rb
+++ modules/claims_api/spec/requests/disability_compensation_request_spec.rb
@@ -3,17 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Disability Claims ', type: :request do
-
   before do
-    @headers = {'X-VA-SSN': '796043735',
-               'X-VA-First-Name': 'WESLEY',
-               'X-VA-Last-Name': 'FORD',
-               'X-VA-EDIPI': '1007697216',
-               'X-Consumer-Username': 'TestConsumer',
-               'X-VA-User': 'adhoc.test.user',
-               'X-VA-Birth-Date': '1986-05-06T00:00:00+00:00',
-               'X-VA-Gender': 'M'}
-    @data = File.read(Rails.root.to_s + "/modules/claims_api/spec/fixtures/form_526.json")
+    @headers = { 'X-VA-SSN': '796043735',
+                 'X-VA-First-Name': 'WESLEY',
+                 'X-VA-Last-Name': 'FORD',
+                 'X-VA-EDIPI': '1007697216',
+                 'X-Consumer-Username': 'TestConsumer',
+                 'X-VA-User': 'adhoc.test.user',
+                 'X-VA-Birth-Date': '1986-05-06T00:00:00+00:00',
+                 'X-VA-Gender': 'M' }
+    @data = File.read(Rails.root.to_s + '/modules/claims_api/spec/fixtures/form_526.json')
   end
 
   it 'should return a successful response with all the data' do
@@ -21,5 +20,17 @@ RSpec.describe 'Disability Claims ', type: :request do
     parsed = JSON.parse(response.body)
     expect(parsed['data']['type']).to eq('claims_api_auto_established_claims')
     expect(parsed['data']['attributes']['status']).to eq('pending')
+  end
+
+  it 'should create the sidekick job' do
+    expect(ClaimsApi::ClaimEstablisher).to receive(:perform_async)
+    post '/services/claims/v0/forms/526', JSON.parse(@data), @headers
+  end
+
+  it 'should build the auth headers' do
+    auth_header_stub = instance_double('EVSS::DisabilityCompensationAuthHeaders')
+    expect(EVSS::DisabilityCompensationAuthHeaders).to receive(:new) { auth_header_stub }
+    expect(auth_header_stub).to receive(:add_headers)
+    post '/services/claims/v0/forms/526', JSON.parse(@data), @headers
   end
 end
