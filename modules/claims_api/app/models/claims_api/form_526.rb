a56fa7eae94809b2c67e3cbe2e15a515b624cb40
Michael Bastos <bastosmichael@gmail.com>
2019-02-20 05:46:23 -0600

    trying to seperate concerns in order to build in validations

--- modules/claims_api/app/models/claims_api/form_526.rb
+++ modules/claims_api/app/models/claims_api/form_526.rb
@@ -4,11 +4,25 @@
 module ClaimsApi
   class Form526
     include Virtus.model
-    attribute :data, Hash
+    extend ActiveModel::Validations
+
+    attribute :veteran, Hash
+    attribute :claimantCertification, Hash
+    attribute :standardClaim, Hash
+    attribute :applicationExpirationDate, Hash
+    attribute :serviceInformation, Hash
+    attribute :disabilities, Hash
+
+    # validates_presence_of :veteran, 
+    #                       :claimantCertification, 
+    #                       :standardClaim, 
+    #                       :applicationExpirationDate, 
+    #                       :serviceInformation, 
+    #                       :disabilities
 
     def to_internal
       {
-        "form526": @data,
+        "form526": self.attributes,
         "form526_uploads": [],
         "form4142": nil,
         "form0781": nil,
