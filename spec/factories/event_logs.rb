b63941857aba90faf4421d8f745da6f2f773b08e
Kam Karshenas <kam@adhocteam.us>
2019-02-22 14:42:02 -0700

    move model specs into folder

--- spec/factories/event_logs.rb
+++ spec/factories/event_logs.rb
@@ -1,13 +1,31 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :event_log do
-    request_id ''
+  factory :event_log, class: 'EventLog::EventLog' do
+    request_id { SecureRandom.uuid }
     type ''
-    ip_address 'MyString'
-    description 'MyString'
-    reference_class 'MyString'
-    reference_id 'MyString'
-    data ''
+    ip_address { [Faker::Internet.ip_v4_address, Faker::Internet.ip_v6_address].sample }
+    state nil
+    account_id nil
+    description nil
+    reference_class nil
+    reference_id nil
+    data { {} }
+  end
+
+  factory :login_init_log, parent: :event_log, class: 'EventLog::LoginInitLog' do
+    type 'EventLog::LoginInitLog'
+  end
+
+  factory :login_callback_log, parent: :event_log, class: 'EventLog::LoginCallbackLog' do
+    type 'EventLog::LoginCallbackLog'
+  end
+
+  factory :logout_init_log, parent: :event_log, class: 'EventLog::LogoutInitLog' do
+    type 'EventLog::LogoutInitLog'
+  end
+
+  factory :logout_callback_log, parent: :event_log, class: 'EventLog::LogoutCallbackLog' do
+    type 'EventLog::LogoutCallbackLog'
   end
 end
