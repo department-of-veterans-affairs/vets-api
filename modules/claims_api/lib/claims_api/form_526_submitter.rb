d594519913b124db7729a5a9d432c27504c0938c
Charley Stran <charley.stran@gmail.com>
2019-02-22 10:56:39 -0600

    make a task and extract

---
+++ modules/claims_api/lib/claims_api/form_526_submitter.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module ClaimsApi
+  class Form526Submitter
+    def self.submit_claim(auto_claim_id)
+      auto_claim = ClaimsApi::AutoEstablishedClaim.find(auto_claim_id)
+      form_data = auto_claim.form.to_internal
+      auth_headers = auto_claim.auth_headers
+      response = service(auth_headers).submit_form526(form_data)
+      auto_claim.evss_id = response.claim_id
+      auto_claim.status = ClaimsApi::AutoEstablishedClaim::SUCCESS
+      auto_claim.save
+    end
+
+    private
+
+    def service(auth_headers)
+      EVSS::DisabilityCompensationForm::ServiceAllClaim.new(
+        auth_headers
+      )
+    end
+  end
+end
