87bc12092c7223abf6bd3fd72a67039bd4b1ad34
Michael Bastos <bastosmichael@gmail.com>
2019-02-26 09:14:03 -0600

    validation errors working

--- modules/claims_api/app/models/claims_api/form_526.rb
+++ modules/claims_api/app/models/claims_api/form_526.rb
@@ -44,7 +44,7 @@ module ClaimsApi
         begin
           send("#{name}=", value)
         rescue StandardError
-          raise Common::Exceptions::ValidationErrors, "#{name} is not a valid attribute"
+          raise Common::Exceptions::ValidationErrors, self
         end
       end
     end
@@ -58,7 +58,7 @@ module ClaimsApi
     end
 
     def to_internal
-      raise Common::Exceptions::ValidationErrors, cleaned_errors unless valid?
+      raise Common::Exceptions::ValidationErrors, self unless valid?
       {
         "form526": attributes,
         "form526_uploads": [],
@@ -66,10 +66,6 @@ module ClaimsApi
         "form0781": nil,
         "form8940": nil
       }.to_json
-    end
-
-    def cleaned_errors
-      errors.messages.map {|key,value| "#{key}" + value.join(' and ')}.join(', ')
     end
   end
 end
