c78f59a49b134d849f0ec9ebeb6486bc56b5c484
ATeal <arteal89@gmail.com>
2019-02-26 07:54:11 -0500

    Setup the worker spec to be ready for a VCR cassette

--- modules/claims_api/spec/workers/claim_establisher_spec.rb
+++ modules/claims_api/spec/workers/claim_establisher_spec.rb
@@ -11,20 +11,24 @@ RSpec.describe ClaimsApi::ClaimEstablisher, type: :job do
 
   subject { described_class }
 
-  let(:user) { FactoryBot.create(:user, :loa3) }
+  let(:user) { FactoryBot.create(:user, :loa3, ssn: 796_104_437) }
   let(:auth_headers) do
-    EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h).to_json
+    EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h)
   end
   let(:claim) do
-    create(:auto_established_claim,
-           auth_headers: auth_headers)
+    claim = create(:auto_established_claim)
+    claim.auth_headers = auth_headers
+    claim.save
+    claim
   end
 
   it 'submits succesfully' do
-    VCR.use_cassette('evss/disability_compensation_form/submit_form') do
+    VCR.use_cassette('evss/disability_compensation_form/external_api/submit_form') do
       expect do
         subject.perform_async(claim.id)
       end.to change(subject.jobs, :size).by(1)
+
+      # perform = subject.new.perform(claim.id)
     end
   end
 end
