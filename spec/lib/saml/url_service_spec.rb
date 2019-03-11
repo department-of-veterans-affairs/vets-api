5c1e374a31ca28ff7741b28cf121305b9c61e04c
Kam Karshenas <kam@adhocteam.us>
2019-03-11 17:36:43 -0400

    update the specs for sessions_controller

--- spec/lib/saml/url_service_spec.rb
+++ spec/lib/saml/url_service_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 require 'saml/url_service'
+require 'support/url_service_helpers'
 
 RSpec.describe SAML::URLService do
   SAML::URLService::VIRTUAL_HOST_MAPPINGS.each do |vhost_url, values|
@@ -215,31 +216,5 @@ RSpec.describe SAML::URLService do
         end
       end
     end
-  end
-
-  RSpec::Matchers.define :be_an_idme_saml_url do |expected_url_partial|
-    match do |actual_url|
-      begin
-        # Use the URI library to parse the string, returning false if this fails.
-        query_params = CGI.parse(URI.parse(actual_url).query)
-        relay_state_params = query_params['RelayState'].present? ? JSON.parse(query_params['RelayState']&.first) : nil
-        other_params = Hash[query_params.except('RelayState', 'SAMLRequest').map { |k, v| [k, v.first] }]
-        values_match?(Regexp.new(Regexp.escape(expected_url_partial)), actual_url) &&
-          (relay_state_params.present? ? values_match?(relay_state_params, @relay_state_params) : true) &&
-          (other_params.present? ? values_match?(other_params, @other_params) : true)
-      rescue URI::InvalidURIError
-        false
-      end
-    end
-
-    chain :with_relay_state do |relay_state_params|
-      @relay_state_params = relay_state_params
-    end
-
-    chain :with_params do |other_params|
-      @other_params = other_params
-    end
-
-    diffable
   end
 end
