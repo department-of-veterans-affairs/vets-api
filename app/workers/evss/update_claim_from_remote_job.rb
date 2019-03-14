94e2a79337cf62e23cd516c457f62ce3f253013f
Alastair Dawson <alastair.j.dawson@gmail.com>
2019-03-14 14:43:21 -0700

    added active record timeout error handling to UpdateClaimFromRemoteJob

--- app/workers/evss/update_claim_from_remote_job.rb
+++ app/workers/evss/update_claim_from_remote_job.rb
@@ -11,15 +11,24 @@ module EVSS
       Sentry::TagRainbows.tag
       user = User.find user_uuid
       claim = EVSSClaim.find claim_id
-      tracker = EVSSClaimsSyncStatusTracker.find_or_build(user_uuid)
-      tracker.claim_id = claim_id
       auth_headers = EVSS::AuthHeaders.new(user).to_h
       raw_claim = EVSS::ClaimsService.new(auth_headers).find_claim_by_id(claim.evss_id).body.fetch('claim', {})
       claim.update_attributes(data: raw_claim)
-      tracker.set_single_status('SUCCESS')
-    rescue StandardError
-      tracker.set_single_status('FAILED')
+      tracker(user_uuid, claim_id).set_single_status('SUCCESS')
+    rescue ActiveRecord::ConnectionTimeoutError
+      tracker(user_uuid, claim_id)&.set_single_status('FAILED')
       raise
+    rescue StandardError
+      tracker(user_uuid, claim_id)&.set_single_status('FAILED')
+      raise
+    end
+
+    private
+
+    def tracker(user_uuid, claim_id)
+      tracker = EVSSClaimsSyncStatusTracker.find_or_build(user_uuid)
+      tracker.claim_id = claim_id
+      tracker
     end
   end
 end
