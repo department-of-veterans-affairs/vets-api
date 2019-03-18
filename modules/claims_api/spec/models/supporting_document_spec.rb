a05b1e6faa76ba52f215a7ef4fdec1f445f8680b
Alex R Teal <arteal89@gmail.com>
2019-03-18 15:14:34 -0400

    Add support for uploading documents to auto established claims (#2868)
    * Add support for uploading documents to auto established claims
    
    * Tests for the requests, and fix up factory
    
    * Add model test for encrypted attr
    
    * add back the clause for setting aws, spec update
    
    * fix factory by specifying class
    
    * Add test for set_file_data!
    
    * Change the response to empty 200
    
    * first draft of upload documents documentation
    
    * Update the route with better nomenclature
    
    * Update the spec with the new route
    
    * update documentation with the new route + fix bug
    
    * mimic the blurbs up top

--- modules/claims_api/spec/models/supporting_document_spec.rb
+++ modules/claims_api/spec/models/supporting_document_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe ClaimsApi::SupportingDocument, type: :model do
       expect(subject).to encrypt_attr(:file_data)
     end
   end
+
+  describe '#set_file_data!' do
+    it 'should store the filename' do
+      attachment = build(:supporting_document)
+
+      attachment.set_file_data!(
+        Rack::Test::UploadedFile.new(
+          "#{::Rails.root}/modules/claims_api/spec/fixtures/extras.pdf"
+        )
+      )
+      attachment.save!
+      attachment.reload
+
+      expect(attachment.file_data).to have_key('filename')
+    end
+  end
 end
