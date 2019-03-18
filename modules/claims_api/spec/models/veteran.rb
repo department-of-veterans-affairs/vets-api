ea094bbbbc188d3b77581b7e020af64611431293
Charley Stran <charley.stran@gmail.com>
2019-03-18 08:35:16 -0500

    refactor spec for claim api veteran

--- modules/claims_api/spec/models/veteran.rb
+++ modules/claims_api/spec/models/veteran.rb
@@ -4,6 +4,15 @@ require 'rails_helper'
 
 RSpec.describe ClaimsApi::Veteran, type: :model do
   describe 'attributes needed for MVI lookup' do
+    let(:headers) do
+      {
+        'X-VA-SSN' => '123456789',
+        'X-VA-First-Name' => 'MARK',
+        'X-VA-Last-Name' => 'WEBB',
+        'X-VA-Birth-Date' => '1928-01-01'
+      }
+    end
+
     before do
       @veteran = ClaimsApi::Veteran.new
       @veteran.loa = { current: 3, highest: 3 }
@@ -22,13 +31,13 @@ RSpec.describe ClaimsApi::Veteran, type: :model do
       expect(@veteran.dslogon_edipi).to be(@veteran.edipi)
     end
 
+    it 'should set edipi not passed in headers' do
+      headers['X-VA-EDIPI'] = '12345'
+      veteran = ClaimsApi::Veteran.from_headers(headers)
+      expect(veteran.edipi).to eq('12345')
+    end
+
     it 'should not set edipi if not passed in headers' do
-      headers = {
-        'X-VA-SSN' => '123456789',
-        'X-VA-First-Name' => 'MARK',
-        'X-VA-Last-Name' => 'WEBB',
-        'X-VA-Birth-Date' => '1928-01-01'
-      }
       veteran = ClaimsApi::Veteran.from_headers(headers)
       expect(veteran.edipi).to be(nil)
     end
