a0f8bb94bf0d27381bf7d8b778db86d250e0b0bd
Lihan Li <frankieteardrop@gmail.com>
2019-03-18 22:24:22 -0700

    lint

--- modules/claims_api/app/models/claims_api/supporting_document.rb
+++ modules/claims_api/app/models/claims_api/supporting_document.rb
@@ -3,7 +3,7 @@
 require_dependency 'claims_api/json_marshal'
 
 module ClaimsApi
-  class SupportingDocument < ActiveRecord::Base
+  class SupportingDocument < ApplicationRecord
     attr_encrypted(:file_data, key: Settings.db_encryption_key, marshal: true, marshaler: ClaimsApi::JsonMarshal)
 
     belongs_to :auto_established_claim
