0d8330e1ce3e547376e1d36a82b330f26f70392d
Michael Bastos <bastosmichael@gmail.com>
2019-03-18 21:02:44 -0500

    final rubocop fix

--- modules/veteran/db/migrate/20190313163050_create_veteran_service_objects.rb
+++ modules/veteran/db/migrate/20190313163050_create_veteran_service_objects.rb
@@ -8,7 +8,6 @@ class CreateVeteranServiceObjects < ActiveRecord::Migration
       t.string :phone
       t.string :state, limit: 2
       t.index :poa, unique: true
-
       t.timestamps null: false
     end
 
@@ -16,16 +15,12 @@ class CreateVeteranServiceObjects < ActiveRecord::Migration
       t.string :representative_id
       t.string :poa, limit: 3
       t.string :first_name
-      t.string :middle_initial
       t.string :last_name
       t.string :email
       t.string :phone
-      t.string :state, limit: 2
-      t.string :zip
       t.index :representative_id, unique: true
       t.index :first_name
       t.index :last_name
-
       t.timestamps null: false
     end
   end
