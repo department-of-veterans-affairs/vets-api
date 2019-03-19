1746f60709b8447591422f80bc78fa1c0a708cf1
Michael Bastos <bastosmichael@gmail.com>
2019-03-19 10:56:13 -0500

    adding comment

--- modules/veteran/app/models/veteran/service/organization.rb
+++ modules/veteran/app/models/veteran/service/organization.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Veteran
+  # Not technically a Service Object, this is a term used by the VA internally.
   module Service
     class Organization < ActiveRecord::Base
       self.primary_key = :poa
--- modules/veteran/app/models/veteran/service/representative.rb
+++ modules/veteran/app/models/veteran/service/representative.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Veteran
+  # Not technically a Service Object, this is a term used by the VA internally.
   module Service
     class Representative < ActiveRecord::Base
       self.primary_key = :representative_id
