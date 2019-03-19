bea3a3615c64df93387e61bf5cccf457837df04c
Alastair Dawson <alastair.j.dawson@gmail.com>
2019-03-19 10:24:29 -0700

    added rated disability response docs

--- lib/evss/disability_compensation_form/special_issue.rb
+++ lib/evss/disability_compensation_form/special_issue.rb
@@ -4,6 +4,10 @@ require 'common/models/base'
 
 module EVSS
   module DisabilityCompensationForm
+    # Model for parsed special issues
+    # @!attribute special_issues
+    #   @return [Array<EVSS::DisabilityCompensationForm::SpecialIssue>] List of complicating issues e.g. ['POW', 'PTSD_1']
+    #
     class SpecialIssue
       include Virtus.model
 
