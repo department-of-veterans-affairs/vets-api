83c53af840aeee0354f077bd4c96b70fb0f8913a
Charley Stran <charley.stran@gmail.com>
2019-03-19 10:40:19 -0500

    Moved over changes from first branch (#2891)
    former branch remove-edipi-for-auto-cest was messed up. this replaces that

--- modules/claims_api/spec/models/veteran_spec.rb
+++ modules/claims_api/spec/models/veteran_spec.rb
@@ -4,6 +4,15 @@ require 'rails_helper'
 
 RSpec.describe ClaimsApi::Veteran, type: :model do
   describe 'attributes needed for MVI lookup' do
+    let(:headers) do
+      {
+        'X-VA-SSN' => '123456789',
+        'X-VA-First-Name' => 'MARK',
+        'X-VA-Last-Name' => 'WEBB',
+        'X-VA-Birth-Date' => '1928-01-01'
+      }
+    end
+
     before do
       @veteran = ClaimsApi::Veteran.new
       @veteran.loa = { current: 3, highest: 3 }
@@ -20,6 +29,17 @@ RSpec.describe ClaimsApi::Veteran, type: :model do
 
     it 'should alias dslogon_edipi to edipi for MVI' do
       expect(@veteran.dslogon_edipi).to be(@veteran.edipi)
+    end
+
+    it 'should set edipi not passed in headers' do
+      headers['X-VA-EDIPI'] = '12345'
+      veteran = ClaimsApi::Veteran.from_headers(headers)
+      expect(veteran.edipi).to eq('12345')
+    end
+
+    it 'should not set edipi if not passed in headers' do
+      veteran = ClaimsApi::Veteran.from_headers(headers)
+      expect(veteran.edipi).to be(nil)
     end
   end
 end
