478b2d0caad334081a1600d77f203e9566b0e0ea
Michael Bastos <bastosmichael@gmail.com>
2019-03-19 09:57:35 -0500

    renaming references and tests

--- modules/veteran/spec/models/veteran/user_spec.rb
+++ modules/veteran/spec/models/veteran/user_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe Veteran::Base do
+describe Veteran::User do
   context 'initialization' do
     let(:user) { FactoryBot.create(:user, :loa3) }
     let(:auth_headers) { EVSS::AuthHeaders.new(user).to_h }
@@ -14,14 +14,14 @@ describe Veteran::Base do
 
     it 'should initialize from a user' do
       allow(@client_stub).to receive(:get_current_info) { get_fixture('json/veteran_with_poa') }
-      veteran = Veteran::Base.new(user)
+      veteran = Veteran::User.new(user)
       expect(veteran.veteran_name).to eq('JEFF TERRELL WATSON')
       expect(veteran.power_of_attorney.code).to eq('A1Q')
     end
 
     it 'should not bomb out if poa is missing' do
       allow(@client_stub).to receive(:get_current_info) { get_fixture('json/veteran_without_poa') }
-      veteran = Veteran::Base.new(user)
+      veteran = Veteran::User.new(user)
       expect(veteran.veteran_name).to eq('JEFF TERRELL WATSON')
       expect(veteran.power_of_attorney).to eq(nil)
     end
