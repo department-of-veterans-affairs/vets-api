679daf6c57307526cb234925f33a091923b384d7
Kam Karshenas <kam@adhocteam.us>
2019-03-22 00:33:13 -0400

    introduce session_activity model, specs, and, migrations

---
+++ spec/factories/session_activities.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :session_activity do
+    originating_request_id { SecureRandom.uuid }
+    originating_ip_address { '200.200.200.200' }
+    originating_user_agent 'Firefox'
+    name 'signup'
+    status 'incomplete'
+    user_uuid { SecureRandom.uuid }
+    sign_in_service_name 'idme'
+    sign_in_account_type nil
+    multifactor_enabled false
+    idme_verified false
+    duration 200
+    additional_data {}
+  end
+end
