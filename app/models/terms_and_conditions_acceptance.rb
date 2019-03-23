e2c44c8b46118440424c1da5ce56c1a669bc2490
Lihan Li <frankieteardrop@gmail.com>
2019-03-22 21:26:08 -0700

    make mhv account relationship optional

--- app/models/terms_and_conditions_acceptance.rb
+++ app/models/terms_and_conditions_acceptance.rb
@@ -2,7 +2,7 @@
 
 class TermsAndConditionsAcceptance < ApplicationRecord
   belongs_to :terms_and_conditions
-  belongs_to :mhv_account, foreign_key: :user_uuid, primary_key: :user_uuid, inverse_of: false
+  belongs_to :mhv_account, foreign_key: :user_uuid, primary_key: :user_uuid, inverse_of: false, optional: true
 
   scope :for_user, ->(user) { where(user_uuid: user.uuid) }
   scope :for_terms, ->(terms_name) { joins(:terms_and_conditions).where(terms_and_conditions: { name: terms_name }) }
--- spec/factories/terms_and_conditions_acceptances.rb
+++ spec/factories/terms_and_conditions_acceptances.rb
@@ -4,8 +4,5 @@ FactoryBot.define do
   factory :terms_and_conditions_acceptance do
     association :terms_and_conditions
     user_uuid { SecureRandom.uuid }
-    after(:build) do |terms_and_conditions_acceptance|
-      terms_and_conditions_acceptance.mhv_account = build(:mhv_account, user_uuid: terms_and_conditions_acceptance.user_uuid)
-    end
   end
 end
