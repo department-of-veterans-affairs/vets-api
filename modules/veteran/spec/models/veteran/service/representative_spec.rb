e240d521358f2ea3f084fde8deb2b382dcb5bd50
Michael Bastos <bastosmichael@gmail.com>
2019-03-25 10:03:34 -0500

    reload data test

--- modules/veteran/spec/models/veteran/service/representative_spec.rb
+++ modules/veteran/spec/models/veteran/service/representative_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe Veteran::Service::Representative do
+describe Veteran::Service::Representative, vcr: true do
   it 'is valid with valid attributes' do
     expect(Veteran::Service::Representative.new(poa: '000')).to be_valid
   end
@@ -10,5 +10,9 @@ describe Veteran::Service::Representative do
   it 'is not valid without a poa' do
     representative = Veteran::Service::Representative.new(poa: nil)
     expect(representative).to_not be_valid
+  end
+
+  it 'should reload data from pulldown' do
+    Veteran::Service::Representative.reload!
   end
 end
