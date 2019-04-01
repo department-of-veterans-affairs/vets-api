04420f1a12f6b3c88d5e7a28d0c09e3645f4b415
Matt Roth <roth_matthew@bah.com>
2019-04-01 10:13:50 -0400

    update ytd and spool submission email lists

--- app/mailers/spool_submissions_report_mailer.rb
+++ app/mailers/spool_submissions_report_mailer.rb
@@ -7,29 +7,12 @@ class SpoolSubmissionsReportMailer < ApplicationMailer
     dana.kuykendall@va.gov
     Jennifer.Waltz2@va.gov
     shay.norton@va.gov
-    DONALD.NOBLE2@va.gov
     Darla.VanNieukerk@va.gov
-    Walter_Jenny@bah.com
-    Hoffmaster_David@bah.com
-    akulkarni@meetveracity.com
-    Brandon.Scott2@va.gov
-    224C.VBAVACO@va.gov
-    peter.chou1@va.gov
-    Joseph.Welton@va.gov
-    222A.VBAVACO@va.gov
   ].freeze
   STAGING_RECIPIENTS = %w[
     lihan@adhocteam.us
-    akulkarni@meetveracity.com
-    Hoffmaster_David@bah.com
     Turner_Desiree@bah.com
     Delli-Gatti_Michael@bah.com
-    Walter_Jenny@bah.com
-    Brandon.Scott2@va.gov
-    224C.VBAVACO@va.gov
-    peter.chou1@va.gov
-    Joseph.Welton@va.gov
-    222A.VBAVACO@va.gov
   ].freeze
 
   def build(report_file)
