56690985c46b485ad43ef5eb8b6304421b43a576
Michael Bastos <bastosmichael@gmail.com>
2019-04-03 19:57:27 -0500

    working version

--- modules/claims_api/app/workers/claims_api/claim_uploader.rb
+++ modules/claims_api/app/workers/claims_api/claim_uploader.rb
@@ -7,27 +7,38 @@ module ClaimsApi
     include Sidekiq::Worker
 
     def perform(auto_claim_id)
+      auto_claim = ClaimsApi::AutoEstablishedClaim.find(auto_claim_id)
+      auth_headers = auto_claim.auth_headers
+      
+    #   document = EVSSClaimDocument.new document_hash
+    # client = EVSS::DocumentsService.new(auth_headers)
+    # uploader = EVSSClaimDocumentUploader.new(user_uuid, document.uploader_ids)
+    # uploader.retrieve_from_store!(document.file_name)
+    # file_body = uploader.read_for_upload
+    # client.upload(file_body, document)
+    # uploader.remove!
+
     #   auto_claim = ClaimsApi::AutoEstablishedClaim.find(auto_claim_id)
 
     #   form_data = auto_claim.form.to_internal
     #   auth_headers = auto_claim.auth_headers
 
-    #   begin
-    #     response = service(auth_headers).submit_form526(form_data)
+      begin
+        response = service(auth_headers).upload(file_body, document_data)
     #     auto_claim.evss_id = response.claim_id
     #     auto_claim.status = ClaimsApi::AutoEstablishedClaim::ESTABLISHED
     #     auto_claim.save
-    #   rescue Common::Exceptions::BackendServiceException => e
+      rescue Common::Exceptions::BackendServiceException => e
     #     auto_claim.status = ClaimsApi::AutoEstablishedClaim::ERRORED
     #     auto_claim.save
     #     raise e
-    #   end
+      end
     end
 
     private
 
     def service(auth_headers)
-      EVSS::DisabilityCompensationForm::ServiceAllClaim.new(
+      EVSS::DocumentsService.new(
         auth_headers
       )
     end
