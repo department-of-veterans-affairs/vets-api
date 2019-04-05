448b2b4059089161edb86d05448366cdad908bbc
Michael Bastos <bastosmichael@gmail.com>
2019-04-05 08:27:14 -0500

    Delete 20190403160125_add_doctype_and_description.rb

--- modules/claims_api/db/migrate/20190403160125_add_doctype_and_description.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddDoctypeAndDescription < ActiveRecord::Migration
-    def change
-      add_column :claims_api_supporting_documents, :document_type, :string
-      add_column :claims_api_supporting_documents, :description, :string
-    end
-  end
