4d515eadd8ea7c79adffe0bbbaad37918a770065
Michael Bastos <bastosmichael@gmail.com>
2019-04-08 16:40:35 -0500

    changing column

--- modules/claims_api/db/migrate/20190408160432_change_column_supporting_documents.rb
+++ modules/claims_api/db/migrate/20190408160432_change_column_supporting_documents.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 class ChangeColumnSupportingDocuments < ActiveRecord::Migration
-  def up
-    change_column :claims_api_supporting_documents, :auto_established_claim_id, :uuid
-  end
-
-  def up
-    change_column :claims_api_supporting_documents, :auto_established_claim_id, :integer
+  def change
+    remove_column :claims_api_supporting_documents, :auto_established_claim_id, :integer, null: false
+    add_column :claims_api_supporting_documents, :auto_established_claim_id, :uuid, null: false
   end
 end
   
