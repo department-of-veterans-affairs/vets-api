eb38d91d3155186c8d0aaacc8d8b0c19d05e7687
Michael Bastos <bastosmichael@gmail.com>
2019-04-08 08:43:43 -0500

    rubocop fix

--- modules/claims_api/spec/workers/claim_uploader_spec.rb
+++ modules/claims_api/spec/workers/claim_uploader_spec.rb
@@ -26,12 +26,12 @@ RSpec.describe ClaimsApi::ClaimUploader, type: :job do
   let(:supporting_document) do
     supporting_document = create(:supporting_document)
     supporting_document.set_file_data!(
-        Rack::Test::UploadedFile.new(
-          "#{::Rails.root}/modules/claims_api/spec/fixtures/extras.pdf"
-        ),
-        'docType',
-        'description'
-      )
+      Rack::Test::UploadedFile.new(
+        "#{::Rails.root}/modules/claims_api/spec/fixtures/extras.pdf"
+      ),
+      'docType',
+      'description'
+    )
     supporting_document.save!
     supporting_document
   end
@@ -42,25 +42,25 @@ RSpec.describe ClaimsApi::ClaimUploader, type: :job do
     end.to change(subject.jobs, :size).by(1)
   end
 
-#   it 'sets a status of established on successful call' do
-#     evss_service_stub = instance_double('EVSS::DisabilityCompensationForm::ServiceAllClaim')
-#     allow(EVSS::DisabilityCompensationForm::ServiceAllClaim).to receive(:new) { evss_service_stub }
-#     allow(evss_service_stub).to receive(:submit_form526) { OpenStruct.new(claim_id: 1337) }
+  #   it 'sets a status of established on successful call' do
+  #     evss_service_stub = instance_double('EVSS::DisabilityCompensationForm::ServiceAllClaim')
+  #     allow(EVSS::DisabilityCompensationForm::ServiceAllClaim).to receive(:new) { evss_service_stub }
+  #     allow(evss_service_stub).to receive(:submit_form526) { OpenStruct.new(claim_id: 1337) }
 
-#     subject.new.perform(claim.id)
-#     claim.reload
-#     expect(claim.evss_id).to eq(1337)
-#     expect(claim.status).to eq(ClaimsApi::AutoEstablishedClaim::ESTABLISHED)
-#   end
+  #     subject.new.perform(claim.id)
+  #     claim.reload
+  #     expect(claim.evss_id).to eq(1337)
+  #     expect(claim.status).to eq(ClaimsApi::AutoEstablishedClaim::ESTABLISHED)
+  #   end
 
-#   it 'sets the status of the claim to an error if it raises an error on EVSS' do
-#     allow_any_instance_of(EVSS::DisabilityCompensationForm::ServiceAllClaim).to(
-#       receive(:submit_form526).and_raise(Common::Exceptions::BackendServiceException)
-#     )
-#     expect { subject.new.perform(claim.id) }.to raise_error(Common::Exceptions::BackendServiceException)
+  #   it 'sets the status of the claim to an error if it raises an error on EVSS' do
+  #     allow_any_instance_of(EVSS::DisabilityCompensationForm::ServiceAllClaim).to(
+  #       receive(:submit_form526).and_raise(Common::Exceptions::BackendServiceException)
+  #     )
+  #     expect { subject.new.perform(claim.id) }.to raise_error(Common::Exceptions::BackendServiceException)
 
-#     claim.reload
-#     expect(claim.evss_id).to eq(nil)
-#     expect(claim.status).to eq(ClaimsApi::AutoEstablishedClaim::ERRORED)
-#   end
+  #     claim.reload
+  #     expect(claim.evss_id).to eq(nil)
+  #     expect(claim.status).to eq(ClaimsApi::AutoEstablishedClaim::ERRORED)
+  #   end
 end
