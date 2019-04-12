6fcf180a6fa0f0e071852285244b4b5dee07a8f3
Silvio Luthi <silvio@adhocteam.us>
2019-04-12 10:06:28 -0700

    Update factory according to PR comment

--- spec/factories/ppiu_payment_account.rb
+++ spec/factories/ppiu_payment_account.rb
@@ -2,9 +2,10 @@
 
 FactoryBot.define do
   factory :ppiu_payment_account, class: 'EVSS::PPIU::PaymentAccount' do
+    sequence(:account_number, 10) { |n| "123456#{n}" }
+
     account_type 'Checking'
     financial_institution_name 'Bank of Ad Hoc'
-    account_number '12345678'
     financial_institution_routing_number '123456789'
   end
 end
