4d18ec0635128be1a51e0921453be6179590823a
Silvio Luthi <silvio@adhocteam.us>
2019-04-11 18:08:27 -0700

    Update swagger docs and add swagger tests

--- app/swagger/requests/ppiu.rb
+++ app/swagger/requests/ppiu.rb
@@ -22,6 +22,37 @@ module Swagger
             end
           end
         end
+
+        operation :post do
+          extend Swagger::Responses::AuthenticationError
+
+          key :description, 'Update a veterans payment information'
+          key :operationId, 'postPaymentInformation'
+          key :tags, %w[form_526]
+
+          parameter :authorization
+
+          parameter do
+            key :name, :body
+            key :in, :body
+            key :description, 'Payment information to be updated for the user'
+            key :required, true
+
+            schema do
+              property :account_type, type: :string, example: 'Checking'
+              property :financial_institution_name, type: :string, example: 'Bank Of America'
+              property :account_number, type: :string, example: '1234567890'
+              property :financial_institution_routing_number, type: :string, pattern: /^\d{9}$/, example: '123456789'
+            end
+          end
+
+          response 200 do
+            key :description, 'Response is OK'
+            schema do
+              key :'$ref', :PPIU
+            end
+          end
+        end
       end
     end
   end
