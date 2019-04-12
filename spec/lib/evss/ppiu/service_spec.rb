5da38b2e83596d175349595c4c80030a1c78722a
Silvio Luthi <silvio@adhocteam.us>
2019-04-11 17:20:20 -0700

    Update spec tests for new endpoint and service call

--- spec/lib/evss/ppiu/service_spec.rb
+++ spec/lib/evss/ppiu/service_spec.rb
@@ -49,4 +49,61 @@ describe EVSS::PPIU::Service do
       end
     end
   end
+
+  describe '#update_payment_information' do
+    let(:request_payload) do
+      {
+        'accountType' => 'Checking',
+        'financialInstitutionName' => 'Fake Bank Name',
+        'accountNumber' => '1234',
+        'financialInstitutionRoutingNumber' => '021000021'
+      }
+    end
+
+    context 'with a valid a valid evss response' do
+      it 'returns a payment information response object' do
+        VCR.use_cassette('evss/ppiu/update_payment_information') do
+          response = subject.update_payment_information(request_payload)
+          expect(response).to be_ok
+          expect(response).to be_an EVSS::PPIU::PaymentInformationResponse
+          expect(response.responses.first.control_information)
+            .to be_an EVSS::PPIU::ControlInformation
+          expect(response.responses.first.payment_account)
+            .to be_an EVSS::PPIU::PaymentAccount
+          expect(response.responses.first.payment_address)
+            .to be_an EVSS::PPIU::PaymentAddress
+        end
+      end
+    end
+
+    context 'with an http timeout' do
+      before do
+        allow_any_instance_of(Faraday::Connection).to receive(:post).and_raise(Faraday::TimeoutError)
+      end
+
+      it 'logs an error and raise GatewayTimeout' do
+        expect(StatsD).to receive(:increment).once.with(
+          'api.evss.update_payment_information.fail', tags: ['error:Common::Exceptions::GatewayTimeout']
+        )
+        expect(StatsD).to receive(:increment).once.with('api.evss.update_payment_information.total')
+        expect { subject.update_payment_information(request_payload) }.to raise_error(
+          Common::Exceptions::GatewayTimeout
+        )
+      end
+    end
+
+    context 'with a client error' do
+      it 'logs the message to sentry' do
+        VCR.use_cassette('evss/ppiu/update_service_error') do
+          expect(StatsD).to receive(:increment).once.with(
+            'api.evss.update_payment_information.fail', tags: [
+              'error:Common::Client::Errors::ClientError', 'status:500'
+            ]
+          )
+          expect(StatsD).to receive(:increment).once.with('api.evss.update_payment_information.total')
+          expect { subject.update_payment_information(request_payload) }.to raise_error(EVSS::PPIU::ServiceException)
+        end
+      end
+    end
+  end
 end
--- spec/request/ppiu_request_spec.rb
+++ spec/request/ppiu_request_spec.rb
@@ -41,4 +41,57 @@ RSpec.describe 'PPIU', type: :request do
       end
     end
   end
+
+  describe 'POST /v0/ppiu/payment_information' do
+    let(:headers) { { 'CONTENT_TYPE' => 'application/json' } }
+    let(:ppiu_response) { File.read('spec/support/ppiu/update_ppiu_response.json') }
+    let(:ppiu_request) { File.read('spec/support/ppiu/update_ppiu_request.json') }
+
+    context 'with a valid evss response' do
+      it 'should match the ppiu schema' do
+        VCR.use_cassette('evss/ppiu/update_payment_information') do
+          post '/v0/ppiu/payment_information', params: ppiu_request, headers: headers
+          expect(response).to have_http_status(:ok)
+          expect(response).to match_response_schema('payment_information')
+          expect(JSON.parse(response.body)).to eq(JSON.parse(ppiu_response))
+        end
+      end
+    end
+
+    context 'with an invalid request payload' do
+      let(:ppiu_request) do
+        {
+          'account_type' => 'Checking',
+          'financial_institution_name' => 'Bank of Ad Hoc',
+          'account_number' => '12345678'
+        }.to_json
+      end
+
+      it 'should return a validation error' do
+        post '/v0/ppiu/payment_information', params: ppiu_request, headers: headers
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response).to match_response_schema('errors')
+      end
+    end
+
+    context 'with a 403 response' do
+      it 'should return a not authorized response' do
+        VCR.use_cassette('evss/ppiu/update_forbidden') do
+          post '/v0/ppiu/payment_information', params: ppiu_request, headers: headers
+          expect(response).to have_http_status(:forbidden)
+          expect(response).to match_response_schema('evss_errors', strict: false)
+        end
+      end
+    end
+
+    context 'with a 500 server error type' do
+      it 'should return a service error response' do
+        VCR.use_cassette('evss/ppiu/update_service_error') do
+          post '/v0/ppiu/payment_information', params: ppiu_request, headers: headers
+          expect(response).to have_http_status(:service_unavailable)
+          expect(response).to match_response_schema('evss_errors')
+        end
+      end
+    end
+  end
 end
