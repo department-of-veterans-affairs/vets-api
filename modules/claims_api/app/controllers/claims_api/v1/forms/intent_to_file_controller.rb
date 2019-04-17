5e60f01be4c62c7f70e7cd174296caa23b0ff5cd
ATeal <arteal89@gmail.com>
2019-04-17 14:52:13 -0400

    v1 itf

---
+++ modules/claims_api/app/controllers/claims_api/v1/forms/intent_to_file_controller.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'claims_api/intent_to_file_serializer'
+
+module ClaimsApi
+  module V1
+    module Forms
+      class IntentToFileController < ApplicationController
+        before_action :validate_json_api_payload
+
+        def submit_form_0966
+          response = service.create_intent_to_file(form_type)
+          render json: response['intent_to_file'],
+                 serializer: ClaimsApi::IntentToFileSerializer
+        end
+
+        private
+
+        def service
+          EVSS::IntentToFile::Service.new(target_veteran)
+        end
+
+        def attributes
+          if request.body.string.present?
+            JSON.parse(request.body.string).dig('data', 'attributes')
+          else
+            {}
+          end
+        end
+
+        def form_type
+          if !attributes.empty?
+            attributes['type']
+          else
+            'compensation'
+          end
+        end
+
+        def validate_json_api_payload
+          unless attributes.empty?
+            # validate
+          end
+        end
+
+        def target_veteran
+          @target_veteran ||= ClaimsApi::Veteran.from_headers(request.headers)
+        end
+      end
+    end
+  end
+end
