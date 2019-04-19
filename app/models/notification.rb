56996b4a711d7a946ad391c9723bb816e88d9f64
hpjaj <hmlevine@gmail.com>
2019-04-19 09:22:42 -0600

    Notification model enum settings, associations & validations

---
+++ app/models/notification.rb
@@ -0,0 +1,13 @@
+class Notification < ApplicationRecord
+  include Notifications::EnumSubjectValues
+  include Notifications::EnumStatusValues
+
+  belongs_to :account
+
+  validates :subject, :account_id, presence: true
+
+  # @see https://api.rubyonrails.org/v5.2/classes/ActiveRecord/Enum.html
+  #
+  enum subject: subjects_mapped_to_database_integers
+  enum status: statuses_mapped_to_database_integers
+end
