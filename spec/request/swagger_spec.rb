6a07ccbccefaad99cda84c94203f826383ede3a0
hpjaj <hmlevine@gmail.com>
2019-04-25 13:23:18 -0600

    Swagger docs and specs

--- spec/request/swagger_spec.rb
+++ spec/request/swagger_spec.rb
@@ -2058,6 +2058,59 @@ RSpec.describe 'the API documentation', type: %i[apivore request], order: :defin
           )
         end
       end
+
+      describe 'PATCH /v0/notifications/dismissed_statuses/{subject}' do
+        let(:patch_body) do
+          {
+            status: Notification::CLOSED,
+            status_effective_at: '2019-04-23T00:00:00.000-06:00'
+          }
+        end
+
+        context 'user has an existing Notification record with the passed subject' do
+          let!(:notification) do
+            create :notification, :dismissed_status, account_id: mhv_user.account.id, read_at: Time.current
+          end
+
+          it 'supports updating dismissed status data' do
+            expect(subject).to validate(
+              :patch,
+              '/v0/notifications/dismissed_statuses/{subject}',
+              200,
+              headers.merge('_data' => patch_body, 'subject' => notification_subject)
+            )
+          end
+
+          it 'supports authorization validation' do
+            expect(subject).to validate(
+              :patch,
+              '/v0/notifications/dismissed_statuses/{subject}',
+              401,
+              '_data' => patch_body, 'subject' => notification_subject
+            )
+          end
+
+          it 'supports validating updated dismissed status data' do
+            expect(subject).to validate(
+              :patch,
+              '/v0/notifications/dismissed_statuses/{subject}',
+              422,
+              headers.merge('_data' => patch_body.merge(status: 'random_status'), 'subject' => notification_subject)
+            )
+          end
+        end
+
+        context 'user does not have a Notification record with the passed subject' do
+          it 'supports validating the presence of an existing record to be updated' do
+            expect(subject).to validate(
+              :patch,
+              '/v0/notifications/dismissed_statuses/{subject}',
+              404,
+              headers.merge('_data' => patch_body, 'subject' => notification_subject)
+            )
+          end
+        end
+      end
     end
   end
 
