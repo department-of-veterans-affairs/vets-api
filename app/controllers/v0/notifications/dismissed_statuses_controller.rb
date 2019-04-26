92932fd856eeb5d4240df4f18648fe1e4042025e
Harry Levine <hmlevine@gmail.com>
2019-04-26 11:20:09 -0600

    Endpoint to persist the dismissal of a user's health care notification status (#3000)
    * Endpoint to create a new dismissed status notification
    
    * Enforces that a user cannot have a duplicate notification record
    
    For a given subject, a user can only have one Notification record.
    
    * Repurposable validation for enum subject and status values
    
    Rails enum values, when invalid, will throw an ArgumentError on .new/.save/.create/.update; and throw a ActiveRecord::StatementInvalid error on .find/.find_by/etc.
    
    This happens before any callbacks or custom validations can take effect.
    
    So our choices for validation were:
    
    1. Use a controller concern
    2. Use tiered rescues/raises in the controller
    3. Monkey patch (https://github.com/rails/rails/issues/13971#issuecomment-383905807)
    4. Custom model methods with rescues/raises
    
    After chatting with a couple of devs about these choices, we decided that the best course of action was the controller concerns.
    
    Our rationale was:
    
    - This can be repurposed in the upcoming notifications_controller.rb class
    - It is transparent
    - maintainable
    - and supports all uses cases.
    
    * Swagger docs and specs

--- app/controllers/v0/notifications/dismissed_statuses_controller.rb
+++ app/controllers/v0/notifications/dismissed_statuses_controller.rb
@@ -4,8 +4,21 @@ module V0
   module Notifications
     class DismissedStatusesController < ApplicationController
       include Accountable
+      include ::Notifications::Validateable
 
+      before_action -> { validate_subject!(subject) }
+      before_action -> { validate_status!(status) }, only: :create
       before_action :set_account, :set_notification
+
+      def create
+        notification = @account.notifications.build(dismissed_statuses_params.merge(read_at: Time.current))
+
+        if notification.save
+          render json: notification, serializer: DismissedStatusSerializer
+        else
+          raise Common::Exceptions::ValidationErrors.new(notification), 'Validation errors present'
+        end
+      end
 
       def show
         if @notification
@@ -26,11 +39,15 @@ module V0
       end
 
       def dismissed_statuses_params
-        params.permit(:subject, :dismissed_status, :status_effective_at)
+        params.permit(:subject, :status, :status_effective_at)
       end
 
       def subject
         dismissed_statuses_params[:subject]
+      end
+
+      def status
+        dismissed_statuses_params[:status]
       end
     end
   end
