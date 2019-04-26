6d2f51ad76f1e1f7edc59989dfa1b61a1415b224
Evangeline Garreau <ergarreau@gmail.com>
2019-04-26 17:10:25 -0500

    Itf docs (#2997)
    * add ITF documentation
    
    * add comment to service exceptions
    
    * cleanup

--- lib/evss/intent_to_file/configuration.rb
+++ lib/evss/intent_to_file/configuration.rb
@@ -2,15 +2,28 @@
 
 module EVSS
   module IntentToFile
+    ##
+    # HTTP client configuration for the {IntentToFile::Service},
+    # sets the base path and a service name for breakers and metrics.
+    #
     class Configuration < EVSS::Configuration
+      ##
+      # @return [String] Base path for intent to file URLs.
+      #
       def base_path
         "#{Settings.evss.url}/wss-intenttofile-services-web/rest/intenttofile/v1"
       end
 
+      ##
+      # @return [String] Service name to use in breakers and metrics.
+      #
       def service_name
         'EVSS/IntentToFile'
       end
 
+      ##
+      # @return [Boolean] Should the service use mock data in lower environments.
+      #
       def mock_enabled?
         Settings.evss.mock_itf || false
       end
