04ffcfebe7a3b859b4aedbd8c1a3d6701e3d1342
Charley Stran <charley.stran@gmail.com>
2019-04-26 09:56:10 -0500

    add 404 status on not found

--- modules/veteran/app/controllers/veteran/v0/representatives_controller.rb
+++ modules/veteran/app/controllers/veteran/v0/representatives_controller.rb
@@ -6,13 +6,15 @@ module Veteran
       skip_before_action :set_tags_and_extra_content, raise: false
       skip_before_action :authenticate
 
+      # Currently only used by the SAML proxy and not documented for external use
       def find_rep
         rep = Veteran::Service::Representative.for_user(first_name: params[:first_name], last_name: params[:last_name])
         if rep.present?
           render json: rep,
                  serializer: Veteran::Service::RepresentativeSerializer
         else
-          render json: { errors: [{ detail: 'Representative not found' }] }
+          render json: { errors: [{ detail: 'Representative not found' }] },
+                 status: :not_found
         end
       end
     end
