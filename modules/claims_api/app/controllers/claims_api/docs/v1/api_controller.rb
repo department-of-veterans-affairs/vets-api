63b092a6be4c2e632700be95d38273d511b0ef4b
Michael Bastos <bastosmichael@gmail.com>
2019-04-30 09:28:36 -0400

    Creating Swagger Blocks Documentation for Claims V1 (#2952)
    * Creating Swagger Blocks Documentation for Claims V1
    
    * adding route
    
    * adding to claims serializer
    
    * rubocop formatting
    
    * fixed blank lines
    
    * removing swagger from existing models
    
    * updating placement of swagger
    
    * docs update
    
    * getting past rubocop
    
    * fixed file.root to rails.root
    
    * added service periods
    
    * fixing formatting
    
    * finally green on rubocop
    
    * updating model
    
    * finished service history
    
    * rubocop fix
    
    * removing rubcop limit from config
    
    * Adding rest of swagger files
    
    * fixing disability
    
    * rubocop again
    
    * fixed service status
    
    * finishing 526 model
    
    * updating controller swaggers
    
    * fixing claims controller
    
    * fixed servers
    
    * adding bearer token component
    
    * updating 526
    
    * reduced description size

--- modules/claims_api/app/controllers/claims_api/docs/v1/api_controller.rb
+++ modules/claims_api/app/controllers/claims_api/docs/v1/api_controller.rb
@@ -2,14 +2,15 @@
 
 require_dependency 'claims_api/form_526_model_swagger'
 require_dependency 'claims_api/form_0966_model_swagger'
-require_dependency 'claims_api/form_526_v0_controller_swagger'
-require_dependency 'claims_api/form_0966_v0_controller_swagger'
+require_dependency 'claims_api/form_526_v1_controller_swagger'
+require_dependency 'claims_api/form_0966_v1_controller_swagger'
 
 module ClaimsApi
   module Docs
-    module V0
+    module V1
       class ApiController < ::ApplicationController
         skip_before_action(:authenticate)
+        # skip_before_action(:log_request)
         include Swagger::Blocks
 
         # A list of all classes that have swagger_* declarations.
@@ -18,10 +19,10 @@ module ClaimsApi
           ClaimsApi::Form526ModelSwagger,
           ClaimsApi::Form0966ModelSwagger,
           ClaimsApi::ErrorModelSwagger,
-          ClaimsApi::ClaimsV0ControllerSwagger,
-          ClaimsApi::Form526V0ControllerSwagger,
-          ClaimsApi::Form0966V0ControllerSwagger,
-          ClaimsApi::ClaimsV0Swagger
+          ClaimsApi::ClaimsV1ControllerSwagger,
+          ClaimsApi::Form526V1ControllerSwagger,
+          ClaimsApi::Form0966V1ControllerSwagger,
+          ClaimsApi::ClaimsV1Swagger
         ].freeze
 
         def index
