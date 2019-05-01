8525e74f2dd48b17c57dc430e807e1e8d4bfc06b
Alex R Teal <arteal89@gmail.com>
2019-05-01 15:38:56 -0400

    Art.claims 404 (#3015)
    * Remove the rescue_evss_errors.
    These are now being handled via lib/sentry/processor/log_as_warning.rb:9
    
    * Rescue from evss errors
    
    * Rescue evss + mvi errors and render appropriate responses
    
    * Port error handling to v1
    
    * move mvi validation to the parent controller
    make before filter
    
    * unused error
    
    * forgot to remove these in the refactor
    
    * This was a bug
    
    * Stub mvi in allthethings.jpeg
    
    * Fix evss tests from refactor
    
    * Add tests for evss failures
    
    * Remove more references to ignored error
    
    * Add helpful logging in case of errors
    
    * no status here

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -9,8 +9,6 @@ Raven.configure do |config|
   config.processors << Sentry::Processor::PIISanitizer
   config.processors << Sentry::Processor::LogAsWarning
 
-  config.excluded_exceptions += ['Sentry::IgnoredError']
-
   config.async = lambda { |event|
     SentryJob.perform_async(event)
   }
--- lib/sentry/ignored_error.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Sentry
-  class IgnoredError < StandardError; end
-end
