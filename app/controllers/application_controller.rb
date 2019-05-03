09ad693cc11b28d043e774fc3e9b0e9df79eb643
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2019-05-03 03:03:16 -0400

    Removed unused method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -159,9 +159,4 @@ class ApplicationController < ActionController::API
   def render_job_id(jid)
     render json: { job_id: jid }, status: 202
   end
-
-  def append_info_to_payload(payload)
-    super
-    payload[:session] = Session.obscure_token(session[:token]) if session && session[:token]
-  end
 end
