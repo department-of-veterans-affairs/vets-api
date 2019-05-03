aa8b0a5184232fbcec66071410e3ed07d4855e8c
Evangeline Garreau <ergarreau@gmail.com>
2019-05-03 14:45:05 -0500

    add pciu yard docs (#3021)

--- lib/evss/pciu/phone_number_response.rb
+++ lib/evss/pciu/phone_number_response.rb
@@ -4,6 +4,21 @@ require 'evss/response'
 
 module EVSS
   module PCIU
+    ##
+    # Model for PCIU phone number response
+    #
+    # @param status [Integer] The HTTP status code
+    # @param response [Hash] The API response
+    #
+    # @!attribute country_code
+    #   @return [String] The country code at the beginning of the phone number
+    # @!attribute number
+    #   @return [String] The main phone number
+    # @!attribute extension
+    #   @return [String] The extension at the end of the phone number
+    # @!attribute effective_date
+    #   @return [String] Date at which the number was known to be valid
+    #
     class PhoneNumberResponse < EVSS::Response
       attribute :country_code, String
       attribute :number, String
