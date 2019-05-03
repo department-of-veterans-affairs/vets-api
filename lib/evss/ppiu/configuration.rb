63e4db58dfe4b77b49922ce46192b0f29578d564
egarreau <ergarreau@gmail.com>
2019-05-03 15:32:42 -0500

    add yard docs to PPIU

--- lib/evss/ppiu/configuration.rb
+++ lib/evss/ppiu/configuration.rb
@@ -2,15 +2,28 @@
 
 module EVSS
   module PPIU
+    ##
+    # HTTP client configuration for the {EVSS::PPIU::Service},
+    # sets the base path and a service name for breakers and metrics.
+    #
     class Configuration < EVSS::Configuration
+      ##
+      # @return [String] Base path for PPIU URLs.
+      #
       def base_path
         "#{Settings.evss.url}/wss-ppiu-services-web/rest/ppiuServices/v1"
       end
 
+      ##
+      # @return [String] Service name to use in breakers and metrics.
+      #
       def service_name
         'EVSS/PPIU'
       end
 
+      ##
+      # @return [Boolean] Should the service use mock data in lower environments.
+      #
       def mock_enabled?
         Settings.evss.mock_ppiu || false
       end
