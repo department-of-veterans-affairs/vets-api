41c537054fbee3344ff842dd79a60e6289f10c1c
Dan Shawkey <shawkey_daniel@bah.com>
2019-05-06 14:17:53 -0400

    create va1995-STEM.rb

---
+++ app/workers/education_form/forms/va1995-STEM.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module EducationForm::Forms
+  class VA1995-STEM < Base
+    FORM_TYPES = {
+      'chapter30': 'CH30',
+      'chapter32': 'CH32',
+      'chapter33': 'CH33',
+      'chapter1606': 'CH1606',
+      'chapter1607': 'CH1607',
+      'transferOfEntitlement': 'TransferOfEntitlement'
+    }.freeze
+
+    def school
+      @applicant.newSchool
+    end
+
+    def form_type(applicant)
+      FORM_TYPES[applicant.benefit&.to_sym]
+    end
+  end
+end
