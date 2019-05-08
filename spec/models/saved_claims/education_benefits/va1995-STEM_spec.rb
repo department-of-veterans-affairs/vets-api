ce26edb7c78f91b41ed4e14cbdda1b867da46145
Dan Shawkey <shawkey_daniel@bah.com>
2019-05-08 09:54:49 -0400

    modify for FactoryBot to build va1995_STEM

--- spec/models/saved_claims/education_benefits/va1995-STEM_spec.rb
+++ spec/models/saved_claims/education_benefits/va1995-STEM_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 require 'lib/saved_claims_spec_helper'
 
 RSpec.describe SavedClaim::EducationBenefits::VA1995_STEM do
-  let(:instance) { FactoryBot.build(:va1995) }
+  let(:instance) { FactoryBot.build(:va1995_STEM) }
   it_should_behave_like 'saved_claim'
 
   validate_inclusion(:form_id, '22-1995-STEM')
