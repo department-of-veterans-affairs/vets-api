14669d8a18ce7a3a878d53c25a19f8dadb2d140d
Silvio Luthi <silvio@adhocteam.us>
2019-05-10 10:16:44 -0700

    Move retry logic to separate method and yard doc it

--- app/workers/evss/disability_compensation_form/submit_form526.rb
+++ app/workers/evss/disability_compensation_form/submit_form526.rb
@@ -33,11 +33,7 @@ module EVSS
         retryable_error_handler(e)
       rescue EVSS::DisabilityCompensationForm::ServiceException => e
         # retry any errors caused by an upstream service from EVSS being unavailable
-        if e.key == 'evss.external_service_unavailable'
-          retryable_error_handler(e)
-        else
-          non_retryable_error_handler(e)
-        end
+        retry_for_unavailable_external_service!(e)
       rescue StandardError => e
         non_retryable_error_handler(e)
       end
@@ -56,6 +52,19 @@ module EVSS
 
       def service(_auth_headers)
         raise NotImplementedError, 'Subclass of SubmitForm526 must implement #service'
+      end
+
+      # Retries any errors caused by an upstream service from EVSS being unavailable
+      # Otherwise it marks it as non-retryable and stops the job
+      #
+      # @param error [ErrorClass] An exception object of type {EVSS::DisabilityCompensationForm::ServiceException}
+      #
+      def retry_for_unavailable_external_service!(error)
+        if error.key == 'evss.external_service_unavailable'
+          retryable_error_handler(error)
+        else
+          non_retryable_error_handler(error)
+        end
       end
     end
   end
