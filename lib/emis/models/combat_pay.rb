a430e07cf5581c0af44676168798921ddce7407c
Lihan Li <frankieteardrop@gmail.com>
2019-05-09 19:50:39 -0700

    emis combat pay type code

--- lib/emis/models/combat_pay.rb
+++ lib/emis/models/combat_pay.rb
@@ -2,8 +2,23 @@
 
 module EMIS
   module Models
+    # rubocop:disable Layout/TrailingWhitespace
+
     # EMIS Combat Pay data for a veteran
+    # @!attribute segment_identifier
+    #   @return [String] identifier that is used to ensure a unique key on each Military Pay record
+    # @!attribute begin_date
+    #   @return [Date] date the specified pay began. Day is not available from the pay files and is defaulted to "01"
+    # @!attribute end_date
+    #   @return [Date] date the specified pay terminated.
+    #     Day is not available from the pay files and is defaulted to the end of the month
+    # @!attribute type_code
+    #   @return [String] code that indicates the type of pay being reported.  
+    #     01 => Combat Zone Tax Exclusion (CZTE)  
+    #     02 => Hostile Fire/Imminent Danger  
+    #     03 => Hazardous Duty incentive
     class CombatPay
+      # rubocop:enable Layout/TrailingWhitespace
       include Virtus.model
 
       attribute :segment_identifier, String
