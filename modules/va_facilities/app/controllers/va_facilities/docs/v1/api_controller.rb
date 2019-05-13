8a4319648941e10ba30165b8e767f1e4452252c3
Nick Fasulo <nrfasulo@gmail.com>
2019-05-13 14:00:17 -0500

    Fix new doc endpoint

--- modules/va_facilities/app/controllers/va_facilities/docs/v1/api_controller.rb
+++ modules/va_facilities/app/controllers/va_facilities/docs/v1/api_controller.rb
@@ -7,7 +7,7 @@ module VaFacilities
         skip_before_action(:authenticate)
 
         def index
-          swagger = YAML.safe_load(File.read(VaFacilities::Engine.root.join('README.yml')))
+          swagger = YAML.safe_load(File.read(VaFacilities::Engine.root.join('README.v1.yml')))
           render json: swagger
         end
       end
