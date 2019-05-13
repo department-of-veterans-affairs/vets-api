6d645a2f3f9959767768297095b492c7742367a3
Dan Shawkey <shawkey_daniel@bah.com>
2019-05-13 15:12:26 -0400

    modify form_id to match tests

--- app/models/saved_claim/education_benefits/va1995stem.rb
+++ app/models/saved_claim/education_benefits/va1995stem.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class SavedClaim::EducationBenefits::VA1995stem < SavedClaim::EducationBenefits
-  add_form_and_validation('22-1995stem')
+  add_form_and_validation('22-1995STEM')
 end
