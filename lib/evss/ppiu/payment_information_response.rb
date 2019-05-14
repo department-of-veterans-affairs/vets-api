3e684e5e32487b43b2a4e950ef96c59bf9d739a9
Evangeline Garreau <ergarreau@gmail.com>
2019-05-14 15:18:29 -0400

    add yard docs to PPIU (#3026)
    * add yard docs to PPIU
    
    * adjust indentation
    
    * add documentation to control information  object

--- lib/evss/ppiu/payment_information_response.rb
+++ lib/evss/ppiu/payment_information_response.rb
@@ -5,6 +5,15 @@ require 'evss/ppiu/payment_information'
 
 module EVSS
   module PPIU
+    ##
+    # Model for payment information responses.
+    #
+    # @param status [Integer] The HTTP status code
+    # @param response [Hash] The API response
+    #
+    # @!attribute responses
+    #   @return [Array[EVSS::PPIU::PaymentInformation]] An array of payment information objects
+    #
     class PaymentInformationResponse < EVSS::Response
       attribute :responses, Array[EVSS::PPIU::PaymentInformation]
 
