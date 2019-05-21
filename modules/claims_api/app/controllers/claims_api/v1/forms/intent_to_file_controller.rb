9fe3d1169dc3e161f7f9fc75cccffb3050ca33fe
Alex R Teal <arteal89@gmail.com>
2019-05-21 13:25:02 -0400

    Fix defaulting in form_attributes (#3054)
    Make active use strong_params
    fix tests

--- modules/claims_api/app/controllers/claims_api/v1/forms/intent_to_file_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v1/forms/intent_to_file_controller.rb
@@ -7,7 +7,8 @@ module ClaimsApi
     module Forms
       class IntentToFileController < BaseFormController
         before_action { permit_scopes %w[claim.write] }
-        before_action :check_future_type
+        before_action :check_future_type, only: [:submit_form_0966]
+        skip_before_action :validate_json_schema, only: [:active]
 
         FORM_NUMBER = '0966'
         def submit_form_0966
@@ -17,12 +18,16 @@ module ClaimsApi
         end
 
         def active
-          response = service.get_active(form_type)
+          response = service.get_active(active_param)
           render json: response['intent_to_file'],
                  serializer: ClaimsApi::IntentToFileSerializer
         end
 
         private
+
+        def active_param
+          params.require(:type)
+        end
 
         def check_future_type
           unless form_type == 'compensation'
@@ -43,16 +48,7 @@ module ClaimsApi
         end
 
         def form_type
-          if !form_attributes.empty?
-            form_attributes['type']
-          else
-            'compensation'
-          end
-        end
-
-        def validate_json_schema
-          # to support default compensation
-          super unless form_attributes.empty?
+          form_attributes['type']
         end
       end
     end
