c1dbc7619f1ac8f7286b518cdc49c622f318d320
Lihan Li <frankieteardrop@gmail.com>
2019-05-20 20:50:27 -0700

    working on GuardReserveServicePeriod docs

--- lib/emis/models/guard_reserve_service_period.rb
+++ lib/emis/models/guard_reserve_service_period.rb
@@ -3,6 +3,28 @@
 module EMIS
   module Models
     # EMIS Guard and Reserve service period data
+    # @!attribute segment_identifier
+    #   @return [String] identifier that is used to ensure a unique key on each Guard/Reserve
+    #     Active Service record.
+    # @!attribute begin_date
+    #   @return [Date] date on which a Service member began a period, or consecutive periods,
+    #     of active service that total, or will total, more than 30 consecutive days. The data
+    #     is received daily and monthly from personnel data feeds. The data is used to
+    #     determine eligibility for active duty benefits (medical, educational, etc.).
+    # @!attribute end_date
+    #   @return [Date] date on which a Service member on a period, or consecutive periods, of
+    #     active service that total, or will total, more than 30 consecutive days terminates,
+    #     or will terminate. The data is received daily and monthly from personnel data feeds.
+    #     The data is used to determine eligibility for active duty benefits (medical,
+    #     educational, etc.).
+    # @!attribute termination_reason
+    #   @return [String] code that represents the reason the service member went off active
+    #     duty.
+    #       C => Completion of Active Service period
+    #       D => Terminated by death
+    #       F => Invalid entry
+    #       S => Separation from personnel category or organization
+    #       W => Not applicable
     class GuardReserveServicePeriod
       include Virtus.model
 
