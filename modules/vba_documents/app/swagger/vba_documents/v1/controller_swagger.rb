35f5f8135b52dc7756bf28dcb38bc98b83da8d76
ATeal <arteal89@gmail.com>
2019-05-22 09:25:29 -0400

    Swagger docs for new endpoint

--- modules/vba_documents/app/swagger/vba_documents/v1/controller_swagger.rb
+++ modules/vba_documents/app/swagger/vba_documents/v1/controller_swagger.rb
@@ -165,6 +165,47 @@ module VbaDocuments
         end
       end
 
+      swagger_path '/uploads/{id}/download' do
+        operation :get do
+          key :summary, 'Download zip of "what the server sees"'
+          key :operationId, 'getBenefitsDocumentUploadDownload'
+
+          key :tags, ['document_uploads']
+
+          security do
+            key :api_key, []
+          end
+
+          parameter do
+            key :name, 'id'
+            key :in, :path
+            key :description, 'ID as returned by a previous create upload request'
+            key :required, true
+            key :example, '6d8433c1-cd55-4c24-affd-f592287a7572'
+            key :type, :string
+          end
+
+          response 200 do
+            key :description, 'Zip file with the contents of your payload as parsed by our server'
+            schema do
+              key :type, :file
+            end
+          end
+
+          response 401 do
+            key :description, 'Unauthorized request'
+          end
+
+          response 403 do
+            key :description, 'Bad API Token'
+          end
+
+          response 404 do
+            key :description, 'Not Found'
+          end
+        end
+      end
+
       swagger_path '/uploads/report' do
         operation :post do
           key :tags, %i[document_uploads]
