9d7b82cc699b75280746e108c141226f9a5bff9f
Alastair Dawson <alastair.j.dawson@gmail.com>
2019-05-23 11:07:59 -0700

    add common client and configuration yard docs

--- lib/common/client/configuration/base.rb
+++ lib/common/client/configuration/base.rb
@@ -3,13 +3,36 @@
 module Common
   module Client
     module Configuration
+      ##
+      # Base class for HTTP service configurations. This class should not be used directly.
+      # Use {Common::Client::Configuration::REST} or {Common::Client::Configuration::SOAP} instead.
+      #
       class Base
         include Singleton
 
+        # @!group Class Attributes
+        # @!attribute [rw]
+        # timeout for opening the connection
         class_attribute :open_timeout
+
+        # @!group Class Attributes
+        # @!attribute [rw]
+        # timeout for reading a resonse
         class_attribute :read_timeout
+
+        # @!group Class Attributes
+        # @!attribute [rw]
+        # allowed requests types e.g. `%i[get put post delete].freeze`
         class_attribute :request_types
+
+        # @!group Class Attributes
+        # @!attribute [rw]
+        # value for the User-Agent header
         class_attribute :user_agent
+
+        # @!group Class Attributes
+        # @!attribute [rw]
+        # headers to include in all requests
         class_attribute :base_request_headers
 
         self.open_timeout = 15
@@ -22,14 +45,25 @@ module Common
           'User-Agent' => user_agent
         }.freeze
 
+        ##
+        # The base path to use for all requests. Implemented by sub classes.
+        #
         def base_path
           raise NotImplementedError, "Subclass #{self.class.name} of Configuration must implement base_path"
         end
 
+        ##
+        # The service name that shows up in breakers metrics and logs. Implemented by sub classes.
+        #
         def service_name
           raise NotImplementedError, "Subclass #{self.class.name} of Configuration must implement service_name"
         end
 
+        ##
+        # Creates a custom service exception with the same namespace as the implementing class.
+        #
+        # @return Common::Exceptions::BackendServiceException execption with the class' namespace
+        #
         def service_exception
           if current_module.const_defined?('ServiceException')
             current_module.const_get('ServiceException')
@@ -38,6 +72,11 @@ module Common
           end
         end
 
+        ##
+        # Default request options, sets the read and open timeouts.
+        #
+        # @return Hash default request options.
+        #
         def request_options
           {
             open_timeout: open_timeout,
@@ -45,6 +84,11 @@ module Common
           }
         end
 
+        ##
+        # Default request options, sets the read and open timeouts.
+        #
+        # @return Hash default request options.
+        #
         def breakers_service
           return @service if defined?(@service)
           base_uri = URI.parse(base_path)
