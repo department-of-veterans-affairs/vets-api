ee6f8a49912652ce1606f809cf51a0b071d6c1b6
Charley Stran <charley.stran@gmail.com>
2019-05-23 09:40:20 -0500

    Adding logic to match claims detail and auto claim serializers

--- modules/claims_api/app/models/claims_api/auto_established_claim.rb
+++ modules/claims_api/app/models/claims_api/auto_established_claim.rb
@@ -18,6 +18,21 @@ module ClaimsApi
     before_validation :set_md5
     validates :md5, uniqueness: true
 
+    # these attributes are added to match the serializer with the claims detail serializer
+    attribute :date_filed
+    attribute :min_est_date
+    attribute :max_est_date
+    attribute :open
+    attribute :waiver_submitted
+    attribute :documents_needed
+    attribute :development_letter_sent
+    attribute :decision_letter_sent
+    attribute :requested_decision
+    attribute :claim_type, default: 'Compensation'
+    attribute :contention_list, default: []
+    attribute :va_representative
+    attribute :events_timeline, default: []
+
     alias token id
 
     def form
--- modules/claims_api/app/serializers/claims_api/auto_established_claim_serializer.rb
+++ modules/claims_api/app/serializers/claims_api/auto_established_claim_serializer.rb
@@ -5,5 +5,20 @@ module ClaimsApi
     attribute :token
     attribute :status
     attribute :evss_id
+
+    # these attributes are added to match the serializer with the claims detail serializer
+    attribute :date_filed
+    attribute :min_est_date
+    attribute :max_est_date
+    attribute :open
+    attribute :waiver_submitted
+    attribute :documents_needed
+    attribute :development_letter_sent
+    attribute :decision_letter_sent
+    attribute :requested_decision
+    attribute :claim_type, default: 'Compensation'
+    attribute :contention_list, default: []
+    attribute :va_representative
+    attribute :events_timeline, default: []
   end
 end
