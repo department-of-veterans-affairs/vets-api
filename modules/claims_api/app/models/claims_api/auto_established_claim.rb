330afe3199f0b20078e7a7428cd153120014db84
Charley Stran <charley.stran@gmail.com>
2019-05-24 11:26:29 -0500

    Share claims detail serializer for 526 form

--- modules/claims_api/app/models/claims_api/auto_established_claim.rb
+++ modules/claims_api/app/models/claims_api/auto_established_claim.rb
@@ -14,14 +14,21 @@ module ClaimsApi
     SUBMITTED = 'submitted'
     ESTABLISHED = 'established'
     ERRORED = 'errored'
+    EVSS_CLAIM_ATTRIBUTES = %i[date_filed min_est_date max_est_date open waiver_submitted
+                               documents_needed development_letter_sent decision_letter_sent
+                               requested_decision va_representative].freeze
 
     before_validation :set_md5
     validates :md5, uniqueness: true
 
-    # these attributes are added to match the serializer with the claims detail serializer
-    attributes :date_filed, :min_est_date, :max_est_date, :open, :waiver_submitted,
-               :documents_needed, :development_letter_sent, :decision_letter_sent,
-               :requested_decision, :va_representative
+    EVSS_CLAIM_ATTRIBUTES.each do |attribute|
+      define_method attribute do
+        nil
+      end
+    end
+
+    # EVSS Claims attributes with defaults
+    attribute :data, default: {}
     attribute :claim_type, default: 'Compensation'
     attribute :contention_list, default: []
     attribute :events_timeline, default: []
@@ -44,6 +51,10 @@ module ClaimsApi
     def set_md5
       headers = auth_headers.except('va_eauth_issueinstant', 'Authorization')
       self.md5 = Digest::MD5.hexdigest form_data.merge(headers).to_json
+    end
+
+    def status_from_phase(*)
+      status
     end
   end
 end
--- modules/claims_api/app/serializers/claims_api/auto_established_claim_serializer.rb
+++ modules/claims_api/app/serializers/claims_api/auto_established_claim_serializer.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
 module ClaimsApi
-  class AutoEstablishedClaimSerializer < ActiveModel::Serializer
+  class AutoEstablishedClaimSerializer < EVSSClaimDetailSerializer
     attributes :token, :status, :evss_id
 
-    # these attributes are added to match the serializer with the claims detail serializer
-    attributes :date_filed, :min_est_date, :max_est_date, :open, :waiver_submitted,
-               :documents_needed, :development_letter_sent, :decision_letter_sent,
-               :requested_decision, :va_representative
-    attribute :claim_type, default: 'Compensation'
-    attribute :contention_list, default: []
-    attribute :events_timeline, default: []
+    type :claims_api_auto_established_claims
   end
 end
