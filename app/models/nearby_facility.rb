a50b54be78c6f6c7a0335121bbacbcfd85fea038
Nick Fasulo <nrfasulo@gmail.com>
2019-06-03 14:16:12 -0500

    Add some explanatory comments

--- app/models/nearby_facility.rb
+++ app/models/nearby_facility.rb
@@ -18,6 +18,8 @@ class NearbyFacility < ApplicationRecord
     def request_isochrone(params)
       params[:drive_time] = '30' unless params[:drive_time]
       address = "#{params[:street_address]} #{params[:city]} #{params[:state]} #{params[:zip]}"
+      # list of all parameters can be found at https://docs.microsoft.com/en-us/bingmaps/rest-services/routes/calculate-an-isochrone#template-parameters
+      # we are currently using today at 7:30 AM (local time for the waypoint) for traffic modelling
       query = {
         waypoint: address,
         maxtime: params[:drive_time],
@@ -31,9 +33,12 @@ class NearbyFacility < ApplicationRecord
     end
 
     def get_facilities_in_isochrone(params, isochrone_response)
+      # an example response can be found at https://docs.microsoft.com/en-us/bingmaps/rest-services/examples/isochrone-example
       isochrone = JSON.parse(isochrone_response)['resourceSets'][0]['resources'][0]['polygons'][0]['coordinates'][0]
       linestring = make_linestring(isochrone)
+      # convert linestring into a polygon
       make_polygon = "ST_MakePolygon(ST_GeomFromText('LINESTRING(#{linestring})'))"
+      # find all facilities that lie inside of the polygon
       conditions = "ST_Intersects(#{make_polygon}, ST_MakePoint(long, lat))"
       facilities_query_base_instance = FacilitiesQuery::Base.new(params)
       BaseFacility::TYPES.flat_map do |facility_type|
@@ -42,6 +47,7 @@ class NearbyFacility < ApplicationRecord
     end
 
     def make_linestring(polygon)
+      # convert array of latitude and longitude points into a string of comma-separated longitude and latitude points
       polygon.map{|point| "#{point[1]} #{point[0]}"}.join(',')
     end
 
