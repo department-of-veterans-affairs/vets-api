befe6d57bc43e160a6305b98bba2b4834e021cb2
Lihan Li <frankieteardrop@gmail.com>
2019-06-04 21:25:37 -0700

    helper for form button

--- spec/feature/hca_spec.rb
+++ spec/feature/hca_spec.rb
@@ -1,6 +1,10 @@
 require 'features_helper'
 
 RSpec.describe('hca', type: :feature) do
+  def next_form_page
+    wait_for_new_url('.usa-button-primary')
+  end
+
   it 'anonymous application', js: true do
     visit("#{DEFAULT_HOST}/health-care/apply/application/introduction")
     wait_for_new_url('.schemaform-start-button')
@@ -14,21 +18,21 @@ RSpec.describe('hca', type: :feature) do
     wait_for_new_url('.usa-button')
     # veteran information
     sleep(1)
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # dob
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # gender
     find('#root_gender').find(:option, 'Male').select_option
     find('#root_maritalStatus').find(:option, 'Never Married').select_option
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # addr
     find('#root_veteranAddress_street').set('123 fake st')
     find('#root_veteranAddress_city').set('city')
     find('#root_veteranAddress_state').find(:option, 'Arizona').select_option
     find('#root_veteranAddress_postalCode').set('12345')
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # email
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # service
     find('#root_lastServiceBranch').find(:option, 'Army').select_option
 
@@ -38,31 +42,31 @@ RSpec.describe('hca', type: :feature) do
       find("#root_last#{type}DateYear").set(type == 'Entry' ? '1970' : '1971')
     end
     find('#root_dischargeType option[value="honorable"]').select_option
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # service special options
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # dd214
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # compensation
     click('#root_vaCompensationType_3', visible: false)
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # financial information
     click('#root_discloseFinancialInformationNo', visible: false)
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # insurance
     click('#root_isMedicaidEligibleNo', visible: false)
     click('#root_isEnrolledMedicarePartANo', visible: false)
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # other coverage
     click('#root_isCoveredByHealthInsuranceNo', visible: false)
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # va facility
     find('#root_view\:preferredFacility_view\:facilityState option[value="AL"]').select_option
     find('#root_view\:preferredFacility_vaMedicalFacility option[value="521GE"]').select_option
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     # review application
     click('#errorable-checkbox-8', visible: false)
-    wait_for_new_url('.usa-button-primary')
+    next_form_page
     expect(current_path).to eq('/health-care/apply/application/confirmation')
   end
 end
