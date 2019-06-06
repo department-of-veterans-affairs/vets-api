e313c594afdbab420bcb3d77ff6fab97fdea6446
Michael Bastos <bastosmichael@gmail.com>
2019-06-06 18:47:51 -0500

    adding metadata spec tests

---
+++ modules/claims_api/spec/requests/metadata_request_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Claims Status Metadata Endpoint', type: :request do
+  describe '#get /docs/metadata' do
+    it 'should return metadata JSON' do
+      get '/services/claims/docs/metadata'
+      expect(response).to have_http_status(:ok)
+      JSON.parse(response.body)
+    end
+  end
+end
