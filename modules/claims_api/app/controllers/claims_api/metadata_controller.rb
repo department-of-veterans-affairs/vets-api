6d36769fdcf007c0cd79348747e0ba0971fc90c6
Michael Bastos <bastosmichael@gmail.com>
2019-06-11 17:47:57 -0500

    Changing meta into object

--- modules/claims_api/app/controllers/claims_api/metadata_controller.rb
+++ modules/claims_api/app/controllers/claims_api/metadata_controller.rb
@@ -6,20 +6,24 @@ module ClaimsApi
 
     def index
       render json: {
-        meta: [
-          {
-            version: '1.0.0',
-            internal_only: false,
-            status: VERSION_STATUS[:draft],
-            path: '/services/claims/docs/v1/api'
-          },
-          {
-            version: '0.0.1',
-            internal_only: true,
-            status: VERSION_STATUS[:current],
-            path: '/services/claims/docs/v0/api'
-          }
-        ]
+        meta: {
+          versions: [
+            {
+              version: '1.0.0',
+              internal_only: false,
+              status: VERSION_STATUS[:draft],
+              path: '/services/claims/docs/v1/api',
+              healthcheck: '/services/claims/v1/healthcheck'
+            },
+            {
+              version: '0.0.1',
+              internal_only: true,
+              status: VERSION_STATUS[:current],
+              path: '/services/claims/docs/v0/api',
+              healthcheck: '/services/claims/v0/healthcheck'
+            }
+          ]
+        }
       }
     end
   end
