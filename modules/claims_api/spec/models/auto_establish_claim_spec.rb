41834857960e4cfc3b347f90885a3d959261ce02
Charley Stran <charley.stran@gmail.com>
2019-06-14 11:51:47 -0500

    another test update

--- modules/claims_api/spec/models/auto_establish_claim_spec.rb
+++ modules/claims_api/spec/models/auto_establish_claim_spec.rb
@@ -53,11 +53,11 @@ RSpec.describe ClaimsApi::AutoEstablishedClaim, type: :model do
     let(:evss_record) { create(:auto_established_claim, evss_id: 123_456) }
 
     it 'should find by model id' do
-      expect(described_class.get_by_id_or_evss_id(evss_record.id)).to be(evss_record.id)
+      expect(described_class.get_by_id_or_evss_id(evss_record).id).to eq(evss_record.id)
     end
 
     it 'should find by evss id' do
-      expect(described_class.get_by_id_or_evss_id(123_456).id).to be(evss_record.id)
+      expect(described_class.get_by_id_or_evss_id(123_456).id).to eq(evss_record.id)
     end
   end
 end
