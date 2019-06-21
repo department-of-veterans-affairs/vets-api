644a60c09ffaa1e43b76a7294936bd01fa4f7656
Kam Karshenas <kam@adhocteam.us>
2019-06-21 10:40:09 -0600

    a few more fixes

--- lib/sidekiq/error_tag.rb
+++ lib/sidekiq/error_tag.rb
@@ -3,12 +3,9 @@
 class Sidekiq::ErrorTag
   def call(_worker, job, _queue)
     Raven.tags_context(job: job['class'])
-
-    if job['request_attributes'].is_a?(Hash)
-      Raven.tags_context(request_id: job['request_id'])
-      Raven.user_context(remote_ip:  job.dig('request_attributes', 'remote_ip'))
-      Raven.user_context(user_agent: job.dig('request_attributes', 'user_agent'))
-    end
+    Raven.tags_context(request_id: job['request_id'] || 'N/A')
+    Raven.user_context(remote_ip:  job['remote_ip'] || 'N/A')
+    Raven.user_context(user_agent: job['user_agent'] || 'N/A')
 
     yield
   end
