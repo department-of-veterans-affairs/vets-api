153dd7c55fbb1297b1e857934af4808ba44594cf
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-27 17:06:12 -0400

    Test test

--- spec/controllers/v0/gi/institutions_controller_spec.rb
+++ spec/controllers/v0/gi/institutions_controller_spec.rb
@@ -41,18 +41,18 @@ RSpec.describe V0::GI::InstitutionsController, type: :controller do
     end
   end
 
-  # describe 'institutions' do
-  #   { 
-  #     'autocomplete' => :get_autocomplete_suggestions,
-  #     'search' => :get_search_results,
-  #     'show' => :get_institution_details,
-  #     'children' => :get_institution_children 
-  #   }.each do |method, client_method|
-  #     it "#{method} calls client method #{client_method}" do
-  #       allow(client).to receive(client_method)
-  #       get method, params: { id: '123', format: 'json' }
-  #       expect(client).to have_received(client_method).with('id' => '123')
-  #     end
-  #   end
-  # end
+  describe 'institutions' do
+    {
+      'autocomplete' => :get_autocomplete_suggestions,
+      'search' => :get_search_results,
+      'show' => :get_institution_details,
+      'children' => :get_institution_children
+    }.each do |controller_method, client_method|
+      it "#{controller_method} calls client method #{client_method}" do
+        allow(client).to receive(client_method)
+        get controller_method, params: { id: '123', format: 'json' }
+        expect(client).to have_received(client_method).with('id' => '123')
+      end
+    end
+  end
 end
