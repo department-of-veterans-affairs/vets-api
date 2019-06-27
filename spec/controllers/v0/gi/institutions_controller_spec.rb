7e3bba2869b0b631121cd18f67126d9492125f2e
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-27 13:03:11 -0400

    Test test

--- spec/controllers/v0/gi/institutions_controller_spec.rb
+++ spec/controllers/v0/gi/institutions_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe V0::GI::InstitutionsController, type: :controller do
     it 'calls client' do
       client_stub = instance_double('GI::Client')
 
-      expect(client).to eq(nil)
+      expect(client_stub).to eq(nil)
 
       # post(:create, params: { hca_attachment: {
       #        file_data: fixture_file_upload('pdf_fill/extras.pdf')
