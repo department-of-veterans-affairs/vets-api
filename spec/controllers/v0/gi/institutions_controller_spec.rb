326d1617dbefd757446a3c1dd46a86914b3f352c
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-27 15:06:54 -0400

    Test test

--- spec/controllers/v0/gi/institutions_controller_spec.rb
+++ spec/controllers/v0/gi/institutions_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe V0::GI::InstitutionsController, type: :controller do
-  let(:client) { instance_double('GI::Client') }
+  let(:@client) { instance_double('GI::Client') }
   describe '#children' do
     it 'calls client' do
       # client_response = client.get_zipcode_rate(id: '20001')
@@ -11,11 +11,12 @@ RSpec.describe V0::GI::InstitutionsController, type: :controller do
       # client = double('GI::Client')
       # allow(GI::Client).to receive(:get_institution_children)
 
+      # allow(@client).to receive(:get_institution_children) { body }
       get 'children', params: { id: 'ccp_12345' }
 
       # expect(response.content_type).to eq('application/json')
 
-      expect(client).to have_received(:get_institution_children)
+      expect(@client).to have_received(:get_institution_children)
 
       # post(:create, params: { hca_attachment: {
       #        file_data: fixture_file_upload('pdf_fill/extras.pdf')
