67eb772c5f3accdfa867bdb2f43858f333d8ca44
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-27 16:10:32 -0400

    Test test

--- spec/controllers/v0/gi/institutions_controller_spec.rb
+++ spec/controllers/v0/gi/institutions_controller_spec.rb
@@ -6,16 +6,9 @@ RSpec.describe V0::GI::InstitutionsController, type: :controller do
   let(:client) { instance_double('GI::Client') }
   describe '#children' do
     it 'calls client' do
-      # client_response = client.get_zipcode_rate(id: '20001')
-      # client_stub = spy('GI::Client')
-      # client = double('GI::Client')
-      # allow(GI::Client).to receive(:get_institution_children)
-
       allow(client).to receive(:get_institution_children)
-      described_class.instance_valiable_set(:@client, client)
-      get 'children', params: { id: 'ccp_12345' }
-
-      # expect(response.content_type).to eq(nil)
+      controller.instance_variable_set(:@client, client)
+      get 'children', params: { id: '123' }
 
       expect(client).to have_received(:get_institution_children)
     end
