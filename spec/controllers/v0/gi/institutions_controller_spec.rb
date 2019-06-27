b618daad328233843aeaf3b5fbc8f0cdf4441beb
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-27 16:26:15 -0400

    Test test

--- spec/controllers/v0/gi/institutions_controller_spec.rb
+++ spec/controllers/v0/gi/institutions_controller_spec.rb
@@ -3,13 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe V0::GI::InstitutionsController, type: :controller do
-  let(:client) { instance_double('GI::Client') }
+  let(:client) { double('GI::Client') }
   describe '#children' do
     it 'calls client' do
       allow(client).to receive(:get_institution_children)
       controller.instance_variable_set(:@client, client)
-      get 'children', params: { id: '123', format: 'json' }
-
+      get 'children', params: { id: '123' }
       expect(client).to have_received(:get_institution_children).with('id' => '123')
     end
   end
