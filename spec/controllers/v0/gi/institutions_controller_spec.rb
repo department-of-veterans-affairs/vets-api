78852f9395cbd6298042c92a740ba616cd1420b7
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-27 16:43:52 -0400

    Test test

--- spec/controllers/v0/gi/institutions_controller_spec.rb
+++ spec/controllers/v0/gi/institutions_controller_spec.rb
@@ -4,23 +4,11 @@ require 'rails_helper'
 
 RSpec.describe V0::GI::InstitutionsController, type: :controller do
   let(:client) { instance_double('GI::Client') }
-
-  before do
-    controller.instance_variable_set(:@client, client)
-  end
-
-  describe '#autocomplete' do
-    it 'calls client method' do
-      controller.instance_variable_set(:@client, client)
-      get 'autocomplete', params: { id: '123' }
-      expect(client).to have_received(:get_autocomplete_suggestions).with('id' => '123')
-    end
-  end
-
   describe '#children' do
-    it 'calls client method' do
+    it 'calls client' do
+      allow(client).to receive(:get_institution_children)
       controller.instance_variable_set(:@client, client)
-      get 'children', params: { id: '123' }
+      get 'children', params: { id: '123', format: 'json' }
       expect(client).to have_received(:get_institution_children).with('id' => '123')
     end
   end
