7fa87ca774f5381cb8e531e0bd3f50ca9fa232e3
Johnny Holton <johnny@holton.co>
2019-07-02 10:20:54 -0400

    lint fixes

--- spec/jobs/direct_deposit_email_job_spec.rb
+++ spec/jobs/direct_deposit_email_job_spec.rb
@@ -3,13 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe DirectDepositEmailJob, type: :model do
-
   describe '#perform' do
     it 'sends a confirmation email' do
       mail = double('mail')
-      allow(DirectDepositMailer).to receive(:build).with('test@example.com', 123456789).and_return(mail)
+      allow(DirectDepositMailer).to receive(:build).with('test@example.com', 123_456_789).and_return(mail)
       expect(mail).to receive(:deliver_now)
-      subject.perform('test@example.com', 123456789)
+      subject.perform('test@example.com', 123_456_789)
     end
   end
 end
