0d9b56296a9edb0522bc28b0579b3be3fdda0a13
Alex R Teal <arteal89@gmail.com>
2019-07-11 13:57:57 -0400

    Art.revert v1 errors (#3150)
    * remove json api errors
    
    * tests for errors
    
    * Update test to reflect new/old error messaging

--- modules/vba_documents/app/controllers/vba_documents/v1/uploads_controller.rb
+++ modules/vba_documents/app/controllers/vba_documents/v1/uploads_controller.rb
@@ -34,8 +34,6 @@ module VBADocuments
 
         if submission.nil? || submission.status == 'expired'
           raise Common::Exceptions::RecordNotFound, params[:id]
-        elsif submission.status == 'error'
-          render json: to_json_api_errors(submission)
         else
           submission.refresh_status!
           render json: submission,
@@ -60,17 +58,6 @@ module VBADocuments
 
       def verify_settings
         render plain: 'Not found', status: 404 unless Settings.vba_documents.enable_download_endpoint
-      end
-
-      def to_json_api_errors(submission)
-        {
-          "errors": [
-            {
-              "status": '422',
-              "details": "#{submission.code} - #{submission.detail}"
-            }
-          ]
-        }
       end
     end
   end
