57545efd4a23ee0d16e3a692a995ef49ba678f46
ATeal <arteal89@gmail.com>
2019-07-16 10:16:33 -0400

    Make the reports controller more simple as we are just returning cached results

--- modules/vba_documents/app/controllers/vba_documents/v1/reports_controller.rb
+++ modules/vba_documents/app/controllers/vba_documents/v1/reports_controller.rb
@@ -8,11 +8,10 @@ module VBADocuments
       skip_before_action(:authenticate)
       before_action :validate_params
 
-      MAX_REPORT_SIZE = 100
       ID_PARAM = 'ids'
 
       def create
-        statuses = VBADocuments::UploadSubmission.refresh_and_get_statuses!(params[ID_PARAM])
+        statuses = VBADocuments::UploadSubmission.where(guid: params[ID_PARAM])
         render json: statuses,
                each_serializer: VBADocuments::V1::UploadSerializer
       end
@@ -22,8 +21,6 @@ module VBADocuments
       def validate_params
         raise Common::Exceptions::ParameterMissing, ID_PARAM if params[ID_PARAM].nil?
         raise Common::Exceptions::InvalidFieldValue.new(ID_PARAM, params[ID_PARAM]) unless params[ID_PARAM].is_a?(Array)
-        raise Common::Exceptions::InvalidFieldValue.new(ID_PARAM, params[ID_PARAM]) if
-          params[ID_PARAM].size > MAX_REPORT_SIZE
       end
     end
   end
