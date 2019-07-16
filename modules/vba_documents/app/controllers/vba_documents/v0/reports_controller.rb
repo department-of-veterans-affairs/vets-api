5174e15294639864aca5d74b3963f2d1dfe4b5c9
ATeal <arteal89@gmail.com>
2019-07-16 11:20:24 -0400

    Readd limit, just increase by 10x

--- modules/vba_documents/app/controllers/vba_documents/v0/reports_controller.rb
+++ modules/vba_documents/app/controllers/vba_documents/v0/reports_controller.rb
@@ -8,6 +8,7 @@ module VBADocuments
       skip_before_action(:authenticate)
       before_action :validate_params
 
+      MAX_REPORT_SIZE = 1000
       ID_PARAM = 'ids'
 
       def create
@@ -21,6 +22,8 @@ module VBADocuments
       def validate_params
         raise Common::Exceptions::ParameterMissing, ID_PARAM if params[ID_PARAM].nil?
         raise Common::Exceptions::InvalidFieldValue.new(ID_PARAM, params[ID_PARAM]) unless params[ID_PARAM].is_a?(Array)
+        raise Common::Exceptions::InvalidFieldValue.new(ID_PARAM, params[ID_PARAM]) if
+          params[ID_PARAM].size > MAX_REPORT_SIZE
       end
     end
   end
