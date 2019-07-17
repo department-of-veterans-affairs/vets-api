e3257ac681d524dde752e672e33c1d747577bfa1
ATeal <arteal89@gmail.com>
2019-07-17 15:59:13 -0400

    Remove uniqueness code Use sidekiq enterprise for uniqueness

--- modules/vba_documents/app/workers/vba_documents/upload_status_updater.rb
+++ modules/vba_documents/app/workers/vba_documents/upload_status_updater.rb
@@ -6,20 +6,16 @@ module VBADocuments
   class UploadStatusUpdater
     include Sidekiq::Worker
 
-    sidekiq_options queue: 'vba_documents', retry: true
+    sidekiq_options(
+      queue: 'vba_documents',
+      retry: true,
+      unique_until: :success
+    )
 
     def perform
-      unless already_running?
-        VBADocuments::UploadSubmission.in_flight.order(created_at: :asc).find_in_batches(batch_size: 100).each do |batch|
-          VBADocuments::UploadSubmission.refresh_statuses!(batch)
-        end
+      VBADocuments::UploadSubmission.in_flight.order(created_at: :asc).find_in_batches(batch_size: 100).each do |batch|
+        VBADocuments::UploadSubmission.refresh_statuses!(batch)
       end
-    end
-
-    def already_running?
-      queue = Sidekiq::Queue.new('vba_documents')
-      job_classes = queue.map(&:klass)
-      job_classes.include? 'UploadStatusUpdater'
     end
   end
 end
