6346c7c0fccb9732371e58fe0ff6da575a01f32d
Alastair Dawson <alastair@adhocteam.us>
2019-07-24 09:45:16 -0700

    adds the database_cleaner gem and its configuration (#3170)

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class DirtyDatabaseError < RuntimeError
+  def initialize(meta)
+    super "#{meta[:full_description]}\n\t#{meta[:location]}"
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:deletion)
+  end
+
+  config.before(:all, :cleaner_for_context) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+
+  config.before(:each) do |example|
+    next if example.metadata[:cleaner_for_context]
+
+    DatabaseCleaner.strategy =
+      if example.metadata[:js]
+        :truncation
+      else
+        example.metadata[:strategy] || :transaction
+      end
+
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do |example|
+    next if example.metadata[:cleaner_for_context]
+
+    DatabaseCleaner.clean
+
+    # raise DirtyDatabaseError.new(example.metadata) if Record.count > 0
+  end
+
+  config.after(:all, :cleaner_for_context) do
+    DatabaseCleaner.clean
+  end
+end
