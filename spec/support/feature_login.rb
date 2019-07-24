ba1d41528b85d1a27218c72d6b1d56c9b420eabf
Lihan Li <frankieteardrop@gmail.com>
2019-07-23 22:34:10 -0700

    lint

--- spec/support/feature_login.rb
+++ spec/support/feature_login.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module FeatureLogin
   def feature_set_user_session(user = FactoryBot.create(:user, :loa3))
     extend AuthenticatedSessionHelper
@@ -5,7 +7,9 @@ module FeatureLogin
 
     visit(DEFAULT_HOST)
     Capybara.current_session.driver.browser.manage.add_cookie(name: Settings.sso.cookie_name, value: 'foo')
-    Capybara.current_session.driver.browser.manage.add_cookie(name: 'api_session', value: cookie.split(';')[0].split('=')[1])
+    Capybara.current_session.driver.browser.manage.add_cookie(
+      name: 'api_session', value: cookie.split(';')[0].split('=')[1]
+    )
     page.execute_script("localStorage.setItem('hasSession', true)")
 
     user
