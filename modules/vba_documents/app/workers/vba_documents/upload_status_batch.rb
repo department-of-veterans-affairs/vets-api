1fd616b79d504e53cd4a36267da990808c0f1b0a
ATeal <arteal89@gmail.com>
2019-07-25 14:29:21 -0400

    Filter out before vbms was implemented

--- modules/vba_documents/app/workers/vba_documents/upload_status_batch.rb
+++ modules/vba_documents/app/workers/vba_documents/upload_status_batch.rb
@@ -2,17 +2,36 @@
 
 module VBADocuments
   class UploadStatusBatch
+    include Sidekiq::Worker
+
+    sidekiq_options(
+      queue: 'vba_documents',
+      retry: true,
+      unique_until: :success
+    )
+
+    # We don't want to check successes before
+    # this date as it used to be the endpoint
+    VBMS_IMPLEMENTATION_DATE = Date.parse('28-06-2019')
     DIVISION_SIZE = 3
+
     def perform
       if Settings.vba_documents.updater_enabled
         batch = Sidekiq::Batch.new
-        submissions = VBADocuments::UploadSubmission.in_flight.order(created_at: :asc)
         batch.jobs do
+          submissions = filtered_submissions
           submissions.each_slice(submissions.count / DIVISION_SIZE) do |slice|
             VBADocuments::UploadStatusUpdater.perform_async(slice)
           end
         end
       end
+    end
+
+    def filtered_submissions
+      VBADocuments::UploadSubmission
+        .in_flight
+        .where.not("status = 'success' AND created_at < ?', VBMS_IMPLEMENTATION_DATE")
+        .order(created_at: :asc)
     end
   end
 end
