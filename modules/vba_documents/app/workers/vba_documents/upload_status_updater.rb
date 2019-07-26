06b2ce13d9a7342ba8d6b77ab4c9ce52bd413f57
ATeal <arteal89@gmail.com>
2019-07-26 09:33:06 -0400

    Pull batch size into a constant

--- modules/vba_documents/app/workers/vba_documents/upload_status_updater.rb
+++ modules/vba_documents/app/workers/vba_documents/upload_status_updater.rb
@@ -12,8 +12,10 @@ module VBADocuments
       unique_until: :success
     )
 
+    BATCH_SIZE = 100
+
     def perform(submission_guids)
-      VBADocuments::UploadSubmission.where(guid: submission_guids).find_in_batches(batch_size: 100) do |slice|
+      VBADocuments::UploadSubmission.where(guid: submission_guids).find_in_batches(batch_size: BATCH_SIZE) do |slice|
         VBADocuments::UploadSubmission.refresh_statuses!(slice)
       end
     end
