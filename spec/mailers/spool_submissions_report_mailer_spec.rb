6209b92dbe5adc3681ba64fa759ecbd44d96a5c9
hughes-dustin <48804654+hughes-dustin@users.noreply.github.com>
2019-08-14 12:49:45 -0400

    Bah 18815 Update Spool Report Distribution for 22-1995 Submission (#3199)
    * conditionally add people to spool submissions report email for 1995 STEM
    
    * fix lint offenses and update unit tests
    
    * update email list

--- spec/mailers/spool_submissions_report_mailer_spec.rb
+++ spec/mailers/spool_submissions_report_mailer_spec.rb
@@ -5,7 +5,8 @@ require 'rails_helper'
 RSpec.describe SpoolSubmissionsReportMailer, type: %i[mailer aws_helpers] do
   describe '#build' do
     let(:filename) { 'foo' }
-    let(:mail) { described_class.build(filename).deliver_now }
+    stem_exists = false
+    let(:mail) { described_class.build(filename, stem_exists).deliver_now }
     subject do
       stub_reports_s3(filename) do
         mail
@@ -38,6 +39,66 @@ RSpec.describe SpoolSubmissionsReportMailer, type: %i[mailer aws_helpers] do
     context 'when not sending staging emails' do
       before do
         expect(FeatureFlipper).to receive(:staging_email?).once.and_return(false)
+      end
+
+      it 'should email the the right recipients' do
+        subject
+
+        expect(mail.to).to eq(
+          %w[
+            lihan@adhocteam.us
+            dana.kuykendall@va.gov
+            Jennifer.Waltz2@va.gov
+            shay.norton@va.gov
+            Darla.VanNieukerk@va.gov
+            Ricardo.DaSilva@va.gov
+          ]
+        )
+      end
+    end
+  end
+
+  describe '#build_stem' do
+    let(:filename) { 'foo' }
+    stem_exists = true
+    let(:mail) { described_class.build(filename, stem_exists).deliver_now }
+    subject do
+      stub_reports_s3(filename) do
+        mail
+      end
+    end
+
+    context 'when sending staging emails' do
+      before do
+        expect(FeatureFlipper).to receive(:staging_email?).twice.and_return(true)
+      end
+      it 'should send the right email' do
+        subject
+        text = described_class::REPORT_TEXT
+        expect(mail.body.encoded).to eq("#{text} (link expires in one week)<br>#{subject}")
+        expect(mail.subject).to eq(text)
+      end
+      it 'should email the the right staging recipients' do
+        subject
+
+        expect(mail.to).to eq(
+          %w[
+            lihan@adhocteam.us
+            Turner_Desiree@bah.com
+            Delli-Gatti_Michael@bah.com
+            kyle.pietrosanto@va.gov
+            robert.shinners@va.gov
+            shay.norton-leonard@va.gov
+            hughes_dustin@bah.com
+            sonntag_adam@bah.com
+          ]
+        )
+      end
+    end
+
+    context 'when not sending staging emails' do
+      before do
+        expect(FeatureFlipper).to receive(:staging_email?).twice.and_return(false)
       end
 
       it 'should email the the right recipients' do
