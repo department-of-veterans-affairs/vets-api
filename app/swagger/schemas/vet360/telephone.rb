3d47c7c2ee12db08a046c43fcd06857f4f310352
Travis Hilton <travis.hilton@oddball.io>
2019-08-19 10:25:49 -0500

    added is_textable and is_text_permitted to vets360 telephone model as well as updated swagger docs to include those attributes along with several other telephone attributes that were missing.

--- app/swagger/schemas/vet360/telephone.rb
+++ app/swagger/schemas/vet360/telephone.rb
@@ -8,67 +8,137 @@ module Swagger
 
         swagger_schema :PostVet360Telephone do
           key :required, %i[phone_number area_code phone_type is_international country_code]
-          property :is_international,
-                   type: :boolean,
-                   enum: [false],
-                   example: false
-          property :country_code,
-                   type: :string,
-                   enum: ['1'],
-                   example: '1'
-          property :phone_number,
-                   type: :string,
-                   example: '5551212',
-                   minLength: 1,
-                   maxLength: 14,
-                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect
           property :area_code,
                    type: :string,
                    example: '303',
                    minLength: 3,
                    maxLength: 3,
                    pattern: ::Vet360::Models::Telephone::VALID_AREA_CODE_REGEX.inspect
+          property :country_code,
+                   type: :string,
+                   enum: ['1'],
+                   example: '1'
+          # property :created_at,
+          #          type: :string,
+          #          format: 'date-time',
+          #          example: '2018-04-21T20:09:50Z'
+          property :effective_end_date,
+                   type: %i[string null],
+                   format: 'date-time',
+                   example: '2018-04-21T20:09:50Z'
+          property :effective_start_date,
+                   type: %i[string null],
+                   format: 'date-time',
+                   example: '2018-04-21T20:09:50Z'
           property :extension,
                    type: :string,
                    example: '101',
                    maxLength: 10
+          property :is_international,
+                   type: :boolean,
+                   example: false
+          property :is_textable,
+                   type: :boolean,
+                   example: true
+          property :is_text_permitted,
+                   type: :boolean,
+                   example: true
+          property :is_tty, 
+                   type: :boolean,
+                   example: true
+          property :is_voicemailable, 
+                   type: :boolean,
+                   example: true
+          property :phone_number,
+                   type: :string,
+                   example: '5551212',
+                   minLength: 1,
+                   maxLength: 14,
+                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect
           property :phone_type,
                    type: :string,
                    enum: ::Vet360::Models::Telephone::PHONE_TYPES,
                    example: ::Vet360::Models::Telephone::MOBILE
+          property :source_date,
+                   type: :string,
+                   format: 'date-time',
+                   example: '2018-04-21T20:09:50Z'
+          # property :updated_at,
+          #          type: :string,
+          #          format: 'date-time',
+          #          example: '2018-04-21T20:09:50Z'
+          property :vet360_id, 
+                   type: :integer, 
+                   example: 1
         end
 
         swagger_schema :PutVet360Telephone do
           key :required, %i[id phone_number area_code phone_type is_international country_code]
-          property :id, type: :integer, example: 1
-          property :is_international,
-                   type: :boolean,
-                   enum: [false],
-                   example: false
-          property :country_code,
-                   type: :string,
-                   enum: ['1'],
-                   example: '1'
-          property :phone_number,
-                   type: :string,
-                   example: '5551212',
-                   minLength: 1,
-                   maxLength: 14,
-                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect
+          property :id, 
+                   type: :integer, 
+                   example: 1
           property :area_code,
                    type: :string,
                    example: '303',
                    minLength: 3,
                    maxLength: 3,
                    pattern: ::Vet360::Models::Telephone::VALID_AREA_CODE_REGEX.inspect
+          property :country_code,
+                   type: :string,
+                   enum: ['1'],
+                   example: '1'
+          # property :created_at,
+          #          type: :string,
+          #          format: 'date-time',
+          #          example: '2018-04-21T20:09:50Z'
+          property :effective_end_date,
+                   type: %i[string null],
+                   format: 'date-time',
+                   example: '2018-04-21T20:09:50Z'
+          property :effective_start_date,
+                   type: %i[string null],
+                   format: 'date-time',
+                   example: '2018-04-21T20:09:50Z'
           property :extension,
                    type: :string,
                    example: '101',
                    maxLength: 10
+          property :is_international,
+                   type: :boolean,
+                   example: false
+          property :is_textable,
+                   type: :boolean,
+                   example: true
+          property :is_text_permitted,
+                   type: :boolean,
+                   example: true
+          property :is_tty, 
+                   type: :boolean,
+                   example: true
+          property :is_voicemailable, 
+                   type: :boolean,
+                   example: true
+          property :phone_number,
+                   type: :string,
+                   example: '5551212',
+                   minLength: 1,
+                   maxLength: 14,
+                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect
           property :phone_type,
                    type: :string,
                    enum: ::Vet360::Models::Telephone::PHONE_TYPES,
                    example: ::Vet360::Models::Telephone::MOBILE
+          property :source_date,
+                   type: :string,
+                   format: 'date-time',
+                   example: '2018-04-21T20:09:50Z'
+          # property :updated_at,
+          #          type: :string,
+          #          format: 'date-time',
+          #          example: '2018-04-21T20:09:50Z'
+          property :vet360_id, 
+                   type: :integer, 
+                   example: 1
         end
       end
     end
