31259cda94b7a85d1ac967f8a13aa6f119b4b85f
Travis <travis.hilton@oddball.io>
2019-08-20 09:02:03 -0500

    added attribute descriptions to vet360 telephone swagger schema and removed a couple of commented out attributes from same.

--- app/swagger/schemas/vet360/telephone.rb
+++ app/swagger/schemas/vet360/telephone.rb
@@ -13,15 +13,13 @@ module Swagger
                    example: '303',
                    minLength: 3,
                    maxLength: 3,
-                   pattern: ::Vet360::Models::Telephone::VALID_AREA_CODE_REGEX.inspect
+                   pattern: ::Vet360::Models::Telephone::VALID_AREA_CODE_REGEX.inspect,
+                   description: 'The three-digit code that begins a North American (the U.S., Canada and Mexico) phone number.'
           property :country_code,
                    type: :string,
                    enum: ['1'],
-                   example: '1'
-          # property :created_at,
-          #          type: :string,
-          #          format: 'date-time',
-          #          example: '2018-04-21T20:09:50Z'
+                   example: '1',
+                   description: 'First two to four digits of a non- North American phone number that routes the call to country of that phone number.'
           property :effective_end_date,
                    type: %i[string null],
                    format: 'date-time',
@@ -33,43 +31,48 @@ module Swagger
           property :extension,
                    type: :string,
                    example: '101',
-                   maxLength: 10
+                   maxLength: 10,
+                   description: 'One-or-more digit number that must be dialed after reaching a main number, typically at an establishment, in order to reach a specific party.'
           property :is_international,
                    type: :boolean,
                    example: false
           property :is_textable,
                    type: :boolean,
-                   example: true
+                   example: true,
+                   description: 'Indicates phone number is capable of receiving text messages.'
           property :is_text_permitted,
                    type: :boolean,
-                   example: true
+                   example: true,
+                   description: 'Indicates person has granted permission to receive text messages to phone number.'
           property :is_tty, 
                    type: :boolean,
-                   example: true
+                   example: true,
+                   description: 'Indicates phone number has TTY capability (TeleTypewriter). This allows deaf or hard of hearing individuals to utilize typing versus voice over a phone connection.'
           property :is_voicemailable, 
                    type: :boolean,
-                   example: true
+                   example: true,
+                   description: 'Indicates person has granted consent to record a voice mail message on a phone number.'
           property :phone_number,
                    type: :string,
                    example: '5551212',
                    minLength: 1,
                    maxLength: 14,
-                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect
+                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect,
+                   description: 'Number that follows an area code for a North American phone number, or a country code for a non-North American phone number.'
           property :phone_type,
                    type: :string,
                    enum: ::Vet360::Models::Telephone::PHONE_TYPES,
-                   example: ::Vet360::Models::Telephone::MOBILE
+                   example: ::Vet360::Models::Telephone::MOBILE,
+                   description: 'Describes specific type of phone number.'
           property :source_date,
                    type: :string,
                    format: 'date-time',
-                   example: '2018-04-21T20:09:50Z'
-          # property :updated_at,
-          #          type: :string,
-          #          format: 'date-time',
-          #          example: '2018-04-21T20:09:50Z'
+                   example: '2018-04-21T20:09:50Z',
+                   description: 'The date the source system received the last update to this bio.'
           property :vet360_id, 
                    type: :integer, 
-                   example: 1
+                   example: 1,
+                   description: 'Unique Identifier of individual within VET360. Created by VET360 after it is validated and accepted. May be considered PII.'
         end
 
         swagger_schema :PutVet360Telephone do
@@ -82,15 +85,13 @@ module Swagger
                    example: '303',
                    minLength: 3,
                    maxLength: 3,
-                   pattern: ::Vet360::Models::Telephone::VALID_AREA_CODE_REGEX.inspect
+                   pattern: ::Vet360::Models::Telephone::VALID_AREA_CODE_REGEX.inspect,
+                   description: 'The three-digit code that begins a North American (the U.S., Canada and Mexico) phone number.'
           property :country_code,
                    type: :string,
                    enum: ['1'],
-                   example: '1'
-          # property :created_at,
-          #          type: :string,
-          #          format: 'date-time',
-          #          example: '2018-04-21T20:09:50Z'
+                   example: '1',
+                   description: 'First two to four digits of a non- North American phone number that routes the call to country of that phone number.'
           property :effective_end_date,
                    type: %i[string null],
                    format: 'date-time',
@@ -102,43 +103,48 @@ module Swagger
           property :extension,
                    type: :string,
                    example: '101',
-                   maxLength: 10
+                   maxLength: 10,
+                   description: 'One-or-more digit number that must be dialed after reaching a main number, typically at an establishment, in order to reach a specific party.'
           property :is_international,
                    type: :boolean,
                    example: false
           property :is_textable,
                    type: :boolean,
-                   example: true
+                   example: true,
+                   description: 'Indicates phone number is capable of receiving text messages.'
           property :is_text_permitted,
                    type: :boolean,
-                   example: true
+                   example: true,
+                   description: 'Indicates person has granted permission to receive text messages to phone number.'
           property :is_tty, 
                    type: :boolean,
-                   example: true
+                   example: true,
+                   description: 'Indicates phone number has TTY capability (TeleTypewriter). This allows deaf or hard of hearing individuals to utilize typing versus voice over a phone connection.'
           property :is_voicemailable, 
                    type: :boolean,
-                   example: true
+                   example: true,
+                   description: 'Indicates person has granted consent to record a voice mail message on a phone number.'
           property :phone_number,
                    type: :string,
                    example: '5551212',
                    minLength: 1,
                    maxLength: 14,
-                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect
+                   pattern: ::Vet360::Models::Telephone::VALID_PHONE_NUMBER_REGEX.inspect,
+                   description: 'Number that follows an area code for a North American phone number, or a country code for a non-North American phone number.'
           property :phone_type,
                    type: :string,
                    enum: ::Vet360::Models::Telephone::PHONE_TYPES,
-                   example: ::Vet360::Models::Telephone::MOBILE
+                   example: ::Vet360::Models::Telephone::MOBILE,
+                   description: 'Describes specific type of phone number.'
           property :source_date,
                    type: :string,
                    format: 'date-time',
-                   example: '2018-04-21T20:09:50Z'
-          # property :updated_at,
-          #          type: :string,
-          #          format: 'date-time',
-          #          example: '2018-04-21T20:09:50Z'
+                   example: '2018-04-21T20:09:50Z',
+                   description: 'The date the source system received the last update to this bio.'
           property :vet360_id, 
                    type: :integer, 
-                   example: 1
+                   example: 1,
+                   description: 'Unique Identifier of individual within VET360. Created by VET360 after it is validated and accepted. May be considered PII.'
         end
       end
     end
