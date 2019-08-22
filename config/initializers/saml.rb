9146f581644e08769a7dc1c653a4fb298889e109
Dan Hinze <hinzed1127@gmail.com>
2019-08-22 11:49:52 -0500

    SSOe integration prototype initial work (#3210)
    Enables a v1 session controller that will consume SAML assertions from SSOe. Clones the settings service and health status endpoints and introduces some redundant controller code but those are intended as temporary.

--- config/initializers/saml.rb
+++ config/initializers/saml.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/LineLength
+
 def new_saml_cert_exists?
   !Settings.saml.cert_new_path.nil? && File.file?(File.expand_path(Settings.saml.cert_new_path))
 end
@@ -7,3 +9,12 @@ end
 Settings.saml.certificate = File.read(File.expand_path(Settings.saml.cert_path))
 Settings.saml.key = File.read(File.expand_path(Settings.saml.key_path))
 Settings.saml.certificate_new = new_saml_cert_exists? ? File.read(File.expand_path(Settings.saml.cert_new_path)) : nil
+
+def new_saml_ssoe_cert_exists?
+  !Settings.saml_ssoe.cert_new_path.nil? && File.file?(File.expand_path(Settings.saml_ssoe.cert_new_path))
+end
+
+Settings.saml_ssoe.certificate = File.read(File.expand_path(Settings.saml_ssoe.cert_path))
+Settings.saml_ssoe.key = File.read(File.expand_path(Settings.saml_ssoe.key_path))
+Settings.saml_ssoe.certificate_new = new_saml_ssoe_cert_exists? ? File.read(File.expand_path(Settings.saml_ssoe.cert_new_path)) : nil
+# rubocop:enable Metrics/LineLength
