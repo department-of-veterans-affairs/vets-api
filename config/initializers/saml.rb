7064ee33bdca41911574a18649f37418497faf41
patrickvinograd <patrick@adhocteam.us>
2019-08-22 12:36:01 -0700

    Make SAML initializer safe to missing SSOe cert/key files (#3231)

--- config/initializers/saml.rb
+++ config/initializers/saml.rb
@@ -10,11 +10,19 @@ Settings.saml.certificate = File.read(File.expand_path(Settings.saml.cert_path))
 Settings.saml.key = File.read(File.expand_path(Settings.saml.key_path))
 Settings.saml.certificate_new = new_saml_cert_exists? ? File.read(File.expand_path(Settings.saml.cert_new_path)) : nil
 
+def saml_ssoe_cert_exists?
+  !Settings.saml_ssoe.cert_path.nil? && File.file?(File.expand_path(Settings.saml_ssoe.cert_path))
+end
+
+def saml_ssoe_key_exists?
+  !Settings.saml_ssoe.key_path.nil? && File.file?(File.expand_path(Settings.saml_ssoe.key_path))
+end
+
 def new_saml_ssoe_cert_exists?
   !Settings.saml_ssoe.cert_new_path.nil? && File.file?(File.expand_path(Settings.saml_ssoe.cert_new_path))
 end
 
-Settings.saml_ssoe.certificate = File.read(File.expand_path(Settings.saml_ssoe.cert_path))
-Settings.saml_ssoe.key = File.read(File.expand_path(Settings.saml_ssoe.key_path))
+Settings.saml_ssoe.certificate = saml_ssoe_cert_exists? ? File.read(File.expand_path(Settings.saml_ssoe.cert_path)) : nil
+Settings.saml_ssoe.key = saml_ssoe_key_exists? ? File.read(File.expand_path(Settings.saml_ssoe.key_path)) : nil
 Settings.saml_ssoe.certificate_new = new_saml_ssoe_cert_exists? ? File.read(File.expand_path(Settings.saml_ssoe.cert_new_path)) : nil
 # rubocop:enable Metrics/LineLength
