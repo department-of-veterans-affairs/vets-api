ac9075ea3df9f8ed7b2fcdeb79ee6b707328ac68
ATeal <arteal89@gmail.com>
2019-08-23 15:09:19 -0400

    Move header validation into it's own concern

--- modules/claims_api/app/controllers/claims_api/application_controller.rb
+++ modules/claims_api/app/controllers/claims_api/application_controller.rb
@@ -1,26 +1,52 @@
 # frozen_string_literal: true
 
-require_dependency 'claims_api/concerns/request_logging'
 require_dependency 'claims_api/concerns/mvi_verification'
+require_dependency 'claims_api/concerns/header_validation'
 
 module ClaimsApi
   class ApplicationController < ::OpenidApplicationController
     include ClaimsApi::MviVerification
+    include ClaimsApi::HeaderValidation
 
     skip_before_action :set_tags_and_extra_context, raise: false
 
     private
 
-    def target_veteran(with_gender: false)
+    def header(key)
+      request.headers[key]
+    end
+
+    def header_request?
       headers_to_check = ['HTTP_X_VA_SSN', 'HTTP_X_VA_Consumer-Username', 'HTTP_X_VA_BIRTH_DATE']
-      header_request = (request.headers.to_h.keys & headers_to_check).length.positive?
-      if header_request
-        vet = ClaimsApi::Veteran.from_headers(request.headers, with_gender: with_gender)
-        vet.loa = @current_user.loa if @current_user
-        vet
+      (request.headers.to_h.keys & headers_to_check).length.positive?
+    end
+
+    def target_veteran(with_gender: false)
+      if header_request?
+
+        headers_to_validate = ['X-VA-SSN', 'X-VA-First-Name', 'X-VA-Last-Name', 'X-VA-Birth-Date']
+        headers_to_validate << 'X-VA-Gender' if with_gender
+        validate_headers(headers_to_validate)
+
+        veteran_from_headers(with_gender: with_gender)
       else
         ClaimsApi::Veteran.from_identity(identity: @current_user)
       end
+    end
+
+    def veteran_from_headers(with_gender: false)
+      vet = ClaimsApi::Veteran.new(
+        uuid: header('X-VA-SSN'),
+        ssn: header('X-VA-SSN'),
+        first_name: header('X-VA-First-Name'),
+        last_name: header('X-VA-Last-Name'),
+        profile: ClaimsApi::Veteran.build_profile(header('X-VA-Birth-Date')),
+        last_signed_in: Time.now.utc
+      )
+      vet.loa = @current_user.loa if @current_user
+      vet.gender = header('X-VA-Gender') if with_gender
+      vet.edipi = header('X-VA-EDIPI')
+      vet
     end
   end
 end
---
+++ modules/claims_api/app/controllers/claims_api/concerns/header_validation.rb
@@ -0,0 +1,13 @@
+module ClaimsApi
+  module HeaderValidation
+    extend ActiveSupport::Concern
+
+    included do
+      def validate_headers(required_headers)
+        required_headers.each do |header|
+          raise Common::Exceptions::ParameterMissing, header unless request.headers[header]
+        end
+      end
+    end
+  end
+end
