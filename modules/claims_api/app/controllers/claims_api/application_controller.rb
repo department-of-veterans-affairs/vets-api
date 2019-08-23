29595fd9a20a50a5f1b41097b2a8a6e45a4fd3cc
ATeal <arteal89@gmail.com>
2019-08-23 10:23:09 -0400

    Remove request logging

--- modules/claims_api/app/controllers/claims_api/application_controller.rb
+++ modules/claims_api/app/controllers/claims_api/application_controller.rb
@@ -5,7 +5,6 @@ require_dependency 'claims_api/concerns/mvi_verification'
 
 module ClaimsApi
   class ApplicationController < ::OpenidApplicationController
-    include ClaimsApi::RequestLogging
     include ClaimsApi::MviVerification
 
     skip_before_action :set_tags_and_extra_context, raise: false
@@ -13,7 +12,7 @@ module ClaimsApi
     private
 
     def target_veteran(with_gender: false)
-      headers_to_check = ['HTTP_X_VA_SSN', 'HTTP_X_VA_Consumer-Username', 'HTTP_X_VA_Birth_Date']
+      headers_to_check = ['HTTP_X_VA_SSN', 'HTTP_X_VA_Consumer-Username', 'HTTP_X_VA_BIRTH_DATE']
       header_request = (request.headers.to_h.keys & headers_to_check).length.positive?
       if header_request
         vet = ClaimsApi::Veteran.from_headers(request.headers, with_gender: with_gender)
--- modules/claims_api/app/controllers/claims_api/base_form_controller.rb
+++ modules/claims_api/app/controllers/claims_api/base_form_controller.rb
@@ -8,7 +8,6 @@ module ClaimsApi
     # schema endpoint should be wide open
     skip_before_action :authenticate, only: %i[schema]
     skip_before_action :verify_mvi, only: %i[schema]
-    skip_before_action :log_request, only: %i[schema]
 
     def schema
       render json: { data: [ClaimsApi::FormSchemas::SCHEMAS[self.class::FORM_NUMBER]] }
