2f07c21c3429851383709d6549d25b3558372067
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2019-08-26 14:41:45 -0400

    ruboco SkipsModelValidations rule improvement
    .update_column skips validations,
    .update_attributes is preferred to run validations

--- spec/jobs/central_mail/delete_old_claims_spec.rb
+++ spec/jobs/central_mail/delete_old_claims_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe CentralMail::DeleteOldClaims, uploader_helpers: true, type: :mode
       old_attachment_success = create(:pension_burial)
       file = old_attachment_success.file
       [new_attachment_success, old_attachment_success].each do |attachment|
-        attachment.saved_claim.central_mail_submission.update_column(:state, 'success')
+        attachment.saved_claim.central_mail_submission.update_attributes(state: 'success')
       end
 
       [old_attachment_pending, old_attachment_success].each do |attachment|
-        attachment.saved_claim.update_column(:created_at, 2.months.ago - 1.day)
+        attachment.saved_claim.update_attributes(created_at: 2.months.ago - 1.day)
       end
 
       described_class.new.perform
