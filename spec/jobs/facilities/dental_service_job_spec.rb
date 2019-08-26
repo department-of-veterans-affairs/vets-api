c9e472a8115af1c49cfda54bc49e24c8cf76151f
Nick Fasulo <nrfasulo@gmail.com>
2019-08-26 12:27:24 -0500

    Create dental service job and Redis namespace

---
+++ spec/jobs/facilities/dental_service_job_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'csv'
+require 'rails_helper'
+
+RSpec.describe Facilities::DentalServiceReloadJob, type: :job do
+  let(:dental_service_data) do
+    %Q(unique_id,facility_type\n402HB,va_health_facility\n436,va_health_facility\n436GH,va_health_facility)
+  end
+
+  before(:each) do
+    allow_any_instance_of(
+      Facilities::DentalServiceReloadJob
+    ).to receive(:fetch_dental_service_data).and_return(CSV.parse(dental_service_data, { headers: true }))
+  end
+
+  it 'populates facilities' do
+    Facilities::DentalServiceReloadJob.new.perform
+    expect(FacilityDentalService.find('402HB')).to_not be_nil
+    expect(FacilityDentalService.find('436')).to_not be_nil
+    expect(FacilityDentalService.find('436GH')).to_not be_nil
+  end
+
+  it 'populates facility data' do
+    now = Time.now.utc.iso8601
+    Facilities::DentalServiceReloadJob.new.perform
+    expect(FacilityDentalService.find('402HB').local_updated).to be >= now
+  end
+end
