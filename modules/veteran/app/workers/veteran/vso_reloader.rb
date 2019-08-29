4dce7953567aef5f6de0258d0881611bd287715c
Michael Bastos <bastosmichael@gmail.com>
2019-08-29 16:24:50 -0500

    adding attorneys and cas

--- modules/veteran/app/workers/veteran/vso_reloader.rb
+++ modules/veteran/app/workers/veteran/vso_reloader.rb
@@ -9,34 +9,66 @@ module Veteran
 
     def perform
       array_of_organizations = reload_representatives
-      Veteran::Service::Organization.import(array_of_organizations, on_duplicate_key_ignore: true)
+      Veteran::Service::Representative.where.not(representative_id: array_of_organizations).destroy_all
     end
 
     private
 
     def reload_representatives
-      data = fetch_data('orgsexcellist.asp')
-      data.each do |hash|
+      vso_reps = []
+      vso_orgs = fetch_data('orgsexcellist.asp').map do |hash|
         find_or_create_vso(hash)
+        vso_reps << hash['Registration Num']
+        { poa: hash['POA'], name: hash['Organization Name'], phone: hash['Org Phone'], state: hash['Org State'] }
+      end
+      Veteran::Service::Organization.import(vso_orgs, on_duplicate_key_ignore: true)
+
+      attorneys = fetch_data('attorneyexcellist.asp').map do |hash|
+        find_or_create_attorneys(hash)
+        hash['Registration Num']
       end
 
-      Veteran::Service::Representative.where.not(representative_id: data.map { |h| h['Registration Num'] }).destroy_all
+      claim_agents = fetch_data('caexcellist.asp').map do |hash|
+        find_or_create_claim_agents(hash)
+        hash['Registration Num']
+      end
 
-      array_of_organizations = data.map do |h|
-        { poa: h['POA'], name: h['Organization Name'], phone: h['Org Phone'], state: h['Org State'] }
-      end.uniq.compact
-      array_of_organizations
+      vso_reps + attorneys + claim_agents
+    end
+
+    def find_or_create_attorneys(hash)
+      rep = Veteran::Service::Representative.find_or_initialize_by( representative_id: hash['Registration Num'], 
+                                                                    first_name: hash['First Name'], 
+                                                                    last_name: hash['Last Name'] )
+      rep.poa_codes ||= []
+      rep.poa_codes << hash['POA Code'].gsub!(/\W/, '')
+      rep.phone = hash['Phone']
+      rep.user_types ||= []
+      rep.user_types << 'attorney'
+      rep.save
+    end
+
+    def find_or_create_claim_agents(hash)
+      rep = Veteran::Service::Representative.find_or_initialize_by( representative_id: hash['Registration Num'], 
+                                                                    first_name: hash['First Name'], 
+                                                                    last_name: hash['Last Name'] )
+      rep.poa_codes ||= []
+      rep.poa_codes << hash['POA Code'].gsub!(/\W/, '')
+      rep.phone = hash['Phone']
+      rep.user_types ||= []
+      rep.user_types << 'claim_agents'
+      rep.save
     end
 
     def find_or_create_vso(hash)
-      rep = Veteran::Service::Representative.find_or_initialize_by(representative_id: hash['Registration Num'])
+      rep = Veteran::Service::Representative.find_or_initialize_by( representative_id: hash['Registration Num'], 
+                                                                    first_name: hash['Representative'].split(' ').second, 
+                                                                    last_name: hash['Representative'].split(',').first )
       rep.poa_codes ||= []
       rep.poa_codes << hash['POA'].gsub!(/\W/, '')
-      rep.first_name = hash['Representative'].split(' ').second
-      rep.last_name = hash['Representative'].split(',').first
       rep.phone = hash['Org Phone']
       rep.user_types ||= []
-      rep.user_types << 'VSO'
+      rep.user_types << 'veteran_service_officer'
       rep.save
     end
 
