90be68d6f5d7a7a7ad84c9e648049dcbae02d336
Michael Bastos <bastosmichael@gmail.com>
2019-08-29 17:30:26 -0500

    adding new index

--- modules/veteran/db/migrate/20190825155789_add_vsr_type.rb
+++ modules/veteran/db/migrate/20190825155789_add_vsr_type.rb
@@ -9,6 +9,6 @@ class AddVsrType < ActiveRecord::Migration[5.2]
     remove_index :veteran_representatives, :representative_id
     remove_index :veteran_representatives, :first_name
     remove_index :veteran_representatives, :last_name
-    add_index :veteran_representatives, [:representative_id, :first_name, :last_name], unique: true, name: 'index_vso_id_first_last_name'
+    add_index :veteran_representatives, %i[representative_id first_name last_name], unique: true, name: 'index_vso_id_first_last_name'
   end
 end
