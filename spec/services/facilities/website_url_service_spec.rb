e05308ae7e70c1cb9f8972f501788fd52de95a67
Ryan Travitz <rtravitz@gmail.com>
2019-09-09 15:20:56 -0400

    Rt/facilities websites (#3265)
    * Add WebsiteUrlService
    
    * Integrate website url service with vha data pull

---
+++ spec/services/facilities/website_url_service_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Facilities::WebsiteUrlService do
+  it 'provides the url when it knows about a station id and facility type' do
+    service = Facilities::WebsiteUrlService.new
+    url = service.find_for_station('0231V', 'va_health_facility')
+    expect(url).to eq('http://www.beckley.va.gov/')
+  end
+
+  it 'provides the url for the correct type of facility' do
+    service = Facilities::WebsiteUrlService.new
+    health_url = service.find_for_station('402', 'va_health_facility')
+    benefits_url = service.find_for_station('402', 'va_benefits_facility')
+    expect(health_url).to eq('http://www.maine.va.gov/')
+    expect(benefits_url).to eq('http://www.benefits.va.gov/togus/')
+  end
+
+  it 'provides an nil when it does not know about a station id' do
+    service = Facilities::WebsiteUrlService.new
+    url = service.find_for_station('fAkE1D', 'va_health_facility')
+    expect(url).to be_nil
+  end
+end
