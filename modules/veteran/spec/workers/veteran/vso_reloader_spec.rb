925bb63aaf345169fce232bb650fee1bd5e13092
Michael Bastos <bastosmichael@gmail.com>
2019-09-09 15:01:50 -0500

    updating test

--- modules/veteran/spec/workers/veteran/vso_reloader_spec.rb
+++ modules/veteran/spec/workers/veteran/vso_reloader_spec.rb
@@ -16,6 +16,9 @@ RSpec.describe Veteran::VsoReloader, type: :job do
       VCR.use_cassette('veteran/ogc_poa_data') do
         Veteran::VsoReloader.new.perform
         expect(Veteran::Service::Representative.count).to eq 22_186
+        expect(Veteran::Service::Representative.where(user_types: ['attorney']).count).to eq 12_242
+        expect(Veteran::Service::Representative.where(user_types: ['veteran_service_officer']).count).to eq 9_420
+        expect(Veteran::Service::Representative.where(user_types: ['claim_agents']).count).to eq 524
         expect(Veteran::Service::Organization.count).to eq 91
       end
     end
