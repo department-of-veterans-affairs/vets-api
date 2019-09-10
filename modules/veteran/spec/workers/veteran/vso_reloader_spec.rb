ed550fdb6b688477331bd4ee44f265612ac4a8c7
Michael Bastos <bastosmichael@gmail.com>
2019-09-10 08:37:36 -0500

    Adding Attorneys and Claims Agents to VSR Model without migrations (#3277)
    * Running VSR migrations first
    
    * updating schema
    
    * fixing migration order
    
    * rubocop fix
    
    * Adding Attorneys and Claims Agents to VSR Model without migrations
    
    * fixing test
    
    * updating test
    
    * trim ogc test data

--- modules/veteran/spec/workers/veteran/vso_reloader_spec.rb
+++ modules/veteran/spec/workers/veteran/vso_reloader_spec.rb
@@ -15,8 +15,11 @@ RSpec.describe Veteran::VsoReloader, type: :job do
     it 'should reload data from pulldown' do
       VCR.use_cassette('veteran/ogc_poa_data') do
         Veteran::VsoReloader.new.perform
-        expect(Veteran::Service::Representative.count).to eq 152
+        expect(Veteran::Service::Representative.count).to eq 435
         expect(Veteran::Service::Organization.count).to eq 3
+        expect(Veteran::Service::Representative.attorneys.count).to eq 241
+        expect(Veteran::Service::Representative.veteran_service_officers.count).to eq 152
+        expect(Veteran::Service::Representative.claim_agents.count).to eq 42
       end
     end
   end
