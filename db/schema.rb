30c19292fb472d2af57adfaf52e23ddfcbbf4175
Michael Bastos <bastosmichael@gmail.com>
2019-09-10 09:23:17 -0500

    updating schema and uploaders

--- db/schema.rb
+++ db/schema.rb
@@ -102,10 +102,13 @@ ActiveRecord::Schema.define(version: 2019_09_04_156435) do
 
   create_table "claims_api_power_of_attorney", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "status"
+    t.string "current_poa"
     t.string "encrypted_form_data"
     t.string "encrypted_form_data_iv"
     t.string "encrypted_auth_headers"
     t.string "encrypted_auth_headers_iv"
+    t.string "encrypted_file_data", null: false
+    t.string "encrypted_file_data_iv", null: false
     t.string "md5"
     t.string "source"
     t.datetime "created_at", null: false
--- modules/claims_api/app/models/claims_api/power_of_attorney.rb
+++ modules/claims_api/app/models/claims_api/power_of_attorney.rb
@@ -7,6 +7,7 @@ module ClaimsApi
   class PowerOfAttorney < ApplicationRecord
     attr_encrypted(:form_data, key: Settings.db_encryption_key, marshal: true, marshaler: ClaimsApi::JsonMarshal)
     attr_encrypted(:auth_headers, key: Settings.db_encryption_key, marshal: true, marshaler: ClaimsApi::JsonMarshal)
+    attr_encrypted(:file_data, key: Settings.db_encryption_key, marshal: true, marshaler: ClaimsApi::JsonMarshal)
 
     before_validation :set_md5
     validates :md5, uniqueness: true
@@ -14,6 +15,24 @@ module ClaimsApi
     def set_md5
       headers = auth_headers.except('va_eauth_issueinstant', 'Authorization')
       self.md5 = Digest::MD5.hexdigest form_data.merge(headers).to_json
+    end
+
+    def file_name
+      file_data['filename']
+    end
+
+    def document_type
+      file_data['doc_type']
+    end
+
+    def set_file_data!(file_data, doc_type)
+      uploader.store!(file_data)
+      self.file_data = { filename: uploader.filename,
+                         doc_type: doc_type }
+    end
+
+    def uploader
+      @uploader ||= ClaimsApi::PowerOfAttorneyUploader.new(self.id)
     end
   end
 end
