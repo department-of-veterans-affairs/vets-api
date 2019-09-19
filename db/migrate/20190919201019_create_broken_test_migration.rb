7d0869d2c7bbf2be9f0649f1c10f9a9b5bd1e751
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2019-09-19 16:12:11 -0400

    test jenkins console output for broken migrations

---
+++ db/migrate/20190919201019_create_broken_test_migration.rb
@@ -0,0 +1,8 @@
+class CreateBrokenTestMigration < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flipper_features do |t|
+      t.string :key, null: false
+      t.timestamps null: false
+    end
+  end
+end
