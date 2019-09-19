dddabf9a87d6e7aeccd6954f5ee88c6e20e72a06
Nick Fasulo <nrfasulo@gmail.com>
2019-09-19 16:39:28 -0500

    Revert Add active_status column migration

--- db/migrate/20190912185242_add_active_status_to_base_facilities.rb
+++
@@ -1,5 +0,0 @@
-class AddActiveStatusToBaseFacilities < ActiveRecord::Migration[5.2]
-  def change
-    add_column :base_facilities, :active_status, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_185242) do
+ActiveRecord::Schema.define(version: 2019_08_16_120445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -66,8 +66,11 @@ ActiveRecord::Schema.define(version: 2019_09_12_185242) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.geography "location", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}
+<<<<<<< HEAD
     t.boolean "mobile"
     t.string "active_status"
+=======
+>>>>>>> parent of 0b46c6a4d... Add active_status column migration
     t.index ["location"], name: "index_base_facilities_on_location", using: :gist
     t.index ["name"], name: "index_base_facilities_on_name", using: :gin
     t.index ["unique_id", "facility_type"], name: "index_base_facilities_on_unique_id_and_facility_type", unique: true
