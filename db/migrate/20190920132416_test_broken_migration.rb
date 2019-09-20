59ad74355cdff878df62de6e5313be0617c0f262
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2019-09-20 10:47:28 -0400

    remove broken migration -- jenkins is now displaying error output for migrations
    :+1:

--- db/migrate/20190920132416_test_broken_migration.rb
+++
@@ -1,8 +0,0 @@
-class TestBrokenMigration < ActiveRecord::Migration[5.2]
-  def change
-    create_table :flipper_features do |t|
-      t.string :key, null: false
-      t.timestamps null: false
-    end
-  end
-end
