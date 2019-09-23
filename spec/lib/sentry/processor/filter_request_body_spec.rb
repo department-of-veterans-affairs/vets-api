1078bcf40cb46b8465c6e230ade53710c912bc83
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2019-09-23 12:28:32 -0400

    Remove previous specs

--- spec/lib/sentry/processor/filter_request_body_spec.rb
+++ spec/lib/sentry/processor/filter_request_body_spec.rb
@@ -3,49 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Sentry::Processor::FilterRequestBody do
-  let(:client) { double('client') }
-  let(:processor) { Sentry::Processor::FilterRequestBody.new(client) }
-  let(:result) { processor.process(sentry_data) }
-
-  let(:sentry_data) do
-    {
-      'tags' => { 'controller_name' => 'ppiu', 'sign_in_method' => { 'service_name' => 'idme', 'acct_type' => nil } },
-      'request' =>
-       {
-         'data' =>
-          "{\n  \"account_type\": \"Checking\"}"
-       }
-    }
-  end
-
-  def self.expect_filter(filtered)
-    it "should#{filtered ? '' : "n't"} filter the request body" do
-      expect(result['request']['data']).public_send(
-        filtered ? 'to' : 'to_not',
-        eq(Sentry::Processor::PIISanitizer::FILTER_MASK)
-      )
-    end
-  end
-
-  context 'with data from a controller in FILTERED_CONTROLLER' do
-    expect_filter(true)
-  end
-
-  context 'with data from another controller' do
-    before do
-      sentry_data['tags']['controller_name'] = 'health_care_applications'
-    end
-
-    expect_filter(false)
-  end
-
-  context 'with no request body' do
-    before do
-      sentry_data['request'].delete('data')
-    end
-
-    expect_filter(false)
-  end
 
   context 'with clearer specs' do
     it 'filters PII found in a FILTERED_CONTROLLER' do
