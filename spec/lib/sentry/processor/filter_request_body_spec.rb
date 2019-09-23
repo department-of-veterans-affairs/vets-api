c05b0f54c873a498f32462b70553e087675d82c3
John Paul Ashenfelter <johnpaulashenfelter@users.noreply.github.com>
2019-09-23 15:34:14 -0400

    Refactor the specs for clarity (#3329)
    * Duplicate existing specs
    
    * Remove previous specs
    
    * Promote client and processor to before(:each)
    
    * Refactor sentry setup into method
    
    * Parameterize method with focus of spec, rename for clarity
    
    * Wordsmithing

--- spec/lib/sentry/processor/filter_request_body_spec.rb
+++ spec/lib/sentry/processor/filter_request_body_spec.rb
@@ -3,47 +3,54 @@
 require 'rails_helper'
 
 RSpec.describe Sentry::Processor::FilterRequestBody do
-  let(:client) { double('client') }
-  let(:processor) { Sentry::Processor::FilterRequestBody.new(client) }
-  let(:result) { processor.process(sentry_data) }
+  context 'with PII in the [:request][:data] hash' do
+    before(:each) do
+      client = double('client')
+      @processor = Sentry::Processor::FilterRequestBody.new(client)
+    end
 
-  let(:sentry_data) do
+    it 'filters PII for a controller found in FILTERED_CONTROLLER' do
+      sentry_request = create_sentry_request_with_pii(controller: 'ppiu')
+      result = @processor.process(sentry_request)
+
+      expect(result['request']['data']).to eql(Sentry::Processor::PIISanitizer::FILTER_MASK)
+    end
+
+    it 'does not filter PII for a contoller not included in FILTERED_CONTROLLER' do
+      sentry_request = create_sentry_request_with_pii(controller: 'another_controller')
+
+      result = @processor.process(sentry_request)
+
+      expect(result['request']['data']).not_to eql(Sentry::Processor::PIISanitizer::FILTER_MASK)
+      expect(result['request']['data']).to eql("{\n  \"account_type\": \"Checking\"}")
+    end
+
+    it 'works when there is no request body' do
+      sentry_request =
+        {
+          'tags' => {
+            'controller_name' => 'ppiu',
+            'sign_in_method' => { 'service_name' => 'idme', 'acct_type' => nil }
+          },
+          'request' => {}
+        }
+      result = @processor.process(sentry_request)
+
+      expect(result['request']['data']).to eql(nil)
+    end
+  end
+
+  private
+
+  def create_sentry_request_with_pii(controller: 'default')
     {
-      'tags' => { 'controller_name' => 'ppiu', 'sign_in_method' => { 'service_name' => 'idme', 'acct_type' => nil } },
-      'request' =>
-       {
-         'data' =>
-          "{\n  \"account_type\": \"Checking\"}"
-       }
+      'tags' => {
+        'controller_name' => controller,
+        'sign_in_method' => { 'service_name' => 'idme', 'acct_type' => nil }
+      },
+      'request' => {
+        'data' => "{\n  \"account_type\": \"Checking\"}"
+      }
     }
-  end
-
-  def self.expect_filter(filtered)
-    it "should#{filtered ? '' : "n't"} filter the request body" do
-      expect(result['request']['data']).public_send(
-        filtered ? 'to' : 'to_not',
-        eq(Sentry::Processor::PIISanitizer::FILTER_MASK)
-      )
-    end
-  end
-
-  context 'with data from a controller in FILTERED_CONTROLLER' do
-    expect_filter(true)
-  end
-
-  context 'with data from another controller' do
-    before do
-      sentry_data['tags']['controller_name'] = 'health_care_applications'
-    end
-
-    expect_filter(false)
-  end
-
-  context 'with no request body' do
-    before do
-      sentry_data['request'].delete('data')
-    end
-
-    expect_filter(false)
   end
 end
