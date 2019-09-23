cc43045cc144b1297e49f091d9053809c9526a75
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2019-09-23 12:46:25 -0400

    Parameterize method with focus of spec, rename for clarity

--- spec/lib/sentry/processor/filter_request_body_spec.rb
+++ spec/lib/sentry/processor/filter_request_body_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe Sentry::Processor::FilterRequestBody do
     end
 
     it 'filters PII found in a FILTERED_CONTROLLER' do
-      sentry_request = create_sentry_request(controller: 'ppiu')
+      sentry_request = create_sentry_request_with_pii(controller: 'ppiu')
       result = @processor.process(sentry_request)
 
       expect(result['request']['data']).to eql(Sentry::Processor::PIISanitizer::FILTER_MASK)
     end
 
     it 'ignores any contoller not specified in FILTERED_CONTROLLER' do
-      sentry_request = create_sentry_request(controller: 'another_controller')
+      sentry_request = create_sentry_request_with_pii(controller: 'another_controller')
 
       result = @processor.process(sentry_request)
 
@@ -28,7 +28,10 @@ RSpec.describe Sentry::Processor::FilterRequestBody do
     it 'works when there is no request body' do
       sentry_request =
         {
-          'tags' => { 'controller_name' => 'ppiu', 'sign_in_method' => { 'service_name' => 'idme', 'acct_type' => nil } },
+          'tags' => {
+            'controller_name' => 'ppiu',
+            'sign_in_method' => { 'service_name' => 'idme', 'acct_type' => nil }
+          },
           'request' => {}
         }
       result = @processor.process(sentry_request)
@@ -39,15 +42,15 @@ RSpec.describe Sentry::Processor::FilterRequestBody do
 
   private
 
-  def create_sentry_request(controller: 'default')
-    pii_content = {
-      'data' => "{\n  \"account_type\": \"Checking\"}"
-    }.freeze
-
-    sentry_request =
-      {
-        'tags' => { 'controller_name' => controller, 'sign_in_method' => { 'service_name' => 'idme', 'acct_type' => nil } },
-        'request' => pii_content
+  def create_sentry_request_with_pii(controller: 'default')
+    {
+      'tags' => {
+        'controller_name' => controller,
+        'sign_in_method' => { 'service_name' => 'idme', 'acct_type' => nil }
+      },
+      'request' => {
+        'data' => "{\n  \"account_type\": \"Checking\"}"
       }
-    end
+    }
+  end
 end
