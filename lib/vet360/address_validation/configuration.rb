8c8b964ec0e05da4e8f43e563680bb0da4f5bf2c
Lihan Li <frankieteardrop@gmail.com>
2019-09-25 23:23:43 -0700

    add api key to address validation header

--- lib/vet360/address_validation/configuration.rb
+++ lib/vet360/address_validation/configuration.rb
@@ -7,6 +7,10 @@ module Vet360
         "https://dev-api.va.gov/services/address_validation/v1/"
       end
 
+      def base_request_headers
+        super.merge('apiKey' => Settings.vet360.address_validation.api_key)
+      end
+
       def service_name
         'Vet360/AddressValidation'
       end
