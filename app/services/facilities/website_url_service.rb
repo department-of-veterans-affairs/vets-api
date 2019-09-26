1995bb8f35810baf547d860a1bd446b5e2f1ae0d
Nick Fasulo <nrfasulo@gmail.com>
2019-09-26 10:54:04 -0500

    Handle lowercase StationNum in websites.csv (#3341)

--- app/services/facilities/website_url_service.rb
+++ app/services/facilities/website_url_service.rb
@@ -20,7 +20,7 @@ module Facilities
     def map_websites_to_stations(station_websites)
       station_websites.each_with_object({}) do |station, hash|
         org = station['Org'].downcase unless station['Org'].nil?
-        unique_id = "#{org}_#{station['StationNum']}"
+        unique_id = "#{org}_#{station['StationNum'].upcase}"
         hash[unique_id] = station['Website_URL']
       end
     end
