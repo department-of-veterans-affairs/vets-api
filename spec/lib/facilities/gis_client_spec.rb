c85024960d6b8bfc7627eee3e435800f2c0031ec
Will Huang <52384927+whuang85@users.noreply.github.com>
2019-09-28 03:51:21 -0500

    reformat code

--- spec/lib/facilities/gis_client_spec.rb
+++ spec/lib/facilities/gis_client_spec.rb
@@ -16,12 +16,12 @@ RSpec.describe Facilities::GisClient do
       r1 = double(Faraday::Response.new)
       r2 = double(Faraday::Response.new)
 
-      allow_any_instance_of(Faraday::Connection).to receive(:get).with(anything, has_order_and_offset('field', 0))
-                                                                 .and_return(r1)
+      allow_any_instance_of(Faraday::Connection)
+        .to receive(:get).with(anything, has_order_and_offset('field', 0)).and_return(r1)
       expect(r1).to receive(:env).and_return(double(body: [*1..12]))
 
-      allow_any_instance_of(Faraday::Connection).to receive(:get).with(anything, has_order_and_offset('field', 10))
-                                                                 .and_return(r2)
+      allow_any_instance_of(Faraday::Connection)
+        .to receive(:get).with(anything, has_order_and_offset('field', 10)).and_return(r2)
       expect(r2).to receive(:env).and_return(double(body: [*1..2]))
 
       data = subject.get_all_facilities('type', 'field', 10)
@@ -33,16 +33,16 @@ RSpec.describe Facilities::GisClient do
       r2 = double(Faraday::Response.new)
       r3 = double(Faraday::Response.new)
 
-      allow_any_instance_of(Faraday::Connection).to receive(:get).with(anything, has_order_and_offset('field', 0))
-                                                                 .and_return(r1)
+      allow_any_instance_of(Faraday::Connection)
+        .to receive(:get).with(anything, has_order_and_offset('field', 0)).and_return(r1)
       expect(r1).to receive(:env).and_return(double(body: [*1..10]))
 
-      allow_any_instance_of(Faraday::Connection).to receive(:get).with(anything, has_order_and_offset('field', 10))
-                                                                 .and_return(r2)
+      allow_any_instance_of(Faraday::Connection)
+        .to receive(:get).with(anything, has_order_and_offset('field', 10)).and_return(r2)
       expect(r2).to receive(:env).and_return(double(body: [*1..10]))
 
-      allow_any_instance_of(Faraday::Connection).to receive(:get).with(anything, has_order_and_offset('field', 20))
-                                                                 .and_return(r3)
+      allow_any_instance_of(Faraday::Connection)
+        .to receive(:get).with(anything, has_order_and_offset('field', 20)).and_return(r3)
       expect(r3).to receive(:env).and_return(double(body: []))
 
       data = subject.get_all_facilities('type', 'field', 10)
@@ -52,8 +52,8 @@ RSpec.describe Facilities::GisClient do
     it 'passes the correct offset to the query when it does not loop' do
       r1 = double(Faraday::Response.new)
 
-      allow_any_instance_of(Faraday::Connection).to receive(:get).with(anything, has_order_and_offset('field', 0))
-                                                                 .and_return(r1)
+      allow_any_instance_of(Faraday::Connection)
+        .to receive(:get).with(anything, has_order_and_offset('field', 0)).and_return(r1)
       expect(r1).to receive(:env).and_return(double(body: [*1..5]))
 
       data = subject.get_all_facilities('type', 'field', 10)
