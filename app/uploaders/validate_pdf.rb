089c9a31ae6ef417f27505ca38d63358db584c17
camerontesterman <cameronbtesterman@gmail.com>
2019-09-30 16:39:34 -0500

    code climate fixes

--- app/uploaders/validate_pdf.rb
+++ app/uploaders/validate_pdf.rb
@@ -12,12 +12,13 @@ module ValidatePdf
   def convert_to_temp_file(file)
     temp_file = file.tempfile
     return unless temp_file.readpartial(4) == '%PDF'
+
     temp_file
   end
 
   def validate(temp_file)
     pdf = Origami::PDF.read temp_file, decrypt: false
-    raise CarrierWave::UploadError, 'The uploaded PDF file is encrypted and cannot be read. Please upload unencrypted PDF files only.' if pdf.encrypted?
+    raise CarrierWave::UploadError, 'The uploaded PDF file is encrypted and cannot be read.' if pdf.encrypted?
   rescue Origami::InvalidPDFError
     raise CarrierWave::UploadError, 'The uploaded PDF file is invalid and cannot be read.'
   end
