9ee11b4d57ac98c74c360b3811dc515c0458f60f
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2019-10-01 23:24:50 -0400

    Pulled in feature view for editing.

--- lib/flipper/configuration_patch.rb
+++ lib/flipper/configuration_patch.rb
@@ -2,10 +2,14 @@
 
 module FlipperExtensions
   module ConfigurationPatch
+    # This attr exists in latest version of Flipper.
+    # To be removed after updating the gem.
+    attr_accessor :add_actor_placeholder
     attr_accessor :custom_views_path
 
     def initialize
       super
+      @add_actor_placeholder = 'a flipper id'
       @custom_views_path = nil
     end
   end
