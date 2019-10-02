f9aa93532681d67ba696ca26c85f268765a737b7
Katherine Rodriguez <edmkitty@users.noreply.github.com>
2019-10-02 11:02:02 -0400

    Create drive time bands migration (#3358)
    * Create 20190920134906_create_drivetime_bands.rb
    
    * Update schema.rb

---
+++ db/migrate/20190920134906_create_drivetime_bands.rb
@@ -0,0 +1,13 @@
+class CreateDrivetimeBands < ActiveRecord::Migration[5.2]
+  def change
+    create_table :drivetime_bands do |t|
+      t.string :name
+      t.integer :value
+      t.string :unit
+      t.st_polygon :polygon, geographic: true, null: false
+      t.string :vha_facility_id, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
