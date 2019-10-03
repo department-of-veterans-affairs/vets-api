dada8c12413a2dba87ed7da933a6f89c4cbead4a
Lihan Li <frankieteardrop@gmail.com>
2019-10-03 12:12:03 -0700

    dont use instance variables for log_exception

--- lib/evss/logged_service_exception.rb
+++ lib/evss/logged_service_exception.rb
@@ -3,30 +3,26 @@
 module EVSS
   class LoggedServiceException < EVSS::ServiceException
     def initialize(res_body, user, req_body = nil)
-      @res_body = res_body
-      @user = user
-      @req_body = req_body
+      log_exception(res_body, user, req_body)
 
-      log_exception
-
-      super(@res_body)
+      super(res_body)
     end
 
     private
 
-    def log_exception
+    def log_exception(res_body, user, req_body)
       # don't need to log GET exceptions, those are already logged to sentry with the response body saved
-      return if @req_body.blank?
+      return if req_body.blank?
 
       PersonalInformationLog.create(
         error_class: self.class.to_s,
         data: {
           user: {
-            uuid: @user.uuid,
-            edipi: @user.edipi
+            uuid: user.uuid,
+            edipi: user.edipi
           },
-          request: @req_body,
-          response: @res_body
+          request: req_body,
+          response: res_body
         }
       )
     end
