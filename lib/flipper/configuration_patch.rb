bed85dd2f5398d9f114782aebcb743ed48443dba
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2019-10-03 16:20:57 -0400

    Initial UI overhaul for feature view.

--- lib/flipper/configuration_patch.rb
+++ lib/flipper/configuration_patch.rb
@@ -2,14 +2,10 @@
 
 module FlipperExtensions
   module ConfigurationPatch
-    # This attr exists in latest version of Flipper.
-    # To be removed after updating the gem.
-    attr_accessor :add_actor_placeholder
     attr_accessor :custom_views_path
 
     def initialize
       super
-      @add_actor_placeholder = 'a flipper id'
       @custom_views_path = nil
     end
   end
