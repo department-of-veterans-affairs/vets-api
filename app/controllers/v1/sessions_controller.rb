80d412e74e89798ec3c91f438ea7a8de8214ee03
Ben Shyong <hello@benshyong.com>
2019-10-07 15:35:09 -0500

    conditionally disable mhv sso cookie

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -153,6 +153,7 @@ module V1
     def set_cookies
       Rails.logger.info('SSO: LOGIN', sso_logging_info)
       set_api_cookie!
+      set_sso_cookie! unless @current_user&.identity&.sign_in&.fetch(:service_name) == 'ssoe' # Sets a cookie "vagov_session_<env>" with attributes needed for SSO.
     end
 
     def after_login_actions
