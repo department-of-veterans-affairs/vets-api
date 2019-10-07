e6ba71bee9873209bff30ba2cc4fcd3e702877a1
Katherine Rodriguez <katr400@gmail.com>
2019-10-07 14:19:22 -0400

    Update drivetime_band_client.rb

--- lib/facilities/drivetime_band_client.rb
+++ lib/facilities/drivetime_band_client.rb
@@ -7,12 +7,12 @@ module Facilities
   class DrivetimeBandClient < Common::Client::Base
     configuration Facilities::DrivetimeBandConfiguration
 
-    def get_all_drive_time_bands(order_field, max_record_count)
+    def get_all_drivetime_bands(max_record_count)
       query_count = 0
       data_collector = []
       loop do
         params = build_params(query_count * max_record_count)
-        response = perform(:get, "/query?", params)
+        response = perform(:get, "/arcgis2/rest/services/Portal/MonthlyVAST_TTB/FeatureServer/0/query?", params)
         data_collector += response.body
         break if response.body.length < max_record_count
 
@@ -22,7 +22,7 @@ module Facilities
     end
 
     def build_params(offset)
-      { where: "", inSR: 4326, outSR: 4326, returnGeometry: true,
+      { where: "1=1", inSR: 4326, outSR: 4326, returnGeometry: true,
         returnCountOnly: false, outFields: '*', returnDistinctValues: false,
         orderByFields: 'Name', f: 'json', resultOffset: offset }
     end
