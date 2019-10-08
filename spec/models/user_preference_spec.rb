6de2a8cc0ae160c3a33585c28328196e921e001e
Travis <53486698+thilton-oddball@users.noreply.github.com>
2019-10-08 10:16:03 -0500

    Rubocop rspec fixes round 8 redux (#3403)
    * Fixing Rubocop RSpec/OverwritingSetup offense.
    
    * Fixing Rubocop RSpec/IteratedExpectation offense.
    
    * Fixing Rubocop RSpec/ExampleWithoutDescription offenses.

--- spec/models/user_preference_spec.rb
+++ spec/models/user_preference_spec.rb
@@ -56,10 +56,8 @@ describe UserPreference, type: :model do
         results = UserPreference.all_preferences_with_choices(account_id)
 
         expect(results.size).to eq 2
-        results.each do |result|
-          expect(result).to have_key(:preference)
-          expect(result).to have_key(:user_preferences)
-        end
+        expect(results).to all(have_key(:preference))
+        expect(results).to all(have_key(:user_preferences))
       end
     end
   end
