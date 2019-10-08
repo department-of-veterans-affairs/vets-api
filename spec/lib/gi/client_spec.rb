e4206f74802c68cc1c1929b68cb69b3a9fb2e0fe
Devin McCurdy <mccurdy_devin@bah.com>
2019-10-08 09:27:32 -0400

    Added tests

--- spec/lib/gi/client_spec.rb
+++ spec/lib/gi/client_spec.rb
@@ -6,8 +6,8 @@ require 'gi/client'
 describe 'gi client' do
   let(:client) { GI::Client.new }
 
-  it 'gets a list of autocomplete suggestions', :vcr do
-    client_response = client.get_autocomplete_suggestions(term: 'university')
+  it 'gets a list of institution autocomplete suggestions', :vcr do
+    client_response = client.get_institution_autocomplete_suggestions(term: 'university')
     expect(client_response[:data]).to be_an(Array)
     # TODO: This should probably be fixed to conform to JSONAPI
     # expect(client_response[:data].first.keys).to contain_exactly(:id, :type, :attributes)
@@ -19,8 +19,8 @@ describe 'gi client' do
     expect(client_response[:data].first.keys).to contain_exactly(:id, :type, :attributes)
   end
 
-  it 'gets search results', :vcr do
-    client_response = client.get_search_results(name: 'illinois')
+  it 'gets institution search results', :vcr do
+    client_response = client.get_institution_search_results(name: 'illinois')
     expect(client_response[:data]).to be_an(Array)
     expect(client_response[:data].first.keys).to contain_exactly(:id, :type, :attributes, :links)
   end
@@ -41,5 +41,16 @@ describe 'gi client' do
     client_response = client.get_zipcode_rate(id: '20001')
     expect(client_response[:data]).to be_a(Hash)
     expect(client_response[:data].keys).to contain_exactly(:id, :type, :attributes)
+  end
+
+  it 'gets institution program search results', :vcr do
+    client_response = client.get_institution_program_search_results(name: 'code')
+    expect(client_response[:data]).to be_an(Array)
+    expect(client_response[:data].first.keys).to contain_exactly(:id, :type, :attributes)
+  end
+
+  it 'gets a list of institution program autocomplete suggestions', :vcr do
+    client_response = client.get_institution_program_autocomplete_suggestions(term: 'code')
+    expect(client_response[:data]).to be_an(Array)
   end
 end
