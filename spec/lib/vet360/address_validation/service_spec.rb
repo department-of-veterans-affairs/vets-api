48ad1787e873def74a3d59de7dee6a3f898c267f
Lihan Li <frankieteardrop@gmail.com>
2019-10-10 02:45:16 -0700

    record invalid addr

--- spec/lib/vet360/address_validation/service_spec.rb
+++ spec/lib/vet360/address_validation/service_spec.rb
@@ -6,37 +6,52 @@ describe Vet360::AddressValidation::Service do
   let(:address) { build(:vet360_address) }
 
   describe '#candidate' do
-    it 'should return suggested addresses for a given address' do
-      address.address_line1 = '5 Stoddard Ct'
-      address.city = 'Sparks Glencoe'
-      address.state_code = 'MD'
-      address.zip_code = '21152'
+    context 'with an invalid address' do
+      it 'should return an error' do
+        address.address_line1 = 'sdfdsfsdf'
 
-      VCR.use_cassette(
-        'vet360/address_validation/candidate_one_match',
-        VCR::MATCH_EVERYTHING
-      ) do
-        res = described_class.new.candidate(address)
+        VCR.use_cassette(
+          'vet360/address_validation/candidate_no_match',
+          record: :once
+        ) do
+          res = described_class.new.candidate(address)
+        end
+      end
+    end
 
-        expect(JSON.parse(res.to_json)).to eq(
-          [{"address"=>
-           {"address_line1"=>"5 Stoddard Ct",
-            "address_type"=>"DOMESTIC",
-            "city"=>"Sparks Glencoe",
-            "country_name"=>"USA",
-            "country_code_iso3"=>"USA",
-            "county_code"=>"24005",
-            "county_name"=>"Baltimore",
-            "state_code"=>"MD",
-            "zip_code"=>"21152",
-            "zip_code_suffix"=>"9367"},
-          "address_meta_data"=>
-           {"confidence_score"=>100.0,
-            "address_type"=>"Domestic",
-            "delivery_point_validation"=>"CONFIRMED",
-            "residential_delivery_indicator"=>"RESIDENTIAL",
-            "validation_key"=>-2025296286}}]
-        )
+    context 'with a found address' do
+      it 'should return suggested addresses for a given address' do
+        address.address_line1 = '5 Stoddard Ct'
+        address.city = 'Sparks Glencoe'
+        address.state_code = 'MD'
+        address.zip_code = '21152'
+
+        VCR.use_cassette(
+          'vet360/address_validation/candidate_one_match',
+          VCR::MATCH_EVERYTHING
+        ) do
+          res = described_class.new.candidate(address)
+
+          expect(JSON.parse(res.to_json)).to eq(
+            [{"address"=>
+             {"address_line1"=>"5 Stoddard Ct",
+              "address_type"=>"DOMESTIC",
+              "city"=>"Sparks Glencoe",
+              "country_name"=>"USA",
+              "country_code_iso3"=>"USA",
+              "county_code"=>"24005",
+              "county_name"=>"Baltimore",
+              "state_code"=>"MD",
+              "zip_code"=>"21152",
+              "zip_code_suffix"=>"9367"},
+            "address_meta_data"=>
+             {"confidence_score"=>100.0,
+              "address_type"=>"Domestic",
+              "delivery_point_validation"=>"CONFIRMED",
+              "residential_delivery_indicator"=>"RESIDENTIAL",
+              "validation_key"=>-2025296286}}]
+          )
+        end
       end
     end
   end
