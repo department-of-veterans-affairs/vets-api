32f156cbd32b56642a9e4843aa3ac0e7e900bb91
Katherine Rodriguez <edmkitty@users.noreply.github.com>
2019-10-10 14:11:37 -0400

    Kr/create mock drive time band data (#3400)
    * Add Drivetimeband clients and configurations
    
    * Change how base path is configured
    
    * Update drivetime_band_client.rb
    
    * Delete metadata
    
    * Update drivetime_band_client.rb
    
    * Add PSSG to betamocks config
    
    * Update settings.yml
    
    * Refactor for rubocop
    
    * Update services_config.yml
    
    * Create drivetime_band_client_spec.rb
    
    * Update services_config.yml
    
    * Update drivetime_band_client.rb
    
    * Update drivetime_band_client_spec.rb
    
    * fix rubocop issue

---
+++ lib/facilities/drivetime_band_client.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'common/client/base'
+
+module Facilities
+  # Core class responsible for api interface operations
+  class DrivetimeBandClient < Common::Client::Base
+    configuration Facilities::DrivetimeBandConfiguration
+
+    def get_drivetime_bands(offset, limit)
+      params = build_params(offset, limit)
+      response = perform(:get, '/arcgis2/rest/services/Portal/MonthlyVAST_TTB/FeatureServer/0/query', params)
+      JSON.parse(response.body)['features']
+    end
+
+    def build_params(offset, limit)
+      { where: '1=1', inSR: 4326, outSR: 4326, returnGeometry: true,
+        returnCountOnly: false, outFields: '*', returnDistinctValues: false,
+        orderByFields: 'Name', f: 'json', resultOffset: offset, resultRecordCount: limit }
+    end
+  end
+end
