39382c65274dfa3cd5a769c571d33fccc23c24f6
Alastair Dawson <alastair.j.dawson@gmail.com>
2019-10-11 16:20:28 -0700

    update create session response

--- modules/vaos/app/services/vaos/user_service.rb
+++ modules/vaos/app/services/vaos/user_service.rb
@@ -17,11 +17,10 @@ module VAOS
       url = '/users/v2/session?processRules=true'
       token = VAOS::JWT.new(user).token
       response = perform(:post, url, token, 'Content-Type' => 'text/plain', 'Referer' => 'https://api.va.gov')
-      if response&.status == 200 && response&.body
-        store = SessionStore.new(user_uuid: user.uuid, token: response.body)
-        store.save
+      if response&.body
+        SessionStore.new(user_uuid: user.uuid, token: response.body).save
+        response.body
       end
-      token
     end
   end
 end
