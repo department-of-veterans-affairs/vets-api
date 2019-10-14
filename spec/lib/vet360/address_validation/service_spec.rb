40364f42a3a3842319c11d9597d232ae5c366960
Lihan Li <frankieteardrop@gmail.com>
2019-10-14 04:26:11 -0700

    multiple suggestions

--- spec/lib/vet360/address_validation/service_spec.rb
+++ spec/lib/vet360/address_validation/service_spec.rb
@@ -6,7 +6,6 @@ describe Vet360::AddressValidation::Service do
   let(:address) { build(:vet360_address) }
 
   describe '#candidate' do
-    # TODO multiple suggestions
     # TODO business address
     context 'with an invalid address' do
       it 'should return an error' do
@@ -22,6 +21,55 @@ describe Vet360::AddressValidation::Service do
     end
 
     context 'with a found address' do
+      context 'with multiple matches' do
+        it 'should return suggested addresses for a given address' do
+          address.address_line1 = '37 1st st'
+          address.city = 'Brooklyn'
+          address.state_code = 'NY'
+          address.zip_code = '11249'
+
+          VCR.use_cassette(
+            'vet360/address_validation/candidate_multiple_matches',
+            VCR::MATCH_EVERYTHING
+          ) do
+            res = described_class.new.candidate(address)
+
+            expect(JSON.parse(res.to_json)).to eq(
+              [{"address"=>
+               {"address_line1"=>"37 N 1st St",
+                "address_type"=>"DOMESTIC",
+                "city"=>"Brooklyn",
+                "country_name"=>"USA",
+                "country_code_iso3"=>"USA",
+                "county_code"=>"36047",
+                "county_name"=>"Kings",
+                "state_code"=>"NY",
+                "zip_code"=>"11249",
+                "zip_code_suffix"=>"3939"},
+              "address_meta_data"=>
+               {"confidence_score"=>100.0, "address_type"=>"Domestic", "delivery_point_validation"=>"UNDELIVERABLE", "validation_key"=>-1384531381}},
+             {"address"=>
+               {"address_line1"=>"37 S 1st St",
+                "address_type"=>"DOMESTIC",
+                "city"=>"Brooklyn",
+                "country_name"=>"USA",
+                "country_code_iso3"=>"USA",
+                "county_code"=>"36047",
+                "county_name"=>"Kings",
+                "state_code"=>"NY",
+                "zip_code"=>"11249",
+                "zip_code_suffix"=>"4101"},
+              "address_meta_data"=>
+               {"confidence_score"=>100.0,
+                "address_type"=>"Domestic",
+                "delivery_point_validation"=>"CONFIRMED",
+                "residential_delivery_indicator"=>"MIXED",
+                "validation_key"=>2017396678}}]
+            )
+          end
+        end
+      end
+
       it 'should return suggested addresses for a given address' do
         address.address_line1 = '5 Stoddard Ct'
         address.city = 'Sparks Glencoe'
