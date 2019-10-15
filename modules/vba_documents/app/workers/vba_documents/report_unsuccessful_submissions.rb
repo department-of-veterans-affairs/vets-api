4592aef88076b3cb7fa02b3011f0bf80e76f7c15
ATeal <arteal89@gmail.com>
2019-10-15 08:58:33 -0400

    Only report unsuccessful stuff on prod via settings

--- modules/vba_documents/app/workers/vba_documents/report_unsuccessful_submissions.rb
+++ modules/vba_documents/app/workers/vba_documents/report_unsuccessful_submissions.rb
@@ -7,13 +7,15 @@ module VBADocuments
     include Sidekiq::Worker
 
     def perform
-      from = 7.days.ago
-      to = Time.zone.today
-      submissions = VBADocuments::UploadSubmission.where(
-        created_at: from..to,
-        status: %w[error expired]
-      )
-      VBADocuments::UnsuccessfulReportMailer.build(submissions, from, to).deliver_now
+      if Settings.vba_documents.unsuccessful_report_enabled
+        from = 7.days.ago
+        to = Time.zone.today
+        submissions = VBADocuments::UploadSubmission.where(
+          created_at: from..to,
+          status: %w[error expired]
+        )
+        VBADocuments::UnsuccessfulReportMailer.build(submissions, from, to).deliver_now
+      end
     end
   end
 end
