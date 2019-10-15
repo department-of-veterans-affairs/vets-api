60365dc338a0d5dfd688b8d2fb6b6dd12ba6eede
talbotmj <talbot58@github.com>
2019-10-15 15:41:49 -0600

    updating person factory to register telephone and permission factory bots

--- spec/factories/vet360/persons.rb
+++ spec/factories/vet360/persons.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
   factory :person, class: 'Vet360::Models::Person' do
     addresses   { [FactoryBot.build(:vet360_address), FactoryBot.build(:vet360_address, :mailing)] }
     emails      { [FactoryBot.build(:email)] }
-    # TODO: the telephone & permission factories will not register??
-    # telephones  [FactoryBot.build(:telephone)]
-    # permissions      [FactoryBot.build(:permission)]
+    # TODO: test that these telephone & permission factories register
+    telephones  { [FactoryBot.build(:telephone)] }
+    permissions { [FactoryBot.build(:permission)] }
     source_date { '2018-04-09T11:52:03-06:00' }
     created_at  { '2017-04-09T11:52:03-06:00' }
     updated_at  { '2017-04-09T11:52:03-06:00' }
