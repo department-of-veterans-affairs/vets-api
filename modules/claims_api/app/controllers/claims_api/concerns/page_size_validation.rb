8f6d5babd926a067cfb53c58f65fcebabc3d3d0c
ATeal <arteal89@gmail.com>
2019-10-17 12:57:51 -0400

    See if this a little less complex

--- modules/claims_api/app/controllers/claims_api/concerns/page_size_validation.rb
+++ modules/claims_api/app/controllers/claims_api/concerns/page_size_validation.rb
@@ -5,13 +5,18 @@ module ClaimsApi
     extend ActiveSupport::Concern
     included do
       def validate_documents_page_size
-        errors = documents.reduce([]) { |cur, doc| valid_page_size?(doc) ? cur : cur << json_api_page_size_error(doc) }
-        render json: { errors: errors }, status: 422 unless errors.empty?
+        render json: { errors: document_errors }, status: 422 unless document_errors.empty?
       end
 
       def valid_page_size?(file)
         size_in_inches = PdfInfo::Metadata.read(file.path).page_size_inches
         size_in_inches[:height] <= 11 && size_in_inches[:width] <= 11
+      end
+
+      def document_errors
+        @document_errors ||= documents.reduce([]) do |cur, doc|
+          valid_page_size?(doc) ? cur : cur << json_api_page_size_error(doc)
+        end
       end
 
       def json_api_page_size_error(document)
