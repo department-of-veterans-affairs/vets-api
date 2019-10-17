860e3392e872bee94f309742b72c385f854f5c25
Lihan Li <frankieteardrop@gmail.com>
2019-10-17 03:29:29 -0700

    lint

--- spec/lib/vet360/address_validation/service_spec.rb
+++ spec/lib/vet360/address_validation/service_spec.rb
@@ -48,7 +48,9 @@ describe Vet360::AddressValidation::Service do
                     'state_code' => 'NY',
                     'zip_code' => '11249',
                     'zip_code_suffix' => '3939' },
-                  'address_meta_data' => { 'confidence_score' => 100.0, 'address_type' => 'Domestic', 'delivery_point_validation' => 'UNDELIVERABLE' } },
+                  'address_meta_data' => { 'confidence_score' => 100.0,
+                                           'address_type' => 'Domestic',
+                                           'delivery_point_validation' => 'UNDELIVERABLE' } },
                 { 'address' =>
                   { 'address_line1' => '37 S 1st St',
                     'address_type' => 'DOMESTIC',
@@ -61,7 +63,8 @@ describe Vet360::AddressValidation::Service do
                     'zip_code' => '11249',
                     'zip_code_suffix' => '4101' },
                   'address_meta_data' =>
-                  { 'confidence_score' => 100.0, 'address_type' => 'Domestic', 'delivery_point_validation' => 'CONFIRMED', 'residential_delivery_indicator' => 'MIXED' } }],
+                  { 'confidence_score' => 100.0, 'address_type' => 'Domestic',
+                    'delivery_point_validation' => 'CONFIRMED', 'residential_delivery_indicator' => 'MIXED' } }],
               'validation_key' => 609_319_007
             )
           end
@@ -77,7 +80,9 @@ describe Vet360::AddressValidation::Service do
           'vet360/address_validation/validate_no_match',
           VCR::MATCH_EVERYTHING
         ) do
-          expect { described_class.new.validate(invalid_address) }.to raise_error(Common::Exceptions::BackendServiceException)
+          expect { described_class.new.validate(invalid_address) }.to raise_error(
+            Common::Exceptions::BackendServiceException
+          )
         end
       end
     end
@@ -92,12 +97,15 @@ describe Vet360::AddressValidation::Service do
             'address' =>
              { 'county' => { 'name' => 'Kings', 'county_fips_code' => '36047' },
                'state_province' => { 'name' => 'New York', 'code' => 'NY' },
-               'country' => { 'name' => 'USA', 'code' => 'USA', 'fips_code' => 'US', 'iso2_code' => 'US', 'iso3_code' => 'USA' },
+               'country' => { 'name' => 'USA', 'code' => 'USA',
+                              'fips_code' => 'US', 'iso2_code' => 'US', 'iso3_code' => 'USA' },
                'address_line1' => '37 S 1st St',
                'city' => 'Brooklyn',
                'zip_code5' => '11249',
                'zip_code4' => '4101' },
-            'geocode' => { 'calc_date' => '2019-10-15T08:05:01+00:00', 'location_precision' => 31.0, 'latitude' => 40.715383, 'longitude' => -73.965421 },
+            'geocode' => { 'calc_date' => '2019-10-15T08:05:01+00:00',
+                           'location_precision' => 31.0, 'latitude' => 40.715383,
+                           'longitude' => -73.965421 },
             'address_meta_data' =>
              { 'confidence_score' => 97.0,
                'address_type' => 'Domestic',
@@ -117,7 +125,9 @@ describe Vet360::AddressValidation::Service do
           'vet360/address_validation/candidate_no_match',
           VCR::MATCH_EVERYTHING
         ) do
-          expect { described_class.new.candidate(invalid_address) }.to raise_error(Common::Exceptions::BackendServiceException)
+          expect { described_class.new.candidate(invalid_address) }.to raise_error(
+            Common::Exceptions::BackendServiceException
+          )
         end
       end
     end
@@ -135,23 +145,30 @@ describe Vet360::AddressValidation::Service do
                [{ 'address' =>
                   { 'county' => { 'name' => 'Kings', 'county_fips_code' => '36047' },
                     'state_province' => { 'name' => 'New York', 'code' => 'NY' },
-                    'country' => { 'name' => 'USA', 'code' => 'USA', 'fips_code' => 'US', 'iso2_code' => 'US', 'iso3_code' => 'USA' },
+                    'country' => { 'name' => 'USA', 'code' => 'USA',
+                                   'fips_code' => 'US', 'iso2_code' => 'US', 'iso3_code' => 'USA' },
                     'address_line1' => '37 N 1st St',
                     'city' => 'Brooklyn',
                     'zip_code5' => '11249',
                     'zip_code4' => '3939' },
-                  'geocode' => { 'calc_date' => '2019-10-14T11:21:44+00:00', 'location_precision' => 31.0, 'latitude' => 40.717018, 'longitude' => -73.964935 },
+                  'geocode' => { 'calc_date' => '2019-10-14T11:21:44+00:00', 'location_precision' => 31.0,
+                                 'latitude' => 40.717018, 'longitude' => -73.964935 },
                   'address_meta_data' =>
-                  { 'confidence_score' => 100.0, 'address_type' => 'Domestic', 'delivery_point_validation' => 'UNDELIVERABLE', 'validation_key' => -1_384_531_381 } },
+                  { 'confidence_score' => 100.0, 'address_type' => 'Domestic',
+                    'delivery_point_validation' => 'UNDELIVERABLE',
+                    'validation_key' => -1_384_531_381 } },
                 { 'address' =>
                   { 'county' => { 'name' => 'Kings', 'county_fips_code' => '36047' },
                     'state_province' => { 'name' => 'New York', 'code' => 'NY' },
-                    'country' => { 'name' => 'USA', 'code' => 'USA', 'fips_code' => 'US', 'iso2_code' => 'US', 'iso3_code' => 'USA' },
+                    'country' => { 'name' => 'USA', 'code' => 'USA',
+                                   'fips_code' => 'US', 'iso2_code' => 'US', 'iso3_code' => 'USA' },
                     'address_line1' => '37 S 1st St',
                     'city' => 'Brooklyn',
                     'zip_code5' => '11249',
                     'zip_code4' => '4101' },
-                  'geocode' => { 'calc_date' => '2019-10-14T11:21:44+00:00', 'location_precision' => 31.0, 'latitude' => 40.715383, 'longitude' => -73.965421 },
+                  'geocode' => { 'calc_date' => '2019-10-14T11:21:44+00:00',
+                                 'location_precision' => 31.0, 'latitude' => 40.715383,
+                                 'longitude' => -73.965421 },
                   'address_meta_data' =>
                   { 'confidence_score' => 100.0,
                     'address_type' => 'Domestic',
@@ -174,12 +191,14 @@ describe Vet360::AddressValidation::Service do
              [{ 'address' =>
                 { 'county' => { 'name' => 'Baltimore', 'county_fips_code' => '24005' },
                   'state_province' => { 'name' => 'Maryland', 'code' => 'MD' },
-                  'country' => { 'name' => 'USA', 'code' => 'USA', 'fips_code' => 'US', 'iso2_code' => 'US', 'iso3_code' => 'USA' },
+                  'country' => { 'name' => 'USA', 'code' => 'USA',
+                                 'fips_code' => 'US', 'iso2_code' => 'US', 'iso3_code' => 'USA' },
                   'address_line1' => '5 Stoddard Ct',
                   'city' => 'Sparks Glencoe',
                   'zip_code5' => '21152',
                   'zip_code4' => '9367' },
-                'geocode' => { 'calc_date' => '2019-10-10T10:40:08+00:00', 'location_precision' => 31.0, 'latitude' => 39.532499, 'longitude' => -76.647183 },
+                'geocode' => { 'calc_date' => '2019-10-10T10:40:08+00:00', 'location_precision' => 31.0,
+                               'latitude' => 39.532499, 'longitude' => -76.647183 },
                 'address_meta_data' =>
                 { 'confidence_score' => 100.0,
                   'address_type' => 'Domestic',
