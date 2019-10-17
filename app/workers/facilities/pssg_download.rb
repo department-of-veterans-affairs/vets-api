c0671577a0e961dd223ce69123038437f3eacf33
Will Huang <52384927+whuang85@users.noreply.github.com>
2019-10-16 19:04:25 -0500

    create pssg download job

---
+++ app/workers/facilities/pssg_download.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'rgeo/geo_json'
+
+module Facilities
+  class PSSGDownload
+    include Sidekiq::Worker
+    include SentryLogging
+
+    def perform
+      @drivetime_band_client = Facilities::DrivetimeBandClient.new
+      download_data
+    end
+
+    private
+
+    def create_and_save_drive_time_data(drive_time_data)
+      attributes = drive_time_data.dig('attributes')
+
+      vha_id = extract_vha_id(attributes)
+      facility = BaseFacility.find_facility_by_id(vha_id)
+      return if facility.nil?
+
+      drive_time_band = facility.drivetime_bands.find_or_initialize_by(vha_facility_id: extract_name(attributes))
+      drive_time_band.min = attributes.dig('FromBreak')
+      drive_time_band.max = attributes.dig('ToBreak')
+      drive_time_band.name = attributes.dig('Name')
+      drive_time_band.polygon = extract_polygon(drive_time_data)
+      facility.save
+    end
+
+    def extract_name(attributes)
+      name = attributes.dig('Name')
+      name.partition(':').first.strip!
+    end
+
+    def extract_vha_id(attributes)
+      'vha_' + extract_name(attributes)
+    end
+
+    def extract_polygon(drive_time_data)
+      rings = drive_time_data.dig('geometry', 'rings')
+      geojson = "{\"type\":\"Polygon\",\"coordinates\":#{rings}}"
+      RGeo::GeoJSON.decode(geojson)
+    end
+
+    def download_data
+      offset = 0
+      loop do
+        response = @drivetime_band_client.get_drivetime_bands(offset, 1)
+        break if response.nil?
+
+        response.each(&method(:create_and_save_drive_time_data))
+        offset += 1
+      end
+    end
+  end
+end
---
+++ spec/jobs/facilities/pssg_download_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Facilities::PSSGDownload, type: :job do
+  let(:drive_time_data) do
+    fixture_file_name = "#{::Rails.root}/spec/fixtures/pssg/drive_time_data.json"
+    File.open(fixture_file_name, 'rb') do |f|
+      JSON.parse(f.read)
+    end
+  end
+
+  let(:pssg_client_stub) { instance_double('Facilities::DrivetimeBandClient') }
+
+  before do
+    allow(Facilities::DrivetimeBandClient).to receive(:new, &method(:pssg_client_stub))
+    allow(pssg_client_stub).to receive(:get_drivetime_bands).and_return(nil)
+  end
+
+  describe 'matching facility' do
+    before do
+      create :vha_648A4
+    end
+
+    it 'populates facility with drive time data' do
+      allow(pssg_client_stub).to receive(:get_drivetime_bands).with(0, 1).and_return(drive_time_data)
+      subject.perform
+      expect(BaseFacility.find_facility_by_id('vha_648A4').drivetime_bands).not_to be_nil
+      expect(BaseFacility.find_facility_by_id('vha_648A4').drivetime_bands.size).to be(1)
+      expect(BaseFacility.find_facility_by_id('vha_648A4').drivetime_bands[0].name).to eql('648A4 : 0 - 10')
+      expect(DrivetimeBand.find_by(vha_facility_id: '648A4').name).to eql('648A4 : 0 - 10')
+    end
+  end
+
+  describe 'no matching facility' do
+    it 'populates facility with drive time data' do
+      allow(pssg_client_stub).to receive(:get_drivetime_bands).with(0, 1).and_return(drive_time_data)
+      subject.perform
+      expect(BaseFacility.find_facility_by_id('vha_648A4')).to be_nil
+      expect(DrivetimeBand.find_by(name: '648A4 : 0 - 10')).to be_nil
+    end
+  end
+end
