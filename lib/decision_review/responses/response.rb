9081cad0aeb6d32ea3134fecf36d5436ac89ae3a
Silvio Luthi <silvio@adhocteam.us>
2019-10-16 18:51:25 -0700

    Add initial service integration.

---
+++ lib/decision_review/responses/response.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'common/client/concerns/service_status'
+require 'common/models/base'
+
+module DecisionReview
+  module Responses
+    ##
+    # Model for DecisonReview responses. Body is passed straight through from the service
+    # with a validation check that it matches the expected schema.
+    #
+    # @!attribute body
+    #   @return [String] Validated response body.
+    # @!attribute status
+    #   @return [Integer] The HTTP status code.
+    #
+    class Response < Common::Base
+      attribute :body, String
+      attribute :status, Integer
+
+      def initialize(status, body, schema_name)
+        self.body = body if json_format_is_valid?(body, schema_name)
+        self.status = status
+      end
+
+      def ok?
+        status == 200
+      end
+
+      def accepted?
+        status == 202
+      end
+
+      private
+
+      def json_format_is_valid?(body, schema_name)
+        schema_path = Rails.root.join('lib', 'decision_review', 'schemas', "#{schema_name}.json").to_s
+        JSON::Validator.validate!(schema_path, body, strict: false)
+      end
+    end
+  end
+end
