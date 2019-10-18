007c608e14febaaa0a56e62bbcd22725e7abeb5a
camerontesterman <cameronbtesterman@gmail.com>
2019-10-17 20:59:35 -0500

    more swagger updates

--- app/swagger/requests/appeals.rb
+++ app/swagger/requests/appeals.rb
@@ -63,7 +63,7 @@ module Swagger
       swagger_path '/v0/appeals/higher_level_reviews/{uuid}' do
         operation :get do
 
-          key :description, 'This endpoint returns the details of a specific Higher Level Review. This endpoint is also referred to as the Show endpoint'
+          key :description, 'This endpoint returns the details of a specific Higher Level Review'
           key :operationId, 'showHigherLevelReview'
           key :tags, %w[higher_level_reviews]
 
@@ -106,26 +106,6 @@ module Swagger
       end
 
       swagger_path 'v0/appeals/higher_level_review/intake_status/{intake_id}' do
-      end
-
-      swagger_path 'v0/appeals/higher_level_reviews' do
-        operation :post do
-
-          key :description, 'Creates a higher level review submission'
-          key :operationId, 'postHigherLevelReview'
-          key :tags, %w[higher_level_reviews]
-
-          parameter do
-            key :name, :higher_level_review
-            key :in, :body
-            key :description, 'Creates a higher level review submission'
-            key :required, true
-          end
-
-          response 202 do
-          end
-
-        end
       end
     end
   end
--- app/swagger/schemas/appeals.rb
+++ app/swagger/schemas/appeals.rb
@@ -11,8 +11,14 @@ module Swagger
       end
 
       swagger_schema :HigherLevelReview do
-        key :required, [:data, :included]
+        key :required, %i[data included]
         property :data, type: :object do
+          key :required, %i[
+            id
+            type
+            attributes
+            relationships
+          ]
           property :id do
             key :type, :string
             key :format, :uuid
@@ -20,10 +26,23 @@ module Swagger
           end
           property :type do
             key :type, :string
-            key :enum, ['HigherLevelReview']
+            key :enum, %w[HigherLevelReview]
             key :description, 'Will be "Higher Level Review"'
           end
           property :attributes, type: :object do
+            key :required, %i[
+              status
+              aoj
+              program_area
+              benefit_type
+              description
+              receipt_date
+              informal_conference
+              same_office
+              legacy_opt_in_approved
+              alerts
+              events
+            ]
             property :status, type: :string
             property :aoj, type: :string, nullable: true
             property :program_area, type: :string
@@ -67,13 +86,16 @@ module Swagger
             key :'$ref', :HigherLevelReviewRelationships
           end
         end
+        property :included do
+          key :'$ref', :HigherLevelReviewIncluded
+        end
       end
 
       swagger_schema :HigherLevelReviewAlerts do
         key :type, :array
         items do
           key :type, :object
-          property :type, type: :string, enum: ['AmaPostDecision']
+          property :type, type: :string, enum: %w[AmaPostDecision]
           property :details do
             key :type, :object
             property :decision_date, type: :string, nullable: true
@@ -93,9 +115,68 @@ module Swagger
       end
 
       swagger_schema :HigherLevelReviewRelationships do
+        key :type, :object
+        property :veteran, type: :object do
+          property :data, type: :object do
+            property :type, type: :string, enum: %w[Veteran]
+            property :id, type: :string
+          end
+        end
+        property :claimaint, type: :object do
+          property :data, type: :object do
+            property :type, type: :string, enum: %w[Claimant]
+          end
+        end
+        property :requestIssues, type: :object do
+          property :data, type: :array do
+            items do
+              key :type, :object
+              property :type, type: :string, enum: %w[RequestIssue]
+              property :id, type: :integer
+            end
+          end
+        end
+        property :decisionIssues, type: :object do
+          property :data, type: :array do
+            items do
+              key :type, :object
+              property :type, type: :string, enum: %w[DecisionIssue]
+              property :id, type: :integer
+            end
+          end
+        end
       end
 
       swagger_schema :HigherLevelReviewEvents do
+        key :type, :array
+        items do
+          key :type, :object
+          property :type do
+            key :type, :string
+            key :enum, %w[
+              hlr_request_event
+              hlr_decision_event
+              hlr_dta_error_event
+              dta_decision_event
+              hlr_other_close_event
+            ]
+          end
+          property :date, type: :string, format: :date
+        end
+      end
+
+      swagger_schema :HigherLevelReviewIncluded do
+        key :type, :array
+        items do
+          property do
+            key :type, :object
+            property :type, type: :string, enum: %w[DecisionIssue]
+            property :id, type: :integer
+            property :attributes, type: :object do
+              property :approxDecisionDate, type: :string, format: :date, nullable: true
+            end
+          end
+        end
       end
     end
   end
