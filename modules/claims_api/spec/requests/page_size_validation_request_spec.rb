80ce99f37de6d40ca9316024de27f98f1b0ff6a9
ATeal <arteal89@gmail.com>
2019-10-18 12:28:35 -0400

    Add success case for new concern

--- modules/claims_api/spec/requests/page_size_validation_request_spec.rb
+++ modules/claims_api/spec/requests/page_size_validation_request_spec.rb
@@ -16,14 +16,29 @@ RSpec.describe 'Page Validation Requests', type: :request do
         'X-VA-Gender': 'M' }
     end
     let(:auto_claim) { create(:auto_established_claim) }
-    let(:params) do
-      { 'attachment': Rack::Test::UploadedFile.new("#{::Rails.root}/modules/claims_api/spec/fixtures/18x22.pdf") }
+
+    context 'with a large pdf' do
+      let(:params) do
+        { 'attachment': Rack::Test::UploadedFile.new("#{::Rails.root}/modules/claims_api/spec/fixtures/18x22.pdf") }
+      end
+
+      it 'returns an error if the file is too large' do
+        allow_any_instance_of(ClaimsApi::SupportingDocumentUploader).to receive(:store!)
+        post "/services/claims/v0/forms/526/#{auto_claim.id}/attachments", params: params, headers: headers
+        expect(response.status).to eq(422)
+      end
     end
 
-    it 'returns an error if the file is too large' do
-      allow_any_instance_of(ClaimsApi::SupportingDocumentUploader).to receive(:store!)
-      post "/services/claims/v0/forms/526/#{auto_claim.id}/attachments", params: params, headers: headers
-      expect(response.status).to eq(422)
+    context 'with a normal pdf' do
+      let(:params) do
+        { 'attachment': Rack::Test::UploadedFile.new("#{::Rails.root}/modules/claims_api/spec/fixtures/extras.pdf") }
+      end
+
+      it 'returns an success' do
+        allow_any_instance_of(ClaimsApi::SupportingDocumentUploader).to receive(:store!)
+        post "/services/claims/v0/forms/526/#{auto_claim.id}/attachments", params: params, headers: headers
+        expect(response.status).to eq(200)
+      end
     end
   end
 end
