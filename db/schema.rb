64f8f627cb9859ff315810992622bf464aebee24
camerontesterman <cameronbtesterman@gmail.com>
2019-10-21 15:43:38 -0500

    rollback schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_213602) do
+ActiveRecord::Schema.define(version: 2019_10_07_182427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
