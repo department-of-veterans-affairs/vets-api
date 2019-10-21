26db2c9799e0e4035a79f82c7cfe68daf36c69a4
ATeal <arteal89@gmail.com>
2019-10-21 12:21:42 -0400

    use mocks / doubles for test

--- modules/claims_api/spec/workers/poa_updater_spec.rb
+++ modules/claims_api/spec/workers/poa_updater_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 require 'sidekiq/testing'
+require 'lighthouse_bgs'
 
 Sidekiq::Testing.fake!
 
@@ -27,6 +28,11 @@ RSpec.describe ClaimsApi::PoaUpdater, type: :job do
   end
 
   it "updates the form's status" do
+    vet_record_stub = LighthouseBGS::Services.new.vet_record
+    allow(vet_record_stub).to receive(:update_birls_record).and_return('return_code' => 'BMOD0001')
+    service_double = instance_double('LighthouseBGS::Services')
+    expect(service_double).to receive(:vet_record).and_return(vet_record_stub)
+    expect(LighthouseBGS::Services).to receive(:new).and_return(service_double)
     subject.new.perform(poa.id)
     poa.reload
     expect(poa.status).to eq('updated')
