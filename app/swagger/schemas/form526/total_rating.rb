972138679f83beaeabde263c16283966f32b6853
Kathleen Crawford <kcrawford@governmentcio.com>
2019-10-21 17:04:38 -0700

    total rating updates

--- app/swagger/schemas/form526/total_rating.rb
+++ app/swagger/schemas/form526/total_rating.rb
@@ -6,24 +6,6 @@ module Swagger
       class TotalRating
         include Swagger::Blocks
 
-        swagger_schema :TotalRating3 do
-          key :required, [:data]
-
-          property :data, type: :object do
-            property :attributes, type: :object do
-              key :required, [:rated_disabilities]
-              property :rated_disabilities do
-                items do
-                  key :type, :object
-                  key :'$ref', :RatedDisability
-                end
-              end
-            end
-            property :id, type: :string, example: nil
-            property :type, type: :string, example: 'evss_disability_compensation_form_total_rating_response'
-          end
-        end
-
         swagger_schema :TotalRating do
           key :required,
               %i[effective_date
