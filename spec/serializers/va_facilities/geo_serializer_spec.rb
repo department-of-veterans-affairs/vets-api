7f2ca61898519392b60e1312289ec7cc23cec650
Katherine Rodriguez <katr400@gmail.com>
2019-10-22 16:03:37 -0400

    Update geo_serializer_spec.rb

--- spec/serializers/va_facilities/geo_serializer_spec.rb
+++ spec/serializers/va_facilities/geo_serializer_spec.rb
@@ -41,6 +41,53 @@ RSpec.describe VaFacilities::GeoSerializer do
       end
       expect(property_object.keys).to eq(expected_property_keys)
     end
+
+    it 'includes id' do
+      property_object = VaFacilities::GeoSerializer.properties(vha_facility)
+
+      expect(property_object['id']).to eq('vha_648A4')
+    end
+
+    it 'includes satisfaction' do
+      property_object = VaFacilities::GeoSerializer.properties(vha_facility)
+
+      expected_satisfaction = {
+        'health' => {
+          'primary_care_urgent' => 0.8,
+          'primary_care_routine' => 0.84
+        },
+        'effective_date' => '2017-08-15'
+      }
+      expect(property_object['satisfaction']).to eq(expected_satisfaction)
+    end
+
+    it 'includes wait times' do
+      property_object = VaFacilities::GeoSerializer.properties(vha_facility)
+
+      expected_wait_times = {
+        'health' => [
+          { 'service' => 'Audiology', 'new' => 35.0, 'established' => 18.0 },
+          { 'service' => 'Optometry', 'new' => 38.0, 'established' => 22.0 },
+          { 'service' => 'Dermatology', 'new' => 4.0, 'established' => nil },
+          { 'service' => 'Ophthalmology', 'new' => 1.0, 'established' => 4.0 },
+          { 'service' => 'PrimaryCare', 'new' => 34.0, 'established' => 5.0 },
+          { 'service' => 'MentalHealth', 'new' => 12.0, 'established' => 3.0 }
+        ],
+        'effective_date' => '2018-02-26'
+      }
+
+      expect(property_object['wait_times']).to eq(expected_wait_times)
+    end
+
+    it 'includes services' do
+      property_object = VaFacilities::GeoSerializer.properties(vha_facility)
+
+      expected_services = {
+        'health' => %w[DentalServices MentalHealthCare PrimaryCare],
+        'last_updated' => '2018-03-15'
+      }
+      expect(property_object['services']).to eq(expected_services)
+    end
   end
 
   context '::to_feature_collection' do
