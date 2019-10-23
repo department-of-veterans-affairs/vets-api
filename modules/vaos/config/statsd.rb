6e4365c6f9abdab72691b6dd78c5965edecf477f
Alastair Dawson <alastair.j.dawson@gmail.com>
2019-10-23 11:13:14 -0700

    rubocop

--- modules/vaos/config/statsd.rb
+++ modules/vaos/config/statsd.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_va_appointments.total", 0)
 StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_va_appointments.fail", 0)
 StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_cc_appointments.total", 0)
