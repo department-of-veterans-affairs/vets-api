fc2352cb5629e4cafc98e82afd066793b4bb6581
Alastair Dawson <alastair.j.dawson@gmail.com>
2019-10-23 10:32:41 -0700

    add vaos statsd initializer

---
+++ modules/vaos/config/statsd.rb
@@ -0,0 +1,4 @@
+StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_va_appointments.total", 0)
+StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_va_appointments.fail", 0)
+StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_cc_appointments.total", 0)
+StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_cc_appointments.fail", 0)
