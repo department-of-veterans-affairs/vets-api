982ed82c3c79460958a94e87b6334196a466c1b0
camerontesterman <cameronbtesterman@gmail.com>
2019-10-25 11:12:56 -0500

    rubocop issue fixes

--- app/swagger/schemas/appeals.rb
+++ app/swagger/schemas/appeals.rb
@@ -127,7 +127,7 @@ module Swagger
             property :type, type: :string, enum: %w[Claimant]
           end
         end
-        [:requestIssues, :decisionIssues].each do |prop|
+        %i[requestIssues decisionIssues].each do |prop|
           property prop, type: :object do
             property :data, type: :array do
               items do
@@ -217,7 +217,8 @@ module Swagger
 
       swagger_schema :IntakeStatus do
         key :type, :object
-        key :description, 'An accepted Decision Review still needs to be processed before the Decision Review can be accessed'
+        key :description, 'An accepted Decision Review still needs to be processed '\ 
+                          'before the Decision Review can be accessed'
         property :data, type: :object do
           property :id, type: :string
           property :type, type: :string, description: 'Will be Intake Status'
@@ -230,12 +231,12 @@ module Swagger
                 submitted
                 not_yet_submitted
               ]
-              key :description, 
-                '`not_yet_submitted` - The DecisionReview has not been submitted yet.
-                `submitted` - The DecisionReview is in the queue to be attempted.
-                `attempted` - Processing of the DecisionReview is being attempted.
-                `canceled` - The DecisionReview has been successfully canceled.
-                `processed` - The DecisionReview has been processed and transmitted to the appropriate government agency.'
+              key :description, '`not_yet_submitted` - The DecisionReview has not been submitted yet.
+                                `submitted` - The DecisionReview is in the queue to be attempted.
+                                `attempted` - Processing of the DecisionReview is being attempted.
+                                `canceled` - The DecisionReview has been successfully canceled.
+                                `processed` - The DecisionReview has been processed and transmitted '\
+                                'to the appropriate government agency.'
             end
           end
         end
