56e525fb7fef743b2c9f528aa1e64c88f66ce0cf
Kathleen Crawford <kcrawford@governmentcio.com>
2019-10-28 15:27:37 -0700

    added user_percent_of_disability

--- app/serializers/rating_info_serializer.rb
+++ app/serializers/rating_info_serializer.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class RatingInfoSerializer < ActiveModel::Serializer
-  attribute :rating_info
+  #attribute :rating_info
+  attribute :user_percent_of_disability
 
   def id
     nil
--- app/swagger/schemas/form526/find_rating_info_pid.rb
+++ app/swagger/schemas/form526/find_rating_info_pid.rb
@@ -14,6 +14,7 @@ module Swagger
           property :effective_date, type: :string, example: '2018-03-27T21:00:41.000+0000'
           property :rating_record, type: :integer, example: '40'
           property :some_text, type: :string, example: 'Dummy Data'
+          property :user_percent_of_disability, type: :integer, example: '90'
         end
 
       end
