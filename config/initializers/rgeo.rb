ae3080b13d83b4c30fe49fe32d170a54bd70058c
Katherine Rodriguez <katr400@gmail.com>
2019-10-28 17:29:42 -0400

    Give rubocop a snickers

--- config/initializers/rgeo.rb
+++ config/initializers/rgeo.rb
@@ -4,6 +4,3 @@ RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
   config.register(RGeo::Geographic.spherical_factory(srid: 4326, uses_lenient_assertions: true),
                   geo_type: 'polygon', srid: 4326)
 end
-
-
- 
