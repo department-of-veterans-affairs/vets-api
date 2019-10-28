5e7f8e6f7518fe1114b07821d507f21f141628ed
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2019-10-28 15:01:33 -0400

    update gis metadata client to inherit from facilities metadata client
    removes dup code

--- lib/facilities/gis/metadata_client.rb
+++ lib/facilities/gis/metadata_client.rb
@@ -3,14 +3,9 @@
 require 'common/client/base'
 
 module Facilities
-  class Gis
-      class MetadataClient < Common::Client::Base
+  module Gis
+    class MetadataClient < Facilities::Metadata::Client
       configuration Facilities::Gis::MetadataConfiguration
-
-      def get_metadata(facility_type)
-        resp = perform(:get, "#{facility_type}/FeatureServer/0?", f: 'json')
-        JSON.parse resp.body
-      end
     end
   end
 end
