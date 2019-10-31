764808ae00473da10f6c72030902caabae5b4fd1
Kam Karshenas <kam@adhocteam.us>
2019-10-30 18:41:26 -0700

    DRY up schemas

--- modules/vaos/app/swagger/vaos/schemas/va_appointments.rb
+++ modules/vaos/app/swagger/vaos/schemas/va_appointments.rb
@@ -15,14 +15,7 @@ module VAOS
         end
 
         property :meta, type: :object do
-          key :required, [:pagination]
-          property :pagination, type: :object do
-            key :required, %i[current_page per_page total_pages total_entries]
-            property :current_page, type: :integer, example: 2
-            property :per_page, type: :integer, example: 10
-            property :total_pages, type: :integer, example: 4
-            property :total_entries, type: :integer, example: 39
-          end
+          key: :'ref', :Pagination
         end
       end
 
