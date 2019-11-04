85845d2607ff6ef16e66bef9652a4d6c06b74ee7
Kam Karshenas <kam@saneshark.com>
2019-11-04 12:57:59 -0800

    Vaos appointments controller (#3476)
    * introducing vaos appointment controller logic and authorization policy with feature flipper
    
    * change VAOS mount point to /v0/vaos

--- modules/vaos/app/controllers/vaos/application_controller.rb
+++ modules/vaos/app/controllers/vaos/application_controller.rb
@@ -2,5 +2,16 @@
 
 module VAOS
   class ApplicationController < ::ApplicationController
+    before_action :authorize
+
+    protected
+
+    def authorize
+      raise_access_denied unless current_user.authorize(:vaos, :access?)
+    end
+
+    def raise_access_denied
+      raise Common::Exceptions::Forbidden, detail: 'You do not have access to online scheduling'
+    end
   end
 end
---
+++ modules/vaos/app/swagger/vaos/schemas/cc_appointments.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module VAOS
+  module Schemas
+    class CCAppointments
+      include Swagger::Blocks
+
+      swagger_schema :Appointments do
+        key :required, %i[data meta]
+
+        property :data, type: :array do
+          items do
+            key :id, type: :string
+            key :type, type: :string, enum: %w[cc_appointments]
+            key :attributes, type: :object do
+              key :'$ref', :CCAppointment
+            end
+          end
+        end
+
+        property :meta, type: :object do
+          key :'$ref', :Pagination
+        end
+      end
+
+      swagger_schema :CCAppointment do
+        key :required, %i[id type attributes]
+
+        property :id, type: :string
+        property :type, type: :string, enum: :va_appointments
+        property :attributes, type: :object do
+          property :appointment_request_id, type: :string
+          property :distance_eligible_confirmed, type: :boolean
+          property :name, type: :object do
+            property :first_name, type: :string
+            property :last_name, type: :string
+          end
+          property :provider_practice, type: :string
+          property :provider_phone, type: :string
+          property :address, type: :object do
+            property :street, type: :string
+            property :city, type: :string
+            property :state, type: :string
+            property :zip_code, type: :string
+          end
+          property :instructions_to_veteran, type: :string
+          property :appointment_time, type: :string
+          property :time_zone, type: :string
+        end
+      end
+    end
+  end
+end
