dac9557cd598b17299c4c2ddcf4d0ba6efa3a17d
Kam Karshenas <kam@adhocteam.us>
2019-11-05 10:24:59 -0800

    good stopping point for commit

---
+++ modules/vaos/app/services/vaos/appointment_requests_service.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require_relative '../vaos/concerns/headers'
+
+module VAOS
+  class AppointmentRequestsService < Common::Client::Base
+    include Common::Client::Monitoring
+    include VAOS::Headers
+
+    configuration VAOS::Configuration
+
+    attr_accessor :user
+
+    STATSD_KEY_PREFIX = 'api.vaos'
+
+    def self.for_user(user)
+      rs = VAOS::AppointmentRequestsService.new
+      rs.user = user
+      rs
+    end
+
+    def get_requests
+      with_monitoring do
+        response = perform(:get, get_appointment_requests_url, headers(user))
+
+        {
+          data: response.body[:appointment_requests].map { |request| OpenStruct.new(request) },
+          meta: pagination
+        }
+      end
+    rescue Common::Client::Errors::ClientError => e
+      raise_backend_exception('VAOS_502', self.class, e)
+    end
+
+    private
+
+    def get_appointment_requests_url
+      url = '/var/VeteranAppointmentRequestService/v4/rest/appointment-service'
+      url += "/patient/ICN/#{user.icn}/appointments"
+      url
+    end
+
+    # TODO find out if this api supports pagination and other parameters
+    def pagination
+      {
+        pagination: {
+          current_page: 0,
+          per_page: 0,
+          total_pages: 0,
+          total_entries: 0
+        }
+      }
+    end
+  end
+end
---
+++ modules/vaos/spec/services/appointment_requests_service_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe VAOS::AppointmentRequestsService do
+  subject { described_class.for_user(user) }
+
+  let(:user) { build(:user, :mhv) }
+  let(:rsa_private) { OpenSSL::PKey::RSA.generate 4096 }
+
+  before { allow_any_instance_of(VAOS::JWT).to receive(:rsa_private).and_return(rsa_private) }
+
+  describe '#get_requests' do
+    context 'with 12 va appointments' do
+      it 'returns an array of size 5' do
+        VCR.use_cassette('vaos/appointment_requests/get_requests', match_requests_on: %i[host path method]) do
+          response = subject.get_requests
+          expect(response[:data].size).to eq(40)
+        end
+      end
+    end
+  end
+end
