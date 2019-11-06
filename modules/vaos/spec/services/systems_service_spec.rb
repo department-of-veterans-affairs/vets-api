99ba51d4552cb97c654a07b8238a830b75d84b5f
Alastair Dawson <alastair.j.dawson@gmail.com>
2019-11-06 07:48:00 -0800

    stub user token

--- modules/vaos/spec/services/systems_service_spec.rb
+++ modules/vaos/spec/services/systems_service_spec.rb
@@ -4,30 +4,25 @@ require 'rails_helper'
 
 describe VAOS::SystemsService do
   let(:user) { build(:user, :mhv) }
-  let(:rsa_private) { OpenSSL::PKey::RSA.generate 4096 }
 
-  before { allow(File).to receive(:read).and_return(rsa_private) }
+  before { allow_any_instance_of(VAOS::UserService).to receive(:session).and_return('stubbed_token') }
 
   describe '#get_systems' do
     context 'with 10 system identifiers' do
       it 'returns an array of size 10' do
-        VCR.use_cassette('vaos/users/post_session') do
-          VCR.use_cassette('vaos/systems/get_systems', match_requests_on: %i[host path method]) do
-            response = subject.get_systems(user)
-            expect(response.size).to eq(10)
-          end
+        VCR.use_cassette('vaos/systems/get_systems', match_requests_on: %i[host path method]) do
+          response = subject.get_systems(user)
+          expect(response.size).to eq(10)
         end
       end
     end
 
     context 'when the upstream server returns a 500' do
       it 'raises a backend exception' do
-        VCR.use_cassette('vaos/users/post_session') do
-          VCR.use_cassette('vaos/systems/get_systems_500', match_requests_on: %i[host path method]) do
-            expect { subject.get_systems(user) }.to raise_error(
-              Common::Exceptions::BackendServiceException
-            )
-          end
+        VCR.use_cassette('vaos/systems/get_systems_500', match_requests_on: %i[host path method]) do
+          expect { subject.get_systems(user) }.to raise_error(
+            Common::Exceptions::BackendServiceException
+          )
         end
       end
     end
