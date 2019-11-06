11b763a2edbe4ff0608aac2cc433aeee1837dbb2
Alastair Dawson <alastair.j.dawson@gmail.com>
2019-11-06 13:48:24 -0800

    check that user service reponse is not blank

--- modules/vaos/spec/services/user_service_spec.rb
+++ modules/vaos/spec/services/user_service_spec.rb
@@ -7,6 +7,7 @@ describe VAOS::UserService do
 
   describe '#session' do
     let(:rsa_private) { OpenSSL::PKey::RSA.generate 4096 }
+    let(:token) { 'abc123' }
 
     before { allow(File).to receive(:read).and_return(rsa_private) }
 
@@ -19,8 +20,6 @@ describe VAOS::UserService do
       end
 
       context 'with a cached session' do
-        let(:token) { 'abc123' }
-
         it 'does not call the VAOS user service' do
           VCR.use_cassette('vaos/users/post_session') do
             VAOS::SessionStore.new(user_uuid: user.uuid, token: token).save
@@ -31,10 +30,17 @@ describe VAOS::UserService do
       end
 
       context 'when there is no saved session token' do
-        it 'makes a call out to the the VAOS user service' do
+        it 'makes a call out to the the VAOS user service once' do
+          response = double('response', body: token)
           VCR.use_cassette('vaos/users/post_session') do
-            expect(subject).to receive(:perform).once
+            expect(subject).to receive(:perform).once.and_return(response)
             subject.session(user)
+          end
+        end
+
+        it 'returns a token' do
+          VCR.use_cassette('vaos/users/post_session') do
+            expect(subject.session(user)).to be_a(String)
           end
         end
       end
@@ -55,6 +61,16 @@ describe VAOS::UserService do
         VCR.use_cassette('vaos/users/post_session_403') do
           expect { subject.session(user) }.to raise_error(
             Common::Client::Errors::ClientError, 'the server responded with status 403'
+          )
+        end
+      end
+    end
+
+    context 'with a blank response' do
+      it 'raises a client error' do
+        VCR.use_cassette('vaos/users/post_session_blank_body') do
+          expect { subject.session(user) }.to raise_error(
+            Common::Exceptions::BackendServiceException
           )
         end
       end
