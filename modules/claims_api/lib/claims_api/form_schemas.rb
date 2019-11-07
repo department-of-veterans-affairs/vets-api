f2cab6f1b95a17bb441bc36fafbb2450ff79e9e4
ATeal <arteal89@gmail.com>
2019-11-07 09:56:04 -0500

    Update to new json schema gem restructure json api error to fit new gem update all schemas to draft7 and refactor/reuse

--- modules/claims_api/lib/claims_api/form_schemas.rb
+++ modules/claims_api/lib/claims_api/form_schemas.rb
@@ -17,32 +17,10 @@ module ClaimsApi
       return_val
     end.call
 
-    def self.date_validator
-      lambda do |value|
-        unless value.match?(/^(\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$/)
-          raise JSON::Schema::CustomFormatError, 'must be in format YYYY-MM-DD'
-        end
-      end
-    end
-
-    def self.address_line_validator
-      lambda do |value|
-        unless value.match?(/^([-a-zA-Z0-9'.,&#]([-a-zA-Z0-9'.,&# ])?)+$/)
-          raise JSON::Schema::CustomFormatError, 'contains invalid characters'
-        end
-      end
-    end
-
-    def self.register_validators
-      JSON::Validator.register_format_validator('date-pattern', date_validator)
-      JSON::Validator.register_format_validator('address-line-pattern', address_line_validator)
-    end
-
     def self.validate!(form, payload)
-      register_validators
-      errors = JSON::Validator.fully_validate(SCHEMAS[form], payload, errors_as_objects: true)
+      schema_validator = JSONSchemer.schema(SCHEMAS[form])
+      errors = schema_validator.validate(payload).to_a
       raise ClaimsApi::JsonApiMissingAttribute, errors unless errors.empty?
-
       true
     end
   end
