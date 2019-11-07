b44c810d74f13127856e9b0bd1af09f9bb465934
Kam Karshenas <kam@adhocteam.us>
2019-11-07 15:50:53 -0800

    add special 403 case for MVI not available to better aid debugging

--- modules/vaos/app/controllers/vaos/application_controller.rb
+++ modules/vaos/app/controllers/vaos/application_controller.rb
@@ -8,10 +8,15 @@ module VAOS
 
     def authorize
       raise_access_denied unless current_user.authorize(:vaos, :access?)
+      raise_access_denied_no_icn if current_user.icn.blank?
     end
 
     def raise_access_denied
       raise Common::Exceptions::Forbidden, detail: 'You do not have access to online scheduling'
+    end
+
+    def raise_access_denied_no_icn
+      raise Common::Exceptions::Forbidden, detail: 'No patient ICN found'
     end
   end
 end
