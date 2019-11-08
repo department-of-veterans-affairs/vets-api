e06761a7cff4daedb25c3f37ca9dbb45c67733cd
Kam Karshenas <kam@saneshark.com>
2019-11-07 16:15:00 -0800

    Vaos requests (#3514)
    * good stopping point for commit
    
    * adding schema specs
    
    * fix the specs
    
    * rubocop related fixes
    
    * fix
    
    * fix
    
    * fixes up the specs

---
+++ modules/vaos/app/services/vaos/appointment_requests_service.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require_relative '../vaos/concerns/headers'
+
+module VAOS
+  class AppointmentRequestsService < Common::Client::Base
+    include Common::Client::Monitoring
+    include VAOS::Headers
+
+    configuration VAOS::Configuration
+
+    attr_accessor :user
+
+    STATSD_KEY_PREFIX = 'api.vaos'
+
+    def self.for_user(user)
+      rs = VAOS::AppointmentRequestsService.new
+      rs.user = user
+      rs
+    end
+
+    def get_requests(start_date = nil, end_date = nil)
+      with_monitoring do
+        response = perform(:get, get_appointment_requests_url(start_date, end_date), headers(user))
+
+        {
+          data: response.body[:appointment_requests].map { |request| OpenStruct.new(request) },
+          meta: pagination
+        }
+      end
+    rescue Common::Client::Errors::ClientError => e
+      raise_backend_exception('VAOS_502', self.class, e)
+    end
+
+    private
+
+    def get_appointment_requests_url(start_date = nil, end_date = nil)
+      url = '/var/VeteranAppointmentRequestService/v4/rest/appointment-service'
+      url += "/patient/ICN/#{user.icn}/appointments"
+      url += get_date_url(start_date, end_date).empty? ? '' : "?#{get_date_url(start_date, end_date).to_query}"
+      url
+    end
+
+    def get_date_url(start_date, end_date)
+      { startDate: date_format(start_date), endDate: date_format(end_date) }
+    end
+
+    def date_format(date)
+      date&.strftime('%Y-%m-%dT%TZ')
+    end
+
+    # TODO: find out if this api supports pagination and other parameters
+    def pagination
+      {
+        pagination: {
+          current_page: 0,
+          per_page: 0,
+          total_pages: 0,
+          total_entries: 0
+        }
+      }
+    end
+  end
+end
