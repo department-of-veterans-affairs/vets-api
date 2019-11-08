d6abcbfe7cfcbf5d9e4d709da1cab4d8927d058e
Johnny Holton <johnny@holton.co>
2019-11-08 15:00:52 -0500

    add Flipper request spec that tests presence of route constraints

--- spec/request/flipper_request_spec.rb
+++ spec/request/flipper_request_spec.rb
@@ -3,37 +3,51 @@
 require 'rails_helper'
 
 RSpec.describe 'Flipper UI', type: :request do
-  before(:all) do
+  def bypass_flipper_authenticity_token
     Rails.application.routes.draw do
       mount Flipper::UI.app(
         Flipper.instance,
         rack_protection: { except: :authenticity_token }
       ) => '/flipper', constraints: Flipper::AdminUserConstraint.new
     end
+    yield
+    Rails.application.reload_routes!
   end
 
-  after(:all) { Rails.application.reload_routes! }
-
-  context 'when authenticated with LOA3 admin user' do
-    let(:user) { build(:user, :loa3) }
-
+  context 'with authenticated admin user' do
     before do
       sign_in_as(user)
       Settings.flipper.admin_user_emails << user.email
-    end
-
-    it 'Displays list of features' do
-      get '/flipper/features', params: nil
-      expect(response.body).to include('flipper/features')
-      assert_response :success
-    end
-
-    it 'Allows user to toggle feature' do
       Flipper.enable(:test_feature)
-      expect(Flipper.enabled?(:test_feature)).to be true
-      post '/flipper/features/test_feature/boolean', params: nil
-      assert_response :found
-      expect(Flipper.enabled?(:test_feature)).to be false
+    end
+
+    context 'with LOA1 access' do
+      let(:user) { build(:user) }
+
+      it 'does not allow feature toggling' do
+        post '/flipper/features/test_feature/boolean', params: nil
+        assert_response :not_found
+        expect(JSON.parse(response.body)['errors'][0]['detail']).to include('There are no routes matching your request')
+      end
+    end
+
+    context 'with LOA3 access' do
+      let(:user) { build(:user, :loa3) }
+
+      it 'Displays list of features' do
+        get '/flipper/features', params: nil
+        expect(response.body).to include('flipper/features')
+        assert_response :success
+      end
+
+      it 'Allows user to toggle feature' do
+        bypass_flipper_authenticity_token do
+          expect(Flipper.enabled?(:test_feature)).to be true
+          post '/flipper/features/test_feature/boolean', params: nil
+          assert_response :found
+          expect(Flipper.enabled?(:test_feature)).to be false
+        end
+      end
     end
   end
 
@@ -45,10 +59,12 @@ RSpec.describe 'Flipper UI', type: :request do
     end
 
     it 'does not allow feature toggles' do
-      Flipper.enable(:test_feature)
-      expect do
-        post '/flipper/features/test_feature/boolean', params: nil
-      end.to raise_error(ActionController::RoutingError)
+      bypass_flipper_authenticity_token do
+        Flipper.enable(:test_feature)
+        expect do
+          post '/flipper/features/test_feature/boolean', params: nil
+        end.to raise_error(ActionController::RoutingError)
+      end
     end
   end
 end
