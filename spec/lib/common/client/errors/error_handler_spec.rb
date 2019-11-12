cbde948b44330dcf5e812bb397a8d24fc7e1fff9
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2019-11-12 09:47:35 -0500

    add test coverage for error handler transformed error case statement

--- spec/lib/common/client/errors/error_handler_spec.rb
+++ spec/lib/common/client/errors/error_handler_spec.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'rails_helper'
 
 describe Common::Client::Errors::ErrorHandler do
+  let(:gateway_timeout_error) { StandardError.new }
   let(:internal_server_error) { Common::Exceptions::InternalServerError }
+  let(:pundit_not_auth_error) { Pundit::NotAuthorizedError }
+  let(:parameter_missing_error) { ActionController::ParameterMissing }
+  let(:unknown_format_error) { ActionController::UnknownFormat }
+  let(:not_safe_host_error) { Common::Exceptions::NotASafeHostError }
+  let(:base_error) { Common::Exceptions::BaseError }
+  let(:breakers_outage_error) { Breakers::OutageException }
+  let(:client_error) { Common::Client::Errors::ClientError }
 
   describe '#log_error' do
     subject { described_class.new(internal_server_error) }
@@ -22,13 +30,56 @@ describe Common::Client::Errors::ErrorHandler do
   end
 
   describe '#transformed_error' do
-    subject { described_class.new(StandardError.new).transformed_error }
-
+  
     context 'when given an error without a specific counterpart' do
+      subject { described_class.new(StandardError.new).transformed_error }
       it 'returns the correct default error' do
         expect(subject).to be_a(internal_server_error)
       end
     end
-    # ... repeat ad nauseum for options in a case statement
+
+    context 'when given an error is an pundit not auth error' do
+      subject { described_class.new(pundit_not_auth_error.new).transformed_error }
+      it 'returns the correct default error' do
+        expect(subject).to be_a(Common::Exceptions::Forbidden)
+      end
+    end
+    
+    context 'when given an error is a parameter missing error' do
+      subject { described_class.new(parameter_missing_error.new("missing_param")).transformed_error }
+      it 'returns the correct parameter missing error' do
+        expect(subject).to be_a(Common::Exceptions::ParameterMissing)
+      end
+    end
+    
+    context 'when given an error is an unknown format error' do
+      subject { described_class.new(unknown_format_error.new).transformed_error }
+      it 'returns the correct unknown format error' do
+        expect(subject).to be_a(Common::Exceptions::UnknownFormat)
+      end
+    end
+    
+    context 'when given an error is a base error' do
+      subject { described_class.new(not_safe_host_error.new("1234")).transformed_error }
+      it 'returns the correct error inheriting from base error' do
+        expect(subject).to be_a(not_safe_host_error)
+      end
+    end
+    
+    #TODO cannot figure out what the first param - outage is supposed to be? 
+    context 'when given an error is a breakers outage error' do
+      subject { described_class.new(breakers_outage_error.new(nil, Search::Service)).transformed_error }
+      xit 'returns the correct service outage error' do
+        expect(subject).to be_a(Common::Exceptions::ServiceOutage)
+      end
+    end
+    
+    context 'when given an error is a client error' do
+      subject { described_class.new(client_error.new).transformed_error }
+      it 'returns the correct service outage error' do
+        expect(subject).to be_a(Common::Exceptions::ServiceOutage)
+      end
+    end
+
   end
 end
