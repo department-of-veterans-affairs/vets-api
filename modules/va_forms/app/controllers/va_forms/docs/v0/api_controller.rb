a8050c30f5a52fb2ef417e31828b07c5143ca4df
Charley Stran <charley.stran@gmail.com>
2019-11-15 06:07:34 -0600

    code cleanup and removing unneccessary file

--- modules/va_forms/app/controllers/va_forms/docs/v0/api_controller.rb
+++ modules/va_forms/app/controllers/va_forms/docs/v0/api_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_dependency 'va_forms/v0/swagger_root'
-# require_dependency 'va_forms/forms_v0_controller_swagger'
 
 module VaForms
   module Docs
--- modules/va_forms/app/swagger/va_forms/v0/swagger_root.rb
+++ modules/va_forms/app/swagger/va_forms/v0/swagger_root.rb
@@ -69,4 +69,3 @@ module VaForms
     end
   end
 end
-
