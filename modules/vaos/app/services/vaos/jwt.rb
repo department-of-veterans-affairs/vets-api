90f93d3a5a2d37e371b430cb29eb82becd082057
Kam Karshenas <kam@adhocteam.us>
2019-11-20 21:08:44 -0800

    updating the JWT object

--- modules/vaos/app/services/vaos/jwt.rb
+++ modules/vaos/app/services/vaos/jwt.rb
@@ -18,16 +18,35 @@ module VAOS
         sub: @user.icn,
         idType: 'ICN',
         iss: 'gov.va.vaos',
-        firstName: @user.first_name,
-        lastName: @user.last_name,
+        firstName: @user.first_name, # from SAML assertion unless MHV sign in user (MVI).
+        lastName: @user.last_name, # from SAML assertion unless MHV sign in user (MVI)
         authenticationAuthority: 'gov.va.iam.ssoe.v1',
-        jti: SecureRandom.uuid,
+        jti: SecureRandom.uuid, # TODO: need to capture this in logs as part of a middleware for each action invoked
         nbf: 1.minute.ago.to_i,
         exp: 1.hour.from_now.to_i,
+        patient: {
+          firstName: @user.mvi&.profile&.given_names&.first,
+          lastName: @user.mvi&.profile&.family_name,
+          gender: gender(@user.mvi&.profile&.gender),
+          dob: @user.mvi&.profile&.birth_date,
+          dateOfBirth: @user.mvi&.profile&.birth_date,
+          edipid: @user.mvi&.profile&.edipi,
+          ssn: @user.mvi&.profile&.ssn,
+        },
         version: 1.0,
         userType: 'VETERAN',
         'vamf.auth.roles' => ['veteran']
       }
+    end
+
+    def gender(type)
+      return '' unless type.is_a?(String)
+      case type.upcase[0,1]
+      when 'M'
+        'MALE'
+      when 'F'
+        'FEMALE'
+      end
     end
 
     def rsa_private
