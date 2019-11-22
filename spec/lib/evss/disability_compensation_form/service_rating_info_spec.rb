c0b538b1532cafbec8893392d3a0ef65f80ef2a9
Kathleen Crawford <kcrawford@governmentcio.com>
2019-11-21 18:19:02 -0800

    linting

--- spec/lib/evss/disability_compensation_form/service_rating_info_spec.rb
+++ spec/lib/evss/disability_compensation_form/service_rating_info_spec.rb
@@ -20,24 +20,9 @@ describe EVSS::DisabilityCompensationForm::ServiceRatingInfo do
           expect(response).to be_ok
           expect(response).to be_an EVSS::DisabilityCompensationForm::RatingInfoResponse
           expect(response.user_percent_of_disability).to be_an String
-          expect(response.user_percent_of_disability).to eq ""
+          expect(response.user_percent_of_disability).to eq ''
         end
       end
     end
-=begin
-    context 'with an http timeout' do
-      before do
-        allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(Faraday::TimeoutError)
-      end
-
-      it 'logs an error and raise GatewayTimeout' do
-        expect(StatsD).to receive(:increment).once.with(
-          'api.evss.get_rating_info.fail', tags: ['error:Common::Exceptions::GatewayTimeout']
-        )
-        expect(StatsD).to receive(:increment).once.with('api.evss.get_rating_info.total')
-        expect { subject.get_rating_info }.to raise_error(Common::Exceptions::GatewayTimeout)
-      end
-    end
-=end
   end
 end
