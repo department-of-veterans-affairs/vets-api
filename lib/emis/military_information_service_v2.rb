42cd09d4cf6cac13eb0a54ff4a24dae207d4b783
Nick Fasulo <nrfasulo@gmail.com>
2019-11-25 15:01:58 -0600

    Create eMIS Military Information Service V2

---
+++ lib/emis/military_information_service_v2.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'emis/service'
+require 'emis/military_information_configuration'
+
+module EMIS
+  # HTTP Client for EMIS Military Information Service requests.
+  class MilitaryInformationServiceV2 < Service
+    configuration EMIS::MilitaryInformationConfigurationV2
+
+    create_endpoints(
+      [
+        [:get_military_service_episodes, 'serviceEpisodeRequest']
+      ]
+    )
+
+    protected
+
+    # Custom namespaces used in EMIS SOAP request message
+    # @return [Config::Options] Custom namespaces object
+    def custom_namespaces
+      Settings.emis.military_information.v2.soap_namespaces
+    end
+  end
+end
