6b07c703c8b7b57d4fbc0cc312b54321eb8cf0ec
Travis <travis.hilton@oddball.io>
2019-11-25 09:22:26 -0600

    Adding data type checks to some specs

--- spec/lib/emis/responses/get_deployment_response_spec.rb
+++ spec/lib/emis/responses/get_deployment_response_spec.rb
@@ -25,27 +25,28 @@ describe EMIS::Responses::GetDeploymentResponse do
       end
 
       it 'has the proper segment identifier' do
-        expect(response.items.first.segment_identifier).to eq('1')
+        expect(response.items.first.segment_identifier).to be_a(String).and eq('1')
       end
 
       it 'has the proper begin date' do
-        expect(response.items.first.begin_date).to eq(Date.parse('2008-09-08'))
+        expect(response.items.first.begin_date).to be_a(Date).and eq(Date.parse('2008-09-08'))
       end
 
       it 'has the proper end date' do
-        expect(response.items.first.end_date).to eq(Date.parse('2009-04-18'))
+        expect(response.items.first.end_date).to be_a(Date).and eq(Date.parse('2009-04-18'))
       end
 
       it 'has the proper project code' do
-        expect(response.items.first.project_code).to eq('9GF')
+        expect(response.items.first.project_code).to be_a(String).and eq('9GF') 
       end
 
       it 'has the proper termination reason' do
-        expect(response.items.first.termination_reason).to eq('C')
+        expect(response.items.first.termination_reason).to be_a(String).and eq('C')
+        # expect(response.items.first.termination_reason).to eq('C')
       end
 
       it 'has the proper transaction date' do
-        expect(response.items.first.transaction_date).to eq(Date.parse('2016-05-18'))
+        expect(response.items.first.transaction_date).to be_a(Date).and eq(Date.parse('2016-05-18'))
       end
 
       context 'with a location' do
@@ -56,31 +57,31 @@ describe EMIS::Responses::GetDeploymentResponse do
         end
 
         it 'has the right segment identifier' do
-          expect(location.segment_identifier).to eq('1')
+          expect(location.segment_identifier).to be_a(String).and eq('1')
         end
 
         it 'has the right country' do
-          expect(location.country).to eq('AE')
+          expect(location.country).to be_a(String).and eq('AE')
         end
 
         it 'has the right ISO Alpha3 country' do
-          expect(location.iso_alpha3_country).to eq('ARE')
+          expect(location.iso_alpha3_country).to be_a(String).and eq('ARE')
         end
 
         it 'has the right begin date' do
-          expect(location.begin_date).to eq(Date.parse('2008-09-08'))
+          expect(location.begin_date).to be_a(Date).and eq(Date.parse('2008-09-08'))
         end
 
         it 'has the right end date' do
-          expect(location.end_date).to eq(Date.parse('2009-04-18'))
+          expect(location.end_date).to be_a(Date).and eq(Date.parse('2009-04-18'))
         end
 
         it 'has the right termination reason code' do
-          expect(location.termination_reason_code).to eq('C')
+          expect(location.termination_reason_code).to be_a(String).and eq('C')
         end
 
         it 'has the right transaction date' do
-          expect(location.transaction_date).to eq(Date.parse('2016-05-14'))
+          expect(location.transaction_date).to be_a(Date).and eq(Date.parse('2016-05-14'))
         end
       end
     end
