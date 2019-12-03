a06365297016395237560db609f8c36811231b13
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2019-12-03 11:51:52 -0500

    wrap metadata get call with_monitoring

--- lib/facilities/metadata/client.rb
+++ lib/facilities/metadata/client.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
+
 module Facilities
   module Metadata
     class Client < Common::Client::Base
+      include Common::Client::Monitoring
       configuration Facilities::Metadata::Configuration
 
+      STATSD_KEY_PREFIX = 'api.facilities_metadata'
+
       def get_metadata(facility_type)
-        JSON.parse perform(:get, "#{facility_type}/FeatureServer/0?", f: 'json').body
+        with_monitoring do
+          JSON.parse perform(:get, "#{facility_type}/FeatureServer/0?", f: 'json').body
+        end
       end
     end
   end
