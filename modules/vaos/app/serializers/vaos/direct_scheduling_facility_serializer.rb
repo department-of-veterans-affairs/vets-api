f6fb8495e068b2a06c9f854efa75017f0992f5da
Alastair Dawson <alastair@adhocteam.us>
2019-12-03 12:58:48 -0800

    VAOS system facilities endpoint (#3566)

---
+++ modules/vaos/app/serializers/vaos/direct_scheduling_facility_serializer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'fast_jsonapi'
+
+module VAOS
+  class DirectSchedulingFacilitySerializer < FacilitySerializer
+    set_id :institution_code
+    attributes :request_supported,
+               :direct_scheduling_supported,
+               :express_times,
+               :institution_timezone
+  end
+end
