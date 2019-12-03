a07228b9658e1b829838001927e7fce14d0227cd
Kam Karshenas <kam@adhocteam.us>
2019-12-03 12:32:14 -0800

    rubocop

--- modules/vaos/app/controllers/vaos/appointment_requests_controller.rb
+++ modules/vaos/app/controllers/vaos/appointment_requests_controller.rb
@@ -16,7 +16,7 @@ module VAOS
       :appointment_type, :visit_type, :phone_number, :email, :purpose_of_visit, :other_purpose_of_visit,
       :provider_id, :provider_name, :second_request, :second_request_submitted, :requested_phone_call, :type_of_care_id,
       best_timeto_call: [], facility: %i[name facility_code state city parent_site_code object_type],
-                         appointment_request_detail_code: [], patient: %i[inpatient text_messaging_allowed]
+      appointment_request_detail_code: [], patient: %i[inpatient text_messaging_allowed]
     ].freeze
 
     def index
