711c71442ffa92e2948c1f074bedd179dccb486d
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-12-03 16:26:06 -0500

    Fix current_page instead

--- app/controllers/v0/search_controller.rb
+++ app/controllers/v0/search_controller.rb
@@ -42,10 +42,6 @@ module V0
     # @see https://api.rubyonrails.org/v4.2/classes/ActionView/Helpers/SanitizeHelper.html#method-i-sanitize
     #
     def page
-      page = 1
-      page = search_params['page'] if params[:page]
-      raise Common::Exceptions::InvalidPaginationParams if Integer(page) > 99
-
       sanitize search_params['page']
     end
   end
