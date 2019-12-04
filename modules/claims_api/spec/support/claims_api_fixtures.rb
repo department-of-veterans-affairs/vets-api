25d9e1c8106067984454eec4a3fb1418a5a3dfaa
Charley Stran <charley.stran@gmail.com>
2019-12-04 08:12:08 -0600

    removing undeeded files

--- modules/claims_api/spec/support/claims_api_fixtures.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module ClaimsApi
-  module Fixtures
-    def get_fixture(filename)
-      File.new(File.join(File.expand_path('../fixtures', __dir__), filename))
-    end
-  end
-end
--- modules/claims_api/spec/support/fake_vbms.rb
+++ modules/claims_api/spec/support/fake_vbms.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative './claims_api_fixtures'
-
 class FakeVbms
   attr_accessor :client
 
