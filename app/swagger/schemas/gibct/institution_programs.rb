2b066c4a334caeb533c4f33720941d132f99659a
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-12-04 14:57:06 -0500

    updated Swagger documentation

--- app/swagger/schemas/gibct/institution_programs.rb
+++ app/swagger/schemas/gibct/institution_programs.rb
@@ -11,9 +11,9 @@ module Swagger
 
           property :data, type: :array, minItems: 0, uniqueItems: true do
             items do
-              property :id, type: :integer, example: '1234'
-              property :value, type: :string, example: '1X12345'
-              property :label, type: :string, example: 'COMPUTER SCIENCE'
+              property :id, type: :integer, example: '1234', description: 'Unique program ID'
+              property :value, type: :string, example: '1X12345', description: 'Program institution facility code'
+              property :label, type: :string, example: 'COMPUTER SCIENCE', description: 'Program name'
             end
           end
 
@@ -37,20 +37,20 @@ module Swagger
               property :attributes do
                 key :required, %i[facility_code description]
 
-                property :program_type, type: %i[null string],
-                                        enum: %w[IHL NCD OJT FLGT CORR]
-                property :description, type: :string, example: 'COMPUTER SCIENCE'
-                property :length_in_hours, type: %i[null string], example: '680'
-                property :length_in_weeks, type: %i[null integer], example: 12
-                property :facility_code, type: :string, example: '1X12345'
-                property :institution_name, type: :string, example: 'CODE PLACE'
-                property :city, type: %i[null string], example: 'ANYTOWN'
-                property :state, type: %i[null string], example: 'WA'
-                property :country, type: %i[null string], example: 'USA'
-                property :preferred_provider, type: %i[null boolean], example: false
-                property :tuition_amount, type: %i[null integer], example: 1000
-                property :va_bah, type: %i[null number], example: 220
-                property :dod_bah, type: %i[null integer], example: 200
+            
+                property :program_type, type: :string, example: 'NCD', description: 'The classification of the program, ex: IHL, NCD, OJT'
+                property :description, type: :string, example: 'Computer Science', description: 'Program name'
+                property :length_in_hours, type: :string, example: '680', description: 'Length of program (in hours)'
+                property :length_in_weeks, type: :integer, example: 12, description: 'Length of program (in weeks)'
+                property :facility_code, type: :string, example: '1X12345', description: 'Program institution facility code'
+                property :institution_name, type: :string, example: 'CODE PLACE', description: 'Program institution name'
+                property :city, type: :string, example: 'ANYTOWN', description: 'Program institution physical city location'
+                property :state, type: :string, example: 'WA', description: 'Program institution physical state location'
+                property :country, type: :string, example: 'USA', description: 'Program institution physical country location'
+                property :preferred_provider, type: :boolean, example: 'FALSE', description: 'Program institution preferred provider indicator; a provider that takes on the costs of the veterans education if the requirements are not met'
+                property :tuition_amount, type: :integer, example: 1000, description: 'Program tuition amount'
+                property :va_bah, type: :number, example: 220, description: 'VA Basic Allowance for Housing'
+                property :dod_bah, type: :integer, example: 200, description: 'DOD Basic Allowance for Housing'
               end
             end
           end
@@ -77,16 +77,18 @@ module Swagger
           property :facets, type: :object do
             key :required, %i[type state country]
             property :type, type: :object do
-              property :ihl, type: :integer, example: 0
+              property :ihl, type: :integer, example: 0 
               property :ncd, type: :integer, example: 1
               property :ojt, type: :integer, example: 0
               property :flgt, type: :integer, example: 0
               property :corr, type: :integer, example: 0
             end
+            
 
             property :state, '$ref': :GibctState
             property :country, '$ref': :GibctCountry
           end
+
         end
 
         swagger_schema :GibctInstitutionProgramsSearchLinks do
