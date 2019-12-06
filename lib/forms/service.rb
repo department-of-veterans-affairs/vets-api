8c13e496c07c577be2d1bac4d6c0b197560eb4ce
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-12-06 12:34:26 -0500

    Change STATSD_KEY_PREFIX

--- lib/forms/service.rb
+++ lib/forms/service.rb
@@ -4,7 +4,7 @@ require 'common/client/base'
 
 module Forms
   class Service < Common::Client::Base
-    STATSD_KEY_PREFIX = 'api.va_forms'
+    STATSD_KEY_PREFIX = 'api.forms'
 
     configuration Forms::Configuration
 
