d6fe95ae2adda73d5d9a2d04b89066ea51336d12
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-12-09 14:24:39 -0500

    Add some monitoring (attempt)

--- lib/forms/client.rb
+++ lib/forms/client.rb
@@ -4,9 +4,12 @@ require 'common/client/base'
 
 module Forms
   class Client < Common::Client::Base
-    STATSD_KEY_PREFIX = 'api.forms'
+    include SentryLogging
+    include Common::Client::Monitoring
 
     configuration Forms::Configuration
+
+    STATSD_KEY_PREFIX = 'api.forms'
 
     attr_reader :query
 
@@ -15,8 +18,12 @@ module Forms
     end
 
     def get_all
-      raw_response = perform(:get, 'forms', query_params)
-      Forms::Responses::Response.new(raw_response.status, raw_response.body, 'forms')
+      with_monitoring do
+        raw_response = perform(:get, 'forms', query_params)
+        Forms::Responses::Response.new(raw_response.status, raw_response.body, 'forms')
+      end
+    rescue => e
+      handle_error(e)
     end
 
     private
@@ -29,6 +36,28 @@ module Forms
 
     def results_url
       config.base_path
+    end
+
+    def parse_messages(error)
+      error.body&.dig('errors')
+    end
+
+    def handle_error(error)
+      case error
+      when Common::Client::Errors::ClientError
+        save_error_details(error.body)
+        raise_backend_exception('FORMS_502', self.class, error)
+      else
+        raise error
+      end
+    end
+
+    def save_error_details(error_message)
+      Raven.extra_context(
+        message: error_message,
+        url: config.base_path
+      )
+      Raven.tags_context(search: 'general_forms_query_error')
     end
   end
 end
