82cdb4e84846b15cb91e00fd468093f1e95d1326
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-12-09 15:32:09 -0500

    Add spec w/cassettes

---
+++ spec/lib/forms/client_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Forms::Client do
+  subject { described_class.new(query) }
+
+  let(:query) { nil }
+
+  describe '#get_all' do
+    context 'with no query' do
+      let(:query) { nil }
+
+      it 'returns a form response object' do
+        VCR.use_cassette('forms/200_all_forms') do
+          response = subject.get_all
+          expect(response).to be_accepted
+          expect(response).to be_an Forms::Responses::Response
+        end
+      end
+    end
+
+    context 'with a query' do
+      let(:query) { 'health' }
+
+      it 'returns a form response object' do
+        VCR.use_cassette('forms/200_form_query') do
+          response = subject.get_all
+          expect(response).to be_accepted
+          expect(response).to be_an Forms::Responses::Response
+        end
+      end
+    end
+  end
+end
