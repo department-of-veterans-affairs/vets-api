dc91bda88a8ba47dd94b0001c51d9ad80b666f85
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-12-10 09:55:05 -0500

    fixed descriptions

--- app/swagger/schemas/gibct/institution_programs.rb
+++ app/swagger/schemas/gibct/institution_programs.rb
@@ -11,7 +11,7 @@ module Swagger
 
           property :data, type: :array, minItems: 0, uniqueItems: true do
             items do
-              property :id, type: :integer, example: '1234'
+              property :id, type: :integer, example: 1234
               property :value, type: :string, example: '1X12345'
               property :label, type: :string, example: 'COMPUTER SCIENCE'
             end
@@ -38,19 +38,21 @@ module Swagger
                 key :required, %i[facility_code description]
 
                 property :program_type, type: %i[null string],
-                                        enum: %w[IHL NCD OJT FLGT CORR]
-                property :description, type: :string, example: 'COMPUTER SCIENCE'
-                property :length_in_hours, type: %i[null string], example: '680'
-                property :length_in_weeks, type: %i[null integer], example: 12
-                property :facility_code, type: :string, example: '1X12345'
-                property :institution_name, type: :string, example: 'CODE PLACE'
-                property :city, type: %i[null string], example: 'ANYTOWN'
-                property :state, type: %i[null string], example: 'WA'
-                property :country, type: %i[null string], example: 'USA'
-                property :preferred_provider, type: %i[null boolean], example: false
-                property :tuition_amount, type: %i[null integer], example: 1000
-                property :va_bah, type: %i[null number], example: 220
-                property :dod_bah, type: %i[null integer], example: 200
+                                        enum: %w[IHL NCD OJT FLGT CORR], 
+                                        description: 'The classification of the program, ex: IHL, NCD, OJT'
+                property :description, type: :string, example: 'COMPUTER SCIENCE', description: 'Program name'
+                property :length_in_hours, type: %i[null string], example: '680', description: 'Length of program (in hours)'
+                property :length_in_weeks, type: %i[null integer], example: 12, description: 'Length of program (in weeks)'
+                property :facility_code, type: :string, example: '1X12345', description: 'Program institution facility code'
+                property :institution_name, type: :string, example: 'CODE PLACE', description: 'Program institution name'
+                property :city, type: %i[null string], example: 'ANYTOWN', description: 'Program institution physical city location'
+                property :state, type: %i[null string], example: 'WA', description: 'Program institution physical state location'
+                property :country, type: %i[null string], example: 'USA', description: 'Program institution physical country location'
+                property :preferred_provider, type: %i[null boolean], example: false, 
+                description: 'Program institution preferred provider indicator; a provider that takes on the costs of the veterans education if the requirements are not met'
+                property :tuition_amount, type: %i[null integer], example: 1000, description: 'Program tuition amount'
+                property :va_bah, type: %i[null number], example: 220, description: 'VA Basic Allowance for Housing'
+                property :dod_bah, type: %i[null integer], example: 200,  description: 'DOD Basic Allowance for Housing'
               end
             end
           end
