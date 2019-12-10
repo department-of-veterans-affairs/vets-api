51b18861d1fc9009f36a33af5b6f070bd9d834dc
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-12-10 10:34:45 -0500

    troubleshooting

--- app/swagger/schemas/gibct/institution_programs.rb
+++ app/swagger/schemas/gibct/institution_programs.rb
@@ -58,19 +58,11 @@ module Swagger
                                    description: 'Program institution physical country location'
                 property :preferred_provider, type: %i[null boolean], example: false,
                                               description: 'Program institution preferred provider indicator;
-<<<<<<< HEAD
                                               a provider that takes on the costs of the veterans education if
                                                the requirements are not met'
                 property :tuition_amount, type: %i[null integer], example: 1000,
                                           description: 'Program tuition amount'
                 property :va_bah, type: %i[null number], example: 220,
-=======
-                                              a provider that takes on the costs of the veterans
-                                              education if the requirements are not met'
-                property :tuition_amount, type: %i[null integer], example: 1000,
-                                          description: 'Program tuition amount'
-                property :va_bah, type: %i[null integer], example: 220,
->>>>>>> 968f85cf52233cff7241b204a69588bc17794d43
                                   description: 'VA Basic Allowance for Housing'
                 property :dod_bah, type: %i[null integer], example: 200,
                                    description: 'DOD Basic Allowance for Housing'
@@ -120,7 +112,7 @@ module Swagger
           property :first, type: :string
           property :prev, type: %i[null string]
           property :next, type: %i[null string]
-          property :last, type: :string, example: 'http://localhos:3000/v0/institution_programs/?name=code&page=3&per_page=30'
+          property :last, type: :string, example: 'http://localhost:3000/v0/institution_programs/?name=code&page=3&per_page=30'
         end
       end
     end
