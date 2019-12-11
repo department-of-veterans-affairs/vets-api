857269105d594bf0a56f96f97c8543b32c594004
Matt Roth <roth_matthew@bah.com>
2019-12-11 11:08:58 -0500

    lint issues

--- spec/models/gi_spec.rb
+++ spec/models/gi_spec.rb
@@ -4,35 +4,34 @@ require 'rails_helper'
 require 'common/exceptions'
 
 describe Gi do
-  let(:rest_call) {:get_institution_details}
-  let(:scrubbed_params) {{}}
-  let(:gi) {Gi.for_controller(rest_call, scrubbed_params)}
-  let(:status) {200}
-  let(:body) {{}}
+  let(:rest_call) { :get_institution_details }
+  let(:scrubbed_params) { {} }
+  let(:gi) { Gi.for_controller(rest_call, scrubbed_params) }
+  let(:status) { 200 }
+  let(:body) { {} }
   let(:gi_response) do
     GI::Responses::GiResponse.new(status: status, body: body)
   end
 
-  describe "for_controller" do
-    it ".rest_call" do
+  describe 'for_controller' do
+    it '.rest_call' do
       expect(gi.rest_call).to eq(rest_call)
     end
 
-    it ".scrubbed_params" do
+    it '.scrubbed_params' do
       expect(gi.scrubbed_params).to eq(scrubbed_params)
     end
   end
 
-  it "body" do
+  it 'body' do
     allow_any_instance_of(GI::Client).to receive(:get_institution_details).and_return(gi_response)
 
     expect(gi.body).to eq(body)
   end
 
-  it "status" do
+  it 'status' do
     allow_any_instance_of(GI::Client).to receive(:get_institution_details).and_return(gi_response)
 
     expect(gi.status).to eq(status)
   end
-
 end
