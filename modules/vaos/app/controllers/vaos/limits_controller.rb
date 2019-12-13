de0f50b3635b16cc85e08dabc5f08e2494affc9f
Kam Karshenas <kam@adhocteam.us>
2019-12-13 03:35:08 -0800

    identified bug whereby, defaulting to root level application controller by not requiring dependency

--- modules/vaos/app/controllers/vaos/limits_controller.rb
+++ modules/vaos/app/controllers/vaos/limits_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_dependency 'vaos/application_controller'
+
 module VAOS
   class LimitsController < ApplicationController
     def index
--- modules/vaos/app/controllers/vaos/pact_controller.rb
+++ modules/vaos/app/controllers/vaos/pact_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_dependency 'vaos/application_controller'
+
 module VAOS
   class PactController < ApplicationController
     def index
