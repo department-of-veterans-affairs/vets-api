9e20f71347c3ed4c21be7943dc03ae7960307c80
camerontesterman <cameronbtesterman@gmail.com>
2019-12-16 18:22:27 -0600

    whitespace fix

--- spec/request/swagger_spec.rb
+++ spec/request/swagger_spec.rb
@@ -1373,27 +1373,27 @@ RSpec.describe 'the API documentation', type: %i[apivore request], order: :defin
 
     describe 'higher_level_reviews' do
       it 'documents higher_level_reviews 401' do
-        expect(subject).to validate(:get, '/v0/appeals/higher_level_reviews/{uuid}', 
+        expect(subject).to validate(:get, '/v0/appeals/higher_level_reviews/{uuid}',
                                     401, 'uuid' => '1234')
       end
 
       it 'documents higher_level_reviews 200' do
         VCR.use_cassette('decision_review/200_review') do
-          expect(subject).to validate(:get, '/v0/appeals/higher_level_reviews/{uuid}', 
+          expect(subject).to validate(:get, '/v0/appeals/higher_level_reviews/{uuid}',
                                       200, headers, 'uuid' => '4bc96bee-c6a3-470e-b222-66a47629dc20')
         end
       end
 
       it 'documents higher_level_reviews 404' do
         VCR.use_cassette('decision_review/404_review') do
-          expect(subject).to validate(:get, '/v0/appeals/higher_level_reviews/{uuid}', 
+          expect(subject).to validate(:get, '/v0/appeals/higher_level_reviews/{uuid}',
                                       404, headers, 'uuid' => '1234')
         end
       end
 
       it 'documents higher_level_reviews 500' do
         VCR.use_cassette('decision_review/500_review') do
-          expect(subject).to validate(:get, '/v0/appeals/higher_level_reviews/{uuid}', 
+          expect(subject).to validate(:get, '/v0/appeals/higher_level_reviews/{uuid}',
                                       500, headers, 'uuid' => '1234')
         end
       end
@@ -1401,34 +1401,34 @@ RSpec.describe 'the API documentation', type: %i[apivore request], order: :defin
 
     describe 'intake_statuses' do
       it 'documents intake_statuses 401' do
-        expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{intake_id}', 
+        expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{intake_id}',
                                     401, 'intake_id' => '1234')
       end
 
       it 'documents intake_statuses 200' do
         VCR.use_cassette('decision_review/200_intake_status') do
-          expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{intake_id}', 
+          expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{intake_id}',
                                       200, headers, 'intake_id' => '1234567890')
         end
       end
 
       it 'documents intake_statuses 303' do
         VCR.use_cassette('decision_review/303_intake_status') do
-          expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{intake_id}', 
+          expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{intake_id}',
                                       303, headers, 'intake_id' => '1234567890')
         end
       end
 
       it 'documents intake_statuses 404' do
         VCR.use_cassette('decision_review/404_get_intake_status') do
-          expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{uuid}', 
+          expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{uuid}',
                                       404, headers, 'intake_id' => '1234')
         end
       end
 
       it 'documents intake_statuses 500' do
         VCR.use_cassette('decision_review/500_intake_status') do
-          expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{uuid}', 
+          expect(subject).to validate(:get, '/v0/appeals/intake_statuses/{uuid}',
                                       500, headers, 'intake_id' => '1234')
         end
       end
