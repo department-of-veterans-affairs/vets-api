1055179816d1418a2442c5c14b02912820eb9334
Ryan Travitz <rtravitz@gmail.com>
2019-12-17 10:58:48 -0500

    Add placeholder request spec for coverage

--- modules/veteran_confirmation/app/controllers/veteran_confirmation/v0/veteran_status_controller.rb
+++ modules/veteran_confirmation/app/controllers/veteran_confirmation/v0/veteran_status_controller.rb
@@ -7,17 +7,6 @@ module VeteranConfirmation
     class VeteranStatusController < ApplicationController
       def index
         render json: { hit_it: 'yep' }
-      rescue
-        raise_error!
-      end
-
-      private
-
-      def raise_error!
-        raise Common::Exceptions::BackendServiceException.new(
-          'EMIS_STATUS502',
-          source: self.class.to_s
-        )
       end
     end
   end
