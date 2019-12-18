009c2eb0af99d439c6232dfddf4f037bf4ae2d8b
Matt Roth <roth_matthew@bah.com>
2019-12-18 16:17:06 -0500

    code review feedback

--- app/models/gids_redis.rb
+++ app/models/gids_redis.rb
@@ -7,8 +7,7 @@ require 'common/models/concerns/cache_aside'
 class GIDSRedis < Common::RedisStore
   include Common::CacheAside
 
-  REDIS_CONFIG_KEY = :gi_response
-  redis_config_key REDIS_CONFIG_KEY
+  redis_config_key :gi_response
 
   # rest_call [Symbol] the GI::Client method to call
   # scrubbed_params [Hash] the params to be used with the rest_call
@@ -16,14 +15,18 @@ class GIDSRedis < Common::RedisStore
 
   # rubocop:disable Style/MethodMissingSuper
   def method_missing(name, *args)
-    self.rest_call = name
-    self.scrubbed_params = *args
-    response_from_redis_or_service.body
+    if respond_to?(name, nil)
+      self.rest_call = name
+      self.scrubbed_params = *args
+      response_from_redis_or_service.body
+    else
+      super
+    end
   end
   # rubocop:enable Style/MethodMissingSuper
 
-  def respond_to_missing?(_name, _include_private)
-    true
+  def respond_to_missing?(name, _include_private)
+    gi_service.respond_to?(name)
   end
 
   private
@@ -36,12 +39,6 @@ class GIDSRedis < Common::RedisStore
 
   def gi_service
     @client ||= ::GI::Client.new
-  end
-
-  def save
-    saved = super
-    expire(record_ttl) if saved
-    saved
   end
 
   def record_ttl
