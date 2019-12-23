d4d074e7874154bae8c7812b8535d42bb863dff2
camerontesterman <cameronbtesterman@gmail.com>
2019-12-23 13:26:59 -0600

    update apidocs controller

--- app/swagger/schemas/appeals/appeals.rb
+++ app/swagger/schemas/appeals/appeals.rb
@@ -2,20 +2,22 @@
 
 module Swagger
   module Schemas
-    class Appeals
-      include Swagger::Blocks
+    module Appeals
+      class AppealsRequests
+        include Swagger::Blocks
 
-      swagger_schema :Appeals do
-        key :required, [:data]
-        property :data, type: :array
-      end
+        swagger_schema :Appeals do
+          key :required, [:data]
+          property :data, type: :array
+        end
 
-      swagger_schema :AppealsErrors do
-        key :type, :object
-        items do
+        swagger_schema :AppealsErrors do
           key :type, :object
-          property :title, type: :string
-          property :detail, type: :string
+          items do
+            key :type, :object
+            property :title, type: :string
+            property :detail, type: :string
+          end
         end
       end
     end
--- app/swagger/schemas/appeals/higher_level_review_request.rb
+++ app/swagger/schemas/appeals/higher_level_review_request.rb
@@ -2,35 +2,37 @@
 
 module Swagger
   module Schemas
-    class Appeals
-      include Swagger::Blocks
+    module Appeals
+      class HigherLevelReviewRequest
+        include Swagger::Blocks
 
-      swagger_schema :HigherLevelReviewRequest, type: :object do
-        key :required, %i[data]
-        property :data, type: :object do
-          key :required, %i[
-            type
-            attributes
-            relationships
-          ]
-          property :type, type: :string, enum: %w[HigherLevelReview]
-          property :attributes, type: :object do
+        swagger_schema :HigherLevelReviewRequest, type: :object do
+          key :required, %i[data]
+          property :data, type: :object do
             key :required, %i[
-              receipt_date
-              informal_conference
-              same_office
-              legacy_opt_in_approved
-              benefit_type
-              veteran
+              type
+              attributes
+              relationships
             ]
-            property :receipt_date, type: :string, format: :date
-            property :informal_conference, type: :boolean do
-              key :description, 'Corresponds to "14. ...REQUEST AN INFORMAL CONFERENCE..." on form 20-0996.'
-            end
-            property :informal_conference_times, type: :array do
-              key :description, '"OPTIONAL. Time slot preference for informal conference (if being requested).'\
-                                'EASTERN TIME. Pick up to two time slots (or none if no preference). Corresponds'\
-                                ' to "14. ...REQUEST AN INFORMAL CONFERENCE..." on form 20-0996."'
+            property :type, type: :string, enum: %w[HigherLevelReview]
+            property :attributes, type: :object do
+              key :required, %i[
+                receipt_date
+                informal_conference
+                same_office
+                legacy_opt_in_approved
+                benefit_type
+                veteran
+              ]
+              property :receipt_date, type: :string, format: :date
+              property :informal_conference, type: :boolean do
+                key :description, 'Corresponds to "14. ...REQUEST AN INFORMAL CONFERENCE..." on form 20-0996.'
+              end
+              property :informal_conference_times, type: :array do
+                key :description, '"OPTIONAL. Time slot preference for informal conference (if being requested).'\
+                                  'EASTERN TIME. Pick up to two time slots (or none if no preference). Corresponds'\
+                                  ' to "14. ...REQUEST AN INFORMAL CONFERENCE..." on form 20-0996."'
+              end
             end
           end
         end
