06a12880e0697dde1b297aeba1ed2774b1e0df7d
Katherine Rodriguez <katr400@gmail.com>
2019-12-26 13:46:51 -0500

    Update vet verification swagger docs

--- modules/veteran_verification/app/controllers/veteran_verification/docs/v0/api_controller.rb
+++ modules/veteran_verification/app/controllers/veteran_verification/docs/v0/api_controller.rb
@@ -7,53 +7,64 @@ module VeteranVerification
         skip_before_action(:authenticate)
 
         def history
-          swagger = YAML.safe_load(service_history_file)
+          swagger = service_history_yaml
           render json: swagger
         end
 
         def rating
-          swagger = YAML.safe_load(disability_rating_file)
+          swagger = disability_rating_yaml
           render json: swagger
         end
 
         def status
-          swagger = YAML.safe_load(status_file)
+          swagger = status_yaml
           render json: swagger
         end
 
         def metadata
-          swagger = YAML.safe_load( metadata_template )
+          swagger = metadata_template
           render json: swagger
         end
 
         private
 
         def metadata_template
-          status_doc = YAML.safe_load(status_file)['paths']['/status']
-          disability_rating_doc = YAML.safe_load(disability_rating_file)['paths']['/disability_rating']
-          service_history_doc = YAML.safe_load(service_history_file)['paths']['/service_history']
-          metadata_doc = YAML.safe_load(metadata_file)
+          status_doc = status_yaml['paths']['/status']
+          status_schema = status_yaml['components']['schemas']
+
+          disability_rating_doc = disability_rating_yaml['paths']['/disability_rating']
+          disability_rating_schema = disability_rating_yaml['components']['schemas']
+
+          service_history_doc = service_history_yaml['paths']['/service_history']
+          service_history_schema = service_history_yaml['components']['schemas']
+
+          metadata_doc = metadata_yaml
+
           metadata_doc['paths']['/status'] = status_doc
           metadata_doc['paths']['/disability_rating'] = disability_rating_doc
           metadata_doc['paths']['/service_history'] = service_history_doc
 
-          metadata_doc.to_yaml
+          schemas = {}.merge(status_schema).merge(disability_rating_schema).merge(service_history_schema)
+
+          metadata_doc['components']['schemas'] = schemas
+
+          metadata_doc
         end
 
-        def metadata_file
-          File.read(VeteranVerification::Engine.root.join('METADATA.yml'))
+        def metadata_yaml
+          @metadata_yaml ||= YAML.safe_load(File.read(VeteranVerification::Engine.root.join('METADATA.yml')))
         end
 
-        def disability_rating_file
-          File.read(VeteranVerification::Engine.root.join('DISABILITY_RATING.yml'))
+        def disability_rating_yaml
+          @disability_yaml ||= YAML.safe_load(File.read(VeteranVerification::Engine.root.join('DISABILITY_RATING.yml')))
         end
 
-        def status_file
-          File.read(VeteranVerification::Engine.root.join('VETERAN_CONFIRMATION.yml'))
+        def status_yaml
+          @status_yaml ||= YAML.safe_load(File.read(VeteranVerification::Engine.root.join('VETERAN_CONFIRMATION.yml')))
         end
 
-        def service_history_file
-          File.read(VeteranVerification::Engine.root.join('SERVICE_HISTORY.yml'))
+        def service_history_yaml
+          @service_history_yaml ||= YAML.safe_load(File.read(VeteranVerification::Engine.root.join('SERVICE_HISTORY.yml')))
         end
       end
     end
