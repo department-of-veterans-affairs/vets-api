14f02f0815a1403995bf52f5cf3b33a57068a2ee
Matt Roth <roth_matthew@bah.com>
2019-12-31 11:34:45 -0500

    remove nil parameter

--- lib/gi/client.rb
+++ lib/gi/client.rb
@@ -6,45 +6,45 @@ module GI
     configuration GI::Configuration
 
     def get_institution_autocomplete_suggestions(params = {})
-      response = perform(:get, 'institutions/autocomplete', params, nil)
+      response = perform(:get, 'institutions/autocomplete', params)
       gids_response(response)
     end
 
     def get_institution_program_autocomplete_suggestions(params = {})
-      response = perform(:get, 'institution_programs/autocomplete', params, nil)
+      response = perform(:get, 'institution_programs/autocomplete', params)
       gids_response(response)
     end
 
     def get_calculator_constants(params = {})
-      response = perform(:get, 'calculator/constants', params, nil)
+      response = perform(:get, 'calculator/constants', params)
       gids_response(response)
     end
 
     def get_institution_search_results(params = {})
-      response = perform(:get, 'institutions', params, nil)
+      response = perform(:get, 'institutions', params)
       gids_response(response)
     end
 
     def get_institution_program_search_results(params = {})
-      response = perform(:get, 'institution_programs', params, nil)
+      response = perform(:get, 'institution_programs', params)
       gids_response(response)
     end
 
     def get_institution_details(params = {})
       facility_code = params[:id]
-      response = perform(:get, "institutions/#{facility_code}", params.except(:id), nil)
+      response = perform(:get, "institutions/#{facility_code}", params.except(:id))
       gids_response(response)
     end
 
     def get_institution_children(params = {})
       facility_code = params[:id]
-      response = perform(:get, "institutions/#{facility_code}/children", params.except(:id), nil)
+      response = perform(:get, "institutions/#{facility_code}/children", params.except(:id))
       gids_response(response)
     end
 
     def get_zipcode_rate(params = {})
       zipcode = params[:id]
-      response = perform(:get, "zipcode_rates/#{zipcode}", {}, nil)
+      response = perform(:get, "zipcode_rates/#{zipcode}", {})
       gids_response(response)
     end
 
