5e2594870e7febcc8f59d1215fba89578c0e1955
Matt Roth <roth_matthew@bah.com>
2019-12-31 11:23:10 -0500

    Update lib/gi/gids_response.rb
    Co-Authored-By: Johnny Holton <johnny@oddball.io>

--- lib/gi/gids_response.rb
+++ lib/gi/gids_response.rb
@@ -13,7 +13,7 @@ module GI
     # Builds a response with a ok status and a response's body
     #
     # @param response returned from the rest call
-    # @return [GI::Responses::GIDSResponse]
+    # @return [GI::GIDSResponse]
     def self.from(response)
       GIDSResponse.new(status: response.status, body: response.body)
     end
