3270763a73d6b340b5736f33b10aa2f3276d28af
Jeremy Britt <56260532+jbritt1@users.noreply.github.com>
2020-01-03 12:26:31 -0500

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,24 @@
 # frozen_string_literal: true
 
 plugin :statsd
+
+# Configure "min" to be the minimum number of threads to use to answer
+# requests and "max" the maximum.
+#
+# The default is "0, 16".
+#
+threads 32, 32
+
+# === Cluster mode ===
+
+# How many worker processes to run.
+#
+# The default is "0".
+#
+workers 2
+
+# Verifies that all workers have checked in to the master process within
+# the given timeout. If not the worker process will be restarted. Default
+# value is 60 seconds.
+#
+worker_timeout 60
