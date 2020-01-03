59a7cfc34e239724472070d85d97cdf4585ad421
ATeal <arteal89@gmail.com>
2020-01-03 15:05:48 -0500

    Validate json body payload for posts

---
+++ modules/claims_api/app/controllers/claims_api/concerns/json_format_validation.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module ClaimsApi
+  module JsonFormatValidation
+    extend ActiveSupport::Concern
+
+    included do
+      def validate_json_format
+        JSON.parse(request.body.string)
+      rescue JSON::ParserError
+        error = {
+          errors: [
+            {
+              type: 'malformed',
+              detail: "The payload body isn't valid JSON format"
+            }
+          ]
+        }
+        render json: error.to_json, status: 422
+      end
+    end
+  end
+end
