779e5b6bb81a39875004ca11b6361a544bf88809
Michael Bastos <bastosmichael@gmail.com>
2020-01-06 11:22:57 -0600

    Adding EVSS response migration first

---
+++ modules/claims_api/db/migrate/20200106171714_add_evss_response_to_526.rb
@@ -0,0 +1,6 @@
+class AddEVSSResponseTo526 < ActiveRecord::Migration[5.2]
+  def change
+    add_column :claims_api_auto_established_claims, :encrypted_evss_response, :string
+    add_column :claims_api_auto_established_claims, :encrypted_evss_response_iv, :string
+  end
+end
