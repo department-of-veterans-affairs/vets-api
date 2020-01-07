9b5d07d7d542ba89de6b9e7024d1fd49be3d777f
Steve Brush <steven.brush@va.gov>
2020-01-07 11:23:20 -0700

    fixed let issues

--- modules/vaos/spec/services/cc_supported_sites_service_spec.rb
+++ modules/vaos/spec/services/cc_supported_sites_service_spec.rb
@@ -12,26 +12,24 @@ describe VAOS::CCSupportedSitesService do
 
   describe '#get_supported_sites', :skip_mvi do
     it 'gets a single supported site' do
-      VCR.use_cassette('vaos/cc_supported_sites/get_one_site', record: :new_episodes) do
+      VCR.use_cassette('vaos/cc_supported_sites/get_one_site', match_requests_on: %i[method uri]) do
         response = subject.get_supported_sites(site_codes)
         expect(response[:data].id).to eq('983')
       end
     end
 
     it 'gets no supported sites' do
-      let(:site_codes) { '1,2,3' }
-      VCR.use_cassette('vaos/cc_supported_sites/get_no_sites', record: :new_episodes) do
-        response = subject.get_supported_sites(site_codes)
+      VCR.use_cassette('vaos/cc_supported_sites/get_no_sites', match_requests_on: %i[method uri]) do
+        response = subject.get_supported_sites(['1','2'])
         expect(response[:data].id).to be_nil
       end
     end
 
     context 'invalid site_codes' do
-      let(:site_codes) { '' }
 
       it 'handles bad parameters appropriately' do
-        VCR.use_cassette('vaos/cc_supported_sites/get_supported_sites_error', record: :new_episodes) do
-          expect { subject.get_supported_sites(site_codes) }.to raise_error(
+        VCR.use_cassette('vaos/cc_supported_sites/get_supported_sites_error', match_requests_on: %i[method uri]) do
+          expect { subject.get_supported_sites(nil) }.to raise_error(
             Common::Exceptions::BackendServiceException
           )
         end
