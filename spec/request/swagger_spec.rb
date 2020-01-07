7db1af147d45dcf18024efdbf9d0ff4adaa73348
omgitsbillryan <bill.ryan@adhocteam.us>
2020-01-07 00:07:22 -0500

    add 400 to hca swagger and rspec

--- spec/request/swagger_spec.rb
+++ spec/request/swagger_spec.rb
@@ -272,6 +272,10 @@ RSpec.describe 'the API documentation', type: %i[apivore request], order: :defin
         )
       end
 
+      it 'returns a 400 if no attachment data is given' do
+        expect(subject).to validate(:post, '/v0/hca_attachments', 400, '')
+      end
+
       it 'supports submitting a health care application', run_at: '2017-01-31' do
         VCR.use_cassette('hca/submit_anon', match_requests_on: [:body]) do
           expect(subject).to validate(
