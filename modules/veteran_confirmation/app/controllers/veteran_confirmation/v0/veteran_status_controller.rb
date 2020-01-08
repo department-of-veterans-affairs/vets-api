66387cf220161633048a51a2ba5efd1bafb7e3b2
Ryan Travitz <rtravitz@gmail.com>
2020-01-08 13:46:46 -0500

    Create Veteran Confirmation /status endpoint (#3676)
    * Add the implementation for an endpoint in the Veteran Confirmation API which queries MVI and eMIS with passed in attributes to return veteran status
    
    Co-authored-by: Katherine Rodriguez <edmkitty@users.noreply.github.com>

--- modules/veteran_confirmation/app/controllers/veteran_confirmation/v0/veteran_status_controller.rb
+++ modules/veteran_confirmation/app/controllers/veteran_confirmation/v0/veteran_status_controller.rb
@@ -5,8 +5,56 @@ require_dependency 'veteran_confirmation/application_controller'
 module VeteranConfirmation
   module V0
     class VeteranStatusController < ApplicationController
+      before_action :validate_body
+
       def index
-        render json: { hit_it: 'yep' }
+        status = StatusService.new.get_by_attributes(
+          ssn: params['ssn'],
+          first_name: params['first_name'],
+          last_name: params['last_name'],
+          birth_date: params['birth_date']
+        )
+
+        render json: { veteran_status: status }
+      end
+
+      private
+
+      def validate_body
+        validate_no_query_params
+
+        params.require(%i[first_name last_name ssn birth_date])
+
+        validate_ssn_format
+        vali_date
+      end
+
+      def validate_no_query_params
+        raise Common::Exceptions::NoQueryParamsAllowed if request.post? && request.query_string.present?
+      end
+
+      def validate_ssn_format
+        raise Common::Exceptions::InvalidFieldValue.new('ssn', 'the provided') unless valid_ssn?(params['ssn'])
+
+        params['ssn'] = params['ssn'].gsub('-', '')
+      end
+
+      def valid_ssn?(ssn)
+        ssn.is_a?(String) && (all_digits?(ssn) || all_digits_with_hyphens?(ssn))
+      end
+
+      def all_digits?(ssn)
+        /^\d{9}$/.match?(ssn)
+      end
+
+      def all_digits_with_hyphens?(ssn)
+        /^\d{3}-\d{2}-\d{4}$/.match?(ssn)
+      end
+
+      def vali_date
+        params['birth_date'] = Date.iso8601(params['birth_date']).strftime('%Y%m%d')
+      rescue ArgumentError
+        raise Common::Exceptions::InvalidFieldValue.new('birth_date', params['birth_date'])
       end
     end
   end
