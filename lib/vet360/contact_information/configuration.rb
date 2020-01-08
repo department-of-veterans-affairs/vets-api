ce39af62fb04b06d63672f740425e0a948a92bf3
Lihan Li <frankieteardrop@gmail.com>
2020-01-07 23:21:18 -0800

    update contact info paths

--- lib/vet360/contact_information/configuration.rb
+++ lib/vet360/contact_information/configuration.rb
@@ -6,7 +6,7 @@ module Vet360
       self.read_timeout = Settings.vet360.contact_information.timeout || 30
 
       def base_path
-        "#{Settings.vet360.url}/person-mdm-cuf-person-hub/cuf/person/contact-information/v1"
+        "#{Settings.vet360.url}/contact-information-hub/cuf/contact-information/v1"
       end
 
       def service_name
