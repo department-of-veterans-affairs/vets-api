711e5ea1067586d5ba6992f4bcf77a5c7df504aa
Alex R Teal <arteal89@gmail.com>
2020-01-10 10:01:46 -0500

    Validate json body payload for posts (#3729)
    * Validate json body payload for posts
    
    * Tests caught an edge case. thnx test
    
    * Store result so we don't parse again
    
    * Specify json:api and include link
    
    Co-authored-by: Michael Bastos <bastosmichael@gmail.com>

--- modules/claims_api/app/controllers/claims_api/application_controller.rb
+++ modules/claims_api/app/controllers/claims_api/application_controller.rb
@@ -3,13 +3,16 @@
 require_dependency 'claims_api/concerns/mvi_verification'
 require_dependency 'claims_api/concerns/header_validation'
 require_dependency 'claims_api/unsynchronized_evss_claims_service'
+require_dependency 'claims_api/concerns/json_format_validation'
 
 module ClaimsApi
   class ApplicationController < ::OpenidApplicationController
     include ClaimsApi::MviVerification
     include ClaimsApi::HeaderValidation
+    include ClaimsApi::JsonFormatValidation
 
     skip_before_action :set_tags_and_extra_context, raise: false
+    before_action :validate_json_format, if: -> { request.post? }
 
     private
 
