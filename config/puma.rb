90d250985122cc0c66de2bf33248a705ad527d02
Jeremy Britt <56260532+jbritt1@users.noreply.github.com>
2020-01-10 09:17:23 -0500

    Set default workers / threads

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 plugin :statsd
+
+workers Integer(ENV.fetch("PUMA_WORKERS", 0))
+threads_count = Integer(ENV.fetch("PUMA_THREADS", 16))
+threads(threads_count, threads_count)
