82e41eadd3009e29082885f8f0485465a658c484
Jeremy Britt <56260532+jbritt1@users.noreply.github.com>
2020-01-10 09:31:58 -0500

    Change to single quotes

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,6 @@
 
 plugin :statsd
 
-workers Integer(ENV.fetch("PUMA_WORKERS", 0))
-threads_count = Integer(ENV.fetch("PUMA_THREADS", 16))
+workers Integer(ENV.fetch('PUMA_WORKERS', 0))
+threads_count = Integer(ENV.fetch('PUMA_THREADS', 16))
 threads(threads_count, threads_count)
