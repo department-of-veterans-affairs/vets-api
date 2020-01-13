6ee1c597c38ed2873db9ec096a7e61cde0fe7a31
Kam Karshenas <kam@adhocteam.us>
2020-01-13 15:34:16 -0800

    changes to support further testing

--- modules/vaos/app/models/vaos/appointment_form.rb
+++ modules/vaos/app/models/vaos/appointment_form.rb
@@ -2,6 +2,7 @@
 
 require 'active_model'
 require 'common/models/form'
+require 'common/models/attribute_types/iso8601_time'
 
 module VAOS
   class AppointmentForm < Common::Form
@@ -9,65 +10,55 @@ module VAOS
     attribute :scheduling_request_type, String
     attribute :type, String
     attribute :appointment_kind, String
-    attribute :desired_date, VAOS::AppointmentTime
-    attribute :date_time, VAOS::AppointmentTime
+    attribute :desired_date, Common::ISO8601Time
+    attribute :date_time, Common::ISO8601Time
     attribute :duration, Integer
     attribute :booking_notes, String
-    attribute :preferred_email, String
-    attribute :time_zone, String
-    attribute :location, Hash
+    attribute :patient_information, Hash
+    attribute :appointment_location, Hash
 
     def initialize(user, json_hash)
       @user = user
+      super(json_hash)
     end
 
     def params
       raise Common::Exceptions::ValidationErrors, self unless valid?
 
-      attribute.compact
-               .except(:preferred_email, :time_zone, :location)
-               .merge(patients: patient)
+      attributes.compact
+               .except(:patient_information, :appointment_location)
+               .merge(patients: patients)
     end
 
-    def test_data
-      {
+    def self.test_data
+      data = {
         appointment_type: 'Primary Care',
         scheduling_request_type: 'NEXT_AVAILABLE_APPT',
         type: 'REGULAR',
         appointment_kind: 'TRADITIONAL',
-        desired_date: '11/22/2019 09:30:00',
-        date_time: '11/22/2019 09:30:00',
+        desired_date: 5.days.from_now.utc.iso8601,
+        date_time: 5.days.from_now.utc.iso8601,
         duration: 20,
         booking_notes: 'tummy hurts',
-        patients:       {
-          patient: [
-            id: {
-              unique_id: @user.icn,
-              assigning_authority: 'ICN'
-            },
-            name: {
-              first_name: first_name,
-              last_name: last_name
-            },
-            contact_information: {
-              preferred_email: 'abraham.lincoln@va.gov',
-              time_zone: 'America/Denver'
-            },
-            location: {
-              type: 'VA',
-              facility: {
-                name: 'CHYSHR-Cheyenne VA Medical Center',
-                siteCode: '983',
-                time_zone: 'America/Denver'
-              },
-              clinic: {
-                ien: '308',
-                name: 'CHY PC KILPATRICK'
-              }
-            }
-          ]
+        patient_information: {
+          preferred_email: 'abraham.lincoln@va.gov',
+          time_zone: 'America/Denver'
+        },
+        appointment_location: {
+          type: 'VA',
+          facility: {
+            name: 'CHYSHR-Cheyenne VA Medical Center',
+            site_code: '983',
+            time_zone: 'America/Denver'
+          },
+          clinic: {
+            ien: '308',
+            name: 'CHY PC KILPATRICK'
+          }
         }
       }
+
+      self.new(FactoryBot.build(:user, :vaos), data)
     end
 
     private
@@ -80,14 +71,11 @@ module VAOS
             assigning_authority: 'ICN'
           },
           name: {
-            first_name: user.first_name,
-            last_name: user.last_name
+            first_name: @user.first_name,
+            last_name: @user.last_name
           },
-          contact_information: {
-            preferred_email: preferred_email,
-            time_zone: time_zone
-          },
-          location: location
+          contact_information: patient_information,
+          location: appointment_location
         ]
       }
     end
