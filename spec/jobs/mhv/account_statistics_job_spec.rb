c3b56116c57c74d2c412e52bf4ce425959e82e0f
John Paul Ashenfelter <johnpaul@oddball.io>
2020-01-13 15:03:13 -0500

    Rubocop

--- spec/jobs/mhv/account_statistics_job_spec.rb
+++ spec/jobs/mhv/account_statistics_job_spec.rb
@@ -9,24 +9,42 @@ RSpec.describe MHV::AccountStatisticsJob, type: :job do
 
       MHV::AccountStatisticsJob.new.perform
 
-      expect(StatsD).to have_received(:gauge).with('mhv.account.created_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.existing_premium_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.existing_upgraded_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.existing_failed_upgrade_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.created_premium_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.created_failed_upgrade_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.created_and_upgraded_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.failed_create_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.total_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.active.created_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.active.existing_premium_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.active.existing_upgraded_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.active.existing_failed_upgrade_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.active.created_premium_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.active.created_failed_upgrade_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.active.created_and_upgraded_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.active.failed_create_count', 0).exactly(1).time
-      expect(StatsD).to have_received(:gauge).with('mhv.account.active.total_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.created_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.existing_premium_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.existing_upgraded_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.existing_failed_upgrade_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.created_premium_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.created_failed_upgrade_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.created_and_upgraded_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.failed_create_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.total_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.active.created_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.active.existing_premium_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.active.existing_upgraded_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.active.existing_failed_upgrade_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.active.created_premium_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.active.created_failed_upgrade_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.active.created_and_upgraded_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.active.failed_create_count', 0).exactly(1).time
+      expect(StatsD).to have_received(:gauge)
+        .with('mhv.account.active.total_count', 0).exactly(1).time
     end
   end
 end
