42b58d329dbc24b290c898a91a5d2bfb6c43181f
John Paul Ashenfelter <johnpaul@oddball.io>
2020-01-13 12:51:56 -0500

    Ensure that the test environment is not using a real rails log

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.logger = Logger.new(nil)
+  config.log_level = :fatal
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
