fc5ec8fc67c612a2b46a485149d28003afc07cb1
Lihan Li <frankieteardrop@gmail.com>
2020-01-13 22:12:53 -0800

    new episode for put_email_success

--- spec/lib/vet360/contact_information/service_spec.rb
+++ spec/lib/vet360/contact_information/service_spec.rb
@@ -76,8 +76,8 @@ describe Vet360::ContactInformation::Service, skip_vet360: true do
 
     context 'when successful' do
       it 'returns a status of 200' do
-        VCR.use_cassette('vet360/contact_information/put_email_success', VCR::MATCH_EVERYTHING) do
-          email.id = 42
+        VCR.use_cassette('vet360/contact_information/put_email_success', record: :new_episodes) do
+          email.id = 8087
           email.email_address = 'person42@example.com'
           response = subject.put_email(email)
           expect(response.transaction.id).to eq('0b6344e3-3348-419c-bad9-c1a634e3d621')
