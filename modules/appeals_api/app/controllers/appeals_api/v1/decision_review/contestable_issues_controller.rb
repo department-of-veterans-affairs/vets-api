ea2f25993bfa9358b2102d566529c3dadc829d88
Ed Mangimelli <edmangimelli@gmail.com>
2020-01-14 08:24:59 -0600

    remove lighthouse api key from headers supplied as argument to

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_review/contestable_issues_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_review/contestable_issues_controller.rb
@@ -2,6 +2,6 @@
 
 class AppealsApi::V1::DecisionReview::ContestableIssuesController < AppealsApi::ApplicationController
   def index
-    render_response(Appeals::Service.new.get_contestable_issues(headers: request.headers))
+    render_response(Appeals::Service.new.get_contestable_issues(headers: request.headers.except("apikey")))
   end
 end
