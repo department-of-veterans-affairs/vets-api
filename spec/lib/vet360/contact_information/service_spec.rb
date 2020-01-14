c51180d24b6ac508a5b9730632eecbc8f7e35773
Lihan Li <frankieteardrop@gmail.com>
2020-01-13 22:19:59 -0800

    post addr success spec

--- spec/lib/vet360/contact_information/service_spec.rb
+++ spec/lib/vet360/contact_information/service_spec.rb
@@ -92,7 +92,7 @@ describe Vet360::ContactInformation::Service, skip_vet360: true do
 
     context 'when successful' do
       it 'returns a status of 200' do
-        VCR.use_cassette('vet360/contact_information/post_address_success', record: :new_episodes) do
+        VCR.use_cassette('vet360/contact_information/post_address_success', VCR::MATCH_EVERYTHING) do
           address.id = nil
           address.address_line1 = '1493 Martin Luther King Rd'
           address.city = 'Fulton'
