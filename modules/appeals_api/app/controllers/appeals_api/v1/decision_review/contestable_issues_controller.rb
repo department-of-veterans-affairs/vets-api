15772a4150b70640c2f8f54123fd105dad7c0076
Ed Mangimelli <edmangimelli@gmail.com>
2020-01-14 09:04:49 -0600

    remove with_okta... from test

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_review/contestable_issues_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_review/contestable_issues_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AppealsApi::V1::DecisionReview::ContestableIssuesController < AppealsApi::ApplicationController
+  skip_before_action(:authenticate)
+
   def index
     render_response(Appeals::Service.new.get_contestable_issues(headers: request.headers.except("apikey")))
   end
--- modules/appeals_api/spec/requests/v1/contestable_issues_controller_spec.rb
+++ modules/appeals_api/spec/requests/v1/contestable_issues_controller_spec.rb
@@ -5,20 +5,18 @@ require "rails_helper"
 describe AppealsApi::V1::DecisionReview::ContestableIssuesController, type: :request do
   describe "#index" do
     it do
-      with_okta_user(["appeals"]) do |auth_header|
-        get(
-          "/services/appeals/v1/decision_review/contestable_issues",
-          headers: {
-            "veteranId" => "123456789",
-            "receiptDate" => Time.zone.today.strftime("%F")
-          }.merge(auth_header)
+      get(
+        "/services/appeals/v1/decision_review/contestable_issues",
+        headers: {
+          "veteranId" => "123456789",
+          "receiptDate" => Time.zone.today.strftime("%F")
+        }
+      )
+      json = JSON.parse(response.body)
+      expect(json).not_to eq(
+        "errors" => [{ "code" => "401", "detail" => "Not authorized", "status" => "401", "title" => "Not authorized" }]
         )
-        json = JSON.parse(response.body)
-        expect(json).not_to eq(
-          "errors" => [{ "code" => "401", "detail" => "Not authorized", "status" => "401", "title" => "Not authorized" }]
-        )
-        expect(response).to have_http_status(:ok)
-      end
+      expect(response).to have_http_status(:ok)
     end
   end
 end
