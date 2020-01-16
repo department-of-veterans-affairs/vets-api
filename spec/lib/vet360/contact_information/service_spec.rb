3bae4ad62ce634855465499be11abb829f2da14d
Lihan Li <frankieteardrop@gmail.com>
2020-01-15 18:42:52 -0800

    new episode for telephone_transaction_status

--- spec/lib/vet360/contact_information/service_spec.rb
+++ spec/lib/vet360/contact_information/service_spec.rb
@@ -242,10 +242,10 @@ describe Vet360::ContactInformation::Service, skip_vet360: true do
 
   describe '#get_telephone_transaction_status' do
     context 'when successful' do
-      let(:transaction_id) { 'e0c79288-ce0b-43b5-adb0-94c8ff667da5' }
+      let(:transaction_id) { 'a2af8cd1-472c-4e6f-bd5a-f95e31e351b7' }
 
       it 'returns a status of 200' do
-        VCR.use_cassette('vet360/contact_information/telephone_transaction_status', VCR::MATCH_EVERYTHING) do
+        VCR.use_cassette('vet360/contact_information/telephone_transaction_status', record: :new_episodes) do
           response = subject.get_telephone_transaction_status(transaction_id)
           expect(response).to be_ok
           expect(response.transaction).to be_a(Vet360::Models::Transaction)
