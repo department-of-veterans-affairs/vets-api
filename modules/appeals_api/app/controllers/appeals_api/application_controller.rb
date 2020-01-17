f5dfc14ab5b75b16a086b80bcaa40e582993b751
Ed Mangimelli <38672641+edmangimelli@users.noreply.github.com>
2020-01-17 13:37:17 -0600

    get contestable issues through lighthouse (#3775)
    * get contestable issues through lighthouse
    
    * push failing test example
    
    * tweak test and routes
    
    * remove lighthouse api key from headers supplied as argument to
    
    * remove with_okta... from test
    
    * wip
    
    * create cassette
    
    * make a constant for v2 path
    
    * update cassette using local caseflow
    
    * rubocop

--- modules/appeals_api/app/controllers/appeals_api/application_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/application_controller.rb
@@ -3,5 +3,9 @@
 module AppealsApi
   class ApplicationController < ::ApplicationController
     skip_before_action :set_tags_and_extra_context, raise: false
+
+    def render_response(response)
+      render json: response.body, status: response.status
+    end
   end
 end
