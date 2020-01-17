b1b838db42f27427bd05e4138151139cebf9dcc8
Ed Mangimelli <edmangimelli@gmail.com>
2020-01-17 12:25:40 -0600

    rubocop

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_review/contestable_issues_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_review/contestable_issues_controller.rb
@@ -3,7 +3,7 @@
 class AppealsApi::V1::DecisionReview::ContestableIssuesController < AppealsApi::ApplicationController
   skip_before_action(:authenticate)
 
-  EXPECTED_HEADERS = %w[veteranId receiptDate]
+  EXPECTED_HEADERS = %w[veteranId receiptDate].freeze
 
   def index
     render_response(Appeals::Service.new.get_contestable_issues(headers))
@@ -13,5 +13,5 @@ class AppealsApi::V1::DecisionReview::ContestableIssuesController < AppealsApi::
     EXPECTED_HEADERS.reduce({}) do |hash, key|
       hash.merge(key => request.headers[key])
     end
-  end 
+  end
 end
