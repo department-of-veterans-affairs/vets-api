ed38ace607937b37c2838cf697f7b97c88adfec2
Michael Bastos <bastosmichael@gmail.com>
2020-01-17 14:35:05 -0600

    rubocop fix

--- modules/claims_api/db/migrate/20200117193734_add_headers_md5.rb
+++ modules/claims_api/db/migrate/20200117193734_add_headers_md5.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class AddHeadersMd5 < ActiveRecord::Migration[5.2]
   safety_assured
-  
+
   def change
     add_column :claims_api_power_of_attorneys, :header_md5, :string
     add_index :claims_api_power_of_attorneys, :header_md5
