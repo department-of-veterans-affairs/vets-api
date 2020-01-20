b5fb3c1172cdbbff7e8b7988f1f7ab3d39b6894d
Ed Mangimelli <edmangimelli@gmail.com>
2020-01-20 16:20:31 -0600

    update cassette

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_review/higher_level_reviews_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_review/higher_level_reviews_controller.rb
@@ -8,6 +8,15 @@ class AppealsApi::V1::DecisionReview::HigherLevelReviewsController < AppealsApi:
   end
 
   def create
-    render_response(Appeals::Service.new.create_higher_level_review(params.permit(:data, :included).as_json))
+    render_response(Appeals::Service.new.create_higher_level_review(create_params))
+  end
+
+  private
+
+  def create_params
+    {
+      'data' => params[:data].as_json,
+      'included' => params[:included].as_json
+    }
   end
 end
--- modules/appeals_api/spec/requests/v1/higher_level_reviews_controller_spec.rb
+++ modules/appeals_api/spec/requests/v1/higher_level_reviews_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe AppealsApi::V1::DecisionReview::HigherLevelReviewsController, type: :request do
-  describe '#index' do
+  describe '#show' do
     it 'show a HLR from Caseflow successfully' do
       VCR.use_cassette('appeals/higher_level_reviews_show') do
         get('/services/appeals/v1/decision_review/higher_level_reviews/97bca3d5-3524-4e5d-81ea-92753892a59c')
@@ -14,38 +14,34 @@ describe AppealsApi::V1::DecisionReview::HigherLevelReviewsController, type: :re
     end
   end
 
-  describe "#create" do
-    it "create an HLR through Caseflow successfully" do
+  describe '#create' do
+    it 'create an HLR through Caseflow successfully' do
       VCR.use_cassette('appeals/higher_level_reviews_create') do
         post(
-          "/services/appeals/v1/decision_review/higher_level_reviews",
+          '/services/appeals/v1/decision_review/higher_level_reviews',
+          as: :json,
           params: {
-            "data" => {
-              "type" => "HigherLevelReview",
-              "attributes" => {
-                "receiptDate" => "2019-07-10",
-                "informalConference" => true,
-                "sameOffice" => false,
-                "legacyOptInApproved" => true,
-                "benefitType" => "compensation"
-              },
-              "relationships" => {
-                "veteran" => {
-                  "data" => {
-                    "type" => "Veteran",
-                    "id" => "888451301"
-                  }
+            'data' => {
+              'type' => 'HigherLevelReview',
+              'attributes' => {
+                'receiptDate' => '2019-07-10',
+                'informalConference' => true,
+                'sameOffice' => false,
+                'legacyOptInApproved' => true,
+                'benefitType' => 'compensation',
+                'veteran' => {
+                  'fileNumberOrSsn' => '872958715'
                 }
               }
             },
-            "included" => [
-              {"type" => "RequestIssue","attributes" => {"decisionIssueId" => 2}}
+            'included' => [
+              { 'type' => 'ContestableIssue', 'attributes' => { 'ratingIssueId' => '826209597423' } }
             ]
           }
         )
         expect(response).to have_http_status(:accepted)
         json = JSON.parse(response.body)
-        expect(json["data"]).not_to be nil
+        expect(json['data']).not_to be nil
       end
     end
   end
