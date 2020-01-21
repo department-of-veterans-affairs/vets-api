2fa5c886325bc6e432943a37e6fef8b3ed67d1bf
Charley Stran <charley.stran@gmail.com>
2020-01-21 12:54:49 -0600

    remove emails from claims mailer

--- modules/vba_documents/app/mailers/vba_documents/unsuccessful_report_mailer.rb
+++ modules/vba_documents/app/mailers/vba_documents/unsuccessful_report_mailer.rb
@@ -6,10 +6,8 @@ module VBADocuments
       andrew.fichter@va.gov
       michael.bastos@oddball.io
       charley.stran@oddball.io
-      alex.teal@oddball.io
       ryan.link@oddball.io
       aubrey.suter@adhocteam.us
-      trista.rowan@adhocteam.us
       valerie.hase@va.gov
     ].freeze
 
