296ddd576f95cbb2428043d16537fb5bb26c64df
Matt Roth <roth_matthew@bah.com>
2020-01-23 10:39:09 -0500

    Update configuration_spec.rb

--- spec/lib/gi/configuration_spec.rb
+++ spec/lib/gi/configuration_spec.rb
@@ -9,25 +9,11 @@ describe GI::Configuration do
         expect(GI::Configuration.instance.open_timeout).to eq(15)
       end
     end
-
-    context 'when Settings.gi.open_timeout is set' do
-      it 'uses the setting' do
-        stub_const(Settings.gi.open_timeout, 2)
-        expect(GI::Configuration.instance.open_timeout).to eq(2)
-      end
-    end
   end
 
   describe '.read_timeout' do
     context 'when Settings.gi.read_timeout is not set' do
       it 'uses the setting' do
-        expect(GI::Configuration.instance.read_timeout).to eq(15)
-      end
-    end
-
-    context 'when Settings.gi.read_timeout is set' do
-      it 'uses the setting' do
-        stub_const(Settings.gi.read_timeout, 2)
         expect(GI::Configuration.instance.read_timeout).to eq(15)
       end
     end
