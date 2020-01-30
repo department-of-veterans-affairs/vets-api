0f58c64625ef22ca348a469b2edb87ac3ff82e89
Charley Stran <charley.stran@gmail.com>
2020-01-30 11:55:04 -0600

    Fixing grammer issue in error message

--- modules/veteran/app/controllers/veteran/v0/representatives_controller.rb
+++ modules/veteran/app/controllers/veteran/v0/representatives_controller.rb
@@ -30,7 +30,7 @@ module Veteran
 
       def error_hash(parameter)
         {
-          detail: "#{parameter.humanize} are required to complete this request",
+          detail: "#{parameter.humanize} is required to complete this request",
           title: 'Missing Parameter',
           source: { parameter: parameter },
           status: 422
