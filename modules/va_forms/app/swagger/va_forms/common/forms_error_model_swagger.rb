287390eb5950a5af4dd015208827a07a0a9ec5e7
Michael Bastos <bastosmichael@gmail.com>
2020-01-30 08:40:54 -0600

    linting error

--- modules/va_forms/app/swagger/va_forms/common/forms_error_model_swagger.rb
+++ modules/va_forms/app/swagger/va_forms/common/forms_error_model_swagger.rb
@@ -1,36 +1,35 @@
 # frozen_string_literal: true
 
 module VaForms
-    module Common
-      class FormsErrorModelSwagger
-        include Swagger::Blocks
-  
-        swagger_component do
-          schema :FormsErrorModel do
-            key :description, 'Errors with some details for the given request'
-  
-            key :required, %i[status details]
-            property :status do
-              key :type, :integer
-              key :format, :int32
-              key :example, '422'
-              key :description, 'Standard HTTP Status returned with Error'
-            end
-  
-            property :source do
-              key :type, :string
-              key :example, '#/query_something'
-              key :description, 'a JSON Pointer to the offending attribute in the payload'
-            end
-  
-            property :details do
-              key :type, :string
-              key :example, 'Your search parameter is not currently support'
-              key :description, 'A more detailed message about why an error occured'
-            end
+  module Common
+    class FormsErrorModelSwagger
+      include Swagger::Blocks
+
+      swagger_component do
+        schema :FormsErrorModel do
+          key :description, 'Errors with some details for the given request'
+
+          key :required, %i[status details]
+          property :status do
+            key :type, :integer
+            key :format, :int32
+            key :example, '422'
+            key :description, 'Standard HTTP Status returned with Error'
+          end
+
+          property :source do
+            key :type, :string
+            key :example, '#/query_something'
+            key :description, 'a JSON Pointer to the offending attribute in the payload'
+          end
+
+          property :details do
+            key :type, :string
+            key :example, 'Your search parameter is not currently support'
+            key :description, 'A more detailed message about why an error occured'
           end
         end
       end
     end
   end
-  
+end
