16168c4f520473ea2bde1737e242ccc3e8f0533d
Ryan Travitz <rtravitz@gmail.com>
2020-02-03 15:58:17 -0500

    Add service to sign Veteran Verification API payloads

--- modules/veteran_verification/app/controllers/veteran_verification/v0/disability_rating_controller.rb
+++ modules/veteran_verification/app/controllers/veteran_verification/v0/disability_rating_controller.rb
@@ -1,20 +1,35 @@
 # frozen_string_literal: true
 
 require_dependency 'veteran_verification/application_controller'
+require_dependency 'notary'
 
 module VeteranVerification
   module V0
     class DisabilityRatingController < ApplicationController
+      include ActionController::MimeResponds
+
+      NOTARY = VeteranVerification::Notary.new(Settings.vet_verification.key_path)
+
       before_action { authorize :evss, :access? }
       before_action { permit_scopes %w[disability_rating.read] }
+      before_action { set_default_format }
 
       def index
-        response = service.get_rated_disabilities
-        render json: response.rated_disabilities,
-               each_serializer: VeteranVerification::DisabilityRatingSerializer
+        disabilities_response = service.get_rated_disabilities
+        serialized = ActiveModel::SerializableResource.new(disabilities_response.rated_disabilities, 
+                                                           each_serializer: VeteranVerification::DisabilityRatingSerializer)
+
+        respond_to do |format|
+          format.json { render json: serialized.to_json }
+          format.jwt { render body: NOTARY.sign(serialized.serializable_hash) }
+        end
       end
 
       private
+
+      def set_default_format
+        request.format = :json if params[:format].nil? && request.headers['HTTP_ACCEPT'].nil?
+      end
 
       def service
         EVSS::DisabilityCompensationForm::Service.new(auth_headers)
---
+++ modules/veteran_verification/lib/notary.rb
@@ -0,0 +1,35 @@
+require 'jwt'
+
+module VeteranVerification
+  class NotaryException < StandardError
+  end
+
+  class Notary
+    def initialize(private_key_path)
+      begin
+        key_file = File.read(private_key_path)
+        @keypair = OpenSSL::PKey::RSA.new(key_file)
+      rescue SystemCallError, OpenSSL::PKey::RSAError => e
+        raise NotaryException, "failed trying to initialize VeteranVerification::Notary with an RSA key #{e}"
+      end
+    end
+
+    def public_key
+      @keypair.public_key
+    end
+
+    # This kid method is taken from the implementation of JWT::JWK in a future 
+    # version of the jwt gem. It provides a stable, deterministic kid for a 
+    # given public key 
+    def kid
+      sequence = OpenSSL::ASN1::Sequence([OpenSSL::ASN1::Integer.new(public_key.n),
+                                          OpenSSL::ASN1::Integer.new(public_key.e)])
+      OpenSSL::Digest::SHA256.hexdigest(sequence.to_der)
+    end
+
+    def sign(payload)
+      headers = { kid: kid }
+      JWT.encode(payload, @keypair, 'RS256', headers)
+    end
+  end
+end
