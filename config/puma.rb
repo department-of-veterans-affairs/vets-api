3ce009a8c4903caf1b59f5f03176cade1b778348
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2020-02-04 19:32:41 -0500

    And another rubocop fix

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,6 @@ workers Integer(ENV.fetch('PUMA_WORKERS', 0))
 threads_count = Integer(ENV.fetch('PUMA_THREADS', 16))
 threads(threads_count, threads_count)
 
-
 if ENV.fetch('PUMA_PRELOAD', false)
   preload_app!
 
