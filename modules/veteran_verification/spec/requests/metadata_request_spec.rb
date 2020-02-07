20b0210d08c89329b12f84a31e21feae4d7ccea0
Charley Stran <charley.stran@gmail.com>
2020-02-07 10:05:10 -0600

    update spec to check for raise error

--- modules/veteran_verification/spec/requests/metadata_request_spec.rb
+++ modules/veteran_verification/spec/requests/metadata_request_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Metadata Endpoint', type: :request do
     it 'returns metadata JSON for address validation' do
       get '/services/veteran_verification/address_validation_metadata'
       expect(response).to have_http_status(:ok)
-      JSON.parse(response.body)
+      expect { JSON.parse(response.body) }.not_to raise_error
     end
   end
 end
