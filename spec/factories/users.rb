3c501d917a94232e63341555bf9ae544d482f743
Silvio Luthi <silvio@adhocteam.us>
2020-02-07 17:18:19 -0800

    add add_person_message xml generation spec tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -20,6 +20,8 @@ FactoryBot.define do
       mhv_account_type { nil }
       dslogon_edipi { nil }
       va_patient { nil }
+      search_token { nil }
+      icn_with_aaid { nil }
 
       sign_in do
         {
