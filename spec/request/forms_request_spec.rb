327b5ce7101b52cc04dafd904e127d48b4076814
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-02-11 11:39:30 -0500

    Attempt to fix test

--- spec/request/forms_request_spec.rb
+++ spec/request/forms_request_spec.rb
@@ -2,28 +2,32 @@
 
 require 'rails_helper'
 
+def type_of_response(response)
+  JSON.parse(response.body)['data'][0]['type']
+end
+
 describe 'forms', type: :request do
   include SchemaMatchers
 
   describe 'GET /v0/forms' do
     context 'with a query' do
-      it 'matches the forms schema', :aggregate_failures do
+      it 'matches the forms schema' do
         VCR.use_cassette('forms/200_form_query') do
           get '/v0/forms', params: { term: 'health' }
 
           expect(response).to have_http_status(:ok)
-          expect(response).to match_response_schema('forms')
+          expect(type_of_response(response)).to eq('va_form')
         end
       end
     end
 
     context 'without a query' do
-      it 'matches the forms schema', :aggregate_failures do
+      it 'matches the forms schema' do
         VCR.use_cassette('forms/200_all_forms') do
           get '/v0/forms'
 
           expect(response).to have_http_status(:ok)
-          expect(response).to match_response_schema('forms')
+          expect(type_of_response(response)).to eq('va_form')
         end
       end
     end
