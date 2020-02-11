25bdfc00511817823076fbcb360a245674fd7895
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-02-11 10:54:39 -0500

    Add a forms request spec

---
+++ spec/request/forms_request_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Forms API', type: :request do
+  context 'with a query' do
+    it 'matches the forms schema', :aggregate_failures do
+      VCR.use_cassette('forms/200_form_query') do
+        get '/v0/forms', params: { term: 'health' }
+
+        expect(response).to have_http_status(:ok)
+        expect(response).to match_response_schema('forms')
+      end
+    end
+  end
+
+  context 'without a query' do
+    it 'matches the forms schema', :aggregate_failures do
+      VCR.use_cassette('forms/200_all_forms') do
+        get '/v0/forms'
+
+        expect(response).to have_http_status(:ok)
+        expect(response).to match_response_schema('forms')
+      end
+    end
+  end
+end
