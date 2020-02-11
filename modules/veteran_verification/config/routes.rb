8d65b3d58ab159ca4e3907abb9093ad02dfa9b23
Ryan Travitz <rtravitz@gmail.com>
2020-02-11 15:42:41 -0800

    Reduce documentation for Veteran Verification API to one yaml file and endpoint

--- modules/veteran_verification/config/routes.rb
+++ modules/veteran_verification/config/routes.rb
@@ -12,9 +12,6 @@ VeteranVerification::Engine.routes.draw do
 
   namespace :docs do
     namespace :v0, defaults: { format: 'json' } do
-      get 'service_history', to: 'api#history'
-      get 'disability_rating', to: 'api#rating'
-      get 'status', to: 'api#status'
       get 'veteran_verification', to: 'api#veteran_verification'
     end
   end
