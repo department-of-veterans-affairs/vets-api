f301704fcde4801a53d661d309c1f402ee6e4b27
Michael Bastos <bastosmichael@gmail.com>
2020-02-13 11:13:43 -0600

    rubocop fix

--- modules/va_forms/db/migrate/20200213160814_add_pdf_boolean_to_forms_api.rb
+++ modules/va_forms/db/migrate/20200213160814_add_pdf_boolean_to_forms_api.rb
@@ -1,6 +1,8 @@
-class AddPdfBooleanToFormsApi < ActiveRecord::Migration[5.2]  
+# frozen_string_literal: true
+
+class AddPdfBooleanToFormsApi < ActiveRecord::Migration[5.2]
   safety_assured
-  
+
   def change
     add_column :va_forms_forms, :valid_pdf, :boolean, null: false
     add_index :va_forms_forms, :valid_pdf
