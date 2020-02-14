63046fd60da6b2dca767c00ebdeff0cdab40ac6c
Lihan Li <frankieteardrop@gmail.com>
2020-02-14 11:25:11 -0800

    lint

--- spec/request/vet360/address_request_spec.rb
+++ spec/request/vet360/address_request_spec.rb
@@ -162,39 +162,39 @@ RSpec.describe 'address', type: :request do
 
   describe 'DELETE /v0/profile/addresses' do
     context 'when the method is DELETE' do
-      let(:frozen_time) { Time.parse('2020-02-13T20:47:45.000Z') }
+      let(:frozen_time) { Time.zone.parse('2020-02-13T20:47:45.000Z') }
       before do
         allow_any_instance_of(User).to receive(:vet360_id).and_return('1')
         allow_any_instance_of(User).to receive(:icn).and_return('1234')
       end
 
       let(:address) do
-        {"address_line1"=>"4041 Victoria Way",
-         "address_line2"=>nil,
-         "address_line3"=>nil,
-         "address_pou"=>"CORRESPONDENCE",
-         "address_type"=>"DOMESTIC",
-         "city"=>"Lexington",
-         "country_name"=>"United States",
-         "country_code_iso2"=>"US",
-         "country_code_iso3"=>"USA",
-         "country_code_fips"=>nil,
-         "county_code"=>"21067",
-         "county_name"=>"Fayette County",
-         "created_at"=>"2019-10-25T17:06:15.000Z",
-         "effective_end_date"=>nil,
-         "effective_start_date"=>"2020-02-10T17:40:15.000Z",
-         "id"=>138225,
-         "international_postal_code"=>nil,
-         "province"=>nil,
-         "source_system_user"=>nil,
-         "state_code"=>"KY",
-         "transaction_id"=>"537b388e-344a-474e-be12-08d43cf35d69",
-         "updated_at"=>"2020-02-10T17:40:25.000Z",
-         "validation_key"=>nil,
-         "vet360_id"=>"1",
-         "zip_code"=>"40515",
-         "zip_code_suffix"=>"4655"}
+        { 'address_line1' => '4041 Victoria Way',
+          'address_line2' => nil,
+          'address_line3' => nil,
+          'address_pou' => 'CORRESPONDENCE',
+          'address_type' => 'DOMESTIC',
+          'city' => 'Lexington',
+          'country_name' => 'United States',
+          'country_code_iso2' => 'US',
+          'country_code_iso3' => 'USA',
+          'country_code_fips' => nil,
+          'county_code' => '21067',
+          'county_name' => 'Fayette County',
+          'created_at' => '2019-10-25T17:06:15.000Z',
+          'effective_end_date' => nil,
+          'effective_start_date' => '2020-02-10T17:40:15.000Z',
+          'id' => 138_225,
+          'international_postal_code' => nil,
+          'province' => nil,
+          'source_system_user' => nil,
+          'state_code' => 'KY',
+          'transaction_id' => '537b388e-344a-474e-be12-08d43cf35d69',
+          'updated_at' => '2020-02-10T17:40:25.000Z',
+          'validation_key' => nil,
+          'vet360_id' => '1',
+          'zip_code' => '40515',
+          'zip_code_suffix' => '4655' }
       end
 
       it 'effective_end_date gets appended to the request body', :aggregate_failures do
