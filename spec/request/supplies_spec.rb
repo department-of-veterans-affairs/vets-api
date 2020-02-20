f029715ff26be4da4d77f7014bd4b2c98af37624
camerontesterman <cameronbtesterman@gmail.com>
2020-02-20 10:41:21 -0600

    added get_supplies_200 request spec test

--- spec/request/supplies_spec.rb
+++ spec/request/supplies_spec.rb
@@ -11,7 +11,10 @@ RSpec.describe 'MDOT Medical Devices & Supplies', type: :request do
     before { sign_in_as(user) }
 
     it 'lists medical devices and supplies for the veteran' do
-
+      VCR.use_cassette('mdot/get_supplies_200') do
+        get '/v0/mdot/supplies'
+        expect(response).to match_response_schema('mdot_supplies')
+      end
     end
   end
 end
