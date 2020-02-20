e9429bd6455130d00989943f56c9aff691024a73
Ben Shyong <hello@benshyong.com>
2020-02-20 00:43:18 -0800

    add process data spec

---
+++ spec/jobs/structured_data/process_data_job.spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe StructuredData::ProcessDataJob, uploader_helpers: true do
+  stub_virus_scan
+  let(:pension_burial) { create(:pension_burial) }
+  let(:claim) { pension_burial.saved_claim }
+
+  describe '#perform' do
+    let(:job) { described_class.new }
+
+    it 'attempts Veteran MVI lookup' do
+      expect_any_instance_of(BipClaims::Service).to receive(:lookup_veteran_from_mvi).with(claim).and_return(OpenStruct.new(participant_id: 123))
+    end
+
+    it 'calls Central Mail process attachments' do
+      expect(claim).to receive(:process_attachments)
+    end
+
+    it 'increments metric for successful claim submission to va.gov' do
+      expect(StatsD).to receive(:increment)
+    end
+  end
+end
