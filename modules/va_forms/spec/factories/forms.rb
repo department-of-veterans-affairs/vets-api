94b84abf0800ec075806eb0b85c73032d0f80f8d
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-02-21 17:56:30 -0500

    Fix factory

--- modules/va_forms/spec/factories/forms.rb
+++ modules/va_forms/spec/factories/forms.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :va_form, class: 'VaForms::Form' do
+  factory :va_form_526ez, class: 'VaForms::Form' do
     form_name { '526ez' }
     url { 'https://va.gov/va_form/21-526ez.pdf' }
     title { 'Disability Compensation' }
@@ -11,7 +11,7 @@ FactoryBot.define do
     sha256 { 'somelongsha' }
   end
 
-  factory :va_form, class: 'VaForms::Form' do
+  factory :va_form_10ez, class: 'VaForms::Form' do
     form_name { '10-10EZ (pdf)' }
     url { 'https://www.va.gov/vaforms/medical/pdf/10-10EZ-fillable.pdf' }
     title { 'Instructions For Completing Enrollment Application For Health Benefits' }
