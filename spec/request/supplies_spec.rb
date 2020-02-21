faeabbe2db1fb62c44fa84f03cf0f1ed0ba2a7c7
camerontesterman <cameronbtesterman@gmail.com>
2020-02-21 11:33:41 -0600

    more tests added

--- spec/request/supplies_spec.rb
+++ spec/request/supplies_spec.rb
@@ -5,15 +5,46 @@ require 'rails_helper'
 RSpec.describe 'MDOT Medical Devices & Supplies', type: :request do
   include SchemaMatchers
 
-  let(:user) { build(:user, :loa3) }
+  context 'with a loa1 user' do
+    let(:user) { FactoryBot.create(:user, :loa1, ssn: '111223333') }
+
+    it 'returns a forbidden error' do
+      get '/v0/mdot/supplies'
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
 
   context 'with an authenticated user' do
+    let(:user) { build(:user, :loa3) }
     before { sign_in_as(user) }
 
-    it 'lists medical devices and supplies for the veteran' do
-      VCR.use_cassette('mdot/get_supplies_200') do
-        get '/v0/mdot/supplies'
-        expect(response).to match_response_schema('mdot_supplies')
+    context 'with a valid response' do
+      it 'lists medical devices and supplies for the veteran' do
+        VCR.use_cassette('mdot/get_supplies_200') do
+          get '/v0/mdot/supplies'
+          expect(response).to have_http_status(:ok)
+          expect(response).to match_response_schema('mdot_supplies')
+        end
+      end
+    end
+
+    context 'with a veteran not in DLC system' do
+      it 'returns a 404 not found' do
+        VCR.use_cassette('mdot/get_supplies_404') do
+          get '/v0/mdot/supplies'
+          expect(response).to have_http_status(:not_found)
+          expect(response).to match_response_schema('errors')
+        end
+      end
+    end
+
+    context 'with a server error' do
+      it 'returns a 502 and logs an error message' do
+        VCR.use_cassette('mdot/get_supplies_502') do
+          get '/v0/mdot/supplies'
+          expect(response).to have_http_status(:bad_gateway)
+          expect(response).to match_response_schema('errors')
+        end
       end
     end
   end
