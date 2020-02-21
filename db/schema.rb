4aab65f0934fc6cfbd88609fc7ca9bb09c284a01
Nick Fasulo <nrfasulo@gmail.com>
2020-02-21 08:51:13 -0600

    Add default value to vssc_extract_date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_154839) do
+ActiveRecord::Schema.define(version: 2020_02_21_144825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -176,7 +176,7 @@ ActiveRecord::Schema.define(version: 2020_02_05_154839) do
     t.datetime "updated_at", null: false
     t.integer "min"
     t.integer "max"
-    t.datetime "vssc_extract_date"
+    t.datetime "vssc_extract_date", default: "2001-01-01 00:00:00"
     t.index ["polygon"], name: "index_drivetime_bands_on_polygon", using: :gist
   end
 
