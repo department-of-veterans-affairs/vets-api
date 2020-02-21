33a82ea7863af419f26dfa476fdf70ab3e2bb089
Lihan Li <frankieteardrop@gmail.com>
2020-02-21 11:35:55 -0800

    add ppiu allow access spec

--- app/policies/ppiu_policy.rb
+++ app/policies/ppiu_policy.rb
@@ -4,4 +4,11 @@ PPIUPolicy = Struct.new(:user, :ppiu) do
   def access?
     user.loa3? && user.multifactor
   end
+
+  def access_update?
+    res = EVSS::PPIU::Service.new(user).get_payment_information
+    control_information = res.responses.first.control_information
+
+    control_information.is_competent_indicator && control_information.no_fiduciary_assigned_indicator
+  end
 end
