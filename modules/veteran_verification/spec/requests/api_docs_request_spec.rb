96ddc800c31044dc25a63905432a289278b85fbb
Ryan Travitz <rtravitz@gmail.com>
2020-02-21 08:29:06 -0800

    Veteran Verification API JWS Signing Service (#3858)
    * Add service to sign Veteran Verification API payloads
    
    * Add ability to wrap Service History response in JWS
    
    * Add endpoint to serve public keys for Veteran Verification API signature validation
    
    * Fix linting errors
    
    * Add comment to Notary class
    
    * Add e and n to comply with JWK spec
    Stop base64url encoding pem field
    
    * Extract default format method to application controller
    Add context comments to Notary and default format methods
    
    * Reduce documentation for Veteran Verification API to one yaml file and endpoint
    
    * Add Open API spec for application/jwt responses and keys endpoint
    
    * Veteran Verification API Public Keys Endpoint (#3874)
    
    * Add endpoint to serve public keys for Veteran Verification API signature validation
    
    * Fix linting errors
    
    * Add e and n to comply with JWK spec
    Stop base64url encoding pem field
    
    * Update comment with JWKS language
    
    Co-authored-by: Nick Fasulo <nrfasulo@gmail.com>
    
    Co-authored-by: Kalil Smith-Nuevelle <kalilsn@gmail.com>
    Co-authored-by: Nick Fasulo <nrfasulo@gmail.com>

--- modules/veteran_verification/spec/requests/api_docs_request_spec.rb
+++ modules/veteran_verification/spec/requests/api_docs_request_spec.rb
@@ -3,50 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Veteran Verification Documentation Endpoints', type: :request do
-  describe '#get /docs/v0/service_history' do
-    it 'returns Open API Spec v3 JSON' do
-      get '/services/veteran_verification/docs/v0/service_history'
-      expect(response).to have_http_status(:ok)
-      JSON.parse(response.body)
-    end
-  end
-
-  describe '#get /docs/v0/disability_rating' do
-    it 'returns Open API Spec v3 JSON' do
-      get '/services/veteran_verification/docs/v0/disability_rating'
-      expect(response).to have_http_status(:ok)
-      JSON.parse(response.body)
-    end
-  end
-
-  describe '#get /docs/v0/status' do
-    it 'returns Open API Spec v3 JSON' do
-      get '/services/veteran_verification/docs/v0/status'
-      expect(response).to have_http_status(:ok)
-      JSON.parse(response.body)
-    end
-  end
-
   describe '#get /docs/v0/veteran_verification' do
     it 'returns Open API Spec v3 JSON' do
       get '/services/veteran_verification/docs/v0/veteran_verification'
       expect(response).to have_http_status(:ok)
       JSON.parse(response.body)
-    end
-
-    it 'uses yaml that exists' do
-      disability_yaml = VeteranVerification::Docs::V0::ApiController.new.send(:disability_rating_yaml)
-      status_yaml = VeteranVerification::Docs::V0::ApiController.new.send(:status_yaml)
-      service_history_yaml = VeteranVerification::Docs::V0::ApiController.new.send(:service_history_yaml)
-
-      expect(disability_yaml['paths']['/disability_rating'].keys.size).to be_positive
-      expect(disability_yaml['components']['schemas'].keys.size).to be_positive
-
-      expect(status_yaml['paths']['/status'].keys.size).to be_positive
-      expect(status_yaml['components']['schemas'].keys.size).to be_positive
-
-      expect(service_history_yaml['paths']['/service_history'].keys.size).to be_positive
-      expect(service_history_yaml['components']['schemas'].keys.size).to be_positive
     end
   end
 end
--- modules/veteran_verification/spec/requests/service_history_request_spec.rb
+++ modules/veteran_verification/spec/requests/service_history_request_spec.rb
@@ -7,12 +7,16 @@ RSpec.describe 'Service History API endpoint', type: :request, skip_emis: true d
 
   let(:scopes) { %w[profile email openid service_history.read] }
 
+  def headers(auth)
+    auth.merge('Accept' => 'application/json')
+  end
+
   context 'with valid emis responses' do
     it 'returns the current users service history with one episode' do
       with_okta_user(scopes) do |auth_header|
         VCR.use_cassette('emis/get_deployment/valid') do
           VCR.use_cassette('emis/get_military_service_episodes/valid') do
-            get '/services/veteran_verification/v0/service_history', params: nil, headers: auth_header
+            get '/services/veteran_verification/v0/service_history', params: nil, headers: headers(auth_header)
             expect(response).to have_http_status(:ok)
             expect(response.body).to be_a(String)
             expect(response).to match_response_schema('service_and_deployment_history_response')
@@ -25,11 +29,40 @@ RSpec.describe 'Service History API endpoint', type: :request, skip_emis: true d
       with_okta_user(scopes) do |auth_header|
         VCR.use_cassette('emis/get_deployment/valid') do
           VCR.use_cassette('emis/get_military_service_episodes/valid_multiple_episodes') do
-            get '/services/veteran_verification/v0/service_history', params: nil, headers: auth_header
+            get '/services/veteran_verification/v0/service_history', params: nil, headers: headers(auth_header)
             expect(response).to have_http_status(:ok)
             expect(response.body).to be_a(String)
             expect(JSON.parse(response.body)['data'].length).to eq(2)
             expect(response).to match_response_schema('service_and_deployment_history_response')
+          end
+        end
+      end
+    end
+
+    context 'with request for a jws' do
+      it 'returns a jwt with the claims in the payload' do
+        with_okta_user(scopes) do |auth_header|
+          VCR.use_cassette('emis/get_deployment/valid') do
+            VCR.use_cassette('emis/get_military_service_episodes/valid') do
+              get '/services/veteran_verification/v0/service_history',
+                  params: nil,
+                  headers: auth_header.merge('Accept' => 'application/jwt')
+              expect(response).to have_http_status(:ok)
+              expect(response.body).to be_a(String)
+
+              key_file = File.read("#{VeteranVerification::Engine.root}/spec/fixtures/verification_test.pem")
+              rsa_public = OpenSSL::PKey::RSA.new(key_file).public_key
+
+              # JWT is mocked above because it is used by the implementation code.
+              # Unfortunately, we also want to use the same module to verify the
+              # response coming back in the tests, so we reset the mock here.
+              # Otherwise, it just returns the fake JWT hash.
+              RSpec::Mocks.space.proxy_for(JWT).reset
+
+              claims = JWT.decode(response.body, rsa_public, true, algorithm: 'RS256').first
+
+              expect(claims['data'].first['type']).to eq('service_history_episodes')
+            end
           end
         end
       end
@@ -43,7 +76,7 @@ RSpec.describe 'Service History API endpoint', type: :request, skip_emis: true d
 
     it 'matches the errors schema', :aggregate_failures do
       with_okta_user(scopes) do |auth_header|
-        get '/services/veteran_verification/v0/service_history', params: nil, headers: auth_header
+        get '/services/veteran_verification/v0/service_history', params: nil, headers: headers(auth_header)
       end
 
       expect(response).to have_http_status(:bad_gateway)
