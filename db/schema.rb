5e12cbeeb8eeb9bcfd064434a8ff2b48a52dae1a
Nick Fasulo <nrfasulo@gmail.com>
2020-02-24 08:40:27 -0600

    Update records created since last migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_144825) do
+ActiveRecord::Schema.define(version: 2020_02_24_143703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
