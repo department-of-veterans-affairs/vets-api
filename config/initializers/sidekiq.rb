b23760ecfadcf69881a35334b1dcc3f496fedf11
Johnny Holton <johnny@holton.co>
2020-02-24 11:58:13 -0500

    evaluate ENV['EXCLUDE_SIDEKIQ_ENTERPRISE'] to determine whether to enable super_fetch

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,7 @@ Sidekiq::Enterprise.unique! if Rails.env.production?
 
 Sidekiq.configure_server do |config|
   config.redis = REDIS_CONFIG['redis']
-  config.super_fetch! if Rails.env.production?
+  config.super_fetch! unless ENV['EXCLUDE_SIDEKIQ_ENTERPRISE'] == 'true'
 
   config.on(:startup) do
     Sidekiq.schedule = YAML.load_file(File.expand_path('../sidekiq_scheduler.yml', __dir__))
