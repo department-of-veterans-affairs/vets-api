aa3a7586e2b7765c77db33e83ffed13fd246b446
Eric Buckley <eric.buckley@gmail.com>
2020-02-24 09:55:36 -0800

    fixing linting issue on ssoe user spec

--- spec/lib/saml/ssoe_user_spec.rb
+++ spec/lib/saml/ssoe_user_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe SAML::User do
           multifactor: false,
           loa: { current: 1, highest: 1 },
           sign_in: { service_name: 'idme', account_type: 1 },
-          disable_mhv_sso_cookie: true,
+          disable_mhv_sso_cookie: true
         )
       end
 
@@ -75,7 +75,7 @@ RSpec.describe SAML::User do
           loa: { current: 1, highest: 3 },
           sign_in: { service_name: 'myhealthevet', account_type: 1 },
           multifactor: multifactor,
-          disable_mhv_sso_cookie: true,
+          disable_mhv_sso_cookie: true
         )
       end
     end
@@ -103,7 +103,7 @@ RSpec.describe SAML::User do
           loa: { current: 1, highest: 3 },
           sign_in: { service_name: 'dslogon', account_type: 1 },
           multifactor: multifactor,
-          disable_mhv_sso_cookie: true,
+          disable_mhv_sso_cookie: true
         )
       end
     end
@@ -131,7 +131,7 @@ RSpec.describe SAML::User do
           loa: { current: 3, highest: 3 },
           sign_in: { service_name: 'dslogon', account_type: 3 },
           multifactor: multifactor,
-          disable_mhv_sso_cookie: true,
+          disable_mhv_sso_cookie: true
         )
       end
     end
