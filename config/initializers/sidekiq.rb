357b0f49f4fd144dad2e4156f29bbe4f63d5cc43
Johnny Holton <johnny@holton.co>
2020-02-27 22:27:01 -0500

    whitespace cleanup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,7 @@ Sidekiq::Enterprise.unique! if Rails.env.production?
 
 Sidekiq.configure_server do |config|
   config.redis = REDIS_CONFIG['redis']
-  # super_fetch! is only available in sidekiq-pro and will cause 
+  # super_fetch! is only available in sidekiq-pro and will cause
   #   "undefined method `super_fetch!'"
   # for those using regular sidekiq
   config.super_fetch! if defined?(Sidekiq::Pro)
