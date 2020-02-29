68e6c73265f17310fa34d08e885f5f335e0d129b
Kathleen Crawford <kcrawford@governmentcio.com>
2020-02-28 16:04:45 -0800

    updating dependents spec

--- spec/controllers/v0/dependents_applications_controller_spec.rb
+++ spec/controllers/v0/dependents_applications_controller_spec.rb
@@ -22,7 +22,6 @@ RSpec.describe V0::DependentsApplicationsController do
           get(:show, params: { id: user.participant_id })
           expect(response.code).to eq('200')
           expect(response).to have_http_status(:ok)
-          expect(JSON.parse(response.body)['return_message']).to eq 'Records found'
         end
       end
     end
