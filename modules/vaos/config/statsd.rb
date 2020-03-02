b1e9448dd02bb6497ced53ad8d0172a5822a22b7
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-03-02 10:17:48 -0800

    rubocop

--- modules/vaos/config/statsd.rb
+++ modules/vaos/config/statsd.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 service_methods = %w[get_appointments get_cancel_reasons get_clinic_institutions get_clinic_institutions
-                   get_eligibility get_facilities get_facility_available_appointments get_facility_clinics get_facility_limits
-                   get_facility_visits get_messages get_preferences get_requests get_supported_sites get_system_facilities
-                   post_appointment post_message post_request put_cancel_appointment put_preferences put_request]
+                     get_eligibility get_facilities get_facility_available_appointments get_facility_clinics
+                     get_facility_limits get_facility_visits get_messages get_preferences get_requests
+                     get_supported_sites get_system_facilities post_appointment post_message post_request
+                     put_cancel_appointment put_preferences put_request]
 
 service_methods.each do |service_method|
   StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.#{service_method}.total", 0)
