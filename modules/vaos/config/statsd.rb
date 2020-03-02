5d2528704c29c585f11848e834a98b9ec47499b1
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-03-02 09:42:47 -0800

    update vaos statsd initializer to cover all service methods

--- modules/vaos/config/statsd.rb
+++ modules/vaos/config/statsd.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_va_appointments.total", 0)
-StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_va_appointments.fail", 0)
-StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_cc_appointments.total", 0)
-StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.get_cc_appointments.fail", 0)
+service_methods = %w[get_appointments get_cancel_reasons get_clinic_institutions get_clinic_institutions
+                   get_eligibility get_facilities get_facility_available_appointments get_facility_clinics get_facility_limits
+                   get_facility_visits get_messages get_preferences get_requests get_supported_sites get_system_facilities
+                   post_appointment post_message post_request put_cancel_appointment put_preferences put_request]
+
+service_methods.each do |service_method|
+  StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.#{service_method}.total", 0)
+  StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.#{service_method}.fail", 0)
+end
