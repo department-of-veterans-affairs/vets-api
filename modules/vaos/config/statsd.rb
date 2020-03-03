9b2c6450a19304e2519fc4e7ce97c0ebc4f360fd
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-03-03 05:09:55 -0800

    rename endpoints var

--- modules/vaos/config/statsd.rb
+++ modules/vaos/config/statsd.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-service_methods = %w[get_appointments get_cancel_reasons get_clinic_institutions get_clinic_institutions
+vaos_endpoints = %w[get_appointments get_cancel_reasons get_clinic_institutions get_clinic_institutions
                      get_eligibility get_facilities get_facility_available_appointments get_facility_clinics
                      get_facility_limits get_facility_visits get_messages get_preferences get_requests
                      get_supported_sites get_system_facilities post_appointment post_message post_request
                      put_cancel_appointment put_preferences put_request]
 
-service_methods.each do |service_method|
-  StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.#{service_method}.total", 0)
-  StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.#{service_method}.fail", 0)
+vaos_endpoints.each do |endpoint|
+  StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.#{endpoint}.total", 0)
+  StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.#{endpoint}.fail", 0)
 end
