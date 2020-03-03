e22ae894ef1ea73c66707ba48976bfd21562691a
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-03-03 05:24:45 -0800

    move statsd init to config/initializers

--- modules/vaos/config/initializers/statsd.rb
+++ modules/vaos/config/initializers/statsd.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../../app/services/vaos/configuration'
+
 vaos_endpoints = %w[get_appointments get_cancel_reasons get_clinic_institutions get_clinic_institutions
                      get_eligibility get_facilities get_facility_available_appointments get_facility_clinics
                      get_facility_limits get_facility_visits get_messages get_preferences get_requests
@@ -7,6 +9,6 @@ vaos_endpoints = %w[get_appointments get_cancel_reasons get_clinic_institutions 
                      put_cancel_appointment put_preferences put_request]
 
 vaos_endpoints.each do |endpoint|
-  StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.#{endpoint}.total", 0)
-  StatsD.increment("#{VAOS::Service::STATSD_KEY_PREFIX}.#{endpoint}.fail", 0)
+  StatsD.increment("api.vaos.#{endpoint}.total", 0)
+  StatsD.increment("api.vaos.#{endpoint}.fail", 0)
 end
