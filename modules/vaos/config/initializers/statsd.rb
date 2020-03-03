def2b38fed3c04b877ca73a13d3c274c46aa9124
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-03-03 05:30:59 -0800

    rubocop

--- modules/vaos/config/initializers/statsd.rb
+++ modules/vaos/config/initializers/statsd.rb
@@ -3,10 +3,10 @@
 require_relative '../../app/services/vaos/configuration'
 
 vaos_endpoints = %w[get_appointments get_cancel_reasons get_clinic_institutions get_clinic_institutions
-                     get_eligibility get_facilities get_facility_available_appointments get_facility_clinics
-                     get_facility_limits get_facility_visits get_messages get_preferences get_requests
-                     get_supported_sites get_system_facilities post_appointment post_message post_request
-                     put_cancel_appointment put_preferences put_request]
+                    get_eligibility get_facilities get_facility_available_appointments get_facility_clinics
+                    get_facility_limits get_facility_visits get_messages get_preferences get_requests
+                    get_supported_sites get_system_facilities post_appointment post_message post_request
+                    put_cancel_appointment put_preferences put_request]
 
 vaos_endpoints.each do |endpoint|
   StatsD.increment("api.vaos.#{endpoint}.total", 0)
