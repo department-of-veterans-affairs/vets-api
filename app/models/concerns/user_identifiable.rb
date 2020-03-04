d00e3debe3a695e60c5542fd89748e13480dd951
Eric Buckley <eric.buckley@gmail.com>
2020-03-04 15:36:55 -0800

    adding a concern to mix into models that are linked via a user_uuid

---
+++ app/models/concerns/user_identifiable.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module UserIdentifiable
+  extend ActiveSupport::Concern
+
+  ACCT_ID_PREFIX = 'acct:'
+
+  included do
+    # returns all instances that match the given User on their available
+    # identifiers, either the users idme uuid or account id
+    scope :for_user, ->(u) { where(:user_uuid => [u.uuid, ACCT_ID_PREFIX + u.account_id]) }
+  end
+end
