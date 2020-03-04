f97179e8daed911f9fdcd72e8e6eefe1ff52525d
Eric Buckley <eric.buckley@gmail.com>
2020-03-04 10:19:45 -0800

    updating test cases for forceAuthn param

--- spec/controllers/v1/sessions_controller_spec.rb
+++ spec/controllers/v1/sessions_controller_spec.rb
@@ -81,7 +81,11 @@ RSpec.describe V1::SessionsController, type: :controller do
       context 'routes not requiring auth' do
         %w[mhv dslogon idme].each do |type|
           context "routes /sessions/#{type}/new to SessionsController#new with type: #{type}" do
-            it 'redirects' do
+            it 'redirects without a forceAuthn' do
+              expect(SAML::SSOeSettingsService)
+                .to receive(:saml_settings)
+                .with({force_authn: false})
+
               expect { get(:new, params: { type: type, clientId: '123123' }) }
                 .to trigger_statsd_increment(described_class::STATSD_SSO_NEW_KEY, tags: ["context:#{type}"], **once)
 
@@ -90,6 +94,23 @@ RSpec.describe V1::SessionsController, type: :controller do
                 .to be_an_idme_saml_url('https://pint.eauth.va.gov/isam/sps/saml20idp/saml20/login?SAMLRequest=')
                 .with_relay_state('originating_request_id' => nil, 'type' => type)
                 .with_params('clientId' => '123123')
+
+            end
+
+            it 'redirects with a forceAuthn' do
+              expect(SAML::SSOeSettingsService)
+                .to receive(:saml_settings)
+                .with({force_authn: true})
+
+              expect { get(:new, params: { type: type, clientId: '123123', force: true }) }
+                .to trigger_statsd_increment(described_class::STATSD_SSO_NEW_KEY, tags: ["context:#{type}"], **once)
+
+              expect(response).to have_http_status(:found)
+              expect(response.location)
+                .to be_an_idme_saml_url('https://pint.eauth.va.gov/isam/sps/saml20idp/saml20/login?SAMLRequest=')
+                .with_relay_state('originating_request_id' => nil, 'type' => type)
+                .with_params('clientId' => '123123')
+
             end
           end
         end
