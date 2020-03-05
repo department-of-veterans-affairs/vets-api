c2917e9e76dba1b9bdc44b6003d69b7340159a64
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-03-05 14:34:43 -0800

    add unit and integration tests for vaos logging middleware

--- modules/vaos/spec/services/middleware/vaos_logging_spec.rb
+++ modules/vaos/spec/services/middleware/vaos_logging_spec.rb
@@ -1,67 +1,73 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative '../../support/fixture_helper'
 
 describe VAOS::Middleware::VaosLogging do
-  let(:user) { build(:user, :vaos) }
-  let(:service) { VAOS::AppointmentService.new(user) }
-  let(:start_date) { Time.zone.parse('2019-11-14T07:00:00Z') }
-  let(:end_date) { Time.zone.parse('2020-03-14T08:00:00Z') }
+  subject(:client) do
+    Faraday.new do |conn|
+      conn.use :vaos_logging
+
+      conn.adapter :test do |stub|
+        stub.get(all_other_uris) { [status, { 'Content-Type' => 'text/plain', 'X-Vamf-Jwt' => sample_jwt }, '{}'] }
+        stub.post(user_service_uri) { [status, { 'Content-Type' => 'text/plain' }, sample_jwt] }
+      end
+    end
+  end
+
+  let(:sample_jwt) { read_fixture_file('sample_jwt.response') }
+  let(:all_other_uris) { 'https://veteran.apps.va.gov/whatever' }
+  let(:user_service_uri) { 'https://veteran.apps.va.gov/users/v2/session?processRules=true' }
 
   before do
-    allow_any_instance_of(VAOS::UserService).to receive(:session).and_return('stubbed_token')
+    Settings.va_mobile.key_path = fixture_file_path('open_ssl_rsa_private.pem')
     Timecop.freeze
   end
 
   after { Timecop.return }
 
-  describe 'vaos logging' do
-    let(:type) { 'va' }
+  context 'with status successful' do
+    let(:status) { 200 }
 
-    context 'with a succesful response' do
-      it 'increments statsd and logs additional details in a success line' do
-        VCR.use_cassette('vaos/appointments/get_appointments', match_requests_on: %i[method uri]) do
-          expect(Rails.logger).to receive(:info).with(
-            '[StatsD] increment api.external_http_request.VAOS.success:1 '\
-'#endpoint:/appointments/v1/patients/xxx/appointments #method:get'
-          )
-          expect(Rails.logger).to receive(:info).with(
-            '[StatsD] measure api.external_http_request.VAOS.time:0.0 '\
-'#endpoint:/appointments/v1/patients/xxx/appointments #method:get'
-          )
-          expect(Rails.logger).to receive(:info).with('[StatsD] increment api.vaos.get_appointments.total:1')
-          expect(Rails.logger).to receive(:info).with('VAOS service call succeeded!',
-                                                      duration: 0.0,
-                                                      jti: 'unknown jti',
-                                                      status: 200,
-                                                      url: '(GET) https://veteran.apps.va.gov/appointments'\
-'/v1/patients/1012845331V153043/appointments?endDate=2020-03-14T08%3A00%3A00Z&pageSize=0&startDate='\
-'2019-11-14T07%3A00%3A00Z&useCache=false')
-          service.get_appointments(type, start_date, end_date)
-        end
-      end
+    it 'user service call logs a success' do
+      expect(Rails.logger).to receive(:info).with('VAOS service call succeeded!',
+                                                  jti: 'ebfc95ef5f3a41a7b15e432fe47e9864',
+                                                  status: 200,
+                                                  duration: 0.0,
+                                                  url: '(POST) https://veteran.apps.va.gov/users/v2/session?processRules=true').and_call_original
+      client.post(user_service_uri)
     end
 
-    context 'with a failed response' do
-      it 'increments statsd and logs additional details in a failure line' do
-        VCR.use_cassette('vaos/appointments/get_appointments_500', match_requests_on: %i[method uri]) do
-          expect(Rails.logger).to receive(:info).with('[StatsD] increment api.vaos.get_appointments.total:1')
-          expect(Rails.logger).to receive(:info).with(
-            '[StatsD] increment api.vaos.get_appointments.fail:1 '\
-              '#error:VAOS::ServiceException'
-          )
-          expect(Rails.logger).to receive(:warn).with('VAOS service call failed!',
-                                                      duration: 0.0,
-                                                      jti: 'unknown jti',
-                                                      status: 500,
-                                                      url: '(GET) https://veteran.apps.va.gov/appointments/v1'\
-'/patients/1012845331V153043/appointments?endDate=2020-03-14T08%3A00%3A00Z&pageSize=0&startDate='\
-'2019-11-14T07%3A00%3A00Z&useCache=false')
-          expect { service.get_appointments(type, start_date, end_date) }.to raise_error(
-            Common::Exceptions::BackendServiceException
-          )
-        end
-      end
+    it 'other requests logs a success' do
+      expect(Rails.logger).to receive(:info).with('VAOS service call succeeded!',
+                                                  jti: 'ebfc95ef5f3a41a7b15e432fe47e9864',
+                                                  status: 200,
+                                                  duration: 0.0,
+                                                  url: '(GET) https://veteran.apps.va.gov/whatever').and_call_original
+      client.get(all_other_uris)
+    end
+  end
+
+  context 'with status failed' do
+    let(:status) { 500 }
+    let(:sample_jwt) { '' }
+
+    it 'user service calls logs a failure' do
+      expect(Rails.logger).to receive(:warn).with('VAOS service call failed!',
+                                                  jti: 'unknown jti',
+                                                  status: 500,
+                                                  duration: 0.0,
+                                                  url: '(POST) https://veteran.apps.va.gov/users/v2/session?processRules=true').and_call_original
+      client.post(user_service_uri)
+    end
+
+    it 'other requests logs a failure' do
+      expect(Rails.logger).to receive(:warn).with('VAOS service call failed!',
+                                                  jti: 'unknown jti',
+                                                  status: 500,
+                                                  duration: 0.0,
+                                                  url: '(GET) https://veteran.apps.va.gov/whatever').and_call_original
+      client.get(all_other_uris)
     end
   end
 end
