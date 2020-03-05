1c1bba606f01aef540a4952f7fc560dcb40c68e7
Kam Karshenas <kam@adhocteam.us>
2020-03-04 22:49:54 -0800

    rubocop fixes

--- modules/vaos/spec/factories/vaos_users.rb
+++ modules/vaos/spec/factories/vaos_users.rb
@@ -25,7 +25,7 @@ FactoryBot.modify do
       end
 
       # add an MHV correlation_id and vha_facility_ids corresponding to va_patient
-      after(:build) do |user, t|
+      after(:build) do |user, _t|
         profile = build(
           :mvi_profile,
           given_names: %w[Judy Snow],
@@ -44,7 +44,7 @@ FactoryBot.modify do
           ],
           icn: '1012845331V153043',
           icn_with_aaid: '1012845331V153043^NI^200M^USVHA',
-          mhv_ids: ["16701377"],
+          mhv_ids: ['16701377'],
           vha_facility_ids: %w[983 984 200MHS 200ESR],
           historical_icns: [],
           gender: 'F',
