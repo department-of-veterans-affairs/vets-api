06f71fa6e4184105472253ff67ac740df6f5746c
Kelson Adams <kadams@governmentcio.com>
2020-03-06 11:44:30 -0700

    Build new /gids/yellow_ribbon_programs endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -172,6 +172,9 @@ Rails.application.routes.draw do
       end
 
       resources :calculator_constants, only: :index, defaults: { format: :json }
+
+      resources :yellow_ribbon_programs, only: :index, defaults: { format: :json }
+
       resources :zipcode_rates, only: :show, defaults: { format: :json }
     end
 
