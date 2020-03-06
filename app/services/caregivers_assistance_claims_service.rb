1577e277010bff7bde172afc5d9d89dc7f00c960
Kevin Mircovich <kevinmircovich@Kevin-Mircovich-MacBook-Pro.local>
2020-03-06 08:50:20 -0500

    remove TODO notes from caregivers_assistance_service

--- app/services/caregivers_assistance_claims_service.rb
+++ app/services/caregivers_assistance_claims_service.rb
@@ -3,21 +3,7 @@
 class CaregiversAssistanceClaimsService
   def submit_claim!(user_context, claim_data)
     claim = SavedClaim::CaregiversAssistanceClaim.new(claim_data)
-
-    unless claim.valid?
-      # TODO: (kevinmirc #5672) - increment failure.client stat
-      raise(Common::Exceptions::ValidationErrors, claim)
-    end
-
-    begin
-      claim.save!
-    rescue => e
-      # TODO: (kevinmirc #5672) - increment failure.server stat
-      raise e
-    end
-
-    # TODO: (kevinmirc #5672) - increment success stat
-
+    claim.valid? ? claim.save! : raise(Common::Exceptions::ValidationErrors, claim)
     destroy_previously_saved_form_for(user_context) if user_context
 
     claim
