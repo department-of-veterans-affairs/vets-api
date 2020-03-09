f60af8813052c23647f6b4ce49c84096b116177b
Eric Buckley <eric.buckley@gmail.com>
2020-03-09 10:10:56 -0700

    updating docstring

--- app/models/concerns/user_identifiable.rb
+++ app/models/concerns/user_identifiable.rb
@@ -10,8 +10,8 @@ module UserIdentifiable
     # identifiers, either the users idme uuid or account id
     scope :for_user, ->(u) { where(user_uuid: [ACCT_ID_PREFIX + u.account_id, u.uuid]) }
 
-    # Return the first instance that matches for the given user + attributes
-    # or if none exists, initialize a new instance
+    # Return the first instance that matches for the given user, or if none
+    # exists, initialize a new instance with the given default values
     scope :first_or_initialize_for_user, lambda { |user, defaults = {}|
       for_user(user)
         .first_or_initialize(user_uuid: ACCT_ID_PREFIX + user.account_id, **defaults)
