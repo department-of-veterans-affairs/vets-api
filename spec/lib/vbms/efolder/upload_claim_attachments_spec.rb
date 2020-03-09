b2cc2b121192af2353357e51f32810d688c387b9
keithdadkins <keithdadkins@me.com>
2020-03-09 17:05:51 -0500

    refactor specs

--- spec/lib/vbms/efolder/upload_claim_attachments_spec.rb
+++ spec/lib/vbms/efolder/upload_claim_attachments_spec.rb
@@ -4,25 +4,27 @@ require 'rails_helper'
 
 
 RSpec.describe VBMS::Efolder::UploadClaimAttachments do
-  subject { described_class }
   let(:bc) { build(:burial_claim)} # plain burial claim with no attachments
   let(:pa) { build_stubbed(:pension_burial) } # persistent attachment
-
+  let(:file) { fixture_file_upload(
+    "#{::Rails.root}/spec/fixtures/pension/attachment.pdf", 'application/pdf')
+  }
   describe 'when processing a burial claim' do
-    it 'finds and loads the claim and its attachments' do
-      allow_any_instance_of(SavedClaim).to receive('persistent_attachments').and_return(pa)
+    before do
+      # save claim to get a created_at date and id
       pa.saved_claim.save
+      allow_any_instance_of(SavedClaim).to receive('persistent_attachments').and_return(pa)
+    end
+
+    it 'finds and loads a claim with attachments' do
       uploader = described_class.new(pa.saved_claim)
       claim = uploader.instance_variable_get(:@claim)
       attachments = uploader.instance_variable_get(:@attachments)
-      
       expect(claim.id).to be(pa.saved_claim.id)
       expect(claim.persistent_attachments.id).to be(pa.id)
     end
 
     it 'loads metadata from a claim' do
-      pa.saved_claim.save
-      allow_any_instance_of(SavedClaim).to receive('persistent_attachments').and_return(pa)
       uploader = described_class.new(pa.saved_claim)
       claim = uploader.instance_variable_get(:@claim)
       metadata = uploader.instance_variable_get(:@metadata)
@@ -38,30 +40,14 @@ RSpec.describe VBMS::Efolder::UploadClaimAttachments do
       expect(metadata['zip_code']).to eq(claim.open_struct_form.claimantAddress.postalCode)
       expect(metadata['receive_date']).to eq(receive_date.strftime('%Y-%m-%d %H:%M:%S'))
     end
+  end
 
-    it 'raises an error if the claim does not contain any supporting documents to upload' do
+  describe "when processing a burial claim with no attachments" do
+    it 'raises an error' do
       bc.save
       msg = "Claim #{bc.id} does not contain any supporting documents."
       expect(bc.persistent_attachments.count).to be(0)
       expect {described_class.new(bc)&.upload!}.to raise_error(ActiveRecord::RecordNotFound, msg)
-    end
-  end
-
-  describe 'uploading attachments to vbms efolder' do
-    it 'fetches an upload token' do
-
-    end
-    it 'uploads attachments' do
-
-    end
-    it 'handles vbms service outages' do
-
-    end
-    it 'cleans up after successful uploads' do
-
-    end
-    it 'retries uploading n times' do
-
     end
   end
 end
