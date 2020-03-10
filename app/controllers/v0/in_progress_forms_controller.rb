95d7d2f202085de4b1441eb54d30276f8215a7e6
Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>
2020-03-10 11:19:12 -0500

    clear notes

--- app/controllers/v0/in_progress_forms_controller.rb
+++ app/controllers/v0/in_progress_forms_controller.rb
@@ -44,22 +44,5 @@ module V0
         end
       end
     end
-
-    # TO DO:
-    # - [x] tests
-    #   - [x] mvi_add_person in spec/models/mvi_spec.rb
-    #     - [x] update: with a successful add... expect_any_instance_of(Mvi).to receive(:destroy).once will fail after new :clear_cache method is added
-    #     - [x] new: with a failed add... for form '526' expect not to call :clear_cache (instead of :destroy)
-    #     - [x] new: test of :clear_cache method
-    #   - [x] add proxy_add test to spec/request/in_progress_forms_request_spec.rb
-    # - [x] redis_store add :clear_cache method (can not use :destroy bc we don't want to freeze)
-    # - [x] new before_action:
-    #   - only on 'show' method
-    #   - can we add conditional to check params[:id] == '526' here? we might not have access to params
-    # - [x] new proxy_add method
-    #   - we must have those ids in auth headers or EVSS (the submission receiver) will error
-    #   - mvi_add_person integration already exists
-    # - [ ] swagger docs - update the GET method description to let users know that proxy_add will be called in this case
-    # - ~routes.rb update~ does not need to be updated because this is not it's own route)
   end
 end
