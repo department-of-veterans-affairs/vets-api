7726ab3d7a9eac847ad168641f70f0c4bb4d7c66
Ed Mangimelli <edmangimelli@gmail.com>
2020-03-11 10:11:35 -0500

    rollback changes

--- lib/evss/disability_compensation_form/configuration.rb
+++ lib/evss/disability_compensation_form/configuration.rb
@@ -25,22 +25,6 @@ module EVSS
       def mock_enabled?
         Settings.evss.mock_disabilities_form || false
       end
-
-      def connection
-        @conn ||= Faraday.new(base_path, request: request_options, ssl: ssl_options) do |faraday|
-          faraday.use      :breakers
-          faraday.use      EVSS::ErrorMiddleware
-          faraday.use      Faraday::Response::RaiseError
-          faraday.response :betamocks if mock_enabled?
-          faraday.response :snakecase, symbolize: false
-          # calls to EVSS returns non JSON responses for some scenarios that don't make it through VAAFI
-          # content_type: /\bjson$/ ensures only json content types are attempted to be parsed.
-          faraday.response :json, content_type: /\bjson$/
-          faraday.use :immutable_headers
-          faraday.use      ::EVSS::DisabilityCompensationForm::Form526TimeoutMiddleware
-          faraday.adapter Faraday.default_adapter
-        end
-      end
     end
   end
 end
