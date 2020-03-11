7b989ac9a989236d2d9a24a8e5bb066f562470c0
Ryan Travitz <rtravitz@gmail.com>
2020-03-11 10:21:02 -0400

    Add deployment method to MilitaryInformationServiceV2 to retrieve pay grade information

--- spec/lib/emis/military_information_service_v2_spec.rb
+++ spec/lib/emis/military_information_service_v2_spec.rb
@@ -4,19 +4,19 @@ require 'rails_helper'
 require 'emis/military_information_service_v2'
 
 describe EMIS::MilitaryInformationServiceV2 do
+  let(:edipi) { '1007697216' }
+
   describe 'get_military_service_episodes' do
     context 'with a valid request' do
-      let(:edipi) { '1007697216' }
-
       it 'calls the get_military_service_episodes endpoint with a proper emis message' do
-        VCR.use_cassette('emis/get_military_service_episodes/valid_v2') do
+        VCR.use_cassette('emis/get_military_service_episodes_v2/valid') do
           response = subject.get_military_service_episodes(edipi: edipi)
           expect(response).to be_ok
         end
       end
 
       it 'includes new fields from v2' do
-        VCR.use_cassette('emis/get_military_service_episodes/valid_v2') do
+        VCR.use_cassette('emis/get_military_service_episodes_v2/valid') do
           response = subject.get_military_service_episodes(edipi: edipi)
 
           first_item = response.items.first
@@ -34,8 +34,19 @@ describe EMIS::MilitaryInformationServiceV2 do
       let(:edipi) { '1005123832' }
 
       it 'calls the get_military_service_episodes endpoint with a proper emis message' do
-        VCR.use_cassette('emis/get_military_service_episodes/valid_no_end_date_v2') do
+        VCR.use_cassette('emis/get_military_service_episodes_v2/valid_no_end_date') do
           response = subject.get_military_service_episodes(edipi: edipi)
+          expect(response).to be_ok
+        end
+      end
+    end
+  end
+
+  describe 'get_deployment' do
+    context 'with a valid request' do
+      it 'calls the get_deplopyment endpoint with a proper emis message' do
+        VCR.use_cassette('emis/get_deployment_v2/valid') do
+          response = subject.get_deployment(edipi: edipi)
           expect(response).to be_ok
         end
       end
