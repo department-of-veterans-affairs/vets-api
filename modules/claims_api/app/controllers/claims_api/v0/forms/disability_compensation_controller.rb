c8f349da347b36581a631e6ca9a4e19bef5cec9f
Ed Mangimelli <edmangimelli@gmail.com>
2020-03-11 13:11:47 -0500

    change source to source_name (to match v1)

--- modules/claims_api/app/controllers/claims_api/v0/forms/disability_compensation_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v0/forms/disability_compensation_controller.rb
@@ -24,7 +24,7 @@ module ClaimsApi
             status: ClaimsApi::AutoEstablishedClaim::PENDING,
             auth_headers: auth_headers,
             form_data: form_attributes,
-            source: source
+            source: source_name
           )
           auto_claim = ClaimsApi::AutoEstablishedClaim.find_by(md5: auto_claim.md5) unless auto_claim.id
           service_object.validate_form526(auto_claim.to_internal)
@@ -36,7 +36,7 @@ module ClaimsApi
           track_526_validation_errors(e.details)
           render json: { errors: format_errors(e.details) }, status: :unprocessable_entity
         rescue Common::Exceptions::GatewayTimeout, Timeout::Error, Faraday::TimeoutError => e
-          req = { auth: auth_headers, form: form_attributes, source: source, auto_claim: auto_claim.as_json }
+          req = { auth: auth_headers, form: form_attributes, source: source_name, auto_claim: auto_claim.as_json }
           PersonalInformationLog.create(
             error_class: "submit_form526 #{e.class.name}", data: { request: req, error: e.try(:as_json) || e }
           )
@@ -61,7 +61,7 @@ module ClaimsApi
 
         private
 
-        def source
+        def source_name
           request.headers['X-Consumer-Username']
         end
       end
