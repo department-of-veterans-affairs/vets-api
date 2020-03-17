e74312577eb03751d363e3f5176f26a32ba1caa2
keithdadkins <keithdadkins@me.com>
2020-03-17 14:35:57 -0500

    fix linting issues

--- spec/lib/vbms/efolder/service_spec.rb
+++ spec/lib/vbms/efolder/service_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe VBMS::Efolder::Service do
     allow(VBMS::Client).to receive(:from_env_vars).and_return(vbms_client)
   end
 
-  it 'should configure statsd key prefix' do
-    expect(VBMS::Efolder::Service::STATSD_KEY_PREFIX).to eq('api.vbms.efolder')  
+  it 'configures statsd key prefix' do
+    expect(VBMS::Efolder::Service::STATSD_KEY_PREFIX).to eq('api.vbms.efolder')
   end
 
   describe '#client' do
-    it 'should return a VBMS::Client' do
+    it 'returns a VBMS::Client' do
       expect(service.client).to be(VBMS::Client)
     end
   end
@@ -24,10 +24,11 @@ RSpec.describe VBMS::Efolder::Service do
   describe 'statsd helper methods' do
     # set the class name to UploadService to ensure the incrementers convert and use upload_service as keyname
     before { allow(service).to receive(:class).and_return(VBMS::Efolder::UploadService) }
+
     upload_success_key = 'api.vbms.efolder.upload_service.upload.success'
     upload_fail_key = 'api.vbms.efolder.upload_service.upload.fail'
 
-    # ensure 
+    # ensure
     it '#increment_success triggers statsd with correct keyname' do
       expect { service.increment_success(:upload) }.to trigger_statsd_increment(upload_success_key)
       service.increment_success(:upload)
