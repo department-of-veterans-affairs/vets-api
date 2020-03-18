2928731de814b68822759d7a1d7fc64bbd9210cd
Ed Mangimelli <edmangimelli@gmail.com>
2020-03-18 08:48:50 -0500

    add hlr_post.json and some tests

---
+++ modules/appeals_api/spec/config/schemas/hlr_post.json.spec.rb
@@ -0,0 +1,111 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'pp'
+
+describe 'hlr_post JSON Schema', type: :request do
+  let(:json_schema) do
+    JSON.parse(
+      File.read(
+        Rails.root.join('modules', 'appeals_api', 'config', 'schemas', 'hlr_post.json')
+      )
+    )
+  end
+
+  let(:errors) { validator.validate(json).to_a }
+
+  let(:validator) { JSONSchemer.schema(json_schema) }
+
+  let(:json) { hash.as_json }
+
+  it { expect(json_schema).to be_a Hash }
+
+  it { expect(validator).to be_truthy }
+
+  context 'minimum object' do
+    let(:hash) do
+      {
+        data: {
+          type: 'HigherLevelReview',
+          attributes: {
+            informalConference: false,
+            sameOffice: false,
+            legacyOptInApproved: true,
+            benefitType: 'compensation',
+            veteran: { ssn: '123456789' }
+          }
+        },
+        included: [
+          {
+            type: 'ContestableIssue',
+            attributes: {
+              decisionIssueId: 205
+            }
+          }
+        ]
+      }
+    end
+
+    it { expect(errors).to be_empty }
+  end
+
+  context "veteran's address" do
+    let(:hash) do
+      {
+        data: {
+          type: 'HigherLevelReview',
+          attributes: {
+            informalConference: false,
+            sameOffice: false,
+            legacyOptInApproved: true,
+            benefitType: 'compensation',
+            veteran: veteran
+          }
+        },
+        included: [
+          {
+            type: 'ContestableIssue',
+            attributes: {
+              decisionIssueId: 205
+            }
+          }
+        ]
+      }
+    end
+
+    let(:ssn) { '123456789' }
+
+    context 'required address fields (if address fields are present)' do
+      context 'no address fields present' do
+        context 'address is nil' do
+          let(:veteran) { { ssn: ssn, address: nil } }
+
+          it('has errors') { expect(errors).not_to be_empty }
+        end
+
+        context 'address is empty object' do
+          let(:veteran) { { ssn: ssn, address: {} } }
+
+          it('has errors') { expect(errors).not_to be_empty }
+        end
+      end
+
+      context 'address_type: DOMESTIC' do
+        let(:veteran) do
+          {
+            ssn: ssn,
+            address: {
+              "address_type": 'DOMESTIC'
+            }.merge(fields)
+          }
+        end
+
+        context 'no other address fields present' do
+          let(:fields) { {} }
+
+          it('has errors') { expect(errors).not_to be_empty }
+        end
+      end
+    end
+  end
+end
