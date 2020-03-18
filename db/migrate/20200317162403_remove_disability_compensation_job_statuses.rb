928146f39727fc5a0dfd86c826c37527a148127c
Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>
2020-03-18 17:38:40 -0500

    undo db changes, split to new pr

--- db/migrate/20200317162403_remove_disability_compensation_job_statuses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDisabilityCompensationJobStatuses < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :disability_compensation_job_statuses
-  end
-end
--- db/migrate/20200317164309_remove_disability_compensation_submissions.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDisabilityCompensationSubmissions < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :disability_compensation_submissions
-  end
-end
