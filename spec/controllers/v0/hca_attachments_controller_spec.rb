36511ce416fc9b2a66aaa3ff020cdfa0de6cb1bd
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-03-18 12:06:33 -0400

    validate nil form attachment param (#4025)
    * validate nil form attachment namespace param
    * update specs
    * add spec for verifying file upload

--- spec/controllers/v0/hca_attachments_controller_spec.rb
+++ spec/controllers/v0/hca_attachments_controller_spec.rb
@@ -8,8 +8,29 @@ RSpec.describe V0::HcaAttachmentsController, type: :controller do
       post(:create, params: { hca_attachment: {
              file_data: fixture_file_upload('pdf_fill/extras.pdf')
            } })
-
       expect(JSON.parse(response.body)['data']['attributes']['guid']).to eq HcaAttachment.last.guid
+      expect(FormAttachment.last).to be_a(HcaAttachment)
+    end
+
+    it 'validates input parameters' do
+      post(:create)
+      expect(response).to have_http_status(:bad_request)
+      expect(JSON.parse(response.body)['errors'].first['detail'])
+        .to eq('The required parameter "hca_attachment", is missing')
+    end
+
+    it 'validates that the upload attachment is not nil' do
+      file_data = fixture_file_upload('pdf_fill/extras.pdf')
+
+      post(:create, params: { hca_attachment: {
+             file_data: file_data
+           } })
+
+      uploaded_file_path =  HcaAttachment.last.get_file.file
+      file_1 = File.open(uploaded_file_path).read
+      file_2 = File.open(file_data).read
+
+      expect(file_1).to eq(file_2)
     end
   end
 end
