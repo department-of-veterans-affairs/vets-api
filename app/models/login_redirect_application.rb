4cc2b56a4b12b98ad4a0327f2432c4a03b307f68
Eric Buckley <eric.buckley@gmail.com>
2020-03-18 21:35:16 -0700

    adding comments

--- app/models/login_redirect_application.rb
+++ app/models/login_redirect_application.rb
@@ -2,6 +2,12 @@
 
 require 'common/models/redis_store'
 
+# Users logging in via SSOe may need to be redirected back to an external
+# application after authentication rather than the VA.gov home page.  When a
+# authentication request comes in, with the necessary parameter, we can use
+# this Redis namespace to temporarily store a value so that when a matching
+# SAML response comes back from SSOe we know where to redirect the newly
+# authenticated user.
 class LoginRedirectApplication < Common::RedisStore
   redis_store REDIS_CONFIG['login_redirect_application']['namespace']
   redis_ttl REDIS_CONFIG['login_redirect_application']['each_ttl']
