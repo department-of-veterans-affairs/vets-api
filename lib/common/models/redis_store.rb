0033060b6e5476aff189f00ea386813102380c64
Eric Buckley <eric.buckley@gmail.com>
2020-03-18 23:35:43 -0700

    RedisStore.initialize_dup is needed for dup calls

--- lib/common/models/redis_store.rb
+++ lib/common/models/redis_store.rb
@@ -115,6 +115,11 @@ module Common
       count
     end
 
+    def initialize_dup(other)
+      initialize_copy(other)
+      @destroyed = false
+    end
+
     def ttl
       redis_namespace.ttl(attributes[redis_namespace_key])
     end
