4b4f85d3685086e321cbe4b71aabc9ef0d24e182
Eric Buckley <eric.buckley@gmail.com>
2020-03-23 11:19:05 -0700

    moving multiple Account sort with idme priority into its own private method

--- app/models/account.rb
+++ app/models/account.rb
@@ -60,17 +60,7 @@ class Account < ApplicationRecord
     attrs = account_attrs_from_user(user)
 
     accts = idme_uuid_match(attrs[:idme_uuid]).or(sec_id_match(attrs[:sec_id]))
-
-    if accts.length > 1
-      data = accts.map(&:attributes)
-      log_message_to_sentry('multiple Account records with matching ids', 'warning', data)
-      # sort the accounts so the ones with matching idme_uuid values come first
-      # in the array, this will provide the users with a more consistent
-      # experience in case they have multiple credentials to login with
-      # https://github.com/department-of-veterans-affairs/va.gov-team/issues/6702
-      accts = accts.sort_by { |a| a.idme_uuid == attrs[:idme_uuid] ? 0 : 1 }
-    end
-
+    accts = sort_with_idme_uuid_priority(accts)
     accts.length.positive? ? accts[0] : create(**attrs)
   end
 
@@ -111,7 +101,22 @@ class Account < ApplicationRecord
     user.uuid || "sec:#{user.sec_id}"
   end
 
-  private_class_method :account_attrs_from_user, :get_key
+  # Sort the given list of Accounts so the ones with matching ID.me UUID values
+  # come first in the array, this will provide users with a more consistent
+  # experience in the case they have multiple credentials to login with
+  # https://github.com/department-of-veterans-affairs/va.gov-team/issues/6702
+  #
+  # @return [Array]
+  def self.sort_with_idme_uuid_priority(accts)
+    if accts.length > 1
+      data = accts.map(&:attributes)
+      log_message_to_sentry('multiple Account records with matching ids', 'warning', data)
+      accts = accts.sort_by { |a| a.idme_uuid == attrs[:idme_uuid] ? 0 : 1 }
+    end
+    accts
+  end
+
+  private_class_method :account_attrs_from_user, :get_key, :sort_with_idme_uuid_priority
 
   private
 
