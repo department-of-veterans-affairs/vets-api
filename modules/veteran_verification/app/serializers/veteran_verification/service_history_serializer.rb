877291f6c1fa268e8e9e5421191c9c4470d42945
dbrown-va <derek.brown5@va.gov>
2020-03-23 16:29:26 -0400

    Update to Concatenate Pay Plan and Pay Grade

--- modules/veteran_verification/app/serializers/veteran_verification/service_history_serializer.rb
+++ modules/veteran_verification/app/serializers/veteran_verification/service_history_serializer.rb
@@ -2,7 +2,7 @@
 
 module VeteranVerification
   class ServiceHistorySerializer < ActiveModel::Serializer
-    attributes :branch_of_service, :start_date, :end_date, :pay_grade_code,
+    attributes :branch_of_service, :start_date, :end_date, :pay_grade,
                :discharge_status, :separation_reason, :deployments
     type 'service_history_episodes'
   end
