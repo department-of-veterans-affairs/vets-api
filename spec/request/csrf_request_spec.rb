279ef0cef6573e05022d15316596d280e75a02a0
Johnny Holton <johnny@holton.co>
2020-03-24 12:06:03 -0400

    Remove CSRF attachment spec.  It's now covered by the POST spec

--- spec/request/csrf_request_spec.rb
+++ spec/request/csrf_request_spec.rb
@@ -76,35 +76,6 @@ RSpec.describe 'CSRF scenarios', type: :request do
     end
   end
 
-  # POST attachment
-  describe 'HcaAttachmentsController#create' do
-    context 'with a CSRF token' do
-      it 'uploads an hca attachment' do
-        post(v0_hca_attachments_path,
-             params: { hca_attachment: { file_data: fixture_file_upload('pdf_fill/extras.pdf') } },
-             headers: { 'X-CSRF-Token' => @token })
-
-        expect(JSON.parse(response.body)['data']['attributes']['guid']).to eq HcaAttachment.last.guid
-      end
-    end
-
-    context 'without a CSRF token' do
-      it 'logs the info to sentry' do
-        expect(Raven).to receive(:capture_message).once
-        post(v0_hca_attachments_path,
-              params: { hca_attachment: { file_data: fixture_file_upload('pdf_fill/extras.pdf') } })
-      end
-
-      it 'returns an error' do
-        skip 'this should be live test when CSRF is enforced'
-        # post(v0_hca_attachments_path,
-        #   params: { hca_attachment: { file_data: fixture_file_upload('pdf_fill/extras.pdf') } }
-        # )
-        # expect(response.status).to eq 500
-      end
-    end
-  end
-
   # SAML callback
   describe 'POST SAML callback' do
     context 'without a CSRF token' do
