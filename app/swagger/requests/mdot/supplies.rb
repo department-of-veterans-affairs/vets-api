10b4c7b01a76a9d73ee1fcdc38b84483cde4df70
Lihan Li <frankieteardrop@gmail.com>
2020-03-31 08:18:25 -0700

    add mdot supply swagger spec stub

---
+++ app/swagger/requests/mdot/supplies.rb
@@ -0,0 +1,28 @@
+module Swagger
+  module Requests
+    module MDOT
+      class Supplies
+        include Swagger::Blocks
+
+        swagger_path '/v0/mdot/supplies' do
+          operation :post do
+            key :description, 'Create a MDOT supply order'
+            key :operationId, 'addMdotOrder'
+            key :tags, %w[mdot]
+
+            response 200 do
+              key :description, 'mdot order response'
+
+              schema do
+                key :required, %i[status order_id]
+
+                property :status, type: :string
+                property :order_id, type: :string
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
