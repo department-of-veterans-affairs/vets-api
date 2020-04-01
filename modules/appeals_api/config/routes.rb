fc31c8a322df5bf8d5f6231cb2c766012d29191d
Ed Mangimelli <edmangimelli@gmail.com>
2020-04-01 14:08:06 -0500

    remove erroneous v0 routes

--- modules/appeals_api/config/routes.rb
+++ modules/appeals_api/config/routes.rb
@@ -5,14 +5,7 @@ AppealsApi::Engine.routes.draw do
   match '/v0/*path', to: 'application#cors_preflight', via: [:options]
 
   namespace :v0, defaults: { format: 'json' } do
-    resources :appeals, only: [:index] do
-      collection do
-        get 'higher_level_reviews/:uuid', to: 'appeals#show_higher_level_review'
-        post 'higher_level_reviews', to: 'appeals#create_higher_level_review'
-        get 'intake_statuses/:intake_id', to: 'appeals#show_intake_status'
-      end
-    end
-
+    resources :appeals, only: [:index]
     get 'healthcheck', to: 'appeals#healthcheck'
   end
 
