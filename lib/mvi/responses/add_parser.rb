1e95de78297fb8f878331bba8b911e67f550094b
Silvio Luthi <silvio@adhocteam.us>
2020-04-06 11:14:52 -0700

    Add id parsing to add_parser so its consumable

--- lib/mvi/responses/add_parser.rb
+++ lib/mvi/responses/add_parser.rb
@@ -56,10 +56,32 @@ module MVI
         raw_codes = locate_elements(@original_body, ACKNOWLEDGEMENT_DETAIL_CODE_XPATH)
         return [] unless raw_codes
 
-        raw_codes.map(&:attributes)
+        attributes = raw_codes.map(&:attributes)
+        parse_ids(attributes)
       end
 
       private
+
+      def parse_ids(attributes)
+        codes = { other: [] }
+        attributes.each do |attribute|
+          if attribute[:code].include? 'BRLS'
+            codes[:birls_id] = sanitize_ids(attribute[:code])
+          elsif attribute[:code].include? 'CORP'
+            codes[:participant_id] = sanitize_ids(attribute[:code])
+          else
+            codes[:other].append(attribute)
+          end
+        end
+        codes.delete(:other) if codes[:other].empty?
+        codes
+      end
+
+      def sanitize_ids(raw_id)
+        return if raw_id.nil?
+
+        raw_id.match(/^\d+/)&.to_s
+      end
 
       def locate_element(el, path)
         locate_elements(el, path)&.first
