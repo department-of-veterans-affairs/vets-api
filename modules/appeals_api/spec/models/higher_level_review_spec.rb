9680803ac23f2949caa93ce49f9c00b67677ba8e
Ed Mangimelli <38672641+edmangimelli@users.noreply.github.com>
2020-04-09 15:43:38 -0500

    Appeals API - HLR Submission Persistence (#4088)
    * WIP
    
    * WIP
    
    * add HlrSubmission model
    
    * clean up HlrSubmission
    
    * restore FileData to Claims (premature)
    
    * require FileData
    
    * add migration
    
    * change names
    
    * WIP
    
    * remove file_data (Charley suggestion)
    
    * add test
    
    * typo
    
    * revert schema.rb
    
    * add another receiptDate test
    
    * typo
    
    * migrate
    
    * tweak engine to run migrations
    
    * make ClaimsApi's engine use Lighthouse::Engine too
    
    * restore claims engine
    
    * base AppealApi::Engine off of ClaimsApi::Engine
    
    * remove valid?
    
    * create model in controller
    
    * renamm HigherLevelReviewSubmission to HigherLevelReview

---
+++ modules/appeals_api/spec/models/higher_level_review_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe AppealsApi::HigherLevelReview, type: :model do
+  let(:higher_level_review) do
+    AppealsApi::HigherLevelReview.new(
+      form_data: form_data,
+      auth_headers: auth_headers
+    )
+  end
+
+  let(:auth_headers) { {} }
+  let(:form_data) { default_form_data }
+  let(:default_form_data) do
+    JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/valid_200996.json"
+  end
+
+  describe '#receipt_date' do
+    subject { higher_level_review.receipt_date }
+
+    context 'new hlr' do
+      context 'receiptDate not given' do
+        let(:form_data) do
+          json = default_form_data
+          json['data']['attributes'] = json['data']['attributes'].except 'receiptDate'
+          json
+        end
+
+        it 'uses today\'s date' do
+          expect(subject.strftime('%F')).to eq Time.now.utc.strftime('%F')
+        end
+      end
+
+      context 'receiptDate given' do
+        let(:date_string) { '2020-02-02' }
+        let(:form_data) do
+          json = default_form_data
+          json['data']['attributes']['receiptDate'] = date_string
+          json
+        end
+
+        it 'pulls date from form_data' do
+          expect(subject.strftime('%F')).to eq date_string
+        end
+      end
+    end
+  end
+end
