a06118cea5d5742a45c0aba6bd6c006a93036caf
Johnny Holton <johnny@oddball.io>
2020-04-10 16:15:07 -0400

    Remove SSL configuration and certs from HCA configuration. (#4117)
    * Remove SSL configuration and certs from HCA configuration.

--- lib/hca/configuration.rb
+++ lib/hca/configuration.rb
@@ -6,25 +6,8 @@ require 'common/client/middleware/response/soap_parser'
 
 module HCA
   class Configuration < Common::Client::Configuration::SOAP
-    def self.cert_store(paths)
-      store = OpenSSL::X509::Store.new
-      Array(paths).each do |path|
-        store.add_file(Rails.root.join('config', 'health_care_application', 'certs', path).to_s)
-      end
-      store
-    end
-
     HEALTH_CHECK_ID = 377_609_264
     WSDL = Rails.root.join('config', 'health_care_application', 'wsdl', 'voa.wsdl')
-    CERT_STORE = Settings.hca.ca.present? ? cert_store(Settings.hca.ca) : nil
-
-    def self.ssl_cert_path
-      Settings.hca.cert_path
-    end
-
-    def self.ssl_key_path
-      Settings.hca.key_path
-    end
 
     def base_path
       Settings.hca.endpoint
@@ -34,20 +17,8 @@ module HCA
       'HCA'
     end
 
-    def ssl_options
-      ssl = {
-        verify: true
-      }
-      ssl[:cert_store] = HCA::Configuration::CERT_STORE if HCA::Configuration::CERT_STORE
-      if ssl_cert && ssl_key
-        ssl[:client_cert] = ssl_cert
-        ssl[:client_key] = ssl_key
-      end
-      ssl
-    end
-
     def connection
-      Faraday.new(base_path, headers: base_request_headers, request: request_options, ssl: ssl_options) do |conn|
+      Faraday.new(base_path, headers: base_request_headers, request: request_options) do |conn|
         conn.use :breakers
         conn.options.open_timeout = 10
         conn.options.timeout = Settings.hca.timeout
