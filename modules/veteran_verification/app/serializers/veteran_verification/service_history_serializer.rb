872228435e4ad6580eaff6ec7271509fef62857c
dbrown-va <derek.brown5@va.gov>
2020-04-10 11:18:52 -0400

    Corrected lint error

--- modules/veteran_verification/app/serializers/veteran_verification/service_history_serializer.rb
+++ modules/veteran_verification/app/serializers/veteran_verification/service_history_serializer.rb
@@ -2,7 +2,7 @@
 
 module VeteranVerification
   class ServiceHistorySerializer < ActiveModel::Serializer
-    attributes :first_name, :last_name,:branch_of_service,
+    attributes :first_name, :last_name, :branch_of_service,
                :start_date, :end_date, :pay_grade,
                :discharge_status, :separation_reason, :deployments
     type 'service_history_episodes'
