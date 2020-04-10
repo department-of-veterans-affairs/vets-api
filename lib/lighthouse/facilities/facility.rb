7f6592f5e50965777f33fe96acc4fb63376f17f3
Nick Fasulo <nrfasulo@gmail.com>
2020-04-10 14:55:28 -0500

    Refactor for rubocop

--- lib/lighthouse/facilities/facility.rb
+++ lib/lighthouse/facilities/facility.rb
@@ -27,24 +27,14 @@ module Lighthouse
       attribute :unique_id, String
 
       def initialize(fac)
+        fac['attributes'].each_key do |key|
+          self[key] = fac['attributes'][key] if attributes.include?(key.to_sym)
+        end
+
         self.id = fac['id']
         self.type = fac['type']
-        self.name = fac['attributes']['name']
-        self.facility_type = fac['attributes']['facility_type']
-        self.classification = fac['attributes']['classification']
-        self.website = fac['attributes']['website']
-        self.lat = fac['attributes']['lat']
-        self.long = fac['attributes']['long']
-        self.address = fac['attributes']['address']
-        self.phone = fac['attributes']['phone']
-        self.hours = fac['attributes']['hours']
-        self.services = fac['attributes']['services']
         self.feedback = fac['attributes']['satisfaction']
         self.access = fac['attributes']['wait_times']
-        self.mobile = fac['attributes']['mobile']
-        self.active_status = fac['attributes']['active_status']
-        self.visn = fac['attributes']['visn']
-        self.operating_status = fac['attributes']['operating_status']
         self.facility_type_prefix, self.unique_id = fac['id'].split('_')
       end
     end
