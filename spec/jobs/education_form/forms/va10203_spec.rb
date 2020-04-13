93a336da64fb14bc5cf4e0145f118c1e81608e15
hughes-dustin <hughes_dustin@bah.com>
2020-04-13 13:37:00 -0400

    delete test

--- spec/jobs/education_form/forms/va10203_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe EducationForm::Forms::VA10203 do
-  subject { described_class.new(education_benefits_claim) }
-  let(:education_benefits_claim) { build(:va10203).education_benefits_claim }
-end
