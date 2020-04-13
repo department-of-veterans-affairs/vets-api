8814a7f93503801cb417a8ca87969edb80b877f3
hughes-dustin <hughes_dustin@bah.com>
2020-04-13 12:25:35 -0400

    create test for 10203 education form

---
+++ spec/jobs/education_form/forms/va10203_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe EducationForm::Forms::VA10203 do
+  subject { described_class.new(education_benefits_claim) }
+
+  let(:education_benefits_claim) { build(:va10203).education_benefits_claim }
+
+  context '#direct_deposit_type' do
+    let(:education_benefits_claim) { create(:va10203_full_form).education_benefits_claim }
+
+    it 'converts internal keys to text' do
+      expect(subject.school['name']).to eq('Test School Name')
+      expect(subject.form_type).to eq('CH33')
+      expect(subject.form_benefit).to eq('STEM')
+      expect(subject.header_form_type).to eq('STEM1995')
+    end
+  end
+end
