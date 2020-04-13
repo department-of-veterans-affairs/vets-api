a2b509a1899768f820bc0342f3ca69fe7a58883b
hughes-dustin <hughes_dustin@bah.com>
2020-04-13 13:33:20 -0400

    remove expectations from test

--- spec/jobs/education_form/forms/va10203_spec.rb
+++ spec/jobs/education_form/forms/va10203_spec.rb
@@ -4,10 +4,5 @@ require 'rails_helper'
 
 RSpec.describe EducationForm::Forms::VA10203 do
   subject { described_class.new(education_benefits_claim) }
-
   let(:education_benefits_claim) { build(:va10203).education_benefits_claim }
-
-  expect(subject.form_type).to eq('CH33')
-  expect(subject.form_benefit).to eq('STEM')
-  expect(subject.header_form_type).to eq('V10203')
 end
