7e33610e225a2426eabe978a88eccd069f4d94f2
hughes-dustin <hughes_dustin@bah.com>
2020-04-13 13:22:38 -0400

    unit test fix

--- spec/jobs/education_form/forms/va10203_spec.rb
+++ spec/jobs/education_form/forms/va10203_spec.rb
@@ -7,13 +7,7 @@ RSpec.describe EducationForm::Forms::VA10203 do
 
   let(:education_benefits_claim) { build(:va10203).education_benefits_claim }
 
-  context '#direct_deposit_type' do
-    let(:education_benefits_claim) { create(:va10203_full_form).education_benefits_claim }
-
-    it 'converts internal keys to text' do
-      expect(subject.form_type).to eq('CH33')
-      expect(subject.form_benefit).to eq('STEM')
-      expect(subject.header_form_type).to eq('V10203')
-    end
-  end
+  expect(subject.form_type).to eq('CH33')
+  expect(subject.form_benefit).to eq('STEM')
+  expect(subject.header_form_type).to eq('V10203')
 end
