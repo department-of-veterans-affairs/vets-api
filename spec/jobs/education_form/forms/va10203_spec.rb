0c4cb37f25699aaeb197ab24ec6656b00fd2e77b
hughes-dustin <hughes_dustin@bah.com>
2020-04-13 18:34:05 -0400

    remove unwanted text from minimal spool

--- spec/jobs/education_form/forms/va10203_spec.rb
+++ spec/jobs/education_form/forms/va10203_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe EducationForm::Forms::VA10203 do
+  subject { described_class.new(education_benefits_claim) }
+
   let(:education_benefits_claim) { build(:va10203).education_benefits_claim }
 
   %w[kitchen_sink minimal].each do |test_application|
