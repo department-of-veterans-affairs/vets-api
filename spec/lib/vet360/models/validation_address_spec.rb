e0d69e2188f3cd9068351d085f8955f5f495898d
Lihan Li <frankieteardrop@gmail.com>
2020-04-14 13:18:29 -0700

    lint

--- spec/lib/vet360/models/validation_address_spec.rb
+++ spec/lib/vet360/models/validation_address_spec.rb
@@ -18,30 +18,28 @@ describe Vet360::Models::ValidationAddress do
 
   describe '#build_from_address_suggestion' do
     let(:address_suggestion_hash) do
-      {"address"=>
-        {"address_line1"=>"898 Broadway W",
-         "city"=>"Vancouver",
-         "international_postal_code"=>"V5Z 1J8",
-         "county"=>{},
-         "state_province"=>{"name"=>"British Columbia", "code"=>"BC"},
-         "country"=>{"name"=>"Canada", "code"=>"CAN", "fips_code"=>"CA", "iso2_code"=>"CA", "iso3_code"=>"CAN"}},
-       "geocode"=>{"calc_date"=>"2020-04-10T17:29:41Z", "location_precision"=>10.0, "latitude"=>49.2635, "longitude"=>-123.13873},
-       "address_meta_data"=>{"confidence_score"=>97.76, "address_type"=>"International", "validation_key"=>-1941145206
-        }
-      }
+      { 'address' =>
+        { 'address_line1' => '898 Broadway W',
+          'city' => 'Vancouver',
+          'international_postal_code' => 'V5Z 1J8',
+          'county' => {},
+          'state_province' => { 'name' => 'British Columbia', 'code' => 'BC' },
+          'country' => { 'name' => 'Canada', 'code' => 'CAN', 'fips_code' => 'CA', 'iso2_code' => 'CA', 'iso3_code' => 'CAN' } },
+        'geocode' => { 'calc_date' => '2020-04-10T17:29:41Z', 'location_precision' => 10.0, 'latitude' => 49.2635, 'longitude' => -123.13873 },
+        'address_meta_data' => { 'confidence_score' => 97.76, 'address_type' => 'International', 'validation_key' => -1_941_145_206 } }
     end
 
     it 'correctly parses international addresses' do
       addr = described_class.build_from_address_suggestion(address_suggestion_hash)
 
       expect(addr.to_h.compact).to eq(
-        {:address_line1=>"898 Broadway W",
-         :address_type=>"INTERNATIONAL",
-         :city=>"Vancouver",
-         :country_name=>"Canada",
-         :country_code_iso3=>"CAN",
-         :international_postal_code=>"V5Z 1J8",
-         :province=>"BC"}
+        { address_line1: '898 Broadway W',
+          address_type: 'INTERNATIONAL',
+          city: 'Vancouver',
+          country_name: 'Canada',
+          country_code_iso3: 'CAN',
+          international_postal_code: 'V5Z 1J8',
+          province: 'BC' }
       )
     end
   end
