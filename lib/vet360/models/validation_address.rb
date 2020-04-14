eefa5a4f5ef0630f841e612dd35264430145de96
Lihan Li <frankieteardrop@gmail.com>
2020-04-14 09:53:52 -0700

    support international province and postal code

--- lib/vet360/models/validation_address.rb
+++ lib/vet360/models/validation_address.rb
@@ -4,6 +4,17 @@ module Vet360
   module Models
     # Model for addresses sent and received from the VA profile address validation API
     class ValidationAddress < BaseAddress
+      def initialize(*args)
+        super
+        # address validation api uses the same field for province and state codes
+        # but our address model wants province used in intl addresses and
+        # state_code used in domestic ones
+        if address_type == INTERNATIONAL
+          self.province ||= state_code
+          self.state_code = nil
+        end
+      end
+
       # Convert a ValidationAddress into a hash that can be sent to the address validation
       # API
       # @return [Hash] hash that is formatted for POSTing to address validation API
@@ -35,7 +46,6 @@ module Vet360
       def self.build_from_address_suggestion(address_suggestion_hash)
         address_hash = address_suggestion_hash['address']
 
-        # add international_postal_code and province for future international support
         new(
           address_line1: address_hash['address_line1'],
           address_line2: address_hash['address_line2'],
@@ -47,6 +57,7 @@ module Vet360
           county_code: address_hash.dig('county', 'county_fips_code'),
           county_name: address_hash.dig('county', 'name'),
           state_code: address_hash['state_province']['code'],
+          international_postal_code: address_hash['international_postal_code'],
           zip_code: address_hash['zip_code5'],
           zip_code_suffix: address_hash['zip_code4']
         )
