c6f3d1e2a1e704889585d2be0994c60fea8f219c
Kevin Mircovich <kevinmircovich@Kevin-Mircovich-MacBook-Pro.local>
2020-04-14 15:21:29 -0400

    remove references to InProgressForm for caregivers_assistance_claim

--- app/services/form1010cg/service.rb
+++ app/services/form1010cg/service.rb
@@ -3,7 +3,7 @@
 # This service manages the interactions between CaregiversAssistanceClaim, CARMA, and Form1010cg::Submission.
 module Form1010cg
   class Service
-    def submit_claim!(user_context, claim_data)
+    def submit_claim!(claim_data)
       claim = SavedClaim::CaregiversAssistanceClaim.new(claim_data)
       claim.valid? || raise(Common::Exceptions::ValidationErrors, claim)
 
@@ -11,14 +11,10 @@ module Form1010cg
 
       carma_submission.submit!
 
-      submission = Form1010cg::Submission.new(
+      Form1010cg::Submission.new(
         carma_case_id: carma_submission.carma_case_id,
         submitted_at: carma_submission.submitted_at
       )
-
-      destroy_previously_saved_form_for(user_context) if user_context
-
-      submission
     end
 
     private
@@ -26,10 +22,6 @@ module Form1010cg
     # Destroy this form it has previously been stored in-progress by this user_context
     def form_schema_id
       SavedClaim::CaregiversAssistanceClaim::FORM
-    end
-
-    def destroy_previously_saved_form_for(user_context)
-      InProgressForm.form_for_user(form_schema_id, user_context)&.destroy
     end
   end
 end
--- spec/services/form1010cg/service_spec.rb
+++ spec/services/form1010cg/service_spec.rb
@@ -7,18 +7,16 @@ RSpec.describe Form1010cg::Service do
   let(:get_schema) { -> { VetsJsonSchema::SCHEMAS['10-10CG'].clone } }
 
   it 'will raise a ValidationErrors when the provided claim is invalid' do
-    user = nil
     invalid_claim_data = { form: {} }
 
     expect(CARMA::Models::Submission).not_to receive(:new)
 
     expect do
-      subject.submit_claim!(user, invalid_claim_data)
+      subject.submit_claim!(invalid_claim_data)
     end.to raise_error(Common::Exceptions::ValidationErrors)
   end
 
   it 'will return a Form1010cg::submission' do
-    user = nil
     claim_data = { form: build_valid_claim_data.call }
     expected = { carma_case_id: 'aB935000000A9GoCAK', submitted_at: DateTime.new }
 
@@ -30,83 +28,10 @@ RSpec.describe Form1010cg::Service do
       expect(carma_submission).to receive(:submitted_at).and_return(expected[:submitted_at])
     }
 
-    submission = subject.submit_claim!(user, claim_data)
+    submission = subject.submit_claim!(claim_data)
 
     expect(submission).to be_an_instance_of(Form1010cg::Submission)
     expect(submission.carma_case_id).to eq(expected[:carma_case_id])
     expect(submission.submitted_at).to eq(expected[:submitted_at])
-  end
-
-  context 'with user context' do
-    it 'will delete the related in progress form' do
-      user = double(uuid: SecureRandom.uuid)
-      claim_data = { form: build_valid_claim_data.call }
-      expected = { carma_case_id: 'aB935000000A9GoCAK', submitted_at: DateTime.new }
-
-      # Related in progress form (should be destroyed)
-      previously_saved_form = build(
-        :in_progress_form,
-        form_id: '10-10CG',
-        form_data: { name: 'kevin' },
-        user_uuid: user.uuid
-      )
-
-      expect(InProgressForm).to receive(:form_for_user).and_return(previously_saved_form)
-      expect(previously_saved_form).to receive(:destroy)
-
-      # Unrelated in progress forms (should not be destroyed)
-      other_form_for_user = create(
-        :in_progress_form,
-        form_id: '22-1990',
-        form_data: { name: 'kevin' },
-        user_uuid: user.uuid
-      )
-
-      same_form_for_different_user = create(
-        :in_progress_form,
-        form_id: '10-10CG',
-        form_data: { name: 'not-kevin' },
-        user_uuid: SecureRandom.uuid
-      )
-
-      expect(other_form_for_user).not_to receive(:destroy)
-      expect(same_form_for_different_user).not_to receive(:destroy)
-
-      carma_submission = double
-
-      expect(CARMA::Models::Submission).to receive(:from_claim).and_return(carma_submission)
-      expect(carma_submission).to receive(:submit!) {
-        expect(carma_submission).to receive(:carma_case_id).and_return(expected[:carma_case_id])
-        expect(carma_submission).to receive(:submitted_at).and_return(expected[:submitted_at])
-      }
-
-      submission = subject.submit_claim!(user, claim_data)
-
-      expect(submission).to be_an_instance_of(Form1010cg::Submission)
-      expect(submission.carma_case_id).to eq(expected[:carma_case_id])
-      expect(submission.submitted_at).to eq(expected[:submitted_at])
-    end
-
-    it 'will function when no related in progress form exists' do
-      user = double(uuid: SecureRandom.uuid)
-      claim_data = { form: build_valid_claim_data.call }
-      expected = { carma_case_id: 'aB935000000A9GoCAK', submitted_at: DateTime.new }
-
-      expect_any_instance_of(InProgressForm).not_to receive(:destroy)
-
-      carma_submission = double
-
-      expect(CARMA::Models::Submission).to receive(:from_claim).and_return(carma_submission)
-      expect(carma_submission).to receive(:submit!) {
-        expect(carma_submission).to receive(:carma_case_id).and_return(expected[:carma_case_id])
-        expect(carma_submission).to receive(:submitted_at).and_return(expected[:submitted_at])
-      }
-
-      submission = subject.submit_claim!(user, claim_data)
-
-      expect(submission).to be_an_instance_of(Form1010cg::Submission)
-      expect(submission.carma_case_id).to eq(expected[:carma_case_id])
-      expect(submission.submitted_at).to eq(expected[:submitted_at])
-    end
   end
 end
