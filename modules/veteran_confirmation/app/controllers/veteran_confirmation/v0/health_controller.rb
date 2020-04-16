084bcbfe6e558b59a4d6bc482e10b1ed062849a9
esubocz <emily.subocz@libertyits.com>
2020-04-16 15:34:52 -0400

    use application controller dependency

--- modules/veteran_confirmation/app/controllers/veteran_confirmation/v0/health_controller.rb
+++ modules/veteran_confirmation/app/controllers/veteran_confirmation/v0/health_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
+require_dependency 'veteran_confirmation/application_controller'
+
 module VeteranConfirmation
   module V0
     # HealthController returns a JSON payload confirmation that the Veteran Confirmation application is
     # properly up and running.
     class HealthController < ApplicationController
-      skip_before_action(:authenticate, raise: false)
-
       def index
         render json: {
           UP: true
