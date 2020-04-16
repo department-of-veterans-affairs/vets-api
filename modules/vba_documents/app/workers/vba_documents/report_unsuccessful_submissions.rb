a5bda7ab6193861e69887f88f50b915f086bca9b
Michael Bastos <bastosmichael@gmail.com>
2020-04-16 08:30:12 -0500

    rubocop fix

--- modules/vba_documents/app/workers/vba_documents/report_unsuccessful_submissions.rb
+++ modules/vba_documents/app/workers/vba_documents/report_unsuccessful_submissions.rb
@@ -8,29 +8,37 @@ module VBADocuments
 
     def perform
       if Settings.vba_documents.unsuccessful_report_enabled
-        to = Time.zone.now
-        from = to.monday? ? 7.days.ago : 1.day.ago
-        errored_submissions = VBADocuments::UploadSubmission.where(
-          created_at: from..to,
-          status: %w[error expired]
-        )
-        stuck_submissions = VBADocuments::UploadSubmission.where(
-          created_at: from..to,
-          status: 'uploaded'
-        )
+        @to = Time.zone.now
+        @from = to.monday? ? 7.days.ago : 1.day.ago
+        @consumers = VBADocuments::UploadSubmission.where(created_at: @from..@to).pluck(:consumer_name).uniq
 
-        consumer_totals = VBADocuments::UploadSubmission.where(created_at: from..to).pluck(:consumer_name).uniq.map do |consumer_name|
-          counts = VBADocuments::UploadSubmission.where(created_at: from..to, consumer_name: consumer_name).group(:status).count
-          totals = counts.sum{|k,v| v }
-          {
-            consumer_name => counts.merge( totals: totals, 
-                                           error_rate: "#{(100.0/totals*counts['error']).round}%", 
-                                           expired_rate: "#{(100.0/totals*counts['expired']).round}%"
-                                          )
-          }
-        end
+        VBADocuments::UnsuccessfulReportMailer.build(totals, stuck, errored, @from, @to).deliver_now
+      end
+    end
 
-        VBADocuments::UnsuccessfulReportMailer.build(consumer_totals, stuck_submissions, errored_submissions, from, to).deliver_now
+    def errored
+      VBADocuments::UploadSubmission.where(
+        created_at: @from..@to,
+        status: %w[error expired]
+      )
+    end
+
+    def stuck
+      VBADocuments::UploadSubmission.where(
+        created_at: @from..@to,
+        status: 'uploaded'
+      )
+    end
+
+    def totals
+      @consumers.map do |name|
+        counts = VBADocuments::UploadSubmission.where(created_at: @from..@to, consumer_name: name).group(:status).count
+        totals = counts.sum { |_k, v| v }
+        {
+          consumer_name => counts.merge(totals: totals,
+                                        error_rate: "#{(100.0 / totals * counts['error']).round}%",
+                                        expired_rate: "#{(100.0 / totals * counts['expired']).round}%")
+        }
       end
     end
   end
