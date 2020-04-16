a50d4200c1ac536a1ad6d09b47e244ffbaafd341
Lihan Li <frankieteardrop@gmail.com>
2020-04-16 11:39:08 -0700

    split build_from_address_suggestion method and add domestic address parsing spec

--- lib/vet360/models/validation_address.rb
+++ lib/vet360/models/validation_address.rb
@@ -30,40 +30,39 @@ module Vet360
         )
       end
 
-      # rubocop:disable Metrics/MethodLength
       # @return [Vet360::Models::ValidationAddress] validation address model created from
       #   address validation API response
       def self.build_from_address_suggestion(address_suggestion_hash)
         address_hash = address_suggestion_hash['address']
+        address_type = address_suggestion_hash['address_meta_data']['address_type'].upcase
         attributes = {
           address_line1: address_hash['address_line1'],
           address_line2: address_hash['address_line2'],
           address_line3: address_hash['address_line3'],
-          address_type: address_suggestion_hash['address_meta_data']['address_type'].upcase,
+          address_type: address_type,
           city: address_hash['city'],
           country_name: address_hash['country']['name'],
           country_code_iso3: address_hash['country']['iso3_code']
-        }
-
-        attributes.merge!(
-          if attributes[:address_type] == INTERNATIONAL
-            {
-              province: address_hash['state_province']['name'],
-              international_postal_code: address_hash['international_postal_code']
-            }
-          else
-            {
-              state_code: address_hash['state_province']['code'],
-              county_code: address_hash.dig('county', 'county_fips_code'),
-              county_name: address_hash.dig('county', 'name'),
-              zip_code: address_hash['zip_code5'], zip_code_suffix: address_hash['zip_code4']
-            }
-          end
-        )
+        }.merge(regional_attributes(address_type, address_hash))
 
         new(attributes)
       end
-      # rubocop:enable Metrics/MethodLength
+
+      def self.regional_attributes(address_type, address_hash)
+        if address_type == INTERNATIONAL
+          {
+            province: address_hash['state_province']['name'],
+            international_postal_code: address_hash['international_postal_code']
+          }
+        else
+          {
+            state_code: address_hash['state_province']['code'],
+            county_code: address_hash.dig('county', 'county_fips_code'),
+            county_name: address_hash.dig('county', 'name'),
+            zip_code: address_hash['zip_code5'], zip_code_suffix: address_hash['zip_code4']
+          }
+        end
+      end
     end
   end
 end
