aa769a71a9f430c0c22df4633a84b01202578984
dbrown-va <derek.brown5@va.gov>
2020-04-16 09:54:37 -0400

    Added a simple health check to veteran_verification API

---
+++ modules/veteran_verification/app/controllers/veteran_verification/v0/health_controller.rb
@@ -0,0 +1,15 @@
+
+module VeteranVerification
+  module V0
+    # HealthController returns a simple health response
+    class HealthController < ApplicationController
+      skip_before_action(:authenticate)
+
+      def index
+        render json: {
+          UP: true
+        }
+      end
+    end
+  end
+end
