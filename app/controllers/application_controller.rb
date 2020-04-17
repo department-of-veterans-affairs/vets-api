926f7ed6c4adc450499410db97c9b40e5c401ec1
Philip Becker <philip.becker@oddball.io>
2020-04-17 13:43:00 -0400

    Update application_controller.rb
    remove brackets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,7 +60,7 @@ class ApplicationController < ActionController::API
   def set_csrf_header
     token = form_authenticity_token
     response.set_header('X-CSRF-Token', token)
-    Rails.logger.info('CSRF response token', { csrf_token: token })
+    Rails.logger.info('CSRF response token', csrf_token: token )
   end
 
   # returns a Bad Request if the incoming host header is unsafe.
