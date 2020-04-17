2c4edc722e8e5c23c3cdc3c69bb576b29b71fe77
Kevin Mircovich <kevinmircovich@Kevin-Mircovich-MacBook-Pro.local>
2020-04-16 20:18:44 -0400

    Add Veteran and Caregiver to CARMA::Submission::Metadata with attr that pase to request objects

--- lib/carma/models/submission.rb
+++ lib/carma/models/submission.rb
@@ -13,7 +13,7 @@ module CARMA
       #
       # @param claim [CaregiversAssistanceClaim] A validated CaregiversAssistanceClaim
       # @param metadata [Hash] Additional data that can be submitted along with the claim's form data
-      # @return [CARMA::Models::Submission] A persisted instance of Account
+      # @return [CARMA::Models::Submission] A CARMA Submission model object
       #
       def self.from_claim(claim, metadata = {})
         new(
--- lib/carma/models/submission/metadata.rb
+++ lib/carma/models/submission/metadata.rb
@@ -4,12 +4,42 @@ module CARMA
   module Models
     class Submission
       class Metadata < CARMA::Models::Base
-        request_payload_key :claim_id
+        request_payload_key :claim_id,
+                            :veteran,
+                            :primary_caregiver,
+                            :secondary_caregiver_one,
+                            :secondary_caregiver_two
 
-        attr_reader :claim_id
+        attr_accessor       :claim_id
+
+        attr_reader         :veteran,
+                            :primary_caregiver,
+                            :secondary_caregiver_one,
+                            :secondary_caregiver_two
 
         def initialize(args = {})
           @claim_id = args[:claim_id]
+
+          self.veteran = args[:veteran] || {}
+          self.primary_caregiver = args[:primary_caregiver] || {}
+          self.secondary_caregiver_one = args[:secondary_caregiver_one] if args[:secondary_caregiver_one]
+          self.secondary_caregiver_two = args[:secondary_caregiver_two] if args[:secondary_caregiver_two]
+        end
+
+        def veteran=(args = {})
+          @veteran = Metadata::Veteran.new(args)
+        end
+
+        def primary_caregiver=(args = {})
+          @primary_caregiver = Metadata::Caregiver.new(args)
+        end
+
+        def secondary_caregiver_one=(args = {})
+          @secondary_caregiver_one = Metadata::Caregiver.new(args)
+        end
+
+        def secondary_caregiver_two=(args = {})
+          @secondary_caregiver_two = Metadata::Caregiver.new(args)
         end
       end
     end
