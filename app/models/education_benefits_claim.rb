c53833e09a1f6902be5970c59af8058990fd1408
hughes-dustin <hughes_dustin@bah.com>
2020-04-20 10:46:32 -0400

    fix merge errors

--- app/models/education_benefits_claim.rb
+++ app/models/education_benefits_claim.rb
@@ -3,7 +3,7 @@
 require 'attr_encrypted'
 class EducationBenefitsClaim < ApplicationRecord
   if Flipper.enabled?(:edu_benefits_stem_scholarship)
-    FORM_TYPES = %w[1990 1995 1990e 5490 5495 1990n 0993 0994].freeze
+    FORM_TYPES = %w[1990 1995 1990e 5490 5495 1990n 0993 0994 10203].freeze
     FORM_HEADERS = %w[
       22-1990 22-1995 22-1990e 22-5490 22-5495 22-1990n 22-0993 22-0994
     ].freeze
@@ -14,7 +14,7 @@ class EducationBenefitsClaim < ApplicationRecord
     ].freeze
   end
   # TODO: TO BE REMOVED IN NEXT PR WHEN REPORTING CAN INCLUDE 10203
-  FORM_TYPES_EXCLUDES_10203 = %w[1990 1995 1990e 5490 5495 1990n 0993 0994 1995s].freeze
+  FORM_TYPES_EXCLUDES_10203 = %w[1990 1995 1990e 5490 5495 1990n 0993 0994].freeze
 
   APPLICATION_TYPES = %w[
     chapter33
