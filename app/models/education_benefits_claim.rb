7f14f8cbdbfb062c63f9de8fc03131dd9e95ceda
hughes-dustin <hughes_dustin@bah.com>
2020-04-20 13:19:58 -0400

    include 10203 in csv_array

--- app/models/education_benefits_claim.rb
+++ app/models/education_benefits_claim.rb
@@ -5,7 +5,7 @@ class EducationBenefitsClaim < ApplicationRecord
   if Flipper.enabled?(:edu_benefits_stem_scholarship)
     FORM_TYPES = %w[1990 1995 1990e 5490 5495 1990n 0993 0994 10203].freeze
     FORM_HEADERS = %w[
-      22-1990 22-1995 22-1990e 22-5490 22-5495 22-1990n 22-0993 22-0994
+      22-1990 22-1995 22-1990e 22-5490 22-5495 22-1990n 22-0993 22-0994 22-10203
     ].freeze
   else
     FORM_TYPES = %w[1990 1995 1990e 5490 5495 1990n 0993 0994 1995s].freeze
