ab5219b5999e4ed6882f00f9d848b2fc3c72466c
Wyatt Walter <wyattwalter@gmail.com>
2020-04-22 16:23:41 -0500

    add config.hosts variable for rails 6

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module VetsAPI
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.hosts = Settings.virtual_hosts
+    
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
