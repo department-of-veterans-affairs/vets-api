48eef8010485a9608d6dede8999f0912b60b1316
Michael Bastos <bastosmichael@gmail.com>
2020-04-28 11:41:42 -0500

    rubocop fix

--- spec/lib/caseflow/error_middleware_spec.rb
+++ spec/lib/caseflow/error_middleware_spec.rb
@@ -5,7 +5,7 @@ require 'caseflow/middleware/errors'
 
 describe Caseflow::Middleware::Errors do
   it 'handles errors' do
-    env = OpenStruct.new("success?" => false, status: 'status', body: {})
+    env = OpenStruct.new('success?' => false, status: 'status', body: {})
     expect do
       described_class.new.on_complete(env)
     end.to change { env[:body]['source'] }.from(nil).to('Appeals Caseflow')
