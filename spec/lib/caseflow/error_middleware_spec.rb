fee7ef19a8739e6f31f46d5713818d2e73bbb286
Michael Bastos <bastosmichael@gmail.com>
2020-04-28 11:34:08 -0500

    adding final test

---
+++ spec/lib/caseflow/error_middleware_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'caseflow/middleware/errors'
+
+describe Caseflow::Middleware::Errors do
+  it 'handles errors' do
+    env = OpenStruct.new("success?" => false, status: 'status', body: {})
+    expect do
+      described_class.new.on_complete(env)
+    end.to change { env[:body]['source'] }.from(nil).to('Appeals Caseflow')
+  end
+end
