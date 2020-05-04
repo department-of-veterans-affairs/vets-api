89a05245b55a6ec9c144ddf1a333a1c9b1d41e11
esubocz <emily.subocz@libertyits.com>
2020-05-04 18:32:05 -0400

    add one rating and no ratings specs

--- modules/veteran_verification/spec/models/disability_rating_spec.rb
+++ modules/veteran_verification/spec/models/disability_rating_spec.rb
@@ -5,14 +5,31 @@ require 'rails_helper'
 describe VeteranVerification::DisabilityRating do
   let(:user) { build(:openid_user, identity_attrs: build(:user_identity_attrs, :loa3)) }
 
-  describe '#formatted_episodes' do
-    it 'returns service history and deployments' do
+  describe '#formatted_ratings' do
+    it 'returns overall rating and list of ratings' do
       VCR.use_cassette('bgs/rating_web_service/rating_data') do
         result = described_class.for_user(user)
         expect(result[:overall_disability_rating]).to eq('100')
         expect(result[:ratings][0][:decision]).to eq('Service Connected')
         expect(result[:ratings][0][:effective_date]).to eq('01012005')
         expect(result[:ratings][0][:rating_percentage]).to eq('100')
+      end
+    end
+
+    it 'returns overall rating and one rating' do
+      VCR.use_cassette('bgs/rating_web_service/rating_data_single_rating') do
+        result = described_class.for_user(user)
+        expect(result[:overall_disability_rating]).to eq('100')
+        expect(result[:ratings][0][:decision]).to eq('Service Connected')
+        expect(result[:ratings][0][:effective_date]).to eq('01012005')
+        expect(result[:ratings][0][:rating_percentage]).to eq('100')
+      end
+    end
+
+    it 'returns overall rating and one rating' do
+      VCR.use_cassette('bgs/rating_web_service/rating_data_no_ratings') do
+        result = described_class.for_user(user)
+        expect(result[:overall_disability_rating]).to eq('100')
       end
     end
   end
