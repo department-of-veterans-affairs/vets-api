ce8a9e624b71c7191e1e0a42bd7b011ddba8c519
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-05-05 09:21:16 -0400

    utilize VCR cassette response strings for external service call stubs

--- spec/service_consumers/provider_states.rb
+++ spec/service_consumers/provider_states.rb
@@ -9,9 +9,13 @@ WebMock.allow_net_connect!
 Pact.provider_states_for 'HCA' do
   provider_state 'enrollment service is up' do
     set_up do
-      stub_request(:post, Settings.hca.endpoint).to_return(
-        body: '<?xml version=\'1.0\' encoding=\'UTF-8\'?><S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/"><S:Body><submitFormResponse xmlns:ns2="http://jaxws.webservices.esr.med.va.gov/schemas" xmlns="http://va.gov/schema/esr/voa/v1"><status>100</status><formSubmissionId>40124668140</formSubmissionId><message><type>Form successfully received for EE processing</type></message><timeStamp>2016-05-25T04:59:39.345-05:00</timeStamp></submitFormResponse></S:Body></S:Envelope>'
-      )
+      
+      response_body_stub = 
+        YAML.load_file(
+          'spec/support/vcr_cassettes/hca/submit_anon.yml'
+        )['http_interactions'][0]['response']['body']['string']
+      
+      stub_request(:post, Settings.hca.endpoint).to_return( body: response_body_stub )
     end
 
     tear_down do
