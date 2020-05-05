83d69964a6caeac849b1913fb5cc8afa28f00a0d
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-05-05 09:40:56 -0400

    linting

--- spec/service_consumers/provider_states.rb
+++ spec/service_consumers/provider_states.rb
@@ -9,13 +9,12 @@ WebMock.allow_net_connect!
 Pact.provider_states_for 'HCA' do
   provider_state 'enrollment service is up' do
     set_up do
-      
-      response_body_stub = 
+      response_body_stub =
         YAML.load_file(
           'spec/support/vcr_cassettes/hca/submit_anon.yml'
         )['http_interactions'][0]['response']['body']['string']
-      
-      stub_request(:post, Settings.hca.endpoint).to_return( body: response_body_stub )
+
+      stub_request(:post, Settings.hca.endpoint).to_return(body: response_body_stub)
     end
 
     tear_down do
