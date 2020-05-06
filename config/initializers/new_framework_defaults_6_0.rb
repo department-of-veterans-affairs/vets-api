680270cb2a8eb0b85ee460a52145c574ce5c3886
John Paul (JP) Ashenfelter <johnpaul.ashenfelter@oddball.io>
2020-05-06 15:38:55 -0400

    Use rails6 activejob configuration defaults

--- config/initializers/new_framework_defaults_6_0.rb
+++ config/initializers/new_framework_defaults_6_0.rb
@@ -20,9 +20,6 @@
 # Change the return value of `ActionDispatch::Response#content_type` to Content-Type header without modification.
 # Rails.application.config.action_dispatch.return_only_media_type_on_content_type = false
 
-# Return false instead of self when enqueuing is aborted from a callback.
-# Rails.application.config.active_job.return_false_on_aborted_enqueue = true
-
 # Send Active Storage analysis and purge jobs to dedicated queues.
 # Rails.application.config.active_storage.queues.analysis = :active_storage_analysis
 # Rails.application.config.active_storage.queues.purge    = :active_storage_purge
