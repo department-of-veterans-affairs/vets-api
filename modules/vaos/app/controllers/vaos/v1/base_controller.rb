3136ffabcba66c9ab01858b83311f9f0a75302d6
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-05-06 16:15:09 -0700

    add global scope to base controller parent

--- modules/vaos/app/controllers/vaos/v1/base_controller.rb
+++ modules/vaos/app/controllers/vaos/v1/base_controller.rb
@@ -12,7 +12,7 @@ module VAOS
     #       class OrganizationsController < BaseController
     #         def index...
     #
-    class BaseController < VAOS::BaseController
+    class BaseController < ::VAOS::BaseController
       before_action :authorize
 
       private
