70393004bdee60644501d7a1c8af80498ebc13e7
Daniel Shawkey <shawkey_daniel@bah.com>
2020-05-11 15:27:06 -0400

    reorder alphabetical

--- app/mailers/spool_submissions_report_mailer.rb
+++ app/mailers/spool_submissions_report_mailer.rb
@@ -5,10 +5,10 @@ class SpoolSubmissionsReportMailer < ApplicationMailer
   RECIPIENTS = %w[
     dana.kuykendall@va.gov
     Jennifer.Waltz2@va.gov
+    kathleen.dalfonso@va.gov
     lihan@adhocteam.us
     Ricardo.DaSilva@va.gov
     shay.norton@va.gov
-    kathleen.dalfonso@va.gov
   ].freeze
 
   STEM_RECIPIENTS = %w[
