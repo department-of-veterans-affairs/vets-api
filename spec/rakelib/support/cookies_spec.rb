a9e4d2d44f1fcf34f8d51a9e77ad72c9ed0d9cc4
Philip Becker <philip.becker@oddball.io>
2020-05-12 10:06:34 -0400

    remove uneccessary assignment

--- spec/rakelib/support/cookies_spec.rb
+++ spec/rakelib/support/cookies_spec.rb
@@ -13,12 +13,11 @@ describe Cookies do
     # much of this code comes from here: https://gist.github.com/wildjcrt/6359713fa770d277927051fdeb30ebbf
     def decrypt_session_cookie(cookie)
       config = Rails.application.config
-      salt   = config.action_dispatch.authenticated_encrypted_cookie_salt
       encrypted_cookie_cipher = config.action_dispatch.encrypted_cookie_cipher || 'aes-256-gcm'
 
       key_generator = ActiveSupport::KeyGenerator.new(Rails.application.secret_key_base, iterations: 1000)
       key_len = ActiveSupport::MessageEncryptor.key_len(encrypted_cookie_cipher)
-      secret = key_generator.generate_key(salt, key_len)
+      secret = key_generator.generate_key(config.action_dispatch.authenticated_encrypted_cookie_salt, key_len)
       # ActiveSupport::MessageEncryptor defaults to `Marshal` if no serializer is provided
       # Make sure this matches the vets-api config: Rails.application.config.action_dispatch.cookies_serializer
       encryptor = ActiveSupport::MessageEncryptor.new(secret, cipher: encrypted_cookie_cipher, serializer: nil)
