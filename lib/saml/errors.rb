ebb615c1ac05699e373da8e53004b707c6000cd1
Patrick Vinograd <patrick@adhocteam.us>
2020-05-13 16:27:29 -0700

    Handles and validates multi-value fields in SSOe (#4235)
    * Handles and validates multi-value fields in SSOe
    
    SSOe may provide comma-separated multivalue attributes for some fields.
    This makes the parsing code robust to that, and also enforces validation
    on the MHV ID, EDIPI, and ICN fields to ensure that there are no
    conflicting values.

--- lib/saml/errors.rb
+++ lib/saml/errors.rb
@@ -2,5 +2,22 @@
 
 module SAML
   class UserAttributeError < StandardError
+    MULTIPLE_MHV_IDS = { code: '101',
+                         tag: :multiple_mhv_ids,
+                         message: 'User attributes contain multiple distinct MHV ID values' }.freeze
+    MULTIPLE_EDIPIS = { code: '102',
+                        tag: :multiple_edipis,
+                        message: 'User attributes contain multiple distinct EDIPI values' }.freeze
+    MHV_ICN_MISMATCH = { code: '103',
+                         tag: :mhv_icn_mismatch,
+                         message: 'MHV credential ICN does not match MPI record' }.freeze
+
+    attr_reader :code, :tag
+
+    def initialize(message:, code:, tag:)
+      @code = code
+      @tag = tag
+      super(message)
+    end
   end
 end
