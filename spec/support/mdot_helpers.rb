060f7282ffa2f203a1bd823487a828a3ca39ade6
camerontesterman <cameronbtesterman@gmail.com>
2020-05-14 14:09:41 -0500

    fix lint issue

--- spec/support/mdot_helpers.rb
+++ spec/support/mdot_helpers.rb
@@ -3,8 +3,8 @@
 module MDOT
   module Helpers
     def set_token_for(user, token='abcd1234abcd1234abcd1234')
-      token = ::MDOT::Token.find_or_build(user.uuid)
-      token.update(token: token)
+      jwt = ::MDOT::Token.find_or_build(user.uuid)
+      jwt.update(token: token)
     end
   end
 end
