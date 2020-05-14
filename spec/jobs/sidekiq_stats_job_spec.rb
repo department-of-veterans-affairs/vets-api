dbc319cb6b3f5d829c8a660c021487a00b036f28
Philip Becker <philip.becker@oddball.io>
2020-05-14 16:44:02 -0400

    sidekiq_stats_job_spec tests all feature of sidekiq_stats_job

--- spec/jobs/sidekiq_stats_job_spec.rb
+++ spec/jobs/sidekiq_stats_job_spec.rb
@@ -7,21 +7,35 @@ RSpec.describe SidekiqStatsJob, type: :job do
     described_class.new
   end
 
-  # allow_any_instance_of(Sidekiq::Stats).to receive().and_return()
   describe '#perform' do
     before(:each) do
       # this method calls gauge multiple times, allow the ones we aren't testing
       allow(StatsD).to receive(:gauge)
     end
-    context 'METRIC_NAMES' do
-      it 'should send statsd a gauge of the info to shared.sidekiq.stats.METRIC_NAME'
+    context 'for METRIC_NAMES' do
+      SidekiqStatsJob::METRIC_NAMES.each do |metric|
+        metric_key = "shared.sidekiq.stats.#{metric}"
+        it "should send statsd a gauge of the info to #{metric_key}" do
+          stat = [1,3,7,11].sample
+          expect_any_instance_of(Sidekiq::Stats).to receive(metric).and_return(stat)
+          expect(StatsD).to receive(:gauge).with(metric_key, stat)
+          subject.perform
+        end
+      end
     end
-    fit 'should gauge number of working jobs' do
+    it 'should gauge number of working jobs' do
       busy_process_set = [1,2,3]
       allow_any_instance_of(Sidekiq::ProcessSet).to receive(:select).and_return(busy_process_set)
       expect(StatsD).to receive(:gauge).with('shared.sidekiq.stats.working', busy_process_set.count)
       subject.perform
     end
-    it 'should gauge the size of each queue'
+    it 'should gauge the size of each queue' do
+      queues = {'queue1' => 4, 'queue2' => 44}
+      allow_any_instance_of(Sidekiq::Stats).to receive(:queues).and_return(queues)
+      queues.each do |name, size|
+        expect(StatsD).to receive(:gauge).with("shared.sidekiq.#{name}.size", size)
+      end
+      subject.perform
+    end
   end
 end
