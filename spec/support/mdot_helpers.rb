7eb827835c3ad2cff155f561afb48573701fa386
camerontesterman <cameronbtesterman@gmail.com>
2020-05-14 14:40:37 -0500

    fix method

--- spec/support/mdot_helpers.rb
+++ spec/support/mdot_helpers.rb
@@ -2,7 +2,7 @@
 
 module MDOT
   module Helpers
-    def set_token_for(user, token = 'abcd1234abcd1234abcd1234')
+    def self.set_token_for(user, token = 'abcd1234abcd1234abcd1234')
       jwt = ::MDOT::Token.find_or_build(user.uuid)
       jwt.update(token: token)
     end
