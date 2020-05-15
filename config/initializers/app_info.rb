a0a9fa0bd8d0979fc47980cdcef5113d29c73aad
Keifer Furzland <kfrz.code@gmail.com>
2020-05-15 07:12:00 -0500

    Add default value if git ref is unavailable

--- config/initializers/app_info.rb
+++ config/initializers/app_info.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module AppInfo
-  GIT_REVISION = ENV['GIT_REVISION']
+  GIT_REVISION = Env.fetch('GIT_REVISION', 'ABC1234')
   GITHUB_URL   = 'https://github.com/department-of-veterans-affairs/vets-api'
 end
