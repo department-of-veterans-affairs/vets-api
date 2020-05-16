c905a06835b5420d4d7bb157d0429fbc149a58c8
Kam Karshenas <kam@adhocteam.us>
2020-05-15 17:22:20 -0700

    add the spec

--- modules/vaos/spec/request/v1/locations_request_spec.rb
+++ modules/vaos/spec/request/v1/locations_request_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe 'VAOS::V1::Location', type: :request do
       context 'with a location id' do
         it 'returns a 200 returning Location resource corresponding to that id' do
           VCR.use_cassette('vaos/fhir/location/read_by_id', record: :new_episodes) do
-            get '/vaos/v1/Location/353030'
+            get '/vaos/v1/Location/393833'
             expect(response).to have_http_status(:success)
           end
         end
@@ -40,8 +40,7 @@ RSpec.describe 'VAOS::V1::Location', type: :request do
       context 'with a location search query by name' do
         it 'returns a 200 returning Location resources corresponding to that name and including organization' do
           VCR.use_cassette('vaos/fhir/location/search_by_name', record: :new_episodes) do
-            get '/vaos/v1/Location?location.name=Iowa&_include=Location.organization&_include=Location.somethingelse'
-            binding.pry
+            get '/vaos/v1/Location?location.name=Iowa&_include=Location.organization'
             expect(response).to have_http_status(:success)
           end
         end
