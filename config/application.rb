7719b2311a34d2f9e26dd824e4f4b10141824077
Philip Becker <philip.becker@oddball.io>
2020-05-19 13:56:56 -0400

    use zeitwerk autoloading

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,6 @@ module VetsAPI
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.autoloader = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
