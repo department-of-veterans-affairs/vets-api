94aa3985fddd9d4999f9e3d37910fc570eb886e5
Lihan Li <frankieteardrop@gmail.com>
2020-05-19 09:20:52 -0700

    add debts service spec

---
+++ spec/lib/debts/service_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Debts::Service do
+  describe '#get_letters' do
+    it 'should fetch the veterans debt letters data' do
+      described_class.new.get_letters(fileNumber: '005628103')
+    end
+  end
+end
