688718b3ed8000eac39c360d30704627869dd9e7
Lihan Li <frankieteardrop@gmail.com>
2020-05-19 09:10:14 -0700

    add get letters endpoint

--- lib/debts/service.rb
+++ lib/debts/service.rb
@@ -1,5 +1,9 @@
 module Debts
   class Service < Common::Client::Base
     configuration Debts::Configuration
+
+    def get_letters(body)
+      request(:post, 'letterdetails/get', body)
+    end
   end
 end
