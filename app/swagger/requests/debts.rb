8ec665f82c3282590cc0bf81e924b49a0fda670f
Lihan Li <frankieteardrop@gmail.com>
2020-05-20 11:27:12 -0700

    debts swagger stub

---
+++ app/swagger/requests/debts.rb
@@ -0,0 +1,24 @@
+module Swagger
+  module Requests
+    class Debts
+      include Swagger::Blocks
+
+      swagger_path '/v0/debts' do
+        operation :get do
+          key :description, 'Get data about the user\'s debts'
+          key :operationId, 'getDebts'
+          key :tags, %w[debts]
+
+          response 200 do
+            key :description, 'Debts data'
+
+            schema do
+              property :data, type: :object do
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
