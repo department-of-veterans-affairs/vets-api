87f0dd37a99060fed776c71610f4d1a8ced98aea
Alastair Dawson <alastair@adhocteam.us>
2020-05-21 10:48:04 -0700

    9094 VAOS FHIR query organization (#4294)

--- modules/vaos/app/controllers/vaos/v1/organizations_controller.rb
+++ modules/vaos/app/controllers/vaos/v1/organizations_controller.rb
@@ -3,8 +3,13 @@
 module VAOS
   module V1
     class OrganizationsController < VAOS::V1::BaseController
+      def index
+        response = fhir_service.search(request.query_string)
+        render json: response.body
+      end
+
       def show
-        response = fhir_service.read(:Organization, params[:id])
+        response = fhir_service.read(params[:id])
         render json: response.body
       end
     end
