d64be680b8353a1c038fab62ba34a37e9d608a4f
Philip Becker <philip.becker@oddball.io>
2020-05-21 17:20:38 -0400

    use inflected controller name for saml_controller

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class SamlController < ApplicationController
+class SAMLController < ApplicationController
   skip_before_action :authenticate, only: [:metadata]
 
   def metadata
