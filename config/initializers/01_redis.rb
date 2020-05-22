88aab5d2138baff7edd60068586d8ec56b15c61f
Bill Ryan <bill.ryan@adhocteam.us>
2020-05-22 15:24:58 -0400

    Redis duplication bug fix (#4321)

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -27,7 +27,7 @@ class RedisDuplicator < Redis
 end
 
 Redis.current = if Settings.redis.app_data.key?(:secondary_url)
-                  secondary_redis = Redis.new(url: Settings.redis.app_data.url)
+                  secondary_redis = Redis.new(url: Settings.redis.app_data.secondary_url)
                   RedisDuplicator.new(secondary_redis, REDIS_CONFIG[:redis].to_hash)
                 else
                   Redis.new(REDIS_CONFIG[:redis].to_hash)
