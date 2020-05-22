b2bbbeee2c4cf15e27c6752817d510fbfc748d06
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-05-22 14:13:02 -0700

    rubocop

--- modules/vaos/app/controllers/vaos/v1/appointments_controller.rb
+++ modules/vaos/app/controllers/vaos/v1/appointments_controller.rb
@@ -4,11 +4,10 @@ module VAOS
   module V1
     class AppointmentsController < VAOS::V1::BaseController
       def index
-        #response = fhir_service.search(request.query_string)
-        #render json: response.body
+        # response = fhir_service.search(request.query_string)
+        # render json: response.body
         head :ok
       end
     end
   end
 end
-
