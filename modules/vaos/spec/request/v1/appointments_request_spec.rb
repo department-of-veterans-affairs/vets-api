30b0f48f5de9a6ae99f95cc762a66b6bdcd59f20
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-05-26 16:13:57 -0700

    add test for appts when query returns no records

--- modules/vaos/spec/request/v1/appointments_request_spec.rb
+++ modules/vaos/spec/request/v1/appointments_request_spec.rb
@@ -36,11 +36,36 @@ RSpec.describe 'Appointment', type: :request do
         end
 
         let(:query_string) do
-          "patient:Patient.identifier=1012845331V153043&date=ge2020-05-01T07:00:00Z&date=lt2020-08-31T17:00:00Z&_include=Appointment:location"
+          'patient:Patient.identifier=1012845331V153043&date=ge2020-05-01T07:00:00Z' \
+            '&date=lt2020-08-31T17:00:00Z&_include=Appointment:location'
         end
 
-        it 'returns a 200' do
+        it 'returns a 200 and passes the body through' do
           VCR.use_cassette('vaos/fhir/search_appointment_200', match_requests_on: %i[method uri]) do
+            get "/vaos/v1/Appointment?#{query_string}"
+
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to eq(expected_body)
+          end
+        end
+      end
+
+      context 'with a multi param query that returns no records' do
+        let(:expected_body) do
+          YAML.load_file(
+            Rails.root.join(
+              'spec', 'support', 'vcr_cassettes', 'vaos', 'fhir', 'search_appointment_no_records.yml'
+            )
+          )['http_interactions'].first.dig('response', 'body', 'string')
+        end
+
+        let(:query_string) do
+          'patient:Patient.identifier=1012845331V153043&date=ge2010-05-01T07:00:00Z' \
+            '&date=lt2010-08-31T17:00:00Z&_include=Appointment:location'
+        end
+
+        it 'returns a 200 and passes the body through' do
+          VCR.use_cassette('vaos/fhir/search_appointment_no_records', match_requests_on: %i[method uri]) do
             get "/vaos/v1/Appointment?#{query_string}"
 
             expect(response).to have_http_status(:ok)
