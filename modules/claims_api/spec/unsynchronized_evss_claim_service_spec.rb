5407f3e00f1fb8df5addffea1fc24e3b6fc2ed1e
Philip Becker <philip.becker@oddball.io>
2020-05-26 11:49:07 -0400

    unsynchronized_evss_claim_service_spec.rb used everywhere without s

--- modules/claims_api/spec/unsynchronized_evss_claim_service_spec.rb
+++ modules/claims_api/spec/unsynchronized_evss_claim_service_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'claims_api/unsynchronized_evss_claims_service'
+require 'claims_api/unsynchronized_evss_claim_service'
 
 RSpec.describe ClaimsApi::UnsynchronizedEVSSClaimService, type: :model do
   let(:user) { FactoryBot.create(:user, :loa3) }
