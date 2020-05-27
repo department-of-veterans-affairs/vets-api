fb458a061d2e40dfc3955581274195d7800597a9
camerontesterman <cameronbtesterman@gmail.com>
2020-05-27 14:19:15 -0500

    .

--- spec/request/debts_spec.rb
+++ spec/request/debts_spec.rb
@@ -5,14 +5,26 @@ require 'rails_helper'
 RSpec.describe 'Debts API Endpoint', type: :request do
   include SchemaMatchers
 
+  let(:user_details) do
+    {
+      first_name: 'Greg',
+      last_name: 'Anderson',
+      middle_name: 'A',
+      birth_date: '1991-04-05',
+      ssn: '000000009'
+    }
+  end
+
+  let(:user) { build(:user, :loa3, user_details) }
+
+  before do
+    sign_in_as(user)
+  end
+
   describe 'GET /v0/debts' do
-    let(:user_with_ssn) { FactoryBot.create(:user, :loa3, ssn: '000000009') }
-    let(:user_without_ssn) { FactoryBot.create(:user, :loa3, ssn: '')}
-  
     context 'with a veteran who has debts' do
       it 'returns a 200 with the array of debts' do
         VCR.use_cassette('debts/get_letters') do
-          sign_in_as(user_with_ssn)
           get '/v0/debts'
           expect(response).to have_http_status(:ok)
           expect(response).to match_response_schema('debts')
@@ -24,7 +36,6 @@ RSpec.describe 'Debts API Endpoint', type: :request do
   context 'with a veteran with empty ssn' do
     it 'returns an error' do
       VCR.use_cassette('debts/get_letters_error', :record => :all) do
-        sign_in_as(user_without_ssn)
         get '/v0/debts'
         expect(response).to have_http_status(:error)
       end
