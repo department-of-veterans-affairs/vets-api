b3df0ea408df14ef0565ad15e3b6fbda776e33d6
camerontesterman <cameronbtesterman@gmail.com>
2020-05-27 14:03:58 -0500

    test

--- spec/request/debts_spec.rb
+++ spec/request/debts_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe 'Debts API Endpoint', type: :request do
   include SchemaMatchers
 
   describe 'GET /debts' do
+    let(:user_with_ssn) { create(:user, :loa3, ssn: '000000009') }
+    let(:user_without_ssn) { create(:user, :loa3, ssn: '')}
+  
     context 'with a veteran who has debts' do
-      let(:user_with_ssn) { create(:user, :loa3, ssn: '000000009') }
-      sign_in_as(user_with_ssn)
-
       it 'returns a 200 with the array of debts' do
         VCR.use_cassette('debts/get_letters') do
+          sign_in_as(user_with_ssn)
           get '/v0/debts'
           expect(response).to have_http_status(:ok)
           expect(response).to match_response_schema('debts')
@@ -21,11 +22,9 @@ RSpec.describe 'Debts API Endpoint', type: :request do
   end
 
   context 'with a veteran with empty ssn' do
-    let(:user_without_ssn) { create(:user, :loa3, ssn: '')}
-    sign_in_as(user_without_ssn)
-
     it 'returns an error' do
       VCR.use_cassette('debts/get_letters_error', :record => :all) do
+        sign_in_as(user_without_ssn)
         get '/v0/debts'
         expect(response).to have_http_status(:error)
       end
