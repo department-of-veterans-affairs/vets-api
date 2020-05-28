3e55ebe95aae6f05c583d893c2b91dc5f2444e54
Philip Becker <philip.becker@oddball.io>
2020-05-28 16:04:28 -0400

    fix spelling of serializable

--- lib/common/exceptions/internal/token_validation_error.rb
+++ lib/common/exceptions/internal/token_validation_error.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'common/exceptions/base_error'
-require 'common/exceptions/serializeable_error'
+require 'common/exceptions/serializable_error'
 
 module Common
   module Exceptions::Internal
