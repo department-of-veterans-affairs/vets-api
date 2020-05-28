4b22b4bff265077471e1e75016b39abfbea32363
Cameron Testerman <cameronbtesterman@gmail.com>
2020-05-28 10:17:42 -0500

    [8509] Add "GET /v0/debts" Endpoint (#4218)
    * debts init
    
    * debts_spec added
    
    * update test
    
    * add debts.json schema
    
    * remove 404
    
    * fix debts schema
    
    * schema fix
    
    * remove data
    
    * remove unnecessary to_json
    
    * missing closing parentheses
    
    * debt tests
    
    * user fixes
    
    * test
    
    * t
    
    * .
    
    * .
    
    * .
    
    * service test
    
    * .
    
    * add context
    
    * remove whitespace
    
    * fix error msg

--- spec/lib/debts/service_spec.rb
+++ spec/lib/debts/service_spec.rb
@@ -4,13 +4,27 @@ require 'rails_helper'
 
 RSpec.describe Debts::Service do
   describe '#get_letters' do
-    it 'fetches the veterans debt letters data' do
-      VCR.use_cassette(
-        'debts/get_letters',
-        VCR::MATCH_EVERYTHING
-      ) do
-        res = described_class.new.get_letters(fileNumber: '000000009')
-        expect(JSON.parse(res.to_json)[0]['fileNumber']).to eq('000000009')
+    context 'with a valid ssn' do
+      it 'fetches the veterans debt letters data' do
+        VCR.use_cassette(
+          'debts/get_letters',
+          VCR::MATCH_EVERYTHING
+        ) do
+          res = described_class.new.get_letters(fileNumber: '000000009')
+          expect(JSON.parse(res.to_json)[0]['fileNumber']).to eq('000000009')
+        end
+      end
+    end
+
+    context 'without a valid ssn' do
+      it 'returns a bad request error' do
+        VCR.use_cassette(
+          'debts/get_letters_empty_ssn',
+          VCR::MATCH_EVERYTHING
+        ) do
+          res = described_class.new.get_letters(fileNumber: '')
+          expect(JSON.parse(res.to_json)['message']).to eq('Bad request')
+        end
       end
     end
   end
