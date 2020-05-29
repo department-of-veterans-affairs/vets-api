f7ba43024d066db54c430820fc914baf85f5d662
Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>
2020-05-29 13:46:49 -0500

    add comment

--- lib/evss/disability_compensation_form/data_translation_all_claim.rb
+++ lib/evss/disability_compensation_form/data_translation_all_claim.rb
@@ -381,6 +381,8 @@ module EVSS
       def translate_treatments
         return {} if input_form['vaTreatmentFacilities'].blank?
 
+        # treatmentCenterName clean up is an approximation of evss regex
+        # validation ([a-zA-Z0-9"\/&\(\)\-'.#]([a-zA-Z0-9(\)\-'.# ])?)+$
         treatments = input_form['vaTreatmentFacilities'].map do |treatment|
           {
             'startDate' => approximate_date(treatment['treatmentDateRange']['from']),
