2fb75e6b66627c4cf067eb43cd476d6afc9a23c5
Michael Bastos <bastosmichael@gmail.com>
2020-05-29 10:38:05 -0500

    Adding extra page for Contestable issues (#4332)
    * Adding extra page for Contestable issues
    
    * working on merge_page
    
    * commenting out merge
    
    * final test fix
    
    * returning sidekiq pro
    
    * changing temp path
    
    * fixing temp_path
    
    * renaming temp_path
    
    * replacing with docker generated pdf

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'betamocks', git: 'https://github.com/department-of-veterans-affairs/betamoc
 gem 'breakers'
 gem 'carrierwave-aws'
 gem 'clam_scan'
+gem 'combine_pdf'
 gem 'config'
 gem 'connect_vbms', git: 'https://github.com/department-of-veterans-affairs/connect_vbms.git', branch: 'master', require: 'vbms'
 gem 'date_validator'
--- modules/appeals_api/spec/factories/higher_level_reviews.rb
+++ modules/appeals_api/spec/factories/higher_level_reviews.rb
@@ -13,4 +13,17 @@ FactoryBot.define do
       status { 'received' }
     end
   end
+
+  factory :extra_higher_level_review, class: 'AppealsApi::HigherLevelReview' do
+    id { SecureRandom.uuid }
+    auth_headers do
+      JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/valid_200996_headers.json"
+    end
+    form_data do
+      JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/valid_200996_extra.json"
+    end
+    trait :status_received do
+      status { 'received' }
+    end
+  end
 end
