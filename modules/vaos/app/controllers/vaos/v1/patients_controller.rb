4d665f8da413105ee57a532790ea0148d968e44f
AJ Magdub <ahmed.magdub@va.gov>
2020-06-01 15:58:03 -0600

    troubleshoot failure to create review instance

--- modules/vaos/app/controllers/vaos/v1/patients_controller.rb
+++ modules/vaos/app/controllers/vaos/v1/patients_controller.rb
@@ -5,7 +5,7 @@ module VAOS
     class PatientsController < VAOS::V1::BaseController
       def index
         response = fhir_service.search(request.query_string)
-        render json: response.body
+        # render json: response.body
       end
     end
   end
