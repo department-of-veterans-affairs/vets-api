3bc98f6ff60bfa0b5e1767a118f9c498b7e8b8ce
Philip Becker <philip.becker@oddball.io>
2020-06-01 17:45:56 -0400

    have responses module space load all the different response classes

--- lib/emis/responses.rb
+++ lib/emis/responses.rb
@@ -4,3 +4,7 @@ module EMIS
   module Responses
   end
 end
+
+# require each response file
+Dir['lib/emis/responses/get_*.rb'].each { |file| require file.gsub('lib/', '') }
+require 'emis/responses/error_response'
