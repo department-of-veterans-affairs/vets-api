7fe4098771d265cf1f57b8198d353399d72126a3
Philip Becker <philip.becker@oddball.io>
2020-06-03 09:23:29 -0400

    Add misssing requires for sentry processor log_as_warning

--- lib/sentry/processor/log_as_warning.rb
+++ lib/sentry/processor/log_as_warning.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'common/exceptions'
+require 'evss/error_middleware'
+require 'evss/disability_compensation_form/gateway_timeout'
+
 module Sentry
   module Processor
     class LogAsWarning < Raven::Processor
