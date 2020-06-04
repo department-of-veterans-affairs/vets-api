f7e155e400d95a46f7903c13c977b08b7ea6142b
Philip Becker <philip.becker@oddball.io>
2020-06-04 12:58:05 -0400

    add missing requries in emis_redis

--- app/models/emis_redis/model.rb
+++ app/models/emis_redis/model.rb
@@ -2,6 +2,7 @@
 
 require 'common/models/redis_store'
 require 'common/models/concerns/cache_aside'
+require 'emis/military_information_service_v2'
 
 module EMISRedis
   # Generic model for caching EMIS data in Redis
