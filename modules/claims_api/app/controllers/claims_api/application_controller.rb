614c85c3b36776bf94bc037cc5bf8b82a87b286d
Philip Becker <philip.becker@oddball.io>
2020-06-05 10:34:16 -0400

    remove require_dependency

--- modules/claims_api/app/controllers/claims_api/application_controller.rb
+++ modules/claims_api/app/controllers/claims_api/application_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_dependency 'claims_api/unsynchronized_evss_claims_service'
-
 module ClaimsApi
   class ApplicationController < ::OpenidApplicationController
     include ClaimsApi::MVIVerification
