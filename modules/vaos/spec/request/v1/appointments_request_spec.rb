9c913941fb4a1bd6818eaaa7c60996f7c54c75d7
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-06-05 14:45:00 -0700

    fix rails rot join dir path

--- modules/vaos/spec/request/v1/appointments_request_spec.rb
+++ modules/vaos/spec/request/v1/appointments_request_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe 'Appointment', type: :request do
         let(:expected_body) do
           YAML.load_file(
             Rails.root.join(
-              'spec', 'support', 'vcr_cassettes', 'vaos', 'fhir', 'appointment/search_200.yml'
+              'spec', 'support', 'vcr_cassettes', 'vaos', 'fhir', 'appointment', 'search_200.yml'
             )
           )['http_interactions'].first.dig('response', 'body', 'string')
         end
