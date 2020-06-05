04009f7823517b344ad9cd901ab24ed9c1f8b182
camerontesterman <cameronbtesterman@gmail.com>
2020-06-05 09:41:20 -0500

    read result

--- lib/mdot/client.rb
+++ lib/mdot/client.rb
@@ -117,7 +117,7 @@ module MDOT
 
     def handle_client_error(error)
       save_error_details(error)
-      code = JSON.parse(error.body)['result'].downcase
+      code = error.body['result'].downcase
 
       raise_backend_exception(
         MDOT::ExceptionKey.new("MDOT_#{code}"),
