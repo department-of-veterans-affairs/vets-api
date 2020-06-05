25e076d75e5b91f2f67c28828c7b66453c18a9fe
Philip Becker <philip.becker@oddball.io>
2020-06-05 13:36:08 -0400

    define Common namespaces in each file

--- lib/common/client/concerns/log_as_warning_helpers.rb
+++ lib/common/client/concerns/log_as_warning_helpers.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
-module Common::Client::Concerns
-  module LogAsWarningHelpers
-    def warn_for_service_unavailable
-      yield
-    rescue Common::Exceptions::External::BackendServiceException => e
-      Raven.extra_context(log_as_warning: true) if e.original_status&.to_i == 503
+module Common
+  module Client
+    module Concerns
+      module LogAsWarningHelpers
+        def warn_for_service_unavailable
+          yield
+        rescue Common::Exceptions::External::BackendServiceException => e
+          Raven.extra_context(log_as_warning: true) if e.original_status&.to_i == 503
 
-      raise
-    rescue Common::Client::Errors::HTTPError => e
-      Raven.extra_context(log_as_warning: true) if e.status&.to_i == 503
+          raise
+        rescue Common::Client::Errors::HTTPError => e
+          Raven.extra_context(log_as_warning: true) if e.status&.to_i == 503
 
-      raise
+          raise
+        end
+      end
     end
   end
 end
