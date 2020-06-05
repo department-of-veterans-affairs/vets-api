0bf7f9fb4e49b91a885ef6f5267a4c50af94cac1
Derek Dyer <derek@digitalderek.com>
2020-06-05 18:50:22 -0400

    Add test for null if no participant_id

--- spec/controllers/v0/profile/va_file_numbers_controller_spec.rb
+++ spec/controllers/v0/profile/va_file_numbers_controller_spec.rb
@@ -5,19 +5,34 @@ require 'rails_helper'
 RSpec.describe V0::Profile::VaFileNumbersController, type: :controller do
   let(:user) { create(:evss_user) }
 
-  before do
-    sign_in_as(user)
-  end
+  # before do
+  #   sign_in_as(user)
+  # end
 
   describe '#show' do
     context 'with a valid bgs response' do
       it 'returns a va file number for the logged-in user' do
         VCR.use_cassette('bgs/person_web_service/find_person_by_ptcpnt_id') do
+          sign_in_as(user)
           get(:show)
           expect(response.code).to eq('200')
           expect(response).to have_http_status(:ok)
 
           expect(JSON.parse(response.body)['data']['type']).to eq('va_file_number')
+        end
+      end
+    end
+
+    context 'with a user that does not have a va_file_number' do
+      it 'returns a va file number for the logged-in user' do
+        VCR.use_cassette('bgs/person_web_service/no_result') do
+          allow(user).to receive(:participant_id).and_return('11111111111')
+          sign_in_as(user)
+          get(:show)
+          expect(response.code).to eq('200')
+          expect(response).to have_http_status(:ok)
+
+          expect(JSON.parse(response.body)['data']['attributes']['va_file_number']).to eq(nil)
         end
       end
     end
