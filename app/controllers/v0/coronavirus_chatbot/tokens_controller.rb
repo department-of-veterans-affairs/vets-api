4c3fbd4613126c9f23d49af5efe37c904240484f
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-06-09 15:22:35 -0400

    [department-of-veterans-affairs/covid19-chatbot#258] logs service exception to sentry
    Co-authored-by: Katie Ho <kho@thoughtworks.com>

--- app/controllers/v0/coronavirus_chatbot/tokens_controller.rb
+++ app/controllers/v0/coronavirus_chatbot/tokens_controller.rb
@@ -61,7 +61,8 @@ module V0
       end
 
       def service_exception_handler(exception)
-        logger.error "An error occured with the microsoft service that issues chatbot tokens #{exception.message}"
+        logger.error "An error occurred with the Microsoft service that issues chatbot tokens #{exception.message}"
+        log_exception_to_sentry(exception, {}, {}, :error)
         render nothing: true, status: :service_unavailable
       end
 
