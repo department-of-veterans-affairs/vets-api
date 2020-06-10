22d1e41b6cb7f52100bf9ef0ab787d4aa95d7e2f
camerontesterman <cameronbtesterman@gmail.com>
2020-06-10 09:47:55 -0500

    update params perms for mdt order submission

--- app/controllers/v0/mdot/supplies_controller.rb
+++ app/controllers/v0/mdot/supplies_controller.rb
@@ -9,20 +9,25 @@ module V0
 
       private
 
+      def address_params
+        %i[
+          street
+          street2
+          city
+          state
+          country
+          postal_code
+        ]
+      end
+
       def supply_params
         params.permit(
-          :use_permanent_address,
+          :use_veteran_address,
           :use_temporary_address,
           :additional_requests,
           order: [:product_id],
-          permanent_address: %i[
-            street
-            street2
-            city
-            state
-            country
-            postal_code
-          ]
+          permanent_address: address_params,
+          temporary_address: address_params
         ).to_hash
       end
 
