bb93ba2ef22622c511af7da8d50ff8b9fde09bcd
Derek Dyer <derek@digitalderek.com>
2020-06-11 22:03:25 -0400

    Add updates for false return

--- spec/controllers/v0/profile/valid_va_file_numbers_controller_spec.rb
+++ spec/controllers/v0/profile/valid_va_file_numbers_controller_spec.rb
@@ -2,33 +2,32 @@
 
 require 'rails_helper'
 
-RSpec.describe V0::Profile::VaFileNumbersController, type: :controller do
+RSpec.describe V0::Profile::ValidVaFileNumbersController, type: :controller do
   let(:user) { create(:evss_user) }
 
   describe '#show' do
     context 'with a valid bgs response' do
-      it 'returns a va file number for the logged-in user' do
+      it 'returns true if a logged-in user has a valid va file number' do
         VCR.use_cassette('bgs/person_web_service/find_person_by_participant_id') do
           sign_in_as(user)
           get(:show)
+
           expect(response.code).to eq('200')
           expect(response).to have_http_status(:ok)
-
-          expect(JSON.parse(response.body)['data']['type']).to eq('va_file_number')
+          expect(JSON.parse(response.body)['data']['attributes']['valid_va_file_number']).to eq(true)
         end
       end
     end
 
-    context 'with a user that does not have a va_file_number' do
+    context 'with a user that does not have a valid va_file_number' do
       it 'returns null' do
-        VCR.use_cassette('bgs/person_web_service/no_result') do
-          allow(user).to receive(:participant_id).and_return('11111111111')
+        VCR.use_cassette('bgs/person_web_service/find_person_by_participant_id_no_result') do
+          allow_any_instance_of(User).to receive(:participant_id).and_return('11111111111')
           sign_in_as(user)
           get(:show)
           expect(response.code).to eq('200')
           expect(response).to have_http_status(:ok)
-
-          expect(JSON.parse(response.body)['data']['attributes']['va_file_number']).to eq(nil)
+          expect(JSON.parse(response.body)['data']['attributes']['valid_va_file_number']).to eq(false)
         end
       end
     end
