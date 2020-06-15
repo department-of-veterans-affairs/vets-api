72a3098d8402d50bfb0ab31a9cb654da4a7c50fb
Ling Tran <lxiutran@gmail.com>
2020-06-15 17:47:17 -0400

    add notifications client config to settings and create worker spec (notifications-api #45)
    Co-authored-by: Marisa Hoenig <marisa.hoenig@thoughtworks.com>

--- app/workers/form526_confirmation_email_job.rb
+++ app/workers/form526_confirmation_email_job.rb
@@ -1,10 +1,21 @@
+require 'notifications/client'
 # frozen_string_literal: true
 
 class Form526ConfirmationEmailJob
   include Sidekiq::Worker
   sidekiq_options expires_in: 1.day
 
-  def perform(id, email="")
-    # send email
+  def perform(id, email='')
+    client = Notifications::Client.new(
+      Settings.notifications_api.secret_token,
+      Settings.notifications_api.client_url
+    )
+    email_response = client.send_email(
+      email_address: email,
+      template_id: Settings.notifications_api.template_id
+    )
+    
+    puts "Id: #{id}"
+    puts "Email was sent with id: #{email_response[:id]}"
   end
 end
