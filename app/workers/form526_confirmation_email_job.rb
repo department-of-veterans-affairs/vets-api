3f9ec590431ae589d4c462a190fbf8ce27672a19
Ling Tran <lxiutran@gmail.com>
2020-06-16 11:09:25 -0400

    modify settings for notification client template info and fix linting errors (notification-api #45)
    Co-authored-by: Marisa Hoenig <marisa.hoenig@thoughtworks.com>

--- app/workers/form526_confirmation_email_job.rb
+++ app/workers/form526_confirmation_email_job.rb
@@ -1,21 +1,19 @@
-require 'notifications/client'
 # frozen_string_literal: true
+
+require 'notifications/client'
 
 class Form526ConfirmationEmailJob
   include Sidekiq::Worker
   sidekiq_options expires_in: 1.day
 
-  def perform(id, email='')
-    client = Notifications::Client.new(
+  def perform(_id, email = '')
+    @notify_client ||= Notifications::Client.new(
       Settings.notifications_api.secret_token,
       Settings.notifications_api.client_url
     )
-    email_response = client.send_email(
+    @notify_client.send_email(
       email_address: email,
-      template_id: Settings.notifications_api.template_id
+      template_id: Settings.notifications_api.template_id.form526_confirmation_email
     )
-    
-    puts "Id: #{id}"
-    puts "Email was sent with id: #{email_response[:id]}"
   end
 end
--- spec/jobs/form526_confirmation_email_job_spec.rb
+++ spec/jobs/form526_confirmation_email_job_spec.rb
@@ -4,32 +4,39 @@ require 'rails_helper'
 require 'notifications/client'
 
 RSpec.describe Form526ConfirmationEmailJob do
+  before { Sidekiq::Worker.clear_all }
+
   describe '#perform' do
     context 'defaults' do
       it 'sends a confirmation email' do
-        email_response = {
-          "content": {
-            "body": "<html><body><h1>Hello</h1> World.</body></html>",
-            "from_email": "from_email",
-            "subject": "Hello World"
-          },
-          "id": "123456789",
-          "reference": nil,
-          "scheduled_for": nil,
-          "template": {
-            "id": Settings.notifications_api.template_id,
-            "uri": "template_url",
-            "version": 1
-          },
-          "uri": "url"
-        }
-        notification_client = double('Notifications::Client', :send_email => email_response)
         email_address = 'foo@example.com'
+        email_response = {
+          'content': {
+            'body': '<html><body><h1>Hello</h1> World.</body></html>',
+            'from_email': 'from_email',
+            'subject': 'Hello World'
+          },
+          'id': '123456789',
+          'reference': nil,
+          'scheduled_for': nil,
+          'template': {
+            'id': Settings.notifications_api.template_id,
+            'uri': 'template_url',
+            'version': 1
+          },
+          'uri': 'url'
+        }
+        notification_client = double('Notifications::Client', { send_email: email_response })
+
         allow(Notifications::Client).to receive(:new).and_return(notification_client)
-        expect(notification_client).to receive(:send_email).with({
-          email_address: email_address,
-          template_id: Settings.notifications_api.template_id
-        })
+
+        expect(notification_client).to receive(:send_email).with(
+          {
+            email_address: email_address,
+            template_id: Settings.notifications_api.template_id.form526_confirmation_email
+          }
+        )
+
         subject.perform(123, email_address)
       end
     end
