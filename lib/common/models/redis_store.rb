a3ceb26aa1ae6ec1dce6db9adfb5b57db02ae91e
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-06-16 15:09:48 -0700

    set vaos token cache ttl from token exp value

--- lib/common/models/redis_store.rb
+++ lib/common/models/redis_store.rb
@@ -86,7 +86,6 @@ module Common
 
     def save
       return false unless valid?
-
       redis_namespace.set(attributes[redis_namespace_key], Oj.dump(attributes))
       expire(redis_namespace_ttl) if defined? redis_namespace_ttl
       @persisted = true
