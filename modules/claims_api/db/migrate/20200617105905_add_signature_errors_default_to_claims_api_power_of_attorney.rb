b9d0ca80e984bc5ad1bfc7ba2394f6693a50b713
Charley Stran <charley.stran@gmail.com>
2020-06-20 05:57:42 -0500

    update migration to make it reversible

--- modules/claims_api/db/migrate/20200617105905_add_signature_errors_default_to_claims_api_power_of_attorney.rb
+++ modules/claims_api/db/migrate/20200617105905_add_signature_errors_default_to_claims_api_power_of_attorney.rb
@@ -2,6 +2,6 @@
 
 class AddSignatureErrorsDefaultToClaimsApiPowerOfAttorney < ActiveRecord::Migration[6.0]
   def change
-    change_column_default :claims_api_power_of_attorneys, :signature_errors, []
+    change_column_default :claims_api_power_of_attorneys, :signature_errors, to: [], from: nil
   end
 end
