2e8ffc96d2c8b4cd31982e691d41051b594881ca
Charley Stran <charley.stran@gmail.com>
2020-06-20 06:13:18 -0500

    Ordering kwargs

--- modules/claims_api/db/migrate/20200617105905_add_signature_errors_default_to_claims_api_power_of_attorney.rb
+++ modules/claims_api/db/migrate/20200617105905_add_signature_errors_default_to_claims_api_power_of_attorney.rb
@@ -2,6 +2,6 @@
 
 class AddSignatureErrorsDefaultToClaimsApiPowerOfAttorney < ActiveRecord::Migration[6.0]
   def change
-    change_column_default :claims_api_power_of_attorneys, :signature_errors, to: [], from: nil
+    change_column_default :claims_api_power_of_attorneys, :signature_errors, from: nil, to: []
   end
 end
