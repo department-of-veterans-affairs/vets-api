ca6892c5bfb2184052d422b33cc60f854c2324e4
Charley Stran <charley.stran@gmail.com>
2020-06-23 08:20:23 -0500

    remove unneeded lines from stamp error

--- modules/claims_api/lib/claims_api/stamp_signature_error.rb
+++ modules/claims_api/lib/claims_api/stamp_signature_error.rb
@@ -2,11 +2,9 @@
 
 module ClaimsApi
   class StampSignatureError < StandardError
-    attr_accessor :code
     attr_accessor :detail
 
     def initialize(message: nil, detail: nil)
-      message = message if message.present?
       super(message)
       @detail = detail
     end
