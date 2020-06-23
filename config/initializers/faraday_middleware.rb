2695598f688485c09405f5818604f4091dc0b390
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-06-23 13:59:54 -0400

    add requires for lib/common

--- config/initializers/faraday_middleware.rb
+++ config/initializers/faraday_middleware.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'common/client/middleware/request/remove_cookies'
+require 'common/client/middleware/request/immutable_headers'
+
 Faraday::Middleware.register_middleware remove_cookies: Common::Client::Middleware::Request::RemoveCookies
 Faraday::Middleware.register_middleware immutable_headers: Common::Client::Middleware::Request::ImmutableHeaders
 
