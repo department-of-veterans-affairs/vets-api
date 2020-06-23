b0806e143961839df1b21b04969c9d2c0b92ca8f
Eric Buckley <eric.buckley@gmail.com>
2020-06-23 11:23:13 -0700

    making redirect_url easier to read with long switch case

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -73,20 +73,32 @@ module V1
 
     private
 
+    # rubocop:disable Metrics/CyclomaticComplexity
     def redirect_url(type)
       raise Common::Exceptions::RoutingError, type unless REDIRECT_URLS.include?(type)
 
       case type
+      when 'signup'
+        url_service.signup_url
+      when 'mhv'
+        url_service.mhv_url
+      when 'dslogon'
+        url_service.dslogon_url
+      when 'idme'
+        url_service.idme_url
+      when 'mfa'
+        url_service.mfa_url
+      when 'verify'
+        url_service.verify_url
       when 'custom'
         raise Common::Exceptions::ParameterMissing, 'authn' if params[:authn].blank?
 
         url_service.custom_url params[:authn]
       when 'slo'
         url_service.ssoe_slo_url # due to shared url service implementation
-      else
-        url_service.public_send("#{type}_url")
       end
     end
+    # rubocop:enable Metrics/CyclomaticComplexity
 
     def force_authn?
       params[:force]&.downcase == 'true'
