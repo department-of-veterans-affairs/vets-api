e75e1645349b83e4f49ed32a9f06a163858d2690
Lihan Li <frankieteardrop@gmail.com>
2020-07-01 11:55:24 -0700

    dont stub subject in letter downloader spec

--- spec/lib/debts/letter_downloader_spec.rb
+++ spec/lib/debts/letter_downloader_spec.rb
@@ -8,40 +8,37 @@ RSpec.describe Debts::LetterDownloader do
 
   let(:file_number) { '796330625' }
   let(:vbms_client) { FakeVbms.new }
-  let(:request_double) do
+
+  def stub_request(request_name, args, return_val)
     request_double = double
-    expect("VBMS::Requests::#{request_name}".constantize).to receive(:new).with(request_args).and_return(request_double)
+    expect("VBMS::Requests::#{request_name}".constantize).to receive(:new).with(args).and_return(request_double)
 
-    request_double
-  end
-
-  before do
-    allow(VBMS::Client).to receive(:from_env_vars).and_return(vbms_client)
+    expect(vbms_client).to receive(:send_request).with(
+      request_double
+    ).and_return(
+      return_val
+    )
   end
 
   def get_vbms_fixture(path)
     get_fixture("vbms/#{path}").map { |r| OpenStruct.new(r) }
   end
 
-  describe '#get_letter' do
-    before do
-      # rubocop:disable RSpec/SubjectStub
-      expect(subject).to receive(:list_letters).and_return(
-        get_fixture('vbms/list_letters').map!(&:symbolize_keys)
-      )
-      # rubocop:enable RSpec/SubjectStub
-    end
+  before do
+    allow(VBMS::Client).to receive(:from_env_vars).and_return(vbms_client)
 
+    stub_request('FindDocumentVersionReference', file_number, get_vbms_fixture('find_document_version_reference'))
+  end
+
+  describe '#get_letter' do
     context 'with a document in the users folder' do
       let(:document_id) { '{93631483-E9F9-44AA-BB55-3552376400D8}' }
-      let(:request_name) { 'GetDocumentContent' }
-      let(:request_args) { document_id }
       let(:content) { File.read('spec/fixtures/pdf_fill/extras.pdf') }
 
       before do
-        expect(vbms_client).to receive(:send_request).with(
-          request_double
-        ).and_return(
+        stub_request(
+          'GetDocumentContent',
+          document_id,
           OpenStruct.new(
             document_id: document_id,
             content: content
@@ -64,15 +61,6 @@ RSpec.describe Debts::LetterDownloader do
   end
 
   describe '#list_letters' do
-    let(:request_name) { 'FindDocumentVersionReference' }
-    let(:request_args) { file_number }
-
-    before do
-      expect(vbms_client).to receive(:send_request).with(
-        request_double
-      ).and_return(get_vbms_fixture('find_document_version_reference'))
-    end
-
     it 'gets letter ids and descriptions' do
       expect(subject.list_letters.to_json).to eq(
         get_fixture('vbms/list_letters').to_json
