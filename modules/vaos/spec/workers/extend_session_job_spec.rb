6ac26aaad548eb9466741ff5d343c8db9684ef12
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-07-01 16:01:57 -0700

    temp disable vaos logging spec

--- modules/vaos/spec/workers/extend_session_job_spec.rb
+++ modules/vaos/spec/workers/extend_session_job_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
 
 RSpec.describe VAOS::ExtendSessionJob, type: :job do
   subject { described_class }
@@ -12,6 +14,12 @@ RSpec.describe VAOS::ExtendSessionJob, type: :job do
   end
 
   describe '.perform_async' do
+    it 'submits succesfully' do
+      expect do
+        subject.perform_async(user.account_uuid)
+      end.to change(subject.jobs, :size).by(1)
+    end
+
     it 'calls user service update_session_token with the user account uuid' do
       expect_any_instance_of(VAOS::UserService).to receive(:update_session_token).with(user.account_uuid)
       subject.new.perform(user.account_uuid)
