2978c3e3815816ef177b623ff59d117abb5c22d1
Ed Mangimelli <edmangimelli@gmail.com>
2020-07-02 03:45:56 -0400

    tweak cassettes

--- modules/appeals_api/spec/requests/v1/contestable_issues_controller_spec.rb
+++ modules/appeals_api/spec/requests/v1/contestable_issues_controller_spec.rb
@@ -2,12 +2,12 @@
 
 require 'rails_helper'
 
-describe AppealsApi::V1::DecisionReviews::ContestableIssuesController, type: :request do
+describe AppealsApi::V1::DecisionReviews::HigherLevelReviews::ContestableIssuesController, type: :request do
   describe '#index' do
     it 'GETs contestable_issues from Caseflow successfully' do
-      VCR.use_cassette('caseflow/contestable_issues') do
+      VCR.use_cassette('caseflow/higher_level_reviews/contestable_issues') do
         get(
-          '/services/appeals/v1/decision_reviews/contestable_issues',
+          '/services/appeals/v1/decision_reviews/higher_level_reviews/contestable_issues/compensation',
           headers: {
             'X-VA-SSN' => '872958715',
             'X-VA-Receipt-Date' => '2019-12-01'
