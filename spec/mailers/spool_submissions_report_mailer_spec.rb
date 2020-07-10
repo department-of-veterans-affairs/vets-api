fb5cee549bfdabc67d415f1c33c7ef150703e6a8
hughes-dustin <hughes_dustin@bah.com>
2020-07-10 14:30:44 -0400

    fix typo in unit test

--- spec/mailers/spool_submissions_report_mailer_spec.rb
+++ spec/mailers/spool_submissions_report_mailer_spec.rb
@@ -92,8 +92,8 @@ RSpec.describe SpoolSubmissionsReportMailer, type: %i[mailer aws_helpers] do
             lihan@adhocteam.us
             Delli-Gatti_Michael@bah.com
             sonntag_adam@bah.com
-            Delli-Gatti_Michael@bah.com
-            sonntag_adam@bah.com
+            Turner_Desiree@bah.com
+            Neel_Darrell@bah.com
           ]
         )
       end
