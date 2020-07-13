ef7cb6953fef5132b2a36364e57c63a537aab1ef
Philip Becker <boofungal316@yahoo.com>
2020-07-13 13:02:48 -0400

    rename readme for MPI in openid_auth

--- modules/openid_auth/app/controllers/openid_auth/docs/v0/mpi_users_controller.rb
+++ modules/openid_auth/app/controllers/openid_auth/docs/v0/mpi_users_controller.rb
@@ -7,7 +7,7 @@ module OpenidAuth
         skip_before_action(:authenticate)
 
         def index
-          swagger = YAML.safe_load(File.read(OpenidAuth::Engine.root.join('MVI_README.yml')))
+          swagger = YAML.safe_load(File.read(OpenidAuth::Engine.root.join('MPI_README.yml')))
           render json: swagger
         end
       end
