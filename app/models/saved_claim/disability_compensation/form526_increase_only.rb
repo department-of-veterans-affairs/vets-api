77e7074f2b702be13973cbf2b1a2bb8f4dfb85af
Anna Carey <anna@adhocteam.us>
2020-07-15 16:41:51 -0400

    526: Remove V1 Claims Code and Configurations (#4514)
    * round 1: remove form526 increase only
    
    * missing specs
    
    * fix up specs
    
    * remove increase example
    
    * removed unnecessary code
    
    * add back spec
    
    * Shamefully reusing error vcr cassettes for v2 api
    >
    > I expect that the error respones are the same.  The coverage I'm adding is for code that has been in prod for years.

--- app/models/saved_claim/disability_compensation/form526_increase_only.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class SavedClaim::DisabilityCompensation::Form526IncreaseOnly < SavedClaim::DisabilityCompensation
-  add_form_and_validation('21-526EZ')
-
-  TRANSLATION_CLASS = EVSS::DisabilityCompensationForm::DataTranslation
-end
