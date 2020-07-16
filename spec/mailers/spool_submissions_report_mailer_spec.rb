a04317d6f5a3eb1b7b08ec34d129458f1bd96f14
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-07-16 14:24:55 -0400

    updated spec

--- spec/mailers/spool_submissions_report_mailer_spec.rb
+++ spec/mailers/spool_submissions_report_mailer_spec.rb
@@ -125,14 +125,17 @@ RSpec.describe SpoolSubmissionsReportMailer, type: %i[mailer aws_helpers] do
 
         expect(mail.to).to eq(
           %w[
-            dana.kuykendall@va.gov
-            Jennifer.Waltz2@va.gov
-            kathleen.dalfonso@va.gov
+            Brian.Grubb@va.gov
+            Darrell.Neel@va.gov
+            Delli-Gatti_Michael@bah.com
+            Joseph.Preisser@va.gov
+            Joseph.Welton@va.gov
             lihan@adhocteam.us
-            Ricardo.DaSilva@va.gov
-            shay.norton@va.gov
-            kyle.pietrosanto@va.gov
-            robert.shinners@va.gov
+            sonntag_adam@bah.com
+            tammy.hurley1@va.gov
+            Turner_Desiree@bah.com
+            Neel_Darrell@bah.com
+            shawkey_daniel@bah.com
           ]
         )
       end
