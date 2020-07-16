a5d7c6774e952caefd443101575200ebc3eced53
Devin McCurdy <mccurdy_devin@bah.com>
2020-07-16 08:31:54 -0400

    10203 spool file

--- app/workers/education_form/forms/va10203.rb
+++ app/workers/education_form/forms/va10203.rb
@@ -2,8 +2,17 @@
 
 module EducationForm::Forms
   class VA10203 < Base
+
     def header_form_type
       'V10203'
+    end
+
+    def form_benefit
+      @applicant.benefit&.titleize
+    end
+
+    def any_remaining_benefit
+      yesno(['moreThanSixMonths', 'sixMonthsOrLess'].include?(@applicant.benefitLeft))
     end
   end
 end
