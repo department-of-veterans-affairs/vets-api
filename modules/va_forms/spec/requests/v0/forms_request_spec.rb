8bda2b6e7aeab97ae0b34c2b8d494fb9769072c0
John Paul (JP) Ashenfelter <johnpaul.ashenfelter@oddball.io>
2020-07-17 14:51:16 -0400

    Add a camel inflection to /services/va_forms/v0/forms

--- modules/va_forms/spec/requests/v0/forms_request_spec.rb
+++ modules/va_forms/spec/requests/v0/forms_request_spec.rb
@@ -9,6 +9,38 @@ RSpec.describe 'VA Forms', type: :request do
   let(:base_url) { '/services/va_forms/v0/forms' }
 
   describe 'GET :index' do
+    context 'with the camel inflection header' do
+      let(:headers) do
+        {
+          'ACCEPT' => 'application/json',
+          'CONTENT_TYPE' => 'application/json',
+          'HTTP_X_KEY_INFLECTION' => 'camel'
+        }
+      end
+
+      it 'returns the forms' do
+        get base_url, headers: headers
+        json = JSON.parse(response.body)
+
+        expect(json.keys.count).to be 1
+        expect(json).to have_key('data')
+        expect(json['data'].size).to be 1
+        expect(json['data'][0]).to have_key 'type'
+        expect(json['data'][0]).to have_key 'id'
+        expect(json['data'][0]).to have_key 'attributes'
+        expect(json['data'][0]['attributes'].size).to be 8
+        expect(json['data'][0]['attributes']).to have_key 'formName'
+        expect(json['data'][0]['attributes']).to have_key 'url'
+        expect(json['data'][0]['attributes']).to have_key 'title'
+        expect(json['data'][0]['attributes']).to have_key 'firstIssuedOn'
+        expect(json['data'][0]['attributes']).to have_key 'lastRevisionOn'
+        expect(json['data'][0]['attributes']).to have_key 'pages'
+        expect(json['data'][0]['attributes']).to have_key 'sha256'
+        expect(json['data'][0]['attributes']).to have_key 'validPdf'
+        expect(json['data'][0]['type']).to eq 'va_form'
+      end
+    end
+
     it 'returns the forms' do
       get base_url
       expect(response).to match_response_schema('va_forms/forms')
