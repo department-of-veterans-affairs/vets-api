43abf5383be5a9681573ecc076cdbe83fc7c9bf1
Michael R. Fleet <michael.fleet@oddball.io>
2020-07-20 14:42:27 -0400

    use VCR cassette for HCA pact

--- spec/service_consumers/provider_states.rb
+++ spec/service_consumers/provider_states.rb
@@ -3,16 +3,11 @@
 Pact.provider_states_for 'HCA' do
   provider_state 'enrollment service is up' do
     set_up do
-      response_body_stub =
-        YAML.load_file(
-          'spec/support/vcr_cassettes/hca/submit_anon.yml'
-        )['http_interactions'][0]['response']['body']['string']
-
-      stub_request(:post, Settings.hca.endpoint).to_return(body: response_body_stub)
+      VCR.insert_cassette('hca/submit_anon')
     end
 
     tear_down do
-      # Any tear down steps to clean up the provider state
+      VCR.eject_cassette
     end
   end
 end
