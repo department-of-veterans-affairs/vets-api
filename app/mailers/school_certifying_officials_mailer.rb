754d6134701d4d06290fb34110b72e7f3f503a59
Matt Roth <roth_matthew@bah.com>
2020-07-21 14:06:34 -0400

    AC 1,3,4

---
+++ app/mailers/school_certifying_officials_mailer.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class SchoolCertifyingOfficialsMailer < ApplicationMailer
+  SUBJECT = 'Applicant for VA Rogers STEM Scholarship'
+
+  STAGING_RECIPIENTS = %w[
+      Delli-Gatti_Michael@bah.com
+      shawkey_daniel@bah.com
+      sonntag_adam@bah.com
+  ].freeze
+
+  def build(recipients = [], application = {})
+    recipients = recipients + STAGING_RECIPIENTS.clone if FeatureFlipper.staging_email?
+
+    mail(
+        {
+            to: ['roth_matthew@bah.com'],
+            cc: application["email"],
+            subject: SUBJECT,
+            body: "TEst ajsflkajs;ldfj test tests alfja;l"
+      }
+    )
+  end
+end
--- app/models/saved_claim/education_benefits/va10203.rb
+++ app/models/saved_claim/education_benefits/va10203.rb
@@ -4,19 +4,37 @@ class SavedClaim::EducationBenefits::VA10203 < SavedClaim::EducationBenefits
   add_form_and_validation('22-10203')
 
   def after_submit(user)
+    return unless Flipper.enabled?(:edu_benefits_stem_scholarship)
+
     authorized = user.authorize :evss, :access?
+    email_sent(false)
 
     if authorized
-      # gi_bill_status = get_gi_bill_status(user)
-      # remaining_entitlement = initialize_entitlement_information(gi_bill_status)
-      # facility_code = facility_code(gi_bill_status)
+      @gi_bill_status = get_gi_bill_status(user)
 
-      email_sent
-      save
+      unless @gi_bill_status.remaining_entitlement&.blank?
+        @facility_code = get_facility_code
+
+        unless @facility_code.blank?
+          @institution = get_institution
+
+          unless more_than_six_months || @institution.blank? || school_changed?
+            send_sco_email
+          end
+        end
+      end
     end
+
+    save
   end
 
   private
+
+  def email_sent(sco_email_sent)
+    application = parsed_form
+    application["scoEmailSent"] = sco_email_sent
+    self.form = JSON.generate(application)
+  end
 
   def get_gi_bill_status(user)
     service = EVSS::GiBillStatus::Service.new(user)
@@ -26,27 +44,55 @@ class SavedClaim::EducationBenefits::VA10203 < SavedClaim::EducationBenefits
     {}
   end
 
-  def initialize_entitlement_information(gi_bill_status)
-    return {} if gi_bill_status == {} || gi_bill_status.remaining_entitlement.blank?
-
-    VA10203::FormEntitlementInformation.new(
-        months: gi_bill_status.remaining_entitlement.months,
-        days: gi_bill_status.remaining_entitlement.days
-    )
-  end
-
-  def facility_code(gi_bill_status)
-    return {} if gi_bill_status == {}
-
-    most_recent = gi_bill_status.enrollments.max_by(&:begin_date)
+  def get_facility_code
+    most_recent = @gi_bill_status.enrollments.max_by(&:begin_date)
 
     return {} if most_recent.blank?
     most_recent.facility_code
   end
 
-  def email_sent
+  def get_institution
+    GIDSRedis.new.get_institution_details({id: @facility_code})[:data][:attributes]
+  end
+
+  def more_than_six_months
+    months = @gi_bill_status.remaining_entitlement.months
+    days = @gi_bill_status.remaining_entitlement.days
+
+    ((months * 30) + days ) > 180
+  end
+
+  def school_changed?
     application = parsed_form
-    application["scoEmailSent"] = true
-    self.form = JSON.generate(application)
+    form_school_name = application["schoolName"]
+    form_school_city = application["schoolCity"]
+    form_school_state = application["schoolState"]
+
+    prefill_name = @institution[:name]
+    prefill_city = @institution[:city]
+    prefill_state = @institution[:state]
+
+    form_school_name != prefill_name ||
+        form_school_city != prefill_city ||
+        form_school_state != prefill_state
+  end
+
+  def send_sco_email
+    return if FeatureFlipper.send_email?
+
+    recipients = []
+    application = parsed_form
+
+    scos = @institution[:versioned_school_certifying_officials]
+    primary = scos.find{ |sco| sco[:priority] == 'Primary' && sco[:email.present?]}
+    secondary = scos.find{ |sco| sco[:priority] == 'Secondary' && sco[:email.present?]}
+
+    unless primary.blank? && secondary.blank?
+      recipients.push(primary[:email]) if primary.present?
+      recipients.push(secondary[:email]) if secondary.present?
+
+      SchoolCertifyingOfficialsMailer.build(recipients, application).deliver_now
+      email_sent(true)
+    end
   end
 end
