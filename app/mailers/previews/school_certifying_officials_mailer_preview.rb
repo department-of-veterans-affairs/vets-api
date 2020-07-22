93cbcf045ce649c14c4294ccc10ac074862c8c4c
Matt Roth <roth_matthew@bah.com>
2020-07-22 15:54:45 -0400

    Update school_certifying_officials_mailer_preview.rb

--- app/mailers/previews/school_certifying_officials_mailer_preview.rb
+++ app/mailers/previews/school_certifying_officials_mailer_preview.rb
@@ -1,6 +1,6 @@
 class SchoolCertifyingOfficialsMailerPreview < ActionMailer::Preview
   def build
-    applicant = SavedClaim::EducationBenefits::VA10203.first.open_struct_form
+    applicant = SavedClaim::EducationBenefits::VA10203.last.open_struct_form
     recipients = []
     ga_client_id = nil
     SchoolCertifyingOfficialsMailer.build(applicant, recipients, ga_client_id)
