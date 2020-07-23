6cf3fb5328c6f87286d91379a6543cb310116a81
Matt Roth <roth_matthew@bah.com>
2020-07-23 08:50:40 -0400

    Update school_certifying_officials_mailer.rb

--- app/mailers/school_certifying_officials_mailer.rb
+++ app/mailers/school_certifying_officials_mailer.rb
@@ -2,9 +2,9 @@
 
 class SchoolCertifyingOfficialsMailer < TransactionalEmailMailer
   SUBJECT = 'Applicant for VA Rogers STEM Scholarship'
-  GA_CAMPAIGN_NAME = ''
-  GA_DOCUMENT_PATH = ''
-  GA_LABEL = ''
+  GA_CAMPAIGN_NAME = 'school-certifying-officials-10203-submission-notification'
+  GA_DOCUMENT_PATH = '/email/form'
+  GA_LABEL = 'school-certifying-officials-10203-submission-notification'
   TEMPLATE = 'school_certifying_officials'
 
   STAGING_RECIPIENTS = %w[
