12a6a9188646de25bbcd4165a45e653473ff2aaf
Derek Dyer <derek@digitalderek.com>
2020-07-23 10:48:46 -0400

    Add 686c-674 failure mailer

---
+++ app/mailers/dependents_application_failure_mailer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class DependentsApplicationFailureMailer < TransactionalEmailMailer
+  TEMPLATE = 'hca_submission_failure'
+
+  def build(user_hash)
+    @user_hash = user_hash
+    template = File.read('app/mailers/views/dependents_application_failure.erb')
+
+    mail(
+      to: @user_hash['email'],
+      subject: "We can't process your dependents application",
+      body: ERB.new(template).result(binding)
+    )
+  end
+end
