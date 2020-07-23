e6bbc3ecb80d0c33a2bbc81537b3040397468ff4
Michael Bastos <bastosmichael@gmail.com>
2020-07-23 13:03:41 -0500

    change

--- modules/va_forms/app/serializers/va_forms/form_detail_serializer.rb
+++ modules/va_forms/app/serializers/va_forms/form_detail_serializer.rb
@@ -12,15 +12,11 @@ module VaForms
     end
 
     def versions
-      if object.versions.any?
-        object.versions.map do |v|
-          {
-            sha256: v.changeset['sha256']&.last,
-            revision_on: v.created_at&.strftime('%Y-%m-%d')
-          }
-        end
-      else
-        []
+      object.versions.map do |v|
+        {
+          sha256: v.changeset['sha256']&.last,
+          revision_on: v.created_at&.strftime('%Y-%m-%d')
+        }
       end
     end
   end
