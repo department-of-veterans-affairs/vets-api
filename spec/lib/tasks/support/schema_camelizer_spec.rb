04749281c24f0f54f6e530869b30d7f58644f036
Philip Becker <boofungal316@yahoo.com>
2020-07-24 13:43:20 -0400

    implement basic camelizing test

--- spec/lib/tasks/support/schema_camelizer_spec.rb
+++ spec/lib/tasks/support/schema_camelizer_spec.rb
@@ -4,10 +4,35 @@ require 'rails_helper'
 require './lib/tasks/support/schema_camelizer.rb'
 
 describe SchemaCamelizer do
+  TEST_DIRECTORY = 'tmp/camel_schema_tests'
+  TEST_SCHEMA_DIRECTORY = TEST_DIRECTORY + '/schemas'
+  TEST_RESULT_DIRECTORY = TEST_DIRECTORY + '/schemas_camelized'
+
+  before(:context) do
+    # create directories for source and result schemas
+    FileUtils.mkdir_p(TEST_SCHEMA_DIRECTORY)
+    FileUtils.mkdir_p(TEST_RESULT_DIRECTORY)
+  end
+
+  after(:context) do
+    FileUtils.remove_dir(TEST_DIRECTORY)
+  end
+
+  def create_source_schema(name, hash)
+    schema_file = "#{TEST_SCHEMA_DIRECTORY}/#{name}.json"
+    File.open(schema_file, 'w') { |file| file.write(JSON.pretty_generate(hash)) }
+    schema_file
+  end
+
   describe '#camel_schema' do
-    it 'should camel-inflect keys'
+    it 'should camel-inflect keys' do
+      schema = {'cat_sound' => 'meow', 'dog_sound' => 'woof'}
+      filename = create_source_schema('basic_inflect', schema)
+      subject = SchemaCamelizer.new(filename)
+      expect(subject.camel_schema.keys).to match %w(catSound dogSound)
+    end
     it 'should camel-inflect nested keys'
-    it 'should camel-inflect nested keys'
+    it 'should camel-inflect values in "required" keys'
   end
 
   describe '#referenced_schemas' do
