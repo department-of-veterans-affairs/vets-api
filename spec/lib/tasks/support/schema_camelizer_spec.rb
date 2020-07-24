a54f981e684cfa30436cc108a5f59eab1a903bdd
Philip Becker <boofungal316@yahoo.com>
2020-07-24 11:44:40 -0400

    outline of tests to support

--- spec/lib/tasks/support/schema_camelizer_spec.rb
+++ spec/lib/tasks/support/schema_camelizer_spec.rb
@@ -4,7 +4,33 @@ require 'rails_helper'
 require './lib/tasks/support/schema_camelizer.rb'
 
 describe SchemaCamelizer do
-  it 'has a test' do
-    expect(1 + 1).not_to be_nil
+  describe '#camel_schema' do
+    it 'should camel-inflect keys'
+    it 'should camel-inflect nested keys'
+    it 'should camel-inflect nested keys'
+  end
+
+  describe '#referenced_schemas' do
+    it 'should be empty with no references'
+    it 'should be an Array of SchemaCamelizers for referenced schemas'
+  end
+
+  describe '#already_camelized' do
+    it 'when the source schema has camel keys it should be true'
+    it 'when the source schema has snake keys it should be false'
+  end
+
+  describe '#camel_path' do
+    it 'should be in schemas_camelized directory'
+  end
+
+  describe '#unchanged_schemas' do
+    it 'should be an array of names of schemas that are already_camelized'
+    it 'should be empty if the original schema was snake case'
+  end
+
+  describe '#save!' do
+    it 'should write a file to the disk'
+    it 'should return an array of paths to saved files'
   end
 end
