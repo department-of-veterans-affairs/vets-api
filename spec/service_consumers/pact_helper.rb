f78051bb5ad12748e21e41fca6b7bf6facbbfca6
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-07-27 15:18:45 -0400

    stub session and sign in in rspec config

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -29,24 +29,26 @@ git_branch = ENV['GIT_BRANCH'] || `git rev-parse --abbrev-ref HEAD`
 publish_flag = ENV['PUBLISH_PACT_VERIFICATION_RESULTS'] == "true" || (! Rails.env.development?)
 
 RSpec.configure do |config|
+  config.include AuthenticatedSessionHelper
+  config.define_derived_metadata do |metadata|
+    metadata[:type] = :request
+  end
+
   config.before do |example|
+
+    user = FactoryBot.build(:user, :loa3)
+    session_object = sign_in(user, nil, nil, true)
+    allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(session_object.to_hash)
+
     stub_mvi unless example.metadata[:skip_mvi]
     stub_emis unless example.metadata[:skip_emis]
     stub_vet360 unless example.metadata[:skip_vet360]
   end
-
-  #If we can figure out which directory the dynamic specs from pact are generated in, 
-  # we may be able to define type: :request and have access to the cookies hash
-  # thus, being able to use sign_in_as(user) <--- which will set the cookie for us
-  
-  # config.define_derived_metadata(:file_path => Regexp.new('/spec/service_consumers/*')) do |metadata|
-  #   metadata[:type] = :request
-  # end
 end
 
 Pact.service_provider 'VA.gov API' do
   # inject the auth headers via Rack reverse proxy
-  app { AddAuthenticationHeaders.new(Rails.application) }
+  # app { AddAuthenticationHeaders.new(Rails.application) }
     
   # This example points to a local file, however, on a real project with a continuous
   # integration box, you would use a [Pact Broker](https://github.com/pact-foundation/pact_broker)
@@ -64,7 +66,7 @@ Pact.service_provider 'VA.gov API' do
 
   app_version git_sha
   app_version_tags git_branch
-  publish_verification_results publish_flag
+  # publish_verification_results publish_flag
 
   # honours_pacts_from_pact_broker do
   #   pact_broker_base_url 'https://vagov-pact-broker.herokuapp.com'
