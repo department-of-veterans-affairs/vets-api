2f0fcb5ee3780524271c6d5fa1faeb8124ceddf3
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-07-27 11:58:05 -0400

    add testing for rspec auth config approach

--- spec/service_consumers/provider_states_for/users.rb
+++ spec/service_consumers/provider_states_for/users.rb
@@ -3,17 +3,11 @@
 Pact.provider_states_for 'Users' do
   provider_state 'show user profile' do
     set_up do
-      # user = double('user')
-      # user = FactoryBot.build(:user, :loa1)
-      # allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-      # allow(controller).to receive(:current_user).and_return(user)
-      # cookie = sign_in(user, nil, true)
-      # binding.pry
-      # form = FactoryBot.create(:in_progress_form, user_uuid: user.uuid, form_id: 'edu-1990')
-      # binding.pry
       # return if Rails.env.production?
 
-      # User.first || FactoryBot.create(:user)
+      # user = FactoryBot.build(:user, :loa3)
+      # sign_in_as(user)
+      # FactoryBot.create(:in_progress_form, user_uuid: user.uuid, form_id: 'edu-1990')
     end
   end
 end
