5bda2239312050328f11676be6015ad3c5539f01
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-07-27 13:05:53 -0400

    no_op for show user profile provider states

--- spec/service_consumers/provider_states_for/users.rb
+++ spec/service_consumers/provider_states_for/users.rb
@@ -2,12 +2,13 @@
 
 Pact.provider_states_for 'Users' do
   provider_state 'show user profile' do
-    set_up do
-      # return if Rails.env.production?
-
-      # user = FactoryBot.build(:user, :loa3)
-      # sign_in_as(user)
-      # FactoryBot.create(:in_progress_form, user_uuid: user.uuid, form_id: 'edu-1990')
-    end
+    # set_up do
+    #   # return if Rails.env.production?
+    #
+    #   # user = FactoryBot.build(:user, :loa3)
+    #   # sign_in_as(user)
+    #   # FactoryBot.create(:in_progress_form, user_uuid: user.uuid, form_id: 'edu-1990')
+    # end
+    no_op
   end
 end
