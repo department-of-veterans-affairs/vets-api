9393222a89b3c2a7b19ba13099e31e48e89101ec
Derek Dyer <derek@digitalderek.com>
2020-07-28 19:56:49 -0400

    Add specs

--- spec/models/bgs_dependents/adult_child_attending_school_spec.rb
+++ spec/models/bgs_dependents/adult_child_attending_school_spec.rb
@@ -3,8 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe BGSDependents::AdultChildAttendingSchool do
-  describe 'stuff' do
-    it 'formats relationship params for submission' do
+  let(:all_flows_payload) do
+    payload = File.read("#{fixtures_path}/all_flows_payload.json")
+    JSON.parse(payload)
+  end
+  describe '#format_info' do
+    it 'formats info' do
+      dependent_application = all_flows_payload['dependent_application']
+      described_class.new(dependent_application, )
     end
   end
 end
