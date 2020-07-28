7a3288be83a5ed4a422d02bd4b7d0547248b9c5a
Derek Dyer <derek@digitalderek.com>
2020-07-28 14:41:26 -0400

    Add models

---
+++ app/models/bgs_dependents/base.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module BGSDependents
+  class Base
+    def format_date(date)
+      return nil if date.nil?
+
+      Date.parse(date).to_time.iso8601
+    end
+  end
+end
