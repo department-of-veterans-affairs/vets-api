2e86b82962be57d9147ee063f0c378214cc3ad36
Kathleen Crawford <kcrawford@governmentcio.com>
2020-07-28 16:48:42 -0700

    updating spec

--- spec/models/bgs_dependents/child_stopped_attending_school_spec.rb
+++ spec/models/bgs_dependents/child_stopped_attending_school_spec.rb
@@ -3,8 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe BGSDependents::ChildStoppedAttendingSchool do
-  describe 'stuff' do
+  describe '#format_info' do
+    let(:child_info) do
+      {
+        'date_child_left_school' => '2019-03-03',
+        'full_name' => { 'first' => 'Billy', 'middle' => 'Yohan', 'last' => 'Johnson', 'suffix' => 'Sr.' }
+      }
+    end
+    let(:formatted_params_result) do
+      {
+        'event_date' => '2019-03-03',
+        'first' => 'Billy',
+        'middle' => 'Yohan',
+        'last' => 'Johnson',
+        'suffix' => 'Sr.'
+      }
+    end
+
     it 'formats relationship params for submission' do
+      formatted_info = described_class.new(child_info).format_info
+
+      expect(formatted_info).to include(formatted_params_result)
     end
   end
 end
