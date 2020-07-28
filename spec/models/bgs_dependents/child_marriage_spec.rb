94c4f477ea4fc8cba8e2121c495caea59dd1db62
Kathleen Crawford <kcrawford@governmentcio.com>
2020-07-28 16:24:50 -0700

    adding specs

---
+++ spec/models/bgs_dependents/child_marriage_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BGSDependents::ChildMarriage do
+
+
+  describe 'stuff' do
+    it 'formats relationship params for submission' do
+
+    end
+  end
+end
---
+++ spec/models/bgs_dependents/step_child_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BGSDependents::StepChild do
+
+
+  describe 'stuff' do
+    it 'formats relationship params for submission' do
+
+    end
+  end
+end
