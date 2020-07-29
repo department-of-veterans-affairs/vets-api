da12b293fa92100e16f551fd36e4a3864349a278
Kathleen Crawford <kcrawford@governmentcio.com>
2020-07-29 16:26:22 -0700

    adding student_school

---
+++ lib/bgs/service.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module BGS
+  class Service
+    # include BGS::Exceptions::BGSErrors
+
+    def initialize(user)
+      @user = user
+    end
+
+    def create_child_school(child_school_params)
+      # with_multiple_attempts_enabled do
+        service.vnp_child_school.child_school_create(child_school_params)
+      # end
+    end
+
+    def create_child_student(child_student_params)
+      # with_multiple_attempts_enabled do
+        service.vnp_child_student.child_student_create(child_student_params)
+      # end
+    end
+
+    def bgs_auth
+      {
+        jrn_dt: Time.current.iso8601,
+        jrn_lctn_id: Settings.bgs.client_station_id,
+        jrn_status_type_cd: 'U',
+        jrn_user_id: Settings.bgs.client_username,
+        jrn_obj_id: Settings.bgs.application,
+        ssn: @user[:ssn] # Just here to make the mocks work
+      }
+    end
+
+    private
+
+    def service
+      @service ||= BGS::Services.new(external_uid: @user[:icn], external_key: @user[:external_key])
+    end
+  end
+end
---
+++ lib/bgs/student_school.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module BGS
+  class StudentSchool
+    def initialize(proc_id:, vnp_participant_id:, payload:, user:)
+      @user = user
+      @proc_id = proc_id
+      @vnp_participant_id = vnp_participant_id
+      @dependents_application = payload['dependents_application']
+    end
+
+    def create
+      bgs_service.create_child_school(child_school.params_for_686c)
+      bgs_service.create_child_student(child_student.params_for_686c)
+    end
+
+    private
+
+    def child_school
+      @child_student = BGSDependents::ChildSchool.new(
+        @dependents_application,
+        proc_participant_auth
+      )
+    end
+
+    def child_student
+      @child_student = BGSDependents::ChildStudent.new(
+        @dependents_application,
+        proc_participant_auth
+      )
+    end
+
+    def proc_participant_auth
+      {
+        vnp_proc_id: @proc_id,
+        vnp_ptcpnt_id: @vnp_participant_id
+      }.merge(bgs_service.bgs_auth)
+    end
+
+    def bgs_service
+      @service ||= BGS::Service.new(@user)
+    end
+  end
+end
