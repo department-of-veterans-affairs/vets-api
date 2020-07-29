804bebbbb91e8bbe0f133d4651863fcf12ad2419
Kathleen Crawford <kcrawford@governmentcio.com>
2020-07-28 17:18:59 -0700

    adding specs

--- spec/models/bgs_dependents/death_spec.rb
+++ spec/models/bgs_dependents/death_spec.rb
@@ -3,8 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe BGSDependents::Death do
-  describe 'stuff' do
-    it 'formats relationship params for submission' do
+  let(:death_info) do
+    {
+      'date' => '2019-03-03',
+      'vet_ind' => 'N',
+      'location' => { 'state' => 'CA', 'city' => 'Hollywood' },
+      'full_name' => { 'first' => 'Billy', 'middle' => 'Yohan', 'last' => 'Johnson', 'suffix' => 'Sr.' },
+      'dependent_type' => 'CHILD',
+      'child_status' => { 'child_under18' => true }
+    }
+  end
+  let(:formatted_params_result) do
+    {
+      :death_date => '2019-03-03',
+      :vet_ind => 'N',
+      'first' => 'Billy',
+      'middle' => 'Yohan',
+      'last' => 'Johnson',
+      'suffix' => 'Sr.'
+    }
+  end
+
+  describe '#format_info' do
+    it 'formats death params for submission' do
+      formatted_info = described_class.new(death_info).format_info
+
+      expect(formatted_info).to eq(formatted_params_result)
     end
   end
 end
