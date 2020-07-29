782ce015db1dbe1d6ecaad91263b291cd5042d1d
Kathleen Crawford <kcrawford@governmentcio.com>
2020-07-28 17:40:16 -0700

    updating spec

--- spec/models/bgs_dependents/child_marriage_spec.rb
+++ spec/models/bgs_dependents/child_marriage_spec.rb
@@ -3,8 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe BGSDependents::ChildMarriage do
-  describe 'stuff' do
-    it 'formats relationship params for submission' do
+  let(:child_marriage_info) do
+    {
+      'date_married' => '1977-02-01',
+      'full_name' => { 'first' => 'Billy', 'middle' => 'Yohan', 'last' => 'Johnson', 'suffix' => 'Sr.' }
+    }
+  end
+  let(:formatted_params_result) do
+    {
+      'event_date' => '1977-02-01',
+      'first' => 'Billy',
+      'middle' => 'Yohan',
+      'last' => 'Johnson',
+      'suffix' => 'Sr.'
+    }
+  end
+
+  describe '#format_info' do
+    it 'formats child marriage params for submission' do
+      formatted_info = described_class.new(child_marriage_info).format_info
+
+      expect(formatted_info).to eq(formatted_params_result)
     end
   end
 end
