4b1edb69776503063982b5426232ba4757bac0fd
Derek Dyer <derek@digitalderek.com>
2020-07-29 12:50:57 -0400

    Add vnp_benefit

--- lib/bgs/service.rb
+++ lib/bgs/service.rb
@@ -8,38 +8,16 @@ module BGS
       @user = user
     end
 
-    # def create_proc
-    #   with_multiple_attempts_enabled do
-    #     service.vnp_proc_v2.vnp_proc_create(
-    #       { vnp_proc_type_cd: 'DEPCHG', vnp_proc_state_type_cd: 'Started' }.merge(bgs_auth)
-    #     )
-    #   end
-    # end
-    #
-    # def create_proc_form(vnp_proc_id)
-    #   with_multiple_attempts_enabled do
-    #     service.vnp_proc_form.vnp_proc_form_create(
-    #       { vnp_proc_id: vnp_proc_id,  form_type_cd: '21-686c' }.merge(bgs_auth)
-    #     )
-    #   end
-    # end
-    #
-    # def create_participant(proc_id, corp_ptcpnt_id = nil)
-    #   with_multiple_attempts_enabled do
-    #     service.vnp_ptcpnt.vnp_ptcpnt_create(
-    #       { vnp_proc_id: proc_id, ptcpnt_type_nm: 'Person', corp_ptcpnt_id: corp_ptcpnt_id }.merge(bgs_auth)
-    #     )
-    #   end
-    # end
-    #
-    # def create_person(person_params)
-    #   with_multiple_attempts_enabled do
-    #     service.vnp_person.vnp_person_create(person_params.merge(bgs_auth))
-    #   end
-    # end
+    def vnp_create_benefit_claim(vnp_benefit_params)
+      with_multiple_attempts_enabled do
+        service.vnp_bnft_claim.vnp_bnft_claim_create(vnp_benefit_params)
+      end
+    end
 
-    def create_relationship(relationship_params)
-      service.vnp_ptcpnt_rlnshp.vnp_ptcpnt_rlnshp_create(relationship_params.merge(bgs_auth))
+    def vnp_benefit_claim_update(vnp_benefit_params)
+      with_multiple_attempts_enabled do
+        service.vnp_bnft_claim.vnp_bnft_claim_update(vnp_benefit_params)
+      end
     end
 
     def bgs_auth
