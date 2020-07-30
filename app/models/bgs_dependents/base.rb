f14c6fd1a5b374e2d19ede84a3b05df0ee1ec952
Derek Dyer <derek@digitalderek.com>
2020-07-30 10:37:00 -0400

    Update base class to inherit from Common::Base

--- app/models/bgs_dependents/base.rb
+++ app/models/bgs_dependents/base.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module BGSDependents
-  class Base
+  class Base < Common::Base
     def dependent_address(dependents_application, lives_with_vet, alt_address)
       return dependents_application.dig('veteran_contact_information', 'veteran_address') if lives_with_vet
 
