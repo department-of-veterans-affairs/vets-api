9b4f35e75969c3dfe474478c694fd280e78fbeab
camerontesterman <cameronbtesterman@gmail.com>
2020-07-30 15:41:16 -0500

    error handling

--- lib/debts/service.rb
+++ lib/debts/service.rb
@@ -5,7 +5,44 @@ module Debts
     configuration Debts::Configuration
 
     def get_letters(body)
-      GetLettersResponse.new(perform(:post, 'letterdetails/get', body).body)
+      with_monitoring do
+        GetLettersResponse.new(perform(:post, 'letterdetails/get', body).body)
+      end
+    rescue => e
+      handle_error(e)
+    end
+
+    private
+
+    def save_error_details(error)
+      Raven.tags_context(
+        external_service: self.class.to_s.underscore
+      )
+
+      Raven.extra_context(
+        url: config.base_path,
+        message: error.message,
+        body: error.body
+      )
+    end
+
+    def handle_error(error)
+      case error
+      when Common::Client::Errors::ClientError
+        handle_client_error(error)
+      else
+        raise error
+      end
+    end
+
+    def handle_client_error(status)
+      save_error_details(error)
+
+      raise_backend_exception(
+          "DEBTS#{error&.status}", 
+          self.class, 
+          error
+        )
     end
   end
 end
