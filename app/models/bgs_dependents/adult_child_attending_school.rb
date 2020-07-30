8e67c7d954d23ac39866eb0b2e961a4efdbae6fa
Derek Dyer <derek@digitalderek.com>
2020-07-30 19:20:08 -0400

    Update from feedback

--- app/models/bgs_dependents/adult_child_attending_school.rb
+++ app/models/bgs_dependents/adult_child_attending_school.rb
@@ -10,10 +10,14 @@ module BGSDependents
     attribute :birth_date, String
     attribute :ever_married_ind, String
 
+    validates :first, presence: true
+    validates :last, presence: true
+
     def initialize(dependents_application)
       @dependents_application = dependents_application
-      @was_married = @dependents_application.dig('student_address_marriage_tuition', 'was_married')
+      @was_married = @dependents_application['student_address_marriage_tuition']['was_married']
       @name_and_ssn = @dependents_application['student_name_and_ssn']
+
       self.attributes = child_attributes
     end
 
@@ -28,9 +32,11 @@ module BGSDependents
     private
 
     def child_attributes
+      # we will raise an error here if not #valid? when we merge in exception PR
+
       {
-        ssn: @name_and_ssn.dig('ssn'),
-        birth_date: @name_and_ssn.dig('birth_date'),
+        ssn: @name_and_ssn['ssn'],
+        birth_date: @name_and_ssn['birth_date'],
         ever_married_ind: @was_married == true ? 'Y' : 'N'
       }.merge(@name_and_ssn.dig('full_name'))
     end
