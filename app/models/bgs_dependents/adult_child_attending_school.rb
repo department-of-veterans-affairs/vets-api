b9f0234b8ef1c9eaff4f3ae643bd44e2d84cebc3
Derek Dyer <derek@digitalderek.com>
2020-07-30 11:48:37 -0400

    Add attributes

--- app/models/bgs_dependents/adult_child_attending_school.rb
+++ app/models/bgs_dependents/adult_child_attending_school.rb
@@ -1,23 +1,38 @@
 # frozen_string_literal: true
 
 module BGSDependents
-  class AdultChildAttendingSchool
+  class AdultChildAttendingSchool < Base
+    attribute :first, String
+    attribute :middle, String
+    attribute :last, String
+    attribute :suffix, String
+    attribute :ssn, String
+    attribute :birth_date, String
+    attribute :ever_married_ind, String
+
     def initialize(dependents_application)
       @dependents_application = dependents_application
-      @name_and_ssn = @dependents_application['student_name_and_ssn']
       @was_married = @dependents_application.dig('student_address_marriage_tuition', 'was_married')
+      @name_and_ssn = @dependents_application['student_name_and_ssn']
+      self.attributes = child_attributes
     end
 
     def format_info
-      {
-        'ssn': @name_and_ssn['ssn'],
-        'birth_date': @name_and_ssn['birth_date'],
-        'ever_married_ind': @was_married == true ? 'Y' : 'N'
-      }.merge(@name_and_ssn['full_name']).with_indifferent_access
+      self.attributes.with_indifferent_access
     end
 
     def address
       @dependents_application['student_address_marriage_tuition']['address']
+    end
+
+    private
+
+    def child_attributes
+      {
+        ssn: @name_and_ssn.dig('ssn'),
+        birth_date: @name_and_ssn.dig('birth_date'),
+        ever_married_ind: @was_married == true ? 'Y' : 'N'
+      }.merge(@name_and_ssn.dig('full_name'))
     end
   end
 end
