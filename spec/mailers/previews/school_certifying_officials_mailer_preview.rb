bc9b0d5600b5d169ae882ef2242c5bc70ca34d5e
Matt Roth <roth_matthew@bah.com>
2020-07-30 13:12:21 -0400

    Update school_certifying_officials_mailer_preview.rb

--- spec/mailers/previews/school_certifying_officials_mailer_preview.rb
+++ spec/mailers/previews/school_certifying_officials_mailer_preview.rb
@@ -9,19 +9,11 @@ class SchoolCertifyingOfficialsMailerPreview < ActionMailer::Preview
     SchoolCertifyingOfficialsMailer.build(applicant, recipients(params[:facility_code]), ga_client_id) if applicant.present?
   end
 
-  def build2
-    return unless FeatureFlipper.staging_email?
-
-    applicant = SavedClaim::EducationBenefits::VA10203.last&.open_struct_form
-    recipients = []
-    ga_client_id = nil
-    SchoolCertifyingOfficialsMailer.build(applicant, recipients, ga_client_id) if applicant.present?
-  end
-
   private
 
   def recipients(facility_code)
     institution = GIDSRedis.new.get_institution_details({ id: facility_code })[:data][:attributes]
+    return [] unless institution.present?
 
     scos = institution[:versioned_school_certifying_officials]
     EducationForm::SendSCOEmail.sco_emails(scos)
