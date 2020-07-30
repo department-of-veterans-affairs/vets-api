7109932d02772056e488dd69e8bf5097bb227af8
Kathleen Crawford <kcrawford@governmentcio.com>
2020-07-29 18:34:52 -0700

    adding relationship

---
+++ lib/bgs/service.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module BGS
+  class Service
+    def initialize(user)
+      @user = user
+    end
+
+    def create_relationship(relationship_params)
+      service.vnp_ptcpnt_rlnshp.vnp_ptcpnt_rlnshp_create(relationship_params.merge(bgs_auth))
+    end
+
+    def bgs_auth
+      {
+        jrn_dt: Time.current.iso8601,
+        jrn_lctn_id: Settings.bgs.client_station_id,
+        jrn_status_type_cd: 'U',
+        jrn_user_id: Settings.bgs.client_username,
+        jrn_obj_id: Settings.bgs.application,
+        ssn: @user[:ssn] # Just here to make the mocks work
+      }
+    end
+
+    private
+
+    def service
+      @service ||= BGS::Services.new(external_uid: @user[:icn], external_key: @user[:external_key])
+    end
+  end
+end
