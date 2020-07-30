b58d47efa5fe590a84eedf37ae0512c556e002fe
Kathleen Crawford <kcrawford@governmentcio.com>
2020-07-29 21:47:15 -0700

    adding dependents

---
+++ lib/bgs/dependents.rb
@@ -0,0 +1,177 @@
+# frozen_string_literal: true
+
+module BGS
+  class Dependents
+    def initialize(proc_id:, payload:, user:)
+      @proc_id = proc_id
+      @payload = payload
+      @dependents = []
+      @dependents_application = @payload['dependents_application']
+      @user = user
+    end
+
+    # rubocop:disable Metrics/PerceivedComplexity
+    # rubocop:disable Metrics/CyclomaticComplexity
+    def create
+      report_674 if @payload['report674']
+      add_children if @payload['add_child']
+      report_deaths if @payload['report_death']
+      report_divorce if @payload['report_divorce']
+      report_stepchild if @payload['report_stepchild_not_in_household']
+      report_child_event('child_marriage') if @payload['report_marriage_of_child_under18']
+      # report_child_event('not_attending_school') if @payload['report_child18_or_older_is_not_attending_school']
+
+      @dependents
+    end
+
+    # rubocop:enable Metrics/CyclomaticComplexity
+    # rubocop:enable Metrics/PerceivedComplexity
+
+    private
+
+    def add_children
+=begin
+      @dependents_application['children_to_add'].each do |child_info|
+        child = BGSDependents::Child.new(child_info)
+        formatted_info = child.format_info
+        participant = bgs_service.create_participant(@proc_id)
+
+        bgs_service.create_person(person_params(child, participant, formatted_info))
+        send_address(child, participant, child.address(@dependents_application))
+
+        @dependents << child.serialize_dependent_result(
+          participant,
+          'Child',
+          formatted_info['family_relationship_type'],
+          {
+            marriage_termination_type_code: formatted_info['reason_marriage_ended'],
+            type: 'child'
+          }
+        )
+      end
+=end
+    end
+
+    def report_deaths
+      @dependents_application['deaths'].each do |death_info|
+        death = BGSDependents::Death.new(death_info)
+        relationship_types = death.relationship_type(death_info)
+
+        next if relationship_types[:family] == 'Child' # BGS does not support child death at this time
+
+        formatted_info = death.format_info
+        death_info['location']['state_code'] = death_info['location'].delete('state')
+        participant = bgs_service.create_participant(@proc_id)
+        bgs_service.create_person(person_params(death, participant, formatted_info))
+        # I think we need the death_location instead of creating an address
+        # There is no support in the API for death location
+        # create_address(@proc_id, participant[:vnp_ptcpnt_id], death_info['location'])
+
+        @dependents << death.serialize_dependent_result(
+          participant,
+          relationship_types[:participant],
+          relationship_types[:family],
+          { type: 'death' }
+        )
+      end
+    end
+
+    def report_divorce
+      divorce = BGSDependents::Divorce.new(@dependents_application['report_divorce'])
+      formatted_info = divorce.format_info
+      participant = bgs_service.create_participant(@proc_id)
+      bgs_service.create_person(person_params(divorce, participant, formatted_info))
+
+      @dependents << divorce.serialize_dependent_result(
+        participant,
+        'Spouse',
+        'Spouse',
+        {
+          divorce_state: formatted_info['divorce_state'],
+          divorce_city: formatted_info['divorce_city'],
+          marriage_termination_type_code: formatted_info['marriage_termination_type_code']
+        }
+      )
+    end
+
+    def report_stepchild
+      @dependents_application['step_children'].each do |stepchild_info|
+        step_child = BGSDependents::StepChild.new(stepchild_info)
+        formatted_info = step_child.format_info
+        participant = bgs_service.create_participant(@proc_id)
+        bgs_service.create_person(person_params(step_child, participant, formatted_info))
+        send_address(step_child, participant, stepchild_info['address'])
+
+        @dependents << step_child.serialize_dependent_result(
+          participant,
+          'Child',
+          'Stepchild',
+          {
+            living_expenses_paid: formatted_info['living_expenses_paid'],
+            'type': 'stepchild'
+          }
+        )
+      end
+    end
+
+    def report_child_event(event_type)
+      child_event = child_event_type(event_type)
+      formatted_info = child_event.format_info
+      participant = bgs_service.create_participant(@proc_id)
+
+      bgs_service.create_person(person_params(child_event, participant, formatted_info))
+
+      @dependents << child_event.serialize_dependent_result(
+        participant,
+        'Child',
+        'Other',
+        {
+          'event_date': formatted_info['event_date'],
+          'type': event_type
+        }
+      )
+    end
+
+    def report_674
+=begin
+      adult_attending_school = BGSDependents::AdultChildAttendingSchool.new(
+        @dependents_application
+      )
+      formatted_info = adult_attending_school.format_info
+      participant = bgs_service.create_participant(@proc_id)
+      bgs_service.create_person(person_params(adult_attending_school, participant, formatted_info))
+      send_address(adult_attending_school, participant, adult_attending_school.address)
+
+      @dependents << adult_attending_school.serialize_dependent_result(
+        participant,
+        'Child',
+        'Other',
+        { 'type': '674' }
+      )
+=end
+    end
+
+    def child_event_type(event_type)
+      if event_type == 'child_marriage'
+        return BGSDependents::ChildMarriage.new(@dependents_application['child_marriage'])
+      end
+
+      # BGSDependents::ChildStoppedAttendingSchool.new(@dependents_application['child_stopped_attending_school'])
+    end
+
+    def person_params(calling_object, participant, dependent_info)
+      calling_object.create_person_params(@proc_id, participant[:vnp_ptcpnt_id], dependent_info)
+    end
+
+    def send_address(calling_object, participant, address_info)
+      address = calling_object.generate_address(address_info)
+      address_params = calling_object.create_address_params(@proc_id, participant[:vnp_ptcpnt_id], address)
+
+      bgs_service.create_address(address_params)
+    end
+
+    def bgs_service
+      @bgs_service ||= BGS::Service.new(@user)
+    end
+  end
+end
---
+++ lib/bgs/service.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module BGS
+  class Service
+    def initialize(user)
+      @user = user
+    end
+
+    def create_participant(proc_id, corp_ptcpnt_id = nil)
+      service.vnp_ptcpnt.vnp_ptcpnt_create(
+        { vnp_proc_id: proc_id, ptcpnt_type_nm: 'Person', corp_ptcpnt_id: corp_ptcpnt_id }.merge(bgs_auth)
+      )
+    end
+
+    def create_person(person_params)
+      service.vnp_person.vnp_person_create(person_params.merge(bgs_auth))
+    end
+
+    def create_address(address_params)
+      service.vnp_ptcpnt_addrs.vnp_ptcpnt_addrs_create(
+        address_params.merge(bgs_auth)
+      )
+    end
+=begin
+    def create_phone(proc_id, participant_id, payload)
+      with_multiple_attempts_enabled do
+        service.vnp_ptcpnt_phone.vnp_ptcpnt_phone_create(
+          {
+            vnp_proc_id: proc_id,
+            vnp_ptcpnt_id: participant_id,
+            phone_type_nm: 'Daytime',
+            phone_nbr: payload['phone_number'],
+            efctv_dt: Time.current.iso8601
+          }.merge(bgs_auth)
+        )
+      end
+    end
+
+    def create_relationship(relationship_params)
+      with_multiple_attempts_enabled do
+        service.vnp_ptcpnt_rlnshp.vnp_ptcpnt_rlnshp_create(relationship_params)
+      end
+    end
+=end
+    def bgs_auth
+      {
+        jrn_dt: Time.current.iso8601,
+        jrn_lctn_id: Settings.bgs.client_station_id,
+        jrn_status_type_cd: 'U',
+        jrn_user_id: Settings.bgs.client_username,
+        jrn_obj_id: Settings.bgs.application,
+        ssn: @user[:ssn] # Just here to make the mocks work
+      }
+    end
+
+    private
+
+    def service
+      @service ||= BGS::Services.new(external_uid: @user[:icn], external_key: @user[:external_key])
+    end
+  end
+end
