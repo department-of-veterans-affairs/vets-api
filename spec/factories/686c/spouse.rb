0c0351e72d8948a961fbfa3a8cc7f9987fdfd77b
Kathleen Crawford <kcrawford@governmentcio.com>
2020-07-31 10:19:40 -0700

    linting

--- spec/factories/686c/spouse.rb
+++ spec/factories/686c/spouse.rb
@@ -4,47 +4,46 @@ FactoryBot.define do
   factory :spouse, class: BGSDependents::Spouse do
     initialize_with do
       {
-        "add_spouse" => true,
-        "dependents_application" => {
-          "veteran_contact_information" => {
-            "veteran_address" => {
-              "country_name" => "USA",
-              "address_line1" => "8200 Doby LN",
-              "city" => "Pasadena",
-              "state_code" => "CA",
-              "zip_code" => "21122"
+        'add_spouse' => true,
+        'dependents_application' => {
+          'veteran_contact_information' => {
+            'veteran_address' => {
+              'country_name' => 'USA',
+              'address_line1' => '8200 Doby LN',
+              'city' => 'Pasadena',
+              'state_code' => 'CA',
+              'zip_code' => '21122'
             },
-            "phone_number" => "1112223333",
-            "email_address" => "foo@foo.com"
+            'phone_number' => '1112223333',
+            'email_address' => 'foo@foo.com'
           },
-          "does_live_with_spouse" => {
-            "spouse_does_live_with_veteran" => true
+          'does_live_with_spouse' => {
+            'spouse_does_live_with_veteran' => true
           },
-          "spouse_information" => {
-            "full_name" => {
-              "first" => "Jenny",
-              "middle" => "Lauren",
-              "last" => "McCarthy",
-              "suffix" => "Sr."
+          'spouse_information' => {
+            'full_name' => {
+              'first' => 'Jenny',
+              'middle' => 'Lauren',
+              'last' => 'McCarthy',
+              'suffix' => 'Sr.'
             },
-            "ssn" => "323454323",
-            "birth_date" => "1981-04-04",
-            "is_veteran" => true,
-            "va_file_number" => "00000000",
-            "service_number" => "11111111"
+            'ssn' => '323454323',
+            'birth_date' => '1981-04-04',
+            'is_veteran' => true,
+            'va_file_number' => '00000000',
+            'service_number' => '11111111'
           },
-          "current_marriage_information" => {
-            "date" => "2014-03-04",
-            "location" => {
-              "state" => "CA",
-              "city" => "Slawson"
+          'current_marriage_information' => {
+            'date' => '2014-03-04',
+            'location' => {
+              'state' => 'CA',
+              'city' => 'Slawson'
             },
-            "type" => "OTHER",
-            "type_other" => "Some Other Thing"
+            'type' => 'OTHER',
+            'type_other' => 'Some Other Thing'
           }
         }
       }
     end
   end
 end
-
