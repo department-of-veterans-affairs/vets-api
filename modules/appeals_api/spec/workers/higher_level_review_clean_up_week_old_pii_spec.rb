cc085dd34ae565db69a9563bf7cab84b26561f3e
Ed Mangimelli <edmangimelli@gmail.com>
2020-08-04 16:38:06 -0400

    add a couple more tests

--- modules/appeals_api/spec/workers/higher_level_review_clean_up_week_old_pii_spec.rb
+++ modules/appeals_api/spec/workers/higher_level_review_clean_up_week_old_pii_spec.rb
@@ -10,5 +10,21 @@ describe AppealsApi::HigherLevelReviewCleanUpWeekOldPii, type: :job do
       described_class.new.perform
       expect(AppealsApi::HigherLevelReview.has_pii).to be_empty
     end
+
+    it 'does not log to sentry if there was nothing to work on' do
+      expect(AppealsApi::HigherLevelReview.has_pii).to be_empty
+      worker = described_class.new
+      expect(worker).not_to receive(:log_message_to_sentry)
+      worker.perform
+    end
+
+    it 'logs to sentry if no pii was removed /but/ some should have been' do
+      allow_any_instance_of(described_class).to(
+        receive(:no_higher_level_reviews_are_ready_to_have_pii_expunged?).and_return(false)
+      )
+      worker = described_class.new
+      expect(worker).to receive(:log_message_to_sentry)
+      worker.perform
+    end
   end
 end
