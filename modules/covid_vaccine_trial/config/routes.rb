f7d1e609851411668267137fe0a4643f32b9f70e
Stephen Barrs <stephen.barrs@va.gov>
2020-08-05 08:29:49 -0400

    update put to post for covide vaccine route

--- modules/covid_vaccine_trial/config/routes.rb
+++ modules/covid_vaccine_trial/config/routes.rb
@@ -1,5 +1,5 @@
 CovidVaccineTrial::Engine.routes.draw do
   namespace :screener, defaults: { format: :json } do
-    put 'create', to: 'submissions#create'
+    post 'create', to: 'submissions#create'
   end
 end
