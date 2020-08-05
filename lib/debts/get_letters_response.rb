1d10571b164b3d0abf4c82a079e191fcd2a75f4e
camerontesterman <cameronbtesterman@gmail.com>
2020-08-05 11:04:17 -0500

    setup debts schema

--- lib/debts/get_letters_response.rb
+++ lib/debts/get_letters_response.rb
@@ -3,11 +3,19 @@
 module Debts
   class GetLettersResponse
     def initialize(res)
+      validate_response_against_schema(res)
       @res = res
     end
 
     def to_json(*_args)
       @res.to_json
+    end
+
+    private
+
+    def validate_response_against_schema(response)
+      schema_path = Rails.root.join('lib', 'debts', 'schemas', "debts.json").to_s
+      JSON::Validator.validate!(schema_path, response.body, strict: false)
     end
   end
 end
