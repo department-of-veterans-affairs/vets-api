f2e1837b80d027e629ceec956caed57b596276f0
kathleencrawford <53828142+kathleencrawford@users.noreply.github.com>
2020-08-07 09:00:35 -0700

    [686] [lib] Add Veteran (#4619)

--- lib/bgs/service.rb
+++ lib/bgs/service.rb
@@ -20,7 +20,12 @@ module BGS
 
     def create_participant(proc_id, corp_ptcpnt_id = nil)
       service.vnp_ptcpnt.vnp_ptcpnt_create(
-        { vnp_proc_id: proc_id, ptcpnt_type_nm: 'Person', corp_ptcpnt_id: corp_ptcpnt_id }.merge(bgs_auth)
+        {
+          vnp_proc_id: proc_id,
+          ptcpnt_type_nm: 'Person',
+          corp_ptcpnt_id: corp_ptcpnt_id,
+          ssn: @user[:ssn]
+        }.merge(bgs_auth)
       )
     end
 
@@ -32,6 +37,36 @@ module BGS
       service.vnp_ptcpnt_addrs.vnp_ptcpnt_addrs_create(
         address_params.merge(bgs_auth)
       )
+    end
+
+    def create_phone(proc_id, participant_id, payload)
+      service.vnp_ptcpnt_phone.vnp_ptcpnt_phone_create(
+        {
+          vnp_proc_id: proc_id,
+          vnp_ptcpnt_id: participant_id,
+          phone_type_nm: 'Daytime',
+          phone_nbr: payload['phone_number'],
+          efctv_dt: Time.current.iso8601
+        }.merge(bgs_auth)
+      )
+    end
+
+    def find_benefit_claim_type_increment
+      increment_params = {
+        ptcpnt_id: @user[:participant_id],
+        bnft_claim_type_cd: '130DPNEBNADJ',
+        pgm_type_cd: 'CPL'
+      }
+
+      increment_params.merge!(user_ssn) if Settings.bgs.mock_response == true
+
+      service.data.find_benefit_claim_type_increment(increment_params)
+    end
+
+    def get_va_file_number
+      person = service.people.find_person_by_ptcpnt_id(@user[:participant_id])
+
+      person[:file_nbr]
     end
 
     def vnp_create_benefit_claim(vnp_benefit_params)
@@ -55,7 +90,7 @@ module BGS
         jrn_obj_id: Settings.bgs.application
       }
 
-      auth_params.merge!(user_ssn) unless Rails.env.production?
+      auth_params.merge!(user_ssn) if Settings.bgs.mock_response == true
 
       auth_params
     end
