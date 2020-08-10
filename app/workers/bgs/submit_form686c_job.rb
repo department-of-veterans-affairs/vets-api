c34a3d93fb04a93feb53fbc2fecbc1f01e672dd1
Derek Dyer <derek@digitalderek.com>
2020-08-10 17:29:45 -0400

    Add fix for specs

--- app/workers/bgs/submit_form686c_job.rb
+++ app/workers/bgs/submit_form686c_job.rb
@@ -9,13 +9,13 @@ module BGS
     # we do individual service retries in lib/bgs/service.rb
     sidekiq_options retry: false
 
-    def perform(user_uuid, saved_claim_id, va_file_number_with_payload)
+    def perform(user_uuid, saved_claim_id, vet_info)
       user = User.find(user_uuid)
       claim = SavedClaim::DependencyClaim.find(saved_claim_id)
 
       raise Invalid686cClaim unless claim.valid?(:run_686_form_jobs)
 
-      claim.add_veteran_info(va_file_number_with_payload)
+      claim.add_veteran_info(vet_info)
 
       BGS::Form686c.new(user).submit(claim.parsed_form)
     end
