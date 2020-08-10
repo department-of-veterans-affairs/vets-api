80eccbe7d51286b63d61ab40419546feb60c8d77
Derek Dyer <derek@digitalderek.com>
2020-08-10 15:33:17 -0400

    Add changes

--- app/workers/bgs/submit_form686c_job.rb
+++ app/workers/bgs/submit_form686c_job.rb
@@ -2,18 +2,21 @@
 
 module BGS
   class SubmitForm686cJob
+    class Invalid686cClaim < StandardError; end
     include Sidekiq::Worker
+    include SentryLogging
 
     # we do individual service retries in lib/bgs/service.rb
     sidekiq_options retry: false
 
-    def perform(_user_uuid, saved_claim_id, va_file_number_with_payload)
+    def perform(user_uuid, saved_claim_id, va_file_number_with_payload)
       user = User.find(user_uuid)
       claim = SavedClaim::DependencyClaim.find(saved_claim_id)
 
+      raise Invalid686cClaim unless claim.valid?(:run_686_form_jobs)
+
       claim.add_veteran_info(va_file_number_with_payload)
 
-      # This PR is blocked until others are ready
       BGS::Form686c.new(user).submit(claim.parsed_form)
     end
   end
