02730d1c83515266f5ac1f9ddb0d250672a2fd00
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-08-11 12:33:54 -0400

    [department-of-veterans-affairs/orchid#17] checks for feature flag
    Co-authored-by: Anita DeWitt <adewitt@thoughtworks.com>
    Co-authored-by: Jacob Gacek <jacob.gacek@thoughtworks.com>

--- app/controllers/v0/ask/asks_controller.rb
+++ app/controllers/v0/ask/asks_controller.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 module V0
-    module Ask
-        class AsksController < ApplicationController
-            skip_before_action :authenticate, only: :create
+  module Ask
+    class AsksController < ApplicationController
+      skip_before_action :authenticate, only: :create
 
-            def create
-                render json: {"message": "200 ok"}
-            end
-        end
+      def create
+        return service_unavailable unless Flipper.enabled?(:get_help_ask_form)
+
+        render json: { 'message': '200 ok' }
+      end
+
+      private
+
+      def service_unavailable
+        render nothing: true, status: :service_unavailable, as: :json
+      end
     end
+  end
 end
--- spec/controllers/v0/ask/asks_controller_spec.rb
+++ spec/controllers/v0/ask/asks_controller_spec.rb
@@ -3,20 +3,34 @@
 require 'rails_helper'
 require 'support/controller_spec_helper'
 
-RSpec.describe V0::Ask::AsksController,  type: :controller do
-    # Test Cases:
-    # If the feature toggle is on or off
-    #   If on and you get a form return 200
-    #   If off then do nothing and return an error
-    # If the form data is valid or invalid
+RSpec.describe V0::Ask::AsksController, type: :controller do
+  # Test Cases:
+  # If the feature toggle is on or off
+  #   If on and you get a form return 200
+  #   If off then do nothing and return an error
+  # If the form data is valid or invalid
   describe '#create' do
     def send_create
       post(:create, params: { ask: { form: form } })
     end
+
     context 'when Flipper :get_help_ask_form is' do
+      context 'disabled' do
+        it 'renders :service_unavailable' do
+          expect(Flipper).to receive(:enabled?).with(:get_help_ask_form).and_return(false)
+
+          post :create
+
+          expect(response).to have_http_status(:service_unavailable)
+        end
+      end
+
       context 'enabled' do
         it 'returns 200' do
+          expect(Flipper).to receive(:enabled?).with(:get_help_ask_form).and_return(true)
+
           post :create
+
           expect(response).to have_http_status(:ok)
         end
       end
