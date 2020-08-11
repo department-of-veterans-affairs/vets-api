0ea71f021cdd05123d014bc05ebd24fd0a7b0989
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-08-11 10:59:13 -0400

    linting

--- spec/service_consumers/provider_states_for/users.rb
+++ spec/service_consumers/provider_states_for/users.rb
@@ -1,9 +1,9 @@
-# frozen_string_literal: true 
+# frozen_string_literal: true
 
 Pact.provider_states_for 'User' do
   provider_state 'user is authenticated' do
     set_up do
-      user = FactoryBot.build(:user, :loa3, va_patient: true, middle_name: "J" )
+      user = FactoryBot.build(:user, :loa3, va_patient: true, middle_name: 'J')
       session_object = sign_in(user, nil, nil, true)
       allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(session_object.to_hash)
     end
