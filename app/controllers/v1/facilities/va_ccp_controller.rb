d021ba90056e3c7a780308bcdb791fb700927694
Lance Sanchez <DarkBushido@users.noreply.github.com>
2020-08-11 18:53:55 -0500

    Removing the code duplication

--- app/controllers/v1/facilities/va_ccp_controller.rb
+++ app/controllers/v1/facilities/va_ccp_controller.rb
@@ -38,17 +38,6 @@ class V1::Facilities::VaCcpController < FacilitiesController
     Lighthouse::Facilities::Client.new
   end
 
-  def pagination_params
-    hsh = super
-    page = Integer(hsh[:page] || 1)
-    per_page = Integer(hsh[:per_page] || 1)
-    total_entries = page * per_page + 1
-    hsh.compact.transform_values!(&:to_i)
-    hsh.merge(
-      total_entries: total_entries
-    )
-  end
-
   def ppms_params
     params.permit(
       :address,
