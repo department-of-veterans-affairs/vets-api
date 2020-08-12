9a26148879cc22484a17aa20d069e4d4067186ff
Philip Becker <boofungal316@yahoo.com>
2020-08-12 11:45:40 -0400

    test for GET :index of v0/forms

--- modules/va_forms/spec/requests/v0/forms_request_spec.rb
+++ modules/va_forms/spec/requests/v0/forms_request_spec.rb
@@ -15,9 +15,19 @@ RSpec.describe 'VA Forms', type: :request do
       expect(response).to match_response_schema('va_forms/forms')
     end
 
+    it 'returns the forms when camel-inflected' do
+      get base_url, headers: inflection_header
+      expect(response).to match_camelized_response_schema('va_forms/forms')
+    end
+
     it 'correctly returns a matched query' do
       get "#{base_url}?query=526"
       expect(response).to match_response_schema('va_forms/forms')
+    end
+
+    it 'correctly returns a matched query when camel-inlfected' do
+      get "#{base_url}?query=526", headers: inflection_header
+      expect(response).to match_camelized_response_schema('va_forms/forms')
     end
 
     it 'correctly returns a matched query while ignoring leading and trailing whitespace' do
@@ -25,9 +35,19 @@ RSpec.describe 'VA Forms', type: :request do
       expect(response).to match_response_schema('va_forms/forms')
     end
 
+    it 'correctly returns a matched query while ignoring leading and trailing whitespace when camel-inflected' do
+      get "#{base_url}?query=%20526%20", headers: inflection_header
+      expect(response).to match_camelized_response_schema('va_forms/forms')
+    end
+
     it 'correctly returns a matched query using keywords separated by whitespace' do
       get "#{base_url}?query=disability%20form"
       expect(response).to match_response_schema('va_forms/forms')
+    end
+
+    it 'correctly returns a matched query using keywords separated by whitespace when camel-inflected' do
+      get "#{base_url}?query=disability%20form", headers: inflection_header
+      expect(response).to match_camelized_response_schema('va_forms/forms')
     end
   end
 
