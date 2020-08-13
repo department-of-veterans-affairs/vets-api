98d52a816c487d54040fb82b43345c4ef9922852
John Paul Ashenfelter <johnpaulashenfelter@users.noreply.github.com>
2020-08-13 08:20:26 -0400

    Clean up old migrations (#4561)

--- db/migrate/20200213231738_add_sec_id_to_accounts.rb
+++
@@ -1,5 +0,0 @@
-class AddSecIdToAccounts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :accounts, :sec_id, :string
-  end
-end
--- modules/appeals_api/db/migrate/20200508201027_change_hlr_status_to_string.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeHlrStatusToString < ActiveRecord::Migration[6.0]
-  def change
-    change_column :appeals_api_higher_level_reviews, :status, :string, default: 'pending', null: false
-  end
-end
