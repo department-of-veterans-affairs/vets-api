d831a7a7811b6b1cecd6cdf3463642db8434303e
John Paul Ashenfelter <johnpaulashenfelter@users.noreply.github.com>
2020-08-13 08:20:26 -0400

    Clean up old migrations (#4561)

--- db/migrate/20180517173822_add_disability_compensation_indexes.rb
+++
@@ -1,9 +0,0 @@
-class AddDisabilityCompensationIndexes < ActiveRecord::Migration[4.2]
-  disable_ddl_transaction!
-
-  def change
-    add_index :disability_compensation_submissions, [:user_uuid, :form_type], \
-      name: 'index_disability_compensation_submissions_on_uuid_and_form_type', unique: true, algorithm: :concurrently
-    add_index :disability_compensation_submissions, :claim_id, unique: true, algorithm: :concurrently
-  end
-end
