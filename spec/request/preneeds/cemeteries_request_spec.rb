b8567bc5159a8a9cd9ca2476f46708372d335736
Philip Becker <philip.becker@oddball.io>
2020-08-14 13:15:24 -0400

    11585 add camel inflected schemas to preneeds (#4725)
    * camelCase schema tests for /v0/preneeds

--- spec/request/preneeds/cemeteries_request_spec.rb
+++ spec/request/preneeds/cemeteries_request_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe 'Cemeteries Integration', type: :request do
     expect(response.body).to be_a(String)
     expect(response).to match_response_schema('preneeds/cemeteries')
   end
+
+  it 'responds to GET #index when camel-inflected' do
+    VCR.use_cassette('preneeds/cemeteries/gets_a_list_of_cemeteries') do
+      get '/v0/preneeds/cemeteries/', headers: { 'X-Key-Inflection' => 'camel' }
+    end
+
+    expect(response).to be_successful
+    expect(response.body).to be_a(String)
+    expect(response).to match_camelized_response_schema('preneeds/cemeteries')
+  end
 end
