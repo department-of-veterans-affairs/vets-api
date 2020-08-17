177fad4f08ca1e8986181888cca1bf06b56a2559
LeakyBucket <DamnBigMan@gmail.com>
2020-08-17 10:57:55 -0600

    Sigh, of course…

--- modules/covid_research/spec/serializers/covid_research/volunteer/name_serializer_spec.rb
+++ modules/covid_research/spec/serializers/covid_research/volunteer/name_serializer_spec.rb
@@ -15,15 +15,15 @@ RSpec.describe CovidResearch::Volunteer::NameSerializer do
 
   describe '#serialize' do
     it 'modifies the first attribute' do
-      expect(subject.serialize(payload)).to include({ QuestionName: 'firstName', QuestionValue: 'Joe'})
+      expect(subject.serialize(payload)).to include({ QuestionName: 'firstName', QuestionValue: 'Joe' })
     end
 
     it 'modifies the last attribute' do
-      expect(subject.serialize(payload)).to include({ QuestionName: 'lastName', QuestionValue: 'Schmoe'})
+      expect(subject.serialize(payload)).to include({ QuestionName: 'lastName', QuestionValue: 'Schmoe' })
     end
 
     it 'preserves other attributes' do
-      expect(subject.serialize(payload)).to include({ QuestionName: 'suffix', QuestionValue: 'II'})
+      expect(subject.serialize(payload)).to include({ QuestionName: 'suffix', QuestionValue: 'II' })
     end
   end
 end
