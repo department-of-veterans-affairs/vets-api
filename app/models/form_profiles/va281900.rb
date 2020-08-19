bb0d715719170470caa76083e41fbff2a62de9bd
Micah Chiang <micahkchiang@gmail.com>
2020-08-19 15:17:14 -0400

    lint file

--- app/models/form_profiles/va281900.rb
+++ app/models/form_profiles/va281900.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class FormProfiles::VA281900 < FormProfile
-    attribute :is_logged_in, Boolean
+  attribute :is_logged_in, Boolean
 
-    def prefill(user)
-        @is_logged_in = true
-        super(user)
-    end
+  def prefill(user)
+    @is_logged_in = true
+    super(user)
+  end
 
-    def metadata
-        {
-            version: 0,
-            prefill: true,
-            returnUrl: '/veteran-information-review'
-        }
-    end
+  def metadata
+    {
+      version: 0,
+      prefill: true,
+      returnUrl: '/veteran-information-review'
+    }
+  end
 end
