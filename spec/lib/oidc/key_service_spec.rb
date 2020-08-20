eb7db0cf4fae13a1d2cb50d2fa651f449b4edcff
dbrown-va <derek.brown5@va.gov>
2020-08-20 11:57:12 -0400

    Corrected spec tests for isolated auth server changes.

--- spec/lib/oidc/key_service_spec.rb
+++ spec/lib/oidc/key_service_spec.rb
@@ -10,12 +10,15 @@ RSpec.describe OIDC::KeyService do
         Settings.oidc,
         auth_server_metadata_url: 'https://example.com/oauth2/default/.well-known/oauth-authorization-server',
         issuer: 'https://example.com/oauth2/default',
+        issuer_prefix: 'https://example.com/oauth2',
         base_api_url: 'https://example.com/api/v1/',
         base_api_token: 'token'
       ) do
-        VCR.use_cassette('okta/keys') do
-          out = described_class.fetch_keys
-          expect(out['keys'].count).to eq(1)
+        with_settings(Settings.oidc.isolated_audience, default: 'api://default') do
+          VCR.use_cassette('okta/keys') do
+            out = described_class.fetch_keys('https://example.com/oauth2/default')
+            expect(out['keys'].count).to eq(1)
+          end
         end
       end
     end
@@ -33,15 +36,15 @@ RSpec.describe OIDC::KeyService do
 
     it 'avoids upstream requests for repeated bad kids' do
       expect(described_class).to receive(:refresh).once
-      described_class.get_key('bad kid')
-      described_class.get_key('bad kid')
+      described_class.get_key('bad kid', anything)
+      described_class.get_key('bad kid', anything)
     end
 
     it 'refreshes a bad kid after one minute' do
       expect(described_class).to receive(:refresh).twice
-      described_class.get_key('bad kid')
+      described_class.get_key('bad kid', anything)
       Timecop.travel(Time.zone.now + 61)
-      described_class.get_key('bad kid')
+      described_class.get_key('bad kid', anything)
       Timecop.return
     end
 
@@ -50,16 +53,16 @@ RSpec.describe OIDC::KeyService do
 
       # Add a kid with a timestamp in the past
       Timecop.travel(Time.zone.now - 30)
-      described_class.get_key('oldest key')
+      described_class.get_key('oldest key', anything)
       Timecop.return
 
       # Fill up the kid cache so the first key we've added is evicted
       (0...1000).each do |x|
-        described_class.get_key(x)
+        described_class.get_key(x, anything)
       end
 
       # Since this key was evicted, this call will trigger the 1002nd refresh
-      described_class.get_key('oldest key')
+      described_class.get_key('oldest key', anything)
     end
 
     context 'with okta api recordings' do
@@ -68,17 +71,20 @@ RSpec.describe OIDC::KeyService do
           Settings.oidc,
           auth_server_metadata_url: 'https://example.com/oauth2/default/.well-known/oauth-authorization-server',
           issuer: 'https://example.com/oauth2/default',
+          issuer_prefix: 'https://example.com/oauth2',
           base_api_url: 'https://example.com/',
           base_api_token: 'token'
         ) do
-          VCR.use_cassette('okta/keys') do
-            example.run
+          with_settings(Settings.oidc.isolated_audience, default: 'api://default') do
+            VCR.use_cassette('okta/keys') do
+              example.run
+            end
           end
         end
       end
 
       it 'downloads new keys if it does not exist' do
-        key = described_class.get_key('1Z0tNc4Hxs_n7ySgwb6YT8JgWpq0wezqupEg136FZHU')
+        key = described_class.get_key('1Z0tNc4Hxs_n7ySgwb6YT8JgWpq0wezqupEg136FZHU', 'https://example.com/oauth2/default')
         expect(key).to be_a(OpenSSL::PKey::RSA)
       end
     end
