a01d968bd76516dc7385fe98de1a4e7a6b1b2c34
Devin McCurdy <mccurdy_devin@bah.com>
2020-08-20 11:39:19 -0400

    10203 - Implement EVSS feature flag (#4744)

--- app/models/saved_claim/education_benefits/va10203.rb
+++ app/models/saved_claim/education_benefits/va10203.rb
@@ -9,7 +9,7 @@ class SavedClaim::EducationBenefits::VA10203 < SavedClaim::EducationBenefits
 
     StemApplicantConfirmationMailer.build(self, nil).deliver_now
 
-    if user.present?
+    if Flipper.enabled?(:stem_sco_email) && user.present?
       authorized = user.authorize(:evss, :access?)
 
       EducationForm::SendSCOEmail.perform_async(user.uuid, id) if authorized
