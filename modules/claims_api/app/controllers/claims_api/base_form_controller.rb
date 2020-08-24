345aa255b2326f7e4243194ef07a6bfea3686efb
Michael Bastos <bastosmichael@gmail.com>
2020-08-24 15:55:40 -0500

    trying to fix code coverage

--- modules/claims_api/app/controllers/claims_api/base_form_controller.rb
+++ modules/claims_api/app/controllers/claims_api/base_form_controller.rb
@@ -72,20 +72,18 @@ module ClaimsApi
     end
 
     def intent_to_file_options
-      date = if v0? && form_attributes['received_date']
-               form_attributes['received_date']
-             else
-               Time.zone.now.strftime('%Y-%m-%dT%H:%M:%S%:z')
-             end
-
       {
         intent_to_file_type_code: ClaimsApi::IntentToFile::ITF_TYPES[form_type],
         participant_claimant_id: form_attributes['participant_claimant_id'] || target_veteran.participant_id,
         participant_vet_id: form_attributes['participant_vet_id'] || target_veteran.participant_id,
-        received_date: date,
+        received_date: v0? && received_date ? received_date : Time.zone.now.strftime('%Y-%m-%dT%H:%M:%S%:z'),
         submitter_application_icn_type_code: ClaimsApi::IntentToFile::SUBMITTER_CODE,
         ssn: target_veteran.ssn
       }
+    end
+
+    def received_date
+      form_attributes['received_date']
     end
 
     def target_veteran_name
