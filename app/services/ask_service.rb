61c4f15e0b8bb7b2fd7af62e65b0efee30d31523
Anita DeWitt <adewitt@thoughtworks.com>
2020-08-25 13:40:42 -0500

    [department-of-veterans-affairs/orchid#21] change project structure to run service test

---
+++ app/services/ask_service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# This service manages the interactions between Ask and Dynamics 365.
+class AskService
+  # def initialize(claim)
+  #   @claim = claim
+  # end
+
+  def self.post_to_xrm
+    # get client url or define client url (something fake)
+    # parse/translate claim
+    # create to entity to add to data entry in Dynamics
+    return 0
+  end
+end
