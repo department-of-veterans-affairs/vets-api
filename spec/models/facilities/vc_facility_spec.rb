2618ffefdc2b51b31215a9aa4a5929c2b4f63b2d
Philip Becker <boofungal316@yahoo.com>
2020-08-25 13:24:30 -0400

    don't use a module to namespace the facility specs

--- spec/models/facilities/vc_facility_spec.rb
+++ spec/models/facilities/vc_facility_spec.rb
@@ -1,30 +1,29 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-module Facilities
-  RSpec.describe VCFacility do
-    before { BaseFacility.validate_on_load = false }
 
-    after { BaseFacility.validate_on_load = true }
+RSpec.describe Facilities::VCFacility do
+  before { BaseFacility.validate_on_load = false }
 
-    it 'is a VCFacility object' do
-      expect(described_class.new).to be_a(VCFacility)
+  after { BaseFacility.validate_on_load = true }
+
+  it 'is a Facilities::VCFacility object' do
+    expect(described_class.new).to be_a(Facilities::VCFacility)
+  end
+
+  describe 'pull_source_data' do
+    it 'pulls data from ArcGIS endpoint' do
+      VCR.use_cassette('facilities/va/vc_facilities') do
+        list = Facilities::VCFacility.pull_source_data
+        expect(list.size).to eq(10)
+      end
     end
 
-    describe 'pull_source_data' do
-      it 'pulls data from ArcGIS endpoint' do
-        VCR.use_cassette('facilities/va/vc_facilities') do
-          list = VCFacility.pull_source_data
-          expect(list.size).to eq(10)
-        end
-      end
-
-      it 'returns an array of VCFacility objects' do
-        VCR.use_cassette('facilities/va/vc_facilities') do
-          list = VCFacility.pull_source_data
-          expect(list).to be_an(Array)
-          expect(list.all? { |item| item.is_a?(VCFacility) }).to be true
-        end
+    it 'returns an array of Facilities::VCFacility objects' do
+      VCR.use_cassette('facilities/va/vc_facilities') do
+        list = Facilities::VCFacility.pull_source_data
+        expect(list).to be_an(Array)
+        expect(list.all? { |item| item.is_a?(Facilities::VCFacility) }).to be true
       end
     end
   end
