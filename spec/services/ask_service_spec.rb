7f57bfbe2eb90d4d76d0fbafeee6d8af93c6fe84
Anita DeWitt <adewitt@thoughtworks.com>
2020-08-25 13:40:42 -0500

    [department-of-veterans-affairs/orchid#21] change project structure to run service test

--- spec/services/ask_service_spec.rb
+++ spec/services/ask_service_spec.rb
@@ -3,8 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe AskService do
-
-  describe "post to xrm" do
+  describe "#post_to_xrm" do
     context "given a call" do
       it "returns zero" do
         expect(AskService.post_to_xrm).to eq(0)
