4921352459a4a6f808635742ff3e7b99d387aced
Derek Dyer <derek@digitalderek.com>
2020-08-27 19:28:52 -0400

    Swag swag

--- app/swagger/schemas/payment_history.rb
+++ app/swagger/schemas/payment_history.rb
@@ -2,16 +2,36 @@
 
 module Swagger
   module Schemas
-    class ValidVaFileNumber
+    class PaymentHistory
       include Swagger::Blocks
 
-      swagger_schema :ValidVaFileNumber do
+      swagger_schema :PaymentHistory do
         key :required, [:data]
 
         property :data, type: :object do
           key :required, [:attributes]
           property :attributes, type: :object do
-            property :valid_va_file_number, type: :boolean, example: true
+            property :payments, type: :array, example: [
+              {
+                "pay_check_amount": '$3,261.10',
+                "pay_check_dt": '04/01/2019',
+                "pay_check_id": '001',
+                "pay_check_return_fiche": 'C',
+                "pay_check_type": 'Compensation & Pension - Recurring'
+              }
+            ]
+            property :return_payments, type: :array, example: [
+              {
+                "returned_check_amount": '$50.00',
+                "returned_check_cancel_dt": '12/01/2012',
+                "returned_check_issue_dt": '11/25/2012',
+                "returned_check_num": '12345678',
+                "returned_check_ro": '17',
+                "returned_check_reason": '6',
+                "returned_check_return_fiche": 'B',
+                "returned_check_type": 'Other'
+              }
+            ]
           end
         end
       end
