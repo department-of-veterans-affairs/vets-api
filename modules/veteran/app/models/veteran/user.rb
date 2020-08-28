484fa3fcd25867a4c00534b6a996acad6b74c4c2
Michael Bastos <bastosmichael@gmail.com>
2020-08-28 09:02:16 -0500

    fixing usermodel to bgs

--- modules/veteran/app/models/veteran/user.rb
+++ modules/veteran/app/models/veteran/user.rb
@@ -4,14 +4,7 @@
 module Veteran
   class User < Base
     attribute :veteran_name
-    attribute :user_poa_info_available
-    attribute :can_be_validated_by_group_one
-    attribute :award_suspendbadaddress_indicator
-    attribute :award_suspend_indicator
     attribute :social_security_number
-    attribute :fiduciary_ind
-    attribute :has_payment
-    attribute :incompetent_ind
     attribute :indentity_ind
     attribute :index_ind
 
@@ -25,7 +18,9 @@ module Veteran
     def build_from_json(json_data)
       json_data = json_data.deep_transform_keys { |key| key.to_s.underscore }
       set_attributes(json_data['info'])
-      self.power_of_attorney = PowerOfAttorney.new(json_data['current_poa']) if json_data['current_poa'].present?
+      if json_data['person_organization_code'].present?
+        self.power_of_attorney = PowerOfAttorney.new(json_data['person_organization_code'])
+      end
     end
 
     private
