0eebabed5ed49a6137f3794dc62de98336a900ae
camerontesterman <cameronbtesterman@gmail.com>
2020-08-28 16:58:52 -0500

    fix payment_history

--- app/services/bgs/payment_service.rb
+++ app/services/bgs/payment_service.rb
@@ -4,7 +4,7 @@ module BGS
   class PaymentService < BaseService
     def payment_history
       # rubocop:disable Rails/DynamicFindBy
-      response = service.payment_history.find_by_ssn(@user.ssn)
+      response = @service.payment_history.find_by_ssn(@user.ssn)
       # rubocop:enable Rails/DynamicFindBy
 
       response[:payment_record]
