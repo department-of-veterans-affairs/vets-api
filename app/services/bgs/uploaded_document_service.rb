c5a75ed6fb219fd6a56b874234d47526f4e01f02
camerontesterman <cameronbtesterman@gmail.com>
2020-08-28 16:41:54 -0500

    disable rubocop dynamic find by

--- app/services/bgs/uploaded_document_service.rb
+++ app/services/bgs/uploaded_document_service.rb
@@ -3,7 +3,9 @@
 module BGS
   class UploadedDocumentService < BaseService
     def get_documents
+      # rubocop:disable Rails/DynamicFindBy
       @service.uploaded_document.find_by_participant_id(@user.participant_id)
+      # rubocop:enable Rails/DynamicFindBy
     rescue => e
       report_error(e)
     end
