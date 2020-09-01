32015086b33cac949da50c6f92790be52ca08d1c
Philip Becker <boofungal316@yahoo.com>
2020-09-01 11:42:54 -0400

    move requires out of test to submit_form526_cleanup

--- spec/jobs/evss/disability_compensation_form/submit_form526_cleanup_spec.rb
+++ spec/jobs/evss/disability_compensation_form/submit_form526_cleanup_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'evss/intent_to_file/response_strategy'
 
 RSpec.describe EVSS::DisabilityCompensationForm::SubmitForm526Cleanup, type: :job do
   subject { described_class }
