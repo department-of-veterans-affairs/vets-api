bb9a54929eded71c34848f3844994b92fcc9b7b3
Eric Buckley <eric.buckley@gmail.com>
2020-09-01 11:15:53 -0700

    Update config/routes.rb
    Co-authored-by: Johnny Holton <johnny@oddball.io>

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
       to: 'v1/sessions#new',
       constraints: ->(request) { V1::SessionsController::REDIRECT_URLS.include?(request.path_parameters[:type]) }
   get '/v1/sessions/ssoe_logout', to: 'v1/sessions#ssoe_slo_callback'
-  get 'v1/sessions/tracker', to: 'v1/sessions#tracker'
+  get '/v1/sessions/tracker', to: 'v1/sessions#tracker'
 
   namespace :v0, defaults: { format: 'json' } do
     resources :appointments, only: :index
