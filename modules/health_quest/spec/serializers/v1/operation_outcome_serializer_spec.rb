26ec7901ed2b149b5a27a5b99e11312073324f59
Derek Dyer <derek@digitalderek.com>
2020-09-01 16:44:28 -0400

    Add health_quest

---
+++ modules/health_quest/spec/serializers/v1/operation_outcome_serializer_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../../support/health_fixture_helper'
+
+describe HealthQuest::V1::OperationOutcomeSerializer do
+  describe 'json serialization' do
+    let(:resource_type) { :Organization }
+    let(:id) { '987654' }
+
+    context 'with a resource of Organization and a backend service exception' do
+      let(:issue) { Common::Exceptions::BackendServiceException.new('HEALTH_QUEST_502', source: 'Klass') }
+      # rubocop:disable Layout/LineLength
+      let(:operation_outcome) { HealthQuest::V1::OperationOutcome.new(resource_type: resource_type, id: id, issue: issue) }
+      # rubocop:enable Layout/LineLength
+
+      it 'serializes the error in FHIR DSTU 2 format' do
+        expect(HealthQuest::V1::OperationOutcomeSerializer.new(operation_outcome).serialized_json).to eq(
+          HealthFixtureHelper.read_fixture_file('operation_outcome_service_exception.json')
+        )
+      end
+    end
+
+    context 'with a resource of Organization and an unexpected error (vets-api 500)' do
+      let(:issue) { NoMethodError.new("undefined method 'to_ary' for \"hello\":String") }
+      # rubocop:disable Layout/LineLength
+      let(:operation_outcome) { HealthQuest::V1::OperationOutcome.new(resource_type: resource_type, id: id, issue: issue) }
+      # rubocop:enable Layout/LineLength
+
+      it 'serializes the error in FHIR DSTU 2 format' do
+        expect(HealthQuest::V1::OperationOutcomeSerializer.new(operation_outcome).serialized_json).to eq(
+          HealthFixtureHelper.read_fixture_file('operation_outcome_system_exception.json')
+        )
+      end
+    end
+
+    context 'with an informational message' do
+      let(:issue) { { detail: 'Additional information may be found in the conformance doc' } }
+      let(:operation_outcome) { HealthQuest::V1::OperationOutcome.new(resource_type: resource_type, issue: issue) }
+
+      it 'serializes the error in FHIR DSTU 2 format' do
+        expect(HealthQuest::V1::OperationOutcomeSerializer.new(operation_outcome).serialized_json).to eq(
+          HealthFixtureHelper.read_fixture_file('operation_outcome_information.json')
+        )
+      end
+    end
+  end
+end
