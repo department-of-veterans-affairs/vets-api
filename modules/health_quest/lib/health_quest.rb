a9df7a582cbf42850e52824f078dc0da318e4879
Laurence Guild <lguild@governmentcio.com>
2020-09-04 14:50:54 -0400

    filter unneccesary files for SimpleCov that don't get covered in limited PR but do nothing specific for health quest service

--- modules/health_quest/lib/health_quest.rb
+++ modules/health_quest/lib/health_quest.rb
@@ -3,8 +3,4 @@
 require 'health_quest/engine'
 
 module HealthQuest
-	# this is just here for RSpec test purposes
-	def self.team_name
-		'Health Care Experience Team'
-	end	
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,8 @@ unless ENV['NOCOVERAGE']
     add_filter 'modules/claims_api/app/controllers/claims_api/v1/forms/disability_compensation_controller.rb'
     add_filter 'modules/claims_api/app/swagger/*'
     add_filter 'modules/claims_api/lib/claims_api/health_checker.rb'
+    add_filter 'modules/health_quest/lib/health_quest.rb'
+    add_filter 'modules/health_quest/lib/health_quest/engine.rb'
     add_filter 'lib/bip_claims/configuration.rb'
     add_filter 'version.rb'
 
