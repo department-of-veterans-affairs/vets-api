dce71acf8a44c694109e5f746a7ca730dcf259c7
Ed Mangimelli <38672641+edmangimelli@users.noreply.github.com>
2020-09-04 12:40:16 -0400

    Require Common Lib in Modules (#4851)
    * require common lib in modules
    
    * typo

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/higher_level_reviews/contestable_issues_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/higher_level_reviews/contestable_issues_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'caseflow/service'
+require 'common/exceptions'
 
 class AppealsApi::V1::DecisionReviews::HigherLevelReviews::ContestableIssuesController < AppealsApi::ApplicationController # rubocop:disable Layout/LineLength
   skip_before_action(:authenticate)
