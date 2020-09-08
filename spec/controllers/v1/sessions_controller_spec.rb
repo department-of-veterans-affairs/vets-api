976b215c3755f0af7f309d76cff44358e1128c54
Eric Buckley <eric.buckley@gmail.com>
2020-09-08 15:23:35 -0700

    allow unauthenticated users to access /v1/sessions/slo/new (#4817)
    * allow unauthenticated users to access /v1/sessions/slo/new
    
    * adding a test case for unauthenticated users attemping to logout

--- spec/controllers/v1/sessions_controller_spec.rb
+++ spec/controllers/v1/sessions_controller_spec.rb
@@ -262,10 +262,17 @@ RSpec.describe V1::SessionsController, type: :controller do
                                   'originating_request_id' => nil, 'type' => 'signup')
           end
         end
+
+        context 'routes /v1/sessions/slo/new to SessionController#new' do
+          it 'redirects' do
+            expect(get(:new, params: { type: :slo }))
+              .to redirect_to('https://int.eauth.va.gov/pkmslogout?filename=vagov-logout.html')
+          end
+        end
       end
 
       context 'routes requiring auth' do
-        %w[mfa verify slo].each do |type|
+        %w[mfa verify].each do |type|
           it "routes /sessions/#{type}/new to SessionsController#new with type: #{type}" do
             get(:new, params: { type: type })
             expect(response).to have_http_status(:unauthorized)
