4db658f9144b86dd2da162daf6f3467d198e486e
camerontesterman <cameronbtesterman@gmail.com>
2020-09-09 10:42:27 -0500

    move controller spec to request spec

--- spec/request/efolder_spec.rb
+++ spec/request/efolder_spec.rb
@@ -3,27 +3,27 @@
 require 'rails_helper'
 require 'support/stub_efolder_documents'
 
-RSpec.describe V0::EfolderController, type: :controller do
+RSpec.describe 'eFolder API Endpoint', type: :request do
   let(:user) { build(:user, :loa3) }
 
   before do
     sign_in_as(user)
   end
 
-  describe '#index' do
+  describe 'GET /v0/efolder' do
     stub_efolder_documents(:index)
 
     it 'shows all documents available to the veteran' do
-      get(:index)
+      get '/v0/efolder'
       expect(JSON.parse(response.body)).to eq(list_documents_res)
     end
   end
 
-  describe '#show' do
+  describe 'GET /v0/efolder/{id}' do
     stub_efolder_documents(:show)
 
     it 'sends the doc pdf' do
-      get(:show, params: { id: document_id, filename: 'test.pdf' })
+      get "/v0/efolder/#{document_id}", params: { filename: 'test.pdf' }
       expect(response.body).to eq(content)
     end
   end
