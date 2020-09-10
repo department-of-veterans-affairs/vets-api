dfc7c9412ea28f6cdb2d30b674d21c7f3e0cdb78
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-09-10 10:00:59 -0700

    add mobile application controller

--- modules/mobile/app/controllers/mobile/application_controller.rb
+++ modules/mobile/app/controllers/mobile/application_controller.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 module Mobile
-  class ApplicationController < ActionController::API
-    before_action :check_feature_flag
+  class ApplicationController < BaseApplicationController
+    before_action :check_feature_flag, :authenticate
 
+    TOKEN_REGEX = /Bearer /.freeze
+
+    private
+    
     def check_feature_flag
       return nil if Flipper.enabled?(:mobile_api)
 
@@ -19,6 +23,17 @@ module Mobile
       }
 
       render json: message, status: :not_found
+    end
+    
+    def authenticate
+      raise Common::Exceptions::Forbidden.new(detail: 'Missing bearer auth token') if auth_token.nil?
+      
+      
+    end
+    
+    def auth_token
+      auth_header = request.authorization.to_s
+      auth_header[TOKEN_REGEX]
     end
   end
 end
