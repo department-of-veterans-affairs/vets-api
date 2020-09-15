77959f13ca8fa71ea503b7a9b1cdfffef875e9ae
camerontesterman <cameronbtesterman@gmail.com>
2020-09-15 10:03:49 -0500

    vbms_documents method added

--- lib/debts/letter_downloader.rb
+++ lib/debts/letter_downloader.rb
@@ -30,18 +30,15 @@ module Debts
     end
 
     def file_name(document_id)
-      document = @client.send_request(
-        VBMS::Requests::FindDocumentVersionReference.new(@service.file_number)
-      ).detect do |doc|
+      document = vbms_documents.detect do |doc|
         doc.document_id == document_id
       end
+
       "#{document.type_description} #{document.upload_date.to_formatted_s(:long)}"
     end
 
     def list_letters
-      debts_records = @client.send_request(
-        VBMS::Requests::FindDocumentVersionReference.new(@service.file_number)
-      ).find_all do |record|
+      debts_records = vbms_documents.find_all do |record|
         DEBTS_DOCUMENT_TYPES.include?(record.doc_type)
       end
 
@@ -53,6 +50,12 @@ module Debts
     end
 
     private
+
+    def vbms_documents
+      @client.send_request(
+        VBMS::Requests::FindDocumentVersionReference.new(@service.file_number)
+      )
+    end
 
     def debts_service
       Debts::Service.new(@user)
