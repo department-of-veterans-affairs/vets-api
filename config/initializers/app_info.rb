2aeb450b189f1880de39d6b53437964037c96078
Keifer Furzland <furzland@gmail.com>
2020-09-15 19:42:58 -0500

    Remove initializer setting

--- config/initializers/app_info.rb
+++ config/initializers/app_info.rb
@@ -3,5 +3,4 @@
 module AppInfo
   GIT_REVISION = ENV.fetch('GIT_REVISION', 'MISSING_GIT_REVISION')
   GITHUB_URL   = 'https://github.com/department-of-veterans-affairs/vets-api'
-  VAGOV_ENV    = Settings.vsp_environment
 end
