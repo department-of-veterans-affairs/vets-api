a51844e3bf9e7443d100a3e93748010711f2cfc8
Ed Mangimelli <edmangimelli@gmail.com>
2020-09-18 10:04:26 -0400

    remove requires (Philip suggestion)

--- spec/lib/decision_review/service_spec.rb
+++ spec/lib/decision_review/service_spec.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'common/exceptions/forbidden'
 require 'decision_review/service'
-require 'decision_review/service_exception'
 
 describe DecisionReview::Service do
   subject { described_class.new }
