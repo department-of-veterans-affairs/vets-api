0bab227008e4d0a43f9714e941170cfbc1146263
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-09-21 14:45:09 -0400

    add pact provider states generator

---
+++ lib/commands/pact/pact_command.rb
@@ -0,0 +1,20 @@
+require 'generators/provider_state/provider_state_generator'
+
+module Pact
+  class Command
+    class PactCommand < Rails::Command::Base
+      namespace 'pact'
+
+      no_commands do
+        def help
+          Rails::Command.invoke :application, ['--help']
+        end
+      end
+
+      def perform(type = nil, *args)
+        Rails::Command.invoke :generate, ['provider_state', '--help'] unless type == 'new'
+        ProviderStateGenerator.start args
+      end
+    end
+  end
+end
---
+++ lib/generators/provider_state/provider_state_generator.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails/generators'
+
+class ProviderStateGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  def create_provider_state_file
+    template 'provider_states.rb.erb', "spec/service_consumers/provider_states_for/#{file_name}.rb"
+    puts "\n"
+    puts "\u{1F64C} new pact provider state generated at spec/service_consumers/provider_states_for/#{file_name}.rb \n\n"
+    puts "\n"
+  end
+
+end
