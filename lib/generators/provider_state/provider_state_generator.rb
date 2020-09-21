a7d10290a717b797cf0379c289cb50370dbb63f8
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-09-21 15:27:40 -0400

    linting

--- lib/generators/provider_state/provider_state_generator.rb
+++ lib/generators/provider_state/provider_state_generator.rb
@@ -7,15 +7,16 @@ class ProviderStateGenerator < Rails::Generators::NamedBase
 
   def create_provider_state_file
     template 'provider_states.rb.erb', "spec/service_consumers/provider_states_for/#{file_name}.rb"
+  end
 
-    # rubocop:disable Rails/Output
-    # :nocov:
-
+  # rubocop:disable Rails/Output
+  # :nocov:
+  def generator_output
     puts "\n"
     puts "\u{1F64C} new pact provider state generated at spec/service_consumers/provider_states_for/#{file_name}.rb \n\n"
     puts "\n"
-
-    # :nocov:
-    # rubocop:enable Rails/Output
   end
+  # :nocov:
+  # rubocop:enable Rails/Output
+
 end
