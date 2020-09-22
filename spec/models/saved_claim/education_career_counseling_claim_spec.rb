77d7c21781da5821cd9b78f83ee93597cfe8d424
Derek Dyer <derek@digitalderek.com>
2020-09-22 10:27:02 -0400

    Add spec template

---
+++ spec/models/saved_claim/education_career_counseling_claim_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SavedClaim::EducationCareerCounselingClaim do
+  let(:education_career_counseling_claim) { create(:education_career_counseling_claim) }
+  describe '#add_veteran_info' do
+    it 'adds veteran information' do
+      
+    end
+  end
+end
