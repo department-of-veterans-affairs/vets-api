3169c51d3b70ff9f560727c8aa8f7771f62cf5c6
Philip Becker <philip.becker@oddball.io>
2020-09-22 12:15:09 -0400

    13281 clean up requires in specs (#4943)
    * mvi_stub is part of rails helper, not needed elsewhere
    
    * remove unnecessary requires in specs
    
    * move okta::userprofile requires to openid_user_identity
    
    * have lib specs require their class
    
    * move requires of rx/client, sm/client, and bb/client to the respective support modules
    
    * add comments for requires in specs that need to stay around

--- spec/models/mvi_spec.rb
+++ spec/models/mvi_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'common/exceptions'
-require 'support/mvi/stub_mvi'
 
 describe Mvi, skip_mvi: true do
   let(:user) { build(:user, :loa3) }
