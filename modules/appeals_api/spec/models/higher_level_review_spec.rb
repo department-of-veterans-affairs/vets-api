16a3dd23a36aa99331d27ec4db51b697b0735dad
Philip Becker <boofungal316@yahoo.com>
2020-09-22 14:38:46 -0400

    do not require central_mail/service in spec

--- modules/appeals_api/spec/models/higher_level_review_spec.rb
+++ modules/appeals_api/spec/models/higher_level_review_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'central_mail/service'
 require AppealsApi::Engine.root.join('spec', 'spec_helper.rb')
 
 describe AppealsApi::HigherLevelReview, type: :model do
