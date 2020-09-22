3c277a90bb419c1888ca60219c04a28dd595b69c
Ed Mangimelli <edmangimelli@gmail.com>
2020-09-22 15:34:28 -0400

    re: HLR controller #create / render an error when the request body isn't a hash

--- app/controllers/appeals_base_controller.rb
+++ app/controllers/appeals_base_controller.rb
@@ -16,4 +16,31 @@ class AppealsBaseController < ApplicationController
   def decision_review_service
     DecisionReview::Service.new
   end
+
+  def render_error_unless_request_body_is_a_hash
+    render_request_body_is_not_a_hash_error unless request_body_hash
+  end
+
+  def request_body_hash
+    @request_body_hash ||= get_hash_from_request_body
+  end
+
+  def get_hash_from_request_body
+    return nil if request.body.class.name == 'Puma::NullIO' # testing string b/c NullIO class doesn't always exist
+
+    body = JSON.parse request.body.string
+    body.is_a?(Hash) ? body : nil
+  rescue JSON::ParserError
+    nil
+  end
+
+  def render_request_body_is_not_a_hash_error
+    status = 422
+    error = {
+      status: status,
+      detail: "The request body isn't a JSON object",
+      source: false
+    }
+    render status: status, json: { errors: [error] }
+  end
 end
--- app/controllers/v0/higher_level_reviews_controller.rb
+++ app/controllers/v0/higher_level_reviews_controller.rb
@@ -2,13 +2,15 @@
 
 module V0
   class HigherLevelReviewsController < AppealsBaseController
+    before_action :render_error_unless_request_body_is_a_hash, only: [:create]
+
     def show
       render json: decision_review_service.get_higher_level_review(params[:id]).body
     end
 
     def create
       render json: decision_review_service
-        .create_higher_level_review(request_body: JSON.parse(request.body.string.presence || '{}'), user: current_user)
+        .create_higher_level_review(request_body: request_body_hash, user: current_user)
         .body
     end
   end
