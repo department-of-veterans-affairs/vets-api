f1a8a636e1091a88f81f5bb585a77707feb1f81e
Lihan Li <frankieteardrop@gmail.com>
2020-09-23 14:55:28 -0700

    lint

--- config/initializers/staccato.rb
+++ config/initializers/staccato.rb
@@ -1,3 +1,5 @@
-def Staccato.ga_collection_uri(*args)
+# frozen_string_literal: true
+
+def Staccato.ga_collection_uri(*_args)
   URI("#{Settings.google_analytics.url}/collect")
 end
--- spec/config/initializers/staccato_spec.rb
+++ spec/config/initializers/staccato_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe Staccato do
-  it 'should use the google analytics url in settings' do
+  it 'uses the google analytics url in settings' do
     VCR.use_cassette('staccato/event', match_requests_on: %i[method uri]) do
       Staccato.tracker('foo').event(
         category: 'foo'
