bd57ea07a18173e12f0cc5bf3dfcf6e84d2bc91b
Lihan Li <frankieteardrop@gmail.com>
2020-09-23 14:46:42 -0700

    use vcr cassette in staccato spec

--- spec/config/initializers/staccato_spec.rb
+++ spec/config/initializers/staccato_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Staccato do
   it 'should use the google analytics url in settings' do
-    VCR.use_cassette('staccato/event', record: :once) do
+    VCR.use_cassette('staccato/event', match_requests_on: %i[method uri]) do
       Staccato.tracker('foo').event(
         category: 'foo'
       )
