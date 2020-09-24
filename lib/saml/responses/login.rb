8e9e6e5dbbd82ef5fff5445f78497b44d691d684
omgitsbillryan <bill.ryan@adhocteam.us>
2020-09-24 14:58:16 -0400

    ignore temp code for coverage

--- lib/saml/responses/login.rb
+++ lib/saml/responses/login.rb
@@ -10,11 +10,13 @@ module SAML
       def initialize(saml_response, options = {})
         super(saml_response, options)
       rescue OpenSSL::PKey::RSAError => e # "padding check failed." when decrypt fails
+        # :nocov: Temporary code only required during key rollovers using a new private key
         raise e unless Settings.saml.key_new
 
         duped_options = options.dup
         duped_options[:settings].private_key = Settings.saml.key_new
         super(saml_response, duped_options)
+        # :nocov:
       end
 
       def errors_message
