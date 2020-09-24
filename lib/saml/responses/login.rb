08d71ab428afe3ae357300a90e3852e4c61355e8
omgitsbillryan <bill.ryan@adhocteam.us>
2020-09-23 23:59:44 -0400

    multi-key saml support when decrypting authn responses

--- lib/saml/responses/login.rb
+++ lib/saml/responses/login.rb
@@ -7,6 +7,18 @@ module SAML
     class Login < OneLogin::RubySaml::Response
       include SAML::Responses::Base
 
+      def initialize(saml_response, options={})
+        begin
+          super(saml_response, options)
+        rescue OpenSSL::PKey::RSAError => e
+          raise e unless Settings.saml.key_new
+
+          duped_options = options.dup
+          duped_options[:settings].private_key = Settings.saml.key_new
+          super(saml_response, duped_options)
+        end
+      end
+
       def errors_message
         @errors_message ||= if errors.any?
                               message = 'Login Failed! '
