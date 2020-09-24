40f7cf0f62d6f1b276bbc1e574c77c116dac56f4
Eric Buckley <eric.buckley@gmail.com>
2020-09-24 13:43:01 -0700

    saml settings should change between v0 and v1

--- lib/saml/url_service.rb
+++ lib/saml/url_service.rb
@@ -197,9 +197,8 @@ module SAML
     end
 
     def previous_saml_uuid(params)
-      if params[:action] == 'saml_callback'
-        settings = SAML::SSOeSettingsService.saml_settings
-        resp = SAML::Responses::Login.new(params[:SAMLResponse] || '', settings: settings)
+      if params[:action] == 'saml_callback' && params[:SAMLResponse]
+        resp = SAML::Responses::Login.new(params[:SAMLResponse], settings: @saml_settings)
         resp&.in_response_to
       end
     end
