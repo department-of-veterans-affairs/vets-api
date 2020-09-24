7135ff627448e9dcc67b8d753398f90257039c57
omgitsbillryan <bill.ryan@adhocteam.us>
2020-09-24 00:11:17 -0400

    lint & comment

--- lib/saml/responses/login.rb
+++ lib/saml/responses/login.rb
@@ -7,16 +7,14 @@ module SAML
     class Login < OneLogin::RubySaml::Response
       include SAML::Responses::Base
 
-      def initialize(saml_response, options={})
-        begin
-          super(saml_response, options)
-        rescue OpenSSL::PKey::RSAError => e
-          raise e unless Settings.saml.key_new
+      def initialize(saml_response, options = {})
+        super(saml_response, options)
+      rescue OpenSSL::PKey::RSAError => e # "padding check failed." when decrypt fails
+        raise e unless Settings.saml.key_new
 
-          duped_options = options.dup
-          duped_options[:settings].private_key = Settings.saml.key_new
-          super(saml_response, duped_options)
-        end
+        duped_options = options.dup
+        duped_options[:settings].private_key = Settings.saml.key_new
+        super(saml_response, duped_options)
       end
 
       def errors_message
