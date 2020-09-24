97da4bf027931d50e376c5c99e97c98c43d6b116
Philip Becker <boofungal316@yahoo.com>
2020-09-24 14:59:39 -0400

    remove unneeded requires from claim uploader worker spec

--- modules/claims_api/spec/workers/claim_uploader_spec.rb
+++ modules/claims_api/spec/workers/claim_uploader_spec.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'evss/disability_compensation_auth_headers'
-require 'evss/auth_headers'
-require 'evss/documents_service'
 
 RSpec.describe ClaimsApi::ClaimUploader, type: :job do
   subject { described_class }
