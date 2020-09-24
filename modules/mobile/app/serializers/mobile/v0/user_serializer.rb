66160cdfe9b31bfb175fc36814209682cf2c264f
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-09-24 14:48:06 -0700

    add issue # to user profile todo

--- modules/mobile/app/serializers/mobile/v0/user_serializer.rb
+++ modules/mobile/app/serializers/mobile/v0/user_serializer.rb
@@ -7,7 +7,8 @@ module Mobile
     class UserSerializer
       include FastJsonapi::ObjectSerializer
 
-      # TODO: return full set of user data, including latest appeals, claims, and appointments
+      # TODO: return full set of user profile data see issue here:
+      # https://github.com/department-of-veterans-affairs/va.gov-team/issues/13458
       attributes :first_name,
                  :middle_name,
                  :last_name,
