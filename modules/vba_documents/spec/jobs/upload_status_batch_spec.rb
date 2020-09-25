b9d7f3eed43f1a0267d59bc9d6712c6f944a965e
Philip Becker <boofungal316@yahoo.com>
2020-09-25 08:51:12 -0400

    remove unneeded require of central mail from vba_documents upload_status_batch job spec

--- modules/vba_documents/spec/jobs/upload_status_batch_spec.rb
+++ modules/vba_documents/spec/jobs/upload_status_batch_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'central_mail/service'
 
 RSpec.describe VBADocuments::UploadStatusBatch, type: :job do
   let(:client_stub) { instance_double('CentralMail::Service') }
