34fbd2a75e499b8babe43ce55201b8a6ad66499b
Bill Ryan <bill.ryan@adhocteam.us>
2020-09-25 12:01:45 -0400

    multi-key saml support when decrypting authn responses (#4965)

--- lib/saml/responses/login.rb
+++ lib/saml/responses/login.rb
@@ -7,6 +7,18 @@ module SAML
     class Login < OneLogin::RubySaml::Response
       include SAML::Responses::Base
 
+      def initialize(saml_response, options = {})
+        super(saml_response, options)
+      rescue OpenSSL::PKey::RSAError => e # "padding check failed." when decrypt fails
+        # :nocov: Temporary code only required during key rollovers using a new private key
+        raise e unless Settings.saml.key_new
+
+        duped_options = options.dup
+        duped_options[:settings].private_key = Settings.saml.key_new
+        super(saml_response, duped_options)
+        # :nocov:
+      end
+
       def errors_message
         @errors_message ||= if errors.any?
                               message = 'Login Failed! '
