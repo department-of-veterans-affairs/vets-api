e9ab7a64ecb67abf9e3e420857547573bd8e258a
Michael Bastos <bastosmichael@gmail.com>
2020-09-25 16:01:32 -0500

    reducing size of Veteran::User

--- modules/veteran/app/models/veteran/user.rb
+++ modules/veteran/app/models/veteran/user.rb
@@ -6,32 +6,17 @@ require 'evss/vso_search/service'
 # Veteran model
 module Veteran
   class User < Base
-    attribute :veteran_name
-    attribute :social_security_number
-    attribute :indentity_ind
-    attribute :index_ind
-
     attr_accessor :power_of_attorney
 
     def initialize(user)
       @user = user
-      build_from_json(bgs_service.claimant.find_poa_by_participant_id(user.participant_id))
-    end
-
-    def build_from_json(json_data)
-      json_data = json_data.deep_transform_keys { |key| key.to_s.underscore }
-      set_attributes(json_data['info'])
+      json_data = bgs_service.claimant.find_poa_by_participant_id(user.participant_id)
       if json_data['person_organization_code'].present?
         self.power_of_attorney = PowerOfAttorney.new(code: json_data['person_organization_code'])
       end
     end
 
     private
-
-    # since assign_attributes is not available, do our own mass assignment
-    def set_attributes(attributes_hash)
-      attributes_hash.each { |key, value| send("#{key}=", value) if respond_to?("#{key}=") }
-    end
 
     def bgs_service
       external_key = @user.common_name || @user.email
