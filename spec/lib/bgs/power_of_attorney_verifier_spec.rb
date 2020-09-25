8329c6334fcd64e44302aa19ae4395829d0ec7c0
Michael Bastos <bastosmichael@gmail.com>
2020-09-25 14:27:06 -0500

    fixing tests

--- spec/lib/bgs/power_of_attorney_verifier_spec.rb
+++ spec/lib/bgs/power_of_attorney_verifier_spec.rb
@@ -8,13 +8,11 @@ describe BGS::PowerOfAttorneyVerifier do
   let(:identity) { FactoryBot.create(:user_identity) }
 
   before do
-    @client_stub = instance_double('BGS::Services')
     external_key = user.common_name || user.email
     allow(BGS::Services).to receive(:new).with({ external_uid: user.icn, external_key: external_key })
-    allow(@client_stub).to receive(:claimant).and_return(nil)
-    allow(@client_stub.claimant).to receive(:find_poa_by_participant_id) { get_fixture('json/bgs_with_poa') }
+    allow(Veteran::User).to receive(:new) { OpenStruct.new(power_of_attorney: PowerOfAttorney.new(code: 'A1Q')) }
     @veteran = Veteran::User.new(user)
-    @veteran.power_of_attorney = PowerOfAttorney.new(ssn: '123456789')
+    @veteran.power_of_attorney = PowerOfAttorney.new(code: 'A1Q')
   end
 
   it 'does not raise an exception if poa matches' do
