8fac4a41be1b277c53f74e0427e119414f590e6a
Michael Bastos <bastosmichael@gmail.com>
2020-09-25 17:02:09 -0500

    Allowing dashes in X-VA-SSN for Claims (#4979)
    * Allowing dashes in X-VA-SSN for Claims
    
    * no dashes
    
    * Update application_controller.rb

--- modules/claims_api/spec/requests/v0/disability_compensation_request_spec.rb
+++ modules/claims_api/spec/requests/v0/disability_compensation_request_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Disability Claims ', type: :request do
   let(:headers) do
-    { 'X-VA-SSN': '796043735',
+    { 'X-VA-SSN': '796-04-3735',
       'X-VA-First-Name': 'WESLEY',
       'X-VA-Last-Name': 'FORD',
       'X-VA-EDIPI': '1007697216',
