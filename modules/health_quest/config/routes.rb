5ca13ddbecf1b81b0cc99761359d3a2c305582ed
Laurence Guild <lguild@governmentcio.com>
2020-09-30 10:48:13 -0400

    address the requested changes

--- modules/health_quest/config/routes.rb
+++ modules/health_quest/config/routes.rb
@@ -2,11 +2,8 @@
 
 HealthQuest::Engine.routes.draw do
   namespace :v0, defaults: { format: :json } do
-    resources :appointments, only: %i[index show] do
-    end
-    resources :pgd_questionnaires, only: %i[show] do
-    end
-    resources :pgd_ques_responses, only: %i[show] do
-    end
+    resources :appointments, only: %i[index show]
+    resources :pgd_questionnaires, only: %i[show]
+    resources :pgd_ques_responses, only: %i[show]
   end
 end
