b4c26155a66a4ed357e5df9717c6a19ad7bb587a
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-09-30 10:53:03 -0700

    update user endpoint to return addresses and authed services

--- modules/mobile/app/controllers/mobile/v0/users_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/users_controller.rb
@@ -5,8 +5,28 @@ require_dependency 'mobile/application_controller'
 module Mobile
   module V0
     class UsersController < ApplicationController
+      AVAILABLE_SERVICES = %w[
+        appeals
+        appointments
+        claims
+        directDepositBenefits
+        letters
+        militaryServiceHistory
+        userProfileUpdate
+      ].freeze
+      
       def show
-        render json: Mobile::V0::UserSerializer.new(@current_user)
+        render json: Mobile::V0::UserSerializer.new(@current_user, options)
+      end
+      
+      private
+      
+      def options
+        {
+          meta: {
+            available_services: AVAILABLE_SERVICES
+          }
+        }
       end
     end
   end
--- modules/mobile/spec/request/user_request_spec.rb
+++ modules/mobile/spec/request/user_request_spec.rb
@@ -8,23 +8,8 @@ RSpec.describe 'user', type: :request do
     context 'with a user who has a cached iam session' do
       before { iam_sign_in }
 
-      let(:expected_body) do
-        {
-          'data' => {
-            'id' => '69ad43ea-6882-5673-8552-377624da64a5',
-            'type' => 'user',
-            'attributes' => {
-              'first_name' => 'GREG',
-              'middle_name' => 'A',
-              'last_name' => 'ANDERSON',
-              'email' => 'va.api.user+idme.008@gmail.com'
-            }
-          }
-        }
-      end
-
       it 'returns returns basic user info without hitting the introspect endpoint' do
-        get '/mobile/v0/user', headers: { 'Authorization' => "Bearer #{access_token}" }
+        get '/mobile/v0/user', headers: iam_headers
 
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)).to eq(expected_body)
