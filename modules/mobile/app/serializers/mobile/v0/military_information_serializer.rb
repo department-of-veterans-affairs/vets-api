e830d80316ba643d4f0c0e323e9cae25ffc26994
Jayson Perkins <jaysonperkins@Jayson-Perkins-Macbook.local>
2020-10-01 11:43:54 -0400

    changed delete to except, renamed object to user

--- modules/mobile/app/serializers/mobile/v0/military_information_serializer.rb
+++ modules/mobile/app/serializers/mobile/v0/military_information_serializer.rb
@@ -7,13 +7,13 @@ module Mobile
     class MilitaryInformationSerializer
       include FastJsonapi::ObjectSerializer
       set_key_transform :camel_lower
-      attribute :service_history do |object|
+      attribute :service_history do |user|
         formatted_result = []
-        object.military_information.service_history.each do |service_period|
-          service_period.delete(:personnel_category_type_code)
+        user.military_information.service_history.each do |service_period|
           service_period[:branch_of_service] = 'United States ' + service_period[:branch_of_service]
           service_period[:formatted_begin_date] = service_period[:begin_date].strftime('%B %d, %Y')
           service_period[:formatted_end_date] = service_period[:end_date].strftime('%B %d, %Y')
+          service_period = service_period.except(:personnel_category_type_code)
           formatted_result.push(service_period.transform_keys{|key| key.to_s.camelize(:lower)})
         end
         formatted_result
