1c463a4970ef7cd26ff312d316a8f88b9dec2d43
Derek Dyer <derek@digitalderek.com>
2020-09-30 23:30:48 -0400

    Add small changes

--- app/models/saved_claim/education_career_counseling_claim.rb
+++ app/models/saved_claim/education_career_counseling_claim.rb
@@ -10,15 +10,13 @@ class SavedClaim::EducationCareerCounselingClaim < CentralMailClaim
   def add_veteran_info(current_user)
     parsed_form.merge!(
       {
-        'claimantInformation' => {
-          'fullName' => {
-            'first' => current_user.first_name,
-            'middle' => current_user.middle_name || '',
-            'last' => current_user.last_name
-          },
-          'ssn' => current_user.ssn,
-          'dateOfBirth' => current_user.birth_date
-        }
+        'fullName' => {
+          'first' => current_user.first_name,
+          'middle' => current_user.middle_name || '',
+          'last' => current_user.last_name
+        },
+        'ssn' => current_user.ssn,
+        'dateOfBirth' => current_user.birth_date
       }
     )
   end
