9254a93af093aba9ce4651a18e43209c36bbccf9
Alastair Dawson <alastair@adhocteam.us>
2020-10-06 08:29:30 -0700

    13458 mobile user endpoint (#5015)
    * to reduce calls remove discovery endpoint and move its data to the meta object of the GET user response.
    
    * update user endpoint to return addresses and authed services
    
    * updated mobile user requests specs for the full user profile

--- modules/mobile/app/serializers/mobile/v0/user_serializer.rb
+++ modules/mobile/app/serializers/mobile/v0/user_serializer.rb
@@ -7,12 +7,49 @@ module Mobile
     class UserSerializer
       include FastJsonapi::ObjectSerializer
 
-      # TODO: return full set of user profile data see issue here:
-      # https://github.com/department-of-veterans-affairs/va.gov-team/issues/13458
-      attributes :first_name,
-                 :middle_name,
-                 :last_name,
-                 :email
+      ADDRESS_KEYS = %i[
+        address_line1
+        address_line2
+        address_line3
+        address_pou
+        address_type
+        city
+        country_code
+        international_postal_code
+        province
+        state_code
+        zip_code
+        zip_code_suffix
+      ].freeze
+
+      SERVICE_DICTIONARY = {
+        appeals: :appeals,
+        appointments: :vaos,
+        claims: :evss,
+        directDepositBenefits: :evss,
+        lettersAndDocuments: :evss,
+        militaryServiceHistory: :emis,
+        userProfileUpdate: :vet360
+      }.freeze
+
+      def self.filter_address(address)
+        address&.to_h&.slice(*ADDRESS_KEYS)
+      end
+
+      attribute :profile do |user|
+        {
+          first_name: user.first_name,
+          middle_name: user.middle_name,
+          last_name: user.last_name,
+          email: user.email,
+          residential_address: filter_address(user.vet360_contact_info&.residential_address),
+          mailing_address: filter_address(user.vet360_contact_info&.mailing_address)
+        }
+      end
+
+      attribute :authorized_services do |user|
+        SERVICE_DICTIONARY.filter { |_k, v| user.authorize v, :access? }.keys
+      end
     end
   end
 end
