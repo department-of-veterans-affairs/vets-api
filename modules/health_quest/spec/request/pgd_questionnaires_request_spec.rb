64e8ee6e3159adea19cb58d01908d6f326dadcdb
Dillo Raju <draju@governmentcio.com>
2020-10-07 11:36:31 -0400

    add questionnaires path

---
+++ modules/health_quest/spec/request/pgd_questionnaires_request_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'health_quest questionnaires', type: :request, skip_mvi: true do
+  include SchemaMatchers
+
+  let(:dummy_headers) { {} }
+  let(:access_denied_message) { 'You do not have access to the health quest service' }
+
+  before do
+    sign_in_as(current_user)
+    allow_any_instance_of(HealthQuest::SessionService).to receive(:headers).and_return(dummy_headers)
+  end
+
+  describe 'GET questionnaires' do
+    context 'loa1 user' do
+      let(:current_user) { build(:user, :loa1) }
+
+      it 'has forbidden status' do
+        get '/health_quest/v0/pgd_questionnaires/32'
+
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it 'has forbidden status' do
+        get '/health_quest/v0/pgd_questionnaires/32'
+
+        expect(JSON.parse(response.body)['errors'].first['detail']).to eq(access_denied_message)
+      end
+    end
+
+    context 'health quest user' do
+      let(:current_user) { build(:user, :health_quest) }
+  
+      it 'has access to questionnaires have access' do
+        get '/health_quest/v0/pgd_questionnaires/32'
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
