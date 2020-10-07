722351a4bfddf53d756b3dd3cee37c1021d1d510
Dillo Raju <draju@governmentcio.com>
2020-10-07 16:53:01 -0400

    add basic questionnaire responses endpoint

--- modules/health_quest/config/routes.rb
+++ modules/health_quest/config/routes.rb
@@ -4,5 +4,6 @@ HealthQuest::Engine.routes.draw do
   namespace :v0, defaults: { format: :json } do
     resources :appointments, only: %i[index show]
     resources :pgd_questionnaires, only: %i[show]
+    resources :pgd_questionnaire_responses, only: %i[show]
   end
 end
