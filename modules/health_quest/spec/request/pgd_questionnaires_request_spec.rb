f914f14b69db671446ab6da5343929e35fe5cebc
Dillo Raju <draju@governmentcio.com>
2020-10-07 11:45:27 -0400

    test names must match their expectations

--- modules/health_quest/spec/request/pgd_questionnaires_request_spec.rb
+++ modules/health_quest/spec/request/pgd_questionnaires_request_spec.rb
@@ -3,14 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe 'health_quest questionnaires', type: :request, skip_mvi: true do
-  include SchemaMatchers
-
-  let(:dummy_headers) { {} }
   let(:access_denied_message) { 'You do not have access to the health quest service' }
 
   before do
     sign_in_as(current_user)
-    allow_any_instance_of(HealthQuest::SessionService).to receive(:headers).and_return(dummy_headers)
   end
 
   describe 'GET questionnaires' do
@@ -23,7 +19,7 @@ RSpec.describe 'health_quest questionnaires', type: :request, skip_mvi: true do
         expect(response).to have_http_status(:forbidden)
       end
 
-      it 'has forbidden status' do
+      it 'has access denied message' do
         get '/health_quest/v0/pgd_questionnaires/32'
 
         expect(JSON.parse(response.body)['errors'].first['detail']).to eq(access_denied_message)
@@ -33,7 +29,7 @@ RSpec.describe 'health_quest questionnaires', type: :request, skip_mvi: true do
     context 'health quest user' do
       let(:current_user) { build(:user, :health_quest) }
   
-      it 'has access to questionnaires have access' do
+      it 'has success status' do
         get '/health_quest/v0/pgd_questionnaires/32'
 
         expect(response).to have_http_status(:ok)
