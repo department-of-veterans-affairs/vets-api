f6c16b879f17053631e2aee633e2fed944fb7e29
Steven Templeman <steven.templeman@oddball.io>
2020-10-07 15:22:30 -0700

    Add NoticeOfDisagreements#ContestableIssuesController + specs

---
+++ modules/appeals_api/spec/requests/v1/notifce_of_disagreements/contestable_issues_controller_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../../../support/shared_examples_contestable_issues'
+
+describe AppealsApi::V1::DecisionReviews::NoticeOfDisagreements::ContestableIssuesController, type: :request do
+  include_examples 'contestable issues index requests', appeal_type: 'notice_of_disagreements'
+end
