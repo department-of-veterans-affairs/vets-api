114ea116e0d988335ad4e5b45417438ce7605a12
Steven Templeman <steven.templeman@oddball.io>
2020-10-07 14:47:03 -0700

    Move spec into higher_level_reviews folder

--- modules/appeals_api/spec/requests/v1/higher_level_reviews/contestable_issues_controller_spec.rb
+++ modules/appeals_api/spec/requests/v1/higher_level_reviews/contestable_issues_controller_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require_relative '../../support/shared_examples_contestable_issues'
+require_relative '../../../support/shared_examples_contestable_issues'
 
 describe AppealsApi::V1::DecisionReviews::HigherLevelReviews::ContestableIssuesController, type: :request do
   include_examples 'contestable issues index requests', appeal_type: 'higher_level_reviews'
