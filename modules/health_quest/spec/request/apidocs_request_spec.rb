837f3d9b1ef61c2526ff2c262761810a303bdd84
Dillo Raju <draju@governmentcio.com>
2020-10-08 14:18:21 -0400

    cleaning up test

--- modules/health_quest/spec/request/apidocs_request_spec.rb
+++ modules/health_quest/spec/request/apidocs_request_spec.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-RSpec.describe 'vaos appointment requests', type: :request do
+
+RSpec.describe 'health quest appointment requests', type: :request do
   describe 'GET /health_quest/v0/apidocs' do
-    it 'renders the apidocs as json' do
+    let(:openapi_version) { %w[openapi 3.0.0] }
+
+    it 'has a success status' do
       get '/health_quest/v0/apidocs'
 
       expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body).first).to eq(%w[openapi 3.0.0])
+    end
+
+    it 'has a correct openapi version' do
+      get '/health_quest/v0/apidocs'
+
+      expect(JSON.parse(response.body).first).to eq(openapi_version)
     end
   end
 end
