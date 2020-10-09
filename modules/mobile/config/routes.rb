f19e627c333c0920a7e27283e9805ed5b55df89e
Jayson Perkins <jaysonperkins51@gmail.com>
2020-10-09 18:10:13 -0400

    moved into same namespace block

--- modules/mobile/config/routes.rb
+++ modules/mobile/config/routes.rb
@@ -5,9 +5,6 @@ Mobile::Engine.routes.draw do
 
   namespace :v0 do
     get '/user', to: 'users#show'
-  end
-
-  namespace :v0 do
     get '/military-service-history', to: 'military_information#get_service_history'
   end
 end
