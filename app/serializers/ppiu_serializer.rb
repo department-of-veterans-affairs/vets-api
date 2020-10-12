a0cc45f4abcc4b8bca5b524caf7ceb5c4afa5cce
Lihan Li <frankieteardrop@gmail.com>
2020-10-12 09:29:54 -0700

    move mask sensitive method to string helpers

--- app/serializers/ppiu_serializer.rb
+++ app/serializers/ppiu_serializer.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'string_helpers'
 
 class PPIUSerializer < ActiveModel::Serializer
   attribute :responses
@@ -6,14 +7,10 @@ class PPIUSerializer < ActiveModel::Serializer
   def responses
     object.responses.each do |response|
       account_number = response.payment_account&.account_number
-      response.payment_account.account_number = mask(account_number) if account_number
+      response.payment_account.account_number = StringHelpers.mask_sensitive(account_number) if account_number
       routing_number = response.payment_account&.financial_institution_routing_number
-      response.payment_account.financial_institution_routing_number = mask(routing_number) if routing_number
+      response.payment_account.financial_institution_routing_number = StringHelpers.mask_sensitive(routing_number) if routing_number
     end
-  end
-
-  def mask(number)
-    number.gsub(/.(?=.{4})/, '*')
   end
 
   def id
