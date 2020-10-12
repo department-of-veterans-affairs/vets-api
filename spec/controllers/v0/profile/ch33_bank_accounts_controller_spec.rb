7d74cbdcf4b0adcfced88c14159645fb83226a56
Lihan Li <frankieteardrop@gmail.com>
2020-10-12 09:38:59 -0700

    finish ch33 bank account index spec

--- spec/controllers/v0/profile/ch33_bank_accounts_controller_spec.rb
+++ spec/controllers/v0/profile/ch33_bank_accounts_controller_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe V0::Profile::Ch33BankAccountsController, type: :controller do
-
   let(:user) { FactoryBot.build(:ch33_dd_user) }
 
   before do
@@ -15,7 +14,18 @@ RSpec.describe V0::Profile::Ch33BankAccountsController, type: :controller do
       VCR.use_cassette('bgs/service/find_ch33_dd_eft', VCR::MATCH_EVERYTHING) do
         get(:index)
       end
-      binding.pry; fail
+
+      expect(JSON.parse(response.body)).to eq(
+        {
+          'data' => {
+            'id' => '', 'type' => 'savon_responses',
+            'attributes' => {
+              'account_type' => 'Checking', 'account_number' => '123',
+              'financial_institution_routing_number' => '*****9982'
+            }
+          }
+        }
+      )
     end
   end
 end
