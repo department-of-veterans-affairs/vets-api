b139b387bf91fa18d15b89e420062ca86f1967fe
Braden Shipley <bradenrshipley@gmail.com>
2020-10-13 11:27:30 -0500

    change validation method for DirectoryApplication model

--- app/models/directory_application.rb
+++ app/models/directory_application.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class DirectoryApplication < ApplicationRecord
-  validate(attribute_names.reject { |attr| attr =~ /id|created_at|updated_at/i })
+  validates :name, :logo_url, :app_type, :service_categories, :platforms,
+            :app_url, :description, :privacy_url, :tos_url, presence: true
 end
