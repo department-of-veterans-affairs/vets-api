c1984e27e1ff86e33872ec5cb36ca28bfaa4154e
Derek Dyer <derek@digitalderek.com>
2020-10-13 12:11:05 -0400

    Add changes from testing with Jese

--- app/models/saved_claim/education_career_counseling_claim.rb
+++ app/models/saved_claim/education_career_counseling_claim.rb
@@ -8,18 +8,23 @@ class SavedClaim::EducationCareerCounselingClaim < CentralMailClaim
   end
 
   def add_claimant_info(current_user)
-    parsed_form.merge!(
-      {
-        'claimantInformation' => {
-          'fullName' => {
-            'first' => current_user.first_name,
-            'middle' => current_user.middle_name || '',
-            'last' => current_user.last_name
-          },
-          'ssn' => current_user.ssn,
-          'dateOfBirth' => current_user.birth_date
-        }
+    updated_form = parsed_form
+    updated_form['claimantInformation'] = {
+        'fullName' => {
+          'first' => current_user.first_name,
+          'middle' => current_user.middle_name || '',
+          'last' => current_user.last_name
+        },
+        'veteranSocialSecurityNumber' => current_user.ssn,
+        'dateOfBirth' => current_user.birth_date
       }
-    )
+
+    updated_form['veteranFullName'] = {
+          'first' => current_user.first_name,
+          'middle' => current_user.middle_name || '',
+          'last' => current_user.last_name
+        }
+
+    update(form: updated_form.to_json)
   end
 end
