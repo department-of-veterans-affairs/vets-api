9798eae76ede93a4a4fd3773fed9390b17c308d7
Derek Dyer <derek@digitalderek.com>
2020-10-13 12:15:29 -0400

    Add lint fixes

--- app/models/saved_claim/education_career_counseling_claim.rb
+++ app/models/saved_claim/education_career_counseling_claim.rb
@@ -10,20 +10,20 @@ class SavedClaim::EducationCareerCounselingClaim < CentralMailClaim
   def add_claimant_info(current_user)
     updated_form = parsed_form
     updated_form['claimantInformation'] = {
-        'fullName' => {
-          'first' => current_user.first_name,
-          'middle' => current_user.middle_name || '',
-          'last' => current_user.last_name
-        },
-        'veteranSocialSecurityNumber' => current_user.ssn,
-        'dateOfBirth' => current_user.birth_date
-      }
+      'fullName' => {
+        'first' => current_user.first_name,
+        'middle' => current_user.middle_name || '',
+        'last' => current_user.last_name
+      },
+      'veteranSocialSecurityNumber' => current_user.ssn,
+      'dateOfBirth' => current_user.birth_date
+    }
 
     updated_form['veteranFullName'] = {
-          'first' => current_user.first_name,
-          'middle' => current_user.middle_name || '',
-          'last' => current_user.last_name
-        }
+      'first' => current_user.first_name,
+      'middle' => current_user.middle_name || '',
+      'last' => current_user.last_name
+    }
 
     update(form: updated_form.to_json)
   end
