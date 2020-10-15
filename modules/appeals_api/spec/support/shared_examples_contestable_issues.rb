96bc6ef36d5231576a1dbaac2d277257407ed7a2
Steven Templeman <steven.templeman@oddball.io>
2020-10-15 14:13:41 -0700

    Add specs for testing file number being used in place of ssn

--- modules/appeals_api/spec/support/shared_examples_contestable_issues.rb
+++ modules/appeals_api/spec/support/shared_examples_contestable_issues.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'contestable issues index requests' do |options|
-  let(:get_issues) do
-    get(
-      "/services/appeals/v1/decision_reviews/#{options[:decision_review_type]}/" \
-      "contestable_issues/#{options[:benefit_type]}",
-      headers: {
-        'X-VA-SSN' => '872958715',
-        'X-VA-Receipt-Date' => '2019-12-01'
-      }
-    )
-  end
-
   describe '#index' do
-    it 'GETs contestable_issues from Caseflow successfully' do
-      VCR.use_cassette("caseflow/#{options[:decision_review_type]}/contestable_issues") do
-        get_issues
-        expect(response).to have_http_status(:ok)
-        json = JSON.parse(response.body)
-        expect(json['data']).not_to be nil
+    context 'when using SSN header as veteran identifier' do
+      it 'GETs contestable_issues from Caseflow successfully' do
+        VCR.use_cassette("caseflow/#{options[:decision_review_type]}/contestable_issues") do
+          get_issues(ssn: '872958715', file_number: nil, options: options)
+          expect(response).to have_http_status(:ok)
+          json = JSON.parse(response.body)
+          expect(json['data']).not_to be nil
+        end
+      end
+    end
+
+    context 'when using file_number header as veteran identifier' do
+      it 'GETs contestable_issues from Caseflow successfully' do
+        VCR.use_cassette("caseflow/#{options[:decision_review_type]}/contestable_issues-by-file-number") do
+          get_issues(file_number: '123456789', ssn: nil, options: options)
+          expect(response).to have_http_status(:ok)
+          json = JSON.parse(response.body)
+          expect(json['data']).not_to be nil
+        end
       end
     end
 
@@ -35,7 +37,7 @@ RSpec.shared_examples 'contestable issues index requests' do |options|
       end
 
       it 'returns a 502 when Caseflow returns an unusable response' do
-        get_issues
+        get_issues(options: options)
         expect(response).to have_http_status(:bad_gateway)
         expect(JSON.parse(response.body)['errors']).to be_an Array
       end
@@ -54,10 +56,26 @@ RSpec.shared_examples 'contestable issues index requests' do |options|
       end
 
       it 'lets 4XX responses passthrough' do
-        get_issues
+        get_issues(options: options)
         expect(response.status).to be status
         expect(JSON.parse(response.body)).to eq body
       end
     end
+  end
+
+  private
+
+  def get_issues(ssn: '872958715', file_number: nil, options:)
+    headers = { 'X-VA-Receipt-Date' => '2019-12-01' }
+
+    if file_number.present?
+      headers['X-VA-File-Number'] = file_number
+    elsif ssn.present?
+      headers['X-VA-SSN'] = ssn
+    end
+
+    get("/services/appeals/v1/decision_reviews/#{options[:decision_review_type]}/" \
+    "contestable_issues/#{options[:benefit_type]}",
+        headers: headers)
   end
 end
