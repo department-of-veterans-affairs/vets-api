3aaa93906a72f78603d636a89362de10d5fcd8e4
Braden Shipley <bradenrshipley@gmail.com>
2020-10-19 16:17:37 -0500

    moved all apps_api related files to apps_api module

--- modules/apps_api/app/models/directory_application.rb
+++ modules/apps_api/app/models/directory_application.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class DirectoryApplication < ApplicationRecord
   validates :name, :logo_url, :app_type, :service_categories, :platforms,
             :app_url, :description, :privacy_url, :tos_url, presence: true
--- modules/apps_api/spec/requests/v0/application_directory_request_spec.rb
+++ modules/apps_api/spec/requests/v0/application_directory_request_spec.rb
@@ -4,20 +4,10 @@ require 'rails_helper'
 
 RSpec.describe 'Application Directory Endpoint', type: :request do
   describe '#get /services/apps/v0/directory' do
-    it 'returns Okta Application Directory JSON' do
-      VCR.use_cassette('okta/directories') do
-        get '/services/apps/v0/directory'
-        expect(response).to have_http_status(:success)
-        JSON.parse(response.body)
-      end
-    end
     it 'returns a populated list of applications' do
-      VCR.use_cassette('okta/directories') do
-        get '/services/apps/v0/directory'
-        expect(response).to have_http_status(:success)
-        body = JSON.parse(response.body)
-        expect(body).not_to be_empty
-      end
+      get '/services/apps/v0/directory'
+      body = JSON.parse(response.body)
+      expect(body).not_to be_empty
     end
   end
 
