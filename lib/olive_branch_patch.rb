78407f0780e0354655b782d73739f10b90f08454
Philip Becker <boofungal316@yahoo.com>
2020-10-20 10:52:29 -0400

    WIP - add key without inflection

--- lib/olive_branch_patch.rb
+++ lib/olive_branch_patch.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
 module OliveBranchMiddlewareExtension
+  VA_KEY_VALUE_PAIR_REGEX = %r[\\\"([^"]+VA[^"]*)\\\":\\\"([^"]*)\\\"].freeze
   def call(env)
     result = super(env)
     status, headers, response = result
     # if response.any? { |json| json =~ /VA/ }
-    if env["HTTP_X_KEY_INFLECTION"] =~ /camel/i
+    if env['HTTP_X_KEY_INFLECTION'] =~ /camel/i
       binding.pry
-      if response.any? { |json| json =~ /\\\"[^"]+VA.*\\\":/ }
-        binding.pry
+      # if response.any? { |json| json =~ /\\\"[^"]+VA.*\\\":/ }
+      if response.is_a? Array
+        response.each do |json|
+          if match = json.match(VA_KEY_VALUE_PAIR_REGEX)
+            key = match[1]
+            value = match[2]
+            json.gsub!(VA_KEY_VALUE_PAIR_REGEX, "\"#{key}\":\"#{value}\", \"#{key.gsub('VA', 'Va')}\":\"#{value}\"")
+          end
+        end
       end
     end
     result
--- spec/lib/olive_branch_patch_spec.rb
+++ spec/lib/olive_branch_patch_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 class ParamsAsJsonController < ActionController::API
   def index
     params.permit!
-    render json: params.reject { |k,_| %w(controller action).include?(k) } .to_json
+    render json: params.reject { |k, _| %w[controller action].include?(k) }.to_json
   end
 end
 
@@ -15,10 +15,16 @@ end
 
 describe 'OliveBranchPatch', type: :request do
   it 'does not change response keys when camel inflection is not used'
+  it 'does not change a Rack::Response response object'
   it 'does not add keys if `VA` is not in the middle of a key' do
     hash = { hello_there: 'hello there' }
     get '/some_json', params: hash, headers: { 'X-Key-Inflection' => 'camel' }
     expect(JSON.parse(response.body).keys).to eq ['helloThere']
   end
-  it 'adds a second key to data with `VA` in the key except the key uses `Va`'
+  it 'adds a second key to data with `VA` in the key except the key uses `Va`' do
+    hash = { we_love_the_va: true }
+    get '/some_json', params: hash, headers: { 'X-Key-Inflection' => 'camel' }
+    expect(JSON.parse(response.body).keys).to include('weLoveTheVa', 'weLoveTheVA')
+  end
+  it 'adds a second key to data with `VA` in the key except the key uses `Va` for each instance of `VA`'
 end
