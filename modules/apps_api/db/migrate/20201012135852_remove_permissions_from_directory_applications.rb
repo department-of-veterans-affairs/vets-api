16a88c105439f0c9223a5ac5ac53e06b5b67ea5d
Braden Shipley <bradenrshipley@gmail.com>
2020-10-21 09:33:38 -0500

    Delete 20201012135852_remove_permissions_from_directory_applications.rb

--- modules/apps_api/db/migrate/20201012135852_remove_permissions_from_directory_applications.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemovePermissionsFromDirectoryApplications < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :directory_applications, :permissions, :text
-  end
-end
