87e66399e3aca4f04f7b7f6126787bda3b8997e9
Lihan Li <frankieteardrop@gmail.com>
2020-10-22 12:54:58 -0700

    remove stub carma responses

--- spec/request/caregivers_assistance_claims_request_spec.rb
+++ spec/request/caregivers_assistance_claims_request_spec.rb
@@ -86,88 +86,57 @@ RSpec.describe 'Caregivers Assistance Claims', type: :request do
       }
     end
 
-    context 'when :stub_carma_responses is disabled' do
-      subject do
-        post endpoint, params: body, headers: headers
+    subject do
+      post endpoint, params: body, headers: headers
+    end
+
+    let(:body) do
+      form_data = build_valid_form_submission.call
+
+      { caregivers_assistance_claim: { form: form_data.to_json } }.to_json
+    end
+
+    it_behaves_like 'any invalid submission', endpoint: '/v0/caregivers_assistance_claims'
+
+    timestamp = DateTime.parse('2020-03-09T06:48:59-04:00')
+
+    context 'with an invalid veteran status' do
+      before do
+        expect_any_instance_of(
+          Form1010cg::Service
+        ).to receive(:icn_for).with('veteran').and_return(Form1010cg::Service::NOT_FOUND)
+        expect(Settings.google_analytics).to receive(:tracking_id).and_return('foo')
+        expect(Settings).to receive(:vsp_environment).and_return('staging')
       end
 
-      let(:body) do
-        form_data = build_valid_form_submission.call
-
-        { caregivers_assistance_claim: { form: form_data.to_json } }.to_json
-      end
-
-      it_behaves_like 'any invalid submission', endpoint: '/v0/caregivers_assistance_claims'
-
-      timestamp = DateTime.parse('2020-03-09T06:48:59-04:00')
-
-      context 'with an invalid veteran status' do
-        before do
-          expect_any_instance_of(
-            Form1010cg::Service
-          ).to receive(:icn_for).with('veteran').and_return(Form1010cg::Service::NOT_FOUND)
-          expect(Settings.google_analytics).to receive(:tracking_id).and_return('foo')
-          expect(Settings).to receive(:vsp_environment).and_return('staging')
+      it 'logs an event to google analytics' do
+        VCR.use_cassette('staccato/1010cg', VCR::MATCH_EVERYTHING) do
+          subject
         end
 
-        it 'logs an event to google analytics' do
-          VCR.use_cassette('staccato/1010cg', VCR::MATCH_EVERYTHING) do
-            subject
-          end
-
-          expect(response.code).to eq('503')
-        end
-      end
-
-      it 'can submit a valid submission', run_at: timestamp.iso8601 do
-        expect(Flipper).to receive(:enabled?).with(:stub_carma_responses).and_return(false).twice
-
-        VCR.use_cassette 'mvi/find_candidate/valid', vcr_options do
-          VCR.use_cassette 'carma/auth/token/200', vcr_options do
-            VCR.use_cassette 'carma/submissions/create/201', vcr_options do
-              VCR.use_cassette 'carma/attachments/upload/201', vcr_options do
-                subject
-              end
-            end
-          end
-        end
-
-        expect(response.code).to eq('200')
-
-        res_body = JSON.parse(response.body)
-
-        expect(res_body['data']).to be_present
-        expect(res_body['data']['type']).to eq 'form1010cg_submissions'
-        expect(DateTime.parse(res_body['data']['attributes']['submittedAt'])).to eq timestamp
-        expect(res_body['data']['attributes']['confirmationNumber']).to eq 'aB935000000F3VnCAK'
+        expect(response.code).to eq('503')
       end
     end
 
-    context 'when :stub_carma_responses is enabled' do
-      it_behaves_like 'any invalid submission', endpoint: '/v0/caregivers_assistance_claims'
-
-      timestamp = DateTime.parse('2020-03-09T06:48:59-04:00')
-
-      it 'will mock responses from CARMA', run_at: timestamp.iso8601 do
-        form_data = build_valid_form_submission.call
-
-        body = { caregivers_assistance_claim: { form: form_data.to_json } }.to_json
-
-        expect(Flipper).to receive(:enabled?).with(:stub_carma_responses).and_return(true).twice
-
-        VCR.use_cassette 'mvi/find_candidate/valid', vcr_options do
-          post endpoint, params: body, headers: headers
+    it 'can submit a valid submission', run_at: timestamp.iso8601 do
+      VCR.use_cassette 'mvi/find_candidate/valid', vcr_options do
+        VCR.use_cassette 'carma/auth/token/200', vcr_options do
+          VCR.use_cassette 'carma/submissions/create/201', vcr_options do
+            VCR.use_cassette 'carma/attachments/upload/201', vcr_options do
+              subject
+            end
+          end
         end
-
-        expect(response.code).to eq('200')
-
-        res_body = JSON.parse(response.body)
-
-        expect(res_body['data']).to be_present
-        expect(res_body['data']['type']).to eq 'form1010cg_submissions'
-        expect(DateTime.parse(res_body['data']['attributes']['submittedAt'])).to eq timestamp
-        expect(res_body['data']['attributes']['confirmationNumber']).to eq 'aB935000000F3VnCAK'
       end
+
+      expect(response.code).to eq('200')
+
+      res_body = JSON.parse(response.body)
+
+      expect(res_body['data']).to be_present
+      expect(res_body['data']['type']).to eq 'form1010cg_submissions'
+      expect(DateTime.parse(res_body['data']['attributes']['submittedAt'])).to eq timestamp
+      expect(res_body['data']['attributes']['confirmationNumber']).to eq 'aB935000000F3VnCAK'
     end
   end
 
