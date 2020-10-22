b3280d227de2654fd4740db51124c5943cf5eb2a
Lee DeBoom <lee.deboom@oddball.io>
2020-10-22 10:33:48 -0500

    API-1544 remove comments

--- modules/vba_documents/spec/request/metadata_request_spec.rb
+++ modules/vba_documents/spec/request/metadata_request_spec.rb
@@ -11,38 +11,6 @@ RSpec.describe 'VBA Documents Metadata Endpoint', type: :request do
     end
   end
 
-  # context 'healthchecks' do
-  #   context 'V0' do
-  #     it 'returns correct response and status when healthy' do
-  #       get '/services/vba_documents/v0/healthcheck'
-  #       parsed_response = JSON.parse(response.body)
-  #       expect(response.status).to eq(200)
-  #       expect(parsed_response['data']['attributes']['healthy']).to eq(true)
-  #     end
-  #
-  #     it 'returns correct status when not healthy' do
-  #       allow(CentralMail::Service).to receive(:current_breaker_outage?).and_return(true)
-  #       get '/services/vba_documents/v0/healthcheck'
-  #       expect(response.status).to eq(503)
-  #     end
-  #   end
-  #
-  #   context 'V1' do
-  #     it 'returns correct response and status when healthy' do
-  #       get '/services/vba_documents/v1/healthcheck'
-  #       parsed_response = JSON.parse(response.body)
-  #       expect(response.status).to eq(200)
-  #       expect(parsed_response['data']['attributes']['healthy']).to eq(true)
-  #     end
-  #
-  #     it 'returns correct status when not healthy' do
-  #       allow(CentralMail::Service).to receive(:current_breaker_outage?).and_return(true)
-  #       get '/services/vba_documents/v1/healthcheck'
-  #       expect(response.status).to eq(503)
-  #     end
-  #   end
-  # end
-
   describe '#healthcheck' do
     context 'v0' do
       it 'returns a successful health check' do
