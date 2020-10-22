c791da1a9a983082df74bd81349386e02e3e7bd9
Cris Shupp <cristopher.shupp@libertyits.com>
2020-10-22 12:32:46 -0400

    Trying permissions in build on jenkins

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,9 @@
 
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
-chmod 777 /srv/vets-api/src/Gemfile.lock
+whoami
+ls -la /srv/vets-api/src/
+ls -la /srv/vets-api/src/Gemfile.lock
 bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin"
 
 exec "$@"
