4d46f6171249c67bb686f66132791dc6681bdc64
Philip Becker <boofungal316@yahoo.com>
2020-10-22 16:29:27 -0400

    add whitespace between tests

--- spec/lib/olive_branch_patch_spec.rb
+++ spec/lib/olive_branch_patch_spec.rb
@@ -34,23 +34,28 @@ describe 'OliveBranchPatch', type: :request do
     get '/some_json', params: hash
     expect(JSON.parse(response.body).keys).to eq ['hello_there']
   end
+
   it 'does not change document responses' do
     # TODO: maybe this should return some pdf document from a fixture
+    #  spec/fixtures/pdf_fill/21-0781a/simple.pdf
     text = 'blah blah blah'
     hash = { text: text }
     get '/some_document', params: hash, headers: { 'X-Key-Inflection' => 'camel' }
     expect(response.body).to eq text
   end
+
   it 'does not add keys if `VA` is not in the middle of a key' do
     hash = { hello_there: 'hello there' }
     get '/some_json', params: hash, headers: { 'X-Key-Inflection' => 'camel' }
     expect(JSON.parse(response.body).keys).to eq ['helloThere']
   end
+
   it 'adds a second key to data with `VA` in the key except the key uses `Va`' do
     hash = { we_love_the_va: true }
     get '/some_json', params: hash, headers: { 'X-Key-Inflection' => 'camel' }
     expect(JSON.parse(response.body).keys).to include('weLoveTheVa', 'weLoveTheVA')
   end
+
   it 'adds additional keys to data with `VA` in multiple keys except the keys use `Va` for each instance of `VA`' do
     hash = { we_love_the_va: true, the_va_loves_our_troops: true }
     get '/some_json', params: hash, headers: { 'X-Key-Inflection' => 'camel' }
