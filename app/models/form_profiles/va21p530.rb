06ae2bfdbf5b6a590a9262b4e4bb975d209680ba
camerontesterman <cameronbtesterman@gmail.com>
2020-10-23 09:25:14 -0500

    lint fix

--- app/models/form_profiles/va21p530.rb
+++ app/models/form_profiles/va21p530.rb
@@ -13,10 +13,10 @@ class FormProfiles::VA21p530 < FormProfile
 
   def prefill
     super
-    if vet360_mailing_address.present?
-      @contact_information.address.country = vet360_mailing_address.country_code_iso2
+    @contact_information.address.country = if vet360_mailing_address.present?
+      vet360_mailing_address.country_code_iso2
     else
-      @contact_information.address.country = convert_to_iso2(va_profile_address_hash.country)
+      convert_to_iso2(va_profile_address_hash.country)
     end
   end
 
