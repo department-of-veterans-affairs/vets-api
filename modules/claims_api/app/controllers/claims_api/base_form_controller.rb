f35a226a3135cf0407ed661334ae4cab119c229b
Dan Hinze <dan.hinze@adhocteam.us>
2020-10-27 10:56:24 -0500

    Linting fixes

--- modules/claims_api/app/controllers/claims_api/base_form_controller.rb
+++ modules/claims_api/app/controllers/claims_api/base_form_controller.rb
@@ -76,7 +76,8 @@ module ClaimsApi
     def intent_to_file_options
       {
         intent_to_file_type_code: ClaimsApi::IntentToFile::ITF_TYPES[form_type],
-        participant_claimant_id: form_attributes['participant_claimant_id'] || @current_user.participant_id || target_veteran.participant_id,
+        participant_claimant_id: form_attributes['participant_claimant_id'] ||
+          @current_user.participant_id || target_veteran.participant_id,
         participant_vet_id: form_attributes['participant_vet_id'] || target_veteran.participant_id,
         received_date: v0? && received_date ? received_date : Time.zone.now.strftime('%Y-%m-%dT%H:%M:%S%:z'),
         submitter_application_icn_type_code: ClaimsApi::IntentToFile::SUBMITTER_CODE,
