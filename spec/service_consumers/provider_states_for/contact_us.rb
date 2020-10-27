dd5d00ed9ec20e07d8806978869180e2bc6f99cf
Jacob Gacek <jacob.gacek@thoughtworks.com>
2020-10-27 14:14:00 -0500

    [department-of-veterans-affairs/orchid#149] Fixes lint issues
    Co-authored-by: Shefali Nayak <shefali.nayak@thoughtworks.com>

--- spec/service_consumers/provider_states_for/contact_us.rb
+++ spec/service_consumers/provider_states_for/contact_us.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
 Pact.provider_states_for 'Contact Us' do
-
-  # define multiple provider states as needed for each interaction in the pact
   provider_state 'minimum required data' do
     set_up do
     end
 
     tear_down do
     end
-
-	# no_op - define no_op if a provider state is not necessary
   end
 end
