cde268b97e5a130c8f11195295e930b632a26c4e
Dillo Raju <draju@governmentcio.com>
2020-10-29 17:21:11 -0400

    cleanup appointment request spec

--- modules/health_quest/app/controllers/health_quest/v0/appointments_controller.rb
+++ modules/health_quest/app/controllers/health_quest/v0/appointments_controller.rb
@@ -18,32 +18,6 @@ module HealthQuest
       def appointment_service
         HealthQuest::AppointmentService.new(current_user)
       end
-
-      def appointments
-        @appointments ||=
-          appointment_service.get_appointments(start_date, end_date, pagination_params)
-      end
-
-      def serializer
-        HealthQuest::V0::VAAppointmentsSerializer
-      end
-
-      def valid_params_present?
-        raise Common::Exceptions::ParameterMissing, 'start_date' if params[:start_date].blank?
-        raise Common::Exceptions::ParameterMissing, 'end_date' if params[:end_date].blank?
-      end
-
-      def start_date
-        DateTime.parse(params[:start_date]).in_time_zone
-      rescue ArgumentError
-        raise Common::Exceptions::InvalidFieldValue.new('start_date', params[:start_date])
-      end
-
-      def end_date
-        DateTime.parse(params[:end_date]).in_time_zone
-      rescue ArgumentError
-        raise Common::Exceptions::InvalidFieldValue.new('end_date', params[:end_date])
-      end
     end
   end
 end
--- modules/health_quest/spec/request/appointment_request_by_id_spec.rb
+++ modules/health_quest/spec/request/appointment_request_by_id_spec.rb
@@ -10,19 +10,35 @@ RSpec.describe 'health_quest appointments', type: :request, skip_mvi: true do
     allow_any_instance_of(HealthQuest::UserService).to receive(:session).and_return('stubbed_token')
   end
 
-  context 'loa3 user' do
-    let(:current_user) { build(:user, :health_quest) }
+  describe 'GET appointments' do
+    context 'loa3 user' do
+      let(:current_user) { build(:user, :health_quest) }
 
-    describe 'GET appointments' do
-      it 'has access and returns va appointments' do
-        VCR.use_cassette('health_quest/appointments/get_appointment_by_id', match_requests_on: %i[method uri]) do
-          get '/health_quest/v0/appointments/132'
-          appointment_attr = JSON.parse(response.body)['data']['attributes']
-          expect(response).to have_http_status(:success)
-          expect(response.body).to be_a(String)
-          expect(appointment_attr['clinic_id']).to eq('848')
-          expect(appointment_attr['clinic_friendly_name']).to eq('CHY PC VAR2')
-        end
+      it 'is successful' do
+        get '/health_quest/v0/appointments/132'
+
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'response body is a String' do
+        get '/health_quest/v0/appointments/132'
+
+        expect(response.body).to be_a(String)
+      end
+
+      it 'has attributes' do
+        get '/health_quest/v0/appointments/132'
+
+        appointment_attr = JSON.parse(response.body)['data']['attributes']
+
+        expect(appointment_attr['start_date']).to eq('2020-08-26T15:00:00Z')
+        expect(appointment_attr['sta6aid']).to eq('983')
+        expect(appointment_attr['clinic_id']).to eq('848')
+        expect(appointment_attr['clinic_friendly_name']).to eq('CHY PC VAR2')
+        expect(appointment_attr['facility_id']).to eq('983')
+        expect(appointment_attr['community_care']).to eq(false)
+        expect(appointment_attr['patient_icn']).to eq('1013124304V115761')
+        expect(appointment_attr['vds_appointments']).to be_a(Array)
       end
     end
   end
