b67d71f673a0976f3641156bfc71a8d224a2e3b2
Lee DeBoom <lee.deboom@oddball.io>
2020-10-29 17:14:00 -0500

    API-3070

--- modules/claims_api/spec/requests/metadata_request_spec.rb
+++ modules/claims_api/spec/requests/metadata_request_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'claims_api/health_checker'
 
 RSpec.describe 'Claims Status Metadata Endpoint', type: :request do
   describe '#get /metadata' do
@@ -11,96 +12,66 @@ RSpec.describe 'Claims Status Metadata Endpoint', type: :request do
     end
   end
 
-  context 'healthchecks' do
+  describe '#healthcheck' do
     context 'v0' do
-      it 'returns correct response and status when healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:services_are_healthy?).and_return(true)
+      it 'returns a successful health check' do
         get '/services/claims/v0/healthcheck'
+
         parsed_response = JSON.parse(response.body)
-        expect(response.status).to eq(200)
-        expect(parsed_response['data']['attributes']['healthy']).to eq(true)
-      end
-
-      it 'returns correct status when evss is not healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:mpi_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:bgs_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:vbms_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:evss_is_healthy?).and_return(false)
-        get '/services/claims/v0/healthcheck'
-        expect(response.status).to eq(503)
-      end
-
-      it 'returns correct status when mpi is not healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:bgs_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:vbms_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:evss_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:mpi_is_healthy?).and_return(false)
-        get '/services/claims/v0/healthcheck'
-        expect(response.status).to eq(503)
-      end
-
-      it 'returns correct status when vbms is not healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:evss_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:mpi_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:bgs_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:vbms_is_healthy?).and_return(false)
-        get '/services/claims/v0/healthcheck'
-        expect(response.status).to eq(503)
-      end
-
-      it 'returns correct status when bgs is not healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:vbms_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:evss_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:mpi_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:bgs_is_healthy?).and_return(false)
-        get '/services/claims/v0/healthcheck'
-        expect(response.status).to eq(503)
+        expect(response).to have_http_status(:ok)
+        expect(parsed_response['description']).to eq('Claims API health check')
+        expect(parsed_response['status']).to eq('UP')
+        expect(parsed_response['time']).not_to be_nil
       end
     end
 
     context 'v1' do
-      it 'returns correct response and status when healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:services_are_healthy?).and_return(true)
+      it 'returns a successful health check' do
         get '/services/claims/v1/healthcheck'
+
         parsed_response = JSON.parse(response.body)
-        expect(response.status).to eq(200)
-        expect(parsed_response['data']['attributes']['healthy']).to eq(true)
+        expect(response).to have_http_status(:ok)
+        expect(parsed_response['description']).to eq('Claims API health check')
+        expect(parsed_response['status']).to eq('UP')
+        expect(parsed_response['time']).not_to be_nil
       end
+    end
+  end
 
-      it 'returns correct status when evss is not healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:bgs_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:vbms_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:mpi_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:evss_is_healthy?).and_return(false)
-        get '/services/claims/v1/healthcheck'
-        expect(response.status).to eq(503)
-      end
+  describe '#upstream_healthcheck' do
+    before do
+      time = Time.utc(2020, 9, 21, 0, 0, 0)
+      Timecop.freeze(time)
+    end
 
-      it 'returns correct status when mpi is not healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:bgs_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:vbms_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:evss_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:mpi_is_healthy?).and_return(false)
-        get '/services/claims/v1/healthcheck'
-        expect(response.status).to eq(503)
-      end
+    after { Timecop.return }
 
-      it 'returns correct status when vbms is not healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:bgs_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:evss_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:mpi_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:vbms_is_healthy?).and_return(false)
-        get '/services/claims/v1/healthcheck'
-        expect(response.status).to eq(503)
-      end
+    context 'v0' do
+      it 'returns correct response and status when healthy' do
+        allow_any_instance_of(ClaimsApi::HealthChecker).to receive(:evss_is_healthy?).and_return(true)
+        allow_any_instance_of(ClaimsApi::HealthChecker).to receive(:mpi_is_healthy?).and_return(true)
+        allow_any_instance_of(ClaimsApi::HealthChecker).to receive(:bgs_is_healthy?).and_return(true)
+        allow_any_instance_of(ClaimsApi::HealthChecker).to receive(:vbms_is_healthy?).and_return(true)
+        get '/services/claims/v0/upstream_healthcheck'
+        expect(response).to have_http_status(:ok)
 
-      it 'returns correct status when bgs is not healthy' do
-        allow(ClaimsApi::HealthChecker).to receive(:evss_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:mpi_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:vbms_is_healthy?).and_return(true)
-        allow(ClaimsApi::HealthChecker).to receive(:bgs_is_healthy?).and_return(false)
-        get '/services/claims/v1/healthcheck'
-        expect(response.status).to eq(503)
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['description']).to eq('Claims API upstream health check')
+        expect(parsed_response['status']).to eq('UP')
+        expect(parsed_response['time']).to eq('2020-09-21T00:00:00Z')
+
+        details = parsed_response['details']
+        expect(details['name']).to eq('All upstream services')
+
+        expect(details['upstreamServices'].size).to eq(4)
+        details['upstreamServices'].each do |upstream_service|
+          expect(upstream_service['description']).to be_in(ClaimsApi::HealthChecker::SERVICES.map(&:upcase))
+          expect(upstream_service['status']).to eq('UP')
+          expect(upstream_service['details']['name']).to be_in(ClaimsApi::HealthChecker::SERVICES.map(&:upcase))
+          expect(upstream_service['details']['statusCode']).to eq(200)
+          expect(upstream_service['details']['status']).to eq('OK')
+          expect(upstream_service['details']['time']).to eq('2020-09-21T00:00:00Z')
+        end
       end
     end
   end
