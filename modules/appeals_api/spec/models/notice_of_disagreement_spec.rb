07cfa6ede7566e8ac71a56548178ab97a9a4c1ae
Steven Templeman <steven.templeman@oddball.io>
2020-10-29 13:05:10 -0700

    Add missing specs

--- modules/appeals_api/spec/models/notice_of_disagreement_spec.rb
+++ modules/appeals_api/spec/models/notice_of_disagreement_spec.rb
@@ -20,6 +20,82 @@ describe AppealsApi::NoticeOfDisagreement, type: :model do
     end
   end
 
+  describe 'validations' do
+    describe '#validate_that_at_least_one_set_of_contact_info_is_present' do
+      context 'when veteran is present and claimant is not' do
+        before { form_data['data']['attributes'].delete('claimant') }
+
+        let(:auth_headers) { headers_without_claimant(default_auth_headers) }
+
+        it do
+          expect(notice_of_disagreement.errors).to be_empty
+        end
+      end
+
+      context 'when claimant is present and veteran is not' do
+        before { form_data['data']['attributes'].delete('veteran') }
+
+        it do
+          expect(notice_of_disagreement.errors).to be_empty
+        end
+      end
+
+      context 'when claimant and veteran are not present' do
+        let(:auth_headers) { headers_without_claimant(default_auth_headers) }
+
+        before do
+          form_data['data']['attributes'].delete('claimant')
+          form_data['data']['attributes'].delete('veteran')
+        end
+
+        it do
+          expect(notice_of_disagreement.errors.count).to be 1
+          expect(notice_of_disagreement.errors.full_messages.first).to eq("Form data at least one must be included: " \
+"'/data/attributes/veteran', '/data/attributes/claimant'")
+        end
+      end
+    end
+
+    describe '#validate_claimant_properly_included_or_absent' do
+      context 'when all claimant fields are present' do
+        it { expect(notice_of_disagreement.errors).to be_empty }
+      end
+
+      context 'when claimant name is missing' do
+        let(:auth_headers) do
+          default_auth_headers.except('X-VA-Claimant-First-Name', 'X-VA-Claimant-Middle-Initial',
+                                      'X-VA-Claimant-Last-Name')
+        end
+
+        it do
+          expect(notice_of_disagreement.errors.count).to be 1
+          expect(notice_of_disagreement.errors.full_messages.first).to eq("Auth headers if any claimant info is" \
+" present, claimant name must also be present")
+        end
+      end
+
+      context 'when claimant birth date is missing' do
+        let(:auth_headers) { default_auth_headers.except('X-VA-Claimant-Birth-Date') }
+
+        it do
+          expect(notice_of_disagreement.errors.count).to be 1
+          expect(notice_of_disagreement.errors.full_messages.first).to eq("Auth headers if any claimant info is" \
+" present, claimant birth date must also be present")
+        end
+      end
+
+      context 'when claimant contact info is missing' do
+        before { form_data['data']['attributes'].delete('claimant') }
+
+        it do
+          expect(notice_of_disagreement.errors.count).to be 1
+          expect(notice_of_disagreement.errors.full_messages.first).to eq("Form data if any claimant info is present," \
+" claimant contact info (data/attributes/claimant) must also be present")
+        end
+      end
+    end
+  end
+
   describe '10182 schema' do
     context 'missing addressLine1' do
       let(:form_data) do
@@ -40,14 +116,7 @@ describe AppealsApi::NoticeOfDisagreement, type: :model do
         }
       end
 
-      let(:auth_headers) do
-        default_auth_headers.except(
-          'X-VA-Claimant-First-Name',
-          'X-VA-Claimant-Middle-Initial',
-          'X-VA-Claimant-Last-Name',
-          'X-VA-Claimant-Birth-Date'
-        )
-      end
+      let(:auth_headers) { headers_without_claimant(default_auth_headers) }
 
       it('has errors') do
         expect(notice_of_disagreement.errors.count).to be 1
@@ -65,5 +134,66 @@ describe AppealsApi::NoticeOfDisagreement, type: :model do
         expect(notice_of_disagreement.errors.full_messages.first).to include 'Veteran-Last-Name'
       end
     end
+  end
+
+  describe '.date_from_string' do
+    context 'when the string is in the correct format' do
+      it { expect(described_class.date_from_string('2005-12-24')).to eq(Date.parse('2005-12-24')) }
+    end
+
+    context 'when the string is in the incorrect format' do
+      it 'returns nil' do
+        expect(described_class.date_from_string('200-12-24')).to be_nil
+        expect(described_class.date_from_string('12-24-2005')).to be_nil
+        expect(described_class.date_from_string('2005')).to be_nil
+        expect(described_class.date_from_string('abc')).to be_nil
+      end
+    end
+  end
+
+  describe '#claimant_name' do
+    context 'when claimant headers are present' do
+      it do
+        expect(notice_of_disagreement.claimant_name).to eq(
+          "#{default_auth_headers['X-VA-Claimant-First-Name']} " \
+          "#{default_auth_headers['X-VA-Claimant-Middle-Initial']} "\
+          "#{default_auth_headers['X-VA-Claimant-Last-Name']}"
+         )
+      end
+    end
+
+    context 'when claimant headers are not present' do
+      let(:auth_headers) do
+        default_auth_headers.except('X-VA-Claimant-First-Name', 'X-VA-Claimant-Middle-Initial',
+                                    'X-VA-Claimant-Last-Name')
+      end
+
+      it { expect(notice_of_disagreement.claimant_name).to be_empty }
+    end
+  end
+
+  describe '#claimant_birth_date' do
+    context 'when claimant headers are present' do
+      it { expect(notice_of_disagreement.claimant_birth_date).to eq(Date.parse("1970-01-01")) }
+    end
+
+    context 'when claimant headers are not present' do
+      let(:auth_headers) do
+        default_auth_headers.except('X-VA-Claimant-Birth-Date')
+      end
+
+      it { expect(notice_of_disagreement.claimant_birth_date).to be_nil }
+    end
+  end
+
+  private
+
+  def headers_without_claimant(default_auth_headers)
+    default_auth_headers.except(
+        'X-VA-Claimant-First-Name',
+        'X-VA-Claimant-Middle-Initial',
+        'X-VA-Claimant-Last-Name',
+        'X-VA-Claimant-Birth-Date'
+    )
   end
 end
