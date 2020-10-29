7d18311a3f0fa2d99c4ab80b89eb7e34534cc6a1
camerontesterman <cameronbtesterman@gmail.com>
2020-10-29 10:38:04 -0500

    add filenumber/ssn to form profile

--- app/models/form_profiles/va5655.rb
+++ app/models/form_profiles/va5655.rb
@@ -8,4 +8,13 @@ class FormProfiles::VA5655 < FormProfile
       returnUrl: '/veteran-information'
     }
   end
+
+  private
+
+  def va_file_number_last_four
+    (
+      BGS::PeopleService.new(user).find_person_by_participant_id[:file_nbr].presence ||
+      user.ssn.presence
+    )&.last(4)
+  end
 end
