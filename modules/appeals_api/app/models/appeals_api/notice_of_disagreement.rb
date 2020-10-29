4937bf7a6a913c3b5624e9b8b2022d09e3b78561
Ed Mangimelli <edmangimelli@gmail.com>
2020-10-29 14:49:44 -0400

    add schema tests

--- modules/appeals_api/app/models/appeals_api/notice_of_disagreement.rb
+++ modules/appeals_api/app/models/appeals_api/notice_of_disagreement.rb
@@ -17,14 +17,65 @@ module AppealsApi
         MultiJson.load File.read Rails.root.join('modules', 'appeals_api', 'config', 'schemas', "#{filename}.json")
       end
 
-      def json_schemer_error_to_string(error)
-        path = error['data_pointer'].presence || '/'
+      # a json schemer error is a hash with this shape:
+      #
+      # {
+      #   "type": "required",
+      #   "details": {
+      #     "missing_keys": ["addressLine1"]
+      #   },
+      #   "data_pointer": "/data/attributes/veteran/address",
+      #   "data": {
+      #     "addressLine2": "Suite #1200",
+      #     "addressLine3": "Box 4",
+      #     "city": "New York",
+      #     "countryName": "United States",
+      #     "stateCode": "NY",
+      #     "zipCode5": "30012",
+      #     "internationalPostalCode": "1"
+      #   },
+      #   "schema_pointer": "/definitions/nodCreateAddress",
+      #   "schema": {
+      #     "type": "object",
+      #     "additionalProperties": false,
+      #     "properties": {
+      #       "addressLine1": {"type": "string"},
+      #       "addressLine2": {"type": "string"},
+      #       "addressLine3": {"type": "string"},
+      #       "city": {"type": "string"},
+      #       "stateCode": {"$ref": "#/definitions/nodCreateStateCode"},
+      #       "countryName": {"type": "string"},
+      #       "zipCode5": {"type": "string", "pattern": "^[0-9]{5}$"},
+      #       "internationalPostalCode": {"type": "string"}
+      #     },
+      #     "required": [
+      #       "addressLine1",
+      #       "city",
+      #       "countryName",
+      #       "zipCode5"
+      #     ]
+      #   },
+      #   "root_schema": {
+      #     ... # entire schema
+      #   }
+      # }
+      def json_schemer_errors(data:, schema:)
+        data ||= {} # this is to compensate for a JSON Schemer bug --nil input always returns 0 errors
+        JSONSchemer.schema(schema).validate(data).to_a
+      end
 
-        reason = if error['type'] == 'required'
-                   "did not contain the required key #{error.dig('details', 'missing_key')}"
+      def json_schemer_error_to_string(error)
+        type = error['type']
+        schema = error['schema']
+        missing_keys = error.dig('details', 'missing_keys')
+
+        reason = if type == 'required' && missing_keys.present?
+                   "did not contain the required keys: #{missing_keys}"
                  else
-                   "did not match the following requirements: #{error['schema']}"
+                   "did not match the following requirements: #{schema}"
                  end
+
+        path = error['data_pointer'].presence || '/'
 
         "The property \"#{path}\" #{reason}"
       end
@@ -41,6 +92,9 @@ module AppealsApi
       :validate_form_data_against_schema,
       :validate_claimant_properly_included_or_absent,
       :validate_that_at_least_one_set_of_contact_info_is_present
+      # At least one must be present ^^^
+      # --not enforced at the JSON Schema level.
+      # Using JSON Schema's conditional keywords (if, oneOf, anyOf, not, etc) produces fairly unreadable errors.
     )
 
     def claimant_name
@@ -70,9 +124,9 @@ module AppealsApi
     end
 
     def validate_against_schema(data:, schema:, attribute_name:)
-      JSONSchemer.schema(schema).validate(data || {}).to_a
-                 .map { |error| self.class.json_schemer_error_to_string error }
-                 .each { |error_message| errors.add attribute_name, error_message }
+      self.class.json_schemer_errors(data: data, schema: schema)
+          .map { |error| self.class.json_schemer_error_to_string error }
+          .each { |error_message| errors.add attribute_name, error_message }
     end
 
     def validate_claimant_properly_included_or_absent
@@ -105,6 +159,9 @@ module AppealsApi
       errors.add attribute_name, "if any claimant info is present, claimant #{field} must also be present"
     end
 
+    # Note: This only checks for veteran or claimant *contact info*
+    # The veteran's name/ssn/birth date/etc is required in the JSON Schema, and the claimant's name/birth date
+    # is checked for in the preceding validation `validate_claimant_properly_included_or_absent`
     def validate_that_at_least_one_set_of_contact_info_is_present
       return if veteran_contact_info.present? || claimant_contact_info.present?
 
--- modules/appeals_api/spec/models/notice_of_disagreement_spec.rb
+++ modules/appeals_api/spec/models/notice_of_disagreement_spec.rb
@@ -19,4 +19,51 @@ describe AppealsApi::NoticeOfDisagreement, type: :model do
       expect(notice_of_disagreement.errors).to be_empty
     end
   end
+
+  describe '10182 schema' do
+    context 'missing addressLine1' do
+      let(:form_data) do
+        {
+          'data' => {
+            'type' => default_form_data['data']['type'],
+            'attributes' => {
+              'boardReviewOption' => default_form_data['data']['attributes']['boardReviewOption'],
+              'veteran' => {
+                'homeless' => default_form_data['data']['attributes']['veteran']['homeless'],
+                'address' => default_form_data['data']['attributes']['veteran']['address'].except('addressLine1'),
+                'phone' => default_form_data['data']['attributes']['veteran']['phone'],
+                'emailAddressText' => default_form_data['data']['attributes']['veteran']['emailAddressText']
+              }
+            }
+          },
+          'included' => default_form_data['included']
+        }
+      end
+
+      let(:auth_headers) do
+        default_auth_headers.except(
+          'X-VA-Claimant-First-Name',
+          'X-VA-Claimant-Middle-Initial',
+          'X-VA-Claimant-Last-Name',
+          'X-VA-Claimant-Birth-Date'
+        )
+      end
+
+      it('has errors') do
+        expect(notice_of_disagreement.errors.count).to be 1
+        expect(notice_of_disagreement.errors.full_messages.first).to include 'addressLine1'
+      end
+    end
+  end
+
+  describe '10182 headers schema' do
+    context 'missing veteran last name' do
+      let(:auth_headers) { default_auth_headers.except('X-VA-Veteran-Last-Name') }
+
+      it('has errors') do
+        expect(notice_of_disagreement.errors.count).to be 1
+        expect(notice_of_disagreement.errors.full_messages.first).to include 'Veteran-Last-Name'
+      end
+    end
+  end
 end
