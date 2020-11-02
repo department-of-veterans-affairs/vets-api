5754ded71f35d7fc6e0b358bd27e53c512df23b9
Braden Shipley <bradenrshipley@gmail.com>
2020-11-02 14:43:20 -0600

    Base files required to populate directory
    Rake task created and scheduled for 1 minute after server start on
    sidekiq. Worker created to invoke task.

---
+++ modules/apps_api/app/workers/apps_api/directory_loader.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+require 'rake'
+
+module AppsApi
+  class DirectoryLoader
+    include Sidekiq::Worker
+    include SentryLogging
+
+    def perform
+      application_name = Rails.application.class.parent_name
+      application = Object.const_get(application_name)
+      application::Application.load_tasks
+      Rake::Task['apps_api:create_applications'].invoke
+    end
+  end
+end
