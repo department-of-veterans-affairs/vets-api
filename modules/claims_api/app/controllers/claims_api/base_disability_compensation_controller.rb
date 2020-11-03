2cb9ec478761744ec91bbf3b61792cd840425c1d
Dan Hinze <dan.hinze@adhocteam.us>
2020-11-03 14:30:27 -0600

    Add better validations for upload_form_526 (#5167)
    * Add better validations for upload_form_526
    
    * Linting fixes
    
    * Deprecate fetch_or_error_local_claim_id method, absorb  into show method
    
    * simplify self.pending conditions
    
    * Remove sidekiq_options from claim_establisher
    
    * Update response for missing claim on upload_form_526
    
    * Linting fixes
    
    * working on source_name refactor
    
    * Forcing a test fail in ClaimUploader
    
    * Refactor to helper method
    
    * Delay sidekiq job by 30 min if evss_id is nil
    
    * Add test case for evss_id being nil
    
    * Revert source_name refactor for now
    
    * Syntax error in revert
    
    * Reject 526 uploads when autoCestPDFGenerationDisabled is false
    
    Co-authored-by: Michael Bastos <bastosmichael@gmail.com>

--- modules/claims_api/app/controllers/claims_api/base_disability_compensation_controller.rb
+++ modules/claims_api/app/controllers/claims_api/base_disability_compensation_controller.rb
@@ -16,12 +16,21 @@ module ClaimsApi
 
     def upload_form_526
       pending_claim = ClaimsApi::AutoEstablishedClaim.pending?(params[:id])
-      pending_claim.set_file_data!(documents.first, params[:doc_type])
-      pending_claim.save!
 
-      ClaimsApi::ClaimUploader.perform_async(pending_claim.id)
+      if pending_claim && (pending_claim.form_data['autoCestPDFGenerationDisabled'] == true)
+        pending_claim.set_file_data!(documents.first, params[:doc_type])
+        pending_claim.save!
 
-      render json: pending_claim, serializer: ClaimsApi::AutoEstablishedClaimSerializer
+        ClaimsApi::ClaimUploader.perform_async(pending_claim.id)
+
+        render json: pending_claim, serializer: ClaimsApi::AutoEstablishedClaimSerializer
+      elsif pending_claim && (pending_claim.form_data['autoCestPDFGenerationDisabled'] == false)
+        # rubocop:disable Layout/LineLength
+        render json: { status: 422, message: 'Claim submission requires that the "autoCestPDFGenerationDisabled" field must be set to "true" in order to allow a 526 PDF to be uploaded' }.to_json, status: :unprocessable_entity
+        # rubocop:enable Layout/LineLength
+      else
+        render json: { status: 404, message: 'Claim not found' }.to_json, status: :not_found
+      end
     rescue => e
       render json: unprocessable_response(e), status: :unprocessable_entity
     end
