1e0a527ca34be72f048ee8b3335e92ed9f527f65
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-11-04 09:07:52 -0500

    Rename MVI lib to MPI (#5213)
    * rename lib MVI module to MPI
    * update requires to mpi
    * update mpi dir name and MPI module namespace
    * MVI to MPI
    * update rubocop rule with new mpi dir name

--- app/models/mpi_data.rb
+++ app/models/mpi_data.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'mvi/responses/find_profile_response'
-require 'mvi/service'
-require 'mvi/orch_search_service'
+require 'mpi/responses/find_profile_response'
+require 'mpi/service'
+require 'mpi/orch_search_service'
 require 'common/models/redis_store'
 require 'common/models/concerns/cache_aside'
 
@@ -81,7 +81,7 @@ class MPIData < Common::RedisStore
 
   # The profile returned from the MVI service. Either returned from cached response in Redis or the MVI service.
   #
-  # @return [MVI::Models::MviProfile] patient 'golden record' data from MVI
+  # @return [MPI::Models::MviProfile] patient 'golden record' data from MVI
   def profile
     return nil unless user.loa3?
 
@@ -92,7 +92,7 @@ class MPIData < Common::RedisStore
   #
   # @return [String] the status of the last MVI response
   def status
-    return MVI::Responses::FindProfileResponse::RESPONSE_STATUS[:not_authorized] unless user.loa3?
+    return MPI::Responses::FindProfileResponse::RESPONSE_STATUS[:not_authorized] unless user.loa3?
 
     mvi_response.status
   end
@@ -106,7 +106,7 @@ class MPIData < Common::RedisStore
     mvi_response.try(:error)
   end
 
-  # @return [MVI::Responses::FindProfileResponse] the response returned from MVI
+  # @return [MPI::Responses::FindProfileResponse] the response returned from MVI
   def mvi_response
     @mvi_response ||= response_from_redis_or_service
   end
@@ -114,15 +114,15 @@ class MPIData < Common::RedisStore
   # The status of the MVI Add Person call. An Orchestrated MVI Search needs to be made before an MVI add person
   # call is made. The response is recached afterwards so the new ids can be accessed on the next call.
   #
-  # @return [MVI::Responses::AddPersonResponse] the response returned from MVI Add Person call
+  # @return [MPI::Responses::AddPersonResponse] the response returned from MVI Add Person call
   def mvi_add_person
-    search_response = MVI::OrchSearchService.new.find_profile(user)
+    search_response = MPI::OrchSearchService.new.find_profile(user)
     if search_response.ok?
       @mvi_response = search_response
       add_response = mpi_service.add_person(user)
       add_ids(add_response) if add_response.ok?
     else
-      add_response = MVI::Responses::AddPersonResponse.with_failed_orch_search(
+      add_response = MPI::Responses::AddPersonResponse.with_failed_orch_search(
         search_response.status, search_response.error
       )
     end
@@ -146,7 +146,7 @@ class MPIData < Common::RedisStore
   end
 
   def mpi_service
-    @service ||= MVI::Service.new
+    @service ||= MPI::Service.new
   end
 
   def save
@@ -156,7 +156,7 @@ class MPIData < Common::RedisStore
   end
 
   def record_ttl
-    if status == MVI::Responses::FindProfileResponse::RESPONSE_STATUS[:ok]
+    if status == MPI::Responses::FindProfileResponse::RESPONSE_STATUS[:ok]
       # ensure default ttl is used for 'ok' responses
       REDIS_CONFIG[REDIS_CONFIG_KEY][:each_ttl]
     else
