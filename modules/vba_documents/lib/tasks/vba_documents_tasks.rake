3f61790a39614a0c923b1d54ecfe457d6dfe1794
Greg Bowman <gregory.bowman@libertyits.com>
2020-11-04 09:09:15 -0500

    checkpoint

--- modules/vba_documents/lib/tasks/vba_documents_tasks.rake
+++ modules/vba_documents/lib/tasks/vba_documents_tasks.rake
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
-# desc "Explaining what the task does"
-# task :vba_documents do
-#   # Task goes here
-# end
+
+namespace :vba_documents do
+
+  desc "Determines the PDF document and/or attachment violating the 21x21 validation error"
+  task :upload_21x21_violations, [:pdf_dir] => [:environment] do |_, args|
+    raise 'No test file directory provided. Point pdf_dir to the directory with the PDF files to test against.' unless args[:pdf_dir]
+    pdf_dir = args[:pdf_dir]
+    raise "The PDF directory, #{pdf_dir} is not a valid directory path" unless Dir.exist?(pdf_dir)
+    files = Dir.entries(pdf_dir).select { |f| File.file? f }
+
+    files.each do |f|
+      puts f
+    end
+  end
+end
+
--- test_files/pdf_21x21.rb
+++ test_files/pdf_21x21.rb
@@ -1,8 +1,45 @@
-require_dependency 'vba_documents/multipart_parser'
+# require_dependency 'vba_documents/multipart_parser'
+require 'vba_documents/multipart_parser'
 require 'pdf_info'
+
+class PDFInspector
+  attr_accessor :file, :metadata
+
+  def initialize(file:)
+    # raise "Invalid file"
+    @file = file
+    @metadata = Hash.new
+    parse_file
+  end
+
+  def page_count
+    @metadata.pages
+  end
+
+  def pdf_dimensions
+    @metadata.page_size_inches
+  end
+
+  def is_oversized?
+    doc_info = @metadata.page_size_inches
+    doc_info[:height] >= 21 || doc_info[:width] >= 21
+  end
+
+  private
+  def parse_file
+    parts = VBADocuments::MultipartParser.parse(@file)
+    parts_metadata = JSON.parse(parts['metadata'])
+    @source = parts_metadata['source'] # all other data is PII
+
+    # read the PDF content
+    @metadata = PdfInfo::Metadata.read(parts['content']) #metadata.pages
+  end
+
+end
 
 # read all files in the test_files directory excluding this Ruby file
 files = Dir.entries("./test_files").reject! {|f| File.directory? f }.reject!{|f| File.basename(__FILE__).eql?(f)}
+files.map! do |f| "#{Rails.root}/test_files/#{f}" end
 puts "processing files: #{files.count}"
 output = Hash.new
 default = Hash.new.merge!({tempfile: '', dimensions: {}, offending_pdf: false})
@@ -10,9 +47,13 @@ default = Hash.new.merge!({tempfile: '', dimensions: {}, offending_pdf: false})
 files.each do |f|
   puts "Processing file: #{f}"
   data = default.clone
-   parts = VBADocuments::MultipartParser.parse(f)
-  # parts_metadata = JSON.parse(parts['metadata'])
-  metadata = PdfInfo::Metadata.read(parts['content'])
+  parts = VBADocuments::MultipartParser.parse(f)
+  parts_metadata = JSON.parse(parts['metadata'])
+  puts "parts_metadata: #{parts_metadata}" # get source all else is PII
+  puts "******************** source= #{parts_metadata['source']}"
+
+  metadata = PdfInfo::Metadata.read(parts['content']) #metadata.pages
+  puts "content metadata: #{metadata}"
   doc_info = metadata.page_size_inches
   data[:tempfile] = parts['content'].path
   data[:dimensions] = metadata.page_size_inches
@@ -22,9 +63,9 @@ files.each do |f|
   end
 
   attachment_names = parts.keys.select { |k| k.match(/attachment\d+/) }
-  data[:attachments] = [] unless attachment_names.empty?
+  data[:attachments] = [] unless attachment_names.empty? # change to if
 
-  attachment_names.each_with_index do |att, i|
+  attachment_names.each do |att|
     attach_metadata = PdfInfo::Metadata.read(parts[att])
     doc_info = attach_metadata.page_size_inches
     attach_data = default.clone
