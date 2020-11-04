0fe2d6b5fd63b85ba66dfb72aa7a78d3e6d80451
Lihan Li <frankieteardrop@gmail.com>
2020-11-04 15:42:58 -0800

    test betamocks conditional in carma client

--- spec/lib/carma/client/client_spec.rb
+++ spec/lib/carma/client/client_spec.rb
@@ -6,11 +6,13 @@ require 'carma/client/client'
 RSpec.describe CARMA::Client::Client, type: :model do
   let(:restforce_client) do
     restforce_client = double
-    builder = double
-
     expect(subject).to receive(:get_client).and_return(restforce_client)
-    expect(restforce_client).to receive(:builder).and_return(builder)
-    expect(builder).to receive(:insert_before).with(Faraday::Adapter::NetHttp, Betamocks::Middleware)
+
+    if Settings['salesforce-carma'].mock
+      builder = double
+      expect(restforce_client).to receive(:builder).and_return(builder)
+      expect(builder).to receive(:insert_before).with(Faraday::Adapter::NetHttp, Betamocks::Middleware)
+    end
 
     restforce_client
   end
@@ -26,22 +28,36 @@ RSpec.describe CARMA::Client::Client, type: :model do
   end
 
   describe '#create_submission' do
-    it 'accepts a payload and submitts to CARMA' do
-      payload           = { 'my' => 'data' }
-      response_double   = double
+    def self.test_carma_submission
+      it 'accepts a payload and submits to CARMA' do
+        payload           = { 'my' => 'data' }
+        response_double   = double
 
-      expect(restforce_client).to receive(:post).with(
-        '/services/apexrest/carma/v1/1010-cg-submissions',
-        payload,
-        'Content-Type': 'application/json',
-        'Sforce-Auto-Assign': 'FALSE'
-      ).and_return(
-        response_double
-      )
+        expect(restforce_client).to receive(:post).with(
+          '/services/apexrest/carma/v1/1010-cg-submissions',
+          payload,
+          'Content-Type': 'application/json',
+          'Sforce-Auto-Assign': 'FALSE'
+        ).and_return(
+          response_double
+        )
 
-      expect(response_double).to receive(:body).and_return(:response_token)
-      response = subject.create_submission(payload)
-      expect(response).to eq(:response_token)
+        expect(response_double).to receive(:body).and_return(:response_token)
+        response = subject.create_submission(payload)
+        expect(response).to eq(:response_token)
+      end
+    end
+
+    context 'with betamocks enabled' do
+      test_carma_submission
+    end
+
+    context 'with betamocks disabled' do
+      before do
+        Settings['salesforce-carma'].mock = false
+      end
+
+      test_carma_submission
     end
   end
 
