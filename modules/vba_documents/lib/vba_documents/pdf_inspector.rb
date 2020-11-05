70dbf9f1c89e693180561a389f3254e9665a1824
Greg Bowman <gregory.bowman@libertyits.com>
2020-11-05 16:50:05 -0500

    checkpoint utility methods and refactored code

--- modules/vba_documents/lib/vba_documents/pdf_inspector.rb
+++ modules/vba_documents/lib/vba_documents/pdf_inspector.rb
@@ -1,8 +1,7 @@
-# require_dependency 'vba_documents/multipart_parser'
 require 'vba_documents/multipart_parser'
 require 'pdf_info'
-module VBADocuments
 
+module VBADocuments
   class PDFInspector
     attr_accessor :file, :pdf_data
 
@@ -10,6 +9,18 @@ module VBADocuments
       raise ArgumentError.new("Invalid file #{pdf}, does not exist!") unless File.exist? pdf
       @file = pdf
       @pdf_data = inspect_pdf
+    end
+
+    def total_documents
+      @pdf_data[:total_documents]
+    end
+
+    def total_pages
+      @pdf_data[:total_pages]
+    end
+
+    def doc_type
+      @pdf_data[:doc_type]
     end
 
     def inspect
@@ -24,29 +35,31 @@ module VBADocuments
       parts = VBADocuments::MultipartParser.parse(@file)
       data = Hash.new
       data[:tempfile] = parts['content'].path
-      source = JSON.parse(parts['metadata'])['source']
+      parts_metadata = JSON.parse(parts['metadata'])
+      source = parts_metadata['source']
       data[:source] = source
+      data[:doc_type] = parts_metadata['docType'] || 'Unknown'
 
       # read the PDF content
-      metadata = PdfInfo::Metadata.read(parts['content'])
-      doc_page_total = metadata.pages
+      parts_content = PdfInfo::Metadata.read(parts['content'])
+      doc_page_total = parts_content.pages
       data[:page_count] = doc_page_total
-      data[:doc_count] = 1
+      data[:total_documents] = 1
       data[:total_pages] = doc_page_total
 
       # get the dimensions
-      doc_info = metadata.page_size_inches
-      data[:dimensions] = doc_info
-      data[:offending_pdf] = doc_info[:height] >= 21 || doc_info[:width] >= 21
+      doc_dim = parts_content.page_size_inches
+      data[:dimensions] = doc_dim
+      data[:offending_pdf] = doc_dim[:height] >= 21 || doc_dim[:width] >= 21
 
       # check if this PDF has attachments
       attachment_names = parts.keys.select { |k| k.match(/attachment\d+/) }
       data[:attachments] = [] unless attachment_names.empty?
 
       attachment_names.each do |att|
-        attach_metadata = PdfInfo::Metadata.read(parts[att])
-        attach_dim = attach_metadata.page_size_inches
-        attach_pages = attach_metadata.pages
+        attach_content = PdfInfo::Metadata.read(parts[att])
+        attach_dim = attach_content.page_size_inches
+        attach_pages = attach_content.pages
 
         attach_data = Hash.new
         attach_data[:tempfile] = parts[att].path
@@ -58,88 +71,14 @@ module VBADocuments
         doc_page_total += attach_pages
       end
       data[:total_pages] = doc_page_total
-      data[:doc_count] = attachment_names.size + 1
+      data[:total_documents] = attachment_names.size + 1
       {@file => data}
-    end
-
-    private
-
-    def parse_file
-      parts = VBADocuments::MultipartParser.parse(@file)
-      parts_metadata = JSON.parse(parts['metadata'])
-      @source = parts_metadata['source'] # all other data is PII
-
-      # read the PDF content
-      @metadata = PdfInfo::Metadata.read(parts['content']) #metadata.pages
     end
   end
 end
 
 =begin
-  # read all files in the test_files directory excluding this Ruby file
-  files = Dir.entries("./test_files").reject! { |f| File.directory? f }.reject! { |f| File.basename(__FILE__).eql?(f) }
-  files.map! do |f|
-    "#{Rails.root}/test_files/#{f}"
-  end
-  puts "processing files: #{files.count}"
-  output = Hash.new
-  default = Hash.new.merge!({tempfile: '', dimensions: {}, offending_pdf: false})
-
-  files.each do |f|
-    puts "Processing file: #{f}"
-    data = default.clone
-    parts = VBADocuments::MultipartParser.parse(f)
-    parts_metadata = JSON.parse(parts['metadata'])
-    puts "parts_metadata: #{parts_metadata}" # get source all else is PII
-    puts "******************** source= #{parts_metadata['source']}"
-
-    metadata = PdfInfo::Metadata.read(parts['content']) #metadata.pages
-    puts "content metadata: #{metadata}"
-    doc_info = metadata.page_size_inches
-    data[:tempfile] = parts['content'].path
-    data[:dimensions] = metadata.page_size_inches
-
-    if doc_info[:height] >= 21 || doc_info[:width] >= 21
-      data[:offending_pdf] = true
-    end
-
-    attachment_names = parts.keys.select { |k| k.match(/attachment\d+/) }
-    data[:attachments] = [] unless attachment_names.empty? # change to if
-
-    attachment_names.each do |att|
-      attach_metadata = PdfInfo::Metadata.read(parts[att])
-      doc_info = attach_metadata.page_size_inches
-      attach_data = default.clone
-      attach_data[:tempfile] = parts[att].path
-      attach_data[:dimensions] = attach_metadata.page_size_inches
-
-      if doc_info[:height] >= 21 || doc_info[:width] >= 21
-        attach_data[:offending_pdf] = true
-      end
-      data[:attachments] << attach_data
-    end
-
-    output[f] = data
-  end
-end
-p output
-
-#=begin
-parts = VBADocuments::MultipartParser.parse('/home/michael/Downloads/209b706f-c290-47b9-bae4-498bd44c7f3d')
-puts "content: #{parts[DOC_PART_NAME]}"
-metadata = PdfInfo::Metadata.read(parts[DOC_PART_NAME])
-puts doc_info = metadata.page_size_inches
-puts doc_info[:size][:height] >= 21 || doc_info[:size][:width] >= 21
-attachment_names = parts.keys.select { |k| k.match(/attachment\d+/) }
-attachment_names.each_with_index do |att, i|
-  puts "attachment: #{parts[att]}"
-  metadata = PdfInfo::Metadata.read(parts[att])
-  puts doc_info = metadata.page_size_inches
-  puts doc_info[:size][:height] >= 21 || doc_info[:size][:width] >= 21
-
 load './modules/vba_documents/lib/vba_documents/pdf_inspector.rb'
 inspector = VBADocuments::PDFInspector.new(pdf: './test_files/0d8f95d1-567a-4801-84e9-62b2fad59bef')
 inspector = VBADocuments::PDFInspector.new(pdf: './test_files/209b706f-c290-47b9-bae4-498bd44c7f3d')
-s = inspector.to_s
-
 =end
