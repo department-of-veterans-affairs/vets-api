32ff827148b60e86041eedd1037a9daafe662a74
Braden Shipley <bradenrshipley@gmail.com>
2020-11-09 17:00:18 -0600

    Update simplecov_helper.rb
    Wrong line was removed

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -34,7 +34,7 @@ class SimpleCovHelper
       add_group 'Services', 'app/services'
       add_group 'Swagger', 'app/swagger'
       add_group 'Uploaders', 'app/uploaders'
-      add_group 'AppsApi', 'modules/apps_api'
+      add_group 'AppealsApi', 'modules/appeals_api/'
       add_group 'ClaimsApi', 'modules/claims_api/'
       add_group 'OpenidAuth', 'modules/openid_auth/'
       add_group 'VBADocuments', 'modules/vba_documents/'
