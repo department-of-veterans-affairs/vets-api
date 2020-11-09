cadeee36e0fe6d34584dad6b7c45bd4b54f90a7c
Beau Grantham <beau.grantham@va.gov>
2020-11-09 08:59:03 -0500

    Validate iss early

--- app/models/token.rb
+++ app/models/token.rb
@@ -48,7 +48,11 @@ class Token
   end
 
   def valid_issuer?
-    payload['iss'].start_with?(Settings.oidc.issuer_prefix)
+    decoded_token = JWT.decode(@token_string, nil, false, algorithm: 'RS256')
+    iss = decoded_token[0]['iss']
+    !iss.nil? && iss.start_with?(Settings.oidc.issuer_prefix)
+  rescue JWT::DecodeError => e
+    raise error_klass(e.message)
   end
 
   def valid_audience?
