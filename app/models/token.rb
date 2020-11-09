4251c55474a10aa034b53ad2308a7b4b969ebbd9
Beau Grantham <beau.grantham@va.gov>
2020-11-09 12:16:01 -0500

    Additional validation

--- app/models/token.rb
+++ app/models/token.rb
@@ -50,7 +50,7 @@ class Token
   def valid_issuer?
     decoded_token = JWT.decode(@token_string, nil, false, algorithm: 'RS256')
     iss = decoded_token[0]['iss']
-    !iss.nil? && iss.start_with?(Settings.oidc.issuer_prefix)
+    !iss.nil? && iss.match?(/^#{Regexp.escape(Settings.oidc.issuer_prefix)}\/\w+$/)
   rescue JWT::DecodeError => e
     raise error_klass(e.message)
   end
