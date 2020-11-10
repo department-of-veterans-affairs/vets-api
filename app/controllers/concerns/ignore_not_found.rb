666bef7bfcf796d2d63316c55115de16a6b047d3
Kam Karshenas <kam@adhocteam.us>
2020-11-10 12:02:34 -0600

    this fixes specs for EVSS controllers

--- app/controllers/concerns/ignore_not_found.rb
+++ app/controllers/concerns/ignore_not_found.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module IgnoreNotFound
-  def skip_sentry_exception_types
-    ApplicationController::SKIP_SENTRY_EXCEPTION_TYPES + [Common::Exceptions::RecordNotFound]
+  def skip_sentry_exception?(exception)
+    return true if exception.is_a?(Common::Exceptions::RecordNotFound)
+    super(exception)
   end
 end
