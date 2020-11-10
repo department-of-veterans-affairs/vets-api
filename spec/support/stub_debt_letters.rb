76cc29ffe62fac354dbcf5bebfb354b84343cbd6
camerontesterman <cameronbtesterman@gmail.com>
2020-11-10 17:46:11 -0600

    test fix

--- spec/support/stub_debt_letters.rb
+++ spec/support/stub_debt_letters.rb
@@ -5,7 +5,7 @@ require 'dmc/debt_letter_downloader'
 def stub_debt_letters(method)
   let!(:letter_downloader) do
     letter_downloader = double
-    expect(Debts::LetterDownloader).to receive(:new).and_return(letter_downloader)
+    expect(DMC::DebtLetterDownloader).to receive(:new).and_return(letter_downloader)
     letter_downloader
   end
 
