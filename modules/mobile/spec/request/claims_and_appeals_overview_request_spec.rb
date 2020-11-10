4fe2332a5be97e6ba0eb2ece4530c6f5a827c276
Jayson Perkins <jaysonperkins51@gmail.com>
2020-11-10 13:25:42 -0500

    added 401 and 500 error tests

--- modules/mobile/spec/request/claims_and_appeals_overview_request_spec.rb
+++ modules/mobile/spec/request/claims_and_appeals_overview_request_spec.rb
@@ -6,11 +6,11 @@ require_relative '../support/matchers/json_schema_matcher'
 
 RSpec.describe 'claims and appeals overview', type: :request do
   include JsonSchemaMatchers
-  before { iam_sign_in }
 
   describe 'GET /v0/claims-and-appeals-overview' do
     context '#index (all user claims) is polled' do
-      it 'uses camel-inflection and returns empty result, kicks off job, returns full result when job is completed' do
+      before { iam_sign_in }
+      it 'and a result that matches our schema is successfully returned with the 200 status ' do
         VCR.use_cassette('evss/claims/claims') do
           VCR.use_cassette('caseflow/appeals') do
             get '/mobile/v0/claims-and-appeals-overview', headers: iam_headers
@@ -19,6 +19,28 @@ RSpec.describe 'claims and appeals overview', type: :request do
           end
         end
       end
+
+      it 'and invalid headers return a 401 status' do
+        VCR.use_cassette('evss/claims/claims') do
+          VCR.use_cassette('caseflow/appeals') do
+            get '/mobile/v0/claims-and-appeals-overview'
+            expect(response).to have_http_status(:unauthorized)
+            expect(response.body).to match_json_schema('evss_errors')
+          end
+        end
+      end
     end
+
+    context '#index is polled without user sign in' do
+      it 'and not user returns a 500 status' do
+        VCR.use_cassette('evss/claims/claims') do
+          VCR.use_cassette('caseflow/appeals') do
+            get '/mobile/v0/claims-and-appeals-overview', headers: iam_headers
+            expect(response).to have_http_status(:internal_server_error)
+          end
+        end
+      end
+    end
+    
   end
 end
