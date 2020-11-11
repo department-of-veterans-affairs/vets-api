9b27bbe82429d8ca18556ea4cac52468827900e4
camerontesterman <cameronbtesterman@gmail.com>
2020-11-10 18:15:24 -0600

    fsr controller test added

--- app/controllers/v0/financial_status_reports_controller.rb
+++ app/controllers/v0/financial_status_reports_controller.rb
@@ -4,8 +4,6 @@ require 'dmc/fsr_service'
 
 module V0
   class FinancialStatusReportsController < ApplicationController
-    skip_before_action :verify_authenticity_token
-
     def create
       render json: service.submit_financial_status_report(fsr_form)
     end
