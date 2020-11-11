563581b125f5132fd74068d4f17b63f370fde882
Dillo Raju <draju@governmentcio.com>
2020-11-11 12:12:40 -0500

    setup fhir_client module and spec

---
+++ modules/health_quest/app/services/health_quest/pgd/fhir_client.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module HealthQuest
+  module Pgd
+    module FhirClient
+      def url
+        "#{Settings.hqva_mobile.url}/smart-pgd-fhir/v1"
+      end
+
+      def client
+        FHIR::Client.new(url).tap do |client|
+          client.use_dstu2
+          client.default_json
+          client.additional_headers = headers&.merge(accept_headers)
+        end
+      end
+
+      def accept_headers
+        { 'Accept' => 'application/json+fhir' }
+      end
+
+      def headers
+        raise NotImplementedError "#{self.class} should have implemented headers ..."
+      end
+    end
+  end
+end
