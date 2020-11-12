3d17b3916089077929b34c7103485004631bc885
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-11-12 12:41:22 -0800

    set appointment status

--- modules/mobile/app/models/mobile/v0/adapters/va_appointments.rb
+++ modules/mobile/app/models/mobile/v0/adapters/va_appointments.rb
@@ -4,42 +4,71 @@ module Mobile
   module V0
     module Adapters
       class VAAppointments
+        CANCELLED_STATUS = [
+          'CANCELLED BY CLINIC & AUTO RE-BOOK',
+          'CANCELLED BY CLINIC',
+          'CANCELLED BY PATIENT & AUTO-REBOOK',
+          'CANCELLED BY PATIENT'
+        ].freeze
+        FUTURE_HIDDEN = %w[NO-SHOW DELETED].freeze
+        FUTURE_HIDDEN_STATUS = [
+          'ACT REQ/CHECKED IN',
+          'ACT REQ/CHECKED OUT'
+        ].freeze
+        PAST_HIDDEN = %w[FUTURE DELETED null <null> Deleted].freeze
+        PAST_HIDDEN_STATUS = [
+          'ACTION REQUIRED',
+          'INPATIENT APPOINTMENT',
+          'INPATIENT/ACT REQ',
+          'INPATIENT/CHECKED IN',
+          'INPATIENT/CHECKED OUT',
+          'INPATIENT/FUTURE',
+          'INPATIENT/NO ACT TAKN',
+          'NO ACTION TAKEN',
+          'NO-SHOW & AUTO RE-BOOK',
+          'NO-SHOW',
+          'NON-COUNT'
+        ].freeze
         VIDEO_GFE_FLAG = 'MOBILE_GFE'
 
         def parse(appointments)
           facilities = Set.new
 
           appointments_list = appointments.dig('data', 'appointmentList')
-          appointments_list.each do |appointment_hash|
+          appointments_list.map do |appointment_hash|
             facilities.add(appointment_hash['facilityId']) if appointment_hash['facilityId']
             details, type = parse_by_appointment_type(appointment_hash)
-            
-            attributes = {
+            start_date = get_start_date(appointment_hash)
+
+            {
               appointment_type: type,
               comment: comment(details, type),
               facility_id: appointment_hash['facilityId'],
               healthcare_service: healthcare_service(details, type),
               location: nil,
               minutes_duration: minutes_duration(details, type),
-              start_date: get_start_date(appointment_hash)
+              start_date: start_date,
+              status: get_status(details, type, start_date),
+              time_zone: nil
             }
-
-            # pp appointment_hash
-            # puts "-"
-            #
-            pp attributes
-            puts '---'
           end
-
-          puts facilities.to_a.inspect
-          appointments_list
         end
 
         private
 
+        def get_status(details, type, start_date)
+          status = va?(type) ? details['currentStatus'] : details.dig('status', 'code')
+          return nil if should_hide_status?(start_date.past?, status)
+          return Mobile::V0::Appointment::STATUSES['CANCELLED'] if CANCELLED_STATUS.include?(status)
+
+          Mobile::V0::Appointment::STATUSES['BOOKED']
+        end
+
+        def should_hide_status?(is_past, status)
+          is_past && PAST_HIDDEN_STATUS.include?(status) || !is_past && FUTURE_HIDDEN_STATUS.include?(status)
+        end
+
         def get_start_date(appointment_hash)
-          return nil unless appointment_hash['startDate']
-          
           Date.parse(appointment_hash['startDate'])
         end
 
@@ -71,19 +100,19 @@ module Mobile
         end
 
         def comment(details, type)
-          video?(type) ? details['instructionsTitle'] : details['bookingNote']
+          va?(type) ? details['bookingNote'] : details['instructionsTitle']
         end
 
         def on_site?(appointment)
           appointment['vdsAppointments']&.size&.positive?
         end
 
-        def video?(type)
-          type != Mobile::V0::Appointment::APPOINTMENT_TYPES['VA']
+        def va?(type)
+          type == Mobile::V0::Appointment::APPOINTMENT_TYPES['VA']
         end
 
         def healthcare_service(details, type)
-          video?(type) ? video_healthcare_service(details) : details.dig('clinic', 'name')
+          va?(type) ? details.dig('clinic', 'name') : video_healthcare_service(details)
         end
 
         def video_healthcare_service(details)
@@ -99,9 +128,9 @@ module Mobile
 
           provider.first.dig('location', 'facility', 'name')
         end
-        
+
         def minutes_duration(details, type)
-          video?(type) ? details['duration'] : details['appointmentLength']
+          va?(type) ? details['appointmentLength'] : details['duration']
         end
       end
     end
--- modules/mobile/spec/models/adapters/va_appointments_adapter_spec.rb
+++ modules/mobile/spec/models/adapters/va_appointments_adapter_spec.rb
@@ -3,12 +3,44 @@
 require 'rails_helper'
 
 describe Mobile::V0::Adapters::VAAppointments do
-  let(:appointments) do
+  let(:adapted_appointments) do
     file = File.read(Rails.root.join('modules', 'mobile', 'spec', 'support', 'fixtures', 'va_appointments.json'))
-    JSON.parse(file)
+    subject.parse(JSON.parse(file))
   end
+  
+  it 'returns a list of appointments at the expected size' do
+    expect(adapted_appointments.size).to eq(10)
+  end
+  
+  context 'with a booked VA appointment' do
+    let(:booked_va) { adapted_appointments[0] }
+    
+    it 'has a type of VA' do
+      expect(booked_va[:appointment_type]).to eq('VA')
+    end
+    
+    it 'has a facility_id that matches the parent facility id' do
+      expect(booked_va[:facility_id]).to eq('983')
+    end
 
-  it 'is true' do
-    expect(subject.parse(appointments).length).to eq(10)
+    it 'has a healthcare_service that matches the clinic name' do
+      expect(booked_va[:facility_id]).to eq('CHY PC KILPATRICK')
+    end
+  end
+  
+  context 'with a booked VA appointment' do
+    let(:cancelled_va) { adapted_appointments[1] }
+  end
+  
+  context 'with a booked VA appointment' do
+    let(:booked_video_home) { adapted_appointments[7] }
+  end
+  
+  context 'with a booked VA appointment' do
+    let(:booked_video_atlas) { adapted_appointments[8] }
+  end
+  
+  context 'with a booked VA appointment' do
+    let(:booked_video_gfe) { adapted_appointments[9] }
   end
 end
