cc1b252ef5bde5570e590555325c2f305117bcc5
Michael Bastos <bastosmichael@gmail.com>
2020-11-13 12:58:27 -0600

    fixing test coverage

--- modules/claims_api/app/controllers/concerns/claims_api/mpi_verification.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/mpi_verification.rb
@@ -13,8 +13,8 @@ module ClaimsApi
         log_message_to_sentry('MPIError in claims',
                               :warning,
                               body: e.message)
-        render json: { errors: [{ status: 404, detail: 'Veteran not found, some functionality may be limited.' }] },
-               status: :not_found
+        render json: { errors: [{ status: 400, detail: 'Not enough Veteran information, functionality limited.' }] },
+               status: :bad_request
       end
     end
   end
