129bcefd9840ff51e04d726681ac96c6a3e00734
Jayson Perkins <jaysonperkins51@gmail.com>
2020-11-16 16:20:36 -0500

    fix no end date issue, added test for it

--- modules/mobile/spec/request/military_information_request_spec.rb
+++ modules/mobile/spec/request/military_information_request_spec.rb
@@ -12,50 +12,78 @@ RSpec.describe 'military_information', type: :request do
 
       let(:expected_body_multi) do
         {
-          'data' => {
-            'id' => '69ad43ea-6882-5673-8552-377624da64a5',
-            'type' => 'militaryInformation',
-            'attributes' => {
-              'serviceHistory' =>
-                    [
-                      {
-                        'branchOfService' => 'United States Air Force',
-                        'beginDate' => '2007-04-01',
-                        'endDate' => '2016-06-01',
-                        'formattedBeginDate' => 'April 01, 2007',
-                        'formattedEndDate' => 'June 01, 2016'
-                      },
-                      {
-                        'branchOfService' => 'United States Air Force',
-                        'beginDate' => '2000-02-01',
-                        'endDate' => '2004-06-14',
-                        'formattedBeginDate' => 'February 01, 2000',
-                        'formattedEndDate' => 'June 14, 2004'
-                      }
-                    ]
+            'data' => {
+                'id' => '69ad43ea-6882-5673-8552-377624da64a5',
+                'type' => 'militaryInformation',
+                'attributes' => {
+                    'serviceHistory' =>
+                        [
+                            {
+                                'branchOfService' => 'United States Air Force',
+                                'beginDate' => '2007-04-01',
+                                'endDate' => '2016-06-01',
+                                'formattedBeginDate' => 'April 01, 2007',
+                                'formattedEndDate' => 'June 01, 2016'
+                            },
+                            {
+                                'branchOfService' => 'United States Air Force',
+                                'beginDate' => '2000-02-01',
+                                'endDate' => '2004-06-14',
+                                'formattedBeginDate' => 'February 01, 2000',
+                                'formattedEndDate' => 'June 14, 2004'
+                            }
+                        ]
+                }
             }
-          }
         }
       end
 
       let(:expected_body_single) do
         {
-          'data' => {
-            'id' => '69ad43ea-6882-5673-8552-377624da64a5',
-            'type' => 'militaryInformation',
-            'attributes' => {
-              'serviceHistory' =>
-                    [
-                      {
-                        'branchOfService' => 'United States Air Force',
-                        'beginDate' => '2007-04-01',
-                        'endDate' => '2016-06-01',
-                        'formattedBeginDate' => 'April 01, 2007',
-                        'formattedEndDate' => 'June 01, 2016'
-                      }
-                    ]
+            'data' => {
+                'id' => '69ad43ea-6882-5673-8552-377624da64a5',
+                'type' => 'militaryInformation',
+                'attributes' => {
+                    'serviceHistory' =>
+                        [
+                            {
+                                'branchOfService' => 'United States Air Force',
+                                'beginDate' => '2007-04-01',
+                                'endDate' => '2016-06-01',
+                                'formattedBeginDate' => 'April 01, 2007',
+                                'formattedEndDate' => 'June 01, 2016'
+                            }
+                        ]
+                }
             }
-          }
+        }
+      end
+
+      let(:expected_body_no_end_date) do
+        {
+            'data' => {
+                'id' => '69ad43ea-6882-5673-8552-377624da64a5',
+                'type' => 'militaryInformation',
+                'attributes' => {
+                    'serviceHistory' =>
+                        [
+                            {
+                                'branchOfService' => 'United States Army',
+                                'beginDate' => '1990-11-02',
+                                'endDate' => nil,
+                                'formattedBeginDate' => 'November 02, 1990',
+                                'formattedEndDate' => nil
+                            },
+                            {
+                                "branchOfService" => "United States Army",
+                                "beginDate" => "1983-02-23",
+                                "endDate" => "1988-10-04",
+                                "formattedBeginDate" => "February 23, 1983",
+                                "formattedEndDate" => "October 04, 1988"
+                            }
+                        ]
+                }
+            }
         }
       end
 
@@ -76,6 +104,17 @@ RSpec.describe 'military_information', type: :request do
             get '/mobile/v0/military-service-history', headers: iam_headers
             expect(response).to have_http_status(:ok)
             expect(JSON.parse(response.body)).to eq(expected_body_single)
+            expect(response.body).to match_json_schema('mobile_service_history_response')
+          end
+        end
+      end
+
+      context 'military service episode with no end date' do
+        it 'matches the mobile service history schema' do
+          VCR.use_cassette('emis/get_military_service_episodes/valid_no_end_date') do
+            get '/mobile/v0/military-service-history', headers: iam_headers
+            expect(response).to have_http_status(:ok)
+            expect(JSON.parse(response.body)).to eq(expected_body_no_end_date)
             expect(response.body).to match_json_schema('mobile_service_history_response')
           end
         end
