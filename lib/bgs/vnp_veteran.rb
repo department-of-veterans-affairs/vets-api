edcb305793dfef8d592e65a49eb36dd75dfc1924
Kathleen Crawford <kcrawford@governmentcio.com>
2020-11-16 22:03:23 -0800

    removing commented line

--- lib/bgs/vnp_veteran.rb
+++ lib/bgs/vnp_veteran.rb
@@ -16,7 +16,6 @@ module BGS
     def create
       participant = bgs_service.create_participant(@proc_id, @user.participant_id)
       claim_type_end_product = bgs_service.find_benefit_claim_type_increment(@claim_type)
-      # va_file_number = @payload['veteran_information']['va_file_number']
       person_params = veteran.create_person_params(@proc_id, participant[:vnp_ptcpnt_id], @veteran_info)
       address_params = veteran.create_address_params(@proc_id, participant[:vnp_ptcpnt_id], @veteran_info)
       address = bgs_service.create_address(address_params)
