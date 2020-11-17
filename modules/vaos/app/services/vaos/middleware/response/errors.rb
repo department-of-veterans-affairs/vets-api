380efff62b4880f26e1e3e90fe829fb483ad75a1
brad <brad@concord.tech>
2020-11-17 11:23:39 -0800

    injected vars directly into methods

--- modules/vaos/app/services/vaos/middleware/response/errors.rb
+++ modules/vaos/app/services/vaos/middleware/response/errors.rb
@@ -9,22 +9,18 @@ module VAOS
         def on_complete(env)
           return if env.success?
 
-          @status = env.status
-          @body = env.body
-          @url = env.url
-
-          Raven.extra_context(vamf_status: @status, vamf_body: @body, vamf_url: @url)
+          Raven.extra_context(vamf_status: env.status, vamf_body: env.body, vamf_url: env.url)
           case env.status
           when 400, 409
-            raise Common::Exceptions::BackendServiceException.new('VAOS_400', response_values, @status, @body)
+            raise Common::Exceptions::BackendServiceException.new('VAOS_400', response_values, env.status, env.body)
           when 403
-            raise Common::Exceptions::BackendServiceException.new('VAOS_403', response_values, @status, @body)
+            raise Common::Exceptions::BackendServiceException.new('VAOS_403', response_values, env.status, env.body)
           when 404
-            raise Common::Exceptions::BackendServiceException.new('VAOS_404', response_values, @status, @body)
+            raise Common::Exceptions::BackendServiceException.new('VAOS_404', response_values, env.status, env.body)
           when 500..510
             error_500
           else
-            raise Common::Exceptions::BackendServiceException.new('VA900', response_values, @status, @body)
+            raise Common::Exceptions::BackendServiceException.new('VA900', response_values, env.status, env.body)
           end
         end
 
@@ -33,29 +29,29 @@ module VAOS
         def error_500
           # NOTE: This is a temporary patch more on that here:
           # https://github.com/department-of-veterans-affairs/vets-api/pull/5082
-          if /APTCRGT/.match?(@body)
-            raise Common::Exceptions::BackendServiceException.new('VAOS_400', response_values, @status, @body)
+          if /APTCRGT/.match?(env.body)
+            raise Common::Exceptions::BackendServiceException.new('VAOS_400', response_values, env.status, env.body)
           else
-            raise Common::Exceptions::BackendServiceException.new('VAOS_502', response_values, @status, @body)
+            raise Common::Exceptions::BackendServiceException.new('VAOS_502', response_values, env.status, env.body)
           end
         end
 
         def response_values
           {
             detail: detail,
-            source: { vamf_url: @url, vamf_body: @body, vamf_status: @status }
+            source: { vamf_url: env.url, vamf_body: env.body, vamf_status: env.status }
           }
         end
 
         def detail
-          parsed = JSON.parse(@body)
+          parsed = JSON.parse(env.body)
           if parsed['errors']
             parsed['errors'].first['errorMessage']
           else
             parsed['message']
           end
         rescue
-          @body
+          env.body
         end
       end
     end
