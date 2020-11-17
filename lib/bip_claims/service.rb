3d881726feb4d26a43c0a807e79519887bce5f3e
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-11-17 10:35:58 -0500

    Revert "Revert "14738 mvi to mpi and caching (#5306)" (#5323)"
    This reverts commit 81f3c82cce1db660f86bfe9e2f3c8df5b3988e22.

--- lib/bip_claims/service.rb
+++ lib/bip_claims/service.rb
@@ -34,7 +34,7 @@ module BipClaims
     end
 
     def lookup_veteran_from_mpi(claim)
-      veteran = MVI::AttrService.new.find_profile(veteran_attributes(claim))
+      veteran = MPI::AttrService.new.find_profile(veteran_attributes(claim))
       if veteran.profile&.participant_id
         StatsD.increment("#{STATSD_KEY_PREFIX}.mvi_lookup_hit")
         veteran.profile
