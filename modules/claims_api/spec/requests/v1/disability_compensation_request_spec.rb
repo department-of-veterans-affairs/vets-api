e3e236f3fd3aeff069b4cb0b81a6465014ddb829
Dan Hinze <dan.hinze@adhocteam.us>
2020-11-16 23:13:14 -0600

    Test tweak, still failing

--- modules/claims_api/spec/requests/v1/disability_compensation_request_spec.rb
+++ modules/claims_api/spec/requests/v1/disability_compensation_request_spec.rb
@@ -67,7 +67,7 @@ RSpec.describe 'Disability Claims ', type: :request do
     it 'sets the flashes' do
       with_okta_user(scopes) do |auth_header|
         VCR.use_cassette('evss/claims/claims') do
-          post path, params: data, headers: headers
+          post path, params: data, headers: headers.merge(auth_header)
           token = JSON.parse(response.body)['data']['attributes']['token']
           aec = ClaimsApi::AutoEstablishedClaim.find(token)
           expect(aec.flashes).to eq(%w[Hardship POW])
