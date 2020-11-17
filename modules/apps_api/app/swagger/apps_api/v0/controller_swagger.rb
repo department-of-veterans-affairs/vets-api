aa4d7da32440e35f1502652c5dd09f720a5f5006
Braden Shipley <bradenrshipley@gmail.com>
2020-11-17 12:09:57 -0600

    Updated swagger documentation to include scopes endpoint
    Signed-off-by: Braden Shipley <bradenrshipley@gmail.com>

--- modules/apps_api/app/swagger/apps_api/v0/controller_swagger.rb
+++ modules/apps_api/app/swagger/apps_api/v0/controller_swagger.rb
@@ -5,7 +5,7 @@ module AppsApi
     class ControllerSwagger
       include Swagger::Blocks
 
-      swagger_path 'apps' do
+      swagger_path 'directory' do
         operation :get do
           key :summary, 'All Applications'
           key :description, 'Returns all Applications currently onboarded to Va.gov'
@@ -27,6 +27,34 @@ module AppsApi
                 end
               end
             end
+          end
+        end
+      end
+      swagger_path 'directory/scopes/{service_category}' do
+        operation :get do
+          key :summary, 'Scopes'
+          key :description, 'Returns all scopes currently available to a given service category'
+          key :operationId, 'getScopes'
+          key :produces, [
+            'application/json'
+          ]
+          key :tags, [
+            'Apps'
+          ]
+          response 200 do
+            key :description, 'Scope response'
+            content 'application/json' do
+              schema do
+                key :type, :object
+                key :required, [:data]
+                property :data do
+                  key :'$ref', :Scopes
+                end
+              end
+            end
+          end
+          response 204 do
+            key :description, 'Empty scope response'
           end
         end
       end
