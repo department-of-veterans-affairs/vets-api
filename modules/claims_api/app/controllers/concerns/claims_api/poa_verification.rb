0ebc7cf04b060fa4ca3ea9704ce3d1853b8e8b11
Michael Bastos <bastosmichael@gmail.com>
2020-11-18 09:48:28 -0600

    quick update

--- modules/claims_api/app/controllers/concerns/claims_api/poa_verification.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/poa_verification.rb
@@ -7,12 +7,13 @@ module ClaimsApi
     extend ActiveSupport::Concern
 
     included do
-      before_action :verify_power_of_attorney, if: :header_request?
+      before_action :verify_power_of_attorney_using_bgs_service, if: :header_request?
 
-      def verify_power_of_attorney
+      def verify_power_of_attorney_using_bgs_service
         logged_in_representative_user = @current_user
-        target_veteran_to_be_verified = BGS::PowerOfAttorneyVerifier.new(target_veteran)
-        target_veteran_to_be_verified.verify(logged_in_representative_user)
+        target_veteran_to_be_verified = target_veteran
+        verifying_bgs_service = BGS::PowerOfAttorneyVerifier.new(target_veteran_to_be_verified)
+        verifying_bgs_service.verify(logged_in_representative_user)
         true
       rescue => e
         log_message_to_sentry('PoA claims', :warning, body: e.message)
