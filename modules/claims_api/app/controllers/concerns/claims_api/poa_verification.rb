24323a2aa88a86a2178cafe644d6064ec5769738
Michael Bastos <bastosmichael@gmail.com>
2020-11-18 10:15:13 -0600

    moving methods

--- modules/claims_api/app/controllers/concerns/claims_api/poa_verification.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/poa_verification.rb
@@ -12,12 +12,17 @@ module ClaimsApi
       def verify_power_of_attorney_using_bgs_service
         logged_in_representative_user = @current_user
         target_veteran_to_be_verified = target_veteran
+        verify_representative_and_veteran(logged_in_representative_user, target_veteran_to_be_verified)
+      rescue # => e
+        # Need to eventually start logging poa error logs.
+        # log_message_to_sentry('PoA claims', :warning, body: e.message)
+        raise Common::Exceptions::Unauthorized, detail: 'Cannot validate Power of Attorney'
+      end
+
+      def verify_representative_and_veteran(logged_in_representative_user, target_veteran_to_be_verified)
         verifying_bgs_service = BGS::PowerOfAttorneyVerifier.new(target_veteran_to_be_verified)
         verifying_bgs_service.verify(logged_in_representative_user)
         true
-      rescue => e
-        log_message_to_sentry('PoA claims', :warning, body: e.message)
-        raise Common::Exceptions::Unauthorized, detail: 'Cannot validate Power of Attorney'
       end
     end
   end
