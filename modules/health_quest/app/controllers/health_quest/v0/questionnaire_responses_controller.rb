be5c698a6cf637101c0196e675ee2d4334a80e47
Dillo Raju <draju@governmentcio.com>
2020-11-18 06:00:09 -0500

    questionnaire responses controller changes and request specs

--- modules/health_quest/app/controllers/health_quest/v0/questionnaire_responses_controller.rb
+++ modules/health_quest/app/controllers/health_quest/v0/questionnaire_responses_controller.rb
@@ -4,7 +4,7 @@ module HealthQuest
   module V0
     class QuestionnaireResponsesController < HealthQuest::V0::BaseController
       def index
-        factory = HealthQuest::PatientGeneratedData::QuestionnaireResponse::Factory.new(current_user)
+        factory = HealthQuest::PatientGeneratedData::QuestionnaireResponse::Factory.manufacture(current_user)
 
         render json: factory.all.response[:body]
       end
--- modules/health_quest/spec/request/questionnaire_responses_request_spec.rb
+++ modules/health_quest/spec/request/questionnaire_responses_request_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe 'health_quest questionnaire_responses', type: :request do
   let(:access_denied_message) { 'You do not have access to the health quest service' }
   let(:questionnaire_responses_id) { '32' }
 
-  before do
-    sign_in_as(current_user)
-  end
-
   describe 'GET questionnaire response' do
+    before do
+      sign_in_as(current_user)
+    end
+
     context 'loa1 user' do
       let(:current_user) { build(:user, :loa1) }
 
@@ -42,6 +42,10 @@ RSpec.describe 'health_quest questionnaire_responses', type: :request do
     context 'loa1 user' do
       let(:current_user) { build(:user, :loa1) }
 
+      before do
+        sign_in_as(current_user)
+      end
+
       it 'has forbidden status' do
         get '/health_quest/v0/questionnaire_responses'
 
@@ -52,6 +56,26 @@ RSpec.describe 'health_quest questionnaire_responses', type: :request do
         get '/health_quest/v0/questionnaire_responses'
 
         expect(JSON.parse(response.body)['errors'].first['detail']).to eq(access_denied_message)
+      end
+    end
+
+    context 'health quest user' do
+      let(:current_user) { build(:user, :health_quest) }
+      let(:headers) { { 'Accept' => 'application/json+fhir' } }
+      let(:session_service) { double('HealthQuest::SessionService', user: current_user, headers: headers) }
+      let(:client_reply) { double('FHIR::ClientReply', response: { body: { 'resourceType' => 'Bundle' } }) }
+
+      before do
+        sign_in_as(current_user)
+        allow(HealthQuest::SessionService).to receive(:new).with(anything).and_return(session_service)
+        allow_any_instance_of(HealthQuest::PatientGeneratedData::QuestionnaireResponse::MapQuery)
+          .to receive(:get).with(anything).and_return(client_reply)
+      end
+
+      it 'returns a FHIR bundle' do
+        get '/health_quest/v0/questionnaire_responses'
+
+        expect(JSON.parse(response.body)).to eq({ 'resourceType' => 'Bundle' })
       end
     end
   end
