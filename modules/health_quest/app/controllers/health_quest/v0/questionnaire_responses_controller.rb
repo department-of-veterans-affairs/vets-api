51b678cff2b8a49208ddf377fe940cd9636629fd
Dillo Raju <draju@governmentcio.com>
2020-11-19 15:31:52 -0500

    adding factory before_action

--- modules/health_quest/app/controllers/health_quest/v0/questionnaire_responses_controller.rb
+++ modules/health_quest/app/controllers/health_quest/v0/questionnaire_responses_controller.rb
@@ -3,22 +3,21 @@
 module HealthQuest
   module V0
     class QuestionnaireResponsesController < HealthQuest::V0::BaseController
-      def index
-        factory = klass.manufacture(current_user)
+      before_action :factory
 
-        render json: factory.search.response[:body]
+      def index
+        render json: @factory.search.response[:body]
       end
 
       def show
-        factory = klass.manufacture(current_user)
-
-        render json: factory.get(params[:id]).response[:body]
+        render json: @factory.get(params[:id]).response[:body]
       end
 
       private
 
-      def klass
-        HealthQuest::PatientGeneratedData::QuestionnaireResponse::Factory
+      def factory
+        @factory =
+          HealthQuest::PatientGeneratedData::QuestionnaireResponse::Factory.manufacture(current_user)
       end
     end
   end
