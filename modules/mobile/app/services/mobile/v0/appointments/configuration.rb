706fe13b90783a3f52decb31a225ce3476fa8ec6
Alastair Dawson <alastair.j.dawson@gmail.com>
2020-11-20 11:34:24 -0800

    config yard docs

--- modules/mobile/app/services/mobile/v0/appointments/configuration.rb
+++ modules/mobile/app/services/mobile/v0/appointments/configuration.rb
@@ -3,11 +3,25 @@
 module Mobile
   module V0
     module Appointments
+      # Configuration for the Mobile::V0::Appointments::Service. A singleton class that returns
+      # a connection that can make parallel requests
+      #
+      # @example set the configuration in the service
+      #   configuration Mobile::V0::Appointments::Configuration
+      #
       class Configuration < VAOS::Configuration
+        # Service name for breakers integration
+        # @return String the service name
+        #
         def service_name
           'MobileAppointments'
         end
 
+        # Faraday connection object with middleware defined and
+        # the adapter set to typhoeus for parallel connections
+        #
+        # @return Faraday::Connection connection to make http calls
+        #
         def parallel_connection
           Faraday.new(
             base_path, headers: base_request_headers, request: request_options
