604415a0f435bafcbfcd806c9808ad06ee10fbd8
brad <brad@concord.tech>
2020-11-20 08:24:58 -0800

    fixed linting errors

--- modules/vaos/app/services/vaos/middleware/response/errors.rb
+++ modules/vaos/app/services/vaos/middleware/response/errors.rb
@@ -6,23 +6,41 @@ module VAOS
   module Middleware
     module Response
       class Errors < Faraday::Response::Middleware
+        # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
         def on_complete(env)
           return if env.success?
 
           Raven.extra_context(vamf_status: env.status, vamf_body: env.body, vamf_url: env.url)
           case env.status
           when 400, 409
-            raise Common::Exceptions::BackendServiceException.new('VAOS_400', response_values(env.url, env.body, env.status), env.status, env.body)
+            raise Common::Exceptions::BackendServiceException.new(
+              'VAOS_400',
+              response_values(env.url, env.body, env.status),
+              env.status, env.body
+            )
           when 403
-            raise Common::Exceptions::BackendServiceException.new('VAOS_403', response_values(env.url, env.body, env.status), env.status, env.body)
+            raise Common::Exceptions::BackendServiceException.new(
+              'VAOS_403',
+              response_values(env.url, env.body, env.status),
+              env.status, env.body
+            )
           when 404
-            raise Common::Exceptions::BackendServiceException.new('VAOS_404', response_values(env.url, env.body, env.status), env.status, env.body)
+            raise Common::Exceptions::BackendServiceException.new(
+              'VAOS_404',
+              response_values(env.url, env.body, env.status),
+              env.status, env.body
+            )
           when 500..510
             error_500(env.url, env.body, env.status)
           else
-            raise Common::Exceptions::BackendServiceException.new('VA900', response_values(env.url, env.body, env.status), env.status, env.body)
+            raise Common::Exceptions::BackendServiceException.new(
+              'VA900',
+              response_values(env.url, env.body, env.status),
+              env.status, env.body
+            )
           end
         end
+        # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
         private
 
@@ -30,9 +48,17 @@ module VAOS
           # NOTE: This is a temporary patch more on that here:
           # https://github.com/department-of-veterans-affairs/vets-api/pull/5082
           if /APTCRGT/.match?(body)
-            raise Common::Exceptions::BackendServiceException.new('VAOS_400', response_values(url, body, status), status, body)
+            raise Common::Exceptions::BackendServiceException.new(
+              'VAOS_400',
+              response_values(url, body, status),
+              status, body
+            )
           else
-            raise Common::Exceptions::BackendServiceException.new('VAOS_502', response_values(url, body, status), status, body)
+            raise Common::Exceptions::BackendServiceException.new(
+              'VAOS_502',
+              response_values(url, body, status),
+              status, body
+            )
           end
         end
 
