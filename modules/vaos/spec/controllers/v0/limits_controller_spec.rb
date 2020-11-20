ebf3c23c8b0a535f8c4daffb456a529e81c15887
AJ Magdub <ahmed.magdub@va.gov>
2020-11-20 13:14:06 -0700

    continuing work on multiple institution codes for request limits

---
+++ modules/vaos/spec/controllers/v0/limits_controller_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'VAOS::V0::LimitsController', type: :request do
+  before do
+    Flipper.enable('va_online_scheduling')
+    sign_in_as(user)
+    allow_any_instance_of(VAOS::UserService).to receive(:session).and_return('stubbed_token')
+  end
+
+  describe 'GET /vaos/v0/facilities/{id}/limits' do
+    context 'with a loa3 user' do
+      let(:user) { FactoryBot.create(:user, :vaos) }
+
+      it 'returns something' do
+        # get '/vaos/v0/facilities/limits?type_of_care_id=1toc&facility_id[]=1&facility_id[]=2'
+        get '/vaos/v0/facilities/123/limits?type_of_care_id=1toc'
+      end
+    end
+  end
+end
