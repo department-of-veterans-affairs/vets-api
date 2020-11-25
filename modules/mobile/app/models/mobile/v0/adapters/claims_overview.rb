0101d1f0a8b8d9bde18d968bc74e2a9efc7f10e3
Jayson Perkins <jaysonperkins51@gmail.com>
2020-11-25 14:34:22 -0500

    lint fix

--- modules/mobile/app/models/mobile/v0/adapters/claims_overview.rb
+++ modules/mobile/app/models/mobile/v0/adapters/claims_overview.rb
@@ -5,24 +5,26 @@ module Mobile
     module Adapters
       class ClaimsOverview
         def parse(entry)
-          parsedEntry = entry['list_data'] ?
-                            {
-                                id: entry['evss_id'].to_s,
-                                type: 'claim',
-                                subtype: entry['list_data']['status_type'],
-                                completed: entry['list_data']['claim_status'] != 'PEND',
-                                date_filed: Date.strptime(entry['list_data']['date'], '%m/%d/%Y').iso8601,
-                                updated_at: entry['updated_at'].to_time.iso8601
-                            } :
-                            {
-                                id: entry['id'],
-                                type: 'appeal',
-                                subtype: entry['type'],
-                                completed: !entry['attributes']['active'],
-                                date_filed: entry['attributes']['events'][1]['date'],
-                                updated_at: entry['attributes']['updated'].to_time.iso8601
-                            }
-          Mobile::V0::ClaimOverview.new(parsedEntry)
+          parsed_entry = if entry['list_data']
+                           {
+                             id: entry['evss_id'].to_s,
+                             type: 'claim',
+                             subtype: entry['list_data']['status_type'],
+                             completed: entry['list_data']['claim_status'] != 'PEND',
+                             date_filed: Date.strptime(entry['list_data']['date'], '%m/%d/%Y').iso8601,
+                             updated_at: entry['updated_at'].to_time.iso8601
+                           }
+                         else
+                           {
+                             id: entry['id'],
+                             type: 'appeal',
+                             subtype: entry['type'],
+                             completed: !entry['attributes']['active'],
+                             date_filed: entry['attributes']['events'][1]['date'],
+                             updated_at: entry['attributes']['updated'].to_time.iso8601
+                           }
+                         end
+          Mobile::V0::ClaimOverview.new(parsed_entry)
         end
       end
     end
