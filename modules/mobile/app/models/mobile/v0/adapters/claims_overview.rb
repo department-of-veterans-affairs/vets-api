f8701c747050d79065e46fc78994bb9cb6f3ebd7
Jayson Perkins <jaysonperkins51@gmail.com>
2020-11-25 15:49:32 -0500

    14938 mobile claims overview endpoint (#5365)
    * nonparallel success test implemented
    
    * reverse sorted entries
    
    * added 401 and 500 error tests
    
    * lint fixes
    
    * catching upstream errors and adding them to resp
    
    * added test for double upstream failure
    
    * lint fixes, upstream service error tests and imp
    
    * removed 20th element check
    
    * removed old comment
    
    * PR comments
    
    * new implementation, need to update response json
    
    * refactor for implementation, lint fix, test update
    
    * removed unused serializers
    
    * pr comments
    
    * lint fix

---
+++ modules/mobile/app/models/mobile/v0/adapters/claims_overview.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Mobile
+  module V0
+    module Adapters
+      class ClaimsOverview
+        def parse(entry)
+          parsed_entry = if entry['list_data']
+                           {
+                             id: entry['evss_id'].to_s,
+                             type: 'claim',
+                             subtype: entry['list_data']['status_type'],
+                             completed: entry['list_data']['claim_status'] != 'PEND',
+                             date_filed: Date.strptime(entry['list_data']['date'], '%m/%d/%Y').iso8601,
+                             updated_at: entry['updated_at'].to_time.iso8601
+                           }
+                         else
+                           {
+                             id: entry['id'],
+                             type: 'appeal',
+                             subtype: entry['type'],
+                             completed: !entry['attributes']['active'],
+                             date_filed: entry['attributes']['events'][1]['date'],
+                             updated_at: entry['attributes']['updated'].to_time.iso8601
+                           }
+                         end
+          Mobile::V0::ClaimOverview.new(parsed_entry)
+        end
+      end
+    end
+  end
+end
---
+++ modules/mobile/spec/request/claims_and_appeals_overview_request_spec.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../support/iam_session_helper'
+require_relative '../support/matchers/json_schema_matcher'
+
+RSpec.describe 'claims and appeals overview', type: :request do
+  include JsonSchemaMatchers
+
+  describe 'GET /v0/claims-and-appeals-overview' do
+    describe '#index (all user claims) is polled' do
+      before { iam_sign_in }
+
+      it 'and a result that matches our schema is successfully returned with the 200 status ' do
+        VCR.use_cassette('evss/claims/claims') do
+          VCR.use_cassette('caseflow/appeals') do
+            get '/mobile/v0/claims-and-appeals-overview', headers: iam_headers
+            expect(response).to have_http_status(:ok)
+            # check a couple entries to make sure the data is correct
+            parsed_response_contents = response.parsed_body.dig('data')
+            expect(parsed_response_contents[0].dig('type')).to eq('claim')
+            expect(parsed_response_contents.last.dig('type')).to eq('appeal')
+            expect(parsed_response_contents[0].dig('attributes', 'dateFiled')).to eq('2017-12-08')
+            expect(parsed_response_contents.last.dig('attributes', 'dateFiled')).to eq('2003-01-06')
+            expect(response.body).to match_json_schema('claims_and_appeals_overview_response')
+          end
+        end
+      end
+
+      it 'and invalid headers return a 401 status' do
+        VCR.use_cassette('evss/claims/claims') do
+          VCR.use_cassette('caseflow/appeals') do
+            get '/mobile/v0/claims-and-appeals-overview'
+            expect(response).to have_http_status(:unauthorized)
+            expect(response.body).to match_json_schema('evss_errors')
+          end
+        end
+      end
+    end
+
+    describe '#index is polled' do
+      before { iam_sign_in }
+
+      it 'and claims service fails, but appeals succeeds' do
+        VCR.use_cassette('evss/claims/claims_with_errors') do
+          VCR.use_cassette('caseflow/appeals') do
+            get '/mobile/v0/claims-and-appeals-overview', headers: iam_headers
+            expect(response).to have_http_status(:multi_status)
+            expect(response.parsed_body.dig('meta', 'errors').length).to eq(1)
+            expect(response.parsed_body.dig('meta', 'errors')[0]['service']).to eq('claims')
+            expect(response.body).to match_json_schema('claims_and_appeals_overview_response')
+          end
+        end
+      end
+
+      it 'and appeals service fails, but claims succeeds' do
+        VCR.use_cassette('evss/claims/claims') do
+          VCR.use_cassette('caseflow/server_error') do
+            get '/mobile/v0/claims-and-appeals-overview', headers: iam_headers
+            expect(response).to have_http_status(:multi_status)
+            expect(response.parsed_body.dig('meta', 'errors').length).to eq(1)
+            expect(response.parsed_body.dig('meta', 'errors')[0]['service']).to eq('appeals')
+            expect(response.body).to match_json_schema('claims_and_appeals_overview_response')
+          end
+        end
+      end
+
+      it 'both fail in upstream service' do
+        VCR.use_cassette('evss/claims/claims_with_errors') do
+          VCR.use_cassette('caseflow/server_error') do
+            get '/mobile/v0/claims-and-appeals-overview', headers: iam_headers
+            expect(response).to have_http_status(:bad_gateway)
+            expect(response.parsed_body.dig('meta', 'errors').length).to eq(2)
+            expect(response.parsed_body.dig('meta', 'errors')[0]['service']).to eq('claims')
+            expect(response.parsed_body.dig('meta', 'errors')[1]['service']).to eq('appeals')
+            expect(response.body).to match_json_schema('claims_and_appeals_overview_response')
+          end
+        end
+      end
+    end
+
+    describe '#index is polled without user sign in' do
+      it 'and not user returns a 500 status' do
+        VCR.use_cassette('evss/claims/claims') do
+          VCR.use_cassette('caseflow/appeals') do
+            get '/mobile/v0/claims-and-appeals-overview', headers: iam_headers
+            expect(response).to have_http_status(:internal_server_error)
+          end
+        end
+      end
+    end
+  end
+end
