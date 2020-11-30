b33825736da3bac1edd96e1d2b713cd3097c7e6b
Anna Carey <anna@adhocteam.us>
2020-11-30 15:48:57 -0500

    fix lint error

--- modules/appeals_api/db/migrate/20201127195258_add_status_to_notice_of_disagreements.rb
+++ modules/appeals_api/db/migrate/20201127195258_add_status_to_notice_of_disagreements.rb
@@ -2,6 +2,6 @@
 
 class AddStatusToNoticeOfDisagreements < ActiveRecord::Migration[6.0]
   def change
-    safety_assured { add_column :appeals_api_notice_of_disagreements, :status, :string, null: false, default: "pending"}
+    safety_assured { add_column :appeals_api_notice_of_disagreements, :status, :string, null: false, default: 'pending' }
   end
 end
