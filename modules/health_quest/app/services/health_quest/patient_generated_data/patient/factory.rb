fc98315ae377943b256cc796d7c873673b8a1a54
Dillo Raju <draju@governmentcio.com>
2020-11-30 10:06:39 -0500

    adding YARD doc to factory

--- modules/health_quest/app/services/health_quest/patient_generated_data/patient/factory.rb
+++ modules/health_quest/app/services/health_quest/patient_generated_data/patient/factory.rb
@@ -3,9 +3,24 @@
 module HealthQuest
   module PatientGeneratedData
     module Patient
+      ##
+      # A service object for isolating dependencies from any implementing service or controller.
+      #
+      # @!attribute session_service
+      #   @return [HealthQuest::SessionService]
+      # @!attribute user
+      #   @return [User]
+      # @!attribute map_query
+      #   @return [PatientGeneratedData::Patient::MapQuery]
       class Factory
         attr_reader :session_service, :user, :map_query
 
+        ##
+        # Builds a PatientGeneratedData::Patient::Factory instance from a given User
+        #
+        # @param user [User] the currently logged in user.
+        # @return [PatientGeneratedData::Patient::Factory] an instance of this class
+        #
         def self.manufacture(user)
           new(user)
         end
@@ -16,6 +31,11 @@ module HealthQuest
           @map_query = PatientGeneratedData::Patient::MapQuery.build(session_service.headers)
         end
 
+        ##
+        # Gets patient information from a user's ICN
+        #
+        # @return [FHIR::DSTU2::Patient::ClientReply] an instance of ClientReply
+        #
         def get_patient
           map_query.get(user.icn)
         end
