ae572eb8ffde720954c1cc3c954fa089db4ff1f5
Anna Carey <anna@adhocteam.us>
2020-11-30 09:20:12 -0500

    Only skip authentication for some attachment controllers (#5385)
    * users of form 526 should all be logged in
    
    therefore users of /v0/upload_supporting_evidence should be logged in as well
    
    * spec udpate
    
    * auth for file upload specs

--- app/controllers/v0/hca_attachments_controller.rb
+++ app/controllers/v0/hca_attachments_controller.rb
@@ -4,6 +4,8 @@ module V0
   class HCAAttachmentsController < ApplicationController
     include FormAttachmentCreate
 
+    skip_before_action(:authenticate, raise: false)
+
     FORM_ATTACHMENT_MODEL = HCAAttachment
   end
 end
