ec8420bc9ed6c0eb65dc2ec8ac9976f5e641df12
Laura <30485143+ElleeB@users.noreply.github.com>
2020-11-30 10:10:06 -0500

    Add status to notice of disagreement migration (#5401)

---
+++ modules/appeals_api/db/migrate/20201127195258_add_status_to_notice_of_disagreements.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddStatusToNoticeOfDisagreements < ActiveRecord::Migration[6.0]
+  def change
+    safety_assured { add_column :appeals_api_notice_of_disagreements, :status, :string, null: false, default: "pending"}
+  end
+end
