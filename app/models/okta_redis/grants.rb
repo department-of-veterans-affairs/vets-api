774bf4fa4e5d8b276ac60a00e71d0f04660f5867
Philip Becker <philip.becker@oddball.io>
2020-12-01 13:38:35 -0500

    Revert "add some sentry logging for activity around okta service calls (#5408)" (#5413)
    This reverts commit 0e898bf70dbf4733823af695c5f4faf99b0d8ede. it added some sentry logging to do some discovery.

--- app/models/okta_redis/grants.rb
+++ app/models/okta_redis/grants.rb
@@ -8,36 +8,23 @@ module OktaRedis
   class Grants < Model
     CLASS_NAME = 'GrantsService'
 
-    after_initialize do |grants|
-      grants.instance_variable_set(:@activity_log, [])
-    end
-
     def all
-      all_grants = okta_response.body
-      log_message_to_sentry('Getting all grants', :info,
-                            grants_service_activity: @activity_log)
-      all_grants
+      okta_response.body
     end
 
     def delete_grants(grant_ids)
       success = grant_ids.reduce(true) { |memo, grant| delete_grant(grant) && memo }
       OktaRedis::Grants.delete(cache_key) if success
-      log_message_to_sentry('Deleting all grants', :info,
-                            grants_service_activity: @activity_log)
       success
     end
 
     def delete_grant(grant_id)
       delete_response = service.delete_grant(@user.okta_profile.id, grant_id)
-      @activity_log << "Deleting grant id #{grant_id}.  Success? #{delete_response.success?}"
       if delete_response.success?
         true
       else
         log_message_to_sentry("Error deleting grant #{grant_id}", :error,
-                              {
-                                body: delete_response.body,
-                                grants_service_activity: @activity_log
-                              })
+                              body: delete_response.body)
         raise 'Unable to delete grant'
       end
     end
@@ -45,14 +32,10 @@ module OktaRedis
     private
 
     def okta_response
-      @activity_log << 'Get okta response including grants'
-      response = do_cached_with(key: cache_key) do
-        @activity_log << 'Fetch new response'
+      do_cached_with(key: cache_key) do
         grants_response = service.grants(@user.okta_profile.id)
         grants_response.success? ? grants_response : []
       end
-      @activity_log << "Retrieved #{response.body.count} grants"
-      response
     end
   end
 end
