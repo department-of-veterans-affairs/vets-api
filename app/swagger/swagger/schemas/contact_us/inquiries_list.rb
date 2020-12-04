27295d3c49bb4851c5fce18fdfbc77db2eae6c1f
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-12-04 12:04:52 -0500

    [department-of-veterans-affairs/orchid#190] Adds inquiry properties to swagger schema for GET ask/inquiries
    Co-authored-by: Paul Phillips <paul.phillips@thoughtworks.com

--- app/swagger/swagger/schemas/contact_us/inquiries_list.rb
+++ app/swagger/swagger/schemas/contact_us/inquiries_list.rb
@@ -7,8 +7,23 @@ module Swagger
         include Swagger::Blocks
 
         swagger_schema :InquiriesList do
-          property :inquiries, type: :object do
-            property :id, type: :string, example: nil
+          property :inquiries, type: :array do
+            items do
+              key :'$ref', :InquirySummary
+            end
+          end
+        end
+
+        swagger_schema :InquirySummary do
+          property :subject, type: :string, example: 'Prosthetics'
+          property :confirmationNumber, type: :string, example: '000-010'
+          property :status, type: :string, example: 'OPEN'
+          property :creationTimestamp, type: :string, example: '2020-11-01T14:58:00+01:00'
+          property :lastActiveTimestamp, type: :string, example: '2020-11-04T13:00:00+01:00'
+          property :links, type: :object do
+            property :thread, type: :object do
+              property :href, type: :string
+            end
           end
         end
       end
