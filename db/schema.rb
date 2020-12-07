a24f7fd93d551ee53c454b735bd151ddcbed3c58
Erik P. Hansen <eph@erikphansen.com>
2020-12-07 07:34:40 -0800

    add ch33_dd_profile feature flag

--- db/schema.rb
+++ db/schema.rb
@@ -289,6 +289,15 @@ ActiveRecord::Schema.define(version: 2020_12_04_151120) do
     t.index ["job_id"], name: "index_form526_job_statuses_on_job_id", unique: true
   end
 
+  create_table "form526_opt_ins", id: :serial, force: :cascade do |t|
+    t.string "user_uuid", null: false
+    t.string "encrypted_email", null: false
+    t.string "encrypted_email_iv", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_uuid"], name: "index_form526_opt_ins_on_user_uuid", unique: true
+  end
+
   create_table "form526_submissions", id: :serial, force: :cascade do |t|
     t.string "user_uuid", null: false
     t.integer "saved_claim_id", null: false
