00d28d0a8c95c0cb31a57d5e46d4c2d7eb04e684
Jayson Perkins <jaysonperkins51@gmail.com>
2020-12-07 13:00:37 -0500

    attempts to fix claim creation

--- modules/mobile/spec/request/individual_claim_request_spec.rb
+++ modules/mobile/spec/request/individual_claim_request_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe 'individual claim', type: :request do
     before { iam_sign_in }
     let!(:claim) do
       FactoryBot.create(:evss_claim, id: 1, evss_id: 600_117_255,
-                        user_uuid: '1234')
+                        user_uuid: '3097e489-ad75-5746-ab1a-e0aabc1b426a')
     end
 
-    it 'and a result that matches our schema is successfully returned with the 200 status ' do
-      VCR.use_cassette('evss/claims/claim_with_docs') do
+    it 'and a result that matches our schema is successfully returned with the 200 status ', run_at: 'Wed, 13 Dec 2017 03:28:23 GMT' do
+      VCR.use_cassette('evss/claims/claim', match_requests_on: %i[uri method body]) do
         get '/mobile/v0/claim/600117255', headers: iam_headers
         expect(response).to have_http_status(:ok)
       end
