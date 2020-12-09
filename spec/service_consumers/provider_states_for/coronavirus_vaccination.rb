37ddc450214aa04fe099f43a065607806eb98c1b
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-09 15:23:16 -0500

    remove comments

--- spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
+++ spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
@@ -14,14 +14,4 @@ Pact.provider_states_for 'Coronavirus Vaccination' do
   provider_state 'unauthenticated user application data' do
     no_op
   end
-
-  # provider_state 'template' do
-  #   set_up do
-  #   end
-
-  #   tear_down do
-  #   end
-
-	# # no_op - define no_op if a provider state is not necessary
-  # end
 end
