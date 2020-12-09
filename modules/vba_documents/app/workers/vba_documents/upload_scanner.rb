4b6f72a5e87535bdfdb9aed39a113380b613e2d2
Greg Bowman <gregory.bowman@libertyits.com>
2020-12-09 10:22:58 -0500

    checkpoint - commenting out async calls for GCIO transition to see what tests fall out with the deployment

--- modules/vba_documents/app/workers/vba_documents/upload_scanner.rb
+++ modules/vba_documents/app/workers/vba_documents/upload_scanner.rb
@@ -21,24 +21,8 @@ module VBADocuments
       Rails.logger.info('VBADocuments: Processing: ' + upload.inspect)
       object = bucket.object(upload.guid)
       return false unless object.exists?
-
-      # this block is valid while transitioning from GDIT to GCIO
-      gdit_pause_date = Time.at(1607720400) # 12/11/2020 4:00:00 pm est this can be anything if we push early
-      gcio_start_date = Time.at(1607972400) # 12/14/2020 2:00:00 pm est
-      pause_status_update = false
-
-      if upload.created_at > gcio_start_date
-        # hr = VBADocuments::UploadSubmission.first.created_at.strftime('%H').to_i
-        # VBADocuments::UploadSubmission.first.created_at.in_time_zone("Eastern Time (US & Canada)").strftime('%H').to_i
-        hr = upload.created_at.in_time_zone("Eastern Time (US & Canada)").strftime('%H').to_i
-        pause_status_update = (hr >= 13 && hr < 15)
-      end
-
-      # remove unless statement when GCIO is live on 12/14 or 12/15 deployment
-      upload.update(status: 'uploaded') unless pause_status_update
-
-      # remove unless statment with GCIO live deployment
-      VBADocuments::UploadProcessor.perform_async(upload.guid) unless Time.now.utc.to_i > gdit_pause_date
+      upload.update(status: 'uploaded')
+      # VBADocuments::UploadProcessor.perform_async(upload.guid)
       true
     end
 
