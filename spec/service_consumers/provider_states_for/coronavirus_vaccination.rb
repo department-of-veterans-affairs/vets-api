2f35211183d4902fc5110eda62fcc560a57fe8e1
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-09 15:22:39 -0500

    add authenticated user to setup in 'authenticated user application data' state

--- spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
+++ spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
-Pact.provider_states_for 'CoronavirusVaccination' do
+Pact.provider_states_for 'Coronavirus Vaccination' do
 
-  # provider_state 'authenticated user application data' do
+  provider_state 'authenticated user application data' do
+    set_up do
+      build_user_and_stub_session
+    end
+
+    tear_down do
+    end
+  end
+
+  provider_state 'unauthenticated user application data' do
+    no_op
+  end
+
+  # provider_state 'template' do
   #   set_up do
   #   end
 
@@ -11,15 +24,4 @@ Pact.provider_states_for 'CoronavirusVaccination' do
 
 	# # no_op - define no_op if a provider state is not necessary
   # end
-
-  provider_state 'unauthenticated user application data' do
-    no_op
-    # set_up do
-    # end
-
-    # tear_down do
-    # end
-
-	# no_op - define no_op if a provider state is not necessary
-  end
 end
