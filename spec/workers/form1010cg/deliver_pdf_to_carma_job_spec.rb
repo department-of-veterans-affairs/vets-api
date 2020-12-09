97698b4740fb3c15fd11c20f11bb6bb334498e68
Kevin Mircovich <kmircovich1@gmail.com>
2020-12-09 14:56:42 -0500

    refactor attachment job spec for better -f d and add test case for failed pdf generation

--- spec/workers/form1010cg/deliver_pdf_to_carma_job_spec.rb
+++ spec/workers/form1010cg/deliver_pdf_to_carma_job_spec.rb
@@ -28,19 +28,46 @@ RSpec.describe Form1010cg::DeliverPdfToCARMAJob do
       end
     end
 
-    it 'raises error when submission is not found' do
-      expect { subject.perform(SecureRandom.uuid) }.to raise_error(ActiveRecord::RecordNotFound) do |error|
-        expect(error.message).to include('Couldn\'t find Form1010cg::Submission')
+    context 'when submission is not found' do
+      it 'raises error' do
+        expect { subject.perform(SecureRandom.uuid) }.to raise_error(ActiveRecord::RecordNotFound) do |error|
+          expect(error.message).to include('Couldn\'t find Form1010cg::Submission')
+        end
       end
     end
 
-    it 'raises error when claim is not found' do
-      claim.delete
+    context 'when claim is not found' do
+      let(:expected_exception_class) { described_class::MissingClaimException }
 
-      expected_exception_class = described_class::MissingClaimException
+      before do
+        claim.delete # delete the claim but not the submission
+      end
 
-      expect { subject.perform(submission.claim_guid) }.to raise_error(expected_exception_class) do |error|
-        expect(error.message).to eq('Could not find a claim associated to this submission')
+      it 'raises error' do
+        expect { subject.perform(claim_guid) }.to raise_error(expected_exception_class) do |error|
+          expect(error.message).to eq('Could not find a claim associated to this submission')
+        end
+      end
+    end
+
+    context 'when PDF generation fails' do
+      let(:pdf_generation_exception) do
+        class MyPdfError < StandardError; end
+        MyPdfError.new('PDF could not be generated')
+      end
+
+      before do
+        expect_any_instance_of(SavedClaim::CaregiversAssistanceClaim).to receive(
+          :to_pdf
+        ).and_raise(
+          pdf_generation_exception
+        )
+      end
+
+      it 'raises error' do
+        expect { subject.perform(claim_guid) }.to raise_error(pdf_generation_exception.class) do |error|
+          expect(error.message).to eq(pdf_generation_exception.message)
+        end
       end
     end
 
