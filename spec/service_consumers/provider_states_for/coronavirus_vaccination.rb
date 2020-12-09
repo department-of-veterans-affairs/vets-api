b6ae0eff1caabc40f5b5ce97385a5ff17921e86e
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-09 12:42:28 -0500

    add no_op to 'unauthenticated user application data' test

--- spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
+++ spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
@@ -2,22 +2,23 @@
 
 Pact.provider_states_for 'CoronavirusVaccination' do
 
-  provider_state 'authenticated user application data' do
-    set_up do
-    end
+  # provider_state 'authenticated user application data' do
+  #   set_up do
+  #   end
 
-    tear_down do
-    end
+  #   tear_down do
+  #   end
 
-	# no_op - define no_op if a provider state is not necessary
-  end
+	# # no_op - define no_op if a provider state is not necessary
+  # end
 
   provider_state 'unauthenticated user application data' do
-    set_up do
-    end
+    no_op
+    # set_up do
+    # end
 
-    tear_down do
-    end
+    # tear_down do
+    # end
 
 	# no_op - define no_op if a provider state is not necessary
   end
