eedc72fe22e3dc793cf8e317505b04e84b1d2429
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-09 16:44:33 -0500

    fixed linting issue - Extra empty line detected at block body beginning.

--- spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
+++ spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Pact.provider_states_for 'Coronavirus Vaccination' do
-
   provider_state 'authenticated user application data' do
     set_up do
       build_user_and_stub_session
