60ab24bbe42072e24d82af9923ca322e186065d6
Jack Schuss <jackschuss@gmail.com>
2020-12-10 16:27:08 -0500

    spacing
    Add Schema Definition for nodCreateInput
    
    Add header information and schema reference
    
    WIP Response schemas/formats
    
    Add swagger documentation for notice_of_disagreements/schema endpoint
    
    update ErrorModel schema for standard JSON error structure
    
    Update schema 500 error to utilize errorModel
    
    Make quotes standard across the swagger file
    
    Add createResponse and contestableIssue schema
    
    incomplete errors, but structure has been defined

--- modules/appeals_api/app/controllers/appeals_api/docs/v1/docs_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/docs/v1/docs_controller.rb
@@ -6,6 +6,7 @@ class AppealsApi::Docs::V1::DocsController < ApplicationController
   SWAGGERED_CLASSES = [
     AppealsApi::V1::HigherLevelReviewsControllerSwagger,
     AppealsApi::V1::NoticeOfDisagreementsControllerSwagger,
+    AppealsApi::V1::Schemas::Definitions,
     AppealsApi::V1::SwaggerRoot
   ].freeze
 
