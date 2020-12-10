25af8bf98217e35b1db290683c6df0734c8be3aa
lihan <frankieteardrop@gmail.com>
2020-12-09 23:19:26 -0800

    add preferred facility migrations (#5431)

--- db/schema.rb
+++ db/schema.rb
@@ -476,6 +476,14 @@ ActiveRecord::Schema.define(version: 2020_12_08_215916) do
     t.index ["code"], name: "index_preferences_on_code", unique: true
   end
 
+  create_table "preferred_facilities", force: :cascade do |t|
+    t.string "facility_code", null: false
+    t.integer "account_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["facility_code", "account_id"], name: "index_preferred_facilities_on_facility_code_and_account_id", unique: true
+  end
+
   create_table "preneed_submissions", id: :serial, force: :cascade do |t|
     t.string "tracking_number", null: false
     t.string "application_uuid"
