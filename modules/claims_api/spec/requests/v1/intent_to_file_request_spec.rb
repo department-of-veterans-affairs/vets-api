c1a31ea0829005c192d707f9abeb974f54a3224b
Lee DeBoom <lee.deboom@oddball.io>
2020-12-10 09:31:20 -0600

    attempt 1

--- modules/claims_api/spec/requests/v1/intent_to_file_request_spec.rb
+++ modules/claims_api/spec/requests/v1/intent_to_file_request_spec.rb
@@ -69,9 +69,10 @@ RSpec.describe 'Intent to file', type: :request do
     end
 
     it 'posts a 403 when a representative who doesn\'t have an MPI account tries to post type "burial"' do
+      expect_any_instance_of(OpenidUser).to receive(:participant_id).and_raise(ArgumentError.new('whatever'))
+
       with_okta_user(scopes) do |auth_header|
         VCR.use_cassette('bgs/intent_to_file_web_service/insert_intent_to_file') do
-          expect_any_instance_of(OpenidUser).to receive(:participant_id).and_raise(ArgumentError.new('whatever'))
           data[:data][:attributes] = { type: 'burial' }
           post path, params: data.to_json, headers: headers.merge(auth_header)
           p '!!!!!!!!!!!!!!!!!!!'
