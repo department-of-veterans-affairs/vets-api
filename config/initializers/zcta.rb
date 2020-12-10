8dbf3df646274ef8647cd54339cb553f0f835211
Patrick Vinograd <patrick@adhocteam.us>
2020-12-10 14:04:02 -0800

    Rubocop on initializer

--- config/initializers/zcta.rb
+++ config/initializers/zcta.rb
@@ -2,7 +2,7 @@
 
 require 'csv'
 
-ZCTA = CSV.read(Rails.root.join('lib', 'facilities', '2019_Gaz_zcta_national.tsv'), 
-                col_sep: "\t", headers: true).to_h { |row| [row[0], row] };
+ZCTA = CSV.read(Rails.root.join('lib', 'facilities', '2019_Gaz_zcta_national.tsv'),
+                col_sep: "\t", headers: true).to_h { |row| [row[0], row] }
 ZCTA_LAT_HEADER = 'INTPTLAT'
 ZCTA_LON_HEADER = 'INTPTLONG'
