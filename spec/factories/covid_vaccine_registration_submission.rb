bf64cc88aec27bab3ea4b187c53e20fad8968bb0
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-11 10:21:22 -0500

    update form_data

--- spec/factories/covid_vaccine_registration_submission.rb
+++ spec/factories/covid_vaccine_registration_submission.rb
@@ -4,17 +4,28 @@ FactoryBot.define do
   factory :covid_vaccine_registration_submission, class: CovidVaccine::V0::RegistrationSubmission do
     form_data do
       {
-        isIdentityVerified: { true }
-        firstName: { nil }
-        lastName: { nil }
-        birthDate: { nil }
-        ssn: { nil }
-        email: { nil }
-        phone: "(111) 222-3333"
-        zipCode: { nil }
-        zipCodeDetails: { "Yes" }
-        vaccineInterest: { "INTERESTED" }
+        "isIdentityVerified": true,
+        "firstName": "Jane",
+        "lastName": "Doe",
+        "birthDate": "1965-01-01",
+        "ssn": "419545182",
+        "email": "jane.doe@va.gov",
+        "phone": "(111) 222-3333",
+        "zipCode": "60601",
+        "zipCodeDetails": "Yes",
+        "vaccineInterest": "INTERESTED",
       }.to_json
+
+      # isIdentityVerified { true }
+      # firstName { 'Jane' }
+      # lastName { 'Doe' }
+      # birthDate { '1965-01-01' }
+      # ssn { '419545182' }
+      # email { 'jane.doe@va.gov' }
+      # phone "(111) 222-3333"
+      # zipCode { '60601' }
+      # zipCodeDetails { "Yes" }
+      # vaccineInterest { "INTERESTED" }
     end
   end
 end
