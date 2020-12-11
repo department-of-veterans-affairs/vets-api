6549b541db378fd7ff4ce9086720b00074577079
Kevin Mircovich <kmircovich1@gmail.com>
2020-12-11 10:02:55 -0500

    Associate CaregiversAssistanceClaims to Form1010cg::Submissions (#5447)
    * Create migrations for form1010cg_submissions table
    
    * Relate Form1010cg::Submission to SavedClaim::CaregiversAssistanceClaim. Update references to #submitted_at, #attachments, and #metadata
    
    * Switch PK and FK on caregivers_assistance_claim's has_one relationship
    
    * accepts_nested_attributes_for :submission on CaregiversAssistanceClaim

--- spec/models/form1010cg/submission_spec.rb
+++ spec/models/form1010cg/submission_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Form1010cg::Submission, type: :model do
   let(:sample_data) do
     {
       carma_case_id: 'aB9r00000004GW9CAK',
-      submitted_at: DateTime.now.iso8601,
+      accepted_at: DateTime.now.iso8601,
       attachments: {
         has_errors: false,
         data: [
@@ -39,34 +39,34 @@ RSpec.describe Form1010cg::Submission, type: :model do
     }
   end
 
-  it 'can initialize with attributes :carma_case_id, :submitted_at, :attachments, :metadata' do
+  it 'can initialize with attributes :carma_case_id, :accepted_at, :attachments, :metadata' do
     instance_1 = described_class.new
     expect(instance_1.carma_case_id).to eq(nil)
-    expect(instance_1.submitted_at).to eq(nil)
-    expect(instance_1.attachments).to eq({})
+    expect(instance_1.accepted_at).to eq(nil)
+    expect(instance_1.attachments).to eq(nil)
     expect(instance_1.metadata).to eq(nil)
 
     instance_2 = described_class.new(sample_data)
     expect(instance_2.carma_case_id).to eq(sample_data[:carma_case_id])
-    expect(instance_2.submitted_at).to eq(sample_data[:submitted_at])
-    expect(instance_2.attachments).to eq(sample_data[:attachments])
-    expect(instance_2.metadata).to eq(sample_data[:metadata])
+    expect(instance_2.accepted_at).to eq(sample_data[:accepted_at])
+    expect(instance_2.attachments).to eq(sample_data[:attachments].deep_stringify_keys)
+    expect(instance_2.metadata).to eq(sample_data[:metadata].deep_stringify_keys)
   end
 
-  it 'has attribute accessors for :carma_case_id, :submitted_at, :attachments, :metadata' do
+  it 'has attribute accessors for :carma_case_id, :accepted_at, :attachments, :metadata' do
     expect(subject.carma_case_id).to eq(nil)
-    expect(subject.submitted_at).to eq(nil)
-    expect(subject.attachments).to eq({})
+    expect(subject.accepted_at).to eq(nil)
+    expect(subject.attachments).to eq(nil)
     expect(subject.metadata).to eq(nil)
 
     subject.carma_case_id = sample_data[:carma_case_id]
-    subject.submitted_at = sample_data[:submitted_at]
+    subject.accepted_at = sample_data[:accepted_at]
     subject.attachments = sample_data[:attachments]
     subject.metadata = sample_data[:metadata]
 
     expect(subject.carma_case_id).to eq(sample_data[:carma_case_id])
-    expect(subject.submitted_at).to eq(sample_data[:submitted_at])
-    expect(subject.attachments).to eq(sample_data[:attachments])
-    expect(subject.metadata).to eq(sample_data[:metadata])
+    expect(subject.accepted_at).to eq(sample_data[:accepted_at])
+    expect(subject.attachments).to eq(sample_data[:attachments].deep_stringify_keys)
+    expect(subject.metadata).to eq(sample_data[:metadata].deep_stringify_keys)
   end
 end
