45ccbb3446652f9568cfe0330e749ea3ce47b4f9
Patrick Vinograd <patrick@adhocteam.us>
2020-12-11 07:57:21 -0800

    Fix up rebase

--- modules/covid_vaccine/config/initializers/statsd.rb
+++ modules/covid_vaccine/config/initializers/statsd.rb
@@ -11,7 +11,7 @@ CovidVaccine::V0::RegistrationService.extend StatsD::Instrument
 CovidVaccine::V0::RegistrationService.statsd_measure :facility_attributes,
                                                      'covid_vaccine.facility_query.measure'
 CovidVaccine::V0::RegistrationService.statsd_count_success :facility_attributes,
-                                                           'covid_vaccine.facility_query' &:present?
+                                                           'covid_vaccine.facility_query', &:present?
 CovidVaccine::V0::RegistrationService.statsd_measure :attributes_from_mpi,
                                                      'covid_vaccine.mpi_query.measure'
 CovidVaccine::V0::RegistrationService.statsd_measure :submit,
