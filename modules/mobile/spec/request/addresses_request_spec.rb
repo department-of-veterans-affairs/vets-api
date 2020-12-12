5003aab40e4cab166ca577774e6e7a880c0e5cf2
Alastair Dawson <alastair@adhocteam.us>
2020-12-11 16:12:55 -0800

    16834 add POST endpoints for mobile profile updates and switch to sync service (#5463)

--- modules/mobile/spec/request/addresses_request_spec.rb
+++ modules/mobile/spec/request/addresses_request_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe 'address', type: :request do
   before { iam_sign_in(user) }
 
   let(:user) { FactoryBot.build(:iam_user) }
-  let(:json_body_headers) { { 'Content-Type' => 'application/json', 'Accept' => 'application/json' } }
   let(:address) { build(:vet360_address, vet360_id: user.vet360_id) }
+  let(:json_body_headers) { { 'Content-Type' => 'application/json', 'Accept' => 'application/json' } }
 
   describe 'update endpoints' do
     before(:all) do
@@ -20,6 +20,85 @@ RSpec.describe 'address', type: :request do
     end
 
     after(:all) { VCR.configure { |c| c.cassette_library_dir = @original_cassette_dir } }
+
+    describe 'POST /mobile/v0/user/addresses' do
+      context 'with a valid address that takes two tries to complete' do
+        before do
+          VCR.use_cassette('profile/get_address_status_complete') do
+            VCR.use_cassette('profile/get_address_status_incomplete') do
+              VCR.use_cassette('profile/post_address_initial') do
+                post '/mobile/v0/user/addresses', params: address.to_json, headers: iam_headers(json_body_headers)
+              end
+            end
+          end
+        end
+
+        it 'returns a 200' do
+          expect(response).to have_http_status(:ok)
+        end
+
+        it 'matches the expected schema' do
+          expect(response.body).to match_json_schema('profile_update_response')
+        end
+
+        it 'includes a transaction id' do
+          id = JSON.parse(response.body).dig('data', 'attributes', 'transactionId')
+          expect(id).to eq('1f450c8e-4bb2-4f5d-a5f3-0d907941625a')
+        end
+      end
+
+      context 'when it has not completed within the timeout window (< 60s)' do
+        before do
+          allow_any_instance_of(Mobile::V0::Profile::SyncUpdateService)
+            .to receive(:seconds_elapsed_since).and_return(61)
+
+          VCR.use_cassette('profile/get_address_status_complete') do
+            VCR.use_cassette('profile/get_address_status_incomplete_2') do
+              VCR.use_cassette('profile/get_address_status_incomplete') do
+                VCR.use_cassette('profile/post_address_initial') do
+                  post '/mobile/v0/user/addresses', params: address.to_json, headers: iam_headers(json_body_headers)
+                end
+              end
+            end
+          end
+        end
+
+        it 'returns a gateway timeout error' do
+          expect(response).to have_http_status(:gateway_timeout)
+        end
+      end
+
+      context 'with missing address params' do
+        before do
+          address.address_line1 = ''
+
+          post('/mobile/v0/user/addresses', params: address.to_json, headers: iam_headers(json_body_headers))
+        end
+
+        it 'returns a 422' do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'matches the error schema' do
+          expect(response.body).to match_json_schema('errors')
+        end
+
+        it 'has a helpful error message' do
+          message = response.parsed_body['errors'].first
+          expect(message).to eq(
+            {
+              'title' => "Address line1 can't be blank",
+              'detail' => "address-line1 - can't be blank",
+              'code' => '100',
+              'source' => {
+                'pointer' => 'data/attributes/address-line1'
+              },
+              'status' => '422'
+            }
+          )
+        end
+      end
+    end
 
     describe 'PUT /mobile/v0/user/addresses' do
       context 'with a valid address that takes two tries to complete' do
@@ -195,6 +274,7 @@ RSpec.describe 'address', type: :request do
             'internationalPostalCode' => nil,
             'province' => nil,
             'stateCode' => 'NY',
+            'validationKey' => nil,
             'zipCode' => '11249',
             'zipCodeSuffix' => '3939'
           }
@@ -206,8 +286,7 @@ RSpec.describe 'address', type: :request do
           {
             'confidenceScore' => 100.0,
             'addressType' => 'Domestic',
-            'deliveryPointValidation' => 'UNDELIVERABLE',
-            'validationKey' => -646_932_106
+            'deliveryPointValidation' => 'UNDELIVERABLE'
           }
         )
       end
