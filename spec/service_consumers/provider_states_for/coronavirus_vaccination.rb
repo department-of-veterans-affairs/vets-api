73ae3e853e2b8b68378583daf1245ac1f6e096b7
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-14 11:06:23 -0500

    update provider state names, add missing calls to set_up and tear_down in 'retrieves previously saved submission data for user' test

--- spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
+++ spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 
 Pact.provider_states_for 'Coronavirus Vaccination' do
-  provider_state 'retreives saved registration for authenticated user' do
-    user = build_user_and_stub_session
-    submission = CovidVaccine::V0::RegistrationSubmission.for_user(user).last
-    submission || FactoryBot.create(:covid_vaccine_registration_submission)
+  provider_state 'retrieves previously saved submission data for user' do
+    set_up do
+      user = build_user_and_stub_session
+      submission = CovidVaccine::V0::RegistrationSubmission.for_user(user).last
+      submission || FactoryBot.create(:covid_vaccine_registration_submission)
+    end
+
+    tear_down do
+    end
+  end
+
+  provider_state 'does not retrieve perviously saved submission data for user becase it does not exist' do
+    set_up do
+      user = build_user_and_stub_session
+      CovidVaccine::V0::RegistrationSubmission.for_user(user).last
+    end
+
+    tear_down do
+    end
   end
 
   provider_state 'authenticated user sumbits registration data' do
@@ -18,14 +33,5 @@ Pact.provider_states_for 'Coronavirus Vaccination' do
 
   provider_state 'unauthenticated user sumbits registration data' do
     no_op
-  end
-
-  provider_state 'authenticated user updates registration data' do
-    set_up do
-      build_user_and_stub_session
-    end
-
-    tear_down do
-    end
   end
 end
