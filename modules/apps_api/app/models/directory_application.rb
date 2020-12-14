b66010110b20beac2afe6fdb2e4801dfddfbc738
Braden Shipley <bradenrshipley@gmail.com>
2020-12-14 09:25:01 -0600

    Added #create and #update, modified routes.rb
    changed model (WIP)

--- modules/apps_api/app/models/directory_application.rb
+++ modules/apps_api/app/models/directory_application.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class DirectoryApplication < ApplicationRecord
-  validates :name, :logo_url, :app_type, :service_categories, :platforms,
+  # TODO: :BRADEN:
+  # Creating a migration for this causes a weird issue. Debug on monday!
+  validates :name, uniqueness: true
+  validates :logo_url, :app_type, :service_categories, :platforms,
             :app_url, :description, :privacy_url, :tos_url, presence: true
 end
