42fd555697b568ef197c63ec428d1ec5a99c2171
Kathleen Crawford <kcrawford@governmentcio.com>
2020-12-14 13:38:39 -0800

    fixing datetime issue

--- app/models/bgs_dependents/divorce.rb
+++ app/models/bgs_dependents/divorce.rb
@@ -12,7 +12,7 @@ module BGSDependents
         divorce_city: @divorce_info.dig('location', 'city'),
         divorce_country: @divorce_info.dig('location', 'country'),
         marriage_termination_type_code: @divorce_info['reason_marriage_ended'],
-        end_date: format_date(@divorce_info['date']),
+        end_date: @divorce_info['date'],
         vet_ind: 'N',
         ssn: @divorce_info['ssn'],
         birth_date: @divorce_info['birth_date'],
