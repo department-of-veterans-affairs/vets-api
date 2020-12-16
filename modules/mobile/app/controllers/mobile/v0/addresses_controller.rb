3bba00022406a7b5c4960e5d6ba1af487e6a0af7
Alastair Dawson <alastair@adhocteam.us>
2020-12-16 15:31:08 -0800

    17086 mobile address validation fix (#5536)
    * move validation key to meta data
    
    * add address POU back in validation response

--- modules/mobile/app/controllers/mobile/v0/addresses_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/addresses_controller.rb
@@ -28,7 +28,9 @@ module Mobile
         suggested_addresses = response.dig('response', 'addresses').map do |a|
           OpenStruct.new(a['address'].merge(
                            'id' => SecureRandom.uuid,
-                           'meta' => a['address_meta_data']
+                           'address_pou' => address_params[:address_pou],
+                           'validation_key' => response['response']['validation_key'],
+                           'address_meta' => a['address_meta_data']
                          ))
         end
 
