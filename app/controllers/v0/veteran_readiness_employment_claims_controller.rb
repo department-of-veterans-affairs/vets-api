1b7a492f991e2655d8095025488990fce1106170
Derek Dyer <derek@digitalderek.com>
2020-12-21 15:23:11 -0500

    Add back line

--- app/controllers/v0/veteran_readiness_employment_claims_controller.rb
+++ app/controllers/v0/veteran_readiness_employment_claims_controller.rb
@@ -12,6 +12,7 @@ module V0
         Raven.tags_context(team: 'vfs-ebenefits') # tag sentry logs with team name
         raise Common::Exceptions::ValidationErrors, claim
       end
+
       Rails.logger.info "ClaimID=#{claim.confirmation_number} Form=#{claim.class::FORM}"
       claim.send_to_vre(current_user)
       render json: claim
