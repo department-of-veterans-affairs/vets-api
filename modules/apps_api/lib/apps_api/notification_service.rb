3b8e6df27636effdbbf470995e5716f45b319605
Braden Shipley <bradenrshipley@gmail.com>
2020-12-28 16:51:54 -0600

    Baseline tests for get_events method and vcr cassettes for logs.
      send_email functionality not yet implemented. Template needs to be
      created by VANotify before manual testing can begin.

--- modules/apps_api/lib/apps_api/notification_service.rb
+++ modules/apps_api/lib/apps_api/notification_service.rb
@@ -6,22 +6,24 @@ require 'okta/service'
 module AppsApi
   class NotificationService < Common::Client::Base
     def initialize
-      @notify_client = VaNotify::Service.new
       @okta_service = Okta::Service.new
+      # should match the timing of the AppsApi::FetchConnections job
       @time_period = 60.minutes.ago.utc.iso8601
+      @connection_template = Settings.vanotify.template_id.connection_template
+      @disconnection_template = Settings.vanotify.template_id.disconnection_template
     end
 
     def handle_connect
       logs = get_events('app.oauth2.as.consent.grant')
       logs.body.each do |event|
-        handle_notification(event, Settings.vanotify.template_id.connection_template)
+        handle_notification(event, @connection_template)
       end
     end
 
     def handle_disconnect
       logs = get_events('app.oauth2.as.token.revoke')
       logs.body.each do |event|
-        handle_notification(event, Settings.vanotify.template_id.disconnection_template)
+        handle_notification(event, @disconnection_template)
       end
     end
 
@@ -35,23 +37,31 @@ module AppsApi
       app_id = event['target'].first['detailEntry']['publicclientapp']
       user = @okta_service.user(user_id)
       okta_app = @okta_service.app(app_id)
-      puts okta_app.body['label']
       app_record = DirectoryApplication.find_by(name: okta_app.body['label'])
-      unless app_record.blank?
-        #personalisation_hash = {
-          #'full_name' => event['actor']['displayName'],
-          #'application' => okta_app.body['label'],
-          #'time' => event['published'],
-          #'privacy_policy' => app_record['privacy_url'],
-          #'password_reset' => Settings.vanotify.links.password_reset,
-          #'connected_applications_link' => Settings.vanotify.links.connected_applications
-        #}
-        #@notify_client.send_email(
-          #email_address: user.body['profile']['email'],
-          #template_id: template,
-          #personalisation: personalisation_hash
-        #)
+      if app_record.present?
+        personalisation_hash = {
+          'full_name' => event['actor']['displayName'],
+          'application' => okta_app.body['label'],
+          'time' => event['published'],
+          'privacy_policy' => app_record['privacy_url'],
+          'password_reset' => Settings.vanotify.links.password_reset,
+          'connected_applications_link' => Settings.vanotify.links.connected_applications
+        }
+        send_email(
+          options: personalisation_hash,
+          email: user.body['profile']['email'],
+          template: template
+        )
       end
+    end
+
+    def send_email(options:, email:, template:)
+      # @notify_client = VaNotify::Service.new
+      # @notify_client.send_email(
+      # email_address: user.body['profile']['email'],
+      # template_id: template,
+      # personalisation: personalisation_hash
+      # )
     end
   end
 end
--- modules/apps_api/spec/lib/notification_service_spec.rb
+++ modules/apps_api/spec/lib/notification_service_spec.rb
@@ -1,22 +1,27 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 require 'apps_api/notification_service'
 
 describe AppsApi::NotificationService do
-  subject {descibed_class.new}
-
-  describe 'handle_connect' do
-    VCR.use_cassette('okta/connection_logs') do
-
-    end
-  end
-
-  describe 'handle_disconnect' do
-    VCR.use_cassette('okta/disconnection_logs') do
-
-    end
-  end
+  subject { AppsApi::NotificationService.new }
 
   describe 'get_events' do
+    it 'returns a response body of connections' do
+      VCR.use_cassette('okta/connection_logs', match_requests_on: %i[method host]) do
+        # to ensure our vcr has data in the response
+        subject.instance_variable_set(:@time_period, 5.days.ago.utc.iso8601)
+        response = subject.get_events('app.oauth2.as.consent.grant')
+        expect(response.body).not_to be_empty
+      end
+    end
+    it 'returns a response body of disconnections' do
+      VCR.use_cassette('okta/disconnection_logs', match_requests_on: %i[method host]) do
+        # to ensure our vcr has data in the response
+        subject.instance_variable_set(:@time_period, 5.days.ago.utc.iso8601)
+        response = subject.get_events('app.oauth2.as.token.revoke')
+        expect(response.body).not_to be_empty
+      end
+    end
   end
 end
