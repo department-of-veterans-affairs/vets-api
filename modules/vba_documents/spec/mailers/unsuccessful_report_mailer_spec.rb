ab2bf226475736c146d7d6770d0a2699e2e859da
Cris Shupp <cristopher.shupp@libertyits.com>
2020-12-28 09:26:24 -0500

    refactoring into lets

--- modules/vba_documents/spec/mailers/unsuccessful_report_mailer_spec.rb
+++ modules/vba_documents/spec/mailers/unsuccessful_report_mailer_spec.rb
@@ -7,28 +7,28 @@ RSpec.describe VBADocuments::UnsuccessfulReportMailer, type: [:mailer] do
   let(:uploaded_upload) { FactoryBot.create(:upload_submission, :status_uploaded) }
   let(:totals) do
     [
-      {
-        'vetraspec' => {
-          'error' => 1,
-          'expired' => 1,
-          'pending' => 4,
-          'uploaded' => 1,
-          :totals => 7,
-          :error_rate => '14%',
-          :expired_rate => '14%'
+        {
+            'vetraspec' => {
+                'error' => 1,
+                'expired' => 1,
+                'pending' => 4,
+                'uploaded' => 1,
+                :totals => 7,
+                :error_rate => '14%',
+                :expired_rate => '14%'
+            }
+        },
+        {
+            'vetpro' => {
+                'error' => 2,
+                'expired' => 1,
+                'pending' => 2,
+                'uploaded' => 2,
+                :totals => 7,
+                :error_rate => '29%',
+                :expired_rate => '14%'
+            }
         }
-      },
-      {
-        'vetpro' => {
-          'error' => 2,
-          'expired' => 1,
-          'pending' => 2,
-          'uploaded' => 2,
-          :totals => 7,
-          :error_rate => '29%',
-          :expired_rate => '14%'
-        }
-      }
     ]
   end
 
@@ -37,46 +37,49 @@ RSpec.describe VBADocuments::UnsuccessfulReportMailer, type: [:mailer] do
       class << self
         attr_reader :env_hash
       end
+
       @env_hash = {
-        staging: 'https://dsva-vetsgov-staging-benefits-documents.s3.us-gov-west-1.amazonaws.com/',
-        dev: 'https://dsva-vetsgov-dev-benefits-documents.s3.us-gov-west-1.amazonaws.com/',
-        sandbox: 'https://dsva-vagov-sandbox-benefits-documents.s3.us-gov-west-1.amazonaws.com/',
-        prod: 'https://dsva-vetsgov-prod-benefits-documents.s3.us-gov-west-1.amazonaws.com/'
+          staging: 'https://dsva-vetsgov-staging-benefits-documents.s3.us-gov-west-1.amazonaws.com/',
+          dev: 'https://dsva-vetsgov-dev-benefits-documents.s3.us-gov-west-1.amazonaws.com/',
+          sandbox: 'https://dsva-vagov-sandbox-benefits-documents.s3.us-gov-west-1.amazonaws.com/',
+          prod: 'https://dsva-vetsgov-prod-benefits-documents.s3.us-gov-west-1.amazonaws.com/'
       }
+    end
 
-      def self.setup(current_env_url, totals, error_upload, uploaded_upload)
-        Settings.vba_documents.location.prefix = current_env_url
-        VBADocuments::Deployment.environment = VBADocuments::Deployment.fetch_environment
-        if VBADocuments::UnsuccessfulReportMailer.const_defined?(:RECIPIENTS)
-          VBADocuments::UnsuccessfulReportMailer.send(:remove_const, :RECIPIENTS)
+    before(:each) do
+      current_env_url = env_url.last
+      Settings.vba_documents.location.prefix = current_env_url
+      VBADocuments::Deployment.environment = VBADocuments::Deployment.fetch_environment
+      VBADocuments::UnsuccessfulReportMailer.send(:remove_const, :RECIPIENTS) if VBADocuments::UnsuccessfulReportMailer.const_defined?(:RECIPIENTS)
+      load './modules/vba_documents/app/mailers/vba_documents/unsuccessful_report_mailer.rb'
+      @email = described_class.build(totals, [error_upload], [uploaded_upload], 7.days.ago, Time.zone.now).deliver_now
+    end
+
+    MailerHelper.env_hash.each_pair do |k, v|
+      context do
+        let(:env_url) { [k, v] }
+        it "sends the email for #{k}" do
+          expect(@email.subject).to eq("Benefits Intake Unsuccessful Submission Report for #{k}")
         end
-        load './modules/vba_documents/app/mailers/vba_documents/unsuccessful_report_mailer.rb'
-        VBADocuments::UnsuccessfulReportMailer.build(totals,
-                                                     [error_upload],
-                                                     [uploaded_upload], 7.days.ago,
-                                                     Time.zone.now).deliver_now
       end
     end
 
     MailerHelper.env_hash.each_pair do |k, v|
-      it "sends the email for #{k}" do
-        email = MailerHelper.setup(v, totals, error_upload, uploaded_upload)
-        expect(email.subject).to eq("Benefits Intake Unsuccessful Submission Report for #{k}")
+      context do
+        let(:env_url) { [k, v] }
+        it "sends to the right people for #{k}" do
+          people = VBADocuments::UnsuccessfulReportMailer.fetch_recipients
+          expect(@email.to).to eq(people)
+        end
       end
     end
 
     MailerHelper.env_hash.each_pair do |k, v|
-      it "sends to the right people for #{k}" do
-        email = MailerHelper.setup(v, totals, error_upload, uploaded_upload)
-        people = VBADocuments::UnsuccessfulReportMailer.fetch_recipients
-        expect(email.to).to eq(people)
-      end
-    end
-
-    MailerHelper.env_hash.each_pair do |k, v|
-      it "State the environment #{k.to_s.upcase} in the body" do
-        email = MailerHelper.setup(v, totals, error_upload, uploaded_upload)
-        expect(email.body.to_s.split("\n").first).to match(/.*#{k.to_s.upcase}.*/)
+      context do
+        let(:env_url) { [k, v] }
+        it "State the environment #{k.to_s.upcase} in the body" do
+          expect(@email.body.to_s.split("\n").first).to match(/.*#{k.to_s.upcase}.*/)
+        end
       end
     end
   end
