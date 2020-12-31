8a8e6bdf4a37f9988dcf7fd79c8976ad54624972
Lee DeBoom <lee.deboom@oddball.io>
2020-12-30 21:39:27 -0600

    API-3277

---
+++ modules/claims_api/db/migrate/20201231033748_add_bgs_special_issues_responses_column_to_auto_established_claims.rb
@@ -0,0 +1,6 @@
+class AddBGSSpecialIssuesResponsesColumnToAutoEstablishedClaims < ActiveRecord::Migration[6.0]
+  def change
+    add_column(:claims_api_auto_established_claims, :encrypted_bgs_special_issue_responses, :string)
+    add_column(:claims_api_auto_established_claims, :encrypted_bgs_special_issue_responses_iv, :string)
+  end
+end
