22ddb95cda3cb1baf09686f669cf58a8f6bdb202
Joe <elder_joseph@bah.com>
2021-01-04 14:01:28 -0500

    17772: Initial commit

---
+++ app/workers/education_form/forms/va10203dny.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module EducationForm::Forms
+  class VA10203DNY < Base
+    def header_form_type
+      'V10203DNY'
+    end
+
+    def form_benefit
+      @applicant.benefit&.titleize
+    end
+
+    def school_name
+      @applicant.schoolName.upcase.strip
+    end
+
+    def any_remaining_benefit
+      yesno(%w[moreThanSixMonths sixMonthsOrLess].include?(@applicant.benefitLeft))
+    end
+
+    def receive_text_message
+      return false if @applicant.receiveTexts.nil?
+
+      @applicant.receiveTexts
+    end
+  end
+end
