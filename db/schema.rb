f589b020d1946f131e5b5f8fe88e608a0b7720f0
Nathan Wright <nathan.wright@oddball.io>
2021-01-05 11:22:21 -0700

    Add source field to nod

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_174800) do
+ActiveRecord::Schema.define(version: 2021_01_05_181435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_01_05_174800) do
     t.string "status", default: "pending", null: false
     t.string "code"
     t.string "detail"
+    t.string "source"
   end
 
   create_table "async_transactions", id: :serial, force: :cascade do |t|
