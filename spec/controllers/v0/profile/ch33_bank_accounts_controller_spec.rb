4ea59582902c0f048d4dac72afebac599bb2c2d0
Lihan Li <frankieteardrop@gmail.com>
2021-01-05 10:36:29 -0800

    rerender find_ch33_dd_eft_res on ch33 dd update

--- spec/controllers/v0/profile/ch33_bank_accounts_controller_spec.rb
+++ spec/controllers/v0/profile/ch33_bank_accounts_controller_spec.rb
@@ -35,44 +35,46 @@ RSpec.describe V0::Profile::Ch33BankAccountsController, type: :controller do
     end
   end
 
+  def expect_find_ch33_dd_eft_res
+    expect(JSON.parse(response.body)).to eq(
+      {
+        'data' => {
+          'id' => '', 'type' => 'savon_responses',
+          'attributes' => {
+            'account_type' => 'Checking', 'account_number' => '123',
+            'financial_institution_routing_number' => '*****9982'
+          }
+        }
+      }
+    )
+  end
+
   describe '#index' do
     it 'returns the right data' do
       VCR.use_cassette('bgs/service/find_ch33_dd_eft', VCR::MATCH_EVERYTHING) do
         get(:index)
       end
 
-      expect(JSON.parse(response.body)).to eq(
-        {
-          'data' => {
-            'id' => '', 'type' => 'savon_responses',
-            'attributes' => {
-              'account_type' => 'Checking', 'account_number' => '123',
-              'financial_institution_routing_number' => '*****9982'
-            }
-          }
-        }
-      )
+      expect_find_ch33_dd_eft_res
     end
   end
 
   describe '#update' do
     it 'sends the update req' do
       VCR.use_cassette('bgs/service/update_ch33_dd_eft', VCR::MATCH_EVERYTHING) do
-        put(
-          :update,
-          params: {
-            account_type: 'Checking',
-            account_number: '444',
-            financial_institution_routing_number: '122239982'
-          }
-        )
-
-        expect(
-          JSON.parse(response.body)['update_ch33_dd_eft_response']['return']['return_message']
-        ).to eq(
-          'SUCCESS'
-        )
+        VCR.use_cassette('bgs/service/find_ch33_dd_eft', VCR::MATCH_EVERYTHING) do
+          put(
+            :update,
+            params: {
+              account_type: 'Checking',
+              account_number: '444',
+              financial_institution_routing_number: '122239982'
+            }
+          )
+        end
       end
+
+      expect_find_ch33_dd_eft_res
     end
   end
 end
