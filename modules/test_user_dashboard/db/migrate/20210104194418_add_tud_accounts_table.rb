bd55a83ee4236844f80dc3bc157abab96cd50801
John Bramley <bramleyjl@gmail.com>
2021-01-05 17:55:43 -0500

    adds dummy db schema

--- modules/test_user_dashboard/db/migrate/20210104194418_add_tud_accounts_table.rb
+++ modules/test_user_dashboard/db/migrate/20210104194418_add_tud_accounts_table.rb
@@ -1,12 +1,14 @@
 class AddTudAccountsTable < ActiveRecord::Migration[6.0]
-  def change
+  def up
     create_table :tud_accounts do |t|
-      t.string :tud_id
       t.boolean :standard, :available
       t.datetime :checkout_time
       t.integer :loa_level
-      t.belongs_to(:account, foreign_key: true)
     end
-    add_index(:tud_accounts, :tud_id)
+    add_foreign_key :accounts, :tud_accounts
+  end
+
+  def down
+    drop_table :tud_accounts
   end
 end
