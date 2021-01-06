6d74fa35a6292ddca5fcb52b2fdf22a8156beb6f
Joe <elder_joseph@bah.com>
2021-01-06 16:08:37 -0500

    17772: update SavedClaim 10203

--- app/models/saved_claim/education_benefits/va10203dny.rb
+++ app/models/saved_claim/education_benefits/va10203dny.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class SavedClaim::EducationBenefits::VA10203dny < SavedClaim::VA10203
+class SavedClaim::EducationBenefits::VA10203dny < SavedClaim::EducationBenefits::VA10203
   add_form_and_validation('22-10203DNY')
 end
