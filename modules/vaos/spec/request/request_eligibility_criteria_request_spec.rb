5a86d740564c99434dbf35f48f63fb96561919db
brad <brad@concord.tech>
2021-01-06 12:02:16 -0800

    removed integration tests related to calling the  endpoint w/ no parameters.  This was done because those particular tests were taking a long time to run and  since in VOAS there is not an use case, there is really no reason to test them.  As adjunct work, an additional ticket will be entered to review adding validation around the associated controller endpoint to only permit calls when appropriate parameters are present in the request.

--- modules/vaos/spec/request/request_eligibility_criteria_request_spec.rb
+++ modules/vaos/spec/request/request_eligibility_criteria_request_spec.rb
@@ -13,107 +13,86 @@ RSpec.describe 'request eligibility criteria', type: :request do
 
   let(:inflection_header) { { 'X-Key-Inflection' => 'camel' } }
 
-  # context 'with a loa1 user' do
-  #   let(:user) { FactoryBot.create(:user, :loa1) }
+  context 'with a loa1 user' do
+    let(:user) { FactoryBot.create(:user, :loa1) }
 
-  #   it 'returns a forbidden error' do
-  #     get '/vaos/v0/request_eligibility_criteria'
-  #     expect(response).to have_http_status(:forbidden)
-  #     expect(JSON.parse(response.body)['errors'].first['detail'])
-  #       .to eq('You do not have access to online scheduling')
-  #   end
-  # end
+    it 'returns a forbidden error' do
+      get '/vaos/v0/request_eligibility_criteria'
+      expect(response).to have_http_status(:forbidden)
+      expect(JSON.parse(response.body)['errors'].first['detail'])
+        .to eq('You do not have access to online scheduling')
+    end
+  end
 
   context 'with a loa3 user' do
     let(:user) { build(:user, :vaos) }
     let(:size) { JSON.parse(response.body)['data'].size }
 
-    context 'with no ids' do
-      # it 'returns a 200 with the correct schema' do
-      #   VCR.use_cassette('vaos/systems/get_request_eligibility_criteria', match_requests_on: %i[method uri]) do
-      #     get '/vaos/v0/request_eligibility_criteria'
-      #     expect(response).to have_http_status(:ok)
-      #     expect(size).to eq(1269)
-      #     expect(response).to match_response_schema('vaos/request_eligibility_criteria', { strict: false })
-      #   end
-      # end
-
-      it 'returns a 200 with the correct camel-inflected schema' do 
-        VCR.use_cassette('vaos/systems/get_request_eligibility_criteria', match_requests_on: %i[method uri]) do
-          binding.pry
-          get '/vaos/v0/request_eligibility_criteria', headers: inflection_header
+    context 'with one id' do
+      it 'returns a 200 with the correct schema' do
+        VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_id', match_requests_on: %i[method uri]) do
+          get '/vaos/v0/request_eligibility_criteria', params: { site_codes: '688' }
           expect(response).to have_http_status(:ok)
-          expect(size).to eq(1269)
+          expect(size).to eq(1)
+          expect(response).to match_response_schema('vaos/request_eligibility_criteria', { strict: false })
+        end
+      end
+
+      it 'returns a 200 with the correct camel-inflected schema' do
+        VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_id', match_requests_on: %i[method uri]) do
+          get '/vaos/v0/request_eligibility_criteria', params: { site_codes: '688' }, headers: inflection_header
+          expect(response).to have_http_status(:ok)
+          expect(size).to eq(1)
           expect(response).to match_camelized_response_schema('vaos/request_eligibility_criteria', { strict: false })
         end
       end
     end
 
-    # context 'with one id' do
-    #   it 'returns a 200 with the correct schema' do
-    #     VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_id', match_requests_on: %i[method uri]) do
-    #       get '/vaos/v0/request_eligibility_criteria', params: { site_codes: '688' }
-    #       expect(response).to have_http_status(:ok)
-    #       expect(size).to eq(1)
-    #       expect(response).to match_response_schema('vaos/request_eligibility_criteria', { strict: false })
-    #     end
-    #   end
+    context 'with multiple site_codes' do
+      it 'returns a 200 with the correct schema' do
+        VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_site_codes',
+                         match_requests_on: %i[method uri]) do
+          get '/vaos/v0/request_eligibility_criteria', params: { site_codes: %w[442 534] }
+          expect(response).to have_http_status(:ok)
+          expect(size).to eq(2)
+          expect(response).to match_response_schema('vaos/request_eligibility_criteria', { strict: false })
+        end
+      end
 
-    #   it 'returns a 200 with the correct camel-inflected schema' do
-    #     VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_id', match_requests_on: %i[method uri]) do
-    #       get '/vaos/v0/request_eligibility_criteria', params: { site_codes: '688' }, headers: inflection_header
-    #       expect(response).to have_http_status(:ok)
-    #       expect(size).to eq(1)
-    #       expect(response).to match_camelized_response_schema('vaos/request_eligibility_criteria', { strict: false })
-    #     end
-    #   end
-    # end
+      it 'returns a 200 with the correct camel-inflected schema' do
+        VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_site_codes',
+                         match_requests_on: %i[method uri]) do
+          get '/vaos/v0/request_eligibility_criteria', params: { site_codes: %w[442 534] }, headers: inflection_header
+          expect(response).to have_http_status(:ok)
+          expect(size).to eq(2)
+          expect(response).to match_camelized_response_schema('vaos/request_eligibility_criteria', { strict: false })
+        end
+      end
+    end
 
-    # context 'with multiple site_codes' do
-    #   it 'returns a 200 with the correct schema' do
-    #     VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_site_codes',
-    #                      match_requests_on: %i[method uri]) do
-    #       get '/vaos/v0/request_eligibility_criteria', params: { site_codes: %w[442 534] }
-    #       expect(response).to have_http_status(:ok)
-    #       expect(size).to eq(2)
-    #       expect(response).to match_response_schema('vaos/request_eligibility_criteria', { strict: false })
-    #     end
-    #   end
+    context 'with multiple parent_sites' do
+      it 'returns a 200 with the correct schema' do
+        VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_parent_sites',
+                         match_requests_on: %i[method uri]) do
+          get '/vaos/v0/request_eligibility_criteria', params: { parent_sites: %w[983 984] }
+          expect(response).to have_http_status(:ok)
+          expect(size).to eq(5)
+          expect(response).to match_response_schema('vaos/request_eligibility_criteria', { strict: false })
+        end
+      end
 
-    #   it 'returns a 200 with the correct camel-inflected schema' do
-    #     VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_site_codes',
-    #                      match_requests_on: %i[method uri]) do
-    #       get '/vaos/v0/request_eligibility_criteria', params: { site_codes: %w[442 534] }, headers: inflection_header
-    #       expect(response).to have_http_status(:ok)
-    #       expect(size).to eq(2)
-    #       expect(response).to match_camelized_response_schema('vaos/request_eligibility_criteria', { strict: false })
-    #     end
-    #   end
-    # end
+      it 'returns a 200 with the correct camel-inflected schema' do
+        VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_parent_sites',
+                         match_requests_on: %i[method uri]) do
+          get '/vaos/v0/request_eligibility_criteria',\
+              params: { parent_sites: %w[983 984] },
+              headers: inflection_header
 
-    # context 'with multiple parent_sites' do
-    #   it 'returns a 200 with the correct schema' do
-    #     VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_parent_sites',
-    #                      match_requests_on: %i[method uri]) do
-    #       get '/vaos/v0/request_eligibility_criteria', params: { parent_sites: %w[983 984] }
-    #       expect(response).to have_http_status(:ok)
-    #       expect(size).to eq(5)
-    #       expect(response).to match_response_schema('vaos/request_eligibility_criteria', { strict: false })
-    #     end
-    #   end
-
-    #   it 'returns a 200 with the correct camel-inflected schema' do
-    #     VCR.use_cassette('vaos/systems/get_request_eligibility_criteria_by_parent_sites',
-    #                      match_requests_on: %i[method uri]) do
-    #       get '/vaos/v0/request_eligibility_criteria',\
-    #           params: { parent_sites: %w[983 984] },
-    #           headers: inflection_header
-
-    #       expect(response).to have_http_status(:ok)
-    #       expect(size).to eq(5)
-    #       expect(response).to match_camelized_response_schema('vaos/request_eligibility_criteria', { strict: false })
-    #     end
-    #   end
-    # end
+          expect(response).to have_http_status(:ok)
+          expect(size).to eq(5)
+          expect(response).to match_camelized_response_schema('vaos/request_eligibility_criteria', { strict: false })
+        end
+      end
+    end
   end
 end
