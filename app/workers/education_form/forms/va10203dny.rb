c76783e0e33762a7bd19f55ee076f1974dd80ef1
Joe <elder_joseph@bah.com>
2021-01-06 16:21:25 -0500

    17772: update education_form 10203

--- app/workers/education_form/forms/va10203dny.rb
+++ app/workers/education_form/forms/va10203dny.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module EducationForm::Forms
-  class VA10203dny < EducationForm::VA10203
+  class VA10203dny < EducationForm::Forms::VA10203
     def header_form_type
       'V10203DNY'
     end
