614e4d8e368723b33c252975a6061ef1ffc47174
Philip Becker <boofungal316@yahoo.com>
2021-01-07 18:04:48 -0500

    concerns namespace not necessary for JsonFormatValidation

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/higher_level_reviews_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/higher_level_reviews_controller.rb
@@ -4,7 +4,7 @@ require 'json_schema/json_api_missing_attribute'
 require 'appeals_api/form_schemas'
 
 class AppealsApi::V1::DecisionReviews::HigherLevelReviewsController < AppealsApi::ApplicationController
-  include AppealsApi::Concerns::JsonFormatValidation
+  include AppealsApi::JsonFormatValidation
 
   skip_before_action(:authenticate)
   before_action :validate_json_format, if: -> { request.post? }
