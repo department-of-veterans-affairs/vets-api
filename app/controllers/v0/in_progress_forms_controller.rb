c10ce11e3786ecc42da573bac86a86e2ee534803
Ed Mangimelli <edmangimelli@gmail.com>
2021-01-07 15:51:42 -0500

    add tests for the in_progress_forms serializer

--- app/controllers/v0/in_progress_forms_controller.rb
+++ app/controllers/v0/in_progress_forms_controller.rb
@@ -5,7 +5,7 @@ module V0
     include IgnoreNotFound
 
     def index
-      render json: InProgressForm.where(user_uuid: @current_user.uuid)
+      render json: current_users_in_progress_forms
     end
 
     def show
@@ -31,6 +31,10 @@ module V0
 
       form.destroy
       render json: form
+    end
+
+    def current_users_in_progress_forms
+      InProgressForm.where(user_uuid: @current_user.uuid)
     end
   end
 end
