f7b6e49bbcec76485bd13d6339710a54bc60a002
Nathan Wright <nathan.wright@oddball.io>
2021-01-08 15:30:05 -0700

    Show all statuses on email report

--- modules/appeals_api/lib/appeals_api/decision_review_report.rb
+++ modules/appeals_api/lib/appeals_api/decision_review_report.rb
@@ -9,20 +9,33 @@ module AppealsApi
       @to = to
     end
 
-    def successful_hlr_count
-      HigherLevelReview.where(created_at: from..to, status: 'success').count
+    def hlr_by_status_and_count
+      group_records(HigherLevelReview)
     end
 
     def hlr_with_errors
       HigherLevelReview.where(created_at: from..to, status: 'error')
     end
 
-    def successful_nod_count
-      NoticeOfDisagreement.where(created_at: from..to, status: 'success').count
+    def nod_by_status_and_count
+      group_records(NoticeOfDisagreement)
     end
 
     def nod_with_errors
       NoticeOfDisagreement.where(created_at: from..to, status: 'error')
+    end
+
+    private
+
+    def group_records(record_type)
+      statuses = record_type::STATUSES.sort.index_with { |_| 0 }
+
+      record_type
+        .where(created_at: from..to)
+        .group_by(&:status)
+        .each { |status, record_list| statuses[status] = record_list.size }
+
+      statuses
     end
   end
 end
--- modules/appeals_api/spec/lib/decision_review_report_spec.rb
+++ modules/appeals_api/spec/lib/decision_review_report_spec.rb
@@ -4,16 +4,30 @@ require 'rails_helper'
 require 'appeals_api/decision_review_report'
 
 describe AppealsApi::DecisionReviewReport do
+  # rubocop:disable Layout/FirstHashElementIndentation
   it 'can correctly calculate hlrs' do
+    create :higher_level_review, status: 'processing'
+    create :higher_level_review, status: 'processing'
+    create :higher_level_review, status: 'processing'
+
     create :higher_level_review, created_at: 1.week.ago, status: 'success'
     create :higher_level_review, status: 'success'
     create :higher_level_review, status: 'success'
 
     errored_hlr = create :higher_level_review, :status_error
-
     subject = described_class.new(from: 5.days.ago, to: Time.now.utc)
 
-    expect(subject.successful_hlr_count).to eq(2)
+    expect(subject.hlr_by_status_and_count).to eq({
+      'error' => 1,
+      'expired' => 0,
+      'pending' => 0,
+      'processing' => 3,
+      'received' => 0,
+      'submitted' => 0,
+      'submitting' => 0,
+      'success' => 2,
+      'uploaded' => 0
+    })
     expect(subject.hlr_with_errors).to eq([errored_hlr])
   end
 
@@ -26,7 +40,18 @@ describe AppealsApi::DecisionReviewReport do
 
     subject = described_class.new(from: 5.days.ago, to: Time.now.utc)
 
-    expect(subject.successful_nod_count).to eq(2)
+    expect(subject.nod_by_status_and_count).to eq({
+      'error' => 1,
+      'expired' => 0,
+      'pending' => 0,
+      'processing' => 0,
+      'received' => 0,
+      'submitted' => 0,
+      'submitting' => 0,
+      'success' => 2,
+      'uploaded' => 0
+    })
     expect(subject.nod_with_errors).to eq([errored_nod])
   end
+  # rubocop:enable Layout/FirstHashElementIndentation
 end
