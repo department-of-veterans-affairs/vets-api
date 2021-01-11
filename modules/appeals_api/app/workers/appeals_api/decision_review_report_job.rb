ea85dad85346f45562b3462b53d53858f55f526e
Nathan Wright <nathan.wright@oddball.io>
2021-01-11 14:24:56 -0700

    Add optional args to report worker (to allow for manually specifying the date range)

--- modules/appeals_api/app/workers/appeals_api/decision_review_report_job.rb
+++ modules/appeals_api/app/workers/appeals_api/decision_review_report_job.rb
@@ -6,11 +6,8 @@ module AppealsApi
   class DecisionReviewReportJob
     include Sidekiq::Worker
 
-    def perform
+    def perform(to: Time.zone.now, from: (to.monday? ? 3.days.ago : 1.day.ago))
       if Settings.modules_appeals_api.report_enabled
-        to = Time.zone.now
-        from = 1.day.ago.utc
-
         DecisionReviewMailer.build(date_from: from, date_to: to).deliver_now
       end
     end
