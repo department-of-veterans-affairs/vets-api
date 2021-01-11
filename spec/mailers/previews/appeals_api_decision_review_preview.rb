82179355e292c988129ec1b7d40e95a6b51ad4f7
Nathan Wright <nathan.wright@oddball.io>
2021-01-11 12:21:18 -0700

    Change preview report date

--- spec/mailers/previews/appeals_api_decision_review_preview.rb
+++ spec/mailers/previews/appeals_api_decision_review_preview.rb
@@ -4,7 +4,7 @@ class AppealsApiDecisionReviewPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/appeals_api/decision_review/report
   def build
     to = Time.zone.now
-    from = 1.day.ago.utc
+    from = Time.at(0).utc
 
     AppealsApi::DecisionReviewMailer.build(date_from: from, date_to: to)
   end
