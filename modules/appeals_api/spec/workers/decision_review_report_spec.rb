b69486f482d482c5a6e9cb6ebbebdb89d5f3d80e
Nathan Wright <nathan.wright@oddball.io>
2021-01-11 14:50:25 -0700

    Fix broken spec

--- modules/appeals_api/spec/workers/decision_review_report_spec.rb
+++ modules/appeals_api/spec/workers/decision_review_report_spec.rb
@@ -8,7 +8,7 @@ describe AppealsApi::DecisionReviewReportJob, type: :job do
       with_settings(Settings.modules_appeals_api, report_enabled: true) do
         Timecop.freeze
         date_to = Time.zone.now
-        date_from = date_to.monday? ? 7.days.ago : 1.day.ago
+        date_from = date_to.monday? ? 3.days.ago : 1.day.ago
 
         expect(AppealsApi::DecisionReviewMailer).to receive(:build).once.with(
           date_from: date_from,
