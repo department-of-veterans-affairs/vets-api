c68a4c01b26c8bc0b8be9464fed3f4e14f46f7ef
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-01-13 10:04:14 -0500

    add test for creating multiple components

--- spec/lib/generators/module_component/module_components_generator_spec.rb
+++ spec/lib/generators/module_component/module_components_generator_spec.rb
@@ -66,6 +66,21 @@ RSpec.describe 'ModuleComponent', type: :generator do
     end
   end
 
+  describe 'creates multiple components' do
+    before(:all) do
+      ModuleGenerator.new(['foo']).create_directory_structure
+      ModuleComponentGenerator.new(%w[foo controller serializer]).create_component
+    end
+
+    after(:all) { FileUtils.rm_rf(Dir[Rails.root.join('modules', 'foo')]) }
+
+    let(:path) { Rails.root.join('modules', 'foo', 'app') }
+
+    it 'creates the module controller and serializer files' do
+       expect(File.exist?("#{path}/controllers/foo/v0/foo_controller.rb")).to be_truthy
+       expect(File.exist?("#{path}/serializers/foo/v0/foo_serializer.rb")).to be_truthy
+    end
+  end
 
   describe 'does not create an invalid component' do
   end
