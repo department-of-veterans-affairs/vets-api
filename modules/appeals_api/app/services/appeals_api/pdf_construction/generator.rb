cac190da8741b46511f3da226e8e9c215fac90cc
Jack Schuss <jackschuss@gmail.com>
2021-01-13 17:52:41 -0500

    Finish up Generator and Nod Structure file

--- modules/appeals_api/app/services/appeals_api/pdf_construction/generator.rb
+++ modules/appeals_api/app/services/appeals_api/pdf_construction/generator.rb
@@ -8,11 +8,11 @@ module AppealsApi
       end
 
       def generate
-        fill_unique_form_fields
+        @form_fill_path = fill_unique_form_fields
         #=> '{appeal.id}-tmp.pdf' OR '/tmp/#{appeal.id}-overlaid-form-fill-tmp.pdf'
 
-        insert_additional_pages
-        #=> '#{appeal.id}-completed-unstamped-tmp.pdf'
+        @unstamped_path = insert_additional_pages
+        #=> '#{appeal.id}-completed-unstamped-tmp.pdf OR @form_fill_path'
 
         stamp
       end
@@ -32,31 +32,38 @@ module AppealsApi
           flatten: true
         )
 
-        @form_fill_path = appeal.insert_overlaid_pages(form_fill_path)
+        appeal.insert_overlaid_pages(form_fill_path)
       end
 
       def insert_additional_pages
-        additional_pages_added_path = "/tmp/#{appeal.id}-additional-pages-tmp.pdf"
-
         pdf = appeal.add_additional_pages
+
+        return @form_fill_path if pdf.blank?
+
+        additional_pages_added_path = "/tmp/#{appeal.id}-additional-pages-tmp.pdf"
         pdf.render_file(additional_pages_added_path) #saves the file
 
-        @unstamped_path = combine_form_fill_and_additional_pages
+        combine_form_fill_and_additional_pages(additional_pages_added_path)
       end
 
-      def combine_form_fill_and_additional_pages
+      def combine_form_fill_and_additional_pages(additional_pages_added_path)
+        path = "/tmp/#{appeal.id}-completed-unstamped-tmp.pdf"
         unstamped_completed_pdf = CombinePDF.load(@form_fill_path) << CombinePDF.load(additional_pages_added_path)
-        unstamped_completed_pdf.save("/tmp/#{appeal.id}-completed-unstamped-tmp.pdf")
+        unstamped_completed_pdf.save(path)
+
+        path
       end
 
       def stamp
         stamper = CentralMail::DatestampPdf.new(@unstamped_path)
+
         bottom_stamped_path = stamper.run(
           text: "API.VA.GOV #{Time.zone.now.utc.strftime('%Y-%m-%d %H:%M%Z')}",
           x: 5,
           y: 5,
           text_only: true
         )
+
         stamped_pdf_path = CentralMail::DatestampPdf.new(bottom_stamped_path).run(
           text: "Submitted by #{appeal.consumer_name} via api.va.gov",
           x: 429,
--- modules/appeals_api/app/services/appeals_api/pdf_construction/notice_of_disagreement/structure.rb
+++ modules/appeals_api/app/services/appeals_api/pdf_construction/notice_of_disagreement/structure.rb
@@ -1,3 +1,5 @@
+require 'prawn/table'
+
 module AppealsApi
   module PdfConstruction
     module NoticeOfDisagreement
@@ -14,15 +16,15 @@ module AppealsApi
             form_fields.date_signed                       => form_data.veteran_dob,
             form_fields.mailing_address_number_and_street => form_data.mailing_address_number_and_street,
             form_fields.homeless?                         => form_data.homeless?,
-            form_fields.preferred_phone                   => form_data.veteran_name,
-            form_fields.preferred_email                   => form_data.veteran_name,
-            form_fields.direct_review?                    => form_data.veteran_name,
-            form_fields.evidence_submission?              => form_data.veteran_name,
-            form_fields.hearing?                          => form_data.veteran_name,
-            form_fields.additional_pages?                 => form_data.veteran_name,
-            form_fields.soc_opt_in?                       => form_data.veteran_name,
+            form_fields.preferred_phone                   => form_data.preferred_phone,
+            form_fields.preferred_email                   => form_data.preferred_email,
+            form_fields.direct_review?                    => form_data.direct_review?,
+            form_fields.evidence_submission?              => form_data.evidence_submission?,
+            form_fields.hearing?                          => form_data.hearing?,
+            form_fields.additional_pages?                 => form_data.additional_pages?,
+            form_fields.soc_opt_in?                       => form_data.soc_opt_in?,
             form_fields.signature                         => form_data.signature,
-            form_fields.date_signed                       => form_data.veteran_name,
+            form_fields.date_signed                       => form_data.date_signed,
           }
 
           fill_first_five_issue_dates!(options)
@@ -30,8 +32,8 @@ module AppealsApi
 
         def insert_overlaid_pages(form_fill_path)
           pdftk = PdfForms.new(Settings.binaries.pdftk)
-          temp_path = "/tmp/#{::Common::FileHelpers.random_file_path}.pdf"
-          output_path = "/tmp/#{appeal.id}-overlaid-form-fill-tmp.pdf"
+          temp_path = "/#{::Common::FileHelpers.random_file_path}.pdf"
+          output_path = "/tmp/#{notice_of_disagreement.id}-overlaid-form-fill-tmp.pdf"
 
           Prawn::Document.generate(temp_path) do |pdf|
             text_opts = {
@@ -67,14 +69,20 @@ module AppealsApi
           end
 
           pdftk.multistamp(form_fill_path, temp_path, output_path)
+
+          output_path
         end
 
         def add_additional_pages
-          # this method must return a Prawn::Document object.
+          return unless form_data.additional_pages?
 
-          add_extra_issues_and_rep_name
+          additional_pages_pdf
 
-          @additional_pages_pdf
+          add_hearing_type_and_extra_issues_page
+
+          #additional_pages_pdf.start_new_page before each new page method
+
+          additional_pages_pdf
         end
 
         def form_title
@@ -82,7 +90,7 @@ module AppealsApi
         end
 
         def stamp(stamped_pdf_path)
-          CentralMail::DatestampPdf.new(stamped_path).run(
+          CentralMail::DatestampPdf.new(stamped_pdf_path).run(
             text: form_data.stamp_text,
             x: 5,
             y: 775,
@@ -92,12 +100,14 @@ module AppealsApi
 
         private
 
+        attr_accessor :notice_of_disagreement
+
         def form_fields
           @form_fields ||= NoticeOfDisagreement::FormFields.new
         end
 
         def form_data
-          @form_data ||= NoticeOfDisagreement::FormData.new(@notice_of_disagreement)
+          @form_data ||= NoticeOfDisagreement::FormData.new(notice_of_disagreement)
         end
 
         def additional_pages_pdf
@@ -117,9 +127,31 @@ module AppealsApi
           options
         end
 
+        def add_hearing_type_and_extra_issues_page
+          additional_pages_pdf.text(hearing_type_text, inline_format: true)
+          additional_pages_pdf.text("\n<b>Additional Issues</b>\n", inline_format: true)
+          additional_pages_pdf.table(extra_issues_table_data, header: true)
+        end
+
+        def hearing_type_text
+          return if notice_of_disagreement.hearing_type_preference.blank?
+
+          "\nHearing Type Preference: #{notice_of_disagreement.hearing_type_preference.humanize}\n"
+        end
+
+        def extra_issues_table_data
+          header = ['A. Specific Issue(s)', 'B. Date of Decision']
+
+          data = form_data.contestable_issues.drop(5).map do |issue|
+            [issue['attributes']['issue'], issue['attributes']['decisionDate']]
+          end
+
+          data.unshift(header)
+        end
+
         def method_missing(method, *args, &block)
-          if @notice_of_disagreement.respond_to?(method)
-            @notice_of_disagreement.send(method)
+          if notice_of_disagreement.respond_to?(method)
+            notice_of_disagreement.send(method)
           else
             super
           end
