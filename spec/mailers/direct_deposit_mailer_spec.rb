4604c915f931de41e8b8c47ff10d93d2f1ba6f20
Lihan Li <frankieteardrop@gmail.com>
2021-01-12 21:56:49 -0800

    add ch33 text to dd email

--- spec/mailers/direct_deposit_mailer_spec.rb
+++ spec/mailers/direct_deposit_mailer_spec.rb
@@ -3,8 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe DirectDepositMailer, type: [:mailer] do
+  let(:dd_type) { :comp_pen }
+
   subject do
-    described_class.build(email, google_analytics_client_id).deliver_now
+    described_class.build(email, google_analytics_client_id, dd_type).deliver_now
   end
 
   let(:email) { 'foo@example.com' }
@@ -24,6 +26,24 @@ RSpec.describe DirectDepositMailer, type: [:mailer] do
       expect { DirectDepositEmailJob.new.perform('test@example.com', 123_456_789) }.to change {
         ActionMailer::Base.deliveries.count
       }.by(1)
+    end
+
+    context 'comp and pen email' do
+      it 'includes the right text' do
+        expect(subject.body.raw_source).to include(
+          "We'll use your updated information to deposit any disability compensation or pension benefits you may receive"
+        )
+      end
+    end
+
+    context 'ch33 email' do
+      let(:dd_type) { :ch33 }
+
+      it 'includes the right text' do
+        expect(subject.body.raw_source).to include(
+          "We'll use your updated information to deposit any education benefits you may receive"
+        )
+      end
     end
   end
 end
