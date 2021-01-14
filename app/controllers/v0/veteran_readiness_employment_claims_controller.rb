1180a7d1ec8191a034527e448217c481710d3ab0
Derek Dyer <derek@digitalderek.com>
2021-01-14 16:49:14 -0500

    Add stuff

--- app/controllers/v0/veteran_readiness_employment_claims_controller.rb
+++ app/controllers/v0/veteran_readiness_employment_claims_controller.rb
@@ -3,7 +3,9 @@
 module V0
   class VeteranReadinessEmploymentClaimsController < ClaimsBaseController
     def create
+      load_user
       claim = SavedClaim::VeteranReadinessEmploymentClaim.new(form: filtered_params[:form])
+      claim.add_claimant_info(current_user) if current_user&.loa3?
 
       unless claim.save
         StatsD.increment("#{stats_key}.failure")
