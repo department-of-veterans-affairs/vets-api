8c8621023c6e569e7025209ea5f1633c3fe512b6
Sam Suddath <srsuddath@gmail.com>
2021-01-14 12:53:37 -0500

    Update tests

--- spec/lib/search_click_tracking/service_spec.rb
+++ spec/lib/search_click_tracking/service_spec.rb
@@ -12,25 +12,33 @@ describe SearchClickTracking::Service do
   let(:client_ip) { 'testIP' }
   let(:user_agent) { 'testUserAgent' }
 
+  # I need to get these tests to pass, but don't know how.
+  # Anyone who can provide support or assistance would be super appreciated.
+  # I have a feeling that the config/betamocks/services_config.yml plays a part in this.
+
   before do
-    allow_any_instance_of(described_class).to receive(:access_key).and_return(
-      'TESTKEY'
-    )
+    allow_any_instance_of(described_class).to receive(:access_key).and_return('TESTKEY')
   end
 
-  describe 'when successful ' do
+  describe 'when successful' do
     it 'returns a status of 200', :aggregate_failures do
-      VCR.use_cassette('searsearch_click_trackingch/success', VCR::MATCH_EVERYTHING) do
+      VCR.use_cassette('search_click_tracking/success', VCR::MATCH_EVERYTHING) do
         response = subject.track_click
         expect(response.status).to eq 200
       end
     end
   end
 
-  #need to mess up params here
-  describe 'error handling ' do
+  #need to mess up params here, which I think this does
+  let(:url) { '' }
+  let(:query) { '' }
+  let(:position) { '' }
+  let(:client_ip) { '' }
+  let(:user_agent) { '' }
+
+  describe 'error handling' do
     it 'raises a 400 exception' do
-      VCR.use_cassette('searsearch_click_trackingch/failure', VCR::MATCH_EVERYTHING) do
+      VCR.use_cassette('search_click_tracking/failure', VCR::MATCH_EVERYTHING) do
         expect { subject.track_click }.to raise_error do |e|
           expect(e).to be_a(Common::Exceptions::BackendServiceException)
           expect(e.status_code).to eq(400)
