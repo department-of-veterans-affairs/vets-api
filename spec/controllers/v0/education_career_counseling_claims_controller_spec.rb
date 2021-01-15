11ddbf0a00bc6e4ef394cc598bcc89a0167b585e
Derek Dyer <derek@digitalderek.com>
2021-01-15 08:34:23 -0500

    Add stuff

--- spec/controllers/v0/education_career_counseling_claims_controller_spec.rb
+++ spec/controllers/v0/education_career_counseling_claims_controller_spec.rb
@@ -7,50 +7,18 @@ RSpec.describe V0::EducationCareerCounselingClaimsController, type: :controller 
   let(:loa3_user) { create(:evss_user) }
   let(:loa1_user) { create(:user) }
 
-  let(:test_form_no_vet_info) do
-    build(:education_career_counseling_claim_no_vet_information)
-  end
-
   let(:test_form) do
     build(:education_career_counseling_claim)
-  end
-
-  let(:no_claimant_info) do
-    hash_copy = JSON.parse(
-      test_form_no_vet_info.form
-    )
-
-    hash_copy['claimantInformation']['fullName'] = nil
-
-    hash_copy.to_json
   end
 
   describe 'POST create' do
     context 'logged in loa3 user' do
       it 'validates successfully' do
         sign_in_as(loa3_user)
-        form_params = { education_career_counseling_claim: { form: no_claimant_info } }
+        form_params = { education_career_counseling_claim: { form: test_form.form } }
 
         post(:create, params: form_params)
         expect(response.code).to eq('200')
-      end
-    end
-
-    context 'logged in loa1 user' do
-      it 'validates successfully' do
-        sign_in_as(loa1_user)
-        form_params = { education_career_counseling_claim: { form: test_form_no_vet_info.form } }
-
-        post(:create, params: form_params)
-        expect(response.code).to eq('200')
-      end
-
-      it 'fails validation when no claimant info is sent' do
-        sign_in_as(loa1_user)
-        form_params = { education_career_counseling_claim: { form: no_claimant_info } }
-
-        post(:create, params: form_params)
-        expect(response.code).to eq('422')
       end
     end
 
