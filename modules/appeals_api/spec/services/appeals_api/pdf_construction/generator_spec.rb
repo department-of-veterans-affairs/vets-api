0b077324adf03f97459fe0d6ea485131d85f0646
Jack Schuss <jackschuss@gmail.com>
2021-01-15 11:11:20 -0500

    Structure spec and rubocop fixes

--- modules/appeals_api/spec/services/appeals_api/pdf_construction/generator_spec.rb
+++ modules/appeals_api/spec/services/appeals_api/pdf_construction/generator_spec.rb
@@ -5,11 +5,11 @@ require 'rails_helper'
 describe AppealsApi::PdfConstruction::Generator do
   let(:appeal) { create(:notice_of_disagreement) }
 
-  describe "#generate" do
-    it "returns a pdf path" do
+  describe '#generate' do
+    it 'returns a pdf path' do
       result = described_class.new(appeal).generate
 
-      expect(result[-4..-1]).to eq('.pdf')
+      expect(result[-4..]).to eq('.pdf')
     end
   end
 end
