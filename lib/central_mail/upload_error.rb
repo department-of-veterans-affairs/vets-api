834c774c5ae1720a2d99b81da315e48de601e0b7
vets-api <mark.viterna@va.gov>
2021-01-18 11:53:12 -0500

    Refactoring to correct verbiage around upstream versus downstream

--- lib/central_mail/upload_error.rb
+++ lib/central_mail/upload_error.rb
@@ -17,7 +17,7 @@ module CentralMail
     DOC107 = 'Empty payload'
     DOC108 = 'Maximum page size exceeded. Limit is 21 in x 21 in.'
 
-    # DOC2xx errors: server errors either local or downstream
+    # DOC2xx errors: server errors either local or upstream
     # not unambiguously related to submitted content
     DOC201 = 'Upload server error. Request will be retried when upstream service is available.'
     DOC202 = 'Error during processing by upstream system'
