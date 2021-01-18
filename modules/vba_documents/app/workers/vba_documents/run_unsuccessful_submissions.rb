7ff4cf96b0e31bbb06245d07aff067c2dad01a9b
Greg Bowman <gregory.bowman@libertyits.com>
2021-01-18 13:46:04 -0500

    lint changes

--- modules/vba_documents/app/workers/vba_documents/run_unsuccessful_submissions.rb
+++ modules/vba_documents/app/workers/vba_documents/run_unsuccessful_submissions.rb
@@ -10,7 +10,7 @@ module VBADocuments
       guids = VBADocuments::UploadSubmission.where(status: 'uploaded').pluck(:guid)
       guids.each do |guid|
         Rails.logger.info("Running VBADocuments::RunUnsuccessfulSubmissions for GUID #{guid}",
-                          {'job' => 'VBADocuments::RunUnsuccessfulSubmissions', guid: guid})
+                          { 'job' => 'VBADocuments::RunUnsuccessfulSubmissions', guid: guid })
         VBADocuments::UploadProcessor.perform_async(guid)
       end
     end
