6939eb2ce8c536e50861eaaf85db16ae67b6cfa0
Anna Carey <anna@adhocteam.us>
2021-01-19 16:45:49 -0500

    ready to document

--- spec/request/v0/disability_compensation_in_progress_forms_controller_request_spec.rb
+++ spec/request/v0/disability_compensation_in_progress_forms_controller_request_spec.rb
@@ -35,16 +35,62 @@ RSpec.describe V0::DisabilityCompensationInProgressFormsController, type: :reque
       end
 
       context 'when a form is found' do
+        let(:rated_disabilites_from_evss) do
+          [{ 'name' => 'Diabetes mellitus0',
+             'ratedDisabilityId' => '0',
+             'ratingDecisionId' => '63655',
+             'diagnosticCode' => 5238,
+             'decisionCode' => 'SVCCONNCTED',
+             'decisionText' => 'Service Connected',
+             'ratingPercentage' => 100 },
+           { 'name' => 'Diabetes mellitus1',
+             'ratedDisabilityId' => '1',
+             'ratingDecisionId' => '63655',
+             'diagnosticCode' => 5238,
+             'decisionCode' => 'SVCCONNCTED',
+             'decisionText' => 'Service Connected',
+             'ratingPercentage' => 100 }]
+        end
+
         it 'returns the form as JSON' do
           VCR.use_cassette('evss/disability_compensation_form/rated_disabilities') do
             get v0_disability_compensation_in_progress_form_url(in_progress_form.form_id), params: nil
           end
           expect(response).to have_http_status(:ok)
-          expect(JSON.parse(response.body)).to eq(
-            'formData' => JSON.parse(in_progress_form.form_data),
-            'metadata' => in_progress_form.metadata
-          )
+          json_response = JSON.parse(response.body)
+          expect(json_response['formData']['ratedDisabilities']).to eq(rated_disabilites_from_evss)
+          expect(json_response['metadata']['returnUrl']).to eq('/disabilities/rated-disabilities')
+          expect(json_response['metadata']['ratedDisabilitiesUpdated']).to be_truthy
         end
+      end
+    end
+
+    describe '#index' do
+      subject do
+        get v0_disability_compensation_in_progress_forms_url, params: nil
+      end
+
+      let(:user) { loa3_user }
+
+      it 'returns a 200' do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    describe '#update' do
+      let(:user) { loa3_user }
+      let(:new_form) { FactoryBot.build(:in_progress_form, user_uuid: user.uuid) }
+
+      it 'inserts the form', run_at: '2017-01-01' do
+        expect do
+          put v0_disability_compensation_in_progress_form_url(new_form.form_id), params: {
+            formData: new_form.form_data,
+            metadata: new_form.metadata
+          }.to_json, headers: { 'CONTENT_TYPE' => 'application/json' }
+        end.to change(InProgressForm, :count).by(1)
+
+        expect(response).to have_http_status(:ok)
       end
     end
   end
