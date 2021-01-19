06a54effd20b509850d0242bc45517adc53c365e
Braden Shipley <bradenrshipley@gmail.com>
2021-01-19 10:27:18 -0600

    removed unnecessary attr_reader

--- modules/apps_api/app/models/directory_application.rb
+++ modules/apps_api/app/models/directory_application.rb
@@ -3,5 +3,4 @@
 class DirectoryApplication < ApplicationRecord
   validates :name, :logo_url, :app_type, :service_categories, :platforms,
             :app_url, :description, :privacy_url, :tos_url, presence: true
-  attr_reader :privacy_url
 end
