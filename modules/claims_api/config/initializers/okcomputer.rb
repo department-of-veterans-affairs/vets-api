90f932e238e5347dc15e40b6de3148e6391c4b90
Lee DeBoom <lee.deboom@oddball.io>
2021-01-20 12:29:21 -0600

    dry things up a bit

--- modules/claims_api/config/initializers/okcomputer.rb
+++ modules/claims_api/config/initializers/okcomputer.rb
@@ -9,33 +9,41 @@ OkComputer.check_in_parallel = true
 
 class EvssCheck < OkComputer::Check
   def check
-    if Settings.evss.mock_claims || EVSS::Service.service_is_up?
-      mark_message 'EVSS is running'
-    else
-      mark_failure
-      mark_message 'EVSS is unavailable'
-    end
+    Settings.evss.mock_claims || EVSS::Service.service_is_up? ? process_success : process_failure
   rescue
+    process_failure
+  end
+
+  private
+
+  def process_success
+    mark_message 'EVSS is running'
+  end
+
+  def process_failure
     mark_failure
     mark_message 'EVSS is unavailable'
   end
 end
-OkComputer::Registry.register 'evss', EvssCheck.new
 
 class MpiCheck < OkComputer::Check
   def check
-    if Settings.mvi.mock || MPI::Service.service_is_up?
-      mark_message 'MPI is running'
-    else
-      mark_failure
-      mark_message 'MPI is unavailable'
-    end
+    Settings.mvi.mock || MPI::Service.service_is_up? ? process_success : process_failure
   rescue
+    process_failure
+  end
+
+  private
+
+  def process_success
+    mark_message 'MPI is running'
+  end
+
+  def process_failure
     mark_failure
     mark_message 'MPI is unavailable'
   end
 end
-OkComputer::Registry.register 'mpi', MpiCheck.new
 
 class BgsCheck < OkComputer::Check
   def check
@@ -43,33 +51,44 @@ class BgsCheck < OkComputer::Check
       external_uid: 'healthcheck_uid',
       external_key: 'healthcheck_key'
     )
-
-    if service.vet_record.healthy?
-      mark_message 'BGS is running'
-    else
-      mark_failure
-      mark_message 'BGS is unavailable'
-    end
+    service.vet_record.healthy? ? process_success : process_failure
   rescue
+    process_failure
+  end
+
+  private
+
+  def process_success
+    mark_message 'BGS is running'
+  end
+
+  def process_failure
     mark_failure
     mark_message 'BGS is unavailable'
   end
 end
-OkComputer::Registry.register 'bgs', BgsCheck.new
 
 class VbmsCheck < OkComputer::Check
   def check
     response = Faraday::Connection.new.get(Settings.vbms.url) { |request| request.options.timeout = 20 }
-
-    if response.status == 200
-      mark_message 'VBMS is running'
-    else
-      mark_failure
-      mark_message 'VBMS is unavailable'
-    end
+    response.status == 200 ? process_success : process_failure
   rescue
+    process_failure
+  end
+
+  private
+
+  def process_success
+    mark_message 'VBMS is running'
+  end
+
+  def process_failure
     mark_failure
     mark_message 'VBMS is unavailable'
   end
 end
+
+OkComputer::Registry.register 'evss', EvssCheck.new
+OkComputer::Registry.register 'mpi', MpiCheck.new
+OkComputer::Registry.register 'bgs', BgsCheck.new
 OkComputer::Registry.register 'vbms', VbmsCheck.new
