b68b444f6057dd95e98b24a53b1526de141f2d4f
Lee DeBoom <lee.deboom@oddball.io>
2021-01-19 18:15:54 -0600

    dont actually need this

--- modules/claims_api/app/controllers/claims_api/upstream_healthcheck_controller.rb
+++ modules/claims_api/app/controllers/claims_api/upstream_healthcheck_controller.rb
@@ -2,14 +2,5 @@
 
 module ClaimsApi
   class UpstreamHealthcheckController < ::OkComputer::OkComputerController
-    skip_before_action :verify_authenticity_token
-    skip_before_action(:authenticate)
-
-    def index
-      checks = OkComputer::Registry.all
-      checks.run
-
-      respond checks, status_code(checks)
-    end
   end
 end
