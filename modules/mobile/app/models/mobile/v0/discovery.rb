8687337b591d6916daff1c6d4d43e43e0c0407e1
Jayson Perkins <jaysonperkins51@gmail.com>
2021-01-20 18:17:44 -0500

    lint fixes

--- modules/mobile/app/models/mobile/v0/discovery.rb
+++ modules/mobile/app/models/mobile/v0/discovery.rb
@@ -6,134 +6,134 @@ module Mobile
       attr_accessor :id, :webviews, :endpoints, :display_message, :app_access, :auth_base_url, :api_root_url
 
       API_VERSION_MAP = {
-          deprecated: {
-              min_build_number: {
-                  ios: 0,
-                  android: 0
-              },
-              max_build_number: {
-                  ios: 19,
-                  android: 19
-              },
-              api_version: 'deprecated',
-              webviews: {
-              },
-              endpoints: {
-              },
-              display_message: 'Please update the app.',
-              app_access: false,
-              auth_base_url: {},
-              api_root_url: {}
+        deprecated: {
+          min_build_number: {
+            ios: 0,
+            android: 0
           },
-          one_zero: {
-              min_build_number: {
-                  ios: 20,
-                  android: 20
-              },
-              max_build_number: {
-                  ios: 999,
-                  android: 999
-              },
-              api_version: '1.0',
-              webviews: {
-                  corona_FAQ: 'https://www.va.gov/coronavirus-veteran-frequently-asked-questions',
-                  facility_locator: 'https://www.va.gov/find-locations/'
-              },
-              endpoints: {
-                  appeal_details: {
-                      url: '/appeal/:id',
-                      method: 'GET'
-                  },
-                  appointments: {
-                      url: '/appointments',
-                      method: 'GET'
-                  },
-                  claims_overview: {
-                      url: '/claims-and-appeals-overview',
-                      method: 'GET'
-                  },
-                  claim_details: {
-                      url: '/claim/:id',
-                      method: 'GET'
-                  },
-                  upload_claim_documents: {
-                      url: '/claim/:id/documents',
-                      method: 'POST'
-                  },
-                  available_letters: {
-                      url: '/letters',
-                      method: 'GET'
-                  },
-                  letters_beneficiary: {
-                      url: '/letters/beneficiary',
-                      method: 'GET'
-                  },
-                  letters_download: {
-                      url: '/letters/:type/download',
-                      method: 'POST'
-                  },
-                  service_history: {
-                      url: '/military-service-history',
-                      method: 'GET'
-                  },
-                  get_payment_info: {
-                      url: '/payment-information/benefits',
-                      method: 'GET'
-                  },
-                  update_payment_info: {
-                      url: '/payment-information/benefits',
-                      method: 'PUT'
-                  },
-                  user: {
-                      url: '/user',
-                      method: 'GET'
-                  },
-                  logout: {
-                      url: '/user/logout',
-                      method: 'GET'
-                  },
-                  create_user_addresses: {
-                      url: '/user/addresses',
-                      method: 'POST'
-                  },
-                  update_user_addresses: {
-                      url: '/user/addresses',
-                      method: 'PUT'
-                  },
-                  user_address_validation: {
-                      url: '/user/addresses/validate',
-                      method: 'POST'
-                  },
-                  create_user_email: {
-                      url: '/user/emails',
-                      method: 'POST'
-                  },
-                  update_user_email: {
-                      url: '/user/emails',
-                      method: 'PUT'
-                  },
-                  create_user_phone: {
-                      url: '/user/phones',
-                      method: 'POST'
-                  },
-                  update_user_phone: {
-                      url: '/user/phones',
-                      method: 'PUT'
-                  }
-              },
-              display_message: '',
-              app_access: true,
-              auth_base_url: {
-                  dev: 'https://sqa.fed.eauth.va.gov/oauthe/sps/oauth/oauth20/',
-                  staging: 'https://int.fed.eauth.va.gov/oauthe/sps/oauth/oauth20/',
-                  prod: 'https://fed.eauth.va.gov/oauthe/sps/oauth/oauth20/'
-              },
-              api_root_url: {
-                  dev: 'https://staging-api.va.gov/mobile',
-                  staging: 'https://staging-api.va.gov/mobile',
-                  prod: 'https://api.va.gov/mobile'
-              }
+          max_build_number: {
+            ios: 19,
+            android: 19
+          },
+          api_version: 'deprecated',
+          webviews: {
+          },
+          endpoints: {
+          },
+          display_message: 'Please update the app.',
+          app_access: false,
+          auth_base_url: {},
+          api_root_url: {}
+        },
+        one_zero: {
+          min_build_number: {
+            ios: 20,
+            android: 20
+          },
+          max_build_number: {
+            ios: 999,
+            android: 999
+          },
+          api_version: '1.0',
+          webviews: {
+            corona_FAQ: 'https://www.va.gov/coronavirus-veteran-frequently-asked-questions',
+            facility_locator: 'https://www.va.gov/find-locations/'
+          },
+          endpoints: {
+            appeal_details: {
+              url: '/appeal/:id',
+              method: 'GET'
+            },
+            appointments: {
+              url: '/appointments',
+              method: 'GET'
+            },
+            claims_overview: {
+              url: '/claims-and-appeals-overview',
+              method: 'GET'
+            },
+            claim_details: {
+              url: '/claim/:id',
+              method: 'GET'
+            },
+            upload_claim_documents: {
+              url: '/claim/:id/documents',
+              method: 'POST'
+            },
+            available_letters: {
+              url: '/letters',
+              method: 'GET'
+            },
+            letters_beneficiary: {
+              url: '/letters/beneficiary',
+              method: 'GET'
+            },
+            letters_download: {
+              url: '/letters/:type/download',
+              method: 'POST'
+            },
+            service_history: {
+              url: '/military-service-history',
+              method: 'GET'
+            },
+            get_payment_info: {
+              url: '/payment-information/benefits',
+              method: 'GET'
+            },
+            update_payment_info: {
+              url: '/payment-information/benefits',
+              method: 'PUT'
+            },
+            user: {
+              url: '/user',
+              method: 'GET'
+            },
+            logout: {
+              url: '/user/logout',
+              method: 'GET'
+            },
+            create_user_addresses: {
+              url: '/user/addresses',
+              method: 'POST'
+            },
+            update_user_addresses: {
+              url: '/user/addresses',
+              method: 'PUT'
+            },
+            user_address_validation: {
+              url: '/user/addresses/validate',
+              method: 'POST'
+            },
+            create_user_email: {
+              url: '/user/emails',
+              method: 'POST'
+            },
+            update_user_email: {
+              url: '/user/emails',
+              method: 'PUT'
+            },
+            create_user_phone: {
+              url: '/user/phones',
+              method: 'POST'
+            },
+            update_user_phone: {
+              url: '/user/phones',
+              method: 'PUT'
+            }
+          },
+          display_message: '',
+          app_access: true,
+          auth_base_url: {
+            dev: 'https://sqa.fed.eauth.va.gov/oauthe/sps/oauth/oauth20/',
+            staging: 'https://int.fed.eauth.va.gov/oauthe/sps/oauth/oauth20/',
+            prod: 'https://fed.eauth.va.gov/oauthe/sps/oauth/oauth20/'
+          },
+          api_root_url: {
+            dev: 'https://staging-api.va.gov/mobile',
+            staging: 'https://staging-api.va.gov/mobile',
+            prod: 'https://api.va.gov/mobile'
           }
+        }
       }.freeze
 
       def initialize(params)
@@ -152,7 +152,7 @@ module Mobile
       def get_api_version(build_number, os)
         API_VERSION_MAP.each do |_version, values|
           if get_os_build_number(values[:max_build_number], os) >= build_number &&
-              get_os_build_number(values[:min_build_number], os) <= build_number
+             get_os_build_number(values[:min_build_number], os) <= build_number
             return values
           end
         end
