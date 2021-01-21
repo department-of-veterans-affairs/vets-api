69c92221f2bfdc9381e7726277325f18ae581da3
Philip Becker <boofungal316@yahoo.com>
2021-01-21 10:33:24 -0500

    test is simple enough to not need a context

--- spec/lib/generators/provider_state/provider_state_generator_spec.rb
+++ spec/lib/generators/provider_state/provider_state_generator_spec.rb
@@ -5,14 +5,10 @@ require 'generators/provider_state/provider_state_generator'
 
 describe ProviderStateGenerator do
   describe 'create_provider_state_file' do
-    context 'once generated' do
-      before(:all) { ProviderStateGenerator.new(['test']).create_provider_state_file }
-
-      after(:all) { FileUtils.rm_rf('spec/service_consumers/provider_states_for/test.rb') }
-
-      it 'creates the pact provider state file for a given consumer' do
-        expect(File).to exist('spec/service_consumers/provider_states_for/test.rb')
-      end
+    it 'creates the pact provider state file for a given consumer' do
+      ProviderStateGenerator.new(['test']).create_provider_state_file
+      expect(File).to exist('spec/service_consumers/provider_states_for/test.rb')
+      FileUtils.rm_rf('spec/service_consumers/provider_states_for/test.rb')
     end
   end
 end
