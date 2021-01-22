246767cce167812dfdb40a064b30798eeec368e2
John Bramley <bramleyjl@gmail.com>
2021-01-22 17:08:57 -0500

    adds create_test_user_account_spec initialize specs

--- modules/test_user_dashboard/spec/services/create_test_user_account_spec.rb
+++ modules/test_user_dashboard/spec/services/create_test_user_account_spec.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 describe TestUserDashboard::CreateTestUserAccount do
+  subject { described_class.new }
+
+  describe '#initialize' do
+    it 'has a test_user_account attribute' do
+      expect(subject.respond_to?(:test_user_account)).to eq(true)
+    end
+
+    it 'its test_user_account is a TudAccount' do
+      expect(subject.test_user_account).to be_a(TestUserDashboard::TudAccount)
+    end
+  end
 end
