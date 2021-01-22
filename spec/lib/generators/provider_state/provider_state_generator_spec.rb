1d8b42622ad4cf6ce95123b6139b679ceeb4ca74
Philip Becker <boofungal316@yahoo.com>
2021-01-22 08:32:58 -0500

    test output and cleanup file removal

--- spec/lib/generators/provider_state/provider_state_generator_spec.rb
+++ spec/lib/generators/provider_state/provider_state_generator_spec.rb
@@ -5,10 +5,16 @@ require 'generators/provider_state/provider_state_generator'
 
 describe ProviderStateGenerator do
   describe 'create_provider_state_file' do
+    after do
+      test_pact_file = 'spec/service_consumers/provider_states_for/test.rb'
+      FileUtils.rm_rf(test_pact_file) if File.exist?(test_pact_file)
+    end
+
     it 'creates the pact provider state file for a given consumer' do
-      ProviderStateGenerator.new(['test']).create_provider_state_file
+      expect do
+        ProviderStateGenerator.new(['test']).create_provider_state_file
+      end.to output("      create  spec/service_consumers/provider_states_for/test.rb\n").to_stdout
       expect(File).to exist('spec/service_consumers/provider_states_for/test.rb')
-      FileUtils.rm_rf('spec/service_consumers/provider_states_for/test.rb')
     end
   end
 end
