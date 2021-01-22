a762ed7b10849142ed82f0c1cc38b0d9e02b1507
Philip Becker <boofungal316@yahoo.com>
2021-01-22 12:03:54 -0500

    remove context blocks

--- spec/lib/generators/module/module_generator_spec.rb
+++ spec/lib/generators/module/module_generator_spec.rb
@@ -23,38 +23,35 @@ describe ModuleGenerator do
   end
 
   describe 'create_engine' do
-    context 'once generated' do
-      before do
-        create_message = <<MESSAGES
+    before do
+      create_message = <<MESSAGES
       create  modules/foo/lib/foo/engine.rb
       create  modules/foo/lib/foo/version.rb
       create  modules/foo/lib/foo.rb
 MESSAGES
-        expect do
-          ModuleGenerator.new([module_name]).create_engine
-        end.to output(create_message).to_stdout
-      end
+      expect do
+        ModuleGenerator.new([module_name]).create_engine
+      end.to output(create_message).to_stdout
+    end
 
-      let(:path) { Rails.root.join('modules', module_name, 'lib') }
+    let(:path) { Rails.root.join('modules', module_name, 'lib') }
 
-      it 'creates the engine file' do
-        expect(File).to exist("#{path}/foo/engine.rb")
-      end
+    it 'creates the engine file' do
+      expect(File).to exist("#{path}/foo/engine.rb")
+    end
 
-      it 'creates the version file' do
-        expect(File).to exist("#{path}/foo/version.rb")
-      end
+    it 'creates the version file' do
+      expect(File).to exist("#{path}/foo/version.rb")
+    end
 
-      it 'creates the module file' do
-        expect(File).to exist("#{path}/foo.rb")
-      end
+    it 'creates the module file' do
+      expect(File).to exist("#{path}/foo.rb")
     end
   end
 
   describe 'create_additional_files' do
-    context 'once generated' do
-      before do
-        create_message = <<MESSAGES
+    before do
+      create_message = <<MESSAGES
       create  modules/foo/Rakefile
       create  modules/foo/README.rdoc
       create  modules/foo/bin/rails
@@ -64,40 +61,39 @@ MESSAGES
       create  modules/foo/foo.gemspec
       create  modules/foo/Gemfile
 MESSAGES
-        expect do
-          ModuleGenerator.new([module_name]).create_additional_files
-        end.to output(create_message).to_stdout
-      end
+      expect do
+        ModuleGenerator.new([module_name]).create_additional_files
+      end.to output(create_message).to_stdout
+    end
 
-      let(:path) { Rails.root.join('modules', module_name) }
+    let(:path) { Rails.root.join('modules', module_name) }
 
-      it 'creates the rakefile' do
-        expect(File).to exist("#{path}/Rakefile")
-      end
+    it 'creates the rakefile' do
+      expect(File).to exist("#{path}/Rakefile")
+    end
 
-      it 'creates the readme' do
-        expect(File).to exist("#{path}/README.rdoc")
-      end
+    it 'creates the readme' do
+      expect(File).to exist("#{path}/README.rdoc")
+    end
 
-      it 'creates the rails file' do
-        expect(File).to exist("#{path}/bin/rails")
-      end
+    it 'creates the rails file' do
+      expect(File).to exist("#{path}/bin/rails")
+    end
 
-      it 'creates the spec_helper' do
-        expect(File).to exist("#{path}/spec/spec_helper.rb")
-      end
+    it 'creates the spec_helper' do
+      expect(File).to exist("#{path}/spec/spec_helper.rb")
+    end
 
-      it 'creates the routes file' do
-        expect(File).to exist("#{path}/config/routes.rb")
-      end
+    it 'creates the routes file' do
+      expect(File).to exist("#{path}/config/routes.rb")
+    end
 
-      it 'creates the gemspec' do
-        expect(File).to exist("#{path}/foo.gemspec")
-      end
+    it 'creates the gemspec' do
+      expect(File).to exist("#{path}/foo.gemspec")
+    end
 
-      it 'creates the Gemfile' do
-        expect(File).to exist("#{path}/Gemfile")
-      end
+    it 'creates the Gemfile' do
+      expect(File).to exist("#{path}/Gemfile")
     end
   end
 end
