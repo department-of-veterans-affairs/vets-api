717846da6799a46cc99cb75e1005e7d848467215
camerontesterman <cameronbtesterman@gmail.com>
2021-01-25 15:28:23 -0600

    add tests for downloader

--- lib/debt_management_center/financial_status_report_downloader.rb
+++ lib/debt_management_center/financial_status_report_downloader.rb
@@ -9,6 +9,21 @@ module DebtManagementCenter
       @financial_status_report = financial_status_report
     end
 
+    def download_pdf
+      uri = URI.parse(
+        "#{Settings.dmc.url}financial-status-report/documentstream?objectId=#{@financial_status_report.filenet_id}"
+      )
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri.request_uri)
+      http.use_ssl = true
+
+      request_headers.each { |header, value| request[header] = value }
+      response = http.request(request)
+      response.body
+    end
+
+    private
+
     def base_url
       Settings.dmc.url
     end
@@ -19,18 +34,6 @@ module DebtManagementCenter
         client_id: Settings.dmc.client_id,
         client_secret: Settings.dmc.client_secret
       }
-    end
-
-    def download_pdf
-      uri = URI.parse(
-        "#{Settings.dmc.url}financial-status-report/documentstream?objectId=#{@financial_status_report.filenet_id}"
-      )
-      http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Get.new(uri.request_uri)
-
-      request_headers.each { |header, value| request[header] = value }
-      response = http.request(request)
-      response.body
     end
   end
 end
