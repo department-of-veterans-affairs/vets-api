db41ec3fbdb049a4d1d7dc9cefa16ebe6789f002
Lance Sanchez <darkbushido@users.noreply.github.com>
2021-01-25 12:48:26 -0600

    Add labels to StatsD measurement for radius and number of results (#5750)
    * Adding additional tags to StatsD
    
    To better understand PPMS performance issues we want to measure
    the relationship between search area radius and the number of results
    returned.

--- spec/request/v1/facilities/va_ccp_request_spec.rb
+++ spec/request/v1/facilities/va_ccp_request_spec.rb
@@ -29,7 +29,13 @@ RSpec.describe 'VA and Community Care Mashup', type: :request, team: :facilities
         expect(StatsD).to receive(:measure).with(
           'facilities.ppms.place_of_service_locator',
           kind_of(Numeric),
-          hash_including(tags: ['facilities.ppms'])
+          hash_including(
+            tags: [
+              'facilities.ppms',
+              'facilities.ppms.radius:200',
+              'facilities.ppms.results:11'
+            ]
+          )
         )
 
         expect do
