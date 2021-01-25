db232dad922eb800aeef03a453bc0557b6220e5e
John Bramley <bramleyjl@gmail.com>
2021-01-25 12:30:47 -0500

    adds spec_users.csv and additional tud_account service spec

--- modules/test_user_dashboard/spec/services/create_test_user_account_spec.rb
+++ modules/test_user_dashboard/spec/services/create_test_user_account_spec.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 describe TestUserDashboard::CreateTestUserAccount do
   subject { described_class.new }
+
+  file_path = Rails.root.join('modules', 'test_user_dashboard', 'spec', 'support', 'spec_users.csv')
+  spec_users = CSV.read(file_path, headers: true)
 
   describe '#initialize' do
     it 'has a test_user_account attribute' do
@@ -10,6 +15,13 @@ describe TestUserDashboard::CreateTestUserAccount do
 
     it 'its test_user_account is a TudAccount' do
       expect(subject.test_user_account).to be_a(TestUserDashboard::TudAccount)
+    end
+  end
+
+  describe '#call' do
+    it 'saves a new test user account' do
+      expect { ::TestUserDashboard::CreateTestUserAccount.new(spec_users[0]).call }
+        .to change(TestUserDashboard::TudAccount, :count).by(1)
     end
   end
 end
