bfee0275b2bc8315b25905d5f4f81be4020fd4bf
Dillo Raju <draju@governmentcio.com>
2021-01-25 17:43:05 -0500

    GET appointment by ID

--- modules/health_quest/spec/services/appointment_service_spec.rb
+++ modules/health_quest/spec/services/appointment_service_spec.rb
@@ -14,6 +14,12 @@ describe HealthQuest::AppointmentService do
 
   describe '#get_appointment_by_id' do
     let(:appointment) { subject.get_appointment_by_id(132) }
+    let(:appt_body) { subject.mock_appointment }
+    let(:appt_response) { double('Faraday::Response', body: appt_body) }
+
+    before do
+      allow_any_instance_of(HealthQuest::AppointmentService).to receive(:perform).and_return(appt_response)
+    end
 
     it 'is a Hash' do
       expect(appointment).to be_a(Hash)
@@ -24,7 +30,7 @@ describe HealthQuest::AppointmentService do
     end
 
     it 'data openstruct object has expected keys' do
-      data = appointment[:data]
+      data = appointment[:data].data
       attributes = %i[
         id start_date clinic_id clinic_friendly_name facility_id sta6aid
         station_name patient_icn community_care vds_appointments
@@ -33,6 +39,28 @@ describe HealthQuest::AppointmentService do
       attributes.each do |attr|
         expect(data.respond_to?(attr)).to be(true)
       end
+    end
+  end
+
+  describe '#get_appointments' do
+    let(:appointments) { subject.get_appointments(Time.zone.now, Time.zone.now) }
+    let(:appt_body) { { data: { appointment_list: [subject.mock_appointment] } } }
+    let(:appt_response) { double('Faraday::Response', body: appt_body) }
+
+    before do
+      allow_any_instance_of(HealthQuest::AppointmentService).to receive(:perform).and_return(appt_response)
+    end
+
+    it 'is a Hash' do
+      expect(appointments).to be_a(Hash)
+    end
+
+    it 'has an appointments array' do
+      expect(appointments[:data]).to be_a(Array)
+    end
+
+    it 'has a data type of OpenStruct' do
+      expect(appointments[:data].first).to be_a(OpenStruct)
     end
   end
 end
