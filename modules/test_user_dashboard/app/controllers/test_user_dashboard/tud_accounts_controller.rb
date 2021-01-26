31878bd70c942705baffcc44071abdbb7a8a5ee4
John Bramley <bramleyjl@gmail.com>
2021-01-26 15:16:08 -0500

    removes unused helper & syntax fixes

--- modules/test_user_dashboard/app/controllers/test_user_dashboard/tud_accounts_controller.rb
+++ modules/test_user_dashboard/app/controllers/test_user_dashboard/tud_accounts_controller.rb
@@ -6,9 +6,7 @@ module TestUserDashboard
   class TudAccountsController < ApplicationController
     def index
       tud_accounts = TudAccount.all
-      render(
-        json: tud_accounts
-      )
+      render json: tud_accounts
     end
   end
 end
