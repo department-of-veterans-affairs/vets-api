b8e7a0e3bd17130fdd26b42082d501dac280d2bd
Ed Mangimelli <edmangimelli@gmail.com>
2021-01-27 14:06:47 -0500

    log failed HLR-related request to the PII log for debugging purposes

--- app/controllers/appeals_base_controller.rb
+++ app/controllers/appeals_base_controller.rb
@@ -7,6 +7,19 @@ class AppealsBaseController < ApplicationController
   include ActionController::Serialization
   before_action { authorize :appeals, :access? }
 
+  class << self
+    def exception_hash(exception)
+      %i[
+        message
+        backtrace
+        key
+        response_values
+        original_status
+        original_body
+      ].reduce({}) { |hash, key| hash.merge({ key => exception.try(key) }) }
+    end
+  end
+
   private
 
   def appeals_service
@@ -35,5 +48,46 @@ class AppealsBaseController < ApplicationController
 
   def request_body_is_not_a_hash_error
     DecisionReview::ServiceException.new key: 'DR_REQUEST_BODY_IS_NOT_A_HASH'
+  end
+
+  def request_body_debug_data
+    {
+      request_body_class_name: request.try(:body).class.name,
+      request_body_string: request.try(:body).try(:string)
+    }
+  end
+
+  def current_user_hash
+    hash = {}
+
+    %i[
+      first_name
+      last_name
+      birls_id
+      icn
+      edipi
+      mhv_correlation_id
+      participant_id
+      vet360_id
+      ssn
+    ].each { |key| hash[key] = @current_user.try(key) }
+
+    hash[:assurance_level] = @current_user.try(:loa)&.dig(:current)&.to_s
+    hash[:birth_date] = begin
+                          @current_user.va_profile.birth_date.to_date.iso8601
+                        rescue
+                          nil
+                        end
+    hash
+  end
+
+  def log_exception_to_personal_information_log(exception, error_class:, data: {})
+    PersonalInformationLog.create!(
+      error_class: error_class,
+      data: {
+        user: current_user_hash,
+        error: self.class.exception_hash(exception)
+      }.merge(data)
+    )
   end
 end
--- app/controllers/v0/higher_level_reviews_controller.rb
+++ app/controllers/v0/higher_level_reviews_controller.rb
@@ -4,12 +4,32 @@ module V0
   class HigherLevelReviewsController < AppealsBaseController
     def show
       render json: decision_review_service.get_higher_level_review(params[:id]).body
+    rescue => e
+      log_exception_to_personal_information_log(
+        e,
+        error_class: "#{self.class.name}#show exception",
+        data: { request_data: { id: params[:id] } }
+      )
+      raise
     end
 
     def create
       render json: decision_review_service
         .create_higher_level_review(request_body: request_body_hash, user: current_user)
         .body
+    rescue => e
+      request_data = begin
+                       { body: request_body_hash }
+                     rescue
+                       request_body_debug_data
+                     end
+
+      log_exception_to_personal_information_log(
+        e,
+        error_class: "#{self.class.name}#create exception",
+        data: { request_data: request_data }
+      )
+      raise
     end
   end
 end
