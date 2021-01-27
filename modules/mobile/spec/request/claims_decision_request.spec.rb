255e33804f0cbf2b5ad784dff8a8c5be0c36e059
Jayson Perkins <jaysonperkins51@gmail.com>
2021-01-27 03:39:27 -0500

    lint fix

--- modules/mobile/spec/request/claims_decision_request.spec.rb
+++ modules/mobile/spec/request/claims_decision_request.spec.rb
@@ -10,16 +10,15 @@ RSpec.describe 'claims decision request', type: :request do
       FactoryBot.create(:evss_claim, id: 1, evss_id: 600_117_255, user_uuid: '3097e489-ad75-5746-ab1a-e0aabc1b426a')
     end
 
-    it 'should return jid with 202 status' do
+    it 'returns jid with 202 status' do
       post '/mobile/v0/claim/600117255/request-decision', headers: iam_headers
       expect(response.status).to eq(202)
       expect(response.parsed_body.dig('data', 'jobId')).to eq(EVSS::RequestDecision.jobs.first['jid'])
     end
 
-    it 'should return 500 for non-existent record' do
+    it 'returns 500 for non-existent record' do
       post '/mobile/v0/claim/3242233/request-decision', headers: iam_headers
       expect(response.status).to eq(500)
     end
   end
 end
-
