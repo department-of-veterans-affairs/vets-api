e792bbd1e52ceaf84d4ffc9314e02be30c00e6ce
Jack Schuss <jackschuss@gmail.com>
2021-01-28 12:25:07 -0500

    WIP

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+  config.cache_classes = false
   config.action_view.cache_template_loading = true
 
   # Do not eager load code on boot. This avoids loading your whole application
