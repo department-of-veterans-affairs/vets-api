e0b516c1f1ace76f182a8358842c4a9cd81583d1
Ed Mangimelli <38672641+edmangimelli@users.noreply.github.com>
2021-01-29 09:35:32 -0500

    Log Failed HLR-related Requests to the PII Log (#5783)
    * log failed HLR-related request to the PII log for debugging purposes
    
    * add more specs
    
    * rubocopo
    
    * more specs
    
    * rubocop
    
    * use concern for PII logging related methods
    
    * use 'additional_data' field instead of merge to avoid clobbering of user and error fields

--- app/controllers/v0/higher_level_reviews_controller.rb
+++ app/controllers/v0/higher_level_reviews_controller.rb
@@ -4,12 +4,24 @@ module V0
   class HigherLevelReviewsController < AppealsBaseController
     def show
       render json: decision_review_service.get_higher_level_review(params[:id]).body
+    rescue => e
+      log_exception_to_personal_information_log e, error_class: "#{self.class.name}#show exception", id: params[:id]
+      raise
     end
 
     def create
       render json: decision_review_service
         .create_higher_level_review(request_body: request_body_hash, user: current_user)
         .body
+    rescue => e
+      request = begin
+                  { body: request_body_hash }
+                rescue
+                  request_body_debug_data
+                end
+
+      log_exception_to_personal_information_log e, error_class: "#{self.class.name}#create exception", request: request
+      raise
     end
   end
 end
