dc297573ed7a0846602db83ae19fc347ce820005
John Bramley <bramleyjl@gmail.com>
2021-02-01 16:50:23 -0500

    adds first_name, last_name, email, gender, available filter methods

--- modules/test_user_dashboard/app/controllers/test_user_dashboard/tud_accounts_controller.rb
+++ modules/test_user_dashboard/app/controllers/test_user_dashboard/tud_accounts_controller.rb
@@ -5,8 +5,15 @@ require_dependency 'test_user_dashboard/application_controller'
 module TestUserDashboard
   class TudAccountsController < ApplicationController
     def index
-      tud_accounts = TudAccount.all
-      render json: tud_accounts
+      @tud_accounts = TudAccount.where(nil)
+      @tud_accounts = @tud_accounts.filter_by_first_name(params[:first_name]) if params[:first_name].present?
+      @tud_accounts = @tud_accounts.filter_by_last_name(params[:last_name]) if params[:last_name].present?
+      @tud_accounts = @tud_accounts.filter_by_email(params[:email]) if params[:email].present?
+      @tud_accounts = @tud_accounts.filter_by_gender(params[:gender]) if params[:gender].present?
+      @tud_accounts = @tud_accounts.filter_by_available(params[:available]) if params[:available].present?
+
+      # tud_accounts = TudAccount.all
+      render json: @tud_accounts
     end
   end
 end
--- modules/test_user_dashboard/app/models/test_user_dashboard/tud_account.rb
+++ modules/test_user_dashboard/app/models/test_user_dashboard/tud_account.rb
@@ -6,6 +6,16 @@ module TestUserDashboard
 
     validates :first_name, :last_name, :email, :gender, presence: true
 
+    scope :filter_by_first_name, ->(first_name) { where('first_name like ?', "#{first_name}%") }
+
+    scope :filter_by_last_name, ->(last_name) { where('last_name like ?', "#{last_name}%") }
+
+    scope :filter_by_email, ->(email) { where('email like ?', "#{email}%") }
+
+    scope :filter_by_gender, ->(gender) { where gender: gender }
+
+    scope :filter_by_available, ->(available) { where available: available }
+
     # uncomment when adding id_provider column information
     # validates :id_provider, presence: true
     # validates :id_provider, inclusion: { in: ID_PROVIDERS }
