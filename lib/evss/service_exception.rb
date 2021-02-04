3c3d0dddbda9ea02dbd550c010d734e7508322cb
Ed Mangimelli <edmangimelli@gmail.com>
2021-02-04 00:52:57 -0500

    fix typo

--- lib/evss/service_exception.rb
+++ lib/evss/service_exception.rb
@@ -17,7 +17,7 @@ module EVSS
     def error_key
       # in case of multiple errors, the ordering of ERROR_MAP is used to decided which error is highest priority.
       # NOTE: The ordering of ERROR_MAP takes precedence over both 1) how the error messages were ordered in the
-      # EVSS response and 2) the "severity" key of those messages.
+      # EVSS response and 2) the "severity" keys of those messages.
       key = self.class::ERROR_MAP.select { |k, _v| messages_has_key?(k) }
       return key.values.first unless key.empty?
 
