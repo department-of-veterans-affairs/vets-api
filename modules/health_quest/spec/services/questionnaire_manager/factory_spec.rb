3b580d43f8bea152aacb5ad52ee0248251c01400
Dillo Raju <dilloshion@gmail.com>
2021-02-04 18:12:37 -0500

    Questionnaire Manager get appointments (#5854)

--- modules/health_quest/spec/services/questionnaire_manager/factory_spec.rb
+++ modules/health_quest/spec/services/questionnaire_manager/factory_spec.rb
@@ -5,10 +5,11 @@ require 'rails_helper'
 describe HealthQuest::QuestionnaireManager::Factory do
   subject { described_class }
 
-  let(:user) { double('User', icn: '1008596379V859838') }
+  let(:user) { double('User', icn: '1008596379V859838', account_uuid: 'abc123') }
   let(:session_store) { double('SessionStore', token: '123abc') }
   let(:session_service) { double('HealthQuest::Lighthouse::Session', user: user, retrieve: session_store) }
   let(:client_reply) { double('FHIR::ClientReply') }
+  let(:default_appointments) { { data: [] } }
 
   before do
     allow(HealthQuest::Lighthouse::Session).to receive(:build).with(user).and_return(session_service)
@@ -18,8 +19,10 @@ describe HealthQuest::QuestionnaireManager::Factory do
     let(:factory) { described_class.manufacture(user) }
 
     it 'responds to attributes' do
+      expect(factory.respond_to?(:appointments)).to eq(true)
       expect(factory.respond_to?(:aggregated_data)).to eq(true)
       expect(factory.respond_to?(:patient)).to eq(true)
+      expect(factory.respond_to?(:appointment_service)).to eq(true)
       expect(factory.respond_to?(:patient_service)).to eq(true)
       expect(factory.respond_to?(:user)).to eq(true)
     end
@@ -32,27 +35,62 @@ describe HealthQuest::QuestionnaireManager::Factory do
   end
 
   describe '#all' do
-    let(:patient) { double('FHIR::Patient') }
-
     context 'when patient does not exist' do
       let(:client_reply) { double('FHIR::ClientReply', resource: nil) }
 
+      before do
+        allow_any_instance_of(subject).to receive(:get_patient).and_return(client_reply)
+      end
+
       it 'returns a default hash' do
         hash = { data: [] }
-        allow_any_instance_of(HealthQuest::PatientGeneratedData::Patient::MapQuery)
-          .to receive(:get).with(user.icn).and_return(client_reply)
 
         expect(described_class.manufacture(user).all).to eq(hash)
       end
+
+      it 'has a nil patient' do
+        factory = described_class.manufacture(user)
+        factory.all
+
+        expect(factory.patient).to be_nil
+      end
     end
 
-    context 'when patient exists' do
-      let(:client_reply) { double('FHIR::ClientReply', resource: patient) }
+    context 'when patient and no appointments' do
+      let(:fhir_patient) { double('FHIR::Patient') }
+      let(:client_reply) { double('FHIR::ClientReply', resource: fhir_patient) }
+
+      before do
+        allow_any_instance_of(subject).to receive(:get_appointments).and_return(default_appointments)
+        allow_any_instance_of(subject).to receive(:get_patient).and_return(client_reply)
+      end
+
+      it 'returns a default hash' do
+        hash = { data: [] }
+
+        expect(described_class.manufacture(user).all).to eq(hash)
+      end
+
+      it 'has a FHIR::Patient patient' do
+        factory = described_class.manufacture(user)
+        factory.all
+
+        expect(factory.patient).to eq(fhir_patient)
+      end
+    end
+
+    context 'when patient and appointments exist' do
+      let(:appointments) { { data: [{}, {}] } }
+      let(:fhir_patient) { double('FHIR::Patient') }
+      let(:client_reply) { double('FHIR::ClientReply', resource: fhir_patient) }
+
+      before do
+        allow_any_instance_of(subject).to receive(:get_appointments).and_return(appointments)
+        allow_any_instance_of(subject).to receive(:get_patient).and_return(client_reply)
+      end
 
       it 'returns a WIP hash' do
         hash = { data: 'WIP' }
-        allow_any_instance_of(HealthQuest::PatientGeneratedData::Patient::MapQuery)
-          .to receive(:get).with(user.icn).and_return(client_reply)
 
         expect(described_class.manufacture(user).all).to eq(hash)
       end
