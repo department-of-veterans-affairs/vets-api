f11ecacc6a7f895bb4f68a90e26e69dc391f855d
Jack Schuss <jackschuss@gmail.com>
2021-02-04 16:10:43 -0500

    Appease the rubocop overlords. Again.

--- modules/appeals_api/spec/workers/appeals_api/daily_error_report_spec.rb
+++ modules/appeals_api/spec/workers/appeals_api/daily_error_report_spec.rb
@@ -6,10 +6,11 @@ describe AppealsApi::DailyErrorReport, type: :job do
   describe '#perform' do
     it 'sends mail' do
       with_settings(Settings.modules_appeals_api.reports.daily_error, enabled: true) do
-        expect(AppealsApi::DailyErrorReportMailer).to receive(:build).once
-        .and_return(double.tap do |mailer|
-          expect(mailer).to receive(:deliver_now).once
-        end)
+        expect(AppealsApi::DailyErrorReportMailer).to receive(:build)
+          .once
+          .and_return(double.tap do |mailer|
+            expect(mailer).to receive(:deliver_now).once
+          end)
 
         described_class.new.perform
       end
