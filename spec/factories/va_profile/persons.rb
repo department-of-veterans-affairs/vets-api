a7a048c2c75ef8376f7b652a499f2680ab824f0e
Lihan Li <frankieteardrop@gmail.com>
2021-02-03 18:03:52 -0800

    fix va profile redis contact information

--- spec/factories/va_profile/persons.rb
+++ spec/factories/va_profile/persons.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :person, class: 'Vet360::Models::Person' do
-    addresses   { [FactoryBot.build(:vet360_address), FactoryBot.build(:vet360_address, :mailing)] }
+  factory :person, class: 'VAProfile::Models::Person' do
+    addresses   { [FactoryBot.build(:va_profile_address), FactoryBot.build(:va_profile_address, :mailing)] }
     emails      { [FactoryBot.build(:email)] }
     # TODO: test that these telephone & permission factories register
     telephones  { [FactoryBot.build(:telephone)] }
--- spec/support/vet360/stub_vet360.rb
+++ spec/support/vet360/stub_vet360.rb
@@ -11,27 +11,27 @@ def stub_vet360(person = nil)
   person ||= build(
     :person,
     addresses: [
-      build(:vet360_address, id: 123),
-      build(:vet360_address, address_pou: Vet360::Models::Address::CORRESPONDENCE, id: 124)
+      build(:va_profile_address, id: 123),
+      build(:va_profile_address, address_pou: VAProfile::Models::Address::CORRESPONDENCE, id: 124)
     ],
     emails: [
       build(:email, id: 456)
     ],
     telephones: [
       build(:telephone, :home, id: 789),
-      build(:telephone, :home, phone_type: Vet360::Models::Telephone::MOBILE, id: 790),
-      build(:telephone, :home, phone_type: Vet360::Models::Telephone::WORK, id: 791),
-      build(:telephone, :home, phone_type: Vet360::Models::Telephone::FAX, id: 792),
-      build(:telephone, :home, phone_type: Vet360::Models::Telephone::TEMPORARY, id: 793)
+      build(:telephone, :home, phone_type: VAProfile::Models::Telephone::MOBILE, id: 790),
+      build(:telephone, :home, phone_type: VAProfile::Models::Telephone::WORK, id: 791),
+      build(:telephone, :home, phone_type: VAProfile::Models::Telephone::FAX, id: 792),
+      build(:telephone, :home, phone_type: VAProfile::Models::Telephone::TEMPORARY, id: 793)
     ],
     permissions: [
       build(:permission, id: 1011),
-      build(:permission, permission_type: Vet360::Models::Permission::TEXT, id: 1012)
+      build(:permission, permission_type: VAProfile::Models::Permission::TEXT, id: 1012)
     ]
   )
 
-  allow_any_instance_of(Vet360::ContactInformation::Service).to receive(:get_person).and_return(
-    Vet360::ContactInformation::PersonResponse.new(200, person: person)
+  allow_any_instance_of(VAProfile::ContactInformation::Service).to receive(:get_person).and_return(
+    VAProfile::ContactInformation::PersonResponse.new(200, person: person)
   )
 end
 # rubocop:enable Metrics/MethodLength
