01791eae90e284daa265428e95eb47ad662f4231
Ed Mangimelli <38672641+edmangimelli@users.noreply.github.com>
2021-02-05 11:51:08 -0500

    Rename Variable for Clarity (EVSS Lib) (#5852)
    * tiny refactor --found it confusing to see two default values in two different place. i think having them together makes it more clear that when ERROR_MAP doesn't specify a default, use this
    
    * rephrase comment to clarify existing behavior. add tests to demonstrate existing behavior
    
    * revert variable rename
    
    * fix typo
    
    * fix typo
    
    * reorder sentence in spec
    
    * renaming variable for clarity

--- lib/evss/service_exception.rb
+++ lib/evss/service_exception.rb
@@ -18,8 +18,8 @@ module EVSS
       # in case of multiple errors, the ordering of ERROR_MAP is used to decided which error is highest priority.
       # NOTE: The ordering of ERROR_MAP takes precedence over both 1) how the error messages were ordered in the
       # EVSS response and 2) the "severity" keys of those messages.
-      key = self.class::ERROR_MAP.select { |k, _v| messages_has_key?(k) }
-      return key.values.first unless key.empty?
+      error_map = self.class::ERROR_MAP.select { |k, _v| messages_has_key?(k) }
+      return error_map.values.first unless error_map.empty?
 
       self.class::ERROR_MAP[:default]
     end
