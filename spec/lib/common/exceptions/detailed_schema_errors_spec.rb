52d823c74d5209fc49d7bc19cea98f0195941146
Drew Fisher <142862+cilestin@users.noreply.github.com>
2021-02-08 14:10:18 -0800

    Fix DetailedSchemaErrors spec when running individually (#5881)

--- spec/lib/common/exceptions/detailed_schema_errors_spec.rb
+++ spec/lib/common/exceptions/detailed_schema_errors_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'rails_helper'
 require 'common/exceptions/detailed_schema_errors'
 
 describe Common::Exceptions::DetailedSchemaErrors do
