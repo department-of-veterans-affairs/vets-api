34bfea3d299d3d97d97370a2c85f425d3c2db625
Jack Schuss <jackschuss@gmail.com>
2021-02-08 16:04:39 -0500

    add reference

--- modules/appeals_api/app/workers/appeals_api/decision_review_report_weekly.rb
+++ modules/appeals_api/app/workers/appeals_api/decision_review_report_weekly.rb
@@ -7,9 +7,7 @@ module AppealsApi
     include Sidekiq::Worker
 
     def perform(to: Time.zone.now, from: 1.week.ago)
-      if Settings.modules_appeals_api.reports.decision_review.enabled
-        DecisionReviewMailer.build(date_from: from, date_to: to, friendly_duration: 'Weekly').deliver_now
-      end
+      DecisionReviewMailer.build(date_from: from, date_to: to, friendly_duration: 'Weekly').deliver_now if enabled?
     end
 
     private
