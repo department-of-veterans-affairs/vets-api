9cdbd787edee5d71d3c131f326b4a3073d43c3c8
Kam Karshenas <kam@adhocteam.us>
2021-02-08 10:08:11 -0600

    Vaos grafana related changes (#5827)
    Co-authored-by: cferris32 <cferris320@gmail.com>
    Co-authored-by: brad <brad@concord.tech>

--- modules/vaos/app/services/vaos/middleware/response/errors.rb
+++ modules/vaos/app/services/vaos/middleware/response/errors.rb
@@ -4,11 +4,27 @@ module VAOS
   module Middleware
     module Response
       class Errors < Faraday::Response::Middleware
+        STATSD_KEY_PREFIX = 'api.vaos.va_mobile.response'
+
         def on_complete(env)
+          statsd_increment("#{STATSD_KEY_PREFIX}.total", env)
           return if env.success?
 
+          statsd_increment("#{STATSD_KEY_PREFIX}.fail", env)
           Raven.extra_context(vamf_status: env.status, vamf_body: env.body, vamf_url: env.url)
           raise VAOS::Exceptions::BackendServiceException, env
+        end
+
+        private
+
+        def statsd_increment(key, env)
+          StatsDMetric.new(key: key).save
+          tags = [
+            "method:#{env.method.upcase}",
+            "url:#{StringHelpers.filtered_endpoint_tag(env.url.path)}",
+            "http_status:#{env.status}"
+          ]
+          StatsD.increment(key, tags: tags)
         end
       end
     end
