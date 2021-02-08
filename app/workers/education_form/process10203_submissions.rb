c4575d3aea18a8b9d8dabc9bbfde88a82f8acee3
Devin McCurdy <mccurdy_devin@bah.com>
2021-02-08 09:32:16 -0500

    10203 Stem Automated Decision Processing (#5793)

--- app/workers/education_form/process10203_submissions.rb
+++ app/workers/education_form/process10203_submissions.rb
@@ -60,10 +60,11 @@ module EducationForm
     #   by EducationForm::CreateDailySpoolFiles
     # Otherwise check submission data and EVSS data to see if submission can be marked as PROCESSED
     def process_user_submissions(user_submissions)
-      user_submissions.each do |user_uuid, submissions|
-        user = User.find(user_uuid)
-        poa = get_user_poa_status(user)
-        gi_bill_status = get_gi_bill_status(user)
+      user_submissions.each_value do |submissions|
+        auth_headers = submissions.first.education_stem_automated_decision.auth_headers
+        gi_bill_status = get_gi_bill_status(auth_headers)
+        poa = get_user_poa_status(auth_headers)
+
         if gi_bill_status == {} || gi_bill_status.remaining_entitlement.blank?
           submissions.each do |submission|
             update_automated_decision(submission, PROCESSED, poa)
@@ -77,18 +78,18 @@ module EducationForm
     end
 
     # Retrieve EVSS gi_bill_status data for a user
-    def get_gi_bill_status(user)
-      service = EVSS::GiBillStatus::Service.new(user)
-      service.get_gi_bill_status
+    def get_gi_bill_status(auth_headers)
+      service = EVSS::GiBillStatus::Service.new(nil, auth_headers)
+      service.get_gi_bill_status(auth_headers)
     rescue => e
       Rails.logger.error "Failed to retrieve GiBillStatus data: #{e.message}"
       {}
     end
 
     # Retrieve poa status fromEVSS VSOSearch for a user
-    def get_user_poa_status(user)
-      service = EVSS::VSOSearch::Service.new(user)
-      service.get_current_info['userPoaInfoAvailable']
+    def get_user_poa_status(auth_headers)
+      service = EVSS::VSOSearch::Service.new(nil, auth_headers)
+      service.get_current_info(auth_headers)['userPoaInfoAvailable']
     rescue => e
       Rails.logger.error "Failed to retrieve VSOSearch data: #{e.message}"
       nil
