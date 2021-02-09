d42c657d0ac689bacd30535f8c9bfac0317d0eb1
Jack <jackschuss@gmail.com>
2021-02-09 09:07:02 -0500

    add Daily Error Report (#5858)
    * add Daily Error Report
    
    * Appease the rubocop overlords
    
    * Test failures
    
    * Remove extra file
    
    * code coverage on worker
    
    * Appease the rubocop overlords. Again.
    
    * Remove accidental Gemfile.lock commit
    
    * Schedule Daily Error Report
    
    * Add backwards compatibility for older settings
    
    * add reference
    
    * Only send error report if reports namespace setting exists
    
    * Appease the rubocop overlords. Again.

---
+++ modules/appeals_api/app/workers/appeals_api/daily_error_report.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+
+module AppealsApi
+  class DailyErrorReport
+    include Sidekiq::Worker
+
+    def perform
+      DailyErrorReportMailer.build.deliver_now if enabled?
+    end
+
+    private
+
+    def enabled?
+      Settings.dig('modules_appeals_api', 'reports') &&
+        Settings.modules_appeals_api.reports.daily_error.enabled
+    end
+  end
+end
