23372744e12c43e4e90412e2840f398fcfa205c5
Alastair Dawson <alastair.j.dawson@gmail.com>
2021-02-09 12:55:19 -0800

    update cancel endpoint docs

--- modules/mobile/spec/request/appointments_request_spec.rb
+++ modules/mobile/spec/request/appointments_request_spec.rb
@@ -331,6 +331,7 @@ RSpec.describe 'appointments', type: :request do
           VCR.use_cassette('appointments/get_cancel_reasons_invalid', match_requests_on: %i[method uri]) do
             put '/mobile/v0/appointments/cancel', headers: iam_headers, params: request_body
 
+            binding.pry
             expect(response).to have_http_status(:not_found)
             expect(response.parsed_body['errors'].first['detail']).to eq(
               'This appointment can not be cancelled online because a prerequisite cancel reason could not be found'
