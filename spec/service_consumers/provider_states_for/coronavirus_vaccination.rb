b0a8344246215d518ab62e7ba0ded63844c757e0
Holden Hinkle <holdenhinkle@gmail.com>
2021-02-11 16:04:56 -0500

    misc changes and comments before pairing session.

--- spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
+++ spec/service_consumers/provider_states_for/coronavirus_vaccination.rb
@@ -5,7 +5,7 @@ Pact.provider_states_for 'Coronavirus Vaccination' do
     set_up do
       user = build_user_and_stub_session
       submission = CovidVaccine::V0::RegistrationSubmission.for_user(user).last
-      submission || FactoryBot.create(:covid_vax_registration)
+      submission || FactoryBot.create(:covid_vax_registration, account_id: user.account_uuid)
     end
 
     tear_down do
@@ -15,7 +15,9 @@ Pact.provider_states_for 'Coronavirus Vaccination' do
   provider_state 'registration data does not exist' do
     set_up do
       user = build_user_and_stub_session
-      CovidVaccine::V0::RegistrationSubmission.for_user(user).last
+      FactoryBot.create(:covid_vax_registration, :unsubmitted, account_id: user.account_uuid)
+      # or should I not create an unsubmitted registration and
+      # follow this example - line 245, modules/covid_vaccine/spec/request/covid_vaccine/v0/registration_request_spec.rb
     end
 
     tear_down do
@@ -25,6 +27,7 @@ Pact.provider_states_for 'Coronavirus Vaccination' do
   provider_state 'authenticated user submits registration data' do
     set_up do
       build_user_and_stub_session
+      # what to do next?
     end
 
     tear_down do
