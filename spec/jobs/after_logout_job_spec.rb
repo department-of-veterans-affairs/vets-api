eb5f9ad3c396f3b1285e3fd668cb935d9bab17e0
John Bramley <bramleyjl@gmail.com>
2021-02-11 14:54:38 -0500

    improves after_logout_job test coverage

--- spec/jobs/after_logout_job_spec.rb
+++ spec/jobs/after_logout_job_spec.rb
@@ -7,9 +7,18 @@ RSpec.describe AfterLogoutJob do
     context 'without an account_uuid' do
       let(:user) { create(:user) }
 
-      it 'does returns' do
+      it 'immediately returns' do
         expect_any_instance_of(TestUserDashboard::CheckinUser).not_to receive(:call)
         described_class.new.perform('account_uuid' => '')
+      end
+    end
+
+    context 'with an account_uuid' do
+      let(:user) { create(:user) }
+
+      it 'calls TestUserDashboard CheckinUser' do
+        expect_any_instance_of(TestUserDashboard::CheckinUser).to receive(:call)
+        described_class.new.perform('account_uuid' => user.account_uuid)
       end
     end
   end
