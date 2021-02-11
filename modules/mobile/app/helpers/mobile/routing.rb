a02f78529677aec2bd77d91918d7c020fa4c5c49
Patrick Vinograd <patrick@adhocteam.us>
2021-02-10 23:09:01 -0800

    Fix link helpers

---
+++ modules/mobile/app/helpers/mobile/routing.rb
@@ -0,0 +1,14 @@
+module Mobile
+  module Routing
+    extend ActiveSupport::Concern
+    include Mobile::Engine.routes.url_helpers
+
+    included do
+      def default_url_options
+        Rails.application.routes.default_url_options
+      end
+    end
+  end
+end
+
+
--- modules/mobile/app/serializers/mobile/v0/folder_serializer.rb
+++ modules/mobile/app/serializers/mobile/v0/folder_serializer.rb
@@ -3,6 +3,9 @@
 module Mobile
   module V0
     class FolderSerializer < ActiveModel::Serializer
+
+      include Mobile::Engine.routes.url_helpers
+
       attribute :id
 
       attribute(:folder_id) { object.id }
@@ -11,7 +14,7 @@ module Mobile
       attribute :unread_count
       attribute :system_folder
 
-      link(:self) { v0_folder_url(object.id) }
+      link(:self) { Mobile::UrlHelper.new.v0_folder_url(object.id) }
     end
   end
 end
