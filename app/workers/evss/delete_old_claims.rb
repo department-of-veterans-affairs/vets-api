df6c5954e4b5a7744f5dbfa0fd2283011193ec24
Anna Carey <anna@adhocteam.us>
2021-02-12 11:33:21 -0500

    add sentry tags for worker owned by claims and appeals (#5897)

--- app/workers/evss/delete_old_claims.rb
+++ app/workers/evss/delete_old_claims.rb
@@ -5,6 +5,7 @@ module EVSS
     include Sidekiq::Worker
 
     def perform
+      Raven.tags_context(source: 'claims-status')
       claims = EVSSClaim.where("updated_at < '#{1.day.ago}'")
       logger.info("Deleting #{claims.count} old EVSS claims")
       claims.delete_all
