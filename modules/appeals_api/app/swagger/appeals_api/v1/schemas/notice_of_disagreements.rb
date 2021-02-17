a74e0d9503906414d6e3b3e6f58832f3336a1ff5
Nathan Wright <nathan.wright@oddball.io>
2021-02-16 17:23:28 -0700

    Revert "Update NOD swagger status options"
    This reverts commit 46baf383a03ece6d62aeb4885532ed6d63fd2119.

--- modules/appeals_api/app/swagger/appeals_api/v1/schemas/notice_of_disagreements.rb
+++ modules/appeals_api/app/swagger/appeals_api/v1/schemas/notice_of_disagreements.rb
@@ -8,7 +8,7 @@ module AppealsApi::V1
       swagger_component do
         schema :nodStatus do
           key :type, :string
-          key :enum, %i[pending submitting submitted error]
+          key :enum, AppealsApi::CentralMailStatus::STATUSES
           key :example, 'submitted'
         end
 
