bb92ebe6c6a9c268449958902e37628b9adbf10b
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-02-18 12:01:32 -0600

    API-4027 - Appease Rubocop

--- modules/claims_api/app/swagger/claims_api/common/authorization/not_authorized_swagger.rb
+++ modules/claims_api/app/swagger/claims_api/common/authorization/not_authorized_swagger.rb
@@ -6,33 +6,33 @@ module ClaimsApi
       class NotAuthorizedSwagger
         include Swagger::Blocks
 
-          swagger_component do
-            schema :NotAuthorizedModel do
-              property :title do
-                key :type, :string
-                key :example, 'Not authorized'
-                key :description, 'HTTP error title'
-              end
+        swagger_component do
+          schema :NotAuthorizedModel do
+            property :title do
+              key :type, :string
+              key :example, 'Not authorized'
+              key :description, 'HTTP error title'
+            end
 
-              property :detail do
-                key :type, :string
-                key :example, 'Not authorized'
-                key :description, 'HTTP error detail'
-              end
+            property :detail do
+              key :type, :string
+              key :example, 'Not authorized'
+              key :description, 'HTTP error detail'
+            end
 
-              property :code do
-                key :type, :string
-                key :example, '401'
-                key :description, 'HTTP error code'
-              end
+            property :code do
+              key :type, :string
+              key :example, '401'
+              key :description, 'HTTP error code'
+            end
 
-              property :status do
-                key :type, :string
-                key :example, '401'
-                key :description, 'HTTP error code'
-              end
+            property :status do
+              key :type, :string
+              key :example, '401'
+              key :description, 'HTTP error code'
             end
           end
+        end
       end
     end
   end
