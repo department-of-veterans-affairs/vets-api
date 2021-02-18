c75b6e7f8d32c01e84216cc3f5de74d949aeb9fe
Luke Markwordt <luke.markwordt@libertyits.com>
2021-02-18 09:25:33 -0700

    Removed unused methods from okta service

--- lib/okta/service.rb
+++ lib/okta/service.rb
@@ -26,14 +26,6 @@ module Okta
       end
     end
 
-    def call_no_token(action, url)
-      connection.send(action) do |req|
-        req.url url
-        req.headers['Content-Type'] = 'application/json'
-        req.headers['Accept'] = 'application/json'
-      end
-    end
-
     def app(app_id)
       with_monitoring do
         get_url_with_token("#{APP_API_BASE_PATH}/#{app_id}")
@@ -76,10 +68,6 @@ module Okta
       define_method("#{http_verb}_url_with_token".to_sym) do |url|
         Okta::Response.new call_with_token(http_verb, url)
       end
-    end
-
-    def get_url_no_token(url)
-      Okta::Response.new call_no_token('get', url)
     end
   end
 end
