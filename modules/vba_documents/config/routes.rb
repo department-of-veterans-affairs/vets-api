eb3b6889328e27de986bd612d8ce5571b3bab51f
Greg Bowman <gregory.bowman@libertyits.com>
2021-02-19 15:10:50 -0500

    checkpoint - using after(:each) to validate upload file record

--- modules/vba_documents/config/routes.rb
+++ modules/vba_documents/config/routes.rb
@@ -8,7 +8,8 @@ VBADocuments::Engine.routes.draw do
   match '/v1/upstream_healthcheck', to: 'metadata#upstream_healthcheck', via: [:get]
   match '/v0/*path', to: 'application#cors_preflight', via: [:options]
   match '/v1/*path', to: 'application#cors_preflight', via: [:options]
-  if Settings.vba_documents.features.include?('v2_upload-endpoint')
+
+  if Settings.vba_documents.v2_upload_endpoint_enabled
     match '/v2/uploads/submit', to: 'v2/uploads#submit', via: [:get, :post]
     # todo eliminate get from route
   end
--- modules/vba_documents/spec/request/v2/uploads_request_spec.rb
+++ modules/vba_documents/spec/request/v2/uploads_request_spec.rb
@@ -10,16 +10,15 @@ RSpec.describe 'VBA Document Uploads Endpoint', type: :request, retry: 3 do
   include VBADocuments::Fixtures
 
   #need a larger limit for sending raw data (base_64 for example)
-  Rack::Utils.key_space_limit = 65536*5
+  Rack::Utils.key_space_limit = 65536 * 5
   SUBMIT_ENDPOINT = '/services/vba_documents/v2/uploads/submit'
 
   describe '#submit /v2/uploads/submit' do
-
     let(:valid_attachments) do
-      {     attachment1: Rack::Test::UploadedFile.new(get_fixture('valid_doc.pdf').path, 'application/pdf',
-                                                      true),
-            attachment2: Rack::Test::UploadedFile.new(get_fixture('valid_doc.pdf').path, 'application/pdf',
-                                                      true)
+      {attachment1: Rack::Test::UploadedFile.new(get_fixture('valid_doc.pdf').path, 'application/pdf',
+                                                 true),
+       attachment2: Rack::Test::UploadedFile.new(get_fixture('valid_doc.pdf').path, 'application/pdf',
+                                                 true)
       }
     end
 
@@ -33,14 +32,21 @@ RSpec.describe 'VBA Document Uploads Endpoint', type: :request, retry: 3 do
                                              true)}
     end
 
-    xit 'returns a UUID with status of uploaded and populated pdf metadata with a valid post' do
+    after(:each) do
+      guid = @attributes['guid']
+      upload = VBADocuments::UploadFile.find_by_guid(guid)
+      expect(upload.uploaded?).to be_truthy
+    end
+
+    it 'returns a UUID with status of uploaded and populated pdf metadata with a valid post' do
       post SUBMIT_ENDPOINT,
-      params: {}.merge(valid_metadata).merge(valid_content).merge(valid_attachments)
+           params: {}.merge(valid_metadata).merge(valid_content).merge(valid_attachments)
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
-      expect(json['data']['attributes']).to have_key('guid')
-      expect(json['data']['attributes']['status']).to eq('uploaded')
-      uploaded_pdf = json['data']['attributes']['uploaded_pdf']
+      @attributes = json['data']['attributes']
+      expect(@attributes).to have_key('guid')
+      expect(@attributes['status']).to eq('uploaded')
+      uploaded_pdf = @attributes['uploaded_pdf']
       expect(uploaded_pdf['total_documents']).to eq(3)
       expect(uploaded_pdf['content']['dimensions']['oversized_pdf']).to eq(false)
       expect(uploaded_pdf['content']['attachments'].first['dimensions']['oversized_pdf']).to eq(false)
@@ -51,12 +57,13 @@ RSpec.describe 'VBA Document Uploads Endpoint', type: :request, retry: 3 do
       post SUBMIT_ENDPOINT, params: get_fixture('base_64').read
       expect(response).to have_http_status(:bad_request)
       json = JSON.parse(response.body)
-      expect(json['data']['attributes']).to have_key('guid')
-      expect(json['data']['attributes']['status']).to eq('error')
-      expect(json['data']['attributes']['uploaded_pdf']).to have_key('total_documents')
-      expect(json['data']['attributes']['uploaded_pdf']).to have_key('total_pages')
-      expect(json['data']['attributes']['uploaded_pdf']).to have_key('content')
-      expect(json['data']['attributes']['uploaded_pdf']['content']['dimensions']['oversized_pdf']).to be_truthy
+      @attributes = json['data']['attributes']
+      expect(@attributes).to have_key('guid')
+      expect(@attributes['status']).to eq('error')
+      expect(@attributes['uploaded_pdf']).to have_key('total_documents')
+      expect(@attributes['uploaded_pdf']).to have_key('total_pages')
+      expect(@attributes['uploaded_pdf']).to have_key('content')
+      expect(@attributes['uploaded_pdf']['content']['dimensions']['oversized_pdf']).to be_truthy
     end
   end
 end
