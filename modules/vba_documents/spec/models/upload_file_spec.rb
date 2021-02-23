0d13b2ca69d40da384709e7f4421f2214e77d637
Cris Shupp <cristopher.shupp@libertyits.com>
2021-02-23 13:13:36 -0500

    Lint results

--- modules/vba_documents/spec/models/upload_file_spec.rb
+++ modules/vba_documents/spec/models/upload_file_spec.rb
@@ -13,7 +13,7 @@ describe VBADocuments::UploadFile, type: :model do
     upload_model.save!
     upload_model.parse_and_upload!
     expect(upload_model).to be_uploaded
-    expect(upload_model.use_active_storage). to be_truthy
+    expect(upload_model.use_active_storage).to be_truthy
     upload_model.remove_from_storage
     expect(upload_model).not_to be_uploaded
   end
@@ -21,8 +21,6 @@ describe VBADocuments::UploadFile, type: :model do
   it 'does not instantiatiate on UploadSubmission' do
     upload_model = VBADocuments::UploadSubmission.new
     upload_model.save!
-    expect { VBADocuments::UploadFile.find_by_guid(upload_model.guid) }.to raise_error(ActiveRecord::StatementInvalid)
+    expect { VBADocuments::UploadFile.find_by(guid: upload_model.guid) }.to raise_error(ActiveRecord::StatementInvalid)
   end
-
-
-  end
+end
