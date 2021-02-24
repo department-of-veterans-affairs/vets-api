c5d5ecd00dd43e828fa600c7156e5e61b52d0377
Laura Trager <laura.trager@adhocteam.us>
2021-02-24 11:24:30 -0500

    Add increased info to error and success responses

--- modules/appeals_api/app/services/appeals_api/uploads/document_validation.rb
+++ modules/appeals_api/app/services/appeals_api/uploads/document_validation.rb
@@ -5,39 +5,100 @@ require 'pdf_info'
 module AppealsApi
   module Uploads
     class DocumentValidation
+      include SentryLogging
+
       def initialize(upload)
         @document = upload[:document]
       end
 
       def validate
-        pdf_metadata_present?
-        raise UploadValidationError, file_size_error unless valid_file_size?
-        raise UploadValidationError, max_dimension_error unless valid_page_dimensions?
+        return file_type_error unless pdf_metadata_present?
+        return file_size_error unless valid_file_size?
+        return max_dimension_error unless valid_page_dimensions?
+
+        successful_validation_message
       end
 
       private
 
       def pdf_metadata_present?
         @pdf_metadata = PdfInfo::Metadata.read(@document)
+      rescue PdfInfo::MetadataReadError => e
+        @pdf_error = 'PdfInfo::MetadataReadError: Syntax Warning: May not be a PDF file'
+        log_exception_to_sentry(e, nil, nil, 'warn')
+      ensure
         @pdf_metadata.present?
       end
 
       def valid_file_size?
-        File.size(@document) <= 100.megabytes
+        current_file_size <= 100.megabytes
       end
 
       def valid_page_dimensions?
-        dimensions = @pdf_metadata.page_size_inches
-        dimensions[:height] <= 11 && dimensions[:width] <= 11
+        @dimensions = @pdf_metadata.page_size_inches
+        @dimensions[:height] <= 11 && @dimensions[:width] <= 11
+      end
+
+      def current_file_size
+        @file_size ||= @document.length
+      end
+
+      # rubocop:disable Layout/SpaceInsideHashLiteralBraces
+      # rubocop:disable Layout/HashAlignment
+      def file_type_error
+        content_type = @document.content_type
+        extension = File.extname(@document)
+        { document: {
+          status: 'error',
+          filename:  @document.original_filename,
+          detail: I18n.t('appeals_api.uploads.unsupported_file_type'),
+          content_type: content_type,
+          file_extension: extension == '.pdf' ? '.pdf is likely an incorrect extension for this document' : extension
+        }}
       end
 
       def max_dimension_error
-        I18n.t('appeals_api.uploads.max_dimensions_error', max_dimensions: '11 inches x 11 inches')
+        message = I18n.t('appeals_api.uploads.max_dimensions_error', max_dimensions: '11 inches x 11 inches')
+        log_exception_to_sentry(UploadValidationError.new(message), {}, {}, :info)
+        { document: {
+          status: 'error',
+          filename:  @document.original_filename,
+          pages: @pdf_metadata.pages,
+          detail: message,
+          file_dimensions: {
+            "height": @dimensions[:height],
+            "width": @dimensions[:width]
+          }
+        }}
       end
 
       def file_size_error
-        I18n.t('appeals_api.uploads.max_file_size_error', max_file_size: '100 megabytes')
+        message = I18n.t('appeals_api.uploads.max_file_size_error', max_file_size: '100 megabytes')
+        log_exception_to_sentry(UploadValidationError.new(message), {}, {}, :info)
+        { document: {
+          status: 'error',
+          filename:  @document.original_filename,
+          pages: @pdf_metadata.pages,
+          detail: message,
+          file_size: "#{current_file_size} MB"
+        }}
       end
+
+      def successful_validation_message
+        message = I18n.t('appeals_api.uploads.successful_validation')
+        { document: {
+          status: 'accepted',
+          filename:  @document.original_filename,
+          pages: @pdf_metadata.pages,
+          detail: message,
+          file_dimensions: {
+            "height": @dimensions[:height],
+            "width": @dimensions[:width]
+          }
+        }}
+      end
+      # rubocop:enable Layout/SpaceInsideHashLiteralBraces
+      # rubocop:enable Layout/HashAlignment
 
       class UploadValidationError < StandardError; end
     end
--- modules/appeals_api/spec/services/appeals_api/uploads/document_validation_spec.rb
+++ modules/appeals_api/spec/services/appeals_api/uploads/document_validation_spec.rb
@@ -12,31 +12,31 @@ describe AppealsApi::Uploads::DocumentValidation do
 
     context 'when supported file uploaded (100MB 11in x 11in PDF)' do
       it 'does not raise an error' do
-        expect { described_class.new(valid_pdf).validate }.not_to raise_error
+        expect(described_class.new(valid_pdf).validate).to match(document: { status: 'accepted', filename: 'expected_10182_extra.pdf', pages: 4, detail: 'File validated', file_dimensions: { height: 8.5, width: 11.0 } })
       end
     end
 
     context 'when unsupported file type uploaded' do
       it 'raises an error when non-pdf file type' do
-        expect { described_class.new(non_pdf).validate }.to raise_error PdfInfo::MetadataReadError
+        expect(described_class.new(non_pdf).validate).to match(document: { status: 'error', filename: 'test_incorrect_type.docx', detail: 'File must be in PDF format', content_type: 'text/plain', file_extension: '.docx' })
       end
 
       it 'raises an exception if pdf extension but wrong type' do
-        expect { described_class.new(pretend_pdf).validate }.to raise_error PdfInfo::MetadataReadError
+        expect(described_class.new(pretend_pdf).validate).to match(document: { status: 'error', filename: 'test_fake_pdf.pdf', detail: 'File must be in PDF format', content_type: 'text/plain', file_extension: '.pdf is likely an incorrect extension for this document' })
       end
     end
 
     context 'when page dimensions are too large' do
       it 'raises and error' do
-        expect { described_class.new(oversize_pdf).validate }.to raise_error AppealsApi::Uploads::DocumentValidation::UploadValidationError
+        expect(described_class.new(oversize_pdf).validate).to match(document: { status: 'error', filename: 'oversize_11x17.pdf', pages: 1, detail: 'File exceeds the maximum page dimensions of 11 inches x 11 inches', file_dimensions: { height: 11.0, width: 17.0 } })
       end
     end
 
     context 'when file size is too large' do
       before { allow_any_instance_of(described_class).to receive(:valid_file_size?).and_return(false) }
 
-      it 'raises and error' do
-        expect { described_class.new(valid_pdf).validate }.to raise_error AppealsApi::Uploads::DocumentValidation::UploadValidationError
+      it 'raises an error' do
+        expect(described_class.new(valid_pdf).validate).to match(document: { status: 'error', filename: 'expected_10182_extra.pdf', pages: 4, detail: 'File cannot exceed a file size of 100 megabytes', file_size: '83846 MB' })
       end
     end
 
