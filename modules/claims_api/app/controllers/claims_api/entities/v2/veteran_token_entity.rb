33bf3a2c92938aa9a1237dcf34fd35eccd2c0bb7
Lee DeBoom <lee.deboom@oddball.io>
2021-02-25 14:29:10 -0600

    switch from jwt to simple base64 for Veteran id generation

--- modules/claims_api/app/controllers/claims_api/entities/v2/veteran_token_entity.rb
+++ modules/claims_api/app/controllers/claims_api/entities/v2/veteran_token_entity.rb
@@ -2,7 +2,7 @@ module ClaimsApi
   module Entities
     module V2
       class VeteranTokenEntity < Grape::Entity
-        expose :token, documentation: { type: String }
+        expose :id, documentation: { type: String }
       end
     end
   end
