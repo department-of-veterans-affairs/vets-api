d71a53762df026e1c3aff568b65d398c9bec3c73
Jayson Perkins <jaysonperkins51@gmail.com>
2021-02-25 15:06:34 -0500

    fixes for claims overview updateAt dates

--- modules/mobile/app/models/mobile/v0/adapters/claims_overview.rb
+++ modules/mobile/app/models/mobile/v0/adapters/claims_overview.rb
@@ -16,26 +16,33 @@ module Mobile
         private
 
         def parse_claim(entry)
-          {
-            id: entry['evss_id'].to_s,
-            type: 'claim',
-            subtype: entry['list_data']['status_type'],
-            completed: entry['list_data']['status'] == 'COMPLETE',
-            date_filed: Date.strptime(entry['list_data']['date'], '%m/%d/%Y').iso8601,
-            updated_at: entry['updated_at'].to_time.iso8601
-          }
+          begin
+            {
+                id: entry['evss_id'].to_s,
+                type: 'claim',
+                subtype: entry['list_data']['status_type'],
+                completed: entry['list_data']['status'] == 'COMPLETE',
+                date_filed: Date.strptime(entry['list_data']['date'], '%m/%d/%Y').iso8601,
+                updated_at: Date.strptime(
+                    entry['list_data']['claim_phase_dates']['phase_change_date'], '%m/%d/%Y'
+                ).to_time.iso8601
+            }
+          rescue Exception => ex
+            binding.pry
+            ex
+          end
         end
 
         def parse_appeal(entry)
           subtype = entry['type']
           filed_index = subtype == 'legacyAppeal' ? 1 : 0
           {
-            id: entry['id'],
-            type: 'appeal',
-            subtype: subtype,
-            completed: !entry['attributes']['active'],
-            date_filed: entry['attributes']['events'][filed_index]['date'],
-            updated_at: entry['attributes']['updated'].to_time.iso8601
+              id: entry['id'],
+              type: 'appeal',
+              subtype: subtype,
+              completed: !entry['attributes']['active'],
+              date_filed: entry['attributes']['events'][filed_index]['date'],
+              updated_at: entry['attributes']['events'].last['date'].to_time.iso8601
           }
         end
       end
