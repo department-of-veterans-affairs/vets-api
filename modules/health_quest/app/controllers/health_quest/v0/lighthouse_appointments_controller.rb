98e83cf376fd4d2466287d4f26dd75aee4293f69
Dillo Raju <dilloshion@gmail.com>
2021-02-26 13:48:09 -0500

    Fetch appointments from lighthouse (#6019)
    * Fetch appointments from lighthouse
    
    * permit strong params for show action

--- modules/health_quest/app/controllers/health_quest/v0/lighthouse_appointments_controller.rb
+++ modules/health_quest/app/controllers/health_quest/v0/lighthouse_appointments_controller.rb
@@ -4,11 +4,22 @@ module HealthQuest
   module V0
     class LighthouseAppointmentsController < HealthQuest::V0::BaseController
       def index
-        render json: []
+        render json: factory.search(request.query_parameters).response[:body]
       end
 
       def show
         render json: {}
+      end
+
+      private
+
+      def appointment_params
+        params.permit(:id)
+      end
+
+      def factory
+        @factory =
+          HealthQuest::HealthApi::Appointment::Factory.manufacture(current_user)
       end
     end
   end
