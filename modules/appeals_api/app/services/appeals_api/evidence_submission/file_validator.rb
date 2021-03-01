e8f92b3ea5a02ade70657491898e90ab7fcc9677
Laura Trager <laura.trager@adhocteam.us>
2021-03-01 10:01:02 -0500

    Reduce error message complexity

--- modules/appeals_api/app/services/appeals_api/evidence_submission/file_validator.rb
+++ modules/appeals_api/app/services/appeals_api/evidence_submission/file_validator.rb
@@ -9,11 +9,13 @@ module AppealsApi
 
       class UploadValidationError < StandardError; end
 
-      MAX_PAGE_SIZE = { width: 11, height: 11 }
+      MAX_PAGE_SIZE = { width: 11, height: 11 }.freeze
       MAX_FILE_SIZE = 100.megabytes
+      FILE_SIZE_STRING = '100 MB'
 
       def initialize(file)
         @file = file
+        @filename = File.basename(@file)
       end
 
       def call
@@ -29,7 +31,7 @@ module AppealsApi
       def pdf_metadata_present?
         @pdf_metadata = PdfInfo::Metadata.read(@file)
       rescue PdfInfo::MetadataReadError => e
-        @pdf_error = I18n.t('appeals_api.evidence_submission.pdf_read_error')
+        @pdf_error = I18n.t('appeals_api.evidence_submission.pdf_read_error', filename: @filename)
         log_exception_to_sentry(e, {}, {}, :warn)
       ensure
         @pdf_metadata.present?
@@ -48,59 +50,44 @@ module AppealsApi
         @dimensions[:height] <= MAX_PAGE_SIZE[:height] && @dimensions[:width] <= MAX_PAGE_SIZE[:width]
       end
 
-      # rubocop:disable Layout/HashAlignment
       def file_type_error
-        extension = File.extname(@file)
-        pdf_ext_error = I18n.t('appeals_api.evidence_submission.pdf_extension_message')
-        { document: {
+        {
           status: 'error',
-          filename:  File.basename(@file),
-          detail: I18n.t('appeals_api.evidence_submission.unsupported_file_type'),
-          file_extension: extension == '.pdf' ? pdf_ext_error : extension
-        } }
+          detail: I18n.t('appeals_api.evidence_submission.unsupported_file_type', filename: @filename)
+        }
       end
 
       def max_dimension_error
-        msg = I18n.t('appeals_api.evidence_submission.max_dimensions_error', max_dimensions: '11 inches x 11 inches')
+        msg = I18n.t('appeals_api.evidence_submission.max_dimensions_error',
+                     max_dimensions: MAX_PAGE_SIZE,
+                     filename: @filename)
+
         log_exception_to_sentry(UploadValidationError.new(msg), {}, {}, :warn)
-        { document: {
+        {
           status: 'error',
-          filename:  File.basename(@file),
-          pages: @pdf_metadata.pages,
-          detail: msg,
-          file_dimensions: {
-            "height": @dimensions[:height],
-            "width": @dimensions[:width]
-          }
-        } }
+          detail: msg
+        }
       end
 
       def file_size_error
-        msg = I18n.t('appeals_api.evidence_submission.max_file_size_error', max_file_size: '100 megabytes')
+        msg = I18n.t('appeals_api.evidence_submission.max_file_size_error',
+                     max_size: FILE_SIZE_STRING,
+                     filename: @filename)
+
         log_exception_to_sentry(UploadValidationError.new(msg), {}, {}, :warn)
-        { document: {
+        {
           status: 'error',
-          filename:  File.basename(@file),
-          pages: @pdf_metadata.pages,
-          detail: msg,
-          file_size: "#{current_file_size} MB"
-        } }
+          detail: msg
+        }
       end
 
       def successful_validation_message
-        msg = I18n.t('appeals_api.evidence_submission.successful_validation')
-        { document: {
-          status: 'accepted',
-          filename:  File.basename(@file),
-          pages: @pdf_metadata.pages,
-          detail: msg,
-          file_dimensions: {
-            "height": @dimensions[:height],
-            "width": @dimensions[:width]
-          }
-        } }
+        msg = I18n.t('appeals_api.evidence_submission.successful_validation', filename: @filename)
+        {
+          status: 'validated',
+          detail: msg
+        }
       end
-      # rubocop:enable Layout/HashAlignment
     end
   end
 end
--- modules/appeals_api/spec/services/appeals_api/evidence_submission/file_validator_spec.rb
+++ modules/appeals_api/spec/services/appeals_api/evidence_submission/file_validator_spec.rb
@@ -13,57 +13,23 @@ describe AppealsApi::EvidenceSubmission::FileValidator do
 
     context 'when supported file uploaded (100MB 11in x 11in PDF)' do
       it 'return success data' do
-        expect(described_class.new(valid_pdf).call).to match(
-          document:
-            {
-              status: 'accepted',
-              filename: 'expected_10182_extra.pdf',
-              pages: 4,
-              detail: 'File validated',
-              file_dimensions: { height: 8.5, width: 11.0 }
-            }
-        )
+        expect(described_class.new(valid_pdf).call).to match({ status: "validated", detail: "expected_10182_extra.pdf validated" })
       end
     end
 
     context 'when unsupported file type uploaded' do
       it 'returns error data when non-pdf file type' do
-        expect(described_class.new(non_pdf).call).to match(
-          document:
-            {
-              status: 'error',
-              filename: 'test_incorrect_type.docx',
-              detail: 'File must be in PDF format',
-              file_extension: '.docx'
-            }
-        )
+        expect(described_class.new(non_pdf).call).to match({ status: "error", detail: "test_incorrect_type.docx must be in PDF format" })
       end
 
       it 'returns error data if pdf extension but wrong type' do
-        expect(described_class.new(pretend_pdf).call).to match(
-          document:
-            {
-              status: 'error',
-              filename: 'test_fake_pdf.pdf',
-              detail: 'File must be in PDF format',
-              file_extension: '.pdf is likely an incorrect extension for this document'
-            }
-        )
+        expect(described_class.new(pretend_pdf).call).to match({ status: "error", detail: "test_fake_pdf.pdf must be in PDF format" })
       end
     end
 
     context 'when page dimensions are too large' do
       it 'returns error data' do
-        expect(described_class.new(oversize_pdf).call).to match(
-          document:
-            {
-              status: 'error',
-              filename: 'oversize_11x17.pdf',
-              pages: 1,
-              detail: 'File exceeds the maximum page dimensions of 11 inches x 11 inches',
-              file_dimensions: { height: 11.0, width: 17.0 }
-            }
-        )
+        expect(described_class.new(oversize_pdf).call).to match({ status: "error", detail: "oversize_11x17.pdf exceeds the maximum page dimensions of {:width=>11, :height=>11}" })
       end
     end
 
@@ -71,16 +37,7 @@ describe AppealsApi::EvidenceSubmission::FileValidator do
       before { allow_any_instance_of(described_class).to receive(:valid_file_size?).and_return(false) }
 
       it 'returns error data' do
-        expect(described_class.new(valid_pdf).call).to match(
-          document:
-            {
-              status: 'error',
-              filename: 'expected_10182_extra.pdf',
-              pages: 4,
-              detail: 'File cannot exceed a file size of 100 megabytes',
-              file_size: '83846 MB'
-            }
-        )
+        expect(described_class.new(valid_pdf).call).to match({ status: "error", detail: "expected_10182_extra.pdf exceeds the max-file-size of 100 MB" })
       end
     end
   end
