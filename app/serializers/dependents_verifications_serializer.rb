dd0171b1a62f70dacfd06acdce6fcb191b27cbae
Derek Dyer <derek@digitalderek.com>
2021-03-01 09:06:42 -0500

    Add prompt renewal

--- app/serializers/dependents_verifications_serializer.rb
+++ app/serializers/dependents_verifications_serializer.rb
@@ -4,19 +4,25 @@ class DependentsVerificationsSerializer < ActiveModel::Serializer
   type :dependency_decs
 
   attribute :dependency_verifications
+  attribute :prompt_renewal
 
   def id
     nil
   end
 
   def dependency_verifications
-    formatted_payload(object[:dependency_decs])
+    formatted_payload
   end
 
-  def formatted_payload(dependency_decs)
+  def prompt_renewal
+    formatted_payload.any? { |diary| diary[:award_effective_date] + 1.year < Date.current }
+  end
+
+  def formatted_payload
+    dependency_decs = object[:dependency_decs]
     ensured_array = dependency_decs.class == Hash ? [dependency_decs] : dependency_decs
 
-    ensured_array.map do |hash|
+    @formatted_payload ||= ensured_array.map do |hash|
       hash.delete(:social_security_number)
       hash
     end
