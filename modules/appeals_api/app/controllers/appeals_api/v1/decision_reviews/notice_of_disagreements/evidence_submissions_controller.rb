0fedfea1cf39c2083634938f30df49c58e29845f
Laura Trager <laura.trager@adhocteam.us>
2021-03-02 16:19:35 -0500

    Add basic upload endpoint

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/notice_of_disagreements/evidence_submissions_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/notice_of_disagreements/evidence_submissions_controller.rb
@@ -5,6 +5,7 @@ module AppealsApi::V1
     module NoticeOfDisagreements
       class EvidenceSubmissionsController < AppealsApi::ApplicationController
         skip_before_action :authenticate
+
         def show
           submissions = AppealsApi::EvidenceSubmission.where(
             supportable_id: params[:id],
@@ -14,6 +15,10 @@ module AppealsApi::V1
           serialized = AppealsApi::EvidenceSubmissionSerializer.new(submissions)
 
           render json: serialized.serializable_hash
+        end
+
+        def upload
+          render json: { message: 'Good work.', document: params[:document], uuid: params[:uuid] }
         end
       end
     end
--- modules/appeals_api/spec/requests/v1/notice_of_disagreements/evidence_submissions_controller_spec.rb
+++ modules/appeals_api/spec/requests/v1/notice_of_disagreements/evidence_submissions_controller_spec.rb
@@ -6,11 +6,17 @@ require AppealsApi::Engine.root.join('spec', 'spec_helper.rb')
 describe AppealsApi::V1::DecisionReviews::NoticeOfDisagreements::EvidenceSubmissionsController, type: :request do
   let(:notice_of_disagreement) { create(:notice_of_disagreement) }
   let(:evidence_submissions) { create_list(:evidence_submission, 3, supportable: notice_of_disagreement) }
-  let(:path) { '/services/appeals/v1/decision_reviews/notice_of_disagreements/evidence_submissions/' }
+
+  def base_path(path)
+    "/services/appeals/v1/decision_reviews/notice_of_disagreements/evidence_submissions/#{path}"
+  end
+  # let(:path) { '/services/appeals/v1/decision_reviews/notice_of_disagreements/evidence_submissions/' }
 
   describe '#show' do
+    let(:path) { base_path notice_of_disagreement.id}
+
     it 'successfully requests the evidence submissions' do
-      get "#{path}#{notice_of_disagreement.id}"
+      get path
 
       expect(response).to have_http_status(:ok)
     end
@@ -18,12 +24,23 @@ describe AppealsApi::V1::DecisionReviews::NoticeOfDisagreements::EvidenceSubmiss
     it 'queries all evidence submissions for the nod' do
       submissions = AppealsApi::EvidenceSubmissionSerializer.new(evidence_submissions).serializable_hash
 
-      get "#{path}#{notice_of_disagreement.id}"
+      get path
 
       body = JSON.parse(response.body)['data']
       serialized = JSON.parse(submissions[:data].to_json)
 
       expect(body).to match_array(serialized)
+    end
+  end
+
+
+  describe '#upload' do
+    let(:path) { base_path 'upload'}
+    let(:upload_params) { { document: Rack::Test::UploadedFile.new("#{::Rails.root}/modules/appeals_api/spec/fixtures/expected_10182_extra.pdf") } }
+
+    it 'successfully responds to document upload' do
+      post(path, params: upload_params)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
