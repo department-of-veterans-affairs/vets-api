688db1253bc0c68ddccb6b9e7c092eaa9917c3f9
vets-api <mark.viterna@va.gov>
2021-03-04 14:49:34 -0500

    adding in json validation

--- db/migrate/20210303164133_add_column_va_form_administration_to_va_forms_forms.rb
+++ db/migrate/20210303164133_add_column_va_form_administration_to_va_forms_forms.rb
@@ -1,5 +1,5 @@
 class AddColumnVAFormAdministrationToVAFormsForms < ActiveRecord::Migration[6.0]
   def change
-    add_column :va_forms_forms, :va_form_administration, :jsonb, default: {}
+    add_column :va_forms_forms, :va_form_administration, :jsonb
   end
 end
