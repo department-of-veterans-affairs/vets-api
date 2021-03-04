eb72e3d184682bb9ebfde385ee6a138af33159eb
Matt Roth <roth_matthew@bah.com>
2021-03-04 13:14:58 -0500

    Update stem_applicant_denial_mailer_preview.rb

--- spec/mailers/previews/stem_applicant_denial_mailer_preview.rb
+++ spec/mailers/previews/stem_applicant_denial_mailer_preview.rb
@@ -4,13 +4,14 @@ class StemApplicantDenialMailerPreview < ActionMailer::Preview
   def build
     return unless FeatureFlipper.staging_email?
 
-    claim = EducationBenefitsClaim.processed.joins(:education_stem_automated_decision).includes(:saved_claim).where(
-      saved_claims: {
-        form_id: '22-10203'
-      },
-      education_stem_automated_decisions: {
-        automated_decision_state: EducationStemAutomatedDecision::DENIED
-      }
+    claim = EducationBenefitsClaim.includes(:saved_claim, :education_stem_automated_decision).where(
+        processed_at: (@time - 24.hours)..@time,
+        saved_claims: {
+            form_id: '22-10203'
+        },
+        education_stem_automated_decisions: {
+            automated_decision_state: EducationStemAutomatedDecision::DENIED
+        }
     )&.last
     StemApplicantDenialMailer.build(claim, nil) if claim.present?
   end
