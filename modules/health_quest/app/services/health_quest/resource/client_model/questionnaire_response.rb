3d842efef4b275a12613711f92a7b6435b9be021
Dillo Raju <draju@governmentcio.com>
2021-03-04 08:34:00 -0500

    remove yardoc comments for now

--- modules/health_quest/app/services/health_quest/resource/client_model/questionnaire_response.rb
+++ modules/health_quest/app/services/health_quest/resource/client_model/questionnaire_response.rb
@@ -3,36 +3,11 @@
 module HealthQuest
   module Resource
     module ClientModel
-      ##
-      # An object for generating a FHIR QuestionnaireResponse data for the PGD.
-      #
-      # @!attribute user
-      #   @return [User]
-      # @!attribute model
-      #   @return [FHIR::Patient]
-      # @!attribute identifier
-      #   @return [FHIR::Identifier]
-      # @!attribute meta
-      #   @return [FHIR::Meta]
-      # @!attribute data
-      #   @return [Hash]
-      # @!attribute source_reference
-      #   @return [FHIR::Reference]
-      # @!attribute subject_reference
-      #   @return [FHIR::Reference]
       class QuestionnaireResponse
         include Shared::IdentityMetaInfo
-        ##
-        # Set the QuestionnaireResponse's status
-        #
+
         COMPLETED_STATUS = 'completed'
-        ##
-        # Set the default Questionnaire ID
-        #
         DEFAULT_QUESTIONNAIRE_ID = '1776c749-91b8-4f33-bece-a5a72f3bb09b'
-        ##
-        # Set the default Questionnaire Title if one is not present
-        #
         DEFAULT_QUESTIONNAIRE_TITLE = 'Pre-Visit Questionnaire'
 
         attr_reader :user,
@@ -43,13 +18,6 @@ module HealthQuest
                     :source_reference,
                     :subject_reference
 
-        ##
-        # Builds a HealthQuest::Resource::ClientModel::QuestionnaireResponse instance from a given User
-        #
-        # @param data [Hash] questionnaire answers and appointment data hash.
-        # @param user [User] the currently logged in user.
-        # @return [HealthQuest::Resource::ClientModel::QuestionnaireResponse] an instance of this class
-        #
         def self.manufacture(data, user)
           new(data, user)
         end
@@ -64,11 +32,6 @@ module HealthQuest
           @subject_reference = ::FHIR::Reference.new
         end
 
-        ##
-        # Builds the FHIR::QuestionnaireResponse object for the PGD.
-        #
-        # @return [FHIR::QuestionnaireResponse]
-        #
         def prepare
           model.tap do |p|
             p.authored = set_date
@@ -83,11 +46,6 @@ module HealthQuest
           end
         end
 
-        ##
-        # Builds the text hash attribute for the FHIR::QuestionnaireResponse object.
-        #
-        # @return [Hash] text information
-        #
         def set_text
           {
             status: 'generated',
@@ -95,88 +53,43 @@ module HealthQuest
           }
         end
 
-        ##
-        # Builds the subject reference.
-        #
-        # @return [FHIR::Reference]
-        #
         def set_subject
           appointment_id = data.dig(:appointment, :id)
           subject_reference.reference = "#{health_api_url_path}/Appointment/#{appointment_id}"
           subject_reference
         end
 
-        ##
-        # Builds the source reference.
-        #
-        # @return [FHIR::Reference]
-        #
         def set_source
           source_reference.reference = "#{health_api_url_path}/Patient/#{user.icn}"
           source_reference
         end
 
-        ##
-        # Builds the questionnaire id.
-        #
-        # @return [String]
-        #
         def set_questionnaire
           questionnaire_id = data.dig(:questionnaire, :id) || DEFAULT_QUESTIONNAIRE_ID
 
           "Questionnaire/#{questionnaire_id}"
         end
 
-        ##
-        # Builds the item array attribute for the FHIR::QuestionnaireResponse object.
-        #
-        # @return [Array]
-        #
         def set_item
           data[:item]
         end
 
-        ##
-        # Returns the questionnaire's title.
-        #
-        # @return [String]
-        #
         def questionnaire_title
           data.dig(:questionnaire, :title) || DEFAULT_QUESTIONNAIRE_TITLE
         end
 
-        ##
-        # Returns today's date.
-        #
-        # @return [String] today's date in String format
-        #
         def set_date
           Time.zone.today.to_s
         end
 
-        ##
-        # Returns the completed status.
-        #
-        # @return [String]
-        #
         def set_status
           COMPLETED_STATUS
         end
 
-        ##
-        # Return the QuestionnaireResponse default ID.
-        #
-        # @return [String]
-        #
         def identifier_value
           DEFAULT_QUESTIONNAIRE_ID
         end
 
-        ##
-        # Return the QuestionnaireResponse's identifier attribute name.
-        #
-        # @return [String]
-        #
         def identifier_code
           'QuestionnaireResponseID'
         end
--- modules/health_quest/app/services/health_quest/resource/query.rb
+++ modules/health_quest/app/services/health_quest/resource/query.rb
@@ -2,25 +2,12 @@
 
 module HealthQuest
   module Resource
-    ##
-    # A service object for querying the lighthouse for FHIR resources.
-    #
-    # @!attribute access_token
-    #   @return [String]
-    # @!attribute headers
-    #   @return [Hash]
     class Query
       include Lighthouse::FHIRClient
       include Lighthouse::FHIRHeaders
 
       attr_reader :access_token, :api, :headers, :resource_identifier
 
-      ##
-      # Builds a Query instance from a redis session.
-      #
-      # @param opts [Hash] options.
-      # @return [Query] an instance of this class
-      #
       def self.build(opts = {})
         new(opts)
       end
@@ -32,33 +19,14 @@ module HealthQuest
         @headers = auth_header
       end
 
-      ##
-      # Queries for FHIR resources using the provided options
-      #
-      # @param options [Hash] the search options.
-      # @return [FHIR::Bundle] an instance of Bundle
-      #
       def search(options)
         client.search(fhir_model, search_options(options))
       end
 
-      ##
-      # Queries for a resource by its id
-      #
-      # @param id [String] the unique ID.
-      # @return [FHIR::ClientReply] an instance of ClientReply
-      #
       def get(id)
         client.read(fhir_model, id)
       end
 
-      ##
-      # Creates a resource in lighthouse for the logged in user.
-      #
-      # @param data [Hash] user submitted data.
-      # @param user [User] the current user.
-      # @return [FHIR::ClientReply] an instance of ClientReply
-      #
       def create(data, user)
         headers.merge!(content_type_header)
 
@@ -66,34 +34,16 @@ module HealthQuest
         client.create(questionnaire_response)
       end
 
-      ##
-      # Returns the FHIR::<resource> class object supplied
-      # by the calling controller
-      #
-      # @return [FHIR::<resource>]
-      #
       def fhir_model
         klass = 'FHIR' + "/#{resource_identifier}".camelize
 
         klass.constantize
       end
 
-      ##
-      # Returns the client class for structuring the data
-      # for POSTing or PUTting to the FHIR server
-      #
-      # @return [ClientModel::<model>]
-      #
       def client_model
         "health_quest/resource/client_model/#{resource_identifier}".camelize.constantize
       end
 
-      ##
-      # Builds a hash of options for the `#search` method
-      #
-      # @param options [Hash] search options.
-      # @return [Hash] a configured set of key values
-      #
       def search_options(options)
         {
           search: {
@@ -102,11 +52,6 @@ module HealthQuest
         }
       end
 
-      ##
-      # Returns the resource specific api path
-      #
-      # @return [String]
-      #
       def api_query_path
         case api
         when 'pgd_api'
