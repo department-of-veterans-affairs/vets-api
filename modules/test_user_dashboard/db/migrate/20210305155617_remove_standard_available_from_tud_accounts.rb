feaa369699050666fd661a954c97a718298f4dea
John Bramley <bramleyjl@gmail.com>
2021-03-05 12:40:17 -0500

    adds updated vets-api schema

--- modules/test_user_dashboard/db/migrate/20210305155617_remove_standard_available_from_tud_accounts.rb
+++ modules/test_user_dashboard/db/migrate/20210305155617_remove_standard_available_from_tud_accounts.rb
@@ -1,6 +1,8 @@
 class RemoveStandardAvailableFromTudAccounts < ActiveRecord::Migration[6.0]
   def change
-    remove_column :test_user_dashboard_tud_accounts, :standard
-    remove_column :test_user_dashboard_tud_accounts, :available
+    safety_assured {
+      remove_column :test_user_dashboard_tud_accounts, :standard
+      remove_column :test_user_dashboard_tud_accounts, :available
+    }
   end
 end
