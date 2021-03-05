eac3fb1e8d94b36106cf1aac3a1a45f2a6a2bc7a
Alastair Dawson <alastair.j.dawson@gmail.com>
2021-03-05 08:51:58 -0800

    rubocop

--- modules/mobile/app/models/mobile/v0/concerns/redis_caching.rb
+++ modules/mobile/app/models/mobile/v0/concerns/redis_caching.rb
@@ -5,18 +5,18 @@ module Mobile
     module Concerns
       module RedisCaching
         extend ActiveSupport::Concern
-        
+
         class_methods do
           def redis_config(config)
             @redis_namespace = config[:namespace]
             @redis_ttl = config[:each_ttl]
             @redis = Redis::Namespace.new(@redis_namespace, redis: Redis.current)
           end
-          
+
           def get_cached(user)
             @redis.get(user.uuid)
           end
-          
+
           def set_cached(user, json)
             @redis.set(user.uuid, json)
             @redis.expire(user.uuid, @redis_ttl)
