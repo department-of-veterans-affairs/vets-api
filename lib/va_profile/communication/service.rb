a96413d4b9016981bd66bd00a9389e25f17f3045
Lihan Li <frankieteardrop@gmail.com>
2021-03-04 18:26:49 -0800

    service stub

---
+++ lib/va_profile/communication/service.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'va_profile/communication/configuration'
+
+module VAProfile
+  module Communication
+    class Service < VAProfile::Service
+      configuration VAProfile::Communication::Configuration
+
+    end
+  end
+end
