37fe093cfa1ed139582cf326ade8c29171e2208a
Jim Ray <jim.ray@oddball.io>
2021-03-08 10:35:48 -0700

    Tud add services (#6020)
    * add test user account MPI integration
    
    * updates for creating user
    
    * add retrieving services and store them for a tud_account
    
    * update tests to test return
    
    * ignore files in the db/seeds directory
    
    * make rubocop happy
    
    * remove test users csv

--- modules/test_user_dashboard/spec/services/create_test_user_account_spec.rb
+++ modules/test_user_dashboard/spec/services/create_test_user_account_spec.rb
@@ -8,14 +8,15 @@ describe TestUserDashboard::CreateTestUserAccount do
   let(:file_path) { Rails.root.join('modules', 'test_user_dashboard', 'spec', 'support', 'spec_users.csv') }
   let(:users) { CSV.read(file_path, headers: true) }
 
-  describe '#initialize' do
-    it { expect(subject.test_user_account).to be_a(TestUserDashboard::TudAccount) }
-  end
-
   describe '#call' do
-    it 'saves a new test user account' do
-      expect { ::TestUserDashboard::CreateTestUserAccount.new(users[0]).call }
-        .to change(TestUserDashboard::TudAccount, :count).by(1)
+    it 'sets the account_id and services' do
+      VCR.use_cassette('mpi/find_candidate/find_profile_with_attributes', VCR::MATCH_EVERYTHING) do
+        tud_account = ::TestUserDashboard::CreateTestUserAccount.new(users[0]).call
+        expect(tud_account.account_uuid).not_to be_nil
+        expect(tud_account.services).to eq %w[facilities hca edu-benefits form-save-in-progress
+                                              form-prefill]
+        expect(tud_account).to be_a(TestUserDashboard::TudAccount)
+      end
     end
   end
 end
