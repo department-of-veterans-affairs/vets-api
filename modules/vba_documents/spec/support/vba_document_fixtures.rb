e5994bc0eb8061f6eb95c389be1ab68a1e7ee27a
Greg Bowman <gpbowman.git@gmail.com>
2021-03-09 10:36:08 -0500

    Greg.bowman/api 4917 monthly report (#6053)
    * uppercased table column headers and rendered styles from common styles erb file
    
    * initial checkin of monthly report artifacts
    
    * Stubbing out rspec, figuring out how to mock out a method
    
    * added fixtures for local testing of report and added mean, median, mode and max page sizes for the prior 12 months and added use_fixtures switch to be used in settings local to use test data for report generation
    
    * added rspec tests
    
    * Adding in unit test for mailer
    
    * linter changes and rspec completion
    
    * linter strikes again
    
    * upcase status headers
    
    Co-authored-by: Greg Bowman <gregory.bowman@libertyits.com>
    Co-authored-by: Cris Shupp <cristopher.shupp@libertyits.com>

--- modules/vba_documents/spec/support/vba_document_fixtures.rb
+++ modules/vba_documents/spec/support/vba_document_fixtures.rb
@@ -5,5 +5,10 @@ module VBADocuments
     def get_fixture(filename)
       File.new(File.join(File.expand_path('../fixtures', __dir__), filename))
     end
+
+    # returns a data structure based on a YAML.dump call
+    def get_fixture_yml(filename)
+      File.open(File.join(File.expand_path('../fixtures', __dir__), filename), 'r') { |f| YAML.safe_load(f) }
+    end
   end
 end
