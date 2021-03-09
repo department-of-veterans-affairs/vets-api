9a31cd2bde1f32ca3e259f639923a5ccd8a71896
Jayson Perkins <jaysonperkins51@gmail.com>
2021-03-09 16:54:00 -0500

    lint fixes

--- spec/lib/vetext/response_error_spec.rb
+++ spec/lib/vetext/response_error_spec.rb
@@ -3,20 +3,19 @@
 require 'rails_helper'
 require 'vetext/response_error'
 
-describe VEText::ResponseError do
-
+describe 'VEText::ResponseError' do
   describe '#initialize' do
     let(:body) do
       {
-          error: 'Invalid Application SID',
-          id: '123',
-          idType: 'appSid',
-          success: false
+        error: 'Invalid Application SID',
+        id: '123',
+        idType: 'appSid',
+        success: false
       }
     end
 
     it 'initializes the error successfully with the provided data' do
-      subject = described_class.new(body)
+      subject = VEText::ResponseError.new(body)
       expect(subject).not_to be_nil
       expect(subject.instance_variable_get(:@id)).to be(body[:id])
       expect(subject.instance_variable_get(:@id_type)).to be(body[:idType])
