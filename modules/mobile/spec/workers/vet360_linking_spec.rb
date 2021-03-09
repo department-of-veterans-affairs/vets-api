1746827dd4d0d8e963625a761c9f9d4686a9c218
Jayson Perkins <jayson.perkins@adhocteam.us>
2021-03-09 13:42:29 -0500

    19467 mobile link to vet360 (#6058)
    * initial implementation
    
    * tests created, implementation updated
    
    * added an expect for testing redis lock
    
    * lint fix
    
    * updated after grabbing example off staging
    
    * better variable names, logging, ensure lock delete
    
    * logger fix, test update, lint fix

---
+++ modules/mobile/spec/workers/vet360_linking_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../support/iam_session_helper'
+
+RSpec.describe Mobile::V0::Vet360LinkingJob, type: :job do
+  before { iam_sign_in(FactoryBot.build(:iam_user, :no_vet360_id)) }
+
+  before(:all) do
+    @original_cassette_dir = VCR.configure(&:cassette_library_dir)
+    VCR.configure { |c| c.cassette_library_dir = 'modules/mobile/spec/support/vcr_cassettes' }
+  end
+
+  after(:all) { VCR.configure { |c| c.cassette_library_dir = @original_cassette_dir } }
+
+  let(:user) { FactoryBot.build(:iam_user, :no_vet360_id) }
+
+  it 'Returns a completed transaction for linking an account with vet360' do
+    VCR.use_cassette('profile/init_vet360_id_status_complete') do
+      VCR.use_cassette('profile/init_vet360_id_status_incomplete') do
+        VCR.use_cassette('profile/init_vet360_id_success') do
+          expect(Rails.logger).not_to receive(:error)
+          subject.perform(user)
+        end
+      end
+    end
+  end
+end
