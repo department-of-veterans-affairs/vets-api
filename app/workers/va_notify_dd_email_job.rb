26be39f33b424f47e65a275d19d466416378164d
Lihan Li <frankieteardrop@gmail.com>
2021-03-10 15:22:55 -0800

    lint

--- app/workers/va_notify_dd_email_job.rb
+++ app/workers/va_notify_dd_email_job.rb
@@ -22,7 +22,8 @@ class VANotifyDdEmailJob
 
   def perform(email, dd_type)
     notify_client = VaNotify::Service.new(Settings.vanotify.services.va_gov.api_key)
-    template_id = Settings.vanotify.template_id.public_send("direct_deposit_#{dd_type.to_sym == :ch33 ? 'edu' : 'comp_pen'}")
+    template_type = "direct_deposit_#{dd_type.to_sym == :ch33 ? 'edu' : 'comp_pen'}"
+    template_id = Settings.vanotify.template_id.public_send(template_type)
 
     notify_client.send_email(
       email_address: email,
