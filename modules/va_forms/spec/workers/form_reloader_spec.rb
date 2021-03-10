e89a7e23420de82ae95084f5a564418bc0f4ae01
vets-api <mark.viterna@va.gov>
2021-03-10 15:15:08 -0500

    Fix for linter

--- modules/va_forms/spec/workers/form_reloader_spec.rb
+++ modules/va_forms/spec/workers/form_reloader_spec.rb
@@ -95,7 +95,7 @@ RSpec.describe VAForms::FormReloader, type: :job do
       end
 
       it 'loads va form administration' do
-        expect(@form.va_form_administration).to eq("Veterans Benefits Administration")
+        expect(@form.va_form_administration).to eq('Veterans Benefits Administration')
       end
 
       it 'loads form type' do
