67897e4e37a30cf6c7537deda7feb79d0ac39e6a
Anna Carey <anna@adhocteam.us>
2021-03-11 17:24:14 -0500

    Log appeal submission to table

--- spec/request/v0/higher_level_reviews_controller_request_spec.rb
+++ spec/request/v0/higher_level_reviews_controller_request_spec.rb
@@ -24,6 +24,8 @@ RSpec.describe V0::HigherLevelReviewsController, type: :request do
       VCR.use_cassette('decision_review/HLR-CREATE-RESPONSE-200') do
         subject
         expect(response).to be_successful
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response['data']['id']).to eq(AppealSubmission.last.submitted_appeal_uuid)
       end
     end
 
