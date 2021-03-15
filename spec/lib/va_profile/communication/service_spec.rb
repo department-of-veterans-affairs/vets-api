d29e482bc37a57738e85528d94934201f44ed4d5
Lihan Li <frankieteardrop@gmail.com>
2021-03-15 10:28:12 -0700

    working on communication items spec

--- spec/lib/va_profile/communication/service_spec.rb
+++ spec/lib/va_profile/communication/service_spec.rb
@@ -3,6 +3,17 @@
 require 'rails_helper'
 require 'va_profile/communication/service'
 
-describe VAProfile::CommunicationService do
+describe VAProfile::Communication::Service do
+  let(:user) { build(:user, :loa3) }
+
   subject { described_class.new(user) }
+
+  describe '#communication_items' do
+    it 'gets communication items' do
+      VCR.use_cassette('va_profile/communication/communication_items', VCR::MATCH_EVERYTHING) do
+        res = subject.communication_items
+        binding.pry; fail
+      end
+    end
+  end
 end
