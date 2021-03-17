6112c3f51c55b1da1d2f00b6300dcd88166bf2a9
Matt Roth <roth_matthew@bah.com>
2021-03-17 13:39:41 -0400

    Update spool_file_event.rb

--- app/models/spool_file_event.rb
+++ app/models/spool_file_event.rb
@@ -1,17 +1,17 @@
 class SpoolFileEvent < ApplicationRecord
   validates(:rpo, inclusion: EducationForm::EducationFacility::FACILITY_IDS.values)
-  validates_uniqueness_of :filename, scope: [:rpo, :filename]
 
   # Look for an existing row with same filename and RPO
   # and increase retry attempt if wasn't successful from previous attempt
   # Otherwise create a new event
   def self.build_event(rpo, filename)
-    event = where(rpo: rpo, filename: filename).first
+    event = SpoolFileEvent.where(rpo: rpo, filename: filename).first
     if event.present?
       event.update(retry_attempt: event.retry_attempt + 1) if event.successful_at.nil?
       return event
     end
 
-    create(rpo: rpo, filename: filename)
+    binding.pry
+    SpoolFileEvent.create(rpo: rpo, filename: filename)
   end
 end
