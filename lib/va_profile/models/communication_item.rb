40ffbbe7e0bff2eaf49c6b60e81ef924d60b7a70
Lihan Li <frankieteardrop@gmail.com>
2021-03-17 10:50:23 -0700

    add communication item channels

--- lib/va_profile/models/communication_item.rb
+++ lib/va_profile/models/communication_item.rb
@@ -1,7 +1,10 @@
 module VAProfile
   module Models
     class CommunicationItem < Base
+      attribute :id, Integer
       attribute :name, String
+
+      attribute :communication_item_channels, Array[VAProfile::Models::CommunicationItemChannel]
     end
   end
 end
---
+++ lib/va_profile/models/communication_item_channel.rb
@@ -0,0 +1,9 @@
+module VAProfile
+  module Models
+    class CommunicationItemChannel < Base
+      attribute :id, Integer
+      attribute :name, String
+      attribute :description, String
+    end
+  end
+end
