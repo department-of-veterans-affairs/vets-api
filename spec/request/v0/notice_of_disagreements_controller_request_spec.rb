0832eb3d9a15a871e671e3f98e5deb4729ea26e8
Ed Mangimelli <edmangimelli@gmail.com>
2021-03-19 00:46:43 -0400

    create skeleton controller for NOD

---
+++ spec/request/v0/notice_of_disagreements_controller_request_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'support/controller_spec_helper'
+
+RSpec.describe V0::NoticeOfDisagreementsController, type: :request do
+  let(:user) { build(:user, :loa3) }
+  let(:headers) { { 'CONTENT_TYPE' => 'application/json' } }
+
+  before { sign_in_as(user) }
+end
