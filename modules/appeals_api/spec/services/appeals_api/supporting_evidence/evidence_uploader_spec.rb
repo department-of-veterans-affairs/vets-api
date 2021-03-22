2b08c3cebde5f28c3d86d4ac72ad075950274ce9
Nathan Wright <nathanielbwright@gmail.com>
2021-03-22 15:05:54 -0600

    Add logging and updating the status if something goes wrong

--- modules/appeals_api/spec/services/appeals_api/supporting_evidence/evidence_uploader_spec.rb
+++ modules/appeals_api/spec/services/appeals_api/supporting_evidence/evidence_uploader_spec.rb
@@ -9,6 +9,13 @@ module AppealsApi
 
       describe '#process!' do
         let(:uploader) { EvidenceUploader.new(appeal, nil, type: :notice_of_disagreement) }
+        let(:double) { instance_double('TemporaryStorageUploader') }
+
+        before do
+          allow(double).to receive(:store!).and_return(true)
+          allow(double).to receive(:filename)
+          allow(TemporaryStorageUploader).to receive(:new).and_return(double)
+        end
 
         it 'generates an EvidenceSubmission record' do
           expect(appeal.evidence_submissions.count).to eq(0)
@@ -19,14 +26,19 @@ module AppealsApi
         end
 
         it 'calls store! for CarrierWave' do
-          double = instance_double('TemporaryStorageUploader')
-          allow(double).to receive(:store!)
-          allow(double).to receive(:filename)
-          allow(TemporaryStorageUploader).to receive(:new).and_return(double)
-
           uploader.process!
 
           expect(double).to have_received(:store!)
+        end
+
+        it 'handles errors on upload' do
+          allow(double).to receive(:store!).and_raise(RuntimeError)
+          allow(uploader).to receive(:log_message_to_sentry)
+
+          expect { uploader.process! }.to raise_error(RuntimeError)
+          expect(uploader).to have_received(:log_message_to_sentry)
+            .with('Error saving to S3', :warning, error: 'RuntimeError')
+          expect(appeal.evidence_submissions.first.status).to eq('Error uploading')
         end
 
         it 'marks the created submission \'submitted\'' do
