93b02fc96553e4ac9c88f6efe5a0a6e523b66705
Saman Moshafi <56931061+saman-moshafi-va@users.noreply.github.com>
2021-03-25 15:05:29 -0400

    remove vanotify_service_enhancement toggle (#6275)
    department-of-veterans-affairs/notification-api#401
    Some entries under settings.yml had become confusing so this PR removes them as well as removing vanotify_service_enhancement feature toggle

--- spec/jobs/form526_confirmation_email_job_spec.rb
+++ spec/jobs/form526_confirmation_email_job_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Form526ConfirmationEmailJob, type: :worker do
           'reference': nil,
           'scheduled_for': nil,
           'template': {
-            'id': Settings.vanotify.template_id.form526_confirmation_email,
+            'id': Settings.vanotify.services.va_gov.template_id.form526_confirmation_email,
             'uri': 'template_url',
             'version': 1
           },
@@ -39,25 +39,10 @@ RSpec.describe Form526ConfirmationEmailJob, type: :worker do
         }
       end
 
-      it 'the service is initialized with the correct parameters with enabled toggle' do
-        Flipper.enable(:vanotify_service_enhancement)
+      it 'the service is initialized with the correct parameters' do
         test_service_api_key = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa-aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
         with_settings(
           Settings.vanotify.services.va_gov, { api_key: test_service_api_key }
-        ) do
-          mocked_notification_service = instance_double('VaNotify::Service')
-          allow(VaNotify::Service).to receive(:new).and_return(mocked_notification_service)
-          allow(mocked_notification_service).to receive(:send_email).and_return(email_response)
-          subject.perform('')
-          expect(VaNotify::Service).to have_received(:new).with(test_service_api_key)
-        end
-      end
-
-      it 'the service is initialized with the correct parameters with disabled toggle' do
-        Flipper.disable(:vanotify_service_enhancement)
-        test_service_api_key = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa-aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
-        with_settings(
-          Settings.vanotify, { api_key: test_service_api_key }
         ) do
           mocked_notification_service = instance_double('VaNotify::Service')
           allow(VaNotify::Service).to receive(:new).and_return(mocked_notification_service)
@@ -71,6 +56,8 @@ RSpec.describe Form526ConfirmationEmailJob, type: :worker do
         requirements = {
           email_address: email_address,
           template_id: Settings.vanotify
+                               .services
+                               .va_gov
                                .template_id
                                .form526_confirmation_email,
           personalisation: {
