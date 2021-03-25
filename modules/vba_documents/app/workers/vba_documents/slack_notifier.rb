4612edd99a59eaabd1fff43e149eb6d18647f49c
Cris Shupp <cristopher.shupp@libertyits.com>
2021-03-25 14:05:53 -0400

    Adding in update alerts.  Drying code.

--- modules/vba_documents/app/workers/vba_documents/slack_notifier.rb
+++ modules/vba_documents/app/workers/vba_documents/slack_notifier.rb
@@ -7,13 +7,15 @@ module VBADocuments
     include Sidekiq::Worker
     include ActionView::Helpers::DateHelper
 
-    SLACK_URL = Settings.vba_documents.slack_notification_url
-    ALERT_HUNGTIME = Settings.vba_documents.slack_notification_hung_time_in_days.to_i
-    RENOTIFY_TIME = Settings.vba_documents.slack_renotification_in_minutes.to_i
+    SLACK_URL = Settings.vba_documents.slack.notification_url
+    IN_FLIGHT_HUNGTIME = Settings.vba_documents.slack.in_flight_notification_hung_time_in_days.to_i
+    RENOTIFY_TIME = Settings.vba_documents.slack.renotification_in_minutes.to_i
+    UPDATE_HUNGTIME = Settings.vba_documents.slack.update_stalled_notification_in_minutes.to_i
 
     def perform
-      alert
-      daily_notification
+      {long_flyers_alerted: long_flyers_alert,
+       update_stalled_alerted: update_stalled_alert,
+       daily_notification: daily_notification}
     end
 
     private
@@ -26,32 +28,25 @@ module VBADocuments
           start_time = UploadSubmission.aged_processing(0, status).where('created_at > ?', 7.days.ago)
                            .first.metadata['status'][status]['start']
           duration = distance_of_time_in_words(Time.now.to_i - start_time)
-          text = text + "\tStatus \'#{status}\' for approximately #{duration}\n"
+          text = text + "\tStatus \'#{status}\' for #{duration}\n"
         end
-        send_to_slack(text)
+        resp = send_to_slack(text)
       end
+      resp&.success?
     end
 
-    def alert
+    def update_stalled_alert
+      spoof_stalled_updates #todo delete me
+      alert_on = fetch_stuck_in_state(['uploaded'], UPDATE_HUNGTIME, :minutes)
+      text = 'ALERT!! GUIDS in updated for too long!\n'
+      alert(alert_on, text)
+    end
+
+    def long_flyers_alert
       spoof_long_flyers #todo delete me
-      guids_found = false
+      alert_on = fetch_stuck_in_state(UploadSubmission::IN_FLIGHT_STATUSES, IN_FLIGHT_HUNGTIME, :days)
       text = 'ALERT!! GUIDS in flight for too long!\n'
-      alert_on = fetch_long_flyers
-      alert_on.each_pair do |status,models|
-        text = text + "#{status.upcase}:\n"
-        models.each do |m|
-          start_time = m.metadata['status'][status]['start']
-          #start_time = start_time - ALERT_LOOKBACK.days #todo remove line
-          duration = distance_of_time_in_words(Time.now.to_i - start_time)
-          text = text + "\tGUID: #{m.guid} for approximately #{duration}\n"
-          guids_found = true
-        end
-      end
-      puts "GUIDS found is #{guids_found}"
-      resp = send_to_slack(text) if guids_found
-      if resp&.success?
-        add_notification_timestamp(alert_on.values.flatten)
-      end
+      alert(alert_on, text)
     end
 
     def send_to_slack(text)
@@ -66,6 +61,17 @@ module VBADocuments
       end
     end
 
+    def spoof_stalled_updates
+      3.times do |i|
+        u = UploadSubmission.new
+        status = 'uploaded'
+        u.status = status
+        u.save!
+        u.metadata['status'][status]['start'] = (6 + i).hours.ago.to_i
+        u.save!
+      end
+    end
+
     def spoof_long_flyers
       UploadSubmission.destroy_all
       1.times do |i|
@@ -73,7 +79,7 @@ module VBADocuments
         status = 'received'
         u.status = status
         u.save!
-        u.metadata['status'][status]['start'] = (15 + i).seconds.ago.to_i
+        u.metadata['status'][status]['start'] = (15 + i).days.ago.to_i
         u.save!
       end
       3.times do |i|
@@ -81,7 +87,7 @@ module VBADocuments
         status = 'processing'
         u.status = status
         u.save!
-        u.metadata['status'][status]['start'] = (15 + i).hours.ago.to_i
+        u.metadata['status'][status]['start'] = (15 + i).days.ago.to_i
         u.save!
       end
       20.times do |i|
@@ -89,20 +95,39 @@ module VBADocuments
         u = UploadSubmission.new
         u.status = status
         u.save!
-        u.metadata['status'][status]['start'] = (15 + i).minutes.ago.to_i
+        u.metadata['status'][status]['start'] = (15 + i).days.ago.to_i
         u.save!
       end
       puts 'done with spoof'
     end
 
-    def fetch_long_flyers
+    def alert(alert_on, initial_text)
+      guids_found = false
+      text = initial_text
+      alert_on.each_pair do |status,models|
+        text = text + "#{status.upcase}:\n"
+        models.each do |m|
+          start_time = m.metadata['status'][status]['start']
+          duration = distance_of_time_in_words(Time.now.to_i - start_time)
+          text = text + "\tGUID: #{m.guid} for #{duration}\n"
+          guids_found = true
+        end
+      end
+      resp = send_to_slack(text) if guids_found
+      if resp&.success?
+        add_notification_timestamp(alert_on.values.flatten)
+      end
+      resp&.success?
+    end
+
+    def fetch_stuck_in_state(states, hungtime, unit_of_measure)
       alerting_on = {}
-      UploadSubmission::IN_FLIGHT_STATUSES.each do |status|
-        alerting_on[status] = UploadSubmission.aged_processing(ALERT_HUNGTIME, status).limit(10).select do |m|
+      states.each do |status|
+        alerting_on[status] = UploadSubmission.aged_processing(hungtime, unit_of_measure, status).limit(10).select do |m|
           last_notified = m.metadata['last_slack_notification'].to_i #nil to zero
           delta = Time.now.to_i - last_notified
           notify = delta > RENOTIFY_TIME*60
-          puts "notify is #{notify} delta is #{delta} with last notified being #{last_notified}"
+          #puts "notify is #{notify} for status #{status} delta is #{delta} with last notified being #{last_notified}"
           notify
         end
       end
