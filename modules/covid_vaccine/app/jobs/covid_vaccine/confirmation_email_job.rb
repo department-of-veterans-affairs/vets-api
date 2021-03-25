72357278df84933bfe3d84eec7afcaadb8d7be01
Kam Karshenas <kam@adhocteam.us>
2021-03-25 11:54:07 -0500

    adding the new VANotify job for covid vaccine confirmation emails

---
+++ modules/covid_vaccine/app/jobs/covid_vaccine/confirmation_email_job.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'sentry_logging'
+require 'va_notify/service'
+
+module CovidVaccine
+  class ConfirmationEmailJob
+    include Sidekiq::Worker
+    include SentryLogging
+    sidekiq_options expires_in: 1.day, retry: 2
+
+    STATSD_ERROR_NAME = 'worker.covid_vaccine_confirmation_email.error'
+    STATSD_SUCCESS_NAME = 'worker.covid_vaccine_confirmation_email.success'
+
+    def perform(email, date, sid)
+      submission = CovidVaccine::V0::RegistrationSubmission.find_by(sid: sid)
+      if submission.nil?
+        log_message_to_sentry('No SID found!', :warn, {email: email, sid: sid})
+        return
+      end
+      return if submission.email_confirmation_id.present?
+
+      if Flipper.enabled?(:vanotify_service_enhancement)
+        notify_client ||= VaNotify::Service.new(Settings.vanotify.services.va_gov.api_key)
+        template_id ||= Settings.vanotify.services.va_gov.template_id.covid_vaccine_registration
+      else
+        notify_client ||= VaNotify::Service.new(Settings.vanotify.api_key)
+        template_id ||= Settings.vanotify.template_id.covid_vaccine_registration
+      end
+
+      email_response = notify_client.send_email(
+        email_address: email,
+        template_id: template_id,
+        personalisation: {
+          'date' => date,
+          'confirmation_id' => sid
+        },
+        reference: sid
+      )
+      handle_success(submission, email_response)
+    rescue => e
+      handle_errors(e, sid)
+    end
+
+    def handle_success(submission, email_response)
+      submission.email_confirmation_id = email_response.id
+      submission.save
+      StatsD.increment(STATSD_SUCCESS_NAME)
+    end
+
+    def handle_errors(ex, sid)
+      log_exception_to_sentry(ex, { sid: sid })
+      StatsD.increment(STATSD_ERROR_NAME)
+
+      if ex.respond_to?(:status_code)
+        raise ex if ex.status_code.between?(500, 599)
+      else
+        raise ex
+      end
+    end
+  end
+end
---
+++ modules/covid_vaccine/spec/jobs/covid_vaccine/confirmation_email_job_spec.rb
@@ -0,0 +1,122 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CovidVaccine::ConfirmationEmailJob, type: :worker do
+  describe '#perform' do
+    subject(:job) { described_class.perform_async(email, date, confirmation_id) }
+
+    let(:email) { 'fakeemail@email.com' }
+    let(:date) { 'December, 10, 2020' }
+    let(:confirmation_id) { 'confirmation_id_uuid' }
+
+    it 'logs message to sentry and returns if no submission exists' do
+      expect(Raven).to receive(:capture_message).with('No SID found!', { level: 'warning' })
+      expect(Raven).to receive(:extra_context).with(email: email, sid: confirmation_id)
+      expect(VaNotify::Service).not_to receive(:new)
+      with_settings(Settings.sentry, dsn: 'T') do
+        described_class.new.perform(email, date, confirmation_id)
+      end
+    end
+
+    it 'returns if an email confirmation id exists on submission without logging' do
+      expect(VaNotify::Service).not_to receive(:new)
+      described_class.new.perform(email, date, confirmation_id)
+    end
+
+    context 'with a valid submission that already has email confirmation id' do
+      before(:each) { create(:covid_vax_registration, sid: confirmation_id, email_confirmation_id: '1234') }
+
+      it 'the service is not invoked and we return right away' do
+        expect(VaNotify::Service).not_to receive(:new)
+        described_class.new.perform(email, date, confirmation_id)
+      end
+    end
+
+    context 'with a valid submission that does not have email confirmation id' do
+      let(:submission) { create(:covid_vax_registration, sid: confirmation_id) }
+      before(:each) { submission }
+
+      it 'the service is initialized with the correct parameters with enabled toggle' do
+        Flipper.enable(:vanotify_service_enhancement)
+        test_service_api_key = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa-aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+        instance = instance_double(VaNotify::Service)
+        allow(instance).to receive(:send_email).and_return(double('EmailResponse', id: 'VANotifyID'))
+        with_settings(
+          Settings.vanotify.services.va_gov, { api_key: test_service_api_key }
+        ) do
+          expect(VaNotify::Service).to receive(:new).with(test_service_api_key).and_return(instance)
+          expect(submission.email_confirmation_id).to be_nil
+          described_class.new.perform(email, date, confirmation_id)
+          expect(submission.reload.email_confirmation_id).to eq('VANotifyID')
+        end
+      end
+
+      it 'the service is initialized with the correct parameters with disabled toggle' do
+        Flipper.disable(:vanotify_service_enhancement)
+        test_service_api_key = 'baaaaaaa-1111-aaaa-aaaa-aaaaaaaaaaaa-aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+        instance = instance_double(VaNotify::Service)
+        allow(instance).to receive(:send_email).and_return(double('EmailResponse', id: 'VANotifyID'))
+        with_settings(
+          Settings.vanotify, { api_key: test_service_api_key }
+        ) do
+          expect(VaNotify::Service).to receive(:new).with(test_service_api_key).and_return(instance)          
+          expect(submission.email_confirmation_id).to be_nil
+          described_class.new.perform(email, date, confirmation_id)
+          expect(submission.reload.email_confirmation_id).to eq('VANotifyID')
+        end
+      end
+
+      it 'queues the job' do
+        expect { job }
+          .to change(described_class.jobs, :size).by(1)
+      end
+
+      it 'is in urgent queue' do
+        expect(described_class.queue).to eq('default')
+      end
+
+      it 'executes perform' do
+        instance = instance_double(VaNotify::Service)
+        allow(VaNotify::Service).to receive(:new).and_return(instance)
+
+        expect(instance)
+          .to receive(:send_email).with(
+            {
+              email_address: email,
+              template_id: Settings.vanotify.template_id.covid_vaccine_confirmation,
+              personalisation: {
+                'date' => date,
+                'confirmation_id' => confirmation_id
+              },
+              reference: confirmation_id
+            }
+          ).and_return(double('EmailResponse', id: 'VANotifyID'))
+        described_class.perform_async(email, date, confirmation_id)
+        expect(Rails.logger).to receive(:info).with(
+          '[StatsD] increment worker.covid_vaccine_confirmation_email.success:1'
+        ).once
+        expect(submission.email_confirmation_id).to be_nil
+        expect { described_class.perform_one }.to change(described_class.jobs, :size).from(1).to(0)
+        expect(submission.reload.email_confirmation_id).to eq('VANotifyID')
+      end
+
+      it 'handles errors' do
+        allow(VaNotify::Service).to receive(:new).and_raise(StandardError)
+
+        described_class.perform_async(email, date, confirmation_id)
+        expect(Raven).to receive(:capture_exception).with(StandardError, { level: 'error' })
+        expect(Raven).to receive(:extra_context).with(sid: 'confirmation_id_uuid')
+        expect(Rails.logger).to receive(:info).with(
+          '[StatsD] increment worker.covid_vaccine_confirmation_email.error:1'
+        ).once
+
+        with_settings(Settings.sentry, dsn: 'T') do
+          expect(submission.email_confirmation_id).to be_nil
+          expect { described_class.perform_one }.to raise_error(StandardError)
+          expect(submission.email_confirmation_id).to be_nil
+        end
+      end
+    end
+  end
+end
