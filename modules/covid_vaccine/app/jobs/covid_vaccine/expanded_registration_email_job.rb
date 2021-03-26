3299b56cc2cb360c2f6c5dde382426472517ea80
Patrick Vinograd <patrick@adhocteam.us>
2021-03-26 14:57:37 -0700

    Adds mailer and specs

---
+++ modules/covid_vaccine/app/jobs/covid_vaccine/expanded_registration_email_job.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'sentry_logging'
+require 'va_notify/service'
+
+module CovidVaccine
+  class ExpandedRegistrationEmailJob
+    include Sidekiq::Worker
+    include SentryLogging
+    sidekiq_options expires_in: 1.day, retry: 2
+
+    STATSD_ERROR_NAME = 'worker.covid_vaccine_expanded_registration_email.error'
+    STATSD_SUCCESS_NAME = 'worker.covid_vaccine_expanded_registration_email.success'
+
+    def perform(record_id, email, date)
+      submission = CovidVaccine::V0::ExpandedRegistrationSubmission.find_by(id: record_id)
+      if submission.nil?
+        log_message_to_sentry('No Record found!', :warn, { record_id: record_id, email: email })
+        return
+      end
+      return if submission.email_confirmation_id.present?
+
+      notify_client ||= VaNotify::Service.new(Settings.vanotify.services.va_gov.api_key)
+      template_id ||= Settings.vanotify.services.va_gov.template_id.covid_vaccine_registration
+
+      notify_response = notify_client.send_email(email_address: email, template_id: template_id,
+                                                 personalisation: { 'date' => date,
+                                                                    'confirmation_id' => submission.submission_uuid },
+                                                 reference: submission.submission_uuid)
+      handle_success(submission, notify_response)
+    rescue => e
+      handle_errors(e, submission.submission_uuid)
+    end
+
+    def handle_success(submission, notify_response)
+      submission.update!(email_confirmation_id: notify_response.id)
+      StatsD.increment(STATSD_SUCCESS_NAME)
+    end
+
+    def handle_errors(ex, submission_uuid)
+      log_exception_to_sentry(ex, { submission_uuid: submission_uuid })
+      StatsD.increment(STATSD_ERROR_NAME)
+
+      if ex.respond_to?(:status_code)
+        raise ex if ex.status_code.between?(500, 599)
+      else
+        raise ex
+      end
+    end
+  end
+end
---
+++ modules/covid_vaccine/spec/jobs/covid_vaccine/expanded_registration_email_job_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CovidVaccine::ExpandedRegistrationEmailJob, type: :worker do
+  subject { described_class.new }
+
+  let(:email_confirmation_id) { nil }
+  let(:email) { 'vets.gov.user+0@gmail.com' }
+  let(:date) { Time.current.to_s }
+  let(:registration_submission) do
+    create(:covid_vax_expanded_registration, email_confirmation_id: email_confirmation_id)
+  end
+
+  describe '#perform' do
+    it 'logs message to sentry and returns if no submission exists' do
+      with_settings(Settings.sentry, dsn: 'T') do
+        expect(VaNotify::Service).not_to receive(:new)
+        expect(Raven).to receive(:capture_message)
+        subject.perform('non-existent-submission-id', email, date)
+      end
+    end
+
+    context 'when an email confirmation ID exists' do
+      let(:email_confirmation_id) { 1234 }
+
+      it 'avoid sending an email if an email confirmation id is already present' do
+        expect(VaNotify::Service).not_to receive(:new)
+        subject.perform(registration_submission.id, email, date)
+      end
+    end
+
+    context 'with a valid submission' do
+      it 'updates the record with a response id' do
+        VCR.use_cassette('covid_vaccine/vanotify/send_email', match_requests_on: %i[method path]) do
+          subject.perform(registration_submission.id, email, date)
+          registration_submission.reload
+          expect(registration_submission.email_confirmation_id).to be_truthy
+        end
+      end
+
+      it 'increments StatsD the record with a response id' do
+        VCR.use_cassette('covid_vaccine/vanotify/send_email', match_requests_on: %i[method path]) do
+          allow(StatsD).to receive(:increment)
+          expect(StatsD).to receive(:increment).with('worker.covid_vaccine_expanded_registration_email.success')
+          subject.perform(registration_submission.id, email, date)
+          registration_submission.reload
+          expect(registration_submission.email_confirmation_id).to be_truthy
+        end
+      end
+    end
+
+    context 'with an error response from VANotify' do
+      it 'raises an exception' do
+        expect_any_instance_of(VaNotify::Service).to receive(:send_email)
+          .and_raise(Common::Exceptions::BadGateway)
+        expect(StatsD).to receive(:increment).with('worker.covid_vaccine_expanded_registration_email.error')
+        expect { subject.perform(registration_submission.id, email, date) }.to raise_error(StandardError)
+      end
+
+      it 'increments the StatsD error counter' do
+        expect_any_instance_of(VaNotify::Service).to receive(:send_email)
+          .and_raise(StandardError.new('test error'))
+        expect(StatsD).to receive(:increment).with('worker.covid_vaccine_expanded_registration_email.error')
+        expect { subject.perform(registration_submission.id, email, date) }.to raise_error(StandardError)
+      end
+    end
+  end
+end
