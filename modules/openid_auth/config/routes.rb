d932d1465e02144a8c2efb0960cd58cc792aa1ec
dbrown-va <derek.brown5@va.gov>
2021-03-30 16:28:25 -0400

    Removed unused route for this PR

--- modules/openid_auth/config/routes.rb
+++ modules/openid_auth/config/routes.rb
@@ -25,8 +25,5 @@ OpenidAuth::Engine.routes.draw do
     namespace :v1, defaults: { format: 'json' } do
       get 'validation', to: 'validation#index'
     end
-    namespace :v2, defaults: { format: 'json' } do
-      get 'validation', to: 'validation#index'
-    end
   end
 end
