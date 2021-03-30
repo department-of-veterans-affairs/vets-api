3c3e751db3b0314a39493f848661137b65a330cd
Lihan Li <frankieteardrop@gmail.com>
2021-03-30 13:12:28 -0700

    fix va profile config

--- lib/va_profile/communication/configuration.rb
+++ lib/va_profile/communication/configuration.rb
@@ -4,7 +4,7 @@ module VAProfile
   module Communication
     class Configuration < VAProfile::Configuration
       def base_path
-        "#{VAProfile::Configuration::SETTINGS.url}/communication-hub/communication/v1/"
+        "#{Settings.vet360.url}/communication-hub/communication/v1/"
       end
 
       def service_name
