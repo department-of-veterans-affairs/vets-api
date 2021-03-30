b95d50ab6f2b9f3acbbc255ae31963828c29f6e5
Riley Anderson <5150r.d.anderson@gmail.com>
2021-03-30 12:44:18 -0600

    use minimum coverage not minimum coverage by file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -71,7 +71,7 @@ unless ENV['NOCOVERAGE']
     # End Modules
 
     if ENV['CI']
-      SimpleCov.minimum_coverage_by_file 90
+      SimpleCov.minimum_coverage 90
       SimpleCov.refuse_coverage_drop
     end
   end
