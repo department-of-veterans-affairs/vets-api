60c5e79d7de7ff5e8ab45cade5bdf6435f10ae65
Lihan Li <frankieteardrop@gmail.com>
2021-03-29 17:54:52 -0700

    add monitoring

--- lib/va_profile/communication/service.rb
+++ lib/va_profile/communication/service.rb
@@ -6,17 +6,18 @@ require 'va_profile/models/communication_item_group'
 module VAProfile
   module Communication
     class Service < VAProfile::Service
+      include Common::Client::Concerns::Monitoring
+
       OID = '2.16.840.1.113883.4.349'
       VA_PROFILE_ID_POSTFIX = '^PI^200VETS^USDVA'
 
       configuration VAProfile::Communication::Configuration
 
-      # TODO add monitoring
       def update_communication_permission(communication_item)
-        perform(
+        perform_with_monitoring(
           communication_item.http_verb,
           "#{get_path_ids}communication-permissions", communication_item.in_json(@user.vet360_id)
-        ).body
+        )
       end
 
       def get_items_and_permissions
@@ -27,14 +28,22 @@ module VAProfile
       end
 
       def get_communication_permissions
-        perform(:get, "#{get_path_ids}communication-permissions").body
+        perform_with_monitoring(:get, "#{get_path_ids}communication-permissions")
       end
 
       def get_communication_items
-        perform(:get, 'communication-items').body
+        perform_with_monitoring(:get, 'communication-items')
       end
 
       private
+
+      def perform_with_monitoring(*args)
+        with_monitoring do
+          perform(*args).body
+        end
+      rescue => e
+        handle_error(e)
+      end
 
       def get_path_ids
         id_with_aaid = ERB::Util.url_encode("#{@user.vet360_id}#{VA_PROFILE_ID_POSTFIX}")
