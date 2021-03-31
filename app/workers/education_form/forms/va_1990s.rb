0c514be3845dc23523ece75b0f18102825979ea5
Matt Roth <roth_matthew@bah.com>
2021-03-31 12:24:08 -0400

    spool file fixes

--- app/workers/education_form/forms/va_1990s.rb
+++ app/workers/education_form/forms/va_1990s.rb
@@ -12,31 +12,12 @@ module EducationForm::Forms
       'onlineAndInPerson': 'Online and in person'
     }.freeze
 
-    def applicant_ssn
-      @applicant.veteranSocialSecurityNumber
-    end
-
-    def applicant_name
-      @applicant.veteranFullName
-    end
-
     def location
       return '' if @applicant.providerName.blank?
 
       "#{@applicant.programCity}, #{@applicant.programState}"
     end
 
-    def full_address_with_street3(address, indent: false)
-      return '' if address.nil?
 
-      seperator = indent ? "\n        " : "\n"
-      [
-        address.street,
-        address.street2,
-        address.street3,
-        [address.city, address.state, address.postalCode].compact.join(', '),
-        address.country
-      ].compact.join(seperator).upcase
-    end
   end
 end
