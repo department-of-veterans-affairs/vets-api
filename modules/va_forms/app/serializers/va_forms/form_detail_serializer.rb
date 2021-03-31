ec79bc05a7f9cac75022dabbeb3c70680b54f941
vets-api <mark.viterna@va.gov>
2021-03-31 16:01:08 -0400

    Reverting back to the original code. I think there may be a bug here that I discovered locally, but not sure it is in prod yet

--- modules/va_forms/app/serializers/va_forms/form_detail_serializer.rb
+++ modules/va_forms/app/serializers/va_forms/form_detail_serializer.rb
@@ -15,13 +15,11 @@ module VAForms
     end
 
     def versions
-      if object&.versions&.last&.created_at
-        object.versions.map do |v|
-          {
-            sha256: v.changeset['sha256']&.last,
-            revision_on: v.created_at&.strftime('%Y-%m-%d')
-          }
-        end
+      object.versions.map do |v|
+        {
+          sha256: v.changeset['sha256']&.last,
+          revision_on: v.created_at&.strftime('%Y-%m-%d')
+        }
       end
     end
   end
