4852c9353e1527696d99dc0e990d6a480e34fd15
Patrick Vinograd <patrick@adhocteam.us>
2021-04-02 10:51:36 -0700

    Simplifies processor

--- modules/covid_vaccine/app/services/covid_vaccine/v0/enrollment_processor.rb
+++ modules/covid_vaccine/app/services/covid_vaccine/v0/enrollment_processor.rb
@@ -12,50 +12,54 @@ module CovidVaccine
 
       attr_reader :batch_id
 
-      def process_and_upload(max = nil)
-        records = batch_records(max)
-        # TODO: write elibility_info for each submission
+      def process_and_upload
+        records = batch_records
         csv_generator = ExpandedRegistrationCsvGenerator.new(records)
         filename = generated_file_name(records.length)
         uploader = CovidVaccine::V0::EnrollmentUploadService.new(csv_generator.io, filename)
         uploader.upload
-        self.class.set_pending_state(@batch_id)
+        update_state_to_pending
+      rescue
+        # TODO
       end
 
-      def write_to_file(max = nil)
-        records = batch_records(max)
-        # TODO: write elibility_info for each submission
-        csv_generator = ExpandedRegistrationCsvGenerator.new(records)
-        filename = generated_file_name(records.length)
-        File.open(filename, 'w') do |file|
-          file.write csv_generator.io.read
-        end
-        filename
+      # rubocop:disable Rails/SkipsModelValidations
+      def update_state_to_pending
+        CovidVaccine::V0::ExpandedRegistrationSubmission
+          .where(batch_id: @batch_id).update_all(state: 'enrollment_pending')
       end
+      # rubocop:enable Rails/SkipsModelValidations
 
-      def self.set_pending_state(batch_id)
-        CovidVaccine::V0::ExpandedRegistrationSubmission.where(batch_id: batch_id).find_each do |submission|
-          submission.submitted_for_enrollment
-          submission.save!
-        end
-      end
+      # def write_to_file(max = nil)
+      #   records = batch_records(max)
+      #   csv_generator = ExpandedRegistrationCsvGenerator.new(records)
+      #   filename = generated_file_name(records.length)
+      #   File.open(filename, 'w') do |file|
+      #     file.write csv_generator.io.read
+      #   end
+      #   filename
+      # end
+
+      # def self.set_pending_state(batch_id)
+      #   CovidVaccine::V0::ExpandedRegistrationSubmission.where(batch_id: batch_id).find_each do |submission|
+      #     submission.submitted_for_enrollment
+      #     submission.save!
+      #   end
+      # end
 
       # TODO: Should this be private? Or public to be used by scanner job
-      def batch_records(max)
-        limit = max.to_i
+      # rubocop:disable Rails/SkipsModelValidations
+      def batch_records
         records = CovidVaccine::V0::ExpandedRegistrationSubmission.where(state: 'received', batch_id: nil)
-        records = records.limit(limit) if limit.positive?
-        records.find_each do |r|
-          r.update!(batch_id: @batch_id)
-        end
+        records.update_all(batch_id: @batch_id)
         CovidVaccine::V0::ExpandedRegistrationSubmission.where(batch_id: @batch_id)
       end
+      # rubocop:enable Rails/SkipsModelValidations
 
       private
 
       def generated_file_name(record_count)
         "#{@prefix}_#{batch_id}_SLA_#{record_count}_records.txt"
-        # "DHS_load_#{Time.now.utc.strftime('%Y%m%d%H%M%S')}_SLA_#{@records.size}_records.txt"
       end
     end
   end
