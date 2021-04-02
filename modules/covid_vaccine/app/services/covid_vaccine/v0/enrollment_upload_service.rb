61b534d3bde301f121fe62743cb8607a44bf5859
Patrick Vinograd <patrick@adhocteam.us>
2021-04-02 09:19:57 -0700

    Upload service updates

--- modules/covid_vaccine/app/services/covid_vaccine/v0/enrollment_upload_service.rb
+++ modules/covid_vaccine/app/services/covid_vaccine/v0/enrollment_upload_service.rb
@@ -6,32 +6,23 @@ require 'covid_vaccine/v0/expanded_registration_submission_csv_generator'
 module CovidVaccine
   module V0
     class EnrollmentUploadService
-      def initialize(io, batch_id, record_count, prefix: 'DHS_load')
+      def initialize(io, file_name)
         @io = io
-        @batch_id = batch_id
-        @record_count = record_count
-        @prefix = prefix
+        @file_name = file_name
       end
 
       attr_reader :records, :io
 
       def upload
-        file_name = generated_file_name
-
         Net::SFTP.start(sftp_host, sftp_username, password: sftp_password) do |sftp|
-          sftp.upload!(@io, remote_file_path(file_name), name: file_name, progress: EnrollmentHandler.new)
+          sftp.upload!(@io, remote_file_path(@file_name), name: @file_name, progress: EnrollmentHandler.new)
         end
       end
 
       private
 
       def remote_file_path(name)
-        "/#{name}"
-      end
-
-      def generated_file_name
-        "#{@prefix}_#{batch_id}_SLA_#{@record_count}_records.txt"
-        # "DHS_load_#{Time.now.utc.strftime('%Y%m%d%H%M%S')}_SLA_#{@records.size}_records.txt"
+        "#{name}"
       end
 
       def sftp_host
