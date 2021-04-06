910acdcf75915015bb2b2f48dfaaf8e2eba3ee1e
Dillo Raju <draju@governmentcio.com>
2021-04-06 15:07:29 -0400

    19061 require marsheller explicitly

--- modules/health_quest/app/models/health_quest/questionnaire_response.rb
+++ modules/health_quest/app/models/health_quest/questionnaire_response.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json_marshal/marshaller'
+
 module HealthQuest
   ##
   # An ActiveRecord object for modeling and persisting questionnaire response and user demographics data to the DB.
