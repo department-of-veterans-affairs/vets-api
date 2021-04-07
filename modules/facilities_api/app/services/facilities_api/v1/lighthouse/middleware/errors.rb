c492311cee4590d637f6acec1cc63842422a5c49
Lance Sanchez <DarkBushido@users.noreply.github.com>
2021-04-07 14:51:54 -0500

    Fixed the name of the errors class
    removed some un-needed requires

--- modules/facilities_api/app/services/facilities_api/v1/lighthouse/middleware/errors.rb
+++ modules/facilities_api/app/services/facilities_api/v1/lighthouse/middleware/errors.rb
@@ -1,30 +1,32 @@
 # frozen_string_literal: true
 
-module Lighthouse
-  module Facilities
-    module Middleware
-      class Errors < Faraday::Response::Middleware
-        def on_complete(env)
-          return if env.success?
+module FacilitiesApi
+  module V1
+    module Lighthouse
+      module Middleware
+        class Errors < Faraday::Response::Middleware
+          def on_complete(env)
+            return if env.success?
 
-          env.body = parse_body(env)
-        end
+            env.body = parse_body(env)
+          end
 
-        private
+          private
 
-        def parse_body(env)
-          body = JSON.parse(env.body)
-          message = body.dig('message')
+          def parse_body(env)
+            body = JSON.parse(env.body)
+            message = body.dig('message')
 
-          body['detail'] = message
-          body['code'] = env.status
-          body['source'] = 'Lighthouse Facilities'
+            body['detail'] = message
+            body['code'] = env.status
+            body['source'] = 'Lighthouse Facilities'
 
-          body
+            body
+          end
         end
       end
     end
   end
 end
 
-Faraday::Response.register_middleware lighthouse_facilities_errors: Lighthouse::Facilities::Middleware::Errors
+Faraday::Response.register_middleware lighthouse_facilities_errors: FacilitiesApi::V1::Lighthouse::Middleware::Errors
--- modules/facilities_api/spec/services/facilities_api/v1/lighthouse/client_spec.rb
+++ modules/facilities_api/spec/services/facilities_api/v1/lighthouse/client_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'lighthouse/facilities/client'
 
 vcr_options = {
   cassette_name: '/lighthouse/facilities',
