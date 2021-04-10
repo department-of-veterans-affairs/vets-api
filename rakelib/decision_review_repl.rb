95a96aa6d6e63ef79a503a8204771800d555d749
Ed Mangimelli <edmangimelli@gmail.com>
2021-04-09 21:48:39 -0400

    reword

--- rakelib/decision_review_repl.rb
+++ rakelib/decision_review_repl.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # frozen_string_literal: true
 
 # run this with:  bin/rails runner rakelib/decision_review_repl.rb
@@ -10,152 +11,179 @@ require_relative 'piilog_repl/piilog_helpers.rb'
 alias _p p
 alias _pp pp
 
-def info_string
-  <<~INFO
-
-    # example: get the PersonalInformationLogs for HLR for the past week
-
-    logs = Q.(:hlr, 7.days)     # returns a PersonalInformationLog relation
-
-    # Q is the PersonalInformationLogQueryBuilder --a lambda that makes it easier to query PersonalInformationLog.
-
-    # It returns a relation:
-
-    logs = Q.(:hlr, :nod).where('updated_at > ?' Time.zone.yesterday)   #  that's hlr /or/ nod exceptions
-
-    # PersonalInformationLogs are hard to wrangle because so much gets dumped into its 'data' attribute.
-
-    # There are a handful of PersonalInformationLog "wrapper classes" that add handy methods depending on
-    # the type of PersonalInformationLog (PiiLogs recorded in the controller as opposed to the service,
-    # for instance). Scroll down to look through them and what they offer, /BUT/, you do *not* need to
-    # explicitly call them. The wrap command will take a PersonalInformationLog (or Logs) and wrap each
-    # one with the appropriate wrapper class. You end up with an array of PersonalInformationLog objects,
-    # that still work just like PersonalInformationLog objects should, but, if the PiiLog was recorded in
-    # the controller, for example, it will have user info helper methods, if it was recorded near where
-    # the response gets tested against the schema, it will have schema helper methods and so on.
-
-    logs = wrap logs    # returns an array with helper methods added to each PersonalInformationLog
-                        # depending on its type (was it thrown in a controller, the service, etc.)
-
-
-    # There are a slew of methods that work with an array of wrapped PiiLogs.
-
-    # for instance:
-
-    hash = by_error_message_category(wrap(Q.(:hlr, :yesterday))) # notice the "wrap" nestled in there
-
-    # this will return a hash that organizes the array of PiiLogs by their error message category
-
-    # if you pretty print it, it will look something like:
-
-    p hash
-
-    {"Outage..."=>
-      [#<PersonalInformationLog:0x0000000000000000
-        id: 0000000,
-        data:
-         {"user"=>
-           {"icn"=> ...
-            "ssn"=> ...
-     "Timeout"=> ...
-
-
-
-    # see "spec/rakelib/piilog_repl/piilog_helpers_spec.rb" for more examples of using
-    # the PersonalInformationLogQueryBuilder
-
-
-
-    # MISC:
-
-    # When a single time is specified (only 1 time arg that is a time not a date), there are a few
-    # special rules to get handy intervals --some examples:
-    # If the current second is non-zero you get the time range of the beginning of the current min
-    # to the end of the current minute.
-    # If the current second is zero, you get a time range of -1 min -> +1 min for the current min
-    # ... and so on .. see TIMES_TO_WHERE_ARGS for the full rule set for how time arguments are
-    # interpreted.
-
-  INFO
-end
-
-def info
-  puts info_string
-end
-
 def recipes_string
   <<~RECIPES
 
-    #############     type 'info' for more info
-    #  RECIPES  #
-    #############
+            #############
+            #  RECIPES  #
+            #############
 
 
 
-    # /\\ get an HLR piilog that
-    # \\/  was just now logged:
+            # 🍝 get an HLR piilog that was created right now (roughly)
 
-    logs = Q.(:hlr, :now)   # this will give you the time range of the current minute.
-                            # beside timestamp strings you can use :now, :today, :yesterday,
-                            # :tomorrow as well as any time/date obj
-
-
-
-
-    # /\\  get the error counts for the
-    # \\/   last week of NOD errors
-
-
-    # print the error counts by category:
-
-      puts_by_error_message_category_counts wrap Q.(:nod, 7.days)
-
-      #  {"Gateway timeout"=>7,
-      #   "BackendServiceException [DR_422]"=>7,
-      #   "Outage..."=>6,
-      #   "BackendServiceException [DR_404]"=>2,
-      #   "BackendServiceException [unmapped_service_exception]"=>1}
-
-    # print the same counts, but first organize by where the exception occurred
-
-      puts_by_error_class_counts wrap Q.(:nod, 7.days)
-
-      #  {"V0::HigherLevelReviews::ContestableIssuesController#index exception Common::Exceptions::GatewayTimeout (HLR)"=>
-      #    {"Gateway timeout"=>7},
-      #   "V0::HigherLevelReviewsController#create exception DecisionReview::ServiceException (HLR)"=>
-      #    {"BackendServiceException [DR_422]"=>7},
-      #   "V0::HigherLevelReviews::ContestableIssuesController#index exception Breakers::OutageException (HLR)"=>
-      #    {"Outage..."=>4},
-      #   "V0::HigherLevelReviews::ContestableIssuesController#index exception DecisionReview::ServiceException (HLR)"=>
-      #    {"BackendServiceException [DR_404]"=>2,
-      #     "BackendServiceException [unmapped_service_exception]"=>1},
-      #   "V0::HigherLevelReviewsController#create exception Breakers::OutageException (HLR)"=>
-      #    {"Outage..."=>2}}
-
-    # now you can see that some of those 6 outages occurred at the ContestableIssuesController#index and
-    # some at HigherLevelReviewsController#create.
+            logs = Q.(:hlr, :now)   # builds/returns a PersonalInformationLog relation
+                                    # this will give you the time range of the current minute.
+                                    # beside timestamp strings you can use :now, :today, :yesterday,
+                                    # :tomorrow as well as any time/date obj
 
 
 
 
-    # /\\ get the error counts for
-    # \\/  the past 14 days
+            # 🍛 get all piilogs that have an error_class that includes either the string
+            # "DocumentUploader" or "nod" (ILIKE) that was created between two dates
 
-    14.downto(1).each do |d|
-      date = (Time.zone.now - d.days).to_date
-      count = Q.(:hlr, :nod, date).count
-      puts "%s  %s %3d" % [date.strftime('%A')[0..2], date, count]
-    end
-
+            logs = Q.('DocumentUploader', 'nod', '2021-03-14', '2021-03-15')   # you can pass up to 2 time arguments
+                                                                               # which can be time, date, duration
+                                                                               # or even nil (to express an open-ended
+                                                                               # range)
 
 
-    (type 'info' for more info)
+
+
+            # 🌯 get all NOD piilogs that occurred on the day before yesterday
+
+            logs = Q.(2.days.ago.to_date, :nod)   # Note the 'to_date'! Passing a single time arg that is a date means
+                                                  # everything on that date. When passing a single time arg that is a
+                                                  # Time class (with hour, min, sec), the resulting range depends on
+                                                  # how precise the time is. Examples: If the time is 00:00:00, then
+                                                  # time range of that date. If minutes and second are 0 (but hour is not),
+                                                  # the time range is that hour. If second is 0, time range is that minute.
+                                                  # And so on.. See TIMES_TO_WHERE_ARGS for the complete rules.
+                                                  # You can always use .to_sql to see the effect.
+
+
+
+
+            # 🥗 get all DecisionReview piilogs that occurred in the past week
+
+            logs = Q.(1.week, :nod, :hlr)   # Note: this will go back 1.week from the current moment.
+                                            # You might want Q.(:nod, :hlr, 1.week.ago.to_date, nil)
+                                            # Use `nil` for open-ended time ranges
+
+
+
+
+            # 🍔 get the error counts for the last week of NOD errors
+
+            # print the error counts by category:
+
+              puts_by_error_message_category_counts wrap Q.(:nod, 7.days)
+
+              #  {"Gateway timeout"=>7,
+              #   "BackendServiceException [DR_422]"=>7,
+              #   "Outage..."=>6,
+              #   "BackendServiceException [DR_404]"=>2,
+              #   "BackendServiceException [unmapped_service_exception]"=>1}
+
+            # print the same counts, but first organize by where the exception occurred
+
+              puts_by_error_class_counts wrap Q.(:nod, 7.days)
+
+              #  {"V0::HigherLevelReviews::ContestableIssuesController#index exception Common::Exceptions::GatewayTimeout (HLR)"=>
+              #    {"Gateway timeout"=>7},
+              #   "V0::HigherLevelReviewsController#create exception DecisionReview::ServiceException (HLR)"=>
+              #    {"BackendServiceException [DR_422]"=>7},
+              #   "V0::HigherLevelReviews::ContestableIssuesController#index exception Breakers::OutageException (HLR)"=>
+              #    {"Outage..."=>4},
+              #   "V0::HigherLevelReviews::ContestableIssuesController#index exception DecisionReview::ServiceException (HLR)"=>
+              #    {"BackendServiceException [DR_404]"=>2,
+              #     "BackendServiceException [unmapped_service_exception]"=>1},
+              #   "V0::HigherLevelReviewsController#create exception Breakers::OutageException (HLR)"=>
+              #    {"Outage..."=>2}}
+
+            # now you can see that some of those 6 outages occurred at the ContestableIssuesController#index and
+            # some at HigherLevelReviewsController#create.
+
+            # Notice the 'wrap' method? 'wrap' adds helper methods to PersonalInformationLog objects. Type 'more' for details.
+
+
+
+
+            # 🍱 get the DecisionReview error counts for the past 15 days
+
+            15.downto(1).each do |d|
+              date = d.days.ago.to_date
+              count = Q.(:hlr, :nod, date).count
+              puts "%s  %s %3d" % [date.strftime('%A')[0..2], date, count]
+            end
+
+
+
+            (type 'more' for more info)
 
   RECIPES
 end
 
 def recipes
   puts recipes_string
+end
+
+def more_string
+  <<~MORE
+
+
+    # Q is an alias for the PersonalInformationLogQueryBuilder --a lambda that makes it easier to write
+    # queries for PersonalInformationLog.
+
+    logs = Q.(:hlr, 7.days)     # returns a PersonalInformationLog relation
+
+    logs = Q.(:hlr, updated_at: nil)     # kwargs work just as they do with where
+
+    logs = Q.(:hlr, :nod).where('updated_at > ?' Time.zone.yesterday)   # chainable. (that's HLR /or/ NOD PiiLogs)
+
+
+
+    # PersonalInformationLogs are hard to wrangle because so much data gets dumped into their 'data' attributes.
+
+    # In 'decision_review_repl.rb', there are a handful of PersonalInformationLog "wrapper classes" that add
+    # helper methods for "specific types" of PersonalInformationLogs (PiiLogs recorded in the controller as
+    # opposed to the service, for instance) (Note: I use PiiLog and PersonalInformationLog interchangeably
+    # although there /is/ a PiiLog class). You can use '.helper_methods' on a PiiLog or scroll through
+    # 'decision_review_repl.rb' to see what they offer, /BUT/, you do *not* need to explicitly call them. The
+    # wrap command will /wrap/ a PersonalInformationLog (or Logs) with the appropriate wrapper class. With `wrap`,
+    # you end up with an array of PersonalInformationLog /SimpleDelegator/ objects that are simply
+    # PersonalInformationLogs with added methods --added methods that are appropriate for each PiiLog. 
+    # For instance, a PiiLog logged in a controller will have user info helper methods (as the 'data' attribute
+    # has user info). If the PiiLog was recorded near the schema code in the Service class, the PiiLog will have
+    # schema helper methods that help you navigate the PiiLog's data.
+
+    logs = wrap Q.(:hlr)  # returns an array with helper methods added to each PersonalInformationLog
+                          # depending on its type (was it thrown in a controller, the service, etc.)
+
+
+    # There are a slew of methods that work with an array of wrapped PiiLogs.
+    # for instance:
+
+    hash = by_error_message_category(wrap(Q.call(:hlr, :yesterday))) # notice the "wrap" nestled in there
+
+    # this will return a hash that organizes the array of PiiLogs by their error message category
+    # if you pretty-print it, it will look something like:
+
+    p hash
+
+    # {"Outage..."=>
+    #   [#<PersonalInformationLog:0x0000000000000000
+    #     id: 0000000,
+    #     data:
+    #      {"user"=>
+    #        {"icn"=> ...
+    #         "ssn"=> ...
+    #      ...
+    #   [#<PersonalInformationLog:0x0000000000000000
+    #      ...
+    #  "Timeout"=> ...
+
+
+    # see "spec/rakelib/piilog_repl/piilog_helpers_spec.rb" for more examples of using
+    # the PersonalInformationLogQueryBuilder
+
+
+  MORE
+end
+
+def more
+  puts more_string
 end
 
 # Ruby pretty print that doesn't echo the value after pretty printing it
@@ -341,6 +369,9 @@ def wrap(value)
 end
 
 # the following methods take an array of wrapped-PersonalInformationLogs
+
+# takes in an array of wrapped-PiiLogs and returns a hash where they're
+# sorted by error_class and then by error message category
 def by_error_class(array)
   new_hash = Hash.new do |hash, key|
     hash[key] = Hash.new { |hash, key| hash[key] = [] }
@@ -353,6 +384,7 @@ def by_error_class(array)
   new_hash
 end
 
+# takes in an array of wrapped-PiiLogs and returns a hash where they're sorted by error message category
 def by_error_message_category(array)
   new_hash = Hash.new { |hash, key| hash[key] = [] }
 
