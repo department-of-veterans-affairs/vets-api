b17f503211f2ce58b7c421470d8edb11b27b0e52
Dan Hinze <dan.hinze@adhocteam.us>
2021-04-12 11:58:51 -0500

    Reinstate helper method

--- modules/claims_api/app/controllers/concerns/claims_api/json_format_validation.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/json_format_validation.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'common/exceptions/unprocessable_entity'
-
 module ClaimsApi
   module JsonFormatValidation
     extend ActiveSupport::Concern
@@ -9,8 +7,7 @@ module ClaimsApi
     included do
       def validate_json_format
         if request.body.class.name == 'Puma::NullIO' # testing string b/c NullIO class doesn't always exist
-          message = "The request body isn't a JSON object: #{response.body.inspect}"
-          raise Common::Exceptions::UnprocessableEntity.new(detail: message)
+          render_body_is_not_a_hash_error request.body
         end
 
         @json_body = JSON.parse request.body.string
@@ -19,6 +16,11 @@ module ClaimsApi
         render_body_is_not_a_hash_error @json_body
       rescue JSON::ParserError
         render_body_is_not_a_hash_error request.body.string
+      end
+
+      def render_body_is_not_a_hash_error(body)
+        message = "The request body isn't a JSON object: #{body.inspect}"
+        raise ::Common::Exceptions::UnprocessableEntity.new(detail: message)
       end
     end
   end
