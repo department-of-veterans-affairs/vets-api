71a12bffdd90ae246dc32f13556a612acbcdc7a9
Greg Bowman <gregory.bowman@libertyits.com>
2021-04-13 15:11:55 -0400

    refactored moving SQL to sql_support.rb

--- modules/vba_documents/app/workers/vba_documents/report_monthly_submissions.rb
+++ modules/vba_documents/app/workers/vba_documents/report_monthly_submissions.rb
@@ -2,165 +2,12 @@
 
 require 'sidekiq'
 require 'date'
-# require './modules/vba_documents/spec/support/vba_document_fixtures' #remove me
+require './modules/vba_documents/lib/vba_documents/sql_support'
 
 module VBADocuments
   class ReportMonthlySubmissions
     include Sidekiq::Worker
-    # include VBADocuments::Fixtures # remove me
-
-    MONTHLY_COUNT_SQL = "
-      select
-        to_char(created_at, 'YYYYMM') as yyyymm,
-        consumer_name,
-        sum(case when status = 'error' then 1 else 0 end) as errored,
-        sum(case when status = 'expired' then 1 else 0 end) as expired,
-        sum(case when status = 'pending' or status = 'uploaded' or
-            status = 'received' or status = 'processing' then 1 else 0 end) as processing,
-        sum(case when status = 'success' then 1 else 0 end) as success,
-        sum(case when status = 'vbms' then 1 else 0 end) as vbms,
-        count(*) as total
-      from vba_documents_upload_submissions a
-      where a.created_at >= $1 and a.created_at < $2
-      group by to_char(created_at, 'YYYYMM'), a.consumer_name
-      order by to_char(created_at, 'YYYYMM') asc, a.consumer_name asc
-    "
-
-    PROCESSING_SQL = "
-      select a.consumer_name, a.guid, a.status, a.created_at, a.updated_at
-      from vba_documents_upload_submissions a
-      where a.status in ('uploaded','received', 'processing','pending')
-      and   a.updated_at < $1
-      order by a.consumer_name asc
-    "
-
-    SUCCESS_SQL = "
-      select a.consumer_name, a.guid, a.status, a.created_at, a.updated_at
-      from vba_documents_upload_submissions a
-      where a.status = 'success'
-      and   a.uploaded_pdf is not null
-      and   a.updated_at < $1
-      order by a.consumer_name asc
-    "
-
-    MAX_AVG_SQL = "
-      select yyyy, mm,
-        NULLIF(sum(max_pages)::integer,0) as max_pages, NULLIF(sum(avg_pages)::integer,0) as avg_pages,
-        NULLIF(sum(max_size)::bigint,0) as max_size, NULLIF(sum(avg_size)::bigint,0) as avg_size
-      from (
-      ( select
-        date_part('year', a.created_at)::integer as yyyy,
-        date_part('month', a.created_at)::integer as mm,
-        max((uploaded_pdf->>'total_pages')::integer) as max_pages,
-        round(avg((uploaded_pdf->>'total_pages')::integer))::integer as avg_pages,
-        0::bigint as max_size,
-        0::bigint as avg_size
-      from vba_documents_upload_submissions a
-      where a.uploaded_pdf is not null
-      and   a.status != 'error'
-      and   a.created_at < $1
-      group by 1,2
-      order by 1 desc, 2 desc
-      limit 12
-      )
-    union
-      ( select
-        date_part('year', a.created_at)::integer as yyyy,
-        date_part('month', a.created_at)::integer as mm,
-        0::integer as max_pages,
-        0::integer as avg_pages,
-        max((metadata->'size')::bigint) as max_size_bytes,
-        round(avg((metadata->'size')::bigint))::bigint as avg_size_bytes
-      from vba_documents_upload_submissions a
-      where a.metadata->'size' is not null
-      and   a.status != 'error'
-      and   a.created_at < $1
-      group by 1,2
-      order by 1 desc, 2 desc
-      limit 12
-      )
-    ) as max_avg
-    group by 1,2
-    order by 1 desc, 2 desc
-    "
-
-    MODE_SQL = "
-    select yyyy, mm, NULLIF(sum(mode_pages)::integer,0) as mode_pages, NULLIF(sum(mode_size)::bigint,0) as mode_size
-    from (
-        ( select
-          date_part('year', a.created_at)::integer as yyyy,
-          date_part('month', a.created_at)::integer as mm,
-          mode() within group (order by (uploaded_pdf->>'total_pages')::integer) as mode_pages,
-          0::bigint as mode_size
-        from vba_documents_upload_submissions a
-        where a.uploaded_pdf is not null
-        and   a.status != 'error'
-        and   a.created_at < $1
-        group by 1,2
-        order by 1 desc, 2 desc
-        limit 12 )
-      union
-        ( select
-            date_part('year', a.created_at)::integer as yyyy,
-            date_part('month', a.created_at)::integer as mm,
-            0::integer as mode_pages,
-            mode() within group (order by (metadata->>'size')::bigint) as mode_size
-          from vba_documents_upload_submissions a
-          where a.metadata is not null
-          and   a.status != 'error'
-          and   a.created_at < $1
-          group by 1,2
-          order by 1 desc, 2 desc
-          limit 12
-        )
-    ) as mode_results
-    group by 1,2
-    order by 1 desc, 2 desc
-    "
-
-    MEDIAN_SQL = "
-      select NULLIF(sum(median_pages),0) as median_pages, NULLIF(sum(median_size),0) as median_size
-      from (
-        (select (uploaded_pdf->>'total_pages')::integer as median_pages, 0::bigint as median_size
-        from vba_documents_upload_submissions a
-        where a.uploaded_pdf is not null
-        and   a.status != 'error'
-        and   to_char(a.created_at,'yyyymm') = $1
-        order by (uploaded_pdf->>'total_pages')::bigint
-        offset (select count(*) from vba_documents_upload_submissions
-        where uploaded_pdf is not null
-        and   status != 'error'
-        and   to_char(created_at,'yyyymm') = $1)/2
-        limit 1)
-        UNION
-        (select 0::integer as median_pages, (metadata->>'size')::bigint as median_size
-        from vba_documents_upload_submissions a
-        where a.metadata is not null
-        and   a.status != 'error'
-        and   to_char(a.created_at,'yyyymm') = $1
-        order by (metadata->>'size')::bigint
-        offset (select count(*) from vba_documents_upload_submissions
-        where metadata->>'size' is not null
-        and   status != 'error'
-        and   to_char(created_at,'yyyymm') = $1)/2
-        limit 1)
-      ) as median_results
-    "
-
-    AVG_TIME_TO_VBMS_SQL = "
-      select
-      date_part('year', a.created_at)::integer as yyyy,
-      date_part('month', a.created_at)::integer as mm,
-      count(*) as count,
-    	avg(date_part('epoch', a.updated_at)::bigint -
-        date_part('epoch', a.created_at)::bigint)::integer as avg_time_secs
-      from vba_documents_upload_submissions a
-      where a.status = 'vbms'
-      and   a.created_at < $1
-      group by 1,2
-      order by 1 desc, 2 desc
-      limit 12
-    "
+    extend VBADocuments::SQLSupport
 
     def perform
       if Settings.vba_documents.monthly_report
@@ -168,21 +15,13 @@ module VBADocuments
         last_month_start = (Date.current - 1.month).beginning_of_month
         last_month_end = Date.current.beginning_of_month
 
-        # leave for testing locally with fixtures
-        # @monthly_counts = get_fixture_yml('monthly_report/monthly_counts.yml')
-        # still_processing = get_fixture_yml('monthly_report/still_processing.yml')
-        # still_success = get_fixture_yml('monthly_report/still_success.yml')
-        # @avg_processing_time = get_fixture_yml('monthly_report/avg_processing_time.yml')
-        # @monthly_mode = get_fixture_yml('monthly_report/mode.yml') # was monthly_mode_pages with median added
-        # @monthly_max_avg = get_fixture_yml('monthly_report/max_avg.yml') #was monthly_max_avg_pages.yml
-
         # execute SQL for monthly counts
-        @monthly_counts = run_sql(MONTHLY_COUNT_SQL, last_month_start, last_month_end)
-        still_processing = run_sql(PROCESSING_SQL, last_month_start)
-        still_success = run_sql(SUCCESS_SQL, last_month_start)
-        @avg_processing_time = run_sql(AVG_TIME_TO_VBMS_SQL, last_month_end)
-        @monthly_max_avg = run_sql(MAX_AVG_SQL, last_month_end)
-        @monthly_mode = run_sql(MODE_SQL, last_month_end)
+        @monthly_counts = run_sql(SQLSupport::MONTHLY_COUNT_SQL, last_month_start, last_month_end)
+        still_processing = run_sql(SQLSupport::PROCESSING_SQL, last_month_start)
+        still_success = run_sql(SQLSupport::SUCCESS_SQL, last_month_start)
+        @avg_processing_time = run_sql(SQLSupport::AVG_TIME_TO_VBMS_SQL, last_month_end)
+        @monthly_max_avg = run_sql(SQLSupport::MAX_AVG_SQL, last_month_end)
+        @monthly_mode = run_sql(SQLSupport::MODE_SQL, last_month_end)
         get_median_results
         add_max_avg_mb
         final_monthly_results = join_monthly_results
@@ -225,7 +64,6 @@ module VBADocuments
         base_row.merge!(@monthly_mode[idx]) if @monthly_mode[idx]
         ret << base_row
       end
-      # File.open("final_monthly_results.yml", "w") { |file| file.write(ret.to_yaml) } #todo delete me
       ret
     end
 
@@ -247,7 +85,7 @@ module VBADocuments
       @monthly_mode.each do |row|
         zero_pad = ('0' + row['mm'].to_s)[-2..2]
         yyyymm = "#{row['yyyy']}#{zero_pad}"
-        median = run_sql(MEDIAN_SQL, yyyymm)
+        median = run_sql(SQLSupport::MEDIAN_SQL, yyyymm)
         row['median_pages'] = median.first['median_pages']
         row['mode_size'] = bytes_to_megabytes(row['mode_size'])
         row['median_size'] = bytes_to_megabytes(median.first['median_size'])
--- modules/vba_documents/spec/jobs/report_monthly_submissions_spec.rb
+++ modules/vba_documents/spec/jobs/report_monthly_submissions_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 require 'date'
 require './modules/vba_documents/spec/support/vba_document_fixtures'
+require './modules/vba_documents/lib/vba_documents/sql_support'
 
 RSpec.describe VBADocuments::ReportMonthlySubmissions, type: :job do
   include VBADocuments::Fixtures
@@ -12,37 +13,30 @@ RSpec.describe VBADocuments::ReportMonthlySubmissions, type: :job do
   after { Timecop.return }
 
   describe '#perform' do
+    include VBADocuments::SQLSupport
     monthly_counts = 'monthly_report/monthly_counts.yml'
     summary = 'monthly_report/monthly_summary.yml'
     still_processing = 'monthly_report/still_processing.yml'
     still_success = 'monthly_report/still_success.yml'
     avg_times = 'monthly_report/avg_processing_time.yml'
     final_monthly_results = 'monthly_report/final_monthly_results.yml'
-    mode_pages = 'monthly_report/mode.yml' # was monthly_mode_pages with median added
-    max_avg_pages = 'monthly_report/max_avg.yml' # was monthly_max_avg_pages.yml
+    mode = 'monthly_report/mode.yml'
+    max_avg = 'monthly_report/max_avg.yml'
 
     it 'sends mail' do
       with_settings(Settings.vba_documents, monthly_report: true) do
         last_month_start = Date.parse('01-03-2021')
         last_month_end = Date.parse('01-04-2021')
 
-        monthly_sql = VBADocuments::ReportMonthlySubmissions::MONTHLY_COUNT_SQL
-        proc_sql = VBADocuments::ReportMonthlySubmissions::PROCESSING_SQL
-        success_sql = VBADocuments::ReportMonthlySubmissions::SUCCESS_SQL
-        avg_sql = VBADocuments::ReportMonthlySubmissions::AVG_TIME_TO_VBMS_SQL
-        mode_sql = VBADocuments::ReportMonthlySubmissions::MODE_SQL
-        median_sql = VBADocuments::ReportMonthlySubmissions::MEDIAN_SQL
-        max_avg_pages_sql = VBADocuments::ReportMonthlySubmissions::MAX_AVG_SQL
-
         job = described_class.new
         allow(job).to receive(:run_sql) do |sql, _args|
-          rval = get_fixture_yml(monthly_counts) if sql.eql? monthly_sql
-          rval = get_fixture_yml(still_processing) if sql.eql? proc_sql
-          rval = get_fixture_yml(still_success) if sql.eql? success_sql
-          rval = get_fixture_yml(avg_times) if sql.eql? avg_sql
-          rval = get_fixture_yml(mode_pages) if sql.eql? mode_sql
-          rval = [{ 'median_pages' => nil, 'median_size' => nil }] if sql.eql? median_sql
-          rval = get_fixture_yml(max_avg_pages) if sql.eql? max_avg_pages_sql
+          rval = get_fixture_yml(monthly_counts) if sql.eql? VBADocuments::SQLSupport::MONTHLY_COUNT_SQL
+          rval = get_fixture_yml(still_processing) if sql.eql? VBADocuments::SQLSupport::PROCESSING_SQL
+          rval = get_fixture_yml(still_success) if sql.eql? VBADocuments::SQLSupport::SUCCESS_SQL
+          rval = get_fixture_yml(avg_times) if sql.eql? VBADocuments::SQLSupport::AVG_TIME_TO_VBMS_SQL
+          rval = get_fixture_yml(mode) if sql.eql? VBADocuments::SQLSupport::MODE_SQL
+          rval = [{ 'median_pages' => nil, 'median_size' => nil }] if sql.eql? VBADocuments::SQLSupport::MEDIAN_SQL
+          rval = get_fixture_yml(max_avg) if sql.eql? VBADocuments::SQLSupport::MAX_AVG_SQL
           rval
         end
 
