9e53f6763b20867d9287c7d74b7674a26ae3ccdd
Stephen Barrs <stephen.barrs@va.gov>
2021-04-13 13:51:14 -0400

    disable retries and do not raise exception out of job

--- modules/covid_vaccine/app/jobs/covid_vaccine/expanded_submission_job.rb
+++ modules/covid_vaccine/app/jobs/covid_vaccine/expanded_submission_job.rb
@@ -6,17 +6,17 @@ module CovidVaccine
   class ExpandedSubmissionJob
     include Sidekiq::Worker
     include SentryLogging
-
+    sidekiq_options retry: false
+    
     def perform(record_id)
-      partial = CovidVaccine::V0::ExpandedRegistrationSubmission.find(record_id)
-      CovidVaccine::V0::ExpandedRegistrationService.new.register(partial)
+      submission = CovidVaccine::V0::ExpandedRegistrationSubmission.find(record_id)
+      CovidVaccine::V0::ExpandedRegistrationService.new.register(submission)
     rescue => e
       handle_errors(e)
     end
 
     def handle_errors(ex)
       log_exception_to_sentry(ex)
-      raise ex
     end
   end
 end
