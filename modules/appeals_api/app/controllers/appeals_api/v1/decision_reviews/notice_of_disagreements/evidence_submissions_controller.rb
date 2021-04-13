16dac7a5e68ea288638520e58451b9bc2b047fb3
Laura Trager <laura.trager@adhocteam.us>
2021-04-13 08:41:46 -0400

    Evidence submission request validation

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/notice_of_disagreements/evidence_submissions_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/notice_of_disagreements/evidence_submissions_controller.rb
@@ -5,16 +5,24 @@ module AppealsApi::V1
     module NoticeOfDisagreements
       class EvidenceSubmissionsController < AppealsApi::ApplicationController
         skip_before_action :authenticate
+        before_action :set_notice_of_disagreement, only: :create
+        before_action :set_veteran_identifier, only: :create
         before_action :set_submission_attributes, only: :create
 
         def create
-          upload = VBADocuments::UploadSubmission.create! consumer_name: 'appeals_api_nod_evidence_submission'
-          submission = AppealsApi::EvidenceSubmission.create! @submission_attributes.merge(upload_submission: upload)
+          status, error = AppealsApi::RequestValidator.new(@notice_of_disagreement, @veteran_id).call
 
-          render json: submission,
-                 serializer: AppealsApi::EvidenceSubmissionSerializer,
-                 key_transform: :camel_lower,
-                 render_location: true
+          if status == :ok
+            upload = VBADocuments::UploadSubmission.create! consumer_name: 'appeals_api_nod_evidence_submission'
+            submission = AppealsApi::EvidenceSubmission.create! @submission_attributes.merge(upload_submission: upload)
+
+            render json: submission,
+                   serializer: AppealsApi::EvidenceSubmissionSerializer,
+                   key_transform: :camel_lower,
+                   render_location: true
+          else
+            render json: { errors: [error] }, status: :unprocessable_entity
+          end
         end
 
         def show
@@ -29,10 +37,19 @@ module AppealsApi::V1
 
         private
 
-        def set_submission_attributes
-          @appeal ||= AppealsApi::NoticeOfDisagreement.find_by(id: params[:nod_id])
-          raise Common::Exceptions::RecordNotFound, params[:nod_id] unless @appeal
+        def set_notice_of_disagreement
+          @nod_id ||= params[:nod_id]
+          raise Common::Exceptions::ParameterMissing, 'nod_id' unless @nod_id
 
+          @notice_of_disagreement ||= AppealsApi::NoticeOfDisagreement.find_by(id: @nod_id)
+          raise Common::Exceptions::RecordNotFound, params[:nod_id] unless @notice_of_disagreement
+        end
+
+        def set_veteran_identifier
+          @veteran_id ||= params[:headers]['X-VA-Veteran-SSN']
+        end
+
+        def set_submission_attributes
           @submission_attributes ||= {
             source: params['headers']['X-Consumer-Username'],
             supportable_id: params[:nod_id],
