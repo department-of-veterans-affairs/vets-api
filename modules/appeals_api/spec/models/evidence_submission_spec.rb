d12436950126a5cf36486dfd8f83ff38273c192c
Drew Fisher <drew.fisher@adhocteam.us>
2021-04-13 17:05:13 -0700

    Rubocop

--- modules/appeals_api/spec/models/evidence_submission_spec.rb
+++ modules/appeals_api/spec/models/evidence_submission_spec.rb
@@ -6,7 +6,9 @@ require AppealsApi::Engine.root.join('spec', 'spec_helper.rb')
 describe AppealsApi::EvidenceSubmission, type: :model do
   let(:notice_of_disagreement) { create(:notice_of_disagreement) }
   let(:upload_submission) { create(:upload_submission) }
-  let(:evidence_submission) { create(:evidence_submission, supportable: notice_of_disagreement, upload_submission: upload_submission) }
+  let(:evidence_submission) do
+    create :evidence_submission, supportable: notice_of_disagreement, upload_submission: upload_submission
+  end
 
   it 'responds to supportable' do
     expect(evidence_submission.respond_to?(:supportable)).to eq(true)
