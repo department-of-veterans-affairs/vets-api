0fb2d63a8a4b51ee90d8437baa250120b8d2fad2
Cris Shupp <cristopher.shupp@libertyits.com>
2021-04-14 10:05:22 -0400

    test fixes

--- modules/vba_documents/spec/models/git_items_spec.rb
+++ modules/vba_documents/spec/models/git_items_spec.rb
@@ -13,7 +13,7 @@ describe VBADocuments::GitItems, type: :model do
 
   before do
     Settings.vba_documents.slack = Config::Options.new
-    Settings.vba_documents.slack.deployment_notification_url = nil #url post mocked out
+    Settings.vba_documents.slack.deployment_notification_url = nil # url post mocked out
     allow(faraday_response).to receive(:success?).and_return(true)
     allow(VBADocuments::GitItems).to receive(:query_git) {
       faraday_response
