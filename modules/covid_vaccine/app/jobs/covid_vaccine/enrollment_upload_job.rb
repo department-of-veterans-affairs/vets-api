0aff5bfa2a4452aeeebe1f233c21dbdfff9da1c9
Alastair Dawson <alastair.j.dawson@gmail.com>
2021-04-14 15:17:16 -0700

    add facility resolution to covid vaccine enrollment upload job

--- modules/covid_vaccine/app/jobs/covid_vaccine/enrollment_upload_job.rb
+++ modules/covid_vaccine/app/jobs/covid_vaccine/enrollment_upload_job.rb
@@ -12,14 +12,34 @@ module CovidVaccine
 
     def perform(batch_id)
       Rails.logger.info('Covid_Vaccine Enrollment_Upload: Start', batch_id: batch_id)
+      resolve_facilities(batch_id)
+      upload(batch_id)
+      StatsD.increment(STATSD_SUCCESS_NAME)
+    rescue => e
+      handle_errors(e, batch_id)
+    end
 
+    private
+
+    def resolve_facilities(batch_id)
+      update_count = 0
+      CovidVaccine::V0::ExpandedRegistrationSubmission.where(batch_id: batch_id).find_each do |submission|
+        resolver = CovidVaccine::V0::FacilityResolver.new
+        mapped_facility = resolver.resolve(submission)
+        submission.eligibility_info = { preferred_facility: mapped_facility }
+        submission.save!
+        update_count += 1
+      end
+
+      Rails.logger.info('Covid_Vaccine Enrollment_Upload: Updated mapped facility info', batch_id: batch_id,
+                                                                                         record_count: update_count)
+    end
+
+    def upload(batch_id)
       processor = CovidVaccine::V0::EnrollmentProcessor.new(batch_id)
       record_count = processor.process_and_upload!
 
       Rails.logger.info('Covid_Vaccine Enrollment_Upload: Success', batch_id: batch_id, record_count: record_count)
-      StatsD.increment(STATSD_SUCCESS_NAME)
-    rescue => e
-      handle_errors(e, batch_id)
     end
 
     def handle_errors(ex, batch_id)
--- modules/covid_vaccine/spec/jobs/covid_vaccine/enrollment_upload_job_spec.rb
+++ modules/covid_vaccine/spec/jobs/covid_vaccine/enrollment_upload_job_spec.rb
@@ -4,30 +4,65 @@ require 'rails_helper'
 
 RSpec.describe CovidVaccine::EnrollmentUploadJob, type: :worker do
   describe '#perform' do
-    let(:batch_id) { '20210101123456' }
     let(:record_count) { 42 }
 
-    context 'when batch creation succeeds' do
+    let(:batch_id) { '20210101123456' }
+    let(:batched_records) do
+      subs = YAML.load_file('modules/covid_vaccine/spec/fixtures/expanded_registration_submissions_upload.yml')
+      subs.values.map do |s|
+        FactoryBot.create(:covid_vax_expanded_registration, state: 'received', raw_form_data: s['raw_form_data'],
+                                                            batch_id: batch_id)
+      end
+    end
+
+    around do |example|
+      Timecop.freeze(Time.zone.parse('2021-04-02T00:00:00Z'))
+      example.run
+      Timecop.return
+    end
+
+    context 'when batch upload succeeds' do
       before do
-        allow_any_instance_of(CovidVaccine::V0::EnrollmentProcessor).to receive(:process_and_upload!).and_return(42)
+        allow_any_instance_of(CovidVaccine::V0::EnrollmentProcessor)
+          .to receive(:process_and_upload!).and_return(batched_records.size)
+      end
+
+      it 'resolves facilities to the expected values' do
+        subject.perform(batch_id)
+        updated_records = CovidVaccine::V0::ExpandedRegistrationSubmission.where(batch_id: batch_id)
+        expect(updated_records.map(&:eligibility_info)).to eq(
+          [
+            { 'preferred_facility' => nil },
+            { 'preferred_facility' => '442' },
+            { 'preferred_facility' => '648' }
+          ]
+        )
       end
 
       it 'logs its progress' do
         expect(Rails.logger).to receive(:info).with('Covid_Vaccine Enrollment_Upload: Start', batch_id: batch_id)
         expect(Rails.logger).to receive(:info).with(
-          'Covid_Vaccine Enrollment_Upload: Success', batch_id: batch_id, record_count: record_count
+          'Covid_Vaccine Enrollment_Upload: Updated mapped facility info', batch_id: batch_id,
+                                                                           record_count: batched_records.size
+        )
+        expect(Rails.logger).to receive(:info).with(
+          'Covid_Vaccine Enrollment_Upload: Success', batch_id: batch_id, record_count: batched_records.size
         )
 
         expect(StatsD).to receive(:increment).once.with('worker.covid_vaccine_enrollment_upload.success')
 
-        subject.perform(batch_id)
+        VCR.use_cassette('covid_vaccine/facilities/query_46953', match_requests_on: %i[path query]) do
+          VCR.use_cassette('covid_vaccine/facilities/query_27330', match_requests_on: %i[path query]) do
+            subject.perform(batch_id)
+          end
+        end
       end
     end
 
     context 'when processing fails' do
       before do
-        allow_any_instance_of(CovidVaccine::V0::EnrollmentProcessor).to receive(:process_and_upload!).and_raise(
-          StandardError
+        allow(CovidVaccine::V0::ExpandedRegistrationSubmission).to receive(:where).and_raise(
+          ActiveRecord::ActiveRecordError
         )
       end
 
