debabc0c6aa5e6c302a483f86b5b1acd13ed1d09
Drew Fisher <142862+cilestin@users.noreply.github.com>
2021-04-14 10:13:29 -0700

    Api 6346 Update Evidence submission create & show (#6566)
    * Update EvidenceSubmissionController #create & #show

--- modules/appeals_api/app/models/appeals_api/evidence_submission.rb
+++ modules/appeals_api/app/models/appeals_api/evidence_submission.rb
@@ -4,30 +4,13 @@ require 'json_marshal/marshaller'
 
 module AppealsApi
   class EvidenceSubmission < ApplicationRecord
-    belongs_to :supportable, polymorphic: true, optional: true
+    include SetGuid
+    self.ignored_columns = ['status'] # Temporary until migrations have run
+    belongs_to :supportable, polymorphic: true
+    belongs_to :upload_submission,
+               class_name: 'VBADocuments::UploadSubmission',
+               dependent: :destroy
 
-    attr_encrypted(:file_data, key: Settings.db_encryption_key, marshal: true, marshaler: JsonMarshal::Marshaller)
-
-    def get_location
-      rewrite_url(signed_url(id))
-    end
-
-    private
-
-    def rewrite_url(url)
-      rewritten = url.sub!(Settings.modules_appeals_api.evidence_submissions.location.prefix,
-                           Settings.modules_appeals_api.evidence_submissions.location.replacement)
-      raise 'Unable to provide document upload location' unless rewritten
-
-      rewritten
-    end
-
-    def signed_url(id)
-      s3 = Aws::S3::Resource.new(region: Settings.modules_appeals_api.s3.region,
-                                 access_key_id: Settings.modules_appeals_api.s3.aws_access_key_id,
-                                 secret_access_key: Settings.modules_appeals_api.s3.aws_secret_access_key)
-      obj = s3.bucket(Settings.modules_appeals_api.s3.bucket).object(id)
-      obj.presigned_url(:put, {})
-    end
+    delegate :status, to: :upload_submission
   end
 end
