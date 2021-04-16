44d420f4b98e57185bbb85bc48bfbee3a28be19b
Anna Carey <anna@adhocteam.us>
2021-04-16 15:30:07 -0400

    remove unused

--- app/swagger/swagger/schemas/appeals/notice_of_disagreement.rb
+++ app/swagger/swagger/schemas/appeals/notice_of_disagreement.rb
@@ -24,38 +24,9 @@ module Swagger
           example JSON.parse(File.read('spec/fixtures/notice_of_disagreements/NOD_show_response_200.json'))
         end
 
-        # recursive
-        def self.remove_null_from_type_array(value)
-          case value
-          when Hash
-            value.reduce({}) do |new_hash, (k, v)|
-              next new_hash.merge(k => x_from_nullable_x_type(v)) if k == 'type' && type_is_nullable?(v)
-
-              new_hash.merge(k => remove_null_from_type_array(v))
-            end
-          when Array
-            value.map { |v| remove_null_from_type_array(v) }
-          else
-            value
-          end
-        end
-
-        def self.x_from_nullable_x_type(type_array)
-          nulls_index = type_array.index('null')
-          types_index = nulls_index.zero? ? 1 : 0
-          type_array[types_index]
-        end
-
-        def self.type_is_nullable?(type)
-          type.is_a?(Array) && type.length == 2 && type.include?('null')
-        end
-
         swagger_schema(
           'nodContestableIssues',
-          remove_null_from_type_array(
-            DecisionReview::Schemas::NOD_CONTESTABLE_ISSUES_RESPONSE_200
-          ).merge(
-            description: 'Fields may either be null or the type specified',
+          DecisionReview::Schemas::NOD_CONTESTABLE_ISSUES_RESPONSE_200.merge(
             example: JSON.parse(
               File.read('spec/fixtures/notice_of_disagreements/NOD_contestable_issues_response_200.json')
             )
