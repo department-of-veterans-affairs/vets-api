432faf6ee54417ac1192ea4e16773dea9eec9a5c
vets-api <mark.viterna@va.gov>
2021-04-19 12:10:50 -0400

    Using pg_search gem in controller

--- modules/va_forms/app/controllers/va_forms/v0/forms_controller.rb
+++ modules/va_forms/app/controllers/va_forms/v0/forms_controller.rb
@@ -6,7 +6,7 @@ module VAForms
       skip_before_action(:authenticate)
 
       def index
-        render json: Form.search(search_term: params[:query]),
+        render json: Form.search(params[:query]),
                serializer: ActiveModel::Serializer::CollectionSerializer,
                each_serializer: VAForms::FormListSerializer
       end
--- modules/va_forms/app/models/va_forms/form.rb
+++ modules/va_forms/app/models/va_forms/form.rb
@@ -2,6 +2,12 @@
 
 module VAForms
   class Form < ApplicationRecord
+    include PgSearch::Model
+    pg_search_scope :search,
+                    against: %i[title form_name],
+                    using: { tsearch: { prefix: true, dictionary: 'english' }, trigram:  {
+                      word_similarity: true
+                    } }
     has_paper_trail only: ['sha256']
 
     validates :title, presence: true
@@ -12,16 +18,6 @@ module VAForms
     validates :valid_pdf, inclusion: { in: [true, false] }
 
     before_save :set_revision
-
-    def self.search(search_term: nil)
-      query = Form.all
-      if search_term.present?
-        search_term.strip!
-        terms = search_term.split.map { |term| "%#{term}%" }
-        query = query.where('form_name ilike ANY ( array[?] ) OR title ilike ANY ( array[?] )', terms, terms)
-      end
-      query
-    end
 
     private
 
