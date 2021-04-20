8c2772273d20b6c1ceddff2005c81a1c8b98f194
Stephen Barrs <stephen.barrs@va.gov>
2021-04-20 13:07:20 -0400

    schedule delay to ensure job works as planned in prod

--- modules/covid_vaccine/spec/jobs/covid_vaccine/expanded_scheduled_submission_job_spec.rb
+++ modules/covid_vaccine/spec/jobs/covid_vaccine/expanded_scheduled_submission_job_spec.rb
@@ -7,29 +7,17 @@ RSpec.describe CovidVaccine::ExpandedScheduledSubmissionJob, type: :worker do
   describe 'schedule' do
     sidekiq_file = Rails.root.join('config', 'sidekiq_scheduler.yml')
     schedule = YAML.load_file(sidekiq_file)['CovidVaccine::ExpandedScheduledSubmissionJob']
-    let(:parsed_schedule) { Fugit.do_parse(schedule['cron']) }
+    let(:parsed_schedule) { Fugit.do_parse(schedule['every']) }
 
     let(:submission) { create(:covid_vax_expanded_registration, :unsubmitted) }
 
     it 'has a job class' do
       expect { schedule['class'].constantize }.not_to raise_error
     end
-
-    it 'is scheduled in the eastern time zone' do
-      expect(parsed_schedule.zone).to eq('America/New_York')
-    end
-
-    it 'is scheduled to run every 15 min' do
-      expect(parsed_schedule.minutes).to eq([0, 15, 30, 45])
-    end
   end
 
   describe '#perform' do
     context 'when records exist with state=enrollment_pending' do
-      # before do
-      #   create(:covid_vax_expanded_registration).raw_form_data.symbolize_keys
-      # end
-
       it 'enqueues a CovidVaccine::ExpandedSubmissionJob job' do
         create(:covid_vax_expanded_registration).raw_form_data.symbolize_keys
         expect { subject.perform }.to change(CovidVaccine::ExpandedSubmissionJob.jobs, :size).by(1)
