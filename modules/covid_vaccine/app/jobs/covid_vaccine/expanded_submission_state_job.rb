72427ec3212f8e25686f3a1b643e371f8076db74
Stephen Barrs <stephen.barrs@va.gov>
2021-04-20 17:12:32 -0400

    service, scheduler and test for hourly job to log state of states for KMI expanded data

--- modules/covid_vaccine/app/jobs/covid_vaccine/expanded_submission_state_job.rb
+++ modules/covid_vaccine/app/jobs/covid_vaccine/expanded_submission_state_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CovidVaccine
   class ExpandedSubmissionStateJob
     include Sidekiq::Worker
@@ -5,21 +7,18 @@ module CovidVaccine
     sidekiq_options retry: false
 
     def perform
-      puts 'Starting state data job'
+      Rails.logger.info('Covid_Vaccine Expanded_Submission_State_Job Start')
       states = CovidVaccine::V0::ExpandedRegistrationSubmission.group('state').count
-      state_data = ''
+      state_data = {}
       states.each do |k, v|
-        puts "#{k || 'nil'}: #{v}"
-        state_data.merge("#{k || 'nil'}: #{v}")
+        state_data.merge!({ "#{k}": v }) unless k.nil?
       end
-      puts state_data
-      binding.pry
-      #   rescue => e
-      #     handle_errors(e)
+      Rails.logger.info("#{self.class.name}: Count of states", state_data)
+    rescue => e
+      handle_errors(e)
     end
 
-    def handle_errors(ex)
-      binding.pry
+    def handle_errors(_ex)
       Rails.logger.error('Covid_Vaccine Expanded_Submission_State_Job Failed')
     end
   end
