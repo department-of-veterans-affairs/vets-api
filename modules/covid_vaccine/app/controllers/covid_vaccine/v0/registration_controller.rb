918809c5f699c081cc71c221fdf281471de8ef13
Patrick Vinograd <patrick@adhocteam.us>
2021-04-20 17:24:41 -0700

    Refines skipping

--- modules/covid_vaccine/app/controllers/covid_vaccine/v0/registration_controller.rb
+++ modules/covid_vaccine/app/controllers/covid_vaccine/v0/registration_controller.rb
@@ -9,7 +9,7 @@ module CovidVaccine
       include IgnoreNotFound
 
       before_action :validate_raw_form_data, only: :create
-      skip_before_action :verify_authenticity_token, only: :opt_in, :opt_out, :create 
+      skip_before_action :verify_authenticity_token, only: :opt_out
 
       def create
         raw_form_data = params[:registration].merge(attributes_from_user)
