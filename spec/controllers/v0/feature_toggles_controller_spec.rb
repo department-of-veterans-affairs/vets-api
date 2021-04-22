b183baca05f2e124838849945df141def3b3c261
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-04-22 10:56:52 -0400

    remove dup test

--- spec/controllers/v0/feature_toggles_controller_spec.rb
+++ spec/controllers/v0/feature_toggles_controller_spec.rb
@@ -23,16 +23,6 @@ RSpec.describe V0::FeatureTogglesController, type: :controller do
       expect(json_data['data']['features'].first['value']).not_to be_nil
     end
 
-    it 'returns true for enabled flag' do
-      get :index
-      expect(response).to have_http_status(:ok)
-      json_data = JSON.parse(response.body)
-
-      expect(json_data['data']['features'].first['value']).not_to be_nil
-      expect(json_data['data']['features']).to include({ 'name' => @feature_name_camel, 'value' => true })
-      expect(json_data['data']['features']).to include({ 'name' => @feature_name, 'value' => true })
-    end
-
     it 'allows strings as actors' do
       @feature_name =  'ssoe'
       @feature_name_camel = @feature_name.camelize(:lower)
@@ -51,18 +41,12 @@ RSpec.describe V0::FeatureTogglesController, type: :controller do
     end
 
     it 'tests that both casing forms (snake and camel) are returned properly' do
-      @feature_name =  'ssoe'
-      @feature_name_camel = @feature_name.camelize(:lower)
-      @cookie_id = 'abc_123'
-      actor = Flipper::Actor.new(@cookie_id)
-      Flipper.disable(@feature_name)
-      Flipper.enable_actor(@feature_name, actor)
-
       get :index, params: { cookie_id: @cookie_id }
 
       expect(response).to have_http_status(:ok)
       json_data = JSON.parse(response.body)
 
+      expect(json_data['data']['features'].first['value']).not_to be_nil
       expect(json_data['data']['features']).to include({ 'name' => @feature_name_camel, 'value' => true })
       expect(json_data['data']['features']).to include({ 'name' => @feature_name, 'value' => true })
     end
