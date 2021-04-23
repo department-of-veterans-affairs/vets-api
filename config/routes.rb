7cad37750f781e3cec4692d3736bb8da24d04c03
Kevin Mircovich <7391284+kevinmirc@users.noreply.github.com>
2021-04-23 11:37:15 -0400

    prevent file upload endpoint on production deployment env

--- config/routes.rb
+++ config/routes.rb
@@ -98,7 +98,7 @@ Rails.application.routes.draw do
     post 'caregivers_assistance_claims/download_pdf', to: 'caregivers_assistance_claims#download_pdf'
 
     namespace :form1010cg do
-      resources :attachments, only: :create
+      resources :attachments, only: :create unless Settings.vsp_environment == 'production'
     end
 
     resources :dependents_applications, only: %i[create show] do
