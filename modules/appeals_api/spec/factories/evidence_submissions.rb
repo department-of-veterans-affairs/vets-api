187ffdf946e695c5125f093d8e831f0e31cacd9a
Laura <30485143+ElleeB@users.noreply.github.com>
2021-04-26 12:51:53 -0400

    Evidence submission request validation (#6591)
    * Evidence submission request validation
    
    * Rebase

--- modules/appeals_api/spec/factories/evidence_submissions.rb
+++ modules/appeals_api/spec/factories/evidence_submissions.rb
@@ -4,15 +4,18 @@ FactoryBot.define do
   factory :evidence_submission, class: 'AppealsApi::EvidenceSubmission' do
     sequence(:id) { |n| n }
     guid { SecureRandom.uuid }
-    association :supportable, factory: :notice_of_disagreement
     upload_submission { create(:upload_submission, guid: SecureRandom.uuid) } # set the guid to pass uniqueness check
 
     trait :with_detail do
       detail { SecureRandom.alphanumeric(150) }
     end
 
-    trait :with_nod do
+    trait :for_nod do
       supportable { create(:notice_of_disagreement) }
+    end
+
+    trait :set_supportable_type do
+      supportable_type { 'AppealsApi::NoticeOfDisagreement' }
     end
   end
 end
