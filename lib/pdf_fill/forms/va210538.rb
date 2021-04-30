5c1aec21aca54563692f768afdfdfb91b8408f3a
Kathleen Crawford <kcrawford@governmentcio.com>
2021-04-30 11:06:58 -0700

    adding 0538 pdf

---
+++ lib/pdf_fill/forms/va210538.rb
@@ -0,0 +1,167 @@
+# frozen_string_literal: true
+
+module PdfFill
+  module Forms
+    class Va210538 < FormBase
+      include FormHelper
+
+      ITERATOR = PdfFill::HashConverter::ITERATOR
+
+      KEY = {
+        'veteranInformation' => {
+          'fullName' => {
+            'first' => {
+              key: 'Veterans_First_Name[0]',
+              limit: 12,
+              question_num: 1,
+              question_suffix: 'A',
+              question_text: 'VETERAN\'S NAME'
+            },
+            'middleInitial' => {
+              key: 'form1[0].#subform[0].Middle_Initial1[0]',
+              limit: 1,
+              question_num: 1,
+              question_suffix: 'B',
+              question_text: 'VETERAN\'S NAME'
+            },
+            'last' => {
+              key: 'form1[0].#subform[0].Last_Name[0]',
+              limit: 18,
+              question_num: 1,
+              question_suffix: 'C',
+              question_text: 'VETERAN\'S NAME'
+            }
+          }, # end fullName
+          'ssn' => {
+            'first' => {
+              key: 'form1[0].#subform[0].Social_Security_Number_FirstThreeNumbers[0]',
+              limit: 3,
+              question_num: 2,
+              question_suffix: 'A',
+              question_text: 'SOCIAL SECURITY NUMBER'
+            },
+            'second' => {
+              key: 'form1[0].#subform[0].Social_Security_Number_SecondTwoNumbers[0]',
+              limit: 2,
+              question_num: 2,
+              question_suffix: 'B',
+              question_text: 'SOCIAL SECURITY NUMBER'
+            },
+            'third' => {
+              key: 'form1[0].#subform[0].Social_Security_Number_LastFourNumbers[0]',
+              limit: 4,
+              question_num: 2,
+              question_suffix: 'C',
+              question_text: 'SOCIAL SECURITY NUMBER'
+            }
+          },
+          'VAFileNumber' => {
+            key: 'form1[0].#subform[0].VA_File_Number[0]',
+            limit: 9,
+            question_num: 3,
+            question_suffix: 'A',
+            question_text: 'VA FILE NUMBER'
+          },
+          'dateOfBirth' => {
+            'month' => {
+              key: 'form1[0].#subform[0].DOB_Month[0]',
+              limit: 2,
+              question_num: 4,
+              question_suffix: 'A',
+              question_text: 'DATE OF BIRTH'
+            },
+            'day' => {
+              key: 'form1[0].#subform[0].DOB_Day[0]',
+              limit: 2,
+              question_num: 4,
+              question_suffix: 'B',
+              question_text: 'DATE OF BIRTH'
+            },
+            'year' => {
+              key: 'form1[0].#subform[0].DOB_Year[0]',
+              limit: 4,
+              question_num: 4,
+              question_suffix: 'C',
+              question_text: 'DATE OF BIRTH'
+            }
+          }
+        }, # end veteran_information
+        'email1' => {
+          key: 'form1[0].#subform[0].E-Mail_Address[0]',
+          limit: 18,
+          question_num: 7,
+          question_suffix: 'A',
+          question_text: 'E-MAIL ADDRESS OF CLAIMANT'
+        },
+        'email2' => {
+          key: 'form1[0].#subform[0].E-Mail_Address[1]',
+          limit: 18,
+          question_num: 7,
+          question_suffix: 'A',
+          question_text: 'E-MAIL ADDRESS OF CLAIMANT'
+        },
+        'updateDiaries' => {
+          'update_diaries_yes' => { key: 'form1[0].#subform[0].YES_CHECKBOX1[0]' },
+          'update_diaries_no' => { key: 'form1[0].#subform[0].NO_CHECKBOX1[0]' }
+        },
+        'signature' => {
+          key: 'signature'
+        },
+        'dateSigned' => {
+          'month' => {
+            key: 'form1[0].#subform[1].#subform[2].Date_Signed_Month[0]'
+          },
+          'day' => {
+            key: 'form1[0].#subform[1].#subform[2].Date_Signed_Day[0]'
+          },
+          'year' => {
+            key: 'form1[0].#subform[1].#subform[2].Date_Signed_Year[0]'
+          }
+        } # end date_signed
+      }.freeze
+
+      def merge_fields(_options = {})
+        merge_veteran_helpers
+
+        expand_signature(@form_data['veteranInformation']['fullName'])
+        @form_data['dateSigned'] = split_date(@form_data['signatureDate'])
+
+        @form_data
+      end
+
+      def merge_veteran_helpers
+        veteran_information = @form_data['veteranInformation']
+        # extract middle initial
+        veteran_information['fullName'] = extract_middle_i(veteran_information, 'fullName')
+
+        # extract ssn
+        ssn = veteran_information['ssn']
+        veteran_information['ssn'] = split_ssn(ssn.delete('-')) if ssn.present?
+
+        # extract birth date
+        veteran_information['dateOfBirth'] = split_date(veteran_information['dateOfBirth'])
+
+        # extract email address
+        extract_email
+
+        update_diaries = @form_data['updateDiaries']
+        @form_data['updateDiaries'] = {
+          'update_diaries_yes' => select_checkbox(update_diaries),
+          'update_diaries_no' => select_checkbox(!update_diaries)
+        }
+      end
+
+      def extract_email
+        email_address = @form_data['emailAddress']
+        return if email_address.nil?
+
+        if email_address.length > 17 && email_address.length < 37
+          @form_data['email1'] = email_address[0..17]
+          @form_data['email2'] = email_address[18..]
+        elsif
+          @form_data['email1'] = email_address
+        end
+      end
+    end
+  end
+end
