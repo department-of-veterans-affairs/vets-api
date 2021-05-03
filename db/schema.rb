3419c9150a8a3ce805c6975e5bfe69f7532dfb3d
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-05-03 11:08:49 -0400

    move to separate PR

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2021_04_20_131905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
-  enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -592,17 +591,6 @@ ActiveRecord::Schema.define(version: 2021_04_20_131905) do
     t.datetime "updated_at", null: false
     t.index ["created_at"], name: "index_personal_information_logs_on_created_at"
     t.index ["error_class"], name: "index_personal_information_logs_on_error_class"
-  end
-
-  create_table "pghero_query_stats", force: :cascade do |t|
-    t.text "database"
-    t.text "user"
-    t.text "query"
-    t.bigint "query_hash"
-    t.float "total_time"
-    t.bigint "calls"
-    t.datetime "captured_at"
-    t.index ["database", "captured_at"], name: "index_pghero_query_stats_on_database_and_captured_at"
   end
 
   create_table "preference_choices", id: :serial, force: :cascade do |t|
