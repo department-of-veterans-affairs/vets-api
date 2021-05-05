8b5fcc0cfe597101c86fe1e1b9fd0d3c3c8ff285
vets-api <mark.viterna@va.gov>
2021-05-05 11:33:27 -0400

    Feedback per Bryan Schofield

--- modules/va_forms/app/swagger/va_forms/v0/controller_swagger.rb
+++ modules/va_forms/app/swagger/va_forms/v0/controller_swagger.rb
@@ -46,15 +46,27 @@ module VAForms
           end
 
           response 401 do
-            key :description, 'Invalid authentication credentials'
-          end
-
-          response 404 do
-            key :description, 'Not Found'
+            key :description, 'Unauthorized'
+            content 'application/json' do
+              schema do
+                property :message do
+                  key :type, :string
+                  key :example, 'Invalid authentication credentials'
+                end
+              end
+            end
           end
 
           response 429 do
-            key :description, 'API rate limit exceeded'
+            key :description, 'Too many requests'
+            content 'application/json' do
+              schema do
+                property :message do
+                  key :type, :string
+                  key :example, 'Too many requests'
+                end
+              end
+            end
           end
         end
       end
@@ -95,15 +107,46 @@ module VAForms
           end
 
           response 401 do
-            key :description, 'Invalid authentication credentials'
+            key :description, 'Unauthorized'
+            content 'application/json' do
+              schema do
+                property :message do
+                  key :type, :string
+                  key :example, 'Invalid authentication credentials'
+                end
+              end
+            end
           end
 
           response 404 do
             key :description, 'Not Found'
+            content 'application/json' do
+              schema do
+                key :type, :object
+                key :required, [:errors]
+                property :errors do
+                  key :type, :array
+                  items do
+                    property :message do
+                      key :type, :string
+                      key :example, 'Form not found'
+                    end
+                  end
+                end
+              end
+            end
           end
 
           response 429 do
-            key :description, 'API rate limit exceeded'
+            key :description, 'Too many requests'
+            content 'application/json' do
+              schema do
+                property :message do
+                  key :type, :string
+                  key :example, 'Too many requests'
+                end
+              end
+            end
           end
         end
       end
