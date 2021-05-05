63554670959f00effa735f3d988434413296adf8
Jack <jackschuss@gmail.com>
2021-05-04 20:02:54 -0400

    Api 6669/91 day validation evidence window (#6771)
    * query when the NOD was set to submitted for validation
    
    * rubocop go brrrr
    
    * add specs, update within_legal_window? method
    
    * extract method

--- modules/appeals_api/app/services/appeals_api/evidence_submission_request_validator.rb
+++ modules/appeals_api/app/services/appeals_api/evidence_submission_request_validator.rb
@@ -2,14 +2,17 @@
 
 module AppealsApi
   class EvidenceSubmissionRequestValidator
+    EVIDENCE_SUBMISSION_DAYS_WINDOW = 91
+
     def initialize(nod_id, request_ssn)
       @nod_id = nod_id
       @request_ssn = request_ssn
     end
 
     def call
-      return [:error, record_not_found_error] unless notice_of_disagreement_exists?
+      return [:error, record_not_found_error] if notice_of_disagreement.blank?
       return [:error, invalid_review_option_error] unless evidence_accepted?
+      return [:error, outside_legal_window_error] unless within_legal_window?
       return [:error, invalid_veteran_id_error] unless ssn_match?
 
       [:ok, {}]
@@ -17,19 +20,32 @@ module AppealsApi
 
     private
 
-    def notice_of_disagreement_exists?
+    def notice_of_disagreement
       @notice_of_disagreement ||= AppealsApi::NoticeOfDisagreement.find_by(id: @nod_id)
     end
 
     def evidence_accepted?
-      @notice_of_disagreement.accepts_evidence?
+      notice_of_disagreement.accepts_evidence?
+    end
+
+    def submitted_status
+      @submitted_status ||= notice_of_disagreement
+                            .status_updates
+                            .where(to: 'submitted').order(created_at: :desc).first
+    end
+
+    def within_legal_window?
+      return true unless submitted_status
+
+      submitted_status.status_update_time >=
+        EVIDENCE_SUBMISSION_DAYS_WINDOW.days.ago.end_of_day
     end
 
     def ssn_match?
       # if PII expunged not validating for matching SSNs
-      return true unless @notice_of_disagreement.auth_headers
+      return true unless notice_of_disagreement.auth_headers
 
-      @request_ssn == @notice_of_disagreement.auth_headers['X-VA-SSN']
+      @request_ssn == notice_of_disagreement.auth_headers['X-VA-SSN']
     end
 
     def record_not_found_error
@@ -42,6 +58,10 @@ module AppealsApi
 
     def invalid_veteran_id_error
       { title: 'unprocessable_entity', detail: I18n.t('appeals_api.errors.mismatched_ssns') }
+    end
+
+    def outside_legal_window_error
+      { title: 'unprocessable_entity', detail: I18n.t('appeals_api.errors.outside_legal_window') }
     end
   end
 end
