daa23116eb5c12a76dfacd2925f99a99f46a27b9
AJ Magdub <ahmed.magdub@va.gov>
2021-05-05 07:55:34 -0600

    added available slots controller and tests

--- modules/vaos/config/routes.rb
+++ modules/vaos/config/routes.rb
@@ -44,5 +44,6 @@ VAOS::Engine.routes.draw do
 
   namespace :v2, defaults: { format: :json } do
     get '/locations/:location_id/clinics', to: 'clinics#index'
+    get '/locations/:location_id/clinics/:clinic_id/slots', to: 'slots#index'
   end
 end
