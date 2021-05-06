511cac0b5b1cf08d6a9613abfb913d036c723280
Devin McCurdy <mccurdy_devin@bah.com>
2021-05-06 13:11:03 -0400

    GIDS api v1 (#6763)

--- app/controllers/v1/gids/yellow_ribbon_programs_controller.rb
+++ app/controllers/v1/gids/yellow_ribbon_programs_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-module V0
+module V1
   module GIDS
     class YellowRibbonProgramsController < GIDSController
       def index
-        render json: service.get_yellow_ribbon_programs_v0(scrubbed_params)
+        render json: service.get_yellow_ribbon_programs_v1(scrubbed_params)
       end
     end
   end
