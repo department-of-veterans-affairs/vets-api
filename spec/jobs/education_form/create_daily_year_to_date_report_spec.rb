0a681737e3ba9315b2d468d2609c7cc5a0bbccd6
Matt Roth <roth_matthew@bah.com>
2021-05-06 10:23:14 -0400

    Update create_daily_year_to_date_report_spec.rb

--- spec/jobs/education_form/create_daily_year_to_date_report_spec.rb
+++ spec/jobs/education_form/create_daily_year_to_date_report_spec.rb
@@ -40,10 +40,9 @@ RSpec.describe EducationForm::CreateDailyYearToDateReport, type: :aws_helpers do
       end
       create(:education_benefits_submission, form_type: '0993', created_at: date, region: :western)
       create(:education_benefits_submission, form_type: '0994',
-             created_at: date, region: :eastern, vettec: true, chapter33: false)
+                                             created_at: date, region: :eastern, vettec: true, chapter33: false)
       create(:education_benefits_submission, form_type: '1990s',
-             created_at: date, region: :western, vrrap: true, chapter33: false)
-
+                                             created_at: date, region: :western, vrrap: true, chapter33: false)
     end
 
     context 'with the date variable set' do
