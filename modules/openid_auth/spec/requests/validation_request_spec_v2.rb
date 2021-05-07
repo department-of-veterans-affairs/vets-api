f9887a0d5a30d30a99f7270979d4de9a18a11978
dbrown-va <43859081+dbrown-va@users.noreply.github.com>
2021-05-07 15:59:23 -0400

    API-6838 Response body charon errors (#6789)
    * API-6841-charon-error-response
    
    * process charaon response body for error details
    
    * Removed charon exception handling as responses are still returned, just non-200
    
    * Removed exception transformation to allow for 401 and 500 responses.
    
    * Standardized error setting.
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Typo fix
    
    * Linting
    
    * Last linting
    
    * Missing space..
    
    * Add missing error in rescue
    
    * Reworked based on actual non-200 responses from RestClient
    
    * Linting
    
    * on 401 message field first, then use status field if
    
    * Small fixes for error code raising
    
    * More modifications.
    
    * Linting
    
    * Linting if to case
    
    * Small sentry syntax update
    
    * Added 403 case
    
    Co-authored-by: dbrown-va <derek.brown5@va.gov>
    Co-authored-by: Larry Crochet <larry.crochet@libertyits.com>

--- modules/openid_auth/spec/requests/validation_request_spec_v2.rb
+++ modules/openid_auth/spec/requests/validation_request_spec_v2.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'webmock'
 
 RSpec.describe 'Validated Token API endpoint', type: :request, skip_emis: true do
   include SchemaMatchers
@@ -106,6 +107,26 @@ RSpec.describe 'Validated Token API endpoint', type: :request, skip_emis: true d
   let(:bad_launch_response) do
     instance_double(RestClient::Response,
                     code: 503)
+  end
+  let(:charon_response) do
+    instance_double(RestClient::Response,
+                    code: 200,
+                    body: { status: '200', value: '1' }.to_json)
+  end
+  let(:invalid_site_charon_response) do
+    instance_double(RestClient::Response,
+                    code: 400,
+                    body: { status: '400', message: 'Unknown vista site specified: [442]' }.to_json)
+  end
+  let(:failed_charon_response) do
+    instance_double(RestClient::Response,
+                    code: 401,
+                    body: { status: '500', value: '-1' }.to_json)
+  end
+  let(:bad_charon_response) do
+    instance_double(RestClient::Response,
+                    code: 500,
+                    body: { status: '500', value: '-2' }.to_json)
   end
   let(:json_api_response) do
     {
@@ -423,8 +444,6 @@ RSpec.describe 'Validated Token API endpoint', type: :request, skip_emis: true d
     end
 
     it 'v2 POST returns json response if valid user' do
-      charon_response = instance_double(RestClient::Response,
-                                        code: 200)
       with_ssoi_charon_configured do
         allow(RestClient).to receive(:get).and_return(launch_with_sta3n_response, charon_response)
         post '/internal/auth/v2/validation',
@@ -435,6 +454,58 @@ RSpec.describe 'Validated Token API endpoint', type: :request, skip_emis: true d
         expect(JSON.parse(response.body)['data']['attributes'].keys)
           .to eq(json_api_response_vista_id['data']['attributes'].keys)
         expect(JSON.parse(response.body)['data']['attributes']['launch']['sta3n']).to eq('456')
+      end
+    end
+
+    it 'v2 POST returns json response if 400 charon response' do
+      with_ssoi_charon_configured do
+        stub_request(:get, 'http://example.com/smart/launch').to_return(
+          body: { launch: 'eyAicGF0aWVudCI6ICIxMjM0NSIsICJzdGEzbiI6ICI0NTYiIH0K' }.to_json, status: 200
+        )
+        stub_request(:get, 'http://example.com/services/charon?duz=789012345&site=456').to_raise(
+          RestClient::ExceptionWithResponse.new(invalid_site_charon_response, 400)
+        )
+        post '/internal/auth/v2/validation',
+             params: { aud: %w[https://example.com/xxxxxxservices/xxxxx] },
+             headers: auth_header
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to be_a(String)
+        expect(JSON.parse(response.body)['errors'].first['detail']).to eq 'Unknown vista site specified: [442]'
+      end
+    end
+
+    it 'v2 POST returns json response if 401 charon response' do
+      with_ssoi_charon_configured do
+        stub_request(:get, 'http://example.com/smart/launch').to_return(
+          body: { launch: 'eyAicGF0aWVudCI6ICIxMjM0NSIsICJzdGEzbiI6ICI0NTYiIH0K' }.to_json, status: 200
+        )
+        stub_request(:get, 'http://example.com/services/charon?duz=789012345&site=456').to_raise(
+          RestClient::ExceptionWithResponse.new(failed_charon_response, 401)
+        )
+        post '/internal/auth/v2/validation',
+             params: { aud: %w[https://example.com/xxxxxxservices/xxxxx] },
+             headers: auth_header
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to be_a(String)
+        expect(JSON.parse(response.body)['errors'].first['detail']).to eq 'Charon menu-code: -1'
+      end
+    end
+
+    it 'v2 POST returns json response if 500 charon response' do
+      with_ssoi_charon_configured do
+        stub_request(:get, 'http://example.com/smart/launch').to_return(
+          body: { launch: 'eyAicGF0aWVudCI6ICIxMjM0NSIsICJzdGEzbiI6ICI0NTYiIH0K' }.to_json, status: 200
+        )
+        stub_request(:get, 'http://example.com/services/charon?duz=789012345&site=456').to_raise(
+          RestClient::ExceptionWithResponse.new(bad_charon_response, 500)
+        )
+
+        post '/internal/auth/v2/validation',
+             params: { aud: %w[https://example.com/xxxxxxservices/xxxxx] },
+             headers: auth_header
+        expect(response).to have_http_status(:internal_server_error)
+        expect(response.body).to be_a(String)
+        expect(JSON.parse(response.body)['errors'].first['detail']).to eq 'Failed validation with Charon.'
       end
     end
 
