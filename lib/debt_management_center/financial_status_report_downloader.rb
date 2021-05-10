f7c38e65833095fb06c222c2f58883154a30aadd
Dillo Raju <draju@governmentcio.com>
2021-05-10 17:34:14 -0400

    24402 trouble shooting fsr pdf download - Adding more exception handling to critical paths - Download the pdf inline

--- lib/debt_management_center/financial_status_report_downloader.rb
+++ lib/debt_management_center/financial_status_report_downloader.rb
@@ -5,13 +5,19 @@ require 'uri'
 
 module DebtManagementCenter
   class FinancialStatusReportDownloader
+    class FilenetIdNotFoundError < StandardError; end
+
     def initialize(financial_status_report)
       @financial_status_report = financial_status_report
     end
 
     def download_pdf
+      id = @financial_status_report.filenet_id
+
+      raise FilenetIdNotFoundError if id.blank?
+
       uri = URI.parse(
-        "#{Settings.dmc.url}financial-status-report/documentstream?objectId=#{@financial_status_report.filenet_id}"
+        "#{Settings.dmc.url}financial-status-report/documentstream?objectId=#{id}"
       )
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new(uri.request_uri)
@@ -20,6 +26,8 @@ module DebtManagementCenter
       request_headers.each { |header, value| request[header] = value }
       response = http.request(request)
       response.body
+    rescue FilenetIdNotFoundError
+      raise_filenet_id_not_found_error
     end
 
     private
@@ -30,6 +38,13 @@ module DebtManagementCenter
         client_id: Settings.dmc.client_id,
         client_secret: Settings.dmc.client_secret
       }
+    end
+
+    def raise_filenet_id_not_found_error
+      raise Common::Exceptions::ServiceError.new(
+        detail: 'Filenet ID not present in the user FSR from Redis.',
+        source: 'Debt Management, financial_status_report_downloader.'
+      )
     end
   end
 end
