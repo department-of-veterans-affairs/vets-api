6a4a12bdfda4d03206b81d53ee0e530b9069ad97
Jack Schuss <jackschuss@gmail.com>
2021-05-10 16:14:03 -0400

    V2

--- modules/appeals_api/app/controllers/appeals_api/v2/decision_reviews/higher_level_reviews_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v2/decision_reviews/higher_level_reviews_controller.rb
@@ -23,7 +23,7 @@ class AppealsApi::V2::DecisionReviews::HigherLevelReviewsController < AppealsApi
 
   def create
     @higher_level_review.save
-    AppealsApi::HigherLevelReviewPdfSubmitJob.perform_async(@higher_level_review.id, version: 'V2')
+    AppealsApi::HigherLevelReviewPdfSubmitJob.perform_async(@higher_level_review.id, 'V2')
     render_higher_level_review
   end
 
