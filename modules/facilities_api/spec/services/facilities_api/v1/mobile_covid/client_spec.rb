310af97e3e8d372d733c6506bf0c0ad71af9b54e
Lance Sanchez <DarkBushido@users.noreply.github.com>
2021-05-11 13:11:29 -0500

    Creating a new client to talk to mobile api unauthenticated
    I know the logic is unpleasant but the presence of a key is all I have.
    If the key is there then covid online scheduling is available, if its
    missing it isn't available.

---
+++ modules/facilities_api/spec/services/facilities_api/v1/mobile_covid/client_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+vcr_options = {
+  cassette_name: 'facilities/mobile/covid',
+  match_requests_on: %i[path query],
+  allow_playback_repeats: true,
+  record: :new_episodes
+}
+
+RSpec.describe FacilitiesApi::V1::MobileCovid::Client, team: :facilities, vcr: vcr_options do
+  let(:mobile_client) { described_class.new }
+  context 'with an http timeout' do
+    it 'logs an error and raise GatewayTimeout' do
+      allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(Faraday::TimeoutError)
+      expect do
+        mobile_client.direct_booking_eligibility_criteria_by_id('523A5')
+      end.to raise_error(Common::Exceptions::GatewayTimeout)
+    end
+  end
+
+  describe '#direct_booking_eligibility_criteria_by_id' do
+    it 'finds a facility by ID' do
+      response = mobile_client.direct_booking_eligibility_criteria_by_id('523A5')
+      expect(response.id).to eql('523A5')
+    end
+    context 'Covid online scheduling is available' do
+      it 'checks covid_online_scheduling_available?' do
+        response = mobile_client.direct_booking_eligibility_criteria_by_id('523A5')
+        expect(response.covid_online_scheduling_available?).to be_truthy
+      end
+    end
+
+  end
+end
