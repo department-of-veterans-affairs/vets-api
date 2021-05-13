4640a6a74059669d87113c2c4085c7dba8c9fcee
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-05-13 07:44:10 -0500

    API-7020 - Hide ClaimsApi V2 docs behind a Setting

--- modules/claims_api/app/controllers/claims_api/metadata_controller.rb
+++ modules/claims_api/app/controllers/claims_api/metadata_controller.rb
@@ -6,8 +6,39 @@ module ClaimsApi
     skip_after_action :set_csrf_header
     skip_before_action(:authenticate)
 
-    def index # rubocop:disable Metrics/MethodLength
-      render json: {
+    V2_DOCS_ENABLED = Settings.claims_api.v2_docs.enabled
+
+    def index
+      render json: V2_DOCS_ENABLED ? version_2_through_previous : version_1_through_previous
+    end
+
+    private
+
+    def version_1_through_previous
+      {
+        meta: {
+          versions: [
+            {
+              version: '1.0.0',
+              internal_only: false,
+              status: VERSION_STATUS[:current],
+              path: '/services/claims/docs/v1/api',
+              healthcheck: '/services/claims/v1/healthcheck'
+            },
+            {
+              version: '0.0.1',
+              internal_only: true,
+              status: VERSION_STATUS[:previous],
+              path: '/services/claims/docs/v0/api',
+              healthcheck: '/services/claims/v0/healthcheck'
+            }
+          ]
+        }
+      }
+    end
+
+    def version_2_through_previous # rubocop:disable Metrics/MethodLength
+      {
         meta: {
           versions: [
             {
