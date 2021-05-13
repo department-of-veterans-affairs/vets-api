5177e920f3ebbc1ceb94e4a02aead48e400bf988
Dan Hinze <dan.hinze@adhocteam.us>
2021-05-13 10:19:19 -0500

    Move to v1

--- modules/claims_api/app/controllers/claims_api/v1/forms/power_of_attorney_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v1/forms/power_of_attorney_controller.rb
@@ -47,6 +47,7 @@ module ClaimsApi
 
           # This job only occurs when a Veteran submits a PoA request, they are not required to submit a document.
           ClaimsApi::PoaUpdater.perform_async(power_of_attorney.id) unless header_request?
+          ClaimsApi::VBMSUpdater.perform_async(power_of_attorney.id)
           data = power_of_attorney.form_data
           ClaimsApi::PoaFormBuilderJob.perform_async(power_of_attorney.id) if data['signatures'].present?
 
@@ -119,6 +120,10 @@ module ClaimsApi
         end
 
         private
+
+        def enable_vmbs_access?
+          form_attributes['recordConsent'] && (form_attributes['consentLimits'].nil? || form_attributes['consentLimits'].empty?)
+        end
 
         def current_poa_begin_date
           return nil if current_poa.try(:begin_date).blank?
