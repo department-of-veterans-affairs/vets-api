e36b5a2dff7bdf0d2947a25b4b1b72ea2b10ef69
Dan Hinze <dan.hinze@adhocteam.us>
2021-05-17 15:50:44 -0500

    Lint fixes

--- modules/claims_api/app/workers/claims_api/vmbs_updater.rb
+++ modules/claims_api/app/workers/claims_api/vmbs_updater.rb
@@ -4,27 +4,21 @@ require 'sidekiq'
 require 'bgs'
 
 module ClaimsApi
-  class # frozen_string_literal: true
+  class VBMSUpdater
+    include Sidekiq::Worker
 
-    require 'sidekiq'
-    require 'bgs'
-    
-    module ClaimsApi
-      class VBMSUpdater
-        include Sidekiq::Worker
-    
-        def perform(power_of_attorney_id, target_veteran)
-          poa_form = ClaimsApi::PowerOfAttorney.find(power_of_attorney_id)
-          service = BGS::Services.new(
-            external_uid: poa_form.external_uid,
-            external_key: poa_form.external_key
-          )
-          service.corporate_update.update_poa_access(
-            participant_id: target_veteran.participant_id,
-            poa_code: poa_form.form_data.dig('serviceOrganization', 'poaCode'),
-            allow_poa_access: 'y',
-            allow_poa_c_add: 'y'
-          )
-        end
-      end
+    def perform(power_of_attorney_id, target_veteran)
+      poa_form = ClaimsApi::PowerOfAttorney.find(power_of_attorney_id)
+      service = BGS::Services.new(
+        external_uid: poa_form.external_uid,
+        external_key: poa_form.external_key
+      )
+      service.corporate_update.update_poa_access(
+        participant_id: target_veteran.participant_id,
+        poa_code: poa_form.form_data.dig('serviceOrganization', 'poaCode'),
+        allow_poa_access: 'y',
+        allow_poa_c_add: 'y'
+      )
     end
+  end
+end
