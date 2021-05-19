2c0ea0257523ccbacfc3797f8687f35a9c8d3619
John Bramley <bramleyjl@gmail.com>
2021-05-19 17:29:15 -0400

    moves rubocop temporary disable to affected specs

--- spec/models/form_profile_spec.rb
+++ spec/models/form_profile_spec.rb
@@ -1175,6 +1175,7 @@ RSpec.describe FormProfile, type: :model do
       end
     end
 
+    # rubocop:disable RSpec/MultipleMemoizedHelpers
     context 'with a higher level review form' do
       let(:schema_name) { '20-0996' }
       let(:schema) { VetsJsonSchema::SCHEMAS[schema_name] }
@@ -1276,6 +1277,7 @@ RSpec.describe FormProfile, type: :model do
         expect(errors.empty?).to eq(true), "schema errors: #{errors}"
       end
     end
+    # rubocop:enable RSpec/MultipleMemoizedHelpers
 
     context 'with a pension application form' do
       it 'returns the va profile mapped to the pension form' do
