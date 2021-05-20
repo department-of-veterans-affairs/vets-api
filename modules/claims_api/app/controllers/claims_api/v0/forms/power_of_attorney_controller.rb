1a7fbc29870c45158131bb3ba022e942f6a0b80c
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-05-20 14:46:40 -0500

    API-7127 - Gets active POA for v0 working in the same manner as v1

--- modules/claims_api/app/controllers/claims_api/v0/forms/power_of_attorney_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v0/forms/power_of_attorney_controller.rb
@@ -77,11 +77,24 @@ module ClaimsApi
         #
         # @return [JSON] Last POA change request through Claims API
         def active
-          power_of_attorney = ClaimsApi::PowerOfAttorney.find_using_identifier_and_source(header_md5: header_md5,
-                                                                                          source_name: source_name)
-          raise ::Common::Exceptions::ResourceNotFound.new(detail: 'Resource not found') unless power_of_attorney
+          raise ::Common::Exceptions::ResourceNotFound.new(detail: 'POA not found') unless current_poa_code
 
-          render json: power_of_attorney, serializer: ClaimsApi::PowerOfAttorneySerializer
+          render json: {
+            data: {
+              id: nil,
+              type: 'claims_api_power_of_attorneys',
+              attributes: {
+                status: ClaimsApi::PowerOfAttorney::UPDATED,
+                date_request_accepted: current_poa_begin_date,
+                representative: {
+                  service_organization: {
+                    poa_code: current_poa_code
+                  }
+                },
+                previous_poa: previous_poa_code
+              }
+            }
+          }
         end
 
         # POST to validate 2122 submission payload.
@@ -124,6 +137,24 @@ module ClaimsApi
               }
             }
           }
+        end
+
+        def current_poa_begin_date
+          return nil if current_poa.try(:begin_date).blank?
+
+          Date.strptime(current_poa.begin_date, '%m/%d/%Y')
+        end
+
+        def current_poa
+          @current_poa ||= BGS::PowerOfAttorneyVerifier.new(target_veteran).current_poa
+        end
+
+        def current_poa_code
+          current_poa.try(:code)
+        end
+
+        def previous_poa_code
+          @previous_poa_code ||= BGS::PowerOfAttorneyVerifier.new(target_veteran).previous_poa_code
         end
       end
     end
