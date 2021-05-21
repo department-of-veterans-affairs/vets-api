452c4b70797c15f3f33f35bdd378a0b9203f2d4d
Lee DeBoom <lee.deboom@oddball.io>
2021-05-21 14:43:49 -0500

    API-6614

--- modules/claims_api/config/initializers/okcomputer.rb
+++ modules/claims_api/config/initializers/okcomputer.rb
@@ -78,7 +78,7 @@ class VbmsCheck < BaseCheck
   def check
     connection = Faraday::Connection.new
     connection.options.timeout = 10
-    response = connection.get(Settings.vbms.url)
+    response = connection.get("#{Settings.vbms.url}/vbms-efolder-svc/upload-v1/eFolderUploadService?wsdl")
     response.status == 200 ? process_success : process_failure
   rescue
     process_failure
