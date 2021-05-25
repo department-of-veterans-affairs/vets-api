b96f3f416bb6d53dc0ff6b4fd507944a858b10b0
Corey Ferris <cferris320@gmail.com>
2021-05-25 20:14:45 +0000

    completed appointment service cassette recordings

--- modules/vaos/spec/request/v2/appointments_request_spec.rb
+++ modules/vaos/spec/request/v2/appointments_request_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'vaos appointments', type: :request, skip_mvi: true do
   before do
     Flipper.enable('va_online_scheduling')
     sign_in_as(current_user)
-    #allow_any_instance_of(VAOS::UserService).to receive(:session).and_return('stubbed_token')
+    allow_any_instance_of(VAOS::UserService).to receive(:session).and_return('stubbed_token')
   end
 
   let(:inflection_header) { { 'X-Key-Inflection' => 'camel' } }
@@ -38,12 +38,12 @@ RSpec.describe 'vaos appointments', type: :request, skip_mvi: true do
 
       context 'returns list of appointments' do
         it 'has access and returns va appointments' do
-          VCR.use_cassette('vaos/v2/appointments/get_appointments', record: :new_episodes) do #match_requests_on: %i[method uri]
+          VCR.use_cassette('vaos/v2/appointments/get_appointments_200', match_requests_on: %i[method uri]) do
             get '/vaos/v2/appointments', params: params
 
             expect(response).to have_http_status(:ok)
             expect(response.body).to be_a(String)
-            expect(JSON.parse(response.body)['data'].size).to eq(1)
+            expect(JSON.parse(response.body)['data'].size).to eq(31)
             expect(response).to match_response_schema('vaos/v2/appointments', { strict: false })
           end
         end
