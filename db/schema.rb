87f844140d275a5c5e320fc5e1d158d29c952a66
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-05-26 14:38:49 -0400

    git merge conflict copy/paste issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_202141) do
+ActiveRecord::Schema.define(version: 2021_05_25_183405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "pg_stat_statements"
