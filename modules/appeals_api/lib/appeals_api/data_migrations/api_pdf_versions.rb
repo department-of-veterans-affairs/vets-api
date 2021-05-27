146c74aabacb313ee61e2530e61181d3bdd3c73c
Jack Schuss <jackschuss@gmail.com>
2021-05-27 11:34:14 -0400

    rubocop

--- modules/appeals_api/lib/appeals_api/data_migrations/api_pdf_versions.rb
+++ modules/appeals_api/lib/appeals_api/data_migrations/api_pdf_versions.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 module AppealsApi
   module DataMigrations
     module ApiPdfVersions
       module_function
 
+      # rubocop:disable Rails/SkipsModelValidations
       def run
-        AppealsApi::HigherLevelReview.
-          update_all(pdf_version: 'V1', api_version: 'V1')
-        AppealsApi::NoticeOfDisagreement.
-          update_all(pdf_version: 'V1', api_version: 'V1')
+        AppealsApi::HigherLevelReview
+          .update_all(pdf_version: 'V1', api_version: 'V1')
+        AppealsApi::NoticeOfDisagreement
+          .update_all(pdf_version: 'V1', api_version: 'V1')
       end
+      # rubocop:enable Rails/SkipsModelValidations
     end
   end
 end
