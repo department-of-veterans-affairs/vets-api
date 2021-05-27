3cb19dabc6d921638d5151743377837f860082f1
Teesh <teesh.s93@gmail.com>
2021-05-27 12:41:09 -0500

    Check for size being non-zero

--- modules/mobile/app/serializers/mobile/v0/attachment_serializer.rb
+++ modules/mobile/app/serializers/mobile/v0/attachment_serializer.rb
@@ -8,7 +8,7 @@ module Mobile
       attribute :id
       attribute :name
       attribute :message_id
-      attribute :attachment_size
+      attribute :attachment_size, if: -> { object.attachment_size > 0 } # TODO: Have MHV fix the issue at the source and revert this patch
 
       link(:download) { Mobile::UrlHelper.new.v0_message_attachment_url(object.message_id, object.id) }
     end
