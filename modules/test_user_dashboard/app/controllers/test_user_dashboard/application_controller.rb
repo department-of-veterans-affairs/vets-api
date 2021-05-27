6ff58d9f77289e4487b7a957436824f5cc163373
John Bramley <bramleyjl@gmail.com>
2021-05-27 11:55:49 -0400

    TUD API dummy route auth added

--- modules/test_user_dashboard/app/controllers/test_user_dashboard/application_controller.rb
+++ modules/test_user_dashboard/app/controllers/test_user_dashboard/application_controller.rb
@@ -1,6 +1,38 @@
 # frozen_string_literal: true
 
+require 'sentry_logging'
+
 module TestUserDashboard
   class ApplicationController < ActionController::API
+    include SentryLogging
+    before_action :require_jwt
+    SECRET = 'password123'
+
+    def require_jwt
+      # payload from example
+      exp = Time.now.to_i + 300
+      payload = { 'iss': 'tud.staging.va.gov',
+                  'exp': exp,
+                  'aud': '238d4793-70de-4183-9707-48ed8ecd19d9',
+                  'sub': '19016b73-3ffa-4b26-80d8-aa9287738677' }
+      token = JWT.encode payload, SECRET, 'HS256'
+
+      head :forbidden unless valid_token(token)
+    end
+
+    private
+
+    def valid_token(token)
+      return false unless token
+
+      token.gsub!('Bearer ', '')
+      begin
+        JWT.decode token, SECRET, true
+        return true
+      rescue JWT::DecodeError => e
+        log_message_to_sentry('Error decoding TUD JWT: ', :error, body: e.message)
+      end
+      false
+    end
   end
 end
