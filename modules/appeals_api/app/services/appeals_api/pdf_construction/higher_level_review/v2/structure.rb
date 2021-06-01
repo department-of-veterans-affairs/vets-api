cccbd44ec3e382f3b5beca8ba6b88ca31879808c
Laura <30485143+ElleeB@users.noreply.github.com>
2021-06-01 14:45:13 -0400

    API-7416 character limits for Veteran first and last names (#7076)
    * Print veteran middle initial
    
    * Update tests and pdf fixtures in response to newly printed middle initial
    
    * Add character limits for veteran first and last to headers schema
    
    * Change emailAddressText schema key to email

--- modules/appeals_api/app/services/appeals_api/pdf_construction/higher_level_review/v2/structure.rb
+++ modules/appeals_api/app/services/appeals_api/pdf_construction/higher_level_review/v2/structure.rb
@@ -17,6 +17,7 @@ module AppealsApi
           options = {
             # Section I: Vet's ID
             # Veteran name is filled out through autosize text box, not pdf fields
+            form_fields.middle_initial => form_data.middle_initial,
             form_fields.first_three_ssn => form_data.first_three_ssn,
             form_fields.second_two_ssn => form_data.second_two_ssn,
             form_fields.last_four_ssn => form_data.last_four_ssn,
