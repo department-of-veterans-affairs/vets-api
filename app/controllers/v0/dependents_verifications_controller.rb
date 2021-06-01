d33bd707fb913f1fb777201aad7d9e1e4225aa32
Kathleen Crawford <kcrawford@governmentcio.com>
2021-06-01 16:10:03 -0700

    linting

--- app/controllers/v0/dependents_verifications_controller.rb
+++ app/controllers/v0/dependents_verifications_controller.rb
@@ -15,7 +15,7 @@ module V0
       claim.add_claimant_info(current_user) if current_user&.loa3?
 
       unless claim.save
-        StatsD.increment('api.dependency_verification_claim.failure')
+        StatsD.increment("#{stats_key}.failure")
         Raven.tags_context(team: 'vfs-ebenefits') # tag sentry logs with team name
         raise Common::Exceptions::ValidationErrors, claim
       end
@@ -36,6 +36,10 @@ module V0
 
     def filtered_params
       params.require(:dependency_verification_claim)
+    end
+
+    def short_name
+      'dependency_verification_claim'
     end
   end
 end
