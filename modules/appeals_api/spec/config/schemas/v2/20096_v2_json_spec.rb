e167ba3aaf657c34523966bd60469db0fb94dd89
Laura <30485143+ElleeB@users.noreply.github.com>
2021-06-02 18:03:26 -0400

    Fixup (#7112)

---
+++ modules/appeals_api/spec/config/schemas/v2/20096_v2_json_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require AppealsApi::Engine.root.join('spec', 'spec_helper.rb')
+
+describe 'VA Form 20-0996 JSON Schema', type: :request do
+  include SchemaHelpers
+  include FixtureHelpers
+
+  let(:json_schema) { read_schema '200996.json', 'v2' }
+
+  let(:errors) { validator.validate(json).to_a }
+  let(:errors_minimal) { validator.validate(json_minimal).to_a }
+  let(:errors_extra) { validator.validate(json_extra).to_a }
+  let(:json) { fixture_as_json 'valid_200996_v2.json' }
+  let(:json_minimal) { fixture_as_json 'valid_200996_minimum_v2.json' }
+  let(:json_extra) { fixture_as_json 'valid_200996_v2_extra.json' }
+  let(:validator) { JSONSchemer.schema(json_schema) }
+
+  it('JSON is valid') { expect(json_schema).to be_a Hash }
+
+  it('JSON Schema is valid') { expect(validator).to be_truthy }
+
+  it('basic fixture has no errors') { expect(errors).to be_empty }
+
+  it('minimal fixture has no errors') { expect(errors_minimal).to be_empty }
+
+  it('extra fixture has no errors') { expect(errors_extra).to be_empty }
+end
