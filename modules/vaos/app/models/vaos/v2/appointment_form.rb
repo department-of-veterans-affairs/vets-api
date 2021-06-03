6316ea9fe1b389271755f8b69259ef72ea43ab70
brad <brad@concord.tech>
2021-06-03 09:21:07 -0700

    added patientIcn to model

--- modules/vaos/app/models/vaos/v2/appointment_form.rb
+++ modules/vaos/app/models/vaos/v2/appointment_form.rb
@@ -11,6 +11,7 @@ module VAOS
       attribute :location_id, String
       attribute :clinic, String
       attribute :reason, String
+      attribute :patient_icn, String
       attribute :slot, Hash
       attribute :contact, Hash
       attribute :service_type, String
