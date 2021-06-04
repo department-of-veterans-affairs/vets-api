58a3b8a7eb2d976d5edbc83aa48742ffab04d2a7
Anna Carey <anna@adhocteam.us>
2021-06-03 20:27:37 -0400

    rename nod_id to nod_uuid (#7147)

--- app/workers/decision_review/submit_upload.rb
+++ app/workers/decision_review/submit_upload.rb
@@ -21,7 +21,7 @@ module DecisionReview
       appeal_submission = appeal_submission_upload.appeal_submission
       upload_url_response = DecisionReview::Service.new
                                                    .get_notice_of_disagreement_upload_url(
-                                                     nod_id: appeal_submission.submitted_appeal_uuid,
+                                                     nod_uuid: appeal_submission.submitted_appeal_uuid,
                                                      ssn: JSON.parse(appeal_submission.upload_metadata)['fileNumber']
                                                    )
       upload_url = upload_url_response.body.dig('data', 'attributes', 'location')
