59fe6acd1f184db6cc66a253b6be44eafb20e1db
John Bramley <bramleyjl@gmail.com>
2021-06-07 13:59:42 -0400

    changes auth example to use async encryption

--- modules/test_user_dashboard/app/controllers/test_user_dashboard/application_controller.rb
+++ modules/test_user_dashboard/app/controllers/test_user_dashboard/application_controller.rb
@@ -6,28 +6,27 @@ module TestUserDashboard
   class ApplicationController < ActionController::API
     include SentryLogging
     before_action :require_jwt
-    # SECRET = 'password123'
 
     def require_jwt
       # payload from example
-      exp = Time.now.to_i + 300
-      payload = { 'iss': 'tud.staging.va.gov',
-                  'exp': exp,
-                  'aud': '238d4793-70de-4183-9707-48ed8ecd19d9',
-                  'sub': '19016b73-3ffa-4b26-80d8-aa9287738677' }
-      token = JWT.encode payload, SECRET, 'HS256'
+      payload = { data: 'test' }
+      rsa_private = OpenSSL::PKey::RSA.generate 2048
+      rsa_public = rsa_private.public_key
+      token = JWT.encode payload, rsa_private, 'RS256'
+      # token = JWT.encode payload, nil, 'none'
 
-      head :forbidden unless valid_token(token)
+      head :forbidden unless valid_token(token, rsa_public)
     end
 
     private
 
-    def valid_token(token)
+    def valid_token(token, rsa_public)
       return false unless token
 
       token.gsub!('Bearer ', '')
       begin
-        JWT.decode token, SECRET, true
+        JWT.decode token, rsa_public, true, { algorithm: 'RS256' }
+        # JWT.decode token, nil, false
         return true
       rescue JWT::DecodeError => e
         log_message_to_sentry('Error decoding TUD JWT: ', :error, body: e.message)
