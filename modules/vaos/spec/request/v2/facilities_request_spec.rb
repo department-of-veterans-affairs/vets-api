8343192b24132f16698731ecde797628eee620a5
brad <brad@concord.tech>
2021-06-10 10:23:31 -0700

    WIP for RI

--- modules/vaos/spec/request/v2/facilities_request_spec.rb
+++ modules/vaos/spec/request/v2/facilities_request_spec.rb
@@ -28,17 +28,17 @@ RSpec.describe 'facilities', type: :request do
         end
       end
 
-      context 'on successful query for a facility and children' do
-        it 'returns facility details' do
-          VCR.use_cassette('vaos/v2/mobile_facility_service/get_facilities_with_children_200',
-                           match_requests_on: %i[method uri]) do
-            get '/vaos/v2/facilities?ids=688&children=true', headers: inflection_header
-            expect(response).to have_http_status(:ok)
-            expect(response.body).to be_a(String)
-            expect(response).to match_camelized_response_schema('vaos/v2/get_facilities', { strict: false })
-          end
-        end
-      end
+      # context 'on successful query for a facility and children' do
+      #   it 'returns facility details' do
+      #     VCR.use_cassette('vaos/v2/mobile_facility_service/get_facilities_with_children_200',
+      #                      match_requests_on: %i[method uri]) do
+      #       get '/vaos/v2/facilities?ids=688&children=true', headers: inflection_header
+      #       expect(response).to have_http_status(:ok)
+      #       expect(response.body).to be_a(String)
+      #       expect(response).to match_camelized_response_schema('vaos/v2/get_facilities', { strict: false })
+      #     end
+      #   end
+      # end
 
       context 'on sending a bad request to the VAOS Service' do
         it 'returns a 400 http status' do
