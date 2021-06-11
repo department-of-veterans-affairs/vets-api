38aefaaedc3c951763ac707a875c91c4c400685f
dbrown-va <derek.brown5@va.gov>
2021-06-11 08:03:56 -0700

    Linting still

--- app/controllers/openid_application_controller.rb
+++ app/controllers/openid_application_controller.rb
@@ -34,7 +34,7 @@ class OpenidApplicationController < ApplicationController
 
   def authenticate_token
     return false if token.blank?
-    
+
     # Only want to fetch the Okta profile if the session isn't already established and not a CC token
     @session = Session.find(token) unless token.client_credentials_token?
     profile = @session.profile unless @session.nil? || @session.profile.nil?
