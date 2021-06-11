c8d4b6029c677ef6f0521d25e4bd069d45f042bb
cbtkc <ctesterman@hey.com>
2021-06-11 13:29:02 -0500

    name fix

--- spec/lib/sidekiq/error_tag_spec.rb
+++ spec/lib/sidekiq/error_tag_spec.rb
@@ -8,7 +8,7 @@ describe Sidekiq::ErrorTag do
     include Sidekiq::Worker
 
     def perform
-      $named_tags = Sidekiq::Logging.logger.named_tags
+      $named_tags = Sidekiq::Logger.logger.named_tags
     end
   end
 
