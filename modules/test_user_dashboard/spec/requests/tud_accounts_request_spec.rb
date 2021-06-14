75469838b4ca31e350d84607b8d334eb25bec032
John Bramley <bramleyjl@gmail.com>
2021-06-14 13:18:14 -0400

    adds unit tests to tud authentication methods

--- modules/test_user_dashboard/spec/requests/tud_accounts_request_spec.rb
+++ modules/test_user_dashboard/spec/requests/tud_accounts_request_spec.rb
@@ -3,19 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Test User Dashboard', type: :request do
-  context 'index' do
+  describe '#index' do
     context 'without any authentication headers' do
-      get '/test_user_dashboard/tud_accounts'
+      it 'refuses the request' do
+        get '/test_user_dashboard/tud_accounts'
 
-      it { expect(response.status).to eq 403 }
-      it { expect(response.content_type).to eq 'text/html' }
+        expect(response.status).to eq 403
+        expect(response.content_type).to eq 'text/html'
+      end
     end
 
-    context 'with authentication headers' do
-      get '/test_user_dashboard/tud_accounts', '', { 'PK' => 'application/json' }
+    context 'with valid authentication headers' do
+      it 'accepts the request and returns a response' do
+        rsa_private = OpenSSL::PKey::RSA.new 2048
+        rsa_public = rsa_private.public_key
+        pub_key = Base64.encode64(rsa_public.to_der)
+        token = JWT.encode 'test', rsa_private, 'RS256'
 
-      it { expect(response.status).to eq 200 }
-      it { expect(response.content_type).to eq 'application/json; charset=utf-8' }
+        get('/test_user_dashboard/tud_accounts',
+            params: '',
+            headers: {
+              'JWT' => token,
+              'PK' => pub_key
+            })
+
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq 'application/json; charset=utf-8'
+      end
     end
   end
 end
