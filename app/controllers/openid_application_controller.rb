2d34fe0edd93a072cbe7d438511baabb4a5a2b65
dbrown-va <derek.brown5@va.gov>
2021-06-14 09:29:49 -0700

    Linting

--- app/controllers/openid_application_controller.rb
+++ app/controllers/openid_application_controller.rb
@@ -145,16 +145,12 @@ class OpenidApplicationController < ApplicationController
   # If mismatched, revoke in Okta, set @session to nil, and return false
   # POA support (profile['icn'].nil?)
   def confirm_icn_match(profile)
-    if profile['icn'].nil? || @current_user&.icn == profile['icn']
-      true
-    else
-      # Temporarily log only to get an accurate count of this issue
-      # Okta::Service.new.clear_user_session(token.identifiers.okta_uid)
-      # @session = nil
-      log_message_to_sentry('Profile ICN mismatch detected.', :warn)
-      # false
-      true
-    end
+    # Temporarily log only to get an accurate count of this issue
+    # Okta::Service.new.clear_user_session(token.identifiers.okta_uid)
+    # @session = nil
+    log_message_to_sentry('Profile ICN mismatch detected.', :warn) unless
+        profile['icn'].nil? || @current_user&.icn == profile['icn']
+    true
   end
 
   def token
