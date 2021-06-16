911b60ac77bb1add8ce0f57d75742aa8e591ee48
Jack Schuss <jackschuss@gmail.com>
2021-06-16 14:16:27 -0400

    update connection so it processes subscriptions

--- modules/appeals_api/config/initializers/event_subscriptions.rb
+++ modules/appeals_api/config/initializers/event_subscriptions.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-if ActiveRecord::Base.connected? && ActiveRecord::Base.connection.table_exists?('appeals_api_event_subscriptions')
+if ActiveRecord::Base.connection.nil? && ActiveRecord::Base.connection.table_exists?('appeals_api_event_subscriptions')
   AppealsApi::Events::Handler.subscribe(:hlr_status_updated, 'AppealsApi::Events::StatusUpdated')
   AppealsApi::Events::Handler.subscribe(:nod_status_updated, 'AppealsApi::Events::StatusUpdated')
 end
+
