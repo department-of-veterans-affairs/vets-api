9d02610d8032abee1b0d0ff2c3c9c36c9d96aad6
Travis Hilton <travis.hilton@oddball.io>
2021-06-17 12:17:06 -0500

    Changes to initializer files to get rid of deprecation warnings

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -3,9 +3,11 @@
 require 'shrine'
 require 'shrine/storage/file_system'
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new('tmp', prefix: 'uploads/cache'), # temporary. could be ebs?
-  store: Shrine::Storage::FileSystem.new('tmp', prefix: 'uploads/store') # permanent
-}
+Rails.application.reloader.to_prepare do
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new('tmp', prefix: 'uploads/cache'), # temporary. could be ebs?
+    store: Shrine::Storage::FileSystem.new('tmp', prefix: 'uploads/store') # permanent
+  }
 
-Shrine.plugin :determine_mime_type
+  Shrine.plugin :determine_mime_type
+end
--- config/initializers/zcta.rb
+++ config/initializers/zcta.rb
@@ -2,7 +2,9 @@
 
 require 'csv'
 
-ZCTA = CSV.read(Rails.root.join('lib', 'facilities', '2019_Gaz_zcta_national.tsv'),
-                col_sep: "\t", headers: true).to_h { |row| [row[0], row] }
-ZCTA_LAT_HEADER = 'INTPTLAT'
-ZCTA_LON_HEADER = 'INTPTLONG'
+Rails.application.reloader.to_prepare do
+  ZCTA = CSV.read(Rails.root.join('lib', 'facilities', '2019_Gaz_zcta_national.tsv'),
+                  col_sep: "\t", headers: true).to_h { |row| [row[0], row] }
+  ZCTA_LAT_HEADER = 'INTPTLAT'
+  ZCTA_LON_HEADER = 'INTPTLONG'
+end
