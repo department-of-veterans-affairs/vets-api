6163bd0daa8326c4de0200da9cf699123581c350
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-06-17 11:00:59 -0500

    API-7793 - Adds logic to only allow a VSR or the veteran themself retrieve the veteran id

--- modules/claims_api/app/controllers/claims_api/v2/veteran_identifier_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v2/veteran_identifier_controller.rb
@@ -11,9 +11,11 @@ module ClaimsApi
         validate_request
         veteran = find_veteran(params)
 
-        unless veteran_icn_found?(veteran)
-          raise ::Common::Exceptions::ResourceNotFound.new(detail: 'Resource not found')
-        end
+        raise ::Common::Exceptions::ResourceNotFound unless veteran_icn_found?(veteran)
+
+        user_is_the_veteran   = current_user_is_the_veteran?(veteran: veteran, user: @current_user)
+        user_is_a_veteran_rep = current_user_is_a_veteran_representative?(@current_user)
+        raise ::Common::Exceptions::Unauthorized unless user_is_the_veteran || user_is_a_veteran_rep
 
         render json: { id: veteran.mpi.icn }
       end
@@ -58,6 +60,32 @@ module ClaimsApi
 
       def veteran_icn_found?(veteran)
         veteran&.mpi&.icn.present?
+      end
+
+      def current_user_is_a_veteran_representative?(user)
+        representative = ::Veteran::Service::Representative.find_by(
+          first_name: user.first_name,
+          last_name: user.last_name
+        )
+
+        representative.present?
+      end
+
+      def current_user_is_the_veteran?(user:, veteran:)
+        return false unless user.first_name.casecmp?(veteran.first_name)
+        return false unless user.last_name.casecmp?(veteran.last_name)
+        return false unless user.ssn == veteran.ssn
+        return false unless birth_dates_match?(user_birth_date: user.birth_date, veteran_birth_date: veteran.birth_date)
+
+        true
+      end
+
+      def birth_dates_match?(user_birth_date:, veteran_birth_date:)
+        validate_birthdate!(veteran_birth_date)
+        validate_birthdate!(user_birth_date)
+        Date.parse(veteran_birth_date) == Date.parse(user_birth_date)
+      rescue ArgumentError
+        false
       end
     end
   end
