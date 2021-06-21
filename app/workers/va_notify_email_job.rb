f94cfe315e87cb21b1886305c12bfe9e5322017e
Lihan Li <frankieteardrop@gmail.com>
2021-06-21 15:18:43 -0700

    add va notify email job

---
+++ app/workers/va_notify_email_job.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class VANotifyEmailJob
+  include Sidekiq::Worker
+  sidekiq_options expires_in: 1.day
+
+  def perform(email, template_id)
+    notify_client = VaNotify::Service.new(Settings.vanotify.services.va_gov.api_key)
+
+    notify_client.send_email(
+      email_address: email,
+      template_id: template_id
+    )
+  end
+end
