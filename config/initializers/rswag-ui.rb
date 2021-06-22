3299793ff4631432d8bbc84183381935e171ed20
Dan Hinze <dan.hinze@adhocteam.us>
2021-06-22 17:20:08 -0500

    Require rswag in ui initializer

--- config/initializers/rswag-ui.rb
+++ config/initializers/rswag-ui.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rswag'
+
 Rswag::Ui.configure do |c|
   c.swagger_endpoint '/services/claims/docs/v1/api', 'Claims API V1 Docs'
   c.swagger_endpoint '/services/claims/docs/v0/api', 'Claims API V0 Docs'
