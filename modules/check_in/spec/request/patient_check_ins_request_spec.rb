cb0250f5c7651eb3b531b2f01850c5d4657dd7f6
Dillo Raju <draju@governmentcio.com>
2021-06-23 10:05:29 -0400

    26267 basic module authorization - Calling requests must pass in the approved cookie_id to proceed

--- modules/check_in/spec/request/patient_check_ins_request_spec.rb
+++ modules/check_in/spec/request/patient_check_ins_request_spec.rb
@@ -4,16 +4,24 @@ require 'rails_helper'
 
 RSpec.describe 'PatientCheckIns', type: :request do
   describe 'GET `show`' do
+    before do
+      allow(Flipper).to receive(:enabled?).with('check_in_experience_enabled', anything).and_return(true)
+    end
+
     it 'returns an empty response' do
-      get '/check_in/v0/patient_check_ins/123aBc'
+      get '/check_in/v0/patient_check_ins/123aBc?cookie_id=23496jgsdf'
 
       expect(JSON.parse(response.body)).to eq({ 'data' => {} })
     end
   end
 
   describe 'POST `create`' do
+    before do
+      allow(Flipper).to receive(:enabled?).with('check_in_experience_enabled', anything).and_return(true)
+    end
+
     it 'returns a default json payload' do
-      post '/check_in/v0/patient_check_ins', params: { patient_check_ins: { id: '123aBc' } }
+      post '/check_in/v0/patient_check_ins', params: { patient_check_ins: { id: '123aBc' }, cookie_id: '23496jgsdf' }
 
       expect(JSON.parse(response.body)).to eq({ 'data' => { 'status' => 'checked-in' } })
     end
