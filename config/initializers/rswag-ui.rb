327c93a6f5581e616ead6bc15442da2d0ce0bfa5
Dan Hinze <dan.hinze@adhocteam.us>
2021-06-23 15:21:42 -0500

    API-7861 Add swagger-ui for claims endpoints (#7284)
    * Orginal rswag swagger-ui changes
    
    * Require rswag in ui initializer

---
+++ config/initializers/rswag-ui.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rswag'
+
+Rswag::Ui.configure do |c|
+  c.swagger_endpoint '/services/claims/docs/v1/api', 'Claims API V1 Docs'
+  c.swagger_endpoint '/services/claims/docs/v0/api', 'Claims API V0 Docs'
+end
