d4f452f6975a05c66e95cf367edac32129117f73
Jack Schuss <jackschuss@gmail.com>
2021-06-24 14:17:53 -0400

    abstract index action into subclasses for deprecation purposes

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/higher_level_reviews/contestable_issues_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/higher_level_reviews/contestable_issues_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class AppealsApi::V1::DecisionReviews::HigherLevelReviews::ContestableIssuesController < AppealsApi::V1::DecisionReviews::BaseContestableIssuesController # rubocop:disable Layout/LineLength
+
+  def index
+    #deprecate here
+    get_contestable_issues_from_caseflow
+    if caseflow_response_has_a_body_and_a_status?
+      render_response(caseflow_response)
+    else
+      render_unusable_response_error
+    end
+  end
+
   private
 
   def decision_review_type
