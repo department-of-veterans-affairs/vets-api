5d0f043db214cabac7e0b6ae06f315b5ea58ed2f
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-06-24 17:17:23 -0600

    fixed service history episode spec

--- modules/veteran_verification/spec/models/service_history_episode_spec.rb
+++ modules/veteran_verification/spec/models/service_history_episode_spec.rb
@@ -24,7 +24,7 @@ describe VeteranVerification::ServiceHistoryEpisode, skip_emis: true do
       VCR.use_cassette('emis/get_deployment_v2/valid') do
         VCR.use_cassette('emis/get_military_service_episodes_v2/valid_multiple_episodes') do
           result = described_class.for_user(user)
-          expect(result.length).to eq(1)
+          expect(result.length).to eq(2)
           expect(result[0][:first_name]).to eq('abraham')
           expect(result[0][:last_name]).to eq('lincoln')
           expect(result[0][:branch_of_service]).to eq('Army National Guard')
@@ -34,17 +34,17 @@ describe VeteranVerification::ServiceHistoryEpisode, skip_emis: true do
       end
     end
 
-    it 'returns service history and deploys when there are multiple episodes' do
+    it 'returns service history and deploys when there are multiple episodes and end date null' do
       VCR.use_cassette('emis/get_deployment_v2/valid') do
         VCR.use_cassette('emis/get_military_service_episodes_v2/valid_multiple_episodes_end_date_null') do
           result = described_class.for_user(user)
-          puts result.to_json
           expect(result.length).to eq(2)
           expect(result[0][:branch_of_service]).to eq('Army National Guard')
           expect(result[0][:pay_grade]).to eq('W04')
-          expect(result[0][:deployments]).length.to eq(0)
+          expect(result[0][:deployments].length).to eq(0)
           expect(result[1][:branch_of_service]).to eq('Army National Guard')
           expect(result[1][:pay_grade]).to eq('W04')
+          expect(result[1][:deployments].length).to eq(3)
         end
       end
     end
