41dad90e8cc238e21fe3dbd54346cf96df97f32f
Cris Shupp <cristopher.shupp@libertyits.com>
2021-06-24 17:20:31 -0400

    start of validations on webhook param

--- modules/vba_documents/app/controllers/vba_documents/v2/uploads_controller.rb
+++ modules/vba_documents/app/controllers/vba_documents/v2/uploads_controller.rb
@@ -6,6 +6,7 @@ require_dependency 'vba_documents/application_controller'
 require_dependency 'vba_documents/upload_error'
 require_dependency 'vba_documents/payload_manager'
 require_dependency 'vba_documents/upload_validator'
+require_dependency 'vba_documents/location_validator'
 require_dependency 'vba_documents/multipart_parser'
 require 'common/exceptions'
 
@@ -13,42 +14,28 @@ module VBADocuments
   module V2
     class UploadsController < ApplicationController
       include VBADocuments::UploadValidations
+      include VBADocuments::LoacationValidations
       skip_before_action(:authenticate)
       before_action :verify_settings, only: [:download]
 
-      def submit
-        upload_model = UploadFile.new
-        begin
-          upload_model.multipart.attach(io: StringIO.new(request.raw_post), filename: upload_model.guid)
-          upload_model.save!
-          parts = VBADocuments::MultipartParser.parse(StringIO.new(request.raw_post))
-          inspector = VBADocuments::PDFInspector.new(pdf: parts)
-          validate_parts(parts)
-          validate_metadata(parts[META_PART_NAME])
-          update_pdf_metadata(upload_model, inspector)
-          perfect_metadata(upload_model, parts, Time.zone.now)
-          VBADocuments::UploadProcessor.perform_async(upload_model.guid, caller: self.class.name)
-        rescue VBADocuments::UploadError => e
-          Rails.logger.warn("UploadError download_and_process for guid #{upload_model.guid}.", e)
-          upload_model.update(status: 'error', code: e.code, detail: e.detail)
-        rescue Seahorse::Client::NetworkingError => e
-          upload_model.update(status: 'error', code: 'DOC104', detail: e.message)
-        end
-        status = upload_model.status.eql?('error') ? 400 : 200
-        render json: upload_model,
-               serializer: VBADocuments::V2::UploadSerializer, status: status
-      end
-
       def create
-        submission = VBADocuments::UploadSubmission.create(
-            consumer_name: request.headers['X-Consumer-Username'],
-            consumer_id: request.headers['X-Consumer-ID']
-        )
-
+        load('./modules/vba_documents/lib/vba_documents/location_validator.rb') #allows changes (remove) todo
+        submission = nil
+        VBADocuments::UploadSubmission.transaction do
+          subscriptions = validate_subscription(params[:subscriptions]) if params[:subscriptions]
+          submission = VBADocuments::UploadSubmission.create(
+              consumer_name: request.headers['X-Consumer-Username'],
+              consumer_id: request.headers['X-Consumer-ID']
+          )
+          # greg_model.create_from_subscriptions(subscriptions, guid)
+        end
         render status: :accepted,
                json: submission,
-               serializer: VBADocuments::V1::UploadSerializer,
+               serializer: VBADocuments::V1::UploadSerializer, # todo v2 validator
                render_location: true
+      rescue => ex
+        # todo handle exception convert 500 error to 400 return appropriate json (returned from validator above)
+        raise ex
       end
 
       def show
@@ -78,6 +65,29 @@ module VBADocuments
         end
 
         File.delete(zip_file_name)
+      end
+
+      def submit
+        upload_model = UploadFile.new
+        begin
+          upload_model.multipart.attach(io: StringIO.new(request.raw_post), filename: upload_model.guid)
+          upload_model.save!
+          parts = VBADocuments::MultipartParser.parse(StringIO.new(request.raw_post))
+          inspector = VBADocuments::PDFInspector.new(pdf: parts)
+          validate_parts(parts)
+          validate_metadata(parts[META_PART_NAME])
+          update_pdf_metadata(upload_model, inspector)
+          perfect_metadata(upload_model, parts, Time.zone.now)
+          VBADocuments::UploadProcessor.perform_async(upload_model.guid, caller: self.class.name)
+        rescue VBADocuments::UploadError => e
+          Rails.logger.warn("UploadError download_and_process for guid #{upload_model.guid}.", e)
+          upload_model.update(status: 'error', code: e.code, detail: e.detail)
+        rescue Seahorse::Client::NetworkingError => e
+          upload_model.update(status: 'error', code: 'DOC104', detail: e.message)
+        end
+        status = upload_model.status.eql?('error') ? 400 : 200
+        render json: upload_model,
+               serializer: VBADocuments::V2::UploadSerializer, status: status
       end
 
       private
---
+++ modules/vba_documents/lib/vba_documents/location_validator.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+require 'json_schemer'
+require 'uri'
+
+module VBADocuments
+  module LoacationValidations
+    SUPPORTED_EVENTS = %w(gov.va.developer.benefits.status_change)
+
+    # perhaps we should consider a json validator?
+    # Validates a subscription request for an upload submission.  Returns an array representing the subscription
+    def validate_subscription(subscription)
+      subscription = subscription.read if subscription.respond_to? :read
+      subscription_array = JSON.parse(subscription)
+      unless subscription_array.is_a? Array
+        raise ArgumentError.new("Invalid Subscription! Subscription should be an Array!")
+      end
+      subscription_array.each do |e|
+        raise ArgumentError.new("Invalid Subscription! Subscription items should be Objects!") unless e.is_a? Hash
+        raise ArgumentError.new("Invalid Subscription! Subscription item needs a url!") unless e.has_key?('url')
+        validate_url(e['url'])
+        unless e.has_key?('events')
+          raise ArgumentError.new("Invalid Subscription! Subscription item needs an array of events!")
+        end
+        e['events'].each do |event|
+          unless SUPPORTED_EVENTS.include?(event)
+            raise ArgumentError.new("Invalid Subscription! event type #{event} is not supported!")
+          end
+        end
+      end
+      subscription_array
+    end
+
+    def validate_url(url)
+      uri = URI(url) # raises URI::InvalidURIError if URI doesn't parse
+      https = uri.scheme.eql? 'https'
+      if (!https && Settings.vba_documents.websockets.require_https)
+        raise ArgumentError.new("URL #{url} must be https!")
+      end
+    end
+
+  end
+end
+=begin
+load('./modules/vba_documents/lib/vba_documents/location_validator.rb')
+json = File.read('./modules/vba_documents/spec/fixtures/subscriptions.json')
+=end
