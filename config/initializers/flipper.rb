976955321dc46f7658e4982792cafc87419b6da2
Dillo Raju <draju@governmentcio.com>
2021-06-28 15:55:27 -0400

    Switching Flipper to use Rails cache

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -3,35 +3,40 @@
 require 'flipper'
 require 'flipper/adapters/active_record'
 require 'active_support/cache'
-# require 'flipper/adapters/redis_cache'
+require 'flipper/adapters/active_support_cache_store'
 require 'flipper/action_patch'
 require 'flipper/configuration_patch'
 require 'flipper/instrumentation/event_subscriber'
 
 FLIPPER_FEATURE_CONFIG = YAML.safe_load(File.read(Rails.root.join('config', 'features.yml')))
-FLIPPER_ACTOR_USER = 'user'
-FLIPPER_ACTOR_STRING = 'cookie_id'
 
-##
-# Flipper configuration
-#
 Flipper.configure do |config|
   config.default do
-    activerecord_adapter = Flipper::Adapters::ActiveRecord.new
-
-    if Rails.env.development? || Rails.env.test? || %w[development sandbox staging].include?(Settings.vsp_environment)
-      instrumented =
-        Flipper::Adapters::Instrumented.new(activerecord_adapter, instrumenter: ActiveSupport::Notifications)
-    else
-      cache = ActiveSupport::Cache::MemoryStore.new
-      cached_adapter = Flipper::Adapters::ActiveSupportCacheStore.new(activerecord_adapter, cache, expires_in: 1.minute)
-      instrumented = Flipper::Adapters::Instrumented.new(cached_adapter, instrumenter: ActiveSupport::Notifications)
-    end
-
+    adapter = Flipper::Adapters::ActiveRecord.new
+    cache = Rails.cache
+    adapter = Flipper::Adapters::ActiveSupportCacheStore.new(adapter, cache, expires_in: 5.minutes)
+    instrumented = Flipper::Adapters::Instrumented.new(adapter, instrumenter: ActiveSupport::Notifications)
     Flipper.new(instrumented, instrumenter: ActiveSupport::Notifications)
   end
 end
 
+# Modify Flipper::UI::Configuration to accept a custom view path.
+Flipper::UI::Configuration.prepend(FlipperExtensions::ConfigurationPatch)
+
+Flipper::UI.configure do |config|
+  config.custom_views_path = Rails.root.join('lib', 'flipper', 'views')
+end
+
+# A contrived example of how we might use a "group"
+# (a method that can be evaluated at runtime to determine feature status)
+#
+# Flipper.register(:first_name_is_hector) do |user|
+#   user.respond_to?(:first_name) && user.first_name == 'HECTOR'
+# end
+FLIPPER_ACTOR_USER = 'user'
+FLIPPER_ACTOR_STRING = 'cookie_id'
+
+Flipper::UI.configuration.feature_creation_enabled = false
 # Make sure that each feature we reference in code is present in the UI, as long as we have a Database already
 begin
   FLIPPER_FEATURE_CONFIG['features'].each do |feature, feature_config|
@@ -50,19 +55,6 @@ rescue
   nil
 end
 
-# A contrived example of how we might use a "group"
-# (a method that can be evaluated at runtime to determine feature status)
-#
-# Flipper.register(:first_name_is_hector) do |user|
-#   user.respond_to?(:first_name) && user.first_name == 'HECTOR'
-# end
-
-Flipper::UI.configuration.feature_creation_enabled = false
 # Modify Flipper::UI::Action to use custom views if they exist
 # and to add descriptions and types for features.
 Flipper::UI::Action.prepend(FlipperExtensions::ActionPatch)
-# Modify Flipper::UI::Configuration to accept a custom view path.
-Flipper::UI::Configuration.prepend(FlipperExtensions::ConfigurationPatch)
-Flipper::UI.configure do |config|
-  config.custom_views_path = Rails.root.join('lib', 'flipper', 'views')
-end
