3380b8a33b3222f5d28b819822c0237a3e702951
Dillo Raju <draju@governmentcio.com>
2021-06-28 12:48:42 -0400

    26057 chip api basic harness - Setup configuration - Setup the Chip API request functionality - Setup the Chip API service object - Setup routing and controllers - Setup basic testing

--- modules/check_in/spec/request/patient_check_ins_request_spec.rb
+++ modules/check_in/spec/request/patient_check_ins_request_spec.rb
@@ -3,27 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe 'PatientCheckIns', type: :request do
+  let(:faraday_response) { Faraday::Response.new }
+
   describe 'GET `show`' do
     before do
       allow(Flipper).to receive(:enabled?).with('check_in_experience_enabled', anything).and_return(true)
+      allow_any_instance_of(ChipApi::Request).to receive(:get).with('123aBc').and_return(faraday_response)
     end
 
     it 'returns an empty response' do
       get '/check_in/v0/patient_check_ins/123aBc?cookie_id=23496jgsdf'
 
-      expect(JSON.parse(response.body)).to eq({ 'data' => {} })
+      expect(JSON.parse(response.body)).to eq({ ':data' => nil })
     end
   end
 
   describe 'POST `create`' do
+    let(:post_params) { { params: { patient_check_ins: { id: '123aBc' } } } }
+
     before do
       allow(Flipper).to receive(:enabled?).with('check_in_experience_enabled', anything).and_return(true)
+      allow_any_instance_of(ChipApi::Request).to receive(:post)
+        .with({ 'id' => '123aBc' }).and_return(faraday_response)
     end
 
     it 'returns a default json payload' do
-      post '/check_in/v0/patient_check_ins', params: { patient_check_ins: { id: '123aBc' }, cookie_id: '23496jgsdf' }
+      post '/check_in/v0/patient_check_ins', post_params
 
-      expect(JSON.parse(response.body)).to eq({ 'data' => { 'status' => 'checked-in' } })
+      expect(JSON.parse(response.body)).to eq({ ':data' => nil })
     end
   end
 end
