b6c49c3c2e607c4016e9a7aef3e870314b2f13eb
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-06-28 14:21:14 -0500

    API-7793 - Consolidate VeteranIdentifier VeteranInfo custom validator logic

--- modules/claims_api/lib/claims_api/v2/params_validation/veteran_identifier/veteran_info.rb
+++ modules/claims_api/lib/claims_api/v2/params_validation/veteran_identifier/veteran_info.rb
@@ -1,33 +1,14 @@
 # frozen_string_literal: true
 
 require 'claims_api/v2/params_validation/base'
+require 'claims_api/v2/params_validation/veteran_identifier/veteran_info_validator'
 
 module ClaimsApi
   module V2
     module ParamsValidation
       module VeteranIdentifier
         class VeteranInfo < Base
-          class BirthdateValidator < ActiveModel::EachValidator
-            def validate_each(record, attribute, value)
-              if value.blank?
-                record.errors.add attribute, 'birthdate cannot be blank'
-                return
-              end
-
-              begin
-                date = Date.parse(value)
-              rescue ArgumentError
-                record.errors.add attribute, 'invalid date'
-                return
-              end
-
-              record.errors.add attribute, 'birthdate cannot be in the future' if date >= Time.zone.today
-            end
-          end
-
-          validates_presence_of :firstName, :lastName
-          validates :ssn, presence: true, format: { with: /\A\d{9}\z/ }
-          validates :birthdate, birthdate: true
+          validates_with VeteranInfoValidator
         end
       end
     end
