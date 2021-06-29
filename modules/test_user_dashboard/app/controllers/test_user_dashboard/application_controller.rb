2001a7992f418400e773e783e7c4257086f5175f
John Bramley <john.bramley@adhocteam.us>
2021-06-29 17:11:32 -0400

    [25292] TUD API Route Authentication (#7051)
    * TUD API dummy route auth added
    
    * comment out hardcoded password to pass brakeman
    
    * changes auth example to use async encryption
    
    * removes dummy frontend JWT generation
    
    * final authentication with broken testing
    
    * adds unit tests to tud authentication methods

--- modules/test_user_dashboard/app/controllers/test_user_dashboard/application_controller.rb
+++ modules/test_user_dashboard/app/controllers/test_user_dashboard/application_controller.rb
@@ -1,6 +1,33 @@
 # frozen_string_literal: true
 
+require 'sentry_logging'
+
 module TestUserDashboard
   class ApplicationController < ActionController::API
+    include SentryLogging
+    before_action :require_jwt
+
+    def require_jwt
+      token = request.headers['JWT']
+      pub_key = request.headers['PK']
+
+      head :forbidden unless valid_token(token, pub_key)
+    end
+
+    private
+
+    def valid_token(token, pub_key)
+      return false unless token && pub_key
+
+      rsa_public = OpenSSL::PKey::RSA.new(Base64.decode64(pub_key))
+      token.gsub!('Bearer ', '')
+      begin
+        JWT.decode token, rsa_public, true, { algorithm: 'RS256' }
+        return true
+      rescue JWT::DecodeError => e
+        log_message_to_sentry('Error decoding TUD JWT: ', :error, body: e.message)
+      end
+      false
+    end
   end
 end
