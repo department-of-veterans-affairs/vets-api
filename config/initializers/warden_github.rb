c98bf8dc7e2ef81bc9fbabbfc803987cc065ac66
Riley Anderson <5150r.d.anderson@gmail.com>
2021-06-30 11:05:04 -0600

    update setting name

--- config/initializers/warden_github.rb
+++ config/initializers/warden_github.rb
@@ -2,6 +2,6 @@
 
 Warden::GitHub::User.module_eval do
   def api
-    Octokit::Client.new(access_token: Settings.github.api_key)
+    Octokit::Client.new(access_token: Settings.sidekiq.github_api_key)
   end
 end
