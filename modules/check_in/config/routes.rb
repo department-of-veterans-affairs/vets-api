71579e4e33c018b359a8f96a004e88a8ef73013c
Dillo Raju <draju@governmentcio.com>
2021-06-30 19:22:33 -0400

    2685 providing explicit prefetch routes for safari

--- modules/check_in/config/routes.rb
+++ modules/check_in/config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 CheckIn::Engine.routes.draw do
+  match '/check_in/v0/*path', to: 'application#cors_preflight', via: [:options]
+
   namespace :v0, defaults: { format: :json } do
     resources :patient_check_ins, only: %i[show create]
   end
