1e1c81dfd1bf1e1d95e036516c508f1e88f3d2e1
Kevin Musiorski <kmusiorski@governmentcio.com>
2021-06-29 20:28:48 -0500

    rubocop

--- spec/workers/vre/create_ch31_submissions_report_spec.rb
+++ spec/workers/vre/create_ch31_submissions_report_spec.rb
@@ -4,9 +4,15 @@ require 'rails_helper'
 
 describe VRE::CreateCh31SubmissionsReport do
   describe '#get_claims_submitted_in_range' do
-    let!(:vre_claim1) { create :veteran_readiness_employment_claim, regional_office: '377 - San Diego', updated_at: 3.minutes.ago }
-    let!(:vre_claim2) { create :veteran_readiness_employment_claim, regional_office: '349 - Waco', updated_at: 2.minutes.ago }
-    let!(:vre_claim3) { create :veteran_readiness_employment_claim, regional_office: '351 - Muskogee', updated_at: 1.minutes.ago }
+    let!(:vre_claim1) do
+      create :veteran_readiness_employment_claim, regional_office: '377 - San Diego', updated_at: 3.minutes.ago
+    end
+    let!(:vre_claim2) do
+      create :veteran_readiness_employment_claim, regional_office: '349 - Waco', updated_at: 2.minutes.ago
+    end
+    let!(:vre_claim3) do
+      create :veteran_readiness_employment_claim, regional_office: '351 - Muskogee', updated_at: 1.minute.ago
+    end
 
     it 'sorts them by Regional Office' do
       expected = [vre_claim2.id, vre_claim3.id, vre_claim1.id]
