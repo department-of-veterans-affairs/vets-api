980c136225bf1488435cdfbfeab036951bc84688
Anna Carey <anna@adhocteam.us>
2021-06-30 10:55:28 -0400

    repl fix

--- rakelib/piilog_repl.rb
+++ rakelib/piilog_repl.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# run this with:  bin/rails runner rakelib/piilog_repl.rb
+# run this by starting a rails console and running: require_relative 'rakelib/piilog_repl
 
 # see "spec/rakelib/piilog_repl/piilog_helpers_spec.rb" for more examples of using
 # the PersonalInformationLogQueryBuilder
@@ -8,5 +8,3 @@
 require_relative 'piilog_repl/piilog_helpers'
 
 Q = PersonalInformationLogQueryBuilder
-
-pry
