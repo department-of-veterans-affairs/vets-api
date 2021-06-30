0dafead67eef8ed81f9f8d59139ed2aa8fbcdce4
Jack Schuss <jackschuss@gmail.com>
2021-06-30 14:41:35 -0400

    newline

--- modules/appeals_api/lib/appeals_api/sidekiq_retry_notifier.rb
+++ modules/appeals_api/lib/appeals_api/sidekiq_retry_notifier.rb
@@ -13,7 +13,7 @@ module AppealsApi
 
       def message_text(params)
         "
-        ENVIRONMENT: #{Settings.vsp_environment}
+        ENVIRONMENT: #{Settings.vsp_environment} \n
         The sidekiq job #{params['class']} has hit #{params['retry_count'] + 1} retries.
         \nError Type: #{params['error_class']} \n Error Message: \n #{params['error_message']} \n\n
 This job failed at: #{Time.zone.at(params['failed_at'])}, and #{retried_at(params['retried_at'])}
