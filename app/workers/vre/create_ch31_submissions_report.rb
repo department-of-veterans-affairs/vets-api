10c4e37a4d05415494e882e983eb42185c195bc0
Kevin Musiorski <1021154+RudyOnRails@users.noreply.github.com>
2021-07-01 08:47:38 -0500

    24945 change sorting of daily summary report email (#7320)
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/24945
    
    This PR alters the order (from created_at ASC using SQL to regionalOffice using sort_by since the form data is encrypted) of the Veteran Readiness Employment Claims that is provided to the Ch31 Submissions Report.

--- app/workers/vre/create_ch31_submissions_report.rb
+++ app/workers/vre/create_ch31_submissions_report.rb
@@ -5,6 +5,10 @@ module VRE
     require 'csv'
     include Sidekiq::Worker
 
+    def initialize
+      @time = Time.zone.now
+    end
+
     def updated_at_range
       (@time - 24.hours)..(@time - 1.second)
     end
@@ -12,13 +16,11 @@ module VRE
     def get_claims_submitted_in_range
       SavedClaim::VeteranReadinessEmploymentClaim.where(
         updated_at: updated_at_range
-      )
+      ).sort_by { |claim| claim.parsed_form['veteranInformation']['regionalOffice'] }
     end
 
     def perform
-      @time = Time.zone.now
       submitted_claims = get_claims_submitted_in_range
-
       Ch31SubmissionsReportMailer.build(submitted_claims).deliver_now
     end
   end
