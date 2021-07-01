40500545e03e0d925017f2097061fe196f979bbe
Laura Trager <laura.trager@adhocteam.us>
2021-07-01 12:03:18 -0400

    Add basic upstream error handling to controller

--- modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/legacy_appeals_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v1/decision_reviews/legacy_appeals_controller.rb
@@ -13,14 +13,41 @@ class AppealsApi::V1::DecisionReviews::LegacyAppealsController < AppealsApi::App
 
   def index
     get_legacy_appeals_from_caseflow
+
+    if caseflow_response_has_a_body_and_a_status?
+      render_response(caseflow_response)
+    else
+      render_unusable_response_error
+    end
   end
 
   private
 
-  attr_reader :caseflow_response
+  attr_reader :caseflow_response, :backend_service_exception
 
   def get_legacy_appeals_from_caseflow
     @caseflow_response = Caseflow::Service.new.get_legacy_appeals headers: headers
+
+  rescue Common::Exceptions::BackendServiceException => @backend_service_exception # rubocop:disable Naming/RescuedExceptionsVariableName
+    raise unless caseflow_returned_a_4xx?
+
+    @caseflow_response = caseflow_response_from_backend_service_exception
+  end
+
+  def caseflow_response_has_a_body_and_a_status?
+    caseflow_response.try(:status) && caseflow_response.try(:body).is_a?(Hash)
+  end
+
+  def caseflow_returned_a_4xx?
+    status = Integer backend_service_exception.original_status
+    status >= 400 && status < 500
+  end
+
+  def caseflow_response_from_backend_service_exception
+    Struct.new(:status, :body).new(
+        backend_service_exception.original_status,
+        backend_service_exception.original_body
+    )
   end
 
   def headers
