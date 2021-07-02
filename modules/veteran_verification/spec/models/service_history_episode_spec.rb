f82c936dcb87ef12da6294dd9c918093c0dc9ed6
lukemarkwordtlibertyits <65039481+lukemarkwordtlibertyits@users.noreply.github.com>
2021-07-02 13:16:17 -0600

    Created Check for service history null dates (#7300)
    * Created Check for service history null dates
    
    * new logic will order service history episodes from earliest to latest so spec needed to be changed
    
    * created test for end_date null
    
    * fixed service history episode spec

--- modules/veteran_verification/spec/models/service_history_episode_spec.rb
+++ modules/veteran_verification/spec/models/service_history_episode_spec.rb
@@ -25,9 +25,26 @@ describe VeteranVerification::ServiceHistoryEpisode, skip_emis: true do
         VCR.use_cassette('emis/get_military_service_episodes_v2/valid_multiple_episodes') do
           result = described_class.for_user(user)
           expect(result.length).to eq(2)
+          expect(result[0][:first_name]).to eq('abraham')
+          expect(result[0][:last_name]).to eq('lincoln')
           expect(result[0][:branch_of_service]).to eq('Army National Guard')
           expect(result[0][:pay_grade]).to eq('W04')
-          expect(result[1][:deployments][0][:location]).to eq('AX1')
+          expect(result[0][:deployments][0][:location]).to eq('AX1')
+        end
+      end
+    end
+
+    it 'returns service history and deploys when there are multiple episodes and end date null' do
+      VCR.use_cassette('emis/get_deployment_v2/valid') do
+        VCR.use_cassette('emis/get_military_service_episodes_v2/valid_multiple_episodes_end_date_null') do
+          result = described_class.for_user(user)
+          expect(result.length).to eq(2)
+          expect(result[0][:branch_of_service]).to eq('Army National Guard')
+          expect(result[0][:pay_grade]).to eq('W04')
+          expect(result[0][:deployments].length).to eq(0)
+          expect(result[1][:branch_of_service]).to eq('Army National Guard')
+          expect(result[1][:pay_grade]).to eq('W04')
+          expect(result[1][:deployments].length).to eq(3)
         end
       end
     end
