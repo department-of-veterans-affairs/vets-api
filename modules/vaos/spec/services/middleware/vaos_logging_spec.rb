19736f3a8136d5fb6ab36246c993b2b4bfce9cc5
Eric Boehs <eric@boehs.com>
2021-07-02 10:34:58 -0500

    Revert "Bump flipper gems to latest (#6910)" (#7348)
    This reverts commit e8d3edc1aeacdbb98e121d1706fe696f9cc2dc23.

--- modules/vaos/spec/services/middleware/vaos_logging_spec.rb
+++ modules/vaos/spec/services/middleware/vaos_logging_spec.rb
@@ -3,11 +3,6 @@
 require 'rails_helper'
 require_relative '../../support/fixture_helper'
 
-#
-# TODO: tests skipped to allow upgrading of flipper gem. The flipper upgrade changes the way StatsD is
-# initialized and no longer calls the Rails.logger when running in the test Rails environment.
-# Will rewrite these test under a new VAOS ticket.
-#
 describe VAOS::Middleware::VAOSLogging do
   subject(:client) do
     Faraday.new do |conn|
@@ -38,7 +33,7 @@ describe VAOS::Middleware::VAOSLogging do
   context 'with status successful' do
     let(:status) { 200 }
 
-    xit 'user service call logs a success' do
+    it 'user service call logs a success' do
       expect(Rails.logger).to receive(:info).with('[StatsD] increment api.vaos.va_mobile.response.total:1 #method:POST'\
         ' #url:/users/v2/session #http_status:')
       expect(Rails.logger).to receive(:info).with('VAOS service call succeeded!',
@@ -49,7 +44,7 @@ describe VAOS::Middleware::VAOSLogging do
       client.post(user_service_uri)
     end
 
-    xit 'other requests with X-Vamf-Jwt log a success' do
+    it 'other requests with X-Vamf-Jwt log a success' do
       expect(Rails.logger).to receive(:info).with('[StatsD] increment api.vaos.va_mobile.response.total:1 #method:GET '\
         '#url:/whatever #http_status:')
       expect(Rails.logger).to receive(:info).with('VAOS service call succeeded!',
@@ -60,7 +55,7 @@ describe VAOS::Middleware::VAOSLogging do
       client.get(all_other_uris, nil, { 'X-Vamf-Jwt' => sample_jwt })
     end
 
-    xit 'other requests with X-VAMF-JWT log a success' do
+    it 'other requests with X-VAMF-JWT log a success' do
       expect(Rails.logger).to receive(:info).with('[StatsD] increment api.vaos.va_mobile.response.total:1 #method:GET '\
         '#url:/user_service_refresh_uri #http_status:')
       expect(Rails.logger).to receive(:info).with('VAOS service call succeeded!',
