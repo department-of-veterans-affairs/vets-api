ad16eec3824e9fe26966ccbea5d239cca4e8be57
Kevin Cranmer <51764175+KevinCranmer@users.noreply.github.com>
2021-07-06 12:49:58 -0500

    Kevin.cranmer/api 3314 expired doc error (#7329)
    * Exploring upload_remover.rb
    
    * Throwing 404 error when trying to download file deleted from S3 already
    
    * Throwing 404 error from within download_raw_file. Also handling unknown guid. rspec test added
    
    * Lint fix
    
    * Updating rspec to handle missing upload file check
    
    * build test
    
    * build test
    
    * Build test
    
    * Swagger doc update
    
    * Swagger doc download endpoint to specify only testing environments
    
    * Build test
    
    * Build test
    
    * guid access consistency
    
    * guid access consistency

--- modules/vba_documents/spec/request/v1/uploads_request_spec.rb
+++ modules/vba_documents/spec/request/v1/uploads_request_spec.rb
@@ -192,9 +192,14 @@ RSpec.describe 'VBA Document Uploads Endpoint', type: :request, retry: 3 do
     it 'returns a 200 with content-type of zip' do
       objstore = instance_double(VBADocuments::ObjectStore)
       version = instance_double(Aws::S3::ObjectVersion)
+      bucket = instance_double(Aws::S3::Bucket)
+      obj = instance_double(Aws::S3::Object)
       allow(VBADocuments::ObjectStore).to receive(:new).and_return(objstore)
       allow(objstore).to receive(:first_version).and_return(version)
       allow(objstore).to receive(:download)
+      allow(objstore).to receive(:bucket).and_return(bucket)
+      allow(bucket).to receive(:object).and_return(obj)
+      allow(obj).to receive(:exists?).and_return(true)
       allow(version).to receive(:last_modified).and_return(DateTime.now.utc)
       allow(VBADocuments::MultipartParser).to receive(:parse) { valid_parts }
 
@@ -208,6 +213,24 @@ RSpec.describe 'VBA Document Uploads Endpoint', type: :request, retry: 3 do
       get "/services/vba_documents/v1/uploads/#{upload.guid}/download"
       expect(response.status).to eq(200)
       expect(response.headers['Content-Type']).to eq('application/zip')
+    end
+
+    it 'returns a 404 if deleted from s3' do
+      objstore = instance_double(VBADocuments::ObjectStore)
+      version = instance_double(Aws::S3::ObjectVersion)
+      bucket = instance_double(Aws::S3::Bucket)
+      obj = instance_double(Aws::S3::Object)
+      allow(VBADocuments::ObjectStore).to receive(:new).and_return(objstore)
+      allow(objstore).to receive(:first_version).and_return(version)
+      allow(objstore).to receive(:download)
+      allow(objstore).to receive(:bucket).and_return(bucket)
+      allow(bucket).to receive(:object).and_return(obj)
+      allow(obj).to receive(:exists?).and_return(false)
+      allow(version).to receive(:last_modified).and_return(DateTime.now.utc)
+      allow(VBADocuments::MultipartParser).to receive(:parse) { valid_parts }
+
+      get "/services/vba_documents/v1/uploads/#{upload.guid}/download"
+      expect(response.status).to eq(404)
     end
   end
 end
