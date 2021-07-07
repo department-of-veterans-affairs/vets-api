0ece13a683a851e74868d6ca0e1a1223bd860c02
Riley Anderson <5150r.d.anderson@gmail.com>
2021-07-07 10:24:32 -0600

    add Rack::Session::Cookie to config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,9 +10,6 @@ require 'sidekiq/set_request_attributes'
 require 'sidekiq/web'
 
 Rails.application.reloader.to_prepare do
-  # Turn off Sidekiq's sessions, which overwrite the main Rails app's session
-  # after the first request
-  Sidekiq::Web.disable(:sessions)
   Sidekiq::Enterprise.unique! if Rails.env.production?
 
   Sidekiq.configure_server do |config|
