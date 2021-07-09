1a6b454f0c750cfd8cabf893fc5c7b5aaa82bfdd
Riley Anderson <5150r.d.anderson@gmail.com>
2021-07-09 09:29:16 -0600

    use cookie_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,5 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :active_record_store, key: '_vets_api_session', expire_after: 1.month
+Rails.application.config.session_store :cookie_store, key: '_vets_api_session', expire_after: 1.month
 Rails.application.config.middleware.use Rails.application.config.session_store, Rails.application.config.session_options
