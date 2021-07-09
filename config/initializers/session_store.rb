c0a8347db4dcb88d3cf00d3e33b00c48cf9f7011
Riley Anderson <5150r.d.anderson@gmail.com>
2021-07-08 19:38:41 -0600

    add expiration to cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,6 +3,6 @@
 # Be sure to restart your server when you modify this file.
 require 'sidekiq/web'
 
-Rails.application.config.session_store :active_record_store, key: '_vets_api_session'
+Rails.application.config.session_store :active_record_store, key: '_vets_api_session', expire_after: 1.month
 
 Sidekiq::Web.set :sessions, false
