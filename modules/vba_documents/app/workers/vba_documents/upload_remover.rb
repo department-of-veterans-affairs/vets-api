d25feddf6000e939927d6b592cae009788ca7efe
Kevin Cranmer <51764175+KevinCranmer@users.noreply.github.com>
2021-07-09 08:56:41 -0500

    Kevin.cranmer/api 8294 s3 file remover bug (#7386)
    * Bug fix, working on rspec
    
    * rspec test added
    
    * lint fix

--- modules/vba_documents/app/workers/vba_documents/upload_remover.rb
+++ modules/vba_documents/app/workers/vba_documents/upload_remover.rb
@@ -20,7 +20,7 @@ module VBADocuments
 
       VBADocuments::UploadSubmission.where(REMOVAL_QUERY, EXPIRATION_TIME.ago).find_each do |upload|
         Rails.logger.info('VBADocuments: Cleaning up s3: ' + upload.inspect)
-        break unless store.object(upload.guid).exists?
+        next unless store.object(upload.guid).exists?
 
         store.delete(upload.guid)
         upload.update(s3_deleted: true)
