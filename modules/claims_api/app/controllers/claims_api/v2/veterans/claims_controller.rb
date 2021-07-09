860bb34d320e33986867ada5827c87c3b5e459f6
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-07-09 11:50:40 -0500

    API-8394 - Invoke BGS 'findClaimsDetailsByParticipantId" logic

--- modules/claims_api/app/controllers/claims_api/v2/veterans/claims_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v2/veterans/claims_controller.rb
@@ -5,20 +5,43 @@ module ClaimsApi
     module Veterans
       class ClaimsController < ClaimsApi::V2::ApplicationController
         def index
-          vet = ClaimsApi::Veteran.new(
-            mhv_icn: params[:veteranId],
-            loa: @current_user.loa
-          )
-          vet.mpi
+          veteran = build_veteran(veteran_id: params[:veteranId], loa: @current_user.loa)
+          detail  = 'Veteran not found'
+          raise ::Common::Exceptions::ResourceNotFound.new(detail: detail) unless veteran_found?(veteran: veteran)
 
-          service = BGS::Services.new(
-            external_uid: vet.participant_id,
-            external_key: vet.participant_id
-          )
+          service   = bgs_service(veteran_participant_id: veteran.participant_id)
+          params    = { participant_id: veteran.participant_id }
+          response  = service.benefit_claims.find_claims_details_by_participant_id(params)
+          claims    = transform_response(response: response)
 
-          response = service.benefit_claims.find_claims_details_by_participant_id(participant_id: vet.participant_id)
+          render json: claims
+        end
 
-          render json: response
+        private
+
+        def build_veteran(veteran_id:, loa:)
+          veteran = ClaimsApi::Veteran.new(mhv_icn: veteran_id, loa: loa)
+          veteran.mpi
+          veteran
+        end
+
+        def veteran_found?(veteran:)
+          veteran.participant_id.present?
+        end
+
+        def bgs_service(veteran_participant_id:)
+          BGS::Services.new(external_uid: veteran_participant_id, external_key: veteran_participant_id)
+        end
+
+        def transform_response(response:)
+          return [] unless response.key?(:bnft_claim_detail)
+
+          response[:bnft_claim_detail].map do |claim|
+            {
+              id: claim[:bnft_claim_id],
+              type: claim[:bnft_claim_type_nm]
+            }
+          end
         end
       end
     end
