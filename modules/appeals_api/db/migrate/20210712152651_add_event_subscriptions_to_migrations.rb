3cc83a23715f50e0d7821f563a110a790a875367
Jack Schuss <jackschuss@gmail.com>
2021-07-12 11:33:57 -0400

    add event subscription to migration

--- modules/appeals_api/db/migrate/20210712152651_add_event_subscriptions_to_migrations.rb
+++ modules/appeals_api/db/migrate/20210712152651_add_event_subscriptions_to_migrations.rb
@@ -2,6 +2,6 @@ class AddEventSubscriptionsToMigrations < ActiveRecord::Migration[6.1]
   def change
     AppealsApi::Events::Handler.subscribe(:hlr_status_updated, 'AppealsApi::Events::StatusUpdated')
     AppealsApi::Events::Handler.subscribe(:nod_status_updated, 'AppealsApi::Events::StatusUpdated')
-    AppealsApi::Events::Handler.subscribe(:hlr_received, 'AppealsApi::Events::AppealReceived')
+    AppealsApi::Events::Handler.subscribe(:higher_level_review, 'AppealsApi::Events::AppealReceived')
   end
 end
