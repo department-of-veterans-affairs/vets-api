9e2a8dce9777ec76232b24e29bf9d3b121eb8a06
Jack Schuss <jackschuss@gmail.com>
2021-07-12 11:37:26 -0400

    remove initializer in favor of migrations

--- config/initializers/event_subscriptions.rb
+++
@@ -1,3 +0,0 @@
-# this file is necessary for the namespaced module initializer to function
-ActiveRecord::Base.connected? && ActiveRecord::Base.connection.table_exists?('appeals_api_event_subscriptions')
-
--- modules/appeals_api/config/initializers/event_subscriptions.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-if ActiveRecord::Base.connection.nil? && ActiveRecord::Base.connection.table_exists?('appeals_api_event_subscriptions')
-  AppealsApi::Events::Handler.subscribe(:hlr_status_updated, 'AppealsApi::Events::StatusUpdated')
-  AppealsApi::Events::Handler.subscribe(:nod_status_updated, 'AppealsApi::Events::StatusUpdated')
-end
-
