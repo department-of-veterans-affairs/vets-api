518d375c0ce2a64e54488b27a87ffd37e433f978
KevinCranmer <cranmerkevin39@gmail.com>
2021-07-13 16:12:56 -0500

    Serializer keep location attr and not hard code api name

--- modules/vba_documents/app/serializers/vba_documents/v2/upload_serializer.rb
+++ modules/vba_documents/app/serializers/vba_documents/v2/upload_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#require './lib/webhooks/utilities'
+require './lib/webhooks/utilities'
 module VBADocuments
   module V2
     class UploadSerializer < VBADocuments::UploadSerializer
@@ -12,12 +12,12 @@ module VBADocuments
 
       def attributes(fields)
         attrs = super
-        attrs.delete(:location)
+        attrs.delete(:location) if Settings.vba_documents.v2_upload_endpoint_enabled
         attrs
       end
 
       def observing
-        @observers = WebhookSubscription.get_observers_by_guid(api_name: 'PLAY_API',
+        @observers = WebhookSubscription.get_observers_by_guid(api_name: Webhooks::Utilities.event_to_api_name['gov.va.developer.benefits-intake.status_change'],
                                                               consumer_id: object.consumer_id,
                                                               api_guid: object.guid)
         @observers.any?
