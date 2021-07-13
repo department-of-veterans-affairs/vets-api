0f01dce96259712ebbb5c9e2e1519a635ca4b948
Jack Schuss <jackschuss@gmail.com>
2021-07-13 17:34:05 -0400

    add data migration rake task

---
+++ modules/appeals_api/lib/appeals_api/data_migrations/event_subscription_subscriber.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module AppealsApi
+  module DataMigrations
+    module EventSubscriptionSubscriber
+      def self.run
+        AppealsApi::Events::Handler.subscribe(:hlr_status_updated, 'AppealsApi::Events::StatusUpdated')
+        AppealsApi::Events::Handler.subscribe(:nod_status_updated, 'AppealsApi::Events::StatusUpdated')
+        AppealsApi::Events::Handler.subscribe(:hlr_received, 'AppealsApi::Events::AppealReceived')
+      end
+    end
+  end
+end
