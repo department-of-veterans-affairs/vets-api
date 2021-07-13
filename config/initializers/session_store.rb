b43a1ed800345ea4dbf16f017fde9c6d148ff208
Riley Anderson <5150r.d.anderson@gmail.com>
2021-07-13 08:18:22 -0600

    try adding back active record session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_vets_api_session', expire_after: 1.month
+Rails.application.config.session_store :active_record_store, key: '_vets_api_session', expire_after: 1.month
