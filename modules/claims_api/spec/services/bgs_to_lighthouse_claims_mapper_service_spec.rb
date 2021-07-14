c91bbcbf9b327051f5e609735123a5e8066576d6
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-07-14 13:47:41 -0500

    API-8394 - Add more testing

--- modules/claims_api/spec/services/bgs_to_lighthouse_claims_mapper_service_spec.rb
+++ modules/claims_api/spec/services/bgs_to_lighthouse_claims_mapper_service_spec.rb
@@ -4,44 +4,128 @@ require 'rails_helper'
 
 describe ClaimsApi::BGSToLighthouseClaimsMapperService do
   describe 'process' do
-    context 'mapping claims results' do
-      context 'when BGS and Lighthouse claims exist' do
+    context 'when BGS and Lighthouse claims exist' do
+      context 'when a BGS claim is also in the Lighthouse collection' do
         let(:bgs_claims) do
           {
             bnft_claim_detail: [
               {
                 bnft_claim_id: '111111111',
                 bnft_claim_type_nm: 'Appeals Control'
-              },
-              {
-                bnft_claim_id: '222222222',
-                bnft_claim_type_nm: 'Claim for Increase'
-              },
-              {
-                bnft_claim_id: '333333333',
-                bnft_claim_type_nm: 'Rating Control'
               }
             ]
           }
         end
         let(:lighthouse_claims) do
           [
-            OpenStruct.new(id: '0958d973-36fb-43ef-8801-2718bd33c825', evss_id: '111111111'),
-            OpenStruct.new(id: '16219cf1-0b81-45b1-b07e-ccb01e591164', evss_id: '2'),
-            OpenStruct.new(id: '24aa5334-d15d-45f6-a331-1be909494de3', evss_id: '3')
+            OpenStruct.new(id: '0958d973-36fb-43ef-8801-2718bd33c825', evss_id: '111111111', claim_type: 'Compensation')
           ]
         end
 
-        it 'returns a combined collection with appropriate identifiers' do
+        it 'returns a collection that contains the claim with the Lighthouse id and BGS type' do
           claims = ClaimsApi::BGSToLighthouseClaimsMapperService.process(
             bgs_claims: bgs_claims, internal_claims: lighthouse_claims
           )
 
-          expect(claims).to be_an_instance_of(Array)
-          expect(claims.count).to eq(5) # 3 from bgs collection and 2 from lighthouse collection
-          expect(claims.any? { |e| e[:id] == lighthouse_claims.first.id }).to be true
-          expect(claims.any? { |e| e[:id] == bgs_claims[:bnft_claim_detail].first[:bnft_claim_id] }).to be false
+          expect(claims.count).to eq(1)
+          expect(claims.first[:id]).to eq('0958d973-36fb-43ef-8801-2718bd33c825')
+          expect(claims.first[:type]).to eq('Appeals Control')
+          expect(claims.any? { |claim| claim[:id] == '111111111' }).to be false
         end
+      end
+
+      context 'when a BGS claim is NOT in the Lighthouse collection' do
+        let(:bgs_claims) do
+          {
+            bnft_claim_detail: [
+              {
+                bnft_claim_id: '111111111',
+                bnft_claim_type_nm: 'Appeals Control'
+              }
+            ]
+          }
+        end
+        let(:lighthouse_claims) do
+          [
+            OpenStruct.new(id: '0958d973-36fb-43ef-8801-2718bd33c825', evss_id: '1', claim_type: 'Compensation')
+          ]
+        end
+
+        it 'returns a collection that contains both claims' do
+          claims = ClaimsApi::BGSToLighthouseClaimsMapperService.process(
+            bgs_claims: bgs_claims, internal_claims: lighthouse_claims
+          )
+
+          expect(claims.count).to eq(2)
+          expect(claims.any? { |claim| claim[:id] == '111111111' }).to be true
+          expect(claims.any? { |claim| claim[:id] == '0958d973-36fb-43ef-8801-2718bd33c825' }).to be true
+        end
+      end
+    end
+
+    context 'when only BGS claims exist' do
+      let(:bgs_claims) do
+        {
+          bnft_claim_detail: [
+            {
+              bnft_claim_id: '111111111',
+              bnft_claim_type_nm: 'Appeals Control'
+            }
+          ]
+        }
+      end
+      let(:lighthouse_claims) { [] }
+
+      it 'returns a collection that contains the BGS claims' do
+        claims = ClaimsApi::BGSToLighthouseClaimsMapperService.process(
+          bgs_claims: bgs_claims, internal_claims: lighthouse_claims
+        )
+
+        expect(claims.count).to eq(1)
+        expect(claims.first[:id]).to eq('111111111')
+        expect(claims.first[:type]).to eq('Appeals Control')
+      end
+    end
+
+    context 'when only Lighthouse claims exist' do
+      let(:bgs_claims) do
+        {
+          bnft_claim_detail: []
+        }
+      end
+
+      let(:lighthouse_claims) do
+        [
+          OpenStruct.new(id: '0958d973-36fb-43ef-8801-2718bd33c825', evss_id: '1', claim_type: 'Compensation')
+        ]
+      end
+
+      it 'returns a collection that contains the Lighthouse claims' do
+        claims = ClaimsApi::BGSToLighthouseClaimsMapperService.process(
+          bgs_claims: bgs_claims, internal_claims: lighthouse_claims
+        )
+
+        expect(claims.count).to eq(1)
+        expect(claims.first[:id]).to eq('0958d973-36fb-43ef-8801-2718bd33c825')
+        expect(claims.first[:type]).to eq('Compensation')
+      end
+    end
+
+    context 'when no Lighthouse claims exist and no BGS claims exist' do
+      let(:bgs_claims) do
+        {
+          bnft_claim_detail: []
+        }
+      end
+
+      let(:lighthouse_claims) { [] }
+
+      it 'returns an empty collection' do
+        claims = ClaimsApi::BGSToLighthouseClaimsMapperService.process(
+          bgs_claims: bgs_claims, internal_claims: lighthouse_claims
+        )
+
+        expect(claims.count).to eq(0)
       end
     end
   end
