db62be97ffbde5cf7815d5e1990e858fefb24079
Riley Anderson <5150r.d.anderson@gmail.com>
2021-07-13 21:23:29 -0600

    add back old cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :active_record_store, key: '_vets_api_session', expires: 1.month
-Rails.application.config.middleware.use Rails.application.config.session_store, Rails.application.config.session_options
+Rails.application.config.session_store :cookie_store, key: '_vets_api_session'
