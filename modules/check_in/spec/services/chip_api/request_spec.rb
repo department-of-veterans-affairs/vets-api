4a6c56f24fff4e2f593895b06723d9c8e45a0ad0
Dillo Raju <draju@governmentcio.com>
2021-07-14 14:36:40 -0400

    26899 CHIP API auth and integration - Setup the signed claims token for CHIP - Setup the Session handler for CHIP - Setup the Token object for data exchange between CHIP and vets-api - Setup the Redis handler for adding and retrieving tokens objects - Setup the Redis handler for adding and retrieving tokens objects - Setup the claims token configuration - Wire up the CHIP API authentication code to the main Check-in

--- modules/check_in/spec/services/chip_api/request_spec.rb
+++ modules/check_in/spec/services/chip_api/request_spec.rb
@@ -12,19 +12,135 @@ describe ChipApi::Request do
   end
 
   describe '#get' do
-    it 'returns a Faraday::Response' do
-      allow_any_instance_of(Faraday::Connection).to receive(:get)
-        .with('/appointments/123aBc').and_return(Faraday::Response.new)
+    let(:opts) do
+      {
+        path: '/dev/appointments/123abc',
+        access_token: 'abc123'
+      }
+    end
+    let(:conn) { double('Faraday::Connection') }
 
-      expect(subject.build.get('123aBc')).to be_a(Faraday::Response)
+    it 'connection is called with get' do
+      allow_any_instance_of(Faraday::Connection).to receive(:get).with(anything).and_return(anything)
+
+      expect_any_instance_of(Faraday::Connection).to receive(:get)
+        .with('/dev/appointments/123abc').once
+
+      subject.build.get(opts)
     end
   end
 
   describe '#post' do
-    it 'returns a Faraday::Response' do
-      allow_any_instance_of(Faraday::Connection).to receive(:post).and_return(Faraday::Response.new)
+    let(:opts) do
+      {
+        path: '/dev/actions/check-in/789',
+        access_token: 'abc123'
+      }
+    end
 
-      expect(subject.build.post('')).to be_an_instance_of(Faraday::Response)
+    it 'connection is called with post' do
+      allow_any_instance_of(Faraday::Connection).to receive(:post).with(anything).and_return(anything)
+
+      expect_any_instance_of(Faraday::Connection).to receive(:post)
+        .with('/dev/actions/check-in/789').once
+
+      subject.build.post(opts)
+    end
+  end
+
+  describe '#connection' do
+    let(:stubs) { Faraday::Adapter::Test::Stubs.new }
+    let(:conn) { Faraday.new { |b| b.adapter(:test, stubs) } }
+    let(:opts) do
+      {
+        path: 'dev/appointments/123',
+        access_token: 'abc123'
+      }
+    end
+
+    after do
+      Faraday.default_connection = nil
+    end
+
+    it 'GET has headers' do
+      stubs.get('/dev/appointments/123') do |_env|
+        [
+          200,
+          { 'Content-Type': 'application/json' },
+          '{"check-in": "foo"}'
+        ]
+      end
+
+      allow_any_instance_of(subject).to receive(:connection).and_return(conn)
+
+      expect(subject.build.get(opts).env.request_headers)
+        .to eq({ 'x-apigw-api-id' => '2dcdrrn5zc', 'Authorization' => 'Bearer abc123' })
+
+      stubs.verify_stubbed_calls
+    end
+
+    context 'with access_token' do
+      let(:opts) do
+        {
+          path: 'dev/actions/check-in/789',
+          access_token: 'abc123'
+        }
+      end
+
+      it 'POST has bearer headers' do
+        stubs.post('/dev/actions/check-in/789') do |_env|
+          [
+            200,
+            { 'Content-Type': 'application/json' },
+            '{"status": "success"}'
+          ]
+        end
+
+        allow_any_instance_of(subject).to receive(:connection).and_return(conn)
+
+        expect(subject.build.post(opts).env.request_headers)
+          .to eq({ 'x-apigw-api-id' => '2dcdrrn5zc', 'Authorization' => 'Bearer abc123', 'Content-Length' => '0' })
+
+        stubs.verify_stubbed_calls
+      end
+    end
+
+    context 'with claims_token' do
+      let(:opts) do
+        {
+          path: 'dev/token',
+          claims_token: 'efgh5678'
+        }
+      end
+
+      it 'POST has basic headers' do
+        stubs.post('dev/token') do |_env|
+          [
+            200,
+            { 'Content-Type': 'application/json' },
+            '{"token": "7896"}'
+          ]
+        end
+
+        allow_any_instance_of(subject).to receive(:connection).and_return(conn)
+
+        expect(subject.build.post(opts).env.request_headers)
+          .to eq({ 'x-apigw-api-id' => '2dcdrrn5zc', 'Authorization' => 'Basic efgh5678', 'Content-Length' => '0' })
+
+        stubs.verify_stubbed_calls
+      end
+    end
+  end
+
+  describe '#headers' do
+    it 'has default headers' do
+      expect(subject.build.headers).to eq({ 'x-apigw-api-id' => '2dcdrrn5zc' })
+    end
+  end
+
+  describe '#url' do
+    it 'has default headers' do
+      expect(subject.build.url).to eq('https://vpce-06399548ef94bdb41-lk4qp2nd.execute-api.us-gov-west-1.vpce.amazonaws.com')
     end
   end
 end
--- modules/check_in/spec/services/chip_api/service_spec.rb
+++ modules/check_in/spec/services/chip_api/service_spec.rb
@@ -5,8 +5,6 @@ require 'rails_helper'
 describe ChipApi::Service do
   subject { described_class }
 
-  let(:faraday_response) { Faraday::Response.new }
-
   describe '.build' do
     it 'returns an instance of Service' do
       expect(subject.build).to be_an_instance_of(ChipApi::Service)
@@ -17,42 +15,50 @@ describe ChipApi::Service do
     Timecop.freeze(Time.zone.now)
 
     let(:id) { '123abc' }
-    let(:resp) do
+    let(:opts) do
       {
-        data: {
-          uuid: '123abc',
-          appointment_time: Time.zone.now.to_s,
-          facility_name: 'Acme VA',
-          clinic_name: 'Green Team Clinic1',
-          clinic_phone: '555-555-5555'
-        }
+        path: '/dev/appointments/123abc',
+        access_token: 'abc123'
       }
     end
+    let(:resp) do
+      {
+        uuid: '123abc',
+        appointment_time: Time.zone.now.to_s,
+        facility_name: 'Acme VA',
+        clinic_name: 'Green Team Clinic1',
+        clinic_phone: '555-555-5555'
+      }
+    end
+    let(:faraday_response) { Faraday::Response.new(body: resp.to_json) }
 
     it 'returns a Faraday::Response' do
-      allow_any_instance_of(ChipApi::Request).to receive(:get).with(id).and_return(faraday_response)
+      allow_any_instance_of(ChipApi::Session).to receive(:retrieve).and_return('abc123')
+      allow_any_instance_of(ChipApi::Request).to receive(:get).with(opts).and_return(faraday_response)
 
-      expect(subject.build.get_check_in(id)).to eq(resp)
+      expect(subject.build.get_check_in(id)).to eq({ data: Oj.load(faraday_response.body) })
     end
 
     Timecop.return
   end
 
   describe '#create_check_in' do
-    let(:data) do
+    let(:opts) do
       {
-        id: 'abc123',
-        check_in_data: {}
+        path: '/dev/actions/check-in/789',
+        access_token: 'abc123'
       }
     end
     let(:resp) do
-      { data: { check_in_status: 'completed' } }
+      { check_in_status: 'completed' }.to_json
     end
+    let(:faraday_response) { Faraday::Response.new(body: resp.to_json) }
 
     it 'returns a Faraday::Response' do
-      allow_any_instance_of(ChipApi::Request).to receive(:post).with(data).and_return(faraday_response)
+      allow_any_instance_of(ChipApi::Session).to receive(:retrieve).and_return('abc123')
+      allow_any_instance_of(ChipApi::Request).to receive(:post).with(opts).and_return(faraday_response)
 
-      expect(subject.build.create_check_in(data)).to eq(resp)
+      expect(subject.build.create_check_in('789')).to eq({ data: Oj.load(faraday_response.body) })
     end
   end
 end
