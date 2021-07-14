d01347ef2303497b45b2fa854bcef5be7648af9a
Riley Anderson <riley.anderson@oddball.io>
2021-07-14 11:12:05 -0600

    Fix sessions on Sidekiq web (#7376)
    * use rack session on Sidekiq web
    
    * add back sessions false sidekiq
    
    * add back ActionDispatch::Cookies
    
    * remove rack config from application.rb
    
    * just set sessions to false
    
    * try middleware config
    
    * add expiration to session cookie
    
    * use rails session settings
    
    * use active_record_session_store and try removig api_session cookie
    
    * add expiration to cookie
    
    * add sidekiq cookie settings
    
    * try sidekiq master
    
    * add test conditional
    
    * add middleware config
    
    * remove sidekiq cookie config
    
    * update sidekiq
    
    * use cookie_store
    
    * add back rack session cookie
    
    * use active record store
    
    * add back session store gem
    
    * remove rack session cookie
    
    * add warden user to session
    
    * add redirect
    
    * just return success
    
    * wrap in conditional
    
    * add back api_session
    
    * switch back to cookie_store
    
    * remove config
    
    * try adding back active record session
    
    * add redirect
    
    * remove session set
    
    * set expiration on cookie
    
    * skip github callback path
    
    * add back session store cookie
    
    * add expiration
    
    * set user in session
    
    * add back old cookies
    
    * remove migrations
    
    * update spec and throws
    
    * cleanup
    
    * cleanup
    
    * remove gem

--- config/initializers/warden_github.rb
+++ config/initializers/warden_github.rb
@@ -5,3 +5,23 @@ Warden::GitHub::User.module_eval do
     Octokit::Client.new(access_token: Settings.sidekiq.github_api_key)
   end
 end
+
+Warden::GitHub::Strategy.module_eval do
+  def authenticate!
+    if session[:user].present?
+      success!(session[:user])
+      redirect!(request.url)
+    elsif in_flow?
+      continue_flow!
+    else
+      begin_flow!
+    end
+  end
+
+  def finalize_flow!
+    session[:user] = load_user
+    redirect!(custom_session['return_to'])
+    teardown_flow
+    throw(:warden)
+  end
+end
