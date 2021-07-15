fd70bad23933d5b09081790efcbb778d75b4f3c8
michelpmcdonald <michel.mcdonald@adhocteam>
2021-07-15 15:57:39 -0400

    Remove attachment type from test

--- spec/models/preneeds/preneed_attachment_hash_spec.rb
+++ spec/models/preneeds/preneed_attachment_hash_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe Preneeds::PreneedAttachmentHash do
     it 'converts to Preneed::Attachment' do
       attachment = preneed_attachment_hash.to_attachment
 
-      expect(attachment.attachment_type.attachment_type_id).to eq(1)
       expect(attachment.file.filename).to eq(preneed_attachment_hash.get_file.filename)
       expect(attachment.name).to eq(preneed_attachment_hash.name)
     end
