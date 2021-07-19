e822ec9a39a7e2aad8779c768b909d63a0f50405
Lance Sanchez <DarkBushido@users.noreply.github.com>
2021-07-19 14:47:40 -0500

    Linting

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -16,7 +16,8 @@ Rails.application.reloader.to_prepare do
       activerecord_adapter = Flipper::Adapters::ActiveRecord.new
       cache = Rails.cache
       # Flipper settings will be stored in postgres and cached in memory for 1 minute in production/staging
-      cached_adapter = Flipper::Adapters::ActiveSupportCacheStore.new(activerecord_adapter, cache, expires_in: 5.minute)
+      cached_adapter = Flipper::Adapters::ActiveSupportCacheStore.new(activerecord_adapter, cache,
+                                                                      expires_in: 5.minutes)
       instrumented = Flipper::Adapters::Instrumented.new(cached_adapter, instrumenter: ActiveSupport::Notifications)
       # pass adapter to handy DSL instance
       Flipper.new(instrumented, instrumenter: ActiveSupport::Notifications)
