acc3af586478ffca600d016630fedbd1e0a02820
Mike <michelpmcdonald@gmail.com>
2021-07-20 16:53:36 -0400

    remove unused preneeds/burial_form new route (#7427)
    * remove unused preneeds/burial_form new route
    
    * restore burial_forms create route
    
    Co-authored-by: michelpmcdonald <michel.mcdonald@adhocteam>

--- config/routes.rb
+++ config/routes.rb
@@ -220,7 +220,7 @@ Rails.application.routes.draw do
       resources :cemeteries, only: :index, defaults: { format: :json }
       resources :discharge_types, only: :index, defaults: { format: :json }
       resources :military_ranks, only: :index, defaults: { format: :json }
-      resources :burial_forms, only: %i[new create], defaults: { format: :json }
+      resources :burial_forms, only: :create, defaults: { format: :json }
       resources :preneed_attachments, only: :create
     end
 
