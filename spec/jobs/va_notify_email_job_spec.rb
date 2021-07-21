57c1ef59a56bfa61622a8cf75e54f94d4677c427
Lihan Li <frankieteardrop@gmail.com>
2021-07-20 17:44:42 -0700

    use test api key in vanotify email job spec

--- spec/jobs/va_notify_email_job_spec.rb
+++ spec/jobs/va_notify_email_job_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe VANotifyEmailJob, type: :model do
   let(:email) { 'user@example.com' }
   let(:template_id) { 'template_id' }
 
+  before do
+    allow_any_instance_of(VaNotify::Configuration).to receive(:base_path).and_return('http://fakeapi.com')
+
+    allow(Settings.vanotify.services.va_gov).to receive(:api_key).and_return(
+      'test-aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa-bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'
+    )
+  end
+
   describe '#perform' do
     it 'sends an email using the template id' do
       client = double
@@ -17,6 +25,14 @@ RSpec.describe VANotifyEmailJob, type: :model do
       )
 
       described_class.new.perform(email, template_id)
+    end
+
+    context 'when vanotify returns a 400 error' do
+      it 'rescues and logs the error' do
+        VCR.use_cassette('va_notify/bad_request') do
+          described_class.new.perform(email, template_id)
+        end
+      end
     end
   end
 end
