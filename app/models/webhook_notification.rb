323737a6c88edc470693dbf5896b71752c0afe3e
Greg Bowman <gregory.bowman@libertyits.com>
2021-07-21 14:22:22 -0400

    refactor

--- app/models/webhook_notification.rb
+++ app/models/webhook_notification.rb
@@ -3,4 +3,9 @@
 class WebhookNotification < ApplicationRecord
   has_many :webhook_notification_attempt_assocs
   has_many :webhook_notification_attempts, through: :webhook_notification_attempt_assocs
+
+  def final_attempt
+    return nil unless final_attempt_id
+    webhook_notification_attempts.select { |a| a.id == final_attempt_id }.first
+  end
 end
