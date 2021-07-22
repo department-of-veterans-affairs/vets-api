c007ec39f97519a53277f79115bfb6360a94c3e7
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-07-22 11:11:16 -0400

    puts statsd metric back

--- lib/search/service.rb
+++ lib/search/service.rb
@@ -31,6 +31,7 @@ module Search
     # @return [Search::ResultsResponse] wrapper around results data
     #
     def results
+      StatsD.increment("#{Search::Service::STATSD_KEY_PREFIX}.total")
       with_monitoring do
         response = perform(:get, results_url, query_params)
         Search::ResultsResponse.from(response)
