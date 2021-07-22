c10a01880fb10f9c6c9accb7f8b3bbb7d72376da
Kevin Mircovich <7391284+kevinmirc@users.noreply.github.com>
2021-07-22 13:07:50 -0400

    Update maintenance_windows_controller.rb

--- app/controllers/v0/maintenance_windows_controller.rb
+++ app/controllers/v0/maintenance_windows_controller.rb
@@ -3,6 +3,7 @@
 module V0
   class MaintenanceWindowsController < ApplicationController
     skip_before_action :authenticate
+    skip_before_action :verify_authenticity_token
 
     def index
       @maintenance_windows = MaintenanceWindow.end_after(Time.zone.now)
