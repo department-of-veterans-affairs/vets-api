51ccdd4333ad7f4c5478e4a27867689649ea0c32
vets-api <mark.viterna@va.gov>
2021-07-22 13:04:55 -0400

    Linter

--- modules/vba_documents/app/swagger/vba_documents/responses/v2/unexpected_error.rb
+++ modules/vba_documents/app/swagger/vba_documents/responses/v2/unexpected_error.rb
@@ -2,18 +2,20 @@
 
 module VBADocuments
   module Responses
-    module UnexpectedError
-      def self.extended(base)
-        base.response 422 do
-          key :description, 'Unprocessable Entity'
-          content 'application/json' do
-            schema do
-              key :type, :object
-              key :required, [:errors]
-              property :errors do
-                key :type, :array
-                items do
-                  key :$ref, :ErrorModel
+    module V2
+      module UnexpectedError
+        def self.extended(base)
+          base.response 422 do
+            key :description, 'Unprocessable Entity'
+            content 'application/json' do
+              schema do
+                key :type, :object
+                key :required, [:errors]
+                property :errors do
+                  key :type, :array
+                  items do
+                    key :$ref, :ErrorModel
+                  end
                 end
               end
             end
