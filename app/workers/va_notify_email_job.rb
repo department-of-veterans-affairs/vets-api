97d59eb3320a8bae69833ad1b8bdb633222ef7b0
lihan li <frankieteardrop@gmail.com>
2021-07-22 16:41:28 -0700

    use retry 14 instead of expires_in (#7463)

--- app/workers/va_notify_email_job.rb
+++ app/workers/va_notify_email_job.rb
@@ -2,7 +2,7 @@
 
 class VANotifyEmailJob
   include Sidekiq::Worker
-  sidekiq_options expires_in: 1.day
+  sidekiq_options retry: 14
 
   def perform(email, template_id, personalisation = nil)
     notify_client = VaNotify::Service.new(Settings.vanotify.services.va_gov.api_key)
