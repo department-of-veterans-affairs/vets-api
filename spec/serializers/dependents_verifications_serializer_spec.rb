d43b1d980f980e92987825f91a33a10fa57c7a3e
Kevin Musiorski <kmusiorski@governmentcio.com>
2021-07-23 17:37:02 -0500

    reorganize spec and add test for when there's one diary entry and it's a hash

--- spec/serializers/dependents_verifications_serializer_spec.rb
+++ spec/serializers/dependents_verifications_serializer_spec.rb
@@ -5,16 +5,31 @@ require 'rails_helper'
 describe DependentsVerificationsSerializer do
   subject { described_class.new(diaries) }
 
-  let(:diaries) do
-    VCR.use_cassette('bgs/diaries_service/read_diaries') do
-      BGS::DependencyVerificationService.new(create(:evss_user, :loa3)).read_diaries
-    end
-  end
-
   describe '#prompt_renewal' do
-    it "returns true when there are multiple diary entries with a diary_reason_type of '24'
+    context 'when there are multiple entries in the diaries call' do
+      let(:diaries) do
+        VCR.use_cassette('bgs/diaries_service/read_diaries') do
+          BGS::DependencyVerificationService.new(create(:evss_user, :loa3)).read_diaries
+        end
+      end
+
+      it "returns true when at least one has a diary_reason_type of '24'
       AND a diary_lc_status_type of 'PEND' AND the due_date is within a year" do
-      expect(subject.prompt_renewal).to eq true
+        expect(subject.prompt_renewal).to eq true
+      end
+    end
+
+    context 'when there is one entry in the diaries call' do
+      let(:diaries) do
+        VCR.use_cassette('bgs/diaries_service/read_diaries_one_entry') do
+          BGS::DependencyVerificationService.new(create(:evss_user, :loa3)).read_diaries
+        end
+      end
+
+      it "returns true when at least one has a diary_reason_type of '24'
+      AND a diary_lc_status_type of 'PEND' AND the due_date is within a year" do
+        expect(subject.prompt_renewal).to eq true
+      end
     end
   end
 end
