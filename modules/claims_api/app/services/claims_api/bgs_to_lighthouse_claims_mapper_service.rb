b58725421f0502a2fdf721181b7fd226ef207127
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2021-07-27 12:16:55 -0500

    API-8399 - Enhance logic readability

--- modules/claims_api/app/services/claims_api/bgs_to_lighthouse_claims_mapper_service.rb
+++ modules/claims_api/app/services/claims_api/bgs_to_lighthouse_claims_mapper_service.rb
@@ -10,14 +10,26 @@ module ClaimsApi
     end
 
     def process
-      return matched_claim if bgs_claim.present? && lighthouse_claim.present?
-      return unmatched_bgs_claim if bgs_claim.present? && lighthouse_claim.blank?
-      return unmatched_lighthouse_claim if bgs_claim.blank? && lighthouse_claim.present?
+      return matched_claim if bgs_and_lighthouse_claims_exist?
+      return unmatched_bgs_claim if bgs_claim_only?
+      return unmatched_lighthouse_claim if lighthouse_claim_only?
 
       {}
     end
 
     private
+
+    def bgs_and_lighthouse_claims_exist?
+      bgs_claim.present? && lighthouse_claim.present?
+    end
+
+    def bgs_claim_only?
+      bgs_claim.present? && lighthouse_claim.blank?
+    end
+
+    def lighthouse_claim_only?
+      bgs_claim.blank? && lighthouse_claim.present?
+    end
 
     def matched_claim
       # this claim was submitted via Lighthouse, so use the 'id' the user is most likely to know
