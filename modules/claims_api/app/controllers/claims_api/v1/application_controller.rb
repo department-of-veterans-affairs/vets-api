6f6828dd6b0ece18e2951b59be4e7bc89bb8f3f8
Lee DeBoom <lee.deboom@oddball.io>
2021-07-30 10:08:16 -0500

    API-8800

--- modules/claims_api/app/controllers/claims_api/v1/application_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v1/application_controller.rb
@@ -31,7 +31,8 @@ module ClaimsApi
 
         if header_request? && !target_veteran.mpi_record?
           raise ::Common::Exceptions::UnprocessableEntity.new(
-            detail: 'Submitting an original claim requires the Veteran to be authenticated with an identity-verified account'
+            detail:
+              'Submitting an original claim requires the Veteran to be authenticated with an identity-verified account'
           )
         end
 
