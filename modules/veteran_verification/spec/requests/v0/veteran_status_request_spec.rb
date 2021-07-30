a1119d1d5ddf8a60b9e54570452b7f90f696d4a8
Derek Brown <derek.brown@libertyits.com>
2021-07-30 10:01:06 -0400

    Last linting..

--- modules/veteran_verification/spec/requests/v0/veteran_status_request_spec.rb
+++ modules/veteran_verification/spec/requests/v0/veteran_status_request_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe 'Veteran Status API endpoint', type: :request, skip_emis: true do
 
     context 'when emis response is invalid' do
       before do
-        allow(EMISRedis::MilitaryInformationV2).to receive(:for_user).and_return( nil )
+        allow(EMISRedis::MilitaryInformationV2).to receive(:for_user).and_return(nil)
       end
 
       it 'matches the errors schema', :aggregate_failures do
