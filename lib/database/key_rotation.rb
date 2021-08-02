c237abd812a8f5a29de95881dc24f3caf6444ec2
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-08-02 08:30:47 -0400

    rubocop

--- lib/database/key_rotation.rb
+++ lib/database/key_rotation.rb
@@ -6,7 +6,7 @@ module Database
       encrypted_attributes[attribute][:operation] == :decrypting
     end
 
-    def encryption_key(attribute)
+    def encryption_key(_attribute)
       Settings.db_encryption_key
     end
   end
