eaba4626d2f0cb790e802e8ff0b1cc9a7f2be1b4
KevinCranmer <cranmerkevin39@gmail.com>
2021-08-03 13:54:08 -0500

    privatizing methods in subscription.rb

--- app/models/webhooks/subscription.rb
+++ app/models/webhooks/subscription.rb
@@ -40,6 +40,8 @@ module Webhooks
       retrieve_observers_by_guid(sql, api_name, consumer_id, api_guid)
     end
 
+    private
+    
     def self.retrieve_event_urls(sql, *args)
       result = ActiveRecord::Base.connection_pool.with_connection do |c|
         c.raw_connection.exec_params(sql, args).to_a
