6b598f3a7e4cd330e69c785a0ebd3d3aa1653a4f
Anna Carey <anna@adhocteam.us>
2021-08-05 15:42:39 -0400

    allow vets to resume a 526 form when EVSS is not responding (#7579)
    * allow vets to resume a 526 form when EVSS is not responding
    
    * style

--- spec/request/v0/disability_compensation_in_progress_forms_controller_request_spec.rb
+++ spec/request/v0/disability_compensation_in_progress_forms_controller_request_spec.rb
@@ -68,6 +68,20 @@ RSpec.describe V0::DisabilityCompensationInProgressFormsController, type: :reque
           expect(json_response['formData']['updatedRatedDisabilities']).to eq(rated_disabilites_from_evss)
           expect(json_response['metadata']['returnUrl']).to eq('/disabilities/rated-disabilities')
         end
+
+        it 'returns an unaltered form if EVSS does not respond' do
+          rated_disabilities_before = JSON.parse(in_progress_form.form_data).dig('ratedDisabilities')
+          allow_any_instance_of(EVSS::DisabilityCompensationForm::Service).to(
+            receive(:get_rated_disabilities).and_raise(Common::Client::Errors::ClientError)
+          )
+          get v0_disability_compensation_in_progress_form_url(in_progress_form.form_id), params: nil
+
+          expect(response).to have_http_status(:ok)
+          json_response = JSON.parse(response.body)
+          expect(json_response['formData']['ratedDisabilities']).to eq(rated_disabilities_before)
+          expect(json_response['formData']['updatedRatedDisabilities']).to be_nil
+          expect(json_response['metadata']['returnUrl']).to eq('/va-employee')
+        end
       end
 
       context 'when a form is found and rated_disabilites are unchanged' do
