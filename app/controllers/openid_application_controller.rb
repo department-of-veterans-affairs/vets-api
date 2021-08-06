c3814c7d7b405cf837a50bad43bdd252cddb39ec
lukemarkwordtlibertyits <65039481+lukemarkwordtlibertyits@users.noreply.github.com>
2021-08-06 10:13:13 -0600

    Access tokens in openid controller are hashed (#7561)
    * Access tokens in openid controller are hashed
    
    * Openid tests changed to reflect hashing
    
    * Session will look for hashed token
    
    * token redis analyze looks for hashed token
    
    * reverted Gemfile
    
    * wording
    
    * wording
    
    * hased okta users helpers session
    
    * rolling change
    
    * linting
    
    * created common hash_token method
    
    * Added space
    
    * removed space
    
    * verification application controller will look for unhashed token is there is a hashed miss

--- app/controllers/openid_application_controller.rb
+++ app/controllers/openid_application_controller.rb
@@ -36,7 +36,8 @@ class OpenidApplicationController < ApplicationController
     return false if token.blank?
 
     # Only want to fetch the Okta profile if the session isn't already established and not a CC token
-    @session = Session.find(token) unless token.client_credentials_token?
+    @session = Session.find(hash_token(token)) unless token.client_credentials_token?
+    @session = Session.find(token) unless token.client_credentials_token? || !@session.nil?
     profile = @session.profile unless @session.nil? || @session.profile.nil?
     profile = fetch_profile(token.identifiers.okta_uid) unless token.client_credentials_token? || !profile.nil?
     populate_ssoi_token_payload(profile) if !profile.nil? && profile.attrs['last_login_type'] == 'ssoi'
@@ -72,7 +73,8 @@ class OpenidApplicationController < ApplicationController
   end
 
   def analyze_redis_launch_context
-    @session = Session.find(token)
+    @session = Session.find(hash_token(token))
+    @session = Session.find(token) if @session.nil?
     # Sessions are not originally created for client credentials tokens, one will be created here.
     if @session.nil?
       ttl = token.payload['exp'] - Time.current.utc.to_i
@@ -169,13 +171,13 @@ class OpenidApplicationController < ApplicationController
   end
 
   def build_session(ttl, profile)
-    session = Session.new(token: token.to_s, uuid: token.identifiers.uuid, profile: profile)
+    session = Session.new(token: hash_token(token), uuid: token.identifiers.uuid, profile: profile)
     session.expire(ttl)
     session
   end
 
   def build_launch_session(ttl, launch)
-    session = Session.new(token: token.to_s, launch: launch)
+    session = Session.new(token: hash_token(token), launch: launch)
     session.expire(ttl)
     session
   end
@@ -198,5 +200,8 @@ class OpenidApplicationController < ApplicationController
     raise error_klass('Invalid launch context')
   end
 
+  def hash_token(token)
+    Digest::SHA256.hexdigest(token.to_s)
+  end
   attr_reader :current_user, :session, :scopes
 end
