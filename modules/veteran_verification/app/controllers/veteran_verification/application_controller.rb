8dcee2b424d5b9da0fc83ce57e9943165a31166e
lukemarkwordtlibertyits <65039481+lukemarkwordtlibertyits@users.noreply.github.com>
2021-08-06 10:13:13 -0600

    Access tokens in openid controller are hashed (#7561)
    * Access tokens in openid controller are hashed
    
    * Openid tests changed to reflect hashing
    
    * Session will look for hashed token
    
    * token redis analyze looks for hashed token
    
    * reverted Gemfile
    
    * wording
    
    * wording
    
    * hased okta users helpers session
    
    * rolling change
    
    * linting
    
    * created common hash_token method
    
    * Added space
    
    * removed space
    
    * verification application controller will look for unhashed token is there is a hashed miss

--- modules/veteran_verification/app/controllers/veteran_verification/application_controller.rb
+++ modules/veteran_verification/app/controllers/veteran_verification/application_controller.rb
@@ -15,7 +15,8 @@ module VeteranVerification
       # Not supported for Client Credentials tokens
       return false if token.blank? || token.client_credentials_token?
 
-      @session = Session.find(token)
+      @session = Session.find(Digest::SHA256.hexdigest(token.to_s))
+      @session = Session.find(token) if @session.nil?
       if @session.nil?
         profile = fetch_profile(token.identifiers.okta_uid)
         establish_session(profile)
