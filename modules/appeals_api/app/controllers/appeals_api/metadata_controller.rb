e57e4925d6f8ecd66dfac3b93b225169cd4e41e9
Nathan Wright <nathan.wright@oddball.io>
2021-08-09 14:47:15 -0600

    Add rswag to metadata controller (in specific envs)

--- modules/appeals_api/app/controllers/appeals_api/metadata_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/metadata_controller.rb
@@ -92,14 +92,16 @@ module AppealsApi
     end
 
     def decision_reviews_versions
-      if v2_enabled?
+      if beta_enabled?
         [
-          decision_reviews_v1.merge({ status: VERSION_STATUS[:previous] }),
-          decision_reviews_v2
+          decision_reviews_v1.merge,
+          decision_reviews_v2.merge({ status: VERSION_STATUS[:previous] }),
+          decision_reviews_v2_beta
         ]
       else
         [
-          decision_reviews_v1
+          decision_reviews_v1,
+          decision_reviews_v2
         ]
       end
     end
@@ -124,7 +126,17 @@ module AppealsApi
       }
     end
 
-    def v2_enabled?
+    def decision_reviews_v2_beta
+      {
+        version: '2.0.0-rwswag',
+        internal_only: true,
+        status: VERSION_STATUS[:current],
+        path: '/services/appeals/docs/v2/decision_reviews_beta',
+        healthcheck: '/services/appeals/v2/healthcheck'
+      }
+    end
+
+    def beta_enabled?
       Settings.modules_appeals_api.documentation.path_enabled_flag
     end
   end
