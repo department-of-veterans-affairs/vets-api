db1629b01dba1bde98f7279649ac0d506301b31d
Jack Schuss <jackschuss@gmail.com>
2021-08-12 16:56:29 -0400

    appease the rubocop overlords

--- modules/appeals_api/app/models/appeals_api/veteran.rb
+++ modules/appeals_api/app/models/appeals_api/veteran.rb
@@ -2,23 +2,21 @@
 
 module AppealsApi
   class Veteran
-
     delegate_missing_to :@veteran
 
     def initialize(ssn:, first_name:, last_name:, birth_date:)
-      @veteran ||= ClaimsApi::Veteran.new(
-          uuid: ssn,
-          ssn: ssn,
-          first_name: first_name,
-          last_name: last_name,
-          va_profile: ClaimsApi::Veteran.build_profile(birth_date),
-          loa: 3
-        )
+      @veteran = ClaimsApi::Veteran.new(
+        uuid: ssn,
+        ssn: ssn,
+        first_name: first_name,
+        last_name: last_name,
+        va_profile: ClaimsApi::Veteran.build_profile(birth_date),
+        loa: 3
+      )
     end
 
     private
 
     attr_reader :veteran
-
   end
 end
