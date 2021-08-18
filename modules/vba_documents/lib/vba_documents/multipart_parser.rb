2053ce67551d45d709d1de6a848b2793050b07be
vets-api <mark.viterna@va.gov>
2021-08-18 11:32:56 -0400

    Linter

--- modules/vba_documents/lib/vba_documents/multipart_parser.rb
+++ modules/vba_documents/lib/vba_documents/multipart_parser.rb
@@ -166,7 +166,7 @@ module VBADocuments
           tf.rewind
           return tf, true
         else
-          # Some clients appends a new line at the end of the pdf,
+          # Some clients appends a new line at the end of the pdf
           # we must remove it to maintain the original sha256 value
           line.chomp! # if end_of_pdf(line)
           tf.write(line)
