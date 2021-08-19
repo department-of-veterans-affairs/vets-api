7cb4c145689c5388d0caf3671fcb364d7e2a8148
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-08-19 13:53:59 -0600

    fixed tests

--- modules/veteran_verification/spec/models/service_history_episode_spec.rb
+++ modules/veteran_verification/spec/models/service_history_episode_spec.rb
@@ -13,7 +13,7 @@ describe VeteranVerification::ServiceHistoryEpisode, skip_emis: true do
           expect(result.length).to eq(1)
           expect(result[0][:first_name]).to eq('abraham')
           expect(result[0][:last_name]).to eq('lincoln')
-          expect(result[0][:branch_of_service]).to eq('Army National Guard')
+          expect(result[0][:branch_of_service]).to eq('Army')
           expect(result[0][:pay_grade]).to eq('W04')
           expect(result[0][:deployments][0][:location]).to eq('AX1')
         end
@@ -27,7 +27,7 @@ describe VeteranVerification::ServiceHistoryEpisode, skip_emis: true do
           expect(result.length).to eq(2)
           expect(result[0][:first_name]).to eq('abraham')
           expect(result[0][:last_name]).to eq('lincoln')
-          expect(result[0][:branch_of_service]).to eq('Army National Guard')
+          expect(result[0][:branch_of_service]).to eq('Army')
           expect(result[0][:pay_grade]).to eq('W04')
           expect(result[0][:deployments][0][:location]).to eq('AX1')
         end
@@ -39,10 +39,10 @@ describe VeteranVerification::ServiceHistoryEpisode, skip_emis: true do
         VCR.use_cassette('emis/get_military_service_episodes_v2/valid_multiple_episodes_end_date_null') do
           result = described_class.for_user(user)
           expect(result.length).to eq(2)
-          expect(result[0][:branch_of_service]).to eq('Army National Guard')
+          expect(result[0][:branch_of_service]).to eq('Army')
           expect(result[0][:pay_grade]).to eq('W04')
           expect(result[0][:deployments].length).to eq(0)
-          expect(result[1][:branch_of_service]).to eq('Army National Guard')
+          expect(result[1][:branch_of_service]).to eq('Army')
           expect(result[1][:pay_grade]).to eq('W04')
           expect(result[1][:deployments].length).to eq(3)
         end
@@ -54,7 +54,7 @@ describe VeteranVerification::ServiceHistoryEpisode, skip_emis: true do
         VCR.use_cassette('emis/get_military_service_episodes_v2/invalid_missing_pay_plan') do
           result = described_class.for_user(user)
           expect(result.length).to eq(1)
-          expect(result[0][:branch_of_service]).to eq('Army National Guard')
+          expect(result[0][:branch_of_service]).to eq('Army')
           expect(result[0][:pay_grade]).to eq('unknown')
           expect(result[0][:deployments][0][:location]).to eq('AX1')
         end
