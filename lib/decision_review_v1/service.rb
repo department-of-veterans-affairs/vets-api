a9a02de6fd7eba5eaa64e9554323fd44db793525
Mike <michelpmcdonald@gmail.com>
2021-08-31 10:18:57 -0400

    Mpm/hlr v1 service (#7771)
    * Create v1 service that uses lighthouse 2.0 HLR
    
    * Lint Cleanup
    
    * Added setting for path to new lighthouse endpoint
    
    * Exclude new service from block argument suggestion, just like the existing services do
    
    Co-authored-by: michelpmcdonald <michel.mcdonald@adhocteam>

---
+++ lib/decision_review_v1/service.rb
@@ -0,0 +1,194 @@
+# frozen_string_literal: true
+
+require 'common/client/base'
+require 'common/client/concerns/monitoring'
+require 'common/client/errors'
+require 'common/exceptions/forbidden'
+require 'common/exceptions/schema_validation_errors'
+require 'decision_review_v1/configuration'
+require 'decision_review_v1/service_exception'
+
+module DecisionReviewV1
+  ##
+  # Proxy Service for the Lighthouse Decision Reviews API.
+  #
+  class Service < Common::Client::Base
+    include SentryLogging
+    include Common::Client::Concerns::Monitoring
+
+    configuration DecisionReviewV1::Configuration
+
+    STATSD_KEY_PREFIX = 'api.decision_review'
+    HLR_CREATE_RESPONSE_SCHEMA = VetsJsonSchema::SCHEMAS.fetch 'HLR-CREATE-RESPONSE-200_V1'
+    HLR_SHOW_RESPONSE_SCHEMA = VetsJsonSchema::SCHEMAS.fetch 'HLR-SHOW-RESPONSE-200_V1'
+    HLR_GET_CONTESTABLE_ISSUES_RESPONSE_SCHEMA = VetsJsonSchema::SCHEMAS.fetch 'HLR-GET-CONTESTABLE-ISSUES-RESPONSE-200'
+    REQUIRED_CREATE_HEADERS = %w[X-VA-First-Name X-VA-Last-Name X-VA-SSN X-VA-Birth-Date].freeze
+
+    ##
+    # Create a Higher-Level Review
+    #
+    # @param request_body [JSON] JSON serialized version of a Higher-Level Review Form (20-0996)
+    # @param user [User] Veteran who the form is in regard to
+    # @return [Faraday::Response]
+    #
+    def create_higher_level_review(request_body:, user:)
+      with_monitoring_and_error_handling do
+        headers = create_higher_level_review_headers(user)
+        response = perform :post, 'higher_level_reviews', request_body, headers
+        raise_schema_error_unless_200_status response.status
+        validate_against_schema json: response.body, schema: HLR_CREATE_RESPONSE_SCHEMA, append_to_error_class: ' (HLR)'
+        response
+      end
+    end
+
+    ##
+    # Retrieve a Higher-Level Review
+    #
+    # @param uuid [uuid] A Higher-Level Review's UUID (included in a create_higher_level_review response)
+    # @return [Faraday::Response]
+    #
+    def get_higher_level_review(uuid)
+      with_monitoring_and_error_handling do
+        response = perform :get, "higher_level_reviews/#{uuid}", nil
+        raise_schema_error_unless_200_status response.status
+        validate_against_schema json: response.body, schema: HLR_SHOW_RESPONSE_SCHEMA, append_to_error_class: ' (HLR)'
+        response
+      end
+    end
+
+    ##
+    # Get Contestable Issues for a Higher-Level Review
+    #
+    # @param user [User] Veteran who the form is in regard to
+    # @param benefit_type [String] Type of benefit the decision review is for
+    # @return [Faraday::Response]
+    #
+    def get_higher_level_review_contestable_issues(user:, benefit_type:)
+      with_monitoring_and_error_handling do
+        path = "higher_level_reviews/contestable_issues/#{benefit_type}"
+        headers = get_contestable_issues_headers(user)
+        response = perform :get, path, nil, headers
+        raise_schema_error_unless_200_status response.status
+        validate_against_schema(
+          json: response.body,
+          schema: HLR_GET_CONTESTABLE_ISSUES_RESPONSE_SCHEMA,
+          append_to_error_class: ' (HLR)'
+        )
+        response
+      end
+    end
+
+    # upstream requirements
+    # ^[a-zA-Z\-\/\s]{1,50}$
+    # Cannot be missing or empty or longer than 50 characters.
+    # Only upper/lower case letters, hyphens(-), spaces and forward-slash(/) allowed
+    def self.transliterate_name(str)
+      I18n.transliterate(str.to_s).gsub(%r{[^a-zA-Z\-/\s]}, '').strip.first(50)
+    end
+
+    private
+
+    def create_higher_level_review_headers(user)
+      headers = {
+        'X-VA-SSN' => user.ssn.to_s.strip.presence,
+        'X-VA-First-Name' => user.first_name.to_s.strip.first(12),
+        'X-VA-Middle-Initial' => middle_initial(user),
+        'X-VA-Last-Name' => user.last_name.to_s.strip.first(18).presence,
+        'X-VA-Birth-Date' => user.birth_date.to_s.strip.presence,
+        'X-VA-File-Number' => nil,
+        'X-VA-Service-Number' => nil,
+        'X-VA-Insurance-Policy-Number' => nil
+      }.compact
+
+      missing_required_fields = REQUIRED_CREATE_HEADERS - headers.keys
+      if missing_required_fields.present?
+        raise Common::Exceptions::Forbidden.new(
+          source: "#{self.class}##{__method__}",
+          detail: { missing_required_fields: missing_required_fields }
+        )
+      end
+
+      headers
+    end
+
+    def middle_initial(user)
+      user.middle_name.to_s.strip.presence&.first&.upcase
+    end
+
+    def get_contestable_issues_headers(user)
+      raise Common::Exceptions::Forbidden.new source: "#{self.class}##{__method__}" unless user.ssn
+
+      {
+        'X-VA-SSN' => user.ssn.to_s,
+        'X-VA-Receipt-Date' => Time.zone.now.strftime('%Y-%m-%d')
+      }
+    end
+
+    def with_monitoring_and_error_handling
+      with_monitoring(2) do
+        yield
+      end
+    rescue => e
+      handle_error(e)
+    end
+
+    def save_error_details(error)
+      PersonalInformationLog.create!(
+        error_class: "#{self.class.name}#save_error_details exception #{error.class} (HLR) (NOD)",
+        data: { error: Class.new.include(FailedRequestLoggable).exception_hash(error) }
+      )
+      Raven.tags_context external_service: self.class.to_s.underscore
+      Raven.extra_context url: config.base_path, message: error.message
+    end
+
+    def handle_error(error)
+      save_error_details error
+      source_hash = { source: "#{error.class} raised in #{self.class}" }
+
+      raise case error
+            when Faraday::ParsingError
+              DecisionReviewV1::ServiceException.new key: 'DR_502', response_values: source_hash
+            when Common::Client::Errors::ClientError
+              Raven.extra_context body: error.body, status: error.status
+              if error.status == 403
+                Common::Exceptions::Forbidden.new source_hash
+              else
+                DecisionReviewV1::ServiceException.new(
+                  key: "DR_#{error.status}",
+                  response_values: source_hash,
+                  original_status: error.status,
+                  original_body: error.body
+                )
+              end
+            else
+              error
+            end
+    end
+
+    def validate_against_schema(json:, schema:, append_to_error_class: '')
+      errors = JSONSchemer.schema(schema).validate(json).to_a
+      return if errors.empty?
+
+      raise Common::Exceptions::SchemaValidationErrors, remove_pii_from_json_schemer_errors(errors)
+    rescue => e
+      PersonalInformationLog.create!(
+        error_class: "#{self.class.name}#validate_against_schema exception #{e.class}#{append_to_error_class}",
+        data: {
+          json: json, schema: schema, errors: errors,
+          error: Class.new.include(FailedRequestLoggable).exception_hash(e)
+        }
+      )
+      raise
+    end
+
+    def raise_schema_error_unless_200_status(status)
+      return if status == 200
+
+      raise Common::Exceptions::SchemaValidationErrors, ["expecting 200 status received #{status}"]
+    end
+
+    def remove_pii_from_json_schemer_errors(errors)
+      errors.map { |error| error.slice 'data_pointer', 'schema', 'root_schema' }
+    end
+  end
+end
---
+++ spec/lib/decision_review_v1/service_spec.rb
@@ -0,0 +1,191 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'decision_review_v1/service'
+
+describe DecisionReviewV1::Service do
+  subject { described_class.new }
+
+  let(:ssn_with_mockdata) { '212222112' }
+  let(:user) { build(:user, :loa3, ssn: ssn_with_mockdata) }
+
+  describe 'VetsJsonSchema used in service' do
+    describe 'ensure HLR schemas are present' do
+      %w[
+        HLR-CREATE-REQUEST-BODY_V1
+        HLR-CREATE-REQUEST-HEADERS
+        HLR-CREATE-RESPONSE-200
+        HLR-CREATE-RESPONSE-422
+        HLR-GET-CONTESTABLE-ISSUES-REQUEST-BENEFIT-TYPE
+        HLR-GET-CONTESTABLE-ISSUES-REQUEST-HEADERS
+        HLR-GET-CONTESTABLE-ISSUES-RESPONSE-200
+        HLR-GET-CONTESTABLE-ISSUES-RESPONSE-404
+        HLR-GET-CONTESTABLE-ISSUES-RESPONSE-422
+        HLR-SHOW-RESPONSE-200
+        HLR-SHOW-RESPONSE-404
+      ].each do |schema_name|
+        it("#{schema_name} schema is present") { expect(VetsJsonSchema::SCHEMAS[schema_name]).to be_a Hash }
+      end
+    end
+
+    describe 'ensure HLR schema examples are present' do
+      %w[
+        HLR-CREATE-REQUEST-BODY_V1
+        HLR-CREATE-REQUEST-HEADERS
+        HLR-CREATE-RESPONSE-200
+        HLR-CREATE-RESPONSE-422
+        HLR-GET-CONTESTABLE-ISSUES-REQUEST-HEADERS
+        HLR-GET-CONTESTABLE-ISSUES-RESPONSE-200
+        HLR-GET-CONTESTABLE-ISSUES-RESPONSE-404
+        HLR-GET-CONTESTABLE-ISSUES-RESPONSE-422
+        HLR-SHOW-RESPONSE-200
+        HLR-SHOW-RESPONSE-404
+      ].each do |schema_name|
+        it("#{schema_name} schema example is present") { expect(VetsJsonSchema::EXAMPLES).to have_key schema_name }
+      end
+    end
+  end
+
+  describe '#create_higher_level_review_headers' do
+    subject { described_class.new.send(:create_higher_level_review_headers, user) }
+
+    let(:user) do
+      name = 'x' * 100
+      build :user, first_name: name, middle_name: name, last_name: name
+    end
+
+    it 'returns a properly formatted 200 response' do
+      expect(subject['X-VA-First-Name']).to eq 'x' * 12
+      expect(subject['X-VA-Middle-Initial']).to eq 'X'
+      expect(subject['X-VA-Last-Name']).to eq 'x' * 18
+    end
+  end
+
+  describe '#create_higher_level_review' do
+    subject { described_class.new.create_higher_level_review(request_body: body.to_json, user: user) }
+
+    let(:body) { VetsJsonSchema::EXAMPLES['HLR-CREATE-REQUEST-BODY_V1'] }
+
+    context '200 response' do
+      it 'returns a properly formatted 200 response' do
+        VCR.use_cassette('decision_review/HLR-CREATE-RESPONSE-200_V1') do
+          expect(subject).to respond_to :status
+          expect(subject.status).to be 200
+          expect(subject).to respond_to :body
+          expect(subject.body).to be_a Hash
+        end
+      end
+    end
+
+    context '422 response' do
+      let(:body) { {} }
+
+      it 'throws a DR_422 exception' do
+        VCR.use_cassette('decision_review/HLR-CREATE-RESPONSE-422_V1') do
+          expect { subject }.to raise_error(
+            an_instance_of(DecisionReviewV1::ServiceException).and(having_attributes(key: 'DR_422'))
+          )
+        end
+      end
+    end
+
+    context 'user is missing data' do
+      before do
+        allow_any_instance_of(User).to receive(:ssn).and_return(nil)
+      end
+
+      it 'throws a Common::Exceptions::Forbidden exception' do
+        expect { subject }.to raise_error Common::Exceptions::Forbidden
+      end
+    end
+  end
+
+  describe '#get_higher_level_review' do
+    subject { described_class.new.get_higher_level_review(uuid) }
+
+    let(:uuid) { '75f5735b-c41d-499c-8ae2-ab2740180254' }
+
+    context '200 response' do
+      it 'returns a properly formatted 200 response' do
+        VCR.use_cassette('decision_review/HLR-SHOW-RESPONSE-200_V1') do
+          expect(subject).to respond_to :status
+          expect(subject.status).to be 200
+          expect(subject).to respond_to :body
+          expect(subject.body).to be_a Hash
+        end
+      end
+    end
+
+    context '404 response' do
+      let(:uuid) { '0' }
+
+      it 'throws a DR_404 exception' do
+        VCR.use_cassette('decision_review/HLR-SHOW-RESPONSE-404_V1') do
+          expect { subject }.to raise_error(
+            an_instance_of(DecisionReviewV1::ServiceException).and(having_attributes(key: 'DR_404'))
+          )
+        end
+      end
+    end
+  end
+
+  describe '#get_higher_level_review_contestable_issues' do
+    subject do
+      described_class.new.get_higher_level_review_contestable_issues(benefit_type: benefit_type, user: user)
+    end
+
+    let(:benefit_type) { 'compensation' }
+
+    context '200 response' do
+      it 'returns a properly formatted 200 response' do
+        VCR.use_cassette('decision_review/HLR-GET-CONTESTABLE-ISSUES-RESPONSE-200_V1') do
+          expect(subject).to respond_to :status
+          expect(subject.status).to be 200
+          expect(subject).to respond_to :body
+          expect(subject.body).to be_a Hash
+        end
+      end
+    end
+
+    context '200 response with a malformed body' do
+      def personal_information_logs
+        PersonalInformationLog.where error_class: 'DecisionReviewV1::Service#validate_against_schema' \
+          ' exception Common::Exceptions::SchemaValidationErrors (HLR)'
+      end
+
+      it 'returns a schema error' do
+        VCR.use_cassette('decision_review/HLR-GET-CONTESTABLE-ISSUES-RESPONSE-200-MALFORMED_V1') do
+          expect(personal_information_logs.count).to be 0
+          expect { subject }.to raise_error an_instance_of Common::Exceptions::SchemaValidationErrors
+          expect(personal_information_logs.count).to be 1
+        end
+      end
+    end
+
+    context '404 response' do
+      before do
+        allow_any_instance_of(User).to receive(:ssn).and_return('000000000')
+      end
+
+      it 'throws a DR_404 exception' do
+        VCR.use_cassette('decision_review/HLR-GET-CONTESTABLE-ISSUES-RESPONSE-404_V1') do
+          expect { subject }.to raise_error(
+            an_instance_of(DecisionReviewV1::ServiceException).and(having_attributes(key: 'DR_404'))
+          )
+        end
+      end
+    end
+
+    context '422 response' do
+      let(:benefit_type) { 'apricot' }
+
+      it 'throws a DR_422 exception' do
+        VCR.use_cassette('decision_review/HLR-GET-CONTESTABLE-ISSUES-RESPONSE-422_V1') do
+          expect { subject }.to raise_error(
+            an_instance_of(DecisionReviewV1::ServiceException).and(having_attributes(key: 'DR_422'))
+          )
+        end
+      end
+    end
+  end
+end
