84519fde321f67ec62aa5b6cb792291f3206c308
Anna Carey <anna@adhocteam.us>
2021-09-02 16:11:05 -0400

    re-add Form526IncreaseOnly class (#7802)
    * re-add Form526IncreaseOnly class
    
    * Update form526_increase_only.rb

---
+++ app/models/saved_claim/disability_compensation/form526_increase_only.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# This model is no longer actvely used, new forms use Form526AllClaim, but it's useful for retreiving old data.
+class SavedClaim::DisabilityCompensation::Form526IncreaseOnly < SavedClaim::DisabilityCompensation
+end
