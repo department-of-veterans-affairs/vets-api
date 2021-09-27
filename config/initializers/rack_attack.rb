1d1fdbd85e6ee266576b58da4fa35c3783e44de0
Dillo Raju <dilloshion@gmail.com>
2021-09-27 11:10:37 -0400

    29965 Check-in module rack-attack (#7988)
    - Enable throttling on all check_in endpoints

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -48,7 +48,7 @@ class Rack::Attack
   end
 
   throttle('check_in/ip', limit: 10, period: 1.minute) do |req|
-    req.remote_ip if req.path.starts_with?('/check_in/v0') && (req.post? || req.get?)
+    req.remote_ip if req.path.starts_with?('/check_in')
   end
 
   throttle('medical_copays/ip', limit: 20, period: 1.minute) do |req|
