17a13d2138c16872ee171547bbb5ce519447c9a8
Dillo Raju <dilloshion@gmail.com>
2021-10-04 11:03:36 -0400

    27857 V2 CHIP versioning and integration (#8054)

---
+++ modules/check_in/app/services/v2/chip/service.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module V2
+  module Chip
+    class Service
+      extend Forwardable
+
+      attr_reader :check_in, :request, :response, :session, :settings, :check_in_body
+
+      def_delegators :check_in, :client_error, :uuid, :valid?
+      def_delegators :settings, :base_path
+
+      def self.build(check_in)
+        new(check_in)
+      end
+
+      def initialize(opts = {})
+        @settings = Settings.check_in.chip_api_v2
+        @check_in = opts[:check_in]
+        @check_in_body = opts[:params]
+        @request = Request.build
+        @response = Response
+        @session = Session.build
+      end
+
+      def create_check_in
+        return response.build(response: client_error).handle unless valid?
+
+        token = session.retrieve
+        resp = request.post(path: "/#{base_path}/actions/check-in/#{uuid}", access_token: token, params: check_in_body)
+
+        response.build(response: resp).handle
+      end
+    end
+  end
+end
---
+++ modules/check_in/spec/services/v2/chip/service_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe V2::Chip::Service do
+  subject { described_class }
+
+  let(:id) { 'd602d9eb-9a31-484f-9637-13ab0b507e0d' }
+  let(:valid_check_in) { CheckIn::V2::Session.build(data: { uuid: id, last4: '1234', last_name: 'Johnson' }, jwt: nil) }
+  let(:invalid_check_in) { CheckIn::V2::Session.build(data: { uuid: '1234' }, jwt: nil) }
+  let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+
+  before do
+    allow(Rails).to receive(:cache).and_return(memory_store)
+
+    Rails.cache.clear
+  end
+
+  describe '.build' do
+    it 'returns an instance of Service' do
+      expect(subject.build(check_in: valid_check_in, params: {})).to be_an_instance_of(V2::Chip::Service)
+    end
+  end
+
+  describe '#create_check_in' do
+    let(:opts) do
+      {
+        path: '/dev/actions/check-in/d602d9eb-9a31-484f-9637-13ab0b507e0d',
+        access_token: 'abc123',
+        params: { appointment_ien: '123-456-abc' }
+      }
+    end
+    let(:resp) { 'Checkin successful' }
+    let(:faraday_response) { Faraday::Response.new(body: resp, status: 200) }
+
+    it 'returns a Faraday::Response' do
+      allow_any_instance_of(V2::Chip::Session).to receive(:retrieve).and_return('abc123')
+      allow_any_instance_of(V2::Chip::Request).to receive(:post).with(opts).and_return(faraday_response)
+
+      hsh = { data: faraday_response.body, status: faraday_response.status }
+
+      Rails.cache.write(
+        'check_in_lorota_v2_d602d9eb-9a31-484f-9637-13ab0b507e0d_read.full',
+        '12345',
+        namespace: 'check-in-lorota-v2-cache'
+      )
+
+      expect(subject.build(check_in: valid_check_in, params: { appointment_ien: '123-456-abc' })
+        .create_check_in).to eq(hsh)
+    end
+  end
+
+  describe '#base_path' do
+    it 'returns base_path' do
+      expect(subject.build(check_in: valid_check_in, params: {}).base_path).to eq('dev')
+    end
+  end
+end
