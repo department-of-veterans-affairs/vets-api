84c8dbf5c103bd0fc194b23e58c0879ec3f2ed4d
Nathan Wright <nathanbwright@users.noreply.github.com>
2021-10-07 09:33:21 -0600

    API-10053 - Make Rswag default for v2 docs (#8009)

--- modules/appeals_api/app/controllers/appeals_api/docs/v2/docs_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/docs/v2/docs_controller.rb
@@ -3,34 +3,12 @@
 class AppealsApi::Docs::V2::DocsController < ApplicationController
   skip_before_action(:authenticate)
 
-  SWAGGERED_CLASSES = [
-    AppealsApi::V2::HigherLevelReviewsControllerSwagger,
-    AppealsApi::V1::NoticeOfDisagreementsControllerSwagger,
-    AppealsApi::V2::LegacyAppealsControllerSwagger,
-    AppealsApi::V1::Schemas::NoticeOfDisagreements,
-    AppealsApi::V2::Schemas::HigherLevelReviews,
-    AppealsApi::V2::Schemas::LegacyAppeals,
-    AppealsApi::V2::SecuritySchemeSwagger,
-    AppealsApi::V2::SwaggerRoot
-  ].freeze
-
   def decision_reviews
-    render json: decision_reviews_swagger_json
-  end
-
-  def decision_reviews_beta
-    swagger = JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger.json')))
+    swagger = if Settings.vsp_environment == 'production'
+                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger.json')))
+              else
+                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger_dev.json')))
+              end
     render json: swagger
-  end
-
-  private
-
-  def decision_reviews_swagger_json
-    Swagger::Blocks.build_root_json(SWAGGERED_CLASSES)
-                   .deep_merge(
-                     AppealsApi::V2::Schemas::HigherLevelReviews.hlr_legacy_schemas
-                   ).deep_merge(
-                     AppealsApi::V1::Schemas::NoticeOfDisagreements.nod_json_schemas
-                   )
   end
 end
--- modules/appeals_api/config/routes.rb
+++ modules/appeals_api/config/routes.rb
@@ -78,7 +78,6 @@ AppealsApi::Engine.routes.draw do
 
     namespace :v2, defaults: { format: 'json' } do
       get 'decision_reviews', to: 'docs#decision_reviews'
-      get 'decision_reviews_beta', to: 'docs#decision_reviews_beta'
     end
   end
 end
