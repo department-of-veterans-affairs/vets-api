c14122ee94743017bfab1a346e1478b42c265e1f
Corey Ferris <cferris320@gmail.com>
2021-10-13 12:31:01 -0400

    30679 VAOS systems endpoints updates (#8051)
    updated specs

--- modules/vaos/spec/request/v2/available_slots_request_spec.rb
+++ modules/vaos/spec/request/v2/available_slots_request_spec.rb
@@ -20,18 +20,18 @@ RSpec.describe 'Available Slots Request', type: :request do
       context 'on a successful request' do
         it 'returns list of available slots' do
           VCR.use_cassette('vaos/v2/systems/get_available_slots_200', match_requests_on: %i[method uri]) do
-            get '/vaos/v2/locations/983/clinics/1081/slots?start=2021-05-26T00:00:00Z&end=2021-06-30T23:59:59Z',
+            get '/vaos/v2/locations/983/clinics/1081/slots?end=2021-12-30T23:59:59Z&start=2021-10-26T00:00:00Z',
                 headers: inflection_header
             expect(response).to have_http_status(:ok)
             expect(response.body).to match_camelized_schema('vaos/v2/slots', { strict: false })
 
             slots = JSON.parse(response.body)['data']
-            expect(slots.size).to eq(401)
+            expect(slots.size).to eq(730)
             slot = slots[1]
-            expect(slot['id']).to eq('3230323130353236313433303A323032313035323631353030')
+            expect(slot['id']).to eq('3230323131303236323133303A323032313130323632323030')
             expect(slot['type']).to eq('slots')
-            expect(slot['attributes']['start']).to eq('2021-05-26T14:30:00Z')
-            expect(slot['attributes']['end']).to eq('2021-05-26T15:00:00Z')
+            expect(slot['attributes']['start']).to eq('2021-10-26T21:30:00Z')
+            expect(slot['attributes']['end']).to eq('2021-10-26T22:00:00Z')
           end
         end
       end
@@ -39,7 +39,8 @@ RSpec.describe 'Available Slots Request', type: :request do
       context 'on a backend service error' do
         it 'returns a 502 status code' do
           VCR.use_cassette('vaos/v2/systems/get_available_slots_500', match_requests_on: %i[method uri]) do
-            get '/vaos/v2/locations/983/clinics/570/slots?start=2021-06-01T00:00:00Z&end=2021-12-31T23:59:59Z'
+            get '/vaos/v2/locations/983/clinics/1081/slots?end=2021-12-31T23:59:59Z&start=2021-10-01T00:00:00Z'
+
             expect(response).to have_http_status(:bad_gateway)
             expect(JSON.parse(response.body)['errors'][0]['detail'])
               .to eq('Received an an invalid response from the upstream server')
--- modules/vaos/spec/services/v2/systems_service_spec.rb
+++ modules/vaos/spec/services/v2/systems_service_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe VAOS::V2::SystemsService do
   subject { VAOS::V2::SystemsService.new(user) }
 
-  let(:user) { build(:user, :mhv) }
+  let(:user) { build(:user, :vaos) }
 
   before { allow_any_instance_of(VAOS::UserService).to receive(:session).and_return('stubbed_token') }
 
@@ -36,8 +36,8 @@ describe VAOS::V2::SystemsService do
       it 'raises a backend exception' do
         VCR.use_cassette('vaos/v2/systems/get_available_slots_500', match_requests_on: %i[method uri]) do
           expect do
-            subject.get_available_slots(location_id: '983', clinic_id: '570',
-                                        start_dt: '2021-06-01T00:00:00Z',
+            subject.get_available_slots(location_id: '983', clinic_id: '1081',
+                                        start_dt: '2021-10-01T00:00:00Z',
                                         end_dt: '2021-12-31T23:59:59Z')
           end.to raise_error(Common::Exceptions::BackendServiceException, /VAOS_502/)
         end
@@ -48,12 +48,12 @@ describe VAOS::V2::SystemsService do
       it 'returns a list of available slots' do
         VCR.use_cassette('vaos/v2/systems/get_available_slots_200', match_requests_on: %i[method uri]) do
           available_slots = subject.get_available_slots(location_id: '983', clinic_id: '1081',
-                                                        start_dt: '2021-05-26T00:00:00Z',
-                                                        end_dt: '2021-06-30T23:59:59Z')
-          expect(available_slots.size).to eq(401)
-          expect(available_slots[400].id).to eq('3230323130363330323133303A323032313036333032323030')
-          expect(available_slots[400].start).to eq('2021-06-30T21:30:00Z')
-          expect(available_slots[400].end).to eq('2021-06-30T22:00:00Z')
+                                                        start_dt: '2021-10-26T00:00:00Z',
+                                                        end_dt: '2021-12-30T23:59:59Z')
+          expect(available_slots.size).to eq(730)
+          expect(available_slots[400].id).to eq('3230323131323031323130303A323032313132303132313330')
+          expect(available_slots[400].start).to eq('2021-12-01T21:00:00Z')
+          expect(available_slots[400].end).to eq('2021-12-01T21:30:00Z')
         end
       end
     end
