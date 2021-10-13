2c8a44e02f3e9ba2961cc1190571a950c4460ac8
Brad Bergeron <bradley.bergeron@accenturefederal.com>
2021-10-13 10:37:44 -0400

    removes 1990ez from form profiles array (#8157)
    * removes 1990ez from form profiles array
    
    * Remove other changes relating to 1990ez

--- app/models/form_profile.rb
+++ app/models/form_profile.rb
@@ -94,7 +94,7 @@ class FormProfile
 
   ALL_FORMS = {
     edu: %w[22-1990 22-1990N 22-1990E 22-1995 22-5490
-            22-5495 22-0993 22-0994 FEEDBACK-TOOL 22-10203 22-1990S 22-1990EZ],
+            22-5495 22-0993 22-0994 FEEDBACK-TOOL 22-10203 22-1990S],
     evss: ['21-526EZ'],
     hca: ['1010ez'],
     pension_burial: %w[21P-530 21P-527EZ],
@@ -131,8 +131,7 @@ class FormProfile
     '22-1990S' => ::FormProfiles::VA1990s,
     '5655' => ::FormProfiles::VA5655,
     '28-8832' => ::FormProfiles::VA288832,
-    '28-1900' => ::FormProfiles::VA281900,
-    '22-1990EZ' => ::FormProfiles::VA1990ez
+    '28-1900' => ::FormProfiles::VA281900
   }.freeze
 
   APT_REGEX = /\S\s+((apt|apartment|unit|ste|suite).+)/i.freeze
