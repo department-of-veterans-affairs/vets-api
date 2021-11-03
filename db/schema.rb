c8d572f45cc19c3dea00bb163a3870084fb76f6d
Trevor Bosaw <bosawt@gmail.com>
2021-11-03 15:08:16 -0700

    Update schema change to reorder a column that moves every time you migrate (#8342)
    Co-authored-by: Trevor Bosaw <trevor.bosaw@oddball.io>

--- db/schema.rb
+++ db/schema.rb
@@ -759,11 +759,10 @@ ActiveRecord::Schema.define(version: 2021_11_03_172528) do
     t.datetime "checkout_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "services"
     t.string "id_type"
     t.string "loa"
     t.string "account_type"
-
+    t.text "services"
     t.uuid "idme_uuid"
     t.text "notes"
   end
