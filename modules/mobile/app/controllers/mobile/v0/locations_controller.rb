e03925e82a984ce12361f321e49a84a737af81a5
Jayson Perkins <jayson.perkins@adhocteam.us>
2021-11-04 12:54:52 -0400

    Mobile location logging (#8375)
    * added logic for SM get signature preferences
    
    * added logs and updated to use .first pattern in other instances
    
    * added id log

--- modules/mobile/app/controllers/mobile/v0/locations_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/locations_controller.rb
@@ -6,12 +6,16 @@ module Mobile
   module V0
     class LocationsController < ApplicationController
       def show
+        Rails.logger.info('Mobile Location', param_id: params[:id])
         lh_location = service.get_location(params[:id])
+        Rails.logger.info('Mobile Location, Lighthouse Info', lh_location: lh_location)
         id = lh_location[:identifier].first[:value][4..]
+        Rails.logger.info('Mobile Location, Location ID', location_id: id)
         facility = Mobile::FacilitiesHelper.get_facilities([id])
-        raise Common::Exceptions::BackendServiceException, 'LIGHTHOUSE_FACILITIES404' if facility[0].nil?
+        Rails.logger.info('Mobile Location, Facility Info', facility: facility.first)
+        raise Common::Exceptions::BackendServiceException, 'LIGHTHOUSE_FACILITIES404' if facility.first.nil?
 
-        parsed_result = locations_adapter.parse(facility[0], params[:id])
+        parsed_result = locations_adapter.parse(facility.first, params[:id])
         render json: Mobile::V0::LocationSerializer.new(parsed_result)
       end
 
