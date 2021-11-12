4b6295d67b12ebce25ec3c5e7f9672e33d3740e7
Darius D <ddzien2@gmail.com>
2021-11-12 14:44:19 -0600

    Upload unit test metrics to BigQuery in CI (#8433)
    * Push unit test data to BigQuery
    
    * Linting
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Remove test environment variable
    
    * WIP
    
    * Use plain Ruby file
    
    * Rubocop fix
    
    * Rubocop fix
    
    * Restructure table schema
    
    * Rubocop fixes
    
    * Add new line
    
    * Add time column
    
    * Rubocop fix
    
    * Rubocop fix
    
    * Fix file path

---
+++ rakelib/test_stats_to_bigquery.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'google/cloud/bigquery'
+require 'nokogiri'
+
+TABLE = 'rspec_statistics'
+DATASET = 'vsp_testing_tools'
+
+def read_files(total_tests = 0, total_failures = 0, total_skipped = 0, total_time = 0)
+  date = 0
+
+  Dir['Test Results/*.xml'].each_with_index do |results_file, index|
+    doc = File.open(results_file) { |f| Nokogiri::XML(f) }
+    date = doc.xpath('//testsuite/@timestamp').to_s.split('T')[0] if index.zero?
+    time = doc.xpath('//testsuite/@time').to_s.to_i
+    total_time = time if time > total_time
+    total_tests += doc.xpath('//testsuite/@tests').to_s.to_i
+    total_failures += doc.xpath('//testsuite/@failures').to_s.to_i
+    total_skipped += doc.xpath('//testsuite/@skipped').to_s.to_i
+  end
+
+  [{
+    date: date,
+    total_tests: total_tests,
+    total_failures: total_failures,
+    total_skipped: total_skipped,
+    total_time: total_time
+  }]
+end
+
+def upload_test_stats
+  bigquery = Google::Cloud::Bigquery.new
+  dataset = bigquery.dataset DATASET, skip_lookup: true
+  table = dataset.table TABLE, skip_lookup: true
+
+  # rubocop:disable Rails/SkipsModelValidations
+  response = table.insert read_files
+  # rubocop:enable Rails/SkipsModelValidations
+
+  if response.success?
+    puts 'Uploaded RSpec data to BigQuery.'
+  else
+    raise('Failed to upload RSpec data to BigQuery.')
+  end
+end
+
+upload_test_stats
