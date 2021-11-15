08389e227d2c68532d59d1483227ef7c4768f649
Eric Boehs <eric@boehs.com>
2021-11-15 12:13:46 -0600

    KMS Rotator: Fix HealthQuest callback skipping (#8445)

--- app/workers/kms_record_rotation_job.rb
+++ app/workers/kms_record_rotation_job.rb
@@ -4,11 +4,19 @@ class KmsRecordRotationJob
   include Sidekiq::Worker
 
   def perform(models = ApplicationRecord.descendants_using_encryption.map(&:name))
-    HealthQuest::QuestionnaireResponse.skip_callback :save, :before, :set_user_demographics, raise: false do
+    skip_hq_callback do
       models.map(&:constantize).each do |m|
         Rails.logger.debug "Re-encrypting #{m.where(encrypted_kms_key: nil).count} #{m} records."
         Lockbox.rotate m.where(encrypted_kms_key: nil), attributes: m.lockbox_attributes.keys
       end
     end
+  end
+
+  private
+
+  def skip_hq_callback
+    HealthQuest::QuestionnaireResponse.skip_callback :save, :before, :set_user_demographics, raise: false
+    yield
+    HealthQuest::QuestionnaireResponse.set_callback :save, :before, :set_user_demographics, raise: false
   end
 end
