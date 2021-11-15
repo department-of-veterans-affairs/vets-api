af853cd19a6f0b49e71d375af6e7e498163a8564
Drew Fisher <142862+cilestin@users.noreply.github.com>
2021-11-15 15:13:42 -0800

    API-11252 Make match_pdf work with checkboxes (#8429)
    * API-11252 Make match_pdf work with checkboxes

--- modules/appeals_api/spec/support/pdf_matcher.rb
+++ modules/appeals_api/spec/support/pdf_matcher.rb
@@ -4,18 +4,20 @@ require 'rspec/expectations'
 
 RSpec::Matchers.define :match_pdf do |expected|
   match do |actual|
+    normalize_page_data = lambda do |pages|
+      # Write-to-write, the xobject keys & order may be different, so get the raw content & sort it
+      # Using xobjects rather than page text lets us check against non-text fields like checkboxes, etc.
+      pages.map { |pg| pg.xobjects.values.map(&:unfiltered_data).sort }
+    end
     actual_reader = PDF::Reader.new(actual)
-    actual_pages = actual_reader.pages.size
-    actual_text = actual_reader.pages.map(&:text)
+    actual_data = normalize_page_data.call actual_reader.pages
 
     expected_reader = PDF::Reader.new(expected)
-    expected_pages = expected_reader.pages.size
-    expected_text = expected_reader.pages.map(&:text)
+    expected_data = normalize_page_data.call expected_reader.pages
 
-    # Check both page count and text content in case there are extraneous blank pages
-    actual_pages == expected_pages && actual_text == expected_text
+    actual_reader.page_count == expected_reader.page_count && actual_data == expected_data
   end
   failure_message do |actual|
-    "expected that content of #{actual} matches content of #{expected}"
+    "expected content of #{actual} to match #{expected}"
   end
 end
