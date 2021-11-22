7651b107234f746c1282a9b2a3600232bb7e22f6
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-11-22 13:34:23 -0500

    Revert "Add null check constraint (#8492)" (#8508)
    This reverts commit 6769d26cfb4cb39ad87f2bf96edf8d051657a131.

--- db/migrate/20211119153822_validate_validate_add_not_null_constraint_to_vet_rep_table.rb
+++
@@ -1,5 +0,0 @@
-class ValidateValidateAddNotNullConstraintToVetRepTable < ActiveRecord::Migration[6.1]
-  def change
-    validate_check_constraint :veteran_representatives, name: "veteran_representatives_representative_id_null"
-  end
-end
