c6a73e3c42d81a8acedbf0b38bdde57041830ca5
kanchanasuriya <89944361+kanchanasuriya@users.noreply.github.com>
2021-11-30 08:35:01 -0800

    32875 Metrics and Swagger doc for pre_check_ins/GET endpoint (#8526)
    Co-authored-by: kanchanasuriya <ksuriyamoorthy@kindsys.us>

--- modules/check_in/config/initializers/statsd.rb
+++ modules/check_in/config/initializers/statsd.rb
@@ -6,11 +6,14 @@ unless Rails.env.test?
     # duration/success/fail of GET, POST calls for controllers
     CheckIn::V2::SessionsController.extend(StatsD::Instrument)
     CheckIn::V2::PatientCheckInsController.extend(StatsD::Instrument)
+    CheckIn::V2::PreCheckInsController.extend(StatsD::Instrument)
     %i[show create].each do |method|
       CheckIn::V2::SessionsController.statsd_measure method, "api.check_in.v2.sessions.#{method}.measure"
       CheckIn::V2::SessionsController.statsd_count_success method, "api.check_in.v2.sessions.#{method}.count"
       CheckIn::V2::PatientCheckInsController.statsd_measure method, "api.check_in.v2.checkins.#{method}.measure"
       CheckIn::V2::PatientCheckInsController.statsd_count_success method, "api.check_in.v2.checkins.#{method}.count"
+      CheckIn::V2::PreCheckInsController.statsd_measure method, "api.check_in.v2.pre_checkins.#{method}.measure"
+      CheckIn::V2::PreCheckInsController.statsd_count_success method, "api.check_in.v2.pre_checkins.#{method}.count"
     end
 
     # Measure the count/duration of GET/POST calls for LoROTA/CHIP services
