e8b71ec8453d013886a6ae443a95879c6272c82b
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-11-30 10:50:55 -0500

    Add updates to KmsEncryptionVerificationJob (#8539)
    * add method to check for nil attributes
    * add approach for better logging and catch error instead
    * add in_batches for timeout error

--- app/workers/kms_encryption_verification_job.rb
+++ app/workers/kms_encryption_verification_job.rb
@@ -7,9 +7,11 @@ class KmsEncryptionVerificationJob
   def perform(models = ApplicationRecord.descendants_using_encryption.map(&:name))
     corrupted_records = models.map(&:constantize).flat_map do |model|
       attributes = model.lockbox_attributes.keys
-      model.where.not(encrypted_kms_key: nil).flat_map do |record|
-        attributes.flat_map do |attribute|
-          record unless can_decrypt?(record, attribute)
+      model.where.not(encrypted_kms_key: nil).in_batches.flat_map do |relation|
+        relation.flat_map do |record|
+          attributes.flat_map do |attribute|
+            record unless can_decrypt?(record, attribute)
+          end
         end
       end
     end.compact
@@ -24,6 +26,10 @@ class KmsEncryptionVerificationJob
                                      context: { model_name: record.class.name, model_id: record.id })
     lockbox = Lockbox.new(key: decrypted_data_key, encode: true)
     lockbox.decrypt record.public_send("#{attribute}_ciphertext")
+  rescue NoMethodError
+    Rails.logger.error("Record with nil attr but has encrypted_kms_key:
+                        #{record.class.name} - id: #{record.id} - attribute: #{attribute}")
+    false
   rescue Lockbox::DecryptionError
     Rails.logger.error("Record re-encryption unsuccessful model:
                         #{record.class.name} - id: #{record.id} - attribute: #{attribute}")
