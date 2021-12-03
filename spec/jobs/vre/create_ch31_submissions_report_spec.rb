6a936ab997a8799926ae59b251fe7e46e3747c8e
Trevor Bosaw <bosawt@gmail.com>
2021-12-03 07:27:40 -0800

    [8549] Fixing failing create ch31 submissions spec (#8550)
    * [8549] Fixing failing create ch31 submissions spec
    
    * removed explicit created_at attr's, since we're switching to timecop
    
    * clean up lets
    
    * reorg spec
    
    * remove let bangs for now
    
    * checkout worker file from master, I think it's fine to use,
    just the spec is failing
    
    * timecop freeze creation time of factories and subject execution so that all times are explcit
    
    Co-authored-by: Trevor Bosaw <trevor.bosaw@oddball.io>
    Co-authored-by: Kevin Musiorski <kevin.musiorski@gcio.com>

--- spec/jobs/vre/create_ch31_submissions_report_spec.rb
+++ spec/jobs/vre/create_ch31_submissions_report_spec.rb
@@ -3,43 +3,55 @@
 require 'rails_helper'
 
 describe VRE::CreateCh31SubmissionsReport do
-  let!(:vre_claim1) do
-    create :veteran_readiness_employment_claim, regional_office: '377 - San Diego', created_at: Time.zone.now.yesterday
+  let(:zone) { 'Eastern Time (US & Canada)' }
+  let(:time) { ActiveSupport::TimeZone[zone].parse('2021-11-15 00:00:00') }
+
+  let(:vre_claim1) do
+    Timecop.freeze(time) { create :veteran_readiness_employment_claim, regional_office: '377 - San Diego' }
   end
-  let!(:vre_claim2) do
-    create :veteran_readiness_employment_claim, regional_office: '349 - Waco', created_at: Time.zone.now.yesterday
+
+  let(:vre_claim2) do
+    Timecop.freeze(time) { create :veteran_readiness_employment_claim, regional_office: '349 - Waco' }
   end
-  let!(:vre_claim3) do
-    create :veteran_readiness_employment_claim, regional_office: '351 - Muskogee', created_at: Time.zone.now.yesterday
+
+  let(:vre_claim3) do
+    Timecop.freeze(time) { create :veteran_readiness_employment_claim, regional_office: '351 - Muskogee' }
   end
-  let!(:vre_claim4) do
-    create :veteran_readiness_employment_claim, regional_office: '377 - San Diego',
-                                                created_at: '2021-11-15 11:59:59 -0500'
+
+  let(:vre_claim4) do
+    Timecop.freeze(time) { create :veteran_readiness_employment_claim, regional_office: '377 - San Diego' }
   end
-  let!(:vre_claim5) do
-    create :veteran_readiness_employment_claim, regional_office: '349 - Waco', created_at: '2021-11-15 11:59:59 -0500'
+
+  let(:vre_claim5) do
+    Timecop.freeze(time) { create :veteran_readiness_employment_claim, regional_office: '349 - Waco' }
   end
-  let!(:vre_claim6) do
-    create :veteran_readiness_employment_claim, regional_office: '351 - Muskogee',
-                                                created_at: '2021-11-15 11:59:59 -0500'
+
+  let(:vre_claim6) do
+    Timecop.freeze(time) { create :veteran_readiness_employment_claim, regional_office: '351 - Muskogee' }
   end
 
   describe '#perform' do
+    subject { described_class.new.perform(sidekiq_scheduler_args, specific_date) }
+
+    before { expect(Ch31SubmissionsReportMailer).to receive(:build).with(submitted_claims).and_call_original }
+
     context 'passed sidekiq_scheduler_args' do
+      let(:sidekiq_scheduler_args) { { 'scheduled_at' => Time.zone.now.to_i } }
+      let(:specific_date) { nil }
+      let(:submitted_claims) { [vre_claim2, vre_claim3, vre_claim1] }
+
       it 'sparks mailer with claims sorted by Regional Office' do
-        submitted_claims = [vre_claim2, vre_claim3, vre_claim1]
-        sidekiq_scheduler_args = { 'scheduled_at' => Time.zone.now.to_i }
-        expect(Ch31SubmissionsReportMailer).to receive(:build).with(submitted_claims).and_call_original
-        described_class.new.perform(sidekiq_scheduler_args)
+        Timecop.freeze(ActiveSupport::TimeZone[zone].parse('2021-11-16 00:00:01')) { subject }
       end
     end
 
     context 'passed specific date in YYYY-MM-DD format' do
+      let(:sidekiq_scheduler_args) { {} }
+      let(:specific_date) { '2021-11-15' }
+      let(:submitted_claims) { [vre_claim5, vre_claim6, vre_claim4] }
+
       it 'sparks mailer with claims sorted by Regional Office' do
-        submitted_claims = [vre_claim5, vre_claim6, vre_claim4]
-        specific_date = '2021-11-15'
-        expect(Ch31SubmissionsReportMailer).to receive(:build).with(submitted_claims).and_call_original
-        described_class.new.perform({}, specific_date)
+        subject
       end
     end
   end
