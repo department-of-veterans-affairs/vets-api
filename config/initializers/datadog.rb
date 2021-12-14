c407c741856514536fd54dfe95f7c65d6e6c6246
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-12-14 11:41:01 -0500

    Revert "Add ddtrace gem to enable APM in Datadog (#8659)" (#8673)
    This reverts commit 90572da938eff5bc2cf9b9a6df72139f1117ab80.

--- config/initializers/datadog.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'ddtrace'
-
-# @see https://docs.datadoghq.com/tracing/setup_overview/setup/ruby
-Datadog.configure do |c|
-  c.use :rails, { log_injection: true }
-  c.use :sidekiq, { tag_args: true }
-end
