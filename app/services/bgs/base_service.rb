d734309c2ab3862c4204c16dc721395c0dee9fef
Kathleen Crawford <53828142+kathleencrawford@users.noreply.github.com>
2021-12-16 09:48:53 -0800

    Limit External Key for BGS Service (#8676)
    * limiting external key
    
    * adding base service spec
    
    Co-authored-by: Travis Hilton <53486698+thilton-oddball@users.noreply.github.com>

--- app/services/bgs/base_service.rb
+++ app/services/bgs/base_service.rb
@@ -14,6 +14,17 @@ module BGS
     def initialize_service
       external_key = @user.common_name || @user.email
 
+      if external_key.length > 39
+        external_key = external_key[0, 39]
+
+        # this is temp logging
+        if @user.common_name.present? && @user.common_name.length > 39
+          log_message_to_sentry('common name greater than 39', :info, {}, { team: 'vfs-ebenefits' })
+        elsif @user.email.present? && @user.email.length > 39
+          log_message_to_sentry('email greater than 39', :info, {}, { team: 'vfs-ebenefits' })
+        end
+      end
+
       BGS::Services.new(
         external_uid: @user.icn,
         external_key: external_key
