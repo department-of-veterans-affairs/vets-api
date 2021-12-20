60f4c9f3abe7c47888fcdedcc620991d10ef9978
Kevin Musiorski <1021154+RudyOnRails@users.noreply.github.com>
2021-12-20 10:55:11 -0600

    adjust log level to :warn in order to retain valuable warnings, but also omit :info (#8727)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :error
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = {
