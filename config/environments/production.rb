82d92409c40a578e3b1fd4a5825202f478e89604
Michael R. Fleet <f1337@users.noreply.github.com>
2022-01-05 13:20:09 -0500

    revert log level to info, hope it doesn't burn (#8804)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :warn
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = {
