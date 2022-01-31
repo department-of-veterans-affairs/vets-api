bd3c9c6214e02047b583cb37229ae8e28cfc92a4
Kevin Cranmer <51764175+KevinCranmer@users.noreply.github.com>
2022-01-31 09:47:15 -0600

    Api 10177 add form tag (#8982)
    * Logging for the action_mailer
    
    * Adding Processing column to daily report
    
    * Lint adjustments
    
    * Lint adjustments
    
    * Adding form tag "526" to 21-526EZ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.logger = Logger.new('./log/mailer.log')
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
