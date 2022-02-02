5b02582b9e23d467cd3614a0df3109f977292822
Yoom Lam <yoom@navapbc.com>
2022-02-02 13:52:24 -0600

    Refactor HypertensionSpecialIssueManager to be more rubyist (#8987)
    * refactor to be more rubyist
    * make most methods private

--- app/services/fast_track/hypertension_special_issue_manager.rb
+++ app/services/fast_track/hypertension_special_issue_manager.rb
@@ -9,26 +9,32 @@ module FastTrack
     end
 
     def add_special_issue
-      data = JSON.parse(submission.form_json, symbolize_names: true)
-      disabilities = data[:form526][:form526][:disabilities]
-      data[:form526][:form526][:disabilities] = add_rrd_to_disabilities(disabilities)
-      submission.update!(form_json: JSON.dump(data))
+      disabilities.each do |disability|
+        add_rrd_code(disability) if hypertension_increase?(disability)
+      end
+      submission.update!(form_json: JSON.dump(submission_data))
     end
 
-    def add_rrd_to_disabilities(disabilities)
-      disabilities.each do |da|
-        add_rrd(da) if da[:diagnosticCode] == 7101 && da[:disabilityActionType].downcase == 'increase'
-      end
-      disabilities
+    private
+
+    def submission_data
+      @submission_data ||= JSON.parse(submission.form_json, symbolize_names: true)
     end
 
-    def add_rrd(disability)
-      rrd_code = 'RRD'
-      if disability[:specialIssues].blank?
-        disability[:specialIssues] = [rrd_code]
-      elsif !disability[:specialIssues].include? rrd_code
-        disability[:specialIssues].append(rrd_code)
-      end
+    def disabilities
+      @disabilities ||= submission_data[:form526][:form526][:disabilities]
+    end
+
+    def hypertension_increase?(disability)
+      disability[:diagnosticCode] == 7101 && disability[:disabilityActionType].downcase == 'increase'
+    end
+
+    RRD_CODE = 'RRD'
+
+    # Must return an array containing special string codes for EVSS
+    def add_rrd_code(disability)
+      disability[:specialIssues] ||= []
+      disability[:specialIssues].append(RRD_CODE) unless disability[:specialIssues].include?(RRD_CODE)
       disability
     end
   end
