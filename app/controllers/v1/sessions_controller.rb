bc5bc3b215ed7966ce2d827b5a552b5d1ab7d8bc
John Bramley <bramleyjl@gmail.com>
2022-02-18 09:59:48 -0700

    sets force_authn to false on staging SAML requests (#9057)
    Co-authored-by: Joe Niquette <71290526+joeniquette@users.noreply.github.com>

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -358,7 +358,7 @@ module V1
     end
 
     def url_service(force_authn = true)
-      force_authn = false unless %w[production staging].include?(Settings.vsp_environment)
+      force_authn = false unless Settings.vsp_environment == 'production'
       @url_service ||= SAML::PostURLService.new(saml_settings(force_authn: force_authn),
                                                 session: @session_object,
                                                 user: current_user,
