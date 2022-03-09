department-of-veterans-affairs/vets-api

commit 4531e2f6566aebb5be12a0f7fd77a9f503ae3642

RRD: prep for new contention type - part 1 (#9206)
* use build instead of create to speed up tests
* DRY up code to use trait :hypertension_claim_for_increase
* fix updated_at typo
* use new refactored start_evss_submission_job
* remove unused SubmitForm526JobStatusesError
* add Sidekiq::Batch comments
* rename to rrd_complete_handler
* DRY up Form526ConfirmationEmailJob

app/models/form526_submission.rb
@@ -23,7 +23,7 @@ class Form526Submission < ApplicationRecord
   #     workflow complete.
   # @!attribute created_at
   #   @return [Timestamp] created at date.
-  # @!attribute workflow_complete
+  # @!attribute updated_at
   #   @return [Timestamp] updated at date.
   #
   has_kms_key
@@ -52,7 +52,7 @@ class Form526Submission < ApplicationRecord
       workflow_batch = Sidekiq::Batch.new
       workflow_batch.on(
         :success,
-        'Form526Submission#start_evss_submission',
+        'Form526Submission#rrd_complete_handler',
         'submission_id' => id
       )
       job_ids = workflow_batch.jobs do
@@ -60,16 +60,23 @@ class Form526Submission < ApplicationRecord
       end
       job_ids.first
     else
-      start_evss_submission(nil, { 'submission_id' => id })
+      start_evss_submission_job
     end
   rescue => e
     Rails.logger.error 'The fast track was skipped due to the following error ' \
-                       " and start_evss_submission wass called: #{e}"
-    start_evss_submission(nil, { 'submission_id' => id })
+                       " and start_evss_submission_job is being called: #{e}"
+    start_evss_submission_job
   end
 
   def rrd_process_selector
     @rrd_process_selector ||= RapidReadyForDecision::ProcessorSelector.new(self)
+  end
+
+  # Afer RapidReadyForDecision is complete, this method is
+  # called by Sidekiq::Batch as part of the Form 526 submission workflow
+  def rrd_complete_handler(_status, options)
+    submission = Form526Submission.find(options['submission_id'])
+    submission.start_evss_submission_job
   end
 
   # Kicks off a 526 submit workflow batch. The first step in a submission workflow is to submit
@@ -78,15 +85,13 @@ class Form526Submission < ApplicationRecord
   #
   # @return [String] the job id of the first job in the batch, i.e the 526 submit job
   #
-
-  def start_evss_submission(_status, options)
-    submission = Form526Submission.find(options['submission_id'])
-    id = submission.id
+  def start_evss_submission_job
     workflow_batch = Sidekiq::Batch.new
     workflow_batch.on(
       :success,
       'Form526Submission#perform_ancillary_jobs_handler',
       'submission_id' => id,
+      # Call get_first_name while the temporary User record still exists
       'first_name' => get_first_name
     )
     job_ids = workflow_batch.jobs do
@@ -96,7 +101,7 @@ class Form526Submission < ApplicationRecord
     job_ids.first
   end
 
-  # Runs start_evss_submission but first looks to see if the veteran has BIRLS IDs that previous start
+  # Runs start_evss_submission_job but first looks to see if the veteran has BIRLS IDs that previous start
   # attempts haven't used before (if so, swaps one of those into auth_headers).
   # If all BIRLS IDs for a veteran have been tried, does nothing and returns nil.
   # Note: this assumes that the current BIRLS ID has been used (that `start` has been attempted once).
@@ -113,7 +118,7 @@ class Form526Submission < ApplicationRecord
 
     self.birls_id = untried_birls_id
     save!
-    start_evss_submission(nil, { 'submission_id' => id })
+    start_evss_submission_job
   rescue => e
     # 1) why have the 'silence_errors_and_log_to_sentry' option? (why not rethrow the error?)
     # This method is primarily intended to be triggered by a running Sidekiq job that has hit a dead end
@@ -245,6 +250,7 @@ class Form526Submission < ApplicationRecord
     @auth_headers_hash = nil # reset cache
   end
 
+  # Called by Sidekiq::Batch as part of the Form 526 submission workflow
   # The workflow batch success handler
   #
   # @param _status [Sidekiq::Batch::Status] the status of the batch
@@ -259,8 +265,6 @@ class Form526Submission < ApplicationRecord
   def jobs_succeeded?
     a_submit_form_526_job_succeeded? && all_other_jobs_succeeded_if_any?
   end
-
-  class SubmitForm526JobStatusesError < StandardError; end
 
   def a_submit_form_526_job_succeeded?
     submit_form_526_job_statuses = form526_job_statuses.where(job_class: SUBMIT_FORM_526_JOB_CLASSES).order(:updated_at)
@@ -302,6 +306,7 @@ class Form526Submission < ApplicationRecord
     end
   end
 
+  # Called by Sidekiq::Batch as part of the Form 526 submission workflow
   # Checks if all workflow steps were successful and if so marks it as complete.
   #
   # @param _status [Sidekiq::Batch::Status] the status of the batch
@@ -310,34 +315,12 @@ class Form526Submission < ApplicationRecord
   def workflow_complete_handler(_status, options)
     submission = Form526Submission.find(options['submission_id'])
     if submission.jobs_succeeded?
-      submission.send_form526_confirmation_email(options['first_name'])
+      Form526ConfirmationEmailJob.perform_async(personalization_parameters(options['first_name']))
       submission.workflow_complete = true
       submission.save
     else
-      submission.send_form526_submission_failed_email(options['first_name'])
+      Form526SubmissionFailedEmailJob.perform_async(personalization_parameters(options['first_name']))
     end
-  end
-
-  def send_form526_confirmation_email(first_name)
-    email_address = form['form526']['form526']['veteran']['emailAddress']
-    personalization_parameters = {
-      'email' => email_address,
-      'submitted_claim_id' => submitted_claim_id,
-      'date_submitted' => created_at.strftime('%B %-d, %Y %-l:%M %P %Z').sub(/([ap])m/, '\1.m.'),
-      'first_name' => first_name
-    }
-    Form526ConfirmationEmailJob.perform_async(personalization_parameters)
-  end
-
-  def send_form526_submission_failed_email(first_name)
-    email_address = form['form526']['form526']['veteran']['emailAddress']
-    personalization_parameters = {
-      'email' => email_address,
-      'submitted_claim_id' => submitted_claim_id,
-      'date_submitted' => created_at.strftime('%B %-d, %Y %-l:%M %P %Z').sub(/([ap])m/, '\1.m.'),
-      'first_name' => first_name
-    }
-    Form526SubmissionFailedEmailJob.perform_async(personalization_parameters)
   end
 
   def bdd?
@@ -345,6 +328,15 @@ class Form526Submission < ApplicationRecord
   end
 
   private
+
+  def personalization_parameters(first_name)
+    {
+      'email' => form['form526']['form526']['veteran']['emailAddress'],
+      'submitted_claim_id' => submitted_claim_id,
+      'date_submitted' => created_at.strftime('%B %-d, %Y %-l:%M %P %Z').sub(/([ap])m/, '\1.m.'),
+      'first_name' => first_name
+    }
+  end
 
   def submit_uploads
     # Put uploads on a one minute delay because of shared workload with EVSS

app/workers/rapid_ready_for_decision/disability_compensation_job.rb
@@ -16,8 +16,8 @@ module RapidReadyForDecision
 
     sidekiq_retries_exhausted do |msg, _ex|
       submission_id = msg['args'].first
-      submission = Form526Submission.new
-      submission.start_evss_submission(nil, { 'submission_id' => submission_id })
+      submission = Form526Submission.find(submission_id)
+      submission.start_evss_submission_job
     end
 
     STATSD_KEY_PREFIX = 'worker.fast_track.disability_compensation_job'

spec/factories/form526_submissions.rb
@@ -2,11 +2,13 @@
 
 FactoryBot.define do
   factory :form526_submission do
-    user_uuid { SecureRandom.uuid }
+    transient do
+      user { create(:disabilities_compensation_user) }
+    end
+    user_uuid { user.uuid }
     saved_claim { create(:va526ez) }
     submitted_claim_id { nil }
     auth_headers_json do
-      user = build(:disabilities_compensation_user)
       EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h).to_json
     end
     form_json do

spec/jobs/rapid_ready_for_decision/disability_compensation_job_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe RapidReadyForDecision::DisabilityCompensationJob, type: :worker d
            submitted_claim_id: '600130094')
   end
 
-  let(:user_full_name) { user.full_name_normalized }
   let(:mocked_observation_data) do
     [{ effectiveDateTime: "#{Time.zone.today.year}-06-21T02:42:52Z",
        practitioner: 'DR. THOMAS359 REYNOLDS206 PHD',

spec/models/form526_submission_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Form526Submission do
     context 'when it is all claims' do
       it 'queues an all claims job' do
         expect do
-          subject.start_evss_submission(nil, 'submission_id' => subject.id)
+          subject.start_evss_submission_job
         end.to change(EVSS::DisabilityCompensationForm::SubmitForm526AllClaim.jobs, :size).by(1)
       end
     end
@@ -65,11 +65,11 @@ RSpec.describe Form526Submission do
         it_behaves_like '#start_evss_submission'
 
         context 'an exception is raised in the start method' do
-          it 'runs start_evss_submission' do
+          it 'calls start_evss_submission_job' do
             allow(Sidekiq::Batch).to receive(:new).and_raise(NoMethodError)
 
             expect(Rails.logger).to receive(:error)
-            expect(form_for_hypertension).to receive(:start_evss_submission)
+            expect(form_for_hypertension).to receive(:start_evss_submission_job)
             form_for_hypertension.start
           end
         end
@@ -100,7 +100,7 @@ RSpec.describe Form526Submission do
     end
   end
 
-  describe '#start_evss_submission' do
+  describe '#start_evss_submission_job' do
     it_behaves_like '#start_evss_submission'
   end
 

spec/services/rapid_ready_for_decision/hypertension_special_issue_manager_spec.rb
@@ -4,23 +4,7 @@ require 'rails_helper'
 require 'rapid_ready_for_decision/disability_compensation_job'
 
 RSpec.describe RapidReadyForDecision::HypertensionSpecialIssueManager do
-  let(:form526_submission) do
-    Form526Submission.create(
-      user_uuid: user.uuid,
-      saved_claim_id: saved_claim.id,
-      auth_headers_json: auth_headers.to_json,
-      form_json: form_json
-    )
-  end
-
-  let(:user) { create(:disabilities_compensation_user) }
-  let(:auth_headers) do
-    EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h)
-  end
-  let(:saved_claim) { FactoryBot.create(:va526ez) }
-  let(:form_json) do
-    File.read('spec/support/disability_compensation_form/submissions/only_526_hypertension.json')
-  end
+  let(:form526_submission) { create(:form526_submission, :hypertension_claim_for_increase) }
 
   def form526_hash(form)
     JSON.parse(form, symbolize_names: true)[:form526][:form526]

spec/services/rapid_ready_for_decision/processor_selector_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe RapidReadyForDecision::ProcessorSelector do
   subject { described_class.new(submission) }
 
-  let(:submission) { create(:form526_submission) }
+  let(:submission) { build(:form526_submission) }
 
   describe '#processor_class' do
     context 'when given non-RRD-applicable claim submission' do
@@ -16,7 +16,7 @@ RSpec.describe RapidReadyForDecision::ProcessorSelector do
     end
 
     context 'when given single-issue hypertension claim for increase submission' do
-      let(:submission) { create(:form526_submission, :hypertension_claim_for_increase) }
+      let(:submission) { build(:form526_submission, :hypertension_claim_for_increase) }
 
       it 'returns DisabilityCompensationJob' do
         expect(subject.rrd_applicable?).to eq true

commit 1edf1024067728478f510e008e2e95a1f38d59af

Fix TypeError bug caused by #9206 (#9305)
* Fix TypeError bug caused by #9206

* Make personalization_parameters public

Co-authored-by: Yoom Lam <yoom@navapbc.com>

app/models/form526_submission.rb
@@ -314,20 +314,19 @@ class Form526Submission < ApplicationRecord
   #
   def workflow_complete_handler(_status, options)
     submission = Form526Submission.find(options['submission_id'])
+    params = submission.personalization_parameters(options['first_name'])
     if submission.jobs_succeeded?
-      Form526ConfirmationEmailJob.perform_async(personalization_parameters(options['first_name']))
+      Form526ConfirmationEmailJob.perform_async(params)
       submission.workflow_complete = true
       submission.save
     else
-      Form526SubmissionFailedEmailJob.perform_async(personalization_parameters(options['first_name']))
+      Form526SubmissionFailedEmailJob.perform_async(params)
     end
   end
 
   def bdd?
     form.dig('form526', 'form526', 'bddQualified') || false
   end
-
-  private
 
   def personalization_parameters(first_name)
     {
@@ -337,6 +336,8 @@ class Form526Submission < ApplicationRecord
       'first_name' => first_name
     }
   end
+
+  private
 
   def submit_uploads
     # Put uploads on a one minute delay because of shared workload with EVSS