f9e2303cff12b26228f07beefe5401318cd091d6
YANG YANG <yangyang@navapbc.com>
2022-03-29 12:00:47 -0400

    RRD: Update claim established message for pending EP offramp (#9480)
    * RRD: Update claim established message for pending EP offramp
    
    * Move rrd_status into a new method on Form526BaseJob
    
    * Simplify test setup
    
    * Address lint warning

--- app/mailers/hypertension_fast_track_pilot_mailer.rb
+++ app/mailers/hypertension_fast_track_pilot_mailer.rb
@@ -5,22 +5,14 @@ class HypertensionFastTrackPilotMailer < ApplicationMailer
     @submission = submission
     @disability = submission.form.dig('form526', 'form526', 'disabilities')&.first
     @disability_code = @disability['diagnosticCode'] if @disability
-    @rrd_claim_processed = RapidReadyForDecision::Form526BaseJob.rrd_claim_processed?(submission)
-    @bp_readings_count = submission.form.dig('rrd_med_stats', 'bp_readings_count') || 'N/A'
-
-    subject =
-      if @rrd_claim_processed
-        'RRD claim - Processed'
-      else
-        'RRD claim - Not processed'
-      end
-    subject += " - #{@disability_code}"
+    @rrd_status = RapidReadyForDecision::Form526BaseJob.rrd_status(submission)
+    @bp_readings_count = submission.form.dig('rrd_metadata', 'med_stats', 'bp_readings_count') || 'N/A'
 
     template = File.read('app/mailers/views/hypertension_fast_track_pilot_mailer.erb.html')
 
     mail(
       to: Settings.rrd.event_tracking.recipients,
-      subject: subject,
+      subject: "RRD claim - #{@rrd_status.to_s.humanize} - #{@disability_code}",
       body: ERB.new(template).result(binding)
     )
   end
