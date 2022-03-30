e602a6c12ca7c13529b0c00f0a2c35cf733e31a1
Josh <josh.scanish@oddball.io>
2022-03-30 16:08:16 -0400

    Mhv api service object 35657 (#9459)
    * Create service file
    
    * Add urls to settings
    
    * Start setting up configuration file
    
    * Find correlation_id
    
    * Fix rescue
    
    * Set up service to determine eligibility
    
    * Use existing correlation_id if we have one
    
    * Fix rubocop issue
    
    * Add service spec file
    
    * Rename files
    
    * Format change to fix strange rspec issue
    
    * Fix typo
    
    * Two tests pass
    
    * Add more specs
    
    * Attempt to fix connection issue
    
    * Fix connection and rubocop, add more tests
    
    * All tests pass
    
    * Fix rubocop issues
    
    * Rename method
    
    * Return hash of information as opposed to eligible value
    
    * Fix tests
    
    * Add code to result of successful service call
    
    * Add comments
    
    * Create redis backed model for identity data
    
    * Minor change
    
    * Create redis backed model for identity data
    
    * Add user_uuid, change ttl
    
    * Remove mhv identity model. Will be handled in different pull request
    
    * Add user_uuid to success response, fix tests
    
    * Create spec file
    
    * Create factory
    
    * Add validation tests
    
    * Fix settings related issues
    
    * Adjust url settings
    
    * Consolidate all actions into single service, update specs
    
    * Simplify mhv service. Essentially jsut an API wrapper now
    
    * Use class methods
    
    * Service to cache data if need and generate code
    
    * Add spec file
    
    * Add tests
    
    * Fix rubocop issues
    
    * Fix typo in spec
    
    * Move breakers line to first in options
    
    * Final changes and test fixes

---
+++ lib/inherited_proofing/mhv/inherited_proofing_verifier.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'inherited_proofing/mhv/service'
+
+module InheritedProofing
+  module MHV
+    class InheritedProofingVerifier
+      attr_reader :user
+
+      def initialize(user)
+        @user = user
+      end
+
+      def perform
+        return if missing_identity_doc?
+
+        cache_identity_data
+        code
+      end
+
+      private
+
+      def correlation_id
+        @correlation_id ||= begin
+          return user.mhv_correlation_id if user.mhv_correlation_id.present?
+
+          correlation_hsh = InheritedProofing::MHV::Service.get_correlation_data(user.icn)
+          correlation_hsh['correlationId']
+        end
+      end
+
+      def identity_info
+        @identity_info ||= begin
+          return {} if correlation_id.blank?
+
+          InheritedProofing::MHV::Service.get_verification_data(correlation_id)
+        end
+      end
+
+      def cache_identity_data
+        InheritedProofing::MHVIdentityData.new(user_uuid: user.uuid, code: code, data: identity_info).save!
+      end
+
+      def missing_identity_doc?
+        identity_info['identityDocumentExist'].blank?
+      end
+
+      def code
+        @code ||= SecureRandom.hex
+      end
+    end
+  end
+end
