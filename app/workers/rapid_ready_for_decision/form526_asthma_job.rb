9c8624c7b362984fb00a160e4bbc10382ddac3ff
Yoom Lam <yoom@navapbc.com>
2022-03-30 10:28:57 -0500

    RRD: in notifications, add medications count grouped by year and status (#9484)
    * add medications count grouped by year and status
    * add Todo comment
    * handle blank response

--- app/workers/rapid_ready_for_decision/form526_asthma_job.rb
+++ app/workers/rapid_ready_for_decision/form526_asthma_job.rb
@@ -11,12 +11,15 @@ module RapidReadyForDecision
     def assess_data(form526_submission)
       client = lighthouse_client(form526_submission)
       response = client.list_resource('medication_requests')
-      medication_requests = response.body['entry']
+      medication_requests = response.blank? ? [] : response.body['entry']
 
       body = <<~BODY
         A single-issue asthma claim for increase was detected and offramped.<br/>
         Submission ID: #{form526_submission.id}<br/>
-        Veterans Health API returned #{medication_requests.count} medication requests.
+        Veterans Health API returned #{medication_requests.count} medication requests.<br/>
+        <br/>
+        Medications by year and status:<br/>
+        #{meds_by_year_and_status_as_string(medication_requests)}<br/>
       BODY
       ActionMailer::Base.mail(
         from: ApplicationMailer.default[:from],
@@ -33,6 +36,22 @@ module RapidReadyForDecision
 
     def generate_pdf(_form526_submission, _assessed_data)
       raise 'method not implemented yet' # RuntimeError
+    end
+
+    def meds_by_year_and_status_as_string(medication_requests)
+      medication_request_stats_by_year(medication_requests).map do |year, tallies|
+        "- #{year}: #{tallies}"
+      end.join("<br/>\n")
+    rescue
+      'Error formatting medications list!'
+    end
+
+    def medication_request_stats_by_year(medication_requests)
+      # Todo later: update HypertensionMedicationRequestData so that it can be used here
+      resources = medication_requests.map { |mr| mr['resource'] }
+      resources.group_by { |mr| Date.parse(mr['authoredOn'])&.year }.transform_values do |med_requests|
+        med_requests.map { |mr| mr['status'] }.tally
+      end.sort.reverse
     end
   end
 end
