9000a4e66ba488ab68913dcd1f6aa7e70ad35e4b
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2022-04-20 13:40:31 -0600

    Api 14342 fix overlapping pdf fields (#9654)
    * Generates new fixtures with new header stamps.
    
    * Adds new method to stamper.rb to adjust the length of the header stamp based on the length of the consumer name.
    
    * Adds parameter and default value  to generate_stamp so we can adjust the size of our header stamp.
    
    * Adjusts stamper.rb to retain minimum size of 8, rather than 5.
    
    * Replaces fixtures with new stamper size and positions.
    
    * Adjusts fixtures based on header stamp changes.
    
    * Re-adjusts position and size to allow max usage of space for the middle stamp - name and ssn.
    
    * Adjusts stamp position of HLR v1.

--- lib/central_mail/datestamp_pdf.rb
+++ lib/central_mail/datestamp_pdf.rb
@@ -11,25 +11,27 @@ module CentralMail
 
     def run(settings)
       stamp_path = Common::FileHelpers.random_file_path
-      generate_stamp(stamp_path, settings[:text], settings[:x], settings[:y], settings[:text_only])
+      generate_stamp(stamp_path, settings[:text], settings[:x], settings[:y], settings[:text_only], settings[:size])
       stamp(@file_path, stamp_path)
     ensure
       Common::FileHelpers.delete_file_if_exists(stamp_path) if defined?(stamp_path)
     end
 
-    def generate_stamp(stamp_path, text, x, y, text_only)
+    # rubocop:disable Metrics/ParameterLists
+    def generate_stamp(stamp_path, text, x, y, text_only, size = 10)
       unless text_only
         text += " #{I18n.l(Time.zone.now, format: :pdf_stamp)}"
         text += ". #{@append_to_stamp}" if @append_to_stamp
       end
 
       Prawn::Document.generate(stamp_path, margin: [0, 0]) do |pdf|
-        pdf.draw_text text, at: [x, y], size: 10
+        pdf.draw_text text, at: [x, y], size: size
       end
     rescue => e
       Rails.logger.error "Failed to generate datestamp file: #{e.message}"
       raise
     end
+    # rubocop:enable Metrics/ParameterLists
 
     def stamp(file_path, stamp_path)
       out_path = "#{Common::FileHelpers.random_file_path}.pdf"
