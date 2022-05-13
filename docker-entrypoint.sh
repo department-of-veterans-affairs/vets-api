2f6e626d33c5ce81d9ac8b76bc08f52d595435da
Eric Boehs <eric@boehs.com>
2022-05-13 10:57:45 -0500

    Update Bundler to 2.3.13 (#9850)
    * Update Bundler to 2.3.13
    
    * Use BUNDLED_WITH; revert Dockerfile changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
 
+BUNDLED_WITH=$(awk '/BUNDLED WITH/{getline; print}' Gemfile.lock | xargs) gem install bundler -v "$BUNDLED_WITH"
+bundle binstubs bundler --force
 bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin" --jobs=4
 
 exec "$@"
