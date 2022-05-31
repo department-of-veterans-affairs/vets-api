c5446f9a92fe6e9865fbe4166b79b09e92c6789b
Nathan Wright <nathanbwright@users.noreply.github.com>
2022-05-31 08:32:20 -0600

    If looking up the ICN fails try adding hyphens to the user_uuid and lookup again (#9956)

--- modules/va_notify/app/models/va_notify/veteran.rb
+++ modules/va_notify/app/models/va_notify/veteran.rb
@@ -10,7 +10,21 @@ module VANotify
     end
 
     def icn
-      @icn ||= Account.lookup_by_user_uuid(user_uuid).icn
+      @icn ||= lookup_icn(user_uuid)
+    end
+
+    private
+
+    def lookup_icn(user_uuid)
+      account = Account.lookup_by_user_uuid(user_uuid) || Account.lookup_by_user_uuid(hyphenate_uuid(user_uuid))
+      account&.icn
+    end
+
+    # ID.me uuids don’t have hyphens
+    # logingov_uuids do have hyphens
+    # so we have to check both
+    def hyphenate_uuid(user_uuid)
+      "#{user_uuid[0, 8]}-#{user_uuid[8, 4]}-#{user_uuid[12, 4]}-#{user_uuid[16, 4]}-#{user_uuid[20, 12]}"
     end
   end
 end
--- modules/va_notify/spec/models/veteran_spec.rb
+++ modules/va_notify/spec/models/veteran_spec.rb
@@ -3,19 +3,62 @@
 require 'rails_helper'
 
 RSpec.describe VANotify::Veteran, type: :model do
-  describe '#initialize' do
-    it 'instantiates a claims veteran' do
-      subject = VANotify::Veteran.new(
-        first_name: 'Melvin',
-        user_uuid: 'user_uuid'
-      )
+  describe 'icn lookup' do
+    let(:user_uuid_with_hyphens) do
+      '11111111-2222-3333-4444-555555555555'
+    end
 
+    let(:user_uuid_without_hyphens) do
+      user_uuid_with_hyphens.gsub('-', '')
+    end
+
+    it 'checks with plain uuid first' do
       account = double('Account')
       allow(Account).to receive(:lookup_by_user_uuid).and_return(account)
       allow(account).to receive(:icn).and_return('icn')
 
+      subject = VANotify::Veteran.new(
+        first_name: 'Melvin',
+        user_uuid: user_uuid_without_hyphens
+      )
+
       expect(subject.first_name).to eq('Melvin')
       expect(subject.icn).to eq('icn')
+
+      expect(Account).to have_received(:lookup_by_user_uuid).with(user_uuid_without_hyphens)
+      expect(Account).not_to have_received(:lookup_by_user_uuid).with(user_uuid_with_hyphens)
+    end
+
+    it 'checks with hyphens if the first lookup returns nil' do
+      account = double('Account')
+      allow(Account).to receive(:lookup_by_user_uuid).and_return(nil, account)
+      allow(account).to receive(:icn).and_return('icn')
+
+      subject = VANotify::Veteran.new(
+        first_name: 'Melvin',
+        user_uuid: user_uuid_without_hyphens
+      )
+
+      expect(subject.first_name).to eq('Melvin')
+      expect(subject.icn).to eq('icn')
+
+      expect(Account).to have_received(:lookup_by_user_uuid).with(user_uuid_without_hyphens)
+      expect(Account).to have_received(:lookup_by_user_uuid).with(user_uuid_with_hyphens)
+    end
+
+    it 'returns nil if no matching account is found' do
+      allow(Account).to receive(:lookup_by_user_uuid).and_return(nil, nil)
+
+      subject = VANotify::Veteran.new(
+        first_name: 'Melvin',
+        user_uuid: user_uuid_without_hyphens
+      )
+
+      expect(subject.first_name).to eq('Melvin')
+      expect(subject.icn).to be nil
+
+      expect(Account).to have_received(:lookup_by_user_uuid).with(user_uuid_without_hyphens)
+      expect(Account).to have_received(:lookup_by_user_uuid).with(user_uuid_with_hyphens)
     end
   end
 end
