d8aba341dcf5626082fc348b13a558b0c150a866
Kris Pethtel <kris.pethtel@gmail.com>
2022-06-22 16:12:51 -0400

    3265 sis application controller (#10071)
    * 3265: inherit mobile application controller from signin application controller

--- app/controllers/concerns/sign_in/instrumentation.rb
+++ app/controllers/concerns/sign_in/instrumentation.rb
@@ -8,8 +8,12 @@ module SignIn
 
     def append_info_to_payload(payload)
       super
-      payload[:session] = @access_token.session_handle if @access_token.present?
+      payload[:session] = session if @access_token.present?
       payload[:user_uuid] = current_user.uuid if current_user.present?
+    end
+
+    def session
+      @access_token.session_handle
     end
   end
 end
