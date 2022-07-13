3a339eb1f67198c736961615a478182d2cc20034
Jeffrey Wallace <jw81@users.noreply.github.com>
2022-07-13 09:30:37 -0500

    [API-17189] - ClaimsApi - Update slackbot (#10208)
    * API-17189 - ClaimsApi - Update slackbot
    
    * API-17189 - ClaimsApi - switch config name to "claims_error_reporting"

--- modules/claims_api/app/workers/claims_api/claim_auditor.rb
+++ modules/claims_api/app/workers/claims_api/claim_auditor.rb
@@ -11,19 +11,18 @@ module ClaimsApi
     def perform
       return unless Settings.claims_api.audit_enabled
 
-      report_threshold = Settings.claims_api.claims_pending_reporting.threshold
-      claims = ClaimsApi::AutoEstablishedClaim.where(status: ClaimsApi::AutoEstablishedClaim::PENDING)
-                                              .where('created_at < ?', (report_threshold / 1000).seconds.ago)
+      claims = ClaimsApi::AutoEstablishedClaim.where(created_at: 1.day.ago..Time.zone.now)
+                                              .where(status: ClaimsApi::AutoEstablishedClaim::ERRORED)
+
       return unless claims.any?
 
       client = SlackNotify::Client.new(webhook_url: Settings.claims_api.slack.webhook_url,
                                        channel: '#api-benefits-claims',
                                        username: 'ClaimAuditor')
-      environment_name = Settings.claims_api.claims_pending_reporting.environment_name
+      environment_name = Settings.claims_api.claims_error_reporting.environment_name
       message = "#{claims.count} claim#{claims.count == 1 ? '' : 's'} in #{environment_name} "\
-                "surpass#{claims.count == 1 ? 'es' : ''} defined pending status threshold of "\
-                "#{report_threshold / 1000 / 60 / 60 / 24} days. Please check these claims to determine "\
-                "if any resubmissions or other fixes are required.\n\n"
+                'has an errored status.  '\
+                'Please check these claims to determine if any resubmissions or other fixes are required.\n\n'
 
       message += "*Claim ids:*\n"
       claims.each { |claim| message += "- `#{claim.id}`\n" }
