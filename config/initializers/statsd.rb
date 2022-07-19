627c1d834dc7310d2d7c1aaffd1c74c3a871ac38
John Bramley <bramleyjl@gmail.com>
2022-07-19 15:05:50 -0400

    updates statsd tags to "type" (#10272)

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -73,35 +73,35 @@ Rails.application.reloader.to_prepare do
       acrs = client_id == 'logingov' ? %w[ial1 ial2 min] : %w[loa1 loa3 min]
       acrs.each do |acr|
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_AUTHORIZE_ATTEMPT_SUCCESS, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, acr:#{acr}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, acr:#{acr}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_AUTHORIZE_ATTEMPT_FAILURE, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, acr:#{acr}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, acr:#{acr}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_CALLBACK_SUCCESS, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, acr:#{acr}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, acr:#{acr}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_CALLBACK_FAILURE, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, acr:#{acr}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, acr:#{acr}"])
       end
       %w[1 3].each do |loa|
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_TOKEN_SUCCESS, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_TOKEN_FAILURE, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_REFRESH_SUCCESS, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_REFRESH_FAILURE, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_REVOKE_SUCCESS, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_REVOKE_FAILURE, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_INTROSPECT_SUCCESS, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_INTROSPECT_FAILURE, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_REVOKE_ALL_SESSIONS_SUCCESS, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
         StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_REVOKE_ALL_SESSIONS_FAILURE, 0,
-                         tags: ["csp:#{type}, client_id:#{client_id}, loa:#{loa}"])
+                         tags: ["type:#{type}, client_id:#{client_id}, loa:#{loa}"])
       end
     end
   end
