65c6364d5c352387d7c1743a1b83a3f9fdb181e8
Nathaniel Buck <nathaniel.buck@accenturefederal.com>
2022-08-01 16:59:00 -0400

    Add New ToE (1990E) App (#10308)

--- app/models/form_profile.rb
+++ app/models/form_profile.rb
@@ -93,7 +93,7 @@ class FormProfile
   MAPPINGS = Dir[Rails.root.join('config', 'form_profile_mappings', '*.yml')].map { |f| File.basename(f, '.*') }
 
   ALL_FORMS = {
-    edu: %w[22-1990 22-1990N 22-1990E 22-1995 22-5490 22-5490E
+    edu: %w[22-1990 22-1990N 22-1990E 22-1990EMEB 22-1995 22-5490 22-5490E
             22-5495 22-0993 22-0994 FEEDBACK-TOOL 22-10203 22-1990S 22-1990EZ],
     evss: ['21-526EZ'],
     hca: ['1010ez'],
@@ -116,6 +116,7 @@ class FormProfile
     '22-1990' => ::FormProfiles::VA1990,
     '22-1990N' => ::FormProfiles::VA1990n,
     '22-1990E' => ::FormProfiles::VA1990e,
+    '22-1990EMEB' => ::FormProfiles::VA1990emeb,
     '22-1995' => ::FormProfiles::VA1995,
     '22-5490' => ::FormProfiles::VA5490,
     '22-5490E' => ::FormProfiles::VA5490e,
