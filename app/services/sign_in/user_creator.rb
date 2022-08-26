16d2f7506113f325dded2b882db9fb5a8b684ec6
Trevor Bosaw <bosawt@gmail.com>
2022-08-26 10:04:00 -0700

    [10561] Updating MPI with idme_uuid when authenticating with MHV sign in service (#10566)
    Co-authored-by: Trevor Bosaw <trevor.bosaw@oddball.io>

--- app/services/sign_in/user_creator.rb
+++ app/services/sign_in/user_creator.rb
@@ -63,8 +63,14 @@ module SignIn
     def update_mpi_record
       return unless user_identity_from_attributes.loa3?
 
-      add_mpi_user unless mpi_find_profile_response
-      update_mpi_correlation_record unless mhv_auth?
+      if mhv_auth?
+        set_user_attributes_from_mpi
+        add_mpi_user
+      elsif mpi_find_profile_response.present?
+        update_mpi_correlation_record
+      else
+        add_mpi_user
+      end
     end
 
     def add_mpi_user
@@ -118,6 +124,20 @@ module SignIn
                         code_challenge: state_payload.code_challenge,
                         user_verification_id: user_verification.id,
                         credential_email: credential_email).save!
+    end
+
+    def set_user_attributes_from_mpi
+      unless mpi_find_profile_response
+        handle_error(Errors::MHVMissingMPIRecordError,
+                     'No MPI Record for MHV Account',
+                     Constants::ErrorCode::GENERIC_EXTERNAL_ISSUE)
+      end
+      user_identity_from_attributes.first_name = mpi_find_profile_response.given_names.first
+      user_identity_from_attributes.last_name = mpi_find_profile_response.family_name
+      user_identity_from_attributes.birth_date = mpi_find_profile_response.birth_date
+      user_identity_from_attributes.ssn = mpi_find_profile_response.ssn
+      user_identity_from_attributes.icn = mpi_find_profile_response.icn
+      user_identity_from_attributes.mhv_icn = mpi_find_profile_response.icn
     end
 
     def user_identity_from_attributes
