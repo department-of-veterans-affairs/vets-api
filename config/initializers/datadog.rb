5bae584df55c55e307f5bc1fb8d74385aca02a4d
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-08-31 09:01:34 -0400

    test commenting this out
    Datadog in k8s uses the socat proxy + unix socket

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -7,8 +7,8 @@ envs = %w[development staging sandbox production]
 Datadog.configure do |c|
   if envs.include? Settings.vsp_environment
     # Talk to DD agent in neighboring container
-    c.agent.host = 'datadog-agent'
-    c.agent.port = 8126
+    # c.agent.host = 'datadog-agent'
+    # c.agent.port = 8126
 
     # Namespace our app
     c.service = 'vets-api'
