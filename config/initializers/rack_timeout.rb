6e6b3ed68ae7a448e1bbdb7312f72a7da17975db
Jeremy Wu <25186448+jeremyhwu@users.noreply.github.com>
2022-09-13 08:56:24 -0600

    Use variables from settings instead of ENV (#10667)

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 # Adds a global timeout for all requests
-unless Rails.env.test?
+if !Rails.env.test? || Settings.rack_timeout
   # @see https://github.com/zombocom/rack-timeout/blob/main/doc/settings.md
   Rails.application.configure do |config|
-    # Default of 0 disables rack timeout unless the ENV variable is set
     config.middleware.insert_before(
       Rack::Runtime, Rack::Timeout,
-      service_timeout: ENV.fetch('RACK_TIMEOUT_SERVICE_TIMEOUT', 0).to_i,
-      wait_timeout: ENV.fetch('RACK_TIMEOUT_WAIT_TIMEOUT', 0).to_i,
-      wait_overtime: ENV.fetch('RACK_TIMEOUT_WAIT_OVERTIME', 0).to_i
+      # Default of 0 disables rack timeout unless the ENV variable is set
+      service_timeout: Settings.rack_timeout.service_timeout || 0,
+      wait_timeout: Settings.rack_timeout.wait_timeout || 0,
+      wait_overtime: Settings.rack_timeout.wait_overtime || 0
     )
   end
 end
