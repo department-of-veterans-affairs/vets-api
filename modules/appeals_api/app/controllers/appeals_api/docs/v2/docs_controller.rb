041b6dd14ed067ee118242e0ede77f067a825cea
Drew Fisher <142862+cilestin@users.noreply.github.com>
2022-09-22 07:25:46 -0700

    API-16005 split doc updates (part 4: CI & LA) (#10763)
    * Add contestable issues segmented doc paths; cleanup file lookup
    
    * Output correct schemas for segmentec contestable issues docs
    
    * Regenerate segmented contestable issues doc files
    
    * Add segmented legacy appeals doc path & action
    
    * Update listed schemas for segmented legacy appeals docs; refactor
    
    * Regenerate segmented legacy appeals doc files
    
    Co-authored-by: Mike <michel.mcdonald@adhocteam.us>

--- modules/appeals_api/app/controllers/appeals_api/docs/v2/docs_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/docs/v2/docs_controller.rb
@@ -4,38 +4,34 @@ class AppealsApi::Docs::V2::DocsController < ApplicationController
   skip_before_action(:authenticate)
 
   def decision_reviews
-    swagger = if Settings.vsp_environment == 'production'
-                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger.json')))
-              else
-                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger_dev.json')))
-              end
-    render json: swagger
+    render json: JSON.parse(swagger_file(nil))
   end
 
   def hlr
-    swagger = if Settings.vsp_environment == 'production'
-                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger_hlr.json')))
-              else
-                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger_hlr_dev.json')))
-              end
-    render json: swagger
+    render json: JSON.parse(swagger_file('hlr'))
   end
 
   def nod
-    swagger = if Settings.vsp_environment == 'production'
-                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger_nod.json')))
-              else
-                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger_nod_dev.json')))
-              end
-    render json: swagger
+    render json: JSON.parse(swagger_file('nod'))
   end
 
   def sc
-    swagger = if Settings.vsp_environment == 'production'
-                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger_sc.json')))
-              else
-                JSON.parse(File.read(AppealsApi::Engine.root.join('app/swagger/appeals_api/v2/swagger_sc_dev.json')))
-              end
-    render json: swagger
+    render json: JSON.parse(swagger_file('sc'))
+  end
+
+  def ci
+    render json: JSON.parse(swagger_file('contestable_issues'))
+  end
+
+  def la
+    render json: JSON.parse(swagger_file('legacy_appeals'))
+  end
+
+  private
+
+  def swagger_file(stub, version: 'v2')
+    filename = Settings.vsp_environment == 'production' ? "swagger_#{stub}.json" : "swagger_#{stub}_dev.json"
+    filename.gsub!('__', '') if stub.nil? # special case for pre-segmented documentation
+    File.read AppealsApi::Engine.root.join("app/swagger/appeals_api/#{version}/#{filename}")
   end
 end
