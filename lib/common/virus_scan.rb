aaa295d38c9ecfbb61a08efc687254cd7da596a6
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2022-09-26 08:35:59 -0500

    Fixes issue with clamdscan scanning whole vets-api directory (#10791)

--- lib/common/virus_scan.rb
+++ lib/common/virus_scan.rb
@@ -7,8 +7,11 @@ module Common
     def scan(file_path)
       # `clamd` runs within service group, needs group read
       File.chmod(0o640, file_path)
-      args = ['--config', Rails.root.join('config', 'clamd.conf').to_s]
-      ClamScan::Client.scan(location: file_path, custom_args: args)
+
+      # NOTE: If using custom_args, no other arguments can be passed to
+      # ClamScan::Client.scan. All other arguments will be ignored
+      args = ['-c', Rails.root.join('config', 'clamd.conf').to_s, file_path]
+      ClamScan::Client.scan(custom_args: args)
     end
   end
 end
