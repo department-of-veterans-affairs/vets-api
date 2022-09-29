5757f999fa010dd59d73a56c4d6776fd6ac31e89
Casey Williams <caseywilliams@users.noreply.github.com>
2022-09-29 15:14:49 -0700

    API-20060 - Healthcheck endpoints for segmented appeals APIs (#10810)
    * API-20060 - Healthcheck endpoints for segmented appeals API
    
    Reuses the existing metadata controller to adds new health check
    endpoints for all segments of the appeals API at
    `/services/appeals/<SEGMENT_NAME>/v2/upstream_healthcheck`.
    
    * API-20060 - Add healthcheck endpoints to split appeals API
    
    Reuses the existing metadata controller actions to create additional
    (non-upstream) healthcheck endpoints to the split appeals APIs

--- modules/appeals_api/spec/requests/metadata_request_spec.rb
+++ modules/appeals_api/spec/requests/metadata_request_spec.rb
@@ -3,6 +3,122 @@
 require 'rails_helper'
 require 'appeals_api/health_checker'
 
+RSpec.shared_examples 'a healthcheck' do |path|
+  it 'returns a successful healthcheck' do
+    get path
+
+    parsed_response = JSON.parse(response.body)
+    expect(response).to have_http_status(:ok)
+    expect(parsed_response['description']).to eq('Appeals API health check')
+    expect(parsed_response['status']).to eq('UP')
+    expect(parsed_response['time']).not_to be_nil
+  end
+end
+
+RSpec.shared_examples 'an upstream healthcheck (caseflow)' do |path|
+  it 'returns a successful healthcheck' do
+    VCR.use_cassette 'caseflow/health-check' do
+      get path
+
+      expect(response).to have_http_status(:ok)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['description']).to eq('Appeals API upstream health check')
+      expect(parsed_response['status']).to eq('UP')
+      expect(parsed_response['time']).to eq('2020-09-21T00:00:00Z')
+
+      details = parsed_response['details']
+      expect(details['name']).to eq('All upstream services')
+
+      upstream_service = details['upstreamServices'].first
+      expect(details['upstreamServices'].size).to eq(1)
+      expect(upstream_service['description']).to eq('Caseflow')
+      expect(upstream_service['status']).to eq('UP')
+      expect(upstream_service['details']['name']).to eq('Caseflow')
+      expect(upstream_service['details']['statusCode']).to eq(200)
+      expect(upstream_service['details']['status']).to eq('OK')
+      expect(upstream_service['details']['time']).to eq('2020-09-21T00:00:00Z')
+    end
+  end
+
+  it 'returns correct status when caseflow is not healthy' do
+    VCR.use_cassette('caseflow/health-check-down') do
+      get path
+
+      expect(response).to have_http_status(:service_unavailable)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['description']).to eq('Appeals API upstream health check')
+      expect(parsed_response['status']).to eq('DOWN')
+      expect(parsed_response['time']).to eq('2020-09-21T00:00:00Z')
+
+      details = parsed_response['details']
+      expect(details['name']).to eq('All upstream services')
+
+      upstream_service = details['upstreamServices'].first
+      expect(details['upstreamServices'].size).to eq(1)
+      expect(upstream_service['description']).to eq('Caseflow')
+      expect(upstream_service['status']).to eq('DOWN')
+      expect(upstream_service['details']['name']).to eq('Caseflow')
+      expect(upstream_service['details']['statusCode']).to eq(503)
+      expect(upstream_service['details']['status']).to eq('Unavailable')
+      expect(upstream_service['details']['time']).to eq('2020-09-21T00:00:00Z')
+    end
+  end
+end
+
+RSpec.shared_examples 'an upstream healthcheck (central mail)' do |path|
+  it 'returns correct status when CentralMail is healthy' do
+    VCR.use_cassette('caseflow/health-check') do
+      allow(CentralMail::Service).to receive(:current_breaker_outage?).and_return(false)
+
+      get path
+
+      expect(response).to have_http_status(:ok)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['description']).to eq('Appeals API upstream health check')
+      expect(parsed_response['status']).to eq('UP')
+      expect(parsed_response['time']).to eq('2020-09-21T00:00:00Z')
+
+      details = parsed_response['details']
+      expect(details['name']).to eq('All upstream services')
+
+      expect(details['upstreamServices'].size).to eq(1)
+      upstream_service = details['upstreamServices'].first
+      expect(upstream_service['description']).to eq('Central Mail')
+      expect(upstream_service['status']).to eq('UP')
+      expect(upstream_service['details']['name']).to eq('Central Mail')
+      expect(upstream_service['details']['statusCode']).to eq(200)
+      expect(upstream_service['details']['status']).to eq('OK')
+      expect(upstream_service['details']['time']).to eq('2020-09-21T00:00:00Z')
+    end
+  end
+
+  it 'returns the correct status when CentralMail is not healthy' do
+    VCR.use_cassette('caseflow/health-check') do
+      allow(CentralMail::Service).to receive(:current_breaker_outage?).and_return(true)
+
+      get path
+      expect(response).to have_http_status(:service_unavailable)
+
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['description']).to eq('Appeals API upstream health check')
+      expect(parsed_response['status']).to eq('DOWN')
+      expect(parsed_response['time']).to eq('2020-09-21T00:00:00Z')
+
+      details = parsed_response['details']
+      expect(details['name']).to eq('All upstream services')
+
+      expect(details['upstreamServices'].size).to eq(1)
+      upstream_service = details['upstreamServices'].first
+      expect(upstream_service['description']).to eq('Central Mail')
+      expect(upstream_service['status']).to eq('DOWN')
+      expect(upstream_service['details']['name']).to eq('Central Mail')
+      expect(upstream_service['details']['statusCode']).to eq(503)
+      expect(upstream_service['details']['status']).to eq('Unavailable')
+      expect(upstream_service['details']['time']).to eq('2020-09-21T00:00:00Z')
+    end
+  end
+end
+
 RSpec.describe 'Appeals Metadata Endpoint', type: :request do
   describe '#get /metadata' do
     it 'returns decision reviews metadata JSON' do
@@ -20,27 +136,19 @@ RSpec.describe 'Appeals Metadata Endpoint', type: :request do
 
   describe '#healthcheck' do
     context 'v0' do
-      it 'returns a successful health check' do
-        get '/services/appeals/v0/healthcheck'
-
-        parsed_response = JSON.parse(response.body)
-        expect(response).to have_http_status(:ok)
-        expect(parsed_response['description']).to eq('Appeals API health check')
-        expect(parsed_response['status']).to eq('UP')
-        expect(parsed_response['time']).not_to be_nil
-      end
+      it_behaves_like 'a healthcheck', '/services/appeals/v0/healthcheck'
     end
 
     context 'v1' do
-      it 'returns a successful health check' do
-        get '/services/appeals/v1/healthcheck'
+      it_behaves_like 'a healthcheck', '/services/appeals/v1/healthcheck'
+    end
 
-        parsed_response = JSON.parse(response.body)
-        expect(response).to have_http_status(:ok)
-        expect(parsed_response['description']).to eq('Appeals API health check')
-        expect(parsed_response['status']).to eq('UP')
-        expect(parsed_response['time']).not_to be_nil
-      end
+    context 'segmented APIs' do
+      it_behaves_like 'a healthcheck', '/services/appeals/higher_level_reviews/v2/healthcheck'
+      it_behaves_like 'a healthcheck', '/services/appeals/notice_of_disagreements/v2/healthcheck'
+      it_behaves_like 'a healthcheck', '/services/appeals/supplemental_claims/v2/healthcheck'
+      it_behaves_like 'a healthcheck', '/services/appeals/contestable_issues/v2/healthcheck'
+      it_behaves_like 'a healthcheck', '/services/appeals/legacy_appeals/v2/healthcheck'
     end
   end
 
@@ -53,53 +161,7 @@ RSpec.describe 'Appeals Metadata Endpoint', type: :request do
     after { Timecop.return }
 
     context 'v0' do
-      it 'returns correct response and status when healthy' do
-        VCR.use_cassette('caseflow/health-check') do
-          get '/services/appeals/v0/upstream_healthcheck'
-          expect(response).to have_http_status(:ok)
-
-          parsed_response = JSON.parse(response.body)
-          expect(parsed_response['description']).to eq('Appeals API upstream health check')
-          expect(parsed_response['status']).to eq('UP')
-          expect(parsed_response['time']).to eq('2020-09-21T00:00:00Z')
-
-          details = parsed_response['details']
-          expect(details['name']).to eq('All upstream services')
-
-          upstream_service = details['upstreamServices'].first
-          expect(details['upstreamServices'].size).to eq(1)
-          expect(upstream_service['description']).to eq('Caseflow')
-          expect(upstream_service['status']).to eq('UP')
-          expect(upstream_service['details']['name']).to eq('Caseflow')
-          expect(upstream_service['details']['statusCode']).to eq(200)
-          expect(upstream_service['details']['status']).to eq('OK')
-          expect(upstream_service['details']['time']).to eq('2020-09-21T00:00:00Z')
-        end
-      end
-
-      it 'returns correct status when caseflow is not healthy' do
-        VCR.use_cassette('caseflow/health-check-down') do
-          get '/services/appeals/v0/upstream_healthcheck'
-          expect(response).to have_http_status(:service_unavailable)
-
-          parsed_response = JSON.parse(response.body)
-          expect(parsed_response['description']).to eq('Appeals API upstream health check')
-          expect(parsed_response['status']).to eq('DOWN')
-          expect(parsed_response['time']).to eq('2020-09-21T00:00:00Z')
-
-          details = parsed_response['details']
-          expect(details['name']).to eq('All upstream services')
-
-          upstream_service = details['upstreamServices'].first
-          expect(details['upstreamServices'].size).to eq(1)
-          expect(upstream_service['description']).to eq('Caseflow')
-          expect(upstream_service['status']).to eq('DOWN')
-          expect(upstream_service['details']['name']).to eq('Caseflow')
-          expect(upstream_service['details']['statusCode']).to eq(503)
-          expect(upstream_service['details']['status']).to eq('Unavailable')
-          expect(upstream_service['details']['time']).to eq('2020-09-21T00:00:00Z')
-        end
-      end
+      it_behaves_like 'an upstream healthcheck (caseflow)', '/services/appeals/v0/upstream_healthcheck'
     end
 
     context 'v1' do
@@ -222,6 +284,29 @@ RSpec.describe 'Appeals Metadata Endpoint', type: :request do
           expect(upstream_service['details']['time']).to eq('2020-09-21T00:00:00Z')
         end
       end
+    end
+
+    context 'segmented APIs' do
+      it_behaves_like(
+        'an upstream healthcheck (central mail)',
+        '/services/appeals/supplemental_claims/v2/upstream_healthcheck'
+      )
+      it_behaves_like(
+        'an upstream healthcheck (central mail)',
+        '/services/appeals/notice_of_disagreements/v2/upstream_healthcheck'
+      )
+      it_behaves_like(
+        'an upstream healthcheck (central mail)',
+        '/services/appeals/higher_level_reviews/v2/upstream_healthcheck'
+      )
+      it_behaves_like(
+        'an upstream healthcheck (caseflow)',
+        '/services/appeals/contestable_issues/v2/upstream_healthcheck'
+      )
+      it_behaves_like(
+        'an upstream healthcheck (caseflow)',
+        '/services/appeals/legacy_appeals/v2/upstream_healthcheck'
+      )
     end
   end
 end
