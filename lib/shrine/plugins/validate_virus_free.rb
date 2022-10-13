2734fec438b04ff13f9bb5eb92d4aa03406399d6
rjohnson2011 <72466113+rjohnson2011@users.noreply.github.com>
2022-10-13 15:28:05 -0400

    APM for DocumentUpload Process (#10900)
    * Trace for document uploads
    
    * Require ddtrace
    
    * Addition APM for Sidekiq Doc Upload
    
    * Variable fix
    
    * Rubocop fixes
    
    * Spec fix
    
    * Log file size
    
    * File size check
    
    * Remove file size check
    
    * APM sidekiq mods
    
    * Alter block
    
    * Spec linting
    
    * Lint

--- lib/shrine/plugins/validate_virus_free.rb
+++ lib/shrine/plugins/validate_virus_free.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 require 'common/virus_scan'
+require 'ddtrace'
 
 class Shrine
   module Plugins
     module ValidateVirusFree
       module AttacherMethods
         def validate_virus_free(message: nil)
-          cached_path = get.download.path
-          result = Common::VirusScan.scan(cached_path)
-          # TODO: Log a the full result to sentry
-          result.safe? || add_error_msg(message || result.body)
+          Datadog::Tracing.trace('Scan Upload for Viruses') do
+            cached_path = get.download.path
+            result = Common::VirusScan.scan(cached_path)
+            result.safe? || add_error_msg(message || result.body)
+          end
         end
 
         private
