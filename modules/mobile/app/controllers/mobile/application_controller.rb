60b4fb54484b70b086ab5ff46d54769bf6bc1424
Jayson Perkins <jayson.perkins@adhocteam.us>
2022-10-25 15:41:25 -0400

    Add new metrics to differentiate SIS from IAM auths (#10991)

--- modules/mobile/app/controllers/mobile/application_controller.rb
+++ modules/mobile/app/controllers/mobile/application_controller.rb
@@ -13,12 +13,14 @@ module Mobile
     def authenticate
       return super if sis_authentication?
 
+      StatsD.increment('iam_ssoe_oauth.auth.total')
       raise_unauthorized('Missing Authorization header') if request.headers['Authorization'].nil?
       raise_unauthorized('Authorization header Bearer token is blank') if access_token.blank?
 
       session_manager = IAMSSOeOAuth::SessionManager.new(access_token)
       @current_user = session_manager.find_or_create_user
       link_user_with_vets360 if @current_user.vet360_id.blank?
+      StatsD.increment('iam_ssoe_oauth.auth.success')
       @current_user
     end
 
