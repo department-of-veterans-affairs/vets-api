158bbac9c5da9d7b6f0421276ce0b7607c37f5b5
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2022-10-26 11:45:40 -0500

    Decoding url to get ID (#10995)

--- app/controllers/v0/claim_letters_controller.rb
+++ app/controllers/v0/claim_letters_controller.rb
@@ -6,11 +6,14 @@ module V0
   class ClaimLettersController < ApplicationController
     def index
       docs = service.list_letters
+
       render json: docs
     end
 
     def show
-      service.get_letter(params[:document_id]) do |data, mime_type, disposition, filename|
+      document_id = CGI.unescape(params[:document_id])
+
+      service.get_letter(document_id) do |data, mime_type, disposition, filename|
         send_data(data, type: mime_type, disposition: disposition, filename: filename)
       end
     end
