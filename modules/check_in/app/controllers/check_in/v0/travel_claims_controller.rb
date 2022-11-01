c6960e9766d9a964f8e3bd248c6b13da87ffdaaa
Gaurav Gupta <randomsync@users.noreply.github.com>
2022-11-01 13:35:50 -0700

    47009 travel claim submission controller (#11019)
    * 47009 initial route and controller
    
    * 47009 controller code and partial specs
    
    * 47009 update controller to return not implemented for now

---
+++ modules/check_in/app/controllers/check_in/v0/travel_claims_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module CheckIn
+  module V0
+    class TravelClaimsController < CheckIn::ApplicationController
+      def create
+        check_in_session = CheckIn::V2::Session.build(data: { uuid: permitted_params[:uuid] }, jwt: low_auth_token)
+
+        unless check_in_session.authorized?
+          render json: check_in_session.unauthorized_message, status: :unauthorized and return
+        end
+
+        head :not_implemented
+      end
+
+      def permitted_params
+        params.require(:travel_claims).permit(:uuid, :appointment_date)
+      end
+
+      def authorize
+        routing_error unless Flipper.enabled?('check_in_experience_travel_reimbursement')
+      end
+    end
+  end
+end
