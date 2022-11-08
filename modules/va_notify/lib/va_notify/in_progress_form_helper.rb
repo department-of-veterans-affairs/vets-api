c655e7db4060db9e1fed78840e30fe6a573df0e2
Nathan Wright <nathanbwright@users.noreply.github.com>
2022-11-08 09:16:46 -0700

    Add 526ez to in progress reminders (behind feature flag) (#11094)

--- modules/va_notify/lib/va_notify/in_progress_form_helper.rb
+++ modules/va_notify/lib/va_notify/in_progress_form_helper.rb
@@ -3,13 +3,16 @@
 module VANotify
   class InProgressFormHelper
     TEMPLATE_ID = {
+      'generic' => Settings.vanotify.services.va_gov.template_id.in_progress_reminder_email_generic,
       '686C-674' => Settings.vanotify.services.va_gov.template_id.form686c_reminder_email,
-      '1010ez' => Settings.vanotify.services.va_gov.template_id.form1010ez_reminder_email
+      '1010ez' => Settings.vanotify.services.va_gov.template_id.form1010ez_reminder_email,
+      '21-526EZ' => Settings.vanotify.services.va_gov.template_id.form526ez_reminder_email
     }.freeze
 
     FRIENDLY_FORM_SUMMARY = {
       '686C-674' => 'Application Request to Add or Remove Dependents',
-      '1010ez' => 'Application for Health Benefits'
+      '1010ez' => 'Application for Health Benefits',
+      '21-526EZ' => 'Application for Disability Compensation and Related Compensation Benefits'
     }.freeze
 
     def self.form_age(in_progress_form)
