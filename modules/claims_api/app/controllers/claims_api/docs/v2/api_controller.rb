f08c94542eaeadb1cf5082029df29effa8f33200
Alexander Wilson <fonzmp@gmail.com>
2022-11-30 09:10:30 -0700

    Api 21642 introduce rswag env (#11250)

--- modules/claims_api/app/controllers/claims_api/docs/v2/api_controller.rb
+++ modules/claims_api/app/controllers/claims_api/docs/v2/api_controller.rb
@@ -4,10 +4,14 @@ module ClaimsApi
   module Docs
     module V2
       class ApiController < ClaimsApi::Docs::ApiController
+        # rubocop:disable Layout/LineLength
         def index
-          swagger = JSON.parse(File.read(ClaimsApi::Engine.root.join('app/swagger/claims_api/v2/swagger.json')))
+          environment_directory = request.base_url == ('https://api.va.gov' || 'https://sandbox-api.va.gov') ? 'production' : 'dev'
+
+          swagger = JSON.parse(File.read(ClaimsApi::Engine.root.join("app/swagger/claims_api/v2/#{environment_directory}/swagger.json")))
           render json: swagger
         end
+        # rubocop:enable Layout/LineLength
       end
     end
   end
