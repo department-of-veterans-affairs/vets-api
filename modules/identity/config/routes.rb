716011c16552183ab4644b950c2f8bd6b583dce2
John Bramley <john.bramley@adhocteam.us>
2022-12-02 08:28:36 -0700

    removes identity module (#11278)

--- modules/identity/config/routes.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-Identity::Engine.routes.draw do
-end
--- modules/identity/lib/identity.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'identity/engine'
-
-module Identity
-  # Your code goes here...
-end
