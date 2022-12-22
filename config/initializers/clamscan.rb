fefa4cbfd5cca3b7d83b1975dd494b7ab1a95d70
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-12-22 11:29:34 -0500

    remove code not necessary

--- config/initializers/clamscan.rb
+++ config/initializers/clamscan.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 ClamScan.configure do |clam_config|
-  File.chmod(0o640, Rails.root.to_s + '/config/clamd.conf')
   clam_config.client_location = Settings.binaries.clamdscan
   clam_config.default_scan_options = { stdout: false }
 end
