b552c5dfac0baf7f57c4d99d2c7d2cec6b25da36
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-12-22 09:19:31 -0500

    try path to config

--- config/initializers/clamscan.rb
+++ config/initializers/clamscan.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 ClamScan.configure do |clam_config|
-  clam_config.client_location = Settings.binaries.clamdscan
+  clam_config.client_location = Rails.root.join('app', 'config', 'clamd.conf')
   clam_config.default_scan_options = { stdout: false }
 end
