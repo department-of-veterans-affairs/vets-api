bddba43dfd752e5f5fe601b535d38aca13cc56d1
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-12-22 09:53:43 -0500

    testing file perms

--- config/initializers/clamscan.rb
+++ config/initializers/clamscan.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 ClamScan.configure do |clam_config|
-  clam_config.client_location = Rails.root.join('config', 'clamd.conf').to_s
+  clam_config.client_location = '/usr/clamav/clamd.conf'
   clam_config.default_scan_options = { stdout: false }
 end
