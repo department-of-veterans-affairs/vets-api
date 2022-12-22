3119931ae42a2bb4cc2e7e6ffa05c2d0d9d7a526
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-12-22 10:12:44 -0500

    testing file perms

--- config/initializers/clamscan.rb
+++ config/initializers/clamscan.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 ClamScan.configure do |clam_config|
-#  clam_config.client_location = Settings.binaries.clamdscan
+  File.chmod(0o640, Rails.root.to_s + '/config/clamd.conf')
+  clam_config.client_location = Settings.binaries.clamdscan
   clam_config.default_scan_options = { stdout: false }
 end
