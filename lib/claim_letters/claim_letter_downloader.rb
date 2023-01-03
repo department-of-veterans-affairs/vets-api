1b83d25391d946cf9b702cfb48ce60bd2b1f9fb5
samcoforma <105806934+samcoforma@users.noreply.github.com>
2023-01-03 10:32:13 -0800

    CST DLD: Added ClaimLetter-YYYY-MM-D.pdf format filename and changed … (#11368)
    * CST DLD: Added ClaimLetter-YYYY-MM-D.pdf format filename and changed how related methods raise errors.
    
    * got rid of memoization after team discussion

--- lib/claim_letters/claim_letter_downloader.rb
+++ lib/claim_letters/claim_letter_downloader.rb
@@ -4,39 +4,42 @@ require 'claim_letters/claim_letter_test_data'
 
 module ClaimStatusTool
   class ClaimLetterDownloader
+    FILENAME = 'ClaimLetter'
+
     def initialize(user)
       @user = user
       @client = VBMS::Client.from_env_vars(env_name: Settings.vbms.env) unless Rails.env.development? || Rails.env.test?
     end
 
     def get_letters
-      @letters ||= begin
-        res = nil
+      res = nil
 
-        if !Rails.env.development? && !Rails.env.test?
-          req = VBMS::Requests::FindDocumentVersionReference.new(file_number)
-          res = @client.send_request(req)
-        else
-          res = ClaimLetterTestData::TEST_DATA
-        end
-
-        format_letter_data(res)
+      if !Rails.env.development? && !Rails.env.test?
+        req = VBMS::Requests::FindDocumentVersionReference.new(file_number)
+        res = @client.send_request(req)
+      else
+        res = ClaimLetterTestData::TEST_DATA
       end
+
+      format_letter_data(res)
     rescue VBMS::FilenumberDoesNotExist
       []
     end
 
     def get_letter(document_id)
-      verify_letter_in_folder(document_id)
+      letter_details = get_letter_details(document_id)
+      raise Common::Exceptions::RecordNotFound, document_id if letter_details.nil?
+
+      filename = filename_with_date(letter_details[:received_at])
 
       if !Rails.env.development? && !Rails.env.test?
         req = VBMS::Requests::GetDocumentContent.new(document_id)
         res = @client.send_request(req)
 
-        yield res.content, 'application/pdf', 'attachment', 'ClaimLetter.pdf'
+        yield res.content, 'application/pdf', 'attachment', filename
       else
         File.open(ClaimLetterTestData::TEST_FILE_PATH, 'r') do |f|
-          yield f.read, 'application/pdf', 'attachment', 'ClaimLetter.pdf'
+          yield f.read, 'application/pdf', 'attachment', filename
         end
       end
     end
@@ -65,15 +68,17 @@ module ClaimStatusTool
 
     def format_letter_data(docs)
       letters = docs.map { |d| filter_letters(d) }.compact
+      # TODO: (rare) Handle nil received_at
       letters.sort_by { |d| d[:received_at] }.reverse
     end
 
-    def verify_letter_in_folder(document_id)
+    def get_letter_details(document_id)
       letters = get_letters
+      letters.find { |d| d[:document_id] == document_id }
+    end
 
-      raise Common::Exceptions::Unauthorized unless letters.any? do |document|
-        document[:document_id] == document_id
-      end
+    def filename_with_date(filedate)
+      "#{FILENAME}-#{filedate.year}-#{filedate.month}-#{filedate.day}.pdf"
     end
   end
 end
