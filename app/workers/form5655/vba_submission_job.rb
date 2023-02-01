fbaebce9ab2cb2bbc664a59709a5043937271dde
Scott James <scott.james@gcio.com>
2023-02-01 14:29:54 -0500

    Turn retries off for 5655 form workers (#11685)

--- app/workers/form5655/vba_submission_job.rb
+++ app/workers/form5655/vba_submission_job.rb
@@ -4,6 +4,8 @@ module Form5655
   class VBASubmissionJob
     include Sidekiq::Worker
 
+    sidekiq_options retry: false
+
     def perform(submission_id, user_uuid)
       submission = Form5655Submission.find(submission_id)
       user = User.find(user_uuid)
