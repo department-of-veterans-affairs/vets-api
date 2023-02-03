ba8b51e957ccbb0bac7cc15766d200e205806b76
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2023-02-03 13:33:54 -0500

    use quotes

--- config/initializers/clamscan.rb
+++ config/initializers/clamscan.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 ClamScan.configure do |clam_config|
-  clam_config.client_location = http://localhost:3310
+  clam_config.client_location = 'http://localhost:3310'
   clam_config.default_scan_options = { stdout: false }
 end
