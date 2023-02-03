60871b1bc204c2f1838503a33ec3f3047fa85b1b
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2023-02-03 14:34:23 -0500

    Adding non-breakered EVSS services (#11723)
    * Adding non-breakered services
    
    * Wrong class

---
+++ lib/evss/disability_compensation_form/non_breakered_service.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'common/client/concerns/monitoring'
+require 'common/client/errors'
+require 'evss/service'
+require 'evss/disability_compensation_auth_headers'
+require_relative 'configuration'
+require_relative 'rated_disabilities_response'
+require_relative 'form_submit_response'
+require_relative 'service_unavailable_exception'
+
+module EVSS
+  module DisabilityCompensationForm
+    class NonBreakeredService < EVSS::DisabilityCompensationForm::Service
+      configuration EVSS::NonBreakeredConfiguration
+    end
+  end
+end
