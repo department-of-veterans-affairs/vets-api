827e0f52bfa5be0bca7d33961cfc86bac72479cf
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2023-02-06 15:59:10 -0500

    Adding 2 extra requirements to the backup submission (#11731)

--- lib/sidekiq/form526_job_status_tracker/job_tracker.rb
+++ lib/sidekiq/form526_job_status_tracker/job_tracker.rb
@@ -38,8 +38,7 @@ module Sidekiq
           new_error = {
             "#{timestamp.to_i}": {
               caller_method: __method__.to_s,
-              error_class: error_class,
-              error_message: error_message,
+              error_class: error_class, error_message: error_message,
               timestamp: timestamp
             }
           }
@@ -54,7 +53,17 @@ module Sidekiq
 
           backup_job_jid = nil
           flipper_sym = :form526_backup_submission_temp_killswitch
-          if additional_birls_to_try.empty? && Settings.form526_backup.enabled && Flipper.enabled?(flipper_sym)
+
+          # Entry-point for backup 526 CMP submission
+          #
+          # Required criteria to send a backup 526 submission from here:
+          # Enabled in settings and flipper
+          # Is an overall submission and NOT an upload attempt
+          # Does not have a valid claim ID (through RRD process)
+          # Does not have additional birls it is going to try and submit under
+          if Settings.form526_backup.enabled && Flipper.enabled?(flipper_sym) &&
+             values[:job_class] == 'SubmitForm526AllClaim' && submission_obj.submitted_claim_id.nil? &&
+             additional_birls_to_try.empty?
             backup_job_jid = Sidekiq::Form526BackupSubmissionProcess::Submit.perform_async(form526_submission_id)
           end
 
