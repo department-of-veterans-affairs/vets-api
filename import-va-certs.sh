0d8fe45c0f1828e5a988bfa66c73f1ddf426a526
Kerry Powell <kerry.powell@adhocteam.us>
2023-02-13 15:01:56 -0700

    Add Dockerfile for preview environment deploys (#11757)
    * Add Dockerfile-pe for preview environments. Add command to clone vets-api-mockdata to that file. Copy and re-name import-va-certs.sh from k8s branch
    
    * Update to implement conditional in Dockerfile to enable consolidation
    
    * Add TODO comment in Dockerfile-k8s
    
    * Update Dockerfile-k8s
    
    Update to clean up import script name.
    
    Co-authored-by: Peter Hill <52456633+pjhill@users.noreply.github.com>
    
    * Update Dockerfile-k8s
    
    Co-authored-by: Peter Hill <52456633+pjhill@users.noreply.github.com>
    
    * rename import-va-certs-k8 to import-va-certs.sh. This should be merged into the k8s branch when on master
    
    ---------
    
    Co-authored-by: Peter Hill <52456633+pjhill@users.noreply.github.com>

---
+++ import-va-certs.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+(
+    cd /usr/local/share/ca-certificates/
+    
+    wget \
+        --level=1 \
+        --quiet \
+        --recursive \
+        --no-parent \
+        --no-host-directories \
+        --no-directories \
+        --accept="VA*.cer" \
+        http://aia.pki.va.gov/PKI/AIA/VA/
+
+    for cert in VA-*.cer
+    do
+        if file "${cert}" | grep 'PEM'
+        then
+            cp "${cert}" "${cert}.crt"
+        else
+            openssl x509 -in "${cert}" -inform der -outform pem -out "${cert}.crt"
+        fi
+        rm "${cert}"
+    done
+
+    update-ca-certificates --fresh
+
+    # Display VA Internal certificates that are now trusted
+    awk -v cmd='openssl x509 -noout -subject' '/BEGIN/{close(cmd)};{print | cmd}' < /etc/ssl/certs/ca-certificates.crt \
+    | grep -i 'VA-Internal'
+)
