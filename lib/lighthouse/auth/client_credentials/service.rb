626341522d3417635ab1ef7fedc976b8609bdfb4
Tom Harrison <tom.harrison@adhocteam.us>
2023-02-16 15:20:34 -0500

    Lighthouse CCG Service (#11805)
    * Add JWT generator for Lighthouse CCG
    
    * Fix rubocop errors
    
    * Update lighthouse direct deposit settings
    
    * Remove direct deposit coupling from jwt_generator_spec
    
    * Remove aud_claim_url value from settings file
    
    * Update settings, pem file names, and added file check for private key
    
    * Update file check for key
    
    * Rubocop fixes
    
    * Remove private key stub - use file instead
    
    * Add service and config for Lighthouse CCG authentication

---
+++ lib/lighthouse/auth/client_credentials/service.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'common/client/base'
+require 'lighthouse/auth/client_credentials/configuration'
+require 'lighthouse/auth/client_credentials/jwt_generator'
+
+module Auth
+  module ClientCredentials
+    class Service < Common::Client::Base
+      configuration Auth::ClientCredentials::Configuration
+
+      ##
+      #
+      # @param [String] token_url - URL of the token endpoint
+      # @param [Array] api_scopes - List of requested API scopes
+      # @param [String] client_id - ID used to identify the application
+      # @param [String] aud_claim_url - The claim URL used as the 'aud' portion of the JWT
+      # @param [String] rsa_key - RSA key used to encode the authentication JWT
+      def initialize(token_url, api_scopes, client_id, aud_claim_url, rsa_key)
+        @url = token_url
+        @scopes = api_scopes
+        @client_id = client_id
+        @aud = aud_claim_url
+        @rsa_key = rsa_key
+        super()
+      end
+
+      ##
+      # Request an access token
+      #
+      # @return [String] the access token needed to make requests
+      #
+      def get_token
+        assertion = build_assertion
+        request_body = build_request_body(assertion, @scopes)
+        res = config.get_access_token(@url, request_body)
+
+        res.body['access_token']
+      end
+
+      private
+
+      ##
+      # @return [String] new JWT token
+      #
+      def build_assertion
+        Auth::ClientCredentials::JWTGenerator.generate_token(@client_id, @aud, @rsa_key)
+      end
+
+      ##
+      # @return [Hash] body of request to get access token
+      #
+      def build_request_body(assertion, scopes)
+        {
+          grant_type: 'client_credentials',
+          client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+          client_assertion: assertion,
+          scope: scopes.join(' ')
+        }
+      end
+    end
+  end
+end
