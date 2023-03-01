119286719589308a25ff833bbd15685badbd06d0
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2023-03-01 13:53:27 -0700

    API-22649-new-526-endpoints (#11938)

---
+++ modules/claims_api/spec/requests/v2/veterans/disability_compensation_request_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Disability Claims', type: :request do
+  let(:headers) do
+    { 'X-VA-SSN': '796-04-3735',
+      'X-VA-First-Name': 'WESLEY',
+      'X-VA-Last-Name': 'FORD',
+      'X-Consumer-Username': 'TestConsumer',
+      'X-VA-Birth-Date': '1986-05-06T00:00:00+00:00',
+      'X-VA-Gender': 'M' }
+  end
+  let(:scopes) { %w[claim.write] }
+
+  before do
+    stub_poa_verification
+    stub_mpi
+    Timecop.freeze(Time.zone.now)
+  end
+
+  after do
+    Timecop.return
+  end
+
+  describe '#526' do
+    context 'submit' do
+    end
+
+    context 'validate' do
+    end
+
+    context 'attachments' do
+    end
+  end
+end
