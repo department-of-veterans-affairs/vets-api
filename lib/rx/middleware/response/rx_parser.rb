8a0e446d678b08c54860af51e8f7a19112db99ba
Andrew Herzberg <andrew.herzberg@adhocteam.us>
2023-03-03 14:13:21 -0700

    add logging to RX endpoints (#11913)

--- lib/rx/middleware/response/rx_parser.rb
+++ lib/rx/middleware/response/rx_parser.rb
@@ -13,6 +13,7 @@ module Rx
         # @return [Faraday::Env]
         #
         def on_complete(env)
+          Rails.logger.info('RX Error Debugging', response: env)
           return unless env.response_headers['content-type']&.match?(/\bjson/)
           # If POST for prescriptions is successful message body is irrelevant
           # if it was not successul an exception would have already been raised
