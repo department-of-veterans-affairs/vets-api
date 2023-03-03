e1560a00d92b2283c3e01146dcf4e962eff32aa5
crolar <65245469+crolarlibertyva@users.noreply.github.com>
2023-03-03 15:03:58 -0500

    API-23731 remove /v0/mvi-user items (#11941)
    * rm v0/mvi_user items
    
    * rm v0/mvi_user items
    
    * rm v0/mvi_user items

--- modules/openid_auth/spec/requests/api_docs_request_spec.rb
+++ modules/openid_auth/spec/requests/api_docs_request_spec.rb
@@ -3,14 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Validation Documentation Endpoint', type: :request do
-  describe '#get /docs/v0/mvi-user' do
-    it 'returns Open API Spec v3 JSON' do
-      get '/internal/auth/docs/v0/mvi-user'
-      expect(response).to have_http_status(:ok)
-      JSON.parse(response.body)
-    end
-  end
-
   describe '#get /docs/v2/validation' do
     it 'returns Open API Spec v3 JSON' do
       get '/internal/auth/docs/v2/validation'
