cda730dde2cd880b61757ba93efadac659254fb3
Garrett <57802560+gmrabian@users.noreply.github.com>
2023-03-06 12:49:32 -0500

    forms_api 26-4555 fix name mappings (#11977)
    * fix name mappings
    
    * add suffix to signature
    
    * fix lint issue

--- modules/forms_api/app/services/forms_api/pdf_stamper.rb
+++ modules/forms_api/app/services/forms_api/pdf_stamper.rb
@@ -4,12 +4,12 @@ require 'central_mail/datestamp_pdf'
 
 module FormsApi
   class PdfStamper
-    FORM_REQUIRES_STAMP = ['vba_26_4555'].freeze
+    FORM_REQUIRES_STAMP = ['26-4555'].freeze
     SUBMISSION_TEXT = 'Signed electronically and submitted via VA.gov at '
 
     def self.stamp_pdf(generated_form_path, data)
       if FORM_REQUIRES_STAMP.include? data['form_number']
-        stamp_method = "stamp_#{data['form_number'].gsub('_', '')}"
+        stamp_method = "stamp#{data['form_number'].gsub('-', '')}"
         send(stamp_method, generated_form_path, data)
       end
       centrail_mail_stamper = CentralMail::DatestampPdf.new(generated_form_path)
@@ -19,7 +19,7 @@ module FormsApi
       File.rename(out_path, generated_form_path)
     end
 
-    def self.stamp_vba264555(generated_form_path, data)
+    def self.stamp264555(generated_form_path, data)
       desired_stamps = []
       desired_stamps.append([73, 390]) if data['previous_sah_application']['has_previous_sah_application'] == false
       desired_stamps.append([73, 355]) if data['previous_hi_application']['has_previous_hi_application'] == false
