7cf65471f696d2d686eb4d3b0d97f84aa68a79dc
Nathan Wright <nathanbwright@users.noreply.github.com>
2023-03-09 09:30:16 -0700

    Refactor 5490 and 1990 email confirmation logic location (#12020)

--- spec/models/saved_claim/education_benefits/va1990_spec.rb
+++ spec/models/saved_claim/education_benefits/va1990_spec.rb
@@ -9,4 +9,56 @@ RSpec.describe SavedClaim::EducationBenefits::VA1990 do
   it_behaves_like 'saved_claim'
 
   validate_inclusion(:form_id, '22-1990')
+
+  describe '#after_submit' do
+    let(:user) { create(:user) }
+
+    describe 'sends confirmation email for the 1990' do
+      it 'chapter 33' do
+        allow(VANotify::EmailJob).to receive(:perform_async)
+
+        subject = create(:va1990_chapter33)
+        confirmation_number = subject.education_benefits_claim.confirmation_number
+
+        subject.after_submit(user)
+
+        expect(VANotify::EmailJob).to have_received(:perform_async).with(
+          'email@example.com',
+          'form1990_confirmation_email_template_id',
+          {
+            'first_name' => 'MARK',
+            'benefit_relinquished' => '',
+            'benefits' => 'Post-9/11 GI Bill (Chapter 33)',
+            'date_submitted' => Time.zone.today.strftime('%B %d, %Y'),
+            'confirmation_number' => confirmation_number,
+            'regional_office_address' => "P.O. Box 4616\nBuffalo, NY 14240-4616"
+          }
+        )
+      end
+
+      it 'with relinquished' do
+        allow(VANotify::EmailJob).to receive(:perform_async)
+
+        subject = create(:va1990_with_relinquished)
+        confirmation_number = subject.education_benefits_claim.confirmation_number
+
+        subject.after_submit(user)
+
+        expect(VANotify::EmailJob).to have_received(:perform_async).with(
+          'email@example.com',
+          'form1990_confirmation_email_template_id',
+          {
+            'first_name' => 'MARK',
+            'benefit_relinquished' => "__Benefits Relinquished:__\n^Montgomery GI Bill (MGIB-AD, Chapter 30)",
+            'benefits' => "Post-9/11 GI Bill (Chapter 33)\n\n^Montgomery GI Bill Selected Reserve " \
+                          "(MGIB-SR or Chapter 1606) Educational Assistance Program\n\n^Post-Vietnam " \
+                          'Era Veterans’ Educational Assistance Program (VEAP or chapter 32)',
+            'date_submitted' => Time.zone.today.strftime('%B %d, %Y'),
+            'confirmation_number' => confirmation_number,
+            'regional_office_address' => "P.O. Box 4616\nBuffalo, NY 14240-4616"
+          }
+        )
+      end
+    end
+  end
 end
--- spec/models/saved_claim/education_benefits/va5490_spec.rb
+++ spec/models/saved_claim/education_benefits/va5490_spec.rb
@@ -9,4 +9,52 @@ RSpec.describe SavedClaim::EducationBenefits::VA5490 do
   it_behaves_like 'saved_claim'
 
   validate_inclusion(:form_id, '22-5490')
+
+  describe '#after_submit' do
+    let(:user) { create(:user) }
+
+    describe 'sends confirmation email for the 5490' do
+      it 'chapter 33' do
+        allow(VANotify::EmailJob).to receive(:perform_async)
+
+        subject = create(:va5490_chapter33)
+        confirmation_number = subject.education_benefits_claim.confirmation_number
+
+        subject.after_submit(user)
+
+        expect(VANotify::EmailJob).to have_received(:perform_async).with(
+          'email@example.com',
+          'form5490_confirmation_email_template_id',
+          {
+            'first_name' => 'MARK',
+            'benefit' => 'The Fry Scholarship (Chapter 33)',
+            'date_submitted' => Time.zone.today.strftime('%B %d, %Y'),
+            'confirmation_number' => confirmation_number,
+            'regional_office_address' => "P.O. Box 4616\nBuffalo, NY 14240-4616"
+          }
+        )
+      end
+
+      it 'chapter 35' do
+        allow(VANotify::EmailJob).to receive(:perform_async)
+
+        subject = create(:va5490)
+        confirmation_number = subject.education_benefits_claim.confirmation_number
+
+        subject.after_submit(user)
+
+        expect(VANotify::EmailJob).to have_received(:perform_async).with(
+          'email@example.com',
+          'form5490_confirmation_email_template_id',
+          {
+            'first_name' => 'MARK',
+            'benefit' => 'Survivors’ and Dependents’ Educational Assistance (DEA, Chapter 35)',
+            'date_submitted' => Time.zone.today.strftime('%B %d, %Y'),
+            'confirmation_number' => confirmation_number,
+            'regional_office_address' => "P.O. Box 4616\nBuffalo, NY 14240-4616"
+          }
+        )
+      end
+    end
+  end
 end
