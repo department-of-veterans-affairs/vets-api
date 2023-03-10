6a70cab95cf289aaa724912043a0d3b2424652e6
Trevor Bosaw <bosawt@gmail.com>
2023-03-10 15:21:48 -0800

    Updating engine spec helper template and unnecessary functions (#12064)
    Co-authored-by: Trevor Bosaw <trevor.bosaw@oddball.io>

--- modules/veteran_confirmation/spec/spec_helper.rb
+++ modules/veteran_confirmation/spec/spec_helper.rb
@@ -2,15 +2,8 @@
 
 # Configure Rails Envinronment
 ENV['RAILS_ENV'] = 'test'
-require File.expand_path('dummy/config/environment.rb', __dir__)
 
 require 'rspec/rails'
-
-ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(ENGINE_RAILS_ROOT, '../../spec/support/**/*.rb')].sort.each { |f| p require f }
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
