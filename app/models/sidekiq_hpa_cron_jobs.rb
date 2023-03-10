ff48e9dd7cb3cc712dbd96525eae8b516b0cb7e1
Rachal Cassity <32692924+RachalCassity@users.noreply.github.com>
2023-03-10 15:46:46 -0600

    Add more jobs to the sidekiq hpa (#12066)

--- app/models/sidekiq_hpa_cron_jobs.rb
+++ app/models/sidekiq_hpa_cron_jobs.rb
@@ -2,6 +2,15 @@
 
 class SidekiqHpaCronJobs < Common::RedisStore
   def self.clean_up_queues
-    SidekiqAlive::CleanupQueues.perform_async
+    10.times.each do |_i|
+      SidekiqAlive::CleanupQueues.perform_async
+      SidekiqStatsJob.perform_async
+      EVSS::DeleteOldClaims.perform_async
+      EVSS::DocumentUpload.perform_async
+      VBADocuments::UploadStatusBatch.perform_async
+      VANotify::InProgressForms.perform_async
+      AppealsApi::DailyErrorReport.perform_async
+      AppealsApi::HigherLevelReviewUploadStatusBatch.perform_async
+    end
   end
 end
