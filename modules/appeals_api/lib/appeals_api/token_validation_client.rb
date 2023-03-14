5e575138114e708f329f4543bb9effbea49e90e5
Casey Williams <caseywilliams@users.noreply.github.com>
2023-03-14 16:21:14 -0700

    Correct content type for appeals token validation (#12107)
    Appeals OAuth APIs recently started using v3 of the token validation
    service instead of v2, and it appears that although v3 continues to
    validate tokens the same way that v2 did, it requires body content to be
    sent as form data instead of JSON. This fixes the appeals APIs' token
    validation client accordingly.

--- modules/appeals_api/lib/appeals_api/token_validation_client.rb
+++ modules/appeals_api/lib/appeals_api/token_validation_client.rb
@@ -4,19 +4,37 @@ require 'common/client/base'
 require 'token_validation/v2/configuration'
 
 module AppealsApi
+  class TokenValidation
+    module V3
+      class Configuration < ::TokenValidation::V2::Configuration
+        def connection
+          Faraday.new(base_path, headers: base_request_headers, request: request_options) do |conn|
+            conn.request :url_encoded # required for token validation service v3 (v2 accepted json)
+            conn.response :snakecase
+            conn.adapter Faraday.default_adapter
+          end
+        end
+      end
+    end
+  end
+
   # This is based on TokenValidation::V2::Client, but allows for more fine-grained status codes in failure responses
   class TokenValidationClient < ::Common::Client::Base
-    configuration TokenValidation::V2::Configuration
+    configuration TokenValidation::V3::Configuration
 
     def initialize(api_key:)
       @api_key = api_key
     end
 
     def validate_token!(audience:, token:, scopes:)
-      json = { 'aud': audience }
-      headers = { 'apiKey': @api_key, 'Authorization': "Bearer #{token}" }
+      params = { 'aud': audience }
+      headers = {
+        'apiKey': @api_key,
+        'Authorization': "Bearer #{token}",
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
 
-      response = perform(:post, 'v3/validation', json, headers)
+      response = perform(:post, 'v3/validation', params, headers)
 
       raise ::Common::Exceptions::Unauthorized unless response.status == 200
 
