2acda5c645002885dbdece1b75a4e9aaa3d81e9d
Lindsey Hattamer <Lindsey.Hattamer@oddball.io>
2023-03-21 09:54:33 -0400

    add clamav config

--- config/initializers/clamav.rb
+++ config/initializers/clamav.rb
@@ -1,4 +1,11 @@
+## If running via docker
 if Rails.env.development?
   ENV["CLAMD_TCP_HOST"] = "clamav"
   ENV["CLAMD_TCP_PORT"] = "3310"
 end
+
+# ## If running hybrid
+# if Rails.env.development?
+#   ENV["CLAMD_TCP_HOST"] = "0.0.0.0"
+#   ENV["CLAMD_TCP_PORT"] = "33100"
+# end
