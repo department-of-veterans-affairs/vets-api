b1ea2cfa50809300d47c2f83891d4a724051ad12
Eugene Lynch <eugenestapler@gmail.com>
2023-03-22 13:48:58 -0400

    [54398] Force SubmitForm674Job to run after SubmitForm686cJob. (#12186)
    Force SubmitForm674Job to run after SubmitForm686cJob.
    
    In https://github.com/department-of-veterans-affairs/vets-api/pull/11998, as a temporary measure to avoid a race condition, I increased a delay between the these two jobs from two minutes to five minutes. Unfortunately, the 686c job can indeed run more than five minutes after the 674 job is set to run, e.g. when the 686c job is stalled due to timeouts from other endpoints. This PR accommodates for situations like this by eliminating the use of delay, and instead enqueuing the 674 job only after the 686c job has successfully submitted a 686c claim to BGS. In the 686c job, I'm still running the 674 job asynchronously, as was done before, because `send_confirmation_email` was never dependent on the 674 job's success.

--- app/services/bgs/dependent_service.rb
+++ app/services/bgs/dependent_service.rb
@@ -17,10 +17,8 @@ module BGS
 
       vet_info_686c_form_hash = vet_info.to_686c_form_hash
 
-      delay = claim.submittable_686? ? 5.minutes : 0
-
       BGS::SubmitForm686cJob.perform_async(@user.uuid, claim.id, vet_info_686c_form_hash) if claim.submittable_686?
-      BGS::SubmitForm674Job.perform_in(delay, @user.uuid, claim.id, vet_info_686c_form_hash) if claim.submittable_674?
+
       VBMS::SubmitDependentsPdfJob.perform_async(
         claim.id,
         vet_info_686c_form_hash,
