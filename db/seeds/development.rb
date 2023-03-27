1ab6e812991d4be8489b3e70f30e5a6c4c5c1a8a
John Bramley <John.Bramley@va.gov>
2023-03-27 10:37:02 -0600

    [53995] SiS template client config (#12204)
    * wip
    
    * adds vatemplate SiS client config
    
    * adds apitemplate
    
    * adds cookie_template & api_template
    
    * updates

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -3,7 +3,6 @@
 require 'sign_in/constants/auth'
 
 # Create Config for va.gov Sign in Service client
-
 vaweb = SignIn::ClientConfig.find_or_initialize_by(client_id: 'vaweb')
 vaweb.update!(authentication: SignIn::Constants::Auth::COOKIE,
               anti_csrf: true,
@@ -40,3 +39,23 @@ vamock.update!(authentication: SignIn::Constants::Auth::MOCK,
                access_token_audience: 'va.gov',
                logout_redirect_uri: 'http://localhost:3001',
                refresh_token_duration: SignIn::Constants::RefreshToken::VALIDITY_LENGTH_SHORT_MINUTES)
+
+# Create Config for example external client using cookie auth
+sample_client_web = SignIn::ClientConfig.find_or_initialize_by(client_id: 'sample_client_web')
+sample_client_web.update!(authentication: SignIn::Constants::Auth::COOKIE,
+                          anti_csrf: true,
+                          redirect_uri: 'http://localhost:4567/auth/result',
+                          access_token_duration: SignIn::Constants::AccessToken::VALIDITY_LENGTH_SHORT_MINUTES,
+                          access_token_audience: 'sample_client',
+                          logout_redirect_uri: 'http://localhost:4567',
+                          refresh_token_duration: SignIn::Constants::RefreshToken::VALIDITY_LENGTH_SHORT_MINUTES)
+
+# Create Config for example external client using api auth
+sample_client_api = SignIn::ClientConfig.find_or_initialize_by(client_id: 'sample_client_api')
+sample_client_api.update!(authentication: SignIn::Constants::Auth::API,
+                          anti_csrf: false,
+                          redirect_uri: 'http://localhost:4567/auth/result',
+                          access_token_duration: SignIn::Constants::AccessToken::VALIDITY_LENGTH_SHORT_MINUTES,
+                          access_token_audience: 'sample_client',
+                          logout_redirect_uri: 'http://localhost:4567',
+                          refresh_token_duration: SignIn::Constants::RefreshToken::VALIDITY_LENGTH_SHORT_MINUTES)
