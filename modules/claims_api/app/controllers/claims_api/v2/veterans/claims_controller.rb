127983b4843df37b778d1169ddff9d5c09f7dac9
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2023-03-29 13:02:17 -0600

    API-25265-tracked-items-attribute-names (#12279)
    * API-25265-tracked-items-attribute-names; changes attribute name on the tracked items object
    
    * Fixes casing on tracked_item: display_name

--- modules/claims_api/app/controllers/claims_api/v2/veterans/claims_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v2/veterans/claims_controller.rb
@@ -416,12 +416,12 @@ module ClaimsApi
             {
               closed_date: date_present(item[:date_closed]),
               description: item[:short_nm],
-              displayed_name: "Request #{i + 1}", # +1 given a 1 index'd array
+              display_name: "Request #{i + 1}", # +1 given a 1 index'd array
               opened_date: date_present(item[:date_open]),
               overdue: item[:suspns_dt].nil? ? false : item[:suspns_dt] < Time.zone.now, # EVSS generates this field
               requested_date: date_present(item[:req_dt]),
               tracked_item_id: id.to_i,
-              tracked_item_status: status, # EVSS generates this field
+              status: status, # EVSS generates this field
               uploaded: !item[:date_rcvd].nil?, # EVSS generates this field
               uploads_allowed: uploads_allowed # EVSS generates this field
             }
