1853e5382d6a4ece28b3b23154ab7e02358fe56d
mattwrightva <107576133+mattwrightva@users.noreply.github.com>
2023-03-31 08:19:15 -0600

    MHV-41341: download immunizations scaffolding complete (#12282)

---
+++ modules/my_health/app/controllers/my_health/v1/vaccines_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module MyHealth
+  module V1
+    class VaccinesController < PhrController
+      def index
+        vaccine_id = params[:id]
+        vaccines_list = vaccine_id ? [1] : [*0...9]
+        filename = vaccine_id ? 'tmp/vaccine.pdf' : 'tmp/vaccines.pdf'
+
+        MyHealth::PdfConstruction::Generator.new(filename, vaccines_list).make_vaccines_pdf
+
+        pdf_file = File.open(filename)
+        base64 = Base64.encode64(pdf_file.read)
+        response = { pdf: base64 }
+        render json: response
+      end
+    end
+  end
+end
