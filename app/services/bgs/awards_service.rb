aa453bb4a608b9447b19ef19d6fe4f713b3b2774
Trevor Bosaw <bosawt@gmail.com>
2023-04-14 16:11:11 -0700

    [12331] Refactor BGS Services to more clearly show what User model fields are being leveraged (#12332)
    Co-authored-by: Trevor Bosaw <trevor.bosaw@oddball.io>

--- app/services/bgs/awards_service.rb
+++ app/services/bgs/awards_service.rb
@@ -1,29 +1,36 @@
 # frozen_string_literal: true
 
 module BGS
-  ##
-  # Allows retrieval of composite monetary award amounts that veterans are entitled
-  #
-  class AwardsService < BaseService
-    ##
-    # Gets composite monetary awards that veterans are entitled along with relevant metadata
-    #
-    # @return [Hash]
-    #
-    def get_awards
-      @service.awards.find_award_by_participant_id(@user.participant_id, @user.ssn) ||
-        @service.awards.find_award_by_ssn(@user.ssn)
-    rescue => e
-      report_error(e)
+  class AwardsService
+    include SentryLogging
+
+    attr_reader :participant_id, :ssn, :common_name, :email, :icn
+
+    def initialize(user)
+      @participant_id = user.participant_id
+      @ssn = user.ssn
+      @common_name = user.common_name
+      @email = user.email
+      @icn = user.icn
     end
 
-    ##
-    # Returns gross amount of composite monetary awards veteran is entitled to
-    #
-    # @return [String]
-    #
-    def gross_amount
-      get_awards[:gross_amt]
+    def get_awards
+      service.awards.find_award_by_participant_id(participant_id, ssn) || service.awards.find_award_by_ssn(ssn)
+    rescue => e
+      log_exception_to_sentry(e, { icn: }, { team: Constants::SENTRY_REPORTING_TEAM })
+    end
+
+    private
+
+    def service
+      @service ||= BGS::Services.new(external_uid: icn, external_key:)
+    end
+
+    def external_key
+      @external_key ||= begin
+        key = common_name.presence || email
+        key.first(Constants::EXTERNAL_KEY_MAX_LENGTH)
+      end
     end
   end
 end
--- app/services/bgs/people/service.rb
+++ app/services/bgs/people/service.rb
@@ -2,16 +2,47 @@
 
 module BGS
   module People
-    class Service < BaseService
+    class Service
       class VAFileNumberNotFound < StandardError; end
 
+      include SentryLogging
+
+      attr_reader :ssn,
+                  :participant_id,
+                  :common_name,
+                  :email,
+                  :icn
+
+      def initialize(user)
+        @ssn = user.ssn
+        @participant_id = user.participant_id
+        @common_name = user.common_name
+        @email = user.email
+        @icn = user.icn
+      end
+
       def find_person_by_participant_id
-        raw_response = @service.people.find_person_by_ptcpnt_id(@user.participant_id, @user.ssn)
-        report_error(VAFileNumberNotFound.new) if raw_response.blank?
+        raw_response = service.people.find_person_by_ptcpnt_id(participant_id, ssn)
+        if raw_response.blank?
+          log_exception_to_sentry(VAFileNumberNotFound.new, { icn: }, { team: Constants::SENTRY_REPORTING_TEAM })
+        end
         BGS::People::Response.new(raw_response, status: :ok)
       rescue => e
-        report_error(e)
+        log_exception_to_sentry(e, { icn: }, { team: Constants::SENTRY_REPORTING_TEAM })
         BGS::People::Response.new(nil, status: :error)
+      end
+
+      private
+
+      def service
+        @service ||= BGS::Services.new(external_uid: icn, external_key:)
+      end
+
+      def external_key
+        @external_key ||= begin
+          key = common_name.presence || email
+          key.first(Constants::EXTERNAL_KEY_MAX_LENGTH)
+        end
       end
     end
   end
