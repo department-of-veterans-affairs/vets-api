01d0c1e4af7cf3af47f8985464051a90bd6304d6
Garrett <57802560+gmrabian@users.noreply.github.com>
2023-04-14 10:27:16 -0400

    Forms API: parse out characters that break json (#12404)
    * parse out characters that break json
    
    * fix comments

--- modules/forms_api/app/services/forms_api/pdf_filler.rb
+++ modules/forms_api/app/services/forms_api/pdf_filler.rb
@@ -31,12 +31,22 @@ module FormsApi
       b = binding
       b.local_variable_set(:data, data)
       result = ERB.new(template).result(b)
-      JSON.parse(result)
+      JSON.parse(escape_json_string(result))
     end
 
     def metadata
       klass = "FormsApi::#{form_number.titleize.gsub(' ', '')}".constantize.new(data)
       klass.metadata("tmp/#{form_number}-tmp.pdf")
+    end
+
+    def escape_json_string(str)
+      # remove characters that will break the json parser
+      # \u0000-\u001f: control characters in the ASCII table,
+      # characters such as null, tab, line feed, and carriage return
+      # \u0080-\u009f: control characters in the Latin-1 Supplement block of Unicode
+      # \u2000-\u201f: various punctuation and other non-printable characters in Unicode,
+      # including various types of spaces, dashes, and quotation marks.
+      str.gsub(/[\u0000-\u001f\u0080-\u009f\u2000-\u201f]/, ' ')
     end
   end
 end
