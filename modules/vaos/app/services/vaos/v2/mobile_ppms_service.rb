0152eddec80b29bccb7878465bf81b6bad9cd0e7
AJ Magdub <ahmed.magdub@va.gov>
2023-05-02 11:27:19 -0600

    VAOS Implement low-level Rails caching for Provider Name (#12525)
    * feat(vaos): add get_cached_provider method to retrieve cached provider data or fetch and cache it if not already cached
    
     * test(vaos): add tests for get_cached_provider method and refactor before block to include cache and cache clearing

--- modules/vaos/app/services/vaos/v2/mobile_ppms_service.rb
+++ modules/vaos/app/services/vaos/v2/mobile_ppms_service.rb
@@ -10,11 +10,32 @@ module VAOS
         VAOS::PPMSConfiguration.instance
       end
 
+      # Retrieve a provider with a specified ID.
+      #
+      # provider_id - The NPI,  of the provider to retrieve.
+      #
+      # Returns a new OpenStruct object that contains the provider data.
       def get_provider(provider_id)
         params = {}
         with_monitoring do
           response = perform(:get, providers_url_with_id(provider_id), params, headers)
           OpenStruct.new(response[:body])
+        end
+      end
+
+      # Retrieve a cached provider with a specified ID. If the provider is not cached, fetch it and store it in
+      #  cache with an expiration time of 12 hours.
+      #
+      # provider_id - The NPI of the provider to retrieve.
+      #
+      # Returns the cached OpenStruct object that contains the provider data
+      # or fetches and returns the OpenStruct provider data from the backend service if not already cached.
+      #
+      # Note: If changing the cached object from OpenStruct to something more complex, reconsider the
+      # cache strategy to something like caching the raw JSON response.
+      def get_cached_provider(provider_id)
+        Rails.cache.fetch("vaos_ppms_provider_#{provider_id}", expires_in: 12.hours) do
+          get_provider(provider_id)
         end
       end
 
