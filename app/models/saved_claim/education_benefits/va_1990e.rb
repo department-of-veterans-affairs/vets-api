a715b39f39b770b127b51c38521e8ca58905ccac
Nathan Wright <nathanbwright@users.noreply.github.com>
2023-05-10 09:32:31 -0600

    VaNotify - Add feature flag (#12611)

--- app/models/saved_claim/education_benefits/va_1990e.rb
+++ app/models/saved_claim/education_benefits/va_1990e.rb
@@ -6,8 +6,10 @@ class SavedClaim::EducationBenefits::VA1990e < SavedClaim::EducationBenefits
   def after_submit(user)
     return unless Flipper.enabled?(:form1990e_confirmation_email)
 
-    # only sending to unauthenticated users at this time
-    return if user.present?
+    if Flipper.enabled?(:form_confirmation_edu_auth_user) && user.present?
+      # only sending to unauthenticated users at this time
+      return
+    end
 
     parsed_form_data ||= JSON.parse(form)
     email = parsed_form_data['email']
