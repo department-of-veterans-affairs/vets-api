814ae00a2daef8d4091c8f4cc803316ea606df97
Casey Williams <caseywilliams@users.noreply.github.com>
2023-05-12 09:47:05 -0700

    Unique docs spec file for Legacy Appeals API v0 (#12576)

--- modules/appeals_api/spec/docs/legacy_appeals/v0_spec.rb
+++ modules/appeals_api/spec/docs/legacy_appeals/v0_spec.rb
@@ -5,17 +5,24 @@ require Rails.root.join('spec', 'rswag_override.rb').to_s
 
 require 'rails_helper'
 require AppealsApi::Engine.root.join('spec', 'spec_helper.rb')
+require AppealsApi::Engine.root.join('spec', 'support', 'doc_helpers.rb')
+
+def swagger_doc
+  "modules/appeals_api/app/swagger/legacy_appeals/v0/swagger#{DocHelpers.doc_suffix}.json"
+end
 
 # rubocop:disable RSpec/VariableName
-describe 'Legacy Appeals', swagger_doc: DocHelpers.output_json_path, type: :request do
+RSpec.describe 'Legacy Appeals', swagger_doc:, type: :request do
   include DocHelpers
-  let(:apikey) { 'apikey' }
+  let(:Authorization) { 'Bearer TEST_TOKEN' }
 
-  path '/legacy_appeals' do
+  path '/legacy-appeals' do
     get 'Returns eligible appeals in the legacy process for a Veteran.' do
+      scopes = AppealsApi::LegacyAppeals::V0::LegacyAppealsController::OAUTH_SCOPES[:GET]
+
       tags 'Legacy Appeals'
       operationId 'getLegacyAppeals'
-      security DocHelpers.security_config
+      security DocHelpers.security_config(scopes)
       consumes 'application/json'
       produces 'application/json'
       description = 'Returns eligible legacy appeals for a Veteran. A legacy appeal is eligible if a statement of ' \
@@ -32,28 +39,27 @@ describe 'Legacy Appeals', swagger_doc: DocHelpers.output_json_path, type: :requ
       let(:'X-VA-SSN') { '123456789' }
 
       parameter AppealsApi::SwaggerSharedComponents.header_params[:veteran_file_number_header].merge(
-        {
-          description: 'Either X-VA-SSN or X-VA-File-Number is required. Example X-VA-File-Number: 123456789'
-        }
+        { description: 'Either X-VA-SSN or X-VA-File-Number is required. Example X-VA-File-Number: 123456789' }
       )
-      parameter AppealsApi::SwaggerSharedComponents.header_params[:veteran_icn_header]
+      parameter AppealsApi::SwaggerSharedComponents.header_params[:veteran_icn_header].merge({ required: true })
+      let(:'X-VA-ICN') { '1234567890V123456' }
 
       response '200', 'Returns eligible legacy appeals for a Veteran' do
         schema '$ref' => '#/components/schemas/legacyAppeals'
 
         it_behaves_like 'rswag example',
                         desc: 'returns a 200 response',
-                        cassette: 'caseflow/legacy_appeals_get_by_ssn'
+                        cassette: 'caseflow/legacy_appeals_get_by_ssn',
+                        scopes:
       end
 
       response '404', 'Veteran record not found' do
-        let(:'X-VA-SSN') { '234840293' }
-
         schema '$ref' => '#/components/schemas/errorModel'
 
         it_behaves_like 'rswag example',
                         desc: 'returns a 404 response',
-                        cassette: 'caseflow/legacy_appeals_no_veteran_record'
+                        cassette: 'caseflow/legacy_appeals_no_veteran_record',
+                        scopes:
       end
 
       response '422', 'Header Errors' do
@@ -65,7 +71,17 @@ describe 'Legacy Appeals', swagger_doc: DocHelpers.output_json_path, type: :requ
 
           it_behaves_like 'rswag example',
                           desc: 'when X-VA-SSN and X-VA-File-Number are missing',
-                          extract_desc: true
+                          extract_desc: true,
+                          scopes:
+        end
+
+        describe 'missing X-VA-ICN' do
+          let(:'X-VA-ICN') { nil }
+
+          it_behaves_like 'rswag example',
+                          desc: 'when X-VA-ICN is missing',
+                          extract_desc: true,
+                          scopes:
         end
 
         describe 'malformed SSN' do
@@ -73,7 +89,8 @@ describe 'Legacy Appeals', swagger_doc: DocHelpers.output_json_path, type: :requ
 
           it_behaves_like 'rswag example',
                           desc: 'when SSN formatted incorrectly',
-                          extract_desc: true
+                          extract_desc: true,
+                          scopes:
         end
 
         context 'malformed ICN' do
@@ -81,15 +98,14 @@ describe 'Legacy Appeals', swagger_doc: DocHelpers.output_json_path, type: :requ
 
           it_behaves_like 'rswag example',
                           desc: 'when ICN formatted incorrectly',
-                          extract_desc: true
+                          extract_desc: true,
+                          scopes:
         end
       end
 
       it_behaves_like 'rswag 500 response'
 
       response '502', 'Unknown Error' do
-        let(:'X-VA-SSN') { nil }
-
         schema type: :object,
                properties: {
                  errors: {
@@ -118,12 +134,16 @@ describe 'Legacy Appeals', swagger_doc: DocHelpers.output_json_path, type: :requ
                }
 
         before do |example|
-          submit_request(example.metadata)
+          with_rswag_auth(scopes) do
+            submit_request(example.metadata)
+          end
         end
 
+        # rubocop:disable RSpec/NoExpectationExample
         it 'returns a 502 response' do |_example|
           # NOOP
         end
+        # rubocop:enable RSpec/NoExpectationExample
       end
     end
   end
--- modules/appeals_api/spec/support/doc_helpers.rb
+++ modules/appeals_api/spec/support/doc_helpers.rb
@@ -45,7 +45,7 @@ module DocHelpers
   end
 
   def self.security_config(oauth_scopes = [])
-    if oauth_scopes.any?
+    if oauth_scopes.any? || DocHelpers.api_name != 'decision_reviews'
       [{ productionOauth: oauth_scopes }, { sandboxOauth: oauth_scopes }, { bearer_token: [] }]
     else
       [{ apikey: [] }]
