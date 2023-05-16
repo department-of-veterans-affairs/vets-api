19484a81376a04ae857fc1dd2db3c85e77b2153c
gia-lexa <103389320+gia-lexa@users.noreply.github.com>
2023-05-16 10:09:26 -0700

    Removes "require_dependency" from test_user_dashboard module (#12582)
    Co-authored-by: Gia Antoniades <giaantoniades@Gias-MacBook-Pro.local>

--- modules/test_user_dashboard/app/controllers/test_user_dashboard/tud_accounts_controller.rb
+++ modules/test_user_dashboard/app/controllers/test_user_dashboard/tud_accounts_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_dependency 'test_user_dashboard/application_controller'
-
 module TestUserDashboard
   class TudAccountsController < ApplicationController
     include ActionView::Helpers::SanitizeHelper
