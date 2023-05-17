fc6e4fb48c553001f3cc55a409b772665099b22f
mchristiansonVA <95487885+mchristiansonVA@users.noreply.github.com>
2023-05-17 12:18:15 -0400

    Api 24829 section 3 validation (#12658)
    * Incremental commit
    
    * Misc fixes, add rspec tests
    
    * Misc fixes and final cleanup
    
    * Add swagger
    
    * Remove extraneous context blocks
    
    * Rubocop whitespace fix
    
    * Clean up return value handling
    
    * Rubocop spacing fix
    
    * Update test data and fix test

--- modules/claims_api/app/controllers/concerns/claims_api/v2/disability_compensation_validation.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/v2/disability_compensation_validation.rb
@@ -12,6 +12,8 @@ module ClaimsApi
         validate_form_526_claimant_certification!
         # ensure mailing address country is valid
         validate_form_526_current_mailing_address_country!
+        # ensure homeless information is valid
+        validate_form_526_veteran_homelessness!
       end
 
       def validate_form_526_submission_claim_date!
@@ -39,6 +41,78 @@ module ClaimsApi
 
       def valid_countries
         @valid_countries ||= ClaimsApi::BRD.new(request).countries
+      end
+
+      def validate_form_526_veteran_homelessness!
+        handle_empty_other_description
+
+        if too_many_homelessness_attributes_provided?
+          raise ::Common::Exceptions::UnprocessableEntity.new(
+            detail: "Must define only one of 'homeless.currentlyHomeless' or " \
+                    "'homeless.riskOfBecomingHomeless'"
+          )
+        end
+
+        if unnecessary_homelessness_point_of_contact_provided?
+          raise ::Common::Exceptions::UnprocessableEntity.new(
+            detail: "If 'homeless.pointOfContact' is defined, then one of " \
+                    "'homeless.currentlyHomeless' or 'homeless.riskOfBecomingHomeless' is required"
+          )
+        end
+
+        if missing_point_of_contact?
+          raise ::Common::Exceptions::UnprocessableEntity.new(
+            detail: "If one of 'homeless.currentlyHomeless' or 'homeless.riskOfBecomingHomeless' is " \
+                    "defined, then 'homeless.pointOfContact' is required"
+          )
+        end
+      end
+
+      def get_homelessness_attributes
+        currently_homeless_attr = form_attributes.dig('homeless', 'currentlyHomeless')
+        homelessness_risk_attr = form_attributes.dig('homeless', 'riskOfBecomingHomeless')
+        [currently_homeless_attr, homelessness_risk_attr]
+      end
+
+      def handle_empty_other_description
+        currently_homeless_attr, homelessness_risk_attr = get_homelessness_attributes
+
+        # Set otherDescription to ' ' to bypass docker container validation
+        if currently_homeless_attr.present?
+          homeless_situation_options = currently_homeless_attr['homelessSituationOptions']
+          other_description = currently_homeless_attr['otherDescription']
+          if homeless_situation_options == 'OTHER' && other_description.blank?
+            form_attributes['homeless']['currentlyHomeless']['otherDescription'] = ' '
+          end
+        elsif homelessness_risk_attr.present?
+          living_situation_options = homelessness_risk_attr['livingSituationOptions']
+          other_description = homelessness_risk_attr['otherDescription']
+          if living_situation_options == 'other' && other_description.blank?
+            form_attributes['homeless']['riskOfBecomingHomeless']['otherDescription'] = ' '
+          end
+        end
+      end
+
+      def too_many_homelessness_attributes_provided?
+        currently_homeless_attr, homelessness_risk_attr = get_homelessness_attributes
+        # EVSS does not allow both attributes to be provided at the same time
+        currently_homeless_attr.present? && homelessness_risk_attr.present?
+      end
+
+      def unnecessary_homelessness_point_of_contact_provided?
+        currently_homeless_attr, homelessness_risk_attr = get_homelessness_attributes
+        homelessness_poc_attr = form_attributes.dig('homeless', 'pointOfContact')
+
+        # EVSS does not allow passing a 'pointOfContact' if neither homelessness attribute is provided
+        currently_homeless_attr.blank? && homelessness_risk_attr.blank? && homelessness_poc_attr.present?
+      end
+
+      def missing_point_of_contact?
+        homelessness_poc_attr = form_attributes.dig('homeless', 'pointOfContact')
+        currently_homeless_attr, homelessness_risk_attr = get_homelessness_attributes
+
+        # 'pointOfContact' is required when either currentlyHomeless or homelessnessRisk is provided
+        homelessness_poc_attr.blank? && (currently_homeless_attr.present? || homelessness_risk_attr.present?)
       end
     end
   end
