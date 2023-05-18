9ac40d1c23dd2cbdc246f8c53421d0d3132fdbbf
Lindsey Hattamer <Lindsey.Hattamer@oddball.io>
2023-05-18 12:26:02 -0400

    remove debug line

--- config/initializers/sidekiq_alive.rb
+++ config/initializers/sidekiq_alive.rb
@@ -3,5 +3,4 @@
 SidekiqAlive.setup do |config|
   # use the POD_NAME environment variable to set the queue name
   config.queue_prefix = "#{ENV.fetch('POD_NAME', 'sidekiq_alive_default_queue')}_sidekiq_alive"
-  Rails.logger.info "SidekiqAlive initialized with queue_prefix: #{config.queue_prefix}"
 end
