965c68490095b35aea1d7292c5ad9cd45df23940
Jason C <jasonconig@me.com>
2023-05-19 14:55:19 -0700

    Mobile: Secure messaging thread schema update (#12738)
    * Add v1 message thread endpoint using new api
    
    * Remove pointless override
    
    * Copy over vcr, remove module from controller, add more thorough tests
    
    * Remove redundant test
    
    * Reset cassettes after tests
    
    * Change name of test
    
    * Debug failures
    
    * Relocate cassette and uncomment test
    
    * Add back .to_i for params[:id]
    
    * Add logging to debug
    
    * Debugging
    
    * Further changes required by new serializer
    
    * Update to use new schema from upstream
    
    * Remove extra route
    
    ---------
    
    Co-authored-by: Andrew Herzberg <andrew.herzberg@adhocteam.us>

---
+++ modules/mobile/app/serializers/mobile/v1/messages_serializer.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Mobile
+  module V1
+    class MessagesSerializer < ActiveModel::Serializer
+      def id
+        object.message_id
+      end
+
+      def body
+        object.message_body
+      end
+
+      attribute :message_id
+      attribute :body
+      attribute :category
+      attribute :subject
+      attribute :message_body
+      attribute :attachment
+      attribute :sent_date
+      attribute :sender_id
+      attribute :sender_name
+      attribute :recipient_id
+      attribute :recipient_name
+      attribute :read_receipt
+      attribute :triage_group_name
+      attribute :proxy_sender_name
+      attribute :thread_id
+      attribute :folder_id
+      attribute :draft_date
+      attribute :to_date
+      attribute :has_attachments
+
+      link(:self) { Mobile::UrlHelper.new.v0_message_url(object.message_id) }
+    end
+  end
+end
