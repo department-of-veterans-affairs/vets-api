8bda850b7f3f500a3112dc95159399b8f6884393
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2023-05-23 11:51:13 -0600

    API-26271-526-v2-sec-5-disability-pdf-mapping (#12725)
    * Adds some logic for disability mapping
    
    * Adds most of disability pdf mapping
    
    * flattens secondary disabilities & maps them.
    
    * Removes unneeded attributes, and adds more test expectations.
    
    * Refactor
    
    * Adds test for the hasConditionsRelatedToToxicExposures attribute

--- modules/claims_api/lib/claims_api/v2/disability_compensation_pdf_mapper.rb
+++ modules/claims_api/lib/claims_api/v2/disability_compensation_pdf_mapper.rb
@@ -14,12 +14,13 @@ module ClaimsApi
         homeless_attributes
         chg_addr_attributes
         veteran_info
+        disability_attributes
 
         @pdf_data
       end
 
       def claim_attributes
-        @pdf_data[:data][:attributes] = @auto_claim.deep_symbolize_keys
+        @pdf_data[:data][:attributes] = @auto_claim&.deep_symbolize_keys
         claim_date
         veteran_info
 
@@ -27,14 +28,16 @@ module ClaimsApi
       end
 
       def claim_date
-        @pdf_data[:data][:attributes].merge!(claimCertificationAndSignature: { dateSigned: @auto_claim['claimDate'] })
+        @pdf_data[:data][:attributes].merge!(claimCertificationAndSignature: {
+                                               dateSigned: @auto_claim&.dig('claimDate')
+                                             })
         @pdf_data[:data][:attributes].delete(:claimDate)
 
         @pdf_data
       end
 
       def homeless_attributes
-        @pdf_data[:data][:attributes][:homelessInformation] = @auto_claim['homeless'].deep_symbolize_keys
+        @pdf_data[:data][:attributes][:homelessInformation] = @auto_claim&.dig('homeless')&.deep_symbolize_keys
         @pdf_data[:data][:attributes].delete(:homeless)
 
         homeless_at_risk_or_currently
@@ -49,7 +52,8 @@ module ClaimsApi
         if currently && !at_risk
           @pdf_data[:data][:attributes][:homelessInformation].merge!(areYouCurrentlyHomeless: true)
         else
-          @pdf_data[:data][:attributes][:homelessInformation].merge!(areYouAtRiskOfBecomingHomeless: true)
+          homeless = @pdf_data[:data][:attributes][:homelessInformation].present?
+          @pdf_data[:data][:attributes][:homelessInformation].merge!(areYouAtRiskOfBecomingHomeless: true) if homeless
         end
 
         @pdf_data
@@ -57,7 +61,7 @@ module ClaimsApi
 
       def chg_addr_attributes
         @pdf_data[:data][:attributes][:changeOfAddress] =
-          @auto_claim['changeOfAddress'].deep_symbolize_keys
+          @auto_claim&.dig('changeOfAddress')&.deep_symbolize_keys
 
         chg_addr_zip
 
@@ -65,23 +69,24 @@ module ClaimsApi
       end
 
       def chg_addr_zip
-        zip = @auto_claim['changeOfAddress']['zipFirstFive'] +
-              @auto_claim['changeOfAddress']['zipLastFour']
-        @pdf_data[:data][:attributes][:changeOfAddress].merge!(zip:)
+        zip = (@auto_claim&.dig('changeOfAddress', 'zipFirstFive') || '') +
+              (@auto_claim&.dig('changeOfAddress', 'zipLastFour') || '')
+        addr = @pdf_data&.dig(:data, :attributes, :identificationInformation, :mailingAddress).present?
+        @pdf_data[:data][:attributes][:changeOfAddress].merge!(zip:) if addr
       end
 
       def toxic_exposure_attributes
         @pdf_data[:data][:attributes].merge!(
-          exposureInformation: { toxicExposure: @auto_claim['toxicExposure'].deep_symbolize_keys }
+          exposureInformation: { toxicExposure: @auto_claim&.dig('toxicExposure')&.deep_symbolize_keys }
         )
         @pdf_data[:data][:attributes].delete(:toxicExposure)
 
-        conditions_related_to_exposure?
+        @pdf_data
       end
 
       def veteran_info
         @pdf_data[:data][:attributes].merge!(
-          identificationInformation: @auto_claim['veteranIdentification'].deep_symbolize_keys
+          identificationInformation: @auto_claim&.dig('veteranIdentification')&.deep_symbolize_keys
         )
         zip
 
@@ -89,16 +94,62 @@ module ClaimsApi
       end
 
       def zip
-        zip = @auto_claim['veteranIdentification']['mailingAddress']['zipFirstFive'] +
-              @auto_claim['veteranIdentification']['mailingAddress']['zipLastFour']
-        @pdf_data[:data][:attributes][:identificationInformation][:mailingAddress].merge!(zip:)
+        zip = (@auto_claim&.dig('veteranIdentification', 'mailingAddress', 'zipFirstFive') || '') +
+              (@auto_claim&.dig('veteranIdentification', 'mailingAddress', 'zipLastFour') || '')
+        mailing_addr = @pdf_data&.dig(:data, :attributes, :identificationInformation, :mailingAddress).present?
+        @pdf_data[:data][:attributes][:identificationInformation][:mailingAddress].merge!(zip:) if mailing_addr
+      end
+
+      def disability_attributes
+        @pdf_data[:data][:attributes][:claimInformation] = {}
+        @pdf_data[:data][:attributes][:claimInformation].merge!(
+          { disabilities: [] }
+        )
+        disabilities = transform_disabilities
+        details = disabilities[:data][:attributes][:claimInformation][:disabilities].map(
+          &:deep_symbolize_keys
+        )
+
+        @pdf_data[:data][:attributes][:claimInformation][:disabilities] = details
+
+        conditions_related_to_exposure?
+
+        @pdf_data
+      end
+
+      def transform_disabilities
+        d2 = []
+        claim_disabilities = @auto_claim&.dig('disabilities')&.map do |disability|
+          disability['disability'] = disability['name']
+          disability.delete('name')
+          disability.delete('classificationCode')
+          disability.delete('ratedDisabilityId')
+          disability.delete('diagnosticCode')
+          disability.delete('disabilityActionType')
+          sec_dis = disability['secondaryDisabilities'].map do |secondary_disability|
+            secondary_disability['disability'] = secondary_disability['name']
+            secondary_disability
+          end
+          d2 << sec_dis
+          disability.delete('secondaryDisabilities')
+          disability
+        end
+        claim_disabilities << d2
+        @pdf_data[:data][:attributes][:claimInformation][:disabilities] = claim_disabilities.flatten.compact
+
+        @pdf_data
       end
 
       def conditions_related_to_exposure?
         # If any disability is included in the request with 'isRelatedToToxicExposure' set to true,
         # set exposureInformation.hasConditionsRelatedToToxicExposures to true.
-        # This will check 'YES' for box 15A.
-        # TODO: for disability mapping ticket
+        @pdf_data[:data][:attributes][:exposureInformation][:hasConditionsRelatedToToxicExposures] = nil
+        has_conditions = @pdf_data[:data][:attributes][:claimInformation][:disabilities].any? do |disabiity|
+          disabiity[:isRelatedToToxicExposure] == true
+        end
+        @pdf_data[:data][:attributes][:exposureInformation][:hasConditionsRelatedToToxicExposures] = has_conditions
+
+        @pdf_data
       end
     end
   end
