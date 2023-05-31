1677bd54937b7081aedcc5bec83775eb179ac2d8
Casey Williams <caseywilliams@users.noreply.github.com>
2023-05-31 09:31:55 -0700

    API-26761 Optimize VBADocuments::UploadRemover job (#12839)
    Updates SQL in the job to hopefully reduce execution time

--- modules/vba_documents/app/workers/vba_documents/upload_remover.rb
+++ modules/vba_documents/app/workers/vba_documents/upload_remover.rb
@@ -21,13 +21,21 @@ module VBADocuments
     def perform
       return unless Settings.vba_documents.s3.enabled
 
-      rows = VBADocuments::UploadSubmission.where(REMOVAL_QUERY, EXPIRATION_TIME.ago)
+      rows = VBADocuments::UploadSubmission.select(:id, :guid, :status, :s3_deleted, :created_at)
+                                           .where(REMOVAL_QUERY, EXPIRATION_TIME.ago)
 
-      rows.find_each do |upload|
-        Rails.logger.info("VBADocuments: Cleaning up s3: #{upload.inspect}")
-        store.delete(upload.guid) if store.object(upload.guid).exists?
-        upload.s3_deleted = true
-        upload.save!(touch: false)
+      rows.find_in_batches do |batch|
+        VBADocuments::UploadSubmission.transaction do
+          batch.each do |upload|
+            Rails.logger.info("VBADocuments: Cleaning up s3: #{upload.inspect}")
+
+            store.delete(upload.guid) if store.object(upload.guid).exists?
+          end
+
+          # rubocop:disable Rails/SkipsModelValidations
+          VBADocuments::UploadSubmission.where(id: batch.pluck(:id)).update_all(s3_deleted: true)
+          # rubocop:enable Rails/SkipsModelValidations
+        end
       end
     end
 
