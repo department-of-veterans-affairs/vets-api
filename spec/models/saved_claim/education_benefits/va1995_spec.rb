5d02f24825096019668bf06bf420980bfe2759e9
Neal Hill <118899033+nhill360@users.noreply.github.com>
2023-06-01 08:03:03 -0700

    824 create 22 1995 confirmation email trigger (#12514)

--- spec/models/saved_claim/education_benefits/va1995_spec.rb
+++ spec/models/saved_claim/education_benefits/va1995_spec.rb
@@ -9,4 +9,31 @@ RSpec.describe SavedClaim::EducationBenefits::VA1995 do
   it_behaves_like 'saved_claim'
 
   validate_inclusion(:form_id, '22-1995')
+
+  describe '#after_submit' do
+    let(:user) { create(:user) }
+
+    describe 'sends confirmation email for the 1995' do
+      it 'successfully submits' do
+        allow(VANotify::EmailJob).to receive(:perform_async)
+
+        subject = create(:va1995_full_form)
+        confirmation_number = subject.education_benefits_claim.confirmation_number
+
+        subject.after_submit(user)
+
+        expect(VANotify::EmailJob).to have_received(:perform_async).with(
+          'test@sample.com',
+          'form1995_confirmation_email_template_id',
+          {
+            'first_name' => 'FIRST',
+            'benefit' => 'Transfer of Entitlement Program (TOE)',
+            'date_submitted' => Time.zone.today.strftime('%B %d, %Y'),
+            'confirmation_number' => confirmation_number,
+            'regional_office_address' => "P.O. Box 4616\nBuffalo, NY 14240-4616"
+          }
+        )
+      end
+    end
+  end
 end
