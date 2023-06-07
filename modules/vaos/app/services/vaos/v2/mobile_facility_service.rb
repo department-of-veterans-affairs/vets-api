1f79f6237eb0dff75429cda4fb16b94b623070b6
AJ Magdub <ahmed.magdub@va.gov>
2023-06-07 07:54:54 -0600

    VAOS Delete Feature Toggle 'va_online_scheduling_convert_utc_to_local' (#12915)
    * chore(features.yml): The feature toggle va_online_scheduling_convert_utc_to_local was removed as it is no longer needed.

--- modules/vaos/app/services/vaos/v2/mobile_facility_service.rb
+++ modules/vaos/app/services/vaos/v2/mobile_facility_service.rb
@@ -6,7 +6,7 @@ require 'common/client/errors'
 module VAOS
   module V2
     class MobileFacilityService < VAOS::SessionService
-      # Retrieves information about a VA clinic from the Mobile Facility Service.
+      # Retrieves information about a VA clinic from the VAOS Service.
       #
       # @param station_id [String] the ID of the VA facility where the clinic is located
       # @param clinic_id [String] the ID of the clinic to retrieve
@@ -23,13 +23,13 @@ module VAOS
         end
       end
 
-      # Retrieves a clinic from the cache if it exists, otherwise retrieves the clinic from the Mobile Facility Service.
+      # Retrieves a clinic from the cache if it exists, otherwise retrieves the clinic from the VAOS Service.
       #
       # @param station_id [String] the ID of the VA facility where the clinic is located
       # @param clinic_id [String] the ID of the clinic to retrieve
       #
       # @return [OpenStruct] an OpenStruct containing information about the clinic,
-      #         retrieved from either the cache or the Mobile Facility Service.
+      #         retrieved from either the cache or the VAOS Service.
       #
       def get_clinic_with_cache(station_id:, clinic_id:)
         Rails.cache.fetch("vaos_clinic_#{station_id}_#{clinic_id}", expires_in: 12.hours) do
