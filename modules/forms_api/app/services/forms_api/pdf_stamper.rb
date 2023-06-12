382c57b1ba523d540d604fd837836a3d4cb8b618
Garrett <57802560+gmrabian@users.noreply.github.com>
2023-06-12 14:51:32 -0400

    VA forms: Add form 21P-0847 (#12960)
    * initially generated files for 21p-0847
    
    * add form mappings and tests
    
    * rename files
    
    * one more name change
    
    * refactor multistamp methods and add tests

--- modules/forms_api/app/services/forms_api/pdf_stamper.rb
+++ modules/forms_api/app/services/forms_api/pdf_stamper.rb
@@ -4,12 +4,12 @@ require 'central_mail/datestamp_pdf'
 
 module FormsApi
   class PdfStamper
-    FORM_REQUIRES_STAMP = %w[26-4555 21-4142 21-10210].freeze
+    FORM_REQUIRES_STAMP = %w[26-4555 21-4142 21-10210 21P-0847].freeze
     SUBMISSION_TEXT = 'Signed electronically and submitted via VA.gov at '
 
     def self.stamp_pdf(generated_form_path, data)
       if FORM_REQUIRES_STAMP.include? data['form_number']
-        stamp_method = "stamp#{data['form_number'].gsub('-', '')}"
+        stamp_method = "stamp#{data['form_number'].gsub('-', '')}".downcase
         send(stamp_method, generated_form_path, data)
       end
       current_time = Time.current.in_time_zone('America/Chicago').strftime('%H:%M:%S')
@@ -33,36 +33,57 @@ module FormsApi
       last_name = data.dig('preparer_identification', 'preparer_full_name', 'last')
       suffix = data.dig('preparer_identification', 'preparer_full_name', 'suffix')
       signature_text = "#{first_name} #{middle_name} #{last_name} #{suffix}"
+      page_configuration = [
+        { type: :new_page },
+        { type: :text, position: desired_stamps[0] },
+        { type: :new_page }
+      ]
 
-      stamp_path = Common::FileHelpers.random_file_path
-      Prawn::Document.generate(stamp_path, margin: [0, 0]) do |pdf|
-        pdf.start_new_page
-        pdf.draw_text signature_text, at: desired_stamps[0], size: 16
-        pdf.start_new_page
-      end
-
-      multistamp(generated_form_path, stamp_path)
-    rescue
-      Rails.logger.error "Failed to generate stamped file: #{e.message}"
-      raise
-    ensure
-      Common::FileHelpers.delete_file_if_exists(stamp_path) if defined?(stamp_path)
+      multistamp(generated_form_path, signature_text, page_configuration)
     end
 
     def self.stamp2110210(generated_form_path, data)
-      first_name, middle_name, last_name = get_name_to_stamp10210(data)
       desired_stamps = [[50, 160]]
+      first_name, middle_name, last_name = get_name_to_stamp10210(data)
       signature_text = "#{first_name} #{middle_name} #{last_name}"
+      page_configuration = [
+        { type: :new_page },
+        { type: :new_page },
+        { type: :text, position: desired_stamps[0] }
+      ]
 
+      multistamp(generated_form_path, signature_text, page_configuration)
+    end
+
+    def self.stamp21p0847(generated_form_path, data)
+      desired_stamps = [[50, 190]]
+      first_name = data.dig('preparer_name', 'first')
+      middle_name = data.dig('preparer_name', 'middle')
+      last_name = data.dig('preparer_name', 'last')
+      signature_text = "#{first_name} #{middle_name} #{last_name}"
+      page_configuration = [
+        { type: :new_page },
+        { type: :text, position: desired_stamps[0] }
+      ]
+
+      multistamp(generated_form_path, signature_text, page_configuration)
+    end
+
+    def self.multistamp(generated_form_path, signature_text, page_configuration)
       stamp_path = Common::FileHelpers.random_file_path
       Prawn::Document.generate(stamp_path, margin: [0, 0]) do |pdf|
-        pdf.start_new_page
-        pdf.start_new_page
-        pdf.draw_text signature_text, at: desired_stamps[0], size: 16
+        page_configuration.each do |config|
+          case config[:type]
+          when :text
+            pdf.draw_text signature_text, at: config[:position], size: 16
+          when :new_page
+            pdf.start_new_page
+          end
+        end
       end
 
-      multistamp(generated_form_path, stamp_path)
-    rescue
+      perform_multistamp(generated_form_path, stamp_path)
+    rescue => e
       Rails.logger.error "Failed to generate stamped file: #{e.message}"
       raise
     ensure
@@ -99,7 +120,7 @@ module FormsApi
       File.rename(current_file_path, generated_form_path)
     end
 
-    def self.multistamp(generated_form_path, stamp_path)
+    def self.perform_multistamp(generated_form_path, stamp_path)
       out_path = "#{Common::FileHelpers.random_file_path}.pdf"
       pdftk = PdfFill::Filler::PDF_FORMS
       pdftk.multistamp(generated_form_path, stamp_path, out_path)
