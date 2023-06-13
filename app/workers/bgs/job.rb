6258392c55dff3a170122f4965874fe352895adf
Eugene Lynch <eugenestapler@gmail.com>
2023-06-13 18:52:35 -0400

    Handle special characters in names and addresses sent to BGS. (#12968)
    BGS doesn't accept name and address_line fields with non-ASCII characters (e.g. ü, ñ), and doesn't accept names with apostrophes. The method introduced by this PR recursively iterates through a given hash and strips unprocessable characters from name and address_line fields. The method is called in `SubmitForm686cJob` and `SubmitForm674Job` with an enormous form payload potentially containing many names and addresses. See `spec/factories/686c/form_686c_674.rb` for an example of such a payload.

--- app/workers/bgs/job.rb
+++ app/workers/bgs/job.rb
@@ -17,5 +17,31 @@ module BGS
       form.user_account = copy.user_account
       form.update(form_data: copy.form_data)
     end
+
+    # BGS doesn't accept name and address_line fields with non-ASCII characters (e.g. ü, ñ), and doesn't accept names
+    # with apostrophes. This method recursively iterates through a given hash and strips unprocessable characters
+    # from name and address_line fields. The method is called in `SubmitForm686cJob` and `SubmitForm674Job` with an
+    # enormous form payload potentially containing many names and addresses.
+    # See `spec/factories/686c/form_686c_674.rb` for an example of such a payload.
+    def normalize_names_and_addresses!(hash)
+      hash.each do |key, val|
+        case val
+        when Hash
+          normalize_names_and_addresses!(val)
+        when Array
+          val.each { |v| normalize_names_and_addresses!(v) if v.is_a?(Hash) }
+        else
+          is_name_key = %w[first middle last].include?(key)
+          if val && (is_name_key || key.include?('address_line'))
+            # NFKD decomposes composite characters (e.g. ü, ñ) into their individual components, and here, `gsub`
+            # removes any non-ASCII components (e.g. ü -> u; ñ -> n).
+            val = val.unicode_normalize(:nfkd).gsub(/[^\p{ASCII}]/, '')
+            # Interestingly, BGS permits names with forward slashes and hyphens, but not apostrophes.
+            val.gsub!(%r{[^a-zA-Z\s/-]}, '') if is_name_key
+            hash[key] = val
+          end
+        end
+      end
+    end
   end
 end
--- spec/jobs/bgs/job_spec.rb
+++ spec/jobs/bgs/job_spec.rb
@@ -48,4 +48,64 @@ RSpec.describe BGS::Job, type: :job do
       expect(in_progress_form).to eq(true)
     end
   end
+
+  describe '#normalize_names_and_addresses!(hash)' do
+    it 'removes non-ASCII characters from name and address values in given hash, modifying the hash in-place' do
+      # rubocop:disable Layout/LineLength
+      raw_string = "Téśt'-Strïñg/1"
+      normalized_name_string = 'Test-String/'
+      normalized_address_string = "Test'-String/1"
+
+      # Should modify name fields
+      all_flows_payload['veteran_information']['full_name']['first'] = raw_string
+      all_flows_payload['veteran_information']['full_name']['middle'] = raw_string
+      all_flows_payload['veteran_information']['full_name']['last'] = raw_string
+
+      # Should modify name fields deeper in the hash
+      all_flows_payload['dependents_application']['report_divorce']['full_name']['first'] = raw_string
+      all_flows_payload['dependents_application']['report_divorce']['full_name']['middle'] = raw_string
+      all_flows_payload['dependents_application']['report_divorce']['full_name']['last'] = raw_string
+
+      # Should modify address_line fields
+      all_flows_payload['dependents_application']['last_term_school_information']['address']['address_line1'] = raw_string
+      all_flows_payload['dependents_application']['last_term_school_information']['address']['address_line2'] = raw_string
+      all_flows_payload['dependents_application']['last_term_school_information']['address']['address_line3'] = raw_string
+
+      # Should modify fields, even within arrays of hashes
+      all_flows_payload['dependents_application']['children_to_add'][0]['child_address_info']['address']['address_line1'] = raw_string
+      all_flows_payload['dependents_application']['children_to_add'][0]['child_address_info']['address']['address_line2'] = raw_string
+      all_flows_payload['dependents_application']['children_to_add'][0]['child_address_info']['address']['address_line3'] = raw_string
+
+      # Should modify fields within arrays of hashes, beyond the first element in such arrays
+      all_flows_payload['dependents_application']['children_to_add'][1]['full_name']['first'] = raw_string
+      all_flows_payload['dependents_application']['children_to_add'][1]['full_name']['middle'] = raw_string
+      all_flows_payload['dependents_application']['children_to_add'][1]['full_name']['last'] = raw_string
+
+      # Should not modify other fields with similar names to the name and address fields that should be modified (e.g. should not modify country_name, email_address, etc.)
+      all_flows_payload['dependents_application']['veteran_contact_information']['email_address'] = raw_string
+      all_flows_payload['dependents_application']['last_term_school_information']['address']['country_name'] = raw_string
+
+      BGS::Job.new.normalize_names_and_addresses!(all_flows_payload)
+
+      expect(all_flows_payload['veteran_information']['full_name']['first']).to eq(normalized_name_string)
+      expect(all_flows_payload['veteran_information']['full_name']['middle']).to eq(normalized_name_string)
+      expect(all_flows_payload['veteran_information']['full_name']['last']).to eq(normalized_name_string)
+      expect(all_flows_payload['dependents_application']['report_divorce']['full_name']['first']).to eq(normalized_name_string)
+      expect(all_flows_payload['dependents_application']['report_divorce']['full_name']['middle']).to eq(normalized_name_string)
+      expect(all_flows_payload['dependents_application']['report_divorce']['full_name']['last']).to eq(normalized_name_string)
+      expect(all_flows_payload['dependents_application']['last_term_school_information']['address']['address_line1']).to eq(normalized_address_string)
+      expect(all_flows_payload['dependents_application']['last_term_school_information']['address']['address_line2']).to eq(normalized_address_string)
+      expect(all_flows_payload['dependents_application']['last_term_school_information']['address']['address_line3']).to eq(normalized_address_string)
+      expect(all_flows_payload['dependents_application']['children_to_add'][0]['child_address_info']['address']['address_line1']).to eq(normalized_address_string)
+      expect(all_flows_payload['dependents_application']['children_to_add'][0]['child_address_info']['address']['address_line2']).to eq(normalized_address_string)
+      expect(all_flows_payload['dependents_application']['children_to_add'][0]['child_address_info']['address']['address_line3']).to eq(normalized_address_string)
+      expect(all_flows_payload['dependents_application']['children_to_add'][1]['full_name']['first']).to eq(normalized_name_string)
+      expect(all_flows_payload['dependents_application']['children_to_add'][1]['full_name']['middle']).to eq(normalized_name_string)
+      expect(all_flows_payload['dependents_application']['children_to_add'][1]['full_name']['last']).to eq(normalized_name_string)
+
+      expect(all_flows_payload['dependents_application']['veteran_contact_information']['email_address']).to eq(raw_string)
+      expect(all_flows_payload['dependents_application']['last_term_school_information']['address']['country_name']).to eq(raw_string)
+      # rubocop:enable Layout/LineLength
+    end
+  end
 end
