149a98a043953ada3decf6178babf47134235be5
Jason C <jasonconig@me.com>
2023-06-14 09:51:44 -0700

    Mobile: Update endpoint to include feature flag for flipping to lighthouse fo… (#12553)
    * Update endpoint to include feature flag for flipping to lighthouse for direct deposit information
    
    * revert accidental changes
    
    * Further changes for payment updates
    
    * Add spec file
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Final progress
    
    * Remove old code
    
    * Use shared lighthouse auth
    
    * Alias method for readability and update naming of memoized variable
    
    * Per user flipper
    
    * Remove unused payment_account method
    
    * Remove unused require

--- modules/mobile/app/controllers/mobile/v0/payment_information_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/payment_information_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'evss/ppiu/service'
+require 'lighthouse/direct_deposit/client'
+require 'lighthouse/direct_deposit/error_parser'
 require_relative '../concerns/sso_logging'
 
 module Mobile
@@ -8,21 +10,33 @@ module Mobile
     class PaymentInformationController < ApplicationController
       include Mobile::Concerns::SSOLogging
 
-      before_action { authorize :evss, :access? }
-      before_action { authorize :ppiu, :access? }
+      before_action { authorize :evss, :access? unless lighthouse? }
+      before_action { authorize :ppiu, :access? unless lighthouse? }
+      before_action { authorize :lighthouse, :mobile_access? if lighthouse? }
+
       before_action :validate_pay_info, only: :update
-      before_action(only: :update) { authorize(:ppiu, :access_update?) }
-      after_action(only: :update) { proxy.send_confirmation_email }
+      before_action(only: :update) { authorize(:ppiu, :access_update?) unless lighthouse? }
+      after_action(only: :update) { evss_proxy.send_confirmation_email unless lighthouse? }
+      after_action(only: :update) { send_lighthouse_confirmation_email if lighthouse? }
 
       def index
-        payment_information = proxy.get_payment_information
+        payment_information = if lighthouse?
+                                lighthouse_adapter.parse(lighthouse_service.get_payment_info)
+                              else
+                                evss_proxy.get_payment_information
+                              end
         render json: Mobile::V0::PaymentInformationSerializer.new(@current_user.uuid,
                                                                   payment_information.payment_account,
                                                                   payment_information.control_information)
       end
 
       def update
-        payment_information = proxy.update_payment_information(pay_info)
+        payment_information = if lighthouse?
+                                lighthouse_adapter.parse(lighthouse_service.update_payment_info(pay_info))
+                              else
+                                evss_proxy.update_payment_information(pay_info)
+                              end
+
         render json: Mobile::V0::PaymentInformationSerializer.new(@current_user.uuid,
                                                                   payment_information.payment_account,
                                                                   payment_information.control_information)
@@ -30,11 +44,11 @@ module Mobile
 
       private
 
-      def proxy
-        @proxy ||= Mobile::V0::PaymentInformation::Proxy.new(@current_user)
+      def evss_proxy
+        @evss_proxy ||= Mobile::V0::PaymentInformation::Proxy.new(@current_user)
       end
 
-      def ppiu_params
+      def evss_ppiu_params
         params.permit(
           :account_type,
           :financial_institution_name,
@@ -43,8 +57,31 @@ module Mobile
         )
       end
 
+      def lighthouse_ppiu_params
+        params[:routing_number] = params[:financial_institution_routing_number]
+        params.permit(:account_type,
+                      :account_number,
+                      :routing_number)
+      end
+
       def pay_info
-        @pay_info ||= EVSS::PPIU::PaymentAccount.new(ppiu_params)
+        @pay_info ||= if lighthouse?
+                        Lighthouse::DirectDeposit::PaymentAccount.new(lighthouse_ppiu_params)
+                      else
+                        EVSS::PPIU::PaymentAccount.new(evss_ppiu_params)
+                      end
+      end
+
+      def lighthouse_service
+        @lighthouse_service ||= DirectDeposit::Client.new(@current_user.icn)
+      end
+
+      def lighthouse_adapter
+        Mobile::V0::Adapters::LighthouseDirectDeposit.new
+      end
+
+      def lighthouse?
+        Flipper.enabled?(:mobile_lighthouse_direct_deposit, @current_user)
       end
 
       def validate_pay_info
@@ -52,6 +89,10 @@ module Mobile
           Raven.tags_context(validation: 'direct_deposit')
           raise Common::Exceptions::ValidationErrors, pay_info
         end
+      end
+
+      def send_lighthouse_confirmation_email
+        VANotifyDdEmailJob.send_to_emails(@current_user.all_emails, :comp_and_pen)
       end
     end
   end
