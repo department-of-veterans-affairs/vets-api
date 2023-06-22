5fb03edd9bf9cbc884bb03da83f8d85191144eac
Eric Boehs <eric@boehs.com>
2023-06-22 14:31:58 -0500

    Revert "Revert "Revert "Docker: Use Debian Bookworm (#13049)" (#13058)" (#13064)" (#13073)
    This reverts commit d018869d135d0167a8b34445315a6e9727aeb3d7.

--- config/initializers/tic_mitm.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Due to the requirement of VA's Trusted Internet Connection (TIC), we need to enable legacy server connections.
-# OpenSSL 1.x's implementation didn't prohibit MitM proxying like the TIC from working but to guard against
-# CVE-2009-3555, OpenSSL 3.x's does.
-
-# Enable legacy server connections
-OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:options] |= OpenSSL::SSL::OP_LEGACY_SERVER_CONNECT
