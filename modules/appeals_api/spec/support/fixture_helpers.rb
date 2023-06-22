19e69a4ffa7dad77450965ba265031364bd08a8e
Casey Williams <caseywilliams@users.noreply.github.com>
2023-06-21 17:04:34 -0700

    API-26837 Finish updating use of appeals FixtureHelpers (#13056)

--- modules/appeals_api/spec/support/fixture_helpers.rb
+++ modules/appeals_api/spec/support/fixture_helpers.rb
@@ -5,27 +5,16 @@ require 'forwardable'
 module FixtureHelpers
   extend Forwardable
 
-  def self.fixture_filepath(path_within_fixtures, version: '')
-    if version.present?
-      # FIXME: the `version` argument in these methods exists only to be backwards-compatible with test code from before
-      # changes made in API-26837, which involved too many changed lines to merge at once. The `version` argument will
-      # soon be removed in favor of expecting a full subpath within the fixtures directory instead of assuming that a
-      # JSON file is in the decision_reviews directory and a PDF is in the pdf directory as we do here:
-      dir = path_within_fixtures.ends_with?('pdf') ? 'pdf' : 'decision_reviews'
-      FixtureHelpers.fixture_filepath(
-        "#{dir}/#{version}/#{path_within_fixtures}"
-      )
-    else
-      AppealsApi::Engine.root.join('spec', 'fixtures', path_within_fixtures)
-    end
+  def self.fixture_filepath(path_within_fixtures)
+    AppealsApi::Engine.root.join('spec', 'fixtures', path_within_fixtures)
   end
 
-  def self.fixture_to_s(path_within_fixtures, version: '')
-    File.read fixture_filepath(path_within_fixtures, version:)
+  def self.fixture_to_s(path_within_fixtures)
+    File.read fixture_filepath(path_within_fixtures)
   end
 
-  def self.fixture_as_json(path_within_fixtures, version: '')
-    JSON.parse fixture_to_s(path_within_fixtures, version:)
+  def self.fixture_as_json(path_within_fixtures)
+    JSON.parse fixture_to_s(path_within_fixtures)
   end
 
   def_delegator self, :fixture_filepath
