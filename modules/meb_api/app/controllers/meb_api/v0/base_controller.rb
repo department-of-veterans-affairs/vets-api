cf1391c6f20bbdef3ba63a6fb6f978f667095113
Brad Bergeron <bradley.bergeron@accenturefederal.com>
2023-07-10 10:08:49 -0400

    Sa 69426 add meb pundit policy (#13188)
    * Adds Pundit Policy and Spec for My Education Benefits Context
    
    * Adds new policy for authorizing the MEB API controller actions from base controller
    
    * Runs Rubocop on policy and policy spec
    
    * Runs Rubocop on modified Controller
    
    * Adds Request Specs for base controller policy functionality and VCR fixtures
    
    * Removes VCR blocks from the context blocks where :forbidden is returned as no fixture is recorded
    
    * Fixes spacing in controller

--- modules/meb_api/app/controllers/meb_api/v0/base_controller.rb
+++ modules/meb_api/app/controllers/meb_api/v0/base_controller.rb
@@ -7,13 +7,17 @@ require 'dgi/status/service'
 module MebApi
   module V0
     class BaseController < ::ApplicationController
-      protected
+      before_action :authorize_access
+
+      private
+
+      def authorize_access
+        authorize(current_user, :access?, policy_class: MebPolicy)
+      end
 
       def check_forms_flipper
         routing_error unless Flipper.enabled?(:show_forms_app)
       end
-
-      private
 
       def claim_status_service
         MebApi::DGI::Status::Service.new(@current_user)
