b6492857cb2852baac0e8a0ca1c8788498d972ae
Chikaodi Adibemma <53785439+cadibemma@users.noreply.github.com>
2023-07-24 17:50:06 -0400

    6165 update claims and appeals to use both policies (#13279)
    * added endpoint to routes
    
    * updated routes
    
    * updated routes
    
    * undo changes to routes
    
    * added policy to controller
    
    * added separate get appeals and claims
    
    * added error messages
    
    * added access check for claims and appeals
    
    * specs - wip
    
    * specs fixes - wip
    
    * proxy fixes for appeals and claims
    
    * specs fixes
    
    * fixed controller
    
    * rename
    
    * controller fixes
    
    * temp revert
    
    * merge conflict fix
    
    * rubocop fixes
    
    * specs fixes
    
    * added test for individual appeals
    
    * controller before action fixes
    
    * modified before_action
    
    * modified before_action
    
    * modified before_action
    
    * before_action fixes

--- modules/mobile/spec/request/individual_appeal_request_spec.rb
+++ modules/mobile/spec/request/individual_appeal_request_spec.rb
@@ -10,18 +10,31 @@ RSpec.describe 'individual appeal', type: :request do
   describe 'GET /v0/appeal/:id' do
     before { iam_sign_in }
 
-    it 'and a result that matches our schema is successfully returned with the 200 status ' do
-      VCR.use_cassette('caseflow/appeals') do
-        get '/mobile/v0/appeal/3294289', headers: iam_headers
-        expect(response).to have_http_status(:ok)
+    context 'with an authorized user' do
+      it 'and a result that matches our schema is successfully returned with the 200 status ' do
+        VCR.use_cassette('caseflow/appeals') do
+          get '/mobile/v0/appeal/3294289', headers: iam_headers
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      it 'and attempting to access a nonexistant appeal returns a 404 wtih an error ' do
+        VCR.use_cassette('caseflow/appeals') do
+          get '/mobile/v0/appeal/1234567', headers: iam_headers
+          expect(response).to have_http_status(:not_found)
+          expect(response.body).to match_json_schema('evss_errors')
+        end
       end
     end
 
-    it 'and attempting to access a nonexistant appeal returns a 404 wtih an error ' do
-      VCR.use_cassette('caseflow/appeals') do
-        get '/mobile/v0/appeal/1234567', headers: iam_headers
-        expect(response).to have_http_status(:not_found)
-        expect(response.body).to match_json_schema('evss_errors')
+    context 'with an unauthorized user' do
+      before { iam_sign_in(FactoryBot.build(:iam_user, :loa2)) }
+
+      it 'returns 403 status' do
+        VCR.use_cassette('caseflow/appeals') do
+          get '/mobile/v0/appeal/3294289', headers: iam_headers
+          expect(response).to have_http_status(:forbidden)
+        end
       end
     end
   end
