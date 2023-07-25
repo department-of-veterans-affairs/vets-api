191c143fab9af4552f2b9259f93a3fa8868a3016
Scott James <Scott.James1@va.gov>
2023-07-25 10:48:33 -0400

    [60600] Add flexibility to prefilled FSR payment data (#13291)
    * Set prefill payment data to window of time based on config
    
    * Actually reference time value of setting...
    
    * Add test for payment window
    
    ---------
    
    Co-authored-by: Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>

--- spec/models/form_profile_spec.rb
+++ spec/models/form_profile_spec.rb
@@ -997,6 +997,57 @@ RSpec.describe FormProfile, type: :model do
       it 'returns a prefilled 5655 form' do
         expect_prefilled('5655')
       end
+
+      context 'payment window' do
+        let(:education_payments) do
+          [
+            { payment_date: DateTime.now - 3.months, payment_amount: '1500' }
+          ]
+        end
+
+        before do
+          allow_any_instance_of(DebtManagementCenter::PaymentsService).to(
+            receive(:education).and_return(education_payments)
+          )
+        end
+
+        it 'filters older payments when window is present' do
+          allow(Settings.dmc).to receive(:fsr_payment_window).and_return(30)
+
+          expect_prefilled('5655')
+        end
+
+        context 'no window present' do
+          let(:v5655_expected) do
+            {
+              'personalIdentification' => {
+                'ssn' => user.ssn.last(4),
+                'fileNumber' => '3735'
+              },
+              'personalData' => {
+                'veteranFullName' => full_name,
+                'address' => address,
+                'telephoneNumber' => us_phone,
+                'emailAddress' => user.pciu_email,
+                'dateOfBirth' => user.birth_date
+              },
+              'income' => [
+                {
+                  'veteranOrSpouse' => 'VETERAN',
+                  'compensationAndPension' => '3000',
+                  'education' => '1500'
+                }
+              ]
+            }
+          end
+
+          it 'includes older payments when no window is present' do
+            allow(Settings.dmc).to receive(:fsr_payment_window).and_return(nil)
+
+            expect_prefilled('5655')
+          end
+        end
+      end
     end
 
     context 'when emis is down', skip_emis: true do
