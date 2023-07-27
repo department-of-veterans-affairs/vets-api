356a36220cbeab6cb288b68c7b89418555baa874
Kevin Suarez <kjsuarez94@gmail.com>
2023-07-27 17:03:27 -0400

    add an attribute to fsr forms that changes box 3 for VHS pdf submissions (#13365)

--- spec/factories/form5655_submission.rb
+++ spec/factories/form5655_submission.rb
@@ -10,4 +10,26 @@ FactoryBot.define do
       JSON.parse(File.read(::Rails.root.join(*'/spec/fixtures/dmc/form5655_submission.json'.split('/')).to_s)).to_json
     end
   end
+  factory :sw_form5655_submission, class: 'Form5655Submission' do
+    transient do
+      user { create(:user, :loa3) }
+    end
+    user_uuid { user.uuid }
+    form_json do
+      JSON.parse(
+        File.read(::Rails.root.join(*'/spec/fixtures/dmc/sw_form5655_submission.json'.split('/')).to_s)
+      ).to_json
+    end
+  end
+  factory :non_sw_form5655_submission, class: 'Form5655Submission' do
+    transient do
+      user { create(:user, :loa3) }
+    end
+    user_uuid { user.uuid }
+    form_json do
+      JSON.parse(
+        File.read(::Rails.root.join(*'/spec/fixtures/dmc/non_sw_form5655_submission.json'.split('/')).to_s)
+      ).to_json
+    end
+  end
 end
