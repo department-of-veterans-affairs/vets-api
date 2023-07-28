268a6d2b22d0e2993a6ae339d18cebbfc4a9df09
Jason C <jasonconig@me.com>
2023-07-28 13:32:58 -0700

    Mobile: Add tracking of vet360 linking attempts (#13346)
    * Add tracking of vet360 linking attempts
    
    * Don't delete mobile users so that we can track if the vet360 id is ever unset
    
    * Reduce repeated successful link messages and remove legacy destroy
    
    * Make application code backwards compatible with pre-migration code
    
    * Remove migration related code
    
    * Make specs also be backwards compatible with migrations
    
    * Correct controller logic
    
    * Fix scoping issue
    
    * Fix scoping issue
    
    * Remove guard code
    
    * Add more specs
    
    * Use attributes API to define defaults instead
    
    * Rubocop

--- modules/mobile/spec/workers/vet360_linking_spec.rb
+++ modules/mobile/spec/workers/vet360_linking_spec.rb
@@ -7,13 +7,32 @@ RSpec.describe Mobile::V0::Vet360LinkingJob, type: :job do
   let(:user) { create(:user, :loa3) }
 
   context 'when linking request is successfully made' do
-    it 'logs the user id and transaction id that linked an account with vet360' do
+    it 'logs the user id, transaction id, and creates a mobile user that linked an account with vet360' do
       VCR.use_cassette('mobile/profile/init_vet360_id_success') do
         expect(Rails.logger).to receive(:info).with(
           'Mobile Vet360 account linking request succeeded for user with uuid',
           { user_uuid: user.uuid, transaction_id: 'd8951c96-5b8c-42ea-9fbe-e656941b7236' }
         )
         subject.perform(user.uuid)
+        expect(Mobile::User.where(icn: user.icn)).to exist
+      end
+    end
+
+    it 'increments mobile user vet360 linked attempts' do
+      Mobile::User.create(icn: user.icn, vet360_link_attempts: 1)
+
+      VCR.use_cassette('mobile/profile/init_vet360_id_success') do
+        subject.perform(user.uuid)
+        expect(Mobile::User.where(icn: user.icn, vet360_link_attempts: 2)).to exist
+      end
+    end
+
+    it 'flips vet360_linked to false if mobile user previously was linked' do
+      Mobile::User.create(icn: user.icn, vet360_link_attempts: 1, vet360_linked: true)
+
+      VCR.use_cassette('mobile/profile/init_vet360_id_success') do
+        subject.perform(user.uuid)
+        expect(Mobile::User.where(icn: user.icn, vet360_link_attempts: 2, vet360_linked: false)).to exist
       end
     end
   end
