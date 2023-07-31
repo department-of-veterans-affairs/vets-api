e94d208b4748bd57a34e1ee607c6d4d908ef1784
Kristen Brown <kristen.brown@adhocteam.us>
2023-07-31 12:35:33 -0400

    API-26763: Remove Unused `VBADocuments` Monthly Report Generation Code (#13378)
    * API-26763: Since the API launched in 2018, we can limit dates to the year 2000 and greater
    
    * API-26763: Remove VBADocuments code unused by new monthly stats generation code
    
    * API-26763: Remove file left over from a reporting feature that never launched

--- modules/vba_documents/spec/models/upload_submission_spec.rb
+++ modules/vba_documents/spec/models/upload_submission_spec.rb
@@ -325,40 +325,6 @@ describe VBADocuments::UploadSubmission, type: :model do
       upload_processing.save
     end
 
-    context 'averages' do
-      before do
-        time = Time.zone.now
-        consumer_1 = VBADocuments::UploadSubmission.new
-        consumer_1.consumer_name = 'consumer_1'
-        @num_times = 5
-        @num_times.times do |index|
-          Timecop.freeze(time)
-          upload = VBADocuments::UploadSubmission.new
-          upload.consumer_name = "consumer_#{index}"
-          Timecop.freeze(time + 1.minute)
-          upload.status = 'uploaded'
-          upload.save
-        end
-        consumer_1.status = 'uploaded'
-        consumer_1.save
-      end
-
-      #  rspec ./modules/vba_documents/spec/models/upload_submission_spec.rb
-      it 'calculates status averages' do
-        avg_times = VBADocuments::UploadSubmission.status_elapsed_times(1.year.ago, 1.minute.from_now).first
-        avg_times_c1 = VBADocuments::UploadSubmission
-                       .status_elapsed_times(1.year.ago, 1.minute.from_now, 'consumer_1').first
-        expect(avg_times['avg_secs'].to_i).to be == 60
-        expect(avg_times['min_secs'].to_i).to be == 60
-        expect(avg_times['max_secs'].to_i).to be == 60
-        expect(avg_times['rowcount'].to_i).to be == @num_times + 1
-        expect(avg_times['status']).to eq('pending')
-        expect(avg_times_c1['avg_secs'].to_i).to be == 60
-        expect(avg_times_c1['rowcount'].to_i).to be == 2
-        expect(avg_times_c1['status']).to eq('pending')
-      end
-    end
-
     it 'records status change times properly' do
       time = Time.zone.now
       Timecop.freeze(time)
