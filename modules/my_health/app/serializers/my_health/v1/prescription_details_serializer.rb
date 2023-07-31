5527c9505e320987c41f6929a05687ffb319092e
robertbylight <107279507+robertbylight@users.noreply.github.com>
2023-07-31 17:24:03 -0400

    Mhv 46162 rx update to new mhv endpoint (#13342)
    * [MHV 45008] updated Model and Serializer with additional attributes
    .
    
    * [MHV 45008] added call to new endpoint, linked controller to new api call, modified parser to handle new api call data.
    
    * [MHV 45008] removed trailing white space.
    
    * [MHV 45008] removed trailing white space.
    
    * [MHV-46162] added additional attributes to the model and serializer, also started the rspec testing
    
    * [MHV-46162] removed id from model and serializer, and accounted for null values in the schema, also created a seperate schema from the one the mobile team uses.
    
    * [MHV-46162] reverted cassette back, and will use seperate cassette for va.gov test.
    
    * [MHV-46162] changed the spec test to point to the new schema and cassette.
    
    * [MHV-46162] added empty line at end of code.
    
    * [MHV-46162] added code for single rx request and updated spec test to seperate va.gov tests from mobile test.
    
    * [MHV-46162] added camel schema and updated spec test for single rx and all rx test cases with camnel inflection.
    
    * [MHV-46162] added camel schema and updated spec test for single rx and all rx test cases with camnel inflection.
    
    * [MHV-46162] added an extended prescription serializer and model to avoid coflict with mobile app code.
    
    * [MHV-46162] updated rspec test with new schema and cassetes that match new fields added with new model and serializer.
    
    * [MHV-46162] updated rspec test with new schema and cassetes that match new fields added with new model and serializer.
    
    * [MHV-46162] fixed linting issues.
    
    * [MHV-46162] removed redundant code since its inherited.

---
+++ modules/my_health/app/serializers/my_health/v1/prescription_details_serializer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module MyHealth
+  module V1
+    class PrescriptionDetailsSerializer < PrescriptionSerializer
+      attribute :cmop_ndc_number
+      attribute :in_cerner_transition
+      attribute :not_refillable_display_message
+      attribute :sig
+      attribute :cmop_division_phone
+      attribute :user_id
+      attribute :provider_first_name
+      attribute :provider_last_name
+      attribute :remarks
+      attribute :division_name
+      attribute :modified_date
+      attribute :institution_id
+      attribute :dial_cmop_division_phone
+      attribute :disp_status
+      attribute :ndc
+      attribute :reason
+      attribute :prescription_number_index
+      attribute :prescription_source
+      attribute :disclaimer
+      attribute :indication_for_use
+      attribute :indication_for_use_flag
+      attribute :category
+      attribute :tracking_list
+      attribute :rx_rf_records
+      attribute :tracking
+    end
+  end
+end
