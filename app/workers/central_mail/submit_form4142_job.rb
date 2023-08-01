5eb6639af0824f06a9c44eaa0ad27d7865bf8813
Sam Stuckey <sam.n.stuckey@gmail.com>
2023-08-01 09:16:57 -0400

    [DBeX-60979] wrap 4142 3rd party action in logging (#13388)
    Additionaly the third party logging service has been updated to allow for instance level context logging.

--- app/workers/central_mail/submit_form4142_job.rb
+++ app/workers/central_mail/submit_form4142_job.rb
@@ -4,9 +4,23 @@ require 'central_mail/service'
 require 'common/exceptions'
 require 'evss/disability_compensation_form/metrics'
 require 'evss/disability_compensation_form/form4142_processor'
+require 'logging/third_party_transaction'
 
 module CentralMail
   class SubmitForm4142Job < EVSS::DisabilityCompensationForm::Job
+    extend Logging::ThirdPartyTransaction::MethodWrapper
+
+    # this is required to make instance variables available to logs via
+    # the wrap_with_logging method
+    attr_accessor :submission_id
+
+    wrap_with_logging(
+      :upload_to_central_mail,
+      additional_instance_logs: {
+        submission_id: [:submission_id]
+      }
+    )
+
     STATSD_KEY_PREFIX = 'worker.evss.submit_form4142'
 
     # Sidekiq has built in exponential back-off functionality for retrys
@@ -17,8 +31,7 @@ module CentralMail
 
     sidekiq_options retry: RETRY
 
-    class CentralMailResponseError < Common::Exceptions::BackendServiceException
-    end
+    class CentralMailResponseError < Common::Exceptions::BackendServiceException; end
 
     # This callback cannot be tested due to the limitations of `Sidekiq::Testing.fake!`
     sidekiq_retries_exhausted do |msg, _ex|
@@ -34,12 +47,14 @@ module CentralMail
     # @param submission_id [Integer] the {Form526Submission} id
     #
     def perform(submission_id)
+      @submission_id = submission_id
+
       Raven.tags_context(source: '526EZ-all-claims')
       super(submission_id)
+
       with_tracking('Form4142 Submission', submission.saved_claim_id, submission.id) do
-        processor = EVSS::DisabilityCompensationForm::Form4142Processor.new(submission, jid)
         @pdf_path = processor.pdf_path
-        response = CentralMail::Service.new.upload(processor.request_body)
+        response = upload_to_central_mail
         handle_service_exception(response) if response.present? && response.status.between?(201, 600)
       end
     rescue => e
@@ -53,6 +68,14 @@ module CentralMail
     end
 
     private
+
+    def processor
+      @processor ||= EVSS::DisabilityCompensationForm::Form4142Processor.new(submission, jid)
+    end
+
+    def upload_to_central_mail
+      CentralMail::Service.new.upload(processor.request_body)
+    end
 
     # Cannot move job straight to dead queue dynamically within an executing job
     # raising error for all the exceptions as sidekiq will then move into dead queue
