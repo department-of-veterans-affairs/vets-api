9d33668e756fedc032edb155365d08ca8fb6d068
Gaurav Gupta <randomsync@users.noreply.github.com>
2023-08-04 16:39:16 -0700

    61126 chip get token (#13395)
    * 61126 initial service and configuration refactor (wip)
    
    * 61126 add token methods to service and config (wip)
    
    * 61126 add/update specs for configuration
    
    * 61126 update specs for service
    
    * 61126 add token method to service class
    
    * 61126 add specs for token method in service class

--- lib/chip/service.rb
+++ lib/chip/service.rb
@@ -1,29 +1,94 @@
 # frozen_string_literal: true
 
-require_relative 'client'
+require 'common/client/base'
+require 'common/client/concerns/monitoring'
+require_relative 'configuration'
+require_relative 'redis_client'
 
 module Chip
-  class Service
-    attr_reader :tenant_name, :tenant_id, :username, :password, :client
+  class Service < Common::Client::Base
+    include Common::Client::Concerns::Monitoring
 
-    def initialize(opts)
+    configuration Chip::Configuration
+    STATSD_KEY_PREFIX = 'api.chip'
+
+    attr_reader :tenant_name, :tenant_id, :username, :password, :redis_client
+
+    ##
+    # Builds a Service instance
+    #
+    # @param opts [Hash] options to create the object
+    # @option opts [String] :tenant_name
+    # @option opts [String] :tenant_id
+    # @option opts [String] :username
+    # @option opts [String] :password
+    #
+    # @return [Service] an instance of this class
+    #
+    def self.build(opts = {})
+      new(opts)
+    end
+
+    def initialize(opts = {})
       @tenant_name = opts[:tenant_name]
       @tenant_id = opts[:tenant_id]
       @username = opts[:username]
       @password = opts[:password]
-      @client = Chip::Client.new(username:, password:)
       validate_arguments!
+
+      @redis_client = RedisClient.build(tenant_id)
+      super()
+    end
+
+    ##
+    # Get the auth token from CHIP
+    #
+    # @return [Faraday::Response] response from CHIP token endpoint
+    #
+    def get_token
+      with_monitoring do
+        perform(:post, "/#{config.base_path}/token", {}, token_headers)
+      end
     end
 
     private
+
+    def request_headers
+      default_headers.merge('Authorization' => "Bearer #{token}")
+    end
+
+    def token_headers
+      claims_token = Base64.encode64("#{username}:#{password}")
+      default_headers.merge('Authorization' => "Basic #{claims_token}")
+    end
+
+    def default_headers
+      {
+        'Content-Type' => 'application/json',
+        'x-apigw-api-id' => config.api_gtwy_id
+      }
+    end
+
+    def token
+      @token ||= begin
+        token = redis_client.get
+        if token.present?
+          token
+        else
+          resp = get_token
+
+          Oj.load(resp.body)&.fetch('token').tap do |jwt_token|
+            redis_client.save(token: jwt_token)
+          end
+        end
+      end
+    end
 
     def validate_arguments!
       raise ArgumentError, 'Invalid username' if username.blank?
       raise ArgumentError, 'Invalid password' if password.blank?
       raise ArgumentError, 'Invalid tenant parameters' if tenant_name.blank? || tenant_id.blank?
-      raise ArgumentError, 'Tenant parameters do not exist' unless Chip::Client.configuration.valid_tenant?(
-        tenant_name:, tenant_id:
-      )
+      raise ArgumentError, 'Tenant parameters do not exist' unless config.valid_tenant?(tenant_name:, tenant_id:)
     end
   end
 end
