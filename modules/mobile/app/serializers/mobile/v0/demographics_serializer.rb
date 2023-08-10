4a6cc52920f815c47032537993a0e9822026f0e2
Andrew Herzberg <andrew.herzberg@adhocteam.us>
2023-08-10 10:34:07 -0700

    mobile: add demographics endpoint (#13447)

---
+++ modules/mobile/app/serializers/mobile/v0/demographics_serializer.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Mobile
+  module V0
+    class DemographicsSerializer
+      include JSONAPI::Serializer
+
+      set_type :demographics
+      attributes :gender_identity, :preferred_name
+      def initialize(user_id, demographics)
+        resource = DemographicsStruct.new(id: user_id,
+                                          gender_identity: demographics.demographics&.gender_identity&.code,
+                                          preferred_name: demographics.demographics&.preferred_name&.text)
+
+        super(resource)
+      end
+    end
+    DemographicsStruct = Struct.new(:id, :gender_identity, :preferred_name)
+  end
+end
