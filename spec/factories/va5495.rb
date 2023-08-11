b913480174c2a4881f5b9bd0df28946e92b08f8a
Nathan Wright <nathanbwright@users.noreply.github.com>
2023-08-11 08:49:27 -0600

    VaNotify - Add email confirmation for 5495 (#13265)
    * Initial work for 5495 trigger
    
    * Update confirmation email details

--- spec/factories/va5495.rb
+++ spec/factories/va5495.rb
@@ -13,4 +13,18 @@ FactoryBot.define do
       }.to_json
     }
   end
+
+  factory :va5495_with_email, class: 'SavedClaim::EducationBenefits::VA5495', parent: :education_benefits do
+    form {
+      {
+        email: 'email@example.com',
+        relativeFullName: {
+          first: 'Mark',
+          last: 'Olson'
+        },
+        benefit: 'chapter35',
+        privacyAgreementAccepted: true
+      }.to_json
+    }
+  end
 end
--- spec/models/saved_claim/education_benefits/va5495_spec.rb
+++ spec/models/saved_claim/education_benefits/va5495_spec.rb
@@ -9,4 +9,28 @@ RSpec.describe SavedClaim::EducationBenefits::VA5495 do
   it_behaves_like 'saved_claim'
 
   validate_inclusion(:form_id, '22-5495')
+
+  describe '#after_submit' do
+    let(:user) { create(:user) }
+
+    it 'sends confirmation email for the 5495' do
+      allow(VANotify::EmailJob).to receive(:perform_async)
+
+      subject = create(:va5495_with_email)
+      confirmation_number = subject.education_benefits_claim.confirmation_number
+
+      subject.after_submit(user)
+
+      expect(VANotify::EmailJob).to have_received(:perform_async).with(
+        'email@example.com',
+        'form5495_confirmation_email_template_id',
+        {
+          'first_name' => 'MARK',
+          'date_submitted' => Time.zone.today.strftime('%B %d, %Y'),
+          'confirmation_number' => confirmation_number,
+          'regional_office_address' => "P.O. Box 4616\nBuffalo, NY 14240-4616"
+        }
+      )
+    end
+  end
 end
