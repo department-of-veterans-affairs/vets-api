5aace2506b4604c77e24196b09532769b6a51578
Ryan McNeil <ryan.mcneil@oddball.io>
2023-08-17 13:05:35 -0600

    Revert "Setup flipper using warden-github pattern (#13302)" (#13558)
    This reverts commit 1b4d0acdebc43d01de1976dad15e704ec925d01e.

--- app/controllers/flipper_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class FlipperController < ApplicationController
-  skip_before_action :authenticate
-  def logout
-    cookies.delete :api_session
-    redirect_to '/flipper/features'
-  end
-end
