c72f95608afd689d89d2cb0b28829f92badbe21c
Kevin Suarez <kjsuarez94@gmail.com>
2023-08-23 09:16:37 -0400

    no flag user submissions should send a nil streamlined value (#13604)

--- lib/debt_management_center/financial_status_report_service.rb
+++ lib/debt_management_center/financial_status_report_service.rb
@@ -176,6 +176,9 @@ module DebtManagementCenter
         form.reject! { |k, _v| k == 'streamlined' }
         form['streamlined'] = streamline_data['value']
       end
+
+      form['streamlined'] = nil unless Flipper.enabled?(:financial_status_report_streamlined_waiver, @user)
+
       form
     end
 
--- modules/debts_api/lib/debts_api/v0/financial_status_report_service.rb
+++ modules/debts_api/lib/debts_api/v0/financial_status_report_service.rb
@@ -170,6 +170,9 @@ module DebtsApi
         form.reject! { |k, _v| k == 'streamlined' }
         form['streamlined'] = streamline_data['value']
       end
+
+      form['streamlined'] = nil unless Flipper.enabled?(:financial_status_report_streamlined_waiver, @user)
+
       form
     end
 
