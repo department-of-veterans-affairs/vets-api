278a408560650a75cd55cc8f6f01e66275ec4dcc
Aurora <aurora.hampton@agile6.com>
2023-08-23 13:04:19 -0700

    DBEX/62666: Enable Rated Disability in production for vets website requests: Pre-launch tasks (#13567)
    * Split rated disabilities lighthouse migration feature toggles
    
    Add logging of provider name (first pass)
    
    * dbex/62666: disability compensation ApiProvider re-factory
    
    * dbex/62666: use feature toggle from class member
    
    * dbex/62666: BACKGROUND -> FOREGROUND
    
    * dbex/62666: Add flipper_id to api_provider Flipper toggle check
    
    * dbex/62666: service -> api provider
    
    * dbex/62666: run rubocop fixes

--- app/models/form_profiles/va_526ez.rb
+++ app/models/form_profiles/va_526ez.rb
@@ -92,13 +92,18 @@ class FormProfiles::VA526ez < FormProfile
   def initialize_rated_disabilities_information
     return {} unless user.authorize :evss, :access?
 
-    service = ApiProviderFactory.rated_disabilities_service_provider(
-      {
-        auth_headers: EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h),
-        icn: user.icn.to_s
-      }
+    api_provider = ApiProviderFactory.call(
+      type: ApiProviderFactory::FACTORIES[:rated_disabilities],
+      provider: nil,
+      options: {
+        icn: user.icn.to_s,
+        auth_headers: EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h)
+      },
+      current_user: user,
+      feature_toggle: ApiProviderFactory::FEATURE_TOGGLE_RATED_DISABILITIES_FOREGROUND
     )
-    response = service.get_rated_disabilities
+
+    response = api_provider.get_rated_disabilities
     ClaimFastTracking::MaxRatingAnnotator.annotate_disabilities(response)
 
     # Remap response object to schema fields
--- lib/disability_compensation/factories/api_provider_factory.rb
+++ lib/disability_compensation/factories/api_provider_factory.rb
@@ -9,66 +9,104 @@ require 'disability_compensation/providers/intent_to_file/intent_to_file_provide
 require 'disability_compensation/providers/ppiu_direct_deposit/ppiu_provider'
 require 'disability_compensation/providers/ppiu_direct_deposit/evss_ppiu_provider'
 require 'disability_compensation/providers/ppiu_direct_deposit/lighthouse_ppiu_provider'
+require 'logging/third_party_transaction'
 
 class ApiProviderFactory
+  extend Logging::ThirdPartyTransaction::MethodWrapper
+  class UndefinedFactoryTypeError < StandardError; end
+
   API_PROVIDER = {
     evss: :evss,
     lighthouse: :lighthouse
   }.freeze
 
-  FEATURE_TOGGLE_RATED_DISABILITIES = 'disability_compensation_lighthouse_rated_disabilities_provider'
+  FACTORIES = {
+    rated_disabilities: :rated_disabilities,
+    intent_to_file: :intent_to_file,
+    ppiu: :ppiu
+  }.freeze
+
+  # Splitting the rated disabilities functionality into two use cases:
+  # 1. foreground tasks (i.e. web requests)
+  # 2. the background jobs (i.e. submit526 job)
+  FEATURE_TOGGLE_RATED_DISABILITIES_FOREGROUND =
+    'disability_compensation_lighthouse_rated_disabilities_provider_foreground'
+  FEATURE_TOGGLE_RATED_DISABILITIES_BACKGROUND =
+    'disability_compensation_lighthouse_rated_disabilities_provider_background'
   FEATURE_TOGGLE_INTENT_TO_FILE = 'disability_compensation_lighthouse_intent_to_file_provider'
 
   # PPIU calls out to Direct Deposit APIs in Lighthouse
   FEATURE_TOGGLE_PPIU_DIRECT_DEPOSIT = 'disability_compensation_lighthouse_ppiu_direct_deposit_provider'
 
-  # @param [hash] options: options to provide auth_headers
-  # @option options [hash] :auth_headers auth headers for the EVSS request
-  # @option options [string] :icn Veteran's ICN
-  def self.rated_disabilities_service_provider(options, api_provider = nil)
-    api_provider ||= if Flipper.enabled?(FEATURE_TOGGLE_RATED_DISABILITIES)
-                       API_PROVIDER[:lighthouse]
-                     else
-                       API_PROVIDER[:evss]
-                     end
+  wrap_with_logging(
+    :rated_disabilities_service_provider,
+    :intent_to_file_service_provider,
+    :ppiu_service_provider,
+    additional_class_logs: {
+      action: 'disability compensation factory choosing API Provider'
+    },
+    additional_instance_logs: {
+      provider: %i[api_provider]
+    }
+  )
 
+  def self.call(**args)
+    new(**args).call
+  end
+
+  def initialize(type:, current_user:, provider: nil, options: {}, feature_toggle: nil)
+    @type = type
+    @api_provider = provider
+    @options = options
+    # current user is necessary for the Flipper toggle to check against
+    @current_user = current_user
+    # for now, rated disabilities is the only special case that needs the feature toggle name sent in
+    @feature_toggle = feature_toggle
+  end
+
+  def call
+    case @type
+    when FACTORIES[:rated_disabilities]
+      rated_disabilities_service_provider
+    when FACTORIES[:intent_to_file]
+      intent_to_file_service_provider
+    when FACTORIES[:ppiu]
+      ppiu_service_provider
+    else
+      raise UndefinedFactoryTypeError
+    end
+  end
+
+  private
+
+  def rated_disabilities_service_provider
     case api_provider
     when API_PROVIDER[:evss]
-      EvssRatedDisabilitiesProvider.new(options[:auth_headers])
+      EvssRatedDisabilitiesProvider.new(@options[:auth_headers])
     when API_PROVIDER[:lighthouse]
-      LighthouseRatedDisabilitiesProvider.new(options[:icn])
+      LighthouseRatedDisabilitiesProvider.new(@options[:icn])
     else
       raise NotImplementedError, 'No known Rated Disabilities Api Provider type provided'
     end
   end
 
-  def self.intent_to_file_service_provider(current_user, api_provider = nil)
-    api_provider ||= if Flipper.enabled?(FEATURE_TOGGLE_INTENT_TO_FILE, current_user)
-                       API_PROVIDER[:lighthouse]
-                     else
-                       API_PROVIDER[:evss]
-                     end
-
+  def intent_to_file_service_provider
+    @feature_toggle = FEATURE_TOGGLE_INTENT_TO_FILE
     case api_provider
     when API_PROVIDER[:evss]
-      EvssIntentToFileProvider.new(current_user, nil)
+      EvssIntentToFileProvider.new(@current_user, nil)
     when API_PROVIDER[:lighthouse]
-      LighthouseIntentToFileProvider.new(current_user)
+      LighthouseIntentToFileProvider.new(@current_user)
     else
       raise NotImplementedError, 'No known Intent to File Api Provider type provided'
     end
   end
 
-  def self.ppiu_service_provider(current_user, api_provider = nil)
-    api_provider ||= if Flipper.enabled?(FEATURE_TOGGLE_PPIU_DIRECT_DEPOSIT)
-                       API_PROVIDER[:lighthouse]
-                     else
-                       API_PROVIDER[:evss]
-                     end
-
+  def ppiu_service_provider
+    @feature_toggle = FEATURE_TOGGLE_PPIU_DIRECT_DEPOSIT
     case api_provider
     when API_PROVIDER[:evss]
-      EvssPPIUProvider.new(current_user)
+      EvssPPIUProvider.new(@current_user)
     when API_PROVIDER[:lighthouse]
       # TODO: Implement in #59698 - Lighthouse provider
       # LighthousePPIUProvider.new(current_user)
@@ -76,5 +114,13 @@ class ApiProviderFactory
     else
       raise NotImplementedError, 'No known PPIU Api Provider type provided'
     end
+  end
+
+  def api_provider
+    @api_provider ||= if Flipper.enabled?(@feature_toggle, @current_user)
+                        API_PROVIDER[:lighthouse]
+                      else
+                        API_PROVIDER[:evss]
+                      end
   end
 end
