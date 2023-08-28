f727cae49eb825134c6fc69a57c8dcd64c8f0834
Nathan Wright <nathanbwright@users.noreply.github.com>
2023-08-28 09:53:33 -0600

    VANotify - Add confirmation email to 21-4142 (#13560)

--- modules/simple_forms_api/app/controllers/simple_forms_api/v1/uploads_controller.rb
+++ modules/simple_forms_api/app/controllers/simple_forms_api/v1/uploads_controller.rb
@@ -22,12 +22,20 @@ module SimpleFormsApi
         Datadog::Tracing.active_trace&.set_tag('form_id', params[:form_number])
 
         form_id = FORM_NUMBER_MAP[params[:form_number]]
-        filler = SimpleFormsApi::PdfFiller.new(form_number: form_id, data: JSON.parse(params.to_json))
+        parsed_form_data = JSON.parse(params.to_json)
+        filler = SimpleFormsApi::PdfFiller.new(form_number: form_id, data: parsed_form_data)
 
         file_path = filler.generate
         metadata = filler.metadata
 
         status, confirmation_number = upload_pdf_to_benefits_intake(file_path, metadata)
+
+        # this will need to be refactored as we add more supported forms
+        if status == 200 && Flipper.enabled?(:form21_4142_confirmation_email)
+          SimpleFormsApi::ConfirmationEmail.new(
+            form_data: parsed_form_data, form_number: form_id, confirmation_number:
+          ).send
+        end
 
         Rails.logger.info(
           "Simple forms api - sent to benefits intake: #{params[:form_number]},
--- modules/simple_forms_api/spec/requests/v1/uploads_spec.rb
+++ modules/simple_forms_api/spec/requests/v1/uploads_spec.rb
@@ -80,5 +80,50 @@ RSpec.describe 'Dynamic forms uploader', type: :request do
     end
 
     test_failed_request_scrubs_error_message2110210
+
+    describe 'email confirmations' do
+      let(:confirmation_number) { 'some_confirmation_number' }
+
+      describe '21_4142' do
+        let(:data) do
+          fixture_path = Rails.root.join(
+            'modules', 'simple_forms_api', 'spec', 'fixtures', 'form_json', 'vba_21_4142.json'
+          )
+          JSON.parse(fixture_path.read)
+        end
+
+        it 'successful submission' do
+          allow(VANotify::EmailJob).to receive(:perform_async)
+          allow_any_instance_of(SimpleFormsApi::V1::UploadsController)
+            .to receive(:upload_pdf_to_benefits_intake).and_return([200, confirmation_number])
+
+          post '/simple_forms_api/v1/simple_forms', params: data
+
+          expect(response).to have_http_status(:ok)
+
+          expect(VANotify::EmailJob).to have_received(:perform_async).with(
+            'veteran.surname@address.com',
+            'form21_4142_confirmation_email_template_id',
+            {
+              'first_name' => 'VETERAN',
+              'date_submitted' => Time.zone.today.strftime('%B %d, %Y'),
+              'confirmation_number' => confirmation_number
+            }
+          )
+        end
+
+        it 'unsuccessful submission' do
+          allow(VANotify::EmailJob).to receive(:perform_async)
+          allow_any_instance_of(SimpleFormsApi::V1::UploadsController)
+            .to receive(:upload_pdf_to_benefits_intake).and_return([500, confirmation_number])
+
+          post '/simple_forms_api/v1/simple_forms', params: data
+
+          expect(response).to have_http_status(:error)
+
+          expect(VANotify::EmailJob).not_to have_received(:perform_async)
+        end
+      end
+    end
   end
 end
