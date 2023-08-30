ed7867a21666d963b1d50c51f39c9fa49961f157
Trevor Bosaw <bosawt@gmail.com>
2023-08-30 13:26:13 -0700

    [64188] Creating MAP security token service to generate tokens for participating applications (#13685)
    Co-authored-by: Trevor Bosaw <trevor.bosaw@oddball.io>

---
+++ lib/mobile_application_platform/security_token/errors.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module MobileApplicationPlatform
+  module SecurityToken
+    module Errors
+      class ApplicationMismatchError < StandardError; end
+    end
+  end
+end
