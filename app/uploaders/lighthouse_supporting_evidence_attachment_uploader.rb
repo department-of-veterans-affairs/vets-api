ec34d695165edfa0cb7daf15d36df7ffe7814df8
Nathan Burgess <burgess.nathan28@gmail.com>
2023-09-01 11:18:08 -0400

    Duplicate EVSS Supporting Evidence Attachment infrastructure for Lighthouse
    When moving our document submission to Lighthouse, we can't use the SupportingEvidenceAttachment class currently referenced in the app/workers/evss/disability_compensation_form/submit_uploads.rb worker. This is because those attachments are associated with an uploader, SupportingEvidenceAttachmentUploader, which inherits from EVSSClaimDocumentUploaderBase and uses its CarrierWave logic. EVSSClaimDocumentUploaderBase is being replaced by LighthouseDocumentUploaderBase, which has Lighthouse API-specific document upload behavior.
    
    Therefore, if we want to reference a Lighthouse-specific evidence attachment in the upload worker, we need to duplicate both SupportingEvidenceAttachment and SupportingEvidenceAttachmentUploader (the subclass of EVSSClaimDocumentUploaderBase), with LighthouseSupportingEvidenceAttachment and its associated  LighthouseSupportingEvidenceAttachmentUploader, which inherits from LighthouseDocumentUploaderBase.
    
    Ports over the basic tests for SupportingEvidenceAttachmentUploader; SupportingEvidenceAttachment is untested directly so there are no unit tests for LighthouseSupportingEvidenceAttachment at the moment

---
+++ app/uploaders/lighthouse_supporting_evidence_attachment_uploader.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+# Files uploaded as part of a form526 submission that will be sent to Lighthouse upon form submission.
+class LighthouseSupportingEvidenceAttachmentUploader < LighthouseDocumentUploaderBase
+  before :store, :log_transaction_start
+  after :store, :log_transaction_complete
+
+  def initialize(guid, _unused = nil)
+    # carrierwave allows only 2 arguments, which they will pass onto
+    # different versions by calling the initialize function again
+    # so the _unused argument is necessary
+
+    super
+    @guid = guid
+
+    #  defaults to CarrierWave::Storage::File if not AWS
+    if Rails.env.production?
+      set_aws_config(
+        Settings.evss.s3.aws_access_key_id,
+        Settings.evss.s3.aws_secret_access_key,
+        Settings.evss.s3.region,
+        Settings.evss.s3.bucket
+      )
+    end
+  end
+
+  def store_dir
+    raise 'missing guid' if @guid.blank?
+
+    "disability_compensation_supporting_form/#{@guid}"
+  end
+
+  def log_transaction_start(uploaded_file = nil)
+    log = {
+      process_id: Process.pid,
+      filesize: uploaded_file.try(:size),
+      file_headers: uploaded_file.try(:headers),
+      upload_start: Time.current
+    }
+
+    Rails.logger.info(log)
+  end
+
+  def log_transaction_complete(uploaded_file = nil)
+    log = {
+      process_id: Process.pid,
+      filesize: uploaded_file.try(:size),
+      file_headers: uploaded_file.try(:headers),
+      upload_complete: Time.current
+    }
+
+    Rails.logger.info(log)
+  end
+end
