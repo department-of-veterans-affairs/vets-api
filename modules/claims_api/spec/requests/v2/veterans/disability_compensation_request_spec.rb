587c1c61951d8d99376dccb3eb8f82102be6d235
mchristiansonVA <95487885+mchristiansonVA@users.noreply.github.com>
2023-09-11 09:56:24 -0400

    Api 28269 service branch validation (#13694)
    * Working implementation
    
    * Update json files, add recorded cassette
    
    * Revert change to fixture, spec file
    
    * Rubocop fixes, add back test
    
    * Add nil checks for branch names, remove extraneous line from cassette
    
    * Add generated swagger, fix description for branch of service
    
    * Remove extraneous comment
    
    * Update for case-insensitive compare
    
    * Remove redundant checks for BRD services
    
    * Fix broken tests, add new for service branches

--- modules/claims_api/spec/requests/v2/veterans/disability_compensation_request_spec.rb
+++ modules/claims_api/spec/requests/v2/veterans/disability_compensation_request_spec.rb
@@ -1609,9 +1609,39 @@ RSpec.describe 'Disability Claims', type: :request do
               json = JSON.parse(data)
               json['data']['attributes']['serviceInformation']['servicePeriods'][0]['serviceBranch'] =
                 service_branch
-              data = json
+              data = json.to_json
               post submit_path, params: data, headers: auth_header
               expect(response).to have_http_status(:unprocessable_entity)
+            end
+          end
+        end
+
+        context 'when the serviceBranch is not in the BRD list' do
+          let(:service_branch) { 'Rogue Force' }
+
+          it 'responds with a 422' do
+            mock_ccg(scopes) do |auth_header|
+              json = JSON.parse(data)
+              json['data']['attributes']['serviceInformation']['servicePeriods'][0]['serviceBranch'] =
+                service_branch
+              data = json.to_json
+              post submit_path, params: data, headers: auth_header
+              expect(response).to have_http_status(:unprocessable_entity)
+            end
+          end
+        end
+
+        context 'when the serviceBranch is in the BRD list but does not match case' do
+          let(:service_branch) { 'PUBLIC Health SERVICE' }
+
+          it 'responds with a 200' do
+            mock_ccg(scopes) do |auth_header|
+              json = JSON.parse(data)
+              json['data']['attributes']['serviceInformation']['servicePeriods'][0]['serviceBranch'] =
+                service_branch
+              data = json.to_json
+              post submit_path, params: data, headers: auth_header
+              expect(response).to have_http_status(:ok)
             end
           end
         end
