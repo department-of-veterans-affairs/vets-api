4832fd8fd103393165444df4f96e14fb871d937e
Kris Pethtel <kris.pethtel@gmail.com>
2023-09-13 12:08:44 -0500

    6698: contact info fixes (#13794)

--- modules/mobile/app/serializers/mobile/v0/contact_info_serializer.rb
+++ modules/mobile/app/serializers/mobile/v0/contact_info_serializer.rb
@@ -5,21 +5,59 @@ module Mobile
     class ContactInfoSerializer
       include JSONAPI::Serializer
 
+      ADDRESS_KEYS = %i[
+        id
+        address_line1
+        address_line2
+        address_line3
+        address_pou
+        address_type
+        city
+        country_name
+        country_code_iso3
+        international_postal_code
+        province
+        state_code
+        zip_code
+        zip_code_suffix
+      ].freeze
+
+      EMAIL_KEYS = %i[
+        id
+        email_address
+      ].freeze
+
+      PHONE_KEYS = %i[
+        id
+        area_code
+        country_code
+        extension
+        phone_number
+        phone_type
+      ].freeze
+
       set_type :contact_info
-      attributes :residential_address, :mailing_address, :home_phone, :mobile_phone, :work_phone
+      attributes :residential_address, :mailing_address, :home_phone, :mobile_phone, :work_phone, :contact_email
 
       def initialize(user_id, contact_info)
         resource = ContactInfoStruct.new(id: user_id,
-                                         residential_address: contact_info&.residential_address,
-                                         mailing_address: contact_info&.mailing_address,
-                                         home_phone: contact_info&.home_phone,
-                                         mobile_phone: contact_info&.mobile_phone,
-                                         work_phone: contact_info&.work_phone)
+                                         residential_address: filter_keys(contact_info&.residential_address,
+                                                                          ADDRESS_KEYS),
+                                         mailing_address: filter_keys(contact_info&.mailing_address, ADDRESS_KEYS),
+                                         home_phone: filter_keys(contact_info&.home_phone, PHONE_KEYS),
+                                         mobile_phone: filter_keys(contact_info&.mobile_phone, PHONE_KEYS),
+                                         work_phone: filter_keys(contact_info&.work_phone, PHONE_KEYS),
+                                         contact_email: filter_keys(contact_info&.email, EMAIL_KEYS))
 
         super(resource)
       end
+
+      def filter_keys(value, keys)
+        value&.to_h&.slice(*keys)
+      end
     end
 
-    ContactInfoStruct = Struct.new(:id, :residential_address, :mailing_address, :home_phone, :mobile_phone, :work_phone)
+    ContactInfoStruct = Struct.new(:id, :residential_address, :mailing_address, :home_phone, :mobile_phone,
+                                   :work_phone, :contact_email)
   end
 end
