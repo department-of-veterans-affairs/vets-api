14ac59d74c4c9bf4d7fe571d6adf3235088ce078
Ryan Johnson <ryan.johnson@oddball.io>
2023-09-22 14:57:09 -0400

    Add veteran_status feature toggle

--- app/models/user.rb
+++ app/models/user.rb
@@ -354,7 +354,18 @@ class User < Common::RedisStore
   end
 
   def veteran_status
-    @veteran_status ||= VAProfile::VeteranStatus::Service.new(self)
+    if Flipper.enabled?(:veteran_status_updated)
+      @veteran_status ||= VAProfile::VeteranStatus::Service.new(self)
+    else
+      define_method(emis_method) do
+        emis_model = instance_variable_get(:"@#{emis_method}")
+        return emis_model if emis_model.present?
+  
+        emis_model = "EMISRedis::#{emis_method.camelize}".constantize.for_user(self)
+        instance_variable_set(:"@#{emis_method}", emis_model)
+        emis_model
+      end
+    end
   end
 
   %w[profile grants].each do |okta_model_name|
