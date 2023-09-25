ac1c6fc4ae38a8e510eb9da3b8edb66c06d99e7d
Khoa Nguyen <134089461+Khoa-V-Nguyen@users.noreply.github.com>
2023-09-25 15:22:08 -0600

    BE | Create Inquiries::Retriever class (#13893)
    * Create Inquiries::Retriever and Inquiries::Entity
    
    Class get all of user's inquiries or a single inquiry.
    
    Issue #283
    
    * Create Dynamics::Service
    Reworked DynamicsService into Dynamics module with a Service class
    and ErrorHandler class. Logging with DatadogLogger class.
    
    Issue #283
    
    ---------
    
    Co-authored-by: khoa-v-nguyen <khoa.nguyen@oddball.io>

--- modules/ask_va_api/app/controllers/ask_va_api/v0/users_controller.rb
+++ modules/ask_va_api/app/controllers/ask_va_api/v0/users_controller.rb
@@ -3,11 +3,29 @@
 module AskVAApi
   module V0
     class UsersController < ApplicationController
-      def show
-        user_inquiries = Users::UserInquiriesCreator.new(uuid: current_user.uuid).call
+      before_action :fetch_user_inquiries, only: [:show]
 
-        render json: Users::UserInquiriesSerializer.new(user_inquiries).serializable_hash, status: :ok
+      def show
+        render json: @user_inquiries.payload, status: @user_inquiries.status
       end
+
+      private
+
+      def fetch_user_inquiries
+        inquiries = Inquiries::Retriever.new(sec_id: current_user.sec_id).fetch_by_sec_id
+        @user_inquiries = Result.new(payload: Inquiries::Serializer.new(inquiries).serializable_hash, status: :ok)
+      rescue ErrorHandler::ServiceError, Dynamics::ErrorHandler::ServiceError => e
+        log_and_render_error('service_error', e, :unprocessable_entity)
+      rescue ArgumentError => e
+        log_and_render_error('argument_error', e, :bad_request)
+      end
+
+      def log_and_render_error(action, exception, status)
+        log_error(action, exception)
+        @user_inquiries = Result.new(payload: { error: exception.message }, status:)
+      end
+
+      Result = Struct.new(:payload, :status, keyword_init: true)
     end
   end
 end
