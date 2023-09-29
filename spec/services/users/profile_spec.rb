7b3bf68d4fd2000ef2722a2848e7154b80354e01
Ryan Johnson <ryan.johnson@oddball.io>
2023-09-29 10:21:24 -0400

    Debugging VCR error on profile spec

--- spec/services/users/profile_spec.rb
+++ spec/services/users/profile_spec.rb
@@ -317,9 +317,9 @@ RSpec.describe Users::Profile do
         end
 
         it 'populates the #errors array with the serialized error', :aggregate_failures do
-          VCR.use_cassette('va_profile/veteran_status/va_profile_veteran_status_200', match_requests_on: [:method, :body], allow_playback_repeats: true) do
+          VCR.use_cassette('va_profile/veteran_status/veteran_status_400_', match_requests_on: [:body], allow_playback_repeats: true) do
+            binding.pry
             error = subject.errors.first
-
             expect(error[:external_service]).to eq 'EMIS'
             expect(error[:start_time]).to be_present
             expect(error[:description]).to include 'NOT_FOUND'
