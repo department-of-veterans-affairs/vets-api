44376196a438275e75bfaa31700e03e63cd56220
Nathan Burgess <burgess.nathan28@gmail.com>
2023-10-03 11:44:19 -0400

    Refactor Form 526 User Upload endpoint for Lighthouse migration
    Refactors the supporting evidence attachement uploads controller and adds the Lighthouse migration feature flag so we can save the correct attachment type when a User uploads a file to supplement their Form 526 submission - LighthouseSupportingEvidenceAttachment if the feature is enabled.
    
    Removes the problematic FormAttachmentCreate concern used in other controllers from this controller because it tightly couples the front end caller to knowing the underlying data model for attachment records, in this case SupportingEvidenceAttachment or LighthouseSupportingEvidenceAttachment. Rather than updating the front end to branch which metadata it sends to the API, we will abandon this bad pattern entirely and have the controller expect the nested supporting_evidence_attachment param the front end currently sends regardless of the feature flag.
    
    Preserved refactor from c84016cb55ca57c56d8fc92ec67216f8d6a874bb that extracts S3 interaction into single method, save_attachment_to_cloud! for use with the 3PI logging framework.
    
    Refactors poorly written file class validation which was simply expecting a class name with the string UploadedFile in it. This isn't deterministic enough so I've opted to validate directly against an array of allowed classes in the VALID_FILE_CLASSES constant.
    
    Duplicates all request specs under LighthouseSupportingEvidenceAttachment branching to ensure file type and size validatiions are preserved.
    
    Removed spec/controllers/v0/upload_supporting_evidences_controller_spec.rb since it is tightly coupled to the FormAttachmentCreate concern and duplicates the logic we are testing in the request spec anyway.
    
    It was also necessary to add a new serializer, LighthouseSupportingEvidenceAttachmentSerializer, to ensure the API returns attachment metadata in the same format

--- app/controllers/v0/upload_supporting_evidences_controller.rb
+++ app/controllers/v0/upload_supporting_evidences_controller.rb
@@ -4,16 +4,15 @@ require 'logging/third_party_transaction'
 
 module V0
   class UploadSupportingEvidencesController < ApplicationController
-    include FormAttachmentCreate
     extend Logging::ThirdPartyTransaction::MethodWrapper
 
-    FORM_ATTACHMENT_MODEL = SupportingEvidenceAttachment
+    VALID_FILE_CLASSES = [ActionDispatch::Http::UploadedFile, Rack::Test::UploadedFile].freeze
 
     wrap_with_logging(
       :save_attachment_to_cloud!,
       additional_class_logs: {
         form: '526ez supporting evidence attachment',
-        action: "upload: #{FORM_ATTACHMENT_MODEL}",
+        action: 'Uploaded user-provided attachment to S3',
         upstream: 'User provided file',
         downstream: "S3 bucket: #{Settings.evss.s3.bucket}"
       },
@@ -21,5 +20,47 @@ module V0
         user_uuid: %i[current_user account_uuid]
       }
     )
+
+    def create
+      validate_file_upload_class!
+      save_attachment_to_cloud!
+      save_attachment_to_db!
+
+      render(json: form_attachment)
+    end
+
+    private
+
+    def validate_file_upload_class!
+      file_class = attachment_params[:file_data].class
+
+      unless VALID_FILE_CLASSES.include?(file_class)
+        raise Common::Exceptions::InvalidFieldValue.new('file_data', file_class)
+      end
+    end
+
+    def save_attachment_to_cloud!
+      form_attachment.set_file_data!(attachment_params[:file_data], attachment_params[:password])
+    end
+
+    def save_attachment_to_db!
+      form_attachment.save!
+    end
+
+    def form_attachment
+      @form_attachment ||= form_attachment_model.new
+    end
+
+    def form_attachment_model
+      if Flipper.enabled?(:disability_compensation_lighthouse_document_service_provider)
+        LighthouseSupportingEvidenceAttachment
+      else
+        SupportingEvidenceAttachment
+      end
+    end
+
+    def attachment_params
+      params.require(:supporting_evidence_attachment).permit(:file_data, :password)
+    end
   end
 end
---
+++ app/serializers/lighthouse_supporting_evidence_attachment_serializer.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class LighthouseSupportingEvidenceAttachmentSerializer < ActiveModel::Serializer
+  attribute :guid
+end
