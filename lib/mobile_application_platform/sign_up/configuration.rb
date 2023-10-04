224689dc9e5e7b7998b252720ede18c792c0ff6b
achievedaniel <achievedaniel@gmail.com>
2023-10-04 11:45:39 -0400

    Changing the SUS variable to the spell out sign_up_service

--- lib/mobile_application_platform/sign_up/configuration.rb
+++ lib/mobile_application_platform/sign_up/configuration.rb
@@ -60,7 +60,7 @@ module MobileApplicationPlatform
           request: request_options
         ) do |conn|
           conn.use :breakers
-          conn.response :betamocks if Settings.mobile_application_platform.sus.mock
+          conn.response :betamocks if Settings.mobile_application_platform.sign_up_service.mock
           conn.use Faraday::Response::RaiseError
           conn.adapter Faraday.default_adapter
         end
