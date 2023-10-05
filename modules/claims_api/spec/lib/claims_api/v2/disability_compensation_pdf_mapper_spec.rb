a9ed98673994c82280bb8115a82074699f256a57
mchristiansonVA <95487885+mchristiansonVA@users.noreply.github.com>
2023-10-05 09:44:56 -0400

    Api 29647 Address Refactoring (#14034)
    * Work in progress
    
    * More work in progress
    
    * Test updates and bug fixes, documentation tweaks
    
    * Rubocop fixes, misc updates
    
    * Documentation tweaks, add generated doc
    
    * Updates to allow null for addressLine2 & addressLine3
    
    * Fix attribute values
    
    * Fix boolean values
    
    * Add generated swagger
    
    * Fix variable names

--- modules/claims_api/spec/lib/claims_api/v2/disability_compensation_pdf_mapper_spec.rb
+++ modules/claims_api/spec/lib/claims_api/v2/disability_compensation_pdf_mapper_spec.rb
@@ -89,9 +89,8 @@ describe ClaimsApi::V2::DisabilityCompensationPdfMapper do
         country = pdf_data[:data][:attributes][:identificationInformation][:mailingAddress][:country]
         zip = pdf_data[:data][:attributes][:identificationInformation][:mailingAddress][:zip]
         state = pdf_data[:data][:attributes][:identificationInformation][:mailingAddress][:state]
-
-        expect(number_and_street).to eq('1234 Couch Street')
-        expect(apartment_or_unit_number).to eq('22')
+        expect(number_and_street).to eq('1234 Couch Street Unit 4 Room 1')
+        expect(apartment_or_unit_number).to eq(nil)
         expect(city).to eq('Portland')
         expect(country).to eq('US')
         expect(zip).to eq('41726-1234')
@@ -144,8 +143,8 @@ describe ClaimsApi::V2::DisabilityCompensationPdfMapper do
         expect(begin_date).to eq({ month: '11', day: '30', year: '2012' })
         expect(end_date).to eq({ month: '10', day: '11', year: '2013' })
         expect(type_of_addr_change).to eq('TEMPORARY')
-        expect(number_and_street).to eq('10 Peach St')
-        expect(apartment_or_unit_number).to eq('22')
+        expect(number_and_street).to eq('10 Peach St Unit 4 Room 1')
+        expect(apartment_or_unit_number).to eq(nil)
         expect(city).to eq('Atlanta')
         expect(country).to eq('US')
         expect(zip).to eq('42220-9897')
