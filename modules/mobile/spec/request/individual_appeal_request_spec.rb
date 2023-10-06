d6195c339ab504a739c30c1b50a9cbce77ae5506
Kris Pethtel <kris.pethtel@gmail.com>
2023-10-06 11:23:50 -0500

    6703: convert specs to SIS (#14035)

--- modules/mobile/spec/request/individual_appeal_request_spec.rb
+++ modules/mobile/spec/request/individual_appeal_request_spec.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require_relative '../support/helpers/iam_session_helper'
+require_relative '../support/helpers/sis_session_helper'
 require_relative '../support/matchers/json_schema_matcher'
 
 RSpec.describe 'individual appeal', type: :request do
   include JsonSchemaMatchers
 
   describe 'GET /v0/appeal/:id' do
-    before { iam_sign_in }
+    let!(:user) { sis_user }
 
     context 'with an authorized user' do
       it 'and a result that matches our schema is successfully returned with the 200 status ' do
         VCR.use_cassette('caseflow/appeals') do
-          get '/mobile/v0/appeal/3294289', headers: iam_headers
+          get '/mobile/v0/appeal/3294289', headers: sis_headers
           expect(response).to have_http_status(:ok)
         end
       end
 
       it 'and attempting to access a nonexistant appeal returns a 404 wtih an error ' do
         VCR.use_cassette('caseflow/appeals') do
-          get '/mobile/v0/appeal/1234567', headers: iam_headers
+          get '/mobile/v0/appeal/1234567', headers: sis_headers
           expect(response).to have_http_status(:not_found)
           expect(response.body).to match_json_schema('evss_errors')
         end
@@ -28,11 +28,11 @@ RSpec.describe 'individual appeal', type: :request do
     end
 
     context 'with an unauthorized user' do
-      before { iam_sign_in(FactoryBot.build(:iam_user, :loa2)) }
+      let!(:user) { sis_user(loa: { current: LOA::TWO, highest: LOA::TWO }) }
 
       it 'returns 403 status' do
         VCR.use_cassette('caseflow/appeals') do
-          get '/mobile/v0/appeal/3294289', headers: iam_headers
+          get '/mobile/v0/appeal/3294289', headers: sis_headers
           expect(response).to have_http_status(:forbidden)
         end
       end
--- modules/mobile/spec/request/individual_claim_request_spec.rb
+++ modules/mobile/spec/request/individual_claim_request_spec.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require_relative '../support/helpers/iam_session_helper'
+require_relative '../support/helpers/sis_session_helper'
 require_relative '../support/matchers/json_schema_matcher'
 
 RSpec.describe 'individual claim', type: :request do
   include JsonSchemaMatchers
 
   describe 'GET /v0/claim/:id' do
+    let!(:user) { sis_user }
+
     before do
       Flipper.disable(:mobile_lighthouse_claims)
-      iam_sign_in
-      FactoryBot.create(:evss_claim, id: 1, evss_id: 600_117_255, user_uuid: '3097e489-ad75-5746-ab1a-e0aabc1b426a')
+      FactoryBot.create(:evss_claim, id: 1, evss_id: 600_117_255, user_uuid: user.uuid)
       FactoryBot.create(:evss_claim, id: 2, evss_id: 111_222_333, user_uuid: '1234567890')
     end
 
@@ -19,7 +20,7 @@ RSpec.describe 'individual claim', type: :request do
       it 'matches our schema is successfully returned with the 200 status ',
          run_at: 'Wed, 13 Dec 2017 03:28:23 GMT' do
         VCR.use_cassette('evss/claims/claim_with_docs') do
-          get '/mobile/v0/claim/600117255', headers: iam_headers
+          get '/mobile/v0/claim/600117255', headers: sis_headers
           expect(response).to have_http_status(:ok)
         end
       end
@@ -29,7 +30,7 @@ RSpec.describe 'individual claim', type: :request do
       it 'returns a 404 with an error ',
          run_at: 'Wed, 13 Dec 2017 03:28:23 GMT' do
         VCR.use_cassette('evss/claims/claim_with_docs') do
-          get '/mobile/v0/claim/2222222', headers: iam_headers
+          get '/mobile/v0/claim/2222222', headers: sis_headers
           expect(response).to have_http_status(:not_found)
           expect(response.body).to match_json_schema('evss_errors')
         end
@@ -40,7 +41,7 @@ RSpec.describe 'individual claim', type: :request do
       it 'returns a 404 with an error ',
          run_at: 'Wed, 13 Dec 2017 03:28:23 GMT' do
         VCR.use_cassette('evss/claims/claim_with_docs') do
-          get '/mobile/v0/claim/111222333', headers: iam_headers
+          get '/mobile/v0/claim/111222333', headers: sis_headers
           expect(response).to have_http_status(:not_found)
           expect(response.body).to match_json_schema('evss_errors')
         end
@@ -50,7 +51,7 @@ RSpec.describe 'individual claim', type: :request do
     context 'when evss returns a failure message' do
       it 'returns a 502 response', run_at: 'Wed, 13 Dec 2017 03:28:23 GMT' do
         VCR.use_cassette('evss/claims/claim_doc_with_errors') do
-          get '/mobile/v0/claim/600117255', headers: iam_headers
+          get '/mobile/v0/claim/600117255', headers: sis_headers
           expect(response).to have_http_status(:bad_gateway)
           expect(response.body).to match_json_schema('errors')
         end
