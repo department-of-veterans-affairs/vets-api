64564ccfa59eb43c06270f9bdb1340c9ed63cc0f
Jacob <jakehova@users.noreply.github.com>
2023-10-10 09:41:53 -0400

    Ava 290 lookups topics (#14036)
    * Updated specs and create more mock json files
    
    Co-authored-by: Jacob <jakehova@users.noreply.github.com>
    
    * Created 'categories/:category_id/ topics' endpoint
    
    Created Retriever
    Created Entity
    Created Serializer
    
    Issue #290
    
    ---------
    
    Co-authored-by: khoa-v-nguyen <khoa.nguyen@oddball.io>

--- modules/ask_va_api/spec/requests/v0/static_data_spec.rb
+++ modules/ask_va_api/spec/requests/v0/static_data_spec.rb
@@ -3,8 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe AskVAApi::V0::StaticDataController, type: :request do
-  let(:categories_path) { '/ask_va_api/v0/categories' }
-  let(:index_path) { '/ask_va_api/v0/static_data' }
   let(:datadog_logger) { instance_double(DatadogLogger) }
   let(:span) { instance_double(Datadog::Tracing::Span) }
 
@@ -16,6 +14,7 @@ RSpec.describe AskVAApi::V0::StaticDataController, type: :request do
   end
 
   describe 'GET #index' do
+    let(:index_path) { '/ask_va_api/v0/static_data' }
     let(:expected_response) do
       {
         'Emily' => {
@@ -45,22 +44,9 @@ RSpec.describe AskVAApi::V0::StaticDataController, type: :request do
   end
 
   describe 'GET #categories' do
-    let(:expected_response) do
-      { 'data' =>
-        [{
-          'id' => '1',
-          'type' => 'categories',
-          'attributes' => {
-            'name' => 'Test Category 1'
-          }
-        },
-         {
-           'id' => '2',
-           'type' => 'categories',
-           'attributes' => {
-             'name' => 'Test Category 2'
-           }
-         }] }
+    let(:categories_path) { '/ask_va_api/v0/categories' }
+    let(:expected_hash) do
+      { 'id' => '2', 'type' => 'categories', 'attributes' => { 'name' => 'Benefits Issues Outside the US' } }
     end
 
     context 'when successful' do
@@ -69,8 +55,8 @@ RSpec.describe AskVAApi::V0::StaticDataController, type: :request do
       end
 
       it 'returns categories data' do
+        expect(JSON.parse(response.body)['data']).to include(a_hash_including(expected_hash))
         expect(response).to have_http_status(:ok)
-        expect(JSON.parse(response.body)).to eq(expected_response)
       end
     end
 
@@ -84,16 +70,68 @@ RSpec.describe AskVAApi::V0::StaticDataController, type: :request do
 
       it 'handles other exceptions and returns an internal server error response' do
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(JSON.parse(response.body)).to eq({ 'error' => 'Bad Request Error: bad request' })
+        expect(JSON.parse(response.body)).to eq({ 'error' => 'Dynamics::ErrorHandler::BadRequestError: bad request' })
       end
 
       it 'logs and renders error and sets datadog tags' do
         expect(response).to have_http_status(status)
-        expect(JSON.parse(response.body)['error']).to eq('Bad Request Error: bad request')
+        expect(JSON.parse(response.body)['error']).to eq('Dynamics::ErrorHandler::BadRequestError: bad request')
         expect(datadog_logger).to have_received(:call).with('service_error')
         expect(span).to have_received(:set_tag).with('error', true)
-        expect(span).to have_received(:set_tag).with('error.msg', 'Bad Request Error: bad request')
-        expect(Rails.logger).to have_received(:error).with('Error during service_error: Bad Request Error: bad request')
+        expect(span).to have_received(:set_tag).with('error.msg',
+                                                     'Dynamics::ErrorHandler::BadRequestError: bad request')
+        expect(Rails.logger).to have_received(:error)
+          .with('Error during service_error: Dynamics::ErrorHandler::BadRequestError: bad request')
+      end
+    end
+  end
+
+  describe 'GET #Topics' do
+    let(:category) do
+      AskVAApi::Categories::Entity.new({ id: 2, category: 'VA Health' })
+    end
+    let(:expected_response) do
+      { 'data' =>
+        [{ 'id' => '6', 'type' => 'topics', 'attributes' => { 'name' => 'Compensation' } },
+         { 'id' => '7', 'type' => 'topics',
+           'attributes' => { 'name' => 'Education (Ch.30, 33, 35, 1606, etc. & Work Study)' } },
+         { 'id' => '8', 'type' => 'topics', 'attributes' => { 'name' => 'GI Bill' } },
+         { 'id' => '9', 'type' => 'topics',
+           'attributes' => { 'name' => 'Health Eligibility/Enrollment (Veterans)' } }] }
+    end
+    let(:topics_path) { "/ask_va_api/v0/categories/#{category.id}/topics" }
+
+    context 'when successful' do
+      before { get topics_path }
+
+      it 'returns topics data' do
+        expect(JSON.parse(response.body)).to eq(expected_response)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when an error occurs' do
+      before do
+        allow_any_instance_of(Dynamics::Service)
+          .to receive(:call)
+          .and_raise(Dynamics::ErrorHandler::BadRequestError.new('bad request'))
+        get topics_path
+      end
+
+      it 'handles other exceptions and returns an internal server error response' do
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq({ 'error' => 'Dynamics::ErrorHandler::BadRequestError: bad request' })
+      end
+
+      it 'logs and renders error and sets datadog tags' do
+        expect(response).to have_http_status(status)
+        expect(JSON.parse(response.body)['error']).to eq('Dynamics::ErrorHandler::BadRequestError: bad request')
+        expect(datadog_logger).to have_received(:call).with('service_error')
+        expect(span).to have_received(:set_tag).with('error', true)
+        expect(span).to have_received(:set_tag).with('error.msg',
+                                                     'Dynamics::ErrorHandler::BadRequestError: bad request')
+        expect(Rails.logger).to have_received(:error)
+          .with('Error during service_error: Dynamics::ErrorHandler::BadRequestError: bad request')
       end
     end
   end
