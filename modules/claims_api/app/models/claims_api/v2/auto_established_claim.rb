4d84c5600d5cbdc3ebdb676a076a0136f4bc2fdd
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2023-10-10 09:33:09 -0600

    Adds v2 model for 526, and adds a bang to the create method. (#14079)

---
+++ modules/claims_api/app/models/claims_api/v2/auto_established_claim.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module ClaimsApi
+  module V2
+    class AutoEstablishedClaim < ClaimsApi::AutoEstablishedClaim
+      private
+
+      def validate_service_dates
+        nil
+      end
+    end
+  end
+end
