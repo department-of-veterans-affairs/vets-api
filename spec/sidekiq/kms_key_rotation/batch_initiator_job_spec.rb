9b7f8585bd54eaf4001928a19872618f4d06cd9d
Eric Boehs <eric@boehs.com>
2023-10-19 16:19:36 -0500

    Fix missing gids variable (#14222)
    * Fix missing gids variable
    
    * Omit rescue as it is catching too much
    
    * The specs tried to tell us (back to 7 jobs)

--- spec/sidekiq/kms_key_rotation/batch_initiator_job_spec.rb
+++ spec/sidekiq/kms_key_rotation/batch_initiator_job_spec.rb
@@ -25,14 +25,14 @@ RSpec.describe KmsKeyRotation::BatchInitiatorJob, type: :job do
     it 'batches jobs for records needing rotation' do
       job.perform
 
-      expect(KmsKeyRotation::RotateKeysJob.jobs.size).to eq(3)
+      expect(KmsKeyRotation::RotateKeysJob.jobs.size).to eq(7)
     end
 
     it 'creates RotateKeysJob jobs with batched_gids as args' do
       job.perform
 
       rotate_jobs = KmsKeyRotation::RotateKeysJob.jobs
-      expect(rotate_jobs.size).to eq(3)
+      expect(rotate_jobs.size).to eq(7)
 
       job_class = rotate_jobs.first['class']
       expect(job_class).to eq('KmsKeyRotation::RotateKeysJob')
