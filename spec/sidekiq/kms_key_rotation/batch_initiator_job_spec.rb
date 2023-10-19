019fc904c8b59e6ccba6d0773a58fc2d18428c66
Ryan McNeil <ryan.mcneil@oddball.io>
2023-10-18 19:05:30 -0600

    KMS - Break batch into chunks (#14212)
    * break batch into chunks
    
    * remove each
    
    * fix specs

--- spec/sidekiq/kms_key_rotation/batch_initiator_job_spec.rb
+++ spec/sidekiq/kms_key_rotation/batch_initiator_job_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe KmsKeyRotation::BatchInitiatorJob, type: :job do
   before do
     KmsKeyRotation::BatchInitiatorJob.send(:remove_const, :MAX_RECORDS_PER_BATCH)
     KmsKeyRotation::BatchInitiatorJob.const_set(:MAX_RECORDS_PER_BATCH, 10)
+    KmsKeyRotation::BatchInitiatorJob.send(:remove_const, :CHUNK_SIZE)
+    KmsKeyRotation::BatchInitiatorJob.const_set(:CHUNK_SIZE, 5)
     KmsKeyRotation::BatchInitiatorJob.send(:remove_const, :MAX_RECORDS_PER_JOB)
     KmsKeyRotation::BatchInitiatorJob.const_set(:MAX_RECORDS_PER_JOB, 2)
   end
@@ -26,14 +28,14 @@ RSpec.describe KmsKeyRotation::BatchInitiatorJob, type: :job do
       job.perform
 
       # MAX_RECORDS_PER_BATCH / MAX_RECORDS_PER_JOB = ~5 jobs
-      expect(KmsKeyRotation::RotateKeysJob.jobs.size).to eq(7)
+      expect(KmsKeyRotation::RotateKeysJob.jobs.size).to eq(8)
     end
 
     it 'creates RotateKeysJob jobs with batched_gids as args' do
       job.perform
 
       rotate_jobs = KmsKeyRotation::RotateKeysJob.jobs
-      expect(rotate_jobs.size).to eq(7)
+      expect(rotate_jobs.size).to eq(8)
 
       job_class = rotate_jobs.first['class']
       expect(job_class).to eq('KmsKeyRotation::RotateKeysJob')
