72fd9a1856dc834d1ed94f0dea2193cc6ce2812e
Chapley Watson <chapley.watson@va.gov>
2023-10-25 15:03:06 -0500

    initial push for instance review

--- app/controllers/v0/form1010_ezrs_controller.rb
+++ app/controllers/v0/form1010_ezrs_controller.rb
@@ -4,7 +4,12 @@ module V0
   class Form1010EzrsController < ApplicationController
     def create
       parsed_form = parse_form(params[:form])
-      result = Form1010Ezr::Service.new(@current_user).submit_form(parsed_form)
+
+      begin
+        Form1010Ezr::Service.new(@current_user).submit_form(parsed_form)
+      rescue HCA::SOAPParser::ValidationError
+        raise Common::Exceptions::BackendServiceException.new('HCA422', status: 422)
+      end
 
       clear_saved_form('10-10EZR')
 
--- lib/form1010_ezr/service.rb
+++ lib/form1010_ezr/service.rb
@@ -9,6 +9,7 @@ module Form1010Ezr
   class Service < Common::Client::Base
     include Common::Client::Concerns::Monitoring
     include VA1010Forms::ServiceUtils
+    include SentryLogging
 
     STATSD_KEY_PREFIX = 'api.1010ezr'
 
@@ -34,8 +35,12 @@ module Form1010Ezr
         content = Gyoku.xml(formatted)
         submission = soap.build_request(:save_submit_form, message: content)
 
-        response = perform(:post, '', submission.body)
+        response = with_monitoring do
+          perform(:post, '', submission.body)
+        end
       rescue => e
+        log_submission_failure(parsed_form)
+
         Rails.logger.error "10-10EZR form submission failed: #{e.message}"
         raise e
       end
@@ -69,6 +74,28 @@ module Form1010Ezr
       required_fields = HCA::EzrPostfill.post_fill_hash(@user)
 
       parsed_form.merge!(required_fields)
+    end
+
+    def log_submission_failure(parsed_form)
+      StatsD.increment("#{Form1010Ezr::Service::STATSD_KEY_PREFIX}.failed_wont_retry")
+
+      if form.present?
+        PersonalInformationLog.create!(
+          data: parsed_form,
+          error_class: 'Form1010Ezr FailedWontRetry'
+        )
+
+        log_message_to_sentry(
+          '1010EZR total failure',
+          :error,
+          {
+            first_initial: parsed_form['veteranFullName']['first'][0],
+            middle_initial: parsed_form['veteranFullName']['middle'].try(:[], 0),
+            last_initial: parsed_form['veteranFullName']['last'][0]
+          },
+          ezr: :total_failure
+        )
+      end
     end
   end
 end
