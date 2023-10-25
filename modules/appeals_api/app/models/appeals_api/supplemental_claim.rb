cba331994d8321123fb771963e5107e978bc25c3
Casey Williams <caseywilliams@users.noreply.github.com>
2023-10-25 11:01:45 -0700

    API-29896 Deep stringify AppealReceivedJob arguments (#14278)

--- modules/appeals_api/app/models/appeals_api/supplemental_claim.rb
+++ modules/appeals_api/app/models/appeals_api/supplemental_claim.rb
@@ -251,7 +251,7 @@ module AppealsApi
             statusable_id: id,
             code:,
             detail:
-          }.stringify_keys
+          }.deep_stringify_keys
         )
 
         return if auth_headers.blank? # Go no further if we've removed PII
@@ -266,7 +266,7 @@ module AppealsApi
               guid: id,
               claimant_email: claimant.email,
               claimant_first_name: claimant.first_name
-            }.stringify_keys
+            }.deep_stringify_keys
           )
         end
       end
