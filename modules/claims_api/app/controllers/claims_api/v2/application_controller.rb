18340f6425fa92cb5a649ea02db424e384720b10
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2023-10-31 12:00:43 -0600

    Converts claims, ews, itf to logging template

--- modules/claims_api/app/controllers/claims_api/v2/application_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v2/application_controller.rb
@@ -205,14 +205,14 @@ module ClaimsApi
         end
       end
 
-      def claims_v2_logging(tag = 'traceability', poa: nil, message: nil, icn: target_veteran&.mpi&.icn)
+      def claims_v2_logging(tag = 'traceability', level: :info, message: nil, icn: target_veteran&.mpi&.icn)
         ClaimsApi::Logger.log(tag,
                               icn:,
                               cid: token&.payload&.[]('cid'),
                               current_user: current_user&.uuid,
                               message:,
                               api_version: 'V2',
-                              poa:)
+                              level:)
       end
     end
   end
