6403c87053bc34fe4ae0a662e3e7af1ae80ed3b9
Dakota Larson <133443504+DLarson-Oddball@users.noreply.github.com>
2023-11-03 09:35:49 -0700

    Add Logs to ch36 Workflow (#14392)
    * Add logs to ch36 workflow
    
    * warn => error

--- app/sidekiq/central_mail/submit_career_counseling_job.rb
+++ app/sidekiq/central_mail/submit_career_counseling_job.rb
@@ -3,13 +3,21 @@
 module CentralMail
   class SubmitCareerCounselingJob
     include Sidekiq::Job
+    include SentryLogging
 
     sidekiq_options retry: false
 
     def perform(claim_id, user_uuid = nil)
-      @claim = SavedClaim.find(claim_id)
-      @claim.send_to_central_mail!
-      send_confirmation_email(user_uuid)
+      begin
+        @claim = SavedClaim.find(claim_id)
+        @claim.send_to_central_mail!
+        send_confirmation_email(user_uuid)
+      rescue => e
+        log_message_to_sentry('Error submitting form 25-8832', :error, { uuid: user_uuid })
+        log_exception_to_sentry(e, { uuid: user_uuid })
+        raise e
+      end
+      log_message_to_sentry('Successfully submitted form 25-8832', :info, { uuid: user_uuid })
     end
 
     def send_confirmation_email(user_uuid)
@@ -19,7 +27,11 @@ module CentralMail
                 @claim.parsed_form.dig('claimantInformation', 'emailAddress')
               end
 
-      return if email.blank?
+      if email.blank?
+        log_message_to_sentry('No email to send confirmation regarding submitted form 25-8832', :info,
+                              { uuid: user_uuid })
+        return
+      end
 
       VANotify::EmailJob.perform_async(
         email,
