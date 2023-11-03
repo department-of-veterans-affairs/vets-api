ee8608f4e9ca611cacec2297efbfb62b78d8f5f5
kanchanasuriya <ksuriyamoorthy@kindsys.us>
2023-11-02 17:09:59 -0700

    Betamocks config & patient_check_in response payload changes

--- modules/check_in/app/controllers/check_in/v2/patient_check_ins_controller.rb
+++ modules/check_in/app/controllers/check_in/v2/patient_check_ins_controller.rb
@@ -20,10 +20,7 @@ module CheckIn
           ::V2::Chip::Service.build(check_in: check_in_session).set_echeckin_started
         end
 
-        # Remove stale setECheckInCalled data from appointment data
-        appointment_data = patient_check_in_data.tap { |appt_data| appt_data[:payload]&.delete(:setECheckInCalled) }
-
-        render json: appointment_data
+        render json: patient_check_in_data
       end
 
       def create
