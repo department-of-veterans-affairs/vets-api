4a6577ac2ccc4f8637f6d608931634d828d95df5
John Bramley <John.Bramley@va.gov>
2023-11-06 10:56:30 -0600

    [67028] adds common_name validation to ToU acceptor/decliner (#14198)
    * adds common_name validation to ToU acceptor/decliner
    
    * spec updates
    
    * spec update
    
    * updates
    
    * improves ToU common_name validator error messaging
    
    * spec improvements

---
+++ lib/terms_of_use/exceptions.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module TermsOfUse
+  module Exceptions
+    class CommonNameMissingError < StandardError; end
+  end
+end
--- spec/services/terms_of_use/decliner_spec.rb
+++ spec/services/terms_of_use/decliner_spec.rb
@@ -15,17 +15,36 @@ RSpec.describe TermsOfUse::Decliner, type: :service do
       allow(TermsOfUse::SignUpServiceUpdaterJob).to receive(:perform_async)
     end
 
-    it 'creates a new terms of use agreement with the given version' do
-      expect { subject }.to change { user_account.terms_of_use_agreements.count }.by(1)
-      expect(user_account.terms_of_use_agreements.last.agreement_version).to eq(version)
+    context 'when initialized without a common_name parameter' do
+      let(:common_name) { nil }
+      let(:expected_error) { TermsOfUse::Exceptions::CommonNameMissingError }
+
+      it 'fails validation' do
+        expect { subject }.to raise_error(expected_error)
+      end
+
+      it 'does not create a new terms of use agreement' do
+        expect do
+          subject
+        rescue
+          nil
+        end.not_to change { user_account.terms_of_use_agreements.count }
+      end
     end
 
-    it 'marks the terms of use agreement as declined' do
-      expect(subject).to be_declined
-    end
+    context 'when initialized with a common_name parameter' do
+      it 'creates a new terms of use agreement with the given version' do
+        expect { subject }.to change { user_account.terms_of_use_agreements.count }.by(1)
+        expect(user_account.terms_of_use_agreements.last.agreement_version).to eq(version)
+      end
 
-    it 'enqueues the SignUpServiceUpdaterJob with the terms of use agreement id' do
-      expect(TermsOfUse::SignUpServiceUpdaterJob).to have_received(:perform_async).with(subject.id, common_name)
+      it 'marks the terms of use agreement as declined' do
+        expect(subject).to be_declined
+      end
+
+      it 'enqueues the SignUpServiceUpdaterJob with the terms of use agreement id' do
+        expect(TermsOfUse::SignUpServiceUpdaterJob).to have_received(:perform_async).with(subject.id, common_name)
+      end
     end
   end
 end
