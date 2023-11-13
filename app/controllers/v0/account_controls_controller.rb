2ed45fcdf575269ac4f486b8561dfab12970b4a9
John Bramley <bramleyjl@gmail.com>
2023-11-13 12:58:14 -0700

    makes AccountControlsController a child of SignIn::ApplicationController

--- app/controllers/v0/account_controls_controller.rb
+++ app/controllers/v0/account_controls_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module V0
-  class AccountControlsController < ApplicationController
+  class AccountControlsController < SignIn::ApplicationController
     skip_before_action :authenticate, :verify_authenticity_token
     before_action :authenticate_service_account
 
