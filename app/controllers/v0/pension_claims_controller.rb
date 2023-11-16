71ff24fbb62ade7b2aae3f7a9d8f917372668bc9
Wayne Weibel <waynehw23@hotmail.com>
2023-11-16 12:36:37 -0500

    wip

--- app/controllers/v0/pension_claims_controller.rb
+++ app/controllers/v0/pension_claims_controller.rb
@@ -11,31 +11,29 @@ module V0
     def claim_class
       SavedClaim::Pension
     end
-  end
 
-  # Creates and validates an instance of the class, removing any copies of
-  # the form that had been previously saved by the user.
-  def create
-    PensionBurial::TagSentry.tag_sentry
+    def create
+      PensionBurial::TagSentry.tag_sentry
 
-    claim = claim_class.new(form: filtered_params[:form])
-    unless claim.save
-      StatsD.increment("#{stats_key}.failure")
-      raise Common::Exceptions::ValidationErrors, claim
+      claim = claim_class.new(form: filtered_params[:form])
+      unless claim.save
+        StatsD.increment("#{stats_key}.failure")
+        raise Common::Exceptions::ValidationErrors, claim
+      end
+
+      use_lighthouse = Flipper.enabled?(:pension_claim_submission_to_lighthouse)
+      if use_lighthouse
+        claim.upload_to_lighthouse
+      else
+        claim.process_attachments!
+      end
+
+      StatsD.increment("#{stats_key}.success")
+      Rails.logger.info "ClaimID=#{claim.confirmation_number} Form=#{claim.class::FORM}"
+
+      clear_saved_form(claim.form_id)
+      render(json: claim)
     end
 
-    use_lighthouse = Flipper.enabled?(:pension_claim_submission_to_lighthouse)
-    byebug
-    if use_lighthouse
-      claim.upload_to_lighthouse
-    else
-      claim.process_attachments!
-    end
-
-    StatsD.increment("#{stats_key}.success")
-    Rails.logger.info "ClaimID=#{claim.confirmation_number} Form=#{claim.class::FORM}"
-
-    clear_saved_form(claim.form_id)
-    render(json: claim)
   end
 end
--- spec/sidekiq/lighthouse/pension_benefit_intake_job_spec.rb
+++ spec/sidekiq/lighthouse/pension_benefit_intake_job_spec.rb
@@ -2,7 +2,12 @@
 
 require 'rails_helper'
 
-RSpec.describe Lighthouse::PensionBenefitIntakeJob, type: :job do
+RSpec.describe Lighthouse::PensionBenefitIntakeJob, uploader_helpers: true do
+  stub_virus_scan
+  let(:pension_burial) { create(:pension_burial) }
+  let(:claim) { pension_burial.saved_claim }
+
   describe '#perform' do
+    let(:job) { described_class.new }
   end
 end
