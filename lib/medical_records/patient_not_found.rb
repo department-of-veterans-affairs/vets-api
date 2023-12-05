f6d8dcb78862286de13a657e85aafe873908031e
Mike Moyer <michael.moyer@va.gov>
2023-12-05 12:24:47 -0500

    MHV-51341 Made some fixes

--- lib/medical_records/patient_not_found.rb
+++ lib/medical_records/patient_not_found.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-require 'common/exceptions/service_error'
-
 ##
 # This error is being used to convey that the user's patient FHIR record likely has not yet been created.
 #
 module MedicalRecords
-  class PatientNotFound < ServiceError
+  class PatientNotFound < StandardError
   end
 end
