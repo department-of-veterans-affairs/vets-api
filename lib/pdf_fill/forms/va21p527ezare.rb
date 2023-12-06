91a12840076cf44a1e470b8d6a2c8b5853e9df59
Dan Lim <danlim715@gmail.com>
2023-12-06 01:04:22 -0600

    pension form section 1 field mapping

--- lib/pdf_fill/forms/va21p527ezare.rb
+++ lib/pdf_fill/forms/va21p527ezare.rb
@@ -2,11 +2,14 @@
 
 require 'pdf_fill/hash_converter'
 require 'pdf_fill/forms/form_base'
+require 'pdf_fill/forms/form_helper'
 require 'string_helpers'
 
 module PdfFill
   module Forms
     class Va21p527ezare < FormBase
+      include FormHelper
+      
       KEY = {
         # the key is used to translate your json schema validated form into a hash that can be passed
         # to the pdf-forms library which will write out text onto the pdf
@@ -37,15 +40,54 @@ module PdfFill
             question_suffix: 'A',
             question_text: "VETERAN'S LAST NAME"
           }
-        }
+        },
+        'veteranSocialSecurityNumber' => {
+          'first' => {
+            key: 'form1[0].#subform[48].VeteransSocialSecurityNumber_FirstThreeNumbers[0]'
+          },
+          'second' => {
+            key: 'form1[0].#subform[48].VeteransSocialSecurityNumber_SecondTwoNumbers[0]'
+          },
+          'third' => {
+            key: 'form1[0].#subform[48].VeteransSocialSecurityNumber_LastFourNumbers[0]'
+          }
+        },
+        'veteranDateOfBirth' => {
+          'month' => {
+            key: 'form1[0].#subform[48].DOBmonth[0]'
+          },
+          'day' => {
+            key: 'form1[0].#subform[48].DOBday[0]'
+          },
+          'year' => {
+            key: 'form1[0].#subform[48].DOByear[0]'
+          }
+        },
+        'vaPreviouslyFiled' => {
+          key: 'form1[0].#subform[48].RadioButtonList[0]'
+        },
+        'vaFileNumber' => {
+          key: 'form1[0].#subform[48].VAFileNumber[0]'
+        },
       }.freeze
+
+      def merge_section_1_fields
+        @form_data['veteranFullName']['first'] = @form_data['veteranFullName']['first']&.titleize
+        @form_data['veteranFullName']['middle'] = @form_data['veteranFullName']['middle']&.titleize
+        @form_data['veteranFullName']['last'] = @form_data['veteranFullName']['last']&.titleize
+
+        ssn = @form_data['veteranSocialSecurityNumber']
+        @form_data["veteranSocialSecurityNumber"] = split_ssn(ssn)
+
+        @form_data['veteranDateOfBirth'] = split_date(@form_data['veteranDateOfBirth'])
+        @form_data['vaPreviouslyFiled'] = @form_data['vaFileNumber'].present? ? 1 : 2 # 1 => yes, 2 => no
+      end
 
       def merge_fields(_options = {})
         # your class must include this method, it can be used to make changes to the form
         # before final processing
-        @form_data['veteranFullName']['first'] = @form_data['veteranFullName']['first']&.titleize
-        @form_data['veteranFullName']['middle'] = @form_data['veteranFullName']['middle']&.titleize
-        @form_data['veteranFullName']['last'] = @form_data['veteranFullName']['last']&.titleize
+        merge_section_1_fields
+
         @form_data
       end
     end
