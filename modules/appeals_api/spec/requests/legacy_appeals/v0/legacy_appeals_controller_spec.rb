0e12d27ad8b2e76908737a6cb59bc55a872adea6
Casey Williams <caseywilliams@users.noreply.github.com>
2023-12-06 12:23:10 -0800

    API-28525 Move Legacy Appeals headers to params (#14777)

--- modules/appeals_api/spec/requests/legacy_appeals/v0/legacy_appeals_controller_spec.rb
+++ modules/appeals_api/spec/requests/legacy_appeals/v0/legacy_appeals_controller_spec.rb
@@ -5,18 +5,18 @@ require AppealsApi::Engine.root.join('spec', 'spec_helper.rb')
 
 describe AppealsApi::LegacyAppeals::V0::LegacyAppealsController, type: :request do
   describe('#schema') do
-    let(:path) { '/services/appeals/legacy-appeals/v0/schemas/headers' }
+    let(:path) { '/services/appeals/legacy-appeals/v0/schemas/params' }
 
-    it 'renders the json schema for request headers with shared refs' do
+    it 'renders the json schema for request params with shared refs' do
       with_openid_auth(described_class::OAUTH_SCOPES[:GET]) do |auth_header|
         get(path, headers: auth_header)
       end
 
       expect(response.status).to eq(200)
       expect(JSON.parse(response.body)['description']).to eq(
-        'JSON Schema for Legacy Appeals endpoint headers (Decision Reviews API)'
+        'JSON Schema for Legacy Appeals endpoint parameters'
       )
-      expect(response.body).to include('{"$ref":"nonBlankString.json"}')
+      expect(response.body).to include('{"$ref":"icn.json"}')
     end
 
     it_behaves_like('an endpoint with OpenID auth', scopes: described_class::OAUTH_SCOPES[:GET]) do
@@ -31,22 +31,28 @@ describe AppealsApi::LegacyAppeals::V0::LegacyAppealsController, type: :request 
     let(:headers) { {} }
     let(:ssn) { '502628285' }
     let(:icn) { '1234567890V012345' }
+    let(:params) { icn ? { 'icn' => icn } : {} }
+    let(:body) { JSON.parse(response.body) }
+    let(:cassette) { 'caseflow/legacy_appeals_get_by_ssn' }
+    let(:mpi_response) { create(:find_profile_response, profile: build(:mpi_profile)) }
 
     before do
-      headers['X-VA-SSN'] = ssn if ssn.present?
-      headers['X-VA-ICN'] = icn if icn.present?
+      allow_any_instance_of(MPI::Service)
+        .to receive(:find_profile_by_identifier)
+        .with(identifier: icn, identifier_type: MPI::Constants::ICN).and_return(mpi_response)
+
+      VCR.use_cassette(cassette) do
+        with_openid_auth(described_class::OAUTH_SCOPES[:GET]) do |auth_header|
+          get(path, headers: auth_header, params:)
+        end
+      end
     end
 
     context 'when all required fields provided' do
       it 'GETs legacy appeals from Caseflow successfully' do
-        VCR.use_cassette('caseflow/legacy_appeals_get_by_ssn') do
-          get_legacy_appeals(headers)
-
-          expect(response).to have_http_status(:ok)
-          json = JSON.parse(response.body)
-          expect(json['data']).not_to be_nil
-          expect(json['data'][0]['attributes']).to include('latestSocSsocDate')
-        end
+        expect(response).to have_http_status(:ok)
+        expect(body['data']).not_to be_nil
+        expect(body['data'][0]['attributes']).to include('latestSocSsocDate')
       end
     end
 
@@ -54,12 +60,10 @@ describe AppealsApi::LegacyAppeals::V0::LegacyAppealsController, type: :request 
       let(:icn) { nil }
 
       it 'returns a 422 error with details' do
-        get_legacy_appeals(headers)
-
         expect(response).to have_http_status(:unprocessable_entity)
         error = JSON.parse(response.body)['errors'][0]
         expect(error['detail']).to include('One or more expected fields were not found')
-        expect(error['meta']['missing_fields']).to include('X-VA-ICN')
+        expect(error['meta']['missing_fields']).to include('icn')
       end
     end
 
@@ -67,8 +71,6 @@ describe AppealsApi::LegacyAppeals::V0::LegacyAppealsController, type: :request 
       let(:icn) { '229384' }
 
       it 'returns a 422 error with details' do
-        get_legacy_appeals(headers)
-
         expect(response).to have_http_status(:unprocessable_entity)
         error = JSON.parse(response.body)['errors'][0]
         expect(error['title']).to eql('Invalid length')
@@ -80,19 +82,11 @@ describe AppealsApi::LegacyAppeals::V0::LegacyAppealsController, type: :request 
       let(:icn) { '22938439103910392' }
 
       it 'returns a 422 error with details' do
-        get_legacy_appeals(headers)
-
         expect(response).to have_http_status(:unprocessable_entity)
         error = JSON.parse(response.body)['errors'][0]
         expect(error['title']).to eql('Invalid pattern')
         expect(error['detail']).to include("'#{icn}' did not match the defined pattern")
       end
-    end
-  end
-
-  def get_legacy_appeals(headers)
-    with_openid_auth(described_class::OAUTH_SCOPES[:GET]) do |auth_header|
-      get(path, headers: headers.merge(auth_header))
     end
   end
 end
