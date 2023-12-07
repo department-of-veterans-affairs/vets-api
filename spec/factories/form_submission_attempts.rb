7c0b0e55f09b80ce8bc1b4d72488d35b8e88f45d
Eric Tillberg <Thrillberg@users.noreply.github.com>
2023-12-06 21:46:29 -0500

    Rebased without db changes

---
+++ spec/factories/form_submission_attempts.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :form_submission_attempt do
+    trait :pending do
+      aasm_state { 'pending' }
+    end
+
+    trait :success do
+      aasm_state { 'success' }
+    end
+
+    trait :vbms do
+      aasm_state { 'vbms' }
+    end
+
+    trait :failure do
+      aasm_state { 'failure' }
+    end
+  end
+end
