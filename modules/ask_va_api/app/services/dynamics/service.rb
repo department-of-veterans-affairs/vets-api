42151168080a67500d04149a4ebdd886b10f5ec0
khoa-v-nguyen <khoa.nguyen@oddball.io>
2023-12-11 10:30:43 -0700

    testing environment

--- modules/ask_va_api/app/services/dynamics/service.rb
+++ modules/ask_va_api/app/services/dynamics/service.rb
@@ -51,13 +51,15 @@ module Dynamics
       parse_response(response.body)
     rescue ErrorHandler::ServiceError => e
       log_error(endpoint, e.class.name)
-      [e, { bearer: token(method, endpoint)&.chars&.first(5), env: Rails.env, tenant: tenant_id.chars.first(5) }]
+      [e,
+       { bearer: token(method)&.chars&.first(5), env: Rails.env, vsp_env: Settings.vsp_environment,
+         tenant: tenant_id.chars.first(5), base_uri: }]
     end
 
     def invoke_request(endpoint, method, payload, params)
       logger.call("api_call.#{method}", tags: build_tags(endpoint)) do
         conn.public_send(method, endpoint, prepare_payload(method, payload, params)) do |req|
-          req.headers = default_header.merge('Authorization' => "Bearer #{token(method, endpoint)}")
+          req.headers = default_header.merge('Authorization' => "Bearer #{token(method)}")
         end
       end
     end
@@ -107,8 +109,8 @@ module Dynamics
       }
     end
 
-    def token(method, endpoint)
-      logger.call("api_call.#{method}", tags: build_tags(endpoint)) do
+    def token(method)
+      logger.call("api_call.#{method}", tags: build_tags("/#{tenant_id}/oauth2/token")) do
         response = conn(url: auth_url).post("/#{tenant_id}/oauth2/token") do |req|
           req.headers = token_headers
           req.body = URI.encode_www_form(auth_params)
