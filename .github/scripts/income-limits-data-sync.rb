a5f163143da6bd99232e028c15db5d6ed4d6e319
Daniel Sasser <daniel.sasser@agile6.com>
2023-12-14 12:40:35 -0800

    VACMS-16217: Output table names.

--- .github/scripts/income-limits-data-sync.rb
+++ .github/scripts/income-limits-data-sync.rb
@@ -26,22 +26,24 @@ temp_directory = ENV['TEMP_FOLDER']
 # Connect to the Oracle database
 conn = OCI8.new(db_username, db_password, db_connection_string)
 
-files.each do |table, file|
-  # Query the data for the table.
-  sql_query = "SELECT * FROM #{table}"
-  result = conn.exec(sql_query)
+# files.each do |table, file|
+#   # Query the data for the table.
+#   sql_query = "SELECT * FROM #{table}"
+#   result = conn.exec(sql_query)
 
-  # Create a CSV file from the results in the temp directory.
-  CSV.open("#{temp_directory}/#{file}", 'w') do |csv|
-    # Write header
-    csv << result.get_col_names
+#   # Create a CSV file from the results in the temp directory.
+#   CSV.open("#{temp_directory}/#{file}", 'w') do |csv|
+#     # Write header
+#     csv << result.get_col_names
 
-    # Write rows
-    result.fetch do |row|
-      csv << row
-    end
-  end
-end
+#     # Write rows
+#     result.fetch do |row|
+#       csv << row
+#     end
+#   end
+# end
 
+query = 'SELECT table_name, owner FROM user_tables ORDER BY owner, table_name'
+conn.exec(query) do |r| puts r.join(',') end
 # Close the database connection
 conn.logoff
