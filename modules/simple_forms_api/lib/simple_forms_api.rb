139e47e500102a83ffaa8e605d99e425e3231d85
Eric Tillberg <Thrillberg@users.noreply.github.com>
2023-12-14 19:58:19 -0500

    Strip upcased form data from error messages

--- modules/simple_forms_api/lib/simple_forms_api.rb
+++ modules/simple_forms_api/lib/simple_forms_api.rb
@@ -37,8 +37,13 @@ module SimpleFormsApi
                  'error message has been redacted to keep PII from getting leaked'
         end
 
+        remove_words(words_to_remove)
+      end
+
+      def remove_words(words_to_remove)
         words_to_remove.compact.each do |word|
           message.gsub!(word, '')
+          message.gsub!(word.upcase, '')
         end
 
         message
