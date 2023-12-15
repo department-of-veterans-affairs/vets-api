4956b6382ad8ff4132d2cbb20cad773bbc8533c1
agravell047 <anthony.gravell@bylight.com>
2023-12-15 16:56:44 -0500

    fixed rx controller

--- modules/my_health/app/controllers/my_health/rx_controller.rb
+++ modules/my_health/app/controllers/my_health/rx_controller.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 require 'rx/client'
+require 'rx/medications_client'
 
 module MyHealth
   class RxController < ApplicationController
     include ActionController::Serialization
     include MyHealth::MHVControllerConcerns
     service_tag 'mhv-medications'
-    skip_before_action :authenticate
 
     protected
 
     def client
-      @client ||= Rx::Client.new(session: { user_id: 17621060 })
+      @client ||= Rx::MedicationsClient.new(session: { user_id: current_user.mhv_correlation_id })
     end
 
     def authorize
-      # raise_access_denied unless current_user.authorize(:mhv_prescriptions, :access?)
+      raise_access_denied unless current_user.authorize(:mhv_prescriptions, :access?)
     end
 
     def raise_access_denied
