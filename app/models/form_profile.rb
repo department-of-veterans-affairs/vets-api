c476284f966d1d8bdede3eb95970c918208e98fc
Eric Tillberg <Thrillberg@users.noreply.github.com>
2023-12-18 09:37:19 -0500

    Run prefill method only if prefill is enabled on the form

--- app/models/form_profile.rb
+++ app/models/form_profile.rb
@@ -189,15 +189,19 @@ class FormProfile
   # * TODO(AJD): MIS (military history)
   #
   def prefill
-    @identity_information = initialize_identity_information
-    @contact_information = initialize_contact_information
-    @military_information = initialize_military_information
-    mappings = self.class.mappings_for_form(form_id)
-
     form = form_id == '1010EZ' ? '1010ez' : form_id
-    form_data = generate_prefill(mappings) if FormProfile.prefill_enabled_forms.include?(form)
+    if FormProfile.prefill_enabled_forms.include?(form)
+      @identity_information = initialize_identity_information
+      @contact_information = initialize_contact_information
+      @military_information = initialize_military_information
+      mappings = self.class.mappings_for_form(form_id)
 
-    { form_data:, metadata: }
+      form_data = generate_prefill(mappings)
+
+      { form_data:, metadata: }
+    else
+      { metadata: }
+    end
   end
 
   def initialize_military_information_vaprofile
