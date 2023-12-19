c1290aa14ee507edd24fd7b1af0d60623ae40c7a
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2023-12-19 16:06:53 -0500

    [#MHV-44453] parametrize route handler

--- modules/my_health/app/controllers/my_health/v1/messages_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/messages_controller.rb
@@ -58,8 +58,12 @@ module MyHealth
 
       def thread
         message_id = params[:id].try(:to_i)
-        # resource = client.get_message_history(message_id)
-        resource = client.get_messages_for_thread(message_id)
+        if params[:full_body] == 'true'
+          #returns full body of message including attachments attributes
+          resource = client.get_full_messages_for_thread(message_id)
+        else
+          resource = client.get_messages_for_thread(message_id)
+        end
         raise Common::Exceptions::RecordNotFound, message_id if resource.blank?
 
         render json: resource,
