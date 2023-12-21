f69525909a6f3388c0efd9ab0f3b4d8942492db0
Vanson Samuel <37637+binq@users.noreply.github.com>
2023-12-21 14:58:28 -0600

    Added Verification model

--- modules/vye/config/routes.rb
+++ modules/vye/config/routes.rb
@@ -3,5 +3,6 @@
 Vye::Engine.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     resource :user_info, only: [:show], path: '/'
+    resource :verifications, only: [:create], path: '/verify'
   end
 end
