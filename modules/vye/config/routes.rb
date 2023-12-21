3ffe8e82f4ebb6cdb3e67a9e6aebf4c22f180b45
Vanson Samuel <37637+binq@users.noreply.github.com>
2023-12-21 14:49:36 -0600

    Added AddressChange model

--- modules/vye/config/routes.rb
+++ modules/vye/config/routes.rb
@@ -3,5 +3,6 @@
 Vye::Engine.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     resource :user_info, only: [:show], path: '/'
+    resource :address_changes, only: [:create], path: '/address'
   end
 end
