b393635cfba91feb22021469b508a4ae9b588c99
Holden Hinkle <holden@holdenhinkle.com>
2023-12-22 11:07:44 -0500

    add multiple poa codes to some veteran_service_officer in before block

--- modules/veteran/spec/requests/v0/accredited_representatives_spec.rb
+++ modules/veteran/spec/requests/v0/accredited_representatives_spec.rb
@@ -262,7 +262,7 @@ RSpec.describe 'Find a Rep - Accredited Representatives spec', type: :request do
     context 'when searching for a veteran service officer' do
       before do
         # Create representatives
-        create(:representative, representative_id: '123', poa_codes: ['A13'], user_types: ['veteran_service_officer'],
+        create(:representative, representative_id: '123', poa_codes: %w[A12 A13], user_types: ['veteran_service_officer'], # rubocop:disable Layout/LineLength
                                 long: -77.050552, lat: 38.820450, location: 'POINT(-77.050552 38.820450)',
                                 first_name: 'Bob', last_name: 'Law') # ~6 miles from Washington, D.C.
 
@@ -270,11 +270,11 @@ RSpec.describe 'Find a Rep - Accredited Representatives spec', type: :request do
                                 long: -77.050552, lat: 38.820450, location: 'POINT(-77.050552 38.820450)',
                                 first_name: 'Bobby', last_name: 'Low') # ~6 miles from Washington, D.C.
 
-        create(:representative, representative_id: '234', poa_codes: ['A13'], user_types: ['veteran_service_officer'],
+        create(:representative, representative_id: '234', poa_codes: %w[A12 A13], user_types: ['veteran_service_officer'], # rubocop:disable Layout/LineLength
                                 long: -77.436649, lat: 39.101481, location: 'POINT(-77.436649 39.101481)',
                                 first_name: 'Eliseo', last_name: 'Schroeder') # ~25 miles from Washington, D.C.
 
-        create(:representative, representative_id: '345', poa_codes: ['A13'], user_types: ['veteran_service_officer'],
+        create(:representative, representative_id: '345', poa_codes: %w[A12 A13], user_types: ['veteran_service_officer'], # rubocop:disable Layout/LineLength
                                 long: -76.609383, lat: 39.299236, location: 'POINT(-76.609383 39.299236)',
                                 first_name: 'Marci', last_name: 'Weissnat') # ~35 miles from Washington, D.C.
 
