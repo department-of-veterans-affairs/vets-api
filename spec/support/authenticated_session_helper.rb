72c43c44b7155a712d9ef99cb2e7d29e6b425964
Ryan McNeil <ryan.mcneil@oddball.io>
2023-12-26 15:25:59 -0700

    use TestSession

--- spec/support/authenticated_session_helper.rb
+++ spec/support/authenticated_session_helper.rb
@@ -4,12 +4,13 @@ module AuthenticatedSessionHelper
   def sign_in(user = FactoryBot.build(:user, :loa3), token = nil, raw = false)
     user = user.persisted? ? user : User.create(user)
     token ||= 'abracadabra'
-    session_object = Session.create(uuid: user.uuid, token:)
+    session_object = ActionController::TestSession.new(uuid: user.uuid, token:)
+    # session_object = Session.create(uuid: user.uuid, token:)
     session_options = { key: 'api_session', secure: false, http_only: true }
     if raw
       Rails::SessionCookie::App.new(session_object.to_hash, session_options).session_cookie
     elsif cookies.is_a?(ActionDispatch::Cookies::CookieJar)
-      request.session = session_object.to_hash
+      request.session = session_object
     else
       raw_session_cookie = Rails::SessionCookie::App.new(session_object.to_hash, session_options).session_cookie
       cookies.merge(raw_session_cookie)
