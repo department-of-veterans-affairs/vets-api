bba42409d3032b0fb0267654877adb1fd9fa9d87
Kristen Brown <11942904+kristen-brown@users.noreply.github.com>
2023-12-28 15:02:14 -0500

    API-32464: VAForms updates

--- modules/va_forms/spec/sidekiq/flipper_status_alert_spec.rb
+++ modules/va_forms/spec/sidekiq/flipper_status_alert_spec.rb
@@ -13,12 +13,14 @@ describe VAForms::FlipperStatusAlert, type: :job do
     let(:config_file_path) { VAForms::Engine.root.join('config', 'flipper', 'enabled_features.yml') }
 
     let(:warning_emoji) { described_class::WARNING_EMOJI }
-    let(:disabled_flag_emoji) { described_class::DISABLED_FLAG_EMOJI }
-    let(:missing_flag_emoji) { described_class::MISSING_FLAG_EMOJI }
+    let(:traffic_light_emoji) { described_class::TRAFFIC_LIGHT_EMOJI }
 
-    let(:missing_file_message) { "#{warning_emoji} #{described_class} features file does not exist" }
-    let(:flag_status_message) do
-      "#{warning_emoji} One or more features expected to be enabled were found disabled or missing"
+    let(:missing_file_message) { "#{warning_emoji} #{described_class} features file does not exist." }
+    let(:flag_message) { "#{warning_emoji} One or more features expected to be enabled were found to be disabled." }
+
+    before do
+      allow(VAForms::Slack::Messenger).to receive(:new).and_return(messenger_instance)
+      allow(messenger_instance).to receive(:notify!)
     end
 
     it 'notifies Slack of missing config file when no config file found' do
@@ -32,11 +34,9 @@ describe VAForms::FlipperStatusAlert, type: :job do
 
     it 'fetches enabled status of common and current env features when config file contains both' do
       allow(YAML).to receive(:load_file).and_return({ 'common' => %w[feature1], 'production' => %w[feature2] })
-      bulk_checker_result = { enabled: [], disabled: [], missing: %w[feature1 feature2] }
+      bulk_checker_result = { enabled: [], disabled: %w[feature1 feature2], missing: [] }
       expect(Flipper::Utilities::BulkFeatureChecker)
         .to receive(:enabled_status).with(%w[feature1 feature2]).and_return(bulk_checker_result)
-      allow(VAForms::Slack::Messenger).to receive(:new).and_return(messenger_instance)
-      allow(messenger_instance).to receive(:notify!)
 
       with_settings(Settings, vsp_environment: 'production') do
         described_class.new.perform
@@ -45,11 +45,9 @@ describe VAForms::FlipperStatusAlert, type: :job do
 
     it 'fetches enabled status of common features only when config file contains no current env features' do
       allow(YAML).to receive(:load_file).and_return({ 'common' => %w[feature1], 'development' => nil })
-      bulk_checker_result = { enabled: [], disabled: [], missing: %w[feature1] }
+      bulk_checker_result = { enabled: [], disabled: %w[feature1], missing: [] }
       expect(Flipper::Utilities::BulkFeatureChecker)
         .to receive(:enabled_status).with(%w[feature1]).and_return(bulk_checker_result)
-      allow(VAForms::Slack::Messenger).to receive(:new).and_return(messenger_instance)
-      allow(messenger_instance).to receive(:notify!)
 
       with_settings(Settings, vsp_environment: 'development') do
         described_class.new.perform
@@ -58,11 +56,9 @@ describe VAForms::FlipperStatusAlert, type: :job do
 
     it 'fetches enabled status of current env features only when config file contains no common features' do
       allow(YAML).to receive(:load_file).and_return({ 'common' => nil, 'staging' => %w[feature1] })
-      bulk_checker_result = { enabled: [], disabled: [], missing: %w[feature1] }
+      bulk_checker_result = { enabled: [], disabled: %w[feature1], missing: [] }
       expect(Flipper::Utilities::BulkFeatureChecker)
         .to receive(:enabled_status).with(%w[feature1]).and_return(bulk_checker_result)
-      allow(VAForms::Slack::Messenger).to receive(:new).and_return(messenger_instance)
-      allow(messenger_instance).to receive(:notify!)
 
       with_settings(Settings, vsp_environment: 'staging') do
         described_class.new.perform
@@ -84,41 +80,8 @@ describe VAForms::FlipperStatusAlert, type: :job do
       allow(Flipper::Utilities::BulkFeatureChecker).to receive(:enabled_status).and_return(bulk_checker_result)
       expected_notify = {
         class: described_class.name,
-        warning: flag_status_message,
-        disabled_flags: "#{disabled_flag_emoji} feature2, feature3 #{disabled_flag_emoji}",
-        missing_flags: 'None'
-      }
-      expect(VAForms::Slack::Messenger).to receive(:new).with(expected_notify).and_return(messenger_instance)
-      expect(messenger_instance).to receive(:notify!).once
-
-      described_class.new.perform
-    end
-
-    it 'notifies Slack when some features are missing' do
-      allow(YAML).to receive(:load_file).and_return({ 'common' => %w[feature1 feature2 feature3] })
-      bulk_checker_result = { enabled: %w[feature1], disabled: [], missing: %w[feature2 feature3] }
-      allow(Flipper::Utilities::BulkFeatureChecker).to receive(:enabled_status).and_return(bulk_checker_result)
-      expected_notify = {
-        class: described_class.name,
-        warning: flag_status_message,
-        disabled_flags: 'None',
-        missing_flags: "#{missing_flag_emoji} feature2, feature3 #{missing_flag_emoji}"
-      }
-      expect(VAForms::Slack::Messenger).to receive(:new).with(expected_notify).and_return(messenger_instance)
-      expect(messenger_instance).to receive(:notify!).once
-
-      described_class.new.perform
-    end
-
-    it 'notifies slack when there are disabled and missing features' do
-      allow(YAML).to receive(:load_file).and_return({ 'common' => %w[feature1 feature2 feature3 feature4 feature5] })
-      bulk_checker_result = { enabled: %w[feature1], disabled: %w[feature2 feature3], missing: %w[feature4 feature5] }
-      allow(Flipper::Utilities::BulkFeatureChecker).to receive(:enabled_status).and_return(bulk_checker_result)
-      expected_notify = {
-        class: described_class.name,
-        warning: flag_status_message,
-        disabled_flags: "#{disabled_flag_emoji} feature2, feature3 #{disabled_flag_emoji}",
-        missing_flags: "#{missing_flag_emoji} feature4, feature5 #{missing_flag_emoji}"
+        warning: flag_message,
+        disabled_flags: "#{traffic_light_emoji} feature2, feature3 #{traffic_light_emoji}"
       }
       expect(VAForms::Slack::Messenger).to receive(:new).with(expected_notify).and_return(messenger_instance)
       expect(messenger_instance).to receive(:notify!).once
