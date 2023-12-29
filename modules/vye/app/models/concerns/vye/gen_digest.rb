595bdcc6e9447a4789975c15fe253bb9637fe7df
Vanson Samuel <37637+binq@users.noreply.github.com>
2023-12-29 15:45:06 -0600

    nesting classes inside modules

--- modules/vye/app/models/concerns/vye/gen_digest.rb
+++ modules/vye/app/models/concerns/vye/gen_digest.rb
@@ -3,25 +3,25 @@
 require 'base64'
 require 'openssl'
 
-module Vye; end
+module Vye
+  module GenDigest
+    extend ActiveSupport::Concern
 
-module Vye::GenDigest
-  extend ActiveSupport::Concern
+    module Common
+      GEN_DIGEST_CONFIG =
+        [Settings.vye.scrypt, %i[salt N r p length]]
+        .then { |s, l| l.collect { |m| [m, s.send(m)] } }
+        .then { |pairs| Hash[*pairs.flatten] }
+        .freeze
 
-  module Common
-    GEN_DIGEST_CONFIG =
-      [Settings.vye.scrypt, %i[salt N r p length]]
-      .then { |s, l| l.collect { |m| [m, s.send(m)] } }
-      .then { |pairs| Hash[*pairs.flatten] }
-      .freeze
-
-    def gen_digest(value)
-      value&.then { |v| Base64.encode64(OpenSSL::KDF.scrypt(v, **GEN_DIGEST_CONFIG)).strip }
+      def gen_digest(value)
+        value&.then { |v| Base64.encode64(OpenSSL::KDF.scrypt(v, **GEN_DIGEST_CONFIG)).strip }
+      end
     end
-  end
 
-  included do
-    extend Common
-    include Common
+    included do
+      extend Common
+      include Common
+    end
   end
 end
