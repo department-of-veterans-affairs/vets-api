e86c92001e5ad0449462e2e6ee314458874fb08b
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2024-01-02 10:19:12 -0500

    [#MHV-51057] mhv session lock on refresh

--- lib/common/client/concerns/mhv_locked_session_client.rb
+++ lib/common/client/concerns/mhv_locked_session_client.rb
@@ -19,8 +19,8 @@ module Common
         extend ActiveSupport::Concern
         include SentryLogging
 
-        LOCK_RETRY_DELAY = 1 # Number of seconds to wait between attempts to acquire a session lock
-        RETRY_ATTEMPTS = 10 # How many times to attempt to acquire a session lock
+        LOCK_RETRY_DELAY = 0.3 # Number of seconds to wait between attempts to acquire a session lock
+        RETRY_ATTEMPTS = 30 # How many times to attempt to acquire a session lock
 
         attr_reader :session
 
@@ -39,20 +39,45 @@ module Common
         def authenticate
           raise 'ICN is required for session creation' unless user_key
 
-          iteration = 0
+          # iteration = 0
+          puts "session.expired: #{session.expired?}"
+          # return self if !session.expired?
+          attempts = 0
+          # max_attempts = 10  # Set this to the maximum number of attempts you want to allow
+          
+          while session.expired? && attempts <= RETRY_ATTEMPTS
 
-          # Loop unless a complete, valid MHV session exists, or until max_iterations is reached
-          while invalid?(session) && iteration < RETRY_ATTEMPTS
-            break if lock_and_get_session # Break out of the loop once a new session is created.
+              puts "redis_key.exists?(lock_key): #{redis_key.exists?(lock_key)}"
+              if redis_key.exists?(lock_key)
+                attempts += 1
+                raise "Max attempts exceeded" if attempts > RETRY_ATTEMPTS
 
-            sleep(LOCK_RETRY_DELAY)
+                sleep(LOCK_RETRY_DELAY) # Wait for a short time before retrying
 
-            # Refresh the MHV session reference in case another thread has updated it.
-            refresh_session(session)
-            iteration += 1
+                puts "refresh session"
+                refresh_session(session)
+              else
+                lock_and_get_session
+                break
+              end
+            
           end
-          if invalid?(session) && iteration >= RETRY_ATTEMPTS
-            Rails.logger.info("Failed to create #{@client_session} after #{iteration} attempts to acquire lock")
+
+          # # Loop unless a complete, valid MHV session exists, or until max_iterations is reached
+          # while invalid?(session) 
+          #   puts "session is invalid"
+          #   while iteration < RETRY_ATTEMPTS
+          #     break if lock_and_get_session # Break out of the loop once a new session is created.
+
+          #     sleep(LOCK_RETRY_DELAY)
+
+          #     # Refresh the MHV session reference in case another thread has updated it.
+          #     refresh_session(session)
+          #     iteration += 1
+          #   end
+          # end
+          if invalid?(session) && attempts >= RETRY_ATTEMPTS
+            Rails.logger.info("Failed to create #{@client_session} after #{attempts} attempts to acquire lock")
           end
 
           self
@@ -90,30 +115,43 @@ module Common
         #
         def lock_and_get_session
           redis_lock = obtain_redis_lock
+          puts "lock_and_get_session redis_lock: #{redis_lock}"
           if redis_lock
             begin
               @session = get_session
+              @session.save
               return true
             ensure
-              release_redis_lock(redis_lock)
+              release_redis_lock
             end
           end
           false
         end
 
-        def obtain_redis_lock
-          lock_key = "mhv_fhir_session_lock:#{user_key}"
-          redis_lock = Redis::Namespace.new(REDIS_CONFIG[session_config_key][:namespace], redis: $redis)
-          success = redis_lock.set(lock_key, 1, nx: true, ex: REDIS_CONFIG[session_config_key][:each_ttl])
-
-          return redis_lock if success
-
-          nil
+        def redis_key
+          Redis::Namespace.new(REDIS_CONFIG[session_config_key][:namespace], redis: $redis)
         end
 
-        def release_redis_lock(redis_lock)
-          lock_key = "mhv_fhir_session_lock:#{user_key}"
-          redis_lock.del(lock_key)
+        def lock_key
+          lock_key = "mhv_session_lock:#{user_key}"
+        end
+
+        def obtain_redis_lock
+          puts "obtain_redis_lock"
+          # lock_key = "mhv_session_lock:#{user_key}"
+          # redis_lock = Redis::Namespace.new(REDIS_CONFIG[session_config_key][:namespace], redis: $redis)
+          # puts "Lock exists: #{redis_key.exists?(lock_key)}"  # Add this line
+          # success = redis_key.set(lock_key, 1, nx: true, ex: REDIS_CONFIG[session_config_key][:each_ttl])
+          redis_key.set(lock_key, 1, nx: true, ex: REDIS_CONFIG[session_config_key][:each_ttl])
+          # return redis_lock if success
+
+          # nil
+        end
+
+        def release_redis_lock
+          puts "release_redis_lock"
+          # lock_key = "mhv_session_lock:#{user_key}"
+          redis_key.del(lock_key)
         end
       end
     end
