b80d92a34d004745a72327e84405ed8dbb13858b
Gerard Sullivan <Gerard.Sullivan@va.gov>
2024-01-04 09:57:46 -0800

    update

--- spec/models/prescription_details_spec.rb
+++ spec/models/prescription_details_spec.rb
@@ -51,56 +51,7 @@ describe PrescriptionDetails do
   end
 
   context 'sorted_dispensed_date test cases with nil' do
-    subject { described_class.new(attributes_for(:prescription_details, rx_rf_records: [
-              [
-                'rf_record',
-                [
-                  {
-                    refill_status: 'suspended',
-                    refill_submit_date: 'Wed, 11 Jan 2023 00:00:00 EDT',
-                    refill_date: 'Sat, 15 Jul 2023 00:00:00 EDT',
-                    refill_remaining: 4,
-                    facility_name: 'DAYT29',
-                    is_refillable: false,
-                    is_trackable: false,
-                    prescription_id: 22_332_828,
-                    sig: nil,
-                    ordered_date: 'Fri, 04 Aug 2023 00:00:00 EDT',
-                    quantity: nil,
-                    expiration_date: nil,
-                    prescription_number: '2720542',
-                    prescription_Name: 'ONDANSETRON 8 MG TAB',
-                    dispensed_date: nil,
-                    station_number: '989',
-                    in_cerner_transition: false,
-                    not_refillable_display_message: nil,
-                    cmop_division_phone: nil,
-                    cmop_ndc_number: nil,
-                    id: 22_332_828,
-                    user_id: 16_955_936,
-                    provider_first_name: nil,
-                    provider_last_name: nil,
-                    remarks: nil,
-                    division_name: nil,
-                    modified_date: nil,
-                    institution_id: nil,
-                    dial_cmop_division_phone: '',
-                    disp_status: 'Suspended',
-                    ndc: nil,
-                    reason: nil,
-                    prescription_number_index: 'RF1',
-                    prescription_source: 'RF',
-                    disclaimer: nil,
-                    indication_for_use: nil,
-                    indication_for_use_flag: nil,
-                    category: 'Rx Medication',
-                    tracking_list: nil,
-                    rx_rf_records: nil,
-                    tracking: false
-                  }
-                ]
-              ]]))   
-            }
+    subject { described_class.new(attributes_for(:prescription_details, rx_rf_records: [['rf_record',[{refill_date: 'Sat, 15 Jul 2023 00:00:00 EDT', dispensed_date: nil}]]])) }
 
     it 'sorted_dispensed_date should be Date.new(0) because rx_rf_records is empty array' do
       expect(subject).to have_attributes(sorted_dispensed_date: Date.new(0))
