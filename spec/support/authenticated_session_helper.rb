9e4559d2f43100b92cf981265c1340e9c64bc94a
Ryan McNeil <ryan.mcneil@oddball.io>
2024-01-04 14:30:04 -0700

    pull out session params

--- spec/support/authenticated_session_helper.rb
+++ spec/support/authenticated_session_helper.rb
@@ -4,8 +4,14 @@ module AuthenticatedSessionHelper
   def sign_in(user = FactoryBot.build(:user, :loa3), token = nil, raw = false)
     user = user.persisted? ? user : User.create(user)
     token ||= 'abracadabra'
-    session_object = ActionController::TestSession.new(uuid: user.uuid, token:)
+    session_params = {
+      uuid: user.uuid,
+      token:,
+      created_at: Time.zone.now
+    }
+    session_object = ActionController::TestSession.new(session_params)
     session_options = { key: 'api_session', secure: false, http_only: true }
+
     if raw
       Rails::SessionCookie::App.new(session_object.to_hash, session_options).session_cookie
     elsif cookies.is_a?(ActionDispatch::Cookies::CookieJar)
