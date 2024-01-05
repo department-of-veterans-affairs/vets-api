dc3b638f6da3c4b9ce0b20ba058b6c7010dc3875
Kenny Santiago <50601724+ksantiagoBAH@users.noreply.github.com>
2024-01-05 10:21:18 -0600

    Updated erb

--- modules/simple_forms_api/app/models/simple_forms_api/vba_40_10007.rb
+++ modules/simple_forms_api/app/models/simple_forms_api/vba_40_10007.rb
@@ -12,13 +12,13 @@ module SimpleFormsApi
 
     def metadata
       {
-        'veteranFirstName' => @data.dig('veteran', 'full_name', 'first'),
-        'veteranLastName' => @data.dig('veteran', 'full_name', 'last'),
-        'fileNumber' => @data.dig('veteran', 'va_file_number').presence || @data.dig('veteran', 'ssn'),
-        'zipCode' => @data.dig('veteran', 'address', 'postal_code'),
-        'source' => 'VA Platform Digital Forms',
-        'docType' => @data['form_number'],
-        'businessLine' => 'CMP'
+        # 'veteranFirstName' => @data.dig('veteran', 'full_name', 'first'),
+        # 'veteranLastName' => @data.dig('veteran', 'full_name', 'last'),
+        # 'fileNumber' => @data.dig('veteran', 'va_file_number').presence || @data.dig('veteran', 'ssn'),
+        # 'zipCode' => @data.dig('veteran', 'address', 'postal_code'),
+        # 'source' => 'VA Platform Digital Forms',
+        # 'docType' => @data['form_number'],
+        # 'businessLine' => 'CMP'
       }
     end
   end
