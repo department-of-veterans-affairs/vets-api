e955bccd34dba907f925c054fe6a40901f0adeb1
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-01-05 12:06:09 -0500

    Add zip code flag to metadata validator (#15030)
    * Add zip code flag to metadata validator
    
    * lint
    
    * lint
    
    * Fixes per feedback
    
    * fix typo

--- lib/simple_forms_api_submission/metadata_validator.rb
+++ lib/simple_forms_api_submission/metadata_validator.rb
@@ -2,11 +2,11 @@
 
 module SimpleFormsApiSubmission
   class MetadataValidator
-    def self.validate(metadata)
+    def self.validate(metadata, zip_code_is_us_based: true)
       validate_first_name(metadata)
         .then { |m| validate_last_name(m) }
         .then { |m| validate_file_number(m) }
-        .then { |m| validate_zip_code(m) }
+        .then { |m| validate_zip_code(m, zip_code_is_us_based) }
         .then { |m| validate_source(m) }
         .then { |m| validate_doc_type(m) }
     end
@@ -14,7 +14,7 @@ module SimpleFormsApiSubmission
     def self.validate_first_name(metadata)
       validate_presence_and_stringiness(metadata['veteranFirstName'], 'veteran first name')
       metadata['veteranFirstName'] =
-        I18n.transliterate(metadata['veteranFirstName']).gsub(/[^a-zA-Z\-\s]/, '').strip.first(50)
+        I18n.transliterate(metadata['veteranFirstName']).gsub(%r{[^a-zA-Z\-\/\s]}, '').strip.first(50)
 
       metadata
     end
@@ -22,7 +22,7 @@ module SimpleFormsApiSubmission
     def self.validate_last_name(metadata)
       validate_presence_and_stringiness(metadata['veteranLastName'], 'veteran last name')
       metadata['veteranLastName'] =
-        I18n.transliterate(metadata['veteranLastName']).gsub(/[^a-zA-Z\-\s]/, '').strip.first(50)
+        I18n.transliterate(metadata['veteranLastName']).gsub(%r{[^a-zA-Z\-\/\s]}, '').strip.first(50)
 
       metadata
     end
@@ -36,9 +36,14 @@ module SimpleFormsApiSubmission
       metadata
     end
 
-    def self.validate_zip_code(metadata)
-      validate_presence_and_stringiness(metadata['zipCode'], 'zip code')
-      metadata['zipCode'] = '00000' unless metadata['zipCode'].match?(/\A\d{5}(-\d{4})?\z/)
+    def self.validate_zip_code(metadata, zip_code_is_us_based)
+      zip_code = metadata['zipCode'].dup.gsub(/[^0-9]/, '')
+      validate_presence_and_stringiness(zip_code, 'zip code')
+
+      zip_code.insert(5, '-') if zip_code.match?(/\A[0-9]{9}\z/)
+      zip_code = '00000' unless zip_code.match?(/\A[0-9]{5}(-[0-9]{4})?\z/)
+      zip_code = '00000' unless zip_code_is_us_based
+      metadata['zipCode'] = zip_code
 
       metadata
     end
