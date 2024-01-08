31e8760177bc5cc5422e4210cc375ed74c08a983
khoa-v-nguyen <khoa.nguyen@oddball.io>
2024-01-08 16:47:57 -0700

    Refactored StaticDataController

--- modules/ask_va_api/spec/app/lib/ask_va_api/categories/retriever_spec.rb
+++ modules/ask_va_api/spec/app/lib/ask_va_api/categories/retriever_spec.rb
@@ -10,54 +10,39 @@ module AskVAApi
       let(:entity_class) { AskVAApi::Categories::Entity }
 
       describe '#call' do
+        let(:retriever) { described_class.new(user_mock_data:, entity_class:) }
+
+        before do
+          allow(Crm::StaticData).to receive(:new).and_return(static_data_service)
+          allow(static_data_service).to receive(:call).and_return(user_mock_data ? 'mock data' : parsed_data)
+          allow(File).to receive(:read).and_return(parsed_data.to_json) if user_mock_data
+          allow(ErrorHandler).to receive(:handle_service_error)
+        end
+
         context 'when using mock data' do
-          subject(:retriever) { described_class.new(user_mock_data: true, entity_class:) }
+          let(:user_mock_data) { true }
+
           it 'reads from a file and returns an array of Entity instances' do
-            expect(retriever.call).to all(be_a(Entity))
+            expect(retriever.call).to all(be_a(entity_class))
           end
         end
 
         context 'when not using mock data' do
-          subject(:retriever) { described_class.new(user_mock_data: false, entity_class:) }
-
-          before do
-            allow(Crm::StaticData).to receive(:new).and_return(static_data_service)
-            allow(static_data_service).to receive(:call).and_return(parsed_data)
-          end
+          let(:user_mock_data) { false }
 
           it 'fetches data using Crm::StaticData service and returns an array of Entity instances' do
-            expect(retriever.call).to all(be_a(Entity))
+            expect(retriever.call).to all(be_a(entity_class))
           end
         end
 
         context 'when an error occurs during data retrieval' do
-          subject(:retriever) { described_class.new(user_mock_data: false, entity_class:) }
+          let(:user_mock_data) { false }
 
-          before do
-            allow(Crm::StaticData).to receive(:new).and_return(static_data_service)
-            allow(static_data_service).to receive(:call).and_raise(StandardError)
-            allow(ErrorHandler).to receive(:handle_service_error)
-          end
+          before { allow(static_data_service).to receive(:call).and_raise(StandardError) }
 
           it 'rescues the error and calls the ErrorHandler' do
             expect { retriever.call }.not_to raise_error
             expect(ErrorHandler).to have_received(:handle_service_error).with(instance_of(StandardError))
-          end
-        end
-
-        context 'when JSON parsing fails' do
-          subject(:retriever) { described_class.new(user_mock_data: false, entity_class:) }
-
-          before do
-            allow(Crm::StaticData).to receive(:new).and_return(static_data_service)
-            allow(static_data_service).to receive(:call).and_return('invalid json')
-            allow(ErrorHandler).to receive(:handle_service_error).and_raise(ErrorHandler::ServiceError,
-                                                                            "unexpected token at 'invalid json'")
-          end
-
-          it 'rescues the JSON::ParserError and calls the ErrorHandler' do
-            expect { retriever.call }.to raise_error(ErrorHandler::ServiceError,
-                                                     "unexpected token at 'invalid json'")
           end
         end
       end
