9a3d424c5fcd960e61c9516b43d737c9c71819b9
Gregg P <117232882+GcioGregg@users.noreply.github.com>
2024-01-08 12:53:52 -0800

    increase timeout for searches (#15035)

--- spec/lib/gi/search_configuration_spec.rb
+++ spec/lib/gi/search_configuration_spec.rb
@@ -7,7 +7,7 @@ describe GI::SearchConfiguration do
   describe '.open_timeout' do
     context 'when Settings.gi.search.open_timeout is not set' do
       it 'uses the setting' do
-        expect(GI::SearchConfiguration.instance.open_timeout).to eq(2)
+        expect(GI::SearchConfiguration.instance.open_timeout).to eq(4)
       end
     end
   end
@@ -15,7 +15,7 @@ describe GI::SearchConfiguration do
   describe '.read_timeout' do
     context 'when Settings.gi.search.read_timeout is not set' do
       it 'uses the setting' do
-        expect(GI::SearchConfiguration.instance.read_timeout).to eq(2)
+        expect(GI::SearchConfiguration.instance.read_timeout).to eq(4)
       end
     end
   end
