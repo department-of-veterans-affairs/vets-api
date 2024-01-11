380d6ea90ee15d03ed2c068c2b9f9c7655618b2d
Daniel da Silva <achievedaniel@gmail.com>
2024-01-11 17:17:25 -0500

    Adding a separate list of apps to control TOU on/off for different environments

--- spec/lib/saml/post_url_service_spec.rb
+++ spec/lib/saml/post_url_service_spec.rb
@@ -617,7 +617,8 @@ RSpec.describe SAML::PostURLService do
               before { allow(Settings).to receive(:vsp_environment).and_return(env) }
 
               it 'returns the appropriate list of clients depending on the environment' do
-                expect(subject.enabled_tou_clients)
+                result = subject.send(:enabled_tou_clients)
+                expect(result)
                   .to eq(list)
               end
             end
