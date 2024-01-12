d016b95fc99aa7a7f06c3e781da46a8b9b7645d3
Jayson Perkins <jaysonperkins51@gmail.com>
2024-01-12 13:48:20 -0500

    lint fix

--- app/policies/mhv_prescriptions_policy.rb
+++ app/policies/mhv_prescriptions_policy.rb
@@ -8,13 +8,15 @@ MHVPrescriptionsPolicy = Struct.new(:user, :mhv_prescriptions) do
   def access?
     service_name = user.identity.sign_in[:service_name]
     access = RX_ACCOUNT_TYPES.include?(user.mhv_account_type) &&
-      (user.va_patient? || service_name == SignIn::Constants::Auth::MHV)
-    Rails.logger.info('RX ACCESS DENIED',
-                      account_type: user.mhv_account_type.presence || 'false',
-                      mhv_id: user.mhv_correlation_id.presence || 'false',
-                      sign_in_service: service_name,
-                      va_facilities: user.va_treatment_facility_ids.length,
-                      va_patient: user.va_patient?) unless access
+             (user.va_patient? || service_name == SignIn::Constants::Auth::MHV)
+    unless access
+      Rails.logger.info('RX ACCESS DENIED',
+                        account_type: user.mhv_account_type.presence || 'false',
+                        mhv_id: user.mhv_correlation_id.presence || 'false',
+                        sign_in_service: service_name,
+                        va_facilities: user.va_treatment_facility_ids.length,
+                        va_patient: user.va_patient?)
+    end
     access
   end
 end
