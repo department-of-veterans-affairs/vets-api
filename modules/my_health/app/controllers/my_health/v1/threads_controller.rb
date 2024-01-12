db9080ca4aedcbf29c41e918804fcb5bba5399c6
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2024-01-12 12:05:17 -0500

    [#MHV-53459] catch 400 on No messages found

--- modules/my_health/app/controllers/my_health/v1/threads_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/threads_controller.rb
@@ -4,13 +4,25 @@ module MyHealth
   module V1
     class ThreadsController < SMController
       def index
-        resource = client.get_folder_threads(
-          params[:folder_id].to_s,
-          params[:page_size],
-          params[:page_number],
-          params[:sort_field],
-          params[:sort_order]
-        )
+        begin 
+          resource = client.get_folder_threads(
+            params[:folder_id].to_s,
+            params[:page_size],
+            params[:page_number],
+            params[:sort_field],
+            params[:sort_order]
+          )
+        rescue => e
+          error = e.errors.first
+          # If there are no messages in the folder, MHV API returns a 400 error
+          # We want to return an empty array in this case
+          if error.status.to_i == 400 && error.detail == "No messages in the requested folder"
+            resource = Common::Collection.new(MessageThread, data: [])
+          else
+            raise e
+          end
+        end
+        
 
         raise Common::Exceptions::RecordNotFound, params[:folder_id] if resource.blank?
 
