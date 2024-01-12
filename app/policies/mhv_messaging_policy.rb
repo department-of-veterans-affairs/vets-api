a71315e3e0b5f9fac0d50e57cdc12a8f9990ed0c
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2024-01-12 12:04:32 -0500

    SM ONLY Premium user toggle for localhost

--- app/policies/mhv_messaging_policy.rb
+++ app/policies/mhv_messaging_policy.rb
@@ -4,6 +4,8 @@ MHVMessagingPolicy = Struct.new(:user, :mhv_messaging) do
   SM_ACCOUNT_TYPES = %w[Premium].freeze
 
   def access?
-    SM_ACCOUNT_TYPES.include?(user.mhv_account_type) && user.va_patient?
+  # SM_ACCOUNT_TYPES.include?(user.mhv_account_type) && user.va_patient? # DO NOT MERGE: FOR LOCAL  DEV PURPOSES ONLY
+    # false # BASIC User
+    true # PREMIUM User
   end
 end
