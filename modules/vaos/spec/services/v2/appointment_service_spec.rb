3b454494c154a50a6560df9beb292003abd5cf4f
Kris Pethtel <kris.pethtel@gmail.com>
2024-01-12 12:16:27 -0600

    wip

--- modules/vaos/spec/services/v2/appointment_service_spec.rb
+++ modules/vaos/spec/services/v2/appointment_service_spec.rb
@@ -298,9 +298,11 @@ describe VAOS::V2::AppointmentsService do
     describe 'schema validations' do
       it 'runs' do
         VCR.use_cassette('vaos/v2/appointments/get_appointments_200_with_facilities_200',
-          match_requests_on: %i[method path query], allow_playback_repeats: true, tag: :force_utf8) do
-          response = subject.get_appointments(start_date2, end_date2)
-          expect().to eq()
+                         match_requests_on: %i[method path query], allow_playback_repeats: true, tag: :force_utf8) do
+          expect(UpstreamSchemaValidationJob).to receive(:perform_async)
+          # this test only really works if you change the appointments service to use perform_sync
+          # expect(Rails.logger).not_to receive(:error)
+          subject.get_appointments(start_date2, end_date2)
         end
       end
     end
