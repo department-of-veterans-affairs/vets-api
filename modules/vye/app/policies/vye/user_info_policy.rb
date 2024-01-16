cbca6433533032ca8b67ff365a18a5cca1e3d269
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-01-16 15:43:36 -0600

    moved feature toggle to controller level

---
+++ modules/vye/app/policies/vye/user_info_policy.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Vye; end
+Vye::UserInfoPolicy = Struct.new(:user, :user_info) do
+  def create?
+    return true if user_info.present?
+
+    raise Pundit::NotAuthorizedError
+  end
+
+  alias_method :show?, :create?
+end
