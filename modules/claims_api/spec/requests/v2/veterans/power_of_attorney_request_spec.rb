9ca1fc1164336a405d49846ecf53db74519a352c
Jonathan VanCourt <jonathan.vancourt@adhocteam.us>
2024-01-16 16:01:34 -0500

    add 2122a json schema validation endpoint and reqest spec

--- modules/claims_api/spec/requests/v2/veterans/power_of_attorney_request_spec.rb
+++ modules/claims_api/spec/requests/v2/veterans/power_of_attorney_request_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe 'Power Of Attorney', type: :request do
   let(:appoint_organization_path) do
     "/services/claims/v2/veterans/#{veteran_id}/power-of-attorney:appoint-organization"
   end
+  let(:validate_2122a_path) { "/services/claims/v2/veterans/#{veteran_id}/2122a/validate" }
   let(:scopes) { %w[system/claim.write] }
   let(:individual_poa_code) { 'A1H' }
   let(:organization_poa_code) { '083' }
@@ -325,6 +326,131 @@ RSpec.describe 'Power Of Attorney', type: :request do
 
               expect(response.status).to eq(401)
             end
+          end
+        end
+      end
+    end
+
+    describe 'validate_2122a' do
+      context 'CCG (Client Credentials Grant) flow' do
+        context 'when provided' do
+          context 'when valid' do
+            context 'when the request data is not a valid json object' do
+              let(:data) { '123abc' }
+
+              it 'returns a meaningful 422' do
+                mock_ccg(%w[claim.write claim.read]) do |auth_header|
+                  detail = 'The request body is not a valid JSON object: '
+
+                  post validate_2122a_path, params: data, headers: auth_header
+
+                  response_body = JSON.parse(response.body)['errors'][0]
+
+                  expect(response).to have_http_status(:unprocessable_entity)
+                  expect(response_body['title']).to eq('Unprocessable entity')
+                  expect(response_body['status']).to eq('422')
+                  expect(response_body['detail']).to eq(detail)
+                end
+              end
+            end
+
+            context 'when the request data is a valid json object' do
+              context 'when the Veteran ICN is not found in MPI' do
+                it 'returns a meaningful 404' do
+                  mock_ccg(%w[claim.write claim.read]) do |auth_header|
+                    allow_any_instance_of(ClaimsApi::Veteran)
+                      .to receive(:mpi_record?).and_return(false)
+
+                    detail = "Unable to locate Veteran's ID/ICN in Master Person Index (MPI). " \
+                             'Please submit an issue at ask.va.gov or call 1-800-MyVA411 (800-698-2411) for assistance.'
+
+                    post validate_2122a_path, params: { data: { attributes: nil } }.to_json, headers: auth_header
+
+                    response_body = JSON.parse(response.body)['errors'][0]
+
+                    expect(response).to have_http_status(:not_found)
+                    expect(response_body['title']).to eq('Resource not found')
+                    expect(response_body['status']).to eq('404')
+                    expect(response_body['detail']).to eq(detail)
+                  end
+                end
+              end
+
+              context 'when the Veteran ICN is found in MPI' do
+                context 'when the request data does not pass schema validation' do
+                  let(:request_body) do
+                    Rails.root.join('modules', 'claims_api', 'spec', 'fixtures', 'v2', 'veterans',
+                                    'power_of_attorney', 'form_2122a_invalid_schema.json').read
+                  end
+
+                  it 'returns a meaningful 422' do
+                    mock_ccg(%w[claim.write claim.read]) do |auth_header|
+                      detail = 'The property /serviceOrganization did not contain the required key poaCode'
+
+                      post validate_2122a_path, params: request_body, headers: auth_header
+
+                      response_body = JSON.parse(response.body)['errors'][0]
+
+                      expect(response).to have_http_status(:unprocessable_entity)
+                      expect(response_body['title']).to eq('Unprocessable entity')
+                      expect(response_body['status']).to eq('422')
+                      expect(response_body['detail']).to eq(detail)
+                    end
+                  end
+                end
+
+                context 'when the request data passes schema validation' do
+                  context 'when no representatives have the provided POA code' do
+                    let(:request_body) do
+                      Rails.root.join('modules', 'claims_api', 'spec', 'fixtures', 'v2', 'veterans',
+                                      'power_of_attorney', 'form_2122a_invalid_poa.json').read
+                    end
+
+                    it 'returns a meaningful 404' do
+                      mock_ccg(%w[claim.write claim.read]) do |auth_header|
+                        detail = 'Could not find an Accredited Representative with code: aaa'
+
+                        post validate_2122a_path, params: request_body, headers: auth_header
+
+                        response_body = JSON.parse(response.body)['errors'][0]
+
+                        expect(response).to have_http_status(:not_found)
+                        expect(response_body['title']).to eq('Resource not found')
+                        expect(response_body['status']).to eq('404')
+                        expect(response_body['detail']).to eq(detail)
+                      end
+                    end
+                  end
+
+                  context 'when at least one representative has the provided POA code' do
+                    let(:request_body) do
+                      Rails.root.join('modules', 'claims_api', 'spec', 'fixtures', 'v2', 'veterans',
+                                      'power_of_attorney', 'form_2122a_valid.json').read
+                    end
+
+                    it 'returns a meaningful 200' do
+                      mock_ccg(%w[claim.write claim.read]) do |auth_header|
+                        post validate_2122a_path, params: request_body, headers: auth_header
+
+                        response_body = JSON.parse(response.body)['data']
+
+                        expect(response).to have_http_status(:ok)
+                        expect(response_body['type']).to eq('appointIndividualPowerOfAttorneyValidation')
+                        expect(response_body['attributes']['status']).to eq('valid')
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+
+        context 'when not valid' do
+          it 'returns a 401' do
+            post validate_2122a_path, headers: { 'Authorization' => 'Bearer HelloWorld' }
+
+            expect(response.status).to eq(401)
           end
         end
       end
