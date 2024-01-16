6aab0cdb6d437e53a7cc0adc1375f3ee1b4e528b
Richard Davis <richard.davis@agile6.com>
2024-01-16 16:13:19 -0500

    Use VA Profile v3 service to obtain NOK/EC

---
+++ lib/va_profile/profile/v3/health_benefit_bio_response.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'va_profile/response'
+require 'va_profile/models/associated_person'
+require 'va_profile/models/message'
+
+module VAProfile::Profile::V3
+  class HealthBenefitBioResponse < VAProfile::Response
+    attribute :associated_persons, Array[VAProfile::Models::AssociatedPerson]
+    attribute :messages, Array[VAProfile::Models::Message]
+
+    def initialize(status_code, data)
+      super(status_code)
+      self.associated_persons = data[:associated_persons]
+      self.messages = data[:messages]
+    end
+
+    class << self
+      def from(response)
+        associated_persons = response.body['profile']['healthBenefit']['associatedPersons']
+                              &.map { |p| VAProfile::Models::AssociatedPerson.build_from(p) }
+        messages = response.body['messages']
+                    &.map { |m| VAProfile::Models::Message.build_from(m) }
+        new(response.status, { associated_persons:, messages: })
+      end
+    end
+  end
+end
--- spec/lib/va_profile/profile/v3/service_spec.rb
+++ spec/lib/va_profile/profile/v3/service_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 require 'va_profile/profile/v3/service'
 
 describe VAProfile::Profile::V3::Service do
+  include SchemaMatchers
+
   subject { described_class.new(user) }
 
-  let(:user) { build(:user, :loa3, edipi: '1100377582') }
-
   describe '#get_military_info' do
-    include SchemaMatchers
+    let(:user) { build(:user, :loa3, edipi: '1100377582') }
 
     context 'when successful' do
       it 'returns a valid schema' do
@@ -19,6 +19,19 @@ describe VAProfile::Profile::V3::Service do
           expect(response.status).to eq(200)
           expect(response).to match_response_schema('va_profile/profile/v3/military_info_response')
         end
+      end
+    end
+  end
+
+  describe '#get_health_benefit_bio' do
+    let(:idme_uuid) { 'e444837a-e88b-4f59-87da-10d3c74c787b' }
+    let(:user) { build(:user, :loa3, idme_uuid:) }
+
+    it 'returns a valid schema' do
+      VCR.use_cassette('va_profile/profile/v3/health_benefit_bio_200') do
+        response = subject.get_health_benefit_bio
+
+        expect(response.status).to eq(200)
       end
     end
   end
