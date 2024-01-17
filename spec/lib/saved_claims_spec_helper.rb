873820da5a416b9132c15b4ddde6906d51a5f48d
Dan Lim <danlim715@gmail.com>
2024-01-17 17:52:52 -0600

    fix specs

--- spec/lib/saved_claims_spec_helper.rb
+++ spec/lib/saved_claims_spec_helper.rb
@@ -38,15 +38,15 @@ shared_examples_for 'saved_claim' do
         expect(JSON::Validator).to receive(:fully_validate).once.and_call_original
       end
 
-      # NOTE: We assume all forms have the privacyAgreementAccepted element. Obviously.
+      # NOTE: We assume all forms have the veteranFullName element. Obviously.
       it 'rejects forms with missing elements' do
         bad_form = instance.parsed_form.deep_dup
-        bad_form.delete('privacyAgreementAccepted')
+        bad_form.delete('veteranFullName')
         instance.form = bad_form.to_json
         instance.remove_instance_variable(:@parsed_form)
         expect(instance.valid?).to be(false)
         expect(instance.errors.full_messages.size).to eq(1)
-        expect(instance.errors.full_messages).to include(/privacyAgreementAccepted/)
+        expect(instance.errors.full_messages).to include(/veteranFullName/)
       end
     end
   end
