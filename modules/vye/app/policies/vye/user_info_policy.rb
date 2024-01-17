7481fbf0e0d0a63965788448555fa1d7858f6136
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-01-16 20:28:03 -0600

    nesting UserInfoPolicy inside the Vye module

--- modules/vye/app/policies/vye/user_info_policy.rb
+++ modules/vye/app/policies/vye/user_info_policy.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-module Vye; end
-Vye::UserInfoPolicy = Struct.new(:user, :user_info) do
-  def create?
-    return true if user_info.present?
+module Vye
+  UserInfoPolicy = Struct.new(:user, :user_info) do
+    def create?
+      return true if user_info.present?
 
-    raise Pundit::NotAuthorizedError
+      raise Pundit::NotAuthorizedError
+    end
+
+    alias_method :show?, :create?
   end
-
-  alias_method :show?, :create?
 end
