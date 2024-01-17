8617aeb392e065429d4a8932964b28503227fe64
jerekshoe <jerek.shoemaker@adhocteam.us>
2024-01-17 09:37:45 -0600

    Disabling rubocop line length rule

--- spec/controllers/v0/benefits_claims_controller_spec.rb
+++ spec/controllers/v0/benefits_claims_controller_spec.rb
@@ -147,7 +147,10 @@ RSpec.describe V0::BenefitsClaimsController, type: :controller do
 
     context 'when LH takes too long to respond' do
       it 'returns a status of 504' do
+        # rubocop:disable Layout/LineLength
         allow_any_instance_of(BenefitsClaims::Configuration).to receive(:post_with_params).and_raise(Faraday::TimeoutError)
+        # rubocop:enable Layout/LineLength
+
         post(:submit5103, params: { id: '60038334' })
 
         expect(response).to have_http_status(:gateway_timeout)
