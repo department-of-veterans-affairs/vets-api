928cca0bf13b40eaebd76e42e488efd4c978d412
Austin Smith <austin.smith@govcio.com>
2024-01-18 11:54:15 -0500

    Add test cases

--- spec/controllers/v0/financial_status_reports_calculations_controller_spec.rb
+++ spec/controllers/v0/financial_status_reports_calculations_controller_spec.rb
@@ -8,13 +8,144 @@ RSpec.describe DebtsApi::V0::FinancialStatusReportsCalculationsController, type:
 
   before do
     sign_in_as(user)
+    populate_monthly_income()
+  end
+
+  def populate_monthly_income
+   income_calculator = DebtsApi::V0::CalculateIncomeCalculations.new
+   calculations_controller = described_class.new
+   @monthly_income = calculations_controller.calculate_monthly_income(income_calculator, valid_form_data)
   end
 
   describe '#calculate_income' do
     it 'calculates monthly income' do
-      income_calculator = DebtsApi::V0::CalculateIncomeCalculations.new
-      calculations_controller = described_class.new
-      calculations_controller.calculate_monthly_income(income_calculator, valid_form_data)
+      expect(@monthly_income).not_to be(nil)
+    end
+
+    it 'has vets income' do
+      vets_income = @monthly_income[:vetIncome]
+      expect(vets_income).not_to be(nil)
+    end
+
+    it 'has spouse income' do 
+      sp_income = @monthly_income[:spIncome]
+      expect(sp_income).not_to be(nil)
+    end 
+
+    it 'has vets gross salary' do
+      vets_income = @monthly_income[:vetIncome]
+      gross_salary = vets_income[:grossSalary]
+      expect(gross_salary).not_to be(nil)
+    end
+
+    it 'has spouse gross salary' do
+      sp_income = @monthly_income[:spIncome]
+      gross_salary = sp_income[:grossSalary]
+      expect(gross_salary).not_to be(nil)
+    end
+
+    it 'has vets deductions' do
+      vets_income = @monthly_income[:vetIncome]
+      deductions = vets_income[:deductions]
+      expect(deductions).not_to be(nil)
+    end
+
+    it 'has vets other deductions' do
+      vets_income = @monthly_income[:vetIncome]
+      deductions = vets_income[:deductions]
+      other_deductions = deductions[:otherDeductions]
+      expect(other_deductions).not_to be(nil)
+    end
+
+    it 'has vets total deductions' do
+      vets_income = @monthly_income[:vetIncome]
+      total_deductions = vets_income[:totalDeductions]
+      expect(total_deductions).not_to be(nil)
+      expect(total_deductions).to be > 0
+    end
+
+    it 'has spouse deductions' do
+      sp_income = @monthly_income[:spIncome]
+      deductions = sp_income[:deductions]
+      expect(deductions).not_to be(nil)
+    end
+
+    it 'has spouse other deductions' do
+      sp_income = @monthly_income[:spIncome]
+      deductions = sp_income[:deductions]
+      other_deductions = deductions[:otherDeductions]
+      expect(other_deductions).not_to be(nil)
+    end
+
+    it 'has spouse total deductions' do
+      sp_income = @monthly_income[:spIncome]
+      total_deductions = sp_income[:totalDeductions]
+      expect(total_deductions).not_to be(nil)
+      expect(total_deductions).to be > 0
+    end
+
+    it 'has vets net take home pay' do
+      vets_income = @monthly_income[:vetIncome]
+      net_take_home_pay = vets_income[:netTakeHomePay]
+      expect(net_take_home_pay).not_to be(nil)
+      expect(net_take_home_pay).to be > 0
+    end
+
+    it 'has vets other income' do
+      vets_income = @monthly_income[:vetIncome]
+      other_income = vets_income[:otherIncome]
+      expect(other_income).not_to be(nil)
+    end
+
+    it 'has vets total monthly net income' do
+      vets_income = @monthly_income[:vetIncome]
+      total_monthly_net_income = vets_income[:totalMonthlyNetIncome]
+      expect(total_monthly_net_income).not_to be(nil)
+      expect(total_monthly_net_income).to be > 0
+    end
+
+    it 'has spouse net take home pay' do
+      sp_income = @monthly_income[:spIncome]
+      net_take_home_pay = sp_income[:netTakeHomePay]
+      expect(net_take_home_pay).not_to be(nil)
+      expect(net_take_home_pay).to be > 0
+    end
+
+    it 'has spouse other income' do
+      sp_income = @monthly_income[:spIncome]
+      other_income = sp_income[:otherIncome]
+      expect(other_income).not_to be(nil)
+    end
+
+    it 'has spouse total monthly net income' do
+      sp_income = @monthly_income[:spIncome]
+      total_monthly_net_income = sp_income[:totalMonthlyNetIncome]
+      expect(total_monthly_net_income).not_to be(nil)
+      expect(total_monthly_net_income).to be > 0
+    end
+
+    it 'checks if vets gross salary is populated' do
+      vets_income = @monthly_income[:vetIncome]
+      gross_salary = vets_income[:grossSalary]
+      expect(gross_salary).to be > 0
+    end
+
+    it 'checks if vets gross salary is calcualted correctly' do
+      vets_income = @monthly_income[:vetIncome]
+      gross_salary = vets_income[:grossSalary]
+      expect(gross_salary).to eq(5000.54)
+    end
+
+    it 'checks if spouse gross salary is populated' do
+      sp_income = @monthly_income[:spIncome]
+      gross_salary = sp_income[:grossSalary]
+      expect(gross_salary).to be > 0
+    end
+
+    it 'checks if vets gross salary is calcualted correctly' do
+      sp_income = @monthly_income[:spIncome]
+      gross_salary = sp_income[:grossSalary]
+      expect(gross_salary).to eq(4000.45)
     end
   end
 end
