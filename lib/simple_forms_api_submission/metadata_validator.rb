430703be003c89b96d5a7c52c5e7455b78f96935
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-01-19 09:49:03 -0500

    Fix metadata validator nil zip code bug

--- lib/simple_forms_api_submission/metadata_validator.rb
+++ lib/simple_forms_api_submission/metadata_validator.rb
@@ -37,8 +37,8 @@ module SimpleFormsApiSubmission
     end
 
     def self.validate_zip_code(metadata, zip_code_is_us_based)
-      zip_code = metadata['zipCode'].dup.gsub(/[^0-9]/, '')
       validate_presence_and_stringiness(zip_code, 'zip code')
+      zip_code = metadata['zipCode'].dup.gsub(/[^0-9]/, '')
 
       zip_code.insert(5, '-') if zip_code.match?(/\A[0-9]{9}\z/)
       zip_code = '00000' unless zip_code.match?(/\A[0-9]{5}(-[0-9]{4})?\z/)
