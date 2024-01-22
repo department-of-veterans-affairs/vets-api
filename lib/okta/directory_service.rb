ff8ecc7619534c567a72ff12ce3ac2902091c756
Steven Cumming <steven.cumming@stevencumming-cr2fcpq90d.attlocal.net>
2024-01-22 11:01:14 -0500

    remove rest-client on okta directory service

--- lib/okta/directory_service.rb
+++ lib/okta/directory_service.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'rest-client'
-
 module Okta
   class DirectoryService < Common::Client::Base
     def scopes(category)
@@ -16,9 +14,11 @@ module Okta
 
       headers = { apiKey: Settings.connected_apps_api.connected_apps.api_key }
 
-      response = RestClient::Request.execute(method: :get, url: scopes_url, headers:)
+      # modules/apps_api/app/controllers/apps_api/v0/directory_controller.rb
+      # expects to catch an error in controller
+      response = Faraday.get(scopes_url, {}, headers)
 
-      if response.code == 200
+      if response.status == 200
         begin
           JSON.parse(response.body)
 
