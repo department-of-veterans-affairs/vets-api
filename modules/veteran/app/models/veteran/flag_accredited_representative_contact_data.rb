9a22ccd1351c0fc5aafb2718f15b0c838f7ee93b
Holden Hinkle <holden@holdenhinkle.com>
2024-01-22 12:38:10 -0500

    add model validations

--- modules/veteran/app/models/veteran/flag_accredited_representative_contact_data.rb
+++ modules/veteran/app/models/veteran/flag_accredited_representative_contact_data.rb
@@ -2,5 +2,12 @@
 
 module Veteran
   class FlaggedVeteranRepresentativeContactData < ApplicationRecord
+    validates :ip_address, :representative_id, :flag_type, presence: true
+
+    enum flag_type: { email: 'email', phone: 'phone', address: 'address', other: 'other' }
+    validates :flag_type, inclusion: { in: flag_types.keys }
+
+    validates :representative_id, uniqueness: { scope: %i[flag_type flagged_value],
+                                                message: 'Combination of representative_id, flag_type, and flagged_value must be unique' } # rubocop:disable Layout/LineLength,Rails/I18nLocaleTexts
   end
 end
