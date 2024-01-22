9e7838b1d5a19585f54728770234b47abdec77ca
Micah Frazier <Micah.frazier@oddball.io>
2024-01-22 11:32:51 -0700

    pulled in master branch and added queue exhaustion exceptioning

--- app/sidekiq/vre/submit1900_job.rb
+++ app/sidekiq/vre/submit1900_job.rb
@@ -3,6 +3,20 @@
 module VRE
   class Submit1900Job
     include Sidekiq::Job
+    include SentryLogging
+
+    STATSD_KEY_PREFIX = 'worker.central_mail.submit_1900_job'
+    RETRY = 14
+
+    sidekiq_options retry: RETRY
+
+    sidekiq_retries_exhausted do |msg, _ex|
+      Rails.logger.send(
+        :error,
+        "Failed all retries on CentralMail::Submit1900Job, last error: #{msg['error_message']}"
+      )
+      StatsD.increment("#{STATSD_KEY_PREFIX}.exhausted")
+    end
 
     def perform(claim_id, user_uuid)
       claim = SavedClaim::VeteranReadinessEmploymentClaim.find claim_id
