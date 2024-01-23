b6cd6bc5d04cb1ab4fe0bebbc2b95673a61ff8ee
Holden Hinkle <holden@holdenhinkle.com>
2024-01-23 11:34:15 -0500

    fix model path, update validations

--- modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
+++ modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
@@ -2,11 +2,11 @@
 
 module Veteran
   class FlaggedVeteranRepresentativeContactData < ApplicationRecord
+    self.table_name = 'flagged_veteran_representative_contact_data'
+
+    attribute :flag_type, :flag_type
     validates :ip_address, :representative_id, :flag_type, presence: true
-
-    enum flag_type: { email: 'email', phone: 'phone', address: 'address', other: 'other' }
-    validates :flag_type, inclusion: { in: flag_types.keys }
-
+    validates :flag_type, inclusion: { in: ActiveRecord::Type.lookup(:flag_type).valid_types }
     validates :ip_address, uniqueness: { scope: %i[representative_id flag_type],
                                          message: 'Combination of representative_id, flag_type, and flagged_value must be unique' } # rubocop:disable Layout/LineLength,Rails/I18nLocaleTexts
   end
