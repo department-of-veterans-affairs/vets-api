e9af585654bda39a614e3809d1c371e6223938f7
Holden Hinkle <holden@holdenhinkle.com>
2024-01-23 11:34:46 -0500

    update model spec path, get basic tests passing

---
+++ modules/veteran/spec/models/veteran/flagged_veteran_representative_contact_data_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Veteran::FlaggedVeteranRepresentativeContactData, type: :model do
+  describe 'validations' do
+    subject do
+      described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'email',
+                          flagged_value: 'name@example.com')
+    end
+
+    context 'flag_type' do
+      it 'is valid with a valid flag_type' do
+        expect(subject).to be_valid
+      end
+
+      it 'is not valid with an invalid flag_type' do
+        subject[:flag_type] = 'invalid_type'
+        expect(subject).not_to be_valid
+        expect(subject.errors[:flag_type]).to include('is not included in the list')
+      end
+    end
+
+    context 'uniqueness' do
+      before do
+        described_class.create!(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'email',
+                                flagged_value: 'name@example.com')
+      end
+
+      it 'is not valid with a duplicate combination of ip_address, representative_id, and flag_type' do
+        duplicate = described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'email',
+                                        flagged_value: 'name@example.com')
+        expect(duplicate).not_to be_valid
+      end
+
+      it 'is valid with a unique combination of ip_address, representative_id, and flag_type' do
+        unique = described_class.new(ip_address: '192.168.1.2', representative_id: '1', flag_type: 'email',
+                                     flagged_value: 'name@example.com')
+        expect(unique).to be_valid
+      end
+    end
+  end
+end
