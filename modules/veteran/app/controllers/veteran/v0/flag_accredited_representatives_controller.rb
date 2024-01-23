0072ea0034f027079b200ba40bc7ce33e2b45dd8
Holden Hinkle <holden@holdenhinkle.com>
2024-01-23 13:20:29 -0500

    update FlagAccreditedRepresentativesController to be able to handle multiple flags per rep at once

--- modules/veteran/app/controllers/veteran/v0/flag_accredited_representatives_controller.rb
+++ modules/veteran/app/controllers/veteran/v0/flag_accredited_representatives_controller.rb
@@ -7,15 +7,23 @@ module Veteran
       skip_before_action :verify_authenticity_token
       skip_before_action :authenticate
       before_action :feature_enabled
-      before_action :flag_params
 
       def create
-        flag = FlaggedVeteranRepresentativeContactData.new(flag_params)
+        flags = params[:flags].map do |flag_data|
+          FlaggedVeteranRepresentativeContactData.new(
+            flag_data.permit(:flag_type, :flagged_value).merge(
+              ip_address: request.remote_ip,
+              representative_id: params[:representative_id]
+            )
+          )
+        end
 
-        if flag.save
-          render json: flag, status: :created
+        saved_flags = flags.select(&:save)
+
+        if saved_flags.length == flags.length
+          render json: saved_flags, status: :created
         else
-          render json: { errors: flag.errors }, status: :unprocessable_entity
+          render json: { errors: flags.map(&:errors).reject(&:empty?) }, status: :unprocessable_entity
         end
       rescue ArgumentError => e
         render json: { errors: { flag_type: [e.message] } }, status: :unprocessable_entity
@@ -25,10 +33,6 @@ module Veteran
 
       def feature_enabled
         routing_error unless Flipper.enabled?(:find_a_representative_enable_api)
-      end
-
-      def flag_params
-        params.require(:flag).permit(:ip_address, :representative_id, :flag_type, :flagged_value)
       end
     end
   end
