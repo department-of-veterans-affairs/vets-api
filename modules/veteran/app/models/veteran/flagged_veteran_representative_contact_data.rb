6e93a5439c0f2044db135969c78f73d9102bfbf5
Holden Hinkle <holden@holdenhinkle.com>
2024-01-23 12:06:33 -0500

    update model error messages

--- modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
+++ modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
@@ -6,8 +6,8 @@ module Veteran
 
     attribute :flag_type, :flag_type
     validates :ip_address, :representative_id, :flag_type, presence: true
-    validates :flag_type, inclusion: { in: ActiveRecord::Type.lookup(:flag_type).valid_types }
+    validates :flag_type, inclusion: { in: ActiveRecord::Type.lookup(:flag_type).valid_types, message: 'Invalid flag type: must be phone, email, address, or other' } # rubocop:disable Layout/LineLength,Rails/I18nLocaleTexts
     validates :ip_address, uniqueness: { scope: %i[representative_id flag_type],
-                                         message: 'Combination of representative_id, flag_type, and flagged_value must be unique' } # rubocop:disable Layout/LineLength,Rails/I18nLocaleTexts
+                                         message: 'Combination of ip_address, representative_id, and flag_type must be unique' } # rubocop:disable Layout/LineLength,Rails/I18nLocaleTexts
   end
 end
