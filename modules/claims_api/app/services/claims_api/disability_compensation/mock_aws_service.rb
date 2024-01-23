a7d8b7273bfd9d45a331b154dfdd678b9873bde9
Matt Christianson <matthew.christianson1@va.gov>
2024-01-23 11:46:30 -0500

    Rubocop & linting fixes

--- modules/claims_api/app/services/claims_api/disability_compensation/mock_aws_service.rb
+++ modules/claims_api/app/services/claims_api/disability_compensation/mock_aws_service.rb
@@ -3,15 +3,14 @@
 module ClaimsApi
   module V2
     class MockAwsService
-
       def sleep_interval
         Settings.claims_api.mock_aws_service.sleep_interval
       end
 
-
       def store(file_data)
         sleep(sleep_interval)
-        ClaimsApi::Logger.log("526 Skipping upload to s3 via claims_load_testing flipper for filename=#{file_data.original_filename}")
+        ClaimsApi::Logger.log("526 Skipping upload to s3 via claims_load_testing flipper " \
+                              "for filename=#{file_data.original_filename}")
       end
     end
   end
