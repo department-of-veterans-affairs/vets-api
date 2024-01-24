7b796d37e91b4db750b5624caa3cf5287c4facd5
Austin Smith <austin.smith@govcio.com>
2024-01-24 16:50:42 -0500

    Add missing service tag to financial status reports calculations controller

--- modules/debts_api/app/controllers/debts_api/v0/financial_status_reports_calculations_controller.rb
+++ modules/debts_api/app/controllers/debts_api/v0/financial_status_reports_calculations_controller.rb
@@ -6,6 +6,8 @@ require 'debts_api/v0/fsr_form_transform/expense_calculator'
 module DebtsApi
   module V0
     class FinancialStatusReportsCalculationsController < ApplicationController
+      service_tag 'financial-report'
+
       def monthly_income
         render json: income_calculator.get_monthly_income
       end
