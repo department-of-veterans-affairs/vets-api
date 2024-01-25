41d483c7a5bec73c9d34112702ef6980a51dc72c
Holden Hinkle <holden@holdenhinkle.com>
2024-01-25 11:31:09 -0500

    add tests for ip_address to model

--- modules/veteran/spec/models/veteran/flagged_veteran_representative_contact_data_spec.rb
+++ modules/veteran/spec/models/veteran/flagged_veteran_representative_contact_data_spec.rb
@@ -4,6 +4,36 @@ require 'rails_helper'
 
 RSpec.describe Veteran::FlaggedVeteranRepresentativeContactData, type: :model do
   describe 'validations' do
+    context 'ip_address' do
+      it 'is valid when ip_address is present' do
+        flag = described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'phone',
+                                   flagged_value: '111-222-3333')
+        expect(flag).to be_valid
+      end
+
+      it 'is not valid when ip_address is missing' do
+        flag = described_class.new(ip_address: nil, representative_id: '1', flag_type: 'phone',
+                                   flagged_value: '111-222-3333')
+        expect(flag).not_to be_valid
+        expect(flag.errors[:ip_address]).to include("can't be blank")
+      end
+    end
+
+    context 'representative_id' do
+      it 'is valid when representative_id is present' do
+        flag = described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'phone',
+                                   flagged_value: '111-222-3333')
+        expect(flag).to be_valid
+      end
+
+      it 'is not valid when representative_id is missing' do
+        flag = described_class.new(ip_address: '192.168.1.1', representative_id: nil, flag_type: 'phone',
+                                   flagged_value: '111-222-3333')
+        expect(flag).not_to be_valid
+        expect(flag.errors[:representative_id]).to include("can't be blank")
+      end
+    end
+
     context 'flag_type' do
       it 'is valid with a valid flag_type' do
         %w[phone email address other].each do |flag_type|
@@ -18,21 +48,6 @@ RSpec.describe Veteran::FlaggedVeteranRepresentativeContactData, type: :model do
                                    flagged_value: 'invalid_type value')
         expect(flag).not_to be_valid
         expect(flag.errors[:flag_type]).to include('Invalid flag type: must be phone, email, address, or other')
-      end
-    end
-
-    context 'representative_id' do
-      it 'is valid with a valid representative_id' do
-        flag = described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'phone',
-                                   flagged_value: '111-222-3333')
-        expect(flag).to be_valid
-      end
-
-      it 'is not valid with an invalid representative_id' do
-        flag = described_class.new(ip_address: '192.168.1.1', representative_id: nil, flag_type: 'phone',
-                                   flagged_value: '111-222-3333')
-        expect(flag).not_to be_valid
-        expect(flag.errors[:representative_id]).to include("can't be blank")
       end
     end
 
