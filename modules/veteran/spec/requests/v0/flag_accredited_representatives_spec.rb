55607acbf9ed69caedb67cfe9e0edb2dd61f4d24
Holden Hinkle <holden@holdenhinkle.com>
2024-01-25 09:40:50 -0500

    fix request specs

--- modules/veteran/spec/requests/v0/flag_accredited_representatives_spec.rb
+++ modules/veteran/spec/requests/v0/flag_accredited_representatives_spec.rb
@@ -8,31 +8,33 @@ RSpec.describe 'FlagAccreditedRepresentativesController', type: :request do
 
     let(:valid_attributes) do
       {
-        ip_address: '192.168.1.1',
         representative_id: '1',
-        flag_type: 'email',
-        flagged_value: 'example@email.com'
+        flags: [
+          flag_type: 'email',
+          flagged_value: 'example@email.com'
+        ]
       }
     end
 
     let(:invalid_attributes) do
       {
-        ip_address: nil,
         representative_id: nil,
-        flag_type: 'invalid_type',
-        flagged_value: 'example@email.com'
+        flags: [
+          flag_type: 'invalid_type',
+          flagged_value: 'example@email.com'
+        ]
       }
     end
 
     context 'with valid parameters' do
       it 'creates a new FlaggedVeteranRepresentativeContactData' do
         expect do
-          post path, params: { flag: valid_attributes }
+          post path, params: valid_attributes
         end.to change(Veteran::FlaggedVeteranRepresentativeContactData, :count).by(1)
       end
 
       it 'returns a created status' do
-        post path, params: { flag: valid_attributes }
+        post path, params: valid_attributes
         expect(response).to have_http_status(:created)
       end
     end
@@ -40,12 +42,12 @@ RSpec.describe 'FlagAccreditedRepresentativesController', type: :request do
     context 'with invalid parameters' do
       it 'does not create a new FlaggedVeteranRepresentativeContactData' do
         expect do
-          post path, params: { flag: invalid_attributes }
+          post path, params: invalid_attributes
         end.not_to change(Veteran::FlaggedVeteranRepresentativeContactData, :count)
       end
 
       it 'returns an unprocessable entity status' do
-        post path, params: { flag: invalid_attributes }
+        post path, params: invalid_attributes
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
