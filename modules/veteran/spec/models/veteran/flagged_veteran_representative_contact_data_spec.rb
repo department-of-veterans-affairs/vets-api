994d18b23b859e3c0d4523828500331bb117bc7c
Holden Hinkle <holden@holdenhinkle.com>
2024-01-25 11:21:45 -0500

    finish writing model tests

--- modules/veteran/spec/models/veteran/flagged_veteran_representative_contact_data_spec.rb
+++ modules/veteran/spec/models/veteran/flagged_veteran_representative_contact_data_spec.rb
@@ -4,20 +4,35 @@ require 'rails_helper'
 
 RSpec.describe Veteran::FlaggedVeteranRepresentativeContactData, type: :model do
   describe 'validations' do
-    subject do
-      described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'email',
-                          flagged_value: 'name@example.com')
-    end
-
     context 'flag_type' do
       it 'is valid with a valid flag_type' do
-        expect(subject).to be_valid
+        %w[phone email address other].each do |flag_type|
+          flag = described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type:,
+                                     flagged_value: "#{flag_type} value")
+          expect(flag).to be_valid
+        end
       end
 
       it 'is not valid with an invalid flag_type' do
-        subject[:flag_type] = 'invalid_type'
-        expect(subject).not_to be_valid
-        expect(subject.errors[:flag_type]).to include('Invalid flag type: must be phone, email, address, or other')
+        flag = described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'invalid_type',
+                                   flagged_value: 'invalid_type value')
+        expect(flag).not_to be_valid
+        expect(flag.errors[:flag_type]).to include('Invalid flag type: must be phone, email, address, or other')
+      end
+    end
+
+    context 'representative_id' do
+      it 'is valid with a valid representative_id' do
+        flag = described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'phone',
+                                   flagged_value: '111-222-3333')
+        expect(flag).to be_valid
+      end
+
+      it 'is not valid with an invalid representative_id' do
+        flag = described_class.new(ip_address: '192.168.1.1', representative_id: nil, flag_type: 'phone',
+                                   flagged_value: '111-222-3333')
+        expect(flag).not_to be_valid
+        expect(flag.errors[:representative_id]).to include("can't be blank")
       end
     end
 
@@ -27,14 +42,26 @@ RSpec.describe Veteran::FlaggedVeteranRepresentativeContactData, type: :model do
                                 flagged_value: 'name@example.com')
       end
 
-      it 'is not valid with a duplicate combination of ip_address, representative_id, and flag_type' do
+      it 'is invalid when duplicating ip_address, representative_id, and flag_type of an existing record' do
         duplicate = described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'email',
                                         flagged_value: 'name@example.com')
         expect(duplicate).not_to be_valid
       end
 
-      it 'is valid with a unique combination of ip_address, representative_id, and flag_type' do
+      it 'is valid when changing only the ip_address while keeping representative_id and flag_type same' do
         unique = described_class.new(ip_address: '192.168.1.2', representative_id: '1', flag_type: 'email',
+                                     flagged_value: 'name@example.com')
+        expect(unique).to be_valid
+      end
+
+      it 'is valid when changing only the representative_id while keeping ip_address and flag_type same' do
+        unique = described_class.new(ip_address: '192.168.1.1', representative_id: '2', flag_type: 'email',
+                                     flagged_value: 'name@example.com')
+        expect(unique).to be_valid
+      end
+
+      it 'is valid when changing only the flag_type while keeping ip_address and representative_id same' do
+        unique = described_class.new(ip_address: '192.168.1.1', representative_id: '1', flag_type: 'phone',
                                      flagged_value: 'name@example.com')
         expect(unique).to be_valid
       end
