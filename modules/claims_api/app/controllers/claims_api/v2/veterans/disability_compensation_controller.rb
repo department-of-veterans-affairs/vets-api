89381016fb07246d9fdbcda0053e3fbeaa048a19
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-01-25 16:50:57 -0600

    Cleans up one last uneeded change from testing

--- modules/claims_api/app/controllers/claims_api/v2/veterans/disability_compensation_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v2/veterans/disability_compensation_controller.rb
@@ -18,8 +18,7 @@ module ClaimsApi
         include ClaimsApi::V2::DisabilityCompensationValidation
         include ClaimsApi::V2::Error::LighthouseErrorHandler
         include ClaimsApi::V2::JsonFormatValidation
-
-        EVSS_DOCUMENT_TYPE = 'L023'
+        
         FORM_NUMBER = '526'
 
         skip_before_action :validate_json_format, only: [:attachments]
