73a3f7fce6cdd119e8df21243749da9c319f17fb
Holden Hinkle <holden@holdenhinkle.com>
2024-01-29 12:19:36 -0500

    use enum for flag_type

--- modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
+++ modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
@@ -4,9 +4,8 @@ module Veteran
   class FlaggedVeteranRepresentativeContactData < ApplicationRecord
     self.table_name = 'flagged_veteran_representative_contact_data'
 
-    attribute :flag_type, :flag_type
+    enum flag_type: { phone: 'phone', email: 'email', address: 'address', other: 'other' }, _suffix: true
     validates :ip_address, :representative_id, :flag_type, :flagged_value, presence: true
-    validates :flag_type, inclusion: { in: ActiveRecord::Type.lookup(:flag_type).valid_types, message: 'Invalid flag type: must be phone, email, address, or other' } # rubocop:disable Layout/LineLength,Rails/I18nLocaleTexts
     validates :ip_address, uniqueness: { scope: %i[representative_id flag_type],
                                          message: 'Combination of ip_address, representative_id, and flag_type must be unique' } # rubocop:disable Layout/LineLength,Rails/I18nLocaleTexts
   end
