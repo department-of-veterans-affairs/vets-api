cc2cde9268bdb2a9416ef33f69952f3e41ff880b
Holden Hinkle <holden@holdenhinkle.com>
2024-01-29 11:37:02 -0500

    don't include calls to render in the transaction

--- modules/veteran/app/controllers/veteran/v0/flag_accredited_representatives_controller.rb
+++ modules/veteran/app/controllers/veteran/v0/flag_accredited_representatives_controller.rb
@@ -8,15 +8,10 @@ module Veteran
       before_action :feature_enabled
 
       def create
-        FlaggedVeteranRepresentativeContactData.transaction do
-          flags = params[:flags].map do |flag_data|
-            FlaggedVeteranRepresentativeContactData.new(
-              flag_data.permit(:flag_type, :flagged_value).merge(
-                ip_address: request.remote_ip,
-                representative_id: params[:representative_id]
-              )
-            )
-          end
+        flags = nil
+
+        begin
+          flags = create_flags
 
           if flags.all?(&:valid?)
             flags.each(&:save!)
@@ -26,12 +21,25 @@ module Veteran
           end
         rescue ActiveRecord::Rollback
           render json: { errors: flags.map(&:errors).reject(&:empty?) }, status: :unprocessable_entity
+        rescue ArgumentError => e
+          render json: { errors: { flag_type: [e.message] } }, status: :unprocessable_entity
         end
-      rescue ArgumentError => e
-        render json: { errors: { flag_type: [e.message] } }, status: :unprocessable_entity
       end
 
       private
+
+      def create_flags
+        FlaggedVeteranRepresentativeContactData.transaction do
+          params[:flags].map do |flag_data|
+            FlaggedVeteranRepresentativeContactData.new(
+              flag_data.permit(:flag_type, :flagged_value).merge(
+                ip_address: request.remote_ip,
+                representative_id: params[:representative_id]
+              )
+            )
+          end
+        end
+      end
 
       def feature_enabled
         routing_error unless Flipper.enabled?(:find_a_representative_enable_api)
