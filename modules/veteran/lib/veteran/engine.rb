deb7b9c7735696a834d1486f6a22cbd251bc0d47
Holden Hinkle <holden@holdenhinkle.com>
2024-01-29 14:58:02 -0500

    remove configin in veteran engine file

--- modules/veteran/lib/veteran/engine.rb
+++ modules/veteran/lib/veteran/engine.rb
@@ -4,8 +4,6 @@ module Veteran
   class Engine < ::Rails::Engine
     isolate_namespace Veteran
 
-    config.autoload_paths += Dir[config.root.join('app', 'types', '**', '*.rb')]
-
     initializer :append_migrations do |app|
       unless app.root.to_s.match? root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
