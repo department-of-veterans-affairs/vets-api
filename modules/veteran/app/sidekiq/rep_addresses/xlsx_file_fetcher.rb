94c48392d4045889006619acc7a49206909eed1a
Holden Hinkle <holden@holdenhinkle.com>
2024-01-30 11:04:40 -0500

    clean up #fetch_file_content and update specs

--- modules/veteran/app/sidekiq/rep_addresses/xlsx_file_fetcher.rb
+++ modules/veteran/app/sidekiq/rep_addresses/xlsx_file_fetcher.rb
@@ -22,7 +22,7 @@ module RepAddresses
 
       return nil unless file_recently_updated?
 
-      fetch_file_content(file_info)
+      fetch_file_content(file_info.download_url)
     rescue => e
       log_error("Error fetching XLSX file: #{e.message}")
       nil
@@ -60,21 +60,10 @@ module RepAddresses
       false
     end
 
-    # Fetches the content of the XLSX file.
-    # @param file_info [Sawyer::Resource] The file information resource from GitHub.
-    # @return [String] The decoded content of the XLSX file.
-    def fetch_file_content(file_info)
-      if file_info.content.empty?
-        download_file_content(file_info.download_url)
-      else
-        Base64.decode64(file_info.content)
-      end
-    end
-
-    # Downloads the file content from a given URL.
-    # @param url [String] The URL to download the file content from.
-    # @return [String] The body of the HTTP response, or nil if not successful.
-    def download_file_content(url)
+    # # Downloads the file content from a given URL.
+    # # @param url [String] The URL to download the file content from.
+    # # @return [String] The body of the HTTP response, or nil if not successful.
+    def fetch_file_content(url)
       uri = URI.parse(url)
       response = Net::HTTP.get_response(uri)
       response.body if response.is_a?(Net::HTTPSuccess)
