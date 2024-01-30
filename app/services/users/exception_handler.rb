66884f4a572fc87b6db2a7797906e833681f6d33
Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>
2024-01-30 14:37:33 -0500

    eMIS code deletion for veteran_status

--- app/services/users/exception_handler.rb
+++ app/services/users/exception_handler.rb
@@ -11,7 +11,7 @@ module Users
     attr_reader :error, :service
 
     # @param error [ErrorClass] An external service error
-    # @param service [String] The name of the external service (i.e. 'Vet360', 'MVI', 'EMIS')
+    # @param service [String] The name of the external service (i.e. 'Vet360', 'MVI', 'VA Profile')
     #
     def initialize(error, service)
       @error = validate!(error)
@@ -19,7 +19,7 @@ module Users
     end
 
     # Serializes the initialized error into one of the predetermined error types.
-    # Uses error classes that can be triggered by MVI, EMIS, or Vet360.
+    # Uses error classes that can be triggered by MVI, VA Profile, or Vet360 (aka VA Profile).
     #
     # The serialized error format is modelled after the Maintenance Windows schema,
     # per the FE's request.
--- config/initializers/breakers.rb
+++ config/initializers/breakers.rb
@@ -8,7 +8,6 @@ require 'debt_management_center/debts_configuration'
 require 'decision_review/configuration'
 require 'emis/military_information_configuration'
 require 'emis/payment_configuration'
-require 'emis/veteran_status_configuration'
 require 'evss/claims_service'
 require 'evss/common_service'
 require 'evss/dependents/configuration'
@@ -50,7 +49,6 @@ Rails.application.reloader.to_prepare do
     BB::Configuration.instance.breakers_service,
     EMIS::MilitaryInformationConfiguration.instance.breakers_service,
     EMIS::PaymentConfiguration.instance.breakers_service,
-    EMIS::VeteranStatusConfiguration.instance.breakers_service,
     EVSS::ClaimsService.breakers_service,
     EVSS::CommonService.breakers_service,
     EVSS::DisabilityCompensationForm::Configuration.instance.breakers_service,
