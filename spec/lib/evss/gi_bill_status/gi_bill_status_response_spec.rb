c2b65a2641e99539562e4130ecd791be54c4778a
Adam King <adam.king0126@gmail.com>
2024-02-02 17:50:48 -0600

    Feature flag cleanup: gibill_status_response_overwrite_inspect_method (#15369)
    * Feature flag cleanup: gibill_status_response_overwrite_inspect_method

--- spec/lib/evss/gi_bill_status/gi_bill_status_response_spec.rb
+++ spec/lib/evss/gi_bill_status/gi_bill_status_response_spec.rb
@@ -4,32 +4,11 @@ require 'rails_helper'
 
 describe EVSS::GiBillStatus::GiBillStatusResponse do
   describe '#inspect' do
-    context 'when the feature flag is on' do
-      before do
-        allow(Flipper).to receive(:enabled?).with(:gibill_status_response_overwrite_inspect_method,
-                                                  nil).and_return(true)
-      end
+    it 'does not include @response=' do
+      instance = EVSS::GiBillStatus::GiBillStatusResponse.new('200')
+      inspect_output = instance.inspect
 
-      it 'does not include @response=' do
-        instance = EVSS::GiBillStatus::GiBillStatusResponse.new('200')
-        inspect_output = instance.inspect
-
-        expect(inspect_output).not_to include('@response=')
-      end
-    end
-
-    context 'when the feature flag is off' do
-      before do
-        allow(Flipper).to receive(:enabled?).with(:gibill_status_response_overwrite_inspect_method,
-                                                  nil).and_return(false)
-      end
-
-      it 'includes @response=' do
-        instance = EVSS::GiBillStatus::GiBillStatusResponse.new('200')
-        inspect_output = instance.inspect
-
-        expect(inspect_output).to include('@response=')
-      end
+      expect(inspect_output).not_to include('@response=')
     end
   end
 end
