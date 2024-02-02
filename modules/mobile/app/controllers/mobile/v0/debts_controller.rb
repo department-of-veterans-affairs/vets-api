47cbc0962f7eb61ecc8320c2faf127a1e47a8007
Andrew Herzberg <andrew.herzberg@adhocteam.us>
2024-02-01 17:10:54 -0700

    add debts show endpoint

--- modules/mobile/app/controllers/mobile/v0/debts_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/debts_controller.rb
@@ -10,7 +10,13 @@ module Mobile
       def index
         response = service.get_debts
 
-        render json: Mobile::V0::DebtSerializer.new(@current_user.uuid, response)
+        render json: Mobile::V0::DebtsSerializer.new(@current_user.uuid, response)
+      end
+
+      def show
+        response = service.get_debt_by_id(params[:id])
+
+        render json: Mobile::V0::DebtSerializer.new(response)
       end
 
       private
--- modules/mobile/app/serializers/mobile/v0/debt_serializer.rb
+++ modules/mobile/app/serializers/mobile/v0/debt_serializer.rb
@@ -5,17 +5,49 @@ module Mobile
     class DebtSerializer
       include JSONAPI::Serializer
 
-      set_type :debts
+      set_type :debt
 
-      attributes :has_dependent_debts,
-                 :debts
-      def initialize(user_id, debt_info)
-        resource = DebtsStruct.new(id: user_id,
-                                   has_dependent_debts: debt_info[:has_dependent_debts],
-                                   debts: debt_info[:debts])
+      attributes :fileNumber,
+                 :payeeNumber,
+                 :personEntitled,
+                 :deductionCode,
+                 :benefitType,
+                 :diaryCode,
+                 :diaryCodeDescription,
+                 :amountOverpaid,
+                 :amountWithheld,
+                 :originalAR,
+                 :currentAR,
+                 :debtHistory
+      def initialize(debt_info)
+        resource = DebtStruct.new(id: debt_info['id'],
+                                  fileNumber: debt_info['fileNumber'],
+                                  payeeNumber: debt_info['payeeNumber'],
+                                  personEntitled: debt_info['personEntitled'],
+                                  deductionCode: debt_info['deductionCode'],
+                                  benefitType: debt_info['benefitType'],
+                                  diaryCode: debt_info['diaryCode'],
+                                  diaryCodeDescription: debt_info['diaryCodeDescription'],
+                                  amountOverpaid: debt_info['amountOverpaid'],
+                                  amountWithheld: debt_info['amountWithheld'],
+                                  originalAR: debt_info['originalAR'],
+                                  currentAR: debt_info['currentAR'],
+                                  debtHistory: debt_info['debtHistory'])
         super(resource)
       end
     end
-    DebtsStruct = Struct.new(:id, :has_dependent_debts, :debts)
+    DebtStruct = Struct.new(:id,
+                            :fileNumber,
+                            :payeeNumber,
+                            :personEntitled,
+                            :deductionCode,
+                            :benefitType,
+                            :diaryCode,
+                            :diaryCodeDescription,
+                            :amountOverpaid,
+                            :amountWithheld,
+                            :originalAR,
+                            :currentAR,
+                            :debtHistory)
   end
 end
