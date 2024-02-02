8cace83833d5ec84c910529ca7cf84bcfe2a1e7b
Adam King <adam.king0126@gmail.com>
2024-02-02 12:32:22 -0600

    lint

--- spec/lib/evss/gi_bill_status/gi_bill_status_response_spec.rb
+++ spec/lib/evss/gi_bill_status/gi_bill_status_response_spec.rb
@@ -4,11 +4,11 @@ require 'rails_helper'
 
 describe EVSS::GiBillStatus::GiBillStatusResponse do
   describe '#inspect' do
-      it 'does not include @response=' do
-        instance = EVSS::GiBillStatus::GiBillStatusResponse.new('200')
-        inspect_output = instance.inspect
+    it 'does not include @response=' do
+      instance = EVSS::GiBillStatus::GiBillStatusResponse.new('200')
+      inspect_output = instance.inspect
 
-        expect(inspect_output).not_to include('@response=')
-      end
+      expect(inspect_output).not_to include('@response=')
+    end
   end
 end
