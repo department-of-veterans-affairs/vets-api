1dff9e63fb9759d80f258bc94f8d4e421d3a6973
Tommasina Miller <144388524+tommasina-va@users.noreply.github.com>
2024-02-06 13:21:16 -0600

    [DBEX-TREX] 60247 Migrate separation locations from EVSS to LH (#15302)

---
+++ lib/disability_compensation/providers/brd/lighthouse_brd_provider.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'disability_compensation/providers/brd/brd_provider'
+require 'disability_compensation/responses/intake_sites_response'
+require 'lighthouse/benefits_reference_data/service'
+
+class LighthouseBRDProvider
+  include BRDProvider
+
+  def initialize(_current_user)
+    @service = BenefitsReferenceData::Service.new
+  end
+
+  def get_separation_locations
+    response = @service.get_data(path: 'intake-sites')
+    if response.status != 200
+      return DisabilityCompensation::ApiProvider::IntakeSitesResponse.new(status: response.status)
+    end
+
+    transform(response.body)
+  end
+
+  private
+
+  def transform(data)
+    separation_locations = data['items'].map do |intake_site|
+      DisabilityCompensation::ApiProvider::SeparationLocation.new(
+        code: intake_site['id'],
+        description: intake_site['description']
+      )
+    end
+
+    DisabilityCompensation::ApiProvider::IntakeSitesResponse.new(separation_locations:, status: 200)
+  end
+end
