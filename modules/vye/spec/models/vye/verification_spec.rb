37b03fd1d7e3ebfb1587b97755688b52ac0da4d1
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-02-07 11:26:33 -0600

    GenDigest cleanup

--- modules/vye/spec/models/vye/verification_spec.rb
+++ modules/vye/spec/models/vye/verification_spec.rb
@@ -5,24 +5,6 @@ require 'rails_helper'
 RSpec.describe Vye::Verification, type: :model do
   let(:user_info) { create(:vye_user_info) }
 
-  before do
-    s =
-      Struct.new(:settings, :scrypt_config) do
-        include Vye::GenDigest
-        settings =
-          Config.load_files(
-            Rails.root / 'config/settings.yml',
-            Vye::Engine.root / 'config/settings/test.yml'
-          )
-        scrypt_config = extract_scrypt_config settings
-        new(settings, scrypt_config)
-      end
-
-    allow_any_instance_of(Vye::GenDigest::Common)
-      .to receive(:scrypt_config)
-      .and_return(s.scrypt_config)
-  end
-
   describe 'create' do
     let(:attributes) { FactoryBot.attributes_for(:vye_verification, user_info:) }
 
