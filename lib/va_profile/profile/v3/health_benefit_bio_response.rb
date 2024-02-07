60f84f3b0e81fa99f06283a61ed7a3b469fe4feb
Richard Davis <richard.davis@agile6.com>
2024-02-07 17:28:39 -0500

    save point

--- lib/va_profile/profile/v3/health_benefit_bio_response.rb
+++ lib/va_profile/profile/v3/health_benefit_bio_response.rb
@@ -6,26 +6,17 @@ require 'va_profile/models/message'
 
 module VAProfile::Profile::V3
   class HealthBenefitBioResponse < VAProfile::Response
+    attribute :body, Hash
     attribute :contacts, Array[VAProfile::Models::AssociatedPerson]
     attribute :messages, Array[VAProfile::Models::Message]
 
-    def initialize(status, data)
-      super(status)
-      self.contacts = data[:contacts]
-      self.messages = data[:messages]
-    end
-
-    class << self
-      def from(response)
-        contacts = response
-                   .body
-                   .dig('profile', 'healthBenefit', 'associatedPersons')
-                   &.map { |p| VAProfile::Models::AssociatedPerson.build_from(p) }
-        messages = response
-                   .body['messages']
-                   &.map { |m| VAProfile::Models::Message.build_from(m) }
-        new(response.status, { contacts:, messages: })
-      end
+    def initialize(response)
+      attributes = {
+        body: response.body,
+        contacts: response.body.dig('profile', 'health_benefit', 'associated_persons'),
+        messages: response.body['messages']
+      }
+      super(response.status, attributes)
     end
   end
 end
