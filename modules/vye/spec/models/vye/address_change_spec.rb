ab52a23fd614570b173bcf03a391086de37a688a
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-02-07 13:20:30 -0600

    Verification update (#15440)
    * formatting updates
    
    * added no change report
    
    * made Award#cur_award_ind, and UserInfo.indicator into enum fields
    
    * verifications controller only accepts a blank post body now
    
    * GenDigest cleanup
    
    * added source_ind validation

--- modules/vye/spec/models/vye/address_change_spec.rb
+++ modules/vye/spec/models/vye/address_change_spec.rb
@@ -6,24 +6,6 @@ RSpec.describe Vye::AddressChange, type: :model do
   describe 'create' do
     let(:user_info) { FactoryBot.create(:vye_user_info) }
 
-    before do
-      s =
-        Struct.new(:settings, :scrypt_config) do
-          include Vye::GenDigest
-          settings =
-            Config.load_files(
-              Rails.root / 'config/settings.yml',
-              Vye::Engine.root / 'config/settings/test.yml'
-            )
-          scrypt_config = extract_scrypt_config settings
-          new(settings, scrypt_config)
-        end
-
-      allow_any_instance_of(Vye::GenDigest::Common)
-        .to receive(:scrypt_config)
-        .and_return(s.scrypt_config)
-    end
-
     it 'creates a record' do
       expect do
         attributes = FactoryBot.attributes_for(:vye_address_change)
