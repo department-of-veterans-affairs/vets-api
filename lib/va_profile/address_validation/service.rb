bab2dd54b58ee96758e44cb2bbf534c860cf9a9d
Gia Antoniades <gia.antoniades@adhocteam.us>
2024-02-09 12:06:21 -0800

    Addresses CI linting error: Useless assignment to variable - res

--- lib/va_profile/address_validation/service.rb
+++ lib/va_profile/address_validation/service.rb
@@ -34,7 +34,7 @@ module VAProfile
       def candidate(address)
         begin
           with_monitoring do
-            res = perform(
+            perform(
               :post,
               'candidate',
               address.address_validation_req.to_json
