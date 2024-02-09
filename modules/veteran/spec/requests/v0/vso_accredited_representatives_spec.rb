f1d70c212bca5a14b39e110a3a712d76ce8b70b4
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-02-09 13:07:16 -0800

    Fixed feature toggle reference. find_a_rep => find_a_representative_enabled (#15482)

--- modules/veteran/spec/requests/v0/vso_accredited_representatives_spec.rb
+++ modules/veteran/spec/requests/v0/vso_accredited_representatives_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'VSOAccreditedRepresentativesController', type: :request do
     let(:long) { -77.0369 }
 
     before do
-      Flipper.enable(:find_a_rep)
+      Flipper.enable(:find_a_representative_enabled)
       # Create representatives
       create(:representative, representative_id: '111', poa_codes: %w[A12 A13], user_types: ['veteran_service_officer'],
                               long: -77.050552, lat: 38.820450, location: 'POINT(-77.050552 38.820450)',
