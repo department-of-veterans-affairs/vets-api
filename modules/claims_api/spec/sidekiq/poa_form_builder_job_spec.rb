b5a5551a1075694857b4c609526407f2418cf170
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-02-09 17:50:59 -0600

    Updates poa builder job for v2 version, merges master branch in

--- modules/claims_api/spec/sidekiq/poa_form_builder_job_spec.rb
+++ modules/claims_api/spec/sidekiq/poa_form_builder_job_spec.rb
@@ -6,14 +6,15 @@ require 'pdf_fill/filler'
 RSpec.describe ClaimsApi::PoaFormBuilderJob, type: :job do
   subject { described_class }
 
-  let(:power_of_attorney) { create(:power_of_attorney, :with_full_headers) }
+  let(:power_of_attorney_v1) { create(:power_of_attorney, :with_full_headers) }
+  let(:power_of_attorney_v2) { create(:power_of_attorney, :with_full_headers) }
   let(:poa_code) { 'ABC' }
   let(:bad_b64_image) { File.read('modules/claims_api/spec/fixtures/signature_b64_prefix_bad.txt') }
 
   before do
     Sidekiq::Job.clear_all
     b64_image = File.read('modules/claims_api/spec/fixtures/signature_b64.txt')
-    power_of_attorney.form_data = {
+    power_of_attorney_v1.form_data = {
       recordConcent: true,
       consentAddressChange: true,
       consentLimits: ['DRUG ABUSE', 'SICKLE CELL'],
@@ -65,7 +66,61 @@ RSpec.describe ClaimsApi::PoaFormBuilderJob, type: :job do
         }
       }
     }
-    power_of_attorney.save
+    power_of_attorney_v1.save
+
+    power_of_attorney_v2.form_data = {
+      recordConcent: true,
+      consentAddressChange: true,
+      consentLimits: ['DRUG ABUSE', 'SICKLE CELL'],
+      signatures: {
+        veteran: b64_image,
+        representative: b64_image
+      },
+      veteran: {
+        serviceBranch: 'ARMY',
+        address: {
+          addressLine1: '2719 Hyperion Ave',
+          city: 'Los Angeles',
+          state: 'CA',
+          country: 'US',
+          zipCode: '92264'
+        },
+        phone: {
+          areaCode: '555',
+          phoneNumber: '5551337'
+        }
+      },
+      claimant: {
+        firstName: 'Lillian',
+        middleInitial: 'A',
+        lastName: 'Disney',
+        email: 'lillian@disney.com',
+        relationship: 'Spouse',
+        address: {
+          addressLine1: '2688 S Camino Real',
+          city: 'Palm Springs',
+          state: 'CA',
+          country: 'US',
+          zipCode: '92264'
+        },
+        phone: {
+          areaCode: '555',
+          phoneNumber: '5551337'
+        }
+      },
+      serviceOrganization: {
+        poaCode: poa_code.to_s,
+        organizationName: 'I Help Vets LLC',
+        address: {
+          addressLine1: '2719 Hyperion Ave',
+          city: 'Los Angeles',
+          state: 'CA',
+          country: 'US',
+          zipCode: '92264'
+        }
+      }
+    }
+    power_of_attorney_v2.save
   end
 
   describe 'generating the filled and signed pdf' do
@@ -74,11 +129,18 @@ RSpec.describe ClaimsApi::PoaFormBuilderJob, type: :job do
         Veteran::Service::Representative.new(representative_id: '12345', poa_codes: [poa_code.to_s]).save!
       end
 
-      it 'generates the pdf to match example' do
+      it 'generates the pdf to match example for v1' do
         allow_any_instance_of(BGS::PersonWebService).to receive(:find_by_ssn).and_return({ file_nbr: '123456789' })
         expect(ClaimsApi::PoaPdfConstructor::Individual).to receive(:new).and_call_original
         expect_any_instance_of(ClaimsApi::PoaPdfConstructor::Individual).to receive(:construct).and_call_original
-        subject.new.perform(power_of_attorney.id)
+        subject.new.perform(power_of_attorney_v1.id)
+      end
+
+      it 'generates the pdf to match example for v2' do
+        allow_any_instance_of(BGS::PersonWebService).to receive(:find_by_ssn).and_return({ file_nbr: '123456789' })
+        expect(ClaimsApi::PoaPdfConstructor::Individual).to receive(:new).and_call_original
+        expect_any_instance_of(ClaimsApi::PoaPdfConstructor::Individual).to receive(:construct).and_call_original
+        subject.new.perform(power_of_attorney_v2.id)
       end
 
       it 'Calls the POA updater job upon successful upload to VBMS' do
@@ -94,7 +156,7 @@ RSpec.describe ClaimsApi::PoaFormBuilderJob, type: :job do
 
         expect(ClaimsApi::PoaUpdater).to receive(:perform_async)
 
-        subject.new.perform(power_of_attorney.id)
+        subject.new.perform(power_of_attorney_v1.id)
       end
     end
 
@@ -104,11 +166,18 @@ RSpec.describe ClaimsApi::PoaFormBuilderJob, type: :job do
         Veteran::Service::Organization.create(poa: 'ABC', name: 'Some org')
       end
 
-      it 'generates the pdf to match example' do
+      it 'generates the pdf to match example for v1' do
         allow_any_instance_of(BGS::PersonWebService).to receive(:find_by_ssn).and_return({ file_nbr: '123456789' })
         expect(ClaimsApi::PoaPdfConstructor::Organization).to receive(:new).and_call_original
         expect_any_instance_of(ClaimsApi::PoaPdfConstructor::Organization).to receive(:construct).and_call_original
-        subject.new.perform(power_of_attorney.id)
+        subject.new.perform(power_of_attorney_v1.id)
+      end
+
+      it 'generates the pdf to match example for v2' do
+        allow_any_instance_of(BGS::PersonWebService).to receive(:find_by_ssn).and_return({ file_nbr: '123456789' })
+        expect(ClaimsApi::PoaPdfConstructor::Organization).to receive(:new).and_call_original
+        expect_any_instance_of(ClaimsApi::PoaPdfConstructor::Organization).to receive(:construct).and_call_original
+        subject.new.perform(power_of_attorney_v2.id)
       end
 
       it 'Calls the POA updater job upon successful upload to VBMS' do
@@ -124,7 +193,7 @@ RSpec.describe ClaimsApi::PoaFormBuilderJob, type: :job do
 
         expect(ClaimsApi::PoaUpdater).to receive(:perform_async)
 
-        subject.new.perform(power_of_attorney.id)
+        subject.new.perform(power_of_attorney_v1.id)
       end
     end
 
@@ -132,7 +201,7 @@ RSpec.describe ClaimsApi::PoaFormBuilderJob, type: :job do
       before do
         Veteran::Service::Representative.new(representative_id: '67890', poa_codes: ['ABC']).save!
         Veteran::Service::Organization.create(poa: 'ABC', name: 'Some org')
-        power_of_attorney.update(form_data: power_of_attorney.form_data.deep_merge(
+        power_of_attorney_v1.update(form_data: power_of_attorney_v1.form_data.deep_merge(
           {
             signatures: {
               veteran: bad_b64_image,
@@ -145,10 +214,10 @@ RSpec.describe ClaimsApi::PoaFormBuilderJob, type: :job do
       it 'sets the status and store the error' do
         expect_any_instance_of(ClaimsApi::PoaPdfConstructor::Organization).to receive(:construct)
           .and_raise(ClaimsApi::StampSignatureError)
-        subject.new.perform(power_of_attorney.id)
-        power_of_attorney.reload
-        expect(power_of_attorney.status).to eq(ClaimsApi::PowerOfAttorney::ERRORED)
-        expect(power_of_attorney.signature_errors).not_to be_empty
+        subject.new.perform(power_of_attorney_v1.id)
+        power_of_attorney_v1.reload
+        expect(power_of_attorney_v1.status).to eq(ClaimsApi::PowerOfAttorney::ERRORED)
+        expect(power_of_attorney_v1.signature_errors).not_to be_empty
       end
     end
   end
