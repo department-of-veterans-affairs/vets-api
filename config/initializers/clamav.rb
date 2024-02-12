07109ecd4174dd3b402edfceab7002c23e237e79
Steven Cumming <steven.cumming@stevencumming-cr2fcpq90d.attlocal.net>
2024-02-12 08:47:47 -0500

    update native clamav to use hybrid approach

--- config/initializers/clamav.rb
+++ config/initializers/clamav.rb
@@ -1,1 +1,6 @@
-ENV['CLAMD_UNIX_SOCKET'] = '/opt/homebrew/opt/clamav/clamd.sock'
+# frozen_string_literal: true
+
+if Rails.env.development?
+  ENV['CLAMD_TCP_HOST'] = 'clamav'
+  ENV['CLAMD_TCP_PORT'] = '3310'
+end
--- lib/vets-api/setups/docker.rb
+++ lib/vets-api/setups/docker.rb
@@ -24,6 +24,8 @@ module VetsApi
         print 'Configuring ClamAV...'
         File.open("config/initializers/clamav.rb", "w") do |file|
           file.puts <<~CLAMD
+            # frozen_string_literal: true
+
             if Rails.env.development?
               ENV['CLAMD_TCP_HOST'] = 'clamav'
               ENV['CLAMD_TCP_PORT'] = '3310'
