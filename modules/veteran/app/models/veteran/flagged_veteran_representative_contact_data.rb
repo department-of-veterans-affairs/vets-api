8cb5fcaa6f08e36dde65e10035d8e9d16b56483d
Holden Hinkle <holden@holdenhinkle.com>
2024-02-12 12:31:28 -0500

    set flagged_value_updated_at to nil before save

--- modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
+++ modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
@@ -4,9 +4,17 @@ module Veteran
   class FlaggedVeteranRepresentativeContactData < ApplicationRecord
     self.table_name = 'flagged_veteran_representative_contact_data'
 
+    before_save :set_flagged_value_updated_at
+
     enum flag_type: { phone: 'phone', email: 'email', address: 'address', other: 'other' }, _suffix: true
     validates :ip_address, :representative_id, :flag_type, :flagged_value, presence: true
     validates :ip_address,
               uniqueness: { scope: %i[representative_id flag_type flagged_value_updated_at], message: 'Combination of ip_address, representative_id, flag_type, and flagged_value_updated_at must be unique' } # rubocop:disable Rails/I18nLocaleTexts,Layout/LineLength
+  end
+
+  private
+
+  def set_flagged_value_updated_at
+    self.flagged_value_updated_at = nil
   end
 end
