51a01eca60fc07aa4c91b40f8c35a25118f9f87a
Kevin Suarez <kjsuarez94@gmail.com>
2024-02-12 14:30:46 -0500

    74795 Add personalization params to mcp email copy (#15491)
    * add personalization params to mcp email copy
    
    * linters be linting
    
    ---------
    
    Co-authored-by: Eric Boehs <eric@boehs.com>

--- app/sidekiq/copay_notifications/new_statement_notification_job.rb
+++ app/sidekiq/copay_notifications/new_statement_notification_job.rb
@@ -32,8 +32,13 @@ module CopayNotifications
     def perform(statement)
       StatsD.increment("#{STATSD_KEY_PREFIX}.total")
       statement_service = DebtManagementCenter::StatementIdentifierService.new(statement)
-      icn = statement_service.get_icn
-      DebtManagementCenter::VANotifyEmailJob.perform_async(icn, MCP_NOTIFICATION_TEMPLATE, nil, 'icn')
+      user_data = statement_service.get_mpi_data
+      icn = user_data[:icn]
+      personalization = {
+        'name' => user_data[:first_name],
+        'date' => Time.zone.today.strftime('%B %d, %Y')
+      }
+      DebtManagementCenter::VANotifyEmailJob.perform_async(icn, MCP_NOTIFICATION_TEMPLATE, personalization, 'icn')
     end
   end
 end
