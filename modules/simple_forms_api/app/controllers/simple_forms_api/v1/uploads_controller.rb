9ea1166214b34b427d0a89d351f68451830cd26c
Don Shin <cloudmagic80@gmail.com>
2024-02-12 11:01:04 -0600

    update upload controller and vha1010d

--- modules/simple_forms_api/app/controllers/simple_forms_api/v1/uploads_controller.rb
+++ modules/simple_forms_api/app/controllers/simple_forms_api/v1/uploads_controller.rb
@@ -24,10 +24,12 @@ module SimpleFormsApi
         '20-10206' => 'vba_20_10206'
       }.freeze
 
+      UNAUTHENTICATED_FORMS = %w[40-0247 21-10210 21P-0847].freeze
+
       def submit
         Datadog::Tracing.active_trace&.set_tag('form_id', params[:form_number])
 
-        if form_is210966 && icn
+        if form_is210966 && icn && first_party?
           handle_210966_authenticated
         else
           submit_form_to_central_mail
@@ -37,7 +39,7 @@ module SimpleFormsApi
       end
 
       def submit_supporting_documents
-        if params[:form_id] == '40-0247' || params[:form_id] == '10-10D'
+          if %w[40-0247 10-10D].include?(params[:form_id])
           attachment = PersistentAttachments::MilitaryRecords.new(form_id: params[:form_id])
           attachment.file = params['file']
           raise Common::Exceptions::ValidationErrors, attachment unless attachment.valid?
@@ -47,11 +49,23 @@ module SimpleFormsApi
         end
       end
 
+      def get_intents_to_file
+        intent_service = SimpleFormsApi::IntentToFile.new(icn)
+        existing_intents = intent_service.existing_intents
+
+        render json: {
+          compensation_intent: existing_intents['compensation'],
+          pension_intent: existing_intents['pension'],
+          survivor_intent: existing_intents['survivor']
+        }
+      end
+      
       def authenticate
         super
       rescue Common::Exceptions::Unauthorized
         Rails.logger.info(
-          "Simple forms api - unauthenticated user submitting form: #{params[:form_number]}"
+          'Simple forms api - unauthenticated user submitting form',
+          { form_number: params[:form_number] }
         )
       end
 
@@ -72,16 +86,9 @@ module SimpleFormsApi
       end
 
       def submit_form_to_central_mail
-        parsed_form_data = JSON.parse(params.to_json)
         form_id = get_form_id
-        form = "SimpleFormsApi::#{form_id.titleize.gsub(' ', '')}".constantize.new(parsed_form_data)
-        filler = SimpleFormsApi::PdfFiller.new(form_number: form_id, form:)
-
-        file_path = filler.generate
-        metadata = SimpleFormsApiSubmission::MetadataValidator.validate(form.metadata)
-
-        form.handle_attachments(file_path) if form_id == 'vba_40_0247' || form_id == 'vha_10_10d'
-
+        parsed_form_data = JSON.parse(params.to_json)
+        file_path, metadata = get_file_path_and_metadata(parsed_form_data)
         status, confirmation_number = upload_pdf_to_benefits_intake(file_path, metadata)
 
         if status == 200 && Flipper.enabled?(:simple_forms_email_confirmations)
@@ -91,17 +98,35 @@ module SimpleFormsApi
         end
 
         Rails.logger.info(
-          "Simple forms api - sent to benefits intake: #{params[:form_number]},
-            status: #{status}, uuid #{confirmation_number}"
+          'Simple forms api - sent to benefits intake',
+          { form_number: params[:form_number], status:, uuid: confirmation_number }
         )
 
-        json = { confirmation_number: }
-        json[:expiration_date] = 1.year.from_now if form_id == 'vba_21_0966'
-
-        render json:, status:
+        render json: get_json(confirmation_number, form_id), status:
       end
 
-      def get_upload_location_and_uuid(lighthouse_service)
+      def get_file_path_and_metadata(parsed_form_data)
+        form_id = get_form_id
+        form = "SimpleFormsApi::#{form_id.titleize.gsub(' ', '')}".constantize.new(parsed_form_data)
+        form.track_user_identity
+        filler = SimpleFormsApi::PdfFiller.new(form_number: form_id, form:)
+
+        file_path = if @current_user
+                      filler.generate(@current_user.loa[:current])
+                    else
+                      filler.generate
+                    end
+        metadata = SimpleFormsApiSubmission::MetadataValidator.validate(form.metadata)
+
+        case form_id
+        when *%w[vba_40_0247 vha_10_10d]
+          form.handle_attachments(file_path)
+        end
+
+        [file_path, metadata]
+      end
+
+    def get_upload_location_and_uuid(lighthouse_service)
         upload_location = lighthouse_service.get_upload_location.body
         {
           uuid: upload_location.dig('data', 'id'),
@@ -122,8 +147,8 @@ module SimpleFormsApi
 
         Datadog::Tracing.active_trace&.set_tag('uuid', uuid_and_location[:uuid])
         Rails.logger.info(
-          "Simple forms api - preparing to upload PDF to benefits intake:
-            location: #{uuid_and_location[:location]}, uuid: #{uuid_and_location[:uuid]}"
+          'Simple forms api - preparing to upload PDF to benefits intake',
+          { location: uuid_and_location[:location], uuid: uuid_and_location[:uuid] }
         )
         response = lighthouse_service.upload_doc(
           upload_url: uuid_and_location[:location],
@@ -139,11 +164,15 @@ module SimpleFormsApi
       end
 
       def should_authenticate
-        params[:form_number] == '21-0966' || params[:form_number] == '21-0845'
+        true unless UNAUTHENTICATED_FORMS.include? params[:form_number]
       end
 
       def icn
         @current_user&.icn
+      end
+
+      def first_party?
+        %w[VETERAN SURVIVING_DEPENDENT].include?(params[:preparer_identification])
       end
 
       def get_form_id
@@ -151,6 +180,13 @@ module SimpleFormsApi
         raise 'missing form_number in params' unless form_number
 
         FORM_NUMBER_MAP[form_number]
+      end
+
+      def get_json(confirmation_number, form_id)
+        json = { confirmation_number: }
+        json[:expiration_date] = 1.year.from_now if form_id == 'vba_21_0966'
+
+        json
       end
     end
   end
