8e35573a5b1fdbbe9b9b27ba412727f3eb76580a
Austin Smith <austin.smith@govcio.com>
2024-02-13 16:45:35 -0500

    Set retries limit to be 14

--- app/sidekiq/evss/request_decision.rb
+++ app/sidekiq/evss/request_decision.rb
@@ -3,6 +3,13 @@
 class EVSS::RequestDecision
   include Sidekiq::Job
 
+  # retry for one day
+  sidekiq_options retry: 14, queue: 'low'
+  # Set minimum retry time to ~1 hour
+  sidekiq_retry_in do |count, _exception|
+    rand(3600..3660) if count < 9
+  end
+
   def perform(auth_headers, evss_id)
     client = EVSS::ClaimsService.new(auth_headers)
     client.request_decision(evss_id)
@@ -13,6 +20,13 @@ end
 # TODO(knkski): Remove after migration
 class EVSSClaim::RequestDecision
   include Sidekiq::Job
+
+  # retry for one day
+  sidekiq_options retry: 14, queue: 'low'
+  # Set minimum retry time to ~1 hour
+  sidekiq_retry_in do |count, _exception|
+    rand(3600..3660) if count < 9
+  end
 
   def perform(auth_headers, evss_id)
     EVSS::RequestDecision.perform_async(auth_headers, evss_id)
