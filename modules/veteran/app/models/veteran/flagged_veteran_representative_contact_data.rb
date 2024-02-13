c12f43644ef08d73d78b35d8aa1f5c42df48904d
Holden Hinkle <holden@holdenhinkle.com>
2024-02-13 14:51:27 -0500

    Revert "remove ignored columns from FlaggedVeteranRepresentativeContactData (…" (#15523)
    This reverts commit 742e42bdcf558521494a3bfbefdc5f771c54975d.

--- modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
+++ modules/veteran/app/models/veteran/flagged_veteran_representative_contact_data.rb
@@ -2,6 +2,7 @@
 
 module Veteran
   class FlaggedVeteranRepresentativeContactData < ApplicationRecord
+    self.ignored_columns += ['flagged_value_updated']
     self.table_name = 'flagged_veteran_representative_contact_data'
 
     enum flag_type: { phone: 'phone', email: 'email', address: 'address', other: 'other' }, _suffix: true
