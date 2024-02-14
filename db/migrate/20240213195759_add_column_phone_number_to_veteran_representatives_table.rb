24cf5ecbf876d0f403e6e033ceddd15d751fbb5a
Holden Hinkle <holden@holdenhinkle.com>
2024-02-14 09:42:12 -0500

    add frozen string literal comment

--- db/migrate/20240213195759_add_column_phone_number_to_veteran_representatives_table.rb
+++ db/migrate/20240213195759_add_column_phone_number_to_veteran_representatives_table.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddColumnPhoneNumberToVeteranRepresentativesTable < ActiveRecord::Migration[7.0]
   def change
     add_column :veteran_representatives, :phone_number, :string
