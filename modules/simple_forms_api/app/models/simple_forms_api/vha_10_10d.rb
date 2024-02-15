3db475d77ca262a8e52742ab97bc66be8ba57fb5
Don Shin <cloudmagic80@gmail.com>
2024-02-15 00:43:20 -0600

    modify test data

--- modules/simple_forms_api/app/models/simple_forms_api/vha_10_10d.rb
+++ modules/simple_forms_api/app/models/simple_forms_api/vha_10_10d.rb
@@ -12,10 +12,10 @@ module SimpleFormsApi
 
     def metadata
       {
-        'veteranFirstName' => @data.dig('veteran', 'full_name', 'first'),
-        'veteranLastName' => @data.dig('veteran', 'full_name', 'last'),
-        'fileNumber' => @data.dig('veteran', 'va_claim_number').presence || @data.dig('veteran', 'ssn_or_tin'),
-        'zipCode' => @data.dig('veteran', 'address', 'postal_code') || '00000',
+        'veteranFirstName' => @data['VeteransFirstName'],
+        'veteranLastName' => @data['VeteransLastName'],
+        'fileNumber' => @data['VAFileNumber'].presence || @data['VeteransSSN'],
+        'zipCode' => @data['VeteransZipCode'],
         'source' => 'VA Platform Digital Forms',
         'docType' => @data['form_number'],
         'businessLine' => 'CMP'
