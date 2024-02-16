2b6980b5bc9274fbe4424d9a6617127643e429d1
Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>
2024-02-16 11:18:46 -0500

    2nd try: Delete emis veteran status (#15393)
    * eMIS code deletion for veteran_status
    
    * remove test
    
    * change wording in comment
    
    * change VA Profile error string to Vet360
    
    * pass 'VAProfile' as the external service instead of 'Vet360'

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -176,7 +176,7 @@ RSpec.describe 'Fetching user data' do
       it 'returns meta.errors information', :aggregate_failures do
         error = body.dig('meta', 'errors').first
 
-        expect(error['external_service']).to eq 'Vet360'
+        expect(error['external_service']).to eq 'VAProfile'
         expect(error['description']).to be_present
         expect(error['status']).to eq 502
       end
