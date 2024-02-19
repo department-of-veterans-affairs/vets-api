e29ad8d05ead784127739d811150a8baf75e3871
Kevin Duensing <kevin.duensing@adhocteam.us>
2024-02-19 16:03:29 -0500

    Add blocking if power_switch disabled
    Blocks requests at a module level if the feature is disabled.

--- modules/travel_pay/app/controllers/travel_pay/application_controller.rb
+++ modules/travel_pay/app/controllers/travel_pay/application_controller.rb
@@ -8,7 +8,32 @@ module TravelPay
 
     protect_from_forgery with: :exception
 
+    before_action :authenticate
+
+    ## 
+    # This before_action is feature flag driven and should be retired
+    # 
+    # Flag name: travel_pay_power_switch
+    # Intent:    allow env-specific request blocking
+    #
+    # Retirement conditions: 
+    # * Feature is in production
+    # * Finer-grained feature flags exist as needed
+    #
+    # Upon retirement:
+    # * Remove feature flag from config/features.yml
+    # * Remove feature flag from database (requires platform support for prod)
+    # * Remove this before_action
+    # * Remove block_if_flag_disabled definition
+    # * Remove flag_conditions_met? definition
+    #
+    before_action :block_if_flag_disabled
+
     protected
+
+    def raise_access_denied
+      raise Common::Exceptions::Unauthorized, detail: 'You do not have access to the requested resource'
+    end
 
     def before_logger
       logger.info('travel-pay') { Utils::Logger.build(self).before }
@@ -18,8 +43,11 @@ module TravelPay
       logger.info('travel-pay') { Utils::Logger.build(self).after }
     end
 
-    def authorize
-      routing_error
+    # Blocks requests from being handled if feature flag is disabled
+    def block_if_flag_disabled
+      unless Flipper.enabled?(:travel_pay_power_switch, @current_user)
+        raise Common::Exceptions::ServiceUnavailable, detail: 'This feature has been temporarily disabled'
+      end
     end
   end
 end
