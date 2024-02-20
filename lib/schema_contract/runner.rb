2fa5e1dd71af40c35110dfaa10addcf855fe52db
Kris Pethtel <kris.pethtel@gmail.com>
2024-02-20 11:39:34 -0600

    wip

--- lib/schema_contract/runner.rb
+++ lib/schema_contract/runner.rb
@@ -6,10 +6,7 @@ module SchemaContract
       if response.success? && Flipper.enabled?("schema_contract_#{test_name}")
         return if SchemaContractTest.where(name: test_name, created_at: Time.zone.today.all_day).any?
 
-        record = SchemaContractTest.create(
-          name: test_name, last_user_uuid: user.uuid, last_response: response.to_json,
-          status: 'initiated'
-        )
+        record = SchemaContractTest.create(name: test_name, user_uuid: user.uuid, response: response.to_json,status: 'initiated')
 
         UpstreamSchemaValidationJob.perform_async(record.id)
       end
--- spec/lib/schema_contract/runner_spec.rb
+++ spec/lib/schema_contract/runner_spec.rb
@@ -5,22 +5,47 @@ require_relative '../../../lib/schema_contract/runner'
 
 describe SchemaContract::Runner do
   describe '.run' do
+    let(:user) { create(:user) }
+    let(:response) do
+      OpenStruct.new({ success?: true, status: 200, body: { foo: 'bar' } })
+    end
+
+    before do
+      Timecop.freeze
+      Flipper.enable(:schema_contract_test_index)
+    end
+
     context 'when a record already exists for the current day' do
+      before do
+        SchemaContractTest.create(name: 'test_index', user_uuid: '1234', response:, status: 'initiated')
+      end
+
       it 'does not create a record or enqueue a job' do
-        SchemaContract::Runner.run(user: , response: {status: 200, body: {foo: 'bar'}}, test_name: 'test_index')
-        
+        expect(UpstreamSchemaValidationJob).not_to receive(:perform_async)
+
+        expect do
+          SchemaContract::Runner.run(user:, response:, test_name: 'test_index')
+        end.not_to change(SchemaContractTest, :count)
       end
     end
 
     context 'when no record exists for the current day' do
-      it 'creates one with provided details and enqueues a job' do
+      before do
+        SchemaContractTest.create(name: 'test_index', user_uuid: '1234', response:, status: 'initiated',
+                                  created_at: Time.zone.yesterday.beginning_of_day)
+      end
 
+      it 'creates one with provided details and enqueues a job' do
+        expect(UpstreamSchemaValidationJob).to receive(:perform_async)
+
+        expect do
+          SchemaContract::Runner.run(user:, response:, test_name: 'test_index')
+        end.to change(SchemaContractTest, :count).by(1)
       end
     end
 
     context 'when an error is encountered' do
       it 'logs but does not raise the error' do
-
       end
     end
   end
