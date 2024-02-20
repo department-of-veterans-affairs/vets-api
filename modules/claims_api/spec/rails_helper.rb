9319de3f5206ee228365a71a1b81908706a15074
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-02-20 14:11:30 -0700

    Adds scripts to rails_helper to truncate the test.log before the test suite runs, and then scan the logs after the test suite runs.

--- modules/claims_api/spec/rails_helper.rb
+++ modules/claims_api/spec/rails_helper.rb
@@ -8,35 +8,19 @@ require 'bd/bd'
 require 'evss_service/base'
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    `truncate -s 0 log/test.log`
+  end
   config.after(:suite) do
-    # # words = %w[ssn secret password security private key id login ssn Full Mailing address Phone Email Social security Drivers license Passport Alien registration Financial Biometric DNA Citizenship documentation Medical Ethnic affiliation identification Sexual orientation Account password birth Criminal history Mothers maiden Credit card].each(&:downcase) # rubocop:disable Layout/LineLength
-    # exclusions = %w[model_id failure arguments filtered error 'data key']
-    # File.readlines('log/development.log', chomp: true).each_with_index do |line, index|
-    #   File.readlines('modules/claims_api/spec/support/pii_key_words.txt', chomp: true).each do |phrase|
-    #     # line_segments = line.split.each(&:downcase!)
-    #     # intersection = line_segments & words
-    #     # if intersection.present?
-    #     include_line = []
-    #     line.split.each do |segment|
-    #       if segment.to_s.downcase.scan(/#{phrase.downcase}/).present?
-    #         exclusions.each do |e|
-    #           include_line << index if segment.to_s.downcase.scan(/#{e}/).blank?
-    #         end
-    #       end
-    #       results << { line: "line number: #{index}, word/s: #{phrase}, line: #{line}" } if include_line.present?
-    #     end
-    #     # end
-    #   end
-    # end
-    results = `fgrep -f modules/claims_api/spec/support/pii_key_words.txt log/development.log`
+    results = `fgrep -f modules/claims_api/spec/support/pii_key_words.txt log/test.log`
     if results.present?
       puts ''
       puts '======================================='
-      puts 'Start check for PII in development log'
+      puts 'Start check for PII in test log'
       puts '======================================='
       puts results
       puts '======================================='
-      puts 'End check for PII in development log'
+      puts 'End check for PII in test log'
       puts '======================================='
     end
   end
