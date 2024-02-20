c098914aa6c91def6706ca64e672cbed706a9169
Steven Cumming <steven.cumming@stevencumming-cr2fcpq90d.attlocal.net>
2024-02-20 10:55:10 -0500

    add options to bin/test

--- lib/vets-api/commands/test.rb
+++ lib/vets-api/commands/test.rb
@@ -1,40 +1,48 @@
 # frozen_string_literal: true
+require 'pry'
 
 module VetsApi
   module Commands
     class Test
       class << self
 
+        attr_accessor :options, :inputs
+
         def run(args)
+          @options = args.select { |a| a.start_with?('--', '-') }
+          input_values = args.reject { |a| a.start_with?('--', '-') }
+          @inputs =  input_values.empty? ? 'spec modules' : input_values.join(' ')
 
-        end
-
-        private
-
-        def setup_developer_environment
           case File.read('.developer-environment')
-          when 'native'
+          when 'native', 'hybrid'
             test_native
           when 'docker'
             test_docker
-          when 'hybrid'
-            test_hybrid
           else
             puts "Invalid option for .developer-environment"
           end
         end
 
-        def test_native
+        private
 
+        def test_native
+          puts "running: #{rspec_command_builer}"
+          system(rspec_command_builer)
         end
 
         def test_docker
-
+         system("docker-compose run --rm --service-ports web bash -c \"#{rspec_command_builer}\"")
         end
 
-        def test_hybrid
-
+        # Verbose rspec output is also uses
+        # RSPEC_VERBOSE_OUTPUT is used in spec/rails_helper.rb:194
+        def rspec_command_builer
+          no_coverage = !@options.include?('--coverage')
+          verbose = @options.include?('--verbose')
+          verbose_out = verbose ? '' : '2> /dev/null'
+          "RAILS_ENV=test DISABLE_BOOTSNAP=true NOCOVERAGE=#{no_coverage} RSPEC_VERBOSE_OUTPUT=#{verbose} bundle exec parallel_rspec #{@inputs} #{verbose_out}".rstrip
         end
+
       end
     end
   end
