819b871f052870e84f152d1b0fc95077b0e2cb16
Riley Anderson <riley.anderson@oddball.io>
2024-02-20 14:03:10 -0700

    Add maintenence_windows to /v0/backend_statuses (#15556)

--- app/serializers/backend_statuses_serializer.rb
+++ app/serializers/backend_statuses_serializer.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class BackendStatusesSerializer < ActiveModel::Serializer
-  attributes :reported_at, :statuses
+  attributes :reported_at, :statuses, :maintenance_windows
 
   def id
     nil
+  end
+
+  def maintenance_windows
+    return [] unless @instance_options[:maintenance_windows]
+
+    ActiveModel::Serializer::CollectionSerializer.new(@instance_options[:maintenance_windows],
+                                                      serializer: MaintenanceWindowSerializer)
   end
 end
