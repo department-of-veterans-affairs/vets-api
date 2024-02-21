9fc2a3a543d30249ce835e1feb8b75b509f77c93
Kenny Santiago <50601724+ksantiagoBAH@users.noreply.github.com>
2024-02-20 18:42:59 -0600

    MBMS-55721 Updated model and erb

--- modules/simple_forms_api/spec/requests/v1/uploads_spec.rb
+++ modules/simple_forms_api/spec/requests/v1/uploads_spec.rb
@@ -189,7 +189,6 @@ RSpec.describe 'Forms uploader', type: :request do
             data = JSON.parse(fixture_path.read)
             attachment = double
             allow(attachment).to receive(:to_pdf).and_return(pdf_path)
-            allow(CombinePDF).to receive(:load).and_return([])
 
             expect(PersistentAttachment).to receive(:where).with(guid: ['a-random-uuid']).and_return([attachment])
 
@@ -212,9 +211,6 @@ RSpec.describe 'Forms uploader', type: :request do
             data = JSON.parse(fixture_path.read)
             attachment = double
             allow(attachment).to receive(:to_pdf).and_return(pdf_path)
-            allow(CombinePDF).to receive(:load).and_return([])
-
-
             expect(PersistentAttachment).to receive(:where).with(guid: ['a-random-uuid']).and_return([attachment])
             post '/simple_forms_api/v1/simple_forms', params: data
             expect(response).to have_http_status(:ok)
