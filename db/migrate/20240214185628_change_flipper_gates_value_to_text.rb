efb9ab850d4b8ac511e04f1b001ec8562ce2f60b
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-02-21 12:19:00 -0500

    Revert "bump flipper gems to 1.2.2 (#15541)"
    This reverts commit 5ea91affdcb8a278a3481d9496f6698d6d024ed4.

--- db/migrate/20240214185628_change_flipper_gates_value_to_text.rb
+++
@@ -1,9 +0,0 @@
-class ChangeFlipperGatesValueToText < ActiveRecord::Migration[7.0]
-  def up
-    change_column :flipper_gates, :value, :text
-  end
-
-  def down
-    change_column :flipper_gates, :value, :string
-  end
-end
