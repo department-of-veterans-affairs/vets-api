d12ee50118c46f3e77a79c89b0b2a87cbc749670
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-02-22 17:12:12 -0600

    API-33700 hourly submission error report to slack
    * Updates periodic jobs table
    * Adds sidekiq job to handle hourly run
    * Adds service to format message and send slcak message
    * Adds RSpec tests for newly created sidekiq job and service class
    	modified:   lib/periodic_jobs.rb
    	new file:   modules/claims_api/app/services/claims_api/slack/failed_submissions_messenger.rb
    	new file:   modules/claims_api/app/sidekiq/claims_api/report_hourly_unsuccessful_submissions.rb
    	new file:   modules/claims_api/spec/services/failed_submissions_messenger_spec.rb
    	new file:   modules/claims_api/spec/sidekiq/report_hourly_unsuccessful_submissions_spec.rb

---
+++ modules/claims_api/app/sidekiq/claims_api/report_hourly_unsuccessful_submissions.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module ClaimsApi
+  class ReportHourlyUnsuccessfulSubmissions < ClaimsApi::ServiceBase
+    def perform
+      return if !Rails.env.test? && !Rails.env.production?
+
+      @to = Time.now.in_time_zone('Eastern Time (US & Canada)').strftime('%l:%M%p %Z')
+      @from = 1.hour.ago.in_time_zone('Eastern Time (US & Canada)').strftime('%l:%M%p %Z')
+      @errored_claims = ClaimsApi::AutoEstablishedClaim.where(status: 'errored').pluck(:id).uniq
+      @errored_poa = ClaimsApi::PowerOfAttorney.where(status: 'errored').pluck(:id).uniq
+      @errored_itf = ClaimsApi::IntentToFile.where(status: 'errored').pluck(:id).uniq
+      @errored_ews = ClaimsApi::EvidenceWaiverSubmission.where(status: 'errored').pluck(:id).uniq
+
+      if errored_submissions_exist?
+        notify(
+          @errored_claims,
+          @errored_poa,
+          @errored_itf,
+          @errored_ews,
+          @to,
+          @from
+        )
+      end
+    end
+
+    # rubocop:disable Metrics/ParameterLists
+    def notify(claims, poa, itf, ews, from, to)
+      ClaimsApi::Slack::FailedSubmissionsMessenger.new(
+        claims,
+        poa,
+        itf,
+        ews,
+        to,
+        from
+      ).notify!
+    end
+    # rubocop:enable Metrics/ParameterLists
+
+    private
+
+    def errored_submissions_exist?
+      [@errored_claims, @errored_poa, @errored_itf, @errored_ews].any? { |var| var.count.positive? }
+    end
+  end
+end
