b79b77fea288451134e5e3b283e186401099b807
Micah Frazier <Micah.frazier@oddball.io>
2024-02-22 10:02:58 -0700

    Spec writin

--- spec/sidekiq/vre/submit1900_job_spec.rb
+++ spec/sidekiq/vre/submit1900_job_spec.rb
@@ -27,6 +27,11 @@ describe VRE::Submit1900Job do
     it 'calls claim.send_to_vre' do
       expect(claim).to receive(:send_to_vre).with(user)
     end
+
+    context 'using Benefits Intake API' do
+
+
+    end
   end
 
   describe 'raises an exception' do
