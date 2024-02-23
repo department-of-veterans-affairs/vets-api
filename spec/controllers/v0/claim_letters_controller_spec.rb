0cd33a5eb249c6b8fb073718b39abf3b0b529ebb
Samuel Cormier <sam.cormier@coforma.io>
2024-02-23 12:23:26 -0800

    updated claim letters to start showing 5103 doctypes, controlled by a new feature toggle

--- spec/controllers/v0/claim_letters_controller_spec.rb
+++ spec/controllers/v0/claim_letters_controller_spec.rb
@@ -39,9 +39,37 @@ RSpec.describe V0::ClaimLettersController, type: :controller do
     end
   end
 
+  describe '#index when "cst_include_ddl_5103_letters" feature flag is enabled' do
+    before do
+      Flipper.enable(:cst_include_ddl_5103_letters)
+    end
+
+    it 'lists correct documents' do
+      get(:index)
+      letters = JSON.parse(response.body)
+      allowed_letters = letters.select { |d| d['doc_type'] == '184' || d['doc_type'] == '65' || d['doc_type'] == '68' }
+
+      expect(allowed_letters.length).to eql(letters.length)
+    end
+  end
+
   describe '#index when "cst_include_ddl_boa_letters" feature flag is disabled' do
     before do
       Flipper.disable(:cst_include_ddl_boa_letters)
+    end
+
+    it 'lists correct documents' do
+      get(:index)
+      letters = JSON.parse(response.body)
+      allowed_letters = letters.select { |d| d['doc_type'] == '184' }
+
+      expect(allowed_letters.length).to eql(letters.length)
+    end
+  end
+
+  describe '#index when "cst_include_ddl_5103_letters" feature flag is disabled' do
+    before do
+      Flipper.disable(:cst_include_ddl_5103_letters)
     end
 
     it 'lists correct documents' do
