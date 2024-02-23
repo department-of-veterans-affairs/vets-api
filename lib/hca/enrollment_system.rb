1f62ed21369ac995fa84e1ba9c96ef0cb670e6fe
lihan li <frankieteardrop@gmail.com>
2024-02-23 06:07:11 -0800

    add radiationcleanupefforts

--- lib/hca/enrollment_system.rb
+++ lib/hca/enrollment_system.rb
@@ -546,7 +546,7 @@ module HCA
           'agentOrangeInd' => veteran['vietnamService'].present? || veteran['exposedToAgentOrange'].present?,
           'envContaminantsInd' => veteran['swAsiaCombat'].present?,
           'campLejeuneInd' => veteran['campLejeune'].present?,
-          'radiationExposureInd' => veteran['exposedToRadiation'].present?
+          'radiationExposureInd' => veteran['exposedToRadiation'].present? || veteran['radiationCleanupEfforts'].present?
         }.merge(veteran_to_tera(veteran))
       }
     end
