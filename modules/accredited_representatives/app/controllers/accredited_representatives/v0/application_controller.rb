b5219fd5000d2de30a938396201627f3aa773f7e
Oren Mittman <nihil2501@gmail.com>
2024-02-26 05:32:21 -0500

    ARF-76996 set forthcoming ARP service_tag comment includes link to related ticket: https://app.zenhub.com/workspaces/accredited-representative-facing-team-65453a97a9cc36069a2ad1d6/issues/gh/department-of-veterans-affairs/va.gov-team/77004

--- modules/accredited_representatives/app/controllers/accredited_representatives/v0/application_controller.rb
+++ modules/accredited_representatives/app/controllers/accredited_representatives/v0/application_controller.rb
@@ -3,9 +3,11 @@
 module AccreditedRepresentatives
   module V0
     class ApplicationController < ApplicationController
-      # Replace SERVICE_NAME with your application's service name in the Datadog Service Catalog
-      # This is the dd-service property for your application here: https://github.com/department-of-veterans-affairs/vets-api/tree/master/datadog-service-catalog
-      service_tag 'SERVICE_NAME'
+      # Add this `service_tag` to the Datadog Service Catalog.
+      #   https://app.zenhub.com/workspaces/accredited-representative-facing-team-65453a97a9cc36069a2ad1d6/issues/gh/department-of-veterans-affairs/va.gov-team/77004
+      # It will be the dd-service property for your application here:
+      #   https://github.com/department-of-veterans-affairs/vets-api/tree/master/datadog-service-catalog
+      service_tag 'accredited-representatives'
     end
   end
 end
