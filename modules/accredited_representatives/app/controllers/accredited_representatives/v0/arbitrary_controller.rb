0f76710c88b15b35a5758e01705cd090ddc18349
Oren Mittman <nihil2501@gmail.com>
2024-02-26 05:32:21 -0500

    Revert "ARF-76996 temporarily validate ARP feature toggle spec"
    This reverts commit 6ea01499699e719365abb6b3a6a1a313b9ab6fb4.

--- modules/accredited_representatives/app/controllers/accredited_representatives/v0/arbitrary_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module AccreditedRepresentatives
-  module V0
-    class ArbitraryController < ApplicationController
-      def arbitrary = head :ok
-    end
-  end
-end
--- modules/accredited_representatives/config/routes.rb
+++ modules/accredited_representatives/config/routes.rb
@@ -2,6 +2,5 @@
 
 AccreditedRepresentatives::Engine.routes.draw do
   namespace :v0, defaults: { format: :json } do
-    get 'arbitrary', to: 'arbitrary#arbitrary'
   end
 end
