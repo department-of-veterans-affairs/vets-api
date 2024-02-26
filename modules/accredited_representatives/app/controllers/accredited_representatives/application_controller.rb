38770c4d94ed5e18606f2467d4a9b9c791730128
Oren Mittman <nihil2501@gmail.com>
2024-02-26 16:06:28 -0500

    ARF-76996 ARP application controller inherits global Discussed [here](https://dsva.slack.com/archives/C06ABHUNBRS/p1708970923101199)

--- modules/accredited_representatives/app/controllers/accredited_representatives/application_controller.rb
+++ modules/accredited_representatives/app/controllers/accredited_representatives/application_controller.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
 module AccreditedRepresentatives
-  # TODO: refactor and inherit from some global controller
-  class ApplicationController < DuplicativeGlobalApplicationController
+  class ApplicationController < ::ApplicationController
     # TODO: Add ARP to Datadog Service Catalog #77004
     #   https://app.zenhub.com/workspaces/accredited-representative-facing-team-65453a97a9cc36069a2ad1d6/issues/gh/department-of-veterans-affairs/va.gov-team/77004
     # It will be the dd-service property for your application here:
     #   https://github.com/department-of-veterans-affairs/vets-api/tree/master/datadog-service-catalog
     service_tag 'accredited-representatives'
 
-    before_action do
-      routing_error unless Flipper.enabled?(:representatives_portal_api)
-    end
+    before_action :verify_feature_enabled!
+
+    # TODO: Integrate SignIn Service to ARP Engine #76157
+    #   https://app.zenhub.com/workspaces/accredited-representative-facing-team-65453a97a9cc36069a2ad1d6/issues/gh/department-of-veterans-affairs/va.gov-team/76157
+    skip_before_action :authenticate
 
     private
+
+    def verify_feature_enabled!
+      return if Flipper.enabled?(:representatives_portal_api)
+
+      routing_error
+    end
 
     def current_user
       nil
