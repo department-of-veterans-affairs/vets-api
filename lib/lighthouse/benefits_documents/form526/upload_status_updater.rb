de4558d98bd2fa88004792c86f99ffb45354c97c
Nathan Burgess <burgess.nathan28@gmail.com>
2024-02-26 16:55:05 -0500

    Implement UploadStatusUpdater
    Implements a service class that encapsulates all of the parsing of a Lighthouse document status data structure, updates a supplied LighthouseDocumentUpload accordingly

--- lib/lighthouse/benefits_documents/form526/upload_status_updater.rb
+++ lib/lighthouse/benefits_documents/form526/upload_status_updater.rb
@@ -1,57 +1,113 @@
 # frozen_string_literal: true
 
-module Lighthouse
-  module BenefitsDocuments
-    module Form526
-      class UploadStatusUpdater
-        # This class parses the response from the Lighthouse Benefits Documents API uploads/status endpoint
-        # Documentation on this endpoint is available here:
-        # https://dev-developer.va.gov/explore/api/benefits-documents/docs?version=current
+module BenefitsDocuments
+  module Form526
+    class UploadStatusUpdater
+      # Parses the current status of a LighthouseDocumentUpload that has been submitted to Lighthouse,
+      # using metadata returned from the Lighthouse Benefits Documents API '/uploads/status' endpoint
+      # Encapsulates convenience methods for understanding if the document has completed all steps in Lighthouse or
+      # if there was a failure, to avoid having code elsewhere know the schema of Lighthouse's status data structure
+      #
+      # Additionally, updates the state of the LighthouseDocumentUpload in vets-api to reflect
+      # the current status of the document as it makes its way from Lighthouse > VBMS > BGS
+      #
+      # Documentation on the Lighthouse '/uploads/status' endpoint is available here:
+      # https://dev-developer.va.gov/explore/api/benefits-documents/docs?version=current
 
-        LIGHTHOUSE_COMPLETED_STATUS_STATE = 'SUCCESS'
-        LIGHTHOUSE_FAILED_STATUS_STATE = 'FAILED'
+      LIGHTHOUSE_DOCUMENT_COMPLETE_STATUS = 'SUCCESS'
+      LIGHTHOUSE_DOCUMENT_FAILED_STATUS = 'FAILED'
 
-        LIGHTHOUSE_COMPLETED_STEP_STATUS_STATE = 'SUCCESS'
+      LIGHTHOUSE_STEP_SUCCEEDED_STATUS = 'SUCCESS'
 
-        LIGHTHOUSE_VBMS_STEP_NAME = 'CLAIMS_EVIDENCE'
-        LIGHTHOUSE_BGS_STEP_NAME = 'BENEFITS_GATEWAY_SERVICE'
+      LIGHTHOUSE_VBMS_STEP_NAME = 'CLAIMS_EVIDENCE'
+      LIGHTHOUSE_BGS_STEP_NAME = 'BENEFITS_GATEWAY_SERVICE'
 
-        def initialize(document_upload_progress, lighthouse_document_upload)
-          @document_upload_progress = document_upload_progress
-          @lighthouse_document_upload = lighthouse_document_upload
+      # @param lighthouse_document_status [Hash] includes a single document's status progress
+      # after it has been submitted to Lighthouse, while Lighthouse attempts to pass it on to
+      # VBMS and then BGS. This data comes from Lighthouse's uploads/status endpoint
+      #
+      # @param lighthouse_document_upload [LighthouseDocumentUpload] the VA.gov record of the document
+      # we submitted to Lighthouse we are tracking.
+      #
+      # example lighthouse_document_status hash:
+      #     {
+      #       {
+      #         "requestId": 600000001,
+      #         "time": {
+      #           "startTime": 1502199000,
+      #           "endTime": 1502199000
+      #         },
+      #         "status": "IN_PROGRESS",
+      #         "steps": [
+      #           {
+      #             "name": "BENEFITS_GATEWAY_SERVICE",
+      #             "nextStepName": "BENEFITS_GATEWAY_SERVICE",
+      #             "description": "string",
+      #             "status": "NOT_STARTED"
+      #           }
+      #         ],
+      #         "error": {
+      #           "detail": "string",
+      #           "step": "BENEFITS_GATEWAY_SERVICE"
+      #         }
+      #       }
+      #     }
+      #
+      def initialize(lighthouse_document_status, lighthouse_document_upload)
+        @lighthouse_document_status = lighthouse_document_status
+        @lighthouse_document_upload = lighthouse_document_upload
+      end
+
+      def failed?
+        @lighthouse_document_status[:status] == LIGHTHOUSE_DOCUMENT_FAILED_STATUS
+      end
+
+      def completed?
+        @lighthouse_document_status[:status] == LIGHTHOUSE_DOCUMENT_COMPLETE_STATUS
+      end
+
+      def update_status
+        if failed?
+          handle_upload_failure
+          return
         end
 
-        def failed?
-          @document_upload_progress[:status] == LIGHTHOUSE_FAILED_STATUS_STATE
+        check_vbms_submission_status if @lighthouse_document_upload.pending_vbms_submission?
+        check_bgs_submission_status if @lighthouse_document_upload.pending_bgs_submission?
+      end
+
+      private
+
+      def handle_upload_failure
+        error_message = @lighthouse_document_status[:error]
+        @lighthouse_document_upload.update!(error_message: error_message.to_json)
+
+        if error_message[:step] == LIGHTHOUSE_VBMS_STEP_NAME
+          @lighthouse_document_upload.vbms_submission_failed!(@document_upload_status)
+        elsif error_message[:step] == LIGHTHOUSE_BGS_STEP_NAME
+          @lighthouse_document_upload.bgs_submission_failed!(@document_upload_status)
         end
+      end
 
-        def completed?
-          @document_upload_progress[:status] == LIGHTHOUSE_COMPLETED_STATUS_STATE
+      def check_vbms_submission_status
+        vbms_step = @lighthouse_document_status[:steps].find { |step| step[:name] == LIGHTHOUSE_VBMS_STEP_NAME }
+
+        if vbms_step[:status] == LIGHTHOUSE_STEP_SUCCEEDED_STATUS
+          @lighthouse_document_upload.vbms_submission_complete!(@document_upload_status)
         end
+      end
 
-        def progressed?
-          return false if failed?
+      def check_bgs_submission_status
+        bgs_step = @lighthouse_document_status[:steps].find { |step| step[:name] == LIGHTHOUSE_BGS_STEP_NAME }
 
-          if @lighthouse_document_upload.pending_vbms_submission?
-            return true if vbms_submission_complete?
-          elsif @lighthouse_document_upload.pending_bgs_submission?
-            return true if bgs_submission_complete?
-          end
+        if bgs_step[:status] == LIGHTHOUSE_STEP_SUCCEEDED_STATUS
+          processing_end_time = @lighthouse_document_status.dig(:time, :endTime)
 
-          # Status is unchanged
-          false
-        end
-
-        private
-
-        def vbms_submission_complete?
-          vbms_step = @document_upload_progress[:steps].find { |step| step[:name] == LIGHTHOUSE_VBMS_STEP_NAME }
-          vbms_step[:status] == LIGHTHOUSE_COMPLETED_STEP_STATUS_STATE
-        end
-
-        def bgs_submission_complete?
-          vbms_step = @document_upload_progress[:steps].find { |step| step[:name] == LIGHTHOUSE_BGS_STEP_NAME }
-          vbms_step[:status] == LIGHTHOUSE_COMPLETED_STEP_STATUS_STATE
+          # Document is complete, save end time
+          # Lighthouse returns date times as UNIX timestamps
+          formatted_end_time = DateTime.strptime(processing_end_time, '%s')
+          @lighthouse_document_upload.update!(lighthouse_processing_ended_at: formatted_end_time)
+          @lighthouse_document_upload.bgs_submission_complete!(@document_upload_status)
         end
       end
     end
--- spec/lib/lighthouse/benefits_documents/form526/upload_status_updater_spec.rb
+++ spec/lib/lighthouse/benefits_documents/form526/upload_status_updater_spec.rb
@@ -3,130 +3,352 @@
 require 'rails_helper'
 require 'lighthouse/benefits_documents/form526/upload_status_updater'
 
-RSpec.describe Lighthouse::BenefitsDocuments::Form526::UploadStatusUpdater do
+RSpec.describe BenefitsDocuments::Form526::UploadStatusUpdater do
   let(:lighthouse_document_upload) { create(:lighthouse_document_upload) }
 
   describe '#failed?' do
     context 'if the document is in a failed state' do
-      let(:failed_document_status) { { status: 'FAILED' } }
+      let(:failed_document_status) { { 'status': 'FAILED' } }
 
       it 'returns true' do
-        status = described_class.new(failed_document_status, lighthouse_document_upload)
-        expect(status.failed?).to eq(true)
+        status_updater = described_class.new(failed_document_status, lighthouse_document_upload)
+        expect(status_updater.failed?).to eq(true)
       end
     end
 
     context 'if the document is not in a failed state' do
-      let(:in_progress_status) { { status: 'IN_PROGRESS' } }
+      let(:in_progress_status) { { 'status': 'IN_PROGRESS' } }
 
       it 'returns false' do
-        status = described_class.new(in_progress_status, lighthouse_document_upload)
-        expect(status.failed?).to eq(false)
+        status_updater = described_class.new(in_progress_status, lighthouse_document_upload)
+        expect(status_updater.failed?).to eq(false)
       end
     end
   end
 
   describe '#completed?' do
     context 'if the document is in a completed state' do
-      let(:completed_document_status) { { status: 'SUCCESS' } }
+      let(:completed_document_status) { { 'status': 'SUCCESS' } }
 
       it 'returns true' do
-        status = described_class.new(completed_document_status, lighthouse_document_upload)
-        expect(status.completed?).to eq(true)
+        status_updater = described_class.new(completed_document_status, lighthouse_document_upload)
+        expect(status_updater.completed?).to eq(true)
       end
     end
 
     context 'if the document is not in a completed state' do
-      let(:in_progress_status) { { status: 'IN_PROGRESS' } }
+      let(:in_progress_status) { { 'status': 'IN_PROGRESS' } }
 
       it 'returns false' do
-        status = described_class.new(in_progress_status, lighthouse_document_upload)
-        expect(status.completed?).to eq(false)
+        status_updater = described_class.new(in_progress_status, lighthouse_document_upload)
+        expect(status_updater.completed?).to eq(false)
       end
     end
   end
 
-  describe '#progressed?' do
-    context 'pending VBMS submission' do
-      let(:lighthouse_document_upload) { create(:lighthouse_document_upload, aasm_state: 'pending_vbms_submission') }
+  describe '#update_status' do
+    context 'when the document is pending submission to VBMS' do
+      let(:vbms_pending_upload) { create(:lighthouse_document_upload, aasm_state: 'pending_vbms_submission') }
 
-      context 'when the document has progressed status' do
-        let(:vmbs_submission_complete_status) do
+      context 'when the document was successfully submitted to VBMS' do
+        let(:vbms_submission_complete_status) do
           {
-            status: 'IN_PROGRESS',
-            steps: [
+            'status': 'IN_PROGRESS',
+            'steps': [
               {
-                name: 'CLAIMS_EVIDENCE',
-                status: 'SUCCESS'
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'SUCCESS'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'NOT_STARTED'
               }
             ]
           }
         end
 
-        it 'returns true' do
-          status = described_class.new(vmbs_submission_complete_status, lighthouse_document_upload)
-          expect(status.progressed?).to eq(true)
+        it 'transitions the document to a pending_bgs_submission state' do
+          status_updater = described_class.new(vbms_submission_complete_status, vbms_pending_upload)
+          expect { status_updater.update_status }.to change(vbms_pending_upload, :aasm_state)
+            .from('pending_vbms_submission').to('pending_bgs_submission')
         end
       end
 
-      context 'when the document has not progressed status' do
-        let(:vmbs_submission_in_progress_status) do
+      context 'when the document is still pending submission to VBMS' do
+        let(:vbms_submission_pending_status) do
           {
-            status: 'IN_PROGRESS',
-            steps: [
+            'status': 'IN_PROGRESS',
+            'steps': [
               {
-                name: 'CLAIMS_EVIDENCE',
-                status: 'IN_PROGRESS'
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'NOT_STARTED'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'NOT_STARTED'
               }
             ]
           }
         end
 
-        it 'returns false' do
-          status = described_class.new(vmbs_submission_in_progress_status, lighthouse_document_upload)
-          expect(status.progressed?).to eq(false)
+        it 'does not transition the document state' do
+          status_updater = described_class.new(vbms_submission_pending_status, vbms_pending_upload)
+          expect { status_updater.update_status }.not_to change(vbms_pending_upload, :aasm_state)
+        end
+      end
+
+      context 'when the document is still being submitted to VBMS' do
+        let(:vbms_submission_in_progress_status) do
+          {
+            'status': 'IN_PROGRESS',
+            'steps': [
+              {
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'IN_PROGRESS'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'NOT_STARTED'
+              }
+            ]
+          }
+        end
+
+        it 'does not transition the document state' do
+          status_updater = described_class.new(vbms_submission_in_progress_status, vbms_pending_upload)
+          expect { status_updater.update_status }.not_to change(vbms_pending_upload, :aasm_state)
+        end
+      end
+
+      # Cover the case where the upload is marked pending_vbms_submission,
+      # but both the VBMS and BGS uploads succeeded since we last polled
+      context 'when the document was submitted to VBMS successfully and BGS successfully' do
+        let(:submission_complete_status) do
+          {
+            'status': 'SUCCESS',
+            'time': {
+              'endTime': '499152060'
+            },
+            'steps': [
+              {
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'SUCCESS'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'SUCCESS'
+              }
+            ]
+          }
+        end
+
+        it 'transitions the document to the complete state' do
+          status_updater = described_class.new(submission_complete_status, vbms_pending_upload)
+          expect { status_updater.update_status }.to change(vbms_pending_upload, :aasm_state).to('complete')
         end
       end
     end
 
-    context 'pending BGS submission' do
-      let(:lighthouse_document_upload) { create(:lighthouse_document_upload, aasm_state: 'pending_bgs_submission') }
+    context 'when the document is pending submission to BGS' do
+      let(:bgs_pending_upload) { create(:lighthouse_document_upload, aasm_state: 'pending_bgs_submission') }
 
-      context 'when the document has progressed status' do
+      context 'when the document was successfully submitted to BGS' do
+        # Lighthouse returns date times as UNIX timestamps
+        let(:completion_time) { '499152060' }
+
         let(:bgs_submission_complete_status) do
           {
-            status: 'SUCCESS',
-            steps: [
+            'status': 'SUCCESS',
+            'time': {
+              'endTime': completion_time
+            },
+            'steps': [
               {
-                name: 'BENEFITS_GATEWAY_SERVICE',
-                status: 'SUCCESS'
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'SUCCESS'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'SUCCESS'
               }
             ]
           }
         end
 
-        it 'returns true' do
-          status = described_class.new(bgs_submission_complete_status, lighthouse_document_upload)
-          expect(status.progressed?).to eq(true)
+        it 'transitions the document to a complete state' do
+          status_updater = described_class.new(bgs_submission_complete_status, bgs_pending_upload)
+          expect { status_updater.update_status }.to change(bgs_pending_upload, :aasm_state)
+            .from('pending_bgs_submission').to('complete')
+        end
+
+        it 'saves the lighthouse_processing_ended_at timestamp' do
+          status_updater = described_class.new(bgs_submission_complete_status, bgs_pending_upload)
+          expect { status_updater.update_status }.to change(bgs_pending_upload, :lighthouse_processing_ended_at)
+            .to(DateTime.strptime(completion_time, '%s'))
         end
       end
 
-      context 'when the document has not progressed status' do
-        let(:bgs_submission_in_progress_status) do
+      context 'when the document is still pending submission to BGS' do
+        let(:vbms_submission_pending_status) do
           {
-            status: 'IN_PROGRESS',
-            steps: [
+            'status': 'IN_PROGRESS',
+            'steps': [
               {
-                name: 'BENEFITS_GATEWAY_SERVICE',
-                status: 'IN_PROGRESS'
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'SUCCESS'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'NOT_STARTED'
               }
             ]
           }
         end
 
-        it 'returns false' do
-          status = described_class.new(bgs_submission_in_progress_status, lighthouse_document_upload)
-          expect(status.progressed?).to eq(false)
+        it 'does not transition the document state' do
+          status_updater = described_class.new(vbms_submission_pending_status, bgs_pending_upload)
+          expect { status_updater.update_status }.not_to change(bgs_pending_upload, :aasm_state)
+        end
+      end
+
+      context 'when the document is still being submitted to BGS' do
+        let(:vbms_submission_in_progress_status) do
+          {
+            'status': 'IN_PROGRESS',
+            'steps': [
+              {
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'SUCCESS'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'IN_PROGRESS'
+              }
+            ]
+          }
+        end
+
+        it 'does not transition the document state' do
+          status_updater = described_class.new(vbms_submission_in_progress_status, bgs_pending_upload)
+          expect { status_updater.update_status }.not_to change(bgs_pending_upload, :aasm_state)
+        end
+      end
+    end
+
+    context 'when the upload failed' do
+      context 'when the Lighthouse upload to VBMS failed' do
+        let(:vbms_pending_upload) { create(:lighthouse_document_upload, aasm_state: 'pending_vbms_submission') }
+
+        let(:vbms_submission_failed_status) do
+          {
+            'status': 'FAILED',
+            'steps': [
+              {
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'FAILED'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'NOT_STARTED'
+              }
+            ],
+            'error': {
+              'detail': 'VBMS System Outage',
+              'step': 'CLAIMS_EVIDENCE'
+            }
+          }
+        end
+
+        it 'transitions the upload to the failed_vbms_submission state' do
+          status_updater = described_class.new(vbms_submission_failed_status, vbms_pending_upload)
+          expect { status_updater.update_status }.to change(vbms_pending_upload, :aasm_state)
+            .from('pending_vbms_submission').to('failed_vbms_submission')
+        end
+
+        it 'saves an error message' do
+          status_updater = described_class.new(vbms_submission_failed_status, vbms_pending_upload)
+          expect { status_updater.update_status }.to change(vbms_pending_upload, :error_message)
+            .to(
+              {
+                detail: 'VBMS System Outage',
+                step: 'CLAIMS_EVIDENCE'
+              }.to_json
+            )
+        end
+      end
+
+      context 'when the Lighthouse upload to BGS failed' do
+        let(:pending_bgs_upload) { create(:lighthouse_document_upload, aasm_state: 'pending_bgs_submission') }
+
+        let(:bgs_submission_failed_status) do
+          {
+            'status': 'FAILED',
+            'steps': [
+              {
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'SUCCESS'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'FAILED'
+              }
+            ],
+            'error': {
+              'detail': 'BGS System Outage',
+              'step': 'BENEFITS_GATEWAY_SERVICE'
+            }
+          }
+        end
+
+        it 'transitions the upload to the failed_bgs_submission state' do
+          status_updater = described_class.new(bgs_submission_failed_status, pending_bgs_upload)
+          expect { status_updater.update_status }.to change(pending_bgs_upload, :aasm_state)
+            .from('pending_bgs_submission').to('failed_bgs_submission')
+        end
+
+        it 'saves an error message' do
+          status_updater = described_class.new(bgs_submission_failed_status, pending_bgs_upload)
+          expect { status_updater.update_status }.to change(pending_bgs_upload, :error_message)
+            .to(
+              {
+                'detail': 'BGS System Outage',
+                'step': 'BENEFITS_GATEWAY_SERVICE'
+              }.to_json
+            )
+        end
+      end
+
+      # NOTE: it's possible in between the last time we polled a document's status a VBMS submission completed
+      # and the BGS submission failed. Because the document was marked pending_vbms_submission the last time we
+      # polled, we need to ensure we allow transitioning the document state from pending_vbms_submission
+      # to failed_bgs_submission even though we missed the VBMS completion transition in between
+      context 'document pending_vbms_submission, BGS submission failed, but we missed the VBMS completion transition' do
+        let(:vbms_pending_upload) { create(:lighthouse_document_upload, aasm_state: 'pending_vbms_submission') }
+
+        let(:bgs_submission_failed_status) do
+          {
+            'status': 'FAILED',
+            'steps': [
+              {
+                'name': 'CLAIMS_EVIDENCE',
+                'status': 'SUCCESS'
+              },
+              {
+                'name': 'BENEFITS_GATEWAY_SERVICE',
+                'status': 'FAILED'
+              }
+            ],
+            'error': {
+              'detail': 'BGS System Outage',
+              'step': 'BENEFITS_GATEWAY_SERVICE'
+            }
+          }
+        end
+
+        it 'transitions from pending_vbms_submission to failed_bgs_submission' do
+          status_updater = described_class.new(bgs_submission_failed_status, vbms_pending_upload)
+          expect { status_updater.update_status }.to change(vbms_pending_upload, :aasm_state)
+            .from('pending_vbms_submission').to('failed_bgs_submission')
         end
       end
     end
