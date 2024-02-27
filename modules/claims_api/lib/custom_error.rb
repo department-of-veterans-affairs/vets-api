5301a63cda5c5e98f59b87820061d5e7a1f23199
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-02-27 07:53:52 -0700

    Addresses PR comments

--- modules/claims_api/lib/custom_error.rb
+++ modules/claims_api/lib/custom_error.rb
@@ -8,29 +8,35 @@ module ClaimsApi
       @method = method
     end
 
-    def build_error
-      custom_error = if @error == Faraday::ConnectionFailed || @error == Faraday::ParsingError
-                       { 'messages' => [{ 'key' => 'ServiceException', 'detail' => 'test detail', status: '500' }] }
+    def build_error # rubocop:disable Metrics/MethodLength
+      if @error == Faraday::ConnectionFailed || @error == Faraday::ParsingError
+        custom_error = { 'messages' => [{ 'key' => 'ServiceException',
+                                          'detail' => 'A Faraday error has occurred.', status: '500' }] }
+        update_claim(custom_error)
+        raise EVSS::DisabilityCompensationForm::ServiceException, custom_error
 
-                     elsif @error.is_a?(::Common::Exceptions::BackendServiceException) # missing bracket on form_data
-                       { 'messages' => [{ 'key' => 'BackendException',
-                                          'detail' => 'Backend exception', status: '500' }] }
-
-                     elsif @error.is_a?(StandardError) # when client_key is blank
-                       { 'messages' => [{ 'key' => 'Client error', 'detail' => 'client exception', status: '400' }] }
-
-                     else
-                       { 'messages' => [{ 'key' => 'Unknown error', 'detail' => 'unknown error', status: '500' }] }
-
-                     end
-      update_claim(custom_error)
-      log_outcome_for_claims_api(custom_error)
-      raise EVSS::DisabilityCompensationForm::ServiceException, custom_error
+      elsif @error.is_a?(::Common::Exceptions::BackendServiceException) # missing bracket on form_data
+        custom_error = { 'messages' => [{ 'key' => 'BackendException',
+                                          'detail' => 'A backend exception occurred.', status: '500' }] }
+        update_claim(custom_error)
+        raise EVSS::DisabilityCompensationForm::ServiceException, custom_error
+      elsif @error.is_a?(StandardError) # when client_key is blank
+        custom_error = { 'messages' => [{ 'key' => 'Client error',
+                                          'detail' => 'A client exception has occurred.', status: '400' }] }
+        update_claim(custom_error)
+        raise custom_error
+      else
+        custom_error = { 'messages' => [{ 'key' => 'Unknown error',
+                                          'detail' => 'An unknown error has occurred.', status: '500' }] }
+        update_claim(custom_error)
+        raise custom_error
+      end
     end
 
     def update_claim(custom_error)
+      log_outcome_for_claims_api(custom_error)
       @claim.status = ClaimsApi::AutoEstablishedClaim::ERRORED
-      @claim.evss_response = custom_error['message']
+      @claim.evss_response = custom_error['messages']
       @claim.save
     end
 
