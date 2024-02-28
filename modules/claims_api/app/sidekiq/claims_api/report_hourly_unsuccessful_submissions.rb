bc608ecc0ed4f9a49442aefaacaf016cbb626561
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-02-28 15:10:39 -0600

    Fixes my oversight when updating the window for searching and reporting

--- modules/claims_api/app/sidekiq/claims_api/report_hourly_unsuccessful_submissions.rb
+++ modules/claims_api/app/sidekiq/claims_api/report_hourly_unsuccessful_submissions.rb
@@ -12,10 +12,10 @@ module ClaimsApi
       @search_from = 1.hour.ago
       @reporting_to = Time.now.in_time_zone('Eastern Time (US & Canada)').strftime('%l:%M%p %Z')
       @reporting_from = 1.hour.ago.in_time_zone('Eastern Time (US & Canada)').strftime('%l:%M%p %Z')
-      @errored_claims = ClaimsApi::AutoEstablishedClaim.where(created_at: @from..@to, status: 'errored').pluck(:id).uniq
-      @errored_poa = ClaimsApi::PowerOfAttorney.where(created_at: @from..@to, status: 'errored').pluck(:id).uniq
-      @errored_itf = ClaimsApi::IntentToFile.where(created_at: @from..@to, status: 'errored').pluck(:id).uniq
-      @errored_ews = ClaimsApi::EvidenceWaiverSubmission.where(created_at: @from..@to,
+      @errored_claims = ClaimsApi::AutoEstablishedClaim.where(created_at: @search_from..@search_to, status: 'errored').pluck(:id).uniq
+      @errored_poa = ClaimsApi::PowerOfAttorney.where(created_at: @search_from..@search_to, status: 'errored').pluck(:id).uniq
+      @errored_itf = ClaimsApi::IntentToFile.where(created_at: @search_from..@search_to, status: 'errored').pluck(:id).uniq
+      @errored_ews = ClaimsApi::EvidenceWaiverSubmission.where(created_at: @search_from..@search_to,
                                                                status: 'errored').pluck(:id).uniq
       @environment = Rails.env
 
