636718622ea9716412ec06566f99a3bb1e568cab
AJ Magdub <ahmed.magdub@va.gov>
2024-02-29 16:29:56 -0700

    added icn anonymization to vamf url logging

--- modules/vaos/app/services/vaos/middleware/response/errors.rb
+++ modules/vaos/app/services/vaos/middleware/response/errors.rb
@@ -7,8 +7,31 @@ module VAOS
         def on_complete(env)
           return if env.success?
 
-          Sentry.set_extras(vamf_status: env.status, vamf_body: env.response_body, vamf_url: env.url)
+          Sentry.set_extras(vamf_status: env.status, vamf_body: env.response_body, vamf_url: anonymize_icn(env.url))
           raise VAOS::Exceptions::BackendServiceException, env
+        end
+
+        private
+
+        # Anonymizes the ICN present in a given URI object by substituting a SHA256 digest for the ICN.
+        # If an ICN is not present in the URL,  it would simply return the original URI.
+        #
+        # @param url [URI] URI in which ICN needs to be anonymized.
+        #
+        # @return [URI] URI with anonymized ICN (If present), original URI otherwise.
+        #
+        def anonymize_icn(uri)
+          return uri if uri.nil?
+
+          # Extract the patient ICN part from the URL
+          url = uri.to_s
+          match = url[/(\d{10}V\d{6})/]
+
+          return uri unless match
+
+          digest = Digest::SHA256.hexdigest(match)
+          url.gsub!(match, digest)
+          URI(url)
         end
       end
     end
--- modules/vaos/spec/services/middleware/vaos_errors_spec.rb
+++ modules/vaos/spec/services/middleware/vaos_errors_spec.rb
@@ -135,4 +135,21 @@ describe VAOS::Middleware::Response::Errors do
       end
     end
   end
+
+  describe 'anonymize_icn' do
+    it 'returns nil if the URI is nil' do
+      expect(subject.send(:anonymize_icn, nil)).to be_nil
+    end
+
+    it 'returns the original URI if the URI does not contain an ICN' do
+      uri = URI.parse('http://example.com')
+      expect(subject.send(:anonymize_icn, uri)).to be(uri)
+    end
+
+    it 'returns a URI with the ICN anonymized' do
+      uri = URI.parse('http://example.com/1234567890V123456')
+      anon_uri = URI.parse('http://example.com/441ab560b8fc574c6bf84d6c6105318b79455321a931ef701d39f4ff91894c64')
+      expect(subject.send(:anonymize_icn, uri)).to eql(anon_uri)
+    end
+  end
 end
