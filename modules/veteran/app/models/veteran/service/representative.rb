c4ab62079c5ef95ef6fba3028967c4d9a853efe6
Holden Hinkle <holden@holdenhinkle.com>
2024-02-29 12:32:12 -0500

    change #diff param to rep_data

--- modules/veteran/app/models/veteran/service/representative.rb
+++ modules/veteran/app/models/veteran/service/representative.rb
@@ -122,12 +122,12 @@ module Veteran
 
       #
       # Compares rep's current info with new data to detect changes in address, email, or phone number.
-      # @param other [Hash] New data with :email, :phone_number, and :request_address keys for comparison.
+      # @param rep_data [Hash] New data with :email, :phone_number, and :request_address keys for comparison.
       #
       # @return [Hash] Hash with "email_changed", "phone_number_changed", "address_changed" keys as booleans.
-      def diff(other)
+      def diff(rep_data)
         %i[address email phone_number].each_with_object({}) do |field, diff|
-          diff["#{field}_changed"] = field == :address ? address_changed?(other) : send(field) != other[field]
+          diff["#{field}_changed"] = field == :address ? address_changed?(rep_data) : send(field) != rep_data[field]
         end
       end
 
@@ -138,11 +138,11 @@ module Veteran
       # @param other_address [Hash] New address data with keys for address components and state code.
       #
       # @return [Boolean] True if current address differs from `other_address`, false otherwise.
-      def address_changed?(other)
+      def address_changed?(rep_data)
         address = [address_line1, address_line2, address_line3, city, zip_code, zip_suffix, state_code].join(' ')
-        other_address = other[:request_address]
+        other_address = rep_data[:request_address]
                         .values_at(:address_line1, :address_line2, :address_line3, :city, :zip_code5, :zip_code4)
-                        .push(other.dig(:request_address, :state_province, :code))
+                        .push(rep_data.dig(:request_address, :state_province, :code))
                         .join(' ')
         address != other_address
       end
