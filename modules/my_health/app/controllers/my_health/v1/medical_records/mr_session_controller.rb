8619a0873c9244b14e716a3f5882238f3a216560
Mike Moyer <michael.moyer@va.gov>
2024-02-29 08:18:11 -0500

    MHV-55245 Handle PatiendNotFound in the session calls

--- modules/my_health/app/controllers/my_health/v1/medical_records/mr_session_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/medical_records/mr_session_controller.rb
@@ -7,11 +7,15 @@ module MyHealth
         def create
           client
           head :no_content
+        rescue ::MedicalRecords::PatientNotFound
+          render body: nil, status: :accepted
         end
 
         def status
           resource = phrmgr_client.get_phrmgr_status
           render json: resource.to_json
+        rescue ::MedicalRecords::PatientNotFound
+          render body: nil, status: :accepted
         end
       end
     end
