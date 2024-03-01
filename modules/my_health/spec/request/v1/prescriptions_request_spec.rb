17bf331935440912f03f0c0073dcb100578a4860
Tony Gravell <146013972+agravell047@users.noreply.github.com>
2024-03-01 11:02:06 -0500

    MHV-55039-Update-Refill-Logic (#15747)
    * updated renewable rx logic
    
    * Added active: submitted status

--- modules/my_health/spec/request/v1/prescriptions_request_spec.rb
+++ modules/my_health/spec/request/v1/prescriptions_request_spec.rb
@@ -134,9 +134,17 @@ RSpec.describe 'prescriptions', type: :request do
         VCR.use_cassette('rx_client/prescriptions/gets_a_list_of_refillable_prescriptions') do
           get '/my_health/v1/prescriptions/list_refillable_prescriptions'
         end
+        six_months_from_today = Time.zone.today - 6.months
+        zero_date = Date.new(0, 1, 1)
+
         response_data = JSON.parse(response.body)['data']
         response_data.each do |prescription|
-          if prescription['is_refillable'] || (prescription['refill_status'] == 'active' && prescription['refill_remaining']&.zero?)
+          sorted_dispensed_date = prescription['rxRfRecords']&.dig(0, 1, 0) || prescription['dispensedDate']
+          if prescription['isRefillable'] || ['Active', 'Active: Submitted'].include?(prescription['dispStatus']) ||
+             (%w[Expired Discontinued].include?(prescription['dispStatus']) &&
+             sorted_dispensed_date >= six_months_from_today &&
+             sorted_dispensed_date != zero_date)
+
             expect(prescription).to be_included
           end
         end
