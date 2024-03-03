4ad7932262047b2a1b46c63acfff3f6d2f183ae0
AJ Magdub <ahmed.magdub@va.gov>
2024-03-03 10:22:58 -0700

    refactored

--- modules/vaos/app/exceptions/vaos/exceptions/backend_service_exception.rb
+++ modules/vaos/app/exceptions/vaos/exceptions/backend_service_exception.rb
@@ -35,7 +35,8 @@ module VAOS
       def response_values
         {
           detail: detail(@env.body),
-          source: { vamf_url: anonymize_icn(@env.url), vamf_body: @env.body, vamf_status: @env.status }
+          source: { vamf_url: VAOS::Anonymizers.anonymize_uri_icn(@env.url), vamf_body: @env.body,
+                    vamf_status: @env.status }
         }
       end
 
@@ -50,27 +51,6 @@ module VAOS
         end
       rescue
         body
-      end
-
-      # Anonymizes the ICN present in a given URI object by substituting a SHA256 digest for the ICN.
-      # If an ICN is not present in the URL,  it would simply return the original URI.
-      #
-      # @param url [URI::Generic] URI in which ICN needs to be anonymized.
-      #
-      # @return [URI::Generic] URI with anonymized ICN (If present), original URI otherwise.
-      #
-      def anonymize_icn(uri)
-        return nil if uri.nil?
-
-        # Extract the patient ICN part from the URL
-        url = uri.to_s
-        match = url[/(\d{10}V\d{6})/]
-
-        return uri unless match
-
-        digest = Digest::SHA256.hexdigest(match)
-        url.gsub!(match, digest)
-        URI(url)
       end
       # rubocop:enable Style/MutableConstant
     end
