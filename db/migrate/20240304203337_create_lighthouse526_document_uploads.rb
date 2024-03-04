d079b55e9288ae9ca9781be9847a102e49eb19f1
Nathan Burgess <burgess.nathan28@gmail.com>
2024-03-04 15:45:38 -0500

    Create Lighthouse526DocumentUploads table
    Create table for tracking the status of a Form 526 supplemental document we have uploaded to Lighthouse

--- db/migrate/20240304203337_create_lighthouse526_document_uploads.rb
+++ db/migrate/20240304203337_create_lighthouse526_document_uploads.rb
@@ -1,16 +1,17 @@
-class CreateLighthouseDocumentUploads < ActiveRecord::Migration[7.0]
+class CreateLighthouse526DocumentUploads < ActiveRecord::Migration[7.0]
   def change
-    create_table :lighthouse_document_uploads do |t|
+    create_table :lighthouse526_document_uploads do |t|
       t.references :form526_submission, foreign_key: true, null: false
       t.references :form_attachment, foreign_key: true
       t.string :lighthouse_document_request_id, null: false
-      t.string :aasm_state
+      t.string :aasm_state, index: true
       t.string :document_type
       t.datetime :lighthouse_processing_started_at
       t.datetime :lighthouse_processing_ended_at
       t.jsonb :error_message
+      t.jsonb :last_status_response
 
       t.timestamps
-    end    
+    end
   end
 end
