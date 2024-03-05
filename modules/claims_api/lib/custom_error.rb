893aed97da1f7c6ebc858b13545d6e8155a0662f
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-03-05 13:35:11 -0700

    Aligns hash keys to over Claims Benefits error objects.

--- modules/claims_api/lib/custom_error.rb
+++ modules/claims_api/lib/custom_error.rb
@@ -10,15 +10,16 @@ module ClaimsApi
 
     def build_error # rubocop:disable Metrics/MethodLength
       get_status
+      get_code
       if @error == Faraday::ConnectionFailed || @error == Faraday::ParsingError ||
          @error == Faraday::NilStatusError || @error == Faraday::TimeoutError ||
          @error == ::Common::Exceptions::BackendServiceException ||
          @error == ::Common::Exceptions::ExternalServerInternalServerError ||
          @error == ::Common::Exceptions::BadGateway || @error == Faraday::SSLError ||
-         @error == Faraday::ServerError || @status.to_s.start_with?('5')
-        errors = { errors: [{ 'key' => 'Service Exception',
+         @error == Faraday::ServerError
+        errors = { errors: [{ 'title' => 'Service Exception',
                               'detail' => 'A re-tryable error has occurred, original_error: ' \
-                                          "#{@error}.", status: '500' }] }
+                                          "#{@error}.", status: @status, code: @code }] }
         log_outcome_for_claims_api(errors)
         raise ::Common::Exceptions::ServiceError, errors
 
@@ -26,17 +27,18 @@ module ClaimsApi
             @error == Faraday::ConflictError || @error == Faraday::ForbiddenError ||
             @error == Faraday::ProxyAuthError || @error == Faraday::ResourceNotFound ||
             @error == Faraday::UnauthorizedError || @error == Faraday::UnprocessableEntityError ||
-            @error == Faraday::ClientError || @status.to_s.start_with?('4')
+            @error == Faraday::ClientError
 
-        errors = { errors: [{ 'key' => 'Client error',
-                              'detail' => 'A client exception has occurred, job will not be re-tried.' \
-                                          "original_error: #{@error}.", status: '400' }] }
+        errors = { errors: [{ 'title' => 'Client error',
+                              'detail' => 'A client exception has occurred, job will not be re-tried. ' \
+                                          "original_error: #{@error}.", status: '400', code: '400' }] }
         log_outcome_for_claims_api(errors)
         raise ::Common::Exceptions::BadRequest, errors
       else
-        errors = { errors: [{ 'key' => 'Unknown error',
+        errors = { errors: [{ 'title' => 'Unknown error',
                               'detail' => 'An unknown error has occurred, and the custom_error file may ' \
-                                          "need to be modified. original_error: #{@error}.", status: '500' }] }
+                                          "need to be modified. original_error: #{@error}.", status: @status,
+                              code: @code }] }
         log_outcome_for_claims_api(errors)
         raise ::Common::Exceptions::ServiceError, errors
       end
@@ -50,12 +52,23 @@ module ClaimsApi
     end
 
     def get_status
-      if @error.respond_to?(:status)
-        @status = @error.status
-      elsif @error.respond_to?(:status_code)
-        @status = @error.status_code
-      end
-      @status
+      @status = if @error.respond_to?(:status)
+                  @error.status
+                elsif @error.respond_to?(:status_code)
+                  @error.status_code
+                else
+                  '500'
+                end
+    end
+
+    def get_code
+      @code = if @error.respond_to?(:va900?)
+                'VA900'
+              elsif @error.respond_to?(:status_code)
+                @error.status_code
+              else
+                @status
+              end
     end
   end
 end
