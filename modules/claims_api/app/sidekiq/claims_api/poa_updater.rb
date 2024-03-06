eb6f23ecad17f92f52411e0e64d99bd93490d1d1
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-03-06 13:10:16 -0600

    API-34542 2122a update poa fix
    * 2122a V2 form removed the organzation object so we need to get the poaCode from the representative object.
    * Adds some tests to track this update
    * Refactors diekiq job to check for both
    * Adds a check for the poaCode to make sure this cannot silently fail in the backround in the event of any other changes
     Changes to be committed:
    	modified:   modules/claims_api/app/sidekiq/claims_api/poa_updater.rb
    	modified:   modules/claims_api/spec/sidekiq/poa_updater_spec.rb

--- modules/claims_api/app/sidekiq/claims_api/poa_updater.rb
+++ modules/claims_api/app/sidekiq/claims_api/poa_updater.rb
@@ -14,13 +14,17 @@ module ClaimsApi
       ssn = poa_form.auth_headers['va_eauth_pnid']
       file_number = service.people.find_by_ssn(ssn)[:file_nbr] # rubocop:disable Rails/DynamicFindBy
 
-      response = service.vet_record.update_birls_record(
-        file_number:,
-        ssn:,
-        poa_code: poa_form.form_data['serviceOrganization']['poaCode']
-      )
+      poa_code = extract_poa_code(poa_form.form_data)
 
-      if response[:return_code] == 'BMOD0001'
+      unless poa_code.nil?
+        response = service.vet_record.update_birls_record(
+          file_number:,
+          ssn:,
+          poa_code:
+        )
+      end
+
+      if response && response[:return_code] == 'BMOD0001'
         poa_form.status = ClaimsApi::PowerOfAttorney::UPDATED
         # Clear out the error message if there were previous failures
         poa_form.vbms_error_message = nil if poa_form.vbms_error_message.present?
@@ -29,15 +33,33 @@ module ClaimsApi
 
         ClaimsApi::PoaVBMSUpdater.perform_async(poa_form.id) if enable_vbms_access?(poa_form:)
       else
+        error_response = set_error_message(response, power_of_attorney_id)
+
         poa_form.status = ClaimsApi::PowerOfAttorney::ERRORED
-        poa_form.vbms_error_message = "BGS Error: update_birls_record failed with code #{response[:return_code]}"
-        ClaimsApi::Logger.log('poa', poa_id: poa_form.id, detail: 'BIRLS Failed', error: response[:return_code])
+        poa_form.vbms_error_message = error_response
+        ClaimsApi::Logger.log('poa', poa_id: poa_form.id, detail: 'BIRLS Failed', error: error_response)
       end
 
       poa_form.save
     end
 
     private
+
+    def extract_poa_code(poa_form_data)
+      if poa_form_data.key?('serviceOrganization')
+        poa_form_data['serviceOrganization']['poaCode']
+      elsif poa_form_data.key?('representative') # V2 2122a
+        poa_form_data['representative']['poaCode']
+      end
+    end
+
+    def set_error_message(response, power_of_attorney_id)
+      if response&.[](:return_code)
+        "BGS Error: update_birls_record failed with code #{response[:return_code]}"
+      else
+        "No POA code found in form #{power_of_attorney_id}"
+      end
+    end
 
     def enable_vbms_access?(poa_form:)
       poa_form.form_data['recordConsent'] && poa_form.form_data['consentLimits'].blank?
