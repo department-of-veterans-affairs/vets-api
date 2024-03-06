9e28e06b8f1f5bd455e6d99242a9dda275ee71a4
Athif Wulandana <athif.wulandana@athifwulandana-dyl9g7mq2f.lan>
2024-03-06 15:51:11 -0600

    Fix travel pay controller specs per feedback

--- modules/travel_pay/spec/controllers/claims_controller_spec.rb
+++ modules/travel_pay/spec/controllers/claims_controller_spec.rb
@@ -4,14 +4,16 @@ require 'rails_helper'
 
 RSpec.describe TravelPay::ClaimsController, type: :request do
   let(:user) { build(:user) }
+  let(:client) { instance_double(TravelPay::Client) }
 
   before do
+    allow(TravelPay::Client).to receive(:new).and_return(client)
     veis_response = double
     allow(veis_response).to receive(:body).and_return( {'access_token' => 'sample_token'} )
-    allow_any_instance_of(TravelPay::Client).to receive(:request_veis_token).and_return(veis_response)
+    allow(client).to receive(:request_veis_token).and_return(veis_response)
     btsss_ping_response = double
-    allow(btsss_ping_response).to receive(:status).and_return('online')
-    allow_any_instance_of(TravelPay::Client).to receive(:ping).and_return(btsss_ping_response)
+    allow(btsss_ping_response).to receive(:status).and_return(200)
+    allow(client).to receive(:ping).and_return(btsss_ping_response)
     sign_in(user)
     Flipper.disable :travel_pay_power_switch
   end
@@ -23,15 +25,16 @@ RSpec.describe TravelPay::ClaimsController, type: :request do
       end
 
       it 'requests a token and sends a ping to BTSSS' do
+        expect(client).to receive(:ping)
         get '/travel_pay/claims'
-        expect(response.body).to include('Received ping from upstream server with status online.')
+        expect(response.body).to include('ping')
       end
     end
 
     context 'the feature switch is disabled' do
       it 'raises the proper error' do
         get '/travel_pay/claims'
-        expect(response).to have_http_status(503)
+        expect(response).to have_http_status(:service_unavailable)
         expect(response.body).to include('This feature has been temporarily disabled')
       end
     end
