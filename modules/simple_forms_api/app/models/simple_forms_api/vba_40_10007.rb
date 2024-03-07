12d206bec9d336046262d3990f3520e5af7790e5
Kenny Santiago <50601724+ksantiagoBAH@users.noreply.github.com>
2024-03-06 21:06:54 -0600

    MBMS-59988 Updated get_attachments function

--- modules/simple_forms_api/app/models/simple_forms_api/vba_40_10007.rb
+++ modules/simple_forms_api/app/models/simple_forms_api/vba_40_10007.rb
@@ -58,17 +58,18 @@ module SimpleFormsApi
 
     def get_attachments
       attachments = []
-
-      supporting_documents = @data['preneed_attachments']
-
+    
+      supporting_documents = @data['application']['preneed_attachments']
+    
       if supporting_documents
-        confirmation_codes = []
-        supporting_documents&.map { |doc| confirmation_codes << doc['confirmation_code'] }
-
-        PersistentAttachment.where(guid: confirmation_codes).map { |attachment| attachments << attachment.to_pdf }
+        confirmation_codes = supporting_documents.map { |doc| doc['confirmation_code'] }
+    
+        PersistentAttachment.where(guid: confirmation_codes).each do |attachment|
+          attachments << attachment.to_pdf
+        end
       end
-
+    
       attachments
-    end
+    end      
   end
 end
