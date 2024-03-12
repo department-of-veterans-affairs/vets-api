a0eebd3af67d85a3039bed0f53759090582c3632
Adrian Rollett <acrollet@gmail.com>
2024-03-12 14:51:36 +0000

    Add ruby lsp ext after start

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# Start postgres & redis.
 nohup bash -c 'docker-compose -f docker-compose-deps.yml up >> log/deps.log 2>&1 &'
 
-timeout 90 sh -c 'until pg_isready -h localhost -p 54320; do sleep 1; done'
-
 bundle install
+code --install-extension "Shopify.ruby-lsp"
+
+# Wait for postgres to be ready before running setup.
+timeout 90 sh -c 'until pg_isready -h localhost -p 54320; do sleep 1; done'
 ./bin/setup
 
 nohup bash -c 'foreman start -m all=1,clamd=0,freshclam=0 >> log/foreman.log 2>&1 &'
