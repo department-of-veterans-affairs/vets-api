db356da581edc2d875ee3b1582dc21b3db73730a
Adrian Rollett <acrollet@gmail.com>
2024-03-13 19:36:41 +0000

    Belt & suspenders

--- .devcontainer/start-services.sh
+++ .devcontainer/start-services.sh
@@ -11,10 +11,14 @@ POSTGRES_UID=$(docker exec -it ${POSTGRES_CONTAINER} id -u postgres | tr -d '\r'
 ./bin/setup
 docker-compose -f docker-compose-deps.yml down
 
+# Ensure permissions are set correctly for postgres user in container.
+echo "Setting file ownership for postgres data to uid: ${POSTGRES_UID}"
+sudo chown -R ${POSTGRES_UID} data
+
 nohup bash -c 'docker-compose -f docker-compose-deps.yml up >> log/deps.log 2>&1 &'
 # Wait for postgres to be ready.
 timeout 90 sh -c 'until pg_isready -h localhost -p 54320; do sleep 1; done'
 
-# Ensure permissions are set correctly for postgres user in container.
+# Re-ensure permissions are set correctly for postgres user in container.
 echo "Setting file ownership for postgres data to uid: ${POSTGRES_UID}"
 sudo chown -R ${POSTGRES_UID} data
