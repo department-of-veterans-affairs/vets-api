a0949315b0d4f56ccbd4290a5c0a7c75532b60ac
Adrian Rollett <acrollet@gmail.com>
2024-03-13 16:34:20 +0000

    Add output for permissions set

--- .devcontainer/start-services.sh
+++ .devcontainer/start-services.sh
@@ -15,4 +15,5 @@ timeout 90 sh -c 'until pg_isready -h localhost -p 54320; do sleep 1; done'
 # Ensure permissions are set correctly for postgres user in container.
 POSTGRES_CONTAINER=$( docker ps|grep postgis|awk '{print $1}' )
 POSTGRES_UID=$(docker exec -it ${POSTGRES_CONTAINER} id -u postgres | tr -d '\r' )
+echo "Setting file ownership for postgres data to uid: ${POSTGRES_UID}"
 sudo chown -R ${POSTGRES_UID} data
