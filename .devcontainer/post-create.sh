79eff29c3f0ba96dd7f40168fc6e83e6f3f419bb
Adrian Rollett <acrollet@gmail.com>
2024-03-13 14:06:14 +0000

    More QoL improvements

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -37,4 +37,7 @@ betamocks:
 EOT
 fi
 
+# Add welcome message
+sudo cp .devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+
 sh .devcontainer/start-services.sh
--- .devcontainer/start-services.sh
+++ .devcontainer/start-services.sh
@@ -7,16 +7,12 @@ if ! pg_isready -h localhost -p 54320; then
   nohup bash -c 'docker-compose -f docker-compose-deps.yml up >> log/deps.log 2>&1 &'
 fi
 
-bundle install
-
 # Wait for postgres to be ready.
 timeout 90 sh -c 'until pg_isready -h localhost -p 54320; do sleep 1; done'
 
+# Ensure permissions are set correctly for postgres user in container.
+POSTGRES_CONTAINER=$( docker ps|grep postgis|awk '{print $1}' )
+POSTGRES_UID=$(docker exec -it ${POSTGRES_CONTAINER} id -u postgres | tr -d '\r' )
+sudo chown -R ${POSTGRES_UID} data
+
 ./bin/setup
-
-if ! curl -s http://localhost:3000|grep -q 'Welcome to the va.gov API'; then
-  nohup bash -c 'foreman start -m all=1,clamd=0,freshclam=0 >> log/foreman.log 2>&1 &'
-fi
-
-# Wait for the API to be ready.
-timeout 120 sh -c 'until curl -s http://localhost:3000|grep -q "Welcome to the va.gov API"; do sleep 1; done'
