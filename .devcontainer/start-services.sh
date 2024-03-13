2a8cac4c3a2e057b5d191b074f79080ace1ea9e4
Adrian Rollett <acrollet@gmail.com>
2024-03-13 21:05:19 +0000

    Simplify startup

--- .devcontainer/start-services.sh
+++ .devcontainer/start-services.sh
@@ -2,19 +2,14 @@
 
 mkdir -p log
 
-docker-compose -f docker-compose-deps.yml up &
+nohup bash -c 'docker-compose -f docker-compose-deps.yml up >> log/deps.log 2>&1 &'
 timeout 90 sh -c 'until pg_isready -h localhost -p 54320; do sleep 1; done'
 
 POSTGRES_CONTAINER=$( docker ps|grep postgis|awk '{print $1}' )
 POSTGRES_UID=$(docker exec -it ${POSTGRES_CONTAINER} id -u postgres | tr -d '\r' )
 
 ./bin/setup
-docker-compose -f docker-compose-deps.yml down
 
 # Ensure permissions are set correctly for postgres user in container.
 echo "Setting file ownership for postgres data to uid: ${POSTGRES_UID}"
 sudo chown -R ${POSTGRES_UID} data
-
-nohup bash -c 'docker-compose -f docker-compose-deps.yml up >> log/deps.log 2>&1 &'
-# Wait for postgres to be ready.
-timeout 90 sh -c 'until pg_isready -h localhost -p 54320; do sleep 1; done'
