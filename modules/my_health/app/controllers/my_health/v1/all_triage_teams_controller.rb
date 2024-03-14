3e9301c7df07c2581bb45d854145bbb4741c38f2
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2024-03-14 16:41:34 -0400

    adding RecordNotFound exceptions

--- modules/my_health/app/controllers/my_health/v1/all_triage_teams_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/all_triage_teams_controller.rb
@@ -5,7 +5,7 @@ module MyHealth
     class AllTriageTeamsController < SMController
       def index
         resource = client.get_all_triage_teams(@current_user.uuid, use_cache?)
-        raise Common::Exceptions::InternalServerError if resource.blank?
+        raise Common::Exceptions::RecordNotFound, "Triage teams for user ID #{@current_user.uuid} not found" if resource.blank?
 
         resource = resource.sort(params.permit(:sort)[:sort])
 
