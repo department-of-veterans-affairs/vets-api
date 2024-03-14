d60b72279f5f2c2c14273d1c8ffd02d587de0288
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-03-14 11:26:40 -0400

    Limit names length to 30 characters for 26-4555 (#15930)

--- modules/simple_forms_api/spec/models/vba_26_4555_spec.rb
+++ modules/simple_forms_api/spec/models/vba_26_4555_spec.rb
@@ -3,28 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe 'SimpleFormsApi::VBA264555' do
-  describe 'veteran middle name' do
-    it 'limits the length to 40 characters' do
-      middle_name = 'Wolfeschlegelsteinhausenbergerdorffwelchevoralternwarengewissenhaftschaferswessenschafe
+  describe 'veteran name' do
+    it 'limits the length to 30 characters' do
+      name = 'Wolfeschlegelsteinhausenbergerdorffwelchevoralternwarengewissenhaftschaferswessenschafe
       warenwohlgepflegeundsorgfaltigkeitbeschutzenvonangreifendurchihrraubgierigfeindewelchevoralternzwolftausend
       jahresvorandieerscheinenvanderersteerdemenschderraumschiffgebrauchlichtalsseinursprungvonkraftgestartsein
       langefahrthinzwischensternartigraumaufdersuchenachdiesternwelchegehabtbewohnbarplanetenkreisedrehensichund
       wohinderneurassevonverstandigmenschlichkeitkonntefortpflanzenundsicherfreuenanlebenslanglichfreudeundruhemit
       nichteinfurchtvorangreifenvonandererintelligentgeschopfsvonhinzwischensternartigraum'
-      shortened_name = 'Wolfeschlegelsteinhausenbergerdorffwelch'
+      shortened_name = 'Wolfeschlegelsteinhausenberger'
 
       form = SimpleFormsApi::VBA264555.new(
         {
           'veteran' => {
             'full_name' => {
-              'middle' => middle_name
+              'first' => name,
+              'middle' => name,
+              'last' => name
             }
           }
         }
       )
 
-      expect(shortened_name.length).to eq 40
+      expect(shortened_name.length).to eq 30
+      expect(form.as_payload[:veteran][:fullName][:first]).to eq shortened_name
       expect(form.as_payload[:veteran][:fullName][:middle]).to eq shortened_name
+      expect(form.as_payload[:veteran][:fullName][:last]).to eq shortened_name
     end
   end
 end
