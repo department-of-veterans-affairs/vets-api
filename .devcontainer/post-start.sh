b950f0933dab6dae1034a895b402d600e89c991e
Adrian Rollett <acrollet@gmail.com>
2024-03-14 18:09:08 +0000

    Fix foreman output

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -2,4 +2,4 @@
 
 sudo /etc/init.d/postgresql restart
 pg_isready -t 60
-nohup bash -c 'foreman start -m all=1,clamd=0,freshclam=0 >> log/foreman.log 2>&1 &'
+nohup bash -c 'foreman start -m all=1,clamd=0,freshclam=0' >> log/foreman.log 2>&1 &
