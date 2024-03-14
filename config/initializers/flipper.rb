19ba6ffe0d02c4bb025befdf5f739d674fe0531a
Oren Mittman <nihil2501@gmail.com>
2024-03-14 17:59:23 -0400

    stop reconfiguring flipper on reload

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -14,55 +14,53 @@ Rails.application.configure do
   config.flipper.test_help = false
 end
 
-Rails.application.reloader.to_prepare do
-  Flipper.configure do |config|
-    config.default do
-      activerecord_adapter = Flipper::Adapters::ActiveRecord.new
-      cache = Rails.cache
+Flipper.configure do |config|
+  config.default do
+    activerecord_adapter = Flipper::Adapters::ActiveRecord.new
+    cache = Rails.cache
 
-      # Flipper settings will be stored in postgres and cached in memory for 1 minute in production/staging
-      cached_adapter = Flipper::Adapters::ActiveSupportCacheStore.new(activerecord_adapter, cache, expires_in: 1.minute)
-      instrumented = Flipper::Adapters::Instrumented.new(cached_adapter, instrumenter: ActiveSupport::Notifications)
+    # Flipper settings will be stored in postgres and cached in memory for 1 minute in production/staging
+    cached_adapter = Flipper::Adapters::ActiveSupportCacheStore.new(activerecord_adapter, cache, expires_in: 1.minute)
+    instrumented = Flipper::Adapters::Instrumented.new(cached_adapter, instrumenter: ActiveSupport::Notifications)
 
-      Flipper.new(instrumented, instrumenter: ActiveSupport::Notifications)
-    end
+    Flipper.new(instrumented, instrumenter: ActiveSupport::Notifications)
   end
-
-  # Modify Flipper::UI::Configuration to accept a custom view path.
-  Flipper::UI::Configuration.prepend(FlipperExtensions::ConfigurationPatch)
-
-  Flipper::UI.configure do |config|
-    config.custom_views_path = Rails.root.join('lib', 'flipper', 'views')
-  end
-
-  FLIPPER_ACTOR_USER = 'user'
-  FLIPPER_ACTOR_STRING = 'cookie_id'
-
-  Flipper::UI.configuration.feature_creation_enabled = false
-  # Make sure that each feature we reference in code is present in the UI, as long as we have a Database already
-  begin
-    FLIPPER_FEATURE_CONFIG['features'].each do |feature, feature_config|
-      unless Flipper.exist?(feature)
-        Flipper.add(feature)
-
-        # Default features to enabled for test and those explicitly set for development
-        if Rails.env.test? || (Rails.env.development? && feature_config['enable_in_development'])
-          Flipper.enable(feature)
-        end
-      end
-
-      # Enable features on dev-api.va.gov if they are set to enable_in_development
-      Flipper.enable(feature) if Settings.vsp_environment == 'development' && feature_config['enable_in_development']
-    end
-
-    removed_features = Flipper.features.collect(&:name) - FLIPPER_FEATURE_CONFIG['features'].keys
-    Rails.logger.warn "Consider removing features no longer in config/features.yml: #{removed_features.join(', ')}"
-  rescue
-    # make sure we can still run rake tasks before table has been created
-    nil
-  end
-
-  # Modify Flipper::UI::Action to use custom views if they exist
-  # and to add descriptions and types for features.
-  Flipper::UI::Action.prepend(FlipperExtensions::ActionPatch)
 end
+
+# Modify Flipper::UI::Configuration to accept a custom view path.
+Flipper::UI::Configuration.prepend(FlipperExtensions::ConfigurationPatch)
+
+Flipper::UI.configure do |config|
+  config.custom_views_path = Rails.root.join('lib', 'flipper', 'views')
+end
+
+FLIPPER_ACTOR_USER = 'user'
+FLIPPER_ACTOR_STRING = 'cookie_id'
+
+Flipper::UI.configuration.feature_creation_enabled = false
+# Make sure that each feature we reference in code is present in the UI, as long as we have a Database already
+begin
+  FLIPPER_FEATURE_CONFIG['features'].each do |feature, feature_config|
+    unless Flipper.exist?(feature)
+      Flipper.add(feature)
+
+      # Default features to enabled for test and those explicitly set for development
+      if Rails.env.test? || (Rails.env.development? && feature_config['enable_in_development'])
+        Flipper.enable(feature)
+      end
+    end
+
+    # Enable features on dev-api.va.gov if they are set to enable_in_development
+    Flipper.enable(feature) if Settings.vsp_environment == 'development' && feature_config['enable_in_development']
+  end
+
+  removed_features = Flipper.features.collect(&:name) - FLIPPER_FEATURE_CONFIG['features'].keys
+  Rails.logger.warn "Consider removing features no longer in config/features.yml: #{removed_features.join(', ')}"
+rescue
+  # make sure we can still run rake tasks before table has been created
+  nil
+end
+
+# Modify Flipper::UI::Action to use custom views if they exist
+# and to add descriptions and types for features.
+Flipper::UI::Action.prepend(FlipperExtensions::ActionPatch)
