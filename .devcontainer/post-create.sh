1a2b1a1b4b2226bbafc5774bdbeda9ba4a73327f
Adrian Rollett <acrollet@gmail.com>
2024-03-14 15:52:51 +0000

    Add native setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,43 +1,43 @@
 #!/bin/sh
 
-# Switch to vets-api ruby version
-asdf install ruby $( cat .ruby-version )
-asdf global ruby $( cat .ruby-version )
+# Add welcome message
+sudo cp .devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 
 git clone https://github.com/department-of-veterans-affairs/vets-api-mockdata.git ../vets-api-mockdata
 
 sudo apt update
-sudo apt install -y libpq-dev pdftk shared-mime-info postgresql-client
+sudo apt install -y libpq-dev pdftk shared-mime-info postgresql-11-postgis-3
 
 gem install bundler
 NUM_CORES=$( cat /proc/cpuinfo | grep '^processor'|wc -l )
 bundle config --global jobs `expr $NUM_CORES - 1`
-bundle install
 
 # Update default test DB config (because this config overrides the local settings when running tests)
-sed -i 's|^test_database_url: .*$|test_database_url: postgis://postgres:password@localhost:54320/vets_api_test?pool=4|' config/settings.yml
+# FIXME: necessary?
+# sed -i 's|^test_database_url: .*$|test_database_url: postgis://postgres:password@localhost:54320/vets_api_test?pool=4|' config/settings.yml
 
-# Add hybrid service config
+# Add service config
 if [ ! -f config/settings.local.yml ]; then
   cp config/settings.local.yml.example config/settings.local.yml
   cat <<EOT >> config/settings.local.yml
-database_url: postgis://postgres:password@localhost:54320/vets_api_development?pool=4
-test_database_url: postgis://postgres:password@localhost:54320/vets_api_test?pool=4
+database_url: postgis://postgres:password@localhost:5432/vets_api_development?pool=4
+test_database_url: postgis://postgres:password@localhost:5432/vets_api_test?pool=4
 
 redis:
   host: localhost
-  port: 63790
+  port: 6379
   app_data:
-    url: redis://localhost:63790
+    url: redis://localhost:6379
   sidekiq:
-    url: redis://localhost:63790
+    url: redis://localhost:6379
 
 betamocks:
   cache_dir: ../vets-api-mockdata
 EOT
 fi
 
-# Add welcome message
-sudo cp .devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
-
-sh .devcontainer/start-services.sh
+mkdir -p log
+sudo /etc/init.d/postgresql restart
+pg_isready -t 60
+sudo -u root sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'password';"
+./bin/setup
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-sh .devcontainer/start-services.sh
+sudo /etc/init.d/postgresql restart
+pg_isready -t 60
+nohup bash -c 'foreman start -m all=1,clamd=0,freshclam=0 >> log/foreman.log 2>&1 &'
