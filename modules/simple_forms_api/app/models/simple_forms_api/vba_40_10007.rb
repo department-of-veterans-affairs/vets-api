a8f16166d97aad604e33684523fdfc9aaf10ffb5
Kenny Santiago <50601724+ksantiagoBAH@users.noreply.github.com>
2024-03-14 07:19:07 -0500

    MBMS-60237 Created new branch

--- modules/simple_forms_api/app/models/simple_forms_api/vba_40_10007.rb
+++ modules/simple_forms_api/app/models/simple_forms_api/vba_40_10007.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'json'
 
 module SimpleFormsApi
   class VBA4010007
@@ -46,6 +47,22 @@ module SimpleFormsApi
               end
 
       value.to_s # Convert nil to an empty string
+    end
+
+    def find_cemetery_by_id(cemetery_id)
+      file_path = 'modules/simple_forms_api/app/json/cemeteries.json'
+      file_content = File.read(file_path)
+      cemeteries = JSON.parse(file_content)
+
+      cemetery = cemeteries['data']&.find do |entry|
+        entry['attributes']&.dig('cemetery_id') == cemetery_id
+      end
+
+      if cemetery
+        cemetery['attributes']['name']
+      else
+        'Cemetery not found.'
+      end
     end
 
     def track_user_identity(confirmation_number); end
