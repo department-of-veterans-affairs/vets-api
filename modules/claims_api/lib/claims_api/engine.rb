545ddff376ed1d69924b808d53c3f518e265a6bd
Oren Mittman <nihil2501@gmail.com>
2024-03-14 21:01:28 -0400

    tiny legibility improvemnt to claims api engine

--- modules/claims_api/lib/claims_api/engine.rb
+++ modules/claims_api/lib/claims_api/engine.rb
@@ -12,13 +12,18 @@ module ClaimsApi
         end
       end
     end
+
     config.generators do |g|
       g.test_framework :rspec, view_specs: false
       g.fixture_replacement :factory_bot
       g.factory_bot dir: 'spec/factories'
     end
+
     initializer 'claims_api.factories', after: 'factory_bot.set_factory_paths' do
-      FactoryBot.definition_file_paths << File.expand_path('../../spec/factories', __dir__) if defined?(FactoryBot)
+      if defined?(FactoryBot)
+        path = File.expand_path('../../spec/factories', __dir__)
+        FactoryBot.definition_file_paths << path
+      end
     end
   end
 end
