a97af33afd70e2d784b2eaa60a6d917c12052e5d
Kristen Brown <11942904+kristen-brown@users.noreply.github.com>
2024-03-15 16:40:18 -0400

    API-33857: Modify specs to rely less on factory

--- modules/va_forms/spec/factories/forms.rb
+++ modules/va_forms/spec/factories/forms.rb
@@ -10,14 +10,6 @@ FactoryBot.define do
     last_revision_on { Time.zone.today }
     pages { 2 }
     valid_pdf { true }
-    last_sha256_change { Date.new(2024, 3, 15) }
-    change_history do
-      { 'versions' =>
-         [{ 'sha256' => 'a8ba72e148e15e4e03476bb7fbbdbc4facd43ceb52d10eb2f605a8aa8b4bad6a',
-            'revision_on' => '2024-01-08' },
-          { 'sha256' => '68b6d817881be1a1c8f323a9073a343b81d1c5a6e03067f27fe595db77645c22',
-            'revision_on' => '2024-03-15' }] }
-    end
     sequence(:sha256) { |n| "abcd#{n}" }
 
     trait :has_been_deleted do
--- modules/va_forms/spec/requests/v0/forms_request_spec.rb
+++ modules/va_forms/spec/requests/v0/forms_request_spec.rb
@@ -7,9 +7,18 @@ RSpec.describe 'VA Forms', type: :request do
 
   let!(:form) do
     create(:va_form)
-    create(:va_form, form_name: '527', row_id: '4157')
+    create(:va_form, form_name: '527', row_id: '4157', last_sha256_change: Date.new(2024, 3, 15))
     create(:deleted_va_form)
-    create(:va_form, form_name: '21-2001', row_id: '4158')
+    create(:va_form,
+           form_name: '21-2001',
+           row_id: '4158',
+           change_history: {
+             'versions' =>
+               [{ 'sha256' => 'a8ba72e148e15e4e03476bb7fbbdbc4facd43ceb52d10eb2f605a8aa8b4bad6a',
+                  'revision_on' => '2024-01-08' },
+                { 'sha256' => '68b6d817881be1a1c8f323a9073a343b81d1c5a6e03067f27fe595db77645c22',
+                  'revision_on' => '2024-03-15' }]
+           })
   end
   let(:base_url) { '/services/va_forms/v0/forms' }
   let(:inflection_header) { { 'X-Key-Inflection' => 'camel' } }
@@ -105,10 +114,7 @@ RSpec.describe 'VA Forms', type: :request do
     it 'returns the form version history' do
       get "#{base_url}/#{form.form_name}"
       versions = JSON.parse(response.body)['data']['attributes']['versions']
-      expect(versions).to eql([{ 'sha256' => 'a8ba72e148e15e4e03476bb7fbbdbc4facd43ceb52d10eb2f605a8aa8b4bad6a',
-                                 'revision_on' => '2024-01-08' },
-                               { 'sha256' => '68b6d817881be1a1c8f323a9073a343b81d1c5a6e03067f27fe595db77645c22',
-                                 'revision_on' => '2024-03-15' }])
+      expect(versions).to eql(form.change_history['versions'])
     end
   end
 end
