746fbf81d1b4fe74c4e00391a55dbcf91aa929d3
Athif Wulandana <athif.wulandana@athifwulandana-dyl9g7mq2f.lan>
2024-03-18 12:34:41 -0500

    Clean up travel pay controller

--- modules/travel_pay/app/controllers/travel_pay/claims_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module TravelPay
-  class ClaimsController < ApplicationController
-    def index; end
-  end
-end
--- modules/travel_pay/config/routes.rb
+++ modules/travel_pay/config/routes.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 TravelPay::Engine.routes.draw do
-  resources :claims
   get '/pings/ping', to: 'pings#ping'
 end
