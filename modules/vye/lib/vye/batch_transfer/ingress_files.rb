885726da4302e948d78eebde90a3b89ee1254ea8
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-03-19 12:01:18 -0500

    BDN import

--- modules/vye/lib/vye/batch_transfer/ingress_files.rb
+++ modules/vye/lib/vye/batch_transfer/ingress_files.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module VYE
+module Vye
   module BatchTransfer
     module IngressFiles
       module_function
@@ -10,6 +10,18 @@ module VYE
 
       def bdn_feed_filename = BDN_FEED_FILENAME
       def tims_feed_filename = TIMS_FEED_FILENAME
+
+      def bdn_import(data)
+        data.each_line do |line|
+          parsed = BdnLineExtraction.new(line: line.chomp, result: {}, award_lines: [], awards: [])
+
+          profile = Vye::UserProfile.build(parsed.attributes[:profile])
+          info = profile.user_infos.build(parsed.attributes[:info])
+          info.address_changes.build({ origin: 'backend' }.merge(parsed.attributes[:address]))
+
+          profile.save!
+        end
+      end
     end
   end
 end
