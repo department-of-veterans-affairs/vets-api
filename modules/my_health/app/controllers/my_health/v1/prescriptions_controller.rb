303dbaf9e23952f8c1cc5ed54a07a7dcb3d02ed1
agravell047 <anthony.gravell@bylight.com>
2024-03-19 13:43:58 -0400

    broke helper file into two PRs

--- modules/my_health/app/controllers/my_health/v1/prescriptions_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/prescriptions_controller.rb
@@ -5,7 +5,6 @@ module MyHealth
     class PrescriptionsController < RxController
       include Filterable
       include MyHealth::PrescriptionHelper::Filtering
-      include MyHealth::PrescriptionHelper::Imaging
       # This index action supports various parameters described below, all are optional
       # This comment can be removed once documentation is finalized
       # @param refill_status - one refill status to filter on
@@ -74,6 +73,68 @@ module MyHealth
         image_url = get_image_uri(params[:cmopNdcNumber])
         image_data = fetch_image(image_url)
         render json: { data: image_data }
+      end
+
+      private
+
+      def fetch_and_include_images(data)
+        threads = []
+        data.each do |item|
+          cmop_ndc_number = get_cmop_value(item)
+          if cmop_ndc_number.present?
+            image_uri = get_image_uri(cmop_ndc_number)
+            threads << Thread.new(item) do |thread_item|
+              thread_item[:prescription_image] = fetch_image(image_uri)
+            rescue => e
+              puts "Error fetching image for NDC #{thread_item[:cmop_ndc_number]}: #{e.message}"
+            end
+          end
+        end
+        threads.each(&:join)
+        data
+      end
+
+      def fetch_image(image_url)
+        uri = URI.parse(image_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = (uri.scheme == 'https')
+        request = Net::HTTP::Get.new(uri.request_uri)
+        response = http.request(request)
+        if response.is_a?(Net::HTTPSuccess)
+          image_data = response.body
+          base64_image = Base64.strict_encode64(image_data)
+          "data:#{response['content-type']};base64,#{base64_image}"
+        end
+      end
+
+      def get_cmop_value(item)
+        cmop_ndc_number = nil
+        if item[:rx_rf_records].present? || item[:cmop_ndc_number].present?
+          cmop_ndc_number = if item[:rx_rf_records]&.[](0)&.[](1)&.[](0)&.key?(:cmop_ndc_number)
+                              item[:rx_rf_records][0][1][0][:cmop_ndc_number]
+                            elsif item[:cmop_ndc_number].present?
+                              item[:cmop_ndc_number]
+                            end
+        end
+        cmop_ndc_number
+      end
+
+      def get_image_uri(cmop_ndc_number)
+        folder_names = %w[1 2 3 4 5 6 7 8 9]
+        folder_name = cmop_ndc_number ? cmop_ndc_number.gsub(/^0+(?!$)/, '')[0] : ''
+        file_name = "NDC#{cmop_ndc_number}.jpg"
+        folder_name = 'other' unless folder_names.include?(folder_name)
+        image_root_uri = 'https://www.myhealth.va.gov/static/MILDrugImages/'
+        "#{image_root_uri + folder_name}/#{file_name}"
+      end
+
+      def collection_resource
+        case params[:refill_status]
+        when nil
+          client.get_all_rxs
+        when 'active'
+          client.get_active_rxs_with_details
+        end
       end
     end
   end
