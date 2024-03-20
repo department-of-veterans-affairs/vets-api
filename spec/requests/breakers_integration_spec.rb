cabe0fcef59085c47c552a8f3954052d547d1471
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-03-20 15:39:32 -0400

    Upgrade Sidekiq to 7.2.2 (#15778)
    * add testcontainers-redis to mock redis during tests
    
    * switch testcontainers-redis to mock_redis
    
    * update gemfile platforms & add redis to docker-compose-test.yml
    
    * update redis ports
    
    * change redis port
    
    * update redis docker compose test configuration
    
    * remove mock_redis
    
    * remove ports from redis service in docker-compose.test
    
    * add logging to debug
    
    * add test config to redis.yml
    
    * add back CSRF protection in spec_helper
    
    * add back mock_redis
    
    * remove Redis.exists_returns_integer which is enabled by default
    
    * remove redis::store in mock_redis
    
    * use to_hash instead of to_h
    
    * update mock_redis url & remove from spec_helper
    
    * redis flush before each
    
    * use global redis variable for Redis instance
    
    * resolve naming conflict
    
    * fix tests related to Redis mocking action for RedisClient classes
    
    * fix redis instance on mobile module test
    
    * rubocop formatting
    
    * comment cleanup
    
    * rubocop formatting
    
    * add mock_redis gem back to Gemfile.lock
    
    * update formatting
    
    ---------
    
    Co-authored-by: Steven Cumming <steven.cumming@stevencumming-cr2fcpq90d.attlocal.net>

--- spec/requests/breakers_integration_spec.rb
+++ spec/requests/breakers_integration_spec.rb
@@ -30,7 +30,6 @@ RSpec.describe 'breakers', type: :request do
     # Not clearing the breakers would cause subsequent RX calls to fail after the breaker is
     # triggered in this group.
 
-    # fakeredis/rspec has a `before` callback, but it's for the suite, not each example. Oops.
     Breakers.client.redis_connection.redis.flushdb
   end
 
