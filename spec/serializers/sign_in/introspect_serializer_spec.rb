322550110da6b2d1e699eba6c27270e30eb16678
Dick Davis <dick@hey.com>
2024-03-20 17:11:11 -0500

    Refactor introspect serializer

--- spec/serializers/sign_in/introspect_serializer_spec.rb
+++ spec/serializers/sign_in/introspect_serializer_spec.rb
@@ -3,118 +3,112 @@
 require 'rails_helper'
 
 RSpec.describe SignIn::IntrospectSerializer do
-  subject { serialize(user, serializer_class: described_class) }
+  subject(:serialized_data) { serialize(access_token, serializer_class: described_class) }
 
-  let(:user) do
-    create(:user, :loa3,
-           middle_name:, logingov_uuid:, idme_uuid:,
-           mhv_ids:, active_mhv_ids: mhv_ids, participant_id:)
-  end
-  let(:middle_name) { 'some-middle-name' }
-  let(:logingov_uuid) { 'some-logingov-uuid' }
-  let(:idme_uuid) { 'some-idme-uuid' }
-  let(:mhv_ids) { %w[123 456] }
-  let(:participant_id) { 'some-participant-id' }
-  let(:data) { JSON.parse(subject)['data'] }
-  let(:attributes) { data['attributes'] }
+  let(:access_token) { create(:access_token) }
+  let(:attributes) { JSON.parse(serialized_data)['data']['attributes'] }
 
-  before do
-    user.send(:mpi_profile).id_theft_flag = true
+  shared_examples 'serializes attribute' do |serialized_attribute, attribute|
+    it "returns the value for #{attribute}" do
+      expect(attributes[serialized_attribute.to_s]).to eq(access_token.public_send(attribute))
+    end
   end
 
-  it 'returns serialized #uuid data' do
-    expect(attributes['uuid']).to be_present
+  shared_examples 'serializes datetime attribute' do |serialized_attribute, attribute|
+    it "returns the value for #{attribute}" do
+      expect(attributes[serialized_attribute.to_s]).to eq(access_token.public_send(attribute).to_i)
+    end
   end
 
-  it 'returns serialized #first_name data' do
-    expect(attributes['first_name']).to be_present
-  end
-
-  it 'returns serialized #middle_name data' do
-    expect(attributes['middle_name']).to be_present
-  end
-
-  it 'returns serialized #last_name data' do
-    expect(attributes['last_name']).to be_present
-  end
-
-  it 'returns serialized #birth_date data' do
-    expect(attributes['birth_date']).to be_present
-  end
-
-  it 'returns serialized #email data' do
-    expect(attributes['email']).to be_present
-  end
-
-  it 'returns serialized #gender data' do
-    expect(attributes['gender']).to be_present
-  end
-
-  it 'returns serialized #idme_uuid data' do
-    expect(attributes['idme_uuid']).to be_present
-  end
-
-  it 'returns serialized #logingov_uuid data' do
-    expect(attributes['logingov_uuid']).to be_present
-  end
-
-  it 'returns serialized #icn data' do
-    expect(attributes['icn']).to be_present
-  end
-
-  it 'returns serialized #edipi data' do
-    expect(attributes['edipi']).to be_present
-  end
-
-  it 'returns serialized #active_mhv_ids data' do
-    expect(attributes['active_mhv_ids']).to be_present
-  end
-
-  it 'returns serialized #sec_id data' do
-    expect(attributes['sec_id']).to be_present
-  end
-
-  it 'returns serialized #vet360_id data' do
-    expect(attributes['vet360_id']).to be_present
-  end
-
-  it 'returns serialized #participant_id data' do
-    expect(attributes['participant_id']).to be_present
-  end
-
-  it 'returns serialized #cerner_id data' do
-    expect(attributes['cerner_id']).to be_present
-  end
-
-  it 'returns serialized #cerner_facility_ids data' do
-    expect(attributes['cerner_facility_ids']).to be_present
-  end
-
-  it 'returns serialized #vha_facility_ids data' do
-    expect(attributes['vha_facility_ids']).to be_present
-  end
-
-  it 'returns serialized #id_theft_flag data' do
-    expect(attributes['id_theft_flag']).to be_present
-  end
-
-  it 'returns serialized #authn_context data' do
-    expect(attributes['authn_context']).to be_present
-  end
-
-  describe '#verified' do
-    let(:expected_verified) { 'some-expected-verified' }
-
-    before do
-      allow(user).to receive(:loa3?).and_return(expected_verified)
+  describe '#active' do
+    context 'when access token is not expired' do
+      it 'returns data with #active set to true' do
+        expect(attributes['active']).to be_truthy
+      end
     end
 
-    it 'returns serialized #verified data' do
-      expect(attributes['verified']).to be_present
+    context 'when access token is expired' do
+      let(:access_token) { create(:access_token, expiration_time: 1.minute.ago) }
+
+      it 'returns data with #active set to false' do
+        expect(attributes['active']).to be_falsey
+      end
+    end
+  end
+
+  describe '#anti_csrf_token' do
+    include_examples 'serializes attribute', :anti_csrf_token, :anti_csrf_token
+  end
+
+  describe '#aud' do
+    include_examples 'serializes attribute', :aud, :audience
+  end
+
+  describe '#client_id' do
+    include_examples 'serializes attribute', :client_id, :client_id
+  end
+
+  describe '#exp' do
+    include_examples 'serializes datetime attribute', :exp, :expiration_time
+  end
+
+  describe '#iat' do
+    include_examples 'serializes datetime attribute', :iat, :created_time
+  end
+
+  describe '#iss' do
+    it 'serializes attribute iss' do
+      expect(attributes['iss']).to eq(SignIn::Constants::AccessToken::ISSUER)
+    end
+  end
+
+  describe '#jti' do
+    include_examples 'serializes attribute', :jti, :uuid
+  end
+
+  describe '#last_regeneration_time' do
+    include_examples 'serializes datetime attribute', :last_regeneration_time, :last_regeneration_time
+  end
+
+  describe '#parent_refresh_token_hash' do
+    include_examples 'serializes attribute', :parent_refresh_token_hash, :parent_refresh_token_hash
+  end
+
+  describe '#refresh_token_hash' do
+    include_examples 'serializes attribute', :refresh_token_hash, :refresh_token_hash
+  end
+
+  describe '#session_handle' do
+    include_examples 'serializes attribute', :session_handle, :session_handle
+  end
+
+  describe '#sub' do
+    include_examples 'serializes attribute', :sub, :user_uuid
+  end
+
+  describe '#user_attributes' do
+    context 'when access token does not include user attributes' do
+      it 'returns empty #user_attributes data' do
+        expect(attributes['user_attributes']).to be_blank
+      end
     end
 
-    it 'returns verified value that maps to existing loa3? function' do
-      expect(attributes['verified']).to eq(expected_verified)
+    context 'when access token includes user attributes' do
+      let(:access_token) { create(:access_token, client_id: client_config.client_id, user_attributes:) }
+      let(:client_config) { create(:client_config, access_token_attributes: %w[first_name last_name]) }
+      let(:user_attributes) { { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name } }
+
+      it 'returns serialized #first_name data' do
+        expect(attributes['user_attributes']['first_name']).to eq(user_attributes[:first_name])
+      end
+
+      it 'returns serialized #last_name data' do
+        expect(attributes['user_attributes']['last_name']).to eq(user_attributes[:last_name])
+      end
     end
+  end
+
+  describe '#version' do
+    include_examples 'serializes attribute', :version, :version
   end
 end
