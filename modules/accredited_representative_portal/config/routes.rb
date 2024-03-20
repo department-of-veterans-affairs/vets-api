7150edfbfeb780a73a55238ecd8fa645279d158d
Gabriel Zurita <gabezurita@gmail.com>
2024-03-19 21:11:54 -0600

    75647  add PowerOfAttorneyController#accept action (#15947)
    * arf.75647 refine ARP engine spec config
    
    * arf.75647 add PowerOfAttorneyController#accept action

--- modules/accredited_representative_portal/config/routes.rb
+++ modules/accredited_representative_portal/config/routes.rb
@@ -2,5 +2,10 @@
 
 AccreditedRepresentativePortal::Engine.routes.draw do
   namespace :v0, defaults: { format: :json } do
+    resources :power_of_attorney_requests, only: [] do
+      member do
+        post :accept
+      end
+    end
   end
 end
