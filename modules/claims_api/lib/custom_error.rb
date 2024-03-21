440006643c51c28feeda23c97004992285ba463b
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-03-21 07:43:01 -0600

    Removes lines to meet 200 line count limit.

--- modules/claims_api/lib/custom_error.rb
+++ modules/claims_api/lib/custom_error.rb
@@ -13,11 +13,9 @@ module ClaimsApi
       when Faraday::ParsingError
         raise_backend_exception('EVSS502', self.class)
       when ::Common::Exceptions::BackendServiceException
-        log_outcome_for_claims_api(@error) if @error&.original_status == 403
         raise ::Common::Exceptions::Forbidden if @error&.original_status == 403
 
         raise_backend_exception('EVSS400', self.class, @error) if @error&.original_status == 400
-        log_outcome_for_claims_api(@error) if @error&.original_status == 401
         raise ::Common::Exceptions::Unauthorized if @error&.original_status == 401
       else
         raise @error
@@ -25,11 +23,6 @@ module ClaimsApi
     end
 
     private
-
-    def log_outcome_for_claims_api(errors)
-      ClaimsApi::Logger.log('526_docker_container',
-                            detail: "claims_api-526-#{@method},  errors: #{errors}", claim: @claim&.id)
-    end
 
     def raise_backend_exception(key, source, error = nil)
       raise ::Common::Exceptions::BackendServiceException.new(
