eed3743b17c754ab6ee58bc1f87f7517ca2aec27
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-03-21 14:05:01 -0400

    add clamav host & port to settings

--- config/initializers/clamav.rb
+++ config/initializers/clamav.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 if Rails.env.development?
-  ENV['CLAMD_TCP_HOST'] = Settings.clamav.host || 'clamav'
-  ENV['CLAMD_TCP_PORT'] = Settings.clamav.port || '3310'
+  ENV['CLAMD_TCP_HOST'] = Settings.clamav.host
+  ENV['CLAMD_TCP_PORT'] = Settings.clamav.port
 end
