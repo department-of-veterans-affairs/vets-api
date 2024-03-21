0428e0aa78784858ee22dd2e1755293bec745c2e
Kevin Duensing <kevin.duensing@adhocteam.us>
2024-03-21 09:35:28 -0400

    Fix ping controller

--- modules/travel_pay/spec/controllers/claims_controller_spec.rb
+++ modules/travel_pay/spec/controllers/claims_controller_spec.rb
@@ -8,12 +8,20 @@ RSpec.describe TravelPay::ClaimsController, type: :request do
   describe '#index' do
     context 'successful response from API' do
       it 'responds with 200' do
-        allow_any_instance_of(TravelPay::Client).to receive(:request_veis_token).and_return('veis_token')
-        allow_any_instance_of(TravelPay::Client).to receive(:request_btsss_token).and_return('btsss_token')
-        allow_any_instance_of(TravelPay::Client).to receive(:get_claims).and_return([])
+        allow_any_instance_of(TravelPay::Client)
+          .to receive(:request_veis_token)
+          .and_return('veis_token')
+
+        allow_any_instance_of(TravelPay::Client)
+          .to receive(:request_btsss_token)
+          .with('veis_token', 'vagov_token')
+          .and_return('btsss_token')
+
+        allow_any_instance_of(TravelPay::Client)
+          .to receive(:get_claims).and_return([])
         sign_in(user)
 
-        get '/travel_pay/claims', params: nil, headers: { 'Authorization' => 'Bearer token' }
+        get '/travel_pay/claims', params: nil, headers: { 'Authorization' => 'Bearer vagov_token' }
 
         expect(response).to have_http_status(:ok)
       end
