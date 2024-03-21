8c35adac712109ec484504b77b85bd0853ec4a03
Adam Antonioli <adam.antonioli@va.gov>
2024-03-21 15:03:36 -0400

    MBMS-61073 fix linting errors

--- spec/lib/central_mail/datestamp_pdf_spec.rb
+++ spec/lib/central_mail/datestamp_pdf_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe CentralMail::DatestampPdf do
       end
 
       it 'adds text with a datestamp for all forms except 40-10007 with expected formatting' do
-        out_path = instance.run(text: 'Received via vets.gov', x: 10, y: 10, timestamp: Time.new(2024,1,30))
+        out_path = instance.run(text: 'Received via vets.gov', x: 10, y: 10, timestamp: Time.zone.local(2024, 1, 30))
         pdf_reader = PDF::Reader.new(out_path)
         expect(pdf_reader.pages[0].text).to eq('Received via vets.gov 2024-01-30. Confirmation=VETS-XX-1234')
         File.delete(out_path)
@@ -55,7 +55,7 @@ RSpec.describe CentralMail::DatestampPdf do
       it 'adds text with a datestamp for form 40-10007 with expected formatting' do
         @file_path = 'tmp/vba_40_10007-stamped.pdf'
         Prawn::Document.new.render_file @file_path
-        out_path = instance.run(text: 'Received via vets.gov', x: 10, y: 10, timestamp: Time.new(2024,1,30))
+        out_path = instance.run(text: 'Received via vets.gov', x: 10, y: 10, timestamp: Time.zone.local(2024, 1, 30))
         pdf_reader = PDF::Reader.new(out_path)
         expect(pdf_reader.pages[0].text).to eq('Received via vets.gov 01/30/2024. Confirmation=VETS-XX-1234')
         File.delete(out_path)
