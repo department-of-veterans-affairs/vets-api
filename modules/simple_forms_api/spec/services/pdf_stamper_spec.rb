b36c3541753e8c92987ca53849508819a632f8d2
Jacob Penner <jacob.penner@adhocteam.us>
2024-03-22 07:39:23 -0400

    update existing pdf_stamper spec to allow additional contextualized tests

--- modules/simple_forms_api/spec/services/pdf_stamper_spec.rb
+++ modules/simple_forms_api/spec/services/pdf_stamper_spec.rb
@@ -4,22 +4,33 @@ require 'rails_helper'
 require SimpleFormsApi::Engine.root.join('spec', 'spec_helper.rb')
 
 describe SimpleFormsApi::PdfStamper do
-  def self.test_pdf_stamp_error(stamp_method, test_payload)
-    it 'raises an error when generating stamped file' do
+  let(:data) { JSON.parse(File.read("modules/simple_forms_api/spec/fixtures/form_json/#{test_payload}.json")) }
+  let(:form) { "SimpleFormsApi::#{test_payload.titleize.gsub(' ', '')}".constantize.new(data) }
+
+  describe 'pdf_stamping' do
+    subject(:stamp) { SimpleFormsApi::PdfStamper.send(stamp_method, generated_form_path, form) }
+
+    before do
       allow(Common::FileHelpers).to receive(:random_file_path).and_return('fake/stamp_path')
       allow(Common::FileHelpers).to receive(:delete_file_if_exists)
+    end
 
-      generated_form_path = 'fake/generated_form_path'
-      data = JSON.parse(File.read("modules/simple_forms_api/spec/fixtures/form_json/#{test_payload}.json"))
-      form = "SimpleFormsApi::#{test_payload.titleize.gsub(' ', '')}".constantize.new(data)
+    context 'when generating stamped file' do
+      [
+        %w[stamp214142 vba_21_4142],
+        %w[stamp2110210 vba_21_10210],
+        %w[stamp21p0847 vba_21p_0847]
+      ].each do |stamp_method, test_payload|
+        context "when #{stamp_method} receives #{test_payload} payload" do
+          let(:test_payload) { test_payload }
+          let(:stamp_method) { stamp_method }
+          let(:generated_form_path) { 'fake/generated_form_path' }
 
-      expect do
-        SimpleFormsApi::PdfStamper.send(stamp_method, generated_form_path, form)
-      end.to raise_error(StandardError, 'An error occurred while verifying stamp.')
+          it 'raises an error' do
+            expect { stamp }.to raise_error(StandardError, 'An error occurred while verifying stamp.')
+          end
+        end
+      end
     end
   end
-
-  test_pdf_stamp_error 'stamp214142', 'vba_21_4142'
-  test_pdf_stamp_error 'stamp2110210', 'vba_21_10210'
-  test_pdf_stamp_error 'stamp21p0847', 'vba_21p_0847'
 end
