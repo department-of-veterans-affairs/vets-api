32848ec92bac85c1b14f95fb7c32fb3852dd4a59
Daniel Cloud <daniel.cloud@adhocteam.us>
2024-03-22 15:22:07 -0400

    Add serlization for `tracking_list`, tests

--- modules/my_health/spec/serializer/v1/prescription_details_serializer_spec.rb
+++ modules/my_health/spec/serializer/v1/prescription_details_serializer_spec.rb
@@ -169,4 +169,11 @@ RSpec.describe MyHealth::V1::PrescriptionDetailsSerializer, type: :serializer do
     expect(attributes['rx_rf_records']).to be_an(Array)
     expect(attributes['rx_rf_records']).to all(be_a(Hash))
   end
+
+  it 'includes the tracking_list records as an array' do
+    tracking_list = attributes['tracking_list']
+    expect(tracking_list).to be_an(Array)
+    expect(tracking_list[0]).to all(be_a(Hash))
+    expect(tracking_list[0]).to include(carrier: 'UPS', complete_date_time: '2023-03-28T04:39:11-04:00')
+  end
 end
