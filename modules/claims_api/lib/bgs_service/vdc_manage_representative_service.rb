570a63efbb5f8d960a903849f9c07cd85aac1d5c
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-03-26 11:23:45 -0500

    WIP: Adds service file

---
+++ modules/claims_api/lib/bgs_service/vdc_manage_representative_service.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module ClaimsApi
+  class VdcManageRepresentativeService < ClaimsApi::LocalBGS
+
+    def manage_representative_service
+      body = Nokogiri::XML::DocumentFragment.parse <<~EOXML
+        <data:POARequestUpdate>
+          <!--Optional:-->
+          <VSOUserEmail>?</VSOUserEmail>
+          <VSOUserFirstName>?</VSOUserFirstName>
+          <VSOUserLastName>?</VSOUserLastName>
+          <dateRequestActioned>?</dateRequestActioned>
+          <!--Optional:-->
+          <declinedReason>?</declinedReason>
+          <procId>?</procId>
+          <secondaryStatus>?</secondaryStatus>
+        </data:POARequestUpdate>
+      EOXML
+
+      make_request(
+        endpoint: 'ManageRepresentativeService', 
+        action: 'updatePOARequest', 
+        body:
+      )
+    end
+  end
+end
