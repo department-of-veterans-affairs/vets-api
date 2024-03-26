74090197316c3c3a550a979180d89e2007a3748a
Tai Wilkin <tai.wilkin@coforma.io>
2024-03-26 12:44:48 -0400

    Move logging to separate class

--- app/controllers/v0/pension_claims_controller.rb
+++ app/controllers/v0/pension_claims_controller.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 require 'pension_21p527ez/tag_sentry'
+require 'pension_21p527ez/monitor'
 
 module V0
   class PensionClaimsController < ClaimsBaseController
     service_tag 'pension-application'
+
+    MONITOR = ::Pension21p527ez::Monitor.new
 
     def short_name
       'pension_claim'
@@ -25,42 +28,38 @@ module V0
       end
       render(json: response)
     rescue ActiveRecord::RecordNotFound => e
-      Rails.logger.error('21P-527EZ submission not found',
-                         { confirmation_number: params[:id], user_uuid: current_user&.uuid, errors: e.message })
+      MONITOR.track_show404(params[:id], current_user, e)
       render(json: { error: e.to_s }, status: :not_found)
     rescue => e
-      Rails.logger.error('Fetch 21P-527EZ submission failed',
-                         { confirmation_number: params[:id], user_uuid: current_user&.uuid, errors: e.message })
+      MONITOR.track_show_error(params[:id], current_user, e)
       raise e
     end
 
     # Creates and validates an instance of the class, removing any copies of
     # the form that had been previously saved by the user.
     def create
-      StatsD.increment("#{stats_key}.attempt")
       Pension21p527ez::TagSentry.tag_sentry
 
       claim = claim_class.new(form: filtered_params[:form])
-      user_uuid = current_user&.uuid
-      Rails.logger.info("Begin #{claim.class::FORM} Submission", { guid: claim.guid, user_uuid: })
+      MONITOR.track_create_attempt(claim, current_user)
 
       in_progress_form = current_user ? InProgressForm.form_for_user(claim.form_id, current_user) : nil
       claim.itf_datetime = in_progress_form.created_at if in_progress_form
 
       unless claim.save
-        track_create_error(in_progress_form, claim)
+        MONITOR.track_create_error(in_progress_form, claim, current_user)
         log_validation_error_to_metadata(in_progress_form, claim)
         raise Common::Exceptions::ValidationErrors, claim.errors
       end
 
       claim.upload_to_lighthouse
 
-      track_create_success(in_progress_form, claim)
+      MONITOR.track_create_success(in_progress_form, claim, current_user)
 
       clear_saved_form(claim.form_id)
       render(json: claim)
     rescue => e
-      track_create_error(in_progress_form, claim, e)
+      MONITOR.track_create_error(in_progress_form, claim, current_user, e)
       raise e
     end
 
@@ -89,21 +88,6 @@ module V0
           }
         }
       }
-    end
-
-    def track_create_error(in_progress_form, claim, e = nil)
-      Rails.logger.error('21P-527EZ submission to Sidekiq failed',
-                         { confirmation_number: claim&.confirmation_number, user_uuid: current_user&.uuid,
-                           in_progress_form_id: in_progress_form&.id, errors: claim&.errors&.errors,
-                           message: e&.message })
-      StatsD.increment("#{stats_key}.failure")
-    end
-
-    def track_create_success(in_progress_form, claim)
-      Rails.logger.info('21P-527EZ submission to Sidekiq success',
-                        { confirmation_number: claim&.confirmation_number, user_uuid: current_user&.uuid,
-                          in_progress_form_id: in_progress_form&.id })
-      StatsD.increment("#{stats_key}.success")
     end
   end
 end
