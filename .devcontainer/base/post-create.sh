80291c81141c8a1e04488a21a9d56b5c904ca1cd
Eric Boehs <eric@boehs.com>
2024-03-26 17:58:39 -0500

    add base

---
+++ .devcontainer/base/post-create.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cat <<EOT >> $HOME/.tmux.conf
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+set -g @plugin 'dreamsofcode-io/catppuccin-tmux'
+
+run '~/.tmux/plugins/tpm/tpm'
+
+if "test ! -d ~/.tmux/plugins/tpm" \
+   "run 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins'"
+EOT
