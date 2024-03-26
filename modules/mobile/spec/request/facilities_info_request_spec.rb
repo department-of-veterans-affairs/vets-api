143e39f7a74df9c5f85a5c6e65a00f32e1fcd415
Tonksthebear <jason.conigliari@adhocteam.us>
2024-03-26 13:48:37 -0700

    Remove logging from tests

--- modules/mobile/spec/request/facilities_info_request_spec.rb
+++ modules/mobile/spec/request/facilities_info_request_spec.rb
@@ -180,8 +180,6 @@ RSpec.describe 'facilities info', type: :request do
           it 'logs the cache is nil and still returns alphabetized facilities' do
             VCR.use_cassette('mobile/appointments/get_multiple_facilities_200', match_requests_on: %i[method uri]) do
               get('/mobile/v0/facilities-info/appointments', headers: sis_headers)
-              expect(Rails.logger).to have_received(:info).with('mobile facilities info appointments cache nil',
-                                                                user_uuid: user.uuid)
               expect(response).to have_http_status(:ok)
               expect(facilities[0]['name']).to eq('American Lake VA Medical Center')
               expect(facilities[1]['name']).to eq('Ayton VA Medical Center')
