51215a21f652b00d302705e497f8144189cde1e9
Samuel Stuckey <sam.n.stuckey@gmail.com>
2024-03-26 11:26:19 -0400

    general save

--- app/sidekiq/form526_status_polling_job.rb
+++ app/sidekiq/form526_status_polling_job.rb
@@ -4,8 +4,8 @@ require 'benefits_intake_service/service'
 
 class Form526StatusPollingJob
   include Sidekiq::Job
-
   extend Logging::ThirdPartyTransaction::MethodWrapper
+  class EmptyPollResult < StandardError; end
 
   sidekiq_options retry: false
 
@@ -20,7 +20,6 @@ class Form526StatusPollingJob
     }
   }
 
-
   def initialize(max_batch_size: MAX_BATCH_SIZE)
     @max_batch_size = max_batch_size
     @total_handled = 0
@@ -29,8 +28,8 @@ class Form526StatusPollingJob
   def perform
     pending_form_submissions.each_slice(max_batch_size) do |batch|
       batch_ids = batch.pluck(&:backup_submitted_claim_id)
-      response = service.get_bulk_status_of_uploads batch_ids
-      handle_response(response, batch)
+      response = api_to_poll.get_bulk_status_of_uploads(batch_ids)
+      handle_response(response)
     end
   rescue => e
     Rails.logger.error('Error processing Intake Status batch', class: self.class.name, message: e.message)
@@ -38,8 +37,8 @@ class Form526StatusPollingJob
 
   private
 
-  def service
-    @service ||= BenefitsIntakeService::Service.new
+  def api_to_poll
+    @api_to_poll ||= BenefitsIntakeService::Service.new
   end
 
   def pending_submissions
@@ -51,19 +50,14 @@ class Form526StatusPollingJob
   def handle_response(response)
     response.body['data']&.each do |submission|
       status = submission.dig('attributes', 'status')
-
-      form_id = pending_submissions.find do |submission_from_db|
-        submission_from_db.benefits_intake_uuid == submission['id']
-      end.form_type
+      submission_guid = submission['id']
 
       if status == 'error' || status == 'expired'
-        log_result('failure', form_id)
-        handle_failure(submission)
+        log_result('failure')
+        handle_failure(submission_guid)
       elsif status == 'vbms'
-        log_result('success', form_id)
-        handle_success(submission)
-      else
-        log_result('pending', form_id)
+        log_result('success')
+        handle_success(submission_guid)
       end
 
       total_handled += 1
@@ -71,23 +65,13 @@ class Form526StatusPollingJob
   end
 
   def handle_failure(submission)
-    form_submission = FormSubmission.find_by(benefits_intake_uuid: submission['id'])
-    form_submission_attempt = form_submission
-                              .form_submission_attempts
-                              .where(aasm_state: 'pending')
-                              .order(created_at: :asc)
-                              .last
-    form_submission_attempt.fail!
+    form_submission = FormSubmission.find_by(backup_submitted_claim_id: submission['id'])
+    form_submission.reject_from_backup!
   end
 
   def handle_success(submission)
-    form_submission = FormSubmission.find_by(benefits_intake_uuid: submission['id'])
-    form_submission_attempt = form_submission
-                              .form_submission_attempts
-                              .where(aasm_state: 'pending')
-                              .order(created_at: :asc)
-                              .last
-    form_submission_attempt.vbms!
+    form_submission = FormSubmission.find_by(backup_submitted_claim_id: submission['id'])
+    form_submission.finalize_success!
   end
 
   def log_result(result, form_id)
