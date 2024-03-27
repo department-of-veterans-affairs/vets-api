825dfeff888a395ab12a44ad99d26adb324adb3c
John Bramley <bramleyjl@gmail.com>
2024-03-27 14:42:19 -0600

    adds representative attributes validator spec

--- modules/accredited_representative_portal/app/services/accredited_representative_portal/errors.rb
+++ modules/accredited_representative_portal/app/services/accredited_representative_portal/errors.rb
@@ -2,6 +2,15 @@
 
 module AccreditedRepresentativePortal
   module Errors
-      class RepresentativeRecordNotFoundError < StandardError; end
+    class StandardError < StandardError
+      attr_reader :code
+
+      def initialize(message:, code: SignIn::Constants::ErrorCode::INVALID_REQUEST)
+        @code = code
+        super(message)
+      end
+    end
+
+    class RepresentativeRecordNotFoundError < StandardError; end
   end
 end
