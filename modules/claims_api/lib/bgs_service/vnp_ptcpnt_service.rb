31d9635a74b3d71e7d6c440dc1cf5a7ec3898a1c
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-03-27 11:28:56 -0600

    Moves convert_nil_values to shareable space in local_bgs, and alters it so it can be used in another service.

--- modules/claims_api/lib/bgs_service/vnp_ptcpnt_service.rb
+++ modules/claims_api/lib/bgs_service/vnp_ptcpnt_service.rb
@@ -6,22 +6,13 @@ module ClaimsApi
     def vnp_ptcpnt_create(options)
       arg_strg = convert_nil_values(options)
       body = Nokogiri::XML::DocumentFragment.parse <<~EOXML
-        #{arg_strg}
+        <arg0>
+          #{arg_strg}
+          </arg0>
       EOXML
 
       make_request(endpoint: 'VnpPtcpntWebServiceBean/VnpPtcpntService', action: 'vnpPtcpntCreate', body:,
                    key: 'return')
-    end
-
-    private
-
-    def convert_nil_values(options)
-      arg_strg = '<arg0>'
-      options.each do |option|
-        arg = option[0].to_s.camelize(:lower)
-        arg_strg += (option[1].nil? ? "<#{arg} xsi:nil='true'/>" : "<#{arg}>#{option[1]}</#{arg}>")
-      end
-      arg_strg += '</arg0>'
     end
   end
 end
