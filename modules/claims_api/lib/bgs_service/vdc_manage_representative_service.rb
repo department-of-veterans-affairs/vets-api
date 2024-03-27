0a7ff599907c16b09e737b1ee137777680940c5c
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-03-27 14:14:41 -0500

    WPI: Adds VCR and test file to start

--- modules/claims_api/lib/bgs_service/vdc_manage_representative_service.rb
+++ modules/claims_api/lib/bgs_service/vdc_manage_representative_service.rb
@@ -2,27 +2,47 @@
 
 module ClaimsApi
   class VdcManageRepresentativeService < ClaimsApi::LocalBGS
+    def bean_name
+      'VDC/ManageRepresentativeService'
+    end
 
-    def manage_representative_service
+    def update_poa
+      body = get_update_poa_body
+      make_request(
+        endpoint: bean_name,
+        action: 'updatePOARequest',
+        body:,
+        additional_namespace:
+      )
+    end
+
+    private
+
+    def additional_namespace
+      Nokogiri::XML::DocumentFragment.parse <<~EOXML
+        xmlns:data='http://gov.va.vba.benefits.vdc/data'
+      EOXML
+    end
+
+    def get_update_poa_body
+      # TEMP VALUES
+      rep_first_name = 'Tamara'
+      rep_last_name = 'Ellis'
+      proc_id = '3854457'
+      # END TEMP VALUES
+      current_date = Time.zone.now.strftime('%Y-%m-%dT%H:%M:%SZ')
+
       body = Nokogiri::XML::DocumentFragment.parse <<~EOXML
         <data:POARequestUpdate>
-          <!--Optional:-->
-          <VSOUserEmail>?</VSOUserEmail>
-          <VSOUserFirstName>?</VSOUserFirstName>
-          <VSOUserLastName>?</VSOUserLastName>
-          <dateRequestActioned>?</dateRequestActioned>
-          <!--Optional:-->
-          <declinedReason>?</declinedReason>
-          <procId>?</procId>
-          <secondaryStatus>?</secondaryStatus>
+          <VSOUserFirstName>#{rep_first_name}</VSOUserFirstName>
+          <VSOUserLastName>#{rep_last_name}</VSOUserLastName>
+          <dateRequestActioned>#{current_date}</dateRequestActioned>
+          <procId>#{proc_id}</procId>
+
+          <secondaryStatus>obsolete</secondaryStatus>
         </data:POARequestUpdate>
       EOXML
-
-      make_request(
-        endpoint: 'VDC/ManageRepresentativeService', 
-        action: 'updatePOARequest', 
-        body:
-      )
+      body.to_s
     end
   end
 end
---
+++ modules/claims_api/spec/lib/claims_api/vdc_manage_representative_service_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'bgs_service/vdc_manage_representative_service'
+
+describe ClaimsApi::VdcManageRepresentativeService do
+  subject { described_class.new external_uid: 'abcdefg', external_key: 'abcdefg' }
+
+  describe 'update_poa' do
+    let(:options) { {} }
+
+    it 'responds with attributes' do
+      VCR.use_cassette('bgs/vdc_manage_representative_service/update_poa') do
+        # Formatting this to show the difference in thedate returned in response and the date sent in request
+        date = Time.parse('2024-03-27T13:05:01Z').getlocal('-05:00').strftime('%Y-%m-%dT%H:%M:%S%:z')
+
+        response = subject.update_poa
+        expect(response[:poa_request_update]).to include(
+          {
+            vso_user_email: nil,
+            vso_user_first_name: 'Test',
+            vso_user_last_name: 'User',
+            date_request_actioned: date,
+            declined_reason: nil,
+            proc_id: '8675309',
+            secondary_status: 'OBS'
+          }
+        )
+      end
+    end
+  end
+end
