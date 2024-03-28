1f555646c221e45efb85e6f5d87dabd920d518d4
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-03-28 09:14:38 -0600

    Symbolizes keys in va claims controller evss formatter.

--- modules/claims_api/app/controllers/claims_api/v1/claims_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v1/claims_controller.rb
@@ -64,8 +64,9 @@ module ClaimsApi
 
       def format_evss_errors(errors)
         errors.map do |error|
-          formatted = error[:key] ? error[:key].gsub('.', '/') : error[:key]
-          { status: 422, detail: "#{error[:severity]} #{error[:detail] || error[:text]}".squish, source: formatted }
+          err = error.deep_symbolize_keys
+          formatted = err[:key] ? err[:key].gsub('.', '/') : err[:key]
+          { status: 422, detail: "#{err[:severity]} #{err[:detail] || err[:text]}".squish, source: formatted }
         end
       end
     end
