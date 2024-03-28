ff96bafee8581057b900da8c106372cc1df55264
Oren Mittman <nihil2501@gmail.com>
2024-03-28 18:37:27 -0400

    API-34440 inline xml body matcher in bgs spec helper rather than add to global VCR config

--- modules/claims_api/spec/support/bgs_client_helpers.rb
+++ modules/claims_api/spec/support/bgs_client_helpers.rb
@@ -1,16 +1,29 @@
 # frozen_string_literal: true
 
-VCR.configure do |config|
-  config.register_request_matcher :xml_body do |req_a, req_b|
-    # I suspect that this is not a correct implementation of XML equality but
-    # that there is a correct implementation of it somewhere out there.
-    xml_a = Nokogiri::XML(req_a.body, &:noblanks).canonicalize
-    xml_b = Nokogiri::XML(req_b.body, &:noblanks).canonicalize
-    xml_a == xml_b
-  end
-end
-
 module BGSClientHelpers
+  # If one finds this request matcher useful elsewhere in the future,
+  # Rather than using a callable custom request matcher:
+  #   https://benoittgt.github.io/vcr/#/request_matching/custom_matcher?id=use-a-callable-as-a-custom-request-matcher 
+  # This could instead be registered as a named custom request matcher:
+  #   https://benoittgt.github.io/vcr/#/request_matching/custom_matcher?id=register-a-named-custom-matcher
+  # Called `:body_as_xml` as inspired by `:body_as_json`:
+  #   https://benoittgt.github.io/vcr/#/request_matching/body_as_json?id=matching-on-body
+  body_as_xml_matcher =
+    lambda do |req_a, req_b|
+      # I suspect that this is not a correct implementation of XML equality but
+      # that there is a correct implementation of it somewhere out there.
+      xml_a = Nokogiri::XML(req_a.body, &:noblanks).canonicalize
+      xml_b = Nokogiri::XML(req_b.body, &:noblanks).canonicalize
+      xml_a == xml_b
+    end
+
+  VCR_OPTIONS = {
+    match_requests_on: [
+      :method, :uri,
+      body_as_xml_matcher.freeze
+    ].freeze
+  }.freeze
+
   def use_bgs_cassette(&)
     example = RSpec.current_example
     metadata = example.metadata[:bgs].to_h
@@ -24,8 +37,7 @@ module BGSClientHelpers
     end
 
     name = File.join('bgs', service, operation, example.full_description)
-    options = { match_requests_on: %i[method uri xml_body] }
-    VCR.use_cassette(name, options, &)
+    VCR.use_cassette(name, VCR_OPTIONS, &)
   end
 end
 
