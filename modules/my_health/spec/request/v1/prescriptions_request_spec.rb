4ec292137e075c52e500b5dbee75d9dde9c4ce89
agravell047 <anthony.gravell@bylight.com>
2024-03-29 14:45:25 -0400

    updated test schema

--- modules/my_health/spec/request/v1/prescriptions_request_spec.rb
+++ modules/my_health/spec/request/v1/prescriptions_request_spec.rb
@@ -252,8 +252,10 @@ RSpec.describe 'prescriptions', type: :request do
           end
 
           res = JSON.parse(response.body)
-
-          dates = res['data'].map { |d| DateTime.parse(d['attributes']['sortedDispensedDate']) }
+          dates = res['data'].map do |d|
+            sorted_date_str = d.dig('attributes', 'sortedDispensedDate')
+            sorted_date_str.present? ? Time.zone.parse(sorted_date_str) : Date.new(0, 1, 1)
+          end
           is_sorted = dates.each_cons(2).all? { |item1, item2| item1 >= item2 }
           expect(response).to be_successful
           expect(response.body).to be_a(String)
