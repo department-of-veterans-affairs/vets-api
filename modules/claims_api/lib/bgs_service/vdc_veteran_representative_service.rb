d9679fc8ebefa566624d06a40b3bdc66e19c9b52
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-04-01 14:44:48 -0500

    WPI:Adds start of service

---
+++ modules/claims_api/lib/bgs_service/vdc_veteran_representative_service.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module ClaimsApi
+  class VdcVeteranRepresentativeService < ClaimsApi::LocalBGS
+    def bean_name
+      '/VDC/VeteranRepresentativeService'
+    end
+
+    def create_veteran_representative(ptcpnt_id, proc_id, form_type, poa_code)
+      body = get_create_representative_body(ptcpnt_id, proc_id, form_type, poa_code)
+
+      make_request(
+        endpoint: bean_name,
+        action: 'createVeteranRepresentative',
+        body:,
+        additional_namespace:
+      )
+    end
+
+    private
+
+    def additional_namespace
+      ans = Nokogiri::XML::DocumentFragment.parse <<~EOXML
+        data
+      EOXML
+      ans.to_s
+    end
+
+    def get_create_representative_body(ptcpnt_id, proc_id, form_type, poa_code)
+      body = Nokogiri::XML::DocumentFragment.parse <<~EOXML
+        <data:VeteranRepresentative>
+          <procId>#{proc_id}</procId>
+          <formTypeCode>#{form_type}</formTypeCode>
+          <poaCode>#{poa_code}</poa_code>
+          <section7332Auth>false</section7332Auth>
+          <limitationDrugAbuse>false</limitationDrugAbuse>
+          <limitationAlcohol>false</limitationAlcohol>
+          <limitationHIV>false</limitationHIV>
+          <limitationSCA>false</limitationSCA>
+          <changeAddressAuth>false</changeAddressAuth>
+          <veteranPtcpntId>#{ptcpnt_id}</veteranPtcpntId>
+        </data:VeteranRepresentative>
+      EOXML
+    end
+  end
+end
