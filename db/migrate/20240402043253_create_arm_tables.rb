2f9ccc115f5a87a7303e715fa6fe826b387aefbd
Holden Hinkle <holden@holdenhinkle.com>
2024-04-02 01:15:50 -0400

    rubocop

--- db/migrate/20240402043253_create_arm_tables.rb
+++ db/migrate/20240402043253_create_arm_tables.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class CreateArmTables < ActiveRecord::Migration[7.1]
+  # rubocop:disable Metrics/MethodLength
   def change
     create_table :accredited_representatives, id: false do |t|
       t.string :number, null: false
@@ -26,18 +29,17 @@ class CreateArmTables < ActiveRecord::Migration[7.1]
       t.jsonb :raw_address
       t.float :lat
       t.float :long
-      t.geography :location, limit: {srid: 4326, type: 'st_point', geographic: true}
+      t.geography :location, limit: { srid: 4326, type: 'st_point', geographic: true }
       t.timestamps
     end
-
+    # rubocop:enable Metrics/MethodLength
     safety_assured do
-      execute "ALTER TABLE accredited_representatives ADD PRIMARY KEY (number);"
+      execute 'ALTER TABLE accredited_representatives ADD PRIMARY KEY (number);' # rubocop:disable Rails/ReversibleMigration
     end
-
     add_index :accredited_representatives, :full_name
     add_index :accredited_representatives, :location, using: :gist
-    add_index :accredited_representatives, [:number, :first_name, :last_name], unique: true, name: 'index_rep_group'
- 
+    add_index :accredited_representatives, %i[number first_name last_name], unique: true, name: 'index_rep_group'
+
     create_table :accredited_organizations, id: false do |t|
       t.string :poa, limit: 3, null: false
       t.string :name
@@ -60,13 +62,12 @@ class CreateArmTables < ActiveRecord::Migration[7.1]
       t.jsonb :raw_address
       t.float :lat
       t.float :long
-      t.geography :location, limit: {srid: 4326, type: 'st_point', geographic: true}
+      t.geography :location, limit: { srid: 4326, type: 'st_point', geographic: true }
       t.timestamps
     end
     safety_assured do
-      execute "ALTER TABLE accredited_organizations ADD PRIMARY KEY (poa);"
+      execute 'ALTER TABLE accredited_organizations ADD PRIMARY KEY (poa);' # rubocop:disable Rails/ReversibleMigration
     end
-
     add_index :accredited_organizations, :location, using: :gist
     add_index :accredited_organizations, :name
     add_index :accredited_organizations, :poa, unique: true
@@ -75,15 +76,20 @@ class CreateArmTables < ActiveRecord::Migration[7.1]
       t.string :accredited_representative_number
       t.string :type
     end
-    add_index :accredited_representative_types, [:accredited_representative_number, :type], unique: true, name: 'index_accredited_representative_types_on_id_and_type'
-    add_foreign_key :accredited_representative_types, :accredited_representatives, column: :accredited_representative_number, primary_key: :number, validate: false
+    add_index :accredited_representative_types, %i[accredited_representative_number type], unique: true,
+                                                                                           name: 'index_accredited_representative_types_on_id_and_type' # rubocop:disable Layout/LineLength
+    add_foreign_key :accredited_representative_types, :accredited_representatives,
+                    column: :accredited_representative_number, primary_key: :number, validate: false
 
     create_table :accredited_organization_accredited_representatives, id: false do |t|
       t.string :accredited_representative_number
       t.string :accredited_organization_poa
     end
-    add_index :accredited_organization_accredited_representatives, [:accredited_representative_number, :accredited_organization_poa], unique: true, name: 'index_organization_representatives_on_rep_and_org'
-    add_foreign_key :accredited_organization_accredited_representatives, :accredited_representatives, column: :accredited_representative_number, primary_key: :number, validate: false
-    add_foreign_key :accredited_organization_accredited_representatives, :accredited_organizations, column: :accredited_organization_poa, primary_key: :poa, validate: false
+    add_index :accredited_organization_accredited_representatives,
+              %i[accredited_representative_number accredited_organization_poa], unique: true, name: 'index_organization_representatives_on_rep_and_org' # rubocop:disable Layout/LineLength
+    add_foreign_key :accredited_organization_accredited_representatives, :accredited_representatives,
+                    column: :accredited_representative_number, primary_key: :number, validate: false
+    add_foreign_key :accredited_organization_accredited_representatives, :accredited_organizations,
+                    column: :accredited_organization_poa, primary_key: :poa, validate: false
   end
 end
