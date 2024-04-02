9fde422fe642f5855ec64662299a51448e0a1879
Oren Mittman <nihil2501@gmail.com>
2024-04-02 03:39:26 -0400

    API-34440 add comment documenting usage and purpose of `BGSClientHelpers`

--- modules/claims_api/spec/support/bgs_client_helpers.rb
+++ modules/claims_api/spec/support/bgs_client_helpers.rb
@@ -10,8 +10,9 @@ module BGSClientHelpers
   #   https://benoittgt.github.io/vcr/#/request_matching/body_as_json?id=matching-on-body
   body_as_xml_matcher =
     lambda do |req_a, req_b|
-      # I suspect that this is not a correct implementation of XML equality but
-      # that there is a correct implementation of it somewhere out there.
+      # I suspect that this is not a fully correct implementation of XML
+      # equality but that there is a fully correct implementation of it
+      # somewhere out there.
       xml_a = Nokogiri::XML(req_a.body, &:noblanks).canonicalize
       xml_b = Nokogiri::XML(req_b.body, &:noblanks).canonicalize
       xml_a == xml_b
@@ -30,6 +31,24 @@ module BGSClientHelpers
     ].freeze
   }.freeze
 
+  # This convenience method affords a handful of quality of life improvements
+  # for developing BGS service operation wrappers. It makes development a less
+  # manual process. It also turns VCR cassettes into a human readable resource
+  # that documents the behavior of BGS.
+  #
+  # In order to take advantage of this method, you will need to have supplied,
+  # to your example or example group, metadata of this form:
+  #   `{ bgs: { service: "service", operation: "operation" } }`.
+  #
+  # Then, HTTP interactions that occur within the block supplied to this method
+  # will be captured by VCR cassettes that have the following convenient
+  # properties:
+  #   - They will be nicely organized at `bgs/:service/:operation/:name`
+  #   - Cassette matching will be done on canonicalized XML bodies, so
+  #     reformatting cassettes for human readability won't defeat matching
+  #   - ERB templating will be enabled with a value `bgs_base_url` supplied so
+  #     that the same cassette will function without modification in multiple
+  #     environments like CI and locally
   def use_bgs_cassette(name, &)
     metadata = RSpec.current_example.metadata[:bgs].to_h
     service, operation = metadata.values_at(:service, :operation)
@@ -37,7 +56,7 @@ module BGSClientHelpers
     if service.blank? || operation.blank?
       raise ArgumentError, <<~HEREDOC
         Must provide spec metadata of the form:
-          { bgs: { service: 'service', operation: 'operation' } }
+          `{ bgs: { service: "service", operation: "operation" } }'
       HEREDOC
     end
 
