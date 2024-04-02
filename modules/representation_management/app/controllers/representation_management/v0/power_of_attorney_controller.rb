db93156a08b69c5f685312d1d971af8a141188ec
Holden Hinkle <holden@holdenhinkle.com>
2024-04-01 22:18:39 -0400

    add error handling to PowerOfAttorneyController #index

--- modules/representation_management/app/controllers/representation_management/v0/power_of_attorney_controller.rb
+++ modules/representation_management/app/controllers/representation_management/v0/power_of_attorney_controller.rb
@@ -8,10 +8,22 @@ module RepresentationManagement
       service_tag 'representation-management'
 
       def index
-        body = lighthouse_service.get_power_of_attorney
+        begin
+          body = lighthouse_service.get_power_of_attorney
+        rescue BenefitsClaims::ServiceException => e
+          Rails.logger.error("Error fetching power of attorney: #{e.message}")
+          return render json: { error: 'Unable to fetch power of attorney information' }, status: :service_unavailable
+        end
+
         type = body.dig(:data, :type)
         code = body.dig(:data, :attributes, :code)
-        render json: code.blank? ? {} : find_poa_by_code(type, code)
+
+        if code.blank?
+          Rails.logger.warn("No power of attorney found: #{body}")
+          render json: {}, status: :ok
+        else
+          render json: find_poa_by_code(type, code)
+        end
       end
 
       private
