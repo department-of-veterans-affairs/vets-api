c28d8482ef2efb212d62393e003bf75192f4b60a
Richard Davis <richard.davis@agile6.com>
2024-04-02 11:19:09 -0400

    Updates

--- lib/va_profile/profile/v3/health_benefit_bio_response.rb
+++ lib/va_profile/profile/v3/health_benefit_bio_response.rb
@@ -8,33 +8,35 @@ module VAProfile
   module Profile
     module V3
       class HealthBenefitBioResponse < VAProfile::Response
-        attr_reader :body
-
         attribute :contacts, Array[VAProfile::Models::AssociatedPerson]
         attribute :messages, Array[VAProfile::Models::Message]
+        attribute :va_profile_tx_audit_id, String
 
         def initialize(response)
-          @body = response.body
           contacts = body.dig('profile', 'health_benefit', 'associated_persons')
                          &.select { |p| valid_contact_types.include?(p['contact_type']) }
                          &.sort_by { |p| valid_contact_types.index(p['contact_type']) }
           messages = body['messages']
-          super(response.status, { contacts:, messages: })
+          va_profile_tx_audit_id = response.response_headers['vaprofiletxauditid']
+          super(response.status, { contacts:, messages:, va_profile_tx_audit_id: })
         end
 
         def metadata
-          { status:, messages: }
+          {
+            status:,
+            message:,
+            va_profile_tx_audit_id:
+          }
         end
 
         private
 
         def valid_contact_types
-          [
-            VAProfile::Models::AssociatedPerson::EMERGENCY_CONTACT,
-            VAProfile::Models::AssociatedPerson::OTHER_EMERGENCY_CONTACT,
-            VAProfile::Models::AssociatedPerson::PRIMARY_NEXT_OF_KIN,
-            VAProfile::Models::AssociatedPerson::OTHER_NEXT_OF_KIN
-          ]
+          VAProfile::Models::AssociatedPerson::PERSONAL_HEALTH_CARE_CONTACT_TYPES
+        end
+
+        def message
+          messages&.first&.to_h
         end
       end
     end
