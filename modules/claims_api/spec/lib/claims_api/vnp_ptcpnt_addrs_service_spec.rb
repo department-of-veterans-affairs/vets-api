9420812a3368ef1e9f90bf20d8f42f9a9f40c63c
Matt Christianson <matthew.christianson1@va.gov>
2024-04-03 10:15:04 -0400

    Test data tweaks

--- modules/claims_api/spec/lib/claims_api/vnp_ptcpnt_addrs_service_spec.rb
+++ modules/claims_api/spec/lib/claims_api/vnp_ptcpnt_addrs_service_spec.rb
@@ -11,8 +11,8 @@ describe ClaimsApi::VnpPtcpntAddrsService do
 
     it 'responds with attributes' do
       options[:vnp_ptcpnt_addrs_id] = nil
-      options[:vnp_proc_id] = '3854566'
-      options[:vnp_ptcpnt_id] = '182014'
+      options[:vnp_proc_id] = '3854571'
+      options[:vnp_ptcpnt_id] = '182017'
       options[:efctv_dt] = '2020-07-16T18:20:17Z'
       options[:addrs_one_txt] = '76 Crowther Ave'
       options[:addrs_two_txt] = ''
@@ -22,12 +22,12 @@ describe ClaimsApi::VnpPtcpntAddrsService do
       options[:cntry_nm] = 'United States'
       options[:county_nm] = 'Fairfield'
       options[:eft_waiver_type_nm] = nil
-      options[:email_addrs_txt] = 281
+      options[:email_addrs_txt] = nil
       options[:end_dt] = '2025-07-16T18:20:17Z'
       options[:fms_addrs_code_txt] = nil
       options[:frgn_postal_cd] = nil
       options[:group_1_verifd_type_cd] = nil
-      options[:jrn_dt] = '2020-07-16T18:20:17Z'
+      options[:jrn_dt] = Time.current.iso8601
       options[:jrn_lctn_id] = 281
       options[:jrn_obj_id] = 'VAgovAPI'
       options[:jrn_status_type_cd] = 'U'
@@ -47,9 +47,9 @@ describe ClaimsApi::VnpPtcpntAddrsService do
       options[:trsury_addrs_one_txt] = nil
       options[:trsury_seq_nbr] = nil
       options[:trtry_nm] = nil
-      options[:zip_first_suffix_nbr] = 0
-      options[:zip_prefix_nbr] = 66
-      options[:zip_second_suffix_nbr] = 05
+      options[:zip_first_suffix_nbr] = '66'
+      options[:zip_prefix_nbr] = '0'
+      options[:zip_second_suffix_nbr] = '05'
 
       VCR.use_cassette('bgs/vnp_ptcpnt_addrs_service/vnp_ptcpnt_addrs_create') do
         byebug
