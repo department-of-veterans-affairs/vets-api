4f90f4a166ed97f467beb2920c426fd6ca2a104e
Tonksthebear <jason.conigliari@adhocteam.us>
2024-04-08 16:44:13 -0700

    Rubocop

--- modules/mobile/lib/scripts/parse_rspec_xml.rb
+++ modules/mobile/lib/scripts/parse_rspec_xml.rb
@@ -1,48 +1,48 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'optparse'
 
+def relative_file_path(file_path)
+  file_path.start_with?('/') ? ".#{file_path}" : file_path
+end
+
 def extract_xml_attributes(file_path, mode)
-  begin
-    # Load XML content from the file
-    xml_content = File.read(file_path)
+  # Load XML content from the file
+  xml_content = File.read(file_path)
 
-    # Parse XML content using Nokogiri
-    doc = Nokogiri::XML(xml_content)
+  # Parse XML content using Nokogiri
+  doc = Nokogiri::XML(xml_content)
 
-    # Extract attributes from XML nodes
-    seed = nil
-    files = []
-    doc.traverse do |node|
-      if node['name'] == 'seed'
-        seed = node.attributes['value'].value
-      end
+  # Extract attributes from XML nodes
+  seed = nil
+  files = []
+  doc.traverse do |node|
+    seed = node.attributes['value'].value if node['name'] == 'seed'
 
-      if node.element? && node.attributes['file']
-        case mode
-        when 'errors'
-          if node.children.any? { |child| child.name == 'failure' }
-            file_path = node.attributes['file'].value
-            files << (file_path.start_with?('/') ? ".#{file_path}" : file_path)
-          end
-        when 'full'
-          file_path = node.attributes['file'].value
-          files << (file_path.start_with?('/') ? ".#{file_path}" : file_path)
+    if node.element? && node.attributes['file']
+      case mode
+      when 'errors'
+        if node.children.any? { |child| child.name == 'failure' }
+          files << relative_file_path(node.attributes['file'].value)
         end
+      when 'full'
+        files << relative_file_path(node.attributes['file'].value)
       end
     end
-
-    # Flatten the array of attributes and join them into a string
-    "bundle exec rspec --seed #{seed} --bisect #{files.uniq.join(' ')}"
-  rescue StandardError => e
-    puts "Error: #{e.message}"
   end
+
+  # Flatten the array of attributes and join them into a string
+  "bundle exec rspec --seed #{seed} --bisect #{files.uniq.join(' ')}"
+rescue => e
+  puts "Error: #{e.message}"
 end
 
 options = {}
 
 OptionParser.new do |opts|
-  opts.banner = "Usage: ruby extract_xml_attributes.rb [options] FILE_PATH"
-  opts.on("-m", "--mode MODE", "Mode: full or errors (default: errors)") do |m|
+  opts.banner = 'Usage: ruby extract_xml_attributes.rb [options] FILE_PATH'
+  opts.on('-m', '--mode MODE', 'Mode: full or errors (default: errors)') do |m|
     options[:mode] = m.downcase
   end
 end.parse!
@@ -51,13 +51,13 @@ file_path = ARGV.join(' ')
 
 # Check if file path is provided
 if file_path.nil?
-  puts "Error: File path is required."
-  puts "Usage: ruby script.rb [options] FILE_PATH"
+  puts 'Error: File path is required.'
+  puts 'Usage: ruby script.rb [options] FILE_PATH'
   exit(1)
 end
 
 # Set default mode to 'errors' if mode is not specified
-mode = options[:mode] || "errors"
+mode = options[:mode] || 'errors'
 
 # Call the method to extract XML attributes
 xml_attributes = extract_xml_attributes(file_path, mode)
