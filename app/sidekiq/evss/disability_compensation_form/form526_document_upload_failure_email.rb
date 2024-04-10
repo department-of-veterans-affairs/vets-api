9303b591bf2f29099da79f1c355aed20b3b8967a
Nathan Burgess <burgess.nathan28@gmail.com>
2024-04-10 13:13:26 -0400

    Refactor Form526DocumentUploadFailureEmail for encapsulation
    Moving filename obfuscation to SupportingEvidenceAttachment class and date formatting to Form526 Submission class so the email doesn't know too much about how this data is structured

--- app/sidekiq/evss/disability_compensation_form/form526_document_upload_failure_email.rb
+++ app/sidekiq/evss/disability_compensation_form/form526_document_upload_failure_email.rb
@@ -9,15 +9,18 @@ module EVSS
       VA_NOTIFY_TEMPLATE_ID = 'form_526_document_upload_failed'
 
       def perform(form_526_submission_id, supporting_evidence_attachment_guid)
+        # Placeholder service
         @notify_client ||= VaNotify::Service.new(Settings.vanotify.services.va_gov.api_key)
         submission = Form526Submission.find(form_526_submission_id)
 
         email_address = submission.get_email_address
         first_name = submission.get_first_name
-        date_submitted = submission.created_at.strftime('%B %-d, %Y %-l:%M %P %Z').sub(/([ap])m/, '\1.m.')
+        date_submitted = submission.get_formatted_creation_time
 
         form_attachment = SupportingEvidenceAttachment.find_by!(guid: supporting_evidence_attachment_guid)
-        filename = obscure_filename(form_attachment)
+        # We need to obscure the original filename since it may contain PII and someone other than the veteran could
+        # potentially read the email
+        filename = form_attachment.obscured_filename
 
         @notify_client.send_email(
           email_address:,
@@ -28,22 +31,6 @@ module EVSS
             date_submitted:
           }
         )
-      end
-
-      private
-
-      def obscure_filename(form_attachment)
-        raw_filename = form_attachment.original_filename
-        extension = raw_filename[/\.\S*$/]
-        filename = raw_filename.gsub(/\.\S*$/, '')
-
-        # Obfuscate filenames longer than five characters
-        if filename.length > 5
-          obfuscated_portion = filename[3..-3].gsub(/[a-zA-Z]/, '*')
-          filename[0..2] + obfuscated_portion + filename[-2..] + extension
-        else
-          raw_filename
-        end
       end
     end
   end
--- spec/sidekiq/evss/disability_compensation_form/form526_document_upload_failure_email_spec.rb
+++ spec/sidekiq/evss/disability_compensation_form/form526_document_upload_failure_email_spec.rb
@@ -29,23 +29,28 @@ RSpec.describe EVSS::DisabilityCompensationForm::Form526DocumentUploadFailureEma
     allow(Notifications::Client).to receive(:new).and_return(notification_client)
   end
 
-  it 'dispatches a failure notification email with an obscured filename' do
-    formatted_submit_date = form526_submission.created_at.strftime('%B %-d, %Y %-l:%M %P %Z').sub(/([ap])m/, '\1.m.')
-    obscured_filename = 'sm_***e1.jpg'
+  describe '#perform' do
+    let(:formatted_submit_date) do
+      form526_submission.created_at.strftime('%B %-d, %Y %-l:%M %P %Z').sub(/([ap])m/, '\1.m.')
+    end
 
-    expect(notification_client).to receive(:send_email).with(
-      # Email address and first_name are from our User fixtures
-      {
-        email_address: 'test@email.com',
-        template_id: 'form_526_document_upload_failed',
-        personalisation: {
-          first_name: 'BEYONCE',
-          filename: obscured_filename,
-          date_submitted: formatted_submit_date
+    it 'dispatches a failure notification email with an obscured filename' do
+      obscured_filename = 'sm_***e1.jpg'
+
+      expect(notification_client).to receive(:send_email).with(
+        # Email address and first_name are from our User fixtures
+        {
+          email_address: 'test@email.com',
+          template_id: 'form_526_document_upload_failed',
+          personalisation: {
+            first_name: 'BEYONCE',
+            filename: obscured_filename,
+            date_submitted: formatted_submit_date
+          }
         }
-      }
-    )
+      )
 
-    subject.perform(form526_submission.id, form_attachment.guid)
+      subject.perform(form526_submission.id, form_attachment.guid)
+    end
   end
 end
