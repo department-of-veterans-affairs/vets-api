da07e7376cfc6fa3792d095562f8f5bb86aebccb
Oren Mittman <nihil2501@gmail.com>
2024-04-10 01:36:49 -0400

    API-34439 specify v2/power-of-attorney-requests response example

--- modules/claims_api/app/controllers/claims_api/v2/power_of_attorney_requests_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v2/power_of_attorney_requests_controller.rb
@@ -4,7 +4,50 @@ module ClaimsApi
   module V2
     class PowerOfAttorneyRequestsController < ClaimsApi::V2::ApplicationController
       def index
-        render json: {}
+        render json: {
+          data: [
+            {
+              type: "powerOfAttorneyRequest",
+              id: 12345,
+              attributes: {
+                status: "DECLINED",
+                submittedAt: Time.zone.at(1712724672),
+                acceptedOrDeclinedAt: Time.zone.at(1712724672),
+                declinedReason: "Because I felt like it",
+                authorizesAddressChanges: true,
+                authorizesTreatmentDisclosures: false,
+                powerOfAttorneyCode: "012",
+                veteran: {
+                  firstName: "Firstus",
+                  middleName: nil,
+                  lastName: "Lastus"
+                },
+                claimant: {
+                  participantId: 23456,
+                  mailingAddress: {
+                    city: "Baltimore",
+                    state: "MD",
+                    zip: "21218",
+                    country: "US",
+                    militaryPostOffice: nil,
+                    militaryPostalCode: nil
+                  },
+                  nonVeteranIndividual: {
+                    firstName: "Alpha",
+                    middleName: nil,
+                    lastName: "Omega",
+                    relationshipToVeteran: "Cousin"
+                  }
+                },
+                representative: {
+                  firstName: "Primero",
+                  lastName: "Ultimo",
+                  email: "primero.ultimo@vsorg.org"
+                }
+              }
+            }
+          ]
+        }
       end
     end
   end
