0acd8d815e81a60035bcf8f1b6d52e59544c767f
Holden Hinkle <holden@holdenhinkle.com>
2024-04-10 09:30:11 -0400

    wip

--- modules/representation_management/app/sidekiq/bigquery_service.rb
+++ modules/representation_management/app/sidekiq/bigquery_service.rb
@@ -4,8 +4,26 @@ require 'google/cloud/bigquery'
 
 module RepresentationManagement
   class BigqueryService
+    TABLE_NAME = 'my_table'
+
     def initialize
       authenticate
+    end
+
+    def drop_table
+      table.delete
+    end
+
+    def create_table
+      # temporary comment
+      # update schema
+      dataset.create_table TABLE_NAME do |schema|
+        schema.string 'first_name', mode: :required
+        schema.record 'cities_lived', mode: :repeated do |nested_schema|
+          nested_schema.string 'place', mode: :required
+          nested_schema.integer 'number_of_years', mode: :required
+        end
+      end
     end
 
     def insert(data) # rubocop:disable Rails/Delegate
@@ -25,10 +43,13 @@ module RepresentationManagement
       @bigquery = Google::Cloud::Bigquery.new
     end
 
+    def dataset
+      @dataset ||= @bigquery.dataset(Settings.representation_management.bigquery.dataset_id)
+    end
+
     def table
       # temporary comment
       # api docs - https://cloud.google.com/ruby/docs/reference/google-cloud-bigquery/1.41.0#loading-records
-      dataset = @bigquery.dataset(Settings.representation_management.bigquery.dataset_id)
       dataset.table(Settings.representation_management.bigquery.table_id)
     end
   end
