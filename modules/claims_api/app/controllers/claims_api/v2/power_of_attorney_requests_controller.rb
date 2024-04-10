0bcd49b1e43524251c45223cf4f62309de8908d3
Oren Mittman <nihil2501@gmail.com>
2024-04-10 01:58:19 -0400

    API-34439 appease linter for temporary hardcoded poa requests index response

--- modules/claims_api/app/controllers/claims_api/v2/power_of_attorney_requests_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v2/power_of_attorney_requests_controller.rb
@@ -3,52 +3,54 @@
 module ClaimsApi
   module V2
     class PowerOfAttorneyRequestsController < ClaimsApi::V2::ApplicationController
+      # rubocop:disable Metrics/MethodLength
       def index
         render json: {
           data: [
             {
-              type: "powerOfAttorneyRequest",
-              id: 12345,
+              type: 'powerOfAttorneyRequest',
+              id: 12_345,
               attributes: {
-                status: "DECLINED",
-                submittedAt: Time.zone.at(1712724672),
-                acceptedOrDeclinedAt: Time.zone.at(1712724672),
-                declinedReason: "Because I felt like it",
+                status: 'DECLINED',
+                submittedAt: Time.zone.at(1_712_724_672),
+                acceptedOrDeclinedAt: Time.zone.at(1_712_724_672),
+                declinedReason: 'Because I felt like it',
                 authorizesAddressChanges: true,
                 authorizesTreatmentDisclosures: false,
-                powerOfAttorneyCode: "012",
+                powerOfAttorneyCode: '012',
                 veteran: {
-                  firstName: "Firstus",
+                  firstName: 'Firstus',
                   middleName: nil,
-                  lastName: "Lastus"
+                  lastName: 'Lastus'
                 },
                 claimant: {
-                  participantId: 23456,
+                  participantId: 23_456,
                   mailingAddress: {
-                    city: "Baltimore",
-                    state: "MD",
-                    zip: "21218",
-                    country: "US",
+                    city: 'Baltimore',
+                    state: 'MD',
+                    zip: '21218',
+                    country: 'US',
                     militaryPostOffice: nil,
                     militaryPostalCode: nil
                   },
                   nonVeteranIndividual: {
-                    firstName: "Alpha",
+                    firstName: 'Alpha',
                     middleName: nil,
-                    lastName: "Omega",
-                    relationshipToVeteran: "Cousin"
+                    lastName: 'Omega',
+                    relationshipToVeteran: 'Cousin'
                   }
                 },
                 representative: {
-                  firstName: "Primero",
-                  lastName: "Ultimo",
-                  email: "primero.ultimo@vsorg.org"
+                  firstName: 'Primero',
+                  lastName: 'Ultimo',
+                  email: 'primero.ultimo@vsorg.org'
                 }
               }
             }
           ]
         }
       end
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
