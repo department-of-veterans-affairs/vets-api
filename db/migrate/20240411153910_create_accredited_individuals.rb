2b70eaf61e024d62dfacb8f57a1dc80619ec7043
Holden Hinkle <holden@holdenhinkle.com>
2024-04-11 12:00:45 -0400

    create accredited_individuals table

---
+++ db/migrate/20240411153910_create_accredited_individuals.rb
@@ -0,0 +1,37 @@
+class CreateAccreditedIndividuals < ActiveRecord::Migration[7.1]
+  def change
+    create_table :accredited_individuals, id: :uuid do |t|
+      t.uuid :individual_id, null: false
+      t.string :registration_number, null: false
+      t.string :poa_code, limit: 3
+      t.string :individual_type, null: false
+      t.string :first_name
+      t.string :middle_initial
+      t.string :last_name
+      t.string :full_name
+      t.string :email
+      t.string :phone
+      t.string :address_type
+      t.string :address_line1
+      t.string :address_line2
+      t.string :address_line3
+      t.string :city
+      t.string :country_code_iso3
+      t.string :country_name
+      t.string :county_name
+      t.string :county_code
+      t.string :international_postal_code
+      t.string :province
+      t.string :state_code
+      t.string :zip_code
+      t.string :zip_suffix
+      t.jsonb :raw_address
+      t.float :lat
+      t.float :long
+      t.geography :location, limit: { srid: 4326, type: 'st_point', geographic: true }
+      t.timestamps
+
+      t.index %i[ registration_number individual_type ], name: 'index_reg_num_and_indi_type', unique: true
+    end
+  end
+end
