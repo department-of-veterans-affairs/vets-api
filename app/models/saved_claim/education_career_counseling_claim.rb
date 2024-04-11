a55a260e617668a2683162cda7493bba051fc3ca
Micah Frazier <Micah.frazier@oddball.io>
2024-04-11 16:26:32 -0600

    Added new feature flag name

--- app/models/saved_claim/education_career_counseling_claim.rb
+++ app/models/saved_claim/education_career_counseling_claim.rb
@@ -27,7 +27,7 @@ class SavedClaim::EducationCareerCounselingClaim < CentralMailClaim
     files = PersistentAttachment.where(guid: refs.map(&:confirmationCode))
     files.find_each { |f| f.update(saved_claim_id: id) }
 
-    if Flipper.enabled?(:central_mail_benefits_intake_submission)
+    if Flipper.enabled?(:ecc_benefits_intake_submission)
       Lighthouse::SubmitBenefitsIntakeClaim.new.perform(id)
     else
       CentralMail::SubmitSavedClaimJob.new.perform(id)
