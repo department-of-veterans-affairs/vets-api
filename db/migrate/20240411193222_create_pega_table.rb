59d2c517973d8456b907d68a5978d6323d8ce9ff
Don Shin <cloudmagic80@gmail.com>
2024-04-11 14:38:26 -0500

    add pegatable

---
+++ db/migrate/20240411193222_create_pega_table.rb
@@ -0,0 +1,20 @@
+class CreatePegaTable < ActiveRecord::Migration[7.1]
+  def change
+    create_table :pega_tables do |t|
+      t.uuid :uuid
+      t.string :veteranfirstname
+      t.string :veteranmiddlename
+      t.string :veteranlastname
+      t.string :sponsorfirstname
+      t.string :sponsormiddlename
+      t.string :sponsorlastname
+      t.jsonb :response
+      t.string :filenumber
+      t.string :doctype
+      t.datetime :date_created
+      t.datetime :date_completed
+
+      t.timestamps
+    end
+  end
+end
