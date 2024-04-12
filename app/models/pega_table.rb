8552d7e9b7ae80bac417d3490609616d38b12ecf
Don Shin <cloudmagic80@gmail.com>
2024-04-12 13:38:22 -0500

    linting rubocop down

--- app/models/pega_table.rb
+++ app/models/pega_table.rb
@@ -1,4 +1,4 @@
-# app/models/pega_table.rb
+# frozen_string_literal: true
 
 class PegaTable < ApplicationRecord
   # Validate presence of essential fields
@@ -12,12 +12,12 @@ class PegaTable < ApplicationRecord
   private
 
   def validate_response_format
-      return unless response.present?
-      response_hash = JSON.parse(response)
-      unless response_hash['status'].present? && [200, 403, 500].include?(response_hash['status'].to_i)
-        errors.add(:response, "must contain a valid HTTP status code (200, 403, 500)")
-      end
-    rescue JSON::ParserError => e
-      errors.add(:response, "must be a valid JSON format")
+    return unless response.present?
+    response_hash = JSON.parse(response)
+    unless response_hash['status'].present? && [200, 403, 500].include?(response_hash['status'].to_i)
+      errors.add(:response, "must contain a valid HTTP status code (200, 403, 500)")
     end
+  rescue JSON::ParserError => e
+    errors.add(:response, "must be a valid JSON format")
   end
+end
