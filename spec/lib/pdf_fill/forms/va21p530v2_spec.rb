c81ef53992df9762898c6370c0956be980b2389d
Tai Wilkin-Corraggio <tai.wilkin@coforma.io>
2024-04-12 13:29:53 -0400

    Leave benefit selections blank on pdf if unselected (#16318)

--- spec/lib/pdf_fill/forms/va21p530v2_spec.rb
+++ spec/lib/pdf_fill/forms/va21p530v2_spec.rb
@@ -142,5 +142,18 @@ describe PdfFill::Forms::Va21p530v2 do
         get_fixture('pdf_fill/21P-530V2/merge_fields').to_json
       )
     end
+
+    it 'leaves benefit selections blank on pdf if unselected', run_at: '2024-03-21 00:00:00 EDT' do
+      unselected_benefits_data = get_fixture('pdf_fill/21P-530V2/kitchen_sink').except(
+        'burialAllowance', 'plotAllowance', 'transportation'
+      )
+      expected_merge_data = get_fixture('pdf_fill/21P-530V2/merge_fields').except(
+        'burialAllowance', 'plotAllowance', 'transportation'
+      )
+      expected_merge_data['hasTransportation'] = nil
+      expect(described_class.new(unselected_benefits_data).merge_fields.to_json).to eq(
+        expected_merge_data.to_json
+      )
+    end
   end
 end
