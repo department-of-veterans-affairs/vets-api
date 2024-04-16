6175903e421e18b362d05e062616b51f0aabc4a9
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-04-16 11:33:29 -0500

    Updates to take in options as the params for create_veteran_representative

--- modules/claims_api/lib/bgs_service/veteran_representative_service/create_veteran_representative_request.rb
+++ modules/claims_api/lib/bgs_service/veteran_representative_service/create_veteran_representative_request.rb
@@ -2,22 +2,11 @@
 
 module ClaimsApi
   class VeteranRepresentativeService < ClaimsApi::LocalBGS
-    # rubocop:disable Metrics/MethodLength
-    def create_veteran_representative(ptcpnt_id:, proc_id:, form_type:, poa_code:)
+    def create_veteran_representative(options)
+      injected = convert_nil_values(options)
       body = Nokogiri::XML::DocumentFragment.parse <<~EOXML
         <data:VeteranRepresentative>
-          <procId>#{proc_id}</procId>
-          <formTypeCode>#{form_type}</formTypeCode>
-          <poaCode>#{poa_code}</poa_code>
-          <section7332Auth>false</section7332Auth>
-          <limitationDrugAbuse>false</limitationDrugAbuse>
-          <limitationAlcohol>false</limitationAlcohol>
-          <limitationHIV>false</limitationHIV>
-          <limitationSCA>false</limitationSCA>
-          <vdcStatus>This</vdcStatus>
-          <representativeType></representativeType>
-          <changeAddressAuth>false</changeAddressAuth>
-          <veteranPtcpntId>#{ptcpnt_id}</veteranPtcpntId>
+          #{injected}
         </data:VeteranRepresentative>
       EOXML
 
@@ -28,6 +17,5 @@ module ClaimsApi
         key: 'VeteranRepresentative'
       )
     end
-    # rubocop:enable Metrics/MethodLength
   end
 end
