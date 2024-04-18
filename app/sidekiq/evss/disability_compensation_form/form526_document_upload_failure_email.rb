8f47b8810bea84b3429590a7d3ea5216512d1897
Nathan Burgess <burgess.nathan28@gmail.com>
2024-04-18 12:22:08 -0400

    Fix mailer job linting errors
    Includes refactoring perform method to get under strict method line count linting

--- app/sidekiq/evss/disability_compensation_form/form526_document_upload_failure_email.rb
+++ app/sidekiq/evss/disability_compensation_form/form526_document_upload_failure_email.rb
@@ -43,47 +43,50 @@ module EVSS
       def perform(form526_submission_id, supporting_evidence_attachment_guid)
         @notify_client ||= VaNotify::Service.new(Settings.vanotify.services.benefits_disability.api_key)
         submission = Form526Submission.find(form526_submission_id)
+        send_notification_mailer(submission, supporting_evidence_attachment_guid)
+
+        StatsD.increment(STATSD_SENT_METRIC_KEY)
+      rescue => e
+        retryable_error_handler(e)
+      end
+
+      private
+
+      def send_notification_mailer(submission, supporting_evidence_attachment_guid)
+        form_attachment = SupportingEvidenceAttachment.find_by!(guid: supporting_evidence_attachment_guid)
+
+        # We need to obscure the original filename since it may contain PII
+        obscured_filename = form_attachment.obscured_filename
 
         email_address = submission.get_email_address
         first_name = submission.get_first_name
         date_submitted = submission.get_formatted_creation_time
-
-        template_id = Settings.vanotify.services
-                              .benefits_disability.template_id.form526_document_upload_failure_notification_template_id
-
-        form_attachment = SupportingEvidenceAttachment.find_by!(guid: supporting_evidence_attachment_guid)
-        # We need to obscure the original filename since it may contain PII
-        obscured_filename = form_attachment.obscured_filename
-
         @notify_client.send_email(
           email_address:,
-          template_id:,
-          personalisation: {
-            first_name:,
-            filename: obscured_filename,
-            date_submitted:
-          }
+          template_id: mailer_template_id,
+          personalisation: { first_name:, filename: obscured_filename, date_submitted: }
         )
 
         Rails.logger.info(
           'Form526DocumentUploadFailureEmail notification dispatched',
           {
             obscured_filename:,
-            form526_submission_id:,
-            supporting_evidence_attachment_guid: supporting_evidence_attachment_guid,
+            form526_submission_id: submission.id,
+            supporting_evidence_attachment_guid:,
             timestamp: Time.now.utc
           }
         )
-
-        StatsD.increment(STATSD_SENT_METRIC_KEY)
-      rescue => e
-        retryable_error_handler(e)
       end
-    end
 
-    def retryable_error_handler(error)
-      super(error)
-      raise error
+      def mailer_template_id
+        Settings.vanotify.services
+                .benefits_disability.template_id.form526_document_upload_failure_notification_template_id
+      end
+
+      def retryable_error_handler(error)
+        super(error)
+        raise error
+      end
     end
   end
 end
