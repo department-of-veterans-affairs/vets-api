4f4149201f78f40a7c1dcd6a7a471c7bac221685
Bryan Alexander <bryan.alexander@adhocteam.us>
2024-04-18 17:16:12 -0400

    80872: Change modle to one liner and add more tests

--- app/models/ivc_champva_form.rb
+++ app/models/ivc_champva_form.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class IvcChampvaForm < ApplicationRecord
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
   # Add more complex data modeling here outside of CRUD
 end
