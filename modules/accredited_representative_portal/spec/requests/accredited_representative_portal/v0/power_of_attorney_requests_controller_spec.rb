11626f40d79decaabf90b081dbd952c9a4213121
cohnjesse <cohnjesse@gmail.com>
2024-04-18 11:35:46 -0400

    Added tests for flipper toggle

--- modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_requests_controller_spec.rb
+++ modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_requests_controller_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
     let(:proc_id) { '123' }
 
     it 'returns a successful response with an accepted message' do
+      Flipper.enable(:accredited_representative_portal_pilot)
       post "/accredited_representative_portal/v0/power_of_attorney_requests/#{proc_id}/accept"
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
@@ -25,6 +26,7 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
     let(:proc_id) { '123' }
 
     it 'returns a successful response with a declined message' do
+      Flipper.enable(:accredited_representative_portal_pilot)
       post "/accredited_representative_portal/v0/power_of_attorney_requests/#{proc_id}/decline"
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
@@ -35,6 +37,7 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
   describe 'GET /index' do
     context 'when valid POA codes are provided' do
       it 'returns a successful response with matching POA requests' do
+        Flipper.enable(:accredited_representative_portal_pilot)
         get '/accredited_representative_portal/v0/power_of_attorney_requests', params: { poa_codes: '091,A1Q' }
         expect(response).to have_http_status(:ok)
         json = JSON.parse(response.body)
@@ -45,6 +48,7 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
 
     context 'when no POA codes are provided' do
       it 'returns a bad request status with an error message' do
+        Flipper.enable(:accredited_representative_portal_pilot)
         get '/accredited_representative_portal/v0/power_of_attorney_requests'
         expect(response).to have_http_status(:bad_request)
         json = JSON.parse(response.body)
@@ -54,6 +58,7 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
 
     context 'when POA codes parameter is empty' do
       it 'returns a bad request status with an error message' do
+        Flipper.enable(:accredited_representative_portal_pilot)
         get '/accredited_representative_portal/v0/power_of_attorney_requests', params: { poa_codes: '' }
         expect(response).to have_http_status(:bad_request)
         expect(JSON.parse(response.body)['error']).to eq('POA codes are required')
@@ -62,12 +67,34 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
 
     context 'when there are no records for the provided POA codes' do
       it 'returns an empty records array and zero records count' do
+        Flipper.enable(:accredited_representative_portal_pilot)
         get '/accredited_representative_portal/v0/power_of_attorney_requests', params: { poa_codes: 'XYZ,ABC' }
         expect(response).to have_http_status(:ok)
         json = JSON.parse(response.body)
         expect(json['records']).to be_an_instance_of(Array)
         expect(json['records']).to be_empty
         expect(json['records_count']).to eq(0)
+      end
+    end
+
+    context 'when pilot feature flag is disabled' do
+      let(:proc_id) { '123' }
+
+      Flipper.disable(:accredited_representative_portal_pilot)
+
+      it 'returns a not found status for POST /accept' do
+        post "/accredited_representative_portal/v0/power_of_attorney_requests/#{proc_id}/accept"
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it 'returns a not found status for POST /decline' do
+        post "/accredited_representative_portal/v0/power_of_attorney_requests/#{proc_id}/decline"
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it 'returns a not found status for GET POA codes' do
+        get '/accredited_representative_portal/v0/power_of_attorney_requests', params: { poa_codes: 'XYZ,ABC' }
+        expect(response).to have_http_status(:forbidden)
       end
     end
   end
