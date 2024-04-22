2f0d359aae0b3b592e3b4c38475637a1bd34cd00
Khoa Nguyen <134089461+Khoa-V-Nguyen@users.noreply.github.com>
2024-04-22 15:11:36 -0600

    BE | Ask Va Api:  `Inquiry` submit (#16445)
    * Updated `Inquiries::Creator`
    
    - update creator to `put` to `CRM API`
    
    * Updated `InquiriesController`
    
    - rework `create` and `unauth_create` for live `put`.
    
    * Update spec and method due to linting
    
    ---------
    
    Co-authored-by: khoa-v-nguyen <khoa.nguyen@oddball.io>

--- modules/ask_va_api/app/controllers/ask_va_api/v0/inquiries_controller.rb
+++ modules/ask_va_api/app/controllers/ask_va_api/v0/inquiries_controller.rb
@@ -26,12 +26,13 @@ module AskVAApi
       end
 
       def create
-        render json: { message: 'success' }, status: :created
+        response = Inquiries::Creator.new(icn: current_user.icn).call(params: inquiry_params)
+        render json: response.to_json, status: :created
       end
 
       def unauth_create
         response = Inquiries::Creator.new(icn: nil).call(params: inquiry_params)
-        render json: { message: response }, status: :created
+        render json: response.to_json, status: :created
       end
 
       def upload_attachment
@@ -60,10 +61,6 @@ module AskVAApi
       end
 
       private
-
-      def inquiry_params
-        params.permit(:first_name, :last_name).to_h
-      end
 
       def get_inquiry_by_id
         inq = retriever.fetch_by_id(id: params[:id])
@@ -96,6 +93,64 @@ module AskVAApi
       def retriever
         entity_class = AskVAApi::Inquiries::Entity
         @retriever ||= Inquiries::Retriever.new(icn: current_user.icn, user_mock_data: params[:mock], entity_class:)
+      end
+
+      def inquiry_params
+        params.permit(
+          *base_parameters,
+          *dependant_parameters,
+          *submitter_parameters,
+          *veteran_parameters,
+          school_obj: school_parameters
+        ).to_h
+      end
+
+      def base_parameters
+        %i[
+          AreYouTheDependent AttachmentPresent BranchOfService City ContactMethod Country
+          DaytimePhone EmailAddress EmailConfirmation FirstName Gender InquiryAbout
+          InquiryCategory InquirySource InquirySubtopic InquirySummary InquiryTopic
+          InquiryType IsVAEmployee IsVeteran IsVeteranAnEmployee IsVeteranDeceased
+          LevelOfAuthentication MedicalCenter MiddleName PreferredName Pronouns
+          StreetAddress2 SupervisorFlag VaEmployeeTimeStamp ZipCode
+        ]
+      end
+
+      def dependant_parameters
+        %i[
+          DependantCity DependantCountry DependantDayTimePhone DependantDOB
+          DependantEmail DependantFirstName DependantGender DependantLastName
+          DependantMiddleName DependantProvince DependantRelationship DependantSSN
+          DependantState DependantStreetAddress DependantZipCode
+        ]
+      end
+
+      def submitter_parameters
+        %i[
+          Submitter SubmitterDependent SubmitterDOB SubmitterGender SubmitterProvince
+          SubmitterSSN SubmitterState SubmitterStateOfResidency SubmitterStateOfSchool
+          SubmitterStateProperty SubmitterStreetAddress SubmitterVetCenter
+          SubmitterZipCodeOfResidency SubmitterQuestion
+        ]
+      end
+
+      def veteran_parameters
+        %i[
+          VeteranCity VeteranClaimNumber VeteranCountry VeteranDateOfDeath
+          VeteranDOB VeteranDodIdEdipiNumber VeteranEmail VeteranEmailConfirmation
+          VeteranEnrolled VeteranFirstName VeteranICN VeteranLastName VeteranMiddleName
+          VeteranPhone VeteranPreferedName VeteranPronouns VeteranProvince
+          VeteranRelationship VeteranServiceEndDate VeteranServiceNumber
+          VeteranServiceStartDate VeteranSSN VeteransState VeteranStreetAddress
+          VeteranSuffix VeteranSuiteAptOther VeteranZipCode WhoWasTheirCounselor
+          YourLastName
+        ]
+      end
+
+      def school_parameters
+        %i[
+          City InstitutionName RegionalOffice SchoolFacilityCode StateAbbreviation
+        ]
       end
 
       Result = Struct.new(:payload, :status, keyword_init: true)
