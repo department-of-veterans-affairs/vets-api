b2eb0f0d29f29fd2f8e6beef999d86ecb1630de2
Tom Harrison <tom.harrison@adhocteam.us>
2024-04-23 10:05:23 -0400

    Update ppiu policy to reject access based on feature toggle

--- app/policies/ppiu_policy.rb
+++ app/policies/ppiu_policy.rb
@@ -13,13 +13,25 @@ PPIUPolicy = Struct.new(:user, :ppiu) do
   end
 
   def access?
+    raise_error if reject?
     user.loa3? &&
       allowed_providers.include?(user.identity.sign_in[:service_name])
   end
 
   def access_update?
+    raise_error if reject?
+
     res = EVSS::PPIU::Service.new(user).get_payment_information
 
     res.responses.first.control_information.authorized?
+  end
+
+  def reject?
+    Flipper.enabled?(:profile_ppiu_reject_requests, user)
+  end
+
+  def raise_error
+    message = 'The EVSS PPIU endpoint will be decommissioned. All requests are now blocked.'
+    raise Common::Exceptions::Forbidden, detail: message
   end
 end
