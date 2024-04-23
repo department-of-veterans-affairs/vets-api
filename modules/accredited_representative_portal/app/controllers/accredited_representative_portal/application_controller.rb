05b57f2c19361d2b39058a13ed1376e49373e57c
cohnjesse <cohnjesse@gmail.com>
2024-04-23 16:27:11 -0400

    Addressed PR feedback

--- modules/accredited_representative_portal/app/controllers/accredited_representative_portal/application_controller.rb
+++ modules/accredited_representative_portal/app/controllers/accredited_representative_portal/application_controller.rb
@@ -11,10 +11,12 @@ module AccreditedRepresentativePortal
     service_tag 'accredited-representative-portal'
     validates_access_token_audience Settings.sign_in.arp_client_id
 
+    before_action :verify_pilot_enabled_for_user
+
     def verify_pilot_enabled_for_user
       unless Flipper.enabled?(:accredited_representative_portal_pilot, @current_user)
         message = 'The accredited_representative_portal_pilot feature flag is disabled ' \
-                  "for the user with email: #{@current_user.email}"
+                  "for the user with uuid: #{@current_user.uuid}"
 
         raise Common::Exceptions::Forbidden, detail: message
       end
