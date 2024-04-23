5c10183f1a988d3a68e83d569a133831c5867930
Seth Darr <seth.darr@agile6.com>
2024-04-23 13:58:01 -0700

    remove aborted test attempt

--- spec/controllers/v0/disability_compensation_forms_controller_spec.rb
+++ spec/controllers/v0/disability_compensation_forms_controller_spec.rb
@@ -123,10 +123,4 @@ RSpec.describe V0::DisabilityCompensationFormsController, type: :controller do
       end
     end
   end
-
-  describe 'submit_all_claim' do
-    it 'returns a job Id' do
-      post(:submit_all_claim)
-    end
-  end
 end
