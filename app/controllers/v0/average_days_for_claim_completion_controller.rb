cd74b5c4cae70673f04000b0d49f550303fabad2
Joshua Backfield <joshua.backfield@va.gov>
2024-04-23 11:31:28 -0400

    98 Moved code from controller into the actual sidekiq job.

--- app/controllers/v0/average_days_for_claim_completion_controller.rb
+++ app/controllers/v0/average_days_for_claim_completion_controller.rb
@@ -5,29 +5,7 @@ module V0
     service_tag 'average-days-to-completion'
     skip_before_action :authenticate, only: :index
 
-    def connection
-      @conn ||= Faraday.new('https://www.va.gov/') do |faraday|
-        faraday.use      :breakers
-        faraday.use      Faraday::Response::RaiseError
-
-        faraday.request :json
-
-        faraday.response :json, content_type: /\bjson/
-        faraday.adapter Faraday.default_adapter
-      end
-    end
-
-    AVERAGE_DAYS_REGEX = /(([0-9]{1,3}(\.[0-9]{1,2})) days)/
-
-    def load_average_days
-      rtn = connection.get('/disability/after-you-file-claim/').body
-      rec = AverageDaysForClaimCompletion.new
-      rec.average_days = AVERAGE_DAYS_REGEX.match(rtn)[2].to_f
-      rec.save!
-    end
-
     def index
-      load_average_days
       rtn = AverageDaysForClaimCompletion.order('created_at DESC').first
       render json: {
         average_days: rtn.present? ? rtn.average_days : -1.0
--- app/sidekiq/load_average_days_for_claim_completion_job.rb
+++ app/sidekiq/load_average_days_for_claim_completion_job.rb
@@ -3,11 +3,32 @@
 class LoadAverageDaysForClaimCompletionJob
   include Sidekiq::Job
 
+  # this will go away once the file publishing is setup.
+  def connection
+    @conn ||= Faraday.new('https://www.va.gov/') do |faraday|
+      faraday.use      :breakers
+      faraday.use      Faraday::Response::RaiseError
+
+      faraday.request :json
+
+      faraday.response :json, content_type: /\bjson/
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  AVERAGE_DAYS_REGEX = /(([0-9]{1,3}(\.[0-9]{1,2})) days)/
+
+  def load_average_days
+    rtn = connection.get('/disability/after-you-file-claim/').body
+    rec = AverageDaysForClaimCompletion.new
+    rec.average_days = AVERAGE_DAYS_REGEX.match(rtn)[2].to_f
+    rec.save!
+  end
+
   def perform
     puts("Running LoadAverageDaysForClaimCompletionJob")
     logger.info("Performing LoadAverageDaysForClaimCompletionJob")
-    # forms = InProgressForm.where('expires_at < ?', Time.now.utc)
-    # logger.info("Deleting #{forms.count} old saved forms")
-    # forms.delete_all
+    load_average_days
   end
+
 end
