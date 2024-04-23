d055bbfe1dfe8de32352a5ac09675ce3faf59334
Eric Boehs <eric@boehs.com>
2024-04-23 14:22:46 -0500

    Fix and silence rubocop; probably not right

--- lib/common/hash_helpers.rb
+++ lib/common/hash_helpers.rb
@@ -11,7 +11,7 @@ module Common
       when Array
         val.map { |v| deep_transform_parameters!(v, &) }
       when Hash, ActionController::Parameters
-        val.keys.each do |k, v = val[k]|
+        val.each_key do |k, v = val[k]|
           val.delete(k)
           val[yield(k)] = deep_transform_parameters!(v, &)
         end
