8aebcd6d13ab77bd3e665802cedf95e5323da818
Rockwell Windsor Rice <129893414+rockwellwindsor-va@users.noreply.github.com>
2024-04-24 14:59:45 -0500

    Api 34938 poa v2 create veteran rep (#16212)
    * WPI:Adds start of service
    
    * WIP
    
    * WIP
    
    * WIP:Updates structure to be more in line with manage rep structure to maintain uniformity
    
    * WIP:Updates params
    
    * Updates to take in options as the params for create_veteran_representative
    
    * Revert Gemfile.lock change because of conflict
    
    * Removes comment
    
    * Adds tests for valid request and incorrect ptcpnt_id being used
    
    * Removes uneeded value from test file
    
    * Revert Gemfile.lock change
    
    * Fixes typo in cassette

--- modules/claims_api/lib/bgs_service/veteran_representative_service.rb
+++ modules/claims_api/lib/bgs_service/veteran_representative_service.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
+require_relative 'veteran_representative_service/create_veteran_representative_request'
 require_relative 'veteran_representative_service/read_all_veteran_representatives'
 
 module ClaimsApi
   class VeteranRepresentativeService < ClaimsApi::LocalBGS
     private
 
-    def make_request(**args)
+    def make_request(namespace:, **args)
       super(
         endpoint: 'VDC/VeteranRepresentativeService',
-        namespaces: { 'ns0' => '/data' },
+        namespaces: { namespace => '/data' },
         transform_response: false,
         **args
       )
