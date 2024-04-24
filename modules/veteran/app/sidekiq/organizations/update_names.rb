9e48558e6bae122a8b00a9eec72b0e83783f6ee1
Holden Hinkle <holden@holdenhinkle.com>
2024-04-24 13:44:25 -0400

    temporarily update org addresses in nightly UpdateNames job

--- modules/veteran/app/sidekiq/organizations/update_names.rb
+++ modules/veteran/app/sidekiq/organizations/update_names.rb
@@ -3,6 +3,7 @@
 require 'sidekiq'
 require 'sentry_logging'
 require_relative 'names'
+require_relative 'addresses'
 
 module Organizations
   class UpdateNames
@@ -18,6 +19,16 @@ module Organizations
       rescue => e
         log_message_to_sentry("Error updating organization name for POA in Organizations::UpdateNames: #{e.message}. POA: '#{org[:poa]}', Org Name: '#{org[:name]}'.") # rubocop:disable Layout/LineLength
         next
+      end
+
+      Organizations::Addresses.all.each do |org| # rubocop:disable Rails/FindEach
+        record = Veteran::Service::Organization.find_by(poa: org[:poa])
+        next unless record
+
+        record.update(address_line1: org[:address_line1], address_line2: org[:address_line2],
+                      address_line3: org[:address_line3], city: org[:city], state_code: org[:state_code], zip_code: org[:zip_code], zip_suffix: org[:zip_suffix]) # rubocop:disable Layout/LineLength
+      rescue => e
+        log_message_to_sentry("Error updating organization address for POA in Organizations::UpdateNames: #{e.message}. POA: '#{org[:poa]}'.") # rubocop:disable Layout/LineLength
       end
     end
   end
