ac6edc5f74a069a96661ca5e01776f6e190db1f0
Holden Hinkle <holden@holdenhinkle.com>
2024-04-24 14:59:42 -0400

    revert previous commit

--- modules/veteran/app/sidekiq/organizations/update_names.rb
+++ modules/veteran/app/sidekiq/organizations/update_names.rb
@@ -3,7 +3,6 @@
 require 'sidekiq'
 require 'sentry_logging'
 require_relative 'names'
-require_relative 'addresses'
 
 module Organizations
   class UpdateNames
@@ -19,16 +18,6 @@ module Organizations
       rescue => e
         log_message_to_sentry("Error updating organization name for POA in Organizations::UpdateNames: #{e.message}. POA: '#{org[:poa]}', Org Name: '#{org[:name]}'.") # rubocop:disable Layout/LineLength
         next
-      end
-
-      Organizations::Addresses.all.each do |org| # rubocop:disable Rails/FindEach
-        record = Veteran::Service::Organization.find_by(poa: org[:poa])
-        next unless record
-
-        record.update(address_line1: org[:address_line1], address_line2: org[:address_line2],
-                      address_line3: org[:address_line3], city: org[:city], state_code: org[:state_code], zip_code: org[:zip_code], zip_suffix: org[:zip_suffix]) # rubocop:disable Layout/LineLength
-      rescue => e
-        log_message_to_sentry("Error updating organization address for POA in Organizations::UpdateNames: #{e.message}. POA: '#{org[:poa]}'.") # rubocop:disable Layout/LineLength
       end
     end
   end
