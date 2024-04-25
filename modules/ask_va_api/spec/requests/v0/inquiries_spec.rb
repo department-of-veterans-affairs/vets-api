6f2022d405de12c82548badbf343e4986de9dcf2
khoa-v-nguyen <khoa.nguyen@oddball.io>
2024-04-25 17:46:49 -0600

    Update `Inquiries::Retriever` and `Correspondences::Retriever`

--- modules/ask_va_api/spec/requests/v0/inquiries_spec.rb
+++ modules/ask_va_api/spec/requests/v0/inquiries_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe AskVAApi::V0::InquiriesController, type: :request do
     JSON.parse(File.read('modules/ask_va_api/config/locales/get_inquiries_mock_data.json'))['Data']
   end
   let(:valid_id) { mock_inquiries.first['InquiryNumber'] }
-  let(:invalid_id) { 'invalid-id' }
+  let(:invalid_id) { 'A-20240423-30709' }
 
   before do
     allow(LogService).to receive(:new).and_return(logger)
@@ -212,14 +212,7 @@ RSpec.describe AskVAApi::V0::InquiriesController, type: :request do
             ',"ExceptionOccurred":true,"ExceptionMessage":"Data Validation: No Inquiries found by ' \
             'ID A-20240423-30709","MessageId":"ca5b990a-63fe-407d-a364-46caffce12c1"}'
         end
-        let(:failure) do
-          {
-            status: 400,
-            body:,
-            response_headers: nil,
-            url: nil
-          }
-        end
+        let(:failure) { Faraday::Response.new(response_body: body, status: 400) }
         let(:service) { instance_double(Crm::Service) }
 
         before do
@@ -234,11 +227,7 @@ RSpec.describe AskVAApi::V0::InquiriesController, type: :request do
 
         it_behaves_like 'common error handling', :unprocessable_entity, 'service_error',
                         'AskVAApi::Inquiries::InquiriesRetrieverError: ' \
-                        '{:status=>400, :body=>"{\"Data\":null,\"Message\":\"Data Validation: ' \
-                        'No Inquiries found by ID A-20240423-30709\",\"ExceptionOccurred\":' \
-                        'true,\"ExceptionMessage\":\"Data Validation: No Inquiries found by ID ' \
-                        'A-20240423-30709\",\"MessageId\":\"ca5b990a-63fe-407d-a364-46caffce12c1\"}",' \
-                        ' :response_headers=>nil, :url=>nil}'
+                        'Data Validation: No Inquiries found by ID A-20240423-30709'
       end
     end
   end
