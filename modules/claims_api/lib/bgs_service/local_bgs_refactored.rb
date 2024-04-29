001dee0b823588de79dfa56be55905c10cecef67
Oren Mittman <nihil2501@gmail.com>
2024-04-29 11:37:32 -0400

    restore original `LocalBGS` w/ feature toggle to delegate to `LocalBGSRefactored`

---
+++ modules/claims_api/lib/bgs_service/local_bgs_refactored.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+# As a work of the United States Government, this project is in the
+# public domain within the United States.
+#
+# Additionally, we waive copyright and related rights in the work
+# worldwide through the CC0 1.0 Universal public domain dedication.
+
+require 'claims_api/claim_logger'
+require 'bgs_service/bgs_helpers'
+require 'bgs_service/bgs_error_handler'
+require 'bgs_service/miscellaneous_bgs_operations'
+
+module ClaimsApi
+  # @deprecated Use {BGSClient.perform_request} instead. There ought to be a
+  #   clear separation between the single method that performs the transport to
+  #   BGS and any business logic that invokes said transport. By housing that
+  #   single method as an instance method of this class, we encouraged
+  #   business logic modules to inherit this class and then inevitably start to
+  #   conflate business logic back into the transport layer here. There was a
+  #   particularly easy temptation to put business object state validation as
+  #   well as the dumping and loading of business object state into this layer,
+  #   but that should live in the business logic layer and not here.
+  class LocalBGSRefactored
+    include ClaimsApi::MiscellaneousBGSOperations
+    include ClaimsApi::BGSHelpers
+
+    class << self
+      def breakers_service
+        BGSClient.breakers_service
+      end
+    end
+
+    attr_reader :external_id
+
+    def initialize(
+      external_uid: Settings.bgs.external_uid,
+      external_key: Settings.bgs.external_key
+    )
+      @external_id =
+        BGSClient::ServiceAction::ExternalId.new(
+          external_uid:,
+          external_key:
+        )
+    end
+
+    def healthcheck(endpoint)
+      definition =
+        BGSClient::ServiceAction::Definition.new(
+          service_path: endpoint,
+          service_namespaces: nil,
+          action_name: nil
+        )
+
+      BGSClient.healthcheck(
+        definition
+      )
+    end
+
+    def make_request( # rubocop:disable Metrics/MethodLength, Metrics/ParameterLists
+      endpoint:, action:, body:, key: nil,
+      namespaces: {}, transform_response: true
+    )
+      definition =
+        BGSClient::ServiceAction::Definition.new(
+          service_path: endpoint,
+          service_namespaces: namespaces,
+          action_name: action
+        )
+
+      request =
+        BGSClient::ServiceAction.const_get(:Request).new(
+          definition:,
+          external_id:
+        )
+
+      result = request.perform(body)
+
+      if result.success?
+        value = result.value.to_h
+        value = value[key].to_h if key.present?
+
+        if transform_response
+          request.send(:log_duration, 'transformed_response') do
+            value.deep_transform_keys! do |key|
+              key.underscore.to_sym
+            end
+          end
+        end
+
+        return value
+      end
+
+      BGSErrorHandler.handle_errors!(
+        result.value
+      )
+
+      {}
+    end
+  end
+end
--- modules/claims_api/spec/lib/claims_api/local_bgs_refactored_spec.rb
+++ modules/claims_api/spec/lib/claims_api/local_bgs_refactored_spec.rb
@@ -7,19 +7,18 @@ require 'claims_api/error/soap_error_handler'
 describe ClaimsApi::LocalBGS do
   subject { described_class.new external_uid: 'xUid', external_key: 'xKey' }
 
-  let(:soap_error_handler) { ClaimsApi::SoapErrorHandler.new }
+  let(:soap_error_handler) { ClaimsApi::SoapErrorHandler }
 
   describe '#find_poa_by_participant_id' do
     it 'responds as expected, with extra ClaimsApi::Logger logging' do
       VCR.use_cassette('claims_api/bgs/claimant_web_service/find_poa_by_participant_id') do
-        allow_any_instance_of(BGS::OrgWebService).to receive(:find_poa_history_by_ptcpnt_id).and_return({})
-
         # Events logged:
-        # 1: establish_ssl_connection - how long to establish the connection
-        # 2: connection_wsdl_get - duration of WSDL request cycle
+        # 1: connection_wsdl_get - duration of WSDL request cycle
+        # 2: built_request - how long to build the request
         # 3: connection_post - how long does the post itself take for the request cycle
         # 4: parsed_response - how long to parse the response
-        expect(ClaimsApi::Logger).to receive(:log).exactly(4).times
+        # 5: transformed_response - how long to transform the response
+        expect(ClaimsApi::Logger).to receive(:log).exactly(5).times
         result = subject.find_poa_by_participant_id('does-not-matter')
         expect(result).to be_a Hash
         expect(result[:end_date]).to eq '08/26/2020'
@@ -44,9 +43,7 @@ describe ClaimsApi::LocalBGS do
     it 'triggers StatsD measurements' do
       VCR.use_cassette('claims_api/bgs/claimant_web_service/find_poa_by_participant_id',
                        allow_playback_repeats: true) do
-        allow_any_instance_of(BGS::OrgWebService).to receive(:find_poa_history_by_ptcpnt_id).and_return({})
-
-        %w[establish_ssl_connection connection_wsdl_get connection_post parsed_response].each do |event|
+        %w[connection_wsdl_get built_request connection_post parsed_response transformed_response].each do |event|
           expect { subject.find_poa_by_participant_id('does-not-matter') }
             .to trigger_statsd_measure("api.claims_api.local_bgs.#{event}.duration")
         end
@@ -107,9 +104,9 @@ describe ClaimsApi::LocalBGS do
                                                                ),
                                                                key: 'PersonDTO').and_return(:bgs_unknown_error_message)
         begin
-          allow(soap_error_handler).to receive(:handle_errors)
+          allow(soap_error_handler).to receive(:handle_errors!)
             .with(:bgs_unknown_error_message).and_raise(Common::Exceptions::UnprocessableEntity)
-          ret = soap_error_handler.send(:handle_errors, :bgs_unknown_error_message)
+          ret = soap_error_handler.send(:handle_errors!, :bgs_unknown_error_message)
           expect(ret.class).to_be Array
           expect(ret.size).to eq 1
         rescue => e
