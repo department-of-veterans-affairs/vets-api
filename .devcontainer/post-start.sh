43edcae92ec7d40e1b4db84de72ed15b33af0513
Adrian Rollett <acrollet@gmail.com>
2024-04-30 22:19:29 +0000

    Update ruby version on start as necessary

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Ensuring ruby is up to date..."
+asdf install ruby $( cat .ruby-version )
+asdf global ruby $( cat .ruby-version )
+
 echo "Starting redis..."
 nohup /home/linuxbrew/.linuxbrew/opt/redis@6.2/bin/redis-server /home/linuxbrew/.linuxbrew/etc/redis.conf >> log/redis.log 2>&1 &
 
