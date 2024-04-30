2c6c4a199b3f10ca65a7a9ce33eac86f809b752e
Adrian Rollett <acrollet@gmail.com>
2024-04-30 22:35:56 +0000

    Do bundle install

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -6,6 +6,9 @@ asdf plugin update ruby
 asdf install ruby $( cat .ruby-version )
 asdf global ruby $( cat .ruby-version )
 
+echo "Ensuring packages are up to date..."
+bundle install
+
 echo "Starting redis..."
 nohup /home/linuxbrew/.linuxbrew/opt/redis@6.2/bin/redis-server /home/linuxbrew/.linuxbrew/etc/redis.conf >> log/redis.log 2>&1 &
 
