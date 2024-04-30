e2b061b66e4733a2095d3f7b07b656196fd5d700
Gabriel Zurita <gabezurita@gmail.com>
2024-04-29 20:00:39 -0600

    arf.80297 Handle VerifiedRepresentative/AccreditedIndividual data issues
    Description:
    - Implement error handling for scenarios where a VerifiedRepresentative has a non-unique or non-existent email address in the associated AccreditedIndividual records.
    - For the pilot, log errors without raising exceptions to ensure a smooth user experience.
    - In the long term, consider raising errors and displaying appropriate messages to users when such inconsistencies are encountered.
    
    Discussion:
    - @raudabaugh discovered that a small number of representatives currently share email addresses in the OGC data set.
    - To maintain data integrity and simplify the login process for ARP, it is recommended to require representatives to have unique email addresses.
    - Allowing non-unique email addresses introduces complexity in two directions:
      1. Using user-entered OGC registration numbers for authentication, which are unique but public, essentially trusting users to provide accurate information.
      2. Using first and last names for authentication, which are not unique and can change (e.g., due to marriage), potentially leading to loss of access to ARP.
    - For the pilot, the current approach of logging errors without raising exceptions is sufficient.
    - In the long term, consider implementing a probabilistic approach (similar to MPI) for identity matching based on names, rather than a deterministic one, to avoid painful access scenarios for users (as reported with SEP by Melissa Rebstock).
    
    Related discussion: https://github.com/department-of-veterans-affairs/vets-api/pull/16493/files#r1579523634
    
    CC: @raudabaugh @amprokop

--- modules/accredited_representative_portal/app/models/accredited_representative_portal/verified_representative.rb
+++ modules/accredited_representative_portal/app/models/accredited_representative_portal/verified_representative.rb
@@ -16,24 +16,81 @@ module AccreditedRepresentativePortal
   # This model may eventually be associated with AccreditedIndividuals to pull POA codes,
   # if they exist, based on the OGC registration number. It currently does so via a helper method.
   class VerifiedRepresentative < ApplicationRecord
-    EMAIL_CONFLICT_ERROR_MESSAGE =
-      'Conflict with multiple `AccreditedIndividuals` having the same email. ' \
-      'Please review before attempting to add this `VerifiedRepresentative` record.'
+    EMAIL_NO_MATCH_MESSAGE = 'No matching email for AccreditedIndividual found for VerifiedRepresentative'
+    EMAIL_MULTIPLE_MATCH_MESSAGE =
+      'Multiple `AccreditedIndividuals` have the same email. Please review.'
 
     validates :ogc_registration_number, presence: true, uniqueness: { case_sensitive: false }
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-    before_save :validate_unique_accredited_individual_email
+    before_save :handle_email_uniqueness
 
-    def validate_unique_accredited_individual_email
-      individuals = AccreditedIndividual.where(email:)
-      errors.add(:email, EMAIL_CONFLICT_ERROR_MESSAGE) if individuals.count > 1
+    # Fetches Power of Attorney (POA) codes based on the representative's registration number.
+    # This method performs checks on the uniqueness of the representative's email
+    # and the existence of a corresponding AccreditedIndividual.
+    #
+    # It handles potential errors in data retrieval gracefully by logging any exceptions
+    # and returning nil in such cases.
+    #
+    # @return [Array<String>, nil] the POA codes if found and valid; otherwise, nil if:
+    #   - no AccreditedIndividual matches the provided registration number,
+    #   - an exception occurs during data retrieval.
+    def poa_codes
+      handle_email_uniqueness
+      fetch_poa_codes
+    rescue => e
+      log_fetch_failure(e)
+      nil
     end
 
-    # NOTE: given there will be RepresentativeUsers who are not VerifiedRepresentatives,
-    # it's okay for this to return nil
-    def poa_codes
-      AccreditedIndividual.find_by(registration_number: ogc_registration_number)&.poa_codes
+    private
+
+    # TODO: change this to a validation that returns error messages when a more automated approach is implemented
+    # See discussion: https://github.com/department-of-veterans-affairs/vets-api/pull/16493/files#r1579523634
+    def handle_email_uniqueness
+      Rails.logger.info(EMAIL_NO_MATCH_MESSAGE) if accredited_individual_email_count.zero?
+      log_accredited_individual_multiple_email_match if accredited_individual_email_count > 1
+    end
+
+    # Fetches the Power of Attorney (POA) codes from an AccreditedIndividual based on the OGC registration number.
+    # Logs and handles cases where no AccreditedIndividual or no POA codes are found.
+    #
+    # @return [Array<String>, nil] The POA codes if available, or nil if not found or if no codes exist.
+    def fetch_poa_codes
+      accredited_individual = AccreditedIndividual.find_by(registration_number: ogc_registration_number)
+
+      if accredited_individual&.poa_codes.present?
+        accredited_individual.poa_codes
+      else
+        log_accredited_individual_missing_data(accredited_individual)
+        nil
+      end
+    end
+
+    def log_accredited_individual_missing_data(accredited_individual)
+      if accredited_individual.nil?
+        Rails.logger.info("No matching AccreditedIndividual found for VerifiedRepresentative ID: #{id}")
+      else
+        Rails.logger.info("No matching POA codes for VerifiedRepresentative ID: #{id}")
+      end
+    end
+
+    def log_accredited_individual_multiple_email_match
+      message = "#{EMAIL_MULTIPLE_MATCH_MESSAGE} AccreditedIndividual IDs: #{fetch_accredited_individual_ids}"
+      message += " VerifiedRepresentative ID: #{id}" unless new_record?
+      Rails.logger.info(message)
+    end
+
+    def fetch_accredited_individual_ids
+      AccreditedIndividual.where(email:).pluck(:id).join(', ')
+    end
+
+    def log_fetch_failure(exception)
+      Rails.logger.info("Fetching POA codes failed for VerifiedRepresentative ID: #{id} - #{exception.message}")
+    end
+
+    def accredited_individual_email_count
+      @accredited_individual_email_count ||= AccreditedIndividual.where(email:).count
     end
   end
 end
