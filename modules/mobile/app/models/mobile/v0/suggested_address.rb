e9dedd9cec9156b278ce47c7e8f2487e24708946
Kris Pethtel <kris.pethtel@gmail.com>
2024-05-01 12:45:52 -0500

    wip

--- modules/mobile/app/models/mobile/v0/suggested_address.rb
+++ modules/mobile/app/models/mobile/v0/suggested_address.rb
@@ -5,7 +5,7 @@ require 'common/models/resource'
 module Mobile
   module V0
     class SuggestedAddress < Common::Resource
-      attribute :id, Types::String.optional.default(nil)
+      attribute :id, Types::String
       attribute :address_line1, Types::String.optional.default(nil)
       attribute :address_line2, Types::String.optional.default(nil)
       attribute :address_line3, Types::String.optional.default(nil)
@@ -18,11 +18,11 @@ module Mobile
       attribute :state_code, Types::String.optional.default(nil)
       attribute :zip_code, Types::String.optional.default(nil)
       attribute :zip_code_suffix, Types::String.optional.default(nil)
-      attribute :validation_key, Types::Integer
+      attribute :validation_key, Types::Integer.optional.default(nil)
       attribute :address_meta do
-        attribute :confidence_score, Types::Float
-        attribute :address_type, Types::String
-        attribute :delivery_point_validation, Types::String
+        attribute :confidence_score, Types::Float.optional.default(nil)
+        attribute :address_type, Types::String.optional.default(nil)
+        attribute :delivery_point_validation, Types::String.optional.default(nil)
       end
     end
   end
