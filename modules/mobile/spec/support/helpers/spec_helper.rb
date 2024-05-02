4bd3a3f53116ad842ce63fca7717c1a6f025d70c
Andrew Herzberg <andrew.herzberg@adhocteam.us>
2024-05-02 10:23:08 -0700

    add spec_helper and validate all request specs against json api

--- modules/mobile/spec/support/helpers/spec_helper.rb
+++ modules/mobile/spec/support/helpers/spec_helper.rb
@@ -5,10 +5,14 @@ RSpec.configure do |config|
     Flipper.enable('va_online_scheduling')
   end
 
+  config.define_derived_metadata(file_path: %r{modules/mobile/spec}) do |metadata|
+    metadata[:json_api] = true
+  end
+
   config.after :each, type: :request do |example|
     content_type = response.header['Content-Type']
 
-    if content_type != 'application/pdf' && response.body.present? &&
+    if content_type != 'application/pdf' && response.body.present? && example.metadata[:json_api] &&
        response.body != 'null' && !example.metadata[:skip_json_api_validation]
 
       expect(JSONAPI.parse_response!(response.parsed_body)).to eq(nil)
