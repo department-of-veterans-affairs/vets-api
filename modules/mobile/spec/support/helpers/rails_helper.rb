2028122c77cc3bd6d14ef77f76b54383c25967e3
Andrew Herzberg <andrew.herzberg@adhocteam.us>
2024-05-03 13:54:58 -0700

    change over to rail_helper

--- modules/mobile/spec/support/helpers/rails_helper.rb
+++ modules/mobile/spec/support/helpers/rails_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative 'spec_helper'
 require_relative 'sis_session_helper'
 require_relative '../../support/matchers/json_schema_matcher'
 
