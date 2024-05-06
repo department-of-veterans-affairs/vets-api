7f030b3b59b3b731d3e494a9fb8bc697affeb6e1
khenson-oddball <145150351+khenson-oddball@users.noreply.github.com>
2024-05-06 16:57:40 -0600

    Iir 628 facilities api v2 ppms (#16646)

--- modules/facilities_api/config/routes.rb
+++ modules/facilities_api/config/routes.rb
@@ -15,6 +15,15 @@ FacilitiesApi::Engine.routes.draw do
   end
 
   namespace :v2, defaults: { format: 'json' } do
+    resources :ccp, only: :index do
+      collection do
+        get 'urgent_care',  to: 'ccp#urgent_care'
+        get 'provider',     to: 'ccp#provider'
+        get 'pharmacy',     to: 'ccp#pharmacy'
+        get 'specialties',  to: 'ccp#specialties'
+      end
+    end
+
     get 'va/:id', to: 'va#show'
     post 'va', to: 'va#search', as: 'va_search'
 
