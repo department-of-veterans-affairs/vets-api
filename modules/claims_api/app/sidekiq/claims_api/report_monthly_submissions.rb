de63db77c7154a3f953835bcbb96e495a6d1c567
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-05-06 08:59:37 -0500

    WIP: Updates cron job timing and initial queries for records

--- modules/claims_api/app/sidekiq/claims_api/report_monthly_submissions.rb
+++ modules/claims_api/app/sidekiq/claims_api/report_monthly_submissions.rb
@@ -9,12 +9,20 @@ module ClaimsApi
 
       @to = Time.zone.now
       @from = 1.month.ago
-      submissions = ClaimsApi::ClaimSubmission.where(created_at: @from..@to)
+      pact_act_claims = ClaimsApi::ClaimSubmission.where(created_at: @from..@to)
+      disability_compensation_claims = ClaimsApi::AutoEstablishedClaim.where(created_at: @from..@to)
+      power_of_attorney = ClaimsApi::PowerOfAttorney.where(created_at: @from..@to)
+      intent_to_file = ClaimsApi::IntentToFile.where(created_at: @from..@to)
+      evidence_waiver = ClaimsApi::EvidenceWaiverSubmission.where(created_at: @from..@to)
 
       ClaimsApi::SubmissionReportMailer.build(
         @from,
         @to,
-        submissions
+        disability_compensation_claims,
+        pact_act_claims,
+        power_of_attorney,
+        intent_to_file,
+        evidence_waiver
       ).deliver_now
     end
   end
