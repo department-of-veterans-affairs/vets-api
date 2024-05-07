c5009f399327f3f59250a39d1cae62e9a4eecacf
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-05-06 23:29:56 -0500

    vye-339-ivr-handling-update

--- modules/vye/app/controllers/vye/v1/verifications_controller.rb
+++ modules/vye/app/controllers/vye/v1/verifications_controller.rb
@@ -10,6 +10,7 @@ module Vye
       class AwardsMismatch < StandardError; end
 
       include Pundit::Authorization
+      include Vye::Ivr
 
       service_tag 'verify-your-enrollment'
 
@@ -64,7 +65,13 @@ module Vye
       end
 
       def source_ind
-        :web
+        api_key? ? :phone : :web
+      end
+
+      def load_user_info
+        return super unless api_key?
+
+        @user_info = user_info_for_ivr
       end
 
       protected
