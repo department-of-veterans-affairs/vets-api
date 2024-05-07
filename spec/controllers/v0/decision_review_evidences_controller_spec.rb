fd2c41f834132efeb3d27f15f377c228a2e10ee2
Derek Fong <151783381+dfong-adh@users.noreply.github.com>
2024-05-07 00:55:37 -0700

    81205: Store DR evidence attachment validation data

--- spec/controllers/v0/decision_review_evidences_controller_spec.rb
+++ spec/controllers/v0/decision_review_evidences_controller_spec.rb
@@ -60,47 +60,26 @@ RSpec.describe V0::DecisionReviewEvidencesController, type: :controller do
       )
     end
 
-    it 'creates a FormAttachment, logs formatted error, and increments statsd' do
-      request.headers['Source-App-Name'] = '10182-board-appeal'
-      params = { param_namespace => { file_data: pdf_file, password: 'test_password' } }
-      allow(Rails.logger).to receive(:info)
-      expect(Rails.logger).to receive(:info).with({
-                                                    message: 'Evidence upload to s3 success!',
-                                                    user_uuid: user.uuid,
-                                                    action: 'Evidence upload to s3',
-                                                    form_id: '10182',
-                                                    upstream_system: nil,
-                                                    downstream_system: 'AWS S3',
-                                                    is_success: true,
-                                                    http: {
-                                                      status_code: nil,
-                                                      body: nil
-                                                    },
-                                                    form_attachment_guid:,
-                                                    encrypted: true
-                                                  })
-      expect(StatsD).to receive(:increment).with('decision_review.form_10182.evidence_upload_to_s3.success')
-      form_attachment = build(attachment_factory_id, guid: form_attachment_guid)
-
-      expect(form_attachment_model).to receive(:new) do
-        expect(form_attachment).to receive(:set_file_data!)
-
-        expect(form_attachment).to receive(:save!) do
-          form_attachment.id = 99
-          form_attachment
-        end
-
-        form_attachment
+    context 'with a param password' do
+      let(:encrypted_log_params_success) do
+        {
+          message: 'Evidence upload to s3 success!',
+          user_uuid: user.uuid,
+          action: 'Evidence upload to s3',
+          form_id: '10182',
+          upstream_system: nil,
+          downstream_system: 'AWS S3',
+          is_success: true,
+          http: {
+            status_code: nil,
+            body: nil
+          },
+          form_attachment_guid:,
+          encrypted: true
+        }
       end
 
-      expect(subject).to receive(:render).with(json: form_attachment).and_call_original # rubocop:disable RSpec/SubjectStub
-
-      post(:create, params:)
-
-      expect(response).to have_http_status(:ok)
-      expect(
-        JSON.parse(response.body)
-      ).to eq(
+      let(:expected_response_body) do
         {
           'data' => {
             'id' => '99',
@@ -110,7 +89,78 @@ RSpec.describe V0::DecisionReviewEvidencesController, type: :controller do
             }
           }
         }
-      )
+      end
+
+      context 'and the feature switch is disabled' do
+        before { Flipper.disable :decision_review_save_encrypted_attachments }
+
+        it 'creates a FormAttachment, logs formatted error, and increments statsd' do
+          request.headers['Source-App-Name'] = '10182-board-appeal'
+          params = { param_namespace => { file_data: pdf_file, password: 'test_password' } }
+
+          expect_any_instance_of(form_attachment_model::ATTACHMENT_UPLOADER_CLASS).not_to receive(:store!)
+          allow(Rails.logger).to receive(:info)
+          expect(Rails.logger).not_to receive(:info).with(
+            'DR-81205: Evidence Attachment Validation upload success', form_attachment_guid:
+          )
+          expect(Rails.logger).to receive(:info).with(encrypted_log_params_success)
+          expect(StatsD).to receive(:increment).with('decision_review.form_10182.evidence_upload_to_s3.success')
+          form_attachment = build(attachment_factory_id, guid: form_attachment_guid)
+
+          expect(form_attachment_model).to receive(:new) do
+            expect(form_attachment).to receive(:set_file_data!)
+
+            expect(form_attachment).to receive(:save!) do
+              form_attachment.id = 99
+              form_attachment
+            end
+
+            form_attachment
+          end
+          expect(subject).to receive(:render).with(json: form_attachment).and_call_original # rubocop:disable RSpec/SubjectStub
+
+          post(:create, params:)
+
+          expect(response).to have_http_status(:ok)
+          expect(JSON.parse(response.body)).to eq(expected_response_body)
+        end
+      end
+
+      context 'and the feature switch is enabled' do
+        before { Flipper.enable :decision_review_save_encrypted_attachments }
+
+        it 'creates a FormAttachment, logs formatted status, and stores the encrypted file' do
+          request.headers['Source-App-Name'] = '10182-board-appeal'
+          params = { param_namespace => { file_data: pdf_file, password: 'test_password' } }
+
+          expect_any_instance_of(form_attachment_model::ATTACHMENT_UPLOADER_CLASS).to receive(:store!)
+          allow(Rails.logger).to receive(:info)
+          expect(Rails.logger).to receive(:info).with(
+            'DR-81205: Evidence Attachment Validation upload success', form_attachment_guid:
+          )
+          expect(Rails.logger).to receive(:info).with(encrypted_log_params_success)
+          expect(StatsD).to receive(:increment).with('decision_review.form_10182.evidence_upload_to_s3.success')
+
+          form_attachment = build(attachment_factory_id, guid: form_attachment_guid)
+
+          expect(form_attachment_model).to receive(:new) do
+            expect(form_attachment).to receive(:set_file_data!)
+
+            expect(form_attachment).to receive(:save!) do
+              form_attachment.id = 99
+              form_attachment
+            end
+
+            form_attachment
+          end
+          expect(subject).to receive(:render).with(json: form_attachment).and_call_original # rubocop:disable RSpec/SubjectStub
+
+          post(:create, params:)
+
+          expect(response).to have_http_status(:ok)
+          expect(JSON.parse(response.body)).to eq(expected_response_body)
+        end
+      end
     end
 
     context 'evidence is uploaded from the NOD (10182) form' do
