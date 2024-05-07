2229177e461df01e358a6b283ad190c3bc62fd19
Oren Mittman <nihil2501@gmail.com>
2024-05-07 11:24:13 -0400

    API-34439 Claims API v2 POA request search

--- modules/claims_api/app/controllers/claims_api/v2/power_of_attorney_requests_controller.rb
+++ modules/claims_api/app/controllers/claims_api/v2/power_of_attorney_requests_controller.rb
@@ -1,13 +1,39 @@
 # frozen_string_literal: true
 
-require 'bgs_service/manage_representative_service'
-
 module ClaimsApi
   module V2
-    class PowerOfAttorneyRequestsController < ClaimsApi::V2::ApplicationController
+    class PowerOfAttorneyRequestsController < ApplicationController
+      BAD_GATEWAY_ERRORS = [
+        BGSClient::Error::BGSFault,
+        BGSClient::Error::ConnectionFailed,
+        BGSClient::Error::SSLError
+      ].freeze
+
       def index
-        poa_requests = ClaimsApi::PowerOfAttorneyRequestService::Search.perform
-        render json: Blueprints::PowerOfAttorneyRequestBlueprint.render(poa_requests, root: :data)
+        result =
+          PowerOfAttorneyRequestService::Search.perform(
+            search_params.to_h
+          )
+
+        result[:data] =
+          Blueprints::PowerOfAttorneyRequestBlueprint.render_as_hash(
+            result[:data]
+          )
+
+        render json: result
+      rescue PowerOfAttorneyRequestService::Search::InvalidQueryError => e
+        detail = { errors: e.errors, params: e.params }
+        raise ::Common::Exceptions::BadRequest, detail:
+      rescue *BAD_GATEWAY_ERRORS => e
+        raise ::Common::Exceptions::BadGateway, detail: e.message
+      rescue BGSClient::Error::TimeoutError
+        raise ::Common::Exceptions::GatewayTimeout
+      end
+
+      private
+
+      def search_params
+        params.permit(filter: {}, page: {}, sort: {})
       end
     end
   end
--- modules/claims_api/app/services/claims_api/power_of_attorney_request_service/search.rb
+++ modules/claims_api/app/services/claims_api/power_of_attorney_request_service/search.rb
@@ -3,38 +3,123 @@
 module ClaimsApi
   module PowerOfAttorneyRequestService
     module Search
+      class InvalidQueryError < StandardError
+        attr_reader :errors, :params
+
+        def initialize(errors, params)
+          @errors = errors
+          @params = params
+          super()
+        end
+      end
+
       class << self
-        def perform
-          # `Array.wrap` (the `ActiveSupport` core extension with nicer behavior
-          # than Ruby core) because upstream invocation of `Hash.from_xml` has
-          # different output depending on the cardinality of sibling XML
-          # elements for a given kind:
-          #    0 => Absent
-          #    1 => Object
-          #   >1 => Array
-          poa_requests = make_request['poaRequestRespondReturnVOList']
-          Array.wrap(poa_requests).map { |data| PoaRequest.new(data) }
+        def perform(params) # rubocop:disable Metrics/MethodLength
+          # This ensures that our data is totally valid. If not, it raises. If
+          # it is valid, it gives back a query object with defaults filled out
+          # that we can then show back to the client as metadata.
+          query = Query.compile!(params)
+
+          total_count = 0
+          data = []
+
+          begin
+            body = dump_query(query)
+            response = perform_request(body)
+
+            result = response['POARequestRespondReturnVO'].to_h
+            total_count = result['totalNbrOfRecords'].to_i
+
+            data = result['poaRequestRespondReturnVOList']
+            data = Array.wrap(data).map! do |datum|
+              # TODO: Rework hydration into `PoaRequest` and possibly collocate
+              # here with the rest of the dump/load code?
+              PoaRequest.load(datum)
+            end
+          rescue BGSClient::Error::BGSFault => e
+            # Sometimes this empty result is for valid reasons, i.e. a filter
+            # combo that just happens to have no records. Other times it is due
+            # to something like a non-existent POA code, which is an invalid
+            # request from the client. But the BGS response looks the same in
+            # either case so we can't distinguish between them.
+            raise unless e.message == 'No Record Found'
+          end
+
+          {
+            metadata: {
+              totalCount: total_count,
+              query:
+            },
+            data:
+          }
         end
 
         private
 
-        def make_request
-          bgs_client =
-            ClaimsApi::ManageRepresentativeService.new(
-              external_uid: 'xUid',
-              external_key: 'xKey'
-            )
+        # Check https://github.com/department-of-veterans-affairs/bgs-catalog for:
+        #   `VDC/ManageRepresentativeService/ManageRepresentativePortBinding/readPOARequest/request.xml`
+        def perform_request(body)
+          service_action =
+            BGSClient::ServiceAction::
+              ManageRepresentativeService::
+              ReadPoaRequest
 
-          bgs_client.read_poa_request(
-            poa_codes: ['012'],
-            statuses: %w[
-              new
-              pending
-              accepted
-              declined
-            ]
+          BGSClient.perform_request(
+            service_action:,
+            body:
           )
         end
+
+        def dump_query(query) # rubocop:disable Metrics/MethodLength
+          builder =
+            Nokogiri::XML::Builder.new(namespace_inheritance: false) do |xml|
+              # Need to declare an arbitrary root element with placeholder
+              # namespace in order to leverage namespaced tag building. The root
+              # element itself is later ignored and only used for its contents.
+              #   https://nokogiri.org/rdoc/Nokogiri/XML/Builder.html#method-i-5B-5D
+              xml.root('xmlns:data' => 'placeholder') do
+                filter = query[:filter]
+
+                xml['data'].SecondaryStatusList do
+                  filter[:statuses].each do |status|
+                    xml.SecondaryStatus(status)
+                  end
+                end
+
+                xml['data'].POACodeList do
+                  filter[:poaCodes].each do |poa_code|
+                    xml.POACode(poa_code)
+                  end
+                end
+
+                xml['data'].POARequestParameter do
+                  page = query[:page]
+                  xml.pageIndex(page[:number])
+                  xml.pageSize(page[:size])
+
+                  sort = query[:sort]
+                  xml.poaSortField(Sort::FIELDS.fetch(sort[:field]))
+                  xml.poaSortOrder(Sort::ORDERS.fetch(sort[:order]))
+                end
+              end
+            end
+
+          builder
+            .doc.at('root')
+            .children
+            .to_xml
+        end
+      end
+
+      module Sort
+        ORDERS = {
+          Query::Sort::Orders::ASCENDING => 'ASCENDING',
+          Query::Sort::Orders::DESCENDING => 'DESCENDING'
+        }.freeze
+
+        FIELDS = {
+          Query::Sort::Fields::SUBMITTED_AT => 'DATE_RECEIVED'
+        }.freeze
       end
     end
   end
