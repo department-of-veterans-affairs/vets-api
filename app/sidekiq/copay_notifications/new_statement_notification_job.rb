892933115622c1133e35db522a14d4774cb9aa68
Kevin Suarez <kjsuarez94@gmail.com>
2024-05-07 16:40:38 -0400

    thanks linter

--- app/sidekiq/copay_notifications/new_statement_notification_job.rb
+++ app/sidekiq/copay_notifications/new_statement_notification_job.rb
@@ -30,6 +30,7 @@ module CopayNotifications
     STATSD_KEY_PREFIX = 'api.copay_notifications.new_statement'
 
     def perform(statement)
+      # rubocop:disable Lint/UselessAssignment
       StatsD.increment("#{STATSD_KEY_PREFIX}.total")
       statement_service = DebtManagementCenter::StatementIdentifierService.new(statement)
       user_data = statement_service.get_mpi_data
@@ -41,7 +42,7 @@ module CopayNotifications
       statement_date = statement['statementDate']
       account_balance = statement['accountBalance']
       Rails.logger.info("Notification Data: date-#{statement_date}, balance-#{account_balance}")
-
+      # rubocop:enable Lint/UselessAssignment
       # pausing until further notice
       # DebtManagementCenter::VANotifyEmailJob.perform_async(icn, MCP_NOTIFICATION_TEMPLATE, personalization, 'icn')
     end
