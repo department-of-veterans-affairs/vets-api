348c29fb3f8c38483d56ea5a04d5bd87b15e61c3
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-05-07 13:46:15 -0500

    vye-339-ivr-handling-update (#16669)

--- modules/vye/app/controllers/vye/v1/user_infos_controller.rb
+++ modules/vye/app/controllers/vye/v1/user_infos_controller.rb
@@ -4,6 +4,7 @@ module Vye
   module V1
     class UserInfosController < Vye::V1::ApplicationController
       include Pundit::Authorization
+      include Vye::Ivr
 
       service_tag 'verify-your-enrollment'
 
@@ -14,13 +15,19 @@ module Vye
                serializer: Vye::UserInfoSerializer,
                key_transform: :camel_lower,
                adapter: :json,
-               include: %i[awards address_changes pending_documents].freeze
+               include: %i[address_changes pending_documents verifications pending_verifications].freeze
       end
 
       private
 
       def load_user_info
-        @user_info = Vye::UserProfile.with_assos.find_and_update_icn(user: current_user)&.active_user_info
+        return super(scoped: Vye::UserProfile.with_assos) unless api_key?
+
+        @user_info = user_info_for_ivr(scoped: Vye::UserProfile.with_assos)
+      end
+
+      def transformed_params
+        @transform_params ||= params.deep_transform_keys!(&:underscore)
       end
     end
   end
