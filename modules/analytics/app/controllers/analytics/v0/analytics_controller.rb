3bc19d869649f3838cc804ba3fbba03efa6859c4
Mark Dewey <mdewey@users.noreply.github.com>
2024-05-08 19:07:26 +0000

    (mvp)

--- modules/analytics/app/controllers/analytics/v0/analytics_controller.rb
+++ modules/analytics/app/controllers/analytics/v0/analytics_controller.rb
@@ -1,11 +1,18 @@
 module Analytics
   module V0
     class AnalyticsController < ApplicationController
-      skip_before_action :authenticate
-
+      before_action :authenticate
       def index
-        data = { hello: 'world', salt: Settings.analytics.unique_user.salt }
-        render json: data
+        return unless current_user
+        render json: {
+          user_fingerprint: get_user_fingerprint(current_user.uuid)
+        }
+      end
+
+      private 
+
+      def get_user_fingerprint(user_property)
+        Digest::SHA256.hexdigest("#{Settings.analytics.unique_user.salt}#{user_property}")
       end
     end
   end
