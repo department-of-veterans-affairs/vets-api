44866663db3d3a08ec68c366b2f40226b0644695
Gaurav Gupta <ggupta@kindsys.us>
2024-05-07 17:13:55 -0700

    81882 code for merging facilities data

--- modules/check_in/app/serializers/check_in/vaos/appointment_serializer.rb
+++ modules/check_in/app/serializers/check_in/vaos/appointment_serializer.rb
@@ -9,6 +9,23 @@ module CheckIn
       set_type :appointments
 
       attributes :kind, :status, :serviceType, :locationId, :clinic, :start, :end, :minutesDuration
+
+      attribute :facilityName do |object|
+        object.dig(:facility, :name)
+      end
+
+      attribute :facilityVistaSite do |object|
+        object.dig(:facility, :vistaSite)
+      end
+
+      attribute :facilityTimezone do |object|
+        object.dig(:facility, :timezone, :timeZoneId)
+      end
+
+      attribute :facilityPhoneMain do |object|
+        object.dig(:facility, :phone, :main)
+      end
+
     end
   end
 end
