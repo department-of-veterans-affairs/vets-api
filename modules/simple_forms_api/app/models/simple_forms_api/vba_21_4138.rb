cd3faf127d913e7ce2c50facdc2b4c21cc19f233
Jacob Penner <jacob.penner@adhocteam.us>
2024-05-08 14:54:24 -0400

    generate initial form metadata

---
+++ modules/simple_forms_api/app/models/simple_forms_api/vba_21_4138.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module SimpleFormsApi
+  class VBA214138
+    include Virtus.model(nullify_blank: true)
+
+    attribute :data
+
+    def initialize(data)
+      @data = data
+    end
+
+    def metadata
+      {
+        'ƒirstName' => @data.dig('full_name', 'first'),
+        'lastName' => @data.dig('full_name', 'last'),
+        'fileNumber' => @data.dig('id_number', 'va_file_number').presence || @data['ssn'],
+        'zipCode' => @data.dig('address', 'postal_code'),
+        'source' => 'VA Platform Digital Forms',
+        'docType' => @data['form_number'],
+        'businessLine' => 'CMP'
+      }
+    end
+  end
+end
