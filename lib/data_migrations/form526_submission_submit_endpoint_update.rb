e20911d8a0e92f1ccaa6cd6a0c9a757653a779ca
Scott Regenthal <scottregenthal@gmail.com>
2024-05-09 12:47:26 -0600

    Update submit_endpoint enum values

--- lib/data_migrations/form526_submission_submit_endpoint_update.rb
+++ lib/data_migrations/form526_submission_submit_endpoint_update.rb
@@ -8,15 +8,19 @@ module DataMigrations
       total_records = Form526Submission.where(status: nil).count
 
       Form526Submission.where(status: nil).find_in_batches do |batch|
+        # rubocop:disable Rails/SkipsModelValidations
         batch.update_all(submit_endpoint: :evss)
+        # rubocop:enable Rails/SkipsModelValidations
         processed_records += batch.size
 
         next unless (processed_records % 50_000).zero?
 
+        # rubocop:disable Rails/Output
         puts "Processed #{processed_records} out of #{total_records} records"
       end
 
       puts 'Submit endpoint updated successfully'
+      # rubocop:enable Rails/Output
     end
   end
 end
