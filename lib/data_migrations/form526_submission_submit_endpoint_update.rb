a5c6f581608c331af8d1c84f365afab88bc900e5
Scott Regenthal <scottregenthal@gmail.com>
2024-05-10 16:23:35 -0600

    Remove output and update batch method

--- lib/data_migrations/form526_submission_submit_endpoint_update.rb
+++ lib/data_migrations/form526_submission_submit_endpoint_update.rb
@@ -5,23 +5,11 @@ module DataMigrations
     module_function
 
     def run
-      submissions = Form526Submission.where(submit_endpoint: nil)
-      total_submissions = submissions.count
-
-      submissions.find_in_batches do |batch|
+      Form526Submission.where(submit_endpoint: nil).in_batches do |batch|
         # rubocop:disable Rails/SkipsModelValidations
         batch.update_all(submit_endpoint: :evss)
         # rubocop:enable Rails/SkipsModelValidations
-        processed_submissions += batch.size
-
-        next unless (processed_submissions % 50_000).zero?
-
-        # rubocop:disable Rails/Output
-        puts "Processed #{processed_submissions} out of #{total_submissions} submissions"
       end
-
-      puts 'Submit endpoint updated successfully'
-      # rubocop:enable Rails/Output
     end
   end
 end
