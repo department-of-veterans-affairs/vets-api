9b3e9943d9433d92344d31bf1358787daf801805
Jacob Penner <jacob.penner@adhocteam.us>
2024-05-10 10:11:30 -0400

    update 21-4138 form class to supply the right info necessary for form submission

--- modules/simple_forms_api/app/models/simple_forms_api/vba_21_4138.rb
+++ modules/simple_forms_api/app/models/simple_forms_api/vba_21_4138.rb
@@ -3,6 +3,7 @@
 module SimpleFormsApi
   class VBA214138
     include Virtus.model(nullify_blank: true)
+    STATS_KEY = 'api.simple_forms_api.21_4138'
 
     attribute :data
 
@@ -16,14 +17,24 @@ module SimpleFormsApi
 
     def metadata
       {
-        'ƒirstName' => @data.dig('full_name', 'first'),
-        'lastName' => @data.dig('full_name', 'last'),
+        'veteranFirstName' => @data.dig('full_name', 'first'),
+        'veteranLastName' => @data.dig('full_name', 'last'),
         'fileNumber' => @data.dig('id_number', 'va_file_number').presence || @data['ssn'],
         'zipCode' => @data.dig('address', 'postal_code'),
         'source' => 'VA Platform Digital Forms',
         'docType' => @data['form_number'],
         'businessLine' => 'CMP'
       }
+    end
+
+    def zip_code_is_us_based
+      @data.dig('mailing_address', 'country') == 'USA'
+    end
+
+    def track_user_identity(confirmation_number)
+      identity = data['preparer_type']
+      StatsD.increment("#{STATS_KEY}.#{identity}")
+      Rails.logger.info('Simple forms api - 21-4138 submission user identity', identity:, confirmation_number:)
     end
   end
 end
