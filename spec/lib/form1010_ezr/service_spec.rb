0203316c19390adda52964d87c2259d70406aa57
Joshua Drumm <joshua.drumm@agile6.com>
2024-05-14 15:33:27 -0400

    Removed extra line of code

--- spec/lib/form1010_ezr/service_spec.rb
+++ spec/lib/form1010_ezr/service_spec.rb
@@ -85,7 +85,6 @@ RSpec.describe Form1010Ezr::Service do
 
       it "increments StatsD, adds/updates 'veteranDateOfBirth' to be equal to the current_user's DOB, and " \
          "returns the parsed form" do
-
         expect(StatsD).to receive(:increment).with('api.1010ezr.missing_date_of_birth')
         expect(service.send(:post_fill_veteran_date_of_birth, parsed_form)).to eq(
           { 'veteranDateOfBirth' => current_user.birth_date }
