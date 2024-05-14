c23bf9d238712e02d2f0907e146d8edfc2a9b564
Jonathan VanCourt <jonathan.vancourt@adhocteam.us>
2024-05-14 08:45:28 -0400

    add poa create request service

--- modules/claims_api/spec/services/power_of_attorney_request_service/terminate_existing_requests_spec.rb
+++ modules/claims_api/spec/services/power_of_attorney_request_service/terminate_existing_requests_spec.rb
@@ -20,31 +20,35 @@ describe ClaimsApi::PowerOfAttorneyRequestService::TerminateExistingRequests do
       end
     end
 
-    it 'updates the non-obsolete requests' do
-      file_name = 'claims_api/power_of_attorney_request_service/terminate_existing_requests/with_requests_to_terminate'
-      VCR.use_cassette(file_name) do
-        receive_count = 0
-        allow_any_instance_of(ClaimsApi::ManageRepresentativeService).to receive(:update_poa_request) {
-                                                                           receive_count += 1
-                                                                         }
+    context 'when there are requests in a non-obsolete state' do
+      it 'updates the non-obsolete requests' do
+        file_name = 'claims_api/power_of_attorney_request_service/terminate_existing_requests/with_requests_to_terminate'
+        VCR.use_cassette(file_name) do
+          receive_count = 0
+          allow_any_instance_of(ClaimsApi::ManageRepresentativeService).to receive(:update_poa_request) {
+                                                                             receive_count += 1
+                                                                           }
 
-        subject.call
+          subject.call
 
-        expect(receive_count).to eq(3)
+          expect(receive_count).to eq(3)
+        end
       end
     end
 
-    it 'does not attempt to update requests' do
-      file_name = 'claims_api/power_of_attorney_request_service/terminate_existing_requests/no_requests_to_terminate'
-      VCR.use_cassette(file_name) do
-        receive_count = 0
-        allow_any_instance_of(ClaimsApi::ManageRepresentativeService).to receive(:update_poa_request) {
-          receive_count += 1
-        }
+    context 'when all requests are obsolete' do
+      it 'does not attempt to update requests' do
+        file_name = 'claims_api/power_of_attorney_request_service/terminate_existing_requests/no_requests_to_terminate'
+        VCR.use_cassette(file_name) do
+          receive_count = 0
+          allow_any_instance_of(ClaimsApi::ManageRepresentativeService).to receive(:update_poa_request) {
+            receive_count += 1
+          }
 
-        subject.call
+          subject.call
 
-        expect(receive_count).to eq(0)
+          expect(receive_count).to eq(0)
+        end
       end
     end
   end
