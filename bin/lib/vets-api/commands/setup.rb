efacc6f4204bb3f582c7d723121d7b85cc959abf
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-05-16 17:27:29 -0400

    grab required ruby version from .ruby_version

--- bin/lib/vets-api/commands/setup.rb
+++ bin/lib/vets-api/commands/setup.rb
@@ -44,12 +44,12 @@ module VetsApi
         end
 
         def setup_native
-          if RUBY_DESCRIPTION.include?('ruby 3.2.4')
+          if RUBY_DESCRIPTION.include?(ruby_version)
             VetsApi::Setups::Native.new.run
           else
-            puts "\nBefore continuing Ruby v3.2.4 must be installed"
-            puts 'We suggest using a Ruby version manager such as rbenv, asdf, rvm, or chruby
-                  to install and maintain your version of Ruby.'
+            puts "\nBefore continuing Ruby #{ruby_version} must be installed"
+            puts 'We suggest using a Ruby version manager such as rbenv, asdf, rvm, or chruby' \
+                 ' to install and maintain your version of Ruby.'
             puts 'More information: https://github.com/department-of-veterans-affairs/vets-api/blob/master/docs/setup/native.md#installing-a-ruby-version-manager'
           end
         end
@@ -64,10 +64,10 @@ module VetsApi
         end
 
         def setup_hybrid
-          if RUBY_DESCRIPTION.include?('ruby 3.2.4') && docker_running?
+          if RUBY_DESCRIPTION.include?(ruby_version) && docker_running?
             VetsApi::Setups::Hybrid.new.run
           else
-            puts "\nBefore continuing Ruby v3.2.4 AND Docker Desktop (Engine + Compose) must be installed"
+            puts "\nBefore continuing Ruby #{ruby_version} AND Docker Desktop (Engine + Compose) must be installed"
             puts 'More information about Ruby managers: https://github.com/department-of-veterans-affairs/vets-api/blob/master/docs/setup/ruby_managers.md'
             puts 'More information about Docker Desktop (Engine + Compose): https://github.com/department-of-veterans-affairs/vets-api/blob/master/docs/setup/ruby_managers.md'
           end
@@ -75,6 +75,10 @@ module VetsApi
 
         def docker_running?
           ShellCommand.run_quiet('docker -v')
+        end
+
+        def ruby_version
+          File.read('.ruby-version').chomp
         end
       end
     end
