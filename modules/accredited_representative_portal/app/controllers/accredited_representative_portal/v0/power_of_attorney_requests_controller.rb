051708303cb8638ef9b849756320de1900c60a7e
William Phelps <williamphelps13@gmail.com>
2024-05-20 16:11:06 -0600

    Revert accidental switch from records to data in controller

--- modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
+++ modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
@@ -34,7 +34,7 @@ module AccreditedRepresentativePortal
 
         poa_requests = AccreditedRepresentativePortal::Services::FetchPoaRequests.new(poa_codes).call
 
-        render json: { records: poa_requests['data'], records_count: poa_requests['meta']['totalRecords'].to_i },
+        render json: { records: poa_requests['records'], records_count: poa_requests['meta']['totalRecords'].to_i },
                status: :ok
       end
 
