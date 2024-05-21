e18c9d7785f42795deb66cd3d99067fdac917cbb
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-05-21 19:03:16 -0400

    rubocop formatting

--- modules/appeals_api/spec/serializers/evidence_submission_serializer_spec.rb
+++ modules/appeals_api/spec/serializers/evidence_submission_serializer_spec.rb
@@ -21,19 +21,20 @@ describe AppealsApi::EvidenceSubmissionSerializer do
     let(:upload_submission) { evidence_submission.upload_submission }
 
     it 'includes location' do
-      allow(upload_submission).to receive(:get_location).and_return("http://another.fakesite.com/rewrittenpath")
-      rendered_with_location_hash = described_class.new(evidence_submission, { render_location: true }).serializable_hash
+      allow(upload_submission).to receive(:get_location).and_return('http://another.fakesite.com/rewrittenpath')
+      rendered_with_location_hash = described_class.new(evidence_submission,
+                                                        { render_location: true }).serializable_hash
 
       location = upload_submission.get_location
       expect(rendered_with_location_hash[:location]).to eq location
     end
 
     it 'raises an error when get_location fails' do
-      allow(upload_submission).to receive(:get_location).and_raise(StandardError, "Test error")
+      allow(upload_submission).to receive(:get_location).and_raise(StandardError, 'Test error')
 
-      expect {
+      expect do
         described_class.new(evidence_submission, { render_location: true }).serializable_hash
-      }.to raise_error(Common::Exceptions::InternalServerError)
+      end.to raise_error(Common::Exceptions::InternalServerError)
     end
   end
 
@@ -51,8 +52,12 @@ describe AppealsApi::EvidenceSubmissionSerializer do
     expect(rendered_hash[:appeal_id]).to eq evidence_submission.supportable.id
   end
 
+  it 'includes :created_at' do
+    expect(rendered_hash[:created_at].to_s).to eq evidence_submission.created_at.to_s
+  end
+
   it 'includes :updated_at' do
-    expect(rendered_hash[:updated_at]).to eq evidence_submission.updated_at
+    expect(rendered_hash[:updated_at].to_s).to eq evidence_submission.updated_at.to_s
   end
 
   context 'with a successful status on parent upload' do
@@ -86,15 +91,5 @@ describe AppealsApi::EvidenceSubmissionSerializer do
       expect(rendered_hash[:detail].length).to eq(max_length_plus_ellipses)
       expect(submission_with_error.detail).to include rendered_hash[:detail][0, 100]
     end
-  end
-
-  # Should this use ISO 8601 format?
-  it 'includes :created_at' do
-    expect(rendered_hash[:created_at].to_s).to eq(evidence_submission.created_at.to_s)
-  end
-
-  # Should this use ISO 8601 format?
-  it 'includes :updated_at' do
-    expect(rendered_hash[:updated_at].to_s).to eq(evidence_submission.updated_at.to_s)
   end
 end
