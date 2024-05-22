111557f6c2dd1cb77c82b3bb76a4190a8ab9eb59
Kris Pethtel <kris.pethtel@gmail.com>
2024-05-22 14:06:43 -0500

    specs

--- modules/vaos/spec/services/v2/provider_names_spec.rb
+++ modules/vaos/spec/services/v2/provider_names_spec.rb
@@ -5,41 +5,7 @@ require 'rails_helper'
 describe VAOS::V2::ProviderNames do
   let(:user) { FactoryBot.build(:user) }
   let(:provider_names) { described_class.new(user) }
-  let(:single_practitioner_with_name) do
-    [
-      {
-        identifier: [
-          {
-            system: 'us-npi',
-            value: '520647669'
-          }
-        ],
-        name: {
-          family: 'KNIEFEL',
-          given: [
-            'CAROLYN'
-          ]
-        }
-      }
-    ]
-  end
-  let(:multiple_practioners_with_names) do
-    single_practitioner_with_name + [{
-      identifier: [
-        {
-          system: 'us-npi',
-          value: '520647363'
-        }
-      ],
-      name: {
-        family: 'NADEAU',
-        given: [
-          'MARCY'
-        ]
-      }
-    }]
-  end
-  let(:practitioner_without_name) do
+  let(:practitioner_list) do
     [
       {
         identifier: [
@@ -68,20 +34,67 @@ describe VAOS::V2::ProviderNames do
       expect(provider_names.form_names_from_appointment_practitioners_list({})).to be_nil
     end
 
-    it 'aggregates multiple names as a comma separated list' do
-      name = provider_names.form_names_from_appointment_practitioners_list(multiple_practioners_with_names)
-      expect(name).to eq('CAROLYN KNIEFEL, MARCY NADEAU')
+    it 'returns nil when practitioners list contains no provider id' do
+      practioner_list_no_value = [
+        {
+          identifier: [
+            {
+              system: 'us-npi',
+              value: nil
+            }
+          ]
+        }
+      ]
+      expect(provider_names.form_names_from_appointment_practitioners_list(practioner_list_no_value)).to eq(nil)
     end
 
-    it 'forms names from upstream when an identifier is found without a name' do
-      allow_any_instance_of(VAOS::V2::MobilePPMSService).to \
-        receive(:get_provider).with('1407938061').and_return(provider_response)
-      name = provider_names.form_names_from_appointment_practitioners_list(practitioner_without_name)
+    it 'returns nil if unless identifier system is us-npi' do
+      practioner_list_wrong_system = [
+        {
+          identifier: [
+            {
+              system: 'dfn-983',
+              value: '520647669'
+            }
+          ]
+        }
+      ]
+      expect(provider_names.form_names_from_appointment_practitioners_list(practioner_list_wrong_system)).to eq(nil)
+    end
+
+    it 'uses the name provided from upstream instead of the name provided in practitioners list' do
+      practioner_list_with_name = [
+        {
+          identifier: [
+            {
+              system: 'us-npi',
+              value: '520647669'
+            }
+          ],
+          name: {
+            family: 'KNIEFEL',
+            given: [
+              'CAROLYN'
+            ]
+          }
+        }
+      ]
+      response = OpenStruct.new({ 'providerIdentifier' => '520647669', 'name' => 'Knieffel, C.' })
+
+      allow_any_instance_of(VAOS::V2::MobilePPMSService)
+        .to receive(:get_provider).with('520647669').and_return(response)
+      expect(provider_names.form_names_from_appointment_practitioners_list(practioner_list_with_name)).to eq(response.name)
+    end
+
+    it 'uses response from upstream to form a name when provider id present' do
+      allow_any_instance_of(VAOS::V2::MobilePPMSService)
+        .to receive(:get_provider).with('1407938061').and_return(provider_response)
+      name = provider_names.form_names_from_appointment_practitioners_list(practitioner_list)
       expect(name).to eq('DEHGHAN, AMIR')
     end
 
-    it 'can request multiple upstream providers' do
-      multiple_practitioners_without_names = practitioner_without_name + [{
+    it 'aggregates multiple practitioner names as a comma separated list' do
+      multiple_practitioners_without_names = practitioner_list + [{
         identifier: [
           {
             system: 'us-npi',
@@ -91,35 +104,35 @@ describe VAOS::V2::ProviderNames do
       }]
       second_provider_response = OpenStruct.new({ 'providerIdentifier' => '1407938062', 'name' => 'J. Jones' })
 
-      allow_any_instance_of(VAOS::V2::MobilePPMSService).to \
-        receive(:get_provider).with('1407938061').and_return(provider_response)
-      allow_any_instance_of(VAOS::V2::MobilePPMSService).to \
-        receive(:get_provider).with('1407938062').and_return(second_provider_response)
+      allow_any_instance_of(VAOS::V2::MobilePPMSService)
+        .to receive(:get_provider).with('1407938061').and_return(provider_response)
+      allow_any_instance_of(VAOS::V2::MobilePPMSService)
+        .to receive(:get_provider).with('1407938062').and_return(second_provider_response)
       name =
         provider_names.form_names_from_appointment_practitioners_list(multiple_practitioners_without_names)
       expect(name).to eq('DEHGHAN, AMIR, J. Jones')
     end
 
     it 'only requests an upstream provider once' do
-      expect_any_instance_of(VAOS::V2::MobilePPMSService).to \
-        receive(:get_provider).with('1407938061').once.and_return(provider_response)
-      provider_names.form_names_from_appointment_practitioners_list(practitioner_without_name)
-      provider_names.form_names_from_appointment_practitioners_list(practitioner_without_name)
+      expect_any_instance_of(VAOS::V2::MobilePPMSService)
+        .to receive(:get_provider).with('1407938061').once.and_return(provider_response)
+      provider_names.form_names_from_appointment_practitioners_list(practitioner_list)
+      provider_names.form_names_from_appointment_practitioners_list(practitioner_list)
     end
 
     it 'returns nil when the ppms service raises an error' do
-      allow_any_instance_of(VAOS::V2::MobilePPMSService).to \
-        receive(:get_provider).and_raise(Common::Exceptions::BackendServiceException)
-      name = provider_names.form_names_from_appointment_practitioners_list(practitioner_without_name)
+      allow_any_instance_of(VAOS::V2::MobilePPMSService)
+        .to receive(:get_provider).and_raise(Common::Exceptions::BackendServiceException)
+      name = provider_names.form_names_from_appointment_practitioners_list(practitioner_list)
       expect(name).to eq(VAOS::V2::ProviderNames::NPI_NOT_FOUND_MSG)
     end
 
     it 'returns nil if the returned provider does not match the expected structure' do
       nameless_provider = OpenStruct.new
-      allow_any_instance_of(VAOS::V2::MobilePPMSService).to \
-        receive(:get_provider).with('1407938061').and_return(nameless_provider)
+      allow_any_instance_of(VAOS::V2::MobilePPMSService)
+        .to receive(:get_provider).with('1407938061').and_return(nameless_provider)
 
-      name = provider_names.form_names_from_appointment_practitioners_list(practitioner_without_name)
+      name = provider_names.form_names_from_appointment_practitioners_list(practitioner_list)
       expect(name).to be_nil
     end
   end
