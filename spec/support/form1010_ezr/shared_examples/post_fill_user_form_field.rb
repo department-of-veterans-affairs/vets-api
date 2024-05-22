dabe6377e90382421a12ea936f3cfd83ae858acb
Joshua Drumm <joshua.drumm@agile6.com>
2024-05-22 15:27:55 -0400

    Fixed linting issues

--- spec/support/form1010_ezr/shared_examples/post_fill_user_form_field.rb
+++ spec/support/form1010_ezr/shared_examples/post_fill_user_form_field.rb
@@ -9,7 +9,7 @@ shared_examples_for 'post-fill user form field' do
     let(:parsed_form) { _parsed_form }
 
     it 'returns nil' do
-      expect(subject.send("#{klass_method}".to_sym, parsed_form)).to eq(nil)
+      expect(subject.send(klass_method.to_sym, parsed_form)).to eq(nil)
     end
   end
 
@@ -23,7 +23,7 @@ shared_examples_for 'post-fill user form field' do
     it "increments StatsD, adds/updates the form field to be equal to the current_user's data, " \
        'and returns the parsed form' do
       expect(StatsD).to receive(:increment).with("api.1010ezr.#{statsd_increment_name}")
-      expect(service.send("#{klass_method}".to_sym, parsed_form)).to eq(
+      expect(service.send(klass_method.to_sym, parsed_form)).to eq(
         { user_field => user_data }
       )
     end
