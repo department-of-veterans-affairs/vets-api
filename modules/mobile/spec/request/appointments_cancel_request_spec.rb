5929428c5a991e01899a2ff2747659fb8fda079d
Devin McCurdy <devin@apothesource.com>
2024-05-30 09:26:52 -0400

    Linter fixes

--- modules/mobile/spec/request/appointments_cancel_request_spec.rb
+++ modules/mobile/spec/request/appointments_cancel_request_spec.rb
@@ -37,15 +37,16 @@ RSpec.describe 'appointments', type: :request do
           Flipper.disable(:va_online_scheduling_enable_OH_cancellations)
           Flipper.disable(:va_online_scheduling_use_vpg)
         end
+
         it 'returns no content' do
           VCR.use_cassette('mobile/appointments/VAOS_v2/cancel_appointment_200', match_requests_on: %i[method uri]) do
-              VCR.use_cassette('mobile/appointments/VAOS_v2/get_facilities_200', match_requests_on: %i[method uri]) do
-                put "/mobile/v0/appointments/cancel/#{cancel_id}", params: nil, headers: sis_headers
+            VCR.use_cassette('mobile/appointments/VAOS_v2/get_facilities_200', match_requests_on: %i[method uri]) do
+              put "/mobile/v0/appointments/cancel/#{cancel_id}", params: nil, headers: sis_headers
 
-                puts response.body
+              puts response.body
 
-                expect(response).to have_http_status(:no_content)
-              end
+              expect(response).to have_http_status(:no_content)
+            end
           end
         end
       end
@@ -55,9 +56,12 @@ RSpec.describe 'appointments', type: :request do
           Flipper.enable(:va_online_scheduling_enable_OH_cancellations)
           Flipper.enable(:va_online_scheduling_use_vpg)
         end
+
         it 'returns no content' do
-          VCR.use_cassette('mobile/appointments/VAOS_v2/cancel_appointment_vpg_204', match_requests_on: %i[method uri]) do
-            VCR.use_cassette('mobile/appointments/VAOS_v2/get_appointment_cancelled_200_vpg', match_requests_on: %i[method uri]) do
+          VCR.use_cassette('mobile/appointments/VAOS_v2/cancel_appointment_vpg_204',
+                           match_requests_on: %i[method uri]) do
+            VCR.use_cassette('mobile/appointments/VAOS_v2/get_appointment_cancelled_200_vpg',
+                             match_requests_on: %i[method uri]) do
               VCR.use_cassette('mobile/appointments/VAOS_v2/get_facilities_200', match_requests_on: %i[method uri]) do
                 put "/mobile/v0/appointments/cancel/#{cancel_id}", params: nil, headers: sis_headers
 
@@ -129,7 +133,8 @@ RSpec.describe 'appointments', type: :request do
     describe 'PUT /mobile/v0/appointments/cancel', :aggregate_failures do
       it 'returns a no content code' do
         VCR.use_cassette('mobile/appointments/VAOS_v2/cancel_appointment_vpg_204', match_requests_on: %i[method uri]) do
-          VCR.use_cassette('mobile/appointments/VAOS_v2/get_appointment_cancelled_200_vpg', match_requests_on: %i[method uri]) do
+          VCR.use_cassette('mobile/appointments/VAOS_v2/get_appointment_cancelled_200_vpg',
+                           match_requests_on: %i[method uri]) do
             VCR.use_cassette('mobile/appointments/VAOS_v2/get_facilities_200', match_requests_on: %i[method uri]) do
               put "/mobile/v0/appointments/cancel/#{cancel_id}", params: nil, headers: sis_headers
 
