12e1139cb531af20e983c065acb8287d14455fab
Joshua Drumm <joshua.drumm@agile6.com>
2024-05-30 11:05:01 -0400

    Changed 'to_s' back to interpolation and addressed 'to_not' issue

--- spec/lib/form1010_ezr/service_spec.rb
+++ spec/lib/form1010_ezr/service_spec.rb
@@ -96,7 +96,7 @@ RSpec.describe Form1010Ezr::Service do
         service.send(:post_fill_required_user_fields, parsed_form)
 
         required_user_fields.each do |key, value|
-          expect(parsed_form[key.to_s]).to_not eq(value)
+          expect(parsed_form["#{key}"]).not_to eq(value)
         end
       end
     end
@@ -116,7 +116,7 @@ RSpec.describe Form1010Ezr::Service do
         service.send(:post_fill_required_user_fields, parsed_form)
 
         required_user_fields.each do |key, value|
-          expect(parsed_form[key.to_s]).to eq(value)
+          expect(parsed_form["#{key}"]).to eq(value)
         end
       end
     end
