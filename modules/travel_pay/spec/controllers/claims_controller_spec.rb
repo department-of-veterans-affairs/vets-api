24b29843176c85bf6fb96a7b8d848abea94e645f
athifw <athif@u.northwestern.edu>
2024-05-30 17:02:22 -0500

    Add VCR cassette for travel pay claims

--- modules/travel_pay/spec/controllers/claims_controller_spec.rb
+++ modules/travel_pay/spec/controllers/claims_controller_spec.rb
@@ -11,52 +11,30 @@ RSpec.describe TravelPay::ClaimsController, type: :request do
 
   describe '#index' do
     context 'successful response from API' do
+      let(:expected_claim_ids) do
+        [
+          "6ea23179-e87c-44ae-a20a-f31fb2c132fb",
+          "6ea23179-e87c-44ae-a20a-f31fb2c021fb",
+          "6ea23179-e87c-44ae-a20a-f31fb2c782fb"
+        ]
+      end
+
       it 'responds with 200' do
-        # allow_any_instance_of(TravelPay::Client)
-        #   .to receive(:request_veis_token)
-        #   .and_return('veis_token')
-
-        # allow_any_instance_of(TravelPay::Client)
-        #   .to receive(:request_sts_token)
-        #   .and_return('sts_token')
-
-        # allow_any_instance_of(TravelPay::Client)
-        #   .to receive(:request_btsss_token)
-        #   .with('veis_token', 'sts_token')
-        #   .and_return('btsss_token')
-
-        # allow_any_instance_of(TravelPay::Client)
-        #   .to receive(:get_claims).and_return([])
         VCR.use_cassette('travel_pay/200_claims', match_requests_on: [:method, :path]) do
           get '/travel_pay/claims', params: nil, headers: { 'Authorization' => 'Bearer vagov_token' }
-
           expect(response).to have_http_status(:ok)
+          claim_ids = JSON.parse(response.body)["data"].map { |claim| claim["id"] }
+          expect(claim_ids).to eq(expected_claim_ids)
         end
       end
     end
 
     context 'unsuccessful response from API' do
       it 'responds with a 404 if the API endpoint is not found' do
-        # allow_any_instance_of(TravelPay::Client)
-        #   .to receive(:request_veis_token)
-        #   .and_return('veis_token')
-
-        # allow_any_instance_of(TravelPay::Client)
-        #   .to receive(:request_sts_token)
-        #   .and_return('sts_token')
-
-        # allow_any_instance_of(TravelPay::Client)
-        #   .to receive(:request_btsss_token)
-        #   .with('veis_token', 'sts_token')
-        #   .and_return('btsss_token')
-
-        # allow_any_instance_of(TravelPay::Client)
-        #   .to receive(:get_claims)
-        #   .and_raise(Faraday::ResourceNotFound)
-
-        get '/travel_pay/claims', params: nil, headers: { 'Authorization' => 'Bearer vagov_token' }
-
-        expect(response).to have_http_status(:not_found)
+        VCR.use_cassette('travel_pay/404_claims', match_requests_on: [:method, :path]) do
+          get '/travel_pay/claims', params: nil, headers: { 'Authorization' => 'Bearer vagov_token' }
+          expect(response).to have_http_status(:bad_request)
+        end
       end
     end
   end
