fe764391e415355032c74817b7d8433057a53cdd
Joshua Drumm <joshua.drumm@agile6.com>
2024-05-30 11:45:09 -0400

    Removed interpolation for keys that have string values

--- spec/lib/form1010_ezr/service_spec.rb
+++ spec/lib/form1010_ezr/service_spec.rb
@@ -96,7 +96,7 @@ RSpec.describe Form1010Ezr::Service do
         service.send(:post_fill_required_user_fields, parsed_form)
 
         required_user_fields.each do |key, value|
-          expect(parsed_form["#{key}"]).not_to eq(value)
+          expect(parsed_form[key]).not_to eq(value)
         end
       end
     end
