f515b8a66671d0a1c9e24cd938953304b877434a
GcioGregg <gregg.puhala@va.gov>
2024-05-31 11:51:19 -0700

    add test

--- spec/mailers/school_certifying_officials_mailer_spec.rb
+++ spec/mailers/school_certifying_officials_mailer_spec.rb
@@ -35,6 +35,11 @@ RSpec.describe SchoolCertifyingOfficialsMailer, type: [:mailer] do
       it 'includes school student id' do
         expect(subject.body.raw_source).to include("Student's school ID number: #{applicant.schoolStudentId}")
       end
+
+      it 'includes updated text' do
+        expect(subject.body.raw_source).to include('this STEM program (specify semester or quarter and exclude in-progress credits):')
+        expect(subject.body.raw_source).to include('Total required credits for STEM degree program')
+      end
     end
   end
 end
