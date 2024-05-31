f2a20e44f8e1121f3e634392801c7394fa655378
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-05-31 15:38:50 -0400

    add ExclusionPeriodSerializer spec

---
+++ modules/meb_api/spec/serializers/exclusion_period_serializer_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'dgi/exclusion_period/response'
+
+describe ExclusionPeriodSerializer do
+
+  let(:exclusion_period) do
+    response = double('response', status: 201, body: { 'exclusion_periods' => ["ROTC", "NoPayDate"] })
+    MebApi::DGI::ExclusionPeriod::Response.new(response)
+  end
+
+  let(:expected_response) do
+    {
+      data: {
+        id: '',
+        type: 'meb_api_dgi_exclusion_period_responses',
+        attributes: {
+          exclusion_periods: ["ROTC", "NoPayDate"]
+        }
+      }
+    }
+  end
+
+  let(:rendered_hash) do
+    ActiveModelSerializers::SerializableResource.new(exclusion_period, { serializer: described_class }).as_json
+  end
+  let(:rendered_attributes) { rendered_hash[:data][:attributes] }
+
+  it 'includes :id' do
+    expect(rendered_hash[:data][:id]).to be_blank
+  end
+
+  it 'includes :exclusion_periods' do
+    expect(rendered_attributes[:exclusion_periods]).to eq expected_response[:data][:attributes][:exclusion_periods]
+  end
+end
