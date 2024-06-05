d3f42f6873f69c8c51e719c6f6cebbf7b5072781
Don Shin <cloudmagic80@gmail.com>
2024-06-05 10:51:01 -0500

    adjust model

--- modules/ivc_champva/app/models/ivc_champva/vha_10_7959a.rb
+++ modules/ivc_champva/app/models/ivc_champva/vha_10_7959a.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
-module SimpleFormsApi
-  class VHA107959A
+module IvcChampva
+  class VHA107959c
     include Virtus.model(nullify_blank: true)
+    include Attachments
 
     attribute :data
+    attr_reader :form_id
 
     def initialize(data)
       @data = data
+      @uuid = SecureRandom.uuid
+      @form_id = 'vha_10_7959a'
     end
 
     def metadata
@@ -19,7 +23,30 @@ module SimpleFormsApi
         'source' => 'VA Platform Digital Forms',
         'docType' => @data['form_number'],
         'businessLine' => 'CMP'
+        'country' => @data.dig('applicant_address', 'country') || 'USA',
+        'uuid' => @uuid,
+        'primaryContactInfo' => @data['primary_contact_info']
       }
+    end
+
+    def submission_date_config
+      {
+        should_stamp_date?: false,
+        page_number: 1,
+        title_coords: [440, 690],
+        text_coords: [440, 670]
+      }
+    end
+
+    # rubocop:disable Naming/BlockForwarding,Style/HashSyntax
+    def method_missing(method_name, *args, &block)
+      super unless respond_to_missing?(method_name)
+      { method: method_name, args: args }
+    end
+    # rubocop:enable Naming/BlockForwarding,Style/HashSyntax
+
+    def respond_to_missing?(_method_name, _include_private = false)
+      true
     end
   end
 end
