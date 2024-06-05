a09032ecfcb7f353561aa677db43097d63a4f144
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-06-05 16:45:39 -0400

    add FolderSerializer spec

---
+++ modules/mobile/spec/serializers/mobile/v0/folder_serializer_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Mobile::V0::FolderSerializer do
+  let(:folder) { build_stubbed(:folder) }
+
+  let(:rendered_hash) do
+    ActiveModelSerializers::SerializableResource.new(folder, { serializer: described_class }).as_json
+  end
+  let(:rendered_attributes) { rendered_hash[:data][:attributes] }
+
+  it 'includes :id' do
+    expect(rendered_hash[:data][:id]).to eq folder.id.to_s
+  end
+
+  it 'includes :folder_id' do
+    expect(rendered_attributes[:folder_id]).to eq folder.id
+  end
+
+  it 'includes :name' do
+    expect(rendered_attributes[:name]).to eq folder.name
+  end
+
+  it 'includes :count' do
+    expect(rendered_attributes[:count]).to eq folder.count
+  end
+
+  it 'includes :unread_count' do
+    expect(rendered_attributes[:unread_count]).to eq folder.unread_count
+  end
+
+  it 'includes :system_folder' do
+    expect(rendered_attributes[:system_folder]).to eq folder.system_folder
+  end
+
+  it 'includes :download link' do
+    expected_url = Mobile::UrlHelper.new.v0_folder_url(folder.id)
+    expect(rendered_hash[:data][:links][:self]).to eq expected_url
+  end
+end
