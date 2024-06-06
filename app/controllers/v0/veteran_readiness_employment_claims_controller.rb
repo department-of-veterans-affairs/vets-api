71ff0742f330c3e3974c44aef70f952a6f4b45fd
Micah Frazier <Micah.frazier@oddball.io>
2024-06-06 12:52:37 -0600

    Added in some more rails logging to VRE controller

--- app/controllers/v0/veteran_readiness_employment_claims_controller.rb
+++ app/controllers/v0/veteran_readiness_employment_claims_controller.rb
@@ -17,6 +17,10 @@ module V0
       else
         StatsD.increment("#{stats_key}.failure")
         Sentry.set_tags(team: 'vfs-ebenefits') # tag sentry logs with team name
+        Rails.logger.error('VR&E claim not saved', { claim_id: claim.id,
+                                                     error_messages: claim.errors,
+                                                     user_logged_in: current_user.present?,
+                                                     current_user_uuid: current_user&.uuid })
         raise Common::Exceptions::ValidationErrors, claim
       end
     end
