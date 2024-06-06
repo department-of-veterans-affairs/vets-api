97bf3b542bded17d20af6639a36d1800fa0d0d5c
Bryan Alexander <bryan.alexander@adhocteam.us>
2024-06-06 11:26:26 -0400

    84169: Remove safety_assured

--- db/migrate/20240605161809_add_case_id_to_ivc_champva_forms.rb
+++ db/migrate/20240605161809_add_case_id_to_ivc_champva_forms.rb
@@ -1,7 +1,5 @@
 class AddCaseIdToIvcChampvaForms < ActiveRecord::Migration[7.1]
   def change
-    safety_assured do
-      add_column :ivc_champva_forms, :pega_case_id, :string, null: true
-    end
+    add_column :ivc_champva_forms, :pega_case_id, :string, null: true
   end
 end
