108a1fc635e314f085e6cdec73b2db5fa411dc85
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-06-06 17:11:55 -0400

    add AllTriageTeamsSerializer spec

---
+++ modules/my_health/spec/factories/all_triage_teams.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :all_triage_team do
+    station_number { '989' }
+    blocked_status { false }
+    relationship_type { 'PATIENT' }
+    preferred_team { false }
+    sequence :triage_team_id do |n|
+      n
+    end
+
+    sequence :name do |n|
+      "Triage Group for patient #{n}"
+    end
+
+  end
+end
