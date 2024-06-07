421d7ff9c7f2176a8e09a1a681a1b2e993b92a6c
Jun Tao Luo <john.luo@agile6.com>
2024-06-07 11:40:34 -0400

    Migrate new appointment date modification logic

--- modules/vaos/app/services/vaos/v2/appointments_service.rb
+++ modules/vaos/app/services/vaos/v2/appointments_service.rb
@@ -68,6 +68,11 @@ module VAOS
       end
 
       def post_appointment(request_object_body)
+
+        if request_object_body[:kind] == 'clinic' && request_object_body[:status] == 'booked' # a direct scheduled appointment
+          modify_desired_date(request_object_body, get_facility_timezone(request_object_body[:location_id]))
+        end
+
         filtered_reason_code_text = filter_reason_code_text(request_object_body)
         request_object_body[:reason_code][:text] = filtered_reason_code_text if filtered_reason_code_text.present?
 
@@ -151,6 +156,35 @@ module VAOS
       memoize :get_facility_timezone_memoized
 
       private
+
+      # Modifies params so that the facility timezone offset is included in the desired date.
+      # The desired date is sent in this format: 2019-12-31T00:00:00-00:00
+      # This modifies the params in place. If params does not contain a desired date, it is not modified.
+      #
+      # @param [ActionController::Parameters] create_params - the params to be modified
+      # @param [String] timezone - the facility timezone id
+      def modify_desired_date(create_params, timezone)
+        desired_date = create_params[:extension]&.[](:desired_date)
+
+        return create_params if desired_date.nil?
+
+        create_params[:extension][:desired_date] = add_timezone_offset(desired_date, timezone)
+      end
+
+      # Returns a [DateTime] object with the timezone offset added. Given a desired date of 2019-12-31T00:00:00-00:00
+      # and a timezone of America/New_York, the returned date will be 2019-12-31T00:00:00-05:00.
+      #
+      # @param [DateTime] date - the date to be modified,  required
+      # @param [String] tz - the timezone id, if nil, the offset is not added
+      # @return [DateTime] date with timezone offset
+      #
+      def add_timezone_offset(date, tz)
+        raise Common::Exceptions::ParameterMissing, 'date' if date.nil?
+
+        utc_date = date.to_time.utc
+        timezone_offset = utc_date.in_time_zone(tz).formatted_offset
+        utc_date.change(offset: timezone_offset).to_datetime
+      end
 
       def fetch_clinic_appointments(start_time, end_time, statuses)
         get_appointments(start_time, end_time, statuses)[:data].select { |appt| appt.kind == 'clinic' }
