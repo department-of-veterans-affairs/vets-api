b4199cc32a2d1e1718e1fd1304a8d3036ffb5704
Samuel Stuckey <sam.n.stuckey@gmail.com>
2024-06-10 14:47:56 -0600

    Notate for handoff

--- lib/disability_compensation/providers/generate_pdf/lighthouse_generate_pdf_provider.rb
+++ lib/disability_compensation/providers/generate_pdf/lighthouse_generate_pdf_provider.rb
@@ -7,16 +7,8 @@ class LighthouseGeneratePdfProvider
     @icn = icn
   end
 
-  # [wipn8923] LH provider
   def generate_526_pdf(form_content)
-    puts("\n\n wipn8923 :: #{File.basename(__FILE__)}-#{self.class.name}##{__method__.to_s} - \n\t form_content: #{form_content} \n\n")
-    puts("\n\n wipn8923 :: #{File.basename(__FILE__)}-#{self.class.name}##{__method__.to_s} - \n\t form_content.class: #{form_content.class} \n\n")
-
     body = transform_service.transform(JSON.parse(form_content))
-    # body = transform_service.transform(form_content)
-
-    puts("\n\n wipn8923 :: #{File.basename(__FILE__)}-#{self.class.name}##{__method__.to_s} - \n\t body: #{body} \n\n")
-
     service.submit526(body, nil, nil, { generate_pdf: true })
   end
 
