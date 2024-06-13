814c94e93e49d77c09ec3388082043644a91f992
Samuel Cormier <sam.cormier@coforma.io>
2024-06-13 11:07:57 -0400

    updated current_phaseback to current_phase_back

--- app/controllers/v0/benefits_claims_controller.rb
+++ app/controllers/v0/benefits_claims_controller.rb
@@ -33,7 +33,7 @@ module V0
                             claim_type_code: claim_info['claimTypeCode'],
                             num_contentions: claim_info['contentions'].count,
                             ep_code: claim_info['endProductCode'],
-                            current_phaseback: claim_info['claimPhaseDates']['currentPhaseBack'],
+                            current_phase_back: claim_info['claimPhaseDates']['currentPhaseBack'],
                             latest_phase_type: claim_info['claimPhaseDates']['latestPhaseType'],
                             decision_letter_sent: claim_info['decisionLetterSent'],
                             development_letter_sent: claim_info['developmentLetterSent'],
