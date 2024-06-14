e44675516bb07f58c74048cc95c85b7d4507745a
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-06-14 07:09:58 -0600

    Rubocop

--- modules/claims_api/spec/lib/claims_api/v2/error/lighthouse_error_mapper_spec.rb
+++ modules/claims_api/spec/lib/claims_api/v2/error/lighthouse_error_mapper_spec.rb
@@ -8,19 +8,23 @@ describe ClaimsApi::V2::Error::LighthouseErrorMapper do
   it '#get_details' do
     error =
       { key: 'form526.submit.save.draftForm.MaxEPCode', severity: 'FATAL',
-        text: 'This claim could not be established. The Maximum number of EP codes have been reached for this benefit type claim code' }
+        text: 'This claim could not be established. The Maximum number of EP codes have been reached for this ' \
+              'benefit type claim code' }
 
     mapper = ClaimsApi::V2::Error::LighthouseErrorMapper.new(error).get_details
 
-    expect(mapper).to eq('This claim could not be established. The Maximum number of EP codes have been reached for this benefit type claim code')
+    expect(mapper).to eq('This claim could not be established. The Maximum number of EP codes have been ' \
+                         'reached for this benefit type claim code')
   end
 
   it '# get_details for a rated disability error' do
     error =
       { key: 'form526.disabilities[0].disabilityActionTypeNONE.ratedDisability.isInvalid', severity: 'ERROR',
-        text: 'An attempt was made to add a secondary disability to an existing rated Disability. The rated Disability could not be found' }
+        text: 'An attempt was made to add a secondary disability to an existing rated Disability. The rated ' \
+              'Disability could not be found' }
     mapper = ClaimsApi::V2::Error::LighthouseErrorMapper.new(error).get_details
 
-    expect(mapper).to eq('The claim could not be established - An attempt was made to add a secondary disability to an existing rated Disability. The rated Disability could not be found.')
+    expect(mapper).to eq('The claim could not be established - An attempt was made to add a secondary disability ' \
+                         'to an existing rated Disability. The rated Disability could not be found.')
   end
 end
