b3bdb3ed232984b15b62581df3ec9b0fd2097f9f
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-06-17 15:30:37 -0400

    remove #grants from OktaAppSerializer

--- app/serializers/okta_app_serializer.rb
+++ app/serializers/okta_app_serializer.rb
@@ -3,16 +3,6 @@
 class OktaAppSerializer < ActiveModel::Serializer
   attributes :id, :title, :logo, :privacy_url, :grants
 
-  def grants
-    object.grants.map do |grant|
-      {
-        title: grant['_links']['scope']['title'],
-        id: grant['id'],
-        created: grant['created']
-      }
-    end
-  end
-
   def read_attribute_for_serialization(attr)
     object[attr.to_s] || object['attributes'][attr.to_s]
   end
