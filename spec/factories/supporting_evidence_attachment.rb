42608d0347919363b2cba6346cc6a4566bd37f14
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-06-18 17:45:02 -0400

    add SupportingEvidenceAttachmentSerializer spec

--- spec/factories/supporting_evidence_attachment.rb
+++ spec/factories/supporting_evidence_attachment.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :supporting_evidence_attachment
+  factory :supporting_evidence_attachment, class: 'SupportingEvidenceAttachment' do
+
+    after(:build) do |supporting_evidence_attachment|
+      supporting_evidence_attachment.set_file_data!(
+        Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'preneeds', 'extras.pdf'), 'application/pdf')
+      )
+    end
+  end
 end
