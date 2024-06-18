a84def07456a669d134731b2f859c246b8063c45
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-06-18 18:07:05 -0400

    remove ZipcodesSerializer

--- spec/factories/supporting_evidence_attachment.rb
+++ spec/factories/supporting_evidence_attachment.rb
@@ -2,7 +2,6 @@
 
 FactoryBot.define do
   factory :supporting_evidence_attachment, class: 'SupportingEvidenceAttachment' do
-
     after(:build) do |supporting_evidence_attachment|
       supporting_evidence_attachment.set_file_data!(
         Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'preneeds', 'extras.pdf'), 'application/pdf')
--- spec/serializers/valid_va_file_number_serializer_spec.rb
+++ spec/serializers/valid_va_file_number_serializer_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe ValidVAFileNumberSerializer, type: :serializer do
   subject { serialize(file_number, serializer_class: described_class) }
 
-  let(:file_number)  { { file_nbr: true }}
+  let(:file_number) { { file_nbr: true } }
   let(:data) { JSON.parse(subject)['data'] }
   let(:attributes) { data['attributes'] }
 
