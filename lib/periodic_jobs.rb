0a9b55aa1d1fc0743777ab5a9bded0621483e359
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-06-18 08:37:17 -0600

    Restores comment

--- lib/periodic_jobs.rb
+++ lib/periodic_jobs.rb
@@ -100,7 +100,7 @@ PERIODIC_JOBS = lambda { |mgr|
   mgr.register('0 13 * * 1', 'Mobile::V0::WeeklyMaintenanceWindowLogger')
   # Weekly logs of maintenance windows
   mgr.register('0 * * * *', 'ClaimsApi::ReportHourlyUnsuccessfulSubmissions')
-  # Daily alert of pending claims longer than acceptable threshold
+  # Hourly slack alert of errored claim submissions
   mgr.register('15 23 * * *', 'ClaimsApi::ReportUnsuccessfulSubmissions')
   # Weekly report of unsuccessful claims submissions
   mgr.register('00 00 1 * *', 'ClaimsApi::ReportMonthlySubmissions')
