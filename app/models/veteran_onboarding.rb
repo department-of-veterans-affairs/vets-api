bf58dc7a7593dc740af578245a6804426e71c995
Adam King <adam.king0126@gmail.com>
2024-06-17 20:37:00 -0500

    Add new feature toggle and logic to check against `verified_at`

--- app/models/veteran_onboarding.rb
+++ app/models/veteran_onboarding.rb
@@ -17,16 +17,43 @@ class VeteranOnboarding < ApplicationRecord
   belongs_to :user_account, primary_key: :id, foreign_key: :user_account_uuid, inverse_of: :veteran_onboarding
   validates :user_account, uniqueness: true
 
+  attr_accessor :user
+
   # Determines whether the onboarding flow should be displayed for a veteran.
-  # Currently, this is based solely on the value of the `display_onboarding_flow` attribute.
-  # In the future, additional information (like MPI info) might be taken into account.
+  # Currently, we have two feature toggle checks:
+  # - veteran_onboarding_show_to_newly_onboarded examines settings for veteran_onboarding.onboarding_threshold_days,
+  #   (default 180 days) and examines the user_verification.verified_at attribute
+  # - veteran_onboarding_beta_flow just checks if the feature toggle is enabled for the user
   def show_onboarding_flow_on_login
-    display_onboarding_flow
+    if @user.user_account&.verified?
+      if Flipper.enabled?(:veteran_onboarding_show_to_newly_onboarded, @user)
+        days_since_verification = (Time.zone.today - @user.user_verification.verified_at.to_date).to_i
+        display_onboarding_flow && days_since_verification <= (Settings.veteran_onboarding&.onboarding_threshold_days || 180)
+      elsif Flipper.enabled?(:veteran_onboarding_beta_flow, @user)
+        display_onboarding_flow
+      end
+    else
+      false
+    end
   end
 
   def self.for_user(user)
-    if Flipper.enabled?(:veteran_onboarding_beta_flow, user) && user.user_account&.verified?
-      find_or_create_by(user_account: user.user_account)
+    if user.user_account&.verified? && (
+      Flipper.enabled?(:veteran_onboarding_beta_flow, user) ||
+      Flipper.enabled?(:veteran_onboarding_show_to_newly_onboarded, user)
+    )
+      veteran_onboarding = find_or_create_by(user_account: user.user_account)
+      veteran_onboarding.user = user
+      veteran_onboarding
+    end
+  end
+
+
+  def find_or_create_for_user
+    if @user.user_account&.verified?
+      if Flipper.enabled?(:veteran_onboarding_beta_flow, @user) || Flipper.enabled?(:veteran_onboarding_show_to_newly_onboarded, @user)
+        self.class.find_or_create_by(user_account: @user.user_account)
+      end
     end
   end
 end
--- spec/models/veteran_onboarding_spec.rb
+++ spec/models/veteran_onboarding_spec.rb
@@ -3,7 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe VeteranOnboarding, type: :model do
-  let(:user_account) { UserAccount.create!(icn: 'some_icn_value') }
+  let(:user_verification) { create(:user_verification)}
+  let(:user_account) { user_verification.user_account}
+  let(:user) { build(:user, :loa3, idme_uuid: user_verification.idme_uuid)}
 
   describe 'validations' do
     it 'validates presence of user_account_uuid' do
@@ -20,10 +22,32 @@ RSpec.describe VeteranOnboarding, type: :model do
     end
   end
 
+  it 'creates a VeteranOnboarding object if toggle is enabled' do
+    Flipper.enable(:veteran_onboarding_beta_flow, user)
+    expect {VeteranOnboarding.for_user(user)}.to change {VeteranOnboarding.count}.by(1)
+  end
+
   describe '#show_onboarding_flow_on_login' do
     it 'returns the value of display_onboarding_flow' do
-      subject = described_class.new(display_onboarding_flow: true, user_account:)
+      subject = described_class.for_user(user)
+      Flipper.enable(:veteran_onboarding_beta_flow, user)
       expect(subject.show_onboarding_flow_on_login).to be_truthy
+    end
+
+
+    # default logic to show onboarding is 180 days, can be overridden by setting value in
+    # Settings.veteran_onboarding.onboarding_threshold_days
+    [
+      { days_ago: 180, expected: true},
+      { days_ago: 0, expected: true},
+      { days_ago: 181, expected: false}
+    ].each do |scenario|
+      it "returns #{scenario[:expected]} when verified #{scenario[:days_ago]} days ago" do
+        verified_at_date = Date.today - scenario[:days_ago]
+        allow(user.user_verification).to receive(:verified_at).and_return(verified_at_date)
+        Flipper.enable(:veteran_onboarding_show_to_newly_onboarded, user)
+        assert user.onboarding&.show_onboarding_flow_on_login == scenario[:expected]
+      end
     end
   end
 end
