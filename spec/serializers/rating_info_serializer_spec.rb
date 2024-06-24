d121155c12388800a099efc1cee2129debcb81df
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-06-24 07:38:52 -0400

    Add Unit Tests for Root AMS Serializers - Part 12 (#17167)
    * add RatedDisabilitiesSerializer spec
    
    * add RatingInfoSerializer spec
    
    * add SavedClaimSerializer spec
    
    * add SearchSerializer spec
    
    * add ServiceHistorySerializer spec
    
    * linting
    
    * update CODEOWNERS
    
    * linting

---
+++ spec/serializers/rating_info_serializer_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'evss/disability_compensation_form/rating_info_response'
+
+describe RatingInfoSerializer, type: :serializer do
+  subject { serialize(rating_info_response, serializer_class: described_class) }
+
+  let(:rating_info_response) do
+    response = double('response', body: { user_percent_of_disability: 100 })
+    EVSS::DisabilityCompensationForm::RatingInfoResponse.new(200, response)
+  end
+  let(:data) { JSON.parse(subject)['data'] }
+  let(:attributes) { data['attributes'] }
+
+  it 'includes :id' do
+    expect(data['id']).to be_blank
+  end
+
+  it 'includes :user_percent_of_disability' do
+    expect(attributes['user_percent_of_disability']).to eq rating_info_response[:user_percent_of_disability]
+  end
+
+  it 'includes :source_system' do
+    expect(attributes['source_system']).to eq 'EVSS'
+  end
+end
