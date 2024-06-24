0569369cabf0697ea5d29ab867e23e9b6096456b
Eric Boehs <eric@boehs.com>
2024-06-24 16:06:46 -0500

    Allow Instance Vars for Faraday patch (RuboCop)

--- lib/common/client/middleware/faraday_middleware_patch.rb
+++ lib/common/client/middleware/faraday_middleware_patch.rb
@@ -11,12 +11,12 @@ module FaradayMiddlewarePatch
   def self.prepended(base)
     class << base
       def default_options=(options = {})
-        @default_options ||= {}
-        @default_options.merge!(options)
+        @default_options ||= {} # rubocop:disable ThreadSafety/InstanceVariableInClassMethod
+        @default_options.merge!(options) # rubocop:disable ThreadSafety/InstanceVariableInClassMethod
       end
 
       def default_options
-        @default_options ||= {}
+        @default_options ||= {} # rubocop:disable ThreadSafety/InstanceVariableInClassMethod
       end
     end
   end
