fbb79d9775640a78247d2e1c13c30c4c0dd94fa9
Mitch Saltykov <mitch.saltykov@agile6.com>
2024-06-26 14:15:08 -0400

    ves data sync script

---
+++ .github/scripts/ves-data-sync.rb
@@ -0,0 +1,28 @@
+# github action - run on push, which executes within the VA network
+
+require 'oci8'
+require 'csv'
+
+# Oracle DB connection parameters
+db_username = ENV['VA_INCOME_LIMITS_VES_DB_USERNAME']
+db_password = ENV['VA_INCOME_LIMITS_VES_DB_PASSWORD']
+db_host = ENV['VA_INCOME_LIMITS_VES_DB_SERVER']
+db_port = ENV['VA_INCOME_LIMITS_VES_DB_PORT']
+db_sid = ENV['VA_INCOME_LIMITS_VES_DB_SID']
+db_connection_string = "//#{db_host}:#{db_port}/#{db_sid}"
+
+# Connect to the Oracle database
+conn = OCI8.new(db_username, db_password, db_connection_string)
+
+# Query the data for the table.
+sql_query = "SELECT * FROM std_institution LIMIT 1;"
+puts "Running query: " + sql_query
+result = conn.exec(sql_query)
+
+puts "Result:"
+puts result
+puts "Result end"
+puts "DB check - StdState.count should be ~110: #{StdState.count}"
+
+# Close the database connection
+conn.logoff if conn
