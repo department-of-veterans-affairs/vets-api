fa70f4e416d073852b53ab276bbb41e4f52d5d85
khoa-v-nguyen <khoa.nguyen@oddball.io>
2024-06-27 11:20:14 -0600

    Refactored HealthFacilitiesController to use facilities_api/v2
    - Updated HealthFacilitiesController to utilize facilities_api/v2.
    - Replaced the deprecated facilities_api/v0 with the latest version.
    - Ensured all endpoints and methods are compatible with the new API version.

--- modules/ask_va_api/app/controllers/ask_va_api/v0/health_facilities_controller.rb
+++ modules/ask_va_api/app/controllers/ask_va_api/v0/health_facilities_controller.rb
@@ -4,16 +4,18 @@ require 'lighthouse/facilities/client'
 
 module AskVAApi
   module V0
-    class HealthFacilitiesController < FacilitiesController
-      around_action :handle_exceptions
+    class HealthFacilitiesController < FacilitiesApi::ApplicationController
+      skip_before_action :verify_authenticity_token
 
-      # Index supports the following query parameters:
-      # @param bbox - Bounding box in form "xmin,ymin,xmax,ymax" in Lat/Long coordinates
-      # @param type - Optional facility type, values = all (default), health, benefits, cemetery
-      # @param services - Optional specialty services filter
-      def index
+      def search
+        params[:facilityIds] = params[:ids] if params[:ids].present?
         api_results = api.get_facilities(lighthouse_params)
 
+        if Flipper.enabled?(:facilities_locator_mobile_covid_online_scheduling) && covid_mobile_params?
+          api_results.each do |api_result|
+            api_result.tmp_covid_online_scheduling = mobile_api_get_by_id(api_result.id)
+          end
+        end
         render_json(serializer, lighthouse_params, api_results)
       end
 
@@ -26,18 +28,19 @@ module AskVAApi
       private
 
       def api
-        Lighthouse::Facilities::Client.new
+        FacilitiesApi::V2::Lighthouse::Client.new
       end
 
       def lighthouse_params
         params.permit(
-          :exclude_mobile,
           :ids,
+          :facilityIds,
           :lat,
           :long,
           :mobile,
           :page,
           :per_page,
+          :radius,
           :state,
           :type,
           :visn,
@@ -48,30 +51,23 @@ module AskVAApi
       end
 
       def serializer
-        Lighthouse::Facilities::FacilitySerializer
-      end
-
-      def handle_exceptions
-        yield
-      rescue => e
-        log_and_render_error('unexpected_error', e, e.status_code)
+        FacilitiesApi::V2::Lighthouse::FacilitySerializer
       end
 
       def resource_path(options)
         v0_health_facilities_url(options)
       end
 
-      def log_and_render_error(action, exception, status)
-        log_error(action, exception)
-        render json: { error: exception.message }, status:
+      def mobile_api
+        FacilitiesApi::V1::MobileCovid::Client.new
       end
 
-      def log_error(action, exception)
-        LogService.new.call(action) do |span|
-          span.set_tag('error', true)
-          span.set_tag('error.msg', exception.message)
-        end
-        Rails.logger.error("Error during #{action}: #{exception.message}")
+      def mobile_api_get_by_id(id)
+        mobile_api.direct_booking_eligibility_criteria_by_id(id).covid_online_scheduling_available?
+      end
+
+      def covid_mobile_params?
+        lighthouse_params.fetch(:type, '')[/health/i] && lighthouse_params[:services]&.any?(/Covid19Vaccine/i)
       end
     end
   end
