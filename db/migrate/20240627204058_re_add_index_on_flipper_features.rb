0dea90d13c1f033eb00db0638a93155a06fcf8d6
Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>
2024-06-28 12:55:27 -0400

    Revert "Fix invalid index `index_flipper_features_on_key` (#17320)"
    This reverts commit 89685c49a1458109898ac3a7c3f56f6516e7a7de.

--- db/migrate/20240627204058_re_add_index_on_flipper_features.rb
+++
@@ -1,8 +0,0 @@
-class ReAddIndexOnFlipperFeatures < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-
-  def change
-    remove_index :flipper_features, column: :key, name: :index_flipper_features_on_key, if_exists: true
-    add_index :flipper_features, :key, unique: true, algorithm: :concurrently
-  end
-end
