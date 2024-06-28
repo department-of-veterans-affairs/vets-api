b0ee3488b39d37a3615328ddc0b22ae740481ea4
Richard Davis <richard.davis@agile6.com>
2024-06-28 10:40:13 -0400

    Updates spec

--- spec/controllers/v0/profile/contacts_controller_spec.rb
+++ spec/controllers/v0/profile/contacts_controller_spec.rb
@@ -18,33 +18,24 @@ RSpec.describe V0::Profile::ContactsController, type: :controller do
       end
     end
 
-    context 'feature enabled' do
-      context 'successful request' do
-        it 'returns emergency contacts' do
-          sign_in_as user
-          expect(subject).to have_http_status(:success)
-          expect(response).to match_response_schema('contacts')
-        end
-      end
-
-      context 'user is not authenticated' do
-        it 'returns an unauthorized status code' do
-          expect(subject).to have_http_status(:unauthorized)
-        end
-      end
-
-      context 'user is loa1' do
-        it 'returns a forbidden status code' do
-          sign_in_as loa1_user
-          expect(subject).to have_http_status(:forbidden)
-        end
+    context 'successful request' do
+      it 'returns emergency contacts' do
+        sign_in_as user
+        expect(subject).to have_http_status(:success)
+        expect(response).to match_response_schema('contacts')
       end
     end
 
-    context 'feature disabled' do
-      it 'returns a not found status code' do
-        sign_in_as user
-        expect(subject).to have_http_status(:not_found)
+    context 'user is not authenticated' do
+      it 'returns an unauthorized status code' do
+        expect(subject).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'user is loa1' do
+      it 'returns a forbidden status code' do
+        sign_in_as loa1_user
+        expect(subject).to have_http_status(:forbidden)
       end
     end
   end
