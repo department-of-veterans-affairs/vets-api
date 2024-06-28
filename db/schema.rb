2d82f81e1d2a1324e4b64cbdc58a008c92affbc0
Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>
2024-06-28 10:29:22 -0400

    fix invalid index index_flipper_features_on_key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_180946) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_27_204058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "pg_stat_statements"
@@ -628,7 +628,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_25_180946) do
     t.datetime "updated_at", null: false
     t.datetime "flagged_value_updated_at"
     t.index ["ip_address", "representative_id", "flag_type", "flagged_value_updated_at"], name: "index_unique_constraint_fields", unique: true
-    t.index ["ip_address", "representative_id", "flag_type"], name: "index_unique_flagged_veteran_representative", unique: true
   end
 
   create_table "flipper_features", force: :cascade do |t|
