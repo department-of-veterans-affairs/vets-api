5ea825b40a49b9475609e8df8b0cb88d66fd1751
Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>
2024-06-28 12:48:18 -0400

    Revert "delete and re-add an invalid index (#17323)"
    This reverts commit 2e11b03686ccca9387879479c88b6eced14048b5.

--- db/migrate/20240628161151_re_add_index_on_flipper_gates.rb
+++
@@ -1,8 +0,0 @@
-class ReAddIndexOnFlipperGates < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-
-  def change
-    remove_index :flipper_gates, name: :index_flipper_gates_on_feature_key_and_key_and_value, if_exists: true
-    add_index :flipper_gates, [:feature_key, :key, :value], unique: true, algorithm: :concurrently
-  end
-end
