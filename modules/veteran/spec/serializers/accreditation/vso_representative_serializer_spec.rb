269aff3dfc77b212846c92340d03f988234bfa5a
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-07-01 17:42:29 -0400

    minor refactoring

--- modules/veteran/spec/serializers/accreditation/vso_representative_serializer_spec.rb
+++ modules/veteran/spec/serializers/accreditation/vso_representative_serializer_spec.rb
@@ -7,28 +7,7 @@ describe Veteran::Accreditation::VSORepresentativeSerializer, type: :serializer 
   subject { serialize(representative, serializer_class: described_class) }
 
   before do
-    create(:representative,
-           representative_id: '123abc',
-           first_name: 'Bob',
-           last_name: 'Law',
-           address_line1: '123 East Main St',
-           address_line2: 'Suite 1',
-           address_line3: 'Address Line 3',
-           address_type: 'DOMESTIC',
-           city: 'My City',
-           country_name: 'United States of America',
-           country_code_iso3: 'USA',
-           province: 'A Province',
-           international_postal_code: '12345',
-           state_code: 'ZZ',
-           zip_code: '12345',
-           zip_suffix: '6789',
-           lat: '39',
-           long: '-75',
-           poa_codes: ['A123'],
-           phone: '222-222-2222',
-           phone_number: '333-333-3333',
-           email: 'email@example.com')
+    create(:representative, :with_address, representative_id: '123abc')
   end
 
   let(:representative) do
