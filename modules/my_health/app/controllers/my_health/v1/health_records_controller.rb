76dc541f4153787acc9096099d71acd707e17cd2
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-07-01 08:26:18 -0400

    swap serializers related serializers (#17290)

--- modules/my_health/app/controllers/my_health/v1/health_records_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/health_records_controller.rb
@@ -15,9 +15,7 @@ module MyHealth
       def eligible_data_classes
         resource = client.get_eligible_data_classes
 
-        render json: resource.data,
-               serializer: EligibleDataClassesSerializer,
-               meta: resource.metadata
+        render json: EligibleDataClassesSerializer.new(resource.data, { meta: resource.metadata })
       end
 
       def create
