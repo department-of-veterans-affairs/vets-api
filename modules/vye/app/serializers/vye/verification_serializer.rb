8d26da9b4189487bb7d1fbb6e430aff23c8c8204
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-07-02 15:22:10 -0400

    Revert "swap AMS serializers for JSONAPI serializers (#17371)"
    This reverts commit c8e8c5417880ad40cea3324df4afd7dd17344dcb.

--- modules/vye/app/serializers/vye/verification_serializer.rb
+++ modules/vye/app/serializers/vye/verification_serializer.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module Vye
-  class VerificationSerializer
-    include JSONAPI::Serializer
-
-    attributes :award_id, :act_begin, :act_end, :transact_date,
-               :monthly_rate, :number_hours, :source_ind
+  class VerificationSerializer < ActiveModel::Serializer
+    attributes(
+      :award_id,
+      :act_begin, :act_end,
+      :transact_date,
+      :monthly_rate, :number_hours, :source_ind
+    )
   end
 end
