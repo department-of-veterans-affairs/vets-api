b4a8a0682b6b9ee25f4b703dab59c4647ee75dfc
Mike Moyer <michael.moyer@va.gov>
2024-07-02 15:53:47 -0400

    MHV-58591 Fixed up unit test to add authentication

--- spec/lib/medical_records/bb_internal/client_spec.rb
+++ spec/lib/medical_records/bb_internal/client_spec.rb
@@ -6,17 +6,21 @@ require 'stringio'
 
 describe BBInternal::Client do
   before(:all) do
-    @client ||= begin
-      client = BBInternal::Client.new(session: { user_id: '11383893' })
-      client.authenticate
-      client
+    VCR.use_cassette 'mr_client/bb_internal/session', record: :new_episodes do
+      VCR.use_cassette 'mr_client/bb_internal/get_patient', record: :new_episodes do
+        @client ||= begin
+          client = BBInternal::Client.new(session: { user_id: '15176497' })
+          client.authenticate
+          client
+        end
+      end
     end
   end
 
   let(:client) { @client }
 
-  describe 'Getting radiology records' do
-    it 'gets the records' do
+  describe 'get_radiology' do
+    it 'gets the radiology records' do
       VCR.use_cassette 'mr_client/bb_internal/get_radiology' do
         radiology_results = client.get_radiology
         expect(radiology_results).to be_an(Array)
