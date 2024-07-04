1b31089736722bd2f83cedc754a609556fc53c3c
nfstern02 <Noah.Stern@va.gov>
2024-07-04 14:39:10 -0500

    Add rake task to reset daily spool file events for the current date

---
+++ spec/rakelib/jobs_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rake'
+
+describe 'jobs rake tasks', type: :request do
+  before :all do
+    Rake.application.rake_require '../rakelib/jobs'
+    Rake::Task.define_task(:environment)
+  end
+
+  describe 'rake jobs:reset_daily_spool_files_for_today' do
+    let :run_rake_task do
+      Rake::Task['jobs:reset_daily_spool_files_for_today'].reenable
+      Rake.application.invoke_task 'jobs:reset_daily_spool_files_for_today'
+    end
+
+    it 'runs without errors in non production environments' do
+      allow(Settings).to receive(:vsp_environment).and_return('staging')
+      expect { run_rake_task }.not_to raise_error
+    end
+
+    it 'raises an exception if the environment is production' do
+      allow(Settings).to receive(:vsp_environment).and_return('production')
+      expect { run_rake_task }.to raise_error Common::Exceptions::Unauthorized
+    end
+  end
+end
