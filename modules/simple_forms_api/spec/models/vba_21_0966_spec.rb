1c548cba60ad39d191b50312d60456edcb0286bf
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-07-05 16:16:36 -0400

    lint

--- modules/simple_forms_api/spec/models/vba_21_0966_spec.rb
+++ modules/simple_forms_api/spec/models/vba_21_0966_spec.rb
@@ -7,9 +7,11 @@ RSpec.describe SimpleFormsApi::VBA210966 do
     context 'data does not already have what it needs' do
       let(:expected_first_name) { 'Rory' }
       let(:expected_last_name) { 'Stewart' }
-      let(:expected_address) { {
-        'postal_code' => '12345'
-      } }
+      let(:expected_address) do
+        {
+          'postal_code' => '12345'
+        }
+      end
       let(:expected_ssn) { 'fake-ssn' }
       let(:user) { create(:user, first_name: expected_first_name, last_name: expected_last_name, ssn: expected_ssn) }
       let(:data) { {} }
@@ -19,27 +21,32 @@ RSpec.describe SimpleFormsApi::VBA210966 do
 
         form = SimpleFormsApi::VBA210966.new(data).populate_veteran_data(user)
 
-        expect(form.data['veteran_full_name']).to eq ({ 'first' => expected_first_name, 'last' => expected_last_name})
+        expect(form.data['veteran_full_name']).to eq({ 'first' => expected_first_name, 'last' => expected_last_name })
         expect(form.data['veteran_mailing_address']).to eq expected_address
-        expect(form.data['veteran_id']).to eq ({ 'ssn' => expected_ssn })
+        expect(form.data['veteran_id']).to eq({ 'ssn' => expected_ssn })
       end
     end
 
     context 'data already has what it needs' do
-      let(:expected_address) { {
-        'postal_code' => '12345'
-      } }
-      let(:expected_full_name) { { 'first' => 'John', 'last' => 'Darwin'} }
+      let(:expected_address) do
+        {
+          'postal_code' => '12345'
+        }
+      end
+      let(:expected_full_name) { { 'first' => 'John', 'last' => 'Darwin' } }
       let(:expected_ssn) { 'fake-ssn' }
       let(:user) { create(:user) }
-      let(:data) { { 'veteran_full_name' => expected_full_name, 'veteran_mailing_address' => expected_address, 'veteran_id' => { 'ssn' => expected_ssn}} }
+      let(:data) do
+        { 'veteran_full_name' => expected_full_name, 'veteran_mailing_address' => expected_address,
+          'veteran_id' => { 'ssn' => expected_ssn } }
+      end
 
       it 'pulls the name from the user' do
         form = SimpleFormsApi::VBA210966.new(data).populate_veteran_data(user)
 
-        expect(form.data['veteran_full_name']).to eq (expected_full_name)
+        expect(form.data['veteran_full_name']).to eq(expected_full_name)
         expect(form.data['veteran_mailing_address']).to eq expected_address
-        expect(form.data['veteran_id']).to eq ({ 'ssn' => expected_ssn })
+        expect(form.data['veteran_id']).to eq({ 'ssn' => expected_ssn })
       end
     end
   end
