db45ae61af21579d4278c9ad51640dc5e847d3fe
Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>
2024-07-09 11:17:00 -0400

    fix invalid index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_01_192934) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_08_210311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "pg_stat_statements"
