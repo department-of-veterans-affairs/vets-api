ad9ffd32b3e64dcdbadd851c49154b09710451e8
Andrew Herzberg <andrew.herzberg@adhocteam.us>
2024-07-10 09:51:30 -0700

    small refactor

--- modules/mobile/app/controllers/mobile/v0/military_information_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/military_information_controller.rb
@@ -33,14 +33,14 @@ module Mobile
         if Flipper.enabled?(:mobile_military_indicator_logger, @current_user)
           discharge_codes = parsed_response.service_history.pluck(:honorable_service_indicator)
 
-          PersonalInformationLog.create(
-            data: { icn: @current_user.icn,
-                    discharge_codes:,
-                    any_y: discharge_codes.any? { |c| c == 'Y' },
-                    any_z: discharge_codes.any? { |c| c == 'Z' },
-                    all_z: discharge_codes.all? { |c| c == 'Z' } },
-            error_class: 'Mobile Military Service Indicator'
-          )
+          data = {
+            icn: @current_user.icn,
+            discharge_codes:,
+            any_y: discharge_codes.any? { |c| c == 'Y' },
+            any_z: discharge_codes.any? { |c| c == 'Z' },
+            all_z: discharge_codes.all? { |c| c == 'Z' }
+          }
+          PersonalInformationLog.create(data:, error_class: 'Mobile Military Service Indicator')
         end
       rescue
         nil
