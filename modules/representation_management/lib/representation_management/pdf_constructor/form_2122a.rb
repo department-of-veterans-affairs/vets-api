b332c4e44e5256e6d6a254bad0ed755648e55462
Josh Fike <josh.fike@oddball.io>
2024-07-10 15:36:03 -0500

    [WIP] Reconcile 2122 and 2122a from original files

--- modules/representation_management/lib/representation_management/pdf_constructor/form_2122a.rb
+++ modules/representation_management/lib/representation_management/pdf_constructor/form_2122a.rb
@@ -14,39 +14,39 @@ module RepresentationManagement
                         'representation_management',
                         'pdf_constructor',
                         'pdf_templates',
-                        '21-22', '1.pdf')
+                        '21-22.pdf')
       end
 
-      # rubocop:disable Layout/LineLength
       def page2_options(data)
-        base_form = 'F[0].Page_2[0]'
+        base_form = 'form1[0].#subform[1]'
         {
           # Header
-          "#{base_form}.SocialSecurityNumber_FirstThreeNumbers[0]": data.dig('veteran', 'ssn')[0..2],
-          "#{base_form}.SocialSecurityNumber_SecondTwoNumbers[0]": data.dig('veteran', 'ssn')[3..4],
-          "#{base_form}.SocialSecurityNumber_LastFourNumbers[0]": data.dig('veteran', 'ssn')[5..8],
+          "#{base_form}.SocialSecurityNumber_FirstThreeNumbers[1]": data.dig('veteran', 'ssn')[0..2],
+          "#{base_form}.SocialSecurityNumber_SecondTwoNumbers[1]": data.dig('veteran', 'ssn')[3..4],
+          "#{base_form}.SocialSecurityNumber_LastFourNumbers[1]": data.dig('veteran', 'ssn')[5..8],
           # Section IV
           # Item 19
-          "#{base_form}.I_Authorize[1]": data['recordConsent'] == true ? 1 : 0,
+          "#{base_form}.AuthorizationForRepAccessToRecords[0]": data['recordConsent'] == true ? 1 : 0,
           # Item 20
-          "#{base_form}.Drug_Abuse[0]": data['consentLimits'].present? && data['consentLimits'].include?('DRUG_ABUSE') ? 1 : 0,
-          "#{base_form}.Alcoholism_Or_Alcohol_Abuse[0]": data['consentLimits'].present? && data['consentLimits'].include?('ALCOHOLISM') ? 1 : 0,
-          "#{base_form}.Infection_With_The_Human_Immunodeficiency_Virus_HIV[0]": data['consentLimits'].present? && data['consentLimits'].include?('HIV') ? 1 : 0,
-          "#{base_form}.sicklecellanemia[0]": data['consentLimits'].present? && data['consentLimits'].include?('SICKLE_CELL') ? 1 : 0,
+          "#{base_form}.LIMITATIONOFCONSENT[0]": data['consentLimits']&.join(', ')&.gsub('_', ' '),
           # Item 21
-          "#{base_form}.I_Authorize[0]": data['consentAddressChange'] == true ? 1 : 0,
+          "#{base_form}.AuthorizationForRepActClaimantsBehalf[0]": data['consentAddressChange'] == true ? 1 : 0,
+          # Conditions of Appointment
           # Item 22B
           "#{base_form}.Date_Signed[0]": I18n.l(Time.zone.now.to_date, format: :va_form),
-          # Item 23B
+          # Item 23
+          "#{base_form}.LIMITATIONS[0]": data['conditionsOfAppointment']&.join(', '),
+          # Item 24B
           "#{base_form}.Date_Signed[1]": I18n.l(Time.zone.now.to_date, format: :va_form)
         }
       end
 
       # rubocop:disable Metrics/MethodLength
+      # rubocop:disable Layout/LineLength
       def template_options(data)
-        base_form = 'F[0].Page_1[0]'
+        base_form = 'form1[0].#subform[0]'
         {
-          # Section I
+          # Section !
           # Item 1
           "#{base_form}.VeteransLastName[0]": data.dig('veteran', 'lastName'),
           "#{base_form}.VeteransFirstName[0]": data.dig('veteran', 'firstName'),
@@ -59,51 +59,57 @@ module RepresentationManagement
           "#{base_form}.DOBday[0]": data.dig('veteran', 'birthdate').split('-').last.first(2),
           "#{base_form}.DOByear[0]": data.dig('veteran', 'birthdate').split('-').first,
           # Item 5
-          "#{base_form}.InsuranceNumber_s[0]": data.dig('veteran', 'insuranceNumber'),
+          "#{base_form}.VeteransServiceNumber[0]": data.dig('veteran', 'serviceNumber'),
+          # Item 6 Service Branch
+          "#{base_form}.ARMYCheckbox1[0]": (data.dig('veteran', 'serviceBranch') == 'ARMY' ? 1 : 0),
+          "#{base_form}.NAVYCheckbox2[0]": (data.dig('veteran', 'serviceBranch') == 'NAVY' ? 1 : 0),
+          "#{base_form}.AIR_FORCECheckbox3[0]": (data.dig('veteran', 'serviceBranch') == 'AIR_FORCE' ? 1 : 0),
+          "#{base_form}.MARINE_CORPSCheckbox4[0]": (data.dig('veteran', 'serviceBranch') == 'MARINE_CORPS' ? 1 : 0),
+          "#{base_form}.COAST_GUARDCheckbox5[0]": (data.dig('veteran', 'serviceBranch') == 'COAST_GUARD' ? 1 : 0),
+          "#{base_form}.SPACE_FORCECheckbox3[0]": (data.dig('veteran', 'serviceBranch') == 'SPACE_FORCE' ? 1 : 0),
+          "#{base_form}.OTHER_Checkbox6[0]": (data.dig('veteran', 'serviceBranch') == 'OTHER' ? 1 : 0),
+          "#{base_form}.JF15[0]": data.dig('veteran', 'serviceBranchOther'),
           # Item 7
           "#{base_form}.Veterans_MailingAddress_NumberAndStreet[0]": data.dig('veteran', 'address', 'addressLine1'),
-          "#{base_form}.Claimants_MailingAddress_ApartmentOrUnitNumber[1]": data.dig('veteran', 'address', 'addressLine2'),
-          "#{base_form}.Claimants_MailingAddress_City[1]": data.dig('veteran', 'address', 'city'),
-          "#{base_form}.Claimants_MailingAddress_StateOrProvince[1]": data.dig('veteran', 'address', 'stateCode'),
-          "#{base_form}.Claimants_MailingAddress_Country[1]": data.dig('veteran', 'address', 'country'),
-          "#{base_form}.Claimants_MailingAddress_ZIPOrPostalCode_FirstFiveNumbers[1]": data.dig('veteran', 'address', 'zipCode'),
-          "#{base_form}.Claimants_MailingAddress_ZIPOrPostalCode_LastFourNumbers[1]": data.dig('veteran', 'address', 'zipCodeSuffix'),
+          "#{base_form}.MailingAddress_ApartmentOrUnitNumber[1]": data.dig('veteran', 'address', 'addressLine2'),
+          "#{base_form}.MailingAddress_City[1]": data.dig('veteran', 'address', 'city'),
+          "#{base_form}.MailingAddress_StateOrProvince[1]": data.dig('veteran', 'address', 'stateCode'),
+          "#{base_form}.MailingAddress_Country[1]": data.dig('veteran', 'address', 'country'),
+          "#{base_form}.MailingAddress_ZIPOrPostalCode_FirstFiveNumbers[1]": data.dig('veteran', 'address', 'zipCode'),
+          "#{base_form}.MailingAddress_ZIPOrPostalCode_ZIPOrPostalCode_LastFourNumbers[1]": data.dig('veteran', 'address', 'zipCodeSuffix'),
           # Item 8
-          "#{base_form}.TelephoneNumber_IncludeAreaCode[1]": "#{data.dig('veteran', 'phone', 'areaCode')} #{data.dig('veteran', 'phone', 'phoneNumber')}",
+          "#{base_form}.TelephoneNumber_IncludeAreaCode[0]": "#{data.dig('veteran', 'phone', 'areaCode')} #{data.dig('veteran', 'phone', 'phoneNumber')}",
           # Item 9
           "#{base_form}.EmailAddress_Optional[0]": data.dig('veteran', 'email'),
 
           # Section II
           # Item 10
-          "#{base_form}.Claimants_FirstName[0]": data.dig('claimant', 'firstName'),
-          "#{base_form}.Claimants_MiddleInitial1[0]": data.dig('claimant', 'middleInitial'),
-          "#{base_form}.Claimants_LastName[0]": data.dig('claimant', 'lastName'),
+          "#{base_form}.Claimants_First_Name[0]": data.dig('claimant', 'firstName'),
+          "#{base_form}.Claimants_Middle_Initial1[0]": data.dig('claimant', 'middleInitial'),
+          "#{base_form}.Claimants_Last_Name[0]": data.dig('claimant', 'lastName'),
           # Item 11
-          "#{base_form}.Claimants_MailingAddress_NumberAndStreet[0]": data.dig('claimant', 'address', 'addressLine1'),
-          "#{base_form}.Claimants_MailingAddress_ApartmentOrUnitNumber[0]": data.dig('claimant', 'address', 'addressLine2'),
-          "#{base_form}.Claimants_MailingAddress_City[0]": data.dig('claimant', 'address', 'city'),
-          "#{base_form}.Claimants_MailingAddress_StateOrProvince[0]": data.dig('claimant', 'address', 'stateCode'),
-          "#{base_form}.Claimants_MailingAddress_Country[0]": data.dig('claimant', 'address', 'country'),
-          "#{base_form}.Claimants_MailingAddress_ZIPOrPostalCode_FirstFiveNumbers[0]": data.dig('claimant', 'address', 'zipCode'),
-          "#{base_form}.Claimants_MailingAddress_ZIPOrPostalCode_LastFourNumbers[0]": data.dig('address', 'zipCodeSuffix'),
+          "#{base_form}.MailingAddress_NumberAndStreet[0]": data.dig('claimant', 'address', 'addressLine1'),
+          "#{base_form}.MailingAddress_ApartmentOrUnitNumber[0]": data.dig('claimant', 'address', 'addressLine2'),
+          "#{base_form}.MailingAddress_City[0]": data.dig('claimant', 'address', 'city'),
+          "#{base_form}.MailingAddress_StateOrProvince[0]": data.dig('claimant', 'address', 'stateCode'),
+          "#{base_form}.MailingAddress_Country[0]": data.dig('claimant', 'address', 'country'),
+          "#{base_form}.MailingAddress_ZIPOrPostalCode_FirstFiveNumbers[0]": data.dig('claimant', 'address', 'zipCode'),
+          "#{base_form}.CurrentMailingAddress_ZIPOrPostalCode_LastFourNumbers[0]": data.dig('address', 'zipCodeSuffix'),
           # Item 12
-          "#{base_form}.TelephoneNumber_IncludeAreaCode[0]": "#{data.dig('claimant', 'phone', 'areaCode')} #{data.dig('claimant', 'phone', 'phoneNumber')}",
+          "#{base_form}.TelephoneNumber_IncludeAreaCode[1]": "#{data.dig('claimant', 'phone', 'areaCode')} #{data.dig('claimant', 'phone', 'phoneNumber')}",
           # Item 13
-          "#{base_form}.Claimants_EmailAddress_Optional[0]": data.dig('claimant', 'email'),
+          "#{base_form}.EmailAddress_Optional[1]": data.dig('claimant', 'email'),
           # Item 14
-          "#{base_form}.Relationship_To_Veteran[0]": data.dig('claimant', 'relationship'),
+          "#{base_form}.RelationshipToVeteran[0]": data.dig('claimant', 'relationship'),
 
           # Section III
-          # Item 15
-          "#{base_form}.Name_Of_Service_Organization[0]": data.dig('serviceOrganization', 'organizationName'),
-          # Item 16A
-          "#{base_form}.Name_Of_Official_Representative[0]": "#{data.dig('serviceOrganization', 'firstName')} #{data.dig('serviceOrganization', 'lastName')}",
-          # Item 16B
-          "#{base_form}.Job_Title_Of_Person_Named_In_Item15A[0]": data.dig('serviceOrganization', 'jobTitle'),
-          # Item 17
-          "#{base_form}.Email_Address[0]": data.dig('serviceOrganization', 'email'),
+          # Item 15A
+          "#{base_form}.NAME_OF_INDIVIDUAL_APPOINTED_AS_REPRESENTATIVE[0]": "#{data.dig('representative', 'firstName')} #{data.dig('representative', 'lastName')}",
+          # Item 15B
+          "#{base_form}.Checkbox1[0]": (data.dig('representative', 'type') == 'ATTORNEY' ? 1 : 0),
+          "#{base_form}.Checkbox2[0]": (data.dig('representative', 'type') == 'AGENT' ? 1 : 0),
           # Item 18
-          "#{base_form}.Date_Of_This_Appointment[0]": I18n.l(Time.zone.now.to_date, format: :va_form)
+          "#{base_form}.ADDRESSOFINDIVIDUALAPPOINTEDASCLAIMANTSREPRESENTATATIVE[0]": stringify_address(data.dig('representative', 'address'))
         }
       end
       # rubocop:enable Metrics/MethodLength
