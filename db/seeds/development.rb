fd5aeb560def93f227cca16c30293584dcc0b026
Darius Dzien <dariusz.dzien@gsa.gov>
2024-07-10 10:23:14 -0500

    Clean up

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -129,14 +129,17 @@ arp.update!(authentication: SignIn::Constants::Auth::COOKIE,
             service_levels: [SignIn::Constants::Auth::LOA3, SignIn::Constants::Auth::IAL2])
 
 # Create VerifiedRepresentative and AccreditedInvidial for logging into accredited_representative_portal
+# Skip validations on save because not all required fields are needed for testing purposes.
 ogc_registration_number = '12345'
+poa_code = 678
+test_rep_email = 'vets.gov.user+1@gmail.com'
 
 accredited_individual = AccreditedIndividual.find_or_initialize_by(registration_number: ogc_registration_number)
-accredited_individual.poa_code = 678
-accredited_individual.save(validate: false)
+accredited_individual.poa_code = poa_code
+accredited_individual.save!(validate: false)
 
-verified_rep = VerifiedRepresentative.find_or_initialize_by(email: 'vets.gov.user+1@gmail.com')
-verified_rep.save(validate: false)
+verified_rep = VerifiedRepresentative.find_or_initialize_by(email: test_rep_email)
+verified_rep.save!(validate: false)
 
 # Create Service Account Config for BTSSS
 btsss = SignIn::ServiceAccountConfig.find_or_initialize_by(service_account_id: 'bbb5830ecebdef04556e9c430e374972')
