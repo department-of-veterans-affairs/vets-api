6e8675c2c25d4d2c1e40e56a2f40b58664329d24
micahaspyr <micah.frazier82@gmail.com>
2024-07-11 17:32:38 -0600

    Added user_account_uuid to evss_user factory instance

--- app/controllers/v0/veteran_readiness_employment_claims_controller.rb
+++ app/controllers/v0/veteran_readiness_employment_claims_controller.rb
@@ -7,10 +7,8 @@ module V0
     skip_before_action :load_user
 
     def create
-      binding.pry
       claim = SavedClaim::VeteranReadinessEmploymentClaim.new(form: filtered_params[:form],
                                                               user_account_id: current_user&.user_account_uuid)
-      binding.pry
       if claim.save
         VRE::Submit1900Job.perform_async(claim.id, current_user.uuid)
         Rails.logger.info "ClaimID=#{claim.confirmation_number} Form=#{claim.class::FORM}"
