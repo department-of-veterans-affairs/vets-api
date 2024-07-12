2c9a7c7430282fd848345bc0a38a34c497394bbf
Brandon Cooper <brandon.cooper@adhocteam.us>
2024-07-12 11:08:31 -0400

    refactor methods into variables

--- lib/carma/client/mule_soft_client.rb
+++ lib/carma/client/mule_soft_client.rb
@@ -36,11 +36,9 @@ module CARMA
         with_monitoring do
           Rails.logger.info "[Form 10-10CG] Submitting to '#{resource}'"
           args = post_args(resource, payload, timeout)
-          resp = perform(*args)
+          response = perform(*args)
 
-          Sentry.set_extras(response_body: resp.body)
-          raise_error_unless_success(resource, resp.status)
-          JSON.parse(resp.body)
+          handle_response(resource, response)
         end
       end
 
@@ -55,6 +53,12 @@ module CARMA
         payload.is_a?(String) ? payload : payload.to_json
       end
 
+      def handle_response(resource, response)
+        Sentry.set_extras(response_body: response.body)
+        raise_error_unless_success(resource, response.status)
+        JSON.parse(response.body)
+      end
+
       def raise_error_unless_success(resource, status)
         Rails.logger.info "[Form 10-10CG] Submission to '#{resource}' resource resulted in response code #{status}"
         return if [200, 201, 202].include? status
@@ -62,15 +66,15 @@ module CARMA
         raise Common::Exceptions::SchemaValidationErrors, ["Expecting 200 status but received #{status}"]
       end
 
+      # New Authentication strategy
+
       # Call Mulesoft with bearer token
       def perform_post(resource, payload)
         Rails.logger.info "[Form 10-10CG] Submitting to '#{resource}' using bearer token"
 
-        resp = perform(:post, resource, get_body(payload), { Authorization: "Bearer #{bearer_token}" })
+        response = perform(:post, resource, get_body(payload), { Authorization: "Bearer #{bearer_token}" })
 
-        Sentry.set_extras(response_body: resp.body)
-        raise_error_unless_success(resource, resp.status)
-        JSON.parse(resp.body)
+        handle_response(resource, response)
       end
 
       # get token
@@ -79,29 +83,23 @@ module CARMA
       end
 
       def get_new_bearer_token
+        encoded_params = URI.encode_www_form({
+                                               grant_type: 'client_credentials',
+                                               scope: 'read' # TODO: this is probably a write?
+                                             })
+
+        basic_auth =  Base64.urlsafe_encode64("#{config.settings.v2.client_id}:#{config.settings.v2.client_secret}")
+
+        token_headers = {
+          'Authorization' => "Basic #{basic_auth}",
+          'Content-Type' => 'application/x-www-form-urlencoded'
+        }
+
         response = perform(:post,
                            config.settings.v2.token_url,
                            encoded_params,
                            token_headers)
         response.body[:access_token]
-      end
-
-      def encoded_params
-        URI.encode_www_form({
-                              grant_type: 'client_credentials',
-                              scope: 'read'
-                            })
-      end
-
-      def token_headers
-        {
-          'Authorization' => "Basic #{basic_auth}",
-          'Content-Type' => 'application/x-www-form-urlencoded'
-        }
-      end
-
-      def basic_auth
-        Base64.urlsafe_encode64("#{config.settings.v2.client_id}:#{config.settings.v2.client_secret}")
       end
     end
   end
