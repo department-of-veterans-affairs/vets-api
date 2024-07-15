e417c03ec843fbf12d2ca231978a98461808a5de
Tom Harrison <tom.harrison@adhocteam.us>
2024-07-15 10:33:03 -0400

    remove unused, commented code from serializer

--- app/serializers/submission_status_serializer.rb
+++ app/serializers/submission_status_serializer.rb
@@ -4,8 +4,4 @@ class SubmissionStatusSerializer
   include JSONAPI::Serializer
 
   attributes :id, :form_type, :status, :created_at, :updated_at
-
-  # def read_attribute_for_serialization(attr)
-  #   return object[attr.to_s]
-  # end
 end
