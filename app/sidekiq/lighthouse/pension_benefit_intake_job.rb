227b66d705b4d191f576a758da218a330e416f0a
Scott Gorman <scott.gorman1@va.gov>
2024-07-15 10:04:13 -0400

    - Pass user uuid to form submission creation.

--- app/sidekiq/lighthouse/pension_benefit_intake_job.rb
+++ app/sidekiq/lighthouse/pension_benefit_intake_job.rb
@@ -144,7 +144,8 @@ module Lighthouse
         form_data: @claim.to_json,
         benefits_intake_uuid: @intake_service.uuid,
         saved_claim: @claim,
-        saved_claim_id: @claim.id
+        saved_claim_id: @claim.id,
+        user_account: UserAccount.find_by(uuid: @user_uuid)
       )
       @form_submission_attempt = FormSubmissionAttempt.create(form_submission:)
 
