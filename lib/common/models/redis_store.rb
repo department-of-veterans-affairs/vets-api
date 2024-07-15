1219ca3d279eb17f0e5c8d6282161c34883769f9
Ryan Johnson <ryan.johnson@oddball.io>
2024-07-15 13:16:12 -0400

    Add connectivity test to redis store

--- lib/common/models/redis_store.rb
+++ lib/common/models/redis_store.rb
@@ -99,6 +99,15 @@ module Common
       redis_namespace.del(redis_key)
     end
 
+    def self.test_connectivity
+      begin
+        redis_namespace.ping == 'PONG'
+      rescue Redis::CannotConnectError => e
+        Rails.logger.error "Redis connection failed: #{e.message}"
+        false
+      end
+    end
+
     def save
       return false unless valid?
 
