822511a48de9a5124a66c77653c7af13e9f04d20
agravell047 <anthony.gravell@bylight.com>
2024-07-15 10:39:28 -0400

    adds new controller, tests

--- modules/my_health/app/controllers/my_health/v1/prescriptions_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/prescriptions_controller.rb
@@ -41,20 +41,6 @@ module MyHealth
         head :no_content
       end
 
-      def documentation
-        if Flipper.enabled?(:mhv_medications_display_documentation_content, @current_user)
-          begin
-            documentation = client.get_rx_documentation(params[:ndc])
-            render json: PrescriptionDocumentation.new({ html: documentation[:data] }),
-                   serializer: PrescriptionDocumentationSerializer
-          rescue => e
-            render json: { error: "Unable to fetch documentation: #{e}" }, status: :service_unavailable
-          end
-        else
-          render json: { error: 'Documentation is not available' }, status: :not_found
-        end
-      end
-
       def refill_prescriptions
         ids = params[:ids]
         successful_ids = []
