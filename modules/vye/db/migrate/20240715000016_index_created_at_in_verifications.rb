2193eeaa2b266303f519c1cba36859ba7e8921cd
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-07-15 23:41:52 -0500

    added indexes

---
+++ modules/vye/db/migrate/20240715000016_index_created_at_in_verifications.rb
@@ -0,0 +1,7 @@
+class IndexCreatedAtInVerifications < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
+  def change
+    add_index :vye_verifications, :created_at, algorithm: :concurrently
+  end
+end
