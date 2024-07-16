af75693abbb1169a25c61814a2b3feccbe727270
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-07-16 09:29:29 -0500

    removing old

--- db/migrate/20240716043510_index_created_at_in_verifications.vye.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from vye (originally 20240715000016)
-class IndexCreatedAtInVerifications < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-
-  def change
-    add_index :vye_verifications, :created_at, algorithm: :concurrently
-  end
-end
--- db/migrate/20240716043512_index_created_at_in_addresses.vye.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from vye (originally 20240715000018)
-class IndexCreatedAtInAddresses < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-
-  def change
-    add_index :vye_address_changes, :created_at, algorithm: :concurrently
-  end
-end
