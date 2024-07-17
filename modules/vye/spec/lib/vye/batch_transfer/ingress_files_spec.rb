ec3de295f4aabaf09a2214bb8cefc7d9a656025e
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-07-17 10:32:33 -0500

    Updates for ingress (#17558)
    * removing validations that are problematic with BDN
    
    * loading TIMS after BDN
    
    * added validation on transact_date
    
    * sidekiq updates
    
    * clear pending documents before new import
    
    * rspec updates

--- modules/vye/spec/lib/vye/batch_transfer/ingress_files_spec.rb
+++ modules/vye/spec/lib/vye/batch_transfer/ingress_files_spec.rb
@@ -28,15 +28,31 @@ RSpec.describe Vye::BatchTransfer::IngressFiles do
     )
   end
 
-  it 'imports lines from TIMS extract' do
-    data = Vye::Engine.root / 'spec/fixtures/tims_sample/tims32towave.txt'
-    expect do
-      described_class.send(:tims_import, data)
-    end.to(
-      change(Vye::UserProfile, :count).by(20).and(
-        change(Vye::PendingDocument, :count).by(20)
-      )
-    )
+  describe 'imports lines from TIMS extract' do
+    before do
+      create(:vye_user_profile_fresh_import, ssn: '441972624', file_number: '227366592')
+      create(:vye_user_profile_fresh_import, ssn: '596100167', file_number: '662929072')
+      create(:vye_user_profile_fresh_import, ssn: '194889304', file_number: '301143261')
+      create(:vye_user_profile_fresh_import, ssn: '261045161', file_number: '704243999')
+      create(:vye_user_profile_fresh_import, ssn: '036662203', file_number: '690756310')
+      create(:vye_user_profile_fresh_import, ssn: '942504788', file_number: '738416685')
+      create(:vye_user_profile_fresh_import, ssn: '261077041', file_number: '823716203')
+      create(:vye_user_profile_fresh_import, ssn: '970447691', file_number: '420365151')
+      create(:vye_user_profile_fresh_import, ssn: '151014371', file_number: '948813522')
+      create(:vye_user_profile_fresh_import, ssn: '807164639', file_number: '444442869')
+      create(:vye_user_profile_fresh_import, ssn: '124496046', file_number: '114591317')
+      create(:vye_user_profile_fresh_import, ssn: '045274951', file_number: '037619065')
+      create(:vye_user_profile_fresh_import, ssn: '500042905', file_number: '732531728')
+      create(:vye_user_profile_fresh_import, ssn: '333224444', file_number: '883200138')
+      create(:vye_user_profile_fresh_import, ssn: '992549762', file_number: '333224444')
+    end
+
+    it 'only loads when there is a UserProfile that matches' do
+      data = Vye::Engine.root / 'spec/fixtures/tims_sample/tims32towave.txt'
+      expect do
+        described_class.send(:tims_import, data)
+      end.to(change(Vye::PendingDocument, :count).by(13))
+    end
   end
 
   it 'loads the BDN feed from AWS' do
