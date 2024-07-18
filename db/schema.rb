a1cd6abd2f53a6bdd74b58847c02d557e1144988
Darius Dzien <dariusz.dzien@gsa.gov>
2024-07-18 11:34:24 -0500

    DB clean up

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2024_07_12_185835) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2024_07_16_143105) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "pg_stat_statements"
