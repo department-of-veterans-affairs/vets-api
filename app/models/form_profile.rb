23b1bf1a26b48693b569123c6db62e292b9bddca
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-07-18 13:38:12 -0400

    Added 21-22 and 21-22a pre-fill to the ALL_FORMS object

--- app/models/form_profile.rb
+++ app/models/form_profile.rb
@@ -103,7 +103,8 @@ class FormProfile
     adapted_housing: ['26-4555'],
     intent_to_file: ['21-0966'],
     ivc_champva: %w[10-7959F-1 10-7959C],
-    form_upload_flow: ['FORM-UPLOAD-FLOW']
+    form_upload_flow: ['FORM-UPLOAD-FLOW'],
+    acc_rep_management:  %w[21-22 21-22A],
   }.freeze
 
   FORM_ID_TO_CLASS = {
@@ -143,7 +144,9 @@ class FormProfile
     '26-4555' => ::FormProfiles::VA264555,
     '21-0966' => ::FormProfiles::VA210966,
     '10-7959F-1' => ::FormProfiles::VA107959f1,
-    'FORM-UPLOAD-FLOW' => ::FormProfiles::FormUploadFlow
+    'FORM-UPLOAD-FLOW' => ::FormProfiles::FormUploadFlow,
+    '21-22' => ::FormProfiles::VA2122,
+    '21-22A' => ::FormProfiles::VA2122a,
   }.freeze
 
   APT_REGEX = /\S\s+((apt|apartment|unit|ste|suite).+)/i
