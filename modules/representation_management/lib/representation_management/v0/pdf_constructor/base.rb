9989c290510bdc71bfea1b7405e6a1961638cb60
Josh Fike <josh.fike@oddball.io>
2024-07-19 12:09:11 -0500

    Cleanup old methods

--- modules/representation_management/lib/representation_management/v0/pdf_constructor/base.rb
+++ modules/representation_management/lib/representation_management/v0/pdf_constructor/base.rb
@@ -11,11 +11,11 @@ module RepresentationManagement
           @template_path = nil
         end
 
-        def construct(data, id: SecureRandom.uuid)
+        def construct(data)
           set_template_path
           # fill_pdf(data)
           # combine_pdf(id, @template_path)
-          fill_and_combine_pdf(data, id)
+          fill_and_combine_pdf(data)
         end
 
         protected
@@ -35,50 +35,16 @@ module RepresentationManagement
         private
 
         #
-        # Produce final pdf with all pages combined.
-        #
-        # @param id [type] [description]
-        # @param template_path [String] Path to page 1 of pdf
-        # @param page2_path [String] Path to page 2 of pdf
-        #
-        # @return [String] Path to final pdf
-        def combine_pdf(id, template_path)
-          output_path = "/tmp/#{id}_final.pdf"
-
-          pdf = CombinePDF.new
-          pdf << CombinePDF.load(template_path)
-          pdf.save(output_path)
-
-          output_path
-        end
-
-        #
-        # Fill in pdf form fields based on data provided.
+        # Fill in pdf form fields based on data provided, then combine all
+        # the pages into a final pdf.  We create an inner tempfile to fill
+        # and the output from this method is written to a tempfile in
+        # the controller.
         #
         # @param data [Hash] Data to fill in pdf form with
-        def fill_pdf(data)
+        def fill_and_combine_pdf(data)
           pdftk = PdfForms.new(Settings.binaries.pdftk)
 
-          Tempfile.create(["poa_#{Time.now.to_i}_page_1", '.pdf'], Rails.root.join('tmp')) do |tempfile|
-            p "template_options(data): #{template_options(data)}", "tempfile path: #{tempfile.path}",
-              "template_path: #{@template_path}"
-            pdftk.fill_form(
-              @template_path,
-              tempfile.path,
-              template_options(data),
-              flatten: true
-            )
-            # Ensure the file is not deleted until we are done with it
-            tempfile.close
-            @template_path = tempfile.path
-          end
-          # Tempfile is automatically deleted here, but @template_path has been updated
-          # If you need to use the tempfile beyond this block, consider adjustments.
-        end
-
-        def fill_and_combine_pdf(data, id)
-          pdftk = PdfForms.new(Settings.binaries.pdftk)
-
+          # We need a Tempfile here because CombinePDF needs a file to load.
           template_tempfile = Tempfile.new
           pdftk.fill_form(
             @template_path,
@@ -96,9 +62,9 @@ module RepresentationManagement
           pdf.save(output_path)
 
           @tempfile.rewind
+          # Delete the tempfile we created now that CombinePDF has saved
+          # the final pdf.
           template_tempfile.unlink
-
-          output_path
         end
       end
     end
