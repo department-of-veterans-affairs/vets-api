2a87a69c49792ca2ddd7d6c77f6355c78d57437a
Devin McCurdy <devin@apothesource.com>
2024-07-22 13:20:24 -0400

    Add VPG appointment slot fields to mobile module

--- modules/mobile/spec/serializers/mobile/v0/clinic_slots_serializer_spec.rb
+++ modules/mobile/spec/serializers/mobile/v0/clinic_slots_serializer_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe VAOS::V2::SlotsSerializer do
+describe Mobile::V0::ClinicSlotsSerializer do
   describe 'json serialization' do
     context 'with a slot' do
       let(:slot) do
@@ -14,9 +14,9 @@ describe VAOS::V2::SlotsSerializer do
       end
 
       it 'serializes correctly' do
-        expect(VAOS::V2::SlotsSerializer.new(slot).serializable_hash.to_json).to eq(
-          '{"data":{"id":"123","type":"slots","attributes":{"start":"2021-10-26T22:00:00Z"' \
-          ',"end":"2021-10-27T00:00:00Z","location_id":null,"clinic_ien":null,"practitioner_name":null}}}'
+        expect(Mobile::V0::ClinicSlotsSerializer.new(slot).serializable_hash.to_json).to eq(
+          '{"data":{"id":"123","type":"clinic_slot","attributes":{"start_date":"2021-10-26T22:00:00Z"' \
+          ',"end_date":"2021-10-27T00:00:00Z","location_id":null,"clinic_ien":null,"practitioner_name":null}}}'
         )
       end
     end
@@ -34,9 +34,9 @@ describe VAOS::V2::SlotsSerializer do
       end
 
       it 'serializes correctly' do
-        expect(VAOS::V2::SlotsSerializer.new(slot_all_fields).serializable_hash.to_json).to eq(
-          '{"data":{"id":"123","type":"slots","attributes":{"start":"2021-10-26T22:00:00Z"' \
-          ',"end":"2021-10-27T00:00:00Z","location_id":"757GC","clinic_ien":"123"' \
+        expect(Mobile::V0::ClinicSlotsSerializer.new(slot_all_fields).serializable_hash.to_json).to eq(
+          '{"data":{"id":"123","type":"clinic_slot","attributes":{"start_date":"2021-10-26T22:00:00Z"' \
+          ',"end_date":"2021-10-27T00:00:00Z","location_id":"757GC","clinic_ien":"123"' \
           ',"practitioner_name":"Doe, John D, MD"}}}'
         )
       end
