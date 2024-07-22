753d94ae232f9d537268362f93a98174eabe6462
Brandon Cooper <brandon.cooper@adhocteam.us>
2024-07-22 09:23:23 -0400

    revert the changes to the existing do_post methods to limit changes in prod

--- lib/carma/client/mule_soft_client.rb
+++ lib/carma/client/mule_soft_client.rb
@@ -18,9 +18,9 @@ module CARMA
       def create_submission_v2(payload)
         with_monitoring do
           res = if Flipper.enabled?(:cg1010_oauth_2_enabled)
-                  perform_post('v2/application/1010CG/submit', payload)
+                  perform_post(payload)
                 else
-                  do_post('v2/application/1010CG/submit', payload)
+                  do_post('v2/application/1010CG/submit', payload, config.settings.async_timeout)
                 end
 
           raise RecordParseError if res.dig('record', 'hasErrors')
@@ -34,31 +34,23 @@ module CARMA
       # @param resource [String] REST-ful path component
       # @param payload [String] JSON payload to submit
       # @return [Hash]
-      def do_post(resource, payload)
+      def do_post(resource, payload, timeout = config.timeout)
         with_monitoring do
           Rails.logger.info "[Form 10-10CG] Submitting to '#{resource}'"
-          args = post_args(resource, payload)
-          response = perform(*args)
-
-          handle_response(resource, response)
+          args = post_args(resource, payload, timeout)
+          resp = perform(*args)
+          Sentry.set_extras(response_body: resp.body)
+          raise_error_unless_success(resource, resp.status)
+          JSON.parse(resp.body)
         end
       end
 
       # @return [Array]
-      def post_args(resource, payload)
+      def post_args(resource, payload, timeout)
+        body = payload.is_a?(String) ? payload : payload.to_json
         headers = config.base_request_headers
-        opts = { timeout: config.settings.async_timeout }
-        [:post, resource, get_body(payload), headers, opts]
-      end
-
-      def get_body(payload)
-        payload.is_a?(String) ? payload : payload.to_json
-      end
-
-      def handle_response(resource, response)
-        Sentry.set_extras(response_body: response.body)
-        raise_error_unless_success(resource, response.status)
-        JSON.parse(response.body)
+        opts = { timeout: }
+        [:post, resource, body, headers, opts]
       end
 
       def raise_error_unless_success(resource, status)
@@ -70,7 +62,8 @@ module CARMA
 
       # New Authentication strategy
       # Call Mulesoft with bearer token
-      def perform_post(resource, payload)
+      def perform_post(payload)
+        resource = 'v2/application/1010CG/submit'
         with_monitoring do
           Rails.logger.info "[Form 10-10CG] Submitting to '#{resource}' using bearer token"
 
@@ -88,6 +81,16 @@ module CARMA
 
       def bearer_token
         @bearer_token ||= CARMA::Client::MuleSoftAuthTokenClient.new.new_bearer_token
+      end
+
+      def get_body(payload)
+        payload.is_a?(String) ? payload : payload.to_json
+      end
+
+      def handle_response(resource, response)
+        Sentry.set_extras(response_body: response.body)
+        raise_error_unless_success(resource, response.status)
+        JSON.parse(response.body)
       end
     end
   end
