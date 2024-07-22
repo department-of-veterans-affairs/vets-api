0fbd4ba97ecc058196763d3265793e9680017bb1
Kris Pethtel <kris.pethtel@gmail.com>
2024-07-22 10:32:20 -0500

    better specs

--- modules/mobile/spec/request/enrollment_status_request_spec.rb
+++ modules/mobile/spec/request/enrollment_status_request_spec.rb
@@ -6,27 +6,23 @@ RSpec.describe 'enrollment status', type: :request do
   let!(:user) { sis_user }
 
   describe 'GET /mobile/v0/enrollment-status' do
-    let(:success_response) do
-      {
-        application_date: '2018-01-24T00:00:00.000-06:00',
-        enrollment_date: nil,
-        preferred_facility: '987 - CHEY6',
-        parsed_status: HCA::EnrollmentEligibility::Constants::INELIG_CHARACTER_OF_DISCHARGE.to_s,
-        primary_eligibility: 'SC LESS THAN 50%',
-        can_submit_financial_info: true
-      }
-    end
-
     context 'with an loa3 user' do
       it 'returns ok with enrollment status information' do
-        expect(HealthCareApplication).to receive(:enrollment_status).with(
-          user.icn, true
-        ).and_return(success_response)
-        get('/mobile/v0/enrollment-status', headers: sis_headers)
+        VCR.use_cassette('hca/ee/lookup_user', erb: true) do
+          get('/mobile/v0/enrollment-status', headers: sis_headers)
+        end
 
         expect(response).to have_http_status(:ok)
-        expected_attributes = success_response.transform_keys { |key| key.to_s.camelize(:lower) }
-        expect(response.parsed_body.dig('data', 'attributes')).to match(expected_attributes)
+        expect(response.parsed_body.dig('data', 'attributes')).to match(
+          {
+            applicationDate: '2018-12-27T00:00:00.000-06:00',
+            enrollmentDate: '2018-12-27T17:15:39.000-06:00',
+            preferredFacility: '988 - DAYT20',
+            parsedStatus: 'enrolled',
+            primaryEligibility: 'SC LESS THAN 50%',
+            canSubmitFinancialInfo: true
+          }
+        )
       end
     end
 
