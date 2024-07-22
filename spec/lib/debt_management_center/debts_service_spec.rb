ce9914d0f9a8d6fb9c355577825a1522e423cdbe
Derek Dyer <derek@digitalderek.com>
2024-07-22 01:00:29 -0400

    Remove comment

--- spec/lib/debt_management_center/debts_service_spec.rb
+++ spec/lib/debt_management_center/debts_service_spec.rb
@@ -24,58 +24,6 @@ RSpec.describe DebtManagementCenter::DebtsService do
     end
   end
 
-  # describe '#get_letters' do
-  #   context 'with a valid file number' do
-  #     it 'fetches the veterans debt data' do
-  #       VCR.use_cassette('bgs/people_service/person_data') do
-  #         VCR.use_cassette('debts/get_letters', VCR::MATCH_EVERYTHING) do
-  #           res = described_class.new(user).get_debts
-  #           expect(JSON.parse(res.to_json)['debts'][0]['fileNumber']).to eq('796043735')
-  #         end
-  #       end
-  #     end
-  #   end
-  #
-  #   context 'without a valid file number' do
-  #     it 'returns a bad request error' do
-  #       VCR.use_cassette('bgs/people_service/no_person_data') do
-  #         VCR.use_cassette('debts/get_letters_empty_ssn', VCR::MATCH_EVERYTHING) do
-  #           expect(StatsD).to receive(:increment).once.with(
-  #             'api.dmc.init_debts.fail', tags: [
-  #             'error:CommonClientErrorsClientError', 'status:400'
-  #           ]
-  #           )
-  #           expect(StatsD).to receive(:increment).once.with(
-  #             'api.dmc.init_debts.total'
-  #           )
-  #           expect(Sentry).to receive(:set_tags).once.with(external_service: described_class.to_s.underscore)
-  #           expect(Sentry).to receive(:set_extras).once.with(
-  #             url: Settings.dmc.url,
-  #             message: 'the server responded with status 400',
-  #             body: { 'message' => 'Bad request' }
-  #           )
-  #           expect { described_class.new(user_no_ssn).get_debts }.to raise_error(
-  #                                                                      Common::Exceptions::BackendServiceException
-  #                                                                    ) do |e|
-  #             expect(e.message).to match(/DMC400/)
-  #           end
-  #         end
-  #       end
-  #     end
-  #   end
-  #
-  #   context 'empty DMC response' do
-  #     it 'handles an empty payload' do
-  #       VCR.use_cassette('bgs/people_service/person_data') do
-  #         VCR.use_cassette('debts/get_letters_empty_response', VCR::MATCH_EVERYTHING) do
-  #           res = described_class.new(user).get_debts
-  #           expect(JSON.parse(res.to_json)['debts']).to eq([])
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
-
   describe '#get_debt_by_id' do
     let(:user) { build(:user, :loa3) }
     let(:debt_id) { '944147b0-7ec0-4a81-ab40-a437b5ce5353' }
