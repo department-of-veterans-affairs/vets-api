31ca0523b42c48b0dd010f91000e51c0dd3d50fe
Josh Fike <josh.fike@oddball.io>
2024-07-23 12:15:11 -0500

    Refactor to reenable linting

--- modules/representation_management/lib/representation_management/v0/pdf_constructor/form_2122.rb
+++ modules/representation_management/lib/representation_management/v0/pdf_constructor/form_2122.rb
@@ -4,6 +4,9 @@ module RepresentationManagement
   module V0
     module PdfConstructor
       class Form2122 < RepresentationManagement::V0::PdfConstructor::Base
+        PAGE1_KEY = 'form1[0].#subform[0]'
+        PAGE2_KEY = 'form1[0].#subform[1]'
+
         protected
 
         def next_steps_page?
@@ -21,92 +24,128 @@ module RepresentationManagement
                           '21-22.pdf')
         end
 
-        # rubocop:disable Layout/LineLength
-        # rubocop:disable Metrics/MethodLength
         def template_options(data)
-          page1_key = 'form1[0].#subform[0]'
-          page2_key = 'form1[0].#subform[1]'
           {
-            # Page 1
-            # Section I
-            # Veteran Name
-            "#{page1_key}.VeteransLastName[0]": data.veteran_last_name,
-            "#{page1_key}.VeteransMiddleInitial1[0]": data.veteran_middle_initial,
-            "#{page1_key}.VeteransFirstName[0]": data.veteran_first_name,
-            # Veteran SSN
-            "#{page1_key}.SocialSecurityNumber_FirstThreeNumbers[0]": data.veteran_social_security_number[0..2],
-            "#{page1_key}.SocialSecurityNumber_SecondTwoNumbers[0]": data.veteran_social_security_number[3..4],
-            "#{page1_key}.SocialSecurityNumber_LastFourNumbers[0]": data.veteran_social_security_number[5..8],
-            # Veteran File Number
-            "#{page1_key}.VAFileNumber[0]": data.veteran_va_file_number,
-            # Veteran DOB
-            "#{page1_key}.DOBmonth[0]": data.veteran_date_of_birth.split('/').first,
-            "#{page1_key}.DOBday[0]": data.veteran_date_of_birth.split('/').second,
-            "#{page1_key}.DOByear[0]": data.veteran_date_of_birth.split('/').last,
-            # Veteran Service Number
-            "#{page1_key}.VeteransServiceNumber_If_Applicable[0]": data.veteran_service_number,
-            # Veteran Insurance Number
-            "#{page1_key}.InsuranceNumber_s[0]": data.veteran_insurance_numbers.join(', '),
-            # Veteran Address
-            "#{page1_key}.Claimants_MailingAddress_NumberAndStreet[1]": data.veteran_address_line1,
-            "#{page1_key}.Claimants_MailingAddress_ApartmentOrUnitNumber[1]": data.veteran_address_line2,
-            "#{page1_key}.Claimants_MailingAddress_City[1]": data.veteran_city,
-            "#{page1_key}.Claimants_MailingAddress_StateOrProvince[1]": data.veteran_state_code,
-            "#{page1_key}.Claimants_MailingAddress_Country[1]": data.veteran_country,
-            "#{page1_key}.Claimants_MailingAddress_ZIPOrPostalCode_FirstFiveNumbers[1]": data.veteran_zip_code,
-            "#{page1_key}.Claimants_MailingAddress_ZIPOrPostalCode_LastFourNumbers[1]": data.veteran_zip_code_suffix,
-            # Veteran Phone Number
-            "#{page1_key}.TelephoneNumber_IncludeAreaCode[1]": data.veteran_phone,
-            # # Veteran Email
-            "#{page1_key}.EmailAddress_Optional[0]": data.veteran_email,
-
-            # # Section II
-            # # Claimant Name
-            "#{page1_key}.Claimants_FirstName[0]": data.claimant_first_name,
-            "#{page1_key}.Claimants_MiddleInitial1[0]": data.claimant_middle_initial,
-            "#{page1_key}.Claimants_LastName[0]": data.claimant_last_name,
-            # Claimant DOB
-            "#{page1_key}.DOBmonth[1]": data.claimant_date_of_birth.split('/').first,
-            "#{page1_key}.DOBday[1]": data.claimant_date_of_birth.split('/').second,
-            "#{page1_key}.DOByear[1]": data.claimant_date_of_birth.split('/').last,
-            # Claimant Relationship
-            "#{page1_key}.Relationship_To_Veteran[0]": data.claimant_relationship,
-            # Claimant Address
-            "#{page1_key}.Claimants_MailingAddress_NumberAndStreet[0]": data.claimant_address_line1,
-            "#{page1_key}.Claimants_MailingAddress_ApartmentOrUnitNumber[0]": data.claimant_address_line2,
-            "#{page1_key}.Claimants_MailingAddress_City[0]": data.claimant_city,
-            "#{page1_key}.Claimants_MailingAddress_StateOrProvince[0]": data.claimant_state_code,
-            "#{page1_key}.Claimants_MailingAddress_Country[0]": data.claimant_country,
-            "#{page1_key}.Claimants_MailingAddress_ZIPOrPostalCode_FirstFiveNumbers[0]": data.claimant_zip_code,
-            "#{page1_key}.Claimants_MailingAddress_ZIPOrPostalCode_LastFourNumbers[0]": data.claimant_zip_code_suffix,
-            # Claimant Phone Number
-            "#{page1_key}.TelephoneNumber_IncludeAreaCode[0]": data.claimant_phone,
-            # Claimant Email
-            "#{page1_key}.Claimants_EmailAddress_Optional[0]": data.claimant_email,
-
-            # Section III
             # Service Organization Name
-            "#{page1_key}.Name_Of_Service_Organization[0]": data.organization_name,
+            "#{PAGE1_KEY}.Name_Of_Service_Organization[0]": data.organization_name
+          }.merge(veteran_identification(data))
+            .merge(veteran_contact_details(data))
+            .merge(claimant_identification(data))
+            .merge(claimant_contact_details(data))
+            .merge(page2_options(data))
+        end
 
-            # Page 2
-            # Header
-            "#{page2_key}.SocialSecurityNumber_FirstThreeNumbers[1]": data.veteran_social_security_number[0..2],
-            "#{page2_key}.SocialSecurityNumber_SecondTwoNumbers[1]": data.veteran_social_security_number[3..4],
-            "#{page2_key}.SocialSecurityNumber_LastFourNumbers[1]": data.veteran_social_security_number[5..8],
-            # # Section IV
-            # Record Consent
-            "#{page2_key}.I_Authorize[1]": data.record_consent == true ? 1 : 0,
-            # # Item 20
-            "#{page2_key}.Drug_Abuse[0]": data.consent_limits.present? && data.consent_limits.include?('DRUG_ABUSE') ? 1 : 0,
-            "#{page2_key}.Alcoholism_Or_Alcohol_Abuse[0]": data.consent_limits.present? && data.consent_limits.include?('ALCOHOLISM') ? 1 : 0,
-            "#{page2_key}.Infection_With_The_Human_Immunodeficiency_Virus_HIV[0]": data.consent_limits.present? && data.consent_limits.include?('HIV') ? 1 : 0,
-            "#{page2_key}.sicklecellanemia[0]": data.consent_limits.present? && data.consent_limits.include?('SICKLE_CELL') ? 1 : 0,
-            # Consent Address Change
-            "#{page2_key}.I_Authorize[0]": data.consent_address_change == true ? 1 : 0
+        def veteran_identification(data)
+          {
+            # Veteran Name
+            "#{PAGE1_KEY}.VeteransLastName[0]": data.veteran_last_name,
+            "#{PAGE1_KEY}.VeteransMiddleInitial1[0]": data.veteran_middle_initial,
+            "#{PAGE1_KEY}.VeteransFirstName[0]": data.veteran_first_name,
+            # Veteran SSN
+            "#{PAGE1_KEY}.SocialSecurityNumber_FirstThreeNumbers[0]": \
+            data.veteran_social_security_number[0..2],
+            "#{PAGE1_KEY}.SocialSecurityNumber_SecondTwoNumbers[0]": \
+            data.veteran_social_security_number[3..4],
+            "#{PAGE1_KEY}.SocialSecurityNumber_LastFourNumbers[0]": \
+            data.veteran_social_security_number[5..8],
+            # Veteran File Number
+            "#{PAGE1_KEY}.VAFileNumber[0]": data.veteran_va_file_number,
+            # Veteran DOB
+            "#{PAGE1_KEY}.DOBmonth[0]": data.veteran_date_of_birth.split('/').first,
+            "#{PAGE1_KEY}.DOBday[0]": data.veteran_date_of_birth.split('/').second,
+            "#{PAGE1_KEY}.DOByear[0]": data.veteran_date_of_birth.split('/').last,
+            # Veteran Service Number
+            "#{PAGE1_KEY}.VeteransServiceNumber_If_Applicable[0]": \
+            data.veteran_service_number,
+            # Veteran Insurance Number
+            "#{PAGE1_KEY}.InsuranceNumber_s[0]": data.veteran_insurance_numbers.join(', ')
           }
         end
-        # rubocop:enable Layout/LineLength
-        # rubocop:enable Metrics/MethodLength
+
+        def veteran_contact_details(data)
+          {
+            # Veteran Address
+            "#{PAGE1_KEY}.Claimants_MailingAddress_NumberAndStreet[1]": \
+            data.veteran_address_line1,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_ApartmentOrUnitNumber[1]": \
+            data.veteran_address_line2,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_City[1]": data.veteran_city,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_StateOrProvince[1]": \
+            data.veteran_state_code,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_Country[1]": data.veteran_country,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_ZIPOrPostalCode_FirstFiveNumbers[1]": \
+            data.veteran_zip_code,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_ZIPOrPostalCode_LastFourNumbers[1]": \
+            data.veteran_zip_code_suffix,
+            # Veteran Phone Number
+            "#{PAGE1_KEY}.TelephoneNumber_IncludeAreaCode[1]": data.veteran_phone,
+            # # Veteran Email
+            "#{PAGE1_KEY}.EmailAddress_Optional[0]": data.veteran_email
+          }
+        end
+
+        def claimant_identification(data)
+          {
+            # # Claimant Name
+            "#{PAGE1_KEY}.Claimants_FirstName[0]": data.claimant_first_name,
+            "#{PAGE1_KEY}.Claimants_MiddleInitial1[0]": data.claimant_middle_initial,
+            "#{PAGE1_KEY}.Claimants_LastName[0]": data.claimant_last_name,
+            # Claimant DOB
+            "#{PAGE1_KEY}.DOBmonth[1]": data.claimant_date_of_birth.split('/').first,
+            "#{PAGE1_KEY}.DOBday[1]": data.claimant_date_of_birth.split('/').second,
+            "#{PAGE1_KEY}.DOByear[1]": data.claimant_date_of_birth.split('/').last,
+            # Claimant Relationship
+            "#{PAGE1_KEY}.Relationship_To_Veteran[0]": data.claimant_relationship
+          }
+        end
+
+        def claimant_contact_details(data)
+          {
+            # Claimant Address
+            "#{PAGE1_KEY}.Claimants_MailingAddress_NumberAndStreet[0]": \
+            data.claimant_address_line1,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_ApartmentOrUnitNumber[0]": \
+            data.claimant_address_line2,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_City[0]": data.claimant_city,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_StateOrProvince[0]": \
+            data.claimant_state_code,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_Country[0]": data.claimant_country,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_ZIPOrPostalCode_FirstFiveNumbers[0]": \
+            data.claimant_zip_code,
+            "#{PAGE1_KEY}.Claimants_MailingAddress_ZIPOrPostalCode_LastFourNumbers[0]": \
+            data.claimant_zip_code_suffix,
+            # Claimant Phone Number
+            "#{PAGE1_KEY}.TelephoneNumber_IncludeAreaCode[0]": data.claimant_phone,
+            # Claimant Email
+            "#{PAGE1_KEY}.Claimants_EmailAddress_Optional[0]": data.claimant_email
+          }
+        end
+
+        def page2_options(data)
+          {
+            # Header
+            "#{PAGE2_KEY}.SocialSecurityNumber_FirstThreeNumbers[1]": \
+            data.veteran_social_security_number[0..2],
+            "#{PAGE2_KEY}.SocialSecurityNumber_SecondTwoNumbers[1]": \
+            data.veteran_social_security_number[3..4],
+            "#{PAGE2_KEY}.SocialSecurityNumber_LastFourNumbers[1]": \
+            data.veteran_social_security_number[5..8],
+            # # Section IV
+            # Record Consent
+            "#{PAGE2_KEY}.I_Authorize[1]": data.record_consent == true ? 1 : 0,
+            # # Item 20
+            "#{PAGE2_KEY}.Drug_Abuse[0]": \
+            data.consent_limits.present? && data.consent_limits.include?('DRUG_ABUSE') ? 1 : 0,
+            "#{PAGE2_KEY}.Alcoholism_Or_Alcohol_Abuse[0]": \
+            data.consent_limits.present? && data.consent_limits.include?('ALCOHOLISM') ? 1 : 0,
+            "#{PAGE2_KEY}.Infection_With_The_Human_Immunodeficiency_Virus_HIV[0]": \
+            data.consent_limits.present? && data.consent_limits.include?('HIV') ? 1 : 0,
+            "#{PAGE2_KEY}.sicklecellanemia[0]": \
+            data.consent_limits.present? && data.consent_limits.include?('SICKLE_CELL') ? 1 : 0,
+            # Consent Address Change
+            "#{PAGE2_KEY}.I_Authorize[0]": data.consent_address_change == true ? 1 : 0
+          }
+        end
       end
     end
   end
