594f18af6966191e6c955520ab6099c027161043
Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>
2024-07-24 11:37:02 -0400

    Revert "[DBX-88811] treat LH BI success state as true success (#17621)"
    This reverts commit f46d201b5482376c372880a5d35abf27729c3dde.

--- spec/models/form526_submission_spec.rb
+++ spec/models/form526_submission_spec.rb
@@ -88,14 +88,6 @@ RSpec.describe Form526Submission do
       end
     end
 
-    context 'when backup_submitted_claim_status is paranoid_success' do
-      let(:backup_submitted_claim_status) { 'paranoid_success' }
-
-      it 'is valid' do
-        expect(subject).to be_valid
-      end
-    end
-
     context 'when backup_submitted_claim_status is neither accepted, rejected nor nil' do
       it 'is invalid' do
         expect do
@@ -116,28 +108,6 @@ RSpec.describe Form526Submission do
     let!(:remediated_and_expired) { create(:form526_submission, :remediated, :created_more_than_3_days_ago) }
     let!(:remediated_and_rejected) { create(:form526_submission, :remediated, :backup_path, :backup_rejected) }
     let!(:no_longer_remediated) { create(:form526_submission, :no_longer_remediated) }
-    let!(:paranoid_success) { create(:form526_submission, :backup_path, :paranoid_success) }
-    let!(:success_by_age) do
-      Timecop.freeze((1.year + 1.day).ago) do
-        create(:form526_submission, :backup_path, :paranoid_success)
-      end
-    end
-
-    describe 'paranoid_success_type' do
-      it 'returns records less than a year old with paranoid_success backup status' do
-        expect(Form526Submission.paranoid_success_type).to contain_exactly(
-          paranoid_success
-        )
-      end
-    end
-
-    describe 'success_by_age_type' do
-      it 'returns records more than a year old with paranoid_success backup status' do
-        expect(Form526Submission.success_by_age_type).to contain_exactly(
-          success_by_age
-        )
-      end
-    end
 
     describe 'pending_backup_submissions' do
       it 'returns records submitted to the backup path but lacking a decisive state' do
@@ -198,9 +168,7 @@ RSpec.describe Form526Submission do
           remediated_and_expired,
           remediated_and_rejected,
           happy_path_success,
-          accepted_backup,
-          paranoid_success,
-          success_by_age
+          accepted_backup
         )
       end
     end
