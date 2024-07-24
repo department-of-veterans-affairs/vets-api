5955a670e77bc0629b60f7d31c06851cb1dec85e
Tyler Fink <tfink419@gmail.com>
2024-07-24 17:24:44 -0400

    Add tests and get working

--- app/serializers/dependents_serializer.rb
+++ app/serializers/dependents_serializer.rb
@@ -1,54 +1,33 @@
 # frozen_string_literal: true
 
-class DependentsSerializer
-  include JSONAPI::Serializer
-  set_id { '' }
-  set_type :dependents
-
-  attribute :persons do |object|
-    arr = object[:persons].instance_of?(Hash) ? [object[:persons]] : object[:persons]
-
-    next arr if dependency_decisions.blank?
-
-    arr.each do |person|
-      upcoming_removal = person[:upcoming_removal] = upcoming_removals[person[:ptcpnt_id]]
-      if upcoming_removal
-        person[:upcoming_removal_date] = upcoming_removal[:award_effective_date]
-        person[:upcoming_removal_reason] = upcoming_removal[:dependency_status_type_description].gsub(/\s+/, ' ')
-      end
-
-      person[:dependent_benefit_type] = dependent_benefit_types[person[:ptcpnt_id]]
-    end
-  end
-
+module DependentsHelper
   private
 
-  def upcoming_removals
-    @upcoming_removals ||= current_and_pending_decisions.transform_values do |decs|
+  def upcoming_removals(decisions)
+    decisions.transform_values do |decs|
       decs.filter { |dec| %w[T18 SCHATTT].include?(dec[:dependency_decision_type]) }
           .max { |a, b| a[:award_effective_date] <=> b[:award_effective_date] }
     end
   end
 
-  def dependent_benefit_types
-    @dependent_benefit_types ||= current_and_pending_decisions.transform_values do |decisions|
-      dec = decisions.find { |d| %w[EMC SCHATTB].include?(d[:dependency_decision_type]) }
-      dec[:dependency_status_type_description].gsub(/\s+/, ' ')
+  def dependent_benefit_types(decisions)
+    decisions.transform_values do |decs|
+      dec = decs.find { |d| %w[EMC SCHATTB].include?(d[:dependency_decision_type]) }
+      dec && dec[:dependency_status_type_description]&.gsub(/\s+/, ' ')
     end
   end
 
-  def current_and_pending_decisions
-    return @current_and_pending_decisions if @current_and_pending_decisions.present?
-
+  def current_and_pending_decisions(diaries)
     # Filter by eligible minor child or school attendance types and if they are the current or future decisions
-    decisions = dependency_decisions
+    decisions = dependency_decisions(diaries)
                 .filter do |dec|
-      (%w[EMC SCHATTB].include?(dec[:dependency_decision_type]) && dec[:award_effective_date] < Time.zone.now) ||
-        (%w[T18 SCHATTT].include?(dec[:dependency_decision_type]) && dec[:award_effective_date] > Time.zone.now)
+      date = Time.zone.parse(dec[:award_effective_date])
+      (%w[EMC SCHATTB].include?(dec[:dependency_decision_type]) && date < Time.zone.now) ||
+        (%w[T18 SCHATTT].include?(dec[:dependency_decision_type]) && date > Time.zone.now)
     end
 
-    @current_and_pending_decisions = decisions.group_by { |dec| dec[:person_id] }
-                                              .transform_values do |decs|
+    decisions.group_by { |dec| dec[:person_id] }
+             .transform_values do |decs|
       # get only most recent active decision and add back to array
       most_recent =
         decs.filter { |dec| %w[EMC SCHATTB].include?(dec[:dependency_decision_type]) }
@@ -56,14 +35,41 @@ class DependentsSerializer
       # include future school attendance
       (decs.filter { |dec|
         %w[T18 SCHATTB SCHATTT].include?(dec[:dependency_decision_type])
-      } + [most_recent]).uniq
+      } + [most_recent]).compact
     end
   end
 
-  def dependency_decisions
-    decisions = object.dig(:diaries, :dependency_decs)
+  def dependency_decisions(diaries)
+    decisions = diaries && diaries[:dependency_decs]
     return if decisions.nil?
 
     decisions.is_a?(Hash) ? [decisions] : decisions
+  end
+end
+
+class DependentsSerializer
+  include JSONAPI::Serializer
+  extend DependentsHelper
+
+  set_id { '' }
+  set_type :dependents
+
+  attribute :persons do |object|
+    arr = object[:persons].instance_of?(Hash) ? [object[:persons]] : object[:persons]
+    diaries = object[:diaries]
+
+    next arr if dependency_decisions(diaries).blank?
+
+    decisions = current_and_pending_decisions(diaries)
+
+    arr.each do |person|
+      upcoming_removal = person[:upcoming_removal] = upcoming_removals(decisions)[person[:ptcpnt_id]]
+      if upcoming_removal
+        person[:upcoming_removal_date] = Time.zone.parse(upcoming_removal[:award_effective_date])
+        person[:upcoming_removal_reason] = upcoming_removal[:dependency_decision_type_description].gsub(/\s+/, ' ')
+      end
+
+      person[:dependent_benefit_type] = dependent_benefit_types(decisions)[person[:ptcpnt_id]]
+    end
   end
 end
