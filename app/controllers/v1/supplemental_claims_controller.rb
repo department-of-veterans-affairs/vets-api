173ccf10605107ba3cc95d091eff6b1d7bacb5d8
Derek Fong <151783381+dfong-adh@users.noreply.github.com>
2024-07-26 12:28:51 -0700

    Changes per code review

--- app/controllers/v1/supplemental_claims_controller.rb
+++ app/controllers/v1/supplemental_claims_controller.rb
@@ -42,6 +42,8 @@ module V1
     end
 
     def handle_4142(request_body:, form4142:, appeal_submission_id:, submitted_appeal_uuid:)
+      return if form4142.blank?
+
       rejiggered_payload = get_and_rejigger_required_info(request_body:, form4142:, user: @current_user)
       jid = decision_review_service.queue_form4142(appeal_submission_id:, rejiggered_payload:, submitted_appeal_uuid:)
       log_form4142_job_queued(appeal_submission_id, submitted_appeal_uuid, jid)
@@ -87,35 +89,34 @@ module V1
       raise
     end
 
-    def process_submission # rubocop:disable Metrics/MethodLength
+    def process_submission
       req_body_obj = request_body_hash.is_a?(String) ? JSON.parse(request_body_hash) : request_body_hash
       saved_claim_request_body = req_body_obj.to_json # serialize before request body is modified
-      uploaded_forms = []
       sc_evidence = req_body_obj.delete('additionalDocuments')
       form4142 = req_body_obj.delete('form4142')
       zip_from_frontend = req_body_obj.dig('data', 'attributes', 'veteran', 'address', 'zipCode5')
+
       sc_response = decision_review_service.create_supplemental_claim(request_body: req_body_obj, user: @current_user)
       submitted_appeal_uuid = sc_response.body.dig('data', 'id')
-      unless submitted_appeal_uuid.nil?
-        ActiveRecord::Base.transaction do
-          appeal_submission_id = create_appeal_submission(submitted_appeal_uuid, zip_from_frontend)
+      return if submitted_appeal_uuid.nil?
 
-          ::Rails.logger.info(post_create_log_msg(appeal_submission_id:, submitted_appeal_uuid:))
-          if form4142.present?
-            handle_4142(request_body: req_body_obj, form4142:, appeal_submission_id:, submitted_appeal_uuid:)
-            uploaded_forms << '21-4142'
-          end
-          submit_evidence(sc_evidence, appeal_submission_id, submitted_appeal_uuid) if sc_evidence.present?
+      ActiveRecord::Base.transaction do
+        appeal_submission_id = create_appeal_submission(submitted_appeal_uuid, zip_from_frontend)
 
-          store_saved_claim(claim_class: SavedClaim::SupplementalClaim, form: saved_claim_request_body,
-                            guid: submitted_appeal_uuid, uploaded_forms:)
+        ::Rails.logger.info(post_create_log_msg(appeal_submission_id:, submitted_appeal_uuid:))
+        handle_4142(request_body: req_body_obj, form4142:, appeal_submission_id:, submitted_appeal_uuid:)
+        submit_evidence(sc_evidence, appeal_submission_id, submitted_appeal_uuid) if sc_evidence.present?
 
-          # Only destroy InProgressForm after evidence upload step
-          # so that we still have references if a fatal error occurs before this step
-          clear_in_progress_form
-        end
-        render json: sc_response.body, status: sc_response.status
+        uploaded_forms = []
+        uploaded_forms << '21-4142' if form4142.present?
+        store_saved_claim(claim_class: SavedClaim::SupplementalClaim, form: saved_claim_request_body,
+                          guid: submitted_appeal_uuid, uploaded_forms:)
+
+        # Only destroy InProgressForm after evidence upload step
+        # so that we still have references if a fatal error occurs before this step
+        clear_in_progress_form
       end
+      render json: sc_response.body, status: sc_response.status
     end
 
     def create_appeal_submission(submitted_appeal_uuid, backup_zip)
