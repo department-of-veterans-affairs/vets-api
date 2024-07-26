4c006579a6f2ad035bcb8d773bf4f9f5b63c8c80
Eli Selkin <eli.j.selkin@gmail.com>
2024-07-26 14:44:11 -0700

    test

---
+++ modules/facilities_api/spec/models/facilities_api/v2/lighthouse/service_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe FacilitiesApi::V2::Lighthouse::Service, team: :facilities, type: :model do
+  context 'Creating' do
+    let(:attributes) do
+      {
+        serviceInfo: { name: 'Audiology and speech', serviceId: 'audiology', serviceType: 'health' },
+        waitTime: { new: 0.2, established: 0.4, effectiveDate: '12-12-1222' }
+      }
+    end
+
+    it 'has object defaults' do
+      service = FacilitiesApi::V2::Lighthouse::Service.new(attributes)
+      expect(service.attributes).to match(
+        {
+          service: 'Audiology and speech',
+          serviceId: 'audiology',
+          serviceType: 'health',
+          new: nil,
+          established: nil,
+          effectiveDate: ''
+        }
+      )
+    end
+  end
+end
