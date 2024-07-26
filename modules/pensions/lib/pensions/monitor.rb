2e23c50ac22cdd00734ede2cca744f38ea5fec29
Wayne Weibel <wayne.weibel@adhocteam.us>
2024-07-26 16:44:59 -0400

    monitor doc; updated rake

--- modules/pensions/lib/pensions/monitor.rb
+++ modules/pensions/lib/pensions/monitor.rb
@@ -3,6 +3,7 @@
 module Pensions
   ##
   # Monitor functions for Rails logging and StatsD
+  # @todo abstract, split logging for controller and sidekiq
   #
   class Monitor
     # statsd key for api
@@ -11,22 +12,54 @@ module Pensions
     # statsd key for sidekiq
     SUBMISSION_STATS_KEY = 'worker.lighthouse.pension_benefit_intake_job'
 
+    ##
+    # log GET 404 from controller
+    # @see PensionClaimsController
+    #
+    # @param confirmation_number [UUID] saved_claim guid
+    # @param current_user [User]
+    # @param e [ActiveRecord::RecordNotFound]
+    #
     def track_show404(confirmation_number, current_user, e)
       Rails.logger.error('21P-527EZ submission not found',
                          { confirmation_number:, user_uuid: current_user&.uuid, message: e&.message })
     end
 
+    ##
+    # log GET 500 from controller
+    # @see PensionClaimsController
+    #
+    # @param confirmation_number [UUID] saved_claim guid
+    # @param current_user [User]
+    # @param e [Error]
+    #
     def track_show_error(confirmation_number, current_user, e)
       Rails.logger.error('21P-527EZ fetching submission failed',
                          { confirmation_number:, user_uuid: current_user&.uuid, message: e&.message })
     end
 
+    ##
+    # log POST processing started
+    # @see PensionClaimsController
+    #
+    # @param claim [Pension::SavedClaim]
+    # @param current_user [User]
+    #
     def track_create_attempt(claim, current_user)
       StatsD.increment("#{CLAIM_STATS_KEY}.attempt")
       Rails.logger.info('21P-527EZ submission to Sidekiq begun',
                         { confirmation_number: claim&.confirmation_number, user_uuid: current_user&.uuid })
     end
 
+    ##
+    # log POST processing failure
+    # @see PensionClaimsController
+    #
+    # @param in_progress_form [InProgressForm]
+    # @param claim [Pension::SavedClaim]
+    # @param current_user [User]
+    # @param e [Error]
+    #
     def track_create_error(in_progress_form, claim, current_user, e = nil)
       StatsD.increment("#{CLAIM_STATS_KEY}.failure")
       Rails.logger.error('21P-527EZ submission to Sidekiq failed',
@@ -35,6 +68,14 @@ module Pensions
                            message: e&.message })
     end
 
+    ##
+    # log POST processing success
+    # @see PensionClaimsController
+    #
+    # @param in_progress_form [InProgressForm]
+    # @param claim [Pension::SavedClaim]
+    # @param current_user [User]
+    #
     def track_create_success(in_progress_form, claim, current_user)
       StatsD.increment("#{CLAIM_STATS_KEY}.success")
       if claim.form_start_date
@@ -50,6 +91,14 @@ module Pensions
       Rails.logger.info('21P-527EZ submission to Sidekiq success', context)
     end
 
+    ##
+    # log Sidkiq job started
+    # @see PensionBenefitIntakeJob
+    #
+    # @param claim [Pension::SavedClaim]
+    # @param lighthouse_service [BenefitsIntake::Service]
+    # @param user_uuid [UUID]
+    #
     def track_submission_begun(claim, lighthouse_service, user_uuid)
       StatsD.increment("#{SUBMISSION_STATS_KEY}.begun")
       Rails.logger.info('Lighthouse::PensionBenefitIntakeJob submission to LH begun',
@@ -61,6 +110,15 @@ module Pensions
                         })
     end
 
+    ##
+    # log Sidkiq job Lighthouse submission attempted
+    # @see PensionBenefitIntakeJob
+    #
+    # @param claim [Pension::SavedClaim]
+    # @param lighthouse_service [BenefitsIntake::Service]
+    # @param user_uuid [UUID]
+    # @param payload [Hash] lighthouse upload data
+    #
     def track_submission_attempted(claim, lighthouse_service, user_uuid, payload)
       StatsD.increment("#{SUBMISSION_STATS_KEY}.attempt")
       Rails.logger.info('Lighthouse::PensionBenefitIntakeJob submission to LH attempted', {
@@ -73,6 +131,14 @@ module Pensions
                         })
     end
 
+    ##
+    # log Sidkiq job completed
+    # @see PensionBenefitIntakeJob
+    #
+    # @param claim [Pension::SavedClaim]
+    # @param lighthouse_service [BenefitsIntake::Service]
+    # @param user_uuid [UUID]
+    #
     def track_submission_success(claim, lighthouse_service, user_uuid)
       StatsD.increment("#{SUBMISSION_STATS_KEY}.success")
       Rails.logger.info('Lighthouse::PensionBenefitIntakeJob submission to LH succeeded', {
@@ -83,6 +149,15 @@ module Pensions
                         })
     end
 
+    ##
+    # log Sidkiq job failed, automatic retry
+    # @see PensionBenefitIntakeJob
+    #
+    # @param claim [Pension::SavedClaim]
+    # @param lighthouse_service [BenefitsIntake::Service]
+    # @param user_uuid [UUID]
+    # @param e [Error]
+    #
     def track_submission_retry(claim, lighthouse_service, user_uuid, e)
       StatsD.increment("#{SUBMISSION_STATS_KEY}.failure")
       Rails.logger.warn('Lighthouse::PensionBenefitIntakeJob submission to LH failed, retrying', {
@@ -94,6 +169,13 @@ module Pensions
                         })
     end
 
+    ##
+    # log Sidkiq job exhaustion, complete failure after all retries
+    # @see PensionBenefitIntakeJob
+    #
+    # @param msg [Hash] sidekiq exhaustion response
+    # @param claim [Pension::SavedClaim]
+    #
     def track_submission_exhaustion(msg, claim = nil)
       StatsD.increment("#{SUBMISSION_STATS_KEY}.exhausted")
       Rails.logger.error('Lighthouse::PensionBenefitIntakeJob submission to LH exhausted!', {
@@ -104,6 +186,15 @@ module Pensions
                          })
     end
 
+    ##
+    # log Sidkiq job cleanup error occurred, this can occur post success or failure
+    # @see PensionBenefitIntakeJob
+    #
+    # @param claim [Pension::SavedClaim]
+    # @param lighthouse_service [BenefitsIntake::Service]
+    # @param user_uuid [UUID]
+    # @param e [Error]
+    #
     def track_file_cleanup_error(claim, lighthouse_service, user_uuid, e)
       Rails.logger.error('Lighthouse::PensionBenefitIntakeJob cleanup failed',
                          {
