781f30eabd51d78be8147c4c3fb18c3dab0fc137
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-07-26 10:18:23 -0400

    add cop for ActiveModelSerializer

---
+++ lib/rubocop/cops/ams_serializer.rb
@@ -0,0 +1,23 @@
+module RuboCop
+  module Cop
+    class AmsSerializer < RuboCop::Cop::Base
+
+      MSG = 'Use JSONAPI::Serializer instead of ActiveModel::Serializer'.freeze
+
+      def_node_matcher :active_model_serializer?, <<~PATTERN
+        (class
+          (const _ _)
+          {
+            (const (const {nil? (cbase)} :ActiveModel) :Serializer)
+            (const (const (const {nil? (cbase)} :ActiveModel) :Serializer) :CollectionSerializer)
+          }
+          ...
+        )
+      PATTERN
+
+      def on_class(node)
+        add_offense(node.children[1]) if active_model_serializer?(node)
+      end
+    end
+  end
+end
