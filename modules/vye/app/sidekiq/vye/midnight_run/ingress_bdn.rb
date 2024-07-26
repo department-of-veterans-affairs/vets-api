3cffd5ee1283fff7fdf704b34b4f6c04d8458043
Vanson Samuel <37637+binq@users.noreply.github.com>
2024-07-26 15:22:51 -0500

    added StatsD instrumentation (#17723)
    * - added some StatsD instrumentation to find out why there are so many 403s
    - removed retries on sidekiq jobs
    - added ENDOFFILE marker to WAVE.txt
    
    * no retries right now

--- modules/vye/app/sidekiq/vye/midnight_run/ingress_bdn.rb
+++ modules/vye/app/sidekiq/vye/midnight_run/ingress_bdn.rb
@@ -4,7 +4,7 @@ module Vye
   class MidnightRun
     class IngressBdn
       include Sidekiq::Job
-      sidekiq_options retry: 8, unique_for: 12.hours
+      sidekiq_options retry: 0
 
       def perform
         Vye::BatchTransfer::IngressFiles.bdn_load
