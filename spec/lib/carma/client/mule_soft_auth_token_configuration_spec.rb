eb040d8c686656589f285f9eb100dd26b913dfcb
Brandon Cooper <brandon.cooper@adhocteam.us>
2024-07-29 11:14:34 -0400

    refactor specs to make more readable

--- spec/lib/carma/client/mule_soft_auth_token_configuration_spec.rb
+++ spec/lib/carma/client/mule_soft_auth_token_configuration_spec.rb
@@ -12,32 +12,23 @@ describe CARMA::Client::MuleSoftAuthTokenConfiguration do
     allow(Settings.form_10_10cg.carma.mulesoft.auth).to receive(:token_url).and_return(token_url)
   end
 
-  describe 'connection' do
-    subject { super().connection }
-
-    it 'sets url prefix' do
-      expect(subject.url_prefix.to_s).to eq("#{token_url}/")
-    end
+  it 'sets base_path on connection' do
+    expect(Faraday).to receive(:new).with("#{token_url}/")
+    subject.connection
   end
 
-  describe 'service_name' do
-    subject { super().service_name }
-
-    it 'returns class name' do
-      expect(subject).to eq('CARMA::Client::MuleSoftAuthTokenConfiguration')
-    end
+  it 'returns class name' do
+    expect(subject.service_name).to eq('CARMA::Client::MuleSoftAuthTokenConfiguration')
   end
 
   describe 'timeout' do
-    subject { super().timeout }
-
     context 'has a configured value' do
       before do
         allow(Settings.form_10_10cg.carma.mulesoft.auth).to receive(:timeout).and_return(23)
       end
 
       it 'returns the configured value' do
-        expect(subject).to eq(23)
+        expect(subject.timeout).to eq(23)
       end
     end
 
@@ -47,7 +38,7 @@ describe CARMA::Client::MuleSoftAuthTokenConfiguration do
       end
 
       it 'returns the default value' do
-        expect(subject).to eq(30)
+        expect(subject.timeout).to eq(30)
       end
     end
   end
@@ -61,14 +52,6 @@ describe CARMA::Client::MuleSoftAuthTokenConfiguration do
 
     it 'returns expected settings path' do
       expect(subject.settings).to eq(expected_settings)
-    end
-  end
-
-  describe 'base_path' do
-    subject { super().send(:base_path) }
-
-    it 'returns the correct value' do
-      expect(subject).to eq("#{token_url}/")
     end
   end
 end
