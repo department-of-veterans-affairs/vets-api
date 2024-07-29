9b7097d0931cb622d29f77af017b98a9cbd5a4ec
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-07-29 15:55:03 -0500

    reverts Gemfile.lock update and removes comment from blueprinter

--- modules/claims_api/app/controllers/claims_api/v2/blueprints/auto_established_claim_blueprint.rb
+++ modules/claims_api/app/controllers/claims_api/v2/blueprints/auto_established_claim_blueprint.rb
@@ -4,6 +4,7 @@ module ClaimsApi
   module V2
     module Blueprints
       class AutoEstablishedClaimBlueprint < Blueprinter::Base
+
         identifier :id
         field :type do |_options|
           'forms/526'
@@ -16,10 +17,6 @@ module ClaimsApi
             claim&.form_data
           end
         end
-
-        # field :meta, if: ->(_field_name, claim, options) { claim.transaction_id != nil } do |claim, options|
-        #   { transaction_id: claim.transaction_id}
-        # end
       end
     end
   end
