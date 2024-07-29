8a655c014c35481ee3c012cc162fc29bc775794a
Tyler Coleman <tyler.coleman@oddball.io>
2024-07-29 14:36:02 -0700

    Add `doc_type`
    Support form number 2122A. If the form number is 2122A, send doc_type
    of L075 to BD. Otherwise (i.e., the form number is 2122), send doc_type
    of L190.

--- modules/claims_api/app/sidekiq/claims_api/v2/poa_form_builder_job.rb
+++ modules/claims_api/app/sidekiq/claims_api/v2/poa_form_builder_job.rb
@@ -23,7 +23,8 @@ module ClaimsApi
         output_path = pdf_constructor(form_number).construct(data(power_of_attorney, form_number, rep),
                                                              id: power_of_attorney.id)
         if Flipper.enabled?(:lighthouse_claims_api_poa_use_bd)
-          benefits_doc_api.upload(claim: power_of_attorney, pdf_path: output_path)
+          doc_type = form_number == '2122A' ? 'L075' : 'L190'
+          benefits_doc_api.upload(claim: power_of_attorney, pdf_path: output_path, doc_type:)
         else
           upload_to_vbms(power_of_attorney, output_path)
         end
