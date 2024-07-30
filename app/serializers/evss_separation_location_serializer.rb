26578f9b1926e279fac652e4d956a5a7404a9fd3
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-07-30 14:37:23 -0400

    use custom serializer for EVSSSeparationLocationSerializer

--- app/serializers/evss_separation_location_serializer.rb
+++ app/serializers/evss_separation_location_serializer.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
-# NOT USING JSON:API Specification
-# app/controllers/v0/disability_compensation_forms_controller.rb
-class EVSSSeparationLocationSerializer < ActiveModel::Serializer
-  attribute :separation_locations
+class EVSSSeparationLocationSerializer
 
-  def id
-    nil
+  def initialize(resource)
+    @resource = resource
+  end
+
+  def to_json(options = {})
+    Oj.dump(serializable_hash, mode: :compat, time_format: :ruby)
+  end
+
+  def serializable_hash
+    {
+      status: @resource.status,
+      separation_locations: @resource.separation_locations
+    }
   end
 end
