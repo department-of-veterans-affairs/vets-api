c9d931bc605448e4928b4a895f569278c8ba3143
RachalCassity <rachal.cassity@oddball.io>
2024-07-30 11:48:15 -0500

    invalide spec

--- spec/models/va_profile_redis/cache_spec.rb
+++ spec/models/va_profile_redis/cache_spec.rb
@@ -8,6 +8,7 @@ describe VAProfileRedis::Cache, :skip_vet360 do
   before do
     allow(user).to receive(:vet360_id).and_return('1')
     allow(VAProfile::Configuration::SETTINGS.contact_information).to receive(:cache_enabled).and_return(true)
+    allow(VAProfile::Configuration::SETTINGS.profile_information).to receive(:cache_enabled).and_return(true)
   end
 
   describe '.invalidate' do
@@ -23,6 +24,47 @@ describe VAProfileRedis::Cache, :skip_vet360 do
         VAProfileRedis::Cache.invalidate(user)
 
         expect(VAProfileRedis::ContactInformation.exists?(user.uuid)).to eq(false)
+      end
+    end
+
+    context 'when user.vet360_contact_info is present for profile information' do
+      let(:contact_info) { VAProfileRedis::ProfileInformation.for_user(user) }
+
+      it 'invalidates the va-profile-profile-info-response cache' do
+        VCR.use_cassette('va_profile/profile_information/person_full', VCR::MATCH_EVERYTHING) do
+          contact_info
+        end
+        expect(VAProfileRedis::ProfileInformation.exists?(user.uuid)).to eq(true)
+
+        VAProfileRedis::Cache.invalidate(user)
+
+        expect(VAProfileRedis::ProfileInformation.exists?(user.uuid)).to eq(false)
+      end
+    end
+
+    context 'when user.vet360_contact_info is nil' do
+      it 'does not call #destroy' do
+        expect_any_instance_of(Common::RedisStore).not_to receive(:destroy)
+
+        VAProfileRedis::Cache.invalidate(user)
+      end
+    end
+  end
+
+  describe '.invalidate profile information' do
+    context 'when user.vet360_contact_info is present for contact information' do
+
+      let(:contact_info) { VAProfileRedis::ProfileInformation.for_user(user) }
+
+      it 'invalidates the va-profile-contact-info-response cache' do
+        VCR.use_cassette('va_profile/contact_information/person_full', VCR::MATCH_EVERYTHING) do
+          contact_info
+        end
+        expect(VAProfileRedis::ProfileInformation.exists?(user.uuid)).to eq(true)
+
+        VAProfileRedis::Cache.invalidate(user)
+
+        expect(VAProfileRedis::ProfileInformation.exists?(user.uuid)).to eq(false)
       end
     end
 
