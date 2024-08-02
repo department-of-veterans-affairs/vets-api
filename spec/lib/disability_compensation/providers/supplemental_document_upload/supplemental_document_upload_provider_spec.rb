e3e338fe5f13ba641f22b370caf00efc53d5a450
Nathan Burgess <burgess.nathan28@gmail.com>
2024-08-02 11:10:55 -0400

    Implement base SupplementalDocumentUploadProvider module
    Implements abstract base module for Document Upload Provider services in line with the same pattern used for other providers such as lib/disability_compensation/providers/generate_pdf/generate_pdf_provider.rb. This is meant to define the API of the including classes which will implement the actual behavior for use with the EVSS and Lighthouse APIs

---
+++ spec/lib/disability_compensation/providers/supplemental_document_upload/supplemental_document_upload_provider_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'disability_compensation/providers/supplemental_document_upload/supplemental_document_upload_provider'
+
+RSpec.describe SupplementalDocumentUploadProvider do
+  it 'raises an error if the generate_upload_document method is not implemented' do
+    expect do
+      subject.generate_upload_document(Faker::File.file_name, 'L023')
+    end.to raise_error NotImplementedError
+  end
+
+  it 'raises an error if the validate_upload_document method is not implemented' do
+    expect do
+      subject.validate_upload_document(LighthouseDocument.new)
+    end.to raise_error NotImplementedError
+  end
+
+  it 'raises an error if the submit_upload_document method is not implemented' do
+    expect do
+      subject.submit_upload_document(LighthouseDocument.new)
+    end.to raise_error NotImplementedError
+  end
+end
