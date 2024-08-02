9bc2353c005cd41200f19696dc431bd117ff129b
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-08-02 10:01:58 -0400

    try better demo

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,7 +1,13 @@
-# Smelly class
-class Smelly
-  # This will reek of UncommunicativeMethodName
-  def x
-    y = 10 # This will reek of UncommunicativeVariableName
+# frozen_string_literal: true
+
+class ShoppingCart < ActiveRecord::Base
+  has_many :items
+
+  def gross_price
+    items.sum { |item| item.net + item.tax }
   end
+end
+
+class Item < ActiveRecord::Base
+  belongs_to :shopping_cart
 end
