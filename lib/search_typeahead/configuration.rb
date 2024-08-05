dcd0dc207b565def5fe20ce9f2af53e6bd8c797c
Eric Boehs <eric@boehs.com>
2024-08-05 14:21:42 -0500

    fix tests

--- lib/search_typeahead/configuration.rb
+++ lib/search_typeahead/configuration.rb
@@ -4,22 +4,12 @@ require 'common/client/configuration/rest'
 
 module SearchTypeahead
   class Configuration < Common::Client::Configuration::REST
-    self.read_timeout = 2
-    self.open_timeout = 2
-
     def base_path
       "#{Settings.search_typeahead.url}/suggestions"
     end
 
     def service_name
       'SearchTypeahead'
-    end
-
-    def connection
-      Faraday.new(base_path, headers: base_request_headers, request: request_options) do |conn|
-        conn.use :breakers
-        conn.adapter Faraday.default_adapter
-      end
     end
   end
 end
