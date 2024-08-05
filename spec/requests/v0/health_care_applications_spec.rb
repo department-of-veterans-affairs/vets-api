1607fecc266628e3f57790729b0170d605b45c39
Mitch Saltykov <mitch.saltykov@agile6.com>
2024-08-05 10:07:46 -0400

    Invert default behavior - filter without a flag

--- spec/requests/v0/health_care_applications_spec.rb
+++ spec/requests/v0/health_care_applications_spec.rb
@@ -256,7 +256,7 @@ RSpec.describe 'V0::HealthCareApplications', type: %i[request serializer] do
 
     it 'filters out facilities not yet supported downstream' do
       VCR.use_cassette('lighthouse/facilities/v1/200_facilities_facility_ids', match_requests_on: %i[method uri]) do
-        get(facilities_v0_health_care_applications_path(facilityIds: %w[vha_757 vha_358], ves_active: true))
+        get(facilities_v0_health_care_applications_path(facilityIds: %w[vha_757 vha_358]))
       end
       expect(response).to have_http_status(:ok)
       expect(response.parsed_body[0]).to be_nil
