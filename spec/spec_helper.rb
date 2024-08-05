d1e31e8850c641a975ea038af84c18583c0d7d26
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-08-05 14:29:01 -0400

    remove controller group

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ unless ENV['NOCOVERAGE']
   SimpleCov.start 'rails' do
     track_files '**/{app,lib}/**/*.rb'
 
+    add_filter 'app/controllers'
     add_filter 'app/controllers/concerns/accountable.rb'
     add_filter 'app/models/in_progress_disability_compensation_form.rb'
     add_filter 'lib/apps/configuration.rb'
