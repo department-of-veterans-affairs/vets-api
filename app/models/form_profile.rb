5f1735f2ecfca2626f904fdd095526e224e4aa33
Belle P <belle@Belle-Poopongpanit.local>
2024-08-05 14:53:22 -0700

    Addressed preliminary PR comments

--- app/models/form_profile.rb
+++ app/models/form_profile.rb
@@ -421,11 +421,11 @@ class FormProfile
   end
 
   # def clean_hash!(hash)
-  #   # hash.deep_transform_keys! { |k| k.camelize(:lower) }
-  #   # hash.each { |k, v| hash[k] = clean!(v) }
-  #   # hash.delete_if { |_k, v| v.blank? }
+  #   hash.deep_transform_keys! { |k| k.camelize(:lower) }
+  #   hash.each { |k, v| hash[k] = clean!(v) }
+  #   hash.delete_if { |_k, v| v.blank? }
   # end
-  
+
   def clean_hash!(hash)
     hash.deep_transform_keys! do |key|
       # convert key to string if it is a symbol
