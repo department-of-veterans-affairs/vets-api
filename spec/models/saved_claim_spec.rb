32fdc1cfbda9d0a56a74e0c12330f79ad86b7e8c
Sam Raudabaugh <sraudabaugh@gmail.com>
2024-08-05 17:14:13 +0000

    Attempt to reproduce VRE validation issue

--- spec/models/saved_claim_spec.rb
+++ spec/models/saved_claim_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe SavedClaim do
   let(:saved_claim) { create(:pension_claim) }
+  let(:vre_claim) { create(:veteran_readiness_employment_claim) }
+  let(:disability_claim) { create(:va526ez) }
 
   describe '#to_pdf' do
     it 'converts form to pdf' do
@@ -11,6 +13,23 @@ RSpec.describe SavedClaim do
 
       expect(PdfFill::Filler).to receive(:fill_form).with(saved_claim, nil).once.and_return(file_path)
       saved_claim.to_pdf
+    end
+  end
+
+  describe '#form_matches_schema' do
+    it 'is thread-safe' do
+      threads = []
+      5.times do
+        threads << Thread.new do
+          disability_claim.form_matches_schema
+        end
+      end
+      5.times do
+        threads << Thread.new do
+          vre_claim.form_matches_schema
+        end
+      end
+      threads.each(&:join)
     end
   end
 end
