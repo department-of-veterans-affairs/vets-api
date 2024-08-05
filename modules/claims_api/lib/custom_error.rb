c1075316d662b8593145e961129a2ce1d60569d1
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-08-05 16:41:47 -0600

    Adds method for handling errors that are strings

--- modules/claims_api/lib/custom_error.rb
+++ modules/claims_api/lib/custom_error.rb
@@ -13,6 +13,8 @@ module ClaimsApi
     end
 
     def build_error
+      handle_strings if @error.is_a?(String)
+
       case @error
       when Faraday::ParsingError
         raise_backend_exception
@@ -77,6 +79,15 @@ module ClaimsApi
       else
         ClaimsApi::V2::Error::LighthouseErrorMapper.new(error).get_details
       end
+    end
+
+    def handle_strings
+      return unless @error.is_a?(String)
+
+      detail = @error
+      title = 'string error'
+      messages = [{ detail:, status: 422, title: }]
+      @error = ClaimsApi::Common::Exceptions::Lighthouse::BackendServiceException.new(messages)
     end
   end
 end
