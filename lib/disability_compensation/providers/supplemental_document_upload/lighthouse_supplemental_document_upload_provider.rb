0532ebc33a0d3d0fecd3fc4f5aa7e5b59f7de628
Nathan Burgess <burgess.nathan28@gmail.com>
2024-08-06 11:48:12 -0400

    Implement basic LighthouseSupplementalDocumentUploadProvider
    Hooks up the bones of an API provider for Lighthouse document uploads; this will actuall be implemented in another ticket

---
+++ lib/disability_compensation/providers/supplemental_document_upload/lighthouse_supplemental_document_upload_provider.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'disability_compensation/providers/supplemental_document_upload/supplemental_document_upload_provider'
+
+class LighthouseSupplementalDocumentUploadProvider
+  include SupplementalDocumentUploadProvider
+
+  # @param form526_submission [Form526Submission]
+  # @param file_body [String]
+  def initialize(form526_submission, file_body)
+    @form526_submission = form526_submission
+    @file_body = file_body
+  end
+
+  def generate_upload_document(file_name, document_type)
+    # TODO: implement in https://github.com/department-of-veterans-affairs/va.gov-team/issues/90059
+  end
+
+  def validate_upload_document(lighthouse_document)
+    # TODO: implement in https://github.com/department-of-veterans-affairs/va.gov-team/issues/90059
+  end
+
+  def submit_upload_document(lighthouse_document)
+    # TODO: implement in https://github.com/department-of-veterans-affairs/va.gov-team/issues/90059
+  end
+end
