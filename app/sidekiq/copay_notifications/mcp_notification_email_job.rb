7f7982a38d0b24cac47171cfbc5310f919dca90f
RachalCassity <rachal.cassity@oddball.io>
2024-08-06 08:41:46 -0500

    test

--- app/sidekiq/copay_notifications/mcp_notification_email_job.rb
+++ app/sidekiq/copay_notifications/mcp_notification_email_job.rb
@@ -15,11 +15,7 @@ module CopayNotifications
     sidekiq_options retry: 14
 
     def perform(vet360_id, template_id, backup_email = nil, personalisation = nil)
-      if Flipper.enabled?(:va_profile_information_v3_service))
-        person_resp = VAProfile::ProfileInformation::Service.get_person(vet360_id)
-      else
-        person_resp = VAProfile::ContactInformation::Service.get_person(vet360_id)
-      end
+      person_resp = VAProfile::ContactInformation::Service.get_person(vet360_id)
       email_address = person_resp.person&.emails&.first&.email_address || backup_email
 
       if email_address
