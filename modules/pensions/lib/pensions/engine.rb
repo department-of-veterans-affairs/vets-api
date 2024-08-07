89b659ca40eda155f12f96e1eb00f50abc234ac1
Scott Gorman <scott.gorman1@va.gov>
2024-08-07 17:16:14 -0400

    - Update engine.rb

--- modules/pensions/lib/pensions/engine.rb
+++ modules/pensions/lib/pensions/engine.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'pdf_fill/va21p527ez'
-
 module Pensions
   # @see https://api.rubyonrails.org/classes/Rails/Engine.html
   class Engine < ::Rails::Engine
@@ -14,6 +12,7 @@ module Pensions
 
     initializer 'pensions.after_initialize' do |app|
       app.config.after_initialize do
+        require 'pdf_fill/va21p527ez'
         # Register our Pension Pdf Fill form
         ::PdfFill::Filler.register_form(Pensions::PdfFill::Va21p527ez::FORM_ID, Pensions::PdfFill::Va21p527ez)
       end
