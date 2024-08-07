6cfc0ecadc79db26fb0c80d0df2404d157887d43
Derek Dyer <derek@digitalderek.com>
2024-08-07 19:10:48 -0400

    87403 Clean up FSR confirmation email feature flag

--- modules/debts_api/lib/debts_api/v0/financial_status_report_service.rb
+++ modules/debts_api/lib/debts_api/v0/financial_status_report_service.rb
@@ -236,7 +236,7 @@ module DebtsApi
     end
 
     def send_confirmation_email(template_id)
-      return unless Flipper.enabled?(:fsr_confirmation_email)
+      return unless Rails.env.production?
 
       email = @user.email&.downcase
       return if email.blank?
