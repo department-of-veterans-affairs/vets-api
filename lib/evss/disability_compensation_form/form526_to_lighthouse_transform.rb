e95529f065d9e6c58eddcf14d22af7637d9e0dfe
Tommasina Miller <144388524+tommasina-va@users.noreply.github.com>
2024-08-09 12:39:49 -0500

    make line less long for the cop

--- lib/evss/disability_compensation_form/form526_to_lighthouse_transform.rb
+++ lib/evss/disability_compensation_form/form526_to_lighthouse_transform.rb
@@ -506,7 +506,8 @@ module EVSS
           veteran['currentMailingAddress']['zipFirstFive']
         veteran_identification.mailing_address.zip_last_four = veteran['currentMailingAddress']['zipLastFour']
         veteran_identification.mailing_address.country = veteran['currentMailingAddress']['country']
-        veteran_identification.mailing_address.international_postal_code = veteran['currentMailingAddress']['internationalPostalCode']
+        veteran_identification.mailing_address.international_postal_code =
+          veteran['currentMailingAddress']['internationalPostalCode']
       end
 
       def transform_service_periods(service_information_source, service_information)
