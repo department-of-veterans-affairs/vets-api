213c76fd27458839ec33227638b85a10c0ff5ee7
Austin Covrig <austin.covrig@oddball.io>
2024-08-12 10:20:45 -0400

    Enable EBenefitsBnftClaimStatusWebService

--- modules/claims_api/lib/bgs_service/local_bgs.rb
+++ modules/claims_api/lib/bgs_service/local_bgs.rb
@@ -14,7 +14,10 @@ require 'bgs_service/local_bgs_refactored'
 
 module ClaimsApi
   class LocalBGS
-    CACHED_SERVICES = %w[ClaimantServiceBean/ClaimantWebService].freeze
+    CACHED_SERVICES = %w[
+      ClaimantServiceBean/ClaimantWebService
+      EBenefitsBnftClaimStatusWebServiceBean/EBenefitsBnftClaimStatusWebService
+    ].freeze
 
     # rubocop:disable Metrics/MethodLength
     def initialize(external_uid:, external_key:)
