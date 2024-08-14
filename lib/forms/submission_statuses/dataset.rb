af14e35df521e09ec4095b0535b48f26511bfc40
Tom Harrison <tom.harrison@adhocteam.us>
2024-08-14 13:30:46 -0400

    Add form submission status error handling (#17981)
    * Add form submission status error handling
    
    * Refactor code based on Reek recommendations
    
    * Refactor based on Reek recommendations

--- lib/forms/submission_statuses/dataset.rb
+++ lib/forms/submission_statuses/dataset.rb
@@ -5,14 +5,22 @@ require 'lighthouse/benefits_intake/service'
 module Forms
   module SubmissionStatuses
     class Dataset
-      attr_accessor :submissions, :statuses
+      attr_accessor :submissions, :intake_statuses, :errors
 
       def submissions?
         @submissions.any?
       end
 
-      def statuses?
-        @statuses.any?
+      def intake_statuses?
+        @intake_statuses.any?
+      end
+
+      def errors?
+        @errors.present?
+      end
+
+      def error_status
+        @errors.first[:status] if errors?
       end
     end
   end
