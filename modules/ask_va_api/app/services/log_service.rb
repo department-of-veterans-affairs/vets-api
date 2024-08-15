d6254ba140d4b14157e0ab6c54220f800a01de75
khoa-v-nguyen <khoa.nguyen@oddball.io>
2024-08-15 14:13:05 -0600

    Refactor LogService
    - Removed Sentry integration from logging due to its upcoming deprecation.

--- modules/ask_va_api/app/services/log_service.rb
+++ modules/ask_va_api/app/services/log_service.rb
@@ -37,7 +37,6 @@ class LogService
   end
 
   def handle_logging_error(action, error)
-    Sentry.capture_exception(error, extra: { action: })
     Rails.logger.error("Error logging action #{action}: #{error.message}")
     span&.set_tag('error', true)
     span&.set_tag('error.msg', error.message)
