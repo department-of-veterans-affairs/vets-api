306e50974e8f6df5e934402ef20d7f6d0961c2eb
Josh Fike <josh.fike@oddball.io>
2024-08-15 14:44:05 -0500

    Make threshold and max results constants

--- modules/representation_management/app/models/representation_management/accredited_entity_query.rb
+++ modules/representation_management/app/models/representation_management/accredited_entity_query.rb
@@ -4,6 +4,9 @@ module RepresentationManagement
   class AccreditedEntityQuery
     include ActiveModel::Model
 
+    WORD_SIMILARITY_THRESHOLD = 0.7
+    MAXIMUM_RESULT_COUNT = 10
+
     def initialize(query_string)
       @query_string = query_string
     end
@@ -11,21 +14,17 @@ module RepresentationManagement
     def results
       (individuals + organizations).sort_by do |record|
         levenshtein_distance(@query_string, record)
-      end.take(10)
+      end.take(MAXIMUM_RESULT_COUNT)
     end
 
     private
 
     def individuals
-      AccreditedIndividual.where('word_similarity(?, full_name) >= ?', @query_string, threshold)
+      AccreditedIndividual.where('word_similarity(?, full_name) >= ?', @query_string, WORD_SIMILARITY_THRESHOLD)
     end
 
     def organizations
-      AccreditedOrganization.where('word_similarity(?, name) >= ?', @query_string, threshold)
-    end
-
-    def threshold
-      0.7
+      AccreditedOrganization.where('word_similarity(?, name) >= ?', @query_string, WORD_SIMILARITY_THRESHOLD)
     end
 
     def levenshtein_distance(query, record)
