8c03de01f792f89da44039857bf89fb727405677
Nathan Burgess <burgess.nathan28@gmail.com>
2024-08-15 12:18:10 -0400

    Implement base logging methods for uploading providers
    Adds logging methods to the SupplementalDocumentUploadProvider module for use in logging success, retries and failures in jobs that use the inheriting EVSS and Lighthouse providers

--- lib/disability_compensation/providers/document_upload/supplemental_document_upload_provider.rb
+++ lib/disability_compensation/providers/document_upload/supplemental_document_upload_provider.rb
@@ -1,15 +1,31 @@
 # frozen_string_literal: true
 
 module SupplementalDocumentUploadProvider
-  def self.generate_upload_document(file_name, document_type)
+  def self.raise_not_implemented_error
     raise NotImplementedError, 'Do not use base module methods. Override this method in implementation class.'
+  end
+
+  def self.generate_upload_document(file_name, document_type)
+    raise_not_implemented_error
   end
 
   def self.validate_upload_document(lighthouse_document)
-    raise NotImplementedError, 'Do not use base module methods. Override this method in implementation class.'
+    raise_not_implemented_error
   end
 
   def self.submit_upload_document(lighthouse_document)
-    raise NotImplementedError, 'Do not use base module methods. Override this method in implementation class.'
+    raise_not_implemented_error
+  end
+
+  def self.log_upload_success(uploading_class_prefix)
+    raise_not_implemented_error
+  end
+
+  def self.log_upload_error_retry(uploading_class_prefix, error)
+    raise_not_implemented_error
+  end
+
+  def self.log_upload_failure(uploading_class_prefix, error)
+    raise_not_implemented_error
   end
 end
