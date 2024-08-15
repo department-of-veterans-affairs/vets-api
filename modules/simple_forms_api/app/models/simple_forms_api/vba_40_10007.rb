0b2a0b063780fa8197ac0e76474236c538427d2e
Kenny Santiago <50601724+ksantiagoBAH@users.noreply.github.com>
2024-08-15 09:09:21 -0500

    Added absolute path to handle attachments function (#18021)
    * Added absolute path to handle attachments function
    
    * Fixed linting
    
    * removed put statements
    
    * fix linting

--- modules/simple_forms_api/app/models/simple_forms_api/vba_40_10007.rb
+++ modules/simple_forms_api/app/models/simple_forms_api/vba_40_10007.rb
@@ -211,18 +211,19 @@ module SimpleFormsApi
         pdf.text "Highest Rank Attained: #{highest_rank_c}", size: 10
       end
     end
-
     # rubocop:enable Metrics/MethodLength
+
     def handle_attachments(file_path)
       attachments = get_attachments
+      absolute_path_attachments = attachments.map do |attachment|
+        File.expand_path(attachment, Rails.root)
+      end
       combined_pdf = CombinePDF.new
       combined_pdf << CombinePDF.load(file_path)
-
       attachment_page_path = 'attachment_page.pdf'
       create_attachment_page(attachment_page_path)
       combined_pdf << CombinePDF.load(attachment_page_path)
-
-      attachments.each do |attachment|
+      absolute_path_attachments.each do |attachment|
         combined_pdf << CombinePDF.load(attachment)
       rescue => e
         Rails.logger.error(
