53bc644a4a5fa1f435b74414c81a4fd8c9e940b0
Tai Wilkin <tai.wilkin@coforma.io>
2024-08-15 12:12:13 -0400

    Handle lint errors

--- lib/pdf_fill/forms/va21p0969.rb
+++ lib/pdf_fill/forms/va21p0969.rb
@@ -15,7 +15,7 @@ module PdfFill
         'CUSTODIAN' => 2,
         'CHILD' => 3,
         'PARENT' => 4,
-        'OTHER' => 5,
+        'OTHER' => 5
       }.freeze
 
       INCOME_TYPES = {
@@ -24,20 +24,20 @@ module PdfFill
         'WAGES' => 2,
         'UNEMPLOYMENT' => 3,
         'CIVIL_SERVICE' => 4,
-        'OTHER' => 5,
-    }.freeze
+        'OTHER' => 5
+      }.freeze
 
       KEY = {
-        #3a
+        # 3a
         'unassociatedIncome' => {
           key: 'F[0].Page_4[0].DependentsReceiving3a[0]'
         },
-        #3b - 3f
+        # 3b - 3f
         'unassociatedIncomes' => {
           limit: 5,
           first_key: 'recipientRelationship',
           'recipientRelationship' => {
-            key: "F[0].IncomeRecipients3[#{ITERATOR}]",
+            key: "F[0].IncomeRecipients3[#{ITERATOR}]"
           },
           'recipientRelationshipOverflow' => {
             question_num: 3,
@@ -54,7 +54,8 @@ module PdfFill
             key: "F[0].NameofIncomeRecipient3[#{ITERATOR}]",
             question_num: 3,
             question_suffix: '(2)',
-            question_text: "SPECIFY NAME OF INCOME RECIPIENT (Only needed if Custodian of child, child, parent, or other)"
+            question_text:
+              'SPECIFY NAME OF INCOME RECIPIENT (Only needed if Custodian of child, child, parent, or other)'
           },
           'incomeType' => {
             key: "F[0].TypeOfIncome3[#{ITERATOR}]"
@@ -62,35 +63,35 @@ module PdfFill
           'incomeTypeOverflow' => {
             question_num: 3,
             question_suffix: '(3)',
-            question_text: "SPECIFY THE TYPE OF INCOME"
+            question_text: 'SPECIFY THE TYPE OF INCOME'
           },
           'otherIncomeType' => {
             key: "F[0].OtherIncomeType3[#{ITERATOR}]",
             question_num: 3,
             question_suffix: '(3)',
-            question_text: "SPECIFY THE TYPE OF INCOME"
+            question_text: 'SPECIFY THE TYPE OF INCOME'
           },
           'grossMonthlyIncome' => {
             'thousands' => {
-              key: "F[0].GrossMonthlyIncome1_3[#{ITERATOR}]",
+              key: "F[0].GrossMonthlyIncome1_3[#{ITERATOR}]"
             },
             'dollars' => {
               key: "F[0].GrossMonthlyIncome2_3[#{ITERATOR}]"
             },
             'cents' => {
               key: "F[0].GrossMonthlyIncome3_3[#{ITERATOR}]"
-            },
+            }
           },
           'grossMonthlyIncomeOverflow' => {
             question_num: 3,
             question_suffix: '(4)',
-            question_text: "GROSS MONTHLY INCOME"
+            question_text: 'GROSS MONTHLY INCOME'
           },
           'payer' => {
-              key: "F[0].IncomePayer3[#{ITERATOR}]",
-              question_num: 3,
-              question_suffix: '(5)',
-              question_text: "SPECIFY INCOME PAYER (Name of business, financial institution, or program, etc.)"
+            key: "F[0].IncomePayer3[#{ITERATOR}]",
+            question_num: 3,
+            question_suffix: '(5)',
+            question_text: 'SPECIFY INCOME PAYER (Name of business, financial institution, or program, etc.)'
           }
         }
       }.freeze
@@ -98,44 +99,53 @@ module PdfFill
       def merge_fields(_options = {})
         expand_unassociated_incomes
 
-        @form_data
+        form_data
       end
 
+      private
+
       def expand_unassociated_incomes
-        @form_data['unassociatedIncome'] = to_radio_yes_no(@form_data['unassociatedIncomes']&.length)
-        @form_data['unassociatedIncomes'] = @form_data['unassociatedIncomes']&.map do |income|
-          {
-            'recipientRelationship' => RECIPIENTS[income['recipientRelationship']],
-            'recipientRelationshipOverflow' => income['recipientRelationship'],
-            'otherRecipientRelationshipType' => income['otherRecipientRelationshipType'],
-            'recipientName' => income['recipientName'],
-            'incomeType' => INCOME_TYPES[income['incomeType']],
-            'incomeTypeOverflow' => income['incomeType'],
-            'otherIncomeType' => income['otherIncomeType'],
-            'grossMonthlyIncome' => split_currency_amount(income['grossMonthlyIncome']),
-            'grossMonthlyIncomeOverflow' => income['grossMonthlyIncome'],
-            'payer' => income['payer'],
-          }
+        unassociated_incomes = form_data['unassociatedIncomes']
+        form_data['unassociatedIncome'] = unassociated_incomes&.length ? 'YES' : 1
+        form_data['unassociatedIncomes'] = unassociated_incomes&.map do |income|
+          expand_unassociated_income(income)
         end
       end
 
-      # UTILITIES
-      def to_radio_yes_no(obj)
-        obj ? 'YES' : 1
+      # :reek:FeatureEnvy
+      def expand_unassociated_income(income)
+        recipient_relationship = income['recipientRelationship']
+        income_type = income['incomeType']
+        gross_monthly_income = income['grossMonthlyIncome']
+        {
+          'recipientRelationship' => RECIPIENTS[recipient_relationship],
+          'recipientRelationshipOverflow' => recipient_relationship,
+          'otherRecipientRelationshipType' => income['otherRecipientRelationshipType'],
+          'recipientName' => income['recipientName'],
+          'incomeType' => INCOME_TYPES[income_type],
+          'incomeTypeOverflow' => income_type,
+          'otherIncomeType' => income['otherIncomeType'],
+          'grossMonthlyIncome' => gross_monthly_income ? split_currency_amount(gross_monthly_income) : {},
+          'grossMonthlyIncomeOverflow' => gross_monthly_income,
+          'payer' => income['payer']
+        }
       end
 
       def split_currency_amount(amount)
-        return {} if amount.nil? || amount.negative? || amount >= 100_000
+        return {} if amount.negative? || amount >= 100_000
 
         arr = number_to_currency(amount).to_s.split(/[,.$]/).reject(&:empty?)
-        cents =  arr.length >= 1 ? arr[-1] : 0
-        dollars =  arr.length >= 2? arr[-2] : 0
-        thousands =  arr.length >= 3 ? arr[-3] : 0
         {
-          'cents' => sprintf('%02d', cents.to_i),
-          'dollars' => sprintf('%03d', dollars.to_i),
-          'thousands' => sprintf('%02d', thousands.to_i)
+          'cents' => get_currency_field(arr, -1, 2),
+          'dollars' => get_currency_field(arr, -2, 3),
+          'thousands' => get_currency_field(arr, -3, 2)
         }
+      end
+
+      # :reek:FeatureEnvy
+      def get_currency_field(arr, neg_i, field_length)
+        value = arr.length >= -neg_i ? arr[neg_i] : 0
+        format("%0#{field_length}d", value.to_i)
       end
     end
   end
