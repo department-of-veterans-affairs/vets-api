a2941c23ff6a1a5e71a3446ec4f3dd80ad0b8686
Nathan Burgess <burgess.nathan28@gmail.com>
2024-08-19 09:43:22 -0400

    Fix linting errors

--- lib/disability_compensation/providers/document_upload/supplemental_document_upload_provider.rb
+++ lib/disability_compensation/providers/document_upload/supplemental_document_upload_provider.rb
@@ -9,27 +9,27 @@ module SupplementalDocumentUploadProvider
     raise NotImplementedError, 'Do not use base module methods. Override this method in implementation class.'
   end
 
-  def self.generate_upload_document(file_name, document_type)
+  def self.generate_upload_document(_file_name, _document_type)
     raise_not_implemented_error
   end
 
-  def self.validate_upload_document(lighthouse_document)
+  def self.validate_upload_document(_lighthouse_document)
     raise_not_implemented_error
   end
 
-  def self.submit_upload_document(lighthouse_document)
+  def self.submit_upload_document(_lighthouse_document)
     raise_not_implemented_error
   end
 
-  def self.log_upload_success(uploading_class_prefix)
+  def self.log_upload_success(_uploading_class_prefix)
     raise_not_implemented_error
   end
 
-  def self.log_upload_error_retry(uploading_class_prefix)
+  def self.log_upload_error_retry(_uploading_class_prefix)
     raise_not_implemented_error
   end
 
-  def self.log_upload_failure(uploading_class_prefix, error)
+  def self.log_upload_failure(_uploading_class_prefix, _error)
     raise_not_implemented_error
   end
 end
