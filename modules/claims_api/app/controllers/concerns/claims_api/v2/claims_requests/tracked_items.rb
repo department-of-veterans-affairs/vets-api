d85ccc0097e56e6371a4e7869627936a3042107f
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-08-21 12:06:45 -0600

    Moves call to BGS for tracked_items to the trackedItems concern.

---
+++ modules/claims_api/app/controllers/concerns/claims_api/v2/claims_requests/tracked_items.rb
@@ -0,0 +1,15 @@
+module ClaimsApi
+  module V2
+    module ClaimsRequests
+      module TrackedItems
+        extend ActiveSupport::Concern
+
+        def find_tracked_items!(claim_id)
+          return if claim_id.blank?
+
+          local_bgs_service.find_tracked_items(claim_id)[:dvlpmt_items] || []
+        end
+      end
+    end
+  end
+end
