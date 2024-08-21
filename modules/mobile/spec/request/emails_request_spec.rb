407ad4fb468de577466160133be6cfc3127bfbac
RachalCassity <rachal.cassity@oddball.io>
2024-08-21 16:17:58 -0500

    fixed mobile requests

--- modules/mobile/spec/request/emails_request_spec.rb
+++ modules/mobile/spec/request/emails_request_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'email', type: :request do
   include JsonSchemaMatchers
 
   let!(:user) { sis_user }
+  Flipper.disable(:va_v3_contact_information_service)
 
   describe 'POST /mobile/v0/user/emails' do
     context 'with a valid email that takes two tries to complete' do
