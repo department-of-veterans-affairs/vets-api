89813b2bd040a6264ce82eb10bd82018eb0aedf3
Gabriel Zurita <gabezurita@gmail.com>
2024-08-22 16:41:22 -0600

    remove unnecessary table defs

--- db/migrate/20240819184218_drop_accredited_representative_portal_pilot_representatives.rb
+++ db/migrate/20240819184218_drop_accredited_representative_portal_pilot_representatives.rb
@@ -1,10 +1,5 @@
 class DropAccreditedRepresentativePortalPilotRepresentatives < ActiveRecord::Migration[6.0]
   def change
-    drop_table :accredited_representative_portal_pilot_representatives do |t|
-      t.string "ogc_registration_number", null: false
-      t.string "email", null: false
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-    end
+    drop_table :accredited_representative_portal_pilot_representatives
   end
 end
