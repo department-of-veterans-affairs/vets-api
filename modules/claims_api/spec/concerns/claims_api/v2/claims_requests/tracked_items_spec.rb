0b28edc29ac4116c043237a5690dcf4c94afaa5c
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-08-26 08:16:22 -0600

    Moves methods out of the claims_controller with corresponding tests.

--- modules/claims_api/spec/concerns/claims_api/v2/claims_requests/tracked_items_spec.rb
+++ modules/claims_api/spec/concerns/claims_api/v2/claims_requests/tracked_items_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 class FakeController
   include ClaimsApi::V2::ClaimsRequests::TrackedItems
+  include ClaimsApi::V2::ClaimsRequests::TrackedItemsAssistance
 
   def local_bgs_service
     @local_bgs_service ||= ClaimsApi::LocalBGS.new(
@@ -32,12 +33,62 @@ end
 describe FakeController do
   context 'when the claims controller calls the tracked_items module' do
     let(:claim_id) { '600118544' }
+    let(:bgs_claim) do
+      { benefit_claim_details_dto: { benefit_claim_id: '111111111',
+                                     phase_chngd_dt: '2024-08-19 06:58:18 -0600',
+                                     phase_type: 'Pending Decision Approval',
+                                     ptcpnt_clmant_id: 10_969_622_233_703_603, ptcpnt_vet_id: '600061742',
+                                     phase_type_change_ind: '76', claim_status_type: 'Compensation',
+                                     bnft_claim_lc_status: [{ max_est_claim_complete_dt: '2024-08-19 06:55:17 -0600',
+                                                              min_est_claim_complete_dt: '2024-08-18 06:07:17 -0600',
+                                                              phase_chngd_dt: '2024-08-18 10:58:59 -0600',
+                                                              phase_type: 'Claim Received',
+                                                              phase_type_change_ind: 'N' }] } }
+    end
 
     it 'makes a successful call to BGS' do
       VCR.use_cassette('claims_api/bgs/tracked_items/find_tracked_items') do
         result = subject.find_tracked_items!(claim_id)
         expect(result[0][:name]).to eq('DevelopmentItem')
         expect(result[0][:claim_id]).to eq('600118544')
+      end
+    end
+
+    it '#map_bgs_tracked_items' do
+      VCR.use_cassette('claims_api/bgs/tracked_items/find_tracked_items') do
+        result = subject.map_bgs_tracked_items(bgs_claim)
+        expect(result).to eq([])
+      end
+    end
+
+    it '#build_wwsnfy_items' do
+      VCR.use_cassette('claims_api/bgs/tracked_items/find_tracked_items') do
+        subject.map_bgs_tracked_items(bgs_claim)
+        result = subject.build_wwsnfy_items
+        expect(result).to eq([])
+      end
+    end
+
+    it '#build_wwd_items' do
+      VCR.use_cassette('claims_api/bgs/tracked_items/find_tracked_items') do
+        subject.map_bgs_tracked_items(bgs_claim)
+        result = subject.build_wwd_items
+        expect(result).to eq([])
+      end
+    end
+
+    it '#build_wwr_items' do
+      VCR.use_cassette('claims_api/bgs/tracked_items/find_tracked_items') do
+        subject.map_bgs_tracked_items(bgs_claim)
+        result = subject.build_wwr_items
+        expect(result).to eq([])
+      end
+    end
+
+    it '#build_no_longer_needed_items' do
+      VCR.use_cassette('claims_api/bgs/tracked_items/find_tracked_items') do
+        result = subject.build_no_longer_needed_items
+        expect(result).to eq([])
       end
     end
   end
