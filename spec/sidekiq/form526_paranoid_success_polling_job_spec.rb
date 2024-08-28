25f8803614a216497e15d617bf2535b767291ee7
Samuel Stuckey <sam.n.stuckey@gmail.com>
2024-08-28 15:29:37 -0600

    general save

--- spec/sidekiq/form526_paranoid_success_polling_job_spec.rb
+++ spec/sidekiq/form526_paranoid_success_polling_job_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Form526ParanoidSuccessPollingJob, type: :job do
   describe '#perform' do
     let!(:new_submission) { create(:form526_submission) }
     let!(:backup_submission) { create(:form526_submission, :backup_path) }
-    let!(:paranoid_submission_1) { create(:form526_submission, :backup_path, :paranoid_success) }
-    let!(:paranoid_submission_2) { create(:form526_submission, :backup_path, :paranoid_success) }
-    let!(:paranoid_submission_3) { create(:form526_submission, :backup_path, :paranoid_success) }
-    let!(:paranoid_submission_4) { create(:form526_submission, :backup_path, :paranoid_success) }
-    let!(:paranoid_submission_5) { create(:form526_submission, :backup_path, :paranoid_success) }
-    let!(:paranoid_submission_6) { create(:form526_submission, :backup_path, :paranoid_success) }
+    let!(:paranoid_submission1) { create(:form526_submission, :backup_path, :paranoid_success) }
+    let!(:paranoid_submission2) { create(:form526_submission, :backup_path, :paranoid_success) }
+    let!(:paranoid_submission3) { create(:form526_submission, :backup_path, :paranoid_success) }
+    let!(:paranoid_submission4) { create(:form526_submission, :backup_path, :paranoid_success) }
+    let!(:paranoid_submission5) { create(:form526_submission, :backup_path, :paranoid_success) }
+    let!(:paranoid_submission6) { create(:form526_submission, :backup_path, :paranoid_success) }
     let!(:success_by_age) do
       Timecop.freeze((1.year + 1.day).ago) do
         create(:form526_submission, :backup_path, :paranoid_success)
@@ -29,48 +29,49 @@ RSpec.describe Form526ParanoidSuccessPollingJob, type: :job do
         {
           'data' => [
             {
-              'id' => paranoid_submission_1.backup_submitted_claim_id,
+              'id' => paranoid_submission1.backup_submitted_claim_id,
               'attributes' => {
-                'guid' => paranoid_submission_1.backup_submitted_claim_id,
+                'guid' => paranoid_submission1.backup_submitted_claim_id,
                 'status' => 'success'
               }
             },
             {
-              'id' => paranoid_submission_2.backup_submitted_claim_id,
+              'id' => paranoid_submission2.backup_submitted_claim_id,
               'attributes' => {
-                'guid' => paranoid_submission_2.backup_submitted_claim_id,
+                'guid' => paranoid_submission2.backup_submitted_claim_id,
                 'status' => 'processing'
               }
             },
             {
-              'id' => paranoid_submission_3.backup_submitted_claim_id,
+              'id' => paranoid_submission3.backup_submitted_claim_id,
               'attributes' => {
-                'guid' => paranoid_submission_3.backup_submitted_claim_id,
+                'guid' => paranoid_submission3.backup_submitted_claim_id,
                 'status' => 'error'
               }
             },
             {
-              'id' => paranoid_submission_4.backup_submitted_claim_id,
+              'id' => paranoid_submission4.backup_submitted_claim_id,
               'attributes' => {
-                'guid' => paranoid_submission_4.backup_submitted_claim_id,
+                'guid' => paranoid_submission4.backup_submitted_claim_id,
                 'status' => 'expired'
               }
             },
             {
-              'id' => paranoid_submission_5.backup_submitted_claim_id,
+              'id' => paranoid_submission5.backup_submitted_claim_id,
               'attributes' => {
-                'guid' => paranoid_submission_5.backup_submitted_claim_id,
+                'guid' => paranoid_submission5.backup_submitted_claim_id,
                 'status' => 'something_crazy'
               }
             },
             {
-              'id' => paranoid_submission_6.backup_submitted_claim_id,
+              'id' => paranoid_submission6.backup_submitted_claim_id,
               'attributes' => {
-                'guid' => paranoid_submission_6.backup_submitted_claim_id,
+                'guid' => paranoid_submission6.backup_submitted_claim_id,
                 'status' => 'vbms'
               }
             }
-          ] }
+          ]
+        }
       end
 
       describe 'submission to the bulk status report endpoint' do
@@ -84,12 +85,12 @@ RSpec.describe Form526ParanoidSuccessPollingJob, type: :job do
             .and_return(response)
 
           expect(paranoid_claim_ids).to contain_exactly(
-            paranoid_submission_1.backup_submitted_claim_id,
-            paranoid_submission_2.backup_submitted_claim_id,
-            paranoid_submission_3.backup_submitted_claim_id,
-            paranoid_submission_4.backup_submitted_claim_id,
-            paranoid_submission_5.backup_submitted_claim_id,
-            paranoid_submission_6.backup_submitted_claim_id
+            paranoid_submission1.backup_submitted_claim_id,
+            paranoid_submission2.backup_submitted_claim_id,
+            paranoid_submission3.backup_submitted_claim_id,
+            paranoid_submission4.backup_submitted_claim_id,
+            paranoid_submission5.backup_submitted_claim_id,
+            paranoid_submission6.backup_submitted_claim_id
           )
 
           expect_any_instance_of(BenefitsIntakeService::Service)
@@ -112,28 +113,28 @@ RSpec.describe Form526ParanoidSuccessPollingJob, type: :job do
             .and_return(response)
 
           expect(paranoid_claim_ids).to contain_exactly(
-            paranoid_submission_1.backup_submitted_claim_id,
-            paranoid_submission_2.backup_submitted_claim_id,
-            paranoid_submission_3.backup_submitted_claim_id,
-            paranoid_submission_4.backup_submitted_claim_id,
-            paranoid_submission_5.backup_submitted_claim_id,
-            paranoid_submission_6.backup_submitted_claim_id
+            paranoid_submission1.backup_submitted_claim_id,
+            paranoid_submission2.backup_submitted_claim_id,
+            paranoid_submission3.backup_submitted_claim_id,
+            paranoid_submission4.backup_submitted_claim_id,
+            paranoid_submission5.backup_submitted_claim_id,
+            paranoid_submission6.backup_submitted_claim_id
           )
 
           Form526ParanoidSuccessPollingJob.new.perform
-          paranoid_submission_1.reload
-          paranoid_submission_2.reload
-          paranoid_submission_3.reload
-          paranoid_submission_4.reload
-          paranoid_submission_5.reload
-          paranoid_submission_6.reload
+          paranoid_submission1.reload
+          paranoid_submission2.reload
+          paranoid_submission3.reload
+          paranoid_submission4.reload
+          paranoid_submission5.reload
+          paranoid_submission6.reload
 
-          expect(paranoid_submission_1.backup_submitted_claim_status).to eq 'paranoid_success'
-          expect(paranoid_submission_2.backup_submitted_claim_status).to eq nil
-          expect(paranoid_submission_3.backup_submitted_claim_status).to eq 'rejected'
-          expect(paranoid_submission_4.backup_submitted_claim_status).to eq 'rejected'
-          expect(paranoid_submission_5.backup_submitted_claim_status).to eq 'rejected'
-          expect(paranoid_submission_6.backup_submitted_claim_status).to eq 'accepted'
+          expect(paranoid_submission1.backup_submitted_claim_status).to eq 'paranoid_success'
+          expect(paranoid_submission2.backup_submitted_claim_status).to eq nil
+          expect(paranoid_submission3.backup_submitted_claim_status).to eq 'rejected'
+          expect(paranoid_submission4.backup_submitted_claim_status).to eq 'rejected'
+          expect(paranoid_submission5.backup_submitted_claim_status).to eq 'rejected'
+          expect(paranoid_submission6.backup_submitted_claim_status).to eq 'accepted'
         end
       end
     end
