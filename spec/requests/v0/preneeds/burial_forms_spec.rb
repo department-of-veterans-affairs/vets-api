b692cc6fe04234c6f8e828807bd6297a919201f8
Ryan McNeil <ryan.mcneil@oddball.io>
2024-08-28 15:01:25 -0600

    remove swagger for attachments

--- spec/requests/v0/preneeds/burial_forms_spec.rb
+++ spec/requests/v0/preneeds/burial_forms_spec.rb
@@ -9,68 +9,7 @@ RSpec.describe 'V0::Preneeds::BurialForm', type: :request do
     { application: attributes_for(:burial_form) }
   end
 
-  def post_burial_forms(additional_headers = {})
-    post '/v0/preneeds/burial_forms',
-         params: params.to_json,
-         headers: { 'CONTENT_TYPE' => 'application/json' }.merge(additional_headers)
-  end
-
-  context 'with invalid input' do
-    it 'returns an with error' do
-      params[:application][:veteran].delete(:military_status)
-      post_burial_forms
-
-      error = JSON.parse(response.body)['errors'].first
-
-      expect(error['status']).to eq('422')
-      expect(error['title']).to match(/validation error/i)
-      expect(error['detail']).to match(/militaryStatus/)
-    end
-
-    it 'does not clear the saved form' do
-      expect_any_instance_of(ApplicationController).not_to receive(:clear_saved_form).with('40-10007')
-
-      params[:application][:veteran].delete(:military_status)
-      post_burial_forms
-    end
-  end
-
-  context 'with a failed burial form submittal from EOAS' do
-    it 'returns with a VA900 error when status is 500' do
-      VCR.use_cassette('preneeds/burial_forms/burial_form_with_invalid_applicant_address2') do
-        params[:application][:applicant][:mailing_address][:address2] = '1' * 21
-        post_burial_forms
-      end
-
-      error = JSON.parse(response.body)['errors'].first
-
-      expect(error['status']).to eq('400')
-      expect(error['title']).to match(/operation failed/i)
-      expect(error['detail']).to match(/Error committing transaction/i)
-    end
-
-    it 'returns with a VA900 error when the status is 200' do
-      VCR.use_cassette('preneeds/burial_forms/burial_form_with_duplicate_tracking_number') do
-        allow_any_instance_of(Preneeds::BurialForm).to receive(:generate_tracking_number).and_return('19')
-        post_burial_forms
-      end
-
-      error = JSON.parse(response.body)['errors'].first
-
-      expect(error['status']).to eq('400')
-      expect(error['title']).to match(/operation failed/i)
-      expect(error['detail']).to match(/Tracking number '19' already exists/i)
-    end
-
-    it 'does not clear the saved form' do
-      expect_any_instance_of(ApplicationController).not_to receive(:clear_saved_form).with('40-10007')
-
-      VCR.use_cassette('preneeds/burial_forms/burial_form_with_duplicate_tracking_number') do
-        allow_any_instance_of(Preneeds::BurialForm).to receive(:generate_tracking_number).and_return('19')
-        post_burial_forms
-      end
-    end
-  end
+  # /v0/preneeds/burial_forms specs already removed in https://github.com/department-of-veterans-affairs/vets-api/pull/18232
 
   describe '#send_confirmation_email' do
     subject { V0::Preneeds::BurialFormsController.new }
