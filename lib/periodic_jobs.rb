9debdcf8616551839488eee651cd9d5ab947d216
Samuel Stuckey <sam.n.stuckey@gmail.com>
2024-08-28 11:00:27 -0600

    [DBX-18184] split out 526 paranoid success polling

--- lib/periodic_jobs.rb
+++ lib/periodic_jobs.rb
@@ -41,7 +41,7 @@ PERIODIC_JOBS = lambda { |mgr|
   # Update Lighthouse526DocumentUpload statuses according to Lighthouse Benefits Documents service tracking
   mgr.register('0 3 * * *', 'Form526StatusPollingJob')
   # Updates status of FormSubmissions per call to Lighthouse Benefits Intake API
-  mgr.register('0 2 * * 0', 'Form526StatusPollingJob', 'args' => { paranoid: true })
+  mgr.register('0 2 * * 0', 'Form526ParanoidSuccessPollingJob')
   # Checks all 'success' type submissions in LH to ensure they haven't changed
   mgr.register('5 4 * * 7', 'Form526StateLoggingJob')
   # Log the state of Form 526 submissions to hydrate Datadog monitor
