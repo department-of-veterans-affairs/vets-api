6b0501e623893cde5194b0fcacdf94454f76353f
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-08-29 15:25:59 -0400

    reorganize travel_pay request specs

--- modules/travel_pay/spec/requests/travel_pay/pings_spec.rb
+++ modules/travel_pay/spec/requests/travel_pay/pings_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe TravelPay::PingsController, type: :request do
+RSpec.describe TravelPay::Pings, type: :request do
   let(:user) { build(:user) }
 
   before do
