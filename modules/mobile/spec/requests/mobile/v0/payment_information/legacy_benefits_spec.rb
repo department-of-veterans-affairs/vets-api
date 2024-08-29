1107141f59688d948b9fbb03d4da788f84c6a61c
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-08-29 10:00:46 -0400

    update rails helper reference payment_info

--- modules/mobile/spec/requests/mobile/v0/payment_information/legacy_benefits_spec.rb
+++ modules/mobile/spec/requests/mobile/v0/payment_information/legacy_benefits_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../support/helpers/rails_helper'
+require_relative '../../../../support/helpers/rails_helper'
 
 RSpec.describe 'legacy Mobile::V0::PaymentInformation::Benefits', type: :request do
   include JsonSchemaMatchers
