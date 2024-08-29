cd5a6b4e033f1e1eced09e26d840f18df7f72ab3
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-08-29 14:02:40 -0400

    rename my_health request specs

--- modules/my_health/spec/requests/my_health/v1/messaging/messages_spec.rb
+++ modules/my_health/spec/requests/my_health/v1/messaging/messages_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 require 'support/sm_client_helpers'
 require 'support/shared_examples_for_mhv'
 
-RSpec.describe 'Messages Integration', type: :request do
+RSpec.describe 'MyHealth::V1::Messaging::Messages', type: :request do
   include SM::ClientHelpers
   include SchemaMatchers
 
