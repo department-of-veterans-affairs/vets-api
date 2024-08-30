abf40741956d659e00475a4c32a39be69bb76ec9
Max Antonucci <maxx1128@gmail.com>
2024-08-30 13:39:23 -0400

    VA-90998: Add caching and metrics to VBS copayments

--- app/services/medical_copays/vbs/service.rb
+++ app/services/medical_copays/vbs/service.rb
@@ -43,7 +43,11 @@ module MedicalCopays
       def get_copays
         raise InvalidVBSRequestError, request_data.errors unless request_data.valid?
 
-        response = request.post("#{settings.base_path}/GetStatementsByEDIPIAndVistaAccountNumber", request_data.to_hash)
+        response = if Flipper.enabled?(:debts_cache_vbs_copays_empty_response)
+                     get_cached_copay_response
+                   else
+                     get_copay_response
+                   end
 
         # enable zero balance debt feature if flip is on
         if Flipper.enabled?(:medical_copays_zero_debt)
@@ -55,6 +59,26 @@ module MedicalCopays
         end
 
         ResponseData.build(response:).handle
+      end
+
+      def get_cached_copay_response
+        StatsD.increment("#{STATSD_KEY_PREFIX}.init_cached_copays.fired")
+
+        cached_response = get_user_cached_response
+        if cached_response
+          StatsD.increment("#{STATSD_KEY_PREFIX}.init_cached_copays.cached_response_returned")
+          return cached_response
+        end
+
+        response = get_copay_response
+        response_body = response.body
+
+        if response_body.is_a?(Array) && response_body.empty?
+          StatsD.increment("#{STATSD_KEY_PREFIX}.init_cached_copays.empty_response_cached")
+          Rails.cache.write("vbs_copays_data_#{user.uuid}", response, expires_in: time_until_5am_utc)
+        end
+
+        response
       end
 
       ##
@@ -91,12 +115,28 @@ module MedicalCopays
         raise e
       end
 
+      def get_copay_response
+        request.post("#{settings.base_path}/GetStatementsByEDIPIAndVistaAccountNumber", request_data.to_hash)
+      end
+
+      def get_user_cached_response
+        cache_key = "vbs_copays_data_#{user.uuid}"
+        Rails.cache.read(cache_key)
+      end
+
       def send_statement_notifications(statements_json_byte)
         CopayNotifications::ParseNewStatementsJob.perform_async(statements_json_byte)
       end
 
       def settings
         Flipper.enabled?(:medical_copays_api_key_change) ? Settings.mcp.vbs_v2 : Settings.mcp.vbs
+      end
+
+      def time_until_5am_utc
+        now = Time.now.utc
+        five_am_utc = Time.utc(now.year, now.month, now.day, 5)
+        five_am_utc += 1.day if now >= five_am_utc
+        five_am_utc - now
       end
     end
   end
