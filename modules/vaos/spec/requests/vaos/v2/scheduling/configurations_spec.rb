bb3db253f170b85c3c024e57ba5ee494ffdd7021
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-09-03 16:36:44 -0400

    rename a couple of files and example groups

--- modules/vaos/spec/requests/vaos/v2/scheduling/configurations_spec.rb
+++ modules/vaos/spec/requests/vaos/v2/scheduling/configurations_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe VAOS::V2::SchedulingController, :skip_mvi, type: :request do
+RSpec.describe 'VAOS::V2::Scheduling::Configurations', :skip_mvi, type: :request do
   include SchemaMatchers
 
   let(:inflection_header) { { 'X-Key-Inflection' => 'camel' } }
