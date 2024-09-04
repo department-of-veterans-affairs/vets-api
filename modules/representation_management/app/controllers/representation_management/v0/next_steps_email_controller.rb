0ba8b964ed96a622a4ae2b0fbe974a741bc47546
Josh Fike <josh.fike@oddball.io>
2024-09-04 13:42:47 -0500

    VA Notify - Next Steps Email - Appoint a Rep (#18194)
    * [WIP] Start endpoint for sending next steps email
    
    * [WIP] Add email address
    
    * [WIP] Tests for data model and controller
    
    * [WIP] Enqueing email
    
    * Add test to check for email job enqueueing
    
    * Remove debugging output
    
    * Add comment about inconsistent key names
    
    * Simplify attr_accessor
    
    * Add feature flag check
    
    * Add request check with feature disabled
    
    * Add some documentation
    
    * Address PR review feedback

---
+++ modules/representation_management/app/controllers/representation_management/v0/next_steps_email_controller.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module RepresentationManagement
+  module V0
+    class NextStepsEmailController < ApplicationController
+      service_tag 'representation-management'
+      skip_before_action :authenticate
+      before_action :feature_enabled
+
+      # Creates and enqueues an email with the provided "next steps" information. This action
+      # validates the input parameters and, if valid, queues an email using the VANotify service.
+      #
+      # @return [JSON] Returns a success message if the email is enqueued, otherwise returns validation errors.
+      #
+      def create
+        data = RepresentationManagement::NextStepsEmailData.new(next_steps_email_params)
+        if data.valid?
+          VANotify::EmailJob.perform_async(
+            data.email_address,
+            Settings.vanotify.services.va_gov.template_id.appoint_a_representative_confirmation_email,
+            {
+              # The first_name is the only key here that has an underscore.
+              # That is intentional.  All the keys here match the keys in the
+              # template.
+              'first_name' => data.first_name,
+              'form name' => data.form_name,
+              'form number' => data.form_number,
+              'representative type' => data.representative_type_humanized,
+              'representative name' => data.representative_name,
+              'representative address' => data.representative_address
+            }
+          )
+          render json: { message: 'Email enqueued' }, status: :ok
+        else
+          render json: { errors: data.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def feature_enabled
+        routing_error unless Flipper.enabled?(:appoint_a_representative_enable_pdf)
+      end
+
+      # Strong parameters method for sanitizing input data for the next steps email.
+      #
+      # @return [ActionController::Parameters] Filtered parameters permitted for the next steps email.
+      def next_steps_email_params
+        params.require(:next_steps_email).permit(
+          :email_address,
+          :first_name,
+          :form_name,
+          :form_number,
+          :representative_type,
+          :representative_name,
+          :representative_address
+        )
+      end
+    end
+  end
+end
