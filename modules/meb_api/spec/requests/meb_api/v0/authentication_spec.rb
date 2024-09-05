42e97a26ba7125c7b26d02e95b426c1a0c4e66a6
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-09-05 13:37:33 -0400

    move/rename files + update example group name with convention IVC and Meb (#18239)

--- modules/meb_api/spec/requests/meb_api/v0/authentication_spec.rb
+++ modules/meb_api/spec/requests/meb_api/v0/authentication_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-RSpec.describe MebApi::V0::BaseController, type: :request do
+RSpec.describe 'MebApi::V0 Authentication', type: :request do
   let(:user_details_loa3) do
     {
       first_name: 'Lara',
