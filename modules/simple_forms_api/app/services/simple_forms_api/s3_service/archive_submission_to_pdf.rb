cab5b3820b3be54cdf4541241dc476eef4fbe364
Jacob Penner <jacob.penner@adhocteam.us>
2024-09-06 11:22:41 -0400

    minor tweaks

--- modules/simple_forms_api/app/services/simple_forms_api/s3_service/archive_submission_to_pdf.rb
+++ modules/simple_forms_api/app/services/simple_forms_api/s3_service/archive_submission_to_pdf.rb
@@ -19,182 +19,178 @@
 # OPTION 2: Run without user groupings
 # ids.each { |id| ArchiveSubmissionToPdf.new(submission_id: id, parent_dir:).run }
 # this will just put each submission in a folder by it's id under the parent dir
-class ArchiveSubmissionToPdf
-  attr_reader :failures, :form_id, :include_json_archive, :include_text_archive,
-              :parent_dir, :quiet_pdf_failures, :quiet_upload_failures, :run_quiet,
-              :submission
+module SimpleFormsApi
+  module S3Service
+    class ArchiveSubmissionToPdf < SimpleFormsApi::S3Service::Utils
+      attr_reader :failures, :form_id, :include_json_archive, :include_text_archive,
+                  :parent_dir, :quiet_pdf_failures, :quiet_upload_failures, :run_quiet,
+                  :submission
 
-  VALID_VFF_FORMS = %w[
-    20-10206 20-10207 21-0845 21-0966 21-0972 21-10210
-    21-4138 21-4142 21P-0847 26-4555 40-0247 40-10007
-  ].freeze
+      VALID_VFF_FORMS = %w[
+        20-10206 20-10207 21-0845 21-0966 21-0972 21-10210
+        21-4138 21-4142 21P-0847 26-4555 40-0247 40-10007
+      ].freeze
 
-  def initialize(form_id: nil, submission_id: nil, submission: nil, **options)
-    defaults = default_options.merge(options)
+      def initialize(form_id: nil, submission_id: nil, submission: nil, **options)
+        defaults = default_options.merge(options)
 
-    @failures = []
-    @form_id = form_id
-    @submission = submission || FormSubmission.find(submission_id)
-    @parent_dir = defaults[:parent_dir]
-    @include_text_archive = defaults[:include_text_archive]
-    @include_json_archive = defaults[:include_json_archive]
-    @quiet_upload_failures = defaults[:quiet_upload_failures]
-    @quiet_pdf_failures = defaults[:quiet_pdf_failures]
-    @run_quiet = defaults[:run_quiet]
-  end
+        @failures = []
+        @form_id = form_id
+        @submission = submission || FormSubmission.find(submission_id)
+        @parent_dir = defaults[:parent_dir]
+        @include_text_archive = defaults[:include_text_archive]
+        @include_json_archive = defaults[:include_json_archive]
+        @quiet_upload_failures = defaults[:quiet_upload_failures]
+        @quiet_pdf_failures = defaults[:quiet_pdf_failures]
+        @run_quiet = defaults[:run_quiet]
+      end
 
-  def run
-    log_info("Processing submission ID: #{submission.id}")
-    process_submission_files
-    output_directory_path
-  rescue => e
-    handle_run_error(e)
-  end
+      def run
+        log_info("Processing submission ID: #{submission.id}")
+        process_submission_files
+        output_directory_path
+      rescue => e
+        handle_run_error(e)
+      end
 
-  private
+      private
 
-  def default_options
-    {
-      include_json_archive: true, # include the form data as a JSON object
-      include_text_archive: true, # include the form data as a text file
-      parent_dir: 'wipn8923-test',
-      quiet_pdf_failures: true, # skip PDF generation silently
-      quiet_upload_failures: true, # skip problematic uploads silently
-      run_quiet: true
-    }
-  end
+      def default_options
+        {
+          include_json_archive: true, # include the form data as a JSON object
+          include_text_archive: true, # include the form data as a text file
+          parent_dir: 'wipn8923-test',
+          quiet_pdf_failures: true, # skip PDF generation silently
+          quiet_upload_failures: true, # skip problematic uploads silently
+          run_quiet: true
+        }
+      end
 
-  def process_submission_files
-    write_pdf
-    write_as_json_archive if include_json_archive
-    write_as_text_archive if include_text_archive
-    write_user_uploads if user_uploads.present?
-    write_metadata
-  end
+      def process_submission_files
+        write_pdf
+        write_as_json_archive if include_json_archive
+        write_as_text_archive if include_text_archive
+        write_user_uploads if user_uploads.present?
+        write_metadata
+      end
 
-  def handle_run_error(error)
-    raise error unless run_quiet
+      def handle_run_error(error)
+        raise error unless run_quiet
 
-    failures << { id: submission.id, error: error.message }
-    log_error("Failed submission: #{submission.id}", error)
-  end
+        failures << { id: submission.id, error: error.message }
+        log_error("Failed submission: #{submission.id}", error)
+      end
 
-  def write_pdf
-    encoded_pdf = generate_pdf_content
-    save_file_to_s3("#{output_directory_path}/form.pdf", Base64.decode64(encoded_pdf))
-  rescue => e
-    quiet_pdf_failures ? write_pdf_error(e) : raise(e)
-  end
+      def write_pdf
+        encoded_pdf = generate_pdf_content
+        save_file_to_s3("#{output_directory_path}/form.pdf", Base64.decode64(encoded_pdf))
+      rescue => e
+        quiet_pdf_failures ? write_pdf_error(e) : raise(e)
+      end
 
-  def generate_pdf_content
-    service = EVSS::DisabilityCompensationForm::NonBreakeredService.new(submission.auth_headers)
-    service.get_form(form_json.to_json).body['pdf']
-  end
+      def generate_pdf_content
+        service = EVSS::DisabilityCompensationForm::NonBreakeredService.new(submission.auth_headers)
+        service.get_form(form_json.to_json).body['pdf']
+      end
 
-  def write_pdf_error(error)
-    log_error("PDF generation failed for submission: #{submission.id}", error)
-    save_file_to_s3("#{output_directory_path}/pdf_generating_failure.txt", error_details(error))
-  end
+      def write_pdf_error(error)
+        log_error("PDF generation failed for submission: #{submission.id}", error)
+        save_file_to_s3("#{output_directory_path}/pdf_generating_failure.txt", error_details(error))
+      end
 
-  def error_details(error)
-    "#{error.message}\n\n#{error.backtrace.join("\n")}"
-  end
+      def error_details(error)
+        "#{error.message}\n\n#{error.backtrace.join("\n")}"
+      end
 
-  def write_as_json_archive
-    save_file_to_s3("#{output_directory_path}/form_text_archive.json", JSON.pretty_generate(form_json))
-  end
+      def write_as_json_archive
+        save_file_to_s3("#{output_directory_path}/form_text_archive.json", JSON.pretty_generate(form_json))
+      end
 
-  def write_as_text_archive
-    save_file_to_s3("#{output_directory_path}/form_text_archive.txt", form_text_archive.to_json)
-  end
+      def write_as_text_archive
+        save_file_to_s3("#{output_directory_path}/form_text_archive.txt", form_text_archive.to_json)
+      end
 
-  def write_metadata
-    save_file_to_s3("#{output_directory_path}/metadata.json", metadata.to_json)
-  end
+      def write_metadata
+        save_file_to_s3("#{output_directory_path}/metadata.json", metadata.to_json)
+      end
 
-  def write_user_uploads
-    log_info("Moving #{user_uploads.count} user uploads")
-    user_uploads.each { |upload| process_user_upload(upload) }
-    write_failure_report if user_upload_failures.present?
-  rescue => e
-    handle_upload_error(e)
-  end
+      def write_user_uploads
+        log_info("Moving #{user_uploads.count} user uploads")
+        user_uploads.each { |upload| process_user_upload(upload) }
+        write_failure_report if user_upload_failures.present?
+      rescue => e
+        handle_upload_error(e)
+      end
 
-  def process_user_upload(upload)
-    log_info("Processing upload: #{upload['name']} - #{upload['confirmationCode']}")
-    local_file = SupportingEvidenceAttachment.find_by(guid: upload['confirmationCode'])
-    raise 'Local record not found' unless local_file
+      def process_user_upload(upload)
+        log_info("Processing upload: #{upload['name']} - #{upload['confirmationCode']}")
+        local_file = SupportingEvidenceAttachment.find_by(guid: upload['confirmationCode'])
+        raise 'Local record not found' unless local_file
 
-    copy_file_between_buckets(local_file)
-  end
+        copy_file_between_buckets(local_file)
+      end
 
-  def copy_file_between_buckets(local_file)
-    source_obj = s3_resource.bucket(local_file.get_file.uploader.aws_bucket).object(local_file.get_file.path)
-    target_obj = s3_resource.bucket(target_bucket).object("#{user_upload_path}/#{local_file.get_file.filename}")
-    target_obj.copy_from(source_obj)
-  end
+      def copy_file_between_buckets(local_file)
+        source_obj = s3_resource.bucket(local_file.get_file.uploader.aws_bucket).object(local_file.get_file.path)
+        target_obj = s3_resource.bucket(target_bucket).object("#{user_upload_path}/#{local_file.get_file.filename}")
+        target_obj.copy_from(source_obj)
+      end
 
-  def write_failure_report
-    save_file_to_s3("#{output_directory_path}/user_upload_failures.txt", JSON.pretty_generate(user_upload_failures))
-  end
+      def write_failure_report
+        save_file_to_s3("#{output_directory_path}/user_upload_failures.txt", JSON.pretty_generate(user_upload_failures))
+      end
 
-  def save_file_to_s3(path, content)
-    s3_resource.bucket(target_bucket).object(path).put(body: content)
-  end
+      def save_file_to_s3(path, content)
+        s3_resource.bucket(target_bucket).object(path).put(body: content)
+      end
 
-  def s3_resource
-    @s3_resource ||= Reports::Uploader.new_s3_resource
-  end
+      def form_json
+        @form_json ||= JSON.parse(submission.form_json)[form_id]
+      end
 
-  def target_bucket
-    @target_bucket ||= Reports::Uploader.s3_bucket
-  end
+      def form_text_archive
+        submission.form.tap do |form|
+          form[form_id]['claimDate'] ||= submission.created_at.iso8601
+        end
+      end
 
-  def form_json
-    @form_json ||= JSON.parse(submission.form_json)[form_id]
-  end
+      def metadata
+        return {} unless submission.auth_headers.present? && submission.form[form_id].present?
 
-  def form_text_archive
-    form = submission.form
-    form[form_id]['claimDate'] ||= submission.created_at.iso8601
-    form
-  end
+        extract_metadata_from_submission
+      end
 
-  def metadata
-    return {} unless submission.auth_headers.present? && submission.form[form_id].present?
+      def extract_metadata_from_submission
+        address = submission.form.dig(form_id, 'veteran', 'currentMailingAddress')
+        zip = [address['zipFirstFive'], address['zipLastFour']].join('-') if address.present?
+        pii = JSON.parse(submission.auth_headers['va_eauth_authorization'])['authorizationResponse']
+        pii.merge({
+                    fileNumber: pii['va_eauth_pnid'],
+                    zipCode: zip || '00000',
+                    claimDate: submission.created_at.iso8601,
+                    formsIncluded: map_form_inclusion
+                  })
+      end
 
-    extract_metadata_from_submission
-  end
+      def map_form_inclusion
+        VALID_VFF_FORMS.select { |type| submission.form[type].present? }
+      end
 
-  def extract_metadata_from_submission
-    address = submission.form.dig(form_id, 'veteran', 'currentMailingAddress')
-    zip = [address['zipFirstFive'], address['zipLastFour']].join('-') if address.present?
-    pii = JSON.parse(submission.auth_headers['va_eauth_authorization'])['authorizationResponse']
-    pii.merge({
-                fileNumber: pii['va_eauth_pnid'],
-                zipCode: zip || '00000',
-                claimDate: submission.created_at.iso8601,
-                formsIncluded: map_form_inclusion
-              })
-  end
+      def output_directory_path
+        @output_directory_path ||= "#{parent_dir}/#{submission.id}"
+      end
 
-  def map_form_inclusion
-    VALID_VFF_FORMS.select { |type| submission.form[type].present? }
-  end
+      def user_uploads
+        @user_uploads ||= submission.form['form_uploads']
+      end
 
-  def output_directory_path
-    @output_directory_path ||= "#{parent_dir}/#{submission.id}"
-  end
+      def user_upload_failures
+        @user_upload_failures ||= []
+      end
 
-  def user_uploads
-    @user_uploads ||= submission.form['form_uploads']
-  end
-
-  def user_upload_failures
-    @user_upload_failures ||= []
-  end
-
-  def user_upload_path
-    @user_upload_path ||= "#{output_directory_path}/user_uploads"
+      def user_upload_path
+        @user_upload_path ||= "#{output_directory_path}/user_uploads"
+      end
+    end
   end
 end
