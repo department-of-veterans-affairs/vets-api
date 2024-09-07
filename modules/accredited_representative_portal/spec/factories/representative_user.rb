f2414aa7473a5b3332d8363d1c54eb0eac16522b
Oren Mittman <nihil2501@gmail.com>
2024-09-06 21:14:59 -0400

    [ARP-86023] ARP IPF: Alphabetize user test factory attributes.

--- modules/accredited_representative_portal/spec/factories/representative_user.rb
+++ modules/accredited_representative_portal/spec/factories/representative_user.rb
@@ -2,20 +2,22 @@
 
 FactoryBot.define do
   factory :representative_user, class: 'AccreditedRepresentativePortal::RepresentativeUser' do
-    uuid { SecureRandom.uuid }
+    authn_context { LOA::IDME_LOA3_VETS }
     email { Faker::Internet.email }
+    fingerprint { Faker::Internet.ip_v4_address }
     first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
     icn { '123498767V234859' }
     idme_uuid { SecureRandom.uuid }
-    logingov_uuid { SecureRandom.uuid }
-    fingerprint { Faker::Internet.ip_v4_address }
+    last_name { Faker::Name.last_name }
     last_signed_in { Time.zone.now }
-    authn_context { LOA::IDME_LOA3_VETS }
     loa { { current: LOA::THREE, highest: LOA::THREE } }
+    logingov_uuid { SecureRandom.uuid }
     sign_in {
-      { service_name: SignIn::Constants::Auth::IDME, client_id: SecureRandom.uuid,
-        auth_broker: SignIn::Constants::Auth::BROKER_CODE }
+      {
+        service_name: SignIn::Constants::Auth::IDME, client_id: SecureRandom.uuid,
+        auth_broker: SignIn::Constants::Auth::BROKER_CODE
+      }
     }
+    uuid { SecureRandom.uuid }
   end
 end
