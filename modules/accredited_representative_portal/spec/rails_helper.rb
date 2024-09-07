b74bfeae0a780b373dc931e9b581ccabcac82250
Oren Mittman <nihil2501@gmail.com>
2024-09-06 21:14:59 -0400

    [ARP-86023] ARP IPF: Expose ARP `parsed_response` spec helper.

--- modules/accredited_representative_portal/spec/rails_helper.rb
+++ modules/accredited_representative_portal/spec/rails_helper.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'rails_helper'
+require_relative 'spec_helper'
 
 require File.expand_path('../../../config/environment', __dir__)
+
+module AccreditedRepresentativePortal
+  module RequestHelper
+    def parsed_response
+      JSON.parse(response.body)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include AccreditedRepresentativePortal::RequestHelper, type: :request
+end
