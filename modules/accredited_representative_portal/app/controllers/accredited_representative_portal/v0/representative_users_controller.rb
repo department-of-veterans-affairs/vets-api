62ef81551c6e04a7b806c3d7154f555594c190b0
athifw <athif@u.northwestern.edu>
2024-09-06 21:14:59 -0400

    Fix for linting

--- modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/representative_users_controller.rb
+++ modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/representative_users_controller.rb
@@ -4,7 +4,8 @@ module AccreditedRepresentativePortal
   module V0
     class RepresentativeUsersController < ApplicationController
       def show
-        # TODO: Once we figure out how we're handling serialization and which library we're using, moving this serialization logic out to to a serialization layer.
+        # TODO: Once we figure out how we're handling serialization and which library we're using,
+        # moving this serialization logic out to to a serialization layer.
         render json: {
           account: {
             account_uuid: @current_user.uuid
