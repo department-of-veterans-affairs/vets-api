4989fc134a5336e0d1efa19fefb3f6da2b34acfe
Richard Davis <richard.davis@agile6.com>
2024-09-09 15:37:07 -0400

    Use nullable syntax

--- app/swagger/swagger/schemas/contacts.rb
+++ app/swagger/swagger/schemas/contacts.rb
@@ -26,16 +26,16 @@ module Swagger::Schemas
         type: :string,
         enum: VAProfile::Models::AssociatedPerson::PERSONAL_HEALTH_CARE_CONTACT_TYPES
       )
-      property :given_name, type: %i[string null]
-      property :family_name, type: %i[string null]
-      property :relationship, type: %i[string null]
-      property :address_line1, type: %i[string null]
-      property :address_line2, type: %i[string null]
-      property :address_line3, type: %i[string null]
-      property :city, type: %i[string null]
-      property :state, type: %i[string null]
-      property :zip_code, type: %i[string null]
-      property :primary_phone, type: %i[string null]
+      property :given_name, type: :string
+      property :family_name, type: :string
+      property :relationship, type: :string, nullable: true
+      property :address_line1, type: :string, nullable: true
+      property :address_line2, type: :string, nullable: true
+      property :address_line3, type: :string, nullable: true
+      property :city, type: :string, nullable: true
+      property :state, type: :string, nullable: true
+      property :zip_code, type: :string, nullable: true
+      property :primary_phone, type: :string
     end
   end
 end
