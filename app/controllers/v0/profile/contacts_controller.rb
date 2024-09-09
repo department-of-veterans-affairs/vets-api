1ec3ea3a98d8d82a163923c6fb075ffcffc6f9dd
Richard Davis <richard.davis@agile6.com>
2024-09-09 10:27:44 -0400

    Adds feature toggle, controller methods, and routes

--- app/controllers/v0/profile/contacts_controller.rb
+++ app/controllers/v0/profile/contacts_controller.rb
@@ -14,10 +14,29 @@ module V0
         render json: ContactSerializer.new(response.contacts), status: response.status
       end
 
+      # POST /v0/profile/contacts
+      def create
+        raise routing_error unless feature_enabled?
+      end
+
+      # PUT/PATCH /v0/profile/contacts
+      def update
+        raise routing_error unless feature_enabled?
+      end
+
+      # DELETE /v0/profile/contacts
+      def destroy
+        raise routing_error unless feature_enabled?
+      end
+
       private
 
       def service
         VAProfile::Profile::V3::Service.new(current_user)
+      end
+
+      def feature_enabled?
+        Flipper.enable?(:profile_contacts_create_update_delete_enabled, current_user)
       end
     end
   end
