22bd8827531a543c268d2e0b28a562ffa6d44f07
Liz Townsend <liz.townsend@adhocteam.us>
2024-09-10 13:55:36 -0500

    remove mock data option from token service

--- modules/travel_pay/app/services/travel_pay/token_service.rb
+++ modules/travel_pay/app/services/travel_pay/token_service.rb
@@ -3,9 +3,7 @@
 module TravelPay
   class TokenService
     #
-    # GET TOKENS
     # returns a hash containing the veis_token & btsss_token
-    ##
     #
     def get_tokens(current_user)
       veis_token = token_client.request_veis_token
@@ -15,13 +13,6 @@ module TravelPay
     end
 
     private
-
-    ##
-    # Syntactic sugar for determining if the client should use
-    # fake api responses or actually connect to the BTSSS API
-    def mock_enabled?
-      Settings.travel_pay.mock
-    end
 
     def token_client
       TravelPay::TokenClient.new
