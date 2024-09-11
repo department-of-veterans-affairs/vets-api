06729e39dd6f0027e96566d32d66ca371d4969d2
Kevin Duensing <kevin.duensing@va.gov>
2024-09-11 11:51:17 -0400

    Add swagger doc for new endpoint

--- app/swagger/swagger/requests/travel_pay.rb
+++ app/swagger/swagger/requests/travel_pay.rb
@@ -16,10 +16,46 @@ module Swagger
 
           parameter :authorization
 
+          parameter do
+            key :in, :path
+            key :name, :id
+            key :pattern, ''
+            key :description, 'The non-PII/PHI id of a claim'
+            key :required, false
+          end
+
           response 200 do
             key :description, 'Successfully retrieved claims for a user'
             schema do
               key :$ref, :TravelPayClaims
+            end
+          end
+        end
+      end
+
+      swagger_path '/travel_pay/claims/{id}' do
+        operation :get do
+          extend Swagger::Responses::AuthenticationError
+          extend Swagger::Responses::BadRequestError
+
+          key :description, 'Get a single travel reimbursment claim summary'
+          key :operationId, 'getTravelPayClaimById'
+          key :tags, %w[travel_pay]
+
+          parameter :authorization
+
+          parameter do
+            key :in, :path
+            key :name, :id
+            key :pattern, '^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[4][0-9A-Fa-f]{3}-[89ABab][0-9A-Fa-f]{3}-[0-9A-Fa-f]{12}$'
+            key :description, 'The non-PII/PHI id of a claim'
+            key :required, false
+          end
+
+          response 200 do
+            key :description, 'Successfully retrieved claim for a user'
+            schema do
+              key :$ref, :TravelPayClaimSummary
             end
           end
         end
