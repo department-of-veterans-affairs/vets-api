3515a13a8872e60f0a87a4d6f1166f335411c20f
Wayne Weibel <wayne.weibel@adhocteam.us>
2024-09-12 17:30:05 -0400

    statsd

--- app/sidekiq/benefits_intake_remediation_status_job.rb
+++ app/sidekiq/benefits_intake_remediation_status_job.rb
@@ -24,15 +24,19 @@ class BenefitsIntakeRemediationStatusJob
   def perform
     Rails.logger.info('BenefitsIntakeRemediationStatusJob started')
 
+    form_submissions = FormSubmission.includes(:form_submission_attempts)
+    submissions = form_submissions.all.group_by { |fs| fs.saved_claim_id }
+    failures = submissions.map { |claim_id, fs|
+      fs.sort_by! { |_fs| _fs.created_at }
+      attempts = fs.map(&:form_submission_attempts).flatten.sort_by { |_att| _att.created_at }
+      vbms = attempts.find { |att| att.aasm_state == 'vbms' }
+      pending = attempts.find { |att| att.aasm_state == 'pending' }
+      !(vbms || pending) ? fs.last : nil
+    }.compact
+
+    batch_process(failures) unless failures.empty?
+
     record_unsubmitted_and_orphaned
-
-    submissions = form_submissions.group_by { |fs| fs.saved_claim_id }
-    submissions.each { |claim_id, fs|
-      attempts = fs.map(&:form_submission_attempts).flatten.sort_by! { |a| a.created_at }
-      submissions[claim_id] = (attempts.select { |att| att.aasm_state == 'vbms' }).empty? ? attempts : nil
-    }.compact!
-
-    batch_process(submissions)
 
     Rails.logger.info('BenefitsIntakeRemediationStatusJob ended', total_handled:)
   end
@@ -42,30 +46,40 @@ class BenefitsIntakeRemediationStatusJob
   attr_reader :batch_size
   attr_accessor :total_handled
 
-  def form_submissions
-    @form_submissions ||= FormSubmission.includes(:form_submission_attempts)
-  end
-
   def record_unsubmitted_and_orphaned
-    form_submission_groups = form_submissions.all.group_by { |fs| fs.form_type }
+    form_submission_groups = FormSubmission.all.group_by { |fs| fs.form_type }
 
     form_submission_groups.each do |form_id, submissions|
       fs_saved_claim_ids = submissions.map(&:saved_claim_id).uniq
 
-      claims = SavedClaim.where(form_id:).where('saved_claims.created_at > ?', submissions.minimum(:created_at))
+      claims = SavedClaim.where(form_id:).where('created_at >= ?', submissions.minimum(:created_at))
       claim_ids = claims.map(&:id).uniq
 
-      puts "UNSUBMITTED CLAIMS: #{claim_ids - fs_saved_claim_ids}"
-      puts "ORPHAN SUBMISSION: #{fs_saved_claim_ids - claim_ids}"
-      # log metrics/list
+      unsubmitted = claim_ids - fs_saved_claim_ids
+      orphaned = fs_saved_claim_ids - claim_ids
+
+      failures = submissions.group_by { |fs| fs.saved_claim_id }
+      failures.reject! { |claim_id, fs|
+        attempts = fs.map(&:form_submission_attempts).flatten.sort_by { |_att| _att.created_at }
+        vbms = attempts.find { |att| att.aasm_state == 'vbms' }
+        pending = attempts.find { |att| att.aasm_state == 'pending' }
+        vbms || pending
+      }
+
+      StatsD.set("#{STATS_KEY}.#{form_id}.unsubmitted_claims", unsubmitted.length)
+      StatsD.set("#{STATS_KEY}.#{form_id}.orphaned_submissions", orphaned.length)
+      StatsD.set("#{STATS_KEY}.#{form_id}.outstanding_failures", failures.length)
+      Rails.logger.info("BenefitsIntakeRemediationStatusJob submission audit #{form_id}", form_id:, unsubmitted:, orphaned:, failures:)
     end
   end
 
-  def batch_process(submissions)
+  def batch_process(failures)
     intake_service = BenefitsIntake::Service.new
 
-    submissions.each_slice(batch_size) do |batch|
+    failures.each_slice(batch_size) do |batch|
       batch_uuids = batch.map(&:benefits_intake_uuid)
+      Rails.logger.info('BenefitsIntakeRemediationStatusJob processing batch', batch_uuids:)
+
       response = intake_service.bulk_status(uuids: batch_uuids)
       raise response.body unless response.success?
 
@@ -74,47 +88,28 @@ class BenefitsIntakeRemediationStatusJob
       handle_response(data, batch)
     end
   rescue => e
-    Rails.logger.error('Error processing Intake Status batch', class: self.class.name, message: e.message)
+    Rails.logger.error('BenefitsIntakeRemediationStatusJob ERROR processing batch', class: self.class.name, message: e.message)
   end
 
-  # rubocop:disable Metrics/MethodLength
-  def handle_response(response_data, form_submissions)
+  def handle_response(response_data, failure_batch)
     response_data.each do |submission|
       uuid = submission['id']
-      form_submission = form_submissions.find do |submission_from_db|
+      form_submission = failure_batch.find do |submission_from_db|
         submission_from_db.benefits_intake_uuid == uuid
       end
       form_id = form_submission.form_type
 
-      form_submission_attempt = form_submission.latest_pending_attempt
+      form_submission_attempt = form_submission.form_submission_attempts.last
 
       # https://developer.va.gov/explore/api/benefits-intake/docs
       status = submission.dig('attributes', 'status')
-      if %w[error expired].include?(status)
-        # Error - Indicates that there was an error. Refer to the error code and detail for further information.
-        # Expired - Indicate that documents were not successfully uploaded within the 15-minute window.
-        form_submission_attempt.fail!
-        log_result('failure', form_id, uuid, time_to_transition)
-      elsif status == 'vbms'
+      if status == 'vbms'
         # submission was successfully uploaded into a Veteran's eFolder within VBMS
-        form_submission_attempt.vbms!
-        log_result('success', form_id, uuid, time_to_transition)
-      elsif time_to_transition > STALE_SLA.days
-        # exceeds SLA (service level agreement) days for submission completion
-        log_result('stale', form_id, uuid, time_to_transition)
-      else
-        # no change being tracked
-        log_result('pending', form_id, uuid)
+        form_submission_attempt.remediate!
       end
 
-      total_handled + 1
+      total_handled += 1
     end
   end
-  # rubocop:enable Metrics/MethodLength
 
-  def log_result(result, form_id, uuid, time_to_transition = nil)
-    StatsD.increment("#{STATS_KEY}.#{form_id}.#{result}")
-    StatsD.increment("#{STATS_KEY}.all_forms.#{result}")
-    Rails.logger.info('BenefitsIntakeRemediationStatusJob', result:, form_id:, uuid:, time_to_transition:)
-  end
 end
