16691e0508fefb669b774c11433b234f6e5b1a54
Wayne Weibel <wayne.weibel@adhocteam.us>
2024-09-13 12:22:24 -0400

    model file, job working, lint

--- app/sidekiq/benefits_intake_remediation_status_job.rb
+++ app/sidekiq/benefits_intake_remediation_status_job.rb
@@ -15,28 +15,15 @@ class BenefitsIntakeRemediationStatusJob
     @total_handled = 0
   end
 
-  def debug(msg)
-    puts
-    puts "#{msg}"
-    puts
-  end
-
   def perform
     Rails.logger.info('BenefitsIntakeRemediationStatusJob started')
 
     form_submissions = FormSubmission.includes(:form_submission_attempts)
-    submissions = form_submissions.all.group_by { |fs| fs.saved_claim_id }
-    failures = submissions.map { |claim_id, fs|
-      fs.sort_by! { |_fs| _fs.created_at }
-      attempts = fs.map(&:form_submission_attempts).flatten.sort_by { |_att| _att.created_at }
-      vbms = attempts.find { |att| att.aasm_state == 'vbms' }
-      pending = attempts.find { |att| att.aasm_state == 'pending' }
-      !(vbms || pending) ? fs.last : nil
-    }.compact
+    failures = outstanding_failures(form_submissions.all)
 
     batch_process(failures) unless failures.empty?
 
-    record_unsubmitted_and_orphaned
+    submission_audit
 
     Rails.logger.info('BenefitsIntakeRemediationStatusJob ended', total_handled:)
   end
@@ -46,31 +33,14 @@ class BenefitsIntakeRemediationStatusJob
   attr_reader :batch_size
   attr_accessor :total_handled
 
-  def record_unsubmitted_and_orphaned
-    form_submission_groups = FormSubmission.all.group_by { |fs| fs.form_type }
-
-    form_submission_groups.each do |form_id, submissions|
-      fs_saved_claim_ids = submissions.map(&:saved_claim_id).uniq
-
-      claims = SavedClaim.where(form_id:).where('created_at >= ?', submissions.minimum(:created_at))
-      claim_ids = claims.map(&:id).uniq
-
-      unsubmitted = claim_ids - fs_saved_claim_ids
-      orphaned = fs_saved_claim_ids - claim_ids
-
-      failures = submissions.group_by { |fs| fs.saved_claim_id }
-      failures.reject! { |claim_id, fs|
-        attempts = fs.map(&:form_submission_attempts).flatten.sort_by { |_att| _att.created_at }
-        vbms = attempts.find { |att| att.aasm_state == 'vbms' }
-        pending = attempts.find { |att| att.aasm_state == 'pending' }
-        vbms || pending
-      }
-
-      StatsD.set("#{STATS_KEY}.#{form_id}.unsubmitted_claims", unsubmitted.length)
-      StatsD.set("#{STATS_KEY}.#{form_id}.orphaned_submissions", orphaned.length)
-      StatsD.set("#{STATS_KEY}.#{form_id}.outstanding_failures", failures.length)
-      Rails.logger.info("BenefitsIntakeRemediationStatusJob submission audit #{form_id}", form_id:, unsubmitted:, orphaned:, failures:)
-    end
+  def outstanding_failures(submissions)
+    failures = submissions.group_by(&:saved_claim_id)
+    failures.map do |_claim_id, fs|
+      fs.sort_by!(&:created_at)
+      attempts = fs.map(&:form_submission_attempts).flatten.sort_by(&:created_at)
+      not_failure = attempts.find { |att| att.aasm_state != 'failure' }
+      not_failure ? nil : fs.last
+    end.compact
   end
 
   def batch_process(failures)
@@ -83,12 +53,13 @@ class BenefitsIntakeRemediationStatusJob
       response = intake_service.bulk_status(uuids: batch_uuids)
       raise response.body unless response.success?
 
-      next unless data = response.body['data']
+      next unless (data = response.body['data'])
 
       handle_response(data, batch)
     end
   rescue => e
-    Rails.logger.error('BenefitsIntakeRemediationStatusJob ERROR processing batch', class: self.class.name, message: e.message)
+    Rails.logger.error('BenefitsIntakeRemediationStatusJob ERROR processing batch', class: self.class.name,
+                                                                                    message: e.message)
   end
 
   def handle_response(response_data, failure_batch)
@@ -97,19 +68,48 @@ class BenefitsIntakeRemediationStatusJob
       form_submission = failure_batch.find do |submission_from_db|
         submission_from_db.benefits_intake_uuid == uuid
       end
-      form_id = form_submission.form_type
+      form_submission.form_type
 
       form_submission_attempt = form_submission.form_submission_attempts.last
 
       # https://developer.va.gov/explore/api/benefits-intake/docs
       status = submission.dig('attributes', 'status')
+      lighthouse_updated_at = submission.dig('attributes', 'updated_at')
       if status == 'vbms'
         # submission was successfully uploaded into a Veteran's eFolder within VBMS
+        form_submission_attempt.update(lighthouse_updated_at:)
         form_submission_attempt.remediate!
       end
 
-      total_handled += 1
+      total_handled + 1
     end
   end
 
+  def submission_audit
+    form_submissions = FormSubmission.includes(:form_submission_attempts)
+    form_submission_groups = form_submissions.all.group_by(&:form_type)
+
+    form_submission_groups.each do |form_id, submissions|
+      fs_saved_claim_ids = submissions.map(&:saved_claim_id).uniq
+
+      claims = SavedClaim.where(form_id:).where('id >= ?', fs_saved_claim_ids.min)
+      claim_ids = claims.map(&:id).uniq
+
+      unsubmitted = claim_ids - fs_saved_claim_ids
+      orphaned = fs_saved_claim_ids - claim_ids
+
+      failures = outstanding_failures(submissions)
+      failures = failures.group_by(&:saved_claim_id)
+      failures.each do |claim_id, fs|
+        last_attempt = fs.form_submission_attempts.max_by(&:created_at)
+        failures[claim_id] = [fs.benefits_intake_uuid, last_attempt.error_message]
+      end
+
+      StatsD.set("#{STATS_KEY}.#{form_id}.unsubmitted_claims", unsubmitted.length)
+      StatsD.set("#{STATS_KEY}.#{form_id}.orphaned_submissions", orphaned.length)
+      StatsD.set("#{STATS_KEY}.#{form_id}.outstanding_failures", failures.length)
+      Rails.logger.info("BenefitsIntakeRemediationStatusJob submission audit #{form_id}", form_id:, unsubmitted:,
+                                                                                          orphaned:, failures:)
+    end
+  end
 end
