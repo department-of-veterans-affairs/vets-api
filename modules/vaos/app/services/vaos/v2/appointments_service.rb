f31ae0d942436392b26e76fbf74dd31d3c8b8e6d
Jun Tao Luo <john.luo@agile6.com>
2024-09-13 10:52:36 -0400

    Add unit tests and fixup behavior

--- modules/vaos/app/services/vaos/v2/appointments_service.rb
+++ modules/vaos/app/services/vaos/v2/appointments_service.rb
@@ -180,27 +180,30 @@ module VAOS
 
       private
 
-      def parse_possible_token_related_errors(e)
+      def parse_possible_token_related_errors(e) # rubocop:disable Metrics/MethodLength
         prefix = 'VAOS::V2::AppointmentService#get_appointments'
         sanitized_icn = VAOS::Anonymizers.anonymize_icns(user.icn)
         sanitized_message = VAOS::Anonymizers.anonymize_icns(e.message)
         case e
-        when Common::Client::Errors::ClientError
-          status = e.status
-          context = { error: e.body.present? ? e.body['error'] : '' }
-          message = "#{config.logging_prefix} token failed, status: #{status}"
-          Rails.logger.error("#{prefix} #{message}", status:, icn: sanitized_icn, context:)
-          return { message:, status:, icn: sanitized_icn, context: }
         when Common::Client::Errors::ParsingError
           Rails.logger.warn("#{prefix} token failed, parsing error", icn: sanitized_icn, context: sanitized_message)
+          sanitized_message
         when Common::Exceptions::GatewayTimeout
           Rails.logger.warn("#{prefix} token failed, gateway timeout", icn: sanitized_icn)
+          sanitized_message
         when MAP::SecurityToken::Errors::ApplicationMismatchError
           Rails.logger.warn("#{prefix} application mismatch", icn: sanitized_icn, context: sanitized_message)
+          sanitized_message
         when MAP::SecurityToken::Errors::MissingICNError
           Rails.logger.warn("#{prefix} missing ICN")
+          sanitized_message
+        when Common::Client::Errors::ClientError
+          status = e.status
+          context = e.body
+          message = "#{prefix} token failed, status: #{status}"
+          Rails.logger.warn(message.to_s, status:, icn: sanitized_icn, context:)
+          { message:, status:, icn: sanitized_icn, context: }
         end
-        sanitized_message
       end
 
       # Modifies the appointment, extracting individual fields from the appointment. This currently includes:
