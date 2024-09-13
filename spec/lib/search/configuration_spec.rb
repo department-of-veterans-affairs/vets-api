5e04c47af31d9668cce581c6956a64673705ce4d
Bryan Thompson <bryan.thompson3@va.gov>
2024-09-13 12:31:43 -0700

    use updated search url when flipper is enabled

--- spec/lib/search/configuration_spec.rb
+++ spec/lib/search/configuration_spec.rb
@@ -8,4 +8,36 @@ describe Search::Configuration do
       expect(described_class.instance.service_name).to eq('Search/Results')
     end
   end
+
+  describe '#base_path' do
+    context 'search_use_v2_gsa Flipper is enabled' do
+      before do
+        Flipper.enable(:search_use_v2_gsa)
+      end
+
+      it 'provides api.gsa.gov search URL' do
+        expect(described_class.instance.base_path).to eq('https://api.gsa.gov/technology/searchgov/v2/results/i14y')
+      end
+    end
+
+    context 'search_use_v2_gsa Flipper is disabled' do
+      before do
+        Flipper.disable(:search_use_v2_gsa)
+      end
+
+      it 'provides search.usa.gov search URL' do
+        expect(described_class.instance.base_path).to eq('https://search.usa.gov/api/v2/search/i14y')
+      end
+    end
+
+    context 'Flipper raises a ActiveRecord::NoDatabaseError' do
+      before do
+        expect(Flipper).to receive(:enabled?).and_return(ActiveRecord::NoDatabaseError)
+      end
+
+      it 'provides search.usa.gov search URL' do
+        expect(described_class.instance.base_path).to eq('https://search.usa.gov/api/v2/search/i14y')
+      end
+    end
+  end
 end
