398838473f72f87737cd5a920d69c0d8d6c75db9
s-caso <sebastian.caso@accenturefederal.com>
2024-09-13 13:21:16 -0400

    lint fix

--- app/models/form_profiles/va_1990ez.rb
+++ app/models/form_profiles/va_1990ez.rb
@@ -4,7 +4,7 @@ class FormProfiles::VA1990ez < FormProfile
   def return_url
     if Flipper.enabled?(:meb_1606_30_automation)
       '/benefit-selection'
-    else 
+    else
       '/applicant-information/personal-information'
     end
   end
