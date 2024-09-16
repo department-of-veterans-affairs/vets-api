f645471ee463f0c429310921471861e816721207
Liz Townsend <liz.townsend@adhocteam.us>
2024-09-16 10:50:20 -0500

    add error handling, save only veis token if btsss call fails

--- modules/travel_pay/app/services/travel_pay/token_service.rb
+++ modules/travel_pay/app/services/travel_pay/token_service.rb
@@ -9,21 +9,21 @@ module TravelPay
     #
     def get_tokens(current_user)
       cached = cached_by_account_uuid(current_user.account_uuid)
-      if cached
+      # Check that btsss token exists, if so, veis token must also be present
+      if cached&.btsss_token
         Rails.logger.info('BTSSS tokens retrieved from cache',
                           { request_id: RequestStore.store['request_id'] })
         cached.tokens
+      elsif cached&.veis_token
+        # if the btsss token isn't cached, check whether or not the veis token is there
+        Rails.logger.info('BTSSS token not cached, requesting new token with existing VEIS token',
+                          { request_id: RequestStore.store['request_id'] })
+        request_btsss_token(cached.tokens[:veis_token], current_user)
       else
         Rails.logger.info('BTSSS tokens not cached, requesting new tokens',
                           { request_id: RequestStore.store['request_id'] })
 
-        veis_token = token_client.request_veis_token
-        btsss_token = token_client.request_btsss_token(veis_token, current_user)
-        save_tokens!(current_user.account_uuid, { veis_token:, btsss_token: })
-        Rails.logger.info('BTSSS tokens saved to cache',
-                          { request_id: RequestStore.store['request_id'] })
-
-        { veis_token:, btsss_token: }
+        request_new_tokens(current_user)
       end
     end
 
@@ -39,6 +39,33 @@ module TravelPay
         tokens:
       )
       token_store.save
+    end
+
+    def request_new_tokens(current_user)
+      begin
+        veis_token = token_client.request_veis_token
+      rescue Faraday::Error => e
+        TravelPay::ServiceError.raise_mapped_error(e)
+      end
+      request_btsss_token(veis_token, current_user)
+    end
+
+    def request_btsss_token(veis_token, current_user)
+      begin
+        btsss_token = token_client.request_btsss_token(veis_token, current_user)
+      rescue Faraday::Error => e
+        Rails.logger.info('BTSSS token could not be retrieved, saving VEIS token only',
+                          { request_id: RequestStore.store['request_id'] })
+        save_tokens!(current_user.account_uuid, { veis_token:, btsss_token: nil })
+
+        TravelPay::ServiceError.raise_mapped_error(e)
+      end
+
+      save_tokens!(current_user.account_uuid, { veis_token:, btsss_token: })
+      Rails.logger.info('BTSSS tokens saved to cache',
+                        { request_id: RequestStore.store['request_id'] })
+
+      { veis_token:, btsss_token: }
     end
 
     def token_client
