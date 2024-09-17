2195c5ef21cb3a662d399b54a40bb715d0173bb0
Andrew Herzberg <andrew.herzberg@adhocteam.us>
2024-09-17 15:22:10 -0700

    add facilities check on mobile appts

--- modules/vaos/app/policies/vaos_policy.rb
+++ modules/vaos/app/policies/vaos_policy.rb
@@ -4,4 +4,8 @@ VAOSPolicy = Struct.new(:user, :vaos) do
   def access?
     Flipper.enabled?('va_online_scheduling', user) && user.loa3?
   end
+
+  def facilities?
+    user.va_patient?
+  end
 end
