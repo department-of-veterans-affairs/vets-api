3e6c8577423e6b22dfac044d59747f94af8e8490
Josh Fike <josh.fike@oddball.io>
2024-09-18 14:17:01 -0500

    Spelling fix
    Co-authored-by: jvcAdHoc <144135615+jvcAdHoc@users.noreply.github.com>

--- modules/representation_management/app/swagger/errors.rb
+++ modules/representation_management/app/swagger/errors.rb
@@ -20,7 +20,7 @@ class Errors
     property :detail, type: :string, example: 'Received a bad request response from the upstream server',
                       description: 'possibly some additional info, or just the class name again'
     property :code, type: :string, example: '422',
-                    description: 'Sometiems just the http code again, sometimes something like "EVSS400", where" \
+                    description: 'Sometimes just the http code again, sometimes something like "EVSS400", where" \
                     " the code can be found in config/locales/exceptions.en.yml'
     property :source, type: %i[string object], example: 'RepresentationManagement::V0::PdfConstructor::Form2122',
                       description: 'error source class'
