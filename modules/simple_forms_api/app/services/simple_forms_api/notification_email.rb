e6f30eadb60ba4eb86d2b6c68bc4347730878521
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-09-18 10:53:56 -0400

    Update modules/simple_forms_api/app/services/simple_forms_api/notification_email.rb
    Co-authored-by: Jacob Penner <161746194+pennja@users.noreply.github.com>

--- modules/simple_forms_api/app/services/simple_forms_api/notification_email.rb
+++ modules/simple_forms_api/app/services/simple_forms_api/notification_email.rb
@@ -22,13 +22,22 @@ module SimpleFormsApi
     SUPPORTED_FORMS = TEMPLATE_IDS.keys
 
     def initialize(config, notification_type: :confirmation, user: nil)
-      @form_data = config.fetch(:form_data) { raise ArgumentError, 'Missing key: :form_data' }
-      @form_number = config.fetch(:form_number) { raise ArgumentError, 'Missing key: :form_number' }
-      @confirmation_number = config.fetch(:confirmation_number) do
-        raise ArgumentError, 'Missing key: :confirmation_number'
-      end
-      @notification_type = notification_type
-      @user = user
+def initialize(config, notification_type: :confirmation, user: nil)
+  check_missing_keys(config)
+
+  @form_data = config[:form_data]
+  @form_number = config[:form_number]
+  @confirmation_number = config[:confirmation_number]
+  @notification_type = notification_type
+  @user = user
+end
+
+private
+
+def check_missing_keys(config)
+  missing_keys = %i[form_data form_number confirmation_number].select { |key| config[key].nil? }
+  raise ArgumentError, "Missing keys: #{missing_keys.join(', ')}" if missing_keys.any?
+end
     end
 
     def send
