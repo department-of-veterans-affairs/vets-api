8cd8590a5111d6f6288871fcf9ced2798568b1ca
Tom Harrison <tom.harrison@adhocteam.us>
2024-09-19 13:08:56 -0400

    Disable rubocop metric to keep the SQL query in one method to improve readability.

--- app/models/form_submission.rb
+++ app/models/form_submission.rb
@@ -11,13 +11,35 @@ class FormSubmission < ApplicationRecord
   validates :form_type, presence: true
 
   class << self
+    # rubocop:disable Metrics/MethodLength
     def with_latest_intake(user_account)
+      # This query retrieves distinct form submissions for a given user account,
+      # along with the latest benefits intake UUID for each form. If a form has any
+      # submission attempts, it uses the latest attempt's UUID; otherwise, it falls
+      # back to the form submission's original UUID.
       FormSubmission
-        .select(select_query)
+        .select(
+          'DISTINCT form_submissions.id,
+          form_submissions.form_type,
+          COALESCE(latest_attempts.benefits_intake_uuid, form_submissions.benefits_intake_uuid) AS benefits_intake_uuid,
+          form_submissions.created_at'
+        )
         .left_joins(:form_submission_attempts)
-        .joins(latest_attempts_join)
+        .joins(
+          'LEFT JOIN (
+            SELECT fsa.form_submission_id, fsa.benefits_intake_uuid
+            FROM form_submission_attempts fsa
+            INNER JOIN (
+              SELECT form_submission_id, MAX(created_at) AS latest_attempt
+              FROM form_submission_attempts
+              GROUP BY form_submission_id
+            ) latest_fsa ON fsa.form_submission_id = latest_fsa.form_submission_id
+                          AND fsa.created_at = latest_fsa.latest_attempt
+          ) latest_attempts ON form_submissions.id = latest_attempts.form_submission_id'
+        )
         .where(user_account:)
     end
+    # rubocop:enable Metrics/MethodLength
 
     def with_form_types(form_types)
       if form_types.present?
@@ -29,32 +51,6 @@ class FormSubmission < ApplicationRecord
 
     def latest_pending_attempt
       form_submission_attempts.where(aasm_state: 'pending').order(created_at: :asc).last
-    end
-
-    private
-
-    def select_query
-      <<~SQL.squish
-        DISTINCT form_submissions.id,
-        form_submissions.form_type,
-        COALESCE(latest_attempts.benefits_intake_uuid, form_submissions.benefits_intake_uuid) AS benefits_intake_uuid,
-        form_submissions.created_at
-      SQL
-    end
-
-    def latest_attempts_join
-      <<~SQL.squish
-        LEFT JOIN (
-          SELECT fsa.form_submission_id, fsa.benefits_intake_uuid
-          FROM form_submission_attempts fsa
-          INNER JOIN (
-            SELECT form_submission_id, MAX(created_at) AS latest_attempt
-            FROM form_submission_attempts
-            GROUP BY form_submission_id
-          ) latest_fsa ON fsa.form_submission_id = latest_fsa.form_submission_id
-                       AND fsa.created_at = latest_fsa.latest_attempt
-        ) latest_attempts ON form_submissions.id = latest_attempts.form_submission_id
-      SQL
     end
   end
 end
