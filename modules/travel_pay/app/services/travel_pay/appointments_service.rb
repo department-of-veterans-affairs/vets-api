dadee438341cbff609672712157799f5a281466c
Liz Townsend <liz.townsend@adhocteam.us>
2024-09-20 09:21:50 -0500

    add appointments service/client and tests

---
+++ modules/travel_pay/app/services/travel_pay/appointments_service.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module TravelPay
+  class AppointmentsService
+    def get_appointments_by_date(veis_token, btsss_token, params = {})
+      faraday_response = client.get_all_appointments(veis_token, btsss_token, { 'excludeWithClaims' => true })
+      raw_appointments = faraday_response.body['data'].deep_dup
+
+      appointments = filter_by_date(params['appt_datetime'], raw_appointments)
+
+      {
+        data: appointments || []
+      }
+    end
+
+    private
+
+    def filter_by_date(date_string, appointments)
+      if date_string.present?
+        parsed_appt_date = Date.parse(date_string)
+
+        appointments.filter do |appointment|
+          appointment['appointmentDateTime'].nil? &&
+            parsed_appt_date == Date.parse(appointment['appointmentDateTime'])
+        end
+      end
+    rescue Date::Error => e
+      Rails.logger.debug(message: "#{e}. Unable to find appointments with date (given: #{date_string}).")
+    end
+
+    def client
+      TravelPay::AppointmentsClient.new
+    end
+  end
+end
---
+++ modules/travel_pay/spec/services/appointments_service_spec.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe TravelPay::AppointmentsService do
+  context 'get_appointments_by_date' do
+    let(:user) { build(:user) }
+    let(:appointments_data) do
+      {
+        'data' => [
+          {
+            'id' => 'uuid1',
+            'appointmentSource' => 'API',
+            'appointmentDateTime' => '2024-01-01T16:45:34.465Z',
+            'appointmentName' => 'string',
+            'appointmentType' => 'EnvironmentalHealth',
+            'facilityName' => 'Cheyenne VA Medical Center',
+            'serviceConnectedDisability' => 30,
+            'currentStatus' => 'string',
+            'appointmentStatus' => 'Completed',
+            'externalAppointmentId' => '12345678-0000-0000-0000-000000000001',
+            'associatedClaimId' => nil,
+            'associatedClaimNumber' => nil,
+            'isCompleted' => true
+          },
+          {
+            'id' => 'uuid2',
+            'appointmentSource' => 'API',
+            'appointmentDateTime' => '2024-03-01T16:45:34.465Z',
+            'appointmentName' => 'string',
+            'appointmentType' => 'EnvironmentalHealth',
+            'facilityName' => 'Cheyenne VA Medical Center',
+            'serviceConnectedDisability' => 30,
+            'currentStatus' => 'string',
+            'appointmentStatus' => 'Completed',
+            'externalAppointmentId' => '12345678-0000-0000-0000-000000000002',
+            'associatedClaimId' => nil,
+            'associatedClaimNumber' => nil,
+            'isCompleted' => true
+          },
+          {
+            'id' => 'uuid3',
+            'appointmentSource' => 'API',
+            'appointmentDateTime' => '2024-02-01T16:45:34.465Z',
+            'appointmentName' => 'string',
+            'appointmentType' => 'EnvironmentalHealth',
+            'facilityName' => 'Cheyenne VA Medical Center',
+            'serviceConnectedDisability' => 30,
+            'currentStatus' => 'string',
+            'appointmentStatus' => 'Completed',
+            'externalAppointmentId' => '12345678-0000-0000-0000-000000000003',
+            'associatedClaimId' => nil,
+            'associatedClaimNumber' => nil,
+            'isCompleted' => true
+          },
+          {
+            'id' => 'uuid4',
+            'appointmentSource' => 'API',
+            'appointmentDateTime' => '2024-02-11T16:45:34.465Z',
+            'appointmentName' => 'string',
+            'appointmentType' => 'EnvironmentalHealth',
+            'facilityName' => 'Cheyenne VA Medical Center',
+            'serviceConnectedDisability' => 30,
+            'currentStatus' => 'string',
+            'appointmentStatus' => 'Completed',
+            'externalAppointmentId' => '12345678-0000-0000-0000-000000000004',
+            'associatedClaimId' => nil,
+            'associatedClaimNumber' => nil,
+            'isCompleted' => true
+          }
+        ]
+      }
+    end
+    let(:appointments_response) do
+      Faraday::Response.new(
+        body: appointments_data
+      )
+    end
+
+    let(:tokens) { %w[veis_token btsss_token] }
+
+    before do
+      allow_any_instance_of(TravelPay::AppointmentsClient)
+        .to receive(:get_all_appointments)
+        .with(*tokens, { exclude_with_claims: true })
+        .and_return(appointments_response)
+    end
+
+    context 'filter by appt date' do
+      it 'returns appointments that match appt date if specified' do
+        service = TravelPay::AppointmentsService.new
+        appts = service.get_appointments_by_date(*tokens, { 'appt_datetime' => '2024-01-01' })
+
+        expect(appts.count).to equal(1)
+      end
+
+      it 'returns 0 appointments if appt date does not match' do
+        service = TravelPay::AppointmentsService.new
+        appts = service.get_appointments_by_date(*tokens, { 'appt_datetime' => '1700-01-01' })
+
+        expect(appts[:data].count).to equal(0)
+      end
+
+      it 'returns 0 appointments if appt date is invalid' do
+        service = TravelPay::AppointmentsService.new
+        appts = service.get_appointments_by_date(*tokens, { 'appt_datetime' => 'banana' })
+        appts_empty_date = service.get_appointments_by_date(*tokens, { 'appt_datetime' => '' })
+
+        expect(appts[:data].count).to equal(0)
+        expect(appts_empty_date[:data].count).to equal(0)
+      end
+    end
+  end
+end
