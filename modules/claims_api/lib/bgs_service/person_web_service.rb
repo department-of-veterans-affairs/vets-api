c2049d82e1811af04554a93beac19f7ca7aaf99c
Tyler Coleman <tyler.coleman@oddball.io>
2024-09-20 10:55:48 -0700

    [skip ci] add manage_ptcpnt_rlnshp_poa

--- modules/claims_api/lib/bgs_service/person_web_service.rb
+++ modules/claims_api/lib/bgs_service/person_web_service.rb
@@ -8,14 +8,30 @@ module ClaimsApi
 
     def find_dependents_by_ptcpnt_id(id)
       body = Nokogiri::XML::DocumentFragment.parse <<~EOXML
-        <ptcpntId />
+        <ptcpntId>#{id}</ptcpntId>
       EOXML
 
-      { ptcpntId: id }.each do |k, v|
-        body.xpath("./*[local-name()='#{k}']").first.content = v
-      end
-
       make_request(endpoint: bean_name, action: 'findDependentsByPtcpntId', body:, key: 'DependentDTO')
+    end
+
+    # ptcpntIdA is the veteranʼs or dependentʼs participant id
+    # ptcpntIdB is the poaʼs participant id
+    def manage_ptcpnt_rlnshp_poa(options = {})
+      body = Nokogiri::XML::DocumentFragment.parse <<~EOXML
+        <PtcpntRlnshpDTO>
+          #{if options[:authzn_change_clmant_addrs_ind].present?
+              '<authznChangeClmantAddrsInd>Y</authznChangeClmantAddrsInd>'
+            end}
+          #{'<authznPoaAccessInd>Y</authznPoaAccessInd>' if options[:authzn_poa_access_ind].present?}
+          <compId>
+            <ptcpntIdA>#{options[:ptcpnt_id_a]}</ptcpntIdA>
+            <ptcpntIdB>#{options[:ptcpnt_id_b]}</ptcpntIdB>
+          </compId>
+          <statusTypeCd>#{options[:status_type_cd] || 'CURR'}</statusTypeCd>
+        </PtcpntRlnshpDTO>
+      EOXML
+
+      make_request(endpoint: bean_name, action: 'managePtcpntRlnshpPoa', body:, key: 'DependentDTO')
     end
   end
 end
