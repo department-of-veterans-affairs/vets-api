ba0fb04a4860e0cde24a8b3ea5d2e7f2ecdc802b
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-09-20 08:55:03 -0400

    minor refactoring

--- app/controllers/v0/backend_statuses_controller.rb
+++ app/controllers/v0/backend_statuses_controller.rb
@@ -7,31 +7,40 @@ module V0
   class BackendStatusesController < ApplicationController
     service_tag 'maintenance-windows'
     skip_before_action :authenticate
+    before_action :validate_service, only: [:show]
 
     # NOTE: this endpoint is somewhat misleading.  Index gets data from PagerDuty and
     # show only looks at GI bill scheduled downtime (and gets no data from PagerDuty)
     def index
-      statuses = ExternalServicesRedis::Status.new.fetch_or_cache
-      maintenance_windows = MaintenanceWindow.end_after(Time.zone.now)
-
       options = { params: { maintenance_windows: } }
-      render json: BackendStatusesSerializer.new(statuses, options)
+      render json: BackendStatusesSerializer.new(backend_statuses, options)
     end
 
     # GET /v0/backend_statuses/:service
     def show
-      raise Common::Exceptions::RecordNotFound, backend_service unless recognized_service?
-
-      # get status
-      be_status = BackendStatus.new(name: backend_service)
-
-      render json: BackendStatusSerializer.new(be_status)
+      render json: BackendStatusSerializer.new(backend_status)
     end
 
     private
 
+    def backend_statuses
+      @backend_statuses ||= ExternalServicesRedis::Status.new.fetch_or_cache
+    end
+
+    def maintenance_windows
+      @maintenance_windows ||= MaintenanceWindow.end_after(Time.zone.now)
+    end
+
+    def backend_status
+      @backend_status ||= BackendStatus.new(name: backend_service)
+    end
+
     def backend_service
       params[:service]
+    end
+
+    def validate_service
+      raise Common::Exceptions::RecordNotFound, backend_service unless recognized_service?
     end
 
     def recognized_service?
