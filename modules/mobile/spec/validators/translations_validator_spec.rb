0d06dd7dd2629bb2e9f263c32e6617b41b578c9d
Kris Pethtel <kris.pethtel@gmail.com>
2024-09-26 09:55:49 -0500

    wip

--- modules/mobile/spec/validators/translations_validator_spec.rb
+++ modules/mobile/spec/validators/translations_validator_spec.rb
@@ -3,21 +3,22 @@
 require 'rails_helper'
 
 # validations to ensure that translations file is not broken
-RSpec.describe 'Translations Validation' do
+RSpec.describe 'Translations Validation' do # rubocop:disable RSpec/DescribeClass
   describe 'translations/en/common.json file' do
-    let(:file) do
-      Rails.root.join('modules', 'mobile', 'app', 'assets', 'translations', 'en', 'common.json')
-    end
-    let(:line_count) { file.readlines.count }
+    let(:file) { Rails.root.join('modules', 'mobile', 'app', 'assets', 'translations', 'en', 'common.json') }
 
     it 'is formatted as expected' do
+      last_line_index = file.readlines.count - 1
+      second_to_last_line_index = last_line_index - 1
+
       File.readlines(file).each_with_index do |line, i|
         case i
         when 0
           expect(line).to eq("{\n")
-        when (line_count - 1)
+        when last_line_index
           expect(line).to eq("}\n")
-        when (line_count - 2)
+        when second_to_last_line_index
+          # no comma in second to last line
           expect(line).to match(/^  ".+": ".+"$/)
         else
           expect(line).to match(/^  ".+": ".+",$/)
@@ -26,17 +27,14 @@ RSpec.describe 'Translations Validation' do
     end
 
     it 'closes all interpolation braces' do
-      File.readlines(file).each_with_index do |line, i|
-        next if i.zero? || i == line_count - 1
-        next unless line.include?('{{')
-
+      File.readlines(file)[1..-2] do |line|
         unclosed_double_braces_pattern = /\{\{(?![^{}]*\}\})/
         expect(line).not_to match(unclosed_double_braces_pattern)
       end
     end
 
     it 'is alphabetized' do
-      keys = File.readlines(file)[1..-2].map { |line| line.strip.split(/\\": \\"/).first.downcase }
+      keys = File.readlines(file)[1..-2].map { |line| line.strip.split(/": "/).first }
       expect(keys).to eq(keys.sort)
     end
   end
