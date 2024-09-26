ff5ff3495f51fcb533dbec0c7b01bc3f7c886d07
Tyler Fink <tfink419@gmail.com>
2024-09-26 10:37:09 -0600

    More Verbosity in Schema and Form Failures (#18554)
    * Pull more info from failed validations in schema and form
    
    * Fix lint
    
    * Add clear cache
    
    * Switch warn to error
    
    * Add extra details, tweak tests to match

--- app/models/saved_claim.rb
+++ app/models/saved_claim.rb
@@ -91,8 +91,24 @@ class SavedClaim < ApplicationRecord
   def form_matches_schema
     return unless form_is_string
 
-    JSON::Validator.fully_validate(VetsJsonSchema::SCHEMAS[self.class::FORM], parsed_form).each do |v|
-      errors.add(:form, v.to_s)
+    schema = VetsJsonSchema::SCHEMAS[self.class::FORM]
+
+    schema_errors = JSON::Validator.fully_validate_schema(schema, { errors_as_objects: true })
+    clear_cache = false
+    unless schema_errors.empty?
+      Rails.logger.error('SavedClaim schema failed validation! Attempting to clear cache.', { errors: schema_errors })
+      clear_cache = true
+    end
+
+    validation_errors = JSON::Validator.fully_validate(schema, parsed_form, { errors_as_objects: true, clear_cache: })
+
+    validation_errors.each do |e|
+      errors.add(e[:fragment], e[:message])
+      e[:errors]&.flatten(2)&.each { |nested| errors.add(nested[:fragment], nested[:message]) if nested.is_a? Hash }
+    end
+
+    unless validation_errors.empty?
+      Rails.logger.error('SavedClaim form did not pass validation', { guid:, errors: validation_errors })
     end
   end
 
