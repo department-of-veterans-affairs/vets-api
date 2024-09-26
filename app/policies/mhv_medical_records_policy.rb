c5fdbdb887c736bb4d33cb66960c546eec07dc2d
Matthew Wright <matwright2010@gmail.com>
2024-09-26 12:24:22 -0600

    MHV-61122: Medical records access updated with new eligibility check

--- app/policies/mhv_medical_records_policy.rb
+++ app/policies/mhv_medical_records_policy.rb
@@ -1,9 +1,38 @@
 # frozen_string_literal: true
 
+require 'medical_records/user_eligibility/client'
+
 MHVMedicalRecordsPolicy = Struct.new(:user, :mhv_medical_records) do
   MR_ACCOUNT_TYPES = %w[Premium].freeze
 
   def access?
-    MR_ACCOUNT_TYPES.include?(user.mhv_account_type) && user.va_patient?
+    if Flipper.enabled?(:mhv_medical_records_new_eligibility_check)
+      client = UserEligibility::Client.new(session: { user_id: user.mhv_correlation_id, icn: user.icn })
+      response = client.get_is_valid_sm_user
+      validate_client(response) && user.va_patient
+    else
+      MR_ACCOUNT_TYPES.include?(user.mhv_account_type) && user.va_patient?
+    end
+  end
+
+  private
+
+  def validate_client(response)
+    [
+      'MHV Premium SM account with no logins  in past 26 months',
+      'MHV Premium SM account with Logins in past 26 months',
+      'MHV Premium account with no SM'
+    ].any? { |substring| response.include?(substring) }
+  rescue
+    log_denial_details
+    false
+  end
+
+  def log_denial_details
+    Rails.logger.info('MR ACCESS DENIED IN USER ELIGIBILITY POLICY',
+                      mhv_id: user.mhv_correlation_id.presence || 'false',
+                      sign_in_service: user.identity.sign_in[:service_name],
+                      va_facilities: user.va_treatment_facility_ids.length,
+                      va_patient: user.va_patient?)
   end
 end
---
+++ spec/lib/medical_records/user_eligibility/client_spec.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'medical_records/user_eligibility/client'
+
+describe UserEligibility::Client do
+  describe 'User eligibility operations', :vcr do
+    context 'when user ID and ICN are valid' do
+      before do
+        Flipper.enable(:mhv_medical_records_new_eligibility_check)
+      end
+
+      let(:icn) { '1000000000V000000' }
+      let(:user_id) { '10000000' }
+      let(:client) { UserEligibility::Client.new(user_id, icn) }
+      let(:expected_response_message) { 'MHV Premium SM account with Logins in past 26 months' }
+
+      it 'performs an eligibility check on the user', :vcr do
+        VCR.use_cassette 'user_eligibility_client/perform_an_eligibility_check' do
+          response = client.get_is_valid_sm_user
+          expect(response['accountStatus']).to include(expected_response_message)
+        end
+      end
+    end
+
+    context 'when user ID is nil' do
+      let(:user_id) { nil }
+      let(:icn) { '1000000000V000000' }
+      let(:client) { UserEligibility::Client.new(user_id, icn) }
+
+      it 'raises an error', :vcr do
+        expect { client.get_is_valid_sm_user }.to raise_error(Common::Exceptions::ParameterMissing)
+      end
+    end
+
+    context 'when ICN is nil' do
+      before do
+        Flipper.enable(:mhv_medical_records_new_eligibility_check)
+      end
+
+      let(:user_id) { '10000000' }
+      let(:icn) { nil }
+      let(:client) { UserEligibility::Client.new(user_id, icn) }
+
+      it 'raises an error', :vcr do
+        expect { client.get_is_valid_sm_user }.to raise_error(Common::Exceptions::ParameterMissing)
+      end
+    end
+
+    context 'when ICN is not properly formatted' do
+      before do
+        Flipper.enable(:mhv_medical_records_new_eligibility_check)
+      end
+
+      let(:user_id) { '10000000' }
+      let(:icn) { '12345' }
+      let(:client) { UserEligibility::Client.new(user_id, icn) }
+
+      it 'raises an error', :vcr do
+        VCR.use_cassette 'user_eligibility_client/perform_an_eligibility_check_with_bad_icn' do
+          expect do
+            client.get_is_valid_sm_user
+          end.to raise_error(Common::Exceptions::BackendServiceException)
+        end
+      end
+    end
+
+    context 'when patient is not premium' do
+      before do
+        Flipper.enable(:mhv_medical_records_new_eligibility_check)
+      end
+
+      let(:user_id) { '10000000' }
+      let(:icn) { '1000000000V000000' }
+      let(:client) { UserEligibility::Client.new(user_id, icn) }
+      let(:expected_response_message) { 'Not MHV Premium account' }
+
+      it 'performs an eligibility check on the user', :vcr do
+        VCR.use_cassette 'user_eligibility_client/perform_an_eligibility_check_on_a_patient_who_is_not_premium' do
+          expect do
+            client.get_is_valid_sm_user
+          end.to raise_error(Common::Exceptions::BackendServiceException)
+        end
+      end
+    end
+
+    context 'when new eligibility check feature flag is disabled' do
+      before do
+        Flipper.disable(:mhv_medical_records_new_eligibility_check)
+      end
+
+      let(:icn) { '1000000000V000000' }
+      let(:user_id) { '10000000' }
+      let(:client) { UserEligibility::Client.new(user_id, icn) }
+      let(:expected_response_message) { 'MHV Premium SM account with Logins in past 26 months' }
+
+      it 'performs an eligibility check on the user', :vcr do
+        expect(a_request(:get, %r{mhvapi/v1/usermgmt/usereligibility/isValidSMUser})).not_to have_been_made
+      end
+    end
+  end
+end
