414619fb321effaffb0ef99e0c1e133ffa3629d2
Matthew Wright <matwright2010@gmail.com>
2024-09-27 09:57:36 -0600

    MHV-61122: Error handling improved for MR access policy

--- app/policies/mhv_medical_records_policy.rb
+++ app/policies/mhv_medical_records_policy.rb
@@ -8,8 +8,14 @@ MHVMedicalRecordsPolicy = Struct.new(:user, :mhv_medical_records) do
   def access?
     if Flipper.enabled?(:mhv_medical_records_new_eligibility_check)
       client = UserEligibility::Client.new(session: { user_id: user.mhv_correlation_id, icn: user.icn })
-      response = client.get_is_valid_sm_user
-      validate_client(response) && user.va_patient
+
+      begin
+        response = client.get_is_valid_sm_user
+        validate_client(response) && user.va_patient?
+      rescue => e
+        log_denial_details('ERROR FETCHING SM USER ELIGIBILITY', e)
+        false
+      end
     else
       MR_ACCOUNT_TYPES.include?(user.mhv_account_type) && user.va_patient?
     end
@@ -23,16 +29,17 @@ MHVMedicalRecordsPolicy = Struct.new(:user, :mhv_medical_records) do
       'MHV Premium SM account with Logins in past 26 months',
       'MHV Premium account with no SM'
     ].any? { |substring| response.include?(substring) }
-  rescue
-    log_denial_details
+  rescue => e
+    log_denial_details('MR ACCESS DENIED IN USER ELIGIBILITY POLICY', e)
     false
   end
 
-  def log_denial_details
-    Rails.logger.info('MR ACCESS DENIED IN USER ELIGIBILITY POLICY',
+  def log_denial_details(message, error)
+    Rails.logger.info(message,
                       mhv_id: user.mhv_correlation_id.presence || 'false',
                       sign_in_service: user.identity.sign_in[:service_name],
                       va_facilities: user.va_treatment_facility_ids.length,
-                      va_patient: user.va_patient?)
+                      va_patient: user.va_patient?,
+                      message: error.message)
   end
 end
