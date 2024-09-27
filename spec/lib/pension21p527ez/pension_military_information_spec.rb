97d04c39501a10a68b04af433f10502486d8dfd4
Tai Wilkin <tai.wilkin@coforma.io>
2024-09-27 13:12:27 -0400

    Add additional test

--- spec/lib/pension21p527ez/pension_military_information_spec.rb
+++ spec/lib/pension21p527ez/pension_military_information_spec.rb
@@ -2,9 +2,20 @@
 
 require 'rails_helper'
 require 'pension_21p527ez/pension_military_information'
+require 'va_profile/military_personnel/service_history_response'
 
 RSpec.describe Pension21p527ez::PensionMilitaryInformation do
   let(:user) { build(:user, :loa3) }
+  let(:army_episode) do
+    VAProfile::Models::ServiceHistory.new(
+      branch_of_service: 'Army'
+    )
+  end
+  let(:unknown_episode) do
+    VAProfile::Models::ServiceHistory.new(
+      branch_of_service: 'Unknown'
+    )
+  end
 
   describe '#format_service_branches_for_pensions' do
     it 'returns an object of valid branches' do
@@ -33,6 +44,30 @@ RSpec.describe Pension21p527ez::PensionMilitaryInformation do
       expect(described_class.new(user).format_service_branches_for_pensions(['army', nil])).to eq({
                                                                                                     'army' => true
                                                                                                   })
+    end
+  end
+
+  describe '#service_branches_for_pensions' do
+    it 'returns an object of valid branches' do
+      military_personnel_stub = instance_double(VAProfile::MilitaryPersonnel::Service)
+
+      allow(VAProfile::MilitaryPersonnel::Service).to receive(:new) { military_personnel_stub }
+      allow(military_personnel_stub).to receive(:get_service_history).and_return(VAProfile::MilitaryPersonnel::ServiceHistoryResponse.new(
+                                                                                   200, episodes: [army_episode]
+                                                                                 ))
+      expect(described_class.new(user).service_branches_for_pensions).to eq({
+                                                                              'army' => true
+                                                                            })
+    end
+
+    it 'filters out invalid branches' do
+      military_personnel_stub = instance_double(VAProfile::MilitaryPersonnel::Service)
+
+      allow(VAProfile::MilitaryPersonnel::Service).to receive(:new) { military_personnel_stub }
+      allow(military_personnel_stub).to receive(:get_service_history).and_return(VAProfile::MilitaryPersonnel::ServiceHistoryResponse.new(
+                                                                                   200, episodes: [unknown_episode]
+                                                                                 ))
+      expect(described_class.new(user).service_branches_for_pensions).to eq({})
     end
   end
 end
