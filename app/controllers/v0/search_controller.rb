06025cf7df226f387e8007ff0ab4c71b208f8147
Bryan Thompson <bryan.thompson3@va.gov>
2024-10-01 10:35:32 -0700

    add flipper check to controller to determine search service utilized

--- app/controllers/v0/search_controller.rb
+++ app/controllers/v0/search_controller.rb
@@ -15,7 +15,7 @@ module V0
     # For example, the app/controllers/v0/prescriptions_controller.rb.
     #
     def index
-      response = Search::Service.new(query, page).results
+      response = search_service.results
       options = { meta: { pagination: response.pagination } }
 
       render json: SearchSerializer.new(response, options)
@@ -25,6 +25,14 @@ module V0
 
     def search_params
       params.permit(:query, :page)
+    end
+
+    def search_service
+      @search_service ||= if Flipper.enabled?(:search_use_v2_gsa)
+                            SearchGsa::Service.new(query, page)
+                          else
+                            Search::Service.new(query, page)
+                          end
     end
 
     # Returns a sanitized, permitted version of the passed query params.
