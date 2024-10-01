9307deb9596e93d0c891ac9c10c2b1324ce4e898
Tyler Coleman <tyler.coleman@oddball.io>
2024-10-01 09:21:09 -0700

    add cassettes

--- modules/claims_api/spec/services/dependent_claimant_poa_assignment_service_spec.rb
+++ modules/claims_api/spec/services/dependent_claimant_poa_assignment_service_spec.rb
@@ -11,36 +11,41 @@ Rspec.describe ClaimsApi::DependentClaimantPoaAssignmentService do
 
     context 'when the dependent has no open claims' do
       it 'assigns the POA to the dependent via manage_ptcpnt_rlnshp' do
-        VCR.insert_cassette('claims_api/bgs/person_web_service/manage_ptcpnt_rlnshp_poa_no_open_claims')
-        VCR.insert_cassette('claims_api/bgs/standard_data_web_service/find_poas')
+        VCR.use_cassette('claims_api/bgs/person_web_service/manage_ptcpnt_rlnshp_poa_no_open_claims') do
+          VCR.use_cassette('claims_api/bgs/standard_data_web_service/find_poas') do
+            allow(service).to receive(:assign_poa_to_dependent_via_manage_ptcpnt_rlnshp?).and_call_original
 
-        allow(service).to receive(:assign_poa_to_dependent_via_manage_ptcpnt_rlnshp?).and_call_original
+            expect do
+              service.assign_poa_to_dependent!
+            end.not_to raise_error
 
-        expect do
-          service.assign_poa_to_dependent!
-        end.not_to raise_error
-
-        expect(service).to have_received(:assign_poa_to_dependent_via_manage_ptcpnt_rlnshp?)
+            expect(service).to have_received(:assign_poa_to_dependent_via_manage_ptcpnt_rlnshp?)
+          end
+        end
       end
     end
 
     context 'when the dependent has open claims' do
       it 'assigns the POA to the dependent via update_benefit_claim' do
-        VCR.insert_cassette('claims_api/bgs/person_web_service/manage_ptcpnt_rlnshp_poa_with_open_claims')
-        VCR.insert_cassette('claims_api/bgs/standard_data_web_service/find_poas')
-        VCR.insert_cassette(
-          'claims_api/bgs/e_benefits_bnft_claim_status_web_service/find_benefit_claims_status_by_ptcpnt_id'
-        )
-        VCR.insert_cassette('claims_api/bgs/benefit_claim_web_service/find_bnft_claim')
-        VCR.insert_cassette('claims_api/bgs/benefit_claim_service/update_benefit_claim')
+        VCR.use_cassette('claims_api/bgs/person_web_service/manage_ptcpnt_rlnshp_poa_with_open_claims') do
+          VCR.use_cassette('claims_api/bgs/standard_data_web_service/find_poas') do
+            VCR.use_cassette(
+              'claims_api/bgs/e_benefits_bnft_claim_status_web_service/find_benefit_claims_status_by_ptcpnt_id'
+            ) do
+              VCR.use_cassette('claims_api/bgs/benefit_claim_web_service/find_bnft_claim') do
+                VCR.use_cassette('claims_api/bgs/benefit_claim_service/update_benefit_claim') do
+                  allow(service).to receive(:assign_poa_to_dependent_via_update_benefit_claim?).and_call_original
 
-        allow(service).to receive(:assign_poa_to_dependent_via_update_benefit_claim?).and_call_original
+                  expect do
+                    service.assign_poa_to_dependent!
+                  end.not_to raise_error
 
-        expect do
-          service.assign_poa_to_dependent!
-        end.not_to raise_error
-
-        expect(service).to have_received(:assign_poa_to_dependent_via_update_benefit_claim?)
+                  expect(service).to have_received(:assign_poa_to_dependent_via_update_benefit_claim?)
+                end
+              end
+            end
+          end
+        end
       end
     end
   end
