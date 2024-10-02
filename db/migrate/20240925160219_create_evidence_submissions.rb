02209d46df3e28c33a98ca844e8c8316a40c248f
Samuel Cormier <sam.cormier@coforma.io>
2024-10-02 08:10:45 -0700

    removed icn since it's available via the associated user_account model, and fixed an issue with the foreign key

--- db/migrate/20240925160219_create_evidence_submissions.rb
+++ db/migrate/20240925160219_create_evidence_submissions.rb
@@ -2,9 +2,8 @@ class CreateEvidenceSubmissions < ActiveRecord::Migration[7.1]
   def change
     create_table :evidence_submissions do |t|
       t.string :job_id
-      t.string :icn
       t.string :claim_id
-      t.references :user_account, null: false, foreign_key: true
+      t.references :user_account, null: false, foreign_key: true, type: :uuid
       t.json :template_metadata_ciphertext
       t.text :encrypted_kms_key
       t.string :upoad_status
