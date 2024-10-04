34abc5bac4dde1a1065cc60783d9973d98cd7803
Nathan Burgess <nathan.burgess28@hotmail.com>
2024-10-04 12:04:47 -0400

    Disability benefits nb refactor document upload provider initialization 1 (#18745)
    * Refactor ApiProviderFactory supplemental upload provider Initializer
    
    Updates the ApiProviderFactory to pass more metadata to the SupplementalDocumentUploadProvider initializers so we can encapsulate more behavior (logging/polling) in the provider instead of the upload jobs that are using them
    
    * Update Lighthouse upload provider for new behavior
    
    Updates LighthouseSupplementalDocumentUploadProvider to use the new initializing signature that allows us to move logging and polling record creation out of the job
    
    * Update EVSS upload provider for new behavior
    
    Updates EVSSSupplementalDocumentUploadProvider to use the new initializing signature that allows us to move logging out of the job
    
    * Revert accidental BDD instruction cherry pick
    
    * Update EVSS upload provider for new behavior
    
    Updates EVSSSupplementalDocumentUploadProvider to use the new initializing signature that allows us to move logging out of the job
    
    * Update shared example for new document upload provider initialize sig
    
    * Revert BDD Instruction-specific provider implementaiton; will follow in another PR
    
    * Trim docstrings to reduce line count in PR
    
    * Fix linting error

--- lib/disability_compensation/providers/document_upload/lighthouse_supplemental_document_upload_provider.rb
+++ lib/disability_compensation/providers/document_upload/lighthouse_supplemental_document_upload_provider.rb
@@ -8,9 +8,20 @@ class LighthouseSupplementalDocumentUploadProvider
 
   STATSD_PROVIDER_METRIC = 'lighthouse_supplemental_document_upload_provider'
 
+  # Maps VA's internal Document Types to the correct document_type attribute for a Lighthouse526DocumentUpload polling
+  # record. We need this to create a valid polling record
+  POLLING_DOCUMENT_TYPES = {
+    'L023' => Lighthouse526DocumentUpload::BDD_INSTRUCTIONS_DOCUMENT_TYPE
+  }.freeze
+
   # @param form526_submission [Form526Submission]
-  def initialize(form526_submission)
+  #
+  # @param va_document_type [String] VA document code, see LighthouseDocument::DOCUMENT_TYPES
+  # @param statsd_metric_prefix [String] prefix, e.g. 'worker.evss.submit_form526_bdd_instructions' from including job
+  def initialize(form526_submission, va_document_type, statsd_metric_prefix)
     @form526_submission = form526_submission
+    @va_document_type = va_document_type
+    @statsd_metric_prefix = statsd_metric_prefix
   end
 
   # Uploads to Lighthouse require both the file body and an instance
@@ -19,16 +30,16 @@ class LighthouseSupplementalDocumentUploadProvider
   # an assembly of file-related Lighthouse metadata, not the actual uploaded file itself
   #
   # @param file_name [String] The name of the file we want to appear in Lighthouse
-  # @param document_type [String] The VA document code, which corresponds to
-  # the type of document being uploaded ('Buddy/Lay Statement', 'Disability Benefits Questionnaire (DBQ)' etc.)
-  # These types are mapped in LighthouseDocument::DOCUMENT_TYPES
   #
   # @return [LighthouseDocument]
-  def generate_upload_document(file_name, document_type)
+  def generate_upload_document(file_name)
+    user = User.find(@form526_submission.user_uuid)
+
     LighthouseDocument.new(
-      evss_claim_id: @form526_submission.submitted_claim_id,
-      file_name:,
-      document_type:
+      claim_id: @form526_submission.submitted_claim_id,
+      participant_id: user.participant_id,
+      document_type: @va_document_type,
+      file_name:
     )
   end
 
@@ -45,19 +56,13 @@ class LighthouseSupplementalDocumentUploadProvider
   #
   # @param lighthouse_document [LighthouseDocument]
   # @param file_body [String]
-  #
-  # return [Faraday::Response] BenefitsDocuments::WorkerService makes http
-  # calls with the Faraday gem under the hood
   def submit_upload_document(lighthouse_document, file_body)
-    BenefitsDocuments::Form526::UploadSupplementalDocumentService.call(file_body, lighthouse_document)
+    api_response = BenefitsDocuments::Form526::UploadSupplementalDocumentService.call(file_body, lighthouse_document)
+    handle_lighthouse_response(api_response)
   end
 
-  def log_upload_success(uploading_class_prefix)
-    StatsD.increment("#{uploading_class_prefix}.#{STATSD_PROVIDER_METRIC}.#{STATSD_SUCCESS_METRIC}")
-  end
-
-  def log_upload_failure(uploading_class_prefix, error_class, error_message)
-    StatsD.increment("#{uploading_class_prefix}.#{STATSD_PROVIDER_METRIC}.#{STATSD_FAILED_METRIC}")
+  def log_upload_failure(error_class, error_message)
+    StatsD.increment("#{@statsd_metric_prefix}.#{STATSD_PROVIDER_METRIC}.#{STATSD_FAILED_METRIC}")
 
     Rails.logger.error(
       'LighthouseSupplementalDocumentUploadProvider upload failure',
@@ -66,6 +71,31 @@ class LighthouseSupplementalDocumentUploadProvider
         error_class:,
         error_message:
       }
+    )
+  end
+
+  private
+
+  # @param api_response [Faraday::Response] Lighthouse API response returned from the UploadSupplementalDocumentService
+  def handle_lighthouse_response(api_response)
+    response_body = api_response.body['data']
+
+    if response_body['success'] == true && response_body['requestId']
+      create_lighthouse_polling_record(response_body['requestId'])
+      StatsD.increment("#{@statsd_metric_prefix}.#{STATSD_PROVIDER_METRIC}.#{STATSD_SUCCESS_METRIC}")
+    end
+  end
+
+  # Creates a Lighthouse526DocumentUpload polling record
+  #
+  # @param lighthouse_request_id [String] unique ID Lighthouse provides us in the API response after we
+  # upload a document. We use this ID in the Form526DocumentUploadPollingJob chron job to check the status
+  # of the document after Lighthouse has received it.
+  def create_lighthouse_polling_record(lighthouse_request_id)
+    Lighthouse526DocumentUpload.create!(
+      form526_submission: @form526_submission,
+      document_type: POLLING_DOCUMENT_TYPES[@va_document_type],
+      lighthouse_document_request_id: lighthouse_request_id
     )
   end
 end
