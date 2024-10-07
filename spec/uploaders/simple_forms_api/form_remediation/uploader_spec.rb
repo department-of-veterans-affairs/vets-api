6e95c2aae8db2b411357c5a6db9b23fb21f2c8c7
Jacob Penner <jacob.penner@adhocteam.us>
2024-10-07 16:04:11 -0400

    fix test coverage for uploader

--- spec/uploaders/simple_forms_api/form_remediation/uploader_spec.rb
+++ spec/uploaders/simple_forms_api/form_remediation/uploader_spec.rb
@@ -1,32 +1,48 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require SimpleFormsApi::Engine.root.join('spec', 'spec_helper.rb')
+require 'simple_forms_api/form_remediation/configuration/vff_config'
 
-RSpec.describe VeteranFacingFormsRemediationUploader do
-  subject { described_class.new(benefits_intake_uuid, directory) }
+module SimpleFormsApi
+  module FormRemediation
+    RSpec.describe Uploader do
+      describe '#initialize' do
+        subject(:new) { described_class.new(directory:, config:) }
 
-  before do
-    allow(Settings.vff_simple_forms).to receive(:aws).and_return(OpenStruct.new(region: 'region', bucket: 'bucket'))
-  end
+        before do
+          allow(Settings.vff_simple_forms).to(
+            receive(:aws).and_return(OpenStruct.new(region: 'region', bucket: 'bucket'))
+          )
+        end
 
-  let(:benefits_intake_uuid) { SecureRandom.uuid }
-  let(:directory) { '/some/path' }
+        let(:config) { Configuration::VffConfig.new }
+        let(:directory) { '/some/path' }
 
-  it 'allows image, pdf, json, csv, and text files' do
-    expect(subject.extension_allowlist).to match_array %w[bmp csv gif jpeg jpg json pdf png tif tiff txt zip]
-  end
+        it 'allows image, pdf, json, csv, and text files' do
+          expect(new.extension_allowlist).to match_array %w[bmp csv gif jpeg jpg json pdf png tif tiff txt zip]
+        end
 
-  it 'returns a store directory containing benefits_intake_uuid' do
-    expect(subject.store_dir).to eq(directory)
-  end
+        it 'returns a store directory containing benefits_intake_uuid' do
+          expect(new.store_dir).to eq(directory)
+        end
 
-  it 'throws an error if no benefits_intake_uuid is given' do
-    expect { described_class.new(nil, directory) }.to raise_error(RuntimeError, 'The benefits_intake_uuid is missing.')
-  end
+        describe 'when config is nil' do
+          let(:config) { nil }
 
-  it 'throws an error if no directory is given' do
-    expect { described_class.new(benefits_intake_uuid, nil) }.to(
-      raise_error(RuntimeError, 'The s3 directory is missing.')
-    )
+          it 'throws an error' do
+            expect { new }.to raise_exception(RuntimeError, 'The configuration is missing.')
+          end
+        end
+
+        describe 'when directory is nil' do
+          let(:directory) { nil }
+
+          it 'throws an error' do
+            expect { new }.to raise_exception(RuntimeError, 'The S3 directory is missing.')
+          end
+        end
+      end
+    end
   end
 end
