8214b66fb266892049630d2578ae1acfbce5a382
Matt Christianson <matthew.christianson1@va.gov>
2024-10-08 08:45:47 -0400

    Move additional shared logic to base class

--- modules/claims_api/app/services/claims_api/disability_compensation/disability_document_service.rb
+++ modules/claims_api/app/services/claims_api/disability_compensation/disability_document_service.rb
@@ -5,14 +5,14 @@ module ClaimsApi
     class DisabilityDocumentService < DocumentServiceBase
       LOG_TAG = '526_v2_Disability_Document_service'
 
-      def create_upload(claim:, pdf_path:, doc_type: 'L122', action: 'post', original_filename: nil)
+      def create_upload(claim:, pdf_path:, doc_type: 'L122', original_filename: nil)
         unless File.exist? pdf_path
           ClaimsApi::Logger.log('benefits_documents', detail: "Error creating upload doc: #{file_path} doesn't exist,
                                                       claim_id: #{claim.id}")
           raise Errno::ENOENT, pdf_path
         end
 
-        body = generate_upload_body(claim:, doc_type:, pdf_path:, action:, original_filename:)
+        body = generate_body(claim:, doc_type:, pdf_path:, original_filename:)
         ClaimsApi::BD.new.upload_document(claim_id: claim.id, doc_type:, body:)
       end
 
@@ -22,27 +22,24 @@ module ClaimsApi
       # Generate form body to upload a document
       #
       # @return {parameters, file}
-      def generate_upload_body(claim:, doc_type:, pdf_path:, action:, original_filename: nil)
-        payload = {}
+      def generate_body(claim:, doc_type:, pdf_path:, original_filename: nil)
         auth_headers = claim.auth_headers
-        veteran_name = compact_veteran_name(auth_headers['va_eauth_firstName'], auth_headers['va_eauth_lastName'])
-        birls_file_num = auth_headers['va_eauth_birlsfilenumber']
+        veteran_name = compact_veteran_name(auth_headers['va_eauth_firstName'],
+                                            auth_headers['va_eauth_lastName'])
+        birls_file_number = auth_headers['va_eauth_birlsfilenumber']
         claim_id = claim.evss_id
-        file_name = generate_file_name(doc_type:, veteran_name:, claim_id:, original_filename:, action:)
+        form_name = doc_type == 'L122' ? '526EZ' : original_filename
+        file_name = generate_file_name(veteran_name:, claim_id:, form_name:, original_filename:)
+        system_name = 'VA.gov'
         tracked_item_ids = claim.tracked_items&.map(&:to_i) if claim&.has_attribute?(:tracked_items)
-        data = build_body(doc_type:, file_name:, claim_id:,
-                          file_number: birls_file_num, tracked_item_ids:)
 
-        fn = Tempfile.new('params')
-        File.write(fn, data.to_json)
-        payload[:parameters] = Faraday::UploadIO.new(fn, 'application/json')
-        payload[:file] = Faraday::UploadIO.new(pdf_path.to_s, 'application/pdf')
-        payload
+        generate_upload_body(claim_id:, system_name:, doc_type:, pdf_path:, file_name:, birls_file_number:,
+                             participant_id: nil, tracked_item_ids:)
       end
 
-      def generate_file_name(doc_type:, veteran_name:, claim_id:, original_filename:, action:)
-        if action == 'post' && doc_type == 'L122'
-          "#{[veteran_name, claim_id, '526EZ'].compact_blank.join('_')}.pdf"
+      def generate_file_name(veteran_name:, claim_id:, form_name:, original_filename:)
+        if form_name == '526EZ'
+          "#{[veteran_name, claim_id, form_name].compact_blank.join('_')}.pdf"
         else
           filename = get_original_supporting_doc_file_name(original_filename)
           "#{[veteran_name, claim_id, filename].compact_blank.join('_')}.pdf"
