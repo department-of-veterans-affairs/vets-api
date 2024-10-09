e2829efe663233b4c7a04e61d8547d21980db8e3
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2024-10-09 09:30:18 -0400

    Moving around

--- spec/factories/form4142_status_polling_records.rb
+++ spec/factories/form4142_status_polling_records.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :form526_ancillary_form4142_status_polling_record do
+  factory :form4142_status_polling_record do
     benefits_intake_uuid { 'MyString' }
     submission_id { 1 }
     status { 1 }
