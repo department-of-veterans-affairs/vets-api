a5eb66cf1471e7b5cd30d4427f5d9d3ec68e2764
Jacob Penner <jacob.penner@adhocteam.us>
2024-10-09 16:10:46 -0400

    fix broken test

--- modules/simple_forms_api/spec/services/s3/submission_archive_builder_spec.rb
+++ modules/simple_forms_api/spec/services/s3/submission_archive_builder_spec.rb
@@ -70,7 +70,7 @@ RSpec.describe SimpleFormsApi::S3::SubmissionArchiveBuilder do
 
     context 'when properly initialized' do
       it 'completes successfully' do
-        expect(run).to eq([temp_file_path, submission, submission_file_path])
+        expect(run).to eq([temp_file_path, submission, submission_file_path, metadata])
       end
 
       it 'writes the submission pdf file' do
