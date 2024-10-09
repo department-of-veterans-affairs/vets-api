a9e2bf2746b59373179aa445fb49d2fb1c980fd3
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2024-10-09 09:23:03 -0400

    Renaming to more generic 4142 class

--- app/models/form526_ancillary_form4142_status_polling_record.rb
+++ app/models/form526_ancillary_form4142_status_polling_record.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
-class Form526AncillaryForm4142StatusPollingRecord < ApplicationRecord
-  def needs_polled; end
+class Form4142StatusPollingRecord < ApplicationRecord
+
+  enum :status, {pending: 0, errored: 1, success: 2}, default: :pending
+
+  scope :needs_polled -> {where(status: :pending)}
+
+
 end
---
+++ db/migrate/20241008150707_create_form4142_status_polling_records.rb
@@ -0,0 +1,11 @@
+class CreateForm4142StatusPollingRecord < ActiveRecord::Migration[7.1]
+  def change
+    create_table :form4142_status_polling_records do |t|
+      t.string :benefits_intake_uuid
+      t.integer :submission_id
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
