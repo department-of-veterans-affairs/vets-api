7897985754ae8cef5f241d32df81c4b68ad0c7a9
Jacob Penner <jacob.penner@adhocteam.us>
2024-10-09 12:34:04 -0400

    memoize submission date

--- modules/simple_forms_api/app/models/simple_forms_api/base_form.rb
+++ modules/simple_forms_api/app/models/simple_forms_api/base_form.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module SimpleFormsApi
   module BaseForm
     def signature_date
-      Time.current.in_time_zone('America/Chicago')
+      @signature_date ||= Time.current.in_time_zone('America/Chicago')
     end
   end
 end
