525f9059aeb693b54c389dc55ebf7ffac112d193
Devin McCurdy <devin@apothesource.com>
2024-10-10 13:07:07 -0400

    Cleanup

--- modules/vaos/spec/support/vaos_path_matcher.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# VCR.configure do |c|
-#   c.register_request_matcher :path do |request1, request2|
-#     request1.parsed_uri.path.sub('vaos-alt', 'vaos') == request2.parsed_uri.path
-#   end
-# end
