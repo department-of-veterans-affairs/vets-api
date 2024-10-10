8f4c0f938660ad7456fe31ec9862212dd46ef7f7
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-10-10 09:08:58 -0600

    Clears up failing test

--- modules/claims_api/spec/sidekiq/shared_reporting_examples_spec.rb
+++ modules/claims_api/spec/sidekiq/shared_reporting_examples_spec.rb
@@ -13,7 +13,7 @@ RSpec.shared_examples 'shared reporting behavior' do
       unsuccessful_poa_submissions = job.unsuccessful_poa_submissions
 
       expect(poa_totals[0]['VA TurboClaim'][:totals]).to eq(6)
-      # TODO: address in API-40862-dynamic-email-report-preview
+      # TODO: address in subsequent ticket
       # expect(unsuccessful_poa_submissions.count).to eq(2)
       expect(unsuccessful_poa_submissions[0][:cid]).to eq('0oa9uf05lgXYk6ZXn297')
     end
@@ -31,7 +31,7 @@ RSpec.shared_examples 'shared reporting behavior' do
       unsuccessful_evidence_waiver_submissions = job.unsuccessful_evidence_waiver_submissions
 
       expect(ews_totals[0]['VA TurboClaim'][:totals]).to eq(6)
-      # TODO: address in API-40862-dynamic-email-report-preview
+      # TODO: address in subsequent ticket
       # expect(unsuccessful_evidence_waiver_submissions.count).to eq(2)
       expect(unsuccessful_evidence_waiver_submissions[0][:cid]).to eq('0oa9uf05lgXYk6ZXn297')
     end
@@ -40,22 +40,22 @@ RSpec.shared_examples 'shared reporting behavior' do
   it 'includes ITF metrics' do
     with_settings(Settings.claims_api,
                   report_enabled: true) do
-      ClaimsApi::IntentToFile.create!(status: ClaimsApi::IntentToFile::SUBMITTED, cid: '0oa9uf05lgXYk6ZXn297')
-      ClaimsApi::IntentToFile.create!(status: ClaimsApi::IntentToFile::ERRORED, cid: '0oa9uf05lgXYk6ZXn297')
+      FactoryBot.create(:intent_to_file, status: 'submitted', cid: '0oa9uf05lgXYk6ZXn297')
+      FactoryBot.create(:intent_to_file, :itf_errored, cid: '0oa9uf05lgXYk6ZXn297')
 
-      ClaimsApi::IntentToFile.create!(status: ClaimsApi::IntentToFile::SUBMITTED, cid: '0oadnb0o063rsPupH297')
-      ClaimsApi::IntentToFile.create!(status: ClaimsApi::IntentToFile::ERRORED, cid: '0oadnb0o063rsPupH297')
+      FactoryBot.create(:intent_to_file, status: 'submitted', cid: '0oadnb0o063rsPupH297')
+      FactoryBot.create(:intent_to_file, :itf_errored, cid: '0oadnb0o063rsPupH297')
 
       job = described_class.new
       job.perform
       itf_totals = job.itf_totals
 
       expect(itf_totals[0]['VA TurboClaim'][:submitted]).to eq(1)
-      expect(itf_totals[0]['VA TurboClaim'][:itf_errored]).to eq(1)
+      expect(itf_totals[0]['VA TurboClaim'][:errored]).to eq(1)
       expect(itf_totals[0]['VA TurboClaim'][:totals]).to eq(2)
 
       expect(itf_totals[1]['VA Connect Pro'][:submitted]).to eq(1)
-      expect(itf_totals[1]['VA Connect Pro'][:itf_errored]).to eq(1)
+      expect(itf_totals[1]['VA Connect Pro'][:errored]).to eq(1)
       expect(itf_totals[1]['VA Connect Pro'][:totals]).to eq(2)
     end
   end
