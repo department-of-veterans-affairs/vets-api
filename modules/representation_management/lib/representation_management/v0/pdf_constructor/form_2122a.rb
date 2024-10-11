04548837e11f3b91e23272f2ed08634dc80d4d69
Josh Fike <josh.fike@oddball.io>
2024-10-11 15:55:34 -0500

    Start getting next steps page ready for 2122

--- modules/representation_management/lib/representation_management/v0/pdf_constructor/form_2122a.rb
+++ modules/representation_management/lib/representation_management/v0/pdf_constructor/form_2122a.rb
@@ -14,6 +14,19 @@ module RepresentationManagement
           true
         end
 
+        def next_steps_part1(pdf)
+          add_text_with_spacing(pdf,
+                                'Request help from a VA accredited representative or VSO', size: 20,
+                                                                                           style: :bold)
+          add_text_with_spacing(pdf, 'VA Form 21-22a')
+          add_text_with_spacing(pdf, 'Your Next Steps', size: 16, style: :bold)
+          str = <<~HEREDOC.squish
+            Both you and the accredited representative will need to sign your form.
+            You can bring your form to them in person or mail it to them.
+          HEREDOC
+          add_text_with_spacing(pdf, str, move_down: 30, font: 'soursesanspro')
+        end
+
         def next_steps_contact(pdf, data)
           rep_name = <<~HEREDOC.squish
             #{data.representative_first_name}
