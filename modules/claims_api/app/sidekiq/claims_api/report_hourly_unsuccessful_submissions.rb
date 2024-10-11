d971a3213040776a7fabd3d7958bd160099e6bbe
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-10-11 15:00:41 -0600

    Adds check for the last 2 days, compares transaction_ids and only pulls in new Transaction_ids from the last hour. Compared to cacheing, and this requires fewer calls to the database, is less complex and takes less time. I will remove my comments at the end of the file, but am leaving them now in case anyone wants to see what I was comparing.

--- modules/claims_api/app/sidekiq/claims_api/report_hourly_unsuccessful_submissions.rb
+++ modules/claims_api/app/sidekiq/claims_api/report_hourly_unsuccessful_submissions.rb
@@ -8,7 +8,7 @@ module ClaimsApi
     def perform
       return unless allow_processing?
 
-      @search_to = 1.minute.ago
+      @search_to = Time.zone.now
       @search_from = @search_to - 60.minutes
       @reporting_to = @search_to.in_time_zone('Eastern Time (US & Canada)').strftime('%l:%M%p %Z')
       @reporting_from = @search_from.in_time_zone('Eastern Time (US & Canada)').strftime('%l:%M%p %Z')
@@ -68,13 +68,40 @@ module ClaimsApi
     end
 
     def va_gov_errored_claims
-      va_gov = ClaimsApi::AutoEstablishedClaim.select(:id, :transaction_id)
-                                              .where(created_at: @search_from..@search_to,
-                                                     status: 'errored', cid: '0oagdm49ygCSJTp8X297')
-                                              .group(
-                                                :id, :transaction_id
-                                              )
-      va_gov.group_by(&:transaction_id)
+      errored_claims = get_unique_errors
+      errored_claims.group_by(&:transaction_id)
+    end
+
+    def get_unique_errors
+      starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+      two_days = ClaimsApi::AutoEstablishedClaim.where(created_at: 48.hours.ago..@search_to,
+                                                       status: 'errored', cid: '0oagdm49ygCSJTp8X297')
+
+      unique = two_days.uniq { |claim| claim[:transaction_id] }
+      errored_claims = unique.select { |claim| claim[:created_at] >= @search_from }
+      # Elapsd time: 0.01658499985933304 seconds
+      
+      ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      elapsed = ending - starting
+      ClaimsApi::Logger.log('hourly_slack_alert_elapsed_time', detail: "Elapsd time: #{elapsed} seconds")
+      
+      errored_claims
     end
   end
 end
+
+# last_modified = ClaimsApi::AutoEstablishedClaim.order(:updated_at).last
+# last_modified_str = last_modified.updated_at.utc.to_s
+# cache_key = "all_errors/#{last_modified_str}"
+
+# errored_claims = []
+# @alert_groups ||= ClaimsApi::AutoEstablishedClaim.where(created_at: 48.hour.ago..Time.zone.now,
+# status: 'errored', cid: '0oagdm49ygCSJTp8X297')
+
+# all_errors = Rails.cache.fetch(cache_key) do
+#   @alert_groups
+# end
+# unique = all_errors.uniq { |claim| claim[:transaction_id] }
+# errored_claims = unique.select { |claim| claim[:created_at] >= @search_from }
+# Elapsd time: 0.0341619998216629 seconds
