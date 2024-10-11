8b1014617fab801ddff5dc2f1b86d05c1945c2de
Tonksthebear <jason.conigliari@adhocteam.us>
2024-10-11 10:49:29 -0700

    Merge VAOS and Mobile Appointment Authorization Policy

--- modules/mobile/app/controllers/mobile/v0/appointments_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/appointments_controller.rb
@@ -5,9 +5,9 @@ require 'mobile/v0/exceptions/custom_errors'
 module Mobile
   module V0
     class AppointmentsController < ApplicationController
+      include AppointmentAuthorization
       UPCOMING_DAYS_LIMIT = 7
 
-      before_action { authorize }
       after_action :clear_appointments_cache, only: %i[cancel create]
 
       def index
@@ -130,24 +130,6 @@ module Mobile
 
       def appointments_cache_interface
         @appointments_cache_interface ||= Mobile::AppointmentsCacheInterface.new
-      end
-
-      def authorize
-        raise_access_denied unless current_user.authorize(:vaos, :access?)
-        raise_access_denied_no_icn if current_user.icn.blank?
-        raise_access_denied_no_facilities unless current_user.authorize(:vaos, :facilities_access?)
-      end
-
-      def raise_access_denied
-        raise Common::Exceptions::Forbidden, detail: 'You do not have access to online scheduling'
-      end
-
-      def raise_access_denied_no_icn
-        raise Common::Exceptions::Forbidden, detail: 'No patient ICN found'
-      end
-
-      def raise_access_denied_no_facilities
-        raise Common::Exceptions::Forbidden, detail: 'No facility associated with user'
       end
 
       def staging_custom_error
