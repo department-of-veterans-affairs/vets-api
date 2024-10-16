1607a44e6bebfe2aca02744603fd05be28659423
RachalCassity <rachal.cassity@oddball.io>
2024-10-16 12:30:13 -0500

    Created index_vba_docs_upload_submissions_status_created_at where s3_deleted is false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_16_171432) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_16_172752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
@@ -1350,6 +1350,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_16_171432) do
     t.index ["created_at"], name: "index_vba_documents_upload_submissions_on_created_at"
     t.index ["guid"], name: "index_vba_documents_upload_submissions_on_guid"
     t.index ["s3_deleted"], name: "index_vba_documents_upload_submissions_on_s3_deleted"
+    t.index ["status", "created_at"], name: "index_vba_docs_upload_submissions_status_created_at_false", where: "(s3_deleted IS FALSE)"
     t.index ["status"], name: "index_vba_documents_upload_submissions_on_status"
   end
 
