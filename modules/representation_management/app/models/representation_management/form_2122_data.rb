0eceaf5dad0aff847a486bde4ec96d5672268488
Josh Fike <josh.fike@oddball.io>
2024-10-17 16:58:26 -0500

    [WIP] org lookup across models

--- modules/representation_management/app/models/representation_management/form_2122_data.rb
+++ modules/representation_management/app/models/representation_management/form_2122_data.rb
@@ -3,12 +3,19 @@
 module RepresentationManagement
   class Form2122Data < RepresentationManagement::Form2122Base
     def organization_name
-      org = Veteran::Service::Organization.find_by(poa: @organization_name)
-      @organization_name = org&.name || @organization_name
+      @organization_name ||= find_organization_name
     end
 
     attr_writer :organization_name
 
     validates :organization_name, presence: true # This will actually be an id of an organization
+
+    private
+
+    def find_organization_name
+      org = AccreditedOrganization.find_by(id: @organization_name) ||
+            Veteran::Service::Organization.find_by(poa: @organization_name)
+      org&.name || @organization_name # If the organization is not found, return the original value
+    end
   end
 end
