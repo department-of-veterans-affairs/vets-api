1cbeb1004816d2f45b4c6d527f2732eb8b528096
Matthew Wright <matwright2010@gmail.com>
2024-10-17 15:12:33 -0600

    MHV-61122: Cleaned up user_eligibility client_spec

--- spec/lib/medical_records/user_eligibility/client_spec.rb
+++ spec/lib/medical_records/user_eligibility/client_spec.rb
@@ -5,17 +5,18 @@ require 'medical_records/user_eligibility/client'
 
 describe UserEligibility::Client do
   describe 'User eligibility operations', :vcr do
-    context 'when user ID and ICN are valid' do
-      before do
-        Flipper.enable(:mhv_medical_records_new_eligibility_check)
-      end
+    let(:icn) { '1000000000V000000' }
+    let(:user_id) { '10000000' }
+    let(:client) { UserEligibility::Client.new(user_id, icn) }
 
-      let(:icn) { '1000000000V000000' }
-      let(:user_id) { '10000000' }
-      let(:client) { UserEligibility::Client.new(user_id, icn) }
+    before do
+      Flipper.enable(:mhv_medical_records_new_eligibility_check)
+    end
+
+    context 'when user ID and ICN are valid' do
       let(:expected_response_message) { 'MHV Premium SM account with Logins in past 26 months' }
 
-      it 'performs an eligibility check on the user', :vcr do
+      it 'performs an eligibility check on the user' do
         VCR.use_cassette 'user_eligibility_client/perform_an_eligibility_check' do
           response = client.get_is_valid_sm_user
           expect(response['accountStatus']).to include(expected_response_message)
@@ -25,61 +26,36 @@ describe UserEligibility::Client do
 
     context 'when user ID is nil' do
       let(:user_id) { nil }
-      let(:icn) { '1000000000V000000' }
-      let(:client) { UserEligibility::Client.new(user_id, icn) }
 
-      it 'raises an error', :vcr do
+      it 'raises a ParameterMissing error' do
         expect { client.get_is_valid_sm_user }.to raise_error(Common::Exceptions::ParameterMissing)
       end
     end
 
     context 'when ICN is nil' do
-      before do
-        Flipper.enable(:mhv_medical_records_new_eligibility_check)
-      end
-
-      let(:user_id) { '10000000' }
       let(:icn) { nil }
-      let(:client) { UserEligibility::Client.new(user_id, icn) }
 
-      it 'raises an error', :vcr do
+      it 'raises a ParameterMissing error' do
         expect { client.get_is_valid_sm_user }.to raise_error(Common::Exceptions::ParameterMissing)
       end
     end
 
     context 'when ICN is not properly formatted' do
-      before do
-        Flipper.enable(:mhv_medical_records_new_eligibility_check)
-      end
-
-      let(:user_id) { '10000000' }
       let(:icn) { '12345' }
-      let(:client) { UserEligibility::Client.new(user_id, icn) }
 
-      it 'raises an error', :vcr do
+      it 'raises a BackendServiceException error' do
         VCR.use_cassette 'user_eligibility_client/perform_an_eligibility_check_with_bad_icn' do
-          expect do
-            client.get_is_valid_sm_user
-          end.to raise_error(Common::Exceptions::BackendServiceException)
+          expect { client.get_is_valid_sm_user }.to raise_error(Common::Exceptions::BackendServiceException)
         end
       end
     end
 
     context 'when patient is not premium' do
-      before do
-        Flipper.enable(:mhv_medical_records_new_eligibility_check)
-      end
-
-      let(:user_id) { '10000000' }
-      let(:icn) { '1000000000V000000' }
-      let(:client) { UserEligibility::Client.new(user_id, icn) }
       let(:expected_response_message) { 'Not MHV Premium account' }
 
-      it 'performs an eligibility check on the user', :vcr do
+      it 'raises a BackendServiceException error' do
         VCR.use_cassette 'user_eligibility_client/perform_an_eligibility_check_on_a_patient_who_is_not_premium' do
-          expect do
-            client.get_is_valid_sm_user
-          end.to raise_error(Common::Exceptions::BackendServiceException)
+          expect { client.get_is_valid_sm_user }.to raise_error(Common::Exceptions::BackendServiceException)
         end
       end
     end
@@ -89,30 +65,15 @@ describe UserEligibility::Client do
         Flipper.disable(:mhv_medical_records_new_eligibility_check)
       end
 
-      let(:icn) { '1000000000V000000' }
-      let(:user_id) { '10000000' }
-      let(:client) { UserEligibility::Client.new(user_id, icn) }
-      let(:expected_response_message) { 'MHV Premium SM account with Logins in past 26 months' }
-
-      it 'performs an eligibility check on the user', :vcr do
+      it 'does not perform an eligibility check on the user' do
         expect(a_request(:get, %r{mhvapi/v1/usermgmt/usereligibility/isValidSMUser})).not_to have_been_made
       end
     end
 
     context 'when user eligibility client fails' do
-      before do
-        Flipper.enable(:mhv_medical_records_new_eligibility_check)
-      end
-
-      let(:icn) { '1000000000V000000' }
-      let(:user_id) { '10000000' }
-      let(:client) { UserEligibility::Client.new(user_id, icn) }
-
-      it 'raises an error', :vcr do
+      it 'raises a BackendServiceException error' do
         VCR.use_cassette 'user_eligibility_client/perform_an_eligibility_check_with_client_failure' do
-          expect do
-            client.get_is_valid_sm_user
-          end.to raise_error(Common::Exceptions::BackendServiceException)
+          expect { client.get_is_valid_sm_user }.to raise_error(Common::Exceptions::BackendServiceException)
         end
       end
     end
