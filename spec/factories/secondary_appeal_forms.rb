b76cad7e305cbf23a5e7e062a0a40c30f95d3a02
Molly Trombley-McCann <molly.trombley-mccann@coforma.io>
2024-10-17 18:28:20 -0700

    SecondaryAppealForm with validations

--- spec/factories/secondary_appeal_forms.rb
+++ spec/factories/secondary_appeal_forms.rb
@@ -1,13 +1,38 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :secondary_appeal_form do
+  factory :secondary_appeal_form4142, class: 'SecondaryAppealForm' do
     guid { SecureRandom.uuid }
+    form_id { '21-4142' }
     form do
       {
+        veteran: {
+          fullName: {
+            first: 'Person',
+            last: 'McPerson'
+          },
+          dateOfBirth: '1983-01-23',
+          ssn: '111223333',
+          address: {
+
+          },
+          homePhone: '123-456-7890',
+
+        },
+        patientIdentification: {
+          isRequestingOwnMedicalRecords: true
+
+        },
+        providerFacility: [{}],
+        preparerIdentification: {
+          relationshipToVeteran: 'self'
+        },
+        acknowledgeToReleaseInformation: true,
+        limitedConsent: 'some string',
         privacyAgreementAccepted: true
+
       }.to_json
     end
-    appeal_submission_id { SecureRandom.uuid }
+    appeal_submission
   end
 end
