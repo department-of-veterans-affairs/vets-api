be9b245ee099785a5867ec518dd3e89728737ce2
Josh Fike <josh.fike@oddball.io>
2024-10-18 17:23:51 -0500

    More switching of mocks for factories

--- modules/representation_management/spec/models/representation_management/form_2122_data_spec.rb
+++ modules/representation_management/spec/models/representation_management/form_2122_data_spec.rb
@@ -3,49 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe RepresentationManagement::Form2122Data, type: :model do
-  describe 'validations' do
-    subject { described_class.new }
+  describe '#organization_name' do
+    context 'when organization is found in AccreditedOrganization' do
+      it 'returns the name from AccreditedOrganization' do
+        accredited_organization = create(:accredited_organization, name: 'Accredited Org Name')
+        form_2122_data = described_class.new(organization_id: accredited_organization.id)
 
-    describe '#organization_name' do
-      let(:form_2122_data) { described_class.new }
-      let(:organization_id) { 'Test Organization' }
-
-      before do
-        form_2122_data.instance_variable_set(:@organization_id, organization_id)
+        expect(form_2122_data.organization_name).to eq('Accredited Org Name')
       end
+    end
 
-      context 'when organization is found in AccreditedOrganization' do
-        it 'returns the name from AccreditedOrganization' do
-          accredited_org = double('AccreditedOrganization', name: 'Accredited Org Name')
-          allow(AccreditedOrganization)
-            .to receive(:find_by)
-            .with(id: organization_id)
-            .and_return(accredited_org)
+    context 'when organization is found in Veteran::Service::Organization' do
+      it 'returns the name from Veteran::Service::Organization' do
+        veteran_org = create(:organization, name: 'Veteran Org Name')
+        form_2122_data = described_class.new(organization_id: veteran_org.poa)
 
-          expect(form_2122_data.organization_name).to eq('Accredited Org Name')
-        end
+        expect(form_2122_data.organization_name).to eq('Veteran Org Name')
       end
+    end
 
-      context 'when organization is found in Veteran::Service::Organization' do
-        it 'returns the name from Veteran::Service::Organization' do
-          allow(AccreditedOrganization).to receive(:find_by).with(id: organization_id).and_return(nil)
-          veteran_org = double('Veteran::Service::Organization', name: 'Veteran Org Name')
-          allow(Veteran::Service::Organization)
-            .to receive(:find_by)
-            .with(poa: organization_id)
-            .and_return(veteran_org)
+    context 'when organization is not found in either' do
+      it 'returns nil' do
+        form_2122_data = described_class.new(organization_id: 'Nonexistent Org')
 
-          expect(form_2122_data.organization_name).to eq('Veteran Org Name')
-        end
-      end
-
-      context 'when organization is not found in either' do
-        it 'returns nil' do
-          allow(AccreditedOrganization).to receive(:find_by).with(id: organization_id).and_return(nil)
-          allow(Veteran::Service::Organization).to receive(:find_by).with(poa: organization_id).and_return(nil)
-
-          expect(form_2122_data.organization_name).to eq(nil)
-        end
+        expect(form_2122_data.organization_name).to eq(nil)
       end
     end
   end
