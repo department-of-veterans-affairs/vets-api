f6f3994aca2c58923fda1c5d8374354fe951d163
Molly Trombley-McCann <molly.trombley-mccann@coforma.io>
2024-10-17 18:28:20 -0700

    SecondaryAppealForm with validations

--- app/models/secondary_appeal_form.rb
+++ app/models/secondary_appeal_form.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class SecondaryAppealForm < ApplicationRecord
-  validates :guid, presence: true
+  validates :guid, :form_id, presence: true
   validate(:form_matches_schema)
   validate(:form_must_be_string)
-  
+
   belongs_to :appeal_submission
 
   has_kms_key
@@ -21,6 +21,26 @@ class SecondaryAppealForm < ApplicationRecord
   end
 
   def form_matches_schema
-    
+    return unless form_is_string
+
+    schema = VetsJsonSchema::SCHEMAS[form_id]
+
+    schema_errors = JSON::Validator.fully_validate_schema(schema, { errors_as_objects: true })
+    clear_cache = false
+    unless schema_errors.empty?
+      Rails.logger.error('SecondaryAppealForm schema failed validation! Attempting to clear cache.', { errors: schema_errors })
+      clear_cache = true
+    end
+
+    validation_errors = JSON::Validator.fully_validate(schema, JSON.parse(form), { errors_as_objects: true, clear_cache: })
+
+    validation_errors.each do |e|
+      errors.add(e[:fragment], e[:message])
+      e[:errors]&.flatten(2)&.each { |nested| errors.add(nested[:fragment], nested[:message]) if nested.is_a? Hash }
+    end
+
+    unless validation_errors.empty?
+      Rails.logger.error('SavedClaim form did not pass validation', { guid:, errors: validation_errors })
+    end
   end
 end
--- spec/models/secondary_appeal_form_spec.rb
+++ spec/models/secondary_appeal_form_spec.rb
@@ -1,28 +1,31 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'decision_review_v1/service'
 
 RSpec.describe SecondaryAppealForm, type: :model do
-  subject { build(:secondary_appeal_form, form_id: '4142') }
+  subject { build(:secondary_appeal_form4142) }
 
-  it 'validates the presence of a guid' do
-    subject.guid = nil
-    expect(subject).not_to be_valid
-  end
-
-  context 'validates against the form schema' do
+  describe 'validations' do
     before do
       expect(subject).to be_valid
-      expect(JSON::Validator).to receive(:fully_validate).once.and_call_original
+      expect(JSON::Validator).to receive(:fully_validate).at_least(2).times.and_call_original
+    end
+
+    it { is_expected.to validate_presence_of(:guid) }
+
+    it 'errors if trying to validate without a form_id' do
+      subject.form_id = nil
+      expect { subject.valid? }.to raise_error(JSON::Schema::SchemaParseError)
     end
 
     it 'rejects forms with missing elements' do
-      bad_form = JSON.parse(subject).deep_dup.delete('something')
+      bad_form = JSON.parse(subject.form).deep_dup
+      bad_form.delete('privacyAgreementAccepted')
       subject.form = bad_form.to_json
       expect(subject).not_to be_valid
       expect(subject.errors.full_messages.size).to eq(1)
-      expect(subject.errors.full_messages).to include(/something/)
+      expect(subject.errors.full_messages).to include(/privacyAgreementAccepted/)
     end
   end
-  
 end
