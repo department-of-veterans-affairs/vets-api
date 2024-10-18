72a2449db00d252bbe63bcb6447792eb93741b7c
Derek Fong <151783381+dfong-adh@users.noreply.github.com>
2024-10-18 13:10:55 -0700

    94461: Add silent failure metrics and clean up other metrics for SavedClaim status updater jobs

--- app/sidekiq/decision_review/saved_claim_hlr_status_updater_job.rb
+++ app/sidekiq/decision_review/saved_claim_hlr_status_updater_job.rb
@@ -66,11 +66,13 @@ module DecisionReview
     end
 
     def handle_form_status_metrics_and_logging(hlr, status)
-      if status == ERROR_STATUS
-        # ignore logging and metrics for stale errors
-        return if JSON.parse(hlr.metadata || '{}')['status'] == ERROR_STATUS
+      # Skip logging and statsd metrics when there is no status change
+      return if JSON.parse(hlr.metadata || '{}')['status'] == status
 
+      if status == ERROR_STATUS
         Rails.logger.info('DecisionReview::SavedClaimHlrStatusUpdaterJob form status error', guid: hlr.guid)
+        StatsD.increment('silent_failure', tags: { service: 'higher-level-review',
+                                                   function: 'lighthouse claim submission' })
       end
 
       StatsD.increment("#{STATSD_KEY_PREFIX}.status", tags: ["status:#{status}"])
