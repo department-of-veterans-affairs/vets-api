4e54db76067ea7a7c1be0224d3738c4488774493
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2024-10-21 20:27:04 -0600

    updates UserAccountJob

--- modules/va_notify/app/sidekiq/va_notify/user_account_job.rb
+++ modules/va_notify/app/sidekiq/va_notify/user_account_job.rb
@@ -7,13 +7,13 @@ module VANotify
     sidekiq_options retry: 14
 
     sidekiq_retries_exhausted do |msg, _ex|
-      job_id = msg['jid']
-      job_class = msg['class']
-      error_class = msg['error_class']
-      error_message = msg['error_message']
+      job_id = msg["jid"]
+      job_class = msg["class"]
+      error_class = msg["error_class"]
+      error_message = msg["error_message"]
 
       message = "#{job_class} retries exhausted"
-      Rails.logger.error(message, { job_id:, error_class:, error_message: })
+      Rails.logger.error(message, {job_id:, error_class:, error_message:})
       StatsD.increment("sidekiq.jobs.#{job_class.underscore}.retries_exhausted")
     end
 
@@ -21,18 +21,23 @@ module VANotify
       user_account_id,
       template_id,
       personalisation = nil,
-      api_key = Settings.vanotify.services.va_gov.api_key
+      api_key = Settings.vanotify.services.va_gov.api_key,
+      callback_options = nil
     )
       user_account = UserAccount.find(user_account_id)
-      notify_client = VaNotify::Service.new(api_key)
+      notify_client = if callback_options
+        VaNotify::Service.new(api_key, callback_options)
+      else
+        VaNotify::Service.new(api_key)
+      end
 
       notify_client.send_email(
         {
-          recipient_identifier: { id_value: user_account.icn, id_type: 'ICN' },
+          recipient_identifier: {id_value: user_account.icn, id_type: "ICN"},
           template_id:, personalisation:
         }.compact
       )
-      StatsD.increment('api.vanotify.user_account_job.success')
+      StatsD.increment("api.vanotify.user_account_job.success")
     rescue Common::Exceptions::BackendServiceException => e
       handle_backend_exception(e, user_account, template_id, personalisation)
     end
@@ -42,10 +47,10 @@ module VANotify
         log_exception_to_sentry(
           e,
           {
-            args: { recipient_identifier: { id_value: user_account.id, id_type: 'UserAccountId' },
-                    template_id:, personalisation: }
+            args: {recipient_identifier: {id_value: user_account.id, id_type: "UserAccountId"},
+                   template_id:, personalisation:}
           },
-          { error: :va_notify_user_account_job }
+          {error: :va_notify_user_account_job}
         )
       else
         raise e
