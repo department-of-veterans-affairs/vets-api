507a4f0f1a9544e05a44bdd20ca41ccfa519fbee
Kris Pethtel <kris.pethtel@gmail.com>
2024-10-22 14:43:39 -0500

    skip better

--- modules/mobile/spec/support/helpers/rails_helper.rb
+++ modules/mobile/spec/support/helpers/rails_helper.rb
@@ -17,9 +17,11 @@ RSpec.configure do |config|
     Flipper.enable('va_online_scheduling')
   end
 
-  config.after :each, :mobile_spec, type: :request do |example|
-    if !example.metadata[:skip_json_api_validation] && response.header['Content-Type'] != 'application/pdf' &&
-       response.body.present? && response.body != 'null'
+  config.after :each, :mobile_spec, skipped?: false, type: :request do |example|
+    content_type = response.header['Content-Type']
+
+    if content_type != 'application/pdf' && response.body.present? &&
+       response.body != 'null' && !example.metadata[:skip_json_api_validation]
 
       expect(JSONAPI.parse_response!(response.parsed_body)).to eq(nil)
     end
