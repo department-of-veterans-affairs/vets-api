3e00e519c08f7d7be21bedd8836bba962fd58d8d
khoa-v-nguyen <khoa.nguyen@oddball.io>
2024-10-23 10:35:21 -0600

    Introduce InquiryPayload class for structured inquiry data handling
    	•	Created the InquiryPayload class to handle the construction of inquiry payloads in a modular and flexible manner.
    	•	Integrated InquiryDetails, SubmitterProfile, and VeteranProfile classes for building different parts of the payload, improving separation of concerns.
    	•	Implemented validation logic to ensure that required inquiry parameters are present before processing.
    	•	Added support for translating key fields using the Translator class, enhancing field consistency across the payload.
    	•	Structured methods like additional_payload_fields and school_state_and_profile_data for better readability and organization.
    	•	Included logic to handle both authenticated and unauthenticated users by dynamically setting LevelOfAuthentication based on user presence.

---
+++ modules/ask_va_api/spec/app/lib/ask_va_api/inquiries/payload_builder/inquiry_payload_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require AskVAApi::Engine.root.join('spec', 'support', 'shared_contexts.rb')
+
+RSpec.describe AskVAApi::Inquiries::PayloadBuilder::InquiryPayload do
+  subject(:builder) { described_class.new(inquiry_params: params, user: authorized_user) }
+
+  # allow to have access to inquiry_params and translated_payload
+  include_context 'shared data'
+
+  let(:cache_data_service) { instance_double(Crm::CacheData) }
+  let(:authorized_user) { build(:user, :accountable_with_sec_id, icn: '234', edipi: '123') }
+
+  before do
+    allow(Crm::CacheData).to receive(:new).and_return(cache_data_service)
+    allow(cache_data_service).to receive(:call).with(
+      endpoint: 'optionset',
+      cache_key: 'optionset'
+    ).and_return(optionset_cached_data)
+  end
+
+  describe '#call' do
+    let(:params) { inquiry_params[:inquiry] }
+
+    context 'when inquiry_params is received' do
+      it 'builds the correct payload' do
+        expect(builder.call).to eq(translated_payload)
+      end
+    end
+
+    context "when there's no user" do
+      let(:authorized_user) { nil }
+
+      it 'set LevelOfAuthentication to Unauthenticated' do
+        expect(builder.call[:LevelOfAuthentication]).to eq('Unauthenticated')
+      end
+    end
+
+    context 'when no params are passed' do
+      let(:params) { nil }
+
+      it 'raise an error' do
+        expect { builder.call }.to raise_error(
+          AskVAApi::Inquiries::PayloadBuilder::InquiryPayload::InquiryPayloadError
+        )
+      end
+    end
+  end
+end
