fb86802d1b1d321701847b4638e1814acf89af2e
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2024-10-24 13:08:33 -0600

    attempts alternative way to constantize string

--- modules/va_notify/app/services/va_notify/status_update.rb
+++ modules/va_notify/app/services/va_notify/status_update.rb
@@ -6,9 +6,7 @@ module VANotify
       notification = VANotify::Notification.find_by(notification_id: notification_callback[:id])
 
       # notification.callback is set by other teams, not user input
-      # rubocop:disable Security/IoMethods
-      klass = notification.callback.constantize
-      # rubocop:enable Security/IoMethods
+      klass = constantized_class(notification.callback)
 
       if klass.respond_to?(:call)
         begin
@@ -25,6 +23,12 @@ module VANotify
       end
     rescue => e
       Rails.logger.info(source: notification.source_location, status: notification.status, error_message: e.message)
+    end
+
+    private
+
+    def constantized_class(class_name)
+      class_name.constantize
     end
   end
 end
