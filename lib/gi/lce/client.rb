59d6c8c842fc0feb5a1a9936fb4c9442124babff
Jeff Marks <jeff.t.marks@gmail.com>
2024-10-24 14:36:47 -0600

    Create lce client methods

--- lib/gi/lce/client.rb
+++ lib/gi/lce/client.rb
@@ -7,6 +7,31 @@ module GI
   module LCE
     class Client < GI::Client
       configuration GI::LCE::Configuration
+
+      def get_lce_search_results_v1(params = {})
+        response = perform(:get, 'v1/lce', params)
+        gids_response(response)
+      end
+
+      def get_license_details_v1(params = {})
+        license_id = params[:id]
+        response = perform(:get, "v1/licenses/#{license_id}")
+      end
+
+      def get_certification_details_v1(params = {})
+        certification_id = params[:id]
+        response = perform(:get, "v1/certifications/#{certification_id}")
+      end
+
+      def get_prep_details_v1(params = {})
+        prep_id = params[:id]
+        response = perform(:get, "v1/preps/#{prep_id}")
+      end
+
+      def get_exam_details_v1(params = {})
+        exam_id = params[:id]
+        response = perform(:get, "v1/exams/#{exam_id}")
+      end
     end
   end
 end
