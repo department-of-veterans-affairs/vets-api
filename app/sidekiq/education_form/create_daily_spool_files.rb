a4e2c5c920c0f8d0bed5ffae04c542d43cb86cb9
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-10-24 15:53:00 -0400

    update settings-related env variables to use Settings directly

--- app/sidekiq/education_form/create_daily_spool_files.rb
+++ app/sidekiq/education_form/create_daily_spool_files.rb
@@ -114,13 +114,13 @@ module EducationForm
             # send copy of staging spool files to testers
             # This mailer is intended to only work for development, staging and NOT production
             # Rails.env will return 'production' on the development & staging servers and  which
-            # will trip the unwary. To be safe, use ENV['HOSTNAME']
+            # will trip the unwary. To be safe, use Settings.hostname
             email_staging_spool_files(contents) if
               # local developer development
               Rails.env.eql?('development') ||
 
               # VA Staging environment where we really want this to work.
-              ENV['HOSTNAME'].eql?('staging-api.va.gov')
+              Settings.hostname.eql?('staging-api.va.gov')
 
             # track and update the records as processed once the file has been successfully written
             track_submissions(region_id)
