d823275fb688743584616d7e2fbf3648bdc5a1f0
Kristen Brown <11942904+kristen-brown@users.noreply.github.com>
2024-10-25 15:50:49 -0400

    API-40398: Do not require gateway header checks for OAuth Appeals APIs

--- modules/appeals_api/app/controllers/appeals_api/v2/decision_reviews/supplemental_claims/evidence_submissions_controller.rb
+++ modules/appeals_api/app/controllers/appeals_api/v2/decision_reviews/supplemental_claims/evidence_submissions_controller.rb
@@ -8,6 +8,7 @@ module AppealsApi::V2
         include SentryLogging
         include AppealsApi::CharacterUtilities
         include AppealsApi::Schemas
+        include AppealsApi::GatewayOriginCheck
 
         class EvidenceSubmissionRequestValidatorError < StandardError; end
 
