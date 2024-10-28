87693b7bec945f86f2f01b48cae1ad07fcd88bb7
Jeff Marks <jeff.t.marks@gmail.com>
2024-10-28 09:45:18 -0600

    Add gids response to lce client

--- lib/gi/lce/client.rb
+++ lib/gi/lce/client.rb
@@ -16,21 +16,25 @@ module GI
       def get_certification_details_v1(params = {})
         certification_id = params[:id]
         response = perform(:get, "v1/lce/certifications/#{certification_id}", params.except(:id))
+        gids_response(response)
       end
 
       def get_exam_details_v1(params = {})
         exam_id = params[:id]
         response = perform(:get, "v1/lce/exams/#{exam_id}", params.except(:id))
+        gids_response(response)
       end
 
       def get_license_details_v1(params = {})
         license_id = params[:id]
         response = perform(:get, "v1/lce/licenses/#{license_id}", params.except(:id))
+        gids_response(response)
       end
 
       def get_prep_details_v1(params = {})
         prep_id = params[:id]
         response = perform(:get, "v1/lce/preps/#{prep_id}", params.except(:id))
+        gids_response(response)
       end
     end
   end
