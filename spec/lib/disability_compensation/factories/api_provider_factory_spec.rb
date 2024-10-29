d89e788a54fe9529b75958daa5fed6e50e37b59b
Alison Jones <alison.jones@oddball.io>
2024-10-29 15:31:35 -0500

    Add 0781 lighthouse flippers to API provider factory

--- spec/lib/disability_compensation/factories/api_provider_factory_spec.rb
+++ spec/lib/disability_compensation/factories/api_provider_factory_spec.rb
@@ -277,5 +277,28 @@ RSpec.describe ApiProviderFactory do
         provider(:random)
       end.to raise_error NotImplementedError
     end
+
+    context 'for 0781 uploads' do
+      def provider
+        ApiProviderFactory.call(
+          type: ApiProviderFactory::FACTORIES[:supplemental_document_upload],
+          options: {
+            form526_submission: submission,
+            document_type: 'L228', # 0781 VA Doc Type
+            statsd_metric_prefix: 'my_stats_metric_prefix_0781'
+          },
+          current_user:,
+          feature_toggle: ApiProviderFactory::FEATURE_TOGGLE_UPLOAD_0781
+        )
+      end
+
+      it 'provides a SupplementalDocumentUploadProvider based on a Flipper' do
+        Flipper.enable(ApiProviderFactory::FEATURE_TOGGLE_UPLOAD_0781)
+        expect(provider.class).to equal(LighthouseSupplementalDocumentUploadProvider)
+
+        Flipper.disable(ApiProviderFactory::FEATURE_TOGGLE_UPLOAD_0781)
+        expect(provider.class).to equal(EVSSSupplementalDocumentUploadProvider)
+      end
+    end
   end
 end
