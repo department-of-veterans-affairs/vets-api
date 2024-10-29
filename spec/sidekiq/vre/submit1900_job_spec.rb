f4a12a90b03632a569d09b32e751414f82521bd5
Evan Smith <evan.smith@oddball.io>
2024-10-29 11:27:37 -0400

    spec tests

--- spec/sidekiq/vre/submit1900_job_spec.rb
+++ spec/sidekiq/vre/submit1900_job_spec.rb
@@ -46,12 +46,44 @@ describe VRE::Submit1900Job do
   end
 
   describe 'raises an exception' do
+
+    let(:user_struct) do
+      OpenStruct.new(
+        edipi: '1007697216',
+        birls_id: '796043735',
+        participant_id: '600061742',
+        pid: '600061742',
+        birth_date: '1986-05-06T00:00:00+00:00'.to_date,
+        ssn: '796043735',
+        vet360_id: '1781151',
+        loa3?: true,
+        icn: '1013032368V065534',
+        uuid: 'b2fab2b5-6af0-45e1-a9e2-394347af91ef',
+        va_profile_email: 'test@test.com'
+      )
+    end
+
+    let(:monitor) { double('monitor') }
+    let(:exhaustion_msg) do
+      { 'args' => [], 'class' => 'VRE::Submit1900Job', 'error_message' => 'An error occured',
+        'queue' => nil }
+    end
+
+    let(:encrypted_user) { KmsEncrypted::Box.new.encrypt(user_struct.to_h.to_json) }
+    let(:user) { OpenStruct.new(JSON.parse(KmsEncrypted::Box.new.decrypt(encrypted_user))) }
+    let(:claim) { create(:veteran_readiness_employment_claim) }
+
+    before do
+      allow(SavedClaim::VeteranReadinessEmploymentClaim).to receive(:find).and_return(claim)
+      allow(VRE::Monitor).to receive(:new).and_return(monitor)
+      allow(monitor).to receive :track_submission_exhaustion
+    end
+
     it 'when queue is exhausted' do
-      VRE::Submit1900Job.within_sidekiq_retries_exhausted_block do
-        expect(Rails.logger).to receive(:error).exactly(:once).with(
-          'Failed all retries on VRE::Submit1900Job, last error: An error occured'
-        )
-        expect(StatsD).to receive(:increment).with('worker.vre.submit_1900_job.exhausted')
+      VRE::Submit1900Job.within_sidekiq_retries_exhausted_block({ 'args' => [claim.id, encrypted_user] }) do
+        expect(SavedClaim).to receive(:find).with(claim.id)
+        exhaustion_msg['args'] = [claim.id, encrypted_user]
+        expect(monitor).to receive(:track_submission_exhaustion).with(exhaustion_msg)
       end
     end
   end
