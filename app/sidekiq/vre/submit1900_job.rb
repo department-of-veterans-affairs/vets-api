50054b5bafd2ad51ab4550ac64cda59750383387
Evan Smith <evan.smith@oddball.io>
2024-10-29 14:59:33 -0400

    add flipper for email

--- app/sidekiq/vre/submit1900_job.rb
+++ app/sidekiq/vre/submit1900_job.rb
@@ -13,24 +13,24 @@ module VRE
     sidekiq_options retry: RETRY
 
     sidekiq_retries_exhausted do |msg, _ex|
-      # uncomment below
-      # claim_id, encrypted_user = msg['args']
-      # claim = SavedClaim.find(claim_id)
-      # user = OpenStruct.new(JSON.parse(KmsEncrypted::Box.new.decrypt(encrypted_user)))
       monitor = VRE::Monitor.new
       monitor.track_submission_exhaustion(msg)
-      # do email here, send to user
-      # email = claim.parsed_form['email'] || user['va_profile_email']
-      # VANotify::EmailJob.perform_async(
-      #   email,
-      #   Settings.vanotify.services.va_gov.template_id.form1900_action_needed_email,
-      #   {
-      #     #get attributes that are needed
-      #     'first_name' => claim.parsed_form.dig('veteranInformation', 'fullName', 'first')
-      #     'date' => Time.zone.today.strftime('%B %d, %Y')
-      #     'confirmation_number' => claim.confirmation_number
-      #   }
-      # )
+
+      if Flipper.enabled?(:vre_trigger_action_needed_email)
+        claim_id, encrypted_user = msg['args']
+        claim = SavedClaim.find(claim_id)
+        user = OpenStruct.new(JSON.parse(KmsEncrypted::Box.new.decrypt(encrypted_user)))
+        email = claim.parsed_form['email'] || user['va_profile_email']
+        VANotify::EmailJob.perform_async(
+          email,
+          Settings.vanotify.services.va_gov.template_id.form1900_action_needed_email,
+          {
+            'first_name' => claim.parsed_form.dig('veteranInformation', 'fullName', 'first'),
+            'date' => Time.zone.today.strftime('%B %d, %Y'),
+            'confirmation_number' => claim.confirmation_number
+          }
+        )
+      end
     end
 
     def perform(claim_id, encrypted_user)
--- spec/sidekiq/vre/submit1900_job_spec.rb
+++ spec/sidekiq/vre/submit1900_job_spec.rb
@@ -45,7 +45,7 @@ describe VRE::Submit1900Job do
     end
   end
 
-  describe 'raises an exception' do
+  describe 'raises an exception with email flipper on' do
     let(:user_struct) do
       OpenStruct.new(
         edipi: '1007697216',
@@ -76,12 +76,63 @@ describe VRE::Submit1900Job do
       allow(SavedClaim::VeteranReadinessEmploymentClaim).to receive(:find).and_return(claim)
       allow(VRE::Monitor).to receive(:new).and_return(monitor)
       allow(monitor).to receive :track_submission_exhaustion
+      Flipper.enable(:vre_trigger_action_needed_email)
     end
 
     it 'when queue is exhausted' do
       VRE::Submit1900Job.within_sidekiq_retries_exhausted_block({ 'args' => [claim.id, encrypted_user] }) do
-        # uncomment below when emails are approved
-        # expect(SavedClaim).to receive(:find).with(claim.id)
+        expect(SavedClaim).to receive(:find).with(claim.id).and_return(claim)
+        exhaustion_msg['args'] = [claim.id, encrypted_user]
+        expect(monitor).to receive(:track_submission_exhaustion).with(exhaustion_msg)
+        expect(VANotify::EmailJob).to receive(:perform_async).with(
+          'test@gmail.xom',
+          'form1900_action_needed_email_template_id',
+          {
+            'first_name' => 'Homer',
+            'date' => Time.now.in_time_zone('Eastern Time (US & Canada)').strftime('%B %d, %Y'),
+            'confirmation_number' => claim.confirmation_number
+          }
+        )
+      end
+    end
+  end
+
+  describe 'raises an exception with email flipper off' do
+    let(:user_struct) do
+      OpenStruct.new(
+        edipi: '1007697216',
+        birls_id: '796043735',
+        participant_id: '600061742',
+        pid: '600061742',
+        birth_date: '1986-05-06T00:00:00+00:00'.to_date,
+        ssn: '796043735',
+        vet360_id: '1781151',
+        loa3?: true,
+        icn: '1013032368V065534',
+        uuid: 'b2fab2b5-6af0-45e1-a9e2-394347af91ef',
+        va_profile_email: 'test@test.com'
+      )
+    end
+
+    let(:monitor) { double('monitor') }
+    let(:exhaustion_msg) do
+      { 'args' => [], 'class' => 'VRE::Submit1900Job', 'error_message' => 'An error occured',
+        'queue' => nil }
+    end
+
+    let(:encrypted_user) { KmsEncrypted::Box.new.encrypt(user_struct.to_h.to_json) }
+    let(:user) { OpenStruct.new(JSON.parse(KmsEncrypted::Box.new.decrypt(encrypted_user))) }
+    let(:claim) { create(:veteran_readiness_employment_claim) }
+
+    before do
+      allow(SavedClaim::VeteranReadinessEmploymentClaim).to receive(:find).and_return(claim)
+      allow(VRE::Monitor).to receive(:new).and_return(monitor)
+      allow(monitor).to receive :track_submission_exhaustion
+      Flipper.disable(:vre_trigger_action_needed_email)
+    end
+
+    it 'when queue is exhausted' do
+      VRE::Submit1900Job.within_sidekiq_retries_exhausted_block({ 'args' => [claim.id, encrypted_user] }) do
         exhaustion_msg['args'] = [claim.id, encrypted_user]
         expect(monitor).to receive(:track_submission_exhaustion).with(exhaustion_msg)
       end
