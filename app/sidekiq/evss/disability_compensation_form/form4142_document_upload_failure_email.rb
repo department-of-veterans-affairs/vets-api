a09e2ea0b812ea32d981e89763825bd7c57c4129
Scott Regenthal <scottregenthal@gmail.com>
2024-10-30 08:57:55 -0600

    [DBEX] Log and monitor failure email events (#19121)

--- app/sidekiq/evss/disability_compensation_form/form4142_document_upload_failure_email.rb
+++ app/sidekiq/evss/disability_compensation_form/form4142_document_upload_failure_email.rb
@@ -7,7 +7,7 @@ module EVSS
   module DisabilityCompensationForm
     class Form4142DocumentUploadFailureEmail < Job
       STATSD_METRIC_PREFIX = 'api.form_526.veteran_notifications.form4142_upload_failure_email'
-      ZSF_DD_TAG_FUNCTION  = 'Form 526 Flow - Form 4142 failure email sending'
+      ZSF_DD_TAG_FUNCTION  = '526_form_4142_upload_failure_email_sending'
 
       # retry for one day
       sidekiq_options retry: 14
