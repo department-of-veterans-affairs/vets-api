e9c0cb739a13662fdea56549585d397b6bb80ba5
Brandon Cooper <brandon.cooper@adhocteam.us>
2024-10-31 10:11:23 -0400

    [10-10CG] Add additional logging to create method in CaregiversAssistanceClaimsController (#19164)
    * add request spec for create
    
    * add spec for assert_veteran_status to request spec
    
    * debug log non validation and outage exceptions in caregiver create controller method
    
    * refactor error spec a bit
    
    * remove usage of caregiver1010 flipper toggle for deleting pdf on download_pdf
    
    * Revert "remove usage of caregiver1010 flipper toggle for deleting pdf on download_pdf"
    
    This reverts commit 836477c550b657a9b6b364836c461cc9e160159b.

--- spec/requests/v0/caregivers_assistance_claims_spec.rb
+++ spec/requests/v0/caregivers_assistance_claims_spec.rb
@@ -15,6 +15,163 @@ RSpec.describe 'V0::CaregiversAssistanceClaims', type: :request do
   let(:build_valid_form_submission) { -> { VetsJsonSchema::EXAMPLES['10-10CG'].clone } }
   let(:get_schema) { -> { VetsJsonSchema::SCHEMAS['10-10CG'].clone } }
 
+  before do
+    allow_any_instance_of(Form1010cg::Auditor).to receive(:record)
+    allow_any_instance_of(Form1010cg::Auditor).to receive(:record_caregivers)
+    allow(Rails.logger).to receive(:debug)
+  end
+
+  describe 'POST /v0/caregivers_assistance_claims' do
+    subject do
+      post('/v0/caregivers_assistance_claims', params: body, headers:)
+    end
+
+    let(:valid_form_data) { get_fixture('pdf_fill/10-10CG/simple').to_json }
+    let(:invalid_form_data) { '{}' }
+
+    before do
+      allow(SavedClaim::CaregiversAssistanceClaim).to receive(:new)
+        .and_return(claim) # Ensure the same claim instance is used
+    end
+
+    context 'when the claim is valid' do
+      let(:body) { { caregivers_assistance_claim: { form: valid_form_data } }.to_json }
+      let(:claim) { build(:caregivers_assistance_claim, form: valid_form_data) }
+
+      before do
+        allow_any_instance_of(Form1010cg::Service).to receive(:assert_veteran_status)
+        allow(Form1010cg::SubmissionJob).to receive(:perform_async)
+      end
+
+      context 'assert_veteran_status is successful' do
+        it 'creates a new claim, enqueues a submission job, and returns claim id' do
+          expect_any_instance_of(Form1010cg::Auditor).to receive(:record).with(:submission_attempt)
+          expect_any_instance_of(Form1010cg::Auditor).to receive(:record_caregivers).with(claim)
+
+          expect { subject }.to change(SavedClaim::CaregiversAssistanceClaim, :count).by(1)
+
+          expect(Form1010cg::SubmissionJob).to have_received(:perform_async).with(claim.id)
+
+          expect(response).to have_http_status(:ok)
+          expect(JSON.parse(response.body)['data']['id']).to eq(SavedClaim::CaregiversAssistanceClaim.last.id.to_s)
+          expect(JSON.parse(response.body)['data']['type']).to eq('claim')
+        end
+
+        context 'when an unexpected error occurs saving claim' do
+          let(:error_message) { 'Some unexpected error' }
+
+          before do
+            allow(claim).to receive(:save!).and_raise(StandardError.new(error_message))
+          end
+
+          it 'logs the error and re-raises it' do
+            expect(Rails.logger).to receive(:debug).with(
+              'CaregiverAssistanceClaim: error submitting claim',
+              { saved_claim_guid: claim.guid, error: kind_of(StandardError) }
+            )
+
+            subject
+            expect(response).to have_http_status(:internal_server_error)
+          end
+        end
+      end
+
+      context 'assert_veteran_status error' do
+        before do
+          allow_any_instance_of(Form1010cg::Service).to receive(
+            :assert_veteran_status
+          ).and_raise(Form1010cg::Service::InvalidVeteranStatus)
+        end
+
+        it 'returns backend service exception' do
+          expect(Rails.logger).not_to receive(:debug).with(
+            'CaregiverAssistanceClaim: error submitting claim',
+            { saved_claim_guid: claim.guid, error: instance_of(Form1010cg::Service::InvalidVeteranStatus) }
+          )
+
+          subject
+
+          expect(response).to have_http_status(:service_unavailable)
+          expect(JSON.parse(response.body)['errors'][0]['detail']).to eq(
+            'Backend Service Outage'
+          )
+        end
+      end
+    end
+
+    context 'when the claim is invalid' do
+      let(:body) { { caregivers_assistance_claim: { form: invalid_form_data } }.to_json }
+      let(:claim) { build(:caregivers_assistance_claim, form: invalid_form_data) }
+
+      before do
+        allow(PersonalInformationLog).to receive(:create!)
+      end
+
+      it 'logs the error and returns unprocessable entity' do
+        expect_any_instance_of(Form1010cg::Auditor).to receive(:record).with(:submission_attempt)
+        expect_any_instance_of(Form1010cg::Auditor).to receive(:record).with(:submission_failure_client_data,
+                                                                             hash_including(
+                                                                               claim_guid: claim.guid,
+                                                                               errors: hash_including('#/': be_present)
+                                                                             ))
+        expect(Rails.logger).not_to receive(:debug).with(
+          'CaregiverAssistanceClaim: error submitting claim',
+          { saved_claim_guid: claim.guid,
+            error: instance_of(Common::Exceptions::ValidationErrors) }
+        )
+
+        subject
+        expect(PersonalInformationLog).to have_received(:create!).with(
+          data: { form: claim.parsed_form },
+          error_class: '1010CGValidationError'
+        )
+
+        expect(response).to have_http_status(:unprocessable_entity)
+
+        res_body = JSON.parse(response.body)
+        expected_errors = [
+          { title: "did not contain a required property of 'veteran'", code: '100', status: '422' },
+          { title: "#/ The property '#/' of type object did not match one or more of the required schemas in schema",
+            code: '100', status: '422' },
+          { title: "#/ The property '#/' did not contain a required property of 'primaryCaregiver'", code: '100',
+            status: '422' },
+          { title: "#/ The property '#/' did not contain a required property of 'secondaryCaregiverOne'", code: '100',
+            status: '422' }
+        ]
+
+        expect(res_body['errors']).to be_present
+        expect(res_body['errors'].size).to eq(expected_errors.size)
+
+        expected_errors.each_with_index do |expected_error, index|
+          actual_error = res_body['errors'][index]
+          expect(actual_error['title']).to include(expected_error[:title])
+          expect(actual_error['code']).to eq(expected_error[:code])
+          expect(actual_error['status']).to eq(expected_error[:status])
+        end
+      end
+    end
+
+    context 'when an unexpected error' do
+      let(:body) { { caregivers_assistance_claim: { form: valid_form_data } }.to_json }
+      let(:claim) { build(:caregivers_assistance_claim, form: valid_form_data) }
+      let(:error_message) { 'Some unexpected error' }
+
+      before do
+        allow(claim).to receive(:valid?).and_raise(StandardError.new(error_message))
+      end
+
+      it 'logs the error and re-raises it' do
+        expect(Rails.logger).to receive(:debug).with(
+          'CaregiverAssistanceClaim: error submitting claim',
+          { saved_claim_guid: claim.guid, error: kind_of(StandardError) }
+        )
+
+        subject
+        expect(response).to have_http_status(:internal_server_error)
+      end
+    end
+  end
+
   describe 'POST /v0/caregivers_assistance_claims/download_pdf' do
     subject do
       post('/v0/caregivers_assistance_claims/download_pdf', params: body, headers:)
@@ -70,6 +227,7 @@ RSpec.describe 'V0::CaregiversAssistanceClaims', type: :request do
 
       expect(SecureRandom).to receive(:uuid).and_return('saved-claim-guid')
       expect(SecureRandom).to receive(:uuid).and_return('file-name-uuid')
+      expect_any_instance_of(Form1010cg::Auditor).to receive(:record).with(:pdf_download)
 
       subject
 
