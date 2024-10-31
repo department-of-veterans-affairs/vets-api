a9d262ea9a46c86b28dbc416ac603fb76bb8d782
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-10-31 12:57:16 -0600

    Adds successful test.

--- modules/claims_api/spec/sidekiq/ews_updater_spec.rb
+++ modules/claims_api/spec/sidekiq/ews_updater_spec.rb
@@ -52,21 +52,22 @@ RSpec.describe ClaimsApi::EwsUpdater, type: :job do
     end
   end
 
-  context 'when the claims_api_ews_updater_enables_local_bgs feature flag is enabled',
-          vcr: { record: :new_episodes } do
-    it 'calls local_bgs instead of bgs-ext' do
-      allow(Flipper).to receive(:enabled?).with(:claims_api_ews_updater_enables_local_bgs).and_return true
-      res = subject.new.perform(ews.id)
-      expect(res).to include(:bnft_claim_dto)
-    end
-  end
+  context 'when the claims_api_ews_updater_enables_local_bgs feature flag is enabled' do
+    let(:benefit_claim_web_service) { instance_double(ClaimsApi::BenefitClaimWebService) }
+    let(:bgs_claim) { { bnft_claim_dto: { some_claim: 'some_value' } } }
 
-  context 'when the claims_api_ews_updater_enables_local_bgs feature flag is disabled',
-          vcr: { record: :new_episodes } do
-    it 'calls bgs-ext instead of local_bgs' do
-      allow(Flipper).to receive(:enabled?).with(:claims_api_ews_updater_enables_local_bgs).and_return false
-      res = subject.new.perform(ews.id)
-      expect(res).to include(:bnft_claim_dto)
+    before do
+      allow(Flipper).to receive(:enabled?).with(:claims_api_ews_updater_enables_local_bgs).and_return true
+      allow(ClaimsApi::BenefitClaimWebService).to receive(:new).with(external_uid: anything,
+                                                                     external_key: anything)
+                                                               .and_return(benefit_claim_web_service)
+      allow(benefit_claim_web_service).to receive(:find_bnft_claim).with(claim_id: anything).and_return(bgs_claim)
+      allow(benefit_claim_web_service).to receive(:update_bnft_claim).with(claim: anything).and_return(bgs_claim)
+    end
+
+    it 'calls local_bgs instead of bgs-ext' do
+      subject.new.perform(ews.id)
+      expect(benefit_claim_web_service).to have_received(:update_bnft_claim)
     end
   end
 end
