32dc10fbb9128c5fd4b5d49ab1640d4de3c965c0
Noah Stern <Noah.Stern@va.gov>
2024-11-03 06:12:19 -0600

    Mock happy path response from dgib

--- modules/vye/app/controllers/vye/v1/dgib_verifications_controller.rb
+++ modules/vye/app/controllers/vye/v1/dgib_verifications_controller.rb
@@ -5,22 +5,45 @@ require 'dgib/claimant_status/service'
 require 'dgib/verification_record/service'
 require 'dgib/verify_claimant/service'
 
-
 module Vye
   module Vye::V1
-    class Vye::V1::DgibVerificationsController < ApplicationController
-      before_action :skip_authorization
-
+    class Vye::V1::DgibVerificationsController < Vye::V1::ApplicationController
       def verification_record
+        authorize user_info, policy_class: UserInfoPolicy
+
         Rails.logger.debug 'Calling verification_record'
-        #response = verification_service.get_verification_record(params[:claimant_id])
+        response = verification_service.get_verification_record(params[:claimant_id])
         serializer = Vye::ClaimantVerificationSerializer
-        #process_response(response, serializer)
-        json_body_file = 'modules/vye/spec/fixtures/claimant_response.json'
-        render json: serializer.new(File.read(json_body_file).to_json)
+
+        # Mocked response, remove when valid data is available.
+        response.status = 200
+        response.claimant_id = 1
+        response.delimiting_date = "2024-11-01"
+        response.enrollment_verifications = [
+          {
+            "verificationMonth": "December 2023",
+            "verificationBeginDate": "2024-11-01",
+            "verificationEndDate": "2024-11-01",
+            "verificationThroughDate": "2024-11-01",
+            "createdDate": "2024-11-01",
+            "verificationMethod": "",
+            "verificationResponse": "Y",
+            "facilityName": "string",
+            "totalCreditHours": 0,
+            "paymentTransmissionDate": "2024-11-01",
+            "lastDepositAmount": 0,
+            "remainingEntitlement": "53-01"
+          }
+        ]
+        response.payment_on_hold = true
+        # Mocked response, remove when valid data is available.
+
+        process_response(response, serializer)
       end
 
       def verify_claimant
+        authorize user_info, policy_class: UserInfoPolicy
+
         response =
           verify_claimant_service
           .verify_claimant(
@@ -36,13 +59,21 @@ module Vye
 
       # the serializer for this endpoint is the same as for verify_claimant
       def claimant_status
+        authorize user_info, policy_class: UserInfoPolicy
+
         response = claimant_status_service.get_claimant_status(params[:claimant_id])
         serializer = Vye::VerifyClaimantSerializer
         process_response(response, serializer)
       end
 
       def claimant_lookup
+        authorize user_info, policy_class: UserInfoPolicy
+
         response = claimant_lookup_service.claimant_lookup(@current_user.ssn)
+
+        # mocked claimant_id, remove when real data is available
+        response.claimant_id = 1
+
         serializer = Vye::ClaimantLookupSerializer
         process_response(response, serializer)
       end
