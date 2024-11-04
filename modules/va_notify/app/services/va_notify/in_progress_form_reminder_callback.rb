302889d8d8cca1e194db354b06542202791628b4
nathanbwright <nathanielbwright@gmail.com>
2024-11-04 10:32:12 -0700

    Add metadata

--- modules/va_notify/app/services/va_notify/in_progress_form_reminder_callback.rb
+++ modules/va_notify/app/services/va_notify/in_progress_form_reminder_callback.rb
@@ -4,7 +4,8 @@ module VANotify
   class InProgressFormReminderCallback
     def self.call(notification)
       Rails.logger.info(message: "VANotify - in_progress_form_reminder for notification: #{notification.id}",
-                        status: notification.status, status_reason: notification.status_reason)
+                        status: notification.status, status_reason: notification.status_reason,
+                        metadata: notification.metadata)
     end
   end
 end
--- modules/va_notify/spec/services/in_progress_form_reminder_callback_spec.rb
+++ modules/va_notify/spec/services/in_progress_form_reminder_callback_spec.rb
@@ -5,11 +5,12 @@ require 'rails_helper'
 describe VANotify::InProgressFormReminderCallback do
   it 'logs status and status_reason' do
     notification_id = SecureRandom.uuid
-    notification = create(:notification, notification_id:, status: 'some_status', status_reason: 'some_status_reason')
+    notification = create(:notification, notification_id:, status: 'some_status', status_reason: 'some_status_reason',
+                                         metadata: 'some_metadata')
 
     expect(Rails.logger).to receive(:info).with(
       message: "VANotify - in_progress_form_reminder for notification: #{notification.id}",
-      status: 'some_status', status_reason: 'some_status_reason'
+      status: 'some_status', status_reason: 'some_status_reason', metadata: 'some_metadata'
     )
 
     described_class.call(notification)
