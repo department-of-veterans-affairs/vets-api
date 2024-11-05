8f36e41709a18e5cfd4f6e3634dc4ca9b6c914b7
Brandon Cooper <brandon.cooper@adhocteam.us>
2024-11-05 14:44:48 -0500

    add saved_claim_spec.rb

---
+++ spec/models/saved_claim_spec.rb
@@ -0,0 +1,226 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+class TestSavedClaim < SavedClaim
+  FORM = 'some_form_id'
+  CONFIRMATION = 'test'
+
+  def regional_office
+    'test_office'
+  end
+
+  def attachment_keys
+    %i[some_key]
+  end
+end
+
+RSpec.describe TestSavedClaim, type: :model do # rubocop:disable RSpec/SpecFilePathFormat
+  subject(:saved_claim) { described_class.new(form: form_data) }
+
+  let(:form_data) { { some_key: 'some_value' }.to_json }
+
+  describe 'associations' do
+    it { is_expected.to have_many(:persistent_attachments).dependent(:destroy) }
+    it { is_expected.to have_many(:form_submissions).dependent(:nullify) }
+    it { is_expected.to have_many(:claim_va_notifications).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    before do
+      allow(VetsJsonSchema::SCHEMAS).to receive(:[]).and_return('schema_content')
+    end
+
+    it { is_expected.to validate_presence_of(:form) }
+
+    context 'no validation errors' do
+      before do
+        allow(JSON::Validator).to receive(:fully_validate).and_return([])
+      end
+
+      it 'returns true' do
+        expect(saved_claim.validate).to eq true
+      end
+    end
+
+    context 'validation errors' do
+      context 'when form does not match schema' do
+        before do
+          allow(JSON::Validator).to receive(:fully_validate).and_return([{ fragment: 'error' }])
+        end
+
+        it 'adds validation errors to the form' do
+          saved_claim.validate
+          expect(saved_claim.errors.full_messages).not_to be_empty
+        end
+      end
+
+      context 'when JSON:Validator.fully_validate_schema throws an exception' do
+        let(:exception) { StandardError.new('Some exception') }
+
+        before do
+          allow(JSON::Validator).to receive(:fully_validate_schema).and_raise(exception)
+          allow(JSON::Validator).to receive(:fully_validate).and_return([])
+        end
+
+        it 'logs exception and returns empty array' do
+          expect(Rails.logger).to receive(:error)
+            .with('Error during schema validation!', { error: exception.message, backtrace: anything })
+
+          saved_claim.validate
+        end
+      end
+
+      context 'when JSON:Validator.fully_validate throws an exception' do
+        let(:exception) { StandardError.new('Some exception') }
+
+        before do
+          allow(JSON::Validator).to receive(:fully_validate_schema).and_return([])
+          allow(JSON::Validator).to receive(:fully_validate).and_raise(exception)
+        end
+
+        it 'logs exception and returns empty array' do
+          expect(Rails.logger).to receive(:error)
+            .with('Error during form validation!', { error: exception.message, backtrace: anything })
+
+          saved_claim.validate
+        end
+      end
+    end
+  end
+
+  describe 'callbacks' do
+    context 'after create' do
+      it 'increments the saved_claim.create metric' do
+        allow(StatsD).to receive(:increment)
+        saved_claim.save!
+        expect(StatsD).to have_received(:increment).with('saved_claim.create', tags: ["form_id:#{saved_claim.form_id}"])
+      end
+
+      context 'if form_start_date is set' do
+        let(:form_start_date) { DateTime.now - 1 }
+
+        it 'increments the saved_claim.time-to-file metric' do
+          allow(StatsD).to receive(:measure)
+          saved_claim.form_start_date = form_start_date
+          saved_claim.save!
+
+          claim_duration = saved_claim.created_at - form_start_date
+          expect(StatsD).to have_received(:measure).with('saved_claim.time-to-file', claim_duration,
+                                                         tags: ["form_id:#{saved_claim.form_id}"])
+        end
+      end
+    end
+
+    context 'after destroy' do
+      it 'increments the saved_claim.destroy metric' do
+        saved_claim.save!
+        allow(StatsD).to receive(:increment)
+        saved_claim.destroy
+        expect(StatsD).to have_received(:increment).with('saved_claim.destroy',
+                                                         tags: ["form_id:#{saved_claim.form_id}"])
+      end
+    end
+  end
+
+  # describe '.add_form_and_validation' do
+  #   # before do
+  #   #   # Reset the class constant if it has been set before
+  #   #   TestSavedClaim.send(:remove_const, :FORM) if TestSavedClaim.const_defined?(:FORM)
+  #   # end
+
+  #   let(:form_id) { 'some_form_id' }
+
+  #   it 'sets the FORM constant' do
+  #     TestSavedClaim.add_form_and_validation(form_id)
+  #     expect(TestSavedClaim::FORM).to eq(form_id)
+  #   end
+
+  #   it 'adds validation for the form_id attribute' do
+  #     TestSavedClaim.add_form_and_validation(form_id)
+
+  #     binding.pry
+
+  #     expect(saved_claim.valid?).to be true
+  #     saved_claim.form_id = 'invalid_form_id'
+  #     expect(saved_claim.valid?).to be false
+  #     expect(saved_claim.errors[:form_id]).to include('is not included in the list')
+  #   end
+  # end
+
+  describe '#process_attachments!' do
+    let(:confirmation_code) { SecureRandom.uuid }
+    let(:form_data) { { some_key: [{ confirmationCode: confirmation_code }] }.to_json }
+
+    it 'processes attachments associated with the claim' do
+      attachment = create(:persistent_attachment, guid: confirmation_code, saved_claim: saved_claim)
+      saved_claim.process_attachments!
+
+      expect(attachment.reload.saved_claim_id).to eq(saved_claim.id)
+      expect(Lighthouse::SubmitBenefitsIntakeClaim).to have_enqueued_sidekiq_job(saved_claim.id)
+    end
+  end
+
+  describe '#confirmation_number' do
+    it 'returns the claim GUID' do
+      expect(saved_claim.confirmation_number).to eq(saved_claim.guid)
+    end
+  end
+
+  describe '#submitted_at' do
+    it 'returns the created_at' do
+      expect(saved_claim.submitted_at).to eq(saved_claim.created_at)
+    end
+  end
+
+  describe '#to_pdf' do
+    it 'calls PdfFill::Filler.fill_form' do
+      expect(PdfFill::Filler).to receive(:fill_form).with(saved_claim, nil)
+      saved_claim.to_pdf
+    end
+  end
+
+  describe '#update_form' do
+    it 'updates the form with new data' do
+      saved_claim.update_form('new_key', 'new_value')
+      expect(saved_claim.parsed_form['new_key']).to eq('new_value')
+    end
+  end
+
+  describe '#business_line' do
+    it 'returns empty string' do
+      expect(saved_claim.business_line).to eq('')
+    end
+  end
+
+  describe '#insert_notification' do
+    it 'creates a new ClaimVANotification record' do
+      saved_claim.save!
+
+      expect do
+        saved_claim.insert_notification(1)
+      end.to change(saved_claim.claim_va_notifications, :count).by(1)
+    end
+  end
+
+  describe '#va_notification?' do
+    let(:email_template_id) { 0 }
+
+    let!(:notification) do
+      ClaimVANotification.create(
+        saved_claim: saved_claim,
+        email_template_id:,
+        form_type: saved_claim.form_id,
+        email_sent: false
+      )
+    end
+
+    it 'returns the notification if it exists' do
+      expect(saved_claim.va_notification?(email_template_id)).to eq(notification)
+    end
+
+    it 'returns nil if the notification does not exist' do
+      expect(saved_claim.va_notification?('non_existent_template')).to be_nil
+    end
+  end
+end
