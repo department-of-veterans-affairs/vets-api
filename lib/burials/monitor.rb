e2b1bbba4986b4d16a0ec9dfeb59d5a89cf2dac8
Bryan Alexander <bryan.alexander@adhocteam.us>
2024-11-06 11:11:55 -0500

    95932: Update Burials monitor

--- lib/burials/monitor.rb
+++ lib/burials/monitor.rb
@@ -26,8 +26,16 @@ module Burials
     # @param e [ActiveRecord::RecordNotFound]
     #
     def track_show404(confirmation_number, current_user, e)
-      Rails.logger.error('21P-530EZ submission not found',
-                         { confirmation_number:, user_uuid: current_user&.uuid, message: e&.message })
+      additional_context = {
+        confirmation_number:,
+        user_account_uuid: current_user&.user_account_uuid,
+        message: e&.message,
+        tags: {
+          form_id: '21P-530EZ'
+        }
+      }
+      track_request('error', '21P-530EZ submission not found', CLAIM_STATS_KEY, additional_context,
+                    call_location: caller_locations.first)
     end
 
     ##
@@ -39,8 +47,16 @@ module Burials
     # @param e [Error]
     #
     def track_show_error(confirmation_number, current_user, e)
-      Rails.logger.error('21P-530EZ fetching submission failed',
-                         { confirmation_number:, user_uuid: current_user&.uuid, message: e&.message })
+      additional_context = {
+        confirmation_number:,
+        user_account_uuid: current_user&.user_account_uuid,
+        message: e&.message,
+        tags: {
+          form_id: '21P-530EZ'
+        }
+      }
+      track_request('error', '21P-530EZ fetching submission failed', CLAIM_STATS_KEY, additional_context,
+                    call_location: caller_locations.first)
     end
 
     ##
@@ -51,10 +67,15 @@ module Burials
     # @param current_user [User]
     #
     def track_create_attempt(claim, current_user)
-      StatsD.increment("#{CLAIM_STATS_KEY}.attempt")
-      Rails.logger.info('21P-530EZ submission to Sidekiq begun',
-                        { confirmation_number: claim&.confirmation_number, user_uuid: current_user&.uuid,
-                          statsd: "#{CLAIM_STATS_KEY}.attempt" })
+      additional_context = {
+        confirmation_number: claim&.confirmation_number,
+        user_account_uuid: current_user&.user_account_uuid,
+        tags: {
+          form_id: '21P-530EZ'
+        }
+      }
+      track_request('error', '21P-530EZ submission to Sidekiq begun', "#{CLAIM_STATS_KEY}.attempt", additional_context,
+                    call_location: caller_locations.first)
     end
 
     ##
@@ -67,11 +88,17 @@ module Burials
     # @param e [Error]
     #
     def track_create_validation_error(in_progress_form, claim, current_user)
-      StatsD.increment("#{CLAIM_STATS_KEY}.validation_error")
-      Rails.logger.error('21P-530EZ submission validation error',
-                         { confirmation_number: claim&.confirmation_number, user_uuid: current_user&.uuid,
-                           in_progress_form_id: in_progress_form&.id, errors: claim&.errors&.errors,
-                           statsd: "#{CLAIM_STATS_KEY}.validation_error" })
+      additional_context = {
+        confirmation_number: claim&.confirmation_number,
+        user_account_uuid: current_user&.user_account_uuid,
+        in_progress_form_id: in_progress_form&.id,
+        errors: claim&.errors&.errors,
+        tags: {
+          form_id: '21P-530EZ'
+        }
+      }
+      track_request('error', '21P-530EZ submission validation error', "#{CLAIM_STATS_KEY}.validation_error",
+                    additional_context, call_location: caller_locations.first)
     end
 
     ##
@@ -84,11 +111,18 @@ module Burials
     # @param e [Error]
     #
     def track_create_error(in_progress_form, claim, current_user, e = nil)
-      StatsD.increment("#{CLAIM_STATS_KEY}.failure")
-      Rails.logger.error('21P-530EZ submission to Sidekiq failed',
-                         { confirmation_number: claim&.confirmation_number, user_uuid: current_user&.uuid,
-                           in_progress_form_id: in_progress_form&.id, errors: claim&.errors&.errors,
-                           message: e&.message, statsd: "#{CLAIM_STATS_KEY}.failure" })
+      additional_context = {
+        confirmation_number: claim&.confirmation_number,
+        user_account_uuid: current_user&.user_account_uuid,
+        in_progress_form_id: in_progress_form&.id,
+        errors: claim&.errors&.errors,
+        message: e&.message,
+        tags: {
+          form_id: '21P-530EZ'
+        }
+      }
+      track_request('error', '21P-530EZ submission to Sidekiq failed', "#{CLAIM_STATS_KEY}.failure",
+                    additional_context, call_location: caller_locations.first)
     end
 
     ##
@@ -100,14 +134,17 @@ module Burials
     # @param current_user [User]
     #
     def track_create_success(in_progress_form, claim, current_user)
-      StatsD.increment("#{CLAIM_STATS_KEY}.success")
-      context = {
+      additional_context = {
         confirmation_number: claim&.confirmation_number,
-        user_uuid: current_user&.uuid,
+        user_account_uuid: current_user&.user_account_uuid,
         in_progress_form_id: in_progress_form&.id,
-        statsd: "#{CLAIM_STATS_KEY}.success"
+        errors: claim&.errors&.errors,
+        tags: {
+          form_id: '21P-530EZ'
+        }
       }
-      Rails.logger.info('21P-530EZ submission to Sidekiq success', context)
+      track_request('info', '21P-530EZ submission to Sidekiq success', "#{CLAIM_STATS_KEY}.success",
+                    additional_context, call_location: caller_locations.first)
     end
 
     ##
@@ -119,15 +156,17 @@ module Burials
     # @param current_user [User]
     #
     def track_process_attachment_error(in_progress_form, claim, current_user)
-      StatsD.increment("#{CLAIM_STATS_KEY}.process_attachment_error")
-      context = {
+      additional_context = {
         confirmation_number: claim&.confirmation_number,
-        user_uuid: current_user&.uuid,
+        user_account_uuid: current_user&.user_account_uuid,
         in_progress_form_id: in_progress_form&.id,
         errors: claim&.errors&.errors,
-        statsd: "#{CLAIM_STATS_KEY}.process_attachment_error"
+        tags: {
+          form_id: '21P-530EZ'
+        }
       }
-      Rails.logger.error('21P-530EZ process attachment error', context)
+      track_request('error', '21P-530EZ process attachment error', "#{CLAIM_STATS_KEY}.process_attachment_error",
+                    additional_context, call_location: caller_locations.first)
     end
 
     ##
@@ -140,16 +179,19 @@ module Burials
     def track_submission_exhaustion(msg, claim = nil)
       user_account_uuid = msg['args'].length <= 1 ? nil : msg['args'][1]
       additional_context = {
+        confirmation_number: claim&.confirmation_number,
+        user_account_uuid: user_account_uuid,
         form_id: claim&.form_id,
         claim_id: msg['args'].first,
-        confirmation_number: claim&.confirmation_number,
-        message: msg
+        message: msg,
+        tags: {
+          form_id: '21P-530EZ'
+        }
       }
       log_silent_failure(additional_context, user_account_uuid, call_location: caller_locations.first)
 
-      StatsD.increment("#{SUBMISSION_STATS_KEY}.exhausted")
-      Rails.logger.error('Lighthouse::SubmitBenefitsIntakeClaim Burial 21P-530EZ submission to LH exhausted!',
-                         user_uuid: user_account_uuid, **additional_context)
+      track_request('error', 'Lighthouse::SubmitBenefitsIntakeClaim Burial 21P-530EZ submission to LH exhausted!',
+                    "#{SUBMISSION_STATS_KEY}.exhausted", additional_context, call_location: caller_locations.first)
     end
   end
 end
