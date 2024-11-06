8d429faedfcafb545a0229e9cba76baa723e1342
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2024-11-06 15:13:19 -0500

    revert refactoring

--- modules/avs/app/controllers/avs/v0/avs_controller.rb
+++ modules/avs/app/controllers/avs/v0/avs_controller.rb
@@ -5,31 +5,54 @@ module Avs
     class AvsController < ApplicationController
       service_tag 'after-visit-summary'
       before_action :feature_enabled
-      before_action :authenticate
-      before_action :validate_search_params, only: %i[index]
-      before_action :validate_sid, only: %i[show]
+      before_action { :authenticate }
 
       def index
-        if avs_appointment_response.body.empty?
-          render json: {}
-        elsif !icns_match?(@current_user.icn, avs_appointment_response.body[0]['icn'])
-          render_client_error('Not authorized', 'User may not view the AVS for this appointment.', :unauthorized)
-        else
-          render json: { path: avs_path(avs_appointment_response.body[0]['sid']) }
+        station_no = params[:stationNo]
+        appointment_ien = params[:appointmentIen]
+        unless validate_search_param?(station_no) && validate_search_param?(appointment_ien)
+          render_client_error('Invalid parameters', 'Station number and Appointment IEN must be present and valid.')
+          return
         end
+
+        response = avs_service.get_avs_by_appointment(station_no, appointment_ien)
+
+        if response.body.empty?
+          data = {}
+        else
+          if response.body[0]['icn'].nil? || !icns_match?(@current_user.icn, response.body[0]['icn'])
+            render_client_error('Not authorized', 'User may not view the AVS for this appointment.', :unauthorized)
+            return
+          end
+
+          data = { path: get_avs_path(response.body[0]['sid']) }
+        end
+
+        render json: data
       end
 
       def show
-        if avs_sid_response[:status] == 404
-          render_client_error('Not found', "No AVS found for sid #{params['sid']}", :not_found)
-        elsif !icns_match?(@current_user.icn, avs_sid_response.avs.icn)
-          render_client_error('Not authorized', 'User may not view this AVS.', :unauthorized)
-        else
-          render json: Avs::V0::AfterVisitSummarySerializer.new(avs_sid_response.avs)
+        sid = params['sid']
+        unless validate_sid?(sid)
+          render_client_error('Invalid AVS id', 'AVS id does not match accepted format.')
+          return
         end
-      end
 
-      private
+        avs_response = avs_service.get_avs(sid)
+
+        if avs_response[:status] == 404
+          render_client_error('Not found', "No AVS found for sid #{sid}", :not_found)
+          return
+        end
+
+        data = avs_response.avs
+        if data['icn'].nil? || !icns_match?(@current_user.icn, data['icn'])
+          render_client_error('Not authorized', 'User may not view this AVS.', :unauthorized)
+          return
+        end
+
+        render json: serializer(data)
+      end
 
       def avs_service
         @avs_service ||= Avs::V0::AvsService.new
@@ -39,38 +62,33 @@ module Avs
         routing_error unless Flipper.enabled?(:avs_enabled, @current_user)
       end
 
-      def avs_path(sid)
+      def get_avs_path(sid)
         # TODO: define and use constant for base path.
         "/my-health/medical-records/summaries-and-notes/visit-summary/#{sid}"
       end
 
-      def avs_params
-        params.permit(:stationNo, :appointmentIen, :sid)
+      def render_client_error(title, message, status = :bad_request)
+        render json: {
+          errors: [
+            {
+              title:,
+              detail: message,
+              status:
+            }
+          ]
+        }, status:
       end
 
-      def avs_sid_response
-        @avs_appointment_response ||= avs_service.get_avs(avs_params[:sid])
-      end
-
-      def avs_appointment_response
-        @avs_appointment_response ||= avs_service.get_avs_by_appointment(avs_params[:stationNo],
-                                                                         avs_params[:appointmentIen])
-      end
-
-      def validate_search_params
-        unless validate_search_param?(avs_params[:stationNo]) && validate_search_param?(avs_params[:appointmentIen])
-          render_client_error('Invalid parameters', 'Station number and Appointment IEN must be present and valid.')
-        end
+      def serializer(avs)
+        Avs::V0::AfterVisitSummarySerializer.new(avs)
       end
 
       def validate_search_param?(param)
         !param.nil? && /^\d+$/.match(param)
       end
 
-      def validate_sid
-        unless /^[[:xdigit:]]{30,40}$/.match(params[:sid])
-          render_client_error('Invalid AVS id', 'AVS id does not match accepted format.')
-        end
+      def validate_sid?(sid)
+        /^[[:xdigit:]]{30,40}$/.match(sid)
       end
 
       def normalize_icn(icn)
@@ -78,14 +96,7 @@ module Avs
       end
 
       def icns_match?(icn_a, icn_b)
-        return false unless icn_a && icn_b
-
         normalize_icn(icn_a) == normalize_icn(icn_b)
-      end
-
-      def render_client_error(title, message, status = :bad_request)
-        error = { title:, detail: message, status: }
-        render json: { errors: [error] }, status:
       end
     end
   end
