d9303a17e586695f18b0df20b7958a17566f0f44
Ian Donovan <iandonovan@users.noreply.github.com>
2024-11-07 11:39:14 -0500

    Line too long in spec

--- spec/lib/lighthouse/benefits_claims/service_spec.rb
+++ spec/lib/lighthouse/benefits_claims/service_spec.rb
@@ -60,7 +60,8 @@ RSpec.describe BenefitsClaims::Service do
             response = @service.get_claims
             # In the cassette, the status is NEEDED_FROM_YOU
             expect(response.dig('data', 0, 'attributes', 'trackedItems', 0, 'status')).to eq('NEEDED_FROM_OTHERS')
-            expect(response.dig('data', 0, 'attributes', 'trackedItems', 0, 'displayName')).to eq('Private Medical Record')
+            expect(response.dig('data', 0, 'attributes', 'trackedItems', 0, 'displayName'))
+              .to eq('Private Medical Record')
           end
         end
       end
