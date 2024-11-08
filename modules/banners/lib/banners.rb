1a9eac70bef44afb7e3ccfd126900fb060ff8e49
Bryan Thompson <18094023+SnowboardTechie@users.noreply.github.com>
2024-11-08 10:49:49 -0800

    add initial thougnts/notes

--- modules/banners/lib/banners.rb
+++ modules/banners/lib/banners.rb
@@ -3,5 +3,19 @@
 require 'banners/engine'
 
 module Banners
-  # Your code goes here...
+  # Here is where I will fetch the banner data from graphql and update the banner table
+  # ensuring old banners are upadated or removed and new banners are added
+  # 
+  # this example from form_reloader.rb is a good example of how to do the fetch
+
+  # def all_forms_data
+  #   query = File.read(Rails.root.join('modules', 'va_forms', 'config', 'graphql_query.txt'))
+  #   body = { query: }
+  #   response = connection.post do |req|
+  #     req.path = 'graphql'
+  #     req.body = body.to_json
+  #     req.options.timeout = 300
+  #   end
+  #   JSON.parse(response.body).dig('data', 'nodeQuery', 'entities')
+  # end
 end
