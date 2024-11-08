f58ab25124bceb029a87d5477020c310a6f539bf
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2024-11-08 06:40:11 -0800

    VACMS-19453: Updates banner endpoint to respond with banners for a given path.

--- app/controllers/v0/banners_controller.rb
+++ app/controllers/v0/banners_controller.rb
@@ -2,15 +2,14 @@
 
 module V0
   class BannersController < ApplicationController
+    service_tag 'banners'
     skip_before_action :authenticate
 
     def by_path
       path = params[:path]
-      banner_type = params[:type]
-      # Filter banners based on path and banner_type if both are provided
-      banners = Banner.all
-      # banners = banners.where('some_path_column = ?', path) if path.present?
-      # banners = banners.where('alert_type = ?', banner_type) if banner_type.present?
+      banner_type = params.fetch(:type, 'full_width_banner_alert')
+      banners = []
+      banners = Banner.by_path_and_type(path, banner_type) if path && banner_type
       response = { banners: banners, path: path, banner_type: banner_type }
       render json: response
     end
--- modules/banners/app/models/banner.rb
+++ modules/banners/app/models/banner.rb
@@ -15,4 +15,20 @@ class Banner < ApplicationRecord
   validates :email_updates_button, inclusion: { in: [true, false] }
   validates :find_facilities_cta, inclusion: { in: [true, false] }
   validates :limit_subpage_inheritance, inclusion: { in: [true, false] }
+
+  # Returns banners for a given path and banner bundle type.
+  scope :by_path_and_type, lambda { |path, type|
+    # Filter by entity_bundle
+    bundle_condition = arel_table[:entity_bundle].eq(type)
+
+    # JSONB containment conditions using @>.
+    operating_system_condition = 'context @> ?'
+    home_condition = 'context @> ?'
+
+    # Use ActiveRecord's `where` with an OR clause for the JSONB paths.
+    where(bundle_condition)
+      .where("#{operating_system_condition} OR #{home_condition}",
+             [{ entity: { entityUrl: { path: path } } }].to_json,
+             [{ entity: { fieldOffice: { entity: { entityUrl: { path: path } } } } }].to_json)
+  }
 end
