19956b8d2a1a72eb6c6916e5d67a944961569a3b
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-11-08 11:37:01 -0500

    require lib

--- modules/va_notify/app/controllers/va_notify/callbacks_controller.rb
+++ modules/va_notify/app/controllers/va_notify/callbacks_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'va_notify/default_callback'
+
 module VANotify
   class CallbacksController < VANotify::ApplicationController
     include ActionController::HttpAuthentication::Token::ControllerMethods
