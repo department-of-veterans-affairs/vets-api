f60e1ac8cf7778015a6848930018f9dee4088041
Ryan McNeil <ryan.mcneil@oddball.io>
2024-11-12 09:40:53 -0700

    remove comment

--- modules/mobile/app/controllers/mobile/application_controller.rb
+++ modules/mobile/app/controllers/mobile/application_controller.rb
@@ -17,7 +17,6 @@ module Mobile
     attr_reader :current_user
 
     def authenticate
-      # DELETE THIS COMMENT
       return super if sis_authentication?
 
       StatsD.increment('iam_ssoe_oauth.auth.total')
