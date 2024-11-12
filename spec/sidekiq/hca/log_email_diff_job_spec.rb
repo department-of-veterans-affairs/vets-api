dc732c9ef281c27283d4aa7e2866802403160574
Brandon Cooper <brandon.cooper@adhocteam.us>
2024-11-12 10:56:51 -0500

    update log_email_diff_job to use new table

--- spec/sidekiq/hca/log_email_diff_job_spec.rb
+++ spec/sidekiq/hca/log_email_diff_job_spec.rb
@@ -6,89 +6,184 @@ RSpec.describe HCA::LogEmailDiffJob, type: :job do
   let!(:in_progress_form) { create(:in_progress_1010ez_form_with_email) }
   let!(:user) { create(:user, :loa3) }
 
-  before do
-    in_progress_form.update!(user_uuid: user.uuid)
-    allow(User).to receive(:find).with(user.uuid).and_return(user)
-  end
-
-  def self.expect_does_nothing
-    it 'does nothing' do
-      expect(StatsD).not_to receive(:increment)
-      expect($redis).not_to receive(:set)
-
-      subject
+  describe 'hca_log_email_diff_in_progress_form enabled' do
+    before do
+      Flipper.enable(:hca_log_email_diff_in_progress_form)
+      in_progress_form.update!(user_uuid: user.uuid)
+      allow(User).to receive(:find).with(user.uuid).and_return(user)
     end
-  end
 
-  def self.expect_email_tag(tag)
-    it "logs that email is #{tag}" do
-      expect do
+    def self.expect_does_nothing
+      it 'does nothing' do
+        expect(StatsD).not_to receive(:increment)
+
         subject
-      end.to trigger_statsd_increment("api.1010ez.in_progress_form_email.#{tag}")
 
-      expect($redis.get("HCA::LogEmailDiffJob:#{user.uuid}")).to eq('t')
-    end
-  end
-
-  describe '#perform' do
-    subject { described_class.new.perform(in_progress_form.id, user.uuid) }
-
-    context 'when the form has been deleted' do
-      before do
-        in_progress_form.destroy!
+        expect(InProgressEmailMatchLog).not_to receive(:create).with(
+          user_uuid: user.uuid, in_progress_form_id: in_progress_form.id
+        )
       end
-
-      expect_does_nothing
     end
 
-    context 'when form email is present' do
-      context 'when email confirmation is different' do
+    def self.expect_email_tag(tag)
+      it "logs that email is #{tag}" do
+        expect do
+          subject
+        end.to trigger_statsd_increment("api.1010ez.in_progress_form_email.#{tag}")
+
+        expect(InProgressForm.where(user_uuid: user.uuid, id: in_progress_form.id)).to exist
+      end
+    end
+
+    describe '#perform' do
+      subject { described_class.new.perform(in_progress_form.id, user.uuid) }
+
+      context 'when the form has been deleted' do
         before do
-          in_progress_form.update!(
-            form_data: JSON.parse(in_progress_form.form_data).except('view:email_confirmation').to_json
-          )
+          in_progress_form.destroy!
         end
 
         expect_does_nothing
       end
 
-      context 'when va profile email is different' do
-        expect_email_tag('different')
-      end
-
-      context 'when va profile is the same' do
-        before do
-          allow(user).to receive(:va_profile_email).and_return('Email@email.com')
-        end
-
-        expect_email_tag('same')
-
-        context 'when redis key for the user is already set' do
+      context 'when form email is present' do
+        context 'when email confirmation is different' do
           before do
-            $redis.set("HCA::LogEmailDiffJob:#{user.uuid}", 't')
+            in_progress_form.update!(
+              form_data: JSON.parse(in_progress_form.form_data).except('view:email_confirmation').to_json
+            )
           end
 
           expect_does_nothing
         end
-      end
 
-      context 'when va profile email is blank' do
-        before do
-          expect(user).to receive(:va_profile_email).and_return(nil)
+        context 'when va profile email is different' do
+          expect_email_tag('different')
         end
 
-        expect_email_tag('different')
+        context 'when va profile is the same' do
+          before do
+            allow(user).to receive(:va_profile_email).and_return('Email@email.com')
+          end
+
+          expect_email_tag('same')
+
+          context 'when InProgressEmailMatchLog already exists' do
+            before do
+              InProgressEmailMatchLog.create(user_uuid: user.uuid, in_progress_form_id: in_progress_form.id)
+            end
+
+            expect_does_nothing
+          end
+        end
+
+        context 'when va profile email is blank' do
+          before do
+            expect(user).to receive(:va_profile_email).and_return(nil)
+          end
+
+          expect_email_tag('different')
+        end
+      end
+
+      context 'when form email is blank' do
+        before do
+          in_progress_form.update!(
+            form_data: JSON.parse(in_progress_form.form_data).except('email').to_json
+          )
+        end
+
+        expect_does_nothing
+      end
+    end
+  end
+
+  describe 'hca_log_email_diff_in_progress_form disabled' do
+    before do
+      Flipper.disable(:hca_log_email_diff_in_progress_form)
+      in_progress_form.update!(user_uuid: user.uuid)
+      allow(User).to receive(:find).with(user.uuid).and_return(user)
+    end
+
+    def self.expect_does_nothing
+      it 'does nothing' do
+        expect(StatsD).not_to receive(:increment)
+        expect($redis).not_to receive(:set)
+
+        subject
       end
     end
 
-    context 'when form email is blank' do
-      before do
-        in_progress_form.update!(
-          form_data: JSON.parse(in_progress_form.form_data).except('email').to_json
-        )
+    def self.expect_email_tag(tag)
+      it "logs that email is #{tag}" do
+        expect do
+          subject
+        end.to trigger_statsd_increment("api.1010ez.in_progress_form_email.#{tag}")
+
+        expect($redis.get("HCA::LogEmailDiffJob:#{user.uuid}")).to eq('t')
+      end
+    end
+
+    describe '#perform' do
+      subject { described_class.new.perform(in_progress_form.id, user.uuid) }
+
+      context 'when the form has been deleted' do
+        before do
+          in_progress_form.destroy!
+        end
+
+        expect_does_nothing
       end
 
-      expect_does_nothing
+      context 'when form email is present' do
+        context 'when email confirmation is different' do
+          before do
+            in_progress_form.update!(
+              form_data: JSON.parse(in_progress_form.form_data).except('view:email_confirmation').to_json
+            )
+          end
+
+          expect_does_nothing
+        end
+
+        context 'when va profile email is different' do
+          expect_email_tag('different')
+        end
+
+        context 'when va profile is the same' do
+          before do
+            allow(user).to receive(:va_profile_email).and_return('Email@email.com')
+          end
+
+          expect_email_tag('same')
+
+          context 'when redis key for the user is already set' do
+            before do
+              $redis.set("HCA::LogEmailDiffJob:#{user.uuid}", 't')
+            end
+
+            expect_does_nothing
+          end
+        end
+
+        context 'when va profile email is blank' do
+          before do
+            expect(user).to receive(:va_profile_email).and_return(nil)
+          end
+
+          expect_email_tag('different')
+        end
+      end
+
+      context 'when form email is blank' do
+        before do
+          in_progress_form.update!(
+            form_data: JSON.parse(in_progress_form.form_data).except('email').to_json
+          )
+        end
+
+        expect_does_nothing
+      end
     end
   end
 end
