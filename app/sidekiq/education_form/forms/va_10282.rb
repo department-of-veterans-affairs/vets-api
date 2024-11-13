8d049a795415deb79cd505836b29f43942c7b2ca
Alex Chan <172081065+alexchan-va@users.noreply.github.com>
2024-11-13 11:28:27 -0500

    Create boilerplate for 10282 form

---
+++ app/sidekiq/education_form/forms/va_10282.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module EducationForm::Forms
+  class VA10282 < Base
+    def veteran_full_name
+    end
+
+    def veteran_desc
+    end
+
+    def email
+    end
+
+    def mobile_phone
+    end
+
+    def home_phone
+    end
+
+    def country
+    end
+
+    def state
+    end
+
+    def race_and_gender
+    end
+
+    def ethnicity
+    end
+
+    def orgin_race
+    end
+
+    def gender
+    end
+
+    def highest_level_of_education
+    end
+
+    def currently_employed
+    end
+
+    def current_annual_salary
+    end
+
+    def is_working_in_tech_industry
+    end
+
+    def tech_industry_focus_area
+    end
+
+    def form_type
+    end
+
+    def header_form_type
+    end
+  end
+end
