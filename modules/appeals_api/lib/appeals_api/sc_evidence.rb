3d26d5140c26f07f03ee9ec20a0ef96607b530cd
Mike <865014+michelpmcdonald@users.noreply.github.com>
2024-11-13 10:07:02 -0500

    removed extra lines

--- modules/appeals_api/lib/appeals_api/sc_evidence.rb
+++ modules/appeals_api/lib/appeals_api/sc_evidence.rb
@@ -12,25 +12,15 @@ module AppealsApi
       return [''] if evidence_dates.blank?
 
       evidence_dates.map do |hash|
-        start_yyyy_mm = hash['startDate'].split('-').take(2).reverse.join('-')
-        end_yyyy_mm = hash['endDate'].split('-').take(2).reverse.join('-')
-
         if (hash['startDate'] == hash['endDate']) && !month_format
           hash['startDate']
         elsif !month_format
           "#{hash['startDate']} to #{hash['endDate']}"
         else
+          start_yyyy_mm = hash['startDate'].split('-').take(2).reverse.join('-')
+          end_yyyy_mm = hash['endDate'].split('-').take(2).reverse.join('-')
           "#{start_yyyy_mm} to #{end_yyyy_mm}"
         end
-
-
-
-
-        # if hash['startDate'] == hash['endDate']
-        #   month_format ? start_yyyy_mm : hash['startDate']
-        # else
-        #   month_format ? "#{start_yyyy_mm} to #{end_yyyy_mm}" : "#{hash['startDate']} to #{hash['endDate']}"
-        # end
       end
     end
   end
