ff61d091b54a4ac2947873d1f5b7eaf395138f8d
Liz Townsend <liz.townsend@adhocteam.us>
2024-11-14 08:43:19 -0600

    DRY up the code, build metadata object

--- modules/travel_pay/app/services/travel_pay/claim_association_service.rb
+++ modules/travel_pay/app/services/travel_pay/claim_association_service.rb
@@ -16,7 +16,7 @@ module TravelPay
     #         metadata => {
     #           status => int (http status code, i.e. 200)
     #           success => boolean
-    #           message => string ('No claim for this appt' | 'Claim service is unavailable')
+    #           message => string ('Data retrieved successfully' | 'Claim service is unavailable')
     #         }
     #         claim => TravelPay::Claim (if a claim matches)
     #   }
@@ -34,16 +34,14 @@ module TravelPay
         { 'start_date' => params['start_date'],
           'end_date' => params['end_date'] }
       )
-      metadata = { 'status' => faraday_response.body['statusCode'],
-                   'success' => faraday_response.body['success'],
-                   'message' => faraday_response.body['message'] }
+
       if faraday_response.status == 200
         append_claims(params['appointments'],
                       faraday_response.body['data'],
-                      metadata)
+                      build_metadata(faraday_response.body))
       else
         append_error(params['appointments'],
-                     metadata)
+                     build_metadata(faraday_response.body))
       end
     end
 
@@ -54,36 +52,21 @@ module TravelPay
         { 'start_date' => appt['start'],
           'end_date' => appt['start'] }
       )
-      if faraday_response.status == 200
-        append_single_claim(appt, faraday_response.body)
-      else
-        appt['travelPayClaim'] = {
-          'metadata' => { 'status' => faraday_response.body['statusCode'],
-                          'success' => faraday_response.body['success'],
-                          'message' => faraday_response.body['message'] }
-        }
-        appt
-      end
-    end
 
-    private
-
-    def append_single_claim(appt, claim_response)
-      metadata = { 'status' => claim_response['statusCode'],
-                   'success' => claim_response['success'],
-                   'message' => claim_response['message'] }
-      appt['travelPayClaim'] = if claim_response['data'].count
+      appt['travelPayClaim'] = if faraday_response.body['data']&.count
                                  {
-                                   'metadata' => metadata,
-                                   'claim' => claim_response['data'][0]
+                                   'metadata' => build_metadata(faraday_response.body),
+                                   'claim' => faraday_response.body['data'][0]
                                  }
                                else
                                  {
-                                   'metadata' => metadata
+                                   'metadata' => build_metadata(faraday_response.body)
                                  }
                                end
       appt
     end
+
+    private
 
     def append_claims(appts, claims, metadata)
       appointments = []
@@ -117,6 +100,12 @@ module TravelPay
         appointments.push(appt)
       end
       appointments
+    end
+
+    def build_metadata(faraday_response_body)
+      { 'status' => faraday_response_body['statusCode'],
+        'success' => faraday_response_body['success'],
+        'message' => faraday_response_body['message'] }
     end
 
     def service
