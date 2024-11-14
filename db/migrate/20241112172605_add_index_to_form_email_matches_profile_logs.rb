bdd6c2beec68fadde0b4d5eafad7700f712a05e9
Brandon Cooper <brandon.cooper@adhocteam.us>
2024-11-14 11:33:10 -0500

    add if_not_exists to index migration

--- db/migrate/20241112172605_add_index_to_form_email_matches_profile_logs.rb
+++ db/migrate/20241112172605_add_index_to_form_email_matches_profile_logs.rb
@@ -2,6 +2,10 @@ class AddIndexToFormEmailMatchesProfileLogs < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def change
-    add_index :form_email_matches_profile_logs, [:user_uuid, :in_progress_form_id], unique: true, algorithm: :concurrently
+    add_index :form_email_matches_profile_logs,
+              %i[user_uuid in_progress_form_id],
+              unique: true,
+              algorithm: :concurrently,
+              if_not_exists: true
   end
 end
