a922f41a3cfbc9f1d2032bd8a17fef694a68eb8c
Kyle Henson <145150351+khenson-oddball@users.noreply.github.com>
2024-11-14 13:55:55 -0700

    Add error messaging to Veteran Verification /status endpoint

---
+++ lib/lighthouse/veteran_verification/constants.rb
@@ -0,0 +1,16 @@
+module VeteranVerification
+  module Constants
+    NOT_FOUND_MESSAGE = [
+      'We’re sorry. There’s a problem with your discharge status records. We can’t provide a Veteran status ' \
+        'card for you right now.',
+      'To fix the problem with your records, call the Defense Manpower Data Center at 800-538-9552 (TTY: 711).' \
+        ' They’re open Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.'
+    ]
+    NOT_ELIGIBLE_MESSAGE = [
+      'Our records show that you’re not eligible for a Veteran status card. To get a Veteran status card, you ' \
+        'must have received an honorable discharge for at least one period of service.',
+      'If you think your discharge status is incorrect, call the Defense Manpower Data Center at 800-538-9552 ' \
+        '(TTY: 711). They’re open Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.'
+    ]
+  end
+end
