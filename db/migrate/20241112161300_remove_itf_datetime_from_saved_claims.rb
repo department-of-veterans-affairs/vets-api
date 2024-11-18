d0e60a003d5a3c77c5e341bcda0863252c72578a
Dan Lim <54864006+danlim715@users.noreply.github.com>
2024-11-18 11:39:51 -0600

    Update db/migrate/20241112161300_remove_itf_datetime_from_saved_claims.rb
    Co-authored-by: Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>

--- db/migrate/20241112161300_remove_itf_datetime_from_saved_claims.rb
+++ db/migrate/20241112161300_remove_itf_datetime_from_saved_claims.rb
@@ -1,5 +1,5 @@
 class RemoveItfDatetimeFromSavedClaims < ActiveRecord::Migration[7.1]
   def change
-    safety_assured { remove_column :saved_claims, :itf_datetime, :datetime }
+    safety_assured { remove_column :saved_claims, :itf_datetime, :datetime, if_exists: true }
   end
 end
