3a5b88f69581584d1964f2e5c42a884f0e76313e
Ian Donovan <iandonovan@users.noreply.github.com>
2024-11-19 14:39:56 -0500

    Adds new claims letters to separately-maintained list for mobile app

--- modules/mobile/app/controllers/mobile/v0/decision_letters_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/decision_letters_controller.rb
@@ -45,11 +45,31 @@ module Mobile
       end
 
       # 27: Board Of Appeals Decision Letter
+      # 34: Correspondence
       # 184: Notification Letter (e.g. VA 20-8993, VA 21-0290, PCGL)
-      # 339: Rating Decision Letter
+      # 408: VA Examination Letter
+      # 700: MAP-D Development letter
+      # 704: Standard 5103 Notice
+      # 706: 5103/DTA Letter
+      # 858: Custom 5103 Notice
+      # 859: Subsequent Development letter
+      # 864: General Records Request (Medical)
+      # 942: Final Attempt Letter
+      # 1605: General Records Request (Non-Medical)
       def allowed_doctypes
         doctypes = %w[184]
-        doctypes << '27' unless Flipper.enabled?(:mobile_filter_doc_27_decision_letters_out)
+        doctypes << '27' if Flipper.enabled?(:cst_include_ddl_boa_letters, @current_user) &&
+                            !Flipper.enabled?(:mobile_filter_doc_27_decision_letters_out)
+        doctypes << '704' if Flipper.enabled?(:cst_include_ddl_5103_letters, @current_user)
+        doctypes << '706' if Flipper.enabled?(:cst_include_ddl_5103_letters, @current_user)
+        doctypes << '858' if Flipper.enabled?(:cst_include_ddl_5103_letters, @current_user)
+        doctypes << '34' if Flipper.enabled?(:cst_include_ddl_sqd_letters, @current_user)
+        doctypes << '408' if Flipper.enabled?(:cst_include_ddl_sqd_letters, @current_user)
+        doctypes << '700' if Flipper.enabled?(:cst_include_ddl_sqd_letters, @current_user)
+        doctypes << '859' if Flipper.enabled?(:cst_include_ddl_sqd_letters, @current_user)
+        doctypes << '864' if Flipper.enabled?(:cst_include_ddl_sqd_letters, @current_user)
+        doctypes << '942' if Flipper.enabled?(:cst_include_ddl_sqd_letters, @current_user)
+        doctypes << '1605' if Flipper.enabled?(:cst_include_ddl_sqd_letters, @current_user)
         doctypes
       end
     end
