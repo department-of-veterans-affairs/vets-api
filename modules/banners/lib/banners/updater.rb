4002b46bf0ecd6949267e3313efe2208f312d423
Bryan Thompson <18094023+SnowboardTechie@users.noreply.github.com>
2024-11-19 10:36:47 -0800

    cleanup updater

--- modules/banners/lib/banners/updater.rb
+++ modules/banners/lib/banners/updater.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'banners/engine'
 require 'banners/builder'
 require 'banners/profile/vacms'
 module Banners
@@ -15,23 +14,6 @@ module Banners
     def update_vacms_banners
       vacms_banner_data.each do |banner_data|
         Builder.perform_async(Profile::Vacms.parsed_banner(banner_data))
-      end
-    end
-
-    def vacms_banner_data
-      banner_graphql_query = Rails.root.join('modules', 'banners', 'config', 'vacms_graphql_query.txt')
-      body = { query: File.read(banner_graphql_query) }
-
-      response = connection.post do |req|
-        req.path = 'graphql'
-        req.body = body.to_json
-        req.options.timeout = 300
-      end
-
-      begin
-        JSON.parse(response.body).dig('data', 'nodeQuery', 'entities')
-      rescue JSON::ParserError
-        []
       end
     end
 
@@ -53,6 +35,23 @@ module Banners
       options = { ssl: { verify: false } }
       options[:proxy] = { uri: URI.parse('socks://localhost:2001') } unless Rails.env.production?
       options
+    end
+
+    def vacms_banner_data
+      banner_graphql_query = Rails.root.join('modules', 'banners', 'config', 'vacms_graphql_query.txt')
+      body = { query: File.read(banner_graphql_query) }
+
+      response = connection.post do |req|
+        req.path = 'graphql'
+        req.body = body.to_json
+        req.options.timeout = 300
+      end
+
+      begin
+        JSON.parse(response.body).dig('data', 'nodeQuery', 'entities')
+      rescue JSON::ParserError
+        []
+      end
     end
   end
 end
