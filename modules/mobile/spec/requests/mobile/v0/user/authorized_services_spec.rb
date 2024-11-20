b03863426820618576d93b340ff3c0cb5fa50d38
Andrew Herzberg <andrew.herzberg@adhocteam.us>
2024-11-20 09:52:19 -0700

    remove authorization of appointments if no facilities

--- modules/mobile/spec/requests/mobile/v0/user/authorized_services_spec.rb
+++ modules/mobile/spec/requests/mobile/v0/user/authorized_services_spec.rb
@@ -6,65 +6,33 @@ require_relative '../../../../support/helpers/committee_helper'
 RSpec.describe 'Mobile::V0::User::AuthorizedServices', type: :request do
   include CommitteeHelper
 
+  let!(:user) { sis_user(vha_facility_ids: [402, 555]) }
   let(:attributes) { response.parsed_body.dig('data', 'attributes') }
 
   describe 'GET /mobile/v0/user/authorized-services' do
-    context 'when user has at least one treatment facility' do
-      let!(:user) { sis_user(vha_facility_ids: [402, 555]) }
+    it 'includes a hash with all available services and a boolean value of if the user has access' do
+      get '/mobile/v0/user/authorized-services', headers: sis_headers,
+                                                 params: { 'appointmentIEN' => '123', 'locationId' => '123' }
+      assert_schema_conform(200)
 
-      it 'gives access to appointments and appointment scheduling' do
-        get '/mobile/v0/user/authorized-services', headers: sis_headers,
-                                                   params: { 'appointmentIEN' => '123', 'locationId' => '123' }
-        assert_schema_conform(200)
-
-        expect(attributes['authorizedServices']).to eq(
-          { 'appeals' => true,
-            'appointments' => true,
-            'claims' => true,
-            'decisionLetters' => true,
-            'directDepositBenefits' => true,
-            'directDepositBenefitsUpdate' => true,
-            'disabilityRating' => true,
-            'genderIdentity' => true,
-            'lettersAndDocuments' => true,
-            'militaryServiceHistory' => true,
-            'paymentHistory' => true,
-            'preferredName' => true,
-            'prescriptions' => false,
-            'scheduleAppointments' => true,
-            'secureMessaging' => false,
-            'userProfileUpdate' => true }
-        )
-      end
-    end
-
-    context 'when user has no treatment facility' do
-      let!(:user) { sis_user(vha_facility_ids: []) }
-
-      it 'does not give access to appointments or appointment scheduling' do
-        get '/mobile/v0/user/authorized-services', headers: sis_headers,
-                                                   params: { 'appointmentIEN' => '123', 'locationId' => '123' }
-        assert_schema_conform(200)
-
-        expect(attributes['authorizedServices']).to eq(
-          { 'appeals' => true,
-            'appointments' => false,
-            'claims' => true,
-            'decisionLetters' => true,
-            'directDepositBenefits' => true,
-            'directDepositBenefitsUpdate' => true,
-            'disabilityRating' => true,
-            'genderIdentity' => true,
-            'lettersAndDocuments' => true,
-            'militaryServiceHistory' => true,
-            'paymentHistory' => true,
-            'preferredName' => true,
-            'prescriptions' => false,
-            'scheduleAppointments' => false,
-            'secureMessaging' => false,
-            'userProfileUpdate' => true }
-        )
-      end
+      expect(attributes['authorizedServices']).to eq(
+        { 'appeals' => true,
+          'appointments' => true,
+          'claims' => true,
+          'decisionLetters' => true,
+          'directDepositBenefits' => true,
+          'directDepositBenefitsUpdate' => true,
+          'disabilityRating' => true,
+          'genderIdentity' => true,
+          'lettersAndDocuments' => true,
+          'militaryServiceHistory' => true,
+          'paymentHistory' => true,
+          'preferredName' => true,
+          'prescriptions' => false,
+          'scheduleAppointments' => true,
+          'secureMessaging' => false,
+          'userProfileUpdate' => true }
+      )
     end
   end
 end
