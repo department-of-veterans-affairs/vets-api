51cc00f652ef435d4b99f58d78166898ae22a871
Andrew Herzberg <andrew.herzberg@adhocteam.us>
2024-11-20 09:29:49 -0700

    remove authorization of appointments if no facilities

--- modules/mobile/spec/requests/mobile/v0/user/authorized_services_spec.rb
+++ modules/mobile/spec/requests/mobile/v0/user/authorized_services_spec.rb
@@ -1,34 +1,70 @@
 # frozen_string_literal: true
 
 require_relative '../../../../support/helpers/rails_helper'
+require_relative '../../../../support/helpers/committee_helper'
+
 RSpec.describe 'Mobile::V0::User::AuthorizedServices', type: :request do
-  let!(:user) { sis_user }
+  include CommitteeHelper
+
   let(:attributes) { response.parsed_body.dig('data', 'attributes') }
 
   describe 'GET /mobile/v0/user/authorized-services' do
-    it 'includes a hash with all available services and a boolean value of if the user has access' do
-      get '/mobile/v0/user/authorized-services', headers: sis_headers,
-                                                 params: { 'appointmentIEN' => '123', 'locationId' => '123' }
-      expect(response).to have_http_status(:ok)
+    context 'when user has at least one treatment facility' do
+      let!(:user) { sis_user(vha_facility_ids: [402, 555]) }
 
-      expect(attributes['authorizedServices']).to eq(
-        { 'appeals' => true,
-          'appointments' => true,
-          'claims' => true,
-          'decisionLetters' => true,
-          'directDepositBenefits' => true,
-          'directDepositBenefitsUpdate' => true,
-          'disabilityRating' => true,
-          'genderIdentity' => true,
-          'lettersAndDocuments' => true,
-          'militaryServiceHistory' => true,
-          'paymentHistory' => true,
-          'preferredName' => true,
-          'prescriptions' => false,
-          'scheduleAppointments' => false,
-          'secureMessaging' => false,
-          'userProfileUpdate' => true }
-      )
+      it 'gives access to appointments and appointment scheduling' do
+        get '/mobile/v0/user/authorized-services', headers: sis_headers,
+                                                   params: { 'appointmentIEN' => '123', 'locationId' => '123' }
+        assert_schema_conform(200)
+
+        expect(attributes['authorizedServices']).to eq(
+          { 'appeals' => true,
+            'appointments' => true,
+            'claims' => true,
+            'decisionLetters' => true,
+            'directDepositBenefits' => true,
+            'directDepositBenefitsUpdate' => true,
+            'disabilityRating' => true,
+            'genderIdentity' => true,
+            'lettersAndDocuments' => true,
+            'militaryServiceHistory' => true,
+            'paymentHistory' => true,
+            'preferredName' => true,
+            'prescriptions' => false,
+            'scheduleAppointments' => true,
+            'secureMessaging' => false,
+            'userProfileUpdate' => true }
+        )
+      end
+    end
+
+    context 'when user has no treatment facility' do
+      let!(:user) { sis_user(vha_facility_ids: []) }
+
+      it 'does not give access to appointments or appointment scheduling' do
+        get '/mobile/v0/user/authorized-services', headers: sis_headers,
+                                                   params: { 'appointmentIEN' => '123', 'locationId' => '123' }
+        assert_schema_conform(200)
+
+        expect(attributes['authorizedServices']).to eq(
+          { 'appeals' => true,
+            'appointments' => false,
+            'claims' => true,
+            'decisionLetters' => true,
+            'directDepositBenefits' => true,
+            'directDepositBenefitsUpdate' => true,
+            'disabilityRating' => true,
+            'genderIdentity' => true,
+            'lettersAndDocuments' => true,
+            'militaryServiceHistory' => true,
+            'paymentHistory' => true,
+            'preferredName' => true,
+            'prescriptions' => false,
+            'scheduleAppointments' => false,
+            'secureMessaging' => false,
+            'userProfileUpdate' => true }
+        )
+      end
     end
   end
 end
