c06cd7c94fd8413df711de2969afc6ca79a1cd7b
s-caso <sebastian.caso@accenturefederal.com>
2024-11-20 12:42:58 -0500

    update form_type assignment

--- modules/meb_api/app/controllers/meb_api/v0/forms_controller.rb
+++ modules/meb_api/app/controllers/meb_api/v0/forms_controller.rb
@@ -40,12 +40,12 @@ module MebApi
         claimant_id = forms_claimant_response['claimant']&.dig('claimant_id')
 
         if claimant_id.blank?
-          form_type = @form_type || 'toe'
           forms_claimant_response = claimant_service.get_claimant_info(form_type)
           claimant_id = forms_claimant_response['claimant_id']
         end
 
-        claim_status_response = claim_status_service.get_claim_status(params, claimant_id, @form_type)
+        form_type = @form_type || 'toe'
+        claim_status_response = claim_status_service.get_claim_status(params, claimant_id, form_type)
         response = valid_claimant_response?(forms_claimant_response) ? claim_status_response : forms_claimant_response
         srlzer = valid_claimant_response?(forms_claimant_response) ? ClaimStatusSerializer : ToeClaimantInfoSerializer
 
