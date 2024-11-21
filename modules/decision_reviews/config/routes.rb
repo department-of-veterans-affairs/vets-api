b3c8127aa51c8b22334c92fcc50c4e6bb8ff22a2
Molly Trombley-McCann <molly.trombley-mccann@coforma.io>
2024-11-20 19:40:08 -0800

    WIP move sidekiq jobs into module

--- modules/decision_reviews/config/routes.rb
+++ modules/decision_reviews/config/routes.rb
@@ -1,5 +1,4 @@
+# frozen_string_literal: true
+
 DecisionReviews::Engine.routes.draw do
-  namespace :v1, defaults: { format: :json } do
-    resources :nod_callbacks, only: [:create], controller: :decision_review_notification_callbacks
-  end
 end
