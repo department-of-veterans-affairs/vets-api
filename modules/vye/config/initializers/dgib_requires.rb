43f64469c97351f0bde7c737263cfbc0bba4930b
RachalCassity <rachal.cassity@oddball.io>
2024-11-21 15:35:41 -0600

    dbgib_requires

--- modules/vye/config/initializers/dgib_requires.rb
+++ modules/vye/config/initializers/dgib_requires.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 # Zeitwerk was giving me fits until I added this.
-# It's a little ugly, but it works.
-require Rails.root.join('modules', 'vye', 'lib', 'dgib', 'claimant_lookup', 'service')
-require Rails.root.join('modules', 'vye', 'lib', 'dgib', 'claimant_status', 'service')
-require Rails.root.join('modules', 'vye', 'lib', 'dgib', 'verification_record', 'service')
-require Rails.root.join('modules', 'vye', 'lib', 'dgib', 'verify_claimant', 'service')
+require Rails.root.join('modules', 'vye', 'lib', 'dgib', 'service')
