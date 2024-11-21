dd4ade0615ff5dc6d37a0ae58c17a7a17aa88dbb
Bryan Thompson <18094023+SnowboardTechie@users.noreply.github.com>
2024-11-21 11:04:44 -0800

    return when rendering error

--- app/controllers/v0/banners_controller.rb
+++ app/controllers/v0/banners_controller.rb
@@ -8,7 +8,7 @@ module V0
     def by_path
       # Default to 'full_width_banner_alert' banner (bundle) type.
       banner_type = params.fetch(:type, 'full_width_banner_alert')
-      render json: { error: 'Path parameter is required' }, status: :bad_request if path.blank?
+      return render json: { error: 'Path parameter is required' }, status: :bad_request if path.blank?
 
       banners = Banner.where(entity_bundle: banner_type).by_path(path)
       render json: { banners:, path:, banner_type: }
