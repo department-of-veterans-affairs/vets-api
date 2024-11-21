dc186321227dadcd90ba67d214ff9ec242fef276
Adrian Rollett <acrollet@gmail.com>
2024-11-21 11:04:50 -0700

    Update modules/my_health/app/controllers/my_health/v1/vitals_controller.rb

--- modules/my_health/app/controllers/my_health/v1/vitals_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/vitals_controller.rb
@@ -4,7 +4,7 @@ module MyHealth
   module V1
     class VitalsController < MrController
       def index
-        resource = client.list_vitals(params[:from] || nil, params[:to] || nil)
+        resource = client.list_vitals(params[:from], params[:to])
         render json: resource.to_json
       end
     end
