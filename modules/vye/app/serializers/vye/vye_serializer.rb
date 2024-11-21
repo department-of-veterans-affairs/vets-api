3a36481291b6bf9442fd448633f71d8f8938a396
RachalCassity <rachal.cassity@oddball.io>
2024-11-21 16:07:37 -0600

    condense serializers

--- modules/vye/app/serializers/vye/vye_serializer.rb
+++ modules/vye/app/serializers/vye/vye_serializer.rb
@@ -16,4 +16,35 @@ module Vye
       @resource&.status
     end
   end
+
+  class ClaimantLookupSerializer < VyeSerializer
+    def serializable_hash
+      {
+        claimant_id: @resource&.claimant_id
+      }
+    end
+  end
+
+  class ClaimantVerificationSerializer < VyeSerializer
+    def serializable_hash
+      {
+        claimant_id: @resource&.claimant_id,
+        delimiting_date: @resource&.delimiting_date,
+        enrollment_verifications: @resource&.enrollment_verifications,
+        verified_details: @resource&.verified_details,
+        payment_on_hold: @resource&.payment_on_hold
+      }
+    end
+  end
+
+  class VerifyClaimantSerializer < VyeSerializer
+    def serializable_hash
+      {
+        claimant_id: @resource&.claimant_id,
+        delimiting_date: @resource&.delimiting_date,
+        verified_details: @resource&.verified_details,
+        payment_on_hold: @resource&.payment_on_hold
+      }
+    end
+  end
 end
