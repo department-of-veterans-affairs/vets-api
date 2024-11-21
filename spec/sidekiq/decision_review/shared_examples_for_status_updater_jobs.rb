07050d806b72620dd4f6c0e4ab49af15791ffb05
Molly Trombley-McCann <molly.trombley-mccann@coforma.io>
2024-11-21 12:53:19 -0800

    Change - add all shared examples in one PR - not using them in any spec files yet

--- spec/sidekiq/decision_review/shared_examples_for_status_updater_jobs.rb
+++ spec/sidekiq/decision_review/shared_examples_for_status_updater_jobs.rb
@@ -117,6 +117,40 @@ RSpec.shared_examples 'status updater job with base forms' do |subclass|
   context 'SavedClaim record with previous metadata' do
     let(:guid4) { SecureRandom.uuid }
     let(:guid5) { SecureRandom.uuid }
+    # let(:upload_id) { SecureRandom.uuid }
+    # let(:upload_id2) { SecureRandom.uuid }
+    # let(:upload_id3) { SecureRandom.uuid }
+
+    # let(:metadata1) do
+    #   {
+    #     'status' => 'submitted',
+    #     'uploads' => [
+    #       {
+    #         'status' => 'error',
+    #         'detail' => 'Invalid PDF',
+    #         'id' => upload_id
+    #       }
+    #     ]
+    #   }
+    # end
+
+    # let(:metadata2) do
+    #   {
+    #     'status' => 'submitted',
+    #     'uploads' => [
+    #       {
+    #         'status' => 'pending',
+    #         'detail' => nil,
+    #         'id' => upload_id2
+    #       },
+    #       {
+    #         'status' => 'processing',
+    #         'detail' => nil,
+    #         'id' => upload_id3
+    #       }
+    #     ]
+    #   }
+    # end
 
     before do
       subclass.create(guid: guid1, form: '{}', metadata: '{"status":"error","uploads":[]}')
@@ -204,6 +238,185 @@ RSpec.shared_examples 'status updater job with base forms' do |subclass|
           .with("#{log_prefix} error", { guid: anything, message: anything })
           .exactly(1).time
       end
+    end
+  end
+end
+
+RSpec.shared_examples 'status updater job when forms include evidence' do |subclass|
+  let(:upload_response_vbms) do
+    response = JSON.parse(File.read('spec/fixtures/supplemental_claims/SC_upload_show_response_200.json'))
+    instance_double(Faraday::Response, body: response)
+  end
+
+  let(:upload_response_processing) do
+    response = JSON.parse(File.read('spec/fixtures/supplemental_claims/SC_upload_show_response_200.json'))
+    response['data']['attributes']['status'] = 'processing'
+    instance_double(Faraday::Response, body: response)
+  end
+
+  let(:upload_response_error) do
+    response = JSON.parse(File.read('spec/fixtures/supplemental_claims/SC_upload_show_response_200.json'))
+    response['data']['attributes']['status'] = 'error'
+    response['data']['attributes']['detail'] = 'Invalid PDF'
+    instance_double(Faraday::Response, body: response)
+  end
+
+  let(:evidence_service_method) { SUBCLASS_INFO[subclass][:evidence_service_method].to_sym }
+
+  context 'SavedClaim records are present with completed status in LH and have associated evidence uploads' do
+    let(:guid4) { SecureRandom.uuid }
+    let(:guid5) { SecureRandom.uuid }
+
+    let(:upload_id) { SecureRandom.uuid }
+    let(:upload_id2) { SecureRandom.uuid }
+    let(:upload_id3) { SecureRandom.uuid }
+    let(:upload_id4) { SecureRandom.uuid }
+
+    let(:metadata1) do
+      {
+        'status' => 'submitted',
+        'uploads' => [
+          {
+            'status' => 'error',
+            'detail' => 'Invalid PDF',
+            'id' => upload_id
+          }
+        ]
+      }
+    end
+
+    let(:metadata2) do
+      {
+        'status' => 'submitted',
+        'uploads' => [
+          {
+            'status' => 'pending',
+            'detail' => nil,
+            'id' => upload_id2
+          },
+          {
+            'status' => 'processing',
+            'detail' => nil,
+            'id' => upload_id3
+          }
+        ]
+      }
+    end
+
+    before do
+      allow(Rails.logger).to receive(:info)
+    end
+
+    it 'only sets delete_date for subclass with all attachments in vbms status' do
+      subclass.create(guid: guid1, form: '{}')
+      subclass.create(guid: guid2, form: '{}')
+      subclass.create(guid: guid3, form: '{}')
+
+      appeal_submission = create(:appeal_submission, submitted_appeal_uuid: guid1)
+      create(:appeal_submission_upload, appeal_submission:, lighthouse_upload_id: upload_id)
+
+      appeal_submission2 = create(:appeal_submission, submitted_appeal_uuid: guid2)
+      create(:appeal_submission_upload, appeal_submission: appeal_submission2, lighthouse_upload_id: upload_id2)
+
+      # One upload vbms, other one still processing
+      appeal_submission3 = create(:appeal_submission, submitted_appeal_uuid: guid3)
+      create(:appeal_submission_upload, appeal_submission: appeal_submission3, lighthouse_upload_id: upload_id3)
+      create(:appeal_submission_upload, appeal_submission: appeal_submission3, lighthouse_upload_id: upload_id4)
+
+      expect(service).to receive(evidence_service_method).with(guid: upload_id)
+                                                         .and_return(upload_response_vbms)
+      expect(service).to receive(evidence_service_method).with(guid: upload_id2)
+                                                         .and_return(upload_response_processing)
+      expect(service).to receive(evidence_service_method).with(guid: upload_id3)
+                                                         .and_return(upload_response_vbms)
+      expect(service).to receive(evidence_service_method).with(guid: upload_id4)
+                                                         .and_return(upload_response_processing)
+
+      expect(service).to receive(service_method).with(guid1).and_return(response_complete)
+      expect(service).to receive(service_method).with(guid2).and_return(response_complete)
+      expect(service).to receive(service_method).with(guid3).and_return(response_complete)
+
+      frozen_time = DateTime.new(2024, 1, 1).utc
+
+      Timecop.freeze(frozen_time) do
+        subject.new.perform
+
+        claim1 = subclass.find_by(guid: guid1)
+        expect(claim1.delete_date).to eq frozen_time + 59.days
+        expect(claim1.metadata_updated_at).to eq frozen_time
+        expect(claim1.metadata).to include 'complete'
+        expect(claim1.metadata).to include 'vbms'
+
+        claim2 = subclass.find_by(guid: guid2)
+        expect(claim2.delete_date).to be_nil
+        expect(claim2.metadata_updated_at).to eq frozen_time
+        expect(claim2.metadata).to include 'complete'
+        expect(claim2.metadata).to include 'processing'
+
+        claim3 = subclass.find_by(guid: guid3)
+        expect(claim3.delete_date).to be_nil
+        expect(claim3.metadata_updated_at).to eq frozen_time
+
+        metadata3 = JSON.parse(claim3.metadata)
+        expect(metadata3['status']).to eq 'complete'
+        expect(metadata3['uploads'].pluck('id', 'status'))
+          .to contain_exactly([upload_id3, 'vbms'], [upload_id4, 'processing'])
+      end
+
+      expect(StatsD).to have_received(:increment)
+        .with("#{statsd_prefix}.processing_records", 3).exactly(1).time
+      expect(StatsD).to have_received(:increment)
+        .with("#{statsd_prefix}.delete_date_update").exactly(1).time
+      expect(StatsD).to have_received(:increment)
+        .with("#{statsd_prefix}.status", tags: ['status:complete'])
+        .exactly(2).times
+      expect(StatsD).to have_received(:increment)
+        .with("#{statsd_prefix}_upload.status", tags: ['status:vbms'])
+        .exactly(2).times
+      expect(StatsD).to have_received(:increment)
+        .with("#{statsd_prefix}_upload.status", tags: ['status:processing'])
+        .exactly(2).times
+      expect(Rails.logger).not_to have_received(:info)
+        .with("#{log_prefix} evidence status error", anything)
+    end
+
+    it 'does not increment metrics for unchanged evidence status or existing final statuses' do
+      subclass.create(guid: guid1, form: '{}', metadata: metadata1.to_json)
+      appeal_submission = create(:appeal_submission, submitted_appeal_uuid: guid1)
+      create(:appeal_submission_upload, appeal_submission:, lighthouse_upload_id: upload_id)
+
+      subclass.create(guid: guid2, form: '{}', metadata: metadata2.to_json)
+      appeal_submission2 = create(:appeal_submission, submitted_appeal_uuid: guid2)
+      create(:appeal_submission_upload, appeal_submission: appeal_submission2, lighthouse_upload_id: upload_id2)
+      create(:appeal_submission_upload, appeal_submission: appeal_submission2, lighthouse_upload_id: upload_id3)
+
+      expect(service).to receive(service_method).with(guid1).and_return(response_pending)
+      expect(service).to receive(service_method).with(guid2).and_return(response_error)
+
+      expect(service).not_to receive(evidence_service_method).with(guid: upload_id)
+      expect(service).to receive(evidence_service_method).with(guid: upload_id2)
+                                                         .and_return(upload_response_error)
+      expect(service).to receive(evidence_service_method).with(guid: upload_id3)
+                                                         .and_return(upload_response_processing)
+
+      subject.new.perform
+
+      expect(StatsD).to have_received(:increment)
+        .with("#{statsd_prefix}.status", tags: ['status:error'])
+        .exactly(1).times
+      expect(StatsD).not_to have_received(:increment)
+        .with("#{statsd_prefix}.status", tags: ['status:processing'])
+
+      expect(Rails.logger).not_to have_received(:info)
+        .with("#{log_prefix} evidence status error",
+              guid: anything, lighthouse_upload_id: upload_id, detail: anything)
+      expect(Rails.logger).to have_received(:info)
+        .with("#{log_prefix} evidence status error",
+              guid: guid2, lighthouse_upload_id: upload_id2, detail: 'Invalid PDF')
+      expect(StatsD).to have_received(:increment)
+        .with('silent_failure', tags: [service_tag,
+                                       'function: evidence submission to Lighthouse'])
+        .exactly(1).time
     end
   end
 end
