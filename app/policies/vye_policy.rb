5107668831cd6093998b5a06fa12654d0ed6847e
GcioGregg <gregg.puhala@va.gov>
2024-11-21 14:08:49 -0800

    add alias to policy

--- app/policies/vye_policy.rb
+++ app/policies/vye_policy.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 VyePolicy = Struct.new(:user, :user_info) do
   def access?
     return true if user.present?
 
     false
   end
+
+  alias_method :claimant_lookup?, :access?
 end
