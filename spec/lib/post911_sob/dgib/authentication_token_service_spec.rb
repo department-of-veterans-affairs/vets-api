7b56db0a90336fda2acdd556b7a9f205940b746a
Jeff Marks <jeff.t.marks@gmail.com>
2024-11-21 10:56:27 -0700

    Linting

--- spec/lib/post911_sob/dgib/authentication_token_service_spec.rb
+++ spec/lib/post911_sob/dgib/authentication_token_service_spec.rb
@@ -6,7 +6,7 @@ require 'post911_sob/dgib/authentication_token_service'
 Rspec.describe Post911SOB::DGIB::AuthenticationTokenService do
   describe '.call' do
     let(:token) { described_class.call }
-    
+
     it 'returns an authentication token' do
       decoded_token = JWT.decode(token,
                                  described_class::RSA_PUBLIC,
@@ -14,17 +14,17 @@ Rspec.describe Post911SOB::DGIB::AuthenticationTokenService do
                                  { algorithm: described_class::ALGORITHM_TYPE,
                                    kid: described_class::KID,
                                    typ: described_class::TYP })
-                expect(decoded_token).to eq(
-                [{
-                  'nbf' => Time.now.to_i,
-                  'exp' => Time.now.to_i + (5 * 60),
-                  'realm_access' => { "roles"=> ["SOB"] }
-                }, {
-                  'kid' => 'sob',
-                  'typ' => 'JWT',
-                  'alg' => 'RS256'
-                }]
-              )
+      expect(decoded_token).to eq(
+        [{
+          'nbf' => Time.now.to_i,
+          'exp' => Time.now.to_i + (5 * 60),
+          'realm_access' => { 'roles' => ['SOB'] }
+        }, {
+          'kid' => 'sob',
+          'typ' => 'JWT',
+          'alg' => 'RS256'
+        }]
+      )
     end
   end
 end
