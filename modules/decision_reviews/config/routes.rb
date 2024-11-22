aa25ac27571f8831161a8054384e626d755b2d4e
Molly Trombley-McCann <molly.trombley-mccann@coforma.io>
2024-11-22 14:16:13 -0800

    WIP move form creation controllers

--- modules/decision_reviews/config/routes.rb
+++ modules/decision_reviews/config/routes.rb
@@ -2,6 +2,10 @@
 
 DecisionReviews::Engine.routes.draw do
   namespace :v1, defaults: { format: :json } do
+    resources :notice_of_disagreements, only: %i[create show]
+
+    resource :decision_review_evidence, only: :create
+
     resources :nod_callbacks, only: [:create], controller: :decision_review_notification_callbacks
   end
 end
