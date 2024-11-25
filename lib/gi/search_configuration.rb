9127391ea273b7a85e0ec1e1c5a93b7b8321687f
Jeff Marks <jeff.t.marks@gmail.com>
2024-11-25 14:23:09 -0700

    Update search client/config to use mocks if program type is flight

--- lib/gi/search_configuration.rb
+++ lib/gi/search_configuration.rb
@@ -4,5 +4,11 @@ module GI
   class SearchConfiguration < GI::Configuration
     self.read_timeout = Settings.gids.search&.read_timeout || 4
     self.open_timeout = Settings.gids.search&.open_timeout || 4
+
+    # Mock response if querying for flight school programs
+    # TO-DO: Remove after flight school program data becomes accessible
+    def use_mocks?
+      (@program_type_flight && Settings.gids.use_mocks) || false
+    end
   end
 end
