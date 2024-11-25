58c5d402924b93cb159223cbc7b7def2b6f9a79a
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-11-25 16:14:06 -0600

    API-41369-remove-v1-base-method-bgs-ext
    * Adds service file
    new file:   modules/claims_api/lib/bgs_service/standard_data_service.rb

---
+++ modules/claims_api/lib/bgs_service/standard_data_service.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module ClaimsApi
+  class StandardDataService < ClaimsApi::LocalBGS
+    def bean_name
+      'StandardDataService'
+    end
+
+    def data
+      self
+    end
+
+    def get_contention_classification_type_code_list
+      body = Nokogiri::XML::DocumentFragment.parse <<~EOXML
+        <getContentionClassificationTypeCodeList/>
+      EOXML
+
+      response = make_request(endpoint: bean_name, action: 'getContentionClassificationTypeCodeList', body:)
+      response[:return]
+    end
+  end
+end
