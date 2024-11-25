fa3ca7b06828d313413938ae4b7266387c1380e7
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2024-11-25 16:05:05 -0700

    updates va_notify::notification factory to permit empty callback_klass

--- modules/va_notify/spec/factories/notifications.rb
+++ modules/va_notify/spec/factories/notifications.rb
@@ -26,7 +26,7 @@ FactoryBot.define do
     }
     provider { %w[ses twilio pinpoint].sample }
     source_location { 'SomeTeam' }
-    callback_klass { 'SomeClass' }
+    callback_klass { }
     callback_metadata { {} }
     template_id { SecureRandom.uuid }
   end
