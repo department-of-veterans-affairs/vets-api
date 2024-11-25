447f7e6089fad87d8320f117fee981c112705e83
Josh Fike <josh.fike@oddball.io>
2024-11-25 11:22:56 -0600

    Restore original param structure

--- modules/representation_management/app/controllers/representation_management/v0/accredited_entities_for_appoint_controller.rb
+++ modules/representation_management/app/controllers/representation_management/v0/accredited_entities_for_appoint_controller.rb
@@ -8,7 +8,7 @@ module RepresentationManagement
       before_action :feature_enabled
 
       def index
-        data = RepresentationManagement::AccreditedEntityQuery.new(form_params[:query]).results
+        data = RepresentationManagement::AccreditedEntityQuery.new(params[:query]).results
         json_response = data.map do |record|
           if record.is_a?(AccreditedIndividual)
             RepresentationManagement::AccreditedEntities::IndividualSerializer.new(record).serializable_hash
@@ -20,10 +20,6 @@ module RepresentationManagement
       end
 
       private
-
-      def form_params
-        params.permit(:query)
-      end
 
       def feature_enabled
         routing_error unless Flipper.enabled?(:appoint_a_representative_enable_pdf)
