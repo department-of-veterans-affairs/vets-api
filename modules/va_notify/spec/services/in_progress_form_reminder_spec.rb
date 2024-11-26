56d4395ec43daef9fc3a36267752b1af4944ac16
nathanbwright <nathanielbwright@gmail.com>
2024-11-26 15:39:50 -0700

    Refactor in progress reminder metadata

--- modules/va_notify/spec/services/in_progress_form_reminder_spec.rb
+++ modules/va_notify/spec/services/in_progress_form_reminder_spec.rb
@@ -10,20 +10,6 @@ describe VANotify::InProgressFormReminder, type: :worker do
   end
 
   describe '#perform' do
-    it 'skips sending if ICN is not present' do
-      user_without_icn = double('VANotify::Veteran')
-      allow(VANotify::Veteran).to receive(:new).and_return(user_without_icn)
-      allow(user_without_icn).to receive_messages(first_name: 'first_name', icn: nil)
-
-      allow(VANotify::UserAccountJob).to receive(:perform_async)
-
-      Sidekiq::Testing.inline! do
-        described_class.new.perform(in_progress_form.id)
-      end
-
-      expect(VANotify::UserAccountJob).not_to have_received(:perform_async)
-    end
-
     it 'skips sending reminder email if there is no first name' do
       veteran_double = double('VaNotify::Veteran')
       allow(veteran_double).to receive_messages(icn: 'icn', first_name: nil)
@@ -74,7 +60,6 @@ describe VANotify::InProgressFormReminder, type: :worker do
           described_class.new.perform(in_progress_form.id)
         end
 
-        callback_klass = 'VANotify::InProgressFormReminderCallback'
         expect(VANotify::UserAccountJob).to have_received(:perform_async)
           .with(in_progress_form.user_account_id, 'fake_template_id',
                 {
@@ -83,8 +68,11 @@ describe VANotify::InProgressFormReminder, type: :worker do
                   'form_age' => ''
                 },
                 'fake_secret',
-                { callback: callback_klass,
-                  metadata: '686C-674' })
+                { callback_metadata: {
+                  form_number: '686C-674', notification_type: 'in_progress_reminder', statsd_tags: {
+                    'function' => '686C-674 in progress reminder', 'service' => 'va-notify'
+                  }
+                } })
       end
     end
 
@@ -164,8 +152,11 @@ describe VANotify::InProgressFormReminder, type: :worker do
             'formatted_form_data' => "\n^ FORM 686C-674\n^\n^__686c something__\n^\n^_Application expires on:_ #{form_1_date}\n\n\n^---\n\n^ FORM form_3_example_id\n^\n^__form_3 something__\n^\n^_Application expires on:_ #{form_3_date}\n\n\n^---\n\n^ FORM form_2_example_id\n^\n^__form_2 something__\n^\n^_Application expires on:_ #{form_2_date}\n\n"
           },
           'fake_secret',
-          { callback: 'VANotify::InProgressFormReminderCallback',
-            metadata: '686C-674' }
+          { callback_metadata: {
+            form_number: 'multiple', notification_type: 'in_progress_reminder', statsd_tags: {
+              'function' => 'multiple in progress reminder', 'service' => 'va-notify'
+            }
+          } }
         )
         # rubocop:enable Layout/LineLength
       end
