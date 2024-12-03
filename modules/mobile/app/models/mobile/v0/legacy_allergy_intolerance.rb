076d9ad67cc29496981e3c1d0fce36b366da190c
Stephen Barrs <stephenBarrs@users.noreply.github.com>
2024-12-03 15:58:52 -0500

    Update modules/mobile/app/models/mobile/v0/legacy_allergy_intolerance.rb
    Co-authored-by: Adrian Rollett <101649+acrollet@users.noreply.github.com>

--- modules/mobile/app/models/mobile/v0/legacy_allergy_intolerance.rb
+++ modules/mobile/app/models/mobile/v0/legacy_allergy_intolerance.rb
@@ -15,7 +15,7 @@ module Mobile
       attribute :recorder, Types::Hash
       attribute :notes, Types::Array
       attribute :reactions, Types::Array
-      attribute :category, Types::Array
+      attribute :category, Types::Array.of(Types::String)
     end
   end
 end
