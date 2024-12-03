314abd34bbebd2f5e4880cbd37bf44097b97f1aa
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2024-12-03 14:34:37 -0700

    Removes actions and service definitions for ITF. Removes corresponding tests.

--- modules/claims_api/spec/lib/claims_api/find_definition_spec.rb
+++ modules/claims_api/spec/lib/claims_api/find_definition_spec.rb
@@ -30,20 +30,6 @@ describe ClaimsApi::LocalBGSRefactored::FindDefinition do
         end
       end
 
-      context 'IntentToFileWebServiceBean' do
-        let(:endpoint) { 'IntentToFileWebServiceBean/IntentToFileWebService' }
-        let(:action) { 'insertIntentToFile' }
-        let(:key) { 'IntentToFileDTO' }
-
-        it 'response with the correct attributes' do
-          result = subject.for_action(endpoint, action)
-          parsed_result = JSON.parse(result.to_json)
-          expect(parsed_result['service']['bean']['path']).to eq 'IntentToFileWebServiceBean'
-          expect(parsed_result['service']['path']).to eq 'IntentToFileWebService'
-          expect(parsed_result['service']['bean']['namespaces']['target']).to eq 'http://intenttofile.services.vetsnet.vba.va.gov/'
-        end
-      end
-
       context 'OrgWebServiceBean' do
         let(:endpoint) { 'OrgWebServiceBean/OrgWebService' }
         let(:action) { 'findPoaHistoryByPtcpntId' }
@@ -231,18 +217,6 @@ describe ClaimsApi::LocalBGSRefactored::FindDefinition do
           expect(parsed_result['bean']['path']).to eq 'EBenefitsBnftClaimStatusWebServiceBean'
           expect(parsed_result['path']).to eq 'EBenefitsBnftClaimStatusWebService'
           expect(parsed_result['bean']['namespaces']['target']).to eq 'http://claimstatus.services.ebenefits.vba.va.gov/'
-        end
-      end
-
-      context 'IntentToFileWebServiceBean' do
-        let(:endpoint) { 'IntentToFileWebServiceBean/IntentToFileWebService' }
-
-        it 'response with the correct namespace' do
-          result = subject.for_service(endpoint)
-          parsed_result = JSON.parse(result.to_json)
-          expect(parsed_result['bean']['path']).to eq 'IntentToFileWebServiceBean'
-          expect(parsed_result['path']).to eq 'IntentToFileWebService'
-          expect(parsed_result['bean']['namespaces']['target']).to eq 'http://intenttofile.services.vetsnet.vba.va.gov/'
         end
       end
 
