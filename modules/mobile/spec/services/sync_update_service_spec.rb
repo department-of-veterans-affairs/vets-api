25262d4c4212c08d8d9a529860872e26416540db
RachalCassity <rachal.cassity@oddball.io>
2024-12-03 19:43:39 -0600

    mobile specs

--- modules/mobile/spec/services/sync_update_service_spec.rb
+++ modules/mobile/spec/services/sync_update_service_spec.rb
@@ -7,12 +7,7 @@ describe Mobile::V0::Profile::SyncUpdateService do
   let(:service) { Mobile::V0::Profile::SyncUpdateService.new(user) }
 
   # DO THIS
-  describe '#save_and_await_response' do
-    before do
-      Flipper.disable(:va_v3_contact_information_service)
-      Flipper.disable(:remove_pciu)
-    end
-
+  describe '#save_and_await_response', :skip_va_profile_user do
     let(:params) { build(:va_profile_address, vet360_id: user.vet360_id, validation_key: nil) }
 
     context 'when it succeeds after one incomplete status check' do
