622b58fc0621dda0e3da561b45a0bad733044511
Don Shin <cloudmagic80@gmail.com>
2024-12-04 00:32:55 -0600

    redo multistamper

--- modules/ivc_champva/app/services/ivc_champva/pdf_stamper.rb
+++ modules/ivc_champva/app/services/ivc_champva/pdf_stamper.rb
@@ -47,26 +47,113 @@ module IvcChampva
       end
     end
 
-    def self.multistamp(stamped_template_path, signature_text, page_configuration, font_size = 16)
-      stamp_path = Common::FileHelpers.random_file_path
-      Prawn::Document.generate(stamp_path, margin: [0, 0]) do |pdf|
-        page_configuration.each do |config|
-          case config[:type]
-          when :text
-            pdf.draw_text signature_text, at: config[:position], size: font_size
-          when :new_page
-            pdf.start_new_page
-          end
-        end
-      end
+    #   def self.multistamp(stamped_template_path, signature_text, page_configuration, font_size = 16)
+    #     if Flipper.enabled?(:champva_multiple_stamp_retry, @user)
+    #     attempt ||= 0
+    #     stamp_path = Common::FileHelpers.random_file_path
 
-      perform_multistamp(stamped_template_path, stamp_path)
-    rescue => e
-      Rails.logger.error 'Simple forms api - Failed to generate stamped file', message: e.message
-      raise
-    ensure
-      Common::FileHelpers.delete_file_if_exists(stamp_path) if defined?(stamp_path)
+    #     begin
+    #       # Generate the stamp PDF using Prawn with the provided configurations
+    #       Prawn::Document.generate(stamp_path, margin: [0, 0]) do |pdf|
+    #         page_configuration.each do |config|
+    #           case config[:type]
+    #           when :text
+    #             pdf.draw_text signature_text, at: config[:position], size: font_size
+    #           when :new_page
+    #             pdf.start_new_page
+    #           end
+    #         end
+    #       end
+
+    #       perform_multistamp(stamped_template_path, stamp_path)
+    #     rescue => e
+    #       if e.message.include?('Error: Failed to open stamp PDF file') && attempt < 1
+    #         attempt += 1
+    #         retry
+    #       else
+    #         Rails.logger.error 'Simple forms api - Failed to generate stamped file', message: e.message
+    #         raise
+    #       end
+    #     ensure
+    #       Common::FileHelpers.delete_file_if_exists(stamp_path)
+    #     end
+    #   else
+    #     stamp_path = Common::FileHelpers.random_file_path
+    #     Prawn::Document.generate(stamp_path, margin: [0, 0]) do |pdf|
+    #       page_configuration.each do |config|
+    #         case config[:type]
+    #         when :text
+    #           pdf.draw_text signature_text, at: config[:position], size: font_size
+    #         when :new_page
+    #           pdf.start_new_page
+    #         end
+    #       end
+    #     end
+
+    #     perform_multistamp(stamped_template_path, stamp_path)
+    #   rescue => e
+    #     Rails.logger.error 'Simple forms api - Failed to generate stamped file', message: e.message
+    #     raise
+    #   ensure
+    #     Common::FileHelpers.delete_file_if_exists(stamp_path) if defined?(stamp_path)
+    #   end
+    # end
+
+    def self.multistamp(stamped_template_path, signature_text, page_configuration, font_size = 16)
+      if Flipper.enabled?(:champva_multiple_stamp_retry, @user)
+            max_attempts = 3
+            attempt = 0
+
+            begin
+              stamp_path = Common::FileHelpers.random_file_path
+      
+              Prawn::Document.generate(stamp_path, margin: [0, 0]) do |pdf|
+                page_configuration.each do |config|
+                  case config[:type]
+                  when :text
+                    pdf.draw_text signature_text, at: config[:position], size: font_size
+                  when :new_page
+                    pdf.start_new_page
+                  end
+                end
+              end
+      
+              perform_multistamp(stamped_template_path, stamp_path)
+            rescue IOError, Prawn::Errors::PrawnError => e
+              if e.message.include?('Error: Failed to open stamp PDF file') && attempt < max_attempts
+                attempt += 1
+                sleep 1 # Add a delay before retrying
+                retry
+              else
+                Rails.logger.error 'Simple forms api - Failed to generate stamped file', message: e.message
+                raise # Re-raise the exception after logging
+              end
+            ensure
+              Common::FileHelpers.delete_file_if_exists(stamp_path)
+            end
+      
+      else
+      
+            stamp_path = Common::FileHelpers.random_file_path
+            Prawn::Document.generate(stamp_path, margin: [0, 0]) do |pdf|
+              page_configuration.each do |config|
+                case config[:type]
+                when :text
+                  pdf.draw_text signature_text, at: config[:position], size: font_size
+                when :new_page
+                  pdf.start_new_page
+                end
+              end
+            end
+            perform_multistamp(stamped_template_path, stamp_path)
+          rescue => e
+            Rails.logger.error 'Simple forms api - Failed to generate stamped file', message: e.message
+            raise
+          ensure
+            Common::FileHelpers.delete_file_if_exists(stamp_path) if defined?(stamp_path)
+      end
     end
+      
 
     def self.stamp(desired_stamp, stamped_template_path, append_to_stamp: false, text_only: true)
       current_file_path = stamped_template_path
