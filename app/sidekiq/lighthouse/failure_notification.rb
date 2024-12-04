3dcd4c4f51b790ac48ea8b404a5a62d9239a96bb
Samuel Cormier <sam.cormier@coforma.io>
2024-12-04 14:03:20 -0800

    fixed lighthouse notify_client method to not be a class method, and added sentry logging

--- app/sidekiq/lighthouse/failure_notification.rb
+++ app/sidekiq/lighthouse/failure_notification.rb
@@ -17,7 +17,7 @@ class Lighthouse::FailureNotification
     ::Rails.logger.info('Lighthouse::FailureNotification email could not be sent')
   end
 
-  def self.notify_client
+  def notify_client
     VaNotify::Service.new(NOTIFY_SETTINGS.api_key)
   end
 
@@ -32,5 +32,6 @@ class Lighthouse::FailureNotification
   rescue => e
     ::Rails.logger.error('Lighthouse::FailureNotification email error',
                          { message: e.message })
+    log_exception_to_sentry(e)
   end
 end
