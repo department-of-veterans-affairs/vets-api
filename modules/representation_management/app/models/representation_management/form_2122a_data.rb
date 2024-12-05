ce6ba4f90aababcbc2b629bd4f1ffe5b79bc110b
Josh Fike <josh.fike@oddball.io>
2024-12-05 12:48:11 -0600

    Start refactoring representative truncation

--- modules/representation_management/app/models/representation_management/form_2122a_data.rb
+++ modules/representation_management/app/models/representation_management/form_2122a_data.rb
@@ -5,6 +5,16 @@ module RepresentationManagement
     REPRESENTATIVE_TYPES = %w[ATTORNEY AGENT INDIVIDUAL VSO_REPRESENTATIVE].freeze
     VETERAN_SERVICE_BRANCHES = %w[ARMY NAVY AIR_FORCE MARINE_CORPS COAST_GUARD SPACE_FORCE NOAA
                                   USPHS].freeze
+    TRUNCATION_LIMITS = {
+      first_name: 12,
+      last_name: 18,
+      address_line1: 30,
+      address_line2: 5,
+      city: 18,
+      state_code: 2,
+      zip_code: 5,
+      email: 78
+    }.freeze
 
     consent_attrs = %i[
       consent_inside_access
@@ -30,6 +40,18 @@ module RepresentationManagement
 
     def representative_last_name_truncated
       representative.last_name[0..17]
+    end
+
+    def representative_address_line1_truncated
+      representative.address_line1[0..29]
+    end
+
+    def representative_address_line2_truncated
+      representative.address_line2[0..4]
+    end
+
+    def representative_field_truncated(field)
+      representative.public_send(field)[0..TRUNCATION_LIMITS[field] - 1]
     end
   end
 end
--- modules/representation_management/spec/models/representation_management/form_2122a_data_spec.rb
+++ modules/representation_management/spec/models/representation_management/form_2122a_data_spec.rb
@@ -12,35 +12,76 @@ RSpec.describe RepresentationManagement::Form2122aData, type: :model do
     }
   end
 
-  describe 'truncate methods' do
+  describe 'representative_field_truncated' do
     subject { described_class.new }
 
-    describe '#representative_first_name_truncated' do
-      it "returns the first 12 characters of the representative's first name" do
-        representative = create(:accredited_individual, first_name: 'A' * 13)
-        subject.representative_id = representative.id
-        expect(subject.representative_first_name_truncated).to eq('A' * 12)
-      end
+    it 'truncates characters beyond the specificied number' do
+      representative = create(:accredited_individual, first_name: 'A' * 13)
+      subject.representative_id = representative.id
+      expect(subject.representative_field_truncated(:first_name)).to eq('A' * 12)
+    end
 
-      it 'returns the full first name if it is 12 characters or less' do
-        representative = create(:accredited_individual, first_name: 'A' * 5)
-        subject.representative_id = representative.id
-        expect(subject.representative_first_name_truncated).to eq('A' * 5)
+    it 'returns the full string if it is below the truncation limit' do
+      representative = create(:accredited_individual, first_name: 'A' * 5)
+      subject.representative_id = representative.id
+      expect(subject.representative_field_truncated(:first_name)).to eq('A' * 5)
+    end
+
+    it 'returns the full string if it is equal to the truncation limit' do
+      representative = create(:accredited_individual, first_name: 'A' * 12)
+      subject.representative_id = representative.id
+      expect(subject.representative_field_truncated(:first_name)).to eq('A' * 12)
+    end
+
+    it 'works for every value in TRUNCATION_LIMITS' do
+      representative = create(:accredited_individual)
+      subject.representative_id = representative.id
+      described_class::TRUNCATION_LIMITS.each do |field, limit|
+        subject.representative.send("#{field}=", 'A' * (limit + 1))
+        expect(subject.representative_field_truncated(field)).to eq('A' * limit)
       end
     end
 
-    describe '#representative_last_name_truncated' do
-      it "returns the first 18 characters of the representative's last name" do
-        representative = create(:accredited_individual, last_name: 'A' * 19)
-        subject.representative_id = representative.id
-        expect(subject.representative_last_name_truncated).to eq('A' * 18)
-      end
+    # describe '#representative_last_name_truncated' do
+    #   it "returns the first 18 characters of the representative's last name" do
+    #     representative = create(:accredited_individual, last_name: 'A' * 19)
+    #     subject.representative_id = representative.id
+    #     expect(subject.representative_last_name_truncated).to eq('A' * 18)
+    #   end
 
-      it 'returns the full last name if it is 17 characters or less' do
-        representative = create(:accredited_individual, last_name: 'A' * 5)
-        subject.representative_id = representative.id
-        expect(subject.representative_last_name_truncated).to eq('A' * 5)
-      end
-    end
+    #   it 'returns the full last name if it is 17 characters or less' do
+    #     representative = create(:accredited_individual, last_name: 'A' * 5)
+    #     subject.representative_id = representative.id
+    #     expect(subject.representative_last_name_truncated).to eq('A' * 5)
+    #   end
+    # end
+
+    # describe '#representative_address_line1_truncated' do
+    #   it "returns the first 30 characters of the representative's address line 1" do
+    #     representative = create(:accredited_individual, address_line1: 'A' * 31)
+    #     subject.representative_id = representative.id
+    #     expect(subject.representative_address_line1_truncated).to eq('A' * 30)
+    #   end
+
+    #   it 'returns the full address line 1 if it is 30 characters or less' do
+    #     representative = create(:accredited_individual, address_line1: 'A' * 5)
+    #     subject.representative_id = representative.id
+    #     expect(subject.representative_address_line1_truncated).to eq('A' * 5)
+    #   end
+    # end
+
+    # describe '#representative_address_line2_truncated' do
+    #   it "returns the first 5 characters of the representative's address line 2" do
+    #     representative = create(:accredited_individual, address_line2: 'A' * 6)
+    #     subject.representative_id = representative.id
+    #     expect(subject.representative_address_line2_truncated).to eq('A' * 5)
+    #   end
+
+    #   it 'returns the full address line 2 if it is 5 characters or less' do
+    #     representative = create(:accredited_individual, address_line2: 'A' * 3)
+    #     subject.representative_id = representative.id
+    #     expect(subject.representative_address_line2_truncated).to eq('A' * 3)
+    #   end
+    # end
   end
 end
