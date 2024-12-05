d93b1ec73f39b0bb2f6d906d05af38abe0e492d1
Alex Chan <172081065+alexchan-va@users.noreply.github.com>
2024-12-05 11:36:54 -0500

    Add 10282 form to save to db

--- app/models/saved_claim/education_benefits/va_10282.rb
+++ app/models/saved_claim/education_benefits/va_10282.rb
@@ -3,17 +3,11 @@
 class SavedClaim::EducationBenefits::VA10282 < SavedClaim::EducationBenefits
   add_form_and_validation('22-10282')
 
-  BENEFIT_TITLE_FOR_10282 = {
-
-}.freeze
-
   def after_submit(user)
-
   end
 
   private
 
   def send_confirmation_email(parsed_form_data, email)
-    
   end
 end
