744235bbb3d37e755f41525820108459e262a9e5
Don Shin <cloudmagic80@gmail.com>
2024-12-05 07:51:55 -0600

    remove comments

--- modules/ivc_champva/spec/requests/ivc_champva/v1/forms/uploads_spec.rb
+++ modules/ivc_champva/spec/requests/ivc_champva/v1/forms/uploads_spec.rb
@@ -307,14 +307,12 @@ RSpec.describe 'IvcChampva::V1::Forms::Uploads', type: :request do
 
         context 'when file uploads fail with "unable to find file" error' do
           before do
-            # Simulate that handle_uploads raises an exception
             allow(file_uploader).to receive(:handle_uploads).and_raise(StandardError.new('Unable to find file'))
           end
 
           it 'returns error statuses and error message' do
             statuses, error_message = controller.send(:handle_file_uploads, form_id, parsed_form_data)
 
-            # Simplify the test by removing the assertion on retries
             expect(statuses).to eq([])
             expect(error_message).to eq('Error handling file uploads')
           end
