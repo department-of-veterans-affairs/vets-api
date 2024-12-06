90bdaf1d871d50f56d38d53845e00c06f1a8dc75
Ian Perera <iputreetek@gmail.com>
2024-12-06 14:47:24 -0500

    #1429: update schema with encrypted fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_222335) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_190908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
@@ -411,18 +411,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_222335) do
     t.string "cid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "claims_api_power_of_attorney_requests", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "proc_id"
-    t.string "veteran_icn"
-    t.string "claimant_icn"
-    t.string "poa_code"
-    t.jsonb "metadata", default: {}
-    t.uuid "power_of_attorney_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["power_of_attorney_id"], name: "idx_on_power_of_attorney_id_9fc9134311"
   end
 
   create_table "claims_api_power_of_attorneys", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -1384,6 +1372,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_222335) do
     t.uuid "notification_id", null: false
     t.text "reference"
     t.text "to"
+    t.text "to_ciphertext"
+    t.text "encrypted_kms_key"
     t.text "status"
     t.datetime "completed_at"
     t.datetime "sent_at"
