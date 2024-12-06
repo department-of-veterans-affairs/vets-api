169c4b585117bd533d09fecf1d0edc76481073e3
agravell047 <anthony.gravell@bylight.com>
2024-12-06 15:03:59 -0500

    removes ndc from url and uses rx ID instead

--- modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
@@ -6,7 +6,14 @@ module MyHealth
       def index
         if Flipper.enabled?(:mhv_medications_display_documentation_content, @current_user)
           begin
-            documentation = client.get_rx_documentation(params[:ndc])
+            id = params[:id]
+            rx = client.get_rx_details(id)
+            cmop_ndc_number = if rx[:rx_rf_records]&.[](0)&.[](1)&.[](0)&.key?(:cmop_ndc_number)
+                                rx[:rx_rf_records][0][1][0][:cmop_ndc_number]
+                              elsif rx[:cmop_ndc_number].present?
+                                rx[:cmop_ndc_number]
+                              end
+            documentation = client.get_rx_documentation(cmop_ndc_number)
             prescription_documentation = PrescriptionDocumentation.new({ html: documentation[:data] })
             render json: PrescriptionDocumentationSerializer.new(prescription_documentation)
           rescue => e
