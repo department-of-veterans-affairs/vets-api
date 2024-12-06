fa7548cb68ba599197af0f24ecd8e5a3bb153883
RachalCassity <rachal.cassity@oddball.io>
2024-12-06 17:38:16 -0600

    removed ssl false

--- lib/va_profile/configuration.rb
+++ lib/va_profile/configuration.rb
@@ -12,8 +12,7 @@ module VAProfile
     end
 
     def connection
-      @conn ||= Faraday.new(base_path, headers: base_request_headers, request: request_options,
-                                       ssl: { verify: false }) do |faraday|
+      @conn ||= Faraday.new(base_path, headers: base_request_headers, request: request_options) do |faraday|
         faraday.use      :breakers
         faraday.use      Faraday::Response::RaiseError
 
