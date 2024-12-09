e5af7b876ba50a2dc83a96f52f698a0dc1435536
Alex Chan <172081065+alexchan-va@users.noreply.github.com>
2024-12-09 10:53:59 -0500

    Remove empty methods for linter

--- app/models/saved_claim/education_benefits/va_10282.rb
+++ app/models/saved_claim/education_benefits/va_10282.rb
@@ -2,12 +2,4 @@
 
 class SavedClaim::EducationBenefits::VA10282 < SavedClaim::EducationBenefits
   add_form_and_validation('22-10282')
-
-  def after_submit(user)
-  end
-
-  private
-
-  def send_confirmation_email(parsed_form_data, email)
-  end
 end
