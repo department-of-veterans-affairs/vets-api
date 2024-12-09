66b00de2d4f1ba756c36ab44b823cf69eebddffc
rockwellwindsor-va <rockwell.rice@oddball.io>
2024-12-09 16:10:53 -0600

    Updates for new information about constentLimits

--- modules/claims_api/app/sidekiq/claims_api/service_base.rb
+++ modules/claims_api/app/sidekiq/claims_api/service_base.rb
@@ -119,7 +119,7 @@ module ClaimsApi
     end
 
     def enable_vbms_access?(poa_form:)
-      poa_form.form_data['recordConsent'].presence || false
+      poa_form.form_data['recordConsent'] && poa_form.form_data['consentLimits'].blank?
     end
 
     def set_vbms_error_message(poa, error)
