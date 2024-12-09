9addbc635a147c071e7c116b1c0b772fe24c2cda
Mike Moyer <87040148+mmoyer-va@users.noreply.github.com>
2024-12-09 18:32:26 -0500

    MHV-64151 Updated test

--- modules/my_health/spec/requests/my_health/v1/medical_records/imaging_spec.rb
+++ modules/my_health/spec/requests/my_health/v1/medical_records/imaging_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'MyHealth::V1::MedicalRecords::ImagingController', type: :request
         user_id: 11_375_034,
         patient_id: '11382904',
         expires_at: 1.hour.from_now,
-        token: 'ENC(MA0ECJh1RjEgZFMhAgEQC4nF/gSOKGSZuYg8kVN8CmHNnCLychZ7Wo2jXGwPj39SQFG4wLsFYlZN)'
+        token: 'SESSION_TOKEN'
       }
     )
 
@@ -35,13 +35,12 @@ RSpec.describe 'MyHealth::V1::MedicalRecords::ImagingController', type: :request
   context 'Premium User' do
     let(:mhv_account_type) { 'Premium' }
 
-    it 'streams DICOM data without the Content-Length header' do
+    it 'streams DICOM data' do
       VCR.use_cassette('bb_client/get_dicom') do
         get "/my_health/v1/medical_records/imaging/#{study_id}/dicom"
       end
 
       expect(response).to be_successful
-      # expect(response.headers).not_to have_key('Content-Length')
       expect(response.headers['Content-Type']).to eq('application/zip')
     end
   end
