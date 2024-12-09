c5b4d0826b0814514020950dc44d3245c5ff224b
agravell047 <anthony.gravell@bylight.com>
2024-12-09 16:16:59 -0500

    reorganizes and improves logic

--- modules/my_health/app/helpers/my_health/rx_grouping_helper.rb
+++ modules/my_health/app/helpers/my_health/rx_grouping_helper.rb
@@ -3,52 +3,76 @@
 module MyHealth
   module RxGroupingHelper
     def group_prescriptions(prescriptions)
+      prescriptions ||= []
       grouped_prescriptions = []
 
-      while prescriptions.any?
-        prescription = prescriptions.shift
-        base_prescription_number = prescription.prescription_number.sub(/[A-Z]$/, '')
+      prescriptions.sort_by!(&:prescription_number)
 
-        related_prescriptions = prescriptions.select do |p|
-          p.prescription_number.sub(/[A-Z]$/,
-                                    '') == base_prescription_number && p.station_number == prescription.station_number
-        end
-        related_prescriptions += [prescription]
+      while prescriptions.any?
+        prescription = prescriptions[0]
+
+        related_prescriptions = select_related_rxs(prescriptions, prescription)
+
         if related_prescriptions.length <= 1
-          puts "Added solo prescription: #{prescription.prescription_number}"
-          grouped_prescriptions << prescription
+          add_solo_med_and_delete(grouped_prescriptions, prescriptions, prescription)
           next
         end
 
-        # puts "Related prescriptions: #{related_prescriptions.map { |rx| rx.prescription_number }.join(', ')}"
-        # puts "Original prescription: #{prescription.prescription_number}"
         base_prescription, related_prescriptions = find_base_prescription(related_prescriptions, prescription)
-
+        related_prescriptions = sort_related_prescriptions(related_prescriptions)
         base_prescription.grouped_medications ||= []
-        related_prescriptions.each do |renewal|
-          base_prescription.grouped_medications << renewal
-          prescriptions.delete(renewal)
-          # puts "Grouped Renewal: #{renewal.prescription_number} under #{base_prescription.prescription_number}"
-        end
+        add_group_meds_and_delete(related_prescriptions, base_prescription, prescriptions)
+
         grouped_prescriptions << base_prescription
-        # puts "Added #{base_prescription.prescription_number} to grouped_prescriptions"
-        # puts '_______________________________________________________________________'
+        prescriptions.delete(base_prescription)
       end
+
       grouped_prescriptions
     end
 
     private
+
+    def add_solo_med_and_delete(grouped_prescriptions, prescriptions, prescription)
+      grouped_prescriptions << prescription
+      prescriptions.delete(prescription)
+    end
+
+    def add_group_meds_and_delete(related_prescriptions, base_prescription, prescriptions)
+      related_prescriptions.each do |renewal|
+        base_prescription.grouped_medications << renewal
+        prescriptions.delete(renewal)
+      end
+    end
+
+    def select_related_rxs(prescriptions, prescription)
+      prescriptions.select do |p|
+        base_prescription = prescription.prescription_number.sub(/[A-Z]$/, '')
+        current_prescription_number = p.prescription_number.sub(/[A-Z]$/, '')
+        current_prescription_number == base_prescription && p.station_number == prescription.station_number
+      end
+    end
 
     def find_base_prescription(related_prescriptions, current_prescription)
       all_prescriptions = [current_prescription] + related_prescriptions
       highest_prescription = all_prescriptions.max_by { |p| [p.prescription_number, p.ordered_date || Time.at(0)] }
 
       related_prescriptions.delete(highest_prescription)
-
-      # puts "Found base prescription: #{highest_prescription.prescription_number}"
-      # puts "Remaining related prescriptions: #{related_prescriptions.map { |rx| rx.prescription_number }.join(', ')}"
-
       [highest_prescription, related_prescriptions]
+    end
+
+    def sort_related_prescriptions(related_prescriptions)
+      related_prescriptions.sort do |rx1, rx2|
+        suffix1 = rx1.prescription_number[/[A-Z]+$/] || ''
+        suffix2 = rx2.prescription_number[/[A-Z]+$/] || ''
+
+        if suffix1 == suffix2
+          base_number1 = rx1.prescription_number.sub(/[A-Z]+$/, '').to_i
+          base_number2 = rx2.prescription_number.sub(/[A-Z]+$/, '').to_i
+          base_number1 <=> base_number2
+        else
+          suffix2 <=> suffix1
+        end
+      end
     end
 
     module_function :group_prescriptions
