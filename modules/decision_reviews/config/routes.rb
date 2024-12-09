5ada27c9cfce4c467f2680cd38abbba69ea62cc2
Molly Trombley-McCann <molly.trombley-mccann@coforma.io>
2024-12-09 10:23:41 -0800

    Duplicate main form endpoints - also contestable issues endpoints -v1 and v2

--- modules/decision_reviews/config/routes.rb
+++ modules/decision_reviews/config/routes.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
 DecisionReviews::Engine.routes.draw do
+  namespace :v1, defaults: { format: 'json' } do
+    namespace :higher_level_reviews do
+      get 'contestable_issues(/:benefit_type)', to: 'contestable_issues#index'
+    end
+    resources :higher_level_reviews, only: %i[create show]
+
+    namespace :notice_of_disagreements do
+      get 'contestable_issues', to: 'contestable_issues#index'
+    end
+    resources :notice_of_disagreements, only: %i[create show]
+
+    namespace :supplemental_claims do
+      get 'contestable_issues(/:benefit_type)', to: 'contestable_issues#index'
+    end
+    resources :supplemental_claims, only: %i[create show]
+  end
+
+  namespace :v2, defaults: { format: 'json' } do
+    resources :higher_level_reviews, only: %i[create show]
+  end
 end
