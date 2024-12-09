f57ce557ab41dbe219b0061d22efd97b7e0f0225
Molly Trombley-McCann <molly.trombley-mccann@coforma.io>
2024-12-09 10:23:41 -0800

    WIP duplicate higher_level_reviews routes and controllers

--- modules/decision_reviews/config/routes.rb
+++ modules/decision_reviews/config/routes.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 DecisionReviews::Engine.routes.draw do
+  namespace :v1, defaults: { format: 'json' } do
+    namespace :higher_level_reviews do
+      get 'contestable_issues(/:benefit_type)', to: 'contestable_issues#index'
+    end
+    resources :higher_level_reviews, only: %i[create show]
+  end
 end
