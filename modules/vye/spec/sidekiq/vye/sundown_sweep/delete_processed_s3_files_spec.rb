bacfc4e7a4522737f60541c315527e3854ab88c6
Alex Chan <172081065+alexchan-va@users.noreply.github.com>
2024-12-10 11:38:23 -0500

    Add holidays logic testing to rspec

--- modules/vye/spec/sidekiq/vye/sundown_sweep/delete_processed_s3_files_spec.rb
+++ modules/vye/spec/sidekiq/vye/sundown_sweep/delete_processed_s3_files_spec.rb
@@ -1,20 +1,45 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require Vye::Engine.root / 'spec/rails_helper'
+require 'timecop'
 
 describe Vye::SundownSweep::DeleteProcessedS3Files, type: :worker do
   before do
     Sidekiq::Job.clear_all
   end
 
-  it 'checks the existence of described_class' do
-    expect(Vye::CloudTransfer).to receive(:remove_aws_files_from_s3_buckets)
+  context 'when it is not a holiday' do
+    before do
+      Timecop.freeze(Time.zone.local(2024, 7, 2)) # Regular work day
+    end
 
-    expect do
-      described_class.perform_async
-    end.to change { Sidekiq::Worker.jobs.size }.by(1)
+    after do
+      Timecop.return
+    end
 
-    described_class.drain
+    it 'checks the existence of described_class' do
+      expect(Vye::CloudTransfer).to receive(:remove_aws_files_from_s3_buckets)
+
+      expect do
+        described_class.perform_async
+      end.to change { Sidekiq::Worker.jobs.size }.by(1)
+
+      described_class.drain
+    end
+  end
+
+  context 'when it is a holiday' do
+    before do
+      Timecop.freeze(Time.zone.local(2024, 7, 4)) # Independence Day
+    end
+
+    after do
+      Timecop.return
+    end
+
+    it 'does not process S3 files' do
+      expect(Vye::CloudTransfer).not_to receive(:remove_aws_files_from_s3_buckets)
+      described_class.new.perform
+    end
   end
 end
