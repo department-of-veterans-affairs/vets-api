3959ce7e14208bd2965f0032f570e63fe6256b41
Don Shin <cloudmagic80@gmail.com>
2024-12-12 10:17:02 -0600

    remove unit test

--- modules/ivc_champva/spec/requests/ivc_champva/v1/forms/uploads_spec.rb
+++ modules/ivc_champva/spec/requests/ivc_champva/v1/forms/uploads_spec.rb
@@ -305,28 +305,6 @@ RSpec.describe 'IvcChampva::V1::Forms::Uploads', type: :request do
           end
         end
 
-        context 'when file uploads fail with "unable to find file" error' do
-          before do
-            allow(Flipper).to receive(:enabled?).with(:champva_multiple_stamp_retry, @current_user).and_return(true)
-            allow(file_uploader).to receive(:handle_uploads).and_raise(StandardError.new('Unable to find file'))
-          end
-
-          it 'returns error statuses and error message' do
-            statuses = nil
-            error_message = nil
-
-            begin
-              statuses, error_message = controller.send(:handle_file_uploads, form_id, parsed_form_data)
-            rescue => e
-              puts "Exception raised: #{e.class} - #{e.message}"
-              expect(e).to be_nil # This will fail if an exception is raised
-            end
-
-            expect(statuses).to eq([])
-            expect(error_message).to eq('Error handling file uploads')
-          end
-        end
-
         context 'when file uploads fail with other errors retry once' do
           subject(:result) { controller.send(:handle_file_uploads, form_id, parsed_form_data) }
 
