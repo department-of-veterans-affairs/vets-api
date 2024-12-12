3977eb71e39cc97716253c6972ac578617060c69
Josh Fike <josh.fike@oddball.io>
2024-12-12 11:45:10 -0600

    [WIP] Compare individual pages

--- modules/representation_management/spec/support/pdf_matcher.rb
+++ modules/representation_management/spec/support/pdf_matcher.rb
@@ -12,8 +12,19 @@ RSpec::Matchers.define :match_pdf_content_of do |expected|
     expected_pages = expected_reader.pages.size
     expected_text = expected_reader.pages.map(&:text)
 
+    actual_hexapdf = HexaPDF::Document.open(actual)
+    expected_hexapdf = HexaPDF::Document.open(expected)
+    return false unless actual_hexapdf.pages.count == expected_hexapdf.pages.count
+
+    actual_hexapdf.pages.count.times do |i|
+      text_a = actual_hexapdf.pages[i].contents
+      text_b = expected_hexapdf.pages[i].contents
+      return false unless text_a == text_b
+    end
+    true
+
     # Check both page count and text content in case there are extraneous blank pages
-    actual_pages == expected_pages && actual_text == expected_text
+    # actual_pages == expected_pages && actual_text == expected_text
   end
   failure_message do |actual|
     "expected that content of #{actual} matches content of #{expected}"
