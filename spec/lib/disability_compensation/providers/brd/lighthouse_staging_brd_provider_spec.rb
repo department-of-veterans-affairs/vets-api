76e92d72b7c8a82798f19b542abb5a9720535c5d
Tommasina Miller <144388524+tommasina-va@users.noreply.github.com>
2024-12-12 15:32:46 -0600

    WIP test

---
+++ spec/lib/disability_compensation/providers/brd/lighthouse_staging_brd_provider_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'disability_compensation/factories/api_provider_factory'
+require 'disability_compensation/providers/brd/lighthouse_staging_brd_provider'
+require 'support/disability_compensation_form/shared_examples/brd_provider'
+require 'lighthouse/service_exception'
+
+RSpec.describe LighthouseStagingBRDProvider do
+  let(:current_user) { build(:user, :loa3) }
+
+  before do
+    @provider = LighthouseStagingBRDProvider.new(current_user)
+    Flipper.enable(ApiProviderFactory::FEATURE_TOGGLE_BRD)
+    Flipper.enable(:disability_compensation_staging_lighthouse_brd)
+  end
+
+  after do
+    Flipper.disable(ApiProviderFactory::FEATURE_TOGGLE_BRD)
+    Flipper.disable(:disability_compensation_staging_lighthouse_brd)
+  end
+
+  it_behaves_like 'brd provider'
+
+  it 'retrieves separation locations from the Lighthouse API' do
+    VCR.use_cassette('brd/separation_locations') do
+      response = @provider.get_separation_locations
+      expect(response.separation_locations.length).to eq(324)
+    end
+  end
+end
