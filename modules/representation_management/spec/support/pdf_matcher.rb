9c2cb16a60705002ed7ba558ab7d5bb354a8a95a
Josh Fike <josh.fike@oddball.io>
2024-12-12 14:45:47 -0600

    Restore original pdf matcher functionality

--- modules/representation_management/spec/support/pdf_matcher.rb
+++ modules/representation_management/spec/support/pdf_matcher.rb
@@ -12,19 +12,8 @@ RSpec::Matchers.define :match_pdf_content_of do |expected|
     expected_pages = expected_reader.pages.size
     expected_text = expected_reader.pages.map(&:text)
 
-    actual_hexapdf = HexaPDF::Document.open(actual)
-    expected_hexapdf = HexaPDF::Document.open(expected)
-    return false unless actual_hexapdf.pages.count == expected_hexapdf.pages.count
-
-    actual_hexapdf.pages.count.times do |i|
-      hash_a = actual_hexapdf.pages[i].contents.hash
-      hash_b = expected_hexapdf.pages[i].contents.hash
-      return false unless hash_a == hash_b
-    end
-    true
-
     # Check both page count and text content in case there are extraneous blank pages
-    # actual_pages == expected_pages && actual_text == expected_text
+    actual_pages == expected_pages && actual_text == expected_text
   end
   failure_message do |actual|
     "expected that content of #{actual} matches content of #{expected}"
