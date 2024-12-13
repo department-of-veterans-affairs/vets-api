66a3e56ed5d819e8bbcff9528ab7ed03f45af5dd
Mike Moyer <87040148+mmoyer-va@users.noreply.github.com>
2024-12-13 15:30:47 -0500

    MHV-65160 Handle missing EDIPI

--- modules/my_health/app/controllers/my_health/v1/medical_records/military_service_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/medical_records/military_service_controller.rb
@@ -4,11 +4,22 @@ module MyHealth
   module V1
     module MedicalRecords
       class MilitaryServiceController < MrController
-        # Gets a user's military service record
+        class MissingEdipiError < StandardError; end
+
+        ##
+        # Get a user's military service record
+        #
         # @return [String] military service record in text format
+        #
         def index
+          if @current_user.nil? || @current_user.edipi.blank?
+            raise MissingEdipiError, 'No EDIPI found for the current user'
+          end
+
           resource = phrmgr_client.get_military_service(@current_user.edipi)
           render json: resource.to_json
+        rescue MissingEdipiError => e
+          render json: { error: e }, status: :bad_request
         end
       end
     end
