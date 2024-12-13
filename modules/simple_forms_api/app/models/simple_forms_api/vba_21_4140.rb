149b6547a3d1b410ec3b2368c60c75a3a80dcdb2
Derek Houck <12766168+derekhouck@users.noreply.github.com>
2024-12-13 14:25:28 -0600

    Add fixture

--- modules/simple_forms_api/app/models/simple_forms_api/vba_21_4140.rb
+++ modules/simple_forms_api/app/models/simple_forms_api/vba_21_4140.rb
@@ -2,16 +2,24 @@
 
 module SimpleFormsApi
   class VBA214140 < BaseForm
+    def desired_stamps
+      []
+    end
+
     def metadata
       {
-        'veteranFirstName' => @data.dig('veteran', 'full_name', 'first'),
-        'veteranLastName' => @data.dig('veteran', 'full_name', 'last'),
-        'fileNumber' => @data.dig('veteran', 'va_file_number').presence || @data.dig('veteran', 'ssn'),
-        'zipCode' => @data.dig('veteran', 'address', 'postal_code'),
+        'veteranFirstName' => @data.dig('full_name', 'first'),
+        'veteranLastName' => @data.dig('full_name', 'last'),
+        'fileNumber' => @data['va_file_number'].presence || @data['ssn'],
+        'zipCode' => @data.dig('address', 'postal_code'),
         'source' => 'VA Platform Digital Forms',
         'docType' => @data['form_number'],
         'businessLine' => 'CMP'
       }
+    end
+
+    def submission_date_stamps(_timestamp)
+      []
     end
   end
 end
