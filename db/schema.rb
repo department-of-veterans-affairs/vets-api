1eeccee1246f425c7b21d7354545711387a8ce69
Bryan Thompson <18094023+SnowboardTechie@users.noreply.github.com>
2024-12-13 09:32:13 -0800

    add path to banners, along with index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_09_231104) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_13_173113) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
@@ -310,7 +310,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_09_231104) do
     t.boolean "limit_subpage_inheritance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "path"
     t.index ["entity_id"], name: "index_banners_on_entity_id"
+    t.index ["path"], name: "index_banners_on_path"
   end
 
   create_table "base_facilities", id: false, force: :cascade do |t|
