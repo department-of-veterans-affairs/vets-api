61a238b1350f3e8de7664c3c2e2955b23802674e
Josh Fike <josh.fike@oddball.io>
2024-12-13 13:00:17 -0600

    All default fixtures regenerated

--- modules/representation_management/spec/support/pdf_matcher.rb
+++ modules/representation_management/spec/support/pdf_matcher.rb
@@ -12,31 +12,10 @@ RSpec::Matchers.define :match_pdf_content_of do |expected|
     expected_pages = expected_reader.pages.size
     expected_text = expected_reader.pages.map(&:text)
 
-    @diffs = []
-
-    actual_pages.times do |index|
-      p "Page #{index + 1}:"
-      if actual_text[index] != expected_text[index]
-        @diffs << Diff::LCS.diff(actual_text[index], expected_text[index])
-        p 'Start MISMATCH ' * 10
-        p "Actual: #{actual_text[index]}"
-        p "Expected: #{expected_text[index]}"
-        p 'End MISMATCH ' * 10
-      end
-    end
-
     # Check both page count and text content in case there are extraneous blank pages
     actual_pages == expected_pages && actual_text == expected_text
   end
   failure_message do |actual|
-    # "expected that content of #{actual} matches content of #{expected}"
-    message = "expected that content of #{actual} matches content of #{expected}\n"
-    @diffs.each_with_index do |diff, index|
-      message += "Differences on page #{index + 1}:\n"
-      diff.each do |change|
-        message += change.to_s + "\n"
-      end
-    end
-    message
+    "expected that content of #{actual} matches content of #{expected}"
   end
 end
