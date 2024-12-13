3df8d0535d4f85620c67c5f6b6c99b68148ec72e
Alex Chan <172081065+alexchan-va@users.noreply.github.com>
2024-12-13 15:17:52 -0500

    Add 10282 Form specs

---
+++ spec/factories/va10282.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :va10282, class: 'SavedClaim::EducationBenefits::VA10282', parent: :education_benefits do
+    form {
+      {
+        veteranFullName: {
+          first: 'a',
+          last: 'c'
+        },
+        veteranDesc: 'veteran',
+        contactInfo: {
+          mobilePhone: '1234567890',
+          email: 'a@c.com'
+        },
+        country: 'United States',
+        state: 'FL',
+        originRace: {
+          isBlackOrAfricanAmerican: true
+        },
+        gender: 'M',
+        highestLevelOfEducation: {
+          level: 'MD'
+        },
+        currentlyEmployed: true,
+        currentAnnualSalary: 'moreThanSeventyFive',
+        isWorkingInTechIndustry: true,
+        techIndustryFocusArea: 'CP'
+      }.to_json
+    }
+  end
+
+  factory :va10282_full_form, class: 'SavedClaim::EducationBenefits::VA10282', parent: :education_benefits do
+    form { File.read(Rails.root.join('spec', 'fixtures', 'education_benefits_claims', '10282', 'minimal.json')) }
+  end
+end
