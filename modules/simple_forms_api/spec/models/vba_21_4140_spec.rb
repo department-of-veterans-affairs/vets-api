20321a5348a9400b8f59615183845ac7572dac30
Derek Houck <12766168+derekhouck@users.noreply.github.com>
2024-12-13 14:39:18 -0600

    Fix spec

--- modules/simple_forms_api/spec/models/vba_21_4140_spec.rb
+++ modules/simple_forms_api/spec/models/vba_21_4140_spec.rb
@@ -5,16 +5,10 @@ require 'rails_helper'
 RSpec.describe SimpleFormsApi::VBA214140 do
   subject(:form) { described_class.new(data) }
 
-  let(:data) do
-    {
-      form_number: '21-4140',
-      veteran: {
-        address: { postal_code: 12_345 },
-        full_name: { first: 'John', last: 'Doe' },
-        va_file_number: 12_345
-      }
-    }
+  let(:fixture_path) do
+    Rails.root.join('modules', 'simple_forms_api', 'spec', 'fixtures', 'form_json', 'vba_21_4140.json')
   end
+  let(:data) { JSON.parse(fixture_path.read) }
 
   describe '#data' do
     subject { form.data }
@@ -28,11 +22,10 @@ RSpec.describe SimpleFormsApi::VBA214140 do
     it 'returns the proper hash' do
       expect(subject).to match(
         {
-          'veteranFirstName' => data.dig('veteran', 'full_name', 'first'),
-          'veteranLastName' => data.dig('veteran', 'full_name', 'last'),
-          'fileNumber' => data.dig('veteran',
-                                   'va_file_number'),
-          'zipCode' => data.dig('veteran', 'address', 'postal_code'),
+          'veteranFirstName' => data.dig('full_name', 'first'),
+          'veteranLastName' => data.dig('full_name', 'last'),
+          'fileNumber' => data['va_file_number'],
+          'zipCode' => data.dig('address', 'postal_code'),
           'source' => 'VA Platform Digital Forms',
           'docType' => data['form_number'],
           'businessLine' => 'CMP'
