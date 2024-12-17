460583c7fd39980677809556db2ec392ba051394
OJ Bucao <ojbucao@users.noreply.github.com>
2024-12-16 17:53:30 -0800

    Add serializer for PowerOfAttorneyRequest with controller and tests
    - Implement `PowerOfAttorneyRequestSerializer` to standardize JSON:API output
    - Add request specs for controller endpoints (`index` and `show`)
    - Add serializer specs to ensure proper formatting of resolution details
    - Normalize response keys for consistency in test expectations

--- modules/accredited_representative_portal/app/serializers/accredited_representative_portal/power_of_attorney_request_serializer.rb
+++ modules/accredited_representative_portal/app/serializers/accredited_representative_portal/power_of_attorney_request_serializer.rb
@@ -4,7 +4,11 @@ module AccreditedRepresentativePortal
   class PowerOfAttorneyRequestSerializer
     include JSONAPI::Serializer
 
-    attributes :id, :claimant_id, :created_at
+    attributes :id, :claimant_id
+
+    attribute :created_at do |object|
+      object.created_at.strftime('%Y-%m-%dT%H:%M:%S.%LZ')
+    end
 
     attribute :resolution do |object|
       next nil unless object.resolution
@@ -12,7 +16,7 @@ module AccreditedRepresentativePortal
       {
         id: object.resolution.id,
         type: object.resolution.resolving_type&.demodulize&.underscore,
-        created_at: object.resolution.created_at.iso8601,
+        created_at: object.resolution.created_at.strftime('%Y-%m-%dT%H:%M:%S.%LZ'),
         reason: object.resolution&.reason,
         creator_id: object.resolution.resolving.try(:creator_id)
       }.compact
