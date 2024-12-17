c9e86035a11f114e5a37fd5c53e71ef15f0f0ecd
RachalCassity <rachal.cassity@oddball.io>
2024-12-17 15:41:05 -0600

    check if icn is present

--- app/models/form_profile.rb
+++ app/models/form_profile.rb
@@ -306,7 +306,7 @@ class FormProfile
     return @vet360_contact_info if @vet360_contact_info_retrieved
 
     @vet360_contact_info_retrieved = true
-    if Flipper.enabled?(:remove_pciu, user)
+    if Flipper.enabled?(:remove_pciu, user) && user.icn.present?
       @vet360_contact_info = VAProfileRedis::V2::ContactInformation.for_user(user)
     elsif VAProfile::Configuration::SETTINGS.prefill && user.vet360_id.present?
       @vet360_contact_info = VAProfileRedis::ContactInformation.for_user(user)
