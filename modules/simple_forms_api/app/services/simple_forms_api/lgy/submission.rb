948bdbc4b35b81a80a116176bc9a9c9fa3b1325e
Jacob Penner <japenner@gmail.com>
2024-12-18 16:45:07 -0500

    pull out LGY specific submission logic from uploads controller

---
+++ modules/simple_forms_api/app/services/simple_forms_api/lgy/submission.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module SimpleFormsApi
+  module LGY
+    class Submission
+      LGY_API_FORMS = %w[26-4555].freeze
+
+      def initialize(current_user, params)
+        @current_user = current_user
+        @params = params
+      end
+
+      def submit
+        parsed_form_data = JSON.parse(params.to_json)
+        form = SimpleFormsApi::VBA264555.new(parsed_form_data)
+        lgy_response = LGY::Service.new.post_grant_application(payload: form.as_payload)
+        reference_number = lgy_response.body['reference_number']
+        status = lgy_response.body['status']
+        Rails.logger.info(
+          'Simple forms api - sent to lgy',
+          { form_number: params[:form_number], status:, reference_number: }
+        )
+        { json: { reference_number:, status: }, status: lgy_response.status }
+      end
+    end
+  end
+end
