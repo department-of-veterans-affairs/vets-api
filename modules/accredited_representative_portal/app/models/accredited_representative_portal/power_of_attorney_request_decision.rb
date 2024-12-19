365ef65564635474fdba7f9e3604ae37c762fec2
O. Bucao, Jr. <ojbucao@users.noreply.github.com>
2024-12-19 09:22:12 -0800

    Issue #97853 Add Power of Attorney Request Models, Factories, and Specs (#19919)
    * (feat) Add PowerOfAttorneyRequest model, factory, and specs
    
    * (feat) Add PowerOfAttorneyRequestDecision model, factory, and specs
    
    * (feat) Add PowerOfAttorneyRequestExpiration model, factory, and specs
    
    * (feat) Add PowerOfAttorneyRequestResolution model, factory, and specs
    
    * (feat) Add PowerOfAttorneyForm model, factory, and specs
    
    * (fix) Rename directory due to misspelling
    
    * (fix) Add table name prefix override and ActiveRecord dependency for POA requests
    
    - (feat) Override `table_name_prefix` to 'ar_' for models in the Accredited Representative Portal
    - (feat) Add `activerecord` as a development dependency to fix spec issues
    
    This ensures Power of Attorney request models in the engine use the correct table prefix
    and that all specs pass successfully.
    
    * (fix) Apply requested PR changes; all specs passing

---
+++ modules/accredited_representative_portal/app/models/accredited_representative_portal/power_of_attorney_request_decision.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module AccreditedRepresentativePortal
+  class PowerOfAttorneyRequestDecision < ApplicationRecord
+    include PowerOfAttorneyRequestResolution::Resolving
+
+    self.inheritance_column = nil
+
+    belongs_to :creator,
+               class_name: 'UserAccount'
+  end
+end
