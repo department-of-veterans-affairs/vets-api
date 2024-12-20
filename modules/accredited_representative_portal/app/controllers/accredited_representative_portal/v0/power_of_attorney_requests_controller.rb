e3032d5989949ccde35b8db198bb55382b43d5d8
OJ Bucao <ojbucao@users.noreply.github.com>
2024-12-20 13:54:02 -0800

    (fix) Refactor AR PowerOfAttorneyRequestController includes for improved query performance
    - Updated `includes` to reference `resolving` instead of just `resolution`
    - Added a limit of 100 records in the `index` action to optimize data retrieval

--- modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
+++ modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
@@ -4,12 +4,12 @@ module AccreditedRepresentativePortal
   module V0
     class PowerOfAttorneyRequestsController < ApplicationController
       def index
-        poa_requests = PowerOfAttorneyRequest.includes(:resolution)
+        poa_requests = PowerOfAttorneyRequest.includes(resolution: :resolving).limit(100)
         render json: PowerOfAttorneyRequestSerializer.new(poa_requests).serializable_hash, status: :ok
       end
 
       def show
-        poa_request = PowerOfAttorneyRequest.includes(:resolution).find(params[:id])
+        poa_request = PowerOfAttorneyRequest.includes(resolution: :resolving).find(params[:id])
         render json: PowerOfAttorneyRequestSerializer.new(poa_request).serializable_hash, status: :ok
       rescue ActiveRecord::RecordNotFound
         render json: { error: 'Record not found' }, status: :not_found
