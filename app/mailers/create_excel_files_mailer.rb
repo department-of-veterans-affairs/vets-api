0a5312701e7a45c85a91f889adf4070e02ee09e7
Alex Chan <172081065+alexchan-va@users.noreply.github.com>
2024-12-20 18:06:39 -0500

    Separate variables in mailer

--- app/mailers/create_excel_files_mailer.rb
+++ app/mailers/create_excel_files_mailer.rb
@@ -4,23 +4,18 @@ class CreateExcelFilesMailer < ApplicationMailer
   def build(filename)
     date = Time.zone.now.strftime('%m/%d/%Y')
     file_contents = File.read("tmp/#{filename}")
-
     headers['Content-Disposition'] = "attachment; filename=#{filename}"
 
-    if Settings.vsp_environment.eql?('production')
-      mail(
-        to: Settings.edu.production_excel_contents.emails,
-        subject: "22-10282 Form CSV file for #{date}",
-        content_type: 'text/csv',
-        body: file_contents
-      )
-    else
-      mail(
-        to: Settings.edu.staging_excel_contents.emails,
-        subject: "Staging 22-10282 Form CSV file for #{date}",
-        content_type: 'text/csv',
-        body: file_contents
-      )
-    end
+    # rubocop:disable Layout/LineLength
+    recipients = Settings.vsp_environment.eql?('production') ? Settings.edu.production_excel_contents.emails : Settings.edu.staging_excel_contents.emails
+    subject = Settings.vsp_environment.eql?('production') ? "22-10282 Form CSV file for #{date}" : "Staging CSV file for #{date}"
+    # rubocop:enable Layout/LineLength
+
+    mail(
+      to: recipients,
+      subject: subject,
+      content_type: 'text/csv',
+      body: file_contents
+    )
   end
 end
