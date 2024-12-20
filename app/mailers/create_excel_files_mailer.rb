cbe4418f0c462dbb920e760b495dd14f7635ae50
Alex Chan <172081065+alexchan-va@users.noreply.github.com>
2024-12-20 15:48:11 -0500

    Change conditional for mailer

--- app/mailers/create_excel_files_mailer.rb
+++ app/mailers/create_excel_files_mailer.rb
@@ -1,26 +1,18 @@
 # frozen_string_literal: true
-
 class CreateExcelFilesMailer < ApplicationMailer
   def build(filename)
     date = Time.zone.now.strftime('%m/%d/%Y')
     file_contents = File.read("tmp/#{filename}")
-
     headers['Content-Disposition'] = "attachment; filename=#{filename}"
 
-    if Settings.vsp_environment.eql?('production')
-      mail(
-        to: Settings.edu.production_excel_contents.emails,
-        subject: "Staging CSV file for #{date}",
-        content_type: 'text/csv',
-        body: file_contents
-      )
-    else
-      mail(
-        to: Settings.edu.staging_excel_contents.emails,
-        subject: "Staging CSV file for #{date}",
-        content_type: 'text/csv',
-        body: file_contents
-      )
-    end
+    recipients = Settings.vsp_environment.eql?('production') ? Settings.edu.production_excel_contents.emails : Settings.edu.staging_excel_contents.emails
+    subject = Settings.vsp_environment.eql?('production') ? "22-10282 Form CSV file for #{date}" : "Staging 22-10282 Form CSV file for #{date}"
+    
+    mail(
+      to: recipients,
+      subject: subject,
+      content_type: 'text/csv',
+      body: file_contents
+    )
   end
 end
