fef724d9921a496544baeb2be14e2c83595dae96
Patrick Brown <patrick.brown@oddball.io>
2024-12-23 18:58:40 -0500

    art/poa-requests/part-8/decisions-endpoint - added common verbiage

--- modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_request_decisions_requests_spec.rb
+++ modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_request_decisions_requests_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestDecisio
 
       expect(response).to have_http_status(:unprocessable_entity)
       response_body = JSON.parse(response.body)
-      expect(response_body['error']).to eq('Resolution already exists')
+      expect(response_body['error']).to eq('Decision already exists')
     end
   end
 
@@ -100,7 +100,7 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestDecisio
 
       expect(response).to have_http_status(:unprocessable_entity)
       response_body = JSON.parse(response.body)
-      expect(response_body['error']).to eq('Resolution already exists')
+      expect(response_body['error']).to eq('Decision already exists')
     end
   end
 end
