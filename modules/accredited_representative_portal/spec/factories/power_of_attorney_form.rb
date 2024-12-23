1ef45c0d6ee33c8dec17508bd1ae4e0c39df764d
Oren Mittman <nihil2501@gmail.com>
2024-12-23 02:54:06 -0500

    [ART] Serialize POA form data with POA request

--- modules/accredited_representative_portal/spec/factories/power_of_attorney_form.rb
+++ modules/accredited_representative_portal/spec/factories/power_of_attorney_form.rb
@@ -1,10 +1,60 @@
 # frozen_string_literal: true
 
+form_data = <<~JSON
+  {
+    "authorizations": {
+      "record_disclosure": true,
+      "record_disclosure_limitations": [],
+      "address_change": true
+    },
+    "dependent": {
+      "name": {
+        "first": "John",
+        "middle": "Middle",
+        "last": "Doe"
+      },
+      "address": {
+        "address_line1": "123 Main St",
+        "address_line2": "Apt 1",
+        "city": "Springfield",
+        "state_code": "IL",
+        "country": "US",
+        "zip_code": "62704",
+        "zip_code_suffix": "6789"
+      },
+      "date_of_birth": "1980-12-31",
+      "relationship": "Spouse",
+      "phone": "1234567890",
+      "email": "veteran@example.com"
+    },
+    "veteran": {
+      "name": {
+        "first": "John",
+        "middle": "Middle",
+        "last": "Doe"
+      },
+      "address": {
+        "address_line1": "123 Main St",
+        "address_line2": "Apt 1",
+        "city": "Springfield",
+        "state_code": "IL",
+        "country": "US",
+        "zip_code": "62704",
+        "zip_code_suffix": "6789"
+      },
+      "ssn": "123456789",
+      "va_file_number": "123456789",
+      "date_of_birth": "1980-12-31",
+      "service_number": "123456789",
+      "service_branch": "ARMY",
+      "phone": "1234567890",
+      "email": "veteran@example.com"
+    }
+  }
+JSON
+
 FactoryBot.define do
   factory :power_of_attorney_form, class: 'AccreditedRepresentativePortal::PowerOfAttorneyForm' do
-    data_ciphertext { 'Test encrypted data' }
-    city_bidx { Faker::Alphanumeric.alphanumeric(number: 44) }
-    state_bidx { Faker::Alphanumeric.alphanumeric(number: 44) }
-    zipcode_bidx { Faker::Alphanumeric.alphanumeric(number: 44) }
+    data { form_data }
   end
 end
