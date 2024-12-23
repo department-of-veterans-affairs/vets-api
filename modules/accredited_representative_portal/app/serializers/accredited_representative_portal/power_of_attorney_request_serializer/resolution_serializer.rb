bfe70f673fc5f9516f521bbb3efb28485b48a595
Oren Mittman <nihil2501@gmail.com>
2024-12-23 02:54:06 -0500

    [ART] Serialize POA form data with POA request

---
+++ modules/accredited_representative_portal/app/serializers/accredited_representative_portal/power_of_attorney_request_serializer/resolution_serializer.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module AccreditedRepresentativePortal
+  class PowerOfAttorneyRequestSerializer
+    class ResolutionSerializer < ApplicationSerializer
+      attributes :created_at
+    end
+  end
+end
