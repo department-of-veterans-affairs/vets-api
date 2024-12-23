780ab0c624647b12de362e059eaa72890e4cad8c
Patrick Brown <patrick.brown@oddball.io>
2024-12-23 10:28:34 -0500

    art/poa-requests/part-8/decisions-endpoint

--- modules/accredited_representative_portal/config/routes.rb
+++ modules/accredited_representative_portal/config/routes.rb
@@ -7,6 +7,8 @@ AccreditedRepresentativePortal::Engine.routes.draw do
     post 'form21a', to: 'form21a#submit'
 
     resources :in_progress_forms, only: %i[update show destroy]
-    resources :power_of_attorney_requests, only:  %i[index show]
+    resources :power_of_attorney_requests, only: [:index, :show] do
+      resource :decision, only: [:show, :create]
+    end
   end
 end
