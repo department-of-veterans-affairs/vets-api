d666f7bbbb6a7a8b2ea68ac17c31a16c1ebae255
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-12-24 12:42:36 -0500

    Form Upload flow, increase file size limit to 25MB

--- app/uploaders/form_upload/uploader.rb
+++ app/uploaders/form_upload/uploader.rb
@@ -14,7 +14,7 @@ class FormUpload::Uploader < VetsShrine
 
   Attacher.validate do
     validate_virus_free
-    validate_max_size 20.megabytes
+    validate_max_size 25.megabytes
     validate_min_size 1.kilobyte
     validate_mime_type_inclusion %w[image/jpg image/jpeg image/png application/pdf]
     validate_max_width 5000 if get.width
