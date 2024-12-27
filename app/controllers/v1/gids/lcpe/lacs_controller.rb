2f3d2ca37a6d8b749f4027835beae30e6a424221
Jeff Marks <jeff.t.marks@gmail.com>
2024-12-27 05:22:48 -0700

    Update controllers and routes

---
+++ app/controllers/v1/gids/lcpe/lacs_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module V1
+  module GIDS
+    module LCPE
+      class LacsController < GIDS::LCPEController
+        def index
+          render json: service.get_licences_and_certs_v1(scrubbed_params)
+        end
+
+        def show
+          render json: service.get_license_and_cert_details_v1(scrubbed_params)
+        end
+      end
+    end
+  end
+end
