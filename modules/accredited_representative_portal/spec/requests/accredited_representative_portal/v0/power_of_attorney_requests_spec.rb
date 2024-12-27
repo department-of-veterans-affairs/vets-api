7bc2346908f21ca0a28cd2bbc7628d879ba4f46e
OJ Bucao <9256675+ojbucao@users.noreply.github.com>
2024-12-27 09:35:02 -0800

    (fix) minor rubocop issue

--- modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_requests_spec.rb
+++ modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_requests_spec.rb
@@ -105,7 +105,6 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
 
   describe 'GET /accredited_representative_portal/v0/power_of_attorney_requests/:id' do
     it 'returns the details of a specific power of attorney request', skip: 'temp skip' do
-
       get("/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}")
 
       parsed_response = JSON.parse(response.body)
