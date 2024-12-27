fa2b982cfa5cbec9662d9ccf1df2382b42f6e073
Jeff Marks <jeff.t.marks@gmail.com>
2024-12-27 05:01:18 -0700

    Update lcpe client and config

---
+++ lib/gi/lcpe/client.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'gi/client'
+require_relative 'configuration'
+
+module GI
+  module LCPE
+    class Client < GI::Client
+      configuration GI::LCPE::Configuration
+
+      def get_licenses_and_certs_v1(params = {})
+        response = perform(:get, 'v1/lcpe/lacs', params)
+        gids_response(response)
+      end
+
+      def get_license_and_cert_details_v1(params = {})
+        enriched_id = params[:id]
+        response = perform(:get, "v1/lcpe/lacs/#{enriched_id}")
+      end
+
+      def get_exams_v1(params = {})
+        response = perform(:get, "v1/lcpe/exams")
+        gids_response(response)
+      end
+
+      def get_exam_details_v1(params = {})
+        enriched_id = params[:id]
+        response = perform(:get, "v1/lcpe/exams/#{enriched_id}", params.except(:id))
+        gids_response(response)
+      end
+    end
+  end
+end
---
+++ lib/gi/lcpe/configuration.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'gi/configuration'
+
+module GI
+  module LCPE
+    class Configuration < GI::Configuration; end
+  end
+end
