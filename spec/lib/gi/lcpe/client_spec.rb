a405f180bb3243384266ca85b1dbfb08f623733d
Jeff Marks <jeff.t.marks@gmail.com>
2024-12-28 05:55:30 -0700

    client spec

--- spec/lib/gi/lcpe/client_spec.rb
+++ spec/lib/gi/lcpe/client_spec.rb
@@ -4,55 +4,44 @@ require 'rails_helper'
 require 'gi/lcpe/client'
 require 'gi/gids_response'
 
-# TO-DO: Replace stubbed data with VCR cassettes after GIDS connection established
 describe GI::LCPE::Client do
-  let(:client) { GI::LCPE::Client.new }
-  let(:search_data) do
-    [
-      {
-        link: 'lcpe/lacs/1',
-        name: 'Certification Name',
-        type: 'certification'
-      }
-    ]
-  end
-  let(:lacs_data) do
-    {
-      desc: 'License Name',
-      type: 'license',
-      tests: [{ name: 'Test Name' }],
-      institution: 'Institution',
-      officials: [{ title: 'Certifying Official' }]
-    }
-  end
-  let(:exam_data) do
-    {
-      name: 'Exam Name',
-      tests: [{ description: 'Description' }],
-      institution: 'Institution'
-    }
+  let(:service) { GI::LCPE::Client.new }
+ 
+  describe '#get_licenses_and_certs_v1' do
+    it 'gets list of licenses and certifications' do
+      VCR.use_cassette('gi/lcpe/get_licenses_and_certs_v1') do
+        response = service.get_license_and_cert_details_v1({}).response
+        lacs = response.body[:lacs]
+        expect(lacs.class).to be(Array)
+        expect(lacs.first.keys).to match_array([:enriched_id, :name, :type])
+      end
+    end
   end
 
-  it 'gets a list of licenses, certifications, and prep courses' do
-    search_response = OpenStruct.new(body: { data: search_data })
-    allow(client).to receive(:get_licenses_and_certs_v1).with(type: 'all').and_return(search_response)
-    client_response = client.get_lce_search_results_v1(type: 'all').body
-    expect(client_response[:data]).to be_an(Array)
+  describe '#get_license_and_cert_details_v1' do
+    it 'gets details for license or certification' do
+      VCR.use_cassette('gi/lcpe/get_license_and_cert_details_v1') do
+        response = service.get_license_and_cert_details_v1({ id: '1@f9822' }).response
+        lac = response.body[:lac]
+        expect(lac.class).to be(Hash)
+        expect(lac.keys).to match_array([:enriched_id, :name, :type, :tests, :institution])
+        expect(lac[:tests].class).to be(Array)
+        expect(lac[:institution].class).to be(Hash)
+      end
+    end
   end
 
-  it 'gets license and certification details' do
-    details_response = OpenStruct.new(body: { data: exam_data })
-    allow(client).to receive(:get_license_and_cert_details_v1).with(id: '1').and_return(details_response)
-    client_response = client.get_license_and_cert_details_v1(id: '1').body
-    expect(client_response[:data]).to be_an(Hash)
-    expect(client_response[:data].keys).to contain_exactly(:name, :tests, :institution)
-  end
-
-  it 'gets exam details' do
-    details_response = OpenStruct.new(body: { data: exam_data })
-    allow(client).to receive(:get_exam_details_v1).with(id: '1').and_return(details_response)
-    client_response = client.get_exam_details_v1(id: '1').body
-    expect(client_response[:data]).to be_an(Hash)
-    expect(client_response[:data].keys).to contain_exactly(:name, :tests, :institution)
+  describe '#get_exams_v1' do
+    it 'gets exams with nested exam details' do
+      VCR.use_cassette('gi/lcpe/get_exams_v1') do
+        response = service.get_exams_v1({})
+        exams = response.body[:exams]
+        expect(exams.class).to be(Array)
+        expect(exams.first.class).to be(Hash)
+        expect(exams.first.keys).to match_array([:enriched_id, :name, :tests, :institution])
+        expect(exams.first[:tests].class).to be(Array)
+        expect(exams.first[:institution].class).to be(Hash)
+      end
+    end
   end
 end
