7fa28996f29aed719299f59302621e7a8b7fbb40
Jeff Marks <jeff.t.marks@gmail.com>
2024-12-29 15:11:42 -0700

    Fix client typo

--- lib/gi/lcpe/client.rb
+++ lib/gi/lcpe/client.rb
@@ -16,10 +16,11 @@ module GI
       def get_license_and_cert_details_v1(params = {})
         enriched_id = params[:id]
         response = perform(:get, "v1/lcpe/lacs/#{enriched_id}", params.except(:id))
+        gids_response(response)
       end
 
       def get_exams_v1(params = {})
-        response = perform(:get, "v1/lcpe/exams", params)
+        response = perform(:get, 'v1/lcpe/exams', params)
         gids_response(response)
       end
     end
