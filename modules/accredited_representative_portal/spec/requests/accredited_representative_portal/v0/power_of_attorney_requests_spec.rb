7e866dedab11c539c00ed3a969df9739c13c2b53
Oren Mittman <nihil2501@gmail.com>
2024-12-30 19:00:56 -0500

    [ART] lint fixes

--- modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_requests_spec.rb
+++ modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_requests_spec.rb
@@ -296,54 +296,54 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
           'claimant_id' => poa_request.claimant_id,
           'claimant_type' => 'dependent',
           'created_at' => time,
-          "power_of_attorney_form" => {
-            "authorizations" => {
-              "record_disclosure" => true,
-              "record_disclosure_limitations" => [],
-              "address_change" => true
+          'power_of_attorney_form' => {
+            'authorizations' => {
+              'record_disclosure' => true,
+              'record_disclosure_limitations' => [],
+              'address_change' => true
             },
-            "dependent" => {
-              "name" => {
-                "first" => "John",
-                "middle" => "Middle",
-                "last" => "Doe"
+            'dependent' => {
+              'name' => {
+                'first' => 'John',
+                'middle' => 'Middle',
+                'last' => 'Doe'
               },
-              "address" => {
-                "address_line1" => "123 Main St",
-                "address_line2" => "Apt 1",
-                "city" => "Springfield",
-                "state_code" => "IL",
-                "country" => "US",
-                "zip_code" => "62704",
-                "zip_code_suffix" => "6789"
+              'address' => {
+                'address_line1' => '123 Main St',
+                'address_line2' => 'Apt 1',
+                'city' => 'Springfield',
+                'state_code' => 'IL',
+                'country' => 'US',
+                'zip_code' => '62704',
+                'zip_code_suffix' => '6789'
               },
-              "date_of_birth" => "1980-12-31",
-              "relationship" => "Spouse",
-              "phone" => "1234567890",
-              "email" => "veteran@example.com"
+              'date_of_birth' => '1980-12-31',
+              'relationship' => 'Spouse',
+              'phone' => '1234567890',
+              'email' => 'veteran@example.com'
             },
-            "veteran" => {
-              "name" => {
-                "first" => "John",
-                "middle" => "Middle",
-                "last" => "Doe"
+            'veteran' => {
+              'name' => {
+                'first' => 'John',
+                'middle' => 'Middle',
+                'last' => 'Doe'
               },
-              "address" => {
-                "address_line1" => "123 Main St",
-                "address_line2" => "Apt 1",
-                "city" => "Springfield",
-                "state_code" => "IL",
-                "country" => "US",
-                "zip_code" => "62704",
-                "zip_code_suffix" => "6789"
+              'address' => {
+                'address_line1' => '123 Main St',
+                'address_line2' => 'Apt 1',
+                'city' => 'Springfield',
+                'state_code' => 'IL',
+                'country' => 'US',
+                'zip_code' => '62704',
+                'zip_code_suffix' => '6789'
               },
-              "ssn" => "123456789",
-              "va_file_number" => "123456789",
-              "date_of_birth" => "1980-12-31",
-              "service_number" => "123456789",
-              "service_branch" => "ARMY",
-              "phone" => "1234567890",
-              "email" => "veteran@example.com"
+              'ssn' => '123456789',
+              'va_file_number' => '123456789',
+              'date_of_birth' => '1980-12-31',
+              'service_number' => '123456789',
+              'service_branch' => 'ARMY',
+              'phone' => '1234567890',
+              'email' => 'veteran@example.com'
             }
           },
           'resolution' => {
