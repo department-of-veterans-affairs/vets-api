4adfce060e49cc2e1bfb1ad1e818359a15e2d145
Oren Mittman <nihil2501@gmail.com>
2024-12-30 21:13:53 -0500

    [ART] Re-sequence POA request representative migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_23_144811) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_27_213062) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
