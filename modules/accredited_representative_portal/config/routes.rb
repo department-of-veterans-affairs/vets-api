4b461756dd2048f5f3dc25a397579240f856a8c0
Oren Mittman <nihil2501@gmail.com>
2024-12-31 07:07:38 -0500

    [ART] lint fixes

--- modules/accredited_representative_portal/config/routes.rb
+++ modules/accredited_representative_portal/config/routes.rb
@@ -7,7 +7,7 @@ AccreditedRepresentativePortal::Engine.routes.draw do
     post 'form21a', to: 'form21a#submit'
 
     resources :in_progress_forms, only: %i[update show destroy]
-    resources :power_of_attorney_requests, only: [:index, :show] do
+    resources :power_of_attorney_requests, only: %i[index show] do
       resource :decision, only: :create, controller: 'power_of_attorney_request_decisions'
     end
   end
