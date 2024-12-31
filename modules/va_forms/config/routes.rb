48aba63d5abc7fd71e0006e219f5f336c28f7c05
Kristen Brown <11942904+kristen-brown@users.noreply.github.com>
2024-12-31 14:59:52 -0500

    API-43073: Remove VA Forms index and show endpoints

--- modules/va_forms/config/routes.rb
+++ modules/va_forms/config/routes.rb
@@ -2,8 +2,4 @@
 
 VAForms::Engine.routes.draw do
   match '/v0/*path', to: 'application#cors_preflight', via: [:options]
-
-  namespace :v0, defaults: { format: 'json' } do
-    resources :forms, only: %i[index show]
-  end
 end
