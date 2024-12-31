088c9c6a36952ee7033bb17bdfcb32c90e40a9c1
Kristen Brown <11942904+kristen-brown@users.noreply.github.com>
2024-12-31 14:48:47 -0500

    API-43073: Remove VA Forms healthcheck

--- modules/va_forms/config/routes.rb
+++ modules/va_forms/config/routes.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 VAForms::Engine.routes.draw do
-  get '/metadata', to: 'metadata#index'
   match '/v0/*path', to: 'application#cors_preflight', via: [:options]
-  get '/v0/healthcheck', to: 'metadata#healthcheck'
-  get '/v0/upstream_healthcheck', to: 'metadata#upstream_healthcheck'
 
   namespace :v0, defaults: { format: 'json' } do
     resources :forms, only: %i[index show]
