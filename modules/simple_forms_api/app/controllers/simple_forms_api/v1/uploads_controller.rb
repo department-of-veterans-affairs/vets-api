bcd538e60f093c7fd6212a72a236ca57f974176a
Jacob Penner <japenner@gmail.com>
2025-01-03 10:40:33 -0500

    update new logic after rebase

--- modules/simple_forms_api/app/controllers/simple_forms_api/v1/uploads_controller.rb
+++ modules/simple_forms_api/app/controllers/simple_forms_api/v1/uploads_controller.rb
@@ -51,45 +51,6 @@ module SimpleFormsApi
         UNAUTHENTICATED_FORMS.include?(params[:form_number]) || UNAUTHENTICATED_FORMS.include?(params[:form_id])
       end
 
-      def submit_intent_to_file
-        submission = SimpleFormsApi::IntentToFile::Submission.new(@current_user, params)
-        submission.submit
-      end
-
-      def submit_form_to_benefits_intake
-        submission = SimpleFormsApi::BenefitsIntake::Submission.new(@current_user, params)
-        submission.submit
-      end
-
-      def handle264555
-        parsed_form_data = JSON.parse(params.to_json)
-        form = SimpleFormsApi::VBA264555.new(parsed_form_data)
-        lgy_response = LGY::Service.new.post_grant_application(payload: form.as_payload)
-        reference_number = lgy_response.body['reference_number']
-        status = lgy_response.body['status']
-        Rails.logger.info(
-          'Simple forms api - sent to lgy',
-          { form_number: params[:form_number], status:, reference_number: }
-        )
-
-        if Flipper.enabled?(:simple_forms_email_confirmations)
-          case status
-          when 'VALIDATED', 'ACCEPTED'
-            send_sahsha_email(parsed_form_data, reference_number, :confirmation)
-          when 'REJECTED'
-            send_sahsha_email(parsed_form_data, reference_number, :rejected)
-          when 'DUPLICATE'
-            send_sahsha_email(parsed_form_data, reference_number, :duplicate)
-          end
-        end
-
-        { json: { reference_number:, status: }, status: lgy_response.status }
-      end
-
-      def form_is264555_and_should_use_lgy_api
-        params[:form_number] == '26-4555' && @current_user&.icn
-      end
-
       def submission
         if intent_service.use_intent_api?
           IntentToFile::Submission.new(@current_user, params)
@@ -98,37 +59,6 @@ module SimpleFormsApi
         else
           BenefitsIntake::Submission.new(@current_user, params)
         end
-      end
-
-      def send_intent_received_email(parsed_form_data, confirmation_number, expiration_date)
-        config = {
-          form_data: parsed_form_data,
-          form_number: 'vba_21_0966_intent_api',
-          confirmation_number:,
-          date_submitted: Time.zone.today.strftime('%B %d, %Y'),
-          expiration_date:
-        }
-        notification_email = SimpleFormsApi::NotificationEmail.new(
-          config,
-          notification_type: :received,
-          user: @current_user
-        )
-        notification_email.send
-      end
-
-      def send_sahsha_email(parsed_form_data, confirmation_number, notification_type)
-        config = {
-          form_data: parsed_form_data,
-          form_number: 'vba_26_4555',
-          confirmation_number:,
-          date_submitted: Time.zone.today.strftime('%B %d, %Y')
-        }
-        notification_email = SimpleFormsApi::NotificationEmail.new(
-          config,
-          notification_type:,
-          user: @current_user
-        )
-        notification_email.send
       end
     end
   end
--- modules/simple_forms_api/app/services/simple_forms_api/lgy/submission.rb
+++ modules/simple_forms_api/app/services/simple_forms_api/lgy/submission.rb
@@ -22,7 +22,38 @@ module SimpleFormsApi
           'Simple forms api - sent to lgy',
           { form_number: params[:form_number], status:, reference_number: }
         )
+
+        handle_emails(status, parsed_form_data, reference_number) if Flipper.enabled?(:simple_forms_email_confirmations)
+
         { json: { reference_number:, status: }, status: lgy_response.status }
+      end
+
+      private
+
+      def handle_emails(status, parsed_form_data, reference_number)
+        case status
+        when 'VALIDATED', 'ACCEPTED'
+          send_sahsha_email(parsed_form_data, reference_number, :confirmation)
+        when 'REJECTED'
+          send_sahsha_email(parsed_form_data, reference_number, :rejected)
+        when 'DUPLICATE'
+          send_sahsha_email(parsed_form_data, reference_number, :duplicate)
+        end
+      end
+
+      def send_sahsha_email(parsed_form_data, confirmation_number, notification_type)
+        config = {
+          form_data: parsed_form_data,
+          form_number: 'vba_26_4555',
+          confirmation_number:,
+          date_submitted: Time.zone.today.strftime('%B %d, %Y')
+        }
+        notification_email = SimpleFormsApi::NotificationEmail.new(
+          config,
+          notification_type:,
+          user: @current_user
+        )
+        notification_email.send
       end
     end
   end
