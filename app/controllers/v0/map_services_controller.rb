31591204bad7a50f22b765570074f6208fd3f024
John Bramley <20125855+bramleyjl@users.noreply.github.com>
2025-01-07 07:05:59 -0700

    [VI-252] MAP STS token validation (#19907)
    * adds map sts token duration validation
    
    * updates
    
    * updates & unit specs
    
    * initial MAP JWT signature validation
    
    * map jwks call
    
    * updates map response to a VA.gov-owned keypair
    
    * spec & mock updates
    
    * updates map jwks handling, spec fixes
    
    * spec updates
    
    * updates
    
    * imports SiS PublicJwks module

--- app/controllers/v0/map_services_controller.rb
+++ app/controllers/v0/map_services_controller.rb
@@ -11,7 +11,7 @@ module V0
       result = MAP::SecurityToken::Service.new.token(application: params[:application].to_sym, icn:, cache: false)
 
       render json: result, status: :ok
-    rescue Common::Client::Errors::ClientError, Common::Exceptions::GatewayTimeout
+    rescue Common::Client::Errors::ClientError, Common::Exceptions::GatewayTimeout, JWT::DecodeError
       render json: sts_client_error, status: :bad_gateway
     rescue MAP::SecurityToken::Errors::ApplicationMismatchError
       render json: application_mismatch_error, status: :bad_request
