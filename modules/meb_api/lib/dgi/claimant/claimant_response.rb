1dbfbb0f2168bfcdad3276ee9fa0c0947ee4f5b9
s-caso <sebastian.caso@accenturefederal.com>
2025-01-07 15:47:48 -0500

    use respond to to check if fetch is viable

--- modules/meb_api/lib/dgi/claimant/claimant_response.rb
+++ modules/meb_api/lib/dgi/claimant/claimant_response.rb
@@ -10,7 +10,7 @@ module MebApi
 
         def initialize(status, response = nil)
           attributes = {
-            claimant_id: response&.body.is_a?(String) ? response&.body : response&.body&.fetch('claimant_id', nil)
+            claimant_id: response&.body.respond_to?(:fetch) ?  response&.body&.fetch('claimant_id', nil) : response&.body
           }
           super(status, attributes)
         end
