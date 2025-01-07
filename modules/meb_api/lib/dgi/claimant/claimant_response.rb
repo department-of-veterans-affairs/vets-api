4399675a5f53b26b69dc1b8d0f2a4b215dc41443
s-caso <sebastian.caso@accenturefederal.com>
2025-01-07 12:37:09 -0500

    handle case where claimant response isn't a hash

--- modules/meb_api/lib/dgi/claimant/claimant_response.rb
+++ modules/meb_api/lib/dgi/claimant/claimant_response.rb
@@ -10,7 +10,7 @@ module MebApi
 
         def initialize(status, response = nil)
           attributes = {
-            claimant_id: response&.body&.fetch('claimant_id', nil)
+            claimant_id: response&.body.is_a? String ? response&.body : response&.body&.fetch('claimant_id', nil)
           }
           super(status, attributes)
         end
