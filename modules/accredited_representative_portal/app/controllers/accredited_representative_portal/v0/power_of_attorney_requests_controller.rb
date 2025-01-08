3c61cf13fba763bb29c4279e2ac78e07158be21f
OJ Bucao <9256675+ojbucao@users.noreply.github.com>
2025-01-08 13:14:05 -0800

    (fix) formatting issues

--- modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
+++ modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
@@ -6,14 +6,12 @@ module AccreditedRepresentativePortal
       def index
         poa_requests = PowerOfAttorneyRequest.includes(resolution: :resolving).limit(100)
         serializer = PowerOfAttorneyRequestSerializer.new(poa_requests)
-
         render json: serializer.serializable_hash, status: :ok
       end
 
       def show
         poa_request = PowerOfAttorneyRequest.includes(resolution: :resolving).find(params[:id])
         serializer = PowerOfAttorneyRequestSerializer.new(poa_request)
-
         render json: serializer.serializable_hash, status: :ok
       rescue ActiveRecord::RecordNotFound
         render json: { error: 'Record not found' }, status: :not_found
