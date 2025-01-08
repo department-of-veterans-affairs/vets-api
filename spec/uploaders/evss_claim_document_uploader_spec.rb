d52e135b534b278701fd39acf4e73174675cc734
Lindsey Hattamer <Lindsey.Hattamer@oddball.io>
2025-01-08 09:24:00 -0500

    update tests with unique data to prevent race conditions

--- spec/uploaders/evss_claim_document_uploader_spec.rb
+++ spec/uploaders/evss_claim_document_uploader_spec.rb
@@ -5,7 +5,9 @@ require 'rails_helper'
 RSpec.describe EVSSClaimDocumentUploader do
   subject { document_uploader }
 
-  let(:document_uploader) { described_class.new('1234', ['11', nil]) }
+  let(:user_uuid) { SecureRandom.uuid }
+  let(:document_uploader) { described_class.new(user_uuid, ['11', nil]) }
+
   let(:uploader_with_tiff) do
     f = Rack::Test::UploadedFile.new('spec/fixtures/evss_claim/image.TIF', 'image/tiff')
     document_uploader.store!(f)
@@ -16,6 +18,10 @@ RSpec.describe EVSSClaimDocumentUploader do
     f = Rack::Test::UploadedFile.new('spec/fixtures/evss_claim/converted_image.TIF.jpg', 'image/jpeg')
     document_uploader.store!(f)
     document_uploader
+  end
+
+  after(:each) do
+    FileUtils.rm_rf(Dir[Rails.root.join('tmp/uploads/evss_claim_documents/*')])
   end
 
   describe 'initialize' do
@@ -152,20 +158,38 @@ RSpec.describe EVSSClaimDocumentUploader do
   end
 
   describe '#store_dir' do
+    let(:user_uuid) { SecureRandom.uuid }
+    let(:tracked_item_id) { '13' }
+    let(:secondary_id) { SecureRandom.uuid }
+
     it 'omits the tracked item id if it is nil' do
-      subject = described_class.new('1234abc', EVSSClaimDocument.new.uploader_ids)
-      expect(subject.store_dir).to eq('evss_claim_documents/1234abc')
+      uploader = described_class.new(user_uuid, [nil, nil])
+      expect(uploader.store_dir).to eq("evss_claim_documents/#{user_uuid}")
     end
 
-    it 'includes the uuid and tracked item id' do
-      subject = described_class.new('1234abc', ['13', nil])
-      expect(subject.store_dir).to eq('evss_claim_documents/1234abc/13')
+    it 'includes the tracked item id if provided' do
+      uploader = described_class.new(user_uuid, [tracked_item_id, nil])
+      expect(uploader.store_dir).to eq("evss_claim_documents/#{user_uuid}/#{tracked_item_id}")
     end
 
-    it 'includes both uuids' do
-      uuid = SecureRandom.uuid
-      subject = described_class.new('1234abc', [nil, uuid])
-      expect(subject.store_dir).to eq("evss_claim_documents/1234abc/#{uuid}")
+    it 'includes both tracked item id and secondary id if provided' do
+      uploader = described_class.new(user_uuid, [tracked_item_id, secondary_id])
+      expect(uploader.store_dir).to eq("evss_claim_documents/#{user_uuid}/#{tracked_item_id}/#{secondary_id}")
+    end
+
+    it 'handles a case where both IDs are nil' do
+      uploader = described_class.new(user_uuid, [nil, nil])
+      expect(uploader.store_dir).to eq("evss_claim_documents/#{user_uuid}")
+    end
+
+    it 'handles a case where user_uuid is missing or nil' do
+      uploader = described_class.new(nil, [tracked_item_id, secondary_id])
+      expect(uploader.store_dir).to eq("evss_claim_documents//#{tracked_item_id}/#{secondary_id}")
+    end
+
+    it 'handles unexpected values in ids' do
+      uploader = described_class.new(user_uuid, ['', 123])
+      expect(uploader.store_dir).to eq("evss_claim_documents/#{user_uuid}/123")
     end
   end
 end
