47e77d72f7cc0e5119152fdd94bd449b9ec4d0cb
Josh Fike <josh.fike@oddball.io>
2025-01-09 14:31:16 -0600

    Add tests

--- modules/representation_management/spec/models/representation_management/form_2122_data_spec.rb
+++ modules/representation_management/spec/models/representation_management/form_2122_data_spec.rb
@@ -3,6 +3,42 @@
 require 'rails_helper'
 
 RSpec.describe RepresentationManagement::Form2122Data, type: :model do
+  describe '#limitations_of_consent_checkbox' do
+    context 'when record_consent is false' do
+      it 'returns 0' do
+        form_2122_data = described_class.new(record_consent: false, consent_limits: [])
+
+        expect(form_2122_data.limitations_of_consent_checkbox('ALCOHOLISM')).to eq(0)
+      end
+    end
+
+    context 'when record_consent is true' do
+      context 'when consent_limits is empty' do
+        it 'returns 1' do
+          form_2122_data = described_class.new(record_consent: true, consent_limits: [])
+
+          expect(form_2122_data.limitations_of_consent_checkbox('ALCOHOLISM')).to eq(0)
+        end
+      end
+
+      context 'when consent_limits does not include the key' do
+        it 'returns 1' do
+          form_2122_data = described_class.new(record_consent: true, consent_limits: %w[DRUG_ABUSE])
+
+          expect(form_2122_data.limitations_of_consent_checkbox('ALCOHOLISM')).to eq(1)
+        end
+      end
+
+      context 'when consent_limits includes the key' do
+        it 'returns 0' do
+          form_2122_data = described_class.new(record_consent: true, consent_limits: %w[ALCOHOLISM])
+
+          expect(form_2122_data.limitations_of_consent_checkbox('ALCOHOLISM')).to eq(0)
+        end
+      end
+    end
+  end
+
   describe '#organization' do
     context 'when organization is found in AccreditedOrganization' do
       it 'returns the AccreditedOrganization' do
