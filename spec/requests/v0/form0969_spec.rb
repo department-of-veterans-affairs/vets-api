93992ba5fe371a382a27e6c9490e4bf7191f06ab
Ian Donovan <iandonovan@users.noreply.github.com>
2025-01-14 12:20:52 -0500

    Remove feature flipper for JSON schemer and always use it

--- spec/requests/v0/form0969_spec.rb
+++ spec/requests/v0/form0969_spec.rb
@@ -13,75 +13,70 @@ RSpec.describe 'V0::Form0969', type: %i[request serializer] do
     build(:income_and_assets_claim).parsed_form
   end
 
-  [true, false].each do |flipper_value|
-    context "when json_schemer flipper is #{flipper_value}" do
+  before do
+    allow(Flipper).to receive(:enabled?).and_call_original
+  end
+
+  describe 'POST create' do
+    subject do
+      post('/v0/form0969',
+           params: params.to_json,
+           headers: { 'CONTENT_TYPE' => 'application/json', 'HTTP_X_KEY_INFLECTION' => 'camel' })
+    end
+
+    context 'with invalid params' do
       before do
-        allow(Flipper).to receive(:enabled?).and_call_original
-        allow(Flipper).to receive(:enabled?).with(:validate_saved_claims_with_json_schemer).and_return(flipper_value)
+        allow(Settings.sentry).to receive(:dsn).and_return('asdf')
       end
 
-      describe 'POST create' do
-        subject do
-          post('/v0/form0969',
-               params: params.to_json,
-               headers: { 'CONTENT_TYPE' => 'application/json', 'HTTP_X_KEY_INFLECTION' => 'camel' })
+      let(:params) do
+        {
+          incomeAndAssetsClaim: {
+            form: full_claim.merge('veteranSocialSecurityNumber' => 'just a string').to_json
+          }
+        }
+      end
+
+      it 'shows the validation errors' do
+        subject
+        expect(response).to have_http_status(:unprocessable_entity)
+
+        if flipper_value
+          expect(
+            JSON.parse(response.body)['errors'][0]['detail'].include?(
+              '/veteran-social-security-number - string at `/veteranSocialSecurityNumber` ' \
+              'does not match pattern: ^[0-9]{9}$'
+            )
+          ).to eq(true)
+        else
+          expect(
+            JSON.parse(response.body)['errors'][0]['detail'].include?(
+              "The property '#/veteranSocialSecurityNumber' value \"just a string\" did not match the regex"
+            )
+          ).to eq(true)
         end
+      end
+    end
 
-        context 'with invalid params' do
-          before do
-            allow(Settings.sentry).to receive(:dsn).and_return('asdf')
-          end
+    context 'with valid params' do
+      let(:params) do
+        {
+          incomeAndAssetsClaim: {
+            form: full_claim.to_json
+          }
+        }
+      end
 
-          let(:params) do
-            {
-              incomeAndAssetsClaim: {
-                form: full_claim.merge('veteranSocialSecurityNumber' => 'just a string').to_json
-              }
-            }
-          end
+      it 'renders success' do
+        subject
+        expect(JSON.parse(response.body)['data']['attributes'].keys.sort)
+          .to eq(%w[confirmationNumber form guid regionalOffice submittedAt])
+      end
 
-          it 'shows the validation errors' do
-            subject
-            expect(response).to have_http_status(:unprocessable_entity)
-
-            if flipper_value
-              expect(
-                JSON.parse(response.body)['errors'][0]['detail'].include?(
-                  '/veteran-social-security-number - string at `/veteranSocialSecurityNumber` ' \
-                  'does not match pattern: ^[0-9]{9}$'
-                )
-              ).to eq(true)
-            else
-              expect(
-                JSON.parse(response.body)['errors'][0]['detail'].include?(
-                  "The property '#/veteranSocialSecurityNumber' value \"just a string\" did not match the regex"
-                )
-              ).to eq(true)
-            end
-          end
-        end
-
-        context 'with valid params' do
-          let(:params) do
-            {
-              incomeAndAssetsClaim: {
-                form: full_claim.to_json
-              }
-            }
-          end
-
-          it 'renders success' do
-            subject
-            expect(JSON.parse(response.body)['data']['attributes'].keys.sort)
-              .to eq(%w[confirmationNumber form guid regionalOffice submittedAt])
-          end
-
-          it 'returns the expected regional office' do
-            subject
-            expect(JSON.parse(response.body)['data']['attributes']['regionalOffice'].join(', '))
-              .to eq(reg_office)
-          end
-        end
+      it 'returns the expected regional office' do
+        subject
+        expect(JSON.parse(response.body)['data']['attributes']['regionalOffice'].join(', '))
+          .to eq(reg_office)
       end
     end
   end
