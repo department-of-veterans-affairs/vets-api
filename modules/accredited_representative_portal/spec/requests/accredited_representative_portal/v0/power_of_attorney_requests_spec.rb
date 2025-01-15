2ccf56daa5e350657024123ca7b2e2a6667f146a
Gregory Hedrick <gregory@pixiitech.net>
2025-01-15 10:27:38 -0500

    Oren rewrite

--- modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_requests_spec.rb
+++ modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_requests_spec.rb
@@ -17,8 +17,6 @@ veteran_claimant_power_of_attorney_form =
 
 RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsController, type: :request do
   let(:test_user) { create(:representative_user, email: 'test@va.gov') }
-  let(:time) { '2024-12-21T04:45:37.458Z' }
-
   let(:poa_request) do
     resolution =
       create(
@@ -29,6 +27,9 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
 
     resolution.power_of_attorney_request
   end
+  let(:time) { '2024-12-21T04:45:37Z' }
+  let(:time_plus_one_day) { '2024-12-22T04:45:37Z' }
+  let(:expires_at) { '2025-02-19T04:45:37.000Z' }
 
   let(:poa_requests) do
     [].tap do |memo|
@@ -66,6 +67,9 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
   end
 
   before do
+    AccreditedRepresentativePortal::PowerOfAttorneyForm.delete_all
+    AccreditedRepresentativePortal::PowerOfAttorneyRequestResolution.delete_all
+    AccreditedRepresentativePortal::PowerOfAttorneyRequest.delete_all
     Flipper.enable(:accredited_representative_portal_pilot)
     login_as(test_user)
     travel_to(time)
@@ -182,11 +186,11 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
     end
 
     context 'when providing a status param' do
-      let!(:declined_request) { create(:power_of_attorney_request_resolution, :declination).power_of_attorney_request }
-      let!(:pending_request) { create(:power_of_attorney_request) }
+      let!(:declined_request) { create(:power_of_attorney_request, :with_declination) }
+      let!(:pending_request) { create(:power_of_attorney_request, skip_resolution: true) }
 
       it 'returns the list of pending power of attorney requests' do
-        get('/accredited_representative_portal/v0/power_of_attorney_requests?status=Pending')
+        get('/accredited_representative_portal/v0/power_of_attorney_requests?status=pending')
         parsed_response = JSON.parse(response.body)
         expect(response).to have_http_status(:ok)
         expect(parsed_response.length).to eq 1
@@ -194,21 +198,23 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsContro
       end
 
       it 'returns the list of completed power of attorney requests' do
-        get('/accredited_representative_portal/v0/power_of_attorney_requests?status=Completed')
+        get('/accredited_representative_portal/v0/power_of_attorney_requests?status=completed')
         parsed_response = JSON.parse(response.body)
         expect(response).to have_http_status(:ok)
         expect(parsed_response.length).to eq 1
         expect(parsed_response[0]['id']).to eq declined_request.id
       end
 
-      it 'throws 400 if any other status filter provided' do
+      it 'throws an error if any other status filter provided' do
         get('/accredited_representative_portal/v0/power_of_attorney_requests?status=delete_all')
-        expect(response).to have_http_status(:bad_request)
+        expect(response).to have_http_status(:internal_server_error)
       end
     end
   end
 
   describe 'GET /accredited_representative_portal/v0/power_of_attorney_requests/:id' do
+    let(:poa_request) { create(:power_of_attorney_request, :with_declination) }
+
     it 'returns the details of a specific power of attorney request' do
       get("/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}")
 
