4c6908c24f3a5a9dcd274a5ae6f6c314bf11a75a
Matt Christianson <95487885+mchristiansonVA@users.noreply.github.com>
2025-01-15 11:41:47 -0500

    Fix blueprinter to use res from service

--- modules/claims_api/app/controllers/claims_api/v2/blueprints/power_of_attorney_request_blueprint.rb
+++ modules/claims_api/app/controllers/claims_api/v2/blueprints/power_of_attorney_request_blueprint.rb
@@ -63,16 +63,16 @@ module ClaimsApi
         end
 
         view :decide do
-          field :id do |form_attributes|
-            form_attributes['id']
+          field :id do |res|
+            res['id']
           end
 
           field :type do
             'power-of-attorney-request-decision'
           end
 
-          field :attributes do |form_attributes|
-            form_attributes.except('id')
+          field :attributes do |res|
+            res.except('id')
           end
 
           transform ClaimsApi::V2::Blueprints::Transformers::LowerCamelTransformer
