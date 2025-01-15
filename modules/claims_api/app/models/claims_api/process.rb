1ea4a4de7373896a22ba53e5e0e342dc3462785c
Tyler Coleman <tyler.coleman@oddball.io>
2025-01-15 11:58:06 -0800

    add validation and spec

--- modules/claims_api/app/models/claims_api/process.rb
+++ modules/claims_api/app/models/claims_api/process.rb
@@ -3,5 +3,28 @@
 module ClaimsApi
   class Process < ApplicationRecord
     belongs_to :processable, polymorphic: true
+
+    VALID_POA_STEP_TYPES = %w[PDF_SUBMISSION POA_UPDATE POA_ACCESS_UPDATE CLAIMANT_NOTIFICATION].freeze
+    VALID_POA_STEP_STATUSES = %w[NOT_STARTED IN_PROGRESS SUCCESS FAILED].freeze
+
+    validates :step_type, presence: true
+    validates :step_status, presence: true
+    validate :validate_step_type_and_status
+
+    private
+
+    def validate_step_type_and_status
+      case processable_type
+      when 'ClaimsApi::PowerOfAttorney'
+        validate_power_of_attorney_enums
+      else
+        errors.add(:processable_type, 'is not recognized')
+      end
+    end
+
+    def validate_power_of_attorney_enums
+      errors.add(:step_type, 'is not recognized') unless VALID_POA_STEP_TYPES.include?(step_type)
+      errors.add(:step_status, 'is not recognized') unless VALID_POA_STEP_STATUSES.include?(step_status)
+    end
   end
 end
