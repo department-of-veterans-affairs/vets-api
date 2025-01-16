e4295f43c6b9e947a441d009019e4d798f4fff15
Trevor Bosaw <bosawt@gmail.com>
2025-01-16 14:19:52 -0800

    Example of Chatbot controller refactored to authenticate with SiS STS

--- app/controllers/v0/virtual_agent_jwt_token_controller.rb
+++ app/controllers/v0/virtual_agent_jwt_token_controller.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
-require 'erb'
-
 module V0
-  class VirtualAgentJwtTokenController < ApplicationController
+  class VirtualAgentJwtTokenController < SignIn::ServiceAccountApplicationController
     service_tag 'virtual-agent'
     rescue_from 'V0::VirtualAgentJwtTokenController::ServiceException', with: :service_exception_handler
     rescue_from Net::HTTPError, with: :service_exception_handler
 
     def create
-      # on a post request, if the flipper is enabled
-      if Flipper.enabled?(:virtual_agent_fetch_jwt_token, current_user)
-        # create a new jwt token
-        jwt_token = VirtualAgent::JwtToken.new.new_jwt_token(current_user)
-        render json: { token: jwt_token }
-      else
-        render json: { token: 'disabled' }
-      end
+      jwt_token = VirtualAgent::JwtToken.new(icn).new_jwt_token
+      render json: { token: jwt_token }
+    end
+
+    private
+
+    def icn
+      @service_account_access_token.user_attributes['icn']
     end
   end
 end
