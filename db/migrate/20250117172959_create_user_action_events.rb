8d0d4229cb8c971f5bee9a15b11e732efc8f2784
Alex Jolovic <alexander.jolovic@bylight.com>
2025-01-17 13:46:45 -0800

    Update column types: change details and ip_address to text, device_info to jsonb

--- db/migrate/20250117172959_create_user_action_events.rb
+++ db/migrate/20250117172959_create_user_action_events.rb
@@ -1,7 +1,7 @@
 class CreateUserActionEvents < ActiveRecord::Migration[7.2]
   def change
     create_table :user_action_events do |t|
-      t.string :details, null: false
+      t.text :details, null: false
       t.timestamps null: false
     end
   end
