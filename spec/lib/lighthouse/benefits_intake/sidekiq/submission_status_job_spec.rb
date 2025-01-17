2bd9fd4077187c591b2b8c5065526deba59be6f1
Wayne Weibel <wayne.weibel@adhocteam.us>
2025-01-17 10:46:37 -0500

    .

--- spec/lib/lighthouse/benefits_intake/sidekiq/submission_status_job_spec.rb
+++ spec/lib/lighthouse/benefits_intake/sidekiq/submission_status_job_spec.rb
@@ -5,6 +5,8 @@ require 'lighthouse/benefits_intake/sidekiq/submission_status_job'
 
 Rspec.describe BenefitsIntake::SubmissionStatusJob, type: :job do
   let(:job) { described_class.new }
+  let(:service) { BenefitsIntake::Service.new }
+  let(:response) { double(body: { 'data' => [] }, success?: true) }
 
   context 'flipper is disabled' do
     before do
@@ -12,8 +14,8 @@ Rspec.describe BenefitsIntake::SubmissionStatusJob, type: :job do
     end
 
     it 'does nothing' do
-      expect(Rails.logger).not_to receive(:info)  # does not log start
-      expect(Rails.logger).not_to receive(:error) # does not log error
+      expect(Rails.logger).not_to receive(:info)
+      expect(Rails.logger).not_to receive(:error)
       expect(FormSubmissionAttempt).not_to receive(:where)
       expect(BenefitsIntake::Service).not_to receive(:new)
       job.perform
@@ -23,7 +25,29 @@ Rspec.describe BenefitsIntake::SubmissionStatusJob, type: :job do
   context 'flipper is enabled' do
     before do
       Flipper.enable(:benefits_intake_submission_status_job)
+
+      allow(BenefitsIntake::Service.new).to receive(:new).and_return(service)
     end
+
+    context 'multiple attempts and multiple form submissions' do
+      before do
+        create_list(:form_submission, 2, :success)
+        create_list(:form_submission, 2, :failure)
+      end
+
+      let(:pending_fsa_ids) do
+        create_list(:form_submission_attempt, 2, :pending).map(&:benefits_intake_uuid)
+      end
+
+      it 'submits only pending form submissions' do
+        expect(service).to receive(:bulk_status).with(uuids: pending_fsa_ids).and_return(response)
+
+        expect(StatsD).not_to receive(:increment)
+
+        job.perform
+      end
+    end
+
   end
 
 end
