2c27db7e724e6f6b6235aecdf898800ebb9626f1
Ian Perera <iputreetek@gmail.com>
2025-01-17 11:43:11 -0500

    revert

--- modules/va_notify/app/models/va_notify/notification.rb
+++ modules/va_notify/app/models/va_notify/notification.rb
@@ -3,6 +3,7 @@
 module VANotify
   class Notification < ApplicationRecord
     self.table_name = 'va_notify_notifications'
+    serialize :to, coder: JsonMarshal::Marshaller
 
     has_kms_key
     has_encrypted :to, key: :kms_key, **lockbox_options
