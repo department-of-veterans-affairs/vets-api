ffbe41eedbd476af7c099eeeea88d34a5b52d641
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-01-17 10:26:40 -0600

    fixing cop re-enable

--- modules/ivc_champva/app/controllers/ivc_champva/v1/pega_controller.rb
+++ modules/ivc_champva/app/controllers/ivc_champva/v1/pega_controller.rb
@@ -111,7 +111,7 @@ module IvcChampva
           end
         end
       end
-      # rubocop:enable
+      # rubocop:enable Metrics/MethodLength
 
       def valid_keys?(data)
         true if VALID_KEYS.all? { |key| data.key?(key) }
