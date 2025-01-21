f5e8629d16156651b89ac4cb55aa301f985e0ada
achan <achan@Ubuntu.myguest.virtualbox.org>
2025-01-21 14:33:26 -0500

    Make excel file a true attachment

--- app/mailers/create_excel_files_mailer.rb
+++ app/mailers/create_excel_files_mailer.rb
@@ -3,19 +3,20 @@
 class CreateExcelFilesMailer < ApplicationMailer
   def build(filename)
     date = Time.zone.now.strftime('%m/%d/%Y')
-    file_contents = File.read("tmp/#{filename}")
-    headers['Content-Disposition'] = "attachment; filename=#{filename}"
 
     # rubocop:disable Layout/LineLength
     recipients = Settings.vsp_environment.eql?('production') ? Settings.edu.production_excel_contents.emails : Settings.edu.staging_excel_contents.emails
     subject = Settings.vsp_environment.eql?('production') ? "22-10282 Form CSV file for #{date}" : "Staging CSV file for #{date}"
     # rubocop:enable Layout/LineLength
 
+    attachments[filename] = File.read("tmp/#{filename}")
+
     mail(
       to: recipients,
-      subject: subject,
-      content_type: 'text/csv',
-      body: file_contents
-    )
+      subject: subject
+    ) do |format|
+      format.text { render plain: "CSV file for #{date} is attached." }
+      format.html { render html: "<p>CSV file for #{date} is attached.</p>" }
+    end
   end
 end
