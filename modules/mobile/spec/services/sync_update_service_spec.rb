76bfb83456a8268287453ab6f540763978fa8943
Michael Harmer <michael.harmer@wilcore.io>
2025-01-22 10:17:34 -0500

    97889 | Moved sync vcrs to va_profile/v3

--- modules/mobile/spec/services/sync_update_service_spec.rb
+++ modules/mobile/spec/services/sync_update_service_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative '../support/helpers/rails_helper'
 
 describe Mobile::V0::Profile::SyncUpdateService do
   let(:service) { Mobile::V0::Profile::SyncUpdateService.new(user) }
@@ -38,9 +39,9 @@ describe Mobile::V0::Profile::SyncUpdateService do
 
     context 'when it succeeds after one incomplete status check' do
       let(:transaction) do
-        VCR.use_cassette('mobile/profile/v2/address_transaction_status') do
-          VCR.use_cassette('mobile/profile/v2/get_address_status_incomplete') do
-            VCR.use_cassette('mobile/profile/v2/put_address_initial') do
+        VCR.use_cassette('va_profile/v3/contact_information/address_transaction_status') do
+          VCR.use_cassette('va_profile/v3/contact_information/get_address_status_incomplete') do
+            VCR.use_cassette('va_profile/v3/contact_information/put_address_initial') do
               service.save_and_await_response(resource_type: :address, params:, update: true)
             end
           end
@@ -58,10 +59,10 @@ describe Mobile::V0::Profile::SyncUpdateService do
 
     context 'when it succeeds after two incomplete checks' do
       let(:transaction) do
-        VCR.use_cassette('mobile/profile/v2/get_address_status_complete') do
-          VCR.use_cassette('mobile/profile/v2/get_address_status_incomplete_2') do
-            VCR.use_cassette('mobile/profile/v2/get_address_status_incomplete') do
-              VCR.use_cassette('mobile/profile/v2/put_address_initial') do
+        VCR.use_cassette('va_profile/v3/contact_information/get_address_status_complete') do
+          VCR.use_cassette('va_profile/v3/contact_information/get_address_status_incomplete_2') do
+            VCR.use_cassette('va_profile/v3/contact_information/get_address_status_incomplete') do
+              VCR.use_cassette('va_profile/v3/contact_information/put_address_initial') do
                 service.save_and_await_response(resource_type: :address, params:, update: true)
               end
             end
@@ -86,10 +87,10 @@ describe Mobile::V0::Profile::SyncUpdateService do
       end
 
       it 'raises a gateway timeout error' do
-        VCR.use_cassette('mobile/profile/v2/get_address_status_complete') do
-          VCR.use_cassette('mobile/profile/v2/get_address_status_incomplete_2') do
-            VCR.use_cassette('mobile/profile/v2/get_address_status_incomplete') do
-              VCR.use_cassette('mobile/profile/v2/put_address_initial') do
+        VCR.use_cassette('va_profile/v3/contact_information/get_address_status_complete') do
+          VCR.use_cassette('va_profile/v3/contact_information/get_address_status_incomplete_2') do
+            VCR.use_cassette('va_profile/v3/contact_information/get_address_status_incomplete') do
+              VCR.use_cassette('va_profile/v3/contact_information/put_address_initial') do
                 expect { service.save_and_await_response(resource_type: :address, params:, update: true) }
                   .to raise_error(Common::Exceptions::GatewayTimeout)
               end
@@ -101,8 +102,8 @@ describe Mobile::V0::Profile::SyncUpdateService do
 
     context 'when it fails on a status check returning an error' do
       it 'raises a backend service exception' do
-        VCR.use_cassette('mobile/profile/v2/get_address_status_error') do
-          VCR.use_cassette('mobile/profile/v2/put_address_initial') do
+        VCR.use_cassette('va_profile/v3/contact_information/get_address_status_error') do
+          VCR.use_cassette('va_profile/v3/contact_information/put_address_initial') do
             expect { service.save_and_await_response(resource_type: :address, params:, update: true) }
               .to raise_error(Common::Exceptions::BackendServiceException)
           end
