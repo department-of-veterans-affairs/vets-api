cba2b63d144faa9de3f183fa61fc4dce12fb9641
Tyler Coleman <tyler.coleman@oddball.io>
2025-01-23 09:00:13 -0800

    remove linebreak

--- modules/claims_api/app/sidekiq/claims_api/poa_updater.rb
+++ modules/claims_api/app/sidekiq/claims_api/poa_updater.rb
@@ -29,7 +29,6 @@ module ClaimsApi
         ClaimsApi::VANotifyAcceptedJob.perform_async(poa_form.id, rep_id) if vanotify?(poa_form.auth_headers, rep_id)
 
         ClaimsApi::PoaVBMSUpdater.perform_async(poa_form.id)
-
       else
         poa_form.status = ClaimsApi::PowerOfAttorney::ERRORED
         poa_form.vbms_error_message = "BGS Error: update_birls_record failed with code #{response[:return_code]}"
