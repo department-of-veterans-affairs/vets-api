373b3409fe179396b56d3163171ad9c4353b0dc5
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-01-23 13:45:58 -0700

    added new flipper logic per platforms request and commented out old code

--- modules/mobile/spec/requests/mobile/v0/claim/legacy_documents_spec.rb
+++ modules/mobile/spec/requests/mobile/v0/claim/legacy_documents_spec.rb
@@ -16,8 +16,10 @@ RSpec.describe 'legacy Mobile::V0::Claim::Document', :skip_json_api_validation, 
   let(:json_body_headers) { { 'Content-Type' => 'application/json', 'Accept' => 'application/json' } }
 
   before do
-    Flipper.disable(:mobile_lighthouse_document_upload)
-    Flipper.disable(:mobile_lighthouse_document_upload, user)
+    # Flipper.disable(:mobile_lighthouse_document_upload)
+    # Flipper.disable(:mobile_lighthouse_document_upload, user)
+    allow(Flipper).to receive(:enabled?).with(:mobile_lighthouse_document_upload,
+                                              instance_of(User)).and_return(false)
     FileUtils.rm_rf(Rails.root.join('tmp', 'uploads', 'cache', '*'))
     user.user_account_uuid = user_account.id
     user.save!
