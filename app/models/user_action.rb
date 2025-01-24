a61d5b77f21d88afbb5be2945a1d9e902a8a8692
Alex Jolovic <alexander.jolovic@bylight.com>
2025-01-24 10:39:10 -0800

    Refactor UserAction and UserActionEvent specs
    - Remove prefix from UserAction status enum for cleaner method names
    - Update specs to use shoulda-matchers for associations
    - Remove redundant tests for Rails built-in functionality (factory traits and enum transitions)

--- app/models/user_action.rb
+++ app/models/user_action.rb
@@ -6,13 +6,13 @@ class UserAction < ApplicationRecord
   belongs_to :user_action_event
   belongs_to :subject_user_verification, class_name: 'UserVerification', optional: true
 
-  enum status: {
+  enum :status, {
     initial: 'initial',
     success: 'success',
     error: 'error'
-  }, _prefix: true
+  }
 
+  validates :status, presence: true, inclusion: { in: statuses.keys }
   validates :acting_ip_address, presence: true
   validates :acting_user_agent, presence: true
-  validates :status, presence: true, inclusion: { in: statuses.keys }
 end
--- spec/models/user_action_spec.rb
+++ spec/models/user_action_spec.rb
@@ -61,76 +61,19 @@ RSpec.describe UserAction, type: :model do
   end
 
   describe 'associations' do
-    let(:user_action) { create(:user_action) }
-
-    it 'belongs to acting_user_account' do
-      expect(user_action.acting_user_account).to be_a(UserAccount)
-    end
-
-    it 'belongs to subject_user_account' do
-      expect(user_action.subject_user_account).to be_a(UserAccount)
-    end
-
-    it 'belongs to user_action_event' do
-      expect(user_action.user_action_event).to be_a(UserActionEvent)
-    end
-
-    context 'with subject_user_verification' do
-      let(:user_action) { create(:user_action, :with_verification) }
-
-      it 'can have an optional subject_user_verification' do
-        expect(user_action.subject_user_verification).to be_a(UserVerification)
-      end
-    end
+    it { is_expected.to belong_to(:acting_user_account).class_name('UserAccount') }
+    it { is_expected.to belong_to(:subject_user_account).class_name('UserAccount') }
+    it { is_expected.to belong_to(:user_action_event) }
+    it { is_expected.to belong_to(:subject_user_verification).class_name('UserVerification').optional }
   end
 
   describe 'enum status' do
     let(:user_action) { create(:user_action) }
 
     it 'defines status predicates' do
-      expect(user_action).to respond_to(:status_initial?)
-      expect(user_action).to respond_to(:status_success?)
-      expect(user_action).to respond_to(:status_error?)
-    end
-  end
-
-  describe 'factory traits' do
-    describe ':success_status' do
-      let(:user_action) { create(:user_action, :success_status) }
-
-      it 'sets the status to success' do
-        expect(user_action.status).to eq('success')
-        expect(user_action).to be_status_success
-      end
-    end
-
-    describe ':error_status' do
-      let(:user_action) { create(:user_action, :error_status) }
-
-      it 'sets the status to error' do
-        expect(user_action.status).to eq('error')
-        expect(user_action).to be_status_error
-      end
-    end
-  end
-
-  describe 'status transitions' do
-    let(:user_action) { create(:user_action) }
-
-    it 'starts with initial status' do
-      expect(user_action).to be_status_initial
-    end
-
-    it 'can transition from initial to success' do
-      user_action.status = 'success'
-      expect(user_action).to be_valid
-      expect(user_action).to be_status_success
-    end
-
-    it 'can transition from initial to error' do
-      user_action.status = 'error'
-      expect(user_action).to be_valid
-      expect(user_action).to be_status_error
+      expect(user_action).to respond_to(:initial?)
+      expect(user_action).to respond_to(:success?)
+      expect(user_action).to respond_to(:error?)
     end
   end
 end
