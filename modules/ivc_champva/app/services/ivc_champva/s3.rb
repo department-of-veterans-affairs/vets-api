5a2373137c7ec252f643eabc8a4cdf02ae698e20
Don Shin <cloudmagic80@gmail.com>
2025-01-25 08:26:04 -0600

    datadog logs for s3

--- modules/ivc_champva/app/services/ivc_champva/s3.rb
+++ modules/ivc_champva/app/services/ivc_champva/s3.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'ivc_champva/monitor'
+
 module IvcChampva
   class S3
     attr_reader :region, :bucket
@@ -13,15 +15,23 @@ module IvcChampva
       Datadog::Tracing.trace('S3 Put File(s)') do
         metadata&.transform_values! { |value| value || '' }
 
-        client.put_object({
-                            bucket:,
-                            key:,
-                            body: File.read(file),
-                            metadata:
-                          })
-        { success: true }
-      rescue => e
-        { success: false, error_message: "S3 PutObject failure for #{file}: #{e.message}" }
+        begin
+          response = client.put_object(
+            bucket:,
+            key:,
+            body: File.read(file),
+            metadata: metadata
+          )
+          result = { success: true }
+        rescue => e
+          result = { success: false, error_message: "S3 PutObject failure for #{file}: #{e.message}" }
+        end
+
+        if Flipper.enabled?(:champva_log_all_s3_uploads, @current_user)
+          response ? handle_put_object_response(response, key, file) : handle_put_object_error(e)
+        else
+          result
+        end
       end
     end
 
@@ -36,7 +46,30 @@ module IvcChampva
       end
     end
 
+    def monitor
+      @monitor ||= IvcChampva::Monitor.new
+    end
+
     private
+
+    def handle_put_object_response(response, key, file)
+      if response.status == 200
+        monitor.track_all_successful_s3_uploads(key)
+        { success: true }
+      else
+        error_message = "S3 PutObject failure for #{file}: Status code: #{response.status}"
+        if response.respond_to?(:body) && response.body.respond_to?(:read)
+          error_message += ", Body: #{response.body.read}"
+        end
+        Rails.logger.error error_message
+        { success: false, error_message: error_message }
+      end
+    end
+
+    def handle_put_object_error(error)
+      Rails.logger.error "S3 PutObject unexpected error: #{error.message}"
+      { success: false, error_message: "S3 PutObject unexpected error: #{error.message.strip}" }
+    end
 
     def client
       @client ||= Aws::S3::Client.new(
