475f95610146ac91cf3b2a0f536193f56e5dafda
Jun Tao Luo <john.luo@agile6.com>
2025-01-26 13:39:40 -0500

    Remove /locations/recent_facilities endpoint and related functions

--- modules/vaos/app/controllers/vaos/v2/clinics_controller.rb
+++ modules/vaos/app/controllers/vaos/v2/clinics_controller.rb
@@ -41,40 +41,6 @@ module VAOS
         render json: VAOS::V2::ClinicsSerializer.new(clinic)
       end
 
-      def recent_facilities
-        sorted_appointments = appointments_service.get_sorted_recent_appointments
-
-        if sorted_appointments.blank?
-          render json: { message: 'No appointments found' }, status: :not_found
-          return
-        end
-
-        facility_ids = []
-
-        sorted_appointments.each do |appt|
-          # if we don't have the information to lookup the clinic, return 'unable to lookup' message
-          unable_to_lookup_facility?(appt) ? log_unable_to_lookup_facility(appt) : facility_ids.push(appt.location_id)
-        end
-
-        # remove duplicate facility ids
-        facility_ids = facility_ids.uniq
-        sorted_facilities = []
-
-        facility_ids.each do |facility_id|
-          # get the facility details using the location id
-          facility = mobile_facility_service.get_facility(facility_id)
-          log_recent_facility_details(facility_id, facility)
-
-          # if facility details are not returned, log 'not found' message
-          facility.nil? ? log_no_facility_details_found(facility_id) : sorted_facilities.push(facility)
-        end
-
-        # Filter facilities
-        sorted_facilities = filter_type_of_care_facilities(sorted_facilities)
-
-        render json: FacilitiesSerializer.new(sorted_facilities)
-      end
-
       private
 
       def appointments_service
@@ -128,31 +94,8 @@ module VAOS
         appt.nil? || appt.location_id.nil? || appt.clinic.nil?
       end
 
-      def log_unable_to_lookup_facility(appt)
-        message = ''
-        if appt.nil?
-          message = 'Appointment not found'
-        elsif appt.location_id.nil?
-          message = 'Appointment does not have location id'
-        end
-
-        Rails.logger.info('VAOS recent_facilities', message) if message.present?
-      end
-
-      def log_no_facility_details_found(location_id)
-        Rails.logger.info 'VAOS recent_facilities', "No facility details found for location: #{location_id}"
-      end
-
-      def log_recent_facility_details(location_id, facility)
-        Rails.logger.info('VAOS recent_facilities', "Details found for location: #{location_id} - #{facility.to_json}")
-      end
-
       def log_no_supported_facilities(service_id)
         Rails.logger.info('VAOS recent_facilities', "No facility supporting #{service_id} service was found.")
-      end
-
-      def unable_to_lookup_facility?(appt)
-        appt.nil? || appt.location_id.nil?
       end
 
       def systems_service
@@ -166,10 +109,6 @@ module VAOS
 
       def location_id
         params.require(:location_id)
-      end
-
-      def recent_facilities_params
-        params.permit(:clinical_service_id)
       end
     end
   end
