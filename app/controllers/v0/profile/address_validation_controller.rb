e0bc574c14c00221cd3d22127126278b1e456b4c
122034971+ConnorJeff@users.noreply.github.com <122034971+ConnorJeff@users.noreply.github.com>
2025-01-27 13:22:36 -0500

    MBMS-71996 Added the create back to the skip authenticate for address validation

--- app/controllers/v0/profile/address_validation_controller.rb
+++ app/controllers/v0/profile/address_validation_controller.rb
@@ -10,7 +10,7 @@ module V0
     class AddressValidationController < ApplicationController
       service_tag 'profile'
 
-      skip_before_action :authenticate
+      skip_before_action :authenticate, only: [:create]
       skip_before_action :verify_authenticity_token
 
       def create
