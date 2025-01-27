bf0d1cd717fdced818ffa46bb1b37e3b6e3a29c1
Alex Jolovic <alexander.jolovic@bylight.com>
2025-01-27 15:25:19 -0800

    Remove system_initiated trait and its tests as requested

--- spec/factories/user_actions.rb
+++ spec/factories/user_actions.rb
@@ -2,25 +2,11 @@
 
 FactoryBot.define do
   factory :user_action do
-    acting_ip_address { Faker::Internet.ip_v4_address }
-    acting_user_agent { Faker::Internet.user_agent }
-    status { 'initial' }
-    association :acting_user_verification, factory: :user_verification
     association :subject_user_verification, factory: :user_verification
     association :user_action_event
-
-    trait :system_initiated do
-      acting_user_verification { nil }
-      acting_ip_address { nil }
-      acting_user_agent { nil }
-    end
-
-    trait :success_status do
-      status { 'success' }
-    end
-
-    trait :error_status do
-      status { 'error' }
-    end
+    status { 'initial' }
+    acting_ip_address { Faker::Internet.ip_v4_address }
+    acting_user_agent { Faker::Internet.user_agent }
+    association :acting_user_verification, factory: :user_verification
   end
-end
+end 
--- spec/models/user_action_spec.rb
+++ spec/models/user_action_spec.rb
@@ -68,30 +68,4 @@ RSpec.describe UserAction, type: :model do
       expect(user_action).to respond_to(:error?)
     end
   end
-
-  describe 'factory traits' do
-    describe ':system_initiated' do
-      let(:system_action) { create(:user_action, :system_initiated) }
-
-      it 'has nil acting_user_verification' do
-        expect(system_action.acting_user_verification).to be_nil
-      end
-
-      it 'has nil acting_ip_address' do
-        expect(system_action.acting_ip_address).to be_nil
-      end
-
-      it 'has nil acting_user_agent' do
-        expect(system_action.acting_user_agent).to be_nil
-      end
-
-      it 'is valid' do
-        expect(system_action).to be_valid
-      end
-
-      it 'still requires subject_user_verification' do
-        expect(system_action.subject_user_verification).to be_present
-      end
-    end
-  end
-end
+end 
