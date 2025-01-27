890ecb2399aff4bdcb8e87e06f3bdc673df74347
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2025-01-26 23:16:10 -0500

    align column names with joined tables names

--- modules/accredited_representative_portal/db/migrate/20250121164657_prepare_ar_pos_holder_data.rb
+++ modules/accredited_representative_portal/db/migrate/20250121164657_prepare_ar_pos_holder_data.rb
@@ -3,8 +3,8 @@ class PrepareArPosHolderData < ActiveRecord::Migration[7.2]
 
   def change
     safety_assured do
-      add_column :ar_power_of_attorney_requests, :ogc_registration_number, :string
-      add_column :ar_power_of_attorney_requests, :ogc_poa_code, :string
+      add_column :ar_power_of_attorney_requests, :accredited_individual_registration_number, :string
+      add_column :ar_power_of_attorney_requests, :power_of_attorney_holder_poa_code, :string
     end
   end
 end
