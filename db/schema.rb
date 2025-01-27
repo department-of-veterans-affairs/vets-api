1bf3a77fc2e867417d05fb2dd3f97a441bbb7d88
Trevor Bosaw <bosawt@gmail.com>
2025-01-27 10:29:37 -0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2025_01_24_211447) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
