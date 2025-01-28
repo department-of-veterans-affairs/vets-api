0e29492ab1744ff5d850e5df7c17618897c9345b
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2025-01-27 22:20:17 -0700

    clarifies unit test for post create

--- modules/va_notify/spec/requests/callbacks_spec.rb
+++ modules/va_notify/spec/requests/callbacks_spec.rb
@@ -17,31 +17,33 @@ RSpec.describe 'VANotify Callbacks', type: :request do
   end
   let(:callback_route) { '/va_notify/callbacks' }
 
-  describe 'POST #notifications' do
-    it 'with found notification' do
-      template_id = SecureRandom.uuid
-      notification = VANotify::Notification.create(notification_id: notification_id,
-                                                   source_location: 'some_location',
-                                                   callback_metadata: 'some_callback_metadata',
-                                                   template_id: template_id)
-      expect(notification.status).to be_nil
-      allow(Rails.logger).to receive(:info)
-      callback_obj = double('VANotify::DefaultCallback')
-      allow(VANotify::DefaultCallback).to receive(:new).and_return(callback_obj)
-      allow(callback_obj).to receive(:call)
+  describe 'POST #create' do
+    context 'with found notification' do
+      it 'updates notification' do
+        template_id = SecureRandom.uuid
+        notification = VANotify::Notification.create(notification_id: notification_id,
+                                                     source_location: 'some_location',
+                                                     callback_metadata: 'some_callback_metadata',
+                                                     template_id: template_id)
+        expect(notification.status).to be_nil
+        allow(Rails.logger).to receive(:info)
+        callback_obj = double('VANotify::DefaultCallback')
+        allow(VANotify::DefaultCallback).to receive(:new).and_return(callback_obj)
+        allow(callback_obj).to receive(:call)
 
-      post(callback_route,
-           params: callback_params.to_json,
-           headers: { 'Authorization' => "Bearer #{valid_token}", 'Content-Type' => 'application/json' })
+        post(callback_route,
+             params: callback_params.to_json,
+             headers: { 'Authorization' => "Bearer #{valid_token}", 'Content-Type' => 'application/json' })
 
-      expect(Rails.logger).to have_received(:info).with(
-        "va_notify callbacks - Updating notification: #{notification.id}",
-        { source_location: 'some_location', template_id: template_id, callback_metadata: 'some_callback_metadata',
-          status: 'delivered' }
-      )
-      expect(response.body).to include('success')
-      notification.reload
-      expect(notification.status).to eq('delivered')
+        expect(Rails.logger).to have_received(:info).with(
+          "va_notify callbacks - Updating notification: #{notification.id}",
+          { source_location: 'some_location', template_id: template_id, callback_metadata: 'some_callback_metadata',
+            status: 'delivered' }
+        )
+        expect(response.body).to include('success')
+        notification.reload
+        expect(notification.status).to eq('delivered')
+      end
     end
 
     it 'with missing notification' do
