d4e2d24e450de23375ee19b3cb6e564cd8f21901
Molly Trombley-McCann <molly.trombley-mccann@coforma.io>
2025-01-28 13:36:00 -0800

    Trigger CI runs for flaky tests

--- spec/sidekiq/decision_review/form4142_submit_spec.rb
+++ spec/sidekiq/decision_review/form4142_submit_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe DecisionReview::Form4142Submit, type: :job do
     service
   end
 
-  let(:user_uuid) { create(:user, :loa3, ssn: '212222112').uuid }
+  let(:user_uuid) { create(:user, :loa3, ssn: '212222111').uuid }
   let(:mpi_profile) { build(:mpi_profile, vet360_id: Faker::Number.number) }
   let(:find_profile_response) { create(:find_profile_response, profile: mpi_profile) }
   let(:mpi_service) do
