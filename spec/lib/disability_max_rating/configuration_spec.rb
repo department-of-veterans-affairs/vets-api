3adca3305090cd65673f04832e5d2ed347abab44
asiisii <ashish.malla@agile6.com>
2025-01-28 12:59:11 -0700

    fix tests

--- spec/lib/disability_max_rating/configuration_spec.rb
+++ spec/lib/disability_max_rating/configuration_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'disability_max_rating/configuration'
 
 RSpec.describe DisabilityMaxRating::Configuration do
-  subject { described_class.new }
+  subject { described_class.send(:new) }
 
   describe '#base_path' do
     it 'returns the correct base URL from the settings' do
@@ -18,10 +19,11 @@ RSpec.describe DisabilityMaxRating::Configuration do
   end
 
   describe '#connection' do
-    it 'builds a Faraday connection' do
+    it 'includes the correct middleware and uses the configured base path' do
       connection = subject.connection
+
       expect(connection.builder.handlers).to include(Faraday::Response::RaiseError)
-      expect(connection.builder.handlers).to include(Faraday::Adapter::NetHttp)
+      expect(connection.builder.handlers).to include(Faraday::Response::Json)
     end
   end
 end
