15173f9ce6cf3cc637653973a15b897a87d8beef
Ian Donovan <iandonovan@users.noreply.github.com>
2025-01-29 14:29:39 -0500

    Override RV1 - Reserve Records Request status

--- spec/lib/lighthouse/benefits_claims/service_spec.rb
+++ spec/lib/lighthouse/benefits_claims/service_spec.rb
@@ -57,12 +57,68 @@ RSpec.describe BenefitsClaims::Service do
       end
 
       describe 'when requesting one single benefit claim' do
-        it 'has overriden PMR Pending tracked items to the NEEDED_FROM_OTHERS status and readable name' do
-          VCR.use_cassette('lighthouse/benefits_claims/show/200_response') do
-            response = @service.get_claim('600383363')
-            # In the cassette, the status is NEEDED_FROM_YOU
-            expect(response.dig('data', 'attributes', 'trackedItems', 0, 'status')).to eq('NEEDED_FROM_OTHERS')
-            expect(response.dig('data', 'attributes', 'trackedItems', 0, 'displayName')).to eq('Private Medical Record')
+        before { allow(Flipper).to receive(:enabled?).and_call_original }
+
+        context 'when the PMR Pending override flipper is enabled' do
+          before do
+            allow(Flipper).to receive(:enabled?).with(:cst_override_pmr_pending_tracked_items).and_return(true)
+          end
+
+          it 'has overridden PMR Pending tracked items to the NEEDED_FROM_OTHERS status and readable name' do
+            VCR.use_cassette('lighthouse/benefits_claims/show/200_response') do
+              response = @service.get_claim('600383363')
+              # In the cassette, the status is NEEDED_FROM_YOU
+              expect(response.dig('data', 'attributes', 'trackedItems', 0, 'status')).to eq('NEEDED_FROM_OTHERS')
+              expect(response.dig('data', 'attributes', 'trackedItems', 0,
+                                  'displayName')).to eq('Private Medical Record')
+            end
+          end
+        end
+
+        context 'when the PMR Pending override flipper is disabled' do
+          before do
+            allow(Flipper).to receive(:enabled?).with(:cst_override_pmr_pending_tracked_items).and_return(false)
+          end
+
+          it 'has overridden PMR Pending tracked items to the NEEDED_FROM_OTHERS status and readable name' do
+            VCR.use_cassette('lighthouse/benefits_claims/show/200_response') do
+              response = @service.get_claim('600383363')
+              # In the cassette, the status is NEEDED_FROM_YOU
+              expect(response.dig('data', 'attributes', 'trackedItems', 0, 'status')).to eq('NEEDED_FROM_YOU')
+              expect(response.dig('data', 'attributes', 'trackedItems', 0, 'displayName')).to eq('PMR Pending')
+            end
+          end
+        end
+
+        context 'when the RV1 override flipper is enabled' do
+          before do
+            allow(Flipper).to receive(:enabled?).with(:cst_override_reserve_records_tracked_items).and_return(true)
+          end
+
+          it 'has overridden "RV1 - Reserve Records Request" to the NEEDED_FROM_OTHERS status' do
+            VCR.use_cassette('lighthouse/benefits_claims/show/200_response') do
+              response = @service.get_claim('600383363')
+              # In the cassette, the status is NEEDED_FROM_YOU
+              expect(response.dig('data', 'attributes', 'trackedItems', 2, 'status')).to eq('NEEDED_FROM_OTHERS')
+              expect(response.dig('data', 'attributes', 'trackedItems', 2,
+                                  'displayName')).to eq('RV1 - Reserve Records Request')
+            end
+          end
+        end
+
+        context 'when the RV1 override flipper is disabled' do
+          before do
+            allow(Flipper).to receive(:enabled?).with(:cst_override_reserve_records_tracked_items).and_return(false)
+          end
+
+          it 'has overridden "RV1 - Reserve Records Request" to the NEEDED_FROM_OTHERS status' do
+            VCR.use_cassette('lighthouse/benefits_claims/show/200_response') do
+              response = @service.get_claim('600383363')
+              # In the cassette, the status is NEEDED_FROM_YOU
+              expect(response.dig('data', 'attributes', 'trackedItems', 2, 'status')).to eq('NEEDED_FROM_YOU')
+              expect(response.dig('data', 'attributes', 'trackedItems', 2,
+                                  'displayName')).to eq('RV1 - Reserve Records Request')
+            end
           end
         end
 
@@ -74,7 +130,7 @@ RSpec.describe BenefitsClaims::Service do
           it 'includes Attorney Fee, Secondary Action Required, and Stage 2 Development tracked items' do
             VCR.use_cassette('lighthouse/benefits_claims/show/200_response') do
               response = @service.get_claim('600383363')
-              expect(response.dig('data', 'attributes', 'trackedItems').size).to eq(3)
+              expect(response.dig('data', 'attributes', 'trackedItems').size).to eq(4)
               expect(response.dig('data', 'attributes', 'trackedItems', 0,
                                   'displayName')).to eq('Private Medical Record')
               expect(response.dig('data', 'attributes', 'trackedItems', 1,
@@ -92,7 +148,7 @@ RSpec.describe BenefitsClaims::Service do
           it 'excludes Attorney Fee, Secondary Action Required, and Stage 2 Development tracked items' do
             VCR.use_cassette('lighthouse/benefits_claims/show/200_response') do
               response = @service.get_claim('600383363')
-              expect(response.dig('data', 'attributes', 'trackedItems').size).to eq(2)
+              expect(response.dig('data', 'attributes', 'trackedItems').size).to eq(3)
               expect(response.dig('data', 'attributes', 'trackedItems', 0,
                                   'displayName')).to eq('Private Medical Record')
               expect(response.dig('data', 'attributes', 'trackedItems', 1,
