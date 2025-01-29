8a39460b04e0f0877bc0a67dcfec8f256285aab9
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2025-01-29 09:40:19 -0500

    Revert "spec fixing"
    This reverts commit 21de6604ea3c6d074707385e5db1b549649679c8.

--- modules/accredited_representative_portal/spec/factories/power_of_attorney_request.rb
+++ modules/accredited_representative_portal/spec/factories/power_of_attorney_request.rb
@@ -7,6 +7,10 @@ FactoryBot.define do
 
     association :power_of_attorney_holder, factory: %i[accredited_organization with_representatives], strategy: :create
 
+    before(:create) do |poa_request|
+      poa_request.accredited_individual_id = AccreditedIndividual.first&.id || create(:accredited_individual).id
+    end
+
     transient do
       resolution_created_at { nil }
     end
