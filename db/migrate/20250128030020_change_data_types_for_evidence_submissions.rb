d009624d4aead1e1288036038752a7a110494b4d
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-01-29 12:53:06 -0700

    remove reversible do and direction up/down

--- db/migrate/20250128030020_change_data_types_for_evidence_submissions.rb
+++ db/migrate/20250128030020_change_data_types_for_evidence_submissions.rb
@@ -5,21 +5,9 @@ class ChangeDataTypesForEvidenceSubmissions < ActiveRecord::Migration[7.2]
 
   def change
     safety_assured do
-      reversible do |direction|
-        change_table :evidence_submissions do |t|
-          direction.up do
-            EvidenceSubmission.where(tracked_item_id: '[nil]').update_all(tracked_item_id: nil)
-            change_column :evidence_submissions, :request_id, :integer, using: 'request_id::integer'
-            change_column :evidence_submissions, :claim_id, :integer, using: 'claim_id::integer'
-            change_column :evidence_submissions, :tracked_item_id, :integer, using: 'tracked_item_id::integer'
-          end
-          direction.down do
-            change_column :evidence_submissions, :request_id, :string
-            change_column :evidence_submissions, :claim_id, :string
-            change_column :evidence_submissions, :tracked_item_id, :string
-          end
-        end
-      end
+      change_column :evidence_submissions, :request_id, :integer, using: 'request_id::integer'
+      change_column :evidence_submissions, :claim_id, :integer, using: 'claim_id::integer'
+      change_column :evidence_submissions, :tracked_item_id, :integer, using: 'tracked_item_id::integer'
     end
   end
 end
