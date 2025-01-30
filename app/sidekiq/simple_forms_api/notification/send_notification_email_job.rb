4e08edf51eab2cd5ebd81d38556f74d735ec7998
Eric Tillberg <Thrillberg@users.noreply.github.com>
2025-01-30 17:06:14 -0500

    feedback

--- app/sidekiq/simple_forms_api/notification/send_notification_email_job.rb
+++ app/sidekiq/simple_forms_api/notification/send_notification_email_job.rb
@@ -9,32 +9,26 @@ module SimpleFormsApi
 
       HOUR_TO_SEND_NOTIFICATIONS = 9
 
-      attr_reader :notification_type, :form_number, :confirmation_number, :date_submitted, :lighthouse_updated_at,
-                  :user_account
+      attr_reader :notification_type, :config, :user_account
 
       def perform(notification_type:, form_submission_attempt:, user_account:)
         @notification_type = notification_type
-        @form_number = V1::UploadsController::FORM_NUMBER_MAP[form_submission_attempt.form_submission.form_type]
-        @confirmation_number = form_submission_attempt.benefits_intake_uuid
-        @date_submitted = form_submission_attempt.created_at.strftime('%B %d, %Y')
-        @lighthouse_updated_at = form_submission_attempt.lighthouse_updated_at&.strftime('%B %d, %Y')
         @user_account = user_account
+        form_submission = form_submission_attempt.form_submission
+        @config = {
+          form_data: JSON.parse(form_submission.form_data),
+          form_number: V1::UploadsController::FORM_NUMBER_MAP[form_submission.form_type],
+          confirmation_number: form_submission_attempt.benefits_intake_uuid,
+          date_submitted: form_submission_attempt.created_at.strftime('%B %d, %Y'),
+          lighthouse_updated_at: form_submission_attempt.lighthouse_updated_at&.strftime('%B %d, %Y')
+        }
 
-        form_data = JSON.parse(form_submission_attempt.form_submission.form_data)
-        notification_email(form_data)
+        notification_email
       end
 
       private
 
-      def notification_email(form_data)
-        config = {
-          form_data:,
-          form_number:,
-          confirmation_number:,
-          date_submitted:,
-          lighthouse_updated_at:
-        }
-
+      def notification_email
         SimpleFormsApi::NotificationEmail.new(
           config,
           notification_type:,
