23b9320123f3cf330f89fbde7eb85aeb5f96b203
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-30 09:08:49 -0700

    update fetch_drive_times method to return nil from guard

--- modules/vaos/app/controllers/vaos/v2/appointments_controller.rb
+++ modules/vaos/app/controllers/vaos/v2/appointments_controller.rb
@@ -382,7 +382,7 @@ module VAOS
       def fetch_drive_times(provider)
         user_address = current_user.vet360_contact_info&.residential_address
 
-        return '' unless user_address&.latitude && user_address.longitude
+        return nil unless user_address&.latitude && user_address.longitude
 
         eps_provider_service.get_drive_times(
           destinations: {
