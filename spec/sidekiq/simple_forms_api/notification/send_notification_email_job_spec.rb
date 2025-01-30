4b1437e9fa3b96d2167acb50d6ced49172fe6a58
Eric Tillberg <Thrillberg@users.noreply.github.com>
2025-01-30 10:34:47 -0500

    Add test for failure case

--- spec/sidekiq/simple_forms_api/notification/send_notification_email_job_spec.rb
+++ spec/sidekiq/simple_forms_api/notification/send_notification_email_job_spec.rb
@@ -5,20 +5,35 @@ require 'rails_helper'
 RSpec.describe SimpleFormsApi::Notification::SendNotificationEmailJob, type: :worker do
   describe '#perform' do
     context 'form was submitted with a digital form submission tool' do
+      let(:notification_type) { :confirmation }
+      let(:form_submission_attempt) { build(:form_submission_attempt) }
+      let(:user_account) { build(:user_account) }
+      let(:notification_email) { double(send: nil) }
+
       it 'sends the email' do
-        notification_type = :confirmation
-        form_submission_attempt = build(:form_submission_attempt)
-        user_account = build(:user_account)
-        notification_email = double(send: nil)
         allow(SimpleFormsApi::NotificationEmail).to receive(:new).and_return(notification_email)
 
-        SimpleFormsApi::Notification::SendNotificationEmailJob.new.perform(
+        described_class.new.perform(
           notification_type:,
           form_submission_attempt:,
           user_account:
         )
 
         expect(notification_email).to have_received(:send).with(at: anything)
+      end
+
+      context 'SimpleFormsApi::NotificationEmail initialization fails' do
+        it 'raises an error' do
+          allow(SimpleFormsApi::NotificationEmail).to receive(:new).and_raise(ArgumentError)
+
+          expect do
+            described_class.new.perform(
+              notification_type:,
+              form_submission_attempt:,
+              user_account:
+            )
+          end.to raise_error(ArgumentError)
+        end
       end
     end
   end
