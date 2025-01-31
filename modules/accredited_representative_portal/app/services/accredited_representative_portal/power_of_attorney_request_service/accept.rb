222c36c8aae7fcad67fd96ea3218e37bfdb21d9b
Gregory Hedrick <gregory@pixiitech.net>
2025-01-31 12:04:33 -0500

    PoA decision controller - added LH (#101919)
    - Added accept and decline services
    - Error handling
    - Request spec

---
+++ modules/accredited_representative_portal/app/services/accredited_representative_portal/power_of_attorney_request_service/accept.rb
@@ -0,0 +1,137 @@
+# frozen_string_literal: true
+
+module AccreditedRepresentativePortal
+  module PowerOfAttorneyRequestService
+    class Accept
+      attr_reader :poa_request, :creator, :reason, :resolution
+      attr_accessor :form_submission
+
+      def initialize(poa_request, creator, reason)
+        @poa_request = poa_request
+        @creator = creator
+        @reason = reason
+      end
+
+      def call
+        ApplicationRecord.transaction do
+          create_resolution!
+          create_form_submission!
+        end
+
+        service = BenefitsClaims::Service.new(poa_request.claimant.icn)
+        response = service.submit2122(attributes)
+
+        form_submission.update(
+          service_id: response.body.dig('data', 'id'),
+          service_response: response.body.to_json
+        )
+        form_submission
+      # TODO: call PowerOfAttorneyFormSubmissionJob.perform_async(poa_form_submission)
+      # Transient 5xx errors: delete objects created, re-raise error
+      rescue *transient_error_types => e
+        form_submission.delete
+        resolution.delete
+        raise e
+      # All other errors: save error data on form submission
+      rescue => e
+        create_error_submission(e.message)
+        raise e
+      end
+
+      def create_resolution!
+        resolving = PowerOfAttorneyRequestDecision.create!(
+          type: PowerOfAttorneyRequestDecision::Types::ACCEPTANCE, creator:
+        )
+        ##
+        # This form triggers the uniqueness validation, while the
+        # `@poa_request.create_resolution!` form triggers a more obscure
+        # `RecordNotSaved` error that is less functional for getting
+        # validation errors.
+        #
+        @resolution = PowerOfAttorneyRequestResolution.create!(
+          power_of_attorney_request: poa_request,
+          resolving:,
+          reason:
+        )
+      end
+
+      def create_form_submission!
+        @form_submission = PowerOfAttorneyFormSubmission.create!(
+          power_of_attorney_request: poa_request,
+          status: :enqueue_succeeded,
+          status_updated_at: DateTime.current
+        )
+      end
+
+      def create_error_submission(message)
+        PowerOfAttorneyFormSubmission.create(
+          service_response: message,
+          error_message: message,
+          status: :enqueue_failed,
+          status_updated_at: DateTime.current
+        )
+      end
+
+      def attributes
+        {}.tap do |a|
+          a[:veteran] = veteran_data
+          a[:serviceOrganization] = service_org_data
+          a[:recordConsent] = form_data.dig('authorizations', 'recordDisclosure')
+          a[:consentLimits] = form_data.dig('authorizations', 'recordDisclosureLimitations')
+          a[:consentAddressChange] = form_data.dig('authorizations', 'addressChange')
+        end
+      end
+
+      def service_org_data
+        {
+          poaCode: poa_request.power_of_attorney_holder_poa_code,
+          registrationNumber: poa_request.accredited_individual_registration_number
+        }
+      end
+
+      def form_data
+        @form_data ||= JSON.parse(poa_request.power_of_attorney_form.data)
+      end
+
+      def veteran_data
+        service_number = form_data.dig('veteran', 'serviceNumber')
+        insurance_number = form_data.dig('veteran', 'insuranceNumber')
+        {}.tap do |v|
+          v[:address] = address_data(form_data.dig('veteran', 'address'))
+          v[:phone] = phone_data(form_data.dig('veteran', 'phone'))
+          v[:email] = form_data.dig('veteran', 'email')
+          v[:serviceNumber] = service_number if service_number.present?
+          v[:insuranceNumber] = insurance_number if insurance_number.present?
+        end
+      end
+
+      def phone_data(phone)
+        phone_number = phone.to_s.gsub(/-| /, '')
+        {}.tap do |p|
+          p[:areaCode] = phone_number[0..2]
+          p[:phoneNumber] = phone_number[3..9]
+          p[:phoneNumberExt] = phone_number[10..] if phone_number[10..].present?
+        end
+      end
+
+      def address_data(address_json)
+        {}.tap do |a|
+          a[:addressLine1] = address_json['addressLine1']
+          a[:addressLine2] = address_json['addressLine2'] if address_json['addressLine2'].present?
+          a[:city] = address_json['city']
+          a[:stateCode] = address_json['stateCode']
+          a[:countryCode] = address_json['country']
+          a[:zipCode] = address_json['zipCode'] if address_json['country'] == 'US'
+          if address_json['zipCodeSuffix'].present?
+            a[:zipCodeSuffix] =
+              address_json['zipCodeSuffix']
+          end
+        end
+      end
+
+      def transient_error_types
+        BenefitsClaims::ServiceException::ERROR_MAP.select { |key, _| key >= 500 }.values
+      end
+    end
+  end
+end
--- modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_request_decisions_spec.rb
+++ modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/power_of_attorney_request_decisions_spec.rb
@@ -4,31 +4,32 @@ require_relative '../../../rails_helper'
 
 RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestDecisionsController, type: :request do
   let(:test_user) { create(:representative_user) }
-  let(:time) { '2024-12-21T04:45:37.458Z' }
+  let(:poa_request) do
+    create(:power_of_attorney_request,
+           power_of_attorney_holder_poa_code: '095',
+           accredited_individual_registration_number: '999999999999')
+  end
 
   before do
+    allow_any_instance_of(Auth::ClientCredentials::Service).to receive(:get_token).and_return('fake_access_token')
     Flipper.enable(:accredited_representative_portal_pilot)
+    poa_request.claimant.update(icn: '1012666183V089914')
     login_as(test_user)
-    travel_to(time)
   end
 
   describe 'POST /accredited_representative_portal/v0/power_of_attorney_requests/:id/decision' do
     context 'with invalid params' do
       it 'complains about an invalid type param' do
-        poa_request = create(:power_of_attorney_request)
-
         post "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}/decision",
              params: { decision: { type: 'invalid_type', reason: nil } }
 
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(parsed_response['errors']).to eq(
-          ['Type is not included in the list']
-        )
+        expect(parsed_response['errors']).to eq([
+                                                  'Invalid type parameter - Types accepted: [acceptance declination]'
+                                                ])
       end
 
       it 'complains about an invalid reason param' do
-        poa_request = create(:power_of_attorney_request)
-
         post "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}/decision",
              params: { decision: { type: 'acceptance', reason: 'not allowed to give reasons for these' } }
 
@@ -37,14 +38,27 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestDecisio
           ['Reason must be blank']
         )
       end
+
+      it 'rep does not have poa for veteran' do
+        poa_request.update(power_of_attorney_holder_poa_code: '111',
+                           accredited_individual_registration_number: '1111111111')
+
+        VCR.use_cassette('lighthouse/benefits_claims/power_of_attorney_decision/404_response.yml') do
+          post "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}/decision",
+               params: { decision: { type: 'acceptance', reason: nil } }
+        end
+        expect(response).to have_http_status(:not_found)
+        poa_request.reload
+
+        expect(poa_request.resolution.present?).to be(true)
+      end
     end
 
-    it 'creates acceptance decision with proper params' do
-      poa_request = create(:power_of_attorney_request)
-
-      post "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}/decision",
-           params: { decision: { type: 'acceptance', reason: nil } }
-
+    it 'creates acceptance decision with veteran claimant' do
+      VCR.use_cassette('lighthouse/benefits_claims/power_of_attorney_decision/202_response.yml') do
+        post "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}/decision",
+             params: { decision: { type: 'acceptance', reason: nil } }
+      end
       expect(response).to have_http_status(:ok)
       expect(parsed_response).to eq({})
       poa_request.reload
@@ -55,7 +69,6 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestDecisio
     end
 
     it 'creates declination decision with proper params' do
-      poa_request = create(:power_of_attorney_request)
       post "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}/decision",
            params: { decision: { type: 'declination', reason: 'bad data' } }
 
@@ -78,7 +91,6 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestDecisio
     end
 
     it 'returns an error if decision already exists' do
-      poa_request = create(:power_of_attorney_request)
       create(:power_of_attorney_request_resolution, :expiration,
              power_of_attorney_request: poa_request)
 
@@ -92,10 +104,23 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestDecisio
     end
   end
 
+  context 'the server returns with a transient error' do
+    let(:lh_config) { double }
+
+    it 'returns an error, does not save anything' do
+      allow(Common::Client::Base).to receive(:configuration).and_return lh_config
+      allow(lh_config).to receive(:post).and_raise(Faraday::TimeoutError)
+
+      post "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}/decision",
+           params: { decision: { type: 'acceptance', reason: '' } }
+
+      expect(response).to have_http_status(:gateway_timeout)
+      expect(AccreditedRepresentativePortal::PowerOfAttorneyFormSubmission.all).to be_empty
+    end
+  end
+
   describe 'full cycle for decision api' do
     it 'returns the correct results for POST GET POST GET' do
-      poa_request = create(:power_of_attorney_request)
-
       # --------------
       # GET REQUEST
       get "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}"
@@ -105,8 +130,10 @@ RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestDecisio
 
       # --------------
       # POST REQUEST
-      post "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}/decision",
-           params: { decision: { type: 'acceptance', reason: nil } }
+      VCR.use_cassette('lighthouse/benefits_claims/power_of_attorney_decision/202_response.yml') do
+        post "/accredited_representative_portal/v0/power_of_attorney_requests/#{poa_request.id}/decision",
+             params: { decision: { type: 'acceptance', reason: nil } }
+      end
 
       expect(response).to have_http_status(:ok)
       expect(parsed_response).to eq({})
