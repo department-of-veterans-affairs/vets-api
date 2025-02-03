87f5952cf8b73cb07dd786b474098f64d1de299d
Mike <865014+michelpmcdonald@users.noreply.github.com>
2025-02-03 10:11:40 -0500

    delete temp files

--- modules/appeals_api/spec/services/appeals_api/pdf_construction/generator_spec.rb
+++ modules/appeals_api/spec/services/appeals_api/pdf_construction/generator_spec.rb
@@ -122,7 +122,7 @@ describe AppealsApi::PdfConstruction::Generator do
         let(:fixture_name) { 'expected_200995.pdf' }
         let(:sc) { create(:supplemental_claim, evidence_submission_indicated: true, created_at:) }
 
-        #after { File.delete(generated_pdf) if File.exist?(generated_pdf) }
+        after { File.delete(generated_pdf) if File.exist?(generated_pdf) }
 
         it 'generates the expected pdf' do
           expect(generated_pdf).to match_pdf(expected_pdf)
