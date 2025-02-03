e67dc95ca879c8892990a0b2cfc8963c9809da3b
PhilipDeFraties <philip.defraties@gmail.com>
2025-02-03 16:38:41 -0700

    remove statsd monitoring implementation from ProvidersController

--- modules/vaos/app/controllers/vaos/v2/providers_controller.rb
+++ modules/vaos/app/controllers/vaos/v2/providers_controller.rb
@@ -3,16 +3,9 @@
 module VAOS
   module V2
     class ProvidersController < VAOS::BaseController
-      STATSD_KEY = 'api.vaos.providers'
-
       def show
         provider_data = vaos_serializer.serialize(provider, 'providers')
         render json: { data: provider_data }
-      rescue => e
-        StatsDMetric.new(key: STATSD_KEY).save
-        StatsD.increment(STATSD_KEY, tags: ["error:#{e.class.name}"])
-
-        raise e
       end
 
       private
