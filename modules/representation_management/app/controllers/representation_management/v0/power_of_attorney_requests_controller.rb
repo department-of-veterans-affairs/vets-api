c8ec734c4bee07eb78fa0b913e5f7d5da3809062
Josh Fike <josh.fike@oddball.io>
2025-02-03 13:50:07 -0600

    Remove email data class

--- modules/representation_management/app/controllers/representation_management/v0/power_of_attorney_requests_controller.rb
+++ modules/representation_management/app/controllers/representation_management/v0/power_of_attorney_requests_controller.rb
@@ -17,22 +17,7 @@ module RepresentationManagement
         if flatten_form_params[:veteran_service_number].present?
           render json: { errors: ['render_error_state_for_failed_submission'] }, status: :unprocessable_entity
         elsif form.valid?
-          # data = RepresentationManagement::PowerOfAttorneyRequestEmailData.new(form)
-          # VANotify::EmailJob.perform_async(
-          #   data.email_address,
-          #   Settings.vanotify.services.va_gov.template_id.appoint_a_representative_confirmation_email,
-          #   {
-          #     # The first_name is the only key here that has an underscore.
-          #     # That is intentional.  All the keys here match the keys in the
-          #     # template.
-          #     'first_name' => data.first_name,
-          #     'form name' => data.form_name,
-          #     'form number' => data.form_number,
-          #     'representative type' => data.entity_display_type,
-          #     'representative name' => data.entity_name,
-          #     'representative address' => data.entity_address
-          #   }
-          # )
+
           render json: { message: 'Email enqueued' }, status: :ok
         else
           render json: { errors: form.errors.full_messages }, status: :unprocessable_entity
