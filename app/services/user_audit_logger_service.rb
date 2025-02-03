822d06e43621d90ff68cc9d2fcd37b887a2e54e9
John Bramley <bramleyjl@gmail.com>
2025-02-03 16:24:53 -0700

    makes logger function without acting_user_verification

--- app/services/user_audit_logger_service.rb
+++ app/services/user_audit_logger_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserAuditLoggerService
-  def self.log_user_action(details:, acting_user_verification:, subject_user_verification:,
+  def self.log_user_action(details:, acting_user_verification: nil, subject_user_verification:,
                            status:, acting_ip_address:, acting_user_agent:)
     user_action_event = UserActionEvent.create!(details:)
     UserAction.create!(acting_user_verification:,
