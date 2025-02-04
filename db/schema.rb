03f6b14205765e3b194f17344d8693ec3503e7bc
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2025-02-04 11:30:34 -0500

    moving migrations under module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_30_040343) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_04_162258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
