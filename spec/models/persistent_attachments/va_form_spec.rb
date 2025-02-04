f6f751afba23165728c560953108127addb71da8
Patrick Brown <patrick.brown@oddball.io>
2025-02-03 19:40:29 -0500

    art/representative-form-upload

--- spec/models/persistent_attachments/va_form_spec.rb
+++ spec/models/persistent_attachments/va_form_spec.rb
@@ -54,6 +54,14 @@ RSpec.describe PersistentAttachments::VAForm, :uploader_helpers do
       end
     end
 
+    context 'form_id 21-686c' do
+      before { instance.form_id = '21-686c' }
+
+      it 'returns 16' do
+        expect(instance.max_pages).to eq 16
+      end
+    end
+
     context 'default' do
       it 'returns 10' do
         expect(instance.max_pages).to eq 10
@@ -88,6 +96,14 @@ RSpec.describe PersistentAttachments::VAForm, :uploader_helpers do
 
     context 'form_id 21P-0516-1' do
       before { instance.form_id = '21P-0516-1' }
+
+      it 'returns 2' do
+        expect(instance.min_pages).to eq 2
+      end
+    end
+
+    context 'form_id 21-686c' do
+      before { instance.form_id = '21-686c' }
 
       it 'returns 2' do
         expect(instance.min_pages).to eq 2
