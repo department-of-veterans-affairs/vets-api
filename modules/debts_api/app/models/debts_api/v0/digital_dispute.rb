66df80745f74eb45130cf312eab7fa9bbc457ad2
Penelope Lischer <102491809+penny-lischer@users.noreply.github.com>
2025-02-05 13:00:21 -0800

    #98335 - digital dispute submission mapping

--- modules/debts_api/app/models/debts_api/v0/digital_dispute.rb
+++ modules/debts_api/app/models/debts_api/v0/digital_dispute.rb
@@ -5,12 +5,12 @@ module DebtsApi
     include ActiveModel::Validations
     STATS_KEY = 'api.digital_dispute_submission'
 
-    attribute :contact_information, Hash
-    attribute :debt_information, Array
+    attribute :veteran_information, Hash
+    attribute :selected_debts, Array
     attribute :user_icn, String
 
-    validate :validate_contact_information
-    validate :validate_debt_information
+    validate :validate_veteran_information
+    validate :validate_selected_debts
 
     def initialize(attributes, user)
       super()
@@ -24,24 +24,24 @@ module DebtsApi
 
     private
 
-    def validate_contact_information
-      required_keys = %w[email phone_number address_line1 city]
-      missing_keys = required_keys - contact_information.keys
+    def validate_veteran_information
+      required_keys = %w[email mobile_phone mailing_address]
+      missing_keys = required_keys - veteran_information.keys
       unless missing_keys.empty?
-        errors.add(:contact_information, "is missing required information: #{missing_keys.join(', ')}")
+        errors.add(:veteran_information, "is missing required information: #{missing_keys.join(', ')}")
       end
-
-      if contact_information['email'].present? && contact_information['email'] !~ URI::MailTo::EMAIL_REGEXP
-        errors.add(:contact_information, 'must include a valid email address')
+      binding.pry
+      if veteran_information['email'].present? && veteran_information['email'] !~ URI::MailTo::EMAIL_REGEXP
+        errors.add(:veteran_information, 'must include a valid email address')
       end
     end
 
-    def validate_debt_information
-      debt_information.each_with_index do |debt, index|
-        required_keys = %w[debt dispute_reason support_statement]
-
+    def validate_selected_debts
+      selected_debts.each_with_index do |debt, index|
+        required_keys = %w[debt_type dispute_reason support_statement]
+        binding.pry
         required_keys.each do |key|
-          errors.add(:debt_information, "entry ##{index + 1}: #{key} cannot be blank") if debt[key].blank?
+          errors.add(:selected_debts, "entry ##{index + 1}: #{key} cannot be blank") if debt[key].blank?
         end
       end
     end
