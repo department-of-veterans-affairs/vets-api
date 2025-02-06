126b5fbdb76c7b8a593fa40454ea27e6eaf52fd9
Tommasina Miller <144388524+tommasina-va@users.noreply.github.com>
2025-02-06 13:59:22 -0600

    Remove generate_pdf toggles

--- lib/disability_compensation/factories/api_provider_factory.rb
+++ lib/disability_compensation/factories/api_provider_factory.rb
@@ -14,7 +14,7 @@ require 'disability_compensation/providers/brd/evss_brd_provider'
 require 'disability_compensation/providers/brd/lighthouse_brd_provider'
 require 'disability_compensation/providers/brd/lighthouse_staging_brd_provider'
 require 'disability_compensation/providers/generate_pdf/generate_pdf_provider'
-require 'disability_compensation/providers/generate_pdf/evss_generate_pdf_provider'
+# require 'disability_compensation/providers/generate_pdf/evss_generate_pdf_provider'
 require 'disability_compensation/providers/generate_pdf/lighthouse_generate_pdf_provider'
 require 'disability_compensation/providers/document_upload/lighthouse_supplemental_document_upload_provider'
 require 'disability_compensation/providers/document_upload/evss_supplemental_document_upload_provider'
@@ -47,11 +47,8 @@ class ApiProviderFactory
     'disability_compensation_lighthouse_rated_disabilities_provider_foreground'
   FEATURE_TOGGLE_RATED_DISABILITIES_BACKGROUND =
     'disability_compensation_lighthouse_rated_disabilities_provider_background'
-  # FEATURE_TOGGLE_CLAIMS_SERVICE = 'disability_compensation_lighthouse_claims_service_provider'
-
   # PPIU calls out to Direct Deposit APIs in Lighthouse
   FEATURE_TOGGLE_BRD = 'disability_compensation_lighthouse_brd'
-  FEATURE_TOGGLE_GENERATE_PDF = 'disability_compensation_lighthouse_generate_pdf'
 
   FEATURE_TOGGLE_UPLOAD_BDD_INSTRUCTIONS = 'disability_compensation_upload_bdd_instructions_to_lighthouse'
   FEATURE_TOGGLE_UPLOAD_0781 = 'disability_compensation_upload_0781_to_lighthouse'
@@ -144,8 +141,6 @@ class ApiProviderFactory
 
   def claims_service_provider
     case api_provider
-    when API_PROVIDER[:evss]
-      EvssClaimsServiceProvider.new(@options[:auth_headers])
     when API_PROVIDER[:lighthouse]
       LighthouseClaimsServiceProvider.new(@options[:icn])
     else
@@ -168,14 +163,6 @@ class ApiProviderFactory
 
   def generate_pdf_service_provider
     case api_provider
-    when API_PROVIDER[:evss]
-      if @options[:auth_headers].nil? || @options[:auth_headers]&.empty?
-        raise StandardError, 'options[:auth_headers] is required to create a generate an EVSS pdf provider'
-      end
-
-      # provide options[:breakered] = false if this needs to use the non-breakered configuration
-      # for instance, in the backup process
-      EvssGeneratePdfProvider.new(@options[:auth_headers], breakered: @options[:breakered])
     when API_PROVIDER[:lighthouse]
       LighthouseGeneratePdfProvider.new(@current_user[:icn])
     else
