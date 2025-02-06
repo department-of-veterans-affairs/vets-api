4c874a729838b448816026a8fb1f9bc3f0f839a5
Ian Perera <iputreetek@gmail.com>
2025-02-06 13:51:30 -0500

    fix lint

--- modules/va_notify/lib/va_notify/default_callback.rb
+++ modules/va_notify/lib/va_notify/default_callback.rb
@@ -35,7 +35,7 @@ module VANotify
       when 'delivered'
         delivered(tags) if notification_type == 'error'
       when 'permanent-failure', 'temporary-failure'
-        # 'temporary-failure' is an end state for the notification and it will not be automatically retried by the VANotify API.
+        # 'temporary-failure' is an end state for the notification; VANotify API does not auto-retry these.
         permanent_failure(tags) if notification_type == 'error'
       end
     end
@@ -45,7 +45,7 @@ module VANotify
       when 'delivered'
         delivered_without_metadata
       when 'permanent-failure', 'temporary-failure'
-        # 'temporary-failure' is an end state for the notification and it will not be automatically retried by the VANotify API.
+        # 'temporary-failure' is an end state for the notification; VANotify API does not auto-retry these.
         permanent_failure_without_metadata
       end
     end
