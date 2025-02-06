387544610e08fa9e3c0c230f22dcd2bf3cae5d77
Rachal Cassity <rachal.cassity@oddball.io>
2025-02-06 16:22:33 -0600

    97889 contact info v2 phones (#20661)
    * phone specs
    
    * lint
    
    * service spec
    
    * test
    
    * test
    
    * Fixed mobile phone spec
    
    * removed extra line
    
    * fixed swagger
    
    * fixed swagger
    
    * telephone spec
    
    * phone spec
    
    * fixed telephone spec

--- modules/mobile/spec/requests/mobile/v0/user/phones_spec.rb
+++ modules/mobile/spec/requests/mobile/v0/user/phones_spec.rb
@@ -10,12 +10,11 @@ RSpec.describe 'Mobile::V0::User::Phones', type: :request do
   let(:headers) do
     sis_headers(json: true)
   end
-  let(:telephone) { build(:telephone, :contact_info_v2, source_system_user: user.icn) }
 
   before do
     allow(Flipper).to receive(:enabled?).with(:mobile_v2_contact_info, instance_of(User)).and_return(true)
     allow(Flipper).to receive(:enabled?).with(:va_v3_contact_information_service, instance_of(User)).and_return(true)
-    allow(Flipper).to receive(:enabled?).with(:remove_pciu, instance_of(User)).and_return(false)
+    allow(Flipper).to receive(:enabled?).with(:remove_pciu, instance_of(User)).and_return(true)
     Timecop.freeze(Time.zone.parse('2024-08-27T18:51:06.012Z'))
   end
 
@@ -24,16 +23,14 @@ RSpec.describe 'Mobile::V0::User::Phones', type: :request do
   end
 
   describe 'POST /mobile/v0/user/phones' do
+    let(:telephone) { build(:telephone, :contact_info_v2, id: nil, source_system_user: user.icn) }
+
     context 'with a valid phone number' do
       before do
-        telephone.id = 42
-
-        VCR.use_cassette('mobile/profile/v2/get_phone_status_complete', VCR::MATCH_EVERYTHING) do
-          VCR.use_cassette('mobile/profile/v2/get_phone_status_incomplete', VCR::MATCH_EVERYTHING) do
-            VCR.use_cassette('mobile/profile/v2/get_phone_status_incomplete_2', VCR::MATCH_EVERYTHING) do
-              VCR.use_cassette('mobile/profile/v2/post_phone_initial', VCR::MATCH_EVERYTHING) do
-                post('/mobile/v0/user/phones', params: telephone.to_json, headers:)
-              end
+        VCR.use_cassette('va_profile/v2/contact_information/post_phone_status_complete') do
+          VCR.use_cassette('va_profile/v2/contact_information/post_phone_status_incomplete') do
+            VCR.use_cassette('va_profile/v2/contact_information/post_telephone_success') do
+              post('/mobile/v0/user/phones', params: telephone.to_json, headers:)
             end
           end
         end
@@ -49,7 +46,7 @@ RSpec.describe 'Mobile::V0::User::Phones', type: :request do
 
       it 'includes a transaction id' do
         id = JSON.parse(response.body).dig('data', 'attributes', 'transactionId')
-        expect(id).to eq('c3c6502d-f660-409c-9bc9-a7b7ce4f0bc5')
+        expect(id).to eq('57d5364b-149a-4802-bc18-b9b0b0742db6')
       end
     end
 
@@ -85,14 +82,17 @@ RSpec.describe 'Mobile::V0::User::Phones', type: :request do
   end
 
   describe 'PUT /mobile/v0/user/phones' do
+    let(:telephone) do
+      build(:telephone, :contact_info_v2, source_system_user: user.icn, id: 458_781, phone_number: 5_551_235)
+    end
+
     context 'with a valid phone number' do
       before do
-        telephone.id = 42
-
-        VCR.use_cassette('mobile/profile/v2/get_phone_status_complete', VCR::MATCH_EVERYTHING) do
-          VCR.use_cassette('mobile/profile/v2/get_phone_status_incomplete', VCR::MATCH_EVERYTHING) do
-            VCR.use_cassette('mobile/profile/v2/put_phone_initial', VCR::MATCH_EVERYTHING) do
-              put('/mobile/v0/user/phones', params: telephone.to_json, headers:)
+        VCR.use_cassette('va_profile/v2/contact_information/put_telephone_status_complete', VCR::MATCH_EVERYTHING) do
+          VCR.use_cassette('va_profile/v2/contact_information/put_telephone_status_incomplete',
+                           VCR::MATCH_EVERYTHING) do
+            VCR.use_cassette('va_profile/v2/contact_information/put_telephone_success', VCR::MATCH_EVERYTHING) do
+              put('/mobile/v0/user/phones', params: telephone.to_json, headers: sis_headers(json: true))
             end
           end
         end
@@ -108,7 +108,7 @@ RSpec.describe 'Mobile::V0::User::Phones', type: :request do
 
       it 'includes a transaction id' do
         id = JSON.parse(response.body).dig('data', 'attributes', 'transactionId')
-        expect(id).to eq('c3c6502d-f660-409c-9bc9-a7b7ce4f0bc5')
+        expect(id).to eq('c915d801-5693-4860-b2df-83baa8c3c910')
       end
     end
 
@@ -144,15 +144,15 @@ RSpec.describe 'Mobile::V0::User::Phones', type: :request do
   end
 
   describe 'DELETE /mobile/v0/user/phones v2' do
+    let(:telephone) { build(:telephone, :contact_info_v2, source_system_user: user.icn, id: 42) }
+
     context 'with a valid phone number' do
       before do
-        telephone.id = 42
-        VCR.use_cassette('mobile/profile/v2/get_phone_status_complete', VCR::MATCH_EVERYTHING) do
-          VCR.use_cassette('mobile/profile/v2/get_phone_status_incomplete', VCR::MATCH_EVERYTHING) do
-            VCR.use_cassette('mobile/profile/v2/delete_phone_initial', VCR::MATCH_EVERYTHING) do
-              delete '/mobile/v0/user/phones',
-                     params: telephone.to_json,
-                     headers:
+        VCR.use_cassette('va_profile/v2/contact_information/delete_telephone_status_complete', VCR::MATCH_EVERYTHING) do
+          VCR.use_cassette('va_profile/v2/contact_information/delete_telephone_status_incomplete',
+                           VCR::MATCH_EVERYTHING) do
+            VCR.use_cassette('va_profile/v2/contact_information/delete_telephone_success', VCR::MATCH_EVERYTHING) do
+              delete '/mobile/v0/user/phones', params: telephone.to_json, headers:
             end
           end
         end
@@ -168,7 +168,7 @@ RSpec.describe 'Mobile::V0::User::Phones', type: :request do
 
       it 'includes a transaction id' do
         id = JSON.parse(response.body).dig('data', 'attributes', 'transactionId')
-        expect(id).to eq('c3c6502d-f660-409c-9bc9-a7b7ce4f0bc5')
+        expect(id).to eq('2c5b8109-e709-463c-a7c4-19dadfbbc2ed')
       end
     end
 
