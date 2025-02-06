76b2e884fed6bed5559b12ad55c4e808b80ad7dd
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2025-02-06 16:11:51 -0500

    smaller dataset

--- modules/accredited_representative_portal/lib/tasks/seed/staging_seed.rb
+++ modules/accredited_representative_portal/lib/tasks/seed/staging_seed.rb
@@ -1,6 +1,12 @@
+require_relative 'staging_seed/methods'
+require_relative 'staging_seed/constants'
+
 module AccreditedRepresentativePortal
   module StagingSeeds
     class << self
+      include Methods
+      include Constants
+
       def run
         ActiveRecord::Base.transaction do
           total_created = { requests: 0, resolutions: 0 }
@@ -12,11 +18,27 @@ module AccreditedRepresentativePortal
           resolved_time = RESOLVED_TIME_TRAVELER
           unresolved_time = UNRESOLVED_TIME_TRAVELER
 
-          Veteran::Service::Representative.find_each do |rep|
-            matching_orgs = Veteran::Service::Organization.where(poa: rep.poa_codes)
-            next if matching_orgs.empty?
+          # CT Digital Organization
+          ct_org = Veteran::Service::Organization.find_by(poa: "008")
+          
+          # Other Digital Organizations (limit 2)
+          other_digital_orgs = Veteran::Service::Organization
+            .where(can_accept_digital_poa_requests: true)
+            .where.not(poa: "008")
+            .limit(2)
+          
+          # Non-Digital Organizations (limit 2)
+          non_digital_orgs = Veteran::Service::Organization
+            .where(can_accept_digital_poa_requests: false)
+            .limit(2)
 
-            matching_orgs.each do |org|
+          # Create requests for each org type
+          [ct_org, *other_digital_orgs, *non_digital_orgs].each do |org|
+            matching_reps = Veteran::Service::Representative
+              .where("poa_codes && ARRAY[?]::varchar[]", [org.poa])
+              .limit(2)
+
+            matching_reps.each do |rep|
               create_request_with_resolution(
                 org: org,
                 rep: rep,
@@ -29,146 +51,11 @@ module AccreditedRepresentativePortal
             end
           end
 
-          log_results(total_created)
-        end
-      end
-
-      private
-
-      RESOLUTION_HISTORY_CYCLE = %i[expiration decision].cycle
-
-      RESOLVED_TIME_TRAVELER =
-        Enumerator.new do |yielder|
-          time = 30.days.ago
-          loop do
-            yielder << time
-            time += 6.hours
-          end
-        end
-
-      UNRESOLVED_TIME_TRAVELER =
-        Enumerator.new do |yielder|
-          time = 10.days.ago
-          loop do
-            yielder << time
-            time += 6.hours
-          end
-        end
-
-      def create_claimants(count = 10)
-        Array.new(count) do
-          FactoryBot.create(:user_account)
-        end
-      end
-
-      def create_request_with_resolution(org:, rep:, claimant:, resolution_cycle:, resolved_time:, unresolved_time:, totals:)
-        if rand < 0.5
-          created_at = resolved_time.next
-          request = create_poa_request(org, rep, claimant, created_at)
-          create_resolution(request, resolution_cycle.next)
-          totals[:resolutions] += 1
-        else
-          request = create_poa_request(org, rep, claimant, unresolved_time.next)
-        end
-        
-        totals[:requests] += 1
-      end
-
-      def create_poa_request(org, rep, claimant, created_at)
-        # Create request without form first
-        request = PowerOfAttorneyRequest.new(
-          claimant_id: claimant.id,
-          claimant_type: 'veteran',
-          power_of_attorney_holder_type: 'AccreditedOrganization',
-          power_of_attorney_holder_poa_code: org.poa,
-          accredited_individual_registration_number: rep.representative_id,
-          created_at: created_at
-        )
-
-        # Create form with request
-        form = AccreditedRepresentativePortal::PowerOfAttorneyForm.new(
-          data: build_poa_form_data.to_json,
-          power_of_attorney_request: request
-        )
-
-        # Save both
-        form.save!
-        request.power_of_attorney_form = form
-        request.save!
-        
-        request
-      end
-
-      def create_resolution(request, resolution_type)
-        resolving = case resolution_type
-        when :expiration
-          exp = AccreditedRepresentativePortal::PowerOfAttorneyRequestExpiration.new
-          if exp.save
-            exp
-          end
-        when :decision
-          dec = AccreditedRepresentativePortal::PowerOfAttorneyRequestDecision.new(
-            type: AccreditedRepresentativePortal::PowerOfAttorneyRequestDecision::Types::ACCEPTANCE,
-            creator_id: request.claimant_id
+          Rails.logger.info(
+            "Seeding complete: Created #{total_created[:requests]} requests " \
+            "and #{total_created[:resolutions]} resolutions"
           )
-          if dec.save
-            dec
-          end
-        else
-          raise "Unknown resolution type: #{resolution_type}"
         end
-      
-        if resolving&.persisted?
-          resolution = AccreditedRepresentativePortal::PowerOfAttorneyRequestResolution.create!(
-            power_of_attorney_request: request,
-            resolving: resolving,
-            created_at: request.created_at + 1.day
-          )
-          resolution
-        else
-          raise "Failed to create resolving record"
-        end
-      end
-
-      def build_poa_form_data
-        {
-          authorizations: {
-            record_disclosure: true,
-            record_disclosure_limitations: [],
-            address_change: false
-          },
-          veteran: {
-            name: {
-              first: "Test",
-              middle: nil,
-              last: "Veteran"
-            },
-            address: {
-              address_line1: "123 Test St",
-              address_line2: nil,
-              city: "Testville",
-              state_code: "TS",
-              country: "US",
-              zip_code: "12345",
-              zip_code_suffix: nil
-            },
-            ssn: "123456789",
-            va_file_number: "123456789",
-            date_of_birth: "1980-01-01",
-            service_number: nil,
-            service_branch: "ARMY",
-            phone: "1234567890",
-            email: "test@example.com"
-          },
-          dependent: nil
-        }
-      end
-
-      def log_results(totals)
-        Rails.logger.info(
-          "Seeding complete: Created #{totals[:requests]} requests " \
-          "and #{totals[:resolutions]} resolutions"
-        )
       end
     end
   end
---
+++ modules/accredited_representative_portal/lib/tasks/seed/staging_seed/methods.rb
@@ -0,0 +1,102 @@
+module AccreditedRepresentativePortal
+  module StagingSeeds
+    module Methods
+      def create_claimants(count = 10)
+        Array.new(count) do
+          FactoryBot.create(:user_account)
+        end
+      end
+
+      def create_request_with_resolution(org:, rep:, claimant:, resolution_cycle:, resolved_time:, unresolved_time:, totals:)
+        if rand < 0.5
+          created_at = resolved_time.next
+          request = create_poa_request(org, rep, claimant, created_at)
+          create_resolution(request, resolution_cycle.next)
+          totals[:resolutions] += 1
+        else
+          request = create_poa_request(org, rep, claimant, unresolved_time.next)
+        end
+        
+        totals[:requests] += 1
+      end
+
+      def create_poa_request(org, rep, claimant, created_at)
+        request = PowerOfAttorneyRequest.new(
+          claimant_id: claimant.id,
+          claimant_type: 'veteran',
+          power_of_attorney_holder_type: 'AccreditedOrganization',
+          power_of_attorney_holder_poa_code: org.poa,
+          accredited_individual_registration_number: rep.representative_id,
+          created_at: created_at
+        )
+
+        form = AccreditedRepresentativePortal::PowerOfAttorneyForm.new(
+          data: build_poa_form_data.to_json,
+          power_of_attorney_request: request
+        )
+
+        form.save!
+        request.power_of_attorney_form = form
+        request.save!
+        
+        request
+      end
+
+      def create_resolution(request, resolution_type)
+        resolving = case resolution_type
+        when :expiration
+          exp = AccreditedRepresentativePortal::PowerOfAttorneyRequestExpiration.new
+          exp.save ? exp : nil
+        when :decision
+          dec = AccreditedRepresentativePortal::PowerOfAttorneyRequestDecision.new(
+            type: AccreditedRepresentativePortal::PowerOfAttorneyRequestDecision::Types::ACCEPTANCE,
+            creator_id: request.claimant_id
+          )
+          dec.save ? dec : nil
+        end
+
+        return unless resolving&.persisted?
+
+        AccreditedRepresentativePortal::PowerOfAttorneyRequestResolution.create!(
+          power_of_attorney_request: request,
+          resolving: resolving,
+          created_at: request.created_at + 1.day
+        )
+      end
+
+      def build_poa_form_data
+        {
+          authorizations: {
+            record_disclosure: true,
+            record_disclosure_limitations: [],
+            address_change: false
+          },
+          veteran: {
+            name: {
+              first: "Test",
+              middle: nil,
+              last: "Veteran"
+            },
+            address: {
+              address_line1: "123 Test St",
+              address_line2: nil,
+              city: "Testville",
+              state_code: "TS",
+              country: "US",
+              zip_code: "12345",
+              zip_code_suffix: nil
+            },
+            ssn: "123456789",
+            va_file_number: "123456789",
+            date_of_birth: "1980-01-01",
+            service_number: nil,
+            service_branch: "ARMY",
+            phone: "1234567890",
+            email: "test@example.com"
+          },
+          dependent: nil
+        }
+      end
+    end
+  end
+end
