2dbfd6caa0fd79644bf7bfe1d00d0e80b30f25b4
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-02-06 20:41:23 -0500

    100832 Do not register silent failure for Sharepoint

--- modules/debts_api/app/models/debts_api/v0/form5655_submission.rb
+++ modules/debts_api/app/models/debts_api/v0/form5655_submission.rb
@@ -74,7 +74,10 @@ module DebtsApi
         submission.submitted!
         StatsD.increment("#{STATS_KEY}.vha.success")
       else
-        submission.register_failure("VHA set completed state: #{status.failure_info}")
+        # we are not registering a failure for Sharepoint requests
+        unless status.failure_info.include?('SharepointRequest')
+          submission.register_failure("VHA set completed state: #{status.failure_info}")
+        end
         StatsD.increment("#{STATS_KEY}.vha.failure")
         Rails.logger.error('Batch FSR Processing Failed', status.failure_info)
       end
