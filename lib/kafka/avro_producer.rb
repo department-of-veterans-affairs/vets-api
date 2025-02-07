86ef512df4ed0bf4f717c8abc2c9ad7d69668360
Andrea Singh <info@madebydna.com>
2025-02-07 08:32:59 -0800

    Introduced Avro encoding with schema registry

--- lib/kafka/avro_producer.rb
+++ lib/kafka/avro_producer.rb
@@ -1,16 +1,31 @@
+require 'avro_turf/messaging'
+
 module Kafka
-  module Producer
-    def self.producer
-      @producer = KAFKA_PRODUCER
+  module AvroProducer
+    def self.avro
+      @avro ||= AvroTurf::Messaging.new(registry_url: Settings.kafka_producer.schema_registry_url)
     end
 
-    # Here we will eventually serialize the payload against an Avro schema
-    def self.produce(topic, payload)
-      producer.produce_sync(topic: topic, payload: payload.to_json)
+    def self.producer
+      @producer ||= KAFKA_PRODUCER
+    end
+
+    def self.produce(topic, payload, schema_version: 1)
+      encoded_payload = avro.encode(
+        payload,
+        subject: topic,
+        version: schema_version,
+        validate: true
+      )
+
+      producer.produce_sync(topic: topic, payload: encoded_payload)
     rescue => e
       # https://karafka.io/docs/WaterDrop-Error-Handling/
       # Errors are rescued and re-raised to demonstrate the types of errors that can occur
       case e
+      when Avro::SchemaValidator::ValidationError
+        Rails.logger.error "Schema validation error: #{e}"
+        raise e
       when WaterDrop::Errors::MessageInvalidError
         # This error is raised when the message is invalid and before attempting to send it to Kafka
         Rails.logger.error "Message is invalid: #{e}"
@@ -20,6 +35,8 @@ module Kafka
         Rails.logger.error 'Producer error. See the logs for more information. This dispatch will not reach Kafka'
         raise e
       else
+        puts "+-- Unexpected error: #{e}"
+        p e
         # Any other errors. This should not happen and indicates trouble.
         Rails.logger.error "An unexpected error occurred while producing a message to #{topic}. Please check the logs for more information. This dispatch will not reach Kafka"
         raise e
--- spec/lib/kafka/avro_producer_spec.rb
+++ spec/lib/kafka/avro_producer_spec.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'kafka/producer'
+require 'kafka/avro_producer'
 
-fdescribe Kafka::Producer do
+describe Kafka::AvroProducer do
+  let(:avro_mock) { instance_double(AvroTurf::Messaging) }
+
+  before do
+    # Mock AvroTurf::Messaging.new to return the avro_mock object
+    allow(described_class).to receive(:avro).and_return(avro_mock)
+    # Stub the encode method on the avro_mock object to return the payload as is
+    allow(avro_mock).to receive(:encode) do |payload, _options|
+      payload
+    end
+  end
+
   context 'when the producer is created' do
     it 'returns a WaterDrop producer' do
       expect(described_class.producer).to be_a(WaterDrop::Producer)
@@ -17,15 +28,35 @@ fdescribe Kafka::Producer do
 
   context 'producing a message successfuly' do
     # Using the buffered client for tests - https://karafka.io/docs/WaterDrop-Testing/#buffered-client
+    after do
+      # reset the client after each test
+      described_class.producer.client.reset
+    end
+
     it 'produces a message to the specified topic' do
-      described_class.produce('topic-1', { key: 'value1' })
-      described_class.produce('topic-1', { key: 'value2' })
-      described_class.produce('topic-2', { key: 'value1' })
+      described_class.produce('topic-1', { key: 'value1' }.to_json)
+      described_class.produce('topic-1', { key: 'value2' }.to_json)
+      described_class.produce('topic-2', { key: 'value1' }.to_json)
 
       expect(described_class.producer.client.messages.length).to eq(3)
       topic_1_messages = described_class.producer.client.messages_for('topic-1')
       expect(topic_1_messages.length).to eq(2)
       expect(topic_1_messages[0][:payload]).to eq('{"key":"value1"}')
+    end
+
+    it 'works with Avro encoded payloads' do
+      avro_turf = AvroTurf.new(schemas_path: 'spec/fixtures/avro_schemas')
+      # Use test encoding for AvroTurf
+      allow(avro_mock).to receive(:encode) do |payload, _options|
+        avro_turf.encode(payload, schema_name: 'test')
+      end
+      described_class.produce('topic-1', { data: { key: 'value1' } })
+      described_class.produce('topic-1', { data: { key: 'value2' } })
+      described_class.produce('topic-2', { data: { key: 'value1' } })
+
+      avro_msg = described_class.producer.client.messages_for('topic-1').first[:payload]
+      decoded_avro_msg = avro_turf.decode(avro_msg, schema_name: 'test')
+      expect(decoded_avro_msg).to eq({ 'data' => { 'key' => 'value1' } })
     end
   end
 
@@ -35,7 +66,7 @@ fdescribe Kafka::Producer do
 
       # Send an invalid message to trigger an error (no topic provided)
       expect do
-        described_class.produce(nil, { key: 'value1' })
+        described_class.produce(nil, { key: 'value1' }.to_json)
       end.to raise_error(WaterDrop::Errors::MessageInvalidError,
                          { topic: 'does not match the topic allowed format' }.to_s)
     end
@@ -62,7 +93,7 @@ fdescribe Kafka::Producer do
 
       # Trigger the error and handle it
       expect do
-        described_class.produce('topic-1', { key: 'value1' })
+        described_class.produce('topic-1', { key: 'value1' }.to_json)
       end.to raise_error(StandardError)
     end
 
@@ -75,8 +106,22 @@ fdescribe Kafka::Producer do
 
       # Trigger the error and handle it
       expect do
-        described_class.produce('topic-1', { key: 'value1' })
+        described_class.produce('topic-1', { key: 'value1' }.to_json)
       end.to raise_error(WaterDrop::Errors::ProduceError)
+    end
+
+    it 'logs a message when a SchemaValidationError occurs' do
+      # Use test encoding for AvroTurf to trigger a schema validation error
+      allow(avro_mock).to receive(:encode) do |payload, _options|
+        avro_turf = AvroTurf.new(schemas_path: 'spec/fixtures/avro_schemas')
+        avro_turf.encode(payload, schema_name: 'test', validate: true)
+      end
+      expect(Rails.logger).to receive(:error).with(/Schema validation error/)
+
+      # Trigger the error using an invalid schema
+      expect do
+        described_class.produce('topic-1', { key: 'value1' })
+      end.to raise_error(Avro::SchemaValidator::ValidationError)
     end
   end
 end
