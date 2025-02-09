6e1c2073207e4975b047961e0255b365f68873f4
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2025-02-09 08:53:49 -0500

    intra-file refactoring

--- modules/debts_api/lib/debts_api/v0/fsr_form_transform/income_calculator.rb
+++ modules/debts_api/lib/debts_api/v0/fsr_form_transform/income_calculator.rb
@@ -8,6 +8,13 @@ module DebtsApi
       class IncomeCalculator
         include ::FsrFormTransform::Utils
 
+        # Refactoring Steps
+        # 1. Remove duplicate instance variables related to `get_monthly_income`
+        # 2. Replace `calculate_income` parameters with respective instance variables
+        # 3. Refactor `transformed_income` to be stringify calculate_income output
+        # 4. Extract private methods from calculate_income
+        # 5. Move calculate_income hash to (vet|spouse)_income
+
         BENEFICIARY_MAP = {
           'veteran' => 'VETERAN',
           'spouse' => 'SPOUSE'
@@ -38,49 +45,7 @@ module DebtsApi
           @enhanced_fsr_active = @form['view:enhanced_financial_status_report']
         end
 
-        # rubocop:disable Metrics/MethodLength
         def get_monthly_income
-          sp_addl_income = @form.dig('additional_income', 'spouse', 'sp_addl_income') || []
-          addl_inc_records = @form.dig('additional_income', 'addl_inc_records') || []
-          vet_employment_records = @form.dig(
-            'personal_data',
-            'employment_history',
-            'veteran',
-            'employment_records'
-          ) || []
-          sp_employment_records = @form.dig(
-            'personal_data',
-            'employment_history',
-            'spouse',
-            'sp_employment_records'
-          ) || []
-          social_security = @form['social_security'] || {}
-          benefits = @form['benefits'] || {}
-          curr_employment = @form['curr_employment'] || []
-          sp_curr_employment = @form['sp_curr_employment'] || []
-          income = @form['income'] || []
-          enhanced_fsr_active = @form['view:enhanced_financial_status_report']
-          vet_income = calculate_income(
-            enhanced_fsr_active,
-            'veteran',
-            vet_employment_records,
-            curr_employment,
-            addl_inc_records,
-            social_security,
-            income,
-            benefits
-          )
-          sp_income = calculate_income(
-            enhanced_fsr_active,
-            'spouse',
-            sp_employment_records,
-            sp_curr_employment,
-            sp_addl_income,
-            social_security,
-            income,
-            benefits
-          )
-
           sp_sum = sp_income.empty? ? 0 : sp_income[:totalMonthlyNetIncome]
           total_monthly_net_income = vet_income[:totalMonthlyNetIncome] + sp_sum
 
@@ -92,29 +57,25 @@ module DebtsApi
         end
 
         def vet_income
-          calculate_income(
-            @enhanced_fsr_active,
-            'veteran',
-            @vet_employment_records,
-            @curr_employment,
-            @addl_inc_records,
-            @social_security,
-            @income,
-            @benefits
-          )
+          @vet_income ||= {
+            grossSalary: veteran_gross_salary,
+            deductions: veteran_deductions,
+            totalDeductions: veteran_total_deductions,
+            netTakeHomePay: (veteran_gross_salary - veteran_total_deductions),
+            otherIncome: veteran_other_income,
+            totalMonthlyNetIncome: (veteran_gross_salary - veteran_total_deductions + veteran_other_income[:amount]).round(2)
+          }
         end
 
         def sp_income
-          calculate_income(
-            @enhanced_fsr_active,
-            'spouse',
-            @sp_employment_records,
-            @sp_curr_employment,
-            @sp_addl_income,
-            @social_security,
-            @income,
-            @benefits
-          )
+          @sp_income ||= {
+            grossSalary: spouse_gross_salary,
+            deductions: spouse_deductions,
+            totalDeductions: spouse_total_deductions,
+            netTakeHomePay: (spouse_gross_salary - spouse_total_deductions),
+            otherIncome: spouse_other_income,
+            totalMonthlyNetIncome: (spouse_gross_salary - spouse_total_deductions + spouse_other_income[:amount]).round(2)
+          }
         end
 
         def get_transformed_income
@@ -122,32 +83,159 @@ module DebtsApi
         end
 
         def transformed_vet_income
-          transform_income(
-            @enhanced_fsr_active,
-            'veteran',
-            @vet_employment_records,
-            @curr_employment,
-            @addl_inc_records,
-            @social_security,
-            @income,
-            @benefits
-          )
+          transform_income('veteran', vet_income)
         end
 
         def transformed_sp_income
-          transform_income(
-            @enhanced_fsr_active,
-            'spouse',
-            @sp_employment_records,
-            @sp_curr_employment,
-            @sp_addl_income,
-            @social_security,
-            @income,
-            @benefits
-          )
+          transform_income('spouse', sp_income)
         end
 
         private
+
+        def transform_income(beneficiary_type, income)
+          {
+            'veteranOrSpouse' => BENEFICIARY_MAP[beneficiary_type],
+            'monthlyGrossSalary' => dollars_cents(income[:grossSalary]),
+            'deductions' => {
+              'taxes' => dollars_cents(income[:deductions][:taxes]),
+              'retirement' => dollars_cents(income[:deductions][:retirement]),
+              'socialSecurity' => dollars_cents(income[:deductions][:socialSecurity]),
+              'otherDeductions' => {
+                'name' => income[:deductions][:otherDeductions][:name],
+                'amount' => dollars_cents(income[:deductions][:otherDeductions][:amount].to_f)
+              }
+            },
+            'totalDeductions' => dollars_cents(income[:totalDeductions]),
+            'netTakeHomePay' => dollars_cents(income[:netTakeHomePay]),
+            'otherIncome' => {
+              'name' => income[:otherIncome][:name],
+              'amount' => dollars_cents(income[:otherIncome][:amount])
+            },
+            'totalMonthlyNetIncome' => dollars_cents(income[:totalMonthlyNetIncome])
+          }
+        end
+
+        def veteran_gross_salary
+          gross_salary = if @enhanced_fsr_active
+            @vet_employment_records.map do |emp|
+              if emp['gross_monthly_income'].nil?
+                0
+              else
+                emp['gross_monthly_income'].to_f
+              end
+            end.sum
+          else
+            @curr_employment.sum do |emp|
+              emp["#{beneficiary_type}_gross_salary"].to_f
+            end
+          end
+          gross_salary.to_f.round(2)
+        end
+
+        def spouse_gross_salary
+          gross_salary = if @enhanced_fsr_active
+            @sp_employment_records.map do |emp|
+              if emp['gross_monthly_income'].nil?
+                0
+              else
+                emp['gross_monthly_income'].to_f
+              end
+            end.sum
+          else
+            @sp_curr_employment.sum do |emp|
+              emp["#{beneficiary_type}_gross_salary"].to_f
+            end
+          end
+          gross_salary.to_f.round(2)
+        end
+
+        def veteran_deductions
+          deductions = if @enhanced_fsr_active
+            @vet_employment_records
+              .select { |emp| emp['is_current'] }
+              .map do |emp|
+              if emp['deductions'].nil?
+                0
+              else
+                emp['deductions']
+              end
+            end
+            .flatten
+          else
+            @curr_employment.pluck('deductions').flatten
+          end
+
+          deduction_details(deductions)
+        end
+
+        def spouse_deductions
+          deductions = if @enhanced_fsr_active
+            @sp_employment_records
+              .select { |emp| emp['is_current'] }
+              .map do |emp|
+              if emp['deductions'].nil?
+                0
+              else
+                emp['deductions']
+              end
+            end
+            .flatten
+          else
+            @sp_curr_employment.pluck('deductions').flatten
+          end
+
+          deduction_details(deductions)
+        end
+
+        def deduction_details(deductions)
+          {
+            taxes: filter_reduce_by_name(deductions, TAX_FILTERS),
+            retirement: filter_reduce_by_name(deductions, RETIREMENT_FILTERS),
+            socialSecurity: filter_reduce_by_name(deductions, SOCIAL_SEC_FILTERS),
+            otherDeductions: {
+              name: other_deductions_name(deductions, ALL_FILTERS),
+              amount: other_deductions_amt(deductions, ALL_FILTERS)
+            }
+          }
+        end
+
+        def veteran_total_deductions
+          # taxes + retirement + socialSecurity + other deductions
+          veteran_deductions.except(:otherDeductions).values.sum + veteran_deductions[:otherDeductions][:amount]
+        end
+
+        def spouse_total_deductions
+          # taxes + retirement + socialSecurity + other deductions
+          spouse_deductions.except(:otherDeductions).values.sum + spouse_deductions[:otherDeductions][:amount]
+        end
+
+        def veteran_other_income
+          addl_inc = @addl_inc_records.sum { |record| record['amount'].to_f }
+          soc_sec_amt = @enhanced_fsr_active ? 0 : @social_security['social_sec_amt'].to_f || 0
+          comp = @income.sum { |item| item['compensation_and_pension'].to_f }
+          edu =  @income.sum { |item| item['education'].to_f }
+
+          other_income_total = addl_inc.to_f.round(2) + comp.to_f.round(2) + edu.to_f.round(2) + soc_sec_amt.to_f.round(2)
+
+          {
+            name: name_str(soc_sec_amt, comp, edu, @addl_inc_records),
+            amount: other_income_total.round(2)
+          }
+        end
+
+        def spouse_other_income
+          addl_inc = @sp_addl_income.sum { |record| record['amount'].to_f }
+          soc_sec_amt = @enhanced_fsr_active ? 0 : @social_security.dig('spouse', 'social_sec_amt').to_f || 0
+          comp = @benefits.dig('spouse_benefits', 'compensation_and_pension').to_f || 0
+          edu = @benefits.dig('spouse_benefits', 'education').to_f || 0
+
+          other_income_total = addl_inc.to_f.round(2) + comp.to_f.round(2) + edu.to_f.round(2) + soc_sec_amt.to_f.round(2)
+
+          {
+            name: name_str(soc_sec_amt, comp, edu, @sp_addl_income),
+            amount: other_income_total.round(2)
+          }
+        end
 
         def filter_reduce_by_name(deductions, filters)
           return 0.0 unless deductions&.any?
@@ -186,183 +274,7 @@ module DebtsApi
 
           other_inc_names&.join(', ') || ''
         end
-
-        # rubocop:disable Metrics/ParameterLists
-        def transform_income(enhanced_fsr_active, beneficiary_type, employment_records = [], curr_employment = [],
-                             addl_inc_records = [], social_security = {}, income = [], benefits = {})
-          gross_salary = if enhanced_fsr_active
-                           employment_records.map do |emp|
-                             if emp['gross_monthly_income'].nil?
-                               0
-                             else
-                               emp['gross_monthly_income'].to_f
-                             end
-                           end.sum
-                         else
-                           curr_employment.sum do |emp|
-                             emp["#{beneficiary_type}_gross_salary"].to_f
-                           end
-                         end
-
-          addl_inc = addl_inc_records.sum { |record| record['amount'].to_f }
-
-          soc_sec_amt = if enhanced_fsr_active
-                          0
-                        elsif beneficiary_type == 'spouse'
-                          social_security.dig('spouse', 'social_sec_amt').to_f || 0
-                        else
-                          social_security['social_sec_amt'].to_f || 0
-                        end
-
-          comp = if beneficiary_type == 'spouse'
-                   benefits.dig('spouse_benefits', 'compensation_and_pension').to_f || 0
-                 else
-                   income.sum { |item| item['compensation_and_pension'].to_f }
-                 end
-
-          edu = if beneficiary_type == 'spouse'
-                  benefits.dig('spouse_benefits', 'education').to_f || 0
-                else
-                  income.sum { |item| item['education'].to_f }
-                end
-
-          benefits_amount = comp + edu
-
-          deductions = if enhanced_fsr_active
-                         employment_records
-                           .select { |emp| emp['is_current'] }
-                           .map do |emp|
-                           if emp['deductions'].nil?
-                             0
-                           else
-                             emp['deductions']
-                           end
-                         end
-                         .flatten
-                       else
-                         curr_employment.pluck('deductions').flatten
-                       end
-
-          gross_salary = gross_salary.to_f.round(2)
-          taxes_values = filter_reduce_by_name(deductions, TAX_FILTERS)
-          retirement_values = filter_reduce_by_name(deductions, RETIREMENT_FILTERS)
-          social_sec = filter_reduce_by_name(deductions, SOCIAL_SEC_FILTERS)
-          other = other_deductions_amt(deductions, ALL_FILTERS)
-          tot_deductions = taxes_values + retirement_values + social_sec + other
-          other_income = addl_inc.to_f.round(2) + benefits_amount.to_f.round(2) + soc_sec_amt.to_f.round(2)
-          net_income = gross_salary - tot_deductions
-          total_monthly_net_income = (net_income + other_income)
-
-          {
-            'veteranOrSpouse' => BENEFICIARY_MAP[beneficiary_type],
-            'monthlyGrossSalary' => dollars_cents(gross_salary),
-            'deductions' => {
-              'taxes' => dollars_cents(taxes_values),
-              'retirement' => dollars_cents(retirement_values),
-              'socialSecurity' => dollars_cents(social_sec),
-              'otherDeductions' => {
-                'name' => other_deductions_name(deductions, ALL_FILTERS),
-                'amount' => dollars_cents(other.to_f)
-              }
-            },
-            'totalDeductions' => dollars_cents(tot_deductions),
-            'netTakeHomePay' => dollars_cents(net_income),
-            'otherIncome' => {
-              'name' => name_str(soc_sec_amt, comp, edu, addl_inc_records),
-              'amount' => dollars_cents(other_income.round(2))
-            },
-            'totalMonthlyNetIncome' => dollars_cents(total_monthly_net_income.round(2))
-          }
-        end
-
-        def calculate_income(enhanced_fsr_active, beneficiary_type, employment_records = [], curr_employment = [],
-                             addl_inc_records = [], social_security = {}, income = [], benefits = {})
-          gross_salary = if enhanced_fsr_active
-                           employment_records.map do |emp|
-                             if emp['gross_monthly_income'].nil?
-                               0
-                             else
-                               emp['gross_monthly_income'].to_f
-                             end
-                           end.sum
-                         else
-                           curr_employment.sum do |emp|
-                             emp["#{beneficiary_type}_gross_salary"].to_f
-                           end
-                         end
-
-          addl_inc = addl_inc_records.sum { |record| record['amount'].to_f }
-
-          soc_sec_amt = if enhanced_fsr_active
-                          0
-                        elsif beneficiary_type == 'spouse'
-                          social_security.dig('spouse', 'social_sec_amt').to_f || 0
-                        else
-                          social_security['social_sec_amt'].to_f || 0
-                        end
-
-          comp = if beneficiary_type == 'spouse'
-                   benefits.dig('spouse_benefits', 'compensation_and_pension').to_f || 0
-                 else
-                   income.sum { |item| item['compensation_and_pension'].to_f }
-                 end
-
-          edu = if beneficiary_type == 'spouse'
-                  benefits.dig('spouse_benefits', 'education').to_f
-                else
-                  income.sum { |item| item['education'].to_f }
-                end
-
-          benefits_amount = comp + edu
-
-          deductions = if enhanced_fsr_active
-                         employment_records
-                           .select { |emp| emp['is_current'] }
-                           .map do |emp|
-                           if emp['deductions'].nil?
-                             0
-                           else
-                             emp['deductions']
-                           end
-                         end
-                         .flatten
-                       else
-                         curr_employment.pluck('deductions').flatten
-                       end
-
-          gross_salary = gross_salary.to_f.round(2)
-          taxes_values = filter_reduce_by_name(deductions, TAX_FILTERS)
-          retirement_values = filter_reduce_by_name(deductions, RETIREMENT_FILTERS)
-          social_sec = filter_reduce_by_name(deductions, SOCIAL_SEC_FILTERS)
-          other = other_deductions_amt(deductions, ALL_FILTERS)
-          tot_deductions = taxes_values + retirement_values + social_sec + other
-          other_income = addl_inc.to_f.round(2) + benefits_amount.to_f.round(2) + soc_sec_amt.to_f.round(2)
-          net_income = gross_salary - tot_deductions
-          total_monthly_net_income = net_income + other_income
-
-          {
-            grossSalary: gross_salary,
-            deductions: {
-              taxes: taxes_values,
-              retirement: retirement_values,
-              socialSecurity: social_sec,
-              otherDeductions: {
-                name: other_deductions_name(deductions, ALL_FILTERS),
-                amount: other
-              }
-            },
-            totalDeductions: tot_deductions,
-            netTakeHomePay: net_income,
-            otherIncome: {
-              name: name_str(soc_sec_amt, comp, edu, addl_inc_records),
-              amount: other_income.round(2)
-            },
-            totalMonthlyNetIncome: total_monthly_net_income.round(2)
-          }
-        end
       end
     end
   end
 end
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/ParameterLists
