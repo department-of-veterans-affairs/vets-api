db29f1d8d30d0152cdeed8a96e9e6dd2fc1ea69b
Dan Lim <danlim715@gmail.com>
2025-02-10 12:15:43 -0600

    Migration to add bpds_uuid to saved_claims

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_04_175219) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_10_175014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
@@ -1163,6 +1163,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_04_175219) do
     t.text "metadata"
     t.datetime "metadata_updated_at"
     t.bigint "user_account_id"
+    t.uuid "bpd_uuid"
     t.index ["created_at", "type"], name: "index_saved_claims_on_created_at_and_type"
     t.index ["delete_date"], name: "index_saved_claims_on_delete_date"
     t.index ["guid"], name: "index_saved_claims_on_guid", unique: true
