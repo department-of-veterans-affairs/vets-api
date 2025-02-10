d32cad7be22b08ae29c8fd0c0937f071b2e31bc4
Peri-Ann McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-02-10 12:32:25 -0700

    PR #5 for silent failure work - Add files to create a job to poll the upload status from lighthouse and to create a cron job to send emails for failed uploads (#20637)

--- spec/factories/lighthouse/benefits_documents/evidence_submission.rb
+++ spec/factories/lighthouse/benefits_documents/evidence_submission.rb
@@ -3,11 +3,6 @@
 require 'lighthouse/benefits_documents/constants'
 
 FactoryBot.define do
-  factory :bd_evidence_submission, class: 'EvidenceSubmission' do
-    association :user_account, factory: :user_account
-    created_at { DateTime.now.utc }
-  end
-
   factory :bd_evidence_submission_timeout, class: 'EvidenceSubmission' do
     association :user_account, factory: :user_account
     created_at { DateTime.new(1985, 10, 26).utc }
