bca85ab856d8d698e1836020e4bb6607ae273dc2
Emily Nguyen <emilyn0614@gmail.com>
2025-02-10 13:39:04 -0600

    add serializers and minor changes to useractionevents controller

---
+++ app/serializers/user_action_serializer.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class UserActionSerializer
+  include JSONAPI::Serializer
+
+  attribute :user_action_event, :status, :subject_user_verification, acting_ip_address,
+            :acting_user_agent, :created_at, :updated_at, :acting_user_verification
+
+  belongs_to :user_action_events
+  belongs_to :acting_user_verification
+  belongs_to :subject_user_verification
+end
