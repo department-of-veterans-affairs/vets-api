d45e46934deea4400070e58e3f59930e8d5b3ae9
OJ Bucao <9256675+ojbucao@users.noreply.github.com>
2025-02-11 10:11:21 -0800

    fix linting issues

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -51,7 +51,9 @@ VCR.configure do |c|
   c.filter_sensitive_data('<VRO_URL>') { Settings.virtual_regional_office.url }
   c.filter_sensitive_data('<VRO_API_KEY>') { Settings.virtual_regional_office.api_key }
   c.filter_sensitive_data('<CONTENTION_CLASSIFICATION_API_URL>') { Settings.contention_classification_api.url }
-  c.filter_sensitive_data('<ARP_ALLOW_LIST_ACCESS_TOKEN>') { Settings.accredited_representative_portal.allow_list.github.access_token }
+  c.filter_sensitive_data('<ARP_ALLOW_LIST_ACCESS_TOKEN>') do
+    Settings.accredited_representative_portal.allow_list.github.access_token
+  end
   c.filter_sensitive_data('<ARP_ALLOW_LIST_REPO>') { Settings.accredited_representative_portal.allow_list.github.repo }
   c.filter_sensitive_data('<ARP_ALLOW_LIST_PATH>') { Settings.accredited_representative_portal.allow_list.github.path }
   c.before_record do |i|
