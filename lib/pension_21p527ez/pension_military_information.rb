6923d038c18218ee76fdef8e04ce232c1ebd95f6
Bryan Alexander <bryan.alexander@adhocteam.us>
2025-02-11 17:53:26 -0500

    90092: Put back old logic

--- lib/pension_21p527ez/pension_military_information.rb
+++ lib/pension_21p527ez/pension_military_information.rb
@@ -7,15 +7,14 @@ module Pension21p527ez
   # ##
   # # extends FormMilitaryInformation to add additional military information fields to Pension prefill.
   # # @see app/models/form_profile.rb FormProfile::FormMilitaryInformation
-  # class PensionFormMilitaryInformation < FormProfile::FormMilitaryInformation
-  # DELETE this after pension_form_profile_module_enabled is fully in use
-  #   include Virtus.model
+  class PensionFormMilitaryInformation < FormProfile::FormMilitaryInformation
+    include Virtus.model
 
-  #   attribute :first_uniformed_entry_date, String
-  #   attribute :last_active_discharge_date, String
-  #   attribute :service_branches_for_pensions, Hash
-  #   attribute :service_number, String
-  # end
+    attribute :first_uniformed_entry_date, String
+    attribute :last_active_discharge_date, String
+    attribute :service_branches_for_pensions, Hash
+    attribute :service_number, String
+  end
 
   ##
   # extends MilitaryInformation to add additional prefill methods to Pensions military information prefill
