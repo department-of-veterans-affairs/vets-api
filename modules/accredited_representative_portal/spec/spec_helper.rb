dc103843276b33e316a268a011b8f5ca4deacf2a
OJ Bucao <9256675+ojbucao@users.noreply.github.com>
2025-02-10 16:22:39 -0800

    refactor/correct allow list syncing

--- modules/accredited_representative_portal/spec/spec_helper.rb
+++ modules/accredited_representative_portal/spec/spec_helper.rb
@@ -17,7 +17,40 @@ module Faker
   end
 end
 
+module VcrHelpers
+  VCR_OPTIONS = {
+    match_requests_on: %i[
+      method uri headers
+    ].freeze
+  }.freeze
+
+  def use_cassette(name, options = {}, &)
+    options.with_defaults!(
+      **VCR_OPTIONS,
+      use_spec_name_prefix: true
+    )
+
+    options.delete(:use_spec_name_prefix) and
+      name = spec_name_prefix / name
+
+    VCR.use_cassette(name, options, &)
+  end
+
+  def spec_name_prefix
+    caller.each do |call|
+      call = call.split(':').first
+      next unless call.end_with?('_spec.rb')
+
+      call.delete_prefix!((AccreditedRepresentativePortal::Engine.root / 'spec/').to_s)
+      call.delete_suffix!('.rb')
+      return Pathname('accredited_representative_portal') / call
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.filter_run :focus
+
+  config.include VcrHelpers
 end
