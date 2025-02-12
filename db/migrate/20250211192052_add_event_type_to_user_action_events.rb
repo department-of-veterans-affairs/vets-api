b639e4a05cf1916d18fc033f1312f30eccf93bd0
Alex Jolovic <alexander.jolovic@bylight.com>
2025-02-12 13:28:41 -0800

    add event_id and event_type columns to user_action_events

--- db/migrate/20250211192052_add_event_type_to_user_action_events.rb
+++ db/migrate/20250211192052_add_event_type_to_user_action_events.rb
@@ -2,12 +2,7 @@
 
 class AddEventTypeToUserActionEvents < ActiveRecord::Migration[7.2]
   def change
-    safety_assured do
-      add_column :user_action_events, :event_id, :string, null: false
-      add_column :user_action_events, :event_type, :integer, null: false
-
-      add_index :user_action_events, :event_id, unique: true
-      add_index :user_action_events, :event_type
-    end
+    add_column :user_action_events, :event_id, :string
+    add_column :user_action_events, :event_type, :integer
   end
 end
