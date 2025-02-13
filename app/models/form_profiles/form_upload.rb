5f36bfd930015c268304338cd72f547e29a3cd51
Jacob Penner <japenner@gmail.com>
2025-02-13 09:22:30 -0500

    streamline the form upload mapping schemas

--- app/models/form_profiles/form_upload.rb
+++ app/models/form_profiles/form_upload.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class FormProfiles::FormUpload < FormProfile
+  def self.load_form_mapping(_form_id)
+    file = Rails.root.join('config', 'form_profile_mappings', 'FORM-UPLOAD.yml')
+
+    YAML.load_file(file)
+  end
+
   def metadata
     {
       version: 0,
