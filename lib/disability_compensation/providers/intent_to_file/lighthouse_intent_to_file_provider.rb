4ad22cd7ae1226537a46325cca6b2868518ac710
aurora-a-k-a-lightning <aurora.hampton@agile6.com>
2025-02-14 10:36:41 -0800

    dbex/101195: fix intent to file lighthouse provider submit response

--- lib/disability_compensation/providers/intent_to_file/lighthouse_intent_to_file_provider.rb
+++ lib/disability_compensation/providers/intent_to_file/lighthouse_intent_to_file_provider.rb
@@ -20,24 +20,32 @@ class LighthouseIntentToFileProvider
   def create_intent_to_file(type, lighthouse_client_id, lighthouse_rsa_key_path)
     data = @service.create_intent_to_file(type, @current_user.ssn, lighthouse_client_id,
                                           lighthouse_rsa_key_path)['data']
-    transform(data)
+    transform_create(data)
   end
 
   private
 
   def transform(data)
     DisabilityCompensation::ApiProvider::IntentToFilesResponse.new(
-      intent_to_file: [
-        DisabilityCompensation::ApiProvider::IntentToFile.new(
-          id: data['id'],
-          creation_date: data['attributes']['creationDate'],
-          expiration_date: data['attributes']['expirationDate'],
-          source: '',
-          participant_id: 0,
-          status: data['attributes']['status'],
-          type: data['attributes']['type']
-        )
-      ]
+      intent_to_file: [intent_to_file_response(data)]
+    )
+  end
+
+  def transform_create(data)
+    DisabilityCompensation::ApiProvider::IntentToFileResponse.new(
+      intent_to_file: intent_to_file_response(data)
+    )
+  end
+
+  def intent_to_file_response(data)
+    DisabilityCompensation::ApiProvider::IntentToFile.new(
+      id: data['id'],
+      creation_date: data['attributes']['creationDate'],
+      expiration_date: data['attributes']['expirationDate'],
+      source: '',
+      participant_id: 0,
+      status: data['attributes']['status'],
+      type: data['attributes']['type']
     )
   end
 end
