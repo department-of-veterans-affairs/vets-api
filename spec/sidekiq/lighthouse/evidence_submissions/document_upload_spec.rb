99a864e5a3c4879bd0b2d604a61b26f16332c230
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-02-14 13:50:13 -0700

    fixed linting errors

--- spec/sidekiq/lighthouse/evidence_submissions/document_upload_spec.rb
+++ spec/sidekiq/lighthouse/evidence_submissions/document_upload_spec.rb
@@ -158,7 +158,8 @@ RSpec.describe Lighthouse::EvidenceSubmissions::DocumentUpload, type: :job do
         evidence_submission = EvidenceSubmission.find_by(job_id: job_id)
         current_personalisation = JSON.parse(evidence_submission.template_metadata)['personalisation']
         expect(evidence_submission.upload_status).to eql(BenefitsDocuments::Constants::UPLOAD_STATUS[:FAILED])
-        expect(evidence_submission.error_message).to eql('Lighthouse::EvidenceSubmissions::DocumentUpload document upload failure')
+        expect(evidence_submission.error_message)
+          .to eql('Lighthouse::EvidenceSubmissions::DocumentUpload document upload failure')
         expect(current_personalisation['date_failed']).to eql(failed_date)
 
         Timecop.freeze(current_date_time) do
