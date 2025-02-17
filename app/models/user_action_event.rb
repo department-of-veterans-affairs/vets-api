5482b9c5ef2070c9695bc5ae34f04e6fcb8091eb
John Bramley <bramleyjl@gmail.com>
2025-02-17 14:59:40 -0700

    removes business logic changes

--- app/models/user_action_event.rb
+++ app/models/user_action_event.rb
@@ -4,6 +4,4 @@ class UserActionEvent < ApplicationRecord
   has_many :user_actions, dependent: :restrict_with_exception
 
   validates :details, presence: true
-  validates :event_id, presence: true
-  validates :event_type, presence: true
 end
--- spec/factories/user_action_events.rb
+++ spec/factories/user_action_events.rb
@@ -3,7 +3,5 @@
 FactoryBot.define do
   factory :user_action_event do
     details { Faker::Lorem.sentence }
-    event_id { 'test_event' }
-    event_type { 1 }
   end
 end
