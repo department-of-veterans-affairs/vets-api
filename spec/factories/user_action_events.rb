ef15a37768e1434618e7f7c070d8ae43770bee7a
Alex Jolovic <alexander.jolovic@bylight.com>
2025-02-18 10:13:53 -0800

    test: update user_action_event factory and specs with new columns
    - Add event_id and event_type to factory
    - Add factory traits for authentication and profile events
    - Update specs to verify new columns

--- spec/factories/user_action_events.rb
+++ spec/factories/user_action_events.rb
@@ -3,5 +3,17 @@
 FactoryBot.define do
   factory :user_action_event do
     details { Faker::Lorem.sentence }
+    event_id { "user_login_#{SecureRandom.hex(4)}" }
+    event_type { 0 }
+
+    trait :authentication do
+      event_type { 0 }
+      event_id { "auth_#{SecureRandom.hex(4)}" }
+    end
+
+    trait :profile do
+      event_type { 1 }
+      event_id { "profile_#{SecureRandom.hex(4)}" }
+    end
   end
 end
