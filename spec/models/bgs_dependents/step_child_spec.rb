86148d12e2351f813109a4c856d3ff713ffd1aa0
Evan Smith <evan.smith@oddball.io>
2025-02-17 19:39:17 -0500

    fix stepchild tests

--- spec/models/bgs_dependents/step_child_spec.rb
+++ spec/models/bgs_dependents/step_child_spec.rb
@@ -20,6 +20,23 @@ RSpec.describe BGSDependents::StepChild do
       'full_name' => { 'first' => 'Billy', 'middle' => 'Yohan', 'last' => 'Johnson', 'suffix' => 'Sr.' }
     }
   end
+  let(:stepchild_info_v2) do
+    {
+      'supporting_stepchild' => true,
+      'living_expenses_paid' => 'Half',
+      'ssn' => '213685794',
+      'birth_date' => '2010-03-03',
+      'who_does_the_stepchild_live_with' => { 'first' => 'Adam', 'middle' => 'Steven', 'last' => 'Huberws' },
+      'address' => {
+        'country' => 'USA',
+        'street' => '412 Crooks Road',
+        'city' => 'Clawson',
+        'state' => 'AL',
+        'postal_code' => '48017'
+      },
+      'full_name' => { 'first' => 'Billy', 'middle' => 'Yohan', 'last' => 'Johnson', 'suffix' => 'Sr.' }
+    }
+  end
   let(:formatted_params_result) do
     {
       'living_expenses_paid' => '.5',
@@ -46,7 +63,7 @@ RSpec.describe BGSDependents::StepChild do
   context 'with va_dependents_v2 off' do
     describe '#format_info' do
       it 'formats stepchild params for submission' do
-        formatted_info = described_class.new(stepchild_info).format_info
+        formatted_info = described_class.new(stepchild_info_v2).format_info
 
         expect(formatted_info).to eq(formatted_params_result)
       end
