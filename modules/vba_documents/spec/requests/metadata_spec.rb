b7700cae19722a1d3be00efc79e6a5fac7345e18
Kristen Brown <11942904+kristen-brown@users.noreply.github.com>
2025-02-18 17:09:56 -0500

    API-44969: Uncomment out spec

--- modules/vba_documents/spec/requests/metadata_spec.rb
+++ modules/vba_documents/spec/requests/metadata_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 require 'vba_documents/health_checker'
 
 RSpec.describe 'VBADocument::V1::Metadata', type: :request do
-  # describe '#get /metadata' do
-  #   it 'returns metadata JSON' do
-  #     get '/services/vba_documents/metadata'
-  #     expect(response).to have_http_status(:ok)
-  #   end
-  # end
+  describe '#get /metadata' do
+    it 'returns metadata JSON' do
+      get '/services/vba_documents/metadata'
+      expect(response).to have_http_status(:ok)
+    end
+  end
 
   describe '#upstream_healthcheck' do
     let(:path) { '/services/vba_documents/v1/upstream_healthcheck' }
