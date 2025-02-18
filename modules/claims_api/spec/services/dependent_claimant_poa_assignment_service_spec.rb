12a0598bc10bc5559f49684abaa81fe197cbd080
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2025-02-18 10:48:44 -0700

    The require statements in the test were preventing the uninitialized error to be reproduced in the tests. I removed those in the tests, added the missing require in the service, and added more tests.

--- modules/claims_api/spec/services/dependent_claimant_poa_assignment_service_spec.rb
+++ modules/claims_api/spec/services/dependent_claimant_poa_assignment_service_spec.rb
@@ -2,9 +2,6 @@
 
 require 'rails_helper'
 require 'bgs_service/local_bgs'
-require 'bgs_service/benefit_claim_web_service'
-require 'bgs_service/benefit_claim_service'
-require 'bgs_service/e_benefits_bnft_claim_status_web_service'
 
 Rspec.describe ClaimsApi::DependentClaimantPoaAssignmentService do
   describe '#assign_poa_to_dependent!' do
@@ -134,27 +131,34 @@ Rspec.describe ClaimsApi::DependentClaimantPoaAssignmentService do
       end
     end
 
-    describe '#dependent_claims' do
-      it 'creates the service' do
-        VCR.use_cassette('claims_api/bgs/e_benefits_bnft_claim_status_web/find_benefit_claims_status_by_ptcpnt_id') do
-          allow(service).to receive(:assign_poa_to_dependent_via_update_benefit_claim?).and_call_original
-          # allow_any_instance_of(ClaimsApi::EbenefitsBnftClaimStatusWebService).to receive(
-          #   :find_benefit_claims_status_by_ptcpnt_id
-          # )
-            # .with(dependent_participant_id).and_return(mock_find_benefit_claims_status_by_ptcpnt_id)
-          allow_any_instance_of(ClaimsApi::BenefitClaimWebService).to receive(:find_bnft_claim)
-            .with(claim_id: '256009').and_return(mock_find_bnft_claim)
-          allow_any_instance_of(ClaimsApi::BenefitClaimService).to receive(:update_benefit_claim)
-            .and_return(mock_update_benefit_claim)
+    describe '#bgs_claim_status_service' do
+      it 'requires the service statement' do
+        # allow(service).to receive(:dependent_claims).and_call_original
+        res = service.send(:bgs_claim_status_service)
+        expect(res).to be_a(ClaimsApi::EbenefitsBnftClaimStatusWebService)
+      end
+    end
 
-          expect do
-            service.assign_poa_to_dependent!
-          end.not_to raise_error
+    describe '#benefit_claim_web_service' do
+      it 'requires the service statement' do
+        # allow(service).to receive(:claim_details).with('1234446').and_call_original
+        res = service.send(:benefit_claim_web_service)
+        expect(res).to be_a(ClaimsApi::BenefitClaimWebService)
+      end
+    end
 
-          # expect(res[0][:appeal_possible]).to eq 'No'
-          # expect(res[0][:attention_needed]).to eq 'No'
-          # expect(res[0][:benefit_claim_id]).to eq '600537706'
-        end
+    describe '#benefit_claim_service' do
+      it 'requires the service statement' do
+        # allow(service).to receive(:assign_poa_to_dependent!).and_call_original
+        res = service.send(:benefit_claim_service)
+        expect(res).to be_a(ClaimsApi::BenefitClaimService)
+      end
+    end
+
+    describe '#person_web_service' do
+      it 'requires the service statement' do
+        res = service.send(:person_web_service)
+        expect(res).to be_a(ClaimsApi::PersonWebService)
       end
     end
   end
