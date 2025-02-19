5f777e107a73aec9de9d0e9cc2b6067983159aa4
Alex Jolovic <alexander.jolovic@bylight.com>
2025-02-19 14:24:39 -0800

    feat(db): add constraints to user_action_events
    - Add NOT NULL constraints to event_id and event_type
    - Add unique index on event_id
    - Add index on event_type

---
+++ db/migrate/20250219221956_add_constraints_to_user_action_event.rb
@@ -0,0 +1,11 @@
+class AddConstraintsToUserActionEvent < ActiveRecord::Migration[7.2]
+  def change
+    safety_assured do
+      add_index :user_action_events, :event_id, unique: true
+      add_index :user_action_events, :event_type
+      
+      change_column_null :user_action_events, :event_id, false
+      change_column_null :user_action_events, :event_type, false
+    end
+  end
+end
