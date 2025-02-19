d03ce27e62695978a2454511789b02612abcfa96
dfong-adh <151783381+dfong-adh@users.noreply.github.com>
2025-02-19 08:58:23 -0800

    97999: Remove old DR controllers (#20561)
    * 97999: Remove old DR controller endpoints
    
    * Remove DR contestable issues controllers
    
    * Remove v1 decision review evidence upload test
    
    * Fix CODEOWNERS
    
    * Fix rebase issue
    
    * Fix codeowners
    
    * Remove old routes

--- config/routes.rb
+++ config/routes.rb
@@ -419,31 +419,11 @@ Rails.application.routes.draw do
       end
     end
 
-    resource :decision_review_evidence, only: :create
-
-    namespace :higher_level_reviews do
-      get 'contestable_issues(/:benefit_type)', to: 'contestable_issues#index'
-    end
-
-    namespace :notice_of_disagreements do
-      get 'contestable_issues', to: 'contestable_issues#index'
-    end
-    resources :notice_of_disagreements, only: %i[create show]
-
     resource :post911_gi_bill_status, only: [:show]
-
-    namespace :supplemental_claims do
-      get 'contestable_issues(/:benefit_type)', to: 'contestable_issues#index'
-    end
-    resources :supplemental_claims, only: %i[create show]
 
     scope format: false do
       resources :nod_callbacks, only: [:create], controller: :decision_review_notification_callbacks
     end
-  end
-
-  namespace :v2, defaults: { format: 'json' } do
-    resources :higher_level_reviews, only: %i[create show]
   end
 
   root 'v0/example#index', module: 'v0'
