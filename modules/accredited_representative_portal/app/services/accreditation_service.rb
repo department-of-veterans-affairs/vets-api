be6bcbb019710ac982239e9a4c571fb346b4155c
OJ Bucao <9256675+ojbucao@users.noreply.github.com>
2025-02-18 21:15:44 -0800

    add logging and monitoring to ARP allow_list_sync_job and accreditation_service

--- modules/accredited_representative_portal/app/services/accreditation_service.rb
+++ modules/accredited_representative_portal/app/services/accreditation_service.rb
@@ -5,26 +5,59 @@
 # The service URL is determined based on the current environment (development, test, production).
 
 class AccreditationService
+  SERVICE_NAME = 'accredited-representative-portal'
+
   # self.submit_form21a(parsed_body): Submits the given parsed body as JSON to the accreditation service.
   #   - Parameters:
   #     - parsed_body: A Hash representing the parsed form data.
   #     - user_uuid: A String representing the user's UUID, which is also stored in the in_progress_forms DB entry.
   #   - Returns: A Faraday::Response object containing the service response.
+  # rubocop:disable Metrics/MethodLength
   def self.submit_form21a(parsed_body, user_uuid)
-    Rails.logger.info("Accreditation Service attempting submit_form21a with service_url: #{service_url}")
-    connection.post do |req|
+    monitor = AccreditedRepresentativePortal::MonitoringService.new(SERVICE_NAME)
+
+    monitor.track_event(
+      :info,
+      'Submitting Form 21a',
+      'api.arp.form21a.submit',
+      ["user_uuid:#{user_uuid}"]
+    )
+
+    response = connection.post do |req|
       req.headers['x-api-key'] = Settings.ogc.form21a_service_url.api_key
       req.body = parsed_body.to_json
     end
-  rescue Faraday::ConnectionFailed => e
-    Rails.logger.error(
-      "Accreditation Service connection failed for user with user_uuid=#{user_uuid}: #{e.message}, URL: #{service_url}"
+
+    monitor.track_event(
+      :info,
+      'Form 21a Submission Success',
+      'api.arp.form21a.success',
+      ["user_uuid:#{user_uuid}"]
     )
+
+    response
+  rescue Faraday::ConnectionFailed => e
+    monitor.track_error(
+      'Accreditation Service Connection Failed',
+      'api.arp.form21a.connection_failed',
+      e.class.name,
+      ["user_uuid:#{user_uuid}",
+       "error:#{e.message}"]
+    )
+
     Faraday::Response.new(status: :service_unavailable, body: { errors: 'Accreditation Service unavailable' }.to_json)
   rescue Faraday::TimeoutError => e
-    Rails.logger.error("Accreditation Service request timed out for user with user_uuid=#{user_uuid}: #{e.message}")
+    monitor.track_error(
+      'Accreditation Service Timeout',
+      'api.arp.form21a.timeout',
+      e.class.name,
+      ["user_uuid:#{user_uuid}",
+       "error:#{e.message}"]
+    )
+
     Faraday::Response.new(status: :request_timeout, body: { errors: 'Accreditation Service request timed out' }.to_json)
   end
+  # rubocop:enable Metrics/MethodLength
 
   # self.connection: Creates and returns a Faraday connection configured with JSON request and response handling.
   def self.connection
