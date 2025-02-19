b4461b321c2bdc0b91f8a18cef0883400c531784
Rockwell Windsor Rice <129893414+rockwellwindsor-va@users.noreply.github.com>
2025-02-19 11:01:18 -0600

    Api 44280 country code translation (#20596)
    * API-44280-country-code-translation
    
    * Adds country code list to BRD
    	modified:   modules/claims_api/lib/brd/brd.rb
    
    * API-44280-country-code-translation
    
    * Updates Schema to accept countryCode in lace of country
    * Updates related JSON files
    * Updates related tests
    * Compiles Dev Docs to reflect the change (this endpoint is not in production yet)
    	modified:   modules/claims_api/app/controllers/claims_api/v2/blueprints/power_of_attorney_request_blueprint.rb
    	modified:   modules/claims_api/app/swagger/claims_api/v2/dev/swagger.json
    	modified:   modules/claims_api/config/schemas/v2/poa_request.json
    	modified:   modules/claims_api/spec/controllers/v2/veterans/power_of_attorney/request_controller_spec.rb
    	modified:   modules/claims_api/spec/fixtures/v2/veterans/power_of_attorney/request_representative/invalid_claimant_schema.json
    	modified:   modules/claims_api/spec/fixtures/v2/veterans/power_of_attorney/request_representative/invalid_poa.json
    	modified:   modules/claims_api/spec/fixtures/v2/veterans/power_of_attorney/request_representative/invalid_schema.json
    	modified:   modules/claims_api/spec/fixtures/v2/veterans/power_of_attorney/request_representative/valid.json
    	modified:   modules/claims_api/spec/fixtures/v2/veterans/power_of_attorney/request_representative/valid_no_claimant.json
    	modified:   modules/claims_api/spec/requests/v2/veterans/power_of_attorney/power_of_attorney_request_spec.rb
    	modified:   spec/support/schemas/claims_api/v2/veterans/power_of_attorney/request_representative/submit.json
    
    * API-44280-country-code-translation
    
    * Adds validation check for create method
    * Adds tests for scenario
    * Updates countries list
    * Adds countryCode handling in
        * `vnp_ptcpnt_addrs_create` in create_request.rb
        * validation method in request_controller.rb
    	modified:   modules/claims_api/app/controllers/claims_api/v2/veterans/power_of_attorney/request_controller.rb
    	modified:   modules/claims_api/app/services/claims_api/power_of_attorney_request_service/create_request.rb
    	modified:   modules/claims_api/lib/brd/brd.rb
    	modified:   modules/claims_api/spec/controllers/v2/veterans/power_of_attorney/request_controller_spec.rb
    	modified:   modules/claims_api/spec/requests/v2/veterans/power_of_attorney/power_of_attorney_request_spec.rb
    
    * Removes comment
    
    * Fixes comments in BTD list, merges in master branch and fixes conflicts
    
    * Adds regex for character limit on countryCode, fixes missed example text
    
    * Reverts unnecessary v1 and production swagger changes
    
    * Revert prod swagger take 2
    
    * Revert prod swagger take 3
    
    * API-44280-country-code-translation
    
    * upates REGEX to allow for numbers and dashes
    * Adds test for character patterns
      * character types
      * upper/lowercase
    * Updates swagger docs for DEV
    * Adds some normalization by uppercasing when checking the BRD list
    	modified:   modules/claims_api/app/controllers/claims_api/v2/veterans/power_of_attorney/request_controller.rb
    	modified:   modules/claims_api/app/services/claims_api/power_of_attorney_request_service/create_request.rb
    	modified:   modules/claims_api/app/swagger/claims_api/v2/dev/swagger.json
    	modified:   modules/claims_api/config/schemas/v2/poa_request.json
    	modified:   modules/claims_api/spec/controllers/v2/veterans/power_of_attorney/request_controller_spec.rb
    	modified:   spec/support/schemas/claims_api/v2/veterans/power_of_attorney/request_representative/submit.json
    
    * API-44280-country-code-translation
    
    * Fixes REGEX for lowercase acceptance
    	modified:   modules/claims_api/app/swagger/claims_api/v2/dev/swagger.json
    	modified:   modules/claims_api/config/schemas/v2/poa_request.json
    	modified:   spec/support/schemas/claims_api/v2/veterans/power_of_attorney/request_representative/submit.json
    
    * Reverts Gemfile.lock changes
    
    * Revert production swagger change

--- modules/claims_api/spec/requests/v2/veterans/power_of_attorney/power_of_attorney_request_spec.rb
+++ modules/claims_api/spec/requests/v2/veterans/power_of_attorney/power_of_attorney_request_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../../../rails_helper'
 require 'token_validation/v2/client'
 require 'bgs_service/local_bgs'
 
-RSpec.describe 'ClaimsApi::V1::PowerOfAttorney::PowerOfAttorneyRequest', type: :request do
+RSpec.describe 'ClaimsApi::V2::PowerOfAttorney::PowerOfAttorneyRequest', type: :request do
   let(:veteran_id) { '1013062086V794840' }
   let(:request_path) { "/services/claims/v2/veterans/#{veteran_id}/power-of-attorney-request" }
   let(:scopes) { %w[system/claim.write system/claim.read] }
@@ -25,6 +25,8 @@ RSpec.describe 'ClaimsApi::V1::PowerOfAttorney::PowerOfAttorneyRequest', type: :
         context 'when the Veteran ICN is not found in MPI' do
           it 'returns a meaningful 404' do
             mock_ccg(scopes) do |auth_header|
+              allow_any_instance_of(ClaimsApi::V2::Veterans::PowerOfAttorney::RequestController)
+                .to receive(:validate_country_code).and_return(nil)
               allow_any_instance_of(ClaimsApi::Veteran).to receive(:mpi_record?).and_return(false)
 
               detail = "Unable to locate Veteran's ID/ICN in Master Person Index (MPI). " \
@@ -193,7 +195,7 @@ RSpec.describe 'ClaimsApi::V1::PowerOfAttorney::PowerOfAttorneyRequest', type: :
                   'addressLine2' => nil,
                   'city' => nil,
                   'stateCode' => nil,
-                  'country' => nil,
+                  'countryCode' => nil,
                   'zipCode' => nil,
                   'zipCodeSuffix' => nil
                 },
@@ -252,7 +254,7 @@ RSpec.describe 'ClaimsApi::V1::PowerOfAttorney::PowerOfAttorneyRequest', type: :
                   'addressLine2' => nil,
                   'city' => nil,
                   'stateCode' => nil,
-                  'country' => nil,
+                  'countryCode' => nil,
                   'zipCode' => nil,
                   'zipCodeSuffix' => nil
                 },
