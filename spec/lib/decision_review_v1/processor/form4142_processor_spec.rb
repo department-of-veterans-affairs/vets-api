ceab947f6b75df748f9d03b91ff14f304ab8a160
Sam Raudabaugh <1930139+raudabaugh@users.noreply.github.com>
2025-02-20 09:10:39 -0500

    Add JSON Schema validation to form 4142

--- spec/lib/decision_review_v1/processor/form4142_processor_spec.rb
+++ spec/lib/decision_review_v1/processor/form4142_processor_spec.rb
@@ -21,21 +21,96 @@ describe DecisionReviewV1::Processor::Form4142Processor do
            submitted_claim_id: 1)
   end
   let(:processor) { described_class.new(form_data: submission.form['form4142'], submission_id: submission.id) }
-  let(:received_date) { submission.created_at.in_time_zone('Central Time (US & Canada)').strftime('%Y-%m-%d %H:%M:%S') }
+  let(:received_date) do
+    submission.created_at.in_time_zone('Central Time (US & Canada)').strftime('%Y-%m-%d %H:%M:%S')
+  end
   let(:form4142) { JSON.parse(form_json)['form4142'].merge({ 'signatureDate' => received_date }) }
 
   describe '#initialize' do
-    it 'initializes with submission and jid' do
-      expect(PdfFill::Filler).to receive(:fill_ancillary_form)
-        .and_call_original
-        .once
-        .with(form4142, anything, '21-4142')
-      # Note on the expectation: #anything is a special keyword that matches any argument.
-      # We used it here since the uuid is created at runtime.
+    context 'with valid form data' do
+      it 'initializes with submission and jid' do
+        expect(PdfFill::Filler).to receive(:fill_ancillary_form)
+          .and_call_original
+          .once
+          .with(form4142, anything, '21-4142')
+        # Note on the expectation: #anything is a special keyword that matches any argument.
+        # We used it here since the uuid is created at runtime.
 
-      expect(processor.instance_variable_get(:@submission)).to eq(submission)
-      expect(processor.instance_variable_get(:@pdf_path)).to be_a(String)
-      expect(processor.instance_variable_get(:@request_body)).to be_a(Hash)
+        expect(processor.instance_variable_get(:@submission)).to eq(submission)
+        expect(processor.instance_variable_get(:@pdf_path)).to be_a(String)
+        expect(processor.instance_variable_get(:@request_body)).to be_a(Hash)
+      end
+    end
+
+    context 'with invalid form data' do
+      context 'when a required field is missing' do
+        let(:invalid_form_data) { form4142.except('providerFacility') }
+
+        it 'raises a validation error' do
+          expect { described_class.new(form_data: invalid_form_data, submission_id: submission.id) }
+            .to raise_error(RuntimeError, /Form 4142 validation failed/)
+        end
+      end
+
+      context 'with invalid provider data' do
+        context 'when more than 5 providers are submitted' do
+          let(:invalid_form_data) do
+            form4142.tap do |data|
+              # Add providers to exceed the 5 provider limit
+              extra_providers = data['providerFacility'].first.dup
+              data['providerFacility'] += [extra_providers] * 5
+            end
+          end
+
+          it 'raises a validation error' do
+            expect { described_class.new(form_data: invalid_form_data, submission_id: submission.id) }
+              .to raise_error(RuntimeError, /Form 4142 validation failed/)
+          end
+        end
+
+        context 'when dates are malformed' do
+          let(:invalid_form_data) do
+            form4142.tap do |data|
+              data['providerFacility'].first['treatmentDateRange'].first['from'] = 'not-a-date'
+            end
+          end
+
+          it 'raises a validation error' do
+            expect { described_class.new(form_data: invalid_form_data, submission_id: submission.id) }
+              .to raise_error(RuntimeError, /Form 4142 validation failed/)
+          end
+        end
+      end
+
+      context 'when required provider fields are missing' do
+        %w[providerFacilityName providerFacilityAddress treatmentDateRange].each do |field|
+          context "when #{field} is missing" do
+            let(:invalid_form_data) do
+              form4142.tap do |data|
+                data['providerFacility'].first.delete(field)
+              end
+            end
+
+            it 'raises a validation error' do
+              expect { described_class.new(form_data: invalid_form_data, submission_id: submission.id) }
+                .to raise_error(RuntimeError, /Form 4142 validation failed/)
+            end
+          end
+        end
+      end
+
+      context 'when provider state code is invalid' do
+        let(:invalid_form_data) do
+          form4142.tap do |data|
+            data['providerFacility'].first['providerFacilityAddress']['state'] = 'NotAState'
+          end
+        end
+
+        it 'raises a validation error' do
+          expect { described_class.new(form_data: invalid_form_data, submission_id: submission.id) }
+            .to raise_error(RuntimeError, /Form 4142 validation failed/)
+        end
+      end
     end
   end
 
