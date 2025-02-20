1e07b758983652bc3429b250c56b2bb6776b0f2c
Riley Anderson <riley.anderson@oddball.io>
2025-02-20 13:55:18 -0700

    create UserInfo endpoint (#20863)

---
+++ app/controllers/sign_in/user_info_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module SignIn
+  class UserInfoController < ApplicationController
+    service_tag 'identity'
+
+    def show
+      authorize access_token, policy_class: SignIn::UserInfoPolicy
+
+      render json: user_info_json, status: :ok
+    end
+
+    private
+
+    def user_info_json
+      {
+        credential_uuid: current_user.uuid,
+        icn: current_user.icn,
+        sec_id: current_user.sec_id,
+        first_name: current_user.first_name,
+        last_name: current_user.last_name,
+        email:
+      }
+    end
+
+    def email
+      user_verification&.user_credential_email&.credential_email
+    end
+
+    def user_verification
+      current_user.user_verification
+    end
+  end
+end
