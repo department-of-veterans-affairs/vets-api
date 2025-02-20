600c565a2c421b4d8052306aed782623fb920785
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-02-20 12:22:44 -0500

    [66349] rename rx_tooltip_controller to tooltip_controller to make it more generic for each mhv team to use. Added update and create methods.

---
+++ modules/my_health/app/controllers/my_health/v1/tooltips_controller.rb
@@ -0,0 +1,92 @@
+module MyHealth
+  module V1
+    class TooltipsController < ApplicationController
+      before_action :set_user_account, only: [:index, :create, :update]
+      before_action :set_tooltip, only: [:update]
+
+      def index
+        tooltips = @user_account.tooltips
+        render json: tooltips
+      end
+
+      def create
+        begin
+          tooltip = @user_account.tooltips.build(tooltip_params)
+          tooltip.last_signed_in = current_user.last_signed_in
+          tooltip.save!
+          render json: tooltip, status: :created
+        rescue ActiveRecord::RecordInvalid => e
+          render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+        rescue StandardError => e
+          log_and_render_error(e, "Error creating tooltip")
+        end
+      end
+
+      def update
+        # Check if at least one parameter is present
+        unless params[:hidden].present? || params[:increment_counter].present?
+          render json: { error: "At least one of 'hidden' or 'increment_counter' must be provided." }, status: :unprocessable_entity
+          return
+        end
+
+        if @tooltip.update(tooltip_params)
+          if params[:increment_counter] == 'true'
+            # Check for session uniqueness before incrementing
+            if @tooltip.last_signed_in != current_user.last_signed_in
+              @tooltip.counter += 1
+              @tooltip.last_signed_in = current_user.last_signed_in
+              @tooltip.hidden = true if @tooltip.counter >= 3
+              @tooltip.save
+            end
+          end
+          render json: @tooltip
+        else
+          render json: { errors: @tooltip.errors.full_messages }, status: :unprocessable_entity
+        end
+      rescue ActiveRecord::RecordNotFound => e
+        log_and_render_error(e, "Tooltip not found")
+      rescue ActiveRecord::RecordInvalid => e
+        log_and_render_error(e, "Invalid tooltip data")
+      rescue StandardError => e
+        log_and_render_error(e, "Error updating tooltip")
+      end
+
+      private
+
+      def log_and_render_error(exception, message)
+        Rails.logger.error("#{message}: #{exception.message}\n#{exception.backtrace.join("\n")}")
+        render json: { error: message }, status: :internal_server_error
+      end
+
+      def set_tooltip
+        @tooltip = @user_account.tooltips.find_by(id: params[:id])
+        render json: { error: 'Tooltip not found' }, status: :not_found unless @tooltip
+      end
+
+      def tooltip_params
+        params.require(:tooltip).permit(:tooltip_name, :hidden)
+      end
+
+      def increment_counter_if_new_session(tooltip)
+        if tooltip.last_signed_in != current_user.last_signed_in
+          tooltip.counter += 1
+          tooltip.last_signed_in = current_user.last_signed_in
+          tooltip.hidden = true if tooltip.counter >= 3
+          tooltip.save
+        end
+      end
+
+      def set_user_account
+        @user_account = current_user.user_account
+        if @user_account.nil?
+          render json: { error: 'User account not found' }, status: :not_found
+        end
+      end
+
+      def log_and_render_error(exception, message)
+        Rails.logger.error("#{message}: #{exception.message}\n#{exception.backtrace.join("\n")}")
+        render json: { error: message }, status: :internal_server_error
+      end
+    end
+  end
+end
