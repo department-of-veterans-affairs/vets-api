198b491732f7645106b805b255aeab99a2e387a8
Corey Ferris <cferris320@gmail.com>
2025-02-20 11:33:58 -0500

    eps logging config hotfix (#20860)
    * eps logging config fix
    
    ---------
    
    Co-authored-by: Lee Delarm <135059743+lee-delarm6@users.noreply.github.com>

--- modules/vaos/app/services/eps/configuration.rb
+++ modules/vaos/app/services/eps/configuration.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require_relative '../vaos/middleware/response/errors'
+require_relative './middleware/eps_logging'
+require 'common/client/configuration/rest'
+
 module Eps
   class Configuration < Common::Client::Configuration::REST
     delegate :access_token_url, :api_url, :base_path, :grant_type, :scopes, :client_assertion_type, to: :settings
