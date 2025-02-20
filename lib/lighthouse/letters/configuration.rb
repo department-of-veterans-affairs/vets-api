e1af2fb2d29edc6bb8be2c2086913e64b298e663
rjohnson2011 <ryan.johnson@oddball.io>
2025-02-20 11:47:35 -0700

    Extend lighthouse configs for letters, PCIU, PCIU address

--- lib/lighthouse/letters/configuration.rb
+++ lib/lighthouse/letters/configuration.rb
@@ -5,31 +5,31 @@ require 'lighthouse/configuration'
 module Lighthouse
   module Letters
     ##
-    # HTTP client configuration for the {EVSS::Letters::Service},
+    # HTTP client configuration for the {Lighthouse::Letters::Service},
     # sets the base path and a service name for breakers and metrics.
     #
     class Configuration < Lighthouse::Configuration
-      self.read_timeout = Settings.evss.letters.timeout || 55
+      self.read_timeout = Settings.lighthouse.letters.timeout || 55
 
       ##
       # @return [String] Base path for letters URLs.
       #
       def base_path
-        "#{Settings.evss.letters.url}/wss-lettergenerator-services-web/rest/letters/v1"
+        "#{Settings.lighthouse.letters.url}/wss-lettergenerator-services-web/rest/letters/v1"
       end
 
       ##
       # @return [String] Service name to use in breakers and metrics.
       #
       def service_name
-        'EVSS/Letters'
+        'Lighthouse/Letters'
       end
 
       ##
       # @return [Boolean] Should the service use mock data in lower environments.
       #
       def mock_enabled?
-        Settings.evss.mock_letters || false
+        Settings.lighthouse.mock_letters || false
       end
     end
   end
--- lib/lighthouse/pciu/configuration.rb
+++ lib/lighthouse/pciu/configuration.rb
@@ -5,31 +5,31 @@ require 'lighthouse/configuration'
 module Lighthouse
   module PCIU
     ##
-    # HTTP client configuration for the {EVSS::PCIU::Service},
+    # HTTP client configuration for the {Lighthouse::PCIU::Service},
     # sets the base path and a service name for breakers and metrics.
     #
     class Configuration < Lighthouse::Configuration
-      self.read_timeout = Settings.evss.pciu.timeout || 30
+      self.read_timeout = Settings.lighthouse.pciu.timeout || 30
 
       ##
       # @return [String] Base path for PCIU URLs.
       #
       def base_path
-        "#{Settings.evss.url}/wss-pciu-services-web/rest/pciuServices/v1"
+        "#{Settings.lighthouse.url}/wss-pciu-services-web/rest/pciuServices/v1"
       end
 
       ##
       # @return [String] Service name to use in breakers and metrics.
       #
       def service_name
-        'EVSS/PCIU'
+        'Lighthouse/PCIU'
       end
 
       ##
       # @return [Boolean] Should the service use mock data in lower environments.
       #
       def mock_enabled?
-        Settings.evss.mock_pciu || false
+        Settings.lighthouse.mock_pciu || false
       end
     end
   end
