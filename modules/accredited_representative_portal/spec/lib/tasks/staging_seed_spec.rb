dd6e8ea009ffb3e5fde818b67b5960959aebab75
Gregory Hedrick <gregory@pixiitech.net>
2025-02-20 10:08:42 -0500

    Fix tests

--- modules/accredited_representative_portal/spec/lib/tasks/staging_seed_spec.rb
+++ modules/accredited_representative_portal/spec/lib/tasks/staging_seed_spec.rb
@@ -65,9 +65,9 @@ RSpec.describe AccreditedRepresentativePortal::StagingSeeds do
       expect(ct_requests).to(be_all { |req| req.accredited_organization == ct_digital_org })
     end
 
-    it 'creates both resolved and unresolved POA requests for matching organizations' do
-      expect(AccreditedRepresentativePortal::PowerOfAttorneyRequest.resolved).to exist
-      expect(AccreditedRepresentativePortal::PowerOfAttorneyRequest.unresolved).to exist
+    it 'creates both resolved and unresolved requests' do
+      expect(AccreditedRepresentativePortal::PowerOfAttorneyRequest.processed).to exist
+      expect(AccreditedRepresentativePortal::PowerOfAttorneyRequest.not_processed).to exist
     end
 
     it 'creates requests with proper claimant data' do
