762314d13d2006004201cdcb4bf389cf07358c65
Jeff Marks <jeff.t.marks@gmail.com>
2025-02-21 13:59:39 -0700

    lcpe redis spec and factory

---
+++ spec/factories/gi_lcpe_response.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'gi/lcpe/response'
+
+FactoryBot.define do
+  factory :gi_lcpe_response, class: 'GI::LCPE::Response' do
+    transient do
+      v_fresh { '3' }
+      v_stale { '2' }
+      version { v_fresh }
+      lac { { enriched_id: "1@#{version}",
+              lac_nm: 'Gas Fitter',
+              edu_lac_type_nm: 'License',
+              state: 'AR' } }
+    end
+
+    body { { lacs: [lac], version: } }
+    status { 200 }
+
+    trait :stale do
+      version { v_stale }
+    end
+  end
+end
--- spec/models/lcpe_redis_spec.rb
+++ spec/models/lcpe_redis_spec.rb
@@ -7,12 +7,14 @@ describe LCPERedis do
   subject { LCPERedis.new(lcpe_type:) }
 
   let(:lcpe_type) { 'lacs' }
-  let(:raw_response) { generate_raw_response(status) }
-  let(:lcpe_response) { GI::LCPE::Response.from(raw_response) }
-  let(:cached_response) { GI::LCPE::Response.from(generate_raw_response(200, v_cache)) }
+  let(:fresh_response) { build(:gi_lcpe_response) }
+  let(:stale_response) { build(:gi_lcpe_response, :stale) }
+  let(:v_fresh) { fresh_response.version }
+  let(:raw_response) do
+    double('FaradayResponse', body: {}, status:, response_headers:, success?: success )
+  end
+  let(:response_headers) { { 'Etag' => v_fresh } }
   let(:status) { 200 }
-  let(:v_fresh) { '3' }
-  let(:v_stale) { '2' }
   let(:success) { true }
   
   describe '.initialize' do
@@ -21,16 +23,19 @@ describe LCPERedis do
     end
   end
 
+  def load_cache(response)
+    LCPERedis.new.cache(lcpe_type, response)
+  end
+
   describe '#fresh_version_from' do
     context 'when GIDS response not modified' do
-      let(:v_cache) { v_fresh }
       let(:status) { 304 }
 
       it 'returns cached response' do
-        load_cache
+        load_cache(fresh_response)
         # stub Common::RedisStore.find because it instantiates new response object
-        allow(subject).to receive(:cached_response).and_return(cached_response)
-        expect(subject.fresh_version_from(raw_response)).to eq(cached_response)
+        allow(subject).to receive(:cached_response).and_return(fresh_response)
+        expect(subject.fresh_version_from(raw_response)).to eq(fresh_response)
       end
     end
 
@@ -40,48 +45,45 @@ describe LCPERedis do
       end
 
       context 'when status 200' do
+        let(:lcpe_response) { fresh_response }
+
         it 'invalidates cache and caches new response' do
           expect(described_class).to receive(:delete).with(lcpe_type)
-          expect(lcpe_response).to receive(:cache?).and_return(true)
+          expect(fresh_response).to receive(:cache?).and_return(true)
           expect(subject.fresh_version_from(raw_response)).to eq(lcpe_response)
         end
       end
 
       context 'when status unsuccessful ' do
+        let(:lcpe_response) { build(:gi_lcpe_response, status:) }
         let(:status) { 500 }
         let(:success) { false }
   
         it 'invalidates cache and caches new response' do
           expect(described_class).not_to receive(:delete)
           expect(lcpe_response).to receive(:cache?).and_return(false)
-          subject.fresh_version_from(raw_response)
+          expect(subject.fresh_version_from(raw_response)).to eq(lcpe_response)
         end
       end
     end
   end
 
   describe '#force_client_refresh_and_cache' do
-    before do
-      load_cache
-      allow(subject).to receive(:cached_response).and_return(cached_response)
-    end
-
     context 'when redis cache stale' do
-      let(:v_cache) { v_stale }
-
       it 'caches gids response and raises error' do
-        allow(GI::LCPE::Response).to receive(:from).with(raw_response).and_return(lcpe_response)
-        expect(subject).to receive(:cache).with(lcpe_type, lcpe_response)
+        load_cache(stale_response)
+        allow(subject).to receive(:cached_response).and_return(stale_response)
+        allow(GI::LCPE::Response).to receive(:from).with(raw_response).and_return(fresh_response)
+        expect(subject).to receive(:cache).with(lcpe_type, fresh_response)
         expect { subject.force_client_refresh_and_cache(raw_response) }
           .to raise_error(LCPERedis::ClientCacheStaleError)
       end
     end
 
     context 'when redis cache fresh' do
-      let(:v_cache) { v_fresh }
-
       it 'caches gids response and raises error' do
-        expect(subject).not_to receive(:cache).with(lcpe_type, lcpe_response)
+        load_cache(fresh_response)
+        expect(subject).not_to receive(:cache).with(lcpe_type, fresh_response)
         expect { subject.force_client_refresh_and_cache(raw_response) }
           .to raise_error(LCPERedis::ClientCacheStaleError)
       end
@@ -96,10 +98,8 @@ describe LCPERedis do
     end
 
     context 'when cache present' do
-      let(:v_cache) { v_fresh }
-
       it 'returns cached response' do
-        load_cache
+        load_cache(fresh_response)
         expect(subject.cached_response).to be_a GI::LCPE::Response
       end
     end
@@ -113,21 +113,10 @@ describe LCPERedis do
     end
 
     context 'when cache present' do
-      let(:v_cache) { v_fresh }
-
       it 'returns cached version' do
-        load_cache
-        expect(subject.cached_version).to eq(v_cache)
+        load_cache(fresh_response)
+        expect(subject.cached_version).to eq(fresh_response.version)
       end
     end
-  end
-
-  def generate_raw_response(status, version = v_fresh)
-    response_headers = { 'Etag' => version }
-    double('FaradayResponse', body: {}, status:, response_headers:, success?: success )
-  end
-
-  def load_cache
-    LCPERedis.new.cache(lcpe_type, cached_response)
   end
 end
