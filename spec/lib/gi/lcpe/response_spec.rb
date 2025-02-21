2ffeea425d7725ba5c36557c7180b74dd7823862
Jeff Marks <jeff.t.marks@gmail.com>
2025-02-21 14:11:21 -0700

    lcpe response spec

---
+++ spec/lib/gi/lcpe/response_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe GI::LCPE::Response do
+  let(:raw_response) { double('FaradayResponse', body: {}, status:, response_headers:) }
+  let(:status) { 200 }
+  let(:response_headers) {{ 'Etag' => v_fresh }}
+  let(:v_fresh) { '3' }
+
+  describe '.from' do
+    it 'generates response object' do
+      expect(described_class.from(raw_response)).to be_a described_class
+    end
+
+    it 'grabs version from response header and inserts into response body' do
+      response = described_class.from(raw_response)
+      expect(response.version).to eq(v_fresh)
+    end
+  end
+end
