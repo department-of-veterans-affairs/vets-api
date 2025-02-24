3f6b7e58b7655540e33956d1188aa5a233bd74b9
John Bramley <bramleyjl@gmail.com>
2025-02-24 15:47:18 -0700

    simplifies user_action_event_creator invocation

--- config/initializers/user_action_events.rb
+++ config/initializers/user_action_events.rb
@@ -3,8 +3,8 @@
 user_action_events_yaml = YAML.load_file(Rails.root.join('config', 'user_action_events.yml'))
 
 Rails.application.config.after_initialize do
-  user_action_events_yaml.each do |event_name, event_config|
-    UserActionEventCreator.new(event_name:, event_config:).perform
+  user_action_events_yaml.each do |identifier, event_config|
+    UserActionEventCreator.new(identifier:, event_config:).perform
   end
 rescue => e
   Rails.logger.error("Error loading user action event: #{e.message}")
