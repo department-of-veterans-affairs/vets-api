92ce81d5ba9f5e51ded88f36ed73980a5c0ec36b
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-02-23 23:03:26 -0500

    Add updates

--- modules/debts_api/app/controllers/debts_api/v0/one_debt_letters_controller.rb
+++ modules/debts_api/app/controllers/debts_api/v0/one_debt_letters_controller.rb
@@ -6,32 +6,16 @@ module DebtsApi
       service_tag 'debt-resolution'
 
       def download_pdf
-        veteran = mocked_veteran
-
         service = DebtsApi::V0::OneDebtLetterService.new(current_user)
         file_contents = service.get_pdf
 
-        send_data file_contents, filename: client_file_name, type: 'application/pdf', disposition: 'attachment'
-      ensure
-        File.delete(source_file_path) if source_file_path && File.exist?(source_file_path)
+        send_data file_contents, filename: file_name_for_pdf, type: 'application/pdf', disposition: 'attachment'
       end
 
       private
 
       def file_name_for_pdf
-        "#{current_user.first_name}_#{current_user.last_name}_debt_letter.pdf"
-      end
-
-      def mocked_veteran
-        {
-          first_name_last_name: 'Travis Jones',
-          file_number: '123456789', # TODO: get filenumber
-          address: {
-            address_line_1: '375 Mountainhigh Dr',
-            address_line_2: nil,
-            city_state_zip: 'Antioch TN 37013-5322'
-          }
-        }
+        "#{current_user.first_name}_#{current_user.last_name}_#{Time.now.strftime('%Y%m%d_%H%M%S')}_debt_letter.pdf"
       end
     end
   end
