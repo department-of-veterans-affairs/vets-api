fad9903c0b7a49ef9a3373ac293cfbfa002a987b
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-02-23 23:03:26 -0500

    Add updates

--- modules/debts_api/lib/debts_api/v0/one_debt_letter_service.rb
+++ modules/debts_api/lib/debts_api/v0/one_debt_letter_service.rb
@@ -5,19 +5,27 @@ module DebtsApi
     end
 
     def get_pdf
-      copays = vbs_service.get_copays
-      debts = dmc_service.get_debts
+      # copays = vbs_service.get_copays
+      # debts = dmc_service.get_debts
 
-      Prawn::Document.new(page_size: 'LETTER').render
-
+      Prawn::Document.new(page_size: 'LETTER') do |pdf|
+        add_and_format_logo(pdf)
+      end.render
     end
 
-    def vbs_service
-      MedicalCopays::VBS::Service.build(user: current_user)
+    private
+
+    def add_and_format_logo(pdf)
+      logo_path = Rails.root.join('modules', 'debts_api', 'app', 'assets', 'images', 'va_logo.png')
+      pdf.image logo_path, at: [pdf.bounds.width / 2 - (250 / 2), pdf.cursor], width: 250
     end
 
-    def dmc_service
-      DebtManagementCenter::DebtsService.new(current_user)
-    end
+    # def vbs_service
+    #   MedicalCopays::VBS::Service.build(user: current_user)
+    # end
+    #
+    # def dmc_service
+    #   DebtManagementCenter::DebtsService.new(current_user)
+    # end
   end
 end
