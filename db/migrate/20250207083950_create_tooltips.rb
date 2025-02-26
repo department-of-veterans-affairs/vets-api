a2392cb80f79dcac2e2491d4ffb91d99c4600823
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-02-26 16:13:56 -0500

    [MHV 66349] explicitly specify user_accounts table in tooltips migration foreign key.

--- db/migrate/20250207083950_create_tooltips.rb
+++ db/migrate/20250207083950_create_tooltips.rb
@@ -1,7 +1,7 @@
 class CreateTooltips < ActiveRecord::Migration[7.2]
   def change
     create_table :tooltips, id: :uuid do |t|
-      t.references :user_account, null: false, foreign_key: true, type: :uuid
+      t.references :user_account, null: false, foreign_key: { to_table: :user_accounts }, type: :uuid
       t.string :tooltip_name, null: false
       t.datetime :last_signed_in, null: false
       t.integer :counter, default: 0
