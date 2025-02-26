9a084ab31e9b58e6c65a8df6de41e500d1d1ac57
John Bramley <bramleyjl@gmail.com>
2025-02-26 11:16:22 -0700

    moves population logic to UserActionEvent modele

--- app/models/user_action_event.rb
+++ app/models/user_action_event.rb
@@ -6,4 +6,21 @@ class UserActionEvent < ApplicationRecord
   validates :details, presence: true
   validates :identifier, presence: true, uniqueness: true
   validates :event_type, presence: true
+
+  def self.setup
+    config_file_path = Rails.root.join('config', 'user_action_events.yml')
+    unless File.exist?(config_file_path)
+      Rails.logger.info('[UserActionEvent::Setup] Config file not found; skipping database population.')
+      return
+    end
+    user_action_events_yaml = YAML.load_file(config_file_path)
+
+    user_action_events_yaml.each do |identifier, event_config|
+      event = UserActionEvent.find_or_initialize_by(identifier:)
+      event.attributes = event_config
+      event.save!
+    end
+  rescue => e
+    Rails.logger.error("[UserActionEvent::Setup] Error loading user action event: #{e.message}")
+  end
 end
