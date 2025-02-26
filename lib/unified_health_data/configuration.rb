878b88ba6f52c098b783ebe9af824d5234b3c2e7
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-02-26 12:25:18 -0700

    Update lib/unified_health_data/configuration.rb

--- lib/unified_health_data/configuration.rb
+++ lib/unified_health_data/configuration.rb
@@ -38,7 +38,7 @@ module UnifiedHealthData
         # conn.response(:logger, ::Logger.new($stdout), bodies: true) unless Rails.env.production?
 
         conn.response :betamocks if settings.mock
-        conn.response :raise_custom_error
+        conn.response :raise_custom_error,  error_prefix: 'UHD'
         conn.adapter Faraday.default_adapter
       end
     end
