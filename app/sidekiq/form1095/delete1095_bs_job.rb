0d805b208e92a49109bac62af7220b2b9120e754
Kris Pethtel <kris.pethtel@gmail.com>
2025-02-27 10:31:01 -0600

    change file names

--- app/sidekiq/form1095/delete1095_bs_job.rb
+++ app/sidekiq/form1095/delete1095_bs_job.rb
@@ -5,9 +5,7 @@ module Form1095
     include Sidekiq::Job
 
     def perform
-      Form1095B.where('tax_year < ?', Form1095B.current_tax_year).in_batches do |batch|
-        batch.delete_all
-      end
+      Form1095B.where('tax_year < ?', Form1095B.current_tax_year).in_batches(&:delete_all)
     end
   end
 end
