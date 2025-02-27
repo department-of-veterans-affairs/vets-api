abcf9a9f00457bbbb794ff472ed3d6008ae1316c
Alex Morgun <87077843+oleksii-morgun@users.noreply.github.com>
2025-02-27 13:22:24 -0500

    helper mixin

--- modules/my_health/spec/requests/my_health/v1/messaging/sm_spec_helper.rb
+++ modules/my_health/spec/requests/my_health/v1/messaging/sm_spec_helper.rb
@@ -7,12 +7,12 @@ RSpec.configure do |config|
   config.before(:suite) do
     # Ensure that Flipper is disabled globally for all tests
     # Flipper.disable(:mhv_secure_messaging_cerner_pilot)
-    allow(Flipper).to receive(:enabled?).with(: mhv_secure_messaging_cerner_pilot, instance_of(User)).and_return(false)
+    allow(Flipper).to receive(:enabled?).with(:mhv_secure_messaging_cerner_pilot, instance_of(User)).and_return(false)
   end
 
   config.after do
     # Ensure that Flipper is disabled globally for all tests
     # Flipper.disable(:mhv_secure_messaging_cerner_pilot)
-    allow(Flipper).to receive(:enabled?).with(: mhv_secure_messaging_cerner_pilot, instance_of(User)).and_return(false)
+    allow(Flipper).to receive(:enabled?).with(:mhv_secure_messaging_cerner_pilot, instance_of(User)).and_return(false)
   end
 end
