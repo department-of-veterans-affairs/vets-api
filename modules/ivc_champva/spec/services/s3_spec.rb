ba02776532cef2e6e366dfe354c3968459c64518
broskj <170366505+brostk@users.noreply.github.com>
2025-02-28 13:42:33 -0500

    remove commented code

--- modules/ivc_champva/spec/services/s3_spec.rb
+++ modules/ivc_champva/spec/services/s3_spec.rb
@@ -60,7 +60,6 @@ describe IvcChampva::S3 do
         allow(Flipper).to receive(:enabled?)
           .with(:champva_log_all_s3_uploads, @current_user)
           .and_return(true)
-        # Flipper.enable(:champva_log_all_s3_uploads)
       end
 
       it 'returns failure response with status code and body' do
@@ -84,7 +83,6 @@ describe IvcChampva::S3 do
         allow(Flipper).to receive(:enabled?)
           .with(:champva_log_all_s3_uploads, @current_user)
           .and_return(true)
-        # Flipper.enable(:champva_log_all_s3_uploads)
       end
 
       it 'returns failure response with exception message' do
@@ -112,7 +110,6 @@ describe IvcChampva::S3 do
         allow(Flipper).to receive(:enabled?)
           .with(:champva_log_all_s3_uploads, @current_user)
           .and_return(true)
-        # Flipper.enable(:champva_log_all_s3_uploads)
       end
 
       it 'returns failure response with status code' do
