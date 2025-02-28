4e662ff8b33591033f350d7f5ca25d6bee340e29
Josh Fike <josh.fike@oddball.io>
2025-02-28 17:09:43 -0600

    Remove unused traits

--- modules/accredited_representative_portal/spec/factories/power_of_attorney_request_notification.rb
+++ modules/accredited_representative_portal/spec/factories/power_of_attorney_request_notification.rb
@@ -6,21 +6,5 @@ FactoryBot.define do
     association :power_of_attorney_request, factory: :power_of_attorney_request
     association :va_notify_notification, factory: :notification
     notification_type { AccreditedRepresentativePortal::PowerOfAttorneyRequestNotification::PERMITTED_TYPES.sample }
-
-    trait :requested do
-      notification_type { 'requested_poa' }
-    end
-
-    trait :declined do
-      notification_type { 'declined_poa' }
-    end
-
-    trait :expiring do
-      notification_type { 'expiring_poa' }
-    end
-
-    trait :expired do
-      notification_type { 'expired_poa' }
-    end
   end
 end
