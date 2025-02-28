85b863342b8b211f4e1132d6e426dbb7ede637da
Wayne Weibel <wayne.weibel@adhocteam.us>
2025-02-28 11:09:50 -0500

    tests

--- modules/burials/spec/lib/burials/monitor_spec.rb
+++ modules/burials/spec/lib/burials/monitor_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Burials::Monitor do
   let(:monitor) { described_class.new }
-  let(:claim) { create(:burial_claim) }
+  let(:claim) { create(:burials_saved_claim) }
   let(:ipf) { create(:in_progress_form) }
   let(:claim_stats_key) { described_class::CLAIM_STATS_KEY }
   let(:submission_stats_key) { described_class::SUBMISSION_STATS_KEY }
@@ -98,28 +98,6 @@ RSpec.describe Burials::Monitor do
       end
     end
 
-    describe '#track_process_attachment_error' do
-      it 'logs process attachment failed' do
-        log = '21P-530EZ process attachment error'
-        payload = {
-          confirmation_number: claim.confirmation_number,
-          user_account_uuid: current_user.user_account_uuid,
-          in_progress_form_id: ipf.id,
-          errors: [],
-          tags: monitor.tags
-        }
-
-        expect(monitor).to receive(:track_request).with(
-          'error',
-          log,
-          "#{claim_stats_key}.process_attachment_error",
-          call_location: anything,
-          **payload
-        )
-        monitor.track_process_attachment_error(ipf, claim, current_user)
-      end
-    end
-
     describe '#track_create_error' do
       it 'logs sidekiq failed' do
         log = '21P-530EZ submission to Sidekiq failed'
@@ -162,6 +140,128 @@ RSpec.describe Burials::Monitor do
           **payload
         )
         monitor.track_create_success(ipf, claim, current_user)
+      end
+    end
+
+    describe '#track_process_attachment_error' do
+      it 'logs process attachment failed' do
+        log = '21P-530EZ process attachment error'
+        payload = {
+          confirmation_number: claim.confirmation_number,
+          user_account_uuid: current_user.user_account_uuid,
+          in_progress_form_id: ipf.id,
+          errors: [],
+          tags: monitor.tags
+        }
+
+        expect(monitor).to receive(:track_request).with(
+          'error',
+          log,
+          "#{claim_stats_key}.process_attachment_error",
+          call_location: anything,
+          **payload
+        )
+        monitor.track_process_attachment_error(ipf, claim, current_user)
+      end
+    end
+
+    describe '#track_submission_begun' do
+      it 'logs sidekiq job started' do
+        log = 'Burial 21P-530EZ submission to LH begun'
+        payload = {
+          claim_id: claim.id,
+          benefits_intake_uuid: lh_service.uuid,
+          confirmation_number: claim.confirmation_number,
+          user_account_uuid: current_user.uuid,
+          tags: monitor.tags
+        }
+
+        expect(monitor).to receive(:track_request).with(
+          'info',
+          log,
+          "#{submission_stats_key}.begun",
+          call_location: anything,
+          **payload
+        )
+
+        monitor.track_submission_begun(claim, lh_service, current_user.uuid)
+      end
+    end
+
+    describe '#track_submission_attempted' do
+      it 'logs sidekiq job upload attempt' do
+        upload = {
+          file: 'pdf-file-path',
+          attachments: %w[pdf-attachment1 pdf-attachment2]
+        }
+
+        log = 'Burial 21P-530EZ submission to LH attempted'
+        payload = {
+          claim_id: claim.id,
+          benefits_intake_uuid: lh_service.uuid,
+          confirmation_number: claim.confirmation_number,
+          user_account_uuid: current_user.uuid,
+          file: upload[:file],
+          attachments: upload[:attachments],
+          tags: monitor.tags
+        }
+
+        expect(monitor).to receive(:track_request).with(
+          'info',
+          log,
+          "#{submission_stats_key}.attempt",
+          call_location: anything,
+          **payload
+        )
+
+        monitor.track_submission_attempted(claim, lh_service, current_user.uuid, upload)
+      end
+    end
+
+    describe '#track_submission_success' do
+      it 'logs sidekiq job successful' do
+        log = 'Burial 21P-530EZ submission to LH succeeded'
+        payload = {
+          claim_id: claim.id,
+          benefits_intake_uuid: lh_service.uuid,
+          confirmation_number: claim.confirmation_number,
+          user_account_uuid: current_user.uuid,
+          tags: monitor.tags
+        }
+
+        expect(monitor).to receive(:track_request).with(
+          'info',
+          log,
+          "#{submission_stats_key}.success",
+          call_location: anything,
+          **payload
+        )
+
+        monitor.track_submission_success(claim, lh_service, current_user.uuid)
+      end
+    end
+
+    describe '#track_submission_retry' do
+      it 'logs sidekiq job failure and retry' do
+        log = 'Burial 21P-530EZ submission to LH failed, retrying'
+        payload = {
+          claim_id: claim.id,
+          benefits_intake_uuid: lh_service.uuid,
+          confirmation_number: claim.confirmation_number,
+          user_account_uuid: current_user.uuid,
+          message: monitor_error.message,
+          tags: monitor.tags
+        }
+
+        expect(monitor).to receive(:track_request).with(
+          'warn',
+          log,
+          "#{submission_stats_key}.failure",
+          call_location: anything,
+          **payload
+        )
+
+        monitor.track_submission_retry(claim, lh_service, current_user.uuid, monitor_error)
       end
     end
 
@@ -249,6 +349,78 @@ RSpec.describe Burials::Monitor do
 
           monitor.track_send_submitted_email_failure(claim, lh_service, current_user.uuid, monitor_error)
         end
+      end
+    end
+
+    describe '#track_send_confirmation_email_failure' do
+      it 'logs sidekiq job send_confirmation_email error' do
+        log = 'Burial 21P-530EZ send_confirmation_email failed'
+        payload = {
+          claim_id: claim.id,
+          benefits_intake_uuid: lh_service.uuid,
+          confirmation_number: claim.confirmation_number,
+          user_account_uuid: current_user.uuid,
+          message: monitor_error.message,
+          tags: monitor.tags
+        }
+
+        expect(monitor).to receive(:track_request).with(
+          'warn',
+          log,
+          "#{submission_stats_key}.send_confirmation_failed",
+          call_location: anything,
+          **payload
+        )
+
+        monitor.track_send_confirmation_email_failure(claim, lh_service, current_user.uuid, monitor_error)
+      end
+    end
+
+    describe '#track_send_submitted_email_failure' do
+      it 'logs sidekiq job send_submitted_email error' do
+        log = 'Burial 21P-530EZ send_submitted_email failed'
+        payload = {
+          claim_id: claim.id,
+          benefits_intake_uuid: lh_service.uuid,
+          confirmation_number: claim.confirmation_number,
+          user_account_uuid: current_user.uuid,
+          message: monitor_error.message,
+          tags: monitor.tags
+        }
+
+        expect(monitor).to receive(:track_request).with(
+          'warn',
+          log,
+          "#{submission_stats_key}.send_submitted_failed",
+          call_location: anything,
+          **payload
+        )
+
+        monitor.track_send_submitted_email_failure(claim, lh_service, current_user.uuid, monitor_error)
+      end
+    end
+
+    describe '#track_file_cleanup_error' do
+      it 'logs sidekiq job ensure file cleanup error' do
+        log = 'Burial 21P-530EZ cleanup failed'
+        payload = {
+          claim_id: claim.id,
+          benefits_intake_uuid: lh_service.uuid,
+          confirmation_number: claim.confirmation_number,
+          user_account_uuid: current_user.uuid,
+          error: monitor_error.message,
+          tags: monitor.tags
+        }
+
+        expect(monitor).to receive(:track_request).with(
+          'error',
+          log,
+          "#{submission_stats_key}.cleanup_failed",
+          call_location: anything,
+          **payload
+        )
+
+        monitor.track_file_cleanup_error(claim, lh_service, current_user.uuid, monitor_error)
       end
     end
   end
