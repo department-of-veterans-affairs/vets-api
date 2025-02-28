e0676f1ddd1b44a26b8c9e0f8653eafb4b0b30bf
rockwellwindsor-va <rockwell.rice@oddball.io>
2025-02-28 10:00:50 -0600

    WIP

--- modules/claims_api/lib/bgs_service/vnp_proc_service_v2.rb
+++ modules/claims_api/lib/bgs_service/vnp_proc_service_v2.rb
@@ -3,20 +3,24 @@
 module ClaimsApi
   class VnpProcServiceV2 < ClaimsApi::LocalBGS
     PROC_TYPE_CD = 'POAAUTHZ'
-    PROC_STATE = 'COMPLETE'
+    PROC_STATE = 'complete'
 
     def bean_name
       'VnpProcWebServiceBeanV2/VnpProcServiceV2'
     end
 
     def vnp_proc_create
-      created_date = Time.current.iso8601
+      byebug
+      # created_date = Date.new()
       body = Nokogiri::XML::DocumentFragment.parse <<~EOXML
         <arg0>
           <VnpProcDTO>
+            <vnpProcId>0</vnpProcId>
             <vnpProcTypeCd>#{PROC_TYPE_CD}</vnpProcTypeCd>
             <vnpProcStateTypeCd>#{PROC_STATE}</vnpProcStateTypeCd>
-            <creatdDt>#{Time.now}</creatdDt>
+            <creatdDt>2025-02-28T14:19:18.637-05:00</creatdDt>
+            <lastModifdDt>2025-02-28T14:19:18.637-05:00</lastModifdDt>
+            <submtdDt>2025-02-28T14:19:18.637-05:00</submtdDt>
           </VnpProcDTO>
         </arg0>
       EOXML
