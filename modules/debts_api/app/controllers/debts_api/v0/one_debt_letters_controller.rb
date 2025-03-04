33e8d8b586ab1583b1dbbf8656f66f267733d799
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-03-03 22:27:13 -0500

    Add modules/

--- modules/debts_api/app/controllers/debts_api/v0/one_debt_letters_controller.rb
+++ modules/debts_api/app/controllers/debts_api/v0/one_debt_letters_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 require 'debts_api/v0/one_debt_letter_service'
 
+require 'debts_api/v0/one_debt_letter_service'
+
 module DebtsApi
   module V0
     class OneDebtLettersController < ApplicationController
