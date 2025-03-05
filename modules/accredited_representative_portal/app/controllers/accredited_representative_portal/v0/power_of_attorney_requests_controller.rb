48654aa1825d21e0c4963a97b612af3101732b95
Gregory Hedrick <gregory@pixiitech.net>
2025-03-05 11:54:24 -0500

    PoA request search backend (#98332)

--- modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
+++ modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
@@ -17,23 +17,8 @@ module AccreditedRepresentativePortal
       end
 
       def index
-        relation = policy_scope(PowerOfAttorneyRequest)
-        status = params[:status].presence
-
-        relation =
-          case status
-          when Statuses::PENDING
-            pending(relation)
-          when Statuses::PROCESSED
-            processed(relation)
-          when NilClass
-            relation
-          else
-            raise ActionController::BadRequest, <<~MSG.squish
-              Invalid status parameter.
-              Must be one of (#{Statuses::ALL.join(', ')})
-            MSG
-          end
+        relation = filter_by_status(policy_scope(PowerOfAttorneyRequest))
+        relation = search_filter(relation)
 
         # `limit(100)` in case pagination isn't introduced quickly enough.
         poa_requests = relation.includes(scope_includes).limit(100)
@@ -66,6 +51,28 @@ module AccreditedRepresentativePortal
         relation.processed.decisioned.order(
           resolution: { created_at: :desc }
         )
+      end
+
+      def search_filter(rel)
+        PowerOfAttorneyRequestSearchService.new(
+          rel, params[:first_name], params[:last_name], params[:dob], params[:ssn]
+        ).call
+      end
+
+      def filter_by_status(rel)
+        case params[:status].presence
+        when Statuses::PENDING
+          pending(rel)
+        when Statuses::PROCESSED
+          processed(rel)
+        when NilClass
+          rel
+        else
+          raise ActionController::BadRequest, <<~MSG.squish
+            Invalid status parameter.
+            Must be one of (#{Statuses::ALL.join(', ')})
+          MSG
+        end
       end
 
       def scope_includes
---
+++ modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/claimant/power_of_attorney_requests_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require_relative '../../../../rails_helper'
+
+RSpec.describe AccreditedRepresentativePortal::V0::PowerOfAttorneyRequestsController, type: :request do
+  before do
+    login_as(test_user)
+    travel_to(time)
+  end
+
+  let!(:poa_code) { 'x23' }
+  let!(:other_poa_code) { 'z99' }
+
+  let!(:test_user) do
+    create(:representative_user, email: 'test@va.gov', icn: '123498767V234859')
+  end
+
+  let!(:accredited_individual) do
+    create(:user_account_accredited_individual,
+           user_account_email: test_user.email,
+           user_account_icn: test_user.icn,
+           poa_code:)
+  end
+
+  let!(:representative) do
+    create(:representative,
+           :vso,
+           representative_id: accredited_individual.accredited_individual_registration_number,
+           poa_codes: [poa_code])
+  end
+
+  let!(:poa_request) { create(:power_of_attorney_request, :with_veteran_claimant, poa_code:) }
+  let!(:other_poa_request) { create(:power_of_attorney_request, :with_veteran_claimant, poa_code: other_poa_code) }
+
+  let(:time) { '2024-12-21T04:45:37.000Z' }
+  let(:time_plus_one_day) { '2024-12-22T04:45:37.000Z' }
+
+  describe 'GET /accredited_representative_portal/v0/power_of_attorney_requests' do
+    context 'when providing incomplete search params' do
+      it 'returns a 400 error' do
+        post('/accredited_representative_portal/v0/claimant/power_of_attorney_requests', params: {
+               first_name: 'John', last_name: 'Smith', dob: '1980-01-01', ssn: ''
+             })
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context 'when providing complete search params' do
+      it 'returns only matching power of attorney requests' do
+        VCR.use_cassette('mpi/find_candidate/valid_icn_full') do
+          post('/accredited_representative_portal/v0/claimant/power_of_attorney_requests', params: {
+                 first_name: 'John', last_name: 'Smith', dob: '1980-01-01', ssn: '666-66-6666'
+               })
+        end
+        parsed_response = JSON.parse(response.body)
+        expect(response).to have_http_status(:ok)
+        expect(parsed_response.map { |poa| poa['id'] }).to eq([poa_request.id])
+      end
+    end
+  end
+end
