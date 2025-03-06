3d552057e0003647345e8752ea0c4ea99c19d203
Oren Mittman <nihil2501@gmail.com>
2025-03-06 09:56:36 -0500

    [ARP] Remove extraneous POA request create route
    Snuck in in an earlier PR but we don't have such a route.

--- modules/accredited_representative_portal/config/routes.rb
+++ modules/accredited_representative_portal/config/routes.rb
@@ -19,7 +19,7 @@ AccreditedRepresentativePortal::Engine.routes.draw do
       resources :in_progress_forms, only: %i[update show destroy]
     end
 
-    resources :power_of_attorney_requests, only: %i[index show create] do
+    resources :power_of_attorney_requests, only: %i[index show] do
       resource :decision, only: :create, controller: 'power_of_attorney_request_decisions'
     end
   end
