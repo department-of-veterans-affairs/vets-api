337a30b22d76a24b0d538f1d783315c0b9082e3b
Jacob Penner <japenner@gmail.com>
2025-03-06 09:53:56 -0500

    initial attempt at extending rspec coverage

--- spec/sidekiq/simple_forms_api/notification/send_notification_email_job_spec.rb
+++ spec/sidekiq/simple_forms_api/notification/send_notification_email_job_spec.rb
@@ -3,78 +3,84 @@
 require 'rails_helper'
 
 RSpec.describe SimpleFormsApi::Notification::SendNotificationEmailJob, type: :worker do
+  let(:notification_email) { double(send: nil) }
+  let(:form_upload_notification_email) { double(send: nil) }
+  let(:notification_type) { :confirmation }
+  let(:form_submission_attempt) { build(:form_submission_attempt) }
+  let(:form_number) { '21-0779' }
+  let(:user_account) { build(:user_account) }
+  let(:args) do
+    {
+      notification_type:,
+      form_submission_attempt_id: form_submission_attempt.id,
+      form_number:,
+      user_account_id: user_account.id
+    }
+  end
+
+  before do
+    allow(UserAccount).to receive(:find).and_return(user_account)
+    allow(FormSubmissionAttempt).to receive(:find).and_return(form_submission_attempt)
+    allow(SimpleFormsApi::NotificationEmail).to receive(:new).and_return(notification_email)
+    allow(SimpleFormsApi::FormUploadNotificationEmail).to receive(:new).and_return(form_upload_notification_email)
+    allow(StatsD).to receive(:increment)
+  end
+
   describe '#perform' do
+    subject(:perform) { described_class.new.perform(args) }
+
+    before { perform }
+
     context 'form was submitted with a digital form submission tool' do
-      let(:notification_type) { :confirmation }
-      let(:form_submission_attempt) { build(:form_submission_attempt) }
-      let(:form_number) { 'abc-123' }
-      let(:user_account) { build(:user_account) }
-      let(:notification_email) { double(send: nil) }
-
       it 'sends the email' do
-        allow(SimpleFormsApi::NotificationEmail).to receive(:new).and_return(notification_email)
-
-        described_class.new.perform(
-          notification_type:,
-          form_submission_attempt:,
-          form_number:,
-          user_account:
-        )
-
         expect(notification_email).to have_received(:send).with(at: anything)
       end
 
       context 'SimpleFormsApi::NotificationEmail initialization fails' do
         it 'increments statsd' do
-          allow(SimpleFormsApi::NotificationEmail).to receive(:new)
-          allow(StatsD).to receive(:increment)
-
-          described_class.new.perform(
-            notification_type: :error,
-            form_submission_attempt:,
-            form_number:,
-            user_account:
-          )
-
           expect(StatsD).to have_received(:increment).with('silent_failure', tags: anything)
         end
       end
     end
 
     context 'form was submitted with Form Upload tool' do
-      let(:notification_type) { :confirmation }
-      let(:form_submission_attempt) { build(:form_submission_attempt) }
-      let(:form_number) { '21-0779' }
-      let(:user_account) { build(:user_account) }
-      let(:form_upload_notification_email) { double(send: nil) }
-
       it 'sends the email' do
-        allow(SimpleFormsApi::FormUploadNotificationEmail).to receive(:new).and_return(form_upload_notification_email)
-
-        described_class.new.perform(
-          notification_type:,
-          form_submission_attempt:,
-          form_number:,
-          user_account:
-        )
-
         expect(form_upload_notification_email).to have_received(:send).with(at: anything)
       end
 
       context 'SimpleFormsApi::FormUploadNotificationEmail initialization fails' do
-        it 'increments statsd' do
+        before do
           allow(SimpleFormsApi::FormUploadNotificationEmail).to receive(:new).and_raise(ArgumentError)
-          allow(StatsD).to receive(:increment)
+        end
 
-          described_class.new.perform(
-            notification_type: :error,
-            form_submission_attempt:,
-            form_number:,
-            user_account:
-          )
+        it 'increments statsd' do
           expect(StatsD).to have_received(:increment).with('silent_failure', tags: anything)
         end
       end
+    end
+  end
+
+  describe '#perform_async' do
+    subject(:perform_async) { described_class.perform_async(args) }
+
+    before do
+      perform_async
+    end
+
+    it 'enqueues the job' do
+      expect(described_class).to respond_to(:perform_async)
+    end
+
+    it 'finds the user account' do
+      expect(UserAccount).to have_received(:find).with(user_account.id)
+    end
+
+    it 'finds the form submission attempt' do
+      expect(FormSubmissionAttempt).to have_received(:find).with(form_submission_attempt.id)
+    end
+
+    it 'initializes the notification email' do
+      expect(SimpleFormsApi::NotificationEmail).to have_received(:new)
     end
   end
 end
