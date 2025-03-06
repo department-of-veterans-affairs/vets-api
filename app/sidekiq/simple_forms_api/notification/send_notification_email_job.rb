ae3c0bfd496a2c58d1f1e9c88260c9933ce4181d
Jacob Penner <japenner@gmail.com>
2025-03-06 10:43:04 -0500

    updates to job

--- app/sidekiq/simple_forms_api/notification/send_notification_email_job.rb
+++ app/sidekiq/simple_forms_api/notification/send_notification_email_job.rb
@@ -9,17 +9,11 @@ module SimpleFormsApi
 
       HOUR_TO_SEND_NOTIFICATIONS = 9
 
-      def perform(args)
-        raise ArgumentError, "Expected a Hash, got #{args.class}" unless args.is_a?(Hash)
-
-        @notification_type = args[:notification_type]
-        @form_number = args[:form_number]
-        @user_account_id = args[:user_account_id]
-        @form_submission_attempt_id = args[:form_submission_attempt_id]
-
-        return handle_exception(StandardError.new('Missing required arguments')) if missing_required_arguments?
-
-        @config = build_config
+      def perform(benefits_intake_uuid, form_number)
+        @benefits_intake_uuid = benefits_intake_uuid
+        @form_number = form_number
+        @user_account = form_submission_attempt.user_account
+        @form_submission = form_submission_attempt.form_submission
 
         return form_upload_notification_email.send(at: time_to_send) if form_supported?
 
@@ -30,13 +24,18 @@ module SimpleFormsApi
 
       private
 
-      def user_account
-        @user_account ||= UserAccount.find_by(id: @user_account_id) ||
-                          raise_not_found_error('UserAccount', id)
+      attr_accessor :user_account, :form_number, :form_submission
+
+      def notification_type
+        if form_submission_attempt.failure?
+          :error
+        elsif form_submission_attempt.vbms?
+          :received
+        end
       end
 
       def form_submission_attempt
-        @form_submission_attempt ||= FormSubmissionAttempt.find_by(id: @form_submission_attempt_id) ||
+        @form_submission_attempt ||= FormSubmissionAttempt.find_by(benefits_intake_uuid: @benefits_intake_uuid) ||
                                      raise_not_found_error('FormSubmissionAttempt', id)
       end
 
@@ -44,16 +43,26 @@ module SimpleFormsApi
         raise ActiveRecord::RecordNotFound, "#{resource} #{id} not found"
       end
 
+      def config
+        {
+          form_data: JSON.parse(form_submission.form_data.presence || '{}'),
+          form_number:,
+          confirmation_number: @benefits_intake_uuid,
+          date_submitted: form_submission_attempt.created_at.strftime('%B %d, %Y'),
+          lighthouse_updated_at: form_submission_attempt.lighthouse_updated_at&.strftime('%B %d, %Y')
+        }
+      end
+
       def form_supported?
-        SimpleFormsApi::FormUploadNotificationEmail::SUPPORTED_FORMS.include?(@form_number)
+        SimpleFormsApi::FormUploadNotificationEmail::SUPPORTED_FORMS.include?(form_number)
       end
 
       def form_upload_notification_email
-        SimpleFormsApi::FormUploadNotificationEmail.new(@config, notification_type: @notification_type)
+        SimpleFormsApi::FormUploadNotificationEmail.new(@config, notification_type:)
       end
 
       def notification_email
-        SimpleFormsApi::NotificationEmail.new(@config, notification_type: @notification_type, user_account:)
+        SimpleFormsApi::NotificationEmail.new(@config, notification_type:, user_account:)
       end
 
       def time_to_send
@@ -64,7 +73,7 @@ module SimpleFormsApi
       end
 
       def statsd_tags
-        { 'service' => 'veteran-facing-forms', 'function' => "#{@form_number} form submission to Lighthouse" }
+        { 'service' => 'veteran-facing-forms', 'function' => "#{form_number} form submission to Lighthouse" }
       end
 
       def handle_exception(e)
@@ -72,27 +81,12 @@ module SimpleFormsApi
           {
             error: 'Error sending simple forms notification email',
             message: e.message,
-            notification_type: @notification_type.to_s,
+            notification_type:,
             confirmation_number: @config&.dig(:confirmation_number)
           }.to_json
         )
 
-        StatsD.increment('silent_failure', tags: statsd_tags) if @notification_type == :error
-      end
-
-      def missing_required_arguments?
-        [@notification_type, @form_number, @user_account_id, @form_submission_attempt_id].any?(&:nil?)
-      end
-
-      def build_config
-        attempt = form_submission_attempt
-        {
-          form_data: JSON.parse(attempt.form_submission.form_data.presence || '{}'),
-          form_number: @form_number,
-          confirmation_number: attempt.benefits_intake_uuid,
-          date_submitted: attempt.created_at.strftime('%B %d, %Y'),
-          lighthouse_updated_at: attempt.lighthouse_updated_at&.strftime('%B %d, %Y')
-        }
+        StatsD.increment('silent_failure', tags: statsd_tags) if notification_type == :error
       end
     end
   end
