7ea64b1dc231ef66fffbcdbc7cd305c2a42e18c3
Riley Anderson <riley.anderson@oddball.io>
2025-03-10 12:24:51 -0600

    Load IdentitySettings secrets (#21147)

--- config/initializers/identity_config.rb
+++ config/initializers/identity_config.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
-identity_settings_files = Config.setting_files(Rails.root.join('config', 'identity_settings'), Settings.vsp_environment)
+ENV_PREFIX = 'IDENTITY_SETTINGS'
+ENV_SEPARATOR = '__'
+SETTINGS_FOLDER = Rails.root.join('config', 'identity_settings')
 
-IdentitySettings = Config.load_files(identity_settings_files)
+IdentitySettings = Config::Options.new
+
+Config.setting_files(SETTINGS_FOLDER, Settings.vsp_environment).each do |file|
+  IdentitySettings.add_source!(file)
+end
+
+secrets_source = Config::Sources::EnvSource.new(ENV, prefix: ENV_PREFIX, separator: ENV_SEPARATOR)
+IdentitySettings.add_source!(secrets_source)
+
+IdentitySettings.reload!
