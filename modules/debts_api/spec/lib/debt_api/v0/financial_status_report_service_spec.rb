d0340731c7593e141002a70b3961fb17d7419099
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-03-09 23:59:10 -0400

    stuff

--- modules/debts_api/spec/lib/debt_api/v0/financial_status_report_service_spec.rb
+++ modules/debts_api/spec/lib/debt_api/v0/financial_status_report_service_spec.rb
@@ -129,7 +129,8 @@ RSpec.describe DebtsApi::V0::FinancialStatusReportService, type: :service do
                 'name' => user_data.first_name,
                 'time' => '48 hours',
                 'date' => Time.zone.now.strftime('%m/%d/%Y')
-              }
+              },
+              { id_type: 'email' }
             )
             service.submit_vba_fsr(valid_form_data)
           end
