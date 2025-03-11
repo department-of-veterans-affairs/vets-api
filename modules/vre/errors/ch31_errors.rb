5fd59299ef7553e42b5315b9b0d01e4b6c8d9dd5
Kyle Henson <145150351+khenson-oddball@users.noreply.github.com>
2025-03-11 17:39:34 -0600

    Add requiring of files and change error class modularization

--- modules/vre/errors/ch31_errors.rb
+++ modules/vre/errors/ch31_errors.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
-module VRE
-  class Ch31Error < StandardError; end
-  class Ch31NilClaimError < StandardError; end
-end
+class Ch31Error < StandardError; end
+class Ch31NilClaimError < StandardError; end
