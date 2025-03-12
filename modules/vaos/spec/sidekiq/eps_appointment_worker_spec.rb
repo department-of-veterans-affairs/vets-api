efb288ee2e85ae0d6d791dec17ced5c65a6b7374
Lee Delarm <lee.delarm@agile6.com>
2025-03-12 11:27:42 -0400

    Fixed tests
    Fixed tests

--- modules/vaos/spec/sidekiq/eps_appointment_worker_spec.rb
+++ modules/vaos/spec/sidekiq/eps_appointment_worker_spec.rb
@@ -2,13 +2,17 @@
 
 require 'rails_helper'
 
+##
+# RSpec tests for Eps::EpsAppointmentWorker
+#
 RSpec.describe Eps::EpsAppointmentWorker, type: :job do
-  subject { described_class }
+  subject(:worker) { described_class.new }
 
-  let(:user) { build(:user, :vaos, :accountable) }
+  let(:user) { build(:user) }
   let(:appointment_id) { '12345' }
   let(:service) { instance_double(Eps::AppointmentService) }
   let(:response) { OpenStruct.new(state: 'completed', appointmentDetails: OpenStruct.new(status: 'booked')) }
+  let(:unfinished_response) { OpenStruct.new(state: 'pending', appointmentDetails: OpenStruct.new(status: 'pending')) }
 
   before do
     Sidekiq::Job.clear_all
@@ -18,28 +22,30 @@ RSpec.describe Eps::EpsAppointmentWorker, type: :job do
   describe '.perform_async' do
     it 'submits successfully' do
       expect do
-        subject.perform_async(appointment_id, user)
-      end.to change(subject.jobs, :size).by(1)
+        described_class.perform_async(appointment_id, user)
+      end.to change(described_class.jobs, :size).by(1)
     end
 
     it 'calls get_appointment with the appointment_id' do
       allow(service).to receive(:get_appointment).with(appointment_id:).and_return(response)
       expect(service).to receive(:get_appointment).with(appointment_id:)
-      subject.new.perform(appointment_id, user)
+      worker.perform(appointment_id, user)
     end
 
-    it 'retries if the appointment is not finished' do
-      unfinished_response = OpenStruct.new(state: 'pending', appointmentDetails: OpenStruct.new(status: 'pending'))
-      allow(service).to receive(:get_appointment).with(appointment_id:).and_return(unfinished_response)
-      expect(subject).to receive(:perform_in).with(1.minute, appointment_id, user, 1)
-      subject.new.perform(appointment_id, user)
-    end
+    context 'when the appointment is not finished' do
+      before do
+        allow(service).to receive(:get_appointment).with(appointment_id:).and_return(unfinished_response)
+      end
 
-    it 'sends failure message after max retries' do
-      unfinished_response = OpenStruct.new(state: 'pending', appointmentDetails: OpenStruct.new(status: 'pending'))
-      allow(service).to receive(:get_appointment).with(appointment_id:).and_return(unfinished_response)
-      expect(subject).to receive(:send_vanotify_message).with(success: false, error: 'Could not complete booking')
-      subject.new.perform(appointment_id, user, 3)
+      it 'retries the job' do
+        expect(described_class).to receive(:perform_in).with(1.minute, appointment_id, user, 1)
+        worker.perform(appointment_id, user)
+      end
+
+      it 'sends failure message after max retries' do
+        expect(worker).to receive(:send_vanotify_message).with(success: false, error: 'Could not complete booking')
+        worker.perform(appointment_id, user, 3)
+      end
     end
   end
 end
