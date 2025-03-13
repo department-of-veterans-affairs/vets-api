d9a582f9d58f395670c275d4aaf92c8acda428e0
Jonathan VanCourt <144135615+jvcAdHoc@users.noreply.github.com>
2025-03-13 13:56:23 -0400

    enforce identity and eligibility business rules on poa request creation

--- modules/representation_management/spec/requests/representation_management/v0/power_of_attorney_requests_controller_spec.rb
+++ modules/representation_management/spec/requests/representation_management/v0/power_of_attorney_requests_controller_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe 'RepresentationManagement::V0::PowerOfAttorneyRequests', type: :r
     let(:user) { create(:user, :loa3) }
     let!(:user_verification) { create(:idme_user_verification, idme_uuid: user.idme_uuid) }
     let(:base_path) { '/representation_management/v0/power_of_attorney_requests' }
-    let(:organization) { create(:organization) }
+    let(:organization) { create(:organization, can_accept_digital_poa_requests: accepts_digital_requests) }
+    let(:accepts_digital_requests) { true }
     let(:representative) { create(:representative) }
     let(:params) do
       {
@@ -15,26 +16,6 @@ RSpec.describe 'RepresentationManagement::V0::PowerOfAttorneyRequests', type: :r
           record_consent: true,
           consent_address_change: true,
           consent_limits: [],
-          claimant: {
-            date_of_birth: '1980-12-31',
-            relationship: 'Spouse',
-            phone: '5555555555',
-            email: 'claimant@example.com',
-            name: {
-              first: 'John',
-              middle: 'Middle',
-              last: 'Claimant'
-            },
-            address: {
-              address_line1: '123 Fake Claimant St',
-              address_line2: '',
-              city: 'Portland',
-              state_code: 'OR',
-              country: 'USA',
-              zip_code: '12345',
-              zip_code_suffix: '6789'
-            }
-          },
           veteran: {
             ssn: '123456789',
             va_file_number: '123456789',
@@ -67,10 +48,10 @@ RSpec.describe 'RepresentationManagement::V0::PowerOfAttorneyRequests', type: :r
     end
 
     context 'when appoint_a_representative_enable_v2_features is enabled' do
-      context 'with a signed in user' do
+      context 'with a signed in user with all identifiers' do
         before do
           sign_in_as(user)
-          Flipper.enable(:appoint_a_representative_enable_v2_features)
+          allow(Flipper).to receive(:enabled?).with(:appoint_a_representative_enable_v2_features).and_return(true)
         end
 
         context 'When submitting all fields with valid data' do
@@ -102,18 +83,108 @@ RSpec.describe 'RepresentationManagement::V0::PowerOfAttorneyRequests', type: :r
         end
 
         context 'when an error occurs' do
-          before do
-            params[:power_of_attorney_request][:veteran][:name][:first] = nil
-            post(base_path, params:)
+          context 'the form data fails validation' do
+            before do
+              params[:power_of_attorney_request][:veteran][:name][:first] = nil
+              post(base_path, params:)
+            end
+
+            it 'responds with an unprocessable entity status' do
+              expect(response).to have_http_status(:unprocessable_entity)
+            end
+
+            it 'responds with a meaningful error message' do
+              expect(response.body).to eq({ errors: ["Veteran first name can't be blank"] }.to_json)
+            end
           end
 
-          it 'responds with an unprocessable entity status' do
-            expect(response).to have_http_status(:unprocessable_entity)
+          context 'the organization does not accept digital poa requests' do
+            let(:accepts_digital_requests) { false }
+
+            before do
+              post(base_path, params:)
+            end
+
+            it 'responds with an unprocessable entity status' do
+              expect(response).to have_http_status(:unprocessable_entity)
+            end
+
+            it 'responds with a meaningful error message' do
+              error_message = 'Accredited organization must be able to accept digital Power of Attorney Requests'
+              expect(response.body).to eq({ errors: [error_message] }.to_json)
+            end
           end
 
-          it 'responds with a meaningful error message' do
-            expect(response.body).to eq({ errors: ["Veteran first name can't be blank"] }.to_json)
+          context 'the user is not submitting as the Veteran' do
+            before do
+              params[:power_of_attorney_request][:claimant] = {
+                date_of_birth: '1980-12-31',
+                relationship: 'Spouse',
+                phone: '5555555555',
+                email: 'claimant@example.com',
+                name: {
+                  first: 'John',
+                  middle: 'Middle',
+                  last: 'Claimant'
+                },
+                address: {
+                  address_line1: '123 Fake Claimant St',
+                  address_line2: '',
+                  city: 'Portland',
+                  state_code: 'OR',
+                  country: 'USA',
+                  zip_code: '12345',
+                  zip_code_suffix: '6789'
+                }
+              }
+              post(base_path, params:)
+            end
+
+            it 'responds with an unprocessable entity status' do
+              expect(response).to have_http_status(:unprocessable_entity)
+            end
+
+            it 'responds with a meaningful error message' do
+              error_message = 'User must submit as the Veteran for digital Power of Attorney Requests'
+              expect(response.body).to eq({ errors: [error_message] }.to_json)
+            end
           end
+        end
+      end
+
+      context 'when the signed in user does not have a corp participant id' do
+        let(:user) { create(:user, participant_id: nil) }
+
+        before do
+          sign_in(user)
+          post(base_path, params:)
+        end
+
+        it 'responds with an unprocessable entity status' do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'responds with a meaningful error message' do
+          error_message = 'User is missing a Corp Participant ID value'
+          expect(response.body).to eq({ errors: [error_message] }.to_json)
+        end
+      end
+
+      context 'when the signed in user does not have an ICN' do
+        let(:user) { create(:user, :loa3, icn: nil) }
+
+        before do
+          sign_in(user)
+          post(base_path, params:)
+        end
+
+        it 'responds with an unprocessable entity status' do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'responds with a meaningful error message' do
+          error_message = 'User is missing an ICN value'
+          expect(response.body).to eq({ errors: [error_message] }.to_json)
         end
       end
     end
@@ -129,7 +200,7 @@ RSpec.describe 'RepresentationManagement::V0::PowerOfAttorneyRequests', type: :r
     context 'when appoint_a_representative_enable_v2_features is disabled' do
       before do
         sign_in_as(user)
-        Flipper.disable(:appoint_a_representative_enable_v2_features)
+        allow(Flipper).to receive(:enabled?).with(:appoint_a_representative_enable_v2_features).and_return(false)
       end
 
       it 'returns a 404/not_found status' do
