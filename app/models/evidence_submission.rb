698d8abc9e3c9dab3491b931e548cff8155c345c
samcoforma <105806934+samcoforma@users.noreply.github.com>
2025-03-14 08:56:38 -0700

    [CST] [ZSF] [Lighthouse] 105099 Resolve evidence submission bug (#21239)
    * added tests and code changes, also renamed evss factory
    
    * added test changes for linter
    
    * added changes to evss for evidence submission bug
    
    * update to master
    
    * updated lh service
    
    * updated lh service with new code
    
    * add back user_account
    
    * update around method
    
    * update lighthouse code and added utc code back in from previous ticket
    
    * updated evss_claim_service_spec.rb
    
    * resolve bug with claim id
    
    * update for bug
    
    * add changes to the service
    
    * add id
    
    * update the service tests
    
    * add changes to tests and per code review
    
    * started updating spec/sidekiq/evss/document_upload_spec.rb
    
    * add to the factory
    
    * more unfinished updates to spec/sidekiq/evss/document_upload_spec.rb
    
    * update code and tests
    
    * update can update evidence method
    
    * update tests for utc change
    
    * update so message exists
    
    * udpate lh and evss tests
    
    * updated the order of the cosntants
    
    * update tests statsd
    
    * Update the service
    
    * fixed bug in 'status_changed?' in upload_status_updater.rb and added more logging when the status changes
    
    * removed EVSS file changes from this Lighthouse PR
    
    * fixed linting errors
    
    * move where evidence submission record is created in the service
    
    * fixed linter errors and added scopes
    
    * updated linting issue
    
    * added more assertions to spec/lib/lighthouse/benefits_documents/upload_status_updater_spec.rb
    
    ---------
    
    Co-authored-by: Peri McLaren <141954992+pmclaren19@users.noreply.github.com>

--- app/models/evidence_submission.rb
+++ app/models/evidence_submission.rb
@@ -7,13 +7,22 @@ class EvidenceSubmission < ApplicationRecord
   has_kms_key
   has_encrypted :template_metadata, key: :kms_key, **lockbox_options
 
+  # Internal Upload statuses:
+  # CREATED: (Internal status) - the evidence submission record is created.
+  # QUEUED: (Internal status) - the evidence submission record has been given a job id.
+  # FAILED: the evidence submission record errored in evss_claim_service.rb, evss/document_upload.rb,
+  # benefits_documents/service.rb, or lighthouse/evidence_submission/document_upload.rb.
+
   # Lighthouse upload statuses:
   # IN_PROGRESS: the workflow is currently executing.
   # SUCCESS: the workflow has completed all steps successfully.
   # FAILED: the workflow could not complete because a step encountered a non-recoverable error.
+
+  scope :created, -> { where(upload_status: BenefitsDocuments::Constants::UPLOAD_STATUS[:CREATED]) }
+  scope :queued, -> { where(upload_status: BenefitsDocuments::Constants::UPLOAD_STATUS[:QUEUED]) }
+  scope :pending, -> { where(upload_status: BenefitsDocuments::Constants::UPLOAD_STATUS[:PENDING]) }
   scope :completed, -> { where(upload_status: BenefitsDocuments::Constants::UPLOAD_STATUS[:SUCCESS]) }
   scope :failed, -> { where(upload_status: BenefitsDocuments::Constants::UPLOAD_STATUS[:FAILED]) }
-  scope :pending, -> { where(upload_status: BenefitsDocuments::Constants::UPLOAD_STATUS[:PENDING]) }
   # used for sending failure notification emails
   scope :va_notify_email_queued, lambda {
     where(upload_status: BenefitsDocuments::Constants::UPLOAD_STATUS[:FAILED])
