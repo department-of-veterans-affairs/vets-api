0ce055583184e25e526dbc7cda5d6b621b656bae
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-03-14 17:42:58 -0400

    Upatead

--- modules/debts_api/app/controllers/debts_api/v0/one_debt_letters_controller.rb
+++ modules/debts_api/app/controllers/debts_api/v0/one_debt_letters_controller.rb
@@ -9,7 +9,6 @@ module DebtsApi
 
       def download_pdf
         service = DebtsApi::V0::OneDebtLetterService.new(current_user)
-
         file_contents = service.get_pdf
 
         send_data file_contents, filename: file_name_for_pdf, type: 'application/pdf', disposition: 'attachment'
