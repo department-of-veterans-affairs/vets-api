6be9a75d16662bbc2033a356d401a3f9fc1e7da8
Brandon Cooper <brandon.cooper@adhocteam.us>
2025-03-17 15:52:30 -0400

    remove model class and factory for health_facility

--- app/models/health_facility.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class HealthFacility < ApplicationRecord
-end
--- spec/factories/health_facilities.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :health_facility do
-    name { 'Fake facility name' }
-    station_number { '123' }
-  end
-end
