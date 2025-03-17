f8f57b87145300ec2149b26bf54b35a1de2465d3
Brendan Dunn <brendan.dunn@va.gov>
2025-03-17 15:53:59 -0400

    Added dynamic check to IT test

--- spec/lib/kafka/avro_producer_payload_spec.rb
+++ spec/lib/kafka/avro_producer_payload_spec.rb
@@ -7,8 +7,8 @@ require 'kafka/oauth_token_refresher'
 describe Kafka::AvroProducer do
   let(:avro_producer) { described_class.new }
   let(:schema_path) { Rails.root.join('lib', 'kafka', 'schemas', 'submission_trace_schema_dev-value-1.avsc') }
+  let(:schema_file) { File.read(schema_path) }
   let(:schema) { Avro::Schema.parse(File.read(schema_path)) }
-
   let(:valid_payload) do
     {
       'priorId' => nil,
@@ -24,6 +24,62 @@ describe Kafka::AvroProducer do
     }
   end
   let(:invalid_payload_format) { 'invalid' }
+
+  context 'producing a message successfully' do
+    let(:topic3_payload_value) { "\x00\x00\x00\x00\x05\x00\n12345\x00\x02\x12ICN123456\x12VASI98765\x00\x00\x00\x00\x00\x00(2024-03-04T12:00:00Z\x00" }
+
+    before do
+      Kafka::ProducerManager.instance.send(:setup_producer)
+    end
+
+    after do
+      # reset the client after each test
+      avro_producer.producer.client.reset
+    end
+
+    context 'with dynamic schema registry retrieval' do
+
+      context 'of an existing schema' do
+        it 'produces a message to the specified topic' do
+          VCR.use_cassette('kafka/topics') do
+            avro_producer.produce('topic-3', valid_payload)
+            avro_producer.produce('topic-4', valid_payload)
+            expect(avro_producer.producer.client.messages.length).to eq(2)        
+            topic_3_messages = avro_producer.producer.client.messages_for('topic-3')
+            expect(topic_3_messages.length).to eq(1)
+            expect(topic_3_messages[0][:payload]).to be_a(String)
+            expect(topic_3_messages[0][:payload]).to eq(topic3_payload_value)
+          end
+        end
+      end
+
+      context 'of an non-existing schema' do
+        it 'raises appropriate error' do
+          VCR.use_cassette('kafka/topics404') do
+            expect do
+              avro_producer.produce('topic-999', valid_payload)
+            end.to raise_error(Faraday::ResourceNotFound)
+          end
+        end
+      end
+    end
+
+    context 'with hardcoded schema registry retrieval' do
+      before do
+        allow(Flipper).to receive(:enabled?).with(:kafka_producer_fetch_schema_dynamically).and_return(false)
+        allow(File).to receive(:read).and_return(schema_file)
+      end
+
+      it 'produces a message to the specified topic' do
+        avro_producer.produce('submission_trace_schema_dev', valid_payload)
+        expect(avro_producer.producer.client.messages.length).to eq(1)
+        topic_3_messages = avro_producer.producer.client.messages_for('submission_trace_schema_dev')
+        expect(topic_3_messages.length).to eq(1)
+        expect(topic_3_messages[0][:payload]).to be_a(String)
+        expect(topic_3_messages[0][:payload]).to eq(topic3_payload_value)
+      end
+    end
+  end
 
   describe '#validate_payload!' do
     # valid data format
