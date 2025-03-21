c4b546342b7c0920dfaa571ea4d67a5096e123c8
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-03-21 13:52:28 -0400

    rspec for poa policy

---
+++ spec/policies/poa_policy_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+describe POAPolicy do
+  subject { described_class }
+
+  permissions :access? do
+    context 'when user has an ICN and is LOA3' do
+      let(:user) { build(:user, :loa3) }
+
+      it 'grants access' do
+        expect(subject).to permit(user, :power_of_attorney)
+      end
+
+
+    end
+
+    context 'when user does not have an ICN nor LOA3' do
+      let(:user) { build(:user, :loa1) }
+
+      before do
+        user.identity.attributes = {
+          icn: nil,
+        }
+      end
+
+      it 'denies access' do
+        expect(subject).not_to permit(user, :power_of_attorney)
+      end
+
+
+    end
+  end
+end
