2803592ac3eabec8281c72161b01319821cd8abb
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-03-21 10:23:09 -0500

    regenerated schema.db to get appropriate header

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_13_190720) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_17_205848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
