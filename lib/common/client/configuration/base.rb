033e750ae72105901f01bc0f7bd129dcabd33bd4
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-03-21 14:11:58 +0000

    Cleanup

--- lib/common/client/configuration/base.rb
+++ lib/common/client/configuration/base.rb
@@ -102,8 +102,10 @@ module Common
         def breakers_matcher
           base_uri = URI.parse(base_path)
           proc do |breakers_service, request_env, request_service_name|
-            return true if request_service_name && request_service_name == breakers_service.name
+            # Match by service_name if available.
+            request_service_name == breakers_service.name if request_service_name
 
+            # Fall back to matching by request URL.
             request_env.url.host == base_uri.host && request_env.url.port == base_uri.port &&
               request_env.url.path =~ /^#{base_uri.path}/
           end
