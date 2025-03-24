8b844fa030bc24cce5bcfd3549b3ad5c5168b329
broskj <170366505+brostk@users.noreply.github.com>
2025-03-24 12:08:10 -0400

    make format_date a class method

--- modules/ivc_champva/app/services/ivc_champva/ves_data_formatter.rb
+++ modules/ivc_champva/app/services/ivc_champva/ves_data_formatter.rb
@@ -140,7 +140,7 @@ module IvcChampva
       digits
     end
 
-    def format_date(date_string)
+    def self.format_date(date_string)
       return nil if date_string.blank?
 
       return date_string if date_string.match?(/^\d{4}-\d{2}-\d{2}$/)
