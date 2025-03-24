7fd4eb4b9a420986d45342d96dbc826c47d05589
Mike Moyer <87040148+mmoyer-va@users.noreply.github.com>
2025-03-24 10:22:35 -0400

    MHV-68159 Fixed tests

--- spec/lib/bb/generate_report_request_form_spec.rb
+++ spec/lib/bb/generate_report_request_form_spec.rb
@@ -24,7 +24,18 @@ describe BB::GenerateReportRequestForm do
 
   let(:attributes) { {} }
 
-  before { allow(subject).to receive(:eligible_data_classes).and_return(eligible_data_classes) }
+  before(:all) do
+    VCR.configure do |vcr_config|
+      vcr_config.default_cassette_options = {
+        allow_playback_repeats: true
+      }
+    end
+  end
+
+  before do
+    Flipper.disable(:mhv_medical_records_migrate_to_api_gateway)
+    allow(subject).to receive(:eligible_data_classes).and_return(eligible_data_classes)
+  end
 
   context 'with null attributes' do
     it 'responds to params' do
