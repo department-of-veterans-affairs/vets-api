94fff0901a0017586a9bc9027c28f0dc7794fe83
Samuel Cormier <sam.cormier@coforma.io>
2025-03-25 12:12:14 -0700

    more refactoring

--- app/sidekiq/lighthouse/evidence_submissions/va_notify_email_status_callback.rb
+++ app/sidekiq/lighthouse/evidence_submissions/va_notify_email_status_callback.rb
@@ -8,31 +8,18 @@ module Lighthouse
       def self.call(notification)
         es = EvidenceSubmission.find_by(va_notify_id: notification.notification_id)
         api_service_name = get_api_service_name(es.job_class)
+        status = notification.status
 
-        case notification.status
+        case status
         when 'delivered'
           # success
           es.update(va_notify_status: BenefitsDocuments::Constants::VANOTIFY_STATUS[:SUCCESS])
-          StatsD.increment('api.vanotify.notifications.delivered')
-          StatsD.increment('callbacks.cst_document_uploads.va_notify.notifications.delivered')
-          tags = ['service:claim-status', "function: #{api_service_name} - VA Notify evidence upload failure email"]
-          StatsD.increment('silent_failure_avoided', tags:)
         when 'permanent-failure'
           # delivery failed
           es.update(va_notify_status: BenefitsDocuments::Constants::VANOTIFY_STATUS[:FAILED])
-          StatsD.increment('api.vanotify.notifications.permanent_failure')
-          StatsD.increment('callbacks.cst_document_uploads.va_notify.notifications.permanent_failure')
-          tags = ['service:claim-status', "function: #{api_service_name} - VA Notify evidence upload failure email"]
-          StatsD.increment('silent_failure', tags:)
-        when 'temporary-failure'
-          # the api will continue attempting to deliver - success is still possible
-          StatsD.increment('api.vanotify.notifications.temporary_failure')
-          StatsD.increment('callbacks.cst_document_uploads.va_notify.notifications.temporary_failure')
-        else
-          StatsD.increment('api.vanotify.notifications.other')
-          StatsD.increment('callbacks.cst_document_uploads.va_notify.notifications.other')
         end
 
+        add_metrics(status, api_service_name)
         add_log(notification, es) if notification.status != 'delivered'
       end
 
@@ -48,6 +35,27 @@ module Lighthouse
         }
 
         Rails.logger.error(name, context)
+      end
+
+      def self.add_metrics(status, api_service_name)
+        case status
+        when 'delivered'
+          StatsD.increment('api.vanotify.notifications.delivered')
+          StatsD.increment('callbacks.cst_document_uploads.va_notify.notifications.delivered')
+          tags = ['service:claim-status', "function: #{api_service_name} - VA Notify evidence upload failure email"]
+          StatsD.increment('silent_failure_avoided', tags:)
+        when 'permanent-failure'
+          StatsD.increment('api.vanotify.notifications.permanent_failure')
+          StatsD.increment('callbacks.cst_document_uploads.va_notify.notifications.permanent_failure')
+          tags = ['service:claim-status', "function: #{api_service_name} - VA Notify evidence upload failure email"]
+          StatsD.increment('silent_failure', tags:)
+        when 'temporary-failure'
+          StatsD.increment('api.vanotify.notifications.temporary_failure')
+          StatsD.increment('callbacks.cst_document_uploads.va_notify.notifications.temporary_failure')
+        else
+          StatsD.increment('api.vanotify.notifications.other')
+          StatsD.increment('callbacks.cst_document_uploads.va_notify.notifications.other')
+        end
       end
 
       def self.get_api_service_name(job_class)
