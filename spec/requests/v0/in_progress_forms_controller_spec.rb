36e10c4e2566a12abcc0f1061280d02be797834a
Kevin Suarez <kjsuarez94@gmail.com>
2025-03-25 12:18:11 -0400

    102210 write specs for MDOT IPF

--- spec/requests/v0/in_progress_forms_controller_spec.rb
+++ spec/requests/v0/in_progress_forms_controller_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe V0::InProgressFormsController do
       allow(Flipper).to receive(:enabled?).with(:remove_pciu, instance_of(User)).and_return(false)
       enabled_forms = FormProfile.prefill_enabled_forms << 'FAKEFORM'
       allow(FormProfile).to receive(:prefill_enabled_forms).and_return(enabled_forms)
+      allow(FormProfile).to receive(:load_form_mapping).and_call_original
       allow(FormProfile).to receive(:load_form_mapping).with('FAKEFORM').and_return(
         'veteran_full_name' => %w[identity_information full_name],
         'gender' => %w[identity_information gender],
@@ -191,6 +192,33 @@ RSpec.describe V0::InProgressFormsController do
             expect(body['metadata']['howNow']['brown-cow']['-an eas-i-ly corRupted KEY.']).to be_present
             expect(body['metadata']).to eq in_progress_form.metadata
           end
+        end
+      end
+
+      context 'for an MDOT form sans addresses' do 
+        before do
+          allow(Flipper).to receive(:enabled?).with(:remove_pciu, instance_of(User)).and_return(true)
+        end
+        let(:user_details) do
+          {
+            first_name: 'Greg',
+            last_name: 'Anderson',
+            middle_name: 'A',
+            birth_date: '19910405',
+            ssn: '000550237'
+          }
+        end
+      
+        let(:user) { build(:user, :loa3, user_details) }
+        
+        it 'returns the form as JSON' do
+          VCR.insert_cassette(
+            'mdot/get_supplies_null_addresses_200',
+            match_requests_on: %i[method uri headers],
+            erb: { icn: user.icn }
+          )
+          get v0_in_progress_form_url("MDOT"), params: nil
+          expect(response).to have_http_status(:ok)
         end
       end
 
