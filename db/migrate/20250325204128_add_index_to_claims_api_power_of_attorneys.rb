3acf4139f919af4580b003d7bd4f6af4f5ea2d79
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2025-03-27 12:45:21 -0600

    Removes migration file.

--- db/migrate/20250325204128_add_index_to_claims_api_power_of_attorneys.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexToClaimsApiPowerOfAttorneys < ActiveRecord::Migration[7.2]
-  disable_ddl_transaction!
-
-  def change
-    add_index :claims_api_power_of_attorneys, :form_data_hash, algorithm: :concurrently
-  end
-end
