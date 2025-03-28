22e861a787f376559027c5cdcdbc384292698337
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-03-28 08:30:18 -0500

    Revert "105513 Add applicants to metadata - form 10-10d (#21458)" (#21476)
    This reverts commit ac4440fa08936da93258e47f6a5da07ca8daf373.

--- modules/ivc_champva/app/models/ivc_champva/vha_10_10d.rb
+++ modules/ivc_champva/app/models/ivc_champva/vha_10_10d.rb
@@ -37,17 +37,7 @@ module IvcChampva
         'primaryContactInfo' => @data['primary_contact_info'],
         'hasApplicantOver65' => @data['has_applicant_over65'].to_s,
         'primaryContactEmail' => @data.dig('primary_contact_info', 'email').to_s
-        # add individual properties for each applicant in the applicants array:
-      }.merge(add_applicant_properties)
-    end
-
-    def add_applicant_properties
-      applicants = @data['applicants']
-      return {} if applicants.blank?
-
-      applicants.each_with_index.with_object({}) do |(app, index), obj|
-        obj["applicant_#{index}"] = extract_applicant_properties(app)
-      end.deep_stringify_keys
+      }
     end
 
     def desired_stamps
@@ -131,10 +121,6 @@ module IvcChampva
       end
 
       stamps
-    end
-
-    def extract_applicant_properties(app)
-      app.symbolize_keys.slice(:applicant_ssn, :applicant_name, :applicant_dob)
     end
   end
 end
