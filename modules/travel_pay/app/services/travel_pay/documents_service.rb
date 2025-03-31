133ccf98684c306836f5bb5425fe1bd2af708ace
Liz Townsend <liz.townsend@adhocteam.us>
2025-03-31 14:08:32 -0500

    initial download docs

---
+++ modules/travel_pay/app/services/travel_pay/documents_service.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module TravelPay
+  class DocumentsService
+    def initialize(auth_manager)
+      @auth_manager = auth_manager
+    end
+
+    def get_document_summaries(claim_id)
+      @auth_manager.authorize => { veis_token:, btsss_token: }
+      documents_response = client.get_document_ids(veis_token, btsss_token, claim_id)
+      documents_response.body['data']
+    end
+
+    def download_document(params = {})
+      params => { claim_id:, doc_id: }
+
+      raise ArgumentError, message: "Missing claim ID or document ID, given: #{params}" unless claim_id && doc_id
+
+      @auth_manager.authorize => { veis_token:, btsss_token: }
+      client.get_document_binary(veis_token, btsss_token, params)
+
+      # {
+      #   data: raw_binary
+      # }
+    end
+
+    private
+
+    def client
+      TravelPay::DocumentsClient.new
+    end
+  end
+end
