18cc671375d7b84d9dbb7cb0ba6a335a284ab0bd
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2025-03-31 11:50:29 -0400

    parallel KMS call outs

--- modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
+++ modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/power_of_attorney_requests_controller.rb
@@ -20,22 +20,33 @@ module AccreditedRepresentativePortal
         status = params[:status].presence
         rel = policy_scope(PowerOfAttorneyRequest)
 
-        rel =
-          case status
-          when Statuses::PENDING
-            rel.unresolved.order(created_at: :asc)
-          when Statuses::PROCESSED
-            rel.resolved.not_expired.order('resolution.created_at DESC')
-          when NilClass
-            rel
-          else
-            raise ActionController::BadRequest, <<~MSG.squish
-              Invalid status parameter.
-              Must be one of (#{Statuses::ALL.join(', ')})
-            MSG
-          end
+        rel = filter_by_status(rel, status)
 
         poa_requests = rel.includes(scope_includes).limit(100)
+
+        # Parallel decrypt all encrypted fields
+        Parallel.map(poa_requests, in_threads: poa_requests.size) do |request|
+          request.tap do |r|
+            if r.power_of_attorney_form.present?
+              form = r.power_of_attorney_form
+              form.data                # Main form data
+              form.claimant_city      # Location fields
+              form.claimant_state_code
+              form.claimant_zip_code
+            end
+
+            if r.resolution.present?
+              r.resolution.reason     # Resolution reason
+            end
+
+            if r.form_submission.present?
+              sub = r.form_submission
+              sub.service_response    # Service response
+              sub.error_message      # Error details
+            end
+          end
+        end
+
         serializer = PowerOfAttorneyRequestSerializer.new(poa_requests)
 
         render json: serializer.serializable_hash, status: :ok
@@ -47,6 +58,22 @@ module AccreditedRepresentativePortal
       end
 
       private
+
+      def filter_by_status(rel, status)
+        case status
+        when Statuses::PENDING
+          rel.unresolved.order(created_at: :asc)
+        when Statuses::PROCESSED
+          rel.resolved.not_expired.order('resolution.created_at DESC')
+        when NilClass
+          rel
+        else
+          raise ActionController::BadRequest, <<~MSG.squish
+            Invalid status parameter.
+            Must be one of (#{Statuses::ALL.join(', ')})
+          MSG
+        end
+      end
 
       module Statuses
         ALL = [
