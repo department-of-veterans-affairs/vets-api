2fd711095d6c673d0dafd301f520ee01c6a67006
Mike Moyer <87040148+mmoyer-va@users.noreply.github.com>
2025-03-31 12:39:22 -0400

    MHV-68159 Move functionality to before(:each) to allow mocking

--- spec/lib/medical_records/bb_internal/client_spec.rb
+++ spec/lib/medical_records/bb_internal/client_spec.rb
@@ -7,19 +7,15 @@ require 'stringio'
 UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
 
 describe BBInternal::Client do
-  before(:all) do
+  before do
+    allow(Flipper).to receive(:enabled?).with(:mhv_medical_records_migrate_to_api_gateway).and_return(false)
     VCR.use_cassette 'mr_client/bb_internal/session_auth' do
-      allow(Flipper).to receive(:enabled?).with(:mhv_medical_records_migrate_to_api_gateway).and_return(false)
       @client ||= begin
         client = BBInternal::Client.new(session: { user_id: '11375034', icn: '1012740022V620959' })
         client.authenticate
         client
       end
     end
-  end
-
-  before do
-    allow(Flipper).to receive(:enabled?).with(:mhv_medical_records_migrate_to_api_gateway).and_return(false)
   end
 
   let(:client) { @client }
--- spec/lib/medical_records/client_spec.rb
+++ spec/lib/medical_records/client_spec.rb
@@ -6,7 +6,8 @@ require 'stringio'
 
 describe MedicalRecords::Client do
   context 'when a valid session exists', :vcr do
-    before(:all) do
+    before do
+      allow(Flipper).to receive(:enabled?).with(:mhv_medical_records_migrate_to_api_gateway).and_return(false)
       VCR.use_cassette('user_eligibility_client/perform_an_eligibility_check_for_premium_user',
                        match_requests_on: %i[method sm_user_ignoring_path_param]) do
         VCR.use_cassette 'mr_client/session' do
@@ -20,10 +21,7 @@ describe MedicalRecords::Client do
           end
         end
       end
-    end
 
-    before do
-      allow(Flipper).to receive(:enabled?).with(:mhv_medical_records_migrate_to_api_gateway).and_return(false)
       MedicalRecords::Client.send(:public, *MedicalRecords::Client.protected_instance_methods)
 
       # Redirect FHIR logger's output to the buffer before each test
