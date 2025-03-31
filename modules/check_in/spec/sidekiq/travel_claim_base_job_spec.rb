84ed715943d85cf202a26e3701f5161da88f63e7
PhilipDeFraties <philip.defraties@gmail.com>
2025-03-31 16:15:41 -0600

    update specs to properly test happy and sad paths

--- modules/check_in/spec/sidekiq/travel_claim_base_job_spec.rb
+++ modules/check_in/spec/sidekiq/travel_claim_base_job_spec.rb
@@ -18,6 +18,13 @@ RSpec.describe CheckIn::TravelClaimBaseJob do
     }
   end
 
+  describe 'retry configuration' do
+    it 'has MAX_RETRIES matching sidekiq_options retry setting' do
+      sidekiq_retry_value = described_class.sidekiq_options_hash['retry']
+      expect(described_class::MAX_RETRIES).to eq(sidekiq_retry_value)
+    end
+  end
+
   describe '#send_notification' do
     subject { described_class.new.send_notification(job_opts) }
 
@@ -26,50 +33,12 @@ RSpec.describe CheckIn::TravelClaimBaseJob do
       allow_any_instance_of(described_class).to receive(:logger).and_return(Logger.new(nil))
     end
 
-    it 'logs the notification and sends SMS' do
-      expect_any_instance_of(described_class).to receive(:log_sending_travel_claim_notification).with(job_opts)
-      expect_any_instance_of(described_class).to receive(:va_notify_send_sms).with(job_opts)
-      subject
-    end
-
-    context 'when an error occurs' do
-      before do
-        allow_any_instance_of(described_class).to receive(:va_notify_send_sms)
-          .and_raise(StandardError.new('Test error'))
-      end
-
-      it 'logs the failure and raises the error' do
-        expect_any_instance_of(described_class).to receive(:log_send_sms_failure)
-        expect { subject }.to raise_error(StandardError, 'Test error')
-      end
-    end
-
-    context 'when it exhausts retries' do
-      before do
-        allow_any_instance_of(described_class).to receive(:va_notify_send_sms)
-          .and_raise(StandardError.new('Test error'))
-      end
-
-      it 'tracks the failure' do
-        job_hash = { 'args' => [job_opts] }
-        CheckIn::TravelClaimBaseJob.within_sidekiq_retries_exhausted_block(job_hash) do
-          expect(StatsD).to receive(:increment).with(CheckIn::Constants::STATSD_NOTIFY_ERROR)
-        end
-      end
-    end
-  end
-
-  describe '#va_notify_send_sms' do
-    let(:notify_client) { instance_double(VaNotify::Service) }
-
-    before do
+    it 'successfully sends SMS notification via VaNotify service' do
+      notify_client = instance_double(VaNotify::Service)
       allow(VaNotify::Service).to receive(:new).and_return(notify_client)
-      allow(notify_client).to receive(:send_sms)
-      allow(Settings.vanotify.services.check_in).to receive(:api_key).and_return('test-api-key')
-    end
 
-    it 'formats the appointment date and sends SMS with OH sender ID' do
       formatted_date = DateTime.strptime(appointment_date, '%Y-%m-%d').to_date.strftime('%b %d')
+
 
       expect(notify_client).to receive(:send_sms).with(
         phone_number: mobile_phone,
@@ -78,24 +47,143 @@ RSpec.describe CheckIn::TravelClaimBaseJob do
         personalisation: { claim_number:, appt_date: formatted_date }
       )
 
-      described_class.new.send(:va_notify_send_sms, job_opts)
+      subject
     end
 
-    context 'with CIE facility type' do
-      let(:facility_type) { 'cie' }
+    context 'when an error occurs' do
+      before do
+        allow_any_instance_of(VaNotify::Service).to receive(:send_sms)
+          .and_raise(StandardError.new('Test error'))
+      end
 
-      it 'sends SMS with CIE sender ID' do
-        formatted_date = DateTime.strptime(appointment_date, '%Y-%m-%d').to_date.strftime('%b %d')
+      it 'logs failures with incrementing retry counts' do
+        job = described_class.new
 
-        expect(notify_client).to receive(:send_sms).with(
-          phone_number: mobile_phone,
-          template_id:,
-          sms_sender_id: CheckIn::Constants::CIE_SMS_SENDER_ID,
-          personalisation: { claim_number:, appt_date: formatted_date }
+        # first attempt
+        expect(job).to receive(:log_send_sms_failure).with(1)
+        expect { job.send_notification(job_opts) }.to raise_error(StandardError)
+
+        # mock the retry count
+        allow(job.class).to receive(:sidekiq_options_hash).and_return({ 'retry_count' => 1 })
+        expect(job).to receive(:log_send_sms_failure).with(2)
+        expect { job.send_notification(job_opts) }.to raise_error(StandardError)
+      end
+    end
+
+    context 'when retry mechanism is exhausted' do
+      before do
+        allow_any_instance_of(VaNotify::Service).to receive(:send_sms)
+          .and_raise(StandardError.new('Test error'))
+      end
+
+      it 'handles errors correctly when retries are exhausted' do
+        oh_job_opts = job_opts.merge(template_id: CheckIn::Constants::OH_FAILURE_TEMPLATE_ID)
+        error = StandardError.new('Test error')
+
+        job_hash = {
+          'args' => [oh_job_opts],
+          'error_message' => 'Test error'
+        }
+
+        expect(SentryLogging).to receive(:log_exception_to_sentry).with(
+          error,
+          hash_including(
+            phone_number: '0123',
+            template_id: CheckIn::Constants::OH_FAILURE_TEMPLATE_ID,
+            claim_number: claim_number
+          ),
+          hash_including(error: :check_in_va_notify_job, team: 'check-in')
         )
 
-        described_class.new.send(:va_notify_send_sms, job_opts)
+        expect(StatsD).to receive(:increment).with(
+          CheckIn::Constants::STATSD_NOTIFY_SILENT_FAILURE,
+          tags: CheckIn::Constants::STATSD_OH_SILENT_FAILURE_TAGS
+        )
+
+        expect(StatsD).to receive(:increment).with(CheckIn::Constants::STATSD_NOTIFY_ERROR)
+
+        described_class.sidekiq_retries_exhausted_block.call(job_hash, error)
       end
+
+      it 'handles different facility types properly when retries are exhausted' do
+        cie_job_opts = job_opts.merge(
+          template_id: CheckIn::Constants::CIE_FAILURE_TEMPLATE_ID,
+          facility_type: 'cie'
+        )
+        job_hash = { 'args' => [cie_job_opts] }
+        error = StandardError.new('Test error')
+
+        expect(StatsD).to receive(:increment).with(
+          CheckIn::Constants::STATSD_NOTIFY_SILENT_FAILURE,
+          tags: CheckIn::Constants::STATSD_CIE_SILENT_FAILURE_TAGS
+        )
+        expect(StatsD).to receive(:increment).with(CheckIn::Constants::STATSD_NOTIFY_ERROR)
+
+        described_class.handle_error(job_hash, error)
+
+        regular_job_opts = job_opts.merge(template_id: 'regular-template-id')
+        job_hash = { 'args' => [regular_job_opts] }
+
+        expect(StatsD).not_to receive(:increment).with(
+          CheckIn::Constants::STATSD_NOTIFY_SILENT_FAILURE,
+          any_args
+        )
+        expect(StatsD).to receive(:increment).with(CheckIn::Constants::STATSD_NOTIFY_ERROR)
+
+        described_class.handle_error(job_hash, error)
+      end
+
+      it 'demonstrates the complete failure flow in an integrated way' do
+        job = described_class.new
+        allow(job.class).to receive(:sidekiq_options_hash).and_return({
+                                                                        'retry' => described_class::MAX_RETRIES,
+                                                                        'retry_count' => described_class::MAX_RETRIES - 1
+                                                                      })
+
+        oh_job_opts = job_opts.merge(template_id: CheckIn::Constants::OH_FAILURE_TEMPLATE_ID)
+        expect(job).to receive(:log_send_sms_failure).with(described_class::MAX_RETRIES)
+        expect { job.send_notification(oh_job_opts) }.to raise_error(StandardError)
+        job_hash = {
+          'args' => [oh_job_opts],
+          'error_message' => 'Test error',
+          'retry_count' => described_class::MAX_RETRIES
+        }
+
+        described_class.sidekiq_retries_exhausted_block.call(job_hash, StandardError.new('Test error'))
+      end
+    end
+  end
+
+  describe 'phone number and date formatting' do
+    let(:notify_client) { instance_double(VaNotify::Service) }
+
+    before do
+      allow(VaNotify::Service).to receive(:new).and_return(notify_client)
+      allow(notify_client).to receive(:send_sms)
+      allow(Settings.vanotify.services.check_in).to receive(:api_key).and_return('test-api-key')
+    end
+
+    it 'formats the appointment date and uses appropriate SMS sender ID based on facility type' do
+      formatted_date = DateTime.strptime(appointment_date, '%Y-%m-%d').to_date.strftime('%b %d')
+
+      # Test OH sender ID
+      expect(notify_client).to receive(:send_sms).with(
+        phone_number: mobile_phone,
+        template_id:,
+        sms_sender_id: CheckIn::Constants::OH_SMS_SENDER_ID,
+        personalisation: { claim_number:, appt_date: formatted_date }
+      )
+      described_class.new.send(:va_notify_send_sms, job_opts)
+
+      # Test CIE sender ID
+      cie_opts = job_opts.merge(facility_type: 'cie')
+      expect(notify_client).to receive(:send_sms).with(
+        phone_number: mobile_phone,
+        template_id:,
+        sms_sender_id: CheckIn::Constants::CIE_SMS_SENDER_ID,
+        personalisation: { claim_number:, appt_date: formatted_date }
+      )
+      described_class.new.send(:va_notify_send_sms, cie_opts)
     end
   end
 end
