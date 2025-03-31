9fbd5aceaf828808f528cb6b25e7acf3696c3c4d
Ari Perez <ari@coa.solutions>
2025-03-31 14:19:44 -0400

    fix robocop issues

--- app/sidekiq/form0781_state_snapshot_job.rb
+++ app/sidekiq/form0781_state_snapshot_job.rb
@@ -36,58 +36,115 @@ class Form0781StateSnapshotJob
   def load_snapshot_state
     {
       # New 0781 form metrics
-      in_progress_new_0781: InProgressForm.where(form_id: '21-526EZ')
-        .select { |ipf| JSON.parse(ipf.form_data)['view:mental_health_workflow_choice'] == 'optForOnlineForm0781' }
-        .map(&:id).sort,
-      
-      submissions_new_0781: Form526Submission
-        .select { |sub| JSON.parse(sub.form_to_json(Form526Submission::FORM_0781))&.keys&.include?('form0781v2') }
-        .map(&:id).sort,
-      
-      successful_submissions_new_0781: Form526Submission
-        .select do |sub|
-          next unless JSON.parse(sub.form_to_json(Form526Submission::FORM_0781))&.keys&.include?('form0781v2')
-          job_statuses = sub.form526_job_statuses.where(job_class: 'SubmitForm0781')
-          job_statuses.first&.success?
-        end.map(&:id).sort,
-      
-      failed_submissions_new_0781: Form526Submission
-        .select do |sub|
-          next unless JSON.parse(sub.form_to_json(Form526Submission::FORM_0781))&.keys&.include?('form0781v2')
-          job_statuses = sub.form526_job_statuses.where(job_class: 'SubmitForm0781')
-          Form526JobStatus::FAILURE_STATUSES.include?(job_statuses.first&.status)
-        end.map(&:id).sort,
-      
-      primary_path_submissions_new_0781: Form526Submission.where.not(submitted_claim_id: nil)
-        .select { |sub| JSON.parse(sub.form_to_json(Form526Submission::FORM_0781))&.keys&.include?('form0781v2') }
-        .map(&:id).sort,
-      
-      secondary_path_submissions_new_0781: Form526Submission.where(submitted_claim_id: nil)
-        .select { |sub| JSON.parse(sub.form_to_json(Form526Submission::FORM_0781))&.keys&.include?('form0781v2') }
-        .map(&:id).sort,
-      
+      in_progress_new_0781_forms: new_0781_in_progress_forms,
+      submissions_new_0781_forms: new_0781_submissions,
+      successful_submissions_new_0781_forms: new_0781_successful_submissions,
+      failed_submissions_new_0781_forms: new_0781_failed_submissions,
+      primary_path_submissions_new_0781_forms: new_0781_primary_path_submissions,
+      secondary_path_submissions_new_0781_forms: new_0781_secondary_path_submissions,
+
       # Old 0781 form metrics
-      in_progress_old_0781: InProgressForm.where(form_id: '21-526EZ')
-        .select { |ipf| !JSON.parse(ipf.form_data)['view:selectable_ptsd_types']&.values&.all?(false) }
-        .map(&:id).sort,
-      
-      submissions_old_0781: Form526Submission
-        .select { |sub| !JSON.parse(sub.form_to_json(Form526Submission::FORM_0781))&.keys&.include?('form0781v2') }
-        .map(&:id).sort,
-      
-      successful_submissions_old_0781: Form526Submission
-        .select do |sub|
-          next if JSON.parse(sub.form_to_json(Form526Submission::FORM_0781))&.keys&.include?('form0781v2')
-          job_statuses = sub.form526_job_statuses.where(job_class: 'SubmitForm0781')
-          job_statuses.first&.success?
-        end.map(&:id).sort,
-      
-      failed_submissions_old_0781: Form526Submission
-        .select do |sub|
-          next if JSON.parse(sub.form_to_json(Form526Submission::FORM_0781))&.keys&.include?('form0781v2')
-          job_statuses = sub.form526_job_statuses.where(job_class: 'SubmitForm0781')
-          Form526JobStatus::FAILURE_STATUSES.include?(job_statuses.first&.status)
-        end.map(&:id).sort
+      in_progress_old_0781_forms: old_0781_in_progress_forms,
+      submissions_old_0781_forms: old_0781_submissions,
+      successful_submissions_old_0781_forms: old_0781_successful_submissions,
+      failed_submissions_old_0781_forms: old_0781_failed_submissions
     }
+  end
+
+  # Helper methods for new 0781 form metrics
+  def new_0781_in_progress_forms
+    InProgressForm.where(form_id: '21-526EZ')
+                  .select { |ipf| new_mental_health_workflow?(ipf) }
+                  .map(&:id).sort
+  end
+
+  def new_mental_health_workflow?(ipf)
+    JSON.parse(ipf.form_data)['view:mental_health_workflow_choice'] == 'optForOnlineForm0781'
+  end
+
+  def new_0781_submissions
+    Form526Submission
+      .select { |sub| new_0781_form?(sub) }
+      .map(&:id).sort
+  end
+
+  def new_0781_successful_submissions
+    Form526Submission
+      .select do |sub|
+        next unless new_0781_form?(sub)
+
+        form0781_job_success?(sub)
+      end.map(&:id).sort
+  end
+
+  def new_0781_failed_submissions
+    Form526Submission
+      .select do |sub|
+        next unless new_0781_form?(sub)
+
+        form0781_job_failure?(sub)
+      end.map(&:id).sort
+  end
+
+  def new_0781_primary_path_submissions
+    Form526Submission.where.not(submitted_claim_id: nil)
+                     .select { |sub| new_0781_form?(sub) }
+                     .map(&:id).sort
+  end
+
+  def new_0781_secondary_path_submissions
+    Form526Submission.where(submitted_claim_id: nil)
+                     .select { |sub| new_0781_form?(sub) }
+                     .map(&:id).sort
+  end
+
+  # Helper methods for old 0781 form metrics
+  def old_0781_in_progress_forms
+    InProgressForm.where(form_id: '21-526EZ')
+                  .select { |ipf| old_ptsd_types_selected?(ipf) }
+                  .map(&:id).sort
+  end
+
+  def old_ptsd_types_selected?(ipf)
+    !JSON.parse(ipf.form_data)['view:selectable_ptsd_types']&.values&.all?(false)
+  end
+
+  def old_0781_submissions
+    Form526Submission
+      .reject { |sub| new_0781_form?(sub) }
+      .map(&:id).sort
+  end
+
+  def old_0781_successful_submissions
+    Form526Submission
+      .select do |sub|
+        next if new_0781_form?(sub)
+
+        form0781_job_success?(sub)
+      end.map(&:id).sort
+  end
+
+  def old_0781_failed_submissions
+    Form526Submission
+      .select do |sub|
+        next if new_0781_form?(sub)
+
+        form0781_job_failure?(sub)
+      end.map(&:id).sort
+  end
+
+  # Common helper methods
+  def new_0781_form?(submission)
+    JSON.parse(submission.form_to_json(Form526Submission::FORM_0781))&.keys&.include?('form0781v2')
+  end
+
+  def form0781_job_success?(submission)
+    job_statuses = submission.form526_job_statuses.where(job_class: 'SubmitForm0781')
+    job_statuses.first&.success?
+  end
+
+  def form0781_job_failure?(submission)
+    job_statuses = submission.form526_job_statuses.where(job_class: 'SubmitForm0781')
+    Form526JobStatus::FAILURE_STATUSES.include?(job_statuses.first&.status)
   end
 end
--- spec/sidekiq/form0781_state_snapshot_job_spec.rb
+++ spec/sidekiq/form0781_state_snapshot_job_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Form0781StateSnapshotJob, type: :worker do
 
   describe '0781 state logging' do
     # Create test data for new 0781 forms
-    let!(:in_progress_new_0781) do
+    let!(:in_progress_new_form) do
       Timecop.freeze(modern_times) do
         ipf = create(:in_progress_form, form_id: '21-526EZ')
         form_data = JSON.parse(ipf.form_data)
@@ -35,7 +35,7 @@ RSpec.describe Form0781StateSnapshotJob, type: :worker do
       Timecop.freeze(modern_times) do
         sub = create(:form526_submission, :with_one_succesful_job)
         allow(sub).to receive(:form_to_json).with(Form526Submission::FORM_0781).and_return('{"form0781v2": {}}')
-        allow(sub.form526_job_statuses.first).to receive(:job_class).and_return('SubmitForm0781')
+        allow(sub.form526_job_statuses.first).to receive_messages(job_class: 'SubmitForm0781')
         sub
       end
     end
@@ -44,8 +44,10 @@ RSpec.describe Form0781StateSnapshotJob, type: :worker do
       Timecop.freeze(modern_times) do
         sub = create(:form526_submission, :with_one_failed_job)
         allow(sub).to receive(:form_to_json).with(Form526Submission::FORM_0781).and_return('{"form0781v2": {}}')
-        allow(sub.form526_job_statuses.first).to receive(:job_class).and_return('SubmitForm0781')
-        allow(sub.form526_job_statuses.first).to receive(:status).and_return('non_retryable_error')
+        allow(sub.form526_job_statuses.first).to receive_messages(
+          job_class: 'SubmitForm0781',
+          status: 'non_retryable_error'
+        )
         sub
       end
     end
@@ -67,7 +69,7 @@ RSpec.describe Form0781StateSnapshotJob, type: :worker do
     end
 
     # Create test data for old 0781 forms
-    let!(:in_progress_old_0781) do
+    let!(:in_progress_old_form) do
       Timecop.freeze(olden_times) do
         ipf = create(:in_progress_form, form_id: '21-526EZ')
         form_data = JSON.parse(ipf.form_data)
@@ -89,7 +91,7 @@ RSpec.describe Form0781StateSnapshotJob, type: :worker do
       Timecop.freeze(olden_times) do
         sub = create(:form526_submission, :with_one_succesful_job)
         allow(sub).to receive(:form_to_json).with(Form526Submission::FORM_0781).and_return('{"form0781": {}}')
-        allow(sub.form526_job_statuses.first).to receive(:job_class).and_return('SubmitForm0781')
+        allow(sub.form526_job_statuses.first).to receive_messages(job_class: 'SubmitForm0781')
         sub
       end
     end
@@ -98,8 +100,10 @@ RSpec.describe Form0781StateSnapshotJob, type: :worker do
       Timecop.freeze(olden_times) do
         sub = create(:form526_submission, :with_one_failed_job)
         allow(sub).to receive(:form_to_json).with(Form526Submission::FORM_0781).and_return('{"form0781": {}}')
-        allow(sub.form526_job_statuses.first).to receive(:job_class).and_return('SubmitForm0781')
-        allow(sub.form526_job_statuses.first).to receive(:status).and_return('non_retryable_error')
+        allow(sub.form526_job_statuses.first).to receive_messages(
+          job_class: 'SubmitForm0781',
+          status: 'non_retryable_error'
+        )
         sub
       end
     end
@@ -107,31 +111,31 @@ RSpec.describe Form0781StateSnapshotJob, type: :worker do
     it 'logs 0781 state metrics correctly' do
       # Instead of trying to mock all the complex ActiveRecord chains, let's just mock the result
       expected_log = {
-        in_progress_new_0781: [in_progress_new_0781.id].sort,
-        submissions_new_0781: [
+        in_progress_new_0781_forms: [in_progress_new_form.id].sort,
+        submissions_new_0781_forms: [
           new_0781_submission.id,
           new_0781_successful_submission.id,
           new_0781_failed_submission.id,
           new_0781_primary_path.id,
           new_0781_secondary_path.id
         ].sort,
-        successful_submissions_new_0781: [new_0781_successful_submission.id].sort,
-        failed_submissions_new_0781: [new_0781_failed_submission.id].sort,
-        primary_path_submissions_new_0781: [new_0781_primary_path.id].sort,
-        secondary_path_submissions_new_0781: [
+        successful_submissions_new_0781_forms: [new_0781_successful_submission.id].sort,
+        failed_submissions_new_0781_forms: [new_0781_failed_submission.id].sort,
+        primary_path_submissions_new_0781_forms: [new_0781_primary_path.id].sort,
+        secondary_path_submissions_new_0781_forms: [
           new_0781_submission.id,
           new_0781_secondary_path.id
         ].sort,
-        in_progress_old_0781: [in_progress_old_0781.id].sort,
-        submissions_old_0781: [
+        in_progress_old_0781_forms: [in_progress_old_form.id].sort,
+        submissions_old_0781_forms: [
           old_0781_submission.id,
           old_0781_successful_submission.id,
           old_0781_failed_submission.id
         ].sort,
-        successful_submissions_old_0781: [old_0781_successful_submission.id].sort,
-        failed_submissions_old_0781: [old_0781_failed_submission.id].sort
+        successful_submissions_old_0781_forms: [old_0781_successful_submission.id].sort,
+        failed_submissions_old_0781_forms: [old_0781_failed_submission.id].sort
       }
-      
+
       # Mock the load_snapshot_state method to return our expected data
       allow_any_instance_of(described_class).to receive(:load_snapshot_state).and_return(expected_log)
 
@@ -143,32 +147,32 @@ RSpec.describe Form0781StateSnapshotJob, type: :worker do
 
       # Create a mock snapshot_state result
       mock_snapshot = {
-        in_progress_new_0781: [1],
-        submissions_new_0781: [1, 2, 3, 4, 5],
-        successful_submissions_new_0781: [2],
-        failed_submissions_new_0781: [3],
-        primary_path_submissions_new_0781: [4],
-        secondary_path_submissions_new_0781: [1, 5],
-        in_progress_old_0781: [6],
-        submissions_old_0781: [7, 8, 9],
-        successful_submissions_old_0781: [8],
-        failed_submissions_old_0781: [9]
+        in_progress_new_0781_forms: [1],
+        submissions_new_0781_forms: [1, 2, 3, 4, 5],
+        successful_submissions_new_0781_forms: [2],
+        failed_submissions_new_0781_forms: [3],
+        primary_path_submissions_new_0781_forms: [4],
+        secondary_path_submissions_new_0781_forms: [1, 5],
+        in_progress_old_0781_forms: [6],
+        submissions_old_0781_forms: [7, 8, 9],
+        successful_submissions_old_0781_forms: [8],
+        failed_submissions_old_0781_forms: [9]
       }
-      
+
       # Stub the snapshot_state method to return our mock data
       allow_any_instance_of(described_class).to receive(:snapshot_state).and_return(mock_snapshot)
 
       # Expect StatsD.gauge to be called for each metric with the correct count
-      expect(StatsD).to receive(:gauge).with("#{prefix}.in_progress_new_0781_count", 1)
-      expect(StatsD).to receive(:gauge).with("#{prefix}.submissions_new_0781_count", 5)
-      expect(StatsD).to receive(:gauge).with("#{prefix}.successful_submissions_new_0781_count", 1)
-      expect(StatsD).to receive(:gauge).with("#{prefix}.failed_submissions_new_0781_count", 1)
-      expect(StatsD).to receive(:gauge).with("#{prefix}.primary_path_submissions_new_0781_count", 1)
-      expect(StatsD).to receive(:gauge).with("#{prefix}.secondary_path_submissions_new_0781_count", 2)
-      expect(StatsD).to receive(:gauge).with("#{prefix}.in_progress_old_0781_count", 1)
-      expect(StatsD).to receive(:gauge).with("#{prefix}.submissions_old_0781_count", 3)
-      expect(StatsD).to receive(:gauge).with("#{prefix}.successful_submissions_old_0781_count", 1)
-      expect(StatsD).to receive(:gauge).with("#{prefix}.failed_submissions_old_0781_count", 1)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.in_progress_new_0781_forms_count", 1)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.submissions_new_0781_forms_count", 5)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.successful_submissions_new_0781_forms_count", 1)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.failed_submissions_new_0781_forms_count", 1)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.primary_path_submissions_new_0781_forms_count", 1)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.secondary_path_submissions_new_0781_forms_count", 2)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.in_progress_old_0781_forms_count", 1)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.submissions_old_0781_forms_count", 3)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.successful_submissions_old_0781_forms_count", 1)
+      expect(StatsD).to receive(:gauge).with("#{prefix}.failed_submissions_old_0781_forms_count", 1)
 
       described_class.new.perform
     end
