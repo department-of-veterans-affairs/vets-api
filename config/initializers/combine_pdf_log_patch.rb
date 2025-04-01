a950f90c1cf0c9d02fb9e68f14636f757c8baaa6
Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>
2025-04-01 16:25:17 -0400

    load log_patch after initialization

--- config/initializers/combine_pdf_log_patch.rb
+++ config/initializers/combine_pdf_log_patch.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
-require 'combine_pdf/parser'
-
 # Suppresses CombinePDF warning "Couldn't connect reference for" and "Form data might be lost"
 # more info: https://github.com/department-of-veterans-affairs/vets-api/pull/16705
 
-if Rails.env.test?
-  CombinePDF::PDFParser.prepend(Module.new do
-    def warn(*msgs, **)
-      msgs.reject! { |msg| msg.start_with?("Couldn't connect reference for") }
-      super
-    end
-  end)
+Rails.application.config.after_initialize do
+  if Rails.env.test?
+    CombinePDF::PDFParser.prepend(Module.new do
+      def warn(*msgs, **)
+        msgs.reject! { |msg| msg.start_with?("Couldn't connect reference for") }
+        super
+      end
+    end)
 
-  CombinePDF::PDF.prepend(Module.new do
-    def warn(*msgs, **)
-      msgs.reject! { |msg| msg.start_with?('Form data might be lost') }
-      super
-    end
-  end)
+    CombinePDF::PDF.prepend(Module.new do
+      def warn(*msgs, **)
+        msgs.reject! { |msg| msg.start_with?('Form data might be lost') }
+        super
+      end
+    end)
+  end
 end
