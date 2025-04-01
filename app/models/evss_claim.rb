eafc41dbb40f33f74afb4ea218768e8c4864ad5d
Alex Jolovic <alexander.jolovic@bylight.com>
2025-04-01 11:46:13 -0700

    Add user_account association to EVSSClaim - Add optional belongs_to association with user_account - Add validations for user_uuid and data - Add for_user and claim_for_user_account class methods - Add comprehensive test coverage for associations and validations - Part of delegate user support work

--- app/models/evss_claim.rb
+++ app/models/evss_claim.rb
@@ -3,21 +3,17 @@
 require 'evss/documents_service'
 
 class EVSSClaim < ApplicationRecord
-  belongs_to :user_account, dependent: nil, optional: true
+  belongs_to :user_account, optional: true
+  validates :user_uuid, presence: true
+  validates :data, presence: true
 
   def self.for_user(user)
-    claim_for_user_uuid(user.uuid).or(claim_for_user_account(user.user_account))
-  end
-
-  def self.claim_for_user_uuid(user_uuid)
-    return EVSSClaim.none unless user_uuid
-
-    where(user_uuid:)
+    return EVSSClaim.none unless user&.user_account
+    where(user_account: user.user_account)
   end
 
   def self.claim_for_user_account(user_account)
-    return EVSSClaim.none unless user_account
-
+    return EVSSClaim.none if user_account.nil?
     where(user_account:)
   end
 end
---
+++ spec/models/evss_claim_spec.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe EVSSClaim, type: :model do
+  let(:user) { create(:user, :accountable) }
+  let(:user_account) { create(:user_account) }
+  let(:user_verification) { create(:user_verification, user_account:) }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:user_account).optional }
+  end
+
+  describe '.for_user' do
+    context 'when user has a user_account' do
+      it 'returns claims associated with the user account' do
+        user_account = create(:user_account)
+        create(:user_verification, idme_uuid: user.idme_uuid, user_account: user_account)
+        claim = create(:evss_claim, user_uuid: user.uuid, user_account: user_account)
+        other_claim = create(:evss_claim, :with_user_account)
+
+        expect(EVSSClaim.for_user(user)).to include(claim)
+        expect(EVSSClaim.for_user(user)).not_to include(other_claim)
+      end
+    end
+
+    context 'when user has no user_account' do
+      it 'returns no claims' do
+        create(:evss_claim, :with_user_account)
+        allow(user).to receive(:user_account).and_return(nil)
+
+        expect(EVSSClaim.for_user(user)).to be_empty
+      end
+    end
+  end
+
+  describe '.claim_for_user_account' do
+    it 'returns claims for the given user account' do
+      claim = create(:evss_claim, :with_user_account, user_account:)
+      other_claim = create(:evss_claim, :with_user_account)
+
+      expect(EVSSClaim.claim_for_user_account(user_account)).to include(claim)
+      expect(EVSSClaim.claim_for_user_account(user_account)).not_to include(other_claim)
+    end
+
+    it 'returns no claims when user_account is nil' do
+      create(:evss_claim, :with_user_account)
+      expect(EVSSClaim.claim_for_user_account(nil)).to be_empty
+    end
+  end
+
+  describe 'user_account association' do
+    it 'allows creating claims with or without user_account' do
+      claim_without_account = EVSSClaim.create(
+        user_uuid: user.uuid,
+        evss_id: 123,
+        data: { 'status' => 'PENDING' }
+      )
+      expect(claim_without_account).to be_valid
+
+      claim_with_account = EVSSClaim.create(
+        user_uuid: user.uuid,
+        evss_id: 124,
+        user_account: user.user_account,
+        data: { 'status' => 'PENDING' }
+      )
+      expect(claim_with_account).to be_valid
+    end
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:user_uuid) }
+    it { is_expected.to validate_presence_of(:data) }
+
+    it 'requires user_uuid' do
+      claim = EVSSClaim.new(data: { 'status' => 'PENDING' })
+      expect(claim).not_to be_valid
+      expect(claim.errors[:user_uuid]).to include("can't be blank")
+    end
+
+    it 'requires data' do
+      claim = EVSSClaim.new(user_uuid: user.uuid)
+      expect(claim).not_to be_valid
+      expect(claim.errors[:data]).to include("can't be blank")
+    end
+  end
+end
