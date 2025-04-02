dd7f68e519f537ac707ea314caeca2359fcdca90
Lindsey Hattamer <Lindsey.Hattamer@oddball.io>
2025-04-02 14:59:13 -0400

    avoid autoloading load ordering error

--- config/initializers/combine_pdf_log_patch.rb
+++ config/initializers/combine_pdf_log_patch.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'combine_pdf/api'
-
 # Suppresses CombinePDF warning "Couldn't connect reference for" and "Form data might be lost"
 # more info: https://github.com/department-of-veterans-affairs/vets-api/pull/16705
 
 Rails.application.config.after_initialize do
+  require 'combine_pdf'
+
   if Rails.env.test?
     CombinePDF::PDFParser.prepend(Module.new do
       def warn(*msgs, **)
