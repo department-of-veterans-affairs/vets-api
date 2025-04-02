c46f71cb11e7f10c35756d90b74597191e2ded41
Alex Jolovic <alexander.jolovic@bylight.com>
2025-04-02 12:29:02 -0700

    [VI-1104] Remove legacy MHV routes - Remove legacy prescriptions, health records, and messaging routes replaced by MyHealth module - Part of the MHV logging refactor for delegate users

--- app/policies/legacy_mhv_messaging_policy.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# TODO: migrate all services off this policy and onto the new one.
-LegacyMHVMessagingPolicy = Struct.new(:user, :legacy_mhv_messaging) do
-  SM_ACCOUNT_TYPES = %w[Premium].freeze
-  def access?
-    SM_ACCOUNT_TYPES.include?(user.mhv_account_type) && user.va_patient?
-  end
-end
