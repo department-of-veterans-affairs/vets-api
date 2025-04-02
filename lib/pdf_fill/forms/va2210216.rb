6695f3ffc7e39e6b9295220620ee31c8747029ca
Alex Chan <172081065+alexchan-va@users.noreply.github.com>
2025-04-02 13:26:02 -0400

    Finalize pdf generation for 10216

--- lib/pdf_fill/forms/va2210216.rb
+++ lib/pdf_fill/forms/va2210216.rb
@@ -10,21 +10,21 @@ module PdfFill
       KEY = {
         'institutionDetails' => {
           'institutionName' => {
-            key: 'F[0].#subform[1].INSTITUTION_NAME[0]',
+            key: 'INSTITUTION_NAME[0].#subform[1].F[0]',
             limit: 50,
             question_num: 1,
             question_suffix: 'A',
             question_text: 'INSTITUTION NAME'
           },
           'facilityCode' => {
-            key: 'F[0].#subform[1].FACILITY_CODE[0]',
+            key: 'FACILITY_CODE[0].#subform[1].F[0]',
             limit: 8,
             question_num: 2,
             question_suffix: 'A',
             question_text: 'FACILITY CODE'
           },
           'termStartDate' => {
-            key: 'F[0].#subform[1].TERM_START_DATE[0]',
+            key: 'TERM_START_DATE[0].#subform[1].F[0]',
             limit: 14,
             question_num: 3,
             question_suffix: 'C',
@@ -33,28 +33,28 @@ module PdfFill
         },
         'studentRatioCalcChapter' => {
           'beneficiaryStudent' => {
-            key: 'F[0].#subform[1].VA_BENEFICIARY_STUDENTS[0]',
+            key: 'VA_BENEFICIARY_STUDENTS[0].#subform[1].F[0]',
             limit: 10,
             question_num: 4,
             question_suffix: 'A',
             question_text: 'NUMBER OF VA BENEFICIARY STUDENTS'
           },
           'numOfStudent' => {
-            key: 'F[0].#subform[1].TOTAL_NUMBER_OF_STUDENTS[0]',
+            key: 'TOTAL_NUMBER_OF_STUDENTS[0].#subform[1].F[0]',
             limit: 10,
             question_num: 5,
             question_suffix: 'A',
             question_text: 'TOTAL NUMBER OF STUDENTS'
           },
           'VABeneficiaryStudentsPercentage' => {
-            key: 'F[0].#subform[1].VA_BENEFICIARY_STUDENTS_BENEFICIARY[0]',
+            key: 'VA_BENEFICIARY_STUDENTS_BENEFICIARY[0].#subform[1].F[0]',
             limit: 10,
             question_num: 6,
             question_suffix: 'A',
             question_text: 'VA BENEFICIARY STUDENTS PERCENTAGE'
           },
           'dateOfCalculation' => {
-            key: 'F[0].#subform[1].Date_Of_Calculation[0]',
+            key: 'Date_Of_Calculation[0].#subform[1].F[0]',
             limit: 20,
             question_num: 7,
             question_suffix: 'C',
@@ -63,14 +63,14 @@ module PdfFill
         },
         'certifyingOfficial' => {
           'fullName' => {
-            key: 'F[0].#subform[1].SCHOOL_OFFICIAL_PRINTED_NAME[0]',
+            key: 'SCHOOL_OFFICIAL_PRINTED_NAME[0].#subform[1].F[0]',
             limit: 50,
             question_num: 8,
             question_suffix: 'A',
             question_text: 'CERTIFYING OFFICIAL NAME'
           },
           'title' => {
-            key: 'F[0].#subform[1].SCHOOL_OFFICIAL_TITLE[0]',
+            key: 'SCHOOL_OFFICIAL_TITLE[0].#subform[1].F[0]',
             limit: 30,
             question_num: 9,
             question_suffix: 'A',
@@ -78,14 +78,14 @@ module PdfFill
           }
         },
         'statementOfTruthSignature' => {
-          key: 'F[0].#subform[1].SCHOOL_OFFICIAL_PRINTED_NAME[0]',
+          key: 'SCHOOL_OFFICIAL_SIGNATURE[0].#subform[1].F[0]',
           limit: 50,
           question_num: 10,
           question_suffix: 'A',
           question_text: 'STATEMENT OF TRUTH SIGNATURE'
         },
         'dateSigned' => {
-          key: 'F[0].#subform[1].Date_Signed[0]',
+          key: 'Date_Signed[0].#subform[1].F[0]',
           limit: 10,
           question_num: 11,
           question_suffix: 'A',
