6934f933bb2703cdf05266e3a2f650d37e00970e
PhilipDeFraties <philip.defraties@gmail.com>
2025-04-03 01:56:50 -0600

    add RDOCs

--- modules/check_in/app/sidekiq/check_in/travel_claim_notification_job.rb
+++ modules/check_in/app/sidekiq/check_in/travel_claim_notification_job.rb
@@ -1,20 +1,48 @@
 # frozen_string_literal: true
 
 module CheckIn
+  # Sidekiq job responsible for sending SMS notifications related to travel claims.
+  # This job handles sending text messages to users via VaNotify service,
+  # including error handling, retries, and logging.
+  #
+  # @example Enqueue the job with required parameters
+  #   CheckIn::TravelClaimNotificationJob.perform_async({
+  #     mobile_phone: '202-555-0123',
+  #     appointment_date: '2023-05-15',
+  #     template_id: 'template-id-123',
+  #     claim_number: '1234',
+  #     facility_type: 'oh'
+  #   })
   class TravelClaimNotificationJob < TravelClaimBaseJob
+    # Maximum number of retry attempts before the job is considered exhausted
     MAX_RETRIES = 12
     sidekiq_options retry: MAX_RETRIES
 
+    REQUIRED_FIELDS = %i[mobile_phone template_id appointment_date claim_number].freeze
+
+    # Performs the job of sending an SMS notification via VaNotify
+    #
+    # Validates input parameters, logs the attempt, and handles success/failure
+    # metrics. Returns early if required parameters like mobile_phone are missing.
+    #
+    # @param opts [Hash] Options hash containing parameters for the notification
+    # @option opts [String] :mobile_phone The phone number to send the SMS to
+    # @option opts [String] :appointment_date The appointment date in YYYY-MM-DD format
+    # @option opts [String] :template_id The VaNotify template ID to use
+    # @option opts [String] :claim_number The claim number to include in the message
+    # @option opts [String] :facility_type The facility type ('oh' or 'cie')
+    # @return [void]
     def perform(opts = {})
+      return unless validate_required_fields(opts)
+
+      parsed_date = parse_appointment_date(opts[:appointment_date])
+      return unless parsed_date
+
       log_sending_travel_claim_notification(opts)
-      retry_attempt = 0
-      if self.class.sidekiq_options_hash&.[]('retry_count')
-        retry_attempt = self.class.sidekiq_options_hash['retry_count'].to_i
-      end
-      attempt_number = retry_attempt + 1
+      attempt_number = current_attempt_number
 
       begin
-        va_notify_send_sms(opts)
+        va_notify_send_sms(opts, parsed_date)
         StatsD.increment(Constants::STATSD_NOTIFY_SUCCESS)
       rescue => e
         log_send_sms_failure(attempt_number)
@@ -23,10 +51,19 @@ module CheckIn
       end
     end
 
+    # Callback executed when all retries are exhausted
+    # @param job [Hash] The Sidekiq job hash containing job metadata
+    # @param ex [Exception] The exception that caused the job to fail
     sidekiq_retries_exhausted do |job, ex|
       CheckIn::TravelClaimNotificationJob.handle_error(job, ex)
     end
 
+    # Handles errors after all retries have been exhausted
+    # Logs the error to Sentry and updates metrics based on the template type and facility
+    #
+    # @param job [Hash] The Sidekiq job hash containing job metadata
+    # @param ex [Exception] The exception that caused the job to fail
+    # @return [void]
     def self.handle_error(job, ex)
       opts = job.dig('args', 0) || {}
 
@@ -34,14 +71,14 @@ module CheckIn
         ex,
         {
           phone_number: phone_last_four(opts),
-          template_id: safe_get(opts, :template_id),
-          claim_number: safe_get(opts, :claim_number)
+          template_id: opts&.dig(:template_id),
+          claim_number: opts&.dig(:claim_number)
         },
         { error: :check_in_va_notify_job, team: 'check-in' }
       )
 
-      if FAILED_CLAIM_TEMPLATE_IDS.include?(safe_get(opts, :template_id))
-        tags = if safe_get(opts, :facility_type) == 'cie'
+      if FAILED_CLAIM_TEMPLATE_IDS.include?(opts&.dig(:template_id))
+        tags = if opts&.dig(:facility_type) == 'cie'
                  Constants::STATSD_CIE_SILENT_FAILURE_TAGS
                else
                  Constants::STATSD_OH_SILENT_FAILURE_TAGS
@@ -52,55 +89,123 @@ module CheckIn
       StatsD.increment(Constants::STATSD_NOTIFY_ERROR)
     end
 
-    def self.safe_get(hash, key)
-      hash&.[](key)
-    end
-
+    # Extracts the last four digits of a phone number from a hash
+    # Removes any non-numeric characters before extracting the last four digits
+    #
+    # @param hash [Hash, nil] Hash containing a :mobile_phone key
+    # @return [String, nil] Last four digits of the phone number, or nil if not present
     def self.phone_last_four(hash)
-      safe_get(hash, :mobile_phone)&.delete('^0-9')&.last(4)
+      hash&.dig(:mobile_phone)&.delete('^0-9')&.last(4)
     end
 
     private
 
+    # Validates that all required fields are present
+    #
+    # @param opts [Hash] Options hash containing job parameters
+    # @return [Boolean] true if all required fields are present, false otherwise
+    def validate_required_fields(opts)
+      missing_fields = missing_required_fields(opts)
+      missing_fields.empty?
+    end
+
+    # Checks for missing required fields and logs if any are missing
+    #
+    # @param opts [Hash] Options hash containing job parameters
+    # @return [Array<String>] List of missing field names
+    def missing_required_fields(opts)
+      missing_data = []
+      REQUIRED_FIELDS.each do |field|
+        missing_data << field.to_s if opts&.dig(field).blank?
+      end
+      log_missing_fields(missing_data) if missing_data.any?
+      missing_data
+    end
+
+    # Logs information about missing fields
+    #
+    # @param missing_data [Array<String>] List of missing field names
+    # @return [void]
+    def log_missing_fields(missing_data)
+      logger.info({ message: "SMS notification skipped: missing #{missing_data.join(', ')}" })
+      StatsD.increment(Constants::STATSD_NOTIFY_ERROR)
+    end
+
+    # Parses the appointment date string into a Date object
+    #
+    # @param date_string [String] Appointment date in YYYY-MM-DD format
+    # @return [Date, nil] Parsed date if format is valid, nil otherwise
+    def parse_appointment_date(date_string)
+      DateTime.strptime(date_string.to_s, '%Y-%m-%d').to_date
+    rescue
+      logger.info({ message: 'SMS notification skipped: invalid appointment date format' })
+      StatsD.increment(Constants::STATSD_NOTIFY_ERROR)
+      nil
+    end
+
+    # Calculates the current attempt number based on Sidekiq retry count
+    #
+    # @return [Integer] The current attempt number (1-based)
+    def current_attempt_number
+      retry_attempt = 0
+      if self.class.sidekiq_options_hash&.[]('retry_count')
+        retry_attempt = self.class.sidekiq_options_hash['retry_count'].to_i
+      end
+      retry_attempt + 1
+    end
+
+    # Returns a configured instance of the VaNotify client
+    #
+    # @return [VaNotify::Service] Configured VaNotify client
     def notify_client
       @notify_client ||= VaNotify::Service.new(Settings.vanotify.services.check_in.api_key)
     end
 
+    # Logs information about the notification attempt
+    #
+    # @param opts [Hash] Options hash containing job parameters
+    # @return [void]
     def log_sending_travel_claim_notification(opts)
       phone_last_four = self.class.phone_last_four(opts)
-      template_id = self.class.safe_get(opts, :template_id)
+      template_id = opts&.dig(:template_id)
 
       log_message_and_context = {
         message: "Sending travel claim notification to #{phone_last_four}, #{template_id}",
-        phone_last_four: phone_last_four,
-        template_id: template_id
+        phone_last_four:,
+        template_id:
       }.compact
 
       logger.info(log_message_and_context)
     end
 
-    def va_notify_send_sms(opts)
-      appt_date = begin
-        DateTime.strptime(self.class.safe_get(opts, :appointment_date).to_s,
-                          '%Y-%m-%d').to_date.strftime('%b %d')
-      rescue
-        'Unknown Date'
-      end
-
-      sms_sender_id = if self.class.safe_get(opts,
-                                             :facility_type) && 'oh'.casecmp?(self.class.safe_get(opts, :facility_type))
+    # Sends the SMS notification using VaNotify service
+    # Uses the parsed date and selects the appropriate sender ID based on facility type
+    #
+    # @param opts [Hash] Options hash containing job parameters
+    # @param parsed_date [Date] Parsed appointment date
+    # @return [Object] The result from the VaNotify send_sms call
+    def va_notify_send_sms(opts, parsed_date)
+      # Format the already parsed date
+      formatted_date = parsed_date.strftime('%b %d')
+      facility_type = opts&.dig(:facility_type)
+      sms_sender_id = if facility_type && 'oh'.casecmp?(facility_type)
                         Constants::OH_SMS_SENDER_ID
                       else
                         Constants::CIE_SMS_SENDER_ID
                       end
 
-      phone_number = self.class.safe_get(opts, :mobile_phone)
-      template_id = self.class.safe_get(opts, :template_id)
-      personalisation = { claim_number: self.class.safe_get(opts, :claim_number), appt_date: appt_date }
+      phone_number = opts&.dig(:mobile_phone)
+      template_id = opts&.dig(:template_id)
+      claim_number = opts&.dig(:claim_number)
+      personalisation = { claim_number:, appt_date: formatted_date }
 
       notify_client.send_sms(phone_number:, template_id:, sms_sender_id:, personalisation:)
     end
 
+    # Logs information about SMS sending failures
+    #
+    # @param attempt_number [Integer] The current attempt number
+    # @return [void]
     def log_send_sms_failure(attempt_number)
       logger.info({ message: "Sending SMS failed, attempt #{attempt_number} of #{MAX_RETRIES + 1}" })
     end
