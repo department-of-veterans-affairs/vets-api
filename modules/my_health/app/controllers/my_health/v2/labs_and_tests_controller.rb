2c1510ab36842308b2707233f0570ab495acde4f
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-04-08 11:22:35 -0600

    v2 controller for labs and tests (web) (#21329)
    * Move labs serializer to shared space
    
    * Refactor lab observation value handling to use a structured object for value representation
    
    * Add feature toggle for MHV accelerated delivery of labs and tests data
    
    * Update spec to use existing test user
    
    * Add my_health LabsAndTestsController with corresponding route and specs
    
    * Add CODEOWNERS entry for lab_or_test_serializer
    
    * Add service tag
    
    * Update config/features.yml
    
    * Implement error handling for unsupported value attachments in labs and tests
    
    * Refactor tests to use symbolic status for not implemented responses in labs and tests
    
    * Linting fixes
    
    * Update specs
    
    * Feature/labs and tests/add sample fields (#21430)
    
    * Add method to extract samples tested
    
    * Commit functiong wip
    
    * Linting fixes
    
    * Update spec fixtures
    
    * Refactor reference ID extraction into a helper method
    
    * cleanup sample method
    
    ---------
    
    Co-authored-by: Patrick Bateman <batemapf@gmail.com>

--- modules/my_health/app/controllers/my_health/v2/labs_and_tests_controller.rb
+++ modules/my_health/app/controllers/my_health/v2/labs_and_tests_controller.rb
@@ -3,10 +3,10 @@
 require 'unified_health_data/service'
 require 'lab_or_test_serializer'
 
-module Mobile
-  module V1
+module MyHealth
+  module V2
     class LabsAndTestsController < ApplicationController
-      before_action :controller_enabled?
+      service_tag 'mhv-medical-records'
 
       def index
         start_date = params[:start_date]
@@ -16,14 +16,6 @@ module Mobile
       end
 
       private
-
-      def controller_enabled?
-        routing_error unless Flipper.enabled?(:mhv_accelerated_delivery_uhd_enabled, @current_user)
-      end
-
-      def routing_error
-        raise Common::Exceptions::RoutingError, params[:path]
-      end
 
       def service
         UnifiedHealthData::Service.new(@current_user)
