a183370bcbdb805f4a14e094fe6c744002907463
Ian Donovan <iandonovan@users.noreply.github.com>
2025-04-10 13:49:51 -0400

    Moves constants into the Lighthouse Benefits Claims Constants file

--- lib/lighthouse/benefits_claims/service.rb
+++ lib/lighthouse/benefits_claims/service.rb
@@ -2,6 +2,7 @@
 
 require 'common/client/base'
 require 'lighthouse/benefits_claims/configuration'
+require 'lighthouse/benefits_claims/constants'
 require 'lighthouse/benefits_claims/service_exception'
 require 'lighthouse/service_exception'
 
@@ -13,81 +14,6 @@ module BenefitsClaims
     FILTERED_STATUSES = %w[CANCELED ERRORED PENDING].freeze
 
     SUPPRESSED_EVIDENCE_REQUESTS = ['Attorney Fees', 'Secondary Action Required', 'Stage 2 Development'].freeze
-
-    FRIENDLY_DISPLAY_MAPPING_FIRST_PARTY = {
-      '21-4142/21-4142a' => 'Authorization to Disclose Information',
-      'Proof of Service (DD214, etc.)' => 'Proof of Service',
-      'Employment info needed' => 'Employment information',
-      'EFT - Treasury Mandate Notification' => 'Direct deposit information',
-      'PTSD - Need stressor details/med evid of stressful incdnt' => 'Details about cause of PTSD',
-      'RV1 - Reserve Records Request' => 'Reserve records'
-    }.freeze
-
-    FRIENDLY_DISPLAY_MAPPING_THIRD_PARTY = {
-      'Proof of Service (DD214, etc.)' => 'Proof of Service',
-      'PMR Request' => 'Non-VA medical records',
-      'General Records Request (Medical)' => 'Non-VA medical records',
-      'DBQ AUDIO Hearing Loss and Tinnitus' => 'Disability exam for hearing',
-      'DBQ PSYCH Mental Disorders' => 'Mental health exam'
-    }.freeze
-
-    FRIENDLY_DESCRIPTION_MAPPING_FIRST_PARTY = {
-      '21-4142/21-4142a' => 'We need your permission to request your personal information from a non-VA source,' \
-                            ' like a private doctor or hospital.',
-      'Proof of Service (DD214, etc.)' => 'We need copies of your separation papers for all periods of service.',
-      'Employment info needed' => 'We need employment information from your most recent employer.',
-      'EFT - Treasury Mandate Notification' => 'We need your direct deposit information in order to pay benefits,' \
-                                               ' if awarded.',
-      'PTSD - Need stressor details/med evid of stressful incdnt' => 'We need information about the cause of' \
-                                                                     ' your posttraumatic stress disorder (PTSD).',
-      'RV1 - Reserve Records Request' => 'We\'ve requested your reserve records on your behalf. No action is needed.'
-    }.freeze
-
-    FRIENDLY_DESCRIPTION_MAPPING_THIRD_PARTY = {
-      'Proof of Service (DD214, etc.)' => 'We\'ve requested your Proof of Service on your behalf. No action is needed.',
-      'PMR Request' => 'We\'ve requested your non-VA medical records on your behalf. No action is needed.',
-      'General Records Request (Medical)' => 'We\'ve requested your non-VA medical records on your behalf.' \
-                                             ' No action is needed.',
-      'DBQ AUDIO Hearing Loss and Tinnitus' => 'We\'ve requested a disability exam for your hearing.' \
-                                               ' The examiner\'s office will contact you to schedule this appointment.',
-      'DBQ PSYCH Mental Disorders' => 'We\'ve requested a mental health exam for you. The examiner\'s office' \
-                                      ' will contact you to schedule this appointment.'
-    }.freeze
-
-    SUPPORT_ALIASES_MAPPING_FIRST_PARTY = {
-      '21-4142/21-4142a' => ['VA Form 21-4142'],
-      'Proof of Service (DD214, etc.)' => ['Form DD214'],
-      'Employment info needed' => ['VA Form 21-4192'],
-      'EFT - Treasury Mandate Notification' => ['EFT - Treasure Mandate Notification'],
-      'PTSD - Need stressor details/med evid of stressful incdnt' => ['VA Form 21-0781',
-                                                                      'PTSD - Need stressor details'],
-      'RV1 - Reserve Records Request' => ['RV1 - Reserve Records Request']
-    }.freeze
-
-    SUPPORT_ALIASES_MAPPING_THIRD_PARTY = {
-      'Proof of Service (DD214, etc.)' => ['Proof of Service (DD214, etc.)'],
-      'PMR Request' => ['PMR Request', 'General Records Request (Medical)'],
-      'General Records Request (Medical)' => ['General Records Request (Medical)', 'PMR Request'],
-      'DBQ AUDIO Hearing Loss and Tinnitus' => ['DBQ AUDIO Hearing Loss and Tinnitus'],
-      'DBQ PSYCH Mental Disorders' => ['DBQ PSYCH Mental Disorders']
-    }.freeze
-
-    UPLOADER_MAPPING_FIRST_PARTY = {
-      '21-4142/21-4142a' => true,
-      'Proof of Service (DD214, etc.)' => true,
-      'Employment info needed' => true,
-      'EFT - Treasury Mandate Notification' => false,
-      'PTSD - Need stressor details/med evid of stressful incdnt' => true,
-      'RV1 - Reserve Records Request' => true
-    }.freeze
-
-    UPLOADER_MAPPING_THIRD_PARTY = {
-      'Proof of Service (DD214, etc.)' => true,
-      'PMR Request' => true,
-      'General Records Request (Medical)' => true,
-      'DBQ AUDIO Hearing Loss and Tinnitus' => true,
-      'DBQ PSYCH Mental Disorders' => true
-    }.freeze
 
     def initialize(icn)
       @icn = icn
@@ -380,10 +306,11 @@ module BenefitsClaims
       tracked_items.select { |i| i['status'] == 'NEEDED_FROM_YOU' }.each do |i|
         display_name = i['displayName']
         i['canUploadFile'] =
-          UPLOADER_MAPPING_FIRST_PARTY[display_name].nil? || UPLOADER_MAPPING_FIRST_PARTY[display_name]
-        i['friendlyName'] = FRIENDLY_DISPLAY_MAPPING_FIRST_PARTY[display_name]
-        i['friendlyDescription'] = FRIENDLY_DESCRIPTION_MAPPING_FIRST_PARTY[display_name]
-        i['supportAliases'] = SUPPORT_ALIASES_MAPPING_FIRST_PARTY[display_name] || []
+          BenefitsClaims::Constants::UPLOADER_MAPPING_FIRST_PARTY[display_name].nil? ||
+          BenefitsClaims::Constants::UPLOADER_MAPPING_FIRST_PARTY[display_name]
+        i['friendlyName'] = BenefitsClaims::Constants::FRIENDLY_DISPLAY_MAPPING_FIRST_PARTY[display_name]
+        i['friendlyDescription'] = BenefitsClaims::Constants::FRIENDLY_DESCRIPTION_MAPPING_FIRST_PARTY[display_name]
+        i['supportAliases'] = BenefitsClaims::Constants::SUPPORT_ALIASES_MAPPING_FIRST_PARTY[display_name] || []
       end
       tracked_items
     end
@@ -395,10 +322,11 @@ module BenefitsClaims
       tracked_items.select { |i| i['status'] == 'NEEDED_FROM_OTHERS' }.each do |i|
         display_name = i['displayName']
         i['canUploadFile'] =
-          UPLOADER_MAPPING_THIRD_PARTY[display_name].nil? || UPLOADER_MAPPING_THIRD_PARTY[display_name]
-        i['friendlyName'] = FRIENDLY_DISPLAY_MAPPING_THIRD_PARTY[display_name]
-        i['friendlyDescription'] = FRIENDLY_DESCRIPTION_MAPPING_THIRD_PARTY[display_name]
-        i['supportAliases'] = SUPPORT_ALIASES_MAPPING_THIRD_PARTY[display_name] || []
+          BenefitsClaims::Constants::UPLOADER_MAPPING_THIRD_PARTY[display_name].nil? ||
+          BenefitsClaims::Constants::UPLOADER_MAPPING_THIRD_PARTY[display_name]
+        i['friendlyName'] = BenefitsClaims::Constants::FRIENDLY_DISPLAY_MAPPING_THIRD_PARTY[display_name]
+        i['friendlyDescription'] = BenefitsClaims::Constants::FRIENDLY_DESCRIPTION_MAPPING_THIRD_PARTY[display_name]
+        i['supportAliases'] = BenefitsClaims::Constants::SUPPORT_ALIASES_MAPPING_THIRD_PARTY[display_name] || []
       end
       tracked_items
     end
