c763ad67b17a235a60f3a49587cf4ca41cb2e888
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-04-10 22:52:23 -0400

    106950 One Debt Letter tempfile params filtering

--- modules/debts_api/lib/debts_api/engine.rb
+++ modules/debts_api/lib/debts_api/engine.rb
@@ -8,5 +8,13 @@ module DebtsApi
     initializer 'model_core.factories', after: 'factory_bot.set_factory_paths' do
       FactoryBot.definition_file_paths << File.expand_path('../../spec/factories', __dir__) if defined?(FactoryBot)
     end
+
+    initializer 'debts_api.filter_uploaded_file_inspect' do
+      ActionDispatch::Http::UploadedFile.prepend(Module.new do
+        def inspect
+          "#<#{self.class} [FILTERED FILE]>"
+        end
+      end)
+    end
   end
 end
