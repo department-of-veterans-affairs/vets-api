6528c533ae66b2c769e17dd9c62bb8b9b1f505dd
Joshua <joshing_you1@yahoo.com>
2025-04-14 17:21:53 -0400

    Removed a comment

--- lib/veteran_enrollment_system/associations/associations/configuration.rb
+++ lib/veteran_enrollment_system/associations/associations/configuration.rb
@@ -6,9 +6,7 @@ module VeteranEnrollmentSystem
   module Associations
     module Associations
       class Configuration < VeteranEnrollmentSystem::BaseConfiguration
-        ##
-        # @return [Hash] The basic headers required for any VES Associations API call
-        #
+        # The basic headers required for any VES Associations API call
         def self.base_request_headers
           super.merge('apiKey' => Settings.veteran_enrollment_system.associations.api_key)
         end
