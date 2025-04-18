cb5b47dae2443b4beb73aec32e2eb7d22207689f
Wayne Weibel <wayne.weibel@adhocteam.us>
2025-04-18 16:21:14 -0400

    PBP 106165 Income and Assets 0969 submitted email

---
+++ modules/income_and_assets/lib/income_and_assets/notification_email.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'income_and_assets/notification_callback'
+require 'veteran_facing_services/notification_email/saved_claim'
+
+module IncomeAndAssets
+  # @see VeteranFacingServices::NotificationEmail::SavedClaim
+  class NotificationEmail < ::VeteranFacingServices::NotificationEmail::SavedClaim
+    # @see VeteranFacingServices::NotificationEmail::SavedClaim#new
+    def initialize(saved_claim_id)
+      super(saved_claim_id, service_name: 'burials')
+    end
+
+    private
+
+    # @see VeteranFacingServices::NotificationEmail::SavedClaim#claim_class
+    def claim_class
+      IncomeAndAssets::SavedClaim
+    end
+
+    # @see VeteranFacingServices::NotificationEmail::SavedClaim#personalization
+    def personalization
+      default = super
+
+      template = {
+        # confirmation, error
+        'first_name' => claim.claimaint_first_name&.upcase,
+        # received
+        'date_received' => claim.form_submissions&.last&.form_submission_attempts&.last&.lighthouse_updated_at
+      }
+
+      default.merge(template)
+    end
+
+    # @see VeteranFacingServices::NotificationEmail::SavedClaim#callback_klass
+    def callback_klass
+      IncomeAndAssets::NotificationCallback.to_s
+    end
+  end
+end
