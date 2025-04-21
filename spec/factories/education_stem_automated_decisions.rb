822064ea935598560bde98f65547995010df8229
John Bramley <john.bramley@wilcore.io>
2025-04-21 11:07:59 -0600

    spec updates

--- spec/factories/education_stem_automated_decisions.rb
+++ spec/factories/education_stem_automated_decisions.rb
@@ -3,11 +3,13 @@
 FactoryBot.define do
   factory :education_stem_automated_decision do
     transient do
-      user { create(:user, :loa3, :with_terms_of_use_agreement, uuid: SecureRandom.uuid) }
+      user do
+        create(:user, :loa3, :with_terms_of_use_agreement, uuid: SecureRandom.uuid, idme_uuid: SecureRandom.uuid)
+      end
     end
     automated_decision_state { EducationStemAutomatedDecision::INIT }
-    user_uuid { user.uuid }
     poa { false }
+    user_uuid { user.uuid }
     user_account { user.user_account }
 
     trait :with_poa do
