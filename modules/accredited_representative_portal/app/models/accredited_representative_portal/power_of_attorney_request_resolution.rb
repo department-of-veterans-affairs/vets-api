953cfd40b1b5083c56bd21ef203b4242e9c0c271
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-04-22 12:23:46 -0400

    Linting + typo

--- modules/accredited_representative_portal/app/models/accredited_representative_portal/power_of_attorney_request_resolution.rb
+++ modules/accredited_representative_portal/app/models/accredited_representative_portal/power_of_attorney_request_resolution.rb
@@ -54,9 +54,9 @@ module AccreditedRepresentativePortal
 
     DECLINATION_REASON_TEXTS = {
       decline_health: "Decline, because protected medical record access is limited",
-      decline_address: "Decline, because change of address isn't authorized"
-      decline_health_address: "Decline, because change of address isn't authorized and protected mediecal record access is limited"
-      decline_VSO: "Decline, because the VSO isn't accepting new clients"
+      decline_address: "Decline, because change of address isn't authorized",
+      decline_health_address: "Decline, because change of address isn't authorized and protected medical record access is limited",
+      decline_VSO: "Decline, because the VSO isn't accepting new clients",
       decline_other: "Decline, because of another reason"
     }.freeze
 
