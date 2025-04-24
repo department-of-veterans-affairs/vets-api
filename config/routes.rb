7579b8a0e09cfa50be70875c1fe012ccd50846c2
Alex Jolovic <alexander.jolovic@bylight.com>
2025-04-24 10:30:50 -0700

    chore: remove legacy Blue-Button controllers, routes, specs

--- config/routes.rb
+++ config/routes.rb
@@ -204,12 +204,6 @@ Rails.application.routes.draw do
       end
     end
 
-    resource :health_records, only: [:create], defaults: { format: :json } do
-      get :refresh, to: 'health_records#refresh', on: :collection
-      get :eligible_data_classes, to: 'health_records#eligible_data_classes', on: :collection
-      get :show, controller: 'health_record_contents', on: :collection
-    end
-
     resources :appeals, only: :index
 
     scope :messaging do
