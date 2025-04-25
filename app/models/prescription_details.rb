5e1a260244c774ba66a06ced4bb907d45108c2d1
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2025-04-25 17:45:20 -0400

    Replace Virtus with Vets::Model - Prescription Models

--- app/models/prescription_details.rb
+++ app/models/prescription_details.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class PrescriptionDetails < Prescription
+
   attribute :cmop_division_phone, String
-  attribute :in_cerner_transition, Boolean
+  attribute :in_cerner_transition, Bool
   attribute :not_refillable_display_message, String
   attribute :cmop_ndc_number, String
   attribute :user_id, Integer
@@ -10,28 +11,28 @@ class PrescriptionDetails < Prescription
   attribute :provider_last_name, String
   attribute :remarks, String
   attribute :division_name, String
-  attribute :modified_date, Common::UTCTime, sortable: { order: 'ASC' }
+  attribute :modified_date, Vets::Type::UTCTime
   attribute :institution_id, String
   attribute :dial_cmop_division_phone, String
-  attribute :disp_status, String, sortable: { order: 'ASC' }, filterable: %w[eq not_eq]
+  attribute :disp_status, String, filterable: %w[eq not_eq]
   attribute :ndc, String
   attribute :reason, String
   attribute :prescription_number_index, String
-  attribute :prescription_source, String, sortable: { order: 'ASC' }
+  attribute :prescription_source, String
   attribute :disclaimer, String
   attribute :indication_for_use, String
   attribute :indication_for_use_flag, String
-  attribute :category, String, sortable: { order: 'ASC' }
-  attribute :tracking_list, Array[String]
-  attribute :rx_rf_records, Array[String]
-  attribute :tracking, Boolean
+  attribute :category, String
+  attribute :tracking_list, String, array: true
+  attribute :rx_rf_records, String, array: true
+  attribute :tracking, Bool
   attribute :orderable_item, String
   attribute :sorted_dispensed_date
   attribute :shape, String
   attribute :color, String
   attribute :back_imprint, String
   attribute :front_imprint, String
-  attribute :grouped_medications, Array[String]
+  attribute :grouped_medications, String, array: true
 
   def sorted_dispensed_date
     has_refills = try(:rx_rf_records).present?
