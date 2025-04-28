d8ef1c936be52b2eaedb1f84a917f45f165393dd
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-04-28 09:42:23 -0400

    rubocop

--- modules/accredited_representative_portal/app/models/accredited_representative_portal/power_of_attorney_request_resolution.rb
+++ modules/accredited_representative_portal/app/models/accredited_representative_portal/power_of_attorney_request_resolution.rb
@@ -51,14 +51,13 @@ module AccreditedRepresentativePortal
       DECLINATION_OTHER: 4
     }
 
-
     DECLINATION_REASON_TEXTS = {
       DECLINATION_HEALTH_RECORDS_WITHHELD: 'Decline, because protected medical record access is limited',
       DECLINATION_ADDRESS_CHANGE_WITHHELD: 'Decline, because change of address isn\'t authorized',
       DECLINATION_BOTH_WITHHELD:
         'Decline, because change of address isn\'t authorized and protected medical record access is limited',
-        DECLINATION_NOT_ACCEPTING_CLIENTS: 'Decline, because the VSO isn\'t accepting new clients',
-        DECLINATION_OTHER: 'Decline, because of another reason'
+      DECLINATION_NOT_ACCEPTING_CLIENTS: 'Decline, because the VSO isn\'t accepting new clients',
+      DECLINATION_OTHER: 'Decline, because of another reason'
     }.freeze
 
     def declination_reason_text
