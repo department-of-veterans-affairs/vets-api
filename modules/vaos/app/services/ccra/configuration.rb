4132b2148cb14d3e349eed8b6101980b83794ab9
Carlos Felix <carlos.felix@agile6.com>
2025-04-28 19:10:34 -0400

    Removed snakecase from Faraday middleware

--- modules/vaos/app/services/ccra/configuration.rb
+++ modules/vaos/app/services/ccra/configuration.rb
@@ -52,7 +52,6 @@ module Ccra
         end
 
         conn.response :betamocks if mock_enabled?
-        conn.response :snakecase
         conn.response :json, content_type: /\bjson$/
         conn.response :vaos_errors
         conn.use :ccra_logging
