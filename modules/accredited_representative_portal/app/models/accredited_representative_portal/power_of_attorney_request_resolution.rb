0e3b164a3266651b5a0895d29d01550e5d387b57
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-04-28 09:37:45 -0400

    Updated key names for declination reason

--- modules/accredited_representative_portal/app/models/accredited_representative_portal/power_of_attorney_request_resolution.rb
+++ modules/accredited_representative_portal/app/models/accredited_representative_portal/power_of_attorney_request_resolution.rb
@@ -44,20 +44,21 @@ module AccreditedRepresentativePortal
     validates :reason, absence: true, unless: -> { resolving.accepts_reasons? }
 
     enum declination_reason: {
-      decline_health: 0,
-      decline_address: 1,
-      decline_health_address: 2,
-      decline_VSO: 3,
-      decline_other: 4
+      DECLINATION_HEALTH_RECORDS_WITHHELD: 0,
+      DECLINATION_ADDRESS_CHANGE_WITHHELD: 1,
+      DECLINATION_BOTH_WITHHELD: 2,
+      DECLINATION_NOT_ACCEPTING_CLIENTS: 3,
+      DECLINATION_OTHER: 4
     }
 
+
     DECLINATION_REASON_TEXTS = {
-      decline_health: 'Decline, because protected medical record access is limited',
-      decline_address: 'Decline, because change of address isn\'t authorized',
-      decline_health_address:
+      DECLINATION_HEALTH_RECORDS_WITHHELD: 'Decline, because protected medical record access is limited',
+      DECLINATION_ADDRESS_CHANGE_WITHHELD: 'Decline, because change of address isn\'t authorized',
+      DECLINATION_BOTH_WITHHELD:
         'Decline, because change of address isn\'t authorized and protected medical record access is limited',
-      decline_VSO: 'Decline, because the VSO isn\'t accepting new clients',
-      decline_other: 'Decline, because of another reason'
+        DECLINATION_NOT_ACCEPTING_CLIENTS: 'Decline, because the VSO isn\'t accepting new clients',
+        DECLINATION_OTHER: 'Decline, because of another reason'
     }.freeze
 
     def declination_reason_text
