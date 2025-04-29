2b60eebd5ad2bef44b6c78f09dc2004e086c5bf4
Ian Donovan <iandonovan@users.noreply.github.com>
2025-04-29 14:32:39 -0400

    Adds basic controller spec

--- app/controllers/v0/event_bus_gateway_controller.rb
+++ app/controllers/v0/event_bus_gateway_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
-service_tag 'event_bus_gateway'
 
 module V0
   class EventBusGatewayController < ApplicationController
+    service_tag 'event_bus_gateway'
+    
     # TEMPORARY FOR INITIAL DEVELOPMENT
     skip_before_action :authenticate, only: :send_email
     EMAIL_PARAMS = %i[
@@ -19,7 +20,7 @@ module V0
     # job to pick up and *that* will send the email.
     def send_email
       if Flipper.enabled?(:event_bus_gateway_emails_enabled)
-        EventBusGateway::DecisionLetters::LetterReadyJob.perform_async(
+        EventBusGateway::DecisionLetters::LetterReadyEmailJob.perform_async(
           participant_id: send_email_params[:participant_id],
           template_id: send_email_params[:template_id],
           personalisation: send_email_params[:personalisation]
---
+++ spec/controllers/v0/event_bus_gateway_controller_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe V0::EventBusGatewayController, type: :controller do
+  describe '#send_email' do
+    let(:params) do
+      {
+        participant_id: '1234',
+        template_id: '5678',
+        personalisation: {}
+      }
+    end
+    context 'when :event_bus_gateway_emails_enabled is enabled' do
+      before do
+        allow(Flipper).to receive(:enabled?).and_call_original
+        allow(Flipper).to receive(:enabled?).with(:event_bus_gateway_emails_enabled).and_return(true)
+      end
+
+      it 'invokes the email-sending job' do
+        expect(EventBusGateway::DecisionLetters::LetterReadyEmailJob).to receive(:perform_async)
+        post(:send_email, params:)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when :event_bus_gateway_emails_enabled is disabled' do
+      before do
+        allow(Flipper).to receive(:enabled?).and_call_original
+        allow(Flipper).to receive(:enabled?).with(:event_bus_gateway_emails_enabled).and_return(false)
+      end
+
+      it 'does not invoke the email-sending job' do
+        expect(EventBusGateway::DecisionLetters::LetterReadyEmailJob).to_not receive(:perform_async)
+        post(:send_email, params:)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
