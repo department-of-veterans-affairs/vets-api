0881a8c37c7d7b84d3e4598a892d6ba25ef642ab
Alex Jolovic <alexander.jolovic@bylight.com>
2025-04-29 09:57:09 -0700

    chore(sm legacy): drop empty messaging scope

--- config/routes.rb
+++ config/routes.rb
@@ -212,30 +212,6 @@ Rails.application.routes.draw do
 
     resources :appeals, only: :index
 
-    scope :messaging do
-      scope :health do
-        resources :triage_teams, only: [:index], defaults: { format: :json }, path: 'recipients'
-
-        resources :folders, only: %i[index show create destroy], defaults: { format: :json } do
-          resources :messages, only: [:index], defaults: { format: :json }
-        end
-
-        resources :messages, only: %i[show create destroy], defaults: { format: :json } do
-          get :thread, on: :member
-          get :categories, on: :collection
-          patch :move, on: :member
-          post :reply, on: :member
-          resources :attachments, only: [:show], defaults: { format: :json }
-        end
-
-        resources :message_drafts, only: %i[create update], defaults: { format: :json } do
-          post ':reply_id/replydraft', on: :collection, action: :create_reply_draft, as: :create_reply
-          put ':reply_id/replydraft/:draft_id', on: :collection, action: :update_reply_draft, as: :update_reply
-        end
-
-        resource :preferences, only: %i[show update], controller: 'messaging_preferences'
-      end
-    end
 
     scope :gi, module: 'gids' do
       resources :institutions, only: :show, defaults: { format: :json } do
