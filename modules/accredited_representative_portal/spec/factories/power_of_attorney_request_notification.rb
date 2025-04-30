0a2412425a3cf929ebbc889676c16967b374cbc5
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-04-30 13:24:33 -0400

    Spec fixes

--- modules/accredited_representative_portal/spec/factories/power_of_attorney_request_notification.rb
+++ modules/accredited_representative_portal/spec/factories/power_of_attorney_request_notification.rb
@@ -10,5 +10,15 @@ FactoryBot.define do
     trait :with_va_notify_notification do
       association :va_notify_notification, factory: :notification
     end
+
+    trait :with_resolution do
+      after(:create) do |notification|
+        create(
+          :power_of_attorney_request_resolution,
+          power_of_attorney_request: notification.power_of_attorney_request,
+          declination_reason: :DECLINATION_OTHER
+        )
+      end
+    end
   end
 end
