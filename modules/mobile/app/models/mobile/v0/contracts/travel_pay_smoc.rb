306cc1a6e74d11d18103213a69f899aa81c73917
Liz Townsend <liz.townsend@adhocteam.us>
2025-04-30 09:03:51 -0500

    add smoc contract

---
+++ modules/mobile/app/models/mobile/v0/contracts/travel_pay_smoc.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Mobile
+  module V0
+    module Contracts
+      class TravelPaySmoc < Base
+        params do
+          required(:appointment_date_time).filled(:date_time)
+          required(:facility_station_number).filled(:string)
+          required(:appointment_type).filled(:string)
+          required(:is_complete).filled(:boolean)
+          # This is optional but will fail if passed an empty string
+          optional(:appointment_name).filled(:string)
+        end
+      end
+    end
+  end
+end
