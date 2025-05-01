28c49d80dfc9c1a35844426f93d9bb4dcb44cd90
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2025-05-01 11:08:53 -0400

    Replace Virtus with Vets::Model - App Models 2

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'common/models/base'
+require 'vets/model'
 
 # Folder model
-class Folder < Common::Base
-  include ActiveModel::Validations
+class Folder
+  include Vets::Model
   include RedisCaching
 
   redis_config REDIS_CONFIG[:secure_messaging_store]
@@ -13,7 +13,7 @@ class Folder < Common::Base
   attribute :name, String
   attribute :count, Integer
   attribute :unread_count, Integer
-  attribute :system_folder, Boolean
+  attribute :system_folder, Bool, default: false
 
   validates :name, presence: true, folder_name_convention: true, length: { maximum: 50 }
 
