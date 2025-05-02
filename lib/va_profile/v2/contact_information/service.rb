875d4961324587226ecca68e8d49f35e7a291ea6
RachalCassity <rachal.cassity@oddball.io>
2025-05-02 08:21:46 -0500

    lint

--- lib/va_profile/v2/contact_information/service.rb
+++ lib/va_profile/v2/contact_information/service.rb
@@ -302,7 +302,8 @@ module VAProfile
             request_path = "#{MPI::Constants::VA_ROOT_OID}/#{ERB::Util.url_encode(vaprofile_aaid)}" + "/#{path}"
             # in_json_v2 method should replace in_json after Contact Information V1 has depreciated
             if path == 'addresses' && log_transaction_id?
-              Rails.logger.info("ContactInformationV2 METHOD: #{method}, POST OR PUT JSON: #{model.in_json_v2}, ADDRESS POU: #{model.address_pou}, REQUEST PATH: #{request_path}")
+              Rails.logger.info("ContactInformationV2 METHOD: #{method}, POST OR PUT JSON: #{model.in_json_v2},
+                ADDRESS POU: #{model.address_pou}, REQUEST PATH: #{request_path}")
             end
             raw_response = perform(method, request_path, model.in_json_v2)
             if path == 'addresses' && log_transaction_id?
