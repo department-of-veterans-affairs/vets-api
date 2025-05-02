e17cfdf9d81d939d599f95f5649e91aeb5fe598a
Holden Hinkle <holden@holdenhinkle.com>
2025-05-02 15:45:27 -0400

    rollback migrations

--- db/migrate/20250501014159_add_foreign_key_to_power_of_attorney_request_decisions.rb
+++
@@ -1,8 +0,0 @@
-class AddForeignKeyToPowerOfAttorneyRequestDecisions < ActiveRecord::Migration[7.2]
-  def change
-    add_foreign_key :ar_power_of_attorney_request_decisions,
-                    :ar_power_of_attorney_requests,
-                    column: :power_of_attorney_request_id,
-                    validate: false
-  end
-end
--- db/migrate/20250501014257_validate_power_of_attorney_request_foreign_key.rb
+++
@@ -1,5 +0,0 @@
-class ValidatePowerOfAttorneyRequestForeignKey < ActiveRecord::Migration[7.2]
-  def change
-    validate_foreign_key :ar_power_of_attorney_request_decisions, :ar_power_of_attorney_requests
-  end
-end
