8b505e0569d257ee8278766afcac5cd91f357db6
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-05-01 20:00:33 -0400

    Specs

--- modules/debts_api/app/sidekiq/debts_api/v0/form5655/send_confirmation_email_job.rb
+++ modules/debts_api/app/sidekiq/debts_api/v0/form5655/send_confirmation_email_job.rb
@@ -4,8 +4,23 @@ require 'debts_api/v0/financial_status_report_service'
 
 module DebtsApi
   class V0::Form5655::SendConfirmationEmailJob
+    include Sidekiq::Job
+
+    sidekiq_retries_exhausted do |job, ex|
+      StatsD.increment("#{STATS_KEY}.retries_exhausted")
+      user_uuid = job['args'][0]['user_uuid']
+
+      Rails.logger.error <<~LOG
+        V0::Form5655::SendConfirmationEmailJob retries exhausted:
+        user_id: #{user_uuid}
+        Exception: #{ex.class} - #{ex.message}
+        Backtrace: #{ex.backtrace.join("\n")}
+      LOG
+    end
+
     def perform(args)
-      if submissions.blank?
+      form_submissions = submissions(args['user_uuid'])
+      if form_submissions.blank?
         Rails.logger.warn(
           "DebtsApi::SendConfirmationEmailJob - No submissions found for user_uuid: #{args['user_uuid']}"
         )
@@ -13,14 +28,16 @@ module DebtsApi
       end
 
       DebtManagementCenter::VANotifyEmailJob.perform_async(
-        args['email'], args['template_id'], email_personalization_info(args), { id_type: 'email' }
+        args['email'], args['template_id'], email_personalization_info(args, form_submissions), { id_type: 'email' }
       )
+    rescue StandardError => e
+      Rails.logger.error("DebtsApi::SendConfirmationEmailJob - Error sending email: #{e.message}")
+      raise e
     end
 
     private
 
-    def email_personalization_info(args)
-      form_submissions = submissions(args['user_uuid'])
+    def email_personalization_info(args, form_submissions)
       confirmation_numbers = form_submissions.map(&:id)
       {
         'first_name' => args['first_name'],
@@ -30,7 +47,7 @@ module DebtsApi
     end
 
     def submissions(user_uuid)
-      DebtsApi::V0::Form5655Submission.where(user_uuid:, state: 'in_progress')
+      DebtsApi::V0::Form5655Submission.where(user_uuid:, state: 1)
     end
   end
 end
