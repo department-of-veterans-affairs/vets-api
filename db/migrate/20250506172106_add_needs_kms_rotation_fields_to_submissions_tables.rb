cf48078c5cad491c45e866fb6b14ebc577c9f9eb
Dan Lim <danlim715@gmail.com>
2025-05-06 12:36:54 -0500

    add migration associated with schema change

---
+++ db/migrate/20250506172106_add_needs_kms_rotation_fields_to_submissions_tables.rb
@@ -0,0 +1,15 @@
+class AddNeedsKmsRotationFieldsToSubmissionsTables < ActiveRecord::Migration[7.2]
+  def change
+    tables = ApplicationRecord.descendants_using_encryption.select do |t|
+      !t.column_names.include?('needs_kms_rotation')
+    end.map(&:table_name).uniq
+
+    tables.each do |table|
+      add_column table, :needs_kms_rotation, :boolean, default: false, null: false
+      add_index table,
+                :needs_kms_rotation,
+                algorithm: :concurrently,
+                if_not_exists: true
+    end
+  end
+end
