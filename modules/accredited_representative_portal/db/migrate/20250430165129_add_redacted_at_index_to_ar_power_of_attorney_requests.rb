cc09126a5fa574ec23a746e4cc18f4bc7267cda2
OJ Bucao <9256675+ojbucao@users.noreply.github.com>
2025-05-07 15:17:46 -0700

    Add index to ar_power_of_attorney_requests.redacted_at
    This commit adds an index to the `redacted_at` column of the `ar_power_of_attorney_requests` table.
    
    Adding this index will improve the performance of queries that filter or order by the `redacted_at` column, particularly when retrieving redacted power of attorney requests.
    
    The index is created concurrently to avoid locking the table during the operation. It also includes a check to prevent attempting to add the index if it already exists.

--- modules/accredited_representative_portal/db/migrate/20250430165129_add_redacted_at_index_to_ar_power_of_attorney_requests.rb
+++ modules/accredited_representative_portal/db/migrate/20250430165129_add_redacted_at_index_to_ar_power_of_attorney_requests.rb
@@ -2,6 +2,8 @@ class AddRedactedAtIndexToArPowerOfAttorneyRequests < ActiveRecord::Migration[7.
   disable_ddl_transaction!
 
   def change
-    add_index :ar_power_of_attorney_requests, :redacted_at, algorithm: :concurrently
+    unless index_exists?(:ar_power_of_attorney_requests, :redacted_at, name: 'index_ar_power_of_attorney_requests_on_redacted_at')
+      add_index :ar_power_of_attorney_requests, :redacted_at, algorithm: :concurrently, name: 'index_ar_power_of_attorney_requests_on_redacted_at'
+    end
   end
 end
