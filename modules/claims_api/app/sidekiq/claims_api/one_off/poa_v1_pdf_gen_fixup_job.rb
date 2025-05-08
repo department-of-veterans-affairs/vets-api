ba97e0669e37fd06f3b0323f7b54cadd15a6e87a
Drew Fisher <142862+cilestin@users.noreply.github.com>
2025-05-08 16:49:39 -0700

    Add job regenerate & upload the pdf of a single POA

---
+++ modules/claims_api/app/sidekiq/claims_api/one_off/poa_v1_pdf_gen_fixup_job.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require 'claims_api/poa_vbms_sidekiq'
+require 'claims_api/v1/poa_pdf_constructor/organization'
+require 'claims_api/v1/poa_pdf_constructor/individual'
+require 'claims_api/stamp_signature_error'
+require 'bd/bd'
+
+module ClaimsApi
+  module OneOff
+    class PoaV1PdfGenFixupJob < ClaimsApi::ServiceBase
+      include ClaimsApi::PoaVbmsSidekiq
+
+      sidekiq_options retry: 5 # Retry for ~10 mins
+
+      # Generate a 21-22 or 21-22a form for a given POA request.
+      # Uploads the generated form to VBMS or BD. If successfully uploaded,
+      # it queues a job to update the POA code in BGS, as well.
+      #
+      # @param power_of_attorney_id [String] Unique identifier of the submitted POA
+      def perform(power_of_attorney_id, action, form_number = nil) # rubocop:disable Metrics/MethodLength
+        power_of_attorney = ClaimsApi::PowerOfAttorney.find(power_of_attorney_id)
+        rep_or_org = form_number == '2122A' ? 'representative' : 'serviceOrganization'
+        poa_code = power_of_attorney.form_data&.dig(rep_or_org, 'poaCode')
+
+        output_path = pdf_constructor(poa_code).construct(data(power_of_attorney), id: power_of_attorney.id)
+
+        if Flipper.enabled?(:lighthouse_claims_api_poa_use_bd)
+          doc_type = form_number == '2122' ? 'L190' : 'L075'
+          benefits_doc_upload(poa: power_of_attorney, pdf_path: output_path, doc_type:, action:)
+        else
+          upload_to_vbms(power_of_attorney, output_path)
+        end
+      rescue VBMS::Unknown
+        rescue_vbms_error(power_of_attorney)
+      rescue Errno::ENOENT
+        rescue_file_not_found(power_of_attorney)
+      rescue ClaimsApi::StampSignatureError => e
+        signature_errors = (power_of_attorney.signature_errors || []).push(e.detail)
+        power_of_attorney.update(status: ClaimsApi::PowerOfAttorney::ERRORED, signature_errors:)
+        ClaimsApi::Logger.log('poa', poa_id: power_of_attorney_id, detail: 'Prawn Signature Error')
+      rescue => e
+        rescue_generic_errors(power_of_attorney, e)
+        raise
+      end
+
+      private
+
+      def benefits_doc_api
+        ClaimsApi::BD.new
+      end
+
+      def benefits_doc_upload(poa:, pdf_path:, doc_type:, action:)
+        if Flipper.enabled?(:claims_api_poa_uploads_bd_refactor)
+          PoaDocumentService.new.create_upload(poa:, pdf_path:, doc_type:, action:)
+        else
+          benefits_doc_api.upload(claim: poa, pdf_path:, doc_type:)
+        end
+      end
+
+      def pdf_constructor(poa_code)
+        if poa_code_in_organization?(poa_code)
+          ClaimsApi::V1::PoaPdfConstructor::Organization.new
+        else
+          ClaimsApi::V1::PoaPdfConstructor::Individual.new
+        end
+      end
+
+      #
+      # Combine form_data with auth_headers
+      #
+      # @param power_of_attorney [ClaimsApi::PowerOfAttorney] Record for this poa change request
+      #
+      # @return [Hash] All data to be inserted into pdf
+      def data(power_of_attorney)
+        power_of_attorney.form_data.deep_merge({
+                                                 'veteran' => {
+                                                   'firstName' => power_of_attorney.auth_headers['va_eauth_firstName'],
+                                                   'lastName' => power_of_attorney.auth_headers['va_eauth_lastName'],
+                                                   'ssn' => power_of_attorney.auth_headers['va_eauth_pnid'],
+                                                   'birthdate' => power_of_attorney.auth_headers['va_eauth_birthdate']
+                                                 }
+                                               })
+      end
+
+      def poa_code_in_organization?(poa_code)
+        ::Veteran::Service::Organization.find_by(poa: poa_code).present?
+      end
+    end
+  end
+end
---
+++ modules/claims_api/spec/sidekiq/one_off/poa_v1_pdf_gen_fixup_job_spec.rb
@@ -0,0 +1,188 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'pdf_fill/filler'
+
+RSpec.describe ClaimsApi::OneOff::PoaV1PdfGenFixupJob, type: :job, vcr: 'bgs/person_web_service/find_by_ssn' do
+  subject { described_class }
+
+  let(:power_of_attorney) { create(:power_of_attorney, :with_full_headers) }
+  let(:poa_code) { 'ABC' }
+  let(:bad_b64_image) { File.read('modules/claims_api/spec/fixtures/signature_b64_prefix_bad.txt') }
+
+  before do
+    Flipper.disable(:lighthouse_claims_api_poa_use_bd)
+    Sidekiq::Job.clear_all
+    allow_any_instance_of(ClaimsApi::V2::BenefitsDocuments::Service)
+      .to receive(:get_auth_token).and_return('some-value-here')
+    b64_image = File.read('modules/claims_api/spec/fixtures/signature_b64.txt')
+    power_of_attorney.form_data = {
+      recordConsent: true,
+      consentAddressChange: true,
+      consentLimits: ['DRUG ABUSE', 'SICKLE CELL'],
+      signatures: {
+        veteran: b64_image,
+        representative: b64_image
+      },
+      veteran: {
+        serviceBranch: 'ARMY',
+        address: {
+          numberAndStreet: '2719 Hyperion Ave',
+          city: 'Los Angeles',
+          state: 'CA',
+          country: 'US',
+          zipFirstFive: '92264'
+        },
+        phone: {
+          areaCode: '555',
+          phoneNumber: '5551337'
+        }
+      },
+      claimant: {
+        firstName: 'Lillian',
+        middleInitial: 'A',
+        lastName: 'Disney',
+        email: 'lillian@disney.com',
+        relationship: 'Spouse',
+        address: {
+          numberAndStreet: '2688 S Camino Real',
+          city: 'Palm Springs',
+          state: 'CA',
+          country: 'US',
+          zipFirstFive: '92264'
+        },
+        phone: {
+          areaCode: '555',
+          phoneNumber: '5551337'
+        }
+      },
+      serviceOrganization: {
+        poaCode: poa_code.to_s,
+        organizationName: 'I Help Vets LLC',
+        address: {
+          numberAndStreet: '2719 Hyperion Ave',
+          city: 'Los Angeles',
+          state: 'CA',
+          country: 'US',
+          zipFirstFive: '92264'
+        }
+      }
+    }
+    power_of_attorney.save
+  end
+
+  describe 'generating the filled and signed pdf' do
+    context 'when representative is part of an organization' do
+      before do
+        create(:veteran_representative, representative_id: '67890', poa_codes: [poa_code.to_s]).save!
+        create(:veteran_organization, poa: 'ABC', name: 'Some org')
+      end
+
+      it 'generates the pdf to match example' do
+        allow_any_instance_of(BGS::PersonWebService).to receive(:find_by_ssn).and_return({ file_nbr: '123456789' })
+        expect(ClaimsApi::V1::PoaPdfConstructor::Organization).to receive(:new).and_call_original
+        expect_any_instance_of(ClaimsApi::V1::PoaPdfConstructor::Organization).to receive(:construct).and_call_original
+        subject.new.perform(power_of_attorney.id, action: 'post')
+      end
+
+      it 'DOES NOT call the POA updater job upon successful upload to VBMS' do
+        token_response = OpenStruct.new(upload_token: '<{573F054F-E9F7-4BF2-8C66-D43ADA5C62E7}')
+        document_response = OpenStruct.new(upload_document_response: {
+          '@new_document_version_ref_id' => '{52300B69-1D6E-43B2-8BEB-67A7C55346A2}',
+          '@document_series_ref_id' => '{A57EF6CC-2236-467A-BA4F-1FA1EFD4B374}'
+        }.with_indifferent_access)
+
+        allow_any_instance_of(ClaimsApi::VBMSUploader).to receive(:fetch_upload_token).and_return(token_response)
+        allow_any_instance_of(ClaimsApi::VBMSUploader).to receive(:upload_document).and_return(document_response)
+        allow_any_instance_of(BGS::PersonWebService).to receive(:find_by_ssn).and_return({ file_nbr: '123456789' })
+
+        expect(ClaimsApi::PoaUpdater).not_to receive(:perform_async)
+
+        subject.new.perform(power_of_attorney.id, action: 'post')
+      end
+    end
+
+    context 'when signature has prefix' do
+      before do
+        create(:veteran_representative, representative_id: '67890', poa_codes: ['ABC']).save!
+        create(:veteran_organization, poa: 'ABC', name: 'Some org')
+        power_of_attorney.update(form_data: power_of_attorney.form_data.deep_merge(
+          {
+            signatures: {
+              veteran: bad_b64_image,
+              representative: bad_b64_image
+            }
+          }
+        ))
+      end
+
+      it 'sets the status and store the error' do
+        expect_any_instance_of(ClaimsApi::V1::PoaPdfConstructor::Organization).to receive(:construct)
+          .and_raise(ClaimsApi::StampSignatureError)
+        subject.new.perform(power_of_attorney.id, action: 'post')
+        power_of_attorney.reload
+        expect(power_of_attorney.status).to eq(ClaimsApi::PowerOfAttorney::ERRORED)
+        expect(power_of_attorney.signature_errors).not_to be_empty
+      end
+    end
+  end
+
+  context 'when an errored job has exhausted its retries' do
+    it 'logs to the ClaimsApi Logger' do
+      error_msg = 'An error occurred for the POA Form Builder Job'
+      msg = { 'args' => [power_of_attorney.id, 'value here'],
+              'class' => subject,
+              'error_message' => error_msg }
+
+      described_class.within_sidekiq_retries_exhausted_block(msg) do
+        expect(ClaimsApi::Logger).to receive(:log).with(
+          'claims_api_retries_exhausted',
+          record_id: power_of_attorney.id,
+          detail: "Job retries exhausted for #{subject}",
+          error: error_msg
+        )
+      end
+    end
+  end
+
+  context 'when the BD upload feature flag is enabled' do
+    let(:errors) { 'some errors' }
+    let(:pdf_path) { 'some/path' }
+    let(:doc_type) { 'L075' }
+
+    it 'calls the benefits document API upload instead of VBMS' do
+      Flipper.enable(:lighthouse_claims_api_poa_use_bd)
+      Flipper.disable(:claims_api_poa_uploads_bd_refactor)
+      expect_any_instance_of(ClaimsApi::VBMSUploader).not_to receive(:upload_document)
+      expect_any_instance_of(ClaimsApi::BD).to receive(:upload)
+
+      subject.new.perform(power_of_attorney.id, action: 'post')
+    end
+
+    it 'calls the benefits document API upload_document instead of upload' do
+      Flipper.enable(:lighthouse_claims_api_poa_use_bd)
+      Flipper.enable(:claims_api_poa_uploads_bd_refactor)
+      expect_any_instance_of(ClaimsApi::VBMSUploader).not_to receive(:upload_document)
+      expect_any_instance_of(ClaimsApi::BD).not_to receive(:upload)
+      expect_any_instance_of(ClaimsApi::BD).to receive(:upload_document)
+
+      subject.new.perform(power_of_attorney.id, 'post')
+    end
+
+    it 'rescues errors from BD and sets the status to errored' do
+      Flipper.enable(:lighthouse_claims_api_poa_use_bd)
+
+      VCR.use_cassette('claims_api/bd/upload_error') do
+        allow(ClaimsApi::BD.new).to receive(:upload).with(claim: power_of_attorney, pdf_path:, doc_type:)
+                                                    .and_raise(Common::Exceptions::BackendServiceException.new(errors))
+        subject.new.perform(power_of_attorney.id, 'post')
+      rescue
+        power_of_attorney.reload
+        expect(power_of_attorney.vbms_error_message).to eq(
+          'BackendServiceException: {:status=>400, :detail=>nil, :code=>"VA900", :source=>nil}'
+        )
+        expect(power_of_attorney.status).to eq(ClaimsApi::PowerOfAttorney::ERRORED)
+      end
+    end
+  end
+end
