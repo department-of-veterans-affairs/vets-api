851292551056f59f3e0d4265d058f3642cd94dc5
Dan Lim <danlim715@gmail.com>
2025-05-08 08:44:28 -0500

    disable ddl transaction

--- db/migrate/20250507151848_add_needs_kms_rotation_indices_to_submission_tables.rb
+++ db/migrate/20250507151848_add_needs_kms_rotation_indices_to_submission_tables.rb
@@ -1,4 +1,6 @@
 class AddNeedsKmsRotationIndicesToSubmissionTables < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+  
   def change
     tables = ApplicationRecord.descendants_using_encryption.select do |t|
       !t.column_names.include?('needs_kms_rotation')
