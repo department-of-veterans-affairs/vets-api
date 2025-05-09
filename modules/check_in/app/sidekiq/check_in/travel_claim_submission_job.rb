c6fab5ea9a5b6d8a80fc9f75456100ac604213d8
PhilipDeFraties <philip.defraties@gmail.com>
2025-05-09 14:34:48 -0600

    update submission job to send notification job object for params

--- modules/check_in/app/sidekiq/check_in/travel_claim_submission_job.rb
+++ modules/check_in/app/sidekiq/check_in/travel_claim_submission_job.rb
@@ -4,25 +4,55 @@ module CheckIn
   class TravelClaimSubmissionJob < TravelClaimBaseJob
     def perform(uuid, appointment_date)
       redis_client = TravelClaim::RedisClient.build
-      mobile_phone = redis_client.patient_cell_phone(uuid:)
+      mobile_phone = redis_client.patient_cell_phone(uuid:) || redis_client.mobile_phone(uuid:)
       station_number = redis_client.station_number(uuid:)
       facility_type = redis_client.facility_type(uuid:)
 
+      log_claim_submission(uuid, appointment_date, station_number, facility_type)
+      claim_number, template_id = submit_claim(uuid:, appointment_date:, station_number:, facility_type:)
+
+      if template_id && mobile_phone && appointment_date
+        opts = { mobile_phone:, appointment_date:, template_id:, claim_number:, facility_type: }
+        TravelClaimNotificationJob.perform_async(opts)
+      else
+        context = {
+          uuid:,
+          appointment_date:,
+          station_number:,
+          facility_type:,
+          template_id:,
+          mobile_phone:
+        }
+        log_missing_data(context)
+      end
+    end
+
+    private
+
+    def log_claim_submission(uuid, appointment_date, station_number, facility_type)
       logger.info({
-                    message: "Submitting travel claim for #{uuid}, #{appointment_date}, " \
-                             "#{station_number}, #{facility_type}",
+                    message: "Submitting travel claim: #{uuid}",
                     uuid:,
                     appointment_date:,
                     station_number:,
                     facility_type:
                   })
+    end
 
-      claim_number, template_id = submit_claim(uuid:, appointment_date:, station_number:, facility_type:)
+    def log_missing_data(context)
+      missing = []
+      missing << 'template_id' unless context[:template_id]
+      missing << 'mobile_phone' unless context[:mobile_phone]
+      missing << 'appointment_date' unless context[:appointment_date]
 
-      unless template_id.nil? || mobile_phone.nil? || appointment_date.nil?
-        TravelClaimNotificationJob.perform_async(mobile_phone:, appointment_date:, template_id:, claim_number:,
-                                                 facility_type:)
-      end
+      logger.error({
+                     message: "Missing data for notification: #{missing.join(', ')} for #{context[:uuid]}",
+                     uuid: context[:uuid],
+                     appointment_date: context[:appointment_date],
+                     station_number: context[:station_number],
+                     facility_type: context[:facility_type],
+                     missing_data: missing
+                   })
     end
 
     def submit_claim(opts = {})
