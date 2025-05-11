a077ba07b2fb168e922f22f78c766b9255416fb0
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2025-05-11 08:31:31 -0400

    linting

--- spec/lib/vets/collection_spec.rb
+++ spec/lib/vets/collection_spec.rb
@@ -154,12 +154,12 @@ RSpec.describe Vets::Collection do
 
     it 'raises an error if an attribute does not exist' do
       expect { collection.order(nonexistent: :asc) }
-        .to raise_error(Common::Exceptions::InvalidSortCriteria, "Invalid sort criteria")
+        .to raise_error(Common::Exceptions::InvalidSortCriteria, 'Invalid sort criteria')
     end
 
     it 'raises an error if the direction is invalid' do
       expect { collection.order(name: :invalid) }
-        .to raise_error(Common::Exceptions::InvalidSortCriteria, "Invalid sort criteria")
+        .to raise_error(Common::Exceptions::InvalidSortCriteria, 'Invalid sort criteria')
     end
 
     it 'forces null values to end of the array' do
