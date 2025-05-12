384d5b0c9be004b9561c9287e2daa5eda39448ff
Lee Delarm <lee.delarm@agile6.com>
2025-05-12 10:29:25 -0400

    rubocop and test additions and fixes

--- modules/vaos/app/services/eps/base_service.rb
+++ modules/vaos/app/services/eps/base_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-require 'eps/token_authentication'
 
+require 'eps/token_authentication'
 
 module Eps
   # Eps::BaseService provides common functionality for making REST API requests
