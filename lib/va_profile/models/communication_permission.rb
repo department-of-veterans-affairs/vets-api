690c3e413d28ce178af23d3d9213332c4f19dde9
emisparkles <emisargent@gmail.com>
2025-05-12 11:54:51 -0700

    Add sensitive field to communication permission model and update spec

--- lib/va_profile/models/communication_permission.rb
+++ lib/va_profile/models/communication_permission.rb
@@ -5,10 +5,12 @@ require_relative 'communication_base'
 module VAProfile
   module Models
     class CommunicationPermission < CommunicationBase
-      attr_accessor :allowed
+      attr_accessor :allowed, :sensitive
       attr_reader :id
 
       validates :allowed, inclusion: { in: [true, false], message: 'must be set' }
+      # the sensitive indicator is only relevant for health appointment reminders
+      validates :sensitive, allow_blank: true, inclusion: { in: [true, false] }
 
       def id=(val)
         @id = val&.to_i
--- spec/lib/va_profile/models/communication_permission_spec.rb
+++ spec/lib/va_profile/models/communication_permission_spec.rb
@@ -5,11 +5,28 @@ require 'va_profile/models/communication_permission'
 
 describe VAProfile::Models::CommunicationPermission, type: :model do
   describe 'validation' do
-    let(:communication_permission) { described_class.new }
+    let(:communication_permission) { described_class.new(allowed: true) }
 
-    %w[allowed].each do |attr|
-      it "validates presence of #{attr}" do
-        expect_attr_invalid(communication_permission, attr, 'must be set')
+    context 'allowed' do
+      it 'validates presence of allowed' do
+        communication_permission.allowed = nil
+        expect_attr_invalid(communication_permission, :allowed, 'must be set')
+      end
+    end
+
+    context 'sensitive' do
+      it 'validates inclusion when sensitive is true or false' do
+        [true, false].each do |sensitive_value|
+          communication_permission.sensitive = sensitive_value
+          expect(communication_permission).to be_valid
+        end
+      end
+
+      it 'skips validation when sensitive is nil or blank' do
+        [nil, ''].each do |sensitive_value|
+          communication_permission.sensitive = sensitive_value
+          expect(communication_permission).to be_valid
+        end
       end
     end
   end
