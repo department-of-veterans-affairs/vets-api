e364c8bea5176150595a66281ffa63676b21b688
Ian Donovan <iandonovan@users.noreply.github.com>
2025-05-13 12:46:50 -0400

    Do not skip authorization

--- app/controllers/v0/event_bus_gateway_controller.rb
+++ app/controllers/v0/event_bus_gateway_controller.rb
@@ -5,7 +5,6 @@ module V0
     service_tag 'event_bus_gateway'
 
     skip_before_action :authenticate, only: :send_email
-    skip_before_action :verify_authenticity_token, only: :send_email
 
     EMAIL_PARAMS = %i[
       participant_id
