b06b998f4b439657a909ce4185735e7b42ec8db6
Gabriel Zurita <gabezurita@gmail.com>
2025-05-12 18:09:07 -0600

    feat: add overflow metadata and redesign section grouping for 21P-0969 - Add overflow metadata (limits, question info) to Section1::KEY - Refactor va21p0969.rb to introduce overflow section grouping and metadata - Update specs to test both with and without extras_redesign option

--- modules/income_and_assets/lib/income_and_assets/pdf_fill/va21p0969.rb
+++ modules/income_and_assets/lib/income_and_assets/pdf_fill/va21p0969.rb
@@ -35,15 +35,58 @@ module IncomeAndAssets
       # The path to the PDF template for the form
       TEMPLATE = "#{IncomeAndAssets::MODULE_PATH}/lib/income_and_assets/pdf_fill/pdfs/#{FORM_ID}.pdf".freeze
 
-      # Each section of the form
-      SECTIONS = [Section1, Section2, Section3, Section4,
-                  Section5, Section6, Section7, Section8,
-                  Section9, Section10, Section11, Section12,
-                  Section13].freeze
+      # Starting page number for overflow pages
+      START_PAGE = 14
+
+      # Map question numbers to descriptive titles for overflow attachments
+      QUESTION_KEY = {
+        1 => 'Veteran name',
+        2 => 'Social Security Number',
+        3 => 'VA file number',
+        4 => 'Claimant name',
+        5 => 'Claimant Social Security Number',
+        6 => 'Claimant telephone number',
+        7 => 'Claimant type',
+        8 => 'Income/net worth date range',
+        9 => 'Unassociated incomes',
+        10 => 'Associated incomes',
+        11 => 'Owned assets',
+        12 => 'Royalties and other properties',
+        13 => 'Asset transfers',
+        14 => 'Trusts',
+        15 => 'Annuities',
+        16 => 'Unreported assets',
+        17 => 'Discontinued incomes',
+        18 => 'Income receipt waivers',
+        19 => 'Statement of truth'
+      }.freeze
+
+      # V2-style sections grouping question numbers for overflow pages
+      SECTIONS = [
+        { label: 'Section I: Veteran Information', question_nums: [1, 2, 3] },
+        { label: 'Section II: Claimant Information', question_nums: [4, 5, 6, 7, 8] },
+        { label: 'Section III: Unassociated Incomes', question_nums: [9] },
+        { label: 'Section IV: Associated Incomes', question_nums: [10] },
+        { label: 'Section V: Owned Assets', question_nums: [11] },
+        { label: 'Section VI: Royalties and Other Properties', question_nums: [12] },
+        { label: 'Section VII: Asset Transfers', question_nums: [13] },
+        { label: 'Section VIII: Trusts', question_nums: [14] },
+        { label: 'Section IX: Annuities', question_nums: [15] },
+        { label: 'Section X: Unreported Assets', question_nums: [16] },
+        { label: 'Section XI: Discontinued Incomes', question_nums: [17] },
+        { label: 'Section XII: Income Receipt Waivers', question_nums: [18] },
+        { label: 'Section XIII: Statement of Truth', question_nums: [19] }
+      ].freeze
+
+      # The list of section classes for form expansion and key building
+      SECTION_CLASSES = [Section1, Section2, Section3, Section4,
+                         Section5, Section6, Section7, Section8,
+                         Section9, Section10, Section11, Section12,
+                         Section13].freeze
 
       key = {}
 
-      SECTIONS.each { |section| key = key.merge(section::KEY) }
+      SECTION_CLASSES.each { |section| key = key.merge(section::KEY) }
 
       # Form configuration hash
       KEY = key.freeze
@@ -56,7 +99,7 @@ module IncomeAndAssets
       # @return [Hash] the processed form data
       #
       def merge_fields(_options = {})
-        SECTIONS.each { |section| section.new.expand(form_data) }
+        SECTION_CLASSES.each { |section| section.new.expand(form_data) }
 
         form_data
       end
--- modules/income_and_assets/spec/lib/income_and_assets/pdf_fill/va21p0969_spec.rb
+++ modules/income_and_assets/spec/lib/income_and_assets/pdf_fill/va21p0969_spec.rb
@@ -15,14 +15,27 @@ describe IncomeAndAssets::PdfFill::Va21p0969 do
     VetsJsonSchema::EXAMPLES.fetch('21P-0969-KITCHEN_SINK')
   end
 
-  it_behaves_like 'a form filler', {
-    form_id: IncomeAndAssets::FORM_ID,
-    factory: :income_and_assets_claim,
-    use_vets_json_schema: true,
-    input_data_fixture_dir: "modules/income_and_assets/spec/fixtures/pdf_fill/#{IncomeAndAssets::FORM_ID}",
-    output_pdf_fixture_dir: "modules/income_and_assets/spec/fixtures/pdf_fill/#{IncomeAndAssets::FORM_ID}",
-    test_data_types: %w[simple kitchen_sink overflow]
-  }
+  [true, false].each do |extras_redesign|
+    context "with extras_redesign: #{extras_redesign}" do
+      before do
+        fixture_pdf = extras_redesign ? 'overflow_redesign_extras.pdf' : 'overflow_extras.pdf'
+        generator_class = extras_redesign ? PdfFill::ExtrasGeneratorV2 : PdfFill::ExtrasGenerator
+        allow_any_instance_of(generator_class).to receive(:generate).and_return(
+          File.expand_path("modules/income_and_assets/spec/fixtures/pdf_fill/21P-0969/#{fixture_pdf}", Rails.root)
+        )
+      end
+
+      it_behaves_like 'a form filler', {
+        form_id: IncomeAndAssets::FORM_ID,
+        factory: :income_and_assets_claim,
+        use_vets_json_schema: true,
+        input_data_fixture_dir: "modules/income_and_assets/spec/fixtures/pdf_fill/#{IncomeAndAssets::FORM_ID}",
+        output_pdf_fixture_dir: "modules/income_and_assets/spec/fixtures/pdf_fill/#{IncomeAndAssets::FORM_ID}",
+        test_data_types: %w[simple kitchen_sink overflow],
+        fill_options: { extras_redesign: }
+      }
+    end
+  end
 
   describe '#merge_fields' do
     it 'merges the right fields', run_at: '2016-12-31 00:00:00 EDT' do
