a22b6503d169c44a99ba7d62265524ab3e2cde13
Drew Fisher <142862+cilestin@users.noreply.github.com>
2025-05-13 12:10:42 -0700

    API-46261 Send emails in batches too

--- modules/claims_api/app/sidekiq/claims_api/one_off/poa_v1_bad_box20_render_report_job.rb
+++ modules/claims_api/app/sidekiq/claims_api/one_off/poa_v1_bad_box20_render_report_job.rb
@@ -12,12 +12,20 @@ module ClaimsApi
 
     class PoaV1BadBox20RenderReportJob < ClaimsApi::ServiceBase
       sidekiq_options retry: 5 # Retry for ~10 mins
+
+      attr_accessor :consumer_id, :emails
+
       LOG_TAG = 'claims_api_poa_box20_report_job'
 
       # rubocop:disable Metrics/MethodLength
-      def perform(consumer_id, emails = [])
+      def perform(consumer_id, emails = [], start_date = nil, finish_date = nil)
+        @consumer_id = consumer_id
+        @emails = emails
+        start_date ||= '2025-01-07'
+        finish_date ||= '2025-04-03'
+
         if consumer_id.blank?
-          ClaimsApi::Logger.log LOG_TAG, level: :warn, detail: 'Invlaid CID.'
+          ClaimsApi::Logger.log LOG_TAG, level: :warn, detail: 'Invalid CID'
           slack_alert_on_failure LOG_TAG, 'Invalid CID'
           return
         end
@@ -26,14 +34,33 @@ module ClaimsApi
           slack_alert_on_failure LOG_TAG, 'Invalid email address list.'
           return
         end
+
         ClaimsApi::Logger.log(LOG_TAG, detail: 'Started processing')
-        date_range = Date.parse('2025-01-07').beginning_of_day...Date.parse('2025-04-03').end_of_day
+        (Date.parse(start_date)..Date.parse(finish_date)).group_by(&:cweek).each_value do |dates|
+          process_date_range dates.first.beginning_of_day..dates.last.end_of_day
+        end
+        ClaimsApi::Logger.log LOG_TAG, detail: 'Job complete'
+      rescue => e
+        # Only alert on the class name since an email exception may output the body, which would have PII
+        ClaimsApi::Logger.log LOG_TAG, detail: 'Exception thrown', level: :error, error: e.class.name
+        slack_alert_on_failure LOG_TAG, "Exception thrown: #{e.class}"
+        raise e
+      end
+      # rubocop:enable Metrics/MethodLength
+
+      protected
+
+      def process_date_range(date_range)
         poas = ClaimsApi::PowerOfAttorney.where cid: consumer_id, created_at: date_range
 
-        memo = +'poa_id,eauth_pnid,created_date'
+        start_date = date_range.first.to_date.to_s
+        finish_date = date_range.last.to_date.to_s
+
+        memo = +"<h3>IDS from #{start_date} to #{finish_date}</h3>"
+        memo << 'poa_id,eauth_pnid,created_date'
         num_records = 0
 
-        poas.find_each(batch_size: 75) do |poa|
+        poas.find_each(batch_size: 100) do |poa|
           next if poa.form_data['consentLimits'].present?
 
           memo << "\n<br>"
@@ -42,22 +69,15 @@ module ClaimsApi
         end
 
         if num_records.zero?
-          ClaimsApi::Logger.log LOG_TAG, level: :warn, detail: 'No records found.'
-          slack_alert_on_failure LOG_TAG, 'No records found.'
+          ClaimsApi::Logger.log LOG_TAG, detail: "No records for week of #{start_date}"
           return
         end
 
         ClaimsApi::Logger.log LOG_TAG, detail: "Found #{num_records} record(s)"
-        ClaimsApi::Logger.log LOG_TAG, detail: 'Sending email'
+        ClaimsApi::Logger.log LOG_TAG, detail: "Sending email for week of #{start_date}"
         ClaimsApi::OneOff::PoaV1BadBox20RenderReportMailer.build(emails, memo).deliver_now
-        ClaimsApi::Logger.log LOG_TAG, detail: 'Email sent. Job complete.'
-      rescue => e
-        # Only alert on the class name since an email exception may output the body, which would have PII
-        ClaimsApi::Logger.log LOG_TAG, detail: 'Exception thrown.', level: :error, error: e.class.name
-        slack_alert_on_failure LOG_TAG, "Exception thrown: #{e.class}"
-        raise e
+        ClaimsApi::Logger.log LOG_TAG, detail: 'Email sent'
       end
-      # rubocop:enable Metrics/MethodLength
     end
   end
 end
