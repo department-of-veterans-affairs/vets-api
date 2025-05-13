de6e2d0a35c42ff4e1c65d459ab7620b8d9441c4
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-05-13 13:11:30 -0400

    457 | Update unit tests for lighthouse vet verification

--- spec/lib/lighthouse/veteran_verification/service_spec.rb
+++ spec/lib/lighthouse/veteran_verification/service_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe VeteranVerification::Service do
         let(:icn) { '123498767V234859' }
 
         it 'retrieves rated disabilities from the Lighthouse API' do
-          VCR.use_cassette('lighthouse/veteran_verification/disability_rating/200_response') do
+          VCR.use_cassette('lighthouse/veteran_verification/disability_rating/200_response', VCR::MATCH_EVERYTHING) do
             response = @service.get_rated_disabilities(icn, '', '')
             expect(response['data']['id']).to eq('12303')
           end
@@ -54,8 +54,12 @@ RSpec.describe VeteranVerification::Service do
       describe 'when requesting status' do
         let(:icn) { '1012667145V762142' }
 
+        before do
+          Flipper.disable(:vet_status_titled_alerts)
+        end
+
         it 'retrieves veteran confirmation status from the Lighthouse API' do
-          VCR.use_cassette('lighthouse/veteran_verification/status/200_response') do
+          VCR.use_cassette('lighthouse/veteran_verification/status/200_response', VCR::MATCH_EVERYTHING) do
             expect(StatsD).to receive(:increment).with(
               VeteranVerification::Constants::STATSD_VET_VERIFICATION_TOTAL_KEY
             )
@@ -71,7 +75,7 @@ RSpec.describe VeteranVerification::Service do
         end
 
         it 'retrieves error status from the Lighthouse API' do
-          VCR.use_cassette('lighthouse/veteran_verification/status/200_error_response') do
+          VCR.use_cassette('lighthouse/veteran_verification/status/200_error_response', VCR::MATCH_EVERYTHING) do
             expect(StatsD).to receive(:increment).with(
               VeteranVerification::Constants::STATSD_VET_VERIFICATION_TOTAL_KEY
             )
@@ -90,7 +94,8 @@ RSpec.describe VeteranVerification::Service do
         end
 
         it 'retrieves veteran not confirmed status from the Lighthouse API' do
-          VCR.use_cassette('lighthouse/veteran_verification/status/200_not_confirmed_response') do
+          VCR.use_cassette('lighthouse/veteran_verification/status/200_not_confirmed_response',
+                           VCR::MATCH_EVERYTHING) do
             expect(Rails.logger).to receive(:info).with(
               'Vet Verification Status Success: not confirmed',
               { not_confirmed: true, not_confirmed_reason: 'NOT_TITLE_38' }
@@ -106,7 +111,8 @@ RSpec.describe VeteranVerification::Service do
         end
 
         it 'retrieves veteran not found status from the Lighthouse API' do
-          VCR.use_cassette('lighthouse/veteran_verification/status/200_person_not_found_response') do
+          VCR.use_cassette('lighthouse/veteran_verification/status/200_person_not_found_response',
+                           VCR::MATCH_EVERYTHING) do
             expect(Rails.logger).to receive(:info).with(
               'Vet Verification Status Success: not confirmed',
               { not_confirmed: true, not_confirmed_reason: 'PERSON_NOT_FOUND' }
@@ -122,7 +128,8 @@ RSpec.describe VeteranVerification::Service do
         end
 
         it 'retrieves more research required status from the Lighthouse API' do
-          VCR.use_cassette('lighthouse/veteran_verification/status/200_more_research_required_response') do
+          VCR.use_cassette('lighthouse/veteran_verification/status/200_more_research_required_response',
+                           VCR::MATCH_EVERYTHING) do
             response = @service.get_vet_verification_status('1012667145V762149', '', '')
 
             expect(response['data']['id']).to eq('1012667145V762149')
@@ -151,6 +158,83 @@ RSpec.describe VeteranVerification::Service do
             VCR.use_cassette(cassette_path) do
               @service.get_vet_verification_status(icn, '', '')
             end
+          end
+        end
+      end
+
+      describe 'with titled alerts enabled' do
+        let(:icn) { '1012667145V762142' }
+
+        before do
+          Flipper.enable(:vet_status_titled_alerts)
+        end
+
+        after do
+          Flipper.disable(:vet_status_titled_alerts)
+        end
+
+        it 'retrieves error status from the Lighthouse API' do
+          VCR.use_cassette('lighthouse/veteran_verification/status/200_error_response', VCR::MATCH_EVERYTHING) do
+            expect(StatsD).to receive(:increment).with(
+              VeteranVerification::Constants::STATSD_VET_VERIFICATION_TOTAL_KEY
+            )
+            expect(Rails.logger).to receive(:info).with(
+              'Vet Verification Status Success: not confirmed',
+              { not_confirmed: true, not_confirmed_reason: 'ERROR' }
+            )
+
+            response = @service.get_vet_verification_status('1012666182V20', '', '')
+
+            expect(response['data']['id']).to eq('1012666182V20')
+            expect(response['data']['attributes']['veteran_status']).to eq('not confirmed')
+            expect(response['data']['attributes']).to have_key('not_confirmed_reason')
+            expect(response['data']['message']).to eq(VeteranVerification::Constants::ERROR_MESSAGE_TITLED)
+          end
+        end
+
+        it 'retrieves veteran not confirmed status from the Lighthouse API' do
+          VCR.use_cassette('lighthouse/veteran_verification/status/200_not_confirmed_response',
+                           VCR::MATCH_EVERYTHING) do
+            expect(Rails.logger).to receive(:info).with(
+              'Vet Verification Status Success: not confirmed',
+              { not_confirmed: true, not_confirmed_reason: 'NOT_TITLE_38' }
+            )
+
+            response = @service.get_vet_verification_status('1012666182V203559', '', '')
+
+            expect(response['data']['id']).to eq('1012666182V203559')
+            expect(response['data']['attributes']['veteran_status']).to eq('not confirmed')
+            expect(response['data']['attributes']).to have_key('not_confirmed_reason')
+            expect(response['data']['message']).to eq(VeteranVerification::Constants::NOT_ELIGIBLE_MESSAGE_TITLED)
+          end
+        end
+
+        it 'retrieves veteran not found status from the Lighthouse API' do
+          VCR.use_cassette('lighthouse/veteran_verification/status/200_person_not_found_response',
+                           VCR::MATCH_EVERYTHING) do
+            expect(Rails.logger).to receive(:info).with(
+              'Vet Verification Status Success: not confirmed',
+              { not_confirmed: true, not_confirmed_reason: 'PERSON_NOT_FOUND' }
+            )
+
+            response = @service.get_vet_verification_status('1012667145V762141', '', '')
+
+            expect(response['data']['id']).to be_nil
+            expect(response['data']['attributes']['veteran_status']).to eq('not confirmed')
+            expect(response['data']['attributes']).to have_key('not_confirmed_reason')
+            expect(response['data']['message']).to eq(VeteranVerification::Constants::NOT_FOUND_MESSAGE_TITLED)
+          end
+        end
+
+        it 'retrieves more research required status from the Lighthouse API' do
+          VCR.use_cassette('lighthouse/veteran_verification/status/200_more_research_required_response',
+                           VCR::MATCH_EVERYTHING) do
+            response = @service.get_vet_verification_status('1012667145V762149', '', '')
+
+            expect(response['data']['id']).to eq('1012667145V762149')
+            expect(response['data']['attributes']['veteran_status']).to eq('not confirmed')
+            expect(response['data']['attributes']).to have_key('not_confirmed_reason')
+            expect(response['data']['message']).to eq(VeteranVerification::Constants::NOT_FOUND_MESSAGE_TITLED)
           end
         end
       end
