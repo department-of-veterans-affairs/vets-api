e6f7de87b8953fb0ca9f91b2beac936ef629479e
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-05-14 10:29:34 -0400

    457 | Fixed typo, changed message to detail and updated detail messages

--- lib/lighthouse/veteran_verification/constants.rb
+++ lib/lighthouse/veteran_verification/constants.rb
@@ -24,17 +24,27 @@ module VeteranVerification
 
     ERROR_MESSAGE_TITLED = {
       title: 'Something went wrong',
-      message: ERROR_MESSAGE,
+      detail: [
+        'We’re sorry. Try to view your Veteran status card again later.'
+      ],
       status: 'error'
     }.freeze
     NOT_FOUND_MESSAGE_TITLED = {
-      title: "There's a proble with your discharge status records",
-      message: NOT_FOUND_MESSAGE,
+      title: "There's a problem with your discharge status records",
+      detail: [
+        'We’re sorry. To fix the problem with your records, call the Defense Manpower Data Center ' \
+        ' at 800-538-9552 (TTY: 711). They’re open Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.'
+      ],
       status: 'warning'
     }.freeze
     NOT_ELIGIBLE_MESSAGE_TITLED = {
       title: "You're not eligible for a Veteran Status Card",
-      message: NOT_ELIGIBLE_MESSAGE,
+      detail: [
+        'To get a Veteran status card, you must have received an honorable discharge for at least one period ' \
+        ' of service.',
+        'If you think your discharge status is incorrect, call the Defense Manpower Data Center at 800-538-9552 ' \
+        '(TTY: 711). They’re open Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.'
+      ],
       status: 'warning'
     }.freeze
   end
