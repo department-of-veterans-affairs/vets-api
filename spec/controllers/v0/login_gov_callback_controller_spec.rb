c5cda824ce85aed3fd320789afbc4aed89eed3e4
Emily Nguyen <emilyn0614@gmail.com>
2025-05-14 13:49:23 -0500

    linting

--- spec/controllers/v0/login_gov_callback_controller_spec.rb
+++ spec/controllers/v0/login_gov_callback_controller_spec.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'sign_in/logingov/service'
-
 
 RSpec.describe 'LoginGovCallbackController', type: :request do
   describe 'POST /v0/login_gov_callback/risc' do
