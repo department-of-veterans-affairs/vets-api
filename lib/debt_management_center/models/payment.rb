c6d27414556834b0ce09483287eb79baa1a107a8
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2025-05-15 10:44:04 -0400

    Replace Virtus with Vets::Model - DMC

--- lib/debt_management_center/models/payment.rb
+++ lib/debt_management_center/models/payment.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require 'vets/model'
+
 module DebtManagementCenter
   class Payment
-    include Virtus.model
+    include Vets::Model
+
     attribute :education_amount, String
     attribute :compensation_amount, String
     attribute :veteran_or_spouse, String
