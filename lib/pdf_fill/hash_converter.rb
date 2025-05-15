685e9c6ab6d6bdc7213af9d9f17c1b78b7c8e7cc
dfitchett <135860892+dfitchett@users.noreply.github.com>
2025-05-15 09:41:52 -0700

    Remove unused reassignment.

--- lib/pdf_fill/hash_converter.rb
+++ lib/pdf_fill/hash_converter.rb
@@ -67,7 +67,6 @@ module PdfFill
       return if key_data[:question_num].blank? || (key_data[:question_text].blank? && key_data[:question_label].blank?)
       return if key_data[:hide_from_overflow]
 
-      i = array_key_data.try(:[], :override_index) || i
       i = nil if key_data[:skip_index]
       v = "$#{v}" if key_data[:dollar]
       v = v.extras_value if v.is_a?(PdfFill::FormValue)
