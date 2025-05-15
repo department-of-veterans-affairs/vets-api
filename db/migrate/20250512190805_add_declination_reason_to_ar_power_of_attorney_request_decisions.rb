7d5728e769e034170dbbe41bc8f9a8411371d730
Holden Hinkle <holden@holdenhinkle.com>
2025-05-15 17:56:19 -0400

    rollback migration

--- db/migrate/20250512190805_add_declination_reason_to_ar_power_of_attorney_request_decisions.rb
+++
@@ -1,5 +0,0 @@
-class AddDeclinationReasonToArPowerOfAttorneyRequestDecisions < ActiveRecord::Migration[7.2]
-  def change
-    add_column :ar_power_of_attorney_request_decisions, :declination_reason, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_12_190805) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_07_151848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
@@ -319,7 +319,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_12_190805) do
   create_table "ar_power_of_attorney_request_decisions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "type", null: false
     t.uuid "creator_id", null: false
-    t.integer "declination_reason"
     t.index ["creator_id"], name: "index_ar_power_of_attorney_request_decisions_on_creator_id"
   end
 
