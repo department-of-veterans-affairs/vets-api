f0bcc9fed5a9bfdf967e7eb602acdcf20942310d
Samreen Zarroug <129233422+szarrougtw@users.noreply.github.com>
2025-05-16 11:03:04 -0500

    Chatbot Auth Updates (#22208)
    * wip
    
    * Chatbot Updates
    
    1. Create new routes for STS auth for claims
    2. Copy old routes to new chatbot folder and update virual_agent references
    
    * move report_to_cxi to lib
    
    * linting
    
    ---------
    
    Co-authored-by: Trevor Bosaw <bosawt@gmail.com>

--- app/controllers/v0/chatbot/users_controller.rb
+++ app/controllers/v0/chatbot/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module V0
-  module VirtualAgent
+  module Chatbot
     class UsersController < SignIn::ServiceAccountApplicationController
       service_tag 'identity'
 
@@ -10,12 +10,21 @@ module V0
       def show
         raise Common::Client::Errors::ClientError unless @icn
 
-        render json: { icn: @icn }, status: :ok
+        render json: { icn: @icn, preferred_name: }, status: :ok
       rescue Common::Client::Errors::ClientError
         render json: invalid_code_error, status: :bad_request
       end
 
       private
+
+      def preferred_name
+        mpi_profile&.preferred_names&.first || mpi_profile&.given_names&.first
+      end
+
+      def mpi_profile
+        @mpi_profile ||= MPI::Service.new.find_profile_by_identifier(identifier_type: MPI::Constants::ICN,
+                                                                     identifier: @icn)&.profile
+      end
 
       def authenticate_one_time_code
         chatbot_code_container = ::Chatbot::CodeContainer.find(params[:code])
