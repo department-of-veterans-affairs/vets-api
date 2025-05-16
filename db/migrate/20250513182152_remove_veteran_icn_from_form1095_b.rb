e5931404eb0c2f16dedc5aa87940ef420819e790
Kris Pethtel <kris.pethtel@gmail.com>
2025-05-16 11:54:13 -0500

    Update migration with type.

--- db/migrate/20250513182152_remove_veteran_icn_from_form1095_b.rb
+++ db/migrate/20250513182152_remove_veteran_icn_from_form1095_b.rb
@@ -1,5 +1,5 @@
 class RemoveVeteranIcnFromForm1095B < ActiveRecord::Migration[7.2]
   def change
-    safety_assured { remove_column :form1095_bs, :veteran_icn }
+    safety_assured { remove_column :form1095_bs, :veteran_icn, :string }
   end
 end
