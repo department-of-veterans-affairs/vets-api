e045814b9c724447ef2919f8da6c23467497ebc2
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-05-20 13:48:00 -0400

    [MHV 69339] rubocop fixes.

--- modules/my_health/app/controllers/my_health/v1/prescriptions_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/prescriptions_controller.rb
@@ -18,9 +18,7 @@ module MyHealth
         raw_data = resource.data.dup
         resource.data = resource_data_modifications(resource)
         filter_count = set_filter_metadata(resource.data, raw_data)
-        if params[:filter].present?
-          resource = apply_filters(resource)
-        end
+        resource = apply_filters(resource) if params[:filter].present?
         resource = params[:sort].is_a?(Array) ? sort_by(resource, params[:sort]) : resource.sort(params[:sort])
         resource.data = sort_prescriptions_with_pd_at_top(resource.data)
         is_using_pagination = params[:page].present? || params[:per_page].present?
@@ -160,21 +158,20 @@ module MyHealth
 
       def apply_filters(resource)
         disp_status = filter_params[:disp_status]
-        if disp_status.present? &&
-          disp_status[:eq]&.downcase == 'active,expired'.downcase
-          filter_renewals(resource)
-        else
-          filters = filter_params[:disp_status][:eq].split(',').map(&:strip).map(&:downcase)
-          resource.data = resource.data.select do |item|
-            filters.include?(item.disp_status.downcase)
-          end
-
-          resource.metadata[:filter] ||= {}
-          resource.metadata[:filter][:dispStatus] = {
-            eq: filter_params[:disp_status][:eq]
-          }
-          resource
+        if disp_status.present? && disp_status[:eq]&.downcase == 'active,expired'.downcase
+          return filter_renewals(resource)
         end
+
+        filters = disp_status[:eq].split(',').map(&:strip).map(&:downcase)
+        resource.data = resource.data.select do |item|
+          filters.include?(item.disp_status.downcase)
+        end
+
+        resource.metadata[:filter] ||= {}
+        resource.metadata[:filter][:dispStatus] = {
+          eq: filter_params[:disp_status][:eq]
+        }
+        resource
       end
 
       def collection_resource
