30b681c6c3161e3e5558e043495fff5f35f44f1c
Brandon Cooper <brandon.cooper@adhocteam.us>
2025-05-21 15:11:13 -0400

    add new va1010cg formatter class

---
+++ lib/pdf_fill/forms/formatters/va1010cg.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'pdf_fill/forms/formatters/base'
+
+module PdfFill
+  module Forms
+    module Formatters
+      class Va1010cg < Base
+      end
+    end
+  end
+end
