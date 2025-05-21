c850c4de404a3259726ad08774b9589f9bb24978
Matthew Dingee <52176351+humancompanion-usds@users.noreply.github.com>
2025-05-21 15:58:13 -0400

    Removes trailing comma

--- app/models/persistent_attachments/va_form.rb
+++ app/models/persistent_attachments/va_form.rb
@@ -32,7 +32,7 @@ class PersistentAttachments::VAForm < PersistentAttachment
       '21-8960' => { max_pages: 2, min_pages: 2 },
       '21-0304' => { max_pages: 4, min_pages: 3 },
       '21-651' => { max_pages: 1, min_pages: 1 },
-      '21P-4185' => { max_pages: 2, min_pages: 2 },
+      '21P-4185' => { max_pages: 2, min_pages: 2 }
     }
   )
 
