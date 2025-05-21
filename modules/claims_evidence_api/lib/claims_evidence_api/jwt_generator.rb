2552d79c8a2d7bb0aeafbd8c9ef81a5f1883cf07
Tai Wilkin <21046714+TaiWilkin@users.noreply.github.com>
2025-05-21 16:20:54 -0400

    Add Claims Evidence API JWT generator

---
+++ modules/claims_evidence_api/lib/claims_evidence_api/jwt_generator.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require 'jwt'
+
+# Usage:
+# Prior to use, Settings.claims_evidence_api.jwt_secret must be set.
+# API requests will fail even with a valid token unless on the VA API.
+# Create token:
+# > require 'claims_evidence_api/jwt_generator'
+# > encoder = ClaimsEvidenceApi::JwtEncoder.new
+# > token = encoder.encode_jwt
+# Use token:
+# > curl -X GET https://claimevidence-api-test.dev.bip.va.gov/api/v1/rest/swagger-ui.html \
+# > -- 'Authentication: Bearer {token}'
+module ClaimsEvidenceApi
+  class JwtEncoder
+    # Issuer assigned by Claim Evidence API team
+    ISSUER = 'VAGOV'
+    # VBMS user logged in to the application; if no user interaction needs to be a system user
+    USER_ID = 'VAGOVSYSACCT'
+    # Station for above user
+    STATION_ID = '283'
+    # Number of seconds for which the JWT is valid; should be limited to 15 minutes or less
+    VALIDITY_LENGTH = 15.minutes
+
+    ALGORITHM = 'HS256'
+
+    # Returns a JWT token for use in Bearer auth
+    def encode_jwt
+      JWT.encode(payload, private_key, ALGORITHM, headers)
+    end
+
+    private
+
+    def headers
+      {
+        typ: 'JWT',
+        alg: ALGORITHM
+      }
+    end
+
+    def payload
+      {
+        jti: SecureRandom.uuid, # random id to identify a unique JWT
+        iat: created_time.to_i,
+        exp: expiration_time.to_i,
+        iss: ISSUER,
+        #  applicationID MUST be the same as the issuer for tracking purposes
+        applicationID: ISSUER,
+        userID: USER_ID,
+        stationID: STATION_ID
+      }
+    end
+
+    # set the token expiration date
+    def expiration_time
+      Time.zone.now + VALIDITY_LENGTH
+    end
+
+    # set the token created time
+    def created_time
+      Time.zone.now
+    end
+
+    # retrieve the secret from settings
+    def private_key
+      Settings.claims_evidence_api.jwt_secret
+    end
+  end
+end
---
+++ modules/claims_evidence_api/spec/lib/claims_evidence_api/jwt_generator_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'claims_evidence_api/jwt_generator'
+
+RSpec.describe ClaimsEvidenceApi::JwtEncoder do
+  describe '#get_token' do
+    it 'returns a token with required fields' do
+      encoded_jwt = ClaimsEvidenceApi::JwtEncoder.new.encode_jwt
+      decoded_jwt = JWT.decode(encoded_jwt, Settings.claims_evidence_api.jwt_secret,
+                               true, { typ: 'JWT', alg: 'HS256' }).first
+      expect(decoded_jwt.keys).to include('iss', 'jti', 'exp', 'iat', 'applicationID', 'userID', 'stationID')
+      expect(decoded_jwt['iss']).to eq('VAGOV')
+      expect(decoded_jwt['applicationID']).to eq('VAGOV')
+      expect(decoded_jwt['userID']).to eq('VAGOVSYSACCT')
+      expect(decoded_jwt['stationID']).to eq('283')
+    end
+  end
+end
