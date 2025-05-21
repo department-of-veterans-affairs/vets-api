0b0b418d801da8250808c613fd1cb18601e3173d
Kris Pethtel <kris.pethtel@gmail.com>
2025-05-21 17:04:24 -0500

    add spec file

---
+++ spec/lib/lighthouse/benefits_discovery/service_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'lighthouse/benefits_discovery/service'
+
+RSpec.describe BenefitsDiscovery::Service do
+  subject { BenefitsDiscovery::Service.new }
+
+  context 'without params' do
+  end
+
+  context 'with params' do
+  end
+end
