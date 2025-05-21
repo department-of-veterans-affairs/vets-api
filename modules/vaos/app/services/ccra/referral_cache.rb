8e9a4196107cca5b8eb4a8159acb0c373769ab1a
Carlos Felix <carlos.felix@agile6.com>
2025-05-21 13:29:49 -0400

    Cache referral detail

---
+++ modules/vaos/app/services/ccra/referral_cache.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Ccra
+  # Ccra::ReferralCache provides a caching mechanism for CCRA referral data.
+  # It stores and retrieves referral data from Redis, using a configurable expiration time.
+  class ReferralCache
+    extend Forwardable
+
+    attr_reader :settings
+
+    def_delegators :settings, :redis_referral_expiry
+
+    REFERRAL_CACHE_KEY = 'vaos_ccra_referral_'
+    REFERRAL_CACHE_NAMESPACE = 'vaos-ccra-cache'
+
+    # Initializes the ReferralCache with settings.
+    #
+    # @return [Ccra::ReferralCache] A new instance of ReferralCache
+    def initialize
+      @settings = Settings.vaos.ccra
+    end
+
+    # Saves referral data to the Redis cache.
+    #
+    # @param id [String] The referral ID to use as the cache key
+    # @param icn [String] The ICN of the patient
+    # @param referral_data [Object] The referral data to be cached
+    # @return [Boolean] True if the cache operation was successful
+    def save_referral_data(id:, icn:, referral_data:)
+      cache_key = generate_cache_key(id, icn)
+      Rails.cache.write(
+        cache_key,
+        referral_data,
+        namespace: REFERRAL_CACHE_NAMESPACE,
+        expires_in: redis_referral_expiry
+      )
+    end
+
+    # Retrieves referral data from the Redis cache.
+    #
+    # @param id [String] The referral ID
+    # @param icn [String] The ICN of the patient
+    # @return [Object, nil] The cached referral data if it exists, otherwise nil
+    def fetch_referral_data(id:, icn:)
+      cache_key = generate_cache_key(id, icn)
+      Rails.cache.read(
+        cache_key,
+        namespace: REFERRAL_CACHE_NAMESPACE
+      )
+    end
+
+    private
+
+    # Generates a consistent cache key for a referral.
+    #
+    # @param id [String] The referral ID
+    # @param icn [String] The ICN of the patient
+    # @return [String] The generated cache key
+    def generate_cache_key(id, icn)
+      "#{REFERRAL_CACHE_KEY}#{icn}_#{id}"
+    end
+  end
+end
