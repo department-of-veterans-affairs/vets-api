3502ad818ec779f4bedb0dc6e57660f64e24bb6a
Oren Mittman <nihil2501@gmail.com>
2025-05-22 17:23:20 -0400

    [ARP] VCR test `SubmitBenefitsIntakeClaimJob` calls to BI API

--- modules/accredited_representative_portal/app/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job.rb
+++ modules/accredited_representative_portal/app/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job.rb
@@ -2,5 +2,18 @@
 
 module AccreditedRepresentativePortal
   class SubmitBenefitsIntakeClaimJob < Lighthouse::SubmitBenefitsIntakeClaim
+    def generate_metadata
+      veteran = @claim.parsed_form['veteran']
+
+      ::BenefitsIntake::Metadata.generate(
+        veteran.dig('name', 'first'),
+        veteran.dig('name', 'last'),
+        veteran['vaFileNumber'] || veteran['ssn'],
+        veteran.dig('address', 'zipCode'),
+        "#{@claim.class} va.gov",
+        @claim.form_id,
+        @claim.business_line
+      )
+    end
   end
 end
--- modules/accredited_representative_portal/spec/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job_spec.rb
+++ modules/accredited_representative_portal/spec/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job_spec.rb
@@ -27,12 +27,45 @@ RSpec.describe AccreditedRepresentativePortal::SubmitBenefitsIntakeClaimJob do
     )
   end
 
-  it 'performs' do
+  before do
     ##
-    # Triggers `VCR::Errors::UnhandledHTTPRequestError`which means we're in the
-    # job and ready to verify outcomes.
+    # This works around some test configuration weirdness. Without this, the
+    # locations used for reading and writing differ, likely due to a difference
+    # in which Shrine plugins have been plugged in at various points.
     #
-    perform
-    expect(true).to be_truthy
+    allow_any_instance_of(Shrine::UploadedFile).to(
+      receive(:storage).and_return(Shrine.storages[:store])
+    )
+  end
+
+  it 'performs' do
+    vcr_options = {
+      ##
+      # It seems as though request bodies and headers are dynamic given static
+      # inputs, which is why we exclude them from VCR matching.
+      #
+      match_requests_on: %i[method uri],
+
+      ##
+      # This job is behaving incorrectly if it does not perform all of the
+      # requests to Benefits Intake API that were recorded in the cassette.
+      # Those are:
+      #   - `POST /uploads` (gets an <upload_location>)
+      #   - `POST /uploads/validate_document` (validates 1st document)
+      #   - `POST /uploads/validate_document` (validates 2nd document)
+      #   - `PUT <upload_location>` (submits the document)
+      #
+      allow_unused_http_interactions: false
+    }
+
+    use_cassette('performs', vcr_options) do
+      expect_any_instance_of(BenefitsIntakeService::Service).to(
+        receive(:upload_doc).and_call_original
+      )
+
+      expect { perform }.to change {
+        FormSubmissionAttempt.where.not(benefits_intake_uuid: nil).count
+      }.by(1)
+    end
   end
 end
