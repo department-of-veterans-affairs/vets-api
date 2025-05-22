77b0a3b9c9e7c8779ab90747f74ca3ec12db394a
Josh Fike <josh.fike@oddball.io>
2025-05-21 22:04:43 -0500

    [WIP]

--- modules/representation_management/app/sidekiq/representation_management/agents_and_attorneys_queue_update.rb
+++ modules/representation_management/app/sidekiq/representation_management/agents_and_attorneys_queue_update.rb
@@ -22,7 +22,8 @@ module RepresentationManagement
         agents.each do |agent|
           # This needs expanded to include the type of AccreditedIndividual and all the correct fields
           # that are needed to create the record.
-          record = AccreditedIndividual.find_or_create_by(agent)
+          agent_hash = data_transform_for_agent(agent)
+          record = AccreditedIndividual.find_or_create_by(agent_hash)
           agent_ids << record.id
         end
         page += 1
@@ -36,7 +37,8 @@ module RepresentationManagement
 
         attorney_responses << attorneys
         attorneys.each do |attorney|
-          record = AccreditedIndividual.find_or_create_by(attorney)
+          attorney_hash = data_transform_for_attorney(attorney)
+          record = AccreditedIndividual.find_or_create_by(attorney_hash)
           attorney_ids << record.id
         end
         page += 1
@@ -53,6 +55,77 @@ module RepresentationManagement
     end
 
     private
+
+    # Agent keys that are a close match to AccreditedIndividual:[
+    #   number -> registration_number,
+    #   poa -> poa_code,
+    #   person_id -> ogc_id,
+    #   firstName -> first_name,
+    #   middleName -> middle_initial,
+    #   lastName -> last_name,
+    #   workAddress1 -> address_line1,
+    #   workAddress2 -> address_line2,
+    #   workAddress3 -> address_line3,
+    #   workZip -> zip_code,
+    #   WorkCountry -> country_code_iso3, country_name,
+    #   workPhoneNumber -> phone,
+    #   workEmailAddress -> email,
+    # ]
+    def data_transform_for_agent(agent)
+      {
+        individual_type: 'claims_agent',
+        registration_number: agent['number'],
+        poa_code: agent['poa'],
+        ogc_id: agent['person_id'],
+        first_name: agent['firstName'],
+        middle_initial: agent['middleName'].to_s.strip.first,
+        last_name: agent['lastName'],
+        address_line1: agent['workAddress1'],
+        address_line2: agent['workAddress2'],
+        address_line3: agent['workAddress3'],
+        zip_code: agent['workZip'],
+        country_code_iso3: agent['WorkCountry'],
+        country_name: agent['WorkCountry'],
+        phone: agent['workPhoneNumber'],
+        email: agent['workEmailAddress']
+      }
+    end
+
+    # Attorney keys that are a close match to AccreditedIndividual:[
+    #   number -> registration_number,
+    #   firstName -> first_name,
+    #   middleName -> middle_initial,
+    #   lastName -> last_name,
+    #   poa -> poa_code,
+    #   workNumber -> phone,
+    #   emailAddress -> email,
+    #   workAddress1 -> address_line1,
+    #   workAddress2 -> address_line2,
+    #   workAddress3 -> address_line3,
+    #   workZip -> zip_code,
+    #   workCity -> city,
+    #   workState -> state_code,
+    #   id, personID, attorneyID -> ogc_id?
+    # ]
+    def data_transform_for_attorney(attorney)
+      {
+        individual_type: 'attorney',
+        registration_number: attorney['number'],
+        poa_code: attorney['poa'],
+        ogc_id: attorney['personID'] || attorney['attorneyID'],
+        first_name: attorney['firstName'],
+        middle_initial: attorney['middleName'].to_s.strip.first,
+        last_name: attorney['lastName'],
+        address_line1: attorney['workAddress1'],
+        address_line2: attorney['workAddress2'],
+        address_line3: attorney['workAddress3'],
+        zip_code: attorney['workZip'],
+        city: attorney['workCity'],
+        state_code: attorney['workState'],
+        phone: attorney['workNumber'],
+        email: attorney['emailAddress']
+      }
+    end
 
     def log_error(message)
       Rails.logger.error("SendExpiredEmailJob error: #{message}")
