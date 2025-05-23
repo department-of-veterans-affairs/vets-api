0ad6813611d1f8f33e2be0cb40de149b3a34c3a6
Ni Chia <ni.chia@adhocteam.us>
2025-05-23 11:58:06 -0700

    fix test specs

--- app/swagger/swagger/requests/mdot/supplies.rb
+++ app/swagger/swagger/requests/mdot/supplies.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# app/controllers/v0/mdot/suppliesc_controller.rb
 module Swagger
   module Requests
     module MDOT
@@ -8,9 +9,10 @@ module Swagger
 
         swagger_path '/v0/mdot/supplies' do
           operation :post do
-            key :description, 'Create a MDOT supply order'
-            key :operationId, 'addMdotOrder'
-            key :tags, %w[mdot]
+            key :summary, 'Order supplies'
+            key :description, 'Place an order for medical supplies'
+            key :operationId, 'orderSupplies'
+            key :tags, %w[ mdot]
 
             extend Swagger::Responses::AuthenticationError
             parameter :authorization
@@ -26,47 +28,52 @@ module Swagger
 
               schema do
                 key :type, :object
-                key :required, [:order]
 
-                property :use_permanent_address, type: :boolean
-                property :use_temporary_address, type: :boolean
-                property :additional_requests, type: :string
-
-                property :permanent_address do
-                  key :type, :object
-
-                  property :street, type: :string
-                  property :street2, type: :string
-                  property :city, type: :string
-                  property :state, type: :string
-                  property :country, type: :string
-                  property :postal_code, type: :string
-                end
-
+                property :use_permanent_address, type: :boolean, example: true
+                property :use_temporary_address, type: :boolean, example: false
+                property :vet_email, type: :string, example: "vet1@va.gov"
                 property :order do
                   key :type, :array
 
                   items do
                     key :type, :object
-
-                    property :product_id, type: :string
+                    property :productId, type: :integer, example: 2499
                   end
+                end
+
+                property :permanent_address do
+                  key :type, :object
+                  property :isMilitary, type: :boolean, example: false
+                  property :street, type: :string, example: "125 SOME RD"
+                  property :street2, type: :string, example: "APT 101"
+                  property :city, type: :string, example: "DENVER"
+                  property :state, type: :string, example: "CO"
+                  property :country, type: :string, example: "United States"
+                  property :postalCode, type: :string, example: "11111"
+                end
+
+                property :temporary_address do
+                  key :type, :object
+                  property :isMilitary, type: :boolean, example: false
+                  property :street, type: :string, example: "17250 w colfax ave"
+                  property :street2, type: :string, example: "a-204"
+                  property :city, type: :string, example: "Golden"
+                  property :state, type: :string, example: "CO"
+                  property :country, type: :string, example: "United States"
+                  property :postalCode, type: :string, example: "80401"
                 end
               end
             end
 
             response 200 do
-              key :description, 'mdot order response'
+              key :description, 'Response is OK'
               schema do
-                key :required, [:order]
-                property :order do
-                  key :type, :array
-                  items do
-                    key :required, %i[status order_id]
-                    property :status, type: :string
-                    property :product_id, type: :string
-                    property :order_id, type: :string
-                  end
+                key :type, :array
+                items do
+                  key :type, :object
+                  property :productId, type: :integer, example: 2499
+                  property :orderID, type: :integer, example: 10001
+                  property :status, type: :string, example: 'Order Processed', enum: %w[Order\ Processed Order\ Pending Unable\ to\ order\ item\ since\ the\ last\ order\ was\ less\ than\ 5\ months\ ago.]
                 end
               end
             end
