41ac581f51e8dbc88564c35cbdd554c0bf1be2b2
Ian Donovan <iandonovan@users.noreply.github.com>
2025-05-22 21:54:44 -0400

    Letter Ready job now includes host and first name in personalisation object to VA Notify

--- app/sidekiq/event_bus_gateway/letter_ready_email_job.rb
+++ app/sidekiq/event_bus_gateway/letter_ready_email_job.rb
@@ -9,13 +9,15 @@ module EventBusGateway
 
     sidekiq_options retry: 0
     NOTIFY_SETTINGS = Settings.vanotify.services.benefits_management_tools
-    MAILER_TEMPLATE_ID = NOTIFY_SETTINGS.template_id.decision_letter_ready_email
 
-    def perform(participant_id, template_id, personalisation)
+    def perform(participant_id, template_id, personalisation_params)
       notify_client.send_email(
         recipient_identifier: { id_value: participant_id, id_type: 'PID' },
         template_id:,
-        personalisation:
+        personalisation: personalisation_params.merge({
+                                                        host: Settings.hostname,
+                                                        first_name: get_first_name_from_participant_id(participant_id)
+                                                      })
       )
     rescue => e
       record_email_send_failure(e)
@@ -27,11 +29,20 @@ module EventBusGateway
       VaNotify::Service.new(NOTIFY_SETTINGS.api_key)
     end
 
+    def get_first_name_from_participant_id(participant_id)
+      bgs = BGS::Services.new(external_uid: participant_id, external_key: participant_id)
+      person = bgs.people.find_person_by_ptcpnt_id(participant_id)
+      if person
+        person[:first_nm].capitalize
+      else
+        record_email_send_failure(OpenStruct.new(message: 'Participant ID cannot be found in BGS'))
+      end
+    end
+
     def record_email_send_failure(error)
       error_message = 'LetterReadyEmailJob VANotify errored'
       ::Rails.logger.error(error_message, { message: error.message })
       StatsD.increment('event_bus_gateway', tags: ['service:event-bus-gateway', "function: #{error_message}"])
-      log_exception_to_sentry(error)
     end
   end
 end
