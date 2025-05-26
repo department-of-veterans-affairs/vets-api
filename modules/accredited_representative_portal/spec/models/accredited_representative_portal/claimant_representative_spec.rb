e85a4df73e2bef6b60b504469a3dc559b76c8e57
Oren Mittman <nihil2501@gmail.com>
2025-05-26 03:03:16 -0400

    [ARP] Introduce `ClaimantRepresentative` Encapsulates relationship validity and 3x participant references: - POA holder (e.g. VSO) - Representative - Claimant

---
+++ modules/accredited_representative_portal/spec/models/accredited_representative_portal/claimant_representative_spec.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AccreditedRepresentativePortal::ClaimantRepresentative, type: :model do
+  describe '.find' do
+    context 'without all required arguments' do
+      subject do
+        described_class.find {}
+      end
+
+      it 'raises Finder::Error' do
+        expect { subject }.to raise_error(
+          described_class::Finder::Error
+        )
+      end
+    end
+
+    context 'with all required arguments' do
+      subject do
+        described_class.find do |finder|
+          finder.for_claimant(
+            icn: claimant_icn
+          )
+
+          finder.for_representative(
+            icn: representative_icn,
+            email: representative_email
+          )
+        end
+      end
+
+      context 'with a representative belonging to 2 VSOs' do
+        before do
+          vso_a = create(:organization, poa: 'PAA')
+          vso_b = create(:organization, poa: 'PBB')
+
+          representative =
+            create(
+              :representative, :vso,
+              poa_codes: [vso_a.poa, vso_b.poa]
+            )
+
+          create(
+            :user_account_accredited_individual,
+            user_account_email: representative_email,
+            user_account_icn: representative_icn,
+            accredited_individual_registration_number:
+              representative.representative_id
+          )
+
+          create(
+            :user_account,
+            icn: representative_icn
+          )
+        end
+
+        let(:representative_icn) { Faker::Number.unique.number(digits: 10) }
+        let(:representative_email) { Faker::Internet.email }
+        let(:claimant_icn) { Faker::Number.unique.number(digits: 10) }
+
+        context '`BenefitsClaims::Service` does raise' do
+          before do
+            allow_any_instance_of(BenefitsClaims::Service).to(
+              receive(:get_power_of_attorney).and_raise(
+                Common::Exceptions::ResourceNotFound
+              )
+            )
+          end
+
+          it 'raises Finder::Error' do
+            expect { subject }.to raise_error(
+              described_class::Finder::Error
+            )
+          end
+        end
+
+        context '`BenefitsClaims::Service` does not raise' do
+          before do
+            allow_any_instance_of(BenefitsClaims::Service).to(
+              receive(:get_power_of_attorney).and_return(
+                JSON.parse(
+                  <<~JSON
+                    {
+                      "data": {
+                        "type": "organization",
+                        "attributes": {
+                          "code": "#{claimant_poa_code}"
+                        }
+                      }
+                    }
+                  JSON
+                )
+              )
+            )
+          end
+
+          context 'and a claimant that has poa with one of them' do
+            let(:claimant_poa_code) { 'PAA' }
+
+            it 'returns a `ClaimantRepresentative`' do
+              expect(subject).to have_attributes(
+                claimant_id: be_a(String),
+                accredited_individual_registration_number: be_a(String),
+                power_of_attorney_holder_poa_code: claimant_poa_code,
+                power_of_attorney_holder_type:
+                  AccreditedRepresentativePortal::PowerOfAttorneyHolder::Types::
+                    VETERAN_SERVICE_ORGANIZATION
+              )
+            end
+          end
+
+          context 'and a claimant that does not have poa with one of them' do
+            let(:claimant_poa_code) { 'ZZZ' }
+
+            it 'returns nil' do
+              expect(subject).to be_nil
+            end
+          end
+        end
+      end
+    end
+  end
+end
