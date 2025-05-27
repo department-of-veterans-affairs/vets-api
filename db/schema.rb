452d1b59f1c70874d85460bca20698fd15823f55
Oren Mittman <nihil2501@gmail.com>
2025-05-27 01:39:56 -0400

    [ARP] Migrate `create_ar_saved_claim_claimant_representatives`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_19_141104) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_27_050638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "fuzzystrmatch"
@@ -366,6 +366,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_19_141104) do
     t.datetime "redacted_at"
     t.index ["claimant_id"], name: "index_ar_power_of_attorney_requests_on_claimant_id"
     t.index ["redacted_at"], name: "index_ar_power_of_attorney_requests_on_redacted_at"
+  end
+
+  create_table "ar_saved_claim_claimant_representatives", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.bigint "saved_claim_id", null: false
+    t.string "claimant_id", null: false
+    t.string "claimant_type", null: false
+    t.string "power_of_attorney_holder_type", null: false
+    t.string "power_of_attorney_holder_poa_code", null: false
+    t.string "accredited_individual_registration_number", null: false
+    t.datetime "created_at", null: false
+    t.index ["saved_claim_id"], name: "idx_on_saved_claim_id_f4f27623c2", unique: true
   end
 
   create_table "ar_user_account_accredited_individuals", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
