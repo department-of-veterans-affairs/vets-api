e00f92b39b6c4281c6a8ce7fe1012b57b67dcf93
Giles Wells <--set>
2025-05-27 16:45:33 -0400

    Make it simplier

--- lib/forms/submission_statuses/report.rb
+++ lib/forms/submission_statuses/report.rb
@@ -24,21 +24,13 @@ module Forms
         results = @formatter.format_data(@dataset)
 
         results = results.select do |result|
-          submission_recent?(result)
+          result.updated_at >= 60.days.ago
         end
 
         OpenStruct.new(
           submission_statuses: results,
           errors: @dataset.errors
         )
-      end
-
-      private
-
-      def submission_recent?(submission)
-        return submission.created_at >= 60.days.ago unless submission.updated_at
-
-        submission.updated_at >= 60.days.ago
       end
     end
   end
