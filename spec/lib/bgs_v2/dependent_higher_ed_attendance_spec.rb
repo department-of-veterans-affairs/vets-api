b656307523abb3aa609a38cc1d7318795d464f0f
Evan Smith <evan.smith@oddball.io>
2025-05-27 16:33:30 -0400

    bgs v2 specs

---
+++ spec/lib/bgs_v2/dependent_higher_ed_attendance_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'bgs/dependent_higher_ed_attendance'
+
+RSpec.describe BGSV2::DependentHigherEdAttendance do
+  let(:user_object) { create(:evss_user, :loa3) }
+  let(:proc_id) { '3831414' }
+  let(:form_674_only) { build(:form_674_only) }
+  let(:form_674_only_v2) { build(:form_674_only_v2) }
+
+  context 'with va_dependents_v2 off' do
+    before do
+      allow(Flipper).to receive(:enabled?).with(:va_dependents_v2).and_return(false)
+    end
+
+    describe '#create' do
+      context 'reporting a child 18 to 23 years old attending school' do
+        it 'returns a hash with a relationship type Child' do
+          VCR.use_cassette('bgs/dependent_higher_ed_attendance/create') do
+            dependents = BGS::DependentHigherEdAttendance.new(
+              proc_id:,
+              payload: form_674_only,
+              user: user_object,
+              student: nil
+            ).create
+
+            expect(dependents).to include(
+              {
+                vnp_participant_id: '151598',
+                participant_relationship_type_name: 'Child',
+                family_relationship_type_name: 'Biological',
+                type: '674'
+              }
+            )
+          end
+        end
+      end
+    end
+  end
+
+  context 'with va_dependents_v2 on' do
+    before do
+      allow(Flipper).to receive(:enabled?).with(:va_dependents_v2).and_return(true)
+    end
+
+    describe '#create' do
+      context 'reporting a child 18 to 23 years old attending school' do
+        it 'returns a hash with a relationship type Child' do
+          VCR.use_cassette('bgs/dependent_higher_ed_attendance/create') do
+            dependents = BGS::DependentHigherEdAttendance.new(
+              proc_id:,
+              payload: form_674_only_v2,
+              user: user_object,
+              student: form_674_only_v2['dependents_application']['student_information'][0]
+            ).create
+
+            expect(dependents).to include(
+              {
+                vnp_participant_id: '151598',
+                participant_relationship_type_name: 'Child',
+                family_relationship_type_name: 'Biological',
+                type: '674'
+              }
+            )
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/bgs_v2/exceptions/bgs_errors_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'bgs/exceptions/bgs_errors'
+
+RSpec.describe BGSV2::Exceptions::BGSErrors do
+  let(:dummy_class) do
+    Class.new do
+      include BGSV2::Exceptions::BGSErrors
+      attr_reader :user
+
+      def initialize
+        @user = { icn: '1234' }
+      end
+    end
+  end
+  let(:dummy_instance) { dummy_class.new }
+
+  describe '#notify_of_service_exception' do
+    context 'large stacktrace with oracle error message and PII returned' do
+      it 'logs the oracle error message to Sentry, but not the stacktrace or PII' do
+        error_message = File.read('spec/fixtures/bgs/bgs_oracle_error.txt')
+        dummy_error = StandardError.new(error_message)
+        expect(dummy_instance).to receive(:log_message_to_sentry).with(
+          'ORA-12899: value too large for column "CORPPROD"."VNP_PERSON"."MIDDLE_NM" (actual: 52, maximum: 30)',
+          :error,
+          { icn: '1234' },
+          { team: 'vfs-ebenefits' }
+        )
+        expect do
+          dummy_instance.notify_of_service_exception(dummy_error, 'dummy_method')
+        end.to raise_error(BGS::ServiceException)
+      end
+    end
+
+    context 'error not related to oracle' do
+      it "raises a BGS::ServiceException with BGS's raw error message" do
+        dummy_error = StandardError.new('(ns0:Server) insertBenefitClaim: City is null')
+        expect(dummy_instance).not_to receive(:log_message_to_sentry)
+        expect do
+          dummy_instance.notify_of_service_exception(dummy_error, 'dummy_method')
+        end.to raise_error(BGS::ServiceException)
+      end
+    end
+
+    context 'no error message' do
+      it 'raises a BGS::ServiceException' do
+        dummy_error = StandardError.new
+        expect(dummy_instance).not_to receive(:log_message_to_sentry)
+        expect do
+          dummy_instance.notify_of_service_exception(dummy_error, 'dummy_method')
+        end.to raise_error(BGS::ServiceException)
+      end
+    end
+  end
+end
