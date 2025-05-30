b5e8da8e1c2ad85d96fecce5db97bf9a4e3e2b83
Josh Fike <josh.fike@oddball.io>
2025-05-30 15:56:45 -0500

    Remove migration for deactivation

--- db/migrate/20250527165539_add_deactivated_at_to_accredited_individuals.rb
+++
@@ -1,5 +0,0 @@
-class AddDeactivatedAtToAccreditedIndividuals < ActiveRecord::Migration[7.2]
-  def change
-    add_column :accredited_individuals, :deactivated_at, :datetime, null: true, default: nil
-  end
-end
