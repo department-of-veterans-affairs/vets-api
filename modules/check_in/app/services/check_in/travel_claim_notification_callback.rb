536be8638b049be12563c1acee557f4f1a183484
PhilipDeFraties <philip.defraties@gmail.com>
2025-05-30 16:51:08 -0600

    add travel claim notification callback class to log the delivery status of notifications

---
+++ modules/check_in/app/services/check_in/travel_claim_notification_callback.rb
@@ -0,0 +1,139 @@
+# frozen_string_literal: true
+
+module CheckIn
+  ##
+  # Custom callback class for handling VA Notify delivery status updates
+  # for travel claim SMS notifications.
+  #
+  # This class implements the VA Notify custom callback pattern to properly
+  # track actual delivery status rather than just API request success.
+  #
+  # @see https://github.com/department-of-veterans-affairs/vets-api/tree/master/modules/va_notify#custom-callback-handler
+  class TravelClaimNotificationCallback
+    ##
+    # Handles VA Notify callback with actual delivery status
+    #
+    # @param notification [VANotify::Notification] The notification record with status update
+    # @return [void]
+    def self.call(notification)
+      metadata = notification.callback_metadata || {}
+
+      message, log_level = case notification.status
+                          when 'delivered'
+                            handle_delivered
+                          when 'permanent-failure'
+                            handle_permanent_failure(metadata)
+                          when 'temporary-failure'
+                            handle_temporary_failure(metadata)
+                          else
+                            handle_other_status
+                          end
+
+      log_notification(notification, metadata, message, log_level)
+    end
+
+    ##
+    # Handles successful SMS delivery
+    #
+    # @return [Array<String, Symbol>] message and log level
+    def self.handle_delivered
+      StatsD.increment(Constants::STATSD_NOTIFY_DELIVERED)
+      ['Travel Claim Notification SMS successfully delivered', :info]
+    end
+
+    ##
+    # Handles permanent delivery failure
+    #
+    # @param metadata [Hash] callback metadata containing template_id
+    # @return [Array<String, Symbol>] message and log level
+    def self.handle_permanent_failure(metadata)
+      handle_failure_metrics(metadata)
+      ['Travel Claim Notification SMS delivery permanently failed', :error]
+    end
+
+    ##
+    # Handles temporary delivery failure (end-state)
+    #
+    # @param metadata [Hash] callback metadata containing template_id
+    # @return [Array<String, Symbol>] message and log level
+    def self.handle_temporary_failure(metadata)
+      handle_failure_metrics(metadata)
+      ['Travel Claim Notification SMS delivery temporarily failed (end-state)', :warn]
+    end
+
+    ##
+    # Handles unknown delivery status
+    #
+    # @return [Array<String, Symbol>] message and log level
+    def self.handle_other_status
+      StatsD.increment(Constants::STATSD_NOTIFY_ERROR)
+      ['Travel Claim Notification SMS has unknown status', :warn]
+    end
+
+    ##
+    # Common failure metrics handling for permanent and temporary failures
+    #
+    # @param metadata [Hash] callback metadata containing template_id
+    # @return [void]
+    def self.handle_failure_metrics(metadata)
+      template_id = metadata['template_id']
+      facility_type = determine_facility_type_from_template(template_id)
+
+      TravelClaimNotificationUtilities.increment_silent_failure_metrics(template_id, facility_type)
+      StatsD.increment(Constants::STATSD_NOTIFY_ERROR)
+    end
+
+    ##
+    # Builds common log data structure
+    #
+    # @param notification [VANotify::Notification] notification object
+    # @param metadata [Hash] callback metadata
+    # @param message [String] log message
+    # @param level [Symbol] log level
+    # @return [void]
+    def self.log_notification(notification, metadata, message, level)
+      template_id = metadata['template_id']
+      facility_type = determine_facility_type_from_template(template_id)
+
+      log_data = {
+        message:,
+        notification_id: notification.notification_id,
+        template_id:,
+        status: notification.status,
+        uuid: metadata['uuid'],
+        phone_last_four: phone_last_four_from_notification(notification)
+      }
+
+      unless notification.status == 'delivered'
+        log_data[:status_reason] = notification.status_reason
+        log_data[:facility_type] = facility_type if level == :warn
+      end
+
+      Rails.logger.public_send(level, log_data)
+    end
+
+    ##
+    # Extracts phone last four digits from notification
+    #
+    # @param notification [VANotify::Notification] notification object
+    # @return [String] last four digits of phone number
+    def self.phone_last_four_from_notification(notification)
+      TravelClaimNotificationUtilities.phone_last_four(notification.to)
+    end
+
+    ##
+    # Determines facility type based on template ID
+    #
+    # @param template_id [String] The template ID
+    # @return [String] 'oh' or 'cie'
+    def self.determine_facility_type_from_template(template_id)
+      if template_id == 'cie-failure-template-id' ||
+         [Constants::CIE_FAILURE_TEMPLATE_ID, Constants::CIE_ERROR_TEMPLATE_ID,
+          Constants::CIE_TIMEOUT_TEMPLATE_ID].include?(template_id)
+        'cie'
+      else
+        'oh'
+      end
+    end
+  end
+end
