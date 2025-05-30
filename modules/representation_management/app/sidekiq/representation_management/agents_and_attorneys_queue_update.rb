f45d9ae7066770882888531f13a8f16e615ac3f4
Josh Fike <josh.fike@oddball.io>
2025-05-30 15:10:37 -0500

    Queue address updates, delete old records

--- modules/representation_management/app/sidekiq/representation_management/agents_and_attorneys_queue_update.rb
+++ modules/representation_management/app/sidekiq/representation_management/agents_and_attorneys_queue_update.rb
@@ -6,20 +6,21 @@ module RepresentationManagement
   class AgentsAndAttorneysQueueUpdate
     include Sidekiq::Job
 
+    SLICE_SIZE = 30
+
     def perform
       @agent_responses = []
       @attorney_responses = []
       @agent_ids = []
       @attorney_ids = []
-      @agent_ids_for_address_validation = []
-      @attorney_ids_for_address_validation = []
+      @agent_json_for_address_validation = []
+      @attorney_json_for_address_validation = []
 
       update_agents
       update_attorneys
       validate_agent_addresses
       validate_attorney_addresses
-      deactivate_agents
-      deactivate_attorneys
+      delete_old_accredited_individuals
     rescue => e
       log_error("Error in AgentsAndAttorneysQueueUpdate: #{e.message}")
     end
@@ -67,16 +68,45 @@ module RepresentationManagement
       }
     end
 
-    def deactivate_agents
-      AccreditedIndividual.where(individual_type: 'agent').where.not(id: @agent_ids).find_each do |agent|
-        agent.update(deactivated_at: Time.zone.now)
+    def delete_old_accredited_individuals
+      AccreditedIndividual.where.not(id: @agent_ids + @attorney_ids).find_each do |record|
+        record.destroy
+      rescue => e
+        log_error("Error deleting old accredited individual with ID #{record.id}: #{e.message}")
       end
     end
 
-    def deactivate_attorneys
-      AccreditedIndividual.where(individual_type: 'attorney').where.not(id: @attorney_ids).find_each do |attorney|
-        attorney.update(deactivated_at: Time.zone.now)
-      end
+    def individual_agent_json(record, agent)
+      agent_raw_address = raw_address_for_agent(agent)
+      {
+        id: record.id,
+        address_changed:,
+        address: {
+          address_pou: 'RESIDENCE/CHOICE',
+          address_line1: agent_raw_address['address_line1'],
+          address_line2: agent_raw_address['address_line2'],
+          address_line3: agent_raw_address['address_line3'],
+          city: nil,
+          zip_code5: agent_raw_address['zip_code']
+        }
+      }
+    end
+
+    def individual_attorney_json(record, attorney)
+      attorney_raw_address = raw_address_for_attorney(attorney)
+      {
+        id: record.id,
+        address_changed:,
+        address: {
+          address_pou: 'RESIDENCE/CHOICE',
+          address_line1: attorney_raw_address['address_line1'],
+          address_line2: attorney_raw_address['address_line2'],
+          address_line3: attorney_raw_address['address_line3'],
+          city: attorney_raw_address['city'],
+          state: { state_code: attorney_raw_address['state_code'] },
+          zip_code5: attorney_raw_address['zip_code']
+        }
+      }
     end
 
     def raw_address_for_agent(agent)
@@ -102,7 +132,6 @@ module RepresentationManagement
 
     def update_agents
       page = 1
-
       loop do
         response = RepresentationManagement::GCLAWS::Client.get_accredited_entities(type: 'agents', page:)
         agents = response.body['items']
@@ -113,7 +142,9 @@ module RepresentationManagement
           agent_identifier = { individual_type: 'claims_agent', ogc_id: agent['id'] }
           agent_hash = data_transform_for_agent(agent)
           record = AccreditedIndividual.find_or_create_by(agent_identifier)
-          @agent_ids_for_address_validation << record.id if record.raw_address != raw_address_for_agent(agent)
+          if record.raw_address != raw_address_for_agent(agent)
+            @agent_json_for_address_validation << individual_agent_json(record, agent)
+          end
           record.update(agent_hash)
           @agent_ids << record.id
         end
@@ -133,7 +164,9 @@ module RepresentationManagement
           attorney_identifier = { individual_type: 'attorney', ogc_id: attorney['id'] }
           attorney_hash = data_transform_for_attorney(attorney)
           record = AccreditedIndividual.find_or_create_by(attorney_identifier)
-          @attorney_ids_for_address_validation << record.id if record.raw_address != raw_address_for_attorney(attorney)
+          if record.raw_address != raw_address_for_attorney(attorney)
+            @attorney_json_for_address_validation << individual_attorney_json(record, attorney)
+          end
           record.update(attorney_hash)
           @attorney_ids << record.id
         end
@@ -141,16 +174,41 @@ module RepresentationManagement
       end
     end
 
+    def validate_addresses(records_for_validation,
+                           description = 'Batching address updates from GCLAWS Accreditation API')
+      return if records_for_validation.empty?
+
+      delay = 0
+      batch = Sidekiq::Batch.new
+      batch.description = description
+
+      begin
+        batch.jobs do
+          records_for_validation.each_slice(SLICE_SIZE) do |individuals|
+            json_individuals = individuals.to_json
+            RepresentationManagement::AccreditedIndividualsUpdate.perform_in(delay.minutes, json_individuals)
+            delay += 1
+          end
+        end
+      rescue => e
+        log_error("Error queuing address updates: #{e.message}")
+      end
+    end
+
     def validate_agent_addresses
-      nil if @agent_ids_for_address_validation.empty?
+      validate_addresses(
+        @agent_json_for_address_validation, 'Batching agent address updates from GCLAWS Accreditation API'
+      )
     end
 
     def validate_attorney_addresses
-      nil if @attorney_ids_for_address_validation.empty?
+      validate_addresses(
+        @attorney_json_for_address_validation, 'Batching attorney address updates from GCLAWS Accreditation API'
+      )
     end
 
     def log_error(message)
-      Rails.logger.error("SendExpiredEmailJob error: #{message}")
+      Rails.logger.error("RepresentationManagement::AgentsAndAttorneysQueueUpdate error: #{message}")
     end
   end
 end
