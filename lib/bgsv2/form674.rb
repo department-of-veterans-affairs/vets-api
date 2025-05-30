3d370ebbf1d45081b31419eff91617aac6f202e1
Evan Smith <evan.smith@oddball.io>
2025-05-30 09:10:09 -0400

    update lib bgsv2 references to remove v1 values, update calls to bgsdependentsv2

--- lib/bgsv2/form674.rb
+++ lib/bgsv2/form674.rb
@@ -98,14 +98,10 @@ module BGSV2
       # use this to make sure the created dependent and student payload line up for process_674
       # if it's nil, it is v1.
       dependent_student_map = {}
-      if Flipper.enabled?(:va_dependents_v2)
-        payload&.dig('dependents_application', 'student_information').to_a.each do |student|
-          dependent = DependentHigherEdAttendance.new(proc_id:, payload:, user: @user, student:).create
-          dependents << dependent
-          dependent_student_map[dependent[:vnp_participant_id]] = student
-        end
-      else
-        dependents << DependentHigherEdAttendance.new(proc_id:, payload:, user: @user, student: nil).create
+      payload&.dig('dependents_application', 'student_information').to_a.each do |student|
+        dependent = DependentHigherEdAttendance.new(proc_id:, payload:, user: @user, student:).create
+        dependents << dependent
+        dependent_student_map[dependent[:vnp_participant_id]] = student
       end
 
       VnpRelationships.new(
