ef64c358f3db790c99831ca3a29f779a1f26dcac
Tai Wilkin-Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-05-30 10:48:28 -0400

    Remove v1 code from bgs_dependents_v2 (#22469)
    * Remove v1 code from bgs_dependents_v2
    
    * Remove _v2 ending
    
    * Remove unused param
    
    * Update tests

--- app/models/bgs_dependents_v2/death.rb
+++ app/models/bgs_dependents_v2/death.rb
@@ -4,17 +4,16 @@ module BGSDependentsV2
   class Death < Base
     def initialize(death_info) # rubocop:disable Lint/MissingSuper
       @death_info = death_info
-      @is_v2 = v2?
     end
 
     def format_info
       dependent_type = relationship_type(@death_info)
       info = {
-        death_date: @is_v2 ? format_date(@death_info['dependent_death_date']) : format_date(@death_info['date']),
+        death_date: format_date(@death_info['dependent_death_date']),
         ssn: @death_info['ssn'],
         birth_date: @death_info['birth_date'],
         vet_ind: 'N',
-        dependent_income: formatted_boolean(@is_v2 ? @death_info['deceased_dependent_income'] : @death_info['dependent_income']) # rubocop:disable Layout/LineLength
+        dependent_income: formatted_boolean(@death_info['deceased_dependent_income'])
       }
       info['marriage_termination_type_code'] = 'Death' if dependent_type[:family] == 'Spouse'
       info.merge(@death_info['full_name']).with_indifferent_access
