2c2259524906d93899d4a068c7c3a44113b56efe
Brandon Cooper <brandon.cooper@adhocteam.us>
2025-06-02 12:39:38 -0400

    use find_or_create_by to ensure we do no try to create duplicate log record

--- app/sidekiq/hca/log_email_diff_job.rb
+++ app/sidekiq/hca/log_email_diff_job.rb
@@ -6,8 +6,11 @@ module HCA
     sidekiq_options retry: false
 
     def perform(in_progress_form_id, user_uuid, user_account_id)
-      return if FormEmailMatchesProfileLog.exists?(user_uuid:, in_progress_form_id:) ||
-                FormEmailMatchesProfileLog.exists?(user_account_id:, in_progress_form_id:)
+      return if FormEmailMatchesProfileLog.exists?(
+        user_uuid:,
+        user_account_id:,
+        in_progress_form_id:
+      )
 
       in_progress_form = InProgressForm.find_by(id: in_progress_form_id)
       return if in_progress_form.nil?
@@ -22,11 +25,17 @@ module HCA
 
       tag_text = va_profile_email&.downcase == form_email.downcase ? 'same' : 'different'
 
-      StatsD.increment(
-        "api.1010ez.in_progress_form_email.#{tag_text}"
+      email_diff_log = FormEmailMatchesProfileLog.find_or_create_by(
+        user_uuid:,
+        user_account_id:,
+        in_progress_form_id:
       )
 
-      FormEmailMatchesProfileLog.create(user_uuid:, user_account_id:, in_progress_form_id:)
+      if email_diff_log.persisted? && email_diff_log.created_at == email_diff_log.updated_at
+        StatsD.increment(
+          "api.1010ez.in_progress_form_email.#{tag_text}"
+        )
+      end
     end
   end
 end
