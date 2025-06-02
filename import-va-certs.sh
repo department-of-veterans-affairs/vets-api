e7e14f7217e7595e757f726ef1b419d672e2e65a
Kyle A. Matheny <1580012+ph-One@users.noreply.github.com>
2025-06-02 13:04:52 -0400

    chore: Add ECA Root CA 5
    - Trust new Issuer
    - https://dsva.slack.com/archives/CBU0KDSB1/p1748878524213609

--- import-va-certs.sh
+++ import-va-certs.sh
@@ -8,6 +8,15 @@ set -euo pipefail
     curl -LO https://cacerts.digicert.com/DigiCertTLSRSASHA2562020CA1-1.crt.pem
     curl -LO https://digicert.tbs-certificats.com/DigiCertGlobalG2TLSRSASHA2562020CA1.crt
 
+    # DoD ECA
+    (
+        curl -LO https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/unclass-certificates_pkcs7_ECA.zip
+        unzip ./unclass-certificates_pkcs7_ECA.zip
+        cd certificates_pkcs7_v5_12_eca
+        openssl pkcs7 -inform DER -in ./certificates_pkcs7_v5_12_eca_ECA_Root_CA_5_der.p7b -print_certs | awk '/BEGIN/{i++} {print > ("eca_cert" i ".pem")}'
+        cp *.pem ../
+    )
+
     wget \
         --level=1 \
         --quiet \
