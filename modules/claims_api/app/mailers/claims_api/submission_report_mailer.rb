8676ef13324d95bfe4e528a96280c7d505012f74
Adam F <va@freemer.com>
2025-06-02 18:08:01 -0400

    Update submission_report_mailer.rb

--- modules/claims_api/app/mailers/claims_api/submission_report_mailer.rb
+++ modules/claims_api/app/mailers/claims_api/submission_report_mailer.rb
@@ -3,7 +3,7 @@
 module ClaimsApi
   class SubmissionReportMailer < ApplicationMailer
     RECIPIENTS = %w[
-		  afreemer@technatomy.com
+			afreemer@technatomy.com
       david.mazik@va.gov
       drew.fisher@adhocteam.us
       janet.coutinho@va.gov
