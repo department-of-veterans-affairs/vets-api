377b2f7ddd4c3a8f23d7b9c1d63fbe2ac33a4001
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-06-02 15:04:44 -0400

    [mhv-65401] remove old sort code.

--- modules/my_health/app/helpers/my_health/prescription_helper.rb
+++ modules/my_health/app/helpers/my_health/prescription_helper.rb
@@ -18,68 +18,6 @@ module MyHealth
         data.reject { |item| item.prescription_source == 'NV' && item.disp_status != 'Active: Non-VA' }
       end
 
-      def sort_by(resource, sort_params)
-        sort_orders = sort_params.map { |param| param.to_s.start_with?('-') }
-        resource.records = resource.data.sort do |a, b|
-          comparison = 0
-          sort_params.each_with_index do |field, index|
-            is_descending = sort_orders[index]
-            field_a, field_b = populate_sort_vars(field, a, b, is_descending)
-            comparison = compare_fields(field_a, field_b, is_descending)
-            break if !comparison.zero? || field.nil?
-          end
-          comparison
-        end
-        sort_params.each_with_index do |field, index|
-          field_camelcase = field.sub(/^-/, '').gsub(/_([a-z])/) { ::Regexp.last_match(1).upcase }
-          if field.present?
-            (resource.metadata[:sort] ||= {}).merge!({ field_camelcase => sort_orders[index] ? 'DESC' : 'ASC' })
-          end
-        end
-        resource
-      end
-
-      def compare_fields(field_a, field_b, is_descending)
-        if field_a > field_b
-          is_descending ? -1 : 1
-        elsif field_a < field_b
-          is_descending ? 1 : -1
-        else
-          0
-        end
-      end
-
-      def populate_sort_vars(field, a, b, is_descending)
-        if field.nil?
-          [nil, nil]
-        else
-          [get_field_data(field, a, is_descending), get_field_data(field, b, is_descending)]
-        end
-      end
-
-      def get_field_data(field, data, is_descending)
-        case field
-        when /dispensed_date/
-          if data.sorted_dispensed_date.nil?
-            is_descending ? Date.new(9999, 12, 31) : Date.new(0, 1, 1)
-          else
-            data.sorted_dispensed_date
-          end
-        when 'prescription_name'
-          if data.disp_status == 'Active: Non-VA' && data.prescription_name.nil?
-            data.orderable_item
-          elsif !data.prescription_name.nil?
-            data.prescription_name
-          else
-            '~'
-          end
-        when 'disp_status'
-          data.disp_status
-        else
-          data.public_send(field.sub(/^-/, '').to_sym)
-        end
-      end
-
       def filter_data_by_refill_and_renew(data)
         data.select do |item|
           next true if item.is_refillable
