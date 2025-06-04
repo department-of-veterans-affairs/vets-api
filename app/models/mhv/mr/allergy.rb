6830b720a38cfdba008055da2af4e357b13d206f
Mike Moyer <87040148+mmoyer-va@users.noreply.github.com>
2025-06-04 19:49:45 -0400

    MHV-71298 Added new allergy model and logic

---
+++ app/models/mhv/mr/allergy.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'common/models/base'
+
+module MHV
+  module MR
+    class Allergy < MHV::MR::FHIRRecord
+      include RedisCaching
+
+      redis_config REDIS_CONFIG[:medical_records_cache]
+
+      attribute :id,               String
+      attribute :name,             String
+      attribute :date,             String # Pass on as-is to the frontend
+      attribute :categories,       String, array: true
+      attribute :reactions,        String, array: true
+      attribute :location,         String
+      attribute :observedHistoric, String # 'o' or 'h'
+      attribute :notes,            String
+      attribute :provider,         String
+
+      def self.map_fhir(fhir)
+        {
+          id: fhir.id,
+          name: fhir.code&.text,
+          date: fhir.recordedDate,
+          categories: categories(fhir),
+          reactions: reactions(fhir),
+          location: location_name(fhir),
+          observedHistoric: observed_historic(fhir),
+          notes: note_text(fhir),
+          provider: provider(fhir)
+        }
+      end
+
+      def self.categories(fhir)
+        fhir.category || []
+      end
+
+      def self.reactions(fhir)
+        Array(fhir.reaction).flat_map do |r|
+          Array(r.manifestation).map { |m| m&.text }.compact
+        end
+      end
+
+      def self.location_name(fhir)
+        ref = fhir.recorder&.extension&.first&.valueReference&.reference
+        find_contained(fhir, ref, type: 'Location')&.name
+      end
+
+      def self.observed_historic(fhir)
+        ext = Array(fhir.extension).find { |e| e.url&.include?('allergyObservedHistoric') }
+        ext&.valueCode
+      end
+
+      def self.note_text(fhir)
+        fhir.note&.first&.text
+      end
+
+      def self.provider(fhir)
+        fhir.recorder&.display
+      end
+    end
+  end
+end
--- modules/my_health/app/controllers/my_health/v1/allergies_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/allergies_controller.rb
@@ -4,12 +4,33 @@ module MyHealth
   module V1
     class AllergiesController < MRController
       def index
-        render_resource client.list_allergies
+        if Flipper.enabled?(:mhv_medical_records_support_new_model_allergy)
+          with_patient_resource(client.list_allergies(@current_user.uuid)) do |resource|
+            resource = resource.sort('-date')
+            if pagination_params[:per_page]
+              resource = resource.paginate(**pagination_params)
+              links = pagination_links(resource) if pagination_params[:per_page]
+            end
+            options = { meta: resource.metadata, links: }
+            render json: AllergySerializer.new(resource.data, options)
+          end
+        else
+          render_resource client.list_allergies(@current_user.uuid)
+        end
       end
 
       def show
         allergy_id = params[:id].try(:strip)
-        render_resource client.get_allergy(allergy_id)
+        if Flipper.enabled?(:mhv_medical_records_support_new_model_allergy)
+          with_patient_resource(client.get_allergy(allergy_id)) do |resource|
+            raise Common::Exceptions::RecordNotFound, allergy_id if resource.blank?
+
+            options = { meta: resource.metadata }
+            render json: AllergySerializer.new(resource, options)
+          end
+        else
+          render_resource client.get_allergy(allergy_id)
+        end
       end
     end
   end
