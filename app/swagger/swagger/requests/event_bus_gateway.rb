876394c1b8c1b79841862eb1547689b77d013395
Ian Donovan <iandonovan@users.noreply.github.com>
2025-06-04 11:41:02 -0400

    Some iteration on the swagger docs

--- app/swagger/swagger/requests/event_bus_gateway.rb
+++ app/swagger/swagger/requests/event_bus_gateway.rb
@@ -8,14 +8,19 @@ module Swagger
       swagger_path '/v0/event_bus_gateway/send_email' do
         operation :post do
           extend Swagger::Responses::AuthenticationError
-
           key :description, 'Sends an email to a veteran about an event managed by Event Bus Gateway'
           key :operationId, 'sendEmail'
-          key :tags, %w[
-            event_bus_gateway
-          ]
+          key :tags, %w[event_bus_gateway]
 
-          parameter :template_id
+          parameter :authorization
+
+          parameter do
+            key :name, :template_id
+            key :in, :body
+            key :description, 'VA Notify template ID'
+            key :required, true
+            key :type, :string
+          end
 
           response 200 do
             key :description, 'Response is OK'
