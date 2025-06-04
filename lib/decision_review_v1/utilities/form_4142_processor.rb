3886123af2081404d4cbe9e5b732285572b86a34
Jerry Sea <Jsea@kindsys.us>
2025-06-04 07:45:15 -0700

    Refactor 4142 processors and update datestamp to add signature

--- lib/decision_review_v1/utilities/form_4142_processor.rb
+++ lib/decision_review_v1/utilities/form_4142_processor.rb
@@ -1,88 +1,29 @@
 # frozen_string_literal: true
 
-require 'pdf_fill/filler'
-require 'pdf_utilities/datestamp_pdf'
+require 'pdf_fill/processors/base_form_4142_processor'
 require 'decision_review_v1/utilities/constants'
-require 'simple_forms_api_submission/metadata_validator'
 
 module DecisionReviewV1
   module Processor
-    class Form4142ValidationError < StandardError
-      def initialize(errors)
-        super
-        @errors = errors
-      end
-    end
-
-    class Form4142Processor
-      SIGNATURE_DATE_KEY = 'signatureDate'
-      SIGNATURE_TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S'
-      TIMEZONE = 'Central Time (US & Canada)'
-      # @return [Pathname] the generated PDF path
-      attr_reader :pdf_path
-
-      # @return [Hash] the generated request body
-      attr_reader :request_body
-
-      def initialize(form_data:, submission_id: nil)
+    class Form4142Processor < Processors::BaseForm4142Processor
+      def initialize(form_data:, submission_id: nil, validate: true)
         @submission = Form526Submission.find_by(id: submission_id)
         @form = set_signature_date(form_data)
-        validate_form4142
-        @pdf_path = generate_stamp_pdf
-        @request_body = {
-          'document' => to_faraday_upload,
-          'metadata' => generate_metadata
-        }
+        super(validate:) # Pass validate flag to parent
       end
 
-      def uuid
-        @uuid ||= SecureRandom.uuid
+      protected
+
+      def form_data
+        @form
       end
 
-      def generate_stamp_pdf
-        pdf = PdfFill::Filler.fill_ancillary_form(@form, uuid, FORM_ID)
-        stamped_path = PDFUtilities::DatestampPdf.new(pdf).run(text: 'VA.gov', x: 5, y: 5,
-                                                               timestamp: submission_date)
-        PDFUtilities::DatestampPdf.new(stamped_path).run(
-          text: 'VA.gov Submission',
-          x: 510,
-          y: 775,
-          text_only: true
-        )
+      def pdf_identifier
+        uuid
       end
 
-      private
-
-      def to_faraday_upload
-        Faraday::UploadIO.new(
-          @pdf_path,
-          Mime[:pdf].to_s
-        )
-      end
-
-      def generate_metadata
-        address = @form['veteranAddress']
-        country_is_us = address['country'] == 'US'
-        veteran_full_name = @form['veteranFullName']
-        metadata = {
-          'veteranFirstName' => veteran_full_name['first'],
-          'veteranLastName' => veteran_full_name['last'],
-          'fileNumber' => @form['vaFileNumber'] || @form['veteranSocialSecurityNumber'],
-          'receiveDt' => received_date,
-          # 'uuid' => "#{@uuid}_4142", # was trying to include the main claim uuid here and just append 4142
-          # but central mail portal does not support that
-          'uuid' => uuid,
-          'zipCode' => address['postalCode'],
-          'source' => 'VA Forms Group B',
-          'hashV' => Digest::SHA256.file(@pdf_path).hexdigest,
-          'numberAttachments' => 0,
-          'docType' => FORM_ID,
-          'numberPages' => PDF::Reader.new(@pdf_path).pages.size
-        }
-
-        SimpleFormsApiSubmission::MetadataValidator.validate(
-          metadata, zip_code_is_us_based: country_is_us
-        ).to_json
+      def metadata_uuid
+        uuid
       end
 
       def submission_date
@@ -93,24 +34,15 @@ module DecisionReviewV1
         end
       end
 
-      def received_date
-        submission_date.strftime(SIGNATURE_TIMESTAMP_FORMAT)
+      # Flip this on (via Flipper) to use the 2024 template
+      def generate_2024_version?
+        Flipper.enabled?(:decision_review_form4142_use_2024_template)
       end
 
-      def set_signature_date(incoming_data)
-        incoming_data.merge({ SIGNATURE_DATE_KEY => received_date })
-      end
+      private
 
-      def validate_form4142
-        return unless Flipper.enabled?(:form4142_validate_schema)
-
-        schema = VetsJsonSchema::SCHEMAS[FORM_ID]
-        errors = JSON::Validator.fully_validate(schema, @form, errors_as_objects: true)
-
-        unless errors.empty?
-          Rails.logger.error('Form 4142 failed validation', { errors: })
-          raise Form4142ValidationError.new({ errors: })
-        end
+      def uuid
+        @uuid ||= SecureRandom.uuid
       end
     end
   end
--- lib/evss/disability_compensation_form/form4142_processor.rb
+++ lib/evss/disability_compensation_form/form4142_processor.rb
@@ -6,103 +6,39 @@ require 'pdf_fill/forms/va210781'
 require 'pdf_fill/forms/va218940'
 require 'pdf_fill/forms/va1010cg'
 require 'pdf_utilities/datestamp_pdf'
+require 'pdf_fill/processors/base_form_4142_processor'
 require 'simple_forms_api_submission/metadata_validator'
 
 module EVSS
   module DisabilityCompensationForm
-    # A {Form4142Processor} handles the work of generating a stamped PDF
-    # and a request body for a 4142 CentralMail submission
-    #
-    class Form4142Processor
-      # @return [Pathname] the generated PDF path
-      attr_reader :pdf_path
-
-      # @return [Hash] the generated request body
-      attr_reader :request_body
-
-      FORM_ID = '21-4142'
-
-      # @param submission [Form526Submission] a user's post-translated 526 submission
-      # @param jid [String] the Sidekiq job id for the job submitting the 4142 form
-      #
-      # @return [EVSS::DisabilityCompensationForm::Form4142Processor] an instance of this class
-      #
-      def initialize(submission, jid)
+    class Form4142Processor < Processors::BaseForm4142Processor
+      def initialize(submission, jid, validate: true)
         @submission = submission
-        @pdf_path = generate_stamp_pdf
-        @request_body = {
-          'document' => to_faraday_upload,
-          'metadata' => generate_metadata(jid)
-        }
+        @jid = jid
+        super(validate:) # Pass validate flag to parent
       end
 
-      # Invokes Filler ancillary form method to generate PDF document
-      # Then calls method PDFUtilities::DatestampPdf to stamp the document.
-      # Its called twice, once to stamp with text "VA.gov YYYY-MM-DD" at the bottom of each page
-      # and second time to stamp with text "FDC Reviewed - Vets.gov Submission" at the top of each page
-      #
-      # @return [Pathname] the stamped PDF path
-      #
-      def generate_stamp_pdf
-        pdf = PdfFill::Filler.fill_ancillary_form(
-          form4142, @submission.submitted_claim_id, FORM_ID
-        )
-        stamped_path = PDFUtilities::DatestampPdf.new(pdf).run(text: 'VA.gov', x: 5, y: 5,
-                                                               timestamp: submission_date)
-        PDFUtilities::DatestampPdf.new(stamped_path).run(
-          text: 'VA.gov Submission',
-          x: 510,
-          y: 775,
-          text_only: true
-        )
+      protected
+
+      def form_data
+        @form_data ||= set_signature_date(@submission.form[Form526Submission::FORM_4142])
       end
 
-      private
-
-      def to_faraday_upload
-        Faraday::UploadIO.new(
-          @pdf_path,
-          Mime[:pdf].to_s
-        )
+      def pdf_identifier
+        @submission.submitted_claim_id
       end
 
-      def generate_metadata(jid)
-        address = form4142['veteranAddress']
-        country_is_us = address['country'] == 'USA'
-        veteran_full_name = form4142['veteranFullName']
-        metadata = {
-          'veteranFirstName' => veteran_full_name['first'],
-          'veteranLastName' => veteran_full_name['last'],
-          'fileNumber' => form4142['vaFileNumber'] || form4142['veteranSocialSecurityNumber'],
-          'receiveDt' => received_date,
-          'uuid' => jid,
-          'zipCode' => address['postalCode'],
-          'source' => 'VA Forms Group B',
-          'hashV' => Digest::SHA256.file(@pdf_path).hexdigest,
-          'numberAttachments' => 0,
-          'docType' => FORM_ID,
-          'numberPages' => PDF::Reader.new(@pdf_path).pages.size
-        }
-
-        SimpleFormsApiSubmission::MetadataValidator.validate(
-          metadata, zip_code_is_us_based: country_is_us
-        ).to_json
+      def metadata_uuid
+        @jid
       end
 
       def submission_date
-        @submission.created_at.in_time_zone('Central Time (US & Canada)')
+        @submission.created_at.in_time_zone(TIMEZONE)
       end
 
-      def received_date
-        submission_date.strftime('%Y-%m-%d %H:%M:%S')
-      end
-
-      def form4142
-        @form4142 ||= set_signature_date(@submission.form[Form526Submission::FORM_4142])
-      end
-
-      def set_signature_date(incoming_data)
-        incoming_data.merge({ 'signatureDate' => received_date })
+      # Flip this on to use the 2024 template, teams can make independent decisions about when to use the 2024 version
+      def generate_2024_version?
+        Flipper.enabled?(:evss_form4142_use_2024_template)
       end
     end
   end
