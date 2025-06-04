b9e37f1c3b6e05da34cabcd7a9ae71770fa048f9
Ian Donovan <iandonovan@users.noreply.github.com>
2025-06-04 11:19:34 -0400

    Rubocop and some testing changes

--- spec/controllers/v0/event_bus_gateway_controller_spec.rb
+++ spec/controllers/v0/event_bus_gateway_controller_spec.rb
@@ -11,29 +11,31 @@ RSpec.describe V0::EventBusGatewayController, type: :request do
       }
     end
 
-    context 'when :event_bus_gateway_emails_enabled is enabled' do
-      before do
-        allow(Flipper).to receive(:enabled?).and_call_original
-        allow(Flipper).to receive(:enabled?).with(:event_bus_gateway_emails_enabled).and_return(true)
+    describe 'POST /v0/event_bus_gateway/send_email' do
+      context 'when :event_bus_gateway_emails_enabled is enabled' do
+        before do
+          allow(Flipper).to receive(:enabled?).and_call_original
+          allow(Flipper).to receive(:enabled?).with(:event_bus_gateway_emails_enabled).and_return(true)
+        end
+
+        it 'invokes the email-sending job' do
+          expect(EventBusGateway::LetterReadyEmailJob).to receive(:perform_async).with('1234', '5678')
+          post v0_event_bus_gateway_send_email_path(params:), headers: service_account_auth_header
+          expect(response).to have_http_status(:ok)
+        end
       end
 
-      it 'invokes the email-sending job' do
-        expect(EventBusGateway::LetterReadyEmailJob).to receive(:perform_async).with('1234', '5678')
-        post v0_event_bus_gateway_send_email_path(params:), headers: service_account_auth_header
-        expect(response).to have_http_status(:ok)
-      end
-    end
+      context 'when :event_bus_gateway_emails_enabled is disabled' do
+        before do
+          allow(Flipper).to receive(:enabled?).and_call_original
+          allow(Flipper).to receive(:enabled?).with(:event_bus_gateway_emails_enabled).and_return(false)
+        end
 
-    context 'when :event_bus_gateway_emails_enabled is disabled' do
-      before do
-        allow(Flipper).to receive(:enabled?).and_call_original
-        allow(Flipper).to receive(:enabled?).with(:event_bus_gateway_emails_enabled).and_return(false)
-      end
-
-      it 'does not invoke the email-sending job' do
-        expect(EventBusGateway::LetterReadyEmailJob).not_to receive(:perform_async)
-        post v0_event_bus_gateway_send_email_path(params:), headers: service_account_auth_header
-        expect(response).to have_http_status(:ok)
+        it 'does not invoke the email-sending job' do
+          expect(EventBusGateway::LetterReadyEmailJob).not_to receive(:perform_async)
+          post v0_event_bus_gateway_send_email_path(params:), headers: service_account_auth_header
+          expect(response).to have_http_status(:ok)
+        end
       end
     end
   end
