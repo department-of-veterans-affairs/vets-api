86c9d026fb434d5683610b5ecdb4b78e5db06c9b
Ian Donovan <iandonovan@users.noreply.github.com>
2025-06-05 22:26:19 -0400

    Moves test over to requests/ folder in an attempt to ameliorate APIVore

--- spec/requests/v0/event_bus_gateway_spec.rb
+++ spec/requests/v0/event_bus_gateway_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe V0::EventBusGatewayController, type: :request do
+RSpec.describe 'V0::EventBusGateway', type: :request do
   include_context 'with service account authentication', 'eventbus', ['http://www.example.com/v0/event_bus_gateway/send_email'], { user_attributes: { participant_id: '1234' } }
   describe 'POST /v0/event_bus_gateway/send_email' do
     let(:params) do
@@ -20,14 +20,14 @@ RSpec.describe V0::EventBusGatewayController, type: :request do
       context 'with the authentication header included' do
         it 'invokes the email-sending job' do
           expect(EventBusGateway::LetterReadyEmailJob).to receive(:perform_async).with('1234', '5678')
-          post v0_event_bus_gateway_send_email_path(params:), headers: service_account_auth_header
+          post '/v0/event_bus_gateway/send_email', params:, headers: service_account_auth_header
           expect(response).to have_http_status(:ok)
         end
       end
 
       context 'without the authentication header' do
         it 'returns an unauthorized response' do
-          post v0_event_bus_gateway_send_email_path(params:), headers: nil
+          post '/v0/event_bus_gateway/send_email', params:, headers: nil
           expect(response).to have_http_status(:unauthorized)
         end
       end
@@ -41,7 +41,7 @@ RSpec.describe V0::EventBusGatewayController, type: :request do
 
       context 'without the authentication header' do
         it 'returns an unauthorized response' do
-          post v0_event_bus_gateway_send_email_path(params:), headers: nil
+          post '/v0/event_bus_gateway/send_email', params:, headers: nil
           expect(response).to have_http_status(:unauthorized)
         end
       end
@@ -49,7 +49,7 @@ RSpec.describe V0::EventBusGatewayController, type: :request do
       context 'with the authentication header included' do
         it 'does not invoke the email-sending job' do
           expect(EventBusGateway::LetterReadyEmailJob).not_to receive(:perform_async)
-          post v0_event_bus_gateway_send_email_path(params:), headers: service_account_auth_header
+          post '/v0/event_bus_gateway/send_email', params:, headers: service_account_auth_header
           expect(response).to have_http_status(:ok)
         end
       end
