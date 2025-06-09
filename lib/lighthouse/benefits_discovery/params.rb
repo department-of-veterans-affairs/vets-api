979845dc18236d0c9d544d8b35652fe7b94fad7b
Kris Pethtel <kris.pethtel@gmail.com>
2025-06-09 16:08:06 -0500

    wip

--- lib/lighthouse/benefits_discovery/params.rb
+++ lib/lighthouse/benefits_discovery/params.rb
@@ -16,16 +16,28 @@ module BenefitsDiscovery
     def prepared_params
       {
         date_of_birth: @user.birth_date,
-        discharge_status: ,
-        branch_of_service: ,
-        disability_rating: ?,
-        service_start_date: ,
-        service_end_date:
+        discharge_status: service_history.character_of_discharge_code,
+        branch_of_service: service_history.branch_of_service,
+        disability_rating: ,
+        service_start_date: service_history.begin_date,
+        service_end_date: service_history.end_date
       }
     end
 
     private
 
-    
+    def disability_rating
+      service = VeteranVerification::Service.new
+      service.get_rated_disabilities(@user.icn)
+    end
+
+    def service_history
+      service = VAProfile::MilitaryPersonnel::Service.new
+      response = service.get_service_history
+      json = JSON.parse(response.episodes.to_json, symbolize_names: true)
+
+      # service = VAProfile::MilitaryPersonnel::Service.new(@current_user)
+      # response = service.get_service_history
+    end
   end
 end
