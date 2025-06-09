9d6e717fd034409c34291c7fd86ff92da11668e0
Bryan Alexander <bryan.alexander@adhocteam.us>
2025-06-09 15:22:04 -0500

    103352: Update logic to use batches and rescues

--- db/migrate/20250605205239_migrate_saved_claim_pension_to_pensions_saved_claim.rb
+++ db/migrate/20250605205239_migrate_saved_claim_pension_to_pensions_saved_claim.rb
@@ -1,11 +1,37 @@
 class MigrateSavedClaimPensionToPensionsSavedClaim < ActiveRecord::Migration[7.2]
+  BATCH_SIZE = 1000
+
+  disable_ddl_transaction!
+
   def up
-    # Update all records with the old STI type to the new one
-    SavedClaim.where(type: 'SavedClaim::Pension').update_all(type: 'Pensions::SavedClaim')
+    say_with_time "Migrating SavedClaim::Pension to Pensions::SavedClaim" do
+      loop do
+        ids = SavedClaim.where(type: 'SavedClaim::Pension').limit(BATCH_SIZE).pluck(:id)
+        break if ids.empty?
+
+        begin
+          SavedClaim.where(id: ids).update_all(type: 'Pensions::SavedClaim')
+        rescue => e
+          Rails.logger.error("MigrateSavedClaimPensionToPensionsSavedClaim batch failed for IDs: #{ids.inspect} - #{e.class}: #{e.message}")
+          raise
+        end
+      end
+    end
   end
 
   def down
-    # Rollback: revert to the old type
-    SavedClaim.where(type: 'Pensions::SavedClaim').update_all(type: 'SavedClaim::Pension')
+    say_with_time "Reverting Pensions::SavedClaim to SavedClaim::Pension" do
+      loop do
+        ids = SavedClaim.where(type: 'Pensions::SavedClaim').limit(BATCH_SIZE).pluck(:id)
+        break if ids.empty?
+
+        begin
+          SavedClaim.where(id: ids).update_all(type: 'SavedClaim::Pension')
+        rescue => e
+          Rails.logger.error("MigrateSavedClaimPensionToPensionsSavedClaim rollback batch failed for IDs: #{ids.inspect} - #{e.class}: #{e.message}")
+          raise
+        end
+      end
+    end
   end
 end
