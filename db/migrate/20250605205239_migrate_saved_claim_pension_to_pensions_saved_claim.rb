bb22bc7d3cffb31f0bdc20b29eb7902ae7469593
Bryan Alexander <bryan.alexander@adhocteam.us>
2025-06-09 16:18:08 -0500

    103352: Fix migration

--- db/migrate/20250605205239_migrate_saved_claim_pension_to_pensions_saved_claim.rb
+++ db/migrate/20250605205239_migrate_saved_claim_pension_to_pensions_saved_claim.rb
@@ -3,8 +3,8 @@ class MigrateSavedClaimPensionToPensionsSavedClaim < ActiveRecord::Migration[7.2
 
   disable_ddl_transaction!
 
-  def up
-    say_with_time "Migrating SavedClaim::Pension to Pensions::SavedClaim" do
+  def change
+    safety_assured do
       loop do
         ids = SavedClaim.where(type: 'SavedClaim::Pension').limit(BATCH_SIZE).pluck(:id)
         break if ids.empty?
@@ -13,21 +13,6 @@ class MigrateSavedClaimPensionToPensionsSavedClaim < ActiveRecord::Migration[7.2
           SavedClaim.where(id: ids).update_all(type: 'Pensions::SavedClaim')
         rescue => e
           Rails.logger.error("MigrateSavedClaimPensionToPensionsSavedClaim batch failed for IDs: #{ids.inspect} - #{e.class}: #{e.message}")
-        end
-      end
-    end
-  end
-
-  def down
-    say_with_time "Reverting Pensions::SavedClaim to SavedClaim::Pension" do
-      loop do
-        ids = SavedClaim.where(type: 'Pensions::SavedClaim').limit(BATCH_SIZE).pluck(:id)
-        break if ids.empty?
-
-        begin
-          SavedClaim.where(id: ids).update_all(type: 'SavedClaim::Pension')
-        rescue => e
-          Rails.logger.error("MigrateSavedClaimPensionToPensionsSavedClaim rollback batch failed for IDs: #{ids.inspect} - #{e.class}: #{e.message}")
         end
       end
     end
