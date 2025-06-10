29bb5b2af30e21020187f255589ab3458802fc6c
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-06-10 11:10:11 -0400

    Add test

--- modules/dependents_verification/spec/lib/dependents_verification/pdf_fill/va210538_spec.rb
+++ modules/dependents_verification/spec/lib/dependents_verification/pdf_fill/va210538_spec.rb
@@ -15,6 +15,21 @@ describe DependentsVerification::PdfFill::Va210538 do
     described_class.new(form_data)
   end
 
+  let(:path) { 'modules/dependents_verification/lib/dependents_verification/pdf_fill/pdfs/21-0538.pdf' }
+  let(:fields) do
+    %w[
+      SectionI1.VeteransName.First SectionI1.VeteransName.Last SectionI1.VeteransName.MI
+      SectionI2.VeteranSSN.First SectionI2.VeteranSSN.Middle SectionI2.VeteranSSN.Last
+      SectionI3.VeteranFileNumber SectionI4.VeteranDOB.Month SectionI4.VeteranDOB.Day SectionI4.VeteranDOB.Year
+      SectionI5.VeteranAddress.Street SectionI5.VeteranAddress.City SectionI5.VeteranAddress.UnitNumber
+      SectionI5.VeteranAddress.Country SectionI5.VeteranAddress.State SectionI5.VeteranAddress.PostalCode.First
+      SectionI5.VeteranAddress.PostalCode.Second SectionI6.VeteranPhone.First SectionI6.VeteranPhone.Second
+      SectionI6.VeteranPhone.Third SectionI7.VeteranEmail.Second SectionI7.VeteranEmail.First
+      SectionI7.VeteranEmail.Agree SectionII8.StatusChange V14.SignatureField V14.SignatureDate.Month
+      V14.SignatureDate.Day V14.SignatureDate.Year VaDateStamp
+    ]
+  end
+
   def class_form_data
     new_form_class.instance_variable_get(:@form_data)
   end
@@ -25,5 +40,17 @@ describe DependentsVerification::PdfFill::Va210538 do
       expected = File.read("#{DependentsVerification::MODULE_PATH}/spec/fixtures/pdf_fill/21-0538/merge_fields.json")
       expect(described_class.new(JSON.parse(actual)).merge_fields.to_json).to eq(JSON.parse(expected).to_json)
     end
+  end
+
+  it 'includes all expected fields in the PDF, even if there are extras' do
+    pdf = PdfForms.new(Settings.binaries.pdftk)
+    fields_in_pdf = pdf.get_fields(path).map(&:name)
+    expect(fields_in_pdf).to include(*fields)
+  end
+
+  it 'does not have duplicate field names in the PDF' do
+    pdf = PdfForms.new(Settings.binaries.pdftk)
+    fields_in_pdf = pdf.get_fields(path).map(&:name)
+    expect(fields_in_pdf.uniq.size).to eq(fields_in_pdf.size)
   end
 end
