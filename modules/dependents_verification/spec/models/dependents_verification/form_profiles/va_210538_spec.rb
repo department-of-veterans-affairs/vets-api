2fb32a8445c349731d98ce342e30e0716be9b56a
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-06-11 12:12:36 -0400

    Lint

--- modules/dependents_verification/spec/models/dependents_verification/form_profiles/va_210538_spec.rb
+++ modules/dependents_verification/spec/models/dependents_verification/form_profiles/va_210538_spec.rb
@@ -7,6 +7,28 @@ RSpec.describe DependentsVerification::FormProfiles::VA210538, type: :model do
 
   let(:user) { build(:user, :loa3) }
   let(:form_id) { '21-0538' }
+  let(:prefilled_data) do
+    {
+      form_data: {
+        'veteranInformation' => { 'fullName' => { 'first' => 'Abraham', 'last' => 'Lincoln', 'suffix' => 'Jr.' },
+                                  'ssn' => '796111863', 'birthDate' => '1809-02-12' },
+        'veteranContactInformation' => {
+          'veteranAddress' => {
+            'street' => '140 Rock Creek Rd',
+            'city' => 'Washington',
+            'state' => 'DC',
+            'country' => 'USA',
+            'postalCode' => '20011'
+          },
+          'mobilePhone' => '3035551234',
+          'homePhone' => '3035551234',
+          'usPhone' => '3035551234',
+          'emailAddress' => be_a(String)
+        }
+      },
+      metadata: { version: 0, prefill: true, returnUrl: '/veteran-information' }
+    }
+  end
 
   before do
     allow(Flipper).to receive(:enabled?).with(:remove_pciu, instance_of(User)).and_return(true)
@@ -27,25 +49,7 @@ RSpec.describe DependentsVerification::FormProfiles::VA210538, type: :model do
 
   describe '#prefill' do
     it 'initializes identity and contact information' do
-      expect(subject.prefill).to match({
-        form_data: {
-          "veteranInformation" => { "fullName" => { "first" => "Abraham", "last" => "Lincoln", "suffix" => "Jr." },
-          "ssn" => "796111863", "birthDate" => "1809-02-12" },
-          "veteranContactInformation" => {
-            "veteranAddress" => {
-              "street" => "140 Rock Creek Rd",
-              "city" => "Washington",
-              "state" => "DC",
-              "country" => "USA",
-              "postalCode" => "20011"
-            },
-            "mobilePhone" => "3035551234",
-            "homePhone" => "3035551234",
-            "usPhone" => "3035551234",
-            "emailAddress" => be_a(String)
-          }
-        },
-        metadata: { version: 0, prefill: true, returnUrl: "/veteran-information" } })
+      expect(subject.prefill).to match(prefilled_data)
     end
   end
 end
