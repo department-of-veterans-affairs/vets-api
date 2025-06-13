c3a0f2766c64f48de426da4dccaa4f07ec9b7e45
Brandon Cooper <brandon.cooper@adhocteam.us>
2025-06-13 10:38:25 -0400

    print cassette files out

--- bin/record_vcr_cassettes.rb
+++ bin/record_vcr_cassettes.rb
@@ -17,6 +17,34 @@ class VcrRecorder
     @modified = false
   end
 
+  # Find cassette paths and check if they exist before recording
+  def find_cassette_paths
+    cassette_paths = @cassette_names.map do |name|
+      # VCR typically stores cassettes in spec/support/vcr_cassettes/
+      path = File.join('spec', 'support', 'vcr_cassettes', "#{name}.yml")
+      
+      # Also check for module-specific paths
+      module_path = nil
+      if spec_path.include?('modules/')
+        module_name = spec_path.match(/modules\/([^\/]+)/).to_a[1]
+        if module_name
+          mod_path = File.join('modules', module_name, 'spec', 'support', 'vcr_cassettes', "#{name}.yml")
+          module_path = mod_path if File.exist?(mod_path)
+        end
+      end
+      
+      [name, { 
+        path: path, 
+        exists: File.exist?(path), 
+        module_path: module_path,
+        module_exists: module_path && File.exist?(module_path)
+      }]
+    end.to_h
+    
+    # Return a hash of cassette name => info
+    cassette_paths
+  end
+
   def run # rubocop:disable Metrics/MethodLength
     unless File.exist?(spec_path)
       puts "Error: Spec file not found: #{spec_path}"
@@ -27,8 +55,35 @@ class VcrRecorder
       create_backup
       modify_spec_file
       if @modified
+        # Find cassettes and check if they exist before running the spec
+        cassette_info = find_cassette_paths
+        
+        # Run the spec to record VCR cassettes
         run_spec
+        
+        # Check which cassettes were updated
+        updated_cassettes = []
+        cassette_info.each do |name, info|
+          path = info[:path]
+          module_path = info[:module_path]
+          
+          if File.exist?(path) && (!info[:exists] || File.mtime(path) > File.mtime(backup_path))
+            updated_cassettes << path
+          end
+          
+          if module_path && File.exist?(module_path) && 
+             (!info[:module_exists] || File.mtime(module_path) > File.mtime(backup_path))
+            updated_cassettes << module_path
+          end
+        end
+        
         puts "\nVCR cassettes have been recorded. Check the changes in the cassette files."
+        if updated_cassettes.any?
+          puts "\nUpdated cassettes:"
+          updated_cassettes.each { |path| puts "  - #{path}" }
+        else
+          puts "\nNo cassette files were updated. Check your VCR configuration."
+        end
         puts 'Original spec file has been restored from backup.'
       else
         puts 'No VCR cassettes found in the spec file.'
