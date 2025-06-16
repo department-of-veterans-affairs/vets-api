5a4bda82cc91a544387e2fce182951cfc0a464ab
agravell047 <anthony.gravell@bylight.com>
2025-06-16 18:08:40 -0400

    Add feature toggle for updating MHV prescriptions policy

--- app/policies/mhv_prescriptions_policy.rb
+++ app/policies/mhv_prescriptions_policy.rb
@@ -1,22 +1,54 @@
 # frozen_string_literal: true
 
+require 'rx/client'
+
 MHVPrescriptionsPolicy = Struct.new(:user, :mhv_prescriptions) do
   RX_ACCOUNT_TYPES = %w[Premium Advanced].freeze
 
-  # NOTE: This check for va_patient, might break functionality for mhv-sign-in users,
-  # since we only query MVI for "Premium", and Rx is technically available to non-premium.
   def access?
+    if Flipper.enabled?(:update_mhv_prescriptions_policy)
+      feature_flag_access_check
+    else
+      default_access_check
+    end
+  end
+
+  private
+
+  def feature_flag_access_check
+    return false unless user.mhv_correlation_id && user.va_patient?
+
+    begin
+      client = Rx::Client.new(session: { user_id: user.mhv_correlation_id, user_uuid: user.uuid })
+      handle_client_session(client)
+    rescue
+      log_access_denied('RX ACCESS DENIED (feature flag)')
+      false
+    end
+  end
+
+  def handle_client_session(client)
+    if client.session.expired?
+      client.authenticate
+      !client.session.expired?
+    else
+      true
+    end
+  end
+
+  def default_access_check
     service_name = user.identity.sign_in[:service_name]
     access = RX_ACCOUNT_TYPES.include?(user.mhv_account_type) &&
              (user.va_patient? || service_name == SignIn::Constants::Auth::MHV)
-    unless access
-      Rails.logger.info('RX ACCESS DENIED',
-                        account_type: user.mhv_account_type.presence || 'false',
-                        mhv_id: user.mhv_correlation_id.presence || 'false',
-                        sign_in_service: service_name,
-                        va_facilities: user.va_treatment_facility_ids.length,
-                        va_patient: user.va_patient?)
-    end
+    log_access_denied('RX ACCESS DENIED') unless access
     access
+  end
+
+  def log_access_denied(message)
+    Rails.logger.info(message,
+                      mhv_id: user.mhv_correlation_id.presence || 'false',
+                      sign_in_service: user.identity.sign_in[:service_name],
+                      va_facilities: user.va_treatment_facility_ids.length,
+                      va_patient: user.va_patient?)
   end
 end
