58bdd636d0db6f19ee582bbc289ce69a8884b05a
Oren Mittman <nihil2501@gmail.com>
2025-06-16 16:22:30 -0400

    [ARP] Spike `stamp_pdf` override

--- modules/accredited_representative_portal/app/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job.rb
+++ modules/accredited_representative_portal/app/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job.rb
@@ -15,5 +15,32 @@ module AccreditedRepresentativePortal
         @claim.business_line
       )
     end
+
+    def stamp_pdf(record)
+      case record
+      when PersistentAttachments::VAFormDocumentation
+        ##
+        # TODO: Our documentation attachments probably have some other stamping
+        # requirements than what the parent class does.
+        #
+        super
+      when SavedClaim::BenefitsIntake
+        record.to_pdf.tap do |stamped_template_path|
+          ##
+          # TODO: Reimplement PDF stamping with our own code. `SimpleFormsApi`'s
+          # abstraction stamps the PDF, but it also fills out forms, which we may
+          # not need.
+          #
+          SimpleFormsApi::PdfStamper.new(
+            form: SimpleFormsApi::VBA21686C.new({}),
+            stamped_template_path:,
+            current_loa: SignIn::Constants::Auth::LOA3,
+            timestamp: @claim.created_at
+          ).stamp_pdf
+        end
+      else
+        raise ArgumentError
+      end
+    end
   end
 end
