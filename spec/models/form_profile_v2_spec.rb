7cecfb4c02f4cb5bc1615d0210637d3766f88c03
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-06-16 10:20:59 -0700

    spec fixes for 10297 models

--- spec/models/form_profile_v2_spec.rb
+++ spec/models/form_profile_v2_spec.rb
@@ -1402,8 +1402,9 @@ RSpec.describe FormProfile, type: :model do
       context 'with VA Profile prefill for 10297' do
         before do
           expect(user).to receive(:authorize).with(:lighthouse, :direct_deposit_access?)
-                                              .and_return(true).at_least(:once)
+                                             .and_return(true).at_least(:once)
           expect(user).to receive(:authorize).with(:evss, :access?).and_return(true).at_least(:once)
+          expect(user).to receive(:authorize).with(:va_profile, :access?).and_return(true).at_least(:once)
         end
 
         it 'prefills 10297' do
@@ -1416,21 +1417,23 @@ RSpec.describe FormProfile, type: :model do
 
       context 'with VA Profile and ppiu prefill for 10297' do
         before do
+          allow_any_instance_of(Auth::ClientCredentials::Service).to receive(:get_token).and_return('usyergd')
           can_prefill_vaprofile(true)
-          expect(user).to receive(:authorize).with(:ppiu, :access?).and_return(true).at_least(:once)
+          expect(user).to receive(:authorize).with(:lighthouse, :direct_deposit_access?)
+                                             .and_return(true).at_least(:once)
           expect(user).to receive(:authorize).with(:evss, :access?).and_return(true).at_least(:once)
           v22_10297_expected['bankAccount'] = {
-            'bankAccountNumber' => '*********1234',
+            'bankAccountNumber' => '******7890',
             'bankAccountType' => 'Checking',
-            'bankName' => 'Comerica',
-            'bankRoutingNumber' => '*****2115'
+            'bankName' => 'WELLS FARGO BANK',
+            'bankRoutingNumber' => '*****0503'
           }
         end
 
         it 'prefills 10297 with VA Profile and payment information' do
           VCR.use_cassette('va_profile/v2/contact_information/get_address') do
             VCR.use_cassette('evss/disability_compensation_form/rated_disabilities') do
-              VCR.use_cassette('evss/ppiu/payment_information') do
+              VCR.use_cassette('lighthouse/direct_deposit/show/200_valid_new_icn') do
                 VCR.use_cassette('va_profile/military_personnel/post_read_service_histories_200',
                                  allow_playback_repeats: true) do
                   expect_prefilled('22-10297')
