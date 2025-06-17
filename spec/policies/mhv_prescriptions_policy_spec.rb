50e6a5d6a76ee95e31fb45f8bf1c30c1d3fc9895
radavis <1299034+radavis@users.noreply.github.com>
2025-06-17 08:42:19 -0400

    Adds MHVPrescriptionsPolicy spec

---
+++ spec/policies/mhv_prescriptions_policy_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe MHVPrescriptionsPolicy do
+  subject { described_class }
+
+  context "post-MHV-Account-Creation-release policy" do
+    before { Flipper.enable(:mhv_medications_new_policy) }
+
+    permissions :access? do
+      context 'with a valid user account' do
+        let(:user) { build(:user, :loa3) }
+
+        it 'grants access' do
+          expect(subject).to permit(user, :rx)
+        end
+      end
+
+      context 'with an invalid user account' do
+        let(:user) { build(:user, :loa1) }
+
+        it 'denies access' do
+          expect(subject).not_to permit(user, :rx)
+        end
+      end
+    end
+  end
+
+  context "pre-MHV-Account-Creation-release policy" do
+    before { Flipper.disable(:mhv_medications_new_policy) }
+
+    permissions :access? do
+      context 'with a valid user account' do
+        let(:user) { build(:user, :loa3) }
+
+        it 'grants access' do
+          expect(subject).to permit(user, :rx)
+        end
+      end
+
+      context 'wiht an invalid account' do
+        let(:user) { build(:user, :loa1) }
+
+        it 'denies access' do
+          expect(subject).not_to permit(user, :rx)
+        end
+      end
+    end
+  end
+end
