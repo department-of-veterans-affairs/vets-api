f7e2b8ce7c163b9ffa9fd9216bcec6a1346329f3
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-06-17 09:04:11 -0600

    Remove service tag from FeatureTogglesController and specify StandardError in rescue block

--- modules/mobile/app/controllers/mobile/v0/feature_toggles_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/feature_toggles_controller.rb
@@ -3,7 +3,6 @@
 module Mobile
   module V0
     class FeatureTogglesController < ApplicationController
-      service_tag 'feature-flag'
       # the feature toggle does not require authentication, but if a user is logged we might use @current_user
       skip_before_action :authenticate
       before_action :set_current_user
@@ -31,7 +30,7 @@ module Mobile
       def set_current_user
         # Try to load user if possible, but don't throw errors if not authenticated
         load_user(skip_expiration_check: true)
-      rescue => e
+      rescue StandardError => e
         Rails.logger.info("Error loading user in feature toggles: #{e.message}")
         @current_user = nil
       end
