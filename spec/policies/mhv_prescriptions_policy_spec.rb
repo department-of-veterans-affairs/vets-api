10a5420f39fbefa05b0188176cee31bfc9b01756
agravell047 <anthony.gravell@bylight.com>
2025-06-17 10:17:44 -0400

    Add CODEOWNERS entry for MHVPrescriptionsPolicy spec

--- spec/policies/mhv_prescriptions_policy_spec.rb
+++ spec/policies/mhv_prescriptions_policy_spec.rb
@@ -8,19 +8,22 @@ require 'mhv_prescriptions_policy'
 describe MHVPrescriptionsPolicy do
   let(:mhv_prescriptions) { double('mhv_prescriptions') }
   let(:user) do
-    OpenStruct.new(
+    u = OpenStruct.new(
       mhv_user_account: OpenStruct.new(patient: true),
       mhv_account_type: 'Premium',
       va_patient: true,
-      va_treatment_facility_ids: ["123"],
+      va_treatment_facility_ids: ['123'],
       mhv_correlation_id: '456',
       identity: OpenStruct.new(sign_in: { service_name: 'idme' })
     )
+    def u.va_patient?
+      va_patient
+    end
+    u
   end
 
   def set_va_patient(user, value)
     user.va_patient = value
-    def user.va_patient?; va_patient; end
   end
 
   context 'when Flipper flag is enabled' do
@@ -29,12 +32,12 @@ describe MHVPrescriptionsPolicy do
     end
 
     it 'returns true if user is a patient' do
-      expect(described_class.new(user, mhv_prescriptions).access?).to eq(true)
+      expect(described_class.new(user, mhv_prescriptions).access?).to be(true)
     end
 
     it 'returns false if user is not a patient' do
       user.mhv_user_account.patient = false
-      expect(described_class.new(user, mhv_prescriptions).access?).to eq(false)
+      expect(described_class.new(user, mhv_prescriptions).access?).to be(false)
     end
   end
 
@@ -46,7 +49,7 @@ describe MHVPrescriptionsPolicy do
     it 'returns true for Premium/Advanced account and va_patient' do
       user.mhv_account_type = 'Premium'
       set_va_patient(user, true)
-      expect(described_class.new(user, mhv_prescriptions).access?).to eq(true)
+      expect(described_class.new(user, mhv_prescriptions).access?).to be(true)
     end
 
     it 'returns true for Premium/Advanced account and MHV sign in' do
@@ -54,13 +57,13 @@ describe MHVPrescriptionsPolicy do
       set_va_patient(user, false)
       user.identity.sign_in[:service_name] = 'myhealthevet'
       stub_const('SignIn::Constants::Auth::MHV', 'myhealthevet')
-      expect(described_class.new(user, mhv_prescriptions).access?).to eq(true)
+      expect(described_class.new(user, mhv_prescriptions).access?).to be(true)
     end
 
     it 'returns false for Basic account' do
       user.mhv_account_type = 'Basic'
       set_va_patient(user, true)
-      expect(described_class.new(user, mhv_prescriptions).access?).to eq(false)
+      expect(described_class.new(user, mhv_prescriptions).access?).to be(false)
     end
 
     it 'returns false for Premium/Advanced account but not va_patient and not MHV sign in' do
@@ -68,7 +71,7 @@ describe MHVPrescriptionsPolicy do
       set_va_patient(user, false)
       user.identity.sign_in[:service_name] = 'idme'
       stub_const('SignIn::Constants::Auth::MHV', 'myhealthevet')
-      expect(described_class.new(user, mhv_prescriptions).access?).to eq(false)
+      expect(described_class.new(user, mhv_prescriptions).access?).to be(false)
     end
   end
 end
