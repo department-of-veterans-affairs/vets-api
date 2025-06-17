4d1c100f647462844612bfe4684209f40c2fdf22
Oren Mittman <nihil2501@gmail.com>
2025-06-17 02:40:47 -0400

    [ARP] Clarify distinct purposes for form ID values

--- modules/accredited_representative_portal/app/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job.rb
+++ modules/accredited_representative_portal/app/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job.rb
@@ -4,15 +4,16 @@ module AccreditedRepresentativePortal
   class SubmitBenefitsIntakeClaimJob < Lighthouse::SubmitBenefitsIntakeClaim
     def generate_metadata
       veteran = @claim.parsed_form['veteran']
+      veteran_name = veteran['name']
 
       ::BenefitsIntake::Metadata.generate(
-        veteran.dig('name', 'first'),
-        veteran.dig('name', 'last'),
+        veteran_name['first'],
+        veteran_name['last'],
         veteran['ssn'],
         veteran['postalCode'],
         "#{@claim.class} va.gov",
-        @claim.form_id,
-        @claim.business_line
+        @claim.class::PROPER_FORM_ID,
+        @claim.class::BUSINESS_LINE
       )
     end
 
