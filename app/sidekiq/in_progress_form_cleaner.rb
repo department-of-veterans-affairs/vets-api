2bf1c655c89e5c03ab0a90df92023fb113ac8361
Adam King <adam.king0126@gmail.com>
2025-06-18 08:58:48 -0500

    lint

--- app/sidekiq/in_progress_form_cleaner.rb
+++ app/sidekiq/in_progress_form_cleaner.rb
@@ -6,9 +6,11 @@ class InProgressFormCleaner
   def perform
     forms = InProgressForm.where('expires_at < ?', Time.now.utc)
 
-    ['28-1900', '28-1900_V2'].each do |form_id|
-      count = forms.where(form_id: form_id).count
-      StatsD.increment("worker.in_progress_form_cleaner.#{form_id.downcase.tr('-', '_')}_deleted", count) if count > 0
+    %w[28-1900 28-1900_V2].each do |form_id|
+      count = forms.where(form_id:).count
+      if count.positive?
+        StatsD.increment("worker.in_progress_form_cleaner.#{form_id.downcase.tr('-', '_')}_deleted", count)
+      end
     end
 
     logger.info("Deleting #{forms.count} old saved forms")
