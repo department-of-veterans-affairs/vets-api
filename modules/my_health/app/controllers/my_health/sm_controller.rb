da28fa93dcf3b635e22728f7314b4660ef96d1d4
Alex Morgun <87077843+alex-morgun-1@users.noreply.github.com>
2025-06-18 11:25:29 -0400

    Update sm_controller.rb

--- modules/my_health/app/controllers/my_health/sm_controller.rb
+++ modules/my_health/app/controllers/my_health/sm_controller.rb
@@ -15,11 +15,15 @@ module MyHealth
     def client
       #  @client ||= SM::Client.new(session: { user_id: current_user.mhv_correlation_id, user_uuid: current_user.uuid })
       # @client ||= SM::Client.new(session: { user_id: 9792157 }) # STAGING USER
-      # @client ||= SM::Client.new(session: { user_id: 1571704 }) # DEV USER
+      # @client ||= SM::Client.new(session: { user_id: 1_571_704 }) # DEV USER
       # @client ||= SM::Client.new(session: { user_id: 7366505 }) # SYST MHV smautotest4
       # @client ||= SM::Client.new(session: { user_id: 10055239 }) # SYST vets.gov.user+41@gmail.co
       # @client ||= SM::Client.new(session: { user_id: 21579884 }) # OH messages user
-      @client ||= SM::Client.new(session: { user_id: 9_712_240 }) # MHVMARK mhvmark.test@id.me
+      # @client ||= SM::Client.new(session: { user_id: 10202439 }) # SYSB MHVTP, ROBERT FORD
+      # @client ||= SM::Client.new(session: { user_id: 9_712_240 }) # MHVMARK mhvmark.test@id.me
+      # @client ||= SM::Client.new(session: { user_id: 22336066 }) # Theo
+      # @client ||= SM::Client.new(session: { user_id: 24_488_346 }) # Gustav OH vagovohtestpatient+gustav@gmail.com
+      @client ||= SM::Client.new(session: { user_id: 9_950_125 }) # INTB OH user
     end
 
     def authorize
