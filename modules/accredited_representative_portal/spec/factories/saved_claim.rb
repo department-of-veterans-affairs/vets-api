dce89639ac17def6047f63e28786b7dac1e09692
Gregory Hedrick <gregory@pixiitech.net>
2025-06-20 15:22:50 -0400

    Fix saved claim factory to match schema, expose representatives

--- modules/accredited_representative_portal/spec/factories/saved_claim.rb
+++ modules/accredited_representative_portal/spec/factories/saved_claim.rb
@@ -1,39 +1,17 @@
 # frozen_string_literal: true
 
 form_data =
-  { 'veteran' =>
-    { 'name' => { 'first' => 'John', 'middle' => 'Middle', 'last' => 'Doe' },
-      'address' => {
-        'addressLine1' => '123 Main St',
-        'addressLine2' => 'Apt 1',
-        'city' => 'Springfield',
-        'stateCode' => 'IL',
-        'country' => 'US',
-        'zipCode' => '62704',
-        'zipCodeSuffix' => '6789'
-      },
-      'ssn' => '123456789',
-      'vaFileNumber' => '123456789',
-      'dateOfBirth' => '1980-12-31',
-      'serviceNumber' => '123456789',
-      'serviceBranch' => 'ARMY',
-      'phone' => '1234567890',
-      'email' => 'veteran@example.com' },
-    'dependent' =>
-      { 'name' => { 'first' => 'John', 'middle' => 'Middle', 'last' => 'Doe' },
-        'address' => {
-          'addressLine1' => '123 Main St',
-          'addressLine2' => 'Apt 1',
-          'city' => 'Springfield',
-          'stateCode' => 'IL',
-          'country' => 'US',
-          'zipCode' => '62704',
-          'zipCodeSuffix' => '6789'
-        },
+  {
+    'veteran' =>
+      { 'name' => { 'first' => 'John', 'last' => 'Doe' },
+        'ssn' => '123456789',
         'dateOfBirth' => '1980-12-31',
-        'relationship' => 'Spouse',
-        'phone' => '1234567890',
-        'email' => 'veteran@example.com' } }
+        'postalCode' => '12345' },
+    'dependent' =>
+      { 'name' => { 'first' => 'John', 'last' => 'Doe' },
+        'dateOfBirth' => '1980-12-31',
+        'ssn' => '123456789' }
+  }
 
 FactoryBot.define do
   factory :saved_claim_benefits_intake,
