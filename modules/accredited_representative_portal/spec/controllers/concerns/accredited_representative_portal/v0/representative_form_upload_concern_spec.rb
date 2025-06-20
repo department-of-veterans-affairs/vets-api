ccbfa2bebf1ac3c7e7184228b12c8332f16fd97f
Oren Mittman <nihil2501@gmail.com>
2025-06-20 11:48:03 -0400

    Arp/form upload create action 2.refactor (#22700)
    * [ARP] Swap claim submission backend in controller
    
    * [ARP] `bundle update vets_json_schema`
    
    * [ARP] Fix missing claimant VSO POA case
    
    * [ARP] Fix claim submission specs
    
    * [ARP] Tiny code tweak
    
    * [ARP] Fix Benefits Intake claim and submission re: new schema
    
    * [ARP] Small submit `before_action` refactor
    
    * [ARP] Fix upload policy spec based on `ClaimantRepresentative`
    
    * [ARP] Remove excessive unit testing for upload controller mixin
    
    * [ARP] Tiny refactor of nilable claimant POA holder
    
    * Revert "[ARP] Remove excessive unit testing for upload controller mixin"
    
    This reverts commit 8409a150e3559ca17b36a05939b2b34d2b49b429.
    
    * [ARP] Skip excessive unit testing for upload controller mixin
    Removed in forthcoming PR
    
    * lint fix
    
    * [ARP] Small refactor of claimant representative spec
    
    * lint fix

--- modules/accredited_representative_portal/spec/controllers/concerns/accredited_representative_portal/v0/representative_form_upload_concern_spec.rb
+++ modules/accredited_representative_portal/spec/controllers/concerns/accredited_representative_portal/v0/representative_form_upload_concern_spec.rb
@@ -6,7 +6,8 @@ class DummyController
   include AccreditedRepresentativePortal::V0::RepresentativeFormUploadConcern
 end
 
-RSpec.describe AccreditedRepresentativePortal::V0::RepresentativeFormUploadConcern, type: :controller do
+RSpec.describe AccreditedRepresentativePortal::V0::RepresentativeFormUploadConcern,
+               skip: 'excessive unit testing of controller concern methods, removed in next PR', type: :controller do
   let(:controller) { DummyController.new }
 
   describe '#validated_metadata' do
