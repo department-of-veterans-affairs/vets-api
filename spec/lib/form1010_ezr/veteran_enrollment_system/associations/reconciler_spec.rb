82399aa4b90adb1fa9963a9562b225d71f818640
Joshua <joshing_you1@yahoo.com>
2025-06-23 12:51:05 -0400

    Fixed linting issue

--- spec/lib/form1010_ezr/veteran_enrollment_system/associations/reconciler_spec.rb
+++ spec/lib/form1010_ezr/veteran_enrollment_system/associations/reconciler_spec.rb
@@ -70,7 +70,10 @@ RSpec.describe Form1010Ezr::VeteranEnrollmentSystem::Associations::Reconciler do
             associations
           ).reconcile_associations
         }.to raise_error(StandardError, "VES association is missing the following field(s): role, name, relationship")
-        expect(Rails.logger).to have_received(:error).with("Error transforming VES association: VES association is missing the following field(s): role, name, relationship")
+        expect(Rails.logger).to have_received(:error).with(
+          "Error transforming VES association: VES association is missing " \
+          "the following field(s): role, name, relationship"
+        )
       end
     end
   end
