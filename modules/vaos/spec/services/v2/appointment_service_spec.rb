f628376909cc6e0212863de88614499fc218e606
Corey Ferris <cferris320@gmail.com>
2025-06-23 14:58:30 -0400

    updated telehealth modality

--- modules/vaos/spec/services/v2/appointment_service_spec.rb
+++ modules/vaos/spec/services/v2/appointment_service_spec.rb
@@ -2330,15 +2330,22 @@ describe VAOS::V2::AppointmentsService do
       end
     end
 
-    it 'is vaInPerson for nil vvsKind' do
+    it 'is vaInPerson for nil vvsKind and false vvsVistaVideoAppt' do
       appt = build(:appointment_form_v2, :va_proposed_valid_reason_code_text, :telehealth).attributes
       appt[:telehealth][:vvs_kind] = nil
       subject.send(:set_modality, appt)
       expect(appt[:modality]).to eq('vaInPerson')
     end
 
+    it 'is vaVideoCareAtHome for nil vvsKind and true vvsVistaVideoAppt' do
+      appt = build(:appointment_form_v2, :va_proposed_valid_reason_code_text, :telehealth, :vistaVideoTrue).attributes
+      appt[:telehealth][:vvs_kind] = nil
+      subject.send(:set_modality, appt)
+      expect(appt[:modality]).to eq('vaVideoCareAtHome')
+    end
+
     it 'is nil for unrecognized vvsKind' do
-      appt = build(:appointment_form_v2, :va_proposed_valid_reason_code_text, :telehealth).attributes
+      appt = build(:appointment_form_v2, :va_proposed_valid_reason_code_text, :telehealth, :vistaVideoFalse).attributes
       appt[:telehealth][:vvs_kind] = 'MOBILE_GFE'
       subject.send(:set_modality, appt)
       expect(appt[:modality]).to be_nil
