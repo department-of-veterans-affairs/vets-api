6615ca8e03d64fd0c8c6b0f38ca2c46d6088d9ee
Wayne Weibel <wayne.weibel@adhocteam.us>
2025-06-23 13:56:03 -0400

    require path

--- spec/models/lighthouse/submission_attempt_spec.rb
+++ spec/models/lighthouse/submission_attempt_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'models/_shared_examples/submission_attempt'
+require 'support/models/shared_examples/submission_attempt'
 
 RSpec.describe Lighthouse::SubmissionAttempt, type: :model do
   it_behaves_like 'a SubmissionAttempt model'
--- spec/models/lighthouse/submission_spec.rb
+++ spec/models/lighthouse/submission_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'models/_shared_examples/submission'
+require 'support/models/shared_examples/submission'
 
 RSpec.describe Lighthouse::Submission, type: :model do
   it_behaves_like 'a Submission model'
