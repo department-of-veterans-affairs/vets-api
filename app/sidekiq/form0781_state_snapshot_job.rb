98a45d7c2a4cd1a0d8aad6e6ad71fdfc79c689d3
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2025-06-23 07:08:41 -0400

    think my linter removed these for no reason

--- app/sidekiq/form0781_state_snapshot_job.rb
+++ app/sidekiq/form0781_state_snapshot_job.rb
@@ -109,11 +109,13 @@ class Form0781StateSnapshotJob
   def old_0781_submissions
     old_0781_submissions_saved_claims.pluck(:id)
   end
+
   def old_0781_successful_submissions
     Form526Submission.where(
       saved_claim_id: old_0781_submissions_saved_claims.pluck(:id)
     ).where('submitted_claim_id IS NOT NULL OR backup_submitted_claim_id IS NOT NULL').pluck(:id)
   end
+  
   def old_0781_failed_submissions
     Form526Submission.where(
       saved_claim_id: old_0781_submissions_saved_claims.pluck(:id)
