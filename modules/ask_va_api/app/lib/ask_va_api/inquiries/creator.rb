7c7f53f746ce0f04ac7224b6660b2f998a90dad8
Rajesh Jain <73859950+rjain21@users.noreply.github.com>
2025-06-25 10:26:25 -0400

    Revert "build_payload may throw an error"
    This reverts commit 870102ce656b8c1f6f285ff4fa648d7df7bea226.

--- modules/ask_va_api/app/lib/ask_va_api/inquiries/creator.rb
+++ modules/ask_va_api/app/lib/ask_va_api/inquiries/creator.rb
@@ -56,9 +56,8 @@ module AskVAApi
           safe_fields = log_safe_fields_from_inquiry(inquiry_params)
           span.set_tag('user.isAuthenticated', user.present?)
           span.set_tag('user.loa', user&.loa&.fetch(:current, nil))
-          # move before building payload to catch which fields are causing an error
-          span.set_tag('inquiry', safe_fields)
           payload = build_payload(inquiry_params)
+          span.set_tag('inquiry', safe_fields)
           if payload.key?(:LevelOfAuthentication)
             span.set_tag('Crm.LevelOfAuthentication', payload[:LevelOfAuthentication])
           end
@@ -73,8 +72,7 @@ module AskVAApi
       private
 
       def log_safe_fields_from_inquiry(inquiry_params)
-        # Logs suggest there may be an issue with inquiry_params. Be safe
-        (inquiry_params || {}).slice(*SAFE_INQUIRY_FIELDS)
+        inquiry_params.slice(*SAFE_INQUIRY_FIELDS)
       end
 
       def default_service
