7372f9547f9578c5cce4bdfa376ccc7db26f70e9
Alex Chan <172081065+alexchan-va@users.noreply.github.com>
2025-06-26 12:05:06 -0400

    Fix lint

--- spec/models/saved_claim/education_benefits/va1919_spec.rb
+++ spec/models/saved_claim/education_benefits/va1919_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 require 'lib/saved_claims_spec_helper'
 
 RSpec.describe SavedClaim::EducationBenefits::VA1919 do
-  let(:instance) { build(:va_1919) }
+  let(:instance) { build(:va1919) }
 
   it_behaves_like 'saved_claim'
 
   validate_inclusion(:form_id, '22-1919')
-end 
+end
