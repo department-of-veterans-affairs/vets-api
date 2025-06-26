36f16a2feb56d7b8567b9cd9bca446f491530e0a
Alex <172081065+alexchan-va@users.noreply.github.com>
2025-06-26 08:40:35 -0400

    Add backend for va1919

---
+++ spec/models/saved_claim/education_benefits/va1919_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'lib/saved_claims_spec_helper'
+
+RSpec.describe SavedClaim::EducationBenefits::VA1919 do
+  let(:instance) { build(:va1919) }
+
+  it_behaves_like 'saved_claim'
+
+  validate_inclusion(:form_id, '22-1919')
+end 
