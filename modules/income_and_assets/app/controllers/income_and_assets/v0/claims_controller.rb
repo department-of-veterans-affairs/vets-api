a77d896d735ce951869b4221194f7304761704b7
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2025-06-26 11:56:10 -0500

    Updating the feature toggle used in the 0969 controller to be the correct one

--- modules/income_and_assets/app/controllers/income_and_assets/v0/claims_controller.rb
+++ modules/income_and_assets/app/controllers/income_and_assets/v0/claims_controller.rb
@@ -50,12 +50,12 @@ module IncomeAndAssets
 
         process_and_upload_to_lighthouse(in_progress_form, claim)
 
-        monitor.track_create_success(in_progress_form&.id, claim, current_user)
+        monitor.track_create_success(in_progress_form, claim, current_user)
 
         clear_saved_form(claim.form_id)
         render json: SavedClaimSerializer.new(claim)
       rescue => e
-        monitor.track_create_error(in_progress_form&.id, claim, current_user, e)
+        monitor.track_create_error(in_progress_form, claim, current_user, e)
         raise e
       end
 
@@ -63,7 +63,7 @@ module IncomeAndAssets
 
       # Raises an exception if the income and assets flipper flag isn't enabled.
       def check_flipper_flag
-        raise Common::Exceptions::Forbidden unless Flipper.enabled?(:pension_income_and_assets_clarification,
+        raise Common::Exceptions::Forbidden unless Flipper.enabled?(:income_and_assets_form_enabled,
                                                                     current_user)
       end
 
@@ -73,7 +73,7 @@ module IncomeAndAssets
       def process_and_upload_to_lighthouse(in_progress_form, claim)
         claim.process_attachments!
 
-        IncomeAndAssets::BenefitsIntake::SubmitClaimJob.perform_async(claim.id, current_user&.user_account_uuid)
+        IncomeAndAssets::BenefitsIntake::SubmitClaimJob.perform(claim.id, current_user&.user_account_uuid)
       rescue => e
         monitor.track_process_attachment_error(in_progress_form, claim, current_user)
         raise e
