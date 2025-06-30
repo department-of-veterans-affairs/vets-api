b89981dc9f5c3bd1e39ca0e2a41dbf31e3caddb4
Jayson Perkins <jayson.perkins@adhocteam.us>
2025-06-30 12:11:38 -0400

    remove unnecessary tests

--- modules/claims_api/spec/lib/claims_api/bgs/vnp_ptcpnt_phone_service_spec.rb
+++ modules/claims_api/spec/lib/claims_api/bgs/vnp_ptcpnt_phone_service_spec.rb
@@ -37,30 +37,4 @@ describe ClaimsApi::VnpPtcpntPhoneService do
       end
     end
   end
-
-  describe 'vnp_ptcpnt_phone_find_by_primary_key' do
-    let(:options) { {} }
-
-    it 'responds with correct phone' do
-      options[:vnp_ptcpnt_addrs_id] = '44693'
-      VCR.use_cassette('claims_api/bgs/vnp_ptcpnt_phone_service/vnp_ptcpnt_phone_find_by_primary_key') do
-        response = subject.vnp_ptcpnt_phone_find_by_primary_key(options)
-        expect(response[:vnp_proc_id]).to eq '29798'
-        expect(response[:vnp_ptcpnt_id]).to eq '44693'
-        expect(response[:phone_type_nm]).to eq 'Daytime'
-        expect(response[:phone_nbr]).to eq '2225552252'
-        expect(response[:efctv_dt]).to eq '2020-07-16T18:20:17Z'
-        expect(response[:vnp_ptcpnt_phone_id]).to eq '30888'
-      end
-    end
-
-    it 'responds appropriately with invalid options' do
-      options[:vnp_ptcpnt_addrs_id] = 'invalid'
-      VCR.use_cassette('claims_api/bgs/vnp_ptcpnt_phone_service/invalid_vnp_ptcpnt_phone_find_by_primary_key') do
-        expect do
-          subject.vnp_ptcpnt_phone_find_by_primary_key(options)
-        end.to raise_error(Common::Exceptions::UnprocessableEntity)
-      end
-    end
-  end
 end
