feabe5e7281ce674dc01de4698a5ecb2aa6041ef
122034971+ConnorJeff@users.noreply.github.com <122034971+ConnorJeff@users.noreply.github.com>
2025-07-03 12:52:25 -0400

    Mocked mulesoft response

--- app/controllers/v0/pre_need_controller.rb
+++ app/controllers/v0/pre_need_controller.rb
@@ -12,14 +12,44 @@ module V0
 
       # Check if mock is enabled in settings
       if Settings.mulesoft.pre_need.mock
-        # Return a mock response
-        mock_response = {
-          "status" => "success",
-          "message" => "This is a mocked PreNeed response.",
-          "data" => payload
-        }
-        render json: mock_response, status: 200
-        return
+        if payload["cmPacketNum"].blank?
+          # Mock failure response
+          mock_response = {
+            "UUID" => "0cfa0c22-52b2-4113-a1b6-b985b841a4f8",
+            "Sync_To_Salesforce" => {
+              "Success" => false,
+              "CaseNumber" => "",
+              "ID_of_Case" => "",
+              "CaseDetail_Name" => "",
+              "ID_of_CaseDetail" => "",
+              "Errors" => [
+                { "Error1" => {
+                  "parentErrorType" => { "parentErrorType" => nil, "identifier" => "ANY", "namespace" => "MULE" },
+                  "identifier" => "EXPRESSION",
+                  "namespace" => "MULE"
+                } },
+                { "Error2" => nil }
+              ]
+            }
+          }
+          render json: mock_response, status: 400
+          return
+        else
+          # Mock success response
+          mock_response = {
+            "UUID" => "6b58f8a0fcc04ef5bf3f9cd7d78c3d79",
+            "Location" => "https://va.lightning.force.com/500ep000004hbnIAAQ",
+            "Sync_To_Salesforce" => {
+              "Success" => true,
+              "CaseNumber" => "12425379",
+              "ID_of_Case" => "500ep000004hbnIAAQ",
+              "CaseDetail_Name" => "Case Detail Name Example",
+              "ID_of_CaseDetail" => "500ep000004hbnIAAQ"
+            }
+          }
+          render json: mock_response, status: 201
+          return
+        end
       end
 
       response = Mulesoft::PreNeed::Service.new.submit_pre_need(payload)
