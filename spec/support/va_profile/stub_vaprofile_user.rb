127df7a8f9a587409c0cf48ae755a8de7ea8c101
Eric Boehs <eric@boehs.com>
2025-07-03 08:36:31 -0500

    Fix factory_bot 6.5.x compatibility in helper methods
    Helper methods called from factory definitions can no longer use
    shorthand factory_bot methods (build, create, etc.) due to scope
    changes in factory_bot 6.5.x. Updated to use FactoryBot.build()
    instead.
    
    Fixed files:
    - spec/support/mpi/stub_mpi.rb
    - spec/support/va_profile/stub_vet360.rb
    - spec/support/va_profile/stub_vaprofile_user.rb
    - spec/support/authenticated_session_helper.rb

--- spec/support/va_profile/stub_vaprofile_user.rb
+++ spec/support/va_profile/stub_vaprofile_user.rb
@@ -9,21 +9,21 @@ require 'va_profile/models/telephone'
 def stub_vaprofile_user(person = nil)
   service = VAProfile::V2::ContactInformation::Service
   person_response = VAProfile::V2::ContactInformation::PersonResponse
-  person ||= build(
+  person ||= FactoryBot.build(
     :person_v2,
     addresses: [
-      build(:va_profile_v3_address, id: 577_127),
-      build(:va_profile_v3_address, address_pou: VAProfile::Models::V3::Address::CORRESPONDENCE, id: 124)
+      FactoryBot.build(:va_profile_v3_address, id: 577_127),
+      FactoryBot.build(:va_profile_v3_address, address_pou: VAProfile::Models::V3::Address::CORRESPONDENCE, id: 124)
     ],
     emails: [
-      build(:email, :contact_info_v2, id: 318_927)
+      FactoryBot.build(:email, :contact_info_v2, id: 318_927)
     ],
     telephones: [
-      build(:telephone, :contact_info_v2, :home, id: 458_781),
-      build(:telephone, :contact_info_v2_mobile, phone_type: VAProfile::Models::Telephone::MOBILE, id: 790),
-      build(:telephone, :contact_info_v2, :home, phone_type: VAProfile::Models::Telephone::WORK, id: 791),
-      build(:telephone, :contact_info_v2, :home, phone_type: VAProfile::Models::Telephone::FAX, id: 792),
-      build(:telephone, :contact_info_v2, :home, phone_type: VAProfile::Models::Telephone::TEMPORARY, id: 793)
+      FactoryBot.build(:telephone, :contact_info_v2, :home, id: 458_781),
+      FactoryBot.build(:telephone, :contact_info_v2_mobile, phone_type: VAProfile::Models::Telephone::MOBILE, id: 790),
+      FactoryBot.build(:telephone, :contact_info_v2, :home, phone_type: VAProfile::Models::Telephone::WORK, id: 791),
+      FactoryBot.build(:telephone, :contact_info_v2, :home, phone_type: VAProfile::Models::Telephone::FAX, id: 792),
+      FactoryBot.build(:telephone, :contact_info_v2, :home, phone_type: VAProfile::Models::Telephone::TEMPORARY, id: 793)
     ]
   )
 
