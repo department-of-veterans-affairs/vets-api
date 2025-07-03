5094987625cd2ed9c2205e04fbec3991f3a0c135
Kyle Henson <145150351+khenson-oddball@users.noreply.github.com>
2025-07-03 15:44:52 -0600

    Remove setting veteranAddress data if no address submitted

--- spec/lib/res/ch31_form_spec.rb
+++ spec/lib/res/ch31_form_spec.rb
@@ -6,9 +6,11 @@ require 'res/ch31_form'
 RSpec.describe RES::Ch31Form do
   let(:claim) { create(:veteran_readiness_employment_claim) }
   let(:claim_with_new_form) { create(:new_veteran_readiness_employment_claim) }
+  let(:claim_with_new_form_minimal) { create(:new_veteran_readiness_employment_claim_minimal) }
   let(:user) { create(:evss_user, :loa3) }
   let(:service) { RES::Ch31Form.new(user:, claim:) }
   let(:service_with_new_form) { RES::Ch31Form.new(user:, claim: claim_with_new_form) }
+  let(:service_with_new_form_minimal) { RES::Ch31Form.new(user:, claim: claim_with_new_form_minimal) }
   let(:new_address_hash) do
     {
       newAddress: {
@@ -40,20 +42,23 @@ RSpec.describe RES::Ch31Form do
 
       it 'adds a new address if the user is moving within 30 days' do
         expect(service).to receive(:new_address) { new_address_hash }
-
         service.submit
       end
     end
 
     context 'with a successful new form submission' do
-      before do
-        allow(service_with_new_form).to receive(:send_to_res).and_return(success_message)
-      end
-
       it 'adds a new address if the user is moving within 30 days' do
+        allow(service_with_new_form).to receive(:send_to_res).and_return(success_message)
         expect(service_with_new_form).to receive(:new_address) { new_address_hash }
 
         service_with_new_form.submit
+      end
+
+      it 'accepts only required fields' do
+        allow(service_with_new_form_minimal).to receive(:send_to_res).and_return(success_message)
+        expect(service_with_new_form_minimal).to receive(:mapped_address_hash).and_return(nil)
+
+        service_with_new_form_minimal.submit
       end
     end
 
@@ -61,7 +66,6 @@ RSpec.describe RES::Ch31Form do
       context 'with old form' do
         it 'does not successfully send to RES' do
           allow(service).to receive(:send_to_res).and_return(OpenStruct.new(body: { 'error' => 'Error' }))
-
           expect { service.submit }.to raise_error(Ch31Error)
         end
       end
@@ -69,14 +73,12 @@ RSpec.describe RES::Ch31Form do
       context 'with new form' do
         it 'does not successfully send to RES' do
           allow(service_with_new_form).to receive(:send_to_res).and_return(OpenStruct.new(body: { 'error' => 'Error' }))
-
           expect { service_with_new_form.submit }.to raise_error(Ch31Error)
         end
       end
 
       it 'handles nil claim' do
         nil_claim_service = RES::Ch31Form.new(user:, claim: nil)
-
         expect { nil_claim_service.submit }.to raise_error(Ch31NilClaimError)
       end
     end
