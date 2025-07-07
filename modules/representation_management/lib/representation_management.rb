dbdf982e3118af0b4c9b6175e7a95bcb1c33bc66
Josh Fike <josh.fike@oddball.io>
2025-07-07 14:05:57 -0500

    New shared constants

--- modules/representation_management/lib/representation_management.rb
+++ modules/representation_management/lib/representation_management.rb
@@ -4,4 +4,34 @@ require 'representation_management/engine'
 
 module RepresentationManagement
   # Your code goes here...
+  AGENTS = 'agents'
+  ATTORNEYS = 'attorneys'
+  REPRESENTATIVES = 'representatives'
+
+  ENTITY_CONFIG = OpenStruct.new({
+                                   AGENTS => OpenStruct.new({
+                                                              api_type: 'agent',
+                                                              individual_type: 'claims_agent',
+                                                              responses_var: :@agent_responses,
+                                                              ids_var: :@agent_ids,
+                                                              json_var: :@agent_json_for_address_validation,
+                                                              validation_description: 'Batching agent address updates from GCLAWS Accreditation API'
+                                                            }),
+                                   ATTORNEYS => OpenStruct.new({
+                                                                 api_type: 'attorney',
+                                                                 individual_type: 'attorney',
+                                                                 responses_var: :@attorney_responses,
+                                                                 ids_var: :@attorney_ids,
+                                                                 json_var: :@attorney_json_for_address_validation,
+                                                                 validation_description: 'Batching attorney address updates from GCLAWS Accreditation API'
+                                                               }),
+                                   REPRESENTATIVES => OpenStruct.new({
+                                                                       api_type: 'representative',
+                                                                       individual_type: 'representative',
+                                                                       responses_var: :@representative_responses,
+                                                                       ids_var: :@representative_ids,
+                                                                       json_var: :@representative_json_for_address_validation,
+                                                                       validation_description: 'Batching representative address updates from GCLAWS Accreditation API'
+                                                                     })
+                                 }).freeze
 end
