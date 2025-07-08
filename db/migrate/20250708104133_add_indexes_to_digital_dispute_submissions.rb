c6659697ecfa98359639127d0eed6890841dfbfd
Josh Mills <josh.troy.mills@gmail.com>
2025-07-08 11:26:43 -0400

    Add table and indexes for digital dispute

---
+++ db/migrate/20250708104133_add_indexes_to_digital_dispute_submissions.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddIndexesToDigitalDisputeSubmissions < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+
+  def change
+    add_index :digital_dispute_submissions, :user_uuid, algorithm: :concurrently
+    add_index :digital_dispute_submissions, :user_account_id, algorithm: :concurrently
+    add_index :digital_dispute_submissions, :needs_kms_rotation, algorithm: :concurrently
+    add_index :digital_dispute_submissions, :debt_identifiers, using: :gin, algorithm: :concurrently
+  end
+end
---
+++ db/migrate/20250708140820_create_digital_dispute_submissions.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class CreateDigitalDisputeSubmissions < ActiveRecord::Migration[7.2]
+  def change
+    create_table :digital_dispute_submissions, id: :uuid do |t|
+      t.uuid :user_uuid, null: false
+      t.references :user_account, foreign_key: true, type: :uuid
+      t.jsonb :debt_identifiers, null: false, default: []
+      t.jsonb :public_metadata, default: {}
+      t.text :form_data_ciphertext
+      t.text :metadata_ciphertext
+      t.text :encrypted_kms_key
+      t.integer :state, default: 0, null: false
+      t.string :error_message
+      t.string :dmc_reference_id
+      t.boolean :needs_kms_rotation, default: false, null: false
+      t.timestamps
+    end
+  end
+end
