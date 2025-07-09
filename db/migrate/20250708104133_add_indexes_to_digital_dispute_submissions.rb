2d5cde29e473ef917266e2c8d50da0a2efdb04bf
Josh Mills <josh.troy.mills@gmail.com>
2025-07-09 09:46:02 -0400

    remove extra index, include schema

--- db/migrate/20250708104133_add_indexes_to_digital_dispute_submissions.rb
+++ db/migrate/20250708104133_add_indexes_to_digital_dispute_submissions.rb
@@ -5,7 +5,6 @@ class AddIndexesToDigitalDisputeSubmissions < ActiveRecord::Migration[7.2]
 
   def change
     add_index :digital_dispute_submissions, :user_uuid, algorithm: :concurrently
-    add_index :digital_dispute_submissions, :user_account_id, algorithm: :concurrently
     add_index :digital_dispute_submissions, :needs_kms_rotation, algorithm: :concurrently
     add_index :digital_dispute_submissions, :debt_identifiers, using: :gin, algorithm: :concurrently
   end
