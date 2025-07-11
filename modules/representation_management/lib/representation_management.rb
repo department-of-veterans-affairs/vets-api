ee0fc18e334b1aae8c4855494c621729875a0015
Holden Hinkle <holden@holdenhinkle.com>
2025-07-11 12:32:40 -0400

    update the RepresentationManagement config

--- modules/representation_management/lib/representation_management.rb
+++ modules/representation_management/lib/representation_management.rb
@@ -13,7 +13,6 @@ module RepresentationManagement
                                    AGENTS => OpenStruct.new({
                                                               api_type: 'agent',
                                                               individual_type: 'claims_agent',
-                                                              responses_var: :@agent_responses,
                                                               ids_var: :@agent_ids,
                                                               json_var: :@agent_json_for_address_validation,
                                                               validation_description: 'Batching agent address updates from GCLAWS Accreditation API'
@@ -21,7 +20,6 @@ module RepresentationManagement
                                    ATTORNEYS => OpenStruct.new({
                                                                  api_type: 'attorney',
                                                                  individual_type: 'attorney',
-                                                                 responses_var: :@attorney_responses,
                                                                  ids_var: :@attorney_ids,
                                                                  json_var: :@attorney_json_for_address_validation,
                                                                  validation_description: 'Batching attorney address updates from GCLAWS Accreditation API'
@@ -29,7 +27,6 @@ module RepresentationManagement
                                    REPRESENTATIVES => OpenStruct.new({
                                                                        api_type: 'representative',
                                                                        individual_type: 'representative',
-                                                                       responses_var: :@representative_responses,
                                                                        ids_var: :@representative_ids,
                                                                        json_var: :@representative_json_for_address_validation,
                                                                        validation_description: 'Batching representative address updates from GCLAWS Accreditation API'
@@ -37,7 +34,6 @@ module RepresentationManagement
                                    VSOS => OpenStruct.new({
                                                             api_type: 'veteran_service_organization',
                                                             individual_type: 'veteran_service_organization',
-                                                            responses_var: :@vso_responses,
                                                             ids_var: :@vso_ids,
                                                             json_var: :@vso_json_for_address_validation,
                                                             validation_description: 'Batching VSO address updates from GCLAWS Accreditation API'
