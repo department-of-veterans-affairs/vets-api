b5bc58d66da1728863d5c2fd51a8179effd3c639
nfstern02 <72567812+nfstern02@users.noreply.github.com>
2025-07-11 16:29:14 -0500

    add flipper_utils to stop cosmetic error showing up in the log on startup

---
+++ spec/lib/flipper_utils_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe FlipperUtils do
+  describe '.safe_enabled?' do
+    let(:feature_name) { :some_feature }
+
+    context 'when Flipper is defined and initialized' do
+      before do
+        stub_const('Flipper', double('Flipper'))
+
+        allow(Flipper).to receive(:respond_to?).with(:enabled).and_return(true)
+        allow(Flipper).to receive(:enabled?).with(feature_name).and_return(true)
+      end
+
+      it 'returns true if the feature is enabled' do
+        expect(described_class.safe_enabled?(feature_name)).to be(true)
+      end
+
+      it 'returns false if the feature is disabled' do
+        allow(Flipper).to receive(:enabled?).with(feature_name).and_return(false)
+        expect(described_class.safe_enabled?(feature_name)).to be(false)
+      end
+    end
+
+    context 'when Flipper is not enabled' do
+      before do
+        stub_const('Flipper', double('Flipper'))
+
+        allow(Flipper).to receive(:respond_to?).with(:enabled).and_return(false)
+      end
+
+      it 'returns false' do
+        expect(described_class.safe_enabled?(feature_name)).to be(false)
+      end
+    end
+
+    context 'when Flipper raises an exception' do
+      before do
+        stub_const('Flipper', double('Flipper'))
+
+        allow(Flipper).to receive(:respond_to?).with(:enabled).and_return(true)
+        allow(Flipper).to receive(:enabled?).and_raise(StandardError.new('Boom'))
+      end
+
+      it 'logs the error and returns false' do
+        expect(Rails.logger).to receive(:warn).with(/Flipper check failed/)
+        expect(described_class.safe_enabled?(feature_name)).to be(false)
+      end
+    end
+  end
+end
