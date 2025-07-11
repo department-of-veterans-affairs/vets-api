616bfd5cb3f98058a38a207a9827e957b3e913e1
Eric Boehs <eric@boehs.com>
2025-07-11 11:57:22 -0500

    Cache deprecation instance to avoid object creation
    Store ActiveSupport::Deprecation instance in constant to avoid
    creating new objects on every method call.

--- lib/sentry_logging.rb
+++ lib/sentry_logging.rb
@@ -9,9 +9,10 @@ module SentryLogging
 
   DEPRECATION_MESSAGE = 'SentryLogging is deprecated and will be removed in October 2025. ' \
                         'Use Vets::SharedLogging instead.'
+  DEPRECATION_INSTANCE = ActiveSupport::Deprecation.new
 
   def log_message_to_sentry(message, level, extra_context = {}, tags_context = {})
-    ActiveSupport::Deprecation.new.warn(DEPRECATION_MESSAGE)
+    DEPRECATION_INSTANCE.warn(DEPRECATION_MESSAGE)
     level = normalize_level(level, nil)
     formatted_message = extra_context.empty? ? message : "#{message} : #{extra_context}"
     rails_logger(level, formatted_message)
@@ -23,7 +24,7 @@ module SentryLogging
   end
 
   def log_exception_to_sentry(exception, extra_context = {}, tags_context = {}, level = 'error')
-    ActiveSupport::Deprecation.new.warn(DEPRECATION_MESSAGE)
+    DEPRECATION_INSTANCE.warn(DEPRECATION_MESSAGE)
     level = normalize_level(level, exception)
 
     if Settings.sentry.dsn.present?
