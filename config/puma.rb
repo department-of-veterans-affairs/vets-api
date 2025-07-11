ec38c5a2c453860013316ff722093769e3d2a780
Eric Boehs <eric@boehs.com>
2025-07-10 23:37:43 -0500

    Fix Puma worker boot warnings for single mode
    - Make on_worker_boot and on_worker_shutdown callbacks conditional
    - Only define when WEB_CONCURRENCY > 0 (cluster mode)
    - Eliminates warnings in development single mode
    - Maintains functionality when running with workers
    
    Clean server startup with no warnings in development mode.

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-workers Integer(ENV.fetch('WEB_CONCURRENCY', 0))
+worker_count = Integer(ENV.fetch('WEB_CONCURRENCY', 0))
+workers worker_count
+
 threads_count_min = Integer(ENV.fetch('RAILS_MIN_THREADS', 5))
 threads_count_max = Integer(ENV.fetch('RAILS_MAX_THREADS', 5))
 threads(threads_count_min, threads_count_max)
@@ -8,12 +10,15 @@ threads(threads_count_min, threads_count_max)
 # used for a healthcheck endpoint that will not consume one of the threads
 activate_control_app 'tcp://0.0.0.0:9293', { no_token: true }
 
-on_worker_boot do
-  SemanticLogger.reopen
-  ActiveRecord::Base.establish_connection
-end
+# Only define worker callbacks when actually using workers (cluster mode)
+if worker_count > 0
+  on_worker_boot do
+    SemanticLogger.reopen
+    ActiveRecord::Base.establish_connection
+  end
 
-on_worker_shutdown do
-  require 'kafka/producer_manager'
-  Kafka::ProducerManager.instance.producer&.close
+  on_worker_shutdown do
+    require 'kafka/producer_manager'
+    Kafka::ProducerManager.instance.producer&.close
+  end
 end
