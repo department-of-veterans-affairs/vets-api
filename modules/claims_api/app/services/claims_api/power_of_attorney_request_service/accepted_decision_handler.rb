e1171e221b81d7da1bb3dfdacf332abaebd9950b
rockwellwindsor-va <rockwell.rice@va.gov>
2025-07-14 16:14:43 -0500

    API-43735-gather-data-for-poa-accept-2
    * Adjusts exsting call to the vnp addrs primary key BEP (formerly BGS) call
    * Adds new service for data mapping the result
    * Adds tests for new service
    	modified:   modules/claims_api/app/services/claims_api/power_of_attorney_request_service/accepted_decision_handler.rb
    	new file:   modules/claims_api/app/services/claims_api/power_of_attorney_request_service/data_mapper/vnp_ptcpnt_addrs_find_by_primary_key_service.rb
    	modified:   modules/claims_api/lib/bgs_service/vnp_ptcpnt_addrs_service.rb
    	new file:   modules/claims_api/spec/services/power_of_attorney_request_service/data_mapper/vnp_ptcpnt_addrs_find_by_primary_key_service_spec.rb

--- modules/claims_api/app/services/claims_api/power_of_attorney_request_service/accepted_decision_handler.rb
+++ modules/claims_api/app/services/claims_api/power_of_attorney_request_service/accepted_decision_handler.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'bgs_service/veteran_representative_service'
+require 'bgs_service/vnp_ptcpnt_addrs_service'
 
 module ClaimsApi
   module PowerOfAttorneyRequestService
@@ -24,22 +25,46 @@ module ClaimsApi
       private
 
       def gather_poa_data
-        records = read_all_vateran_representative_records
+        read_all_data = gather_read_all_veteran_representative_data
 
-        gather_read_all_veteran_representative_data(records)
-      end
+        vnp_find_addrs_data = gather_vnp_addrs_data(@vet_pctpnt_id, 'veteran')
 
-      def gather_read_all_veteran_representative_data(records)
-        ClaimsApi::PowerOfAttorneyRequestService::DataMapper::ReadAllVeteranRepresentativeDataMapper.new(
-          proc_id: @proc_id,
-          records:
-        ).call
+        if @claimant_ptcpnt_id.present?
+          claimant_addr_data = gather_vnp_addrs_data(@claimant_ptcpnt_id, 'claimant')
+
+          read_all_data.merge!(claimant: claimant_addr_data)
+        end
+
+        read_all_data.merge!(veteran: vnp_find_addrs_data)
       end
 
       def read_all_vateran_representative_records
         ClaimsApi::VeteranRepresentativeService
           .new(external_uid: @vet_ptcpnt_id, external_key: @vet_ptcpnt_id)
           .read_all_veteran_representatives(type_code: FORM_TYPE_CODE, ptcpnt_id: @vet_ptcpnt_id)
+      end
+
+      def gather_read_all_veteran_representative_data
+        records = read_all_vateran_representative_records
+        # error if records nil
+        ClaimsApi::PowerOfAttorneyRequestService::DataMapper::ReadAllVeteranRepresentativeDataMapper.new(
+          proc_id: @proc_id,
+          records:
+        ).call
+      end
+
+      # key is 'veteran' or 'claimant'
+      def gather_vnp_addrs_data(pctpnt_id, key)
+        primary_key = @metadata.dig(key, 'vnp_mail_id')
+        # error if primary_key nil
+        res = ClaimsApi::VnpPtcpntAddrsService
+              .new(external_uid: pctpnt_id, external_key: pctpnt_id)
+              .vnp_ptcpnt_addrs_find_by_primary_key(id: primary_key)
+
+        ClaimsApi::PowerOfAttorneyRequestService::DataMapper::VnpPtcpntAddrsFindByPrimaryKeyService.new(
+          record: res,
+          key:
+        ).call
       end
     end
   end
