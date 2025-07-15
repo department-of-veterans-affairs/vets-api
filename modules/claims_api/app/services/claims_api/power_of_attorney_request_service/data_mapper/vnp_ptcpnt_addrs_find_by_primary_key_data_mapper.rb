b1b30048b2c2a6b577413083566d1cb744ee581e
rockwellwindsor-va <129893414+rockwellwindsor-va@users.noreply.github.com>
2025-07-15 10:43:38 -0500

    Fixes naming on files to be inline with previous mapper files

--- modules/claims_api/app/services/claims_api/power_of_attorney_request_service/data_mapper/vnp_ptcpnt_addrs_find_by_primary_key_data_mapper.rb
+++ modules/claims_api/app/services/claims_api/power_of_attorney_request_service/data_mapper/vnp_ptcpnt_addrs_find_by_primary_key_data_mapper.rb
@@ -14,6 +14,7 @@ module ClaimsApi
 
         private
 
+<<<<<<< HEAD
         # The data structure of the data returned from these calls to
         # BEP (BGS) is not uniform. The data returned here is like data[:value]
         def build_data_object
@@ -26,6 +27,18 @@ module ClaimsApi
             'zip_prefix_nbr' => @record[:zip_prefix_nbr],
             'zip_first_suffix_nbr' => @record[:zip_first_suffix_nbr],
             'email_addrs_txt' => @record[:email_addrs_txt]
+=======
+        def build_data_object
+          {
+            addrs_one_txt: @record[:addrs_one_txt],
+            addrs_two_txt: @record[:addrs_two_txt],
+            city_nm: @record[:city_nm],
+            cntry_nm: @record[:cntry_nm],
+            prvnc_nm: @record[:prvnc_nm],
+            zip_prefix_nbr: @record[:zip_prefix_nbr],
+            zip_first_suffix_nbr: @record[:zip_first_suffix_nbr],
+            email_addrs_txt: @record[:email_addrs_txt]
+>>>>>>> 05485bf6ad (Fixes naming on files to be inline with previous mapper files)
           }
         end
       end
--- modules/claims_api/spec/services/power_of_attorney_request_service/data_mapper/vnp_ptcpnt_addrs_find_by_primary_key_data_mapper_spec.rb
+++ modules/claims_api/spec/services/power_of_attorney_request_service/data_mapper/vnp_ptcpnt_addrs_find_by_primary_key_data_mapper_spec.rb
@@ -11,7 +11,10 @@ describe ClaimsApi::PowerOfAttorneyRequestService::DataMapper::VnpPtcpntAddrsFin
       addrs_two_txt: 'Apt 2',
       city_nm: 'Los Angeles',
       cntry_nm: 'USA',
+<<<<<<< HEAD
       postal_cd: 'CA',
+=======
+>>>>>>> 05485bf6ad (Fixes naming on files to be inline with previous mapper files)
       zip_first_suffix_nbr: '0200',
       zip_prefix_nbr: '92264'
     }
@@ -19,6 +22,7 @@ describe ClaimsApi::PowerOfAttorneyRequestService::DataMapper::VnpPtcpntAddrsFin
 
   let(:expected_data_obj) do
     {
+<<<<<<< HEAD
       'addrs_one_txt' => '2719 Atlas Ave',
       'addrs_two_txt' => 'Apt 2',
       'city_nm' => 'Los Angeles',
@@ -27,6 +31,16 @@ describe ClaimsApi::PowerOfAttorneyRequestService::DataMapper::VnpPtcpntAddrsFin
       'zip_prefix_nbr' => '92264',
       'zip_first_suffix_nbr' => '0200',
       'email_addrs_txt' => nil
+=======
+      addrs_one_txt: '2719 Atlas Ave',
+      addrs_two_txt: 'Apt 2',
+      city_nm: 'Los Angeles',
+      cntry_nm: 'USA',
+      prvnc_nm: nil,
+      zip_prefix_nbr: '92264',
+      zip_first_suffix_nbr: '0200',
+      email_addrs_txt: nil
+>>>>>>> 05485bf6ad (Fixes naming on files to be inline with previous mapper files)
     }
   end
 
