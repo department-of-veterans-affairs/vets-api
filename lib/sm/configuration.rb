d250e84053b8af21f8880fd8b78ec2df2a65aa64
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-07-15 15:16:44 -0600

    Update Flipper usage in configuration files and remove obsolete tests

--- lib/sm/configuration.rb
+++ lib/sm/configuration.rb
@@ -33,7 +33,7 @@ module SM
     # @return [String] Base path for dependent URLs
     #
     def base_path
-      if FlipperUtils.safe_enabled?(:mhv_secure_messaging_migrate_to_api_gateway)
+      if Flipper.enabled?(:mhv_secure_messaging_migrate_to_api_gateway)
         "#{Settings.mhv.api_gateway.hosts.sm_patient}/#{Settings.mhv.sm.gw_base_path}"
       else
         "#{Settings.mhv.sm.host}/#{Settings.mhv.sm.base_path}"
