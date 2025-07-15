56a1343d6fdf20a26de7ec85e09d13bf8037c332
rockwellwindsor-va <129893414+rockwellwindsor-va@users.noreply.github.com>
2025-07-15 09:06:56 -0500

    API-43735-gather-data-for-poa-accept-2
    * Adds spport for claimant information
    * Updates the province_nm to postal_cd so country code is grabbed as expected
    
    	modified:   modules/claims_api/app/controllers/claims_api/v2/veterans/power_of_attorney/request_controller.rb
    	modified:   modules/claims_api/app/services/claims_api/power_of_attorney_request_service/accepted_decision_handler.rb
    	modified:   modules/claims_api/app/services/claims_api/power_of_attorney_request_service/decision_handler.rb
    	modified:   modules/claims_api/spec/controllers/v2/veterans/power_of_attorney/request_controller_spec.rb
    	modified:   modules/claims_api/spec/services/power_of_attorney_request_service/decision_handler_spec.rb

--- modules/claims_api/app/services/claims_api/power_of_attorney_request_service/data_mapper/read_all_veteran_representative_data_mapper.rb
+++ modules/claims_api/app/services/claims_api/power_of_attorney_request_service/data_mapper/read_all_veteran_representative_data_mapper.rb
@@ -25,22 +25,22 @@ module ClaimsApi
           return [] if data.nil?
 
           {
-            service_number: data['serviceNumber'],
-            insurance_numbers: data['insuranceNumbers'],
-            phone_number: data['phoneNumber'],
-            claimant_relationship: data['claimantRelationship'],
-            poa_code: data['poaCode'],
-            organization_name: data['organizationName'],
-            representativeLawFirmOrAgencyName: data['representativeLawFirmOrAgencyName'],
-            representative_first_name: data['representativeFirstName'],
-            representative_last_name: data['representativeLastName'],
-            representative_title: data['representativeTitle'],
-            section_7332_auth: data['section7332Auth'],
-            limitation_alcohol: data['limitationAlcohol'],
-            limitation_drug_abuse: data['limitationDrugAbuse'],
-            limitation_hiv: data['limitationHIV'],
-            limitation_sca: data['limitationSCA'],
-            change_address_auth: data['changeAddressAuth']
+            'service_number' => data['serviceNumber'],
+            'insurance_numbers' => data['insuranceNumbers'],
+            'phone_number' => data['phoneNumber'],
+            'claimant_relationship' => data['claimantRelationship'],
+            'poa_code' => data['poaCode'],
+            'organization_name' => data['organizationName'],
+            'representativeLawFirmOrAgencyName' => data['representativeLawFirmOrAgencyName'],
+            'representative_first_name' => data['representativeFirstName'],
+            'representative_last_name' => data['representativeLastName'],
+            'representative_title' => data['representativeTitle'],
+            'section_7332_auth' => data['section7332Auth'],
+            'limitation_alcohol' => data['limitationAlcohol'],
+            'limitation_drug_abuse' => data['limitationDrugAbuse'],
+            'limitation_hiv' => data['limitationHIV'],
+            'limitation_sca' => data['limitationSCA'],
+            'change_address_auth' => data['changeAddressAuth']
           }
         end
       end
---
+++ modules/claims_api/app/services/claims_api/power_of_attorney_request_service/data_mapper/vnp_ptcpnt_addrs_find_by_primary_key_data_mapper.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module ClaimsApi
+  module PowerOfAttorneyRequestService
+    module DataMapper
+      class VnpPtcpntAddrsFindByPrimaryKeyDataMapper
+        def initialize(record:)
+          @record = record
+        end
+
+        def call
+          build_data_object
+        end
+
+        private
+
+        def build_data_object
+          {
+            'addrs_one_txt' => @record['addrs_one_txt'],
+            'addrs_two_txt' => @record['addrs_two_txt'],
+            'city_nm' => @record['city_nm'],
+            'cntry_nm' => @record['cntry_nm'],
+            'postal_cd' => @record['postal_cd'],
+            'zip_prefix_nbr' => @record['zip_prefix_nbr'],
+            'zip_first_suffix_nbr' => @record['zip_first_suffix_nbr'],
+            'email_addrs_txt' => @record['email_addrs_txt']
+          }
+        end
+      end
+    end
+  end
+end
