b40c1c12682988b8d15b7f85bab1ba5ff160c2b2
rockwellwindsor-va <129893414+rockwellwindsor-va@users.noreply.github.com>
2025-07-16 16:58:14 -0500

    Fixes test in accepted_decision_handler_spec.rb

--- modules/claims_api/spec/services/power_of_attorney_request_service/accepted_decision_handler_spec.rb
+++ modules/claims_api/spec/services/power_of_attorney_request_service/accepted_decision_handler_spec.rb
@@ -99,37 +99,6 @@ describe ClaimsApi::PowerOfAttorneyRequestService::AcceptedDecisionHandler do
       end
       let(:expected_veteran_response) do
         {
-<<<<<<< HEAD
-          name: 'Ralph Lee',
-          ssn: '796378782',
-          file_number: '796378782',
-          date_of_birth: '1948-10-30',
-          service_number: '123678453',
-          insurance_numbers: '1234567890',
-          phone_number: '5555551234',
-          claimant_relationship: 'Spouse',
-          poa_code: '083',
-          organization_name: 'DISABLED AMERICAN VETERANS',
-          representativeLawFirmOrAgencyName: nil,
-          representative_first_name: 'John',
-          representative_last_name: 'Doe',
-          representative_title: nil,
-          section_7332_auth: 'true',
-          limitation_alcohol: 'true',
-          limitation_drug_abuse: 'true',
-          limitation_hiv: 'true',
-          limitation_sca: 'true',
-          change_address_auth: 'true',
-          addrs_one_txt: '2719 Hyperion Ave',
-          addrs_two_txt: 'Apt 2',
-          city_nm: 'Los Angeles',
-          cntry_nm: 'USA',
-          prvnc_nm: nil,
-          registration_number: '12399998',
-          zip_prefix_nbr: '92264',
-          zip_first_suffix_nbr: '0200',
-          email_addrs_txt: nil
-=======
           'name' => 'Ralph Lee',
           'ssn' => '796378782',
           'file_number' => '796378782',
@@ -155,10 +124,14 @@ describe ClaimsApi::PowerOfAttorneyRequestService::AcceptedDecisionHandler do
           'city_nm' => 'Los Angeles',
           'cntry_nm' => 'USA',
           'prvnc_nm' => nil,
+          'registration_number' => '12399998',
           'zip_prefix_nbr' => '92264',
           'zip_first_suffix_nbr' => '0200',
           'email_addrs_txt' => nil
+<<<<<<< HEAD
 >>>>>>> 2d0b7b7aa2 (Merges in upstream, fixes conflicts and cleans up requests to match)
+=======
+>>>>>>> 4bf4a11c3e (Fixes test in accepted_decision_handler_spec.rb)
         }
       end
 
@@ -201,52 +174,6 @@ describe ClaimsApi::PowerOfAttorneyRequestService::AcceptedDecisionHandler do
 
       let(:expected_dependent_response) do
         {
-<<<<<<< HEAD
-          name: 'Ralph Lee',
-          ssn: '796378782',
-          file_number: '796378782',
-          date_of_birth: '1948-10-30',
-          service_number: '123678453',
-          insurance_numbers: '1234567890',
-          phone_number: '5555551234',
-          claimant_relationship: 'Spouse',
-          poa_code: '083',
-          organization_name: 'DISABLED AMERICAN VETERANS',
-          representativeLawFirmOrAgencyName: nil,
-          representative_first_name: 'John',
-          representative_last_name: 'Doe',
-          representative_title: nil,
-          section_7332_auth: 'true',
-          limitation_alcohol: 'true',
-          limitation_drug_abuse: 'true',
-          limitation_hiv: 'true',
-          limitation_sca: 'true',
-          change_address_auth: 'true',
-          addrs_one_txt: '2719 Hyperion Ave',
-          addrs_two_txt: 'Apt 2',
-          city_nm: 'Los Angeles',
-          cntry_nm: 'USA',
-          prvnc_nm: nil,
-          registration_number: '12399998',
-          zip_prefix_nbr: '92264',
-          zip_first_suffix_nbr: '0200',
-          email_addrs_txt: nil,
-          claimant: {
-            claimant_id: '1013093331V548481',
-            name: 'Wally Morell',
-            ssn: '796378782',
-            file_number: '796378782',
-            date_of_birth: '1948-10-30',
-            addrs_one_txt: '123 Main St',
-            addrs_two_txt: 'Apt 3',
-            city_nm: 'Boston',
-            cntry_nm: 'USA',
-            prvnc_nm: nil,
-            zip_prefix_nbr: '02110',
-            zip_first_suffix_nbr: '1000',
-            email_addrs_txt: nil,
-            phone_nbr: '5555559876'
-=======
           'name' => 'Ralph Lee',
           'ssn' => '796378782',
           'file_number' => '796378782',
@@ -272,6 +199,7 @@ describe ClaimsApi::PowerOfAttorneyRequestService::AcceptedDecisionHandler do
           'city_nm' => 'Los Angeles',
           'cntry_nm' => 'USA',
           'prvnc_nm' => nil,
+          'registration_number' => '12399998',
           'zip_prefix_nbr' => '92264',
           'zip_first_suffix_nbr' => '0200',
           'email_addrs_txt' => nil,
@@ -283,13 +211,17 @@ describe ClaimsApi::PowerOfAttorneyRequestService::AcceptedDecisionHandler do
             'addrs_one_txt' => '123 Main St',
             'addrs_two_txt' => 'Apt 3',
             'city_nm' => 'Boston',
+            'claimant_id' => '1013093331V548481',
             'cntry_nm' => 'USA',
             'prvnc_nm' => nil,
             'zip_prefix_nbr' => '02110',
             'zip_first_suffix_nbr' => '1000',
             'email_addrs_txt' => nil,
             'phone_nbr' => '5555559876'
+<<<<<<< HEAD
 >>>>>>> 2d0b7b7aa2 (Merges in upstream, fixes conflicts and cleans up requests to match)
+=======
+>>>>>>> 4bf4a11c3e (Fixes test in accepted_decision_handler_spec.rb)
           }
         }
       end
