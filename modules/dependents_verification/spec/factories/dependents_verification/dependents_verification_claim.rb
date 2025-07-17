fa76d680da114caa32047e4bffd60cc3f3b3af31
David Becker <david@beckersite.com>
2025-07-17 09:31:52 -0600

    Dv 114121 (#23114)
    * Update 21-0538 fixtures to match FE parameters
    
    * Update 210538 pdf section fillers to match form data and remove addtl fields
    
    * Remove unneeded spec, update 210538 pdf filler spec
    
    * Add run_at param for fill form examples
    
    * override run at param for 210538 pdf filler spec to match fixtures
    
    * rubocop
    
    * deep dup for better code
    
    * update dependents_verification_claim factory
    
    * Add full ssn into DV 0538 saved claim for pdf fill
    
    * update DV 0538 saved claim methods
    
    * update specs

--- modules/dependents_verification/spec/factories/dependents_verification/dependents_verification_claim.rb
+++ modules/dependents_verification/spec/factories/dependents_verification/dependents_verification_claim.rb
@@ -5,19 +5,51 @@ FactoryBot.define do
     form_id { '21-0538' }
     form do
       {
-        veteranFullName: {
-          first: 'John',
-          middle: 'Edmund',
-          last: 'Doe'
+        veteranInformation: {
+          fullName: {
+            first: 'Jane',
+            middle: 'Elizabeth',
+            last: 'Maximal'
+          },
+          birthDate: '2020-01-01',
+          ssnLastFour: '4445'
         },
-        claimantFullName: {
-          first: 'Derrick',
-          middle: 'A',
-          last: 'Stewart'
+        address: {
+          addressType: 'DOMESTIC',
+          street: '123 Main St',
+          unitNumber: '4B',
+          city: 'Anytown',
+          state: 'NY',
+          postalCode: '12345-1234',
+          country: 'USA'
         },
-        veteranSocialSecurityNumber: '333224444',
-        statementOfTruthCertified: true,
-        statementOfTruthSignature: 'John Edmund Doe'
+        dependents: [
+          {
+            awardIndicator: 'Y',
+            dateOfBirth: '07/07/1970',
+            firstName: 'SPOUSY',
+            gender: 'F',
+            lastName: 'FOSTER',
+            proofOfDependency: 'N',
+            ptcpntId: '600055042',
+            relatedToVet: 'Y',
+            relationship: 'Spouse',
+            ssn: '3332',
+            ssnVerifyStatus: '0',
+            veteranIndicator: 'N',
+            dob: 'July 7, 1970',
+            fullName: 'SPOUSY FOSTER',
+            age: 55,
+            removalDate: ''
+          }
+        ],
+        hasDependentsStatusChanged: 'N',
+        email: 'maximal@example.com',
+        phone: '5551234567',
+        internationalPhone: '15552229999',
+        electronicCorrespondence: 'true',
+        statementOfTruthSignature: 'Jane Elizabeth Maximal',
+        statementOfTruthCertified: true
       }.to_json
     end
   end
