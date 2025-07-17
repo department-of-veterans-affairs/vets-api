d597f3679f56b2b3206dddffda13b70fcbe81056
Evan Smith <evan.smith@oddball.io>
2025-07-17 11:01:59 -0400

    pass nil to postal_cd and prvnc_nm if foriegn address

--- spec/models/bgs_dependents_v2/base_spec.rb
+++ spec/models/bgs_dependents_v2/base_spec.rb
@@ -102,6 +102,20 @@ RSpec.describe BGSDependentsV2::Base do
         base.adjust_country_name_for!(address:)
         expect(address['country']).to eq('Italy')
       end
+
+      it 'receives nil for state in the address' do
+        address = sample_v2_dependent_application['veteran_contact_information']['veteran_address']
+        address['country'] = 'ITA'
+        address['international_postal_code'] = '12345'
+        address['state'] = 'Tuscany'
+        base.dependent_address(
+          dependents_application: sample_v2_dependent_application,
+          lives_with_vet: true,
+          alt_address: nil
+        )
+        expect(address['postal_cd']).to eq(nil)
+        expect(address['prvnc_nm']).to eq(nil)
+      end
     end
   end
 end
