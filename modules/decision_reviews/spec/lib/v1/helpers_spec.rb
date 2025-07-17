a7f0f414872115359582d782c42c0057ddfaff5f
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-07-17 16:23:10 -0500

    Reorganize tests per PR comments

--- modules/decision_reviews/spec/lib/v1/helpers_spec.rb
+++ modules/decision_reviews/spec/lib/v1/helpers_spec.rb
@@ -7,57 +7,61 @@ describe DecisionReviews::V1::Helpers do
   let(:helper) { Class.new { include DecisionReviews::V1::Helpers }.new }
 
   describe 'format_phone_number' do
-    it 'returns {} if phone is nil' do
-      expect(helper.format_phone_number(nil)).to eq({})
+    context 'international phone numbers' do
+      it 'returns {} if phone is nil' do
+        expect(helper.format_phone_number(nil)).to eq({})
+      end
+
+      it 'formats phone number with country code, area code, and number' do
+        phone = { 'countryCode' => '44', 'areaCode' => '20', 'phoneNumber' => '5550456' }
+        expect(helper.format_phone_number(phone)).to eq({
+                                                          internationalPhoneNumber: '+44 205550456'
+                                                        })
+      end
+
+      it 'formats phone number with nil area code' do
+        phone = { 'areaCode' => nil, 'countryCode' => '44', 'phoneNumber' => '5550456' }
+        expect(helper.format_phone_number(phone)).to eq({
+                                                          internationalPhoneNumber: '+44 5550456'
+                                                        })
+      end
+
+      it 'formats phone number with empty area code' do
+        phone = { 'areaCode' => '', 'countryCode' => '44', 'phoneNumber' => '5550456' }
+        expect(helper.format_phone_number(phone)).to eq({
+                                                          internationalPhoneNumber: '+44 5550456'
+                                                        })
+      end
+
+      it 'formats phone number with no area code' do
+        phone = { 'countryCode' => '44', 'phoneNumber' => '5550456' }
+        expect(helper.format_phone_number(phone)).to eq({
+                                                          internationalPhoneNumber: '+44 5550456'
+                                                        })
+      end
     end
 
-    it 'formats phone number with country code, area code, and number' do
-      phone = { 'countryCode' => '44', 'areaCode' => '20', 'phoneNumber' => '5550456' }
-      expect(helper.format_phone_number(phone)).to eq({
-                                                        internationalPhoneNumber: '+44 205550456'
-                                                      })
-    end
+    context 'domestic phone numbers' do
+      it 'formats phone number with nil country code' do
+        phone = { 'countryCode' => nil, 'areaCode' => '210', 'phoneNumber' => '5550456' }
+        expect(helper.format_phone_number(phone)).to eq({
+                                                          veteranPhone: '2105550456'
+                                                        })
+      end
 
-    it 'formats phone number with nil area code' do
-      phone = { 'areaCode' => nil, 'countryCode' => '44', 'phoneNumber' => '5550456' }
-      expect(helper.format_phone_number(phone)).to eq({
-                                                        internationalPhoneNumber: '+44 5550456'
-                                                      })
-    end
+      it 'formats phone number with empty country code' do
+        phone = { 'countryCode' => '', 'areaCode' => '210', 'phoneNumber' => '5550456' }
+        expect(helper.format_phone_number(phone)).to eq({
+                                                          veteranPhone: '2105550456'
+                                                        })
+      end
 
-    it 'formats phone number with empty area code' do
-      phone = { 'areaCode' => '', 'countryCode' => '44', 'phoneNumber' => '5550456' }
-      expect(helper.format_phone_number(phone)).to eq({
-                                                        internationalPhoneNumber: '+44 5550456'
-                                                      })
-    end
-
-    it 'formats phone number with no area code' do
-      phone = { 'countryCode' => '44', 'phoneNumber' => '5550456' }
-      expect(helper.format_phone_number(phone)).to eq({
-                                                        internationalPhoneNumber: '+44 5550456'
-                                                      })
-    end
-
-    it 'formats phone number with nil country code' do
-      phone = { 'countryCode' => nil, 'areaCode' => '210', 'phoneNumber' => '5550456' }
-      expect(helper.format_phone_number(phone)).to eq({
-                                                        veteranPhone: '2105550456'
-                                                      })
-    end
-
-    it 'formats phone number with empty country code' do
-      phone = { 'countryCode' => '', 'areaCode' => '210', 'phoneNumber' => '5550456' }
-      expect(helper.format_phone_number(phone)).to eq({
-                                                        veteranPhone: '2105550456'
-                                                      })
-    end
-
-    it 'formats phone number with no country code' do
-      phone = { 'areaCode' => '210', 'phoneNumber' => '5550456' }
-      expect(helper.format_phone_number(phone)).to eq({
-                                                        veteranPhone: '2105550456'
-                                                      })
+      it 'formats phone number with no country code' do
+        phone = { 'areaCode' => '210', 'phoneNumber' => '5550456' }
+        expect(helper.format_phone_number(phone)).to eq({
+                                                          veteranPhone: '2105550456'
+                                                        })
+      end
     end
   end
 end
