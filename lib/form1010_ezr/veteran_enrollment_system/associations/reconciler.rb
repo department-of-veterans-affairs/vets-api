64413153d77be1615c7562e11919d93609052db4
Joshua <joshing_you1@yahoo.com>
2025-07-20 21:05:22 -0400

    DRYd up code

--- lib/form1010_ezr/veteran_enrollment_system/associations/reconciler.rb
+++ lib/form1010_ezr/veteran_enrollment_system/associations/reconciler.rb
@@ -34,7 +34,7 @@ module Form1010Ezr
           # As of 07/18/2025, we are not sending OTHER_NEXT_OF_KIN or OTHER_EMERGENCY_CONTACT data to the Associations API
           transformed_ves_associations = 
             transform_ves_associations(
-              @ves_associations.reject { |obj| obj['role'] == 'OTHER_NEXT_OF_KIN' || obj['role'] == 'OTHER_EMERGENCY_CONTACT' }
+              @ves_associations.reject { |obj| %w[OTHER_NEXT_OF_KIN OTHER_EMERGENCY_CONTACT].include?(obj['role']) }
             )
           # Create a lookup set of contactTypes in the submitted array.
           # We'll use this to find missing association objects (e.g. associations that were deleted on the frontend)
