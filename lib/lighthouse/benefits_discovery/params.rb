2d5c05bb3705e2cf1bd0176748a6adbd85de3712
Kris Pethtel <kris.pethtel@gmail.com>
2025-07-22 15:51:36 -0500

    1834: refactor benefits eligibility to prevent va profile request

--- lib/lighthouse/benefits_discovery/params.rb
+++ lib/lighthouse/benefits_discovery/params.rb
@@ -2,19 +2,40 @@
 
 module BenefitsDiscovery
   class Params
-    def initialize(user_uuid)
-      @user = User.find(user_uuid)
-      raise Common::Exceptions::RecordNotFound, user_uuid if @user.nil?
+    def initialize(user)
+      @user = user
     end
 
-    def prepared_params
+    def prepared_params(service_history_params)
       {
         dateOfBirth: @user.birth_date,
-        dischargeStatus: discharge_status,
-        branchOfService: service_history.map { |sh| sh.branch_of_service&.upcase },
-        disabilityRating: disability_rating,
-        serviceDates: service_history.map { |sh| { beginDate: sh.begin_date, endDate: sh.end_date } }
-      }.compact_blank
+        disabilityRating: disability_rating
+      }.merge(service_history_params).compact_blank
+    end
+
+    class << self
+      def service_history_params(service_history_episodes)
+        {
+          dischargeStatus: discharge_status(service_history_episodes),
+          branchOfService: service_history_episodes.map { |sh| sh.branch_of_service&.upcase },
+          serviceDates: service_history_episodes.map { |sh| { beginDate: sh.begin_date, endDate: sh.end_date } }
+        }
+      end
+
+      private
+
+      def discharge_status(episodes)
+        episodes.map do |sh|
+          code = sh[:character_of_discharge_code]
+          discharge_type = VAProfile::Prefill::MilitaryInformation::DISCHARGE_TYPES[code]
+          if discharge_type.nil?
+            Rails.logger.error("No matching discharge code for: #{discharge_type}")
+            nil
+          else
+            "#{discharge_type.upcase.gsub('-', '_')}_DISCHARGE"
+          end
+        end.compact
+      end
     end
 
     private
@@ -23,27 +44,6 @@ module BenefitsDiscovery
       service = VeteranVerification::Service.new
       response = service.get_rated_disabilities(@user.icn)
       response.dig('data', 'attributes', 'combined_disability_rating')
-    end
-
-    def service_history
-      @service_history ||= begin
-        service = VAProfile::MilitaryPersonnel::Service.new(@user)
-        response = service.get_service_history
-        response.episodes
-      end
-    end
-
-    def discharge_status
-      service_history.map do |sh|
-        code = sh[:character_of_discharge_code]
-        discharge_type = VAProfile::Prefill::MilitaryInformation::DISCHARGE_TYPES[code]
-        if discharge_type.nil?
-          Rails.logger.error("No matching discharge code for: #{discharge_type}")
-          nil
-        else
-          "#{discharge_type.upcase.gsub('-', '_')}_DISCHARGE"
-        end
-      end.compact
     end
   end
 end
--- spec/lib/lighthouse/benefits_discovery/params_spec.rb
+++ spec/lib/lighthouse/benefits_discovery/params_spec.rb
@@ -4,60 +4,68 @@ require 'rails_helper'
 require 'lighthouse/benefits_discovery/params'
 
 RSpec.describe BenefitsDiscovery::Params do
-  subject { described_class.new(user.uuid) }
+  subject { described_class.new(user) }
 
   let(:user) { create(:user, :loa3, :accountable, :legacy_icn) }
+  let(:prepared_service_history_params) do
+    {
+      dischargeStatus: [
+        'HONORABLE_DISCHARGE'
+      ],
+      branchOfService: [
+        'ARMY'
+      ],
+      serviceDates: [
+        {
+          beginDate: '2012-03-02',
+          endDate: '2018-10-31'
+        }
+      ]
+    }
+  end
 
   before do
     allow_any_instance_of(VeteranVerification::Configuration).to receive(:access_token).and_return('token')
-  end
-
-  describe '.new' do
-    context 'when user does not exist' do
-      it 'raises not found error' do
-        expect { described_class.new('boo') }.to raise_error(Common::Exceptions::RecordNotFound)
-      end
-    end
   end
 
   describe '#prepared_params' do
     it 'returns the correct prepared parameters' do
       expected_params = {
         dateOfBirth: '1809-02-12',
-        dischargeStatus: ['GENERAL_DISCHARGE'],
+        dischargeStatus: ['HONORABLE_DISCHARGE'],
         branchOfService: ['ARMY'],
         disabilityRating: 100,
-        serviceDates: [{ beginDate: '2002-02-02', endDate: '2008-12-01' }]
+        serviceDates: [{ beginDate: '2012-03-02', endDate: '2018-10-31' }]
       }
 
       VCR.use_cassette('lighthouse/veteran_verification/show/200_response') do
-        VCR.use_cassette('va_profile/military_personnel/post_read_service_history_200') do
-          expect(subject.prepared_params).to eq(expected_params)
-        end
+        expect(subject.prepared_params(prepared_service_history_params)).to eq(expected_params)
+      end
+    end
+
+    it 'omits any missing params' do
+      VCR.use_cassette('lighthouse/veteran_verification/show/200_response') do
+        expect(subject.prepared_params({})).to eq({
+                                                    dateOfBirth: '1809-02-12',
+                                                    disabilityRating: 100
+                                                  })
       end
     end
 
     context 'when veteran verification service fails' do
       it 'raises error' do
         VCR.use_cassette('lighthouse/veteran_verification/disability_rating/504_response') do
-          VCR.use_cassette('va_profile/military_personnel/post_read_service_history_200') do
-            expect { subject.prepared_params }.to raise_error(Common::Exceptions::GatewayTimeout, 'Gateway timeout')
-          end
+          expect { subject.prepared_params(prepared_service_history_params) }.to \
+            raise_error(Common::Exceptions::GatewayTimeout, 'Gateway timeout')
         end
       end
     end
+  end
 
-    context 'when military personnel request fails' do
-      it 'raises error' do
-        VCR.use_cassette('lighthouse/veteran_verification/show/200_response') do
-          VCR.use_cassette('va_profile/military_personnel/post_read_service_history_500') do
-            expect { subject.prepared_params }.to raise_error(
-              Common::Exceptions::BackendServiceException,
-              'BackendServiceException: {:source=>"VAProfile::MilitaryPersonnel::Service", :code=>"VET360_CORE100"}'
-            )
-          end
-        end
-      end
+  describe '.service_history_params' do
+    it 'returns discharge status, branch of service, and service dates' do
+      service_history_episodes = Array.wrap(build(:service_history))
+      expect(described_class.service_history_params(service_history_episodes)).to eq(prepared_service_history_params)
     end
   end
 end
