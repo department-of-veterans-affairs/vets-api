cfb92d750185f6a8426c64080cc4acd0f9f9ddda
OJ Bucao <9256675+ojbucao@users.noreply.github.com>
2025-07-22 09:39:18 -0700

    minor fixes per PR review

--- modules/accredited_representative_portal/app/services/accredited_representative_portal/poa_request_failure_notifier.rb
+++ modules/accredited_representative_portal/app/services/accredited_representative_portal/poa_request_failure_notifier.rb
@@ -7,6 +7,8 @@ module AccreditedRepresentativePortal
     end
 
     def call
+      raise ArgumentError, 'PoaRequest is required' unless @poa_request&.is_a?(PowerOfAttorneyRequest)
+
       recipient_types.each do |recipient_type|
         notification = @poa_request.notifications.create!(
           type: "enqueue_failed_for_#{recipient_type}"
@@ -19,7 +21,7 @@ module AccreditedRepresentativePortal
 
     def recipient_types
       [].tap do |types|
-        types << :claimant if Flipper.enabled?(:ar_poa_request_failure_claimant_notification_email)
+        types << :claimant if Flipper.enabled?(:ar_poa_request_claimant_failure_notification_email)
         types << :representative if Flipper.enabled?(:ar_poa_request_rep_failure_notification)
       end
     end
