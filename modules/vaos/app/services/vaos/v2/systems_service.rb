1826ad465bd85efda50d9c07a7ee3aebc9a44375
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-07-22 21:11:43 -0700

    Removes uneeded conditional from service
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- modules/vaos/app/services/vaos/v2/systems_service.rb
+++ modules/vaos/app/services/vaos/v2/systems_service.rb
@@ -16,8 +16,7 @@ module VAOS
 
       def get_available_slots(location_id:, clinic_id:, clinical_service:, provider_id:, start_dt:, end_dt:)
         with_monitoring do
-          response = if !provider_id.nil? &&
-                        Flipper.enabled?(:va_online_scheduling_use_vpg, user) &&
+          response = if Flipper.enabled?(:va_online_scheduling_use_vpg, user) &&
                         Flipper.enabled?(:va_online_scheduling_enable_OH_slots_search, user)
                        get_slots_vpg(location_id:, clinic_id:, clinical_service:, provider_id:, start_dt:, end_dt:)
                      else
