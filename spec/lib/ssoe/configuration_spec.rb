00d4a9b0646aacaa3031d9c938c001fb378ccb64
Emily Nguyen <emilyn0614@gmail.com>
2025-07-24 15:20:35 -0500

    Do not mock cert and key

--- spec/lib/ssoe/configuration_spec.rb
+++ spec/lib/ssoe/configuration_spec.rb
@@ -8,33 +8,32 @@ require 'ssoe/configuration'
 RSpec.describe SSOe::Configuration do
   subject(:config) { described_class.send(:new) }
 
-  let(:base_url) { 'https://api.example.com/soap' }
-  let(:cert_path) { '/tmp/client.pem' }
-  let(:key_path) { '/tmp/client.key' }
+  let(:base_url) { 'https://int.services.eauth.va.gov:9303/psim_webservice/dev/IdMSSOeWebService' }
+  let(:cert_path) { 'spec/fixtures/certs/vetsgov-localhost.crt' }
+  let(:key_path) { 'spec/fixtures/certs/vetsgov-localhost.key' }
 
-  let(:cert_data) { 'PEM-DATA' }
-  let(:key_data) { 'KEY-DATA' }
-  let(:cert_obj) { instance_double(OpenSSL::X509::Certificate) }
-  let(:key_obj) { instance_double(OpenSSL::PKey::RSA) }
+  let(:cert_obj) { OpenSSL::X509::Certificate.new(File.read(cert_path)) }
+  let(:key_obj) { OpenSSL::PKey::RSA.new(File.read(key_path)) }
 
   let(:faraday_connection) { instance_double(Faraday::Connection) }
 
   before do
-    allow(IdentitySettings.ssoe_get_traits).to receive_messages(url: base_url, client_cert_path: cert_path,
-                                                                client_key_path: key_path)
-
-    allow(OpenSSL::X509::Certificate).to receive(:new).with(cert_data).and_return(cert_obj)
-    allow(OpenSSL::PKey::RSA).to receive(:new).with(key_data).and_return(key_obj)
-
-    allow(File).to receive(:read).with(cert_path).and_return(cert_data)
-    allow(File).to receive(:read).with(key_path).and_return(key_data)
+    allow(IdentitySettings.ssoe_get_traits).to receive_messages(
+      url: base_url,
+      client_cert_path: cert_path,
+      client_key_path: key_path
+    )
   end
 
   describe '#connection' do
     it 'creates a Faraday connection with correct SSL options' do
       expect(Faraday).to receive(:new)
-        .with(base_url, hash_including(ssl: { client_cert: cert_obj, client_key: key_obj }))
-        .and_return(faraday_connection)
+        .with(base_url, hash_including(
+                          ssl: hash_including(
+                            client_cert: a_kind_of(OpenSSL::X509::Certificate),
+                            client_key: a_kind_of(OpenSSL::PKey::RSA)
+                          )
+                        )).and_return(faraday_connection)
 
       connection = config.connection
       expect(connection).to be(faraday_connection)
