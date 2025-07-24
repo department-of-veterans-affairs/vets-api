18658e408f789a1a84015a4ec3531b40b0c3e852
broskj <170366505+brostk@users.noreply.github.com>
2025-07-24 12:07:56 -0400

    initial insights code

--- modules/ivc_champva/app/services/ivc_champva/prod_support_utilities/insights.rb
+++ modules/ivc_champva/app/services/ivc_champva/prod_support_utilities/insights.rb
@@ -10,42 +10,99 @@ module IvcChampva
       # TODO: add docstrings
       def count_batches_by_email_grouped(days_ago = 7, gate = 2, form_number = '10-10D')
         puts "#{form_number} submits over the last #{days_ago} days:"
-        # Get the current date and time
+
+        metrics = gather_submission_metrics(days_ago, gate, form_number)
+
+        puts "\n#{metrics[:unique_individuals]} unique email addresses"
+        puts "#{metrics[:emails_with_multi_submits]} unique email addresses associated with repeat submissions"
+        puts "#{metrics[:percentage]}% of submitters have #{gate} or more submissions\n--"
+
+        display_frequency(metrics[:frequency_data])
+        display_averages_from_data(metrics[:average_time_data])
+
+        nil
+      end
+
+      # Returns the same metrics as count_batches_by_email_grouped but as data instead of printing
+      def gather_submission_metrics(days_ago = 7, gate = 2, form_number = '10-10D')
+        submissions_by_email = get_submissions_by_email(days_ago, form_number)
+        basic_stats = calculate_basic_statistics(submissions_by_email, gate)
+        frequency_data = get_frequency_data(submissions_by_email)
+        average_time_data = get_average_time_data(submissions_by_email, days_ago, form_number)
+
+        {
+          form_number:,
+          days_ago:,
+          gate:,
+          unique_individuals: basic_stats[:unique_individuals],
+          emails_with_multi_submits: basic_stats[:emails_with_multi_submits],
+          percentage: basic_stats[:percentage],
+          frequency_data:,
+          average_time_data:,
+          submissions_by_email:
+        }
+      end
+
+      private
+
+      def get_submissions_by_email(days_ago, form_number)
         end_date = Time.zone.now
         start_date = end_date - days_ago.days
 
-        # Grab count of individual submissions made per unique email (a submission means
-        # a single form submit and all supporting docs associated)
         submissions = IvcChampvaForm
                       .where(created_at: start_date..end_date)
                       .where(form_number:)
                       .select('email, COUNT(DISTINCT form_uuid) AS unique_submission_count')
                       .group(:email)
 
-        # Clean up the result into a hash mapping emails to counts of submissions
-        # e.g.: {'name@mail.com' => 1, 'name2@mail.com' => 5}
-        submissions = submissions.to_h { |r| [r.email, r.unique_submission_count] }
+        submissions.to_h { |r| [r.email, r.unique_submission_count] }
+      end
 
-        # Count keys to give us the total number of unique individuals via their emails
-        unique_individuals = submissions.keys.count.to_f
-        puts "\n#{unique_individuals} unique email addresses"
-
-        # Identify those who submitted more than `gate` times
-        multi_submits = submissions.reject { |_key, value| value < gate }
+      def calculate_basic_statistics(submissions_by_email, gate)
+        unique_individuals = submissions_by_email.keys.count.to_f
+        multi_submits = submissions_by_email.reject { |_key, value| value < gate }
         emails_with_multi_submits = multi_submits.keys.count.to_f
-        puts "#{emails_with_multi_submits} unique email addresses associated with repeat submissions"
-
         percentage = (emails_with_multi_submits / unique_individuals) * 100
-        puts "#{percentage.round(2)}% of submitters have #{gate} or more submissions\n--"
-        # Show breakdown of how many of each volume of submits we get
-        freq = count_frequency(submissions)
-        display_frequency(freq)
-        display_averages(submissions, start_date, end_date, gate, form_number)
+
+        {
+          unique_individuals: unique_individuals.to_i,
+          emails_with_multi_submits: emails_with_multi_submits.to_i,
+          percentage: percentage.round(2)
+        }
+      end
+
+      def get_frequency_data(submissions_by_email)
+        frequency_hash = count_frequency(submissions_by_email)
+        frequency_hash.sort_by(&:first).reverse.to_h
+      end
+
+      def get_average_time_data(submissions_by_email, days_ago, form_number)
+        end_date = Time.zone.now
+        start_date = end_date - days_ago.days
+
+        # Get unique submission counts and calculate averages for each
+        unique_nums = submissions_by_email.values.uniq.select { |num| num > 1 }
+
+        unique_nums.map do |num_submissions|
+          avg_time_seconds = average_time_between_resubmissions(submissions_by_email, start_date, end_date,
+                                                                num_submissions, form_number)
+          avg_time_str = avg_time_seconds.nil? ? nil : make_time_str(avg_time_seconds)
+
+          {
+            num_submissions:,
+            avg_time_seconds:,
+            avg_time_formatted: avg_time_str
+          }
+        end.sort_by { |data| data[:num_submissions] }.reverse
+      end
+
+      def display_averages_from_data(average_time_data)
+        average_time_data.each do |data|
+          puts "Avg time between resubmits for users with #{data[:num_submissions]} submissions: #{data[:avg_time_formatted]}"
+        end
 
         nil
       end
-
-      private
 
       def average_time_between_resubmissions(submissions, start_date, end_date, num_of_resubmits, form_number)
         all_time_differences = []
@@ -94,16 +151,13 @@ module IvcChampva
       end
 
       def count_frequency(submitters)
-        submitters.each.with_object(Hash.new(0)) do |num_submissions, result_hash|
+        submitters.each.with_object(Hash.new(0)) do |(email, num_submissions), result_hash|
           result_hash[num_submissions] += 1
         end
       end
 
       def display_frequency(freq_hash)
-        # Make sure hash is sorted by key:
-        descending = freq_hash.sort_by(&:first).reverse.to_h
-
-        descending.each do |num_submissions, quantity|
+        freq_hash.each do |num_submissions, quantity|
           puts "Number of users with #{num_submissions} submissions: #{quantity}" unless num_submissions == 1
         end
 
