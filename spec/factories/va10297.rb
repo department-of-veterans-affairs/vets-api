cf822c56b93225946d004da05831a088d5edef93
nfstern02 <72567812+nfstern02@users.noreply.github.com>
2025-07-28 18:20:43 +0000

    Vebt 2056 - add rspec tests to cover all live forms to provide more complete coverage in case of changes such as the evss initiative. (#23218)
    * add rspecs to test for every live form with create_daily_spool_files
    
    * fix rspec errors
    
    * pull in latest vets-json-schema and update fixture
    
    * latest fixes to get rspec passing for 10297
    
    * update brakeman
    
    * copilot recommended changes

--- spec/factories/va10297.rb
+++ spec/factories/va10297.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
     factory :va10297_simple_form do
       form { Rails.root.join('spec', 'fixtures', 'education_benefits_claims', '10297', 'simple.json').read }
     end
+
+    factory :va10297_full_form, class: 'SavedClaim::EducationBenefits::VA10297', parent: :education_benefits do
+      form { Rails.root.join('spec', 'fixtures', 'education_benefits_claims', '10297', 'kitchen_sink.json').read }
+    end
   end
 end
