8f62befa5631537ea46059412748d011588a6ba1
Jane Cho <87551323+janechodance@users.noreply.github.com>
2025-07-28 10:46:10 -0400

    fix lint

--- spec/controllers/v0/chatbot/claim_status_controller_spec.rb
+++ spec/controllers/v0/chatbot/claim_status_controller_spec.rb
@@ -222,7 +222,7 @@ RSpec.describe 'V0::Chatbot::ClaimStatusController', type: :request do
           parsed_body = JSON.parse(response.body)
           expect(parsed_body.dig('data', 'data', 'attributes', 'trackedItems').size).to eq(13)
           expect(parsed_body.dig('data', 'data', 'attributes', 'trackedItems', 0,
-                                  'displayName')).to eq('PMR Pending')
+                                 'displayName')).to eq('PMR Pending')
           expect(parsed_body.dig('data', 'data', 'attributes', 'trackedItems', 1,
                                  'displayName')).to eq('Submit buddy statement(s)')
         end
