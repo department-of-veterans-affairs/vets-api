8cef5a1909fb68d14eb63480a32e9920166baa11
Emily Nguyen <emilyn0614@gmail.com>
2025-07-31 12:03:24 -0500

    Create parsing logic for get traits service

--- lib/ssoe/service.rb
+++ lib/ssoe/service.rb
@@ -39,7 +39,7 @@ module SSOe
                                    ssn, email, phone, street1, city, state, zipcode),
           soapaction: nil
         )
-        raw_response.body
+        parse_response(raw_response.body)
       end
     rescue *CONNECTION_ERRORS => e
       Rails.logger.error("[SSOe::Service::get_traits] Connection error: #{e.class} - #{e.message}")
@@ -69,5 +69,47 @@ module SSOe
       ).perform
     end
     # rubocop:enable Metrics/ParameterLists
+
+    def parse_response(response_body)
+      doc = Nokogiri::XML(response_body)
+      doc.remove_namespaces!
+
+      icn = extract_icn(doc)
+      return { success: true, icn: } if icn.present?
+
+      fault = extract_fault(doc)
+      return fault if fault
+
+      unknown_error
+    end
+
+    def extract_icn(doc)
+      doc.at_xpath('//getSSOeTraitsByCSPIDResponse/icn')&.text
+    end
+
+    def extract_fault(doc)
+      fault = doc.at_xpath('//Fault')
+      return unless fault
+
+      fault_code = fault.at_xpath('faultcode')&.text
+      fault_string = fault.at_xpath('faultstring')&.text
+      {
+        success: false,
+        error: {
+          code: fault_code,
+          message: fault_string
+        }
+      }
+    end
+
+    def unknown_error
+      {
+        success: false,
+        error: {
+          code: 'UnknownError',
+          message: 'Unable to parse SOAP response'
+        }
+      }
+    end
   end
 end
--- spec/lib/ssoe/service_spec.rb
+++ spec/lib/ssoe/service_spec.rb
@@ -27,17 +27,62 @@ RSpec.describe SSOe::Service, type: :service do
       }
     end
 
-    context 'when the response is successful' do
-      let(:raw_response) { double('raw_response', body: '<xml><icn>123456789</icn></xml>') }
+    shared_examples 'a parsed response' do |xml:, expected:|
+      let(:raw_response) { double('raw_response', body: xml) }
 
-      before do
-        allow(service).to receive(:perform).and_return(raw_response)
-      end
+      before { allow(service).to receive(:perform).and_return(raw_response) }
 
-      it 'returns the raw response body' do
+      it 'parses and returns the expected result' do
         response = service.get_traits(**valid_params)
-        expect(response).to eq('<xml><icn>123456789</icn></xml>')
+        expect(response).to eq(expected)
       end
+    end
+
+    context 'when the response contains a valid ICN' do
+      it_behaves_like 'a parsed response',
+                      xml: <<~XML,
+                        <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+                          <soap:Body>
+                            <getSSOeTraitsByCSPIDResponse>
+                              <icn>123498767V234859</icn>
+                            </getSSOeTraitsByCSPIDResponse>
+                          </soap:Body>
+                        </soap:Envelope>
+                      XML
+                      expected: { success: true, icn: '123498767V234859' }
+    end
+
+    context 'when the response contains a fault' do
+      it_behaves_like 'a parsed response',
+                      xml: <<~XML,
+                        <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+                          <soap:Body>
+                            <soap:Fault>
+                              <faultcode>soap:Client</faultcode>
+                              <faultstring>Invalid CSPID</faultstring>
+                            </soap:Fault>
+                          </soap:Body>
+                        </soap:Envelope>
+                      XML
+                      expected: {
+                        success: false,
+                        error: {
+                          code: 'soap:Client',
+                          message: 'Invalid CSPID'
+                        }
+                      }
+    end
+
+    context 'when the response is unexpected' do
+      it_behaves_like 'a parsed response',
+                      xml: '<unexpected>response</unexpected>',
+                      expected: {
+                        success: false,
+                        error: {
+                          code: 'UnknownError',
+                          message: 'Unable to parse SOAP response'
+                        }
+                      }
     end
 
     context 'when there is a connection error' do
@@ -74,6 +119,39 @@ RSpec.describe SSOe::Service, type: :service do
         response = service.get_traits(**valid_params)
         expect(response).to be_nil
       end
+    end
+  end
+
+  describe '#parse_response' do
+    it 'parses ICN response' do
+      body = <<~XML
+        <Envelope>
+          <Body>
+            <getSSOeTraitsByCSPIDResponse>
+              <icn>123498767V234859</icn>
+            </getSSOeTraitsByCSPIDResponse>
+          </Body>
+        </Envelope>
+      XML
+
+      result = service.send(:parse_response, body)
+      expect(result).to eq({ success: true, icn: '123498767V234859' })
+    end
+
+    it 'parses fault response' do
+      body = <<~XML
+        <Envelope>
+          <Body>
+            <Fault>
+              <faultcode>soap:Client</faultcode>
+              <faultstring>Error</faultstring>
+            </Fault>
+          </Body>
+        </Envelope>
+      XML
+
+      result = service.send(:parse_response, body)
+      expect(result).to eq({ success: false, error: { code: 'soap:Client', message: 'Error' } })
     end
   end
 end
