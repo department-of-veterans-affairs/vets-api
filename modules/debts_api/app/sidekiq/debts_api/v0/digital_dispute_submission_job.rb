ba63f22ce83acfb16c21b98f92644dc17d7d2dd3
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-07-31 18:01:06 -0400

    Idea 1

---
+++ modules/debts_api/app/sidekiq/debts_api/v0/digital_dispute_submission_job.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'debts_api/v0/financial_status_report_service'
+
+module DebtsApi
+  class V0::DigitalDisputeJob
+    def perform(submission_id, user_uuid)
+  end
+end
