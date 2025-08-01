689174786f70edc163de6e538dcec6c48bab51f3
Yang Yang <yangyang@navapbc.com>
2025-08-01 11:40:05 -0400

    Appease rubocop

--- lib/pdf_fill/filler.rb
+++ lib/pdf_fill/filler.rb
@@ -177,9 +177,7 @@ module PdfFill
         template_path, file_path, new_hash, flatten: Rails.env.production?
       )
 
-      if should_stamp_form?(form_id, fill_options, submit_date)
-        file_path = stamp_form(file_path, submit_date)
-      end
+      file_path = stamp_form(file_path, submit_date) if should_stamp_form?(form_id, fill_options, submit_date)
       output = combine_extras(file_path, hash_converter.extras_generator)
       Rails.logger.info('PdfFill done', fill_options.merge(form_id:, file_name_extension:, extras: output != file_path))
       output
