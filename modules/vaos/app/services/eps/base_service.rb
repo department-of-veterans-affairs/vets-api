7b77e3b2dea0c9390e3af18e99e2629c7ca19055
PhilipDeFraties <philip.defraties@gmail.com>
2025-08-04 17:21:36 -0600

    update to include const in logger message

--- modules/vaos/app/services/eps/base_service.rb
+++ modules/vaos/app/services/eps/base_service.rb
@@ -48,7 +48,7 @@ module Eps
 
       # Log the error without PII - only include safe context information
       method_name ||= caller_locations(1, 1)[0].label
-      Rails.logger.warn('EPS appointment error', {
+      Rails.logger.warn("#{CC_APPOINTMENTS}: EPS appointment error", {
                           error_type: error_value,
                           method: method_name,
                           status: response.status || 'unknown'
