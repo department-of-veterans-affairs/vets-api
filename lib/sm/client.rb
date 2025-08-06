4e191bdb9e61d20dc6f30313bba674d591f75844
Ryan McNeil <ryan.mcneil@oddball.io>
2025-08-06 11:26:42 -0600

    Filter teams where blocked_status == true

--- lib/sm/client.rb
+++ lib/sm/client.rb
@@ -541,13 +541,15 @@ module SM
     #
     def get_all_triage_teams(user_uuid, use_cache)
       cache_key = "#{user_uuid}-all-triage-teams"
-      get_cached_or_fetch_data(use_cache, cache_key, AllTriageTeams) do
+      data = get_cached_or_fetch_data(use_cache, cache_key, AllTriageTeams) do
         path = append_requires_oh_messages_query('alltriageteams', 'requiresOHTriageGroup')
         json = perform(:get, path, nil, token_headers).body
         data = Vets::Collection.new(json[:data], AllTriageTeams, metadata: json[:metadata], errors: json[:errors])
         AllTriageTeams.set_cached(cache_key, data.records)
         data
       end
+      data.records = data.records.filter { |team| !team.blocked_status }
+      data
     end
     # @!endgroup
 
