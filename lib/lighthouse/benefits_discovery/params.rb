18d2ab7bbbf348676595db50e4ea81ac0baefa28
Kris Pethtel <kris.pethtel@gmail.com>
2025-08-05 21:10:49 -0700

    1788: refactor benefits discovery to reflect their current api schema

--- lib/lighthouse/benefits_discovery/params.rb
+++ lib/lighthouse/benefits_discovery/params.rb
@@ -9,10 +9,8 @@ module BenefitsDiscovery
     def prepared_params
       {
         dateOfBirth: @user.birth_date,
-        dischargeStatus: discharge_status,
-        branchOfService: service_history.map { |sh| sh.branch_of_service&.upcase },
         disabilityRating: disability_rating,
-        serviceDates: service_history.map { |sh| { beginDate: sh.begin_date, endDate: sh.end_date } }
+        serviceHistory: service_history_episodes,
       }.compact_blank
     end
 
@@ -25,6 +23,23 @@ module BenefitsDiscovery
     end
 
     private
+
+    # maybe change name with service_history
+    def service_history_episodes
+      service_history.map do |sh|
+        code = sh.character_of_discharge_code
+        discharge_type = VAProfile::Prefill::MilitaryInformation::DISCHARGE_TYPES[code]
+        if discharge_type.nil?
+          Rails.logger.error("No matching discharge code for: #{discharge_type}")
+        end
+        {
+          startDate: sh.begin_date,
+          endDate: sh.end_date,
+          dischargeStatus: discharge_type,
+          branchOfService: sh.branch_of_service&.upcase
+        }
+      end
+    end
 
     def disability_rating
       service = VeteranVerification::Service.new
@@ -76,6 +91,19 @@ module BenefitsDiscovery
             "#{discharge_type.upcase.gsub('-', '_')}_DISCHARGE"
           end
         end.compact
+      end
+
+      def service_history_episodes(episodes)
+        episodes.map do |e|
+          code = e.character_of_discharge_code
+          discharge_type = VAProfile::Prefill::MilitaryInformation::DISCHARGE_TYPES[code]
+          {
+            startDate: e.begin_date,
+            endDate: e.end_date,
+            dischargeStatus: discharge_type,
+            branchOfService: e.branch_of_service&.upcase
+          }
+        end
       end
     end
   end
