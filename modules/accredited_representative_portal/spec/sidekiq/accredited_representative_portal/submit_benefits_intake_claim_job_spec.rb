bcef0b72799395594e66a9dd0b10686fe96c9466
Patrick Brown <patrick.brown@oddball.io>
2025-08-06 15:11:10 -0400

    arp/received_and_confirmation_emails_for_form_upload - fixed test and rubocop

--- modules/accredited_representative_portal/spec/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job_spec.rb
+++ modules/accredited_representative_portal/spec/sidekiq/accredited_representative_portal/submit_benefits_intake_claim_job_spec.rb
@@ -28,27 +28,37 @@ RSpec.describe AccreditedRepresentativePortal::SubmitBenefitsIntakeClaimJob, :up
       allow(lighthouse_service).to receive_messages(uuid:, location:, upload_doc: response)
       allow(intake_service).to receive(:valid_document?).and_return(pdf_path)
       allow(monitor).to receive(:track_submission_success)
-      allow(job).to receive_messages(init: nil, process_record: pdf_path, create_form_submission_attempt: nil,
-                                     send_confirmation_email: nil, cleanup_file_paths: nil)
+
+      allow(job).to receive_messages(
+        init: nil,
+        process_record: pdf_path,
+        create_form_submission_attempt: nil,
+        send_confirmation_email: nil,
+        cleanup_file_paths: nil
+      )
     end
 
     it 'performs' do
       expect(job).to receive(:process_record).with(claim)
       expect(job).to receive(:create_form_submission_attempt)
+
+      expected_metadata = {
+        veteranFirstName: 'John',
+        veteranLastName: 'Doe',
+        fileNumber: '123456789',
+        zipCode: '12345',
+        source: 'AccreditedRepresentativePortal::SavedClaim::BenefitsIntake::DependencyClaim va.gov',
+        docType: '21-686c',
+        businessLine: 'CMP'
+      }.to_json
+
       expect(lighthouse_service).to receive(:upload_doc).with(
         upload_url: location,
         file: { file: pdf_path, file_name: 'pdf' },
-        metadata: '{
-            "veteranFirstName":"John",
-            "veteranLastName":"Doe",
-            "fileNumber":"123456789"
-            "zipCode":"12345",
-            "source":"AccreditedRepresentativePortal::SavedClaim::BenefitsIntake::DependencyClaim va.gov",
-            "docType":"21-686c",
-            "businessLine":"CMP"
-          }',
+        metadata: expected_metadata,
         attachments: []
       )
+
       expect(job).to receive(:send_confirmation_email)
       expect(job).to receive(:cleanup_file_paths)
 
@@ -60,33 +70,29 @@ RSpec.describe AccreditedRepresentativePortal::SubmitBenefitsIntakeClaimJob, :up
       around { |example| Timecop.freeze { example.run } }
 
       let(:stamper) { double }
-      let(:va_form_attachment) { create(:persistent_attachment_va_form_documentation, form_id: '21-686c') }
+      let(:va_form_attachment) do
+        create(:persistent_attachment_va_form_documentation, form_id: '21-686c')
+      end
 
       before do
-        # Override the process_record method to actually call stamp_pdf
         allow(job).to receive(:process_record).and_call_original
         allow(job).to receive(:stamp_pdf).and_call_original
-
-        # Set up the claim to have attachments that will trigger stamping
         allow(claim).to receive(:persistent_attachments).and_return([va_form_attachment])
-
-        # Mock the valid_document? method on lighthouse_service as well
         allow(lighthouse_service).to receive(:valid_document?).and_return(pdf_path)
       end
 
       it 'stamps the footer of the additional docs' do
         timestamp = DateTime.now.utc.strftime('%H:%M:%S  %Y-%m-%d %I:%M %p')
 
-        # mock stamping of provided VA form
         allow(SimpleFormsApi::PdfStamper).to receive(:new).and_return(stamper)
         allow(stamper).to receive(:stamp_pdf)
 
-        # Mock the to_pdf method to return a path
         allow(va_form_attachment).to receive(:to_pdf).and_return('/tmp/test.pdf')
 
-        # Mock PDFUtilities::DatestampPdf instead of expecting the real class
         datestamp_pdf_double = double('datestamp_pdf')
-        allow(PDFUtilities::DatestampPdf).to receive(:new).with('/tmp/test.pdf').and_return(datestamp_pdf_double)
+        allow(PDFUtilities::DatestampPdf).to receive(:new)
+          .with('/tmp/test.pdf').and_return(datestamp_pdf_double)
+
         expect(datestamp_pdf_double).to receive(:run).with(
           text: "Submitted via VA.gov at #{timestamp} UTC. Signed in and submitted with an identity-verified account.",
           text_only: true,
@@ -114,11 +120,9 @@ RSpec.describe AccreditedRepresentativePortal::SubmitBenefitsIntakeClaimJob, :up
     context 'when email sends successfully' do
       it 'sends a confirmation email' do
         allow(AccreditedRepresentativePortal::NotificationEmail)
-          .to receive(:new)
-          .with(claim.id)
-          .and_return(notification)
-        expect(notification).to receive(:deliver).with(:confirmation)
+          .to receive(:new).with(claim.id).and_return(notification)
 
+        expect(notification).to receive(:deliver).with(:confirmation)
         job.send(:send_confirmation_email)
       end
     end
@@ -128,13 +132,18 @@ RSpec.describe AccreditedRepresentativePortal::SubmitBenefitsIntakeClaimJob, :up
 
       it 'logs the error but does not reraise' do
         allow(AccreditedRepresentativePortal::NotificationEmail)
-          .to receive(:new)
-          .with(claim.id)
-          .and_return(notification)
-        allow(notification).to receive(:deliver).with(:confirmation).and_raise(email_error)
-        expect(monitor)
-          .to receive(:track_send_email_failure)
-          .with(claim, intake_service, user_account_uuid, 'confirmation', email_error)
+          .to receive(:new).with(claim.id).and_return(notification)
+
+        allow(notification).to receive(:deliver)
+          .with(:confirmation).and_raise(email_error)
+
+        expect(monitor).to receive(:track_send_email_failure).with(
+          claim,
+          intake_service,
+          user_account_uuid,
+          'confirmation',
+          email_error
+        )
 
         expect { job.send(:send_confirmation_email) }.not_to raise_error
       end
