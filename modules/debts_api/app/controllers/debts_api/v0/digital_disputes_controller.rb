12d603018345ff18da2f316f49466988f0d2571e
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-08-06 19:04:02 -0400

    97118 Financial Management Digital Dispute Sidekiq Job

--- modules/debts_api/app/controllers/debts_api/v0/digital_disputes_controller.rb
+++ modules/debts_api/app/controllers/debts_api/v0/digital_disputes_controller.rb
@@ -8,23 +8,49 @@ module DebtsApi
       service_tag 'debt-resolution'
 
       def create
-        StatsD.increment("#{V0::DigitalDispute::STATS_KEY}.initiated")
+        StatsD.increment("#{V0::DigitalDisputeSubmission::STATS_KEY}.initiated")
 
-        result = process_submission
-
-        if result[:success]
-          StatsD.increment("#{V0::DigitalDispute::STATS_KEY}.success")
-          render json: {
-            message: result[:message],
-            submission_id: result[:submission_id]
-          }, status: :ok
+        if Flipper.enabled?(:financial_management_digital_dispute_async)
+          execute_async_job
+          render json: { message: 'Digital dispute submission received successfully' }, status: :ok
         else
-          StatsD.increment("#{V0::DigitalDispute::STATS_KEY}.failure")
-          render json: { errors: result[:errors] }, status: :unprocessable_entity
+          result = process_submission
+
+          if result[:success]
+            StatsD.increment("#{V0::DigitalDisputeSubmission::STATS_KEY}.success")
+            render json: {
+              message: result[:message],
+              submission_id: result[:submission_id]
+            }, status: :ok
+          else
+            StatsD.increment("#{V0::DigitalDisputeSubmission::STATS_KEY}.failure")
+            render json: { errors: result[:errors] }, status: :unprocessable_entity
+          end
         end
       end
 
       private
+
+      def execute_async_job
+        user_params = {
+            'uuid' => current_user.uuid,
+            'ssn' => current_user.ssn,
+            'participant_id' => current_user.participant_id
+          }
+
+          metadata = parse_metadata(submission_params[:metadata])
+
+          # base64 encoding so job can handle files
+          base_64_files = submission_params[:files].map do |file|
+            {
+              'fileName' => file.original_filename,
+              'fileContents' => Base64.strict_encode64(file.read)
+            }
+        end
+          DebtsApi::V0::DigitalDisputeJob.perform_async(
+            user_params, metadata, base_64_files
+          )
+      end
 
       def process_submission
         metadata = parse_metadata(submission_params[:metadata])
---
+++ modules/debts_api/app/sidekiq/debts_api/v0/digital_dispute_job.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'debts_api/v0/financial_status_report_service'
+
+module DebtsApi
+  class V0::DigitalDisputeJob
+    def perform(user_data, files, metadata)
+      user = OpenStruct.new(uuid: user_data['uuid'], ssn: user_data['ssn'], participant_id: user_data['participant_id'])
+
+      decoded_files = files.map do |file|
+        decoded_file(file)
+      end
+
+      service = DigitalDisputeSubmissionService.new(user, decoded_files, metadata)
+      service.call
+    end
+
+    private
+
+    def decoded_file(file_hash)
+      tempfile = Tempfile.new(['upload', '.pdf'], binmode: true)
+      tempfile.write(Base64.decode64(file_hash['fileContents']))
+      tempfile.rewind
+
+      ActionDispatch::Http::UploadedFile.new(
+        filename: file_hash['fileName'],
+        type: 'application/pdf',
+        tempfile: tempfile,
+        head: "Content-Disposition: form-data; name=\"file\"; filename=\"#{file_hash['fileName']}\"\r\nContent-Type: application/pdf\r\n"
+      )
+    end
+  end
+end
