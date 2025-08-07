5ab59154aced747286ca188a89fbd1f36e7e1efa
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-08-07 08:26:19 -0500

    business rule 4:serviceInformation:a:separationLocationCode (#23442)

--- modules/claims_api/app/controllers/concerns/claims_api/revised_disability_compensation_validations.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/revised_disability_compensation_validations.rb
@@ -12,6 +12,15 @@ module ClaimsApi
     def validate_form_526_submission_values!
       # ensure 'claimDate', if provided, is a valid date not in the future
       validate_form_526_submission_claim_date!
+      # ensure any provided 'separationLocationCode' values are valid EVSS ReferenceData values
+      validate_form_526_location_codes!
+    end
+
+    def retrieve_separation_locations
+      ClaimsApi::BRD.new.intake_sites
+    rescue
+      exception_msg = 'Failed To Obtain Intake Sites (Request Failed)'
+      raise ::Common::Exceptions::ServiceUnavailable.new({ source: 'intake_sites', detail: exception_msg })
     end
 
     def validate_form_526_submission_claim_date!
@@ -20,6 +29,25 @@ module ClaimsApi
 
       exception_msg = 'The request failed validation, because the claim date was in the future.'
       raise ::Common::Exceptions::InvalidFieldValue.new('claimDate', exception_msg)
+    end
+
+    def validate_form_526_location_codes!
+      # only retrieve separation locations if we'll need them
+      need_locations = form_attributes['serviceInformation']['servicePeriods'].detect do |service_period|
+        Date.parse(service_period['activeDutyEndDate']) > Time.zone.today
+      end
+      separation_locations = retrieve_separation_locations if need_locations
+
+      form_attributes['serviceInformation']['servicePeriods'].each do |service_period|
+        next if Date.parse(service_period['activeDutyEndDate']) <= Time.zone.today
+        next if separation_locations.any? do |location|
+                  location[:id]&.to_s == service_period['separationLocationCode']
+                end
+
+        exception_msg = "Provided separation location code is not valid: #{service_period['separationLocationCode']}"
+        raise ::Common::Exceptions::InvalidFieldValue.new('Invalid separation location code',
+                                                          exception_msg)
+      end
     end
   end
 end
