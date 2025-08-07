d9beb16df58ac0262b5d5cfb1b61f7afa413eee4
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-08-07 08:26:19 -0500

    business rule 4:serviceInformation:a:separationLocationCode (#23442)

--- modules/claims_api/spec/requests/v1/forms/526_revised_validations_spec.rb
+++ modules/claims_api/spec/requests/v1/forms/526_revised_validations_spec.rb
@@ -13,8 +13,6 @@ RSpec.describe ClaimsApi::RevisedDisabilityCompensationValidations do
   let(:test_class) do
     Class.new do
       include ClaimsApi::RevisedDisabilityCompensationValidations
-
-      # Mock any methods the module might expect to be present
       attr_accessor :form_attributes
 
       def initialize(attributes = {})
@@ -45,6 +43,108 @@ RSpec.describe ClaimsApi::RevisedDisabilityCompensationValidations do
       it 'raises an InvalidFieldValue error' do
         expect { subject.validate_form_526_submission_claim_date! }
           .to raise_error(Common::Exceptions::InvalidFieldValue)
+      end
+    end
+  end
+
+  describe '#validate_form_526_location_codes!' do
+    context 'when service periods are in the past' do
+      let(:form_attributes) do
+        {
+          'serviceInformation' => {
+            'servicePeriods' => [
+              { 'activeDutyEndDate' => 1.year.ago.to_date.iso8601, 'separationLocationCode' => '123' }
+            ]
+          }
+        }
+      end
+
+      it 'does not raise an error' do
+        expect { subject.validate_form_526_location_codes! }.not_to raise_error
+      end
+    end
+
+    context 'when service periods are in the future' do
+      let(:brd_client) { instance_double(ClaimsApi::BRD) }
+      let(:separation_locations) { [{ id: '123' }, { id: '456' }] }
+      let(:form_attributes) do
+        {
+          'serviceInformation' => {
+            'servicePeriods' => [
+              { 'activeDutyEndDate' => 1.year.from_now.to_date.iso8601, 'separationLocationCode' => '123' }
+            ]
+          }
+        }
+      end
+
+      before do
+        allow(ClaimsApi::BRD).to receive(:new).and_return(brd_client)
+        allow(brd_client).to receive(:intake_sites).and_return(separation_locations)
+      end
+
+      context 'with a valid separation location code' do
+        it 'does not raise an error' do
+          expect { subject.validate_form_526_location_codes! }.not_to raise_error
+        end
+      end
+
+      context 'with an invalid separation location code' do
+        let(:form_attributes) do
+          {
+            'serviceInformation' => {
+              'servicePeriods' => [
+                { 'activeDutyEndDate' => 1.year.from_now.to_date.iso8601, 'separationLocationCode' => '999' }
+              ]
+            }
+          }
+        end
+
+        it 'raises an InvalidFieldValue error' do
+          expect { subject.validate_form_526_location_codes! }
+            .to raise_error(Common::Exceptions::InvalidFieldValue)
+        end
+      end
+
+      context 'with multiple service periods' do
+        let(:form_attributes) do
+          {
+            'serviceInformation' => {
+              'servicePeriods' => [
+                { 'activeDutyEndDate' => 1.year.ago.to_date.iso8601, 'separationLocationCode' => '999' },
+                { 'activeDutyEndDate' => 1.year.from_now.to_date.iso8601, 'separationLocationCode' => '123' }
+              ]
+            }
+          }
+        end
+
+        it 'only validates future service periods' do
+          expect { subject.validate_form_526_location_codes! }.not_to raise_error
+        end
+      end
+    end
+
+    context 'when BRD client raises an error' do
+      let(:brd_client) { instance_double(ClaimsApi::BRD) }
+      let(:form_attributes) do
+        {
+          'serviceInformation' => {
+            'servicePeriods' => [
+              { 'activeDutyEndDate' => 1.year.from_now.to_date.iso8601, 'separationLocationCode' => '123' }
+            ]
+          }
+        }
+      end
+
+      before do
+        allow(ClaimsApi::BRD).to receive(:new).and_return(brd_client)
+        allow(brd_client).to receive(:intake_sites).and_raise(
+          StandardError.new('Failed to retrieve intake sites')
+        )
+      end
+
+      it 'propagates a ServiceUnavailable error' do
+        expect { subject.validate_form_526_location_codes! }
+          .to raise_error(Common::Exceptions::ServiceUnavailable)
       end
     end
   end
