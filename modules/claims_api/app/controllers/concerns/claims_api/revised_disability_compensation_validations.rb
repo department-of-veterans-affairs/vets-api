be8eb70ce947beb7b39873d611d4e4b6ab8f4016
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-08-07 10:34:12 -0500

    added 180day end date check + tests

--- modules/claims_api/app/controllers/concerns/claims_api/revised_disability_compensation_validations.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/revised_disability_compensation_validations.rb
@@ -82,5 +82,24 @@ module ClaimsApi
         end
       end
     end
+
+    def validate_form_526_no_active_duty_end_date_more_than_180_days_in_future!
+      service_periods = form_attributes.dig('serviceInformation', 'servicePeriods')
+
+      end_date_180_days_in_future = service_periods.find do |sp|
+        active_duty_end_date = sp['activeDutyEndDate']
+        next if active_duty_end_date.blank?
+
+        Date.parse(active_duty_end_date) > 180.days.from_now.end_of_day
+      end
+
+      unless end_date_180_days_in_future.nil?
+        raise ::Common::Exceptions::InvalidFieldValue.new(
+          'serviceInformation/servicePeriods/activeDutyEndDate',
+          "Provided service period duty end date is more than 180 days in the future: \
+          #{end_date_180_days_in_future['activeDutyEndDate']}"
+        )
+      end
+    end
   end
 end
