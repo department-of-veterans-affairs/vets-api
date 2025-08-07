2d4d8105b7ebc5304b9d0d5ab41434fe9e716a2a
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-08-07 10:28:20 -0500

    added service period chronology validation + tests

--- modules/claims_api/app/controllers/concerns/claims_api/revised_disability_compensation_validations.rb
+++ modules/claims_api/app/controllers/concerns/claims_api/revised_disability_compensation_validations.rb
@@ -66,5 +66,21 @@ module ClaimsApi
                                                           must be between 1 and 100 inclusive")
       end
     end
+
+    def validate_service_periods_chronology!
+      form_attributes.dig('serviceInformation', 'servicePeriods')
+      form_attributes['serviceInformation']['servicePeriods'].each do |service_period|
+        begin_date = service_period['activeDutyBeginDate']
+        end_date = service_period['activeDutyEndDate']
+        next if end_date.blank?
+
+        if Date.parse(end_date) < Date.parse(begin_date)
+          raise ::Common::Exceptions::InvalidFieldValue.new('serviceInformation.servicePeriods',
+                                                            "Invalid service period duty dates - \
+                                                            Provided service period duty dates are \
+                                                            out of order: begin=#{begin_date} end=#{end_date}")
+        end
+      end
+    end
   end
 end
