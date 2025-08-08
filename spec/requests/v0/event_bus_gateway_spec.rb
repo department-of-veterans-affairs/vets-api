585f5348cc6c5288b73e1078799b8e0b9723564d
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2025-08-08 09:38:47 -0600

    Fix linting error

--- spec/requests/v0/event_bus_gateway_spec.rb
+++ spec/requests/v0/event_bus_gateway_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'V0::EventBusGateway', type: :request do
     context 'with the authentication header included' do
       it 'invokes the email-sending job' do
         expect(EventBusGateway::LetterReadyEmailJob).to receive(:perform_async).with('1234', '5678', 'EP110')
-        post '/v0/event_bus_gateway/send_email', params: params, headers: service_account_auth_header
+        post '/v0/event_bus_gateway/send_email', params:, headers: service_account_auth_header
         expect(response).to have_http_status(:ok)
       end
     end
