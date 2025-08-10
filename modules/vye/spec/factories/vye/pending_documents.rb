1fd55dd051134f9120783ff89a299cc252138182
nfstern02 <72567812+nfstern02@users.noreply.github.com>
2025-08-10 07:24:33 -0500

    remove vye tests, add temp cleanup task

--- modules/vye/spec/factories/vye/pending_documents.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :vye_pending_document, class: 'Vye::PendingDocument' do
-    doc_type { Faker::Lorem.word }
-    queue_date { Faker::Date.between(from: 10.days.ago, to: 20.days.ago) }
-    rpo { Faker::Lorem.word }
-  end
-end
