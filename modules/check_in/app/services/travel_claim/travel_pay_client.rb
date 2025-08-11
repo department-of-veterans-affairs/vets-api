c12bf81618feed901a92db87da597791b775e05a
PhilipDeFraties <philip.defraties@gmail.com>
2025-08-11 16:19:51 -0600

    add new client for use with new travel pay api for travel claims submission

---
+++ modules/check_in/app/services/travel_claim/travel_pay_client.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+module TravelClaim
+  # Client for Travel Pay specific auth needs
+  class TravelPayClient
+    extend Forwardable
+    include SentryLogging
+
+    GRANT_TYPE = 'client_credentials'
+
+    attr_reader :settings, :check_in
+
+    def_delegators :settings,
+                   :auth_url,
+                   :tenant_id,
+                   :travel_pay_client_id,
+                   :travel_pay_client_secret,
+                   :scope,
+                   :service_name,
+                   :subscription_key,
+                   :e_subscription_key,
+                   :s_subscription_key
+
+    def self.build(opts = {})
+      new(opts)
+    end
+
+    def initialize(opts)
+      @settings = Settings.check_in.travel_reimbursement_api_v2
+      @check_in = opts[:check_in]
+    end
+
+    # Obtain a VEIS OAuth token using Travel Pay client credentials
+    #
+    # @return [Faraday::Response]
+    def token
+      connection(server_url: auth_url).post("/#{tenant_id}/oauth2/v2.0/token") do |req|
+        req.headers = default_headers
+        req.body = URI.encode_www_form(auth_params)
+      end
+    rescue => e
+      log_message_to_sentry(e.original_body, :error,
+                            { uuid: check_in&.uuid },
+                            { external_service: service_name, team: 'check-in' })
+      raise e
+    end
+
+    # Exchange a VEIS access token for a BTSSS system access token (Travel Pay API v4)
+    #
+    # @param veis_access_token [String] The OAuth access token obtained from VEIS
+    # @return [Faraday::Response]
+    def system_access_token(veis_access_token:)
+      connection(server_url: travel_pay_base_url).post('/api/v4/auth/system-access-token') do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['X-Correlation-ID'] = SecureRandom.uuid
+        req.headers['OCP-APIM-Subscription-Key'] = subscription_key
+        req.headers['OCP-APIM-Subscription-Key-E'] = e_subscription_key
+        req.headers['OCP-APIM-Subscription-Key-S'] = s_subscription_key
+        req.headers['Authorization'] = "Bearer #{veis_access_token}"
+        req.body = { secret: travel_pay_client_secret }.to_json
+      end
+    rescue => e
+      log_message_to_sentry(e.original_body, :error,
+                            { uuid: check_in&.uuid },
+                            { external_service: service_name, team: 'check-in' })
+      raise e
+    end
+
+    private
+
+    def connection(server_url:)
+      Faraday.new(url: server_url) do |conn|
+        conn.use(:breakers, service_name:)
+        conn.response :raise_custom_error, error_prefix: service_name
+        conn.response :betamocks if mock_enabled?
+        conn.adapter Faraday.default_adapter
+      end
+    end
+
+    def default_headers
+      { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    end
+
+    def auth_params
+      {
+        client_id: travel_pay_client_id,
+        client_secret: travel_pay_client_secret,
+        scope:,
+        grant_type: GRANT_TYPE
+      }
+    end
+
+    def travel_pay_base_url
+      settings.claims_url_v2
+    end
+
+    def mock_enabled?
+      settings.mock || Flipper.enabled?('check_in_experience_mock_enabled') || false
+    end
+  end
+end
---
+++ modules/check_in/spec/services/travel_claim/travel_pay_client_spec.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe TravelClaim::TravelPayClient do
+  subject { described_class.build(check_in:) }
+
+  let(:uuid) { 'd602d9eb-9a31-484f-9637-13ab0b507e0d' }
+  let(:check_in) { CheckIn::V2::Session.build(data: { uuid: }) }
+
+  describe '.build' do
+    it 'returns an instance of described_class' do
+      expect(subject).to be_an_instance_of(described_class)
+    end
+  end
+
+  describe '#token' do
+    context 'when veis auth returns success' do
+      let(:token_response) do
+        {
+          token_type: 'Bearer',
+          expires_in: 3599,
+          access_token: 'tp-token'
+        }
+      end
+      let(:veis_token_response) { Faraday::Response.new(body: token_response, status: 200) }
+
+      it 'posts to the VEIS path and yields the request' do
+        expect_any_instance_of(Faraday::Connection).to receive(:post)
+          .with("/#{Settings.check_in.travel_reimbursement_api_v2.tenant_id}/oauth2/v2.0/token")
+          .and_yield(Faraday::Request.new)
+          .and_return(veis_token_response)
+
+        expect(subject.token).to eq(veis_token_response)
+      end
+
+      it 'encodes travel pay client credentials in the request body' do
+        fake_req = Faraday::Request.new
+
+        with_settings(Settings.check_in.travel_reimbursement_api_v2,
+                      travel_pay_client_id: 'tp-id',
+                      travel_pay_client_secret: 'tp-secret',
+                      scope: 'api://some.scope/.default') do
+          expect_any_instance_of(Faraday::Connection).to receive(:post)
+            .with("/#{Settings.check_in.travel_reimbursement_api_v2.tenant_id}/oauth2/v2.0/token")
+            .and_yield(fake_req)
+            .and_return(veis_token_response)
+
+          subject.token
+          body = fake_req.body
+          expect(body).to include('client_id=tp-id')
+          expect(body).to include('client_secret=tp-secret')
+          expect(body).to include('grant_type=client_credentials')
+        end
+      end
+    end
+
+    context 'when veis auth returns an error' do
+      let(:resp) { Faraday::Response.new(body: { error: 'Unauthorized' }, status: 401) }
+      let(:exception) { Common::Exceptions::BackendServiceException.new(nil, {}, resp.status, resp.body) }
+
+      it 'logs message and raises exception' do
+        allow_any_instance_of(Faraday::Connection).to receive(:post).and_raise(exception)
+
+        expect_any_instance_of(SentryLogging).to receive(:log_message_to_sentry)
+        expect { subject.token }.to raise_exception(exception)
+      end
+    end
+  end
+
+  describe '#system_access_token' do
+    let(:veis_token) { 'veis-access' }
+
+    context 'when v4 system access returns success' do
+      let(:ok_resp) { Faraday::Response.new(response_body: { data: { accessToken: 'btsss' } }, status: 200) }
+
+      it 'posts to v4 path with required headers and body' do
+        fake_req = OpenStruct.new(options: OpenStruct.new, headers: {}, body: nil)
+
+        expect_any_instance_of(Faraday::Connection).to receive(:post)
+          .with('/api/v4/auth/system-access-token')
+          .and_yield(fake_req)
+          .and_return(ok_resp)
+
+        resp = subject.system_access_token(veis_access_token: veis_token)
+        expect(resp).to eq(ok_resp)
+        expect(fake_req.headers['Content-Type']).to eq('application/json')
+        expect(fake_req.headers['X-Correlation-ID']).to be_a(String)
+        expect(fake_req.headers['Authorization']).to eq("Bearer #{veis_token}")
+      end
+    end
+
+    context 'when v4 system access returns error' do
+      let(:exception) { Common::Exceptions::BackendServiceException.new(nil, {}, 401, { error: 'Unauthorized' }) }
+
+      it 'logs message and raises exception' do
+        allow_any_instance_of(Faraday::Connection).to receive(:post).and_raise(exception)
+        expect_any_instance_of(SentryLogging).to receive(:log_message_to_sentry)
+        expect { subject.system_access_token(veis_access_token: veis_token) }.to raise_exception(exception)
+      end
+    end
+  end
+end
