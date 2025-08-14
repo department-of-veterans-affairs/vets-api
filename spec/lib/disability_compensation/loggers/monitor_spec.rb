b17b5a45c677f534a1493aa480ccc06415b49f9d
Nathan Burgess <burgess.nathan28@gmail.com>
2025-08-14 09:48:15 -0400

    Small cleanup

--- spec/lib/disability_compensation/loggers/monitor_spec.rb
+++ spec/lib/disability_compensation/loggers/monitor_spec.rb
@@ -68,12 +68,13 @@ RSpec.describe DisabilityCompensation::Loggers::Monitor do
     end
 
     # NOTE: in_progress_form_id, user_account_uuid, and errors keys are whitelisted payload keys
-    # for monitors inheriting from Logging::BaseMonitor; ensures this informaiton will not be filtered out when it is
+    # for monitors inheriting from Logging::BaseMonitor; ensures this information will not be filtered out when it is
     # written to the Rails logger; see config/initializers/filter_parameter_logging.rb
     it 'does not filter out error details when writing to the Rails logger' do
       expect(Rails.logger).to receive(:error) do |_, payload|
         expect(payload[:context][:user_account_uuid]).to eq(user.uuid)
         expect(payload[:context][:errors]).to eq([{ form: mock_form_error }].to_s)
+        expect(payload[:context][:in_progress_form_id]).to eq(in_progress_form.id)
       end
 
       monitor.track_saved_claim_save_error(
