f84aae4c7580f308e8b1653ad7ce5a138a6a30ba
Tommasina Miller <144388524+tommasina-va@users.noreply.github.com>
2025-08-15 12:58:53 -0500

    Revert to using form key

--- lib/evss/disability_compensation_form/form4142_processor.rb
+++ lib/evss/disability_compensation_form/form4142_processor.rb
@@ -72,7 +72,7 @@ module EVSS
 
       # Flip this on to use the 2024 PDF template
       def generate_2024_version?
-        Flipper.enabled?(:disability_526_form4142_use_2024_version)
+        form_data['completed2024Form'] == true
       end
 
       # Flip this on to validate the schema of the form data
