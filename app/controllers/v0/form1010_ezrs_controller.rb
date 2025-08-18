539517dfc176c08dbfa7cde86950f3981b7c61a1
Brandon Cooper <brandon.cooper@adhocteam.us>
2025-08-18 16:14:01 -0400

    call pdf filler in ezr controller

--- app/controllers/v0/form1010_ezrs_controller.rb
+++ app/controllers/v0/form1010_ezrs_controller.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 require 'form1010_ezr/service'
+require 'pdf_fill/filler'
 
 module V0
   class Form1010EzrsController < ApplicationController
+    include RetriableConcern
+
     service_tag 'health-information-update'
 
     before_action :record_submission_attempt, only: :create
@@ -18,7 +21,20 @@ module V0
       render(json: result)
     end
 
-    def download_pdf; end
+    def download_pdf
+      parsed_form = parse_form(params[:form])
+      file_name = SecureRandom.uuid
+
+      source_file_path = with_retries('Generate 10-10EZR PDF') do
+        PdfFill::Filler.fill_ancillary_form(parsed_form, file_name, '10-10EZR')
+      end
+
+      file_contents = File.read(source_file_path)
+
+      send_data file_contents, filename: file_name_for_pdf, type: 'application/pdf', disposition: 'attachment'
+    ensure
+      File.delete(source_file_path) if source_file_path && File.exist?(source_file_path)
+    end
 
     private
 
@@ -28,6 +44,16 @@ module V0
 
     def parse_form(form)
       JSON.parse(form)
+    end
+
+    def file_name_for_pdf
+      '10-10EZR.pdf'
+
+      # full_name = current_user.full_name_normalized
+      # first_name = full_name[:first].presence || "First"
+      # last_name = full_name[:last].presence || "Last"
+
+      # "10-10EZR_#{first_name}_#{last_name}.pdf"
     end
   end
 end
