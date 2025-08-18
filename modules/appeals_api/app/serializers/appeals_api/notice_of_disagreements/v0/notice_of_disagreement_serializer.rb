bc97d3632428647314b6e073bd461a08f8134ba5
Peter Stevens <211847398+peter-stevens-va@users.noreply.github.com>
2025-08-18 15:00:12 -0400

    Update notice_of_disagreement_serializer.rb

--- modules/appeals_api/app/serializers/appeals_api/notice_of_disagreements/v0/notice_of_disagreement_serializer.rb
+++ modules/appeals_api/app/serializers/appeals_api/notice_of_disagreements/v0/notice_of_disagreement_serializer.rb
@@ -6,14 +6,6 @@ class AppealsApi::NoticeOfDisagreements::V0::NoticeOfDisagreementSerializer
   set_type :noticeOfDisagreement
   attributes :status
   attribute :code, if: proc { |nod| nod.status == 'error' }
-
-  attribute :final_status, if: proc { |_, _|
-    # The final_status will be serialized only if the decision_reviews_final_status_field flag is enabled
-    Flipper.enabled?(:decision_reviews_final_status_field)
-  } do |object, _|
-    object.upload_submission.in_final_status?
-  end
-
   attribute :detail, if: proc { |nod| nod.status == 'error' }
   # These names are required by Lighthouse standards
   attribute :createDate, &:created_at
