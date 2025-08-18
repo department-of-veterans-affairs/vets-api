1da27fb89c137220435cf7a6b5d6252d6cd34f16
Peter Stevens <211847398+peter-stevens-va@users.noreply.github.com>
2025-08-18 15:02:14 -0400

    Update supplemental_claim_serializer.rb

--- modules/appeals_api/app/serializers/appeals_api/supplemental_claims/v0/supplemental_claim_serializer.rb
+++ modules/appeals_api/app/serializers/appeals_api/supplemental_claims/v0/supplemental_claim_serializer.rb
@@ -5,14 +5,6 @@ class AppealsApi::SupplementalClaims::V0::SupplementalClaimSerializer
   set_key_transform :camel_lower
   set_type :supplementalClaim
   attributes :status
-
-  attribute :final_status, if: proc { |_, _|
-    # The final_status will be serialized only if the decision_reviews_final_status_field flag is enabled
-    Flipper.enabled?(:decision_reviews_final_status_field)
-  } do |object, _|
-    object.upload_submission.in_final_status?
-  end
-
   attribute :code, if: proc { |sc| sc.status == 'error' }
   attribute :detail, if: proc { |sc| sc.status == 'error' }
   # These names are required by Lighthouse standards
