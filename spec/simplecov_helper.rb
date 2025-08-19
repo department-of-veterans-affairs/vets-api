65d374f4b463dc1f99ca6120a34fc07473a2d9da
Rachal.Cassity <rachal.cassity@oddball.io>
2025-08-18 19:22:43 -0500

    added command_name no module track files

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -3,38 +3,48 @@
 # spec/simplecov_helper.rb
 require 'active_support/inflector'
 require 'simplecov'
+require_relative 'support/codeowners_parser'
 
 class SimpleCovHelper
   def self.start_coverage
     SimpleCov.start 'rails' do
+      ENV.fetch('SKIP_COVERAGE_CHECK', 'false')
+      print(ENV.fetch('TEST_ENV_NUMBER', nil))
+      # parallel_tests_count = ParallelTests.number_of_running_processes
+      # SimpleCov.command_name "(#{ENV['TEST_ENV_NUMBER'] || '1'}/#{parallel_tests_count})"
+
       SimpleCov.command_name "rspec-#{ENV['TEST_ENV_NUMBER'] || '0'}"
-      track_files '{app,lib,modules}/**/*.rb'
-      # track_files '**/{app,lib}/**/*.rb'
+      # track_files '{app,lib,modules}/**/*.rb'
+      track_files '**/{app,lib}/**/*.rb'
 
       add_filters
       add_modules
-      parse_codeowners
+      # parse_codeowners
 
-      skip_check_coverage = ENV.fetch('SKIP_COVERAGE_CHECK', 'false')
-      minimum_coverage(90) unless skip_check_coverage
-      refuse_coverage_drop unless skip_check_coverage
-      merge_timeout(3600)
+      # skip_check_coverage = ENV.fetch('SKIP_COVERAGE_CHECK', 'false')
+      # minimum_coverage(90) unless skip_check_coverage
+      # refuse_coverage_drop unless skip_check_coverage
+      # merge_timeout(3600)
+      if ENV['CI']
+        SimpleCov.minimum_coverage 90
+        SimpleCov.refuse_coverage_drop
+      end
     end
 
     if ENV['TEST_ENV_NUMBER'] # parallel specs
       SimpleCov.at_exit do
-        SimpleCovHelper.report_coverage
-        # result = SimpleCov.result
-        # result.format! if ParallelTests.number_of_running_processes <= 1
+        # SimpleCovHelper.report_coverage
+        result = SimpleCov.result
+        result.format!
+        # SimpleCovHelper.report_coverage # merge and format
       end
     end
   end
 
-  def self.report_coverage(base_dir: './coverage_results')
+  def self.report_coverage(base_dir: './coverage')
     SimpleCov.collate Dir["#{base_dir}/.resultset*.json"] do
       add_filters
       add_modules
-      parse_codeowners
     end
   rescue RuntimeError
     nil
@@ -119,37 +129,3 @@ class SimpleCovHelper
     add_group 'OctoIdentity', octo_identity_files
   end
 end
-
-#  def merge_results
-#     SimpleCov.collate(all_results) do
-#       add_filters
-#       add_modules
-#       parse_codeowners
-#     end
-#   rescue RuntimeError
-#     nil
-#   end
-
-#   attr_reader :base_dir
-
-#   def initialize(base_dir:)
-#     @base_dir = base_dir
-#   end
-
-#   def all_results
-#     Dir["#{base_dir}/.resultset*.json"]
-#   end
-
-#   if ENV['CI']
-#     SimpleCov.minimum_coverage 90
-#     SimpleCov.refuse_coverage_drop
-#   end
-# end
-
-#   if ENV['TEST_ENV_NUMBER'] # parallel specs
-#     SimpleCov.at_exit do
-#       result = SimpleCov.result
-#       result.format! if ParallelTests.number_of_running_processes <= 1
-#     end
-#   end
-# end
