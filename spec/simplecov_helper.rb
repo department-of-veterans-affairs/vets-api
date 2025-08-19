3b7b4bf40972cb4e3551ae960268cf827b5478e7
Rachal Cassity <rachal.cassity@oddball.io>
2025-08-19 13:03:14 -0500

    Refactored SimpleCov configuration for more consistent coverage results. (#23641)
    * Added simplecov command name
    
    * Refactor simplecov_helper
    
    * lint
    
    * require simplecov helper
    
    * added command_name no module track files
    
    * added command_line and track_files modules

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -3,41 +3,55 @@
 # spec/simplecov_helper.rb
 require 'active_support/inflector'
 require 'simplecov'
+require_relative 'support/codeowners_parser'
 
 class SimpleCovHelper
-  def self.report_coverage(base_dir: './coverage_results')
+  def self.start_coverage
     SimpleCov.start 'rails' do
-      skip_check_coverage = ENV.fetch('SKIP_COVERAGE_CHECK', 'false')
+      ENV.fetch('SKIP_COVERAGE_CHECK', 'false')
+      print(ENV.fetch('TEST_ENV_NUMBER', nil))
+      # parallel_tests_count = ParallelTests.number_of_running_processes
+      # SimpleCov.command_name "(#{ENV['TEST_ENV_NUMBER'] || '1'}/#{parallel_tests_count})"
 
-      track_files '**/{app,lib}/**/*.rb'
+      SimpleCov.command_name "rspec-#{ENV['TEST_ENV_NUMBER'] || '0'}"
+      track_files '{app,lib,modules}/**/*.rb'
+      # track_files '**/{app,lib}/**/*.rb'
 
       add_filters
       add_modules
+      # parse_codeowners
 
-      minimum_coverage(90) unless skip_check_coverage
-      refuse_coverage_drop unless skip_check_coverage
-      merge_timeout(3600)
+      # skip_check_coverage = ENV.fetch('SKIP_COVERAGE_CHECK', 'false')
+      # minimum_coverage(90) unless skip_check_coverage
+      # refuse_coverage_drop unless skip_check_coverage
+      # merge_timeout(3600)
+      if ENV['CI']
+        SimpleCov.minimum_coverage 90
+        SimpleCov.refuse_coverage_drop
+      end
     end
-    new(base_dir:).merge_results
+
+    if ENV['TEST_ENV_NUMBER'] # parallel specs
+      SimpleCov.at_exit do
+        # SimpleCovHelper.report_coverage
+        result = SimpleCov.result
+        result.format!
+        # SimpleCovHelper.report_coverage # merge and format
+      end
+    end
   end
 
-  attr_reader :base_dir
-
-  def initialize(base_dir:)
-    @base_dir = base_dir
-  end
-
-  def all_results
-    Dir["#{base_dir}/.resultset*.json"]
-  end
-
-  def merge_results
-    SimpleCov.collate all_results
+  def self.report_coverage(base_dir: './coverage')
+    SimpleCov.collate Dir["#{base_dir}/.resultset*.json"] do
+      add_filters
+      add_modules
+    end
   rescue RuntimeError
     nil
   end
 
   def self.add_filters
+    add_filter 'app/models/in_progress_disability_compensation_form.rb'
     add_filter 'lib/apps/configuration.rb'
     add_filter 'lib/apps/responses/response.rb'
     add_filter 'lib/config_helper.rb'
@@ -45,12 +59,23 @@ class SimpleCovHelper
     add_filter 'lib/gibft/configuration.rb'
     add_filter 'lib/ihub/appointments/response.rb'
     add_filter 'lib/salesforce/configuration.rb'
-    add_filter 'lib/vet360/address_validation/configuration.rb'
     add_filter 'lib/search/response.rb'
+    add_filter 'lib/search_gsa/response.rb'
+    add_filter 'lib/va_profile/v3/address_validation/configuration.rb'
+    add_filter 'lib/va_profile/exceptions/builder.rb'
+    add_filter 'lib/va_profile/response.rb'
+    add_filter 'lib/vet360/address_validation/configuration.rb'
     add_filter 'lib/vet360/exceptions/builder.rb'
     add_filter 'lib/vet360/response.rb'
+    add_filter 'modules/appeals_api/app/swagger'
+    add_filter 'modules/apps_api/app/controllers/apps_api/docs/v0/api_controller.rb'
+    add_filter 'modules/apps_api/app/swagger'
+    add_filter 'modules/burials/lib/benefits_intake/submission_handler.rb'
+    add_filter 'modules/check_in/config/initializers/statsd.rb'
     add_filter 'modules/claims_api/app/controllers/claims_api/v1/forms/disability_compensation_controller.rb'
     add_filter 'modules/claims_api/app/swagger/*'
+    add_filter 'modules/pensions/app/swagger'
+    add_filter 'modules/pensions/lib/benefits_intake/submission_handler.rb'
     add_filter 'version.rb'
   end
 
@@ -58,12 +83,15 @@ class SimpleCovHelper
     # Modules
     add_group 'AccreditedRepresentativePortal', 'modules/accredited_representative_portal/'
     add_group 'AppealsApi', 'modules/appeals_api/'
+    add_group 'AppsApi', 'modules/apps_api'
     add_group 'AskVAApi', 'modules/ask_va_api/'
     add_group 'Avs', 'modules/avs/'
     add_group 'Banners', 'modules/banners/'
+    add_group 'Burials', 'modules/burials/'
     add_group 'CheckIn', 'modules/check_in/'
     add_group 'ClaimsApi', 'modules/claims_api/'
     add_group 'ClaimsEvidenceApi', 'modules/claims_evidence_api/'
+    add_group 'CovidResearch', 'modules/covid_research/'
     add_group 'DebtsApi', 'modules/debts_api/'
     add_group 'DependentsVerification', 'modules/dependents_verification/'
     add_group 'DhpConnectedDevices', 'modules/dhp_connected_devices/'
@@ -75,6 +103,7 @@ class SimpleCovHelper
     add_group 'SimpleFormsApi', 'modules/simple_forms_api/'
     add_group 'IncomeLimits', 'modules/income_limits/'
     add_group 'MebApi', 'modules/meb_api/'
+    add_group 'Mobile', 'modules/mobile/'
     add_group 'MyHealth', 'modules/my_health/'
     add_group 'Pensions', 'modules/pensions/'
     add_group 'Policies', 'app/policies'
@@ -91,5 +120,12 @@ class SimpleCovHelper
     add_group 'Veteran', 'modules/veteran/'
     add_group 'VeteranVerification', 'modules/veteran_verification/'
     add_group 'Vye', 'modules/vye/'
+  end
+
+  def self.parse_codeowners
+    # Team Groups
+    codeowners_parser = CodeownersParser.new
+    octo_identity_files = codeowners_parser.perform('octo-identity')
+    add_group 'OctoIdentity', octo_identity_files
   end
 end
