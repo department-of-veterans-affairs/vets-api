10b08a11d83011a514c958b6259575ecbf9c3c33
John Bramley <bramleyjl@gmail.com>
2025-08-19 15:04:06 -0600

    breaks up authorize tests

---
+++ spec/controllers/v0/sign_in_controller_authorize_basic_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative 'sign_in_controller_shared_examples_spec'
+
+RSpec.describe V0::SignInController, type: :controller do
+  include_context 'authorize_shared_setup'
+
+  describe 'GET authorize' do
+    subject do
+      get(:authorize, params: authorize_params)
+    end
+
+    let(:authorize_params) do
+      {}.merge(type)
+        .merge(code_challenge)
+        .merge(code_challenge_method)
+        .merge(client_state)
+        .merge(client_id)
+        .merge(acr)
+        .merge(operation)
+        .merge(scope)
+    end
+    let(:statsd_tags) do
+      ["type:#{type_value}", "client_id:#{client_id_value}", "acr:#{acr_value}", "operation:#{operation_value}"]
+    end
+
+    context 'when client_id is not given' do
+      let(:client_id) { {} }
+      let(:client_id_value) { nil }
+      let(:expected_error) { 'Client id is not valid' }
+      let(:expected_error_json) { { 'errors' => expected_error } }
+      let(:expected_error_status) { :bad_request }
+      let(:statsd_auth_failure) { SignIn::Constants::Statsd::STATSD_SIS_AUTHORIZE_FAILURE }
+
+      it_behaves_like 'api based error response'
+    end
+
+    context 'when client_id is an arbitrary value' do
+      let(:client_id_value) { 'some-client-id' }
+      let(:expected_error) { 'Client id is not valid' }
+      let(:expected_error_json) { { 'errors' => expected_error } }
+      let(:expected_error_status) { :bad_request }
+      let(:statsd_auth_failure) { SignIn::Constants::Statsd::STATSD_SIS_AUTHORIZE_FAILURE }
+
+      it_behaves_like 'api based error response'
+    end
+
+    context 'when client_id maps to a client configuration' do
+      let(:client_id_value) { client_config.client_id }
+      let(:expected_redirect_uri_param) { { redirect_uri: expected_redirect_uri }.to_query }
+
+      context 'when type param is not given' do
+        let(:type) { {} }
+        let(:type_value) { nil }
+        let(:expected_error) { 'Type is not valid' }
+
+        it_behaves_like 'error response'
+      end
+
+      context 'when type param is given but not in client credential_service_providers' do
+        let(:type_value) { 'idme' }
+        let(:type) { { type: type_value } }
+        let(:credential_service_providers) { ['logingov'] }
+        let(:expected_error) { 'Type is not valid' }
+
+        it_behaves_like 'error response'
+      end
+    end
+  end
+end
