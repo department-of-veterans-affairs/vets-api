5513e987326245c1d4c78020acfd3897e1d15787
Gregory Hedrick <gregory@pixiitech.net>
2025-08-19 14:04:14 -0400

    linter

--- modules/accredited_representative_portal/spec/factories/persistent_attachments.rb
+++ modules/accredited_representative_portal/spec/factories/persistent_attachments.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :ar_persistent_attachment_va_form,
-    class: 'AccreditedRepresentativePortal::PersistentAttachments::VAForm' do
+          class: 'AccreditedRepresentativePortal::PersistentAttachments::VAForm' do
     form_id { '20-10207' }
 
     after(:build) do |attachment|
@@ -13,7 +13,7 @@ FactoryBot.define do
   end
 
   factory :ar_persistent_attachment_va_form_documentation,
-    class: 'AccreditedRepresentativePortal::PersistentAttachments::VAFormDocumentation' do
+          class: 'AccreditedRepresentativePortal::PersistentAttachments::VAFormDocumentation' do
     after(:build) do |attachment|
       file = Rails.root.join('spec', 'fixtures', 'files', 'doctors-note.pdf').open('rb')
       uploaded_file = Shrine.upload(file, :store)
