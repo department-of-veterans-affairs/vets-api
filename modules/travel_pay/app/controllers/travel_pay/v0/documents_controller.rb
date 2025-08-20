cd26f7a8c4701ece0a9ce3358dacf5904872cd27
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-08-20 16:28:08 -0600

    clean up controller and logic

--- modules/travel_pay/app/controllers/travel_pay/v0/documents_controller.rb
+++ modules/travel_pay/app/controllers/travel_pay/v0/documents_controller.rb
@@ -24,19 +24,17 @@ module TravelPay
 
       def create
         verify_feature_flag_enabled!
-        validate_claim_id_exists!
-        validate_document_exists!
 
         claim_id = params[:claim_id]
         document = params[:document]
+
+        validate_params!(claim_id, document)
+        
 
         Rails.logger.info(
           message: "Creating attachment for claim #{claim_id.slice(0, 8)}"
         )
 
-        # Verify the document is valid
-        # Add file extension, file size
-        # Validate the file extensions (kevin will provide to me) and file size (5 MB limit)
         # Call the service with claim_id and document
         document_id = service.upload_document(claim_id, document)
 
@@ -72,6 +70,13 @@ module TravelPay
         )
       end
 
+      def validate_params!(claim_id, document)
+        validate_claim_id_exists!(claim_id)
+        validate_document_exists!(document)
+        validate_document_extension!(document)
+        validate_document_size!(document)
+      end
+
       def verify_feature_flag_enabled!
         return if Flipper.enabled?(:travel_pay_enable_complex_claims, @current_user)
 
@@ -80,28 +85,41 @@ module TravelPay
         raise Common::Exceptions::ServiceUnavailable, message:
       end
 
-      def validate_claim_id_exists!
-        return if params[:claim_id].present?
+      def validate_claim_id_exists!(claim_id)
+        return if claim_id.present?
 
         raise Common::Exceptions::BadRequest, detail: 'Claim ID is required'
       end
 
-      def validate_document_exists!
-        return if params[:document].present?
+      def validate_document_exists!(document)
+        return if document.present?
 
         raise Common::Exceptions::BadRequest, detail: 'Document is required'
       end
 
       def validate_document_extension!(document)
-        return if !document.present?
+        return if document.blank?
 
         allowed_extensions = %w[pdf jpeg jpg png gif bmp tif tiff doc docx]
 
         # Extract the extension from the original filename
         extension = File.extname(document.original_filename).delete('.').downcase
 
-        unless allowed_extensions.include?(extension)
-          message = "Invalid document type: .#{extension}. Allowed types are: #{allowed_extensions.join(', ')}"
+        return if allowed_extensions.include?(extension)
+
+        message = "Invalid document type: .#{extension}. Allowed types are: #{allowed_extensions.join(', ')}"
+        Rails.logger.error(message)
+        raise Common::Exceptions::BadRequest, detail: message
+      end
+
+      def validate_document_size!(document)
+        return if document.blank?
+
+        max_size_in_bytes = 5.megabytes
+        file_size = document.size
+
+        if file_size > max_size_in_bytes
+          message = "Uploaded document size (#{file_size} bytes) exceeds the 5 MB limit."
           Rails.logger.error(message)
           raise Common::Exceptions::BadRequest, detail: message
         end
