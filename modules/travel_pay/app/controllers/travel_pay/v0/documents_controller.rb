c77ecf13669dc132affa9a538df97668f5f7e7f8
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-08-21 15:05:50 -0600

    moved some validations out of the conttroller and into the service, cleaned up code, attempted to update bad requests to return an error with details

--- modules/travel_pay/app/controllers/travel_pay/v0/documents_controller.rb
+++ modules/travel_pay/app/controllers/travel_pay/v0/documents_controller.rb
@@ -3,6 +3,8 @@
 module TravelPay
   module V0
     class DocumentsController < ApplicationController
+      rescue_from Common::Exceptions::BadRequest, with: :render_bad_request
+
       def show
         document_data = service.download_document(params[:claim_id], params[:id])
 
@@ -26,18 +28,20 @@ module TravelPay
         verify_feature_flag_enabled!
 
         claim_id = params[:claim_id]
-        document = params[:document]
+        document = params[:document] || params[:Document] # accept capital D from API
 
-        validate_params!(claim_id, document)
+        validate_claim_id_exists!(claim_id)
+        validate_document_exists!(document)
 
         Rails.logger.info(
           message: "Creating attachment for claim #{claim_id.slice(0, 8)}"
         )
 
-        # Call the service with claim_id and document
         document_id = service.upload_document(claim_id, document)
 
         render json: { documentId: document_id }, status: :created
+      rescue Common::Exceptions::BadRequest => e
+        render json: { errors: [{ detail: e.detail }] }, status: :bad_request
       rescue Faraday::ResourceNotFound => e
         handle_resource_not_found_error(e)
       rescue Faraday::Error => e
@@ -49,6 +53,10 @@ module TravelPay
       end
 
       private
+
+      def render_bad_request(e)
+        render json: { errors: [{ detail: e.detail }] }, status: :bad_request
+      end
 
       def auth_manager
         @auth_manager ||= TravelPay::AuthManager.new(Settings.travel_pay.client_number, @current_user)
@@ -69,13 +77,6 @@ module TravelPay
         )
       end
 
-      def validate_params!(claim_id, document)
-        validate_claim_id_exists!(claim_id)
-        validate_document_exists!(document)
-        validate_document_extension!(document)
-        validate_document_size!(document)
-      end
-
       def verify_feature_flag_enabled!
         return if Flipper.enabled?(:travel_pay_enable_complex_claims, @current_user)
 
@@ -85,43 +86,21 @@ module TravelPay
       end
 
       def validate_claim_id_exists!(claim_id)
-        return if claim_id.present?
+        # NOTE: In request specs, you can’t make params[:claim_id] truly missing because
+        # it’s part of the URL path and Rails routing prevents that.
+        if claim_id.blank?
+          raise Common::Exceptions::BadRequest, detail: 'Claim ID is required'
+        end
 
-        raise Common::Exceptions::BadRequest, detail: 'Claim ID is required'
+        unless claim_id =~ /\A[\w-]+\z/
+          raise Common::Exceptions::BadRequest, detail: 'Claim ID is invalid'
+        end
       end
 
       def validate_document_exists!(document)
         return if document.present?
 
         raise Common::Exceptions::BadRequest, detail: 'Document is required'
-      end
-
-      def validate_document_extension!(document)
-        return if document.blank?
-
-        allowed_extensions = %w[pdf jpeg jpg png gif bmp tif tiff doc docx]
-
-        # Extract the extension from the original filename
-        extension = File.extname(document.original_filename).delete('.').downcase
-
-        return if allowed_extensions.include?(extension)
-
-        message = "Invalid document type: .#{extension}. Allowed types are: #{allowed_extensions.join(', ')}"
-        Rails.logger.error(message)
-        raise Common::Exceptions::BadRequest, detail: message
-      end
-
-      def validate_document_size!(document)
-        return if document.blank?
-
-        max_size_in_bytes = 5.megabytes
-        file_size = document.size
-
-        if file_size > max_size_in_bytes
-          message = "Uploaded document size (#{file_size} bytes) exceeds the 5 MB limit."
-          Rails.logger.error(message)
-          raise Common::Exceptions::BadRequest, detail: message
-        end
       end
     end
   end
