6ee94874520cce0280d8d1c0add2e32b4ed95a7b
Brandon Cooper <brandon.cooper@adhocteam.us>
2025-08-21 11:29:52 -0400

    pull out logic from perform method to private methods

--- app/sidekiq/form_pdf_version_job.rb
+++ app/sidekiq/form_pdf_version_job.rb
@@ -15,27 +15,27 @@ class FormPdfVersionJob
     forms = response.body['data']
 
     cache_keys, current_sha_map = get_current_form_data(forms)
+    cached_sha_map = fetch_cached_values(cache_keys)
 
-    cached_sha_map = Rails.cache.fetch_multi(*cache_keys) do |_key|
-      nil
-    end
-
-    current_sha_map.each do |cache_key, data|
-      current_sha256 = data[:sha256]
-      form = data[:form]
-      last_known_sha256 = cached_sha_map[cache_key]
-
-      log_form_revision(form['attributes'], form['id']) if last_known_sha256 && last_known_sha256 != current_sha256
-    end
-
-    cache_data = current_sha_map.transform_values { |data| data[:sha256] }
-    Rails.cache.write_multi(cache_data, expires_in: CACHE_TTL) unless cache_data.empty?
+    log_form_revisions(current_sha_map, cached_sha_map)
+    update_cached_values(current_sha_map)
   rescue => e
     Rails.logger.error "Error in FormPdfVersionJob: #{e.message}"
     raise e
   end
 
   private
+
+  def fetch_cached_values(cache_keys)
+    Rails.cache.fetch_multi(*cache_keys) do |_key|
+      nil
+    end
+  end
+
+  def update_cached_values(current_sha_map)
+    cache_data = current_sha_map.transform_values { |data| data[:sha256] }
+    Rails.cache.write_multi(cache_data, expires_in: CACHE_TTL) unless cache_data.empty?
+  end
 
   def get_current_form_data(forms)
     cache_keys = []
@@ -58,6 +58,16 @@ class FormPdfVersionJob
     end
 
     [cache_keys, current_sha_map]
+  end
+
+  def log_form_revisions(current_sha_map, cached_sha_map)
+    current_sha_map.each do |cache_key, data|
+      current_sha256 = data[:sha256]
+      form = data[:form]
+      last_known_sha256 = cached_sha_map[cache_key]
+
+      log_form_revision(form['attributes'], form['id']) if last_known_sha256 && last_known_sha256 != current_sha256
+    end
   end
 
   def log_form_revision(form_attributes, form_id)
