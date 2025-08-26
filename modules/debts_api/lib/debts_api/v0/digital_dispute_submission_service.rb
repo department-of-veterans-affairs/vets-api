7c7abd51e50060cd25f2f66ef28c98ff29388cf1
Josh Mills <61624970+jtmst@users.noreply.github.com>
2025-08-26 10:37:17 -0400

    Update dispute model and service with email triggers and tests (#23534)
    * Update dispute model and service with email triggers and tests
    
    * clean up some logging and error handling, tests
    
    * flipper tests
    
    * remove success log
    
    * all emails behind flags
    
    * lint
    
    * add logging for email triggers

--- modules/debts_api/lib/debts_api/v0/digital_dispute_submission_service.rb
+++ modules/debts_api/lib/debts_api/v0/digital_dispute_submission_service.rb
@@ -26,6 +26,7 @@ module DebtsApi
         return duplicate_submission_result(submission) if check_duplicate?(submission)
 
         send_to_dmc
+        send_submission_email if email_notifications_enabled?
         submission.register_success
         in_progress_form&.destroy
 
@@ -150,6 +151,23 @@ module DebtsApi
                   end
 
         base_hash.merge!(details)
+      end
+
+      def email_notifications_enabled?
+        Flipper.enabled?(:digital_dispute_email_notifications) && @user.email.present?
+      end
+
+      def send_submission_email
+        DebtsApi::V0::Form5655::SendConfirmationEmailJob.perform_in(
+          5.minutes,
+          {
+            'submission_type' => 'digital_dispute',
+            'email' => @user.email,
+            'first_name' => @user.first_name,
+            'user_uuid' => @user.uuid,
+            'template_id' => DigitalDisputeSubmission::SUBMISSION_TEMPLATE
+          }
+        )
       end
     end
   end
