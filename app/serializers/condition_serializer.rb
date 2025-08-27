9aa72b49ae39c284813c5c71038ccb303c02ef86
Aswin Malla <aswinmalla12@gmail.com>
2025-08-26 20:40:20 -0600

    Update condition serializer with format_date

--- app/serializers/condition_serializer.rb
+++ app/serializers/condition_serializer.rb
@@ -4,20 +4,29 @@ class ConditionSerializer
   include JSONAPI::Serializer
 
   def self.serialize(record)
+    return {} if record.nil?
+
+    attributes = record.attributes || OpenStruct.new
+
     {
-      id: record.id,
-      type: record.type,
-      attributes: serialize_attributes(record.attributes)
+      id: record.id.to_s,
+      date: format_date(attributes.date),
+      name: attributes.name.to_s,
+      provider: attributes.provider&.to_s&.strip.presence,
+      facility: attributes.facility&.to_s&.strip.presence,
+      comments: attributes.comments&.to_s&.strip.presence
     }
   end
 
-  def self.serialize_attributes(attributes)
-    {
-      date: attributes.date,
-      name: attributes.name,
-      provider: attributes.provider,
-      facility: attributes.facility,
-      comments: attributes.comments
-    }
+  def self.format_date(value)
+    return nil if value.blank?
+
+    begin
+      DateTime.parse(value.to_s).iso8601.sub(/\+00:00$/, 'Z')
+    rescue ArgumentError
+      value.to_s
+    end
   end
+
+  private_class_method :format_date
 end
