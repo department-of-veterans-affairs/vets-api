bc2d39666ffc90da7f983640eeed795a3204eecd
Liz Townsend <liz.townsend@adhocteam.us>
2025-08-27 10:05:47 -0500

    add controller, serializer, tests, and vcr cassettes

---
+++ app/serializers/clinical_notes_serializer.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class ClinicalNotesSerializer
+  include JSONAPI::Serializer
+
+  set_id :id
+  set_type :clinical_note
+
+  attributes :id,
+             :name,
+             :type,
+             :date,
+             :date_signed,
+             :written_by,
+             :signed_by,
+             :location,
+             :note # base64 encoded
+end
