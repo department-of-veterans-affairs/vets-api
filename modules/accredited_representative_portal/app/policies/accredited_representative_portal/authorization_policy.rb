12fb161b4784aebfcfe4e9b42423b1150d489fdc
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2025-08-27 10:43:28 -0400

    rep authorization endpoint

---
+++ modules/accredited_representative_portal/app/policies/accredited_representative_portal/authorization_policy.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module AccreditedRepresentativePortal
+  class AuthorizationPolicy < ApplicationPolicy
+    def self.policy_name
+      'AccreditedRepresentativePortal::Authorization'
+    end
+
+    def authorize_as_representative?
+      # Must be logged in is enforced by ApplicationPolicy initialize
+      # Authorize only if the current user's account has at least one registration
+      account = @user.user_account
+
+      rep_account =
+        AccreditedRepresentativePortal::RepresentativeUserAccount.find(account.id)
+
+      registrations = rep_account.registrations
+      registrations.present?
+    rescue Common::Exceptions::Forbidden
+      false
+    end
+  end
+end
---
+++ modules/accredited_representative_portal/spec/requests/accredited_representative_portal/v0/authorized_as_representative_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require_relative '../../../rails_helper'
+
+RSpec.describe 'AccreditedRepresentativePortal::V0::AuthorizeAsRepresentative', type: :request do
+  describe 'GET /accredited_representative_portal/v0/authorize_as_representative' do
+    context 'when not authenticated' do
+      it 'responds with unauthorized (401)' do
+        get '/accredited_representative_portal/v0/authorize_as_representative'
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when authenticated' do
+      let(:user) { create(:representative_user, email: 'rep@example.com', all_emails: ['rep@example.com']) }
+
+      before do
+        login_as(user)
+      end
+
+      context 'and user is an accredited representative' do
+        before do
+          allow_any_instance_of(AccreditedRepresentativePortal::RepresentativeUserAccount)
+            .to receive(:registrations).and_return([
+                                                     OpenStruct.new(
+                                                       accredited_individual_registration_number: '1234',
+                                                       power_of_attorney_holder_type: 'veteran_service_organization'
+                                                     )
+                                                   ])
+        end
+
+        it 'returns 204 No Content' do
+          get '/accredited_representative_portal/v0/authorize_as_representative'
+          expect(response).to have_http_status(:no_content)
+          expect(response.body).to be_blank
+        end
+      end
+
+      context 'and user is not an accredited representative' do
+        before do
+          allow_any_instance_of(AccreditedRepresentativePortal::RepresentativeUserAccount)
+            .to receive(:registrations).and_return([])
+        end
+
+        it 'returns 403 Forbidden' do
+          get '/accredited_representative_portal/v0/authorize_as_representative'
+          expect(response).to have_http_status(:forbidden)
+        end
+      end
+
+      context 'and RepresentativeUserAccount raises Forbidden (e.g., OGC conflict/none)' do
+        before do
+          allow_any_instance_of(AccreditedRepresentativePortal::RepresentativeUserAccount)
+            .to receive(:registrations).and_raise(Common::Exceptions::Forbidden)
+        end
+
+        it 'returns 403 Forbidden' do
+          get '/accredited_representative_portal/v0/authorize_as_representative'
+          expect(response).to have_http_status(:forbidden)
+        end
+      end
+    end
+  end
+end
