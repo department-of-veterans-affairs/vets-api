c09348c735ecd55e5cec24c95a1e4c353c05a95c
Rachal.Cassity <rachal.cassity@oddball.io>
2025-08-28 12:27:55 -0500

    baseaddres requirement

--- lib/va_profile/models/base_address.rb
+++ lib/va_profile/models/base_address.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../base'
+require_relative 'base'
 require 'common/models/attribute_types/iso8601_time'
 require 'va_profile/concerns/defaultable'
 require 'va_profile/concerns/expirable'
