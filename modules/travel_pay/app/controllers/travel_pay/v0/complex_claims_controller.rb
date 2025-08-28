47771889b5ed2eb1a5388e0f31f3525e8d586f37
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-08-28 08:40:00 -0600

    added test file for complex claims controller and updated controller to handle bad request errors and capture error details

--- modules/travel_pay/app/controllers/travel_pay/v0/complex_claims_controller.rb
+++ modules/travel_pay/app/controllers/travel_pay/v0/complex_claims_controller.rb
@@ -3,24 +3,69 @@
 module TravelPay
   module V0
     class ComplexClaimsController < ApplicationController
+      rescue_from Common::Exceptions::BadRequest, with: :render_bad_request
+
       def create
         verify_feature_flag_enabled!
         validate_params_exist!(params)
-
         appt_id = get_appt!(params)
         claim_id = create_claim(appt_id)
-        render json: claim_id, status: :created
+        render json: { claimId: claim_id }, status: :created
       rescue Faraday::ResourceNotFound => e
         handle_resource_not_found_error(e)
       rescue Faraday::Error => e
-        Rails.logger.error("Error downloading document: #{e.message}")
-        render json: { error: 'Error downloading document' }, status: e.response[:status]
+        Rails.logger.error("Faraday error creating complex claim: #{e.message}")
+        render json: { error: 'Error creating complex claim' }, status: e.response[:status]
+      rescue Common::Exceptions::ServiceUnavailable => e
+        Rails.logger.error("Feature flag disabled: #{e.message}")
+        render json: { error: e.message }, status: :service_unavailable
       rescue Common::Exceptions::BackendServiceException => e
-        Rails.logger.error("Error downloading document: #{e.message}")
-        render json: { error: 'Error downloading document' }, status: e.original_status
+        Rails.logger.error("Backend service error creating complex claim: #{e.message}")
+        render json: { error: 'Error creating complex claim' }, status: e.original_status
       end
 
       private
+
+      def render_bad_request(e)
+        # If the error has a list of messages, use those
+        errors = if e.respond_to?(:errors) && e.errors.present?
+                   # Make sure each message looks like { detail: "something" }
+                   e.errors.map do |err|
+                     err.is_a?(Hash) ? err : { detail: err.to_s }
+                   end
+                 else
+                   # If nothing special came through, just send a basic message
+                   [{ detail: 'Bad request' }]
+                 end
+
+        render json: { errors: }, status: :bad_request
+      end
+
+      def verify_feature_flag_enabled!
+        return if Flipper.enabled?(:travel_pay_enable_complex_claims, @current_user)
+
+        message = 'Travel Pay create complex claim unavailable per feature toggle'
+        Rails.logger.error(message:)
+        raise Common::Exceptions::ServiceUnavailable, message:
+      end
+
+      def validate_params_exist!(params = {})
+        required_fields = {
+          'appointment_date_time' => 'Appointment date time is required',
+          'facility_station_number' => 'Facility station number is required',
+          'appointment_type' => 'Appointment type is required',
+          'is_complete' => 'The Is complete field is required'
+        }
+
+        errors = required_fields.each_with_object([]) do |(key, message), arr|
+          value = params[key]
+          missing = key == 'is_complete' ? value.nil? : value.blank?
+          arr << { 'detail' => message } if missing
+        end
+
+        # raise only if there are missing fields
+        raise Common::Exceptions::BadRequest.new(errors:) unless errors.empty?
+      end
 
       def auth_manager
         @auth_manager ||= TravelPay::AuthManager.new(Settings.travel_pay.client_number, @current_user)
@@ -52,25 +97,6 @@ module TravelPay
         claim = claims_service.create_new_claim({ 'btsss_appt_id' => appt_id })
 
         claim['claimId']
-      end
-
-      def validate_params_exist!(params = {})
-        {
-          'appointment_date_time' => 'Appointment date time is required',
-          'facility_station_number' => 'Facility station number is required',
-          'appointment_type' => 'Appointment type is required',
-          'is_complete' => 'The Is complete field is required'
-        }.each do |key, message|
-          raise Common::Exceptions::BadRequest.new(detail: message) if params[key].blank?
-        end
-      end
-
-      def verify_feature_flag_enabled!
-        return if Flipper.enabled?(:travel_pay_enable_complex_claims, @current_user)
-
-        message = 'Travel Pay create complex claim unavailable per feature toggle'
-        Rails.logger.error(message:)
-        raise Common::Exceptions::ServiceUnavailable, message:
       end
     end
   end
---
+++ modules/travel_pay/spec/requests/travel_pay/complex_claims_spec.rb
@@ -0,0 +1,153 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TravelPay::V0::ComplexClaimsController, type: :request do
+  let(:user) { build(:user) }
+  # let(:claims_service) { instance_double(TravelPay::ClaimsService) }
+  # let(:appts_service) { instance_double(TravelPay::AppointmentsService) }
+  let(:params) do
+    {
+      'appointment_date_time' => '2024-01-01T16:45:34.465Z',
+      'facility_station_number' => '123',
+      'appointment_type' => 'Other',
+      'is_complete' => false
+    }
+  end
+  let(:appointment_id) { 'aa0f63e0-5fa7-4d74-a17a-a6f510dbf69e' }
+  let(:claim_id) { '3fa85f64-5717-4562-b3fc-2c963f66afa6' }
+
+  before do
+    allow_any_instance_of(TravelPay::AuthManager).to receive(:authorize).and_return({ veis_token: 'veis_token',
+                                                                                      btsss_token: 'btsss_token' })
+    sign_in(user)
+    allow_any_instance_of(TravelPay::V0::ComplexClaimsController).to receive(:current_user).and_return(user)
+  end
+
+  # POST /travel_pay/v0/complex_claims/
+  describe '#create' do
+    context 'when feature flag is enabled' do
+      before do
+        allow(Flipper).to receive(:enabled?).with(:travel_pay_enable_complex_claims, instance_of(User)).and_return(true)
+      end
+
+      context 'VCR-backed integration tests' do
+        it 'creates a complex claim and returns claimId using vcr_cassette' do
+          VCR.use_cassette('travel_pay/submit/200_find_or_create_appt', match_requests_on: %i[method path]) do
+            VCR.use_cassette('travel_pay/submit/200_create_claim', match_requests_on: %i[method path]) do
+              post('/travel_pay/v0/complex_claims', params:, as: :json)
+
+              expect(response).to have_http_status(:created)
+              expect(JSON.parse(response.body)['claimId']).to eq(claim_id)
+            end
+          end
+        end
+      end
+
+      context 'stubbed controller behavior' do
+        before do
+          # Stub the appointment service
+          appts_service_double = instance_double(TravelPay::AppointmentsService)
+          allow(appts_service_double).to receive(:find_or_create_appointment)
+            .with(hash_including('appointment_date_time' => params['appointment_date_time']))
+            .and_return({ data: { 'id' => appointment_id } })
+
+          # Stub the claims service
+          claims_service_double = instance_double(TravelPay::ClaimsService)
+          allow(claims_service_double).to receive(:create_new_claim)
+            .with({ 'btsss_appt_id' => appointment_id })
+            .and_return({ 'claimId' => claim_id })
+
+          # Inject stubs into controller
+          allow_any_instance_of(TravelPay::V0::ComplexClaimsController)
+            .to receive(:appts_service).and_return(appts_service_double)
+          allow_any_instance_of(TravelPay::V0::ComplexClaimsController)
+            .to receive(:claims_service).and_return(claims_service_double)
+        end
+
+        it 'creates complex claim and returns claimId' do
+          post('/travel_pay/v0/complex_claims', params:)
+
+          expect(response).to have_http_status(:created)
+          expect(JSON.parse(response.body)).to eq('claimId' => claim_id)
+        end
+
+        context 'when params are missing' do
+          it 'returns bad request when all params are missing' do
+            post('/travel_pay/v0/complex_claims', params: { complex_claim: {} }, as: :json)
+
+            expect(response).to have_http_status(:bad_request)
+            body = JSON.parse(response.body)
+            expect(body['errors'].first['detail']).to eq('Appointment date time is required')
+          end
+
+          it 'returns bad request when all required params are missing' do
+            post('/travel_pay/v0/complex_claims', params: { complex_claim: {} }, as: :json)
+
+            expect(response).to have_http_status(:bad_request)
+            body = JSON.parse(response.body)
+
+            expected_errors = [
+              'Appointment date time is required',
+              'Facility station number is required',
+              'Appointment type is required',
+              'The Is complete field is required'
+            ]
+byebug
+            expect(body['errors'].map { |e| e['detail'] }).to match_array(expected_errors)
+          end
+        end
+
+        it 'returns not found when Faraday::ResourceNotFound' do
+          exception = Faraday::ResourceNotFound.new('Not found')
+          allow(exception).to receive(:response).and_return(
+            request: { headers: { 'X-Correlation-ID' => 'abc' } }
+          )
+          allow(service).to receive(:upload_document).and_raise(exception)
+
+          post("/travel_pay/v0/claims/#{claim_id}/documents", params: { Document: valid_document })
+
+          expect(response).to have_http_status(:not_found)
+          expect(JSON.parse(response.body)['error']).to eq('Document not found')
+          expect(JSON.parse(response.body)['correlation_id']).to eq('abc')
+        end
+
+        it 'returns error json with original status when BackendServiceException' do
+          error = Common::Exceptions::BackendServiceException.new('ERROR')
+          allow(error).to receive(:original_status).and_return(503)
+          allow(service).to receive(:upload_document).and_raise(error)
+
+          post("/travel_pay/v0/claims/#{claim_id}/documents", params: { Document: valid_document })
+
+          expect(response).to have_http_status(:service_unavailable)
+          expect(JSON.parse(response.body)['error']).to eq('Error downloading document')
+        end
+
+        it 'returns error json with status when Faraday::Error' do
+          error = Faraday::Error.new('ERROR')
+          allow(error).to receive(:response).and_return({ status: 502 }) # 502 = bad_gateway error
+          allow(service).to receive(:upload_document).and_raise(error)
+
+          post("/travel_pay/v0/claims/#{claim_id}/documents", params: { Document: valid_document })
+
+          expect(response).to have_http_status(:bad_gateway)
+          expect(JSON.parse(response.body)['error']).to eq('Error downloading document')
+        end
+      end
+    end
+
+    context 'when feature flag disabled' do
+      before do
+        allow(Flipper).to receive(:enabled?)
+          .with(:travel_pay_enable_complex_claims, instance_of(User))
+          .and_return(false)
+      end
+
+      it 'returns 503 Service Unavailable' do
+        post('/travel_pay/v0/complex_claims', params: {})
+
+        expect(response).to have_http_status(:service_unavailable)
+      end
+    end
+  end
+end
