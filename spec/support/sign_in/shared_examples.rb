4fbc0707b53e144eeba9025574035b026d6d8fcd
John Bramley <bramleyjl@gmail.com>
2025-08-28 15:41:39 -0600

    updates

--- spec/support/sign_in/shared_examples.rb
+++ spec/support/sign_in/shared_examples.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'service_account_authorization_context'
-
-# Shared contexts
-require_relative 'shared_contexts/authorize/setup'
-require_relative 'shared_contexts/authorize/client_state_handling'
-
-require_relative 'shared_contexts/callback/setup'
-require_relative 'shared_contexts/callback/state_jwt_setup'
-
-# Shared examples
 require_relative 'shared_examples/authorize/api_error_response'
 require_relative 'shared_examples/authorize/error_response'
 require_relative 'shared_examples/authorize/successful_response'
