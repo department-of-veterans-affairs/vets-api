61b0ffbdbb057b5eacc36b7b800644a51ac7f216
Ian Seabock <iseabock@MacBook-Pro.local>
2025-09-02 14:40:59 -0700

    fix test

--- modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
@@ -4,7 +4,18 @@ module MyHealth
   module V1
     class PrescriptionDocumentationController < RxController
       def index
-        render json: { error: 'Documentation is not available' }, status: :not_found
+          begin
+            id = params[:id]
+            rx = client.get_rx_details(id)
+            raise StandardError, 'Rx not found' if rx.nil?
+            raise StandardError, 'Missing NDC number' if rx.cmop_ndc_value.nil?
+
+            documentation = client.get_rx_documentation(rx.cmop_ndc_value)
+            prescription_documentation = PrescriptionDocumentation.new({ html: documentation[:data] })
+            render json: PrescriptionDocumentationSerializer.new(prescription_documentation)
+          rescue => e
+            render json: { error: "Unable to fetch documentation: #{e}" }, status: :service_unavailable
+          end
       end
     end
   end
