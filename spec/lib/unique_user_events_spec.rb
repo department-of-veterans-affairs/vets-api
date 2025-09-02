50746c5fd976b8048507bd05a4a5eb3d6e2aada0
Carlos Felix <carlos.felix@agile6.com>
2025-09-02 11:34:52 -0400

    Removed feature flag use

--- spec/lib/unique_user_events_spec.rb
+++ spec/lib/unique_user_events_spec.rb
@@ -10,44 +10,25 @@ RSpec.describe UniqueUserEvents do
   describe '.log_event' do
     before do
       allow(UniqueUserEvents::Service).to receive(:log_event)
+      allow(StatsD).to receive(:measure) # Stub StatsD calls that aren't being tested
     end
 
-    context 'when feature flag is enabled' do
-      before do
-        allow(Flipper).to receive(:enabled?).with(:unique_user_metrics_logging).and_return(true)
-        allow(StatsD).to receive(:measure) # Stub StatsD calls that aren't being tested
-      end
+    it 'delegates to service and returns result' do
+      allow(UniqueUserEvents::Service).to receive(:log_event).and_return(true)
 
-      it 'delegates to service and returns result' do
-        allow(UniqueUserEvents::Service).to receive(:log_event).and_return(true)
+      result = described_class.log_event(user_id:, event_name:)
 
-        result = described_class.log_event(user_id:, event_name:)
-
-        expect(result).to be(true)
-        expect(UniqueUserEvents::Service).to have_received(:log_event).with(user_id:, event_name:)
-      end
-
-      it 'passes through service result when false' do
-        allow(UniqueUserEvents::Service).to receive(:log_event).and_return(false)
-
-        result = described_class.log_event(user_id:, event_name:)
-
-        expect(result).to be(false)
-        expect(UniqueUserEvents::Service).to have_received(:log_event).with(user_id:, event_name:)
-      end
+      expect(result).to be(true)
+      expect(UniqueUserEvents::Service).to have_received(:log_event).with(user_id:, event_name:)
     end
 
-    context 'when feature flag is disabled' do
-      before do
-        allow(Flipper).to receive(:enabled?).with(:unique_user_metrics_logging).and_return(false)
-      end
+    it 'passes through service result when false' do
+      allow(UniqueUserEvents::Service).to receive(:log_event).and_return(false)
 
-      it 'returns false without calling service' do
-        result = described_class.log_event(user_id:, event_name:)
+      result = described_class.log_event(user_id:, event_name:)
 
-        expect(result).to be(false)
-        expect(UniqueUserEvents::Service).not_to have_received(:log_event)
-      end
+      expect(result).to be(false)
+      expect(UniqueUserEvents::Service).to have_received(:log_event).with(user_id:, event_name:)
     end
   end
 
@@ -56,61 +37,29 @@ RSpec.describe UniqueUserEvents do
       allow(UniqueUserEvents::Service).to receive(:event_logged?)
     end
 
-    context 'when feature flag is enabled' do
-      before do
-        allow(Flipper).to receive(:enabled?).with(:unique_user_metrics_logging).and_return(true)
-      end
+    it 'delegates to service and returns result' do
+      allow(UniqueUserEvents::Service).to receive(:event_logged?).and_return(true)
 
-      it 'delegates to service and returns result' do
-        allow(UniqueUserEvents::Service).to receive(:event_logged?).and_return(true)
+      result = described_class.event_logged?(user_id:, event_name:)
 
-        result = described_class.event_logged?(user_id:, event_name:)
-
-        expect(result).to be(true)
-        expect(UniqueUserEvents::Service).to have_received(:event_logged?).with(user_id:,
-                                                                                event_name:)
-      end
-
-      it 'passes through service result when false' do
-        allow(UniqueUserEvents::Service).to receive(:event_logged?).and_return(false)
-
-        result = described_class.event_logged?(user_id:, event_name:)
-
-        expect(result).to be(false)
-        expect(UniqueUserEvents::Service).to have_received(:event_logged?).with(user_id:,
-                                                                                event_name:)
-      end
+      expect(result).to be(true)
+      expect(UniqueUserEvents::Service).to have_received(:event_logged?).with(user_id:,
+                                                                              event_name:)
     end
 
-    context 'when feature flag is disabled' do
-      before do
-        allow(Flipper).to receive(:enabled?).with(:unique_user_metrics_logging).and_return(false)
-      end
+    it 'passes through service result when false' do
+      allow(UniqueUserEvents::Service).to receive(:event_logged?).and_return(false)
 
-      it 'returns false without calling service' do
-        result = described_class.event_logged?(user_id:, event_name:)
+      result = described_class.event_logged?(user_id:, event_name:)
 
-        expect(result).to be(false)
-        expect(UniqueUserEvents::Service).not_to have_received(:event_logged?)
-      end
-    end
-  end
-
-  describe 'feature flag integration' do
-    it 'uses the correct feature flag name' do
-      allow(Flipper).to receive(:enabled?).with(:unique_user_metrics_logging).and_return(true)
-      allow(UniqueUserEvents::Service).to receive(:log_event).and_return(true)
-      allow(StatsD).to receive(:measure) # Stub StatsD calls that aren't being tested
-
-      described_class.log_event(user_id:, event_name:)
-
-      expect(Flipper).to have_received(:enabled?).with(:unique_user_metrics_logging)
+      expect(result).to be(false)
+      expect(UniqueUserEvents::Service).to have_received(:event_logged?).with(user_id:,
+                                                                              event_name:)
     end
   end
 
   describe 'performance metrics' do
     before do
-      allow(Flipper).to receive(:enabled?).with(:unique_user_metrics_logging).and_return(true)
       allow(UniqueUserEvents::Service).to receive(:log_event).and_return(true)
     end
 
