b6414f85dc48fc7314cc171a86567e312e38baaa
= <=>
2025-09-02 11:43:17 -0500

    Implement feature flag for ML classifier in contention classification
    - Add contention_classification_ml_classifier feature flag in features.yml
    - Add hybrid_contention_classification_path to settings configurations
    - Implement classify_vagov_contentions_hybrid method in ContentionClassification::Client
    - Update classify_vagov_contentions method to use feature flag for endpoint selection
    - Add comprehensive tests for hybrid endpoint and feature flag behavior
    - Enable gradual rollout of ML classifier with safe fallback to expanded endpoint
    
    Addresses GitHub issue #864

--- spec/sidekiq/evss/disability_compensation_form/submit_form526_all_claim_spec.rb
+++ spec/sidekiq/evss/disability_compensation_form/submit_form526_all_claim_spec.rb
@@ -205,11 +205,24 @@ RSpec.describe EVSS::DisabilityCompensationForm::SubmitForm526AllClaim, type: :j
       end
 
       context 'with contention classification enabled' do
-        it 'uses the migrated endpoint' do
+        it 'uses the expanded endpoint when ML classifier feature flag is disabled' do
+          Flipper.disable(:contention_classification_ml_classifier)
           mock_cc_client = instance_double(ContentionClassification::Client)
           allow(ContentionClassification::Client).to receive(:new).and_return(mock_cc_client)
           expect_any_instance_of(Form526Submission).to receive(:classify_vagov_contentions).and_call_original
           expect(mock_cc_client).to receive(:classify_vagov_contentions_expanded)
+          expect(mock_cc_client).not_to receive(:classify_vagov_contentions_hybrid)
+          subject.perform_async(submission.id)
+          described_class.drain
+        end
+
+        it 'uses the hybrid endpoint when ML classifier feature flag is enabled' do
+          Flipper.enable(:contention_classification_ml_classifier)
+          mock_cc_client = instance_double(ContentionClassification::Client)
+          allow(ContentionClassification::Client).to receive(:new).and_return(mock_cc_client)
+          expect_any_instance_of(Form526Submission).to receive(:classify_vagov_contentions).and_call_original
+          expect(mock_cc_client).to receive(:classify_vagov_contentions_hybrid)
+          expect(mock_cc_client).not_to receive(:classify_vagov_contentions_expanded)
           subject.perform_async(submission.id)
           described_class.drain
         end
