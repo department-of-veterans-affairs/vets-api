53f0eb6560d4a526bead5180438a765501b8d5f7
Ian Seabock <iseabock@MacBook-Pro.local>
2025-09-03 10:49:25 -0700

    address linting errors

--- modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
@@ -4,18 +4,16 @@ module MyHealth
   module V1
     class PrescriptionDocumentationController < RxController
       def index
-          begin
-            id = params[:id]
-            rx = client.get_rx_details(id)
-            raise StandardError, 'Rx not found' if rx.nil?
-            raise StandardError, 'Missing NDC number' if rx.cmop_ndc_value.nil?
+        id = params[:id]
+        rx = client.get_rx_details(id)
+        raise StandardError, 'Rx not found' if rx.nil?
+        raise StandardError, 'Missing NDC number' if rx.cmop_ndc_value.nil?
 
-            documentation = client.get_rx_documentation(rx.cmop_ndc_value)
-            prescription_documentation = PrescriptionDocumentation.new({ html: documentation[:data] })
-            render json: PrescriptionDocumentationSerializer.new(prescription_documentation)
-          rescue => e
-            render json: { error: "Unable to fetch documentation: #{e}" }, status: :service_unavailable
-          end
+        documentation = client.get_rx_documentation(rx.cmop_ndc_value)
+        prescription_documentation = PrescriptionDocumentation.new({ html: documentation[:data] })
+        render json: PrescriptionDocumentationSerializer.new(prescription_documentation)
+      rescue => e
+        render json: { error: "Unable to fetch documentation: #{e}" }, status: :service_unavailable
       end
     end
   end
