3582b7bd5f2607efbbdfff2ca1b2b4345adf0f35
Aswin Malla <aswinmalla12@gmail.com>
2025-09-03 09:01:09 -0600

    Add error handling for conditions controller and update relative tests

--- modules/my_health/app/controllers/my_health/v2/conditions_controller.rb
+++ modules/my_health/app/controllers/my_health/v2/conditions_controller.rb
@@ -11,12 +11,56 @@ module MyHealth
 
       def index
         conditions = service.get_conditions
-        render json: UnifiedHealthData::Serializers::ConditionSerializer.new(conditions).serializable_hash[:data]
+        render json: UnifiedHealthData::Serializers::ConditionSerializer.new(conditions).serializable_hash[:data],
+               status: :ok
+      rescue Common::Client::Errors::ClientError,
+             Common::Exceptions::BackendServiceException,
+             StandardError => e
+        handle_error(e)
       end
 
       private
 
+      def handle_error(error)
+        log_error(error)
+
+        case error
+        when Common::Client::Errors::ClientError
+          render_error('FHIR API Error', error.message, error.status, error.status, :bad_gateway)
+        when Common::Exceptions::BackendServiceException
+          render json: { errors: error.errors }, status: :bad_gateway
+        else
+          render_error('Internal Server Error',
+                       'An unexpected error occurred while retrieving conditions.',
+                       '500', 500, :internal_server_error)
+        end
+      end
+
+      def log_error(error)
+        message = case error
+                  when Common::Client::Errors::ClientError
+                    "Conditions FHIR API error: #{error.message}"
+                  when Common::Exceptions::BackendServiceException
+                    "Backend service exception: #{error.errors.first&.detail}"
+                  else
+                    "Unexpected error in conditions controller: #{error.message}"
+                  end
+        Rails.logger.error(message)
+      end
+
+      def render_error(title, detail, code, status, http_status)
+        error = {
+          title:,
+          detail:,
+          code:,
+          status:
+        }
+        render json: { errors: [error] }, status: http_status
+      end
+
       def service
+        # test_user = OpenStruct.new(icn: '1014135410V826374')
+        # UnifiedHealthData::Service.new(test_user)
         UnifiedHealthData::Service.new(@current_user)
       end
     end
--- spec/controllers/my_health/v2/conditions_controller_spec.rb
+++ spec/controllers/my_health/v2/conditions_controller_spec.rb
@@ -62,5 +62,69 @@ RSpec.describe MyHealth::V2::ConditionsController, type: :controller do
       get :index
       expect(response).to have_http_status(:ok)
     end
+
+    context 'error handling' do
+      it 'handles Common::Client::Errors::ClientError' do
+        error = Common::Client::Errors::ClientError.new('FHIR API error', 502)
+        allow_any_instance_of(UnifiedHealthData::Service).to receive(:get_conditions)
+          .and_raise(error)
+
+        get :index
+
+        expect(response).to have_http_status(:bad_gateway)
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']).to be_an(Array)
+        expect(json_response['errors'].first).to include(
+          'title' => 'FHIR API Error',
+          'detail' => 'FHIR API error',
+          'code' => 502,
+          'status' => 502
+        )
+      end
+
+      it 'handles Common::Exceptions::BackendServiceException' do
+        error_detail = { 'detail' => 'Backend service is unavailable' }
+        error = Common::Exceptions::BackendServiceException.new('UHD_SERVICE_ERROR', {}, 502, error_detail)
+        allow(error).to receive(:errors).and_return([error_detail])
+        allow_any_instance_of(UnifiedHealthData::Service).to receive(:get_conditions)
+          .and_raise(error)
+
+        get :index
+
+        expect(response.status).to be_in([500, 502])
+        json_response = JSON.parse(response.body)
+        expect(json_response).to have_key('errors')
+        expect(json_response['errors']).to be_an(Array)
+        expect(json_response['errors']).not_to be_empty
+      end
+
+      it 'handles StandardError' do
+        allow_any_instance_of(UnifiedHealthData::Service).to receive(:get_conditions)
+          .and_raise(StandardError.new('Unexpected error'))
+
+        get :index
+
+        expect(response).to have_http_status(:internal_server_error)
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']).to be_an(Array)
+        expect(json_response['errors'].first).to include(
+          'title' => 'Internal Server Error',
+          'detail' => 'An unexpected error occurred while retrieving conditions.',
+          'code' => '500',
+          'status' => 500
+        )
+      end
+
+      it 'logs errors appropriately' do
+        allow(Rails.logger).to receive(:error)
+        allow_any_instance_of(UnifiedHealthData::Service).to receive(:get_conditions)
+          .and_raise(StandardError.new('Test error'))
+
+        get :index
+
+        expect(Rails.logger).to have_received(:error)
+          .with('Unexpected error in conditions controller: Test error')
+      end
+    end
   end
 end
