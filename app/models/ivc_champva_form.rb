2e6271678971966a675c55ab4280a8c88cb5648c
broskj <170366505+brostk@users.noreply.github.com>
2025-09-04 18:04:19 -0400

    encryption step 2 - remove migrating:true and old columns

--- app/models/ivc_champva_form.rb
+++ app/models/ivc_champva_form.rb
@@ -5,7 +5,9 @@ class IvcChampvaForm < ApplicationRecord
 
   has_kms_key
   has_encrypted :ves_request_data, key: :kms_key, **lockbox_options
-  has_encrypted :first_name, migrating: true, key: :kms_key, **lockbox_options
-  has_encrypted :last_name, migrating: true, key: :kms_key, **lockbox_options
-  has_encrypted :email, migrating: true, key: :kms_key, **lockbox_options
+  has_encrypted :first_name, key: :kms_key, **lockbox_options
+  has_encrypted :last_name, key: :kms_key, **lockbox_options
+  has_encrypted :email, key: :kms_key, **lockbox_options
+
+  blind_index :email
 end
