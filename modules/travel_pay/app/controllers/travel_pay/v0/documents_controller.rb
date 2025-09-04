d22fea0881d7b50d694172fc992c35e4ec9e4a77
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-09-04 10:29:44 -0600

    updated the doc controller to use the new concerns auth helper methods

--- modules/travel_pay/app/controllers/travel_pay/v0/documents_controller.rb
+++ modules/travel_pay/app/controllers/travel_pay/v0/documents_controller.rb
@@ -3,6 +3,8 @@
 module TravelPay
   module V0
     class DocumentsController < ApplicationController
+      include AuthHelper
+
       rescue_from Common::Exceptions::BadRequest, with: :render_bad_request
 
       def show
@@ -25,7 +27,7 @@ module TravelPay
       end
 
       def create
-        verify_feature_flag_enabled!
+        verify_feature_flag!(:travel_pay_enable_complex_claims, current_user, error_message: 'Travel Pay create document unavailable per feature toggle' )
 
         claim_id = params[:claim_id]
         document = params[:document] || params[:Document] # accept capital D from API
@@ -86,21 +88,6 @@ module TravelPay
         message = 'Travel Pay create document unavailable per feature toggle'
         Rails.logger.error(message:)
         raise Common::Exceptions::ServiceUnavailable, message:
-      end
-
-      def validate_claim_id_exists!(claim_id)
-        # NOTE: In request specs, you can’t make params[:claim_id] truly missing because
-        # it’s part of the URL path and Rails routing prevents that.
-        raise Common::Exceptions::BadRequest.new(detail: 'Claim ID is required') if claim_id.blank?
-
-        # ensure claim ID is the right format, allowing any version
-        uuid_all_version_format = /\A[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[89ABCD][0-9A-F]{3}-[0-9A-F]{12}\z/i
-
-        unless uuid_all_version_format.match?(claim_id)
-          raise Common::Exceptions::BadRequest.new(
-            detail: 'Claim ID is invalid'
-          )
-        end
       end
 
       def validate_document_exists!(document)
