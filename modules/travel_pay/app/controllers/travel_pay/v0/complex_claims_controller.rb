cb236abb96d30f2974698dd9766c454c26573d4c
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-09-04 11:42:08 -0600

    remove the create method since this is just for submit

--- modules/travel_pay/app/controllers/travel_pay/v0/complex_claims_controller.rb
+++ modules/travel_pay/app/controllers/travel_pay/v0/complex_claims_controller.rb
@@ -8,29 +8,6 @@ module TravelPay
       rescue_from Common::Exceptions::BadRequest, with: :render_bad_request
       rescue_from Common::Exceptions::ServiceUnavailable, with: :render_service_unavailable
 
-      def create
-        verify_feature_flag!(
-          :travel_pay_enable_complex_claims,
-          current_user,
-          error_message: 'Travel Pay create complex claim unavailable per feature toggle'
-        )
-        validate_params_exist!(params)
-        validate_datetime_format!(params[:appointment_date_time])
-        appt_id = get_appt!(params)
-        claim_id = create_claim(appt_id)
-        render json: { claimId: claim_id }, status: :created
-      rescue Common::Exceptions::ResourceNotFound => e
-        Rails.logger.error("Appointment not found: #{e.message}")
-        render json: { error: e.message }, status: :not_found
-      rescue Faraday::Error => e
-        Rails.logger.error("Faraday error creating complex claim: #{e.message}")
-        status_code = (e.respond_to?(:response) && e.response && e.response[:status]) || :internal_server_error
-        render json: { error: 'Error creating complex claim' }, status: status_code
-      rescue Common::Exceptions::BackendServiceException => e
-        Rails.logger.error("Backend service error creating complex claim: #{e.message}")
-        render json: { error: 'Error creating complex claim' }, status: e.original_status
-      end
-
       def submit
         verify_feature_flag!(
           :travel_pay_enable_complex_claims,
@@ -80,68 +57,12 @@ module TravelPay
         render json: { error: e.message }, status: :service_unavailable
       end
 
-      def validate_params_exist!(params = {})
-        required_fields = {
-          'appointment_date_time' => 'Appointment date time is required',
-          'facility_station_number' => 'Facility station number is required',
-          'appointment_type' => 'Appointment type is required',
-          'is_complete' => 'The Is complete field is required'
-        }
-
-        errors = required_fields.each_with_object([]) do |(key, message), arr|
-          value = params[key]
-          missing = key == 'is_complete' ? value.nil? : value.blank?
-          arr << { 'detail' => message } if missing
-        end
-
-        # raise only if there are missing fields
-        raise Common::Exceptions::BadRequest.new(errors:) unless errors.empty?
-      end
-
-      def validate_datetime_format!(datetime_str)
-        DateTime.iso8601(datetime_str)
-      rescue ArgumentError
-        raise Common::Exceptions::BadRequest.new(
-          errors: [{ 'detail' => 'Appointment date time must be a valid datetime' }]
-        )
-      end
-
       def auth_manager
         @auth_manager ||= TravelPay::AuthManager.new(Settings.travel_pay.client_number, @current_user)
       end
 
       def claims_service
         @claims_service ||= TravelPay::ClaimsService.new(auth_manager, @current_user)
-      end
-
-      def appts_service
-        @appts_service ||= TravelPay::AppointmentsService.new(auth_manager)
-      end
-
-      def get_appt(params = {})
-        Rails.logger.info(message: "Get appt by date time: #{params['appointment_date_time']}")
-        appt = appts_service.find_or_create_appointment(params)
-
-        return nil if appt.nil? || appt[:data].nil?
-
-        appt[:data]['id']
-      end
-
-      def get_appt!(params = {})
-        get_appt(params) ||
-          raise(
-            Common::Exceptions::ResourceNotFound.new(
-              resource: 'Appointment',
-              id: params['appointment_date_time']
-            )
-          )
-      end
-
-      def create_claim(appt_id)
-        Rails.logger.info(message: 'Create complex claim')
-        claim = claims_service.create_new_claim({ 'btsss_appt_id' => appt_id })
-
-        claim['claimId']
       end
     end
   end
