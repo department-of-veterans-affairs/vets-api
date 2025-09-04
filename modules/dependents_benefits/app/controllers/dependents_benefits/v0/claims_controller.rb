304b1bd91dd0e8525d93019173a44d8a4a818d83
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-09-04 15:46:29 -0400

    Add controller for Dependents Benefits

---
+++ modules/dependents_benefits/app/controllers/dependents_benefits/v0/claims_controller.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require 'dependents_benefits/monitor'
+
+module DependentsBenefits
+  module V0
+    ###
+    # The Dependents Benefits claim controller that handles form submissions
+    #
+    class ClaimsController < ClaimsBaseController
+      before_action :load_user, only: %i[create show]
+      before_action :check_flipper_flag
+
+      service_tag 'dependent-change'
+
+      def show
+        dependents = create_dependent_service.get_dependents
+        dependents[:diaries] = dependency_verification_service.read_diaries
+        render json: DependentsSerializer.new(dependents)
+      rescue => e
+        monitor.track_error_event('Failure fetching dependents data', "#{stats_key}.show_error",
+                                  { error: e.message })
+        raise Common::Exceptions::BackendServiceException.new(nil, detail: e.message)
+      end
+
+      def create
+        claim = DependentsBenefits::SavedClaim.new(form: dependent_params.to_json)
+
+        # Populate the form_start_date from the IPF if available
+        in_progress_form = current_user ? InProgressForm.form_for_user(claim.form_id, current_user) : nil
+        claim.form_start_date = in_progress_form.created_at if in_progress_form
+
+        raise Common::Exceptions::ValidationErrors, claim unless claim.save
+
+        monitor.track_info_event('Successfully created claim', "#{stats_key}.create_success",
+                                 { claim_id: claim.id, user_account_uuid: current_user&.user_account_uuid })
+
+        render json: SavedClaimSerializer.new(claim)
+      end
+
+      private
+
+      def dependent_params
+        params.permit(
+          :add_spouse,
+          :veteran_was_married_before,
+          :add_child,
+          :report674,
+          :report_divorce,
+          :spouse_was_married_before,
+          :report_stepchild_not_in_household,
+          :report_death,
+          :report_marriage_of_child_under18,
+          :report_child18_or_older_is_not_attending_school,
+          :statement_of_truth_signature,
+          :statement_of_truth_certified,
+          'view:selectable686_options': {},
+          dependents_application: {},
+          supporting_documents: []
+        )
+      end
+
+      def stats_key
+        'api.dependents_application'
+      end
+
+      # Raises an exception if the dependents verification flipper flag isn't enabled.
+      def check_flipper_flag
+        raise Common::Exceptions::Forbidden unless Flipper.enabled?(:dependents_module_enabled, current_user)
+      end
+
+      def create_dependent_service
+        @dependent_service ||= BGS::DependentV2Service.new(current_user)
+      end
+
+      def dependency_verification_service
+        @dependency_verification_service ||= BGS::DependencyVerificationService.new(current_user)
+      end
+
+      def monitor
+        DependentsBenefits::Monitor.new
+      end
+    end
+  end
+end
