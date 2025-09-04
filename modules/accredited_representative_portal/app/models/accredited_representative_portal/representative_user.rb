8ada907d0603b2b2344787fbcff467b163b97fb0
Oren Mittman <nihil2501@gmail.com>
2025-09-04 05:12:54 -0400

    Remove `accredited_representative_portal_self_service_auth` feature toggle

--- modules/accredited_representative_portal/app/models/accredited_representative_portal/representative_user.rb
+++ modules/accredited_representative_portal/app/models/accredited_representative_portal/representative_user.rb
@@ -33,8 +33,7 @@ module AccreditedRepresentativePortal
     def user_account
       @user_account ||=
         RepresentativeUserAccount.find(user_account_uuid).tap do |account|
-          account.set_email(email)
-          account.set_all_emails(all_emails) if Flipper.enabled?(:accredited_representative_portal_self_service_auth)
+          account.set_all_emails(all_emails)
         end
     end
 
