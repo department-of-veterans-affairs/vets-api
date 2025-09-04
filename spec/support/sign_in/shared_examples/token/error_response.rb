7979636c68800e60a7eb3f51a51f934ad4f1dc18
John Bramley <bramleyjl@gmail.com>
2025-09-04 14:08:49 -0600

    updates & spec fixes

--- spec/support/sign_in/shared_examples/token/error_response.rb
+++ spec/support/sign_in/shared_examples/token/error_response.rb
@@ -4,7 +4,7 @@ RSpec.shared_examples 'token_error_response' do
   let(:expected_error_json) { { 'errors' => expected_error } }
   let(:statsd_token_failure) { SignIn::Constants::Statsd::STATSD_SIS_TOKEN_FAILURE }
   let(:expected_error_log) { '[SignInService] [V0::SignInController] token error' }
-  let(:expected_error_context) { { errors: expected_error.to_s } }
+  let(:expected_error_context) { { errors: expected_error.to_s, grant_type: grant_type_value } }
 
   it 'renders expected error' do
     expect(JSON.parse(subject.body)).to eq(expected_error_json)
