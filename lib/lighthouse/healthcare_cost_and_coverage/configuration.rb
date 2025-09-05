20aba055fc974b4c98f9458de2b95358f770b29d
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-09-05 09:04:16 -0400

    114271 Financial Management Lighthouse Healthcare Cost and Coverage Invoice

---
+++ lib/lighthouse/healthcare_cost_and_coverage/configuration.rb
@@ -0,0 +1,112 @@
+# frozen_string_literal: true
+
+require 'common/client/configuration/rest'
+require 'lighthouse/auth/client_credentials/service'
+
+module Lighthouse
+  module HealthcareCostAndCoverage
+    class Configuration < Common::Client::Configuration::REST
+      self.read_timeout = Settings.lighthouse.healthcare_cost_and_coverage.timeout || 30
+
+      # Paths relative to Settings.lighthouse.healthcare_cost_and_coverage.host
+      API_PATH   = 'services/health-care-costs-coverage/v0'.freeze
+      TOKEN_PATH = 'oauth2/health-care-costs-coverage/system/v1/token'.freeze
+
+      ##
+      # Settings block for convenience
+      def settings
+        Settings.lighthouse.healthcare_cost_and_coverage
+      end
+
+      ##
+      # Base host (e.g., https://sandbox-api.va.gov)
+      def base_path(host = nil)
+        (host || settings.host).to_s
+      end
+
+      ##
+      # Full API base (FHIR endpoints live under here)
+      def base_api_path(host = nil)
+        "#{base_path(host)}/#{API_PATH}"
+      end
+
+      ##
+      # Service name (for breakers/metrics)
+      def service_name
+        'HealthcareCostAndCoverage'
+      end
+
+      ##
+      # GET with auth header (bearer is minted using ICN -> launch)
+      def get(path, options = {})
+        connection.get(path, options[:params], { 'Authorization' => "Bearer #{access_token(options)}" })
+      end
+
+      ##
+      # POST with auth header (not commonly needed for read-only, but here for parity)
+      def post(path, body, options = {})
+        connection.post(path, body, { 'Authorization' => "Bearer #{access_token(options)}" })
+      end
+
+      ##
+      # Faraday connection configured for FHIR
+      def connection
+        @conn ||= Faraday.new(base_api_path, headers: base_request_headers, request: request_options) do |faraday|
+          faraday.use(:breakers, service_name:)
+          faraday.use Faraday::Response::RaiseError
+
+          faraday.response :json, content_type: /\bjson\b/
+
+          faraday.response :betamocks if settings.use_mocks
+
+          faraday.adapter Faraday.default_adapter
+        end
+      end
+
+      private
+
+      def base_request_headers
+        super.merge('Accept' => 'application/fhir+json')
+      end
+
+      def access_token(options = {})
+        return nil if settings.use_mocks && !Settings.betamocks.recording
+
+        token_service(
+          options[:client_id],
+          options[:rsa_key],
+          options[:aud_claim_url],
+          options[:host],
+          options[:scopes]
+        ).get_token(auth_params(options))
+      end
+
+      # Build auth params for the token request
+      # Expects :icn in options; converts to Base64 launch JSON.
+      def auth_params(options)
+        icn = options[:icn]
+        raise ArgumentError, 'icn is required to mint a HCCC token' if icn.blank?
+
+        launch = Base64.strict_encode64({ patient: icn }.to_json)
+        { launch: launch }
+      end
+
+      # Construct the Auth::ClientCredentials::Service using settings (or caller overrides)
+      def token_service(client_id, rsa_key, aud_claim_url = nil, host = nil, scopes = nil)
+        s = settings
+        host ||= base_path
+        url  = "#{host}/#{TOKEN_PATH}"
+
+        client_id    ||= s.access_token.client_id
+        rsa_key      ||= s.access_token.rsa_key
+        aud_claim_url ||= s.access_token.aud_claim_url
+        scopes       ||= Array(s.scopes)
+
+        # Reuse/cached by service_name (includes scopes so ChargeItem+Invoice can share)
+        @token_service ||= Auth::ClientCredentials::Service.new(
+          url, scopes, client_id, aud_claim_url, rsa_key, 'hccc'
+        )
+      end
+    end
+  end
+end
