77cac9a784b4910b07c7bf9b3f1a9225fbffac2d
broskj <170366505+brostk@users.noreply.github.com>
2025-09-08 11:27:21 -0400

    add ignored columns

--- app/models/ivc_champva_form.rb
+++ app/models/ivc_champva_form.rb
@@ -3,6 +3,9 @@
 class IvcChampvaForm < ApplicationRecord
   validates :form_uuid, presence: true
 
+  # Ignore old unencrypted columns during transition period
+  self.ignored_columns += %w[first_name last_name email]
+
   has_kms_key
   has_encrypted :ves_request_data, key: :kms_key, **lockbox_options
   has_encrypted :first_name, key: :kms_key, **lockbox_options
