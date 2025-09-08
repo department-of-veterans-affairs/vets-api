49cefeba5f9f18bb2cb919ba6f6b70d64bdc0978
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-09-08 05:59:25 -0400

    Updates

--- lib/lighthouse/healthcare_cost_and_coverage/configuration.rb
+++ lib/lighthouse/healthcare_cost_and_coverage/configuration.rb
@@ -8,48 +8,33 @@ module Lighthouse
     class Configuration < Common::Client::Configuration::REST
       self.read_timeout = Settings.lighthouse.healthcare_cost_and_coverage.timeout || 30
 
-      # Paths relative to Settings.lighthouse.healthcare_cost_and_coverage.host
       API_PATH   = 'services/health-care-costs-coverage/v0'
       TOKEN_PATH = 'oauth2/health-care-costs-coverage/system/v1/token'
 
-      ##
-      # Settings block for convenience
       def settings
         Settings.lighthouse.healthcare_cost_and_coverage
       end
 
-      ##
-      # Base host (e.g., https://sandbox-api.va.gov)
       def base_path(host = nil)
         (host || settings.host).to_s
       end
 
-      ##
-      # Full API base (FHIR endpoints live under here)
       def base_api_path(host = nil)
         "#{base_path(host)}/#{API_PATH}"
       end
 
-      ##
-      # Service name (for breakers/metrics)
       def service_name
         'HealthcareCostAndCoverage'
       end
 
-      ##
-      # GET with auth header (bearer is minted using ICN -> launch)
       def get(path, options = {})
         connection.get(path, options[:params], { 'Authorization' => "Bearer #{access_token(options)}" })
       end
 
-      ##
-      # POST with auth header (not commonly needed for read-only, but here for parity)
       def post(path, body, options = {})
         connection.post(path, body, { 'Authorization' => "Bearer #{access_token(options)}" })
       end
 
-      ##
-      # Faraday connection configured for FHIR
       def connection
         @conn ||= Faraday.new(base_api_path, headers: base_request_headers, request: request_options) do |faraday|
           faraday.use(:breakers, service_name:)
@@ -81,8 +66,6 @@ module Lighthouse
         ).get_token(auth_params(options))
       end
 
-      # Build auth params for the token request
-      # Expects :icn in options; converts to Base64 launch JSON.
       def auth_params(options)
         icn = options[:icn]
         raise ArgumentError, 'icn is required to mint a HCCC token' if icn.blank?
@@ -91,18 +74,18 @@ module Lighthouse
         { launch: }
       end
 
-      # Construct the Auth::ClientCredentials::Service using settings (or caller overrides)
       def token_service(client_id, rsa_key, aud_claim_url = nil, host = nil, scopes = nil)
         s = settings
         host ||= base_path
-        url  = "#{host}/#{TOKEN_PATH}"
+        url = "#{host}/#{TOKEN_PATH}"
 
         client_id ||= s.access_token.client_id
         rsa_key ||= s.access_token.rsa_key
         aud_claim_url ||= s.access_token.aud_claim_url
         scopes ||= Array(s.scopes)
-        # Reuse/cached by service_name (includes scopes so ChargeItem+Invoice can share)
-        @token_service ||= Auth::ClientCredentials::Service.new(
+
+        # no memoization; let Redis cache the token
+        Auth::ClientCredentials::Service.new(
           url, scopes, client_id, aud_claim_url, rsa_key, 'hccc'
         )
       end
