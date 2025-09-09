d38dd874d45712a33476f810c32c9af99a3c279b
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-09-09 16:38:10 -0600

    clean up document controller

--- modules/travel_pay/app/controllers/travel_pay/v0/documents_controller.rb
+++ modules/travel_pay/app/controllers/travel_pay/v0/documents_controller.rb
@@ -86,29 +86,6 @@ module TravelPay
         )
       end
 
-      def verify_feature_flag_enabled!
-        return if Flipper.enabled?(:travel_pay_enable_complex_claims, @current_user)
-
-        message = 'Travel Pay create document unavailable per feature toggle'
-        Rails.logger.error(message:)
-        raise Common::Exceptions::ServiceUnavailable, message:
-      end
-
-      def validate_claim_id_exists!(claim_id)
-        # NOTE: In request specs, you can’t make params[:claim_id] truly missing because
-        # it’s part of the URL path and Rails routing prevents that.
-        raise Common::Exceptions::BadRequest.new(detail: 'Claim ID is required') if claim_id.blank?
-
-        # ensure claim ID is the right format, allowing any version
-        uuid_all_version_format = /\A[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[89ABCD][0-9A-F]{3}-[0-9A-F]{12}\z/i
-
-        unless uuid_all_version_format.match?(claim_id)
-          raise Common::Exceptions::BadRequest.new(
-            detail: 'Claim ID is invalid'
-          )
-        end
-      end
-
       def validate_document_exists!(document)
         return if document.present?
 
