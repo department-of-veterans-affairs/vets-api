59203fa732737a8ecfeedb68f87cde865a0163b8
Rockwell Windsor Rice <129893414+rockwellwindsor-va@users.noreply.github.com>
2025-09-09 15:29:42 -0500

    Api 47337 use pdf generator v1 (#23730)
    * API-47337-use-pdf-generator-v1 - Move out of v2 namespace
    * Moves the PDF Generator sidekiq job out of the v2 space
    * Updates related tests and file paths
    * API-47337-use-pdf-generator-v1
    * Updates v1 claims controller to kick off the PDF Generator sidekiq job if the FES v1 flipper is enabled
    * API-47337-use-pdf-generator-v1 - Moves into the v1 namespace
    * Updates paths and related tests

--- modules/claims_api/spec/controllers/v1/disability_compensation_controller_spec.rb
+++ modules/claims_api/spec/controllers/v1/disability_compensation_controller_spec.rb
@@ -19,4 +19,64 @@ RSpec.describe ClaimsApi::V1::Forms::DisabilityCompensationController, type: :co
                                                    source: error[0][:key] })
     end
   end
+
+  describe '#submit_form_526' do
+    let(:claim_date) { (Time.zone.today - 1.day).to_s }
+    let(:auto_cest_pdf_generation_disabled) { false }
+    let(:claim_data) do
+      temp = Rails.root.join('modules', 'claims_api', 'spec', 'fixtures', 'form_526_json_api.json').read
+      temp = JSON.parse(temp)
+      temp['data']['attributes']['autoCestPDFGenerationDisabled'] = auto_cest_pdf_generation_disabled
+      temp['data']['attributes']['claimDate'] = claim_date
+      temp['data']['attributes']['applicationExpirationDate'] = (Time.zone.today + 1.day).to_s
+
+      temp.to_json
+    end
+
+    let(:claim) { create(:auto_established_claim, form_data: JSON.parse(claim_data)) }
+
+    let(:veteran) do
+      OpenStruct.new(
+        middle_name: 'William',
+        mpi: OpenStruct.new(
+          icn: '1012861229V078999'
+        )
+      )
+    end
+
+    let(:token) do
+      OpenStruct.new(
+        payload: OpenStruct.new(
+          cid: '75648392047777685'
+        )
+      )
+    end
+
+    context 'using the PDF Generator' do
+      before do
+        subject.instance_variable_set(:@json_body, JSON.parse(claim_data)) # mock for form_attributes method calls
+        allow_any_instance_of(
+          ClaimsApi::RevisedDisabilityCompensationValidations
+        ).to receive(:validate_form_526_submission_values!).and_return(nil) # mock passing custom validations
+        allow_any_instance_of(
+          described_class
+        ).to receive(:validate_veteran_identifiers).with(anything).and_return(nil) # valid veteran
+        allow(ClaimsApi::AutoEstablishedClaim).to receive(:create).with(any_args).and_return(claim)
+        allow(Flipper).to receive(:enabled?).with(:lighthouse_claims_api_v1_enable_FES).and_return(true)
+        allow(ClaimsApi::V1::DisabilityCompensationPdfGenerator).to receive(:perform_async)
+        allow(ClaimsApi::AutoEstablishedClaimSerializer).to receive(:new).with(claim).and_return(double(as_json: {}))
+        allow_any_instance_of(
+          described_class
+        ).to receive_messages(validate_json_schema: nil, validate_initial_claim: nil,
+                              target_veteran: veteran, token:, auth_headers: {}, render: nil)
+      end
+
+      it 'calls the PDF Generator Sidekiq job with the expected params when the flipper is enabled' do
+        subject.send(:submit_form_526) # rubocop:disable Naming/VariableNumber
+
+        expect(ClaimsApi::V1::DisabilityCompensationPdfGenerator).to have_received(:perform_async)
+          .with(claim.id, 'W') # 'W' here mimics the method call in the controller to pass just the middle initial
+      end
+    end
+  end
 end
