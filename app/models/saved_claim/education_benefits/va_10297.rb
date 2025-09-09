73f71cec9ce5ebbb1687f139e843e9ecfd7c0bda
nfstern02 <72567812+nfstern02@users.noreply.github.com>
2025-09-09 02:58:34 -0500

    fix code and spec errors found by testing

--- app/models/saved_claim/education_benefits/va_10297.rb
+++ app/models/saved_claim/education_benefits/va_10297.rb
@@ -17,7 +17,7 @@ class SavedClaim::EducationBenefits::VA10297 < SavedClaim::EducationBenefits
   private
 
   def send_confirmation_email(parsed_form_data, email)
-    if Flipper.enabled?(:form1995_confirmation_email_with_silent_failure_processing)
+    if Flipper.enabled?(:form10297_confirmation_email_with_silent_failure_processing)
       # this method is in the parent class
       send_education_benefits_confirmation_email(email, parsed_form, {})
     else
--- spec/models/saved_claim/education_benefits/va10297_spec.rb
+++ spec/models/saved_claim/education_benefits/va10297_spec.rb
@@ -13,11 +13,14 @@ RSpec.describe SavedClaim::EducationBenefits::VA10297 do
   describe '#after_submit' do
     let(:user) { create(:user) }
 
+    before do
+      allow(VANotify::EmailJob).to receive(:perform_async)
+    end
+
     describe 'confirmation email for 10297' do
       context 'when the form22_10297_confirmation_email feature flag is disabled' do
         it 'does not send a confirmation email' do
           allow(Flipper).to receive(:enabled?).with(:form22_10297_confirmation_email).and_return(false)
-          allow(VANotify::EmailJob).to receive(:perform_async)
 
           subject = create(:va10297_simple_form)
           subject.after_submit(user)
@@ -29,7 +32,6 @@ RSpec.describe SavedClaim::EducationBenefits::VA10297 do
       context 'when the form22_10297_confirmation_email feature flag is enabled' do
         before do
           allow(Flipper).to receive(:enabled?).with(:form22_10297_confirmation_email).and_return(true)
-          allow(VANotify::EmailJob).to receive(:perform_async)
         end
 
         context 'when the form10297_confirmation_email_with_silent_failure_processing feature flag is disabled' do
@@ -69,12 +71,9 @@ RSpec.describe SavedClaim::EducationBenefits::VA10297 do
             }
           end
 
-          it 'sends an email with the silent failure callback parameters' do
-            allow(Flipper)
-              .to receive(:enabled?)
-              .with(:form10297_confirmation_email_with_silent_failure_processing)
-              .and_return(true)
+          before { Flipper.enable(:form10297_confirmation_email_with_silent_failure_processing) }
 
+          it 'sends an email with the silent failure callback parameters' do
             subject = create(:va10297_simple_form)
             confirmation_number = subject.education_benefits_claim.confirmation_number
 
@@ -87,7 +86,9 @@ RSpec.describe SavedClaim::EducationBenefits::VA10297 do
                 'date_submitted' => Time.zone.today.strftime('%B %d, %Y'),
                 'confirmation_number' => confirmation_number,
                 'regional_office_address' => "P.O. Box 4616\nBuffalo, NY 14240-4616"
-              }
+              },
+              Settings.vanotify.services.va_gov.api_key,
+              callback_options
             )
           end
         end
