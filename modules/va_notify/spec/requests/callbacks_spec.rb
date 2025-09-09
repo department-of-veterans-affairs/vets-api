5869b36c784f68d70c5b8b69d3f3af9d28800cd6
Sarah Wheeler <sarah.wheeler@wilcore.io>
2025-09-09 14:28:52 -0700

    Linting fix

--- modules/va_notify/spec/requests/callbacks_spec.rb
+++ modules/va_notify/spec/requests/callbacks_spec.rb
@@ -43,8 +43,13 @@ RSpec.describe 'VANotify Callbacks', type: :request do
 
           expect(Rails.logger).to have_received(:info).with(
             "va_notify callbacks - Updating notification: #{notification.id}",
-            { notification_id: notification.id, source_location: 'some_location', template_id:, callback_metadata: 'some_callback_metadata',
-              status_reason: '', status: 'delivered' }
+            {
+              notification_id: notification.id,
+              source_location: 'some_location',
+              template_id:,
+              callback_metadata: 'some_callback_metadata',
+              status_reason: '', status: 'delivered'
+            }
           )
           expect(response.body).to include('success')
           notification.reload
