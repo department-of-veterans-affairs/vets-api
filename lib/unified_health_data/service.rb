6de499d61b1dc1abef44447f33329a9a227c0d56
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-09-10 10:58:06 -0400

    Update get_prescriptions method to include date range parameters

--- lib/unified_health_data/service.rb
+++ lib/unified_health_data/service.rb
@@ -59,11 +59,11 @@ module UnifiedHealthData
       end
     end
 
-    def get_prescriptions
+    def get_prescriptions(start_date:, end_date:)
       with_monitoring do
         headers = { 'Authorization' => fetch_access_token, 'x-api-key' => config.x_api_key }
         patient_id = @user.icn
-        path = "#{config.base_path}prescriptions?patientId=#{patient_id}"
+        path = "#{config.base_path}medicalrecords/medications?patientId=#{patient_id}&startDate=#{start_date}&endDate=#{end_date}"
 
         response = perform(:get, path, nil, headers)
         body = parse_response_body(response.body)
