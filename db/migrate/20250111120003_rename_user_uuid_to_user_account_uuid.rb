d48107962dcc7ee995d2d364a8e2a762e5ed1423
Jennica Stiehl <jennica.stiehl@oddball.io>
2025-09-11 13:48:52 -0600

    TEST 1: Remove columns with model changes (should WARN)
    Testing MigrationIsolator with:
    - Migration removing columns with safety_assured
    - Model file with ignored_columns (Strong Migrations pattern)
    
    Expected: WARNING message explaining these are allowed changes

--- db/migrate/20250111120003_rename_user_uuid_to_user_account_uuid.rb
+++
@@ -1,7 +0,0 @@
-class RenameUserUuidToUserAccountUuid < ActiveRecord::Migration[7.1]
-  def change
-    safety_assured do
-      rename_column :saved_claims, :user_uuid, :user_account_uuid
-    end
-  end
-end
