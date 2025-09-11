a890b7850188a75a9cc373b8b6bcb2515c392539
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-09-11 11:55:06 -0500

    added if_not_exists check

--- db/migrate/20250910170038_add_index_to_record_metadata_needs_kms_rotation.rb
+++ db/migrate/20250910170038_add_index_to_record_metadata_needs_kms_rotation.rb
@@ -4,6 +4,6 @@ class AddIndexToRecordMetadataNeedsKmsRotation < ActiveRecord::Migration[7.2]
   disable_ddl_transaction!
 
   def change
-    add_index :claims_api_record_metadata, :needs_kms_rotation, algorithm: :concurrently
+    add_index :claims_api_record_metadata, :needs_kms_rotation, algorithm: :concurrently, if_not_exists: true
   end
 end
