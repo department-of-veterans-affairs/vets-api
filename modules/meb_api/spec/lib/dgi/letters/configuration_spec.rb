3d8b8316b3a03e352ec2ada8b4c211ae1effc24d
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-09-11 12:50:34 -0400

    DGI letters service spec memoization issue

--- modules/meb_api/spec/lib/dgi/letters/configuration_spec.rb
+++ modules/meb_api/spec/lib/dgi/letters/configuration_spec.rb
@@ -15,6 +15,13 @@ describe MebApi::DGI::Letters::Configuration do
     )
   end
 
+  after(:each) do
+    cfg = described_class.instance
+    %i[@connection @conn @faraday].each do |ivar|
+      cfg.remove_instance_variable(ivar) if cfg.instance_variable_defined?(ivar)
+    end
+  end
+
   context 'when mock is disabled' do
     let(:mock_enabled) { false }
 
