e04dbd56a2fc7d88582de482fe16d77efb067469
Monsoud Zanaty <227215732+MonsoudZanaty@users.noreply.github.com>
2025-09-12 17:23:52 -0400

    broke up the upload method in the form526_establishment_service to handle both verisons, i tried an ugly way of throwing two if statements in and it broke alot of test i think trying to upload twice

--- modules/claims_api/app/services/claims_api/disability_compensation/form526_establishment_service.rb
+++ modules/claims_api/app/services/claims_api/disability_compensation/form526_establishment_service.rb
@@ -17,49 +17,24 @@ module ClaimsApi
 
       def upload(claim_id)
         auto_claim = get_claim(claim_id)
-
         log_job_progress(claim_id, 'Form526 Establishment service started', auto_claim.transaction_id)
-
         update_auth_headers(auto_claim) if auto_claim.transaction_id.present?
 
-        if Flipper.enabled?(:lighthouse_claims_api_v2_enable_FES)
-          fes_data = get_fes_data(auto_claim)
-          log_job_progress(claim_id, 'Submitting mapped data to FES', auto_claim.transaction_id)
-          fes_res = fes_service.submit(auto_claim, fes_data, ASYNC)
-          log_job_progress(claim_id, "Successfully submitted to FES with response: #{fes_res}",
-                           auto_claim.transaction_id)
-          # update with the evss_id returned
-          auto_claim.update!(evss_id: fes_res[:claimId])
+        case claim_version(auto_claim)
+        when :v1
+          if Flipper.enabled?(:lighthouse_claims_api_v1_enable_FES)
+            submit_fes(claim_id, auto_claim, get_v1_fes_data(auto_claim), 'v1')
+          else
+            submit_evss(claim_id, auto_claim, get_evss_data(auto_claim), 'v1')
+          end
+        when :v2
+          if Flipper.enabled?(:lighthouse_claims_api_v2_enable_FES)
+            submit_fes(claim_id, auto_claim, get_fes_data(auto_claim), 'v2')
+          else
+            submit_evss(claim_id, auto_claim, get_evss_data(auto_claim), 'v2')
+          end
         else
-          evss_data = get_evss_data(auto_claim)
-          log_job_progress(claim_id, 'Submitting mapped data to Form 526 Establishment service',
-                           auto_claim.transaction_id)
-          evss_res = evss_service.submit(auto_claim, evss_data, ASYNC)
-          log_job_progress(claim_id, "Successfully submitted to 526 Establishment service with response: #{evss_res}",
-                           auto_claim.transaction_id)
-          # update with the evss_id returned
-          auto_claim.update!(evss_id: evss_res[:claimId])
-        end
-
-        # This is not pretty but an easy way to do it as a temp job before we remove v1
-
-        if Flipper.enabled?(:lighthouse_claims_api_v1_enable_FES)
-          fes_data = get_v1_fes_data(auto_claim)
-          log_job_progress(claim_id, 'Submitting mapped data to FES', auto_claim.transaction_id)
-          fes_res = fes_service.submit(auto_claim, fes_data, ASYNC)
-          log_job_progress(claim_id, "Successfully submitted to FES with response: #{fes_res}",
-                           auto_claim.transaction_id)
-          # update with the evss_id returned
-          auto_claim.update!(evss_id: fes_res[:claimId])
-        else
-          evss_data = get_evss_data(auto_claim)
-          log_job_progress(claim_id, 'Submitting mapped data to Form 526 Establishment service',
-                           auto_claim.transaction_id)
-          evss_res = evss_service.submit(auto_claim, evss_data, ASYNC)
-          log_job_progress(claim_id, "Successfully submitted to 526 Establishment service with response: #{evss_res}",
-                           auto_claim.transaction_id)
-          # update with the evss_id returned
-          auto_claim.update!(evss_id: evss_res[:claimId])
+          raise ArgumentError, "Unknown claim version for claim_id=#{claim_id}"
         end
       rescue => e
         auto_claim.status = ClaimsApi::AutoEstablishedClaim::ERRORED
@@ -76,6 +51,33 @@ module ClaimsApi
         updated_auth_headers = auto_claim.auth_headers
         updated_auth_headers['va_eauth_service_transaction_id'] = auto_claim.transaction_id
         auto_claim.update!(auth_headers: updated_auth_headers)
+      end
+
+      def claim_version(auto_claim)
+        version_control = auto_claim.respond_to?(:api_version) ? auto_claim.api_version&.to_sym : nil
+        return version_control if version_control
+
+        data = auto_claim.form_data || {}
+        return :v2 if data.key?('veteranIdentification') || data.key?(:veteranIdentification)
+        return :v1 if data.key?('veteran') || data.key?(:veteran)
+
+        :v1
+      end
+
+      def submit_fes(claim_id, auto_claim, payload, tag)
+        log_job_progress(claim_id, "Submitting mapped data to FES (#{tag})", auto_claim.transaction_id)
+        fes_res = fes_service.submit(auto_claim, payload, ASYNC)
+        log_job_progress(claim_id, "Submitted to FES (#{tag}) with response: #{fes_res}", auto_claim.transaction_id)
+        auto_claim.update!(evss_id: fes_res[:claimId] || fes_res['claimId'])
+      end
+
+      def submit_evss(claim_id, auto_claim, payload, tag)
+        log_job_progress(claim_id, "Submitting mapped data to 526 Establishment service (#{tag})",
+                         auto_claim.transaction_id)
+        evss_res = evss_service.submit(auto_claim, payload, ASYNC)
+        log_job_progress(claim_id, "Submitted to 526 Establishment service (#{tag}) with response: #{evss_res}",
+                         auto_claim.transaction_id)
+        auto_claim.update!(evss_id: evss_res[:claimId] || evss_res['claimId'])
       end
 
       def get_evss_data(auto_claim)
