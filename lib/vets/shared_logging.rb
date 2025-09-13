2f7934712370102b0667f3619889984ed7364d92
Rajesh Jain <73859950+rjain21@users.noreply.github.com>
2025-09-13 14:48:37 -0400

    Another attempt to be backwards compatible

--- lib/vets/shared_logging.rb
+++ lib/vets/shared_logging.rb
@@ -55,24 +55,22 @@ module Vets
       # guards against nil exceptions!
       if exception.nil?
         Rails.logger.warn('[No Exception provided]')
-      elsif exception.is_a?(Common::Exceptions::BackendServiceException) # For backward compatibility
-        error_details = exception.errors.first.attributes.compact.reject { |_k, v| v.try(:empty?) }
-        log_message_to_rails(exception.message, level, error_details.merge(backtrace: exception.backtrace))
       else
         Rails.logger.send(level, exception)
       end
     end
 
     def normalize_shared_level(level, exception)
+      # If explicit level supplied (not nil), honor it directly
+      return level.to_s if level
+
       case exception
       when Pundit::NotAuthorizedError
         'info'
       when Common::Exceptions::BaseError
-        # could change this attribute to log_level
-        # to make clear it is not just a Sentry concern
         exception.sentry_type.to_s
       else
-        level.to_s
+        'error'
       end
     end
 
