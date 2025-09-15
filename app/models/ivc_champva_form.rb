c58fb0d77b15e590224322def988393501965c45
broskj <170366505+brostk@users.noreply.github.com>
2025-09-15 15:03:45 -0400

    fix encryption step

--- app/models/ivc_champva_form.rb
+++ app/models/ivc_champva_form.rb
@@ -4,7 +4,7 @@ class IvcChampvaForm < ApplicationRecord
   validates :form_uuid, presence: true
 
   has_kms_key
-  has_encrypted :ves_request_data, migrating: true, key: :kms_key, **lockbox_options
+  has_encrypted :ves_request_data, key: :kms_key, **lockbox_options
   has_encrypted :first_name, key: migrate: true, :kms_key, **lockbox_options
   has_encrypted :last_name, key: migrate: true, :kms_key, **lockbox_options
   has_encrypted :email, key: migrate: true, :kms_key, **lockbox_options
