bd88fe7aeac2c23b4c7ac91db6c636cf5ccd7e98
broskj <170366505+brostk@users.noreply.github.com>
2025-09-15 14:31:32 -0400

    separate index addition into new migration

--- db/migrate/20250905195344_add_encryption_to_personal_data_ivc_champva_forms.rb
+++ db/migrate/20250905195344_add_encryption_to_personal_data_ivc_champva_forms.rb
@@ -1,14 +1,11 @@
 class AddEncryptionToPersonalDataIvcChampvaForms < ActiveRecord::Migration[7.2]
-    disable_ddl_transaction!
-  
-    def change
-      add_column :ivc_champva_forms, :first_name_ciphertext, :text
-      add_column :ivc_champva_forms, :last_name_ciphertext, :text
-      add_column :ivc_champva_forms, :email_ciphertext, :text
-  
-      # Add blind index for searchable encrypted email field
-      add_column :ivc_champva_forms, :email_bidx, :string
-      add_index :ivc_champva_forms, :email_bidx, algorithm: :concurrently
-    end
+  def change
+    add_column :ivc_champva_forms, :first_name_ciphertext, :text, if_not_exists: true
+    add_column :ivc_champva_forms, :last_name_ciphertext, :text, if_not_exists: true
+    add_column :ivc_champva_forms, :email_ciphertext, :text, if_not_exists: true
+    
+    # Add blind index for searchable encrypted email field
+    add_column :ivc_champva_forms, :email_bidx, :string, if_not_exists: true
   end
+end
   
