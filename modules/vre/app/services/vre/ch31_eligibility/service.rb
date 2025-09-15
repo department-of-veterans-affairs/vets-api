ee35367f264db550fff8d7c15d03d4a5c0133f6e
Jeff Marks <jeff.t.marks@gmail.com>
2025-09-14 20:20:00 -0600

    add 503

--- modules/vre/app/services/vre/ch31_eligibility/service.rb
+++ modules/vre/app/services/vre/ch31_eligibility/service.rb
@@ -5,14 +5,10 @@ module VRE
     class Service < VRE::Service
       configuration VRE::Ch31Eligibility::Configuration
 
-      # TO-DO: coordinate not found error with RES
-      RES_NOT_FOUND_STATUS = 500
-      ERRORS_KEY = 'errorMessageList'
-      NOT_FOUND_MESSAGE = 'Internal server error occurred while retrieving data from MPI'
-
       class Ch31EligibilityError < StandardError; end
 
       STATSD_KEY_PREFIX = 'api.res.eligibility'
+      SERVICE_UNAVAILABLE = 'APNX-1-4187-000'
 
       def initialize(icn)
         super()
@@ -30,11 +26,7 @@ module VRE
         VRE::Ch31Eligibility::Response.new(raw_response.status, raw_response)
       rescue Common::Exceptions::BackendServiceException => e
         log_error(e)
-        if record_not_found?(e)
-          raise e.class.new('RES_CH31_ELIGIBILITY_404', e.response_values)
-        else
-          raise e
-        end
+        raise e unless service_downtime?(e)
       end
 
       private
@@ -43,14 +35,15 @@ module VRE
         'chapter31-eligibility-details-search'
       end
 
-      def record_not_found?(e)
-        e.original_status == RES_NOT_FOUND_STATUS &&
-        e.original_body[ERRORS_KEY]&.any? { |err| err.include?(NOT_FOUND_MESSAGE)}
-      end
-
       def log_error(e)
         Rails.logger.error(e)
         Rails.logger.error({ messages: e.original_body[ERRORS_KEY] })
+      end
+
+      def service_unavailable?(e)
+        return unless e.original_body['error'] == SERVICE_UNAVAILABLE
+
+        raise e.class.new('RES_CH31_ELIGIBILITY_503', e.response_values)
       end
     end
   end
