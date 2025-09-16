4bf1b0b7336f1c19e30236e4df2e25b71a4195db
Jennica Stiehl <jennica.stiehl@oddball.io>
2025-09-16 09:37:53 -0600

    Clean up: Remove test scenario 1 files
    Test confirmed that model + migration correctly triggers ERROR

--- app/models/user_test.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class UserTest < ApplicationRecord
-  # Following Strong Migrations pattern - but this should NOT be in same PR as migration!
-  self.ignored_columns += ['legacy_field']
-  
-  validates :email, presence: true
-end
--- db/migrate/20250113_test_remove_legacy_column.rb
+++
@@ -1,7 +0,0 @@
-class TestRemoveLegacyColumn < ActiveRecord::Migration[7.1]
-  def change
-    safety_assured do
-      remove_column :users, :legacy_field, :string
-    end
-  end
-end
