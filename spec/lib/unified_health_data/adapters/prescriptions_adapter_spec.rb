a3124d1ba9c61382e12b21d66eab50d884e15056
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-09-16 12:24:33 -0600

    Add prescription_source attribute and update adapters to include it

--- spec/lib/unified_health_data/adapters/prescriptions_adapter_spec.rb
+++ spec/lib/unified_health_data/adapters/prescriptions_adapter_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'unified_health_data/models/prescription_attributes'
 require 'unified_health_data/models/prescription'
 require 'unified_health_data/adapters/prescriptions_adapter'
 require 'unified_health_data/adapters/oracle_health_prescription_adapter'
@@ -271,7 +270,7 @@ describe UnifiedHealthData::Adapters::PrescriptionsAdapter do
       context 'when parsing raises an error' do
         let(:adapter_with_error) do
           adapter = described_class.new
-          allow(adapter).to receive(:build_prescription_attributes).and_raise(StandardError, 'Test error')
+          allow(adapter).to receive(:extract_refill_date).and_raise(StandardError, 'Test error')
           adapter
         end
 
