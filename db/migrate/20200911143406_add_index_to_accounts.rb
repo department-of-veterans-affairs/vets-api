541f4122dd17b381463e2cef333993daa73c67a7
RachalCassity <rachal.cassity@oddball.io>
2025-09-16 10:19:50 -0500

    undo migration changes

--- db/migrate/20200911143406_add_index_to_accounts.rb
+++ db/migrate/20200911143406_add_index_to_accounts.rb
@@ -4,6 +4,6 @@ class AddIndexToAccounts < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def change
-    add_index :accounts, :icn, algorithm: :concurrently, name: 'index_accounts_on_icn'
+    add_index :accounts, :icn, algorithm: :concurrently
   end
 end
