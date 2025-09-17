29dd589cefd41600792ff2d1bd8d70f2f8a2b87c
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-09-17 09:36:44 -0700

    [#115850] Travel Pay, Get expense (#24001)
    * Initial commit
    
    * Only support other
    
    * Update tests
    
    * Split log call into multiple
    
    * Lint error fix
    
    * Update log to use heredoc
    
    * Remove method for expenses GET url construction
    
    * Remove begin for expenses controllers
    
    * Remove begin from expenses service methods
    
    * Add uuid validation for expense id
    
    * Add test case for missing expense id controller handling
    
    * Add cassettes for get/create expense
    
    * Use correct base url for cassettes
    
    * Generify the error handling condition for expenses controllers
    
    * Avoid allow_any_instance_of in auth manager mock
    
    * Remove redundant test cases and their cassettes

--- modules/travel_pay/config/routes.rb
+++ modules/travel_pay/config/routes.rb
@@ -7,6 +7,8 @@ TravelPay::Engine.routes.draw do
     scope '/claims/:claim_id', constraints: { claim_id: %r{[^/]+} } do
       resources :documents, only: %i[index show create destroy]
       post 'expenses/:expense_type', to: 'expenses#create', constraints: { expense_type: %r{[^/]+} }
+      get 'expenses/:expense_type/:expense_id', to: 'expenses#show',
+                                                constraints: { expense_type: %r{[^/]+}, expense_id: %r{[^/]+} }
     end
 
     resources :complex_claims, only: %i[create], param: :claim_id do
