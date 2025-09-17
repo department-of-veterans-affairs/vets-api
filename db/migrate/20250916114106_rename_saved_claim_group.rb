263db20b6e2c55d5dfe5b1f35a5d808ac683ebac
Wayne Weibel <wayne.weibel@adhocteam.us>
2025-09-17 12:57:51 -0400

    safety assured and schema

--- db/migrate/20250916114106_rename_saved_claim_group.rb
+++ db/migrate/20250916114106_rename_saved_claim_group.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+# add 's' to the table name so no override in the class is needed
 class RenameSavedClaimGroup < ActiveRecord::Migration[7.2]
 
   def change
-    rename_table :saved_claim_group, :saved_claim_groups
+    safety_assured { rename_table :saved_claim_group, :saved_claim_groups }
   end
 
 end
