52e0d7ccbc6fe9a52eb6eeffad010a9203c153e8
Liz Townsend <liz.townsend@adhocteam.us>
2025-09-18 07:45:02 -0500

    use new serializer in controllers

--- modules/mobile/app/controllers/mobile/v1/labs_and_tests_controller.rb
+++ modules/mobile/app/controllers/mobile/v1/labs_and_tests_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'unified_health_data/service'
-require 'lab_or_test_serializer'
+require 'unified_health_data/serializers/lab_or_test_serializer'
 
 module Mobile
   module V1
@@ -12,7 +12,7 @@ module Mobile
         start_date = params[:startDate]
         end_date = params[:endDate]
         labs = service.get_labs(start_date:, end_date:)
-        response = { data: labs.map { |record| LabOrTestSerializer.serialize(record) } }
+        response = UnifiedHealthData::LabOrTestSerializer.new(labs)
         render json: response
       end
 
--- modules/my_health/app/controllers/my_health/v2/labs_and_tests_controller.rb
+++ modules/my_health/app/controllers/my_health/v2/labs_and_tests_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'unified_health_data/service'
-require 'lab_or_test_serializer'
+require 'unified_health_data/serializers/lab_or_test_serializer'
 
 module MyHealth
   module V2
@@ -12,7 +12,11 @@ module MyHealth
         start_date = params[:start_date]
         end_date = params[:end_date]
         labs = service.get_labs(start_date:, end_date:)
-        render json: labs.map { |record| LabOrTestSerializer.serialize(record) }
+        # TODO: we will probably want to return the full data wrapper here
+        # But that will require FE changes as well
+        serialized_labs = UnifiedHealthData::LabOrTestSerializer.new(labs).serializable_hash[:data]
+        render json: serialized_labs,
+               status: :ok
       end
 
       private
