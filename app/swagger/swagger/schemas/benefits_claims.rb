f116ff6afe204e3dfb67da7df6872a4411b98385
Joshua <joshing_you1@yahoo.com>
2025-09-18 14:16:04 -0400

    Updated swagger schema

--- app/swagger/swagger/schemas/benefits_claims.rb
+++ app/swagger/swagger/schemas/benefits_claims.rb
@@ -6,19 +6,19 @@ module Swagger
       include Swagger::Blocks
 
       swagger_schema :EvidenceSubmission do
-        property :acknowledgement_date, type: :string
+        property :acknowledgement_date, type: :string, nullable: true
         property :claim_id, type: :integer
         property :created_at, type: :string
-        property :delete_date, type: :string
+        property :delete_date, type: :string, nullable: true
         property :document_type, type: :string
-        property :failed_date, type: :string
+        property :failed_date, type: :string, nullable: true
         property :file_name, type: :string
         property :id, type: :integer
         property :lighthouse_upload, type: :boolean
-        property :tracked_item_id, type: :integer
+        property :tracked_item_id, type: :integer, nullable: true
         property :tracked_item_display_name, type: :string
         property :upload_status, type: :string
-        property :va_notify_status, type: :string
+        property :va_notify_status, type: :string, nullable: true
       end
     end
   end
