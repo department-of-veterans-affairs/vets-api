37f82a059987550a2e74109dc6d4311da0149e3c
rockwellwindsor-va <129893414+rockwellwindsor-va@users.noreply.github.com>
2025-09-18 14:18:17 -0500

    Adds mapping for confinements
    * Maps confinement dates
    
    	modified:   modules/claims_api/lib/claims_api/v1/disability_compensation_pdf_mapper.rb
    	modified:   modules/claims_api/spec/lib/claims_api/v1/disability_compensation_pdf_mapper_spec.rb

--- modules/claims_api/lib/claims_api/v1/disability_compensation_pdf_mapper.rb
+++ modules/claims_api/lib/claims_api/v1/disability_compensation_pdf_mapper.rb
@@ -410,6 +410,7 @@ module ClaimsApi
         set_pdf_data_for_service_information
 
         service_periods
+        confinements if @auto_claim.dig('serviceInformation', 'confinements')
       end
 
       def set_pdf_data_for_service_information
@@ -469,6 +470,33 @@ module ClaimsApi
           }
         end
         @pdf_data[:data][:attributes][:serviceInformation][:additionalPeriodsOfService] = additional_periods
+      end
+
+      def confinements
+        set_pdf_data_for_pow_confinement
+
+        confinement_periods
+      end
+
+      def set_pdf_data_for_pow_confinement
+        return if @pdf_data[:data][:attributes][:serviceInformation]&.key?(:prisonerOfWarConfinement)
+
+        @pdf_data[:data][:attributes][:serviceInformation][:prisonerOfWarConfinement] = {}
+      end
+
+      # 'confinementBeginDate' & 'confinementEndDate' are required via the schema if confinements are present
+      def confinement_periods
+        confinements_data = @auto_claim.dig('serviceInformation', 'confinements')
+
+        periods_of_confinement = []
+        confinements_data.each do |c|
+          begin_date = make_date_object(c['confinementBeginDate'], c['confinementBeginDate'].length)
+          end_date = make_date_object(c['confinementEndDate'], c['confinementEndDate'].length)
+
+          periods_of_confinement << { start: begin_date, end: end_date }
+        end
+        @pdf_data[:data][:attributes][:serviceInformation][:prisonerOfWarConfinement] =
+          { confinementDates: periods_of_confinement }
       end
     end
   end
