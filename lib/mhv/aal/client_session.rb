826efaf296a1c0fc98e83af9956176203a9321ed
Michael Moyer <87040148+mmoyer-va@users.noreply.github.com>
2025-09-18 14:22:56 -0400

    Removed user_id as it is in the parent class

--- lib/mhv/aal/client_session.rb
+++ lib/mhv/aal/client_session.rb
@@ -4,24 +4,18 @@ require 'common/client/session'
 
 module AAL
   class MRClientSession < Common::Client::Session
-    attribute :user_uuid, String
-
     redis_store REDIS_CONFIG[:aal_mr_store][:namespace]
     redis_ttl REDIS_CONFIG[:aal_mr_store][:each_ttl]
     redis_key :user_id
   end
 
   class RXClientSession < Common::Client::Session
-    attribute :user_uuid, String
-
     redis_store REDIS_CONFIG[:aal_rx_store][:namespace]
     redis_ttl REDIS_CONFIG[:aal_rx_store][:each_ttl]
     redis_key :user_id
   end
 
   class SMClientSession < Common::Client::Session
-    attribute :user_uuid, String
-
     redis_store REDIS_CONFIG[:aal_sm_store][:namespace]
     redis_ttl REDIS_CONFIG[:aal_sm_store][:each_ttl]
     redis_key :user_id
--- lib/rx/client_session.rb
+++ lib/rx/client_session.rb
@@ -4,8 +4,6 @@ require 'common/client/session'
 module Rx
   # Session model
   class ClientSession < Common::Client::Session
-    attribute :user_uuid, String
-
     redis_store REDIS_CONFIG[:rx_store][:namespace]
     redis_ttl 900
     redis_key :user_id
