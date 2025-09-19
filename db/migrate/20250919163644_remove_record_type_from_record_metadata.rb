32ca8fc6b99eda9de55e34b3d6d144b68f4304b6
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-09-19 15:18:53 -0500

    reverted new migrations, deleted existing record_metadata model

--- db/migrate/20250919163644_remove_record_type_from_record_metadata.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveRecordTypeFromRecordMetadata < ActiveRecord::Migration[7.2]
-  def change
-    safety_assured { remove_column :claims_api_record_metadata, :record_type, :string }
-  end
-end
--- db/migrate/20250919164027_allow_null_metadata_for_record_metadata.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AllowNullMetadataForRecordMetadata < ActiveRecord::Migration[7.2]
-  def change
-    change_column_null :claims_api_record_metadata, :metadata_ciphertext, true
-  end
-end
