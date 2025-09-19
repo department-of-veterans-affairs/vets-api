ee15fa394f01c97bb00ce2ccd0a31a331fa98064
Liz Townsend <liz.townsend@adhocteam.us>
2025-09-19 08:19:54 -0500

    .new for mhv controller too

--- modules/my_health/app/controllers/my_health/v2/labs_and_tests_controller.rb
+++ modules/my_health/app/controllers/my_health/v2/labs_and_tests_controller.rb
@@ -12,7 +12,10 @@ module MyHealth
         start_date = params[:start_date]
         end_date = params[:end_date]
         labs = service.get_labs(start_date:, end_date:)
-        render json: UnifiedHealthData::LabOrTestSerializer.serialize(labs).serializable_hash[:data]
+        # TODO: return full object - requires FE changes as well
+        serialized_labs = UnifiedHealthData::LabOrTestSerializer.new(labs).serializable_hash[:data]
+        render json: serialized_labs,
+               status: :ok
       end
 
       private
