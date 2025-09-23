24463bf9416efdbc6e0a1543aab69e2ee13ee734
Monsoud Zanaty <227215732+MonsoudZanaty@users.noreply.github.com>
2025-09-23 17:01:32 -0400

    first commit, adding some of the methods found in v2 that have the same mapping v1 fes needs

--- modules/claims_api/lib/claims_api/v1/disability_compensation_fes_mapper.rb
+++ modules/claims_api/lib/claims_api/v1/disability_compensation_fes_mapper.rb
@@ -28,6 +28,82 @@ module ClaimsApi
       def extract_claimant_participant_id
         @auto_claim.auth_headers.dig('dependent', 'participant_id')
       end
+
+      def veteran_identification_info
+        # this will be the parent method for all the mailing info
+      end
+
+      def disability_claim_info
+
+      end
+
+      def military_service_info
+      end
+
+      def handle_military_address
+        addr = veteran_mailing_address || {}
+
+        # Handle both field formats
+        line1 = addr[:addressLine1] || format_address_line(addr[:numberAndStreet], addr[:apartmentOrUnitNumber])
+
+        formatted_addr = {
+          addressLine1: line1,
+          addressLine2: addr[:addressLine2],
+          addressLine3: addr[:addressLine3],
+          country: addr[:country] || 'USA',
+          militaryPostOfficeTypeCode: military_city(addr),
+          militaryStateCode: military_state(addr),
+          zipFirstFive: addr[:zipFirstFive],
+          zipLastFour: addr[:zipLastFour],
+          addressType: 'MILITARY'
+        }.compact_blank
+
+        @fes_claim[:veteran] ||= {}
+        @fes_claim[:veteran][:currentMailingAddress] = formatted_addr
+      end
+
+      def handle_domestic_or_international_address
+        addr = veteran_mailing_address || {}
+        type = addr[:internationalPostalCode].present? ? 'INTERNATIONAL' : 'DOMESTIC'
+
+        # Handle both field formats
+        line1 = addr[:addressLine1] || format_address_line(addr[:numberAndStreet], addr[:apartmentOrUnitNumber])
+
+        formatted_addr = {
+          addressLine1: line1,
+          addressLine2: addr[:addressLine2],
+          addressLine3: addr[:addressLine3],
+          country: addr[:country] || 'USA',
+          zipFirstFive: addr[:zipFirstFive],
+          zipLastFour: addr[:zipLastFour],
+          addressType: type
+        }
+
+        if type == 'INTERNATIONAL'
+          formatted_addr[:internationalPostalCode] = addr[:internationalPostalCode]
+        else
+          formatted_addr[:city] = addr[:city]
+          formatted_addr[:state] = addr[:state]
+        end
+
+        @fes_claim[:veteran] ||= {}
+        @fes_claim[:veteran][:currentMailingAddress] = formatted_addr.compact_blank
+      end
+
+      def change_of_address
+        change_data = @data[:changeOfAddress]
+        return if change_data.blank?
+
+        addr = build_change_of_address_base(change_data)
+        apply_address_type_fields(addr, change_data)
+
+        addr[:zipFirstFive] = change_data[:zipFirstFive] if change_data[:zipFirstFive].present?
+        addr[:zipLastFour] = change_data[:zipLastFour] if change_data[:zipLastFour].present?
+
+        @fes_claim[:veteran] ||= {}
+        @fes_claim[:veteran][:changeOfAddress] = addr.compact_blank
+      end
+
     end
   end
 end
