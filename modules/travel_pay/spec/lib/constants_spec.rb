dd13afc7521a82be167333b53d077970ba44b056
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-09-23 14:25:15 -0700

    Update constant spec from master

--- modules/travel_pay/spec/lib/constants_spec.rb
+++ modules/travel_pay/spec/lib/constants_spec.rb
@@ -4,32 +4,6 @@ require 'rails_helper'
 require_relative '../../lib/travel_pay/constants'
 
 RSpec.describe TravelPay::Constants do
-  describe 'EXPENSE_TYPES' do
-    it 'contains the expected expense type mappings' do
-      expected_types = {
-        meal: 'meal',
-        mileage: 'mileage',
-        parking: 'parking',
-        other: 'other'
-      }
-      expect(described_class::EXPENSE_TYPES).to eq(expected_types)
-    end
-
-    it 'is frozen' do
-      expect(described_class::EXPENSE_TYPES).to be_frozen
-    end
-  end
-
-  describe 'TRIP_TYPES' do
-    it 'contains the expected trip type values' do
-      expect(described_class::TRIP_TYPES).to eq(%w[OneWay RoundTrip Unspecified])
-    end
-
-    it 'is frozen' do
-      expect(described_class::TRIP_TYPES).to be_frozen
-    end
-  end
-
   describe 'UUID_REGEX' do
     let(:valid_uuid)   { SecureRandom.uuid } # Any UUID format accepted (not limited to v4)
     let(:invalid_uuid) { 'not-a-uuid' }
