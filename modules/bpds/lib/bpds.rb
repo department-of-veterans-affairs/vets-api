5ccb15e49fbcc94224865dc1e13c5d1c412d1fc3
Bryan Alexander <bryan.alexander@adhocteam.us>
2025-09-23 15:21:09 -0400

    116944: Port over BPDS logic to module

--- modules/bpds/lib/bpds.rb
+++ modules/bpds/lib/bpds.rb
@@ -2,6 +2,14 @@
 
 require 'bpds/engine'
 
+# BPDS module provides constants and functionality related to the BPDS component.
+#
+# Constants:
+# - MODULE_PATH: String representing the relative path to the BPDS module within the project.
+#
+# Example usage:
+#   BPDS::MODULE_PATH #=> 'modules/bpds'
 module BPDS
-  # Your code goes here...
+  # The module path
+  MODULE_PATH = 'modules/bpds'
 end
