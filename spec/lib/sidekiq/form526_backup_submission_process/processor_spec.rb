a88325f7a21b6f9dc53e17845a8bd98435af07ee
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2025-09-24 14:11:32 -0400

    Adding in accidentially removed test

--- spec/lib/sidekiq/form526_backup_submission_process/processor_spec.rb
+++ spec/lib/sidekiq/form526_backup_submission_process/processor_spec.rb
@@ -12,6 +12,21 @@ RSpec.describe Sidekiq::Form526BackupSubmissionProcess::Processor do
     EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h)
   end
 
+  context 'veteran with a foreign address' do
+    describe 'submission and document metadata' do
+      before do
+        allow(Settings.form526_backup).to receive(:enabled).and_return(true)
+      end
+
+      let!(:submission) { create(:form526_submission, :with_non_us_address) }
+
+      it 'sets the submission metadata zip code to a default value' do
+        instance = subject.new(submission.id, get_upload_location_on_instantiation: false)
+        expect(instance.zip).to eq('00000')
+      end
+    end
+  end
+
   describe '#choose_provider' do
     let(:user) { create(:user, :loa3, :with_terms_of_use_agreement) }
     let(:user_account) { user.user_account }
