71dd95d3c96a6c99d36438ff3dac4a31fae92287
rockwellwindsor-va <129893414+rockwellwindsor-va@users.noreply.github.com>
2025-09-24 15:50:45 -0500

    API-50225-v1-fes-mapper-section-5-disabilities
    * Maps disabilities into the object FES wants.
    * Flattens secondary disabilities into disabilities before mapping
    	modified:   modules/claims_api/lib/claims_api/v1/disability_compensation_fes_mapper.rb
    	modified:   modules/claims_api/spec/lib/claims_api/v1/disability_compensation_fes_mapper_spec.rb

--- modules/claims_api/lib/claims_api/v1/disability_compensation_fes_mapper.rb
+++ modules/claims_api/lib/claims_api/v1/disability_compensation_fes_mapper.rb
@@ -3,6 +3,8 @@
 module ClaimsApi
   module V1
     class DisabilityCompensationFesMapper
+      IGNORED_DISABILITY_FIELDS = %i[serviceRelevance secondaryDisabilities].freeze
+
       def initialize(auto_claim)
         @auto_claim = auto_claim
         @data = auto_claim&.form_data&.deep_symbolize_keys
@@ -10,10 +12,80 @@ module ClaimsApi
       end
 
       def map_claim
+        claim_attributes
+
         wrap_in_request_structure
       end
 
       private
+
+      def claim_attributes
+        disabilities
+      end
+
+      # a 'disability' is required via the schema
+      # 'disabilityActionType' & 'name' are required via the schema
+      def disabilities
+        disablities_data = flatten_disabilities(@data[:disabilities])
+        @fes_claim[:disabilities] = disablities_data.map do |disability|
+          transform_disability_values!(disability.deep_dup)
+        end
+      end
+
+      def transform_disability_values!(disability)
+        # Remove nil values for the optional fields
+        %i[diagnosticCode classificationCode ratedDisabilityId specialIssues].each do |field|
+          disability.delete(field) if disability[field].blank?
+        end
+
+        # Transform approximate date to FES format
+        begin_date = disability[:approximateBeginDate]
+        disability[:approximateBeginDate] = make_date_object(begin_date, begin_date.length) if begin_date.present?
+
+        # Remove fields not needed for FES
+        disability.except(*IGNORED_DISABILITY_FIELDS)
+      end
+
+      def flatten_disabilities(disabilities_array)
+        disabilities_array.flat_map do |disability|
+          primary_disablity = disability.dup
+          # Remove secondaryDisabilities from primary_disablity before adding to list
+          secondary_disability_data = primary_disablity.delete(:secondaryDisabilities)
+
+          list = [primary_disablity]
+
+          if secondary_disability_data.present?
+            secondaries = secondary_disability_data.map do |secondary|
+              secondary_copy = secondary.dup
+              secondary_copy[:name] = secondary[:name]
+              secondary_copy
+            end
+            list.concat(secondaries)
+          end
+
+          list
+        end
+      end
+
+      def make_date_object(date, date_length)
+        year, month, day = regex_date_conversion(date)
+        return if year.nil? || date_length.nil?
+
+        if date_length == 4
+          { year: }
+        elsif date_length == 7
+          { month:, year: }
+        else
+          { year:, month:, day: }
+        end
+      end
+
+      def regex_date_conversion(date)
+        if date.present?
+          date_match = date.match(/^(?:(?<year>\d{4})(?:-(?<month>\d{2}))?(?:-(?<day>\d{2}))*|(?<month>\d{2})?(?:-(?<day>\d{2}))?-?(?<year>\d{4}))$/) # rubocop:disable Layout/LineLength
+          date_match&.values_at(:year, :month, :day)
+        end
+      end
 
       def wrap_in_request_structure
         {
--- modules/claims_api/spec/lib/claims_api/v1/disability_compensation_fes_mapper_spec.rb
+++ modules/claims_api/spec/lib/claims_api/v1/disability_compensation_fes_mapper_spec.rb
@@ -35,6 +35,79 @@ describe ClaimsApi::V1::DisabilityCompensationFesMapper do
           expect(fes_data[:data]).to have_key(:form526)
         end
       end
+
+      context 'section 5 disabilities' do
+        let(:secondary_disability) do
+          [
+            {
+              'name' => 'Left Hip Pain',
+              'disabilityActionType' => 'SECONDARY',
+              'serviceRelevance' => 'Caused by a service-connected disability',
+              'approximateBeginDate' => '2018-05'
+            },
+            {
+              'name' => 'Left Elbow Pain',
+              'disabilityActionType' => 'SECONDARY',
+              'serviceRelevance' => 'Caused by a service-connected disability',
+              'approximateBeginDate' => '2019'
+            }
+          ]
+        end
+
+        it 'maps the FES attributes' do
+          form_data['data']['attributes']['disabilities'][0]['classificationCode'] = '123456'
+          form_data['data']['attributes']['disabilities'][0]['approximateBeginDate'] = '2018-02-22'
+
+          disability_object = fes_data[:data][:form526][:disabilities]
+
+          expect(disability_object).not_to be_nil
+          expect(disability_object[0][:name]).to eq('PTSD (post traumatic stress disorder)')
+          expect(disability_object[0][:classificationCode]).to eq('123456')
+          expect(disability_object[0][:ratedDisabilityId]).to eq('1100583')
+          expect(disability_object[0][:diagnosticCode]).to eq(9999)
+          expect(disability_object[0][:disabilityActionType]).to eq('NEW')
+          expect(disability_object[0][:specialIssues]).to eq(['Fully Developed Claim', 'PTSD/2'])
+          expect(disability_object[0][:approximateBeginDate]).to eq({ year: '2018', month: '02', day: '22' })
+        end
+
+        it 'maps secondary disabilities if included' do
+          form_data['data']['attributes']['disabilities'][0]['secondaryDisabilities'] = secondary_disability
+
+          disability_object = fes_data[:data][:form526][:disabilities]
+
+          expect(disability_object[1][:name]).to eq('Left Hip Pain')
+          expect(disability_object[1]).not_to have_key(:classificationCode)
+          expect(disability_object[1]).not_to have_key(:ratedDisabilityId)
+          expect(disability_object[1]).not_to have_key(:diagnosticCode)
+          expect(disability_object[1][:disabilityActionType]).to eq('SECONDARY')
+          expect(disability_object[1]).not_to have_key(:specialIssues)
+          expect(disability_object[1][:approximateBeginDate]).to eq({ year: '2018', month: '05' })
+          expect(disability_object[2][:name]).to eq('Left Elbow Pain')
+          expect(disability_object[2][:approximateBeginDate]).to eq({ year: '2019' })
+        end
+
+        it 'does not map the ignored fields' do
+          form_data['data']['attributes']['disabilities'][0]['serviceRelevance'] = 'Hurt while working.'
+
+          disability_object = fes_data[:data][:form526][:disabilities]
+
+          expect(disability_object[0]).not_to have_key(:serviceRelevance)
+          expect(disability_object[0]).not_to have_key(:secondaryDisabilities)
+        end
+
+        it 'removes a missing optional attribute' do
+          form_data['data']['attributes']['disabilities'][0]['ratedDisabilityId'] = ' '
+          form_data['data']['attributes']['disabilities'][0]['diagnosticCode'] = nil
+          form_data['data']['attributes']['disabilities'][0]['specialIssues'] = []
+
+          disability_object = fes_data[:data][:form526][:disabilities]
+
+          expect(disability_object[0]).not_to have_key(:classificationCode)
+          expect(disability_object[0]).not_to have_key(:ratedDisabilityId)
+          expect(disability_object[0]).not_to have_key(:diagnosticCode)
+          expect(disability_object[0]).not_to have_key(:specialIssues)
+        end
+      end
     end
   end
 end
