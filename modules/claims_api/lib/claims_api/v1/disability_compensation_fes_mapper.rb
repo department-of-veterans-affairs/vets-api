e8fd32c7e9a22864d990a502afbc4ed21a65a95f
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-09-24 10:43:24 -0500

    initial pass at remapping secondary disabilities for v1

--- modules/claims_api/lib/claims_api/v1/disability_compensation_fes_mapper.rb
+++ modules/claims_api/lib/claims_api/v1/disability_compensation_fes_mapper.rb
@@ -5,7 +5,7 @@ module ClaimsApi
     class DisabilityCompensationFesMapper
       def initialize(auto_claim)
         @auto_claim = auto_claim
-        @data = auto_claim&.form_data&.deep_symbolize_keys
+        @data = get_auto_claim_data(auto_claim)
         @fes_claim = {}
       end
 
@@ -23,6 +23,40 @@ module ClaimsApi
             form526: @fes_claim
           }
         }
+      end
+
+      def get_auto_claim_data(auto_claim)
+        return {} unless auto_claim&.form_data
+        data = auto_claim.form_data.deep_symbolize_keys
+        process_disabilities(data)
+      end
+
+      def process_disabilities(data)
+        return data unless data[:disabilities].is_a?(Array)
+        
+        secondaries = extract_secondary_disabilities(data)
+        data[:disabilities] += secondaries_with_action_type(secondaries)
+        data
+      end
+
+      def extract_secondary_disabilities(data)
+        secondaries = []
+        
+        data[:disabilities].each_with_index do |disability, index|
+          if disability[:secondaryDisabilities].present?
+            secondaries.concat(disability[:secondaryDisabilities])
+            # Remove secondaries from original disability
+            data[:disabilities][index] = disability.except(:secondaryDisabilities)
+          end
+        end
+        
+        secondaries
+      end
+
+      def secondaries_with_action_type(secondaries)
+        secondaries.map do |disability|
+          disability.merge(disabilityActionType: 'NEW')
+        end
       end
 
       def extract_claimant_participant_id
