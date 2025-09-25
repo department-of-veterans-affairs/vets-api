bc28bad408494ad25ab41192ec649e598d09ec22
nfstern02 <72567812+nfstern02@users.noreply.github.com>
2025-09-25 03:08:41 -0500

    refactor spec for performance & coverage, fix bug in code

--- lib/dependents/monitor.rb
+++ lib/dependents/monitor.rb
@@ -17,8 +17,8 @@ module Dependents
     # stats key for pdf submission
     PDF_SUBMISSION_STATS_KEY = 'worker.submit_dependents_pdf'
 
-    # statsd key for backup sidekiq
-    SUBMISSION_STATS_KEY = 'worker.submit_686c_674_backup_submission'
+    # statsd key for sidekiq
+    SUBMISSION_STATS_KEY = 'worker.submit_686c_674_job'
 
     # statsd key for email notifications
     EMAIL_STATS_KEY = 'dependents.email_notification'
@@ -28,6 +28,18 @@ module Dependents
       @claim = claim(claim_id)
       @use_v2 = use_v2
       super('dependents-application')
+    end
+
+    # lib/logging/base_monitor (on or about line 33) requires a `name` method
+    delegate :name, to: :self.class
+
+    # lib/logging/base_monitor (on or about line 37) requires a `form_id` method
+    def form_id
+      @claim&.form_id
+    end
+
+    def submission_stats_key
+      SUBMISSION_STATS_KEY
     end
 
     def use_v2
@@ -109,8 +121,13 @@ module Dependents
     end
 
     def track_send_received_email_failure(e, user_account_uuid = nil)
-      track_send_email_failure("'Received' email failure for claim #{@claim_id}", "#{EMAIL_STATS_KEY}.received.failure",
-                               e, user_account_uuid)
+      track_send_email_failure(
+        @claim,
+        nil, # lighthouse_service (I don't know if we have this) TODO: Research by application team.
+        user_account_uuid,
+        'submitted',
+        e
+      )
     end
 
     def track_pdf_upload_error
