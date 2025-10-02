22aba2cb6f0b4395bd4180a7eada69c72effddee
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-10-02 09:13:42 -0700

    Add common carrier expense model

---
+++ modules/travel_pay/app/models/travel_pay/common_carrier_expense.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative '../../../lib/travel_pay/constants'
+
+module TravelPay
+  class CommonCarrierExpense < BaseExpense
+    attribute :reason_not_using_pov, :string
+    attribute :carrier_type, :string
+
+    validates :reason_not_using_pov, presence: true, inclusion: { in: TravelPay::Constants::COMMON_CARRIER_EXPLANATIONS.values }
+    validates :carrier_type, presence: true, inclusion: { in: TravelPay::Constants::COMMON_CARRIER_TYPES.values }
+
+    # Returns the expense type for common carrier expenses
+    #
+    # @return [String] the expense type
+    def expense_type
+      TravelPay::Constants::EXPENSE_TYPES[:common_carrier]
+    end
+  end
+end
