574327b3a31e7cc1506cbbb059afe0c2b4f9fc8d
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-10-03 15:19:03 -0400

    Add BGS686c job to module

--- app/models/bgs/submission_attempt.rb
+++ app/models/bgs/submission_attempt.rb
@@ -17,7 +17,8 @@ class BGS::SubmissionAttempt < SubmissionAttempt
 
   STATS_KEY = 'api.bgs.submission_attempt'
 
-  def fail!
+  def fail!(error:)
+    update(error_message: error&.message)
     failure!
     log_hash = status_change_hash
     log_hash[:message] = 'BGS Submission Attempt failed'
@@ -47,5 +48,15 @@ class BGS::SubmissionAttempt < SubmissionAttempt
 
   def monitor
     @monitor ||= Logging::Monitor.new('bgs_submission_attempt')
+  end
+
+  def status_change_hash
+    {
+      submission_id: submission.id,
+      claim_id: submission.saved_claim_id,
+      form_type: submission.form_id,
+      from_state: previous_changes[:status]&.first,
+      to_state: status
+    }
   end
 end
--- modules/dependents_benefits/lib/dependents_benefits/claim_processor.rb
+++ modules/dependents_benefits/lib/dependents_benefits/claim_processor.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'dependents_benefits/jobs/bgs_686c_job'
 require 'dependents_benefits/monitor'
 
 module DependentsBenefits
@@ -61,10 +62,11 @@ module DependentsBenefits
     def enqueue_686c_submission(claim)
       jobs_count = 0
 
-      # Enqueue primary 686c submission job
+      # Enqueue primary 686c submission jobs
+      Jobs::BGS686cJob.perform_async(claim.id, proc_id)
+      jobs_count += 1
+
       # TODO: Add calls to submission jobs here as they are implemented
-      # Example: DependentsBenefits::SubmissionJob.perform_async(claim.id, proc_id)
-      # jobs_count += 1
 
       monitor.track_processor_info('Enqueued 686c submission jobs', 'enqueue_686c',
                                    parent_claim_id:, claim_id: claim.id)
