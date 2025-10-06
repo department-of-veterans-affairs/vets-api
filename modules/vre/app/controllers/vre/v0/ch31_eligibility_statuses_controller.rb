3daddba8ab2b69d6f87bff1dc78a7b529e1c690f
Jeff Marks <jeff.t.marks@gmail.com>
2025-10-06 12:33:06 -0600

    revert change

--- modules/vre/app/controllers/vre/v0/ch31_eligibility_statuses_controller.rb
+++ modules/vre/app/controllers/vre/v0/ch31_eligibility_statuses_controller.rb
@@ -5,8 +5,6 @@ module VRE
     class Ch31EligibilityStatusesController < ApplicationController
       service_tag 'vre-application'
 
-      skip_before_action :authenticate
-
       def show
         response = eligibility_service.get_details
         render json: VRE::Ch31EligibilitySerializer.new(response)
@@ -15,7 +13,7 @@ module VRE
       private
 
       def eligibility_service
-        VRE::Ch31Eligibility::Service.new('200')
+        VRE::Ch31Eligibility::Service.new(@current_user&.icn)
       end
     end
   end
