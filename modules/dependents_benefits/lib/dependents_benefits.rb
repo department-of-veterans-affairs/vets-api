7a2e8e56e4963b05c8b3494d5fab7583a6a6d9d7
David Becker <david@beckersite.com>
2025-10-06 09:14:20 -0600

    Add submission handler for dependents benefits

--- modules/dependents_benefits/lib/dependents_benefits.rb
+++ modules/dependents_benefits/lib/dependents_benefits.rb
@@ -8,6 +8,7 @@ require 'dependents_benefits/engine'
 module DependentsBenefits
   # The form_id
   FORM_ID = '686C-674'
+  # FORM_ID_V2 = '686C-674-V2'
   ADD_REMOVE_DEPENDENT = '21-686C'
   SCHOOL_ATTENDANCE_APPROVAL = '21-674'
   PARENT_DEPENDENCY = '21-509'
---
+++ modules/dependents_benefits/lib/dependents_benefits/benefits_intake/submission_handler.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'lighthouse/benefits_intake/submission_handler/saved_claim'
+require 'dependents_benefits/monitor'
+
+module DependentsBenefits
+  module BenefitsIntake
+    # @see BenefitsIntake::SubmissionHandler::SavedClaim
+    class SubmissionHandler < ::BenefitsIntake::SubmissionHandler::SavedClaim
+      # Retrieves all pending Lighthouse::SubmissionAttempt records associated with submissions
+      # where the form_id is '686C-674-V2'.
+      #
+      # @return [ActiveRecord::Relation] a relation containing pending submission attempts for form '686C-674-V2'
+      def self.pending_attempts
+        Lighthouse::SubmissionAttempt.joins(:submission).where(status: 'pending',
+                                                               'lighthouse_submissions.form_id' => "#{DependentsBenefits::FORM_ID}-V2")
+      end
+
+      private
+
+      # BenefitsIntake::SubmissionHandler::SavedClaim#claim_class
+      def claim_class
+        DependentsBenefits::SavedClaim
+      end
+
+      # BenefitsIntake::SubmissionHandler::SavedClaim#monitor
+      def monitor
+        @monitor ||= DependentsBenefits::Monitor.new
+      end
+
+      # BenefitsIntake::SubmissionHandler::SavedClaim#notification_email
+      def notification_email
+        # TODO: in #120766
+      end
+
+      # handle a failure result
+      # inheriting class must assign @avoided before calling `super`
+      def on_failure
+        # TODO: in #120766
+      end
+
+      # handle a success result
+      def on_success
+        # TODO: in #120766
+      end
+
+      # handle a stale result
+      def on_stale
+        true
+      end
+    end
+  end
+end
