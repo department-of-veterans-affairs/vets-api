29e28eec5511b6e1aecf52ac8517caf8e723c996
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-10-07 15:22:29 -0700

    first pass at confirmation email for form 10275

--- app/models/saved_claim/education_benefits/va_10275.rb
+++ app/models/saved_claim/education_benefits/va_10275.rb
@@ -2,4 +2,87 @@
 
 class SavedClaim::EducationBenefits::VA10275 < SavedClaim::EducationBenefits
   add_form_and_validation('22-10275')
+
+  POE_EMAIL = 'tbd@example.com'
+
+  def after_submit(_user)
+    return unless Flipper.enabled?(:form22_10275_submission_email)
+
+    email_template = Settings.vanotify.services.va_gov.template_id.form10275_submission_email
+
+    email_params = construct_email_parts
+
+    VANotify::EmailJob.perform_async(
+      POE_EMAIL,
+      email_template,
+      email_params
+    )
+  end
+
+  private
+
+  def construct_email_parts
+    address_hash = parsed_form.dig('mainInstitution', 'institutionAddress') || {}
+
+    header = <<-HEADER
+      Agreement Type: #{parsed_form['agreementType']}
+      Institution Name: #{parsed_form.dig('mainInstitution', 'institutionName')}
+      Institution Facility Code: #{parsed_form.dig('mainInstitution', 'facilityCode')}
+      Institution Address: #{format_address(address_hash)}
+    HEADER
+
+    locations = (parsed_form['additionalInstitutions'] || []).map do |location|
+      format_location(location)
+    end.join("\n\n")
+
+    officials = construct_officials
+
+    signature = <<-SIGNATURES
+      Signed: #{parsed_form['statementOfTruthSignature']} (#{parsed_form['dateSigned']})
+    SIGNATURES
+
+    { header:, locations:, officials:, signature: }
+  end
+
+  def construct_officials
+    if parsed_form['agreementType'] == 'newCommitment'
+      poc = parsed_form['newCommitment']['principlesOfExcellencePointOfContact']
+      sco = parsed_form['newCommitment']['schoolCertifyingOfficial']
+      <<-OFFICIALS
+        Principles of Excellence Point of Contact
+          #{format_name(poc)} #{poc['title']}
+          #{poc['usPhone'] || pos['internationalPhone']}
+          #{poc['email']}
+
+        School Certifying Official
+          #{format_name(sco)} #{sco['title']}
+          #{sco['usPhone'] || pos['internationalPhone']}
+          #{sco['email']}
+      OFFICIALS
+    else
+      ''
+    end
+  end
+
+  def format_location(location_hash)
+    <<-LOCATION
+      Name: #{location_hash['institutionName']}
+      Facility Code: #{location_hash['facilityCode']}
+      Address: #{format_address(location_hash['institutionAddress'])}
+      POC: #{format_name(location_hash['pointOfContact']['fullName'])} #{location_hash['pointOfContact']['email']}
+    LOCATION
+  end
+
+  def format_address(address_hash)
+    <<-ADDRESS
+      #{address_hash['street']}
+      #{address_hash['street2']}
+      #{address_hash['city']}, #{address_hash['state']}, #{address_hash['postalCode']}
+      #{address_hash['country']}
+    ADDRESS
+  end
+
+  def format_name(name_hash)
+    "#{name_hash['first']} #{name_hash['middle']} #{name_hash['last']}"
+  end
 end
--- spec/models/saved_claim/education_benefits/va10275_spec.rb
+++ spec/models/saved_claim/education_benefits/va10275_spec.rb
@@ -9,4 +9,43 @@ RSpec.describe SavedClaim::EducationBenefits::VA10275 do
   it_behaves_like 'saved_claim'
 
   validate_inclusion(:form_id, '22-10275')
+
+  describe '#after_submit' do
+    let(:user) { create(:user) }
+
+    describe 'confirmation email for 10275' do
+      it 'is skipped when feature flag is turned off' do
+        allow(Flipper).to receive(:enabled?).with(:form22_10275_submission_email).and_return(false)
+        allow(VANotify::EmailJob).to receive(:perform_async)
+
+        subject = create(:va10275)
+        subject.after_submit(user)
+
+        expect(VANotify::EmailJob).not_to have_received(:perform_async)
+        Flipper.enable(:form0994_confirmation_email)
+      end
+
+      it 'sends the email when feature flag is on' do
+        allow(Flipper).to receive(:enabled?).with(:form22_10275_submission_email).and_return(true)
+        allow(VANotify::EmailJob).to receive(:perform_async)
+
+        subject = create(:va10275)
+        subject.after_submit(user)
+
+        expect(VANotify::EmailJob).to have_received(:perform_async).with(
+          'tbd@example.com',
+          'form10275_submission_email_template_id',
+          satisfy do |args|
+            args[:header].include?('Springfield University') &&
+            args[:header].include?('US123456') &&
+            args[:locations].include?('Springfield Technical Institute') &&
+            args[:locations].include?('US654321') &&
+            args[:officials].include?('michael.brown@springfield.edu') &&
+            args[:officials].include?('emily.johnson@springfield.edu') &&
+            args[:signature].include?('Robert Smith')
+          end
+        )
+      end
+    end
+  end
 end
