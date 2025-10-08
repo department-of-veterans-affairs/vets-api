1674f964dd69e85be5d66f02475a3e6f29233396
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-10-08 12:14:25 -0400

    changed sentry logging to rails logger.

--- app/controllers/v0/sign_in_controller.rb
+++ app/controllers/v0/sign_in_controller.rb
@@ -46,7 +46,7 @@ module V0
       StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_AUTHORIZE_FAILURE)
       handle_pre_login_error(e, client_id)
     rescue => e
-      log_message_to_sentry(e.message, :error)
+      sign_in_logger.error('authorize error', { errors: e.message, client_id:, type:, acr: })
       StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_AUTHORIZE_FAILURE)
       handle_pre_login_error(e, client_id)
     end
@@ -85,7 +85,10 @@ module V0
       StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_CALLBACK_FAILURE)
       handle_pre_login_error(e, state_payload&.client_id)
     rescue => e
-      log_message_to_sentry(e.message, :error)
+      sign_in_logger.error('callback error', { errors: e.message,
+                                               client_id: state_payload&.client_id,
+                                               type: state_payload&.type,
+                                               acr: state_payload&.acr })
       StatsD.increment(SignIn::Constants::Statsd::STATSD_SIS_CALLBACK_FAILURE)
       handle_pre_login_error(e, state_payload&.client_id)
     end
