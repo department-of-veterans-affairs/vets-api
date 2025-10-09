028ed6c369fc89948ebd6796ec66e7de0f340810
Tonksthebear <jconigliari@ironarchtech.com>
2025-10-09 09:58:50 -0700

    Update client.rb

--- lib/sm/client.rb
+++ lib/sm/client.rb
@@ -784,7 +784,7 @@ module SM
 
     # Polling integration for OH messages on send/reply
     def poll_status(message)
-      if Settings.vsp_environment == 'staging'
+      if %w[staging production].include?(Settings.vsp_environment)
         Rails.logger.info("MHV SM: message id #{message.id} is in the OH polling path")
       end
       result = poll_message_status(message.id, timeout_seconds: 60, interval_seconds: 1, max_errors: 2)
