c5e1597909e0354c2a24d5cb25f77ba2db8d2a0e
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-10-09 11:19:29 -0400

    rubocop fixes

--- app/models/mpi_data.rb
+++ app/models/mpi_data.rb
@@ -261,7 +261,7 @@ class MPIData < Common::RedisStore
     do_cached_with(key: user_key) do
       find_profile
     rescue ArgumentError, MPI::Errors::ArgumentError => e
-      Rails.logger.warn("[MPIData] request error", message: e.message)
+      Rails.logger.warn('[MPIData] request error', message: e.message)
       return nil
     end
   end
