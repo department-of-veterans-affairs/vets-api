cb33e93006eb08921c18bb751d38849f63e6bbd5
Patrick Brown <patrick.brown@oddball.io>
2025-10-15 12:44:41 -0400

    arc/21a_document_upload_new_endpoint

--- modules/accredited_representative_portal/config/routes.rb
+++ modules/accredited_representative_portal/config/routes.rb
@@ -5,7 +5,31 @@ AccreditedRepresentativePortal::Engine.routes.draw do
     get 'authorize_as_representative', to: 'representative_users#authorize_as_representative'
     get 'user', to: 'representative_users#show'
 
+    # Form 21a routes
     post 'form21a', to: 'form21a#submit'
+
+    scope 'form21a' do
+      post ':details_slug', to: 'form21a#details', constraints: {
+        details_slug: /
+          (?:conviction-details|
+             court-martialed-details|
+             under-charges-details|
+             resigned-from-education-details|
+             withdrawn-from-education-details|
+             disciplined-for-dishonesty-details|
+             resigned-for-dishonesty-details|
+             representative-for-agency-details|
+             reprimanded-in-agency-details|
+             resigned-from-agency-details|
+             applied-for-va-accreditation-details|
+             terminated-by-vsorg-details|
+             condition-that-affects-representation-details|
+             condition-that-affects-examination-details
+          )
+        /x
+      }
+    end
+
     resources :in_progress_forms, only: %i[update show destroy]
 
     post '/submit_representative_form', to: 'representative_form_upload#submit'
