171e3bbbec8d3383b568ee7019e0fa763a473853
nfstern02 <72567812+nfstern02@users.noreply.github.com>
2025-10-18 19:07:33 -0500

    refactor to fix broken spec(s)

--- spec/services/bgs/dependent_service_spec.rb
+++ spec/services/bgs/dependent_service_spec.rb
@@ -435,11 +435,9 @@ RSpec.describe BGS::DependentService do
     it 'in case of error it logs the exception and raises a custom error' do
       service = BGS::DependentService.new(user)
       allow(VBMS::SubmitDependentsPdfJob).to receive(:perform_sync).and_raise(StandardError)
-      expect(Rails.logger).to receive(:warn)
-      expect do
-        service.send(:submit_pdf_job, claim:,
-                                      encrypted_vet_info:)
-      end.to raise_error(BGS::DependentService::PDFSubmissionError)
+      expect(Rails.logger).to receive(:error).at_least(:once)
+      expect { service.send(:submit_pdf_job, claim:, encrypted_vet_info:) }
+        .to raise_error(BGS::DependentService::PDFSubmissionError)
     end
   end
 end
