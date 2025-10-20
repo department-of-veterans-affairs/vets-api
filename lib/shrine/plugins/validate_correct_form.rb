dbfb0e077f24f256ea545b54a2b1d7aa064b12ec
Dominic Padula <dominic.padula@wilcore.io>
2025-10-20 09:19:05 -0500

    write integration test

--- lib/shrine/plugins/validate_correct_form.rb
+++ lib/shrine/plugins/validate_correct_form.rb
@@ -17,10 +17,10 @@ class Shrine
           pdf = MiniMagick::Image.open(file.path)
           MiniMagick.convert do |convert|
             convert.density 150
-            convert << pdf.pages.first.path
             convert.background 'white'
             convert.flatten
             convert.quality 100
+            convert << pdf.pages.first.path
             convert << image_path
           end
           file_as_string = RTesseract.new(image_path).to_s
