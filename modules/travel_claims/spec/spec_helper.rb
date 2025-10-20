edc03184de2a63c3c07f196eac391e5ac59c4bf3
Lee Delarm <lee.delarm@agile6.com>
2025-10-20 10:35:03 -0400

    minor addition to attempt to pass isolated error in github actions
    minor addition to attempt to pass isolated error in github actions

--- modules/travel_claims/spec/spec_helper.rb
+++ modules/travel_claims/spec/spec_helper.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 # Configure Rails Environment
-ENV['RAILS_ENV'] = 'test'
+ENV['RAILS_ENV'] ||= 'test'
 
+require File.expand_path('../../../config/environment', __dir__)
 require 'rspec/rails'
 
 RSpec.configure do |config|
