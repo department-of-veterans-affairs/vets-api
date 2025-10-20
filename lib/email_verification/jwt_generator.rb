a9d956a7f5fde1e7f4f519d5e5339c91582bd977
Diana20920 <diana.pamelaet@gmail.com>
2025-10-20 14:18:46 -0600

    Add email verification service layer

---
+++ lib/email_verification/jwt_generator.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module EmailVerification
+  # JWT generator for email verification tokens
+  class JwtGenerator
+    VALIDITY_LENGTH = 24.hours # Token valid for 24 hours
+    ALGORITHM = 'HS256'
+    ISSUER = 'vets-api-email-verification'
+
+    def initialize(user)
+      @user = user
+    end
+
+    def encode_jwt
+      JWT.encode(payload, secret, ALGORITHM, headers)
+    end
+
+    private
+
+    def headers
+      { typ: 'JWT', alg: ALGORITHM }
+    end
+
+    def payload
+      {
+        jti: SecureRandom.uuid,
+        iat: Time.zone.now.to_i,
+        exp: (Time.zone.now + VALIDITY_LENGTH).to_i,
+        iss: ISSUER,
+        uuid: @user.uuid,
+        email: @user.email
+      }
+    end
+
+    def secret
+      Settings.email_verification.jwt_secret
+    end
+  end
+end
