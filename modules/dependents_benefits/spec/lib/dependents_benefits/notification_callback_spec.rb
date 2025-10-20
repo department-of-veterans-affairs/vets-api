eb683f73f5ebe05a25e5d4971ecace53bca775f8
David Becker <david@beckersite.com>
2025-10-20 15:40:50 -0600

    Re-order imports for notification callback dependents module

--- modules/dependents_benefits/spec/lib/dependents_benefits/notification_callback_spec.rb
+++ modules/dependents_benefits/spec/lib/dependents_benefits/notification_callback_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
 require 'dependents_benefits/monitor'
 require 'dependents_benefits/notification_callback'
-
-require 'rails_helper'
 require 'lib/veteran_facing_services/notification_callback/shared/saved_claim'
 
 RSpec.describe DependentsBenefits::NotificationCallback do
