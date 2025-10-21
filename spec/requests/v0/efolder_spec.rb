e27536247c2a40c08d86486e94f0e5fd68076579
Kris Pethtel <kris.pethtel@gmail.com>
2025-10-20 21:39:56 -0500

    specs

--- spec/requests/v0/efolder_spec.rb
+++ spec/requests/v0/efolder_spec.rb
@@ -41,46 +41,38 @@ RSpec.describe 'VO::Efolder', type: :request do
   end
 
   describe 'GET /v0/efolder/tsa_letter' do
-    let(:list_documents_res) do
-      [{ document_id: '{73CD7B28-F695-4337-BBC1-2443A913ACF6}',
-         doc_type: '702',
-         type_description: 'Disability Benefits Questionnaire (DBQ) - Veteran Provided',
-         received_at: Date.new(2024, 9, 13) },
-       { document_id: '{EF7BF420-7E49-4FA9-B14C-CE5F6225F615}',
-         doc_type: '45',
-         type_description: 'Military Personnel Record',
-         received_at: Date.new(2024, 9, 13) }]
+    let(:tsa_letter) do
+      {
+        document_id: '{73CD7B28-F695-4337-BBC1-2443A913ACF6}',
+        doc_type: '34',
+        type_description: 'Correspondence',
+        received_at: Date.new(2024, 9, 13)
+      }
     end
 
     before do
-      expect(efolder_service).to receive(:list_documents).and_return(
-        list_documents_res
+      expect(efolder_service).to receive(:get_tsa_letter).and_return(
+        tsa_letter
       )
     end
 
-    get '/v0/efolder/tsa_letter'
-    expect(response.body).to eq()
+    it 'returns the tsa letter metadata' do
+      get '/v0/efolder/tsa_letter'
+      expect(response.body).to eq(tsa_letter.to_json)
+    end
   end
 
   describe 'GET /v0/efolder/download_tsa_letter' do
-    let(:list_documents_res) do
-      [{ document_id: '{73CD7B28-F695-4337-BBC1-2443A913ACF6}',
-         doc_type: '702',
-         type_description: 'Disability Benefits Questionnaire (DBQ) - Veteran Provided',
-         received_at: Date.new(2024, 9, 13) },
-       { document_id: '{EF7BF420-7E49-4FA9-B14C-CE5F6225F615}',
-         doc_type: '45',
-         type_description: 'Military Personnel Record',
-         received_at: Date.new(2024, 9, 13) }]
-    end
+    let(:document_id) { '{93631483-E9F9-44AA-BB55-3552376400D8}' }
+    let(:content) { File.read('spec/fixtures/files/error_message.txt') }
 
     before do
-      expect(efolder_service).to receive(:list_documents).and_return(
-        list_documents_res
-      )
+      expect(efolder_service).to receive(:download_tsa_letter).with(document_id).and_return(content)
     end
 
-    get '/v0/efolder/tsa_letter'
-    expect(response.body).to eq()
+    it 'sends the doc pdf' do
+      get "/v0/efolder/download_tsa_letter/#{CGI.escape(document_id)}", params: { filename: 'test.pdf' }
+      expect(response.body).to eq(content)
+    end
   end
 end
