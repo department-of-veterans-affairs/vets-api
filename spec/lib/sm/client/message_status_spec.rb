a4902e1276195949fae20dcf0ff38ba7753a5abb
Tonksthebear <jconigliari@ironarchtech.com>
2025-10-21 10:32:54 -0700

    remove stubbing

--- spec/lib/sm/client/message_status_spec.rb
+++ spec/lib/sm/client/message_status_spec.rb
@@ -19,12 +19,11 @@ describe SM::Client, '#status' do
         VCR.use_cassette('sm_client/messages/creates/status_sent') do
           VCR.use_cassette('sm_client/messages/creates/a_new_oh_message_without_attachments') do
             expect(client).to receive(:poll_message_status).with(674_838,
-                                                                 hash_including(timeout_seconds: 60)).and_return(
-                                                                   { message_id: 674_838, status: 'SENT',
-                                                                     is_oh_message: true }
-                                                                 )
+                                                                 hash_including(timeout_seconds: 60))
 
-            client.post_create_message(message_params, poll_for_status: true)
+            expect do
+              client.post_create_message(message_params, poll_for_status: true)
+            end.not_to raise_error
           end
         end
       end
@@ -32,12 +31,6 @@ describe SM::Client, '#status' do
       it 'raises UnprocessableEntity on FAILED' do
         VCR.use_cassette('sm_client/messages/creates/status_failed') do
           VCR.use_cassette('sm_client/messages/creates/a_new_oh_message_without_attachments') do
-            expect(client).to receive(:poll_message_status).with(674_838,
-                                                                 hash_including(timeout_seconds: 60)).and_return(
-                                                                   { message_id: 674_838, status: 'FAILED',
-                                                                     is_oh_message: true }
-                                                                 )
-
             expect do
               client.post_create_message(message_params, poll_for_status: true)
             end.to raise_error(Common::Exceptions::UnprocessableEntity)
@@ -48,12 +41,6 @@ describe SM::Client, '#status' do
       it 'raises UnprocessableEntity on INVALID' do
         VCR.use_cassette('sm_client/messages/creates/status_invalid') do
           VCR.use_cassette('sm_client/messages/creates/a_new_oh_message_without_attachments') do
-            expect(client).to receive(:poll_message_status).with(674_838,
-                                                                 hash_including(timeout_seconds: 60)).and_return(
-                                                                   { message_id: 674_838, status: 'INVALID',
-                                                                     is_oh_message: true }
-                                                                 )
-
             expect do
               client.post_create_message(message_params, poll_for_status: true)
             end.to raise_error(Common::Exceptions::UnprocessableEntity)
@@ -66,10 +53,7 @@ describe SM::Client, '#status' do
           VCR.use_cassette("sm_client/messages/creates/status_#{terminal.downcase}") do
             VCR.use_cassette('sm_client/messages/creates/a_new_oh_message_without_attachments') do
               expect(client).to receive(:poll_message_status).with(674_838,
-                                                                   hash_including(timeout_seconds: 60)).and_return(
-                                                                     { message_id: 674_838, status: terminal,
-                                                                       is_oh_message: true }
-                                                                   )
+                                                                   hash_including(timeout_seconds: 60))
 
               client.post_create_message(message_params, poll_for_status: true)
             end
@@ -80,36 +64,30 @@ describe SM::Client, '#status' do
 
     context 'when message is not OH' do
       it 'does not poll and returns the message' do
-        # Return non-OH record
-        allow(client).to receive(:perform).and_return(
-          double(body: { data: { id: 99, is_oh_message: false }, metadata: {} })
-        )
-        expect(client).not_to receive(:poll_message_status)
+        VCR.use_cassette('sm_client/messages/creates/a_new_message_without_attachments') do
+          expect(client).not_to receive(:poll_message_status)
 
-        msg = client.post_create_message(subject: 's', category: 'OTHER', recipient_id: 1, body: 'b')
+          msg = client.post_create_message(subject: 's', category: 'OTHER', recipient_id: 1, body: 'b')
 
-        expect(msg).to be_a(Message)
-        expect(msg.id).to eq(99)
-        expect(msg.is_oh_message).to be(false)
+          expect(msg).to be_a(Message)
+          expect(msg.id).to eq(674_838)
+          expect(msg.is_oh_message).to be(false)
+        end
       end
     end
 
     context 'reply variants' do
-      it 'polls for OH replies and returns message on SENT' do
-        # First call (reply) returns OH message
-        allow(client).to receive(:perform).and_return(
-          double(body: { data: { id: 77, is_oh_message: true }, metadata: {} })
-        )
-        expect(client).to receive(:poll_message_status).with(77, hash_including(timeout_seconds: 60)).and_return(
-          { message_id: 77, status: 'SENT', is_oh_message: true }
-        )
+      it 'returns reply message' do
+        VCR.use_cassette('sm_client/messages/creates/a_reply_without_attachments') do
+          expect(client).not_to receive(:poll_message_status)
 
-        msg = client.post_create_message_reply(55, { subject: 's', category: 'OTHER', recipient_id: 1, body: 'b' },
-                                               poll_for_status: true)
+          msg = client.post_create_message_reply(674_838, { subject: 's', category: 'OTHER', recipient_id: 1, body: 'b' },
+                                                 poll_for_status: false)
 
-        expect(msg).to be_a(Message)
-        expect(msg.id).to eq(77)
-        expect(msg.is_oh_message).to be(true)
+          expect(msg).to be_a(Message)
+          expect(msg.id).to eq(674_874)
+          expect(msg.is_oh_message).to be(false)
+        end
       end
     end
   end
@@ -141,7 +119,7 @@ describe SM::Client, '#status' do
 
     it 'raises error after exceeding max_errors consecutive errors' do
       VCR.use_cassette('sm_client/messages/creates/status_error', allow_playback_repeats: true) do
-        expect { client.send(:poll_message_status, 674_838, max_errors: 2) }.to raise_error(SM::ServiceException)
+        expect { client.send(:poll_message_status, 674_838, max_errors: 2) }.to raise_error(Common::Exceptions::BackendServiceException)
       end
     end
 
