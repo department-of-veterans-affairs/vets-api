acd0e52942c779b079d43cf2bc0fc687f4e085b0
Evan Smith <evan.smith@oddball.io>
2025-10-20 21:48:23 -0400

    add new relationship to student attribute

--- modules/dependents_benefits/spec/factories/dependents_benefits/dependents_claim.rb
+++ modules/dependents_benefits/spec/factories/dependents_benefits/dependents_claim.rb
@@ -363,7 +363,7 @@ FactoryBot.define do
             },
             student_income: true,
             ssn: '987654321',
-            is_parent: true,
+            relationship_to_student: 'biological',
             full_name: {
               first: 'test',
               middle: 'middle',
@@ -587,7 +587,7 @@ FactoryBot.define do
             },
             'student_income' => true,
             'ssn' => '987654321',
-            'is_parent' => true,
+            'relationship_to_student' => 'biological',
             'full_name' => {
               'first' => 'test',
               'middle' => 'middle',
