65dc55c136fd1d9b2889bfe7b966d095a96062b4
Anna Carey <acarey@gmail.com>
2025-10-21 20:21:59 -0400

    stub controller response

--- app/controllers/v0/form212680_controller.rb
+++ app/controllers/v0/form212680_controller.rb
@@ -9,9 +9,22 @@ module V0
     service_tag 'form-21-2680'
 
     def download_pdf
-      render json: {
-        message: 'PDF generation stub - not yet implemented'
-      }, status: :ok
+      placeholder_response = {
+        message: 'PDF generation stub - not yet implemented',
+        instructions: {
+          title: 'Next Steps: Get Physician to Complete Form',
+          steps: [
+            'Download the pre-filled PDF below',
+            'Take the form to your physician',
+            'Have your physician complete Sections VI-VIII',
+            'Upload the completed form at: va.gov/upload-supporting-documents'
+          ],
+          upload_url: 'https://va.gov/upload-supporting-documents',
+          form_number: '21-2680',
+          regional_office: 'Department of Veterans Affairs, Pension Management Center,P.O. Box 5365, Janesville, WI 53547-5365'
+        }
+      }
+      render json: placeholder_response
     end
   end
 end
