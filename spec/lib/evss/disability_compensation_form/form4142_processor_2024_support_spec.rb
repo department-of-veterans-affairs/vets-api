c46bb32b27ec34cf6828ce8c7a3e996a66f56b30
nfstern02 <72567812+nfstern02@users.noreply.github.com>
2025-10-23 04:09:24 -0500

    pull in form4142_processor_2024_support_spec

--- spec/lib/evss/disability_compensation_form/form4142_processor_2024_support_spec.rb
+++ spec/lib/evss/disability_compensation_form/form4142_processor_2024_support_spec.rb
@@ -28,9 +28,24 @@ describe EVSS::DisabilityCompensationForm::Form4142Processor do
   let(:received_date) { submission.created_at.in_time_zone('Central Time (US & Canada)').strftime('%Y-%m-%d %H:%M:%S') }
   let(:form4142) { JSON.parse(form_json)['form4142'].merge({ 'signatureDate' => received_date }) }
 
+  let(:file_path) { 'tmp/pdfs/mock_form_final.pdf' }
+
   describe '#initialize' do
+    let(:mock_digest) { instance_double(Digest::SHA256, hexdigest: 'mocked_sha256_digest') }
+    let(:mock_page)   { instance_double(PDF::Reader::Page) }
+    let(:mock_reader) { instance_double(PDF::Reader, pages: Array.new(3, mock_page)) }
+
     before do
       allow(Flipper).to receive(:enabled?).with(:disability_526_form4142_validate_schema).and_return(false)
+
+      # Mock expensive PDF operations to avoid file I/O
+      allow(PdfFill::Filler).to receive(:fill_ancillary_form).and_return(file_path)
+      allow(Common::FileHelpers).to receive(:delete_file_if_exists)
+      datestamp_instance = instance_double(PDFUtilities::DatestampPdf, run: file_path)
+      allow(PDFUtilities::DatestampPdf).to receive(:new).and_return(datestamp_instance)
+      allow(Faraday::UploadIO).to receive(:new).with(file_path, 'application/pdf')
+      allow(Digest::SHA256).to receive(:file).with(file_path).and_return(mock_digest)
+      allow(PDF::Reader).to receive(:new).with(file_path).and_return(mock_reader)
     end
 
     it 'initializes with submission and jid' do
@@ -39,8 +54,6 @@ describe EVSS::DisabilityCompensationForm::Form4142Processor do
       end
       expected = form4142.clone.merge('providerFacility' => expected_provider_facility)
       expect(PdfFill::Filler).to receive(:fill_ancillary_form)
-        .and_call_original
-        .once
         .with(expected, submission.submitted_claim_id, '21-4142-2024')
 
       expect(processor.instance_variable_get(:@pdf_path)).to be_a(String)
@@ -93,7 +106,7 @@ describe EVSS::DisabilityCompensationForm::Form4142Processor do
       let(:legacy_form_json) { File.read('spec/support/disability_compensation_form/submissions/with_4142.json') }
       let(:legacy_processor) { described_class.new(submission_with_legacy4142, jid) }
       let(:legacy_received_date) do
-        submission_with_legacy4142.created_at.in_time_zone('Central Time (US & Canada)').strftime('%Y-%m-%d %H:%M:%S')
+        submission_with_legacy4142.created_at.in_time_zone('Central Time (US & Canada)')
       end
       let(:submission_with_legacy4142) do
         create(:form526_submission,
@@ -104,17 +117,24 @@ describe EVSS::DisabilityCompensationForm::Form4142Processor do
                submitted_claim_id: 2)
       end
       let(:saved_claim2) { create(:va526ez) }
-      let(:legacy_form4142) { JSON.parse(legacy_form_json)['form4142'].merge({ 'signatureDate' => received_date }) }
+      let(:legacy_form4142) do
+        JSON.parse(legacy_form_json)['form4142']
+            .merge({ 'signatureDate' => legacy_received_date.strftime('%Y-%m-%d %H:%M:%S') })
+      end
+
+      before do
+        allow(PdfFill::Filler).to receive(:fill_form).and_return(file_path)
+      end
 
       it 'parses frontend submission data and uses the legacy template' do
-        expect(PdfFill::Filler).to receive(:fill_ancillary_form)
-          .and_call_original
-          .once
-          .with(legacy_form4142, submission_with_legacy4142.submitted_claim_id, '21-4142')
+        Timecop.freeze(legacy_received_date) do
+          expect(PdfFill::Filler).to receive(:fill_ancillary_form)
+            .with(legacy_form4142, submission_with_legacy4142.submitted_claim_id, '21-4142')
 
-        expect(legacy_processor.instance_variable_get(:@submission)).to eq(submission_with_legacy4142)
-        expect(legacy_processor.instance_variable_get(:@pdf_path)).to be_a(String)
-        expect(legacy_processor.instance_variable_get(:@request_body)).to be_a(Hash)
+          expect(legacy_processor.instance_variable_get(:@submission)).to eq(submission_with_legacy4142)
+          expect(legacy_processor.instance_variable_get(:@pdf_path)).to be_a(String)
+          expect(legacy_processor.instance_variable_get(:@request_body)).to be_a(Hash)
+        end
       end
     end
 
