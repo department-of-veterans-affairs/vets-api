30ccc5fe57619e948d15f9edaad60e02c588ab3e
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-10-23 20:48:25 -0500

    condensed before dos

--- modules/claims_api/spec/requests/v2/veterans/rswag_526_spec.rb
+++ modules/claims_api/spec/requests/v2/veterans/rswag_526_spec.rb
@@ -557,11 +557,6 @@ describe 'DisabilityCompensation', openapi_spec: Rswag::TextHelpers.new.claims_a
           end
 
           context 'when federalActivation is present but obligationTermsOfService is missing' do
-            before do
-              allow(Flipper).to receive(:enabled?).with(:lighthouse_claims_api_v2_enable_FES).and_return(false)
-              make_request(example)
-            end
-
             def make_request(example)
               allow(Flipper).to receive(:enabled?).with(:claims_load_testing).and_return false
 
@@ -595,6 +590,11 @@ describe 'DisabilityCompensation', openapi_spec: Rswag::TextHelpers.new.claims_a
             end
 
             let(:disability_comp_request) { data }
+
+            before do |example|
+              allow(Flipper).to receive(:enabled?).with(:lighthouse_claims_api_v2_enable_FES).and_return(false)
+              make_request(example)
+            end
 
             after do |example|
               append_example_metadata(example, response)
