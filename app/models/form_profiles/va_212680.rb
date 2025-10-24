9de47f2b3d656eb08ae6333f28421ca41b3e14da
Justin Linn <justin@linnjs.com>
2025-10-23 23:06:59 -0400

    fix(2680): Add CODEOWNERS

--- app/models/form_profiles/va_212680.rb
+++ app/models/form_profiles/va_212680.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# FormProfile for VA Form 21-2680
+# Examination for Housebound Status or Permanent Need for Regular Aid and Attendance
 class FormProfiles::VA212680 < FormProfile
   def metadata
     {
