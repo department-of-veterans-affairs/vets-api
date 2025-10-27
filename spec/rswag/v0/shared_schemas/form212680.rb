97830b0f606cdceba1628631842c43772c6c2baf
Anna Carey <acarey@gmail.com>
2025-10-27 10:24:59 -0400

    additional openapi nullable and formats

--- spec/rswag/v0/shared_schemas/form212680.rb
+++ spec/rswag/v0/shared_schemas/form212680.rb
@@ -7,13 +7,13 @@ module Rswag
         FORM_212680_FULL_NAME =
           { type: 'object',
             properties: { first: { type: 'string', example: 'John', maxLength: 12 },
-                          middle: { type: 'string', example: 'A', maxLength: 1 },
+                          middle: { type: 'string', example: 'A', maxLength: 1, nullable: true },
                           last: { type: 'string', example: 'Doe', maxLength: 18 } } }.freeze
 
         FORM_212680_ADDRESS =
           { type: 'object',
             properties: { street: { type: 'string', example: '123 Main St', maxLength: 30 },
-                          street2: { type: 'string', example: 'Apt 4B', maxLength: 5 },
+                          street2: { type: 'string', example: 'Apt 4B', maxLength: 5, nullable: true },
                           city: { type: 'string', example: 'Springfield', maxLength: 18 },
                           state: { type: 'string', example: 'IL', maxLength: 2 },
                           postalCode: { type: 'string', example: '62701', maxLength: 9 },
