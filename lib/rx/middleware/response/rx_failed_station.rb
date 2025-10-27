df226df92656e1206f19a7fff79304272c0b280b
Kevin Suarez <kjsuarez94@gmail.com>
2025-10-27 11:52:24 -0400

    120547 removing more references to sentry

--- lib/rx/middleware/response/rx_failed_station.rb
+++ lib/rx/middleware/response/rx_failed_station.rb
@@ -7,7 +7,7 @@ module Rx
       # Middleware class responsible for logging Rx Failed Station messages to Sentry
       #
       class RxFailedStation < Faraday::Middleware
-        include SentryLogging
+        include Vets::SharedLogging
         ##
         # Override the Faraday #on_complete method to log Rx failed station message to Sentry
         # @param env [Faraday::Env] the request environment
@@ -20,7 +20,7 @@ module Rx
           return if station_list.blank?
 
           message = "Warning: prescription failed station list is not empty, '#{station_list}'"
-          log_message_to_sentry(message, :warn)
+          log_message_to_rails(message, 'warn')
         end
       end
     end
