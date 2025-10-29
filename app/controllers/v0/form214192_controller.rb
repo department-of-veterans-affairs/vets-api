9c5eadb983385dc2581db90c346945e7c39ada6a
Cameron Testerman <Cameron.Testerman@va.gov>
2025-10-28 20:25:21 -0500

    just target params in the body, using request.request_parameters

--- app/controllers/v0/form214192_controller.rb
+++ app/controllers/v0/form214192_controller.rb
@@ -9,6 +9,7 @@ module V0
 
     service_tag 'employment-information'
     skip_before_action :authenticate, only: %i[create download_pdf]
+    skip_before_action :verify_authenticity_token, only: %i[create download_pdf]
 
     def create
       confirmation_number = SecureRandom.uuid
@@ -30,10 +31,7 @@ module V0
     end
 
     def download_pdf
-      # permit! is ok here because:
-      # - no mass assignment is occurring. nothing is changing at all in the db.
-      # - the paramaters are being validated by committee-rails.
-      parsed_form = params.permit!.to_h
+      parsed_form = request.request_parameters
 
       source_file_path = with_retries('Generate 21-4192 PDF') do
         PdfFill::Filler.fill_ancillary_form(parsed_form, SecureRandom.uuid, '21-4192')
