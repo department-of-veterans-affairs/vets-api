fc071bbbb94caaeb2e8a328f1563dcbcf65c6d72
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2025-10-29 15:43:48 -0500

    Fixing yardoc issues

--- modules/pensions/lib/pensions/pdf_fill/sections/section_05.rb
+++ modules/pensions/lib/pensions/pdf_fill/sections/section_05.rb
@@ -9,6 +9,15 @@ module Pensions
       # Section configuration hash
       KEY = {}.freeze
 
+      ##
+      # Expand the form data for prior marital history.
+      #
+      # @param form_data [Hash] The form data hash.
+      #
+      # @return [void]
+      #
+      # Note: This method modifies `form_data`
+      #
       def expand(form_data)
         # Add expansion logic here
       end
