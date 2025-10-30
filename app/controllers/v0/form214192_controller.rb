db521dfc1a3c88050cde42d0821de6c0988aa6d3
Cameron Testerman <Cameron.Testerman@va.gov>
2025-10-30 18:06:57 -0500

    Fix 21-4192 Signature at the bottom

--- app/controllers/v0/form214192_controller.rb
+++ app/controllers/v0/form214192_controller.rb
@@ -6,6 +6,7 @@ module V0
 
     service_tag 'employment-information'
     skip_before_action :authenticate, only: %i[create download_pdf]
+    skip_before_action :verify_authenticity_token, only: %i[create download_pdf]
 
     def create
       # Body parsed by Rails; schema validated by committee before hitting here.
@@ -41,6 +42,9 @@ module V0
       source_file_path = with_retries('Generate 21-4192 PDF') do
         PdfFill::Filler.fill_ancillary_form(parsed_form, SecureRandom.uuid, '21-4192')
       end
+
+      # Stamp signature (SignatureStamper returns original path if signature is blank)
+      source_file_path = PdfFill::Forms::Va214192.stamp_signature(source_file_path, parsed_form)
 
       client_file_name = "21-4192_#{SecureRandom.uuid}.pdf"
 
