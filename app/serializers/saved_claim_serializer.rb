a66a468d50d0885cb3dcc659b6a0fd578f9ef7cd
Joshua K. Farrar <435746+joshuakfarrar@users.noreply.github.com>
2025-10-30 15:44:18 -0400

    add pdf_url to SavedClaimSerializer for 21P-8416

--- app/serializers/saved_claim_serializer.rb
+++ app/serializers/saved_claim_serializer.rb
@@ -8,4 +8,8 @@ class SavedClaimSerializer
   attributes :submitted_at, :regional_office, :confirmation_number, :guid
 
   attribute :form, &:form_id
+
+  attribute :pdfUrl, if: proc { |_record, params| params && params[:pdf_url].present? } do |_record, params|
+    params[:pdf_url]
+  end
 end
