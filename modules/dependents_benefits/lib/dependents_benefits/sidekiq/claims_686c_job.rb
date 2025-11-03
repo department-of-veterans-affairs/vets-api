923280d9b52b474e0b562bdcdff427f9903ecd9e
David Becker <david@beckersite.com>
2025-11-03 14:32:02 -0500

    Move claims evidence shared logic to claim behavior concern

--- modules/dependents_benefits/lib/dependents_benefits/sidekiq/claims_686c_job.rb
+++ modules/dependents_benefits/lib/dependents_benefits/sidekiq/claims_686c_job.rb
@@ -8,6 +8,7 @@ require 'claims_evidence_api/uploader'
 module DependentsBenefits
   module Sidekiq
     class Claims686cJob < DependentSubmissionJob
+      FORM_ID = DependentsBenefits::ADD_REMOVE_DEPENDENT.freeze
       ##
       # Service-specific submission logic - BGS vs Lighthouse vs Fax
       # @return [ServiceResponse] Must respond to success? and error methods
@@ -16,21 +17,16 @@ module DependentsBenefits
 
         raise Invalid686cClaim unless saved_claim.valid?(:run_686_form_jobs)
 
-        ssn = user_data.dig('veteran_information', 'ssn')
-        participant_id = user_data.dig('veteran_information', 'participant_id')
-        icn = user_data.dig('veteran_information', 'icn')
-        ce_uploader = ClaimsEvidenceApi::Uploader.new(folder_identifier(ssn:, participant_id:, icn:))
-        form_id = DependentsBenefits::ADD_REMOVE_DEPENDENT
-        lighthouse_submission = DependentsBenefits::BenefitsIntake::LighthouseSubmission.new(saved_claim, user_data)
         file_path = lighthouse_submission.process_pdf(
-          saved_claim.to_pdf(form_id:),
-          saved_claim.created_at, form_id
+          saved_claim.to_pdf(form_id: FORM_ID),
+          saved_claim.created_at,
+          FORM_ID
         )
 
-        ce_uploader.upload_evidence(
+        claims_evidence_uploader.upload_evidence(
           saved_claim.id,
           file_path:,
-          form_id:,
+          form_id: FORM_ID,
           doctype: saved_claim.document_type
         )
 
@@ -86,20 +82,12 @@ module DependentsBenefits
         BGS::Job::FILTERED_ERRORS.any? { |filtered| error.message.include?(filtered) || error.cause&.message&.include?(filtered) }
       end
 
-      # def claims_evidence_uploader
-      #   @ce_uploader ||= ClaimsEvidenceApi::Uploader.new(saved_claim.folder_identifier)
-      # end
+      def claims_evidence_uploader
+        @ce_uploader ||= ClaimsEvidenceApi::Uploader.new(saved_claim.folder_identifier)
+      end
 
-      def folder_identifier(ssn:, participant_id:, icn:)
-        fid = 'VETERAN'
-        { ssn:, participant_id:, icn: }.each do |k, v|
-          if v.present?
-            fid += ":#{k.to_s.upcase}:#{v}"
-            break
-          end
-        end
-
-        fid
+      def lighthouse_submission
+        @lighthouse_submission ||= DependentsBenefits::BenefitsIntake::LighthouseSubmission.new(saved_claim, user_data)
       end
     end
   end
