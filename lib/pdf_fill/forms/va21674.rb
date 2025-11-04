1a84a1f8d2545833580a09ef74e02dc8defb1ade
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-11-04 15:55:39 -0500

    Handle missing vet info

--- lib/pdf_fill/forms/va21674.rb
+++ lib/pdf_fill/forms/va21674.rb
@@ -560,6 +560,9 @@ module PdfFill
           @form_data['veteran_information'] = @form_data.dig('dependents_application', 'veteran_information')
         end
 
+        # We add the user data to the form data in some jobs. It should always be present here.
+        raise 'Veteran information is missing' unless @form_data['veteran_information']
+
         expand_signature(@form_data['veteran_information']['full_name'], created_at&.to_date || Time.zone.today)
 
         @form_data['signature_date'] = split_date(@form_data['signatureDate'])
