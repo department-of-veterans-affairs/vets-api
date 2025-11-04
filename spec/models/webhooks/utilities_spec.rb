fb5640e7da2f71a7dec24346db692a173b402881
Kristen Brown <11942904+kristen-brown@users.noreply.github.com>
2025-11-04 09:49:08 -0500

    API-48806: Remove webhook models

--- spec/models/webhooks/utilities_spec.rb
+++ spec/models/webhooks/utilities_spec.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require './lib/webhooks/utilities'
 
-# use these loads when running in a rails console
-# load './app/models/webhooks/notification.rb'
-# load './app/models/webhooks/subscription.rb'
-# load './lib/webhooks/utilities.rb'
-# load './app/models/webhooks/utilities.rb'
-
-describe Webhooks::Utilities, type: :model do
+describe Webhooks::Utilities, type: :model, skip: 'specs to be removed in future PR' do
   API_NAME = 'testing'
   let(:consumer_id) { 'f7d83733-a047-413b-9cce-e89269dcb5b1' }
   let(:consumer_name) { 'tester' }
