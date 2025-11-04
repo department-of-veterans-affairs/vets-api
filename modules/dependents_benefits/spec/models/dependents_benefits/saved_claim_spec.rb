8bc7f871d83b6305772f2abf06b64b5423addc3c
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-11-04 12:52:07 -0500

    Fix nil form_id

---
+++ modules/dependents_benefits/spec/models/dependents_benefits/saved_claim_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DependentsBenefits::SavedClaim do
+  before do
+    allow_any_instance_of(DependentsBenefits::SavedClaim).to receive(:pdf_overflow_tracking)
+  end
+
+  let(:saved_claim) { create(:dependents_claim) }
+
+  describe '#form_id' do
+    it 'returns the correct form id' do
+      claim = DependentsBenefits::SavedClaim.new(form: saved_claim.form)
+      claim.save!
+      expect(claim.form_id).to eq('686C-674')
+    end
+  end
+end
