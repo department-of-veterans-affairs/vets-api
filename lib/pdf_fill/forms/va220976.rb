3afdebce0410e7d880b2481c241f72904dd9fcf0
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-11-06 13:03:31 -0800

    spec coverage for 0976 pdf filling

--- lib/pdf_fill/forms/va220976.rb
+++ lib/pdf_fill/forms/va220976.rb
@@ -6,10 +6,10 @@ module PdfFill
       include FormHelper
 
       INSTITUTION_TYPE_ENUM = {
-        "public" => 'PUBLIC',
-        "privateForProfit" => 'PRIVATE-FOR-PROFIT',
-        "privateNotForProfit" => 'PRIVATE-NOT-FOR-PROFIT'
-      }
+        'public' => 'PUBLIC',
+        'privateForProfit' => 'PRIVATE-FOR-PROFIT',
+        'privateNotForProfit' => 'PRIVATE-NOT-FOR-PROFIT'
+      }.freeze
 
       ITERATOR = PdfFill::HashConverter::ITERATOR
 
@@ -32,7 +32,7 @@ module PdfFill
           },
           'website' => {
             key: 'institution_website'
-          },
+          }
         },
         'generalInfo' => {
           'typeInitial' => {
@@ -80,56 +80,56 @@ module PdfFill
           },
           'address' => {
             key: "branch_#{ITERATOR}_address"
-          },
+          }
         },
         'programs' => {
           limit: 4,
           label_all: true,
-          "programName" => {
+          'programName' => {
             key: "degree_program_#{ITERATOR}_name"
           },
-          "totalProgramLength" => {
+          'totalProgramLength' => {
             key: "degree_program_#{ITERATOR}_length"
           },
-          "weeksPerTerm" => {
+          'weeksPerTerm' => {
             key: "degree_program_#{ITERATOR}_weeks_per_term"
           },
-          "entryRequirements" => {
+          'entryRequirements' => {
             key: "degree_program_#{ITERATOR}_entry_requirements"
           },
-          "creditHours" => {
+          'creditHours' => {
             key: "degree_program_#{ITERATOR}_credit_hours"
           }
         },
-        "acknowledgement7" => {
+        'acknowledgement7' => {
           key: 'authorizing_initials_1'
         },
-        "acknowledgement8" => {
+        'acknowledgement8' => {
           key: 'authorizing_initials_2'
         },
-        "acknowledgement9" => {
+        'acknowledgement9' => {
           key: 'authorizing_initials_3'
         },
-        "acknowledgement10a" => {
-          "financiallySound" => {
+        'acknowledgement10a' => {
+          'financiallySound' => {
             key: 'is_financially_sound'
           },
-          "financialSoundnessExplanation" => {
+          'financialSoundnessExplanation' => {
             key: 'financially_sound_description'
           }
         },
-        "acknowledgement10b" => {
+        'acknowledgement10b' => {
           key: 'authorizing_initials_4'
         },
         'faculty' => {
           limit: 7,
           label_all: true,
-          "name" => {
+          'name' => {
             key: "faculty_#{ITERATOR}_name"
           },
-          "title" => {
+          'title' => {
             key: "faculty_#{ITERATOR}_title"
-          },
+          }
         },
         'medicalData' => {
           'isInWDOMS' => {
@@ -175,7 +175,7 @@ module PdfFill
           },
           'authorizingOfficialSignature' => {
             key: 'authorizing_official_signature'
-          },
+          }
         },
         'dateSigned' => {
           key: 'date_signed'
@@ -196,9 +196,16 @@ module PdfFill
 
       def format_institutions(form_data)
         form_data['primaryInstitution'] = form_data['institutionDetails'].first
-        form_data['primaryInstitution']['physicalAddress'] = combine_full_address(form_data['primaryInstitution']['physicalAddress'])
-        form_data['primaryInstitution']['mailingAddress'] = combine_full_address(form_data['primaryInstitution']['mailingAddress'])
-        form_data['primaryInstitution']['country'] = form_data['primaryInstitution']['isForeignCountry'] ? form_data['primaryInstitution']['physicalAddress']['country'] : ''
+        form_data['primaryInstitution']['physicalAddress'] =
+          combine_full_address(form_data['primaryInstitution']['physicalAddress'])
+        form_data['primaryInstitution']['mailingAddress'] =
+          combine_full_address(form_data['primaryInstitution']['mailingAddress'])
+        form_data['primaryInstitution']['country'] =
+          if form_data['primaryInstitution']['isForeignCountry']
+            form_data['primaryInstitution']['physicalAddress']['country']
+          else
+            ''
+          end
         form_data['primaryInstitution']['website'] = form_data['website']
 
         form_data['branches'] = form_data['institutionDetails'][1..].map do |data|
@@ -227,7 +234,8 @@ module PdfFill
       end
 
       def format_acknowledgements(form_data)
-        form_data["acknowledgement10a"]['financiallySound'] = form_data["acknowledgement10a"]['financiallySound'] ? 'YES' : 'NO'
+        form_data['acknowledgement10a']['financiallySound'] =
+          form_data['acknowledgement10a']['financiallySound'] ? 'YES' : 'NO'
       end
 
       def format_faculty(form_data)
@@ -240,7 +248,7 @@ module PdfFill
       end
 
       def format_medical_data(form_data)
-        unless form_data["isMedicalSchool"]
+        unless form_data['isMedicalSchool']
           form_data['medicalData'] = {}
           return
         end
@@ -249,7 +257,7 @@ module PdfFill
           'isInWDOMS' => form_data['listedInWDOMS'] ? 'YES' : 'NO',
           'accreditingAgency' => form_data['accreditingAuthorityName'],
           'providesClassroomInstruction' => form_data['programAtLeast32Months'] ? 'YES' : 'NO',
-          'hasRecentGraduatingClasses' => form_data['graduatedLast12Months'] ? 'YES' : 'NO',
+          'hasRecentGraduatingClasses' => form_data['graduatedLast12Months'] ? 'YES' : 'NO'
         }
         if form_data['graduatedClasses'].present? && form_data['graduatedClasses'].size >= 2
           form_data['medicalData']['graduation1Date'] = form_data['graduatedClasses'][0]['graduationDate']
