d73eabdf61fb337c60b56535484238285de5218f
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-11-08 20:33:52 -0500

    114566 Copay List Endpoint Part_1

---
+++ app/services/medical_copays/lighthouse_integration/service.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'lighthouse/healthcare_cost_and_coverage/account/service'
+require 'lighthouse/healthcare_cost_and_coverage/invoice/service'
+require 'lighthouse/healthcare_cost_and_coverage/charge_item/service'
+
+module MedicalCopays
+  module LighthouseIntegration
+    class Service
+      def initialize(icn)
+        @icn = icn
+      end
+
+      def list(count:, page:)
+        # Datadog stuff
+        raw_invoices = invoice_service.list(count:, page:)
+        entries = raw_invoices['entry'].map do |entry|
+          ::Lighthouse::HCC::Invoice.new(entry)
+        end
+
+        Lighthouse::HCC::Bundle.new(raw_invoices, entries)
+      rescue => e
+        # Datadog stuff here?
+        Rails.logger.error("MedicalCopays::Lighthouse::Service#list error: #{e.message}")
+        raise e
+      end
+
+      def invoice_service
+        ::Lighthouse::HealthcareCostAndCoverage::Invoice::Service.new(@icn)
+      end
+    end
+  end
+end
