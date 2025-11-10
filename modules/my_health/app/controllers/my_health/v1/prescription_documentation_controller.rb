d11b6759ddab1a6f7bd2d338fccfe33f038e5e26
Andrea Uhr <andreauhr@Andrea-Uhr.local>
2025-11-10 13:16:40 -0500

    Add comments for clarification about upstream service

--- modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
@@ -8,8 +8,19 @@ module MyHealth
         rx = client.get_rx_details(id)
         raise StandardError, 'Rx not found' if rx.nil?
         raise StandardError, 'Missing NDC number' if rx.cmop_ndc_value.nil?
-
+        # Get Prescription Documentation Endpoint
+        # https://api.krames.com/v3/content/search?ndc=<NDC>
+        #
+        # @param [rx.cmop_ndc_value] - NDC number of the prescription to fetch documentation for
+        #
+        # @return [documentation] - Drug documentation JSON object from the API
         documentation = client.get_rx_documentation(rx.cmop_ndc_value)
+        # Build PrescriptionDocumentation object
+        # https://api.krames.com/v3/content/2<ContentType>-<ContentID>
+        #
+        # @param [documentation] - Drug sheet JSON object from the API
+        #
+        # @return [prescription_documentation] - PrescriptionDocumentation object with HTML content containing drug information
         prescription_documentation = PrescriptionDocumentation.new({ html: documentation[:data] })
         render json: PrescriptionDocumentationSerializer.new(prescription_documentation)
       rescue => e
