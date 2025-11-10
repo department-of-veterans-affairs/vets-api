917288ecebcd798523cb8d0cd5b32a86de5e802b
Andrea Uhr <andreauhr@Andrea-Uhr.local>
2025-11-10 13:29:12 -0500

    corrected syntax

--- modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
@@ -8,24 +8,20 @@ module MyHealth
         rx = client.get_rx_details(id)
         raise StandardError, 'Rx not found' if rx.nil?
         raise StandardError, 'Missing NDC number' if rx.cmop_ndc_value.nil?
-        # Get Prescription Documentation Endpoint
+        # Fetch prescription documentation using the NDC number
         # https://api.krames.com/v3/content/search?ndc=<NDC>
         #
-        # @param [rx.cmop_ndc_value] - NDC number of the prescription to fetch documentation for
-        #
-        # @return [documentation] - Drug documentation JSON object from the API
+        # documentation: Drug documentation JSON object from the API
         documentation = client.get_rx_documentation(rx.cmop_ndc_value)
         # Build PrescriptionDocumentation object
         # https://api.krames.com/v3/content/2<ContentType>-<ContentID>
         #
-        # @param [documentation] - Drug sheet JSON object from the API
-        #
-        # @return [prescription_documentation] - PrescriptionDocumentation object with HTML content containing drug information
+        # prescription_documentation: PrescriptionDocumentation object with HTML content containing drug information
         prescription_documentation = PrescriptionDocumentation.new({ html: documentation[:data] })
         render json: PrescriptionDocumentationSerializer.new(prescription_documentation)
       rescue => e
         render json: { error: "Unable to fetch documentation: #{e}" }, status: :service_unavailable
       end
     end
-  end
+  endx
 end
