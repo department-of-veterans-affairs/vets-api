81a94c4b40683f0fa2ca567d14f9aa85253a6da7
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2025-11-10 13:21:03 -0600

    Moving sections 3, 4, and 5 into their section files

--- modules/pensions/lib/pensions/pdf_fill/sections/section_03.rb
+++ modules/pensions/lib/pensions/pdf_fill/sections/section_03.rb
@@ -7,7 +7,122 @@ module Pensions
     # Section III: Veteran Service Information
     class Section3 < Section
       # Section configuration hash
-      KEY = {}.freeze
+      KEY = {
+        # 3a
+        'previousNames' => {
+          item_label: 'Other service name',
+          limit: 1,
+          first_key: 'first',
+          'first' => {
+            limit: 12,
+            question_num: 3,
+            question_suffix: 'A',
+            question_label: 'Other First Name',
+            question_text: 'OTHER FIRST NAME',
+            key: "form1[0].#subform[48].Other_Name_You_Served_Under_First_Name[#{ITERATOR}]"
+          },
+          'last' => {
+            limit: 18,
+            question_num: 3,
+            question_suffix: 'A',
+            question_label: 'Other Last Name',
+            question_text: 'OTHER LAST NAME',
+            key: "form1[0].#subform[48].Other_Name_You_Served_Under_Last_Name[#{ITERATOR}]"
+          }
+        },
+        # 3b
+        'activeServiceDateRange' => {
+          'from' => {
+            'month' => {
+              key: 'form1[0].#subform[48].Date_Entered_Active_Duty_Month[0]'
+            },
+            'day' => {
+              key: 'form1[0].#subform[48].Date_Entered_Active_Duty_Day[0]'
+            },
+            'year' => {
+              key: 'form1[0].#subform[48].Date_Entered_Active_Duty_Year[0]'
+            }
+          },
+          'to' => {
+            'month' => {
+              key: 'form1[0].#subform[48].Date_Of_Release_From_Active_Duty_Month[0]'
+            },
+            'day' => {
+              key: 'form1[0].#subform[48].Date_Of_Release_From_Active_Duty_Day[0]'
+            },
+            'year' => {
+              key: 'form1[0].#subform[48].Date_Of_Release_From_Active_Duty_Year[0]'
+            }
+          }
+        },
+        # 3e
+        'serviceBranch' => {
+          'army' => {
+            key: 'form1[0].#subform[48].Army[0]'
+          },
+          'navy' => {
+            key: 'form1[0].#subform[48].Navy[0]'
+          },
+          'airForce' => {
+            key: 'form1[0].#subform[48].Air_Force[0]'
+          },
+          'coastGuard' => {
+            key: 'form1[0].#subform[48].Coast_Guard[0]'
+          },
+          'marineCorps' => {
+            key: 'form1[0].#subform[48].Marine_Corps[0]'
+          },
+          'spaceForce' => {
+            key: 'form1[0].#subform[48].Space_Force[0]'
+          },
+          'usphs' => {
+            key: 'form1[0].#subform[48].USPHS[0]'
+          },
+          'noaa' => {
+            key: 'form1[0].#subform[48].NOAA[0]'
+          }
+        },
+        # 3d
+        'serviceNumber' => {
+          key: 'form1[0].#subform[48].Your_Service_Number[0]'
+        },
+        # 3f
+        'placeOfSeparationLineOne' => {
+          key: 'form1[0].#subform[48].Place_Of_Your_Last_Separation[1]'
+        },
+        'placeOfSeparationLineTwo' => {
+          key: 'form1[0].#subform[48].Place_Of_Your_Last_Separation[0]'
+        },
+        # 3g
+        'pow' => {
+          key: 'form1[0].#subform[48].RadioButtonList[1]'
+        },
+        # 3h
+        'powDateRange' => {
+          'from' => {
+            'month' => {
+              key: 'form1[0].#subform[48].Date_Confinement_Started_Month[1]'
+            },
+            'day' => {
+              key: 'form1[0].#subform[48].Date_Confinement_Started_Day[1]'
+            },
+            'year' => {
+              key: 'form1[0].#subform[48].Date_Confinement_Started_Year[1]'
+            }
+          },
+          'to' => {
+            'month' => {
+              key: 'form1[0].#subform[48].Date_Confinement_Ended_Month[1]'
+            },
+            'day' => {
+              key: 'form1[0].#subform[48].Date_Confinement_Ended_Day[1]'
+            },
+            'year' => {
+              key: 'form1[0].#subform[48].Date_Confinement_Ended_Year[1]'
+            }
+          }
+        }
+      }.freeze
 
       ##
       # Expand the form data for Veteran service history.
