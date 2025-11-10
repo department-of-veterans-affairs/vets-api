8ccfdf7c63a558bff521421fe1ae90fe6f399f3c
Diana20920 <diana.pamelaet@gmail.com>
2025-11-10 09:57:26 -0700

    Add logging to telephone controllers for PHON301 investigation

--- app/controllers/v0/profile/telephones_controller.rb
+++ app/controllers/v0/profile/telephones_controller.rb
@@ -7,6 +7,7 @@ module V0
       service_tag 'profile'
 
       before_action { authorize :va_profile, :access_to_v2? }
+      before_action :log_telephone_request
       after_action :invalidate_cache
 
       def create
@@ -23,6 +24,7 @@ module V0
           telephone_params,
           http_verb: 'update'
         )
+        Rails.logger.warn('TelephonesController#create_or_update request completed', sso_logging_info)
       end
 
       def update
@@ -44,6 +46,21 @@ module V0
       end
 
       private
+
+      def log_telephone_request
+        log_data = {
+          action: action_name,
+          phone_type: params[:phone_type],
+          has_id: params[:id].present?,
+          phone_id: params[:id],
+          transaction_id: params[:transaction_id],
+          user_agent: request.user_agent,
+          request_id: request.request_id,
+          timestamp: Time.now.utc.iso8601
+        }.merge(sso_logging_info)
+
+        Rails.logger.info('TelephonesController request initiated', log_data)
+      end
 
       def telephone_params
         params.permit(
--- modules/mobile/app/controllers/mobile/v0/phones_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/phones_controller.rb
@@ -5,6 +5,8 @@ require 'common/exceptions/validation_errors'
 module Mobile
   module V0
     class PhonesController < ProfileBaseController
+      before_action :log_phone_request
+
       def create
         render_transaction_to_json(
           service.save_and_await_response(resource_type: :telephone, params: phone_params)
@@ -25,6 +27,23 @@ module Mobile
       end
 
       private
+
+      def log_phone_request
+        log_data = {
+          controller: 'Mobile::V0::PhonesController',
+          action: action_name,
+          phone_type: params[:phone_type],
+          has_id: params[:id].present?,
+          phone_id: params[:id],
+          transaction_id: params[:transaction_id],
+          user_agent: request.user_agent,
+          request_id: request.request_id,
+          timestamp: Time.now.utc.iso8601,
+          sis_user_uuid: current_user&.sis_uuid
+        }
+
+        Rails.logger.info('Mobile PhonesController request initiated', log_data)
+      end
 
       def phone_params
         params.permit(
