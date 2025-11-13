7dddf1ef3bf603fe5c8aeab6b910d61d329c2c9e
Anna Carey <acarey@gmail.com>
2025-11-13 09:05:03 -0500

    add saved_claim/form210779_spec.rb

--- app/models/saved_claim/form210779.rb
+++ app/models/saved_claim/form210779.rb
@@ -2,6 +2,7 @@
 
 class SavedClaim::Form210779 < SavedClaim
   FORM = '21-0779'
+  before_validation :duplicate_data_for_lighthouse
 
   def process_attachments!
     # Form 21-0779 does not support user-uploaded attachments in MVP
@@ -34,5 +35,30 @@ class SavedClaim::Form210779 < SavedClaim
     first = parsed_form.dig('veteranInformation', 'fullName', 'first')
     last = parsed_form.dig('veteranInformation', 'fullName', 'last')
     "#{first} #{last}".strip.presence || 'Veteran'
+  end
+
+  def send_to_benefits_intake_api
+    Lighthouse::SubmitBenefitsIntakeClaim.new.perform(id)
+  end
+
+  # Lighthouse::SubmitBenefitsIntakeClaim#generate_metadata makes the assumption that `claim.parsed_form`
+  # has the following attributes:
+  # {
+  #   veteranFullName: {first: "", last: ""}
+  #   vaFileNumber: "", # either vaFileNumber OR veteranSocialSecurityNumber if file number is null
+  #   veteranSocialSecurityNumber: "",
+  #   veteranAddress: {postalCode: ""} # or claimantAddress
+  # }
+
+  def duplicate_data_for_lighthouse
+    unless parsed_form['veteranFullName']
+      updated_form = parsed_form
+      updated_form['veteranFullName'] = parsed_form.dig('veteranInformation', 'fullName')
+      updated_form['veteranAddress'] = parsed_form.dig('nursingHomeInformation', 'nursingHomeAddress')
+      updated_form['vaFileNumber'] = parsed_form.dig('veteranInformation', 'veteranId', 'vaFileNumber')
+      updated_form['veteranSocialSecurityNumber'] = parsed_form.dig('veteranInformation', 'veteranId', 'ssn')
+      @parsed_form = updated_form # because parsed_form memoizes
+      self.form = updated_form.to_json
+    end
   end
 end
