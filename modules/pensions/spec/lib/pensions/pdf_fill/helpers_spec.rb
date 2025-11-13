11757214de5aa5728788c9a7d0e5f846a78af05c
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2025-11-13 13:54:05 -0600

    Fixing linting issues

--- modules/pensions/spec/lib/pensions/pdf_fill/helpers_spec.rb
+++ modules/pensions/spec/lib/pensions/pdf_fill/helpers_spec.rb
@@ -3,16 +3,16 @@
 require 'pensions/pdf_fill/helpers'
 
 describe Pensions::PdfFill::Helpers do
-    let(:helper_class) do
+  let(:helper_class) do
     Class.new do
       include Pensions::PdfFill::Helpers
-      
+
       def initialize(data = {})
         @form_data = data
       end
     end
   end
-  
+
   let(:helper) { helper_class.new({}) }
 
   describe '#to_radio_yes_no' do
@@ -35,20 +35,20 @@ describe Pensions::PdfFill::Helpers do
       expect(helper.split_currency_amount(-1)).to eq({})
       expect(helper.split_currency_amount(nil)).to eq({})
       expect(helper.split_currency_amount(100)).to eq({
-                                                                         'part_one' => '100',
-                                                                         'part_cents' => '00'
-                                                                       })
+                                                        'part_one' => '100',
+                                                        'part_cents' => '00'
+                                                      })
       expect(helper.split_currency_amount(999_888.77)).to eq({
-                                                                                'part_two' => '999',
-                                                                                'part_one' => '888',
-                                                                                'part_cents' => '77'
-                                                                              })
+                                                               'part_two' => '999',
+                                                               'part_one' => '888',
+                                                               'part_cents' => '77'
+                                                             })
       expect(helper.split_currency_amount(9_888_777.66)).to eq({
-                                                                                  'part_three' => '9',
-                                                                                  'part_two' => '888',
-                                                                                  'part_one' => '777',
-                                                                                  'part_cents' => '66'
-                                                                                })
+                                                                 'part_three' => '9',
+                                                                 'part_two' => '888',
+                                                                 'part_one' => '777',
+                                                                 'part_cents' => '66'
+                                                               })
     end
   end
 end
