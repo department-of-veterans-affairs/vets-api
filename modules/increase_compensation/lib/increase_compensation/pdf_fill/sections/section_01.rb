8c738800b6c47d4f5693a836ed0d2c6e26d2ab14
Tim <20232041+Tchase44@users.noreply.github.com>
2025-11-14 09:04:05 -0500

    safer address parse

--- modules/increase_compensation/lib/increase_compensation/pdf_fill/sections/section_01.rb
+++ modules/increase_compensation/lib/increase_compensation/pdf_fill/sections/section_01.rb
@@ -192,7 +192,9 @@ module IncreaseCompensation
         form_data['veteranSocialSecurityNumber2'] = form_data['veteranSocialSecurityNumber']
         form_data['veteranSocialSecurityNumber3'] = form_data['veteranSocialSecurityNumber']
         form_data['veteranDateOfBirth'] = split_date(form_data['dateOfBirth'])
-        form_data['veteranAddress']['postalCode'] = split_postal_code(form_data['veteranAddress'])
+        if form_data['veteranAddress'].present?
+          form_data['veteranAddress']['postalCode'] = split_postal_code(form_data['veteranAddress'])
+        end
         form_data['electronicCorrespondance'] = form_data['electronicCorrespondance'] ? 1 : 0
         form_data['emailAddresses'] = two_line_overflow(form_data['email'], 'email', 17)
       end
