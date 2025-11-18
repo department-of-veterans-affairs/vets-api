3aafe7be0e01891818e7d4fd7c4f854cb576a9c7
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2025-11-18 16:15:26 -0700

    Add implementation example

--- lib/benefits_claims/providers/benefits_claims/benefits_claims_provider.rb
+++ lib/benefits_claims/providers/benefits_claims/benefits_claims_provider.rb
@@ -3,9 +3,36 @@
 # Interface for a BenefitsClaimsProvider
 #
 # Include this module in a new ClaimsProvider class to define a standardized interface
-#   for fetching benefits claims data from various sources.
-
+# for fetching benefits claims data from various sources.
+#
+# @example Implementing a new claims provider
+#   class MyClaimsProvider
+#     include BenefitsClaimsProvider
+#
+#     def initialize(current_user)
+#       @current_user = current_user
+#     end
+#
+#     def get_claims
+#       raw_claims = fetch_from_my_api
+#       transform_to_dto(raw_claims)
+#     end
+#
+#     def get_claim(id)
+#       raw_claim = fetch_single_claim_from_my_api(id)
+#       transform_to_dto(raw_claim)
+#     end
+#
+#     private
+#
+#     def transform_to_dto(data)
+#       # Transform your data source format to ClaimResponse DTO format
+#     end
+#   end
+#
 # The new class MUST implement these methods to be a valid BenefitsClaimsProvider:
+# - get_claims: Returns an array of claims for the current user
+# - get_claim(id): Returns a single claim by its ID
 #
 # TODO:Create a standardized claim response structure that represents what VA.gov expects
 # (i.e. `lib/benefits_claims/responses/claim_response.rb`).
