6c4dbf92c7363df7e070038433caaec931e43fbe
Tim Chase <20232041+Tchase44@users.noreply.github.com>
2025-11-19 11:10:44 -0500

    [BIO] Form 21-8940 -- Part2 -- Logic and tests for writing to form (#24723)
    * Part 2, adds logic and tests to 21-8940 pdf filler
    
    update document type
    
    update test and section 1 limits
    
    add more tests
    
    rubocop warning fix
    
    * hanlde blank phone better
    
    * remove unsued helpers
    
    * move module 3 to own pr to lessen the size
    
    * rubocop
    
    * use a safe operator
    
    * feedback fixes
    
    * safer address parse
    
    * safer phone check
    
    * remove unused imports
    
    ---------
    
    Co-authored-by: Joshua K. Farrar <435746+joshuakfarrar@users.noreply.github.com>

---
+++ modules/increase_compensation/lib/increase_compensation/pdf_fill/sections/section_05.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'increase_compensation/pdf_fill/section'
+
+module IncreaseCompensation
+  module PdfFill
+    # Section V:  REMARKS
+    class Section5 < Section
+      # Section configuration hash
+      KEY = {
+        'remarks' => {
+          key: 'form1[0].#subform[4].Remarks_Ifany[0]'
+        }
+      }.freeze
+      def expand(form_data = {}); end
+    end
+  end
+end
