0bdcefb7e178276bdfeea277c6b1c19b06740063
rjohnson2011 <ryan.johnson@oddball.io>
2025-11-20 12:49:02 -0700

    Add compatibility patch for mail 2.9.0 upgrade
    The mail gem removed the deprecated CheckDeliveryParams module in version 2.9.0.
    The govdelivery-tms gem still requires this module, though it doesn't use any of its methods.
    
    This patch provides an empty Mail::CheckDeliveryParams module for compatibility
    until govdelivery-tms is updated to remove the dependency.
    
    Changes:
    - Created lib/mail/check_delivery_params.rb with empty module
    - Updated config/boot.rb to add lib to load path

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# Add lib to load path for mail compatibility patch
+$LOAD_PATH.unshift(File.expand_path('../lib', __dir__)) unless $LOAD_PATH.include?(File.expand_path('../lib', __dir__))
