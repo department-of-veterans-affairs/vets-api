49ed12c138bd967642e95a77972d1ecfa2d96a52
radavis <1299034+radavis@users.noreply.github.com>
2025-11-24 16:45:37 -0500

    Refactor index method for logging/metrics

--- app/controllers/v0/profile/contacts_controller.rb
+++ app/controllers/v0/profile/contacts_controller.rb
@@ -11,6 +11,27 @@ module V0
       # GET /v0/profile/contacts
       def index
         start_ms = Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond)
+        log_upstream_request_start
+
+        response = service.get_health_benefit_bio
+
+        contacts_count = response.contacts&.length || 0
+        response_ok = response.ok?
+        log_upstream_request_finish(start_ms:, contacts_count:, response_ok:)
+
+        render json: ContactSerializer.new(response.contacts), status: response.status
+      rescue => e
+        log_exception(start_ms:, e:)
+        raise
+      end
+
+      private
+
+      def service
+        VAProfile::Profile::V3::Service.new(current_user)
+      end
+
+      def log_upstream_request_start
         Rails.logger.info(
           event: 'profile.contacts.request.start',
           request_id: request.request_id,
@@ -19,53 +40,39 @@ module V0
           idme_uuid_present: current_user.idme_uuid.present?,
           logingov_uuid_present: current_user.logingov_uuid.present?
         )
+      end
 
-        response = service.get_health_benefit_bio
-
+      def log_upstream_request_finish(start_ms:, contacts_count:, response_ok:)
         elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond) - start_ms
-        contacts_count = response.contacts&.length || 0
         Rails.logger.info(
           event: 'profile.contacts.request.finish',
           request_id: request.request_id,
           upstream_status: response.status,
           contacts_count:,
-          ok: response.ok?,
+          ok: response_ok,
           empty: contacts_count.zero?,
           latency_ms: elapsed
         )
         StatsD.measure('profile.contacts.latency', elapsed)
         StatsD.increment('profile.contacts.empty') if contacts_count.zero?
         StatsD.increment('profile.contacts.success') if response.ok?
-
-        render json: ContactSerializer.new(response.contacts), status: response.status
-      rescue Common::Exceptions::BackendServiceException => e
-        elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond) - start_ms
-        Rails.logger.error(
-          event: 'profile.contacts.backend_error',
-          request_id: request.request_id,
-          error_class: e.class.name,
-          message: e.message,
-          latency_ms: elapsed
-        )
-        StatsD.increment('profile.contacts.error')
-        raise
-      rescue => e
-        elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond) - start_ms
-        Rails.logger.error(
-          event: 'profile.contacts.unhandled_error',
-          request_id: request.request_id,
-          error_class: e.class.name,
-          message: e.message,
-          latency_ms: elapsed
-        )
-        StatsD.increment('profile.contacts.error')
-        raise
       end
 
-      private
-
-      def service
-        VAProfile::Profile::V3::Service.new(current_user)
+      def log_exception(start_ms:, e:)
+        elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC, :millisecond) - start_ms
+        event = if e.is_a?(Common::Exceptions::BackendServiceException)
+                  'profile.contacts.backend_error'
+                else
+                  'profile.contacts.unhandled_error'
+                end
+        Rails.logger.error(
+          event:,
+          request_id: request.request_id,
+          error_class: e.class.name,
+          message: e.message,
+          latency_ms: elapsed
+        )
+        StatsD.increment('profile.contacts.error')
       end
     end
   end
