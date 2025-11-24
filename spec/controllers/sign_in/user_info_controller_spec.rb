20dcd9f7cf5a6f003aac7cc7988d2928e05c2c73
Emily Nguyen <emilyn0614@gmail.com>
2025-11-24 15:12:26 -0600

    update specs

--- spec/controllers/sign_in/user_info_controller_spec.rb
+++ spec/controllers/sign_in/user_info_controller_spec.rb
@@ -6,16 +6,25 @@ describe SignIn::UserInfoController do
   let(:user_account) { create(:user_account, icn:) }
   let(:user_verification) { create(:idme_user_verification, idme_uuid: credential_uuid, user_account:) }
   let!(:user) do
-    create(:user, user_account:, user_verification:, icn:, idme_uuid: credential_uuid, sec_id:, first_name:,
-                  last_name:, mpi_profile:)
+    create(
+      :user,
+      user_account:,
+      user_verification:,
+      icn:,
+      idme_uuid: credential_uuid,
+      sec_id:,
+      first_name:,
+      last_name:,
+      mpi_profile:
+    )
   end
   let!(:client_config) { create(:client_config, client_id:) }
   let!(:session) { create(:oauth_session, client_id:, user_account:, user_verification:) }
   let!(:user_credential_email) do
     create(:user_credential_email, user_verification:, credential_email: email)
   end
-  let(:mpi_profile) { build(:mpi_profile, icn:, sec_id:, given_names: [first_name], family_name: last_name) }
 
+  let(:mpi_profile) { build(:mpi_profile, icn:, sec_id:, given_names: [first_name], family_name: last_name) }
   let(:credential_uuid) { 'some-uuid' }
   let(:icn) { 'some-icn' }
   let(:sec_id) { 'some-sec-id' }
@@ -35,22 +44,23 @@ describe SignIn::UserInfoController do
 
   describe 'GET #show' do
     context 'when the client_id is in the list of valid clients' do
-      let(:expected_user_info_json) do
-        {
-          sub: user_account.id,
-          credential_uuid:,
-          icn:,
-          sec_id:,
-          first_name:,
-          last_name:,
-          email:
-        }
-      end
-
-      it 'returns the user info' do
+      it 'returns the key user info fields' do
         get :show
+
         expect(response).to have_http_status(:ok)
-        expect(response.body).to eq(expected_user_info_json.to_json)
+
+        body = JSON.parse(response.body)
+
+        expect(body['sub']).to eq(user.uuid)
+        expect(body['email']).to eq(email)
+        expect(body['first_name']).to eq(first_name)
+        expect(body['last_name']).to eq(last_name)
+        expect(body['full_name'].downcase).to include(first_name.downcase, last_name.downcase)
+        expect(body['icn']).to eq(icn)
+        expect(body['sec_id']).to eq(sec_id)
+        expect(body['csp_type']).to eq('200VIDM')
+        expect(body['csp_uuid']).to eq(credential_uuid)
+        expect(body['ial']).to eq('2')
       end
     end
 
