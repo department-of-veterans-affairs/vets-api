f2b6bb90451e93bc652a159daf249f3b0e62c4f9
Josh Mills <josh.troy.mills@gmail.com>
2025-11-25 13:03:36 -0500

    add disable_ddl to migration

--- db/migrate/20251125141602_validate_not_null_digital_dispute_submissions_guid.rb
+++ db/migrate/20251125141602_validate_not_null_digital_dispute_submissions_guid.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ValidateNotNullDigitalDisputeSubmissionsGuid < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+
   def up
     validate_check_constraint :digital_dispute_submissions, name: 'digital_dispute_submissions_guid_null'
     change_column_null :digital_dispute_submissions, :guid, false
@@ -8,7 +10,10 @@ class ValidateNotNullDigitalDisputeSubmissionsGuid < ActiveRecord::Migration[7.2
   end
 
   def down
-    add_check_constraint :digital_dispute_submissions, 'guid IS NOT NULL', name: 'digital_dispute_submissions_guid_null', validate: false
+    add_check_constraint :digital_dispute_submissions,
+                         'guid IS NOT NULL',
+                         name: 'digital_dispute_submissions_guid_null',
+                         validate: false
     change_column_null :digital_dispute_submissions, :guid, true
   end
 end
