d8f5ac9038e75a28152e32690b0be4d004253ff5
Jayson Perkins <jayson.perkins@adhocteam.us>
2025-11-25 09:23:28 -0500

    copilot comments

--- lib/mhv/oh_facilities_helper/service.rb
+++ lib/mhv/oh_facilities_helper/service.rb
@@ -2,24 +2,32 @@
 
 module MHV
   module OhFacilitiesHelper
-    class Service < Common::Client::Base
+    class Service
       def initialize(user)
         super()
         @current_user = user
       end
 
       def user_at_pretransitioned_oh_facility?
-        @current_user.va_treatment_facility_ids.each do |facility|
-          return true if Settings.mhv.oh_facility_checks.pretransitioned_oh_facilities.include?(facility.to_s)
+        @current_user.va_treatment_facility_ids.any? do |facility|
+          pretransitioned_oh_facilities.include?(facility.to_s)
         end
-        false
       end
 
       def user_facility_ready_for_info_alert?
-        @current_user.va_treatment_facility_ids.each do |facility|
-          return true if Settings.mhv.oh_facility_checks.facilities_ready_for_info_alert.include?(facility.to_s)
+        @current_user.va_treatment_facility_ids.any? do |facility|
+          facilities_ready_for_info_alert.include?(facility.to_s)
         end
-        false
+      end
+
+      private
+
+      def pretransitioned_oh_facilities
+        @pretransitioned_oh_facilities ||= Settings.mhv.oh_facility_checks.pretransitioned_oh_facilities.split(',').map(&:strip)
+      end
+
+      def facilities_ready_for_info_alert
+        @facilities_ready_for_info_alert ||= Settings.mhv.oh_facility_checks.facilities_ready_for_info_alert.split(',').map(&:strip)
       end
     end
   end
