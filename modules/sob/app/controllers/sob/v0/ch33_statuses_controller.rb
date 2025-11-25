6c47551f94b7fad81d3434d198a69f62b4477f95
Jeff Marks <jeff.t.marks@gmail.com>
2025-11-25 07:52:09 -0700

    simplify controller

--- modules/sob/app/controllers/sob/v0/ch33_statuses_controller.rb
+++ modules/sob/app/controllers/sob/v0/ch33_statuses_controller.rb
@@ -5,27 +5,14 @@ module SOB
     class Ch33StatusesController < ApplicationController
       service_tag 'statement-of-benefits'
 
-      STATSD_KEY_PREFIX = 'api.sob'
-
       skip_before_action :authenticate
 
       def show
         response = service.get_ch33_status
         render json: SOB::Ch33StatusSerializer.new(response)
-      rescue => e
-        byebug
-        handle_error(e)
-      ensure
-        StatsD.increment("#{STATSD_KEY_PREFIX}.total")
       end
 
       private
-
-      def handle_error(e)
-        status = e.errors.first[:status].to_i
-        StatsD.increment("#{STATSD_KEY_PREFIX}.fail", tags: ["error:#{status}"])
-        render json: { errors: e.errors }, status: status || :internal_server_error
-      end
 
       def service
         SOB::DGIB::Service.new('796126859')
