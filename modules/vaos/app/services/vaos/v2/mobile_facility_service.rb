a3717d7dc6d684cb82e2b2e5cd894b228871693c
Corey Ferris <cferris320@gmail.com>
2025-11-28 13:36:01 -0500

    vaos specs

--- modules/vaos/app/services/vaos/v2/mobile_facility_service.rb
+++ modules/vaos/app/services/vaos/v2/mobile_facility_service.rb
@@ -289,6 +289,7 @@ module VAOS
       end
 
       def scheduling_url
+        # binding.pry
         if Flipper.enabled?(:va_online_scheduling_cscs_migration, user)
           '/cscs/v1/configurations'
         else
