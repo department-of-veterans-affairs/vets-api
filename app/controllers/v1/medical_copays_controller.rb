8609172a2f2c71f419a401b29eacf30e9693445a
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-11-28 12:40:50 -0500

    Updates

--- app/controllers/v1/medical_copays_controller.rb
+++ app/controllers/v1/medical_copays_controller.rb
@@ -6,7 +6,6 @@ module V1
 
     def index
       invoice_bundle = medical_copay_service.list(count: params[:count] || 10, page: params[:page] || 1)
-      # we need to loop for the last 6 months
       render json: Lighthouse::HCC::InvoiceSerializer.new(
         invoice_bundle.entries, links: invoice_bundle.links, meta: invoice_bundle.meta
       )
