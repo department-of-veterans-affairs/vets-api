31d48584c63198f709fd4965fa4aeb9a5d97593e
Liana Fleming <8458903+lianafleming@users.noreply.github.com>
2025-11-28 08:27:50 -0800

    BMT3/121452 - fix bad merge with sidekiq constants (#25337)
    * BMT3/121452 - fix bad merge with sidekiq constants
    
    * BMT3/121452 - add specs
    
    * BMT3/121452 - rubocop fixes

--- app/sidekiq/event_bus_gateway/letter_ready_push_job.rb
+++ app/sidekiq/event_bus_gateway/letter_ready_push_job.rb
@@ -13,7 +13,7 @@ module EventBusGateway
 
     STATSD_METRIC_PREFIX = 'event_bus_gateway.letter_ready_push'
 
-    sidekiq_options Constants::SIDEKIQ_RETRY_OPTIONS
+    sidekiq_options retry: Constants::SIDEKIQ_RETRY_COUNT_FIRST_PUSH
 
     sidekiq_retries_exhausted do |msg, _ex|
       job_id = msg['jid']
