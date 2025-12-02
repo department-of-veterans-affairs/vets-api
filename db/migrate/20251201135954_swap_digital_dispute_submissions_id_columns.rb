f46ab3ac37d5bb1d1dbb443b2070921b5fed3c97
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-12-02 12:58:57 -0500

    revelations

--- db/migrate/20251201135954_swap_digital_dispute_submissions_id_columns.rb
+++ db/migrate/20251201135954_swap_digital_dispute_submissions_id_columns.rb
@@ -11,28 +11,22 @@ class SwapDigitalDisputeSubmissionsIdColumns < ActiveRecord::Migration[7.2]
 
   def down
     safety_assured do
-      # Drop current PK
       execute 'ALTER TABLE digital_dispute_submissions DROP CONSTRAINT IF EXISTS digital_dispute_submissions_pkey;'
 
-      # Swap back
       rename_column :digital_dispute_submissions, :id, :new_id
       rename_column :digital_dispute_submissions, :old_uuid_id, :id
 
-      # Restore PK on UUID `id`
       execute 'ALTER TABLE digital_dispute_submissions ADD PRIMARY KEY (id);'
 
-      # Restore UUID default
-      execute 'ALTER TABLE digital_dispute_submissions ALTER COLUMN id SET DEFAULT gen_random_uuid();'
+      # restore uuid default
+      execute "ALTER TABLE digital_dispute_submissions ALTER COLUMN id SET DEFAULT gen_random_uuid();"
     end
   end
 
   private
 
   def drop_uuid_primary_key
-    execute <<~SQL
-      ALTER TABLE digital_dispute_submissions
-      DROP CONSTRAINT IF EXISTS digital_dispute_submissions_pkey;
-    SQL
+    execute 'ALTER TABLE digital_dispute_submissions DROP CONSTRAINT IF EXISTS digital_dispute_submissions_pkey;'
   end
 
   def swap_columns
@@ -41,15 +35,12 @@ class SwapDigitalDisputeSubmissionsIdColumns < ActiveRecord::Migration[7.2]
   end
 
   def setup_bigint_primary_key
-    # Add new PK
     execute 'ALTER TABLE digital_dispute_submissions ADD PRIMARY KEY (id);'
 
-    # Dynamically fetch the sequence name for "id"
     seq = select_value <<~SQL
       SELECT pg_get_serial_sequence('digital_dispute_submissions', 'id');
     SQL
 
-    # Apply default + ownership
     execute "ALTER TABLE digital_dispute_submissions ALTER COLUMN id SET DEFAULT nextval('#{seq}');"
     execute "ALTER SEQUENCE #{seq} OWNED BY digital_dispute_submissions.id;"
   end
