2dd2f0e196de452ad649537110177618b3ad4ed8
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-12-02 12:50:19 -0500

    Updates

--- db/migrate/20251201135954_swap_digital_dispute_submissions_id_columns.rb
+++ db/migrate/20251201135954_swap_digital_dispute_submissions_id_columns.rb
@@ -3,45 +3,54 @@
 class SwapDigitalDisputeSubmissionsIdColumns < ActiveRecord::Migration[7.2]
   def up
     safety_assured do
-      # Drop PK
-      execute <<~SQL
-        ALTER TABLE digital_dispute_submissions
-        DROP CONSTRAINT IF EXISTS digital_dispute_submissions_pkey;
-      SQL
-
-      # Swap columns
-      rename_column :digital_dispute_submissions, :id, :old_uuid_id
-      rename_column :digital_dispute_submissions, :new_id, :id
-
-      # Add PK on bigint id
-      execute "ALTER TABLE digital_dispute_submissions ADD PRIMARY KEY (id);"
-
-      # Fetch correct sequence
-      seq = select_value(<<~SQL)
-        SELECT pg_get_serial_sequence('digital_dispute_submissions', 'id');
-      SQL
-
-      # Attach sequence + default
-      execute "ALTER TABLE digital_dispute_submissions ALTER COLUMN id SET DEFAULT nextval('#{seq}');"
-      execute "ALTER SEQUENCE #{seq} OWNED BY digital_dispute_submissions.id;"
+      drop_uuid_primary_key
+      swap_columns
+      setup_bigint_primary_key
     end
   end
 
   def down
     safety_assured do
-      execute <<~SQL
-        ALTER TABLE digital_dispute_submissions
-        DROP CONSTRAINT IF EXISTS digital_dispute_submissions_pkey;
-      SQL
+      # Drop current PK
+      execute 'ALTER TABLE digital_dispute_submissions DROP CONSTRAINT IF EXISTS digital_dispute_submissions_pkey;'
 
+      # Swap back
       rename_column :digital_dispute_submissions, :id, :new_id
       rename_column :digital_dispute_submissions, :old_uuid_id, :id
 
-      # Restore PK on uuid
-      execute "ALTER TABLE digital_dispute_submissions ADD PRIMARY KEY (id);"
+      # Restore PK on UUID `id`
+      execute 'ALTER TABLE digital_dispute_submissions ADD PRIMARY KEY (id);'
 
-      # Restore UUID generation default
-      execute "ALTER TABLE digital_dispute_submissions ALTER COLUMN id SET DEFAULT gen_random_uuid();"
+      # Restore UUID default
+      execute 'ALTER TABLE digital_dispute_submissions ALTER COLUMN id SET DEFAULT gen_random_uuid();'
     end
+  end
+
+  private
+
+  def drop_uuid_primary_key
+    execute <<~SQL
+      ALTER TABLE digital_dispute_submissions
+      DROP CONSTRAINT IF EXISTS digital_dispute_submissions_pkey;
+    SQL
+  end
+
+  def swap_columns
+    rename_column :digital_dispute_submissions, :id, :old_uuid_id
+    rename_column :digital_dispute_submissions, :new_id, :id
+  end
+
+  def setup_bigint_primary_key
+    # Add new PK
+    execute 'ALTER TABLE digital_dispute_submissions ADD PRIMARY KEY (id);'
+
+    # Dynamically fetch the sequence name for "id"
+    seq = select_value <<~SQL
+      SELECT pg_get_serial_sequence('digital_dispute_submissions', 'id');
+    SQL
+
+    # Apply default + ownership
+    execute "ALTER TABLE digital_dispute_submissions ALTER COLUMN id SET DEFAULT nextval('#{seq}');"
+    execute "ALTER SEQUENCE #{seq} OWNED BY digital_dispute_submissions.id;"
   end
 end
