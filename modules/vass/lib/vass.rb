f2da230d5afc48fe1bf32663347f428a921759a3
Lee Delarm <lee.delarm@agile6.com>
2025-12-02 18:34:18 -0500

    Trying to load middleware a different way
    Trying to load middleware a different way

--- modules/vass/lib/vass.rb
+++ modules/vass/lib/vass.rb
@@ -2,9 +2,6 @@
 
 require 'vass/engine'
 require 'vass/errors'
-require 'vass/response_middleware'
 
 module Vass
-  # Register custom Faraday middleware for VASS error handling
-  Faraday::Response.register_middleware(vass_errors: Vass::ResponseMiddleware)
 end
