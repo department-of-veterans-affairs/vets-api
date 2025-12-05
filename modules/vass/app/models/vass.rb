bae07b6c0b8de1d31d43e55c515456465dff69a3
PhilipDeFraties <philip.defraties@gmail.com>
2025-12-05 15:30:37 -0700

    remove unnecessary directory / model

--- modules/vass/app/models/vass.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Vass
-  # Base module for VASS models
-end
-
--- modules/vass/app/models/vass/v0.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Vass
-  module V0
-    # Base module for VASS V0 models
-  end
-end
-
