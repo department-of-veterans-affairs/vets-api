c0ce321169809452b2e14f60505a9056a4e2e36f
Tim <20232041+Tchase44@users.noreply.github.com>
2025-12-05 11:44:14 -0500

    [BIO] Adds PDF url to 21-8940 claim response > > * Dependant on this [PR](https://github.com/department-of-veterans-affairs/vets-api/pull/25236) specifically this [File](https://github.com/department-of-veterans-affairs/vets-api/blob/dbfe38245ca40b0d226607d31ff76aa867edaa96/app/serialize) to be reused
    * Also dependant on email templates for the 21-8940

--- modules/increase_compensation/app/controllers/increase_compensation/v0/claims_controller.rb
+++ modules/increase_compensation/app/controllers/increase_compensation/v0/claims_controller.rb
@@ -24,10 +24,20 @@ module IncreaseCompensation
         IncreaseCompensation::SavedClaim
       end
 
+      # points to S3 settings, where PDFs are archived
+      def config
+        Settings.bio.increase_compensation
+      end
+
+      # returns the URL to the PDF in S3 so the person completing the forms can download them
+      def pdf_url(guid)
+        SimpleFormsApi::FormRemediation::S3Client.fetch_presigned_url(guid, config:)
+      end
+
       # GET serialized Increase Compensation form data
       def show
         claim = claim_class.find_by!(guid: params[:id]) # raises ActiveRecord::RecordNotFound
-        render json: IncreaseCompensation::SavedClaimSerializer.new(claim)
+        render json: ArchivedClaimSerializer.new(claim, params: { pdf_url: pdf_url(claim.guid)})
       rescue ActiveRecord::RecordNotFound => e
         monitor.track_show404(params[:id], current_user, e)
         render(json: { error: e.to_s }, status: :not_found)
@@ -57,12 +67,9 @@ module IncreaseCompensation
         IncreaseCompensation::BenefitsIntake::SubmitClaimJob.perform_async(claim.id, current_user&.user_account_uuid)
 
         monitor.track_create_success(in_progress_form, claim, current_user)
-        # TODO: pdf url needed in response, s3 settings? settings.yml?
-        # config = SimpleFormsApi::FormRemediation::Configuration::VffConfig.new || nil
-        # pdf_url = SimpleFormsApi::FormRemediation::S3Client.fetch_presigned_url(claim.guid, config:)
-        pdf_url = nil
+
         clear_saved_form(claim.form_id)
-        render json: IncreaseCompensation::SavedClaimSerializer.new(claim, params: { pdf_url: })
+        render json: ArchivedClaimSerializer.new(claim, params: { pdf_url: pdf_url(claim.guid)})
       rescue => e
         monitor.track_create_error(in_progress_form, claim, current_user, e)
         raise e
