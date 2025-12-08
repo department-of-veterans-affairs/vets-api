34a87921785f7d7104f2c73888a182f8943c60fd
Amy Cashbaugh <77475284+amycashbaugh@users.noreply.github.com>
2025-12-08 16:28:54 -0600

    added V2 status updates to filter count

--- modules/my_health/app/controllers/my_health/v2/prescriptions_controller.rb
+++ modules/my_health/app/controllers/my_health/v2/prescriptions_controller.rb
@@ -15,18 +15,7 @@ module MyHealth
       include MyHealth::RxGroupingHelperV2
       include JsonApiPaginationLinks
 
-      service_tag 'mhv-medications'
-
-      # Include V2StatusMapper to get access to V2_STATUS_GROUPS and ORIGINAL_TO_V2_STATUS_MAPPING constants
-      include UnifiedHealthData::Adapters::V2StatusMapper
-
-      before_action :validate_feature_flag
-
-      # Add V2_STATUS_MAPPING for backward compatibility with tests
-      # This provides a flat mapping from original status to V2 status
-      V2_STATUS_MAPPING = V2_STATUS_GROUPS.each_with_object({}) do |(v2_status, originals), hash|
-        originals.each { |original| hash[original] = v2_status }
-      end.freeze
+      service_tag 'mhv-prescriptions'
 
       def refill
         return unless validate_feature_flag
@@ -52,181 +41,71 @@ module MyHealth
       def index
         return unless validate_feature_flag
 
-        # Service returns prescriptions with V2 status mapping already applied
         prescriptions = service.get_prescriptions(current_only: false).compact
-
-        # Grouping and other modifications (status mapping already done)
+        recently_requested = get_recently_requested_prescriptions(prescriptions)
+        raw_data = prescriptions.dup
         prescriptions = resource_data_modifications(prescriptions).compact
 
-        # No need to call apply_v2_status_mapping here - already done in adapter
-
-        recently_requested = get_recently_requested_prescriptions(prescriptions)
-        filter_count = set_filter_metadata(prescriptions, prescriptions)
-
+        filter_count = set_filter_metadata(prescriptions, raw_data)
         prescriptions, sort_metadata = apply_filters_and_sorting(prescriptions)
+
         records, options = build_response_data(prescriptions, filter_count, recently_requested, sort_metadata)
 
-        render json: serializer_class.new(records, options)
-      end
-
-      def show
-        return unless validate_feature_flag
-
-        prescriptions = service.get_prescriptions(current_only: false, station_number: params[:station_number]).compact
-        resource = find_prescription_by_id(prescriptions, params[:id])
-
-        raise Common::Exceptions::RecordNotFound, params[:id] if resource.blank?
-
-        options = { meta: {} }
-        render json: MyHealth::V2::PrescriptionDetailsSerializer.new(resource, options)
+        log_prescriptions_access
+        render json: MyHealth::V2::PrescriptionDetailsSerializer.new(records, options)
       end
 
       def list_refillable_prescriptions
         return unless validate_feature_flag
 
-        # Service returns prescriptions with V2 status mapping already applied when cerner_pilot is enabled
         prescriptions = service.get_prescriptions(current_only: false).compact
         recently_requested = get_recently_requested_prescriptions(prescriptions)
         refillable_prescriptions = filter_data_by_refill_and_renew(prescriptions)
 
-        # V2 status mapping is handled by the service when cerner_pilot feature flag is enabled
-
-        records, options = build_paginated_response(refillable_prescriptions,
-                                                    { recently_requested: })
-        render json: MyHealth::V2::PrescriptionDetailsSerializer.new(records, options)
+        options = { meta: { recently_requested: } }
+        render json: MyHealth::V2::PrescriptionDetailsSerializer.new(refillable_prescriptions, options)
       end
 
       private
 
-      # V2 status mapping is controlled by the cerner_pilot feature flag.
-      # When enabled, prescriptions display simplified V2 statuses (Active, In progress, Inactive, etc.)
-      # instead of the original granular statuses (Active: Refill in Process, Expired, etc.)
-      def cerner_pilot_enabled?
-        Flipper.enabled?(:mhv_medications_display_cerner_pilot, @current_user)
-      end
-
       def service
-        # V2 controller uses V2 statuses when cerner_pilot feature flag is enabled
-        @service ||= UnifiedHealthData::Service.new(@current_user, use_v2_statuses: cerner_pilot_enabled?)
+        @service ||= UnifiedHealthData::Service.new(@current_user)
       end
 
-      def orders
-        @orders ||= begin
-          raw_orders = params.require(:orders)
-          unless raw_orders.is_a?(Array)
-            raise Common::Exceptions::InvalidFieldValue.new('orders', 'Expected an array of order objects')
-          end
+      def validate_feature_flag
+        return true if Flipper.enabled?(:mhv_medications_cerner_pilot, @current_user)
 
-          parsed_orders = raw_orders.map do |order|
-            order = order.permit(:stationNumber, :id) if order.respond_to?(:permit)
-            { station_number: order[:stationNumber] || order['stationNumber'], id: order[:id] || order['id'] }
-          end
-
-          parsed_orders
-        rescue JSON::ParserError
-          raise Common::Exceptions::InvalidFieldValue.new('orders', 'Invalid JSON format')
-        end
-      end
-
-      def pagination_params
-        {
-          page: params[:page]&.to_i || 1,
-          per_page: params[:per_page]&.to_i || 10
-        }
+        render json: {
+          error: {
+            code: 'FEATURE_NOT_AVAILABLE',
+            message: 'This feature is not currently available'
+          }
+        }, status: :forbidden
+        false
       end
 
       def apply_filters_and_sorting(prescriptions)
         prescriptions = apply_filters_to_list(prescriptions) if params[:filter].present?
-        sorted = apply_sorting_to_list(prescriptions)
-        sort_metadata = build_sort_metadata(params[:sort])
-        [sorted, sort_metadata]
+        prescriptions, sort_metadata = apply_sorting_to_list(prescriptions, params[:sort])
+        [sort_prescriptions_with_pd_at_top(prescriptions), sort_metadata]
       end
 
-      def apply_filters_to_list(prescriptions)
-        filter_params = params.require(:filter).permit(disp_status: [:eq])
-        disp_status = filter_params[:disp_status]
+      def build_response_data(prescriptions, filter_count, recently_requested, sort_metadata = {})
+        is_using_pagination = params[:page].present? || params[:per_page].present?
 
-        return prescriptions if disp_status.blank?
+        base_meta = filter_count.merge(recently_requested:)
+        # sort_metadata is the entire metadata hash from the resource, access the :sort key
+        base_meta[:sort] = sort_metadata[:sort] if sort_metadata.is_a?(Hash) && sort_metadata[:sort].present?
 
-        if renewal_filter?(disp_status[:eq])
-          prescriptions.select(&method(:renewable))
+        if is_using_pagination
+          build_paginated_response(prescriptions, base_meta)
         else
-          filter_by_disp_status(prescriptions, disp_status[:eq])
+          [Array(prescriptions), { meta: base_meta }]
         end
-      end
-
-      def renewal_filter?(filter_value)
-        normalized = filter_value&.downcase
-        ['active,expired', 'active,inactive'].include?(normalized)
-      end
-
-      def filter_by_disp_status(prescriptions, filter_value)
-        expanded_filters = expand_status_filters(filter_value)
-
-        prescriptions.select do |item|
-          item.respond_to?(:disp_status) && item.disp_status &&
-            expanded_filters.any? { |f| item.disp_status.casecmp(f).zero? }
-        end
-      end
-
-      def apply_sorting_to_list(prescriptions)
-        sort_param = params[:sort]
-        return prescriptions if sort_param.blank?
-
-        sort_fields = sort_param.is_a?(Array) ? sort_param : [sort_param]
-        prescriptions.sort do |a, b|
-          compare_by_fields(a, b, sort_fields)
-        end
-      end
-
-      def compare_by_fields(item_a, item_b, sort_fields)
-        sort_fields.each do |field|
-          descending = field.start_with?('-')
-          field_name = descending ? field[1..] : field
-          comparison = compare_field_values(item_a, item_b, field_name)
-          comparison = -comparison if descending
-          return comparison unless comparison.zero?
-        end
-        0
-      end
-
-      def compare_field_values(item_a, item_b, field_name)
-        value_a = item_a.respond_to?(field_name) ? item_a.send(field_name) : nil
-        value_b = item_b.respond_to?(field_name) ? item_b.send(field_name) : nil
-        (value_a || '') <=> (value_b || '')
-      end
-
-      def build_sort_metadata(sort_param)
-        return {} if sort_param.blank?
-
-        { sort: sort_param }
-      end
-
-      def find_prescription_by_id(prescriptions, id)
-        prescriptions.find { |p| p.prescription_id.to_s == id.to_s }
-      end
-
-      def resource_data_modifications(prescriptions)
-        remove_pf_pd(prescriptions)
-      end
-
-      def build_response_data(prescriptions, filter_count, recently_requested, sort_metadata)
-        paginated = collection_resource(prescriptions).paginate(
-          page: pagination_params[:page],
-          per_page: pagination_params[:per_page]
-        )
-
-        options = {
-          meta: filter_count.merge(pagination: paginated.metadata[:pagination])
-                            .merge(sort_metadata)
-                            .merge(recently_requested:),
-          links: pagination_links(paginated)
-        }
-        [paginated.data, options]
       end
 
       def build_paginated_response(prescriptions, base_meta)
-        collection = collection_resource(prescriptions)
+        collection = Vets::Collection.new(prescriptions)
         paginated = collection.paginate(
           page: pagination_params[:page],
           per_page: pagination_params[:per_page]
@@ -246,6 +125,89 @@ module MyHealth
         )
       end
 
+      def get_recently_requested_prescriptions(prescriptions)
+        # V2 uses "In progress" status instead of granular statuses
+        prescriptions.select do |item|
+          item.respond_to?(:disp_status) && item.disp_status == 'In progress'
+        end
+      end
+
+      def apply_filters_to_list(prescriptions)
+        filter_params = params.require(:filter).permit(disp_status: [:eq], shipped: [:eq])
+        disp_status = filter_params[:disp_status]
+        shipped_param = filter_params[:shipped]
+
+        return prescriptions if disp_status.blank? && shipped_param.blank?
+
+        # Handle shipped filter explicitly via shipped param
+        if shipped_param.present? && shipped_param[:eq]&.downcase == 'true'
+          return filter_shipped_prescriptions(prescriptions)
+        end
+
+        filter_value = disp_status&.dig(:eq)
+        return prescriptions if filter_value.blank?
+
+        # Handle renewal filter (Active,Inactive combination)
+        return filter_renewable_prescriptions(prescriptions) if renewal_filter?(filter_value)
+
+        # Standard status filter
+        filter_by_disp_status(prescriptions, filter_value)
+      end
+
+      # V2 filter values that should match the renewal criteria
+      def renewal_filter?(filter_value)
+        normalized = filter_value&.downcase
+        ['active,expired', 'active,inactive'].include?(normalized)
+      end
+
+      def filter_renewable_prescriptions(prescriptions)
+        prescriptions.select do |item|
+          item.respond_to?(:is_renewable) && item.is_renewable == true
+        end
+      end
+
+      def filter_shipped_prescriptions(prescriptions)
+        prescriptions.select do |item|
+          item.respond_to?(:disp_status) &&
+            item.disp_status&.casecmp('Active')&.zero? &&
+            item.respond_to?(:is_trackable) &&
+            item.is_trackable == true
+        end
+      end
+
+      def filter_by_disp_status(prescriptions, filter_value)
+        # V2 status values: Active, In progress, Inactive, Transferred, Status not available
+        filters = filter_value.split(',').map(&:strip).map(&:downcase)
+        prescriptions.select do |item|
+          item.respond_to?(:disp_status) && item.disp_status &&
+            filters.include?(item.disp_status.downcase)
+        end
+      end
+
+      def apply_sorting_to_list(prescriptions, sort_param)
+        # Create a mock resource object for the helper methods
+        resource = Struct.new(:records, :metadata).new(prescriptions, {})
+
+        # Use the helper's apply_sorting method which sets the metadata
+        sorted_resource = apply_sorting(resource, sort_param)
+
+        [sorted_resource.records, sorted_resource.metadata]
+      end
+
+      def resource_data_modifications(prescriptions)
+        display_pending_meds = Flipper.enabled?(:mhv_medications_display_pending_meds, @current_user)
+
+        prescriptions = if params[:filter].blank? && display_pending_meds
+                          prescriptions.reject do |item|
+                            item.respond_to?(:prescription_source) && item.prescription_source == 'PF'
+                          end
+                        else
+                          remove_pf_pd(prescriptions)
+                        end
+
+        group_prescriptions(prescriptions)
+      end
+
       def set_filter_metadata(list, non_modified_collection)
         {
           filter_count: {
@@ -253,39 +215,33 @@ module MyHealth
             active: count_by_status(list, 'Active'),
             in_progress: count_by_status(list, 'In progress'),
             shipped: count_shipped_prescriptions(list),
-            renewal: list.count { |item| renewable(item) },
+            renewal: count_renewable_prescriptions(list),
             inactive: count_by_status(list, 'Inactive'),
             transferred: count_by_status(list, 'Transferred'),
-            unknown: count_by_status(list, ['Unknown', 'Status not available'])
+            status_not_available: count_by_status(list, 'Status not available')
           }
         }
       end
 
-      def count_by_status(prescriptions, statuses)
-        statuses = Array(statuses)
+      def count_by_status(prescriptions, status)
+        prescriptions.count do |rx|
+          rx.respond_to?(:disp_status) && rx.disp_status&.casecmp(status)&.zero?
+        end
+      end
+
+      def count_renewable_prescriptions(prescriptions)
         prescriptions.count do |item|
-          item.respond_to?(:disp_status) && item.disp_status &&
-            statuses.any? { |s| item.disp_status.casecmp(s).zero? }
+          item.respond_to?(:is_renewable) && item.is_renewable == true
         end
       end
 
       def count_shipped_prescriptions(prescriptions)
         prescriptions.count do |item|
-          active_status?(item) && trackable?(item)
+          item.respond_to?(:disp_status) &&
+            item.disp_status&.casecmp('Active')&.zero? &&
+            item.respond_to?(:is_trackable) &&
+            item.is_trackable == true
         end
-      end
-
-      def active_status?(item)
-        item.respond_to?(:disp_status) && item.disp_status&.casecmp('Active')&.zero?
-      end
-
-      def trackable?(item)
-        item.respond_to?(:is_trackable) && item.is_trackable == true
-      end
-
-      def count_grouped_prescriptions(prescriptions)
-        # Group by station number and prescription ID, count unique combinations
-        prescriptions.group_by { |rx| [rx.station_number, rx.prescription_id] }.count
       end
 
       def remove_pf_pd(data)
@@ -296,14 +252,43 @@ module MyHealth
       end
 
       def sort_prescriptions_with_pd_at_top(prescriptions)
-        pd_prescriptions = prescriptions.select { |rx| rx.prescription_source == 'PD' }
-        other_prescriptions = prescriptions.reject { |rx| rx.prescription_source == 'PD' }
+        pd_prescriptions = prescriptions.select do |med|
+          med.respond_to?(:prescription_source) && med.prescription_source == 'PD'
+        end
+        other_prescriptions = prescriptions.reject do |med|
+          med.respond_to?(:prescription_source) && med.prescription_source == 'PD'
+        end
 
         pd_prescriptions + other_prescriptions
       end
 
-      def collection_resource(prescriptions = nil)
-        Vets::Collection.new(prescriptions || [])
+      def orders
+        @orders ||= begin
+          parsed_orders = JSON.parse(request.body.read)
+
+          # Validate that orders is an array
+          unless parsed_orders.is_a?(Array)
+            raise Common::Exceptions::InvalidFieldValue.new('orders',
+                                                            'Must be an array')
+          end
+
+          # Validate that orders array is not empty (treat empty array same as missing required parameter)
+          raise Common::Exceptions::ParameterMissing, 'orders' if parsed_orders.empty?
+
+          # Validate that each order has required fields
+          parsed_orders.each_with_index do |order, index|
+            unless order.is_a?(Hash) && order['stationNumber'] && order['id']
+              raise Common::Exceptions::InvalidFieldValue.new(
+                "orders[#{index}]",
+                'Each order must contain stationNumber and id fields'
+              )
+            end
+          end
+
+          parsed_orders
+        rescue JSON::ParserError
+          raise Common::Exceptions::InvalidFieldValue.new('orders', 'Invalid JSON format')
+        end
       end
     end
   end
