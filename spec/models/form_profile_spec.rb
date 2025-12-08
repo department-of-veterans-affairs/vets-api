eac32edb9f78808f8e3101918f3611c7acf89f32
Rachal.Cassity <rachal.cassity@oddball.io>
2025-12-08 10:03:15 -0600

    removed ppiu from form profile spec

--- spec/models/form_profile_spec.rb
+++ spec/models/form_profile_spec.rb
@@ -1483,27 +1483,19 @@ RSpec.describe FormProfile, type: :model do
         end
       end
 
-      context 'with VA Profile and ppiu prefill for 0994', skip: "REMOVE TESTING FOR 0994" do
+      context 'with VA Profile and ppiu prefill for 0994' do
         before do
           can_prefill_vaprofile(true)
           expect(user).to receive(:authorize).with(:ppiu, :access?).and_return(true).at_least(:once)
           expect(user).to receive(:authorize).with(:evss, :access?).and_return(true).at_least(:once)
-          v22_0994_expected['bankAccount'] = {
-            'bankAccountNumber' => '*********1234',
-            'bankAccountType' => 'Checking',
-            'bankName' => 'Comerica',
-            'bankRoutingNumber' => '*****2115'
-          }
         end
 
         it 'prefills 0994 with VA Profile and payment information' do
           VCR.use_cassette('va_profile/v2/contact_information/get_address') do
             VCR.use_cassette('evss/disability_compensation_form/rated_disabilities') do
-              VCR.use_cassette('evss/ppiu/payment_information') do
-                VCR.use_cassette('va_profile/military_personnel/post_read_service_histories_200',
-                                 allow_playback_repeats: true) do
-                  expect_prefilled('22-0994')
-                end
+              VCR.use_cassette('va_profile/military_personnel/post_read_service_histories_200',
+                                allow_playback_repeats: true) do
+                expect_prefilled('22-0994')
               end
             end
           end
