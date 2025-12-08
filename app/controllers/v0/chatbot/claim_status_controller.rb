1415b52aead8ec283753e3f219e206fb0a4593de
Elliot Mackinnon <emackinnon100390@gmail.com>
2025-12-08 16:17:38 -0700

    add pundit_user and test

--- app/controllers/v0/chatbot/claim_status_controller.rb
+++ app/controllers/v0/chatbot/claim_status_controller.rb
@@ -11,10 +11,12 @@ module V0
     class ClaimStatusController < SignIn::ServiceAccountApplicationController
       include IgnoreNotFound
       include Vets::SharedLogging
+      # before_action { authorize :lighthouse, :access? }
       service_tag 'chatbot'
       rescue_from 'EVSS::ErrorMiddleware::EVSSError', with: :service_exception_handler
 
       def index
+        Rails.logger.error('SHUT YOUR FACE')
         render json: {
           data: poll_claims_from_lighthouse,
           meta: { sync_status: 'SUCCESS' }
@@ -22,6 +24,7 @@ module V0
       end
 
       def show
+        Rails.logger.error('SHUT YOUR FACE')
         render json: {
           data: get_claim_from_lighthouse(params[:id]),
           meta: { sync_status: 'SUCCESS' }
@@ -138,6 +141,25 @@ module V0
       end
 
       class ServiceException < RuntimeError; end
+
+      # # Pundit uses `pundit_user` (falls back to `current_user`) to build the policy.
+      # # Service account authentication does not set `current_user`, it only sets
+      # # `@service_account_access_token` with `user_attributes`. That made `authorize`
+      # # pass a nil user into `LighthousePolicy`, triggering `undefined method `icn' for nil`.
+      # # We expose a lightweight user object built from the token so LighthousePolicy
+      # # can evaluate `icn.present? && participant_id.present?`.
+      # def pundit_user
+      #   return @current_user if defined?(@current_user) && @current_user.present?
+
+      #   attrs = @service_account_access_token&.user_attributes || {}
+      #   return nil if attrs.blank?
+
+      #   # Minimal struct responding to the methods LighthousePolicy calls.
+      #   OpenStruct.new(
+      #     icn: attrs['icn'],
+      #     participant_id: attrs['participant_id']
+      #   )
+      # end
     end
   end
 end
