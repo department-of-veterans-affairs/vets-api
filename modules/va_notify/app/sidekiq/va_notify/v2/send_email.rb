9946faecff1b73cf3a01748b3e89820805f81dda
Sarah Wheeler <sarah.wheeler@wilcore.io>
2025-12-08 16:13:52 -0800

    Add Statsd and error handling

--- modules/va_notify/app/sidekiq/va_notify/v2/send_email.rb
+++ modules/va_notify/app/sidekiq/va_notify/v2/send_email.rb
@@ -4,6 +4,19 @@ module VANotify
   module V2
     class SendEmail
       include Sidekiq::Job
+      include Vets::SharedLogging
+      sidekiq_options retry: 14
+
+      sidekiq_retries_exhausted do |msg, _ex|
+        job_id = msg['jid']
+        job_class = msg['class']
+        error_class = msg['error_class']
+        error_message = msg['error_message']
+
+        message = "#{job_class} retries exhausted"
+        Rails.logger.error(message, { job_id:, error_class:, error_message: })
+        StatsD.increment("sidekiq.jobs.#{job_class.underscore}.retries_exhausted")
+      end
 
       def perform(email, template_id, attr_package_key, api_key, callback_options = {})
         personalisation = fetch_and_cleanup_personalisation(attr_package_key)
@@ -14,6 +27,11 @@ module VANotify
           template_id:,
           personalisation:
         )
+
+        StatsD.increment('api.vanotify.email_job.success')
+        response
+        rescue VANotify::Error => e
+          handle_backend_exception(e)
       end
 
       def self.enqueue(email, template_id, personalisation, api_key, callback_options = {})
@@ -27,6 +45,14 @@ module VANotify
         attrs = Sidekiq::AttrPackage.find(attr_package_key)
         Sidekiq::AttrPackage.delete(attr_package_key) if attrs
         attrs&.dig(:personalisation)
+      end
+
+      def handle_backend_exception(e)
+        if e.status_code == 400
+          log_exception_to_rails(e)
+        else
+          raise e
+        end
       end
     end
   end
