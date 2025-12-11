4257438fa615299ca2cb836aafeeac4e2bc044c4
Jennica Stiehl <jennica.stiehl@oddball.io>
2025-12-11 16:07:46 -0700

    Change 'Consider using' to 'has been considered'
    Makes it a clear verification item rather than a suggestion.
    
    Addresses review comment: https://github.com/department-of-veterans-affairs/vets-api/pull/25414#discussion_r2611097819

--- lib/dangerfile/parameter_filtering_allowlist_checker.rb
+++ lib/dangerfile/parameter_filtering_allowlist_checker.rb
@@ -68,7 +68,7 @@ module Dangerfile
         - The added key(s) **DO NOT** contain PII, PHI, or secrets
         - The key name isn't too generic. If it's a name used in other areas of the app, it could expose sensitive data
         - The business need for unfiltering this parameter is documented in the PR description
-        - Consider using the new `log_allowlist` parameter for per-call filtering instead (see #121130)
+        - The new `log_allowlist` parameter for per-call filtering has been considered (see #121130)
 
         **Per-call filtering alternative:**
         ```ruby
