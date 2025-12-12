a17ae17250e4f262a48eb077cf0e553b5aff9e10
Riley Anderson <5150r.d.anderson@gmail.com>
2025-12-12 08:56:03 -0700

    update cerner cookie log

--- app/controllers/concerns/authentication_and_sso_concerns.rb
+++ app/controllers/concerns/authentication_and_sso_concerns.rb
@@ -107,8 +107,8 @@ module AuthenticationAndSSOConcerns # rubocop:disable Metrics/ModuleLength
 
   def set_cerner_eligibility_cookie
     cookie_name = V1::SessionsController::CERNER_ELIGIBLE_COOKIE_NAME
-    previous_value = ActiveModel::Type::Boolean.new.cast(cookies.signed[cookie_name] || cookies[cookie_name])
 
+    previous_value = cookies[cookie_name]
     eligible = @current_user.cerner_eligible?
 
     cookies.permanent[cookie_name] = {
