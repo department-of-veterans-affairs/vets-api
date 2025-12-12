1fdad0d2e9d0dc98ad0de525eb75711e8fa22956
Joshua <joshing_you1@yahoo.com>
2025-12-12 12:34:44 -0500

    Fixed constant location

--- lib/lighthouse/benefits_claims/constants.rb
+++ lib/lighthouse/benefits_claims/constants.rb
@@ -202,15 +202,15 @@ module BenefitsClaims
       'Secondary Action Required',
       'Stage 2 Development' # Not currently used by VBMS but will eventually replace `Secondary Action Required`
     ].freeze
-  end
 
-  FIRST_TO_THIRD_PARTY_EVIDENCE_REQUESTS = [
-    'PMR Pending',
-    'Proof of service (DD214, etc.)',
-    'NG1 - National Guard Records Request',
-    'VHA Outpatient Treatment Records (10-7131)',
-    'HAIMS STR Follow-up',
-    'Audit Request'
-  ].freeze
+    FIRST_TO_THIRD_PARTY_EVIDENCE_REQUESTS = [
+      'PMR Pending',
+      'Proof of service (DD214, etc.)',
+      'NG1 - National Guard Records Request',
+      'VHA Outpatient Treatment Records (10-7131)',
+      'HAIMS STR Follow-up',
+      'Audit Request'
+    ].freeze
+  end
 end
 # rubocop:enable Metrics/ModuleLength
