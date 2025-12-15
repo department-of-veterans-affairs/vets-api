262e923183aa6f779032b8c3227e7d889d09f6a4
Josh Mills <josh.troy.mills@gmail.com>
2025-12-15 16:25:46 -0500

    api layer for copay detail data fetching

--- app/serializers/lighthouse/hcc/copay_detail_serializer.rb
+++ app/serializers/lighthouse/hcc/copay_detail_serializer.rb
@@ -1,24 +1,96 @@
 # frozen_string_literal: true
 
-class Lighthouse::HCC::CopayDetailSerializer
-  include JSONAPI::Serializer
+module Lighthouse
+  module HCC
+    class CopayDetailSerializer
+      include JSONAPI::Serializer
 
-  set_type :medical_copay_details
-  set_key_transform :camel_lower
-  set_id :external_id
+      set_type :medical_copay_details
+      set_key_transform :camel_lower
+      set_id :external_id
 
-  attributes :external_id,
-             :facility,
-             :bill_number,
-             :status,
-             :status_description,
-             :invoice_date,
-             :payment_due_date,
-             :original_amount,
-             :principal_balance,
-             :interest_balance,
-             :administrative_cost_balance,
-             :principal_paid,
-             :interest_paid,
-             :administrative_cost_paid
+      attributes :external_id,
+                 :facility,
+                 :bill_number,
+                 :status,
+                 :status_description,
+                 :invoice_date,
+                 :payment_due_date,
+                 :account_number,
+                 :original_amount,
+                 :principal_balance,
+                 :interest_balance,
+                 :administrative_cost_balance,
+                 :principal_paid,
+                 :interest_paid,
+                 :administrative_cost_paid
+
+      attribute :line_items do |object|
+        object.line_items.map { |li| transform_line_item(li) }
+      end
+
+      attribute :payments do |object|
+        object.payments.map { |p| transform_payment(p) }
+      end
+
+      class << self
+        def transform_line_item(line_item)
+          result = {
+            billingReference: line_item[:billing_reference],
+            datePosted: line_item[:date_posted],
+            description: line_item[:description],
+            providerName: line_item[:provider_name],
+            priceComponents: transform_price_components(line_item[:price_components])
+          }
+          result[:medication] = transform_medication(line_item[:medication]) if line_item[:medication]
+          result
+        end
+
+        def transform_price_components(components)
+          return [] unless components
+
+          components.map do |pc|
+            { type: pc[:type], code: pc[:code], amount: pc[:amount] }
+          end
+        end
+
+        def transform_medication(medication)
+          return nil unless medication
+
+          {
+            medicationName: medication[:medication_name],
+            rxNumber: medication[:rx_number],
+            quantity: medication[:quantity],
+            daysSupply: medication[:days_supply]
+          }
+        end
+
+        def transform_payment(payment)
+          {
+            paymentId: payment[:payment_id],
+            paymentDate: payment[:payment_date],
+            paymentAmount: payment[:payment_amount],
+            transactionNumber: payment[:transaction_number],
+            billNumber: payment[:bill_number],
+            invoiceReference: payment[:invoice_reference],
+            disposition: payment[:disposition],
+            detail: transform_payment_detail(payment[:detail])
+          }
+        end
+
+        def transform_payment_detail(detail)
+          return [] unless detail
+
+          detail.map { |d| { type: d[:type], amount: d[:amount] } }
+        end
+      end
+
+      meta do |object|
+        {
+          lineItemCount: object.line_items.size,
+          paymentCount: object.payments.size
+        }
+      end
+    end
+  end
 end
