114c738fd8762124d57e18e3a16d447189fd5b50
Craig Donavin <craig.donavin@oddball.io>
2025-12-15 14:07:32 -0500

    add expires_at to form.update! remove skip_expiry

--- app/controllers/v0/in_progress_forms_controller.rb
+++ app/controllers/v0/in_progress_forms_controller.rb
@@ -49,7 +49,11 @@ module V0
 
       ClaimFastTracking::MaxCfiMetrics.log_form_update(form, params)
 
-      form.update!(form_data: params[:form_data] || params[:formData], metadata: params[:metadata])
+      form.update!(
+        form_data: params[:form_data] || params[:formData],
+        metadata: params[:metadata],
+        expires_at: Time.current + form.expires_after
+      )
 
       render json: InProgressFormSerializer.new(form)
     end
@@ -76,7 +80,11 @@ module V0
 
         ClaimFastTracking::MaxCfiMetrics.log_form_update(form, params)
 
-        form.update!(form_data:, metadata: params[:metadata])
+        form.update!(
+          form_data:,
+          metadata: params[:metadata],
+          expires_at: Time.current + form.expires_after
+        )
 
         render json: InProgressFormSerializer.new(form)
       end
