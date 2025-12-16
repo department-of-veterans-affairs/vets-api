5f48db93949462a4ec912f90c2fc9e1fdfeba47a
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-12-16 17:00:01 -0500

    Updates

--- modules/debts_api/lib/debts_api/v0/fsr_form_transform/full_transform_service.rb
+++ modules/debts_api/lib/debts_api/v0/fsr_form_transform/full_transform_service.rb
@@ -56,7 +56,7 @@ module DebtsApi
         private
 
         def report_form_types
-          tracking_label = "full_transform.#{streamlined? ? 'has' : 'no'}_streamlined_data"
+          tracking_label = "full_transform.#{@streamlined['type'] ? 'has' : 'no'}_streamlined_data"
           streamlined_type = @streamlined['type']
           StatsD.increment("#{DebtsApi::V0::Form5655Submission::STATS_KEY}.#{tracking_label}")
           StatsD.increment("#{DebtsApi::V0::Form5655Submission::STATS_KEY}.#{streamlined_type}_streamlined_type")
