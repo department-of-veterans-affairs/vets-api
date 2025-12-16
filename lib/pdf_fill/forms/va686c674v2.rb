e148f073a9c583995d86d166f676b9156cf20263
David Becker <david@beckersite.com>
2025-12-16 10:29:48 -0700

    127385 pdf income question fix (#25587)
    * Handle string value for radio questions rather than boolean
    
    * Fix specs
    
    * Add codeowners entry

--- lib/pdf_fill/forms/va686c674v2.rb
+++ lib/pdf_fill/forms/va686c674v2.rb
@@ -1963,7 +1963,7 @@ module PdfFill
       def add_dependent_income(dependent_name, dependent_income)
         return '' if dependent_name.blank?
 
-        "\n\nDid #{dependent_name} have an income in the last 365 days? #{format_boolean(dependent_income)}"
+        "\n\nDid #{dependent_name} have an income in the last 365 days? #{format_radio_yes_no(dependent_income)}"
       end
 
       def add_dependents(dependents_hash, income_attr)
