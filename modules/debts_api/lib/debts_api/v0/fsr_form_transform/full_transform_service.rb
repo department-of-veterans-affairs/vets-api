c62174fc5ce1faeae227fa8928ababed2213283e
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-12-16 15:12:14 -0500

    Cool

--- modules/debts_api/lib/debts_api/v0/fsr_form_transform/full_transform_service.rb
+++ modules/debts_api/lib/debts_api/v0/fsr_form_transform/full_transform_service.rb
@@ -59,7 +59,7 @@ module DebtsApi
           tracking_label = "full_transform.#{streamlined? ? 'has' : 'no'}_streamlined_data"
           streamlined_type = @streamlined['type']
           StatsD.increment("#{DebtsApi::V0::Form5655Submission::STATS_KEY}.#{tracking_label}")
-          StatsD.increment("#{DebtsApi::V0::Form5655Submission::STATS_KEY}.#{streamlined_type}_streamlined_data")
+          StatsD.increment("#{DebtsApi::V0::Form5655Submission::STATS_KEY}.#{streamlined_type}_streamlined_type")
         rescue => e
           Rails.logger.error("FsrFormTransform::FullTransformService::#report_form_types error: #{e.message}")
           nil
