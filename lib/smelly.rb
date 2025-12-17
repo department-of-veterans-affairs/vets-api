f922df17b0c524793987794f45908515dc0e7ed5
Joseph Weissman <joseph.weissman@oddball.io>
2025-12-17 16:23:41 -0500

    exclude pathsfrom reek and add more problems to example code

--- lib/smelly.rb
+++ lib/smelly.rb
@@ -1,6 +1,36 @@
 # frozen_string_literal: true
 
 class Smelly
-  # Should trigger a code smell
+  # Should trigger a MissingSafeMethod code smell
   def missing_safe_method! = Rails.logger.debug 'This method is smelly'
+
+  # Should trigger a DuplicateMethodCall code smell
+  def dup_meth_calls(user)
+    user.profile.name
+    user.profile.name
+    user.profile.name
+    user.profile.name
+    user.profile.name
+    user.profile.name
+  end
+
+  # Should trigger a NestedIterators code smell
+  def nested_iterators(items)
+    items.each do |item|
+      item.children.each do |child|
+        child.each_value do |value|
+          process(value)
+        end
+      end
+    end
+  end
+
+  # Control param
+  def perform(action)
+    if action == :create
+      create_record
+    else
+      update_record
+    end
+  end
 end
