5f0427fef04b830f83faf07ae5ba9071bc33e5b7
Joseph Weissman <joseph.weissman@oddball.io>
2025-12-17 16:08:20 -0500

    fix rubocop issues for example code

--- lib/smelly.rb
+++ lib/smelly.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 class Smelly
   # Should trigger a code smell
-  def missing_safe_method!
-  end
+  def missing_safe_method! = Rails.logger.debug 'This method is smelly'
 end
