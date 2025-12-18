75d8616c5777ffa078865ba981e9a18b3ad9522d
Joseph Weissman <joseph.weissman@oddball.io>
2025-12-18 13:55:28 -0500

    describe new initializer more precisely

--- config/initializers/safe_semantic_logging.rb
+++ config/initializers/safe_semantic_logging.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 # Patch SemanticLogger to handle non-exception 'exception' values gracefully
+#
+# When logging with SemanticLogger, it's possible that the :exception key in the
+# log payload may contain a String or other non-exception object. This can lead to
+# logging failures since SemanticLogger expects an Exception object with a
+# backtrace method.
+#
+# This monkey-patch prevents logging failures when a String or other non-exception object
+# is passed as the :exception key in log payloads
 module SafeSemanticLogging
   def error(message = nil, payload = nil, &)
     if payload.is_a?(Hash)
