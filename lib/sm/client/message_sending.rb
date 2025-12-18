d3da8beeaed05d94d082ee421f35f7f75c9c2251
Alex Morgun <87077843+alex-morgun-1@users.noreply.github.com>
2025-12-18 11:55:38 -0500

    fixing copilot suggestions

--- lib/sm/client/message_sending.rb
+++ lib/sm/client/message_sending.rb
@@ -124,13 +124,13 @@ module SM
         perform(method, path, args.to_h, headers).body
       rescue => e
         if oh_pilot_user?
-          log_exception_to_rails('MHV SM OH Pilot User: Message Send Failed', {
-                                   error: e.message,
-                                   recipient_id: args[:recipient_id].to_s[-6..],
-                                   path:,
-                                   mhv_correlation_id: "****#{current_user&.mhv_correlation_id.to_s[-6..]}",
-                                   client_type: client_type_name
-                                 })
+          log_message_to_rails('MHV SM OH Pilot User: Message Send Failed', {
+                                 error: e.message,
+                                 recipient_id: args[:recipient_id]&.to_s&.last(6),
+                                 path:,
+                                 mhv_correlation_id: "****#{current_user&.mhv_correlation_id.to_s[-6..]}",
+                                 client_type: client_type_name
+                               }, 'error')
         end
         raise e
       end
@@ -157,9 +157,9 @@ module SM
         log_message_to_rails("MHV SM OH Pilot User: #{method_name}",
                              {
                                message_id: message&.id,
-                               recipient_id: message&.recipient_id.to_s[-6..],
+                               recipient_id: message&.recipient_id&.to_s&.last(6),
                                is_oh_message: message&.is_oh_message,
-                               mhv_correlation_id: "****#{current_user&.mhv_correlation_id.to_s[-6..]}",
+                               mhv_correlation_id: "****#{current_user&.mhv_correlation_id.to_s.last(6)}",
                                client_type: client_type_name
                              }, 'info')
       end
