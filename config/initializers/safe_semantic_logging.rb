ac9fcca253f555802e933f02b2e106e9de1cd459
Joseph Weissman <joseph.weissman@oddball.io>
2025-12-19 11:12:57 -0500

    override SemanticLogger::Base#log_internal directly instead of instrumenting Rails.logger

--- config/initializers/safe_semantic_logging.rb
+++ config/initializers/safe_semantic_logging.rb
@@ -10,21 +10,36 @@
 # This monkey-patch prevents logging failures when a String or other non-exception object
 # is passed as the :exception key in log payloads
 module SafeSemanticLogging
-  def error(message = nil, payload = nil, &)
+  # Note: We _could_ override error directly but this breaks RSpec spies on Rails.logger
+  # def error(message = nil, payload = nil, &)
+  #   if SafeSemanticLogging.safe_log_enabled? && payload.is_a?(Hash)
+  #     ex = payload[:exception]
+  #     payload = payload.merge(exception: RuntimeError.new(ex.to_s)) if ex && !ex.respond_to?(:backtrace)
+
+  #     # Maybe worthwhile to see if coverage can help us here?
+  #     # if Rails.env.test? && ex && !ex.is_a?(Exception)
+  #     #   raise 'SafeSemanticLogging enabled - non-exception logged as exception'
+  #     # end
+
+  #     # Handle nil exception too
+  #     payload = payload.merge(exception: RuntimeError.new('No exception provided')) if ex.nil?
+  #   end
+  #   super
+  # end
+
+  def log_internal(level, index, message, payload = nil, exception, &block)
     if SafeSemanticLogging.safe_log_enabled? && payload.is_a?(Hash)
       ex = payload[:exception]
-      payload = payload.merge(exception: RuntimeError.new(ex.to_s)) if ex && !ex.respond_to?(:backtrace)
-
-      # Maybe worthwhile to see if coverage can help us here?
-      # if Rails.env.test? && ex && !ex.is_a?(Exception)
-      #   raise 'SafeSemanticLogging enabled - non-exception logged as exception'
-      # end
-
-      # Handle nil exception too
-      payload = payload.merge(exception: RuntimeError.new('No exception provided')) if ex.nil?
+      if ex && !ex.respond_to?(:backtrace)
+        exception ||= RuntimeError.new(ex.to_s)
+      elsif ex.nil?
+        exception ||= RuntimeError.new('No exception provided')
+      end
     end
-    super
+
+    super(level, index, message, payload, exception, &block)
   end
+
 
   def self.safe_log_enabled?
     return false unless database_exists?
