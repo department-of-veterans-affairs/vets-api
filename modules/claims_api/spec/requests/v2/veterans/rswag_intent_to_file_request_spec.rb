dc53985867be7ee6f1fe628c637ae6b25ec6b183
siddharthalamsal-va <242282145+siddharthalamsal@users.noreply.github.com>
2025-12-22 19:26:28 +0000

    reword

--- modules/claims_api/spec/requests/v2/veterans/rswag_intent_to_file_request_spec.rb
+++ modules/claims_api/spec/requests/v2/veterans/rswag_intent_to_file_request_spec.rb
@@ -336,7 +336,7 @@ describe 'IntentToFile', openapi_spec: Rswag::TextHelpers.new.claims_api_docs do
         end
       end
 
-      describe 'Getting 500 Internal Server Error from BGS' do
+      describe 'When BGS returns 500 Internal Server Error' do
         response '404', '0966 Response' do
           schema JSON.parse(Rails.root.join('spec', 'support', 'schemas', 'claims_api', 'v2', 'errors',
                                             'default.json').read)
@@ -368,7 +368,7 @@ describe 'IntentToFile', openapi_spec: Rswag::TextHelpers.new.claims_api_docs do
             }
           end
 
-          it 'returns a 404 response' do |example|
+          it 'return a 404 response' do |example|
             assert_response_matches_metadata(example.metadata)
           end
         end
