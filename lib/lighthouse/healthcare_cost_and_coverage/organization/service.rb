b6ed37c7d96f2c6a624a8520cdebafd828fc4db9
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-12-26 15:37:53 -0500

    Add City to summary

---
+++ lib/lighthouse/healthcare_cost_and_coverage/organization/service.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'common/client/base'
+require 'lighthouse/healthcare_cost_and_coverage/configuration'
+require 'lighthouse/service_exception'
+
+module Lighthouse
+  module HealthcareCostAndCoverage
+    module Organization
+      class Service < Common::Client::Base
+        configuration Lighthouse::HealthcareCostAndCoverage::Configuration
+        STATSD_KEY_PREFIX = 'api.lighthouse.hccc.organization'
+
+        def initialize(icn)
+          @icn = icn
+          raise ArgumentError, 'no ICN passed in for HCCC request' if icn.blank?
+
+          super()
+        end
+
+        def read(id)
+          raise ArgumentError, 'no ID passed in for HCCC Invoice read request' if id.blank?
+
+          endpoint = "r4/Organization/#{id}"
+
+          config.get(endpoint, icn: @icn).body
+        rescue Faraday::TimeoutError, Faraday::ClientError, Faraday::ServerError, Faraday::ParsingError => e
+          handle_error(e, endpoint)
+        end
+
+        private
+
+        def handle_error(error, endpoint)
+          Lighthouse::ServiceException.send_error(
+            error,
+            self.class.to_s.underscore,
+            nil,
+            "#{config.base_api_path}/#{endpoint}"
+          )
+        end
+      end
+    end
+  end
+end
