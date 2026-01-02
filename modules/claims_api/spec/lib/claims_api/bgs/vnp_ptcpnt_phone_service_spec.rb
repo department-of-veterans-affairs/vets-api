8e0e1bf034526e0eb5804ed865c8ca4f7b0b6339
rockwellwindsor-va <129893414+rockwellwindsor-va@users.noreply.github.com>
2026-01-02 12:07:52 -0500

    Small style tweak to tests
    * The blocks of test sometimes can be hard to read, for some of these more dense test bloacks a Arrange-Act-Assert style helps readability
    modified:   modules/claims_api/spec/lib/claims_api/bgs/vnp_ptcpnt_phone_service_spec.rb

--- modules/claims_api/spec/lib/claims_api/bgs/vnp_ptcpnt_phone_service_spec.rb
+++ modules/claims_api/spec/lib/claims_api/bgs/vnp_ptcpnt_phone_service_spec.rb
@@ -14,8 +14,10 @@ describe ClaimsApi::VnpPtcpntPhoneService do
       options[:vnp_ptcpnt_id] = '44693'
       options[:phone_nbr] = '2225552252'
       options[:efctv_dt] = '2020-07-16T18:20:17Z'
+
       VCR.use_cassette('claims_api/bgs/vnp_ptcpnt_phone_service/vnp_ptcpnt_phone_create') do
         response = subject.vnp_ptcpnt_phone_create(options)
+        
         expect(response[:vnp_proc_id]).to eq '29798'
         expect(response[:vnp_ptcpnt_id]).to eq '44693'
         expect(response[:phone_type_nm]).to eq 'Daytime'
@@ -30,10 +32,28 @@ describe ClaimsApi::VnpPtcpntPhoneService do
       options[:vnp_ptcpnt_id] = nil
       options[:phone_nbr] = '2225552252'
       options[:efctv_dt] = '2020-07-16T18:20:17Z'
+
       VCR.use_cassette('claims_api/bgs/vnp_ptcpnt_phone_service/invalid_vnp_ptcpnt_phone_create') do
         expect do
           subject.vnp_ptcpnt_phone_create(options)
         end.to raise_error(Common::Exceptions::UnprocessableEntity)
+      end
+    end
+
+    it 'responds appropriately with an international number' do
+      options[:vnp_proc_id] = '3874842'
+      options[:vnp_ptcpnt_id] = '209001'
+      options[:phone_nbr] = ' '
+      options[:cntry_nbr] = '11'
+      options[:frgn_phone_rfrnc_txt] = '223333 4444'
+      options[:efctv_dt] = '2025-12-30T19:54:55Z'
+
+      VCR.use_cassette('claims_api/bgs/vnp_ptcpnt_phone_service/valid_vnp_ptcpnt_phone_create_international_phone') do
+        res = subject.vnp_ptcpnt_phone_create(options)
+
+        expect(res[:cntry_nbr]).to eq('11')
+        expect(res[:frgn_phone_rfrnc_txt]).to eq('223333 4444')
+        expect(res[:phone_nbr]).to eq(' ')
       end
     end
   end
