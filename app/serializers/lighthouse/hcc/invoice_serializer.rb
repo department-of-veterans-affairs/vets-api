cebb18d70f4ace7c6fd51356a14f4c066c39c414
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2026-01-05 15:02:51 -0500

    Add facility_id

--- app/serializers/lighthouse/hcc/invoice_serializer.rb
+++ app/serializers/lighthouse/hcc/invoice_serializer.rb
@@ -9,6 +9,7 @@ class Lighthouse::HCC::InvoiceSerializer
 
   attributes :url,
              :facility,
+             :facility_id,
              :city,
              :external_id,
              :latest_billing_ref,
