dce7628d80cd50eb45cb2b5a072b13f313604d04
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2026-01-05 13:28:25 -0700

    Remove return and refactor error handling

--- app/controllers/v0/concerns/multi_provider_support.rb
+++ app/controllers/v0/concerns/multi_provider_support.rb
@@ -12,13 +12,17 @@ module V0
       end
 
       def get_claims_from_providers
-        return configured_providers.first.new(@current_user).get_claims if configured_providers.count == 1
-
         claims_data = []
         provider_errors = []
         configured_providers.each do |provider_class|
           provider = provider_class.new(@current_user)
           claims_data.concat(provider.get_claims['data'])
+        rescue Common::Exceptions::Unauthorized,
+               Common::Exceptions::Forbidden,
+               Common::Exceptions::GatewayTimeout,
+               Common::Exceptions::ServiceUnavailable,
+               Common::Exceptions::ResourceNotFound => e
+          raise e
         rescue => e
           handle_provider_error(provider_class, e, provider_errors)
         end
@@ -26,7 +30,6 @@ module V0
       end
 
       def handle_provider_error(provider_class, error, provider_errors)
-
         provider_errors << { provider: provider_class.name, error: 'Provider temporarily unavailable' }
 
         ::Rails.logger.error(
@@ -38,15 +41,28 @@ module V0
       end
 
       def get_claim_from_providers(claim_id)
-        return configured_providers.first.new(@current_user).get_claim(claim_id) if configured_providers.count == 1
-
         configured_providers.each do |provider_class|
           return provider_class.new(@current_user).get_claim(claim_id)
-        rescue => e
+        rescue Common::Exceptions::RecordNotFound
+          # Expected case: this provider doesn't have the claim, try next provider
           ::Rails.logger.info(
             "Provider #{provider_class.name} doesn't have claim",
-            { error_class: e.class.name }
+            { error_class: 'Common::Exceptions::RecordNotFound' }
           )
+        rescue Common::Exceptions::Unauthorized,
+               Common::Exceptions::Forbidden,
+               Common::Exceptions::GatewayTimeout,
+               Common::Exceptions::ServiceUnavailable,
+               Common::Exceptions::ResourceNotFound => e
+          raise e
+        rescue => e
+          # Unexpected error: log and try next provider
+          ::Rails.logger.error(
+            "Provider #{provider_class.name} error fetching claim",
+            { error_class: e.class.name, backtrace: e.backtrace&.first(3) }
+          )
+          StatsD.increment("#{self.class::STATSD_METRIC_PREFIX}.get_claim.provider_error",
+                           tags: self.class::STATSD_TAGS + ["provider:#{provider_class.name}"])
         end
         raise Common::Exceptions::RecordNotFound, claim_id
       end
