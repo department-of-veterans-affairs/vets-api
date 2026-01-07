e8c5773a164776befe46e9cbbcc49b6957f17820
Joseph Weissman <joseph.weissman@oddball.io>
2026-01-07 12:51:02 -0500

    reset changes to flipper_utils

--- lib/flipper_utils.rb
+++ lib/flipper_utils.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 # lib/flipper_utils.rb
+# catch that annoying set of errors that happens during startup on a couple of configurations
+# What's happening is that Flipper is not fully initialized and enabled and calls like
+# Flipper.enabled?(feature_name) throw an exception which produces an error in the log on startup.
+# These errors also appear anytime you run a migration or a couple of other tasks.
 module FlipperUtils
-  # catch that annoying set of errors that happens during startup on a couple of configurations
-  # What's happening is that Flipper is not fully initialized and enabled and calls like
-  # Flipper.enabled?(feature_name) throw an exception which produces an error in the log on startup.
-  # These errors also appear anytime you run a migration or a couple of other tasks.
   def self.safe_enabled?(feature_name)
     return false unless defined?(Flipper) && Flipper.respond_to?(:enabled)
 
