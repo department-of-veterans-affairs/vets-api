5d0bad0703ddb65d46d8046ea1d37a4a1a913a27
Rachal.Cassity <rachal.cassity@oddball.io>
2026-01-07 16:39:21 -0600

    Created CarrierWave::Storage::AWSFile patch

---
+++ config/initializers/carrierwave_aws_file.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module CarrierWave
+  module Storage
+    class AWSFile
+      def store(new_file)
+        if new_file.is_a?(self.class)
+          new_file.move_to(path)
+        elsif Aws::S3.const_defined?(:TransferManager)
+          options = aws_options.write_options(new_file).except(:body)
+          options[:multipart_threshold] = AWSOptions::MULTIPART_THRESHOLD
+          Aws::S3::TransferManager.new(client: connection.client).upload_file(new_file.path, bucket: bucket.name,
+                                                                                             key: path, **options)
+        else
+          file.upload_file(new_file.path, aws_options.write_options(new_file))
+        end
+      end
+    end
+  end
+end
