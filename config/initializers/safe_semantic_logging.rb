4261496b33d059c5a5f59655eeb031962b98100b
Rachal Cassity <rachal.cassity@oddball.io>
2026-01-08 16:07:16 -0600

    Fixed some of the semantic logger errors to help stabilize vets-api servers (#25868)
    * Fix some semantic logger errors
    
    * Updated more files
    
    * lint
    
    * added another flipper

--- config/initializers/safe_semantic_logging.rb
+++ config/initializers/safe_semantic_logging.rb
@@ -12,9 +12,19 @@
 module SafeSemanticLogging
   # NOTE: We _could_ override error directly but this breaks RSpec spies on Rails.logger
   def log_internal(level, index, message = nil, payload = nil, exception = nil, &)
-    if SafeSemanticLogging.safe_log_enabled? && payload.is_a?(Hash)
-      ex = payload[:exception]
-      exception ||= RuntimeError.new(ex.to_s) if ex && !ex.respond_to?(:backtrace)
+    if SafeSemanticLogging.safe_log_enabled?
+      # Handle exception passed as third positional argument
+      exception = RuntimeError.new(exception.to_s) if exception.present? && !exception.respond_to?(:backtrace)
+
+      # Handle exception inside payload hash
+      if payload.present? && payload.is_a?(Hash)
+        ex = payload[:exception]
+        if ex && !ex.respond_to?(:backtrace)
+          payload = payload.dup
+          payload[:exception] = RuntimeError.new(ex.to_s)
+        end
+
+      end
     end
 
     super(level, index, message, payload, exception, &)
