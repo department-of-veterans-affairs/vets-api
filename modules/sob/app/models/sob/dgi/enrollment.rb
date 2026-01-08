1b18552bd053e1a68bc98cb680035f4be964dd9c
Jeff Marks <jeff.t.marks@gmail.com>
2026-01-08 10:17:23 -0700

    add begin date

--- modules/sob/app/models/sob/dgi/enrollment.rb
+++ modules/sob/app/models/sob/dgi/enrollment.rb
@@ -5,9 +5,10 @@ module SOB
     class Enrollment
       include Vets::Model
 
-      # Facility code of most recent enrollment required by form 22-10203 in order to send SCO email
+      # Facility code and begin date required by form 22-10203 in order to send SCO email
       # Additional enrollment data available from DGI response but not currently required
       attribute :facility_code, String
+      attribute :begin_date, DateTime
     end
   end
 end
