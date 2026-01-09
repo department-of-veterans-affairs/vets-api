05c0a0cf4f8c143eb75d1e4e5d37b3cefabcd112
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2026-01-09 15:45:19 -0600

    updated spec with new stamp total

--- modules/simple_forms_api/spec/models/vba_21p_0537_spec.rb
+++ modules/simple_forms_api/spec/models/vba_21p_0537_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe SimpleFormsApi::VBA21p0537 do
         }
       end
 
-      it 'returns the signature stamp' do
+      it 'returns the "in reply refer to" and signature stamps' do
         stamps = described_class.new(short_email_data).desired_stamps
         expect(stamps.length).to eq(2) # "in reply refer to" and signature text
         expect(stamps.first[:text]).to eq 'Test Signature'
