8db382a1cf23b59ee3b4865ef53d42eaedf3011b
Kris Pethtel <kris.pethtel@gmail.com>
2026-01-12 10:52:20 -0600

    cleanup

--- spec/requests/v0/tsa_letter_spec.rb
+++ spec/requests/v0/tsa_letter_spec.rb
@@ -19,19 +19,8 @@ RSpec.describe 'VO::TsaLetter', type: :request do
       end
     end
 
-    it 'returns the tsa letter metadata' do
-      # params = {:pageRequest=>{:resultsPerPage=>10, :page=>1},
-      #  :filters=>{"providerData.subject"=>{:evaluationType=>"CONTAINS", :value=>"[\"VETS Safe Travel Outreach Letter\"]"}},
-      #  :sort=>[]}
-      # mocked_response = Faraday::Response.new(response_body: tsa_letters, status: 200)
-      # mocked_env = Faraday::Env.new(response: mocked_response).tap do |e|
-      #   e.status = mocked_response.status
-      #   e.body = mocked_response.body
-      # end
-      # expect_any_instance_of(Faraday::Connection).to receive(:post).with("folders/files:search", params).and_return(mocked_response)
-      # allow(mocked_response).to receive(:env).and_return(mocked_env)
-
-      VCR.use_cassette('tsa_letters/index_success', { match_requests_on: %i[method uri] }) do
+    it 'renders error message' do
+      VCR.use_cassette('tsa_letters/index_not_found', { match_requests_on: %i[method uri] }) do
         get '/v0/tsa_letter'
         expect(response.body).to eq({uuid: 'c75438b4-47f8-44d3-9e35-798158591456', version: '920debba-cc65-479c-ab47-db9b2a5cd95f'}.to_json)
       end
