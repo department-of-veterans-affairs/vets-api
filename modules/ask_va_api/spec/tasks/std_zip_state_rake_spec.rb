de9217cfc304a71cb8aa43a7ede9bf550cba8d25
Alex Castillo <acastillo@Alex-Castillo-MacBook-Pro.local>
2026-01-12 15:13:52 -0500

    add spec to ensure seed task only runs in development

---
+++ modules/ask_va_api/spec/tasks/std_zip_state_rake_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rake'
+
+RSpec.describe 'ask_va_api:seed:std_zip_state', type: :task do
+  subject(:task) { Rake::Task[task_name] }
+
+  let(:task_name) { 'ask_va_api:seed:std_zip_state' }
+  let(:task_path) { File.expand_path('../../lib/tasks/ask_va_api/seed/std_zip_state.rake', __dir__) }
+
+  before do
+    Rake.application = Rake::Application.new
+    load task_path
+    Rake::Task.define_task(:environment)
+  end
+
+  after do
+    task.reenable
+  end
+
+  def stub_environment(env_name)
+    allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new(env_name))
+  end
+
+  describe 'environment guard' do
+    it 'aborts outside development' do
+      stub_environment('test')
+
+      expect { task.invoke }.to raise_error(SystemExit)
+    end
+  end
+
+end
