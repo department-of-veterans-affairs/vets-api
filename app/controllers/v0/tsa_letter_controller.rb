ddd3c369ce920321d3db18629cc49c8d2e4d50f4
Kris Pethtel <kris.pethtel@gmail.com>
2026-01-13 13:58:28 -0600

    nearly complete

--- app/controllers/v0/tsa_letter_controller.rb
+++ app/controllers/v0/tsa_letter_controller.rb
@@ -6,15 +6,6 @@ module V0
   class TsaLetterController < ApplicationController
     service_tag 'tsa_letter'
 
-    # x figure out routing
-    # "record" 404 vcr cassette
-    # serializer
-    # finish specs
-    # add spec for empty response
-    # make sorting safer
-    # x match on body
-    # turn off feature flag in staging
-
     def show
       search_service = ClaimsEvidenceApi::Service::Search.new
       filters = { subject: ['VETS Safe Travel Outreach Letter'] }
@@ -22,19 +13,10 @@ module V0
       search_service.folder_identifier = folder_identifier
       response = search_service.find(filters:)
       files = response.body['files']
-      latest = files.max do |a, b|
-        # this needs to be more secure
-        a_time = DateTime.parse(a['currentVersion']['systemData']['uploadedDateTime'])
-        b_time = DateTime.parse(b['currentVersion']['systemData']['uploadedDateTime'])
-        a_time <=> b_time
-      end
-      latest_uuid = latest['uuid']
-      latest_version = latest['currentVersionUuid']
-      upload_date = latest['currentVersion']['systemData']['uploadedDateTime']
-      render(json: {uuid: latest_uuid, version: latest_version, upload_date:})
+      serialized = most_recent_letter(files)
+      render(json: serialized)
     rescue => e
-      if e.respond_to?(:status) && e.status == 404
-        # should this return an empty instead?
+      if e.respond_to?(:status) && e.status == 403
         raise Common::Exceptions::RecordNotFound, current_user.user_account_uuid
       else
         raise e
@@ -53,6 +35,24 @@ module V0
 
     def service
       Efolder::Service.new(@current_user)
+    end
+
+    def most_recent_letter(files)
+      return { data: nil } if files.nil?
+
+      latest = files.max do |a, b|
+        # this needs to be more secure
+        a_time = DateTime.parse(a['currentVersion']['systemData']['uploadedDateTime'])
+        b_time = DateTime.parse(b['currentVersion']['systemData']['uploadedDateTime'])
+        a_time <=> b_time
+      end
+      document_id = latest['uuid']
+      document_version = latest['currentVersionUuid']
+      upload_datetime = latest['currentVersion']['systemData']['uploadedDateTime']
+      tsa_letter_metadata = OpenStruct.new(document_id:, document_version:, upload_datetime:)
+      TsaLetterSerializer.new(tsa_letter_metadata)
+      rescue Date::Error => e
+        Rails.logger.error()
     end
   end
 end
--- spec/requests/v0/tsa_letter_spec.rb
+++ spec/requests/v0/tsa_letter_spec.rb
@@ -14,7 +14,21 @@ RSpec.describe 'VO::TsaLetter', type: :request do
     it 'returns the most recent tsa letter metadata' do
       VCR.use_cassette('tsa_letters/show_success', { match_requests_on: %i[method uri body] }) do
         get '/v0/tsa_letter'
-        expect(response.body).to eq({uuid: 'c75438b4-47f8-44d3-9e35-798158591456', version: '920debba-cc65-479c-ab47-db9b2a5cd95f', "upload_date": '2025-09-09T14:18:53'}.to_json)
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to eq({ data: { id: '', type: 'tsa_letter',
+                                              attributes: {
+                                                document_id: 'c75438b4-47f8-44d3-9e35-798158591456',
+                                                document_version: '920debba-cc65-479c-ab47-db9b2a5cd95f',
+                                                upload_datetime: '2025-09-09T14:18:53'
+                                              } } }.to_json)
+      end
+    end
+
+    it 'returns an empty response' do
+      VCR.use_cassette('tsa_letters/show_success_empty', { match_requests_on: %i[method uri body] }) do
+        get '/v0/tsa_letter'
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to eq({ data: nil }.to_json)
       end
     end
 
@@ -22,22 +36,22 @@ RSpec.describe 'VO::TsaLetter', type: :request do
       it 'returns 404' do
         VCR.use_cassette('tsa_letters/show_not_found', { match_requests_on: %i[method uri body] }) do
           get '/v0/tsa_letter'
-          expect(response.status).to eq(404)
+          expect(response).to have_http_status(:not_found)
         end
       end
     end
 
     context 'when upstream returns other error' do
-      it 'it returns 503' do
+      it 'returns 503' do
         VCR.use_cassette('tsa_letters/show_error', { match_requests_on: %i[method uri body] }) do
           get '/v0/tsa_letter'
-          expect(response.status).to eq(503)
+          expect(response).to have_http_status(:service_unavailable)
         end
       end
     end
   end
 
-  describe 'GET /v0/tsa_letter/:id', pending: 'route change' do
+  describe 'GET /v0/tsa_letter/:id' do
     let(:document_id) { '{93631483-E9F9-44AA-BB55-3552376400D8}' }
     let(:content) { File.read('spec/fixtures/files/error_message.txt') }
 
@@ -45,7 +59,7 @@ RSpec.describe 'VO::TsaLetter', type: :request do
       expect(efolder_service).to receive(:get_tsa_letter).with(document_id).and_return(content)
     end
 
-    it 'sends the doc pdf' do
+    it 'sends the doc pdf', pending: 'route change' do
       get "/v0/tsa_letter/#{CGI.escape(document_id)}"
       expect(response.body).to eq(content)
     end
