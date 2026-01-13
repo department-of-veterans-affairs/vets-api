a0e427bcfe4cf86837fcde452a61b1ae51fe1d3c
Rachal.Cassity <rachal.cassity@oddball.io>
2026-01-13 08:37:39 -0600

    reverted appeals

--- app/controllers/v0/appeals_controller.rb
+++ app/controllers/v0/appeals_controller.rb
@@ -3,7 +3,6 @@
 module V0
   class AppealsController < AppealsBaseController
     service_tag 'appeal-status'
-    before_action { authorize :appeals, :access? }
 
     def index
       appeals_response = appeals_service.get_appeals(current_user)
