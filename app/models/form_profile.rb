4b1d7cecd92ee558d85bb9d4102713303a5802eb
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2026-01-14 11:07:07 -0600

    enable 10278 prefill

--- app/models/form_profile.rb
+++ app/models/form_profile.rb
@@ -98,7 +98,7 @@ class FormProfile
     dispute_debt: ['DISPUTE-DEBT'],
     edu: %w[22-1990 22-1990EMEB 22-1995 22-5490 22-5490E
             22-5495 22-0993 22-0994 FEEDBACK-TOOL 22-10203 22-1990EZ
-            22-10297 22-0803 22-10272],
+            22-10297 22-0803 22-10272 22-10278],
     evss: ['21-526EZ'],
     form_mock_ae_design_patterns: ['FORM-MOCK-AE-DESIGN-PATTERNS'],
     form_mock_prefill: ['FORM-MOCK-PREFILL'],
@@ -164,6 +164,7 @@ class FormProfile
     '22-10203' => ::FormProfiles::VA10203,
     '22-10272' => ::FormProfiles::VA10272,
     '22-10297' => ::FormProfiles::VA10297,
+    '22-10278' => ::FormProfiles::VA10278,
     '22-1990' => ::FormProfiles::VA1990,
     '22-1990EMEB' => ::FormProfiles::VA1990emeb,
     '22-1990EZ' => ::FormProfiles::VA1990ez,
