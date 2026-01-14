ffc780d0dfcfbf1a972f89d45388e12f714d4da4
Alex Castillo <acastillo@Alex-Castillo-MacBook-Pro.local>
2026-01-14 18:01:36 -0500

    add spec to verify rake task seeds data in development

--- modules/ask_va_api/spec/tasks/std_zip_state_rake_spec.rb
+++ modules/ask_va_api/spec/tasks/std_zip_state_rake_spec.rb
@@ -30,4 +30,31 @@ RSpec.describe 'ask_va_api:seed:std_zip_state', type: :task do
       expect { task.invoke }.to raise_error(SystemExit)
     end
   end
+
+  describe 'development seeding' do
+    let(:seed_data) do
+      require Rails.root.join('modules', 'ask_va_api', 'lib', 'ask_va_api', 'seed', 'std_zip_state_records')
+      AskVAApi::Seed::StdZipStateRecords
+    end
+
+    before { stub_environment('development') }
+
+    it 'creates the expected states and zip codes' do
+      old_reset = ENV.fetch('RESET', nil)
+      ENV['RESET'] = 'true'
+
+      expected_state_codes = seed_data::STATES.map { |s| s[:postal_name] }
+      expected_zip_codes   = seed_data::ZIPCODES.map { |z| z[:zip_code] }
+
+      expect do
+        task.invoke
+      end.to change { StdState.where(postal_name: expected_state_codes).count }
+        .to(seed_data::STATES.size)
+        .and change { StdZipcode.where(zip_code: expected_zip_codes).count }
+        .to(seed_data::ZIPCODES.size)
+    ensure
+      ENV['RESET'] = old_reset
+      task.reenable
+    end
+  end
 end
