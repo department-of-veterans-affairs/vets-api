077358ce0479dd7b2758b0708a81d581475ce385
Rachal.Cassity <rachal.cassity@oddball.io>
2026-01-15 13:58:12 -0600

    updated vet360_contact_info

--- app/models/user.rb
+++ app/models/user.rb
@@ -383,6 +383,7 @@ class User < Common::RedisStore
   delegate :show_onboarding_flow_on_login, to: :onboarding, allow_nil: true
 
   def vet360_contact_info
+    return nil unless icn.present?
     @vet360_contact_info ||= VAProfileRedis::V2::ContactInformation.for_user(self)
   end
 
