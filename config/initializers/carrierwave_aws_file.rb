31db28c8192368cbf1bae0ba728f7bf27761d4ab
Rachal.Cassity <rachal.cassity@oddball.io>
2026-01-16 14:40:13 -0600

    freeze ALLOWED_UPLOAD_OPTIONS

--- config/initializers/carrierwave_aws_file.rb
+++ config/initializers/carrierwave_aws_file.rb
@@ -5,29 +5,26 @@ Rails.application.config.after_initialize do
   module CarrierWave
     module Storage
       class AWSFile
+        ALLOWED_UPLOAD_OPTIONS = %i[
+          acl multipart_threshold content_type cache_control content_disposition
+          content_encoding content_language expires server_side_encryption
+          storage_class metadata ssekms_key_id sse_customer_algorithm
+          sse_customer_key sse_customer_key_md5 tagging checksum_algorithm
+          content_md5 bucket_key_enabled
+        ].freeze
+
         def store(new_file) # rubocop:disable Metrics/MethodLength
           if new_file.is_a?(self.class)
             new_file.move_to(path)
           elsif Aws::S3.const_defined?(:TransferManager)
             options = aws_options.write_options(new_file).except(:body)
-            # MULTIPART_TRESHOLD was renamed to MULTIPART_THRESHOLD https://github.com/carrierwaveuploader/carrierwave-aws/commit/1117b0e1ccd2b653717136d846814d9a64aa5d72
+
             # MULTIPART_TRESHOLD was renamed to MULTIPART_THRESHOLD
+            # https://github.com/carrierwaveuploader/carrierwave-aws/commit/1117b0e1ccd2b653717136d846814d9a64aa5d72
             options[:multipart_threshold] = CarrierWave::Storage::AWSOptions::MULTIPART_TRESHOLD
 
             # Filter to only the options upload_file accepts
-            upload_options = options.slice(
-              :acl,
-              :multipart_threshold,
-              :content_type,
-              :cache_control,
-              :content_disposition,
-              :content_encoding,
-              :content_language,
-              :expires,
-              :server_side_encryption,
-              :storage_class,
-              :metadata
-            )
+            upload_options = options.slice(*ALLOWED_UPLOAD_OPTIONS)
 
             Aws::S3::TransferManager.new(client: connection.client).upload_file(
               new_file.path,
