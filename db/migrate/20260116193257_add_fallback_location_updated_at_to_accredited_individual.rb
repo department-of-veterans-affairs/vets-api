1cb276b51ee1700ca1c10340eccbc5fd940d3bb9
Josh Fike <josh.fike@oddball.io>
2026-01-16 13:52:27 -0600

    Revert "New column to track geolocation updates"
    This reverts commit bc526c8b4189f7b346835cca82a23644daf42739.

--- db/migrate/20260116193257_add_fallback_location_updated_at_to_accredited_individual.rb
+++
@@ -1,5 +0,0 @@
-class AddFallbackLocationUpdatedAtToAccreditedIndividual < ActiveRecord::Migration[7.2]
-  def change
-    add_column :accredited_individuals, :fallback_location_updated_at, :datetime
-  end
-end
