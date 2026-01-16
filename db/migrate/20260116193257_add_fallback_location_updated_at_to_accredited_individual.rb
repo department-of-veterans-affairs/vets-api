bc526c8b4189f7b346835cca82a23644daf42739
Josh Fike <josh.fike@oddball.io>
2026-01-16 13:39:48 -0600

    New column to track geolocation updates

---
+++ db/migrate/20260116193257_add_fallback_location_updated_at_to_accredited_individual.rb
@@ -0,0 +1,5 @@
+class AddFallbackLocationUpdatedAtToAccreditedIndividual < ActiveRecord::Migration[7.2]
+  def change
+    add_column :accredited_individuals, :fallback_location_updated_at, :datetime
+  end
+end
