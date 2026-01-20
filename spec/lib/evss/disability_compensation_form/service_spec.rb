17083fddd12604d7c4d26d19a5aa01722870ce84
Oren Mittman <nihil2501@gmail.com>
2026-01-19 23:24:08 -0500

    [DBC] Remove unused `EVSS::DisabilityCompensationForm::Service` spec
    Only removing this one spec example at this juncture in order to remove
    the reference to the cassette that I'm going to start reusing for valid
    purposes: `bdd_fe_submission.json`

--- spec/lib/evss/disability_compensation_form/service_spec.rb
+++ spec/lib/evss/disability_compensation_form/service_spec.rb
@@ -91,22 +91,6 @@ describe EVSS::DisabilityCompensationForm::Service do
       end
     end
 
-    context 'with valid BDD input' do
-      let(:valid_bdd_form_content) do
-        File.read 'spec/support/disability_compensation_form/bdd_fe_submission.json'
-      end
-
-      it 'returns a form submit response object' do
-        # TODO: this cassette seems hand-written and is missing a body
-        VCR.use_cassette('evss/disability_compensation_form/submit_form_v2') do
-          response = subject.submit_form526(valid_bdd_form_content)
-          expect(response).to be_ok
-          expect(response).to be_an EVSS::DisabilityCompensationForm::FormSubmitResponse
-          expect(response.claim_id).to be_an Integer
-        end
-      end
-    end
-
     context 'with an http timeout' do
       before do
         allow_any_instance_of(Faraday::Connection).to receive(:post).and_raise(Faraday::TimeoutError)
