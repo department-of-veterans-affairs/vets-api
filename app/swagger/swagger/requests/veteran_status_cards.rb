24090c0653dd7e63ff9f5ed64ebf88f1e63a9e87
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2026-01-21 12:14:28 -0500

    1056 | Resolved rubocop issues

--- app/swagger/swagger/requests/veteran_status_cards.rb
+++ app/swagger/swagger/requests/veteran_status_cards.rb
@@ -18,7 +18,9 @@ module Swagger
 
             schema do
               key :required, [:confirmed]
-              property :confirmed, type: :boolean, description: 'True or False depending on if the veteran is eligible for a status card'
+              property :confirmed, type: :boolean,
+                                   description: 'True or False depending on if the veteran is eligible \
+                                    for a status card'
               property :full_name, type: :object do
                 key :description, 'Displays the veterans name when eligible for a status card'
                 property :first, type: :string
@@ -26,7 +28,9 @@ module Swagger
                 property :last, type: :string
                 property :suffix, type: :string
               end
-              property :user_percent_of_disability, type: :integer, description: 'Displays the veterans disability rating when eligible for a status card'
+              property :user_percent_of_disability, type: :integer,
+                                                    description: 'Displays the veterans disability rating \
+                                                      when eligible for a status card'
               property :latest_service_history, type: :object do
                 key :description, 'Displays the veterans latest service history when eligible for a status card'
                 property :branch_of_service, type: :string
@@ -35,9 +39,15 @@ module Swagger
                   property :end_date, type: :string
                 end
               end
-              property :title, type: :string, description: 'Displays an error/warning title when the veteran is ineligible for a status card'
-              property :message, type: :string, description: 'Displays an error/warning message when the veteran is ineligible for a status card'
-              property :status, type: :string, description: "Displays either 'error' or 'warning' when the veteran is ineligible for a status card"
+              property :title, type: :string,
+                               description: 'Displays an error/warning title when the veteran is ineligible \
+                                for a status card'
+              property :message, type: :string,
+                                 description: 'Displays an error/warning message when the veteran is ineligible \
+                                  for a status card'
+              property :status, type: :string,
+                                description: "Displays either 'error' or 'warning' when the veteran is ineligible \
+                                  for a status card"
             end
           end
         end
