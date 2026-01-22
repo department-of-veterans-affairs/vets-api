657ae047b0bd6df4ad1f2f47e46fcbc63b1e9502
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2026-01-22 14:49:54 -0800

    cleanup

--- modules/facilities_api/app/controllers/facilities_api/v2/facilities_error.rb
+++ modules/facilities_api/app/controllers/facilities_api/v2/facilities_error.rb
@@ -3,31 +3,20 @@
 # Handles errors for Facilities API V2 controllers
 module FacilitiesApi::V2::FacilitiesError
   def handle_error(error, method)
-    return unless error
-
-    status = error.respond_to?(:response) ? error.response[:status] : nil
-
-    Rails.logger.error("Facilities API V2 #{method} error: #{error.message}")
-    if error.is_a?(Common::Exceptions::RecordNotFound) || error.is_a?(Faraday::ResourceNotFound) ||
-       error.is_a?(Net::HTTPNotFound) || status == 404
-      json_error(error, 'Not Found', '404', :not_found)
-    elsif error.is_a?(Common::Exceptions::BackendServiceException) && status == 502
-      json_error(error, 'Bad Gateway', '502', :bad_gateway)
-    elsif error.is_a?(Common::Exceptions::BackendServiceException) && status == 503
-      json_error(error, 'Service Unavailable', '503', :service_unavailable)
-    elsif error.is_a?(Common::Exceptions::Timeout) || error.is_a?(Net::ReadTimeout) ||
-          error.is_a?(Faraday::TimeoutError) || status == 504
-      json_error(error, 'Gateway Timeout', '504', :gateway_timeout)
-    elsif error.is_a?(Common::Exceptions::BackendServiceException)
-      json_error(error, 'Backend Service Error', error.response&.dig(:message).to_s, 500)
-    else
-      # Some other error we handle by giving something like a 400 to the user
-      # i.e. invalid parameters, etc.
-      raise error
-    end
+    nil unless error
+  rescue Common::Exceptions::RecordNotFound, Faraday::ResourceNotFound, Net::HTTPNotFound
+    json_error(method, error, 'Not Found', '404', :not_found)
+  rescue Common::Exceptions::BackendServiceException
+    json_error(method, error, 'Bad Gateway', '502', :bad_gateway)
+  rescue Common::Exceptions::ServiceUnavailable
+    json_error(method, error, 'Service Unavailable', '503', :service_unavailable)
+  rescue Common::Exceptions::Timeout, Net::ReadTimeout, Faraday::TimeoutError
+    json_error(method, error, 'Gateway Timeout', '504', :gateway_timeout)
   end
 
-  def json_error(error, title, code, status = :internal_server_error)
-    render json: { errors: [{ title:, detail: error.message, code: }] }, status:
+  def json_error(method, error, title, code, status = :internal_server_error)
+    Rails.logger.error("Facilities API V2 #{method} error: #{error.message}")
+    real_status = Rack::Utils.status_code(status)
+    render json: { errors: [{ title:, detail: error.message, code: }] }, status: real_status
   end
 end
