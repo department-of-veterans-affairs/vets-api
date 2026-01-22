892e6b0d18bdffc86756314443d0d80a2f47b08b
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2026-01-22 14:32:38 -0800

    repeated status checks

--- modules/facilities_api/app/controllers/facilities_api/v2/facilities_error.rb
+++ modules/facilities_api/app/controllers/facilities_api/v2/facilities_error.rb
@@ -5,18 +5,18 @@ module FacilitiesError
   def handle_error(error, method)
     return unless error
 
+    status = error.respond_to?(:response) ? error.response[:status] : nil
+
     Rails.logger.error("Facilities API V2 #{method} error: #{error.message}")
     if error.is_a?(Common::Exceptions::RecordNotFound) || error.is_a?(Faraday::ResourceNotFound) ||
-       error.is_a?(Net::HTTPNotFound) || (error.respond_to?(:response) && error.response[:status] == 404)
+       error.is_a?(Net::HTTPNotFound) || status == 404
       json_error(error, 'Not Found', '404', :not_found)
-    elsif error.is_a?(Common::Exceptions::BackendServiceException) &&
-          error.respond_to?(:response) && error.response[:status] == 502
+    elsif error.is_a?(Common::Exceptions::BackendServiceException) && status == 502
       json_error(error, 'Bad Gateway', '502', :bad_gateway)
-    elsif error.is_a?(Common::Exceptions::BackendServiceException) &&
-          error.respond_to?(:response) && error.response[:status] == 503
+    elsif error.is_a?(Common::Exceptions::BackendServiceException) && status == 503
       json_error(error, 'Service Unavailable', '503', :service_unavailable)
     elsif error.is_a?(Common::Exceptions::Timeout) || error.is_a?(Net::ReadTimeout) ||
-          error.is_a?(Faraday::TimeoutError) || (error.respond_to?(:response) && error.response[:status] == 504)
+          error.is_a?(Faraday::TimeoutError) || status == 504
       json_error(error, 'Gateway Timeout', '504', :gateway_timeout)
     elsif error.is_a?(Common::Exceptions::BackendServiceException)
       json_error(error, 'Backend Service Error', error.response[:message].to_s, 500)
