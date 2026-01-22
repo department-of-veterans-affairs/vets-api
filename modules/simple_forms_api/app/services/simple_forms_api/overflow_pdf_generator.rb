abef82ec005850ea4e0183c11139b0a7882179b5
Dominic Padula <dominic.padula@wilcore.io>
2026-01-22 09:47:22 -0600

    remove default to avoid confusion

--- modules/simple_forms_api/app/services/simple_forms_api/overflow_pdf_generator.rb
+++ modules/simple_forms_api/app/services/simple_forms_api/overflow_pdf_generator.rb
@@ -6,9 +6,8 @@ require 'securerandom'
 module SimpleFormsApi
   class OverflowPdfGenerator
     HEADER = 'VA Form 21-4138 — Overflow data from remark section'
-    DEFAULT_CUTOFF = 3685
 
-    def initialize(data, cutoff: DEFAULT_CUTOFF)
+    def initialize(data, cutoff:)
       @data = data || {}
       @cutoff = cutoff
     end
