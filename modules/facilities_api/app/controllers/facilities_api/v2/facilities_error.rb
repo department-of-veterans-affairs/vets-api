e356418a89be5aed0313040ed89b3f7d07f1ae48
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2026-01-22 14:25:33 -0800

    slight change

--- modules/facilities_api/app/controllers/facilities_api/v2/facilities_error.rb
+++ modules/facilities_api/app/controllers/facilities_api/v2/facilities_error.rb
@@ -9,15 +9,17 @@ module FacilitiesError
     if error.is_a?(Common::Exceptions::RecordNotFound) || error.is_a?(Faraday::ResourceNotFound) ||
        error.is_a?(Net::HTTPNotFound) || (error.respond_to?(:response) && error.response[:status] == 404)
       json_error(error, 'Not Found', '404', :not_found)
-    elsif error.is_a?(Common::Exceptions::BackendServiceException) ||
-          (error.respond_to?(:response) && error.response[:status] == 502)
+    elsif error.is_a?(Common::Exceptions::BackendServiceException) &&
+          error.respond_to?(:response) && error.response[:status] == 502
       json_error(error, 'Bad Gateway', '502', :bad_gateway)
-    elsif error.is_a?(Common::Exceptions::BackendServiceException) ||
-          (error.respond_to?(:response) && error.response[:status] == 503)
+    elsif error.is_a?(Common::Exceptions::BackendServiceException) &&
+          error.respond_to?(:response) && error.response[:status] == 503
       json_error(error, 'Service Unavailable', '503', :service_unavailable)
     elsif error.is_a?(Common::Exceptions::Timeout) || error.is_a?(Net::ReadTimeout) ||
           error.is_a?(Faraday::TimeoutError) || (error.respond_to?(:response) && error.response[:status] == 504)
       json_error(error, 'Gateway Timeout', '504', :gateway_timeout)
+    elsif error.is_a?(Common::Exceptions::BackendServiceException)
+      json_error(error, 'Backend Service Error', error.response[:message].to_s, 500)
     else
       # Some other error we handle by giving something like a 400 to the user
       # i.e. invalid parameters, etc.
