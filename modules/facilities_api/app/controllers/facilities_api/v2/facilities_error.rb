013991c40dd176d27e79c91e103e7052a7f904b3
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2026-01-22 16:22:17 -0800

    comments

--- modules/facilities_api/app/controllers/facilities_api/v2/facilities_error.rb
+++ modules/facilities_api/app/controllers/facilities_api/v2/facilities_error.rb
@@ -2,6 +2,14 @@
 
 # Handles errors for Facilities API V2 controllers
 module FacilitiesApi::V2::FacilitiesError
+  # To use this module, wrap controller actions with a rescue block that calls handle_error
+  # Example:
+  # def index
+  #   api_results = ppms_search
+  #   render_json(V2::PPMS::ProviderSerializer, ppms_params, api_results)
+  # rescue => e
+  #   handle_error('ccp_index', e)
+  # end
   def handle_error(method, e)
     raise e
   rescue Common::Exceptions::RecordNotFound, Faraday::ResourceNotFound, Net::HTTPNotFound => e
@@ -14,6 +22,14 @@ module FacilitiesApi::V2::FacilitiesError
     json_error(method, e, 'Bad Gateway', '502', :bad_gateway)
   end
 
+  private
+
+  # Helper method to render JSON error responses
+  # @param method [String] the name of the method where the error occurred
+  # @param error [Exception] the exception object
+  # @param title [String] the title of the error
+  # @param code [String] the error code
+  # @param status [Symbol] the HTTP status symbol
   def json_error(method, error, title, code, status)
     Rails.logger.error("Facilities API V2 #{method} error: #{error.class}")
 
