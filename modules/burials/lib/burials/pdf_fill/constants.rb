af49c5d4cb216d9cee31508c7d699cc829e7cb58
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2026-01-23 16:06:55 -0500

    Add Section 4 V2

--- modules/burials/lib/burials/pdf_fill/constants.rb
+++ modules/burials/lib/burials/pdf_fill/constants.rb
@@ -14,7 +14,10 @@ module Burials
         'otherFamily' => 5
       }.freeze
 
+<<<<<<< HEAD
       # The final resting place options
+=======
+>>>>>>> fc33d785f3 (Add Section 4 V2)
       RESTING_PLACES = {
         'cemetery' => 0,
         'mausoleum' => 1,
@@ -22,7 +25,10 @@ module Burials
         'other' => 3
       }.freeze
 
+<<<<<<< HEAD
       # The cemetery location options
+=======
+>>>>>>> fc33d785f3 (Add Section 4 V2)
       CEMETARY_LOCATION = {
         'cemetery' => 0,
         'tribalLand' => 1,
