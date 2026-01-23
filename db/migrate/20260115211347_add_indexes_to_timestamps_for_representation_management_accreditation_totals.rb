68bf84b74de80c372df5b2948945da03c3337cf0
Patrick Brown <patrick.brown@oddball.io>
2026-01-23 14:30:15 -0500

    arc/move_vso_reloader_to_representation_management_add_accreditation_totals - deleted old migration file

--- db/migrate/20260115211347_add_indexes_to_timestamps_for_representation_management_accreditation_totals.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexesToTimestampsForRepresentationManagementAccreditationTotals < ActiveRecord::Migration[7.2]
-  disable_ddl_transaction!
-
-  def change
-    add_index :representation_management_accreditation_totals, :created_at, algorithm: :concurrently
-  end
-end
