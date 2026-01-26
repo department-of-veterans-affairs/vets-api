2f749c0d3270ab9385d968fbd3cdf784be598051
Nate kutub <nate-kutub@Nader-Kutub.local>
2026-01-26 20:42:50 +0000

    seperate index creation into new migration

--- db/migrate/20260123162837_create_form_intake_submissions.rb
+++ db/migrate/20260123162837_create_form_intake_submissions.rb
@@ -34,20 +34,5 @@ class CreateFormIntakeSubmissions < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-
-    # Indexes for common query patterns
-    add_index :form_intake_submissions, :aasm_state
-    add_index :form_intake_submissions, :benefits_intake_uuid
-    add_index :form_intake_submissions, %i[form_submission_id aasm_state],
-              name: 'idx_form_intake_sub_on_form_sub_id_and_state'
-    add_index :form_intake_submissions, %i[aasm_state created_at],
-              name: 'idx_form_intake_sub_on_state_and_created'
-    add_index :form_intake_submissions, :form_intake_submission_id,
-              unique: true,
-              name: 'idx_form_intake_sub_on_intake_id',
-              where: "form_intake_submission_id IS NOT NULL"
-    add_index :form_intake_submissions, :last_attempted_at,
-              name: 'idx_form_intake_sub_on_last_attempted',
-              where: "aasm_state = 'pending'"
   end
 end
