9784575aa13509f4de59b8b2664733b20fbede0c
Kevin Duensing <462039+kjduensing@users.noreply.github.com>
2026-01-27 10:02:29 -0500

    Fix post-migration current_phase

--- lib/mhv/oh_facilities_helper/service.rb
+++ lib/mhv/oh_facilities_helper/service.rb
@@ -81,7 +81,7 @@ module MHV
 
       # Builds the migration response array for user's matching facilities
       def build_migration_response
-        return [] if @current_user.va_treatment_facility_ids.blank?
+        # return [] if @current_user.va_treatment_facility_ids.blank?
 
         parsed_migrations = parse_oh_migrations_list
         return [] if parsed_migrations.empty?
@@ -145,7 +145,6 @@ module MHV
       # Filters migrations to only include user's facilities and merges same-date entries
       def filter_and_merge_user_facilities(migrations)
         user_facility_ids = @current_user.va_treatment_facility_ids.map(&:to_s)
-
         # Group by migration date and collect matching facilities
         grouped = migrations.each_with_object({}) do |migration, acc|
           matching_facilities = migration[:facilities].select do |facility|
@@ -183,6 +182,10 @@ module MHV
       def determine_current_phase(migration_date)
         today = Time.zone.today
         days_until_migration = (migration_date - today).to_i
+
+        # After the last phase is complete, return nil
+        last_phase_offset = PHASES.values.max
+        return nil if days_until_migration < -last_phase_offset
 
         # Find the current phase by checking from latest phase to earliest
         # Phase boundaries are inclusive - if today is day -45, we're in p1
--- spec/lib/mhv/oh_facilities_helper/service_spec.rb
+++ spec/lib/mhv/oh_facilities_helper/service_spec.rb
@@ -478,11 +478,35 @@ RSpec.describe MHV::OhFacilitiesHelper::Service do
       end
 
       context 'when migration_status is COMPLETE' do
-        it 'current phase is last phase' do
-          # After last phase the current phase will be the last phase (the last phase we passed)
+        it 'current phase is nil' do
+          # After last phase is complete, current phase should be nil
           allow(Time.zone).to receive(:today).and_return(migration_date + last_phase_offset + 3)
           result = service.get_migration_schedules
+          expect(result.first[:phases][:current]).to be_nil
+        end
+      end
+
+      context 'when exactly at the last phase boundary' do
+        it 'current phase is the last phase' do
+          allow(Time.zone).to receive(:today).and_return(migration_date + last_phase_offset)
+          result = service.get_migration_schedules
           expect(result.first[:phases][:current]).to eq(last_phase_key)
+        end
+      end
+
+      context 'when one day after the last phase boundary' do
+        it 'current phase is nil (migration complete)' do
+          allow(Time.zone).to receive(:today).and_return(migration_date + last_phase_offset + 1)
+          result = service.get_migration_schedules
+          expect(result.first[:phases][:current]).to be_nil
+        end
+      end
+
+      context 'when well after the last phase' do
+        it 'current phase is nil' do
+          allow(Time.zone).to receive(:today).and_return(migration_date + last_phase_offset + 30)
+          result = service.get_migration_schedules
+          expect(result.first[:phases][:current]).to be_nil
         end
       end
     end
