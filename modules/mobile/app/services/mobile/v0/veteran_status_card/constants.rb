2ee9dd1ed8d3b07d13c288faf450a3572d0d5d27
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2026-01-27 14:45:51 -0500

    1103 | Created mobile instance of VSC service and constants

---
+++ modules/mobile/app/services/mobile/v0/veteran_status_card/constants.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Mobile
+  module VeteranStatusCard
+    module Constants
+      # This is currently a placeholder so that in the future, when mobile messages are going
+      # to differ from messages for web, they can be defined here.
+      # When this is the case, the protected methods from 'lib/veteran_status_card/service.rb'
+      # can be overridden in the mobile service to call on the constants defined here.
+    end
+  end
+end
---
+++ modules/mobile/app/services/mobile/v0/veteran_status_card/service.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'veteran_status_card/constants'
+
+module Mobile
+  module V0
+    module VeteranStatusCard
+      class Service < ::VeteranStatusCard::Service
+      end
+    end
+  end
+end
