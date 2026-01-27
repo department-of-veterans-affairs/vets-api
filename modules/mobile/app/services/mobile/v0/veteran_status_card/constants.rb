7b4763e6f1e23f58ac408e2f429d8d9e05d3d489
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2026-01-27 16:02:50 -0500

    1103 | Declared mobile instances of constants, ready for future customization

--- modules/mobile/app/services/mobile/v0/veteran_status_card/constants.rb
+++ modules/mobile/app/services/mobile/v0/veteran_status_card/constants.rb
@@ -1,12 +1,62 @@
 # frozen_string_literal: true
 
+require 'veteran_status_card/constants'
+
 module Mobile
-  module VeteranStatusCard
-    module Constants
-      # This is currently a placeholder so that in the future, when mobile messages are going
-      # to differ from messages for web, they can be defined here.
-      # When this is the case, the protected methods from 'lib/veteran_status_card/service.rb'
-      # can be overridden in the mobile service to call on the constants defined here.
+  module V0
+    module VeteranStatusCard
+      module Constants
+        SUPPORT_PHONE = ::VeteranStatusCard::Constants::SUPPORT_PHONE
+        SUPPORT_HOURS = ::VeteranStatusCard::Constants::SUPPORT_HOURS
+
+        WARNING_STATUS = ::VeteranStatusCard::Constants::WARNING_STATUS
+        ERROR_STATUS = ::VeteranStatusCard::Constants::ERROR_STATUS
+
+        STANDARD_ERROR_TITLE = ::VeteranStatusCard::Constants::STANDARD_ERROR_TITLE
+        STANDARD_ERROR_MESSAGE = ::VeteranStatusCard::Constants::STANDARD_ERROR_MESSAGE
+
+        DISHONORABLE_RESPONSE = {
+          title: STANDARD_ERROR_TITLE,
+          message: ::VeteranStatusCard::Constants::DISHONORABLE_MESSAGE,
+          status: WARNING_STATUS
+        }.freeze
+
+        INELIGIBLE_SERVICE_RESPONSE = {
+          title: STANDARD_ERROR_TITLE,
+          message: ::VeteranStatusCard::Constants::INELIGIBLE_SERVICE_MESSAGE,
+          status: WARNING_STATUS
+        }.freeze
+
+        UNKNOWN_SERVICE_RESPONSE = {
+          title: STANDARD_ERROR_TITLE,
+          message: ::VeteranStatusCard::Constants::UNKNOWN_SERVICE_MESSAGE,
+          status: WARNING_STATUS
+        }.freeze
+
+        EDIPI_NO_PNL_RESPONSE = {
+          title: STANDARD_ERROR_TITLE,
+          message: ::VeteranStatusCard::Constants::EDIPI_NO_PNL_MESSAGE,
+          status: WARNING_STATUS
+        }.freeze
+
+        CURRENTLY_SERVING_RESPONSE = {
+          title: STANDARD_ERROR_TITLE,
+          message: STANDARD_ERROR_MESSAGE,
+          status: WARNING_STATUS
+        }.freeze
+
+        ERROR_RESPONSE = {
+          title: STANDARD_ERROR_TITLE,
+          message: STANDARD_ERROR_MESSAGE,
+          status: ERROR_STATUS
+        }.freeze
+
+        SOMETHING_WENT_WRONG_RESPONSE = {
+          title: ::VeteranStatusCard::Constants::SOMETHING_WENT_WRONG_TITLE,
+          message: ::VeteranStatusCard::Constants::SOMETHING_WENT_WRONG_MESSAGE,
+          status: ERROR_STATUS
+        }.freeze
+      end
     end
   end
 end
