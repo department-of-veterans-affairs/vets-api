395a329aefc9cf0d2f6c7cde0cb55a4799d08e14
Andrea Uhr <andreauhr@Andrea-Uhr.local>
2026-01-27 12:31:55 -0500

    add in some better errors for not found, etc

--- modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
+++ modules/my_health/app/controllers/my_health/v1/prescription_documentation_controller.rb
@@ -6,18 +6,16 @@ module MyHealth
       def index
         id = params[:id]
         rx = client.get_rx_details(id)
-        raise StandardError, 'Rx not found' if rx.nil?
-        raise StandardError, 'Missing NDC number' if rx.cmop_ndc_value.nil?
 
-        # Fetch prescription documentation using the NDC number
-        # https://api.krames.com/v3/content/search?ndc=<NDC>
-        #
-        # Drug documentation JSON object from the API
+        raise Common::Exceptions::RecordNotFound, id if rx.nil?
+
+        if rx.cmop_ndc_value.nil?
+          raise Common::Exceptions::UnprocessableEntity.new(
+            detail: 'Prescription is missing required drug information (NDC)'
+          )
+        end
+
         documentation = client.get_rx_documentation(rx.cmop_ndc_value)
-        # Build PrescriptionDocumentation object
-        # https://api.krames.com/v3/content/<ContentType>-<ContentID>
-        #
-        # PrescriptionDocumentation object with HTML content containing drug information
         prescription_documentation = PrescriptionDocumentation.new({ html: documentation[:data] })
         render json: PrescriptionDocumentationSerializer.new(prescription_documentation)
       end
