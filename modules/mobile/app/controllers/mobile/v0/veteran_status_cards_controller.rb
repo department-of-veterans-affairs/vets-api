f67764831394c49a5c35878506dae48c2ed910bf
Kayley Bogemann <kayley.bogemann@oddball.io>
2026-01-28 12:09:08 -0700

    typo

--- modules/mobile/app/controllers/mobile/v0/veteran_status_cards_controller.rb
+++ modules/mobile/app/controllers/mobile/v0/veteran_status_cards_controller.rb
@@ -8,7 +8,7 @@ module Mobile
       rescue ArgumentError => e
         Rails.logger.error("Mobile::VeteranStatusCardsController argument error: #{e.message}",
                            backtrace: e.backtrace)
-        render json: { error: 'An argument error occured' }, status: :unprocessable_entity
+        render json: { error: 'An argument error occurred' }, status: :unprocessable_entity
       rescue => e
         Rails.logger.error("Mobile::VeteranStatusCardsController unexpected error: #{e.message}",
                            backtrace: e.backtrace)
