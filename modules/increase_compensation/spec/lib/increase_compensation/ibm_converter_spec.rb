d5f216d390a2fd9bd84852e35df3d1ab5f86c20e
Tim <20232041+Tchase44@users.noreply.github.com>
2026-01-28 12:33:02 -0500

    * more testing * add call to ibm in submit_claim_job

--- modules/increase_compensation/spec/lib/increase_compensation/ibm_converter_spec.rb
+++ modules/increase_compensation/spec/lib/increase_compensation/ibm_converter_spec.rb
@@ -1,44 +1,43 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'increase_compensation/ibm_converter'
 
 RSpec.describe IncreaseCompensation::IbmConverter do
-  subject { dummy_class.new }
-
-  let(:dummy_class) { Class.new { include IncreaseCompensation::IbmConverter } }
-
   describe '#convert' do
     it 'converts a blank obj into a blank list of keys' do
       key_file = File.read File.join(__dir__, 'ibm_keys_blank.json')
       keys = JSON.parse(key_file)
-      expect(subject.convert({})).to eq(keys)
+      expect(described_class.convert({})).to eq(keys)
     end
 
     it 'converts a Claims parsed_form to the Keys and formats expected by IBM' do
       converted = File.read File.join(__dir__, 'ibm_keys_filled.json')
       parsed_form = File.read File.join(__dir__, 'mock_parsed_form.json')
 
-      expect(subject.convert(JSON.parse(parsed_form))).to eq(converted)
+      expect(described_class.convert(JSON.parse(parsed_form))).to eq(JSON.parse(converted))
     end
   end
 
   describe '#extract_first_char' do
     it 'returns the 1st charatcher of a string' do
-      expect(subject.extract_first_char('Bender')).to eq('B')
+      expect(described_class.extract_first_char('Bender')).to eq('B')
     end
 
-    it 'returns blank stirng if given blank' do
-      expect(subject.extract_first_char('')).to eq('')
+    it 'returns blank stirng if given blank/nil' do
+      expect(described_class.extract_first_char('')).to eq('')
     end
 
     it 'returns blank string if given nil' do
-      expect(subject.extract_first_char(nil)).to eq('')
+      expect(described_class.extract_first_char(nil)).to eq('')
     end
   end
 
   describe '#format_date' do
-    it 'changes date string to MM/DD/YYYY' do
-      expect(subject.format_date('1975-03-15')).to eq('03/15/1975')
+    it 'changes date string from YYYY-MM-DD to MM/DD/YYYY' do
+      expect(described_class.format_date('1975-03-15')).to eq('03/15/1975')
+      expect(described_class.format_date('')).to eq('')
+      expect(described_class.format_date(nil)).to eq('')
     end
   end
 
@@ -49,9 +48,15 @@ RSpec.describe IncreaseCompensation::IbmConverter do
         'middleinitial' => 'Joshua',
         'last' => 'Fry'
       }
-      expect(subject.full_name(vets_name)).to eq('Philip Joshua Fry')
+      expect(described_class.full_name(vets_name)).to eq('Philip Joshua Fry')
       vets_name = { 'first' => 'Philip', 'last' => 'Fry' }
-      expect(subject.full_name(vets_name)).to eq('Philip Fry')
+      expect(described_class.full_name(vets_name)).to eq('Philip Fry')
+    end
+
+    it 'returns a blank string if given nil/blank' do
+      expect(described_class.full_name('')).to eq('')
+      expect(described_class.full_name(nil)).to eq('')
+      expect(described_class.full_name({})).to eq('')
     end
   end
 end
