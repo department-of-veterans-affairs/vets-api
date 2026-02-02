86a5a098955e9e872aa56ad2c0402171ac7100ee
Jerry Sea <Jsea@kindsys.us>
2026-02-02 07:32:08 -0800

    Refactor error handling

--- modules/decision_reviews/lib/decision_reviews/v1/appealable_issues/service.rb
+++ modules/decision_reviews/lib/decision_reviews/v1/appealable_issues/service.rb
@@ -1,41 +1,21 @@
-# lib/decision_reviews/v1/appealable_issues/service.rb
 # frozen_string_literal: true
 
 require 'common/client/base'
-require 'common/client/concerns/monitoring'
-require 'common/client/errors'
-require 'common/exceptions/forbidden'
-require 'common/exceptions/schema_validation_errors'
 require 'decision_reviews/v1/appealable_issues/configuration'
-require 'decision_reviews/v1/service_exception'
+require 'decision_reviews/v1/concerns/error_handling'
 require 'decision_reviews/v1/constants'
-require 'decision_reviews/v1/supplemental_claim_services'
 require 'decision_reviews/v1/logging_utils'
 
 module DecisionReviews
   module V1
     module AppealableIssues
       class Service < Common::Client::Base
+        include DecisionReviews::V1::Concerns::ErrorHandling
         include DecisionReviews::V1::LoggingUtils
-        include Common::Client::Concerns::Monitoring
 
         configuration DecisionReviews::V1::AppealableIssues::Configuration
 
         STATSD_KEY_PREFIX = 'api.decision_reviews.appealable_issues'
-
-        ERROR_MAP = {
-          504 => Common::Exceptions::GatewayTimeout,
-          503 => Common::Exceptions::ServiceUnavailable,
-          502 => Common::Exceptions::BadGateway,
-          500 => Common::Exceptions::ExternalServerInternalServerError,
-          429 => Common::Exceptions::TooManyRequests,
-          422 => Common::Exceptions::UnprocessableEntity,
-          413 => Common::Exceptions::PayloadTooLarge,
-          404 => Common::Exceptions::ResourceNotFound,
-          403 => Common::Exceptions::Forbidden,
-          401 => Common::Exceptions::Unauthorized,
-          400 => Common::Exceptions::BadRequest
-        }.freeze
 
         # API paths
         HIGHER_LEVEL_REVIEWS_PATH = 'appealable-issues/higher-level-reviews'
@@ -142,6 +122,13 @@ module DecisionReviews
 
         private
 
+        ##
+        # Handles API response by validating status and schema
+        #
+        # @param response [Faraday::Response] The API response
+        # @param error_key [String] Key to append to error class for identification
+        # @return [Faraday::Response] The validated response
+        #
         def handle_response(response, error_key = '')
           raise_schema_error_unless_200_status response.status
           validate_against_schema(
@@ -150,92 +137,6 @@ module DecisionReviews
             append_to_error_class: " #{error_key}"
           )
           response
-        end
-
-        ##
-        # Common method used by both monitoring approaches
-        #
-        def with_monitoring_and_error_handling(&)
-          with_monitoring(2, &)
-        rescue => e
-          handle_error(error: e)
-        end
-
-        def save_error_details(error)
-          PersonalInformationLog.create!(
-            error_class: "#{self.class.name}#save_error_details exception #{error.class} (DECISION_REVIEW_V1)",
-            data: { error: Class.new.include(FailedRequestLoggable).exception_hash(error) }
-          )
-        end
-
-        def log_error_details(error:, message: nil)
-          info = {
-            message:,
-            error_class: error.class,
-            error:
-          }
-          ::Rails.logger.info(info)
-        end
-
-        def error_log_params(error)
-          log_params = { is_success: false, response_error: error }
-          log_params[:body] = error.body if error.try(:status) == 422
-          log_params
-        end
-
-        def handle_error(error:, message: nil)
-          save_and_log_error(error:, message:)
-          source_hash = { source: "#{error.class} raised in #{self.class}" }
-
-          raise case error
-                when Faraday::ParsingError
-                  DecisionReviews::V1::ServiceException.new key: 'DR_502', response_values: source_hash
-                when Common::Client::Errors::ClientError
-                  error_status = error.status
-
-                  if ERROR_MAP.key?(error_status)
-                    ERROR_MAP[error_status].new(source_hash.merge(detail: error.body))
-                  elsif error_status == 403
-                    Common::Exceptions::Forbidden.new source_hash
-                  else
-                    DecisionReviews::V1::ServiceException.new(key: "DR_#{error_status}", response_values: source_hash,
-                                                              original_status: error_status, original_body: error.body)
-                  end
-                else
-                  error
-                end
-        end
-
-        def save_and_log_error(error:, message:)
-          save_error_details(error)
-          log_error_details(error:, message:)
-        end
-
-        def validate_against_schema(json:, schema:, append_to_error_class:)
-          errors = JSONSchemer.schema(schema).validate(json).to_a
-          return if errors.empty?
-
-          raise Common::Exceptions::SchemaValidationErrors, remove_pii_from_json_schemer_errors(errors)
-        rescue => e
-          PersonalInformationLog.create!(
-            error_class: "#{self.class.name}#validate_against_schema exception #{e.class}#{append_to_error_class}",
-            data: {
-              json:, schema:, errors:,
-              error: Class.new.include(FailedRequestLoggable).exception_hash(e)
-            }
-          )
-
-          raise
-        end
-
-        def raise_schema_error_unless_200_status(status)
-          return if status == 200
-
-          raise Common::Exceptions::SchemaValidationErrors, ["expecting 200 status received #{status}"]
-        end
-
-        def remove_pii_from_json_schemer_errors(errors)
-          errors.map { |error| error.slice 'data_pointer', 'schema', 'root_schema' }
         end
       end
     end
