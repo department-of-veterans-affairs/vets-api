e3673e8da48b034a7746a354267867ff1be925c9
james-taggart-kind <jtaggart@kindsys.us>
2026-02-02 12:22:17 -0800

    Validation functions
    Updates to two functions that log a validation message at various choke points in the application. Policy and BGS person service.

--- app/controllers/v0/profile/payment_history_controller.rb
+++ app/controllers/v0/profile/payment_history_controller.rb
@@ -19,9 +19,9 @@ module V0
       def validate_user_identifiers
         missing = []
 
-        missing << 'ICN' if current_user&.icn.present?
-        missing << 'SSN' if current_user&.ssn.present?
-        missing << 'participant_id' if current_user&.participant_id.present?
+        missing << 'ICN' if current_user&.icn.blank?
+        missing << 'SSN' if current_user&.ssn.blank?
+        missing << 'participant_id' if current_user&.participant_id.blank?
 
         if missing.any?
           Rails.logger.warn('User missing required identifiers for BGS payment history', {
@@ -83,6 +83,8 @@ module V0
         person = BGS::People::Request.new.find_person_by_participant_id(user: current_user)
         log_after_bgs_people_request
 
+        validate_person_attributes(person)
+
         log_before_bgs_payment_service_request
         payment_history = BGS::PaymentService.new(current_user).payment_history(person)
         log_after_bgs_payment_service_request
@@ -95,6 +97,30 @@ module V0
         end
 
         payment_history
+      end
+
+      def validate_person_attributes(person)
+        if person.nil?
+          Rails.logger.error('BGS::People::Request returned nil person', {
+                               user_uuid: current_user&.uuid
+                             })
+          StatsD.increment('api.payment_history.bgs_person.nil')
+          return
+        end
+
+        missing = []
+        missing << 'status' if person&.status.blank?
+        missing << 'file_number' if person&.file_number.blank?
+        missing << 'participant_id' if person&.participant_id.blank?
+        missing << 'ssn_number' if person&.ssn_number.blank?
+
+        if missing.any?
+          Rails.logger.warn('BGS person missing required attributes', {
+                              user_uuid: current_user&.uuid,
+                              missing_attributes: missing.join(', ')
+                            })
+          StatsD.increment('api.payment_history.bgs_person.missing_attributes')
+        end
       end
     end
   end
