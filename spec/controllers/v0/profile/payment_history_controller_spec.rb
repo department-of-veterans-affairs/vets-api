0bac7dda77c6f4cd8281af3c84ac68f3a744b22f
james-taggart-kind <jtaggart@kindsys.us>
2026-02-03 15:54:54 -0800

    Fixing existing tests
    Existing tests were all failing locally. I opted to add the new feature flags to the existing tests and use dig which fixed the broken tests. Used stubs for flipper so it didn't effect global state.

--- spec/controllers/v0/profile/payment_history_controller_spec.rb
+++ spec/controllers/v0/profile/payment_history_controller_spec.rb
@@ -8,16 +8,24 @@ RSpec.describe V0::Profile::PaymentHistoryController, type: :controller do
   describe '#index' do
     context 'with only regular payments' do
       it 'returns only payments and no return payments' do
+        allow(Flipper).to receive(:enabled?).with(:payment_history_detailed_logging).and_return(false)
+        allow(Flipper).to receive(:enabled?).with(:payment_history_exception_logging).and_return(false)
+        allow(Flipper).to receive(:enabled?).with(:payment_history_validation_logging).and_return(false)
+        allow(Flipper).to receive(:enabled?).with(:payment_history).and_return(true)
+
         VCR.use_cassette('bgs/person_web_service/find_person_by_participant_id') do
           VCR.use_cassette('bgs/payment_history/retrieve_payment_summary_with_bdn') do
             sign_in_as(user)
             get(:index)
 
-            expect(response).to have_http_status(:ok)
+            response_body = JSON.parse(response.body)
+            payments_count = response_body.dig('data', 'attributes', 'payments')&.count
+            return_payments_count = response_body.dig('data', 'attributes', 'return_payments')&.count
+
             expect(response).to have_http_status(:ok)
 
-            expect(JSON.parse(response.body)['data']['attributes']['payments'].count).to eq(47)
-            expect(JSON.parse(response.body)['data']['attributes']['return_payments'].count).to eq(0)
+            expect(payments_count).to eq(47)
+            expect(return_payments_count).to eq(0)
           end
         end
       end
@@ -25,16 +33,24 @@ RSpec.describe V0::Profile::PaymentHistoryController, type: :controller do
 
     context 'with mixed payments and return payments' do
       it 'returns both' do
+        allow(Flipper).to receive(:enabled?).with(:payment_history_detailed_logging).and_return(false)
+        allow(Flipper).to receive(:enabled?).with(:payment_history_exception_logging).and_return(false)
+        allow(Flipper).to receive(:enabled?).with(:payment_history_validation_logging).and_return(false)
+        allow(Flipper).to receive(:enabled?).with(:payment_history).and_return(true)
+
         VCR.use_cassette('bgs/person_web_service/find_person_by_participant_id') do
           VCR.use_cassette('bgs/payment_history/retrieve_payment_summary_with_bdn_returns') do
             sign_in_as(user)
             get(:index)
 
-            expect(response).to have_http_status(:ok)
+            response_body = JSON.parse(response.body)
+            payments_count = response_body.dig('data', 'attributes', 'payments')&.count
+            return_payments_count = response_body.dig('data', 'attributes', 'return_payments')&.count
+
             expect(response).to have_http_status(:ok)
 
-            expect(JSON.parse(response.body)['data']['attributes']['payments'].count).to eq(2)
-            expect(JSON.parse(response.body)['data']['attributes']['return_payments'].count).to eq(2)
+            expect(payments_count).to eq(2)
+            expect(return_payments_count).to eq(2)
           end
         end
       end
@@ -42,20 +58,26 @@ RSpec.describe V0::Profile::PaymentHistoryController, type: :controller do
 
     context 'with mixed payments and flipper disabled' do
       it 'does not return both' do
-        Flipper.disable('payment_history')
+        allow(Flipper).to receive(:enabled?).with(:payment_history_detailed_logging).and_return(false)
+        allow(Flipper).to receive(:enabled?).with(:payment_history_exception_logging).and_return(false)
+        allow(Flipper).to receive(:enabled?).with(:payment_history_validation_logging).and_return(false)
+        allow(Flipper).to receive(:enabled?).with(:payment_history).and_return(false)
+
         VCR.use_cassette('bgs/person_web_service/find_person_by_participant_id') do
           VCR.use_cassette('bgs/payment_history/retrieve_payment_summary_with_bdn_returns') do
             sign_in_as(user)
             get(:index)
 
-            expect(response).to have_http_status(:ok)
+            response_body = JSON.parse(response.body)
+            payments_count = response_body.dig('data', 'attributes', 'payments')&.count
+            return_payments_count = response_body.dig('data', 'attributes', 'return_payments')&.count
+
             expect(response).to have_http_status(:ok)
 
-            expect(JSON.parse(response.body)['data']['attributes']['payments'].count).to eq(0)
-            expect(JSON.parse(response.body)['data']['attributes']['return_payments'].count).to eq(0)
+            expect(payments_count).to eq(0)
+            expect(return_payments_count).to eq(0)
           end
         end
-        Flipper.enable('payment_history')
       end
     end
   end
