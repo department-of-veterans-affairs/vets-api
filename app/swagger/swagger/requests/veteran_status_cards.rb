2db01fb9e99a9c315e561476477612f21808b613
Kayley Bogemann <kayley.bogemann@oddball.io>
2026-02-04 14:48:27 -0700

    Update null format

--- app/swagger/swagger/requests/veteran_status_cards.rb
+++ app/swagger/swagger/requests/veteran_status_cards.rb
@@ -25,51 +25,42 @@ module Swagger
               property :attributes, type: :object do
                 key :required, %i[veteran_status service_summary_code service_history_status]
                 key :description, "Displays data specific to either 'confirmed' or 'not confirmed' veteran status card"
-                property :full_name, type: :string do
-                  key :'x-nullable', true
+                property :full_name, type: %i[string null] do
                   key :description,
                       'The concatenated full name of the veteran, displayed when eligible for a status card'
                   key :example, 'John T Doe Jr'
                 end
-                property :disability_rating, type: :integer do
-                  key :'x-nullable', true
+                property :disability_rating, type: %i[integer null] do
                   key :description, 'The disability rating for the veteran when eligible for a status card'
                   key :example, 50
                 end
-                property :latest_service, type: :object do
+                property :latest_service, type: %i[object null] do
                   key :required, %i[branch begin_date end_date]
-                  key :'x-nullable', true
                   key :description,
                       'The latest branch of service, begin date, and end date when the veteran ' \
                       'is eligible for a status card'
-                  property :branch, type: :string do
-                    key :'x-nullable', true
+                  property :branch, type: %i[string null] do
                     key :description, "The veteran's latest branch of service"
                     key :example, 'Army'
                   end
-                  property :begin_date, type: :string, format: :date do
-                    key :'x-nullable', true
+                  property :begin_date, type: %i[string null], format: :date do
                     key :description, "The start date of the veteran's latest service"
                     key :example, '2025-01-01'
                   end
-                  property :end_date, type: :string, format: :date do
-                    key :'x-nullable', true
+                  property :end_date, type: %i[string null], format: :date do
                     key :description, "The end date of the veteran's latest service"
                     key :example, '2025-12-31'
                   end
                 end
-                property :edipi, type: :string do
-                  key :'x-nullable', true
+                property :edipi, type: %i[string null] do
                   key :description, "The user's EDIPI number when eligible for a veteran status card"
                   key :example, '001001999'
                 end
-                property :header, type: :string do
-                  key :'x-nullable', true
+                property :header, type: %i[string null] do
                   key :description, 'Displays an error/warning title when the veteran is ineligible for a status card'
                   key :example, "You're not eligible for a Veteran Status Card"
                 end
-                property :body, type: :array do
-                  key :'x-nullable', true
+                property :body, type: %i[array null] do
                   key :description,
                       'A list of message components to display when the veteran is ineligible for a status card'
                   items do
@@ -84,20 +75,17 @@ module Swagger
                       key :description, 'The content to be displayed based on the data type'
                       key :example, 'Your record is missing information about your service history or discharge status.'
                     end
-                    property :tty, type: :boolean do
-                      key :'x-nullable', true
+                    property :tty, type: %i[boolean null] do
                       key :description, "An optional parameter when data type is 'phone' to signify TTY"
                       key :example, true
                     end
-                    property :url, type: :string do
-                      key :'x-nullable', true
+                    property :url, type: %i[string null] do
                       key :description, "When data type is 'link', this field contains the hyperlink"
                       key :example, 'https://www.va.gov'
                     end
                   end
                 end
-                property :alert_type, type: :string do
-                  key :'x-nullable', true
+                property :alert_type, type: %i[string null] do
                   key :enum, %w[error warning]
                   key :description, "The type of alert - can either be 'error' or 'warning'"
                   key :example, 'warning'
@@ -108,14 +96,12 @@ module Swagger
                       "'confirmed' or 'not confirmed' depending on if the veteran is eligible for a status card"
                   key :example, 'confirmed'
                 end
-                property :not_confirmed_reason, type: :string do
-                  key :'x-nullable', true
+                property :not_confirmed_reason, type: %i[string null] do
                   key :enum, %w[ERROR MORE_RESEARCH_REQUIRED NOT_TITLE_38 PERSON_NOT_FOUND]
                   key :description, 'Displays the reason the veteran is not eligible for a status card'
                   key :example, 'PERSON_NOT_FOUND'
                 end
-                property :confirmation_status, type: :string do
-                  key :'x-nullable', true
+                property :confirmation_status, type: %i[string null] do
                   key :enum,
                       %w[DISCHONORABLE_SSC_CODE INELIGIBLE_SSC_CODE UNKNOWN_SSC_CODE EDIPI_NO_PNL_SSC_CODE
                          CURRENTLY_SERVING_SSC_CODE ERROR_SSC_CODE UNCAUGHT_SSC_CODE UNKNOWN_REASON]
