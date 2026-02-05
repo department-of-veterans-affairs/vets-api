f48bee7a835753c4e258a93fc41344e1f53f32a6
stevenjcumming <134282106+stevenjcumming@users.noreply.github.com>
2026-02-05 10:21:40 -0500

    revert protected environment

--- config/initializers/console1984_patch.rb
+++ config/initializers/console1984_patch.rb
@@ -3,7 +3,7 @@
 module Console1984
   class << self
     def running_protected_environment?
-      Rails.env.development?
+      %w[development staging].include?(Settings.vsp_environment)
     end
   end
 end
