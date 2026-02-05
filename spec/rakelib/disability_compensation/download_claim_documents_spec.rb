92527819b6f1ef98df8ccf52f2dcdeadbcb938a2
Oren Mittman <nihil2501@gmail.com>
2026-02-05 11:10:36 -0500

    [DBC] fix `download_claim_documents_spec` in ci

--- spec/rakelib/disability_compensation/download_claim_documents_spec.rb
+++ spec/rakelib/disability_compensation/download_claim_documents_spec.rb
@@ -5,14 +5,30 @@ require Rails.root.join('rakelib', 'disability_compensation', 'download_claim_do
 
 module DisabilityCompensation
   module DownloadClaimDocuments
-    class MockFileIO
-      def calls = @calls ||= []
+    module SpecHelpers
+      FAKE_RSA_KEY_PATH = 'spec/support/certificates/lhdd-fake-private.pem'
 
-      OPERATIONS = %i[write binwrite mkdir_p].freeze
-      OPERATIONS.each do |operation|
-        define_method(operation) do |subject, *|
-          subject = subject.to_s
-          calls << { operation:, subject: }
+      def with_lighthouse_token_signing_key
+        VCR.current_cassette&.recording? and
+          return yield
+
+        rsa_key = FAKE_RSA_KEY_PATH
+        with_settings(Settings.lighthouse.benefits_claims.access_token, { rsa_key: }) do
+          with_settings(Settings.lighthouse.auth.ccg, { rsa_key: }) do
+            yield
+          end
+        end
+      end
+
+      class MockFileIO
+        def calls = @calls ||= []
+
+        OPERATIONS = %i[write binwrite mkdir_p].freeze
+        OPERATIONS.each do |operation|
+          define_method(operation) do |subject, *|
+            subject = subject.to_s
+            calls << { operation:, subject: }
+          end
         end
       end
     end
@@ -20,8 +36,10 @@ module DisabilityCompensation
 end
 
 describe DisabilityCompensation::DownloadClaimDocuments do
+  include described_class::SpecHelpers
+
   it 'writes files with network-derived data' do
-    mock_file_io = described_class::MockFileIO.new
+    mock_file_io = described_class::SpecHelpers::MockFileIO.new
     allow(described_class).to(receive(:file_io).and_return(mock_file_io))
 
     vcr_name = 'disability_compensation/download_claim_documents'
@@ -31,10 +49,12 @@ describe DisabilityCompensation::DownloadClaimDocuments do
     }.freeze
 
     VCR.use_cassette(vcr_name, vcr_options) do
-      described_class.perform(
-        claim_id: '600878948',
-        icn: '1012667122V019349'
-      )
+      with_lighthouse_token_signing_key do
+        described_class.perform(
+          claim_id: '600878948',
+          icn: '1012667122V019349'
+        )
+      end
     end
 
     directory = 'tmp/disability_compensation/download_claim_documents/600878948'
