46d8a5677f7ec7d9fb0745e78020879545076160
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2026-02-09 10:48:38 -0500

    linter error

--- lib/search/pii_redactor.rb
+++ lib/search/pii_redactor.rb
@@ -8,8 +8,12 @@ module Search
     SSN_PATTERN = /\b(?!000|666|9\d{2})\d{3}-?(?!00)\d{2}-?(?!0{4})\d{4}\b/
     PHONE_PATTERN = /(\+?\d{1,2}\s?)?\(?\b\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b/
     ZIP_CODE_PATTERN = /\b\d{5}(?:-\d{4})?\b/
-    STREET_ADDRESS_PATTERN =
-      /\b\d+\s+[A-Z0-9\s.#]+(?:Street|St\.?|Avenue|Ave\.?|Road|Rd\.?|Boulevard|Blvd\.?|Drive|Dr\.?|Lane|Ln\.?|Court|Ct\.?|Place|Pl\.?|Way|Circle|Cir\.?|Highway|Hwy\.?)\b/i
+    STREET_ADDRESS_PATTERN = /
+      \b\d+\s+
+      [A-Z0-9\s.#]+
+      (?:Street|St\.?|Avenue|Ave\.?|Road|Rd\.?|Boulevard|Blvd\.?|Drive|Dr\.?|Lane|Ln\.?|Court|Ct\.?|Place|Pl\.?|Way|Circle|Cir\.?|Highway|Hwy\.?)
+      \b
+    /ix
 
     module_function
 
