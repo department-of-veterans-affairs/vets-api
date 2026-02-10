1599ad21ad9b2bc33a954d7f2e49f42cb22b432b
Daniel Gading <2748839+dgading@users.noreply.github.com>
2026-02-10 11:57:56 -0500

    Add prefill to 21P-534ez

---
+++ app/models/form_profiles/va_21p534ez.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class FormProfiles::VA21p534ez < FormProfile
+  def metadata
+    {
+      version: 0,
+      prefill: true,
+      returnUrl: '/veteran'
+    }
+  end
+end
