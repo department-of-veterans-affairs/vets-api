418abe24268d063f8c0d97ebccba118d07c813a2
Adam Stoler <89949977+6StringCode@users.noreply.github.com>
2026-02-13 11:32:12 -0500

    Fix duplicate describe block in v0 messages_spec.rb
    Merged two 'schema contract validation' describe blocks into one to fix RSpec/RepeatedExampleGroupDescription Rubocop error.

--- modules/mobile/spec/requests/mobile/v0/messaging/health/messages_spec.rb
+++ modules/mobile/spec/requests/mobile/v0/messaging/health/messages_spec.rb
@@ -526,6 +526,46 @@ RSpec.describe 'Mobile::V0::Messaging::Health::Messages', type: :request do
             end
           end
         end
+
+        describe 'message show endpoint' do
+          context 'when :schema_contract_message_show is enabled' do
+            before do
+              allow(Flipper).to receive(:enabled?).with('schema_contract_message_show').and_return(true)
+            end
+
+            it 'validates schema for get_message' do
+              VCR.use_cassette('mobile/messages/gets_a_message_with_id_and_attachment') do
+                VCR.use_cassette('sm_client/triage_teams/gets_a_collection_of_all_triage_team_recipients') do
+                  get "/mobile/v0/messaging/health/messages/#{message_id}", headers: sis_headers
+                end
+              end
+              expect(response).to be_successful
+              SchemaContract::ValidationJob.drain
+              validation = SchemaContract::Validation.find_by(contract_name: 'message_show')
+              expect(validation).to be_present
+              expect(validation.status).to eq('success')
+            end
+          end
+
+          context 'when :schema_contract_triage_teams is enabled' do
+            before do
+              allow(Flipper).to receive(:enabled?).with('schema_contract_triage_teams').and_return(true)
+            end
+
+            it 'validates schema for get_all_triage_teams' do
+              VCR.use_cassette('mobile/messages/gets_a_message_with_id_and_attachment') do
+                VCR.use_cassette('sm_client/triage_teams/gets_a_collection_of_all_triage_team_recipients') do
+                  get "/mobile/v0/messaging/health/messages/#{message_id}", headers: sis_headers
+                end
+              end
+              expect(response).to be_successful
+              SchemaContract::ValidationJob.drain
+              validation = SchemaContract::Validation.find_by(contract_name: 'triage_teams')
+              expect(validation).to be_present
+              expect(validation.status).to eq('success')
+            end
+          end
+        end
       end
 
       describe '#thread' do
@@ -579,53 +619,6 @@ RSpec.describe 'Mobile::V0::Messaging::Health::Messages', type: :request do
                                               'status' => '404' }] }
             expect(response).to have_http_status(:not_found)
             expect(response.parsed_body).to eq(expected_error)
-          end
-        end
-      end
-
-      describe 'schema contract validation' do
-        let(:user_account) { create(:user_account) }
-
-        before do
-          user.user_account_uuid = user_account.id
-          user.save!
-        end
-
-        context 'when :schema_contract_message_show is enabled' do
-          before do
-            allow(Flipper).to receive(:enabled?).with('schema_contract_message_show').and_return(true)
-          end
-
-          it 'validates schema for get_message' do
-            VCR.use_cassette('mobile/messages/gets_a_message_with_id_and_attachment') do
-              VCR.use_cassette('sm_client/triage_teams/gets_a_collection_of_all_triage_team_recipients') do
-                get "/mobile/v0/messaging/health/messages/#{message_id}", headers: sis_headers
-              end
-            end
-            expect(response).to be_successful
-            SchemaContract::ValidationJob.drain
-            validation = SchemaContract::Validation.find_by(contract_name: 'message_show')
-            expect(validation).to be_present
-            expect(validation.status).to eq('success')
-          end
-        end
-
-        context 'when :schema_contract_triage_teams is enabled' do
-          before do
-            allow(Flipper).to receive(:enabled?).with('schema_contract_triage_teams').and_return(true)
-          end
-
-          it 'validates schema for get_all_triage_teams' do
-            VCR.use_cassette('mobile/messages/gets_a_message_with_id_and_attachment') do
-              VCR.use_cassette('sm_client/triage_teams/gets_a_collection_of_all_triage_team_recipients') do
-                get "/mobile/v0/messaging/health/messages/#{message_id}", headers: sis_headers
-              end
-            end
-            expect(response).to be_successful
-            SchemaContract::ValidationJob.drain
-            validation = SchemaContract::Validation.find_by(contract_name: 'triage_teams')
-            expect(validation).to be_present
-            expect(validation.status).to eq('success')
           end
         end
       end
