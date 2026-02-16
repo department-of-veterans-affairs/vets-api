5aa3f9f06603d7a6f92241f5520d643387d9f872
Jerry Sea <jsea@kindsys.us>
2026-02-16 08:13:05 -0800

    Migrate appealable issues (#26444)
    * WIP adding new appealable issues service for decision reviews
    
    * WIP
    
    * Update controllers and service methods
    
    * Add request specs for new service
    
    * Add comment for fake bearer token
    
    * Add service specs and settings config
    
    * Fix linting issues
    
    * Add updated gemfile.lock file with new schema
    
    * update RSA file path reading
    
    * update rsa_key
    
    * Add keys to other env settings
    
    * Add flipper key
    
    * Refactor error handling
    
    * Update gemfile.lock
    
    ---------
    
    Co-authored-by: Grace Xu <grace@kind.systems>
    Co-authored-by: Craig Donavin <44502373+Crankums@users.noreply.github.com>

--- modules/decision_reviews/app/controllers/decision_reviews/v1/notice_of_disagreements/contestable_issues_controller.rb
+++ modules/decision_reviews/app/controllers/decision_reviews/v1/notice_of_disagreements/contestable_issues_controller.rb
@@ -7,14 +7,24 @@ module DecisionReviews
         service_tag 'board-appeal'
 
         def index
-          render json: decision_review_service
-            .get_notice_of_disagreement_contestable_issues(user: current_user)
-            .body
+          render json: get_appealable_issues.body
         rescue => e
           log_exception_to_personal_information_log e,
                                                     error_class:
                                                     "#{self.class.name}#index exception #{e.class} (NOD_V1)"
           raise
+        end
+
+        private
+
+        def get_appealable_issues
+          if use_new_appealable_issues_service?
+            appealable_issues_service
+              .get_notice_of_disagreement_issues(user: current_user, benefit_type: params[:benefit_type])
+          else
+            decision_review_service
+              .get_notice_of_disagreement_contestable_issues(user: current_user)
+          end
         end
       end
     end
