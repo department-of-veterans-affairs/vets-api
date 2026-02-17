731fd12f97979373642448fe32e3b4ae93964ab7
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2026-02-17 14:17:46 -0800

    revert change since tests don't line up with real data

--- modules/mobile/app/models/mobile/v0/adapters/vaos_v2_appointment.rb
+++ modules/mobile/app/models/mobile/v0/adapters/vaos_v2_appointment.rb
@@ -85,7 +85,7 @@ module Mobile
             vetext_id:,
             reason: appointment[:reason_for_appointment],
             is_covid_vaccine: appointment[:service_type] == COVID_SERVICE,
-            is_pending: appointment[:pending],
+            is_pending: appointment_request?,
             proposed_times:,
             type_of_care: appointment[:type_of_care],
             patient_phone_number:,
