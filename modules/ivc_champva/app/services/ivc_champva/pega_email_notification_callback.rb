bdb04edf3c2d3206865adbb03491b5b9a74a06ca
Steve Long <2211897+stevelong00@users.noreply.github.com>
2026-02-17 16:38:55 -0500

    First pass at implementing callbacks

--- modules/ivc_champva/app/services/ivc_champva/pega_email_notification_callback.rb
+++ modules/ivc_champva/app/services/ivc_champva/pega_email_notification_callback.rb
@@ -1,31 +1,27 @@
 # frozen_string_literal: true
 
 module IvcChampva
-  # General purpose callback class used for when we send emails to users.
-  # This is used so we can maintain email numbers in DD.
+  # Callback class used for when we notify Pega about a form missing a Pega status
   #
   # Modified from https://github.com/department-of-veterans-affairs/vets-api/tree/master/modules/va_notify#how-teams-can-integrate-with-callbacks
   #
 
-  class EmailNotificationCallback
+  class PegaEmailNotificationCallback
     def self.call(notification)
-      # @param ac [hash] contains properties form_id, form_uuid, and confirmation_type
+      # @param ac [hash] contains properties form_id and form_uuid
       # as defined under callback_metadata.additional_context when the email was sent
       #   (e.g.: {form_id: '10-10d', form_uuid: '12345678-1234-5678-1234-567812345678',
-      #   notification_type: 'confirmation'})
+      #   notification_type: 'pega_alert'})
       ac = notification.callback_metadata['additional_context']
-
-      # This is the actual contribution we care about:
-      monitor.track_email_sent(ac['form_id'], ac['form_uuid'], notification.status, ac['notification_type'])
-
       case notification.status
       when 'delivered'
         # success
         StatsD.increment('api.vanotify.notifications.delivered')
+        monitor.track_pega_alert_email_sent(ac['form_id']) # e.g., '10-10d'
       when 'permanent-failure'
         # delivery failed
-        # possibly log error or increment metric and use the optional metadata - notification.callback_metadata
         StatsD.increment('api.vanotify.notifications.permanent_failure')
+        monitor.track_pega_alert_email_failed(ac['form_id'], notification.status, notification.status_reason)
         Rails.logger.error(notification_id: notification.notification_id, source: notification.source_location,
                            status: notification.status, status_reason: notification.status_reason)
       when 'temporary-failure'
