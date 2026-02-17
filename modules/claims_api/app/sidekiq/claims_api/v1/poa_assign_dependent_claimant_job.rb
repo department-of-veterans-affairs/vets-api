a509770efb6fbb49795d11db76a7af2df404c878
jeremyhunton-va <99915461+jeremyhunton-va@users.noreply.github.com>
2026-02-17 14:11:49 +0000

    created placeholder job files for stuck 526 and poa assignment jobs.

---
+++ modules/claims_api/app/sidekiq/claims_api/v1/poa_assign_dependent_claimant_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module ClaimsApi
+  module V1
+    class PoaAssignDependentClaimantJob < ClaimsApi::ServiceBase
+
+      def perform(claim_id)
+
+      end
+
+    end
+  end
+end
