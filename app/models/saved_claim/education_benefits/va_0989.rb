f5f1900f984e5056ae44c17c61dd481dad5f139d
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2026-02-19 15:37:06 -0800

    benefits intake job and monitor for all education benefits form types

--- app/models/saved_claim/education_benefits/va_0989.rb
+++ app/models/saved_claim/education_benefits/va_0989.rb
@@ -10,4 +10,16 @@ class SavedClaim::EducationBenefits::VA0989 < SavedClaim::EducationBenefits
   def retention_period
     60.days
   end
+
+  def generate_benefits_intake_metadata
+    ::BenefitsIntake::Metadata.generate(
+      parsed_form['applicantName']['first'],
+      parsed_form['applicantName']['last'],
+      parsed_form['vaFileNumber'] || parsed_form['ssn'],
+      parsed_form['mailingAddress']['postalCode'],
+      self.class.to_s,
+      '22-0989', # doc type
+      'EDU' # busines line
+    )
+  end
 end
