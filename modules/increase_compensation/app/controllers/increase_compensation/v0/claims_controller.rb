6a60d1c67759ca95463e88c5fc77c5fe29a3300c
Tim <20232041+Tchase44@users.noreply.github.com>
2026-02-25 10:43:39 -0500

    [BIO]21-8940 Handle `nil` in progress form

--- modules/increase_compensation/app/controllers/increase_compensation/v0/claims_controller.rb
+++ modules/increase_compensation/app/controllers/increase_compensation/v0/claims_controller.rb
@@ -94,6 +94,8 @@ module IncreaseCompensation
       # @param claim
       # @raise [Exception]
       def process_attachments(in_progress_form, claim)
+        return if in_progress_form.nil?
+
         @docs = JSON.parse(in_progress_form.form_data)['supporting_documents']
         claim.process_attachments!(@docs)
       rescue => e
