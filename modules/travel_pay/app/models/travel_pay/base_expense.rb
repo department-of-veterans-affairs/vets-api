44c874c8ab745ff5ba50b3a3c7520985035af2b4
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2026-02-26 07:10:20 -0800

    Remove base expense conversion implementation

--- modules/travel_pay/app/models/travel_pay/base_expense.rb
+++ modules/travel_pay/app/models/travel_pay/base_expense.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'mini_magick'
 require 'base64'
 
 module TravelPay
@@ -14,26 +13,12 @@ module TravelPay
     attribute :cost_requested, :float
     attribute :claim_id, :string
 
-    # Receipt attribute accessor with custom setter for HEIC conversion
-    attr_reader :receipt
+    # Receipt attribute accessor
+    attr_accessor :receipt
 
     validates :purchase_date, presence: true, unless: -> { is_a?(MileageExpense) }
     validates :description, length: { maximum: 2000 }, allow_nil: true, unless: -> { is_a?(MileageExpense) }
     validates :cost_requested, presence: true, numericality: { greater_than: 0 }, unless: -> { is_a?(MileageExpense) }
-
-    # Custom setter for receipt that automatically converts HEIC images to JPG
-    #
-    # @param receipt_data [Hash, nil] the receipt hash containing file_data, content_type, etc.
-    def receipt=(receipt_data)
-      @receipt = if receipt_data.nil? || receipt_data.blank?
-                   nil
-                 elsif Flipper.enabled?(:travel_pay_enable_heic_conversion)
-                   process_receipt_with_heic_conversion(receipt_data)
-                 else
-                   # When disabled: exact same behavior as master
-                   receipt_data
-                 end
-    end
 
     # Returns the list of permitted parameters for this expense type
     # Subclasses can override completely or extend with super + [...]
@@ -122,68 +107,6 @@ module TravelPay
     end
 
     private
-
-    # Processes receipt data with HEIC conversion support
-    #
-    # @param receipt_data [Hash] the receipt hash to process
-    # @return [Hash] processed receipt hash with potential HEIC to JPG conversion
-    def process_receipt_with_heic_conversion(receipt_data)
-      receipt_hash = receipt_data.with_indifferent_access
-
-      if heic_image?(receipt_hash[:content_type])
-        convert_heic_to_jpg(receipt_hash)
-      else
-        receipt_data
-      end
-    rescue => e
-      error_message = "HEIC conversion failed: #{e.message}"
-      Rails.logger.error(error_message)
-      raise Common::Exceptions::UnprocessableEntity.new(detail: error_message)
-    end
-
-    # Checks if the content type is HEIC/HEIF format
-    #
-    # @param content_type [String, nil] the content type to check
-    # @return [Boolean] true if content type is HEIC/HEIF
-    def heic_image?(content_type)
-      content_type.to_s.match?(%r{^image/(heic|heif)$}i)
-    end
-
-    # Converts a HEIC image receipt to JPG format
-    #
-    # @param receipt_hash [Hash] the receipt hash with base64-encoded HEIC data
-    # @return [Hash] updated receipt hash with JPG data
-    def convert_heic_to_jpg(receipt_hash)
-      Rails.logger.info('Converting HEIC receipt to JPG')
-
-      file_data = receipt_hash[:file_data]
-      return receipt_hash if file_data.blank?
-
-      jpg_binary = convert_image_to_jpg(Base64.strict_decode64(file_data))
-
-      # Create updated hash without mutating the argument
-      receipt_hash.merge(
-        file_data: Base64.strict_encode64(jpg_binary),
-        content_type: 'image/jpeg',
-        length: jpg_binary.bytesize.to_s,
-        file_name: receipt_hash[:file_name]&.sub(/\.hei[cf]$/i, '.jpg')
-      ).tap do |_updated|
-        Rails.logger.info("Successfully converted HEIC to JPG (size: #{jpg_binary.bytesize} bytes)")
-      end
-    end
-
-    # Converts binary image data to JPG format using MiniMagick
-    #
-    # @param binary_data [String] binary image data
-    # @return [String] JPG binary data
-    def convert_image_to_jpg(binary_data)
-      image = MiniMagick::Image.read(binary_data)
-      image.format('jpg')
-
-      File.binread(image.path)
-    ensure
-      image&.destroy! if defined?(image) && image
-    end
 
     # Finds a claim by ID - this will need to be implemented based on
     # which claim model is being used in the travel pay system
