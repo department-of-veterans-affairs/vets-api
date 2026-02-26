48dc54f5ae638b840b8bf8f64d84c923b1264aeb
Gregory Hedrick <gregory@pixiitech.net>
2026-02-26 16:32:23 -0500

    Fixes per code review

--- modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/claimant_controller.rb
+++ modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/claimant_controller.rb
@@ -7,10 +7,9 @@ module AccreditedRepresentativePortal
 
       before_action :validate_benefit_type!, only: :show
       before_action :ensure_claimant_details_enabled!, only: :show
+      before_action { authorize nil, policy_class: ClaimantPolicy }
 
       def search # rubocop:disable Metrics/MethodLength
-        authorize nil, policy_class: ClaimantPolicy
-
         claimant_profile =
           MPI::Service.new.find_profile_by_attributes(
             first_name: params[:first_name],
@@ -22,12 +21,7 @@ module AccreditedRepresentativePortal
         claimant_profile.present? or
           raise Common::Exceptions::RecordNotFound, 'Claimant not found'
 
-        claimant_representative =
-          ClaimantRepresentative.find(
-            claimant_icn: claimant_profile.icn,
-            power_of_attorney_holder_memberships:
-              current_user.power_of_attorney_holder_memberships
-          )
+        @icn = claimant_profile.icn
 
         power_of_attorney_requests =
           policy_scope(PowerOfAttorneyRequest).joins(:claimant).not_withdrawn.where(
@@ -53,22 +47,12 @@ module AccreditedRepresentativePortal
       end
 
       def show
-        raise ActionController::BadRequest unless Flipper.enabled?(
-          :accredited_representative_portal_claimant_details
-        )
-
-        skip_authorization
-        # Using a ClaimantPolicy instance directly here instead of `authorize`
-        # so that the ClaimantRepresentative can be re-used and only one call
-        # made to the Lighthouse power-of-attorney endpoint for PoA check and
-        # returning the organization name to the user.
-        icn = IcnTemporaryIdentifier.lookup_icn(params[:id])
-        policy = ClaimantPolicy.new(current_user, icn)
-        policy.show? or raise Pundit::NotAuthorizedError
+        @icn = IcnTemporaryIdentifier.lookup_icn(params[:id])
+        claimant_representative.present? or raise Pundit::NotAuthorizedError
 
         payload = AccreditedRepresentativePortal::ClaimantDetailsService.new(
-          icn:,
-          representative_name: policy.claimant_representative.power_of_attorney_holder.name,
+          icn: @icn,
+          representative_name: claimant_representative.power_of_attorney_holder.name,
           benefit_type_param: params[:benefitType]
         ).call
 
@@ -93,6 +77,16 @@ module AccreditedRepresentativePortal
         raise Common::Exceptions::UnprocessableEntity.new(
           detail: "benefitType must be one of: #{BENEFIT_TYPES.join(', ')}"
         )
+      end
+
+      def claimant_representative
+        @claimant_representative ||= ClaimantRepresentative.find(
+          claimant_icn: @icn,
+          power_of_attorney_holder_memberships:
+            current_user.power_of_attorney_holder_memberships
+        )
+      rescue ActiveRecord::RecordNotFound, ClaimantRepresentative::Finder::Error
+        nil
       end
     end
   end
