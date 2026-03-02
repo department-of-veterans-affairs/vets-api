5e9fe769177294a52c967b2c79ff2e1d85f1fce1
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2026-03-02 09:15:48 -0800

    add 10278 form to edu benefits intake upload job

--- app/models/saved_claim/education_benefits/va_10278.rb
+++ app/models/saved_claim/education_benefits/va_10278.rb
@@ -2,4 +2,35 @@
 
 class SavedClaim::EducationBenefits::VA10278 < SavedClaim::EducationBenefits
   add_form_and_validation('22-10278')
+
+  def generate_benefits_intake_metadata
+    personal_info = parsed_form['claimantPersonalInformation']
+    ::BenefitsIntake::Metadata.generate(
+      personal_info['fullName']['first'],
+      personal_info['fullName']['last'],
+      personal_info['vaFileNumber'] || personal_info['ssn'],
+      parsed_form['claimantAddress']['postalCode'],
+      self.class.to_s,
+      '22-10278', # doc type
+      'EDU' # busines line
+    )
+  end
+
+  def send_email(email_type)
+    EducationBenefitsClaims::NotificationEmail.new(id).deliver(email_type)
+  end
+
+  # the personalization params to send with VANotify
+  def personalisation
+    personal_info = parsed_form['claimantPersonalInformation']
+    {
+      first_name: personal_info['fullName']['first'],
+      last_name: personal_info['fullName']['last']
+    }
+  end
+
+  # the email address to send VANotify success/failure emails to
+  def email
+    parsed_form['claimantContactInformation']['emailAddress']
+  end
 end
