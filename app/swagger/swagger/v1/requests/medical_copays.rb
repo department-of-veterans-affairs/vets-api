1589e1c7795c4a15509002365b70c9288b6a4ef1
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2026-03-03 14:28:13 -0500

    Swagger update

--- app/swagger/swagger/v1/requests/medical_copays.rb
+++ app/swagger/swagger/v1/requests/medical_copays.rb
@@ -5,10 +5,167 @@ class Swagger::V1::Requests::MedicalCopays
 
   swagger_path '/v1/medical_copays' do
     operation :get do
-      key :oneOf, [
-        { '$ref' => '#/definitions/LighthouseMedicalCopayListResponse' },
-        { '$ref' => '#/definitions/VbsMedicalCopayListResponse' }
-      ]
+      key :description, 'List of user medical copay statements (HCCC-backed)'
+      key :operationId, 'getMedicalCopays'
+      key :tags, %w[medical_copays]
+
+      parameter :authorization
+
+      response 200 do
+        key :description, 'Successful copays lookup'
+
+        schema do
+          property :isCerner, type: :boolean, example: false
+          property :status, type: :integer, example: 200
+          # Lighthouse response (conditionally returned based on Cerner location(s))
+          property :data, type: :array do
+            items do
+              property :id, type: :string, example: '675-K3FD983'
+              property :type, type: :string, example: 'medical_copays'
+
+              property :attributes, type: :object do
+                property :url,
+                         type: :string,
+                         example: 'https://api.va.gov/v1/medical_copays/675-K3FD983'
+
+                property :facility,
+                         type: :string,
+                         example: 'TEST VAMC'
+
+                property :city,
+                         type: :string,
+                         example: 'Tampa'
+
+                property :facilityId,
+                         type: :string,
+                         example: '1234'
+
+                property :externalId,
+                         type: :string,
+                         example: '675-K3FD983'
+
+                property :latestBillingRef,
+                         type: :string,
+                         example: '4-6c9ZE23XQjkA9CC'
+
+                property :currentBalance,
+                         type: :number,
+                         format: :float,
+                         example: 284.59
+
+                property :previousBalance,
+                         type: :number,
+                         format: :float,
+                         example: 76.19
+
+                property :previousUnpaidBalance,
+                         type: :number,
+                         format: :float,
+                         example: 0.0
+
+                property :lastUpdatedAt,
+                         type: :string,
+                         format: :'date-time',
+                         example: '2012-11-01T04:00:00.000+00:00'
+              end
+
+              # VBS response (conditionally returned based on Cerner location(s))
+              property :accountNumber, type: :string
+              property :pSSeqNum, type: :integer, example: 0
+              property :pSTotSeqNum, type: :integer, example: 0
+              property :pSFacilityNum, type: :string
+              property :pSFacPhoneNum, type: :string
+              property :pSTotStatement, type: :integer, example: 0
+              property :pSStatementVal, type: :string
+              property :pSStatementDate, type: :string
+              property :pSStatementDateOutput, type: :string
+              property :pSProcessDate, type: :string
+              property :pSProcessDateOutput, type: :string
+              property :pHPatientLstNme, type: :string
+              property :pHPatientFstNme, type: :string
+              property :pHPatientMidNme, type: :string
+              property :pHAddress1, type: :string
+              property :pHAddress2, type: :string
+              property :pHAddress3, type: :string
+              property :pHCity, type: :string
+              property :pHState, type: :string
+              property :pHZipCde, type: :string
+              property :pHZipCdeOutput, type: :string
+              property :pHCtryNme, type: :string
+              property :pHAmtDue, type: :integer, example: 0
+              property :pHAmtDueOutput, type: :string
+              property :pHPrevBal, type: :integer, example: 0
+              property :pHPrevBalOutput, type: :string
+              property :pHTotCharges, type: :integer, example: 0
+              property :pHTotChargesOutput, type: :string
+              property :pHTotCredits, type: :integer, example: 0
+              property :pHTotCreditsOutput, type: :string
+              property :pHNewBalance, type: :integer, example: 0
+              property :pHNewBalanceOutput, type: :string
+              property :pHSpecialNotes, type: :string
+              property :pHroParaCdes, type: :string
+              property :pHNumOfLines, type: :integer, example: 0
+              property :pHDfnNumber, type: :integer, example: 0
+              property :pHCernerStatementNumber, type: :integer, example: 0
+              property :pHCernerPatientId, type: :string
+              property :pHCernerAccountNumber, type: :string
+              property :pHIcnNumber, type: :string
+              property :pHAccountNumber, type: :integer, example: 0
+              property :pHLargeFontIndcator, type: :integer, example: 0
+              property :details, type: :array do
+                items do
+                  property :pDDatePosted, type: :string
+                  property :pDDatePostedOutput, type: :string
+                  property :pDTransDesc, type: :string
+                  property :pDTransDescOutput, type: :string
+                  property :pDTransAmt, type: :integer, example: 0
+                  property :pDTransAmtOutput, type: :string
+                  property :pDRefNo, type: :string
+                end
+              end
+              property :station, type: :object do
+                property :facilitYNum, type: :string
+                property :visNNum, type: :string
+                property :facilitYDesc, type: :string
+                property :cyclENum, type: :string
+                property :remiTToFlag, type: :string
+                property :maiLInsertFlag, type: :string
+                property :staTAddress1, type: :string
+                property :staTAddress2, type: :string
+                property :staTAddress3, type: :string
+                property :city, type: :string
+                property :state, type: :string
+                property :ziPCde, type: :string
+                property :ziPCdeOutput, type: :string
+                property :baRCde, type: :string
+                property :teLNumFlag, type: :string
+                property :teLNum, type: :string
+                property :teLNum2, type: :string
+                property :contacTInfo, type: :string
+                property :dM2TelNum, type: :string
+                property :contacTInfo2, type: :string
+                property :toPTelNum, type: :string
+                property :lbXFedexAddress1, type: :string
+                property :lbXFedexAddress2, type: :string
+                property :lbXFedexAddress3, type: :string
+                property :lbXFedexCity, type: :string
+                property :lbXFedexState, type: :string
+                property :lbXFedexZipCde, type: :string
+                property :lbXFedexBarCde, type: :string
+                property :lbXFedexContact, type: :string
+                property :lbXFedexContactTelNum, type: :string
+              end
+            end
+          end
+
+          # Only in Lighthouse response (conditionally returned based on Cerner location(s))c
+          property :meta, type: :object do
+            property :total, type: :integer, example: 50
+            property :page, type: :integer, example: 1
+            property :per_page, type: :integer, example: 10
+          end
+        end
+      end
     end
   end
 
