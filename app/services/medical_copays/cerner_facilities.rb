f02e785dc5aa509db4216e1d2e2589624c2d864d
Josh Mills <josh.troy.mills@gmail.com>
2026-03-03 10:46:22 -0500

    add check for future cerner sites

---
+++ app/services/medical_copays/cerner_facilities.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module MedicalCopays
+  module CernerFacilities
+    FUTURE_CERNER_FACILITY_IDS = %w[
+      553
+      655
+      506
+      515
+      552
+      538
+      539
+      583
+      610
+      541
+      463
+      676
+      607
+      585
+      695
+      550
+      537
+      578
+      656
+      438
+      437
+      618
+      636
+      568
+    ].freeze
+
+    def self.cerner_copay_user?(user)
+      return true if user.cerner_facility_ids&.any?
+
+      user_facility_ids = user.vha_facility_ids
+      user_facility_ids.any? { |id| FUTURE_CERNER_FACILITY_IDS.include?(id) }
+    end
+  end
+end
---
+++ spec/services/medical_copays/cerner_facilities_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MedicalCopays::CernerFacilities do
+  describe '.cerner_copay_user?' do
+    let(:user) { build(:user, :loa3) }
+
+    context 'when the user has existing cerner facility IDs from MPI' do
+      before do
+        allow(user).to receive_messages(cerner_facility_ids: %w[757], vha_facility_ids: %w[999])
+      end
+
+      it 'returns true' do
+        expect(described_class.cerner_copay_user?(user)).to be true
+      end
+    end
+
+    context 'when the user has a future cerner facility' do
+      before do
+        allow(user).to receive_messages(cerner_facility_ids: [], vha_facility_ids: %w[553])
+      end
+
+      it 'returns true' do
+        expect(described_class.cerner_copay_user?(user)).to be true
+      end
+    end
+
+    context 'when the user has no cerner or future cerner facilities' do
+      before do
+        allow(user).to receive_messages(cerner_facility_ids: [], vha_facility_ids: %w[999])
+      end
+
+      it 'returns false' do
+        expect(described_class.cerner_copay_user?(user)).to be false
+      end
+    end
+
+    context 'when the user has nil cerner facility IDs' do
+      before do
+        allow(user).to receive_messages(cerner_facility_ids: nil, vha_facility_ids: %w[999])
+      end
+
+      it 'returns false' do
+        expect(described_class.cerner_copay_user?(user)).to be false
+      end
+    end
+  end
+end
