7286115413e8957877c0d2de81bb8deb7ba556a4
broskj <170366505+brostk@users.noreply.github.com>
2026-03-04 13:31:58 -0500

    update client code for ves ohi

--- modules/ivc_champva/lib/ves_api/client.rb
+++ modules/ivc_champva/lib/ves_api/client.rb
@@ -18,7 +18,13 @@ module IvcChampva
         Settings.ivc_champva_ves_api
       end
 
-      delegate :api_key, to: :settings
+      ##
+      # Returns the OHI API key, falling back to the standard api_key if not configured
+      #
+      # @return [String] the API key for OHI requests
+      def ohi_api_key
+        settings.ohi_api_key.presence || settings.api_key
+      end
 
       ##
       # HTTP POST call to the VES VFMP CHAMPVA Application service to submit a 10-10d application.
@@ -28,7 +34,7 @@ module IvcChampva
       # @param ves_request_data [IvcChampva::VesRequest] preformatted request data
       def submit_1010d(transaction_uuid, acting_user, ves_request_data)
         resp = connection.post("#{config.base_path}/ves-vfmp-app-svc/champva-applications") do |req|
-          req.headers = headers(transaction_uuid, acting_user)
+          req.headers = headers(transaction_uuid, acting_user, settings.api_key)
           req.body = ves_request_data.to_json
         end
 
@@ -47,24 +53,34 @@ module IvcChampva
       # @param transaction_uuid [string] the UUID for the transaction
       # @param acting_user [string, nil] the acting user for the application
       # @param ves_request_data [IvcChampva::VesOhiRequest] preformatted request data
-      #
-      # @raise [NotImplementedError] This method is a stub pending VES API endpoint availability
-      def submit_7959c(_transaction_uuid, _acting_user, _ves_request_data)
-        # TODO: Implement once VES OHI endpoint is available
-        # Expected endpoint: "#{config.base_path}/ves-vfmp-app-svc/ohi-certifications"
-        raise NotImplementedError, 'VES OHI submission endpoint not yet implemented - awaiting VES API specification'
+      # @return [Faraday::Response] the response from VES
+      # @raise [VesApiError] if the response status is not 200
+      def submit_7959c(transaction_uuid, acting_user, ves_request_data)
+        resp = connection.post("#{config.base_path}/ves-vfmp-app-svc/champva-insurance-form") do |req|
+          req.headers = headers(transaction_uuid, acting_user, ohi_api_key)
+          req.body = ves_request_data.to_json
+        end
+
+        monitor.track_ves_response(transaction_uuid, resp.status, resp.body)
+
+        raise "response code: #{resp.status}, response body: #{resp.body}" unless resp.status == 200
+
+        resp
+      rescue => e
+        raise VesApiError, e.message.to_s
       end
 
       ##
-      # Assembles headers for the VES API request
+      # Assembles headers for VES API requests
       #
-      # @param transaction_uuid [string] the start date of the report
-      # @param acting_user [string, nil] the end date of the report
+      # @param transaction_uuid [string] the transaction UUID
+      # @param acting_user [string, nil] the acting user
+      # @param key [string] the API key to use
       # @return [Hash] the headers
-      def headers(transaction_uuid, acting_user)
+      def headers(transaction_uuid, acting_user, key)
         {
           :content_type => 'application/json',
-          'apiKey' => settings.api_key,
+          'apiKey' => key,
           'transactionUUId' => transaction_uuid.to_s,
           'acting-user' => acting_user.to_s
         }
