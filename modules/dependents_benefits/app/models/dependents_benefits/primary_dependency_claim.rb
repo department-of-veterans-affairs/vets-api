f6097624ef69e9696639f3fcf1293915a35cb1d3
Wayne Weibel <wayne.weibel@adhocteam.us>
2026-03-04 15:18:14 -0500

    DMT 133210 module attachments

--- modules/dependents_benefits/app/models/dependents_benefits/primary_dependency_claim.rb
+++ modules/dependents_benefits/app/models/dependents_benefits/primary_dependency_claim.rb
@@ -34,5 +34,18 @@ module DependentsBenefits
 
     # DependentsBenefit Form ID
     FORM = DependentsBenefits::FORM_ID
+
+    # Run after a claim is saved, this processes any files/supporting documents that are present
+    def process_attachments!
+      child_documents = parsed_form.dig('dependents_application', 'child_supporting_documents')
+      spouse_documents = parsed_form.dig('dependents_application', 'spouse_supporting_documents')
+      # add the two arrays together but also account for nil arrays
+      supporting_documents = [child_documents, spouse_documents].compact.reduce([], :|)
+      if supporting_documents.present?
+        files = PersistentAttachment.where(guid: supporting_documents.pluck('confirmation_code'))
+        files.find_each { |f| f.update(saved_claim_id: id) }
+      end
+    end
+
   end
 end
