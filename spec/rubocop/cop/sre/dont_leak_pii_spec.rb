adb2138ae4e34c91710f54b9299b1ca4849c24d6
Alastair Dawson <283834+kreek@users.noreply.github.com>
2026-03-04 08:47:28 -0800

    Expand DontLeakPii RuboCop cop with new detection patterns and specs

--- spec/rubocop/cop/sre/dont_leak_pii_spec.rb
+++ spec/rubocop/cop/sre/dont_leak_pii_spec.rb
@@ -48,6 +48,66 @@ RSpec.describe RuboCop::Cop::Sre::DontLeakPii do
     end
   end
 
+  describe 'hash values with PII (rendered response body leak vector)' do
+    it 'registers an offense for backend_response with .try(:original_body)' do
+      offenses = inspect_source(<<~RUBY)
+        { backend_response: error.try(:original_body) }
+      RUBY
+      expect(offenses.size).to eq(1)
+      expect(offenses.first.message).to include('[Play 01]')
+      expect(offenses.first.message).to include('original_body')
+    end
+
+    it 'registers an offense for vamf_body with @env.body' do
+      offenses = inspect_source(<<~RUBY)
+        { vamf_body: @env.body }
+      RUBY
+      expect(offenses.size).to eq(1)
+      expect(offenses.first.message).to include('[Play 01]')
+      expect(offenses.first.message).to include('body')
+    end
+
+    it 'registers an offense for backend_response with direct .original_body call' do
+      offenses = inspect_source(<<~RUBY)
+        { backend_response: error.original_body }
+      RUBY
+      expect(offenses.size).to eq(1)
+      expect(offenses.first.message).to include('[Play 01]')
+    end
+
+    it 'registers an offense for original_body key with .body value' do
+      offenses = inspect_source(<<~RUBY)
+        { original_body: response.body }
+      RUBY
+      expect(offenses.size).to eq(1)
+      expect(offenses.first.message).to include('[Play 01]')
+    end
+
+    it 'registers an offense for raw_body key with .try(:body)' do
+      offenses = inspect_source(<<~RUBY)
+        { raw_body: error.try(:body) }
+      RUBY
+      expect(offenses.size).to eq(1)
+      expect(offenses.first.message).to include('[Play 01]')
+    end
+
+    it 'registers an offense for response_body key with .try!(:original_body)' do
+      offenses = inspect_source(<<~RUBY)
+        { response_body: error.try!(:original_body) }
+      RUBY
+      expect(offenses.size).to eq(1)
+      expect(offenses.first.message).to include('[Play 01]')
+    end
+
+    it 'registers an offense for original_error key with .original_error method' do
+      offenses = inspect_source(<<~RUBY)
+        { original_error: error.original_error }
+      RUBY
+      expect(offenses.size).to eq(1)
+      expect(offenses.first.message).to include('[Play 01]')
+    end
+  end
+
   describe 'allowed patterns' do
     it 'does not register an offense for typed exception without interpolation' do
       expect_no_offenses(<<~RUBY)
@@ -64,6 +124,30 @@ RSpec.describe RuboCop::Cop::Sre::DontLeakPii do
     it 'does not register an offense for raise with interpolation that is not .body/.params' do
       expect_no_offenses(<<~'RUBY')
         raise "Failed: #{status_code}"
+      RUBY
+    end
+
+    it 'does not register an offense for safe hash with error class and status' do
+      expect_no_offenses(<<~RUBY)
+        { error_class: error.class.name, code: error.original_status }
+      RUBY
+    end
+
+    it 'does not register an offense for hash with non-suspicious keys even with .body' do
+      expect_no_offenses(<<~RUBY)
+        { content_type: response.content_type, status: response.status }
+      RUBY
+    end
+
+    it 'does not register an offense for hash with safe identifiers' do
+      expect_no_offenses(<<~RUBY)
+        { user_uuid: current_user.uuid, appointment_id: appt.id }
+      RUBY
+    end
+
+    it 'does not register an offense for meta hash with only safe fields' do
+      expect_no_offenses(<<~RUBY)
+        { meta: { error_class: e.class.name, status: 500 } }
       RUBY
     end
   end
