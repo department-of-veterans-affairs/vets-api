e764888648e504c3b7997a30457153960fd14837
nfstern02 <72567812+nfstern02@users.noreply.github.com>
2026-03-04 01:50:26 -0600

    Add logic to handle overflow when college name is too long

--- spec/factories/va0839.rb
+++ spec/factories/va0839.rb
@@ -9,6 +9,22 @@ FactoryBot.define do
     form { Rails.root.join('spec', 'fixtures', 'education_benefits_claims', '0839', 'overflow.json').read }
   end
 
+  factory :va0839_overflow1_degree, class: 'SavedClaim::EducationBenefits::VA0839', parent: :education_benefits do
+    form {
+      Rails.root.join(
+        'spec', 'fixtures', 'education_benefits_claims', '0839', 'overflow1_degree.json'
+      ).read
+    }
+  end
+
+  factory :va0839_overflow2_degree, class: 'SavedClaim::EducationBenefits::VA0839', parent: :education_benefits do
+    form {
+      Rails.root.join(
+        'spec', 'fixtures', 'education_benefits_claims', '0839', 'overflow2_degree.json'
+      ).read
+    }
+  end
+
   factory :va0839_withdrawal, class: 'SavedClaim::EducationBenefits::VA0839', parent: :education_benefits do
     form { Rails.root.join('spec', 'fixtures', 'education_benefits_claims', '0839', 'withdrawal.json').read }
   end
