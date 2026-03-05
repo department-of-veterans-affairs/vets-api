2ade09e233dfb347726c6f207368e1d172b9aa06
Amaar Fazlani <256628763+amfazlani92@users.noreply.github.com>
2026-03-04 18:36:07 -0600

    Re-raise exception if ibm upload fails so job can retry

--- app/sidekiq/simple_forms_api/mms/ibm_upload_job.rb
+++ app/sidekiq/simple_forms_api/mms/ibm_upload_job.rb
@@ -26,44 +26,35 @@ module SimpleFormsApi
 
       def perform(ibm_payload, form_number, confirmation_number)
         ibm_service = Ibm::Service.new
+
         ibm_response = ibm_service.upload_form(
           form: ibm_payload.to_json,
           guid: confirmation_number
         )
 
-        handle_response(ibm_response, form_number, confirmation_number)
+        handle_response(ibm_response)
       rescue => e
         Rails.logger.error(
           "Simple Forms API - MMS submission failed: #{e.message}",
           guid: confirmation_number,
           form_number:
         )
-
         raise
       end
 
       private
 
-      def handle_response(ibm_response, form_number, confirmation_number)
-        if ibm_response && ibm_response.status == 200
-          Rails.logger.info(
-            'Simple Forms API - MMS submission complete',
-            guid: confirmation_number,
-            form_number:
-          )
-        elsif ibm_response
-          Rails.logger.error(
-            "Simple Forms API - MMS submission failed: IBM upload returned status #{ibm_response.status}",
-            guid: confirmation_number,
-            form_number:
-          )
-        else
-          Rails.logger.error(
-            'Simple Forms API - MMS submission failed: IBM upload returned no response',
-            guid: confirmation_number,
-            form_number:
-          )
-        end
+      def handle_response(ibm_response)
+        return if ibm_response&.status == 200
+
+        reason =
+          if ibm_response
+            "IBM upload returned status #{ibm_response.status}"
+          else
+            'IBM upload returned no response'
+          end
+
+        raise StandardError, reason
       end
     end
   end
--- spec/sidekiq/simple_forms_api/mms/ibm_upload_job_spec.rb
+++ spec/sidekiq/simple_forms_api/mms/ibm_upload_job_spec.rb
@@ -5,109 +5,96 @@ require 'sidekiq/testing'
 require 'ibm/service'
 
 RSpec.describe SimpleFormsApi::Mms::IbmUploadJob, type: :job do
-  let(:ibm_payload) { double('IbmPayload', to_json: '{"key":"value"}') }
+  let(:ibm_payload) { { key: 'value' } }
   let(:form_number) { '12345' }
   let(:confirmation_number) { '67890' }
+
   let(:ibm_service) { instance_double(Ibm::Service) }
-  let(:faraday_response) { instance_double(Faraday::Response, status: 500, body: 'Internal Server Error') }
   let(:job) { described_class.new }
 
   before do
     allow(Ibm::Service).to receive(:new).and_return(ibm_service)
+    allow(Rails.logger).to receive(:info)
+    allow(Rails.logger).to receive(:error)
   end
 
   describe '#perform' do
     context 'when IBM upload is successful' do
+      let(:response) { instance_double(Faraday::Response, status: 200) }
+
       before do
-        allow(ibm_service).to receive(:upload_form).and_return(faraday_response)
-        allow(faraday_response).to receive(:status).and_return(200) # Simulating a successful 200 response
+        allow(ibm_service).to receive(:upload_form).and_return(response)
       end
 
-      it 'logs the success message' do
-        expect(Rails.logger).to receive(:info).with(
-          'Simple Forms API - MMS submission complete',
-          hash_including(guid: confirmation_number, form_number:)
-        )
+      it 'does not log an error and does not raise' do
+        expect do
+          job.perform(ibm_payload, form_number, confirmation_number)
+        end.not_to raise_error
 
-        described_class.new.perform(ibm_payload, form_number, confirmation_number)
+        expect(Rails.logger).not_to have_received(:error)
       end
     end
 
-    context 'when IBM upload fails with no response' do
+    context 'when IBM upload returns no response' do
       before do
         allow(ibm_service).to receive(:upload_form).and_return(nil)
       end
 
-      it 'logs the error message indicating no response from IBM' do
-        expect(Rails.logger).to receive(:error).with(
-          'Simple Forms API - MMS submission failed: IBM upload returned no response',
-          guid: confirmation_number,
-          form_number:
-        )
+      it 'logs the error and raises the exception' do
+        expect do
+          job.perform(ibm_payload, form_number, confirmation_number)
+        end.to raise_error(StandardError, 'IBM upload returned no response')
 
-        described_class.new.perform(ibm_payload, form_number, confirmation_number)
+        expect(Rails.logger).to have_received(:error).with(
+          'Simple Forms API - MMS submission failed: IBM upload returned no response',
+          hash_including(
+            guid: confirmation_number,
+            form_number:
+          )
+        )
       end
     end
 
     context 'when IBM upload returns a non-200 status code' do
+      let(:response) { instance_double(Faraday::Response, status: 500) }
+
       before do
-        allow(ibm_service).to receive(:upload_form).and_return(faraday_response)
-        allow(faraday_response).to receive(:status).and_return(500)
+        allow(ibm_service).to receive(:upload_form).and_return(response)
       end
 
-      it 'logs the error message with the response status' do
-        expect(Rails.logger).to receive(:error).with(
-          'Simple Forms API - MMS submission failed: IBM upload returned status 500',
-          guid: confirmation_number,
-          form_number:
-        )
+      it 'logs the error and raises the exception' do
+        expect do
+          job.perform(ibm_payload, form_number, confirmation_number)
+        end.to raise_error(StandardError, 'IBM upload returned status 500')
 
-        described_class.new.perform(ibm_payload, form_number, confirmation_number)
+        expect(Rails.logger).to have_received(:error).with(
+          'Simple Forms API - MMS submission failed: IBM upload returned status 500',
+          hash_including(
+            guid: confirmation_number,
+            form_number:
+          )
+        )
       end
     end
 
-    context 'when IBM upload raises an exception' do
+    context 'when IBM upload itself raises an exception' do
       before do
-        allow(Ibm::Service).to receive(:new).and_return(ibm_service)
-
-        allow(ibm_service).to receive(:upload_form).and_raise(
-          StandardError.new('Upload error')
-        )
+        allow(ibm_service).to receive(:upload_form)
+          .and_raise(StandardError, 'Upload error')
       end
 
       it 'logs the error message and re-raises the exception' do
-        expect(Rails.logger).to receive(:error).with(
-          'Simple Forms API - MMS submission failed: Upload error',
-          guid: confirmation_number,
-          form_number:
-        )
-
         expect do
-          described_class.new.perform(
-            ibm_payload,
-            form_number,
-            confirmation_number
-          )
+          job.perform(ibm_payload, form_number, confirmation_number)
         end.to raise_error(StandardError, 'Upload error')
-      end
-    end
 
-    context 'when job retries are exhausted' do
-      let(:ibm_payload) { { some: 'payload' } }
-      let(:form_number) { '21-4140' }
-      let(:confirmation_number) { 'abc123' }
-
-      it 'logs the retries exhausted message' do
-        msg = { 'args' => [ibm_payload, form_number, confirmation_number] }
-        ex = StandardError.new('Upload error')
-
-        expect(Rails.logger).to receive(:error).with(
-          'Sidekiq retries exhausted for SimpleForms::API::MMS::IbmUploadJob - StandardError: Upload error',
-          guid: confirmation_number,
-          form_number:
+        expect(Rails.logger).to have_received(:error).with(
+          'Simple Forms API - MMS submission failed: Upload error',
+          hash_including(
+            guid: confirmation_number,
+            form_number:
+          )
         )
-
-        described_class.sidekiq_retries_exhausted_block.call(msg, ex)
       end
     end
   end
