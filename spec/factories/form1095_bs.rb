a2dc9c1276e2a8b95a063e4ef35ddf4b9ffa7cf1
Kris Pethtel <kristopher.pethtel@va.gov>
2026-03-06 16:37:34 -0600

    1831: remove 1095b model (#26936)
    * 1831: remove 1095b model

--- spec/factories/form1095_bs.rb
+++ spec/factories/form1095_bs.rb
@@ -1,27 +1,6 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :form1095_b do
-    veteran_icn { '3456787654324567' }
-    tax_year { 2021 }
-    form_data {
-      {
-        first_name: 'John',
-        middle_name: 'Michael',
-        last_name: 'Smith',
-        last_4_ssn: '1234',
-        address: '123 Test st',
-        city: 'Hollywood',
-        state: 'CA',
-        zip_code: '12345',
-        country: 'USA',
-        is_beneficiary: false,
-        is_corrected: false,
-        coverage_months: Array.new(13, true)
-      }.to_json
-    }
-  end
-
   factory :enrollment_system_form1095_b, class: 'VeteranEnrollmentSystem::Form1095B::Form1095B' do
     tax_year { 2024 }
     first_name { 'John' }
