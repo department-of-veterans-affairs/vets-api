f8e663fda55c5dcb91b09836b07846bec3bafc99
nathanbwright <nathan.wright@oddball.io>
2026-03-06 15:49:48 -0700

    Remove unused param

--- modules/va_notify/lib/va_notify/service.rb
+++ modules/va_notify/lib/va_notify/service.rb
@@ -133,11 +133,11 @@ module VaNotify
       metadata.slice(:notification_type, :form_number)
     end
 
-    def log_error_details(error, context = {})
+    def log_error_details(error)
       log_message_to_rails(
         error.message,
         'error',
-        { url: config.base_path, body: error.try(:body) }.merge(context.compact)
+        { url: config.base_path, body: error.try(:body) }
       )
     end
 
